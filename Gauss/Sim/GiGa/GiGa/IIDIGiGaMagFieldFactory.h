0ea0d8dc6bf299919f2e60ab2ab8a14fe1f39a18
Vanya Belyaev <ivan.belyaev@cern.ch>
2001-03-22 14:19:54 +0000

    remove InterfaceID clash
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@90250 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 68c98c7ea76c940c046119bd109372df193618b4

--- Gauss/Sim/GiGa/GiGa/IIDIGiGaMagFieldFactory.h
+++ Gauss/Sim/GiGa/GiGa/IIDIGiGaMagFieldFactory.h
@@ -1,10 +1,11 @@
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Sim/GiGa/GiGa/IIDIGiGaMagFieldFactory.h,v 1.2 2001-03-22 14:19:54 ibelyaev Exp $ 
 #ifndef    GIGA_IIDIGIGAMagFieldFACTORY_H
 #define    GIGA_IIDIGIGAMagFieldFACTORY_H 1 
 
 
 
 /// unique ID 
-static const InterfaceID IID_IGiGaMagFieldFactory( 221 , 1 , 0 );
+static const InterfaceID IID_IGiGaMagFieldFactory( 223 , 1 , 0 );
 
 
 #endif  // GIGA_IIDIGIGAMagFieldFACTORY_H
