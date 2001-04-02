59607108b47ce9e366c6ed327d80f3f0c7ac6143
Pere Mato Vila <pere.mato@cern.ch>
2001-04-02 13:50:55 +0000

    Added mising file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@22611 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 015e1cef1d1c0851ec2fa8d54ad8463441b2050d

---
+++ LHCb/Event/VeloEvent/VeloEvent/VeloCLIDs.h
@@ -0,0 +1,18 @@
+/**
+
+   Class IDs for the VELO detector
+
+ */
+static const CLID CLID_RCluster2MCParticles       = 4001;
+static const CLID CLID_PhiCluster2MCParticles     = 4002;
+static const CLID CLID_MCParticle2RClusters       = 4003;
+static const CLID CLID_MCParticle2PhiClusters     = 4004;
+static const CLID CLID_RawVeloHit2MCVeloHits      = 4005;
+static const CLID CLID_RawVeloHit2MCParticles     = 4006;
+static const CLID CLID_MCVeloHit2MCParticles      = 4007;
+static const CLID CLID_MCParticle2RawVeloHits     = 4008;
+
+
+
+
+
