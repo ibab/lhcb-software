cf81d77a031175d09a4368058a955f7e35e752e4
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2001-05-03 12:08:35 +0000

    Moved here from ../src
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54412 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7681a625beea0a72587741cf06d5b6e1857047b5

---
+++ Lbcom/L0/L0Muon/L0Muon/MuonLayout.h
@@ -0,0 +1,66 @@
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/L0/L0Muon/L0Muon/MuonLayout.h,v 1.1 2001-05-03 12:08:34 atsareg Exp $
+
+#ifndef L0MUON_MUONLAYOUT_H
+#define L0MUON_MUONLAYOUT_H 1   
+
+// Include files
+#include <vector>
+#include "GaudiKernel/SmartDataPtr.h"  
+#include "GaudiKernel/MsgStream.h"  
+
+/** @class MuonLayout MuonLayout.h MuonLayout.h 
+   
+   Contains a Muon station logical layout
+   
+   @author A.Tsaregorodtsev
+   @date 6 April 2001 
+*/
+
+// Forward declarations
+
+class MuonTile;
+
+class MuonLayout {
+
+public:
+  /// Constructors
+  MuonLayout();
+  /// Constructor taking X and Y granularities
+  MuonLayout(int xgrid, int ygrid);
+  
+  /// Destructor
+  ~MuonLayout();
+    
+  /// Accessor to X granularity
+  int xGrid() const { return m_xgrid; }
+  /// Accessor to Y granularity
+  int yGrid() const { return m_ygrid; }  
+  
+  /** find a vector of its MuonTile's around the given MuonTile 
+      defined in other MuonLayout within given limits
+      
+      @param pad : central for the search
+      @param layout: MuonLayout in which the seed pad is defined
+      @param areaX : limits of the search area in X
+      @param areaY : limits of the search area in Y
+  */          
+  std::vector<MuonTile> tiles(const MuonTile& pad, 
+			      int areaX = 0,
+			      int areaY = 0);
+  /// find a tile containing the argument tile
+  MuonTile contains(const MuonTile& pad);
+  /// find magnification factor of pads in the given region
+  int rfactor (int nr) const ;
+  /// find region for the given pad indices
+  int region (int bx, int by) const ;
+  
+private:
+
+  int m_xgrid; 
+  int m_ygrid;
+    
+};   
+
+MsgStream& operator<<(MsgStream& log, const MuonLayout& ml);
+
+#endif    // L0MUON_MUONLAYOUT_H
