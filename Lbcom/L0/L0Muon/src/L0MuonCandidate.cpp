aba8b175a72c4fa33b50d25b0d2ce69c0a4c0018
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2001-06-07 16:44:08 +0000

    Files are moved to either Lib or component directories after subdivision of the library in the component and Lib parts  Removed Files:  	L0MuonCandidate.cpp L0Muon_dll.cpp L0Muon_load.cpp L0mPad.cpp  	L0mPadBuilder.cpp L0mPadBuilder.h L0mTower.cpp L0mTower.h  	L0mTrigger.cpp L0mTrigger.h L0mTriggerProc.cpp  	L0mTriggerProc.h MuonLayout.cpp MuonPadLayout.cpp  	MuonPadLayout.h MuonTile.cpp Parameter.cpp Parameter.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54437 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f03a1775da70a0ede0beae6f9c6bd87513414d93

--- Lbcom/L0/L0Muon/src/L0MuonCandidate.cpp
+++
@@ -1,5 +0,0 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/L0/L0Muon/src/L0MuonCandidate.cpp,v 1.2 2001-05-03 09:12:19 atsareg Exp $
-
-#include "L0Muon/L0MuonCandidate.h"
-
-extern const CLID& CLID_L0MuonCandidate = 6002 ;
