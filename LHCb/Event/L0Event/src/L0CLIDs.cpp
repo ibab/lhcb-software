90f4baa022ac0ed6650fb473206ad43445274610
ocallot <ocallot@cern.ch>
2001-06-27 12:10:44 +0000

    Try again to remove obsolete files
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@32219 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b09164f07d25bbbcb70c9d6a27da086eed846de

--- LHCb/Event/L0Event/src/L0CLIDs.cpp
+++
@@ -1,8 +0,0 @@
-#include "GaudiKernel/ClassID.h"
-
-// Class IDs for the Level 0 detector start here
-const CLID& CLID_Level0                 = 8000;  // 8001 -> 7999 reserved for Level 0 stuff
-const CLID& CLID_Level0Calo2x2Sum       = 8001;  // FrontEnd Level 0 calorimeter 2x2 sum
-const CLID& CLID_Level0CaloClus         = 8002;  // FrontEnd Level 0 calorimeter cluster candidates
-const CLID& CLID_Level0MuonTrack        = 8003;  // FrontEnd Level 0 muon tracks
-const CLID& CLID_Level0PileUpVeto       = 8004;  // FrontEnd Level 0 pileup veto decision
