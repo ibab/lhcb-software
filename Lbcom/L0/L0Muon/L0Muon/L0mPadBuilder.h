770b2d6cddcc1ee725f46eb53cdc9705c9af2b1d
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2001-07-09 19:21:55 +0000

    Use MuonLogicalPadLayout instead of MuonPadLayout for building pads
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54477 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2bde0475ed056ee1b3f0087d92bc51dd671b102e

--- Lbcom/L0/L0Muon/L0Muon/L0mPadBuilder.h
+++ Lbcom/L0/L0Muon/L0Muon/L0mPadBuilder.h
@@ -1,4 +1,4 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/L0/L0Muon/L0Muon/L0mPadBuilder.h,v 1.1 2001-06-07 16:46:14 atsareg Exp $
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/L0/L0Muon/L0Muon/L0mPadBuilder.h,v 1.2 2001-07-09 19:21:55 atsareg Exp $
 
 #ifndef L0MUON_L0MPADBUILDER_H
 #define L0MUON_L0MPADBUILDER_H 1
@@ -11,7 +11,7 @@
 #include "GaudiKernel/Property.h"
 
 // Forward declarations
-class MuonPadLayout;
+class MuonLogicalPadLayout;
 
 /** @class L0mPadBuilder L0mPadBuilder.h L0mPadBuilder.h 
 
@@ -29,9 +29,10 @@ public:
   StatusCode initialize();
   StatusCode execute();
   StatusCode finalize();
+  
 private:
 
-  MuonPadLayout* m_layout;
+  MuonLogicalPadLayout* m_layout;
   bool m_layout_xml;
   std::vector<double> m_zStation;
   std::vector<double> m_basicCellX;
