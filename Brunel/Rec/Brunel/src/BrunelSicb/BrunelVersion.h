682e11e043e2e10da0373081c261a73475176d57
cattaneb <cattaneb@cern.ch>
2001-09-13 12:01:35 +0000

    for v7r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@28341 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 54c5ac0046ecf2296834a24a471ca1a59b1e40ba

--- Brunel/Rec/Brunel/src/BrunelSicb/BrunelVersion.h
+++ Brunel/Rec/Brunel/src/BrunelSicb/BrunelVersion.h
@@ -1,7 +1,7 @@
-C// $Id: BrunelVersion.h,v 1.7 2001-08-31 11:33:37 cattaneb Exp $
+C// $Id: BrunelVersion.h,v 1.8 2001-09-13 12:01:35 cattaneb Exp $
 #ifndef BRUNELVERSION_H
 #define BRUNELVERSION_H 1
 
 C/// Brunel version number (= 10000*major (v) + 100* minor (r) number)
-#define BrunelVersion 60000
+#define BrunelVersion 70100
 #endif // BRUNELVERSION_H
