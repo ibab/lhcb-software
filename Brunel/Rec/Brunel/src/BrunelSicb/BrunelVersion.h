0bf0a16760ecbf92894948bdf3fce197b6130f77
Marco Cattaneo <marco.cattaneo@cern.ch>
2001-10-16 14:25:50 +0000

    changes for v8
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@28345 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9f86680668e8bbdb606bfa64f185800ddbb7dc81

--- Brunel/Rec/Brunel/src/BrunelSicb/BrunelVersion.h
+++
@@ -1,7 +0,0 @@
-C// $Id: BrunelVersion.h,v 1.8 2001-09-13 12:01:35 cattaneb Exp $
-#ifndef BRUNELVERSION_H
-#define BRUNELVERSION_H 1
-
-C/// Brunel version number (= 10000*major (v) + 100* minor (r) number)
-#define BrunelVersion 70100
-#endif // BRUNELVERSION_H
