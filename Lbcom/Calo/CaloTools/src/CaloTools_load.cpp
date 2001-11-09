23f4832a8f1e7adb14cccb65840d43de966b16db
Vanya Belyaev <ivan.belyaev@cern.ch>
2001-11-09 16:25:02 +0000

    add new tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@38535 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ae62c8de9923d3a82de82be329262351db393ab7

--- Lbcom/Calo/CaloTools/src/CaloTools_load.cpp
+++ Lbcom/Calo/CaloTools/src/CaloTools_load.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloTools_load.cpp,v 1.2 2001-11-08 10:58:34 ibelyaev Exp $
+// $Id: CaloTools_load.cpp,v 1.3 2001-11-09 16:25:02 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2001/11/08 10:58:34  ibelyaev
+//  new tools are added for selection of subclusters within the cluster
+//
 // Revision 1.1  2001/11/04 15:30:40  ibelyaev
 // add concrete tool
 // 
@@ -22,6 +25,7 @@ DECLARE_FACTORY_ENTRIES(CaloTools) {
   DECLARE_TOOL( ClusterCovarianceMatrixTool  );
   DECLARE_TOOL( SubClusterSelectorAll        );
   DECLARE_TOOL( SubClusterSelector3x3        );
+  DECLARE_TOOL( SubClusterSelectorMax2x2     );
   DECLARE_TOOL( SubClusterSelectorSwissCross );
 
 };
