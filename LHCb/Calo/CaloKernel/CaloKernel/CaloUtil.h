fdf7c3d53bb38458ddf9a36b1183083ecb413f59
Vanya Belyaev <ivan.belyaev@cern.ch>
2001-11-26 18:57:19 +0000

    new utilities added
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38570 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7f8c5f56d1313524c3cd13b3687a03a60612f840

--- LHCb/Calo/CaloKernel/CaloKernel/CaloUtil.h
+++ LHCb/Calo/CaloKernel/CaloKernel/CaloUtil.h
@@ -1,8 +1,11 @@
-// $Id: CaloUtil.h,v 1.1.1.1 2001-11-25 14:07:38 ibelyaev Exp $ 
+// $Id: CaloUtil.h,v 1.2 2001-11-26 18:57:19 ibelyaev Exp $ 
 // ===========================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ===========================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.1.1.1  2001/11/25 14:07:38  ibelyaev
+// New Package: substitution of the  previous CaloGen package
+//
 // Revision 1.1  2001/07/29 10:54:42  ibelyaev
 // add new namespace with a number of useful utilities
 // 
@@ -100,7 +103,54 @@ namespace CaloUtil
     inline TYPE* operator() ( TYPE1* obj ) const 
     { return 0 == obj ? 0 : dynamic_cast<TYPE*> ( obj ) ; };
   };
-  
+
+  /** @class SafeCast 
+   * 
+   *  casting from one object TYPE1 to another TYPE2
+   *
+   *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
+   *  @date 23/07/2001
+   */
+  template <class TYPE1,class TYPE2> 
+  class SafeCast: public std::unary_function<TYPE1,TYPE2>
+  {
+  public:
+    /** casting from TYPE1 to TYPE2 
+     *  @param   obj  object of TYPE1 
+     *  @return  dynamic_cast<TYPE2> (obj) 
+     */ 
+    inline TYPE2 operator() ( TYPE1 obj ) const
+    { return ( !obj ) ? (TYPE2) 0 : dynamic_cast<TYPE2>(obj) ; } 
+    ///
+  };
+
+  /** @class CmpSmartRef
+   *  
+   *  compare smart references via resolving to raw  pointers 
+   *  
+   *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
+   *  @date 23/07/2001
+   */
+  template <class TYPE> 
+  class CmpSmartRef: 
+    public std::binary_function<const SmartRef<TYPE>,const SmartRef<TYPE>,bool>
+  {
+  public:
+    /**  compare smart references via resolving to raw pointers 
+     *   @param o1 first object 
+     *   @param o2 second object 
+     *   @return result of comparison (equality test) between resolved pointers
+     */
+    inline bool operator() 
+      ( const SmartRef<TYPE> o1 , 
+        const SmartRef<TYPE> o2 ) const
+    {
+      const TYPE* p1 = o1 ; 
+      const TYPE* p2 = o2 ;
+      return p1 == p2 ; 
+    }
+  };
+
 }; ///< end of namespace CaloUtil 
 
 // ===========================================================================
