de9e4f89be12fe507a4b2aac5744c024113715e0
Andrea Valassi <andreav@cern.ch>
2001-11-29 13:56:24 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@2622 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 43274593c85abc321a5857c14839d3679e105cfe

--- LHCb/Ex/DetCondExample/src/BootDetectorStore.h
+++ LHCb/Ex/DetCondExample/src/BootDetectorStore.h
@@ -1,17 +1,25 @@
-//$Id: BootDetectorStore.h,v 1.1 2001-11-29 13:16:04 andreav Exp $
+//$Id: BootDetectorStore.h,v 1.2 2001-11-29 13:56:24 andreav Exp $
 #ifndef DETCONDEXAMPLE_BOOTDETECTORSTORE_H
 #define DETCONDEXAMPLE_BOOTDETECTORSTORE_H 1
 
 // Base class
 #include "GaudiKernel/Algorithm.h"
 
+// Type definitions
+#include "GaudiKernel/ClassID.h"
+
+//Forward declarations
+class IAddressCreator;
+class IDataManagerSvc;
+
 ///---------------------------------------------------------------------------
 /** @class BootDetectorStore BootDetectorStore.h DetCondExample/BootDetectorStore.h
 
-    Simple algorithm to boot the detector data store.
+    Simple algorithm to bootstrap the detector data store.
+    Emulates what will eventually be done by the XmlCnvSvc.
 
     @author Andrea Valassi 
-    @date August 2001
+    @date November 2001
 *///--------------------------------------------------------------------------
 
 class BootDetectorStore : public Algorithm {
@@ -26,9 +34,42 @@ class BootDetectorStore : public Algorithm {
   StatusCode execute();
   StatusCode finalize();
   
+ private:
+
+  // Private helper methods
+
+  /// Convert the folder name in the CondDB to the transient data store path
+  StatusCode i_getNameInCondDB   ( std::string&       folderName,
+				   const std::string& path );
+  
+  /// Convert the transient data store path to the folder name in the CondDB
+  StatusCode i_getNameInStore    ( std::string&       path,
+				   const std::string& folderName );
+  
+  /// Register a condition in the detector store by CondDB folder name.
+  /// In the address, specify the global tag (owned by the ConditionsDBCnvSvc),
+  /// the technology type for the strings stored in the CondDB and the classID.
+  StatusCode i_registerCondition ( const std::string&   folderName,
+				   const CLID&          classID,
+				   const unsigned char& type );
+  
+  /// Register a condition in the detector store by CondDB folder name.
+  /// If not specifed, type and classID are discovered at runtime in the CondDB
+  StatusCode i_registerCondition ( const std::string&   folderName );
+  
+ private:
+
+  // Private data members
+
+  /// TDS root path for the naming convention (default is "/dd")
+  std::string m_conditionStoreRoot;
+
+  /// Handle to the IDataManagerSvc interface of the detector data service
+  IDataManagerSvc* m_detDataManager;
+
+  /// Handle to the IAddressCreator interface of the ConditionsDBCnvSvc
+  IAddressCreator* m_conditionsDBCnvSvc;
+
 };
 
 #endif    // DETCONDEXAMPLE_BOOTDETECTORSTORE_H
-
-
-
