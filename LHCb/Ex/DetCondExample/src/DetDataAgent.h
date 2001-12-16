06693717b7d5d59bf8fa7f520cb9ce36db2980a1
Andrea Valassi <andreav@cern.ch>
2001-12-16 16:27:24 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@2652 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 617cdb28756f5248f35bd8c80be27ff223ecb483

--- LHCb/Ex/DetCondExample/src/DetDataAgent.h
+++ LHCb/Ex/DetCondExample/src/DetDataAgent.h
@@ -1,34 +1,32 @@
-//$Id: DetDataAgent.h,v 1.2 2001-11-23 18:14:20 andreav Exp $
+//$Id: DetDataAgent.h,v 1.3 2001-12-16 16:27:24 andreav Exp $
 #ifndef DETCONDEXAMPLE_DETDATAAGENT_H
 #define DETCONDEXAMPLE_DETDATAAGENT_H
 
-#include "GaudiKernel/DataObject.h"
 #include "GaudiKernel/IDataStoreAgent.h"
-#include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/IRegistry.h"
+#include "GaudiKernel/MsgStream.h"
 
 class IMessageSvc;
 
 ///---------------------------------------------------------------------------
 /** @class DetDataAgent DetDataAgent.h DetCondExample/DetDataAgent.h
 
-    Simple agent to dump the contents of DetDataSvc TDS.
-    Copied from Examples/DDexample.
+    Simple agent to dump the contents of DetectorDataSvc data store.
+    Copied from Ex/DetDescExample.
 
-    @author Andrea Valassi 
-    @date February 2001
+    @date December 2001
 *///--------------------------------------------------------------------------
 
-class DetDataAgent : virtual public IDataStoreAgent                        {
+class DetDataAgent : virtual public IDataStoreAgent {
 
  public:
-  
-  DetDataAgent( IMessageSvc* msgSvc );
-  
-  virtual ~DetDataAgent();
-  
-  virtual bool analyse( IRegistry* dir, int level );
-  
+
+  DetDataAgent( IMessageSvc* msgSvc ) : m_msgSvc( msgSvc ) {};
+
+  virtual ~DetDataAgent() {};
+
+  virtual bool analyse(IRegistry* dir, int level );
+
  private:
   
   IMessageSvc* m_msgSvc;
@@ -37,34 +35,12 @@ class DetDataAgent : virtual public IDataStoreAgent                        {
 
 //---------------------------------------------------------------------------
 
-inline DetDataAgent::DetDataAgent( IMessageSvc* msgSvc )
-  : m_msgSvc( msgSvc ) {
-}
-
-//---------------------------------------------------------------------------
-
-inline DetDataAgent::~DetDataAgent( ) { }
-
-//---------------------------------------------------------------------------
-
-inline bool DetDataAgent::analyse( IRegistry* dir, int level ) {
-  
-  MsgStream log( m_msgSvc, "DetDataAgent" );
-  
-  if ( level == 0 ) 
-    log << MSG::INFO 
-	<< "Dumping the contents of the DetectorDataSvc store" << endreq;
-
-  log << MSG::INFO << "(" << level << ")" << dir->identifier();
-  
-  if( 0 != dir->object() ) {
-    log << " with class ID " << dir->object()->clID() << endreq;
-  } else {
-    log << endreq;
-  }
-  
+inline bool DetDataAgent::analyse(IRegistry* dir, int /*level*/ ){
+  MsgStream log( m_msgSvc, "detDataAgent" );
+  log << MSG::INFO << dir->identifier()
+      << ( dir->object() ? " [loaded] " : " " )
+      << endreq; 
   return true;
-
 }
 
 //---------------------------------------------------------------------------
