253f52047acdaf265e8ff3322949e8d1b39885d3
Pere Mato Vila <pere.mato@cern.ch>
2002-01-21 09:54:25 +0000

    see release notes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@2691 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9ad4ed511a80d75f77d45e7673aa7a08f1bf4faf

---
+++ LHCb/GaudiObjDesc/src/DaDiEnum.h
@@ -0,0 +1,85 @@
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/GaudiObjDesc/src/DaDiEnum.h,v 1.1 2002-01-21 09:54:25 mato Exp $
+#ifndef DADIENUM_H 
+#define DADIENUM_H 1
+
+// Include files
+#include "dom/DOMString.hpp"
+
+/** @class DaDiAttribute DaDiAttribute.h
+ *  
+ *
+ *  @author Stefan Roiser
+ *  @date   14/06/2001
+ */
+class DaDiEnum {
+public:
+  /// Standard constructor
+  DaDiEnum() {}; 
+
+  virtual ~DaDiEnum() {}; ///< Standard destructor
+
+  DOMString name();
+  void setName(DOMString value);
+    
+  DOMString desc();
+  void setDesc(DOMString value);
+  
+  DOMString value();
+  void setValue(DOMString value);
+
+  DOMString access();
+  void setAccess(DOMString value);
+  
+protected:
+
+private:
+
+  DOMString m_name, 
+            m_desc, 
+            m_value, 
+            m_access;
+
+};
+
+
+inline DOMString DaDiEnum::name()
+{
+  return m_name;
+}
+
+inline void DaDiEnum::setName(DOMString value)
+{
+  m_name = value;
+}
+
+inline DOMString DaDiEnum::desc()
+{
+  return m_desc;
+}
+
+inline void DaDiEnum::setDesc(DOMString value)
+{
+  m_desc = value;
+}
+
+inline DOMString DaDiEnum::value()
+{
+  return m_value;
+}
+
+inline void DaDiEnum::setValue(DOMString value)
+{
+  m_value = value;
+}
+
+inline DOMString DaDiEnum::access()
+{
+  return m_access;
+}
+
+inline void DaDiEnum::setAccess(DOMString value)
+{
+  m_access = value;
+}
+
+#endif // DADIENUM_H
