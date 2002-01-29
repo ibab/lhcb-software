1d3b2be853aba01b7fc1a095501238352f7ffe1b
Pere Mato Vila <pere.mato@cern.ch>
2002-01-29 13:34:14 +0000

    adding scripts for copying gdd.dtd to local xml-dir
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@2717 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 54a9f5f0f8aa3b0cf881666d813da749e0781f16

---
+++ LHCb/GaudiObjDesc/cmt/setDTD.bat
@@ -0,0 +1,9 @@
+@echo off
+
+if exist ..\xml goto :copydtd else goto :end
+
+:copydtd
+  echo Copying gdd.dtd to local xml-directory
+  copy %GAUDIOBJDESCROOT%\xml_files\gdd.dtd ..\xml\gdd.dtd 
+
+:end
