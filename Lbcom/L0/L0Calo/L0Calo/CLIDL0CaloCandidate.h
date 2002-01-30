1c481c38cedc45af13c8d9524896074d648d5cec
ocallot <ocallot@cern.ch>
2002-01-30 16:09:57 +0000

    remove obsolete header file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54555 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cd2d118162367d1fbe643f7bfa909455c2625ae5

--- Lbcom/L0/L0Calo/L0Calo/CLIDL0CaloCandidate.h
+++
@@ -1,7 +0,0 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/L0/L0Calo/L0Calo/CLIDL0CaloCandidate.h,v 1.1 2001-06-12 14:28:01 ocallot Exp $
-#ifndef L0CALO_CLIDL0CALOCANDIDATE_H 
-#define L0CALO_CLIDL0CALOCANDIDATE_H 1
-
-static const CLID& CLID_L0CaloCandidate  = 6001 ;
-
-#endif // L0CALO_CLIDL0CALOCANDIDATE_H
