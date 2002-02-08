e262d2f4069cdab89cb4386c117ef686b454d9ca
Gloria Corti <gloria.corti@cern.ch>
2002-02-08 18:21:16 +0000

    moved to DaVinciSicb
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62265 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c1162809faeeeb5a576d36193e5f32a871457731

--- Phys/Phys/DaVinciTools/src/static/DaVinciVersion.h
+++
@@ -1,8 +0,0 @@
-C// $Id: DaVinciVersion.h,v 1.1.1.1 2001-07-09 09:28:42 gcorti Exp $
-#ifndef DAVINCIVERSION_H
-#define DAVINCIVERSION_H 1
-
-C/// DaVinci version number 
-C///(= 100000*major (v) + 100* minor (r) number + patch (p) number)
-#define DaVinciVersion 100000
-#endif // DAVINCIVERSION_H
