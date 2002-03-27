58160101510acc12cf2aaa1f00a6c7ddf7b15e4a
Marco Cattaneo <marco.cattaneo@cern.ch>
2002-03-27 14:11:15 +0000

    add LHCbStreams.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46620 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a755b39f18559a9d99bba2219646985a5a863dfb

---
+++ LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
@@ -0,0 +1,9 @@
+// $Id: LHCbStreams.h,v 1.1 2002-03-27 14:11:15 cattanem Exp $
+#ifndef KERNEL_LHCBSTREAMS_H 
+#define KERNEL_LHCBSTREAMS_H 1
+
+// Include files for streams definitions
+#include "LHCbKernel/SerialiseStl.h" // also includes GaudiKernel/StreamBuffer.h
+#include "LHCbKernel/CLHEPStreams.h" // also includes GaudiKernel/MsgStream.h
+
+#endif // KERNEL_LHCBSTREAMS_H
