41e5b7bae5d30474a8d0e9b2525c0319799252f3
Marco Cattaneo <marco.cattaneo@cern.ch>
2002-03-27 14:15:38 +0000

    fix typo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46621 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7bfdb7d25f7700d496fec1d25a45caadd6c03dd

--- LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
+++ LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
@@ -1,9 +1,9 @@
-// $Id: LHCbStreams.h,v 1.1 2002-03-27 14:11:15 cattanem Exp $
+// $Id: LHCbStreams.h,v 1.2 2002-03-27 14:15:38 cattanem Exp $
 #ifndef KERNEL_LHCBSTREAMS_H 
 #define KERNEL_LHCBSTREAMS_H 1
 
 // Include files for streams definitions
-#include "LHCbKernel/SerialiseStl.h" // also includes GaudiKernel/StreamBuffer.h
-#include "LHCbKernel/CLHEPStreams.h" // also includes GaudiKernel/MsgStream.h
+#include "Kernel/SerialiseStl.h" // also includes GaudiKernel/StreamBuffer.h
+#include "Kernel/CLHEPStreams.h" // also includes GaudiKernel/MsgStream.h
 
 #endif // KERNEL_LHCBSTREAMS_H
