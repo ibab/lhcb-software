1cf3ef7dfd715c3bc2229afa6205f90f0047cd2c
Marco Cattaneo <marco.cattaneo@cern.ch>
2002-03-28 12:14:29 +0000

    Add ostream
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46627 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 40c3a1364a5b370096af221e521283a8d0603866

--- LHCb/Kernel/LHCbKernel/Kernel/SerializeStl.h
+++ LHCb/Kernel/LHCbKernel/Kernel/SerializeStl.h
@@ -1,4 +1,4 @@
-// $Id: SerializeStl.h,v 1.1 2002-03-19 08:13:09 cattanem Exp $
+// $Id: SerializeStl.h,v 1.2 2002-03-28 12:14:29 cattanem Exp $
 /*
     Small header file to serialize vectors and lists
     to standard Gaudi StreamBuffer objects
@@ -9,6 +9,8 @@
 #include "GaudiKernel/StreamBuffer.h"
 #include <vector>
 #include <list>
+
+static const int NUMBERS_PER_LINE = 6;
 
 // Output serialize a vector of items
 template <class T> inline 
@@ -30,6 +32,20 @@ StreamBuffer& operator >> (StreamBuffer& s, std::vector<T>& v)  {
     T    temp;
     s >> temp;
     v.push_back(temp);
+  }
+  return s;
+}
+
+// Output operator (ASCII)
+template <class T> inline
+std::ostream& operator<< ( std::ostream& s, const std::vector<T>& v ) 
+{
+  int cnt = 0;
+  for( std::vector<T>::const_iterator i=v.begin(); i!=v.end(); i++, cnt=0 ) {
+    s << "[" << cnt << "]="; 
+    s.width(12); 
+    s << (*i) << " ";
+    if ( (++cnt)%NUMBERS_PER_LINE == 0 )  s << endmsg;
   }
   return s;
 }
@@ -58,4 +74,18 @@ StreamBuffer& operator >> (StreamBuffer& s, std::list<T>& l)  {
   return s;
 }
 
+// Output operator (ASCII)
+template <class T> inline
+std::ostream& operator<< ( std::ostream& s, const std::list<T>& l ) 
+{
+  int cnt = 0;
+  for ( std::list<T>::const_iterator i = l.begin(); i != l.end(); i++ )  {
+    s << "[" << cnt << "]="; 
+    s.width(12); 
+    s << (*i) << " ";
+    if ( (++cnt)%NUMBERS_PER_LINE == 0 )  s << endmsg;
+  }
+  return s;
+}
+  
 #endif LHCBKERNEL_SERIALIZESTL_H
