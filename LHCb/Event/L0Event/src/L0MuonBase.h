3407e57322f54d0b77e0bc6181c6383970a9f1c5
ocallot <ocallot@cern.ch>
2002-04-04 05:52:34 +0000

    Added L0Muon, some restructuring
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@32512 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e4b26ba236b22b09b28a536837256c88bb8b0ff2

---
+++ LHCb/Event/L0Event/src/L0MuonBase.h
@@ -0,0 +1,59 @@
+// $Id: L0MuonBase.h,v 1.1 2002-04-04 05:52:34 ocallot Exp $
+//
+#ifndef      L0MUON_L0MUONBASE_H
+#define      L0MUON_L0MUONBASE_H 1
+
+
+/** @class L0MuonBase L0MuonBase.h L0Muon/L0MuonBase.h
+
+    Namespace for code/decode rules of L0MuonCandidate class
+
+    @author  Andrei Tsaregorodtsev
+    @date    9/01/2002
+*/
+
+
+namespace L0MuonBase {
+
+  typedef unsigned int   ContentType ; 
+  //
+  static const unsigned int BitsBCID         = 8 ;
+  static const unsigned int BitsStatus       = 4 ;
+  static const unsigned int BitsPtSign       = 1 ;
+  static const unsigned int BitsPt           = 7 ;
+  static const unsigned int BitsRegion       = 2 ; 
+  static const unsigned int BitsX            = 6 ;
+  static const unsigned int BitsY            = 4 ;
+  //
+  static const unsigned int ShiftX        = 0 ;
+  static const unsigned int ShiftY        = ShiftX       + BitsX  ;
+  static const unsigned int ShiftRegion   = ShiftY       + BitsY  ;
+  static const unsigned int ShiftPt       = ShiftRegion  + BitsRegion ;  
+  static const unsigned int ShiftPtSign   = ShiftPtSign  + BitsPtSign ;  
+  static const unsigned int ShiftStatus   = ShiftPt      + BitsPt ;
+  static const unsigned int ShiftBCID     = ShiftStatus  + BitsStatus  ;
+  //
+  static const ContentType  MaskX     =
+  ( ( ( (ContentType) 1 ) << BitsX  ) - 1  ) << ShiftX  ;
+  static const ContentType  MaskY     =
+  ( ( ( (ContentType) 1 ) << BitsY  ) - 1  ) << ShiftY  ;
+  static const ContentType  MaskRegion     =
+  ( ( ( (ContentType) 1 ) << BitsRegion  ) - 1  ) << ShiftRegion  ;
+  static const ContentType  MaskPt     =
+  ( ( ( (ContentType) 1 ) << BitsPt  ) - 1  ) << ShiftPt   ;
+  static const ContentType  MaskPtSign     =
+  ( ( ( (ContentType) 1 ) << BitsPtSign  ) - 1  ) << ShiftPtSign   ;
+  static const ContentType  MaskStatus     =
+  ( ( ( (ContentType) 1 ) << BitsStatus  ) - 1  ) << ShiftStatus  ;
+  static const ContentType  MaskBCID     =
+  ( ( ( (ContentType) 1 ) << BitsBCID  ) - 1  ) << ShiftBCID  ;
+  
+  // Pt decoding constants
+  static const double PT_BIN_WIDTH = 40.;
+  // X decoding constant
+  static const double X_BASIC_CELL = 1.;
+  // Y decoding constant
+  static const double Y_BASIC_CELL = 2.5;
+  
+};
+#endif   // L0MUON_L0MUONBASE_H
---
+++ LHCb/Event/L0Event/src/L0MuonCandidate.cpp
@@ -0,0 +1,75 @@
+// $Id: L0MuonCandidate.cpp,v 1.1 2002-04-04 05:52:34 ocallot Exp $
+
+#include <vector>
+#include "Event/L0MuonCandidate.h"
+#include "L0MuonBase.h"
+
+L0MuonCandidate::L0MuonCandidate(int status) {
+  setStatus(status);
+}
+
+L0MuonCandidate::L0MuonCandidate(double pt, 
+                                 double theta, 
+				 double phi, 
+				 const std::vector<MuonTileID>& pads, 
+				 int status) {
+  setPt(pt);
+  m_theta = theta;
+  m_phi = phi;
+  std::vector<MuonTileID>::const_iterator ip;
+  for(ip = pads.begin(); ip!=pads.end(); ip++ ) {
+    m_pads.push_back(*ip);
+  }	
+  setStatus(status);			 
+}				 
+
+double L0MuonCandidate::pt() const
+{
+
+  int tmp = (m_bitcode & L0MuonBase::MaskPt) >> L0MuonBase::ShiftPt;
+  double rtmp = L0MuonBase::PT_BIN_WIDTH * ((double)tmp + 0.5);
+
+  int sign = (m_bitcode & L0MuonBase::MaskPtSign)
+               >> L0MuonBase::ShiftPtSign;
+  return  sign ? rtmp : -rtmp ;
+
+} 
+
+int L0MuonCandidate::status() const
+{
+  int tmp = (m_bitcode & L0MuonBase::MaskStatus) >> L0MuonBase::ShiftStatus;
+  return  tmp ;
+} 
+
+void L0MuonCandidate::setBit(int value, 
+                             int shift, 
+			     int mask) {
+  int tmp1 , tmp2 ;
+  tmp1 = ( value << shift ) & mask ; 
+  tmp2 = m_bitcode & ~mask ; 
+  m_bitcode = ( tmp1 | tmp2 ) ;			     
+}
+
+void L0MuonCandidate::setPt(double pt) {
+
+  if ( pt > 0.) {
+    setBit(1,L0MuonBase::ShiftPtSign,L0MuonBase::MaskPtSign);
+  } else {
+    setBit(0,L0MuonBase::ShiftPtSign,L0MuonBase::MaskPtSign);
+  }
+  unsigned int roundedPt = int((fabs(pt)+L0MuonBase::PT_BIN_WIDTH/2.)/
+                                              L0MuonBase::PT_BIN_WIDTH);
+  if ( roundedPt > L0MuonBase::BitsPt ) roundedPt = L0MuonBase::BitsPt;
+  setBit(roundedPt,L0MuonBase::ShiftPt,L0MuonBase::MaskPt);
+  
+}
+
+void L0MuonCandidate::setStatus(int status) {
+  setBit(status,L0MuonBase::ShiftStatus,L0MuonBase::MaskStatus);
+}
+
+void L0MuonCandidate::setPadM1(const MuonTileID& pad) {
+  setBit(pad.region(),L0MuonBase::ShiftRegion,L0MuonBase::MaskRegion);
+  setBit(pad.nX(),L0MuonBase::ShiftX,L0MuonBase::MaskX);
+  setBit(pad.nY(),L0MuonBase::ShiftY,L0MuonBase::MaskY);
+}
