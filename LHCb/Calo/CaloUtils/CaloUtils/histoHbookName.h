b9ddf67b3bd7b880e21a3ebfa414a2ed5d4b0fe4
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-04-04 13:58:05 +0000

    add 'dirHbookName(...)' function
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38667 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fadfb5956d8646fde7f3c97e6d322358639aa74b

--- LHCb/Calo/CaloUtils/CaloUtils/histoHbookName.h
+++ LHCb/Calo/CaloUtils/CaloUtils/histoHbookName.h
@@ -1,24 +1,42 @@
+// $Id: histoHbookName.h,v 1.2 2002-04-04 13:58:05 ibelyaev Exp $ 
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $
+// ============================================================================
+// $Log: not supported by cvs2svn $
+// Revision 1.1.1.1  2001/11/02 14:39:53  ibelyaev
+// New package: The first commit into CVS
+//
+// Revision 1.2  2001/07/12 21:54:23  ibelyaev
+// adaptation for Win2K
+//
+// Revision 1.1  2001/07/06 11:18:16  ibelyaev
+// update to use histoHbookName function
+//
+// ============================================================================
+#ifndef CALOUTILS_HISTOHBOOKNAME_H 
+#define CALOUTILS_HISTOHBOOKNAME_H 1
 /// ===========================================================================
-/// $Log: not supported by cvs2svn $
-/// Revision 1.2  2001/07/12 21:54:23  ibelyaev
-/// adaptation for Win2K
-///
-/// Revision 1.1  2001/07/06 11:18:16  ibelyaev
-/// update to use histoHbookName function
-///
-/// ===========================================================================
-#ifndef HISTOHBOOKNAME_H 
-#define HISTstriOHBOOKNAME_H 1
-/// ===========================================================================
-
 // Include files
 #include <cstdlib>
 #include <string>
 #include <algorithm> 
 
-/** 
+/** @file histoHbookName.h
+ *  
+ *  file with definition of 2 functions for handling Hbook 
+ *  names for histograms and directories 
+ *
+ *  @see histoHbookName 
+ *  @see dirHbookName 
+ *
+ *  @authjor Vanya Belyaev Ivan.Belyaev@itep.ru
+ *  @date 04/04/2002
+ */
+
+/** @fn histoHbookName
+ *  
  *  simple function to convert any valid Gaudi address(name in Transient Store)
- *  to address, which is simultaneously valid for Hbook
+ *  to address, which is simultaneously valid for Hbook histogram
  *
  *    examples:  
  *   "verylongname/116"  -->  "verylong/name/116"
@@ -30,17 +48,18 @@
  *  @param   maxLen maximum allowed length of directory name (8 for Hbook) 
  *  @return  new  address 
  *
- *  @author Ivan Belyaev
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
  *  @date   06/07/2001
  */
-static inline std::string 
-histoHbookName ( const std::string& addr , const int maxLen = 8 )
+inline std::string histoHbookName 
+( const std::string& addr       , 
+  const int          maxLen = 8 )
 {
-  /// ignore empty locations 
+  // ignore empty locations 
   if( addr.empty() ) { return std::string(); }
-  ///
+  //
   std::string old( addr );
-  { /// make valid histogram ID (integer)  
+  { // make valid histogram ID (integer)  
     std::string::size_type pos 
       = old.find_last_of( '/' );
     if      ( std::string::npos == pos ) { old += "/1" ; }
@@ -52,7 +71,47 @@ histoHbookName ( const std::string& addr , const int maxLen = 8 )
         if( 0 == id ) { old+="/1"; }
       }
   }
-  /// remove long names
+  // remove long names
+  if( 0 < maxLen &&  maxLen < (int) old.size() ) 
+    { 
+      std::string::iterator p1,p2;
+      p1 = old.begin() ;
+      while( old.end() != p1 )
+        {
+          p2 = std::find( p1 + 1 , old.end() , '/' ) ;
+          if( ( p2 - p1 ) <= (int) maxLen ) { p1 = p2 ; continue ; }
+          old.insert( p1 + maxLen , '/' ) ;  
+          p1 = old.begin() ; 
+        }
+    }
+  //
+  return old;
+};
+
+/** @fn dirHbookName
+ *  
+ *  simple function to convert any valid Gaudi address(name in Transient Store)
+ *  to address, which is simultaneously valid for Hbook directory
+ *
+ *    examples:  
+ *   "verylongname"  -->  "verylong/name"
+ *
+ *  @param   old    old address 
+ *  @param   maxLen maximum allowed length of directory name (8 for Hbook) 
+ *  @return  new  address 
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
+ *  @date   06/07/2001
+ */
+inline std::string dirHbookName 
+( const std::string& addr       , 
+  const int          maxLen = 8 )
+{
+  // ignore empty locations 
+  if( addr.empty() ) { return std::string(); }
+  //
+  std::string old( addr );
+  // remove long names
   if( 0 < maxLen &&  maxLen < (int) old.size() ) 
     { 
       std::string::iterator p1,p2;
@@ -69,6 +128,8 @@ histoHbookName ( const std::string& addr , const int maxLen = 8 )
   return old;
 };
 
-/// ===========================================================================
-#endif ///< HISTOHBOOKNAME_H
-/// ===========================================================================
+// ============================================================================
+// The End 
+// ============================================================================
+#endif ///< CALOUTILS_HISTOHBOOKNAME_H
+// ============================================================================
