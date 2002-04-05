c2245822ae75861a4cf1bcc51acffac7c16e79c9
ocallot <ocallot@cern.ch>
2002-04-05 15:10:16 +0000

    L0Calo code separated. L0CaloCandidate is printable
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@32530 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 49b7243e583ddfd9105859631868d9335812d1db

---
+++ LHCb/Event/L0Event/src/L0Calo.cpp
@@ -0,0 +1,71 @@
+// $Id: L0Calo.cpp,v 1.1 2002-04-05 15:10:16 ocallot Exp $
+// Include files 
+
+
+
+// local
+#include "Event/L0CaloCandidate.h"
+
+//-----------------------------------------------------------------------------
+// Auxilliary Implementation file for L0Calo Event classes
+//
+// 05/04/2002 : Olivier Callot
+//-----------------------------------------------------------------------------
+
+
+//=========================================================================
+//  Return the name of the candidate
+//=========================================================================
+std::string L0CaloCandidate::typeName() const {
+  switch (m_type) {
+  case L0Calo::Electron:  return "electron";
+  case L0Calo::Photon:    return "photon  ";
+  case L0Calo::Hadron:    return "hadron  ";
+  case L0Calo::Hadron2:   return "hadron_2";
+  case L0Calo::Pi0Local:  return "Pi0 loc ";
+  case L0Calo::Pi0Global: return "Pi0 glob";
+  case L0Calo::SumEt:     return "Sum Et  ";
+  default:            return "unknown ";
+  }
+}
+
+
+//=========================================================================
+// Print on Gaudi MsgStream 
+//=========================================================================
+MsgStream& L0CaloCandidate::fillStream( MsgStream& log ) const {
+
+  log << "L0Calo "  <<  typeName() <<  " Et(GeV) " ;
+  char line[80];
+  sprintf( line, "%4d %6.2f ", m_etCode, m_et/GeV );
+  log << line;
+  if ( L0Calo::SumEt != m_type ) {
+    log << " ID " << m_ID << " x,y,z(cm)";
+    sprintf( line, "%7.1f %7.1f %7.1f Error(cm) %5.2f",
+      m_position.x()/centimeter,
+      m_position.y()/centimeter,
+      m_position.z()/centimeter,
+      m_posTol/centimeter );
+      log << line; 
+    }
+  return log; 
+}
+
+
+//=========================================================================
+//  Operators for printing on Gaudi Stream
+//=========================================================================
+inline MsgStream& operator<<( MsgStream& ms, const L0CaloCandidate& cand) {
+  return cand.fillStream( ms );
+}
+
+inline MsgStream& operator<<( MsgStream&  ms, const L0CaloCandidate* cand) {
+  if ( 0 != cand ) {
+    ms << *cand;
+  } else {
+    ms << "L0CaloCandidate* points to null";
+  }
+  return ms;
+}
+
+//=============================================================================
