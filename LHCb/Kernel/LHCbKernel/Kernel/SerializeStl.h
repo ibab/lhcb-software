89b6211847044ebc417c592adab8faaef6430e63
Marco Cattaneo <marco.cattaneo@cern.ch>
2002-04-19 11:16:44 +0000

    changes for Gaudi v10
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46644 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f68084132828607a53066942822be435746fc5a5

--- LHCb/Kernel/LHCbKernel/Kernel/SerializeStl.h
+++ LHCb/Kernel/LHCbKernel/Kernel/SerializeStl.h
@@ -1,4 +1,4 @@
-// $Id: SerializeStl.h,v 1.4 2002-04-05 14:55:03 roiser Exp $
+// $Id: SerializeStl.h,v 1.5 2002-04-19 11:16:44 cattanem Exp $
 /*
     Small header file to serialize vectors and lists
     to standard Gaudi StreamBuffer objects
@@ -14,30 +14,6 @@
 
 static const int NUMBERS_PER_LINE = 6;
 
-// Output serialize a vector of items
-template <class T> inline 
-StreamBuffer& operator << (StreamBuffer& s, const std::vector<T>& v)  {
-  s << v.size();
-  for ( std::vector<T>::const_iterator i = v.begin(); i != v.end(); i++ )  {
-    s << (*i);
-  }
-  return s;
-}
-
-// Input serialize a vector of items
-template <class T> inline
-StreamBuffer& operator >> (StreamBuffer& s, std::vector<T>& v)  {
-  long i, len;
-  s >> len;
-  v.clear();
-  for ( i = 0; i < len; i++ )  {
-    T    temp;
-    s >> temp;
-    v.push_back(temp);
-  }
-  return s;
-}
-
 // Output operator (ASCII)
 template <class T> inline
 std::ostream& operator<< ( std::ostream& s, const std::vector<T>& v ) 
@@ -48,31 +24,6 @@ std::ostream& operator<< ( std::ostream& s, const std::vector<T>& v )
     s.width(12); 
     s << (*i) << " ";
     if ( (++cnt)%NUMBERS_PER_LINE == 0 )  s << endmsg;
-  }
-  return s;
-}
-
-
-// Output serialize a list of items
-template <class T> inline 
-StreamBuffer& operator << (StreamBuffer& s, const std::list<T>& l)  {
-  s << l.size();
-  for ( std::list<T>::const_iterator i = l.begin(); i != l.end(); i++ )  {
-    s << (*i);
-  }
-  return s;
-}
-
-// Input serialize a list of items
-template <class T> inline
-StreamBuffer& operator >> (StreamBuffer& s, std::list<T>& l)  {
-  long i, len;
-  s >> len;
-  l.clear();
-  for ( i = 0; i < len; i++ )  {
-    T    temp;
-    s >> temp;
-    l.push_back(temp);
   }
   return s;
 }
