f66fda754cc56922814b7baa5b3ca386ebca1e0c
Marco Cattaneo <marco.cattaneo@cern.ch>
2002-04-30 14:22:57 +0000

    add factories
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@32591 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 307324ed1d45d541ad25faaa473622296462d0c7

---
+++ LHCb/Event/L0Event/src/L0Event_load.cpp
@@ -0,0 +1,64 @@
+// $Id: L0Event_load.cpp,v 1.3 2002-04-30 14:22:57 cattanem Exp $
+// Include files 
+
+#include "Event/EventFactoryDefs.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for classes in Event
+//
+// 12/03/2002: Gloria Corti
+//-----------------------------------------------------------------------------
+
+/// ====================================================================
+#include "Event/L0DUReport.h"
+_ImplementDataObjectFactory(L0DUReport);
+
+/// ====================================================================
+#include "Event/L0PuVeto.h"
+_ImplementDataObjectFactory(L0PuVeto);
+
+/// ====================================================================
+#include "Event/L0CaloAdc.h"
+_ImplementContainedObjectFactory(L0CaloAdc)
+_ImplementDataObjectFactory(L0CaloAdcs)
+
+/// ====================================================================
+#include "Event/L0CaloCandidate.h"
+_ImplementContainedObjectFactory(L0CaloCandidate)
+_ImplementDataObjectFactory(L0CaloCandidates)
+
+/// ===================================================================
+#include "Event/L0MuonCandidate.h"
+_ImplementContainedObjectFactory(L0MuonCandidate)
+_ImplementDataObjectFactory(L0MuonCandidates)
+
+/// ===================================================================
+#include "Event/L0PrsSpdHit.h"
+_ImplementContainedObjectFactory(L0PrsSpdHit)
+_ImplementDataObjectFactory(L0PrsSpdHitVector)
+
+/// ====================================================================
+
+void L0Event_load() {
+
+  // Declaration of data object factories
+  DLL_DECL_OBJECTFACTORY( L0DUReport );
+  DLL_DECL_OBJECTFACTORY( L0PuVeto );
+  
+  // Declaration of contained object factories
+  DLL_DECL_OBJECTFACTORY( L0CaloAdc );
+  DLL_DECL_OBJECTFACTORY( L0CaloAdcs );
+
+  DLL_DECL_OBJECTFACTORY( L0CaloCandidate );
+  DLL_DECL_OBJECTFACTORY( L0CaloCandidates );
+  
+  DLL_DECL_OBJECTFACTORY( L0MuonCandidate );
+  DLL_DECL_OBJECTFACTORY( L0MuonCandidates );
+  
+  DLL_DECL_OBJECTFACTORY( L0PrsSpdHit );
+  DLL_DECL_OBJECTFACTORY( L0PrsSpdHitVector );
+}
+
+extern "C" void L0Event_loadRef()  {
+  L0Event_load();
+}
