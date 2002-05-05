88b64f3c688b0bab1680bff1730997b8f1c31df1
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-05-05 13:23:49 +0000

    bug fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@90393 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 35118c64df4929d6ff396d275f0c82c43ab3b18f

--- Gauss/Sim/GiGa/GiGa/GiGaExternalPhysList.h
+++ Gauss/Sim/GiGa/GiGa/GiGaExternalPhysList.h
@@ -1,34 +1,52 @@
-// $Id: GiGaExternalPhysList.h,v 1.1 2002-04-25 13:02:04 ibelyaev Exp $
+// $Id: GiGaExternalPhysList.h,v 1.2 2002-05-05 13:23:49 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
-// $Log: not supported by cvs2svn $ 
+// $Log: not supported by cvs2svn $
+// Revision 1.1  2002/04/25 13:02:04  ibelyaev
+//  small update
+// 
 // ============================================================================
 #ifndef GIGA_GIGAEXTERNALPHYSLIST_H 
 #define GIGA_GIGAEXTERNALPHYSLIST_H 1
 // Include files
 // GiGa 
-#include "GiGa/GiGaPhisListBase.h"
-#include "GiGa/GiGaPhisListFactory.h"
+#include "GiGa/GiGaPhysListBase.h"
+#include "GiGa/GiGaPhysListFactory.h"
 
 /** @class GiGaExternalPhysList GiGaExternalPhysList.h 
  *  
  *  Helper class to import external physics list into GiGa.
  *
+ *  "External Physics List" PHYSLIST
+ *  MUST have default constructor and 
+ *  MUST have VIRTUAL inheritnce fro G4VUserPhysicsList
+ *  
  *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
  *  @date   25/04/2002
  */
 
-template<class ImportedPhysList>
+template<class PHYSLIST>
 class GiGaExternalPhysList : 
-  virtual public GiGaPhisListBase ,
-  virtual public ImportedPhysList
+  virtual public GiGaPhysListBase ,
+  virtual public PHYSLIST
 { 
   /// own type 
-  typedef GiGaExternalPhysList<ImportedPhysList>   MyType ;
+  typedef GiGaExternalPhysList<PHYSLIST>   MyType ;
   /// friend factory for instantiation 
   friend class GiGaPhysListFactory<MyType>                ;
   ///
+public:
+  
+  /// standard method for construction of particles
+  virtual void ConstructParticle () { PHYSLIST::ConstructParticle () ; }
+  
+  /// standard method for construction of processes
+  virtual void ConstructProcess  () { PHYSLIST::ConstructProcess  () ; }
+  
+  /// standard method for setting ot cuts 
+  virtual void SetCuts           () { PHYSLIST::SetCuts () ; }
+  
 protected:
   
   /** standard constructor 
@@ -40,9 +58,17 @@ protected:
   GiGaExternalPhysList
   ( const std::string& name , 
     ISvcLocator*       svc  )
-    : GiGaPhysListBase( name , svc )
-    , ImportedPhysList() 
-  {};
+    : GiGaPhysListBase   ( name , svc )
+    , PHYSLIST           () 
+    //    , G4VUserPhysicsList ()
+  {
+    // check for virtual inheritance 
+    // the following line could produce compiler warning
+    // for CORRECT  inheritance schema and 
+    // it MUST produce the compiler error 
+    // for INCORECT inheritance schema  
+    G4ParticleTable* tt = theParticleTable ;
+  };
   
   /// destructor (virtual and protected)
   virtual ~GiGaExternalPhysList(){};
