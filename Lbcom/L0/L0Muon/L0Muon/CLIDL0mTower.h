aa64f33063ea02e8325bda7205db15dbad5343ad
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2002-05-07 07:11:23 +0000

    L0mPad and L0mTower are no more in the TES Removed Files:  	CLIDL0mPad.h CLIDL0mTower.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54614 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4195562ef2bca929150b90200450039feebe4615

--- Lbcom/L0/L0Muon/L0Muon/CLIDL0mTower.h
+++
@@ -1,9 +0,0 @@
-// $Id: CLIDL0mTower.h,v 1.1 2001-07-26 13:06:07 cattanem Exp $ 
-#ifndef    L0MUON_CLIDL0MTOWER_H
-#define    L0MUON_CLIDL0MTOWER_H 1 
-
-/// unique CLID
-static const CLID& CLID_L0mTower          = 6005;
-
-
-#endif  // L0MUUON_CLIDL0MTOWER_H
