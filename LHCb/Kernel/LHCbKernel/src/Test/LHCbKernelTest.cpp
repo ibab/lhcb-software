017dd44bff7d900e5e557e0cf8929c98d727da05
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-05-10 12:42:39 +0000

    bug fix for Win2K
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46673 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4fca8238dd4e9c1a5d8d3073d8b215d0227c9d66

--- LHCb/Kernel/LHCbKernel/src/Test/LHCbKernelTest.cpp
+++ LHCb/Kernel/LHCbKernel/src/Test/LHCbKernelTest.cpp
@@ -1,15 +1,13 @@
-// $Id: LHCbKernelTest.cpp,v 1.1 2002-05-10 12:30:09 ibelyaev Exp $
+// $Id: LHCbKernelTest.cpp,v 1.2 2002-05-10 12:42:39 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2002/05/10 12:30:09  ibelyaev
+//  see $LHCBKERNELROOT/doc/release.notes 10 May 2002
+//
 // ============================================================================
 // Include files
-#include <iostream>
-#include <string>
-// CLHEP
-#include "CLHEP/Vector/LorentzVector.h"
-#include "CLHEP/Matrix/SymMatrix.h"
 // LHCbKernel
 #include "Relations/TypeConversion.h"
 #include "Relations/TypeSelect.h"
@@ -17,6 +15,12 @@
 #include "Relations/ObjectTypeTraits.h"
 #include "Relations/Relation1D.h"
 #include "Relations/RelationWeighted1D.h"
+// STD & STL 
+#include <iostream>
+#include <string>
+// CLHEP
+#include "CLHEP/Vector/LorentzVector.h"
+#include "CLHEP/Matrix/SymMatrix.h"
 // Gaudi Kernel
 #include "GaudiKernel/ContainedObject.h"
 #include "GaudiKernel/DataObject.h"
@@ -441,7 +445,7 @@ int main()
     }
     
     {
-      typedef RelationWeighted1D<ContainedObject,string,double>  Rel;
+		typedef RelationWeighted1D<ContainedObject,std::string,double>  Rel;
       Rel * rel = new Rel();
       
       std::cout 
