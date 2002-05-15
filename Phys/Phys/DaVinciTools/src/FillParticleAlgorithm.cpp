f453f97df0ddf8f194231e7664da6f1d7e2962f0
Gloria Corti <gloria.corti@cern.ch>
2002-05-15 23:22:20 +0000

    new event model and memory leak
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62294 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e0c122463542ad7c331a40eeac851eb9226a629e

--- Phys/Phys/DaVinciTools/src/FillParticleAlgorithm.cpp
+++ Phys/Phys/DaVinciTools/src/FillParticleAlgorithm.cpp
@@ -1,4 +1,4 @@
-// $Id: FillParticleAlgorithm.cpp,v 1.2 2002-04-03 16:19:30 gcorti Exp $
+// $Id: FillParticleAlgorithm.cpp,v 1.3 2002-05-15 23:22:20 gcorti Exp $
 // Include files 
 
 // from Gaudi
@@ -7,8 +7,8 @@
 #include "GaudiKernel/SmartDataPtr.h"
 #include "GaudiKernel/IDataProviderSvc.h"
 #include "Event/Particle.h"
-#include "LHCbEvent/Event.h"
-#include "LHCbEvent/AxPartCandidate.h"
+#include "Event/EventHeader.h"
+#include "Event/AxPartCandidate.h"
 #include "GaudiKernel/IParticlePropertySvc.h"
 #include "GaudiKernel/ParticleProperty.h"
 
@@ -75,11 +75,11 @@ StatusCode FillParticleAlgorithm::execute() {
   log << MSG::DEBUG << "==> Execute" << endreq;
 
   // Retrieve informations about event
-  SmartDataPtr<Event> evt(eventSvc(), "/Event" );
+  SmartDataPtr<EventHeader> evt(eventSvc(), EventHeaderLocation::Default );
 
   if ( evt ) {   
-    log << MSG::INFO << "    retrieved EVENT: " << evt->event()
-        << " RUN: " << evt->run() << endreq;
+    log << MSG::INFO << "    retrieved EVENT: " << evt->evtNum()
+        << " RUN: " << evt->runNum() << endreq;
   }
   else {
     log << MSG::ERROR << "    not able to retrieve event" << endreq;
@@ -91,8 +91,8 @@ StatusCode FillParticleAlgorithm::execute() {
   
   
   // Retrieve AxPartCandidates
-  SmartDataPtr<AxPartCandidateVector> 
-               candidates (eventSvc(),"/Event/Anal/AxPartCandidates");
+  SmartDataPtr<AxPartCandidates> 
+               candidates (eventSvc(),AxPartCandidateLocation::Default);
   if ( ! candidates ) { 
     log << MSG::INFO << "    No AxPartCandidates retrieved" << endreq;
   }
@@ -103,134 +103,86 @@ StatusCode FillParticleAlgorithm::execute() {
         << candidates->size() << endreq;
 
     // Loop over all particles and fill Particle class
-    AxPartCandidateVector::iterator icand = 0;
+    AxPartCandidates::iterator icand = 0;
 
     for( icand = candidates->begin(); icand != candidates->end(); icand++ ) {
 
+      if( ((*icand)->charge() != 0 && (*icand)->trkQuality() == 1 ) ||
+          ((*icand)->charge() == 0) ){
       
-      Particle* particle = new Particle();
-//      particle->setCharge((*icand)->charge());
-      particle->setParticleID((*icand)->particleID());
-      particle->setConfLevel((*icand)->pIDConfidenceLevel());
-      //Get mass from Particle Property Service to calculate the 4-momentum
-      ParticleProperty* partProp = 
-             m_ppSvc->findByStdHepID((*icand)->particleID().id());      
-      double mass = (*partProp).mass();
-      particle->setMomentum((*icand)->fourMomentum(mass));
-      particle->setPointOnTrack((*icand)->firstPoint());
-      particle->setMass(mass);
-      particle->setMassErr(0.0);
-      // The following already has false as default value     
-      // particle->setIsResonance(false);
+        Particle* particle = new Particle();
+        particle->setParticleID((*icand)->particleID());
+        particle->setConfLevel((*icand)->pIDConfidenceLevel());
+        //Get mass from Particle Property Service to calculate the 4-momentum
+        ParticleProperty* partProp = 
+          m_ppSvc->findByStdHepID((*icand)->particleID().pid());      
+        double mass = (*partProp).mass();
+        particle->setMomentum((*icand)->fourMomentum(mass));
+        particle->setPointOnTrack((*icand)->firstPoint());
+        particle->setMass(mass);
+        particle->setMassErr(0.0);
 
-      // The following are calculated on the fly using the Px/Py/Pz
-      // the setmethods do not exist
-      // particle->setFitSlopeX((*icand)->fitSlopeX());
-      // particle->setFitSlopeY((*icand)->fitSlopeY());
+        const HepSymMatrix& trkCov = (*icand)->fitCovariance();
+        HepSymMatrix pointOnTrackErr = trkCov.sub(1,3);
+        particle->setPointOnTrackErr(pointOnTrackErr);
 
-//    log << MSG::DEBUG << "    Slope X   = "
-//        << particle->fitSlopeX() << endreq;
+        // This we need to do by hand...
+        HepMatrix posSlpMomCorrelationErr(3, 3, 0.0);
+        int i, j;
+        for( i = 1; i <= 3; i++ ) {
+          for ( j = 1; j <= 3; j++ ) {
+            int istart = i+3;
+            posSlpMomCorrelationErr(i,j) = trkCov(istart,j);
+          }
+        }
+        particle->setPosSlopesCorr(posSlpMomCorrelationErr);
 
-      const HepSymMatrix& trkCov = (*icand)->fitCovariance();
-      HepSymMatrix pointOnTrackErr = trkCov.sub(1,3);
-      particle->setPointOnTrackErr(pointOnTrackErr);
+        // as for point on track
+        HepSymMatrix slpMomCorrelationErr = trkCov.sub(4,6);
+        particle->setSlopesMomErr(slpMomCorrelationErr);
 
-      // m_pointOnTrack violates coding conventions
-      // All of the following can be replaced with the single line above
-      //m_pointOnTrackErr(1, 1) = trkCov(1, 1);
-      //m_pointOnTrackErr(2, 1) = trkCov(2, 1);
-      //m_pointOnTrackErr(2, 2) = trkCov(2, 2);
-      //m_pointOnTrackErr(3, 1) = trkCov(3, 1);
-      //m_pointOnTrackErr(3, 2) = trkCov(3, 2);
-      //m_pointOnTrackErr(3, 3) = trkCov(3, 3);
+        log << MSG::DEBUG << "AxPartCand error" << endreq;
+        for( i = 1; i <= trkCov.num_row(); i++ ) {
+          for ( j = 1; j <= i; j++ ) {          
+            log << MSG::DEBUG 
+                << "(" << i << "," << j << ") = " << trkCov(i, j) << endreq;
+          }
+        }
 
-      // The follwoing is already done at creation
-      //HepSymMatrix m_momentumErr(4, 0.0);
-      //particle->setMomentumErr(m_momentumErr);
+        log << MSG::DEBUG << "pointOnTrackErr" << endreq;
+        for( i = 1; i <= pointOnTrackErr.num_row(); i++ ) {
+          for ( j = 1; j <= i; j++ ) {          
+            log << MSG::DEBUG 
+                << "(" << i << "," << j << ") = " << pointOnTrackErr(i, j) 
+                << endreq;
+          }
+        }
       
-      //HepMatrix m_correlationErr(3, 3, 0.0);
-      //particle->setPosMomCorr(m_correlationErr);
-
-
-      // This we need to do by hand...
-      HepMatrix posSlpMomCorrelationErr(3, 3, 0.0);
-      int i, j;
-      for( i = 1; i <= 3; i++ ) {
-        for ( j = 1; j <= 3; j++ ) {
-          int istart = i+3;
-          posSlpMomCorrelationErr(i,j) = trkCov(istart,j);
+        log << MSG::DEBUG << "slopesMomErr" << endreq;
+        for( i = 1; i <= 3; i++ ) {
+          for ( j = 1; j <= i; j++ ) {          
+            log << MSG::DEBUG  
+                << "(" << i << "," << j << ") = " << slpMomCorrelationErr(i, j)
+                << endreq;
+          }
         }
-      }
-      particle->setPosSlopesCorr(posSlpMomCorrelationErr);
-
-      //m_posSlpMomCorrelationErr(1, 1) = trkCov(4, 1);
-      //m_posSlpMomCorrelationErr(1, 2) = trkCov(4, 2);
-      //m_posSlpMomCorrelationErr(1, 3) = trkCov(4, 3);
-      //m_posSlpMomCorrelationErr(2, 1) = trkCov(5, 1);
-      //m_posSlpMomCorrelationErr(2, 2) = trkCov(5, 2);
-      //m_posSlpMomCorrelationErr(2, 3) = trkCov(5, 3);
-      //m_posSlpMomCorrelationErr(3, 1) = trkCov(6, 1);
-      //m_posSlpMomCorrelationErr(3, 2) = trkCov(6, 2);
-      //m_posSlpMomCorrelationErr(3, 3) = trkCov(6, 3);
-
-      // as for point on track
-      HepSymMatrix slpMomCorrelationErr = trkCov.sub(4,6);
-      particle->setSlopesMomErr(slpMomCorrelationErr);
-      //m_slpMomCorrelationErr(1, 1) = trkCov(4, 4);
-      //m_slpMomCorrelationErr(2, 1) = trkCov(5, 4);
-      //m_slpMomCorrelationErr(2, 2) = trkCov(5, 5);
-      //m_slpMomCorrelationErr(3, 1) = trkCov(6, 4);
-      //m_slpMomCorrelationErr(3, 2) = trkCov(6, 5);
-      //m_slpMomCorrelationErr(3, 3) = trkCov(6, 6);
-
-      log << MSG::DEBUG << "AxPartCand error" << endreq;
-      for( i = 1; i <= trkCov.num_row(); i++ ) {
-        for ( j = 1; j <= i; j++ ) {          
-          log << MSG::DEBUG 
-              << "(" << i << "," << j << ") = " << trkCov(i, j) << endreq;
-        }
-      }
-
-      log << MSG::DEBUG << "pointOnTrackErr" << endreq;
-      for( i = 1; i <= pointOnTrackErr.num_row(); i++ ) {
-        for ( j = 1; j <= i; j++ ) {          
-          log << MSG::DEBUG 
-              << "(" << i << "," << j << ") = " << pointOnTrackErr(i, j) 
-              << endreq;
-        }
-      }
       
-      log << MSG::DEBUG << "slopesMomErr" << endreq;
-      for( i = 1; i <= 3; i++ ) {
-        for ( j = 1; j <= i; j++ ) {          
-          log << MSG::DEBUG  
-              << "(" << i << "," << j << ") = " << slpMomCorrelationErr(i, j)
-              << endreq;
+        log << MSG::DEBUG << "correlation" << endreq;
+        for( i = 1; i <= posSlpMomCorrelationErr.num_row(); i++ ) {
+          for ( j = 1; j <= posSlpMomCorrelationErr.num_col(); j++ ) {        
+            log << MSG::DEBUG 
+                << "(" << i << "," << j << ") = " 
+                << posSlpMomCorrelationErr(i, j) 
+                << endreq;
+          }
         }
-      }
-      
-      log << MSG::DEBUG << "correlation" << endreq;
-      for( i = 1; i <= posSlpMomCorrelationErr.num_row(); i++ ) {
-        for ( j = 1; j <= posSlpMomCorrelationErr.num_col(); j++ ) {          
-          log << MSG::DEBUG 
-              << "(" << i << "," << j << ") = " 
-              << posSlpMomCorrelationErr(i, j) 
-              << endreq;
-        }
-      }
-      particle->setOrigin((*icand));
+        particle->setOrigin((*icand));
 
-//    log << MSG::DEBUG << "    Particle ID   = "
-//        << particle->particleID().id() << endreq;
-      
-      if( ((*icand)->charge() != 0) && ((*icand)->trkQuality() == 1) ) {
-        particles->insert(particle);
-      }
-      else if( (*icand)->charge() == 0 ) {
         particles->insert(particle);
       }
     }
   }
+  
 
   // Now register the container in the store
   StatusCode sc = 
