af6426563e4c5912172970a0566bb9a639b2567f
Gloria Corti <gloria.corti@cern.ch>
2002-05-15 23:03:59 +0000

    New tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62280 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 720e786d950963c2211fee5a00efbe84c14cdb48

---
+++ Phys/Phys/DaVinciTools/DaVinciTools/IParticleMaker.h
@@ -0,0 +1,32 @@
+// $Id: IParticleMaker.h,v 1.1 2002-05-15 23:03:59 gcorti Exp $
+#ifndef DAVINCITOOLS_IPARTICLEMAKER_H 
+#define DAVINCITOOLS_IPARTICLEMAKER_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+static const InterfaceID IID_IParticleMaker("IParticleMaker", 1 , 0);
+
+/** @class IParticleMaker IParticleMaker.h DaVinciTools/IParticleMaker.h
+ *  Interface class to make Particles from other classes
+ *  (e.g. ProtoParticles).
+ *  This tools is internally used by the PhysDesktop.
+ *
+ *  @author Jose Helder Lopes
+ *  @date   22/04/2002
+ */
+class IParticleMaker : virtual public IAlgTool {
+public:
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_IParticleMaker; }
+  
+  /// Dispatch the makong of particles 
+  virtual StatusCode makeParticles( ParticleVector & parts ) = 0;
+  
+
+};
+#endif // DAVINCITOOLS_IPARTICLEMAKER_H
