6574c223fee5c468d3cc13f352cffcf005825568
Gloria Corti <gloria.corti@cern.ch>
2002-05-15 23:10:17 +0000

    change methods names and shortcut methods
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62285 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d6b15e949a541df04fc46795a43e9b4259479740

--- Phys/Phys/DaVinciTools/DaVinciTools/IMassVertexFitter.h
+++ Phys/Phys/DaVinciTools/DaVinciTools/IMassVertexFitter.h
@@ -1,4 +1,4 @@
-// $Id: IMassVertexFitter.h,v 1.2 2002-03-28 17:51:36 gcorti Exp $
+// $Id: IMassVertexFitter.h,v 1.3 2002-05-15 23:10:17 gcorti Exp $
 #ifndef DAVINCITOOLS_IMASSVERTEXFITTER_H 
 #define DAVINCITOOLS_IMASSVERTEXFITTER_H 1
 
@@ -11,13 +11,13 @@ class Particle;
 class StatusCode;
 class IParticlePropertySvc;
 
-static const InterfaceID IID_IMassVertexFitter("IMassVertexFitter", 1 , 0); 
+static const InterfaceID IID_IMassVertexFitter("IMassVertexFitter", 2 , 0); 
 
 /** @class IMassVertexFitter
- *  Perform a mass constrained fit
+ *  Interface Class for mass constrained vertex fit.
  *
- *  @author J. de Mello, C. Nunes 
- *          based on Fortran code by J. Saborido
+ *  @author J. de Mello, C. Nunes. 
+ *          based on Fortran code by J. Saborido.
  *  @date   26/02/2002
  */
 
@@ -25,14 +25,28 @@ class IMassVertexFitter : virtual public IAlgTool {
  public:
   /// Retrieve interface ID
   static const InterfaceID& interfaceID() { return IID_IMassVertexFitter; }
-  /// Calculate
 
-  // inputs: string name of the mother particle
-  //         vector of particles (2 or 3)
-  // output: vertex
-  virtual StatusCode
-  lagrangeFitter(const std::string&,const ParticleVector&,
-                 Vertex&,Particle&) = 0; 
+  /// Perform mass contrains vertex fit.
+  /// inputs: string name of the mother particle to contrain to
+  ///         vector of particles to fit to a vertex
+  /// output: resulting Vertex and Particle
+  virtual StatusCode fitWithMass(const std::string&,const ParticleVector&,
+                                 Vertex&, Particle&) = 0; 
+
+  /// Perform mass contrains vertex fit.
+  /// inputs: string name of the mother particle to contrain to
+  ///         two Particles to fit to a vertex
+  /// output: resulting Vertex and Particle
+  virtual StatusCode fitWithMass(const std::string&, Particle&, Particle&,
+                                 Vertex&, Particle&) = 0;
+
+  /// Perform mass contrains vertex fit.
+  /// inputs: string name of the mother particle to contrain to
+  ///         three Particles to fit to a vertex
+  /// output: resulting Vertex and Particle   
+  virtual StatusCode fitWithMass(const std::string&,
+                                 Particle&, Particle&, Particle&,
+                                 Vertex&, Particle&) = 0; 
 };
 
 #endif // DAVINCITOOLS_IMASSVERTEXFITTER_H
