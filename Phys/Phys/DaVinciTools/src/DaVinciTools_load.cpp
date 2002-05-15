891f0a3c190bbecd5b7c3041707c2e7f719448b7
Gloria Corti <gloria.corti@cern.ch>
2002-05-15 23:41:02 +0000

    remove and add tools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62306 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4a99e19045230a7972e1b75bc12c48aa120127de

--- Phys/Phys/DaVinciTools/src/DaVinciTools_load.cpp
+++ Phys/Phys/DaVinciTools/src/DaVinciTools_load.cpp
@@ -1,4 +1,4 @@
-// $Id: DaVinciTools_load.cpp,v 1.2 2002-03-27 20:34:59 gcorti Exp $
+// $Id: DaVinciTools_load.cpp,v 1.3 2002-05-15 23:41:02 gcorti Exp $
 // Include files 
 
 
@@ -14,10 +14,8 @@
 
 DECLARE_FACTORY_ENTRIES( DaVinciTools ) {
 
-  DECLARE_TOOL( AxPart2MCParticleAsct );
-  DECLARE_TOOL( MCUtilityTool );
   DECLARE_TOOL( PhysDesktop );
-  DECLARE_TOOL( ParticleTransporter );
+  DECLARE_TOOL( AxParticleMaker );
   DECLARE_TOOL( LagrangeMassVertexFitter );
   DECLARE_TOOL( UnconstVertexFitter );  
   DECLARE_TOOL( GeomDispCalculator );
@@ -25,6 +23,10 @@ DECLARE_FACTORY_ENTRIES( DaVinciTools ) {
   DECLARE_TOOL( KinFilterCriterion );
   DECLARE_TOOL( ParticleFilter );
   DECLARE_TOOL( ParticleStuffer );
+  DECLARE_TOOL( CombinedTransporter );
+  DECLARE_TOOL( LinearTransporter );
+  DECLARE_TOOL( NsctVeloTransporter );
+  DECLARE_TOOL( ParabolicTransporter );
 
   DECLARE_ALGORITHM( FillParticleAlgorithm );
   
