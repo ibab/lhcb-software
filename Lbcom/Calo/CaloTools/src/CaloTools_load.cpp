e1a8eb53beaa6926ff54c736599631b39b6db008
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-06-14 11:38:08 +0000

    update and version change
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@38776 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f6762f5dd1330fff188cd56f1fc486e8f032854b

--- Lbcom/Calo/CaloTools/src/CaloTools_load.cpp
+++ Lbcom/Calo/CaloTools/src/CaloTools_load.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloTools_load.cpp,v 1.8 2002-06-13 12:32:39 ibelyaev Exp $
+// $Id: CaloTools_load.cpp,v 1.9 2002-06-14 11:38:08 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.8  2002/06/13 12:32:39  ibelyaev
+//  version update
+//
 // Revision 1.7  2002/04/27 19:21:30  ibelyaev
 //  several 'technical' tools are added
 //
@@ -11,28 +14,36 @@
 
 DECLARE_FACTORY_ENTRIES(CaloTools) {
   
-  DECLARE_TOOL( ClusterCovarianceMatrixTool  );
-  DECLARE_TOOL( ClusterSpreadTool            );
-  DECLARE_TOOL( SubClusterSelectorAll        );
-  DECLARE_TOOL( SubClusterSelector3x3        );
-  DECLARE_TOOL( SubClusterSelectorMax2x2     );
-  DECLARE_TOOL( SubClusterSelectorSwissCross );
-  DECLARE_TOOL( SubClusterSelectorNeighbors  );
+  DECLARE_TOOL( CaloClusterResetFractions    ) ;
+  DECLARE_TOOL( ClusterCovarianceMatrixTool  ) ;
+  DECLARE_TOOL( ClusterSpreadTool            ) ;
 
-  DECLARE_TOOL( CaloCorrASINH                );
-  DECLARE_TOOL( CaloCorr2TANH                );
+  DECLARE_TOOL( SubClusterSelectorAll        ) ;
+  DECLARE_TOOL( SubClusterSelector3x3        ) ;
+  DECLARE_TOOL( SubClusterSelectorMax2x2     ) ;
+  DECLARE_TOOL( SubClusterSelectorSwissCross ) ;
+  DECLARE_TOOL( SubClusterSelectorNeighbors  ) ;
+
+  DECLARE_TOOL( CaloCorrASINH                ) ;
+  DECLARE_TOOL( CaloCorr2TANH                ) ;
   
-  DECLARE_TOOL( CaloSelector                 );
-  DECLARE_TOOL( CaloSelectorAND              );
-  DECLARE_TOOL( CaloSelectorOR               );
-  DECLARE_TOOL( CaloSelectorNOT              );
+  DECLARE_TOOL( CaloSelector                 ) ;
+  DECLARE_TOOL( CaloSelectorAND              ) ;
+  DECLARE_TOOL( CaloSelectorOR               ) ;
+  DECLARE_TOOL( CaloSelectorNOT              ) ;
+   
+  DECLARE_TOOL( CaloClusterToolSequence      ) ;
+  DECLARE_TOOL( CaloHypoToolSequence         ) ;
   
-  DECLARE_TOOL( CaloClusterToolSequence      );
-  DECLARE_TOOL( CaloHypoToolSequence         );
-  
-  DECLARE_TOOL( CaloPhotonMomentumTool       );
+  DECLARE_TOOL( CaloExtraDigits              ) ;
+  DECLARE_TOOL( CaloSShape                   ) ;
+
+  DECLARE_TOOL( CaloPhotonMomentumTool       ) ;
+
+  DECLARE_TOOL( CaloClusterSOMSelector       ) ;
   
 };
+// ============================================================================
 
 // ============================================================================
 // The End 
