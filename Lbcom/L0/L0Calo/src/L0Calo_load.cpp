136edd572a5bf1e9784ef403b69df68cfe200a6f
ocallot <ocallot@cern.ch>
2002-07-01 10:57:01 +0000

    forgot the _load incantations !
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@54678 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6de0f4f7f5c89f8cc7ef63a1a6a90fe389d61219

--- Lbcom/L0/L0Calo/src/L0Calo_load.cpp
+++ Lbcom/L0/L0Calo/src/L0Calo_load.cpp
@@ -1,9 +1,26 @@
-// $Id: L0Calo_load.cpp,v 1.3 2002-01-30 15:58:20 ocallot Exp $
+// $Id: L0Calo_load.cpp,v 1.4 2002-07-01 10:57:01 ocallot Exp $
 // Include files 
 
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
+#include "GaudiKernel/SmartRef.h"
+#include "GaudiKernel/StreamBuffer.h"
+
+// Event
+#include "Event/KeyedObject.h"
+#include "Event/MCParticle.h"
+
+// TrFitEvent
+#include "Event/L0CaloCandidate.h"
+
+// LHCbKernel
+#include "Relations/RelationMACROs.h"
+#include "Relations/AssociatorMACROs.h"
+
+IMPLEMENT_RelationW1D( L0CaloCandidate, MCParticle, double );
+IMPLEMENT_WAssociator( L0CaloCandidate, MCParticle, double );
+ 
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
 //
@@ -13,7 +30,13 @@
 // They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
 
 DECLARE_FACTORY_ENTRIES(L0Calo) {
+
+  DECLARE_RelationW1D( L0CaloCandidate, MCParticle, double );
+  DECLARE_WAssociator( L0CaloCandidate, MCParticle, double );
+ 
+
   DECLARE_ALGORITHM( L0CaloDigit );
   DECLARE_ALGORITHM( L0CaloAlg   );
   DECLARE_ALGORITHM( L0CaloMonit );
+  DECLARE_ALGORITHM( L0CaloToMCParticleAsct );
 }
