3e463977539f4a37b4dca7788f016d0f60de2067
odie <odie@cern.ch>
2002-07-12 09:31:28 +0000

    Increased interface to support keyed container.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62330 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b52de968bde107c075a495f6eeb986b44413ba9c

--- Phys/Phys/DaVinciTools/DaVinciTools/IDecayFinder.h
+++ Phys/Phys/DaVinciTools/DaVinciTools/IDecayFinder.h
@@ -1,4 +1,4 @@
-// $Id: IDecayFinder.h,v 1.1 2002-05-20 23:05:14 gcorti Exp $
+// $Id: IDecayFinder.h,v 1.2 2002-07-12 09:31:28 odie Exp $
 #ifndef TOOLS_IDECAYFINDER_H 
 #define TOOLS_IDECAYFINDER_H 1
 
@@ -10,7 +10,8 @@
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
 
-class Particle;
+#include "Event/Particle.h"
+//class Particle;
 
 // Declaration of the interface ID (interface id, major version, minor version)
 static const InterfaceID IID_IDecayFinder("IDecayFinder", 1, 0);
@@ -29,8 +30,12 @@ public:
   static const InterfaceID& interfaceID() { return IID_IDecayFinder; }
 
   /// Does the described decay exists in the event?
-  virtual bool hasDecay( const std::vector<Particle*> &event ) = 0;
-  virtual bool findDecay( const std::vector<Particle*> &event,
+  virtual bool hasDecay( const ParticleVector &event ) = 0;
+  virtual bool findDecay( const ParticleVector &event,
+                          const Particle *&previous_result ) = 0;
+
+  virtual bool hasDecay( const Particles &event ) = 0;
+  virtual bool findDecay( const Particles &event,
                           const Particle *&previous_result ) = 0;
 };
 #endif // TOOLS_IDECAYFINDER_H
