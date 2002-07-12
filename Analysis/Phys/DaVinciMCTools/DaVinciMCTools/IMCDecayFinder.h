41d4d638fcaeca256d188b3b711804037964dd30
odie <odie@cern.ch>
2002-07-12 09:28:28 +0000

    Increased interface to support keyed container.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@66564 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dc02a81cd4517e80f1e173c797ba59cebb54ede5

--- Analysis/Phys/DaVinciMCTools/DaVinciMCTools/IMCDecayFinder.h
+++ Analysis/Phys/DaVinciMCTools/DaVinciMCTools/IMCDecayFinder.h
@@ -1,4 +1,4 @@
-// $Id: IMCDecayFinder.h,v 1.1.1.1 2002-05-21 07:32:02 gcorti Exp $
+// $Id: IMCDecayFinder.h,v 1.2 2002-07-12 09:28:28 odie Exp $
 #ifndef TOOLS_IMCDECAYFINDER_H 
 #define TOOLS_IMCDECAYFINDER_H 1
 
@@ -10,7 +10,8 @@
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
 
-class MCParticle;
+#include "Event/MCParticle.h"
+//class MCParticle;
 
 // Declaration of the interface ID (interface id, major version, minor version)
 static const InterfaceID IID_IMCDecayFinder("IMCDecayFinder", 1, 0);
@@ -31,6 +32,10 @@ public:
   /// Does the described decay exists in the event?
   virtual bool hasDecay( const std::vector<MCParticle*> &event ) = 0;
   virtual bool findDecay( const std::vector<MCParticle*> &event,
+                          const MCParticle *&previous_result ) = 0;
+
+  virtual bool hasDecay( const MCParticles &event ) = 0;
+  virtual bool findDecay( const MCParticles &event,
                           const MCParticle *&previous_result ) = 0;
 };
 #endif // TOOLS_IMCDECAYFINDER_H
