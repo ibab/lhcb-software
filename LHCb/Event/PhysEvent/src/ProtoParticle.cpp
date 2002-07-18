486821950edf13ab37973e958b167aa7dd3f8e70
Gloria Corti <gloria.corti@cern.ch>
2002-07-18 17:40:40 +0000

    new methods
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@32756 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6c3b0a95b661d1a9951d5c83264021415da2a52c

--- LHCb/Event/PhysEvent/src/ProtoParticle.cpp
+++ LHCb/Event/PhysEvent/src/ProtoParticle.cpp
@@ -1,4 +1,4 @@
-// $Id: ProtoParticle.cpp,v 1.1 2002-07-10 15:39:55 gcorti Exp $
+// $Id: ProtoParticle.cpp,v 1.2 2002-07-18 17:40:40 gcorti Exp $
 // Include files 
 
 // STD and STL
@@ -33,7 +33,7 @@ double ProtoParticle::probOfPID(int pid) const {
 //=============================================================================
 // PDG code of most likely particleID
 //=============================================================================
-double ProtoParticle::ProbOfBestPID() const {
+double ProtoParticle::probOfBestPID() const {
 
   double probmax = 0.0;
   for( PIDInfoVector::const_iterator id=pIDInfo().begin(); 
@@ -76,5 +76,102 @@ std::vector<int> ProtoParticle::pIDList() const {
   return pids;
 } 
 
+//============================================================================
+// Return if specific detectorID technique is available
+//============================================================================
+bool ProtoParticle::detPID(const ProtoParticle::detectorPID& det) const {
+
+  bool found = false;
+  for( PIDDetVector::const_iterator id = pIDDetectors().begin();
+       pIDDetectors().end() != id; ++id ) {
+    if( det == (*id).first ) {
+      found = true;
+      break;
+    }
+  }
+  return found;
+}
+
+//=============================================================================
+// Value for specific technique if it is available (-1.0 if not)
+//=============================================================================
+double ProtoParticle::detPIDvalue(const ProtoParticle::detectorPID& det) const{
+ 
+  double value = -1.0;
+  for( PIDDetVector::const_iterator id = pIDDetectors().begin();
+       pIDDetectors().end() != id; ++id ) {
+    if( det == (*id).first ) {
+      value = (*id).second;
+      break;
+    }
+  }
+  return value;
+}
+
+//=============================================================================
+// Return TrStateP at measure point closest to IP
+//=============================================================================
+TrStateP* ProtoParticle::trStateP() {
+
+  TrStateP* trackState = NULL;
+  if( !track() ) return trackState;
+  if( track()->velo() ) return trackState;
+  
+  SmartRefVector<TrState>& states = track()->states();
+  double firstZ=1000000;
+  for ( SmartRefVector<TrState>::iterator it = states.begin() ;
+        states.end() != it; it++ ) {
+    TrState* temp = *it;
+    if ( firstZ > temp->z() )  {
+      TrState* temp = *it;
+      if ( firstZ > temp->z() )  {
+        firstZ = temp->z();
+        trackState = dynamic_cast<TrStateP*>(temp);
+      }
+    }
+  }
+  return trackState;
+}
+
+//=============================================================================
+// Return TrStateP at measure point closest to IP
+//=============================================================================
+const TrStateP* ProtoParticle::trStateP() const {
+
+  const TrStateP* trackState = NULL;
+  if( !track() ) return trackState;
+  if( track()->velo() ) return trackState;
+  
+  const SmartRefVector<TrState>& states = track()->states();
+  double firstZ=1000000;
+  for ( SmartRefVector<TrState>::const_iterator it = states.begin() ;
+        states.end() != it; it++ ) {
+    const TrState* temp = *it;
+    if ( firstZ > temp->z() )  {
+      const TrState* temp = *it;
+      if ( firstZ > temp->z() )  {
+        firstZ = temp->z();
+        trackState = dynamic_cast<const TrStateP*>(temp);
+      }
+    }
+  }
+  return trackState;
+
+
+}
+
+//=============================================================================
+// Return charge of ProtoParticle
+//=============================================================================
+double ProtoParticle::charge() const {
+ 
+  double tkcharge = 0.0;
+  
+  const TrStateP* tkstate = trStateP();
+  if( !tkstate ) return tkcharge;
+  tkcharge = tkstate->qDivP() > 0.0 ? 1.0 : -1.0;
+  return tkcharge;
+
+}
 //=============================================================================
 
