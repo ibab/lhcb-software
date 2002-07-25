136be5b902a55b839d032d912f66d5afd0b972f2
Andrea Valassi <andreav@cern.ch>
2002-07-25 15:40:12 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@2942 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7b1667b44512b49fcd243c61f0130f299cc45c06

---
+++ LHCb/Ex/DetCondExample/src/DAQCondDBAlgorithm.cpp
@@ -0,0 +1,312 @@
+//$Id: DAQCondDBAlgorithm.cpp,v 1.1 2002-07-25 15:39:43 andreav Exp $
+
+#include "DAQCondDBAlgorithm.h"
+
+#include "DetCond/IConditionsDBCnvSvc.h"
+#include "DetCond/IConditionsDBGate.h"
+
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/Timing.h"
+
+#include "ConditionsDB/CondDBObjFactory.h"
+#include "ConditionsDB/ICondDBMgr.h"
+#include "ConditionsDB/ICondDBFolderMgr.h"
+#include "ConditionsDB/ICondDBDataAccess.h"
+
+/// Instantiation of a static factory to create instances of this algorithm
+static const AlgFactory<DAQCondDBAlgorithm> Factory;
+const IAlgFactory& DAQCondDBAlgorithmFactory = Factory;
+
+// Define the name of the root folderset
+// This used to be "/" in Objectivity.
+// In the Oracle implementation it is required to have a name "/XXX"
+const std::string rootName = "/CONDDB";  
+
+// Define the attributes for all folders created in this example
+const std::string folderAttributes = "";
+
+//----------------------------------------------------------------------------
+
+/// Constructor
+DAQCondDBAlgorithm::DAQCondDBAlgorithm( const std::string& name, 
+                                        ISvcLocator* pSvcLocator )
+  : Algorithm(name, pSvcLocator),
+    m_daqRecord(""),
+    m_daqEventNumber(0),
+    m_nsExec(0),
+    m_nsDBIO(0),
+    m_condDBMgr(0)
+{
+  declareProperty( "daqFolderName",    m_daqFolderName   = "DAQ" );
+  declareProperty( "daqRecordSize",    m_daqRecordSize   = 10    );
+  declareProperty( "daqShowProgress",  m_daqShowProgress = 0     );
+}
+
+//----------------------------------------------------------------------------
+
+/// Initialization of the algorithm. 
+/// Get CondDB folder name and string size.
+/// Create the CondDB folders where DAQ data will be stored.
+
+StatusCode DAQCondDBAlgorithm::initialize() {
+
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Initialize()" << endreq;
+
+  // Locate the ConditionsDBCnvSvc
+  // Get a handle to the ConditionsDBGate
+  // Get a handle to the CondDBMgr
+  IConditionsDBCnvSvc* conditionsDBCnvSvc;
+  StatusCode sc = serviceLocator()->service 
+    ( "ConditionsDBCnvSvc", conditionsDBCnvSvc );
+  if( !sc.isSuccess() ) {
+    log << MSG::ERROR << "Can't locate ConditionsDBCnvSvc" << endreq;
+    return sc;
+  } else {
+    log << MSG::DEBUG << "Succesfully located ConditionDBCnvSvc" << endreq;
+  }
+  m_condDBMgr = conditionsDBCnvSvc->conditionsDBGate()->condDBManager();
+  log << MSG::DEBUG << "Retrieved a handle to the CondDBMgr" << endreq;
+
+  // Get properties from the JobOptionsSvc
+  sc = setProperties();
+  if ( !sc.isSuccess() ) {
+    log << MSG::ERROR << "Could not set jobOptions properties" << endreq;
+    return sc;
+  }
+  log << MSG::DEBUG << "Properties were read from jobOptions" << endreq;
+  log << MSG::INFO << "Name of the DAQ CondDB folder: "
+      << m_daqFolderName << endreq;
+  log << MSG::INFO << "Size of the DAQ CondDB record: "
+      << m_daqRecordSize << endreq;
+
+  // Make sure that the record size is not null
+  if ( m_daqRecordSize <= 0 ) {
+	log << MSG::DEBUG << "Invalid size of the DAQ record: "
+        << m_daqRecordSize << endreq;
+    m_daqRecordSize = 1;
+  }
+
+  // Build the dummy condition record of the requested size
+  long iSize;
+  for ( iSize=0; iSize<m_daqRecordSize; iSize++ ) { 
+    m_daqRecord += "X";
+  }
+  //log << MSG::VERBOSE << "DAQ record: " << m_daqRecord << endreq;
+
+  // Make sure that test folder name does not contain any "/"
+  // TODO
+
+  // Check if root folderSet exists
+  log << MSG::DEBUG 
+      << "Checking if CondDB root folderSet `"
+      << rootName << "' already exists" << endreq;
+  bool rootFolderSetExists;
+  try {
+    m_condDBMgr->startRead();
+    rootFolderSetExists = m_condDBMgr->getCondDBFolderMgr()->exist( rootName );
+    m_condDBMgr->commit();
+  } catch (CondDBException &e) {
+    log << MSG::ERROR
+	<< "Could not check existence of root folderSet" << endreq;
+    log << MSG::ERROR
+	<< "*** ConditionsDB exception caught:"          << endreq;
+    log << MSG::ERROR
+	<< "***  error message: " << e.getMessage()      << endreq;
+    log << MSG::ERROR
+	<< "***  error code:    " << e.getErrorCode()    << endreq;
+    return StatusCode::FAILURE;
+  } 
+
+  // If root folderSet exists already then do nothing
+  // If root folderSet does not exist then create it
+  if ( rootFolderSetExists ) {
+    log << MSG::DEBUG
+	<< "Root folderSet already exists: no need to create it" << endreq;
+  } else {      
+    log << MSG::INFO 
+	<< "Root folderSet does not exist: create it" << endreq;
+    try {
+      m_condDBMgr->startUpdate();
+      m_condDBMgr->getCondDBFolderMgr()->createCondDBFolderSet( 
+        rootName,
+	    folderAttributes,
+	    "Root folderSet for the ConditionsDB",
+	    true ); 
+      m_condDBMgr->commit();    
+    } catch (CondDBException &e) {
+      log << MSG::ERROR
+	  << "Error in creating root folderSet in the CondDB" << endreq;
+      log << MSG::ERROR
+	  << "*** ConditionsDB exception caught:"             << endreq;
+      log << MSG::ERROR
+	  << "***  error message: " << e.getMessage()         << endreq;
+      log << MSG::ERROR
+	  << "***  error code:    " << e.getErrorCode()       << endreq;
+      return StatusCode::FAILURE;
+    }
+    log << MSG::DEBUG
+	<< "Root folderSet did not exist and was succesfully created" 
+	<< endreq;
+  }
+ 
+  // Check if DAQ CondDB folder exists
+  std::string daqFolderFullName = rootName + "/" + m_daqFolderName;
+  log << MSG::DEBUG 
+      << "Checking if DAQ CondDB folder `"
+      << daqFolderFullName << "' already exists" << endreq;
+  log << MSG::DEBUG
+      << "" << endreq;
+  bool daqFolderExists;
+  try {
+    m_condDBMgr->startRead();
+    daqFolderExists = m_condDBMgr->getCondDBFolderMgr()->exist( daqFolderFullName );
+    m_condDBMgr->commit();
+  } catch (CondDBException &e) {
+    log << MSG::ERROR
+	<< "Could not check existence of DAQ CondDB folder" << endreq;
+    log << MSG::ERROR
+	<< "*** ConditionsDB exception caught:"             << endreq;
+    log << MSG::ERROR
+	<< "***  error message: " << e.getMessage()         << endreq;
+    log << MSG::ERROR
+	<< "***  error code:    " << e.getErrorCode()       << endreq;
+    return StatusCode::FAILURE;
+  } 
+
+  // If DAQ CondDB folder exists already then do nothing
+  // If DAQ CondDB folder does not exist then create it
+  if ( daqFolderExists ) {
+    log << MSG::DEBUG
+	<< "DAQ CondDB folder already exists: no need to create it" << endreq;
+  } else {      
+    log << MSG::INFO 
+	<< "DAQ CondDB folder does not exist: create it" << endreq;
+    try {
+      m_condDBMgr->startUpdate();
+      m_condDBMgr->getCondDBFolderMgr()->createCondDBFolder( 
+        daqFolderFullName,
+	    folderAttributes,
+	    "DAQ folder for the ConditionsDB",
+	    true ); 
+      m_condDBMgr->commit();    
+    } catch (CondDBException &e) {
+      log << MSG::ERROR
+	  << "Error in creating DAQ CondDB folder in the CondDB" << endreq;
+      log << MSG::ERROR
+	  << "*** ConditionsDB exception caught:"                << endreq;
+      log << MSG::ERROR
+	  << "***  error message: " << e.getMessage()            << endreq;
+      log << MSG::ERROR
+	  << "***  error code:    " << e.getErrorCode()          << endreq;
+      return StatusCode::FAILURE;
+    }
+    log << MSG::DEBUG
+	<< "DAQ CondDB folder did not exist and was succesfully created" 
+	<< endreq;
+  }
+  	
+  // Initialization completed  
+  log << MSG::INFO << "Initialization completed" << endreq;
+  return StatusCode::SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+
+StatusCode DAQCondDBAlgorithm::execute( ) {
+
+  MsgStream log(msgSvc(), name());
+  log << MSG::DEBUG << "Execute()" << endreq;
+  m_daqEventNumber++;
+
+  // Start the nsExec timer 
+  m_nsExec -= System::currentTime(System::nanoSec);
+
+  // At every event store a new condition
+  ICondDBObject* condObject;
+  std::string daqFolderFullName = rootName + "/" + m_daqFolderName;
+  condObject = CondDBObjFactory::createCondDBObject
+    ( System::currentTime(System::nanoSec), CondDBplusInf, m_daqRecord, "");
+  try {
+    longlong startIO = System::currentTime(System::nanoSec);
+    m_condDBMgr->startUpdate();
+    m_condDBMgr->getCondDBDataAccess()->storeCondDBObject
+      ( daqFolderFullName, condObject );
+    m_condDBMgr->commit();
+    longlong endIO = System::currentTime(System::nanoSec);
+    if ( m_daqShowProgress > 0 ) {
+      if ( m_daqEventNumber == 
+	   (m_daqEventNumber/m_daqShowProgress)*m_daqShowProgress ) {
+	log << MSG::INFO << "Evt # " << m_daqEventNumber << endreq;
+	log << MSG::INFO << "Start dbIO at: " << startIO << " ns" << endreq;
+	log << MSG::INFO << "End   dbIO at: " << endIO   << " ns" << endreq;
+      }
+    }
+    m_nsDBIO += (endIO - startIO);
+  } catch (CondDBException &e) {
+    log << MSG::ERROR
+        << "Error in storing condition object in the CondDB" << endreq;
+    log << MSG::ERROR
+	    << "*** ConditionsDB exception caught:"              << endreq;
+    log << MSG::ERROR
+        << "***  error message: " << e.getMessage()          << endreq;
+    log << MSG::ERROR
+	    << "***  error code:    " << e.getErrorCode()        << endreq;
+    return StatusCode::FAILURE;
+  }
+  log << MSG::DEBUG
+	  << "Condition object written to the database " 
+	  << endreq;
+  CondDBObjFactory::destroyCondDBObject(condObject);
+
+  // Stop the nsExec timer
+  m_nsExec += System::currentTime(System::nanoSec);
+  return StatusCode::SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+
+StatusCode DAQCondDBAlgorithm::finalize( ) {
+  MsgStream log(msgSvc(), name());
+  log << MSG::INFO << "Finalize()" << endreq;
+  log << MSG::INFO << "Total number of events processed: "
+      << m_daqEventNumber << endreq;
+  log << MSG::INFO << "DAQ record size in bytes: "
+      << m_daqRecordSize << endreq;
+#ifdef WIN32
+  log << MSG::INFO << "Accuracy of timing results: 100 ns" << endreq;
+#else
+  log << MSG::INFO << "Accuracy of timing results: 1 ms" << endreq;
+#endif
+  log << MSG::INFO << "Total exec time (ms): "
+      << m_nsExec / 1e6 << endreq;
+  log << MSG::INFO << "Total dbIO time (ms): "
+      << m_nsDBIO / 1e6 << endreq;
+  log << MSG::INFO << "Total exec time (ms) per event: "
+      << float(m_nsExec) / 1e6 / m_daqEventNumber << endreq;
+  log << MSG::INFO << "Total dbIO time (ms) per event: "
+      << float(m_nsDBIO) / 1e6 / m_daqEventNumber << endreq;
+  log << MSG::INFO << "Total exec time (ms) per event record byte: "
+      << float(m_nsExec) / 1e6 / m_daqEventNumber / m_daqRecordSize << endreq;
+  log << MSG::INFO << "Total dbIO time (ms) per event record byte: "
+      << float(m_nsDBIO) / 1e6 / m_daqEventNumber / m_daqRecordSize << endreq;
+  if ( m_nsDBIO > 0 ) {
+    log << MSG::INFO << "Average dbIO rate (MB/s):   "
+	<< ( 1e9 / m_nsDBIO ) * m_daqEventNumber * ( m_daqRecordSize / 1e6 )
+	<< endreq;
+    log << MSG::INFO << "Average dbIO rate (#evt/s): "
+	<< ( 1e9 / m_nsDBIO ) * m_daqEventNumber
+	<< endreq;
+  } else {
+    log << MSG::INFO << "Average dbIO rate (MB/s):    "
+	<< "NOT AVAILABLE" << endreq;
+    log << MSG::INFO << "Average dbIO rate (#evts/s): "
+	<< "NOT AVAILABLE" << endreq;
+  }
+  log << MSG::INFO << "Finalization completed" << endreq;
+  return StatusCode::SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+
---
+++ LHCb/Ex/DetCondExample/src/DAQCondDBAlgorithm.h
@@ -0,0 +1,63 @@
+//$Id: DAQCondDBAlgorithm.h,v 1.1 2002-07-25 15:39:43 andreav Exp $
+#ifndef DETCONDEXAMPLE_DAQCONDDBALGORITHM_H
+#define DETCONDEXAMPLE_DAQCONDDBALGORITHM_H 1
+
+// Base class
+#include "GaudiKernel/Algorithm.h"
+
+// Forward declarations
+class ICondDBMgr;
+
+///---------------------------------------------------------------------------
+/** @class DAQCondDBAlgorithm DAQCondDBAlgorithm.h DetCondExample/DAQCondDBAlgorithm.h
+
+    Simple algorithm emulating a DAQ task to populate the ConditionsDB.
+    This can be useful to measure the I/O rate for writing in the database.
+
+    @author Andrea Valassi 
+    @date July 2002
+*///--------------------------------------------------------------------------
+
+class DAQCondDBAlgorithm : public Algorithm {
+
+ public:
+
+  /// Constructor
+  DAQCondDBAlgorithm ( const std::string& name, ISvcLocator* pSvcLocator ); 
+  
+  // Algorithm standard methods
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+  
+ private:
+
+  /// DAQ CondDB relative folder name (from the job options)
+  std::string m_daqFolderName;
+
+  /// DAQ CondDB record size (from the job options)
+  long m_daqRecordSize;
+
+  /// DAQ CondDB record (built only once during the initialization)
+  std::string m_daqRecord;
+
+  /// Show event by event progress ( every n events if n>0 )
+  long m_daqShowProgress;
+
+  /// Number of events analysed so far
+  long m_daqEventNumber;
+
+  /// Total time (ns) spent in event processing
+  longlong m_nsExec;
+
+  /// Total time (ns) spent in writing to the DB
+  longlong m_nsDBIO;
+
+  /// Handle to the CondDBMgr (retrieved during the initialization)
+  ICondDBMgr* m_condDBMgr;
+
+};
+
+#endif    // DETCONDEXAMPLE_DAQCONDDBALGORITHM_H
+
+
