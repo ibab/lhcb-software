7242fd53a6098690740f8941287cdc047a1ab22c
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-09-04 15:16:14 +0000

    add new associator and it's algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@39006 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bff179239bf854fefe9a862be4bd2fbaba799922

--- Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
+++ Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloAssociators_load.cpp,v 1.5 2002-06-26 19:03:56 ibelyaev Exp $
+// $Id: CaloAssociators_load.cpp,v 1.6 2002-09-04 15:16:14 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.5  2002/06/26 19:03:56  ibelyaev
+//  add one more new algorithm for MCTruth
+//
 // Revision 1.4  2002/06/13 12:28:47  ibelyaev
 //  new options and new algorithm
 //
@@ -18,18 +21,29 @@
 #include "Event/MCVertex.h"
 // CaloEvent/Event 
 #include "Event/CaloCluster.h"
+// PhysEvent/Event
+#include "Event/ProtoParticle.h"
+#include "Event/Particle.h"
 // LHCbKernel
 #include "Relations/RelationMACROs.h"
 #include "Relations/AssociatorMACROs.h"
 
-IMPLEMENT_RelationW2D( CaloCluster , MCParticle , float );
-IMPLEMENT_RelationW1D( CaloCluster , MCParticle , float );
-IMPLEMENT_WAssociator( CaloCluster , MCParticle , float );
+IMPLEMENT_RelationW2D( CaloCluster   , MCParticle , float );
+IMPLEMENT_RelationW1D( CaloCluster   , MCParticle , float );
+IMPLEMENT_WAssociator( CaloCluster   , MCParticle , float );
+
+IMPLEMENT_RelationW2D( ProtoParticle , MCParticle , float );
+IMPLEMENT_RelationW1D( ProtoParticle , MCParticle , float );
+IMPLEMENT_WAssociator( ProtoParticle , MCParticle , float );
+
+IMPLEMENT_RelationW2D( Particle      , MCParticle , float );
+IMPLEMENT_RelationW1D( Particle      , MCParticle , float );
+IMPLEMENT_WAssociator( Particle      , MCParticle , float );
 
 /** @file CaloAssociators_load.cpp 
  * 
  *  Mandatory file to IMPLEMENT and DECLARE all factories for 
- *  all generic relation tablles and associsator tools 
+ *  all generic relation tables and associator tools 
  *
  *  @see Relation1D
  *  @see RelationWeighted1D
@@ -43,14 +57,24 @@ IMPLEMENT_WAssociator( CaloCluster , MCParticle , float );
  */
 
 DECLARE_FACTORY_ENTRIES(CaloAssociators) {
-
-  DECLARE_RelationW1D( CaloCluster , MCParticle , float ) ;
-  DECLARE_RelationW2D( CaloCluster , MCParticle , float ) ;
-  DECLARE_WAssociator( CaloCluster , MCParticle , float ) ;
-
-  DECLARE_ALGORITHM  ( CaloClustersMCTruthAlg           ) ;
-  DECLARE_ALGORITHM  ( CaloClustersMCTruth2Alg          ) ;
-  DECLARE_ALGORITHM  ( CaloClustersMCTruth3Alg          ) ;
+  
+  DECLARE_RelationW1D( CaloCluster   , MCParticle , float ) ;
+  DECLARE_RelationW2D( CaloCluster   , MCParticle , float ) ;
+  DECLARE_WAssociator( CaloCluster   , MCParticle , float ) ;
+  
+  DECLARE_RelationW1D( ProtoParticle , MCParticle , float ) ;
+  DECLARE_RelationW2D( ProtoParticle , MCParticle , float ) ;
+  DECLARE_WAssociator( ProtoParticle , MCParticle , float ) ;
+  
+  DECLARE_RelationW1D( Particle      , MCParticle , float ) ;
+  DECLARE_RelationW2D( Particle      , MCParticle , float ) ;
+  DECLARE_WAssociator( Particle      , MCParticle , float ) ;
+  
+  DECLARE_ALGORITHM  ( CaloClustersMCTruthAlg             ) ;
+  DECLARE_ALGORITHM  ( CaloClustersMCTruth2Alg            ) ;
+  DECLARE_ALGORITHM  ( CaloClustersMCTruth3Alg            ) ;
+  
+  DECLARE_ALGORITHM  ( NeutralPPMCTruthAlg                ) ;
   
 };
 
---
+++ Lbcom/Calo/CaloAssociators/src/NeutralPPMCTruthAlg.cpp
@@ -0,0 +1,207 @@
+// $Id: NeutralPPMCTruthAlg.cpp,v 1.1 2002-09-04 15:16:14 ibelyaev Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $
+// ============================================================================
+// $Log: not supported by cvs2svn $
+// Revision 1.1  2002/06/26 19:03:56  ibelyaev
+//  add one more new algorithm for MCTruth
+//
+// Revision 1.5  2002/06/16 17:19:19  ibelyaev
+//  update
+//
+// Revision 1.4  2002/06/13 12:28:48  ibelyaev
+//  new options and new algorithm
+//
+// ============================================================================
+// Include files
+// LHCbKernel 
+#include "Relations/IAssociatorWeighted.h"
+#include "Relations/RelationWeighted2D.h"
+#include "Relations/RelationWeighted1D.h"
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/SmartRef.h"
+#include "GaudiKernel/StreamBuffer.h"
+// Event 
+#include "Event/MCParticle.h"
+#include "Event/KeyedObject.h"
+// CaloDet
+#include "CaloDet/DeCalorimeter.h"
+// CasloEvent/Event
+#include "Event/CaloCluster.h"
+#include "Event/CaloHypo.h"
+#include "Event/CaloMCTools.h"
+// PhysEvent/Event 
+#include "Event/ProtoParticle.h"
+// local
+#include "CaloMCTools.h"
+#include "NeutralPPMCTruthAlg.h"
+
+// ============================================================================
+/** @file
+ * 
+ *  Implementation file for class : NeutralPPMCTruthAlg
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
+ *  @date 08/04/2002 
+ */
+// ============================================================================
+
+// ============================================================================
+// Declaration of the Algorithm Factory
+// ============================================================================
+static const  AlgFactory<NeutralPPMCTruthAlg>         s_factory ;
+const        IAlgFactory&NeutralPPMCTruthAlgFactory = s_factory ; 
+// ============================================================================
+
+// ============================================================================
+/** Standard constructor
+ *  @param name name of the algorithm
+ *  @param svc  service locator
+ */
+// ============================================================================
+NeutralPPMCTruthAlg::NeutralPPMCTruthAlg
+( const std::string& name ,
+  ISvcLocator*       svc  )
+  : CaloAlgorithm ( name , svc                  )
+  , m_asctType ( "AssociatorWeighted<ProtoParticle,MCParticle,float>" )
+  , m_asctName ( "CCs2MCPs" )
+  , m_asct     ( 0 ) 
+{ 
+  declareProperty( "MCAssociatorType" , m_asctType  ) ;
+  declareProperty( "MCAssociatorName" , m_asctName  ) ;
+};
+// ============================================================================
+
+// ============================================================================
+/// Destructor
+// ============================================================================
+NeutralPPMCTruthAlg::~NeutralPPMCTruthAlg() {}; 
+// ============================================================================
+
+// ============================================================================
+/** standard initialization of the algorithm
+ *  @see CaloAlgorithm 
+ *  @see     Algorithm 
+ *  @see    IAlgorithm 
+ *  @return StatusCode
+ */
+// ============================================================================
+StatusCode NeutralPPMCTruthAlg::initialize()
+{  
+  MsgStream log(msgSvc(), name());
+  log << MSG::DEBUG << "==> Initialise" << endreq;
+  
+  /// initialize the base class 
+  StatusCode sc = CaloAlgorithm::initialize() ;
+  if( sc.isFailure() ) 
+    { return Error("Could not initialize the base class CaloAlgorithm",sc);}
+  
+  /// locate the associator 
+  m_asct = tool( m_asctType , m_asctName , m_asct );
+  if( 0 == m_asct ) { return StatusCode::FAILURE ; }
+  
+  return StatusCode::SUCCESS;
+};
+// ============================================================================
+
+// ============================================================================
+/** standard finalization of the algorithm
+ *  @see CaloAlgorithm 
+ *  @see     Algorithm 
+ *  @see    IAlgorithm 
+ *  @return StatusCode
+ */
+// ============================================================================
+StatusCode NeutralPPMCTruthAlg::finalize() 
+{
+  MsgStream log(msgSvc(), name());
+  log << MSG::DEBUG << "==> Finalize" << endreq;
+  /// finalize thebase class
+  return CaloAlgorithm::finalize () ;
+};
+
+// ============================================================================
+/** standard execution of the algorithm
+ *  @see CaloAlgorithm 
+ *  @see     Algorithm 
+ *  @see    IAlgorithm 
+ *  @return StatusCode
+ */
+// ============================================================================
+StatusCode NeutralPPMCTruthAlg::execute() 
+{
+  /// avoid the long name and always use "const" qualifier  
+  
+  typedef const ProtoParticles                               PPs      ;
+  typedef const SmartRefVector<CaloHypo>                     Hypos    ;
+  typedef const SmartRefVector<CaloCluster>                  Clusters ;
+  
+  typedef RelationWeighted1D<ProtoParticle,MCParticle,float> Table    ;
+  
+  typedef const MCAsct::DirectType                           MCTable  ;
+  typedef const MCTable::Range                               Range    ;
+  typedef       MCTable::iterator                            relation ;
+  
+  
+  MsgStream  log( msgSvc(), name() );
+  log << MSG::DEBUG << "==> Execute" << endreq;
+  
+  // get input protoparticles 
+  const PPs*   pps  = get ( eventSvc () , inputData () , pps ) ;
+  if( 0 == pps ) { return StatusCode::FAILURE ; }
+  
+  // create relation table and register it in the event transient store 
+  Table* table = new Table();
+  StatusCode sc = put( table , outputData () );
+  if( sc.isFailure() ) { return sc ; }
+  
+  // get MC association table 
+  const MCTable* mctable = m_asct->direct();  
+  if( 0 == mctable ) { return Warning("MCrelation table points to NULL!" , 
+                                      StatusCode::SUCCESS                ) ; }
+  
+  // loop over all protoparticles 
+  for( PPs::const_iterator pp = pps->begin() ; pps->end() != pp ; ++pp )
+    {
+      // skip nulls 
+      if( 0 == *pp ) { continue ; }
+      // get all calo hypos 
+      const Hypos& hypos = (*pp)->calo() ;
+      // loop over all hypotheses 
+      for( Hypos::const_iterator hypo = hypos.begin() ; 
+           hypos.end() != hypo ; ++hypo)
+        {
+          // skip nulls
+          if( 0 == *hypo ) { continue ; }                         // CONTINUE 
+          // get all clusters
+          const Clusters& clusters = (*hypo)->clusters();
+          // loop over all clusters
+          for( Clusters::const_iterator cluster = clusters.begin() ; 
+               clusters.end() != cluster ; ++cluster ) 
+            {
+              // skip nulls 
+              if( 0 == *cluster ) { continue ; }                  // CONTINUE 
+              const Range range = mctable->relations( *cluster );
+              for( relation rel = range.begin() ; range.end() != rel ; ++rel ) 
+                {
+                  const MCParticle* particle = rel->to();
+                  // skip nulls 
+                  if( 0 == particle ) { continue ; }              // CONTINUE 
+                  // propagate the relation to protoparticle 
+                  table->relate( *pp                             , 
+                                 particle                        , 
+                                 rel->weight() / (*cluster)->e() );  
+                }
+            }
+        }
+    }
+  
+  return StatusCode::SUCCESS;
+};
+// ============================================================================
+
+// ============================================================================
+// The End 
+// ============================================================================
