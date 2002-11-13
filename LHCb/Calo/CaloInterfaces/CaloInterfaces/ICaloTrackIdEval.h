a4efa1d114c8ec98924c2fda6f9542d0cdcdeb5d
Vanya Belyaev <ivan.belyaev@cern.ch>
2002-11-13 20:32:31 +0000

    version update + new interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39050 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eee95426cbaf466f2f5d4ac835acfe6a46f53b5d

---
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/ICaloTrackIdEval.h
@@ -0,0 +1,74 @@
+// $Id: ICaloTrackIdEval.h,v 1.1 2002-11-13 20:32:30 ibelyaev Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+#ifndef CALOINTERFACES_ICALOTRACKIDEVAL_H 
+#define CALOINTERFACES_ICALOTRACKIDEVAL_H 1
+// Include files
+// STD & STL 
+#include <functional>
+// GaudiKernel
+#include "GaudiKernel/IAlgTool.h"
+// local
+#include "CaloInterfaces/IIDICaloTrackIdEval.h"
+// forward declaration
+class TrStoredTrack;
+
+/** @class ICaloTrackIdEval ICaloTrackIdEval.h CaloInterfaces/ICaloTrackIdEval.h
+ *  
+ *  The abstract interface for generic track ID estimator 
+ *  with calorimeters  
+ *
+ *  The obvious candidates for implementation
+ *   - e+/e-   identification with Prs/Spd 
+ *   - e+/e-   identification with Ecal  
+ *   - e+/e-   identification with Hcal  
+ *   - mu+/mu- identification with Hcal  
+ *  
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
+ *  @date   2002-11-10
+ */
+
+class ICaloTrackIdEval : 
+  public virtual                                 IAlgTool ,
+  public std::unary_function<const TrStoredTrack*,double>
+{
+  
+public:
+  
+  /** static interface identification
+   *  @see IInterface
+   *  @return unique interface identifier
+   */
+  static const InterfaceID& interfaceID() { return IID_ICaloTrackIdEval ; }
+  
+  /** The main processing method 
+   *  It evaluated the Track ID estimators using the calorimeter information  
+   *  @param  track  pointer to thobject to be processed
+   *  @param  value  (return) the value of the estimator
+   *  @return status code 
+   */  
+  virtual StatusCode process    ( const TrStoredTrack* track , 
+                                  double&              value ) const = 0 ;
+  
+  /** The main processing method (functor interface)
+   *  It evaluated the Track ID estimators using the calorimeter information  
+   *  @param  track  pointer to thobject to be processed
+   *  @param  the value of the estimator
+   */  
+  virtual double     operator() ( const TrStoredTrack* track ) const = 0 ;
+  
+protected:
+  
+  /// protected destructor 
+  virtual ~ICaloTrackIdEval(){};
+  
+};
+
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // CALOINTERFACES_ICALOTRACKIDEVAL_H
+// ============================================================================
---
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/IIDICaloTrackIdEval.h
@@ -0,0 +1,36 @@
+// $Id: IIDICaloTrackIdEval.h,v 1.1 2002-11-13 20:32:31 ibelyaev Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+#ifndef CALOINTERFACES_IIDICALOTRACKIDEVAL_H 
+#define CALOINTERFACES_IIDICALOTRACKIDEVAL_H 1
+
+// Include files
+
+/** @class IIDICaloTrackIdEval 
+ *  IIDICaloTrackIdEval.h CaloInterfaces/IIDICaloTrackIdEval.h
+ *  
+ *  The definition of unique interface identifier 
+ *  for interface ICaloTrackIdEval
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru 
+ *  @date   2002-11-10
+ */
+
+/** @var IID_ICaloTrackIdEval
+ *  
+ *  The definition of unique interface identifier 
+ *  for interface ICaloTrackIdEval
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@itep.ru 
+ *  @date   2002-11-10
+ */
+static const InterfaceID IID_ICaloTrackIdEval( "ICaloTrackIdEval" , 1 , 0 );
+
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // CALOINTERFACES_IIDICALOTRACKIDEVAL_H
+// ============================================================================
