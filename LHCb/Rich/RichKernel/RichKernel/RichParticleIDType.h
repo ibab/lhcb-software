b74843efe9ee81621f4d00842b2c9d52a79e8096
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2002-11-14 12:47:42 +0000

    Updates for OO reconstruction
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@6015 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 81247714bdb3dec4b4fb8b8d8502a94035a24dd1

--- LHCb/Rich/RichKernel/RichKernel/RichParticleIDType.h
+++ LHCb/Rich/RichKernel/RichKernel/RichParticleIDType.h
@@ -1,4 +1,4 @@
-// $Id: RichParticleIDType.h,v 1.4 2002-11-12 16:59:08 jonrob Exp $
+// $Id: RichParticleIDType.h,v 1.5 2002-11-14 12:47:29 jonrob Exp $
 #ifndef RICHKERNEL_PARTICLEIDTYPE_H
 #define RICHKERNEL_PARTICLEIDTYPE_H 1
 
@@ -22,8 +22,8 @@
 namespace Rich {
 
   /// Number of Rich particle types
-  static const int NRichParticleTypes = 5;
   static const int NParticleTypes = 5;
+  static const int NRichParticleTypes = 5; // keep for backward compatibility
 
   /// Enumeration for particle ID types
   enum ParticleIDType {
