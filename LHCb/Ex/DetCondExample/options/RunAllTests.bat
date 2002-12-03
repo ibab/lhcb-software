c4ed625da333250e14e4e4f8e42de4a05005fef2
Andrea Valassi <andreav@cern.ch>
2002-12-03 17:18:10 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3030 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 71db2dc50ead7ea2e37d2df8d92d1ad6344a442e

--- LHCb/Ex/DetCondExample/options/RunAllTests.bat
+++ LHCb/Ex/DetCondExample/options/RunAllTests.bat
@@ -1,17 +1,19 @@
-rem $Id: RunAllTests.bat,v 1.1 2002-12-02 11:37:05 andreav Exp $
+rem $Id: RunAllTests.bat,v 1.2 2002-12-03 17:16:42 andreav Exp $
 echo off
 if "%DETCONDEXAMPLEROOT%." == "." (
   echo ERROR! Must 'setup' before running this script"
 ) else (
   cd %DETCONDEXAMPLEROOT%\options
-  ..\sql\dropCondDB.bat
-  # YOU MUST SET YOUR VALUE OF DETCONDEXAMPLEROOT WITH DOUBLE BACKSLASHES HERE!
-  run DetCondExample testXml.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r1\\\\|DETCONDEXAMPLEROOT/|" > log.testXml.%CMTCONFIG%
+  rem # YOU MUST SET DETCONDEXAMPLEROOT WITH DOUBLE BACKSLASHES HERE!
+  rem # Under WIN32 you must modify CONDDB_implementation in cmt/requirements!
+  mkdir ..\job\NEWlog.%CONDDB_implementation%
+  del ..\job\NEWlog.%CONDDB_implementation%\log.*.%CMTCONFIG%
+  run DetCondExample testXml.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r2\\\\|DETCONDEXAMPLEROOT/|" > ..\job\NEWlog.%CONDDB_implementation%\log.testXml.%CMTCONFIG%
 ..\cmt\cleanupDB.bat
-  run DetCondExample fillCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r1\\\\|DETCONDEXAMPLEROOT/|" > log.fillCondDB_1.%CMTCONFIG%
-  run DetCondExample fillCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r1\\\\|DETCONDEXAMPLEROOT/|" > log.fillCondDB_2.%CMTCONFIG%
-  run DetCondExample testCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r1\\\\|DETCONDEXAMPLEROOT/|" > log.testCondDB.%CMTCONFIG%
-  run DetCondExample daqCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r1\\\\|DETCONDEXAMPLEROOT/|" > log.daqCondDB.%CMTCONFIG% 
+  run DetCondExample fillCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r2\\\\|DETCONDEXAMPLEROOT/|" > ..\job\NEWlog.%CONDDB_implementation%\log.fillCondDB_1.%CMTCONFIG%
+  run DetCondExample fillCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r2\\\\|DETCONDEXAMPLEROOT/|" > ..\job\NEWlog.%CONDDB_implementation%\log.fillCondDB_2.%CMTCONFIG%
+  run DetCondExample testCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r2\\\\|DETCONDEXAMPLEROOT/|" > ..\job\NEWlog.%CONDDB_implementation%\log.testCondDB.%CMTCONFIG%
+  run DetCondExample daqCondDB.opts | sed "s|I:\\\\newmycmt11\\\\Ex\\\\DetCondExample\\\\v4r2\\\\|DETCONDEXAMPLEROOT/|" > ..\job\NEWlog.%CONDDB_implementation%\log.daqCondDB.%CMTCONFIG% 
 )
 
 
--- LHCb/Ex/DetCondExample/options/RunAllTests.csh
+++ LHCb/Ex/DetCondExample/options/RunAllTests.csh
@@ -1,37 +1,35 @@
 #!/usr/local/bin/tcsh -f 
-# $Id: RunAllTests.csh,v 1.1 2002-12-02 11:37:05 andreav Exp $
+# $Id: RunAllTests.csh,v 1.2 2002-12-03 17:16:42 andreav Exp $
 if ( 1 != ${?DETCONDEXAMPLEROOT} ) then
   echo "ERROR! Must 'source setup.csh -tag=$CMTDEB' before running this script"
   exit 1
 endif
 set echo
 cd ${DETCONDEXAMPLEROOT}/options
-../sql/dropCondDB.csh
 
-/bin/rm -f log.testXml.${CMTCONFIG}
+mkdir -p ../job/NEWlog.${CONDDB_implementation}
+rm -f ../job/NEWlog.${CONDDB_implementation}/log.*.${CMTCONFIG}
+
 ../$CMTDEB/DetCondExample.exe testXml.opts \
-  | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" > log.testXml.${CMTCONFIG}
+  | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" \
+  > ../job/NEWlog.${CONDDB_implementation}/log.testXml.${CMTCONFIG}
 
 source ../cmt/cleanupDB.csh -tag=$CMTDEB
 source ../cmt/setupDB.csh   -tag=$CMTDEB
-/bin/rm -f log.fillCondDB_1.${CMTCONFIG}
-/bin/rm -f log.fillCondDB_2.${CMTCONFIG}
 ../$CMTDEB/DetCondExample.exe fillCondDB.opts \
   | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" \
-  > log.fillCondDB_1.${CMTCONFIG}
+  > ../job/NEWlog.${CONDDB_implementation}/log.fillCondDB_1.${CMTCONFIG}
 ../$CMTDEB/DetCondExample.exe fillCondDB.opts \
   | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" \
-  > log.fillCondDB_2.${CMTCONFIG}
+  > ../job/NEWlog.${CONDDB_implementation}/log.fillCondDB_2.${CMTCONFIG}
 
-/bin/rm -f log.testCondDB.${CMTCONFIG}
 ../$CMTDEB/DetCondExample.exe testCondDB.opts \
   | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" \
-  > log.testCondDB.${CMTCONFIG}
+  > ../job/NEWlog.${CONDDB_implementation}/log.testCondDB.${CMTCONFIG}
 
-/bin/rm -f log.daqCondDB.Objy.Linux61
 ../$CMTDEB/DetCondExample.exe daqCondDB.opts \
   | sed "s|$DETCONDEXAMPLEROOT|DETCONDEXAMPLEROOT|" \
-  > log.daqCondDB.${CMTCONFIG}
+  > ../job/NEWlog.${CONDDB_implementation}/log.daqCondDB.${CMTCONFIG}
 
 #-----------------------------------------------------------------------------
 
