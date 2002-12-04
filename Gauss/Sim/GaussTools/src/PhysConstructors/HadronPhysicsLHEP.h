bb950427ab23d825fd7ae81f6a97f5712cea03a1
Witold Pokorski <witold.pokorski@cern.ch>
2002-12-04 12:56:37 +0000

    added missing header files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@90577 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3ade3267e437b36f8ada438f0de2662a8c76e361

---
+++ Gauss/Sim/GaussTools/src/PhysConstructors/HadronPhysicsLHEP.h
@@ -0,0 +1,86 @@
+// ============================================================================
+#ifndef    GaussTools_HadronPhysicsLHEP_H
+#define    GaussTools_HadronPhysicsLHEP_H 1 
+// ============================================================================
+// include files 
+
+//GaudiKernel 
+
+// GiGa 
+#include "GiGa/GiGaPhysConstructorBase.h"
+
+//G4
+#include "globals.hh"
+#include "G4ios.hh"
+
+#include "G4HadronQEDBuilder.hh"
+#include "G4StoppingHadronBuilder.hh"
+#include "G4MiscLHEPBuilder.hh"
+
+#include "G4LHEPPiKBuilder.hh"
+#include "G4PiKBuilder.hh"
+
+#include "G4ProtonBuilder.hh"
+#include "G4LHEPProtonBuilder.hh"
+
+#include "G4NeutronBuilder.hh"
+#include "G4LHEPNeutronBuilder.hh"
+
+// forward declarations 
+template <class TYPE> class GiGaFactory;
+
+class HadronPhysicsLHEP : public GiGaPhysConstructorBase
+{
+  /// friend factory for instantiation 
+  friend class GiGaFactory<HadronPhysicsLHEP>;
+
+public:
+
+  HadronPhysicsLHEP  
+  ( const std::string& type   ,
+    const std::string& name   ,
+    const IInterface*  parent ) ;
+  
+  virtual ~HadronPhysicsLHEP();
+
+
+public:
+  
+  /// from G4VUserPhysicsList 
+  virtual void ConstructParticle ();
+  virtual void ConstructProcess  ();
+  ///
+
+private:
+  G4NeutronBuilder theNeutrons;
+  G4LHEPNeutronBuilder theLHEPNeutron;
+  
+  G4PiKBuilder thePiK;
+  G4LHEPPiKBuilder theLHEPPiK;
+  
+  G4ProtonBuilder thePro;
+  G4LHEPProtonBuilder theLHEPPro;
+  
+  G4MiscLHEPBuilder theMiscLHEP;
+  G4StoppingHadronBuilder theStoppingHadron;
+  G4HadronQEDBuilder theHadronQED;
+  ///
+
+};
+// ============================================================================
+
+
+// ============================================================================
+#endif   ///< GaussTools_HadronPhysicsLHEP_H
+// ============================================================================
+
+
+
+
+
+
+
+
+
+
+
