b9d33358cd3d6f93479817cf90d8d8ada1154833
Gloria Corti <gloria.corti@cern.ch>
2002-12-20 16:29:50 +0000

    calo tools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@28732 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d1f679caa0c96bbc5f52b66208dfcc912701280a

--- Rec/Rec/GlobalReco/src/GlobalReco_load.cpp
+++ Rec/Rec/GlobalReco/src/GlobalReco_load.cpp
@@ -1,8 +1,11 @@
-// $Id: GlobalReco_load.cpp,v 1.4 2002-11-20 20:00:23 ibelyaev Exp $
+// $Id: GlobalReco_load.cpp,v 1.5 2002-12-20 16:29:50 gcorti Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.4  2002/11/20 20:00:23  ibelyaev
+//  new algorithm for creation of Neutral ProtoParticles
+//
 // Revision 1.3  2002/07/15 19:27:08  ibelyaev
 //  add new algorithm for creation on Neutral ProtoParticles
 // 
@@ -20,6 +23,10 @@ DECLARE_FACTORY_ENTRIES(GlobalReco) {
 
   DECLARE_ALGORITHM(PrimaryVertexFinderAlg) ;
   DECLARE_TOOL(PrimaryVertexFitterTool);
+
+  // Temporarly for reprocessing of Brunel v13r1 data
+  DECLARE_TOOL( CaloE1Correction );
+  DECLARE_TOOL( CaloSummerPrsCorrection );
 
 };
 
