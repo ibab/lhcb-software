185b617f5b11125b51c5822f814b2002dac9661f
ocallot <ocallot@cern.ch>
2003-01-07 07:33:10 +0000

    Fix the pitch to get 512 R strips
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@22913 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8ffed1c06bdb7d1e54948c2ca6c13e681c925c97

--- LHCb/Det/VeloDet/src/Lib/DeVelo.cpp
+++ LHCb/Det/VeloDet/src/Lib/DeVelo.cpp
@@ -1,4 +1,4 @@
-// $Id: DeVelo.cpp,v 1.27 2003-01-06 09:31:47 ocallot Exp $
+// $Id: DeVelo.cpp,v 1.28 2003-01-07 07:33:10 ocallot Exp $
 //
 // ============================================================================
 #define  VELODET_DEVELO_CPP 1
@@ -146,7 +146,7 @@ StatusCode DeVelo::initialize() {
   switch (m_design){
   case 1:
     m_fixPitchRadius = m_innerRadius;
-    m_outerPitch     = .1030*mm;
+    m_outerPitch     = .10263*mm;
     m_nbRInner       = 512;
     break;
   case 2:
