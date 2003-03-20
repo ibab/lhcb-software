9272f5b7c41a00b9690099d9b6a24f5434c38561
Gloria Corti <gloria.corti@cern.ch>
2003-03-20 07:50:02 +0000

    add l0calo and l1track factories
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@105766 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4be306df20dd49ada1f83ded1cf2af987475e54d

--- Lbcom/Associators/Associators/src/Associators_load.cpp
+++ Lbcom/Associators/Associators/src/Associators_load.cpp
@@ -1,11 +1,14 @@
-// $Id: Associators_load.cpp,v 1.1.1.1 2002-07-10 12:12:38 hierck Exp $
+// $Id: Associators_load.cpp,v 1.2 2003-03-20 07:50:02 gcorti Exp $
 // Include files
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Event
 //#include "Event/MCHit.h"
-#include "Event/MCVeloHit.h"
 //#include "Event/MCParticle.h"
+#include "Event/MCVeloHit.h"
+#include "Event/L0CaloCandidate.h"
+#include "Event/L1Track3d.h"
+#include "Event/L1Track2d.h"
 
 // LHCbKernel
 #include "Relations/RelationMACROs.h"
@@ -16,6 +19,15 @@ IMPLEMENT_Associator( MCParticle, MCHit);
 
 IMPLEMENT_Relation1D( MCParticle, MCVeloHit);
 IMPLEMENT_Associator( MCParticle, MCVeloHit);
+
+IMPLEMENT_RelationW1D( L0CaloCandidate, MCParticle, double );
+IMPLEMENT_WAssociator( L0CaloCandidate, MCParticle, double );
+
+IMPLEMENT_RelationW1D( L1Track3d, MCParticle, double );
+IMPLEMENT_WAssociator( L1Track3d, MCParticle, double );
+
+IMPLEMENT_RelationW1D( L1Track2d, MCParticle, double );
+IMPLEMENT_WAssociator( L1Track2d, MCParticle, double );
 
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
@@ -37,4 +49,13 @@ DECLARE_FACTORY_ENTRIES(Associators) {
   DECLARE_ALGORITHM( MCParticle2MCHitAlg );
   DECLARE_ALGORITHM( MCParticle2MCVeloHitAlg );
   
+  DECLARE_RelationW1D( L0CaloCandidate, MCParticle, double );
+  DECLARE_WAssociator( L0CaloCandidate, MCParticle, double );
+
+  DECLARE_RelationW1D( L1Track3d, MCParticle, double );
+  DECLARE_WAssociator( L1Track3d, MCParticle, double );
+
+  DECLARE_RelationW1D( L1Track2d, MCParticle, double );
+  DECLARE_WAssociator( L1Track2d, MCParticle, double );
+
 }  
