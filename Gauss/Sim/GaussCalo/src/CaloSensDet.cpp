42446bef5a619b0ccca88530b60f9ef75c781ad7
Patrick Robbe <robbe@lal.in2p3.fr>
2003-05-14 08:43:42 +0000

    Addition of specific calo corrections in CaloSensDet : - Birk's law - timing - local non uniformity
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@90799 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e2cca60f36f7ff46ea9c626c9dd887dc7c9a68e4

--- Gauss/Sim/GaussCalo/src/CaloSensDet.cpp
+++ Gauss/Sim/GaussCalo/src/CaloSensDet.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloSensDet.cpp,v 1.2 2002-12-13 16:52:57 ibelyaev Exp $ 
+// $Id: CaloSensDet.cpp,v 1.3 2003-05-14 08:43:42 robbep Exp $ 
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2002/12/13 16:52:57  ibelyaev
+//  put updated versions of the packages
+//
 // Revision 1.2  2002/12/07 21:19:14  ibelyaev
 //  few optimization updates
 //
@@ -18,10 +21,12 @@
 #include <vector>
 /// CLHEP 
 #include "CLHEP/Geometry/Point3D.h"
+#include "CLHEP/Units/PhysicalConstants.h"
 /// GaudiKernel
 #include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/SmartDataPtr.h" 
 #include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/AlgFactory.h"
 /// GiGa 
 #include "GiGa/GiGaMACROs.h"
 #include "GiGa/GiGaHashMap.h"
@@ -33,6 +38,7 @@
 #include "G4VPhysicalVolume.hh"
 #include "G4LogicalVolume.hh"
 #include "G4SDManager.hh"
+#include "G4EnergyLossTables.hh"
 // GiGaCnv 
 #include "GiGaCnv/GiGaVolumeUtils.h"
 // CaloDet
@@ -109,11 +115,72 @@ CaloSensDet::CaloSensDet
   , m_caloName         ( DeCalorimeterLocation::Ecal ) 
   , m_calo             ( 0 ) 
   ///
-  , m_zmin             (  1 * km )  // minimal z of forbidden zone 
-  , m_zmax             ( -1 * km )  // maximal z of forbidden zone 
+  , m_zmin             ( -1 * km )  // minimal z of forbidden zone 
+  , m_zmax             (  1 * km )  // maximal z of forbidden zone 
   ///
   , m_collection       ( 0 )
+  , m_c1               ( 0.013 * g/MeV/cm2 ) // 1st coef. of Birk's law
+  , m_c2               ( 9.6E-6 * g*g/MeV/MeV/cm2/cm2 ) // 2nd coef
+  //. of Birk's law
+  , m_c1correction     ( 0.57142857 ) // correction of c1 for 2e charged part.
+  , m_a_local_inner_ecal ( 0.004 ) // local non uniformity amplitude inner ecal
+  , m_a_local_middle_ecal ( 0.004 ) // local non uniformity amplitude
+  //  middle ecal
+  , m_a_local_outer_ecal ( 0.012 ) // local non uniformity amplitude 
+  // outer ecal
 {
+  // Initialisation of Histograms for timing of ECAL
+  m_ecalTimeHisto[0][0]  = 1. ;
+  m_ecalTimeHisto[0][1]  = 1. ;
+  m_ecalTimeHisto[0][2]  = 0.99 ;
+  m_ecalTimeHisto[0][3]  = 0.985 ;
+  m_ecalTimeHisto[0][4]  = 0.98 ;
+  m_ecalTimeHisto[0][5]  = 0.96 ;
+  m_ecalTimeHisto[0][6]  = 0.935 ;
+  m_ecalTimeHisto[0][7]  = 0.9 ;
+  m_ecalTimeHisto[0][8]  = 0.86 ;
+  m_ecalTimeHisto[0][9]  = 0.81 ;
+  m_ecalTimeHisto[0][10] = 0.75 ;
+  m_ecalTimeHisto[0][11] = 0.68 ;
+  m_ecalTimeHisto[0][12] = 0.59 ;
+  m_ecalTimeHisto[0][13] = 0.51 ;
+  m_ecalTimeHisto[0][14] = 0.42 ;
+  m_ecalTimeHisto[0][15] = 0.35 ;
+  m_ecalTimeHisto[0][16] = 0.28 ;
+  m_ecalTimeHisto[0][17] = 0.2  ;
+  m_ecalTimeHisto[0][18] = 0.14 ;
+  m_ecalTimeHisto[0][19] = 0.09 ;
+  m_ecalTimeHisto[0][20] = 0.06 ;
+  m_ecalTimeHisto[0][21] = 0.025 ;
+  m_ecalTimeHisto[0][22] = 0.01 ;
+  m_ecalTimeHisto[0][23] = 0. ;
+  m_ecalTimeHisto[0][24] = 0. ;
+  //
+  m_ecalTimeHisto[1][0]  = 0. ;
+  m_ecalTimeHisto[1][1]  = 0. ;
+  m_ecalTimeHisto[1][2]  = 0. ;
+  m_ecalTimeHisto[1][3]  = 0.005 ;
+  m_ecalTimeHisto[1][4]  = 0.035 ;
+  m_ecalTimeHisto[1][5]  = 0.065 ;
+  m_ecalTimeHisto[1][6]  = 0.09 ;
+  m_ecalTimeHisto[1][7]  = 0.13 ;
+  m_ecalTimeHisto[1][8]  = 0.18 ;
+  m_ecalTimeHisto[1][9]  = 0.24 ;
+  m_ecalTimeHisto[1][10] = 0.31 ;
+  m_ecalTimeHisto[1][11] = 0.36 ;
+  m_ecalTimeHisto[1][12] = 0.44 ;
+  m_ecalTimeHisto[1][13] = 0.51 ;
+  m_ecalTimeHisto[1][14] = 0.59 ;
+  m_ecalTimeHisto[1][15] = 0.67 ;
+  m_ecalTimeHisto[1][16] = 0.74 ;
+  m_ecalTimeHisto[1][17] = 0.8 ;
+  m_ecalTimeHisto[1][18] = 0.85 ;
+  m_ecalTimeHisto[1][19] = 0.91 ;
+  m_ecalTimeHisto[1][20] = 0.94 ;
+  m_ecalTimeHisto[1][21] = 0.96 ;
+  m_ecalTimeHisto[1][22] = 0.985 ;
+  m_ecalTimeHisto[1][23] = 0.99 ;
+  m_ecalTimeHisto[1][24] = 1. ;
   ///
   setProperty     ( "DetectorDataProvider" ,  "DetectorDataSvc"  ) ;
   
@@ -123,6 +190,12 @@ CaloSensDet::CaloSensDet
   declareProperty ( "Detector"             ,  m_caloName         ) ;
   declareProperty ( "zMin"                 ,  m_zmin             ) ;
   declareProperty ( "zMax"                 ,  m_zmax             ) ;
+  declareProperty ( "c1"                   ,  m_c1               ) ;
+  declareProperty ( "c2"                   ,  m_c2               ) ;
+  declareProperty ( "c1correction"         ,  m_c1correction     ) ;
+  declareProperty ( "a_local_inner_ecal"   ,  m_a_local_inner_ecal ) ;
+  declareProperty ( "a_local_middle_ecal"  ,  m_a_local_middle_ecal ) ;
+  declareProperty ( "a_local_outer_ecal"   ,  m_a_local_outer_ecal ) ;
   //
 };
 // ============================================================================
@@ -214,7 +287,7 @@ StatusCode  CaloSensDet::locateVolumes()
   return StatusCode::SUCCESS ;
 };
 // ============================================================================
-
+ 
 // ============================================================================
 /** method from G4 
  *  (Called at the begin of each event)
@@ -224,24 +297,28 @@ StatusCode  CaloSensDet::locateVolumes()
 // ============================================================================
 void CaloSensDet::Initialize( G4HCofThisEvent* HCE )
 {
-  ///
-  if( !m_volumesLocated ) 
-    {
-      StatusCode sc = locateVolumes();
-      if( sc.isFailure() ) { Error("Error from 'locateVolumes' method",sc); }
+  //
+  if( !m_volumesLocated ) {
+    StatusCode sc = locateVolumes();
+    if( sc.isFailure() ) { 
+      Error("Error from 'locateVolumes' method",sc); 
     }
+  }
   Assert( m_volumesLocated , "Could not locate volumes!");
-  ///
+  //
+  
   m_collection = new CaloHitsCollection ( SensitiveDetectorName , 
                                           collectionName[0]     ) ; 
-  /// 
-  const int id  = GetCollectionID( 0 )  ;
+  //
+  const int id  = GetCollectionID( 0 ) ;
+  
   HCE -> AddHitsCollection( id , m_collection );
-  ///
+  
+  //
   Print(" Initialize(): CollectionName='" + m_collection->GetName   () +
         "' for SensDet='"                 + m_collection->GetSDname () + 
         "'" , StatusCode::SUCCESS , MSG::VERBOSE                       ) ;
-  ///
+  //
   m_hitmap.clear() ;
 };
 // ============================================================================
@@ -267,7 +344,6 @@ bool CaloSensDet::ProcessHits( G4Step* step ,
                                G4TouchableHistory* /* history */ ) 
 {
 
-  
   if( 0 == step ) { return false ; } 
   ///
   const double      edeposit  = step-> GetTotalEnergyDeposit                () ;
@@ -337,14 +413,16 @@ bool CaloSensDet::ProcessHits( G4Step* step ,
   
   /// ID of track to be stored 
   int sTrackID     = track -> GetParentID () ;
+  
   // already marked to be stored:
-  if     ( info -> toBeStored()     ) { sTrackID = trackID ; }
-  else  
-    {
-      const double z0 = track->GetVertexPosition().z() ;
+  if     ( info -> toBeStored()     ) { 
+    sTrackID = trackID ; 
+  }  else {
+    const double z0 = track->GetVertexPosition().z() ;
       // outside the "forbidden zone" 
-      if ( z0 < m_zmin || z0 > m_zmax   ) { sTrackID = trackID ; }
-    }
+    if ( z0 < m_zmin || z0 > m_zmax   ) { sTrackID = trackID ; }
+  }
+
   /// hit is exist for given track? 
   CaloSubHit*& sub  = hit->hit( sTrackID ) ;                   // ATTENTION
   // create new subhit if needed 
@@ -352,13 +430,174 @@ bool CaloSensDet::ProcessHits( G4Step* step ,
   /// update the track informoation
   if( trackID == sTrackID ) { info->addToHits( sub ); }
   // add current energy deposition to the sub-hit
-  CaloSubHit::Time tm = 0   ; 
-  sub->add( tm , edeposit ) ;
-  
+  CaloSubHit::Time tm = 0   ;
+
+  // Birk's Law Correction
+  double ecorrected(edeposit) ;
+  ecorrected *= birkCorrection(step) ;
+
+  // Timing
+  double frac1, frac2 ;
+  int timeBin ;
+  timeFraction( step , cellID , frac1 , frac2 , timeBin ) ;
+
+  //Local NonUniformity
+  ecorrected *= localNonUniformity( step , cellID ) ;
+
+  if ( frac1 > 1E-7 ) sub->add( timeBin   , ecorrected*frac1 ) ;
+  if ( frac2 > 1E-7 ) sub->add( timeBin+1 , ecorrected*frac2 ) ;
+
   return true ;
   
 };
 // ============================================================================
+// Birk's Law
+// ============================================================================
+  /** Correction factor from Birk's Law
+   *  Factor = 1/(1+C1*dEdx/rho+C2*(dEdx/rho)^2)
+   *  Where :
+   *      - C1 = 0.013 g/MeV/cm^2 [Ref NIM 80 (1970) 239]
+   *      - C2 = 9.6.10^-6 g^2/MeV^2/cm^4
+   *      - dEdx in MeV/cm
+   *      - rho = density in g/cm^3
+   */
+double CaloSensDet::birkCorrection(G4Step* step) 
+{
+  double result = 1. ;
+  if (step) {
+
+    // Track
+    const G4Track* track  = step->GetTrack() ;
+    const double charge   = track->GetDefinition()->GetPDGCharge()  ;
+
+    // Only charged tracks
+    if (abs(charge)>0.1) {
+
+      // Birk's law coefficients
+      double C1       = m_c1 ;
+      double C2       = m_c2 ;
+
+      // Correction for charge 2 particles
+      if (abs(charge)>1.5) {
+        C1 *= m_c1correction ;
+      }
+
+      // Material
+      const G4Material* mat = step->GetPreStepPoint()->GetMaterial() ;
+      double density  = mat->GetDensity() ;
+
+      // dEdx
+      double dEdx = G4EnergyLossTables::GetDEDX(
+                                                track->GetDefinition(),
+                                                track->GetKineticEnergy(),
+                                                mat ) ;      
+      
+      result = 1./(1. + C1*dEdx/density + C2*dEdx*dEdx/density/density ) ;
+    }
+  }
+  
+  return result ;
+};
+
+// ============================================================================
+// Get Fraction of energy in consecutive time windows
+// ============================================================================
+  
+  /** The energy deposited in the cell by the step contributes 
+   *  - frac1*energy in the time bin timeBin (25 ns bins)
+   *  - frac2*energy in the time bin timeBin+1
+   *  the origin is the time of collision
+   */
+
+void CaloSensDet::timeFraction(G4Step* step, const CaloCellID& cell,
+                               double& frac1, double& frac2, int& timeBin)
+{
+  frac1 = 1. ;
+  frac2 = 0. ;
+  timeBin = 0 ;
+
+  // Only for ECAL for the moment
+  if ( CaloCellCode::CaloNameFromNum( cell.calo() ) == "Ecal" ) {  
+
+    // Time of the step in Geant = time since the primary event
+    double timeOfStep = step->GetPreStepPoint()->GetGlobalTime() ;
+    // Time of flight between z=0 and z=Ecal 
+    double dist       = sqrt( m_calo->cellX(cell)*
+                              m_calo->cellX(cell)
+                              + m_calo->cellY(cell)*
+                              m_calo->cellY(cell) 
+                              + (12490.*mm) * (12490.*mm) ) ;
+    
+    double tFlight = dist / c_light ;
+    // substract 0.5 ns (from SICB)
+    tFlight -= 0.5 * ns ;
+    
+    // Find bin number in term of 25ns sampling
+    double tNow = timeOfStep - tFlight ;
+    int bin     = (int) floor( tNow - floor( tNow / (25.*ns) ) * (25.*ns) ) ;
+    
+    // Frac1 = fraction of energy in first 25ns time window
+    frac1 = m_ecalTimeHisto[0][bin] ;
+    // Frac2 = fraction of energy in second 25 ns time window
+    frac2 = m_ecalTimeHisto[1][bin] ;
+    // timeBin = absolute bin number with respect to tFlight
+    timeBin = (int) floor( tNow / (25.*ns) ) ;
+  }
+};
+
+// ============================================================================
+// Local Non Uniformity
+// ============================================================================
+  /** Correction due to the local non uniformity due to the light 
+   *  collection efficiency in cell cell
+   */
+
+double CaloSensDet::localNonUniformity(G4Step* step, const CaloCellID& cell) 
+{
+  
+  // Only for ECal for the moment
+  double correction = 1. ;
+  
+  if ( CaloCellCode::CaloNameFromNum( cell.calo() ) == "Ecal" ) {
+  
+    // Find the position of the step
+    double x        = step->GetPreStepPoint()->GetPosition().x() ;
+    double y        = step->GetPreStepPoint()->GetPosition().y() ;
+    // Center of the cell
+    double x0       = m_calo->cellX( cell ) ;
+    double y0       = m_calo->cellY( cell ) ;
+    // Distance between fibers 
+    // and correction amplitude
+    double d        = 10.1 * mm ;
+    double A_local  = m_a_local_inner_ecal ;
+
+    // Assign amplitude of non uniformity as a function of the
+    // Ecal region
+    
+    if ( cell.area() == 0 ) { // outer Ecal
+      A_local = m_a_local_outer_ecal ;
+      d       = 15.25 * mm ;
+    }
+    else if ( cell.area() == 1 ) { // middle Ecal
+      A_local = m_a_local_middle_ecal ;
+    }
+
+    
+    // Local uniformity is product of x and y sine-like functions
+    // The Amplitude of the sin-like function is a function of x and 
+    // y
+    correction =
+      1. - 
+      A_local * 
+      cos(2.*pi*(x-x0)/d) *
+      cos(2.*pi*(y-y0)/d) ;
+  }
+  
+  return correction ;
+  
+};
+
+
 
 // ============================================================================
 // The END 
--- Gauss/Sim/GaussCalo/src/CaloSensDet.h
+++ Gauss/Sim/GaussCalo/src/CaloSensDet.h
@@ -1,8 +1,11 @@
-// $Id: CaloSensDet.h,v 1.2 2002-12-13 16:52:57 ibelyaev Exp $ 
+// $Id: CaloSensDet.h,v 1.3 2003-05-14 08:43:42 robbep Exp $ 
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2002/12/13 16:52:57  ibelyaev
+//  put updated versions of the packages
+//
 // Revision 1.1  2002/12/07 14:41:44  ibelyaev
 //  add new Calo stuff
 //
@@ -54,7 +57,7 @@ public :
    *  @see GiGaBase 
    *  @see   AlgTool 
    *  @see  IAlgTool 
-   *  @return statsu code 
+   *  @return status code 
    */
   virtual StatusCode initialize   ();
   
@@ -63,7 +66,7 @@ public :
    *  @see GiGaBase 
    *  @see   AlgTool 
    *  @see  IAlgTool 
-   *  @return statsu code 
+   *  @return status code 
    */
   virtual StatusCode finalize    ();
   
@@ -155,6 +158,41 @@ private:
   double                                      m_zmax                ;
   
   CaloHitsCollection*                         m_collection          ;
+
+  double                                      m_c1                  ;
+  double                                      m_c2                  ;
+  double                                      m_c1correction        ;
+
+  double                                      m_a_local_inner_ecal  ;
+  double                                      m_a_local_middle_ecal ;
+  double                                      m_a_local_outer_ecal  ;
+
+  double                                      m_ecalTimeHisto[2][25] ;
+
+public:
+
+  /** Correction factor from Birk's Law
+   *  Factor = 1/(1+C1*dEdx/rho+C2*(dEdx/rho)^2)
+   *  Where :
+   *      - C1 = 0.013 g/MeV/cm^2 [Ref NIM 80 (1970) 239]
+   *      - C2 = 9.6.10^-6 g^2/MeV^2/cm^4
+   *      - dEdx in MeV/cm
+   *      - rho = density in g/cm^3
+   */
+  double birkCorrection( G4Step* step )                             ;
+
+  /** The energy deposited in the cell by the step contributes 
+   *  - frac1*energy in the time bin timeBin (25 ns bins)
+   *  - frac2*energy in the time bin timeBin+1
+   *  the origin is the time of collision
+   */
+  void timeFraction( G4Step* step , const CaloCellID& cell ,
+                     double& frac1 , double& frac2 , int& timeBin ) ;
+
+  /** Correction due to the local non uniformity due to the light 
+   *  collection efficiency in cell cell
+   */
+  double localNonUniformity( G4Step* step , const CaloCellID& cell ) ;  
   
 };
 // ============================================================================
