ecf4e5ce4cc5084cc894de4c7a8a43085f16d975
Witold Pokorski <witold.pokorski@cern.ch>
2003-06-17 15:55:23 +0000

    added check if UserInfo exists
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@6123 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cd4a33f91db6413f0f0e943a65ef37662ab95201

--- Gauss/Sim/GaussRICH/src/Misc/RichG4CherenkovPhotProdTag.cpp
+++ Gauss/Sim/GaussRICH/src/Misc/RichG4CherenkovPhotProdTag.cpp
@@ -1,4 +1,3 @@
-
 #include "GaussTools/GaussTrackInformation.h"
 #include "RichInfo.h"
 #include "RichPhotInfo.h"
@@ -12,46 +11,62 @@ G4Track* RichG4CherenkovPhotProdTag(const G4Track& aChTrack,
                                     G4Track* aCkvPhotTrack,
                                     const G4double  CkvCosTheta,
                                     const G4double CkvPhi,
-                                    const G4double CkvPhotEnergy ){
+                                    const G4double CkvPhotEnergy )
+{
   // The test on the Z coord is commented out for now.
   // it can be uncommented in the future.
   //    G4double PhotonOrigZcoord= aCkvPhotTrack->GetVertexPosition().z();
-    //    if((   PhotonOrigZcoord >= ZUpsRich1Analysis 
-    //       && PhotonOrigZcoord <= ZDnsRich1Analysis) ||
-    //      ( PhotonOrigZcoord >= ZUpsRich2Analysis 
-    //      && PhotonOrigZcoord <= ZDnsRich2Analysis) ) {
-    //          RichGiGaPhotTrackInformation* aRichPhotTrackInfo 
-    //                =new RichGiGaPhotTrackInformation();
-    //          RichPhotInfo* aPhotInfo=aRichPhotTrackInfo->
-    //          AttachedRichPhotInfo();
-              
-          const G4DynamicParticle* aChTrackParticle 
-                        = aChTrack.GetDynamicParticle(); 
+  //    if((   PhotonOrigZcoord >= ZUpsRich1Analysis 
+  //       && PhotonOrigZcoord <= ZDnsRich1Analysis) ||
+  //      ( PhotonOrigZcoord >= ZUpsRich2Analysis 
+  //      && PhotonOrigZcoord <= ZDnsRich2Analysis) ) {
+  //          RichGiGaPhotTrackInformation* aRichPhotTrackInfo 
+  //                =new RichGiGaPhotTrackInformation();
+  //          RichPhotInfo* aPhotInfo=aRichPhotTrackInfo->
+  //          AttachedRichPhotInfo();
+  
+  const G4DynamicParticle* aChTrackParticle 
+    = aChTrack.GetDynamicParticle(); 
+  
+  RichPhotInfo* aPhotInfo = new  RichPhotInfo();
+  aPhotInfo->
+    setMotherChTrackPDGcode(aChTrackParticle->
+                            GetDefinition()->GetPDGEncoding());
+  aPhotInfo->setCkvPhotonEnergyAtProd(CkvPhotEnergy);
+  aPhotInfo->setCkvAngleThetaAtProd(acos(CkvCosTheta));
+  aPhotInfo->setCkvAnglePhiAtProd(CkvPhi);
+  // G4double CurChTrakMom = 
+  //       aChTrackParticle->GetTotalMomentum();
+  // aPhotInfo->setChTrackMomAtProd( CurChTrakMom);
+  
+  aPhotInfo->
+    setChTrackMomAtProd(aChTrackParticle->GetTotalMomentum());
+  aPhotInfo->
+    setChTrackMomentumVector(aChTrackParticle->GetMomentum());  
+  
+  RichInfo* aCkvPhotonTypeRichInfo = new   RichInfo(aPhotInfo);
 
-          RichPhotInfo* aPhotInfo = new  RichPhotInfo();
-          aPhotInfo->
-                setMotherChTrackPDGcode(aChTrackParticle->
-                    GetDefinition()->GetPDGEncoding());
-          aPhotInfo->setCkvPhotonEnergyAtProd(CkvPhotEnergy);
-          aPhotInfo->setCkvAngleThetaAtProd(acos(CkvCosTheta));
-          aPhotInfo->setCkvAnglePhiAtProd(CkvPhi);
-          //    G4double CurChTrakMom = 
-          //       aChTrackParticle->GetTotalMomentum();
-          // aPhotInfo->setChTrackMomAtProd( CurChTrakMom);
+  // check if GaussTrackInformation already exists and if not
+  // attach it to the track
+  G4VUserTrackInformation* uinf = aCkvPhotTrack->GetUserInformation();
+  GaussTrackInformation* aRichPhotTrackInfo;
+  
+  if(uinf)
+    {
+      aRichPhotTrackInfo = (GaussTrackInformation*) uinf;
+    }
+  else
+    {
+      aRichPhotTrackInfo = new GaussTrackInformation(); 
+      aCkvPhotTrack->SetUserInformation(aRichPhotTrackInfo);
+    }    
+  //
+  
+  //GaussTrackInformation* aRichPhotTrackInfo = new  GaussTrackInformation;
+  //aCkvPhotTrack -> SetUserInformation(aRichPhotTrackInfo);
 
-          aPhotInfo->
-           setChTrackMomAtProd(aChTrackParticle->GetTotalMomentum());
-          aPhotInfo->
-           setChTrackMomentumVector(aChTrackParticle->GetMomentum());
-
-
-          RichInfo* aCkvPhotonTypeRichInfo = new   RichInfo(aPhotInfo);
-          GaussTrackInformation* aRichPhotTrackInfo 
-                =new  GaussTrackInformation;
-          aRichPhotTrackInfo-> setRichInfo(aCkvPhotonTypeRichInfo);
-          
-          aCkvPhotTrack->SetUserInformation(aRichPhotTrackInfo);
-          
-          //    }
-return aCkvPhotTrack; 
+  // set pointer to RichInfo in GaussTrackInformation
+  aRichPhotTrackInfo->setRichInfo(aCkvPhotonTypeRichInfo);
+  
+  return aCkvPhotTrack; 
 }
--- Gauss/Sim/GaussRICH/src/Misc/RichPEInfoAttach.cpp
+++ Gauss/Sim/GaussRICH/src/Misc/RichPEInfoAttach.cpp
@@ -5,91 +5,92 @@
 #include "RichPEInfo.h"
 #include "RichG4AnalysisConstGauss.h"
 
-G4Track* RichPEInfoAttach(const G4Track& aPhotonTk, G4Track* aPETk) {
-
+G4Track* RichPEInfoAttach(const G4Track& aPhotonTk, G4Track* aPETk) 
+{
   G4VUserTrackInformation* aTkInfo=aPhotonTk.GetUserInformation();
   GaussTrackInformation* CurRichPhotTrackInfo=
-                   (0 ==aTkInfo ) ? 0 : 
+    (0 ==aTkInfo ) ? 0 : 
                    dynamic_cast<GaussTrackInformation*>(aTkInfo);
-
+  
   // the test on photon origin is commented out for now. It 
-   // can be revived in the future.
-
+  // can be revived in the future.
+  
   G4double CurPhotonOrigZcoord= aPhotonTk.GetVertexPosition().z();
   //    if(( CurPhotonOrigZcoord >= ZUpsRich1Analysis && 
   //     CurPhotonOrigZcoord <= ZDnsRich1Analysis) ||
   //     ( CurPhotonOrigZcoord >= ZUpsRich2Analysis && 
   //     CurPhotonOrigZcoord <= ZDnsRich2Analysis) ) {
-
-           G4int CurRadiatorNumber=-1;
-           if(CurPhotonOrigZcoord >=AgelZBeginAnalysis && 
-              CurPhotonOrigZcoord <= AgelZEndAnalysis) {
-	             CurRadiatorNumber=0;
-           }else if(CurPhotonOrigZcoord >= C4F10ZBeginAnalysis && 
-                     CurPhotonOrigZcoord <= C4F10ZEndAnalysis) {
-                     CurRadiatorNumber=1;
-           }else if(CurPhotonOrigZcoord >= CF4ZBeginAnalysis &&  
-                     CurPhotonOrigZcoord <= CF4ZEndAnalysis) {
-             CurRadiatorNumber=2;
-           }
-           
-
-
-           RichPEInfo* CurRichPEInfo = new   RichPEInfo();
-           
-
-           if(CurRichPhotTrackInfo != 0 ){
-             if(CurRichPhotTrackInfo->richInfo()){                 
-                 RichInfo* aPhRichInfo= CurRichPhotTrackInfo->richInfo();
-                 if( aPhRichInfo !=0 && aPhRichInfo-> HasUserPhotInfo() ) {
-                                   
- 	               RichPhotInfo* aPhotInfo = 
-                   aPhRichInfo->RichPhotInformation() ;
-              
-                 if(aPhotInfo != 0 ) {	  
-	                 CurRichPEInfo->
-                   setMotherOfPhotonPDGcode(aPhotInfo-> MotherChTrackPDGcode());
-
-                   CurRichPEInfo-> 
-		                setPhotonEnergyAtCkvProd(aPhotInfo->CkvPhotonEnergyAtProd());
-                   CurRichPEInfo-> 
-                   setCherenkovThetaAtProd(aPhotInfo->CkvAngleThetaAtProd());
-                   CurRichPEInfo->
-                   setCherenkovPhiAtProd(aPhotInfo->CkvAnglePhiAtProd());
-                   CurRichPEInfo->
-                   setMotherofPhotonMomAtProd(aPhotInfo->ChTrackMomAtProd() );
-                   CurRichPEInfo->
-                    setMotherofPhotonMomVectAtProd
-                     (aPhotInfo->ChTrackMomentumVector() );
+  
+  G4int CurRadiatorNumber=-1;
+  if(CurPhotonOrigZcoord >=AgelZBeginAnalysis && 
+     CurPhotonOrigZcoord <= AgelZEndAnalysis) {
+    CurRadiatorNumber=0;
+  }else if(CurPhotonOrigZcoord >= C4F10ZBeginAnalysis && 
+           CurPhotonOrigZcoord <= C4F10ZEndAnalysis) {
+    CurRadiatorNumber=1;
+  }else if(CurPhotonOrigZcoord >= CF4ZBeginAnalysis &&  
+           CurPhotonOrigZcoord <= CF4ZEndAnalysis) {
+    CurRadiatorNumber=2;
+  }
+  
+  RichPEInfo* CurRichPEInfo = new   RichPEInfo();  
+  
+  if(CurRichPhotTrackInfo != 0 ){
+    if(CurRichPhotTrackInfo->richInfo()){                 
+      RichInfo* aPhRichInfo= CurRichPhotTrackInfo->richInfo();
+      if( aPhRichInfo !=0 && aPhRichInfo-> HasUserPhotInfo() ) {
+        
+        RichPhotInfo* aPhotInfo = 
+          aPhRichInfo->RichPhotInformation() ;
+        
+        if(aPhotInfo != 0 ) {	  
+          CurRichPEInfo->
+            setMotherOfPhotonPDGcode(aPhotInfo-> MotherChTrackPDGcode());
+          
+          CurRichPEInfo-> 
+            setPhotonEnergyAtCkvProd(aPhotInfo->CkvPhotonEnergyAtProd());
+          CurRichPEInfo-> 
+            setCherenkovThetaAtProd(aPhotInfo->CkvAngleThetaAtProd());
+          CurRichPEInfo->
+            setCherenkovPhiAtProd(aPhotInfo->CkvAnglePhiAtProd());
+          CurRichPEInfo->
+            setMotherofPhotonMomAtProd(aPhotInfo->ChTrackMomAtProd() );
+          CurRichPEInfo->
+            setMotherofPhotonMomVectAtProd
+            (aPhotInfo->ChTrackMomentumVector() );
                    
-                 }                
-                 
-                 }                 
-                 
-             }  
-             
-             
-           }
-           
-           
-           
-           
-           
-           CurRichPEInfo-> setPhotonEmisPoint(aPhotonTk.GetVertexPosition());
-           CurRichPEInfo->  setMotherOfPhotonId(aPhotonTk.GetParentID());
-           CurRichPEInfo->  setPhotOriginRadiatorNumber(CurRadiatorNumber);
+        } 
+      } 
+    }  
+  }
+  
+  CurRichPEInfo-> setPhotonEmisPoint(aPhotonTk.GetVertexPosition());
+  CurRichPEInfo->  setMotherOfPhotonId(aPhotonTk.GetParentID());
+  CurRichPEInfo->  setPhotOriginRadiatorNumber(CurRadiatorNumber);
+  
+  RichInfo* aPETypeRichInfo = new  RichInfo( CurRichPEInfo);
 
-           RichInfo* aPETypeRichInfo = new  RichInfo( CurRichPEInfo);
-           GaussTrackInformation* aRichPETrackInfo = 
-                                       new  GaussTrackInformation();
-           aRichPETrackInfo->setRichInfo( aPETypeRichInfo);
-           
-
-           aPETk->SetUserInformation(aRichPETrackInfo);
-
-           
-           //    }
-    
+  // check if GaussTrackInformation already exists and if not
+  // attach it to the track
+  G4VUserTrackInformation* uinf = aPETk->GetUserInformation();
+  GaussTrackInformation* aRichPETrackInfo;
+  
+  if(uinf)
+    {
+      aRichPETrackInfo = (GaussTrackInformation*) uinf;
+    }
+  else
+    {
+      aRichPETrackInfo = new GaussTrackInformation(); 
+      aPETk->SetUserInformation(aRichPETrackInfo);
+    }    
+  //
+  
+  //  GaussTrackInformation* aRichPETrackInfo = 
+  //    new  GaussTrackInformation();
+  //  aPETk->SetUserInformation(aRichPETrackInfo);
+  
+  aRichPETrackInfo->setRichInfo(aPETypeRichInfo);
   return aPETk;
 }
 
