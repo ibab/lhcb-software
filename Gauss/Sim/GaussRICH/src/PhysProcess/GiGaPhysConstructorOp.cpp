bc600b63fe39af8ecb3eb587a2f070cee0f427dd
Sajan Easo <sajan.easo@cern.ch>
2003-07-16 13:59:08 +0000

    CommitWithoutPe
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@6216 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 397e51e11a924c985fad1f06e39a2f816585112f

--- Gauss/Sim/GaussRICH/src/PhysProcess/GiGaPhysConstructorOp.cpp
+++ Gauss/Sim/GaussRICH/src/PhysProcess/GiGaPhysConstructorOp.cpp
@@ -49,7 +49,7 @@ GiGaPhysConstructorOp::~GiGaPhysConstructorOp(){};
 // ============================================================================
 void GiGaPhysConstructorOp::ConstructParticle()
 {
-   RichPhotoElectron::PhotoElectronDefinition(); 
+  //   RichPhotoElectron::PhotoElectronDefinition(); 
 };
 
 // ============================================================================
