3e9e6e33c1fdf9d27dd5b59c085325a868b4bbb4
Marco Cattaneo <marco.cattaneo@cern.ch>
2003-09-25 12:28:33 +0000

    add HltVertex
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@33284 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d921a50b79c6a55d6926ee508846c27cfc953fd8

--- LHCb/Event/HltEvent/src/HltEvent_load.cpp
+++ LHCb/Event/HltEvent/src/HltEvent_load.cpp
@@ -1,17 +1,23 @@
-// $Id: HltEvent_load.cpp,v 1.2 2003-06-03 06:17:34 cattanem Exp $
+// $Id: HltEvent_load.cpp,v 1.3 2003-09-25 12:28:33 cattanem Exp $
 // Include files 
 
 #include "Event/EventFactoryDefs.h"
 
 /// ====================================================================
 #include "Event/HltTrack.h"
-
 _ImplementContainedObjectFactory( HltTrack )
 _ImplementDataObjectFactory( HltTracks )
+
+#include "Event/HltVertex.h"
+_ImplementContainedObjectFactory( HltVertex )
+_ImplementDataObjectFactory( HltVertices )
+
 
 void HltEvent_load() {
   DLL_DECL_OBJECTFACTORY( HltTrack  );
   DLL_DECL_OBJECTFACTORY( HltTracks );
+  DLL_DECL_OBJECTFACTORY( HltVertex );
+  DLL_DECL_OBJECTFACTORY( HltVertices );
 }
 
 extern "C" void HltEvent_loadRef() 
