e44e8d8e6aecef2c655cee846e6818b754ea209b
Marco Cattaneo <marco.cattaneo@cern.ch>
2003-11-24 13:56:40 +0000

    fix for OS X
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3159 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d728bbacb86980f209dd6fa0d753a1f92026519a

--- LHCb/Tools/XmlTools/src/component/XmlTools_dll.cpp
+++ LHCb/Tools/XmlTools/src/component/XmlTools_dll.cpp
@@ -1,28 +1,5 @@
-// $Id: XmlTools_dll.cpp,v 1.1.1.1 2003-04-23 13:38:46 sponce Exp $
+// $Id: XmlTools_dll.cpp,v 1.2 2003-11-24 13:56:40 cattanem Exp $
 
-/**
- * Package    : XmlTools
- * Implementation of DllMain routine.
- * The DLL initialisation must be done seperately for each DLL. 
- *
- * @author Sebastien Ponce
- */
+#include "GaudiKernel/LoadFactoryEntries.h"
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-
-void GaudiDll::initialize(void* /*hinstDLL*/) {}
-
-void GaudiDll::finalize(void* /*hinstDLL*/) {}
-
-extern void XmlTools_load();
-#include "GaudiKernel/FactoryTable.h"
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {
-    XmlTools_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
-
+LOAD_FACTORY_ENTRIES(XmlTools)
