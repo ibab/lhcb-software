40230aacd5d974a44ffa7d4890acf6d84378660d
Marco Cattaneo <marco.cattaneo@cern.ch>
2003-11-24 15:38:25 +0000

    fix for mac os X
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3164 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 09a9f1601996a005de8a2fd858163ea9315e9c29

--- LHCb/Det/DetDescCnv/src/component/DetDescCnv_dll.cpp
+++ LHCb/Det/DetDescCnv/src/component/DetDescCnv_dll.cpp
@@ -1,32 +1,4 @@
-//====================================================================
-//  DetdescCnv_dll.cpp
-//--------------------------------------------------------------------
-//
-//  Package    : DetDescCnv
-//
-//  Description: Implementation of DllMain routine.
-//               The DLL initialisation must be done seperately for 
-//               each DLL. 
-//
-//  Author     : S. Ponce
-//
-//====================================================================
+// $Id: DetDescCnv_dll.cpp,v 1.2 2003-11-24 15:38:25 cattanem Exp $
+#include "GaudiKernel/LoadFactoryEntries.h"
 
-// DllMain entry point
-#include "GaudiKernel/DllMain.icpp"
-
-void GaudiDll::initialize(void* /*hinstDLL*/) {}
-
-void GaudiDll::finalize(void* /*hinstDLL*/) {}
-
-extern void DetDescCnv_load();
-#include "GaudiKernel/FactoryTable.h"
-extern "C" FactoryTable::EntryList* getFactoryEntries() {
-  static bool first = true;
-  if ( first ) {
-    DetDescCnv_load();
-    first = false;
-  }
-  return FactoryTable::instance()->getEntries();
-} 
-
+LOAD_FACTORY_ENTRIES(DetDescCnv)
