4968e0ca5b0fa8bd203ee0f24517f6e23122aeab
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-01-22 22:35:43 +0000

    update for new Bender
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@87609 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b6dc959e858f35c07a97d94d13376a1ea470eeb8

--- Bender/Ex/BenderExample/python/B2PhiK0S.py
+++ Bender/Ex/BenderExample/python/B2PhiK0S.py
@@ -1,4 +1,4 @@
-# $Id: B2PhiK0S.py,v 1.1 2003-12-07 16:14:30 ibelyaev Exp $
+# $Id: B2PhiK0S.py,v 1.2 2004-01-22 22:35:43 ibelyaev Exp $
 # =============================================================================
 # CVS tag $Name: not supported by cvs2svn $
 # =============================================================================
@@ -9,9 +9,15 @@
 # date    2003-12-04
 # =============================================================================
 
-from BenderModule import *
- 
-class B2PhiK0S(Bender):
+# import the Bender itself  
+from   bendermodule import *
+import benderconfig as bender 
+
+g.JobOptionsType = 'NONE'
+g.OutputLevel = 3
+g.config()
+
+class B2PhiK0S(Algo):
     "Simple selection/analysis algorithm for Bd -> Phi(K+K-) K0S(pi+pi-) decay"
     def analyse ( self ) :
 
@@ -19,7 +25,8 @@ class B2PhiK0S(Bender):
         self.setFilterPassed( FALSE )
         
         # all primary vertices
-        primaries = self.vselect ( tag = "PVs" , cuts = VTYPE == VertexType.Primary )
+        # primaries = self.vselect ( tag = "PVs" , cuts = VTYPE == VertexType.Primary )
+        primaries = self.vselect ( tag = "PVs" , cuts = VTYPE == 1 )
         if primaries.empty() : return SUCCESS                                    # RETURN
         
         # select all reconstructed K0S 
@@ -28,17 +35,18 @@ class B2PhiK0S(Bender):
     
         # select kaons for Phi-> K+ K-  reconstruction  
         mips = MIPCHI2( primaries , self.geo() )
-        kaons     = self.select  ( tag = "K"   , cuts = abs( ID ) == 321   )
-        if kaons.empty()   : return SUCCESS                                      # RETURN
-        kplus     = self.select  ( tag = "K+"  , source = kaons , cuts = Q >  0.5 )
-        kminus    = self.select  ( tag = "K-"  , source = kaons , cuts = Q < -0.5 )
+        # kaons     = self.select  ( tag = "K"   , cuts = abs( ID ) == 321   )
+        kplus     = self.select  ( tag = "K+"  , cuts = ID ==  321  )
+        kminus    = self.select  ( tag = "K-"  , cuts = ID == -321 )
+        if kplus.empty()  : return SUCCESS                                      # RETURN
+        if kminus.empty() : return SUCCESS                                      # RETURN
         
         # some MC truth indfromation
         mc     = self.mctruth()
         # lists of all true B0, true K0S and true Phi  
-        B0mc   = mc.findDecays ( decay = " B0        -> phi(1020) KS0 " )  
-        K0Smc  = mc.findDecays ( decay = " KS0       -> pi+       pi- " )
-        PHImc  = mc.findDecays ( decay = " phi(1020) -> K+        K-  " )
+        B0mc   = mc.find ( decay = " B0        -> phi(1020) KS0 " )  
+        K0Smc  = mc.find ( decay = " KS0       -> pi+       pi- " )
+        PHImc  = mc.find ( decay = " phi(1020) -> K+        K-  " )
 
         # MC cut for true B0, true K0S and true Phi  
         mcB0   = MCTRUTH       ( mc , B0mc  )
@@ -46,53 +54,53 @@ class B2PhiK0S(Bender):
         mcPHI  = MCTRUTH       ( mc , PHImc )
         
         # cut on phi delta mass 
-        dmPHI  = abs( DMASS("phi(1020)") ) <= 20 * MeV 
-        
+        # dmPHI  = abs( DMASS("phi(1020)") ) <= 20 * MeV 
+
         phis  = self.loop( formula='K+ K-' , pid="phi(1020)" )
         for phi in phis :
             if phi.mass(1,2) > 1050 * MeV             : continue
+            if not phi()                              : continue 
             if VCHI2( phi )  > 100                    : continue
-            if not dmPHI( phi )                       : continue
-            phi.save("phi")
-
+            #if not dmPHI( phi )                       : continue
+            phi.save( "phi" )
+            
         # check selected phis 
-        Phis = self.selected("phi") 
+        Phis = self.selected("phi")
         if Phis.empty()    :  return SUCCESS                                    # RETURN
 
         # some general information
-        evtHdr = self.evthdr_1()
-        genHdr = self.genhdr_1()
-        l0     = self.l0_1    ()
-        l1     = self.l1_1    ()
         
-        tuple = self.ntuple ( name = "B0 -> phi K0S analysis " )
+        tuple = self.ntuple ( title = "B0 -> phi K0S analysis " )
         B0s   = self.loop( formula = "phi K0S" , pid = "B0")
         for B0 in B0s :
             if B0.mass(1,2)   > 6 * GeV          : continue
             if B0.mass(1,2)   < 4 * GeV          : continue
             if VCHI2( B0 )    > 49               : continue
+            
+            if not B0() : continue 
             Phi = B0(1)
             K0S = B0(2)
+            if not Phi  : continue 
+            if not K0S  : continue 
             K1  = child( Phi , 1 ) 
             K2  = child( Phi , 2 ) 
             pi1 = child( K0S , 1 ) 
             pi2 = child( K0S , 2 ) 
 
+            if not  K1  : continue 
+            if not  K2  : continue 
+            if not pi1  : continue 
+            if not pi2  : continue 
+
             # find 'own' primary vertex
             vip = VIP( particle( B0 ) , self.geo() )
-            primary = selectPrimaryVertexMin ( primaries , vip  )
-            if None == primary                   : continue
+            primary = SelectPrimaryVertexMin ( primaries , vip  )
+            if not primary : continue
+            
             B0.setPV( primary )
 
-            # general information 
-            tuple.column  ( name = ""         , value = evtHdr           )
-            tuple.column  ( name = "EvType"   , value = genHdr.evType () )
-            tuple.column  ( name = "L0"       , value = l0.decision   () )
-            tuple.column  ( name = "L0Type"   , value = l0.typeL0Trig () )
-            tuple.column  ( name = "L1"       , value = l1.decision   () )
-
             # multiplicity of primary vertices 
-            tuple.column  ( name = "nPVs"     , value = primaries.size() )
+            #tuple.column  ( name = "nPVs"     , value = primaries.size() )
 
             # masses 
             tuple.column  ( name = "B0Mass"   , value = M     ( B0  )   /  GeV   ) 
@@ -110,28 +118,28 @@ class B2PhiK0S(Bender):
             tuple.column  ( name = "mipK0S"   , value = mips ( K0S ) )
             tuple.column  ( name = "mipB0"    , value = mips ( B0  ) )
             
-            ctau = VDTIME ( self.point( vertex = primary ) )
-            tuple.column  ( name = "B0Ctau"   , value = ctau( B0 ) )
-
-            dira = DIRA   ( self.point( vertex = primary ) )
-            tuple.column  ( name = "B0DirA"   , value = dira( B0 ) )
+            #ctau = VDTIME ( self.point( vertex = primary ) )
+            #tuple.column  ( name = "B0Ctau"   , value = ctau( B0 ) )
+            
+            #dira = DIRA   ( self.point( vertex = primary ) )
+            #tuple.column  ( name = "B0DirA"   , value = dira( B0 ) )
             
             # vertex positions 
-            tuple.column  ( name = "B0Vx"      , value = vertex( B0  ) .position() )
-            tuple.column  ( name = "PhiVx"     , value = vertex( Phi ) .position() )
-            tuple.column  ( name = "K0SVx"     , value = vertex( K0S ) .position() )
-            tuple.column  ( name = "PVVx"      , value = primary       .position() )
+            #tuple.column  ( name = "B0Vx"      , value = vertex( B0  ) .position() )
+            #tuple.column  ( name = "PhiVx"     , value = vertex( Phi ) .position() )
+            #tuple.column  ( name = "K0SVx"     , value = vertex( K0S ) .position() )
+            #tuple.column  ( name = "PVVx"      , value = primary       .position() )
             
-            tuple.column  ( name = "B0P"       , value = B0  .momentum() ) ;
-            tuple.column  ( name = "PhiP"      , value = Phi .momentum() ) ;
-            tuple.column  ( name = "K0SP"      , value = K0S .momentum() ) ;
+            #tuple.column  ( name = "B0P"       , value = B0  .momentum() ) ;
+            #tuple.column  ( name = "PhiP"      , value = Phi .momentum() ) ;
+            #tuple.column  ( name = "K0SP"      , value = K0S .momentum() ) ;
             
             tuple.column  ( name = "LV01"      , value = LV01( B0  ) ) ;
             tuple.column  ( name = "LV01K0"    , value = LV01( K0S ) ) ;
 
-            tuple.column  ( name = "B0MC"      , value = mcB0  ( B0  ) ) ;
-            tuple.column  ( name = "PHIMC"     , value = mcPHI ( Phi ) ) ;
-            tuple.column  ( name = "K0SMC"     , value = mcK0S ( K0S ) ) ;
+            #tuple.column  ( name = "B0MC"      , value = mcB0  ( B0  ) ) ;
+            #tuple.column  ( name = "PHIMC"     , value = mcPHI ( Phi ) ) ;
+            #tuple.column  ( name = "K0SMC"     , value = mcK0S ( K0S ) ) ;
             
             tuple.write()
             B0.save("B0")
@@ -139,22 +147,73 @@ class B2PhiK0S(Bender):
         # all selected B0
         B0S = self.selected("B0")
         if not B0S.empty() : self.setFilterPassed( TRUE )
-
+        
         return SUCCESS
 
-    
-    
-# create my algorithm 
-myAlg = B2PhiK0S('B2PhiK0S')
+
+# =============================================================================
+# Generic job configuration 
+# =============================================================================
+
+bender.config( files   = [ '$DAVINCIROOT/options/DaVinci.opts' ] ,
+               options = [ 'EcalPIDmu.OutputLevel   =   5  '   ,
+                           'HcalPIDmu.OutputLevel   =   5  '   ,
+                           'EcalPIDe.OutputLevel    =   5  '   ,
+                           'HcalPIDe.OutputLevel    =   5  '   ,
+                           'BremPIDe.OutputLevel    =   5  '   ,
+                           'PrsPIDe.OutputLevel     =   5  '   ,
+                           'EventSelector.PrintFreq =  10  '   ] )
+
+g.HistogramPersistency = "HBOOK" ;
+
+# =============================================================================
+# specific job configuration 
+# =============================================================================
+
+bender.config( files   = [ '$KS2PIPISELROOT/options/PreLoadPions.opts'  ,
+                           '$KS2PIPISELROOT/options/bestKs2PiPi.opts' ] ,
+               options = [ 'loadUPions.OutputLevel   = 4 ' ,
+                           'loadLPions.OutputLevel   = 4 ' ,
+                           'loadVTTPions.OutputLevel = 4 ' ,
+                           'LLKs2PiPi.OutputLevel    = 4 ' , 
+                           'UUKs2PiPi.OutputLevel    = 4 ' , 
+                           'LVKs2PiPi.OutputLevel    = 4 ' , 
+                           'LUKs2PiPi.OutputLevel    = 4 ' , 
+                           'LLKs2PiPi.UnconstVertexFitter.OutputLevel = 5 ' ,
+                           'LVKs2PiPi.UnconstVertexFitter.OutputLevel = 5 ' ,
+                           'LUKs2PiPi.UnconstVertexFitter.OutputLevel = 5 ' , 
+                           'UUKs2PiPi.UnconstVertexFitter.OutputLevel = 5 ' ,
+                           'NeutralPP2MC.InputData = {} '   ] )
+
+# preload algorithm(s)
+g.topAlg += [ 'LoKiPreLoad/Charged']
+
+# configure the preload algorithm(s) 
+preload  = g.property('Charged')
+preload.OutputLevel = 4
+
+desktop                   = g.property('Charged.PhysDesktop')
+desktop.ParticleMakerType = "CombinedParticleMaker"
+desktop.OutputLocation    = "/Event/Phys/Charged"
+
+maker                    = g.property('Charged.PhysDesktop.CombinedParticleMaker')
+maker.ExclusiveSelection = 0
+maker.Particles          = [ "kaon" , "pion" ]
+maker.KaonSelection      = [ "det='RICH' k-pi='-5.0'   k-p='-5.0' "]
+maker.PionSelection      = [ "det='RICH' pi-k='-5.0' "]
+ 
+
+# create my algorithm and add it to top level algorithms 
+alg = B2PhiK0S('B2PhiK0S')
+g.topAlg += ['B2PhiK0S']
 
 # configure my algorithm 
-prop1 = Property('B2PhiK0S')
-# prop1.OutputLevel = 4
-prop1.TupleLUN    = 'PHIK0S'
+alg = gaudi.iProperty('B2PhiK0S')
+alg.TupleLUN    = 'PHIK0S'
 # confugure my desktop
 
 # configure own desktop 
-desktop                 = Property('B2PhiK0S.PhysDesktop')
+desktop                 = g.property('B2PhiK0S.PhysDesktop')
 desktop.InputLocations  = [ "/Event/Phys/Charged" ,
                             "/Event/Phys/LLKs"    ,
                             "/Event/Phys/UUKs"    ,
@@ -162,23 +221,34 @@ desktop.InputLocations  = [ "/Event/Phys/Charged" ,
                             "/Event/Phys/LVKs"    ]
 desktop.OutputLocation  =   "/Event/Phys/B2PhiK0S"
 
-# initialize my algorithm 
-myAlg.initialize()
 
-# append my algorithm to the list of top level algorithms 
-g.topAlg = g.topAlg + [ 'B2PhiK0S' ] 
+# output histogram file 
+hsvc = g.property( 'HistogramPersistencySvc' )
+hsvc.OutputFile = 'B2PhiK0S.hbook'
 
-# execute algorithm (main Gaudi event loop)
-g.execute(1)
+# input data 
+g.readOptions('/afs/cern.ch/lhcb/project/web/cards/412000.opts')
 
-# finalize my algorithm
-# myAlg.finalize()
 
-# say: "Good Bye, everybody!"
-# g.exit()
+# N-tuples
+tup = gaudi.iProperty( 'NTupleSvc' )
+tup.Output = [ "PHIK0S DATAFILE='B2PhiK0S_tup.hbook' TYP='HBOOK' OPT='NEW'" ]
 
+# =============================================================================
+# job execution 
+# =============================================================================
+
+g.initialize()
+
+#g.run(100)  ## crash !!
+g._evtpro.executeRun(100)
+
+g.exit()
 # =============================================================================
 # The END 
 # =============================================================================
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2003/12/07 16:14:30  ibelyaev
+#  update for Bender application and project
+#
 # =============================================================================
--- Bender/Ex/BenderExample/python/Phi.py
+++ Bender/Ex/BenderExample/python/Phi.py
@@ -1,4 +1,6 @@
-# $Id: Phi.py,v 1.1 2003-12-07 16:14:30 ibelyaev Exp $
+#!/usr/bin/env python
+# =============================================================================
+# $Id: Phi.py,v 1.2 2004-01-22 22:35:43 ibelyaev Exp $
 # =============================================================================
 # CVS tag $Name: not supported by cvs2svn $
 # =============================================================================
@@ -9,52 +11,111 @@
 # @date   2003-12-04
 # =============================================================================
 
-# import the bender itself  
-from BenderModule import *
+# import the Bender itself  
+from   bendermodule import *
+import benderconfig as bender 
 
-# create my own algorithm  
-class Phi(Bender):
+g.JobOptionsType = 'NONE'
+g.OutputLevel = 3
+g.config()
+
+# =============================================================================
+# Specific physics analysis algorithm 
+# =============================================================================
+
+class Phi(Algo):
     " My own analysis algorithm for selection of phi -> K+ K- "
+    def __init__(self, name = 'Phi' ) :
+        Algo.__init__( self, name )
+        
     def analyse ( self ) :
-
+        # get kaons 
         kplus  = self.select( tag="K+" , cuts =  321 == ID )
         kminus = self.select( tag="K-" , cuts = -321 == ID )
-        
+        # create loop
         phis   = self.loop( formula = "K+ K-" , pid = "phi(1020)" )
+        # loop over KK combinations 
         for phi in phis:
             if phi.mass(1,2) > 1050 * MeV : continue
-            self.plot( title = "K+ K- mass" , value = M(phi)/MeV , low = 1000 , high = 1050 )
+            self.plot( title = "K+ K- mass"      ,
+                       value = M ( phi ) / MeV   ,
+                       low   = 1000              ,
+                       high  = 1050              )
             
         return SUCCESS 
+        
 
-#    create my algorithm 
-myAlg = Phi('Phi')
+# =============================================================================
+# Generic job configuration 
+# =============================================================================
 
-#def property( tag ):   # temporary trick!
-#    return Service(tag)
+bender.config( files   = [ '$DAVINCIROOT/options/DaVinci.opts' ] ,
+               options = [ 'EcalPIDmu.OutputLevel     =   5  ' ,
+                           'HcalPIDmu.OutputLevel     =   5  ' ,
+                           'EcalPIDe.OutputLevel      =   5  ' ,
+                           'HcalPIDe.OutputLevel      =   5  ' ,
+                           'BremPIDe.OutputLevel      =   5  ' ,
+                           'PrsPIDe.OutputLevel       =   5  ' ,
+                           'EventSelector.PrintFreq   =  10  ' ] )
 
-# configure my algorithm 
-p1 = Property('Phi')
-p1.OutputLevel = 4
-# configure my desktop 
-desktop                 = Property('Phi.PhysDesktop')
-desktop.InputLocations  = [ "/Event/Phys/Charged" ]
+g.HistogramPersistency = "HBOOK" ;
+
+# =============================================================================
+# specific job configuration 
+# =============================================================================
+
+# preload algorithm(s)
+g.topAlg += [ "LoKiPreLoad/Charged"]
+
+# configure the preload algorithm(s) 
+preload  = g.property('Charged')
+preload.OutputLevel = 4
+
+desktop                   = g.property('Charged.PhysDesktop')
+desktop.ParticleMakerType = "CombinedParticleMaker"
+desktop.OutputLocation    = "/Event/Phys/Charged"
+
+maker                    = g.property('Charged.PhysDesktop.CombinedParticleMaker')
+maker.ExclusiveSelection = 0
+maker.Particles          = [ "kaon" , "pion" ]
+maker.KaonSelection      = [ "det='RICH' k-pi='-5.0'   k-p='-5.0' "]
+maker.PionSelection      = [ "det='RICH' pi-k='-5.0' "]
+
+# create analysis algorithm and add it to the list of
+phi = Phi('Phi')
+
+g.topAlg += [ 'Phi' ]
+
+phi = gaudi.iProperty('Phi')
+phi.OutputLevel = 5
+
+desktop                 = g.property('Phi.PhysDesktop')
+desktop.InputLocations  = [ "/Event/Phys/Charged"]
 desktop.OutputLocation  =   "/Event/Phys/Phi"
 
-# initialize the algorithms
-myAlg.initialize()
+# output histogram file 
+hsvc = g.property( 'HistogramPersistencySvc' )
+hsvc.OutputFile = 'phi.hbook'
 
-# append it to the list of top level algorithms
-g.topAlg = g.topAlg + [ 'Phi' ] 
+# input data 
+g.readOptions('/afs/cern.ch/lhcb/project/web/cards/615300.opts')
 
-# execute 1 event 
-g.execute(1)
+# =============================================================================
+# job execution 
+# =============================================================================
 
-#myAlg.finalize()
-#g.exit()
+g.initialize()
+
+## g.run(100)  ## crash !!
+g._evtpro.executeRun(100)
+
+g.exit()
 
 # =============================================================================
 # The END 
 # =============================================================================
-# $Log: not supported by cvs2svn $ 
+# $Log: not supported by cvs2svn $
+# Revision 1.1  2003/12/07 16:14:30  ibelyaev
+#  update for Bender application and project
+# 
 # =============================================================================
