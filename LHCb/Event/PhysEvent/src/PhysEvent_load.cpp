7a4b1eeffc558a6f883a14e1ee51ef74a01d6c87
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-03-05 14:41:14 +0000

    Add SelResult...
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@33562 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 856606bbb3544f38aab27fa0f18033a8728bf85a

--- LHCb/Event/PhysEvent/src/PhysEvent_load.cpp
+++ LHCb/Event/PhysEvent/src/PhysEvent_load.cpp
@@ -1,4 +1,4 @@
-// $Id: PhysEvent_load.cpp,v 1.10 2003-12-18 15:33:29 cattanem Exp $
+// $Id: PhysEvent_load.cpp,v 1.11 2004-03-05 14:41:14 cattanem Exp $
 
 // Include files
 #include "GaudiKernel/ContainerFactoryDefs.h"
@@ -30,13 +30,9 @@ _ImplementContainedObjectFactory( FlavourTag )
 _ImplementDataObjectFactory( FlavourTags )
 
 /// ====================================================================
-#include "Event/PhysSel.h"
-_ImplementDataObjectFactory( PhysSel );
-
-// ===================================================================
-//#include "Event/AxPartCandidate.h"
-//_ImplementContainedObjectFactory( AxPartCandidate )
-//_ImplementDataObjectFactory( AxPartCandidates )
+#include "Event/SelResult.h"
+_ImplementContainedObjectFactory( SelResult );
+_ImplementDataObjectFactory( SelResults );
 
 // ===================================================================
 #include "Event/ProtoParticle.h"
@@ -51,9 +47,6 @@ _ImplementDataObjectFactory( V0s )
 // ====================================================================
 void PhysEvent_load()  {
 
-  // Declaration of data object factories
-  DLL_DECL_OBJECTFACTORY  ( PhysSel );
-
   // Declaration of contained object factories
   DLL_DECL_OBJECTFACTORY( Particle );
   DLL_DECL_OBJECTFACTORY( Particles );
@@ -67,8 +60,8 @@ void PhysEvent_load()  {
   DLL_DECL_OBJECTFACTORY( FlavourTag );
   DLL_DECL_OBJECTFACTORY( FlavourTags );
   
-  //DLL_DECL_OBJECTFACTORY( AxPartCandidate );
-  //DLL_DECL_OBJECTFACTORY( AxPartCandidates );
+  DLL_DECL_OBJECTFACTORY( SelResult );
+  DLL_DECL_OBJECTFACTORY( SelResults );
 
   DLL_DECL_OBJECTFACTORY( ProtoParticle );
   DLL_DECL_OBJECTFACTORY( ProtoParticles );
