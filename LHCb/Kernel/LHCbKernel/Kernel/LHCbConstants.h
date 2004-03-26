1971c7de293a6452624efa9178aaaa1e0354e690
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-03-26 08:16:17 +0000

    for v4r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46859 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 397192a7a0a34858a39fc22dd6a867a75142798e

---
+++ LHCb/Kernel/LHCbKernel/Kernel/LHCbConstants.h
@@ -0,0 +1,23 @@
+// $Id: LHCbConstants.h,v 1.1 2004-03-26 08:16:17 cattanem Exp $
+#ifndef KERNEL_LHCBCONSTANTS_H 
+#define KERNEL_LHCBCONSTANTS_H 1
+
+// Include files
+
+/** @file LHCbConstants.h Kernel/LHCbConstants.h
+ *  Namespace for constants of LHCb general interest 
+ *
+ *  @author Marco Cattaneo
+ *  @date   2004-03-26
+ */
+namespace LHCbConstants {
+
+  // Silicon parameters, used by ST silicon simulation (M.Needham)
+  static const double SiExcitationEnergy = 174*eV;
+  static const double SiEnergyPerIonPair = 3.6*eV;
+
+  // Description of Beetle chip (M.Needham)
+  static const unsigned int nStripsInPort = 32;
+
+};
+#endif // KERNEL_LHCBCONSTANTS_H
