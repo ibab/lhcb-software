4199696546ae41be9b60a21c5f109599c76690e2
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-04-20 04:28:21 +0000

    add the main program
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91341 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1a8e5126a555643d624c74282061f54b537e42b9

---
+++ Gauss/Sim/Gauss/src/GaussMain.cpp
@@ -0,0 +1,53 @@
+// $Id: GaussMain.cpp,v 1.1 2004-04-20 04:28:21 ibelyaev Exp $
+//------------------------------------------------------------------------------
+//
+//  Package    : GaudiConf
+//
+//  Description: Main Program for Gaudi applications
+//               Sets up default job options path as ../options/job.opts
+//
+//------------------------------------------------------------------------------
+// Include files
+#include "GaudiKernel/SmartIF.h"
+#include "GaudiKernel/Bootstrap.h"
+#include "GaudiKernel/IAppMgrUI.h"
+#include "GaudiKernel/IProperty.h"
+#include <iostream>
+
+#include "GiGa/IGiGaRunManager.h"
+
+//--- Example main program
+int main ( int argc, char** argv ) {
+  // Create an instance of an application manager
+  IInterface* iface = Gaudi::createApplicationMgr();
+  SmartIF<IProperty>     propMgr ( IID_IProperty, iface );
+  SmartIF<IAppMgrUI>     appMgr  ( IID_IAppMgrUI, iface );
+
+  if( !appMgr.isValid() || !propMgr.isValid() ) {
+    std::cout << "Fatal error while creating the ApplicationMgr " << std::endl;
+    return 1;
+  }
+
+  // Get the input configuration file from arguments
+  std:: string opts = (argc>1) ? argv[1] :  "";
+
+  propMgr->setProperty( "JobOptionsPath", opts );
+  if( opts != "" && opts.substr( opts.length() - 3, 3 ) == ".py" ) {
+    propMgr->setProperty( "EvtSel",         "NONE" );
+    propMgr->setProperty( "JobOptionsType", "NONE" );
+    propMgr->setProperty( "DLLs",           "['GaudiPython']" );
+    propMgr->setProperty( "Runable",        "PythonScriptingSvc" );
+  }
+
+  // Run the application manager and process events
+  appMgr->run();
+
+  // Release Application Manager
+  iface->release();
+
+  // destroy Geant4 
+  IGiGaRunManager::destroy() ;
+  
+  // All done - exit
+  return 0;
+}
