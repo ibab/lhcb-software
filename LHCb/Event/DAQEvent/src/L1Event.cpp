96d64347162c96b7b645d73cc17eb4ecaf9927a0
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-04-23 12:20:33 +0000

    fix bug 3130
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@33614 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df22c133dcaf6032c07b7967b4a9ec24c59b0328

--- LHCb/Event/DAQEvent/src/L1Event.cpp
+++ LHCb/Event/DAQEvent/src/L1Event.cpp
@@ -1,4 +1,4 @@
-// $Id: L1Event.cpp,v 1.3 2003-11-10 15:51:46 cattanem Exp $
+// $Id: L1Event.cpp,v 1.4 2004-04-23 12:20:33 cattanem Exp $
 // Include files 
 #include "GaudiKernel/Kernel.h"
 #include <stdio.h>
@@ -26,7 +26,7 @@ L1Event::L1Event( L1Buffer& l1Buffer ) {
   while( i < l1Size ){
     int bankSize = int(buffer[i]&127);
     //int sourceID =int(((buffer[i]>>7)&63));
-    int bankType  =int( (buffer[i]>>13));
+    int bankType = int( (buffer[i]>>13) &7 );
     // Add this bank address to Event Map
     (m_eventMap[bankType]).push_back(L1Bank(buffer+i));
     // Go to the next bank
