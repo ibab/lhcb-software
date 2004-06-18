7359c7ddde9ad4c1f4edde65f323d823f02e36a1
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2004-06-18 12:11:36 +0000

    ParticleMakerSeq
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62510 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4a87bd0879ea5d49b6647b10b867ac7ced9b4f33

---
+++ Phys/Phys/DaVinciTools/src/ParticleMakerSeq.cpp
@@ -0,0 +1,151 @@
+// $Id: ParticleMakerSeq.cpp,v 1.1 2004-06-18 12:11:36 pkoppenb Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $ 
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+// Include files 
+// ============================================================================
+// from Gaudi
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h"
+#include "GaudiKernel/MsgStream.h" 
+// ============================================================================
+// Event 
+// ============================================================================
+#include "Event/Particle.h"
+// ============================================================================
+// local
+// ============================================================================
+#include "ParticleMakerSeq.h"
+// ============================================================================
+
+/** @file 
+ * 
+ *  Implementation file for class : ParticleMakerSeq
+ *
+ *  @date 2004-04-29 
+ *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+ */
+
+// ============================================================================
+// Declaration of the Tool Factory
+// ============================================================================
+static const  ToolFactory<ParticleMakerSeq>         s_Factory ;
+const        IToolFactory&ParticleMakerSeqFactory = s_Factory ; 
+// ============================================================================
+
+
+// ============================================================================
+/// constructor 
+// ============================================================================
+ParticleMakerSeq::ParticleMakerSeq 
+( const std::string& type,
+  const std::string& name,
+  const IInterface* parent )
+  : GaudiTool ( type, name , parent )
+    , m_private (   )  
+    , m_public  (   )
+    , m_makers  (   ) 
+    , m_counter ( 0 ) 
+{
+  declareInterface<IParticleMaker>(this);
+
+  declareProperty( "PrivateMakers" , m_private ) ;
+  declareProperty( "PublicMakers"  , m_public  ) ;
+  
+};
+// ============================================================================
+
+
+// ============================================================================
+/// destructor 
+// ============================================================================
+ParticleMakerSeq::~ParticleMakerSeq() {}; 
+// ============================================================================
+
+// ============================================================================
+// ============================================================================
+StatusCode ParticleMakerSeq::initialize()
+{
+  StatusCode sc = GaudiTool::initialize() ;
+  if ( sc.isFailure() ) { return sc ; }
+   
+  m_makers.clear() ;
+  
+  { // locate private makers 
+    for ( Names::const_iterator i = m_private.begin() ; 
+          m_private.end() != i ; ++i ) 
+    {
+      IParticleMaker* maker = tool<IParticleMaker>( *i , this ) ;
+      if ( 0 == maker ) { return StatusCode::FAILURE ; }
+      m_makers.push_back( maker ) ;
+    }
+  }
+  { // locate public makers 
+    for ( Names::const_iterator i = m_public.begin() ; 
+          m_public.end() != i ; ++i ) 
+    {
+      IParticleMaker* maker = tool<IParticleMaker>( *i ) ;
+      if ( 0 == maker ) { return StatusCode::FAILURE ; }
+      m_makers.push_back( maker ) ;
+    }
+  }
+  
+  if ( m_makers.empty() ) 
+  { Warning ( " Empty list of available ParticleMakers" ) ; }
+  
+  return StatusCode::SUCCESS  ;
+  
+};
+// ============================================================================
+
+// ============================================================================
+// ============================================================================
+StatusCode ParticleMakerSeq::finalize()
+{
+  // clear list of makers 
+  m_makers.clear() ;
+  
+  MsgStream log( msgSvc() , name() ) ;
+  log << MSG::ALWAYS 
+      << " Created Particles = " << m_counter 
+      << " (Maker='" << name() <<"'" << endreq ;
+
+  return GaudiTool::finalize () ;
+};
+// ============================================================================
+
+// ============================================================================
+// ============================================================================
+StatusCode ParticleMakerSeq::makeParticles ( ParticleVector& particles )
+{
+  if ( m_makers.empty() ) { return StatusCode::SUCCESS ; }
+  
+  ParticleVector tmp ; 
+  for( Makers::const_iterator imaker = m_makers.begin() ;
+       m_makers.end() != imaker ; ++imaker ) 
+  {
+    IParticleMaker* maker = *imaker ;
+    if ( 0 == maker ) { continue ; }
+    tmp.clear() ; tmp.reserve(128) ;
+    StatusCode sc = maker->makeParticles ( tmp ) ;
+    if ( sc.isFailure() ) 
+    {
+      Error ( " Error from Maker='" 
+              + maker->type() + "/" 
+              + maker->name() + "'" , sc ) ;
+      continue ;
+    }
+    m_counter += tmp.size() ;
+    std::copy ( tmp.begin() , tmp.end() , std::back_inserter( particles ) ) ;
+  }
+  
+  return StatusCode::SUCCESS ;
+};
+// ============================================================================
+
+
+// ============================================================================
+// The END 
+// ============================================================================
---
+++ Phys/Phys/DaVinciTools/src/ParticleMakerSeq.h
@@ -0,0 +1,111 @@
+// $Id: ParticleMakerSeq.h,v 1.1 2004-06-18 12:11:36 pkoppenb Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $ 
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+#ifndef DAVINCITOOLS_PARTICLEMAKERSEQ_H 
+#define DAVINCITOOLS_PARTICLEMAKERSEQ_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// from STL
+// ============================================================================
+#include <string>
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h"
+// ============================================================================
+// from GaudiTools
+// ============================================================================
+#include "GaudiTools/GaudiTool.h"
+// ============================================================================
+// DaVincitTools 
+// ============================================================================
+#include "DaVinciTools/IParticleMaker.h"
+// ============================================================================
+
+
+/** @class ParticleMakerSeq ParticleMakerSeq.h
+ *
+ *  Simple particle maker which does delegates all its job to few concrete
+ *  Particle makers 
+ *  
+ *  In principel this maker does nor provide any gain in the code ,
+ *  but it results in a simplicication of job configuration
+ *  Preload algorithm become not nesessary anymore
+ *
+ *  In combines few "public" makers and few "private" makers
+ *  
+ *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+ *  @date   2004-04-29
+ */
+class ParticleMakerSeq : 
+  public         GaudiTool      ,
+  public virtual IParticleMaker 
+{
+  /// friend factory for instantiation
+  friend class ToolFactory<ParticleMakerSeq>;
+  //
+public:
+  
+  /** make the particles
+   *  @see IParticleMaker
+   *  @param particles (output) vector of Particles
+   *  @return status code 
+   */
+  virtual StatusCode makeParticles( ParticleVector & particles ) ;
+  
+  /// standard initializetion of the tool
+  virtual StatusCode initialize () ;
+
+  /// standard finalization of the tool
+  virtual StatusCode finalize   () ;
+
+protected:
+
+  /** Standard constructor
+   *  @see GaudiTool
+   *  @see AlgTool
+   *  @see ToolFactory
+   *  @see IToolFactory
+   *  @param type tool type (?)
+   *  @param name tool name 
+   *  @param parent tool's parent
+   */
+  ParticleMakerSeq 
+  ( const std::string& type   ,
+    const std::string& name   ,
+    const IInterface*  parent ) ;
+  
+  /// destructor 
+  virtual ~ParticleMakerSeq ();  
+
+private:
+
+  // default constructor is private
+  ParticleMakerSeq() ;
+  // copy    constructor is private
+  ParticleMakerSeq( const ParticleMakerSeq& ) ;
+  // assignement operator is private is private
+  ParticleMakerSeq& operator=( const ParticleMakerSeq& ) ;
+  
+private:
+
+  typedef std::vector<std::string>     Names   ;
+  typedef std::vector<IParticleMaker*> Makers  ;
+  typedef unsigned long                Counter ;
+  
+  Names   m_private ;
+  Names   m_public  ;
+  Makers  m_makers  ;
+  Counter m_counter ;
+  
+};
+
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // PARTICLEMAKERSEQ_H
+// ============================================================================
