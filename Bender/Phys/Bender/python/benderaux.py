84e0727320f3acf68435dab33561d3c1d19a130f
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-07-25 12:06:13 +0000

    remove Bender.RootHelper
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@87689 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f4dfc7a3eca7f266c7fd73065005bf956db1304a

--- Bender/Phys/Bender/python/benderaux.py
+++ Bender/Phys/Bender/python/benderaux.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: benderaux.py,v 1.3 2004-07-24 14:05:00 ibelyaev Exp $ 
+# $Id: benderaux.py,v 1.4 2004-07-25 12:06:13 ibelyaev Exp $ 
 # =============================================================================
 # CVS tag $Name: not supported by cvs2svn $ 
 # =============================================================================
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2004/07/24 14:05:00  ibelyaev
+#  v3r5
+#
 # Revision 1.2  2004/07/23 13:49:23  ibelyaev
 # *** empty log message ***
 #
@@ -77,11 +80,8 @@ VertexType.Brem            = 5
 VertexType.Pair            = 6
 VertexType.Kink            = 7
 
-# extract the native ROOT histograms from AIDA
-aida2root = gaudi.gbl.Bender.RootHelper.aida2root
 
 fillStream             = gbl.Bender.FillStream.fillStream 
-
 
 # import useful functions
 vertex                 = gbl.Bender.Accessors.vertex
