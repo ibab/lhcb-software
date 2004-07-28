5ae3875c6edd626cd6dbfe3fd31f63a0ec2e0a8f
Christopher Rob Jones <jonesc@cern.ch>
2004-07-28 11:58:23 +0000

    Remove typing error
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@6822 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 881e1c486b9c2e0917bec5cbfb8307036007d2fa

--- Rec/Rich/RichRecQC/src/RichRecQC_load.cpp
+++ Rec/Rich/RichRecQC/src/RichRecQC_load.cpp
@@ -4,8 +4,11 @@
  *  Declaration of objects in component library RichRecQC
  *
  *  CVS Log :-
- *  $Id: RichRecQC_load.cpp,v 1.2 2004-07-27 13:56:30 jonrob Exp $
+ *  $Id: RichRecQC_load.cpp,v 1.3 2004-07-28 11:58:23 jonesc Exp $
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.2  2004/07/27 13:56:30  jonrob
+ *  Add doxygen file documentation and CVS information
+ *
  *
  *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
  *  @date   2002-06-13
@@ -13,7 +16,7 @@
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
-DECLARE_FACTORY_ENTRIES ( RichRecQC ) #
+DECLARE_FACTORY_ENTRIES ( RichRecQC )
 {
 
   DECLARE_ALGORITHM( RichPIDQC );
