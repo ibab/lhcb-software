ffc77f511fdd1d60e9459663bdd022ed460d5f55
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-07-29 16:37:09 +0000

    add factories for Calo* relations/associators
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@108504 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 66fd22ec855524f74dd48f1570cf844064c8a9fd

--- Lbcom/Associators/Associators/src/Associators_load.cpp
+++ Lbcom/Associators/Associators/src/Associators_load.cpp
@@ -1,12 +1,15 @@
-// $Id: Associators_load.cpp,v 1.3 2004-06-11 12:31:11 cattanem Exp $
+// $Id: Associators_load.cpp,v 1.4 2004-07-29 16:37:09 ibelyaev Exp $
 // Include files
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 // Event
-//#include "Event/MCHit.h"
-//#include "Event/MCParticle.h"
+#include "Event/MCHit.h"
+#include "Event/MCParticle.h"
 #include "Event/MCVeloHit.h"
 #include "Event/L0CaloCandidate.h"
+#include "Event/CaloCluster.h"
+#include "Event/CaloHypo.h"
+#include "Event/TrStoredTrack.h"
 
 // LHCbKernel
 #include "Relations/RelationMACROs.h"
@@ -20,6 +23,24 @@ IMPLEMENT_Associator( MCParticle, MCVeloHit);
 
 IMPLEMENT_RelationW1D( L0CaloCandidate, MCParticle, double );
 IMPLEMENT_WAssociator( L0CaloCandidate, MCParticle, double );
+
+/// factory for relations between CaloClusters and MCParticles 
+IMPLEMENT_RelationW1D ( CaloCluster   , MCParticle    , float ) ;
+IMPLEMENT_RelationW2D ( CaloCluster   , MCParticle    , float ) ;
+/// factory for relations between clusters  and tracks
+IMPLEMENT_RelationW1D ( CaloCluster   , TrStoredTrack , float ) ;
+IMPLEMENT_RelationW2D ( CaloCluster   , TrStoredTrack , float ) ;
+/// factory for relations between hypos     and tracks
+IMPLEMENT_RelationW1D ( CaloHypo      , TrStoredTrack , float ) ;
+IMPLEMENT_RelationW2D ( CaloHypo      , TrStoredTrack , float ) ;
+/// add some weight ( namely the ID estimator ) for tracks 
+IMPLEMENT_Relation1D  (                 TrStoredTrack , float ) ;
+/// associators 
+IMPLEMENT_WAssociator ( CaloCluster   , MCParticle    , float ) ;
+IMPLEMENT_WAssociator ( CaloCluster   , TrStoredTrack , float ) ;
+IMPLEMENT_WAssociator ( CaloHypo      , TrStoredTrack , float ) ;
+IMPLEMENT_Associator  (                 TrStoredTrack , float ) ;
+
 
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
@@ -42,4 +63,19 @@ DECLARE_FACTORY_ENTRIES(Associators) {
   
   DECLARE_RelationW1D( L0CaloCandidate, MCParticle, double );
   DECLARE_WAssociator( L0CaloCandidate, MCParticle, double );
-}  
+
+  DECLARE_RelationW1D ( CaloCluster , MCParticle    , float ) ;
+  DECLARE_RelationW2D ( CaloCluster , MCParticle    , float ) ;
+  DECLARE_RelationW1D ( CaloCluster , TrStoredTrack , float ) ;
+  DECLARE_RelationW2D ( CaloCluster , TrStoredTrack , float ) ;
+  DECLARE_RelationW1D ( CaloHypo    , TrStoredTrack , float ) ;
+  DECLARE_RelationW2D ( CaloHypo    , TrStoredTrack , float ) ;
+  DECLARE_Relation1D  (               TrStoredTrack , float ) ;
+
+  DECLARE_WAssociator ( CaloCluster , MCParticle    , float ) ;
+  DECLARE_WAssociator ( CaloCluster , TrStoredTrack , float ) ;
+  DECLARE_WAssociator ( CaloHypo    , TrStoredTrack , float ) ;
+  DECLARE_Associator  (               TrStoredTrack , float ) ;
+  
+} ;
+
