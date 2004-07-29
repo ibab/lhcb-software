c7ae0399f72866fcc32ee2d37760f80e2b48e40e
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-07-29 16:51:52 +0000

    move factories to Associators/Associators package
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@39320 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 468da58e4a00224e13749b4baf133b9d499ec8f9

--- Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
+++ Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
@@ -1,34 +1,14 @@
-// $Id: CaloAssociators_load.cpp,v 1.9 2003-12-18 15:33:36 cattanem Exp $
+// $Id: CaloAssociators_load.cpp,v 1.10 2004-07-29 16:51:52 ibelyaev Exp $
 // ============================================================================
 // Include files
+// ============================================================================
 // GaudiKernel 
+// ============================================================================
 #include "GaudiKernel/DeclareFactoryEntries.h"
-// Event
-#include "Event/MCParticle.h"
-#include "Event/MCVertex.h"
-// CaloEvent/Event 
-#include "Event/CaloCluster.h"
-// LHCbKernel
-#include "Relations/RelationMACROs.h"
-#include "Relations/AssociatorMACROs.h"
-
-IMPLEMENT_RelationW2D( CaloCluster   , MCParticle , float );
-IMPLEMENT_RelationW1D( CaloCluster   , MCParticle , float );
-IMPLEMENT_WAssociator( CaloCluster   , MCParticle , float );
-
+// ============================================================================
 
 /** @file CaloAssociators_load.cpp 
  * 
- *  Mandatory file to IMPLEMENT and DECLARE all factories for 
- *  all generic relation tables and associator tools 
- *
- *  @see Relation1D
- *  @see RelationWeighted1D
- *  @see Relation2D
- *  @see RelationWeighted2D
- *  @see Associator
- *  @see AssociatorWeighted
- *
  *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
  *  @date   08 Apr 2002 
  */
@@ -36,10 +16,6 @@ IMPLEMENT_WAssociator( CaloCluster   , MCParticle , float );
 DECLARE_FACTORY_ENTRIES(CaloAssociators) 
 {
   
-  DECLARE_RelationW1D ( CaloCluster   , MCParticle , float ) ;
-  DECLARE_RelationW2D ( CaloCluster   , MCParticle , float ) ;
-  DECLARE_WAssociator ( CaloCluster   , MCParticle , float ) ;
-
   DECLARE_ALGORITHM   ( CaloClustersMCTruthAlg             ) ;
   DECLARE_ALGORITHM   ( CaloClustersMCTruth2Alg            ) ;
   DECLARE_ALGORITHM   ( CaloClustersMCTruth3Alg            ) ;
