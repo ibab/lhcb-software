9f120caa90cd9a82c9dbbae6cd9ad5f255a9a5da
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-07-31 13:22:56 +0000

    remoev extra factories
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@39323 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a217d51dfbab5551cf2e1840b731da1c7303d0c8

--- Rec/Calo/CaloPIDs/src/CaloPIDs_load.cpp
+++ Rec/Calo/CaloPIDs/src/CaloPIDs_load.cpp
@@ -1,19 +1,15 @@
-// $Id: CaloPIDs_load.cpp,v 1.2 2004-03-17 16:11:40 ibelyaev Exp $
+// $Id: CaloPIDs_load.cpp,v 1.3 2004-07-31 13:22:56 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $
 // ============================================================================
 // $Log: not supported by cvs2svn $
-// Revision 1.1.1.1  2003/03/13 18:52:02  ibelyaev
-// The first import of new package 
-// 
 // ============================================================================
 // Include files
-// LHCbKernel
-#include "Relations/RelationMACROs.h"
+// ============================================================================
 // GaudiKernel
+// ============================================================================
 #include "GaudiKernel/DeclareFactoryEntries.h"
-// Event/TrEvent 
-#include "Event/TrStoredTrack.h"
+// ============================================================================
 
 /** @file
  *
@@ -23,21 +19,8 @@
  *  @date   2003-03-13
  */
 
-// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
-// The statements are like that:
-//
-// DECLARE_CONVERTER( MyConverter );
-// DECLARE_OBJECT( DataObject );
-//
-// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
-
-/// implement the relation associator 
-IMPLEMENT_Associator (               TrStoredTrack , float ) ;
-
 DECLARE_FACTORY_ENTRIES(CaloPIDs) 
 {
-  
-  DECLARE_Associator  ( TrStoredTrack , float               ) ;
   
   //  
   DECLARE_ALGORITHM   ( CaloTrack2EstimatorAlg              ) ;
