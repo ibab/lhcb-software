fff0d28158825dc4591a6e226fbd8bbb21600afa
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2004-08-19 09:45:41 +0000

    Update doxygen comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46922 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1e29e04c2ccbf1456cd71d246a64e87fb7b5a2f6

--- LHCb/Kernel/LHCbKernel/src/RichRadiatorType.cpp
+++ LHCb/Kernel/LHCbKernel/src/RichRadiatorType.cpp
@@ -1,17 +1,23 @@
-// $Id: RichRadiatorType.cpp,v 1.3 2004-08-17 11:27:48 jonrob Exp $
-// Include files 
+
+//-----------------------------------------------------------------------------
+/** @file RichRadiatorType.cpp
+ *
+ *  Implementation file for enumeration : Rich::RadiatorType
+ *
+ *  CVS Log :-
+ *  $Id: RichRadiatorType.cpp,v 1.4 2004-08-19 09:45:41 jonrob Exp $
+ *  $Log: not supported by cvs2svn $
+ *
+ *  @author  Chris Jones  Christopher.Rob.Jones@cern.ch
+ *  @date    2002-06-10
+ */
+//-----------------------------------------------------------------------------
 
 // local
 #include "Kernel/RichRadiatorType.h"
 
-//-----------------------------------------------------------------------------
-// Implementation file for enumeration : Rich::RadiatorType
-//
-// 2002-06-19 : Chris Jones   Christopher.Rob.Jones@cern.ch
-//-----------------------------------------------------------------------------
-
 // Text conversion for RadiatorType enumeration
-std::string Rich::text( const Rich::RadiatorType & radiator ) 
+std::string Rich::text( const Rich::RadiatorType & radiator )
 {
   switch( radiator ) {
   case Rich::Aerogel:            return "aerogel";
