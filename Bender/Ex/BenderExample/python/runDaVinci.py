0ec5182e59a92cc331ff1eb1b068574eda32f8b0
Vanya Belyaev <ivan.belyaev@cern.ch>
2004-08-21 14:33:42 +0000

    v3r7
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@87710 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9bae6cb3a96c6a0bf74d2ddc78602e9b22f3a553

--- Bender/Ex/BenderExample/python/runDaVinci.py
+++ Bender/Ex/BenderExample/python/runDaVinci.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# $Id: runDaVinci.py,v 1.2 2004-08-06 12:12:03 ibelyaev Exp $
+# $Id: runDaVinci.py,v 1.3 2004-08-21 14:33:42 ibelyaev Exp $
 # =============================================================================
 # CVS tag $Name: not supported by cvs2svn $ 
 # =============================================================================
@@ -15,10 +15,17 @@ import sys
 import gaudimodule   as gaudi 
 import benderconfig  as bender
 
+
 jobopts = bender.getDVoptions ( sys.argv[1:] )
 if not jobopts :
     raise TypeError , "no DaVinci options fiel is specified! "
 
+# =============================================================================
+# globals 
+# =============================================================================
+SUCCESS = gaudi.gbl.StatusCode( gaudi.gbl.StatusCode.SUCCESS ) 
+g       = gaudi.AppMgr()
+# =============================================================================
 
 # uncomment if some Bender-specific features need to be used 
 # from   bendermodule  import *
@@ -44,11 +51,12 @@ if __name__ == '__main__' :
     import sys 
     # analyse the options
     nEvents = bender.getNEvents( sys.argv[1:] )
-    if not nEvents : nEvents = 1000 
+    if not nEvents : nEvents = 1000
     # configure the job
     configure() 
-    # run job 
-    g.run  ( nEvents )
+    # run job
+    #g.run  ( nEvents )
+    g.run  ( 500  )
     # terminate the Application Manager 
     g.exit ()
 
