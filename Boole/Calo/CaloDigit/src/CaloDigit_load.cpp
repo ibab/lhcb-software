0ad3425b74f4aa8cddcad0404c80edd3d8c7ba45
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-09-10 07:52:55 +0000

    Remove CaloDigitToMCParticleAsct
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@39344 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 21566aaa22d462c26c4fad6dee7f26865fb1dfcf

--- Boole/Calo/CaloDigit/src/CaloDigit_load.cpp
+++ Boole/Calo/CaloDigit/src/CaloDigit_load.cpp
@@ -1,19 +1,8 @@
-// $Id: CaloDigit_load.cpp,v 1.5 2004-01-07 09:57:33 cattanem Exp $
+// $Id: CaloDigit_load.cpp,v 1.6 2004-09-10 07:52:55 cattanem Exp $
 // Include files 
 
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
-
-// Event
-#include "Event/MCParticle.h"
-#include "Event/CaloDigit.h"
-
-// LHCbKernel
-#include "Relations/RelationMACROs.h"
-#include "Relations/AssociatorMACROs.h"
-
-IMPLEMENT_RelationW1D( CaloDigit, MCParticle, double );
-IMPLEMENT_WAssociator( CaloDigit, MCParticle, double );
  
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
@@ -24,13 +13,8 @@ IMPLEMENT_WAssociator( CaloDigit, MCParticle, double );
 // They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
 
 DECLARE_FACTORY_ENTRIES(CaloDigit) {
-
-  DECLARE_RelationW1D( CaloDigit, MCParticle, double );
-  DECLARE_WAssociator( CaloDigit, MCParticle, double );
  
   DECLARE_ALGORITHM( CaloSignalAlg );
   DECLARE_ALGORITHM( CaloDigitAlg  );
   DECLARE_ALGORITHM( CaloZSupAlg   );
-
-  DECLARE_ALGORITHM( CaloDigitToMCParticleAsct );
 }
