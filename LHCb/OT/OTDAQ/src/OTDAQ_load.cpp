ab47550656f2490066a4a3acc2ba318928c5bef9
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-09-17 10:13:05 +0000

    fix typo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@56246 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 91a50723d4ee6c11f7c556f97a8658f04efafe19

--- LHCb/OT/OTDAQ/src/OTDAQ_load.cpp
+++ LHCb/OT/OTDAQ/src/OTDAQ_load.cpp
@@ -1,9 +1,9 @@
-// $Id: OTDAQ_load.cpp,v 1.3 2004-09-10 13:14:58 cattanem Exp $
+// $Id: OTDAQ_load.cpp,v 1.4 2004-09-17 10:13:05 cattanem Exp $
 // Include files 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(OTDAQ) {
-  DECLARE_TOOL( OTReadoutWindow );
+  DECLARE_TOOL( OTReadOutWindow );
   DECLARE_ALGORITHM( OTTimeCreator );
   DECLARE_ALGORITHM( OTFillBufferFromOTTime );
 }
