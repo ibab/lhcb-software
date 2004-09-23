e78c242dfff6157a0bd63f1697b833264160d642
Marco Cattaneo <marco.cattaneo@cern.ch>
2004-09-23 07:01:55 +0000

    add include to compile with Gaudi v15r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@6860 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 046928f3d360cdeff9b4e1208bd6d63ff34f0baf

--- LHCb/Rich/RichKernel/RichKernel/BoostArray.h
+++ LHCb/Rich/RichKernel/RichKernel/BoostArray.h
@@ -4,8 +4,11 @@
  *  Header file for boost::array + Additional Gaudi methods
  *
  *  CVS Log :-
- *  $Id: BoostArray.h,v 1.3 2004-08-19 13:55:58 jonrob Exp $
+ *  $Id: BoostArray.h,v 1.4 2004-09-23 07:01:55 cattanem Exp $
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.3  2004/08/19 13:55:58  jonrob
+ *  Add new method to MC truth tool + various other things after CVS server switch
+ *
  *  Revision 1.2  2004/07/26 17:53:16  jonrob
  *  Various improvements to the doxygen comments
  *
@@ -18,6 +21,7 @@
 
 // Include files
 #include "boost/array.hpp"
+#include "GaudiKernel/MsgStream.h"
 
 /// Implement textual MsgStream << method
 template <class TYPE, std::size_t N>
