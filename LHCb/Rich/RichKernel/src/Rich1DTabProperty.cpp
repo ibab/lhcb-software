3e5d17e894a1197a7514abb5985014dbf75f482c
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2004-10-20 16:18:24 +0000

    Handle a null TabulatedProperty pointer
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@6880 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 506dddff1e4c89a8b541b619dab058124ddf4dce

--- LHCb/Rich/RichKernel/src/Rich1DTabProperty.cpp
+++ LHCb/Rich/RichKernel/src/Rich1DTabProperty.cpp
@@ -5,8 +5,11 @@
  *  Implementation file for class : Rich1DTabProperty
  *
  *  CVS Log :-
- *  $Id: Rich1DTabProperty.cpp,v 1.3 2004-07-26 17:53:17 jonrob Exp $
+ *  $Id: Rich1DTabProperty.cpp,v 1.4 2004-10-20 16:18:24 jonrob Exp $
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.3  2004/07/26 17:53:17  jonrob
+ *  Various improvements to the doxygen comments
+ *
  *
  *  @author Chris Jones    Christopher.Rob.Jones@cern.ch
  *  @date   2002-07-26
@@ -15,6 +18,7 @@
 
 // local
 #include "RichKernel/Rich1DTabProperty.h"
+#include "RichKernel/RichException.h"
 
 // Constructor from a tabulated property pointer
 Rich1DTabProperty::Rich1DTabProperty( const TabulatedProperty * tab,
@@ -22,6 +26,9 @@ Rich1DTabProperty::Rich1DTabProperty( const TabulatedProperty * tab,
   : Rich1DTabFunc (     ),
     m_tabProp     ( tab )
 {
+
+  // Check the data is valid
+  if ( !tab ) throw RichException("Rich1DTabProperty : Null TabulatedProperty");
 
   // copy data to internal container
   for ( TabulatedProperty::Table::const_iterator it = tab->table().begin();
