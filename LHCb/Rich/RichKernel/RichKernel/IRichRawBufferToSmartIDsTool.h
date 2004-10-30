79d15b3cc09e7a3b534072730245fc15e58d4a28
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2004-10-30 19:18:14 +0000

    Add in tool  interface for RawBuffer decoding
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@6913 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 98646e3d3dedfb340baabe6389e165fc0e1fef0d

---
+++ LHCb/Rich/RichKernel/RichKernel/IRichRawBufferToSmartIDsTool.h
@@ -0,0 +1,47 @@
+
+/** @file IRichRawBufferToSmartIDsTool.h
+ *
+ *  Header file for tool interface : IRichRawBufferToSmartIDsTool
+ *
+ *  CVS Log :-
+ *  $Id: IRichRawBufferToSmartIDsTool.h,v 1.1 2004-10-30 19:18:14 jonrob Exp $
+ *  $Log: not supported by cvs2svn $
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   2003-07-31
+ */
+
+#ifndef RICHDAQ_IRICHRAWBUFFERTOSMARTIDSTOOL_H
+#define RICHDAQ_IRICHRAWBUFFERTOSMARTIDSTOOL_H 1
+
+// Kernel
+#include "Kernel/RichSmartID.h"
+
+/// Static Interface Identification
+static const InterfaceID IID_IRichRawBufferToSmartIDsTool( "IRichRawBufferToSmartIDsTool", 1, 0 );
+
+/** @class IRichRawBufferToSmartIDsTool IRichRawBufferToSmartIDsTool.h RichKernel/IRichRawBufferToSmartIDsTool.h
+ *
+ *  Interface for tool for decoding the Rich Raw Buffer
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   15/03/2002
+ */
+
+class IRichRawBufferToSmartIDsTool : public virtual IAlgTool {
+
+public:
+
+  /** static interface identification
+   *  @return unique interface identifier
+   */
+  static const InterfaceID& interfaceID() { return IID_IRichRawBufferToSmartIDsTool; }
+
+  /** Access all RichSmartIDs for the current Event
+   * @return Container of RichSmartIDs
+   */
+  virtual const RichSmartID::Vector & allRichSmartIDs() const = 0;
+
+};
+
+#endif // RICHDAQ_IRICHRAWBUFFERTOSMARTIDSTOOL_H
