3f37c5f8f31e5a5adfda89a74b64dd38c211afbf
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2004-12-17 08:43:58 +0000

    Comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62648 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 58c38985a6a2d847ebed1c72885f155fd4c33092

--- Phys/Phys/DaVinciKernel/DaVinciTools/IPhysDesktop.h
+++ Phys/Phys/DaVinciKernel/DaVinciTools/IPhysDesktop.h
@@ -1,4 +1,4 @@
-// $Id: IPhysDesktop.h,v 1.8 2004-12-16 15:48:02 pkoppenb Exp $
+// $Id: IPhysDesktop.h,v 1.9 2004-12-17 08:43:58 pkoppenb Exp $
 #ifndef DAVINCITOOLS_IPHYSDESKTOP_H 
 #define DAVINCITOOLS_IPHYSDESKTOP_H 1
 
@@ -26,39 +26,39 @@ public:
   /// Retrieve interface ID
   static const InterfaceID& interfaceID() { return IID_IPhysDesktop; }
 
-  // Fill the particle and vertex containers. Called by DVAlgorithm::sysExecute()
+  /// Fill the particle and vertex containers. Called by DVAlgorithm::sysExecute()
   virtual StatusCode getEventInput() = 0;
 
-  // Retrieve the particles containers
+  /// Retrieve the particles containers
   virtual const ParticleVector& particles() = 0;
 
-  // Retrieve the vertex container
+  /// Retrieve the vertex container
   virtual const VertexVector& vertices() = 0;
 
-  // Add the particles  to the DeskTop
+  /// Add the particles  to the DeskTop
   virtual Particle* createParticle( Particle* input=0 ) = 0;
 
-  // Add the vertices  to the DeskTop
+  /// Add the vertices  to the DeskTop
   virtual Vertex* createVertex( Vertex* input=0 ) = 0;
 
-  // Save the particles to the TES
+  /// Save the particles to the TES
   virtual StatusCode saveDesktop() = 0;
  
-  // Save the particles to the TES
+  /// Save the particles to the TES
   virtual StatusCode saveDesktop(ParticleVector&,VertexVector& ) = 0;
  
-  // Save a vector of Particles
-  // If a particle is composite its descendents are also saved
+  /// Save a vector of Particles
+  /// If a particle is composite its descendents are also saved
   virtual StatusCode saveTrees( ParticleVector& ) = 0;
   
-  // Save a vector of vertices. all descendents of a vertex are also saved
+  /// Save a vector of vertices. all descendents of a vertex are also saved
   virtual StatusCode saveTrees( VertexVector& ) = 0;
 
-  //Save all Particles with a given particleID code
+  /// Save all Particles with a given particleID code
   virtual StatusCode saveTrees( int pid ) = 0;
 
-  //Clone all particles given by a list. This duplicates information on the TES
-  // and should be used only when necessary.
+  /// Clone all particles given by a list. This duplicates information on the TES
+  /// and should be used only when necessary.
   virtual StatusCode cloneTrees( ParticleVector& ) = 0;
 
   //Save all Particles with a given particleID
@@ -67,11 +67,10 @@ public:
   //Save all Particles with a given particleID, and a minimum ConfLevel
   //  virtual StatusCode saveTrees( int pid, double cl ) = 0;
 
-  //hrp
-
+  /// Impose output location
   virtual void imposeOutputLocation(std::string outputLocationString) = 0;
 
-  //PK
+  /// Get output location
   virtual std::string getOutputLocation() = 0 ;
   
   /// Find all particles & vertices in a tree. 
