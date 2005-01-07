4fd26c546fd50d2277d4fc95eae84ec0bf8821d0
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-01-07 13:21:22 +0000

    Add new algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@7021 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9734dcd9e98759c7e34b621b80631e310d2d7996

--- Lbcom/Rich/RichMCTools/src/RichMCTools_load.cpp
+++ Lbcom/Rich/RichMCTools/src/RichMCTools_load.cpp
@@ -5,8 +5,11 @@
  * Declaration of entries in the RichMCTools component library
  *
  * CVS Log :-
- * $Id: RichMCTools_load.cpp,v 1.5 2004-07-26 17:56:09 jonrob Exp $
+ * $Id: RichMCTools_load.cpp,v 1.6 2005-01-07 13:18:00 jonrob Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2004/07/26 17:56:09  jonrob
+ * Various improvements to the doxygen comments
+ *
  *
  * @author Chris Jones   Christopher.Rob.Jones@cern.ch
  * @date 14/01/2002
@@ -30,6 +33,10 @@ DECLARE_FACTORY_ENTRIES(RichMCTools)
   // builds the association table for MCRichHits to MCRichOpticalPhotons
   DECLARE_ALGORITHM( MCRichHitToMCRichOpPhotAlg );
 
+  // Builds RichTrackSegments from MC information
   DECLARE_TOOL( RichTrSegMakerFromMCRichTracks );
+
+  // Create links between RichDigits and MCRichDigits
+  DECLARE_ALGORITHM( BuildMCRichDigitLinks );
 
 }
