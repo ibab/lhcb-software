7d5d824ef5b85d739acecf86d0dc95c93bc9afe5
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-01-07 13:21:22 +0000

    Add new algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@7021 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0a7e9bdcf287c436eebec0a4badde27bb960b93c

---
+++ Boole/Rich/RichReadout/src/MCRichDigitsToRawBufferAlg.cpp
@@ -0,0 +1,99 @@
+
+/** @file MCRichDigitsToRawBufferAlg.cpp
+ *
+ *  Implementation file for RICH DAQ algorithm : MCRichDigitsToRawBufferAlg
+ *
+ *  CVS Log :-
+ *  $Id: MCRichDigitsToRawBufferAlg.cpp,v 1.1 2005-01-07 13:21:22 jonrob Exp $
+ *  $Log: not supported by cvs2svn $
+ *
+ *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
+ *  @date   2003-11-09
+ */
+
+// local
+#include "MCRichDigitsToRawBufferAlg.h"
+
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+static const  AlgFactory<MCRichDigitsToRawBufferAlg>          s_factory ;
+const        IAlgFactory& MCRichDigitsToRawBufferAlgFactory = s_factory ;
+
+// Standard constructor
+MCRichDigitsToRawBufferAlg::MCRichDigitsToRawBufferAlg( const std::string& name,
+                                                        ISvcLocator* pSvcLocator )
+  : RichAlgBase   ( name, pSvcLocator ),
+    m_rawFormatT  ( 0 ),
+    m_level1      ( 0 )
+{
+
+  declareProperty( "MCRichDigitsLocation",
+                   m_digitsLoc = MCRichDigitLocation::Default );
+  declareProperty( "DataVersion", m_version = 1 );
+
+}
+
+// Destructor
+MCRichDigitsToRawBufferAlg::~MCRichDigitsToRawBufferAlg() {};
+
+// Initialisation.
+StatusCode MCRichDigitsToRawBufferAlg::initialize()
+{
+
+  // intialise base lcass
+  const StatusCode sc = RichAlgBase::initialize();
+  if ( sc.isFailure() ) { return sc; }
+
+  // acquire tools
+  acquireTool( "RichRawDataFormatTool", m_rawFormatT );
+  acquireTool( "RichHPDToLevel1Tool",   m_level1     );
+
+  info() << "Using RICH Level1 buffer format : " << m_version << endreq;
+
+  return StatusCode::SUCCESS;
+};
+
+// Main execution
+StatusCode MCRichDigitsToRawBufferAlg::execute()
+{
+
+  // debug message
+  debug() << "Execute" << endreq;
+
+  // Retrieve MCRichDigits
+  const MCRichDigits * digits = get<MCRichDigits>( m_digitsLoc );
+
+  // new rich data map
+  RichDAQ::L1Map L1Data;
+
+  // Loop over digits and sort according to L1 and HPD
+  for ( MCRichDigits::const_iterator iDigit = digits->begin();
+        iDigit != digits->end(); ++iDigit ) {
+
+    // Get Level 1 ID number
+    const RichDAQ::Level1ID L1ID = m_level1->levelL1ID( (*iDigit)->key() );
+
+    // Get reference to L1 group
+    RichDAQ::PDMap & PDs = L1Data[ L1ID ];
+
+    // Finally, insert this pixel into correct place
+    PDs[ (*iDigit)->key().pdID() ].push_back( (*iDigit)->key() );
+
+  }
+
+  debug() << "Created " << L1Data.size() << " RICH L1 bank(s) for "
+          << digits->size() << " MCRichDigits" << endreq;
+
+  // Fill raw buffer
+  m_rawFormatT->createDataBank( L1Data, (RichDAQ::BankVersion)m_version );
+
+  return StatusCode::SUCCESS;
+};
+
+//  Finalize
+StatusCode MCRichDigitsToRawBufferAlg::finalize()
+{
+  // finalise base
+  return RichAlgBase::finalize();
+}
---
+++ Boole/Rich/RichReadout/src/MCRichDigitsToRawBufferAlg.h
@@ -0,0 +1,70 @@
+
+/** @file MCRichDigitsToRawBufferAlg.h
+ *
+ *  Header file for RICH DAQ algorithm : MCRichDigitsToRawBufferAlg
+ *
+ *  CVS Log :-
+ *  $Id: MCRichDigitsToRawBufferAlg.h,v 1.1 2005-01-07 13:21:22 jonrob Exp $
+ *  $Log: not supported by cvs2svn $
+ *
+ *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
+ *  @date   2003-11-06
+ */
+
+#ifndef RICHDAQ_MCRICHDIGITSTORAWBUFFERALG_H
+#define RICHDAQ_MCRICHDIGITSTORAWBUFFERALG_H 1
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/MsgStream.h"
+
+// base class
+#include "RichKernel/RichAlgBase.h"
+
+// Event Model
+#include "Event/MCRichDigit.h"
+
+// Interfaces
+#include "RichKernel/IRichRawDataFormatTool.h"
+#include "RichKernel/IRichHPDToLevel1Tool.h"
+
+/** @class MCRichDigitsToRawBufferAlg MCRichDigitsToRawBufferAlg.h
+ *
+ *  Algorithm to fill the Raw buffer with RICH information from MCRichDigits.
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   2003-11-06
+ *
+ *  @todo Remove DC04 hacks when no longer needed
+ */
+
+class MCRichDigitsToRawBufferAlg : public RichAlgBase {
+
+public:
+
+  /// Standard constructor
+  MCRichDigitsToRawBufferAlg(const std::string& name, ISvcLocator* pSvcLocator);
+
+  virtual ~MCRichDigitsToRawBufferAlg( ); ///< Destructor
+
+  virtual StatusCode initialize();    // Algorithm initialization
+  virtual StatusCode execute   ();    // Algorithm execution
+  virtual StatusCode finalize  ();    // Algorithm finalization
+
+private: // data
+
+  /// Pointer to RICH raw data format tool
+  IRichRawDataFormatTool * m_rawFormatT;
+
+  /// Pointer to RICH Level1 tool
+  IRichHPDToLevel1Tool * m_level1;
+
+  /// Location of input MCRichDigits in TES
+  std::string m_digitsLoc;
+
+  /// Data Format version
+  unsigned int m_version;
+
+};
+
+#endif // RICHDAQ_RICHDIGITSTORAWBUFFERALG_H
