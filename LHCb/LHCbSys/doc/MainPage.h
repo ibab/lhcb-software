01e8f6b66cc009905e6608729903e90f341fb926
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-01-13 07:09:23 +0000

    add files for doxygen
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@56430 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8fe5338419e3cff8f0e4345badc4f071d60eab55

---
+++ LHCb/LHCbSys/doc/MainPage.h
@@ -0,0 +1,22 @@
+/** \mainpage notitle
+ *  \anchor lhcbdoxygenmain
+ *
+ * This is the code reference manual for the LHCb core classes. These include:
+ * \li common interfaces
+ * \li common base classes
+ * \li event model classes
+ * \li detector description
+ * \li associators to MonteCarlo truth
+ * \li tools to encode and decode the DAQ buffers
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the LHCbSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-comp/Frameworks/LHCbSys/">web pages</a>
+ * of the LHCbSys project
+ *
+ * \sa
+ * \li \ref gaudidoxygenmain "Gaudi documentation" 
+ * \li \ref externaldocs     "Related external libraries"
+ 
+ */
