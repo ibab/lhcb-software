3a915c381f6b033c5b4e607f343f7fa2021bb1f8
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-01-14 16:47:20 +0000

    Change printout order to match sorting order
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@46991 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dfb32032e2149473aeb8ae5ef247759490e89d00

--- LHCb/Kernel/LHCbKernel/src/RichSmartID.cpp
+++ LHCb/Kernel/LHCbKernel/src/RichSmartID.cpp
@@ -5,8 +5,11 @@
  *  Implementation file for RICH Channel ID class : RichSmartID
  *
  *  CVS Log :-
- *  $Id: RichSmartID.cpp,v 1.1 2005-01-07 12:28:56 jonrob Exp $
+ *  $Id: RichSmartID.cpp,v 1.2 2005-01-14 16:47:20 jonrob Exp $
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.1  2005/01/07 12:28:56  jonrob
+ *  Add implementation file, containing fillStream method
+ *
  *
  *  @author  Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date    2005-01-06
@@ -26,10 +29,10 @@ std::ostream& RichSmartID::fillStream(std::ostream& s) const
     s << "{";
     if ( richIsSet()     ) s << " "       << rich();
     if ( panelIsSet()    ) s << " Panel " << static_cast<unsigned int>(panel());
-    if ( PDRowIsSet()    ) s << " PDRow"  << format("%3i",PDRow());
     if ( PDColIsSet()    ) s << " PDCol"  << format("%3i",PDCol());
-    if ( pixelRowIsSet() ) s << " pixRow" << format("%3i",pixelRow());
+    if ( PDRowIsSet()    ) s << " PDRow"  << format("%3i",PDRow());
     if ( pixelColIsSet() ) s << " pixCol" << format("%3i",pixelCol());
+    if ( pixelRowIsSet() ) s << " pixRow" << format("%3i",pixelRow());
     if ( subPixelIsSet() ) s << " subPix" << format("%2i",subPixel());
     s << " }";
 
