6625ac9bb5b4affb8720a4edf7e0f91c75828fe3
ranjard <ranjard@cern.ch>
2005-01-26 08:28:26 +0000

    v19r0 - add doxygen input files and fix GeneratorModules for clhep 1.9
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91573 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4cfafdf0c260e5b2f867d7a685229a1a2a46696f

---
+++ Gauss/GaussSys/doc/MainPage.h
@@ -0,0 +1,18 @@
+/** \mainpage notitle
+ *  \anchor Gaussdoxygenmain
+ *
+ * This is the code reference manual for the Gauss Simulation Application  classes.
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the GaussSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-comp/Simulation/">web pages</a>
+ * of the Gauss project
+ *
+ * \sa
+ * \li \ref lhcbdoxygenmain    "LHCbSys documentation"
+ * \li \ref gaudidoxygenmain   "Gaudi documentation"
+ * \li \ref geant4doxygenmain  "Geant4 documentation"
+ * \li \ref externaldocs       "Related external libraries"
+
+ */
