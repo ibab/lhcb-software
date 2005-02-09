f21fbcce4b6ef2e14de71f35649ee30f8dc4f36e
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-02-09 13:00:29 +0000

    remove obsolete include
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47019 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 90a519f963a9962eb5b879964c2e979629a0b5f9

--- LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
+++ LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
@@ -1,9 +1,9 @@
-// $Id: LHCbStreams.h,v 1.3 2002-03-27 14:17:26 cattanem Exp $
+// $Id: LHCbStreams.h,v 1.4 2005-02-09 13:00:29 cattanem Exp $
 #ifndef KERNEL_LHCBSTREAMS_H 
 #define KERNEL_LHCBSTREAMS_H 1
 
-// Include files for streams definitions
+// This file is obsolete!! Kept only for backward compatibility.
+// Please include SerializeStl.h directly!
 #include "Kernel/SerializeStl.h" // also includes GaudiKernel/StreamBuffer.h
-#include "Kernel/CLHEPStreams.h" // also includes GaudiKernel/MsgStream.h
 
 #endif // KERNEL_LHCBSTREAMS_H
