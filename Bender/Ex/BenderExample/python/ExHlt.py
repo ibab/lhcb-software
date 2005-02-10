b24ec8865aa3ade669639195038f6e935512685d
Vanya Belyaev <ivan.belyaev@cern.ch>
2005-02-10 12:27:21 +0000

    resurrect ExHlt.py example
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@87811 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 87cb217df244675ceef631da8efc7eb992ca02ef

--- Bender/Ex/BenderExample/python/ExHlt.py
+++ Bender/Ex/BenderExample/python/ExHlt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: ExHlt.py,v 1.4 2005-01-24 17:33:00 ibelyaev Exp $
+# $Id: ExHlt.py,v 1.5 2005-02-10 12:27:21 ibelyaev Exp $
 # =============================================================================
 # CVS tag $Name: not supported by cvs2svn $
 # =============================================================================
@@ -26,14 +26,13 @@ class ExHlt(Algo):
     def analyse ( self ) :
         # get kaons
         
-        pions  = self.select( tag="pi+" , cuts = ( 'pi+' == ABSID ) & HASTRGTR )
-        muons  = self.select( tag="mu+" , cuts = ( 'mu+' == ABSID ) & HASTRGTR )
+        pions  = self.select( tag = "pi" , cuts = ( 'pi+' == ABSID ) & HASTRGTR )
+        muons  = self.select( tag = "mu" , cuts = ( 'mu+' == ABSID ) & HASTRGTR )
+        kaons  = self.select( tag = "K"  , cuts = ( 'K+'  == ABSID ) & HASTRGTR )
+        
+        print ' # muons/pions/kaons = %2d/%2d/%2d ' %( len(muons),len(pions),len(kaons) ) 
 
-        pions = muons
-        
-        print 'muons = ', muons.size()
-        
-        if pions.empty() : return SUCCESS
+        if muons.empty() : return SUCCESS
         
         mcpi    = self.mcselect( tag = 'mpi' , cuts = 'pi+' == MCABSID )
         mcK     = self.mcselect( tag = 'mK'  , cuts = 'K+'  == MCABSID )
@@ -47,31 +46,17 @@ class ExHlt(Algo):
         trueP   = MCTRUTH( mc , mcP )
         trueE   = MCTRUTH( mc , mcE  )
         trueMu  = MCTRUTH( mc , mcMu )
-        
-        pi1  = self.select( tag    = "pimc1+" ,
-                            source = pions    ,
-                            cuts   = truePi   )
-        pi2  = self.select( tag    = "pimc2+" ,
-                            source = pions    ,
-                            cuts   = trueK    )
-        pi3  = self.select( tag    = "pimc3+" ,
-                            source = pions    ,
-                            cuts   = trueP    )
-        pi4  = self.select( tag    = "pimc4+" ,
-                            source = pions    ,
-                            cuts   = trueE    )
-        pi5  = self.select( tag    = "pimc5+" ,
-                            source = pions    ,
-                            cuts   = trueMu   )
-        
-        
-        print " #pions = %3d [ MC: 'pi+'=%3d 'K+'=%3d 'p+'=%3d 'e+'=%3d 'mu+'=%3d ]" % ( pions.size () ,
-                                                                                         pi1.size() ,
-                                                                                         pi2.size() ,
-                                                                                         pi3.size() ,
-                                                                                         pi4.size() ,
-                                                                                         pi5.size() )
-        
+
+        for muon in muons :
+            line  = ' Muon PT: %5.2f [GeV]; ' % ( PT( muon ) / GeV, ) 
+            line += ' MCTruth: Mu/pi/K/p/e %1d/%1d/%1d/%1d/%1d '
+            line  = line % ( trueMu ( muon ) ,
+                             truePi ( muon ) ,
+                             trueK  ( muon ) ,
+                             trueP  ( muon ) ,
+                             trueE  ( muon ) )
+            print line
+            
         return SUCCESS 
     
     
@@ -84,9 +69,10 @@ def configure() :
                     '$BENDEREXAMPLEOPTS/PoolCatalogs.opts'  ,   # pool catalogs
                     '$LOKIEXAMPLEOPTS/Bs_phiphi_DC04.opts'  ,   # input data 
                     '$TRGSYSROOT/options/L1.opts'           ,   # L1 procession 
-                    '$TRGSYSROOT/options/Hlt.opts'          ,   # HLT configuration
-                    '$BENDEREXAMPLEOPTS/TrgTracks.opts'     ,   # HLT configuration
-                    '$TRGSYSROOT/options/TrgChecking.opts'  ] , # HLT configuration
+                    '$TRGSYSROOT/options/Hlt.opts'              # HLT configuration
+                    #'$BENDEREXAMPLEOPTS/TrgTracks.opts'    ,   # HLT configuration
+                    #'$TRGSYSROOT/options/TrgChecking.opts'
+                    ] , # HLT configuration
                   options =
                   [ 'EcalPIDmu.OutputLevel     =   5  ' ,
                     'HcalPIDmu.OutputLevel     =   5  ' ,
@@ -100,13 +86,27 @@ def configure() :
     
     _libs_ = ( 'TrgTools' , 'TrgVelo' , 'TrgVeloTT' ,
                'STDAQ' , 'TrgChecker' , 'PhysSelections' )
-    for lib in _libs_ : if not lib in gaudi.DLLs : gaudi.DLLs += [ lib ]
+    for lib in _libs_ :
+        if not lib in gaudi.DLLs : gaudi.DLLs += [ lib ]
+
+    seq = gaudi.algorithm('TrgChecking')
+    seq.MeasureTime = True
+    seq = gaudi.algorithm('L1Processing')
+    seq.MeasureTime = True
+    seq = gaudi.algorithm('HLTPRocessing')
+    seq.MeasureTime = True
 
     # specific job configuration 
 
-    gaudi.addAlgorithm ( 'TrgTrack2MCParticle' )
-    gaudi.addAlgorithm ( 'TrgLinks2TrgTables/Trg2MCTables' ) 
-        
+    gaudi.addAlgorithm ( 'GaudiSequencer/Links2Tables' )
+    seq = gaudi.algorithm ('Links2Tables')
+    
+    seq.Members = [ 'TrgTrackToContainer'             ,
+                    'TrgTrack2MCParticle'             ,
+                    'TrgLinks2TrgTables/Trg2MCTables' ]
+    
+    seq.MeasureTime = True
+    
     # create analysis algorithm and add it to the list of
     ex = ExHlt('ExHlt')
     
@@ -117,8 +117,8 @@ def configure() :
     
     desktop = gaudi.tool('ExHlt.PhysDesktop')
     desktop.InputLocations  = [
-        #"/Event/Phys/Trg"
-        #"Phys/HLTPions",
+        "Phys/Trg"      ,
+        "Phys/HLTPions" ,
         "Phys/HLTMuons"
         ]
     
@@ -133,21 +133,14 @@ if __name__ == '__main__' :
     # configure the job
     configure() 
     # run job 
-    gaudi.run  ( 50  )
+    gaudi.run  ( 100 )
+    evtSvc = gaudi.evtSvc()
+    
     # terminate the Application Manager 
     gaudi.exit ()
     
 # =============================================================================
 # $Log: not supported by cvs2svn $
-# Revision 1.3  2004/11/30 16:31:37  ibelyaev
-#  update for ExHlt for MC mathching
-#
-# Revision 1.2  2004/11/25 15:21:02  ibelyaev
-#  some polishing of ExHLT.py example
-#
-# Revision 1.1  2004/11/25 14:55:05  ibelyaev
-#  add Trg -> MC example
-#
 # =============================================================================
 # The END 
 # =============================================================================
