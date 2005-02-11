9a1f2a5183bd97709fd490d3d4383dfc75f0b08a
Gloria Corti <gloria.corti@cern.ch>
2005-02-11 13:29:47 +0000

    protection for strange hits
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91607 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0fd3fa397ff661725d1fe2a30f463738125827e0

--- Gauss/Sim/GaussTracker/src/GiGaSensDetMuon.cpp
+++ Gauss/Sim/GaussTracker/src/GiGaSensDetMuon.cpp
@@ -94,101 +94,119 @@ bool GiGaSensDetMuon::ProcessHits( G4Step* step ,
           G4TouchableHistory* TT =  
             (G4TouchableHistory*)(step->GetPreStepPoint()->GetTouchable());
           G4VPhysicalVolume*  PV =   TT->GetVolume();
-          
-          TT -> MoveUpHistory(1);
-          G4int CurrentGapNumber = TT -> GetVolume() -> GetCopyNo() ;
-
-          TT -> MoveUpHistory(1);
-          G4int CurrentChamNumber= TT -> GetVolume() -> GetCopyNo() ;
-
-          G4int GapNumber=CurrentGapNumber - 999;
-
-          G4int ChamNumber=0;
-          if(CurrentChamNumber<1012) {
-            ChamNumber=CurrentChamNumber - 999;
-          }
-          if(CurrentChamNumber>1011 && CurrentChamNumber<1036) {
-            ChamNumber=CurrentChamNumber - 1011;
-          }
-          if(CurrentChamNumber>1035 && CurrentChamNumber<1084) {
-            ChamNumber=CurrentChamNumber - 1035;
-          }
-          if(CurrentChamNumber>1083) {
-            ChamNumber=CurrentChamNumber - 1083;
-          }
-
-          // Check if chamber number is allowed for this collection 
-          // (i.e. region)
-          
-          if( ChamNumber <= 0 || ChamNumber > m_chamLimit ) {
+          std::string pvName = PV->GetName();
+          int depth = TT->GetHistoryDepth();
+          // Check if the depth is allowed or not
+          if( depth <= 3 ) {
             MsgStream msg( msgSvc() , name() );
-            std::string colname = TT->GetVolume()->GetName();
-            Error( "Chamber out of limits in "+colname);
-            msg << MSG::WARNING << colname
-                << " :hit out of limit. Entry - " << prepos 
-                << " Exit - " << postpos << " dE/dx = " << edep
-                << " Tof = " << timeof << " Chamber = " << ChamNumber
-                << " Gap = " << GapNumber << endmsg;
-          }
-          else {
-            
-          
-            //MsgStream log( msgSvc() , name() );
-            
-            //log << MSG::INFO << "************************" << endreq;
-            //log << MSG::INFO << "Cham =" << CurrentChamNumber << " , " <<
-            //  ChamNumber << "   Gap =" << CurrentGapNumber << " , " <<
-            //  GapNumber << endreq;
-          
-            
-            //           G4TouchableHistory* postTT =  
-            //   (G4TouchableHistory*)(step->GetPostStepPoint()
-            //          ->GetTouchable());
-            //           G4VPhysicalVolume*  postPV =   postTT->GetVolume();
-            //      G4LogicalVolume*    postLV =   postPV->GetLogicalVolume();
-          
-            // std::string pvname=PV->GetName();
-            // G4TouchableHistory* MotherTT = TT;
-            // MotherTT ->MoveUpHistory(1);
+            Error( "Non correct depth in sensitive volume "+pvName);
+            msg << MSG::WARNING << pvName 
+                << ": incorrect Depth = " << depth << std::endl
+                << " Entry - " << prepos << std::endl
+                << " Exit - " << postpos << std::endl
+                << " dE/dx = " << edep
+                << " Tof = " << timeof  
+                << " TrackID = " << trid 
+                << endmsg;
+          } else {
+            TT -> MoveUpHistory(1);
+            G4int CurrentGapNumber = TT -> GetVolume() -> GetCopyNo() ;
 
-            //           std::string stname=MotherTT -> GetVolume()->GetName();
+            TT -> MoveUpHistory(1);
+            G4int CurrentChamNumber= TT -> GetVolume() -> GetCopyNo() ;
+
+            G4int GapNumber=CurrentGapNumber - 999;
+
+            G4int ChamNumber=0;
+            if(CurrentChamNumber<1012) {
+              ChamNumber=CurrentChamNumber - 999;
+            }
+            if(CurrentChamNumber>1011 && CurrentChamNumber<1036) {
+              ChamNumber=CurrentChamNumber - 1011;
+            }
+            if(CurrentChamNumber>1035 && CurrentChamNumber<1084) {
+              ChamNumber=CurrentChamNumber - 1035;
+            }
+            if(CurrentChamNumber>1083) {
+              ChamNumber=CurrentChamNumber - 1083;
+            }
+
+            // Check if chamber number is allowed for this collection 
+            // (i.e. region)
           
-            // log << MSG::INFO << "Mother: " << stname <<  endreq;      
+            if( ChamNumber <= 0 || ChamNumber > m_chamLimit ) {
+              MsgStream msg( msgSvc() , name() );
+              std::string colname = TT->GetVolume()->GetName();
+                Error( "Chamber out of limits in "+colname);
+              msg << MSG::WARNING << colname
+                  << ": hit out of limit. Entry - " << prepos 
+                  << " Exit - " << postpos << " dE/dx = " << edep
+                  << " Tof = " << timeof << " Chamber = " << ChamNumber
+                  << " Gap = " << GapNumber 
+                  << " TrackID = " << trid 
+                  << " Name = " << pvName
+                  << " Depth = " << depth
+                  << endmsg;
+            }
+            else {
+            
           
-            // log << MSG::INFO 
-            //     << " PrePos=("  << prepos.x() << "," 
-            //     << prepos.y() << "," << prepos.z() 
-            //     << ")" 
-            //     << " PrePV="    << pvname  
-            //     << " PreLV="    << LV->GetName() << endreq;
+              //MsgStream log( msgSvc() , name() );
+            
+              //log << MSG::INFO << "************************" << endreq;
+              //log << MSG::INFO << "Cham =" << CurrentChamNumber << " , " <<
+              //  ChamNumber << "   Gap =" << CurrentGapNumber << " , " <<
+              //  GapNumber << endreq;
           
-            // log << MSG::INFO 
-            //     << " PostPos=("
-            //     << postpos.x() << "," << postpos.y() << "," 
-            // << postpos.z() << ")" 
-            //     << " PostPV="    << postPV->GetName()  
-            //    << " PostLV="    << postLV->GetName() << endreq;
-            // end of temp
+            
+              //           G4TouchableHistory* postTT =  
+              //   (G4TouchableHistory*)(step->GetPostStepPoint()
+              //          ->GetTouchable());
+              //           G4VPhysicalVolume*  postPV =   postTT->GetVolume();
+              //      G4LogicalVolume*    postLV =   postPV->GetLogicalVolume();
+          
+              // std::string pvname=PV->GetName();
+              // G4TouchableHistory* MotherTT = TT;
+              // MotherTT ->MoveUpHistory(1);
+
+              //           std::string stname=MotherTT -> GetVolume()->GetName();
+          
+              // log << MSG::INFO << "Mother: " << stname <<  endreq;      
+          
+              // log << MSG::INFO 
+              //     << " PrePos=("  << prepos.x() << "," 
+              //     << prepos.y() << "," << prepos.z() 
+              //     << ")" 
+              //     << " PrePV="    << pvname  
+              //     << " PreLV="    << LV->GetName() << endreq;
+          
+              // log << MSG::INFO 
+              //     << " PostPos=("
+              //     << postpos.x() << "," << postpos.y() << "," 
+              // << postpos.z() << ")" 
+              //     << " PostPV="    << postPV->GetName()  
+              //    << " PostLV="    << postLV->GetName() << endreq;
+              // end of temp
   
-            ///
-            MuonHit* newHit = new MuonHit();
-            newHit->SetEdep( edep );
-            newHit->SetEntryPos( prepos );
-            newHit->SetExitPos( postpos );
-            newHit->SetTimeOfFlight( timeof );  
-            newHit->SetTrackID( trid );
-            newHit->SetChamberID( ChamNumber );
-            newHit->SetGapID( GapNumber );
-            ///
-            G4VUserTrackInformation* ui = track->GetUserInformation(); 
-            GaussTrackInformation*    gi = (GaussTrackInformation*)  ui;
-            gi->setCreatedHit(true);
-            gi->setToBeStored(true);
-            gi->addHit(newHit);
+              ///
+              MuonHit* newHit = new MuonHit();
+              newHit->SetEdep( edep );
+              newHit->SetEntryPos( prepos );
+              newHit->SetExitPos( postpos );
+              newHit->SetTimeOfFlight( timeof );  
+              newHit->SetTrackID( trid );
+              newHit->SetChamberID( ChamNumber );
+              newHit->SetGapID( GapNumber );
+              ///
+              G4VUserTrackInformation* ui = track->GetUserInformation(); 
+              GaussTrackInformation*    gi = (GaussTrackInformation*)  ui;
+              gi->setCreatedHit(true);
+              gi->setToBeStored(true);
+              gi->addHit(newHit);
           
-            muonCol->insert( newHit );
+              muonCol->insert( newHit );
+            }
           }
-          
         }
     }
   return false;
