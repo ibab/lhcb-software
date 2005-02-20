76164f925358e14feecc0ca60c0d7c5c2d8c4676
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-02-20 18:36:41 +0000

    add doxygen info
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@7138 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ca2a31eb08d4d861e691620e7f3c5c17b91a9f89

--- Boole/Rich/RichDigiQC/src/RichDigiQC_load.cpp
+++ Boole/Rich/RichDigiQC/src/RichDigiQC_load.cpp
@@ -1,11 +1,21 @@
-// $Id: RichDigiQC_load.cpp,v 1.2 2003-09-20 15:45:18 jonrob Exp $
-// Include files 
+
+//------------------------------------------------------------------------------------
+/** @file RichDigiQC_load.cpp
+ *
+ *  Implementation file for classes in the RichDigiQC Component Library
+ *
+ *  CVS Log :-
+ *  $Id: RichDigiQC_load.cpp,v 1.3 2005-02-20 18:36:41 jonrob Exp $
+ *
+ *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
+ *  @date   2003-09-08
+ */
+//------------------------------------------------------------------------------------
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
-DECLARE_FACTORY_ENTRIES( RichDigiQC ) {
-
+DECLARE_FACTORY_ENTRIES( RichDigiQC )
+{
   DECLARE_ALGORITHM( RichDigitQC );
   DECLARE_ALGORITHM( RichDigiDataObjVerifier );
-
 }
