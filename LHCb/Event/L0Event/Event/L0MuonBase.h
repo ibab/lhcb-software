3e65c970df54c30a00565c7deb3ac460ac0937cf
dlunesu <dlunesu@cern.ch>
2005-02-24 16:54:08 +0000

    changes for L1Buffer
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@33967 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b01eeb7c21127c96ea67ae13ba5b0756c98453c8

--- LHCb/Event/L0Event/Event/L0MuonBase.h
+++ LHCb/Event/L0Event/Event/L0MuonBase.h
@@ -1,4 +1,4 @@
-// $Id: L0MuonBase.h,v 1.1 2004-01-26 16:23:46 cattanem Exp $
+// $Id: L0MuonBase.h,v 1.2 2005-02-24 16:54:08 dlunesu Exp $
 //
 #ifndef      L0MUON_L0MUONBASE_H
 #define      L0MUON_L0MUONBASE_H 1
@@ -24,6 +24,23 @@ namespace L0MuonBase {
   static const unsigned int BitsRegion       = 2 ; 
   static const unsigned int BitsX            = 6 ;
   static const unsigned int BitsY            = 4 ;
+
+  //For L1Buffer
+  static const unsigned int BitsBufStatus    = 2 ;
+  static const unsigned int BitsBufPtSign    = 1 ;
+  static const unsigned int BitsBufPt        = 7 ;
+  static const unsigned int BitsBufQuarter   = 2 ;
+  static const unsigned int BitsBufRegion    = 2 ;
+  static const unsigned int BitsBufPBx       = 1 ;
+  static const unsigned int BitsBufPBy       = 1 ;
+  
+  static const unsigned int BitsBufPUx       = 1 ;
+  static const unsigned int BitsBufPUy       = 1 ;
+  static const unsigned int BitsBufM3x       = 5 ;
+  static const unsigned int BitsBufM3y       = 2 ;
+  static const unsigned int BitsBufOffM2     = 4 ;
+  static const unsigned int BitsBufOffM1     = 3 ;
+
   //
   static const unsigned int ShiftX        = 0 ;
   static const unsigned int ShiftY        = ShiftX       + BitsX  ;
@@ -32,6 +49,26 @@ namespace L0MuonBase {
   static const unsigned int ShiftPtSign   = ShiftPt      + BitsPt ;  
   static const unsigned int ShiftStatus   = ShiftPtSign  + BitsPtSign ;
   static const unsigned int ShiftBCID     = ShiftStatus  + BitsStatus  ;
+
+  //For L1Buffer
+  static const unsigned int ShiftBufStatus  = 0 ;
+  static const unsigned int ShiftBufPtSign  = ShiftBufStatus  + BitsBufStatus ;
+  static const unsigned int ShiftBufPt      = ShiftBufPtSign  + BitsBufPtSign ;
+  static const unsigned int ShiftBufQuarter = ShiftBufPt      + BitsBufPt ;
+  static const unsigned int ShiftBufPB      = ShiftBufQuarter + BitsBufQuarter ;
+  static const unsigned int ShiftBufRegion  = ShiftBufQuarter + BitsBufQuarter ;
+  static const unsigned int ShiftBufPBx     = ShiftBufRegion  + BitsBufRegion ;
+  static const unsigned int ShiftBufPBy     = ShiftBufPBx     + BitsBufPBx ;
+
+  static const unsigned int ShiftBufPUx       = 0 ;
+  static const unsigned int ShiftBufPUy       = ShiftBufPUx       + BitsBufPUx ;
+  static const unsigned int ShiftBufM3x       = ShiftBufPUy       + BitsBufPUy ;
+  static const unsigned int ShiftBufM3y       = ShiftBufM3x       + BitsBufM3x ;
+  
+  static const unsigned int ShiftBufOffM2     = ShiftBufM3y + BitsBufM3y ;
+  static const unsigned int ShiftBufOffM1     = ShiftBufOffM2 + BitsBufOffM2 ;
+
+
   //
   static const ContentType  MaskX     =
   ( ( ( (ContentType) 1 ) << BitsX  ) - 1  ) << ShiftX  ;
@@ -48,7 +85,44 @@ namespace L0MuonBase {
   static const ContentType  MaskBCID     =
   ( ( ( (ContentType) 1 ) << BitsBCID  ) - 1  ) << ShiftBCID  ;
   
-  // Pt decoding constants
+
+  //For L1Buffer                                                                                        
+  static const ContentType  MaskBufStatus     =
+  ( ( ( (ContentType) 1 ) << BitsBufStatus  ) - 1  ) << ShiftBufStatus ;
+  static const ContentType  MaskBufQuarter     =
+  ( ( ( (ContentType) 1 ) << BitsBufQuarter  ) - 1  ) << ShiftBufQuarter ;
+  static const ContentType  MaskBufRegion     =
+  ( ( ( (ContentType) 1 ) << BitsBufRegion  ) - 1  ) << ShiftBufRegion ;
+  static const ContentType  MaskBufPBx     =
+  ( ( ( (ContentType) 1 ) << BitsBufPBx  ) - 1  ) << ShiftBufPBx ;
+  static const ContentType  MaskBufPBy     =
+  ( ( ( (ContentType) 1 ) << BitsBufPBy  ) - 1  ) << ShiftBufPBy ;
+
+  static const ContentType  MaskBufPUx     =
+  ( ( ( (ContentType) 1 ) << BitsBufPUx  ) - 1  ) << ShiftBufPUx ;
+  static const ContentType  MaskBufPUy     =
+  ( ( ( (ContentType) 1 ) << BitsBufPUy  ) - 1  ) << ShiftBufPUy ;
+  static const ContentType  MaskBufM3x     =
+  ( ( ( (ContentType) 1 ) << BitsBufM3x  ) - 1  ) << ShiftBufM3x ;
+  static const ContentType  MaskBufM3y     =
+  ( ( ( (ContentType) 1 ) << BitsBufM3y  ) - 1  ) << ShiftBufM3y ;
+
+  static const ContentType  MaskBufOffM2     =
+  ( ( ( (ContentType) 1 ) << BitsBufOffM2  ) - 1  ) << ShiftBufOffM2 ;
+  static const ContentType  MaskBufOffM1     =
+  ( ( ( (ContentType) 1 ) << BitsBufOffM1  ) - 1  ) << ShiftBufOffM1 ;  
+
+  static const ContentType  MaskBufPtSign     =
+  ( ( ( (ContentType) 1 ) << BitsBufPtSign  ) - 1  ) << ShiftBufPtSign ;
+  static const ContentType  MaskBufPt     =
+  ( ( ( (ContentType) 1 ) << BitsBufPt  ) - 1  ) << ShiftBufPt ;
+  
+
+//define M1 offset extrapolation relatively at M3
+  static const int EXTR_OFFSET_M1_M3[6] = {0,4,7,11,14,18};
+
+
+// Pt decoding constants
   static const double PT_BIN_WIDTH = 40.;
   
 };
