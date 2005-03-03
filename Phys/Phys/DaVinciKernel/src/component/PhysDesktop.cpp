ef3dfb062a4c14c920f156bb4eb6f7524a6bdded
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-03-03 14:22:50 +0000

    More clear message
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62715 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 87dedd102e9c1cc7aa49b34a59d75fa8244cbe85

--- Phys/Phys/DaVinciKernel/src/component/PhysDesktop.cpp
+++ Phys/Phys/DaVinciKernel/src/component/PhysDesktop.cpp
@@ -1,4 +1,4 @@
-// $Id: PhysDesktop.cpp,v 1.14 2005-02-25 14:36:13 pkoppenb Exp $
+// $Id: PhysDesktop.cpp,v 1.15 2005-03-03 14:22:50 pkoppenb Exp $
 // Include files 
 
 // from Gaudi
@@ -162,7 +162,8 @@ const VertexVector& PhysDesktop::vertices()
 {
   
   if ( ( m_verts.empty()) && !((m_primVerts.empty()) && (m_secVerts.empty()) )){
-    Warning("Deprecated method: Use faster primaryVertices() and secondaryVertices() methods instead",StatusCode::SUCCESS,1);
+    Warning("Deprecated method PhysDesktop::vertices(): Use faster primaryVertices() and secondaryVertices() methods instead",
+            StatusCode::SUCCESS,1);
     m_verts = m_primVerts ;
     m_verts.insert(m_verts.end(),m_secVerts.begin(),m_secVerts.end()); // concatenate
   }
