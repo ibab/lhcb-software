dc0b3d21395f897cd79676bb6c018c49952bf259
Vanya Belyaev <ivan.belyaev@cern.ch>
2005-03-05 14:06:45 +0000

    improvements in 'Higgs' example
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@49162 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 20786da56cdacf35b4a6d23a8aa0c71cbb4e5571

--- Analysis/Ex/LoKiExample/src/LoKi_Higgs1.cpp
+++ Analysis/Ex/LoKiExample/src/LoKi_Higgs1.cpp
@@ -1,11 +1,8 @@
-// $Id: LoKi_Higgs1.cpp,v 1.2 2005-02-18 11:40:40 ibelyaev Exp $
+// $Id: LoKi_Higgs1.cpp,v 1.3 2005-03-05 14:06:45 ibelyaev Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $ ; version $Release:$
+// CVS tag $Name: not supported by cvs2svn $ ; version $Revision: 1.3 $
 // ============================================================================
 // $Log: not supported by cvs2svn $
-// Revision 1.1  2005/02/16 11:16:58  ibelyaev
-//  add new exmaples: H->bb and Bs -> phi mu+ mu-
-//
 // ============================================================================
 // Include files 
 // ============================================================================
@@ -13,7 +10,109 @@
 // ============================================================================
 #include "LoKi/LoKi.h"
 // ============================================================================
+// Event 
+// ============================================================================
+#include "Event/L0DUReport.h"
+#include "Event/L1Score.h"
+#include "Event/TrgDecision.h"
+// ============================================================================
 
+namespace LoKi
+{
+  namespace MCParticles
+  {
+    /** @struct PseudoRapidity
+     *  evaluator of the pseudirapidity 
+     *  @author Vanya BELYAEV belyaev@lapp.in2p3.fr 
+     *  @date   2005-03-04
+     */
+    struct PseudoRapidity : public LoKi::Function<const MCParticle*>
+    {      
+      /// clone method (mandatory!)
+      virtual PseudoRapidity* clone() const 
+      { return new PseudoRapidity(*this) ; };      
+      /// the only one essential method 
+      result_type operator() ( argument p ) const 
+      {
+        if ( 0 == p ) 
+        {
+          Warning( "MCParticle* points to NULL!" ) ;
+          return -1000 ;
+        }
+        return p->momentum().pseudoRapidity() ;
+      };
+    };
+    /** @struct Phi
+     *  trivial evaluator of Phi
+     *  @author Vanya BELYAEV belyaev@lapp.in2p3.fr 
+     *  @date   2005-03-04
+     */
+    struct Phi : public LoKi::Function<const MCParticle*>
+    {      
+      /// clone method (mandatory!)
+      virtual Phi* clone() const { return new Phi(*this) ; };      
+      /// the only one essential method 
+      result_type operator() ( argument p ) const 
+      {
+        if ( 0 == p ) 
+        {
+          Warning( "MCParticle* points to NULL!" ) ;
+          return -1000 ;
+        }
+        return p->momentum().phi() ;
+      };
+    };
+    
+  }; // end of namespace MCParticles 
+  
+  namespace Particles
+  {
+    /** @struct PseudoRapidity
+     *  evaluator of the pseudirapidity 
+     *  @author Vanya BELYAEV belyaev@lapp.in2p3.fr 
+     *  @date   2005-03-04
+     */
+    struct PseudoRapidity : public LoKi::Function<const Particle*>
+    {      
+      /// clone method (mandatory!)
+      virtual PseudoRapidity* clone() const 
+      { return new PseudoRapidity(*this) ; };      
+      /// the only one essential method 
+      result_type operator() ( argument p ) const 
+      {
+        if ( 0 == p ) 
+        {
+          Warning( "Particle* points to NULL!" ) ;
+          return -1000 ;
+        }
+        return p->momentum().pseudoRapidity() ;
+      };
+    };
+    /** @struct Phi
+     *  trivial evaluator of Phi
+     *  @author Vanya BELYAEV belyaev@lapp.in2p3.fr 
+     *  @date   2005-03-04
+     */
+    struct Phi : public LoKi::Function<const Particle*>
+    {      
+      /// clone method (mandatory!)
+      virtual Phi* clone() const { return new Phi(*this) ; };      
+      /// the only one essential method 
+      result_type operator() ( argument p ) const 
+      {
+        if ( 0 == p ) 
+        {
+          Warning( "Particle* points to NULL!" ) ;
+          return -1000 ;
+        }
+        return p->momentum().phi() ;
+      };
+    };
+    
+  }; // end of namespace Particles 
+  
+}; //end of namespace LoKi
+  
 
 LOKI_ALGORITHM( LoKi_Higgs1 )
 {
@@ -26,44 +125,50 @@ LOKI_ALGORITHM( LoKi_Higgs1 )
   const EventHeader* evtHdr = 
     get<EventHeader>( EventHeaderLocation::Default ) ;
   
+  // trigger decisions 
+  const L0DUReport*  L0  = get<L0DUReport>  ( L0DUReportLocation  ::Default ) ;
+  const L1Score*     L1  = get<L1Score>     ( L1ScoreLocation     ::Default ) ;
+  const TrgDecision* TRG = get<TrgDecision> ( TrgDecisionLocation ::Default ) ;
+
   MCMatch mc = mctruth() ;
   
   // # select all stable MC particles, produced near collision point
-  MCCut   mccut = MCVXFUN( MCVZ ) < (  30 *  cm )  ;
-  MCRange mcall = 
-    mcselect ( "mcall" , mccut && ( ( "e+"    == MCABSID ) ||
-                                    ( "mu+"   == MCABSID ) ||
-                                    ( "K+"    == MCABSID ) ||
-                                    ( "p+"    == MCABSID ) ||
-                                    ( "pi+"   == MCABSID ) ||
-                                    ( "gamma" == MCABSID ) ) ) ;
+  MCCut   mccut = abs( MCVXFUN( MCVZ ) ) < (  40 *  cm )  ;
+  MCRange mcall = mcselect ( "mcall" , mccut    && ( MCPZ > 0 ) && 
+                             ( ( "e+"    == MCABSID ) ||
+                               ( "mu+"   == MCABSID ) ||
+                               ( "K+"    == MCABSID ) ||
+                               ( "p+"    == MCABSID ) ||
+                               ( "pi+"   == MCABSID ) ||
+                               ( "gamma" == MCABSID ) ) ) ;
   
   // # book N-Tuple for MC 
   Tuple tup1 = nTuple( "MC-tuple" ) ;
-
-  // # fill energy flow ntuple for MC particles   
-  for ( MCRange::iterator imcp = mcall.begin() ; mcall.end() != imcp ; ++imcp ) 
-  {
-    const MCParticle* mcp = *imcp ;
-    if ( 0 == mcp ) { continue ; }                              // CONTINUE 
-    
-    
-    const double eta = mcp->momentum().pseudoRapidity () ;
-    const double phi = mcp->momentum().phi            () ;
-    
-    // from W or Z ? 
-    const bool prim = 0 == mcp->mother() ;
-    
-    tup1 << Tuples::Column ( "" , evtHdr         ) ;
-    tup1 -> column ( "pid"  , MCID ( mcp )       ) ;
-    tup1 -> column ( "pt"   , MCPT ( mcp ) / GeV ) ;
-    tup1 -> column ( "e"    , MCE  ( mcp ) / GeV ) ;
-    tup1 -> column ( "phi"  , phi                ) ;
-    tup1 -> column ( "eta"  , eta                ) ;
-    tup1 -> column ( "prim" , prim               ) ;
-
-    tup1 -> write  () ;
-  };
+  
+  LoKi::MCParticles::PseudoRapidity MCETA ;
+  LoKi::MCParticles::Phi            MCPHI ;
+  // W or Z ? 
+  MCCut MCPRIM = MCMOTHCUT( !MCVALID , true ) ;
+  
+  tup1 << Tuples::Column ( "" , evtHdr         ) ;
+  tup1 << Tuples::Column ( "" , L0             ) ;
+  tup1 << Tuples::Column ( "" , L1             ) ;
+  tup1 << Tuples::Column ( "" , TRG            ) ;
+  
+  tup1 -> farray ( "pid"  , MCID       ,
+                   "pt"   , MCPT / GeV ,
+                   "e"    , MCE  / GeV ,
+                   mcall  .begin ()    , 
+                   mcall  .end   ()    , 
+                   "nMC"  , 5000       ) ;
+  tup1 -> farray ( "eta"  , MCETA      ,
+                   "phi"  , MCPHI      ,
+                   "prim" , MCPRIM     ,
+                   mcall  .begin ()    , 
+                   mcall  .end   ()    , 
+                   "nMC"  , 5000       ) ;
+  
+  tup1 -> write  () ;
   
   // # get only MC leptons 
   MCRange mcleps = mcselect ( "mclep" , 
@@ -86,28 +191,34 @@ LOKI_ALGORITHM( LoKi_Higgs1 )
   // # select *ALL* recontructed particles 
   Range all = select ( "all" , PALL ) ;
   
+  LoKi::Particles::PseudoRapidity ETA ;
+  LoKi::Particles::Phi            PHI ;
+  
   // # retrieve tuple for reconstructed energy flow 
   Tuple tup2 = nTuple( "RC-tuple" ) ;
-  for ( Range::iterator ip = all.begin() ; all.end() != ip ; ++ip ) 
-  {
-    const Particle* p = *ip ;
-    if ( 0 == p ) { continue ; }
-    
-    const double eta = p->momentum().pseudoRapidity () ;
-    const double phi = p->momentum().phi            () ;
-    
-    tup2 << Tuples::Column ( "" , evtHdr     ) ;
-    tup2 -> column ( "pid"  , ID ( p )       ) ;
-    tup2 -> column ( "pt"   , PT ( p ) / GeV ) ;
-    tup2 -> column ( "e"    , E  ( p ) / GeV ) ;
-    tup2 -> column ( "phi"  , phi            ) ;
-    tup2 -> column ( "eta"  , eta            ) ;
-    tup2 -> column ( "mcm"  , mcCutLep( p )  ) ;
-    tup2 -> column ( "pidm" , PIDmu   ( p )  ) ;
-    
-    tup2 -> write() ;
-  };
   
+  tup2 << Tuples::Column ( "" , evtHdr         ) ;
+  tup2 << Tuples::Column ( "" , L0             ) ;
+  tup2 << Tuples::Column ( "" , L1             ) ;
+  tup2 << Tuples::Column ( "" , TRG            ) ;
+  
+  tup2 -> farray ( "pid" , ID        , 
+                   "pt"  , PT / GeV  , 
+                   "e"   , E  / GeV  ,
+                   all   .begin ()   ,
+                   all   .end   ()   ,
+                   "nRC" , 500       ) ;
+  
+  tup2 -> farray ( "phi" , PHI       , 
+                   "eta" , ETA       , 
+                   "mcm" , mcCutLep  ,
+                   "pidm", PIDmu     ,
+                   all   .begin ()   ,
+                   all   .end   ()   ,
+                   "nRC" , 500       ) ;
+  
+  
+  tup2 -> write() ;
 
   return StatusCode::SUCCESS ;
 };
