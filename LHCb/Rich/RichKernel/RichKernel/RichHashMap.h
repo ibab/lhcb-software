2a4c9f1440a1ae4fc7245d26801f0c484919e665
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-03-06 13:02:34 +0000

    update doxygen comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@7202 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 384dcff97ab68c940f9d1db0a8f00a965493c53e

--- LHCb/Rich/RichKernel/RichKernel/RichHashMap.h
+++ LHCb/Rich/RichKernel/RichKernel/RichHashMap.h
@@ -2,10 +2,10 @@
 //--------------------------------------------------------------------------------
 /** @file RichHashMap.h
  *
- *  Header file for utility has map for the RICH : Rich::HashMap
+ *  Header file for utility hash map for the RICH : RichHashMap
  *
  *  CVS Log :-
- *  $Id: RichHashMap.h,v 1.2 2005-03-05 16:52:17 jonrob Exp $
+ *  $Id: RichHashMap.h,v 1.3 2005-03-06 13:02:34 jonrob Exp $
  *
  *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date   2005-01-11
@@ -15,12 +15,6 @@
 #ifndef RICHKERNEL_RICHHASHMAP_H
 #define RICHKERNEL_RICHHASHMAP_H 1
 
-// GaudiKernel
-#include "GaudiKernel/stl_hash.h"
-
-// STD
-#include <map>
-
 //--------------------------------------------------------------------------------
 /** @class RichHashMap RichHashMap.h RichKernel/RichHashMap.h
  *
@@ -28,28 +22,40 @@
  *  loop up table like access.
  *
  *  @attention
- *  On windows, implementation uses a standard std::map as this appears
- *  to be faster. This might be a feature of the debug libraries. 
- *  To be reviewed if optimised libraries are ever used on windows
+ *  Hash maps are not currently a part of the standard stl, only the stl
+ *  extensions. Also on windows, the hash map appears to be less efficient 
+ *  than the standard map, so this is used instead. 
+ *  To be reviewed as the stl hash maps evolve.
  *
  *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
  *  @date   2005-01-11
  *
- *  @todo Keep an eye on the windows implementation
+ *  @todo Keep an eye on the underlying hash map implementations for both 
+ *        linux and windows
  */
 //--------------------------------------------------------------------------------
 
+#ifdef __GNUC__
+// Linux : Use std extension hash map
+
+// GaudiKernel
+#include "GaudiKernel/stl_hash.h"
+
 template < class KEY, class VALUE >
 class RichHashMap
-#ifdef __GNUC__
-  // Linux : Use std extension hash map
-  : public __gnu_cxx::hash_map < KEY, VALUE, __gnu_cxx::hash< KEY > >
+  : public __gnu_cxx::hash_map < KEY, VALUE, __gnu_cxx::hash< KEY > > { };
+
 #else
-  // For windows, using normal map as it appears to be faster
-  //: public stdext::hash_map < KEY, VALUE, stdext::hash_compare< KEY > >
-  : public std::map < KEY, VALUE >
+// All other platforms use standard stl map
+
+// STD
+#include <map>
+
+template < class KEY, class VALUE >
+class RichHashMap
+  : public std::map < KEY, VALUE > { };
+//: public __gnu_cxx::hash_map < KEY, VALUE, __gnu_cxx::hash_compare< KEY > > { };
+
 #endif
-{
-};
 
 #endif // RICHKERNEL_RICHHASHMAP_H
