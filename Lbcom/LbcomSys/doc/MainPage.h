435809ad46d969aea306454ce23554cbb3ff232d
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-03-14 07:19:29 +0000

    start v1r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@56592 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 82efe38d2b2598ecd7b2b6bf82e85dc32fd084a8

--- Lbcom/LbcomSys/doc/MainPage.h
+++ Lbcom/LbcomSys/doc/MainPage.h
@@ -2,16 +2,15 @@
  *  \anchor lbcomdoxygenmain
  *
  * This is the code reference manual for LHCb component libraries that are
- * shared by several applications. These include:
- * \li Monitoring packages (OTMonitor)
-
- * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the LbcomSys packages. More
- * information is available from the 
- * <a href="http://cern.ch/lhcb-comp/Frameworks/LbcomSys/">web pages</a>
- * of the Lbcom project
+ * shared by several applications.
  *
- * \sa
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the LbcomSys packages. 
+ *
+ * \sa 
+ * \li <a href="../release.notes"><b>Lbcom release notes</b></a>
+ * \li <a href="requirements-source.html"><b>List of Lbcom packages</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Frameworks/LbcomSys/"><b>Lbcom project Web pages</b></a><p>
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
  * \li \ref gaudidoxygenmain "Gaudi documentation" 
  * \li \ref externaldocs     "Related external libraries"
