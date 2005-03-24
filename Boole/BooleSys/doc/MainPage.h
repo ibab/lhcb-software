ddc6a03868b794150915901f3b3967c18f68ee6e
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-03-24 16:56:14 +0000

    v8r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@56625 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3782ab23de7f3515711123a1d793a81aab4743d1

--- Boole/BooleSys/doc/MainPage.h
+++ Boole/BooleSys/doc/MainPage.h
@@ -2,14 +2,14 @@
  *  \anchor booledoxygenmain
  *
  * This is the code reference manual for the Boole digization classes.
-
+ *
  * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the BooleSys packages. More
- * information is available from the 
- * <a href="http://cern.ch/lhcb-comp/Digitization/">web pages</a>
- * of the Boole project
+ * state of the software for this version of the BooleSys packages.
  *
  * \sa
+ * \li <a href="../release.notes"><b>Boole release notes</b></a>
+ * \li <a href="requirements-source.html"><b>List of BooleSys packages</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Digitization/"><b>Boole project Web pages</b></a><p>
  * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
  * \li \ref gaudidoxygenmain "Gaudi documentation"
