6518f8d2d7ae674a5caffb860439943ecffdb8c9
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2005-04-01 19:02:21 +0000

    files added for Windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15512 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0b8b6b69e4422c48e9610026d792bf7011e18413

---
+++ Rec/Tr/TrackTools/src/TrackTools_load.cpp
@@ -0,0 +1,17 @@
+// Include files 
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+// The statements are like that:
+//
+// DECLARE_CONVERTER( MyConverter );
+// DECLARE_OBJECT( DataObject );
+//
+// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
+
+DECLARE_FACTORY_ENTRIES( TrackTools ) {
+
+  DECLARE_TOOL( ClusterLHCbIDCnv );
+                                                 
+}
