64c3aa9858b0ea8144a380bf24dcd20e823e1986
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-04-07 14:56:45 +0000

    updates for v5r9
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47042 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b78af568acdbc0d820c82aae7bfef886e85d0f5c

--- LHCb/Kernel/LHCbKernel/Kernel/CLHEPStreams.h
+++
@@ -1,8 +0,0 @@
-// $Id: CLHEPStreams.h,v 1.3 2005-01-26 07:15:08 cattanem Exp $
-#ifndef KERNEL_CLHEPSTREAMS_H 
-#define KERNEL_CLHEPSTREAMS_H 1
-
-// Dummy file for backward compatibility
-// Can be removed once it has been removed from all event model XML files
-
-#endif // KERNEL_CLHEPSTREAMS_H
--- LHCb/Kernel/LHCbKernel/Kernel/LHCbStreams.h
+++
@@ -1,9 +0,0 @@
-// $Id: LHCbStreams.h,v 1.4 2005-02-09 13:00:29 cattanem Exp $
-#ifndef KERNEL_LHCBSTREAMS_H 
-#define KERNEL_LHCBSTREAMS_H 1
-
-// This file is obsolete!! Kept only for backward compatibility.
-// Please include SerializeStl.h directly!
-#include "Kernel/SerializeStl.h" // also includes GaudiKernel/StreamBuffer.h
-
-#endif // KERNEL_LHCBSTREAMS_H
