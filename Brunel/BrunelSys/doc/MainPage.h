abfdcd538f2e94391ab82f26a33b90568ab79210
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-04-07 06:15:32 +0000

    begin v26r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@29243 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 35da2642d850518f1134a60d4368215903382cb2

--- Brunel/BrunelSys/doc/MainPage.h
+++ Brunel/BrunelSys/doc/MainPage.h
@@ -2,14 +2,14 @@
  *  \anchor bruneldoxygenmain
  *
  * This is the code reference manual for the Brunel reconstruction classes.
-
- * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the BrunelSys packages. More
- * information is available from the 
- * <a href="http://cern.ch/lhcb-comp/Reconstruction/">web pages</a>
- * of the Brunel project
+ *
+ * These pages have been generated directly from the code and reflect the exact
+ * state of the software for this version of the BrunelSys packages.
  *
  * \sa
+ * \li <a href="../release.notes"><b>Brunel release notes</b></a>
+ * \li <a href="requirements-source.html"><b>List of BrunelSys packages</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Reconstruction/"><b>Brunel project Web pages</b></a><p>
  * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
  * \li \ref gaudidoxygenmain "Gaudi documentation"
