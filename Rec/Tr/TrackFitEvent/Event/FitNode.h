3a37c196e718bf9b2487cbc916c8d45b69f5305b
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2005-04-15 15:01:44 +0000

    FitNode class added
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15530 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 08a5640a07cc839d6e28153fb76d4d1dd147a91c

---
+++ Rec/Tr/TrackFitEvent/Event/FitNode.h
@@ -0,0 +1,119 @@
+#ifndef TRACKFITEVENT_FITNODE_H
+#define TRACKFITEVENT_FITNODE_H 1
+
+// from CLHEP
+#include "CLHEP/Matrix/Matrix.h"
+
+// from TrackEvent
+#include "Event/Node.h"
+#include "Event/Measurement.h"
+
+/** @class FitNode FitNode.h
+ *
+ *  This File contains the declaration of the FitNode.
+ *  A FitNode is a basket of objects at a given z position.
+ *  The information inside the FitNode has to be sufficient 
+ *  to allow smoothing and refitting.
+ *  At the moment a FitNode contains or allows you to access
+ *  info on the the (kth) measurement, 
+ *  transport from k --> k + 1 , predicted state at k+1  
+ *  (predicted from filter step)  and the best state at k  
+ *  (notation note filter procedes from k -> k + 1 -> k + 2 ......)
+ *  
+ *  @author Eduardo Rodrigues (adaptations to the new track event model)
+ *  @date   2005-04-15
+ *
+ *  @author Matthew Needham
+ *  @date   19-08-1999
+ */
+
+class FitNode: public Node {
+public:
+  /// Default constructor
+  FitNode();
+
+  /// Constructor from a Measurement
+  FitNode( Measurement* aHit );
+
+  /// Destructor
+  virtual ~FitNode();
+
+//  /// retrieve pointer to Node Measurement
+//  Measurement* measurementOnTrack() const { return m_meas; }
+
+  /// retrieve transport matrix
+  const HepMatrix& transportMatrix() const  { return m_transportMatrix; }
+
+  /// retrieve transport vector
+  const HepVector& transportVector() const { return m_transportVector; }
+
+  /// retrieve noise matrix
+  const HepSymMatrix& noiseMatrix() const { return m_noiseMatrix; }
+
+  /// set transport matrix
+  void setTransportMatrix( HepMatrix transportMatrix ) {
+    m_transportMatrix = transportMatrix;
+  }
+  
+  /// set transport vector
+  void setTransportVector( const HepVector& transportVector ) {
+    m_transportVector = transportVector;
+  }
+  
+  /// set noise matrix
+  void setNoiseMatrix( const HepSymMatrix& noiseMatrix ) {
+    m_noiseMatrix = noiseMatrix;
+  }  
+
+  /// retrieved state predicted by the kalman filter step
+  State* predictedState() const             { return m_predictedState; }
+
+  /// set state predicted by the kalman filter
+  void setPredictedState( State* predictedState ) {
+    m_predictedState = predictedState; 
+  }
+
+  /// Update the State predicted by the Kalman filter
+  void updatePredictedState( State* predictedState );
+
+  /// get filtered state from the kalman filter step
+  State* filteredState() const              { return m_filteredState; }
+  
+
+  /// set filtered state from the kalman filter step
+  void setFilteredState( State* filteredState ) {
+    m_filteredState = filteredState;
+  }
+
+  /// Update the filtered state from the Kalman filter step
+  void updateFilteredState( State* filteredState );
+
+  /// get best = smoothed state from node
+  State* bestState() const                 {  return m_state;  }
+
+  /// set best = smoothed state from node
+  void setBestState( State* bestState )      {  m_state = bestState; }
+  
+  /// update best = smoothed state from node
+  void updateBestState( State* state );
+
+  /// add the transport transformation of prevNode to this node
+  void addNode( FitNode* node );
+
+  /// z position of Node
+  double z() const   { return m_measurement -> z(); };
+
+private:
+
+  HepMatrix      m_transportMatrix;  ///< transport matrix
+  HepVector      m_transportVector;  ///< transport vector
+  HepSymMatrix   m_noiseMatrix;      ///< noise matrix
+//  Measurement*   m_meas;             ///< measurement
+  State*         m_predictedState;   ///< predicted state from filter step
+  State*         m_filteredState;    ///< filtered State at this Node
+//  State*         m_bestState;        ///< best state at this node
+
+};
+
+
+#endif // TRACKFITEVENT_FITNODE_H
---
+++ Rec/Tr/TrackFitEvent/src/FitNode.cpp
@@ -0,0 +1,138 @@
+// Include files
+// -------------
+// local
+#include "Event/FitNode.h"
+
+/** @file FitNode.cpp
+ *
+ *  This File contains the implementation of the FitNode.
+ *  A FitNode is a basket of objects at a given z position.
+ *  The information inside the FitNode has to be sufficient 
+ *  to allow smoothing and refitting.
+ *  At the moment a FitNode contains or allows you to access
+ *  info on the the (kth) measurement, 
+ *  transport from k --> k + 1 , predicted state at k+1  
+ *  (predicted from filter step)  and the best state at k  
+ *  (notation note filter procedes from k -> k + 1 -> k + 2 ......)
+ *  
+ *  @author Eduardo Rodrigues (adaptations to the new track event model)
+ *  @date   2005-04-15
+ *
+ *  @author Matt Needham
+ *  @date   11-11-1999
+ */
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+FitNode::FitNode():
+  m_predictedState(0),
+  m_filteredState(0)
+{
+  // FitNode default constructer
+  m_transportMatrix = HepMatrix(5, 5, 1);
+  m_transportVector = HepVector(5, 0);
+  m_noiseMatrix     = HepSymMatrix(5, 0);
+  m_state           = 0;
+}
+
+//=============================================================================
+// Constructor from a Measurement
+//=============================================================================
+FitNode::FitNode(Measurement* aMeas):
+  m_predictedState(0),
+  m_filteredState(0)
+{
+  //FitNode constructer
+  m_transportMatrix = HepMatrix(5, 5, 1);
+  m_transportVector = HepVector(5, 0);
+  m_noiseMatrix     = HepSymMatrix(5, 0);
+  m_state           = 0;
+  m_measurement     = aMeas;
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+FitNode::~FitNode()
+{
+  delete m_predictedState;
+  delete m_filteredState;
+  delete m_state;
+}
+
+//=============================================================================
+// Update the State predicted by the Kalman filter
+//=============================================================================
+void FitNode::updatePredictedState( State* predictedState )
+{  
+  //if pointer not set clone state - else copy (not great)
+  if ( !m_predictedState ) {
+    m_predictedState = predictedState -> clone();    
+  }
+  else{
+    // get reference to the state vector and cov
+    HepVector& tX    = m_predictedState -> state();
+    HepSymMatrix& tC = m_predictedState -> covariance();
+   
+    //update 
+    tX = predictedState -> state();
+    tC = predictedState -> covariance();
+
+  }
+}
+
+//=============================================================================
+// Update the filtered state from the Kalman filter step
+//=============================================================================
+void FitNode::updateFilteredState( State* filteredState )
+{  
+  //if pointer not set clone state - else copy (not great)
+  if ( !m_filteredState ) {
+    m_filteredState = filteredState -> clone();
+  }
+  else{
+    // get reference to the state vector and cov
+    HepVector& tX    = m_filteredState -> state();
+    HepSymMatrix& tC = m_filteredState -> covariance();
+   
+    //update 
+    tX = filteredState -> state();
+    tC = filteredState -> covariance();
+
+  }
+}
+
+//=============================================================================
+// 
+//=============================================================================
+void FitNode::updateBestState( State* state )
+{
+  /// update the best state 
+  //if pointer not set clone state - else copy (not great)
+  if ( !m_state ) {
+    m_state = state -> clone();
+  }
+  else{
+    // get reference to the state vector and cov
+    HepVector& tX    = m_state -> state();
+    HepSymMatrix& tC = m_state -> covariance();
+
+    //update 
+    tX = state -> state();
+    tC = state -> covariance();
+  }
+}
+
+//=============================================================================
+// Add the transport transformation of prevNode to this node
+//=============================================================================
+void FitNode::addNode( FitNode* prevNode )
+{
+  // add the transport transformation of prevNode to this node
+  m_transportVector += m_transportMatrix * prevNode->transportVector() ;
+  m_noiseMatrix += (prevNode->noiseMatrix()).similarity( m_transportMatrix );
+  m_transportMatrix = m_transportMatrix * prevNode->transportMatrix();  
+}
+
+//=============================================================================
