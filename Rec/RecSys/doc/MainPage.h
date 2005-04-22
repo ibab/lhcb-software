271427cca6199bf74337933dda2e57fc1efdf97a
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-04-22 13:47:57 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@29261 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: aa45b106338faed6be0da04730fbed97fd235f9a

---
+++ Rec/RecSys/doc/MainPage.h
@@ -0,0 +1,17 @@
+/** \mainpage notitle
+ *  \anchor recsysdoxygenmain
+ *
+ * This is the code reference manual for the LHCb reconstruction classes.
+ *
+ * These pages have been generated directly from the code and reflect the exact
+ * state of the software for this version of the RecSys packages.
+ *
+ * \sa
+ * \li <a href="../release.notes"><b>Brunel release notes</b></a>
+ * \li <a href="requirements-source.html"><b>List of RecSys packages</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Reconstruction/"><b>Brunel project Web pages</b></a><p>
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref externaldocs     "Related external libraries"
+
+ */
