4aa8faf5355dce3c53ddea3a00414f2af2b2f496
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-04-25 13:41:42 +0000

    update to use RecSys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@29265 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8908ad129945222ae45eead20f66056f59cdfd81

--- Brunel/BrunelSys/doc/MainPage.h
+++ Brunel/BrunelSys/doc/MainPage.h
@@ -1,15 +1,15 @@
 /** \mainpage notitle
  *  \anchor bruneldoxygenmain
  *
- * This is the code reference manual for the Brunel reconstruction classes.
+ * This is the code reference manual for the Brunel reconstruction program.
  *
  * These pages have been generated directly from the code and reflect the exact
- * state of the software for this version of the BrunelSys packages.
+ * state of the software for this version of Brunel.
  *
  * \sa
  * \li <a href="../release.notes"><b>Brunel release notes</b></a>
- * \li <a href="requirements-source.html"><b>List of BrunelSys packages</b></a>
  * \li <a href="http://cern.ch/lhcb-comp/Reconstruction/"><b>Brunel project Web pages</b></a><p>
+ * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction classes)"
  * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
  * \li \ref gaudidoxygenmain "Gaudi documentation"
