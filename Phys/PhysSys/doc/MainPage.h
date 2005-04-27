66a2dfcdbd79c673d7608cb36f2eca813c2703b3
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-04-27 16:06:28 +0000

    New physics project
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62754 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 18afa971985ef81d3a453cedbf20cb3385ce5cd8

---
+++ Phys/PhysSys/doc/MainPage.h
@@ -0,0 +1,17 @@
+/** \mainpage notitle
+ *  \anchor bruneldoxygenmain
+ *
+ * This is the code reference manual for the DaVinci Physics Analysis classes.
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the DaVinciSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-comp/Analysis/">web pages</a>
+ * of the DaVinci project
+ *
+ * \sa
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
+ * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref externaldocs     "Related external libraries"
+
+ */
