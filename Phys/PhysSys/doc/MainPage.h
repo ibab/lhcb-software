e2736c109a7e3bee67cb333e36ed8a39a7cfca83
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-04-27 16:12:10 +0000

    Doxygen
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62756 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9de63d735dc901e09fea0d025a3fe680857d2b2c

--- Phys/PhysSys/doc/MainPage.h
+++ Phys/PhysSys/doc/MainPage.h
@@ -1,10 +1,10 @@
 /** \mainpage notitle
  *  \anchor bruneldoxygenmain
  *
- * This is the code reference manual for the DaVinci Physics Analysis classes.
+ * This is the code reference manual for the Physics Analysis classes.
 
  * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the DaVinciSys packages. More
+ * state of the software for this version of the PhysSys packages. More
  * information is available from the 
  * <a href="http://cern.ch/lhcb-comp/Analysis/">web pages</a>
  * of the DaVinci project
