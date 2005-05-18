55fd180205bf1514007cb399f72211c6c2b29314
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-05-18 09:11:42 +0000

    improvements to doc
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@56778 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d7b85ed9301ec0728542745552ec0bdc1035605e

--- Lbcom/LbcomSys/doc/MainPage.h
+++ Lbcom/LbcomSys/doc/MainPage.h
@@ -2,17 +2,18 @@
  *  \anchor lbcomdoxygenmain
  *
  * This is the code reference manual for LHCb component libraries that are
- * shared by several applications.
- *
- * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the LbcomSys packages. 
- *
- * \sa 
- * \li <a href="../release.notes"><b>Lbcom release notes</b></a>
- * \li <a href="requirements-source.html"><b>List of Lbcom packages</b></a>
- * \li <a href="http://cern.ch/lhcb-comp/Frameworks/LbcomSys/"><b>Lbcom project Web pages</b></a><p>
- * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
- * \li \ref gaudidoxygenmain "Gaudi documentation" 
+ * shared by several applications. See:
+ * <a href="requirements-source.html"><b>Lbcom constituent packages</b></a>
+ * <p><b>See also:</b> 
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core packages)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)" 
  * \li \ref externaldocs     "Related external libraries"
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ *
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Frameworks/LbcomSys/"><b>Lbcom project Web pages</b></a><p>
  
  */
