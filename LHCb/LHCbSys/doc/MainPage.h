8191e3a38c6be9b03ccb0cdbe956dc24d123a13c
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-05-24 12:33:32 +0000

    v18r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@56784 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7144eb8e86a99cbaf004cefa79244cc4e95c669d

--- LHCb/LHCbSys/doc/MainPage.h
+++ LHCb/LHCbSys/doc/MainPage.h
@@ -1,22 +1,25 @@
 /** \mainpage notitle
  *  \anchor lhcbdoxygenmain
  *
- * This is the code reference manual for the LHCb core classes. These include:
+ * This is the code reference manual for the LHCb core classes (See
+ * <a href="requirements-source.html"><b>LHCb constituent packages</b></a>).
+ * These include:
  * \li common interfaces
- * \li common base classes
+ * \li common base classes and tools
  * \li event model classes
  * \li detector description
  * \li associators to MonteCarlo truth
  * \li tools to encode and decode the DAQ buffers
 
- * These pages have been generated directly from the code and reflect the exact
- * state of the software for this version of the LHCbSys packages.
- *
- * \sa
- * \li <a href="../release.notes"><b>LHCbSys release notes</b></a>
- * \li <a href="requirements-source.html"><b>List of LHCbSys packages</b></a>
- * \li <a href="http://cern.ch/lhcb-comp/Frameworks/LHCbSys/"><b>LHCbSys project Web pages</b></a><p>
- * \li \ref gaudidoxygenmain "Gaudi documentation" 
+ * <p>See also:
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
  * \li \ref externaldocs     "Related external libraries"
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ *
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Frameworks/LHCbSys/"><b>LHCb project Web pages</b></a><p>
  
  */
