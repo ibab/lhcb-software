54bcc1687e4dfea16f608f21c2e149dcd4183772
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2005-05-24 12:45:19 +0000

    creation of Tr/TrackMCTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15570 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b61c42a37f148f040b94c96c375b4281ebbe8298

---
+++ Rec/Tr/TrackMCTools/src/TrackMCTools_load.cpp
@@ -0,0 +1,9 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_FACTORY_ENTRIES( TrackMCTools ) {
+
+  DECLARE_TOOL( TrackAcceptance );
+  DECLARE_TOOL( TrackSelector );
+  DECLARE_TOOL( TrueStateCreator );
+
+}
