35072e7e410df006980c8cacd2ed3a3f6519b3d9
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-06-03 11:26:08 +0000

    Moore application for HLT
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@68540 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7d20977e4bb3278ae5f746a8c61fd094662b6c25

---
+++ Moore/MooreSys/doc/MainPage.h
@@ -0,0 +1,22 @@
+/** \mainpage notitle
+ *  \anchor davincidoxygenmain
+ *
+ * This is the code reference manual for the DaVinci Physics Analysis classes.
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the DaVinciSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-comp/Analysis/">web pages</a>
+ * of the DaVinci project
+ *
+ * \sa
+ * \li <a href="../release.notes"><b>DaVinci release notes</b></a>
+ * \li <a href="http://lhcb-comp.web.cern.ch/lhcb-comp/Analysis/default.htm"><b>DaVinci project Web pages</b></a><p>
+ * \li \ref physsysdoxygenmain "PhysSys documentation (LHCb physics classes)"
+ * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction classes)"
+ * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
+ * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref externaldocs     "Related external libraries"
+
+ */
