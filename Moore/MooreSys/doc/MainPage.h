82cd74a45e27ce5a74f04009e0906bb383847c6f
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-06-10 14:47:15 +0000

    cleanups for first release
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@68569 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4c51c18c79d609ebb0f0af6825178ee08bbc00cb

--- Moore/MooreSys/doc/MainPage.h
+++ Moore/MooreSys/doc/MainPage.h
@@ -1,22 +1,23 @@
 /** \mainpage notitle
- *  \anchor davincidoxygenmain
+ *  \anchor mooredoxygenmain
  *
- * This is the code reference manual for the DaVinci Physics Analysis classes.
-
- * These pages have been generated directly from the code and reflect the exact 
- * state of the software for this version of the DaVinciSys packages. More
- * information is available from the 
- * <a href="http://cern.ch/lhcb-comp/Analysis/">web pages</a>
- * of the DaVinci project
+ * <b>The classes used by Moore are documented in the following reference manuals:</b>
  *
- * \sa
- * \li <a href="../release.notes"><b>DaVinci release notes</b></a>
- * \li <a href="http://lhcb-comp.web.cern.ch/lhcb-comp/Analysis/default.htm"><b>DaVinci project Web pages</b></a><p>
- * \li \ref physsysdoxygenmain "PhysSys documentation (LHCb physics classes)"
- * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction classes)"
- * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
- * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
- * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref physsysdoxygenmain "PhysSys documentation (LHCb physics analysis classes)"
+ * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction packages)"
+ * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb packages shared with Digitization)"
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core packages)"
+ * \li \ref onlinedoxygenmain  "OnlineSys documentation (LHCb online packages)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
  * \li \ref externaldocs     "Related external libraries"
+ *
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+
+ * <b>Additional information:</b>
+ * \li <a href="http://cern.ch/lhcb-trig/HLT/"><b>High Level Trigger Web pages</b></a>
+ * \li <a href="http://cern.ch/lhcb-comp/Reconstruction/"><b>Brunel project Web pages</b></a>
+ * \li <a href="../release.notes"><b>Release notes history</b></a><p>
 
  */
