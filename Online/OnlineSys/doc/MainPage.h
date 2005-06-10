d51683cc5147242e708f80a24f7096bbd70970bc
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-06-10 14:59:25 +0000

    Cleanup for release
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@95885 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33ecd10cc91dbe0f93750175ea2554eda3a265da

---
+++ Online/OnlineSys/doc/MainPage.h
@@ -0,0 +1,18 @@
+/** \mainpage notitle
+ *  \anchor onlinedoxygenmain
+ *
+ * This is the code reference manual for LHCb libraries linked by applications
+ * running in the Online trigger farm. See:
+ * <a href="requirements-source.html"><b>Online constituent packages</b></a>
+ * <p><b>See also:</b> 
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core packages)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)" 
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ *
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href="http://cern.ch/lhcb-online/"><b>Online project Web pages</b></a><p>
+ 
+ */
