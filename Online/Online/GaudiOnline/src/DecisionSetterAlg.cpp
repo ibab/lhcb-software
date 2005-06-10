68231f5a5cf024d386ad8b8dcc0efaf365f68fb1
Markus Frank <frankb@cern.ch>
2005-06-10 10:08:44 +0000

    Fill L1 decision information
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@95884 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9a35cfc102f83872898e40b82a8f1ca5346a43d4

--- Online/Online/GaudiOnline/src/DecisionSetterAlg.cpp
+++ Online/Online/GaudiOnline/src/DecisionSetterAlg.cpp
@@ -1,4 +1,4 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/GaudiOnline/src/DecisionSetterAlg.cpp,v 1.3 2005-05-04 17:14:09 frankb Exp $
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/GaudiOnline/src/DecisionSetterAlg.cpp,v 1.4 2005-06-10 10:08:44 frankb Exp $
 //	====================================================================
 //  DecisionSetterAlg.cpp
 //	--------------------------------------------------------------------
@@ -14,9 +14,10 @@
 #include "GaudiKernel/SmartDataPtr.h"
 #include "GaudiOnline/OnlineAddress.h"
 #include "GaudiOnline/StreamDescriptor.h"
+#include "Event/L1Score.h"
+#include "Event/L1Event.h"
 
 using GaudiOnline::StreamDescriptor;
-
 
 /**@class DecisionSetterAlg 
   *
@@ -44,17 +45,54 @@ public:
         GaudiOnline::Address* addr = dynamic_cast<GaudiOnline::Address*>(reg->address());
         const GaudiOnline::StreamDescriptor* dsc = addr->descriptor();
         int data[8];     // 8 words of L1 decision data see: LHCb 2003-80
+
         memset(data,0,sizeof(data));
         if ( dsc )  {
           switch( dsc->type() ) {
           case GaudiOnline::StreamDescriptor::L1_BUFFER:
-            data[0] = 1;
+	    {
+              SmartDataPtr<L1Score> l1_score(eventSvc(),L1ScoreLocation::Default);
+              L1Score* score = l1_score;
+              const double ushort_max = 327670.;
+              if ( score ) {
+                int l1_decision = score->decisionPattern();
+                // Values according to T.Schietinger (10/06/2005)
+                l1_int pt1 = static_cast<l1_int>(std::max(ushort_max,score->pt1()));
+                l1_int pt2 = static_cast<l1_int>(std::max(ushort_max,score->pt2()));
+                l1_int ptm = static_cast<l1_int>(std::max(ushort_max,score->ptMu()));
+                l1_int dmm = static_cast<l1_int>(std::max(ushort_max,score->diMuMass()));
+                l1_int dmi = static_cast<l1_int>(std::max(ushort_max,score->diMuIP()));
+                l1_int ete = static_cast<l1_int>(std::max(ushort_max,score->etElec()));
+                l1_int etp = static_cast<l1_int>(std::max(ushort_max,score->etPhot()));
+                data[0] = score->eventNumber();
+                data[1] = ((l1_decision&0xFFFFFF)<<8) + (l1_decision&0x1);
+                data[2] = l1_decision;
+                data[3] = static_cast<int>((pt1 << 16)  + pt2);
+                data[4] = static_cast<int>(ptm << 16);
+                data[5] = static_cast<int>((dmm << 16) + dmi);
+                data[6] = static_cast<int>((ete << 16) + etp);
+                data[7] = 0;
+                return dsc->setDecision(&data[0], sizeof(data));
+	      }
+	      MsgStream err(msgSvc(),name());
+              err << MSG::ERROR << "No L1Score result available." << endmsg;
+              return StatusCode::FAILURE;              
+	    }
             break;
           case GaudiOnline::StreamDescriptor::DAQ_BUFFER:
-            data[0] = 1;
+	    {
+              SmartDataPtr<L1Score> l1_score(eventSvc(),L1ScoreLocation::Default);
+              L1Score* score = l1_score;
+              if ( score ) {
+                // Values according to T.Schietinger (10/06/2005)
+                int hlt_decision = 1;
+                data[0] = score->eventNumber();
+                data[1] = ((~0x0)<<8) + (hlt_decision&0xFF);
+                return dsc->setDecision(&data[0], sizeof(data));
+	      }
+	    }
             break;
           }
-          return dsc->setDecision(&data[0], sizeof(data));
         }
       }
     }
