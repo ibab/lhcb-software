e5b550b1a5f8b5c2119e878ed4fee2de3af2e7cb
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-06-15 06:26:39 +0000

    FOI
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21121 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9c7610ea2fb03534ce1486f14f39c081a6faee54

--- Rec/Muon/MuonID/src/MuonIDDLLTool.cpp
+++ Rec/Muon/MuonID/src/MuonIDDLLTool.cpp
@@ -1,11 +1,8 @@
-// $Id: MuonIDDLLTool.cpp,v 1.1 2003-05-30 09:16:32 gcorti Exp $
+// $Id: MuonIDDLLTool.cpp,v 1.2 2005-06-15 06:26:39 pkoppenb Exp $
 
 // Include files
 // from Gaudi
 #include "GaudiKernel/ToolFactory.h"
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/IToolSvc.h"
-#include "GaudiKernel/GaudiException.h"
 
 #include "MuonTools/IMuonTileXYZTool.h"
 #include "MuonTools/IMuonGeometryTool.h"
@@ -19,27 +16,32 @@
 //  Description: Calculates the likelihood variable for the MuonID
 //
 //  Author     : Joao R. Torres de Mello Neto, Miriam Gandelman 
-//
+//   updated   : Andre Massafferri 
 //--------------------------------------------------------------------
 
 //==================================================================
 // Instantiation of a static factory class used by clients to create
 // instances of this service
 //==================================================================
+
 static ToolFactory<MuonIDDLLTool> s_factory;
 const IToolFactory& MuonIDDLLToolFactory = s_factory;
 
 //==================================================================
 // Standard Constructor
 //==================================================================
+
 MuonIDDLLTool::MuonIDDLLTool(const std::string& type, 
-                    const std::string& name, const IInterface* parent) 
-  : AlgTool( type, name, parent)
+                             const std::string& name, const IInterface* parent) 
+  : GaudiTool( type, name, parent)
+  , m_trackSlopeX(0.)
+  , m_Momentum(.0)
+  , m_NStation()
 {
 
   declareInterface<IMuonIDDLLTool>(this);
-  declareProperty("distPion",m_distPion);
   declareProperty("distMuon",m_distMuon);
+  declareProperty("distPion",m_distPion);
 
 }
 //==================================================================
@@ -47,42 +49,42 @@ MuonIDDLLTool::MuonIDDLLTool(const std::string& type,
 //==================================================================
 StatusCode MuonIDDLLTool::initialize() {
   
-  MsgStream saida(msgSvc(), name());
-  saida << MSG::DEBUG << "==> Initialise" << endreq;
-   // get geometry tool
-     StatusCode sc;
-     sc = toolSvc()->retrieveTool("MuonTileIDXYZ", m_iTileTool);
-           if( sc.isFailure() ) {
-            saida << MSG::FATAL << "    Unable to create MuonTileIDToXYZ tool" << endreq;
-            return sc;
-           }
-     sc = toolSvc()->retrieveTool("MuonGeometryTool", m_iGeomTool);
-	   if( sc.isFailure() ) {
-	    saida << MSG::FATAL << "    Unable to create MuonGeometry tool" << endreq;
-	   return sc;
-	   }
+  StatusCode sc = GaudiTool::initialize();
+  if (!sc) return sc; // in principle useless
 
-     sc = m_iGeomTool->nStation(m_NStation);
-         if(!sc){
-           return sc;
-         }
-     m_stationZ.resize(m_NStation);
-     int station;
-     double dx,dy,dz;
-     for(station = 0 ; station < m_NStation ; station++ ){
-           sc = m_iGeomTool->getStationBox(station,dx,dy,
-                                m_stationZ[station],dz);
-        if(!sc){
-          return sc;
-        }
+  debug() << "==> Initialise" << endreq;
+  // get geometry tool
+  m_iTileTool = tool<IMuonTileXYZTool>("MuonTileIDXYZ");
+  if( !m_iTileTool ) {
+    fatal() << "    Unable to create MuonTileIDToXYZ tool" << endreq;
+    return StatusCode::FAILURE;
+  }
+  m_iGeomTool = tool<IMuonGeometryTool>("MuonGeometryTool");
+  if( !m_iGeomTool) {
+    fatal() << "Unable to create MuonGeometry tool" << endreq;
+    return StatusCode::FAILURE;
+  }
+
+  sc = m_iGeomTool->nStation(m_NStation);
+  if(!sc){
+    return sc;
+  }
+  m_stationZ.resize(m_NStation);
+  int station;
+  double dx,dy,dz;
+  for(station = 0 ; station < m_NStation ; station++ ){
+    sc = m_iGeomTool->getStationBox(station,dx,dy,
+                                    m_stationZ[station],dz);
+    if(!sc){
+      return sc;
     }
-    if( m_distPion.size() == 0 || m_distMuon.size() == 0){
-          saida << MSG::ERROR
-           << "OPTIONS are wrong:"
-           << " size of m_distPion or m_distMuon vector is not correct"
-           << endreq;
-       return StatusCode::FAILURE;
-    }
+  }
+  if( m_distPion.size() != 9 || m_distMuon.size() != 9){
+    err() << "OPTIONS are wrong:"
+          <<" size of m_distPion or m_distMuon vector is not correct"
+          << endreq;
+    return StatusCode::FAILURE;
+  }
 
   return StatusCode::SUCCESS;
   
@@ -91,63 +93,131 @@ StatusCode MuonIDDLLTool::initialize() {
 //==================================================================
 // Calculate the likelihood given a MuonID
 //==================================================================
+
 StatusCode MuonIDDLLTool::calcMuonDLL( const MuonID* muonid, double& dll ) {
 
-  MsgStream saida(msgSvc(), name());
-
   if ( !muonid->IsMuon() ) {
-      return StatusCode::FAILURE;
+    return StatusCode::FAILURE;
   }
-     const TrStoredTrack* pTrack = muonid->idTrack();
-     // do the track extrapolations
-     StatusCode sc = trackExtrapolate(pTrack);
-     if(!sc){
-           saida << MSG::ERROR << "stopped in trackExtrapolate"  << endreq;
-           return StatusCode::FAILURE;
-     }
 
-     SmartRefVector<MuonCoord> mcoord = muonid->Coords();
-     SmartRefVector<MuonCoord>::iterator iCoord;
-     double dist = 0.;
-     for( iCoord = mcoord.begin() ; iCoord != mcoord.end() ; iCoord++ ){
-        double x,dx,y,dy,z,dz;
-        StatusCode sc =
-         m_iTileTool->calcTilePos((*iCoord)->key(),x,dx,y,dy,z,dz);
-         if(sc.isFailure()){
-            MsgStream saida(msgSvc(), name());
-            saida << MSG::ERROR << "Failed to get x,y,z of tile " << (*iCoord)->key() << endreq;
-            return sc;
-	 }
-        int station = (*iCoord)->key().station();
-	dist = dist + sqrt( pow(x - m_trackX[station],2) +
-			    pow(y - m_trackY[station],2) );
+  const TrStoredTrack* pTrack = muonid->idTrack();
+  // do the track extrapolations
+  StatusCode sc = trackExtrapolate(pTrack);
+
+  if (!sc){
+    err() << "stopped in trackExtrapolate"  << endreq;
+    return StatusCode::FAILURE;
+  }
+
+  SmartRefVector<MuonCoord> mcoord = muonid->Coords();
+
+  SmartRefVector<MuonCoord>::iterator iCoord;
+  double dist = 0.;
+
+  for ( iCoord = mcoord.begin() ; iCoord != mcoord.end() ; iCoord++ ){
+
+    double x,dx,y,dy,z,dz;
+        
+    StatusCode sc =
+      m_iTileTool->calcTilePos((*iCoord)->key(),x,dx,y,dy,z,dz);
+         
+    if (sc.isFailure()){
+      err() << "Failed to get x,y,z of tile " << (*iCoord)->key() << endreq;
+      return sc;
+    }
+
+    int station = (*iCoord)->key().station();
 	
-     }
-     dist = dist / mcoord.size(); 
-     double distmax = 600.;
-     double nbins = 30.;
-     double small = 1.*pow(10,-20);
-     int ibin = (int)( dist * nbins / distmax );
-     saida << MSG::DEBUG << "ibin =  " << ibin << endreq;
-     saida << MSG::DEBUG << "mdistpion =  " << m_distPion[0] << endreq;
-     saida << MSG::DEBUG << "m_distPion[ibin] =  " << m_distPion[ibin] << endreq;
-     saida << MSG::DEBUG << "m_distMuon[ibin] =  " << m_distMuon[ibin] << endreq;
-     if (dist < distmax && m_distPion[ibin] >0.) {
-	 dll = m_distMuon[ibin] / m_distPion[ibin];
-	 if (dll < small) {
-	    dll = -20.;
-	 }
-         else{
-	    dll = log(dll);	 
-	 } 	     
-     }
-     else {
-           return StatusCode::FAILURE;
-     }
-     saida << MSG::DEBUG << "dist =  " << dist << endreq;
-     saida << MSG::DEBUG << "dll =  " << dll << endreq;
+    dist = dist + pow(((x - m_trackX[station])/dx),2) +
+      pow(((y - m_trackY[station])/dy),2) ;
+  }
+
+  dist = dist / mcoord.size(); 
+     
+  double ProbMu = m_distMuon[0] * (landau_root (dist,m_distMuon[1],m_distMuon[2])) + 
+    m_distMuon[3] * (landau_root (dist,m_distMuon[4],m_distMuon[5]));
+  double ProbPi = m_distPion[0] * (landau_root (dist,m_distPion[1],m_distPion[2])) + 
+    m_distPion[3] * (landau_root (dist,m_distPion[4],m_distPion[5])) +
+    m_distPion[6] * (landau_root (dist,m_distPion[7],m_distPion[8]));
+  double small  = 1e-20;    
+     
+  dll = 5.0;  // initialization: is a muon
+  
+  if ((ProbPi >= small) && (dist < 200.0)){
+    dll = ProbMu / ProbPi;   // the main case
+    if (dll < small) {   // small ProbMu or/and high ProbPi
+      dll = -11.0;      // thus is a pion
+    } else {
+      dll = log(dll);  // the main case
+    }
+  } else {
+    dll = 5.0; // is a muon
+    if (dist >= 200.0){  // change the initialization for very high dists
+      dll = -11.0;  // is a pion
+    }
+  }
 
   return StatusCode::SUCCESS;
+
+}
+
+// defining the Landau function as placed in root
+
+
+double MuonIDDLLTool::landau_root(double x, double mpv, double sigma){
+
+  double p1[5] = {0.4259894875,-0.1249762550, 0.03984243700, -0.006298287635,   0.001511162253};
+  double q1[5] = {1.0         ,-0.3388260629, 0.09594393323, -0.01608042283,    0.003778942063};
+  double p2[5] = {0.1788541609, 0.1173957403, 0.01488850518, -0.001394989411,   0.0001283617211};
+  double q2[5] = {1.0         , 0.7428795082, 0.3153932961,   0.06694219548,    0.008790609714};
+  double p3[5] = {0.1788544503, 0.09359161662,0.006325387654, 0.00006611667319,-0.000002031049101};
+  double q3[5] = {1.0         , 0.6097809921, 0.2560616665,   0.04746722384,    0.006957301675};
+  double p4[5] = {0.9874054407, 118.6723273,  849.2794360,   -743.7792444,      427.0262186};
+  double q4[5] = {1.0         , 106.8615961,  337.6496214,    2016.712389,      1597.063511};
+  double p5[5] = {1.003675074,  167.5702434,  4789.711289,    21217.86767,     -22324.94910};
+  double q5[5] = {1.0         , 156.9424537,  3745.310488,    9834.698876,      66924.28357};
+  double p6[5] = {1.000827619,  664.9143136,  62972.92665,    475554.6998,     -5743609.109};
+  double q6[5] = {1.0         , 651.4101098,  56974.73333,    165917.4725,     -2815759.939};
+  double a1[3] = {0.04166666667,-0.01996527778, 0.02709538966};
+  double a2[2] = {-1.845568670,-4.284640743};
+					          
+  if (sigma <= 0) return 0;
+  double v = (x-mpv)/sigma;
+  double u, ue, us, den;
+  if (v < -5.5) {
+    u   = exp(v+1.0);
+    ue  = exp(-1/u);
+    us  = sqrt(u);
+    den = 0.3989422803*(ue/us)*(1+(a1[0]+(a1[1]+a1[2]*u)*u)*u);
+  } else if(v < -1) {
+    u   = exp(-v-1);
+    den = exp(-u)*sqrt(u)*
+      (p1[0]+(p1[1]+(p1[2]+(p1[3]+p1[4]*v)*v)*v)*v)/
+      (q1[0]+(q1[1]+(q1[2]+(q1[3]+q1[4]*v)*v)*v)*v);
+  } else if(v < 1) {
+    den = (p2[0]+(p2[1]+(p2[2]+(p2[3]+p2[4]*v)*v)*v)*v)/
+      (q2[0]+(q2[1]+(q2[2]+(q2[3]+q2[4]*v)*v)*v)*v);
+  } else if(v < 5) {
+    den = (p3[0]+(p3[1]+(p3[2]+(p3[3]+p3[4]*v)*v)*v)*v)/
+      (q3[0]+(q3[1]+(q3[2]+(q3[3]+q3[4]*v)*v)*v)*v);
+  } else if(v < 12) {
+    u   = 1/v;
+    den = u*u*(p4[0]+(p4[1]+(p4[2]+(p4[3]+p4[4]*u)*u)*u)*u)/
+      (q4[0]+(q4[1]+(q4[2]+(q4[3]+q4[4]*u)*u)*u)*u);
+  } else if(v < 50) {
+    u   = 1/v;
+    den = u*u*(p5[0]+(p5[1]+(p5[2]+(p5[3]+p5[4]*u)*u)*u)*u)/
+      (q5[0]+(q5[1]+(q5[2]+(q5[3]+q5[4]*u)*u)*u)*u);
+  } else if(v < 300) {
+    u   = 1/v;
+    den = u*u*(p6[0]+(p6[1]+(p6[2]+(p6[3]+p6[4]*u)*u)*u)*u)/
+      (q6[0]+(q6[1]+(q6[2]+(q6[3]+q6[4]*u)*u)*u)*u);
+  } else {
+    u   = 1/(v-v*log(v)/(v+1));
+    den = u*u*(1+(a2[0]+a2[1]*u)*u);
+  }
+
+  return den;
 
 }
 
@@ -156,39 +226,37 @@ StatusCode MuonIDDLLTool::calcMuonDLL( const MuonID* muonid, double& dll ) {
 //=============================================================================
 StatusCode MuonIDDLLTool::trackExtrapolate(const TrStoredTrack *pTrack){
 
-  MsgStream saida(msgSvc(), name());
   resetTrackLocals();
 
   // get state closest to M1
   const TrStateP *stateP =
-     dynamic_cast<const TrStateP*>(pTrack->closestState(m_stationZ[0]));
-     if(!stateP){
-       MsgStream saida(msgSvc(), name());
-       saida << MSG::ERROR << " Failed to get stateP from track " << endreq;
-     return StatusCode::FAILURE;
-     }
+    dynamic_cast<const TrStateP*>(pTrack->closestState(m_stationZ[0]));
+  if(!stateP){
+    err() << " Failed to get stateP from track " << endreq;
+    return StatusCode::FAILURE;
+  }
 
   // get the momentum (MeV/c)
-     m_Momentum = stateP->p();
+  m_Momentum = stateP->p();
  
   // get the track slope
-     m_trackSlopeX = stateP->tx();
+  m_trackSlopeX = stateP->tx();
   
   // Project the state into the muon stations
-     int station;
-     for(station = 0; station < 5 ; station++){
-  // x(z') = x(z) + (dx/dz * (z' - z))
-        m_trackX.push_back(stateP->x() + ( stateP->tx() * 
-				(m_stationZ[station] - stateP->z()) ));
-        m_trackY.push_back(stateP->y() + ( stateP->ty() * 
-				(m_stationZ[station] - stateP->z()) ));
-      }
-       saida << MSG::DEBUG << " mom =  " << m_Momentum << endreq;
-       saida << MSG::DEBUG << " z =  " << m_stationZ[0] << endreq;
-       saida << MSG::DEBUG << " m_trackX " << m_trackX[0] << endreq;
-       saida << MSG::DEBUG << " m_trackY " << m_trackY[0] << endreq;
+  int station;
+  for(station = 0; station < 5 ; station++){
+    // x(z') = x(z) + (dx/dz * (z' - z))
+    m_trackX.push_back(stateP->x() + ( stateP->tx() * 
+                                       (m_stationZ[station] - stateP->z()) ));
+    m_trackY.push_back(stateP->y() + ( stateP->ty() * 
+                                       (m_stationZ[station] - stateP->z()) ));
+  }
+  debug() << " mom =  " << m_Momentum << endreq;
+  debug() << " z =  " << m_stationZ[0] << endreq;
+  debug() << " m_trackX " << m_trackX[0] << endreq;
+  debug() << " m_trackY " << m_trackY[0] << endreq;
 
- return StatusCode::SUCCESS;
+  return StatusCode::SUCCESS;
 }
 void MuonIDDLLTool::resetTrackLocals(){
   m_Momentum = -1.;
--- Rec/Muon/MuonID/src/MuonIDDLLTool.h
+++ Rec/Muon/MuonID/src/MuonIDDLLTool.h
@@ -1,9 +1,9 @@
-// $Id: MuonIDDLLTool.h,v 1.1 2003-05-30 09:16:32 gcorti Exp $
+// $Id: MuonIDDLLTool.h,v 1.2 2005-06-15 06:26:39 pkoppenb Exp $
 #ifndef MUONIDDLLTOOL_H
 #define MUONIDDLLTOOL_H 1
 
 // from Gaudi
-#include "GaudiKernel/AlgTool.h"
+#include "GaudiAlg/GaudiTool.h"
 
 // from DaVinciTools
 #include "MuonID/IMuonIDDLLTool.h"
@@ -15,16 +15,16 @@ class IMuonTileXYZTool;
  *
  * @author Joao R. Torres de Mello Neto, Miriam Gandelman
  * @date 24/04/2003 
-*/
+ */
 
-class MuonIDDLLTool : public AlgTool,
-                   virtual public IMuonIDDLLTool {
+class MuonIDDLLTool : public GaudiTool,
+                      virtual public IMuonIDDLLTool {
 
 public:
 
   /// Standard Constructor
   MuonIDDLLTool( const std::string& type, const std::string& name, 
-              const IInterface* parent);
+                 const IInterface* parent);
 
   /// Standard Destructor
   virtual ~MuonIDDLLTool() { }
@@ -35,6 +35,16 @@ public:
   /// Inputs: a MuonID object
   StatusCode calcMuonDLL( const MuonID* muonid, double& dll );
 
+protected:
+  /// Extract the momentum and extrapolate the track to each station
+  StatusCode trackExtrapolate(const TrStoredTrack *pTrack);
+  /// clear track based local variables
+  void resetTrackLocals();
+
+  double landau_root(double x, double mpv, double sigma);		   
+
+private:
+
   double m_trackSlopeX;
   std::vector<double> m_trackX; // position of track in x(mm) in each station
   std::vector<double> m_trackY; // position of track in y(mm) in each station
@@ -43,14 +53,13 @@ public:
   IMuonGeometryTool *m_iGeomTool;
   int m_NStation;
   std::vector<double> m_stationZ; // station position
-  /// Extract the momentum and extrapolate the track to each station
-  StatusCode trackExtrapolate(const TrStoredTrack *pTrack);
-  /// clear track based local variables
-  void resetTrackLocals();
+
   std::vector<double> m_distPion;
   std::vector<double> m_distMuon;
-//double m_distPion;
-//double m_distMuon;
+
+  //double m_distPion;
+  //double m_distMuon;
+
 };
 
 #endif // MUONIDDLLTOOL_H
