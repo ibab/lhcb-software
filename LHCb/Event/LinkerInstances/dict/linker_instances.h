ca16c4bccfbba4b2aa8997b72d4a2966da30ddfa
Thomas Ruf <thomas.ruf@cern.ch>
2005-06-30 07:25:06 +0000

    Modified Files:  	dict/linker_instances.h doc/release.notes Version to be used for after-DC04 software suites
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34181 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e83a196d15fe4cb9791d871ca39ca7ad5969edc2

--- LHCb/Event/LinkerInstances/dict/linker_instances.h
+++ LHCb/Event/LinkerInstances/dict/linker_instances.h
@@ -5,13 +5,13 @@
 #include "Event/TrgTrack.h"
 #include "Event/MuonCoord.h"
 #include "Event/ITCluster.h"
-#include "Event/OTCluster.h"
+#include "Event/OTTime.h"
 #include "Event/TrStoredTrack.h"
 
 namespace {
   LinkedTo<MCParticle,VeloCluster> i1 (0,0,"");
   LinkedTo<MCParticle,TrgTrack>    i2 (0,0,"");
-  LinkedTo<MCParticle,OTCluster> i3 (0,0,"");
+  LinkedTo<MCParticle,OTTime> i3 (0,0,"");
   LinkedTo<MCParticle,ITCluster> i4 (0,0,"");
   LinkedTo<MCParticle,MuonCoord> i5 (0,0,"");
   LinkedTo<MCParticle,TrStoredTrack> i6 (0,0,"");
