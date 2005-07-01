4959526fe2d824124406ba82d2f5163cb6292d91
Julien Cogan <jucogan@cern.ch>
2005-07-01 12:41:28 +0000

    Add a new generic Class: Property which is use to store the properties of a unit.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@56917 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5225e9c3ceaab0bf40246e30fc006bcf90abcac5

---
+++ LHCb/L0/ProcessorKernel/ProcessorKernel/Property.h
@@ -0,0 +1,120 @@
+#ifndef PROPERTY_H
+#define PROPERTY_H 1
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "MuonKernel/MuonTileID.h" 
+
+//! Property contains the value of a L0MConf database field.
+/**
+    It defines operators to output the field value and to convert 
+    it to a integer, a string, a MuonTileID or a L0MTileType. 
+*/
+namespace L0Muon {
+
+class Property{
+ private:
+  std::string m_value;
+
+ public:
+
+  /**
+     Construct an empty Property.
+  */
+  Property():m_value(){}
+
+  /**
+     Construct an Property from a string.
+  */
+  Property(const std::string &value):m_value(value){}
+
+  /**
+     Destruct the Property.
+  */
+  ~Property(){};
+
+
+  const std::string value() const {return m_value;}
+  
+  /** 
+      Build vectors
+  */
+  const std::vector<int> getVectorOfInt() const;
+  const std::vector<long> getVectorOfLong() const; 
+  const std::vector<float> getVectorOfFloat() const; 
+  const std::vector<double> getVectorOfDouble() const; 
+  
+
+   /**
+     Convert to bool.
+  */
+  operator const bool() const {return bool(atoi(m_value.c_str()));}
+
+  /**
+     Convert to int.
+  */
+  operator const int() const {return atoi(m_value.c_str());}
+
+  /**
+     Convert to long.
+  */
+  operator const long() const {return atol(m_value.c_str());}
+
+  /**
+     Convert to float.
+  */
+  operator const float() const {return float(atof(m_value.c_str()));}
+
+  /**
+     Convert to double.
+  */
+  operator const double() const {return atof(m_value.c_str());}
+
+  /**
+     Convert to vector of int.
+  */
+  operator const std::vector<int>() const {return getVectorOfInt();}
+
+  /**
+     Convert to vector of int.
+  */
+  operator const std::vector<long>() const {return getVectorOfLong();}
+
+  /**
+     Convert to vector of float.
+  */
+  operator const std::vector<float>() const {return getVectorOfFloat();}
+
+  /**
+     Convert to vector of double.
+  */
+  operator const std::vector<double>() const {return getVectorOfDouble();}
+
+  /**
+     Convert to char.
+  */
+  //operator const char*() const {return m_value.c_str();}
+
+  /**
+     Convert to string.
+  */
+  operator const std::string() const {return m_value;}
+
+  /**
+     Convert to MuonTileID.
+  */
+  operator const MuonTileID() const {return MuonTileID(atol(m_value.c_str()));}
+
+  
+};
+
+/**
+    Define ouput operator.
+    Output the internal string.
+*/
+std::ostream& operator<< (std::ostream& os, const Property  &fv);
+};  // namespace L0Muon
+
+#endif // PROPERTY
---
+++ LHCb/L0/ProcessorKernel/src/Property.cpp
@@ -0,0 +1,82 @@
+#include "ProcessorKernel/Property.h"
+
+
+
+/** 
+    Build vectors
+*/
+const std::vector<int> L0Muon::Property::getVectorOfInt() const{
+  std::vector<int> vi;
+  const char * str = m_value.c_str();
+  int length = m_value.size();
+  char * endptr;
+
+  for (int k=0;k<length;k++){
+    int i = strtol(str,&endptr,0);
+    if (endptr==str) {
+      break;
+    }
+    str = endptr+1;
+    vi.push_back(i);
+  }
+  return vi;
+}
+const std::vector<long> L0Muon::Property::getVectorOfLong() const{
+  std::vector<long> vl;
+  const char * str = m_value.c_str();
+  int length = m_value.size();
+  char * endptr;
+
+  for (int k=0;k<length;k++){
+    long l = strtol(str,&endptr,0);
+    if (endptr==str) {
+      break;
+    }
+    str = endptr+1;
+    vl.push_back(l);
+  }
+  return vl;
+}
+const std::vector<float> L0Muon::Property::getVectorOfFloat() const{
+  std::vector<float> vf;
+  const char * str = m_value.c_str();
+  int length = m_value.size();
+  char * endptr;
+
+  for (int k=0;k<length;k++){
+    float f = strtof(str,&endptr);
+    if (endptr==str) {
+      break;
+    }
+    str = endptr+1;
+    vf.push_back(f);
+  }
+  return vf;
+}
+const std::vector<double> L0Muon::Property::getVectorOfDouble() const{
+  std::vector<double> vd;
+  const char * str = m_value.c_str();
+  int length = m_value.size();
+  char * endptr;
+
+  for (int k=0;k<length;k++){
+    double d = strtod(str,&endptr);
+    if (endptr==str) {
+      break;
+    }
+    str = endptr+1;
+    vd.push_back(d);
+  }
+  return vd;
+}
+/**
+    Define ouput operator.
+    Output the internal string.
+*/
+std::ostream& operator<< (std::ostream& os, const L0Muon::Property  &property)
+{
+
+  const std::string s = property;
+  return os << s;
+
+}
