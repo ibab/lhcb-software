40a6c488688c38ffb44f999d7a1d8781919f3f6f
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2005-07-01 15:45:24 +0000

    clean-up of debugging info
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15621 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0e8959d119a936a42876f27ec746fcd48c5b2ec9

--- Rec/Tr/TrackFitEvent/src/ITMeasurement.cpp
+++ Rec/Tr/TrackFitEvent/src/ITMeasurement.cpp
@@ -1,4 +1,4 @@
-// $Id: ITMeasurement.cpp,v 1.3 2005-06-15 15:24:10 erodrigu Exp $
+// $Id: ITMeasurement.cpp,v 1.4 2005-07-01 15:45:24 erodrigu Exp $
 // Include files 
 
 #include "STDet/STDetectionLayer.h"
@@ -29,24 +29,13 @@ ITMeasurement::ITMeasurement( ITCluster& itCluster,
   // set the LHCbID
   setLhcbID ( LHCbID( ITChan ) );
 
-  std::cout << "- ITChannelID is " << ITChan << std::endl
-            << " with channelID = " << ITChan.channelID() << std::endl
-            << " station        = " << ITChan.station() << std::endl
-            << " layer          = " << ITChan.layer() << std::endl
-            << " wafer          = " << ITChan.wafer() << std::endl
-            << " strip          = " << ITChan.strip() << std::endl;
-
-  std::cout << "- IT channelID from LHCbID is:" << std::endl
-            << " lhcbID().stID()      = " << lhcbID().stID() << std::endl
-            << " lhcbID().channelID() = " << lhcbID().channelID() << std::endl;
-
   const STDetectionLayer* ITLay = geom.layer( ITChan );
   m_measure      = ITLay->U( ITChan ) + m_cluster->distToStripCenter() ;
   m_errMeasure    = m_cluster->distToStripError();
   m_z            = ITLay->centerZ( ITChan );
 //  m_stereoAngle  = ITLay->stereoAngle();
 
-  std::cout << "- stereo angle = " << ITLay->stereoAngle() << std::endl;
+//  std::cout << "- stereo angle = " << ITLay->stereoAngle() << std::endl;
 
 }
 
