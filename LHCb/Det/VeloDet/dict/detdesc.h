166f77a97410b21beed5082178baceedd8bdda15
jpalac <jpalac@cern.ch>
2005-07-04 09:38:56 +0000

    2005-07-29 - Juan PALACIOS  - Add dict directory with dictionary generation. Add necessary patterns to    requirements file (from P. Mato).
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@23137 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5a22c98903bd0aded32ce3da49852c89d68fbdf5

---
+++ LHCb/Det/VeloDet/dict/detdesc.h
@@ -0,0 +1,16 @@
+#include "VeloDet/DeVeloSensor.h"
+#include "VeloDet/DeVeloRType.h"
+#include "VeloDet/DeVeloPhiType.h"
+#include "VeloDet/DeVelo.h"
+#include "DetDesc/Condition.h"
+#include "DetDesc/TabulatedProperty.h"
+
+
+//--- Templace instantiations
+namespace {
+  struct __Instantiations {
+    
+  };
+}
+
+ 
