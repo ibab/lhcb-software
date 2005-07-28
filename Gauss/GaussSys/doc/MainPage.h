0df0a19f035ce4901e91b71e1d80238d7e7a1f06
Gloria Corti <gloria.corti@cern.ch>
2005-07-28 10:01:42 +0000

    doxygen main page as for Boole/Brunel
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91804 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d0ff911330f07fcefe86ffeec565c339b4b93e8f

--- Gauss/GaussSys/doc/MainPage.h
+++ Gauss/GaussSys/doc/MainPage.h
@@ -1,18 +1,29 @@
 /** \mainpage notitle
  *  \anchor Gaussdoxygenmain
  *
- * This is the code reference manual for the Gauss Simulation Application  classes.
+ * <b>This code reference manual contains classes specific to Gauss Simulation Application</b>.<p>
 
  * These pages have been generated directly from the code and reflect the exact 
  * state of the software for this version of the GaussSys packages. More
  * information is available from the 
  * <a href="http://cern.ch/lhcb-comp/Simulation/">web pages</a>
- * of the Gauss project
+ * of the Gauss project<p>
  *
- * \sa
- * \li \ref lhcbdoxygenmain    "LHCbSys documentation"
- * \li \ref gaudidoxygenmain   "Gaudi documentation"
- * \li \ref geant4doxygenmain  "Geant4 documentation"
+ * <b> Other classes used by Gauss are documented in the following reference manuals:</b>
+   
+ *
+ * \li \ref lhcbdoxygenmain    "LHCbSys documentation (LHCb core packages)"
+ * \li \ref gaudidoxygenmain   "Gaudi documentation (Framework packages)"
+ * \li \ref geant4doxygenmain  "Geant4 documentation (Simulation toolkit)"
+ * \li <a href="http://mdobbs.home.cern.ch/mdobbs/HepMC/html_reference/index.html">HepMC (Generator event format)</a>
  * \li \ref externaldocs       "Related external libraries"
-
+ *
+ * <hr>
+ * <b> See also: </b><a href="_gauss_tape_8opts-source.html"><b>Sim content</b></a>, <a href="requirements-source.html"><b>Constituent packages</b></a><p>
+ * <hr>
+ *
+ * <b>Additional information:</b>
+ * \li <a href="http://cern.ch/lhcb-comp/Simulation/"><b>Gauss project Web pages</b></a>
+ * \li <a href="../release_notes.html"><b>Release notes history</b></a>
+ *
  */
