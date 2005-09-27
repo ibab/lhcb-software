733ee9b87020cf07cb78b57e0171f143675dc30a
Markus Frank <frankb@cern.ch>
2005-09-27 06:11:21 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@95944 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 69d4ea55e1eb4dde5f3b44cafc18e23aff7b9d96

--- Online/Online/OnlineKernel/src/RTL/rtl_globsec.cpp
+++ Online/Online/OnlineKernel/src/RTL/rtl_globsec.cpp
@@ -41,7 +41,7 @@ namespace {
       address[1] = c.address[1];
     }
   };
-  typedef std::map<std::string, GblDesc> GblDescMap;
+  typedef std::map<std::string, GblDesc*> GblDescMap;
   struct NamedGblHandlers : public GblDescMap  {
   };
   GblDescMap& namedGblHandlers() {
@@ -55,8 +55,8 @@ namespace {
 int lib_rtl_create_section(const char* sec_name, int size, void* address) {
   GblDescMap::const_iterator i = namedGblHandlers().find(sec_name);
   if ( i == namedGblHandlers().end() )  {
-    int* add = (int*)address;
 #ifdef VMS
+    int* add = (int*)address;
     int inadd[2]={0,0};
     DESCRIPTOR(name,sec_name);
     int size  = (size + 511)>>9;
@@ -67,6 +67,7 @@ int lib_rtl_create_section(const char* sec_name, int size, void* address) {
       return sc;
     }
 #elif linux
+    void** add = (void**)address;
     int fd = 0;
     int sysprot  = PROT_READ+PROT_WRITE;
     int sysflags = MAP_SHARED;
@@ -74,21 +75,24 @@ int lib_rtl_create_section(const char* sec_name, int size, void* address) {
     n += sec_name;
     fd = shm_open(n.c_str(),O_RDWR|O_CREAT,0644);
     if ( fd ) {
-      ::ftruncate(ft, size);
+      ::ftruncate(fd, size);
     }
-    add[1] = fd;
-    add[0] = (int)mmap (0, size, sysprot, sysflags, fd, 0);
+    add[0] = ::mmap (0, size, sysprot, sysflags, fd, 0);
     if ( add[0] != 0 )  {
-      GblDesc dsc;
-      dsc.fd = fd;
-      dsc.name = sec_name;
-      dsc.address[0] = add[0];
-      namedGblHandlers().insert(dsc); 
+      GblDesc *dsc = new GblDesc;
+      dsc->fd = fd;
+      dsc->size = size;
+      dsc->name = sec_name;
+      dsc->address[0] = add[0];
+      dsc->address[1] = add[1];
+      namedGblHandlers().insert(std::make_pair(dsc->name,dsc)); 
+      add[1] = dsc;
       return 1;
     }
     ::shm_unlink(n.c_str());
     return 0;
 #elif _WIN32
+    void** add = (void**)address;
     // Setup inherited security attributes (FIXME: merge somewhere else)
     SECURITY_ATTRIBUTES	sa = {sizeof(SECURITY_ATTRIBUTES), NULL, true};
     DWORD  siz  = (size/4096)*4096 + (((size%4096)==0) ? 0 : 4096);  //  multiple of page size
@@ -98,9 +102,16 @@ int lib_rtl_create_section(const char* sec_name, int size, void* address) {
       hMap = ::OpenFileMapping(FILE_MAP_ALL_ACCESS,FALSE,sec_name);
     }
     if ( hMap )  {
-      add[0] = (int)::MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,0);
+      add[0] = ::MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,0);
       if ( add[0] != 0 )  {
-        add[1] = (int)hMap;
+	GblDesc *dsc = new GblDesc;
+	dsc->fd = fd;
+	dsc->size = size;
+	dsc->name = sec_name;
+	dsc->address[0] = add[0];
+	dsc->address[1] = hMap;
+	namedGblHandlers().insert(std::make_pair(dsc->name,dsc)); 
+	add[1] = dsc;
         return 1;
       }
     }
@@ -112,8 +123,10 @@ int lib_rtl_create_section(const char* sec_name, int size, void* address) {
 
 /// Delete named global section
 int lib_rtl_delete_section(const char *sec_name)    {
-  GblDescMap::const_iterator i = namedGblHandlers().find(sec_name);
+  GblDescMap::iterator i = namedGblHandlers().find(sec_name);
   if ( i != namedGblHandlers().end() )  {
+    delete (*i).second;
+    namedGblHandlers().erase(i);
 #ifdef linux
     std::string n = "/";
     n += sec_name;
@@ -127,44 +140,86 @@ int lib_rtl_delete_section(const char *sec_name)    {
 
 /// Map global section a a specific address
 int lib_rtl_map_section(const char* sec_name, int size, void* address)   {
-  int* add = (int*)address;
+  void** add = (void**)address;
+  GblDescMap::const_iterator i = namedGblHandlers().find(sec_name);
+  if ( i == namedGblHandlers().end() )  {
 #ifdef VMS
-  DESCRIPTOR(name,sec_name);
-  int inadd[2]={0,0};
-  int flags   = SEC$M_EXPREG+SEC$M_WRT+SEC$M_SYSGBL;
-  str$upcase (&name, &name);
-  int status = sys$mgblsc (inadd,add,PSL$C_USER,flags,&name,0,0);
-  return status;
+    int* add = (int*)address;
+    DESCRIPTOR(name,sec_name);
+    int inadd[2]={0,0};
+    int flags   = SEC$M_EXPREG+SEC$M_WRT+SEC$M_SYSGBL;
+    str$upcase (&name, &name);
+    int status = sys$mgblsc (inadd,add,PSL$C_USER,flags,&name,0,0);
+    return status;
 #elif linux
-  return lib_rtl_create_section(sec_name, size, address);
-#elif _WIN32
-  HANDLE hMap = ::OpenFileMapping(FILE_MAP_ALL_ACCESS,FALSE,sec_name);
-  if ( hMap )  {
-    add[0] = (int)::MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,0);
+    int fd = 0;
+    int sysprot  = PROT_READ+PROT_WRITE;
+    int sysflags = MAP_SHARED;
+    std::string n = "/";
+    n += sec_name;
+    fd = shm_open(n.c_str(),O_RDWR|O_CREAT,0644);
+    if ( fd ) {
+      ::ftruncate(fd, size);
+    }
+    add[0] = ::mmap (0, size, sysprot, sysflags, fd, 0);
     if ( add[0] != 0 )  {
-      add[1] = (int)hMap;
+      GblDesc *dsc = new GblDesc;
+      dsc->fd = fd;
+      dsc->size = size;
+      dsc->name = sec_name;
+      dsc->address[0] = add[0];
+      dsc->address[1] = add[1];
+      namedGblHandlers().insert(std::make_pair(dsc->name,dsc)); 
+      add[1] = dsc;
       return 1;
     }
-  }
-  return 0;
+    ::shm_unlink(n.c_str());
+    return 0;
+#elif _WIN32
+    HANDLE hMap = ::OpenFileMapping(FILE_MAP_ALL_ACCESS,FALSE,sec_name);
+    if ( hMap )  {
+      add[0] = ::MapViewOfFile(hMap,FILE_MAP_ALL_ACCESS,0,0,0);
+      if ( add[0] != 0 )  {
+	GblDesc *dsc = new GblDesc;
+	dsc->fd = fd;
+	dsc->size = size;
+	dsc->name = sec_name;
+	dsc->address[0] = add[0];
+	dsc->address[1] = hMap;
+	namedGblHandlers().insert(std::make_pair(dsc->name,dsc)); 
+	add[1] = dsc;
+	return 1;
+      }
+    }
+    return 0;
 #endif
+  }
+  add[1] = (*i).second;
+  add[0] = (*i).second->address[0];
+  // Section already mapped: Success
+  return 1;
 }
 
 /// Unmap global section: address is quadword: void*[2]
 int lib_rtl_unmap_section(void* address)   {
+  GblDesc* dsc = (GblDesc*)(((void**)address)[1]);
+  GblDescMap::iterator i = namedGblHandlers().find(dsc->name);
+  if ( i != namedGblHandlers().end() )  {
 #ifdef VMS
-  int radd[2];
-  int* inadd = (int*)address;
-  int status = sys$deltva (inadd, radd, PSL$C_USER);
-  return sys$purgws (inadd);
+    int radd[2];
+    int* inadd = (int*)address;
+    int status = sys$deltva (inadd, radd, PSL$C_USER);
+    int sc = sys$purgws (inadd);
 #elif linux
-  void** inadd = (void**)address;
-  int sc = ::munmap(inadd[0],1024*1024*1024);
-  return sc==0 ? 1 : 1;  
+    int sc = ::munmap(dsc->address[0],dsc->size)==0 ? 1 : 0;
 #elif _WIN32
-  int* inadd = (int*)address;
-  return (::UnmapViewOfFile((void*)inadd[0]) == 0) ? 0 : 1;
+    int sc = (::UnmapViewOfFile(dsc->address[0]) == 0) ? 0 : 1;
 #endif
+    delete (*i).second;
+    namedGblHandlers().erase(i);
+    return sc;
+  }
+  return 0;
 }
 
 /// Flush global section to disk file
@@ -174,7 +229,7 @@ int lib_rtl_flush_section(void* address, int len)   {
   DWORD sc = ::FlushViewOfFile((void*)inadd[0],0);
 #elif linux
   void** inadd = (void**)address;
-  ::msync(inadd[0],len,MS_INVALIDATE|MS_SYNC);
+  ::msync(inadd[0], len, MS_INVALIDATE|MS_SYNC);
 #endif
   return 1;
 }
