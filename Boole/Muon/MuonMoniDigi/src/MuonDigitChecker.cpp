c70fb0ba8f5af5a2bc273fb190d3f6a59d5bbc23
Alessio Sarti <alessio.sarti@lnf.infn.it>
2005-09-30 15:23:13 +0000

    Cleanup sugested by M.Cattaneo
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@21146 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 825a0b9b96aee6683174ba4d5c144be88a1a6dec

--- Boole/Muon/MuonMoniDigi/src/MuonDigitChecker.cpp
+++ Boole/Muon/MuonMoniDigi/src/MuonDigitChecker.cpp
@@ -5,36 +5,13 @@
 //
 //   Author: A. Sarti
 //   Created: 20-04-2005
-#include <cmath>
-
-// from Gaudi
 #include "GaudiKernel/AlgFactory.h"
-#include "GaudiAlg/Tuples.h"
-#include "GaudiAlg/GaudiHistoAlg.h"
 
 // local
-#include "MuonDet/MuonBasicGeometry.h"
-#include "Event/MCMuonHit.h"
 #include "MuonDigitChecker.h"
-#include "Event/MCParticle.h"
-#include "Event/MCHit.h"
-// from Event 
-#include "Event/EventHeader.h"
 
 static const  AlgFactory<MuonDigitChecker>  s_Factory ;
 const        IAlgFactory& MuonDigitCheckerFactory = s_Factory ; 
-
-std::string MuonDigitChecker::spill[5] = 
-{"","/Prev","/PrevPrev","/Next","/NextNext"};
-std::string MuonDigitChecker::numreg[4] = 
-{"1","2","3","4"};
-std::string MuonDigitChecker::numsta[5]= 
-{"1","2","3","4","5"};
-std::string MuonDigitChecker::TESPathOfHitsContainer[4]=
-{"Hits","ChamberNoiseHits","FlatSpilloverHits","BackgroundHits"};
-std::string MuonDigitChecker::HitOr[6]=
-  {"Geant","FlastSpillOver","Low Energy Background","Chamber Noise",
-   "Crosstalk","Electronic Noise"};      
 
 //=============================================================================
 // Standard constructor, initializes variables
@@ -42,14 +19,13 @@ std::string MuonDigitChecker::HitOr[6]=
 MuonDigitChecker::MuonDigitChecker( const std::string& name,
                             ISvcLocator* pSvcLocator)
   : GaudiTupleAlg ( name , pSvcLocator ),
-    m_hitMonitor   ( false ), 
-    m_detailedMonitor   ( false )
+    m_hitMonitor   ( false )
 {
+  declareProperty( "hitMonitor"              ,m_hitMonitor );
+  setProperty( "NTupleProduce", "false" );
   setProperty( "HistoTopDir",  "MUON/" );
   setProperty( "NTupleTopDir", "MUON/" );
-  
-  declareProperty( "hitMonitor"              ,m_hitMonitor );
-  declareProperty( "DetailedMonitor"         ,m_detailedMonitor );
+
 }
 
 //=============================================================================
@@ -67,21 +43,10 @@ StatusCode MuonDigitChecker::initialize() {
     return Error("Failed to initialize", sc);
   }
   
-  StatusCode scs=toolSvc()->retrieveTool("MuonTileIDXYZ",m_MuonTileXYZ); 
-  // sc=toolSvc()->retrieveTool("MuonGetInfoTool",m_pGetInfo);
-  if(scs.isFailure())return StatusCode::FAILURE;
-
-  debug() << "==> Initialise" << endmsg;
+  m_MuonTileXYZ = tool<IMuonTileXYZTool>("MuonTileIDXYZ");
 
   MuonBasicGeometry basegeometry(detSvc(),msgSvc());
-  m_stationNumber=basegeometry.getStations();
-  m_regionNumber=basegeometry.getRegions();  int i=0;
-
-  while(i<m_stationNumber){
-    numsta[i]=basegeometry.getStationName(i);
-    debug()<<" station "<<i<<" "<<numsta[i]<<endmsg;
-    i++;
-  }
+  m_regionNumber=basegeometry.getRegions();
   m_partition=basegeometry.getPartitions();
 
   for(int ix=0; ix<6; ix++) {
@@ -101,63 +66,11 @@ StatusCode MuonDigitChecker::initialize() {
   return StatusCode::SUCCESS;
 };
 
-StatusCode MuonDigitChecker::crNtuples() {
-
-  if(m_hitMonitor) {
-    Tuple nt1 = nTuple(41,"MC HITS",CLID_ColumnWiseTuple);
-    
-    nt1->column("Run", m_run,0,1000000);
-    nt1->column("Event",m_evt,0,10000);
-    
-    nt1->farray("is", m_sta ,"Nhits",1000);
-    nt1->farray("ir", m_reg ,"Nhits",1000);
-    nt1->farray("ic", m_con ,"Nhits",1000);
-    nt1->farray("x",  m_x   ,"Nhits",1000);
-    nt1->farray("y",  m_y   ,"Nhits",1000);
-    nt1->farray("z",  m_z   ,"Nhits",1000);
-    nt1->farray("t",  m_time,"Nhits",1000);
-    nt1->farray("id", m_id  ,"Nhits",1000);
-    nt1->farray("px", m_px  ,"Nhits",1000);
-    nt1->farray("py", m_py  ,"Nhits",1000);
-    nt1->farray("pz", m_pz  ,"Nhits",1000);
-    nt1->farray("E",  m_E   ,"Nhits",1000);
-    nt1->farray("xv", m_xv  ,"Nhits",1000);
-    nt1->farray("yv", m_yv  ,"Nhits",1000);
-    nt1->farray("zv", m_zv  ,"Nhits",1000);
-    nt1->farray("tv", m_tv  ,"Nhits",1000);
-    nt1->farray("idm",m_mom ,"Nhits",1000);
-    nt1->write();  
-  }
-
-  Tuple nt2 = nTuple(42,"DIGITS",CLID_ColumnWiseTuple);
- 
-  nt2->column("Run"  , m_digit_run, 0,1000000);
-  nt2->column("Event", m_digit_evt, 0,10000);
-  nt2->farray("is",    m_digit_s,      "Ndigits",1000);
-  nt2->farray("ir",    m_digit_r,      "Ndigits",1000);
-  nt2->farray("x" ,    m_digit_x,      "Ndigits",1000);
-  nt2->farray("y" ,    m_digit_y,      "Ndigits",1000);
-  nt2->farray("z" ,    m_digit_z,      "Ndigits",1000);
-  nt2->farray("dx",    m_digit_dx,     "Ndigits",1000);
-  nt2->farray("dy",    m_digit_dy,     "Ndigits",1000);
-  nt2->farray("dz",    m_digit_dz,     "Ndigits",1000);
-  nt2->farray("t" ,    m_digit_time,   "Ndigits",1000);
-  nt2->farray("origin",m_digit_origin, "Ndigits",1000);
-  nt2->farray("bx"    ,m_digit_bx,     "Ndigits",1000);
-  nt2->farray("firing",m_digit_firing, "Ndigits",1000);
-  nt2->farray("multip",m_digit_multi,  "Ndigits",1000);
-  nt2->write();  
-
-  return StatusCode::SUCCESS;
-
-}
 //=============================================================================
 // Main execution
 //=============================================================================
 StatusCode MuonDigitChecker::execute() {
 
-  debug() << "==> Execute MuonDigitChecker" << endmsg;
- // Header
   const EventHeader* evt = get<EventHeader>(EventHeaderLocation::Default);
   int tnhit[5][4];    int tnDhit[5][4][6];  
   for(int ix=0; ix<6; ix++) {
@@ -174,29 +87,19 @@ StatusCode MuonDigitChecker::execute() {
       }
     }
   }
-  m_run = evt->runNum();
-  m_evt = evt->evtNum();
-  m_run = 1;
-  //m_evt = 1;
+  long m_run = evt->runNum();
+  long m_evt = evt->evtNum();
 
-  m_digit_run = evt->runNum();
-  m_digit_evt = evt->evtNum();
-  m_digit_run = 1;
-  // m_digit_evt = 1;
+  long m_digit_run = evt->runNum();
+  long m_digit_evt = evt->evtNum();
   
-  // loop over Muon Chambers : 4 regions x 5 stations
-  if(m_hitMonitor) {
-    m_sta.clear();  m_reg.clear();  m_con.clear();  m_x.clear();  m_y.clear();
-    m_z.clear();  m_time.clear();   m_id.clear();  m_px.clear();  m_py.clear();
-    m_pz.clear();  m_E.clear();  m_xv.clear();  m_yv.clear();  m_zv.clear();
-    m_tv.clear();   m_mom.clear();  
-  }
-  m_digit_s.clear();  m_digit_r.clear();
-  m_digit_x.clear();  m_digit_y.clear();  m_digit_z.clear();  
-  m_digit_dx.clear();
-  m_digit_dy.clear();  m_digit_dz.clear();  m_digit_time.clear();  
-  m_digit_origin.clear();  m_digit_bx.clear();
-  m_digit_multi.clear(); m_digit_firing.clear();
+  std::vector<float> m_sta,m_reg,m_con,m_x,m_y,m_z,m_time, m_id;
+  std::vector<float> m_px,m_py,m_pz,m_E,m_xv,m_yv,m_zv,m_tv, m_mom;
+  std::vector<float> m_ple,m_hen,m_dix,m_dxz,m_dyz;
+
+  std::vector<float> m_digit_s,m_digit_r,m_digit_x,m_digit_y;
+  std::vector<float> m_digit_z,m_digit_dx,m_digit_dy,m_digit_dz,m_digit_time;
+  std::vector<float> m_digit_origin,m_digit_bx,m_digit_firing,m_digit_multi;
 
   // loop over Muon Hits only if required
   if(m_hitMonitor) {
@@ -212,16 +115,19 @@ StatusCode MuonDigitChecker::execute() {
       //              3 = Low Energy Background
       for(int container=0; container<4; container++){
       
-	// spill[0] = evento corrente
-	//       n  (n=1,4) = evento a n*25 ns precedente
+	// spill[0] = current event
+	//       n  (n=1,4) = event preceeding the current for n*25 ns 
       
-	std::string path="/Event"+spill[0]+"/MC/Muon/"+
-	  numsta[station]+"/R"+numreg[region]+"/"+
-	  TESPathOfHitsContainer[container];
+	char subpath[200];
+	std::string spill[5] = {"","/Prev","/PrevPrev","/Next","/NextNext"};
+	std::string TESContainer[4]=
+	  {"Hits","ChamberNoiseHits","FlatSpilloverHits","BackgroundHits"};
+
+	sprintf(subpath,"%s %d %s %d %s","/MC/Muon/",station,"/R",region,"/");
+	std::string path="/Event"+spill[0]+subpath+TESContainer[container];
+
       
-	debug() <<" path" <<path <<endmsg;
-      
-	SmartDataPtr<MCMuonHits> hitPointer(eventSvc(),path);
+	MCMuonHits * hitPointer = get<MCMuonHits>(path);
 	MCMuonHits::const_iterator iter;	 
 
 	// Loop over Muon Hits of given type
@@ -234,13 +140,6 @@ StatusCode MuonDigitChecker::execute() {
 	    float zpos=((*iter)->entry().z()+(*iter)->exit().z())/2.0;
 	    float time=(*iter)->timeOfFlight();
 
-	    //New monitored quantities. Not used for now
-      //	    float plen=(*iter)->pathLength();
-      //	    float hene=(*iter)->energy();
-      //	    float hdis=(*iter)->displacement().x();
-      //	    float hdxz=(*iter)->dxdz();
-      //	    float hdyz=(*iter)->dydz();
-
 	    double tof=time-sqrt(xpos*xpos+ypos*ypos+zpos*zpos)/300.0;
 	    if(tof<0.1)tof=0.1;
 	    float r=sqrt(xpos*xpos+ypos*ypos);
@@ -249,16 +148,22 @@ StatusCode MuonDigitChecker::execute() {
 	    m_reg.push_back(region);
 	    m_con.push_back(container);
 
+	    //Temporary monitoring (need to check if already available)
+	    m_ple.push_back((*iter)->pathLength());
+	    m_hen.push_back((*iter)->energy());
+	    m_dix.push_back((*iter)->displacement().x());
+	    m_dxz.push_back((*iter)->dxdz());
+	    m_dyz.push_back((*iter)->dydz());
+	    
 	    m_x.push_back(xpos); m_y.push_back(ypos); m_z.push_back(zpos);
 	    m_time.push_back(time);
 
 	    //Fill some histos	  
 	    int hh =station*4+region;
 
-	    if (fullDetail() == true) {
-	      plot( r, hh+2000, "Radial Multiplicity", 0., 6000.,200 );
-	      plot(tof,hh+1000, "Time multiplicity",   0.,  100.,200 ); 
-	    }
+	    plot( r, hh+2000, "Radial Multiplicity", 0., 6000.,200 );
+	    plot(tof,hh+1000, "Time multiplicity",   0.,  100.,200 ); 
+
 	    //MC truth
 	    MCParticle* particle=(*iter)->mcParticle();
 	    if(particle){            
@@ -306,10 +211,8 @@ StatusCode MuonDigitChecker::execute() {
   }    
   
   // Loop on Digits (strips)
-  SmartDataPtr<MuonDigits> digit(eventSvc(),
-				 MuonDigitLocation::MuonDigit);
-  SmartDataPtr<MCMuonDigits> mcdigit(eventSvc(),
-				     MCMuonDigitLocation::MCMuonDigit);
+  MuonDigits* digit = get<MuonDigits>(MuonDigitLocation::MuonDigit);
+  MCMuonDigits* mcdigit = get<MCMuonDigits>(MCMuonDigitLocation::MCMuonDigit);
   
   MuonDigits::const_iterator jdigit;
   int Dsta, Dreg, Dcon;
@@ -325,9 +228,8 @@ StatusCode MuonDigitChecker::execute() {
     m_digit_r.push_back(Dreg);
     m_digit_time.push_back((*jdigit)->TimeStamp());
 
-    // attenzione !
-    // TimeStamp e' uno degli 8 intervalli (8 digits) in cui
-    // e' divisa la finestra di accettanza di 20 ns dopo il beam crossing
+    // Timestamp is one of the 8 intervals (8 digits) in which
+    // the 20ns acceptance window is subdivided after beam crossing
     
     m_MuonTileXYZ->calcTilePos((*jdigit)->key(),x,dx,y,dy,z,dz);
     m_digit_x.push_back(x);
@@ -338,8 +240,6 @@ StatusCode MuonDigitChecker::execute() {
     m_digit_dy.push_back(dy);
     m_digit_dz.push_back(dz);
     
-    debug()<<" Digits" << x<<y<<z <<endmsg;
-
     //Match with "true" MC digit    
     ContainedObject* MCd=mcdigit->containedObject((*jdigit)->key());  
     MCMuonDigit* MCmd=dynamic_cast<MCMuonDigit *> (MCd);       
@@ -376,12 +276,56 @@ StatusCode MuonDigitChecker::execute() {
     }    
   }    
   
-  // book vectors of histos
-  StatusCode sc;
-  if(m_detailedMonitor && (fullDetail() == true) ) sc = crNtuples();
-   
+  if(m_hitMonitor) {
+    Tuple nt1 = nTuple(41,"MC HITS",CLID_ColumnWiseTuple);
+    
+    nt1->column("Run", m_run,0,1000000);
+    nt1->column("Event",m_evt,0,10000);
+    
+    nt1->farray("is", m_sta ,"Nhits",1000);
+    nt1->farray("ir", m_reg ,"Nhits",1000);
+    nt1->farray("ic", m_con ,"Nhits",1000);
+    nt1->farray("x",  m_x   ,"Nhits",1000);
+    nt1->farray("y",  m_y   ,"Nhits",1000);
+    nt1->farray("z",  m_z   ,"Nhits",1000);
+    nt1->farray("t",  m_time,"Nhits",1000);
+    nt1->farray("id", m_id  ,"Nhits",1000);
+    nt1->farray("px", m_px  ,"Nhits",1000);
+    nt1->farray("py", m_py  ,"Nhits",1000);
+    nt1->farray("pz", m_pz  ,"Nhits",1000);
+    nt1->farray("E",  m_E   ,"Nhits",1000);
+    nt1->farray("xv", m_xv  ,"Nhits",1000);
+    nt1->farray("yv", m_yv  ,"Nhits",1000);
+    nt1->farray("zv", m_zv  ,"Nhits",1000);
+    nt1->farray("tv", m_tv  ,"Nhits",1000);
+    nt1->farray("idm",m_mom ,"Nhits",1000);
+    nt1->farray("pl", m_ple ,"Nhits",1000);
+    nt1->farray("he", m_hen ,"Nhits",1000);
+    nt1->farray("dx", m_dix ,"Nhits",1000);
+    nt1->farray("xz", m_dxz ,"Nhits",1000);
+    nt1->farray("yz", m_dyz ,"Nhits",1000);
+    nt1->write();  
+  }
+  Tuple nt2 = nTuple(42,"DIGITS",CLID_ColumnWiseTuple);
+    
+  nt2->column("Run"  , m_digit_run, 0,1000000);
+  nt2->column("Event", m_digit_evt, 0,10000);
+  nt2->farray("is",    m_digit_s,      "Ndigits",1000);
+  nt2->farray("ir",    m_digit_r,      "Ndigits",1000);
+  nt2->farray("x" ,    m_digit_x,      "Ndigits",1000);
+  nt2->farray("y" ,    m_digit_y,      "Ndigits",1000);
+  nt2->farray("z" ,    m_digit_z,      "Ndigits",1000);
+  nt2->farray("dx",    m_digit_dx,     "Ndigits",1000);
+  nt2->farray("dy",    m_digit_dy,     "Ndigits",1000);
+  nt2->farray("dz",    m_digit_dz,     "Ndigits",1000);
+  nt2->farray("t" ,    m_digit_time,   "Ndigits",1000);
+  nt2->farray("origin",m_digit_origin, "Ndigits",1000);
+  nt2->farray("bx"    ,m_digit_bx,     "Ndigits",1000);
+  nt2->farray("firing",m_digit_firing, "Ndigits",1000);
+  nt2->farray("multip",m_digit_multi,  "Ndigits",1000);
+  nt2->write();  
 
- return StatusCode::SUCCESS;
+  return StatusCode::SUCCESS;
 };
 
 //=============================================================================
@@ -393,14 +337,13 @@ StatusCode MuonDigitChecker::finalize() {
   info() << "       Muon Monitoring Table " << endmsg;
   info() << "-----------------------------------------------------------------"
 	 << endmsg;
-  if(m_detailedMonitor && (fullDetail() == true) && m_hitMonitor) {
+  if(fullDetail() == true && m_hitMonitor) {
     info() << "       Hit Information     " << endmsg;
     info()<<" M1      M2      M3      M4      M5 "<<endmsg;
     for(int r=0; r<4; r++) {
       for(int s=0; s<5; s++) {
 	if(cnt[s][r])  {
 	  info()<<format("%5.3lf  ",(double)nhit[s][r]/cnt[s][r]);
-	  //	info()<<(double)nhit[s][r]/cnt[s][r]<<"     ";
 	} else {
 	  info()<<"0.000  ";
 	}
@@ -412,6 +355,11 @@ StatusCode MuonDigitChecker::finalize() {
 
   info() << "    Digi Information     " << endmsg;
   info() << "-----------------------------------------------------------------" << endmsg;
+
+  std::string HitOr[6]=
+  {"Geant","FlastSpillOver","Low Energy Background","Chamber Noise",
+   "Crosstalk","Electronic Noise"};      
+
 
   for(int c=0; c<6; c++) {
     std::string myCon = HitOr[c];
@@ -432,7 +380,6 @@ StatusCode MuonDigitChecker::finalize() {
   }
   
 
-  if( m_MuonTileXYZ ) toolSvc()->releaseTool( m_MuonTileXYZ );
   return StatusCode::SUCCESS;
 }
 
--- Boole/Muon/MuonMoniDigi/src/MuonDigitChecker.h
+++ Boole/Muon/MuonMoniDigi/src/MuonDigitChecker.h
@@ -1,4 +1,4 @@
-// $Id: MuonDigitChecker.h,v 1.3 2005-06-15 13:05:44 asarti Exp $
+// $Id: MuonDigitChecker.h,v 1.4 2005-09-30 15:23:13 asarti Exp $
 #ifndef MuonDigitChecker_H 
 #define MuonDigitChecker_H 1
 
@@ -11,10 +11,14 @@
 #include "GaudiAlg/GaudiHistoAlg.h"
 
 // for Muons
+#include "MuonDet/MuonBasicGeometry.h"
 #include "MuonTools/IMuonTileXYZTool.h"
-#include "MuonTools/MuonChannel.h"
 #include "MuonTools/IMuonGetInfoTool.h"  
 #include "MuonKernel/MuonTile.h"
+
+//From event
+#include "Event/EventHeader.h"
+#include "Event/MCHit.h"   
 #include "Event/MCMuonHit.h"   
 #include "Event/MCMuonDigit.h"   
 #include "Event/MuonDigit.h"  
@@ -31,77 +35,22 @@ public:
   MuonDigitChecker( const std::string& name, ISvcLocator* pSvcLocator );
 
   virtual ~MuonDigitChecker( ); ///< Destructor
-  virtual StatusCode initialize();    ///< Algorithm initialization
-  virtual StatusCode execute   ();    ///< Algorithm execution
-  virtual StatusCode finalize  ();    ///< Algorithm finalization
-  StatusCode locateMuonTileFromXYZ(double x,double y,double z,int&
-	numberTileOutput, MuonTileID phChTileID[2],bool debug=false);
-/*
-*
-*/
+  virtual StatusCode initialize();    /// initialization
+  virtual StatusCode execute   ();    /// execution
+  virtual StatusCode finalize  ();    /// finalization
+
 protected:
-  static std::string spill[5];
-  static std::string numreg[4];   
-  static std::string numsta[5];
-  static std::string TESPathOfHitsContainer[4];
-  static std::string HitOr[6];
-  int m_stationNumber;
-  std::vector<int> m_numberOfGaps;
+
   int m_regionNumber;
   int m_partition;
 
 private:
   IMuonTileXYZTool* m_MuonTileXYZ ;
 
-  IHistogram1D * m_pointer1Dradial[20];
-  IHistogram1D * m_pointer1Dtime[20];
-
-  virtual StatusCode crNtuples();
   int nhit[5][4],cnt[5][4];
   int nDhit[5][4][6],Dcnt[5][4][6];
 
   bool m_hitMonitor;
-  bool m_detailedMonitor;
-
-  /// N-tuple items to be written away (hit)
-  long  m_run;
-  long  m_evt;
-  long  m_nhits;
-  std::vector<float> m_sta;
-  std::vector<float> m_reg;
-  std::vector<float> m_con;
-  std::vector<float> m_x;
-  std::vector<float> m_y;
-  std::vector<float> m_z;
-  std::vector<float> m_time;
-  std::vector<float>  m_id;
-  std::vector<float> m_px;
-  std::vector<float> m_py;
-  std::vector<float> m_pz;
-  std::vector<float> m_E;
-  std::vector<float> m_xv;
-  std::vector<float> m_yv;
-  std::vector<float> m_zv;
-  std::vector<float> m_tv;
-  std::vector<float>  m_mom;
-
-  /// N-tuple items to be written away (Digit)
-  long  m_digit_run;
-  long  m_digit_evt;
-  long  n_digits;
-  std::vector<float> m_digit_s;
-  std::vector<float> m_digit_r;
-  std::vector<float> m_digit_x;
-  std::vector<float> m_digit_y;
-  std::vector<float> m_digit_z;
-  std::vector<float> m_digit_dx;
-  std::vector<float> m_digit_dy;
-  std::vector<float> m_digit_dz;
-  std::vector<float> m_digit_time;
-  std::vector<float> m_digit_origin;
-  std::vector<float> m_digit_bx;
-  std::vector<float> m_digit_firing;
-  std::vector<float> m_digit_multi;
 
 };
 #endif // MuonDigitChecker_H
