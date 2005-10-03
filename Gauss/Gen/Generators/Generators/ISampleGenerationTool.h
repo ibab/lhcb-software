3975affde705f9cdda5f3ceed1ff581e0213dd66
Patrick Robbe <robbe@lal.in2p3.fr>
2005-10-03 09:52:12 +0000

    New abstract interface to tools used to generate the different samples
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91843 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3b8f462fe4e19c9095d82a646e874d00a5a6ad6d

---
+++ Gauss/Gen/Generators/Generators/ISampleGenerationTool.h
@@ -0,0 +1,37 @@
+// $Id: ISampleGenerationTool.h,v 1.1 2005-10-03 09:52:12 robbep Exp $
+#ifndef GENERATORS_ISAMPLEGENERATIONTOOL_H 
+#define GENERATORS_ISAMPLEGENERATIONTOOL_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+class HepMCEvent ;
+class HardInfo ;
+
+/** @class ISampleGenerationTool ISampleGenerationTool.h 
+ *         Generators/ISampleGenerationTool.h
+ *  
+ *  Abstract interface to generation of event samples
+ * 
+ *  @author Patrick Robbe
+ *  @date   2005-08-17
+ */
+
+static const InterfaceID IID_ISampleGenerationTool( "ISampleGenerationTool" , 
+                                                    1 , 0 ) ;
+
+class ISampleGenerationTool : virtual public IAlgTool {
+public:
+  typedef std::vector< HepMCEvent * > EventVector ;
+  typedef std::vector< HardInfo * >   HardVector  ;
+
+  static const InterfaceID& interfaceID() { return IID_ISampleGenerationTool; }
+
+  virtual bool generate( const unsigned int nPileUp , 
+                         EventVector & theEventVector ,
+                         HardVector  & theHardVector ) = 0 ;
+
+  virtual void printCounters( ) const = 0 ;
+};
+#endif // GENERATORS_ISAMPLEGENERATIONTOOL_H
