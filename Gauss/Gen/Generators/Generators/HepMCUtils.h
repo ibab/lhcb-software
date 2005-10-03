1deb507e633211972687595ea673663c8f59e0ef
Patrick Robbe <robbe@lal.in2p3.fr>
2005-10-03 09:39:18 +0000

    Use a static class
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91836 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8df357def01d76abe590e57444893fd6536cab1a

--- Gauss/Gen/Generators/Generators/HepMCUtils.h
+++ Gauss/Gen/Generators/Generators/HepMCUtils.h
@@ -1,32 +1,35 @@
-// $Id: HepMCUtils.h,v 1.1.1.1 2005-06-20 21:42:17 robbep Exp $
+// $Id: HepMCUtils.h,v 1.2 2005-10-03 09:39:18 robbep Exp $
 #ifndef GENERATORS_HEPMCUTILS_H 
 #define GENERATORS_HEPMCUTILS_H 1
 
 #include <vector>
 #include "HepMC/GenEvent.h"
 
-namespace HepMCUtils {
-  typedef std::vector< HepMC::GenParticle * > GenParticleVector ;
+class HepMCUtils {
+ public:
+  typedef std::vector< HepMC::GenParticle * > ParticleVector ;
   
-  bool Compare( const HepMC::GenParticle * thePart1 , 
-                const HepMC::GenParticle * thePart2 ) ;
+  static bool Compare( const HepMC::GenParticle * thePart1 , 
+                       const HepMC::GenParticle * thePart2 ) ;
   
   template<class ITTYPE>
-  void SortHepMCVector( GenParticleVector & theHepMCVector , 
-                        unsigned int size , const ITTYPE &pBegin , 
-                        const ITTYPE &pEnd ) ;
-}  
+  static void SortHepMC( ParticleVector & theHepMCVector , 
+                         const ParticleVector::size_type vectorSize ,
+                         const ITTYPE &pBegin, const ITTYPE &pEnd ) ; 
+
+  static bool IsBAtProduction( const HepMC::GenParticle * thePart ) ;
+};
 
 // Implementation of templated function
 template <class ITTYPE>
-void HepMCUtils::SortHepMCVector( GenParticleVector &theHepMCVector ,
-                                  unsigned int size , const ITTYPE &pBegin , 
-                                  const ITTYPE &pEnd ) {
-  theHepMCVector.reserve( size ) ;
+inline void HepMCUtils::SortHepMC( ParticleVector &theHepMCVector ,
+                                   const ParticleVector::size_type vectorSize ,
+                                   const ITTYPE &pBegin , 
+                                   const ITTYPE &pEnd ) {
+  theHepMCVector.reserve( vectorSize ) ;
   ITTYPE pit ;
-  for ( pit = pBegin ; pit != pEnd ; ++pit ) { 
+  for ( pit = pBegin ; pit != pEnd ; ++pit ) 
     theHepMCVector.push_back( (*pit) ) ; 
-  }
   // Sort the temporary vector to be able to reproduce events
   std::sort ( theHepMCVector.begin() , theHepMCVector.end() ,
               HepMCUtils::Compare ) ;
--- Gauss/Gen/Generators/src/Lib/HepMCUtils.cpp
+++ Gauss/Gen/Generators/src/Lib/HepMCUtils.cpp
@@ -1,11 +1,23 @@
-// $Id: HepMCUtils.cpp,v 1.1.1.1 2005-06-20 21:42:17 robbep Exp $
+// $Id: HepMCUtils.cpp,v 1.2 2005-10-03 09:39:18 robbep Exp $
 // Include files
 
 // local
 #include "Generators/HepMCUtils.h"
 
 bool HepMCUtils::Compare( const HepMC::GenParticle * thePart1 , 
-              const HepMC::GenParticle * thePart2 ) { 
+                          const HepMC::GenParticle * thePart2 ) { 
   return thePart1->barcode() < thePart2->barcode() ; 
+}
+
+bool HepMCUtils::IsBAtProduction( const HepMC::GenParticle * thePart ) {
+  if ( ( abs( thePart -> pdg_id() ) != 511 ) && 
+       ( abs( thePart -> pdg_id() ) != 531 ) ) return true ;
+  if ( 0 == thePart -> production_vertex() ) return true ;
+  HepMC::GenVertex * theVertex = thePart -> production_vertex() ;
+  if ( 1 != theVertex -> particles_in_size() ) return true ;
+  HepMC::GenParticle * theMother = 
+    (* theVertex -> particles_in_const_begin() ) ;
+  if ( theMother -> pdg_id() == - thePart -> pdg_id() ) return false ;
+  return true ;
 }
 
