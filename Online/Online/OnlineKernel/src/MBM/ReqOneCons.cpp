3a1f691906c737ef8abe20c1798426288eec1c84
Markus Frank <frankb@cern.ch>
2005-10-07 18:32:55 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@95957 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cd24af01c8d4d1055a225430fc412f448227f624

---
+++ Online/Online/OnlineKernel/src/MBM/ReqOneCons.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include "RTL/rtl.h"
+#include "MBM/Consumer.h"
+
+static void help()  {
+  ::printf("mbm_cons_one -opt [-opt]\n");
+  ::printf("    -n=<name>      buffer member name\n");
+  ::printf("    -s=<number>    sleep interval between events [milli seconds]\n");
+}
+
+extern "C" int mbm_cons_one(int argc,char **argv) {
+  int  trnumber  = -1, nbad = 0, sleep_msecs = 0;
+  int  trmask[4] = {-1,-1,-1,-1};
+  int  vetomask[4] = {0,0,0,0};
+
+  RTL::CLI cli(argc, argv, help);
+  std::string name = "cons_one";
+  cli.getopt("name", 1,name);
+  cli.getopt("sleep",1,sleep_msecs);
+
+  MBM::Consumer c("0",name,0x103);
+  ::printf("Consumer \"%s\" (pid:%d) including buffer:\"%s\"\n",name.c_str(),c.pid(),"0");
+  c.addRequest(1,trmask,vetomask,BM_MASK_ANY,BM_REQ_ONE,BM_FREQ_PERC,100.);
+  while(1)  {
+    c.getEvent();
+    const MBM::EventDesc& e = c.event();
+    if ( -1 == trnumber )  {
+      trnumber = *e.data;
+    }
+    else if( trnumber != *e.data ) {
+      //::printf("======= Mismatch [%d] found %d %d [0x%p]\n", nbad++, trnumber, *e.data, e.data);
+      trnumber = *e.data;
+    }
+    trnumber++;
+    if ( sleep_msecs ) lib_rtl_sleep(sleep_msecs);
+    c.freeEvent();
+  }
+}
