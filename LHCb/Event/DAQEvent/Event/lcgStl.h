df0b5fb1f20f6064d1f326a8a849210d9bce8463
Marco Cattaneo <marco.cattaneo@cern.ch>
2005-10-10 16:02:04 +0000

    fixes for lcgdict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34300 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e639ef129c8963d6b2067c6cd0d68d785748b330

--- LHCb/Event/DAQEvent/Event/lcgStl.h
+++ LHCb/Event/DAQEvent/Event/lcgStl.h
@@ -1,25 +1,31 @@
-// $Id: lcgStl.h,v 1.2 2005-02-03 10:21:32 cattanem Exp $
+// $Id: lcgStl.h,v 1.3 2005-10-10 16:02:04 cattanem Exp $
 #ifndef DAQEVENT_LCGSTL_H 
 #define DAQEVENT_LCGSTL_H 1
 
-// Dummy use of complex STL classes used in LHCb event model
-// See also similar file in LHCbKernel
-// For lcg dictionary generation
+// Additional classes to be added to automatically generated lcgdict
 
-// Include files
+// Reflex include files
+#include "Reflex/Builder/ReflexBuilder.h"
+
+// begin include files
 #include <vector>
 #include <map>
 #include "Event/L1Bank.h"
 #include "Event/RawBank.h"
 #include "Event/L1Buffer.h"
 #include "Event/RawBuffer.h"
+// end include files
 
 namespace {
-  std::map<int,std::vector<L1Bank> >  a;
-  std::map<int,std::vector<RawBank> > b;
-  std::vector<L1Bank>                 c;
-  std::vector<RawBank>                d;
-  BufferSerializer<RawBuffer>         e;
-  BufferSerializer<L1Buffer>          f;
+  struct _Instantiations {
+    // begin instantiations
+    std::map<int,std::vector<L1Bank> >  m_std_map_int_std_vector_L1Bank;
+    std::map<int,std::vector<RawBank> > m_std_map_int_std_vector_RawBank;
+    std::vector<L1Bank>                 m_std_vector_L1Bank;
+    std::vector<RawBank>                m_std_vector_RawBank;
+    BufferSerializer<RawBuffer>         m_BufferSerializer_RawBuffer;
+    BufferSerializer<L1Buffer>          m_BufferSerializer_L1Buffer;    
+    // end instantiations
+  };
 }
 #endif // DAQEVENT_LCGSTL_H
