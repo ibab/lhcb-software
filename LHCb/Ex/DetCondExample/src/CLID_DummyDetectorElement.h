90e25bfa5087373c6714a6eaa83fadb097d8d8ea
Marco Clemencic <marcocle@cern.ch>
2005-10-18 16:28:05 +0000

    changed Class Id because of a conflict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3617 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 59f4ad6a21f431a59731f8a065a3c7f9792f0c92

--- LHCb/Ex/DetCondExample/src/CLID_DummyDetectorElement.h
+++ LHCb/Ex/DetCondExample/src/CLID_DummyDetectorElement.h
@@ -1,11 +1,11 @@
-// $Id: CLID_DummyDetectorElement.h,v 1.1 2005-04-22 15:17:54 marcocle Exp $
+// $Id: CLID_DummyDetectorElement.h,v 1.2 2005-10-18 16:28:05 marcocle Exp $
 #ifndef     _CLID_DUMMYDETECTORELEMENT_H
 #define     _CLID_DUMMYDETECTORELEMENT_H 1
 
 
 ///
 /// unique ID of the class
-static const CLID CLID_DummyDetectorElement = 9999;
+static const CLID CLID_DummyDetectorElement = 6669999;
 
 
 #endif //   _CLID_DUMMYDETECTORELEMENT_H
