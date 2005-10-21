947bffd3e0967a30199df1442891a773be8fc182
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-10-21 15:04:49 +0000

    add new monitor
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@7469 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c8b68d26b1a32e2adf399894a4525eda051784e7

---
+++ Rec/Rich/RichRecMonitors/src/RichCherenkovResMoni.cpp
@@ -0,0 +1,162 @@
+
+//---------------------------------------------------------------------------
+/** @file RichCherenkovResMoni.cpp
+ *
+ *  Implementation file for algorithm class : RichCherenkovResMoni
+ *
+ *  CVS Log :-
+ *  $Id: RichCherenkovResMoni.cpp,v 1.1 2005-10-21 15:04:49 jonrob Exp $
+ *
+ *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
+ *  @date   05/04/2002
+ */
+//---------------------------------------------------------------------------
+
+// local
+#include "RichCherenkovResMoni.h"
+
+//---------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+static const  AlgFactory<RichCherenkovResMoni>          s_factory ;
+const        IAlgFactory& RichCherenkovResMoniFactory = s_factory ;
+
+// Standard constructor, initializes variables
+RichCherenkovResMoni::RichCherenkovResMoni( const std::string& name,
+                                            ISvcLocator* pSvcLocator )
+  : RichRecMoniAlgBase ( name, pSvcLocator ),
+    m_richRecMCTruth   ( 0 ),
+    m_ckAngle          ( 0 ),
+    m_ckAngleRes       ( 0 )
+{
+
+  // track selector
+  declareProperty( "TrackSelection", m_trSelector.selectedTrackTypes() );
+
+}
+
+// Destructor
+RichCherenkovResMoni::~RichCherenkovResMoni() {};
+
+//  Initialize
+StatusCode RichCherenkovResMoni::initialize()
+{
+  // Sets up various tools and services
+  const StatusCode sc = RichRecMoniAlgBase::initialize();
+  if ( sc.isFailure() ) { return sc; }
+
+  // Acquire instances of tools
+  acquireTool( "RichRecMCTruthTool",   m_richRecMCTruth );
+  acquireTool( "RichCherenkovAngle",      m_ckAngle     );
+  acquireTool( "RichCherenkovResolution", m_ckAngleRes  );
+
+  // Configure track selector
+  if ( !m_trSelector.configureTrackTypes() ) return StatusCode::FAILURE;
+  m_trSelector.printTrackSelection( info() );
+
+  return sc;
+}
+
+// Main execution
+StatusCode RichCherenkovResMoni::execute()
+{
+
+  // Check event status
+  if ( !richStatus()->eventOK() ) return StatusCode::SUCCESS;
+
+  // Iterate over segments
+  for ( RichRecSegments::const_iterator iSeg = richSegments()->begin();
+        iSeg != richSegments()->end(); ++iSeg )
+  {
+    RichRecSegment * segment = *iSeg;
+
+    // apply track selection
+    if ( !m_trSelector.trackSelected(segment->richRecTrack()) ) continue;
+
+    // MC type
+    const Rich::ParticleIDType mcType = m_richRecMCTruth->mcParticleType( segment );
+    if ( mcType == Rich::Unknown ) continue;
+
+    // track segment
+    const RichTrackSegment & trackSeg = segment->trackSegment();
+    // track type
+    const Rich::Track::Type tkType = segment->richRecTrack()->trackID().trackType();
+
+    // radiator
+    const Rich::RadiatorType rad = trackSeg.radiator();
+
+    // Segment momentum
+    const double ptot = segment->trackSegment().bestMomentum().mag() / GeV;
+
+    // CK resolution and angle for true type
+    const double trueCKres = m_ckAngleRes->ckThetaResolution(segment,mcType);
+    const double trueCKang = m_ckAngle->avgCherenkovTheta(segment,mcType);
+
+    // histo range
+    const double ckMax[]    = { 0.3, 0.06, 0.04 };
+    const double ckResMax[] = { 0.011, 0.011, 0.003 };
+    // name tag
+    const std::string nameTag = Rich::text(rad)+" "+Rich::text(tkType);
+
+    // Loop over all particle codes
+    for ( int iHypo = 0; iHypo < Rich::NParticleTypes; ++iHypo )
+    {
+      const Rich::ParticleIDType hypo = static_cast<Rich::ParticleIDType>(iHypo);
+
+      // CK resolution
+      const double ckres = m_ckAngleRes->ckThetaResolution(segment,hypo);
+      // CK angle
+      const double ckang = m_ckAngle->avgCherenkovTheta(segment,hypo);
+
+      if ( ckang > 0 )
+      {
+        // histograms
+        plot1D( ckres, "Calculated CKres "+nameTag, 0, ckResMax[rad] );
+        plot2D( ckang, ckres, "Calculated CKres V CKangle "+nameTag, 0, ckMax[rad], 0, ckResMax[rad] );
+        plot2D( ptot, ckres, "Calculated CKres V ptot "+nameTag, 0, 100, 0, ckResMax[rad] );
+        //profile1D( ckang, ckres, "Calculated CKres V CKangle "+nameTag, 0, ckMax[rad] );
+        //profile1D( ptot, ckres, "Calculated CKres V ptot "+nameTag, 0, 100 );
+        //profile2D( ptot, ckang, ckres, "Calculated CKres V ptot+ckang "+nameTag, 0, 100, 0, ckMax[rad] );
+      }
+
+    } // particle ID codes
+
+    // loop over photons
+    const RichRecSegment::Photons & photons = photonCreator()->reconstructPhotons( segment );
+    for ( RichRecSegment::Photons::const_iterator iPhot = photons.begin();
+          iPhot != photons.end();
+          ++iPhot )
+    {
+      RichRecPhoton * photon = *iPhot;
+
+      // MC photon
+      const MCRichOpticalPhoton * mcPhot = m_richRecMCTruth->trueOpticalPhoton(photon);
+      if ( mcPhot )
+      {
+        
+        // Cherenkov angles
+        const double thetaRec = photon->geomPhoton().CherenkovTheta();
+        const double thetaMC  = mcPhot->cherenkovTheta();
+        const double delCK    = thetaRec-thetaMC;
+
+        plot2D( delCK, trueCKres, "True V calculated CKres "+nameTag, 0, ckResMax[rad], 0, ckResMax[rad] );
+        plot2D( trueCKang, delCK, "True CKres V true CK angle "+nameTag, 0, ckMax[rad], 0, ckResMax[rad] );
+        //profile1D( delCK, trueCKres, "True V calculated CKres "+nameTag, 0, ckResMax[rad] );
+        //profile1D( trueCKang, delCK, "True CKres V true CK angle "+nameTag, 0, ckMax[rad] );
+
+      }
+
+    }
+
+  }
+
+
+  return StatusCode::SUCCESS;
+}
+
+//  Finalize
+StatusCode RichCherenkovResMoni::finalize()
+{
+  // Execute base class method
+  return RichRecMoniAlgBase::finalize();
+}
---
+++ Rec/Rich/RichRecMonitors/src/RichCherenkovResMoni.h
@@ -0,0 +1,72 @@
+
+//---------------------------------------------------------------------------
+/** @file RichCherenkovResMoni.h
+ *
+ *  Header file for algorithm class : RichCherenkovResMoni
+ *
+ *  CVS Log :-
+ *  $Id: RichCherenkovResMoni.h,v 1.1 2005-10-21 15:04:49 jonrob Exp $
+ *
+ *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
+ *  @date   05/04/2002
+ */
+//---------------------------------------------------------------------------
+
+#ifndef RICHRECMONITOR_RICHTRACKRESOLUTIONMONI_H
+#define RICHRECMONITOR_RICHTRACKRESOLUTIONMONI_H 1
+
+// base class
+#include "RichRecBase/RichRecMoniAlgBase.h"
+#include "RichRecBase/RichTrackSelector.h"
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// Event
+#include "Event/MCRichOpticalPhoton.h"
+
+// Interfaces
+#include "RichRecBase/IRichRecMCTruthTool.h"
+#include "RichRecBase/IRichCherenkovAngle.h"
+#include "RichRecBase/IRichCherenkovResolution.h"
+
+//---------------------------------------------------------------------------
+/** @class RichCherenkovResMoni RichCherenkovResMoni.h
+ *
+ *  Monitors the resolution of the tracking information, in terms important
+ *  for the RICH reconstruction.
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   05/04/2002
+ */
+//---------------------------------------------------------------------------
+
+class RichCherenkovResMoni : public RichRecMoniAlgBase {
+
+public:
+
+  /// Standard constructor
+  RichCherenkovResMoni( const std::string& name,
+                        ISvcLocator* pSvcLocator );
+
+  virtual ~RichCherenkovResMoni( ); ///< Destructor
+
+  virtual StatusCode initialize();    // Algorithm initialization
+  virtual StatusCode execute   ();    // Algorithm execution
+  virtual StatusCode finalize  ();    // Algorithm finalization
+
+private: // methods
+
+
+private: // data
+
+  const IRichRecMCTruthTool* m_richRecMCTruth;   ///< Pointer to RichRecMCTruthTool interface
+  const IRichCherenkovAngle * m_ckAngle;         ///< Rich Cherenkov angle calculator tool
+  const IRichCherenkovResolution * m_ckAngleRes; ///< Cherenkov angle resolution tool
+
+  /// Track selector
+  RichTrackSelector m_trSelector;
+
+};
+
+#endif // RICHRECMONITOR_RICHTRACKRESOLUTIONMONI_H
