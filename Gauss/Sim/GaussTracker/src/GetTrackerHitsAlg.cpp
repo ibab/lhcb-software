e71d0834cfd1cadd52a1d5e1bee9e130957ceb7b
Gloria Corti <gloria.corti@cern.ch>
2005-10-25 18:53:51 +0000

    new algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91904 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4a8d8c349d0d3f0359771ad2fdaae5c30ecf51f

---
+++ Gauss/Sim/GaussTracker/src/GetTrackerHitsAlg.cpp
@@ -0,0 +1,166 @@
+// $Id: GetTrackerHitsAlg.cpp,v 1.1 2005-10-25 18:52:52 gcorti Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+// from GiGa 
+#include "GiGa/IGiGaSvc.h"
+#include "GiGa/GiGaHitsByName.h"
+
+// from GiGaCnv
+#include "GiGaCnv/IGiGaKineCnvSvc.h" 
+#include "GiGaCnv/IGiGaCnvSvcLocation.h"
+#include "GiGaCnv/GiGaKineRefTable.h"
+
+// from Geant4
+#include "G4HCofThisEvent.hh"
+
+// from LHCb
+#include "Event/MCHit.h"
+
+// local
+#include "GetTrackerHitsAlg.h"
+#include "TrackerHit.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : GetTrackerHitsAlg
+//
+// 2005-10-02 : Gloria CORTI
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+static const  AlgFactory<GetTrackerHitsAlg>          s_factory ;
+const        IAlgFactory& GetTrackerHitsAlgFactory = s_factory ; 
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+GetTrackerHitsAlg::GetTrackerHitsAlg( const std::string& name,
+                                                ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator )
+  , m_gigaSvc      ( 0 )
+  , m_gigaKineCnvSvc ( 0 )
+{
+  declareProperty( "GiGaService",    m_gigaSvcName    = "GiGa" );
+  declareProperty( "KineCnvService", m_kineSvcName    = IGiGaCnvSvcLocation::Kine );
+  declareProperty( "MCHitsLocation", m_hitsLocation   = "/Event/MC/BCM/Hits" );
+  declareProperty( "CollectionName", m_colName        = "BCMSDet/Hits" );
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+GetTrackerHitsAlg::~GetTrackerHitsAlg() {}; 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode GetTrackerHitsAlg::initialize() {
+
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  debug() << "==> Initialize" << endmsg;
+
+  m_gigaSvc = svc<IGiGaSvc>( m_gigaSvcName ); // GiGa has to already exist!
+
+  // get kineCnv service that hold the MCParticle/Geant4 table list
+  m_gigaKineCnvSvc = svc<IGiGaKineCnvSvc>( m_kineSvcName );
+
+  return StatusCode::SUCCESS;
+};
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode GetTrackerHitsAlg::execute() {
+
+  debug() << "==> Execute" << endmsg;
+
+  if( 0 == gigaSvc() ) {
+    return Error( " execute(): IGiGaSvc* points to NULL" );
+  }
+
+  // Create the MCHits and put them in the TES
+  MCHits* hits = getOrCreate<MCHits,MCHits>( m_hitsLocation );
+
+  // Get the G4 necessary hit collection from GiGa
+  G4HCofThisEvent* hitsCollections = 0;
+  GiGaHitsByName col( m_colName );
+  *gigaSvc() >> col;   // also StatusCode sc = retrieveHitCollection( col );
+                       // in TRY/CATCH&PRINT
+  
+  if( 0 == col.hits() ) { 
+    return Warning( "The hit collection='" + m_colName + "' is not found!",
+                    StatusCode::SUCCESS ); 
+  }
+  
+  const TrackerHitsCollection* hitCollection = trackerHits( col.hits() );
+  if( 0 == hitCollection ) { 
+    return Error( "Wrong Collection type" );
+  }
+  
+  // The MCParticles should have already been filled
+  if( !( exist<MCParticles>( MCParticleLocation::Default ) ) ) {
+    return Error( "MCParticles do not exist at'" 
+                  + MCParticleLocation::Default +"'" );
+  }
+    
+  MCParticles* parts = get<MCParticles>( MCParticleLocation::Default );
+
+  // Get the Geant4->MCParticle table
+  GiGaKineRefTable& table = kineSvc()->table();
+  
+  // reserve elements on output container
+  int numOfHits = hitCollection->entries();
+  hits->reserve( numOfHits );
+  
+  // tranform G4Hit into MCHit
+  for( int iHit = 0; iHit < numOfHits; ++iHit ) { 
+
+    // create hit
+    MCHit* mcHit = new MCHit();
+
+    // fill data members
+    HepPoint3D entry = (*hitCollection)[iHit]->GetEntryPos();
+    HepPoint3D exit  = (*hitCollection)[iHit]->GetExitPos();
+    mcHit->setEntry( entry );
+    mcHit->setDisplacement( exit-entry );
+    mcHit->setEnergy( (*hitCollection)[iHit]->GetEdep() );
+    mcHit->setTimeOfFlight( (*hitCollection)[iHit]->GetTimeOfFlight() );
+
+    // fill reference to MCParticle   
+    int trackID = (*hitCollection)[iHit]->GetTrackID();
+    if( table[trackID].particle() ) {
+      mcHit->setMCParticle( table[trackID].particle() );
+    } else {
+      warning() << "No pointer to MCParticle for MCHit associated to G4 trackID: "
+                << trackID << endmsg;
+    }
+
+    // insert it in container
+    hits->insert( mcHit );
+  }
+  
+  // Check that all hits have been transformed
+  if( (size_t) hits->size() != (size_t) hitCollection->entries() ) {
+    return Error("MCHits and G4TrackerHitsCollection have different sizes!");
+  }  
+
+  return StatusCode::SUCCESS;
+  
+};
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode GetTrackerHitsAlg::finalize() {
+
+  debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+
---
+++ Gauss/Sim/GaussTracker/src/GetTrackerHitsAlg.h
@@ -0,0 +1,57 @@
+// $Id: GetTrackerHitsAlg.h,v 1.1 2005-10-25 18:52:50 gcorti Exp $
+#ifndef GETTRACKERHITSALG_H 
+#define GETTRACKERHITSALG_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+// Forward declarations
+class IGiGaSvc;
+class IGiGaKineCnvSvc;
+
+/** @class GetTrackerHitsAlg GetTrackerHitsAlg.h
+ *  
+ *
+ *  @author Gloria CORTI
+ *  @date   2005-10-02
+ */
+class GetTrackerHitsAlg : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  GetTrackerHitsAlg( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~GetTrackerHitsAlg( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+  /** accessor to GiGa Service
+   *  @return pointer to GiGa Service
+   */
+  IGiGaSvc* gigaSvc() const { 
+    return m_gigaSvc; 
+  }
+
+  /** accessor to kinematics  conversion service 
+   *  @return pointer to kinematics conversion service 
+   */
+  IGiGaKineCnvSvc*      kineSvc   () const { 
+    return m_gigaKineCnvSvc;
+  } 
+
+private:
+
+  std::string        m_gigaSvcName;      ///< Name of GiGa Service
+  std::string        m_kineSvcName;      ///< Name of GiGaCnv Service
+  IGiGaSvc*          m_gigaSvc;          ///< Pointer to GiGa Service
+  IGiGaKineCnvSvc*   m_gigaKineCnvSvc;   ///< Pointer to GiGaKine Service
+  
+  std::string        m_hitsLocation;     ///< Name of TES path for MCHits
+  std::string        m_colName;          ///< Name of G4 hits collection
+  
+};
+#endif // GETTRACKERHITSALG_H
