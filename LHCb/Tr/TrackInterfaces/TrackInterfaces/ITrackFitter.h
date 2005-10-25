6efede4d24db2678c0fbe56c8bfb876eb843fffc
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2005-10-25 12:59:30 +0000

    ITrackFitter ready for up/down-stream fitting
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@15704 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: edc0baa9f09286304e3954443c8e788519279f8c

--- LHCb/Tr/TrackInterfaces/TrackInterfaces/ITrackFitter.h
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/ITrackFitter.h
@@ -1,8 +1,9 @@
-// $Id: ITrackFitter.h,v 1.3 2005-06-01 12:35:16 erodrigu Exp $
+// $Id: ITrackFitter.h,v 1.4 2005-10-25 12:59:30 erodrigu Exp $
 #ifndef TRACKINTERFACES_ITRACKFITTER_H 
 #define TRACKINTERFACES_ITRACKFITTER_H 1
 
 // Include files
+// -------------
 // from STL
 #include <string>
 
@@ -17,8 +18,9 @@ static const InterfaceID IID_ITrackFitter ( "ITrackFitter", 1, 0 );
 
 /** @class ITrackFitter ITrackFitter.h TrackInterfaces/ITrackFitter.h
  *  
+ *  Interface for a track fitting tool.
  *
- *  @author Jose A. Hernando 
+ *  @author Jose A. Hernando, Eduardo Rodrigues
  *  @date   2005-05-25
  *
  *  @author Rutger van der Eijk  07-04-1999
@@ -30,17 +32,23 @@ public:
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_ITrackFitter; }
 
-  //! fit a track without a seed state
-  virtual StatusCode fit(Track& track) = 0;
+  //! fit a track upstream without a seed state (filter and smooth)
+  virtual StatusCode fitUpstream( Track& track ) = 0;
 
-  //! fit the track with a seed state
-  virtual StatusCode fit(Track& track, const State& seed) = 0;
-  
-  //! filter the track (only filter)
-  virtual StatusCode filter(Track& track, const State& seed) = 0;
-  
+  //! fit a track downstream without a seed state (filter and smooth)
+  virtual StatusCode fitDownstream( Track& track ) = 0;
+
+  //! fit the track upstream with a seed state (filter and smooth)
+  virtual StatusCode fitUpstream( Track& track, State& seed ) = 0;
+
+  //! fit the track downstream with a seed state (filter and smooth)
+  virtual StatusCode fitDownstream( Track& track, State& seed ) = 0;
+
+  //! filter the track (only filter) with a seed state
+  virtual StatusCode filter( Track& track, State& seed ) = 0;
+
   //! filter/update state with this measurement
-  virtual StatusCode filter(State& state, Measurement& meas) = 0;
+  virtual StatusCode filter( State& state, Measurement& meas ) = 0;
 
 };
 #endif // TRACKINTERFACES_ITRACKFITTER_H
