9b5c985100fd5388f0f1b4774e6fe2f7566e6821
Jose Angel Hernando Morata <jose.angel.hernando-morata@cern.ch>
2005-10-28 16:02:30 +0000

    entering intersection between 2 lines and a version of OT projector
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15707 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bc2b401ec92eda7cb86b3db124f12b77f1451a5d

---
+++ Rec/Tr/TrackProjectors/src/TrackOTRayProjector.h
@@ -0,0 +1,46 @@
+#ifndef TRACKPROJECTORS_TRACKOTRAYPROJECTOR_H 
+#define TRACKPROJECTORS_TRACKOTRAYPROJECTOR_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTool.h"
+#include "TrackProjector.h"            // Interface
+#include "RayIntersector.h"
+
+// from OTDet
+#include"OTDet/DeOTDetector.h"
+
+/** @class TrackOTRayProjector TrackOTRayProjector.h TrackProjectors/TrackOTRayProjector.h
+ *  
+ *  @author Jose Hernando
+ *  @author Eduardo Rodrigues
+ *  @date   2005-04-08
+ */
+class TrackOTRayProjector : public TrackProjector {
+public:
+  /// Project a state onto a measurement.
+  /// It returns the chi squared of the projection
+  virtual StatusCode project( const State& state,
+                              Measurement& meas );
+
+  /// initialize
+  virtual StatusCode initialize();
+
+  /// Standard constructor
+  TrackOTRayProjector( const std::string& type, 
+                    const std::string& name,
+                    const IInterface* parent);
+
+  virtual ~TrackOTRayProjector( ); ///< Destructor
+
+protected:
+
+private:
+
+  DeOTDetector* m_det;
+
+  RayIntersector m_intersector;
+
+  std::string m_otTrackerPath;   ///< Name of the OT XML geom path
+};
+#endif // TRACKPROJECTORS_TRACKOTRAYPROJECTOR_H
