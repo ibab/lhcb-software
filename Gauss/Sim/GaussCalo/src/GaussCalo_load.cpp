8aa501f4dcc1d2da441ba661ebf792b09de94b3d
Patrick Robbe <robbe@lal.in2p3.fr>
2005-11-14 14:59:37 +0000

    Use algorithm instead of converter for G4 hits
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91972 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 90000e6dd0915bc2095b9c55a15afb1cee59478a

--- Gauss/Sim/GaussCalo/src/GaussCalo_load.cpp
+++ Gauss/Sim/GaussCalo/src/GaussCalo_load.cpp
@@ -1,8 +1,11 @@
-// $Id: GaussCalo_load.cpp,v 1.5 2004-01-14 13:38:10 ranjard Exp $ 
+// $Id: GaussCalo_load.cpp,v 1.6 2005-11-14 14:59:37 robbep Exp $ 
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.5  2004/01/14 13:38:10  ranjard
+// v6r0 - fix to be used with Gaudi v14r0
+//
 // Revision 1.4  2003/07/08 19:40:57  ibelyaev
 //  Sensitive Plane Detector + improved printout
 //
@@ -26,15 +29,17 @@
 
 void GaussCalo_load() 
 { 
-  /// Sensitive detectors
+  // Sensitive detectors
   DECLARE_GiGaFactory     ( EcalSensDet          ) ;
   DECLARE_GiGaFactory     ( HcalSensDet          ) ;
   DECLARE_GiGaFactory     ( SpdPrsSensDet        ) ;
   DECLARE_GiGaFactory     ( GaussSensPlaneDet    ) ;
   
-  /// Converters 
-  DECLARE_CONVERTER       ( GiGaCaloHitCnv       ) ;  
+  // Converters 
   DECLARE_CONVERTER       ( GaussSensPlaneHitCnv ) ;  
+  
+  // Algorithms
+  DECLARE_ALGORITHM       ( GetCaloHitsAlg       ) ;
 };
 // ============================================================================
 
