915505660ac4a072da0d08521dd73804ebeb929b
Marco Clemencic <marcocle@cern.ch>
2005-11-22 17:13:15 +0000

    Many improvements and refinements
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3661 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 921dcd25cfd270d94d699de2caec716c5ff2d7e5

--- LHCb/Ex/DetCondExample/python/copy_files_to_db.py
+++ LHCb/Ex/DetCondExample/python/copy_files_to_db.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from PyCool import cool,pool
+
+from optparse import OptionParser
 import os
 import re
 
@@ -12,10 +14,7 @@ def fix_system_ids(xml_data,path):
         s = m.start(1)+1
         e = m.end(1)-1
         p = os.path.join(path,data[s:e])
-        pnew = re.sub("/[^/.]*/\.\./","/",p) # remove relative pathes. FIX-ME it is not perfect
-        while p != pnew: # iterate
-            p = pnew
-            pnew = re.sub("/[^/.]*/\.\./","/",p) # remove relative pathes
+        p = os.path.normpath(p)
         data = data[0:s] + p + data[e:] 
         m = sysIdRE.search(data,pos+1)
     return data
@@ -40,71 +39,105 @@ def fix_env_vars(xml_data):
         m = envVarRE.search(data,pos+1)
     return data
 
-connectString = "oracle://intr1-v.cern.ch/INTR;schema=lhcb_cool;user=lhcb_cool;dbname=DDDB"
-#connectString = "sqlite://none;schema=XmlDDDB_v26r2.sqlite;user=none;password=none;dbname=DDDB"
-#connectString = "mysql://pclhcb91.cern.ch;schema=MARCOCLE;user=marcocle;dbname=DDDB"
-#connectString = "oracle://oradev10.cern.ch:10520/D10;schema=lhcb_lcg_cool_dev;user=lhcb_lcg_cool_dev;dbname=DDDB"
+#### connectStrings: ####
+# "oracle://intr1-v.cern.ch/INTR;schema=lhcb_cool;user=lhcb_cool;dbname=DDDB"
+# "sqlite://none;schema=XmlDDDB_v26r2.sqlite;user=none;password=none;dbname=DDDB"
+# "mysql://pclhcb91.cern.ch;schema=MARCOCLE;user=marcocle;dbname=DDDB"
+# "oracle://oradev10.cern.ch:10520/D10;schema=lhcb_lcg_cool_dev;user=lhcb_lcg_cool_dev;dbname=DDDB"
 
-DDDB_top = os.path.join(os.environ['XMLDDDBROOT'],'DDDB')
+################################################################################
+def main():
+    parser = OptionParser()
+    parser.add_option("-c", "--connect-string",
+                      dest="connectString", type="string",
+                      help="cool::DatabaseId to use for the connection",
+                      default="sqlite://none;schema=XmlConditions_20051118.sqlite;user=none;password=none;dbname=DDDB")
+    parser.add_option("-s", "--source",
+                      dest="source", type="string",
+                      help="directory where the files to copy to the DB are",
+                      metavar="SRCDIR")
+    parser.add_option("-d", "--dest",
+                      dest="dest", type="string",
+                      help="directory, in the database, where to copy the files (default: '/')",
+                      default="/",
+                      metavar="DESTPATH")
+    parser.add_option("-x", "--drop-db",
+                      action="store_true", dest="drop",
+                      help="drop the database before the copy (default)",
+                      default=True)
+    parser.add_option("-k", "--keep-db",
+                      action="store_false", dest="drop",
+                      help="keep the existing database and merge with the new files")
+    (options, args) = parser.parse_args()
+    if len(args) != 0 or not options.source or not options.dest:
+        parser.error("Bad arguments, use -h for help.")
 
-folderspec = cool.ExtendedAttributeListSpecification()
-#folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_255)
-#folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_4K)
-#folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_64K)
-folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_16M)
-folderdesc = "<storage_type=7>"
+    #### prepare structres to create and fill the folders
+    folderspec = cool.ExtendedAttributeListSpecification()
+    #folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_255)
+    #folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_4K)
+    #folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_64K)
+    folderspec.push_back("data","string",cool.PredefinedStorageHints.STRING_MAXSIZE_16M)
+    folderdesc = "<storage_type=7>"
 
-payload = pool.AttributeList(folderspec.attributeListSpecification())
-payload["data"] = "XML stuff"
-
-dbs = cool.DatabaseSvcFactory.databaseService()
-
-dbs.dropDatabase(connectString)
-
-db = dbs.createDatabase(connectString)
-
-excludes = [ 'clhep2dtd.pl' ]
-
-# count files (just to know)
-sum = 0
-for root, dirs, files in os.walk(DDDB_top):
-    if 'CVS' in root: # skip CVS directories
-        continue
-    for x in excludes:
-        if x in files:
-            files.remove(x)
-    sum += len(files)
-
-num_len = len(str(sum))
-# populate the database
-n = 0
-for root, dirs, files in os.walk(DDDB_top):
-    # remove base path
-    dbroot = root.replace(DDDB_top,"")
-    if 'CVS' in root: # skip CVS directories
-        continue
-
-    if dbroot != "": # it means it is not "/"
-        db.createFolderSet(dbroot)
-    else:
-        dbroot = '/'
+    payload = pool.AttributeList(folderspec.attributeListSpecification())
+    payload["data"] = "XML stuff" # dummy value, just to start
     
-    print dbroot
-    for x in excludes:
-        if x in files:
-            files.remove(x)
-    for f in files:
-        n+=1
-        print ("%" + str(num_len) + "d %" + str(num_len) + "d  %s")%(n,sum-n,f)
-        folder = db.createFolderExtended(os.path.join(dbroot,f),
-                                         folderspec,folderdesc,
-                                         cool.FolderVersioning.MULTI_VERSION)
-        xml_data = open(os.path.join(root,f)).read()
-        fixed_data = fix_system_ids(xml_data,"conddb:"+dbroot)
-        fixed_data = fix_env_vars(fixed_data)
-        if fixed_data.find('Velo/v200507/DTD/geometry.dtd') >= 0 :
-            print fixed_data
-        payload["data"] = fixed_data
-        folder.storeObject(cool.ValidityKeyMin,cool.ValidityKeyMax,payload,0)
+    #### initialize COOL
+    dbs = cool.DatabaseSvcFactory.databaseService()
 
-print "Total %d files"%sum
+    if options.drop:
+        dbs.dropDatabase(options.connectString)
+        db = dbs.createDatabase(options.connectString)
+    else:
+        db = dbs.openDatabase(options.connectString)
+    
+    excludes = [ 'clhep2dtd.pl' ]
+    
+    # count files (just to know)
+    sum = 0
+    for root, dirs, files in os.walk(options.source):
+        if 'CVS' in root: # skip CVS directories
+            continue
+        for x in excludes:
+            if x in files:
+                files.remove(x)
+        sum += len(files)
+
+    num_len = len(str(sum))
+    # populate the database
+    n = 0
+    for root, dirs, files in os.walk(options.source):
+        # remove base path
+        dbroot = root.replace(options.source,options.dest)
+        dbroot = dbroot.replace('//','/')
+        
+        if 'CVS' in root: # skip CVS directories
+            continue
+    
+        if dbroot != "/": # it means it is not "/"
+            db.createFolderSet(dbroot)
+        #else:
+        #    dbroot = "/"
+        
+        print dbroot
+        for x in excludes:
+            if x in files:
+                files.remove(x)
+        for f in files:
+            n+=1
+            print ("%" + str(num_len) + "d %" + str(num_len) + "d  %s")%(n,sum-n,f)
+            folder = db.createFolderExtended(os.path.join(dbroot,f),
+                                             folderspec,folderdesc,
+                                             cool.FolderVersioning.MULTI_VERSION)
+            xml_data = open(os.path.join(root,f)).read()
+            fixed_data = fix_system_ids(xml_data,"conddb:"+dbroot)
+            fixed_data = fix_env_vars(fixed_data)
+            payload["data"] = fixed_data
+            folder.storeObject(cool.ValidityKeyMin,cool.ValidityKeyMax,payload,0)    
+
+    print "Total files inserted = %d"%sum
+
+
+if __name__ == '__main__':
+    main()
