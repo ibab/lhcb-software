c998a98b87e22ec367f904047c2a7928c717059b
Jeroen Van Tilburg <jeroen.van.tilburg@cern.ch>
2005-11-24 11:55:37 +0000

    Removed obsolete tool.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@15741 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4cf1e1d67fee16c8a638dde9b969b18ac7474808

--- Rec/Tr/TrackFitter/src/TrackFitter_load.cpp
+++ Rec/Tr/TrackFitter/src/TrackFitter_load.cpp
@@ -5,7 +5,6 @@ DECLARE_FACTORY_ENTRIES( TrackFitter ) {
    DECLARE_ALGORITHM( TrackEventFitter );
 
    DECLARE_TOOL( TrackKalmanFilter );
-   DECLARE_TOOL( TrackOldKalmanFilter );
    DECLARE_TOOL( TrackMasterFitter );
 
 }
