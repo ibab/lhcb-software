0adfc982f46b55063a2afcf4bd08de3aa991c817
Patrick Robbe <robbe@lal.in2p3.fr>
2005-11-29 15:55:34 +0000

    Add LeptonInAcceptance tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@91999 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f72756bae5e1629b7827987dcd054c6b28bfd8be

--- Gauss/Gen/Generators/src/component/Generators_entries.cpp
+++ Gauss/Gen/Generators/src/component/Generators_entries.cpp
@@ -1,4 +1,4 @@
-//  $Id: Generators_entries.cpp,v 1.4 2005-11-17 15:56:02 robbep Exp $
+//  $Id: Generators_entries.cpp,v 1.5 2005-11-29 15:55:34 robbep Exp $
 //====================================================================
 //  Generators_entries.cxx
 //--------------------------------------------------------------------
@@ -13,6 +13,7 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(Generators) {
+  // Old structure
   DECLARE_TOOL( EvtGenTool ) ;
   DECLARE_ALGORITHM( DumpMC );
   DECLARE_ALGORITHM( DumpMCDecay );
@@ -20,18 +21,34 @@ DECLARE_FACTORY_ENTRIES(Generators) {
   DECLARE_ALGORITHM( SetDecayAlg );
   DECLARE_ALGORITHM( SmearVertexAlg);
   DECLARE_ALGORITHM( SignalDecayAlg ) ;
+  // New structure:
+
+  // Main algorithm
   DECLARE_ALGORITHM( Generation ) ;
+
+  // Luminosity Tools
   DECLARE_TOOL( VariableLuminosity ) ;
   DECLARE_TOOL( FixedNInteractions ) ;
+
+  // Sample Generation tools
   DECLARE_TOOL( MinimumBias ) ;
-  DECLARE_TOOL( CollidingBeams ) ;
-  DECLARE_TOOL( FixedTarget ) ;
-  DECLARE_TOOL( EvtGenDecay ) ;
-  DECLARE_TOOL( LHCbAcceptance ) ;
   DECLARE_TOOL( Inclusive ) ;
   DECLARE_TOOL( SignalPlain ) ;
   DECLARE_TOOL( SignalRepeatedHadronization ) ;
   DECLARE_TOOL( SignalForcedFragmentation ) ;
   DECLARE_TOOL( Special ) ;
+
+  // Beam Tools
+  DECLARE_TOOL( CollidingBeams ) ;
+  DECLARE_TOOL( FixedTarget ) ;
+
+  // Decay Tools
+  DECLARE_TOOL( EvtGenDecay ) ;
+
+  // Cut Tools
+  DECLARE_TOOL( LHCbAcceptance ) ;
+
+  // Full event cut tools
+  DECLARE_TOOL( LeptonInAcceptance ) ;
 }
   
