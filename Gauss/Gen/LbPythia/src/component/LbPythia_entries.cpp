c53c9ca7a7773684988f8b44dfa970a0991cc330
Patrick Robbe <robbe@lal.in2p3.fr>
2005-11-29 16:05:25 +0000

    Add PythiaHiggs cut tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92008 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2333e4633cfddee34e2598cae01fdaa8ea7cc573

--- Gauss/Gen/LbPythia/src/component/LbPythia_entries.cpp
+++ Gauss/Gen/LbPythia/src/component/LbPythia_entries.cpp
@@ -1,4 +1,4 @@
- //  $Id: LbPythia_entries.cpp,v 1.2 2005-10-03 10:34:52 robbep Exp $
+ //  $Id: LbPythia_entries.cpp,v 1.3 2005-11-29 16:03:18 robbep Exp $
 //====================================================================
 //  LbPythia_entries.cxx
 //--------------------------------------------------------------------
@@ -13,9 +13,17 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES( LbPythia ) {
+  // Old structure
   DECLARE_ALGORITHM( PythiaAlg );
   DECLARE_ALGORITHM( HiggsPythiaAlg );
   DECLARE_ALGORITHM( TTbarPythiaAlg );
+
+  // New structure:
+
+  // Production tools
   DECLARE_TOOL( PythiaProduction ) ;
+
+  // Cut tools
+  DECLARE_TOOL( PythiaHiggs ) ;
 }
   
