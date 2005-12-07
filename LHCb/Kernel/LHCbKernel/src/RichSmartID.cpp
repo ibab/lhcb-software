212a3d55becc5344eb1d7cc9a0ab74c5ea8cedda
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2005-12-07 12:00:04 +0000

    Update naming scheme in RichSmartID
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47162 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bf9e110fff208af1ca3da387964e0dda2d6d42e1

--- LHCb/Kernel/LHCbKernel/src/RichSmartID.cpp
+++ LHCb/Kernel/LHCbKernel/src/RichSmartID.cpp
@@ -4,7 +4,7 @@
  *
  *  Implementation file for RICH Channel ID class : RichSmartID
  *
- *  $Id: RichSmartID.cpp,v 1.6 2005-11-25 16:15:48 cattanem Exp $
+ *  $Id: RichSmartID.cpp,v 1.7 2005-12-07 12:00:04 jonrob Exp $
  *
  *  @author  Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date    2005-01-06
@@ -24,24 +24,23 @@ std::ostream& LHCb::RichSmartID::fillStream(std::ostream& s) const
 {
 
   // Is this smart ID valid
-  if ( isValid() ) {
-
+  if ( isValid() ) 
+  {
     // If so, print out only the explicitly set fields
     s << "{";
-    if ( richIsSet()     ) s << " "       << rich();
-    if ( panelIsSet()    ) s << " Panel " << static_cast<unsigned int>(panel());
-    if ( pdColIsSet()    ) s << " PDCol"  << format("%3i",pdCol());
-    if ( pdRowIsSet()    ) s << " PDRow"  << format("%3i",pdRow());
-    if ( pixelColIsSet() ) s << " pixCol" << format("%3i",pixelCol());
-    if ( pixelRowIsSet() ) s << " pixRow" << format("%3i",pixelRow());
-    if ( subPixelIsSet() ) s << " subPix" << format("%2i",subPixel());
+    if ( richIsSet()          ) s << " "            << rich();
+    if ( panelIsSet()         ) s << " Panel "      << static_cast<unsigned int>(panel());
+    if ( hpdColIsSet()        ) s << " HPDCol"      << format("%3i",hpdCol());
+    if ( hpdNumInColIsSet()   ) s << " HPDNumInCol" << format("%3i",hpdNumInCol());
+    if ( pixelColIsSet()      ) s << " pixCol"      << format("%3i",pixelCol());
+    if ( pixelNumInColIsSet() ) s << " pixNumInCol" << format("%3i",pixelNumInCol());
+    if ( subPixelIsSet()      ) s << " subPix"      << format("%2i",subPixel());
     s << " }";
-
-  } else {
-
-    // This SmartID has no valid bits set. This is bad...
+  } 
+  else 
+  {
+    // This SmartID has no valid bits set. This is bad ...
     s << "WARNING : Invalid RichSmartID";
-
   }
 
   return s;
