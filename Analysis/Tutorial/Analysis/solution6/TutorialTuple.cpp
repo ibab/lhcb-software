30499d57839ddf2d96a2ab47e4f3bc52f1787603
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2005-12-12 12:19:13 +0000

    Prepare for Cambridge
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@68907 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f49195040b9756464fc5d13482d0da4d78a93070

--- Analysis/Tutorial/Analysis/solution6/TutorialTuple.cpp
+++ Analysis/Tutorial/Analysis/solution6/TutorialTuple.cpp
@@ -1,8 +1,12 @@
-// $Id: TutorialTuple.cpp,v 1.2 2005-11-15 08:26:43 pkoppenb Exp $
+// $Id: TutorialTuple.cpp,v 1.3 2005-12-12 12:19:13 pkoppenb Exp $
 // Include files 
 
 // from Gaudi
 #include "GaudiKernel/AlgFactory.h" 
+
+#include "Event/TrgDecision.h"
+#include "Event/HltScore.h"
+#include "Event/L1Score.h"
 
 #include "Event/MCParticle.h"
 #include "Event/TrgDecision.h"
@@ -117,11 +121,79 @@ StatusCode TutorialTuple::fillTrigger(Tuple& tuple) {
     TrgDecision* trg = get<TrgDecision> (TrgDecisionLocation::Default);
     tuple->column ( "L0", trg->L0() ) ;
     tuple->column ( "L1", trg->L1() ) ;
+    tuple->column ( "HLT",trg->HLT() ) ;
   } else{
     tuple->column ( "L0", false ) ;
     tuple->column ( "L1", false ) ;
+    tuple->column ( "HLT", false );
   }    
-  // One could also access L1Score and HltScore
+
+  // L1 score
+  L1Score* scorel1 = NULL;
+  if (exist<L1Score>(L1ScoreLocation::Default) )
+  {
+     scorel1 = get<L1Score> (L1ScoreLocation::Default);
+     if ( NULL == scorel1 )
+     {
+       err() << "Null L1Score at " << L1ScoreLocation::Default << endmsg ;
+     }
+     else
+     {
+      tuple->column ( "L1GEN", scorel1->decisionGen() );
+      tuple->column ( "L1MU", scorel1->decisionMu() );
+      tuple->column ( "L1JPSI", scorel1->decisionDiMuJPsi() );
+      tuple->column ( "L1DIMU", scorel1->decisionDiMu() );
+      tuple->column ( "L1PHOT", scorel1->decisionPhot() );
+      tuple->column ( "L1ELEC", scorel1->decisionElec() );
+     }
+  } 
+  else
+  {
+     Warning("No L1Score at "+ L1ScoreLocation::Default);
+  }
+  if ( NULL == scorel1 )
+  {
+    tuple->column ( "L1GEN", false ); 
+    tuple->column ( "L1MU", false );
+    tuple->column ( "L1JPSI", false );
+    tuple->column ( "L1DIMU", false );
+    tuple->column ( "L1PHOT", false );
+    tuple->column ( "L1ELEC", false );
+  }
+
+
+
+  // HLT score
+  HltScore* score = NULL;
+  if (exist<HltScore>(HltScoreLocation::Default) )
+  {
+     score = get<HltScore> (HltScoreLocation::Default);
+     if ( NULL == score )
+     {
+       err() << "Null HLTScore at " << HltScoreLocation::Default << endmsg ;
+     }
+     else
+     {
+      tuple->column ( "HLTGEN", score->decisionGen() );
+      tuple->column ( "HLTINC", score->decisionInclusiveB() );
+      tuple->column ( "HLTDIM", score->decisionDimuon() );
+      tuple->column ( "HLTDST", score->decisionDstar() );
+      tuple->column ( "HLTEXC", score->decisionExclusive() );
+     }
+  } 
+  else
+  {
+     Warning("No HLTScore at "+ HltScoreLocation::Default);
+  }
+  if ( NULL == score )
+  {
+    tuple->column ( "HLTGEN", false ); 
+    tuple->column ( "HLTINC", false );
+    tuple->column ( "HLTDIM", false );
+    tuple->column ( "HLTDST", false );
+    tuple->column ( "HLTEXC", false );
+  }
+
   
   return StatusCode::SUCCESS;
 }
