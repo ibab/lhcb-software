7df20494dff0b1a8a6cb5768ef3459f84e660dd0
ocallot <ocallot@cern.ch>
2005-12-20 13:47:37 +0000

    New implementation of L0Muon form Julien Cogan
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34540 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1fcee3eef82dd890589a63c1d5e1414267a0541b

--- LHCb/Event/L0Event/Event/L0MuonBase.h
+++ LHCb/Event/L0Event/Event/L0MuonBase.h
@@ -1,4 +1,4 @@
-// $Id: L0MuonBase.h,v 1.2 2005-02-24 16:54:08 dlunesu Exp $
+// $Id: L0MuonBase.h,v 1.3 2005-12-20 13:47:37 ocallot Exp $
 //
 #ifndef      L0MUON_L0MUONBASE_H
 #define      L0MUON_L0MUONBASE_H 1
@@ -6,7 +6,7 @@
 
 /** @class L0MuonBase L0MuonBase.h L0Muon/L0MuonBase.h
 
-    Namespace for code/decode rules of L0MuonCandidate class
+    Namespace for code/decode rules of L0ProcessorData class for muon
 
     @author  Andrei Tsaregorodtsev
     @date    9/01/2002
@@ -16,114 +16,38 @@
 namespace L0MuonBase {
 
   typedef unsigned int   ContentType ; 
-  //
-  static const unsigned int BitsBCID         = 8 ;
-  static const unsigned int BitsStatus       = 4 ;
-  static const unsigned int BitsPtSign       = 1 ;
+
+  // Empty L0ProcessorData
+  static const unsigned int emptyMuonData    = 1<<16 ;
+
+  // Bits reserved for the various fields
   static const unsigned int BitsPt           = 7 ;
-  static const unsigned int BitsRegion       = 2 ; 
-  static const unsigned int BitsX            = 6 ;
-  static const unsigned int BitsY            = 4 ;
+  static const unsigned int BitsAddM3        = 4 ;
+  static const unsigned int BitsPU           = 2 ;
+  static const unsigned int BitsBoard        = 4 ; 
+  static const unsigned int BitsCharge       = 1 ;
 
-  //For L1Buffer
-  static const unsigned int BitsBufStatus    = 2 ;
-  static const unsigned int BitsBufPtSign    = 1 ;
-  static const unsigned int BitsBufPt        = 7 ;
-  static const unsigned int BitsBufQuarter   = 2 ;
-  static const unsigned int BitsBufRegion    = 2 ;
-  static const unsigned int BitsBufPBx       = 1 ;
-  static const unsigned int BitsBufPBy       = 1 ;
-  
-  static const unsigned int BitsBufPUx       = 1 ;
-  static const unsigned int BitsBufPUy       = 1 ;
-  static const unsigned int BitsBufM3x       = 5 ;
-  static const unsigned int BitsBufM3y       = 2 ;
-  static const unsigned int BitsBufOffM2     = 4 ;
-  static const unsigned int BitsBufOffM1     = 3 ;
+
+  // CU Word (16 bits per candidate)
+  static const unsigned int ShiftPt     =  1 ;
+  static const unsigned int ShiftAddM3  =  8 ;
+
+  // SU Word (16 bits per candidate)
+  static const unsigned int ShiftPU     =  1 ; 
+  static const unsigned int ShiftBoard  =  3 ; 
+  static const unsigned int ShiftCharge =  7 ;
 
   //
-  static const unsigned int ShiftX        = 0 ;
-  static const unsigned int ShiftY        = ShiftX       + BitsX  ;
-  static const unsigned int ShiftRegion   = ShiftY       + BitsY  ;
-  static const unsigned int ShiftPt       = ShiftRegion  + BitsRegion ;  
-  static const unsigned int ShiftPtSign   = ShiftPt      + BitsPt ;  
-  static const unsigned int ShiftStatus   = ShiftPtSign  + BitsPtSign ;
-  static const unsigned int ShiftBCID     = ShiftStatus  + BitsStatus  ;
-
-  //For L1Buffer
-  static const unsigned int ShiftBufStatus  = 0 ;
-  static const unsigned int ShiftBufPtSign  = ShiftBufStatus  + BitsBufStatus ;
-  static const unsigned int ShiftBufPt      = ShiftBufPtSign  + BitsBufPtSign ;
-  static const unsigned int ShiftBufQuarter = ShiftBufPt      + BitsBufPt ;
-  static const unsigned int ShiftBufPB      = ShiftBufQuarter + BitsBufQuarter ;
-  static const unsigned int ShiftBufRegion  = ShiftBufQuarter + BitsBufQuarter ;
-  static const unsigned int ShiftBufPBx     = ShiftBufRegion  + BitsBufRegion ;
-  static const unsigned int ShiftBufPBy     = ShiftBufPBx     + BitsBufPBx ;
-
-  static const unsigned int ShiftBufPUx       = 0 ;
-  static const unsigned int ShiftBufPUy       = ShiftBufPUx       + BitsBufPUx ;
-  static const unsigned int ShiftBufM3x       = ShiftBufPUy       + BitsBufPUy ;
-  static const unsigned int ShiftBufM3y       = ShiftBufM3x       + BitsBufM3x ;
-  
-  static const unsigned int ShiftBufOffM2     = ShiftBufM3y + BitsBufM3y ;
-  static const unsigned int ShiftBufOffM1     = ShiftBufOffM2 + BitsBufOffM2 ;
-
-
-  //
-  static const ContentType  MaskX     =
-  ( ( ( (ContentType) 1 ) << BitsX  ) - 1  ) << ShiftX  ;
-  static const ContentType  MaskY     =
-  ( ( ( (ContentType) 1 ) << BitsY  ) - 1  ) << ShiftY  ;
-  static const ContentType  MaskRegion     =
-  ( ( ( (ContentType) 1 ) << BitsRegion  ) - 1  ) << ShiftRegion  ;
-  static const ContentType  MaskPt     =
-  ( ( ( (ContentType) 1 ) << BitsPt  ) - 1  ) << ShiftPt   ;
-  static const ContentType  MaskPtSign     =
-  ( ( ( (ContentType) 1 ) << BitsPtSign  ) - 1  ) << ShiftPtSign   ;
-  static const ContentType  MaskStatus     =
-  ( ( ( (ContentType) 1 ) << BitsStatus  ) - 1  ) << ShiftStatus  ;
-  static const ContentType  MaskBCID     =
-  ( ( ( (ContentType) 1 ) << BitsBCID  ) - 1  ) << ShiftBCID  ;
-  
-
-  //For L1Buffer                                                                                        
-  static const ContentType  MaskBufStatus     =
-  ( ( ( (ContentType) 1 ) << BitsBufStatus  ) - 1  ) << ShiftBufStatus ;
-  static const ContentType  MaskBufQuarter     =
-  ( ( ( (ContentType) 1 ) << BitsBufQuarter  ) - 1  ) << ShiftBufQuarter ;
-  static const ContentType  MaskBufRegion     =
-  ( ( ( (ContentType) 1 ) << BitsBufRegion  ) - 1  ) << ShiftBufRegion ;
-  static const ContentType  MaskBufPBx     =
-  ( ( ( (ContentType) 1 ) << BitsBufPBx  ) - 1  ) << ShiftBufPBx ;
-  static const ContentType  MaskBufPBy     =
-  ( ( ( (ContentType) 1 ) << BitsBufPBy  ) - 1  ) << ShiftBufPBy ;
-
-  static const ContentType  MaskBufPUx     =
-  ( ( ( (ContentType) 1 ) << BitsBufPUx  ) - 1  ) << ShiftBufPUx ;
-  static const ContentType  MaskBufPUy     =
-  ( ( ( (ContentType) 1 ) << BitsBufPUy  ) - 1  ) << ShiftBufPUy ;
-  static const ContentType  MaskBufM3x     =
-  ( ( ( (ContentType) 1 ) << BitsBufM3x  ) - 1  ) << ShiftBufM3x ;
-  static const ContentType  MaskBufM3y     =
-  ( ( ( (ContentType) 1 ) << BitsBufM3y  ) - 1  ) << ShiftBufM3y ;
-
-  static const ContentType  MaskBufOffM2     =
-  ( ( ( (ContentType) 1 ) << BitsBufOffM2  ) - 1  ) << ShiftBufOffM2 ;
-  static const ContentType  MaskBufOffM1     =
-  ( ( ( (ContentType) 1 ) << BitsBufOffM1  ) - 1  ) << ShiftBufOffM1 ;  
-
-  static const ContentType  MaskBufPtSign     =
-  ( ( ( (ContentType) 1 ) << BitsBufPtSign  ) - 1  ) << ShiftBufPtSign ;
-  static const ContentType  MaskBufPt     =
-  ( ( ( (ContentType) 1 ) << BitsBufPt  ) - 1  ) << ShiftBufPt ;
-  
-
-//define M1 offset extrapolation relatively at M3
-  static const int EXTR_OFFSET_M1_M3[6] = {0,4,7,11,14,18};
-
-
-// Pt decoding constants
-  static const double PT_BIN_WIDTH = 40.;
+  static const ContentType  MaskPt     =  
+  ( ( ( (ContentType) 1 ) << BitsPt     ) - 1  ) << ShiftPt     ;
+  static const ContentType  MaskAddM3  =  
+  ( ( ( (ContentType) 1 ) << BitsAddM3  ) - 1  ) << ShiftAddM3  ;
+  static const ContentType  MaskPU     =  
+  ( ( ( (ContentType) 1 ) << BitsPU     ) - 1  ) << ShiftPU     ;
+  static const ContentType  MaskBoard  =  
+  ( ( ( (ContentType) 1 ) << BitsBoard  ) - 1  ) << ShiftBoard  ;
+  static const ContentType  MaskCharge =  
+  ( ( ( (ContentType) 1 ) << BitsCharge ) - 1  ) << ShiftCharge ;
   
 };
 #endif   // L0MUON_L0MUONBASE_H
--- LHCb/Event/L0Event/src/L0MuonCandidate.cpp
+++ LHCb/Event/L0Event/src/L0MuonCandidate.cpp
@@ -1,104 +1,36 @@
-// $Id: L0MuonCandidate.cpp,v 1.8 2005-12-15 08:35:42 ocallot Exp $
+// $Id: L0MuonCandidate.cpp,v 1.9 2005-12-20 13:47:37 ocallot Exp $
 
 #include <cmath>
 #include <vector>
 #include "Event/L0MuonCandidate.h"
-#include "Event/L0MuonBase.h"
 
-LHCb::L0MuonCandidate::L0MuonCandidate(int status) : m_bitcode(0),
-					       m_theta(0.0),
-					       m_phi(0.0),
-					       m_pads() {
-  setStatus(status);
+LHCb::L0MuonCandidate::L0MuonCandidate() : 
+                 m_pt(0.0),
+                 m_theta(0.0),
+                 m_phi(0.0),
+					       m_pads(),
+                 m_encodedPt(0){
 }
 
-LHCb::L0MuonCandidate::L0MuonCandidate(double pt, 
-                                 double theta, 
-				 double phi, 
-				 const std::vector<LHCb::MuonTileID>& pads, 
-				 int status) {
-  m_bitcode = 0;				 
-  setPt(pt);
-  m_theta = theta;
-  m_phi = phi;
-  std::vector<LHCb::MuonTileID>::const_iterator ip;
-  for(ip = pads.begin(); ip!=pads.end(); ip++ ) {
-    m_pads.push_back(*ip);
-  }	
-  if( ! m_pads.empty() ) {
-    setPadM1(m_pads[0]);
-  }
-  setStatus(status);			 
+LHCb::L0MuonCandidate::L0MuonCandidate(double pt, double theta, double phi, const std::vector<MuonTileID> &pads, int encodedPt) {
+  m_pt        = pt;
+  m_theta     = theta;
+  m_phi       = phi;
+  m_pads      = pads;
+  m_encodedPt = encodedPt;
 }				 
 
-double LHCb::L0MuonCandidate::pt() const
-{
-
-  int tmp = (m_bitcode & L0MuonBase::MaskPt) >> L0MuonBase::ShiftPt;
-  double rtmp = L0MuonBase::PT_BIN_WIDTH * ((double)tmp);
-
-  int sign = (m_bitcode & L0MuonBase::MaskPtSign)
-               >> L0MuonBase::ShiftPtSign;
-  return  sign ? rtmp : -rtmp ;
-
-} 
-
-int LHCb::L0MuonCandidate::status() const
-{
-  int tmp = (m_bitcode & L0MuonBase::MaskStatus) >> L0MuonBase::ShiftStatus;
-  return  tmp ;
-} 
-
-void LHCb::L0MuonCandidate::setBit(int value, 
-                             int shift, 
-			     int mask) {
-  int tmp1 , tmp2 ;
-  tmp1 = ( value << shift ) & mask ; 
-  tmp2 = m_bitcode & ~mask ; 
-  m_bitcode = ( tmp1 | tmp2 ) ;			     
-}
-
-void LHCb::L0MuonCandidate::setPt(double pt) {
-
-  if ( pt > 0.) {
-    setBit(1,L0MuonBase::ShiftPtSign,L0MuonBase::MaskPtSign);
-  } else {
-    setBit(0,L0MuonBase::ShiftPtSign,L0MuonBase::MaskPtSign);
-  }
-  unsigned int roundedPt = int((fabs(pt)+L0MuonBase::PT_BIN_WIDTH/2.)/
-                                              L0MuonBase::PT_BIN_WIDTH);
-  if ( roundedPt > (( 1<< L0MuonBase::BitsPt ) - 1) ) {
-    roundedPt = (1 << L0MuonBase::BitsPt) - 1 ;
-  }  
-  
-  // cout << "L0MuonCandidate::setPt " << pt << " " << roundedPt << endl;
-  
-  setBit(roundedPt,L0MuonBase::ShiftPt,L0MuonBase::MaskPt);
-  
-}
-
-void LHCb::L0MuonCandidate::setStatus(int status) {
-  setBit(status,L0MuonBase::ShiftStatus,L0MuonBase::MaskStatus);
-}
-
-void LHCb::L0MuonCandidate::setPadM1(const LHCb::MuonTileID& pad) {
-  setBit(pad.region(),L0MuonBase::ShiftRegion,L0MuonBase::MaskRegion);
-  setBit(pad.nX(),L0MuonBase::ShiftX,L0MuonBase::MaskX);
-  setBit(pad.nY(),L0MuonBase::ShiftY,L0MuonBase::MaskY);
-}
-
-bool LHCb::L0MuonCandidate::operator<(const LHCb::L0MuonCandidate& lmc) const {
-  return pt() < lmc.pt();
-}
 
 LHCb::MuonTileID LHCb::L0MuonCandidate::pad(unsigned int station) const {
   if ( station < m_pads.size() ) {
     return m_pads[station];
   } else {
-    return LHCb::MuonTileID();
+    return MuonTileID();
   }
 }
 
-LHCb::MuonTileID LHCb::L0MuonCandidate::padM1() const {
-  return pad(0);
+
+bool LHCb::L0MuonCandidate::operator<(const LHCb::L0MuonCandidate& lmc) const {
+  return pt() < lmc.pt();
 }
+
