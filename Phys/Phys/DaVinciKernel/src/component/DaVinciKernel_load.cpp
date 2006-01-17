7bdbe18f86ace533c3dfbb3c6529538479937653
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2006-01-17 12:40:08 +0000

    Particle2VertexAsct restored
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@62983 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2afc96059d8b5d32d92a72ae1ece47bb53eb6107

--- Phys/Phys/DaVinciKernel/src/component/DaVinciKernel_load.cpp
+++ Phys/Phys/DaVinciKernel/src/component/DaVinciKernel_load.cpp
@@ -1,8 +1,16 @@
-// $Id: DaVinciKernel_load.cpp,v 1.2 2004-08-31 11:25:28 pkoppenb Exp $
+// $Id: DaVinciKernel_load.cpp,v 1.3 2006-01-17 12:40:08 pkoppenb Exp $
 // Include files 
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/ObjectFactory.h"
+
+// Associator tool
+#include "Kernel/Particle2VertexAsct.h"
+#include "Relations/RelationMACROs.h"
+#include "Relations/AssociatorMACROs.h"
+
+DECLARE_TOOL_FACTORY( Particle2VertexAsct);
+DECLARE_DATAOBJECT_FACTORY( Particle2VertexTable);
 
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
