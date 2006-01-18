e014cfabd2c32d6946b2f50dda07f4ee4bad5bb1
Jose Angel Hernando Morata <jose.angel.hernando-morata@cern.ch>
2006-01-18 16:35:15 +0000

    adding HltSummary class
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34600 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1eec89d5d1c60852eafc3395ada7003b0f00b13d

--- LHCb/Event/HltEvent/src/HltEvent_load.cpp
+++ LHCb/Event/HltEvent/src/HltEvent_load.cpp
@@ -1,23 +1,15 @@
-// $Id: HltEvent_load.cpp,v 1.4 2003-12-18 15:33:31 cattanem Exp $
+// $Id: HltEvent_load.cpp,v 1.5 2006-01-18 16:35:14 hernando Exp $
 // Include files 
 
 #include "GaudiKernel/ContainerFactoryDefs.h"
 
 /// ====================================================================
-#include "Event/HltTrack.h"
-_ImplementContainedObjectFactory( HltTrack )
-_ImplementDataObjectFactory( HltTracks )
+#include "Event/HltSummary.h"
 
-#include "Event/HltVertex.h"
-_ImplementContainedObjectFactory( HltVertex )
-_ImplementDataObjectFactory( HltVertices )
-
+_ImplementDataObjectFactory( HltSummary )
 
 void HltEvent_load() {
-  DLL_DECL_OBJECTFACTORY( HltTrack  );
-  DLL_DECL_OBJECTFACTORY( HltTracks );
-  DLL_DECL_OBJECTFACTORY( HltVertex );
-  DLL_DECL_OBJECTFACTORY( HltVertices );
+  DLL_DECL_OBJECTFACTORY( HltSummary );
 }
 
 extern "C" void HltEvent_loadRef() 
