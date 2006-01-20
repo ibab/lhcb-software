983e25b892d450f0834808f972d6c9c8dd6d5fbb
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-01-20 14:16:01 +0000

    renamed algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@57301 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 656a970329383db139cfabf9439d4a1a69329714

--- Boole/OT/OTSimulation/src/OTSimulation_load.cpp
+++ Boole/OT/OTSimulation/src/OTSimulation_load.cpp
@@ -1,4 +1,4 @@
-// $Id: OTSimulation_load.cpp,v 1.2 2004-09-10 13:14:23 cattanem Exp $
+// $Id: OTSimulation_load.cpp,v 1.3 2006-01-20 14:16:01 cattanem Exp $
 // include file
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
@@ -6,7 +6,7 @@ DECLARE_FACTORY_ENTRIES(OTSimulation) {
 
   DECLARE_ALGORITHM( MCOTDepositCreator );
   DECLARE_ALGORITHM( MCOTTimeCreator );
-  DECLARE_ALGORITHM( OTFillRawBuffer );
+  DECLARE_ALGORITHM( OTFillRawEvent );
   
   DECLARE_TOOL( OTSmearer );
   DECLARE_TOOL( OTrtRelation );
