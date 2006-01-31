2a5e9eadd1eb958d3c3ab23e8eadca7f49bb5a97
Gloria Corti <gloria.corti@cern.ch>
2006-01-31 10:06:02 +0000

    remove classes no longer necessary
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92145 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 05ac86c72e68af25d3f6e0defab3a50180f07624

--- Gauss/Sim/GiGaCnv/src/component/GiGaCnv_entries.cpp
+++ Gauss/Sim/GiGaCnv/src/component/GiGaCnv_entries.cpp
@@ -1,8 +1,11 @@
-// $Id: GiGaCnv_entries.cpp,v 1.7 2004-02-20 19:12:00 ibelyaev Exp $ 
+// $Id: GiGaCnv_entries.cpp,v 1.8 2006-01-31 10:06:02 gcorti Exp $ 
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.7  2004/02/20 19:12:00  ibelyaev
+//  upgrade for newer GiGa
+//
 // Revision 1.6  2002/12/07 14:36:25  ibelyaev
 //  see $GIGACNVROOT/doc/release.notes
 //
@@ -28,8 +31,6 @@ DECLARE_FACTORY_ENTRIES( GiGaCnv )
   DECLARE_SERVICE    (  GiGaKine               ) ;
   DECLARE_CONVERTER  (  GiGaMCParticleCnv      ) ; 
   DECLARE_CONVERTER  (  GiGaMCVertexCnv        ) ; 
-  /// service and it s converters 
-  DECLARE_SERVICE    (  GiGaHits               ) ;
   /// Algorithms 
   DECLARE_ALGORITHM  (  GiGaDataStoreAlgorithm ) ;
 };
