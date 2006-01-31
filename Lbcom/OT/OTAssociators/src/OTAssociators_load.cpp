448bf14e6f1868a5cc3ec56b904d369f4a4813cf
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-01-31 13:31:05 +0000

    remove obsolete files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@57313 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0617a2832a86dfdc20dce78dabcac029ba77aead

--- Lbcom/OT/OTAssociators/src/OTAssociators_load.cpp
+++ Lbcom/OT/OTAssociators/src/OTAssociators_load.cpp
@@ -1,4 +1,4 @@
-// $Id: OTAssociators_load.cpp,v 1.9 2006-01-30 13:34:33 janos Exp $
+// $Id: OTAssociators_load.cpp,v 1.10 2006-01-31 13:31:05 cattanem Exp $
 // Include files 
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -8,6 +8,4 @@ DECLARE_FACTORY_ENTRIES( OTAssociators )
   DECLARE_ALGORITHM( OTTime2MCHitLinker );
   DECLARE_ALGORITHM( OTTime2MCDepositLinker );
   DECLARE_ALGORITHM( OTTime2MCParticleLinker );
-  //DECLARE_ALGORITHM( TestOTTime2MCHitRel );
-
 }
