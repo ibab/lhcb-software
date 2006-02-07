911a299160f68f93d1ac0731b2d7d79a6f9b083d
Matthew David Needham <mneedham@cern.ch>
2006-02-07 15:58:09 +0000

    Add some protection against 0 size banks
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34692 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6cd78a378b9944de070d12747d01b42ad178099c

--- LHCb/Event/DAQEvent/Event/BankWriter.h
+++ LHCb/Event/DAQEvent/Event/BankWriter.h
@@ -1,4 +1,4 @@
-// $Id: BankWriter.h,v 1.3 2005-11-23 13:28:21 mneedham Exp $
+// $Id: BankWriter.h,v 1.4 2006-02-07 15:58:09 mneedham Exp $
 #ifndef EVENT_BANKWRITER_H 
 #define EVENT_BANKWRITER_H 1
 
@@ -6,6 +6,7 @@
 #include <stddef.h>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 /** @class LHCb::BankWriter BankWriter.h Event/BankWriter.h
  *  Helper class for writing banks. 
@@ -60,9 +61,9 @@ namespace LHCb
   inline BankWriter::BankWriter(size_t blockSize):
     m_pos(0){
     // constructor
-
-    m_dataBank.resize(blockSize);
-    m_byteSize = sizeof(int)*blockSize;
+    size_t startSize = std::max(size_t(1),blockSize); 
+    m_dataBank.resize(startSize);
+    m_byteSize = sizeof(int)*startSize;
     m_cData = reinterpret_cast<unsigned char*>(&m_dataBank[0]);
   }
 
@@ -77,7 +78,8 @@ namespace LHCb
   }
 
   void BankWriter::extend(){
-    m_dataBank.push_back(0);
+    m_dataBank.resize(m_dataBank.size()+1);
+    m_cData = reinterpret_cast<unsigned char*>(&m_dataBank[0]);
     m_byteSize += sizeof(int);
   }
 
