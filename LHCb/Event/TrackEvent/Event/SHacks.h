a955ad8d7a87ddea50d8ebb199b7b7cd56e0a475
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-02-10 16:24:21 +0000

    added TrackUnitsConverters.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34704 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5b92d49def97fa1c8a1bc55db6452cc77b3af0cf

--- LHCb/Event/TrackEvent/Event/SHacks.h
+++ LHCb/Event/TrackEvent/Event/SHacks.h
@@ -7,16 +7,20 @@
 
 // Hacks to SMatrix to give CLHEP functionality
 
-namespace SHacks{
-
- // similarity a la CLHEP 
- template <typename TYPE1, typename TYPE2>
- TYPE2 Similarity(TYPE1 F, TYPE2 C); 
-
- // similarity a la CLHEP 
- template <unsigned int N, typename TYPE>
- double Similarity(ROOT::Math::SVector<double,N> V, TYPE C);
-
+namespace SHacks {
+  
+  // similarity a la CLHEP 
+  template <typename TYPE1, typename TYPE2>
+  TYPE2 Similarity(TYPE1 F, TYPE2 C); 
+  
+  // similarity a la CLHEP 
+  template <unsigned int N, typename TYPE>
+  double Similarity(ROOT::Math::SVector<double,N> V, TYPE C);
+  
+  // hack to symmetrize a square matrix
+  template <unsigned int N>
+  void Symmetrize( ROOT::Math::SMatrix<double,N,N> M );
+  
 };
 
 template <typename TYPE1, typename TYPE2>
@@ -27,6 +31,16 @@ inline TYPE2 SHacks::Similarity(TYPE1 F, TYPE2 C) {
 template <unsigned int N, typename TYPE>
 inline double SHacks::Similarity(ROOT::Math::SVector<double,N> V, TYPE C) {
   return ROOT::Math::Product( V, C );
+};
+
+template <unsigned int N>
+void Symmetrize( ROOT::Math::SMatrix<double,N,N> M ) 
+{
+  for ( unsigned int i = 0; i < M.kCols; ++i ) {
+    for ( unsigned int j = i+1; j < M.kCols; ++j ) {
+      M(j,i) = M(i,j);
+    }
+  }
 };
 
 #endif
---
+++ LHCb/Event/TrackEvent/Event/TrackUnitsConverters.h
@@ -0,0 +1,112 @@
+#ifndef TRACKEVENT_TRACKUNITSCONVERTERS_H
+#define TRACKEVENT_TRACKUNITSCONVERTERS_H_H 1
+
+// Include files
+// -------------
+// from LHCbKernel
+#include "Kernel/PhysicalConstants.h"
+#include "Kernel/TrackTypes.h"
+
+// from TrackEvent
+#include "Event/SHacks.h"
+
+/** @namespace TrackUnitsConverters
+ *
+ * This namespace defines some useful units converters
+ *
+ * @author Eduardo Rodrigues
+ * created 2006-02-10
+ * 
+ */
+
+namespace TrackUnitsConverters
+{
+  
+  //===========================================================================
+  // Change some units for better matrix invertability
+  //===========================================================================
+  void convertToG3( Gaudi::TrackMatrix& C )
+  {
+    C(0,0) /= cm2;
+    C(0,1) /= cm2; 
+    C(0,2) /= cm;
+    C(0,3) /= cm;
+    C(0,4) /= cm*GeV;
+    
+    C(1,1) /= cm2; 
+    C(1,2) /= cm;
+    C(1,3) /= cm;
+    C(1,4) /= cm*GeV;
+    
+    C(2,4) /= GeV;
+    
+    C(3,4) /= GeV;
+    
+    C(4,4) /= GeV*GeV;
+
+    SHacks::Symmetrize<5>( C );
+  };
+
+  //===========================================================================
+  // Change some units for better matrix invertability
+  //===========================================================================
+  void convertToG3( Gaudi::SymMatrix4x4& C )
+  {
+    C(0,0) /= cm2;
+    C(0,1) /= cm2; 
+    C(0,2) /= cm;
+    C(0,3) /= cm;
+    
+    C(1,1) /= cm2; 
+    C(1,2) /= cm;
+    C(1,3) /= cm;
+
+    SHacks::Symmetrize<4>( C );
+  };
+  
+  //===========================================================================
+  // Restore units after inversion. same as convertToG3 !
+  //===========================================================================
+  void convertToG4( Gaudi::TrackMatrix& invC )
+  {
+    invC(0,0) /= cm2;
+    invC(0,1) /= cm2;
+    invC(0,2) /= cm;
+    invC(0,3) /= cm;
+    invC(0,4) /= cm*GeV;
+
+    invC(1,1) /= cm2;
+    invC(1,2) /= cm;
+    invC(1,3) /= cm;
+    invC(1,4) /= cm*GeV;
+
+    invC(2,4) /= GeV;
+
+    invC(3,4) /= GeV;
+
+    invC(4,4) /= GeV*GeV;
+
+    SHacks::Symmetrize<5>( invC );
+  };
+  
+  //===========================================================================
+  // Restore units after inversion. same as convertToG3 !
+  //===========================================================================
+  void convertToG4( Gaudi::SymMatrix4x4& invC )
+  {
+    invC(0,0) /= cm2;
+    invC(0,1) /= cm2; 
+    invC(0,2) /= cm;
+    invC(0,3) /= cm;
+    
+    invC(1,1) /= cm2; 
+    invC(1,2) /= cm;
+    invC(1,3) /= cm;
+
+    SHacks::Symmetrize<4>( invC );
+  };
+  
+  //===========================================================================
+}
+
+#endif /// TRACKEVENT_TRACKUNITSCONVERTERS_H
