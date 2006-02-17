6e9d32493506b577045bd964aa710d4268377e8a
Patrick Robbe <robbe@lal.in2p3.fr>
2006-02-17 13:23:35 +0000

    Fix overload of EvtGen output facility
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92243 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: edcc87fc085154ab5ce48061e187f91ab674859f

--- Gauss/Gen/Generators/src/Lib/EvtReport.cpp
+++ Gauss/Gen/Generators/src/Lib/EvtReport.cpp
@@ -1,34 +1,36 @@
-// $Id: EvtReport.cpp,v 1.1 2005-11-08 00:05:30 robbep Exp $
+// $Id: EvtReport.cpp,v 1.2 2006-02-17 13:23:35 robbep Exp $
 // Overwrite EvtGen output messages
 
 #include "GaudiKernel/MsgStream.h" 
 #include "EvtGenBase/EvtReport.hh"
 
 MsgStream * evtgenStream ;
+std::ostringstream dummyStr ;
 
 //=============================================================================
 // Reimplementation of print facility of EvtGen
 //=============================================================================
 std::ostream & report( Severity severity , const char * facility ) 
-{
+{  
+  dummyStr.str("") ;
   if ( severity < WARNING ) {
-    (*evtgenStream) << endreq ;
     if ( 0 != facility[0] ) { 
-      std::cerr << facility << ":" ;
       (*evtgenStream) << MSG::ERROR << facility << " Error from EvtGen" 
-                      << endreq ;
+                      << endmsg ; 
     }
     else (*evtgenStream) << MSG::ERROR << "Error from EvtGen" 
-                         << endreq ;
-    return ( std::cerr ) ;
-  }
-  
-  if ( severity < INFO ) {
-    if ( 0 != facility[0] ) (*evtgenStream) << MSG::INFO << facility << ":" ;
-    else (*evtgenStream) << MSG::INFO ;
+                         << endmsg ;
+  } else if ( severity < INFO ) {
+    if ( 0 != facility[0] ) {
+      (*evtgenStream) << MSG::INFO ; 
+      if ( evtgenStream -> isActive() ) std::cout << facility << ":" ;
+    } else (*evtgenStream) << MSG::INFO ;
   } else {
-    if ( 0 != facility[0] ) (*evtgenStream) << MSG::DEBUG << facility << ":" ;
-    else (*evtgenStream) << MSG::DEBUG ;
+    if ( 0 != facility[0] ) {
+      (*evtgenStream) << MSG::DEBUG ;
+      if ( evtgenStream -> isActive() ) std::cout << facility << ":" ;
+    } else (*evtgenStream) << MSG::DEBUG ;
   }
-  return evtgenStream -> stream() ;
+  if ( evtgenStream -> isActive() ) return std::cout ;
+  else return dummyStr ;
 }
