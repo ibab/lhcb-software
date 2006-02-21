0ade4e2e9863f9abf8e6b78af4b81a704e724a80
Matthew David Needham <mneedham@cern.ch>
2006-02-21 08:30:19 +0000

    Add new interfaces
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@15889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bf53814af3e7456b3408796517b1513b0a224e93

---
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/ITrackCaloMatch.h
@@ -0,0 +1,33 @@
+// $Id: ITrackCaloMatch.h,v 1.1 2006-02-21 08:30:19 mneedham Exp $ 
+#ifndef _ITrackCaloMatch_H
+#define _ITrackCaloMatch_H
+ 
+#include "GaudiKernel/IAlgTool.h"
+ 
+static const InterfaceID IID_ITrackCaloMatch( "ITrackCaloMatch", 0, 0 );
+ 
+/** @class ITrackCaloMatch
+ *
+ *  interface for selecting tracks....
+ *
+ *  @author M.Needham
+ *  @date   31/05/2005
+ */
+
+namespace LHCb{
+ class Track;
+};
+  
+class ITrackCaloMatch: virtual public IAlgTool{
+ 
+public:
+                                                                                
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_ITrackCaloMatch ; }
+                                                                                                   
+  /// the method
+  virtual double energy(const LHCb::Track& aTrack) const = 0;
+                                                                                
+};
+                                                                                
+#endif
