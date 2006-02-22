1a017738a3b08f9c768d977c645adedfe26b37b4
Tomasz Szumlak <szumlat@cern.ch>
2006-02-22 11:27:03 +0000

    new velo cluster interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47260 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cf372b740b6be53034881fc699774723294b18f9

--- LHCb/Kernel/LHCbKernel/Kernel/IVeloClusterPosition.h
+++ LHCb/Kernel/LHCbKernel/Kernel/IVeloClusterPosition.h
@@ -1,4 +1,4 @@
-// $Id: IVeloClusterPosition.h,v 1.1 2006-02-21 17:38:15 szumlat Exp $
+// $Id: IVeloClusterPosition.h,v 1.2 2006-02-22 11:27:03 szumlat Exp $
 #ifndef VELOALGORITHMS_IVELOCLUSTERPOSITION_H 
 #define VELOALGORITHMS_IVELOCLUSTERPOSITION_H 1
 
@@ -28,39 +28,69 @@ class IVeloClusterPosition : virtual public IAlgTool{
 public:
 
   typedef std::pair<double, double> Pair;
+  // stripPair keeps the closest channel assigned to strip that is
+  // the closest one to the calculated cluster centre and fractional
+  // distance to the strip
   typedef std::pair<LHCb::VeloChannelID, double> stripPair;
+  // object that is returned from the tool, consists of strip
+  // info and error estimate of the cluster position in the
+  // units of strip distance (local pitch)
   typedef std::pair<stripPair, double> toolPair;
   // Return the interface ID
   static const InterfaceID& interfaceID() {return IID_IVeloClusterPos;}
-  //
+  // the main method to retrieve the cluster position and error
+  // userInfo is the input object that must be provided by user
+  // this is a pair that holds information of projected angle of
+  // a track (while not good enough parametrizations for the
+  // angled track are available set this to 0.) and a radius
+  // for clusters on the Phi sensor; the radius also could be
+  // set to 0., in that case default radius will be used 
+  // (depending on sensor zone)
   virtual toolPair position(const LHCb::VeloCluster* cluster,
                             Pair& userInfo)=0;
+  // the method calculate the position using linear charge sharing
+  // approximation
   virtual Pair fractionalPosMean(
                    const LHCb::VeloCluster* cluster)=0;
+  // cluster position calculation based on eta variabel - non linear
+  // charge sharing approximation (particularly useful for low angle 
+  // tracks)
   virtual Pair fractionalPosEta(
           const LHCb::VeloCluster* cluster,
           double alphaOfTrack)=0;
+  // method used to calibration plots - eta variable vs. fractional
+  // position
   virtual Pair etaFrac(
                    const LHCb::VeloCluster* cluster,
                    double fracPosTrue)=0;
+  // helper method
   virtual std::string sensType()=0;
   virtual void setSensType(std::string type)=0;
   
 protected:
-  
+
+  // algorithm to calculate the centre position of the cluster
+  // based on linear approximation
   virtual toolPair weightedMeanPos(
                    const LHCb::VeloCluster* cluster,
                    Pair& userInfo)=0;
+  // algorithm to calculate cluster centre position using eta
+  // variable 
   virtual toolPair etaFitPos(
                    const LHCb::VeloCluster* cluster,
                    Pair& userInfo)=0;
+  // determination of the VeloChannelID and error (the same code for
+  // both linear and eta variable approches)
   virtual void posAndError(
                const LHCb::VeloCluster* cluster,
                const LHCb::VeloChannelID& intDistanceID,
                const double& fractionalPos,
                Pair& userInfo, Pair& toolInfo)=0;
+  // error estimate based on resolution parametrization from silicon
+  // simulation
   virtual double resolution(const double& pitch,
                             const Pair& resInfo)=0;
+  // helper methods
   virtual void createResParTable()=0;
   virtual void createEtaParTable()=0;
 
