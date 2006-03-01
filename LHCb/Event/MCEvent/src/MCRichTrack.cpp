1a057c24d962db1219f6817b4c6cc91ff4a2475e
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-03-01 09:38:42 +0000

    update RICH classes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34773 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 274f4ddc4d8d00b1c8f134ee96f705ce7d35bc4a

--- LHCb/Event/MCEvent/src/MCRichTrack.cpp
+++ LHCb/Event/MCEvent/src/MCRichTrack.cpp
@@ -4,7 +4,7 @@
  *
  *  Implementation file for class : MCRichTrack
  *
- *  $Id: MCRichTrack.cpp,v 1.1 2005-12-06 13:53:35 jonrob Exp $
+ *  $Id: MCRichTrack.cpp,v 1.2 2006-03-01 09:38:42 jonrob Exp $
  *
  *  @author  Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date    2004-01-22
@@ -18,9 +18,16 @@
 const LHCb::MCRichSegment * 
 LHCb::MCRichTrack::segmentInRad( const Rich::RadiatorType rad ) const
 {
+  const LHCb::MCRichSegment * segment = 0;
   for ( SmartRefVector<MCRichSegment>::const_iterator iSeg = mcSegments().begin();
-        iSeg != mcSegments().end(); ++iSeg ) {
-    if ( *iSeg && rad == (*iSeg)->radiator() ) return *iSeg;
+        iSeg != mcSegments().end(); ++iSeg ) 
+  {
+    if ( *iSeg && rad == (*iSeg)->radiator() ) 
+    {
+      if (segment != 0) 
+      { std::cout << "WARNING : MCRichTrack has multiple segments for the same radiator !!" << std::endl; }
+      segment = *iSeg;
+    }
   }
-  return NULL;
+  return segment;
 }
