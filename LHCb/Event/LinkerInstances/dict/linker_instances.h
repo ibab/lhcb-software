e5e70f1fc0cffb3c4c68212e879a3acc8067c8ec
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-03-06 13:54:36 +0000

    updated to new XxxEvent packages and new track event model
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34779 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 826e59b842053fa754a543e263e5f4bb5f660476

--- LHCb/Event/LinkerInstances/dict/linker_instances.h
+++ LHCb/Event/LinkerInstances/dict/linker_instances.h
@@ -2,17 +2,15 @@
 
 #include "Event/MCParticle.h"
 #include "Event/VeloCluster.h"
-#include "Event/TrgTrack.h"
-#include "Event/MuonCoord.h"
-#include "Event/ITCluster.h"
+#include "Event/STCluster.h"
 #include "Event/OTTime.h"
-#include "Event/TrStoredTrack.h"
+#include "Event/MuonCoord.h
+#include "Event/Track.h"
 
 namespace {
   LinkedTo<MCParticle,VeloCluster> i1 (0,0,"");
-  LinkedTo<MCParticle,TrgTrack>    i2 (0,0,"");
-  LinkedTo<MCParticle,OTTime> i3 (0,0,"");
-  LinkedTo<MCParticle,ITCluster> i4 (0,0,"");
-  LinkedTo<MCParticle,MuonCoord> i5 (0,0,"");
-  LinkedTo<MCParticle,TrStoredTrack> i6 (0,0,"");
+  LinkedTo<MCParticle,STCluster>   i2 (0,0,"");
+  LinkedTo<MCParticle,OTTime>      i3 (0,0,"");
+  LinkedTo<MCParticle,MuonCoord>   i4 (0,0,"");
+  LinkedTo<MCParticle,Track>       i5 (0,0,"");
 }
