a4671e2f5933029703ce30c117e5ed5e0732d8d8
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-03-16 16:09:50 +0000

    update radiator intersection helper class
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@7846 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5a3aef2ee73ca7f766f906c4f264ea9f55a46143

--- LHCb/Det/RichDet/RichDet/RichRadIntersection.h
+++ LHCb/Det/RichDet/RichDet/RichRadIntersection.h
@@ -1,4 +1,4 @@
-// $Id: RichRadIntersection.h,v 1.1 2006-03-01 14:53:01 papanest Exp $
+// $Id: RichRadIntersection.h,v 1.2 2006-03-16 16:09:50 jonrob Exp $
 #ifndef RICHDET_RICHRADINTERSECTION_H 
 #define RICHDET_RICHRADINTERSECTION_H 1
 
@@ -9,59 +9,72 @@ class DeRichRadiator;
 
 /** @class RichRadIntersection RichRadIntersection.h RichDet/RichRadIntersection.h
  *  
- *
+ *  RICH Radiator intersection information
+ *  
  *  @author Antonis Papanestis
  *  @date   2006-02-28
  */
-class RichRadIntersection {
+
+class RichRadIntersection 
+{
+
 public: 
+
   /// Standard constructor
-  RichRadIntersection( ); 
+  RichRadIntersection( ) : m_radiator ( NULL ) { }
 
   /// Constructor from two points and a pointer
-  RichRadIntersection(Gaudi::XYZPoint entryPoint,
-                      Gaudi::XYZPoint exitPoint,
-                      const DeRichRadiator* rad ); 
-
-  virtual ~RichRadIntersection( ); ///< Destructor
+  RichRadIntersection( const Gaudi::XYZPoint & entryPoint,
+                       const Gaudi::XYZPoint & exitPoint,
+                       const DeRichRadiator * rad ) :
+    m_entryPoint ( entryPoint ),
+    m_exitPoint  ( exitPoint  ),
+    m_radiator   ( rad        ) { }
+  
+  ~RichRadIntersection( ) { } ///< Destructor
 
   /**
-   * Set accessor to member m_entryPoint
-   * @param entryPoint the new value for m_entryPoint
+   * Set the entry point to the radiator
+   * @param entryPoint the new entry point to the radiator
    */
-  void setEntryPoint (Gaudi::XYZPoint entryPoint) {
+  inline void setEntryPoint ( const Gaudi::XYZPoint & entryPoint ) 
+  {
     m_entryPoint = entryPoint;
   }
 
   /**
-   * Get accessor to member m_entryPoint
-   * @return the current value of m_entryPoint
+   * Get the entry point to the radiator
+   * @return the current value of the entry point to the radiator
    */
-  inline Gaudi::XYZPoint entryPoint () {
+  inline const Gaudi::XYZPoint & entryPoint () const
+  {
     return m_entryPoint;
   }
 
   /**
-   * Set accessor to member m_exitPoint
-   * @param exitPoint the new value for m_exitPoint
+   * Set the exit point of the radiator
+   * @param exitPoint the new exit point of the radiator
    */
-  void setExitPoint (Gaudi::XYZPoint exitPoint) {
+  inline void setExitPoint ( const Gaudi::XYZPoint & exitPoint ) 
+  {
     m_exitPoint = exitPoint;
   }
 
   /**
-   * Get accessor to member m_exitPoint
-   * @return the current value of m_exitPoint
+   * Get the exit point to the radiator
+   * @return the current value of the exit point to the radiator
    */
-  inline Gaudi::XYZPoint exitPoint () {
+  inline const Gaudi::XYZPoint & exitPoint () const 
+  {
     return m_exitPoint;
   }
-
+  
   /**
    * Set accessor to member m_radiator
    * @param radiator the new value for m_radiator
    */
-  void setRadiator (const DeRichRadiator* radiator) {
+  inline void setRadiator ( const DeRichRadiator* radiator ) 
+  {
     m_radiator = radiator;
   }
 
@@ -69,19 +82,22 @@ public:
    * Get accessor to member m_radiator
    * @return the current value of m_radiator
    */
-  inline const DeRichRadiator* radiator () {
+  inline const DeRichRadiator* radiator () const
+  {
     return m_radiator;
   }
 
-
-protected:
-
 private:
+
   /// Entry point for radiator
   Gaudi::XYZPoint m_entryPoint;
+
   /// Exit point for radiator
   Gaudi::XYZPoint m_exitPoint;
+
   /// pointer to DeRichRadiator for this radiator
   const DeRichRadiator* m_radiator;
+
 };
+
 #endif // RICHDET_RICHRADINTERSECTION_H
