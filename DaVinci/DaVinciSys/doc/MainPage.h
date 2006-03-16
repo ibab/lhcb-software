9948152005123c948aeb2dcd1d30b6bb4fe30719
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2006-03-16 12:23:56 +0000

    DC06 series
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@69135 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 54db2e7079ac3f4418938a20b7e0c64d36af0efe

--- DaVinci/DaVinciSys/doc/MainPage.h
+++ DaVinci/DaVinciSys/doc/MainPage.h
@@ -1,5 +1,5 @@
 /** \mainpage notitle
- *  \anchor bruneldoxygenmain
+ *  \anchor davincidoxygenmain
  *
  * This is the code reference manual for the DaVinci Physics Analysis classes.
 
@@ -9,7 +9,11 @@
  * <a href="http://cern.ch/lhcb-comp/Analysis/">web pages</a>
  * of the DaVinci project
  *
- * \sa
+ * <b>The classes used by DaVinci are documented in the following reference manuals:</b>
+ *
+ * \li \ref physsysdoxygenmain "PhysSys documentation (LHCb physics classes)"
+ * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction classes)"
+ * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb shared components)"
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
  * \li \ref gaudidoxygenmain "Gaudi documentation"
  * \li \ref externaldocs     "Related external libraries"
