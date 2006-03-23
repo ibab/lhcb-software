909a5dd5e4875ec85261a45df897ca699723cd19
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-03-23 09:21:39 +0000

    update according to EDMS 704084 v2.0
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34854 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 825257b6a3288daf9054e6a98d5506f2cd55f2d6

--- LHCb/Event/DAQEvent/Event/ODINDefinitions.h
+++ LHCb/Event/DAQEvent/Event/ODINDefinitions.h
@@ -1,4 +1,4 @@
-// $Id: ODINDefinitions.h,v 1.1 2006-03-15 15:00:44 cattanem Exp $
+// $Id: ODINDefinitions.h,v 1.2 2006-03-23 09:21:37 cattanem Exp $
 #ifndef ODINDEFINITIONS_H 
 #define ODINDEFINITIONS_H 1
 
@@ -13,54 +13,63 @@
 namespace LHCb 
 {
   /** @namespace LHCb::ODIN
-   *  Parameters describing structure of ODIN bank as defined in EDMS 704084 v1.2
+   *  Parameters describing structure of ODIN bank as defined in EDMS 704084 v2.0
    *  @author Marco Cattaneo
    *  @date   2006-03-15
    */
   namespace ODIN 
   {
     enum Data {
-      RunNumber = 0, 
+      RunNumber = 0,
+      EventType,
       OrbitNumber,
-      L0EventIDLo,
       L0EventIDHi,
-      GPSTimeLo,
+      L0EventIDLo,
       GPSTimeHi,
-      Word6,
-      Word7
+      GPSTimeLo,
+      Word7,
+      Word8
     }; // enum ODIN::Data
 
-    enum Word6Bits {
+    enum EventTypeBits {
+      EventTypeBits = 0;
+    }; // enum ODIN::EventTypeBits
+    
+    enum EventTypeMasks {
+      EventTypeMask = 0xFFL;
+    }; // enum ODIN::EventTypeMasks
+
+    enum Word7Bits {
       DetectorStatusBits = 0,
       ErrorBits          = 24,
       SynchErrorBits     = 24,
       ErrorForcedBits    = 25
-    }; // enum ODIN::Word6Bits
+    }; // enum ODIN::Word7Bits
      
-    enum Word6Masks {
+    enum Word7Masks {
       DetectorStatusMask = 0xFFFFFFL,
       ErrorMask          = 0xFF000000L,
       SynchErrorMask     = 0x01000000L,
       ErrorForcedMask    = 0x02000000L
-    }; // enum ODIN::Word6Masks
+    }; // enum ODIN::Word7Masks
 
-    enum Word7Bits {
+    enum Word8Bits {
       BunchIDBits      = 0,
       TriggerTypeBits  = 16,
       ReadoutTypeBits  = 19,
       ForceBits        = 21,
       BXTypeBits       = 22,
       BunchCurrentBits = 24
-    }; // enum ODIN::Word7Bits
+    }; // enum ODIN::Word8Bits
 
-    enum Word7Masks {
-      BunchIDMask      = 0xFFFFL,
+    enum Word8Masks {
+      BunchIDMask      = 0xFFFL,
       TriggerTypeMask  = 0x70000L,
       ReadoutTypeMask  = 0x180000L,
       ForceMask        = 0x200000L,
       BXTypeMask       = 0xC00000L,
       BunchCurrentMask = 0xFF000000L
-    }; // enum ODIN::Word7Masks
+    }; // enum ODIN::Word8Masks
 
     enum ReadoutTypes {
       ZeroSuppressed    = 0,
