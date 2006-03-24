4bc1d08dcd593596922b0b8f993c9f4fd68f5be8
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-03-24 07:17:52 +0000

    fix for Windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3816 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0fdcb26f786fdb5fbd07ec4ddbe4b6609e182e18

--- LHCb/Det/DetCond/src/dict/DetCondDict.h
+++ LHCb/Det/DetCond/src/dict/DetCondDict.h
@@ -1,7 +1,15 @@
-// $Id: DetCondDict.h,v 1.1 2006-03-22 13:38:20 cattanem Exp $
+// $Id: DetCondDict.h,v 1.2 2006-03-24 07:17:52 cattanem Exp $
 // ============================================================================
 #ifndef DETCOND_DETCONDDICT_H 
 #define DETCOND_DETCONDDICT_H 1
+// ============================================================================
+// Hack to get round gccxml parsing problem (SEAL bug 9704)
+// ============================================================================
+#ifdef _WIN32
+#define LONG_LONG_MAX  0x7fffffffffffffffLL  /*maximum   signed __int64 value */
+#define LONG_LONG_MIN  0x8000000000000000LL  /*minimum   signed __int64 value */
+#define ULONG_LONG_MAX 0xffffffffffffffffLL  /*maximum unsigned __int64 value */
+#endif
 // ============================================================================
 // GaudiKernel
 // ============================================================================
