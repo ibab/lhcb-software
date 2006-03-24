14f63bc60dd1e0931d16ab639bec117e948a574d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-03-24 10:53:51 +0000

    add missing hash functions
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47299 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d158858cd7b65d42ac2d9482b60a97fe0a8e3545

--- LHCb/Kernel/LHCbKernel/Kernel/RichSmartIDHashFuncs.h
+++ LHCb/Kernel/LHCbKernel/Kernel/RichSmartIDHashFuncs.h
@@ -5,7 +5,7 @@
  *  Header file to define hash functions for RichSmartIDs
  *
  *  CVS Log :-
- *  $Id: RichSmartIDHashFuncs.h,v 1.2 2006-03-01 09:41:46 jonrob Exp $
+ *  $Id: RichSmartIDHashFuncs.h,v 1.3 2006-03-24 10:53:51 jonrob Exp $
  *
  *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date   2005-01-11
@@ -22,17 +22,33 @@
 #include "Kernel/RichSmartID.h"
 
 #ifdef __GNUC__
+
+// GNU hash funcs
 namespace __gnu_cxx
 {
   template <> struct hash<LHCb::RichSmartID>
-  { size_t operator() ( const LHCb::RichSmartID& s ) const { return s.key(); } };
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
   template <> struct hash<const LHCb::RichSmartID>
-  { size_t operator() ( const LHCb::RichSmartID& s ) const { return s.key(); } };
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
   template <> struct hash<LHCb::RichSmartID&>
-  { size_t operator() ( const LHCb::RichSmartID& s ) const { return s.key(); } };
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
   template <> struct hash<const LHCb::RichSmartID&>
-  { size_t operator() ( const LHCb::RichSmartID& s ) const { return s.key(); } };
-};
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
+}
+
+// GaudiUtils::Hash
+namespace GaudiUtils 
+{
+  template <> struct Hash<LHCb::RichSmartID>
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
+  template <> struct Hash<const LHCb::RichSmartID>
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
+  template <> struct Hash<LHCb::RichSmartID&>
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
+  template <> struct Hash<const LHCb::RichSmartID&>
+  { inline size_t operator() ( const LHCb::RichSmartID& s ) const { return (size_t)s.key(); } };
+}
+
 #endif
 
 #endif // RICHKERNEL_RICHSMARTIDHASHFUNCS_H
