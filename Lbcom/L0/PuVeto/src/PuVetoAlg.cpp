0137539abe6f2502856a883f7aa8818319ceafa4
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-04-05 12:16:03 +0000

    use enum for location
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@57602 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8d9b3c23e04bc5868824377597bca07b64378179

--- Lbcom/L0/PuVeto/src/PuVetoAlg.cpp
+++ Lbcom/L0/PuVeto/src/PuVetoAlg.cpp
@@ -1,4 +1,4 @@
-// $Id: PuVetoAlg.cpp,v 1.29 2006-04-05 08:59:05 mtobin Exp $
+// $Id: PuVetoAlg.cpp,v 1.30 2006-04-05 12:16:03 cattanem Exp $
 // Include files
 #include <fstream>
 // from Gaudi
@@ -60,7 +60,7 @@ StatusCode PuVetoAlg::initialize() {
   
   debug() << "==> Initialise" << endreq;
 
-  DeVelo* m_velo = get<DeVelo>( detSvc(), "/dd/Structure/LHCb/BeforeMagnetRegion/Velo" );
+  DeVelo* m_velo = get<DeVelo>( DeVeloLocation::Default );
 
   m_nbPuSensor = m_velo->numberPileUpSensors();
   m_firstPuSensor = (*(m_velo->pileUpRSensorsBegin()))->sensorNumber();
