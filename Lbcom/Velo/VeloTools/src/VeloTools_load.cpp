808cdf77cd1afa7d7bfd0c689d4bb65ec0f6cc95
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-04-10 11:21:30 +0000

    first import of VeloTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@23349 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1684a68e322a2373724f764ef286afc4a5e87fa7

---
+++ Lbcom/Velo/VeloTools/src/VeloTools_load.cpp
@@ -0,0 +1,8 @@
+// $Id: VeloTools_load.cpp,v 1.1.1.1 2006-04-10 11:21:30 cattanem Exp $
+// Include files 
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+DECLARE_FACTORY_ENTRIES(VeloTools) {
+  DECLARE_TOOL( VeloClusterPosition );
+}
