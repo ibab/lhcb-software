05eaa8bd340033fd122976e31c221cc20cd079e2
Erica Polycarpo Macedo <erica.polycarpo@cern.ch>
2006-04-12 13:47:44 +0000

    v4r0 - new version for DC06
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21246 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 725a0274c8abf0dc6878ee6e1a870bddc7bc53d6

--- Rec/Muon/MuonID/src/MuonID.h
+++ Rec/Muon/MuonID/src/MuonID.h
@@ -1,40 +1,45 @@
-// $Id: MuonIDFOI.h,v 1.10 2005-12-07 07:01:55 pkoppenb Exp $
-#ifndef MUONIDFOI_H 
-#define MUONIDFOI_H 1
+#ifndef MUONID_H 
+#define MUONID_H 1
 
 // Include files
 // from STL
 #include <string>
+#include <map>
+#include <vector>
 
 // from Gaudi
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "MuonDet/MuonBasicGeometry.h"
 
-// forward declaration of tools
-class IMuonTileXYZTool;
-class IMuonGeometryTool;
+// From event packages
+#include "Event/MuonPID.h"
+#include "Event/Track.h"
+#include "Event/MuonCoord.h"
 
-//forward declaration of MuonID and TrStoredTrack from Event classes
-class MuonID;
-class TrStoredTrack;
+#include "MuonDet/DeMuonDetector.h"
+#include "TMath.h"
 
-/** @class MuonIDFOI MuonIDFOI.h
+class MuonPID;
+class Track;
+class MuonCoord;
+class DeMuonDetector;
+
+/** @class MuonID MuonID.h
  *  
- *  This is an Algorithm to create MuonID objects starting from tracks and
+ *  This is an Algorithm to create MuonPID objects starting from tracks and
  *  using the hits in the muon system
  *
- *  @author David Hutchcroft
- *  @date   02/05/2002
+ *  @author Erica Polycarpo, Miriam Gandelman
+ *  @date   20/03/2006
  *  
- *  updated by jtmn, miriam 22/04/2003
  *  
  */
-class MuonIDFOI : public GaudiAlgorithm{
+class MuonID : public GaudiAlgorithm{
 public:
   /// Standard constructor
-  MuonIDFOI( const std::string& name, ISvcLocator* pSvcLocator );
+  MuonID( const std::string& name, ISvcLocator* pSvcLocator );
 
-  virtual ~MuonIDFOI( ); ///< Destructor
+  virtual ~MuonID( ); ///< Destructor
 
   virtual StatusCode initialize();    ///< Algorithm initialization
   virtual StatusCode execute   ();    ///< Algorithm execution
@@ -42,13 +47,27 @@ public:
 
 protected:
 
+
 private:
 
   /// Do the identification
-  StatusCode doID(MuonID * pMuid);
+  StatusCode doID(LHCb::MuonPID* pMuid);
 
+  /// Calculates the Likelihood given a MuonPID
+  StatusCode calcMuonLL( LHCb::MuonPID* muonid);
+
+  /// Calculates the number of tracks that share hits
+  StatusCode calcSharedHits( LHCb::MuonPID* muonid, LHCb::MuonPIDs* pMuids );
+
+  /// Calculates the distance from the hit to the extrapolated position
+  /// Input: a MuonPID object
+  StatusCode calcDist( LHCb::MuonPID* muonid );
+
+  /// Compare the coordinates of two MuonPIDs
+  bool compareHits( LHCb::MuonPID* muonid1, LHCb::MuonPID* muonid2 );
+ 
   /// check the track is in the p and angle acceptance
-  StatusCode preSelection(MuonID * pMuid, bool &passed);
+  StatusCode preSelection(LHCb::MuonPID* pMuid, bool &passed);
 
   /// Fill the local vector of coord positions
   StatusCode fillCoordVectors();
@@ -57,10 +76,13 @@ private:
   void clearCoordVectors();
 
   /// Set the MuonCoords used in this ID
-  StatusCode setCoords(MuonID *pMuid);
+  StatusCode setCoords(LHCb::MuonPID *pMuid);
 
   /// Extract the momentum and extrapolate the track to each station
-  StatusCode trackExtrapolate(TrStoredTrack *pTrack);
+  StatusCode trackExtrapolate(const LHCb::Track *pTrack);
+
+  // Calculates MuProb based on DeltaSx (slope difference)
+  float calcMuProb(LHCb::MuonPID * pMuid);
 
   /// return the FOI in x in a station and region for momentum (in MeV/c)
   double foiX(const int &station, const int &region, const double &p, const double &dx);
@@ -73,10 +95,10 @@ private:
   // Properties
 
   /// TES path of the tracks to analyse
-  std::string m_TrStoredTracksPath;
+  std::string m_TracksPath;
 
-  /// TES path to output the MuonIDs to
-  std::string m_MuonIDsPath;
+  /// TES path to output the MuonPIDs to
+  std::string m_MuonPIDsPath;
 
   /// Preselection momentum (no attempt to ID below this)
   double m_PreSelMomentum;
@@ -84,6 +106,9 @@ private:
   /// Momentum ranges: different treatement of M4/M5 in each
   std::vector<double> m_MomentumCuts; // vector of momentum ranges
 
+  /// muon and pion distributions
+  std::vector<double> m_distPion;
+  std::vector<double> m_distMuon;
 
   // function that defines the field of interest size
   // formula is p(1) + p(2)*momentum+p(3)*exp(-p(4)*momentum)
@@ -102,7 +127,9 @@ private:
   int m_NRegion;
   //Names of the station
   std::vector<std::string> m_stationNames;
-
+  // fill local arrays of pad sizes and region sizes
+  DeMuonDetector*  m_mudet;
+    
   // local array of pad sizes in mm
   // all std::vectors here are indexed: [station * m_NRegion + region]
   std::vector<double> m_padSizeX;
@@ -119,6 +146,7 @@ private:
   std::vector<double> m_stationZ; // station position
 
   // local track parameters: momentum and linear extrapolation to each station
+  double m_dist;
   double m_Momentum; // in MeV/c
   double m_trackSlopeX;
   std::vector<double> m_trackX; // position of track in x(mm) in each station
@@ -126,25 +154,23 @@ private:
 
   //test if found a hit in the MuonStations
   std::vector<int> m_occupancy;
+  std::map< LHCb::MuonPID*, std::vector<LHCb::MuonCoord*> > m_muonMap;
+
   // store X of hits for dx/dz matching with track (only need M2/M3)
   std::vector<double> m_CoordX;
   int m_xMatchStation; // first station to calculate slope (M2)
   
-  // MuonTileID to ZYX tool
-  IMuonTileXYZTool *m_iTileTool;
-  IMuonGeometryTool *m_iGeomTool;
-
   // OK nasty optimisation here, store x,dx,y,dy of each coord to test against
   // track extrapolation
   class coordExtent_{
   public:
-    coordExtent_(double x, double dx, double y, double dy, MuonCoord *pCoord) :
+    coordExtent_(double x, double dx, double y, double dy, LHCb::MuonCoord *pCoord) :
       m_x(x), m_dx(dx), m_y(y), m_dy(dy), m_pCoord(pCoord)  {};
     double m_x;
     double m_dx;
     double m_y;
     double m_dy;
-    MuonCoord *m_pCoord;
+    LHCb::MuonCoord *m_pCoord;
   };
 
   // vector of positions of coords (innner vector coords, 
@@ -152,4 +178,4 @@ private:
   std::vector<std::vector<coordExtent_> > m_coordPos; 
 
 };
-#endif // MUONIDFOI_H
+#endif // MUONID_H
--- Rec/Muon/MuonID/src/MuonID_load.cpp
+++ Rec/Muon/MuonID/src/MuonID_load.cpp
@@ -1,4 +1,3 @@
-// $Id: MuonID_load.cpp,v 1.8 2005-12-07 07:01:55 pkoppenb Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
@@ -9,10 +8,7 @@
 
 DECLARE_FACTORY_ENTRIES(MuonID) {
   
-  DECLARE_ALGORITHM( MuonIDFOI );
-  DECLARE_ALGORITHM( MuonDSTPrepare );
-  DECLARE_TOOL     ( MuonIDDLLTool );
-  DECLARE_TOOL     ( MuonIDNSharedHitsTool );
+  DECLARE_ALGORITHM( MuonID );
 };
 
 
