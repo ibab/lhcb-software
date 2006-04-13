44c304e0c306d6bc11ba21d623d168a02ab20831
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-04-13 09:27:45 +0000

    Ready for latest GaudiPython. Several changes/improvements/additions
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@34937 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7f1c0acb8f47bb390f6c27b75b39be0d659d0ff6

--- LHCb/Event/LinkerInstances/python/eventassoc.py
+++ LHCb/Event/LinkerInstances/python/eventassoc.py
@@ -2,13 +2,13 @@ import gaudimodule
 gaudimodule.loaddict('LinkerInstancesDict')
 
 def linkedTo( t1, t2, location ):
-  gaudi = gaudimodule.AppMgr()
+  appMgr = gaudimodule.AppMgr()
   cl = gaudimodule.gbl.LinkedTo( t1, t2 )
   lt = cl( appMgr.evtsvc()._idp, None, location )
   return lt
 
 def linkedFrom( t1, t2, location ):
-  gaudi = gaudimodule.AppMgr()
+  appMgr = gaudimodule.AppMgr()
   cl = gaudimodule.gbl.LinkedFrom( t1, t2 )
   lt = cl( appMgr.evtsvc()._idp, None, location )
   return lt
