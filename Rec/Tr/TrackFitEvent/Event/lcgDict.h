84529c4255d9187956bb4deadfdfc97e0a998b7c
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-04-19 16:28:16 +0000

    added creation of dictionary for StateTraj
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16031 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: db8d1fbdf95d9177395578127fde7599d5734bb7

---
+++ Rec/Tr/TrackFitEvent/Event/lcgDict.h
@@ -0,0 +1,19 @@
+// $Id: lcgDict.h,v 1.1 2006-04-19 16:28:15 erodrigu Exp $
+#ifndef TRACKFITEVENT_LCGDICT_H
+#define TRACKFITEVENT_LCGDICT_ 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "Event/StateTraj.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    // end instantiations
+  };
+}
+
+#endif // TRACKFITEVENT_LCGDICT_H
