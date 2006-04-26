586a08b4e23946c46d985761fccb168abffed734
Niko Neufeld <niko@cern.ch>
2006-04-26 18:40:58 +0000

    does not compile properly yet...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96093 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6720578072277b74aed4cbd92d22cd6c7e324bec

--- Online/Online/GaudiOnline/GaudiOnline/MEPRxSvc.h
+++ Online/Online/GaudiOnline/GaudiOnline/MEPRxSvc.h
@@ -1,3 +1,4 @@
+#if 0
 //	============================================================
 //
 //	MEPRxSvc.h
@@ -5,16 +6,22 @@
 //
 //	Package   : GaudiOnline
 //
-//	Author    : Markus Frank
+//	Author    : Niko Neufeld 
+//                  using code by B. Gaidioz and M. Frank
 //
 //	===========================================================
 #ifndef GAUDIONLINE_MEPRXSVC_H
 #define GAUDIONLINE_MEPRXSVC_H 1
 
+#include <sys/types.h>
+#include <vector>
+#include <string>
+
 // Framework include files
 #include "GaudiKernel/Service.h"
 #include "GaudiKernel/IRunable.h"
 #include "RTL/rtl.h"
+
 // Forward declarations
 class ISvcLocator;
 class IIncidentSvc;
@@ -26,18 +33,31 @@ namespace LHCb  {
 
   /** @class MEPRxSvc MEPRxSvc.h GaudiOnline/MEPRxSvc.h
     *
-    *  @author Markus Frank
+    *  @author Niko Neufeld (using code by B. Gaidioz and M. Frank)
     *  @version 1.0
     */
-  class MEPRxSvc : public Service, virtual public IRunable    {
+  class MEPRxSvc : public Service, virtual public IRunable {
     lib_rtl_thread_t m_handle;
-    bool   m_receiveEvents;
+    int m_MEPBuffers, m_maxMsForGetSpace, m_pktSamplingCount, m_ethDevIn, 
+	m_nSrc, m_sockBuf, m_IPProtoIn, m_refCount, m_MEPBufSize;
+    std::string m_IPOdin, m_bufName;
+    std::vector<std::string> m_IPSrc;
+    float m_maxBadPktRatio;
+    std::map<u_int32_t, int> m_srcAddr;
+    std::vector<std::string> m_srcName;
+    u_int32_t m_partitionID;
     IIncidentSvc* m_incidentSvc;
+    struct iovec m_trashVec[1]; 
     /// Thread execution routine.
     static int exec(void* arg);
     /// Worker routine
     int execute();
   public:
+    /* Counters */
+    u_int64_t *m_rxOct, *m_rxPkt;
+    /* Error-counters */
+    u_int32_t *m_badPkt, *m_misPkt;
+    u_int64_t notReqPkt;
     /// Standard Constructor
     MEPRxSvc(const std::string& name, ISvcLocator* svc);
     /// Standard Destructor
@@ -53,3 +73,4 @@ namespace LHCb  {
   };
 }      // End namespace LHCb
 #endif //  GAUDIONLINE_MEPRXSVC_H
+#endif /* if 0 ////////
