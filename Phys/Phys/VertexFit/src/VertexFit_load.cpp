4e9ce661cddaaf5fd91854089f63db72d43480b1
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2006-05-09 16:26:10 +0000

    Moved GeomDispCalculator to DaVinciTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63130 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 882a91aae806c76670a03a43c79d83c7805a0801

--- Phys/Phys/VertexFit/src/VertexFit_load.cpp
+++ Phys/Phys/VertexFit/src/VertexFit_load.cpp
@@ -14,7 +14,7 @@ DECLARE_FACTORY_ENTRIES( VertexFit ) {
 //   DECLARE_TOOL( UnconstVertexFitter );  
 //   DECLARE_TOOL( KFFitTool );
   DECLARE_TOOL( TrgVertexFitter );  
-  DECLARE_TOOL ( GeomDispCalculator );
+  DECLARE_TOOL ( OfflineVertexFitter );
 /// @todo Restore all other fitters
 //   DECLARE_TOOL( LifetimeFitter );
 //   DECLARE_TOOL( NewDirectionFitter );
