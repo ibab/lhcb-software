42afe05faf441f5ef507d82d960e3f019957ad3e
jpalac <jpalac@cern.ch>
2006-05-16 18:43:21 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35017 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 88f4acf21742ceaf77defc776ad4bbed908206b2

--- LHCb/Event/TrackEvent/Event/TrackParameters.h
+++ LHCb/Event/TrackEvent/Event/TrackParameters.h
@@ -17,12 +17,13 @@
 namespace TrackParameters
 {
   /// Proportionality factor for calculating equation of motion in B-field
-  static const double kappa = 0.3 * GeV / ( c_light * tesla * meter );
+  static const double kappa = 0.3 * Gaudi::Units::GeV / 
+  ( Gaudi::Units::c_light * Gaudi::Units::tesla * Gaudi::Units::meter );
 
   static const double hiTolerance     = 1e-40;          ///< High tolerance
   static const double lowTolerance    = 1e-20;          ///< Low tolerance
   static const double looseTolerance  = 1e-5;           ///< Loose tolerance
-  static const double moliereFactor   = 13.6*MeV;       ///< Moliere factor
+  static const double moliereFactor   = 13.6*Gaudi::Units::MeV; ///< Moliere factor
 
 };
 
