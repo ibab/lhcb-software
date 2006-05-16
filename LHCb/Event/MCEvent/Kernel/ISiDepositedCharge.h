010941f4deb443560862fa492f2e07749360925a
Matthew David Needham <mneedham@cern.ch>
2006-05-16 08:29:41 +0000

    Add ISiDepositedCharge
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35012 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 74ed3985a9753319ff7bb6a210c747551cceead3

---
+++ LHCb/Event/MCEvent/Kernel/ISiDepositedCharge.h
@@ -0,0 +1,41 @@
+// $Id: ISiDepositedCharge.h,v 1.1 2006-05-16 08:29:41 mneedham Exp $
+#ifndef _ISiDepositedCharge_H
+#define _ISiDepositedCharge_H
+
+#include "GaudiKernel/IAlgTool.h"
+
+namespace LHCb{
+ class MCHit;
+};
+
+/** @class ISiDepositedCharge ISiDepositedCharge.h Kernel/ISiDepositedCharge.h
+ *
+ *  Interface Class for estimating deposited charge in Silicon
+ *
+ *  @author M.Needham
+ *  @date   13/3/2002
+ */
+
+// Declaration of the interface ID ( interface id, major version, minor version) 
+static const InterfaceID IID_ISiDepositedCharge("ISiDepositedCharge", 0 , 0); 
+
+class ISiDepositedCharge : virtual public IAlgTool {
+
+public: 
+
+  /** Static access to interface id */
+  static const InterfaceID& interfaceID() { return IID_ISiDepositedCharge; }
+   
+  /** calculate deposited charge (in electrons)
+  * @param hit 
+  * @return deposited charge 
+  */
+  virtual double charge(const LHCb::MCHit* aHit) const=0;
+
+};
+
+#endif // _ISiDepositedCharge_H
+
+
+
+
