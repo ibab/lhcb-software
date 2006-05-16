e5d8d401000237a0ed54be8623a92529e5265d27
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2006-05-16 18:03:18 +0000

    IParticle2State
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63153 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1995f595dbbd516045441836aed03b42c9cf1120

---
+++ Phys/Phys/DaVinciKernel/Kernel/IParticle2State.h
@@ -0,0 +1,48 @@
+// $Id: IParticle2State.h,v 1.1 2006-05-16 18:03:18 pkoppenb Exp $
+#ifndef DAVINCIKERNEL_IPARTICLE2STATE_H 
+#define DAVINCIKERNEL_IPARTICLE2STATE_H 1
+
+// Include files
+#include "GaudiKernel/IAlgTool.h"
+
+// Forward declarations
+// from Event
+#include "Event/Particle.h"
+
+static const InterfaceID IID_IParticle2State("IParticle2State", 1 , 0); 
+
+/** @class IParticle2State IParticle2State.h 
+ *  Convert Particles to State and Back.
+ *  For experts
+ *  @author P. Koppenburg 
+ *  @date   2006-05-16
+ */
+
+class IParticle2State : virtual public IAlgTool {
+
+public:
+
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() {
+    return IID_IParticle2State;
+  }
+  /// Fill a Particle from a State
+  virtual  StatusCode state2Particle( const LHCb::State& state, LHCb::Particle& particle ) const = 0 ;
+  
+  /// Fill a State from Particle
+  virtual  StatusCode particle2State( const LHCb::Particle& particle, LHCb::State& state ) const = 0 ;
+
+  /// Get Jacobian for Tx,Ty,q/P -> Px,Py,Pz transformation
+  //  virtual Gaudi::Matrix5x5 stateJacobian(int charge, const Gaudi::XYZVector&) const = 0 ;
+
+
+  /// test 
+  virtual  StatusCode test( const LHCb::Particle& particle ) const = 0 ;
+  /// test 
+  virtual  StatusCode test( const LHCb::State& state ) const = 0 ;
+  
+
+};
+#endif // DAVINCIKERNEL_IPARTICLE2STATE_H
+
+
