6771431bbe86613ea3b27feb3ef1e81e779049c6
Niko Neufeld <niko@cern.ch>
2006-05-27 17:46:10 +0000

    Changes for DC06 data
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96112 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33daef4e3a9e2c6b07dbf5379c6f9d3132d9b77d

--- Online/Online/MEPMaker/src/MEPMaker_load.cpp
+++ Online/Online/MEPMaker/src/MEPMaker_load.cpp
@@ -1,11 +1,11 @@
-// $Id: MEPMaker_load.cpp,v 1.1 2006-05-23 17:24:18 niko Exp $
+// $Id: MEPMaker_load.cpp,v 1.2 2006-05-27 17:46:10 niko Exp $
 //====================================================================
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(MEPMaker) 
 {
-  DECLARE_OBJECT( L1MEP );
-  DECLARE_OBJECT( RawMEP );
+//  DECLARE_OBJECT( L1MEP );
+  DECLARE_OBJECT( MEPMaker );
   DECLARE_OBJECT( DAQdummy );
   DECLARE_OBJECT( RootTune );
 } 
--- Online/Online/MEPMaker/src/RawMEP.cpp
+++ Online/Online/MEPMaker/src/RawMEP.cpp
@@ -25,15 +25,17 @@
 #include "RawMEP.h"
 
 // Event Model related classes
-#include "Event/RawBuffer.h"
 #include "Event/RawEvent.h"
-
+#include "Event/RawBank.h"
 
 #include "GaudiKernel/System.h"
 #include <sys/types.h>
 
 static const AlgFactory<RawMEP>  Factory;
 const IFactory& RawMEPFactory = Factory;
+
+using LHCb::RawBank;
+using LHCb::RawEvent;
 
 typedef struct {
   union {
@@ -53,33 +55,29 @@ typedef struct {
   } bits2;
 } raw_hdr_t __attribute__((packed));
 	 
-enum BankType{ L0Calo=0,
-               L0DU,
-               PrsE,
-               EcalE,
-               HcalE,
-               PrsTrig,
-               EcalTrig,
-               HcalTrig,
-               Velo,
-               Rich,
-               TT,
-               IT,
-               OT,
-               Muon,
-               L0PU,
-               DAQ,
-               ODIN, 
-               HLT };
+static const char  * rawtypstr[] = { "L0CA", "L0DU", "PRSE", "ECAE", "HCAE", "PRTR",
+				     "ECTR", "HCTR", "VELO", "RICH",   "TT",   "IT", 
+				     "OT", "MUON", "L0PU",  "DAQ",  "ODIN",  "HLT",
+				     "VELOFULL", "TTFULL", "ITFULL", "ECALPCK", "HCALPCK",
+				     "PRSPCK", "L0MU", "ITERR", "TTERR", "ITPED", "TTPED",
+				     "VELOERR","VELOPED", "TOOLARGE" };
+
+
+int rawtyp2det[] = { 0,      1,      2,      3,      4,      2, 
+		     3,      4,      5 /* VELO */,   6 /* RICH */,     
+		     7 /* TT */, 8 /* IT */, 9 /* OT */, 10 /* MUON */,
+		     11 /* L0PU */, 12 /* DAQ */, 13 /* ODIN */, 14 /* HLT */,
+                     5 /* VELOFULL */, 7 /* TTFULL */, 8 /* ITFULL */, 
+                     3, 4, 2 /* PrsPacked */, 15 /* L0MUON */, 
+                     8, 7, 8, 7, 5, 5 };
 
 
 int
-RawMEP::inactivetell1(int tell1id) {
+RawMEP::ignoreTELL1(int tell1id) {
     switch (tell1id) {
-    case 1: return 1; /*  L0DU */
-    case 247: return 1; /* DAQ 2*/
+	//case (1 + 1 + 88 + 19 + 42 + 48 + 24 + 36 + 2 + 1): return 1; /*  HLT */
+    default: return 0;
     }
-    return 0;
 }
      
 int
@@ -87,43 +85,44 @@ RawMEP::rawtell1id(int type, int src)
 {
 
   switch (type) {
-  case RawBuffer::L0Calo:     return 0;
-  case RawBuffer::L0DU:     return 1;
-  case RawBuffer:: PrsE:     
-      if (src < 8) return 2 + src;
-      else *m_log << MSG::ERROR << "Severe: illegal src in PRsE " << src << endmsg;
-      break;
-  case RawBuffer:: EcalE:    return 10 + src;
-  case RawBuffer:: HcalE:    return 20 + src;
-  case RawBuffer:: PrsTrig:   return 2 + src;
-  case RawBuffer:: EcalTrig: return 10 + src;
-  case RawBuffer:: HcalTrig: return 20 + src;
-  case RawBuffer:: Velo:     
-      if ((src >= 42 && src < 64) || src > 105) {
+  case RawBank::L0Calo:     return 0;
+  case RawBank::L0DU:     return 1;
+  case RawBank:: Velo:     
+      if ((src >= 42 && src < 64) || (src > 105 && src < 128) || (src > 131)) {
 	  *m_log << MSG::ERROR << "Severe: illegal src in Velo " << src << endmsg;
 	  break;
       }
-      return 24 + ((src < 42) ? src : (src - 64 + 42));
-  case RawBuffer:: Rich:     return 108 + src;
-  case RawBuffer:: TT:       
-    if (src < 16) return 119 + src;
-    if (src < 32) return 119 + 11 + (src - 16);
-    if (src < 48) return 119 + 22 + (src - 32);
-    if (src < 64) return 119 + 35 + (src - 48);
+      if (src < 42) return 1 + 1 + src;
+      if (src < 106) return 1 + 1 + 42 + src - 64;
+      if (src < 132) return 1 + 1 + 84 + src - 128; 
+  case RawBank:: Rich:     return src + 1 + 1 + 88;
+  case RawBank:: TT:       
+    if (src < 16)  return 1 + 1 + 88 + 18 + src;
+    if (src < 64)  return 1 + 1 + 88 + 18 + 11 + (src - 32);
+    if (src < 90)  return 1 + 1 + 88 + 18 + 22 + (src - 64);
+    if (src < 128) return 1 + 1 + 88 + 18 + 35 + (src - 96);
     *m_log << MSG::ERROR << "Severe: illegal src in TT " << src << endmsg;
     break;
-  case RawBuffer:: IT:       
-    if (src < 16) return 119 + 48 + src;
-    if (src < 32) return 119 + 48 + 14 + (src - 16);
-    if (src < 64) return 119 + 48 + 28 + (src - 32);
-    *m_log << MSG::ERROR << "Severe: illegal src in IT " << src << endmsg; 
+  case RawBank:: IT:       
+    if (src < 16)  return 1 + 1 + 88 + 18 + 48 + src;
+    // if (src < 32) return 1 + 1 + 88 + 18 + 48 + 14 + (src - 11);
+    if (src < 64)  return 1 + 1 + 88 + 18 + 48 + 14 + (src - 32);
+    if (src < 90)  return 1 + 1 + 88 + 18 + 48 + 28 + (src - 64);
+     *m_log << MSG::ERROR << "Severe: illegal src in IT " << src << endmsg; 
     break;
-  case RawBuffer:: OT:       return 119 + 90 + (src - 1);
-  case RawBuffer:: Muon:     return 209 + 24 + ((src > 99 ) ?  src - 100 : src);
-  case RawBuffer:: L0PU:     return 209 + 24 + 12 + src;
-  case RawBuffer:: DAQ:      return 209 + 36 + 1 + (src - 1);
-  case RawBuffer:: ODIN:     return 209 + 36 + 2 + src;
-  case RawBuffer:: HLT:      return 209 + 36 + 2 + 1 + src;
+  case RawBank:: OT:         return 1 + 1 + 88 + 18 + 42 + 48 + (src - 1);
+  case RawBank:: Muon:       return 1 + 1 + 88 + 18 + 42 + 48 + 24 + ((src > 99 ) ?  src - 100 : src);
+  case RawBank:: L0PU:       return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + src;
+  case RawBank:: DAQ:        return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 +  (src - 1);
+  case RawBank:: ODIN:       return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 + 1 + src;
+  case RawBank:: EcalPacked: return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 + 1 + 1 + src;
+  case RawBank:: HcalPacked: return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 + 1 + 1 + 26 + src;
+  case RawBank:: PrsPacked:  return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 + 1 + 1 + 26 + 8 + src;
+  case RawBank:: L0Muon:     return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 12 + 1 + 1 + 1 + 26 + 8 + 8 + src;
+      // case RawBank:: HLT:        return 1 + 1 + 88 + 18 + 42 + 48 + 24 + 36 + 2 + 1 + src;
+  default:
+      *m_log << MSG::ERROR << "Unexpect bank type " << type << " " << rawtypstr[type]  << endmsg;
+	
   }
   return -1;
 }
@@ -379,20 +378,9 @@ static const int id2tpsrc[249 * 2] = {
 -1,-1, /* ODIN */
 17,0}; /* HLT */
 
-int rawtyp2det[18] = {       0,      1,      2,      3,      4,      2, 
-                             3,      4,      5,      6,      7,      8, 
-                             9,     10,      11,     12,    13,     14};
-
-char *rawtypstr[18] = { "L0CA", "L0DU", "PRSE", "ECAE", "HCAE", "PRTR",
-                        "ECTR", "HCTR", "VELO", "RICH",   "TT",   "IT", 
-                          "OT", "MUON", "L0PU",  "DAQ",  "TFC",  "HLT" };
-int  rawmaxsrc[] =  {        1,      1,      8,     10,      4,     84,   
-                             48,    11,     24,     24,      1,      1,      
-                             1,      1};   
-
-char *rawsubdetstr[15] = { "L0CA", "L0DU", "PRSH", "ECAL", "HCAL", "VELO", 
+char *rawsubdetstr[16] = { "L0CA", "L0DU", "PRSH", "ECAL", "HCAL", "VELO", 
                            "RICH",   "TT",   "IT",   "OT", "MUON", "L0PU",  
-                           "DAQ",  "TFC",   "HLT"};
+                           "DAQ",  "ODIN",   "HLT", "L0MU"};
 
 
 /* map bank-types to TELL1s */
@@ -407,18 +395,18 @@ typedef union {
 } efg_t;
 
 
-static u_int8_t frgbuf[300][0x10000];
-static int bankpresent[300];
+static u_int8_t frgbuf[RAW_MAX_SRC][0x10000];
+static int bankpresent[RAW_MAX_SRC];
 
 RawMEP::RawMEP(const std::string& name, ISvcLocator* pSvcLocator)  
   : Algorithm(name, pSvcLocator) { 
-    memset(m_rawfd, 0, 300 * sizeof(void *));
-    memset(m_rawhh, 0, 300 * sizeof(void *));
-    memset(m_fgwritten, 0, sizeof(long long) * 300);
-    memset(m_missbank,  0, sizeof(long) * 300);
+    memset(m_rawfd, 0, RAW_MAX_SRC * sizeof(void *));
+    memset(m_rawhh, 0, RAW_MAX_SRC * sizeof(void *));
+    memset(m_fgwritten, 0, sizeof(long long) * RAW_MAX_SRC);
+    memset(m_missbank,  0, sizeof(long) * RAW_MAX_SRC);
     declareProperty("RAWPFX", m_rawpfx = "");
     declareProperty("RAWHPFX", m_rawhpfx = "raw");
-    declareProperty("RAWNTELL1", m_rawntell1 = 249);
+    declareProperty("RAWNTELL1", m_rawntell1 = 280);
     declareProperty("MarkusFile", m_rawbufout = "");
     m_rmout = -1;
     m_nomep = 0;
@@ -435,126 +423,112 @@ StatusCode RawMEP::initialize() {
     }
     if (!m_rawpfx.compare("")) {
 	m_nomep = 1;
-	return StatusCode::SUCCESS;
     }
-    for (int i = 0; i < m_rawntell1; ++i) {
-	if (inactivetell1(i)) continue;
-	int type, src;
-	char srcstr[10];
-	type = id2tpsrc[2 * i]; src = id2tpsrc[2 * i + 1];
-	sprintf(srcstr, "%03d", src);
-	std::string filename = m_rawpfx + "-" + 
-	    rawsubdetstr[rawtyp2det[type]] + "-" + srcstr + ".mep"; 
-	*m_log << MSG::INFO << filename << " TELL1 " << i << endmsg;	
-	if (!(m_rawfd[i] = fopen(filename.c_str(), "w"))) {
-	    *m_log << MSG::ERROR  << "fopen: " << sys_errlist[errno] << 
-		endmsg;
-	    return StatusCode::FAILURE;
-	}
-    }	    
     return StatusCode::SUCCESS;
 }
 
 StatusCode
-RawMEP::writeraw(raw_int *b, int n)
+RawMEP::writeraw(const RawEvent * evt)
 {
     static int eventid = 0;
     static int first = 1;
     char srcstr[12];
     int len, tell1id;
     int wlen = 0;
-    int nbytes = sizeof(raw_int) * n;
+    int totlen = 0;
 
-    for (int i = 0; i < 300; ++i) {
+    for (int i = 0; i < RAW_MAX_SRC; ++i) {
 	efg_t *fgh = (efg_t *) &frgbuf[i][0];
 	fgh->part.len = 0;
     }
-    if (m_rmout != -1) wlen = write(m_rmout, &nbytes, 4);
-    for (int i = 0; i < n; i+= len) {
-	raw_hdr_t *hdr = (raw_hdr_t *) &b[i];
+    for (int t = RawBank::L0Calo; t < RawBank::LastType; ++t) {
+      RawEvent *raw = const_cast<RawEvent *>(evt);
+      const std::vector<RawBank *> &b = raw->banks(RawBank::BankType(t));
+      for (unsigned int i = 0; i < b.size(); ++i) {
+	RawBank *hdr = b[i];
 	int type, src;
 	
-	type = hdr->bits2.w2.bankType;
-	src  = hdr->bits2.w2.srcid;
-	len  = hdr->bits1.w1.size;
+	type = hdr->type();
+	src  = hdr->sourceID();
+	len  = hdr->totalSize();
+	*m_log << MSG::DEBUG << "type " << type << " " << rawtypstr[type] << " sourceID " << src << endmsg;
 	if ((tell1id = rawtell1id(type, src)) == -1) {
+	    continue;
 	    return StatusCode::FAILURE;
 	}
-//	std::cout << "GGG " << tell1id << " " << type << " " << rawsubdetstr[rawtyp2det[type]] << " " << src << std::endl;
+	if (ignoreTELL1(tell1id)) continue;
+	totlen += len; //for Markusfiles
+//	std::cout << "GGG  " << tell1id << " " << type << " " << rawsubdetstr[rawtyp2det[type]] << " " << src << std::endl;
 	bankpresent[tell1id]++;
 	efg_t *fgh = (efg_t *) &frgbuf[tell1id][0];
 	if (len == 0) {
-	    *m_log << MSG::ERROR << "Severe error 0-size frag" << endmsg;
-	    return StatusCode::FAILURE;
+	  *m_log << MSG::ERROR << "Severe error 0-size frag" << endmsg;
+	  return StatusCode::FAILURE;
 	}
 	sprintf(srcstr, "%03d", src);
 	if (!m_rawfd[tell1id]) {
-	    std::string filename = m_rawpfx + "-" + 
-		rawsubdetstr[rawtyp2det[type]] + "-" + srcstr + ".mep"; 
-	    *m_log << MSG::INFO << filename << " TELL1 " << tell1id << endmsg;	
-	    if (!(m_rawfd[tell1id] = fopen(filename.c_str(), "w"))) {
-		*m_log << MSG::ERROR  << "fopen: " << sys_errlist[errno] << 
-		    endmsg;
-		return StatusCode::FAILURE;
-	    }
+	  std::string filename = m_rawpfx + "-" + 
+	    rawsubdetstr[rawtyp2det[type]] + "-" + srcstr + ".mep"; 
+	  *m_log << MSG::INFO << filename << " TELL1 " << tell1id << endmsg;	
+	  if (!(m_rawfd[tell1id] = fopen(filename.c_str(), "w"))) {
+	    *m_log << MSG::ERROR  << "fopen: " << sys_errlist[errno] << 
+	      endmsg;
+	    return StatusCode::FAILURE;
+	  }
 	}
 #if 1
 	if (m_rawhh[tell1id] == NULL) {
-	    std::string histname = m_rawhpfx + "-" + 
-		rawsubdetstr[rawtyp2det[type]] + "-" + srcstr;
-	    *m_log << MSG::DEBUG <<  "histoname: " << histname << endmsg;
-	    if ((m_rawhh[tell1id] = histoSvc()->book(histname, histname, 512, 
-						     0.0, 8191.0)) == NULL) {
+	  std::string histname = m_rawhpfx + "-" + 
+	    rawsubdetstr[rawtyp2det[type]] + "-" + srcstr;
+	  *m_log << MSG::DEBUG <<  "histoname: " << histname << endmsg;
+	  if ((m_rawhh[tell1id] = histoSvc()->book(histname, histname, 512, 
+						   0.0, 8191.0)) == NULL) {
 		*m_log << MSG::ERROR << "booking histogram" << endmsg;
 		return StatusCode::FAILURE;
-	    } 
+	  } 
 	}
 #endif
-	int nb = sizeof(raw_int) * len;
-	memcpy(&frgbuf[tell1id][4 + fgh->part.len], &b[i], nb);
+	int nb = len;
+	memcpy(&frgbuf[tell1id][4 + fgh->part.len], hdr, nb);
 	fgh->part.len += nb;
 	fgh->part.evtid = eventid;
+      }
     }
-    
+
+    /* write to files */
+
+    if (m_rmout != -1) wlen = write(m_rmout, &totlen, 4);
+
     unsigned int total = 0;
     for (int i = 0; i < m_rawntell1; ++i) {
-	if (inactivetell1(i)) {
-	    if (first) 
-		*m_log << MSG::INFO << "Ignoring inactive TELL1 " << i << endmsg;
-	    continue;
+      if (ignoreTELL1(i)) {
+	if (first) 
+	  *m_log << MSG::INFO << "Ignoring inactive TELL1 " << i << endmsg;
+	continue;
+      }
+      if (!bankpresent[i]) {
+	if (++m_missbank[i] < 6)
+	  *m_log << MSG::WARNING << "Missing bank from TELL1: " << 
+	    i << endmsg;
+	if (m_missbank[i] == 5) 
+	  *m_log << MSG::WARNING << 
+	    "This message Will be supressed from now on" << endmsg;
+      }
+      efg_t *fgh = (efg_t *) &frgbuf[i][0];
+      total += fgh->part.len;
+      if (!m_nomep) {
+	if (fwrite(&frgbuf[i][0], sizeof(efg_t) + fgh->part.len, 1, 
+		   m_rawfd[i]) != 1) {
+	  *m_log << MSG::ERROR << "write: " << sys_errlist[errno] << 
+	    endmsg;
+	  return StatusCode::FAILURE;
 	}
-	if (!bankpresent[i]) {
-	    if (++m_missbank[i] < 6)
-		*m_log << MSG::WARNING << "Missing bank from TELL1: " << 
-		    i << endmsg;
-	    if (m_missbank[i] == 5) 
-		*m_log << MSG::WARNING << 
-		    "This message Will be supressed from now on" << endmsg;
-	}
-	efg_t *fgh = (efg_t *) &frgbuf[i][0];
-	total += fgh->part.len;
-	if (!m_nomep) {
-	    if (fwrite(&frgbuf[i][0], sizeof(efg_t) + fgh->part.len, 1, 
-		       m_rawfd[i]) != 1) {
-		*m_log << MSG::ERROR << "write: " << sys_errlist[errno] << 
-		    endmsg;
-		return StatusCode::FAILURE;
-	    }
-	    m_fgwritten[i]++;
-	}
-	if (m_rmout != -1) 
-	    wlen += write(m_rmout, &frgbuf[i][4], fgh->part.len);
+	m_fgwritten[i]++;
+      }
+      if (m_rmout != -1) 
+	wlen += write(m_rmout, &frgbuf[i][4], fgh->part.len);
 //	    *m_log << MSG::DEBUG << "wrote event " << eventid << endmsg;
-	if (m_rawhh[i]) m_rawhh[i]->fill((double) fgh->part.len);	    
-    }  
-    if (n * sizeof(raw_int) != total) { /* should not happen :-) */
-	*m_log << MSG::ERROR << "mep len " << total << "!= raw len" << 
-	    n * sizeof(raw_int) << endmsg;
-	return StatusCode::FAILURE;
-    }
-    if (m_rmout != -1 && wlen != (n *sizeof(raw_int) + 4)) {
-	*m_log << MSG::ERROR << "raw write len mismatch!" << endmsg;
-	return StatusCode::FAILURE;
+      if (m_rawhh[i]) m_rawhh[i]->fill((double) fgh->part.len);	    
     }
     ++eventid;
     first = 0;
@@ -564,10 +538,10 @@ RawMEP::writeraw(raw_int *b, int n)
 // Execute
 //--------------------------------------------------------------------
 StatusCode RawMEP::execute() {
-  SmartDataPtr<RawBuffer> raw(eventSvc(), RawBufferLocation::Default);
+  SmartDataPtr<RawEvent> raw(eventSvc(), LHCb::RawEventLocation::Default);
 
   if ( raw != 0 )  {
-    StatusCode sc = writeraw(raw->buffer(), raw->currentSize());
+    StatusCode sc = writeraw(raw);
     return sc;
   }
   *m_log << MSG::ERROR << "Unable to retrieve Event object" << endreq;
@@ -585,7 +559,7 @@ RawMEP::finalize()
 	if (m_rawfd[i]) fclose(m_rawfd[i]);       	
 	*m_log << MSG::INFO << i << " " << m_fgwritten[i] << "   " << 
 	    m_missbank[i];
-	if (inactivetell1(i)) *m_log << " INACTIVE ";
+	if (ignoreTELL1(i)) *m_log << " INACTIVE ";
 	else if (!locked) {
 	    locked = 1;
 	    written = m_fgwritten[i];
