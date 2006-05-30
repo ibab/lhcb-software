f6bba6e5069b5690f0945854e435efd19b444d9d
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-05-30 08:43:03 +0000

    v1r4
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35036 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3423a393905fe18768633fa78379385f6f98e035

--- LHCb/Event/RecEvent/Event/CaloHypotheses.h
+++ LHCb/Event/RecEvent/Event/CaloHypotheses.h
@@ -1,32 +1,15 @@
-// $Id: CaloHypotheses.h,v 1.2 2006-05-29 15:26:09 odescham Exp $
+// $Id: CaloHypotheses.h,v 1.3 2006-05-30 08:43:03 cattanem Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
-// ============================================================================
-// $Log: not supported by cvs2svn $
-// Revision 1.1  2006/02/15 10:43:43  cattanem
-// add classes
-//
-// Revision 1.1  2005/12/14 13:05:31  odescham
-// v14r0 - Reviewed CaloEvent Model
-//
-// Revision 1.1  2002/03/18 17:21:47  ibelyaev
-//  add new stuff from CaloKernel package
-//
-// Revision 1.1.1.1  2001/11/25 14:07:38  ibelyaev
-// New Package: substitution of the  previous CaloGen package
-//
-// Revision 1.1  2001/10/30 11:30:04  ibelyaev
-// update for new compiler, CaloHypotheses namespace is added
-//
 // ============================================================================
 #ifndef RECEVENT_CALOHYPOTHESES_H 
 #define RECEVENT_CALOHYPOTHESES_H 1
 
 // Include files
 
-/** @namespace CaloHypotheses CaloHypotheses.h Kernel/CaloHypotheses.h
+/** @namespace CaloHypotheses 
  *  
- *  The namespace containe the deficinion of all used hypotheses for 
+ *  Namespace for definition of hypotheses for 
  *  calorimetry reconstruction/analysis 
  *
  *  @author Vany Belyaev Ivan.Belyaev@itep.ru
@@ -65,8 +48,6 @@ namespace CaloHypotheses
 };
 };
 
-// ============================================================================
-// The End 
 // ============================================================================
 #endif ///< RECEVENT_CALOHYPOTHESES_H
 // ============================================================================
