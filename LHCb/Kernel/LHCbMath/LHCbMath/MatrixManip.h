060066b83e022dd316db53cbaa9057d593ab55ae
jpalac <jpalac@cern.ch>
2006-05-31 14:27:56 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47371 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 308eca564f74231a3a01b9750077b2aca3604116

---
+++ LHCb/Kernel/LHCbMath/LHCbMath/MatrixManip.h
@@ -0,0 +1,51 @@
+// $Id: MatrixManip.h,v 1.1 2006-05-31 14:27:56 jpalac Exp $
+#ifndef LHCBMATH_MATRIXMANIP_H 
+#define LHCBMATH_MATRIXMANIP_H 1
+
+// Include files
+
+#include "Math/SMatrix.h"
+
+/** @namespace MatrixManip MatrixManip.h LHCbMath/MatrixManip.h
+ *  
+ *  Transform to and from symmetric and standard Smatrices.
+ *  
+ *  @author Juan Palacios
+ *  @date   2006-03-28
+ */
+namespace Gaudi 
+{
+  
+  namespace Math {
+
+    using namespace ROOT::Math;
+
+    /** Create a new symmetric matrix from the upper triangle of the input
+     *  matrix.
+     *
+     *  @code
+     *
+     *  @endcode
+     *
+     *  @author Juan PALACIOS juan.palacios@cern.ch
+     *  @date   2006-03-28
+     */  
+    template <typename M>  
+    SMatrix<typename M::value_type, M::kRows, M::kRows, 
+            MatRepSym<typename M::value_type, M::kRows> > Symmetrize(const M& rhs) {
+
+      SMatrix<typename M::value_type, M::kRows, M::kRows, 
+        MatRepSym<typename M::value_type, M::kRows> > result;
+    
+      for ( unsigned int i = 0; i < M::kRows; ++i ) {
+        for ( unsigned int j = 0; j < M::kRows; ++j ) {
+          if (i<=j) result(i,j) = rhs(i,j);
+        }
+      }
+      return result;
+    }
+
+  }
+}
+
+#endif // LHCB_MATRIXMANIP_H
