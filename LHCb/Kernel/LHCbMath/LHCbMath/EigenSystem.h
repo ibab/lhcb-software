0fc7b10ad796aef323d18b8629fef423a0e7c748
jpalac <jpalac@cern.ch>
2006-05-31 14:49:54 +0000

    Add EigenSystem, make into link library, tag as v1r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47373 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 60dc0e4086e91248f7aacb2e396165b07488f716

---
+++ LHCb/Kernel/LHCbMath/LHCbMath/EigenSystem.h
@@ -0,0 +1,269 @@
+// $Id: EigenSystem.h,v 1.1 2006-05-31 14:49:53 jpalac Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $ 
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+#ifndef EIGENSYSTEM_H 
+#define EIGENSYSTEM_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// STD & STL 
+// ============================================================================
+#include <vector>
+// ============================================================================
+// ROOT 
+// ============================================================================
+#include "Math/SMatrix.h"
+#include "Math/SVector.h"
+// ============================================================================
+// GSL 
+// ============================================================================
+#include "gsl/gsl_eigen.h"
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/StatusCode.h"
+// ============================================================================
+
+namespace Gaudi 
+{
+  namespace Math 
+  {
+    namespace GSL
+    {      
+      /** @class EigenSystem EigenSystem.h
+       *  
+       *  Helper class with allows to find eigenvalues and eigenvector
+       *  for symmetrical MathLib matrices ("SMatrix") using GSL library
+       *
+       *  @author Vanya BELYAEV
+       *  @date   2006-05-24
+       */
+      class EigenSystem
+      {
+      public:
+        /// error codes 
+        enum 
+          { 
+            MatrixAllocationFailure     = 101 , 
+            VectorAllocationFailure     = 102 , 
+            WorkspaceAllocationFailure  = 103 , 
+            // the actual return value is ErrorFromGSL + error code )
+            ErrorFromGSL                = 199 , ///< ErrorFromGSL + error code
+          } ;
+      public:
+        /// Standard constructor
+        EigenSystem  () ;
+        /// destructor 
+        ~EigenSystem () ;
+      public:
+        /** evaluate the eigenvalues of symmetrical matrix 
+         * 
+         *  @code 
+         * 
+         *  // create the evaluator 
+         *  EigenSystem eval ;
+         *
+         *  const Gaudi::SymMatrix3x3 matrix = ... ;
+         * 
+         *  // get the sorted vector of eigenvalues:
+         *  const Gaudi::Vector2 result = eval.eigenValues ( matrix ) ;
+         * 
+         *  @endcode 
+         *
+         *  @exception GaudiException is thrown in the case of errors 
+         *
+         *  @param mtrx   (input) the matrix itself 
+         *  @param sorted (input) flag to be use for sorting 
+         *  @return vector of eigenvalues 
+         */
+        template <class T,unsigned int D>
+        inline ROOT::Math::SVector<T,D>
+        eigenValues
+        ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& mtrx , 
+          const bool sorted = true ) const ;
+        /** evaluate the eigenvalues of symmetrical matrix 
+         *
+         *  @code 
+         * 
+         *  // create the evaluator 
+         *  EigenSystem eval ;
+         *
+         *  const Gaudi::SymMatrix3x3 matrix = ... ;
+         *  Gaudi::Vector3 resutl 
+         * 
+         *  // find the eigenvalues:
+         *  StatusCode sc = eval.eigenValues ( matrix , result ) ;
+         * 
+         *  @endcode 
+         *
+         *  @param mtrx   (input)  the matrix itself 
+         *  @param vals   (output) the vector fo eigenvalues 
+         *  @param sorted (input)  flag to be use for sorting 
+         *  @return status code 
+         */
+        template <class T,unsigned int D>
+        inline StatusCode
+        eigenValues
+        ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& mtrx ,
+          ROOT::Math::SVector<T,D>&                                     vals , 
+          const bool sorted = true ) const ;
+        /** evaluate the eigenvalues and eigenvectors of the symmetrical matrix 
+         *
+         *  @code 
+         * 
+         *  // create the evaluator 
+         *  EigenSystem eval ;
+         *
+         *  const Gaudi::SymMatrix3x3 matrix = ... ;
+         *  // matrix with eigenvectors: 
+         *  Gaudi::Matrix3x3          vectors ; 
+         *  // vector of eigenvalues:
+         *  Gausi::Vector3            values  ;
+         *
+         *  // find the eigenvalues & eigenvectors: 
+         *  StatusCode sc = eval.eigenvectors ( matrix , values , vectors ) ;
+         * 
+         *  @endcode 
+         *
+         *  Eigenvectors are returned as columns for the matrix "vecs".
+         *  This matrix could be easily used to for diagonalization of 
+         *  the initial matrix, e.g. 
+         * 
+         *  @code 
+         *
+         *  // avoid long names:
+         *  using namespace ROOT::Math ;
+         *  // *NUMERICALLY* DIAGONAL MATRIX: 
+         *  Gaudi::SymMatrix3x3 res = 
+         *      Similarity ( Transpose ( vectors ) , matrix ) ;
+         *
+         *  @endcode 
+         * 
+         *  
+         *  @param mtrx   (input)  the matrix itself 
+         *  @param vals   (output) the vector fo eigenvalues 
+         *  @param vecs   (output) the matrix with eigenvectors 
+         *  @param sorted (input)  flag to be use for sorting 
+         *  @return status code 
+         */
+        template <class T, unsigned int D>
+        inline StatusCode 
+        eigenVectors 
+        ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& mtrx ,
+          ROOT::Math::SVector<T,D>&                                     vals , 
+          ROOT::Math::SMatrix<T,D,D>&                                   vecs , 
+          const bool sorted = true ) const ;
+        /** evaluate the eigenvalues and eigenvectors of the symmetrical matrix 
+         *
+         *  @code 
+         * 
+         *  // create the evaluator 
+         *  EigenSystem eval ;
+         *
+         *  const Gaudi::SymMatrix3x3   matrix = ... ;
+         *  // vector  with eigenvectors: 
+         *  std::vector<Gaudi::Vector3> vectors ; 
+         *  // vector of eigenvalues:
+         *  Gaudi::Vector3              values  ;
+         *
+         *  // find the eigenvalues & eigenvectors: 
+         *  StatusCode sc = eval.eigenvectors ( matrix , values , vectors ) ;
+         * 
+         *  @endcode 
+         *
+         *  @param mtrx   (input)  the matrix itself 
+         *  @param vals   (output) the vector fo eigenvalues 
+         *  @param vecs   (output) the vector of eigenvectors 
+         *  @param sorted (input)  flag to be use for sorting 
+         *  @return status code 
+         */
+        template <class T, unsigned int D>
+        inline StatusCode 
+        eigenVectors 
+        ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& mtrx ,
+          ROOT::Math::SVector<T,D>&                                     vals , 
+          std::vector<ROOT::Math::SVector<T,D> >&                       vecs , 
+          const bool sorted = true ) const ;
+      protected:
+        /// find the eigenvalues   (& sort them if needed ) 
+        StatusCode _fun1 ( const bool         sorted    ) const ;
+        /// find the eigenvalues&eigenvectors (& sort them if needed ) 
+        StatusCode _fun2 ( const bool         sorted    ) const ;
+      private:        
+        /// fill the internal structures with the input data 
+        template <class T,unsigned int D>
+        inline StatusCode _fill
+        ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& mtrx ) const;
+        /// check/adjust the  internal structures 
+        StatusCode _check    ( const unsigned int D ) const ;
+        // thrown the exception 
+        StatusCode Exception ( const StatusCode& sc ) const ;       
+      private:
+        // the size of workspace 
+        mutable unsigned int   m_dim1   ; ///< the size of workspace 
+        mutable unsigned int   m_dim2   ; ///< the size of workspace 
+        // workspace itself 
+        mutable gsl_eigen_symm_workspace*  m_work1 ; ///< workspace itself 
+        // workspace itself 
+        mutable gsl_eigen_symmv_workspace* m_work2 ; ///< workspace itself 
+        // the matrix with input data  
+        mutable gsl_matrix*     m_matrix ; ///< the matrix with input data 
+        // the matrix with eigenvectors  
+        mutable gsl_matrix*     m_evec   ; ///< the matrix with eigenvectors  
+        // the vector with eigenvalues 
+        mutable gsl_vector*     m_vector ; ///< the vector with eigenvalues 
+      } ;      
+      // ======================================================================
+      /** copy GSL vector into MathLib vector 
+       *  @attention Fast!no checks are performed!
+       *  @param input   GSL vector to be copyed (source)
+       *  @param output  MathLib vector (destination)
+       *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+       *  @date 2006-05-24
+       */
+      template <class T, unsigned int D>
+      inline void 
+      _copy 
+      ( const gsl_vector*         input  , 
+        ROOT::Math::SVector<T,D>& output ) ;
+      // ======================================================================
+      /** copy symmetric MathLib matrix into GSL matrix 
+       *  @attention Fast!no checks are performed!
+       *  @param input  MathLib symmetric matrix (source)
+       *  @param output GSL matrix  (destination)
+       *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+       *  @date 2006-05-24
+       */
+      template < class T, unsigned int D> 
+      inline void 
+      _copy 
+      ( const ROOT::Math::SMatrix<T,D,D,ROOT::Math::MatRepSym<T,D> >& input , 
+        gsl_matrix* output ) ;
+      // ======================================================================
+      /** copy GSL matrix into MathLib matrix into GSL  
+       *  @attention Fast!no checks are performed!
+       *  @param input  GSL matrix (source)
+       *  @param output MathLib matrix (destination)
+       *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+       *  @date 2006-05-24
+       */
+      template < class T, unsigned int D> 
+      inline void 
+      _copy 
+      ( const gsl_matrix*           input  ,
+        ROOT::Math::SMatrix<T,D,D>& output ) ;
+      // ======================================================================      
+    } //  end of namespace GSL
+  } //end of namespace Math 
+} // end of namespace Gaudi
+
+// ============================================================================
+#endif // EIGENSYSTEM_H
+// ============================================================================
+#include "EigenSystem.icpp"  
+// ============================================================================
+// The END 
+// ============================================================================
---
+++ LHCb/Kernel/LHCbMath/src/EigenSystem.cpp
@@ -0,0 +1,145 @@
+// $Id: EigenSystem.cpp,v 1.1 2006-05-31 14:49:54 jpalac Exp $
+// ============================================================================
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $ 
+// ============================================================================
+// $Log: not supported by cvs2svn $ 
+// ============================================================================
+// Include files 
+// ============================================================================
+// STD & STL 
+// ============================================================================
+#include <vector>
+// ============================================================================
+// GSL
+// ============================================================================
+#include "gsl/gsl_eigen.h"
+#include "gsl/gsl_sort_vector.h"
+// ============================================================================
+// ROOT 
+// ============================================================================
+#include "Math/SMatrix.h"
+#include "Math/SVector.h"
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/StatusCode.h"
+#include "GaudiKernel/GaudiException.h"
+// ============================================================================
+// local
+// ============================================================================
+#include "LHCbMath/EigenSystem.h"
+#include "LHCbMath/EigenSystem.icpp"
+// ============================================================================
+
+// ============================================================================
+/** @file 
+ *  Implementation fiel for class Gaudi::Math::GSL::EigenSystem
+ *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+ *  @date 2005-05-24
+ */
+// ============================================================================
+
+// ============================================================================
+// Standard constructor, initializes variables
+// ============================================================================
+Gaudi::Math::GSL::EigenSystem::EigenSystem() 
+  : m_dim1   ( 0 )
+  , m_dim2   ( 0 )
+  , m_work1  ( 0 ) 
+  , m_work2  ( 0 ) 
+  , m_matrix ( 0 ) 
+  , m_evec   ( 0 ) 
+  , m_vector ( 0 )  
+{} ;
+// ============================================================================
+// Destructor
+// ============================================================================
+Gaudi::Math::GSL::EigenSystem::~EigenSystem() 
+{
+  if ( 0 != m_matrix ) { gsl_matrix_free      ( m_matrix ) ; }
+  if ( 0 != m_evec   ) { gsl_matrix_free      ( m_evec   ) ; }
+  if ( 0 != m_vector ) { gsl_vector_free      ( m_vector ) ; }
+  if ( 0 != m_work1  ) { gsl_eigen_symm_free  ( m_work1  ) ; }
+  if ( 0 != m_work2  ) { gsl_eigen_symmv_free ( m_work2  ) ; }
+} ;
+// ============================================================================
+// check/adjust the  internal structures 
+// ============================================================================
+StatusCode Gaudi::Math::GSL::EigenSystem::_check
+( const unsigned int D ) const 
+{
+  // check existing GSL matrix, (re)allocate if needed  
+  if ( 0 != m_matrix && ( D != m_matrix->size1 || D != m_matrix->size2 ) ) 
+  { gsl_matrix_free ( m_matrix ) ; m_matrix = 0 ; }
+  if ( 0 == m_matrix   ) { m_matrix = gsl_matrix_alloc ( D , D ) ; }
+  if ( 0 == m_matrix   ) { return StatusCode ( MatrixAllocationFailure    ) ; }
+  
+  // check existing GSL matrix, (re)allocate if needed 
+  if ( 0 != m_evec && ( D != m_evec->size1 || D != m_evec->size2 ) ) 
+  { gsl_matrix_free ( m_evec ) ; m_evec = 0 ; }
+  if ( 0 == m_evec     ) { m_evec = gsl_matrix_alloc   ( D , D ) ; }
+  if ( 0 == m_evec     ) { return StatusCode ( MatrixAllocationFailure    ) ; }
+  
+  // check the GSL vector, (re)allocate if needed 
+  if ( 0 != m_vector && D != m_vector->size )
+  { gsl_vector_free ( m_vector ) ; m_vector = 0 ; }
+  if ( 0 == m_vector   ) { m_vector = gsl_vector_alloc ( D ) ; }
+  if ( 0 == m_vector   ) { return StatusCode ( VectorAllocationFailure    ) ; }
+ 
+  return StatusCode::SUCCESS ;
+} ;
+// ============================================================================
+// find the eigenvalues   (& sort them if needed ) 
+// ============================================================================
+StatusCode Gaudi::Math::GSL::EigenSystem::_fun1 
+( const bool sorted ) const
+{
+  // check the working space, (re)allocate if needed  
+  if ( 0 != m_work1 && m_dim1 != m_vector->size ) 
+  { gsl_eigen_symm_free  ( m_work1 ) ; m_work1 = 0 ; }
+  if ( 0 == m_work1 ) 
+  { m_dim1 = m_vector->size ; m_work1 = gsl_eigen_symm_alloc ( m_dim1 ) ; ; }
+  if ( 0 == m_work1 ) { return StatusCode ( WorkspaceAllocationFailure ) ; }
+  
+  const int result = gsl_eigen_symm  ( m_matrix , m_vector , m_work1 ) ;
+  if ( result ) { return StatusCode ( ErrorFromGSL + result ) ; }
+  if ( sorted ) { gsl_sort_vector   ( m_vector )  ; }
+  return StatusCode::SUCCESS ;
+} ;
+// ============================================================================
+/// find the eigenvalues&eigenvectors (& sort them if needed ) 
+// ============================================================================
+StatusCode Gaudi::Math::GSL::EigenSystem::_fun2 
+( const bool sorted ) const
+{
+  // check the working space, (re)allocate if needed  
+  if ( 0 != m_work2 &&  m_dim2 != m_vector->size ) 
+  { gsl_eigen_symmv_free ( m_work2 ) ; m_work2 = 0 ; }  
+  if ( 0 == m_work2 ) 
+  { m_dim2 = m_vector->size ; m_work2 = gsl_eigen_symmv_alloc ( m_dim2 ) ; ; }
+  if ( 0 == m_work2 ) { return StatusCode ( WorkspaceAllocationFailure ) ; }
+  
+  const int result = gsl_eigen_symmv ( m_matrix , m_vector , m_evec , m_work2 ) ;
+  if ( result ) { return StatusCode ( ErrorFromGSL + result ) ; }
+  if ( sorted ) 
+  { gsl_eigen_symmv_sort ( m_vector , m_evec , GSL_EIGEN_SORT_VAL_ASC )  ; }
+
+  return StatusCode::SUCCESS ;
+} ;
+// ============================================================================
+// thrown the exception 
+// ============================================================================
+StatusCode Gaudi::Math::GSL::EigenSystem::Exception
+( const StatusCode& sc ) const 
+{
+  throw GaudiException
+    ("Error from Gaudi::Math::GSL::EigenSystem" , "*GSL*" , sc ) ;
+  return sc ;
+};
+// ============================================================================
+
+// ============================================================================
+// The END 
+// ============================================================================
+
+
