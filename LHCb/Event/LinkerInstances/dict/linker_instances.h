7171cfcd99c7cc4ea510200750a33dab7cbc456a
Thomas Ruf <thomas.ruf@cern.ch>
2006-06-08 15:35:13 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35058 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1afbc81a49ba8f7412d9940fbac01943fda45c6e

--- LHCb/Event/LinkerInstances/dict/linker_instances.h
+++ LHCb/Event/LinkerInstances/dict/linker_instances.h
@@ -2,6 +2,7 @@
 #include "Linker/LinkedFrom.h"
 
 #include "Event/MCParticle.h"
+#include "Event/MCHit.h"
 #include "Event/VeloCluster.h"
 #include "Event/STCluster.h"
 #include "Event/OTTime.h"
@@ -14,10 +15,12 @@ namespace {
   LinkedTo<LHCb::MCParticle,LHCb::OTTime>      i3 (0,0,"");
   LinkedTo<LHCb::MCParticle,LHCb::MuonCoord>   i4 (0,0,"");
   LinkedTo<LHCb::MCParticle,LHCb::Track>       i5 (0,0,"");
+  LinkedTo<LHCb::MCParticle,LHCb::MCHit>       i6 (0,0,"");
 
   LinkedFrom<LHCb::VeloCluster,LHCb::MCParticle> i11 (0,0,"");
   LinkedFrom<LHCb::STCluster,LHCb::MCParticle>   i12 (0,0,"");
   LinkedFrom<LHCb::OTTime,LHCb::MCParticle>      i13 (0,0,"");
   LinkedFrom<LHCb::MuonCoord,LHCb::MCParticle>   i14 (0,0,"");
   LinkedFrom<LHCb::Track,LHCb::MCParticle>       i15 (0,0,"");
+  LinkedFrom<LHCb::MCHit,LHCb::MCParticle>       i16 (0,0,"");
 }
