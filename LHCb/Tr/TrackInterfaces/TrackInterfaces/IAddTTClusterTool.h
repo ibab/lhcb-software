282f8629747451e36f2021d7ba834d3b8afa3887
Jeroen Van Tilburg <jeroen.van.tilburg@cern.ch>
2006-06-14 19:46:53 +0000

    Added IAddTTClusterTool interface.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@16161 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b44249eed81cc3101206027841b23c3e7e38393a

---
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IAddTTClusterTool.h
@@ -0,0 +1,40 @@
+// $Id: IAddTTClusterTool.h,v 1.1 2006-06-14 19:46:53 jvantilb Exp $
+#ifndef TRACKMATCHING_IADDTTCLUSTERTOOL_H
+#define TRACKMATCHING_IADDTTCLUSTERTOOL_H 1
+
+// Include files
+// -------------
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+static const InterfaceID IID_IAddTTClusterTool ( "IAddTTClusterTool", 1, 0 );
+
+/** @class IAddTTClusterTool IAddTTClusterTool.h  
+ *
+ *  @author:  Jeroen van Tilburg Jeroen.van.Tilburg@cern.nl
+ *  @date:    12-05-2006
+ */
+
+// forward declaration
+class LHCb::STCluster;
+
+class IAddTTClusterTool : virtual public IAlgTool {
+  
+public: 
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IAddTTClusterTool; }
+
+  /// Add TT clusters to matched tracks
+  virtual StatusCode addTTClusters( LHCb::Track& track ) = 0;
+
+  /// Add TT clusters to matched tracks
+  virtual StatusCode addTTClusters( LHCb::Track& track, 
+                                    std::vector<LHCb::STCluster*>& ttClusters,
+                                    std::vector<double>& ttChi2s ) = 0;
+
+  /// Get the distance to the strip
+  virtual double distanceToStrip( const LHCb::Track& track, 
+                                  const LHCb::STCluster& ttCluster ) = 0;
+
+};
+#endif // TRACKMATCHING_IADDTTCLUSTERTOOL_H
