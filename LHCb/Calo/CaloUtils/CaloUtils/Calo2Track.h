84512e15d4c9832f95229f6fa52501a1bc3b88f4
Olivier Deschamps <odescham@cern.ch>
2006-06-15 17:45:24 +0000

    improve Calo2Track.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39589 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d6578249709155a105419c45d329071a6c891311

--- LHCb/Calo/CaloUtils/CaloUtils/Calo2Track.h
+++ LHCb/Calo/CaloUtils/CaloUtils/Calo2Track.h
@@ -1,4 +1,4 @@
-// $Id: Calo2Track.h,v 1.1 2006-06-14 16:40:19 odescham Exp $
+// $Id: Calo2Track.h,v 1.2 2006-06-15 17:45:24 odescham Exp $
 #ifndef CALO2TRACK_H 
 #define CALO2TRACK_H 1
 
@@ -20,15 +20,30 @@ namespace LHCb{
  */
 namespace LHCb { 
   namespace CaloIdLocation {
+    // Calo-Track matching tables   : IRelationWeighted< CaloCluster/Hypo , Track , float > 
     const std::string ClusterMatch  = "Rec/Calo/ClusterMatch";
     const std::string PhotonMatch   = "Rec/Calo/PhotonMatch";
     const std::string BremMatch     = "Rec/Calo/BremMatch";
     const std::string ElectronMatch = "Rec/Calo/ElectronMatch";
+    // Acceptance tables       : IRelation< Track , bool  >
+    const std::string InSpd         =  "Rec/Calo/InAccSpd";
+    const std::string InPrs         =  "Rec/Calo/InAccPrs";
+    const std::string InBrem        =  "Rec/Calo/InAccBrem";
+    const std::string InEcal        =  "Rec/Calo/InAccEcal";
+    const std::string InHcal        =  "Rec/Calo/InAccHcal";
+        // Intermediate estimators : IRelation< Track , float >   
     const std::string PrsE          = "Rec/Calo/PrsE";
     const std::string EcalE         = "Rec/Calo/EcalE";
     const std::string HcalE         = "Rec/Calo/HcalE";
     const std::string EcalChi2      = "Rec/Calo/EcalChi2";
     const std::string BremChi2      = "Rec/Calo/BremChi2";
+    // CaloPID DLLs            : IRelation< Track , float >   
+    const std::string PrsPIDe       = "Rec/Calo/PrsPIDe";
+    const std::string EcalPIDe      = "Rec/Calo/EcalPIDe";
+    const std::string BremPIDe      = "Rec/Calo/BremPIDe";
+    const std::string HcalPIDe      = "Rec/Calo/HcalPIDe";
+    const std::string EcalPIDmu     = "Rec/Calo/EcalPIDmu";
+    const std::string HcalPIDmu     = "Rec/Calo/HcalPIDmu";
   };
   namespace Calo2Track {
     typedef IRelationWeighted< LHCb::CaloCluster , LHCb::Track , float >             IClusTrTable;
@@ -36,6 +51,7 @@ namespace LHCb {
     typedef IRelationWeighted< LHCb::CaloHypo , LHCb::Track , float >                IHypoTrTable;
     typedef IRelationWeighted< LHCb::Track , LHCb::CaloHypo , float >                ITrHypoTable;
     typedef IRelation< LHCb::Track , float >                                         ITrEvalTable;
+    typedef IRelation< LHCb::Track , bool  >                                         ITrAccTable;
   };   
 };
 
