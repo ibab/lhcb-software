0855e0dbc6b15b8cdf5a256dada70511475d2de1
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-06-19 19:25:01 +0000

    more dicts
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47409 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d71888f74fffbe13d0e34ddaff543b573a051443

--- LHCb/Kernel/LHCbKernel/Kernel/lcgStl.h
+++ LHCb/Kernel/LHCbKernel/Kernel/lcgStl.h
@@ -1,4 +1,4 @@
-// $Id: lcgStl.h,v 1.21 2006-06-19 16:35:10 cattanem Exp $
+// $Id: lcgStl.h,v 1.22 2006-06-19 19:25:01 cattanem Exp $
 #ifndef KERNEL_LCGSTL_H 
 #define KERNEL_LCGSTL_H 1
 
@@ -37,7 +37,9 @@ namespace {
     std::auto_ptr<LHCb::Trajectory>                  m_auto_ptr_Trajectory;
     GaudiUtils::VectorMap<int,double>                m_vector_map_int_double;
     ROOT::Math::RowOffsets<5>                        m_row_offsets5;
+    ROOT::Math::RowOffsets<4>                        m_row_offsets4;
     ROOT::Math::RowOffsets<3>                        m_row_offsets3;
+    ROOT::Math::RowOffsets<2>                        m_row_offsets2;
     // end instantiations
   };
 }
