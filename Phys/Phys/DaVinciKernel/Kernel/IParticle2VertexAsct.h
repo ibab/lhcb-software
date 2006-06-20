7401a266ff50592afb246193cc8ca25efc016a3f
jpalac <jpalac@cern.ch>
2006-06-20 05:58:50 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63214 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 913e09d379e80020e341f969b3b8d74d8fac4375

--- Phys/Phys/DaVinciKernel/Kernel/IParticle2VertexAsct.h
+++ Phys/Phys/DaVinciKernel/Kernel/IParticle2VertexAsct.h
@@ -1,4 +1,4 @@
-// $Id: IParticle2VertexAsct.h,v 1.4 2006-06-20 05:55:52 jpalac Exp $
+// $Id: IParticle2VertexAsct.h,v 1.5 2006-06-20 05:58:50 jpalac Exp $
 #ifndef DAVINCIKERNEL_IPARTICLE2VERTEXASCT_H
 #define DAVINCIKERNEL_IPARTICLE2VERTEXASCT_H 1
 
@@ -8,7 +8,6 @@
 // from LHCb
 #include "Event/Particle.h"
 #include "Event/Vertex.h"
-#include "Event/PrimVertex.h"
 #include "Relations/RelationWeighted1D.h"
 
 class IGeomDispCalculator;
@@ -27,58 +26,32 @@ class IParticle2VertexAsct : virtual public IAlgTool
 
 public:
 
-  typedef LHCb::RelationWeighted1D<LHCb::Particle,
-                                   LHCb::PrimVertex,double> TablePV;
-  typedef LHCb::RelationWeighted1D<LHCb::Particle,
-                                   LHCb::Vertex,double> TableV;
+  typedef LHCb::RelationWeighted1D<LHCb::Particle,LHCb::Vertex,double> Table;
 
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IParticle2VertexAsct; }
 
-  // Return the relation table linking Particles to PrimVertices
-  virtual TablePV table(const LHCb::Particle::Container& particles,
-                        const LHCb::PrimVertex::Container& vertices,
-                        const IGeomDispCalculator* pIPTool) const = 0;
+  // Return the relation table linking particles to PVs
+  virtual Table table(const LHCb::Particle::Container& particles,
+                      const LHCb::Vertex::Container& vertices,
+                      const IGeomDispCalculator* pIPTool) const = 0;
 
-  virtual TablePV table(const LHCb::Particle::ConstVector& particles,
-                        const LHCb::PrimVertex::ConstVector& vertices,
-                        const IGeomDispCalculator* pIPTool) const = 0;
-
-
-  virtual TablePV table(const LHCb::Particle::Container::const_iterator pBegin,
-                        const LHCb::Particle::Container::const_iterator pEnv,
-                        const LHCb::PrimVertex::Container::const_iterator vBegin,
-                        const LHCb::PrimVertex::Container::const_iterator vEnd,
-                        const IGeomDispCalculator* pIPTool) const = 0;
-
-  virtual TablePV table(const LHCb::Particle::ConstVector::const_iterator pBegin,
-                        const LHCb::Particle::ConstVector::const_iterator pEnv,
-                        const LHCb::PrimVertex::ConstVector::const_iterator vBegin,
-                        const LHCb::PrimVertex::ConstVector::const_iterator vEnd,
-                        const IGeomDispCalculator* pIPTool) const = 0;
-
-  // Return the relation table linking Particles to Vertices
-  virtual TableV table(const LHCb::Particle::Container& particles,
-                       const LHCb::Vertex::Container& vertices,
-                       const IGeomDispCalculator* pIPTool) const = 0;
-
-  virtual TableV table(const LHCb::Particle::ConstVector& particles,
-                       const LHCb::Vertex::ConstVector& vertices,
-                       const IGeomDispCalculator* pIPTool) const = 0;
+  virtual Table table(const LHCb::Particle::ConstVector& particles,
+                      const LHCb::Vertex::ConstVector& vertices,
+                      const IGeomDispCalculator* pIPTool) const = 0;
 
 
-  virtual TableV table(const LHCb::Particle::Container::const_iterator pBegin,
-                       const LHCb::Particle::Container::const_iterator pEnv,
-                       const LHCb::Vertex::Container::const_iterator   vBegin,
-                       const LHCb::Vertex::Container::const_iterator   vEnd,
-                       const IGeomDispCalculator* pIPTool) const = 0;
+  virtual Table table(const LHCb::Particle::Container::const_iterator pBegin,
+                      const LHCb::Particle::Container::const_iterator pEnv,
+                      const LHCb::Vertex::Container::const_iterator   vBegin,
+                      const LHCb::Vertex::Container::const_iterator   vEnd,
+                      const IGeomDispCalculator* pIPTool) const = 0;
 
-  virtual TableV table(const LHCb::Particle::ConstVector::const_iterator pBegin,
-                       const LHCb::Particle::ConstVector::const_iterator pEnv,
-                       const LHCb::Vertex::ConstVector::const_iterator   vBegin,
-                       const LHCb::Vertex::ConstVector::const_iterator   vEnd,
-                       const IGeomDispCalculator* pIPTool) const = 0;
-
+  virtual Table table(const LHCb::Particle::ConstVector::const_iterator pBegin,
+                      const LHCb::Particle::ConstVector::const_iterator pEnv,
+                      const LHCb::Vertex::ConstVector::const_iterator   vBegin,
+                      const LHCb::Vertex::ConstVector::const_iterator   vEnd,
+                      const IGeomDispCalculator* pIPTool) const = 0;
 
 };
 #endif // DAVINCIKERNEL_IPARTICLE2VERTEXASCT_H
---
+++ Phys/Phys/DaVinciKernel/Kernel/StringUtils.h
@@ -0,0 +1,42 @@
+// $Id: StringUtils.h,v 1.1 2006-06-20 05:55:52 jpalac Exp $
+#ifndef KERNEL_STRINGUTILS_H 
+#define KERNEL_STRINGUTILS_H 1
+
+// Include files
+
+#include <string>
+
+/** @file StringUtils StringUtils.h Kernel/StringUtils.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2006-06-19
+ */
+namespace DaVinci 
+{
+  namespace StringUtils 
+  {
+    /** Strip particle name down to basic type.
+     *  Strips off any "+", "-", "~", "anti-" from an std::string.
+     *
+     *  Example: mu+ -> mu, Delta++ -> Delta, anti-nu_tau -> nu_tau
+     *
+     *  @code
+     *  std::string myParticleName("tau+");
+     *  DaVinci::StringUtils::stripParticleName(myParticleName);
+     *  @endcode
+     *
+     *  @param name (input) string to be stripped.
+    */
+
+    void stripParticleName(std::string& name)
+    {
+      if ( 0 == name.find("anti-") ) { name.erase(0,5); }
+      std::string::size_type pos = name.find_first_of("+-~") ;
+      if ( std::string::npos == pos ) { return; }
+      stripParticleName( name.erase( pos , 1 ) ) ;  
+    }
+
+  }
+}
+#endif // KERNEL_STRINGUTILS_H
