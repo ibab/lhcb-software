371a791acffa03e712950c0da07e80cae9fd7f69
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-06-21 13:40:02 +0000

    update enums
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@8038 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7b9e1653c147e08279d595d8deea869cd7f629a

--- LHCb/Rich/RichRecBase/src/base/RichTrackID.cpp
+++ LHCb/Rich/RichRecBase/src/base/RichTrackID.cpp
@@ -5,7 +5,7 @@
  *  Implementation file for class : RichTrackID
  *
  *  CVS Log :-
- *  $Id: RichTrackID.cpp,v 1.16 2006-05-09 09:25:02 jonrob Exp $
+ *  $Id: RichTrackID.cpp,v 1.17 2006-06-21 13:37:52 jonrob Exp $
  *
  *  @author Chris Jones    Christopher.Rob.Jones@cern.ch
  *  @date   2003-09-23
@@ -84,14 +84,14 @@ Rich::Track::Type Rich::Track::type( const LHCb::Track * track )
     // track algorithm type
     const LHCb::Track::History hist = (LHCb::Track::History)track->history();
     // check all known track types (order according to abundance)
-    if      ( LHCb::Track::PatForward  == hist )  { return Rich::Track::Forward;  }
-    // Old converted track types (removed)
-    //else if ( LHCb::Track::CnvForward  == hist )  { return Rich::Track::Forward;  }
-    //else if ( LHCb::Track::CnvMatch    == hist )  { return Rich::Track::Match;    }
-    //else if ( LHCb::Track::CnvSeed     == hist )  { return Rich::Track::Seed;     }
+    if      ( LHCb::Track::PatForward    == hist )  { return Rich::Track::Forward;  }
+    else if ( LHCb::Track::TrackMatching == hist )  { return Rich::Track::Match;    }
+    else if ( LHCb::Track::TsaTrack      == hist )  { return Rich::Track::Seed;     }
+    else if ( LHCb::Track::TrackSeeding  == hist )  { return Rich::Track::Seed;     }
     //else if ( LHCb::Track::CnvKsTrack  == hist )  { return Rich::Track::KsTrack;  }
-    //else if ( LHCb::Track::CnvVeloTT   == hist )  { return Rich::Track::VeloTT;   }
-    //else if ( LHCb::Track::CnvVelo     == hist )  { return Rich::Track::Velo;     }
+    else if ( LHCb::Track::TrackVeloTT   == hist )  { return Rich::Track::VeloTT;   }
+    else if ( LHCb::Track::PatVeloTT     == hist )  { return Rich::Track::VeloTT;   }
+    else if ( LHCb::Track::PatVelo       == hist )  { return Rich::Track::Velo;     }
     //else if ( LHCb::Track::CnvVeloBack == hist )  { return Rich::Track::Unusable; }
     else 
     { // Should not get here ...
