8c32578c9011c65df6081906be2ac133f2806148
Gloria Corti <gloria.corti@cern.ch>
2006-07-04 11:59:40 +0000

    follow _load, _dll convention
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92580 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fa449d95a5d5b39e33c06bcfcc386a31c15f0e5f

---
+++ Gauss/Gen/LbBcVegPy/src/component/LbBcVegPy_dll.cpp
@@ -0,0 +1,8 @@
+// $Id: LbBcVegPy_dll.cpp,v 1.1 2006-07-04 11:59:40 gcorti Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+
+LOAD_FACTORY_ENTRIES(LbBcVegPy)
+
--- Gauss/Gen/LbBcVegPy/src/component/LbBcVegPy_load.cpp
+++ Gauss/Gen/LbBcVegPy/src/component/LbBcVegPy_load.cpp
@@ -1,5 +1,22 @@
-// $Id: LbBcVegPy_load.cpp,v 1.1.1.1 2006-04-24 21:45:50 robbep Exp $
-#include "GaudiKernel/LoadFactoryEntries.h"
+// $Id: LbBcVegPy_load.cpp,v 1.2 2006-07-04 11:59:40 gcorti Exp $
+// Include files 
 
-LOAD_FACTORY_ENTRIES( LbBcVegPy )
 
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+// The statements are like that:
+//
+// DECLARE_ALGORITHM( MyAlgorithm );
+// DECLARE_TOOL( MyTool );
+// DECLARE_OBJECT( DataObject );
+//
+// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
+
+DECLARE_FACTORY_ENTRIES(LbBcVegPy) {
+  // New structure:
+ 
+  // Production tools
+  DECLARE_TOOL( BcVegPyProduction ) ;
+ 
+}
