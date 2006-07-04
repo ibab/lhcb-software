7f66657371f66999ad2b801bedfef6f178f532fb
Gloria Corti <gloria.corti@cern.ch>
2006-07-04 11:32:56 +0000

    follow _load, _dll convention
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92575 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f7b69fbe781f61a1b071ecb37a72c2cee3d5d121

---
+++ Gauss/Gen/LbPythia/src/component/LbPythia_dll.cpp
@@ -0,0 +1,8 @@
+// $Id: LbPythia_dll.cpp,v 1.1 2006-07-04 11:32:56 gcorti Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+
+LOAD_FACTORY_ENTRIES(LbPythia)
+
--- Gauss/Gen/LbPythia/src/component/LbPythia_load.cpp
+++ Gauss/Gen/LbPythia/src/component/LbPythia_load.cpp
@@ -1,5 +1,31 @@
-// $Id: LbPythia_load.cpp,v 1.1.1.1 2005-06-21 12:40:51 robbep Exp $
-#include "GaudiKernel/LoadFactoryEntries.h"
+// $Id: LbPythia_load.cpp,v 1.2 2006-07-04 11:32:56 gcorti Exp $
+// Include files 
 
-LOAD_FACTORY_ENTRIES( LbPythia )
+//
+//  Package    : Gen/LbPythia
+//
+//  Description: Declaration of the components (factory entries)
+//               specified by this component library.
+//
 
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+// The statements are like that:
+//
+// DECLARE_ALGORITHM( MyAlgorithm );
+// DECLARE_TOOL( MyTool );
+// DECLARE_OBJECT( DataObject );
+//
+// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
+
+DECLARE_FACTORY_ENTRIES(LbPythia) {
+  // New structure:
+ 
+  // Production tools
+  DECLARE_TOOL( PythiaProduction ) ;
+ 
+  // Cut tools
+  DECLARE_TOOL( PythiaHiggs ) ;
+  
+}
