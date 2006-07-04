b6d15d49a2c636aeb2605ffd9a0c79b391ebf559
Gloria Corti <gloria.corti@cern.ch>
2006-07-04 16:57:17 +0000

    remove ^M
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92588 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef2d34ba8ea2f87f1b3cf7f0ea78412193b7cc66

--- Gauss/Sim/GaussCalo/src/EcalSensDet.h
+++ Gauss/Sim/GaussCalo/src/EcalSensDet.h
@@ -1,8 +1,11 @@
-// $Id: EcalSensDet.h,v 1.7 2006-06-24 16:23:45 ibelyaev Exp $ 
+// $Id: EcalSensDet.h,v 1.8 2006-07-04 16:57:17 gcorti Exp $ 
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.7  2006/06/24 16:23:45  ibelyaev
+//  many minor fixes
+//
 // Revision 1.6  2006/01/17 15:52:57  odescham
 // v8r0 - Adapt to new Event Model & LHCb v20 migration
 //
