2c16852aefc5e5787db9191073f19f7052c0bb8d
Gloria Corti <gloria.corti@cern.ch>
2006-07-04 10:09:37 +0000

    replaces _entries
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92572 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d71f47114686d384420af1696e70ee06235b50b2

--- Gauss/Gen/Generators/src/component/Generators_load.cpp
+++ Gauss/Gen/Generators/src/component/Generators_load.cpp
@@ -1,5 +1,61 @@
-// $Id: Generators_load.cpp,v 1.1.1.1 2005-06-20 21:42:17 robbep Exp $
-#include "GaudiKernel/LoadFactoryEntries.h"
+// $Id: Generators_load.cpp,v 1.2 2006-07-04 10:09:37 gcorti Exp $
+// Include files 
 
-LOAD_FACTORY_ENTRIES(Generators)
+//
+//  Package    : Gen/Generators
+//
+//  Description: Declaration of the components (factory entries)
+//               specified by this component library.
+//
 
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+// The statements are like that:
+//
+// DECLARE_ALGORITHM( MyAlgorithm );
+// DECLARE_TOOL( MyTool );
+// DECLARE_OBJECT( DataObject );
+//
+// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
+
+DECLARE_FACTORY_ENTRIES(Generators) {
+  
+  DECLARE_ALGORITHM( DumpMC );
+  DECLARE_ALGORITHM( DumpMCDecay );
+
+  // New structure:
+ 
+  // Main algorithm
+  DECLARE_ALGORITHM( Generation ) ;
+ 
+  // Luminosity Tools
+  DECLARE_TOOL( VariableLuminosity ) ;
+  DECLARE_TOOL( FixedLuminosity ) ;
+  DECLARE_TOOL( FixedNInteractions ) ;
+ 
+  // Sample Generation tools
+  DECLARE_TOOL( MinimumBias ) ;
+  DECLARE_TOOL( Inclusive ) ;
+  DECLARE_TOOL( SignalPlain ) ;
+  DECLARE_TOOL( SignalRepeatedHadronization ) ;
+  DECLARE_TOOL( SignalForcedFragmentation ) ;
+  DECLARE_TOOL( Special ) ;
+  DECLARE_TOOL( StandAloneDecayTool ) ;
+                                                                                                                 
+  // Beam Tools
+  DECLARE_TOOL( CollidingBeams ) ;
+  DECLARE_TOOL( FixedTarget ) ;
+ 
+  // Decay Tools
+  DECLARE_TOOL( EvtGenDecay ) ;
+ 
+  // Cut Tools
+  DECLARE_TOOL( LHCbAcceptance ) ;
+  DECLARE_TOOL( BiasedBB       ) ;
+ 
+  // Full event cut tools
+  DECLARE_TOOL( LeptonInAcceptance ) ; 
+  DECLARE_TOOL( MuXMaxBias         ) ;
+
+}
