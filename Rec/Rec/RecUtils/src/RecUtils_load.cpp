5d34955124c9b915f6c20a05258f5fa27fac6de0
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-07-07 15:10:38 +0000

    for DLL....
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@29525 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ec1eeb66564713a5582fd17494132e29703b97d4

---
+++ Rec/Rec/RecUtils/src/RecUtils_load.cpp
@@ -0,0 +1,12 @@
+// $Id: RecUtils_load.cpp,v 1.1 2006-07-07 15:10:38 cattanem Exp $
+// Include files 
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+
+DECLARE_FACTORY_ENTRIES(RecUtils) {
+
+  DECLARE_ALGORITHM( ReadStripETC );
+
+}
