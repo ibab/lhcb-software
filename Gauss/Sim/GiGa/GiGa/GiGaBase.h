3f086de6fb52029d2f83ce4a22712f2b9a22f78d
Gloria Corti <gloria.corti@cern.ch>
2006-07-27 09:34:30 +0000

    clean up of comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92606 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ce42a920c5ba3cce73d5cddfc94b4ff94e9d2219

--- Gauss/Sim/GiGa/GiGa/GiGaBase.h
+++ Gauss/Sim/GiGa/GiGa/GiGaBase.h
@@ -1,39 +1,27 @@
-// $Id: GiGaBase.h,v 1.22 2004-08-02 13:13:46 gcorti Exp $
-// ============================================================================
-// CVS tag $Name: not supported by cvs2svn $ 
-// ============================================================================
-// $Log: not supported by cvs2svn $
-// Revision 1.21  2004/02/20 18:13:34  ibelyaev
-//  major update in GiGaBase and GiGaTrajectory
-//
-// ============================================================================
-#ifndef     GIGA_GIGABASE_H
-#define     GIGA_GIGABASE_H 1 
-// ============================================================================
-// STD & STL
-// ============================================================================
+// $Id: GiGaBase.h,v 1.23 2006-07-27 09:34:30 gcorti Exp $
+#ifndef GIGA_GIGABASE_H
+#define GIGA_GIGABASE_H 1 
+
+// Include files
+// from STL
 #include <string>
 #include <exception>
 #include <map>
-// ============================================================================
-// GaudiKernel 
-// ============================================================================
+
+// from Gaudi 
 #include "GaudiKernel/DataObject.h"
 #include "GaudiKernel/IIncidentListener.h"
 #include "GaudiKernel/IToolSvc.h"
 #include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/System.h"
 #include "GaudiKernel/SmartDataPtr.h"
-// ============================================================================
-// GaudiTools 
-// ============================================================================
 #include "GaudiAlg/GaudiTool.h"
-// ============================================================================
-// GiGa
-// ============================================================================
+
+// from GiGa
 #include "GiGa/IGiGaInterface.h"
 #include "GiGa/GiGaFactory.h"
-// forward declarations 
+
+// Forward declarations 
 class IMessageSvc          ;
 class ISvcLocator          ;
 class IChronoStatSvc       ;
@@ -59,10 +47,9 @@ class GaudiException       ;
  *  @date    23/01/2001
  */
 
-class  GiGaBase:
-  public virtual IGiGaInterface    , 
-  public virtual IIncidentListener , 
-  public                 GaudiTool 
+class  GiGaBase: public virtual IGiGaInterface   , 
+                 public virtual IIncidentListener, 
+                 public         GaudiTool 
 {
 protected:
   
@@ -76,6 +63,7 @@ protected:
   GiGaBase( const std::string& type   ,
             const std::string& name   , 
             const IInterface*  parent );
+
   /// virtual destructor 
   virtual ~GiGaBase();
 
@@ -87,7 +75,7 @@ public:
    *  @see  IAlgTool
    *  @return status code 
    */
-  virtual StatusCode initialize() ;
+  virtual StatusCode initialize();
 
   /** finalize the object 
    *  @see GaudiTool
@@ -95,59 +83,40 @@ public:
    *  @see  IAlgTool
    *  @return status code 
    */
-  virtual StatusCode finalize  () ;
+  virtual StatusCode finalize();
   
   /** handle the incident
    *  @param i reference to the incident
    */ 
-  virtual void handle( const Incident& i ) ;
-  /// 
+  virtual void handle( const Incident& i );
+
 public:
   
   /** accessor to GiGa Service 
    *  @return pointer to GiGa Service 
    */
-  inline IGiGaSvc*         gigaSvc   () const { return m_gigaSvc   ; }; 
+  inline IGiGaSvc* gigaSvc() const { return m_gigaSvc; }; 
   
   /** accessor to GiGa SetUp Service 
    *  @return pointer to GiGa SetUp Service 
    */
-  inline IGiGaSetUpSvc*    setupSvc  () const { return m_setupSvc  ; };
+  inline IGiGaSetUpSvc* setupSvc() const { return m_setupSvc; };
 
 private: 
   
-  // default constructor    is disabled 
-  GiGaBase();                              ///< no default 
-  // copy    constructor    is disabled 
-  GiGaBase           ( const GiGaBase& );  ///< no copy 
-  // assigenement  operator is disabled 
-  GiGaBase& operator=( const GiGaBase& );  ///< no assignment
+  GiGaBase();                              ///< no default constructor
+  GiGaBase           ( const GiGaBase& );  ///< no copy constructor 
+  GiGaBase& operator=( const GiGaBase& );  ///< no assignment operator
   
 private:
-  // name of GiGa Service   
-  std::string           m_gigaName   ; 
-  // name of GiGa SetUp Service   
-  std::string           m_setupName  ; 
-  // pointer to GiGa Service 
-  IGiGaSvc*             m_gigaSvc    ;
-  // pointer to GiGa SetUp Service 
-  IGiGaSetUpSvc*        m_setupSvc   ;
-private:
+
+  std::string    m_gigaName;        ///< Name of GiGa service
+  std::string    m_setupName;       ///< Name of GiGa SetUp Service 
+  IGiGaSvc*      m_gigaSvc;         ///< Pointer to GiGa Service
+  IGiGaSetUpSvc* m_setupSvc;        ///< Pointer to GiGa SetUp Service
+
 };
-// ============================================================================
 
-// ============================================================================
+
 #endif ///<   GIGA_GIGABASE_H
-// ============================================================================
-
-
-
-
-
-
-
-
-
-
-
 
