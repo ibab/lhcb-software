d5c254c331aacdd714f659fa6f1fe010876e48c1
Gloria Corti <gloria.corti@cern.ch>
2006-07-27 09:33:00 +0000

    move initialization of properties
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92603 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4f4f735d1bbbe4834b225ef3b833170d4612708

--- Gauss/Sim/GiGa/src/Lib/GiGaBase.cpp
+++ Gauss/Sim/GiGa/src/Lib/GiGaBase.cpp
@@ -1,92 +1,52 @@
-// $Id: GiGaBase.cpp,v 1.23 2005-12-09 17:20:22 gcorti Exp $
-// ============================================================================
-// CVS tag $Name: not supported by cvs2svn $ 
-// ============================================================================
-// $Log: not supported by cvs2svn $
-// Revision 1.22  2004/02/20 18:13:35  ibelyaev
-//  major update in GiGaBase and GiGaTrajectory
-//
-// Revision 1.21  2003/09/22 13:57:10  ibelyaev
-//  polishing of addRef/release/releaseTools/finalize
-//
-// ===========================================================================
+// $Id: GiGaBase.cpp,v 1.24 2006-07-27 09:33:00 gcorti Exp $
 #define GIGA_GIGABASE_CPP 1 
-// ============================================================================
-/// includes 
-///  STL
+
+// includes 
+// from STL
 #include <string>
 #include <map>
-///  GaudiKernel
-#include "GaudiKernel/ISvcLocator.h" 
-#include "GaudiKernel/IProperty.h"
-#include "GaudiKernel/ISerialize.h"
-#include "GaudiKernel/IIncidentListener.h"
-#include "GaudiKernel/IMessageSvc.h" 
-#include "GaudiKernel/IChronoStatSvc.h" 
-#include "GaudiKernel/IDataProviderSvc.h" 
-#include "GaudiKernel/IParticlePropertySvc.h" 
-#include "GaudiKernel/IMagneticFieldSvc.h" 
-#include "GaudiKernel/IIncidentSvc.h"
-#include "GaudiKernel/IJobOptionsSvc.h"
-///  GaudiKernel
-#include "GaudiKernel/PropertyMgr.h"
-#include "GaudiKernel/Property.h"
-#include "GaudiKernel/MsgStream.h"
-#include "GaudiKernel/StreamBuffer.h"
-#include "GaudiKernel/Stat.h"
-///  from GiGa 
+
+// from GiGa 
 #include "GiGa/IGiGaSvc.h"
 #include "GiGa/IGiGaSetUpSvc.h"
 #include "GiGa/GiGaException.h"
 #include "GiGa/GiGaUtil.h" 
 #include "GiGa/GiGaBase.h" 
 
-// ============================================================================
-/** standard constructor 
- *  @see AlgTool 
- *  @param type tool   type (?)  
- *  @param Name object name 
- *  @param parent point to parent object 
- */
-// ============================================================================
-GiGaBase::GiGaBase
-( const std::string& type   ,
-  const std::string& Name   ,
-  const IInterface*  parent )
+//=============================================================================
+// Standard constructor 
+//=============================================================================
+GiGaBase::GiGaBase( const std::string& type,
+                    const std::string& Name,
+                    const IInterface* parent )
   : GaudiTool( type , Name , parent )
-  ///
-  , m_gigaName   ( "GiGa"                ) 
-  , m_setupName  ( "GiGa"                ) 
-  ///
-  , m_gigaSvc    ( 0                     ) 
-  , m_setupSvc   ( 0                     ) 
+  , m_gigaSvc( 0 ) 
+  , m_setupSvc( 0 ) 
 {
-  ///
+
   if( 0 == svcLoc() ) 
   { throw GiGaException("GiGaBase():: ISvcLocator* points to NULL!") ; }
-  ///
+
+  /// Interfaces
   declareInterface<IGiGaInterface>    ( this );
   declareInterface<IIncidentListener> ( this );
-  ///
-  declareProperty( "GiGaService"               ,  m_gigaName    ) ;
-  declareProperty( "GiGaSetUpService"          ,  m_setupName   ) ;
-  ///
-};
-// ============================================================================
 
-// ============================================================================
+  /// Properties
+  declareProperty( "GiGaService"     , m_gigaName  = "GiGa" );
+  declareProperty( "GiGaSetUpService", m_setupName = "GiGa" );
+
+}
+
+
+//=============================================================================
 // destructor 
-// ============================================================================
-GiGaBase::~GiGaBase() {};
-// ============================================================================
+//=============================================================================
+GiGaBase::~GiGaBase() {}
 
-// ============================================================================
-/** initialize the object
- *  @see  AlgTool
- *  @see IAlgTool 
- *  @return status code 
- */
-// ============================================================================
+
+//=============================================================================
+// initialize the object (Gaudi)
+//=============================================================================
 StatusCode GiGaBase::initialize() 
 {
   // initialize the base class 
@@ -96,7 +56,7 @@ StatusCode GiGaBase::initialize()
   }
 
   // Check that can locate Services
-  if( 0 == svcLoc()     ) {
+  if( 0 == svcLoc() ) {
     throw GiGaException("GiGaBase::ini ISvcLocator* points to NULL!");
   }
 
@@ -111,9 +71,8 @@ StatusCode GiGaBase::initialize()
   // If the setup service is the same, just get the interface
   if( !m_setupName.empty() ) { 
     if( m_setupName == m_gigaName ) {
-      m_setupSvc = dynamic_cast<IGiGaSetUpSvc*>( m_gigaSvc );
-      
       // cast to correct type
+      m_setupSvc = dynamic_cast<IGiGaSetUpSvc*>( m_gigaSvc ); 
     }
     else {
       m_setupSvc = svc<IGiGaSetUpSvc> ( m_setupName , true );
@@ -122,36 +81,30 @@ StatusCode GiGaBase::initialize()
   else { 
     Warning("GiGaSetUp Service is not requested to be located");
   }
-  ///
-  return StatusCode::SUCCESS ;
-};
-// ============================================================================
 
-// ============================================================================
-/** finalize the object 
- *  @see  AlgTool
- *  @see IAlgTool 
- *  @return status code 
- */
-// ============================================================================
-StatusCode GiGaBase::finalize() { return GaudiTool::finalize() ; };
-// ============================================================================
+  return StatusCode::SUCCESS;
+}
 
-// ============================================================================
-/** handle the incident
- *  @param incident reference to the incident
- */
-// ============================================================================
+
+//=============================================================================
+// finalize the object (Gaudi)
+//=============================================================================
+StatusCode GiGaBase::finalize() { 
+  return GaudiTool::finalize();
+}
+
+
+//=============================================================================
+// handle the incident
+//=============================================================================
 void  GiGaBase::handle( const Incident& incident ) 
 { 
   Print ( " Incident = '"  + incident .type   () + "' " + 
           " \tsource = '"  + incident .source () + "' " ) ; 
-};
-// ============================================================================
+}
 
-// ============================================================================
-// The END 
-// ============================================================================
+
+//=============================================================================
 
 
 
