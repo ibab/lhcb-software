3bd885ff043d0b5223edb67ec346db40e328407b
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-08-02 13:01:23 +0000

    added new file StateParameters.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35232 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5c386f3071b959911ea4b9f4c73eb1e037a61f2c

---
+++ LHCb/Event/TrackEvent/Event/StateParameters.h
@@ -0,0 +1,33 @@
+#ifndef TRACKEVENT_STATEPARAMETERS_H
+#define TRACKEVENT_STATEPARAMETERS_H 1
+
+// Include files
+#include "GaudiKernel/PhysicalConstants.h"
+
+/** @namespace StateParameters
+ *
+ * This namespace defines some useful variables for the
+ * manipulation of States.
+ *
+ *  @author Eduardo Rodrigues
+ *  @date   2006-08-02
+ * 
+ */
+
+namespace StateParameters
+{
+  /// Pre-defined z-positions of states for certain State locations
+  /// (see Location enum in State.h)
+  static const double ZBegRich1 =   990. * Gaudi::Units::mm;
+  static const double ZEndRich1 =  2165. * Gaudi::Units::mm;
+  static const double ZAtT      =  9410. * Gaudi::Units::mm;
+  static const double ZBegRich2 =  9450. * Gaudi::Units::mm;
+  static const double ZEndRich2 = 11900. * Gaudi::Units::mm;
+  
+};
+
+// ----------------------------------------------------------------------------
+//   end of namespace
+// ----------------------------------------------------------------------------
+
+#endif /// TRACKEVENT_STATEPARAMETERS_H
