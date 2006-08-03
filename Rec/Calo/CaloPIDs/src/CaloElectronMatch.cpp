ec800d5f86dbb75f9857bf096202bfc6f4500a45
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-08-03 08:45:59 +0000

    Add missing data initialisations
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@39644 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a874109ac565138b01232ff0a9003190c768ab69

--- Rec/Calo/CaloPIDs/src/CaloElectronMatch.cpp
+++ Rec/Calo/CaloPIDs/src/CaloElectronMatch.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloElectronMatch.cpp,v 1.1 2006-06-18 18:35:27 ibelyaev Exp $
+// $Id: CaloElectronMatch.cpp,v 1.2 2006-08-03 08:45:59 jonrob Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $ 
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.2 $ 
 // ============================================================================
-// $Log: not supported by cvs2svn $  
+// $Log: not supported by cvs2svn $
+// Revision 1.1  2006/06/18 18:35:27  ibelyaev
+//  the firstcommmit for DC06 branch
+//  
 // ============================================================================
 // Include files
 // ============================================================================
@@ -88,6 +91,11 @@ protected:
     const IInterface*  parent ) 
     : CaloTrackMatch  ( type , name , parent ) 
     //
+    , m_position ( NULL ) 
+    , m_caloMatch  () 
+    , m_trackMatch ()
+    , m_plane      ()  
+    , m_showerMax  ()
     , m_cBad ( 0 ) 
     , m_tBad ( 0 )
   {
