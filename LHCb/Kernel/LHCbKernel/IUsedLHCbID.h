b1f074c45f24ee7dd506577571cc72455cdf5c77
Matthew David Needham <mneedham@cern.ch>
2006-08-17 09:34:37 +0000

    add missed file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47472 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 75b1f26b0ad103551f2340234e3a76bf8622acaf

---
+++ LHCb/Kernel/LHCbKernel/IUsedLHCbID.h
@@ -0,0 +1,36 @@
+// $Id: IUsedLHCbID.h,v 1.1 2006-08-17 09:34:37 mneedham Exp $ 
+#ifndef _IUsedLHCbID_H
+#define _IUsedLHCbID_H
+ 
+#include "GaudiKernel/IAlgTool.h"
+ 
+static const InterfaceID IID_IUsedLHCbID( "IUsedLHCbID", 0, 0 );
+ 
+/** @class IUsedLHCbIDs 
+ *
+ *  interface for testing ida given id is used. 
+ *
+ *  @author M.Needham
+ *  @date   02/08/2006
+ */
+
+namespace LHCb{
+ class LHCbID;
+};
+  
+class IUsedLHCbID: virtual public IAlgTool{
+ 
+public:
+                                                                                
+  /** Retrieve interface ID */
+  static const InterfaceID& interfaceID() { return IID_IUsedLHCbID ; }                                                                                                
+
+  /** Test if the LHCbID is used 
+  * @param id to be test tested
+  *  @return true if used
+  */  
+  virtual bool used(const LHCb::LHCbID id) const = 0;
+                                                                                
+};
+                                                                                
+#endif
