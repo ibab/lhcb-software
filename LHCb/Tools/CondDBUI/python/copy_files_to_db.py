07687396532f0a55e1be78acfe92b36b4d8f3309
Marco Clemencic <marcocle@cern.ch>
2006-08-22 08:39:17 +0000

    added the copy_files_to_db.py script
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@3987 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4ba1016df9dbe0f27487ed7988d38d414adb557c

---
+++ LHCb/Tools/CondDBUI/python/copy_files_to_db.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import sys
+
+_tmp_argv = sys.argv
+sys.argv = sys.argv[0:1]
+from PyCool import cool, coral
+sys.argv = _tmp_argv
+
+import conddbui
+
+from optparse import OptionParser
+import os
+import re
+
+sysIdRE = re.compile('SYSTEM[^>"\']*("[^">]*"|'+"'[^'>]*')")
+def fix_system_ids(xml_data,path):
+    data = xml_data
+    m = sysIdRE.search(data)
+    while m != None:
+        pos = m.start()
+        s = m.start(1)+1
+        e = m.end(1)-1
+        p = os.path.join(path,data[s:e])
+        p = os.path.normpath(p)
+        data = data[0:s] + p + data[e:] 
+        m = sysIdRE.search(data,pos+1)
+    return data
+
+envVarRE = re.compile('\$([A-Za-z][A-Za-z0-9_]*)')
+#cvs_vars = [ 'Id', 'Name', 'Log' ]
+def fix_env_vars(xml_data):
+    data = xml_data
+    m = envVarRE.search(data)
+    while m != None:
+        pos = m.start()
+        s = m.start(1)
+        e = m.end(1)
+        name = m.group(1)
+        
+        if os.environ.has_key(name):
+            val = os.environ[name]
+        else:
+            val = name
+            
+        data = data[0:pos] + val + data[e:]
+        m = envVarRE.search(data,pos+1)
+    return data
+
+#### connectStrings: ####
+# "oracle://intr1-v.cern.ch/INTR;schema=lhcb_cool;user=lhcb_cool;dbname=DDDB"
+# "sqlite://none;schema=XmlDDDB_v26r2.sqlite;user=none;password=none;dbname=DDDB"
+# "mysql://pclhcb55.cern.ch;schema=MARCOCLE;user=marcocle;dbname=DDDB"
+# "oracle://oradev10.cern.ch:10520/D10;schema=lhcb_lcg_cool_dev;user=lhcb_lcg_cool_dev;dbname=DDDB"
+
+################################################################################
+def main():
+    parser = OptionParser()
+    parser.add_option("-c", "--connect-string",
+                      dest="connectString", type="string",
+                      help="cool::DatabaseId to use for the connection",
+                      default="sqlite://none;schema=sqlite.db;user=none;password=none;dbname=DDDB")
+    parser.add_option("-s", "--source",
+                      dest="source", type="string",
+                      help="directory where the files to copy to the DB are",
+                      metavar="SRCDIR")
+    parser.add_option("-d", "--dest",
+                      dest="dest", type="string",
+                      help="directory, in the database, where to copy the files (default: '/')",
+                      default="/",
+                      metavar="DESTPATH")
+    parser.add_option("-x", "--drop-db",
+                      action="store_true", dest="drop",
+                      help="drop the database before the copy",
+                      default=False)
+    parser.add_option("-X", "--noext",
+                      action="store_true", dest="noextension",
+                      help="Remove filename extension when creating a COOL Folder",
+                      default=False)
+#    parser.add_option("-k", "--keep-db",
+#                      action="store_false", dest="drop",
+#                      help="keep the existing database and merge with the new files (default)")
+    (options, args) = parser.parse_args()
+    if len(args) != 0 or not options.source or not options.dest:
+        parser.error("Bad arguments, use -h for help.")
+
+    if options.drop:
+        conddbui.CondDB.dropDatabase(options.connectString)
+    db = conddbui.CondDB(options.connectString)
+    
+    excludes = [ 'clhep2dtd.pl' ]
+    
+    # count files (just to know)
+    sum = 0
+    for root, dirs, files in os.walk(options.source):
+        if 'CVS' in root: # skip CVS directories
+            continue
+        for x in excludes:
+            if x in files:
+                files.remove(x)
+        sum += len(files)
+
+    num_len = len(str(sum))
+    # populate the database
+    n = 0
+    for root, dirs, files in os.walk(options.source):
+        # remove base path
+        dbroot = root.replace(options.source,options.dest)
+        dbroot = dbroot.replace('//','/')
+        
+        if 'CVS' in root: # skip CVS directories
+            continue
+    
+        if dbroot != "/": # it means it is not "/"
+            if not db.db.existsFolderSet(dbroot):
+                db.createNode(path = dbroot, storageType = 'NODE')
+        #else:
+        #    dbroot = "/"
+        
+        print dbroot
+        for x in excludes:
+            if x in files:
+                files.remove(x)
+        for f in files:
+            n+=1
+            print ("%" + str(num_len) + "d %" + str(num_len) + "d  %s")%(n,sum-n,f)
+            if options.noextension:
+                folder_name = os.path.splitext(f)[0]
+            else:
+                folder_name = f
+            folder_path = os.path.join(dbroot,folder_name)
+            if not db.db.existsFolder(folder_path):
+                db.createNode(path = folder_path)
+            
+            xml_data = open(os.path.join(root,f)).read()
+            fixed_data = fix_system_ids(xml_data,"conddb:"+dbroot)
+            #fixed_data = xml_data
+            fixed_data = fix_env_vars(fixed_data)
+            db.storeXMLString(folder_path,fixed_data,cool.ValidityKeyMin,cool.ValidityKeyMax,0)    
+
+    print "Total files inserted = %d"%sum
+
+if __name__ == '__main__':
+    main()
