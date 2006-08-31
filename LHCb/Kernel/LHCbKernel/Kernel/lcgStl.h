f7aac2bfefaa8fb7cc07e33c4f1599852ee8fe2d
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2006-08-31 14:53:26 +0000

    added creation of dictionaries for ISTClusterPosition, IVeloClusterPosition and the templated SiPositionInfo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47479 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f691c599a9c1297b93edfb38dad38b4b5d00e98b

--- LHCb/Kernel/LHCbKernel/Kernel/lcgStl.h
+++ LHCb/Kernel/LHCbKernel/Kernel/lcgStl.h
@@ -1,4 +1,4 @@
-// $Id: lcgStl.h,v 1.24 2006-07-19 13:09:16 cattanem Exp $
+// $Id: lcgStl.h,v 1.25 2006-08-31 14:53:26 erodrigu Exp $
 #ifndef KERNEL_LCGSTL_H 
 #define KERNEL_LCGSTL_H 1
 
@@ -7,6 +7,7 @@
 // begin include files
 #include <vector>
 #include <utility>
+#include "GaudiKernel/VectorMap.h"
 #include "Kernel/VeloChannelID.h"
 #include "Kernel/STChannelID.h"
 #include "Kernel/OTChannelID.h"
@@ -16,13 +17,17 @@
 #include "Kernel/CircleTraj.h"
 #include "Kernel/LineTraj.h"
 #include "Kernel/ParabolaTraj.h"
-#include "GaudiKernel/VectorMap.h"
+#include "Kernel/SiPositionInfo.h"
+
+#include "Kernel/ISTClusterPosition.h"
+#include "Kernel/IVeloClusterPosition.h"
 
 // end include files
 
 namespace {
   struct _Instantiations {
     // begin instantiations
+    GaudiUtils::VectorMap<int,double>                m_vector_map_int_double;
     std::pair<LHCb::VeloChannelID,int>               m_std_pair_VeloChannelID_int;
     std::pair<LHCb::STChannelID,int>                 m_std_pair_STChannelID_int;
     std::pair<LHCb::OTChannelID,int>                 m_std_pair_OTChannelID_int;
@@ -34,14 +39,9 @@ namespace {
     std::vector<std::pair<double,LHCb::CaloCellID> > m_std_vector_std_pair_double_CaloCellID;
     std::vector<std::pair<LHCb::MuonTileID,int> >    m_std_vector_std_pair_MuonTileID_int;
     std::auto_ptr<LHCb::Trajectory>                  m_auto_ptr_Trajectory;
-    GaudiUtils::VectorMap<int,double>                m_vector_map_int_double;
+    LHCb::SiPositionInfo<LHCb::STChannelID>          m_SiPositionInfo_STChannelID;
+    LHCb::SiPositionInfo<LHCb::VeloChannelID>        m_SiPositionInfo_VeloChannelID;
     // end instantiations
-  };
-}
-
-namespace { // Anonymous namespace 
-  struct _instances {
-    LHCb::CircleTraj m_CircleTraj; // class gets instantiated  
   };
 }
 
