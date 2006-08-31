ffb12c70925d8da8f0efe1d6b2c9a1d3473ff7b1
Markus Frank <frankb@cern.ch>
2006-08-31 17:19:48 +0000

    remove obsolete scripts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96234 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8ef49ce694b9176ea33eabd2cf86210ee1a62584

--- Online/Online/GaudiOnline/cmt/DummyMoore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./setupOnline.sh
-export MSGSVC=MessageSvc
-${gaudi_exe} ${gaudi_exe_args} -opt=../options/ReadMBM.opts\
-    -msgsvc=${MSGSVC} &
--- Online/Online/GaudiOnline/cmt/mbm.bat
+++
@@ -1,6 +0,0 @@
-start "Install" %ONLINEKERNELROOT%\win32_vc71_dbg\test.exe mbm_install -s=8096 -e=64 -u=64 -i=EVENT -m
-sleep 1
-start "Cons_0" %ONLINEKERNELROOT%\win32_vc71_dbg\test.exe mbm_cons  -b=EVENT   -n=cons_0 -q
-start "Cons_1" %ONLINEKERNELROOT%\win32_vc71_dbg\test.exe mbm_cons  -b=EVENT   -n=cons_1 -q
-start "Cons_2" %ONLINEKERNELROOT%\win32_vc71_dbg\test.exe mbm_cons  -b=EVENT   -n=cons_2 -q
-start "ReadDummyMDF" ..\win32_vc71_dbg\Gaudi.exe gaudi_task ..\options\ReadDummyMDF.opts
