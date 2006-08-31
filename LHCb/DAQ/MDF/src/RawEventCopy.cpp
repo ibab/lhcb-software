53c478c522cdb1c42fc851fc4f40746f9edc13b0
Markus Frank <frankb@cern.ch>
2006-08-31 16:18:53 +0000

    see release notes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35317 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 369ac8f4f590fb378befee12f0395bf1de12b4f7

---
+++ LHCb/DAQ/MDF/src/RawEventCopy.cpp
@@ -0,0 +1,56 @@
+// $Id: RawEventCopy.cpp,v 1.1 2006-08-31 16:18:53 frankb Exp $
+// Include files from Gaudi
+#include "GaudiKernel/Algorithm.h" 
+#include "GaudiKernel/IDataProviderSvc.h" 
+#include "MDF/RawEventHelpers.h"
+#include "Event/RawEvent.h"
+
+/*
+ *    LHCb namespace declaration
+ */
+namespace LHCb  {
+class RawEvent;
+
+  /** @class RawEventCopy RawEventCopy.h tests/RawEventCopy.h
+    *
+    *  Creates and copies RawEvent data structure to new location
+    *  in the data store.
+    *
+    *  @author Markus Frank
+    *  @date   2005-10-13
+    */
+  class RawEventCopy : public Algorithm {
+    /// Property: source leaf for rawevent copy
+    std::string m_source;
+    /// Property: target leaf name for rawevent copy
+    std::string m_destination;
+
+  public: 
+    /// Standard constructor
+    RawEventCopy(const std::string& nam, ISvcLocator* pSvc) 
+    : Algorithm(nam,pSvc) {
+      declareProperty("Destination",m_destination="/Event/DAQ/RawCopy");
+      declareProperty("Source",     m_source=LHCb::RawEventLocation::Default);
+    }
+    /// Destructor
+    virtual ~RawEventCopy()  {} 
+    /// Main execution
+    virtual StatusCode execute()  {
+      RawEvent *org = 0, *res = 0;
+      StatusCode sc = eventSvc()->retrieveObject(LHCb::RawEventLocation::Default,(DataObject*&)org);
+      if ( sc.isSuccess() )  {
+        sc = cloneRawEvent(org, res);
+        if ( sc.isSuccess() )  {
+          sc = eventSvc()->registerObject(m_destination, res);
+          if ( !sc.isSuccess() )  {
+            delete res;
+          }
+        }
+      }
+      return sc;
+    }
+  };
+}
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+DECLARE_NAMESPACE_ALGORITHM_FACTORY(LHCb,RawEventCopy)
