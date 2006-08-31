415966d26be9dc43154b764254ddd141057c5491
Markus Frank <frankb@cern.ch>
2006-08-31 17:23:13 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96235 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5bcdb1d6c3e6bef3652bdc0e337a6254b43c9f08

--- Online/Online/GaudiOnline/cmt/EvtHolder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./setupOnline.sh
-export MSGSVC=MessageSvc
-${gaudi_exe} ${gaudi_exe_args} -opt=../options/MEPHolder.opts\
-    -msgsvc=${MSGSVC} &
-
-
--- Online/Online/GaudiOnline/cmt/EvtProd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. ./setupOnline.sh
-export MSGSVC=MessageSvc
-${gaudi_exe} ${gaudi_exe_args} -opt=../options/MEPConverter.opts\
-    -msgsvc=${MSGSVC} &
