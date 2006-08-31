1a4fccdff1b817e2dc8f9bfe467ad7f646422d09
Markus Frank <frankb@cern.ch>
2006-08-31 18:00:45 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96239 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2406956db2d0e7a18ef84796d6125ff75c508ff8

--- Online/Online/OnlineTasks/job/hltrx.sh
+++ Online/Online/OnlineTasks/job/hltrx.sh
@@ -14,14 +14,14 @@ export gaudi_exe2="${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe libGaudiOnline.so O
 export UTGID=MBMInit   && ${gaudi_exe} -main=../options/MBMinit.opts  -opt=../options/Daemon.opts  -auto &
 export UTGID=ErrServ   &&  ${gaudi_exe} -opt=../options/ErrorSrv.opts -auto &
 sleep 2
-$WIDETERM ErrorLogger@${HOST}    -e "export UTGID=ErrLog; $gaudi_exe2 -opts=../options/ErrorLogger.opts" &
+$WIDETERM ErrorLogger@${HOST}    -e bash -c "export UTGID=ErrLog; $gaudi_exe2 -opts=../options/ErrorLogger.opts" &
 sleep 3
-$MINITERM ErrServ@${HOST} -e "setenv UTGID ErrServ   ; $gaudi_exe -opt=../options/ErrorSrv.opts" &
+$MINITERM ErrServ@${HOST} -e bash -c "export UTGID=ErrServ; $gaudi_exe -opt=../options/ErrorSrv.opts" &
 #
 export UTGID=EvtHolder &&  ${gaudi_exe} -opt=../options/MEPHolder.opts    -auto &
-#export UTGID=Moore_0   &&  ${gaudi_exe} -opt=../options/ReadMBM.opts      -auto &
+export UTGID=Moore_0   &&  ${gaudi_exe} -opt=../options/ReadMBM.opts      -auto &
 export UTGID=Moore_1   &&  ${gaudi_exe} -opt=../options/ReadMBM.opts       -auto &
-export UTGID=MBMMon    && xterm -geometry 120x30 -e "${test_exe} mbm_mon" &
+export UTGID=MBMMon    && xterm -geometry 120x30 -e bash -c "${test_exe} mbm_mon" &
 sleep 1
 echo $# $*
 sleep 1
@@ -31,11 +31,7 @@ export UTGID=EvtProd   && ${gaudi_exe} -opt=../options/MEPConverter.opts -msgsvc
 if [ $# -eq 1 ] ; then
 
 echo  libGaudiOnline.so OnlineTask -opt=../options/MEPRxSvc.opts -msgsvc=${MSGSVC} -auto &
-export UTGID=MEPRx      &&  gdb ${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe 
+export UTGID=MEPRx      &&  gdb  -x meprx.gdb ${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe 
 else 
 export UTGID=MEPRx      &&  ${gaudi_exe} -opt=../options/MEPRxSvc.opts -msgsvc=${MSGSVC} -auto &
 fi
-
-echo libGaudiOnline.so OnlineTask  -opt=../options/ReadMBM.opts      -msgsvc=${MSGSVC} -auto
-
-#export UTGID=Moore_2 && gdb -x meprx.gdb ${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe
