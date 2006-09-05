fdaca40921be6b31a41f27463c5f2bf64e9458a0
Matthew David Needham <mneedham@cern.ch>
2006-09-05 15:51:09 +0000

    add missing functionality
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16446 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1302b7ce06e6b1de061f3a0ea8cdde7e6048b338

---
+++ Rec/Tr/TsaAlgorithms/src/TsaCollector.cpp
@@ -0,0 +1,145 @@
+
+
+// Gaudo
+#include "GaudiKernel/ToolFactory.h"
+#include "GaudiKernel/IIncidentSvc.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "LHCbMath/GeomFun.h"
+
+#include "TsaKernel/Cluster.h"
+#include "TsaKernel/STCluster.h"
+#include "TsaKernel/OTCluster.h"
+
+#include "TsaKernel/Line3D.h"
+
+#include "TsaCollector.h" 
+
+  
+static const ToolFactory<TsaCollector>  s_factory;
+const IToolFactory& TsaCollectorFactory = s_factory;
+  
+TsaCollector::TsaCollector(const std::string& type,
+                     const std::string& name,
+                     const IInterface* parent):
+GaudiTool(type, name, parent),
+m_initEvent(false){
+
+ declareProperty("curvatureFactor", m_curvFactor = 42.0);
+ declareProperty("searchOT", m_searchOT = true);
+ declareProperty("searchIT", m_searchIT = true);
+ declareProperty("xWindow", m_xWindowSigma = 3.0);
+ declareProperty("yWindow", m_yWindowSigma = 3.0);
+ declareProperty("itClusterLocation", m_itClusterLocation = Tsa::STClusterLocation::IT );
+ declareProperty("otClusterLocation", m_otClusterLocation = Tsa::OTClusterLocation::Default);
+
+ declareInterface<ITsaCollector>(this);
+
+}
+
+TsaCollector::~TsaCollector(){
+  // destructer
+}
+
+StatusCode TsaCollector::initialize(){
+
+  StatusCode sc = GaudiTool::initialize();
+  if (sc.isFailure()){
+     return Error("Failed to initialize",sc);
+  }
+
+  // Setup incident services
+  incSvc()->addListener( this, IncidentType::BeginEvent );
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TsaCollector::collect(const LHCb::State& aState, 
+                                 Tsa::Clusters* clusters) const{
+
+  // check init
+  if (m_initEvent == false) getClusters(); 
+ 
+  // make paramterization of the track
+  Tsa::Line line = yParam(aState);
+  Tsa::Parabola parab = xParam(aState);
+  
+  WindowSize winSize(m_xWindowSigma*sqrt(aState.errX2()),m_yWindowSigma*sqrt(aState.errY2()));
+
+  if (m_searchOT) searchOT(parab, line, winSize,clusters);  
+  if (m_searchIT) searchIT(parab, line, winSize,clusters);
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TsaCollector::collect(const LHCb::State& aState, 
+                                 Tsa::STClusters* clusters) const{
+
+  // check init
+  if (m_initEvent == false) getClusters(); 
+ 
+  // make paramterization of the track
+  Tsa::Line line = yParam(aState);
+  Tsa::Parabola parab = xParam(aState);
+  
+  WindowSize winSize(m_xWindowSigma*sqrt(aState.errX2()),m_yWindowSigma*sqrt(aState.errY2()));
+  
+  searchIT(parab, line, winSize,clusters);
+
+  return StatusCode::SUCCESS;
+}
+
+
+StatusCode TsaCollector::collect(const LHCb::State& aState, 
+                                 Tsa::OTClusters* clusters) const{
+
+  // check init
+  if (m_initEvent == false) getClusters(); 
+ 
+  // make paramterization of the track
+  Tsa::Line line = yParam(aState);
+  Tsa::Parabola parab = xParam(aState);
+
+  WindowSize winSize(m_xWindowSigma*sqrt(aState.errX2()),m_yWindowSigma*sqrt(aState.errY2()));
+  
+  searchOT(parab, line, winSize,clusters);
+  return StatusCode::SUCCESS;
+}
+
+
+
+void TsaCollector::getClusters() const {
+
+  m_itClusters = get<Tsa::STClusters>(m_itClusterLocation);
+  m_otClusters = get<Tsa::OTClusters>(m_otClusterLocation);
+  m_initEvent = true;
+}
+
+void TsaCollector::handle ( const Incident& incident )
+{
+  if ( IncidentType::BeginEvent == incident.type() ) { m_initEvent = false; }
+}
+
+bool TsaCollector::inWindow(const Tsa::Parabola& parab,
+                            const Tsa::Line& line, 
+                            const Tsa::Cluster* cluster,
+                            const TsaCollector::WindowSize& win) const{
+ 
+  // get z of cluster
+  const double zMid = cluster->zMid(); 
+ 
+  // check y  
+  if (cluster->isInsideY(line.value(zMid), win.second) == false) return false;
+
+  // check x - saves some time ? but not exact - hence big window
+  if (cluster->isInsideX(parab.value(zMid), 20.0*Gaudi::Units::cm) == false) return false;
+
+  // now look at 3-d intersection --> need two lines
+  Tsa::Line tanLine = parab.tangent(zMid);
+  Tsa::Line3D aLine3D = Tsa::createLine3D(tanLine,line,zMid);
+  Tsa::Line3D traj(cluster->trajectory().position(0.),
+                   cluster->trajectory().direction(0.));
+
+  double doca = Gaudi::Math::distance(traj, aLine3D);
+  return (doca < win.first);     
+}
---
+++ Rec/Tr/TsaAlgorithms/src/TsaCollector.h
@@ -0,0 +1,153 @@
+// $Id: TsaCollector.h,v 1.1 2006-09-05 15:51:09 mneedham Exp $
+#ifndef _TsaCollector_H
+#define _TsaCollector_H
+
+/** @class TsaTsaCollector TsaCollector Tsa/TsaCollector.h
+ *
+ *  Interface to isolation tool
+ *
+ *  @author M.Needham
+ *  @date   30/04/2004
+ */
+#include "GaudiKernel/IIncidentListener.h"  
+#include "GaudiAlg/GaudiTool.h"
+
+#include "TsaKernel/ITsaCollector.h"
+#include "TsaKernel/OTCluster.h"
+#include "TsaKernel/STCluster.h"
+
+namespace Tsa{
+
+  class Cluster;
+  class OTCluster;
+  class STCluster;
+  class Line;
+  class Parabola;
+
+}
+
+#include "Event/State.h" 
+
+
+class TsaCollector: public GaudiTool , 
+                    virtual public ITsaCollector, 
+                    virtual public IIncidentListener {
+
+public:
+
+  /// constructer
+  TsaCollector(const std::string& type,
+                      const std::string& name,
+                      const IInterface* parent);
+                                                                                
+  virtual ~TsaCollector();
+
+  StatusCode initialize();
+			   
+  /// the method
+  virtual StatusCode collect(const LHCb::State& aState, 
+                             Tsa::Clusters* clusters) const;
+
+  /// the method
+  virtual StatusCode collect(const LHCb::State& aState, 
+                             Tsa::STClusters* clusters) const;
+
+  /// the method
+  virtual StatusCode collect(const LHCb::State& aState, 
+                             Tsa::OTClusters* clusters) const;
+ 
+private:
+
+  typedef std::pair<double, double> WindowSize;
+
+  template<typename TYPE>
+  void searchOT(const Tsa::Parabola& parab,
+                const Tsa::Line& line,
+                const WindowSize& win,
+		TYPE& clusters) const ;
+
+  template<typename TYPE>
+  void searchIT(const Tsa::Parabola& parab,
+                const Tsa::Line& line,
+                const WindowSize& win,
+		TYPE& clusters) const ;
+
+  void getClusters() const;
+
+  void handle ( const Incident& incident );
+ 
+  Tsa::Line yParam(const LHCb::State& aState) const;
+  Tsa::Parabola xParam(const LHCb::State& aState) const;
+
+  bool TsaCollector::inWindow(const Tsa::Parabola& parab,
+                              const Tsa::Line& line, 
+                              const Tsa::Cluster* cluster,
+                              const TsaCollector::WindowSize& win) const;                                                                 
+
+  mutable bool m_initEvent;
+
+  mutable Tsa::STClusters* m_itClusters;
+  mutable Tsa::OTClusters* m_otClusters;
+
+  double m_curvFactor;
+  bool m_searchIT;
+  bool m_searchOT;
+  double m_xWindowSigma;
+  double m_yWindowSigma;
+  std::string m_otClusterLocation;
+  std::string m_itClusterLocation;
+
+};
+
+#include "Event/State.h"
+#include "TsaKernel/Parabola.h"
+#include "TsaKernel/Line.h"
+
+template<typename TYPE>
+inline void TsaCollector::searchOT(const Tsa::Parabola& parab,
+                            const Tsa::Line& line,
+                            const WindowSize& win , 
+                            TYPE& clusters) const {
+
+ // flat loop for now...nothing fancy
+ for ( Tsa::OTClusters::iterator iter = m_otClusters->begin();  iter != m_otClusters->end() ; ++iter){
+   if (inWindow(parab,line,*iter,win) == true) {
+     Tsa::OTCluster* newClus = (*iter)->clone();
+     clusters->add(newClus);
+   }
+ } // iter
+}
+
+template<typename TYPE>
+inline void TsaCollector::searchIT(const Tsa::Parabola& parab,
+                                   const Tsa::Line& line,
+                                   const WindowSize& win , 
+                                   TYPE& clusters) const{
+
+  // flat loop for now...nothing fancy
+  for ( Tsa::STClusters::iterator iter = m_itClusters->begin();  iter != m_itClusters->end() ; ++iter){
+    if (inWindow(parab,line,*iter, win) == true) {
+     Tsa::STCluster* newClus = (*iter)->clone();
+     clusters->add(newClus);
+    }
+  } // iter
+}
+
+inline Tsa::Line TsaCollector::yParam(const LHCb::State& aState) const{
+
+  const double m = aState.ty();
+  const double c = aState.y() - m*aState.z();
+  return Tsa::Line(m,c);
+}
+
+inline Tsa::Parabola TsaCollector::xParam(const LHCb::State& aState) const{
+ 
+  const double z = aState.z();
+  const double a = aState.qOverP()/m_curvFactor;
+  const double b = aState.tx() -2*z*a;  
+  const double c = aState.x() - z*(b + a*z); 
+  return Tsa::Parabola(a,b,c);
+}
+
+				       	       
+#endif
