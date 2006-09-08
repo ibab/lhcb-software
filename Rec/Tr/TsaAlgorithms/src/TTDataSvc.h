324c779157e347ffef272db8a3f0306aaaa6ea4c
Matthew David Needham <mneedham@cern.ch>
2006-09-08 08:25:51 +0000

    missed files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16447 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 67a8f6ef0d6970380f1a56577b8ec091c6504289

---
+++ Rec/Tr/TsaAlgorithms/src/TTDataSvc.h
@@ -0,0 +1,73 @@
+// $Id: TTDataSvc.h,v 1.1 2006-09-08 08:25:51 mneedham Exp $
+#ifndef _TTDataSvc_H
+#define _TTDataSvc_H
+
+/** @class TTDataSvc TTDataSvc Tsa/TTDataSvc.h
+ *
+ *  Interface to data svc
+ *
+ *  @author M.Needham
+ *  @date   30/04/2004
+ */
+
+#include "GaudiAlg/GaudiTool.h"
+#include "TsaKernel/ITTDataSvc.h"
+
+#include <map>
+#include <vector>
+#include <utility>
+#include <string>
+
+class DeSTDetector;
+
+class TTDataSvc: public GaudiTool,
+                 virtual public ITTDataSvc{
+
+public:
+
+  /// constructer
+  TTDataSvc(const std::string& type,
+            const std::string& name,
+            const IInterface* parent);
+
+  // destructer
+  virtual ~TTDataSvc();
+
+  /// init
+  virtual StatusCode initialize();
+
+  // init event
+  virtual StatusCode initializeEvent();
+
+  virtual  Tsa::STRange layer(const int& iStation, const int& iLayer) const;
+   
+  virtual unsigned int dataSize() const;
+
+private:
+
+ 
+  StatusCode initPartitions();
+  int offSet(const int& iStation, const int& iStation) const;
+
+  std::vector<Tsa::STRange> m_iterVector; 
+  std::map<LHCb::STChannelID,unsigned int> m_Mapping;
+  unsigned int m_dataSize;
+
+  unsigned int m_layersPerStation;
+  unsigned int m_nPartitions;
+
+  std::vector<int> m_nSectorsPerPartition;
+  std::vector<int> m_nRegions;
+
+  DeSTDetector* m_tracker;
+
+  std::string m_inputLocation;
+
+};
+
+inline int TTDataSvc::offSet(const int& iStation, const int& iLayer) const{
+  return (((iStation)*m_layersPerStation)+iLayer-1);
+}
+
+#endif
+
