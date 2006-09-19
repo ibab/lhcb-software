4026708d936047b13d475da0689c52a096fc4f8b
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2006-09-19 13:18:35 +0000

    New context tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63317 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 16c7011c86a488b25d331464d5f4b388866cf95e

---
+++ Phys/Phys/DaVinciKernel/Kernel/IContextTool.h
@@ -0,0 +1,50 @@
+// $Id: IContextTool.h,v 1.1 2006-09-19 13:18:34 pkoppenb Exp $
+#ifndef DAVINCIKERNEL_ICONTEXTTOOL_H 
+#define DAVINCIKERNEL_ICONTEXTTOOL_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+// from Event
+#include "Event/RecVertex.h"
+
+// DaVinci
+class IVertexFit;
+class IPhysDesktop;
+class IGeomDispCalculator;
+
+static const InterfaceID IID_IContextTool ( "IContextTool", 1, 0 );
+
+/** @class IContextTool IContextTool.h Kernel/IContextTool.h
+ *  
+ *  Tool that tells the application if one runs in On or Offline Mode
+ *  Now uses context. Can only be used as private tool.
+ *
+ *  @author Patrick KOPPENBURG
+ *  @date   2005-06-08
+ */
+class IContextTool : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IContextTool; }
+
+  /// Get primary vertices
+  virtual LHCb::RecVertex::ConstVector primaryVertices() const = 0 ;
+  
+  /// Get geom tool
+  virtual const IGeomDispCalculator* geomTool() const = 0 ;
+
+  /// Get default vertex fitter
+  virtual const IVertexFit* vertexFitter() const = 0 ;
+
+  /// Get physDesktop if available
+  virtual IPhysDesktop* desktop() const = 0 ;
+
+
+};
+#endif // DAVINCIKERNEL_ICONTEXTTOOL_H
