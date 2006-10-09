8d5bb788e773d0e5addfc289386eaae6bb9b48e0
jpalac <jpalac@cern.ch>
2006-10-09 15:43:44 +0000

    First import of Phys/PhysDict
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63358 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d1e6ed2da58d212ba36671ec57a157b00cbf88e2

---
+++ Phys/Phys/PhysDict/dict/PhysDict.h
@@ -0,0 +1,76 @@
+// $Id: PhysDict.h,v 1.1.1.1 2006-10-09 15:43:44 jpalac Exp $
+#ifndef DICT_PHYSDICT_H 
+#define DICT_PHYSDICT_H 1
+
+// Include files
+
+/** @file PhysDict PhysDict.h dict/PhysDict.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2006-10-02
+ */
+// ============================================================================
+// DaVinciKernel
+// ============================================================================
+#include "Kernel/IAlgorithm2ID.h"
+#include "Kernel/IBestParticles.h"
+#include "Kernel/IBTaggingTool.h"
+#include "Kernel/ICaloParticleMaker.h"
+#include "Kernel/ICheckOverlap.h"
+#include "Kernel/ICheckSelResults.h"
+#include "Kernel/IContextTool.h"
+#include "Kernel/IDecayFinder.h"
+#include "Kernel/IDecodeSimpleDecayString.h"
+#include "Kernel/IDirectionFit.h"
+#include "Kernel/IFilterCriteriaTESTool.h"
+#include "Kernel/IFilterCriterion.h"
+#include "Kernel/IGeomDispCalculator.h"
+#include "Kernel/IJetMaker.h"
+#include "Kernel/ILifetimeFitter.h"
+#include "Kernel/IMassVertexFit.h"
+#include "Kernel/IOnOffline.h"
+#include "Kernel/IParticle2State.h"
+#include "Kernel/IParticle2VertexAsct.h"
+#include "Kernel/IParticleCombiner.h"
+#include "Kernel/IParticleDescendants.h"
+#include "Kernel/IParticleFilter.h"
+#include "Kernel/IParticleMaker.h"
+#include "Kernel/IParticleReFitter.h"
+#include "Kernel/IParticleStuffer.h"
+#include "Kernel/IPhysDesktop.h"
+#include "Kernel/IPlotTool.h"
+#include "Kernel/IProtoParticleFilter.h"
+#include "Kernel/IPVReFitter.h"
+#include "Kernel/IRelatedPV.h"
+//#include "Kernel/ISecondaryVertexTool.h"
+//#include "Kernel/ITagger.h"
+#include "Kernel/IVertexFit.h"
+// ============================================================================
+// DaVinciMCTools
+// ============================================================================
+#include "DaVinciMCTools/IBackgroundCategory.h"
+#include "DaVinciMCTools/ICheatedLifetimeFitter.h"
+#include "DaVinciMCTools/IDebugTool.h"
+#include "DaVinciMCTools/IMCReconstructible.h"
+#include "DaVinciMCTools/IVisPrimVertTool.h"
+// ============================================================================
+// MCEvent
+// ============================================================================
+#include "Kernel/IMCDecayFinder.h"
+// ============================================================================
+namespace {
+
+/// @todo remove ostream operator when included in new Gaudi
+  std::ostream& operator<< ( std::ostream& s , const StatusCode& sc ) 
+  {
+    if ( sc.isSuccess() ) { return s << "SUCCESS" ; }
+    s << "FAILURE" ;
+    if ( StatusCode::FAILURE != sc.getCode() ) 
+    { s << "(" << sc.getCode() << ")" ;}
+    return s ;
+  }
+
+}
+
+#endif // DICT_PHYSDICT_H
