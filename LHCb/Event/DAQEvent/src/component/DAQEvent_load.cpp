ed3b6fc1603d2f41d46c53fe60b8c1ac3fbc3952
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-10-10 09:07:22 +0000

    add missing factory
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35374 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 65fb130e8871d7a8d015e5e0721b0fe1eb501169

--- LHCb/Event/DAQEvent/src/component/DAQEvent_load.cpp
+++ LHCb/Event/DAQEvent/src/component/DAQEvent_load.cpp
@@ -1,9 +1,10 @@
-// $Id: DAQEvent_load.cpp,v 1.4 2006-09-07 17:03:29 odescham Exp $
+// $Id: DAQEvent_load.cpp,v 1.5 2006-10-10 09:07:22 cattanem Exp $
 
 // Include files 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(DAQEvent) {
+  DECLARE_TOOL( OdinTimeDecoder );
   DECLARE_ALGORITHM( RawEventDump );
   DECLARE_ALGORITHM( bankKiller );
 }
