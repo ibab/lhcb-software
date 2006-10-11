396116ab8cb2255acc346cec01c2c0d65675fcb0
jpalac <jpalac@cern.ch>
2006-10-11 15:30:53 +0000

    added Kernel/Particle2MCMethod.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63365 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: be60501dfa5049d8a3e1bffdb5f5ce95f143ca40

---
+++ Phys/Phys/DaVinciKernel/Kernel/Particle2MCMethod.h
@@ -0,0 +1,39 @@
+// $Id: Particle2MCMethod.h,v 1.1 2006-10-11 15:30:53 jpalac Exp $
+#ifndef KERNEL_PARTICLE2MCMETHOD_H 
+#define KERNEL_PARTICLE2MCMETHOD_H 1
+
+// Include files
+
+/** @namespace Particle2MCMethod Particle2MCMethod.h Kernel/Particle2MCMethod.h
+ *  
+ *  @author Philippe CHARPENTIER
+ *  @author Juan PALACIOS
+ *  @date   2006-10-11
+ */
+namespace Particle2MCMethod {
+
+  enum {
+    No = 0, WithChi2, Chi2, Links, Composite, ChargedPP, NeutralPP, Max 
+  } ;
+
+  static const std::string extension[Max] = {
+    "",
+    "/WithChi2",
+    "/Chi2",
+    "/Links",
+    "/Composite",
+    "/PP2MC",
+    "/PP2MC"
+  };
+  static const std::string algType[Max] = {
+    "",
+    "Particle2MCWithChi2",
+    "Particle2MCChi2",
+    "Particle2MCLinks",
+    "CompositeParticle2MCLinks",
+    "ChargedPP2MC",
+    "NeutralPP2MC"
+  };
+
+}
+#endif // KERNEL_PARTICLE2MCMETHOD_H
