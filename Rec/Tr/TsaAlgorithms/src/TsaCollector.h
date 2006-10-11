b31e4609c35c8c892687f73c6f10318f73edc500
Matthew David Needham <mneedham@cern.ch>
2006-10-11 15:39:19 +0000

    change to remove duplicates in TsaCollector
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16496 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2a09dc64ebe4463241efb948423b4935846c578

--- Rec/Tr/TsaAlgorithms/src/TsaCollector.h
+++ Rec/Tr/TsaAlgorithms/src/TsaCollector.h
@@ -1,4 +1,4 @@
-// $Id: TsaCollector.h,v 1.2 2006-10-10 14:21:00 mneedham Exp $
+// $Id: TsaCollector.h,v 1.3 2006-10-11 15:39:19 mneedham Exp $
 #ifndef _TsaCollector_H
 #define _TsaCollector_H
 
@@ -15,18 +15,17 @@
 #include "TsaKernel/ITsaCollector.h"
 #include "TsaKernel/OTCluster.h"
 #include "TsaKernel/STCluster.h"
+#include "TsaKernel/Cluster.h"
+#include "Event/State.h" 
+#include "Kernel/LHCbID.h"
 
 namespace Tsa{
-
-  class Cluster;
-  class OTCluster;
-  class STCluster;
   class Line;
   class Parabola;
-
 }
 
-#include "Event/State.h" 
+
+
 
 
 class TsaCollector: public GaudiTool , 
@@ -64,13 +63,13 @@ private:
   void searchOT(const Tsa::Parabola& parab,
                 const Tsa::Line& line,
                 const WindowSize& win,
-		TYPE& clusters) const ;
+		ObjectVector<TYPE>*& clusters) const ;
 
   template<typename TYPE>
   void searchIT(const Tsa::Parabola& parab,
                 const Tsa::Line& line,
                 const WindowSize& win,
-		TYPE& clusters) const ;
+		ObjectVector<TYPE>*& clusters) const ;
 
   void getClusters() const;
 
@@ -108,32 +107,66 @@ template<typename TYPE>
 inline void TsaCollector::searchOT(const Tsa::Parabola& parab,
                             const Tsa::Line& line,
                             const WindowSize& win , 
-                            TYPE& clusters) const {
+                            ObjectVector<TYPE>*& clusters) const {
 
  // flat loop for now...nothing fancy
+ std::vector<Tsa::OTCluster*> tmpVector; tmpVector.reserve(24);
  for ( Tsa::OTClusters::iterator iter = m_otClusters->begin();  iter != m_otClusters->end() ; ++iter){
    if (inWindow(parab,line,*iter,win) == true) {
      Tsa::OTCluster* newClus = (*iter)->clone();
-     if (m_correctDriftDist == true) newClus->setDriftRadius(parab.value((*iter)->zMid()), line.value((*iter)->zMid()));
-     clusters->add(newClus);
-   }
+     if (m_correctDriftDist == true) {
+       newClus->setDriftRadius(parab.value((*iter)->zMid()), line.value((*iter)->zMid()));
+       clusters->add(newClus);
+     }
+     else {
+       tmpVector.push_back(newClus);
+      } // window
+   } // in window
  } // iter
+      
+ // remove duplicates
+ for (std::vector<Tsa::OTCluster*>::iterator iterTemp = tmpVector.begin(); iterTemp != tmpVector.end();
+      ++iterTemp){
+
+   typename ObjectVector<TYPE>::iterator iterC = clusters->begin();
+   while ((iterC != clusters->end()) && ((*iterC)->sameID((*iterTemp)->id()) == false) ){
+     ++iterC;
+   } // iterC       
+   if (iterC == clusters->end()) clusters->add(*iterTemp);
+ } // remove duplicates
+    
 }
 
 template<typename TYPE>
 inline void TsaCollector::searchIT(const Tsa::Parabola& parab,
                                    const Tsa::Line& line,
                                    const WindowSize& win , 
-                                   TYPE& clusters) const{
+                                   ObjectVector<TYPE>*& clusters) const{
 
   // flat loop for now...nothing fancy
   for ( Tsa::STClusters::iterator iter = m_itClusters->begin();  iter != m_itClusters->end() ; ++iter){
+
+    std::vector<Tsa::STCluster*> tmpVector; tmpVector.reserve(24);
     if (inWindow(parab,line,*iter, win) == true) {
      Tsa::STCluster* newClus = (*iter)->clone();
-     clusters->add(newClus);
-    }
+     tmpVector.push_back(newClus);
+    } // window
+  
+    // remove duplicates
+    for (std::vector<Tsa::STCluster*>::iterator iterTemp = tmpVector.begin(); iterTemp != tmpVector.end();
+         ++iterTemp){
+
+      typename ObjectVector<TYPE>::iterator iterC = clusters->begin();
+      while ((iterC != clusters->end()) && ((*iterC)->sameID((*iterTemp)->id()) == false) ){
+        ++iterC;
+      } // iterC       
+      if (iterC == clusters->end()) clusters->add(*iterTemp);
+     
+    } // iterTemp
+
   } // iter
 }
+
 
 inline Tsa::Line TsaCollector::yParam(const LHCb::State& aState) const{
 
@@ -151,5 +184,5 @@ inline Tsa::Parabola TsaCollector::xParam(const LHCb::State& aState) const{
   return Tsa::Parabola(a,b,c);
 }
 
-				       	       
+	       	       
 #endif
