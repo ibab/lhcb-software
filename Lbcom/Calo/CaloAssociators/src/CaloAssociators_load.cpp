af678618a8aaed9fbe8f0a47e1d3ead68a275995
Olivier Deschamps <odescham@cern.ch>
2006-10-13 20:15:53 +0000

    flag unused algorithm in CaloAssociators_load
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@39681 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f30304dd58713f2f33b71ce08328c5908a5fc22f

--- Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
+++ Lbcom/Calo/CaloAssociators/src/CaloAssociators_load.cpp
@@ -1,8 +1,11 @@
-// $Id: CaloAssociators_load.cpp,v 1.12 2006-02-21 10:04:45 odescham Exp $
+// $Id: CaloAssociators_load.cpp,v 1.13 2006-10-13 20:15:53 odescham Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.12 $ 
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.13 $ 
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.12  2006/02/21 10:04:45  odescham
+// update for new Event Model
+//
 // ============================================================================
 // Include files
 // ============================================================================
@@ -23,7 +26,12 @@ DECLARE_FACTORY_ENTRIES(CaloAssociators)
   DECLARE_ALGORITHM   ( CaloDigitMCTruth         ) ;
   DECLARE_ALGORITHM   ( CaloClusterMCTruth       ) ;
   DECLARE_ALGORITHM   ( CaloDigit2MCLinks2Table  ) ;
-  
+
+  // Not used in DC06 stream :
+  // DECLARE_ALGORITHM   ( CaloClustersMCTruthAlg  ); 
+
+  // Not yet updated for DC06 :
+  // DECLARE_ALGORITHM   ( aloTrgClusterMTruth ) ;
 };
 
 // ============================================================================
