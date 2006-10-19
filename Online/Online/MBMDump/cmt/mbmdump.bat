7c45ea0dd53e5f737603d2e6ce81fc75687151d4
Markus Frank <frankb@cern.ch>
2006-10-19 14:15:35 +0000

    no message
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96381 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b917e1766e1933909b73a2e1089095009cd52a69

--- Online/Online/MBMDump/cmt/mbmdump.bat
+++ Online/Online/MBMDump/cmt/mbmdump.bat
@@ -1,1 +1,1 @@
-start /b xterm -132 -geometry 132x45 -title MBMDump -e /bin/bash -l -c "cd ${MBMDUMPROOT}/cmt; sh ./mbmdump"
+start /b xterm -132 -geometry 132x45 -title MBMDump -e /bin/bash -l -c "cd ${MBMDUMPROOT}/cmt; sh ./mbmdump.sh"
---
+++ Online/Online/MBMDump/main/mbmdump.cpp
@@ -0,0 +1,4 @@
+extern "C" int mbmdump(int, char**);
+int main(int argc, char** argv)  {
+  return mbmdump(argc,argv);
+}
