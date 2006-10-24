e9529be06319de6572a5b465ee03926b495aacb2
jpalac <jpalac@cern.ch>
2006-10-24 11:13:36 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63391 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0b621fda63748455afc127e4447c2ce219ff8441

--- Phys/Phys/DaVinciKernel/src/component/DaVinciKernel_load.cpp
+++ Phys/Phys/DaVinciKernel/src/component/DaVinciKernel_load.cpp
@@ -1,16 +1,6 @@
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/ObjectFactory.h"
 
-/// @todo Restore Particle2VertexAsct, needed for Particle to PV association
-/*
-// Associator tool
-#include "Kernel/Particle2VertexAsct.h"
-#include "Relations/RelationMACROs.h"
-#include "Relations/AssociatorMACROs.h"
-
-DECLARE_TOOL_FACTORY( Particle2VertexAsct);
-DECLARE_DATAOBJECT_FACTORY( Particle2VertexTable);
-*/
 
 // Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
 // The statements are like that:
@@ -24,7 +14,6 @@ DECLARE_FACTORY_ENTRIES( DaVinciKernel ) {
 
   DECLARE_TOOL( PhysDesktop ); 
   DECLARE_TOOL( Particle2VertexIPSAsct );
-//  DECLARE_DATAOBJECT( Particle2VertexTable );
   
 }
 
