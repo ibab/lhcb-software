810ecfc8e20ddfa9a94e4b651f77f5189ac71694
Marco Clemencic <marcocle@cern.ch>
2006-10-25 15:12:34 +0000

    Add scripts to easy the tests on win32
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@4066 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 66fd30687b69e5be8dca73c7701ccff9305c9e7f

---
+++ LHCb/Ex/DetCondExample/cmt/run_all.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem cmt config
+rem setup.bat
+
+rem prepare_DBs.bat
+
+..\%CMTCONFIG%\DetCondExample.exe ..\options\testXml.opts
+
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\fillCondDB.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testCondDB.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testCondDB-COLD.opts 
+
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testCache.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testMultiDB.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testLayers.opts 
+
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testUpdateMgr.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\testUMSError.opts 
+
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\TestTimeOut.opts 
+rem ..\%CMTCONFIG%\DetCondExample.exe ..\options\TestConditionsOverride.opts 
+
+
