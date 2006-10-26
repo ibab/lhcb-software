2272897e5092a4cb73f97d57e67abc819849ca2c
Olivier Deschamps <odescham@cern.ch>
2006-10-26 11:15:33 +0000

    add IBremAdder.h interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63415 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: edc0af654ce248da3fa7ef7983a56dc6720f7012

---
+++ Phys/Phys/DaVinciKernel/Kernel/IBremAdder.h
@@ -0,0 +1,38 @@
+// $Id: IBremAdder.h,v 1.1 2006-10-26 11:15:33 odescham Exp $
+#ifndef KERNEL_IBREMADDER_H 
+#define KERNEL_IBREMADDER_H 1
+
+// Include files
+// from STL
+#include <string>
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+// from LHCb
+#include "Event/Particle.h"
+
+static const InterfaceID IID_IBremAdder ( "IBremAdder", 1, 0 );
+
+/** @class IBremAdder IBremAdder.h Kernel/IBremAdder.h
+ *  
+ * Interface for BremStrahlung correction to electron particle
+ *
+ *  @author Olivier Deschamps
+ *  @date   2006-10-25
+ */
+class IBremAdder : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IBremAdder; }
+  // Add Brem
+  virtual  bool addBrem( LHCb::Particle* particle ) const = 0 ;
+  // Remove Brem
+  virtual  bool removeBrem( LHCb::Particle* particle ) const = 0 ;
+
+
+protected:
+
+private:
+
+};
+#endif // KERNEL_IBREMADDER_H
