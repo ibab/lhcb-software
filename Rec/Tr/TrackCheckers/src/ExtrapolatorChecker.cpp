d440aa295caeae6bdc4367dc34423f9a24a9613d
Jeroen Van Tilburg <jeroen.van.tilburg@cern.ch>
2006-11-02 15:34:45 +0000

    - Improvements in ExtrapolatorChecker: include plots of q/p pull, adapted    binning, added seperate plots for long extrapolations, apply correction on    slopes.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16539 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bd9f715a43104fde1d8631e0b181582281456b5

--- Rec/Tr/TrackCheckers/src/ExtrapolatorChecker.cpp
+++ Rec/Tr/TrackCheckers/src/ExtrapolatorChecker.cpp
@@ -1,11 +1,12 @@
-// $Id: ExtrapolatorChecker.cpp,v 1.1 2006-07-06 13:14:59 jvantilb Exp $
+// $Id: ExtrapolatorChecker.cpp,v 1.2 2006-11-02 15:34:45 jvantilb Exp $
 // Include files 
 
 // local
 #include "ExtrapolatorChecker.h"
 
 // from Gaudi
-#include "GaudiKernel/AlgFactory.h" 
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/IMagneticFieldSvc.h"
 
 // from Event/LinkerEvent
 #include "Linker/LinkedTo.h"
@@ -58,7 +59,8 @@ StatusCode ExtrapolatorChecker::initialize()
                                                   "TrackSelector", this );
   m_stateCreator = tool<IIdealStateCreator>( "IdealStateCreator"       );
   m_extrapolator = tool<ITrackExtrapolator>( "TrackMasterExtrapolator" );
-  
+  m_magSvc = svc<IMagneticFieldSvc>( "MagneticFieldSvc", true );
+
   return StatusCode::SUCCESS;
 };
 
@@ -86,7 +88,7 @@ StatusCode ExtrapolatorChecker::execute()
       MCHit* mcHit;
       int detID = findNextHit( mcPart, stateVtx->z(), mcHit, detName );
       
-      // Get the entry point pf th MCHit
+      // Get the entry point of the MCHit
       XYZPoint entryP = mcHit -> entry();
 
       // Extrapolate through RF foil
@@ -94,11 +96,16 @@ StatusCode ExtrapolatorChecker::execute()
       TrackVector vec    = stateVtx -> stateVector();
       TrackSymMatrix cov = stateVtx -> covariance();
 
+      // Correct tx and ty from the MCHit for the magnetic field
+      double tx,ty;
+      correctSlopes( mcPart, mcHit, tx, ty );
+      
       // Determine the resolutions
       double dx  = vec(0) - entryP.x();
       double dy  = vec(1) - entryP.y();
-      double dtx = vec(2) - mcHit -> dxdz();
-      double dty = vec(3) - mcHit -> dydz();
+      double dtx = vec(2) - tx;
+      double dty = vec(3) - ty;
+      double dqp = vec(4) - qOverP(mcPart, mcHit);
 
       // fill the histograms
       plot1D( dx, 1, "X resolution at 1st meas", -0.5, 0.5, 100 );
@@ -106,59 +113,88 @@ StatusCode ExtrapolatorChecker::execute()
       plot1D( dtx, 3, "Tx resolution at 1st meas", -0.01, 0.01, 100 );
       plot1D( dty, 4, "Ty resolution at 1st meas", -0.01, 0.01, 100 );
       plot1D( stateVtx->p() - mcHit->p(), 5,"dp at 1st meas", 
-	      -0.3, 0.3, 100 );
+              -0.3, 0.3, 100 );
       if ( cov(0,0) != 0 && cov(1,1) != 0 && cov(2,2) != 0 && cov(3,3) != 0){
-	plot1D( dx / sqrt(cov(0,0)), 11,"X pull at 1st meas", -5., 5., 100 );
-	plot1D( dy / sqrt(cov(1,1)), 12,"Y pull at 1st meas", -5., 5., 100 );
-	plot1D( dtx / sqrt(cov(2,2)), 13,"Tx pull at 1st meas", -5., 5., 100);
-	plot1D( dty / sqrt(cov(3,3)), 14,"Ty pull at 1st meas", -5., 5., 100);
+        plot1D( dx / sqrt(cov(0,0)), 11,"X pull at 1st meas", -5., 5., 100 );
+        plot1D( dy / sqrt(cov(1,1)), 12,"Y pull at 1st meas", -5., 5., 100 );
+        plot1D( dtx / sqrt(cov(2,2)), 13,"Tx pull at 1st meas", -5., 5., 100);
+        plot1D( dty / sqrt(cov(3,3)), 14,"Ty pull at 1st meas", -5., 5., 100);
       }
+      if ( cov(4,4) != 0 )
+        plot1D( dqp / sqrt(cov(4,4)), 15,"q/p pull at 1st meas", -5., 5., 100);
+
       delete stateVtx;
 
       const Gaudi::TrackSymMatrix zeroCov;
       State state;
       state.setState( entryP.x(),  entryP.y(), entryP.z(),
-		      mcHit -> dxdz(), mcHit -> dydz(), qOverP(mcPart, mcHit));
+                      tx, ty, qOverP(mcPart, mcHit));
       state.setCovariance( zeroCov );
 
       detID = findNextHit( mcPart, state.z(), mcHit, detName );
 
       while( 0 != mcHit ) {
 
-	entryP = mcHit -> entry(); 
+        entryP = mcHit -> entry(); 
+        double dz = entryP.z() - state.z();
 	
-	// Extrapolate to next MCHit
-	m_extrapolator -> propagate( state, entryP.z() );
-	TrackVector vec    = state.stateVector();
-	TrackSymMatrix cov = state.covariance();
+        // Extrapolate to next MCHit
+        m_extrapolator -> propagate( state, entryP.z() );
+        TrackVector vec    = state.stateVector();
+        TrackSymMatrix cov = state.covariance();
 
-	// Determine the resolutions
-	double dx  = vec(0) - entryP.x();
-	double dy  = vec(1) - entryP.y();
-	double dtx = vec(2) - mcHit -> dxdz();
-	double dty = vec(3) - mcHit -> dydz();
+        // Correct tx and ty from the MCHit for the magnetic field
+        correctSlopes( mcPart, mcHit, tx, ty );
 
-	// fill the histograms
-	int ID = 100 * detID; 
-	std::string title = detName + " hits" ;
-	plot1D( dx, ID+1, "X resolution at "+title, -0.5, 0.5, 100 );
-	plot1D( dy, ID+2, "Y resolution at "+title, -0.5, 0.5, 100 );
-	plot1D( dtx, ID+3, "Tx resolution at "+title, -0.01, 0.01, 100 );
-	plot1D( dty, ID+4, "Ty resolution at "+title, -0.01, 0.01, 100 );
-	plot1D( state.p() - mcHit->p(), ID+5,"dp at "+title, 
-		-0.3, 0.3, 100 );
-	if ( cov(0,0) != 0 && cov(1,1) != 0 && cov(2,2) != 0 && cov(3,3) != 0){
-	  plot1D( dx/sqrt(cov(0,0)), ID+11, "X pull at "+title, -5., 5.,100);
-	  plot1D( dy/sqrt(cov(1,1)), ID+12, "Y pull at "+title, -5., 5.,100);
-	  plot1D( dtx/sqrt(cov(2,2)), ID+13, "Tx pull at "+title, -5., 5.,100);
-	  plot1D( dty/sqrt(cov(3,3)), ID+14, "Ty pull at "+title, -5., 5.,100);
-	}
-	
-	state.setState( entryP.x(),  entryP.y(), entryP.z(),
-			mcHit -> dxdz(), mcHit -> dydz(),qOverP(mcPart,mcHit));
-	state.setCovariance( zeroCov );
+        // Determine the resolutions
+        double dx  = vec(0) - entryP.x();
+        double dy  = vec(1) - entryP.y();
+        double dtx = vec(2) - tx;
+        double dty = vec(3) - ty;
+        double dqp = vec(4) - qOverP(mcPart, mcHit);
 
-	detID = findNextHit( mcPart, entryP.z(), mcHit, detName );
+        // Define the ranges for the resolution plots
+        double xr = 0.04;
+        double tr = 0.001;
+        double pr = 0.3;
+
+        // Determine the histogram title
+        int ID = 100 * detID; 
+        std::string title = " at " + detName + " hits" ;
+        if ( dz > 4.*m && (detName == "OT" || detName == "IT" )) {
+          ID = 1100;
+          title = " after magnet extrapolation";
+          xr = 5.;
+          tr = 0.005;
+          pr = 5.;
+        } else if ( dz > 1.*m && detName == "TT" ) {          
+          ID = 1000;
+          title = " after RICH1 extrapolation";
+          xr = 5.;
+          tr = 0.005;
+          pr = 10.;
+        }
+
+        // fill the histograms
+        plot1D( dx, ID+1, "X resolution"+title, -xr, xr, 100 );
+        plot1D( dy, ID+2, "Y resolution"+title, -xr, xr, 100 );
+        plot1D( dtx, ID+3, "Tx resolution"+title, -tr, tr, 100 );
+        plot1D( dty, ID+4, "Ty resolution"+title, -tr, tr, 100 );
+        plot1D( state.p() - mcHit->p(), ID+5,"dp"+title, -pr, pr, 100 );
+        if ( cov(0,0) != 0 && cov(1,1) != 0 && cov(2,2) != 0 && cov(3,3) != 0){
+          plot1D( dx/sqrt(cov(0,0)), ID+11, "X pull"+title, -5., 5.,100);
+          plot1D( dy/sqrt(cov(1,1)), ID+12, "Y pull"+title, -5., 5.,100);
+          plot1D( dtx/sqrt(cov(2,2)), ID+13, "Tx pull"+title, -5., 5.,100);
+          plot1D( dty/sqrt(cov(3,3)), ID+14, "Ty pull"+title, -5., 5.,100);
+        }
+	      if ( cov(4,4) != 0 )
+          plot1D( dqp / sqrt(cov(4,4)), ID+15,"q/p pull"+title, -5., 5., 100);
+
+        state.setState( entryP.x(),  entryP.y(), entryP.z(),
+                        tx, ty, qOverP(mcPart,mcHit));
+        state.setCovariance( zeroCov );
+        
+        detID = findNextHit( mcPart, entryP.z(), mcHit, detName );
       }
     }
   } // End loop over MCParticles
@@ -174,15 +210,14 @@ StatusCode ExtrapolatorChecker::finalize()
   return GaudiHistoAlg::finalize();
 }
 
-
 //=============================================================================
 // Find the next MCHit starting from a given z
 // looping over the hits in all the tracking detectors
 //=============================================================================
 int ExtrapolatorChecker::findNextHit( const MCParticle* mcPart,
-				      const double zRec,
-				      MCHit*& closestHit,
-				      std::string& detectorName) const
+                                      const double zRec,
+                                      MCHit*& closestHit,
+                                      std::string& detectorName) const
 {
   detectorName = "Not found!";
   int detID = 0;
@@ -193,11 +228,11 @@ int ExtrapolatorChecker::findNextHit( const MCParticle* mcPart,
   std::vector<std::string>::const_iterator itDets;
   for( itDets = m_dets.begin(); itDets < m_dets.end(); ++itDets ) {
     findNextXxxHit( mcPart, zRec,
-		    MCParticleLocation::Default + "2MC" + *itDets + "Hits",
-		    tmpClosestHit );
+                    MCParticleLocation::Default + "2MC" + *itDets + "Hits",
+                    tmpClosestHit );
     if ( tmpClosestHit != 0 &&
-	 tmpClosestHit->entry().z() > zRec && 
-	 tmpClosestHit->entry().z() < closestZ ) {
+         tmpClosestHit->entry().z() > zRec && 
+         tmpClosestHit->entry().z() < closestZ ) {
       closestHit = tmpClosestHit;
       closestZ = tmpClosestHit->entry().z();
       detectorName = *itDets;
@@ -211,9 +246,9 @@ int ExtrapolatorChecker::findNextHit( const MCParticle* mcPart,
 // Find the next MCHit of type Xxx starting from a given z
 //=============================================================================
 void ExtrapolatorChecker::findNextXxxHit( const MCParticle* mcPart,
-					  const double zRec,
-					  std::string linkPath,
-					  MCHit*& closestHit ) const
+                                          const double zRec,
+                                          std::string linkPath,
+                                          MCHit*& closestHit ) const
 {
   // Retrieve MCParticle to MCHit linker tables
   LinkedFrom<MCHit,MCParticle> mcp2mchitLink( evtSvc(), msgSvc(), linkPath );
@@ -233,19 +268,38 @@ void ExtrapolatorChecker::findNextXxxHit( const MCParticle* mcPart,
 }
 
 //=============================================================================
-//  
+// Determine q/p given an MCHit and a MCParticle
 //=============================================================================
 double ExtrapolatorChecker::qOverP( const MCParticle* mcPart,
-				    const MCHit* mcHit ) const
+                                    const MCHit* mcHit ) const
 {
-  double momentum = mcPart -> p();
-  if ( mcHit != NULL ) momentum = mcHit -> p();
   double charge = ( mcPart -> particleID().threeCharge() ) / 3.;
-  // TODO: hack as it happens that some MCHits have p = 0!
-  if ( mcHit != NULL &&  mcHit -> p() == 0. ) momentum = mcPart -> p();
-  if( momentum > TrackParameters::lowTolerance ) {
-    return charge / momentum;
-  }
-  else { return 0.; }
+  double momentum = mcPart -> p();
+  if ( mcHit != NULL && mcHit -> p() != 0. ) momentum = mcHit -> p();
+  return ( momentum > TrackParameters::lowTolerance ) ? charge / momentum : 0.;
 }
+
+//=============================================================================
+// Correct slopes for magnetic field given an MCHit and a MCParticle
+//=============================================================================
+void ExtrapolatorChecker::correctSlopes( const MCParticle* mcPart,
+                                         const MCHit* mcHit,
+                                         double& tx, double& ty ) const
+{
+  // TODO: I hope this method can be removed as soon as the displacement vector
+  // in the MCHit is calculated in Gauss using the momentum direction of the
+  // *entry point*. (JvT: 27/10/2006).
+
+  // Get magnetic field vector
+  Gaudi::XYZVector B;
+  m_magSvc -> fieldVector( mcHit -> midPoint() , B );
+
+  // Calculate new displacement vector and tx,ty slopes
+  Gaudi::XYZVector d = mcHit -> displacement();
+  Gaudi::XYZVector dNew = d - ( 0.5 * d.R() * qOverP(mcPart, mcHit) * 
+                                d.Cross(B) * eplus * c_light);
+  tx = dNew.x() / dNew.z();
+  ty = dNew.y() / dNew.z();  
+}
+
 
--- Rec/Tr/TrackCheckers/src/ExtrapolatorChecker.h
+++ Rec/Tr/TrackCheckers/src/ExtrapolatorChecker.h
@@ -1,4 +1,4 @@
-// $Id: ExtrapolatorChecker.h,v 1.1 2006-07-06 13:14:59 jvantilb Exp $
+// $Id: ExtrapolatorChecker.h,v 1.2 2006-11-02 15:34:45 jvantilb Exp $
 #ifndef EXTRAPOLATORCHECKER_H 
 #define EXTRAPOLATORCHECKER_H 1
 
@@ -21,10 +21,35 @@ namespace LHCb
   class MCHit;
 }
 
+class IMagneticFieldSvc;
+
 /** @class ExtrapolatorChecker ExtrapolatorChecker.h
  *  
- *  ...
+ *  This algorithm checks the performance of the TrackExtrapolators. This
+ *  algorithm can be run in Boole or Brunel, since it relies only on the
+ *  presence of MCHits. Algorithm description:
+ *  @li create a State from a MCHit,
+ *  @li extrapolate this State to the next MCHit on the track,
+ *  @li compare extrapolated state with MCHit parameters,
+ *  @li and make the corresponding resolution and pull plots.
  *
+ *  The plots are split up in:
+ *  @li first MCHit (extrapolated from true vertex)
+ *  @li from VELO MCHit to VELO MCHit
+ *  @li from TT MCHit to TT MCHit
+ *  @li from IT MCHit to IT MCHit
+ *  @li from OT MCHit to OT MCHit
+ *  @li through RICH1: from VELO MCHit to TT MCHit 
+ *  @li through magnet: from TT MCHit to IT/OT MCHit 
+ *
+ *  Note that the extrapolations are done starting from the @b entry @b point
+ *  of the MCHit and extrapolating to the entry point of the next MCHit. A
+ *  correction for the magnetic field is applied on the slopes (@e tx and @e ty)
+ *  of the MCHit. This is required since the displacement vector of the MCHit 
+ *  is calculated from the difference between the exit and entry point, and
+ *  therefore this displacement vector already accounts for half the effect of
+ *  the magnetic field. This effect is subtracted in the correction method.
+ * 
  *  @author Jeroen van Tilburg
  *  @date   2006-07-06
  */
@@ -33,50 +58,54 @@ class ExtrapolatorChecker : public GaudiHistoAlg {
 
 public:
 
-  // Standard constructor
+  /// Standard constructor
   ExtrapolatorChecker( const std::string& name, ISvcLocator* pSvcLocator );
 
-  // Destructor
+  /// Destructor
   virtual ~ExtrapolatorChecker();
 
-  // Algorithm initialization
+  /// Algorithm initialization
   virtual StatusCode initialize();
 
-  // Algorithm execution
+  /// Algorithm execution
   virtual StatusCode execute();
 
-  // Algorithm finalization
+  /// Algorithm finalization
   virtual StatusCode finalize();
 
 private:
 
-  // Get the q/p for a given MCHit
-  double qOverP( const LHCb::MCParticle* mcPart, 
-		 const LHCb::MCHit* mcHit ) const;
- 
-  // Find the next MCHit belonging to the same MCParticle starting from a z-pos.
+  /// Find the next MCHit belonging to the same MCParticle starting from a z-pos
   int findNextHit( const LHCb::MCParticle* mcPart,
-		   const double zRec,
-		   LHCb::MCHit*& closestHit,
-		   std::string& detectorName) const;
+                   const double zRec,
+                   LHCb::MCHit*& closestHit,
+                   std::string& detectorName) const;
 
-  // Helper function to find the next MCHit
+  /// Helper function to find the next MCHit
   void findNextXxxHit( const LHCb::MCParticle* mcPart,
-		       const double zRec,
-		       std::string linkPath,
-		       LHCb::MCHit*& closestHit ) const;
+                       const double zRec,
+                       std::string linkPath,
+                       LHCb::MCHit*& closestHit ) const;
 
+  /// Get the q/p for a given MCHit
+  double qOverP( const LHCb::MCParticle* mcPart,
+                 const LHCb::MCHit* mcHit ) const;
+ 
+  /// Correct slopes for magnetic field given an MCHit and a MCParticle
+  void correctSlopes( const LHCb::MCParticle* mcPart, const LHCb::MCHit* mcHit,
+                      double& tx, double& ty ) const;
+ 
   // Interfaces
-  ITrackCriteriaSelector* m_trackSelector;// Pointer to TrackCriteriaSelector
-  IIdealStateCreator*     m_stateCreator; // Pointer to IdealStateCreator
-  ITrackExtrapolator*     m_extrapolator; // Pointer to TrackMasterExtrapolator
+  ITrackCriteriaSelector* m_trackSelector;///< Pointer to TrackCriteriaSelector
+  IIdealStateCreator*     m_stateCreator; ///< Pointer to IdealStateCreator
+  ITrackExtrapolator*     m_extrapolator; ///< Pointer to the extrapolator
+  IMagneticFieldSvc*      m_magSvc;       ///< Pointer to magnetic field service
 
-  // String of the available detectors.
+  /// String of the available detectors.
   std::vector<std::string> m_dets;
 
   // jobOptions
-  std::string m_trackSelectorName; // Tracks selector tool name
-
+  std::string m_trackSelectorName; ///< Tracks selector tool name
 
 };
 #endif // EXTRAPOLATORCHECKER_H
