f02d7040dfdc6b89facde77c4f171361a04b9654
Vanya Belyaev <ivan.belyaev@cern.ch>
2006-11-27 11:45:24 +0000

    update for LoKi v4r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@49710 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 649ec0790ad2b2bd7e34724f1d0c9183dd43d65e

--- Phys/Phys/LoKi/LoKi/LoKi.h
+++ Phys/Phys/LoKi/LoKi/LoKi.h
@@ -1,8 +1,11 @@
-// $Id: LoKi.h,v 1.1 2006-04-09 14:56:48 ibelyaev Exp $
+// $Id: LoKi.h,v 1.2 2006-11-27 11:45:23 ibelyaev Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.2 $
 // ============================================================================
-// $Log: not supported by cvs2svn $ 
+// $Log: not supported by cvs2svn $
+// Revision 1.1  2006/04/09 14:56:48  ibelyaev
+//  add the library
+// 
 // ============================================================================
 #ifndef LOKI_LOKI_H 
 #define LOKI_LOKI_H 1
@@ -38,8 +41,184 @@
  *  @date 2006-04-09 
  */
 // ============================================================================
+/** @namepace LoKi 
+ *  The major namespace in LoKi project
+ *  All classes resided in it
+ *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+ *  @date 2006-04-09
+ */
 namespace LoKi 
 {
+  /** @namespace LoKi::Cuts
+   *  The namespace with all LoKi fuctions and cuts 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-04-09
+   */
+  namespace Cuts   {} 
+  /** @namespace LoKi::Cast 
+   *  few casting utilities  
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-17
+   */
+  namespace  Cast {}
+  /** @namespace LoKi::Constants 
+   *  Namespace  with the collection of useful constants
+   *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+   *  @date   2003-07-10
+   */
+  namespace Constants {}  
+  /** @namespace LoKi::Algs 
+   *  Namespace with few generic "STL-like" algorithms 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25 
+   */
+  namespace Adapters {}
+  /** @namespace LoKi::Constants
+   *  Namespace  with the collection of useful constants
+   *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+   *  @date   2003-07-10
+   */
+  namespace Kinematics {}
+  /** @namespace  LoKi::Geometry
+   *  Namespace with the collection of useful "geoemtry" functions   
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-17
+   */
+  namespace Geometry{}
+  /** @namespace LoKi::Print
+   *  Collection of useful utilities for printout 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-16
+   */
+  namespace Print {}
+  /** @namespace LoKi::Objects 
+   *  Collection of useful small objects for LoKi 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-08
+   */
+  namespace Objects {}
+  /** @namespace LoKi::Particles
+   *  Simple namespace with functions related to particles 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  Particles {}  
+  /** @namespace LoKi::Vertices 
+   *  Simple namespace with functions related to vertices 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  Vertices  {}  
+  /** @namespace LoKi::MCParticles
+   *  Simple namespace with functions related to MC-particles 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  MCParticles {}  
+  /** @namespace LoKi::MCVertices 
+   *  Simple namespace with functions related to MC-vertices 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  MCVertices  {}  
+  /** @namespace LoKi::GenParticles
+   *  Simple namespace with functions related 
+   *  to Generator particles 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  GenParticles {}  
+  /** @namespace LoKi::GenVertices
+   *  Simple namespace with functions related to 
+   *  Generator particles 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-11-25
+   */
+  namespace  GenVertices {}  
+
+  /** @namespace LoKi::Types 
+   *  colelction of basic LoKi-types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-16
+   */
+  namespace Types {}
+  /** @namespace LoKi::AlgoTypes
+   *  basic types used for LoKi::Algo class 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-03-23
+   */
+  namespace AlgoTypes {}  
+  /** @namespace LoKi::CoreTypes 
+   *  The core namespace for some "core"  types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace CoreTypes {}
+  /** @namespace LoKi::MCTypes 
+   *  The core namespace for some Monte Carlo  types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace MCTypes {}
+  /** @namespace LoKi::GenTypes 
+   *  The core namespace for some Generator types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace GenTypes {}
+  /** @namespace LoKi::GenMCTypes 
+   *  The core namespace for some Generator-MC types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace GenMCTypes {}
+  /** @namespace LoKi::PhysMCTypes 
+   *  The core namespace for some Phys-MC types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace PhysMCTypes {}
+  /** @namespace LoKi::PhysTypes 
+   *  The core namespace for some Phys types 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace PhysTypes {}
+
+
+  /** @namespace LoKi::Extract
+   *  The core namespace for "extraction" function
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace Extract {}
+
+  /** @namespace  LoKi::Algs
+   *  Helper namespace with auxillary 
+   *  functions/algorithms of the general interest 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace  Algs {}
+  /** @namespace  LoKi::MCAlgs
+   *  Helper namespace with MC algorithms  
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace  MCAlgs {}
+  /** @namespace  LoKi::GenAlgs
+   *  Helper namespace with Generator algorithms  
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace  GenAlgs {}
+  /** @namespace  LoKi::PhysAlgs
+   *  Helper namespace with Phys algorithms  
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-02-10
+   */
+  namespace  PhysAlgs {}
+  
 } ; // end of namespace LoKi 
 
 // ============================================================================
