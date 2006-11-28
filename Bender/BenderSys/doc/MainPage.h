3d24841a75718bcbc1f2fd4ae8567e975a921c77
Vanya Belyaev <ivan.belyaev@cern.ch>
2006-11-28 14:55:54 +0000

    prepare for v6r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@87970 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1d819f7dd04a3a210887f29dd6b79100e0f1ecd5

---
+++ Bender/BenderSys/doc/MainPage.h
@@ -0,0 +1,28 @@
+/** \mainpage notitle
+ *  \anchor benderdoxygenmain
+ *
+ * This is the code reference manual for the Bender codes
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the BenderSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-comp/Analysis/Bender/index.html">web pages</a>
+ * of the Bender project
+ *
+ * <b>The classes used by Bender are documented in the following reference manuals:</b>
+ *
+ * \li \ref davincisysdoxygenmain "DaVinciSys documentation (DaVinci application/configuration)"
+ * \li \ref physsysdoxygenmain    "PhysSys documentation  (LHCb physics classes)"
+ * \li \ref recsysdoxygenmain     "RecSys documentation   (LHCb reconstruction classes)"
+ * \li \ref lbcomdoxygenmain      "LbcomSys documentation (LHCb shared components)"
+ * \li \ref lhcbdoxygenmain       "LHCbSys documentation" (LHCb event and detector model classes)
+ * \li \ref gaudidoxygenmain      "Gaudi documentation"   
+ * \li \ref externaldocs          "Related external libraries"
+ *
+ * \sa
+ * <a href="../release.notes"><b>Bender release notes history</b></a>
+ *
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ */
