db631fef2a5524573a8e5c181d395be86f943b24
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2006-11-30 14:26:46 +0000

    add new flag to RichRayTrace
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47541 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 01a2f8eac455a2026a62c42acd668726622f50cf

--- LHCb/Kernel/LHCbKernel/src/RichTraceMode.cpp
+++ LHCb/Kernel/LHCbKernel/src/RichTraceMode.cpp
@@ -5,7 +5,7 @@
  *  Implementation file for utility class : RichTraceMode
  *
  *  CVS History :-
- *  $Id: RichTraceMode.cpp,v 1.1 2006-08-09 10:44:19 jonrob Exp $
+ *  $Id: RichTraceMode.cpp,v 1.2 2006-11-30 14:26:46 jonrob Exp $
  *
  *  @author Chris Jones Christopher.Rob.Jones@cern.ch
  *  @date   05/08/2006
@@ -21,9 +21,10 @@ std::ostream& LHCb::RichTraceMode::fillStream(std::ostream& os) const
 {
   os << "RichTraceMode[ DetectionPrecision=" << detPrecision()
      << " DetPlaneBoundaries=" << detPlaneBound();
-  if ( forcedSide() )        { os << " [Will force given RICH side]"; }
-  if ( outMirrorBoundary() ) { os << " [Will Respect Outside Mirror Boundaries]"; }
-  if ( mirrorSegBoundary() ) { os << " [Will Respect Mirror Seg Boundaries]"; }
+  if ( forcedSide() )         { os << " [Will force given RICH side]"; }
+  if ( outMirrorBoundary() )  { os << " [Will Respect Outside Mirror Boundaries]"; }
+  if ( mirrorSegBoundary() )  { os << " [Will Respect Mirror Seg Boundaries]"; }
+  if ( beamPipeIntersects() ) { os << " [Will Test for intersections with beampipe]"; }
   return os << " ]";
 }
 
