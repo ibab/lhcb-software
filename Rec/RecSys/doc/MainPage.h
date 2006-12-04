ccf617c7eac1df8699dd3c64cd51e159b45cc7a0
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-12-04 10:34:27 +0000

    v3r14
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@29658 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dd57c6ff2a9fc60a347fb33111a526262465385e

--- Rec/RecSys/doc/MainPage.h
+++ Rec/RecSys/doc/MainPage.h
@@ -14,6 +14,6 @@
  *
  * <b>Additional information:</b>
  * \li <a href="../release.notes"><b>Release notes history</b></a>
- * \li <a href="http://cern.ch/lhcb-comp/Reconstruction/"><b>Reconstruction application Web pages</b></a><p>
+ * \li <a href=" http://cern.ch/lhcb-release-area/DOC/rec/"><b>Rec project Web pages</b></a><p>
 
  */
