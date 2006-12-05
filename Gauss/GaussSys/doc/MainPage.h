47a20ab1391f3672df4367af6ab660fa718cfd45
Gloria Corti <gloria.corti@cern.ch>
2006-12-05 20:02:01 +0000

    update link
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92808 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fa8cdc298553600f827342b7e842824a7d9c07a3

--- Gauss/GaussSys/doc/MainPage.h
+++ Gauss/GaussSys/doc/MainPage.h
@@ -17,7 +17,7 @@
  * \li \ref geant4doxygenmain  "Geant4 documentation (Simulation toolkit)"
  * \li <a href="http://mdobbs.home.cern.ch/mdobbs/HepMC/html_reference/index.html"> HepMC (Generator event format)</a>
  * \li <a href="http://www.thep.lu.se/~torbjorn/Pythia.html"> Pythia - pp collisions default generator in Gauss</a>
- * \li <a href="http://lhcb-comp.web.cern.ch/lhcb-comp/Simulation/evtgen.htm"> EvtGen in LHCb - decay package</a>
+ * \li <a href="http://cern.ch/LHCb-release-area/DOC/gauss/evtgen.htm"> EvtGen in LHCb - decay package</a>
  * \li \ref externaldocs       "Related external libraries"
  *
  * <hr>
