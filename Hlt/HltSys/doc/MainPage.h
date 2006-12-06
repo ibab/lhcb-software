022c5aad1002a1ba9aa4254e4dbea595a2ef0046
Marco Cattaneo <marco.cattaneo@cern.ch>
2006-12-06 16:12:44 +0000

    first import of HltSys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@69646 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 28391958dd34f32cd8ca30643a86d4f634136ecc

---
+++ Hlt/HltSys/doc/MainPage.h
@@ -0,0 +1,19 @@
+/** \mainpage notitle
+ *  \anchor hltsysdoxygenmain
+ *
+ * This is the code reference manual for the LHCb High Level Trigger classes. See:
+ * <a href="requirements-source.html"><b>HltSys constituent packages</b></a>
+ * <p><b>See also:</b>
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
+ * \li \ref externaldocs     "Related external libraries"
+
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ *
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href=" http://cern.ch/lhcb-release-area/DOC/hlt/"><b>Hlt project Web pages</b></a><p>
+
+ */
