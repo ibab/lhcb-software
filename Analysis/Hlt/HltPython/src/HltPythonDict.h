51cbc03cc9483b0f0e86de9396e9897898be93b5
Jose Angel Hernando Morata <jose.angel.hernando-morata@cern.ch>
2006-12-20 09:57:34 +0000

    exposing Hlt tools to Python
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@69691 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5ea7d73b7a61a0b2a9e78c022de7ea766312e78d

---
+++ Analysis/Hlt/HltPython/src/HltPythonDict.h
@@ -0,0 +1,9 @@
+#ifndef DICT_HLTPYTHONDICT_H 
+#define DICT_HLTPYTHONDICT_H 1
+
+#include "HltBase/IHltTrackL0CaloMatchTool.h"
+#include "HltBase/IL0CaloStatesTool.h"
+#include "HltBase/IHltDataStore.h"
+
+#endif // DICT_TRACKPYTHONDICT_H
+
