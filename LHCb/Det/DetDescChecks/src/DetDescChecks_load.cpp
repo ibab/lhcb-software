35db6ce2692a7aef34103bac11bc42876d8c5d64
Marco Clemencic <marcocle@cern.ch>
2007-01-31 13:44:47 +0000

    - Added the algorithm LoadDDDB to validate the consistency of the DTS    (cross references).  - Added commented out options and requirements to use Det/SQLDDDB.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@4122 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f982c7c17cecb6c3711cde3407f9ee5521745fe3

--- LHCb/Det/DetDescChecks/src/DetDescChecks_load.cpp
+++ LHCb/Det/DetDescChecks/src/DetDescChecks_load.cpp
@@ -1,8 +1,11 @@
-// $Id: DetDescChecks_load.cpp,v 1.1.1.1 2002-05-26 12:47:06 ibelyaev Exp $
+// $Id: DetDescChecks_load.cpp,v 1.2 2007-01-31 13:44:46 marcocle Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
-// $Log: not supported by cvs2svn $ 
+// $Log: not supported by cvs2svn $
+// Revision 1.1.1.1  2002/05/26 12:47:06  ibelyaev
+// New package: collection of components for checks of Detector Description
+// 
 // ============================================================================
 // Include files
 #include "GaudiKernel/DeclareFactoryEntries.h"
@@ -20,6 +23,7 @@ DECLARE_FACTORY_ENTRIES(DetDescChecks)
   
   DECLARE_ALGORITHM( MaterialBudgetAlg ) ;
   DECLARE_ALGORITHM( VolumeCheckAlg    ) ;
+  DECLARE_ALGORITHM( LoadDDDB          ) ;
   
 };
 
