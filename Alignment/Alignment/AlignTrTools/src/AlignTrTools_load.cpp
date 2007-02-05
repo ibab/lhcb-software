85e32928a8682e16891b99cf7153224e6d579b72
Johan Blouw <j.blouw@cern.ch>
2007-02-05 18:26:36 +0000

    v2r2 added ATrackSelector and updated factory to reflet that
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88007 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 47c1012ceadf3002a0265d45c9f6c2a4eb80227e

--- Alignment/Alignment/AlignTrTools/src/AlignTrTools_load.cpp
+++ Alignment/Alignment/AlignTrTools/src/AlignTrTools_load.cpp
@@ -3,5 +3,9 @@
 DECLARE_FACTORY_ENTRIES( AlignTrTools ) {
   DECLARE_ALGORITHM( AlignTrackMonitor );
   DECLARE_TOOL( AlignSelTool );
+<<<<<<< AlignTrTools_load.cpp
+  DECLARE_ALGORITHM( ATrackSelector );
+=======
+>>>>>>> 1.4
 }
 
