6faf9dc5b4b7f6fdb267ab96caaa13a34c8423eb
Johan Blouw <j.blouw@cern.ch>
2007-02-06 09:14:17 +0000

    ready for retagging v2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88016 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 98296394587a64ebcd5c26414e04d5c0c1b91bca

--- Alignment/Alignment/AlignTrTools/src/AlignTrTools_load.cpp
+++ Alignment/Alignment/AlignTrTools/src/AlignTrTools_load.cpp
@@ -3,9 +3,6 @@
 DECLARE_FACTORY_ENTRIES( AlignTrTools ) {
   DECLARE_ALGORITHM( AlignTrackMonitor );
   DECLARE_TOOL( AlignSelTool );
-<<<<<<< AlignTrTools_load.cpp
   DECLARE_ALGORITHM( ATrackSelector );
-=======
->>>>>>> 1.4
 }
 
