3acc7086abba24891d96dd7a212f2f0f9c6bde25
Julien Cogan <jucogan@cern.ch>
2007-02-09 14:05:57 +0000

    Add L0MuonCandidate::muonTileIDs method. Remove L0MuonCandidate::pad.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35589 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6614b66dcf9296f5d2dd9e122b47d187d4fb777a

--- LHCb/Event/L0Event/src/L0MuonCandidate.cpp
+++ LHCb/Event/L0Event/src/L0MuonCandidate.cpp
@@ -1,4 +1,4 @@
-// $Id: L0MuonCandidate.cpp,v 1.9 2005-12-20 13:47:37 ocallot Exp $
+// $Id: L0MuonCandidate.cpp,v 1.10 2007-02-09 14:05:57 jucogan Exp $
 
 #include <cmath>
 #include <vector>
@@ -12,21 +12,21 @@ LHCb::L0MuonCandidate::L0MuonCandidate() :
                  m_encodedPt(0){
 }
 
-LHCb::L0MuonCandidate::L0MuonCandidate(double pt, double theta, double phi, const std::vector<MuonTileID> &pads, int encodedPt) {
+LHCb::L0MuonCandidate::L0MuonCandidate(double pt, double theta, double phi, 
+                                       const std::vector<LHCb::MuonTileID> &pads, int encodedPt) {
   m_pt        = pt;
   m_theta     = theta;
   m_phi       = phi;
   m_pads      = pads;
   m_encodedPt = encodedPt;
-}				 
+}
 
-
-LHCb::MuonTileID LHCb::L0MuonCandidate::pad(unsigned int station) const {
+std::vector<LHCb::MuonTileID> LHCb::L0MuonCandidate::muonTileIDs(unsigned int station) const {
+  std::vector<LHCb::MuonTileID> mids;
   if ( station < m_pads.size() ) {
-    return m_pads[station];
-  } else {
-    return MuonTileID();
-  }
+    mids = m_pads[station].layout().tiles(m_pads[station]);
+  } 
+  return mids;
 }
 
 
