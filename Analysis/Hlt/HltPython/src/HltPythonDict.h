6f4acc56a1669b79417bded2c7d4c8d912c090d8
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-02-12 17:23:49 +0000

    remove obsolete interfaces
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@69848 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5dbf356d0ee21f70aacb580768a85bd9d616dbcc

--- Analysis/Hlt/HltPython/src/HltPythonDict.h
+++ Analysis/Hlt/HltPython/src/HltPythonDict.h
@@ -1,8 +1,7 @@
 #ifndef DICT_HLTPYTHONDICT_H 
 #define DICT_HLTPYTHONDICT_H 1
 
-#include "HltBase/IHltTrackL0CaloMatchTool.h"
-#include "HltBase/IL0CaloStatesTool.h"
+#include "HltBase/ITrackL0CaloMatchTool.h"
 #include "HltBase/IHltDataStore.h"
 
 #endif // DICT_TRACKPYTHONDICT_H
