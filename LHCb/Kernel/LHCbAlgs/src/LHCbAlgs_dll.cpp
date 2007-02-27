77e63a8c494ebd6456afd2858f54dfe8f33c4cb3
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-02-27 16:07:58 +0000

    change include order to please windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47595 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c2bba4bb7af2b99a650e31b41c4fd1996f11537c

--- LHCb/Kernel/LHCbAlgs/src/LHCbAlgs_dll.cpp
+++ LHCb/Kernel/LHCbAlgs/src/LHCbAlgs_dll.cpp
@@ -1,8 +1,8 @@
-// $Id: LHCbAlgs_dll.cpp,v 1.2 2007-02-26 12:02:06 cattanem Exp $
+// $Id: LHCbAlgs_dll.cpp,v 1.3 2007-02-27 16:07:58 cattanem Exp $
 // Include files 
-#include "GaudiKernel/LoadFactoryEntries.h"
-#include "GaudiKernel/AlgFactory.h"
 #include "Kernel/LbAppInit.h"
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/LoadFactoryEntries.h" // Put this include last, to please winwows
 
 // Put LbAppInit base class factory in this library, to avoid having it in
 // LHCbKernel component library
