e076091a2a5bf1b54ccda190468f9ab08fa14f4c
Marco Clemencic <marcocle@cern.ch>
2007-02-28 18:28:48 +0000

    Added dictionaries for IXmlParserSvc and IXmlEntityResolverSvc
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@4196 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4b6069926450cab8dfc4045ec02aac60d4f4cd20

---
+++ LHCb/Tools/XmlTools/dict/XmlToolsDict.h
@@ -0,0 +1,9 @@
+// $Id: XmlToolsDict.h,v 1.1 2007-02-28 18:28:48 marcocle Exp $
+#ifndef DICT_XMLTOOLSDICT_H 
+#define DICT_XMLTOOLSDICT_H 1
+
+// Include files
+#include "XmlTools/IXmlEntityResolverSvc.h"
+#include "XmlTools/IXmlParserSvc.h"
+
+#endif // DICT_XMLTOOLSDICT_H
