b1feabd4cd003dd064409a53760b08cb7e233df0
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-03-01 20:32:08 +0000

    add implementation file for IMCReconstructible
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35650 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 298593bfae1aa201108f9bcc919116b6fb37e7fc

---
+++ LHCb/Event/MCEvent/src/IMCReconstructible.cpp
@@ -0,0 +1,34 @@
+
+//-----------------------------------------------------------------------------
+/** @file IMCReconstructible.h
+ *
+ *  Implementation file for class : IMCReconstructible
+ *
+ *  CVS Log :-
+ *  $Id: IMCReconstructible.cpp,v 1.1 2007-03-01 20:32:08 jonrob Exp $
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date 28/02/2007
+ */
+//-----------------------------------------------------------------------------
+
+// local
+#include "Kernel/IMCReconstructible.h"
+
+//-----------------------------------------------------------------------------
+
+std::string IMCReconstructible::text( const RecCategory cat )
+{
+  switch ( cat )
+  {
+  case IMCReconstructible::NotReconstructible : return "Not Reconstructible";
+  case IMCReconstructible::ChargedLong        : return "Charged Long Particle";
+  case IMCReconstructible::ChargedDownstream  : return "Charged Downstream Particle";
+  case IMCReconstructible::ChargedUpstream    : return "Charged Upstream Particle";
+  case IMCReconstructible::ChargedTtrack      : return "Charged Ttrack Particle";
+  case IMCReconstructible::ChargedVelo        : return "Charged VELO Particle";
+  case IMCReconstructible::Neutral            : return "Neutral Particle";
+  case IMCReconstructible::NoClassification   : return "No MC Classification";
+  default                                     : return "UNKNOWN TYPE";
+  }
+}
