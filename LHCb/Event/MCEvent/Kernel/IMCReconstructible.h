0df143a4a2e66bc2e565f09210aff60c1c562ec3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-03-01 20:14:49 +0000

    update interface for MCReconstructible tool add additional charge sharing check to MCRichDigitHistoryCode::isSignal
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35649 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 053b68a03458123a5abf9c6b7a38c1ed9ea5d4cd

--- LHCb/Event/MCEvent/Kernel/IMCReconstructible.h
+++ LHCb/Event/MCEvent/Kernel/IMCReconstructible.h
@@ -1,45 +1,90 @@
-// $Id: IMCReconstructible.h,v 1.1 2007-01-12 14:17:33 cattanem Exp $
+
+//-----------------------------------------------------------------------------
+/** @file IMCReconstructible.h
+ *
+ *  Header file for class : IMCReconstructible
+ *
+ *  CVS Log :-
+ *  $Id: IMCReconstructible.h,v 1.2 2007-03-01 20:14:49 jonrob Exp $
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date 28/02/2007
+ */
+//-----------------------------------------------------------------------------
+
 #ifndef KERNEL_IMCEFFRECONSTRUCTIBLE_H 
 #define KERNEL_IMCEFFRECONSTRUCTIBLE_H 1
 
-// Include files
 // from STL
 #include <string>
 
-// from Gaudi
+// Gaudi
 #include "GaudiKernel/IAlgTool.h"
 
-namespace LHCb{
-  class MCParticle ;
+// base class
+#include "Kernel/IMCParticleSelector.h"
+
+// Event Model
+namespace LHCb
+{
+  class MCParticle;
 }
 
+/// Interface ID
 static const InterfaceID IID_IMCReconstructible ( "IMCReconstructible", 1, 0 );
 
 /** @class IMCReconstructible IMCReconstructible.h Kernel/IMCReconstructible.h
  *  
+ *  Returns if an MCParticle should be reconstructible or not
  *
  *  @author Christian Jacoby
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
  *  @date   2004-04-28
  */
-class IMCReconstructible : virtual public IAlgTool {
+class IMCReconstructible : virtual public IAlgTool
+{
+
 public: 
 
-  /// Reconstructability category
-  enum RecblCategory { NotRec=0, Long=1, TTT=2, VTT=3, Neutral=4 };
+  /// Reconstructability categories
+  enum RecCategory 
+    { 
+      NoClassification=-1,  ///< No MC classification possible (e.g. NO MC)
+      NotReconstructible=0, ///< Not reconstructible at all
+      ChargedLong,          ///< Reconstructible as a Long charged track
+      ChargedDownstream,    ///< Reconstructible as a Downstream charged track
+      ChargedUpstream,      ///< Reconstructible as an Upstream charged track
+      ChargedTtrack,        ///< Reconstructible as a T charged track
+      ChargedVelo,          ///< Reconstructible as a VELO charged track
+      Neutral=50            ///< Reconstructible as a Neutral particle (To Be Extended)
+    };
+
+  /// Convert type enum to string
+  static std::string text( const RecCategory cat );
+
+public:
 
   /// Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IMCReconstructible; }
 
-  /// Get the reconstruction category for the given MCParticle
-  virtual IMCReconstructible::RecblCategory reconstructible( const LHCb::MCParticle* mcPart ) = 0;
+  /** Get the reconstruction category for the given MCParticle
+   *
+   *  @param mcPart Pointer to the MCParticle
+   *
+   *  @return The Reconstructibility category
+   */  
+  virtual IMCReconstructible::RecCategory reconstructible( const LHCb::MCParticle* mcPart ) const = 0;
 
-  /// Is the MCParticle in the detector acceptance?
-  virtual bool accepted( const LHCb::MCParticle* part ) = 0;
- 
-
-protected:
-
-private:
+  /** Is the MCParticle in the detector acceptance or not
+   *
+   *  Acceptance criteria depend on the particle type
+   *
+   *  @param mcPart Pointer to the MCParticle
+   *
+   *  @return boolean indicating if the MCParticle is in acceptance or not
+   */
+  virtual bool inAcceptance( const LHCb::MCParticle* mcPart ) const = 0;
 
 };
+
 #endif // KERNEL_IMCEFFRECONSTRUCTIBLE_H
