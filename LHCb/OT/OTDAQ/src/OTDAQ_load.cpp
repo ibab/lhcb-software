485e87bcf2ac6fa60fbfb59609192185f5c27db4
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-03-05 12:36:06 +0000

    remove obsolete file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@58561 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a94f79d85a9d7d3c80e037959c2b9b642d589d0d

--- LHCb/OT/OTDAQ/src/OTDAQ_load.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: OTDAQ_load.cpp,v 1.5 2006-02-02 15:31:50 janos Exp $
-// Include files 
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_FACTORY_ENTRIES(OTDAQ) {
-  DECLARE_TOOL( OTReadOutWindow );
-  DECLARE_ALGORITHM( OTTimeCreator );
-  DECLARE_ALGORITHM( OTFillEventFromOTTime );
-}
