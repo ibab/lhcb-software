990ffd0f3b91d97fc51538419e2ae0df41e4915d
Patrick Robbe <robbe@lal.in2p3.fr>
2007-03-08 13:43:21 +0000

    pileup tool for rare processes generation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@92910 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0fe81ac50d6bd3b78371bc7daf91a5551b907c39

---
+++ Gauss/Gen/Generators/src/component/FixedLuminosityForRareProcess.cpp
@@ -0,0 +1,96 @@
+// $Id: FixedLuminosityForRareProcess.cpp,v 1.1 2007-03-08 13:43:21 robbep Exp $
+// Include files 
+
+// local
+#include "FixedLuminosityForRareProcess.h"
+
+// from Gaudi
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IRndmGenSvc.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+// From Generators
+#include "Generators/GenCounters.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : FixedLuminosityForRareProcess
+//
+// 2005-08-17 : Patrick Robbe
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+
+DECLARE_TOOL_FACTORY( FixedLuminosityForRareProcess );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+FixedLuminosityForRareProcess::FixedLuminosityForRareProcess( const std::string& type,
+                                  const std::string& name,
+                                  const IInterface* parent )
+  : GaudiTool ( type, name , parent ) ,
+    m_nEvents( 0 ) {
+    declareInterface< IPileUpTool >( this ) ;
+    declareProperty ( "Luminosity"    , m_luminosity    = 2.e32 /Gaudi::Units::cm2/Gaudi::Units::s      ) ;
+    declareProperty ( "CrossingRate"  , m_crossingRate  = 30.0 * Gaudi::Units::megahertz  ) ;
+    declareProperty ( "TotalXSection" , m_totalXSection = 102.4 * Gaudi::Units::millibarn ) ;
+}
+
+//=============================================================================
+// Destructor 
+//=============================================================================
+FixedLuminosityForRareProcess::~FixedLuminosityForRareProcess( ) { ; }
+
+//=============================================================================
+// Initialize method
+//=============================================================================
+StatusCode FixedLuminosityForRareProcess::initialize( ) {
+  StatusCode sc = GaudiTool::initialize( ) ;
+  if ( sc.isFailure() ) return sc ;
+
+  // Initialize the number generator
+  m_randSvc = svc< IRndmGenSvc >( "RndmGenSvc" , true ) ;  
+
+  info() << "Poisson distribution with fixed luminosity. " << endmsg ;
+  info() << "Luminosity (10^32 / cm^2 s): " 
+         << m_luminosity / 1.e32 * Gaudi::Units::cm2 * Gaudi::Units::s << endmsg ;
+  info() << "Bunch crossing rate (MHz): " << m_crossingRate / Gaudi::Units::megahertz 
+         << endmsg ;
+  info() << "Total cross section (mbarn): " << m_totalXSection / Gaudi::Units::millibarn 
+         << endreq ;
+
+  m_mean = m_luminosity * m_totalXSection / m_crossingRate ;
+
+  return sc ;
+}
+
+//=============================================================================
+// Compute the number of pile up to generate according to beam parameters
+//=============================================================================
+unsigned int FixedLuminosityForRareProcess::numberOfPileUp( double & currentLuminosity ) {
+  currentLuminosity = m_luminosity ;
+  unsigned int result = 0 ;
+  m_nEvents++ ;
+  Rndm::Numbers poissonGenerator( m_randSvc , Rndm::Poisson( m_mean ) ) ;
+  result = (unsigned int) poissonGenerator() + 1.0 ;
+  return result ;
+}
+
+//=============================================================================
+// Print the specific pile up counters
+//=============================================================================
+void FixedLuminosityForRareProcess::printPileUpCounters( ) {
+  using namespace GenCounters ;
+  info() << "***********   Luminosity counters   **************" << std::endl ;
+  printCounter( info() , "all events (including empty events)", m_nEvents ) ;
+  info() << endmsg ;
+}
+
+//=============================================================================
+// Finalize method
+//=============================================================================
+StatusCode FixedLuminosityForRareProcess::finalize( ) {
+  release( m_randSvc ) ;
+  return GaudiTool::finalize( ) ;
+}
