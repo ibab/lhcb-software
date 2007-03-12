45be95283d53c7eb7085818f95c4245934b7a696
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-03-12 07:45:19 +0000

    add doxygen files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@69997 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 76c1a4000d32007a809093405ec0b4d0b4efdcbc

---
+++ Analysis/AnalysisSys/doc/MainPage.h
@@ -0,0 +1,24 @@
+/** \mainpage notitle
+ *  \anchor analysissysdoxygenmain
+ *
+ * This is the code reference manual for the Physics Analysis classes.
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the AnalysisSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-release-area/DOC/davinci/">web pages</a>
+ * of the DaVinci project
+ *
+ * \sa
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
+ * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref externaldocs     "Related external libraries"
+ * \li <a href="https://twiki.cern.ch/twiki/bin/view/LHCb/DaVinci">DaVinci wiki page</a>
+ 
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href="http://cern.ch/lhcb-release-area/DOC/analysis/"><b>Analysis project Web pages</b></a><p>
+ */
