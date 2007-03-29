415de8d5d2847ad61d8490a5fb8cef649412b86a
janos <janos@cern.ch>
2007-03-29 01:42:51 +0000

    fix includes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88045 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4417df5e07c01cc99d763177e42d6e9468f87557

--- Alignment/Alignment/TAlignment/src/AlignAlgorithm.h
+++ Alignment/Alignment/TAlignment/src/AlignAlgorithm.h
@@ -1,4 +1,4 @@
-// $Id: AlignAlgorithm.h,v 1.1 2007-03-09 17:08:56 janos Exp $
+// $Id: AlignAlgorithm.h,v 1.2 2007-03-29 01:42:51 janos Exp $
 #ifndef ALIGNALGORITHM_H 
 #define ALIGNALGORITHM_H 1
 
@@ -9,7 +9,7 @@
 
 // from Gaudi
 #include "GaudiAlg/GaudiAlgorithm.h"
-#include "Kernel/GenericMatrixTypes.h"
+#include "GaudiKernel/GenericMatrixTypes.h"
 #include "GaudiKernel/Point3DTypes.h"
 
 // forward declarations
