b47261e1cfd884ee48eebe3169c0facfa5b8e939
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-04-10 13:55:55 +0000

    add dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@23928 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9b4a985b2f072726026cce78f0bbbf90a579f094

---
+++ LHCb/Det/VeloDet/dict/VeloDetDict.h
@@ -0,0 +1,13 @@
+// $Id: VeloDetDict.h,v 1.1 2007-04-10 13:55:54 cattanem Exp $
+// ============================================================================
+#ifndef VELODET_DETDICT_H 
+#define VELODET_DETDICT_H 1
+
+#include "VeloDet/DeVelo.h"
+#include "VeloDet/DeVeloSensor.h"
+#include "VeloDet/DeVeloPhiType.h"
+#include "VeloDet/DeVeloRType.h"
+// ============================================================================
+#endif // VELODET_DETDICT_H
+// ============================================================================
+
