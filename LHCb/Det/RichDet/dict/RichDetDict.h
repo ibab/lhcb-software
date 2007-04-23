b8ff5e326afcc78af3c20bb5bd492091cbc1498a
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-04-23 16:21:56 +0000

    add LCG dicts
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@8669 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3588b1d02c1b9170664df6553cd043c98389701d

---
+++ LHCb/Det/RichDet/dict/RichDetDict.h
@@ -0,0 +1,25 @@
+// $Id: RichDetDict.h,v 1.1 2007-04-23 16:21:56 cattanem Exp $
+// ============================================================================
+#ifndef RICHDET_DETDICT_H 
+#define RICHDET_DETDICT_H 1
+
+#include "RichDet/DeRich1.h"
+#include "RichDet/DeRich2.h"
+#include "RichDet/DeRichAerogelRadiator.h"
+#include "RichDet/DeRichBeamPipe.h"
+#include "RichDet/DeRichGasRadiator.h"
+#include "RichDet/DeRich.h"
+#include "RichDet/DeRichHPDPanel.h"
+#include "RichDet/DeRichHPD.h"
+#include "RichDet/DeRichMultiSolidRadiator.h"
+#include "RichDet/DeRichRadiator.h"
+#include "RichDet/DeRichSingleSolidRadiator.h"
+#include "RichDet/DeRichSphMirror.h"
+#include "RichDet/DeRichSystem.h"
+#include "RichDet/Rich1DTabFunc.h"
+#include "RichDet/Rich1DTabProperty.h"
+#include "RichDet/RichMirrorSegPosition.h"
+
+// ============================================================================
+#endif // RICHDET_DETDICT_H
+// ============================================================================
