58ec7d04397d4af84dc283ae0651746d855c2e31
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-04-24 11:57:57 +0000

    Fix for Windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@8684 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4923d89516363b61e0f0e80755f3a4cf57e5fb91

--- LHCb/Rich/RichRecBase/dict/RichRecBaseDict.h
+++ LHCb/Rich/RichRecBase/dict/RichRecBaseDict.h
@@ -7,23 +7,25 @@
 #include "RichRecBase/IRichExpectedTrackSignal.h"
 #include "RichRecBase/IRichGasQuartzWindowAbs.h"
 #include "RichRecBase/IRichGeomEff.h"
+#include "RichRecBase/IRichPhotonPredictor.h"
+#include "RichRecBase/IRichPhotonSignal.h"
+#include "RichRecBase/IRichRayleighScatter.h"
+#include "RichRecBase/IRichSellmeirFunc.h"
+#include "RichRecBase/IRichSignalDetectionEff.h"
+#include "RichRecBase/IRichTrSegMaker.h"
+#include "RichRecBase/IRichTrackSelector.h"
+#ifndef WIN32 // These include RichRecSegment.h, which breaks gccxml compilation on Windows
 #include "RichRecBase/IRichMassHypothesisRingCreator.h"
 #include "RichRecBase/IRichPhotonCreator.h"
-#include "RichRecBase/IRichPhotonPredictor.h"
 #include "RichRecBase/IRichPhotonReconstruction.h"
-#include "RichRecBase/IRichPhotonSignal.h"
 #include "RichRecBase/IRichPixelCreator.h"
 #include "RichRecBase/IRichRayTraceCherenkovCone.h"
-#include "RichRecBase/IRichRayleighScatter.h"
 #include "RichRecBase/IRichRecGeomTool.h"
 #include "RichRecBase/IRichRecMCTruthTool.h"
 #include "RichRecBase/IRichSegmentCreator.h"
-#include "RichRecBase/IRichSellmeirFunc.h"
-#include "RichRecBase/IRichSignalDetectionEff.h"
 #include "RichRecBase/IRichStatusCreator.h"
-#include "RichRecBase/IRichTrSegMaker.h"
 #include "RichRecBase/IRichTrackCreator.h"
-#include "RichRecBase/IRichTrackSelector.h"
+#endif // WIN32
 
 #endif // DICT_RICHRECBASEDICT_H
 
