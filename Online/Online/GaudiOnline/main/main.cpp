d3ce207f69323b338e519fe3f649bdd818ca94d5
Markus Frank <frankb@cern.ch>
2007-04-25 17:27:03 +0000

    Preparing ONLINE_v3r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96812 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fdae780f63e681e716517939b539fd7a560b4599

--- Online/Online/GaudiOnline/main/main.cpp
+++ Online/Online/GaudiOnline/main/main.cpp
@@ -1,9 +1,3 @@
-#include "GaudiKernel/Algorithm.h"
-
-static std::string never(Algorithm* p) {
-   return p ? p->name() : "";
-}   
-
 #ifndef LOAD_LIBRARY
 #define THE_LOAD_LIBRARY argv[1]
 #else
@@ -18,51 +12,30 @@ static std::string never(Algorithm* p) {
 
 typedef long (*func)(int, char**);
 
-#ifdef _WIN32
-  #include <windows.h>
-  #define LOAD_LIB(x)  ::LoadLibrary( x )
-  #define GETPROC(h,x) ::GetProcAddress ( HMODULE(h), x )
-  #define DLERROR      __dl_error()
-  const char* __dl_error()  {
-    static char s[1024] = "No error reporting implemented";
-    void* lpMessageBuffer;
-    ::FormatMessage( 
-      FORMAT_MESSAGE_ALLOCATE_BUFFER |  FORMAT_MESSAGE_FROM_SYSTEM,
-      NULL,
-      ::GetLastError(),
-      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), //The user default language
-      (LPTSTR) &lpMessageBuffer,
-      0,
-      NULL );
-    strcpy(s, (const char*)lpMessageBuffer);
-    ::LocalFree( lpMessageBuffer ); 
-    return s;
-  }
-#else
-  #include <dlfcn.h>
-  #include <unistd.h>
-  #define LOAD_LIB(x)  ::dlopen( x , RTLD_NOW)
-  inline void* GETPROC(void* h, const char* x) {  return ::dlsym(h,x); }
-  #define DLERROR      ::dlerror()
-#endif
-#include <string>
-#include <map>
+#include <cstdlib>
 #include <iostream>
+#include "GaudiKernel/System.h"
+#include "GaudiKernel/StatusCode.h"
 
 int main (int argc, char** argv)  {
-  never(0);
-  void* handle = LOAD_LIB( THE_LOAD_LIBRARY );
-  if ( 0 != handle )  {
-    func fun = (func)GETPROC(handle, THE_EXEC_FUNCTION );
-    if ( fun ) {
-      return (*fun)(argc-2, &argv[2]);
+  System::ImageHandle h = 0;
+  std::string env = std::string("__STARTUP__=")+THE_LOAD_LIBRARY;
+  ::putenv((char*)env.c_str());
+  StatusCode sc = System::loadDynamicLib("__STARTUP__",&h);
+  if ( sc.isSuccess() ) {
+    System::EntryPoint e;
+    sc = System::getProcedureByName(h,THE_EXEC_FUNCTION,&e);
+    if ( sc.isSuccess() ) {
+      func fun = (func)e;
+      if ( fun ) {
+        return (*fun)(argc-2, &argv[2]);
+      }
     }
     if ( argc>2 ) std::cout << "Failed to access procedure:" << THE_EXEC_FUNCTION << std::endl;
-    else          std::cout << "Failed to access procedure." << std::endl;
   }
   std::cout << "Failed to load library:";
   if ( argc>1 ) std::cout << THE_LOAD_LIBRARY << std::endl;
   else          std::cout << "<Unknown: No argument given>" << std::endl;
-  std::cout << "Error: " << DLERROR << std::endl;
+  std::cout << "Error: " << System::getLastErrorString() << std::endl;
   return 0;
 }
