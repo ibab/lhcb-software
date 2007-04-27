bac06b59a0cf4a44258af46f62f7699c990b6421
psomogyi <psomogyi@cern.ch>
2007-04-27 12:49:13 +0000

    revert
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96834 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 96180814404c958061fcdb9d3225c02f9d2e5bd2

--- Online/Online/Presenter/cmt/cleanup.bat
+++ Online/Online/Presenter/cmt/cleanup.bat
@@ -1,5 +1,5 @@
 @echo off
-if NOT DEFINED CMTROOT set CMTROOT=Z:\cern.ch\sw\contrib\CMT\v1r19& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+if NOT DEFINED CMTROOT set CMTROOT=Z:\cern.ch\sw\contrib\CMT\v1r20p20070208& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
 
 set cmttempfile="%TEMP%\tmpsetup.bat"
 %CMTROOT%\%CMTBIN%\cmt.exe cleanup -bat  -pack=Presenter -version=v0r1 -path="%~d0%~p0..\..\.."   %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
--- Online/Online/Presenter/cmt/setup.bat
+++ Online/Online/Presenter/cmt/setup.bat
@@ -1,6 +1,6 @@
 rem Setting Presenter v0r1 in %~d0%~p0
 @echo off
-if NOT DEFINED CMTROOT set CMTROOT=Z:\cern.ch\sw\contrib\CMT\v1r19& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+if NOT DEFINED CMTROOT set CMTROOT=Z:\cern.ch\sw\contrib\CMT\v1r20p20070208& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
 
 set cmttempfile="%TEMP%\tmpsetup.bat"
 %CMTROOT%\%CMTBIN%\cmt.exe setup -bat  -pack=Presenter -version=v0r1 -path="%~d0%~p0..\..\.."   -no_cleanup %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
