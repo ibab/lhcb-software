2983f41a2a7b64492b28f6a2c7b275868186962e
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-05-03 13:04:46 +0000

    move DetDesc dictionaries to DetDesc package
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35758 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2f7535b839d2c814786167c0aa95f4ca3bc4c643

--- LHCb/Det/DetSys/dict/DetDict.h
+++ LHCb/Det/DetSys/dict/DetDict.h
@@ -1,77 +1,7 @@
-// $Id: DetDict.h,v 1.21 2007-04-23 16:23:02 cattanem Exp $
+// $Id: DetDict.h,v 1.22 2007-05-03 13:04:46 cattanem Exp $
 // ============================================================================
 #ifndef DETSYS_DETDICT_H 
 #define DETSYS_DETDICT_H 1
-// ============================================================================
-// Hack to get round gccxml parsing problem (SEAL bug 9704)
-// ============================================================================
-#ifdef _WIN32
-#define LONG_LONG_MAX     0x7fffffffffffffffLL  /*maximum signed __int64 value */
-#define LONG_LONG_MIN     0x8000000000000000LL  /*minimum signed __int64 value */
-#define ULONG_LONG_MAX    0xffffffffffffffffLL  /*maximum unsigned __int64 value */
-#endif
-// ============================================================================
-// GaudiKernel
-// ============================================================================
-#include "GaudiKernel/IUpdateManagerSvc.h"
-// ============================================================================
-// DetDesc 
-// ============================================================================
-#include "DetDesc/IDetectorElement.h"
-#include "DetDesc/IGeometryInfo.h"
-#include "DetDesc/ILVolume.h"
-#include "DetDesc/IPVolume.h"
-#include "DetDesc/ISolid.h"
-#include "DetDesc/ITransportSvc.h"
-// ============================================================================
-#include "DetDesc/IAlignment.h"
-#include "DetDesc/ICalibration.h"
-#include "DetDesc/IConditionInfo.h"
-#include "DetDesc/IFastControl.h"
-#include "DetDesc/ISlowControl.h"
-#include "DetDesc/IReadOut.h"
-// ============================================================================
-#include "DetDesc/Material.h"
-#include "DetDesc/Mixture.h"
-#include "DetDesc/Element.h"
-#include "DetDesc/Isotope.h"
-// ============================================================================
-#include "DetDesc/LogVolBase.h"
-#include "DetDesc/LVolume.h"
-#include "DetDesc/LAssembly.h"
-// ============================================================================
-#include "DetDesc/Condition.h"
-#include "DetDesc/AlignmentCondition.h"
-#include "DetDesc/Surface.h"
-#include "DetDesc/DetectorElement.h"
-#include "DetDesc/TabulatedProperty.h"
-#include "DetDesc/ValidDataObject.h"
-#include "DetDesc/ParamValidDataObject.h"
-// ============================================================================
-#include "DetDesc/SolidBase.h"
-#include "DetDesc/SolidBoolean.h"
-#include "DetDesc/SolidBox.h"
-#include "DetDesc/SolidChild.h"
-#include "DetDesc/SolidCons.h"
-#include "DetDesc/SolidIntersection.h"
-#include "DetDesc/SolidPolycone.h"
-#include "DetDesc/SolidPolyHedronHelper.h"
-#include "DetDesc/SolidSphere.h"
-#include "DetDesc/SolidSubtraction.h"
-#include "DetDesc/SolidTrap.h"
-#include "DetDesc/SolidTrd.h"
-#include "DetDesc/SolidTubs.h"
-#include "DetDesc/SolidUnion.h"
-// ============================================================================
-#include "DetDesc/PVolume.h"
-#include "DetDesc/GeometryInfoPlus.h"
-#include "DetDesc/AlignmentInfo.h"
-#include "DetDesc/CalibrationInfo.h"
-#include "DetDesc/ConditionInfo.h"
-#include "DetDesc/FastControlInfo.h"
-#include "DetDesc/ReadOutInfo.h"
-#include "DetDesc/SlowControlInfo.h"
-#include "DetDesc/SimpleValidity.h"
 // ============================================================================
 // CaloDet 
 // ============================================================================
@@ -100,35 +30,6 @@
 #include "MuonDet/MuonODEBoard.h"
 #include "MuonDet/MuonStationCabling.h"
 #include "MuonDet/IMuonPosTool.h"
-// ============================================================================
-
-
-/** @file 
- *   
- *  Helper file to generate LCG dictionaries for detector description classes
- *
- *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
- *  @date   2004-03-01
- *  @date   updated 2005-07-07
- */
-
-namespace DetDescPython
-{
-  //--- Templace instantiations
-  struct __Instantiations 
-  {
-    ILVolume::PVolumes      i1 ;
-    ILVolume::ReplicaPath   i2 ;
-    ILVolume::PVolumePath   i3 ;
-    ILVolume::Interval      i4 ;
-    ILVolume::Intersection  i5 ;
-    ILVolume::Intersections i6 ;
-    ISolid::Ticks           i7 ;
-    
-    SmartRef<Condition> sref_inst1;
-  };
-  
-};
 
 // ============================================================================
 #endif // DETSYS_DETDICT_H
