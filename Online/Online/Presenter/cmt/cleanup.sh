1d915701a213c03a717d278aaf854271d3c3c6b4
psomogyi <psomogyi@cern.ch>
2007-05-08 08:27:02 +0000

    cleanup
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@96878 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2ef970c43340ff22c911514bc77f35dd58ab3ae

--- Online/Online/Presenter/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/cern.ch/sw/contrib/CMT/v1r20p20070208; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=Presenter -version=v0r1 -path=/home/psomogyi/cmtuser/Online_v3r1/Online $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
