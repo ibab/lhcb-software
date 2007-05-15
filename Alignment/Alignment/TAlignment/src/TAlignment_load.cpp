500e4260fc5a64effc602cc5895be36e42458f8a
Johan Blouw <j.blouw@cern.ch>
2007-05-15 14:07:56 +0000

    removed src/TAlignment_load.cpp since it is not needed anymore
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88128 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c75c0a9bfbc36a89c132785a099a323e7c8fb5a5

--- Alignment/Alignment/TAlignment/src/TAlignment_load.cpp
+++
@@ -1,9 +0,0 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-DECLARE_FACTORY_ENTRIES( TAlignment ) {
-  DECLARE_ALGORITHM(AlignAlgorithm);
-  DECLARE_ALGORITHM(AlignOTDetector);
-  DECLARE_ALGORITHM( TStation );
-  DECLARE_ALGORITHM( GAlign );
-}
-
