6db8958c4381392954b4ffdcfbcb6c6d6c45ed21
Gloria Corti <gloria.corti@cern.ch>
2007-05-16 17:14:28 +0000

    first import of package for MIB
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93017 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e5a6215afc778666bfe9581e7689772e9d60996e

---
+++ Gauss/Gen/LbMIB/src/LbMIB_load.cpp
@@ -0,0 +1,20 @@
+// $Id: LbMIB_load.cpp,v 1.1.1.1 2007-05-16 17:14:28 gcorti Exp $
+// Include files 
+
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Declare  OBJECT / CONVERTER / ALGORITHM / TOOL using the macros DECLARE_xxx
+// The statements are like that:
+//
+// DECLARE_ALGORITHM( MyAlgorithm );
+// DECLARE_TOOL( MyTool );
+// DECLARE_OBJECT( DataObject );
+//
+// They should be inside the 'DECLARE_FACTORY_ENTRIES' body.
+
+DECLARE_FACTORY_ENTRIES(LbMIB) {
+
+  DECLARE_ALGORITHM( CollimatorSourceAlg );
+
+}
