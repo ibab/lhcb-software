9ce59f6420f881c3c4c20ed106bbfb3f24ba26e2
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-05-18 09:51:16 +0000

    v14r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@58714 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 85681022c24e4843a87fbc7333c9ae4704159673

--- Boole/BooleSys/doc/MainPage.h
+++ Boole/BooleSys/doc/MainPage.h
@@ -1,20 +1,21 @@
 /** \mainpage notitle
  *  \anchor booledoxygenmain
  *
- * <b>This reference manual contains classes specific to the Boole digization program.<p> 
- * Other classes used by Boole are documented in the following reference manuals:</b>
- * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb packages shared with Reconstruction and Analysis)"
- * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core packages)"
- * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
- * \li \ref externaldocs     "Related external libraries"
+ * This reference manual documents all %LHCb and %Gaudi classes accessible from
+ * the Boole digitization program environment. More information on Boole,
+ * including a list of available releases, can be found in the 
+ * <a href=" http://cern.ch/lhcb-release-area/DOC/boole/">Boole project Web pages</a>
  *
  * <hr>
  * \htmlinclude new_release.notes
- * <b>See also: </b><a href="_digi_content_8opts-source.html"><b>Digi content</b></a>, <a href="requirements-source.html"><b>Constituent packages</b></a><p>
- * <hr>
+ * <b>See also: </b>
+ * \li \ref externaldocs     "Related external libraries"
 
- * <b>Additional information:</b>
- * \li <a href="../release.notes"><b>Release notes history</b></a>
- * \li <a href=" http://cern.ch/lhcb-release-area/DOC/boole/"><b>Boole project Web pages</b></a><p>
+ * <b>Top level options files</b>
+ * \li v200601.opts: DC06 geometry
+ * \li PilotRun.opts: DC06 geometry, field off, no spillover
+ * \li TAE.opts: Time alignment events with DC06 geometry
+ * \li Copy.opts: To make a copy of .digi files
 
+ *
  */
