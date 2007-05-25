421e636b08fe04c47ad81961ae795800b0a3ad57
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-05-25 12:40:10 +0000

    add dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35781 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 545d455cd134e106be528e114142f17b90ecc024

---
+++ LHCb/Det/OTDet/dict/OTDetDict.h
@@ -0,0 +1,15 @@
+// $Id: OTDetDict.h,v 1.1 2007-05-25 12:40:10 cattanem Exp $
+// ============================================================================
+#ifndef DICT_OTDETDICT_H 
+#define DICT_OTDETDICT_H 1
+
+#include "OTDet/DeOTDetector.h"
+#include "OTDet/DeOTModule.h"
+#include "OTDet/DeOTLayer.h"
+#include "OTDet/DeOTQuarter.h"
+#include "OTDet/DeOTStation.h"
+
+// ============================================================================
+#endif // DICT_OTDETDICT_H
+// ============================================================================
+
