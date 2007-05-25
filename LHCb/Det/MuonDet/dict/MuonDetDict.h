563d496d51a1e2eb7f61f4dc9ba0287096466940
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-05-25 14:20:54 +0000

    add dict generation
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35783 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0efd7b1739ad970933d987a2ab5f30255da7cb72

---
+++ LHCb/Det/MuonDet/dict/MuonDetDict.h
@@ -0,0 +1,21 @@
+// $Id: MuonDetDict.h,v 1.1 2007-05-25 14:20:54 cattanem Exp $
+// ============================================================================
+#ifndef DICT_MUONDETDICT_H 
+#define DICT_MUONDETDICT_H 1
+
+#include "MuonDet/DeMuonDetector.h"
+#include "MuonDet/DeMuonChamber.h"
+#include "MuonDet/DeMuonRegion.h" 
+#include "MuonDet/DeMuonGasGap.h"   
+#include "MuonDet/MuonBasicGeometry.h"
+#include "MuonDet/MuonReadoutCond.h"
+#include "MuonDet/MuonTSMap.h"
+#include "MuonDet/MuonL1Board.h"
+#include "MuonDet/MuonODEBoard.h"
+#include "MuonDet/MuonStationCabling.h"
+#include "MuonDet/IMuonPosTool.h"
+
+// ============================================================================
+#endif // DICT_MUONDETDICT_H
+// ============================================================================
+
