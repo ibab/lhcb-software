73e6fef6f4cd715192ec2d2cd99a30f4bdd4329f
Olivier Deschamps <odescham@cern.ch>
2007-05-30 14:45:59 +0000

    new interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39796 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 972e50e4e378537d0954266f68ec82e1e823a0c8

---
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/ICalo2Calo.h
@@ -0,0 +1,49 @@
+// $Id: ICalo2Calo.h,v 1.1 2007-05-30 14:45:59 odescham Exp $
+#ifndef ICALO2CALO_H 
+#define ICALO2CALO_H 1
+
+// Include files
+// from STL
+#include <string>
+
+#include "Kernel/CaloCellID.h"
+#include "Event/CaloDigit.h"
+#include "Event/CaloCluster.h"
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+// forward declaration
+
+static const InterfaceID IID_ICalo2Calo ( "ICalo2Calo", 1, 0 );
+
+/** @class ICalo2Calo ICalo2Calo.h
+ *  
+ *
+ *  @author Olivier Deschamps
+ *  @date   2007-05-29
+ */
+
+
+class ICalo2Calo : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_ICalo2Calo; }
+
+  virtual StatusCode initialize()=0;
+  virtual   std::vector<LHCb::CaloCellID> cellIDs(LHCb::CaloCluster fromCluster, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloCellID> cellIDs(LHCb::CaloCellID  fromId     , std::string toCalo, bool init=true)=0;
+  virtual std::vector<LHCb::CaloCellID> cellIDs()=0;
+  virtual std::vector<LHCb::CaloDigit> digits(LHCb::CaloCellID   fromId     , std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloDigit> digits(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloDigit> digits()=0;
+  virtual double energy(LHCb::CaloCellID   fromId     , std::string toCalo)=0;
+  virtual double energy(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual double energy()=0;
+  virtual bool isLocalMax(LHCb::CaloDigit digit)=0;
+
+protected:
+
+private:
+
+};
+#endif // ICALO2CALO_H
