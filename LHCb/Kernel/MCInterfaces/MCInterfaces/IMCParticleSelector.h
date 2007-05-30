19938c12f9be395f90b47432734799b0c53ae7d5
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-05-30 10:02:53 +0000

    add bool operator
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47643 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2c1e23d34c8f88716e656d4c7f1d87c39f8abfd2

--- LHCb/Kernel/MCInterfaces/MCInterfaces/IMCParticleSelector.h
+++ LHCb/Kernel/MCInterfaces/MCInterfaces/IMCParticleSelector.h
@@ -15,20 +15,35 @@ namespace LHCb {
  *  @date   13/3/2002
  */
 
-// Declaration of the interface ID ( interface id, major version, minor version) 
-static const InterfaceID IID_IMCParticleSelector("IMCParticleSelector", 0 , 0); 
+// Declaration of the interface ID ( interface id, major version, minor version)
+static const InterfaceID IID_IMCParticleSelector("IMCParticleSelector", 0 , 0);
 
 class IMCParticleSelector : virtual public IAlgTool {
 
-public: 
+public:
 
   /// Static access to interface id
   static const InterfaceID& interfaceID() { return IID_IMCParticleSelector; }
-   
- /// select
- virtual bool accept(const LHCb::MCParticle* aParticle) const=0;
+
+  /** Method to select or reject a given MCParticle
+   *  @param aParticle Pointer to the MCParticle to consider
+   *  @return Boolean indicating if the given MCParticle is selected or not
+   *  @retval true MCParticle is selected
+   *  @retval true MCParticle is rejected
+   */
+  virtual bool accept( const LHCb::MCParticle* aParticle ) const = 0;
+
+  /** Boolean operator to select or reject a given MCParticle
+   *  @param aParticle Pointer to the MCParticle to consider
+   *  @return Boolean indicating if the given MCParticle is selected or not
+   *  @retval true MCParticle is selected
+   *  @retval true MCParticle is rejected
+   */
+  inline bool operator() ( const LHCb::MCParticle* aParticle ) const
+  {
+    return this->accept(aParticle);
+  }
 
 };
 
 #endif // MCINTERFACES_IMCPARTICLESELECTOR_H
-
