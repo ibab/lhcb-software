c8979791ae933935d2437122b05d2549dbfa9cb2
jpalac <jpalac@cern.ch>
2007-06-01 15:29:32 +0000

    . Add functions to make Gaudi::Transform3D out of 3 parameters representing either a       cartesian translation or three rotation angles about the cartesian axes.     . Add a function to construct a local to global Gaudi::Transform3D out of nine parameters       describing a pivot, a translation and a rotation about three cartesian axes.     . All these functions are independent of DetDesc and should be put elsewhere, but for the time       being they rely on src/Lib/3DConversions.h, which will disappear when a version of       MathCore with a fully integrated ROOT::Math::RotationXYZ is released.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@4318 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ab616779ce0cd1d2529d08b8c7c98a80ec5d9902

--- LHCb/Det/DetDesc/DetDesc/GlobalToLocalDelta.h
+++ LHCb/Det/DetDesc/DetDesc/GlobalToLocalDelta.h
@@ -1,4 +1,4 @@
-// $Id: GlobalToLocalDelta.h,v 1.1 2007-05-31 14:46:55 jpalac Exp $
+// $Id: GlobalToLocalDelta.h,v 1.2 2007-06-01 15:29:32 jpalac Exp $
 #ifndef DETDESC_GLOBALTOLOCALDELTA_H 
 #define DETDESC_GLOBALTOLOCALDELTA_H 1
 
@@ -50,5 +50,47 @@ namespace DetDesc {
   const Gaudi::Transform3D localDeltaMatrix(const IGeometryInfo* GI,
                                             const Gaudi::Transform3D& globalDelta);
 
+  /**
+   * Build the local to global transformation matrix given 
+   *
+   * @param pivotParams vector containing the X,Y,Z coordinates of a pivot point for the
+   *        rotation part of the transformation.
+   * @param translationParams vector containing the X,Y,Z coordinates of the translation
+   *        part of the transformation
+   * @param rotationParams vector containing the rotation angles about the X,Y,Z axes. 
+   *        The rotation is applied in the following order: Z-Y'-X"
+   * @return The full 3D transformation object. 
+   * @author Juan Palacios juan.palacios@cern.ch
+   * @todo Move this function outside of DetDesc once the ROOT::Math::RotationZYX class
+   *       is fully available in MathCore.
+   */
+  const Gaudi::Transform3D localToGlobalTransformation(std::vector<double>& pivotParams,
+                                                       std::vector<double>& translationParams,
+                                                       std::vector<double>& rotationParams);
+  /**
+   * Make a 3D transformation object from a simple 3D translation
+   * 
+   * @author Juan Palacios juan.palacios@cern.ch
+   *
+   * @param params vector containing the X,Y,Z coordinates of the translation.
+   * @return 3D transformation object representing the translation.
+   * @todo Move this function outside of DetDesc once the ROOT::Math::RotationZYX class              
+   *       is fully available in MathCore. 
+   */
+  const Gaudi::Transform3D XYZTranslation(const std::vector<double>& params);
+  /**
+   * Build a 3D rotation from three angles.
+   *
+   * @author Juan Palacios juan.palacios@cern.ch
+   *
+   * @param params vector containing the rotations about the X,Y,Z axes. The rotations are applied
+   *        in the following order: Z-Y'-X"
+   * @return 3D transformation object describing the rotation
+   * @todo Move this function outside of DetDesc once the ROOT::Math::RotationZYX class              
+   *       is fully available in MathCore.  
+   *
+   */
+  const Gaudi::Transform3D ZYXRotation(const std::vector<double>& params);
+  
 }
 #endif // DETDESC_GLOBALTOLOCALDELTA_H
--- LHCb/Det/DetDesc/src/Lib/GlobalToLocalDelta.cpp
+++ LHCb/Det/DetDesc/src/Lib/GlobalToLocalDelta.cpp
@@ -1,9 +1,11 @@
-// $Id: GlobalToLocalDelta.cpp,v 1.2 2007-05-31 16:53:30 jpalac Exp $
+// $Id: GlobalToLocalDelta.cpp,v 1.3 2007-06-01 15:29:32 jpalac Exp $
 // Include files 
 #include "DetDesc/IDetectorElement.h"
 #include "DetDesc/IGeometryInfo.h"
 // local
 #include "DetDesc/GlobalToLocalDelta.h"
+#include "RotationZYX.h"
+#include "3DConversions.h"
 namespace DetDesc {
 
 const Gaudi::Transform3D localDeltaMatrix(const IDetectorElement* DE,
@@ -13,15 +15,45 @@ const Gaudi::Transform3D localDeltaMatrix(const IDetectorElement* DE,
 
 const Gaudi::Transform3D localDeltaMatrix(const IGeometryInfo* GI,
                                           const Gaudi::Transform3D& toMisaligned_global) {
-    
+
   const Gaudi::Transform3D toLocal_total       = GI->matrix();
   const Gaudi::Transform3D toGlobal_ideal       = GI->idealMatrixInv();
   const Gaudi::Transform3D toMisalignedOld_local = GI->localDeltaMatrix().Inverse();
   return toMisalignedOld_local*toLocal_total*toGlobal_ideal*toMisaligned_global;
   
 }
-  
+// @todo move these to a general Transform3D tools suite.  
+const Gaudi::Transform3D localToGlobalTransformation(std::vector<double>& pivotParams,
+                                                     std::vector<double>& translationParams,
+                                                     std::vector<double>& rotationParams) 
+{
+  const Gaudi::Transform3D pivot    = DetDesc::XYZTranslation(pivotParams);
+  const Gaudi::Transform3D rotation = DetDesc::ZYXRotation(rotationParams);
+  const Gaudi::Transform3D translation = DetDesc::XYZTranslation(translationParams);
 
+  return translation*pivot.Inverse()*rotation*pivot;
+
+}
+
+const Gaudi::Transform3D XYZTranslation(const std::vector<double>& params) 
+{
+  return (params.size()==3) ? Gaudi::Transform3D( Gaudi::TranslationXYZ(params[0], 
+                                                                        params[1], 
+                                                                        params[2]) )
+    : Gaudi::Transform3D();
+  
+}
+
+const Gaudi::Transform3D ZYXRotation(const std::vector<double>& params)
+{
+  if (params.size() != 3) return Gaudi::Transform3D();
+  
+  Gaudi::RotationZYX rotZYX(params[2], params[1], params[0]);
+  Gaudi::Rotation3D tmp;
+  Gaudi::Math::convert(rotZYX, tmp);
+
+  return Gaudi::Transform3D(tmp);
+}
   
 } // namespace DetDesc
 //=============================================================================
