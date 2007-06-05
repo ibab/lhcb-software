e65e793b51f4182386a6539e40566f42fb33d363
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-06-05 13:33:23 +0000

    standardize dict selection files
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35793 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9a9baab27941775f99ae26638f8fa6bf86548332

---
+++ LHCb/Event/PackedEvent/dict/PackedEventDict.h
@@ -0,0 +1,9 @@
+// $Id: PackedEventDict.h,v 1.1 2007-06-05 13:33:23 cattanem Exp $
+#ifndef DICT_PACKEDEVENTDICT_H 
+#define DICT_PACKEDEVENTDICT_H 1
+
+// Include files for LCG dictionary
+#include "Event/PackedMCParticle.h"
+#include "Event/PackedMCVertex.h"
+
+#endif // DICT_PACKEDEVENTDICT_H
