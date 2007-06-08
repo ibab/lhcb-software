c875750580aeadf5feb3cf768c5a876c40eaa734
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-06-08 14:12:51 +0000

    v7r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39801 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 55b449a0e9b9a3ef1403f887117a43ce2a01b447

--- LHCb/Calo/CaloInterfaces/dict/CaloInterfacesDict.h
+++ LHCb/Calo/CaloInterfaces/dict/CaloInterfacesDict.h
@@ -1,9 +1,8 @@
-// $Id: CaloInterfacesDict.h,v 1.1 2007-03-29 16:57:31 odescham Exp $
+// $Id: CaloInterfacesDict.h,v 1.2 2007-06-08 14:08:15 cattanem Exp $
 #ifndef DICT_CALOINTERFACESDICT_H 
 #define DICT_CALOINTERFACESDICT_H 1
 
-
-
+#include "CaloInterfaces/ICalo2Calo.h"     
 #include "CaloInterfaces/ICaloClusterSelector.h"     
 #include "CaloInterfaces/ICaloClusterTool.h"         
 #include "CaloInterfaces/ICaloCorrection.h"          
@@ -15,7 +14,5 @@
 #include "CaloInterfaces/ICaloSubClusterTag.h"       
 #include "CaloInterfaces/ICaloTrackIdEval.h"         
 #include "CaloInterfaces/ICaloTrackMatch.h"          
-
-
 
 #endif //DICT_CALOINTERFACESDICT_H 
