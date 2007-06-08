7aef9d06dce96b1c0b3f4c79c4ee95a05f990e8d
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-06-08 14:24:58 +0000

    updates for GaudiObjDesc v10r0
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47655 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 490b5c97f6770269dc18ac1675211037246bc5d0

--- LHCb/Kernel/LHCbKernel/Kernel/IUsedLHCbID.h
+++ LHCb/Kernel/LHCbKernel/Kernel/IUsedLHCbID.h
@@ -1,23 +1,20 @@
-// $Id: IUsedLHCbID.h,v 1.2 2006-12-06 16:28:53 cattanem Exp $ 
+// $Id: IUsedLHCbID.h,v 1.3 2007-06-08 14:24:58 cattanem Exp $ 
 #ifndef _IUsedLHCbID_H
 #define _IUsedLHCbID_H
  
 #include "GaudiKernel/IAlgTool.h"
+#include "Kernel/LHCbID.h"
  
 static const InterfaceID IID_IUsedLHCbID( "IUsedLHCbID", 0, 0 );
  
-/** @class IUsedLHCbID
+/** @class IUsedLHCbID IUsedLHCbID.h Kernel/IUsedLHCbID.h
  *
- *  interface for testing ida given id is used. 
+ *  interface for testing if a given id is used. 
  *
  *  @author M.Needham
  *  @date   02/08/2006
  */
 
-namespace LHCb{
- class LHCbID;
-};
-  
 class IUsedLHCbID: virtual public IAlgTool{
  
 public:
