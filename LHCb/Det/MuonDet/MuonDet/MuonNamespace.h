2d207bcf033c1dac93f47dd78a7da9984a76b74d
Alessia Satta <asatta@cern.ch>
2007-06-11 08:37:06 +0000

    add the default location for muon det
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@35816 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 16c0b95b09ad4331c63bb0f20b1f326244732afb

---
+++ LHCb/Det/MuonDet/MuonDet/MuonNamespace.h
@@ -0,0 +1,7 @@
+// $Id: MuonNamespace.h,v 1.1 2007-06-11 08:37:06 asatta Exp $
+#ifndef MUONDET_MUONNAMESPACE_H 
+#define MUONDET_MUONNAMESPACE_H 1
+namespace DeMuonLocation{
+  static const std::string& Default="/dd/Structure/LHCb/DownstreamRegion/Muon";
+}
+#endif // MUONDET_MUONNAMESPACE_H
