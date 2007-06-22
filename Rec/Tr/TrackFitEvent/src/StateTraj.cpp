18d7d757b5653767ec7ee9966e345407c36bd944
ebos <ebos@cern.ch>
2007-06-22 13:51:25 +0000

    Replaced TrackParameters::kappa with c_light.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 313de580abfc419270d303a41220f4b9008bd9c9

--- Rec/Tr/TrackFitEvent/src/StateTraj.cpp
+++ Rec/Tr/TrackFitEvent/src/StateTraj.cpp
@@ -1,4 +1,4 @@
-// $Id: StateTraj.cpp,v 1.12 2007-04-17 06:35:06 graven Exp $
+// $Id: StateTraj.cpp,v 1.13 2007-06-22 13:51:25 ebos Exp $
 // Include files
 
 // Units
@@ -7,128 +7,124 @@
 // local
 #include "Event/StateTraj.h"
 
+// Namespaces
 using namespace Gaudi;
 using namespace LHCb;
 
-StateTraj::StateTraj( const LHCb::State& state,
-                      const Gaudi::XYZVector& bField )
-  : DifTraj<kSize>(10*Gaudi::Units::km,10*Gaudi::Units::km),
-    m_pos(state.position()),
+// Constructor
+StateTraj::StateTraj( const State& state,
+                      const XYZVector& bField )
+  : DifTraj<kSize>( 10.*Units::km, 10.*Units::km ),
+    m_pos( state.position() ),
     // True when approximating the trajectory as a straight line
-    m_dir(state.slopes().unit()),
-    m_qOverP(state.qOverP()),
+    m_dir( state.slopes().unit() ),
+    m_qOverP( state.qOverP() ),
     m_bField(bField),
-    m_curv(TrackParameters::kappa*m_qOverP*(m_dir.Cross(bField)))
-{
-};
+    m_curv( Units::c_light*m_qOverP*( m_dir.Cross(bField) ) ) {}
 
-StateTraj::StateTraj( const Gaudi::TrackVector& stateVector,
+// Constructor
+StateTraj::StateTraj( const TrackVector& stateVector,
                       double z,
-                      const Gaudi::XYZVector& bField )
-  : DifTraj<kSize>(10*Gaudi::Units::km,10*Gaudi::Units::km),
-    m_pos(stateVector(0),stateVector(1),z),
-    m_qOverP(stateVector(4)),
+                      const XYZVector& bField )
+  : DifTraj<kSize>( 10.*Units::km, 10.*Units::km ),
+    m_pos( stateVector(0), stateVector(1), z ),
+    m_qOverP( stateVector(4) ),
     m_bField(bField)
 {
   XYZVector slopes = XYZVector( stateVector(2), stateVector(3), 1. );
   // True when approximating the trajectory as a straight line
   m_dir  = slopes.unit();
-  m_curv = TrackParameters::kappa * m_qOverP * ( m_dir.Cross(m_bField) );   
-};
+  m_curv = Units::c_light*m_qOverP*( m_dir.Cross(m_bField) );   
+}
 
+// StateTraj clone method
 std::auto_ptr<Trajectory> StateTraj::clone() const
 {
-        return std::auto_ptr<Trajectory>(new StateTraj(*this));
+  return std::auto_ptr<Trajectory>( new StateTraj(*this) );
 }
 
 XYZPoint StateTraj::position( double arclength ) const
 {
-  return m_pos + arclength * ( m_dir + 0.5 * arclength * curvature(arclength) );
-};
+  return m_pos + arclength*( m_dir + 0.5 * arclength*curvature(arclength) );
+}
     
 XYZVector StateTraj::direction( double arclength ) const
 {
-  return (m_dir + arclength * curvature(arclength)).unit();
-};
+  return ( m_dir + arclength*curvature(arclength) ).unit();
+}
 
-XYZVector StateTraj::curvature(double /*arclength*/) const 
+XYZVector StateTraj::curvature( double /*arclength*/ ) const 
 {
   return m_curv;
-};
+}
 
 void StateTraj::expansion( double arclength,
-                           Gaudi::XYZPoint& p,
-                           Gaudi::XYZVector& dp,
-                           Gaudi::XYZVector& ddp ) const 
+                           XYZPoint& p,
+                           XYZVector& dp,
+                           XYZVector& ddp ) const 
 {
   p   = position(arclength);
   dp  = direction(arclength);
   ddp = curvature(arclength);
-};
-
-StateTraj::Parameters
-StateTraj::parameters( ) const
-{
-  return Parameters(m_pos.X(),m_pos.Y(),
-                    m_dir.X()/m_dir.Z(),m_dir.Y()/m_dir.Z(),
-                    m_qOverP );
 }
 
-StateTraj&
-StateTraj::operator+=(const Parameters& delta) 
+StateTraj::Parameters StateTraj::parameters() const
 {
- m_pos += XYZVector(delta(0),delta(1),0);
- m_dir += m_dir.Z()*XYZVector(delta(2),delta(3),0);
- m_dir = m_dir.unit(); // renormalize slopes to direction
- m_qOverP += delta(4);
- return *this;
+  return Parameters( m_pos.X(), m_pos.Y(),
+		     m_dir.X()/m_dir.Z(), m_dir.Y()/m_dir.Z(),
+		     m_qOverP );
 }
 
-StateTraj::Derivative
-StateTraj::derivative( double arclength ) const
+StateTraj& StateTraj::operator+=( const Parameters& delta ) 
+{
+  m_pos += XYZVector( delta(0), delta(1), 0. );
+  m_dir += m_dir.Z()*XYZVector( delta(2), delta(3), 0. );
+  m_dir = m_dir.unit(); // renormalize slopes to direction
+  m_qOverP += delta(4);
+  return *this;
+}
+
+StateTraj::Derivative StateTraj::derivative( double arclength ) const
 {
   Derivative deriv;
   double vx  = m_dir.x();
   double vy  = m_dir.y();
   double vz  = m_dir.z();
-  double svz = arclength * vz;
+  double svz = arclength*vz;
   double arcvxyz  = arclength * vx * vy * vz;
-  double arcvx = 1.0 - vx * vx;
-  double arcvy = 1.0 - vy * vy;
-  double C     = 0.5 * arclength * arclength * TrackParameters::kappa;
+  double arcvx = 1. - vx * vx;
+  double arcvy = 1. - vy * vy;
+  double C     = 0.5 * arclength*arclength*Units::c_light;
   double cvz   = C * m_qOverP * vz;
   XYZVector crossB = m_dir.Cross(m_bField);
 
-  deriv(0,0) = 1.0;
+  deriv(0,0) = 1.;
   deriv(0,2) = svz * arcvx - cvz * vx * crossB.x();
-  deriv(0,3) = - arcvxyz
-               + cvz * ( vy * vz * m_bField.y() + arcvy * m_bField.z() );
+  deriv(0,3) = -arcvxyz + cvz * ( vy * vz * m_bField.y() + arcvy*m_bField.z() );
   deriv(0,4) = C * crossB.x();
-  deriv(1,1) = 1.0;
-  deriv(1,2) = - arcvxyz
-               - cvz * ( vx * vz * m_bField.x() + arcvx * m_bField.z() );
+  deriv(1,1) = 1.;
+  deriv(1,2) = -arcvxyz - cvz * ( vx * vz * m_bField.x() + arcvx*m_bField.z() );
   deriv(1,3) = svz * arcvy - cvz * vy * crossB.y();
   deriv(1,4) = C * crossB.y();
-  deriv(2,2) = - vx * vz * svz
-               + cvz * ( vx * vy * m_bField.x() + arcvx * m_bField.y() );
-  deriv(2,3) = - vy * vz * svz
-               - cvz * ( vx * vy * m_bField.y() + arcvy * m_bField.x() );
+  deriv(2,2) = -vx * vz * svz
+    + cvz * ( vx * vy * m_bField.x() + arcvx*m_bField.y() );
+  deriv(2,3) = -vy * vz * svz
+    - cvz * ( vx * vy * m_bField.y() + arcvy*m_bField.x() );
   deriv(2,4) = C * crossB.z();
 
   return deriv;        
-};
+}
 
-double StateTraj::arclength( const Gaudi::XYZPoint& point) const
+double StateTraj::arclength( const XYZPoint& point ) const
 {
   // for now assume we're a straight line, i.e. |m_curve|<<|m_dir|
   // and return our zeroth order approximation..
-  double s = m_dir.Dot(point-m_pos);
-  return s;
+  return m_dir.Dot( point - m_pos );
 
   // need to check if the code below is safe for m_curv=0!!!!
   // get vector from m_pos to point projected into plane of parabola
-  //Gaudi::XYZVector normal = m_dir.Cross(m_curv).unit();
-  //Gaudi::XYZPoint r( ( point - normal.Dot(point-m_pos)*normal )-m_pos);
+  //XYZVector normal = m_dir.Cross(m_curv).unit();
+  //XYZPoint r( ( point - normal.Dot(point-m_pos)*normal )-m_pos);
   //// get normalized 'x' and 'y' coordinates of this vector by projecting onto the
   //// axes. In terms of these, the trajectory is parameterized as (arclen, arclen^2/2)
   //// (i.e. arclen is actually the distance along the 'x' coordinate!)
@@ -146,11 +142,11 @@ double StateTraj::arclength( const Gaudi::XYZPoint& point) const
 // Not yet implemented
 double StateTraj::distTo1stError( double , double , int ) const 
 {
-  return 10*Gaudi::Units::km;  
-};
+  return 10.*Units::km;  
+}
 
 // Not yet implemented
 double StateTraj::distTo2ndError( double , double , int ) const
 {
-  return 10*Gaudi::Units::km;  
-};
+  return 10.*Units::km;  
+}
