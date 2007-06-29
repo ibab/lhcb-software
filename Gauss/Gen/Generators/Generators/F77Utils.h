0f5bbb8e55963ed7a4aa3eeba923ef4c31113d2e
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-06-29 13:21:06 +0000

    add F77Utils:getUnit function
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93039 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4e26309e814c241bd66f74f26afe8c7c0bfd31d3

--- Gauss/Gen/Generators/Generators/F77Utils.h
+++ Gauss/Gen/Generators/Generators/F77Utils.h
@@ -1,8 +1,4 @@
-// $Id: F77Utils.h,v 1.1 2006-10-04 13:36:46 ibelyaev Exp $
-// ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $
-// ============================================================================
-// $Log: not supported by cvs2svn $
+// $Id: F77Utils.h,v 1.2 2007-06-29 13:21:06 ibelyaev Exp $
 // ============================================================================
 #ifndef GENERATORS_F77UTILS_H 
 #define GENERATORS_F77UTILS_H 1
@@ -18,105 +14,137 @@
 #include "GaudiKernel/StatusCode.h"
 // ============================================================================
 /** @namespace  F77Utils F77Utils.h Generators/F77Utils.h
- *
  *  Collection of primitive utilities to open/close FORTRAN units 
- *
  *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
  *  @date   2006-10-03
  */
 namespace F77Utils 
 {
   // ==========================================================================
-  /** @fn close 
-   *  close the opened F77 file 
+  /** Close the opened F77 file 
    *  @param lun logical file number 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode close       ( int lun ) ;
+  StatusCode close       
+  ( int lun , const bool verbose = false ) ;
   // ==========================================================================
-  /** @fn openNew 
-   *  Open Fortran file with status="NEW" 
+  /** Open Fortran file with status="NEW" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openNew     ( int lun , const std::string& file ) ;
+  StatusCode openNew    
+  ( int   lun                          , 
+    const std::string& file            , 
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn openOld
-   *  Open Fortran file with status="OLD" 
+  /** Open Fortran file with status="OLD" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openOld     ( int lun , const std::string& file ) ;
+  StatusCode openOld     
+  ( int   lun                          , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn openUnknown
-   *  Open Fortran file with status="UNKNOWN" 
+  /** Open Fortran file with status="UNKNOWN" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openUnknown ( int lun , const std::string& file ) ;
+  StatusCode openUnknown 
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn open
-   *  Open Fortran file with status="UNKNOWN" 
+  /** Open Fortran file with status="UNKNOWN" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode open ( int lun , const std::string& file )  ;
+  StatusCode open 
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
   /** @fn openNewFormatted 
    *  Open "FORMATTED" Fortran file with status="NEW" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param verbose verbosity flag
+   *  @param file file name 
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openNewFormatted ( int lun , const std::string& file ) ;
+  StatusCode openNewFormatted
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn openOldFormatted 
-   *  Open "FORMATTED" Fortran file with status="OLD" 
+  /** Open "FORMATTED" Fortran file with status="OLD" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openOldFormatted ( int lun , const std::string& file ) ;
+  StatusCode openOldFormatted 
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn openUnknownFormatted
-   *  Open "FORMATTED" Fortran file with status="UNKNOWN" 
+  /** Open "FORMATTED" Fortran file with status="UNKNOWN" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openUnknownFormatted ( int lun , const std::string& file ) ;
+  StatusCode openUnknownFormatted 
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-  /** @fn openFormatted
-   *  Open "FORMATTED" Fortran file with status="UNKNOWN" 
+  /** Open "FORMATTED" Fortran file with status="UNKNOWN" 
    *  @param lun logical file number 
-   *  @param file fiel name 
+   *  @param file file name 
+   *  @param verbose verbosity flag
    *  @return status code 
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-10-03
    */
-  StatusCode openFormatted ( int lun , const std::string& file )  ;
+  StatusCode openFormatted
+  ( int                lun             , 
+    const std::string& file            ,
+    const bool         verbose = false ) ;
   // ==========================================================================
-
+  /** find free fortran units
+   *  Program scans fortran units from 99 to 10 to find "free" units, 
+   *  @param verbose verbosity flag
+   *  @return unit 
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date   2006-10-03
+   */
+  int getUnit( const bool verbose = false ) ;
+  // ==========================================================================
 } // end of namespace F77Utils 
 // ============================================================================
 // The END 
--- Gauss/Gen/Generators/src/Lib/F77Utils.cpp
+++ Gauss/Gen/Generators/src/Lib/F77Utils.cpp
@@ -1,14 +1,11 @@
-// $Id: F77Utils.cpp,v 1.1 2006-10-04 13:36:46 ibelyaev Exp $
-// ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $
-// ============================================================================
-// $Log: not supported by cvs2svn $ 
+// $Id: F77Utils.cpp,v 1.2 2007-06-29 13:21:06 ibelyaev Exp $
 // ============================================================================
 // Include files
 // ============================================================================
 // STD & STL 
 // ============================================================================
 #include <ctype.h>
+#include <iostream>
 // ============================================================================
 // Generators 
 // ============================================================================
@@ -20,30 +17,22 @@
  *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
  */
 // ============================================================================
-/** @fn open
- *  Open Fortran file with status="UNKNOWN" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
-StatusCode F77Utils::open ( int lun , const std::string& file )  
-{ return openUnknown ( lun , file ) ; }
+//  Open Fortran file with status="UNKNOWN" 
 // ============================================================================
-/** @fn openFormatted
- *  Open "FORMATTED" Fortran file with status="UNKNOWN" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
-StatusCode F77Utils::openFormatted ( int lun , const std::string& file )  
-{ return openUnknownFormatted ( lun , file ) ; }
+StatusCode F77Utils::open 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
+{ return openUnknown ( lun , file , verbose ) ; }
 // ============================================================================
-
-
+// Open "FORMATTED" Fortran file with status="UNKNOWN" 
+// ============================================================================
+StatusCode F77Utils::openFormatted 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
+{ return openUnknownFormatted ( lun , file , verbose ) ; }
+// ============================================================================
 extern "C"
 {
 #ifdef WIN32 
@@ -54,21 +43,24 @@ extern "C"
   int __stdcall F77ONFFILE  (int* LUN ,const char* FILE, int LEN );
   int __stdcall F77OOFFILE  (int* LUN ,const char* FILE, int LEN );
   int __stdcall F77OUFFILE  (int* LUN ,const char* FILE, int LEN );
+  int __stdcall F77GETUNIT  (int* LUN ) ;
 #define f77clfile  F77CLFILE  
 #define f77onfile  F77ONFILE  
 #define f77oofile  F77OOFILE  
 #define f77oufile  F77OUFILE  
 #define f77onffile F77ONFFILE  
 #define f77ooffile F77OOFFILE  
-#define f77ouffile F77OUFFILE  
+#define f77ouffile F77OUFFILE
+#define f77getunit F77GETUNIT
 #else 
-  int           f77clfile_  (int* LUN );
+  int           f77clfile_  (int* LUN ) ;
   int           f77onfile_  (int* LUN , const char* FILE, int LEN ) ;
   int           f77oofile_  (int* LUN , const char* FILE, int LEN ) ;
   int           f77oufile_  (int* LUN , const char* FILE, int LEN ) ;
   int           f77onffile_ (int* LUN , const char* FILE, int LEN ) ;
   int           f77ooffile_ (int* LUN , const char* FILE, int LEN ) ;
   int           f77ouffile_ (int* LUN , const char* FILE, int LEN ) ;
+  int           f77getunit_ (int* LUN ) ;
 #define f77clfile  f77clfile_
 #define f77onfile  f77onfile_
 #define f77oofile  f77oofile_
@@ -76,120 +68,172 @@ extern "C"
 #define f77onffile f77onffile_
 #define f77ooffile f77ooffile_
 #define f77ouffile f77ouffile_
+#define f77getunit f77getunit_
 #endif 
 } ;
-
 // ==========================================================================
-/** @fn close 
- *  close the opened F77 file 
- *  @param lun logical file number 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// close the opened F77 file 
 // ==========================================================================
-StatusCode F77Utils::close ( int lun ) 
+StatusCode F77Utils::close ( int lun , const bool verbose ) 
 { 
-  return lun != f77clfile( &lun ) ?  StatusCode::FAILURE : StatusCode::SUCCESS ;
-} ;
+  const int result = f77clfile( &lun )  ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::close()               ERROR Error in closing LUN=" 
+              << lun << std::endl ; 
+  }
+  else if ( verbose ) 
+  { 
+    std::cout << "F77Utils::close()               INFO  LUN=" 
+              << lun << " is closed " << std::endl ; 
+  }
+  return result != lun ?  StatusCode::FAILURE : StatusCode::SUCCESS ;
+} 
 // ==========================================================================
-/** @fn openNew 
- *  Open Fortran file with status="NEW" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open Fortran file with status="NEW" 
 // ==========================================================================
-StatusCode F77Utils::openNew ( int lun , const std::string& file )  
+StatusCode F77Utils::openNew 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77onfile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-} ;
+  const int result = f77onfile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openNew               ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose ) 
+  {
+    std::cout << "F77Utils::openNew               INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+} 
 // ==========================================================================
-/** @fn openOld
- *  Open Fortran file with status="OLD" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open Fortran file with status="OLD" 
 // ==========================================================================
-StatusCode F77Utils::openOld     ( int lun , const std::string& file ) 
+StatusCode F77Utils::openOld 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77oofile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-} ;
+  const int result = f77oofile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openOld               ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose ) 
+  {
+    std::cout << "F77Utils::openOld               INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }  
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+} 
 // ==========================================================================
-/** @fn openUnknown
- *  Open Fortran file with status="UNKNOWN" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open Fortran file with status="UNKNOWN" 
 // ==========================================================================
-StatusCode F77Utils::openUnknown ( int lun , const std::string& file ) 
+StatusCode F77Utils::openUnknown 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77oufile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-} ;
+  const int result = f77oufile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openUnknown           ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose ) 
+  {
+    std::cout << "F77Utils::openUnknown           INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }    
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+} 
 // ==========================================================================
-/** @fn openNewFormatted 
- *  Open "FORMATTED" Fortran file with status="NEW" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open "FORMATTED" Fortran file with status="NEW" 
 // ==========================================================================
-StatusCode F77Utils::openNewFormatted ( int lun , const std::string& file ) 
+StatusCode F77Utils::openNewFormatted 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77onffile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-};
+  const int result = f77onffile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openNewFormatted      ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose ) 
+  {
+    std::cout << "F77Utils::openNewFormatted      INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }    
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+}
 // ==========================================================================
-/** @fn openOldFormatted 
- *  Open "FORMATTED" Fortran file with status="OLD" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open "FORMATTED" Fortran file with status="OLD" 
 // ==========================================================================
-StatusCode F77Utils::openOldFormatted ( int lun , const std::string& file ) 
+StatusCode F77Utils::openOldFormatted 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77ooffile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-};
+  const int result = f77ooffile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openOldFormatted      ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose ) 
+  {
+    std::cout << "F77Utils::openOldFormatted      INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }  
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+}
 // ==========================================================================
-/** @fn openUnknownFormatted
- *  Open "FORMATTED" Fortran file with status="UNKNOWN" 
- *  @param lun logical file number 
- *  @param file fiel name 
- *  @return status code 
- *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date   2006-10-03
- */
+// Open "FORMATTED" Fortran file with status="UNKNOWN" 
 // ==========================================================================
 StatusCode F77Utils::openUnknownFormatted 
-( int lun , const std::string& file ) 
+( int                lun     , 
+  const std::string& file    , 
+  const bool         verbose )  
 {
-  return 
-    lun != f77ouffile ( &lun , file.c_str() , file.size() ) ? 
-    StatusCode::FAILURE : StatusCode::SUCCESS ;
-};
+  const int result = f77ouffile ( &lun , file.c_str() , file.size() ) ;
+  if ( result != lun ) 
+  { 
+    std::cerr << "F77Utils::openUnknownFormatted  ERROR Error in opening LUN=" << lun 
+              << " FILE='"<< file << "' "<< std::endl ; 
+  }
+  else if ( verbose )
+  {
+    std::cout << "F77Utils::openUnknownFormatted  INFO  LUN=" << lun 
+              << " FILE='"<< file << "' is opened "<< std::endl ; 
+  }  
+  return result != lun ? StatusCode::FAILURE : StatusCode::SUCCESS ;
+}
 // ==========================================================================
+// find free fortran unit
+// ==========================================================================
+int F77Utils::getUnit( const bool verbose ) 
+{
+  int n = 0 ;
+  const int result = f77getunit ( &n ) ;
+  if ( 0 >= result || 100 <= result ) 
+  {
+    std::cerr << "F77Utils::getUnit               ERROR No free units" << std::endl ;
+  } 
+  else if ( verbose )  
+  {
+    std::cout << "F77Utils::getUnit               INFO  Free unit LUN=" << result << std::endl ;
+  }
+  return result ;
+}
+// ==========================================================================
+
 
 
 // ============================================================================
