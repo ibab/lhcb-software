3e89ae30ad84285d9e58e3a42e4dfec86015b3f5
Johannes Albrecht <johannes.albrecht@cern.ch>
2007-07-04 12:08:19 +0000

    first release of Calo seed preparation to a track for L0 confirmation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@70300 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 13eba3a916301d831f3cf009ffa24700f3d5d00a

---
+++ Hlt/Hlt/HltBase/HltBase/ICaloSeedTool.h
@@ -0,0 +1,43 @@
+// $Id: ICaloSeedTool.h,v 1.1 2007-07-04 12:08:19 albrecht Exp $
+#ifndef HLTBASE_ICALOSEEDTOOL_H 
+#define HLTBASE_ICALOSEEDTOOL_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+static const InterfaceID IID_ICaloSeedTool ( "ICaloSeedTool", 1, 0 );
+
+namespace LHCb
+{
+  class L0CaloCandidate;
+  class Track;
+};
+
+
+/** @class ICaloSeedTool ICaloSeedTool.h HltBase/ICaloSeedTool.h
+ *  
+ *
+ *  @author Johannes Albrecht
+ *  @date   2007-07-03
+ */
+class ICaloSeedTool : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_ICaloSeedTool; }
+
+  virtual StatusCode makeTrack( const LHCb::L0CaloCandidate& eL0Cand , 
+                                LHCb::Track& seedTrack ) = 0;
+  
+
+
+protected:
+
+private:
+
+};
+#endif // HLTBASE_ICALOSEEDTOOL_H
