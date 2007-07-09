1061cbd63d7d656353b8533a21847f94f71d5555
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-07-09 17:11:49 +0000

    v2r13
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@8875 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4b67190b270cef72ee86573db9322e0ce8ca351

--- Rec/Rich/RichRecQC/src/RichPIDQC.cpp
+++ Rec/Rich/RichRecQC/src/RichPIDQC.cpp
@@ -5,7 +5,7 @@
  *  Implementation file for RICH reconstruction monitoring algorithm : Rich::Rec::MC::PIDQC
  *
  *  CVS Log :-
- *  $Id: RichPIDQC.cpp,v 1.61 2007-06-14 12:59:30 jonrob Exp $
+ *  $Id: RichPIDQC.cpp,v 1.62 2007-07-09 17:11:49 cattanem Exp $
  *
  *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
  *  @date   2002-06-13
@@ -40,7 +40,7 @@ PIDQC::PIDQC( const std::string& name,
   declareProperty( "MaximumTrackMultiplicity", m_maxMultCut = 999999 );
   declareProperty( "HistoBins",     m_bins = 50 );
   declareProperty( "FinalPrintout", m_finalPrintOut = true );
-  declareProperty( "ExtraHistos",   m_extraHistos = true );
+  declareProperty( "ExtraHistos",   m_extraHistos = false );
   declareProperty( "IgnoreRecoThresholds", m_ignoreRecoThres = false );
   declareProperty( "IgnoreMCThresholds", m_ignoreMCThres     = false );
   declareProperty( "KaonDLLCut", m_dllKaonCut = 9999999 );
