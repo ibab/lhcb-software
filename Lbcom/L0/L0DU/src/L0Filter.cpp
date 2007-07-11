1b73f2c0bcc1f27103091768a599f3c159b8b76e
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2007-07-11 12:53:09 +0000

    New L0Filter algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@58837 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8839310adf99db4159bea1fc4b28d5240b5a2e79

---
+++ Lbcom/L0/L0DU/src/L0Filter.cpp
@@ -0,0 +1,109 @@
+// $Id: L0Filter.cpp,v 1.1 2007-07-11 12:53:09 pkoppenb Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+// from Event
+#include "Event/L0DUReport.h"
+
+// local
+#include "L0Filter.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : L0Filter
+//
+// 2007-07-11 : Patrick Koppenburg
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( L0Filter );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+L0Filter::L0Filter( const std::string& name,
+                    ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator )
+{
+  m_l0channels.clear();
+  declareProperty("L0DULocation", m_l0Location = LHCb::L0DUReportLocation::Default );
+  declareProperty("OrChannels", m_l0channels );
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+L0Filter::~L0Filter() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode L0Filter::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+  debug() << "==> Initialize" << endmsg;
+
+  info() << "Will require that any " ;
+  if ( !m_l0channels.empty()) {
+    info() << "of " ;
+    for ( std::vector<std::string>::const_iterator c = m_l0channels.begin() ; 
+          c != m_l0channels.end() ; ++c ) {
+      info() << *c << " " ;
+    }
+  }
+  info() << "channel(s) is passed" << endmsg ;
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode L0Filter::execute() {
+
+  debug() << "==> Execute" << endmsg;
+
+  const LHCb::L0DUReport* l0 = get<LHCb::L0DUReport>(m_l0Location);
+
+  setFilterPassed(false); // switch off by default
+
+  if (l0->decision()){ 
+    if ( !m_l0channels.empty()) {
+      for ( std::vector<std::string>::const_iterator c = m_l0channels.begin() ; 
+            c != m_l0channels.end() ; ++c ) {
+        if (l0->channelDecisionByName(*c)){
+          setFilterPassed(true);
+          if ( msgLevel(MSG::VERBOSE)) verbose() << "Event is accepted by " 
+                                                 << *c << endmsg ;
+        } else if ( msgLevel(MSG::VERBOSE)) verbose() << "Event is not accepted by " 
+                                                      << *c << endmsg ; 
+      }
+    } else { // any decision will do
+      if ( msgLevel(MSG::VERBOSE)) verbose() << "Event is accepted by L0" << endmsg ;
+      setFilterPassed(true);
+    }
+  } else {
+    setFilterPassed(false);
+    verbose() << "Event is rejected by L0" << endmsg ;
+  }
+  if ( msgLevel(MSG::DEBUG)){
+    if ( filterPassed()) debug() << "Event is accepted" << endmsg ;
+    else debug() << "Event is rejected" << endmsg ;
+  }
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode L0Filter::finalize() {
+
+  debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Lbcom/L0/L0DU/src/L0Filter.h
@@ -0,0 +1,51 @@
+// $Id: L0Filter.h,v 1.1 2007-07-11 12:53:09 pkoppenb Exp $
+#ifndef L0FILTER_H 
+#define L0FILTER_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+
+/** @class L0Filter L0Filter.h
+ *  
+ *  Algorithm that checks if L0Filter passed. Useful for conditional sequencing.
+ *  
+ *  To require any L0-yes, just run the algorithm unconfigured.
+ *  
+ *  To require a specific L0 channel do for instance
+ *   
+ *  @code
+ *  L0Filter.OrChannels = { "L0electron_High", "L0globalPi0_High" };
+ *  @endcode
+ *  
+ *  This will accept only events which passed L0electron_High or L0globalPi0_High.
+ *  Only the or-mode is supported. To accept events that passed two or more given 
+ *  channels use two instances of L0Filter.
+ *  
+ *  Valid channel names are given in $L0DUROOT/options/L0DU_Lumi2Setting.opts 
+ *  be very careful about spelling. Misspelled L0 channels will return false
+ *  with no warning.
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2007-07-11
+ */
+class L0Filter : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  L0Filter( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~L0Filter( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  std::string m_l0Location ;              ///< Location of L0 decision
+  std::vector<std::string> m_l0channels ; ///< required channels (Or-ed)
+  
+};
+#endif // L0FILTER_H
