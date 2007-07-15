08a8c667949947aac37714b1b2366c8778de8d28
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2007-07-15 16:57:48 +0000

    Updated L0ETC
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@58855 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a2a75f1cb8120f6ea23c4ad73302fc92333ae45c

--- Lbcom/L0/L0DU/src/L0ETC.cpp
+++ Lbcom/L0/L0DU/src/L0ETC.cpp
@@ -1,28 +1,24 @@
-// $Id: L0ETC.cpp,v 1.1 2007-07-13 18:01:38 pkoppenb Exp $
+// $Id: L0ETC.cpp,v 1.2 2007-07-15 16:57:48 pkoppenb Exp $
+// Include files 
 
 // from Gaudi
-#include "GaudiKernel/DeclareFactoryEntries.h" 
+#include "GaudiKernel/AlgFactory.h" 
 #include "GaudiKernel/IRegistry.h" 
-#include "GaudiKernel/IAlgManager.h"
 
-// from RecEvent
-#include "Event/SelResult.h"
-
-// the data
-#include "Event/RecHeader.h"
-#include "Event/ProtoParticle.h"
-#include "Event/RecVertex.h"
+// LHCb
+#include "Event/MCHeader.h"
+#include "Event/L0DUReport.h"
 
 // local
 #include "L0ETC.h"
 
-using namespace LHCb;
 //-----------------------------------------------------------------------------
 // Implementation file for class : L0ETC
 //
-// 01/05/2004 : Hugo Ruiz
+// 2007-07-13 : Patrick Koppenburg
 //-----------------------------------------------------------------------------
 
+// Declaration of the Algorithm Factory
 DECLARE_ALGORITHM_FACTORY( L0ETC );
 
 
@@ -30,47 +26,56 @@ DECLARE_ALGORITHM_FACTORY( L0ETC );
 // Standard constructor, initializes variables
 //=============================================================================
 L0ETC::L0ETC( const std::string& name,
-            ISvcLocator* pSvcLocator)
-  : GaudiTupleAlg ( name , pSvcLocator ) 
+              ISvcLocator* pSvcLocator)
+  : GaudiTupleAlg ( name , pSvcLocator )
     , m_events(0)
 {
     declareProperty( "CollectionName", m_collectionName = "<not set>");
-    declareProperty( "ToLookAt", m_toLookAt );
 }
+//=============================================================================
+// Destructor
+//=============================================================================
+L0ETC::~L0ETC() {} 
 
 //=============================================================================
-// Initialisation. Check parameters
+// Initialization
 //=============================================================================
 StatusCode L0ETC::initialize() {
-  
-  // Initialisation of ETC
-  StatusCode sc = GaudiTupleAlg::initialize();
-  if (!sc) return sc;
-  
-  
-  // Prepare a list of instances of the PostScaler Algorithm
-  // from the list of selections
-  
-  // Make a list of candidate instance names
-  std::vector<std::string> nameList;
-  for (std::vector<std::string>::iterator iName=m_toLookAt.begin();
-       iName!=m_toLookAt.end(); ++iName ) {
-    nameList.push_back("Print"+(*iName));
-  }
-  
-  return StatusCode::SUCCESS ;
-  
-};
+  StatusCode sc = GaudiTupleAlg::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiTupleAlg
+
+  debug() << "==> Initialize" << endmsg;
+
+  /// @todo would be nice to get that from /Event or from L0DU.
+  m_l0channels.push_back("L0electron_High");
+  m_l0channels.push_back("L0electron_Low");
+  m_l0channels.push_back("L0photon_High");
+  m_l0channels.push_back("L0photon_Low");
+  m_l0channels.push_back("L0localPi0_High");
+  m_l0channels.push_back("L0localPi0_Low");
+  m_l0channels.push_back("L0globalPi0_High");
+  m_l0channels.push_back("L0globalPi0_Low");
+  m_l0channels.push_back("L0hadron_High");
+  m_l0channels.push_back("L0hadron_Low");
+  m_l0channels.push_back("L0muon_High");
+  m_l0channels.push_back("L0muon_Low");
+  m_l0channels.push_back("L0dimuon_High");
+  m_l0channels.push_back("L0dimuon_Low");
+
+  return StatusCode::SUCCESS;
+}
+
 //=============================================================================
 // Main execution
 //=============================================================================
 StatusCode L0ETC::execute() {
+
   debug() << "==> Execute" << endmsg;
-  StatusCode sc = StatusCode::SUCCESS ;
-  m_events++;
-  
+
   // always pass, will use selection on tag to reduce later...
   setFilterPassed(true);
+
+  m_events++ ;
   
   // This is what it is about...
   Tuple tup = evtCol(m_collectionName);
@@ -79,11 +84,47 @@ StatusCode L0ETC::execute() {
   // this is what makes the tag collection a collection...
   DataObject* pObject = get<DataObject>("/Event");
   if (pObject!=0) {
-    sc = tup->column("Address", pObject->registry()->address() ); 
+    StatusCode sc = tup->column("Address", pObject->registry()->address() ); 
+    if (!sc) return sc;
   } else {
     Error("    not able to retrieve IOpaqueAddress");
   }
   
-  return sc ;
-    
-};
+  // Retrieve informations about event
+  if (exist<LHCb::MCHeader>(LHCb::MCHeaderLocation::Default)){
+    const LHCb::MCHeader *evtHeader = get<LHCb::MCHeader>(LHCb::MCHeaderLocation::Default);
+    tup->column("event", (int)evtHeader->evtNumber() );
+    tup->column("run",   (int)evtHeader->runNumber() );
+  } else {
+    Warning("    not able to retrieve MCHeader");
+  }
+
+  // get L0 result
+  if ( exist<LHCb::L0DUReport>(LHCb::L0DUReportLocation::Default)){
+    LHCb::L0DUReport* L0 = get<LHCb::L0DUReport>(LHCb::L0DUReportLocation::Default);
+    tup->column ( "L0",  L0->decision() ) ;
+    for ( std::vector<std::string>::const_iterator c = m_l0channels.begin();
+          c != m_l0channels.end(); ++c){
+      verbose() << *c << " says " << L0->channelDecisionByName(*c) << endmsg ;
+      tup->column ( (*c), L0->channelDecisionByName(*c)  ) ;
+    }
+  } else {
+    Warning("    not able to retrieve L0DUReport");
+    tup->column ( "L0", false ) ;
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode L0ETC::finalize() {
+
+  debug() << "==> Finalize" << endmsg;
+  info() << "Number of events " << m_events << endmsg ;
+
+  return GaudiTupleAlg::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
--- Lbcom/L0/L0DU/src/L0ETC.h
+++ Lbcom/L0/L0DU/src/L0ETC.h
@@ -1,48 +1,37 @@
-// $Id: L0ETC.h,v 1.1 2007-07-13 18:01:38 pkoppenb Exp $
+// $Id: L0ETC.h,v 1.2 2007-07-15 16:57:48 pkoppenb Exp $
 #ifndef L0ETC_H 
 #define L0ETC_H 1
 
 // Include files
-// from Standard Library
-#include <string>
-#include <map>
-#include <vector>
-
 // from Gaudi
 #include "GaudiAlg/GaudiTupleAlg.h"
-
 /** @class L0ETC L0ETC.h
  *
- *  @author P. Koppenburg
- *  @date   12/07/2007
+ *  Writes out ETC for L0 stripping  
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2007-07-13
  */
-
 class L0ETC : public GaudiTupleAlg {
-public:
+public: 
   /// Standard constructor
   L0ETC( const std::string& name, ISvcLocator* pSvcLocator );
 
-  virtual ~L0ETC( ){}; ///< Destructor
+  virtual ~L0ETC( ); ///< Destructor
 
   virtual StatusCode initialize();    ///< Algorithm initialization
   virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
 
+protected:
 
 private:
-
-private:
-
   /// ETC name
   std::string m_collectionName;
-
-  /// List of SelResult objects to look for
-  std::vector<std::string> m_toLookAt;
-
-  typedef std::map<std::string,std::string> map_t;
-  map_t m_columns;
+  /// L0 channels
+  std::vector<std::string> m_l0channels ;
 
   long m_events ; ///< Number of events
-  
 
 };
 #endif // L0ETC_H
