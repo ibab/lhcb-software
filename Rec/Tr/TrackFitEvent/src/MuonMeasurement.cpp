16d4967145234736d3cd8a9587c8eda9781e3e09
spozzi <spozzi@cern.ch>
2007-07-23 11:12:35 +0000

    - removed 2nd trajectory from MuonMeasurement
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@16966 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9f0a88defdc380fcc42d18f7ee1f6721d77de7f0

--- Rec/Tr/TrackFitEvent/src/MuonMeasurement.cpp
+++ Rec/Tr/TrackFitEvent/src/MuonMeasurement.cpp
@@ -1,4 +1,4 @@
-// $Id: MuonMeasurement.cpp,v 1.1 2007-04-27 20:38:51 polye Exp $
+// $Id: MuonMeasurement.cpp,v 1.2 2007-07-23 11:12:35 spozzi Exp $
 // Include files 
 
 // local
@@ -10,27 +10,35 @@ using namespace LHCb;
 //-----------------------------------------------------------------------------
 // Implementation file for class : MuonMeasurement
 //
-// Author: Jose Hernando, Miriam Gandelman, Erica Polycarpo
+// Author: Jose Hernando, Miriam Gandelman, Erica Polycarpo, Silvia Pozzi
 // Created: 27-04-2007
 //-----------------------------------------------------------------------------
 
 /// Standard constructor, initializes variables
 MuonMeasurement::MuonMeasurement( const LHCbID& lhcbID,
-                              const Gaudi::XYZPoint& position,
-                              double dx, double dy ): 
-  m_position(position), m_dx(dx), m_dy(dy) 
+                                  const Gaudi::XYZPoint& position,
+                                  double dx, double dy,
+                                  MuonMeasurement::MuonMeasurementType& XY): 
+  m_position(position),  m_muonProjection(XY) 
 {
   setLhcbID(lhcbID);
   setZ(position.z());
   setType(Measurement::Muon);
-  setMeasure(position.x());
-  setErrMeasure(2.*dx/sqrt(12.));
-  Gaudi::XYZVector x_dir(1,0,0); 
-  Gaudi::XYZVector y_dir(0,1,0); 
-  std::pair<double,double> x_range(position.x()-dx,position.x()+dx);
-  std::pair<double,double> y_range(position.y()-dy,position.y()+dy);
-  m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, x_dir, x_range, true));
-  m_trajectoryY = std::auto_ptr<Trajectory>(new LineTraj(position, y_dir, y_range, true));
+  if(m_muonProjection == Y){
+    setMeasure(position.x());
+    setErrMeasure(2.*dy/sqrt(12.));
+    Gaudi::XYZVector x_dir(1,0,0); 
+    std::pair<double,double> x_range(-dx,dx);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, x_dir, x_range, true));
+
+  }else{
+    setMeasure(position.y());
+    setErrMeasure(2.*dx/sqrt(12.));
+    Gaudi::XYZVector y_dir(0,1,0); 
+    std::pair<double,double> y_range(-dy,dy);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, y_dir, y_range, true));  
+
+  }
 }
 
 /// Copy constructor
