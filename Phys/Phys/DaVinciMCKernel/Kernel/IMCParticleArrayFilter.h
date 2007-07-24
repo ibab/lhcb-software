25a41727bca58f7cb24b14d3e902434c99b5ab75
jpalac <jpalac@cern.ch>
2007-07-24 12:10:00 +0000

    Interface to filter out MCParticles from within an array
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63740 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1e486ec65aaf7618486adc78e56a40b5409882aa

---
+++ Phys/Phys/DaVinciMCKernel/Kernel/IMCParticleArrayFilter.h
@@ -0,0 +1,42 @@
+// $Id: IMCParticleArrayFilter.h,v 1.1 2007-07-24 12:10:00 jpalac Exp $
+#ifndef KERNEL_IMCPARTICLEARRAYFILTER_H 
+#define KERNEL_IMCPARTICLEARRAYFILTER_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+// from Event
+#include "Event/MCParticle.h"
+
+static const InterfaceID IID_IMCParticleArrayFilter ( "IMCParticleArrayFilter", 1, 0 );
+
+/** @class IMCParticleArrayFilter IMCParticleArrayFilter.h Kernel/IMCParticleArrayFilter.h
+ *  
+ *
+ *  @author Juan Palacios
+ *  @date   2007-07-24
+ */
+class IMCParticleArrayFilter : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IMCParticleArrayFilter; }
+
+  /// Filter and put results into new array
+  virtual StatusCode filter( const LHCb::MCParticle::ConstVector&, 
+                             LHCb::MCParticle::ConstVector& ) const = 0;
+  /// Filter and remove elements that do not pass filter from array
+  virtual StatusCode filter( LHCb::MCParticle::ConstVector&,) const = 0;
+
+
+
+protected:
+
+private:
+
+};
+#endif // KERNEL_IMCPARTICLEARRAYFILTER_H
