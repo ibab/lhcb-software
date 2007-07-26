243926758e64ded887ff2d0994a61fbb52c6d004
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-07-26 14:48:51 +0000

    add LoKi::Hybrid:(ParticleArrayFilter&FilterParticles)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@49993 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c0ee41d3623248c3f2cff3a1ca11f1934819ca33

---
+++ Phys/Phys/LoKiArrayFunctors/src/Components/HybridFilterParticles.cpp
@@ -0,0 +1,112 @@
+// $Id: HybridFilterParticles.cpp,v 1.1 2007-07-26 14:48:51 ibelyaev Exp $
+// ============================================================================
+// Include files 
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h" 
+// ============================================================================
+// GaudiAlg
+// ============================================================================
+#include "GaudiAlg/GaudiTool.h" 
+// ============================================================================
+// DaVinciKernel
+// ============================================================================
+#include "Kernel/IFilterParticles.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/IHybridFactory.h"
+#include "LoKi/select.h"
+#include "LoKi/Primitives.h"
+// ============================================================================
+namespace LoKi
+{
+  namespace Hybrid 
+  {
+    /** @class FilterParticles HybridFilterParticles.cpp
+     *  Simple "hybrid-based" implementation of the interface 
+     *  IFilterParticles
+     *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+     *  @date 2007-07-26
+     */
+    class FilterParticles
+      : public GaudiTool 
+      , public virtual IFilterParticles
+    {
+      /// the friend factory for istantiation
+      friend class ToolFactory<LoKi::Hybrid::FilterParticles> ;
+    public:
+      /// Test if filter is satisfied on ensemble of particles
+      virtual bool isSatisfied
+      ( const LHCb::Particle::ConstVector& a ) const { return m_cut ( a ) ; }
+      /// Test if filter is satisfied on ensemble of particles
+      virtual bool operator()
+      ( const LHCb::Particle::ConstVector& a ) const { return m_cut ( a ) ; }
+    public:
+      // ==================================================================
+      /// intialize the tool 
+      virtual StatusCode initialize () ;      
+      // ==================================================================
+    protected:
+      // ==================================================================
+      /// Standard constructor
+      FilterParticles
+      ( const std::string& type, 
+        const std::string& name,
+        const IInterface* parent)
+        : GaudiTool ( type , name , parent )
+        , m_cut     ( LoKi::BooleanConstant<LoKi::ATypes::Combination>( false ) ) 
+        , m_code    ( "ANONE" )
+        , m_factory ( "LoKi::Hybrid::Tool/HybridFactory:PUBLIC") 
+      {
+        declareInterface<IFilterParticles>(this);
+        declareProperty 
+          ( "Code"    , m_code    , "Python pseudocode for the filter criteria"  ) ;
+        declareProperty 
+          ( "Factory" , m_factory , "Type/Name for C++/Python Hybrid Factory" ) ;
+      } ;
+      // ==================================================================      
+      /// destructor : virtual and protected
+      virtual ~FilterParticles () {}
+      // ==================================================================
+    private:
+      // selection criteria itself 
+      LoKi::Types::ACut m_cut ; ///< selection criteria itself
+      // python pseudo-code
+      std::string       m_code    ; ///< python pseudo-code
+      // factory type/name
+      std::string       m_factory ; ///< factory type/name
+    } ;
+    // ==================================================================
+  } // end of namespace LoKi::Hybrid
+} // end of namespace LoKi 
+// ============================================================================
+/// Declaration of the Tool Factory
+DECLARE_NAMESPACE_TOOL_FACTORY(LoKi::Hybrid,FilterParticles);
+// ============================================================================
+// the intializationfo the tool
+// ============================================================================
+StatusCode LoKi::Hybrid::FilterParticles::initialize () 
+{
+  // (1) initialize the base 
+  StatusCode  sc = GaudiTool::initialize() ;
+  if ( sc.isFailure() ) { return sc ; }                          // RETURN 
+  // (2) get the factory:
+  IHybridFactory* factory = tool<IHybridFactory> ( m_factory , this ) ;  
+  if ( 0 == factory ) 
+  { return Error ( "Could not locate IHybridFactory" ) ; }      // RETURN 
+  // (3) use the factory to get the cuts 
+  sc = factory->get (  m_code , m_cut ) ;
+  if ( sc.isFailure() ) 
+  { return Error ( "Error from IHybridFactory", sc   ) ; }     // RETURN 
+  // 
+  info() << "CUT: '" << m_cut << "' "<< endreq ;
+  //
+  return StatusCode::SUCCESS ;  
+}
+// ============================================================================
+// The END 
+// ============================================================================
+
+
---
+++ Phys/Phys/LoKiArrayFunctors/src/Components/HybridParticleArrayFilter.cpp
@@ -0,0 +1,140 @@
+// $Id: HybridParticleArrayFilter.cpp,v 1.1 2007-07-26 14:48:51 ibelyaev Exp $
+// ============================================================================
+// Include files 
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h" 
+// ============================================================================
+// GaudiAlg
+// ============================================================================
+#include "GaudiAlg/GaudiTool.h" 
+// ============================================================================
+// DaVinciKernel
+// ============================================================================
+#include "Kernel/IParticleArrayFilter.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/IHybridFactory.h"
+#include "LoKi/select.h"
+#include "LoKi/Primitives.h"
+#include "LoKi/Operators.h"
+// ============================================================================
+namespace LoKi
+{
+  namespace Hybrid 
+  {
+    /** @class ParticleArrayFilter HybridParticleArrayFilter.cpp
+     *  Simple "hybrid-based" implementation of the interface 
+     *  IParticelArrayFilter 
+     *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+     *  @date 2007-07-26
+     */
+    class ParticleArrayFilter
+      : public GaudiTool 
+      , public virtual IParticleArrayFilter 
+    {
+      /// the friend factory for istantiation
+      friend class ToolFactory<LoKi::Hybrid::ParticleArrayFilter> ;
+    public:
+      // ==================================================================
+      /** Filter and put the results into new array
+       *  @see IParticleArrayFilter
+       */
+      virtual StatusCode filter
+      ( const LHCb::Particle::ConstVector& input    , 
+        LHCb::Particle::ConstVector&       filtered ) const 
+      {
+        filtered.clear() ;
+        // copy the particles,whcih satisfy the cut into output conterner
+        LoKi::select                      ///< missing "std::copy_if" 
+          ( input.begin ()              , ///< begin of the input sequence 
+            input.end   ()              , ///< end of the input sequence      
+            std::back_inserter ( filtered ) ,   ///< destination 
+            m_cut                           ) ; ///< criteria  
+        //
+        return StatusCode::SUCCESS ;
+      }
+      // ==================================================================
+      /** Filter and remove elements that do not pass filter from array
+       *  @see IParticleArrayFilter
+       */
+      virtual StatusCode filter
+      ( LHCb::Particle::ConstVector& particles ) const 
+      {
+        // remove all particles which do *NOT* satisfy the criteria:
+        particles.erase 
+          ( std::remove_if ( particles.begin () , 
+                             particles.end   () , 
+                             !m_cut             ) , particles.end () ) ;
+        //
+        return StatusCode::SUCCESS ;
+      }
+    public:
+      // ==================================================================
+      /// intialize the tool 
+      virtual StatusCode initialize () ;      
+      // ==================================================================
+    protected:
+      // ==================================================================
+      /// Standard constructor
+      ParticleArrayFilter  
+      ( const std::string& type, 
+        const std::string& name,
+        const IInterface* parent)
+        : GaudiTool ( type , name , parent )
+        , m_cut     ( LoKi::BooleanConstant<const LHCb::Particle*>( false ) ) 
+        , m_code    ( "NONE" )
+        , m_factory ( "LoKi::Hybrid::Tool/HybridFactory:PUBLIC") 
+      {
+        declareInterface<IParticleArrayFilter>(this);
+        declareProperty 
+          ( "Code"    , m_code    , "Python pseudocode for the filter criteria"  ) ;
+        declareProperty 
+          ( "Factory" , m_factory , "Type/Name for C++/Python Hybrid Factory" ) ;
+      } ;
+      // ==================================================================      
+      /// destructor : virtual and protected
+      virtual ~ParticleArrayFilter( ){}
+      // ==================================================================
+    private:
+      // selection criteria itself 
+      LoKi::Types::Cut  m_cut ; ///< selection criteria itself
+      // python pseudo-code
+      std::string       m_code    ; ///< python pseudo-code
+      // factory type/name
+      std::string       m_factory ; ///< factory type/name
+    } ;
+    // ==================================================================
+  } // end of namespace LoKi::Hybrid
+} // end of namespace LoKi 
+// ============================================================================
+/// Declaration of the Tool Factory
+DECLARE_NAMESPACE_TOOL_FACTORY(LoKi::Hybrid,ParticleArrayFilter);
+// ============================================================================
+// the intializationfo the tool
+// ============================================================================
+StatusCode LoKi::Hybrid::ParticleArrayFilter::initialize () 
+{
+  // (1) initialize the base 
+  StatusCode  sc = GaudiTool::initialize() ;
+  if ( sc.isFailure() ) { return sc ; }                          // RETURN 
+  // (2) get the factory:
+  IHybridFactory* factory = tool<IHybridFactory> ( m_factory , this ) ;  
+  if ( 0 == factory ) 
+  { return Error ( "Could not locate IHybridFactory" ) ; }      // RETURN 
+  // (3) use the factory to get the cuts 
+  sc = factory->get (  m_code , m_cut ) ;
+  if ( sc.isFailure() ) 
+  { return Error ( "Error from IHybridFactory", sc   ) ; }     // RETURN 
+  // 
+  info() << "CUT: '" << m_cut << "' "<< endreq ;
+  //
+  return StatusCode::SUCCESS ;  
+}
+// ============================================================================
+// The END 
+// ============================================================================
+
+
