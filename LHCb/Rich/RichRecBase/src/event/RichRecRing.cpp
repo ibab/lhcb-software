f393adb203fc1919bdfe05cbd7adff5bd9261478
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-08-13 13:04:53 +0000

    minor speed up
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@8934 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3e5cdf69ca1047b70b3d40763afb021e28ee95e2

--- LHCb/Rich/RichRecBase/src/event/RichRecRing.cpp
+++ LHCb/Rich/RichRecBase/src/event/RichRecRing.cpp
@@ -5,7 +5,7 @@
  *  Implementation file for class : LHCb::RichRecRing
  *
  *  CVS Log :-
- *  $Id: RichRecRing.cpp,v 1.1 2007-08-09 15:51:12 jonrob Exp $
+ *  $Id: RichRecRing.cpp,v 1.2 2007-08-13 13:04:53 jonrob Exp $
  *
  *  @author Chris Jones    Christopher.Rob.Jones@cern.ch
  *  @date   23/07/2007
@@ -28,15 +28,16 @@ const LHCb::RichRecPointOnRing*
 LHCb::RichRecRing::getPointClosestInAzimuth(const double angle) const
 {
   const LHCb::RichRecPointOnRing* point =  NULL;
-  if ( !ringPoints().empty() )
+  const unsigned int nPoints = ringPoints().size();
+  if ( nPoints>0 )
   {
 
     // Increment in phi
-    const double incPhi = Gaudi::Units::twopi / static_cast<double>(ringPoints().size());
+    const double incPhi = Gaudi::Units::twopi / static_cast<double>(nPoints);
 
     // which point
-    unsigned int iPoint = (int)((angle/incPhi) - 0.5);
-    if ( iPoint >= ringPoints().size() ) iPoint = ringPoints().size()-1;
+    unsigned int iPoint = (unsigned int)((angle/incPhi) - 0.5);
+    if ( iPoint >= nPoints ) iPoint = nPoints-1;
 
     // set the point
     point = &ringPoints()[iPoint];
