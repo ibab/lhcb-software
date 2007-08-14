2ac55dc2340c428749d4b0a035a5b31eb40013e3
Markus Frank <frankb@cern.ch>
2007-08-14 15:48:31 +0000

    v4r0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97048 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 706098de213d0d31725da1dd995a462bdd87e40e

--- Online/OnlineSys/cmt/build.sh
+++ Online/OnlineSys/cmt/build.sh
@@ -1,5 +1,20 @@
-#/bin/sh
+#/bin/bash
 #
-${CMTROOT}/${CMTBIN}/cmt.exe broadcast "rm -rf ../${CMTCONFIG} || /bin/true"
-${CMTROOT}/${CMTBIN}/cmt.exe broadcast "${CMTROOT}/${CMTBIN}/cmt.exe config"
-${CMTROOT}/${CMTBIN}/cmt.exe broadcast make
+if test -n "$1"; 
+then
+    for i in $*;
+    do 
+	export CMTCONFIG=$i;
+        echo "Now building configuration: $CMTCONFIG "
+	#${CMTROOT}/${CMTBIN}/cmt.exe broadcast "rm -rf ../${CMTCONFIG} || /bin/true";
+	#${CMTROOT}/${CMTBIN}/cmt.exe broadcast "${CMTROOT}/${CMTBIN}/cmt.exe config";
+	if test -e build_${CMTCONFIG}.log; then rm -f build_${CMTCONFIG}.log; fi;
+	${CMTROOT}/${CMTBIN}/cmt.exe broadcast make >build_${CMTCONFIG}.log 2>&1;
+    done;
+else
+    echo "No platform specification given: slc4_ia32_gcc34_dbg";
+    echo "                                 slc4_ia32_gcc34";
+    echo "                                 slc4_amd64_gcc34_dbg";
+    echo "                                 slc4_amd64_gcc34";
+    echo "                                 ....";
+fi;
---
+++ Online/OnlineSys/cmt/checkVersions.py
@@ -0,0 +1,70 @@
+#/usr/bin/python
+import os
+
+lines = os.popen('cmt show uses | grep -v "#" | grep "use "').readlines()
+curr_dir = os.getcwd()
+dir2 = curr_dir[0:curr_dir.rfind('/')]
+dir2 = dir2[0:dir2.rfind('/')+1]
+print dir2
+
+for line in lines:
+  items = line.split()[1:]
+  package = items[0]
+  version = items[1]
+  head = ''
+  if len(items) == 3:
+    dir = items[2][1:-1]
+    path = dir+package
+    pkg = package
+  else:
+    head = items[2]
+    dir = items[3][1:-1]
+    path = dir+head+os.sep+package
+    pkg = head+os.sep+package
+  if dir == dir2:
+    last_tag = []
+    lines = os.popen('cd '+path+'; cvs status -v cmt/requirements').readlines()
+    p = 0
+    for line in lines:
+      if p == 1:
+        tag = line.replace(' ','').replace('(',' (').replace('\t','').replace('revision:','').split('(')
+        if len(tag):
+          last_tag.append(tag[0])
+          if len(last_tag)>4: 
+            last_tag = str(last_tag).replace('[','').replace(']','').replace("'",'')
+            break
+      if line.find('Existing Tags:')>0:
+        p = 1
+    pipe = os.popen3('cd '+path+'; cvs diff -r '+version)
+    diffs = pipe[1].readlines()
+    new = pipe[2].readlines()
+    changes = 0
+    prefix = '%-28s %-10s'%(pkg,'['+version+']',)
+    if len(diffs) > 0:
+      for d in diffs:
+        if d[:2] == '? ': continue
+        changes = 1
+    if changes == 0:
+      for n in new:
+        if n.find('cvs diff: tag')==0:
+	  idx = n.find('not in file')
+	  print prefix,'New file:     ',pkg+n[idx+len('not in file '):-1]
+          changes = 1
+        elif n.find('no longer exists') > 0:
+	  print prefix,'File removed: ',pkg+n.split(' ')[2]
+	  changes = 1
+        elif n.find('cvs diff: Diffing') == 0:
+	  pass
+        else:
+          print 'Nothing abnormal:',n[:-1]
+    if changes:
+      tag = version.replace('v','').replace('r',' ').replace('p',' ').split(' ')
+      print prefix,'========> HEAD differs from version '+version+' of package:',pkg
+      print prefix,'          Last tags:',last_tag
+    else:
+      print prefix,'UNCHANGED Package version '+version+'    package:',pkg
+      # print prefix,'          Last tags:',last_tag
+  else:
+    #print 'REJECT',package,version,dir,'Head:',head
+    pass
+
