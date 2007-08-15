b25b2c1829cf6ddc7d83d9dffd388e8a69b32631
Markus Frank <frankb@cern.ch>
2007-08-15 11:46:43 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97052 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eaf097757b2b7651f6bab0286ac89c379e506d1f

--- Online/OnlineSys/cmt/build.sh
+++ Online/OnlineSys/cmt/build.sh
@@ -1,15 +1,35 @@
 #/bin/bash
 #
-if test -n "$1"; 
+show=;
+config=0;
+clean=0;
+args="";
+#
+#
+for i in $*;
+do
+  if test $i = "--clean"; then clean=1;
+  elif test $i = "--config"; then config=1;
+  else args=`echo $args $i`;
+  fi;
+done;
+echo $args
+if test -n "$args"; 
 then
-    for i in $*;
+    for i in $args;
     do 
 	export CMTCONFIG=$i;
         echo "Now building configuration: $CMTCONFIG "
-	#${CMTROOT}/${CMTBIN}/cmt.exe broadcast "rm -rf ../${CMTCONFIG} || /bin/true";
-	#${CMTROOT}/${CMTBIN}/cmt.exe broadcast "${CMTROOT}/${CMTBIN}/cmt.exe config";
-	if test -e build_${CMTCONFIG}.log; then rm -f build_${CMTCONFIG}.log; fi;
-	${CMTROOT}/${CMTBIN}/cmt.exe broadcast make >build_${CMTCONFIG}.log 2>&1;
+        if test $clean = 1; 
+	then
+	    $show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast "rm -rf ../${CMTCONFIG} || /bin/true";
+	fi;
+        if test $config = 1;
+        then
+            $show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast "${CMTROOT}/${CMTBIN}/cmt.exe config";
+	fi;
+        if test -e build_${CMTCONFIG}.log; then $show rm -f build_${CMTCONFIG}.log; fi;
+	$show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast make >build_${CMTCONFIG}.log 2>&1;
     done;
 else
     echo "No platform specification given: slc4_ia32_gcc34_dbg";
