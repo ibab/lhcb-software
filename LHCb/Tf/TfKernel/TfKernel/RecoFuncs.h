3af3ead4bb7aae1cef2759fcfc7c33445e8ed122
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-08-16 14:23:52 +0000

    improve comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17007 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 82f3e0439060feb5ff07df6f7a204d669a5bf066

--- LHCb/Tf/TfKernel/TfKernel/RecoFuncs.h
+++ LHCb/Tf/TfKernel/TfKernel/RecoFuncs.h
@@ -1,99 +1,177 @@
 #ifndef RECOFUNCS_H 
 #define RECOFUNCS_H 1
 
+//-----------------------------------------------------------------------------
 /** @file RecoFuncs.h
+ *
  *  Collection of functions frequently used in all
  *  pattern reco algorithms
+ *
+ *  CVS Log :-
+ *  $Id: RecoFuncs.h,v 1.2 2007-08-16 14:23:52 jonrob Exp $
+ *
  *  @authors S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
  *  @date   2007-06-03
  **/
+//-----------------------------------------------------------------------------
 
+#include <algorithm>
 #include "TfKernel/OTHit.h"
 #include "GaudiKernel/Plane3DTypes.h"
 #include "GaudiKernel/Point3DTypes.h"
 #include "GaudiKernel/Vector3DTypes.h"
 #include "LHCbMath/GeomFun.h"
 
-namespace Tf {
+namespace Tf
+{
 
+  /** @class compByX_LB TfKernel/RecoFuncs.h
+   *  Binary function to compare a hit to a given x value and return true if hit is upstream of that point.
+   *  @author S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
+   *  @date   2007-05-30
+   */
   template <class Hit>
-  class compByX_LB: public std::binary_function<const Hit*, const double, bool> 
+  class compByX_LB : public std::binary_function<const Hit*, const double, bool>
   {
   public:
     compByX_LB() {}
-    inline bool operator() (const Hit * obj, const double& testVal) const
+    /** Comparison operator
+     *  @param[in] obj     The Hit object to test
+     *  @param[in] testVal The test value of x
+     *  @return boolean
+     *  @retval TRUE hit is upstream of testVal
+     *  @retval TRUE hit is downstream of testVal
+     */
+    inline bool operator() (const Hit * obj, const double testVal) const
     {
       return obj->hit()->xAtYEq0() < testVal ;
     }
   };
 
-
+  /** @class increasingByProjection TfKernel/RecoFuncs.h
+   *  Binary sorting function to sort hits by projection
+   *  @author S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
+   *  @date   2007-05-30
+   */
   template<class Hit>
-    class increasingByProjection{
-    public:
-    bool operator() ( const Hit* lhs, 
-                      const Hit* rhs ) const {
+  class increasingByProjection : public std::binary_function<const Hit*, const Hit*, bool>
+  {
+  public:
+    /** Comparison operator
+     *  @param[in] lhs Hit object on lhs of < operator
+     *  @param[in] rhs Hit object on rhs of < operator
+     *  @return boolean indicating the sorting of lhs and rhs by projection
+     */
+    inline bool operator() ( const Hit* lhs,
+                             const Hit* rhs ) const
+    {
       return lhs->projection() < rhs->projection();
     }
   };
-  
 
+  /** @class increasingByZ TfKernel/RecoFuncs.h
+   *  Binary sorting function to sort hits by z
+   *  @author S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
+   *  @date   2007-05-30
+   */
   template<class Hit>
-    class increasingByZ{
-    public:
-    bool operator() (const Hit*  lhs,  
-		     const Hit*  rhs){
+  class increasingByZ : public std::binary_function<const Hit*, const Hit*, bool>
+  {
+  public:
+    /** Comparison operator
+     *  @param[in] lhs Hit object on lhs of < operator
+     *  @param[in] rhs Hit object on rhs of < operator
+     *  @return boolean indicating the sorting of lhs and rhs by z
+     */
+    inline bool operator() (const Hit*  lhs,
+                            const Hit*  rhs)
+    {
       return lhs->z() < rhs->z();
-    } 
+    }
   };
-   
 
+  /** @class increasingByX TfKernel/RecoFuncs.h
+   *  Binary sorting function to sort hits by x
+   *  @author S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
+   *  @date   2007-05-30
+   */
   template<class Hit>
-    class increasingByX{
-    public:
-    bool operator() (const Hit*  lhs,  
-		     const Hit*  rhs){
+  class increasingByX : public std::binary_function<const Hit*, const Hit*, bool>
+  {
+  public:
+    /** Comparison operator
+     *  @param[in] lhs Hit object on lhs of < operator
+     *  @param[in] rhs Hit object on rhs of < operator
+     *  @return boolean indicating the sorting of lhs and rhs by x
+     */
+    inline bool operator() (const Hit*  lhs,
+                            const Hit*  rhs)
+    {
       return lhs->x() < rhs->x();
     }
   };
 
-   template<class Hit>
-    class increasingByXAtYEq0{
-    public:
-    bool operator() (const Hit*  lhs,  
-		     const Hit*  rhs){
+  /** @class increasingByXAtYEq0 TfKernel/RecoFuncs.h
+   *  Binary sorting function to sort hits by x at the point y=0
+   *  @author S. Hansmann-Menzemer, W. Houlsbergen, C. Jones, K. Rinnert
+   *  @date   2007-05-30
+   */
+  template<class Hit>
+  class increasingByXAtYEq0 : public std::binary_function<const Hit*, const Hit*, bool>
+  {
+  public:
+    /** Comparison operator
+     *  @param[in] lhs Hit object on lhs of < operator
+     *  @param[in] rhs Hit object on rhs of < operator
+     *  @return boolean indicating the sorting of lhs and rhs by x at the point y=0
+     */
+    inline bool operator() (const Hit*  lhs,
+                            const Hit*  rhs)
+    {
       return lhs->hit()->xAtYEq0() < rhs->hit()->xAtYEq0();
     }
   };
 
-
-   // y0  is here y of track @ z position plane
-   template<class Hit>
-   inline void updateTTHitForTrack(Hit* hit, double y0, double dyDz){
-     double z = hit->hit()->zAtYEq0() + dyDz * hit->hit()->dzDy();
-     double y = y0 + dyDz*(z-hit->hit()->zAtYEq0());
-     hit->setZ( z ) ;
-     hit->setX( hit->hit()->x(y) ) ;
-   }
-  
-  
-
-   // y0 is here y of track @ z=0
+  /// XXX???XXX What is this for ?
   template<class Hit>
-  inline void updateHitForTrack(Hit* hit, double y0, double dyDz){
-    double y  = ( y0 + dyDz * hit->hit()->zAtYEq0() ) / ( 1. - hit->hit()->dzDy() * dyDz );
+  inline void updateTTHitForTrack ( Hit* hit, 
+                                    const double y0, 
+                                    const double dyDz ) 
+  {
+    const double z = hit->hit()->zAtYEq0() + dyDz * hit->hit()->dzDy();
+    const double y = y0 + dyDz*(z-hit->hit()->zAtYEq0());
+    hit->setZ( z ) ;
+    hit->setX( hit->hit()->x(y) ) ;
+  }
+
+  /// XXX???XXX What is this for ?
+  template<class Hit>
+  inline void updateHitForTrack ( Hit* hit, 
+                                  const double y0, 
+                                  const double dyDz ) 
+  {
+    const double y  = ( y0 + dyDz * hit->hit()->zAtYEq0() ) / ( 1. - hit->hit()->dzDy() * dyDz );
     hit->setZ( hit->hit()->z(y) ) ;
     hit->setX( hit->hit()->x(y) ) ;
     const Tf::OTHit* otHit = hit->hit()->othit();
-    if(otHit) {
+    if(otHit) 
+    {
       hit->setDriftDistance(otHit->untruncatedDriftDistance(y));
     }
   }
 
+  /** Function to find the intersection of a (line) hit with a plane
+   *  @param[in] hit The hit object pointer
+   *  @param[in] plane The plane to find the intersection with
+   *  @param[out] intersect The interestion point
+   *  @return boolean indicating the status of the interestion
+   *  @retval TRUE An intersection point was successfully found
+   *  @retval FALSE No intersection point was found
+   */
   template<class Hit>
-  inline bool intersection( Hit* hit,
+  inline bool intersection( const Hit* hit,
                             const Gaudi::Plane3D& plane,
-                            Gaudi::XYZPoint& intersect ) 
+                            Gaudi::XYZPoint& intersect )
   {
     double mu = 0.0;
     return Gaudi::Math::intersection(*hit->hit(), plane, intersect, mu);
