ef4a0d058052f122cf9e75a50efee6c0e4d8b99b
Stephanie Hansmann-Menzemer <menzemer@physi.uni-heidelberg.de>
2007-08-22 15:26:23 +0000

    interface for PatSeedinTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17046 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 62a37b9c27dac3d877f11d5a4d7d476c1817df9b

---
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IPatSeedingTool.h
@@ -0,0 +1,41 @@
+// $Id: IPatSeedingTool.h,v 1.1 2007-08-22 15:26:23 smenzeme Exp $
+#ifndef TRACKINTERFACES_IPATSEEDINGTOOL_H 
+#define TRACKINTERFACES_IPATSEEDINGTOOL_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+#include "Event/Track.h"
+
+static const InterfaceID IID_IPatSeedingTool ( "IPatSeedingTool", 1, 0 );
+
+/** @class IPatSeedingTool IPatSeedingTool.h TrackInterfaces/IPatSeedingTool.h
+ *  Interface to the forward pattern tool
+ *
+ *  @author Olivier Callot
+ *  @date   2005-10-04
+ */
+class IPatSeedingTool : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IPatSeedingTool; }
+
+  virtual StatusCode performTracking(LHCb::Tracks* output ) = 0; 
+
+  virtual StatusCode performTracking(std::vector<LHCb::Track*>& outputTracks ) = 0;
+
+  virtual void prepareHits () = 0;
+
+  virtual void prepareHits(const LHCb::State & aState, const double nSigma) = 0;
+
+protected:
+
+private:
+
+};
+#endif // TRACKINTERFACES_IPATSEEDINGTOOL_H
