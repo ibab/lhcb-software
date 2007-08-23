5ecc8cc97f932fd720ac32c0838110a6a9be348d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-08-23 13:27:26 +0000

    don't need to include Kernel/STChannelID.h twice
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17058 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f5eff2734fc7ccee54a84a0a7c0ff890fbc9cd7a

--- LHCb/Tf/TfKernel/TfKernel/RegionID.h
+++ LHCb/Tf/TfKernel/TfKernel/RegionID.h
@@ -5,7 +5,7 @@
  *  Header file for track find class Tf::RegionID
  *
  *  CVS Log :-
- *  $Id: RegionID.h,v 1.6 2007-08-22 15:24:56 smenzeme Exp $
+ *  $Id: RegionID.h,v 1.7 2007-08-23 13:27:26 jonrob Exp $
  *
  *  @author S. Hansmann-Menzemer, W. Hulsbergen, C. Jones, K. Rinnert
  *  @date   2007-05-30
@@ -19,7 +19,6 @@
 #include "Kernel/OTChannelID.h"
 #include "Kernel/STChannelID.h"
 #include "Kernel/VeloChannelID.h"
-#include "Kernel/STChannelID.h"
 
 namespace Tf
 {
