41b015da6e6235fbe353850e9a5f78d0c8add152
Johan Blouw <j.blouw@cern.ch>
2007-08-28 11:45:21 +0000

    I am stupide; forgot to cvs add the new interface...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88167 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 17c45a6cd8281cd2a9294b8882b323315215e1de

---
+++ Alignment/Alignment/AlignmentInterfaces/AlignmentInterfaces/ICentipede.h
@@ -0,0 +1,47 @@
+#ifndef ALIGNMENTINTERFACES_ICENTIPEDE_H
+#define ALIGNMENTINTERFACES_ICENTIPEDE_H 1
+
+// Include files
+// from Gaudi:
+
+#include "AlignmentInterfaces/IMillepede.h"
+
+/** @class ICentipede ICentipede.h AlignmentInterfaces/ICentipede.h
+ *
+ * @author Johan Blouw, Physikalisches Institut, Uni Heidelberg
+ * @date 2007-08-17
+ *
+ * This is a helper class which enherits from the Millepede class.
+ * It is used to provide the Heidelberg alignment algorithm with
+ * a unified interface to Millepede.
+ */
+
+static const InterfaceID IID_ICentipede ( "ICentipede", 1, 0 );
+
+class ICentipede : virtual public IMillepede {
+ public:
+  // Retrieve Interface ID
+  static const InterfaceID& interfaceID() { return IID_ICentipede;};
+  virtual StatusCode InitMille ( bool DOF[],
+				 int nglo,
+				 int nloc,
+				 double startfact,
+				 int nstd,
+				 double res_cut,
+				 double res_cut_init ) = 0;
+
+  virtual StatusCode EquLoc ( std::vector<double> &dergb,
+			      std::vector<double> &derlc,
+			      const double &rmeas,
+			      double sigma ) = 0;
+  virtual StatusCode ZerLoc( std::vector<double> &dergb,
+			     std::vector<double> &derlc ) = 0;
+
+  virtual StatusCode FitLoc ( int n,
+			      std::vector<double> &track_params,
+			      int single_fit,
+			      std::vector<double> &estimated_para,
+			      double &summ,
+			      double &res ) = 0;
+};
+#endif // ALIGNMENTINTERFACES_ICENTIPEDE_H 
