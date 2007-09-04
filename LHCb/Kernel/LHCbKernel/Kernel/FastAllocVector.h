9184cc8e250d3eeadbcfabe4d1dae66db19c3ff5
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2007-09-04 16:43:15 +0000

    add fast alloc vector class
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47704 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 25e09218185a064cf63ede0b0cba4eb74ddf627f

---
+++ LHCb/Kernel/LHCbKernel/Kernel/FastAllocVector.h
@@ -0,0 +1,91 @@
+
+//--------------------------------------------------------------------------------
+/** @file FastAllocVector.h
+ *
+ *  Header file for vector class LHCb::FastAllocVector
+ *
+ *  CVS Log :-
+ *  $Id: FastAllocVector.h,v 1.1 2007-09-04 16:38:23 jonrob Exp $
+ *
+ *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
+ *  @date   29/03/2007
+ */
+//--------------------------------------------------------------------------------
+
+#ifndef KERNEL_FastAllocVector_H
+#define KERNEL_FastAllocVector_H 1
+
+// Include files
+#include <vector>
+
+// need to use macro as template typedefs don't work yet :(
+
+#ifdef __GNUC__
+#if __GNUC_MINOR__ > 3
+// This is gcc 3.4.X so has the custom allocators
+#include <ext/mt_allocator.h>
+#define LHCb_FastAllocVector_allocator(TYPE) __gnu_cxx::__mt_alloc< TYPE >
+//#include <ext/pool_allocator.h>
+//#define LHCb_FastAllocVector_allocator(TYPE) __gnu_cxx::__pool_alloc< TYPE >
+//#define LHCb_FastAllocVector_allocator(TYPE) std::allocator< TYPE >
+#else
+// This is older than gcc 3.4.X so use standard allocator
+#define LHCb_FastAllocVector_allocator(TYPE) std::allocator< TYPE >
+#endif
+#else
+// Windows
+#define LHCb_FastAllocVector_allocator(TYPE) std::allocator< TYPE >
+#endif
+
+namespace LHCb
+{
+
+  //--------------------------------------------------------------------------------
+  /** @class FastAllocVector FastAllocVector.h Kernel/FastAllocVector.h
+   *
+   *  Vector with fast allocator
+   *
+   *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+   *  @date   29/03/2007
+   */
+  //--------------------------------------------------------------------------------
+
+  template < typename TYPE, typename ALLOC = LHCb_FastAllocVector_allocator(TYPE) >
+  class FastAllocVector : public std::vector<TYPE,ALLOC>
+  {
+
+  public:
+
+    /// Default constructor
+    FastAllocVector( ) { }
+
+    /** Constructor with initial size
+     *  @param size Initialisation size for vector */
+    FastAllocVector( const typename std::vector<TYPE,ALLOC>::size_type size )
+      : std::vector<TYPE,ALLOC> (size) { }
+
+    /** Constructor with initial size and initialisation value
+     *  @param size Initialisation size for vector
+     *  @param init Initialisation value
+     */
+    FastAllocVector( const typename std::vector<TYPE,ALLOC>::size_type size,
+                     const TYPE & init ) : std::vector<TYPE,ALLOC> (size,init) { }
+
+    /// Copy Constructor
+    FastAllocVector( const FastAllocVector & init ) : std::vector<TYPE,ALLOC> (init) { }
+
+    /// Operator overloading for ostream
+    friend inline std::ostream& operator << ( std::ostream& str ,
+                                              const FastAllocVector<TYPE,ALLOC> & v )
+    {
+      str << "[ ";
+      for ( typename FastAllocVector<TYPE,ALLOC>::const_iterator i = v.begin(); i != v.end(); ++i )
+      { str << *i << " "; }
+      return str << "]";
+    }
+
+  };
+
+}
+
+#endif // KERNEL_FastAllocVector_H
