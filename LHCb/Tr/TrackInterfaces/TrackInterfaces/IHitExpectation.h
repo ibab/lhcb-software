fec6286ad3a02b5f9e5acad63ab1b122f6de8851
Matthew David Needham <mneedham@cern.ch>
2007-09-17 08:28:40 +0000

    add missing heaser
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17144 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2761bf9e0ce9e05c468819a28b417dc165b14dc5

---
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IHitExpectation.h
@@ -0,0 +1,61 @@
+// $Id: IHitExpectation.h,v 1.1 2007-09-17 08:28:40 mneedham Exp $ 
+#ifndef _IHitExpectation_H
+#define _IHitExpectation_H
+ 
+#include "GaudiKernel/IAlgTool.h"
+ 
+static const InterfaceID IID_IHitExpectation( "IHitExpectation", 0, 0 );
+ 
+/** @class IHitExpectation IHitExpectation.h TrackInterfaces/IHitExpectation
+ *
+ *  How many hits (of a given type) do we expect on a track ?
+ *
+ *  @author M.Needham
+ *  @date   07/09/2007
+ */
+
+namespace LHCb{ 
+ class Track;
+};
+  
+class IHitExpectation: virtual public IAlgTool{
+ 
+public:
+
+  /** small struct returning hit info....
+  * nExpected --> number of expected hits
+  * number of the expected hits that are found 
+  * likelihood -> what these hits would contribute to likelihood 
+  * (In fact this only matters for OT where eff = function of r)  
+  */ 
+  typedef struct {
+    unsigned int nExpected;
+    unsigned int nFound;
+    double likelihood;
+  } Info;
+      
+                                                                        
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_IHitExpectation ; }
+                                                                               
+  /** Returns number of hits expected, from zFirst to inf
+  *
+  *  @param aTrack Reference to the Track to test
+  *
+  *  @return unsigned int number of hits expected
+  */
+  virtual unsigned int nExpected ( const LHCb::Track& aTrack ) const = 0;
+                                                                           
+
+  /** Returns number of hits expected
+  *
+  *  @param aTrack Reference to the Track to test
+  *
+  *  @return Info info including likelihood
+  */
+  virtual Info expectation( const LHCb::Track& aTrack ) const = 0;
+                                                                                   
+                                                                      
+};
+                                                                               
+#endif
