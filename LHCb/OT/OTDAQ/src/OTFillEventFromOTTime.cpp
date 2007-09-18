ac9dc0688aab4f224287f0d481d46c87282d453b
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-09-18 11:12:03 +0000

    undo version number change
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@58932 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 967ba52b9fd3aa13d6cfafbbf278dbb12607849d

--- LHCb/OT/OTDAQ/src/OTFillEventFromOTTime.cpp
+++ LHCb/OT/OTDAQ/src/OTFillEventFromOTTime.cpp
@@ -1,4 +1,4 @@
-// $Id: OTFillEventFromOTTime.cpp,v 1.4 2007-06-15 15:56:01 janos Exp $
+// $Id: OTFillEventFromOTTime.cpp,v 1.5 2007-09-18 11:12:03 cattanem Exp $
 // Include files
 
 // Gaudi
@@ -107,7 +107,7 @@ StatusCode OTFillEventFromOTTime::execute()
 
     int bankID = iBank->first;
     dataBank& bBank = (*aBank);
-    rawEvent->addBank(bankID , LHCb::RawBank::OT, 2, bBank);
+    rawEvent->addBank(bankID , LHCb::RawBank::OT, 1, bBank);
     aBank->clear();
   }
   
