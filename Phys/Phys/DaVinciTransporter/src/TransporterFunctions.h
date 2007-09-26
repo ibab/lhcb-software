bc4313af5b9603448264d0e477dd8092da2e7fb8
jpalac <jpalac@cern.ch>
2007-09-26 16:25:06 +0000

    Increase version to v3r7p1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63831 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bba8ba4381fdf2883f2d0fc22b4863a05a80f9ee

--- Phys/Phys/DaVinciTransporter/src/TransporterFunctions.h
+++ Phys/Phys/DaVinciTransporter/src/TransporterFunctions.h
@@ -1,4 +1,4 @@
-// $Id: TransporterFunctions.h,v 1.7 2007-09-26 13:48:27 jpalac Exp $
+// $Id: TransporterFunctions.h,v 1.8 2007-09-26 16:25:06 jpalac Exp $
 #ifndef TRANSPORTERFUNCTIONS_H 
 #define TRANSPORTERFUNCTIONS_H 1
 
@@ -22,38 +22,46 @@ namespace DaVinciTransporter {
    *  extrapolation.
    *  The transformation on the state vector, v_0 -> v_1, is
    *  @code
-   *  x_1  = x_0 + dZ*pX_0/pZ_0
-   *  y_1  = y_0 + dZ*pY_0/pZ_0
-   *  z_1  = z_0 + dZ
+   *  x_1  = x_0 + (z_1-z_0)*pX_0/pZ_0
+   *  y_1  = y_0 + (z_1-z_0)*pY_0/pZ_0
+   *  z_1  = z_1
    *  pX_1 = pX_0
    *  pY_1 = pY_0
    *  pZ_1 = pZ_0
-   *  E_1  = E_old
+   *  E_1  = E_0
    *  @endcode
    *
-   *  The transport matrix D is [dv_1/dv_0], or
+   *  The transport matrix D is [dv_1/dv_0]. Here dz = ( z_1-z_0 )
    *  @code  
-   *   1  0  0 dz/Pz   0    -dz*Px/Pz/Pz  0
-   *   0  1  0   0   dz/Pz  -dz*Py/Pz/Pz  0
-   *   0  0  1   0     0          0       0
-   *   0  0  0   1     0          0       0
-   *   0  0  0   0     1          0       0
-   *   0  0  0   0     0          1       0
-   *   0  0  0   0     0          0       1
+   *   1  0 -Px/Pz dz/Pz   0    -dz*Px/Pz/Pz  0
+   *   0  1 -Py/Pz   0   dz/Pz  -dz*Py/Pz/Pz  0
+   *   0  0    0     0     0          0       0
+   *   0  0    0     1     0          0       0
+   *   0  0    0     0     1          0       0
+   *   0  0    0     0     0          1       0
+   *   0  0    0     0     0          0       1
    *  @endcode
    *
    *  Or, in block structure,
    *  @code
-   *   I(3x3)   Dxp(3x4)
+   *   A(3x3)  Dxp(3x4)  
    *   0(4x3)    I(4x4)
    *  @endcode
    *
    *  Where Dxp is
    *   
    *   @code      
-   *         /  1    0 -Px/Pz  0  \
-   *  1/Pz * |  0    1 -Py/Pz  0  |
-   *         \  0    0    0    0  /
+   *
+   *   dz/Pz * /  1  0  -dz*Px/Pz  0  \
+   *           |  0  1  -dz*Py/Pz  0  |
+   *           \  0  0       0     0  /
+   *
+   *  @endcode
+   *  A is
+   *  @code
+   *  / 1 0 -Px/Pz \
+   *  | 0 1 -Py/Pz |
+   *  \ 0 0    0   /
    *  @endcode
    * The transformation on the Particle's 7x7 covariance matrix C_0 is given by
    *
@@ -61,15 +69,15 @@ namespace DaVinciTransporter {
    * 
    * or, in block form,
    *  @code
-   *  /  I  Dxp  \   /  Cx_0  Cpx_0^T  \   /  I      0  \    
+   *  /  A  Dxp  \   /  Cx_0  Cpx_0^T  \   /  A^T    0  \    
    *  |          | * |                 | * |            |
    *  \  0   I   /   \  Cpx_0   Cp_0   /   \  Dxp^T  I  /
    *  @endcode
    * 
-   *  resulting in the expressions
+   *  This results in the expressions
    *  @code
-   *  Cx_1  = Cx_0 + Cpx_0^T*Dxp^T + Dxp*Cpx_0 + Dxp*Cp_0*Dxp^T
-   *  Cpx_1 = Cpx_0 + Cp*Dxp^T
+   *  Cx_1  = A*Cx_0*A^T + A*Cpx_0^T*Dxp^T + Dxp*Cpx_0*A^T + Dxp*Cp_0*Dxp^T
+   *  Cpx_1 = Cpx_0*A^T + Cp_0*Dxp^T
    *  Cp_1  = Cp_0 
    *  @endcode
    *
@@ -85,17 +93,28 @@ namespace DaVinciTransporter {
     using namespace Gaudi;
     
     const double dZ = zNew-particle->referencePoint().Z();
+    const double Px = particle->momentum().X();
+    const double Py = particle->momentum().Y();
     const double Pz = particle->momentum().Z();
-    const double coef = 1./Pz;
+    const double coef = dZ/Pz;
 
     // Transport matrix position-momentum block
     Matrix3x4 Dxp;
     Dxp(0,0) = coef;
-    Dxp(0,2) = -1*coef*particle->momentum().X()/Pz;
+    Dxp(0,2) = -1*coef*Px/Pz;
     Dxp(1,1) = coef;
-    Dxp(1,2) = -1*coef*particle->momentum().Y()/Pz;
+    Dxp(1,2) = -1*coef*Py/Pz;
 
     const Matrix4x3 DxpT = ROOT::Math::Transpose(Dxp);
+
+    // Transport matrix position block
+    Matrix3x3 A;
+    A(0,0) = 1.;
+    A(1,1) = 1.;
+    A(0,2) = -1.*Px/Pz;
+    A(1,2) = -1.*Py/Pz;
+    
+    const Matrix3x3 AT = ROOT::Math::Transpose(A);
 
     // Old covariance matrix blocks
     const SymMatrix3x3  Cx_0  = particle->posCovMatrix();
@@ -105,11 +124,11 @@ namespace DaVinciTransporter {
 
     // New covariance matrix blocks
     const Matrix3x3 Cx_1  = 
-      Cx_0 + Cxp_0*DxpT +
-      Matrix3x3(Dxp*Cpx_0) + 
+      A*Cx_0*AT + Matrix3x3(A*Cxp_0*DxpT) +
+      Matrix3x3(Dxp*Cpx_0*AT) + 
       Matrix3x3( Dxp*Cp_0*DxpT );
 
-    const Matrix4x3 Cpx_1 = Cpx_0 + Cp_0*DxpT;
+    const Matrix4x3 Cpx_1 = Cpx_0*AT + Matrix4x3(Cp_0*DxpT);
 
     // Update covariance matrices in particle
     transParticle.setPosCovMatrix( Cx_1.LowerBlock() );
