a9616b2add6947bd7273284350ac6d9fc13c213d
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-09-29 14:03:43 +0000

    v8r0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@88205 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 295183bdc5ce185a7734b95b1db732c4ccf3e60f

---
+++ Bender/Phys/BenderUser/template/TEMPLATE.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python2.4
+# =============================================================================
+"""
+This a template file for the Bender-based scriopt/module
+"""
+# =============================================================================
+## @file
+#  This a template file for the Bender-based scriopt/module
+#  @author ...
+#  @date   ...
+# =============================================================================
+__author__ = " Do not forget your name here "
+# =============================================================================
+
+
+# =============================================================================
+## import all nesessary stuff from Bender
+from Bender.MainMC import * 
+# =============================================================================
+
+
+# =============================================================================
+## @class Template
+class Tempate(AlgoMC) :
+    """
+    This is the template algorithm 
+    """
+        
+    ## standard constructor
+    def __init__ ( self , name = 'Template' ) :
+        """
+        Standard constructor
+        """ 
+        return AlgoMC.__init__ ( self , name )
+
+    
+   ## standard mehtod for analyses
+    def analyse( self ) :
+        """
+        Standard method for analysis
+        """
+
+        return SUCCESS
+
+# =============================================================================
+## job configuration:
+def configure ( **args ) :
+    """
+    Configure the job
+    """
+    
+    ## get the input data
+    import data_Bs2Jpsiphi_mm as input 
+    
+
+    ## read external configruation files
+    gaudi.config (
+        files = [
+        '$DAVINCIROOT/options/DaVinciCommon.opts'         ,
+        '$COMMONPARTICLESROOT/options/StandardKaons.opts' ] )
+
+    #
+    # perform some specific configuration 
+    #
+
+    return SUCCESS
+
+# =============================================================================
+## job steering 
+if __name__ == '__main__' :
+
+    ## make printout of the own documentations 
+    print __doc__
+    
+    ## configure the job:
+    configure()
+
+    ## run the job
+    gaudi.run(1000)
+
+# =============================================================================
+# The END 
+# =============================================================================
