497c2a6d2bf12947d31df4899c494eab138046e2
Markus Frank <markus.frank@cern.ch>
2007-10-02 16:20:12 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97114 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dda9f1946a70af70e58503d28acd3d2414d56b4a

--- Online/Online/OnlineControls/pvss/scripts/InstallMonitoring2.cpp
+++ Online/Online/OnlineControls/pvss/scripts/InstallMonitoring2.cpp
@@ -3,9 +3,15 @@
 #uses "StreamControlPanel.cpp"
 main()   {
   string stream = "Monitoring";
+  string ctrl_node = "mona08";
+  StreamTaskMgr_stopAllTree(stream);
   StreamTaskMgr_installStream(stream);
-  StreamTaskMgr_createTree(stream,"Slice00",5,40,5,15,"STORAGE:Storage",10);  
+  StreamTaskMgr_createAllTree(stream,5,40,5,15,"STORAGE:Storage",10);  
+  StreamTaskMgr_connectTaskManager(stream);
   // Finally generate all FSMs
-  //StreamTaskMgr_genAllFSM(stream);
+  StreamTaskMgr_genAllFSM(stream);
+  StreamTaskMgr_startAllTree(stream);
+  fwInstallation_addManager("PVSS00dim","always", 30, 3, 3, "-num 40 -dim_dp_config DimStorage -dim_dns_node "+ctrl_node);
+  JobOptions_installManager(59,"PVSS00Monitoring","../python/PVSS00Monitoring.py");
   exit(0);
 }
