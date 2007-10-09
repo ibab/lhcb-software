1ab519af8fc6be5c75ef1a956c65724eb534b883
Stephanie Hansmann-Menzemer <menzemer@physi.uni-heidelberg.de>
2007-10-09 18:41:19 +0000

    move Tf/PatChecker to Tr/PatChecker
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@17272 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 24e5f9d00d1cb5dc18ec4d513dabbaddf9e62864

---
+++ Rec/Tr/PatChecker/src/PatCheckerNTuple.h
@@ -0,0 +1,38 @@
+// $Id: PatCheckerNTuple.h,v 1.1.1.1 2007-10-09 18:41:19 smenzeme Exp $
+#ifndef PATCHECKERNTUPLE_H 
+#define PATCHECKERNTUPLE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTupleAlg.h"
+
+
+/** @class PatCheckerNTuple PatCheckerNTuple.h
+ *  
+ *   Algorithm to produce an NTuple to check Pat tracks.
+ *  @author Olivier Callot
+ *  @date   2006-05-11
+ *  @update for A-Team framework 2007-08-20
+ */
+
+
+  class PatCheckerNTuple : public GaudiTupleAlg {
+  public: 
+    /// Standard constructor
+    PatCheckerNTuple( const std::string& name, ISvcLocator* pSvcLocator );
+    
+    virtual ~PatCheckerNTuple( ); ///< Destructor
+    
+    virtual StatusCode initialize();    ///< Algorithm initialization
+    virtual StatusCode execute   ();    ///< Algorithm execution
+    virtual StatusCode finalize  ();    ///< Algorithm finalization
+    
+  protected:
+    
+  private:
+    std::string m_tupleName;
+    std::string m_container;
+    
+  };
+
+#endif // PATCHECKERNTUPLE_H
---
+++ Rec/Tr/PatChecker/src/PatLHCbID2MCParticle.h
@@ -0,0 +1,64 @@
+// $Id: PatLHCbID2MCParticle.h,v 1.1.1.1 2007-10-09 18:41:19 smenzeme Exp $
+#ifndef PATLHCBID2MCPARTICLE_H 
+#define PATLHCBID2MCPARTICLE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+// from LinkerEvent
+#include "Linker/LinkerWithKey.h"
+#include "Linker/LinkedTo.h"
+
+#include "GaudiKernel/ToolHandle.h"
+#include "TfKernel/IOTHitCreator.h"
+#include "TfKernel/IITHitCreator.h"
+#include "TfKernel/ITTHitCreator.h"
+ 
+
+/** @class PatLHCbID2MCParticle PatLHCbID2MCParticle.h
+ *  Build the linker table relating LHCbID used in pattern to MCParticles
+ *
+ *  @author Olivier Callot
+ *  @date   2005-06-01
+ *  @update for A-Team framework 2007-08-20 SHM
+ */
+
+  class PatLHCbID2MCParticle : public GaudiAlgorithm {
+  public: 
+    /// Standard constructor
+    PatLHCbID2MCParticle( const std::string& name, ISvcLocator* pSvcLocator );
+    
+    virtual ~PatLHCbID2MCParticle( ); ///< Destructor
+    
+    virtual StatusCode initialize();    ///< Algorithm initialization
+    virtual StatusCode execute   ();    ///< Algorithm execution
+    virtual StatusCode finalize  ();    ///< Algorithm finalization
+    
+  protected:
+    
+    void addToList( const LHCb::MCParticle* part )  {
+      bool found = false;
+      for ( std::vector<const LHCb::MCParticle*>::iterator itP = 
+	      m_partList.begin();  m_partList.end() != itP; itP++ ) {
+	if ( part == (*itP) ) {
+	  found = true;
+	  break;
+	}
+      }
+      if ( !found ) m_partList.push_back( part );
+    }  
+    
+  private:
+    bool        m_level1;
+    std::string m_targetName;
+    std::vector<const LHCb::MCParticle*> m_partList;
+    ToolHandle<Tf::IOTHitCreator> m_othitcreator;
+    ToolHandle<Tf::IITHitCreator> m_ithitcreator;
+    ToolHandle<Tf::ITTHitCreator> m_tthitcreator;
+    bool m_linkOT;
+    bool m_linkIT;
+    bool m_linkTT;
+    bool m_linkVELO;
+  };
+#endif // PATLHCBID2MCPARTICLE_H
