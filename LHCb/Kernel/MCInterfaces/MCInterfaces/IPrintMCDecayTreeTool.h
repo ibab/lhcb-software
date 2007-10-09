a8f32bbdf8d3ddc394604ef3dd38d4153dbdb166
jpalac <jpalac@cern.ch>
2007-10-09 15:25:13 +0000

    First commit of IPrintMCDecayTreeTool, containing the MC part of the old IDebugTool.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47726 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9733d0796cdb7bd996b33b23b8cd0f3517e3070a

---
+++ LHCb/Kernel/MCInterfaces/MCInterfaces/IPrintMCDecayTreeTool.h
@@ -0,0 +1,59 @@
+// $Id:
+#ifndef MCINTERFACES_IMCPRINTDECAYTREETOOL_H
+#define MCINTERFACES_IMCPRINTDECAYTREETOOL_H 1
+
+// Include files
+// from STL
+#include <vector>
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+#include "Event/MCParticle.h"
+
+// Declaration of the interface ID (interface id, major version, minor version)
+static const InterfaceID IID_IPrintMCDecayTreeTool("IPrintMCDecayTreeTool", 1, 0);
+
+/** @class IPrintMCDecayTreeTool IPrintMCDecayTreeTool.h DaVinciMCTools/IPrintMCDecayTreeTool.h
+ *  
+ *
+ *  Interface for the printing of decay trees of MCParticles.
+ *  Taken from the original IDebugTool from Olivier Dormond.
+ *  
+ *  @todo figure out exactly what each method is supposed to do 
+ *        and clarify the doxygen!!!!
+ *  @author Juan Palacios juancho@nikhef.nl
+ *  @date   09/10/2007
+ */
+class IPrintMCDecayTreeTool : virtual public IAlgTool {
+public:
+
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_IPrintMCDecayTreeTool; }
+
+  /// Print decay tree for a given MC particle up to a depth maxDepth.
+  /// Default value of -1 is what exactly???
+  virtual void printTree( const LHCb::MCParticle* mother, 
+                          int maxDepth = -1 ) = 0;
+
+  /// Print all the MC particles leading to this one.
+  virtual void printAncestor( const LHCb::MCParticle* child ) = 0;
+
+  /// Print all the MCParticles in a vector as a tree.
+  /// Top level particles are those without parent.
+  virtual void printEventAsTree( const LHCb::MCParticle::ConstVector& event ) = 0;
+  /// Print all the MCParticles in a keyed container as a tree.
+  /// Top level particles are those without parent.
+  virtual void printEventAsTree( const LHCb::MCParticles& event ) = 0;
+
+  /// Print all the MCParticles in a vector as a flat list.
+  /// Top level particles are those without parent.
+  virtual void printEventAsList( const LHCb::MCParticle::ConstVector& event ) = 0;
+
+  /// Print all the MCParticles in a keyed container as a flat list.
+  /// Top level particles are those without parent.
+  virtual void printEventAsList( const LHCb::MCParticles& event ) = 0;
+
+};
+#endif // MCINTERFACES_IPRINTDECAYTREETOOL_H
