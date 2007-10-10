3f57f8216fff29509ad75750df3cbef9a8df333a
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-10-10 15:43:27 +0000

    for running from python
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@59032 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 74e9f04ad0e55fb8f97fe8a625e8ff848963cc60

---
+++ Boole/Digi/Boole/options/v200601.py
@@ -0,0 +1,11 @@
+# Minimal file for running Boole from python prompt
+# Syntax is:
+#   gaudirun.py ../options/v200601.py
+#
+from Gaudi.Configuration import *
+
+#--Setup for v200601 geometry
+importOptions('../options/v200601.opts')
+
+ApplicationMgr( AppName    = 'Boole',
+                AppVersion = 'v14r6' )
