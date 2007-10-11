9c68ee21795450669843202f02c85ed612177d5d
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-10-11 11:49:18 +0000

    to execute from Python
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@29918 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9278b9ec4d473fc93c24c165ac7aedcd691338e9

---
+++ Brunel/Rec/Brunel/options/v200601.py
@@ -0,0 +1,11 @@
+# Minimal file for running Brunel from python prompt
+# Syntax is:
+#   gaudirun.py ../options/v200601.py
+#
+from Gaudi.Configuration import *
+
+#--Setup for v200601 geometry
+importOptions('$BRUNELOPTS/v200601.opts')
+
+ApplicationMgr( AppName    = 'Brunel',
+                AppVersion = 'v31r10' )
