43c52c5ecf6afe448425cd2fda5e633354751af0
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-10-19 15:39:03 +0000

    add custom dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36011 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8c2941edc0477bc52e980f054dbdc25bab327a97

---
+++ LHCb/Event/TrackEvent/dict/lcgDict.h
@@ -0,0 +1,20 @@
+// $Id: lcgDict.h,v 1.1 2007-10-19 15:39:03 cattanem Exp $
+#ifndef TRACKEVENT_LCGDICT_H 
+#define TRACKEVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "Event/Track.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::vector<const LHCb::Track*> _i1;
+    // end instantiations
+  };
+}
+
+#endif // TRACKEVENT_LCGDICT_H
