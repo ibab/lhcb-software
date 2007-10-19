e9d5c107b5e6f322988bb250f5fa29396756423d
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-10-19 15:47:20 +0000

    add custom dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36012 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f609566d6af2bc2a622977716b40223c8e1f7cb9

---
+++ LHCb/Event/RecEvent/dict/lcgDict.h
@@ -0,0 +1,20 @@
+// $Id: lcgDict.h,v 1.1 2007-10-19 15:47:20 cattanem Exp $
+#ifndef RECEVENT_LCGDICT_H 
+#define RECEVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "Event/RecVertex.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::vector<const LHCb::RecVertex*> _i1;
+    // end instantiations
+  };
+}
+
+#endif // RECEVENT_LCGDICT_H
