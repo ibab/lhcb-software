2717dbb196e2cf52a5ac85360d7a13cb0dffdd76
psomogyi <psomogyi@cern.ch>
2007-10-22 11:42:11 +0000

    + bash script
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97157 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 88d2ac450fab8fee2dc272548b011524df5314b6

---
+++ Online/Online/Presenter/test/startTest.sh
@@ -0,0 +1,19 @@
+#Clean old processes
+#killall Gaudi.exe
+killall Ctest.exe
+killall DNS.exe
+
+#Setup environment variables
+export MALLOC_CHECK_=2
+export DIM_DNS_NODE=localhost
+echo "DIM_DNS_NODE set to " $DIM_DNS_NODE
+
+#export LHCBHOME /afs/cern.ch/lhcb
+#source $LHCBHOME/scripts/CMT.csh
+
+#exportOnline v3r2
+
+#source /afs/cern.ch/lhcb/software/releases/ONLINE/ONLINE_v2r2/OnlineSys/v2r2/cmt/setup.csh
+. $LHCb_release_area/ONLINE/ONLINE_v4r0/OnlineSys/v4r0/cmt/setup.sh
+$DIMROOT/$CMTCONFIG/DNS.exe&
+$CCPCHISTROOT/$CMTCONFIG/Ctest.exe&
