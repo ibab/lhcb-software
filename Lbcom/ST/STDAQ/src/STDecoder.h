65546ee911ddc27c80c29db9f29910da4c2cbebd
Matthew David Needham <mneedham@cern.ch>
2007-10-23 08:01:24 +0000

    bug fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@26776 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 31800a3016138c046ed7ac474f69d9a74e9f7c02

--- Lbcom/ST/STDAQ/src/STDecoder.h
+++ Lbcom/ST/STDAQ/src/STDecoder.h
@@ -1,6 +1,6 @@
 #ifndef _STDecoder_H 
 #define _STDecoder_H 
 #include "SiDAQ/SiRawBankDecoder.h"
-#include "SiDAQ/SiHeaderWord.h"
+#include "STClusterWord.h"
 typedef SiRawBankDecoder<STClusterWord > STDecoder;
 #endif
