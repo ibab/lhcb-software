2e4fd4ab5a390b139dd40d3cbe708726f28b7e7c
jpalac <jpalac@cern.ch>
2007-10-26 12:34:55 +0000

    - Add options/DaVinci.py, a minimal python script to run DaVinci    from the python prompt.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@70780 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c6345a624421eb45c272f1745cc3fa475dbe8c4a

---
+++ DaVinci/Phys/DaVinci/options/DaVinci.py
@@ -0,0 +1,10 @@
+# Minimal file for running DaVinci from python prompt
+# Syntax is:
+#   gaudirun.py ../options/DaVinci.py
+#
+from Gaudi.Configuration import *
+
+importOptions('../options/DaVinci.opts')
+
+ApplicationMgr( AppName    = 'DaVinci',
+                AppVersion = 'v19r6' )
