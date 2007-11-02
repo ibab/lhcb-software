c86c2f40d47a2f2786a58696ca824bb9e1ed399f
Johannes Albrecht <johannes.albrecht@cern.ch>
2007-11-02 14:59:36 +0000

    do the seed-track conversion in tool, algorithm just calls teh tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@17380 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d46f95afddc7f7ac3ee6dc78be0991d043a8e13c

--- Rec/Tf/TsaAlgorithms/src/TsaSeedTrackCnv.cpp
+++ Rec/Tf/TsaAlgorithms/src/TsaSeedTrackCnv.cpp
@@ -1,4 +1,4 @@
-// $Id: TsaSeedTrackCnv.cpp,v 1.3 2007-09-27 14:29:44 mneedham Exp $
+// $Id: TsaSeedTrackCnv.cpp,v 1.4 2007-11-02 14:59:33 albrecht Exp $
 //
 // This File contains the implementation of the TsaEff
 // C++ code for 'LHCb Tracking package(s)'
@@ -6,7 +6,6 @@
 
 // Gaudi
 #include "GaudiKernel/AlgFactory.h"
-#include "GaudiKernel/SystemOfUnits.h"
 
 #include "TsaSeedTrackCnv.h"
 
@@ -19,25 +18,7 @@ SeedTrackCnv::SeedTrackCnv(const std::string& name,
   BaseAlg(name, pSvcLocator)
 {
   // constructor
-  /*
-    declareProperty("eX2",m_EX2 = 40*Gaudi::Units::mm2);
-    declareProperty("eY2",m_EY2 = 40*Gaudi::Units::mm2);
-    declareProperty("eTX2",m_ETx2 = 1e-4);
-    declareProperty("eTY2",m_ETy2 = 1.0e-4); // 2e-7;
-    declareProperty("eQdivP2",m_EQdivP2 = 0.1);
-  */
-
-  declareProperty("eX2",m_EX2 = 31*Gaudi::Units::mm2);
-  declareProperty("eY2",m_EY2 = 12*Gaudi::Units::mm2);
-  declareProperty("eTX2",m_ETx2 = 2.0e-3);
-  declareProperty("eTY2",m_ETy2 = 2.0e-3); // 2e-7;
-  declareProperty("eQdivP2",m_EQdivP2 = 0.01);
-
   declareProperty("LikCut", m_likCut = -30.);
-  declareProperty("curvatureFactor", m_curvFactor = 42.0/Gaudi::Units::MeV);
-  declareProperty("pFromCurvature", m_pFromCurvature = false);
-  declareProperty("increaseErrors", m_largeErrors = true);
-
   declareProperty("inputLocation", m_inputLocation = SeedTrackLocation::Default);
   declareProperty("outputLocation", m_outputLocation = LHCb::TrackLocation::Tsa );
 }
@@ -52,7 +33,7 @@ StatusCode SeedTrackCnv::initialize()
     return Error("Failed to initialize");
   }
 
-  m_ptKickTool = tool<ITrackPtKick>("TrackPtKick");
+  m_Tsa2TrackCnv = tool<ITsaSeedTrackCnvTool>("Tf::Tsa::SeedTrackCnvTool");
 
   return sc;
 }
@@ -78,7 +59,8 @@ StatusCode SeedTrackCnv::execute()
               << " " << (*iterTrack)->lik() << endreq;
     if ( ((*iterTrack)->select() != 0) && ((*iterTrack)->lik() > m_likCut) )
     {
-      LHCb::Track* fitTrack = this->convert(*iterTrack);
+      //LHCb::Track* fitTrack = this->convert(*iterTrack);
+      LHCb::Track* fitTrack = m_Tsa2TrackCnv->convert(*iterTrack);
       output->insert(fitTrack, (*iterTrack)->key());
     }
   }
@@ -87,89 +69,4 @@ StatusCode SeedTrackCnv::execute()
 
   return StatusCode::SUCCESS;
 }
-
-
-LHCb::Track* SeedTrackCnv::convert(const SeedTrack* aTrack) const{
-
-  // make a fit track...
-  LHCb::Track* fitTrack = new LHCb::Track();
-
-
-  std::vector<SeedPnt> pnts = aTrack->usedPnts();
-  //std::cout << " starting a track " << pnts.size() << std::endl;
-  for ( std::vector<SeedPnt>::const_iterator itP = pnts.begin(); pnts.end() != itP; ++itP ) {
-    fitTrack->addToLhcbIDs((*itP).hit()->lhcbID());
-    //std::cout << (*itP).hit()->ITChan() << std::endl;
-  } //it
-
-  addState(aTrack, fitTrack, pnts.back().z());
-
-  // add history
-  fitTrack->setHistory(LHCb::Track::TsaTrack);
-  fitTrack->setType(LHCb::Track::Ttrack);
-  fitTrack ->setPatRecStatus( LHCb::Track::PatRecIDs );
-  fitTrack->addInfo(LHCb::Track::TsaLikelihood, aTrack->lik());
-
-  return fitTrack;
-}
-
-void SeedTrackCnv::addState(const SeedTrack* aTrack, LHCb::Track* lTrack, const double z) const{
-
-  // first  state info
-  Gaudi::TrackVector stateVec = Gaudi::TrackVector();
-  Gaudi::TrackSymMatrix stateCov = Gaudi::TrackSymMatrix();
-
-  // initial errors - either large or from seed
-  if (m_largeErrors == true){
-    stateCov(0,0) = m_EX2;
-    stateCov(1,1) = m_EY2;
-    stateCov(2,2) = m_ETx2;
-    stateCov(3,3) = m_ETy2;
-  }
-  else {
-    // x
-    stateCov(0,0) = aTrack->xErr(0);
-    stateCov(2,2) = aTrack->xErr(3);
-    stateCov(0,1) = aTrack->xErr(1);
-    if (m_pFromCurvature == true){
-      stateCov(0,4) = aTrack->xErr(2)*m_curvFactor;
-      stateCov(2,4) = aTrack->xErr(4)*m_curvFactor;
-    }
-    // y
-    stateCov(1,1) = aTrack->yErr(0);
-    stateCov(3,3) = aTrack->yErr(2);
-    stateCov(1,3) = aTrack->yErr(1);
-  }
-
-
-  LHCb::State aState = LHCb::State(stateVec,stateCov,z,LHCb::State::AtT);
-
-  // state all parameters but p
-  aState.setX(aTrack->x(z,0.));
-  aState.setY(aTrack->y(z,0.));
-  aState.setTx(aTrack->xSlope(z,0.));
-  aState.setTy(aTrack->sy());
-
-  // p estimate can come from the curvature or the pt kick
-  if (m_pFromCurvature) {
-    aState.setQOverP(estimateCurvature(aTrack->tx(), m_curvFactor));
-    if (m_largeErrors) {
-      aState.setErrQOverP2( m_EQdivP2*gsl_pow_2(stateVec(4)));
-    }
-    else {
-      aState.setErrQOverP2(aTrack->xErr(5)*gsl_pow_2(m_curvFactor));
-    }
-  }
-  else {
-    StatusCode sc = m_ptKickTool->calculate(&aState);
-    if( sc.isFailure() ) {
-      Warning( "Pt Kick tool failed, but still adding State" );
-    }
-  }
-
-  // add to states
-  lTrack->addToStates(aState);
-
-}
-
 
--- Rec/Tf/TsaAlgorithms/src/TsaSeedTrackCnv.h
+++ Rec/Tf/TsaAlgorithms/src/TsaSeedTrackCnv.h
@@ -1,4 +1,4 @@
-// $Id: TsaSeedTrackCnv.h,v 1.3 2007-10-10 18:48:10 smenzeme Exp $
+// $Id: TsaSeedTrackCnv.h,v 1.4 2007-11-02 14:59:36 albrecht Exp $
 #ifndef _TsaSeedTrackCnv_H_
 #define _TsaSeedTrackCnv_H_
 
@@ -14,6 +14,7 @@
 #include "TsaKernel/SeedPnt.h"
 #include "TsaKernel/TsaConstants.h"
 #include "TsaKernel/TsaFun.h"
+#include "TsaKernel/ITsaSeedTrackCnvTool.h"
 
 //  Event
 #include "Event/State.h"
@@ -29,7 +30,7 @@ namespace Tf
 
     /** @class SeedTrackCnv TsaSeedTrackCnv.h
      *
-     *  Converter for Tsa TYracks to standard LHCb Tracks
+     *  Converter for Tsa Tracks to standard LHCb Tracks
      *
      *  @author M.Needham
      *  @date   30/04/2004
@@ -51,25 +52,11 @@ namespace Tf
 
     private:
 
-      LHCb::Track* convert(const SeedTrack* aTrack) const;
-      void addState(const SeedTrack* aTrack,
-                    LHCb::Track* lTrack, const double z) const;
-
-      ITrackPtKick* m_ptKickTool;
-
-      double m_EX2;
-      double m_EY2;
-      double m_ETx2;
-      double m_ETy2;
-      double m_EQdivP2;
-
       double m_likCut;
-      double m_curvFactor;
-      bool m_pFromCurvature;
-      bool m_largeErrors;
-
       std::string m_inputLocation;
       std::string m_outputLocation;
+
+      Tf::Tsa::ITsaSeedTrackCnvTool* m_Tsa2TrackCnv;
 
     };
 
