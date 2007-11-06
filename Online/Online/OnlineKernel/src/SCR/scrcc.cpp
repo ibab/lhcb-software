4418f873d17957358c04ac26cb13f7d3ba726921
Markus Frank <frankb@cern.ch>
2007-11-06 17:08:10 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97226 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9ecc71073f638f1561ebce15cdb974628eb4cd3

---
+++ Online/Online/OnlineKernel/src/SCR/scrcc.cpp
@@ -0,0 +1,14 @@
+#include "SCR/scrcc.h"
+using namespace SCR;
+
+//---------------------------------------------------------------------------
+SCR::PasteboardContext scrcc_create_pasteboard(const char* dev) {
+  PasteboardContext pb;
+  scrc_create_pasteboard(&pb.pb,dev,&pb.rows,&pb.cols);
+  return pb;
+}
+SCR::Display* scrcc_create_display(int rows, int cols, int attr, flag border,const char* title) {
+  Display* d = 0;
+  scrc_create_display(&d,rows,cols,attr,border,title);
+  return d;
+}
