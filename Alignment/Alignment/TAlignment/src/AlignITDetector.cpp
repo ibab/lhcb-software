b29188aac8f44ec61c77d564223e861c91b6cef1
lnicolas <lnicolas@cern.ch>
2007-11-06 14:44:59 +0000

    bug fix in code for boxes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88245 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bb9246773765f12d562816ff56cdd47d3a438751

--- Alignment/Alignment/TAlignment/src/AlignITDetector.cpp
+++ Alignment/Alignment/TAlignment/src/AlignITDetector.cpp
@@ -1,4 +1,4 @@
-// $Id: AlignITDetector.cpp,v 1.1 2007-10-15 16:24:49 lnicolas Exp $
+// $Id: AlignITDetector.cpp,v 1.2 2007-11-06 14:44:59 lnicolas Exp $
 // Include files 
 
 // from Gaudi
@@ -41,99 +41,100 @@ DECLARE_TOOL_FACTORY( AlignITDetector );
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-AlignITDetector::AlignITDetector( const std::string& type,
-                                  const std::string& name,
-                                  const IInterface* parent )
+AlignITDetector::AlignITDetector ( const std::string& type,
+				   const std::string& name,
+				   const IInterface* parent )
   : GaudiTool ( type, name , parent ),
-    m_constraints(0)
+    m_constraints ( 0 )
 {
-  declareInterface<IAlignDetectorSelector>(this);
-  declareProperty("Align", m_align="Stations");
-  m_fixed = boost::assign::list_of(false)(false)(false);
-  declareProperty("Fixed", m_fixed);
-  declareProperty("Constraints", m_constraints);
+  declareInterface<IAlignDetectorSelector>( this );
+  declareProperty( "Align", m_align = "Stations" );
+  m_fixed = boost::assign::list_of( false )( false )( false );
+  declareProperty( "Fixed", m_fixed );
+  declareProperty( "Constraints", m_constraints );
 }
 
 //=============================================================================
 // Destructor
 //=============================================================================
-AlignITDetector::~AlignITDetector() {} 
+AlignITDetector::~AlignITDetector ( ) { } 
 
-StatusCode AlignITDetector::initialize() 
-{
-  StatusCode sc = GaudiTool::initialize();
-  if ( sc.isFailure() ) return Error( "Failed to initialize!", sc );
+StatusCode AlignITDetector::initialize ( ) {
+
+  StatusCode sc = GaudiTool::initialize ( );
+  if ( sc.isFailure( ) ) return Error ( "Failed to initialize!", sc );
   
-  DeITDetector* tracker = getDet<DeITDetector>(DeSTDetLocation::IT);
-  if (!tracker) return Error("Failed to retrieve OT detector element", StatusCode::FAILURE);
+  DeITDetector* tracker = getDet<DeITDetector>( DeSTDetLocation::IT );
+  if ( !tracker ) return Error( "Failed to retrieve IT detector element", StatusCode::FAILURE );
   
-  if (m_align == "Stations") {
-    const DeITDetector::Stations& stations = tracker->stations();
-    m_indexMap = createIndices(stations.begin()                               , 
-                               stations.end()                                 , 
-                               m_fixed                                        ,
-                               bind<STChannelID>(&DeITStation::elementID, _1) , 
-                               bind<unsigned>(&STChannelID::station, _1)      , 
-                               m_elements                                     );
-  } else if (m_align == "Layers") {
-    const DeITDetector::Layers& layers = tracker->layers();
-    m_indexMap = createIndices(layers.begin()                                , 
-                               layers.end()                                  , 
-                               m_fixed                                       ,
-                               bind<STChannelID>(&DeITLayer::elementID, _1)  , 
-                               bind<unsigned>(&STChannelID::uniqueLayer, _1) , 
-                               m_elements                                    );
-  } else if (m_align == "Boxes") {
-    const DeITDetector::Boxes& boxes = tracker->boxes();
-    m_indexMap = createIndices(boxes.begin()                                , 
-                               boxes.end()                                  , 
-                               m_fixed                                       ,
-                               bind<STChannelID>(&DeITSector::elementID, _1)  , 
-                               bind<unsigned>(&STChannelID::uniqueDetRegion, _1) , 
-                               m_elements                                    );
-  } else if (m_align == "Sectors") {
-    const DeITDetector::Sectors& sectors = tracker->sectors();
-    m_indexMap = createIndices(sectors.begin()                                , 
-                               sectors.end()                                  , 
-                               m_fixed                                       ,
-                               bind<STChannelID>(&DeITSector::elementID, _1)  , 
-                               bind<unsigned>(&STChannelID::uniqueSector, _1) , 
-                               m_elements                                    );
-  } else return Error("Unknown elements " + m_align 
-                      + ". You can only align Stations, Layers, Boxes or Sectors", StatusCode::FAILURE);
+  if ( m_align == "Stations" ) {
+    const DeITDetector::Stations& stations = tracker->stations( );
+    m_indexMap = createIndices( stations.begin( )                                ,
+				stations.end( )                                  ,
+				m_fixed                                          ,
+				bind<STChannelID>( &DeITStation::elementID, _1 ) ,
+				bind<unsigned>( &STChannelID::station, _1 )      ,
+				m_elements                                       );
+  } else if ( m_align == "Layers" ) {
+    const DeITDetector::Layers& layers = tracker->layers( );
+    m_indexMap = createIndices( layers.begin( )                                 ,
+				layers.end( )                                   ,
+				m_fixed                                         ,
+				bind<STChannelID>( &DeITLayer::elementID, _1 )  ,
+				bind<unsigned>( &STChannelID::uniqueLayer, _1 ) ,
+				m_elements                                      );
+  } else if ( m_align == "Boxes" ) {
+    const DeITDetector::Boxes& boxes = tracker->boxes( );
+    m_indexMap = createIndices( boxes.begin( )                                      ,
+				boxes.end( )                                        ,
+				m_fixed                                             ,
+				bind<STChannelID>( &DeITBox::elementID, _1 )        ,
+				bind<unsigned>( &STChannelID::uniqueDetRegion, _1 ) ,
+				m_elements                                          );
+  } else if ( m_align == "Sectors" ) {
+    const DeITDetector::Sectors& sectors = tracker->sectors( );
+    m_indexMap = createIndices( sectors.begin( )                                 ,
+				sectors.end( )                                   ,
+				m_fixed                                          ,
+				bind<STChannelID>( &DeITSector::elementID, _1 )  ,
+				bind<unsigned>( &STChannelID::uniqueSector, _1 ) ,
+				m_elements                                       );
+  } else
+    return Error( "Unknown elements " + m_align 
+		  + ". You can only align Stations, Layers, Boxes or Sectors",
+		  StatusCode::FAILURE );
   
   return StatusCode::SUCCESS;
 }
 
 // return index for a given channel id
-BoolIndex AlignITDetector::index(const LHCbID& anLHCbID) const
-{
-  
+BoolIndex AlignITDetector::index( const LHCbID& anLHCbID ) const {
+
   BoolIndex boolIndex;
-  if (m_align == "Stations") {
-    boolIndex = getAssociatedIndex(anLHCbID                                , 
-                                   m_indexMap                              , 
-                                   bind<bool>(&LHCbID::isIT, _1)           , 
-                                   bind<STChannelID>(&LHCbID::stID,_1)     , 
-                                   bind<unsigned>(&STChannelID::station,_1)); 
-  } else if (m_align == "Layers") {
-    boolIndex = getAssociatedIndex(anLHCbID                                    , 
-                                   m_indexMap                                  , 
-                                   bind<bool>(&LHCbID::isIT, _1)               , 
-                                   bind<STChannelID>(&LHCbID::stID,_1)         , 
-                                   bind<unsigned>(&STChannelID::uniqueLayer,_1));
-  } else if (m_align == "Boxes") {
-    boolIndex = getAssociatedIndex(anLHCbID                                     , 
-                                   m_indexMap                                   ,
-                                   bind<bool>(&LHCbID::isIT, _1)                , 
-                                   bind<STChannelID>(&LHCbID::stID,_1)          , 
-                                   bind<unsigned>(&STChannelID::uniqueDetRegion,_1));
-  } else if (m_align == "Sectors") {
-    boolIndex = getAssociatedIndex(anLHCbID                                     , 
-                                   m_indexMap                                   ,
-                                   bind<bool>(&LHCbID::isIT, _1)                , 
-                                   bind<STChannelID>(&LHCbID::stID,_1)          , 
-                                   bind<unsigned>(&STChannelID::uniqueSector,_1));
+  if ( m_align == "Stations" ) {
+    boolIndex = getAssociatedIndex( anLHCbID                                    ,
+				    m_indexMap                                  ,
+				    bind<bool>( &LHCbID::isIT, _1 )             ,
+				    bind<STChannelID>( &LHCbID::stID, _1 )      ,
+				    bind<unsigned>( &STChannelID::station, _1 ) ); 
+  } else if ( m_align == "Layers" ) {
+    boolIndex = getAssociatedIndex( anLHCbID                                        ,
+				    m_indexMap                                      ,
+				    bind<bool>( &LHCbID::isIT, _1 )                 ,
+				    bind<STChannelID>( &LHCbID::stID, _1 )          ,
+				    bind<unsigned>( &STChannelID::uniqueLayer, _1 ) );
+  } else if ( m_align == "Boxes" ) {
+    boolIndex = getAssociatedIndex( anLHCbID                                            ,
+				    m_indexMap                                          ,
+				    bind<bool>( &LHCbID::isIT, _1 )                     ,
+				    bind<STChannelID>( &LHCbID::stID, _1 )              ,
+				    bind<unsigned>( &STChannelID::uniqueDetRegion, _1 ) );
+  } else if ( m_align == "Sectors" ) {
+    boolIndex = getAssociatedIndex( anLHCbID                                         ,
+				    m_indexMap                                       ,
+				    bind<bool>( &LHCbID::isIT, _1 )                  ,
+				    bind<STChannelID>( &LHCbID::stID, _1 )           ,
+				    bind<unsigned>( &STChannelID::uniqueSector, _1 ) );
   }
   
   return boolIndex;
