c32e0b1df81c18ce49878accd0b58ef556d725f1
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-11-09 17:28:42 +0000

    add customdict to remove a duplicate dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36057 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ea2da1af0868ddf5123098228a87d045a6d42ad1

---
+++ LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
@@ -0,0 +1,9 @@
+// $Id: lcg_selection.h,v 1.1 2007-11-09 17:28:42 cattanem Exp $
+#ifndef L0EVENT_LCGDICT_H 
+#define L0EVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// Dummy file in this case, custom dictionary just there to exclude duplicates
+
+#endif // L0EVENT_LCGDICT_H
