35da8f2f7850235ec5493bbd559e2fc921203775
Giacomo Graziani <ggiacomo@cern.ch>
2007-11-12 11:35:26 +0000

    add mapping of Oracle object for display options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97234 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1af4a89c8e7f26157840ebe9b2ed9bfd2a5019cd

---
+++ Online/Online/OnlineHistDB/OnlineHistDB/OHDB_ocitypes.h
@@ -0,0 +1,91 @@
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OnlineHistDB/OnlineHistDB/OHDB_ocitypes.h,v 1.1 2007-11-12 11:35:26 ggiacomo Exp $
+#ifndef OHDB_OCITYPES_H
+#define OHDB_OCITYPES_H 1
+struct dispopt_s
+{
+  OCIString * LABEL_X;
+  OCIString * LABEL_Y;
+  OCIString * LABEL_Z;
+  OCINumber YMIN;
+  OCINumber YMAX;
+  OCINumber STATS;
+  OCINumber FILLSTYLE;
+  OCINumber FILLCOLOR;
+  OCINumber LINESTYLE;
+  OCINumber LINECOLOR;
+  OCINumber LINEWIDTH;
+  OCIString * DRAWOPTS;
+  OCINumber XMIN;
+  OCINumber XMAX;
+  OCINumber ZMIN;
+  OCINumber ZMAX;
+  OCINumber LOGX;
+  OCINumber LOGY;
+  OCINumber LOGZ;
+  OCIString * TIMAGE;
+  OCIString * REF;
+  OCINumber REFRESH;
+  OCINumber TIT_X_SIZE;
+  OCINumber TIT_X_OFFS;
+  OCINumber TIT_Y_SIZE;
+  OCINumber TIT_Y_OFFS;
+  OCINumber TIT_Z_SIZE;
+  OCINumber TIT_Z_OFFS;
+  OCINumber LAB_X_SIZE;
+  OCINumber LAB_X_OFFS;
+  OCINumber LAB_Y_SIZE;
+  OCINumber LAB_Y_OFFS;
+  OCINumber LAB_Z_SIZE;
+  OCINumber LAB_Z_OFFS;
+  OCINumber GRIDX;
+  OCINumber GRIDY;
+  OCINumber THETA;
+  OCINumber PHI;
+};
+typedef struct dispopt_s dispopt;
+
+struct dispopt_ind
+{
+  OCIInd _atomic;
+  OCIInd LABEL_X;
+  OCIInd LABEL_Y;
+  OCIInd LABEL_Z;
+  OCIInd YMIN;
+  OCIInd YMAX;
+  OCIInd STATS;
+  OCIInd FILLSTYLE;
+  OCIInd FILLCOLOR;
+  OCIInd LINESTYLE;
+  OCIInd LINECOLOR;
+  OCIInd LINEWIDTH;
+  OCIInd DRAWOPTS;
+  OCIInd XMIN;
+  OCIInd XMAX;
+  OCIInd ZMIN;
+  OCIInd ZMAX;
+  OCIInd LOGX;
+  OCIInd LOGY;
+  OCIInd LOGZ;
+  OCIInd TIMAGE;
+  OCIInd REF;
+  OCIInd REFRESH;
+  OCIInd TIT_X_SIZE;
+  OCIInd TIT_X_OFFS;
+  OCIInd TIT_Y_SIZE;
+  OCIInd TIT_Y_OFFS;
+  OCIInd TIT_Z_SIZE;
+  OCIInd TIT_Z_OFFS;
+  OCIInd LAB_X_SIZE;
+  OCIInd LAB_X_OFFS;
+  OCIInd LAB_Y_SIZE;
+  OCIInd LAB_Y_OFFS;
+  OCIInd LAB_Z_SIZE;
+  OCIInd LAB_Z_OFFS;
+  OCIInd GRIDX;
+  OCIInd GRIDY;
+  OCIInd THETA;
+  OCIInd PHI;
+};
+typedef struct dispopt_ind dispoptInd;
+
+#endif // OHDB_OCITYPES_H
