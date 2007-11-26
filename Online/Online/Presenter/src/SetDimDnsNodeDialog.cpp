de2d26542c08fb6290d13a68863e4894376a527a
psomogyi <psomogyi@cern.ch>
2007-11-26 19:53:45 +0000

    root downgrade
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97262 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f0e0bf8d1deb9dbd1b58b326f30c13753a280d65

--- Online/Online/Presenter/src/SetDimDnsNodeDialog.cpp
+++ Online/Online/Presenter/src/SetDimDnsNodeDialog.cpp
@@ -37,8 +37,8 @@ void SetDimDnsNodeDialog::build()
   
   m_okButton = new TGTextButton(this, "OK");
   m_okButton->SetTextJustify(36);
-  m_okButton->SetMargins(0,0,0,0);
-  m_okButton->SetWrapLength(-1);
+//  m_okButton->SetMargins(0,0,0,0);
+//  m_okButton->SetWrapLength(-1);
   m_okButton->Resize(92,24);
   AddFrame(m_okButton, new TGLayoutHints(kLHintsLeft | kLHintsTop,2,2,2,2));
   m_okButton->MoveResize(88,88,92,24);
@@ -46,8 +46,8 @@ void SetDimDnsNodeDialog::build()
   
   m_cancelButton = new TGTextButton(this, "Cancel");
   m_cancelButton->SetTextJustify(36);
-  m_cancelButton->SetMargins(0,0,0,0);
-  m_cancelButton->SetWrapLength(-1);
+//  m_cancelButton->SetMargins(0,0,0,0);
+//  m_cancelButton->SetWrapLength(-1);
   m_cancelButton->Resize(92,24);
   AddFrame(m_cancelButton, new TGLayoutHints(kLHintsLeft | kLHintsTop,2,2,2,2));
   m_cancelButton->MoveResize(192,88,92,24);
@@ -56,8 +56,8 @@ void SetDimDnsNodeDialog::build()
 
   TGLabel* fLabel541 = new TGLabel(this, "DIM DNS node:");
   fLabel541->SetTextJustify(36);
-  fLabel541->SetMargins(0,0,0,0);
-  fLabel541->SetWrapLength(-1);
+//  fLabel541->SetMargins(0,0,0,0);
+//  fLabel541->SetWrapLength(-1);
   AddFrame(fLabel541, new TGLayoutHints(kLHintsLeft | kLHintsTop,2,2,2,2));
   fLabel541->MoveResize(16,8,112,18);
     
