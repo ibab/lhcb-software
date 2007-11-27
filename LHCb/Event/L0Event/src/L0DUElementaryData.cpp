4d020cd4d1017499a4d6ffd1793bab872b9e1318
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-11-27 07:46:10 +0000

    Fix for serializeStl.h move to Gaudi
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36089 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 04351d1c436bcb692a970d61143256df8a58cb06

--- LHCb/Event/L0Event/src/L0DUElementaryData.cpp
+++ LHCb/Event/L0Event/src/L0DUElementaryData.cpp
@@ -1,4 +1,4 @@
-// $Id: L0DUElementaryData.cpp,v 1.3 2007-10-31 14:31:15 odescham Exp $
+// $Id: L0DUElementaryData.cpp,v 1.4 2007-11-27 07:46:10 cattanem Exp $
 // Include files 
 #include <utility>
 #include <string>
@@ -34,6 +34,7 @@ unsigned long LHCb::L0DUElementaryData::operation(const std::string& v_operator,
 
 
 std::string LHCb::L0DUElementaryData::summary(){
+  using GaudiUtils::operator<<;
   std::stringstream s(" ");
   std::stringstream ss(" ");
   if(m_operator != "Id")
@@ -46,6 +47,7 @@ std::string LHCb::L0DUElementaryData::summary(){
 
 
 std::string LHCb::L0DUElementaryData::description(){
+  using GaudiUtils::operator<<;
   std::stringstream s(" ");
   std::stringstream ss(" ");
   if(m_operator != "Id")ss << " = [" << m_operator << "](" << m_operandsName << ")" ;
