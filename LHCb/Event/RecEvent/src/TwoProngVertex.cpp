9554e669975f8085485d5fc5bb99e094425595a6
Wouter Hulsbergen <wouterh@nikhef.nl>
2007-11-28 16:00:29 +0000

    changes to TwoProngVertex::covMatrix7x7 - fixed bug in calculation of covmommom (one transpose was missing). rewrote that part of calculation to make it more readible. - tidied up the comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36105 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8dab0d6d55fc1eff41665abfeea8bec8fa9fb965

--- LHCb/Event/RecEvent/src/TwoProngVertex.cpp
+++ LHCb/Event/RecEvent/src/TwoProngVertex.cpp
@@ -1,4 +1,4 @@
-// $Id: TwoProngVertex.cpp,v 1.5 2007-11-27 18:36:50 sean Exp $
+// $Id: TwoProngVertex.cpp,v 1.6 2007-11-28 16:00:29 wouter Exp $
 // Include files
 
 // local
@@ -62,65 +62,36 @@ namespace LHCb
   const Gaudi::SymMatrix7x7 TwoProngVertex::covMatrix7x7(const double mass1, const double mass2 ) const
   {
     //< Return the covariance matrix for the V0
-    //    Gaudi::LorentzVector V0Mom = momentum(mass1, mass2);
-   
-Gaudi::SymMatrix7x7 theMatrix;      
-if(tracks().size()<2){ throwError("covMatrix7x7 needs at least 2 V0 daughters");
- return theMatrix; }
+    
     //the Jacobians to change momenta of daughters from tx,ty,qop to PxPyPzE space
-
     Gaudi::Matrix4x3 JacobP1;
     Gaudi::Matrix4x3 JacobP2;
-    //Moved a function to Gaudi::Math, means RecEvent now requires 
-    //LHCbMath v2r3 (or greater) in Kernel
     Gaudi::Math::JacobdP4dMom( m_momA, mass1, JacobP1);
     Gaudi::Math::JacobdP4dMom( m_momB, mass2, JacobP2);
-
-
-    
-    
-       
-       // fill Linear Algebra vector from 3D-point
-  
-      Gaudi::Matrix4x3    mtrx1;
-       Gaudi::Math::scale ( mtrx1 , 100.0   ) ;
-
     
     //covmompos contains the transformed covariance matrix for the off diagonal part (position and momentum)
     Gaudi::Matrix4x3 covmompos;
-    //. . . . . . . 
-    //. . . . . . . 
-    //. . . . . . . 
-    //x x x . . . . 
-    //x x x . . . . 
-    //x x x . . . .
-    //x x x . . . . 
-    covmompos =JacobP1* (m_momposcovA) ;
+    covmompos =  JacobP1*  m_momposcovA;
     covmompos += JacobP2 * m_momposcovB; 
-
-    //covmommom contains the V0 symmetric covariance for the  momentum part
-    Gaudi::SymMatrix4x4 covmommom ;
     
-    ROOT::Math::AssignSym::Evaluate
-      (covmommom,
-       (Gaudi::Matrix4x4)  ((JacobP1*m_mommomcov) * ROOT::Math::Transpose(JacobP2)) +
-       (Gaudi::Matrix4x4)((JacobP2*m_mommomcov) * (Gaudi::Matrix3x4) ROOT::Math::Transpose(JacobP1))
-
-       );
+    //covmommom contains the V0 symmetric covariance for the  momentum part
+    Gaudi::Matrix4x4 covmommom12 = JacobP1 * m_mommomcov * ROOT::Math::Transpose(JacobP2) ;
+    Gaudi::SymMatrix4x4 covmommom ;
+    ROOT::Math::AssignSym::Evaluate(covmommom, covmommom12 + ROOT::Math::Transpose(covmommom12)) ;
     covmommom+=ROOT::Math::Similarity(JacobP1, m_momcovA); //J * C * JT
     covmommom+=ROOT::Math::Similarity(JacobP2, m_momcovB);
     
-    //theMatrix.Place_at(m_poscov, 0,0);
+    // finally, fill the returned matrix
+    Gaudi::SymMatrix7x7 theMatrix;
+    // position part from rec vertex
+    theMatrix.Place_at(LHCb::VertexBase::covMatrix(), 0,0);
+    // momentum part
     theMatrix.Place_at(covmommom, 3,3);
-    theMatrix.Place_at(LHCb::VertexBase::covMatrix(), 0,0); //position part from rec vertex
-
-    //now to copy off diagonal elements into full 7x7 matrix
-    for(int i = 0 ; i <4; ++i){
-      for(int j =0;j<3;++j){
+    // off diagonal elements
+    for(int i = 0 ; i <4; ++i)
+      for(int j =0;j<3;++j)
         theMatrix(i+3,j) = covmompos(i,j);
-      }
-    }
-      
+    
     return theMatrix;
   }
 
