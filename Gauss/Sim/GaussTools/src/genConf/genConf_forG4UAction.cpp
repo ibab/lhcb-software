4548159b941da715b27c3ebf77f5cec1d0cb0548
Gloria Corti <gloria.corti@cern.ch>
2007-11-30 16:59:56 +0000

    allow to generate genConf for G4 User actions
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93199 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d67e301be2ec052a61b984c170e80960e7bb7fcd

---
+++ Gauss/Sim/GaussTools/src/genConf/genConf_forG4UAction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "G4RunManager.hh"
+#include "G4VUserPhysicsList.hh"
+
+namespace 
+{
+  /** 
+   * Hack to allow the generation of the configurables for Gauss.
+   * Some components inherit from Geant4 classes that cannot be instantiated if
+   * other structures have not been initialized first.
+   * This class and its static instance should enfore the correct order if
+   * loaded with LD_PRELOAD when calling genconf.
+   */
+  struct GenConfForG4UAction 
+  {
+    class GenConfG4PhysicsList: public G4VUserPhysicsList {
+      virtual void SetCuts() {}
+      virtual void ConstructParticle() {}
+      virtual void ConstructProcess() {}
+    };
+    
+    GenConfForG4UAction()
+    {
+      std::cout << "**************************************************" << std::endl;
+      std::cout << "**************************************************" << std::endl;
+      std::cout << "**************************************************" << std::endl;
+      std::cout << "**************************************************" << std::endl;
+      std::cout << "**************************************************" << std::endl;
+
+      // Minimal G4 initialization needed by G4 actions.
+      G4RunManager * runManager = new G4RunManager;
+      G4VUserPhysicsList* physics = new GenConfG4PhysicsList;
+      runManager->SetUserInitialization(physics);
+
+    }
+  };
+  
+  GenConfForG4UAction genConfG4UAction;
+}
