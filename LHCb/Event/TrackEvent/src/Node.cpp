9d716c56b0f8f6c54c368fedb147166da63500c9
Wouter Hulsbergen <wouterh@nikhef.nl>
2007-11-30 13:12:29 +0000

    moved refvector from Measurement to Node
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36119 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5a741f49b07a388a98a903d4c6071539d2d15efa

--- LHCb/Event/TrackEvent/src/Node.cpp
+++ LHCb/Event/TrackEvent/src/Node.cpp
@@ -1,4 +1,4 @@
-// $Id: Node.cpp,v 1.3 2007-09-17 09:21:36 jonrob Exp $
+// $Id: Node.cpp,v 1.4 2007-11-30 13:12:29 wouter Exp $
 
 // local
 #include "Event/Node.h"
@@ -8,34 +8,6 @@
 //
 // 2006-06-10 : M. Needham
 //-----------------------------------------------------------------------------
-
-LHCb::Node::Node( const LHCb::Node& rhs ):
-  m_measurement(rhs.m_measurement),
-  m_residual(rhs.m_residual),
-  m_errResidual(rhs.m_errResidual),
-  m_errMeasure(rhs.m_errMeasure),
-  m_projectionMatrix(rhs.m_projectionMatrix)
-{
-  //Node copy constructer
-  // Clone the state !
-  m_state = rhs.state().clone();
-}
-
-LHCb::Node::~Node(){
-  // destructer
-  if (m_state != 0) delete m_state;
-}
-
-void LHCb::Node::reset() 
-{
-
-  if (m_state != 0 ) delete m_state;
-  m_state = 0;
-  m_residual = 0.;
-  m_errResidual = 0.;
-  m_errMeasure = 0.;
-        
-}
 
 LHCb::Node* LHCb::Node::clone() const 
 {
