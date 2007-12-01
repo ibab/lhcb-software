c56b8374dc89a13d6f507e5b23d793df71bf0cfe
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-12-01 15:54:24 +0000

    update the tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@50116 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 36e99cdc634bfb33dbfd479bbefcf1a8d0277715

--- LHCb/Phys/LoKiGen/python/LoKiGen/tests.py
+++ LHCb/Phys/LoKiGen/python/LoKiGen/tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 
 # =============================================================================
-# $Id: tests.py,v 1.3 2007-12-01 13:53:22 ibelyaev Exp $
+# $Id: tests.py,v 1.4 2007-12-01 15:54:24 ibelyaev Exp $
 # =============================================================================
 ## @file   LoKiGen/tests.py
 #
@@ -30,9 +30,49 @@ A.Golutvin, P.Koppenburg have been used in the design.
 __author__ = "Vanya BELYAEV ibelyaev@physics.syr.edu" 
 # =============================================================================
 import LoKiNumbers.decorators
+import LoKiGen.HepMC
+
 from   LoKiGen.decorators     import *
 from   LoKiCore.functions     import *
 from   LoKiCore.math          import *
+
+
+def test1() :
+    
+    p = HepMC.GenParticle()
+    p.set_pdg_id(11)
+    
+    print 'particle: %s/%s '%(p.name(),pname(p))
+    print 'particle: (GPX,GPY,GPZ,GM)=(%s,%s,%s,%s)'%(GPX(p),GPY(p),GPZ(p),GM(p))
+    print 'particle: ( p>>GID , p>>cos(GE) )=(%s,%s)'    %(p>>GID,p>>cos(GE))
+    
+    v = std.vector('const HepMC::GenParticle*')()
+    
+    v.push_back(p)
+    p1 = HepMC.GenParticle()
+    p1.set_pdg_id(13)
+    v.push_back(p1)
+    p2 = HepMC.GenParticle()
+    p2.set_pdg_id(22)
+    v.push_back( p2 )
+    
+    print ' v>>GID                           : %s'%(v>>GID)
+    print ' v>>yields(GID)                   : %s'%(v>>yields(GID))
+    print ' v>>process(GID)>>min_value(GP)   : %s'%(v>>process(GID)>>min_value(GP))
+    print '(v>>min_element(GID)).name()      : %s'%(v>>min_element(GID)).name()
+    print ' v>>min_value(GID)                : %s'%(v>>min_value(GID))
+    print '(v>>("e+"!=GABSID)).size()        : %s'%(v>>('e+'!=GABSID)).size()
+    print '(v>>select("mu+"!=GABSID)).size() : %s'%(v>>select('mu+'!=GABSID)).size()
+
+# =============================================================================
+def testAll() :
+    test1()
+    
+# =============================================================================
+if '__main__' == __name__ :
+    testAll() 
+        
+
 # =============================================================================
 # The END 
 # =============================================================================
