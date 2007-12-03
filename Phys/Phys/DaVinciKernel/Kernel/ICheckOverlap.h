c4dafacf9eabddc17c8b9448cb3ac553112fbedf
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-12-03 09:17:41 +0000

    add two templated methdos for ICheckOverlap
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63920 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 37de611076d068f2e4a878c26c46535334f7dc69

--- Phys/Phys/DaVinciKernel/Kernel/ICheckOverlap.h
+++ Phys/Phys/DaVinciKernel/Kernel/ICheckOverlap.h
@@ -1,19 +1,25 @@
-// $Id: ICheckOverlap.h,v 1.9 2006-05-11 13:34:00 jpalac Exp $
+// $Id: ICheckOverlap.h,v 1.10 2007-12-03 09:17:41 ibelyaev Exp $
+// ============================================================================
 #ifndef DAVINCIKERNEL_ICHECKOVERLAP_H 
 #define DAVINCIKERNEL_ICHECKOVERLAP_H 1
-
+// ============================================================================
 // Include files
-// from STL
+// ============================================================================
+// STL&STL
+// ============================================================================
 #include <string>
-
-// from Gaudi
+// ============================================================================
+// GaudiKernel
+// ============================================================================
 #include "GaudiKernel/IAlgTool.h"
+// ============================================================================
+// Event/PhysEvent 
+// ============================================================================
 #include "Event/Particle.h"
-#include "Event/Vertex.h"
-
-// Declaration of the interface ID ( interface id, major version, minor version)
+// ============================================================================
+/// Declaration of the interface ID ( interface id, major version, minor version)
 static const InterfaceID IID_ICheckOverlap("ICheckOverlap", 2, 1);
-
+// ============================================================================
 /** @class ICheckOverlap ICheckOverlap.h
  *  
  *  Tool to check if more than one particle originate 
@@ -22,8 +28,8 @@ static const InterfaceID IID_ICheckOverlap("ICheckOverlap", 2, 1);
  *  @author Jose Helder Lopes, redesigned by P. Koppenburg
  *  @date   28/06/2002
  */
-
-class ICheckOverlap : virtual public IAlgTool {
+class ICheckOverlap : virtual public IAlgTool 
+{
   
 public:
   /// Retrieve interface ID
@@ -32,7 +38,7 @@ public:
   /// Check for duplicate use of a protoparticle to produce particles.
   /// Argument: parts is a vector of pointers to particles.  
   virtual bool foundOverlap( const LHCb::Particle::ConstVector & parts  ) = 0;
-
+  
   /// Check for duplicate use of a protoparticle to produce particles.  
   virtual bool foundOverlap( const LHCb::Particle* ) = 0;
   
@@ -50,7 +56,7 @@ public:
                              const LHCb::Particle*,
                              const LHCb::Particle*,
                              const LHCb::Particle*) = 0;
-
+  
   /// Check for duplicate use of a protoparticle to produce particles.
   /// Continue a previous check using the contents of the vector of pointers 
   /// to protoparticles.(Most intended for internal use by the other methods).
@@ -62,14 +68,57 @@ public:
   /// Check for duplicate use of a protoparticle to produce decay tree of
   /// any particle in vector. Removes found particles from vector.
   virtual StatusCode removeOverlap( LHCb::Particle::ConstVector& ) = 0;
-
+  
   /// Check for duplicate use of a protoparticle to produce decay tree of
   /// any particle in vector. Removes found particles from vector.
   virtual StatusCode removeOverlap( LHCb::Particle::Vector& ) = 0;
-
+  // ==========================================================================  
+  /** Check for the overlap for the arbitrary sequence of the objects, 
+   *  implicitely convertible to <c>const LHCb::Particle*</c>.
+   *  E.g. any STL container with <c>const LHCb::Particle*</c> or 
+   *  <c>LHCb::Particle*</c>, <c>SmartRefVector<LHCb::Particle> 
+   *  and <c>LHCb::Particle::Container</c> are OK.
+   *  The various container adapters (e.g. <c>LoKi::Range_</c>)
+   *  also fit nicely into the concept
+   *  @param begin  begin-iterator of the input sequence of "particles"
+   *  @param end    end-iterator   of the input sequence of "particles" 
+   *  @return true of overlarps are found 
+   */
+  template <class PARTICLE>
+  bool foundOverlap 
+  ( PARTICLE begin ,
+    PARTICLE end   ) 
+  { return foundOverlap ( LHCb::Particle::ConstVector ( begin, end ) ) ; }
+  // ==========================================================================  
+  /** Remove the overlap for the arbitrary sequence of the objects, 
+   *  implicitely convertible to <c>const LHCb::Particle*</c>.
+   *  E.g. any STL container with <c>const LHCb::Particle*</c> or 
+   *  <c>LHCb::Particle*</c>, <c>SmartRefVector<LHCb::Particle> and 
+   *  <c>LHCb::Particle::Container</c> are OK.
+   *  The various container adapters (e.g. <c>LoKi::Range_</c>)
+   *  also fit nicely into the concept
+   *  @param begin  begin-iterator of the input sequnce of "particles"
+   *  @param end    end-iterator   of the input sequnce of "particles" 
+   *  @param result the resulting (output) vector with no overlaps
+   *  @return status code 
+   */
+  template <class PARTICLE>
+  StatusCode  removeOverlap 
+  ( PARTICLE begin                      ,
+    PARTICLE end                        , 
+    LHCb::Particle::ConstVector& result ) 
+  { 
+    result =  LHCb::Particle::ConstVector ( begin, end ) ;
+    return removeOverlap ( result ) ; 
+  }
+  // ==========================================================================  
 protected:
   
 private:
   
 };
+// ============================================================================
+// The END 
+// ============================================================================
 #endif // DAVINCIKERNEL_ICHECKOVERLAP_H
+// ============================================================================
