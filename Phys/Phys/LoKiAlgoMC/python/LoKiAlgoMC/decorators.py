989463eebb1398f6107d5c366dac9864b5e633d7
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-12-03 11:56:49 +0000

    v7r0: the powerful streamers
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@50126 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5c1b22048f315e09af77f47e08fdada23279bf0f

--- Phys/Phys/LoKiAlgoMC/python/LoKiAlgoMC/decorators.py
+++ Phys/Phys/LoKiAlgoMC/python/LoKiAlgoMC/decorators.py
@@ -5,57 +5,37 @@
 #  The file is a part of LoKi and Bender projects
 #  @author Vanya BELYAEV ibelyaev@physics.syr.edu
 # =============================================================================
-""" The set of basic decorators for obejcts from LoKiAlgoMC library """
-_author_ = "Vanya BELYAEV ibelyaev@physics.syr.edu" 
+"""
+The set of basic decorators for objects from LoKiAlgoMC library
+"""
+# =============================================================================
+__author__ = "Vanya BELYAEV ibelyaev@physics.syr.edu" 
 # =============================================================================
 
 import LoKiCore.decorators   as _LoKiCore
 import LoKiPhysMC.decorators as _LoKiPhysMC
 from   LoKiAlgoMC.functions  import *
-import LoKiAlgo
+import LoKiAlgo.decorators   as _LoKiAlgo
 
 LoKi = _LoKiCore.LoKi
 LHCb = _LoKiCore.LHCb
 
 # =============================================================================
 ## decorate MC-algorithm: 
-AlgoMC = LoKiAlgo.decorators._decorateAlg( 'LoKi::AlgoMC' )
+AlgoMC = _LoKiAlgo._decorateAlg( 'LoKi::AlgoMC' )
 # =============================================================================
 
 _name = _LoKiPhysMC.__name__
 
 # =============================================================================
 ## make the decoration
-def _decorate ( name = _name ) :
-    """ Make the decoration """
-    ##
-    _decorated   = _LoKiCore.getAndDecorateFunctions (
-        name                                         , ## module name 
-        LoKi.Function('const LHCb::Particle*')        , ## the base 
-        LoKi.Dicts.ExtFunCalls(LHCb.Particle)         , ## call-traits 
-        LoKi.Dicts.FuncOps('const LHCb::Particle*')   ) ## operators 
-    _decorated |= _LoKiCore.getAndDecorateFunctions  (
-        name                                          , ## module name 
-        LoKi.Function('const LHCb::VertexBase*')      , ## the base 
-        LoKi.Dicts.ExtFunCalls(LHCb.VertexBase)       , ## call-traits 
-        LoKi.Dicts.FuncOps('const LHCb::VertexBase*') ) ## operators
-    _decorated |=  _LoKiCore.getAndDecoratePredicates (
-        name                                          , ## module name 
-        LoKi.Predicate('const LHCb::Particle*')       , ## the base 
-        LoKi.Dicts.ExtCutCalls(LHCb.Particle)         , ## call-traits 
-        LoKi.Dicts.FuncOps('const LHCb::Particle*')   ) ## operators 
-    _decorated |= _LoKiCore.getAndDecoratePredicates (
-        _name                                         , ## module name 
-        LoKi.Predicate('const LHCb::VertexBase*')     , ## the base 
-        LoKi.Dicts.ExtCutCalls(LHCb.VertexBase)       , ## call-traits 
-        LoKi.Dicts.FuncOps('const LHCb::VertexBase*') ) ## the operators
-    ## 
-    return _decorated                                   ##
+_decorate = _LoKiAlgo._decorate 
+
 # =============================================================================
 
 # =============================================================================
-## perform the actua decoration
-_decorated = _decorate() 
+## perform the actual decoration
+_decorated = _decorate( _name ) 
 # =============================================================================
 
     
