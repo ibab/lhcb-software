c7e6541660c14ffb796a032e28b0020c762b626f
Mariusz Witek <witekma@cern.ch>
2007-12-04 08:51:01 +0000

    Add IPVOfflineTool.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17525 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 06bc0ad86d0c6c07495a360d67888805083cb8fa

---
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IPVOfflineTool.h
@@ -0,0 +1,62 @@
+// $Id: IPVOfflineTool.h,v 1.1 2007-12-04 08:48:09 witekma Exp $
+#ifndef IPVOFFLINETOOL_H 
+#define IPVOFFLINETOOL_H 1
+// from STL
+#include <string>
+#include <vector>
+// from Gaudi
+#include "GaudiKernel/AlgTool.h"
+// Interface ID 
+static const InterfaceID IID_IPVOfflineTool("IPVOfflineTool",1,0);
+
+class IPVOfflineTool : virtual public IAlgTool {
+ public:
+  // Retrieve interface ID
+  static const InterfaceID& interfaceID() {return IID_IPVOfflineTool;}
+
+
+  /** Reconstructs single PV excluding tracks from a given vector.
+   *
+   *  The full reconstruction of single PV is performed for a given 
+   *  seed point with the tracks from the default location
+   *  excluding tracks in tracks2exclude vector.
+   *  It is not a refit. Procedure can fail due to tracks excluded. 
+   */
+  virtual StatusCode reDoSinglePV(const Gaudi::XYZPoint xyzseed,
+			          std::vector<const LHCb::Track*>& tracks2exclude,
+			          LHCb::RecVertex& outvtx)=0;
+
+  /** Reconstructs all PVs excluding tracks from a given vector and returns new PV.
+   *
+   *  The full reconstruction of all PVs is performed with the tracks
+   *  from the default location excluding tracks in tracks2exclude vector.
+   *  The returned outvtx matches invtx by comparing track contents.
+   *  It is not a refit. Procedure can fail due to tracks excluded. 
+   */
+  virtual StatusCode reDoMultiPV(const LHCb::RecVertex& invtx, 
+			         std::vector<const LHCb::Track*>& tracks2exclude, 
+			         LHCb::RecVertex& outvtx)=0;  
+
+
+  /// Reconstructs single PV with tracks specified in a vector.
+  virtual StatusCode reconstructSinglePVFromTracks(const Gaudi::XYZPoint xyzseed,
+			          std::vector<const LHCb::Track*>& tracks2use,
+			          LHCb::RecVertex& outvtx)=0;
+
+  /// Reconstructs all PVs with tracks specified in a vector.
+  virtual StatusCode reconstructMultiPVFromTracks(
+			          std::vector<const LHCb::Track*>& tracks2use,
+			          std::vector<LHCb::RecVertex>& outvtxVec)=0;
+
+
+
+  /// Reconstructs all PVs with tracks from default location.
+  virtual StatusCode reconstructMultiPV(std::vector<LHCb::RecVertex>& outvtxVec)=0; 
+
+  /// Reconstructs single PV for a given seed with tracks from default location.
+  virtual StatusCode reconstructSinglePV(const Gaudi::XYZPoint xyzseed,
+			                 LHCb::RecVertex& outvtx)=0;
+
+
+};
+#endif // IPVOFFLINETOOL_H
