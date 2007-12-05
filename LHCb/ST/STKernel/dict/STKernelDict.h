60a3c827e54505873107bfb93d44ee6ef33f1243
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-12-05 15:40:58 +0000

    add ISTSignalToNoiseTool.h, moved from DigiEvent
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@26829 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5516f6ae85fd23339061d6ab129e2b93d61ee0aa

--- LHCb/ST/STKernel/dict/STKernelDict.h
+++ LHCb/ST/STKernel/dict/STKernelDict.h
@@ -1,7 +1,8 @@
-// $Id: STKernelDict.h,v 1.1 2007-11-28 14:08:30 cattanem Exp $
+// $Id: STKernelDict.h,v 1.2 2007-12-05 15:40:56 cattanem Exp $
 #ifndef DICT_STKERNELDICT_H 
 #define DICT_STKERNELDICT_H 1
 
 #include "Kernel/ISTReadoutTool.h"
+#include "Kernel/ISTSignalToNoiseTool.h"
 
 #endif // DICT_STKERNELDICT_H
