60e87ecaa4c42a291af96b9a228ddd7fc2331a81
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-12-06 12:43:43 +0000

    add dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39859 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c493904b7c29dd47e09ecac3f83b6ca6b62230d0

---
+++ LHCb/Calo/CaloDAQ/dict/CaloDAQDict.h
@@ -0,0 +1,12 @@
+// $Id: CaloDAQDict.h,v 1.1 2007-12-06 12:43:42 cattanem Exp $
+#ifndef DICT_CALODAQDICT_H 
+#define DICT_CALODAQDICT_H 1
+
+#include "CaloDAQ/ICaloDataProvider.h"
+#include "CaloDAQ/ICaloEnergyFromRaw.h"
+#include "CaloDAQ/ICaloL0DataProvider.h"
+#include "CaloDAQ/ICaloReadoutTool.h"
+#include "CaloDAQ/ICaloTriggerAdcsFromRaw.h"
+#include "CaloDAQ/ICaloTriggerBitsFromRaw.h"
+
+#endif // DICT_CALODAQDICT_H
