b4aea21ed9ccc426b3b792a8249c9f7159925851
Wouter Hulsbergen <wouterh@nikhef.nl>
2007-12-06 14:46:50 +0000

    added missing const
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@17547 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8f9594d6bfed31e7103f14ab1e39d372ab7975d8

--- LHCb/Tr/TrackInterfaces/TrackInterfaces/IStateCorrectionTool.h
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IStateCorrectionTool.h
@@ -1,4 +1,4 @@
-// $Id: IStateCorrectionTool.h,v 1.3 2007-02-19 12:38:15 cattanem Exp $
+// $Id: IStateCorrectionTool.h,v 1.4 2007-12-06 14:46:50 wouter Exp $
 #ifndef TRACKINTERFACES_ISTATECORRECTIONTOOL_H 
 #define TRACKINTERFACES_ISTATECORRECTIONTOOL_H 1
 
@@ -29,7 +29,7 @@ public:
   virtual void correctState( LHCb::State& state,
                              const Material* material,
                              double wallThickness = 0,
-                             bool upstream = true ) = 0;
+                             bool upstream = true ) const = 0;
   
   // Return the interface ID
   static const InterfaceID& interfaceID()
