a58a78dd63acdcd317033839f0019de05bbcdadd
Johannes Albrecht <johannes.albrecht@cern.ch>
2007-12-07 18:00:44 +0000

    add interface to prepare tracks from muon coords
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71079 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 63f3e5480c3b99804edb981b58e49564a209644a

--- Hlt/Hlt/HltBase/HltBase/IMuonSeedTool.h
+++ Hlt/Hlt/HltBase/HltBase/IMuonSeedTool.h
@@ -1,4 +1,4 @@
-// $Id: IMuonSeedTool.h,v 1.1 2007-07-03 12:39:05 albrecht Exp $
+// $Id: IMuonSeedTool.h,v 1.2 2007-12-07 18:00:44 albrecht Exp $
 #ifndef IMUONSEEDTOOL_H 
 #define IMUONSEEDTOOL_H 1
 
@@ -29,9 +29,28 @@ public:
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IMuonSeedTool; }
 
-  
+  /*
+   *    this method prepares a L0MuonCandidate to a track at the M2
+   *    which can then be used to seed L0 confirmation
+   *
+   *    input:  a L0MuonCandidate
+   *    output: a Track which hold a state at M2 
+   *            errors of the state define search window size for L0Confirmation
+   */
   virtual StatusCode makeTrack( const LHCb::L0MuonCandidate& muonL0Cand,
                                 LHCb::Track& seedTrack ) = 0; 
+ 
+  /*
+   *    this method prepares a track from MuonCoords to a track at the M2
+   *    which can then be used to seed L0 confirmation
+   *
+   *    input:  a Track from the muon coords
+   *    output: a Track which hold a state at M2 
+   *            errors of the state define search window size for L0Confirmation
+   */
+  virtual StatusCode makeTrack( const LHCb::Track& inputTrack,
+                                LHCb::Track& seedTrack ) = 0; 
+  
 protected:
   
 private:
