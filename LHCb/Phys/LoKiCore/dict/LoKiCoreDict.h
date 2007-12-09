426f3bba358cd2c7f1e664bfd9cdb6d6ddba379a
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-12-09 19:17:43 +0000

    minor fix for dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@50153 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 77ab1b48626528f5236fe60935856da7fd2dacfd

--- LHCb/Phys/LoKiCore/dict/LoKiCoreDict.h
+++ LHCb/Phys/LoKiCore/dict/LoKiCoreDict.h
@@ -1,4 +1,4 @@
-// $Id: LoKiCoreDict.h,v 1.4 2007-12-03 12:03:23 ibelyaev Exp $
+// $Id: LoKiCoreDict.h,v 1.5 2007-12-09 19:17:32 ibelyaev Exp $
 // ============================================================================
 #ifndef LOKI_LOKICOREDICT_H 
 #define LOKI_LOKICOREDICT_H 1
@@ -30,10 +30,11 @@ namespace
   struct _Interfaces 
   {
     // 
-    LoKi::Interface<LoKi::ILoKiSvc>  m_1 ;
-    LoKi::Interface<LoKi::IReporter> m_2 ;
-    LoKi::Interface<GaudiTool>       m_3 ;
-    LoKi::Interface<GaudiAlgorithm>  m_4 ;
+    LoKi::Interface<LoKi::ILoKiSvc>   m_1 ;
+    LoKi::Interface<LoKi::IReporter>  m_2 ;
+    LoKi::Interface<GaudiTool>        m_3 ;
+    LoKi::Interface<GaudiAlgorithm>   m_4 ;
+    LoKi::Interface<IDataProviderSvc> m_5 ;
     // fictive constructor 
     _Interfaces() ;
   } ;  
