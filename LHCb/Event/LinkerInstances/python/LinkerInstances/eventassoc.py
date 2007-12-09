c8f2fae1b694c5e0fdb93475379cdfb8e61003f4
Vanya Belyaev <ivan.belyaev@cern.ch>
2007-12-09 20:13:20 +0000

    update for new structure of python modules
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36166 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 58b671e0455406adf0703ace510d9811246d18aa

---
+++ LHCb/Event/LinkerInstances/python/LinkerInstances/eventassoc.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# =============================================================================
+# $Id: eventassoc.py,v 1.1 2007-12-09 20:13:20 ibelyaev Exp $ 
+# =============================================================================
+"""
+The helper module for easy manipulations with Linker objects
+
+The module exports two funcions:
+
+   - linkedTo 
+   - linkedFrom
+   
+"""
+# =============================================================================
+__author__ = "Thomas RUF Thomas.Ruf@cern.ch"
+# =============================================================================
+
+from GaudiPython.Bindings import AppMgr as __AppMgr 
+
+## helper function for manipulation with linker objects 
+def linkedTo( t1, t2, location ):
+  """
+  Helper function for easy manipulation with linker objects
+
+  e.g.
+  [
+  the example is stollen from LHCb/GaudiPython TWiki page:
+  
+    http://cern.ch/twiki/bin/view/GaudiPython#How_to_access_Linker_tables
+    
+  ]
+  
+  >>> linked = linkedTo ( LHCb.MCParticle , LHCb.Track , 'Rec/Track/Velo' )
+  >>> track = ...        
+  >>> links = linked.range ( track )   ## get all related MC-particles 
+  >>> for i in range(0,links.size())   ## loop over all related MC-particles 
+  ...       mcpi = links[i]                ## get the related MC-particle  
+  ...       print mcpi                     ## print it 
+  >>> ....
+    
+  """
+  appMgr = __AppMgr()
+  cl = GaudiPython.gbl.LinkedTo( t1, t2)
+  lt = cl( appMgr.evtsvc()._idp, None, location )
+  return lt
+
+## helper function for manipulation with linker objects 
+def linkedFrom( t1, t2, location ):
+  """
+  Helper function for manipulation with linker objects
+  """
+  appMgr = __AppMgr()
+  cl = GaudiPython.gbl.LinkedFrom( t1, t2, int)
+  lt = cl( appMgr.evtsvc()._idp, None, location )
+  return lt
+
+# =============================================================================
+if '__main__' == __name__ :
+  print __doc__, " by ", __author__
+  o = dir() 
+  for i in o : print i   
+  
+# =============================================================================
+# The END 
+# =============================================================================
--- LHCb/Event/LinkerInstances/python/eventassoc.py
+++ LHCb/Event/LinkerInstances/python/eventassoc.py
@@ -1,13 +1,53 @@
-import GaudiPython
+#!/usr/bin/env python
+# =============================================================================
+# $Id: eventassoc.py,v 1.8 2007-12-09 20:13:20 ibelyaev Exp $ 
+# =============================================================================
+"""
+The helper module for manipulations with Linker objects 
 
-def linkedTo( t1, t2, location ):
-  appMgr = GaudiPython.AppMgr()
-  cl = GaudiPython.gbl.LinkedTo( t1, t2)
-  lt = cl( appMgr.evtsvc()._idp, None, location )
-  return lt
+WARNING: The module is deprecated, please use  LinkerIntances.eventassoc instead
 
-def linkedFrom( t1, t2, location ):
-  appMgr = GaudiPython.AppMgr()
-  cl = GaudiPython.gbl.LinkedFrom( t1, t2, int)
-  lt = cl( appMgr.evtsvc()._idp, None, location )
-  return lt
+ ---------------------------------------------------------------------------------------------------
+ |  OLD semantics  (deprecated)           |          NEW SEMANTICS                                 |
+ ---------------------------------------------------------------------------------------------------
+ | import eventassoc                      | import LinkerInstances.eventassoc                      |
+ | import eventassoc as XXX               | import LinkerInstances.eventassoc as XXX               |
+ | from eventassoc import *               | from LinkerInstances.eventassoc import *               |
+ | from eventassoc import linkedTo        | from LinkerInstances.eventassoc import linkedTo        |
+ | from eventassoc import linkedTo   ax Y | from LinkerInstances.eventassoc import linkedTo   as Y |
+ | from eventassoc import linkedFrom      | from LinkerInstances.eventassoc import linkedFrom      |  
+ | from eventassoc import linkedFrom as X | from LinkerInstances.eventassoc import linkedFrom as X |
+ ---------------------------------------------------------------------------------------------------
+"""
+# =============================================================================
+__author__ = "Thomas RUF Thomas.Ruf@cern.ch"
+# =============================================================================
+
+print """
+eventassoc.py WARNING  Deprecated module to be removed soon
+                       Please use LinkerIntances.eventassoc instead
+
+ ---------------------------------------------------------------------------------------------------
+ |  OLD semantics  (deprecated)           |          NEW SEMANTICS                                 |
+ ---------------------------------------------------------------------------------------------------
+ | import eventassoc                      | import LinkerInstances.eventassoc                      |
+ | import eventassoc as XXX               | import LinkerInstances.eventassoc as XXX               |
+ | from eventassoc import *               | from LinkerInstances.eventassoc import *               |
+ | from eventassoc import linkedTo        | from LinkerInstances.eventassoc import linkedTo        |
+ | from eventassoc import linkedTo   ax Y | from LinkerInstances.eventassoc import linkedTo   as Y |
+ | from eventassoc import linkedFrom      | from LinkerInstances.eventassoc import linkedFrom      |  
+ | from eventassoc import linkedFrom as X | from LinkerInstances.eventassoc import linkedFrom as X |
+ ---------------------------------------------------------------------------------------------------
+"""
+
+from LinkerInstances.eventassoc import *
+      
+# =============================================================================
+if '__main__' == __name__ :
+  print __doc__ 
+  o = dir() 
+  for i in o : print i
+  
+# =============================================================================
+# The END 
+# =============================================================================
