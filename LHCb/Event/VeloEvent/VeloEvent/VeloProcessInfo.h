381d37a35e0371ee0c4a0b2587985900f564c6c7
Tomasz Szumlak <szumlat@cern.ch>
2007-12-10 00:28:48 +0000

    update of VeloProcessInfo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@24219 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b91b91d744469b0ccde38aeb837473fb38845544

--- LHCb/Event/VeloEvent/VeloEvent/VeloProcessInfo.h
+++ LHCb/Event/VeloEvent/VeloEvent/VeloProcessInfo.h
@@ -1,4 +1,4 @@
-// $Id: VeloProcessInfo.h,v 1.4 2007-12-09 23:32:01 szumlat Exp $
+// $Id: VeloProcessInfo.h,v 1.5 2007-12-10 00:28:19 szumlat Exp $
 #ifndef EVENT_VELOPROCESSINFO_H 
 #define EVENT_VELOPROCESSINFO_H 1
 
@@ -34,6 +34,7 @@ public:
   bool isEnable(unsigned int proc);
   void setDataProcessInfo(unsigned int inValue);
   void setSimProcessInfo(VeloTELL1::dataVec inVec);
+  void setDevProcessInfo(VeloTELL1::dataVec inVec);
   void setProcType(unsigned int inValue);
   unsigned int procType();
   void setConvLimit(unsigned int inValue);
@@ -50,7 +51,8 @@ private:
   unsigned int m_procType;
   unsigned int m_convergenceLimit;
   unsigned int m_runType;
-  
+  VeloTELL1::dataVec m_devProcessInfo;
+
 };
 //
 inline bool VeloProcessInfo::isEnable(unsigned int proc)
@@ -59,8 +61,13 @@ inline bool VeloProcessInfo::isEnable(unsigned int proc)
     return ( (m_dataProcessInfo>>proc)&1 );
   }else if(m_procType==VeloTELL1::SIM_DATA){
     return ( *(m_simProcessInfo.begin()+proc) );
+  }else if(m_procType==VeloTELL1::DEV_DATA){
+    return ( *(m_devProcessInfo.begin()+proc) );
   }else{
-    std::cout<< " Unknown data type! " <<std::endl;
+    std::cout<< " ---------------------- " <<std::endl;
+    std::cout<< " ==> Unknown data type! " <<std::endl;
+    std::cout<< " ---------------------- " <<std::endl;
+    return ( false );
   }
 }
 //
@@ -72,6 +79,11 @@ inline void VeloProcessInfo::setDataProcessInfo(unsigned int inValue)
 inline void VeloProcessInfo::setSimProcessInfo(VeloTELL1::dataVec inVec)
 {
   m_simProcessInfo=inVec;
+}
+//  
+inline void VeloProcessInfo::setDevProcessInfo(VeloTELL1::dataVec inVec)
+{
+  m_devProcessInfo=inVec;
 }
 //  
 inline void VeloProcessInfo::setProcType(unsigned int inValue)
