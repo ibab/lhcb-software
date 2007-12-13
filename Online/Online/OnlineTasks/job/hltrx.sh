8b9a32b111eea380f6e4584aea82d21e92b645ce
Niko Neufeld <niko@cern.ch>
2007-12-13 09:24:33 +0000

    Improvements on standalone scripts (hostname, bigger alloc size, bigger socket bufs)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97361 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 964804fd588fe05e3543487809b58161fe2f754e

--- Online/Online/OnlineTasks/job/hltrx.sh
+++ Online/Online/OnlineTasks/job/hltrx.sh
@@ -5,7 +5,7 @@
 # Requires CMTPROJECTPATH set and setup.sh sourced in $ONLINETASKSROOT/cmt
 # 
 #
-if [ -z DIM_DNS_NODE ]; then
+if [ -z $DIM_DNS_NODE ]; then
 	echo "Please define DIM_DNS_NODE"
 	exit 1
 fi
@@ -14,10 +14,10 @@ pkill Gaudi.exe
 pkill test.exe
 pkill gentest.exe
 rm -f /dev/shm/bm_*  /dev/shm/sem.bm_* /dev/shm/TAN* /dev/shm/sem.TAN*
-
+HOST=/$(hostname --short | awk '{ print toupper($1) }')
 # message are send via the DIM Messageservice
-export MSGSVC=MessageSvc
-export MSGSVC=LHCb::DimMessageSvc
+#export MSGSVC=LHCb::MessageSvc
+export MSGSVC=LHCb::FmcMessageSvc
 
 # TAN - do not touch unless you know what you're doing
 export TAN_PORT YES
@@ -36,29 +36,29 @@ export gaudi_exe2="${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe libGaudiOnline.so O
 # Online tasks - be careful when changing the startup order 
  
 # MBMInit initializes the shared memory
-$MINITERM MEPInit@${HOST} -e "export UTGID=MEPInit ; ${gaudi_exe} -main=${ONLINETASKSROOT}/options/MEPInit.opts  -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
+$MINITERM MEPInit@${HOST} -e "export UTGID=${HOST}/MEPInit ; ${gaudi_exe} -main=${ONLINETASKSROOT}/options/MEPInit.opts  -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
 #
 # ErrorLogger (not required for running)
-$WIDETERM ErrorLogger@${HOST}    -e "export UTGID=ErrLog; ${gaudi_exe2} -opts=$GAUDIONLINEROOT/options/ErrorLogger.opts -main=$GAUDIONLINEROOT/options/Remap.opts " &
+$WIDETERM ErrorLogger@${HOST}    -e "export UTGID=${HOST}/ErrLog; ${gaudi_exe2} -opts=$GAUDIONLINEROOT/options/ErrorLogger.opts -main=$GAUDIONLINEROOT/options/Remap.opts " &
 #
-$MINITERM ErrServ@${HOST} -e "export UTGID=ErrServ ; ${gaudi_exe} -main=$GAUDIONLINEROOT/options/ErrorSrv.opts -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
+$MINITERM ErrServ@${HOST} -e "export UTGID=${HOST}/ErrServ ; ${gaudi_exe} -main=$GAUDIONLINEROOT/options/ErrorSrv.opts -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
 #
 sleep 2
 #
 #  Monitors:
 #
-$BIGTERM MBMMon@${HOST} -e "export UTGID=MBMMon ; ${gaudi_run} libOnlineKernel.so mbm_mon" &
+$BIGTERM MBMMon@${HOST} -e "export UTGID=${HOST}/MBMMon ; ${gaudi_run} libOnlineKernel.so mbm_mon" &
 #
 sleep 2
 
 # The following 2 processes are essential
-$MINITERM EvtProd@${HOST}   -e "export UTGID=EvtProd  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPConverter.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
-$MINITERM EvtHolder@${HOST} -e "export UTGID=EvtHolder ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPHolder.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$MINITERM EvtProd@${HOST}   -e "export UTGID=${HOST}/EvtProd  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPConverter.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$MINITERM EvtHolder@${HOST} -e "export UTGID=${HOST}/EvtHolder ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPHolder.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 #
 # At least one Consumer is needed - for example the (dummy) Moore process
 #
-$MINITERM Moore_0@${HOST}   -e "export UTGID=Moore_0  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
-$MINITERM Moore_1@${HOST}   -e "export UTGID=Moore_1  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$MINITERM Moore_0@${HOST}   -e "export UTGID=${HOST}/Moore_0  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$MINITERM Moore_1@${HOST}   -e "export UTGID=${HOST}/Moore_1  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 #$MINITERM Moore_2@${HOST}   -e "export UTGID=Moore_2  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 # $MINITERM Moore_3@${HOST}   -e "export UTGID= Moore_3  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 #
@@ -68,4 +68,4 @@ $MINITERM Moore_1@${HOST}   -e "export UTGID=Moore_1  ; ${gaudi_exe} -opt=$GAUDI
 #
 # Last not least the event-builder
 #
-$BIGTERM MEPRx@${HOST} -e "export UTGID=MEPRx ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/MEPRxSvc.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$BIGTERM MEPRx@${HOST} -e "export UTGID=${HOST}/MEPRx ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/MEPRxSvc.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
