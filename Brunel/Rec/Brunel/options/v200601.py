0d3110a2f9e99aa95b79adc5fd90c7c19bd9c538
Marco Cattaneo <marco.cattaneo@cern.ch>
2007-12-20 14:59:49 +0000

    v32r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30027 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f0d39bfbbe0a3706bb6bfa5300a43a5c2acc84ed

--- Brunel/Rec/Brunel/options/v200601.py
+++ Brunel/Rec/Brunel/options/v200601.py
@@ -8,4 +8,4 @@ from Gaudi.Configuration import *
 importOptions('$BRUNELOPTS/v200601.opts')
 
 ApplicationMgr( AppName    = 'Brunel',
-                AppVersion = 'v32r2' )
+                AppVersion = 'v32r3' )
