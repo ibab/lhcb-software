816092657309fe55b429b299633a8f33b1eae3fe
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-01-07 14:04:29 +0000

    Change in comments
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@63951 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5d66f018f71f2f170568e80b197b45441c038c4f

--- Phys/Phys/DaVinciKernel/Kernel/IEventTupleTool.h
+++ Phys/Phys/DaVinciKernel/Kernel/IEventTupleTool.h
@@ -1,10 +1,6 @@
-// $Id: IEventTupleTool.h,v 1.1 2007-12-18 15:10:17 pkoppenb Exp $
+// $Id: IEventTupleTool.h,v 1.2 2008-01-07 14:04:29 pkoppenb Exp $
 #ifndef DECAYTUPLE_IEVENTTUPLETOOL_H
 #define DECAYTUPLE_IEVENTTUPLETOOL_H  1
-
-// Include files
-// from STL
-#include <string>
 
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
@@ -14,41 +10,15 @@ static const InterfaceID IID_IEventTupleTool ( "IEventTupleTool", 1, 0 );
 namespace Tuples{
   class Tuple;
 };
-namespace LHCb {
-  class RecVertex;
-  class Particle;
-};
 
 // ==================================================
 // ==================================================
 
 /** @class IEventTupleTool
- *
- * \brief Base class interface for the tool used by the DecayTreeTuple
- * algorithm
- *
- * The fill method gets called once for every candidates. It gets the
- * tuple to fill as argument.
- *
- * The tuple can be filled with anything that Tuples::Tuple
- * instances can accept, yet the following usages are strongly advised:
- *
- * - Dont call the tuple->write() methode as this result in a
- undefined behavior.
  * 
- * - Avoid filling column with useless values like the propertime of a
- * pions
+ * Fill some variables into a tuple.
  *
- * - If you fill column only in the case some condition are verified,
- * be sure either to always fill it or never do so. If an ambiguity
- * comes in the decision for some column, returns a
- * StatusCode::FAILURE. This will prevent the whole candidate to be
- * written to the tuple.
- *
- * - Use meaningful column name and document them. If it happens that
- two columns have the same name, the job will raise a fatal error.
- *
- * \sa DecayTreeTuple IParticleTupleTool
+ * \sa IParticleTupleTool
  * 
  * @author Jeremie Borel
  * @date November 2007
@@ -58,6 +28,8 @@ class IEventTupleTool : virtual public IAlgTool {
   virtual ~IEventTupleTool(){};
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IEventTupleTool; }
+
+  //! Fill the tuple. 
   virtual StatusCode fill( Tuples::Tuple& )=0;
 };
 
--- Phys/Phys/DaVinciKernel/Kernel/IParticleTupleTool.h
+++ Phys/Phys/DaVinciKernel/Kernel/IParticleTupleTool.h
@@ -1,4 +1,4 @@
-// $Id: IParticleTupleTool.h,v 1.1 2007-12-18 15:10:18 pkoppenb Exp $
+// $Id: IParticleTupleTool.h,v 1.2 2008-01-07 14:04:29 pkoppenb Exp $
 #ifndef DECAYTUPLE_IPARTICLETUPLETOOL_H
 #define DECAYTUPLE_IPARTICLETUPLETOOL_H  1
 
@@ -7,8 +7,6 @@
 #include <string>
 
 // from Gaudi
-// #include "Event/Particle.h"
-// #include "Event/MCParticle.h"
 #include "GaudiKernel/IAlgTool.h"
 
 static const InterfaceID IID_IParticleTupleTool ( "IParticleTupleTool", 1, 0 );
@@ -17,52 +15,14 @@ namespace Tuples{
   class Tuple;
 };
 namespace LHCb {
-  class RecVertex;
   class Particle;
 };
 
 /** @class IParticleTupleTool
- *
- * \brief Base class interface for the tool used by the DecayTreeTuple
- * algorithm
- *
- * The fill method gets called for every particles of every
- * candidates. It has the following arguments:
- *
- * - \b top is a pointer to the top of the reconstructed decay. It
- allows to see where \b part is located in the structure.
-
- * - \b part is the particle to consider.
- *
- * - \b head is a prefix that should be applied to the column name
-     (e.g. tuple->column( head+"_PT", p->pt() ) );
- *
- * The tuple can be filled with anything that Tuples::Tuple
- * instances can accept, yet the following usages are strongly advised:
- *
- * - Dont call the tuple->write() methode as this result in a
- undefined behavior.
  * 
- * - Avoid filling column with useless values like the propertime of a
- * pions
- *
- * - If you fill column only in the case some condition are verified,
- * be sure either to always fill it or never do so. If an ambiguity
- * comes in the decision for some candidates, returns a
- * StatusCode::FAILURE. This will prevents the whole candidate to be
- * written to the tuple.
- *
- * - Use the head as prefix if the information you stored is indeed
- related to the particle. Nothing prevents you not doing so. If it
- happens that two columns have the same name, the job will raise a
- fatal error.
- *
- * - tulpe column names are in principle composed of the head, a '_'
- and a variable name. Stick as much as possible to 
- * https://twiki.cern.ch/twiki/bin/view/LHCb/LoKiParticleFunctions   
- *
- *
- * \sa DecayTreeTuple IEventTupleTool
+ * Fill some particle related variables into a tuple.
+ * 
+ * \sa IEventTupleTool
  * 
  * @author Jeremie Borel
  * @date November 2007
@@ -72,6 +32,13 @@ class IParticleTupleTool : virtual public IAlgTool {
   virtual ~IParticleTupleTool(){};
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IParticleTupleTool; }
+
+  //! Fill the tuple. 
+
+  //! - \b top  : may be used to provide additional information about \b part, like the top particle of the decay.
+  //! - \b part : the particle about which some info are filled.
+  //! - \b head : prefix for the tuple column name.
+  //! - \b tuple: the tuple to fill
   virtual StatusCode fill( const LHCb::Particle* top
 			   , const LHCb::Particle* part
 			   , const std::string& head
