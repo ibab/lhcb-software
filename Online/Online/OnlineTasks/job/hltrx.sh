a255b3f543db27d2badea01c691617c34f2a4e06
Markus Frank <frankb@cern.ch>
2008-01-09 13:10:00 +0000

    Fix options for v4r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97386 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 031eaeea1acea46888dd6af1d36b954cd3d35999

--- Online/Online/OnlineTasks/job/hltrx.sh
+++ Online/Online/OnlineTasks/job/hltrx.sh
@@ -28,29 +28,25 @@ export WIDETERM='xterm  -sl 10000 -ls -132 -geometry 160x50 -title '
 export MINITERM='xterm -sl 100000 -ls -132 -geometry 132x10 -title '
 
 # shortcuts for starting jobs
-export test_exe=${ONLINEKERNELROOT}/${CMTCONFIG}/test.exe
 export gaudi_run="${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe libGaudiOnline.so OnlineStart "
 export gaudi_exe="${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe libGaudiOnline.so OnlineTask -msgsvc=${MSGSVC} -auto"  
 export gaudi_exe2="${GAUDIONLINEROOT}/${CMTCONFIG}/Gaudi.exe libGaudiOnline.so OnlineTask -msgsvc=MessageSvc -auto"  
 
 # Online tasks - be careful when changing the startup order 
- 
-# MBMInit initializes the shared memory
-$MINITERM MEPInit@${HOST} -e "export UTGID=${HOST}/MEPInit ; ${gaudi_exe} -main=${ONLINETASKSROOT}/options/MEPInit.opts  -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
 #
 # ErrorLogger (not required for running)
-$WIDETERM ErrorLogger@${HOST}    -e "export UTGID=${HOST}/ErrLog; ${gaudi_exe2} -opts=$GAUDIONLINEROOT/options/ErrorLogger.opts -main=$GAUDIONLINEROOT/options/Remap.opts " &
-#
-$MINITERM ErrServ@${HOST} -e "export UTGID=${HOST}/ErrServ ; ${gaudi_exe} -main=$GAUDIONLINEROOT/options/ErrorSrv.opts -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
-#
-sleep 2
+$WIDETERM ErrorLogger@${HOST}    -e "export UTGID=${HOST}/ErrLog; /opt/FMC/bin/logViewer " &
+sleep 1
+# 
+# MBMInit initializes the shared memory
+$MINITERM MEPInit@${HOST} -e "export UTGID=${HOST}/MEPInit ; ${gaudi_exe} -main=${ONLINETASKSROOT}/options/MEPInit.opts  -opt=$GAUDIONLINEROOT/options/Daemon.opts " &
 #
 #  Monitors:
 #
 $BIGTERM MBMMon@${HOST} -e "export UTGID=${HOST}/MBMMon ; ${gaudi_run} libOnlineKernel.so mbm_mon" &
 #
 sleep 2
-
+#
 # The following 2 processes are essential
 $MINITERM EvtProd@${HOST}   -e "export UTGID=${HOST}/EvtProd  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPConverter.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 $MINITERM EvtHolder@${HOST} -e "export UTGID=${HOST}/EvtHolder ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/MEPHolder.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
@@ -59,13 +55,13 @@ $MINITERM EvtHolder@${HOST} -e "export UTGID=${HOST}/EvtHolder ; ${gaudi_exe} -o
 #
 $MINITERM Moore_0@${HOST}   -e "export UTGID=${HOST}/Moore_0  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 $MINITERM Moore_1@${HOST}   -e "export UTGID=${HOST}/Moore_1  ; ${gaudi_exe} -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
-#$MINITERM Moore_2@${HOST}   -e "export UTGID=Moore_2  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
-# $MINITERM Moore_3@${HOST}   -e "export UTGID= Moore_3  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+#$MINITERM Moore_2@${HOST}   -e "export UTGID=${HOST}/Moore_2  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+# $MINITERM Moore_3@${HOST}   -e "export UTGID=${HOST}/Moore_3  ; $gaudi_exe -opt=$GAUDIONLINEROOT/options/ReadMBM.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 #
 # Optional if you want write the data
 #
-#$MINITERM DiskWR@${HOST}    -e "export UTGID=DiskWR   ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/DiskWR.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+$MINITERM DiskWR@${HOST}    -e "export UTGID=${HOST}/DiskWR   ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/DiskWR.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
 #
 # Last not least the event-builder
 #
-$BIGTERM MEPRx@${HOST} -e "export UTGID=${HOST}/MEPRx ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/MEPRxSvc.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
+#$BIGTERM MEPRx@${HOST} -e "export UTGID=${HOST}/MEPRx ; ${gaudi_exe} -opt=$ONLINETASKSROOT/options/MEPRxSvc.opts -main=$GAUDIONLINEROOT/options/Main.opts "&
