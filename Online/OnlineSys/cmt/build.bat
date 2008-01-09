da8cc4cc9030595986f7baae645adcaead89a724
Markus Frank <frankb@cern.ch>
2008-01-09 16:09:03 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97392 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2085dc434995fbd16d25f9cd957b931460341c4

--- Online/OnlineSys/cmt/build.bat
+++ Online/OnlineSys/cmt/build.bat
@@ -3,4 +3,5 @@ cmt broadcast if exist ..\Visual rd /q/s ..\Visual
 cmt broadcast if exist ..\%CMTCONFIG% rd /q/s ..\%CMTCONFIG%
 cmt broadcast cmt config
 cmt broadcast cmt build vsnet
-cmt broadcast nmake /f nmake
+cmt broadcast nmake /f nmake > buid_%CMTCONFIG%.log
+
--- Online/OnlineSys/cmt/build.sh
+++ Online/OnlineSys/cmt/build.sh
@@ -12,6 +12,8 @@ do
   if test $i = "--clean"; then clean=1;
   elif test $i = "--config"; then config=1;
   elif test $i = "--quiet"; then quiet=1;
+  elif test $i = "--help"; then args=""; break;
+  elif test $i = "-h"; then args=""; break;
   else args=`echo $args $i`;
   fi;
 done;
@@ -35,12 +37,46 @@ then
             $show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast "${CMTROOT}/${CMTBIN}/cmt.exe config";
 	fi;
         if test -e build_${CMTCONFIG}.log; then $show rm -f build_${CMTCONFIG}.log; fi;
-	$show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast make >build_${CMTCONFIG}.log 2>&1;
+        if test -e build_${CMTCONFIG}.log.tmp; then $show rm -f build_${CMTCONFIG}.log.tmp; fi;
+	$show ${CMTROOT}/${CMTBIN}/cmt.exe broadcast make >build_${CMTCONFIG}.log.tmp 2>&1;
+        $show cat build_${CMTCONFIG}.log.tmp \
+	    | grep -v "${CMTCONFIG}.make ok" \
+	    | grep -v "CMTCONFIG=${CMTCONFIG}" \
+	    | grep -v "Makefile.header" \
+	    | grep -v "(constituents.make)" \
+	    | grep -v "dependencies.make" \
+	    | grep -v "setup.make" \
+	    | grep -v "installation done" \
+	    | grep -v ": library ok" \
+	    | grep -v ": Protos ok" \
+	    | grep -v "ar: creating " \
+	    | grep -v "do-merge" \
+	    | grep -v "input-file" \
+	    | grep -v install_more_includes \
+	    | grep -v "merged-file" \
+	    | grep -v "/merge_files.py" \
+	    | grep -v "/bin/genmap" \
+	    | grep -v merge_rootmap \
+	    | grep -v "Create Link to" \
+	    | grep -v "/bin/genreflex" \
+	    | grep -v "gccxmlpath" \
+	    | grep -v " merge_genconfDb " \
+	    | grep -v -x "Document[[:space:]]\+[a-zA-Z]*ConfDbMerge" \
+	    | grep -v -x "Document[[:space:]]\+[a-zA-Z]*RootMap" \
+	    | grep -v -x "Document[[:space:]]\+[a-zA-Z]*MergeMap" \
+	    > build_${CMTCONFIG}.log;
     done;
 else
-    echo "No platform specification given: slc4_ia32_gcc34_dbg";
-    echo "                                 slc4_ia32_gcc34";
-    echo "                                 slc4_amd64_gcc34_dbg";
-    echo "                                 slc4_amd64_gcc34";
-    echo "                                 ....";
+    echo "Usage: build.sh <option> [<option>] <platform specifications>";
+    echo "   Options:";
+    echo "        --config            execute cmt config";
+    echo "        --clean             remove target directory";
+    echo "        --quiet             execute in quiet mode";
+    echo "        --help              show this help";
+    echo " ";
+    echo "   Platform specifications: slc4_ia32_gcc34_dbg";
+    echo "                            slc4_ia32_gcc34";
+    echo "                            slc4_amd64_gcc34_dbg";
+    echo "                            slc4_amd64_gcc34";
+    echo "                            ....";
 fi;
