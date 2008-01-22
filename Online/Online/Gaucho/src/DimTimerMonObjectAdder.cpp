6eb26e9507f12b30fe7a62f4aadb8c4ecf8396fd
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2008-01-22 14:55:45 +0000

    Some files missing.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97422 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: aeedb68afc8dc60723768be1c9dfb86cc00ef82f

---
+++ Online/Online/Gaucho/src/DimTimerMonObjectAdder.cpp
@@ -0,0 +1,27 @@
+#include "DimTimerMonObjectAdder.h"
+#include "DimInfoMonObject.h"
+#include "DimServiceMonObject.h"
+#include "Gaucho/MonObject.h"
+#include "DimMonObjectManager.h"
+
+DimTimerMonObjectAdder::DimTimerMonObjectAdder (DimMonObjectManager* manager, int ttime, std::vector<std::vector<DimInfoMonObject*> > dimInfo2D, std::vector<DimServiceMonObject*> dimService1D) : DimTimer(ttime) {
+  m_dimMonObjectManager  = manager;
+  m_dimInfoMonObjectVect2D    = dimInfo2D;
+  m_dimServiceMonObjectVect1D = dimService1D;
+  m_time=ttime;
+  start(m_time);
+}
+
+void DimTimerMonObjectAdder::timerHandler(){
+  for (int i = 0; i < (int)m_dimServiceMonObjectVect1D.size(); i++) {
+    MonObject* monObjectAdder = m_dimServiceMonObjectVect1D[i]->monObject();
+    monObjectAdder->reset();
+    for (int j = 0; j < (int)m_dimInfoMonObjectVect2D[i].size(); j++) {
+      MonObject* monObject = m_dimInfoMonObjectVect2D[i][j]->monObject();
+      monObjectAdder->combine(monObject);
+    }
+  }
+  start(m_time);
+}
+
+
