1953941d193a872160c7e2e5fde9d2f1cd96f2fd
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-01-23 15:12:26 +0000

    o add more comments...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71238 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1fe878c06234ec6395f4d13e6dfb20a52519a0a5

--- Hlt/Hlt/HltBase/HltBase/IANNSvc.h
+++ Hlt/Hlt/HltBase/HltBase/IANNSvc.h
@@ -1,4 +1,4 @@
-// $Id: IANNSvc.h,v 1.2 2008-01-23 15:06:53 graven Exp $
+// $Id: IANNSvc.h,v 1.3 2008-01-23 15:12:26 graven Exp $
 #ifndef IANNSVC_H 
 #define IANNSVC_H 1
 
@@ -13,6 +13,7 @@
 /** @class IANNSvc IANNSvc.h
  *
  * ANN: Assigned Names and Numbers
+ *   or Assigned Numbers and Names
  *  
  *  functionality:
  *         Interface for obtaining names for numbers,
@@ -22,6 +23,11 @@
  *         The code relies on an invertable relation between
  *         names and numbers, i.e. it represent a 'bijective' 
  *         mapping.
+ *
+ *         Each mapping is labeled by a 'major' key,
+ *         i.e. for each major key, there exists an individual
+ *         map from a set of numbers to a set of names, and 
+ *         vice-versa.
  *
  *  @author Gerhard Raven
  *  @date   2007-01-02
@@ -42,13 +48,9 @@ public:
   virtual boost::optional<int>           asInt(   const major_key_type& major, const std::string& minor) const = 0;
   virtual boost::optional<std::string>   asString(const major_key_type& major, int minor) const = 0;
 
-
-
   virtual bool hasMajor(const major_key_type& major) const = 0;
   virtual std::vector< major_key_type > majors() const = 0;
 
-
-
-  virtual std::vector<minor_value_type> items(const std::string& major) const = 0;
+  virtual std::vector<minor_value_type> items(const major_key_type& major) const = 0;
 };
 #endif // IANNSVC_H
