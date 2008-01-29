3b4baaba55d2ae6d14c95b402042277c47a0caab
Antonio Perez-Calero Yzquierdo <antonio.perez.calero.yzquierdo@cern.ch>
2008-01-29 10:08:28 +0000

    added decay descriptor for Bs2DsMuNu in descriptors.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@71254 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e543fd9de86d27017c197e3d0b9a998105917b52

--- Analysis/Hlt/HltPython/python/modules/descriptors.py
+++ Analysis/Hlt/HltPython/python/modules/descriptors.py
@@ -3,7 +3,8 @@
 @brief Collection of decay descriptors and tools to run on different types of data
 @author Hugo RUIZ hugo.ruiz@cern.ch
 @author Elias LOPEZ elopez@ecm.ub.es
-@date 2007-12-16
+@author Antonio PEREZ-CALERO aperez@ecm.ub.es
+@date 2008-01-29
 """
 
 #---------------------------------------------------
@@ -22,27 +23,28 @@ KStar          = ' ([K*(892)0]cc -> [K+]cc [pi-]cc '+someGammas+' ) '
 #---------------------------------------------------
 # decayDescriptor dictionary
 decayDescriptorDict = {
-    'semilept'         : '{[<Xb>]cc   -> [mu+]cc..., [<Xb>]cc -> ([tau+]cc ->[ mu+]cc...)...}',
-    'Bd2DstarMuNu'     : '[B0]cc ->[{mu+ ,tau+ }]cc [{nu_mu , nu_tau}]cc '+threeBodyDstar+'...',# ... neded if  pi0 emitted
-    'Bd2D0Kstar'       : '[B0]cc   -> [D~0]cc '+KStar+someGammas , 
-    'Bs2MuMu'          : '[B_s0]cc -> [ mu+]cc [mu-]cc '+lotsOfGammas, # someGammas not enough
-    'Bd2KPi'           : '[B0]cc -> [K+]cc [pi-]cc '+someGammas,
-    'Bd2PiPi'          : '[ B0 ]cc -> [ pi+ ]cc [ pi- ]cc ' + someGammas,
-    'Bd2KK'            : '[ B0 ]cc -> [ K+ ]cc [ K- ]cc ' + someGammas,
+    'semilept'         : '{[<Xb>]cc  -> [mu+]cc..., [<Xb>]cc -> ([tau+]cc ->[ mu+]cc...)...}',
+    'Bd2DstarMuNu'     : '[B0]cc     ->[{mu+ ,tau+ }]cc [{nu_mu , nu_tau}]cc '+threeBodyDstar+'...',# ... neded if  pi0 emitted
+    'Bs2DsMuNu'        : '[B_s0]cc   ->[{mu+ ,tau+ }]cc [{nu_mu , nu_tau}]cc '+threeBodyDs+'...',# ... neded if  pi0 emitted
+    'Bd2D0Kstar'       : '[B0]cc     -> [D~0]cc '+KStar+someGammas , 
+    'Bs2MuMu'          : '[B_s0]cc   -> [ mu+]cc [mu-]cc '+lotsOfGammas, # someGammas not enough
+    'Bd2KPi'           : '[B0]cc     -> [K+]cc [pi-]cc '+someGammas,
+    'Bd2PiPi'          : '[ B0 ]cc   -> [ pi+ ]cc [ pi- ]cc ' + someGammas,
+    'Bd2KK'            : '[ B0 ]cc   -> [ K+ ]cc [ K- ]cc ' + someGammas,
     'Bs2PiPi'          : '[ B_s0 ]cc -> [ pi+ ]cc [ pi- ]cc ' + someGammas,
     'Bs2PiK'           : '[ B_s0 ]cc -> [ pi+ ]cc [ K- ]cc ' + someGammas,
     'Bs2KK'            : '[ B_s0 ]cc -> [ K+ ]cc [ K- ]cc ' + someGammas,
-    'Bd2KstarD2KSPiPi' : '[B0]cc-> '+threeBodyD0+KStar+someGammas,
-    'Bs2DsDs'          : '[B_s0]cc  :> '+threeBodyDs+threeBodyDs+someGammas, # => needed if Ds through Ds*
-    'Bs2DsPi'          : '[B_s0]cc  -> '+threeBodyDs+' [pi-]cc'+someGammas,
-    'Bs2PhiPhi'        : '[B_s0]cc  -> '+Phi+Phi+someGammas,
-    'Bu2KD2KSPiPi'     : '[B+]cc -> [K+]cc '+threeBodyD0+lotsOfGammas,
-    'Bu2KMuMu'         : '[B+]cc -> [K+]cc mu+ mu- '+lotsOfGammas, # someGammas not enough
-    'Bd2MuMuKstar'     : '[B0]cc ->  mu+ mu- '+KStar+lotsOfGammas, # someGammas not enough
-    'Bd2PiPiPi0'       : '[B0]cc -> [pi+]cc [pi-]cc pi0 '+someGammas,
-    'Bs2PhiGamma'      : '[B_s0]cc ->'+someGammas,
-    'Bs2PsiPhi'        : '[B_s0]cc -> '+Phi+JPsi+' '+someGammas,
-    'Bu2Kee'           : '[B+]cc -> [K+]cc e+ e- '+someGammas
+    'Bd2KstarD2KSPiPi' : '[B0]cc     -> '+threeBodyD0+KStar+someGammas,
+    'Bs2DsDs'          : '[B_s0]cc   -> '+threeBodyDs+threeBodyDs+someGammas, # => needed if Ds through Ds*
+    'Bs2DsPi'          : '[B_s0]cc   -> '+threeBodyDs+' [pi-]cc'+someGammas,
+    'Bs2PhiPhi'        : '[B_s0]cc   -> '+Phi+Phi+someGammas,
+    'Bu2KD2KSPiPi'     : '[B+]cc     -> [K+]cc '+threeBodyD0+lotsOfGammas,
+    'Bu2KMuMu'         : '[B+]cc     -> [K+]cc mu+ mu- '+lotsOfGammas, # someGammas not enough
+    'Bd2MuMuKstar'     : '[B0]cc     ->  mu+ mu- '+KStar+lotsOfGammas, # someGammas not enough
+    'Bd2PiPiPi0'       : '[B0]cc     -> [pi+]cc [pi-]cc pi0 '+someGammas,
+    'Bs2PhiGamma'      : '[B_s0]cc   ->'+someGammas,
+    'Bs2PsiPhi'        : '[B_s0]cc   -> '+Phi+JPsi+' '+someGammas,
+    'Bu2Kee'           : '[B+]cc     -> [K+]cc e+ e- '+someGammas
     }
 
 
