44bf8d7bfb96c4ea8dee9cd9e1b6c8359eb3ea73
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2008-02-05 14:20:53 +0000

    New files and changes for fast run changes.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97469 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5f264ba75266f0b0eef4e7a32572c0ef8a994e8f

---
+++ Online/Online/Gaucho/src/UpdateAndReset.cpp
@@ -0,0 +1,97 @@
+//	====================================================================
+//  UpdateAndReset.cpp
+//	--------------------------------------------------------------------
+//
+//	Package   : Gaucho
+//
+//	Author    : Eric van Herwijnen
+//
+//	====================================================================
+
+
+// Include files
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/LinkManager.h"
+#include "GaudiKernel/IDataProviderSvc.h"
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/ToolFactory.h"
+
+#include "RTL/rtl.h"
+
+#include "Event/RawEvent.h"
+#include "Event/RawBank.h"
+#include "Event/ODIN.h"
+#include "Event/RecHeader.h"
+
+#include "UpdateAndReset.h"
+#include "MonitorSvc.h"
+
+
+// Static Factory declaration
+DECLARE_ALGORITHM_FACTORY(UpdateAndReset)
+
+
+// Constructor
+//------------------------------------------------------------------------------
+UpdateAndReset::UpdateAndReset(const std::string& name, ISvcLocator* ploc)
+  : GaudiAlgorithm(name, ploc) {}
+
+//------------------------------------------------------------------------------
+StatusCode UpdateAndReset::initialize() {
+  //------------------------------------------------------------------------------
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  MsgStream msg(msgSvc(), name());    
+  return StatusCode::SUCCESS;
+}
+
+
+//------------------------------------------------------------------------------
+StatusCode UpdateAndReset::execute() {
+  //------------------------------------------------------------------------------
+  MsgStream         msg( msgSvc(), name() );
+  
+
+  LHCb::ODIN* odin=NULL;
+  struct tm *currentTimeInfo;
+  m_currentTime = time (NULL);
+  currentTimeInfo = localtime(&m_currentTime);
+
+
+  if (exist<LHCb::ODIN> ( LHCb::ODINLocation::Default ) ) {
+     odin=get<LHCb::ODIN> ( LHCb::ODINLocation::Default ); }
+  else {
+       msg << MSG::WARNING<< "No ODIN Bank found. Can't update DIM services. " <<endreq;   
+       return StatusCode::SUCCESS;
+  }     
+  int currentRunNumber = odin->runNumber();
+  if (currentRunNumber != m_runNumber)
+  { 
+     //fast or other run change
+     //update immediately and reset
+     updateAll();
+     resetHistos();
+     }
+  else {  
+     m_timerElapsed=getTimerElapsed();
+     msg << MSG::INFO << "getTimerElapsed " << m_timerElapsed << endreq;
+     //check if update flag is set
+     //update if so
+     if (m_timerElapsed) updateAll();
+  }
+  m_runNumber = currentRunNumber;
+  return StatusCode::SUCCESS;
+}
+
+
+//------------------------------------------------------------------------------
+StatusCode UpdateAndReset::finalize() {
+  //------------------------------------------------------------------------------
+  MsgStream msg(msgSvc(), name());
+  msg << MSG::INFO << "finalizing...." << endreq;
+
+  return StatusCode::SUCCESS;
+}
+
