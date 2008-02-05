d3002fa04a191afd9c9b969308a3355ff50abc96
Markus Frank <frankb@cern.ch>
2008-02-05 16:57:05 +0000

    Add tests script
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36304 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b2a09054b506ae4abcf73c1c942ee5cfbb490847

---
+++ LHCb/DAQ/MDF/cmt/runTests.py
@@ -0,0 +1,40 @@
+import os, sys, string
+
+exe = os.popen('cmt show macro_value GAUDI_install_scripts').readlines()[0][:-1]
+exe = exe + os.sep + '..' + os.sep + os.environ['CMTCONFIG']+os.sep+'bin'+os.sep+'Gaudi.exe'
+mdf = os.environ['MDFROOT']
+def nextTest(msg):
+  print msg
+  val = ''
+  try:
+    val=str(input('Continue [Y=CR/N]'))
+  except NameError, X:
+    val = 'N'
+  except SyntaxError, X:
+    return 1
+  except Exception, X:
+    print X.__class__, str(X)
+    val = 'N'
+  if len(val) > 0:
+    sys.exit(0)
+    return 0
+  return 1
+
+def execute(cmd):
+  print '-->',cmd
+  os.system(exe+" "+cmd)
+
+print 'gaudi executable is:',exe
+tests = []
+tests.append(mdf+"/options/CreateMDF.opts")
+tests.append(mdf+"/options/ReadMDF.opts")
+tests.append(mdf+"/options/CreateMIF.opts")
+tests.append(mdf+"/options/ReadMIF.opts")
+tests.append(mdf+"/options/CreatePOOL.opts")
+tests.append(mdf+"/options/ReadPOOL.opts")
+tests.append(mdf+"/options/CreateTAE.opts")
+tests.append(mdf+"/options/ReadTAE.opts")
+tests.append(mdf+"/options/ReadTAEPOOL.opts")
+for i in tests:
+  if nextTest(i)>0:
+    execute(i)
