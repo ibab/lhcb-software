e2d094244bfd68e21b578803cfc56e9534414439
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-02-07 17:54:35 +0000

    add new method
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@9272 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 44cf0e6497585177648596a881c66a9c67adf104

--- LHCb/Rich/RichKernel/RichKernel/RichHistoID.h
+++ LHCb/Rich/RichKernel/RichKernel/RichHistoID.h
@@ -5,7 +5,7 @@
  *  Header file for RICH utility class : RichHistoID
  *
  *  CVS Log :-
- *  $Id: RichHistoID.h,v 1.5 2007-02-26 13:52:01 cattanem Exp $
+ *  $Id: RichHistoID.h,v 1.6 2008-02-07 17:54:35 jonrob Exp $
  *
  *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
  *  @date   27/10/2005
@@ -23,6 +23,7 @@
 #include "Kernel/RichRadiatorType.h"
 #include "Kernel/RichDetectorType.h"
 #include "Kernel/RichParticleIDType.h"
+#include "Kernel/RichSide.h"
 
 namespace Rich
 {
@@ -75,7 +76,7 @@ namespace Rich
       return Rich::text(rad)+"/"+tag;
     }
 
-    /** Create histo ID for given radiator type and histogram tag
+    /** Create histo ID for given RICH detector and histogram tag
      *
      *  @param det RICH detector type
      *  @param tag Histogram tag
@@ -86,6 +87,21 @@ namespace Rich
                                           const std::string & tag ) const
     {
       return Rich::text(det)+"/"+tag;
+    }
+
+    /** Create histo ID for given RICH detector, HPD panel and histogram tag
+     *
+     *  @param det  RICH detector type
+     *  @param side The HPD panel
+     *  @param tag  Histogram tag
+     *
+     *  @return Full histogram identifier string
+     */
+    inline const std::string operator() ( const Rich::DetectorType det,
+                                          const Rich::Side side,
+                                          const std::string & tag ) const
+    {
+      return Rich::text(det)+"/"+Rich::text(det,side)+"/"+tag;
     }
 
     /** Create histo ID for given radiator type, mass hypothesis and histogram tag
