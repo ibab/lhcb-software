edfb124d4080ec1b1e580cc73f3845b166a7a0f2
Markus Frank <markus.frank@cern.ch>
2008-02-14 10:55:55 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97605 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e5fa38a58e6565ed19732eb7871e480167cd4c34

--- Online/Online/OnlineControls/scripts/LogCollect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-EXECS=/opt/FMC/sbin:/opt/FMC/bin:${PATH};
-export LD_LIBRARY_PATH=/opt/FMC/lib;
-the_items=`printenv | grep -v UTGID | grep -v LD_LIBRARY_PATH | grep -v DIM_DNS_NODE`
-eval `python -c "for i in \"\"\"$the_items\"\"\".split('\n'): print 'unset '+i.split('=')[0]"`
-export PATH=${EXECS}
-exec -a ${UTGID} /opt/FMC/bin/logViewer -l 3 -o /tmp/logsum.fifo
