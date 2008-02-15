fdb7087cf35b54fb254b4035dddc6d27c4922ba3
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-02-15 07:53:23 +0000

    add cistom dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36335 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bbe220b7e22059306a8360a49352f80bbb1b510

---
+++ LHCb/Event/PhysEvent/dict/lcgDict.h
@@ -0,0 +1,21 @@
+// $Id: lcgDict.h,v 1.1 2008-02-15 07:53:23 cattanem Exp $
+#ifndef PHYSEVENT_LCGDICT_H 
+#define PHYSEVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include <vector>
+#include "Event/Particle.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::vector<const LHCb::Particle*>  _i1;
+    // end instantiations
+  };
+}
+
+#endif // PHYSEVENT_LCGDICT_H
