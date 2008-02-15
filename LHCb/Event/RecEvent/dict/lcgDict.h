04b322d56833f16ac6b8ccd9759a672f4ec0b4a5
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-02-15 07:42:17 +0000

    add missing dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36334 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4975b6dfa85a1dbc842d481376eb495c7689f8d4

--- LHCb/Event/RecEvent/dict/lcgDict.h
+++ LHCb/Event/RecEvent/dict/lcgDict.h
@@ -1,18 +1,21 @@
-// $Id: lcgDict.h,v 1.1 2007-10-19 15:47:20 cattanem Exp $
+// $Id: lcgDict.h,v 1.2 2008-02-15 07:42:17 cattanem Exp $
 #ifndef RECEVENT_LCGDICT_H 
 #define RECEVENT_LCGDICT_H 1
 
 // Additional classes to be added to automatically generated lcgdict
 
 // begin include files
+#include <vector>
 #include "Event/RecVertex.h"
+#include "Event/VertexBase.h"
 
 // end include files
 
 namespace {
   struct _Instantiations {
     // begin instantiations
-    std::vector<const LHCb::RecVertex*> _i1;
+    std::vector<const LHCb::RecVertex*>  _i1;
+    std::vector<const LHCb::VertexBase*> _i2;
     // end instantiations
   };
 }
