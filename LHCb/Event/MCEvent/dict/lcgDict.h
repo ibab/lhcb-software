45ea21af7b35cae22da709f159456edf881f25cc
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-02-15 07:30:31 +0000

    add a dict previously in LoKiMC
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36333 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1183daaf8dafc917a86d09202eca0737c50138e8

--- LHCb/Event/MCEvent/dict/lcgDict.h
+++ LHCb/Event/MCEvent/dict/lcgDict.h
@@ -1,12 +1,15 @@
-// $Id: lcgDict.h,v 1.8 2008-02-13 15:56:17 cattanem Exp $
+// $Id: lcgDict.h,v 1.9 2008-02-15 07:30:31 cattanem Exp $
 #ifndef MCEVENT_LCGDICT_H 
 #define MCEVENT_LCGDICT_H 1
 
 // Additional classes to be added to automatically generated lcgdict
 
 // begin include files
+#include <utility>
+#include <vector>
 #include "Event/MCTrackInfo.h"
 #include "Event/MCParticle.h"
+#include "Event/MCVertex.h"
 
 // end include files
 
@@ -15,6 +18,7 @@ namespace {
     // begin instantiations
     std::vector<const LHCb::MCParticle*> _i1;
     std::pair<LHCb::MCParticle*,double>  _i2;
+    std::vector<const LHCb::MCVertex*>   _i3;
     // end instantiations
   };
 }
