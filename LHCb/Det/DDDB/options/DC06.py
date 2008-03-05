073ea003d269d1fbe414bb6d87d65ab9902a2944
Marco Clemencic <marcocle@cern.ch>
2008-03-05 15:47:37 +0000

    Fixed the option for MagneticFieldSvc.FieldMapFile.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36415 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 93dd638dbd252507177ba65bcbaf1d016be8cacb

--- LHCb/Det/DDDB/options/DC06.py
+++ LHCb/Det/DDDB/options/DC06.py
@@ -2,6 +2,7 @@ from Gaudi.Configuration import *
 # Defines standard conditions for DC06 production data
 
 from Configurables import CondDBAccessSvc, MagneticFieldSvc
+import os
 
 # DC06 database tags
 tag = "DC06-repro0710"
@@ -9,5 +10,5 @@ CondDBAccessSvc("DDDB",DefaultTAG = tag)
 CondDBAccessSvc("LHCBCOND",DefaultTAG = tag)
 
 # DC06 magnetic field
-MagneticFieldSvc(FieldMapFile = "$FIELDMAPROOT/cdf/field047.cdf")
-
+MagneticFieldSvc(FieldMapFile = 
+                 os.path.join(os.environ["FIELDMAPROOT"],"cdf","field047.cdf"))
