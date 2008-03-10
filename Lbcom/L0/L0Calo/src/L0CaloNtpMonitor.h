229e9aa7362825aeff1c9981f72056cdccde78b6
Patrick Robbe <robbe@lal.in2p3.fr>
2008-03-10 19:38:36 +0000

    New ntuple algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@59303 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 375803f0471429d2c606ca354a2d6a8062061321

---
+++ Lbcom/L0/L0Calo/src/L0CaloNtpMonitor.h
@@ -0,0 +1,36 @@
+// $Id: L0CaloNtpMonitor.h,v 1.1 2008-03-10 19:38:36 robbep Exp $
+#ifndef L0CALONTPMONITOR_H
+#define L0CALONTPNOMITOR_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTupleAlg.h"
+
+class DeCalorimeter ;
+
+/** @class L0CaloNtpMonitor L0CaloMonitor.h
+ *
+ *  @author Marie-Helene Schune
+ *  @date 2008-02-22
+ */
+ class L0CaloNtpMonitor : public GaudiTupleAlg {
+ public:
+  /// Standard constructor
+  L0CaloNtpMonitor( const std::string &name , ISvcLocator * pSvcLocator ) ;
+  
+  virtual ~L0CaloNtpMonitor( ) ; ///< Destructor
+  
+  virtual StatusCode initialize() ; ///< Algorithm initialization
+  virtual StatusCode execute   () ; ///< Algorithm execution
+  virtual StatusCode finalize  () ; ///< Algorithm finalization
+  
+protected:
+
+private: 
+  std::string              m_inputDataSuffix ;
+
+  DeCalorimeter * m_ecal            ; ///< Pointer to Ecal detector element
+  DeCalorimeter * m_hcal            ; ///< Pointer to Hcal detector element
+
+};
+#endif  // L0CALONTPMONITOR_H
