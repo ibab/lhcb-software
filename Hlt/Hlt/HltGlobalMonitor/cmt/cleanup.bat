1f96bf065a9f1ceb6ffd6e8123e30caab38841c5
Jose Angel Hernando Morata <jose.angel.hernando-morata@cern.ch>
2008-03-12 16:54:38 +0000

    removing unnecessary files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71575 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 44a2629e59bb4e884622a771471b6da9f8f7a1f2

--- Hlt/Hlt/HltGlobalMonitor/cmt/cleanup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if NOT DEFINED CMTROOT set CMTROOT=Z:\cern.ch\sw\contrib\CMT\v1r19& set PATH=%CMTROOT%\%CMTBIN%;%PATH%& set CMTBIN=VisualC& if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
-
-set cmttempfile="%TEMP%\tmpsetup.bat"
-%CMTROOT%\%CMTBIN%\cmt.exe cleanup -bat  -pack=HltCommon -version=v1r4 -path="%~d0%~p0..\..\.."   %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
-if exist %cmttempfile% call %cmttempfile%
-if exist %cmttempfile% del %cmttempfile%
-set cmttempfile=
-
