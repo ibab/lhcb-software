17c0ad6e7a1050363511b04de73b8b3cf9e05fb6
Johannes Albrecht <johannes.albrecht@cern.ch>
2008-03-20 11:34:51 +0000

    simple class to hold track hypothesis, to be used by the Tf::TStationHitManager
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71617 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7e8d0e1ee44745f38725fa78211ce9ab776db317

---
+++ Hlt/Hlt/HltL0Conf/src/LineHypothesis.h
@@ -0,0 +1,38 @@
+#ifndef LINEHYPOTHESIS_H 
+#define LINEHYPOTHESIS_H 1
+
+// Include files
+#include "TfKernel/TStationHitManager.h"
+
+/** @class LineHypothesis LineHypothesis.h
+ *
+ *  Helper class which gives a line hypothesis in x and
+ *  a straight line in y 
+ *  to be used for T-station decoding in the Tf framework
+ *  
+ *
+ *  @author Johannes Albrecht
+ *  @date   2008-01-18
+ */
+class LineHypothesis : virtual public Tf::IStationSelector {
+public: 
+  /// Standard constructor
+  LineHypothesis( float my , float cy , float mx , float cx,
+                  float dx, float dy ) :
+	  m_y(my), m_cy(cy), m_x(mx), m_cx(cx), m_dx(dx), m_dy(dy)
+  { }
+
+  Tf::XYSearchWindow searchWindow(float zz) const
+  {
+    float z = zz;
+    float x = m_x * z + m_cx;
+    float y = m_y * z + m_cy;
+    return Tf::XYSearchWindow(x - m_dx, x + m_dx, y - m_dy, y + m_dy);
+  }
+
+private:
+  float m_y, m_cy;
+  float m_x, m_cx;
+  float m_dx, m_dy;
+};
+#endif // LINEHYPOTHESIS_H
---
+++ Hlt/Hlt/HltL0Conf/src/ParabolaHypothesis.h
@@ -0,0 +1,38 @@
+#ifndef PARABOLAHYPOTHESIS_H 
+#define PARABOLAHYPOTHESIS_H 1
+
+// Include files
+#include "TfKernel/TStationHitManager.h"
+
+/** @class ParabolaHypothesis ParabolaHypothesis.h
+ *
+ *  Helper class which gives a parabola track hypothesis 
+ *  in x and a straight line in y 
+ *  to be used for T-station decoding in the Tf framework
+ *  
+ *
+ *  @author Johannes Albrecht
+ *  @date   2008-01-18
+ */
+class ParabolaHypothesis : virtual public Tf::IStationSelector {
+public: 
+  /// Standard constructor
+  ParabolaHypothesis( float my , float cy , float ax , float bx , float cx,
+                      float dx, float dy ) :
+	  m_y(my), m_cy(cy), m_ax(ax), m_bx(bx), m_cx(cx), m_dx(dx), m_dy(dy)
+  { }
+
+  Tf::XYSearchWindow searchWindow(double zz) const
+  {
+    float z = zz;
+    float y = m_y * z + m_cy;
+    float x = (m_ax * z + m_bx) * z + m_cx;
+    return Tf::XYSearchWindow(x - m_dx, x + m_dx, y - m_dy, y + m_dy);
+  }
+
+private:
+  float m_y, m_cy;
+  float m_ax, m_bx, m_cx;
+  float m_dx, m_dy;
+};
+#endif // PARABOLAHYPOTHESIS_H
