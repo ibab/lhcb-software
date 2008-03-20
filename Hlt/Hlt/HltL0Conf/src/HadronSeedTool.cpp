f539b70b111c7825c94391ceef16c8bb1da66e03
Johannes Albrecht <johannes.albrecht@cern.ch>
2008-03-20 14:17:16 +0000

    add option to run w/o calo decoding, tidy up code
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71630 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 021d31078293472ef742caa4e1a085d653b4279e

--- Hlt/Hlt/HltL0Conf/src/HadronSeedTool.cpp
+++ Hlt/Hlt/HltL0Conf/src/HadronSeedTool.cpp
@@ -1,5 +1,7 @@
-// $Id: HadronSeedTool.cpp,v 1.4 2007-11-16 11:06:50 albrecht Exp $
+// $Id: HadronSeedTool.cpp,v 1.5 2008-03-20 14:17:16 albrecht Exp $
 // Include files 
+
+#include <cmath>
 
 // from Gaudi
 #include "GaudiKernel/DeclareFactoryEntries.h" 
@@ -12,7 +14,6 @@
 
 // from Det
 #include "CaloDet/DeCalorimeter.h"
-
 #include "CaloDAQ/ICaloDataProvider.h"
 
 //boost
@@ -32,6 +33,10 @@ using namespace LHCb;
 // Declaration of the Tool Factory
 DECLARE_TOOL_FACTORY( HadronSeedTool );
 
+// static constants
+const double HadronSeedTool::zECal = 12830.;
+const double HadronSeedTool::zHCal = 13690.;
+const double HadronSeedTool::zT3 = 9315.;
 
 //=============================================================================
 // Standard constructor, initializes variables
@@ -41,14 +46,20 @@ HadronSeedTool::HadronSeedTool( const std::string& type,
                                 const IInterface* parent )
   : GaudiTool ( type, name , parent )
 {
-  //this is preliminary!!!
-  //declare search windows, tuned JA: 31-05-2007 
-  declareProperty("sigmaX2", m_sigmaX2 = boost::assign::list_of(441.)(441.)(1681.)(961.)(6889.) );
+  //resolutions w/o calo decoding --> only region 3,4 (HCal) sensible
+  declareProperty("l0SigmaX2", m_l0SigmaX2 = boost::assign::list_of(0.)(0.)(0.)(1849.)(8281.) );
+  declareProperty("l0SigmaY2", m_l0SigmaY2 = boost::assign::list_of(0.)(0.)(0.)(1521.)(6400.) );
+  declareProperty("l0SigmaTx2", m_l0SigmaTx2 = boost::assign::list_of(0.)(0.)(0.)(8.1e-5)(1.69e-4) );
+  declareProperty("l0SigmaTy2", m_l0SigmaTy2 = boost::assign::list_of(0.)(0.)(0.)(3.6e-5)(1.e-4) );
+  //improved resolutions with decoded calo cells
+  declareProperty("sigmaX2", m_sigmaX2 = boost::assign::list_of(441.)(441.)(1600.)(729.)(2601.) );
   declareProperty("sigmaY2", m_sigmaY2 = boost::assign::list_of(196.)(169.)(625.)(625.)(2704.) );
   declareProperty("sigmaTx2", m_sigmaTx2 = boost::assign::list_of(2.5e-5)(3.6e-5)(1.44e-4)(4.9e-5)(2.56e-4) );
   declareProperty("sigmaTy2", m_sigmaTy2 = boost::assign::list_of(4.9e-5)(1.e-4)(1.e-4)(6.4e-5)(1.44e-4) );
-  declareProperty("debugMode", m_debugMode = false );
 
+  declareProperty("debugMode", m_debugMode = false );
+  declareProperty("decodeCalos",m_decodeCalos = true );
+  
   declareInterface<ICaloSeedTool>(this);
 
 }
@@ -80,11 +91,18 @@ StatusCode HadronSeedTool::initialize()
   
   //tool for debug information
   m_DataStore = tool<L0ConfDataStore>("L0ConfDataStore");
+
+  m_l0ConfExtrapolator = tool<IL0ConfExtrapolator>("L0ConfExtrapolator");
   
-  cellSizeECal[0]=40.56; cellSizeECal[1]=60.85; cellSizeECal[2]=121.7;
-  zECal = 12830.;
-  zHCal = 13690.;
-  zT3 = 9315.;  
+  //check vectors set by job options
+  if( 5!=m_sigmaX2.size() || 5!=m_sigmaY2.size() 
+      || 5!=m_sigmaTx2.size() || 5!=m_sigmaTy2.size() 
+      || 5!=m_l0SigmaX2.size() || 5!=m_l0SigmaY2.size() 
+      || 5!=m_l0SigmaTx2.size() || 5!=m_l0SigmaTy2.size() ){
+    error()<<"Size of search winow vectors is not valid, check you options!"<<endmsg;
+    return StatusCode::FAILURE;
+  }
+  
   return StatusCode::SUCCESS;
 }
 
@@ -92,232 +110,227 @@ StatusCode HadronSeedTool::initialize()
 StatusCode HadronSeedTool::makeTrack(const LHCb::L0CaloCandidate& hadL0Cand,
                                      LHCb::Track& seedTrack )
 {
-  if(msgLevel(MSG::DEBUG)) debug()<<"HadronSeedTool::execute!"<<endmsg;
- 
-  //initialize caloDAQ for this event
-  m_ecalDaq->getBanks();
-  m_hcalDaq->getBanks();
+  if(msgLevel(MSG::DEBUG)) {
 
-  /* 
-   *   calculate barycenter of HCal cluster around seed
-   */
-  double XHCalBarycenter = 0; 
-  double YHCalBarycenter = 0;
-  double energyHCal = 0;
-  int region = 4; //0-2 for ECal, 3-4 for HCal
-  
-  StatusCode sc = getHCalBarycenter( hadL0Cand, energyHCal , XHCalBarycenter , YHCalBarycenter , region );
-  if( !sc ){ 
-    warning()<<"Failed to calculate 2x2 HCal cluster"<<endmsg;
-    return StatusCode::SUCCESS;
+    debug()<<"HadronSeedTool::execute!"<<endmsg;
+    debug()<<"Properties of L0 candidate:  ( Et, x , y ) = ( "
+           <<hadL0Cand.et()<<" , "<<hadL0Cand.position().X() <<" , "<<hadL0Cand.position().Y() <<endmsg;
   }
-  
-  //set the calo id to LHCbids of the track
-  seedTrack.addToLhcbIDs( LHCbID( hadL0Cand.id() ) );
-  
-  /*
-   *  if enought energy deposited in ECal, use this to get positions
-   */
-  double nECalCells = 0;    
-  double energyECal = 0.;
-  double XECalBarycenter = 0.;
-  double YECalBarycenter = 0.;
-  int tmpReg = 0;
-  
-  sc = getECalBarycenter( XHCalBarycenter , YHCalBarycenter , 
-                          nECalCells , energyECal  , XECalBarycenter , YECalBarycenter , tmpReg );
-  if( !sc ){ 
-    warning()<<"Failed to calculate 2x2 ECal cluster"<<endmsg;
-    return StatusCode::SUCCESS;
-  }
-  
 
-  //====================================================================================================
-  double xPos, yPos, zPos, oneOverP;
-  LHCb::State seedState;
+  // generate a state from L0 candidate information alone to make sure
+  // we have a fallback solution
+  double z = zHCal;
+  double x = hadL0Cand.position().X();
+  double y = hadL0Cand.position().Y();
+  double zL0 = hadL0Cand.position().Z();
+  double e = std::abs(hadL0Cand.et()) *
+	  std::sqrt(x * x + y * y + zL0 * zL0) / std::sqrt(x * x + y * y);
 
-  if( 2 < nECalCells ){
-    
-    region = tmpReg;
-    xPos = XECalBarycenter;
-    yPos = YECalBarycenter;
-    zPos = zECal;
-    oneOverP = 1./(energyECal+energyHCal);
-  }
-  else{ // use only HCAL
-    xPos = XHCalBarycenter;
-    yPos = YHCalBarycenter;
-    zPos = zHCal;
-    oneOverP = 1/(energyHCal);
+  //region:  0-2 for ECal, 3-4 for HCal
+  int region = m_l0ConfExtrapolator->getCaloRegion(x,y,z);
+  if( -1 == region ){
+    error()<<"calo region not valid, region = "<<region<<endmsg;
+    return StatusCode::FAILURE;
   }
   
   // set the covariance matrix
+  //set the resolution for L0 information first (no calo decoding)
   Gaudi::TrackSymMatrix stateCov = Gaudi::TrackSymMatrix();
-  stateCov(0,0) = m_sigmaX2[region];
-  stateCov(1,1) = m_sigmaY2[region];
-  stateCov(2,2) = m_sigmaTx2[region];
-  stateCov(3,3) = m_sigmaTy2[region];
+  stateCov(0,0) = m_l0SigmaX2[region];
+  stateCov(1,1) = m_l0SigmaY2[region];
+  stateCov(2,2) = m_l0SigmaTx2[region];
+  stateCov(3,3) = m_l0SigmaTy2[region];
   stateCov(4,4) = 8.41e-6;
-      
-
-  // set states and covariance matrix
-  seedState.setState(xPos,yPos,zPos,0,0, oneOverP);
-  seedState.setCovariance(stateCov);
-  seedState.setLocation(State::MidHCal);
-
-  if(m_debugMode) m_DataStore->region.push_back( region );
-
-  seedTrack.addToStates(seedState);
-
-  return sc;
   
+  if( m_decodeCalos ){
+    
+    // initialize caloDAQ for this event
+    m_hcalDaq->getBanks();
+    
+    // calculate barycenter of HCal cluster around seed
+    StatusCode sc = getHCalBarycenter( hadL0Cand, x, y, e, region );
+    if ( sc.isFailure() ) {
+      if ( -1 == region ) { 
+        error() << "Failed to calculate 2x2 HCal cluster or its region!" << endmsg;
+        return StatusCode::SUCCESS;
+      }
+      info() << "Falling back on position information from L0 candidate"<<endmsg;
+    } else {
+      // L0 candidate position was refined using hcal - let's see if the
+      // ecal helps to narrow down things even further
+      // want at least 2 ecal cells with valid data
+      m_ecalDaq->getBanks();
+      sc = getECalBarycenter( x, y, z, e, 2, region );
+      // we can just ignore the error code, because we are not guaranteed
+      // to find something in the ecal
+    }
+    //we have decoded Calos, set finer resolution
+    stateCov(0,0) = m_sigmaX2[region];
+    stateCov(1,1) = m_sigmaY2[region];
+    stateCov(2,2) = m_sigmaTx2[region];
+    stateCov(3,3) = m_sigmaTy2[region];
+
+  }//end if decode calos
+  
+  // add state to track
+  Gaudi::TrackVector stateVec(x, y, 0., 0., 1. / e);
+  LHCb::State state(stateVec, stateCov, z, State::MidHCal);
+  seedTrack.addToStates(state);
+  
+  //set the calo id to LHCbids of the track
+  seedTrack.addToLhcbIDs( LHCbID( hadL0Cand.id() ) );
+    
+  if (m_debugMode) {
+    m_DataStore->region.push_back( region );
+    m_DataStore->region.push_back( region );
+  }
+  
+  return StatusCode::SUCCESS; 
 }
 
+int HadronSeedTool::HCALpart(const LHCb::CaloCellID& cell) const
+{
+  if (!m_hcal->valid(cell)) return -1;
+  // HCal IP: cell size 131.3mm -> region 3, 
+  // HCal OP cell size 262.6mm -> region 4
+  int cellSize = int(m_hcal->cellSize(cell) / 100.);
+  if (0 >= cellSize || 3 <= cellSize) return -1;
+  else return cellSize + 2;
+}
 
-StatusCode HadronSeedTool::getHCalBarycenter( const LHCb::L0CaloCandidate& cand, double& clusterEnergy , 
-                                              double& xBarycenter , double& yBarycenter , int& reg )
+StatusCode HadronSeedTool::getHCalBarycenter(const LHCb::L0CaloCandidate& cand,
+                                             double& x, double& y, double& e,
+                                             int& reg ) const
 {
   // get the cand cell ID
   const LHCb::CaloCellID& candcellid = cand.id();
-  
+
   LHCb::CaloCellID caloId2(3,candcellid.area(),candcellid.row(),candcellid.col()+1);
   LHCb::CaloCellID caloId3(3,candcellid.area(),candcellid.row()+1,candcellid.col());
   LHCb::CaloCellID caloId4(3,candcellid.area(),candcellid.row()+1,candcellid.col()+1);
 
-  // first test that the candidate cell id is valid...
-  if( m_hcal->valid(candcellid) ){
-  
-    double energy = m_hcalDaq->digit( candcellid );
-    clusterEnergy += energy;
-    xBarycenter += energy*m_hcal->cellX(candcellid);
-    yBarycenter += energy*m_hcal->cellY(candcellid);
+  // figure out calo region - we may have to test two cells 
+  if (-1 == (reg = HCALpart(candcellid))) {
+    if (-1 == (reg = HCALpart(caloId4))) {
+      warning() << "cand cell and +1/+1 cell not valid! return!!!!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+  }
 
-    double cellSize = m_hcal->cellSize(candcellid);
-    if( cellSize > 100 && cellSize < 200 )  reg = 3;
-    else if( cellSize > 200 && cellSize < 300 ) reg = 4;
-    
+  const LHCb::CaloCellID* cands[4] = { &candcellid, &caloId2, &caloId3, &caloId4 };
+
+  // calculate barycenter
+  double xBarycenter = 0., yBarycenter = 0., clusterEnergy = 0.;
+  for (int i = 4; i--; ) {
+    if (!m_hcal->valid(*cands[i])) continue;
+    double energy = m_hcalDaq->digit(*cands[i]);
+    if (0. >= energy) continue;
+    clusterEnergy += energy;
+    xBarycenter += energy * m_hcal->cellX(*cands[i]);
+    yBarycenter += energy * m_hcal->cellY(*cands[i]);
   }
-  else if( m_hcal->valid(caloId4) ){
-    if(msgLevel(MSG::DEBUG)) debug()<<"cell id of first hcal cand is not valid!!!!"<<endmsg;
-    double cellSize = m_hcal->cellSize(caloId2);
-    if( cellSize > 100 && cellSize < 200 )  reg = 3;
-    else if( cellSize > 200 && cellSize < 300 ) reg = 4;
-  }
-  else{
-    warning()<<"cand cell and +1/+1 cell not valid! return!!!!"<<endmsg;
+
+  if ( 0. >= clusterEnergy ) {
+    info() << "HCAL cluster energy zero or negative!" << endmsg;
     return StatusCode::FAILURE;
   }
+  x = xBarycenter / clusterEnergy;
+  y = yBarycenter / clusterEnergy;
+  e = clusterEnergy;
   
-  double energy = m_hcalDaq->digit( caloId2 );
-  clusterEnergy += energy;
-  xBarycenter += energy*m_hcal->cellX(caloId2);
-  yBarycenter += energy*m_hcal->cellY(caloId2);
-  
-  energy = m_hcalDaq->digit( caloId3 );
-  clusterEnergy += energy;
-  xBarycenter += energy*m_hcal->cellX(caloId3);
-  yBarycenter += energy*m_hcal->cellY(caloId3);
-  
-  energy = m_hcalDaq->digit( caloId4 );
-  clusterEnergy += energy;
-  xBarycenter += energy*m_hcal->cellX(caloId4);
-  yBarycenter += energy*m_hcal->cellY(caloId4);
-    
-  xBarycenter = xBarycenter / clusterEnergy;
-  yBarycenter = yBarycenter / clusterEnergy;
-  
-  if(msgLevel(MSG::DEBUG)){
-    debug()<<"reconstruction in HCAl: \n ( E, x , y ) = ( "
-           <<clusterEnergy<<" , "<<xBarycenter <<" , "<<yBarycenter <<endmsg;
+  if ( msgLevel(MSG::DEBUG) ) {
+    debug() << "x, y, clusterE = "<< xBarycenter << ", " 
+            <<yBarycenter << ", " << clusterEnergy << endmsg;
   }
   
-
   return StatusCode::SUCCESS;
-  
 }
 
-
-StatusCode HadronSeedTool::getECalBarycenter( double xHCal , double yHCal , double& nCells , 
-                                              double&  clusterEnergy, double& xECal, double& yECal , int& region  )
+StatusCode HadronSeedTool::getECalBarycenter(double& x, double& y, double& z, double& e,
+                                             int minCells, int& region ) const
 {
+  static const double cellSizeECal[3] = { 40.56, 60.85, 121.7 };
   // extrapolate HCal position to ECal 
-  double xPredicted = xHCal*zECal/zHCal;
-  double yPredicted = yHCal*zECal/zHCal;
-  region = ECALpart(xPredicted,yPredicted);
-  if( 0 > region ) {
-    warning()<<"Problem with ECALPART, region = "<<region<<" , exit!"<<endmsg;
+  double xPred = x * zECal / z;
+  double yPred = y * zECal / z;
+  int reg = ECALpart(xPred, yPred);
+  // check that the result is valid
+  if( 0 > reg ) {
+    warning() << "Problem with ECALPART, region = " << reg << " , exit!"
+              << endmsg;
     return StatusCode::FAILURE;
   }
 
   // now get the ECAL cells around this point  
-  const Gaudi::XYZPoint centerpoint = Gaudi::XYZPoint(xPredicted,yPredicted,zECal);
+  const Gaudi::XYZPoint centerpoint = Gaudi::XYZPoint(xPred,yPred,zECal);
   LHCb::CaloCellID centerCell = m_ecal->Cell( centerpoint );
 
   std::vector<LHCb::CaloCellID> ecalCells;
-  //ecalCells.push_back(centerCell);
+  ecalCells.reserve(9);
   
   int cellArea = centerCell.area();
   int cellRow = centerCell.row();
   int cellCol = centerCell.col();
   
-  for( int iRow = 0 ; iRow < 3 ; iRow++ ){
-    for( int iCol = 0 ; iCol < 3 ; iCol++ ){
-      LHCb::CaloCellID tmpid( 2 , cellArea , cellRow-1+iRow  , cellCol-1+iCol );      
-      if( m_ecal->valid( tmpid ) ){
-        ecalCells.push_back( tmpid );
+  for ( int iRow = 0 ; iRow < 3 ; iRow++ ) {
+    for ( int iCol = 0 ; iCol < 3 ; iCol++ ) {
+      LHCb::CaloCellID tmpid( 2 , cellArea , cellRow-1+iRow  , cellCol-1+iCol );
+      // check that ecal id is valid and daq recorded positive energy
+      if( m_ecal->valid( tmpid ) && 0. < m_ecalDaq->digit(tmpid) ) {
+        // require cell to lie within 1 cell size to prediction
+        double cellX = m_ecal->cellX(tmpid);
+        double cellY = m_ecal->cellY(tmpid);
+        double cellSize = m_ecal->cellSize(tmpid);
+        if ( std::abs(cellX - xPred) < cellSizeECal[reg] &&
+             std::abs(cellY - yPred) < cellSizeECal[reg] &&
+             std::abs(cellSize - cellSizeECal[reg]) < 1 )
+          ecalCells.push_back( tmpid );
       }
     }
   }
   
-  if(msgLevel(MSG::DEBUG))  debug()<<"number of ecal cells: "<<ecalCells.size() <<endmsg;
-  
+  if (ecalCells.empty()) {
+    debug() << "No acceptable ECal cells found." << endmsg;
+    return StatusCode::FAILURE;
+  }
+  if (int(ecalCells.size()) < minCells) {
+    debug() << "Too few ECal cells: " << ecalCells.size() << " found, need "
+            << minCells << endmsg;
+    return StatusCode::FAILURE;
+  }
 
+  if ( msgLevel(MSG::DEBUG) )
+	  debug() << "number of ecal cells: " << ecalCells.size() << endmsg;
+  
+  // calulate barycenter
+  double xECal = 0., yECal = 0., clusterEnergy = 0.;
   std::vector<LHCb::CaloCellID>::const_iterator id;
-  for( id=ecalCells.begin(); ecalCells.end()!=id; ++id ){
-    
-    const LHCb::CaloCellID& cellid = (*id);
-    
-    if( m_ecal->valid(cellid) ){
-   
-      double cellE = m_ecalDaq->digit( cellid );
-      
-      if( 0 < cellE ){
-
-        double cellX = m_ecal->cellX(cellid);
-        double cellY = m_ecal->cellY(cellid);
-        double cellSize = m_ecal->cellSize(cellid);
-        
-        //require cell to lie within 1 cell size to prediction
-        if( fabs(cellX-xPredicted)                       < cellSizeECal[region]
-            && fabs(cellY-yPredicted)                    < cellSizeECal[region]
-            && fabs(cellSize - cellSizeECal[region])     < 1 ) {
-          
-          nCells++;
-          clusterEnergy+=cellE;
-          xECal+=cellX*cellE;
-          yECal+=cellY*cellE;
-    
-        }
-      }//cellE > 0
-    }//valid cell id
-  }//loop ecal digis
+  for ( id=ecalCells.begin(); ecalCells.end()!=id; ++id ) {
+    const LHCb::CaloCellID& cellid = *id;
+    double cellE = m_ecalDaq->digit( cellid );
+    clusterEnergy += cellE;
+    xECal += m_ecal->cellX(cellid) * cellE;
+    yECal += m_ecal->cellY(cellid) * cellE;
+  }
   
-  xECal=xECal/clusterEnergy;
-  yECal=yECal/clusterEnergy;
+  x = xECal / clusterEnergy;
+  y = yECal / clusterEnergy;
+  z = zECal;
+  e += clusterEnergy;
+  region = reg;
   
-  if(msgLevel(MSG::DEBUG)){
-    debug()<<"reconstruction in ECAl: \n ( E, x , y ) = ( "
-           <<clusterEnergy<<" , "<<xECal <<" , "<<yECal <<endmsg;
-  }  
+  if ( msgLevel(MSG::DEBUG) ) {
+    debug() << "reconstruction in ECAL: \n ( E, x , y ) = ( "
+            << clusterEnergy << " , " << xECal << " , " << yECal << endmsg;
+  }
   
   return StatusCode::SUCCESS;
 }
 
-int HadronSeedTool::ECALpart( double x , double y ){
-  
-  if     ( x==0. && y==0. )                       return -1;
-  else if( fabs(x) <  969.6 && fabs(y) <  727.2 ) return 0;
-  else if( fabs(x) < 1939.2 && fabs(y) < 1212.0 ) return 1;
-  else                                            return 2;
-
+int HadronSeedTool::ECALpart( double x , double y ) const
+{
+  if ( 0. == x && 0. == y ) return -1;
+  else if ( fabs(x) <  650. && fabs(y) <  650. ) return 0;
+  else if ( fabs(x) < 1940. && fabs(y) < 1450. ) return 1;
+  else return 2;
 }
