4deed5ada8b5cd1d626ede48edad6e761b19d870
jpalac <jpalac@cern.ch>
2008-03-26 13:46:37 +0000

    . Add doxygen documentation.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@71650 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ce6076d83265a013eeb2a46e6fe82647b0707f4

--- Analysis/MicroDST/MicroDSTAlgorithm/src/CopyRelatedMCParticles.h
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyRelatedMCParticles.h
@@ -1,4 +1,4 @@
-// $Id: CopyRelatedMCParticles.h,v 1.12 2008-03-20 13:38:36 jpalac Exp $
+// $Id: CopyRelatedMCParticles.h,v 1.13 2008-03-26 13:46:37 jpalac Exp $
 #ifndef COPYRELATEDMCPARTICLES_H 
 #define COPYRELATEDMCPARTICLES_H 1
 
@@ -17,6 +17,40 @@ class MCVertex;
 
 /** @class CopyRelatedMCParticles CopyRelatedMCParticles.h
  *  
+ * MicroDSTAlgorithm to clone the LHCb::MCParticles that are related to a
+ * set of LHCb::Particle from one TES location to a parallel one.
+ * It inherits the std::string properties InputLocation and OutputPrefix from
+ * MicroDSTCommon. The LHCb::Particles are taken from the TES location 
+ * defined by InputLocation, and their associated LHCb::MCParticles are
+ * searched using a Particle2MCLinker. The associated LHCb::MCParticles are
+ * then cloned and placed in a TES location derived from the original one 
+ * (typically "/Event/MC/Particles") with the value of OutputPrefix added
+ * after the leading "/Event".
+ *
+ * The actual cloning of individual LHCb::MCParticles is performed by the 
+ * ICloneMCParticle, the implementation of which is set by the property 
+ * IMCCloneMCParticle (default MCParticleCloner). The depth and content of the 
+ * cloning action depends on the implementation of the ICloneMCParticle.
+ * The algorithm also stores the associations between the input set of 
+ * LHCb::Particle and the cloned LHCb::MCParticles. This is stored in the form 
+ * of an LHCb::RelationWeighted2D<LHCb::Particle, LHCb::MCParticle, double> in
+ * a TES location defined by InputLocation + "/RelatedMCParticles".
+ * @see IMCCloneParticle
+ * @see MCParticleCloner
+ *
+ * <b>Example</b>: Clone LHCb::MCParticles associated to LHCb::Particles in 
+ * "/Event/MyLocation/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particles"  using an MCParticleCloner. 
+ * Copy associations to "/Event/MyLocation/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particles/RelatedMCParticles"
+ *
+ *  @code
+ *
+ *  // Add a CopyRelatedMCParticles instance to a selection sequence
+ *  SeqDC06selBd2Jpsi2MuMu_Kst2KPi.Members += {"CopyRelatedMCParticles/CopyMC"};
+ *  CopyMC.OutputPrefix = "MyLocation";
+ *  CopyMC.InputLocation = "MyLocation/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particles";
+ *  CopyMC.ICloneMCParticle = "MCParticleCloner" // this is the default anyway.
+ *  @endcode
+ * 
  *
  *  @author Juan PALACIOS juan.palacios@nikhef.nl
  *  @date   2007-11-02
