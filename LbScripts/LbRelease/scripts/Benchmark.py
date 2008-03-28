d20020563928f703a2c94417aa4f59ae5affa016
hmdegaud <hmdegaud@cern.ch>
2008-03-28 14:06:19 +0000

    Incomplete - task 8: Write Benchmark script
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13105 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbRelease/scripts/Benchmark.py
+++ LbScripts/LbRelease/scripts/Benchmark.py
@@ -1,70 +1,234 @@
 #!/usr/bin/env python
 
 import sys
+import os, re
 from optparse import OptionParser
 from threading import Thread
 from subprocess import Popen
+from shutil import copy, rmtree
+from time import time
+from math import sqrt
+
 
 class TimedJob(Thread):
     id = 0
-    def __init__(self, jobexe, joboptionfile, nbevent=None):
+    def __init__(self, jobexe, joboptionfile, nbevent=0, randomseed=0, outputdir=None):
         # constructor of parent class
         Thread.__init__(self)
         self._id = TimedJob.id
         TimedJob.id += 1
         self._exename = jobexe
-        self._optfile = joboptionfile
-        self.setNumberOfEvents(nbevent)
-        self.setOuputFile()
+        self._optfile = ""
+        self._workdir = "."
+        self._startdir = os.getcwd()
+        self._outfile = ""
+        self._outdir = outputdir
+        self.preparePool(joboptionfile, nbevent, randomseed)
         # output data
-        self._outfile = None        
+        self._startextime = None
+        self._stopextime = None
         self._extime = None
-        self._intime = None
+        self._intime = 0.0
         self._intevtusertime = None
         self._intevtclocktime = None
+        self._intevtmintime = None
+        self._intevtmaxtime = None
         self._intscale = None
-    def setOuputFile(self, outputfile=None):
-        if outputfile :
-            self._outfile = outputfile
-        else :
-            self._outfile = jobexe + "%s" % self._id + ".log"
+        self._status = 0
+    def setOuputFile(self):
+        self._outfile = os.path.join(self._workdir,self._exename + ".log")
     def setNumberOfEvents(self, nbevent):
         self._nbevent = nbevent
+        f = open(self._optfile,"a")
+        f.write("\n ApplicationMgr.EvtMax     = %s;\n" % nbevent)
+        f.close()
+    def setRandomSeed(self, randomseed):
+        self._randomseed = randomseed
+        f = open(self._optfile,"a")
+        f.write("\n RndmGenSvc.Engine.Seeds     = [ %s ] ; \n" % randomseed)
+        f.close()
+    def setOptFile(self,joboptionfile, nbevent, randomseed):
+        self._optfile = os.path.join(self._workdir, "bench.opts")
+        copy(joboptionfile, self._optfile)
+        self.setNumberOfEvents(nbevent)
+        self.setRandomSeed(randomseed)        
+    def preparePool(self, joboptionfile, nbevent, randomseed):
+        self._workdir = os.path.join(self._startdir, "pool_%04d" % self._id) 
+        if os.path.exists(self._workdir):
+            rmtree(self._workdir)
+        os.mkdir(self._workdir)
+        self.setOptFile(joboptionfile, nbevent, randomseed)
+        self.setOuputFile()
     def run(self):
+        os.chdir(self._workdir)
+        self._startextime = time()
         outfile = open(self._outfile, "w")
         p = Popen([self._exename, self._optfile],
                   stdout=outfile,
                   stderr=outfile,
                   close_fds=True)
+        self._status = os.waitpid(p.pid,0)[1]
+        outfile.close()
+        if self._status != 0 :
+            print "there was a problem with the job in thread %s: status was %s" % (self._id, self._status)
+        else :
+            self._stopextime = time()
+            self._extime = self._stopextime - self._startextime
+            os.chdir(self._startdir)
+            print "Job external event rate: %e (%d events in %e s)" % (self.externalRate(), self._nbevent, self._extime)
+            self.getInternalTime()
+            print "  Job internal event rate: %e (%d events in %e s)" % (self.internalRate(), self._nbevent, self._intime),
+            print "    Event time: mean %f min %f max %f" % ( self._intevtclocktime, self._intevtmintime, self._intevtmaxtime) 
     def externalTime(self):
         return self._extime
+    def externalRate(self):
+        if self._extime :
+            return self._nbevent/self._extime
+        else :
+            return 0.0 ;
     def internalTime(self):
         return self._intime
+    def getInternalTime(self):
+        _exponentexpr = "(e|E)[+-]?\d+"
+        _fracexpr = "\.\d+"
+        _intpartexpr = "\d+"
+        _pointfloatexpr = "(%s)?%s|(%s)\." % (_intpartexpr, _fracexpr, _intpartexpr)
+        _exponentfloatexpr = "(%s|%s)%s" % (_intpartexpr, 
+                                            _pointfloatexpr, 
+                                            _exponentexpr)
+        _floatnumberexpr = "(%s|%s)" % (_pointfloatexpr, _exponentfloatexpr)
+        thefile = open(self._outfile, "r")
+        p = re.compile(r"Auditor\.TIMER\s+INFO\s+EVENT\s+LOOP\s+\|\s*(?P<user>%s)\s*\|\s*(?P<clock>%s)\s*\|\s*(?P<min>%s)\s+(?P<max>%s)\s*\|\s*(?P<entries>%s)\s*\|\s*(?P<total>%s)"% (_floatnumberexpr, _floatnumberexpr, _floatnumberexpr, _floatnumberexpr, _intpartexpr, _floatnumberexpr))
+        for l in thefile:
+            m = p.search(l, 1)
+            if m :
+                self._intime = float(m.group("total"))
+                self._intevtclocktime = float(m.group("clock"))
+                self._intevtusertime = float(m.group("user"))
+                self._intevtmaxtime  = float(m.group("max"))
+                self._intevtmintime  = float(m.group("min"))
+        thefile.close()
+        return self._intime
+    def internalRate(self):
+        if self._intime :
+            return self._nbevent/self._intime
+        else :
+            return 0.0
+    def status(self):
+        return self._status
 
+        
+class Bench:
+    def __init__(self, jobexe, joboptionfile, nbevent=None, maxthreads=1):
+        self._jobexe = jobexe
+        self._startdir = os.getcwd()
+        self._nbevent = nbevent
+
+
+def RateMean(values):
+    total = 0.0
+    for x in values:
+        total += x
+    return total/len(values)
+
+def RateMeanError(values):
+    mean = RateMean(values)
+    total = 0.0
+    n = len(values)
+    for x in values:
+        total += (x-mean)*(x-mean)
+    if n > 1 :
+        total = total/((n-1)*n)
+    return sqrt(total)
+
+def totRate(values):
+    return RateMean(values)*len(values)
+
+def totRateError(values):
+    return RateMeanError(values)*len(values)
+
+def printResults(thread_list, nthr):
+    totextrate = totRate([s.externalRate() for s in thread_list])
+    totintrate = totRate([s.internalRate() for s in thread_list])
+    totexterr = totRateError([s.externalRate() for s in thread_list])
+    totinterr = totRateError([s.internalRate() for s in thread_list])
+    nbthread = len(thread_list)
+    print "Total external event rate for %s threads: %e +/- %e" % (nbthread, totextrate, totexterr)
+    print "Total internal event rate for %s threads: %e +/- %e" % (nbthread, totintrate, totinterr)
+    print "==================================================================================================="
+    if nbthread and nbthread != nthr :
+        ratio = float(nthr)/float(nbthread)
+        totextrate = totextrate * ratio 
+        totintrate = totintrate * ratio 
+        totexterr = totexterr * ratio 
+        totinterr = totinterr * ratio
+        print "Corrected values:"
+        print "Total external event rate for %s threads: %e +/- %e" % (nthr, totextrate, totexterr)
+        print "Total internal event rate for %s threads: %e +/- %e" % (nthr, totintrate, totinterr)
+        print "==================================================================================================="
+        
+        
+    
 
 def runBench(jobexe, joboptionfile, nbevent=None, nbthread=1):
-    job_threads = ( TimedJob(jobexe, joboptionfile, nbevent) for x in range(nbthread) )
+    job_threads = [ TimedJob(jobexe, joboptionfile, nbevent, x) for x in range(nbthread) ]
+    glstarttime = time()
     for s in job_threads :
         s.start()
     for s in job_threads :
         s.join()
+    glstoptime = time()
+    gltime = glstoptime - glstarttime
+    if gltime :
+        glrate = (nbthread*nbevent)/gltime
+    else :
+        glrate = 0.0
+    print "==================================================================================================="
+    print "Global event rate for %s threads: %e (%d events in %e s)" % (nbthread, glrate, nbthread*nbevent, gltime)
+    print "==================================================================================================="
+    print "Raw results:"
+    printResults(job_threads, nbthread)
+    valid_threads = filter(lambda x: not x.status(), job_threads)
+    print "Valid results:"
+    printResults(valid_threads, nbthread)
+
+
+def getNbProc():
+    n = 0
+    for line in open("/proc/cpuinfo", "r"):
+        if line.startswith("processor"):
+            n += 1
+    return n
 
 if __name__ == '__main__':
+
+    usage = "%prog [options] exe optfile"
+    version = "$Id: Benchmark.py,v 1.2 2008-03-28 14:06:19 hmdegaud Exp $"
+    
     parser = OptionParser(usage=usage, version=version)
+    parser.set_defaults(nbevent=1)
     parser.add_option("-n",
                       type="int",
                       dest="nbevent",
                       help="Number of Event to be processed")
+    parser.set_defaults(nbthread=0)
     parser.add_option("-t", "--threads",
                       type="int",
                       dest="nbthread",
                       help="Number of parallel threads")
+    parser.set_defaults(outputdir=".")
     parser.add_option("-o", "--output-dir",
-                      dest="outputfile",
-                      help="Initial random seed")
+                      dest="outputdir",
+                      help="results output directory")
     
 
     options, args = parser.parse_args(sys.argv[1:])
+
+    if options.nbthread == 0:
+        options.nbthread = getNbProc()
+
+    print "number of threads: %s" % options.nbthread
+
     
     runBench(args[0], args[1], options.nbevent, options.nbthread)
     
--- LbScripts/LbRelease/scripts/GaussBench.csh
+++ LbScripts/LbRelease/scripts/GaussBench.csh
@@ -4,19 +4,30 @@
 
 cd $TMPDIR
 mkdir soft run
-#source $LHCBSCRIPTS/setup_external.csh --lcg 54 Python
 cd soft
+rm -rf install_project.py
 wget http://lhcbproject.web.cern.ch/lhcbproject/dist/install_project.py
 setenv MYSITEROOT `pwd`
-set gaussver=v30r5
-python install_project.py -p Gauss -v $gaussver -b sl4_ia32_gcc34
-python install_project.py -p Gauss -v $gaussver -b sl4_amd64_gcc34
-
+python install_project.py -p Gaudi -v v19r7 -b slc4_ia32_gcc34
+python install_project.py -p Gaudi -v v19r7 -b slc4_amd64_gcc34
 source scripts/ExtCMT.csh
+setenv PYTHONPATH ${LHCBPYTHON}:${PYTHONPATH}
 setenv CMTPROJECTPATH $LHCBPROJECTPATH
 unsetenv CMTPATH
+source $LHCBSCRIPTS/setup_external.csh --lcg 54 Python
+
+
+
+set gaussver=v30r5
+python install_project.py -p Gauss -v $gaussver -b slc4_ia32_gcc34 -m do_config
+python install_project.py -p Gauss -v $gaussver -b slc4_amd64_gcc34 -m do_config
+python install_project.py -p Gauss -v $gaussver -b slc4_ia32_gcc34_dbg -m do_config
+python install_project.py -p Gauss -v $gaussver -b slc4_amd64_gcc34_dbg -m do_config
+
+setenv PYTHONPATH
 SetupProject Gauss $gaussver
 cd $TMPDIR/run
+rm -rf BenchMark.opts 
 cat > BenchMark.opts << EOF
 #include "\$GAUSSROOT/options/v200601.opts"
 
@@ -29,9 +40,21 @@ ApplicationMgr.EvtMax     = 3 ;
 
 EOF
 
-Gauss.exe BenchMark.opts
 
-unsetenv LD_LIBRARY_PATH
-unsetenv PYTHONPATH
+
+
+# Gauss.exe BenchMark.opts
+
+
+#Benchmark -n 1000 -t 1 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 2 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 3 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 4 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 5 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 6 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 7 Gauss.exe $TMPDIR/run/BenchMark.opts
+Benchmark -n 500 Gauss.exe $TMPDIR/run/BenchMark.opts
+#Benchmark -n 1000 -t 9 Gauss.exe $TMPDIR/run/BenchMark.opts
+
 
 # rm -rf $TMPDIR/*
