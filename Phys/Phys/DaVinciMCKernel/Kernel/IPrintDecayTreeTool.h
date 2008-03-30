130298f24fc0c86ae12aeec4caf1ab2efab98bd9
Vanya Belyaev <ivan.belyaev@cern.ch>
2008-03-30 18:00:53 +0000

    modify (in compatible way) IPrintDecayTreeTool interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64040 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 646111fffcc1abc2f77c1991947250cbe164bd8c

--- Phys/Phys/DaVinciMCKernel/Kernel/IPrintDecayTreeTool.h
+++ Phys/Phys/DaVinciMCKernel/Kernel/IPrintDecayTreeTool.h
@@ -1,41 +1,62 @@
-// $Id: IPrintDecayTreeTool.h,v 1.5 2008-02-11 12:33:48 jpalac Exp $
+// $Id: IPrintDecayTreeTool.h,v 1.6 2008-03-30 18:00:53 ibelyaev Exp $
+// ============================================================================
 #ifndef DAVINCIMCTOOLS_IPRINTDECAYTREETOOL_H
 #define DAVINCIMCTOOLS_IPRINTDECAYTREETOOL_H 1
-
+// ============================================================================
 // Include files
-// from STL
+// ============================================================================
+// STL&STL
+// ============================================================================
 #include <vector>
 #include <string>
-
-// from Gaudi
+// ============================================================================
+// GaudiKernel
+// ============================================================================
 #include "GaudiKernel/IAlgTool.h"
-
+// ============================================================================
+// DaVinciKernel
+// ============================================================================
+#include "Kernel/IPrintDecay.h"
+// ============================================================================
+// Event 
+// ============================================================================
 #include "Event/Particle.h"
 #include "Event/MCParticle.h"
 #include "Kernel/Particle2MCLinker.h"
-
+// ============================================================================
 // Declaration of the interface ID (interface id, major version, minor version)
-static const InterfaceID IID_IPrintDecayTreeTool("IPrintDecayTreeTool", 1, 0);
-
+static const InterfaceID IID_IPrintDecayTreeTool("IPrintDecayTreeTool", 2, 0);
+// ============================================================================
 /** @class IPrintDecayTreeTool IPrintDecayTreeTool.h Kernel/IPrintDecayTreeTool.h
  *  
  *
  *  @author Juan Palacios juancho@nikhef.nl
  *  @date   09/10/2007
  */
-class IPrintDecayTreeTool : virtual public IAlgTool {
+class IPrintDecayTreeTool : virtual public IPrintDecay
+{
 public:
-
-  /// Retrieve interface ID
-  static const InterfaceID& interfaceID() { return IID_IPrintDecayTreeTool; }
-
-  /// Print decay tree for a given particle
-  virtual void printTree( const LHCb::Particle* mother, 
-                          int maxDepth = -1 ) = 0;
-
-  /**
-   * Print side by side a decay tree for an MCParticle 
-   * and it's associated particles
+  // ==========================================================================
+  /** Print the decay tree for a given particle
+   *
+   *  @code
+   * 
+   *  IPrintDecay* tool = ... ;
+   *  const LHCb::Particle* B = ... ;
+   * 
+   *  tool -> printDecay ( B ) ; 
+   *
+   *  @endcode 
+   *  @see IPrintDecay 
+   *  @param mother the pointer to the particle 
+   *  @param maxDepth the maximal depth level
+   */
+  virtual void printTree
+  ( const LHCb::Particle* mother        , 
+    int                   maxDepth = -1 ) = 0;
+  // ==========================================================================
+  /** Print side by side a decay tree for an MCParticle 
+   *  and it's associated particles
    *
    * @param mother The MCParticle who's tree is to be printed
    * @param assoc Associator linking mother to reconstructed LHCb::Particles
@@ -44,13 +65,12 @@ public:
    *                 implementations.
    * @todo Does this print two side-=by side trees, or a tree
    * of side-by side items?
-  */
-  virtual void printTree( const LHCb::MCParticle* mother, 
-                          Particle2MCLinker* assoc,
-                          int maxDepth = -1) = 0;
-
-  /**
-   * Print side by side a decay tree for a Particle 
+   */
+  virtual void printTree
+  ( const LHCb::MCParticle* mother, 
+    Particle2MCLinker* assoc,
+    int maxDepth = -1) = 0;
+  /** Print side by side a decay tree for a Particle 
    * and it's associated MCParticles
    *
    * @param mother The Particle who's tree is to be printed
@@ -60,89 +80,85 @@ public:
    *                 implementations.
    * @todo Does this print two side-=by side trees, or a tree
    * of side-by side items?
-  */
-  virtual void printTree( const LHCb::Particle* mother, 
-                          Particle2MCLinker* assoc, 
-                          int maxDepth = -1 ) = 0;
-
-  /**
-   * Print the contents of an MCParticle::ConstVector as a decay tree.
+   */
+  virtual void printTree
+  ( const LHCb::Particle* mother, 
+    Particle2MCLinker* assoc, 
+    int maxDepth = -1 ) = 0;
+  /** Print the contents of an MCParticle::ConstVector as a decay tree.
    * Print the associated reconstructed Particles if available.
    *
    * @param particles The container of MCParticles to be printed.
    * @param assoc Associator linking mother to LHCb::MCParticles
    *
-  */
+   */
   virtual void printAsTree( const LHCb::MCParticle::ConstVector& particles,
                             Particle2MCLinker* assoc ) = 0;
-
-  /**
-   * Print the contents of an MCParticle keyed container as a decay tree.
-   * Print the associated reconstructed Particles if available.
+  /** Print the contents of an MCParticle keyed container as a decay tree.
+   *  Print the associated reconstructed Particles if available.
    *
    * @param particles The keyed container of MCParticles to be printed.
    * @param assoc Associator linking mother to LHCb::MCParticles
    *
-  */
-  virtual void printAsTree( const LHCb::MCParticles& particles,
-                            Particle2MCLinker* assoc ) = 0;
-
-  /**
-   * Print the contents of an Particle::ConstVector as a flat list.
-   * @param particles The vector of Particles to be printed.
-   *
    */
-  virtual void printAsList( const LHCb::Particle::ConstVector& particles ) = 0;
-
-  /**
-   * Print the contents of an Particle::ConstVector as a flat list.
-   * Print the associated MCParticles if available.
-   *
+  virtual void printAsTree
+  ( const LHCb::MCParticles& particles,
+    Particle2MCLinker* assoc ) = 0;
+  /** Print the contents of an Particle::ConstVector as a flat list.
    * @param particles The vector of Particles to be printed.
-   * @param assoc Associator linking mother to LHCb::MCParticles
+   */
+  virtual void printAsList
+  ( const LHCb::Particle::ConstVector& particles ) = 0;
+  /** Print the contents of an Particle::ConstVector as a flat list.
+   *  Print the associated MCParticles if available.
    *
-  */
-  virtual void printAsList( const LHCb::Particle::ConstVector& particles,
-                            Particle2MCLinker* assoc ) = 0;
-
-  /**
-   * Print the contents of an MCParticle::ConstVector as a flat list.
+   *  @param particles The vector of Particles to be printed.
+   *  @param assoc Associator linking mother to LHCb::MCParticles
+   */
+  virtual void printAsList
+  ( const LHCb::Particle::ConstVector& particles,
+    Particle2MCLinker* assoc ) = 0;
+  /** Print the contents of an MCParticle::ConstVector as a flat list.
    * Print the associated recunstructed Particles if available.
    *
    * @param particles The vector of MCParticles to be printed.
    * @param assoc Associator linking mother to LHCb::Particles
-   *
-  */
-  virtual void printAsList( const LHCb::MCParticle::ConstVector& particles,
-                            Particle2MCLinker* assoc ) = 0;
-
-  /**
-   * Print the contents of a Particle keyed container as a flat list.
-   * @param particles The keyed container of Particles to be printed.
-   *
    */
-  virtual void printAsList( const LHCb::Particles& particles ) = 0;
-
-  /**
-   * Print the contents of an Particle keyed container as a flat list.
-   * Print the associated MCParticles if available.
+  virtual void printAsList
+  ( const LHCb::MCParticle::ConstVector& particles,
+    Particle2MCLinker* assoc ) = 0;
+  /** Print the contents of a Particle keyed container as a flat list.
+   *  @param particles The keyed container of Particles to be printed.
+   */
+  virtual void printAsList
+  ( const LHCb::Particles& particles ) = 0;
+  /** Print the contents of an Particle keyed container as a flat list.
+   *  Print the associated MCParticles if available.
    *
    * @param particles The keyed container of Particles to be printed.
    * @param assoc Associator linking mother to LHCb::MCParticles
-   *
-  */
-  virtual void printAsList( const LHCb::Particles& particles,
-                            Particle2MCLinker* assoc ) = 0;
-
-  /**
-   * Print the contents of an MCParticle keyed container as a flat list.
+   */
+  virtual void printAsList
+  ( const LHCb::Particles& particles,
+    Particle2MCLinker* assoc ) = 0;
+  /* Print the contents of an MCParticle keyed container as a flat list.
    * Print the associated reconstructed Particles if available.
    *
    * @param particles The keyed container of MCParticles to be printed.
    * @param assoc Associator linking mother to LHCb::Particles
-   *
-  */
-  virtual void printAsList( const LHCb::MCParticles& particles,
-                            Particle2MCLinker* assoc ) = 0;
+   */
+  virtual void printAsList
+  ( const LHCb::MCParticles& particles,
+    Particle2MCLinker* assoc ) = 0;
+  // ==========================================================================
+public:
+  // ==========================================================================
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_IPrintDecayTreeTool; }
+  // ==========================================================================
 };
+// ============================================================================
+// The END 
+// ============================================================================
 #endif // DAVINCIMCTOOLS_IPRINTDECAYTREETOOL_H
+// ============================================================================
