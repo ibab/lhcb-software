52c7d1aeae003f7439be03c39b9f368aaeff4449
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2008-04-02 13:39:02 +0000

    get daughter tracks of a particle
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71693 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 73b9132a49bebf88765864b5e5dca9571ee71197

---
+++ Hlt/Hlt/HltCommon/src/HltTrackFromParticle.cpp
@@ -0,0 +1,121 @@
+// $Id: HltTrackFromParticle.cpp,v 1.4 2008-04-02 13:38:12 gligorov Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/DeclareFactoryEntries.h" 
+
+// from Event
+#include "HltTrackFromParticle.h"
+
+using namespace LHCb;
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : HltMCChecker
+//
+// 2007-06-04 : Jose A. Hernando
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( HltTrackFromParticle );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+HltTrackFromParticle::HltTrackFromParticle( const std::string& name, ISvcLocator* pSvcLocator)
+  : HltAlgorithm ( name , pSvcLocator )
+{
+
+  declareProperty( "ParticlesName" , m_particlesName = "");
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+HltTrackFromParticle::~HltTrackFromParticle() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode HltTrackFromParticle::initialize() {
+
+  info() << "Starts" << endmsg;
+  
+  StatusCode sc = HltAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  m_outputTracks = &(registerTSelection<LHCb::Track>(m_outputSelectionName));
+
+  saveConfiguration();
+  info() << "HltAlgorithm initialized";
+
+  return sc;
+
+  saveConfiguration();
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode HltTrackFromParticle::execute() {
+
+  StatusCode sc = HltAlgorithm::execute(); // must be executed first
+
+  //Check if particles exist
+  if (!exist<Particles>(m_particlesName)) return sc;
+
+  Particles* pars = get<Particles>(m_particlesName);
+  if (m_debug) {
+    if (pars == 0) verbose() << " no particles found! " << endreq;
+    else verbose() << " particles found " << pars->size() << endreq;
+  }  
+  if (pars == 0) return sc;
+  
+  m_outputTracks->clear();
+  for (Particles::iterator it = pars->begin(); it != pars->end(); ++it) {
+    loadParticle(**it);
+  }
+  
+  int ncan = m_outputTracks->size();
+  //candidateFound(ncan);
+  
+  debug() << " candidates found " << ncan << endreq;
+  if (m_debug)
+    printInfo(" tracks from particles ",*m_outputTracks);
+  
+  return sc;
+  
+}
+
+void HltTrackFromParticle::loadParticle(const Particle& par) {
+  verbose() << " loading " << par.pt() << endreq;
+  if (par.isBasicParticle()) {
+    const Track* track = par.proto()->track();
+    if (track)  m_outputTracks->push_back( const_cast<Track*>(track));
+    verbose() << " loading particle " << par.pt() 
+              << " as track " << track->pt() << endreq;
+  } else {
+    const SmartRefVector<Particle>& pars2 = par.daughters();
+    for (SmartRefVector<Particle>::const_iterator it2 = pars2.begin(); 
+         it2 != pars2.end(); ++it2) {
+      loadParticle(**it2);
+    }
+  }  
+}
+
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode HltTrackFromParticle::finalize() {
+
+  StatusCode sc = HltAlgorithm::finalize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+  
+  debug() << "==> Finalize" << endreq;
+
+  return  StatusCode::SUCCESS;
+}
+
