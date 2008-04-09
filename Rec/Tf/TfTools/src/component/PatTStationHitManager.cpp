18e4b5013af71a86ef98b14e442d22d3fef852c1
Stephanie Hansmann-Menzemer <menzemer@physi.uni-heidelberg.de>
2008-04-09 07:51:57 +0000

    move PatTStationHitManager.cpp and TsaTStationHitManger.cpp from Tsa/PatKernel to here
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@17760 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 46714b57517b1926ddd61417b1da243dd24f6599

---
+++ Rec/Tf/TfTools/src/component/PatTStationHitManager.cpp
@@ -0,0 +1,89 @@
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+
+//From TfKernel
+#include "TfKernel/STHit.h"
+#include "TfKernel/OTHit.h"
+#include "TfKernel/RecoFuncs.h"
+
+// local
+#include "PatKernel/PatTStationHitManager.h"
+
+
+DECLARE_TOOL_FACTORY( PatTStationHitManager );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+PatTStationHitManager::PatTStationHitManager( const std::string& type,
+                                              const std::string& name,
+                                              const IInterface* parent )
+  : Tf::TStationHitManager<PatForwardHit> ( type, name , parent )
+{
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+PatTStationHitManager::~PatTStationHitManager() { }
+
+void PatTStationHitManager::prepareHits() const
+{
+  if ( !allHitsPrepared() )   
+    sortMyHits();
+}
+
+
+void PatTStationHitManager::prepareHitsInWindow(const Tf::IStationSelector & selector) 
+{ 
+
+  Tf::TStationHitManager<PatForwardHit>::prepareHitsInWindow(selector);
+
+  sortMyHits();
+}
+
+void PatTStationHitManager::sortMyHits() const
+{
+  PatFwdHit* prevHit = NULL;
+
+  for (Tf::TStationID sta=0; sta<maxStations(); ++sta)
+  {
+    for (Tf::TLayerID lay=0; lay<maxLayers(); ++lay)
+    {
+      for (Tf::OTRegionID t=0; t<maxOTRegions(); ++t)
+      {
+
+        double lastCoord = -10000000.;
+
+        Tf::TStationHitManager<PatForwardHit>::HitRange range = hits(sta,lay,t);
+
+        for ( PatFwdHits::const_iterator itH = range.begin();
+              range.end() != itH; ++itH ) {
+          PatFwdHit* hit = *itH;
+          if ( hit->hit()->xAtYEq0() - lastCoord < 3. ) {
+            hit->setHasPrevious( true );
+            prevHit->setHasNext( true );
+          }
+          lastCoord = hit->hit()->xAtYEq0();
+          prevHit = hit;
+        }
+      
+	this->sortHits < Tf::increasingByXAtYEq0<PatForwardHit> > (sta,lay,t);
+      }
+
+      for (Tf::ITRegionID t=0; t<maxITRegions(); t++)
+      {
+        const int index = t+maxOTRegions();
+	Tf::TStationHitManager<PatForwardHit>::HitRange range = hits(sta,lay,index);
+        this->sortHits < Tf::increasingByXAtYEq0<PatForwardHit> > (sta,lay,index);
+      }
+    }
+  }
+}
+
+
+
+
