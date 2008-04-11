436d3603f86a4bd198629f720a7baa475a72cb06
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-04-11 13:10:50 +0000

    change default tag
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36460 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 997c060e7db0a94f24f0e5120e8b6579ad6f150d

--- LHCb/Det/DDDB/options/DC06.py
+++ LHCb/Det/DDDB/options/DC06.py
@@ -5,7 +5,7 @@ from Configurables import CondDBAccessSvc, MagneticFieldSvc
 import os
 
 # DC06 database tags
-tag = "DC06-repro0710"
+tag = "DC06-20080407"
 CondDBAccessSvc("DDDB",DefaultTAG = tag)
 CondDBAccessSvc("LHCBCOND",DefaultTAG = tag)
 
