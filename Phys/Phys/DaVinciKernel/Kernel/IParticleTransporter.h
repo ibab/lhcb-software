a5c914abdfeec1fbbb8d3ea1495f2bbb174cb2e5
jpalac <jpalac@cern.ch>
2008-04-15 14:17:01 +0000

    - Add public virtual method     StatusCode transportAndProject(const LHCb::Particle*,                                    const double znew,                                     LHCb::Particle& transParticle) = 0;    to IParticleTransporter interface.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64064 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 700a943a533a3e5706c676e1b9d38932c3a8dc0f

--- Phys/Phys/DaVinciKernel/Kernel/IParticleTransporter.h
+++ Phys/Phys/DaVinciKernel/Kernel/IParticleTransporter.h
@@ -1,4 +1,4 @@
-// $Id: IParticleTransporter.h,v 1.4 2007-06-11 08:07:31 cattanem Exp $
+// $Id: IParticleTransporter.h,v 1.5 2008-04-15 14:16:43 jpalac Exp $
 #ifndef KERNEL_IPARTICLETRANSPORTER_H 
 #define KERNEL_IPARTICLETRANSPORTER_H 1
 
@@ -17,9 +17,12 @@ static const InterfaceID IID_IParticleTransporter("IParticleTransporter",3,0);
  *
  *  @author Edgar de Oliveira
  *  @date   22/02/2002
+ *
  *  @author P. Koppenburg
  *  @date   16/12/2004
- *  -> Remove non const method. 
+ *
+ *  @author J. Palacios
+ *  @date   15/04/2008
  */
 
 class IParticleTransporter : virtual public IAlgTool {
@@ -30,7 +33,12 @@ public:
   /// Transport a Particle to specified z position.
   virtual StatusCode transport(const LHCb::Particle*, 
                                const double znew,
-                               LHCb::Particle &transParticle) = 0;
+                               LHCb::Particle& transParticle) = 0;
+
+  /// Transport and project a Particle to specified z position.
+  virtual StatusCode transportAndProject(const LHCb::Particle*, 
+                                         const double znew,
+                                         LHCb::Particle& transParticle) = 0;
 
 };
 #endif // KERNEL_IPARTICLETRANSPORTER_H
