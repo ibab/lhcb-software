4708269d15973d18dc9ddc225c470ca2142f0f61
janos <janos@cern.ch>
2008-04-15 19:34:01 +0000

    o fixed in Gaudi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88458 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 550a370a12ed166398cd254d4f463ee90ebc858f

--- Alignment/Alignment/TAlignment/python/FixGaudiOpts.py
+++
@@ -1,6 +0,0 @@
-import ProcessJobOptions
-
-import GaudiKernel.ProcessJobOptions
-
-GaudiKernel.ProcessJobOptions.JobOptsParser = ProcessJobOptions.JobOptsParser
-GaudiKernel.ProcessJobOptions._parser = ProcessJobOptions._parser = GaudiKernel.ProcessJobOptions.JobOptsParser()
