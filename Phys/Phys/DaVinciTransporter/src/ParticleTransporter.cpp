a5d4484f4790a9c0725ee329064ce25e7dbf61ad
jpalac <jpalac@cern.ch>
2008-04-15 15:45:33 +0000

    .  split transport methods into method for neutrals or composites, plus     private helper methods     StatusCode transportChargedBasic(const LHCb::Particle*,                                      const double zNew,                                      LHCb::Particle& transParticle);     and     StatusCode checkParticle(const LHCb::Particle& transParticle);   . Add implementation for transportandProject method if IParticletransporter.   . Add doxygen documentation.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64068 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 67e8ca9135c13a10990f65e8275b4e8a1dc6236e

--- Phys/Phys/DaVinciTransporter/src/ParticleTransporter.cpp
+++ Phys/Phys/DaVinciTransporter/src/ParticleTransporter.cpp
@@ -1,4 +1,4 @@
-// $Id: ParticleTransporter.cpp,v 1.20 2008-03-10 11:55:17 ibelyaev Exp $
+// $Id: ParticleTransporter.cpp,v 1.21 2008-04-15 15:45:33 jpalac Exp $
 // Include files 
 
 // from Gaudi
@@ -28,14 +28,15 @@ ParticleTransporter::ParticleTransporter( const std::string& type,
                                           const std::string& name,
                                           const IInterface* parent )
   : GaudiTool ( type, name , parent )
-  , m_tracks()
+  , m_trackExtrapolator()
   , m_ppSvc(0)
-  , m_p2s()
+  , m_particle2State()
   , m_eID(0)
 {
   declareInterface<IParticleTransporter>(this);
 
-  declareProperty("TrackExtrapolator", m_tracksName = "TrackParabolicExtrapolator");
+  declareProperty("TrackExtrapolator", 
+                  m_trackExtrapolatorName = "TrackParabolicExtrapolator");
 }
 //=============================================================================
 // Destructor
@@ -49,15 +50,15 @@ StatusCode ParticleTransporter::initialize(){
   StatusCode sc = GaudiTool::initialize();
   if (!sc) return sc;
   
-  if ( m_tracksName != "" ){
-    debug() << "Using the " << m_tracksName 
+  if ( m_trackExtrapolatorName != "" ){
+    debug() << "Using the " << m_trackExtrapolatorName 
             << " tool to extrapolate particles from tracks" << endmsg;
-    m_tracks = tool<ITrackExtrapolator>(m_tracksName,this);
+    m_trackExtrapolator = tool<ITrackExtrapolator>(m_trackExtrapolatorName,this);
   } else warning() << "No TrackExtrapolator given for tracks" << endmsg ;
 
   m_ppSvc = svc<IParticlePropertySvc>("ParticlePropertySvc", true);
 
-  m_p2s = tool<IParticle2State>("Particle2State"); // not private
+  m_particle2State = tool<IParticle2State>("Particle2State"); // not private
 
   // a complicated way of getting 11
   ParticleProperty* pe = m_ppSvc->find("e-");
@@ -66,67 +67,120 @@ StatusCode ParticleTransporter::initialize(){
   return sc;
 }
 //=============================================================================
-// transport method
+// transport methods
 //=============================================================================
 StatusCode ParticleTransporter::transport(const LHCb::Particle* P, 
-                                          const double znew,
+                                          const double zNew,
                                           LHCb::Particle& transParticle){
   StatusCode sc = StatusCode::SUCCESS;
   debug() << "Transport PID " << P->particleID().pid() 
           << " p " << P->momentum() << " from " 
-          << P->referencePoint()  << " to " << znew << endmsg ;
+          << P->referencePoint()  << " to " << zNew << endmsg ;
+  
+  // avoid some "extra" self-assignements:
+  if ( &transParticle != P ) { transParticle = LHCb::Particle(*P) ; }
+
+  if ( ! (P->isBasicParticle()) ) {
+    verbose() << "Using DaVinciTransporter::transportComposite" << endmsg;
+    sc = DaVinciTransporter::transportComposite(P, zNew, transParticle);
+    if (!sc) return sc;
+  } else if (P->charge()==0. ) {
+    verbose() << "Using DaVinciTransporter::transportNeutralBasic" << endmsg;
+    sc = DaVinciTransporter::transportNeutralBasic(P, zNew, transParticle);
+    if (!sc) return sc;
+  } else {
+    sc = transportChargedBasic(P, zNew, transParticle);
+    if (!sc) return sc;
+  }
+
+  sc = checkParticle(transParticle);
+  
+  return sc;
+}
+//=============================================================================
+StatusCode ParticleTransporter::transportAndProject(const LHCb::Particle* P, 
+                                                    const double zNew,
+                                                    LHCb::Particle& transParticle){
+  StatusCode sc = StatusCode::SUCCESS;
   
   // avoid some "extra" self-assignements:
   if ( &transParticle != P ) { transParticle = LHCb::Particle(*P) ; }
   
-  
   if ( ! (P->isBasicParticle()) ) {
-    verbose() << "Using DaVinciTransporter::transportComposite" << endmsg;
-    sc = DaVinciTransporter::transportComposite(P, znew, transParticle);
+    sc = DaVinciTransporter::transportAndProjectComposite(P, 
+                                                          zNew, 
+                                                          transParticle);
     if (!sc) return sc;
   } else if (P->charge()==0. ) {
-    verbose() << "Using DaVinciTransporter::transportNeutralBasic" << endmsg;
-    sc = DaVinciTransporter::transportNeutralBasic(P, znew, transParticle);
+    sc = DaVinciTransporter::transportAndProjectNeutralBasic(P, 
+                                                             zNew, 
+                                                             transParticle);
     if (!sc) return sc;
   } else {
-    if (msgLevel(MSG::VERBOSE)){
-      sc = m_p2s->test(*P);
-      if (!sc) return sc;
-    }
-
-    LHCb::State s ; // state to extrapolate
-    sc = state(P,znew,s);
-    if (!sc) return sc;
-    if (NULL==m_tracks){
-      err() << "No extrapolator defined" << endmsg ;
-      return StatusCode::FAILURE ;      
-    }
-    sc = m_tracks->propagate(s,znew,P->particleID());
-    if (!sc) return sc;
-
-    verbose() << "Extrapolated state is" << endmsg;
-    verbose() << s << endmsg ;
-
-    sc = m_p2s->state2Particle(s,transParticle);
+    sc = transportChargedBasic(P, zNew, transParticle);
     if (!sc) return sc;
   }
+
+  sc = checkParticle(transParticle);
+
+  return sc;
+}
+//=============================================================================
+StatusCode 
+ParticleTransporter::transportChargedBasic(const LHCb::Particle* P, 
+                                           const double zNew,
+                                           LHCb::Particle& transParticle)
+{
+
+  StatusCode sc = StatusCode::SUCCESS;
+
+  if (msgLevel(MSG::VERBOSE)){
+    sc = m_particle2State->test(*P);
+    if (!sc) return sc;
+  }
+
+  LHCb::State s ; // state to extrapolate
+  sc = state(P,zNew,s);
+  if (!sc) return sc;
+  if (0==m_trackExtrapolator){
+    err() << "No extrapolator defined" << endmsg ;
+    return StatusCode::FAILURE ;      
+  }
+  sc = m_trackExtrapolator->propagate(s,zNew,P->particleID());
+
+  if (!sc) return sc;
+
+  verbose() << "Extrapolated state is" << endmsg;
+  verbose() << s << endmsg ;
+
+  sc = m_particle2State->state2Particle(s,transParticle);
+  
+  return sc;
+
+}
+//=============================================================================
+StatusCode 
+ParticleTransporter::checkParticle(const LHCb::Particle& transParticle)
+{
+  
   if ( lfin(transParticle.momentum().E() )){
-    verbose() << "Obtained Particle " << transParticle.particleID().pid() << endmsg;
+    verbose() << "Obtained Particle " 
+              << transParticle.particleID().pid() << endmsg;
     verbose() << transParticle << endmsg ;  
-    debug() << "Transported " << P->particleID().pid() << " " << transParticle.momentum() << " to " 
+    debug() << "Transported " << transParticle.particleID().pid() 
+            << " " << transParticle.momentum() << " to " 
             << transParticle.referencePoint() << endmsg ;
   } else {
     Warning("Transported Particle gets infinite momentum. Check Track states used.");
     return StatusCode::FAILURE ;
   }
-  
-  
-  return sc;
+
+  return StatusCode::SUCCESS;
 }
 //=============================================================================
 // get a state from a Particle
 //=============================================================================
-StatusCode ParticleTransporter::state(const LHCb::Particle* P, const double znew, LHCb::State& s) const { 
+StatusCode ParticleTransporter::state(const LHCb::Particle* P, const double zNew, LHCb::State& s) const { 
   // charged basic: no need to make 
   verbose() << "Starting from Particle \n " << *P << endmsg ;
 
@@ -134,26 +188,26 @@ StatusCode ParticleTransporter::state(const LHCb::Particle* P, const double znew
   
   if (P->charge()!=0 && P->isBasicParticle()){
     // Particles from MCParticles
-    if (NULL==P->proto()){
+    if (0==P->proto()){
       ParticleProperty *pp = m_ppSvc->findByPythiaID(P->particleID().pid());
       if (0!=pp) Warning(pp->particle()+" has no proto nor endVertex. Assuming it's from MC.", 
                          StatusCode::SUCCESS) ;
       else err() << "Particle with unknown PID " << P->particleID().pid() << " has no endVertex. " 
                  <<  "Assuming it's from MC" << endmsg ;
-      sc = m_p2s->particle2State(*P,s);
+      sc = m_particle2State->particle2State(*P,s);
     } else if (m_eID==P->particleID().abspid()){
       // Electrons. We don't want to lose Bremsstrahlung correction
       verbose() << "Special treatment for electrons" << endmsg ;
-      sc = m_p2s->particle2State(*P,s);
-    } else if (NULL==P->proto()->track()){
+      sc = m_particle2State->particle2State(*P,s);
+    } else if (0==P->proto()->track()){
       // Charged protopraticle without track -> error
       err() << "Basic Particle of ID " << P->particleID().pid() << " has no track" << endmsg;
       return StatusCode::FAILURE; 
     } else {
       // That's fine
-      s = P->proto()->track()->closestState(znew);
+      s = P->proto()->track()->closestState(zNew);
       if (msgLevel(MSG::DEBUG)) {
-        debug() << "Getting state closest to " << znew << " at "
+        debug() << "Getting state closest to " << zNew << " at "
                 << s.position().z() << " : " << s.stateVector() << endmsg ;
         debug() << "Track has " << P->proto()->track()->nStates() << ". First at "
                 << P->proto()->track()->firstState().position().z() << " : " 
@@ -161,7 +215,7 @@ StatusCode ParticleTransporter::state(const LHCb::Particle* P, const double znew
       }      
     }
   } else { // make a state
-    return m_p2s->particle2State(*P,s);
+    return m_particle2State->particle2State(*P,s);
   }
   verbose() << "Got state: \n" << s << endmsg ;
 
--- Phys/Phys/DaVinciTransporter/src/ParticleTransporter.h
+++ Phys/Phys/DaVinciTransporter/src/ParticleTransporter.h
@@ -1,4 +1,4 @@
-// $Id: ParticleTransporter.h,v 1.7 2007-08-07 15:55:56 pkoppenb Exp $
+// $Id: ParticleTransporter.h,v 1.8 2008-04-15 15:45:33 jpalac Exp $
 #ifndef PARTICLETRANSPORTER_H 
 #define PARTICLETRANSPORTER_H 1
 
@@ -13,13 +13,33 @@ class IParticleStuffer;
 class IParticlePropertySvc ;
 /** @class ParticleTransporter ParticleTransporter.h
  *  
- *  Particle transporter calling 
- *  the master track extrapolator for Particles made from Tracks,
- *  the parabolic track extrapolator for charged composites
- *  and the linear track extrapolator for neutrals.
+ *  Tool to transport an LHCb::Particle to a new Z position.
+ *  For composite or neutral particles, there is a choice of 
+ *  simple transport or a transport plus a projeciton into
+ *  track-like 5x5 space.
  *
+ *  For all charged, basic particles, an ITrackExtrapolator is used. The 
+ *  implementation is controlled by the "TrackExtrapolator" property, it's 
+ *  default value being "TrackParabolicExtrapolator". The basic principle 
+ *  here is to obtain a 5x5 track-space state from the Particle, transport 
+ *  it with the track extrapolator, and project the transported state into 
+ *  Particle 7x7 space. For electrons and particles without an 
+ *  associated LHCb::ProtoParticle (assumed to be from MCParticles) the 5x5
+ *  state is obtained from the Particle. For all other charged, basic particles
+ *  it is obtained from the track state closest to the Z position to which
+ *  the particle is to be transported.
+ *  The projections between 5x5 track and 7x7 particle space are performed 
+ *  using the Particle2State implementation of IParticle2State.
+ *
+ *  @todo state method does a lot of useful stuff, yet is private. See if the 
+ *  funcitonality can be put in a tool or incorporated into an IParticleToState
+ *  implementation.
+ *  
  *  @author Patrick KOPPENBURG
  *  @date   2006-01-20
+ *
+ *  @author Juan Palacios juan.palacios@nikhef.nl
+ *  @date   2008-04-15
  */
 class ParticleTransporter : public GaudiTool, virtual public IParticleTransporter {
 public: 
@@ -34,23 +54,72 @@ public:
 
   /// Transport a Particle to specified z position.
   StatusCode transport(const LHCb::Particle*, 
-                       const double znew,
+                       const double zNew,
                        LHCb::Particle& transParticle);
+
+  StatusCode transportAndProject(const LHCb::Particle*, 
+                                 const double zNew,
+                                 LHCb::Particle& transParticle);
 protected:
 
 private:
+  /**
+   * Get or make a state from a Particle
+   *
+   * @author Patrick Koppenburg
+   *
+   */
+  StatusCode state(const LHCb::Particle* P,  
+                   const double zNew, 
+                   LHCb::State&) const ;
 
-  StatusCode state(const LHCb::Particle* P,  const double znew, LHCb::State&) const ;   ///< Get or make a state from a Particle
 
-  ITrackExtrapolator* m_tracks;        ///< Track extrapolator for particles from tracks
+  /**
+   * Transport a charged, basic LHCb::Particle to a new Z position.
+   * It simply obtains an LHCb::State using the state method, propagates
+   * it using the ITrackExtrapolator, and transforms back to 7x7 space
+   * using the IParticle2State tool.
+   *
+   * @param particle Input LHCb::Particle.
+   * @param zNew     Z-position to which particle is to be transported to.
+   * @param transParticle transported equivalent of input particle.
+   * @return StatusCode FAILURE if the track state creation, track propagation 
+   * or particle stare creation fail. SUCCESS otherwise.
+   * 
+   * 
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   * 
+   */
 
-  std::string m_tracksName;            ///< Type of Track extrapolator for particles from tracks
+  StatusCode transportChargedBasic(const LHCb::Particle* particle, 
+                                   const double zNew,
+                                   LHCb::Particle& transParticle);
+
+  /**
+   *  Check a transported Particle for infinite momentum and print
+   *  an error warning if necessary.
+   *  
+   *  @param transParticle The transported LHCb::Particle.
+   *  @return StautsCode::FAILURE if the particle's momentum is infinity.
+   *
+   *  @author Patrick Koppenburg
+   *  @author Juan Palacios
+   * 
+   */
+  StatusCode checkParticle(const LHCb::Particle& transParticle);
+  
+
+private:
+
+  ITrackExtrapolator* m_trackExtrapolator;        ///< Track extrapolator for particles from tracks
+
+  std::string m_trackExtrapolatorName;            ///< Type of Track extrapolator for particles from tracks
   
   /// Accessor for ParticlePropertySvc
   IParticlePropertySvc* m_ppSvc ;
   
   /// Particle to state convertion tool
-  IParticle2State* m_p2s ;
+  IParticle2State* m_particle2State ;
 
   /// Electron ID
   unsigned int m_eID ;
