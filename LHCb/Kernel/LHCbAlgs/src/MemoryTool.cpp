090a45bcd47e2797a75a5bcf518cd71d78ecf50d
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-04-16 08:11:25 +0000

    improve histograms
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47827 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f8790cd86b6c4675af92dc4eae596c5fb9153506

--- LHCb/Kernel/LHCbAlgs/src/MemoryTool.cpp
+++ LHCb/Kernel/LHCbAlgs/src/MemoryTool.cpp
@@ -1,4 +1,4 @@
-// $Id: MemoryTool.cpp,v 1.4 2006-11-30 15:57:28 cattanem Exp $
+// $Id: MemoryTool.cpp,v 1.5 2008-04-16 08:11:25 cattanem Exp $
 // Include files 
 
 // from Gaudi
@@ -26,6 +26,7 @@ MemoryTool::MemoryTool( const std::string& type,
   : GaudiHistoTool ( type, name , parent ), m_counter(0), m_bins(0)
 {
   declareProperty( "HistoSize", m_bins = 500 );
+  setProperty( "HistoPrint", false );
   declareInterface<IGenericTool>(this);
 }
 //=============================================================================
@@ -41,12 +42,12 @@ void MemoryTool::execute() {
   double mem = (double)System::virtualMemory();
 
   if( m_bins > m_counter ) {
-    plot( m_counter+1, 2, "Virtual memory (kB), first 'HistoSize' entries",
+    plot( m_counter+1, "Virtual mem, all entries", "Virtual memory (kB), first 'HistoSize' entries",
           0.5, m_bins+0.5, m_bins, mem );
   }
   if( 0 == m_counter%m_bins ) {
     unsigned int bin = 1 + ( m_counter/m_bins );
-    plot( bin, 3, "Virtual memory (kB), downscaled entries",
+    plot( bin, "Virtual mem, downscaled", "Virtual memory (kB), downscaled entries",
           0.5, m_bins+0.5, m_bins, mem );
   }
   ++m_counter;
