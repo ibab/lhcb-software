7e07ec547cb7dc8adc0281643f6ff4c39e43ae63
Victor Egorychev <victor.egorychev@cern.ch>
2008-04-21 13:54:22 +0000

    new interface for clusterization were added
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@39922 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 23b11cb27f280c5f0bd8a9eab257fcffaf0631b2

---
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/ICaloClusterization.h
@@ -0,0 +1,77 @@
+// $Id: ICaloClusterization.h,v 1.1 2008-04-21 13:54:22 vegorych Exp $
+#ifndef CALOINTERFACES_ICALOCLUSTERIZATION_H 
+#define CALOINTERFACES_ICALOCLUSTERIZATION_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+static const InterfaceID IID_ICaloClusterization ( "ICaloClusterization", 1, 0 );
+
+/** @class ICaloClusterization ICaloClusterization.h CaloInterfaces/ICaloClusterization.h
+ *  
+ *
+ *  @author Victor Egorychev
+ *  @date   2008-04-03
+ */
+class ICaloClusterization : virtual public IAlgTool 
+{
+public:
+  // ==========================================================================
+  /**  The main clusterization method
+   * @ param pointer to CaloDigit
+   * @ param pointer to DeCalorimeter object
+   * @ param vector of Calo cell ID's
+   * @ param number of neigbour levels around cell
+   * @ return vector of pointers of Calo clusters
+   */
+  virtual StatusCode clusterize
+  ( std::vector<LHCb::CaloCluster*>&      clusters   , 
+    const LHCb::CaloDigits*               digits     , 
+    const DeCalorimeter*                  detector   , 
+    const std::vector<LHCb::CaloCellID>&  seeds      , 
+    const unsigned int                    level      ) = 0 ;
+  // ==========================================================================
+  /**  The main clusterization method
+   * @ param pointer to CaloDigit
+   * @ param pointer to DeCalorimeter object
+   * @ param one Calo cell ID
+   * @ param number of neigbour levels around cell
+   * @ return vector of pointers of Calo clusters
+   */
+  virtual StatusCode clusterize
+  ( std::vector<LHCb::CaloCluster*>&      clusters   , 
+    const LHCb::CaloDigits*               digits     , 
+    const DeCalorimeter*                  detector   , 
+    const LHCb::CaloCellID&               seed       , 
+    const unsigned int                    level      ) = 0 ;
+  // ==========================================================================
+  /**  The main clusterization method
+   * @ param pointer to CaloDigit
+   * @ param pointer to DeCalorimeter object
+   * @ return vector of pointers of Calo clusters
+   */
+  virtual StatusCode clusterize
+  ( std::vector<LHCb::CaloCluster*>&      clusters   , 
+    const LHCb::CaloDigits*               digits     , 
+    const DeCalorimeter*                  detector   ) = 0 ;
+  
+public: 
+
+  /** static interface identification
+   *  @see IInterface
+   *  @see IID_ICaloClusterization
+   *  Return the interface ID
+   */
+  static const InterfaceID& interfaceID() { return IID_ICaloClusterization; }
+
+
+protected:
+
+private:
+
+};
+#endif // CALOINTERFACES_ICALOCLUSTERIZATION_H
