9623d69f4c2e062b6093a4bf04d018844c9aeca6
jpalac <jpalac@cern.ch>
2008-04-23 13:00:31 +0000

    . Add property "ICloneMCParticle" to control implementation of     ICloneMCVertex to use.   . Add safety to operator(): check if clonee already exists before invoking     clone() method.   . clone(const LHCb::MCVertex*) method now takes control of cloning the     LHCb::Particle products of the MCVertex using the ICloneMCParticle of     choice.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@71769 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 54278b5754aaabf11276a57a516eaf3d499caa6e

--- Analysis/MicroDST/MicroDSTTools/src/MCVertexCloner.cpp
+++ Analysis/MicroDST/MicroDSTTools/src/MCVertexCloner.cpp
@@ -1,4 +1,4 @@
-// $Id: MCVertexCloner.cpp,v 1.2 2008-02-11 22:40:07 jpalac Exp $
+// $Id: MCVertexCloner.cpp,v 1.3 2008-04-23 13:00:31 jpalac Exp $
 // Include files 
 
 // from Gaudi
@@ -26,20 +26,48 @@ MCVertexCloner::MCVertexCloner( const std::string& type,
                                 const std::string& name,
                                 const IInterface* parent )
   : 
-  MicroDSTTool ( type, name , parent )
+  MicroDSTTool ( type, name , parent ),
+  m_particleClonerName("MCParticleCloner")
 {
   declareInterface<ICloneMCVertex>(this);
+  declareProperty("ICloneMCParticle", m_particleClonerName);
+
 }
 //=============================================================================
 LHCb::MCVertex* MCVertexCloner::operator() (const LHCb::MCVertex* vertex)
 {
-  return this->clone(vertex);
+  if (0==vertex) return 0;
+  LHCb::MCVertex* clone = getStoredClone<LHCb::MCVertex>(vertex);
+  return 0 != clone ? clone : this->clone(vertex);
+
 }
-//=============================================================================
 //=============================================================================
 LHCb::MCVertex* MCVertexCloner::clone(const LHCb::MCVertex* vertex)
 {
-  return cloneKeyedContainerItem<LHCb::MCVertex, BasicMCVtxCloner>(vertex);
+
+  LHCb::MCVertex* clone = 
+    cloneKeyedContainerItem<LHCb::MCVertex, BasicCloner>(vertex);
+
+  clone->setMother((*m_particleCloner)(vertex->mother()));
+
+  clone->clearProducts();
+
+  cloneDecayProducts(vertex->products(), clone);
+
+  return clone;
+
+}
+//=============================================================================
+void MCVertexCloner::cloneDecayProducts(const SmartRefVector<LHCb::MCParticle>& products,
+                                        LHCb::MCVertex* clonedVertex) 
+{
+  for (SmartRefVector<LHCb::MCParticle>::const_iterator iProd = products.begin();
+       iProd != products.end();
+       ++iProd) {
+    LHCb::MCParticle* productClone = (*m_particleCloner)(*iProd);
+    productClone->setOriginVertex( clonedVertex );
+    if (productClone) clonedVertex->addToProducts(productClone );
+  }
 }
 //=============================================================================
 StatusCode MCVertexCloner::initialize()
@@ -49,7 +77,7 @@ StatusCode MCVertexCloner::initialize()
   
   if (! sc.isSuccess() ) return sc;
 
-  m_particleCloner = tool<ICloneMCParticle>( "MCParticleCloner",
+  m_particleCloner = tool<ICloneMCParticle>( m_particleClonerName,
                                              this->parent()       );
 
   return StatusCode::SUCCESS;
