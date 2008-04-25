1812f981ea9f4bc6a64adff99b1fa458b984dc61
Beat Jost <beat.jost@cern.ch>
2008-04-25 09:57:38 +0000

    no message
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97833 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 48209160568e265db61d8323d0971fc67a0ba437

---
+++ Online/Online/GaudiOnline/GaudiOnline/RawBankSizeMonitor.h
@@ -0,0 +1,41 @@
+// $Id: RawBankSizeMonitor.h,v 1.1 2008-04-25 09:57:38 jost Exp $
+#ifndef COMPONENT_RAWBANKSIZEMONITOR_H 
+#define COMPONENT_RAWBANKSIZEMONITOR_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiHistoAlg.h"
+// from LHCb
+#include "Event/RawEvent.h"
+
+/** @class RawBankSizeMonitor RawBankSizeMonitor.h component/RawBankSizeMonitor.h
+ *  
+ *
+ *  @author Olivier Deschamps
+ *  @date   2008-03-27
+ */
+class RawBankSizeMonitor : public GaudiHistoAlg {
+public: 
+  /// Standard constructor
+  RawBankSizeMonitor( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~RawBankSizeMonitor( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  LHCb::RawEvent* m_rawEvt;
+  //
+  std::vector<std::string> m_bankNames;
+  std::vector<LHCb::RawBank::BankType> m_bankTypes;
+  std::map<std::string,int> m_max;
+  int m_def;
+  int m_bin;
+  bool m_prof;
+  bool m_firstevent;
+};
+#endif // COMPONENT_RAWBANKSIZEMONITOR_H
