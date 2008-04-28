e2d4acaab951682888769110b163224730013ace
Marco Clemencic <marcocle@cern.ch>
2008-04-28 10:40:33 +0000

    Updated tests.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13113 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/tests/test_VersionsDB.py
+++ LbScripts/LbUtils/tests/test_VersionsDB.py
@@ -1,7 +1,7 @@
 import unittest
 from LbUtils import VersionsDB
 __author__ = "Marco Clemencic <Marco.Clemencic@cern.ch>"
-__version__ = "$Id: test_VersionsDB.py,v 1.1 2008-04-25 15:33:51 marcocle Exp $"
+__version__ = "$Id: test_VersionsDB.py,v 1.2 2008-04-28 10:40:33 marcocle Exp $"
 
 class VersionsDBTest(unittest.TestCase):
     def _prepareXML(self, data):
@@ -26,6 +26,29 @@ class VersionsDBTest(unittest.TestCase):
     def tearDown(self):
         VersionsDB.Release.__releases__ = {}
     
+    def test_001_version_class(self):
+        from LbUtils.VersionsDB import Version
+        # parse string, evaluate representation and convert to string
+        # (all tests in one line...)
+        v = "v1r2p3"
+        self.assertEquals(str(eval(repr(Version(v)))),v)
+        v = "v1r2"
+        self.assertEquals(str(eval(repr(Version(v)))),v)
+        # copy constructor
+        v = Version("v1r2p3")
+        self.assertEquals(Version(v),v)
+        # errors
+        self.assertRaises(ValueError,Version,"test")
+        self.assertRaises(TypeError,Version,)
+        self.assertRaises(TypeError,Version,1)
+        self.assertRaises(TypeError,Version,1,2,3,4)
+        # sorting
+        sl = ["v0r0","v0r0p1","v0r1","v1r0","v1r0p0","v1r1p0","v2r0","v10r5"]
+        l = [ Version(s) for s in sl ]
+        l.sort()
+        res = [ str(v) for v in l ]
+        self.assertEquals(res,sl)
+        
     def test_010_create_release(self):
         ra = VersionsDB.Release("ReleaseA")
         rb = VersionsDB.Release("ReleaseB",base="ReleaseA")
@@ -47,11 +70,11 @@ class VersionsDBTest(unittest.TestCase):
         r["Project2"] = "version2b"
         
         r = VersionsDB.getRelease("ReleaseA")
-        self.assertEquals(r["Project1"],"version1")
-        self.assertEquals(r["Project2"],"version2")
+        self.assertEquals(r["Project1"].version,"version1")
+        self.assertEquals(r["Project2"].version,"version2")
         r = VersionsDB.getRelease("ReleaseB")
-        self.assertEquals(r["Project1"],"version1")
-        self.assertEquals(r["Project2"],"version2b")
+        self.assertEquals(r["Project1"].version,"version1")
+        self.assertEquals(r["Project2"].version,"version2b")
     def test_040_missing_project(self):
         r = VersionsDB.Release("ReleaseA")
         r["Project1"] = "version1"
@@ -143,6 +166,51 @@ class VersionsDBTest(unittest.TestCase):
         self.assertEquals(VersionsDB.getCompatibleVersions("P3","V3",
                                                            "P4",
                                                            "P5"),[])
+        
+    def _prepareDBWithDeps(self):
+        r = VersionsDB.Release("r1")
+        for i in range(1,7):
+            r["p%d"%i] = "v1"
+        r["p2"].addBuildTimeDep("p1")
+        r["p3"].addBuildTimeDep("p1")
+        r["p3"].addRunTimeDep("p2")
+        r["p4"].addBuildTimeDep("p1")
+        r["p5"].addRunTimeDep("p4")
+        r["p6"].addBuildTimeDep("p3")
+        r["p6"].addRunTimeDep("p5")
+        
+    def test_400_dependencies(self):
+        self._prepareDBWithDeps()
+        r = VersionsDB.getRelease("r1")
+        
+        deps = r.expandBuildTimeDeps("p6")
+        deps.sort()
+        self.assertEquals(deps,["p1","p3"])
+        
+        deps = r.expandRunTimeDeps("p6")
+        deps.sort()
+        self.assertEquals(deps,["p2","p4","p5"])
+    def test_900_generateXML(self):
+        data = [("R1",None,[("P1","V1"),("P2","V1"),("P3","V1"),("P4","V1")]),
+                ("R2",None,[("P1","V2"),("P2","V2"),("P3","V1"),("P4","V1")]),
+                ("R3",None,[("P1","V3"),("P2","V2"),("P3","V2"),("P4","V1")]),
+                ("R4",None,[("P1","V4"),("P2","V2"),("P3","V2"),("P4","V2")]),
+                ("R5","R4",[                        ("P3","V3")]),
+                ("R6",None,[("P1","V5"),("P2","V2"),("P3","V4"),("P4","V3")]),
+                ]
+        self._fillDB(data)
+        xml = VersionsDB.generateXML()
+        #print repr(xml)
+        VersionsDB.loadString(xml)
+        self.assertEquals(xml,VersionsDB.generateXML())
 
-
-unittest.main()
+    def test_910_generateXML_deps(self):
+        self._prepareDBWithDeps()
+        
+        xml = VersionsDB.generateXML()
+        #print repr(xml)
+        VersionsDB.loadString(xml)
+        self.assertEquals(xml,VersionsDB.generateXML())
+        
+if __name__ == "__main__":
+    unittest.main()
