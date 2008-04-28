918e9ab7bef200ec0ff2f97f57d1825d2333be63
Marco Clemencic <marcocle@cern.ch>
2008-04-28 10:40:23 +0000

    Marco Clemencic: - Added support for run-time and build-time dependencies. - Generate XML based on XSD. - Class "Version" to simplify handling of version strings ("v1r2p3").
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13112 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/python/LbUtils/VersionsDB.py
+++ LbScripts/LbUtils/python/LbUtils/VersionsDB.py
@@ -4,7 +4,12 @@ The database is based on an XML file containing the list of (project,version)s
 for each version of the complete software stack.
 """
 __author__ = "Marco Clemencic <Marco.Clemencic@cern.ch>"
-__version__ = "$Id: VersionsDB.py,v 1.1 2008-04-25 15:33:51 marcocle Exp $"
+__version__ = "$Id: VersionsDB.py,v 1.2 2008-04-28 10:40:23 marcocle Exp $"
+
+# Hack to simplify the usage of sets with older versions of Python.
+import sys
+if sys.version_info < (2,4):
+    from sets import Set as set
 
 #--- Error definitions
 class VersionsDBError(RuntimeError):
@@ -31,6 +36,85 @@ class DuplicatedProjectError(VersionsDBError):
         self.project = proj
 
 #--- Main classes
+import re
+class Version:
+    """
+    Handle version numbers.
+    """
+    __regexp__ = re.compile(r"v([0-9]+)r([0-9]+)(?:p([0-9]+))?")
+    def __init__(self,*args):
+        """
+        Construct a version object from a version string like "v1r2p3" or
+        from 2-3 integers .
+        """
+        self._version = ()
+        if len(args) < 1:
+            raise TypeError("__init__() takes at least 1 argument (0 given)")
+        elif len(args) == 1:
+            if type(args[0]) is str:
+                import re
+                m = self.__regexp__.match(args[0])
+                if m:
+                    self._version = tuple([int(i)
+                                           for i in m.groups()
+                                           if i is not None])
+                else:
+                    raise ValueError("wrong version string: '%s'"%args[0])
+            elif type(args[0]) is self.__class__:
+                # copy constructor
+                self._version = args[0]._version                
+            else:
+                raise TypeError("__init__() needs a string as single argument ('%s' given)"%type(args[0]))
+        elif len(args) <= 3:
+            self._version = tuple([int(i) for i in args]) 
+        else:
+            raise TypeError("__init__() takes at most 3 arguments (%d given)"%len(args))
+    def __repr__(self):
+        """
+        Returns a string that can be evaluated to get a version object
+        equivalent to the current one.
+        """
+        return self.__class__.__name__ + repr(self._version)
+    def __str__(self):
+        """
+        Return a the version as version string like "v1r2p3".
+        """
+        if len(self._version) == 2:
+            s = "v%dr%d"
+        else:
+            s = "v%dr%dp%d"
+        return s%self._version
+    def __hash__(self):
+        return hash(self._version)
+    def __cmp__(self,other):
+        if other.__class__ == self.__class__:
+            other = other._version
+        if self._version < other: return -1
+        elif self._version == other: return 0
+        else: return 1
+
+class Project:
+    """
+    Informations about a project.
+    """
+    def __init__(self,name,version,buildtimedeps=[],runtimedeps=[]):
+        self.name = name
+        self.version = version
+        self.buildtimedeps = buildtimedeps[:]
+        self.runtimedeps = runtimedeps[:]
+    def addBuildTimeDep(self,proj_name):
+        if proj_name not in self.buildtimedeps:
+            self.buildtimedeps.append(proj_name)
+    def addRunTimeDep(self,proj_name):
+        if proj_name not in self.runtimedeps:
+            self.runtimedeps.append(proj_name)
+    def __repr__(self):
+        return "Project(name=%s,version=%s,buildtimedeps=%s,runtimedeps=%s)" % \
+                (repr(self.name),
+                 repr(self.version),
+                 repr(self.buildtimedeps),
+                 repr(self.runtimedeps))
+    
 class Release:
     """
     Class implementing the versions database storage.
@@ -68,7 +152,29 @@ class Release:
     def __setitem__(self,key,value):
         if key in self.projects:
             raise DuplicatedProjectError(self.name,key)
-        self.projects[key] = value
+        self.projects[key] = Project(name=key,version=value)
+    def add(self,project):
+        if project.name in self.projects:
+            raise DuplicatedProjectError(self.name,project.name)
+        self.projects[project.name] = project
+    def items(self):
+        return self.projects.items()
+    def values(self):
+        return self.projects.values()
+    def _expandDeps(self,project,function):
+        deps = function(self[project])
+        deps += reduce(lambda a,b: a+b,
+                       [self._expandDeps(p,function) for p in deps],
+                       [])
+        return deps
+    def expandBuildTimeDeps(self,project):
+        return list(set(self._expandDeps(project,lambda p: p.buildtimedeps)))
+    def expandRunTimeDeps(self,project):
+        deps = reduce(lambda a,b: a+b,
+                      [ self.expandRunTimeDeps(p)
+                        for p in self.expandBuildTimeDeps(project) ],
+                      [])
+        return list(set(deps + self._expandDeps(project,lambda p: p.runtimedeps)))
     
 from xml.sax.handler import ContentHandler
 class _ReleaseSAXHandler(ContentHandler):
@@ -82,6 +188,7 @@ class _ReleaseSAXHandler(ContentHandler):
         Release.__releases__ = {} 
         self.releases = Release.__releases__
         self._currentRelease = None
+        self._currentProject = None
     def endDocument(self):
         #print "endDocument"
         #pprint(self.releases)
@@ -95,11 +202,21 @@ class _ReleaseSAXHandler(ContentHandler):
             base = attrs.get(u'base',None)
             self._currentRelease = Release(name_attr, base = base)
         elif name == u'project':
-            self._currentRelease[name_attr] = attrs.getValue(u'version')
+            self._currentProject = Project(name = name_attr,
+                                           version = attrs.getValue(u'version'))
+            self._currentRelease.add(self._currentProject)
+        elif name == u'buildtimedep':
+            self._currentProject.addBuildTimeDep(name_attr)
+        elif name == u'runtimedep':
+            self._currentProject.addRunTimeDep(name_attr)
+        else:
+            print "Warning: unknown element '%s'"%name
     def endElement(self,name):
         #print "end element %s:"%name
         if name == u'release':
             self._currentRelease = None
+        elif name == u'project':
+            self._currentProject = None
 
 #--- Initialization functions
 def load(source):
@@ -114,7 +231,7 @@ def loadString(source):
     Load the database from an XML string.
     """
     from xml.sax import parseString as parse
-    parse(source,_ReleaseSAXHandler())
+    parse(str(source),_ReleaseSAXHandler())
 
 #--- Internal query functions
 def _findReleases(project,version):
@@ -123,34 +240,27 @@ def _findReleases(project,version):
     """
     return [ r.name
              for r in Release.__releases__.values()
-             if project in r and r[project] == version ]
+             if project in r and r[project].version == version ]
 
 def _findAllReleases(project,versions):
     """
     Find all the releases that feature the specified project in any of the
     specified versions.
     """
-    tmp = []
+    tmp = set()
     for v in versions:
-        tmp += _findReleases(project,v)
-    tmp.sort()
-    return tmp
+        tmp.update(_findReleases(project,v))
+    return list(tmp)
 
 def _getVersions(project,releases):
     """
     Given a set of releases and a project, find all the versions of the project
     used in those releases.
     """
-    versions = [ getRelease(r)[project]
+    versions = [ getRelease(r)[project].version
                  for r in releases
                  if project in getRelease(r) ] # do we want this check?
-    versions.sort()
-    tmp = versions
-    versions = []
-    for i in tmp:
-        if i not in versions:
-            versions.append(i)
-    return versions
+    return list(set(versions))
 
 #--- Public query functions
 def getCompatibleVersions(project,version,otherproject,keyproj="LHCb"):
@@ -163,11 +273,33 @@ def getCompatibleVersions(project,version,otherproject,keyproj="LHCb"):
                                          _getVersions(keyproj,
                                                       _findReleases(project, version))))
 
+def getRuntimeVersions(project,version,keyproj="LHCb"):
+    deps = []
+    proj_releases = _findReleases(project, version)
+    #proj_releases.sort()
+    if proj_releases:
+        rt_deps =proj_releases[-1].expandRuntimeDeps(project)
+        comp_releases = _findAllReleases(keyproj,
+                                         _getVersions(keyproj,
+                                                      proj_releases))
+        for p in rt_deps:
+            vers = _getVersions(p,comp_releases)
+            if vers:
+                vers.sort()
+                deps.append(p,vers[-1])
+    return deps
+
 def getRelease(name):
     """
     Retrieve a release instance by name. 
     """
     return Release.__releases__[name]
+
+def getReleases():
+    """
+    Return a list of the available release names. 
+    """
+    return Release.__releases__.keys()
 
 def releaseExists(name):
     """
@@ -176,20 +308,31 @@ def releaseExists(name):
     return name in Release.__releases__
 
 # @TODO: Use some alphabetical order.
-def generateXML():
+def generateXML(withDTD = False, withSchema = True):
     # header
-    xml = """<?xml version="1.0" encoding="ISO-8859-1"?>
-<!DOCTYPE releases_db SYSTEM "releases_db.dtd">
-<releases_db>
-"""
+    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    if withDTD:
+        xml += '<!DOCTYPE releases_db SYSTEM "releases_db.dtd">\n<releases_db>\n'
+    elif withSchema:
+        xml += '<releases_db\n' + \
+               ' xmlns="http://cern.ch/lhcb-comp/"\n' + \
+               ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' + \
+               ' xsi:schemaLocation="http://cern.ch/lhcb-comp/ releases_db.xsd ">\n'
+    else:
+        xml += '<releases_db>\n'
     # body
     for rel in Release.__releases__.values():
         xml += '  <release name="' + rel.name
         if rel.base is not None:
             xml += '" base="' + rel.base
         xml += '">\n'
-        for p_v in rel.projects.items():
-            xml += '    <project name="%s" version="%s"/>\n'%p_v
-        xml += '</release>\n'
+        for p in rel.values():
+            xml += '    <project name="%s" version="%s">\n'%(p.name,p.version)
+            for dep in p.buildtimedeps:
+                xml += '      <buildtimedep name="%s"/>\n'%dep
+            for dep in p.runtimedeps:
+                xml += '      <runtimedep name="%s"/>\n'%dep
+            xml += '    </project>\n'
+        xml += '  </release>\n'
     xml += '</releases_db>\n'
     return xml
