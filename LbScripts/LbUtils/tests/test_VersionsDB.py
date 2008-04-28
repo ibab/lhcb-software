579c6d90554fcb3e2ef61debfc69b454fe3e4d95
Marco Clemencic <marcocle@cern.ch>
2008-04-28 18:13:31 +0000

    updates
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13115 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/tests/test_VersionsDB.py
+++ LbScripts/LbUtils/tests/test_VersionsDB.py
@@ -1,13 +1,13 @@
 import unittest
 from LbUtils import VersionsDB
 __author__ = "Marco Clemencic <Marco.Clemencic@cern.ch>"
-__version__ = "$Id: test_VersionsDB.py,v 1.2 2008-04-28 10:40:33 marcocle Exp $"
+__version__ = "$Id: test_VersionsDB.py,v 1.3 2008-04-28 18:13:31 marcocle Exp $"
 
 class VersionsDBTest(unittest.TestCase):
     def _prepareXML(self, data):
         """Prepare the XML to be tested.
         'data' must have this format:
-        [("ReleaseName","BaseRelease",[("proj1","v1"),("proj2","v2"),...]),...]"""
+        [("ReleaseName","BaseRelease",[("proj1","v1"),("proj2","v2r0"),...]),...]"""
         xml = """<?xml version="1.0" encoding="ISO-8859-1"?><releases_db>"""
         for name,base,projects in data:
             xml += '<release name="' + name
@@ -34,16 +34,26 @@ class VersionsDBTest(unittest.TestCase):
         self.assertEquals(str(eval(repr(Version(v)))),v)
         v = "v1r2"
         self.assertEquals(str(eval(repr(Version(v)))),v)
+        v = "54"
+        self.assertEquals(str(eval(repr(Version(v)))),v)
+        v = "54e"
+        self.assertEquals(str(eval(repr(Version(v)))),v)
         # copy constructor
         v = Version("v1r2p3")
+        self.assertEquals(Version(v),v)
+        v = Version("54e")
         self.assertEquals(Version(v),v)
         # errors
         self.assertRaises(ValueError,Version,"test")
         self.assertRaises(TypeError,Version,)
-        self.assertRaises(TypeError,Version,1)
         self.assertRaises(TypeError,Version,1,2,3,4)
         # sorting
         sl = ["v0r0","v0r0p1","v0r1","v1r0","v1r0p0","v1r1p0","v2r0","v10r5"]
+        l = [ Version(s) for s in sl ]
+        l.sort()
+        res = [ str(v) for v in l ]
+        self.assertEquals(res,sl)
+        sl = ["51","52","52a","52b","54","54a","54e"]
         l = [ Version(s) for s in sl ]
         l.sort()
         res = [ str(v) for v in l ]
@@ -64,27 +74,27 @@ class VersionsDBTest(unittest.TestCase):
             self.assertEquals(x.release,"ARelease")
     def test_030_add_project(self):
         r = VersionsDB.Release("ReleaseA")
-        r["Project1"] = "version1"
-        r["Project2"] = "version2"
+        r["Project1"] = "v1r0"
+        r["Project2"] = "v2r0"
         r = VersionsDB.Release("ReleaseB",base="ReleaseA")
-        r["Project2"] = "version2b"
+        r["Project2"] = "v2r1"
         
         r = VersionsDB.getRelease("ReleaseA")
-        self.assertEquals(r["Project1"].version,"version1")
-        self.assertEquals(r["Project2"].version,"version2")
+        self.assertEquals(r["Project1"].version,"v1r0")
+        self.assertEquals(r["Project2"].version,"v2r0")
         r = VersionsDB.getRelease("ReleaseB")
-        self.assertEquals(r["Project1"].version,"version1")
-        self.assertEquals(r["Project2"].version,"version2b")
+        self.assertEquals(r["Project1"].version,"v1r0")
+        self.assertEquals(r["Project2"].version,"v2r1")
     def test_040_missing_project(self):
         r = VersionsDB.Release("ReleaseA")
-        r["Project1"] = "version1"
-        r["Project2"] = "version2"
+        r["Project1"] = "v1r0"
+        r["Project2"] = "v2r0"
         self.assert_("Project3" not in r)
         self.assert_(not ("Project3" in r))
         
     def test_110_basic_parse(self):
         """Basic parsing"""
-        data = [("ReleaseName",None,[("Proj%d"%i,"v%d"%i) for i in range(3)])]
+        data = [("ReleaseName",None,[("Proj%d"%i,"v%dr0"%i) for i in range(3)])]
         VersionsDB.loadString(self._prepareXML(data))
 
     def test_120_parse_errors(self):
@@ -96,7 +106,7 @@ class VersionsDBTest(unittest.TestCase):
         except VersionsDB.DuplicatedReleaseError:
             pass
 
-        data = [("ReleaseName",None,[("P","V"),("P","V")])]
+        data = [("ReleaseName",None,[("P","v1r0"),("P","v1r0")])]
         try:
             VersionsDB.loadString(self._prepareXML(data))
             self.fail("DuplicatedProjectError exception expected")
@@ -104,73 +114,73 @@ class VersionsDBTest(unittest.TestCase):
             pass
         
     def test_200_findReleases(self):
-        data = [("R1",None,[("P1","V1"),("P2","V1"),("P3","V1")]),
-                ("R2",None,[("P1","V1"),("P2","V2"),("P3","V1")]),
-                ("R3","R2",[("P3","V2")]),
+        data = [("R1",None,[("P1","v1r0"),("P2","v1r0"),("P3","v1r0")]),
+                ("R2",None,[("P1","v1r0"),("P2","v2r0"),("P3","v1r0")]),
+                ("R3","R2",[("P3","v2r0")]),
                 ]
         self._fillDB(data)
-        self.assertEquals(VersionsDB._findReleases("P1","V1"),["R1","R2","R3"])
-        self.assertEquals(VersionsDB._findReleases("P2","V1"),["R1"])
-        self.assertEquals(VersionsDB._findReleases("P2","V2"),["R2","R3"])
-        self.assertEquals(VersionsDB._findReleases("P3","V1"),["R1","R2"])
-        self.assertEquals(VersionsDB._findReleases("P3","V2"),["R3"])
+        self.assertEquals(VersionsDB._findReleases("P1","v1r0"),["R1","R2","R3"])
+        self.assertEquals(VersionsDB._findReleases("P2","v1r0"),["R1"])
+        self.assertEquals(VersionsDB._findReleases("P2","v2r0"),["R2","R3"])
+        self.assertEquals(VersionsDB._findReleases("P3","v1r0"),["R1","R2"])
+        self.assertEquals(VersionsDB._findReleases("P3","v2r0"),["R3"])
         # all releases
-        self.assertEquals(VersionsDB._findAllReleases("P2",["V1","V2"]),
+        self.assertEquals(VersionsDB._findAllReleases("P2",["v1r0","v2r0"]),
                           ["R1","R2","R3"])
         # non-existing versions
-        self.assertEquals(VersionsDB._findReleases("P1","V2"),[])
-        self.assertEquals(VersionsDB._findReleases("P4","V1"),[])
+        self.assertEquals(VersionsDB._findReleases("P1","v2r0"),[])
+        self.assertEquals(VersionsDB._findReleases("P4","v1r0"),[])
         # get versions
-        self.assertEquals(VersionsDB._getVersions("P1",["R1"]),["V1"])
-        self.assertEquals(VersionsDB._getVersions("P1",["R1","R2","R3"]),["V1"])
-        self.assertEquals(VersionsDB._getVersions("P2",["R1","R2","R3"]),["V1","V2"])
-        self.assertEquals(VersionsDB._getVersions("P3",["R1","R2","R3"]),["V1","V2"])
+        self.assertEquals(VersionsDB._getVersions("P1",["R1"]),["v1r0"])
+        self.assertEquals(VersionsDB._getVersions("P1",["R1","R2","R3"]),["v1r0"])
+        self.assertEquals(VersionsDB._getVersions("P2",["R1","R2","R3"]),["v1r0","v2r0"])
+        self.assertEquals(VersionsDB._getVersions("P3",["R1","R2","R3"]),["v1r0","v2r0"])
         self.assertEquals(VersionsDB._getVersions("P5",["R1","R2","R3"]),[])
 
     def test_300_getCompatibleVersions(self):
-        data = [("R1",None,[("P1","V1"),("P2","V1"),("P3","V1"),("P4","V1")]),
-                ("R2",None,[("P1","V2"),("P2","V2"),("P3","V1"),("P4","V1")]),
-                ("R3",None,[("P1","V3"),("P2","V2"),("P3","V2"),("P4","V1")]),
-                ("R4",None,[("P1","V4"),("P2","V2"),("P3","V2"),("P4","V2")]),
-                ("R5","R4",[                        ("P3","V3")]),
-                ("R6",None,[("P1","V5"),("P2","V2"),("P3","V4"),("P4","V3")]),
+        data = [("R1",None,[("P1","v1r0"),("P2","v1r0"),("P3","v1r0"),("P4","v1r0")]),
+                ("R2",None,[("P1","v2r0"),("P2","v2r0"),("P3","v1r0"),("P4","v1r0")]),
+                ("R3",None,[("P1","v3r0"),("P2","v2r0"),("P3","v2r0"),("P4","v1r0")]),
+                ("R4",None,[("P1","v4r0"),("P2","v2r0"),("P3","v2r0"),("P4","v2r0")]),
+                ("R5","R4",[                        ("P3","v3r0")]),
+                ("R6",None,[("P1","v5r0"),("P2","v2r0"),("P3","v4r0"),("P4","v3r0")]),
                 ]
         self._fillDB(data)
         
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V2",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v2r0",
                                                            "P4",
-                                                           "P1"),["V1","V2"])
+                                                           "P1"),["v1r0","v2r0"])
         
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V3",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v3r0",
                                                            "P4",
-                                                           "P1"),["V2"])
+                                                           "P1"),["v2r0"])
         
-        self.assertEquals(VersionsDB.getCompatibleVersions("P4","V1",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P4","v1r0",
                                                            "P3",
-                                                           "P1"),["V1","V2"])
+                                                           "P1"),["v1r0","v2r0"])
                 
-        self.assertEquals(VersionsDB.getCompatibleVersions("P2","V2",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P2","v2r0",
                                                            "P3",
-                                                           "P1"),["V1","V2","V3","V4"])
+                                                           "P1"),["v1r0","v2r0","v3r0","v4r0"])
         
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V3",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v3r0",
                                                            "P4",
-                                                           "P2"),["V1","V2","V3"])
+                                                           "P2"),["v1r0","v2r0","v3r0"])
 
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V5",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v5r0",
                                                            "P4",
                                                            "P1"),[])
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V3",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v3r0",
                                                            "P5",
                                                            "P1"),[])
-        self.assertEquals(VersionsDB.getCompatibleVersions("P3","V3",
+        self.assertEquals(VersionsDB.getCompatibleVersions("P3","v3r0",
                                                            "P4",
                                                            "P5"),[])
         
     def _prepareDBWithDeps(self):
         r = VersionsDB.Release("r1")
-        for i in range(1,7):
-            r["p%d"%i] = "v1"
+        for i in range(1,8):
+            r["p%d"%i] = "v1r0"
         r["p2"].addBuildTimeDep("p1")
         r["p3"].addBuildTimeDep("p1")
         r["p3"].addRunTimeDep("p2")
@@ -178,6 +188,13 @@ class VersionsDBTest(unittest.TestCase):
         r["p5"].addRunTimeDep("p4")
         r["p6"].addBuildTimeDep("p3")
         r["p6"].addRunTimeDep("p5")
+        r["p7"].addBuildTimeDep("p1")
+        r["p7"].addBuildTimeDep("p2")
+        r = VersionsDB.Release("r2",base="r1")
+        r["z10"] = "v1r0"
+        r["p5"] = "v1r1"
+        r["p5"].addRunTimeDep("p4")
+        r["p5"].addRunTimeDep("z10")
         
     def test_400_dependencies(self):
         self._prepareDBWithDeps()
@@ -190,13 +207,26 @@ class VersionsDBTest(unittest.TestCase):
         deps = r.expandRunTimeDeps("p6")
         deps.sort()
         self.assertEquals(deps,["p2","p4","p5"])
+    def test_410_sort_projects(self):
+        self._prepareDBWithDeps()
+        
+        r = VersionsDB.getRelease("r1")
+        projs = [ p.name for p in r.allProjects() ]
+        self.assertEquals(projs,['p1', 'p2', 'p4', 'p3', 'p5', 'p7', 'p6'])
+        
+        r = VersionsDB.getRelease("r2")
+        projs = [ p.name for p in r.allProjects() ]
+        self.assertEquals(projs,['p1', 'z10', 'p2', 'p4', 'p3', 'p5', 'p7', 'p6'])
+        
+        projs = [ p.name for p in r.values() ]
+        self.assertEquals(projs,['z10', 'p5'])
     def test_900_generateXML(self):
-        data = [("R1",None,[("P1","V1"),("P2","V1"),("P3","V1"),("P4","V1")]),
-                ("R2",None,[("P1","V2"),("P2","V2"),("P3","V1"),("P4","V1")]),
-                ("R3",None,[("P1","V3"),("P2","V2"),("P3","V2"),("P4","V1")]),
-                ("R4",None,[("P1","V4"),("P2","V2"),("P3","V2"),("P4","V2")]),
-                ("R5","R4",[                        ("P3","V3")]),
-                ("R6",None,[("P1","V5"),("P2","V2"),("P3","V4"),("P4","V3")]),
+        data = [("R1",None,[("P1","v1r0"),("P2","v1r0"),("P3","v1r0"),("P4","v1r0")]),
+                ("R2",None,[("P1","v2r0"),("P2","v2r0"),("P3","v1r0"),("P4","v1r0")]),
+                ("R3",None,[("P1","v3r0"),("P2","v2r0"),("P3","v2r0"),("P4","v1r0")]),
+                ("R4",None,[("P1","v4r0"),("P2","v2r0"),("P3","v2r0"),("P4","v2r0")]),
+                ("R5","R4",[                        ("P3","v3r0")]),
+                ("R6",None,[("P1","v5r0"),("P2","v2r0"),("P3","v4r0"),("P4","v3r0")]),
                 ]
         self._fillDB(data)
         xml = VersionsDB.generateXML()
