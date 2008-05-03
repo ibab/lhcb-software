7a81b08dd0c4085314343f6563cefd1172fce99d
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-05-03 08:53:14 +0000

    remove obsolete _dll.cpp file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71867 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b0606036792158444e1166e5945cb10c4453271e

--- Hlt/Hlt/HltL0MuonConf/src/HltL0MuonConf_dll.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: HltL0MuonConf_dll.cpp,v 1.1.1.1 2007-05-22 10:20:31 asatta Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-
-LOAD_FACTORY_ENTRIES(HltL0MuonConf)
-
