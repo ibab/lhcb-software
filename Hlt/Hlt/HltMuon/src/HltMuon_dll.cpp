05d147ae50d07079df4981d2041b4851bcd739cb
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-05-03 08:30:04 +0000

    remove obsolete _dll.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@71862 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7d221f0547d6c90d2b09ac674cfb929e8e081e43

--- Hlt/Hlt/HltMuon/src/HltMuon_dll.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: HltMuon_dll.cpp,v 1.1.1.1 2006-09-25 16:14:59 asatta Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-
-LOAD_FACTORY_ENTRIES(HltMuon)
-
--- Hlt/Hlt/HltSelChecker/src/HltSelChecker_dll.cpp
+++
@@ -1,8 +0,0 @@
-// $Id: HltSelChecker_dll.cpp,v 1.3 2007-07-09 18:45:08 pkoppenb Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-LOAD_FACTORY_ENTRIES(HltSelChecker) 
-
