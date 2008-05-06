c40e8aef4d5d5c57c736c09c6f5c86de3b3154a4
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2008-05-06 12:18:09 +0000

    For MonObjects
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@97889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2b89750f1f63f135a0363cd832cd18609b6c2ffe

--- Online/Online/Saver/src/Saver.cpp
+++ Online/Online/Saver/src/Saver.cpp
@@ -23,7 +23,6 @@
 #include "GaudiKernel/IDataManagerSvc.h"
 #include "GaudiKernel/Incident.h"
 #include "GaudiKernel/IIncidentSvc.h"
-#include "Saver.h"
 
 #include <cstring>
 #include <stdlib.h>
@@ -31,21 +30,7 @@
 #include <string>
 #include <vector>
 
-#include "TApplication.h"
-#include "TCanvas.h"
-#include "TPaveLabel.h"
-#include "TStyle.h"
-#include "TH1.h"
-#include "TH2.h"
-#include "TProfile.h"
-#include "TAxis.h"
-#include "TSystem.h"
 #include <time.h>
-
-
-// for saver
-#include "DimInfoHistos.h"
-#include "DimInfoTitle.h"
 
 // for online monitoring
 #include "GaudiKernel/IMonitorSvc.h" 
@@ -53,39 +38,81 @@
 #include "GaudiKernel/ISvcLocator.h"
 #include "GaudiKernel/Bootstrap.h"
 #include "RTL/rtl.h"
+#include "dic.hxx"
+#include "dis.hxx"
 
-
+#include "Gaucho/MonObject.h"
+#include "Gaucho/MonObjectCreator.h"
+#include "Gaucho/DimInfoMonObject.h"
+#include "DimInfoServerStatus.h"
+#include "Saver.h"
+#include "Misc.h"
 // Static Factory declaration
 DECLARE_ALGORITHM_FACTORY(Saver)
 
 // Constructor
 //------------------------------------------------------------------------------
 Saver::Saver(const std::string& name, ISvcLocator* ploc) : Algorithm(name, ploc)
-  {
+{
   declareProperty("nodename",m_nodeName);
-  declareProperty("histogramname",m_histogramName);
+  declareProperty("objectname",m_objectName);
   declareProperty("algorithmname",m_algorithmName);
   declareProperty("dimclientdns",m_dimClientDns);
   declareProperty("taskname",m_taskName);
   declareProperty("savedir",m_saveDir);
-  }
+
+  m_serverStatus = new std::map<std::string, bool, std::less<std::string> >();
+
+}
 
 //------------------------------------------------------------------------------
 StatusCode Saver::initialize() {
-  //------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
   StatusCode sc = Algorithm::initialize(); // must be executed first
+  MsgStream msg(msgSvc(), name());
   IIncidentSvc* incsvc;
   sc = service("IncidentSvc",incsvc);
   int priority=100;
   m_refreshTime=5;
+  
+  msg << MSG::DEBUG << "***************************************************** " << endreq;
+  msg << MSG::DEBUG << "*************Welcome to Saver v1r05******************* " << endreq;
+  msg << MSG::DEBUG << "***************************************************** " << endreq;
+  msg << MSG::DEBUG << "This Saver will save data published in : " << m_dimClientDns << endreq;
+  
+  msg << MSG::DEBUG << "Consider node " << m_nodeName << endreq;
+  
+  msg << MSG::DEBUG << "TaskNames: " << endreq;
+  for(unsigned int i=0; i < m_taskName.size();++i) {
+    msg << MSG::DEBUG << "         taskName: " << m_taskName[i] << endreq;
+    msg << MSG::DEBUG << endreq;
+  }
+  
+  msg << MSG::DEBUG << "Properties: " << endreq;
+  for(unsigned int i=0; i < m_objectName.size();++i) {
+    msg << MSG::DEBUG << "         objectName: " << m_objectName[i] << endreq;
+    msg << MSG::DEBUG << "         algorithmName: " << m_algorithmName[i] << endreq;
+    msg << MSG::DEBUG << endreq;
+  }
+  
   if (sc.isSuccess()) incsvc->addListener(this,"SAVE_HISTOS",priority);
-  MsgStream msg(msgSvc(), name());
 
   sc = service("HistogramDataSvc", histoSvc, true );
   if( !sc.isSuccess() )   {
     msg << MSG::FATAL << "Unable to locate HistogramSvc" << endreq;
     return sc;
   }
+  
+  msg << MSG::DEBUG << "creating Server/Service Status Objects" << endreq;
+  m_dimInfoServerStatus = new DimInfoServerStatus(m_serverStatus, 
+                                                  m_nodeName, 
+                                                  m_taskName, 
+                                                  msgSvc());
+  
+  m_fileName="Waiting for command to save histograms............."; 
+  m_fileSize=0;
+  declareInfo("file",m_fileName,"Filename of latest saveset");
+  declareInfo("filesize",m_fileSize,"Filesize of latest saveset");
 
   return StatusCode::SUCCESS;
 }
@@ -93,35 +120,22 @@ StatusCode Saver::initialize() {
 
 //------------------------------------------------------------------------------
 StatusCode Saver::execute() {
-  //------------------------------------------------------------------------------
-  MsgStream         msg( msgSvc(), name() );
+//------------------------------------------------------------------------------
+  MsgStream  msg( msgSvc(), name() );
+  //msg << MSG::INFO << "executing saver....command " << m_command << endreq;
 
-  char *service; 
-  char *format;
   time_t rawTime;
-  struct tm *timeInfo;
-  int type;
-  int icount=0;
-  int icount2d=0;
-  int icountp=0;
-  std::string hSvcName;
-  std::string hSvcName2d;
-  std::string hSvcNames;
-  std::string hSvcNames2d;
-  std::string pSvcName;
-  std::string pSvcNames;
-  std::string commentSvcNames;
-  std::string commentSvcName;
-  std::string filename;
+  struct tm * timeInfo;
+  
+  std::string monSvcname;
+  std::string monSvcnames;
+  
   char year[5];
   char month[3];
   char day[3];
   char hour[3];
   char min[3];
   char sec[3];
-  DimInfoHistos* hinfo=0;
-  DimInfoHistos* hinfo2d=0;
-  DimInfoHistos* pinfo=0;
   rawTime=time(NULL);
   timeInfo = localtime(&rawTime);
   strftime(year,sizeof(year),"%Y",timeInfo);
@@ -130,183 +144,34 @@ StatusCode Saver::execute() {
   strftime(hour,sizeof(hour),"%H",timeInfo);
   strftime(min,sizeof(min),"%M",timeInfo);
   strftime(sec,sizeof(sec),"%S",timeInfo);
-  TFile *f=0;
-  if (command=="SAVE_HISTOS") {
-  //NB the ISO extended timestamp format is yearmonthdayThoursminssecs
-  //can't use separators as ":" is no good for windows filenames
-  //stick strictly to this standard so boost can parse it
-  filename=m_saveDir+"/"+m_taskName+year+month+day+"T"+hour+min+sec+".root";  
-  msg << MSG::DEBUG << "Saving to filename: " <<filename.c_str() << endreq;  
-  for (int j=0; j<= (int)m_histogramName.size()-1;j++) { 
-    //j counts the histograms
-    std::vector<TH1*> hist;
-    std::vector<TH2*> hist2d;
-    std::vector<TProfile*> histp;
-    hSvcNames="H1D/"+m_nodeName+"*"+m_algorithmName[j]+"/"+m_histogramName[j];
-    hSvcNames2d="H2D/"+m_nodeName+"*"+m_algorithmName[j]+"/"+m_histogramName[j];
-    pSvcNames="HPD/"+m_nodeName+"*"+m_algorithmName[j]+"/"+m_histogramName[j];
-    commentSvcNames=m_nodeName+"*"+m_algorithmName[j]+"/"+m_histogramName[j]+"/gauchocomment";
-     
-    DimClient::setDnsNode(m_dimClientDns.c_str());  
-    DimBrowser dbr;  
-    icount = 0;
-    icount2d = 0;
-    icountp = 0;
-     // icount is the number of tasks publishing histogram nr j
-    while ((( icount==0 )|(icount2d==0))|(icountp==0))
-    {
-      //look for the services at each iteration of eventloop - task may have died
-      msg << MSG::DEBUG << "Looking for hSvcName: " << hSvcNames.c_str() << endreq;     
-      dbr.getServices(hSvcNames.c_str());
-      while( (type = dbr.getNextService(service, format)) )
-      { 
-	hSvcName=service;
-        msg << MSG::DEBUG << "Found service: " << hSvcName << endreq;     
-	icount++;
-      }   
-      if (icount==0) {
-        msg << MSG::DEBUG << "Looking for hSvcName2d: " << hSvcNames2d.c_str() << endreq;     
-        dbr.getServices(hSvcNames2d.c_str());
-        while( (type = dbr.getNextService(service, format)) )
-        { 	
-	  hSvcName2d=service;
-          msg << MSG::DEBUG << "Found 2D service: " << hSvcName2d << endreq;   
-  	  icount2d++;
-	} 
-      }  
-      if (icount2d==0) {
-        msg << MSG::DEBUG << "Looking for pSvcName: " << pSvcNames.c_str() << endreq;     
-        dbr.getServices(pSvcNames.c_str());
-        while( (type = dbr.getNextService(service, format)) )
-        { 	
-	  pSvcName=service;
-          msg << MSG::DEBUG << "Found profile service: " << hSvcName2d << endreq;   
-  	  icountp++;		 
-	} 
-      } 
-      dbr.getServices(commentSvcNames.c_str());
-      while( (type = dbr.getNextService(service, format)) )
-      {               
-	commentSvcName=service;
-        msg << MSG::DEBUG << "Found comment service: " << commentSvcName  <<endreq;     
-      }
-      if (((icount>0)|(icount2d>0))|(icountp>1)) break;      
-      else lib_rtl_sleep(500);
-    } 
   
-
-    if (icount>0) {     
-      hinfo = new DimInfoHistos(hSvcName,m_refreshTime);
+  TFile *f=0;
+  if (m_command=="SAVE_HISTOS") {
+     msg << MSG::INFO << "SAVE_HISTOS" << endreq;
+    for (unsigned int i = 0;i < m_taskName.size();i++) {
+      m_fileSize=0;
+      //set the filesize to 0 when starting to save
+      //NB the ISO extended timestamp format is yearmonthdayThoursminssecs
+      //can't use separators as ":" is no good for windows filenames
+      //stick strictly to this standard so boost can parse it
+      m_fileName=m_saveDir+"/"+m_taskName[i]+year+month+day+"T"+hour+min+sec+".root";
+      msg << MSG::INFO << "Saver will save histograms in file " << m_fileName << endreq;
+      msg << MSG::INFO << "Creating DimNames"<< endreq;
+      CreateDimNames(m_nodeName, m_taskName[i], m_algorithmName, m_objectName);
+      msg << MSG::INFO << "Creating MonObjects"<< endreq;
+      CreateMonObjects(m_refreshTime);
+      f = new TFile(m_fileName.c_str(),"create");
+      msg << MSG::INFO << "Writing MonObjects"<< endreq;
+      WriteMonObjects();
+      m_fileSize=(int)f->GetSize();
+      f->Close();
+      delete f;
+      f=0;
+      msg << MSG::INFO << "Deleting MonObjects"<< endreq;
+      DeleteMonObjects();
+      m_command="";
     }
-    if (icount2d>0) {
-      hinfo2d = new DimInfoHistos(hSvcName2d,m_refreshTime);
-    }
-    if (icountp>0) {
-      pinfo = new DimInfoHistos(pSvcName,m_refreshTime);
-    }
-
-    // convert DIM buffer to ROOT
-
-
-    if (icount>0) {
-      TH1* hist1=0;
-      int ntries=0;
-      DimInfoTitle * ttemp;
-      char* temptitle=0;  
-      while (1)
-      {
-        hist1=0;
-	hist1= hinfo->getHisto();
-        if (hist1 != 0) {
-	  ttemp = new DimInfoTitle(commentSvcName,m_refreshTime);
-	  lib_rtl_sleep(m_refreshTime*100);    	
-	  temptitle =ttemp->getTitle();
-          if (temptitle!=0) { 		 
-	    hist1->SetTitle(temptitle);
-	  }
-
-	  msg << MSG::DEBUG << "Histogram "<< hist1->GetTitle() << " found." << endreq;   
-	  if ( 0 == f ) {
-	    f = new TFile(filename.c_str(),"create"); 
-	  }
-	  hist1->Write();
-	 
-	  break;
-	}  	     
-        ntries++;
-	msg << MSG::DEBUG << "No histogram found after " << ntries << " attempts." << endreq;
-        if(ntries==10) break;
-        lib_rtl_sleep(500); 
-      } 
-    } 
-    if (icount2d>0) {
-      TH2* hist2=0;
-      int ntries=0;  
-      DimInfoTitle * ttemp;
-      char* temptitle=0;         
-      while (1)
-      {
-        hist2=0;
-        hist2= hinfo2d->get2DHisto();
-        if (hist2 != 0) {
-	  ttemp = new DimInfoTitle(commentSvcName,m_refreshTime);
-	  lib_rtl_sleep(m_refreshTime*100);    	
-	  temptitle =ttemp->getTitle();
-          if (temptitle!=0) { 		 
-	    hist2->SetTitle(temptitle);
-	  }
-	  msg << MSG::DEBUG << "Histogram "<< hist2->GetTitle() << " found." << endreq;   
-	  if ( 0 == f ) {	      
-	    f = new TFile(filename.c_str(),"create");
-	  }   
-	  hist2->Write();		 
-	  break;
-        }    
-        ntries++;
-        msg << MSG::DEBUG << "No histogram found after " << ntries << " attempts." << endreq;
-        if(ntries==10) break;
-        lib_rtl_sleep(500); 
-      } 
-    }   
-
-    if (icountp>0) {
-      TProfile* histp=0;
-      int ntries=0;
-      DimInfoTitle * ttemp;
-      char* temptitle=0;           
-      while (1)
-      {
-        histp=0;
-        histp= pinfo->getPDHisto();
-        if (histp != 0) {
-	  ttemp = new DimInfoTitle(commentSvcName,m_refreshTime);
-	  lib_rtl_sleep(m_refreshTime*100);    	
-	  temptitle =ttemp->getTitle();
-          if (temptitle!=0) { 		 
-	    histp->SetTitle(temptitle);
-	  }
-	  msg << MSG::DEBUG << "Histogram "<< histp->GetTitle() << " found." << endreq;   
-	  if ( 0 == f ) {	      
-	    f = new TFile(filename.c_str(),"create");
-	  }   
-	  histp->Write();		 
-	  break;
-        }    
-        ntries++;
-        msg << MSG::DEBUG << "No histogram found after " << ntries << " attempts." << endreq;
-        if(ntries==10) break;
-        lib_rtl_sleep(500); 
-      } 
-    }   	       
   }
-    f->Close();
-    delete f;
-    delete hinfo;
-    delete hinfo2d;
-    delete pinfo;
-    command="";
-  }
- 
   return StatusCode::SUCCESS;
 }
 
@@ -315,10 +180,8 @@ void Saver::handle(const Incident& inc) {
   //------------------------------------------------------------------------------
   MsgStream msg(msgSvc(), name());
   msg << MSG::INFO << "Got incident " << inc.type() << " from " << inc.source() <<endmsg;
-  command=inc.type();
-
+  m_command=inc.type();
 }
-
 
 
 //------------------------------------------------------------------------------
@@ -329,3 +192,121 @@ StatusCode Saver::finalize() {
 //  delete f;
   return StatusCode::SUCCESS;
 }
+
+void Saver::CreateDimNames(std::string nodeName, std::string taskName, std::vector<std::string> algorithmName, std::vector<std::string> objectName){
+   
+  MsgStream mes1(msgSvc(), name());
+  DimClient::setDnsNode(m_dimClientDns.c_str());
+  DimBrowser m_dimBrowser;
+  for (unsigned int j=0; j< objectName.size() ;j++) {
+    //Ex--> MON/node00101_Test_1/GaudiExample/ExMonH1D
+    std::string svcName = "*/" + nodeName+"*"+taskName +"*"+algorithmName[j]+"/"+objectName[j];
+
+    //mes1 << MSG::INFO << "svcName=" + svcName  << endreq;
+    
+    m_dimBrowser.getServices(svcName.c_str());
+
+    char *service;
+    char *format;
+    int type;
+    int count=0;
+    std::vector<std::string> dimInfoNamesVect1;
+
+    while (count==0)
+    {
+
+      while (type = m_dimBrowser.getNextService(service, format))
+      {
+        std::string  serviceStr = service;//Ex--> MON/node00101_Test_1/GaudiExample/ExMonH1D
+        mes1 << MSG::INFO << "Service found: " + serviceStr << endreq;
+        dimInfoNamesVect1.push_back(serviceStr);
+        count++;
+      } 
+      if (count>0) break;
+      else lib_rtl_sleep(500);
+    }
+
+    /************************************************/
+    m_dimInfoNamesVect2.push_back(dimInfoNamesVect1);
+    /************************************************/
+  }
+  // print for test
+/*    mes1 << MSG::DEBUG << "PRINTING DIM NAMES : " << endreq;
+
+    for (unsigned int k = 0; k < m_dimInfoNamesVect2.size(); k++) {
+      for (unsigned int j = 0; j < m_dimInfoNamesVect2[k].size(); j++) {
+        mes1 << MSG::DEBUG << "	Info : " << m_dimInfoNamesVect2[k][j] << endreq;
+      }
+    }*/
+}
+
+void Saver::CreateMonObjects(int refreshTime) {
+  MsgStream msg(msgSvc(), name());
+  for (unsigned int k = 0; k < m_dimInfoNamesVect2.size(); k++) {
+    std::vector<MonObject*> tmpMonObjectVect1;
+    for (unsigned int i = 0; i < m_dimInfoNamesVect2[k].size(); i++) {
+      std::string serverName = getServerName(m_dimInfoNamesVect2[k][i]);
+      if (!m_dimInfoServerStatus->isServerActive(serverName)) {
+        msg << MSG::WARNING <<"Server "<< serverName << " desactived. MonObjects in this server will not be Saved." << endreq;
+        continue;
+      }
+      DimInfoMonObject* tmpDimInfoMonObject = new DimInfoMonObject(m_dimInfoNamesVect2[k][i], refreshTime, m_source);
+      tmpDimInfoMonObject->setMsgSvc(msgSvc());
+      tmpDimInfoMonObject->createMonObject();
+      MonObject *tmpMonObject = 0;
+      int ntries=0;
+      while (1)
+      {
+        if (tmpDimInfoMonObject->monObject() != 0){
+          //tmpMonObject = tmpDimInfoMonObject->monObject();
+          tmpMonObject = MonObjectCreator::createMonObject(tmpDimInfoMonObject->monObject()->typeName(), msgSvc(), name());
+	  tmpMonObject->copyFrom(tmpDimInfoMonObject->monObject());
+          break;
+        }
+        ntries++;
+
+        msg << MSG::WARNING <<"No MonObject found after " << ntries << " attempts." << endreq;
+
+        if(ntries==10) break;
+        lib_rtl_sleep(500); 
+      }
+      tmpMonObjectVect1.push_back(tmpMonObject);
+      if (tmpDimInfoMonObject) {delete tmpDimInfoMonObject;tmpDimInfoMonObject=0;}
+    }
+    m_monObjectVect2.push_back(tmpMonObjectVect1);
+  }
+}
+
+void Saver::WriteMonObjects(){
+  for (unsigned int k = 0; k < m_monObjectVect2.size(); k++) {
+    for (unsigned int i = 0; i < m_monObjectVect2[k].size(); i++) {
+      if (m_monObjectVect2[k][i] != 0)
+        m_monObjectVect2[k][i]->write();
+    }
+  }
+}
+
+void Saver::DeleteMonObjects(){
+  for (unsigned int k = 0; k < m_monObjectVect2.size(); k++) {
+    for (unsigned int i = 0; i < m_monObjectVect2[k].size(); i++) {
+      if (m_monObjectVect2[k][i] != 0)
+        delete m_monObjectVect2[k][i];
+        m_monObjectVect2[k][i] = 0;
+    }
+  }
+  for (unsigned int k = 0; k < m_monObjectVect2.size(); k++) {
+    m_monObjectVect2[k].clear();
+  }
+  m_monObjectVect2.clear();
+  for (unsigned int k = 0; k < m_dimInfoNamesVect2.size(); k++) {
+    m_dimInfoNamesVect2[k].clear();
+  }
+  m_dimInfoNamesVect2.clear();
+}
+
+std::string Saver::getServerName(const std::string &serviceName){
+  std::vector<std::string> serviceParts = Misc::splitString(serviceName, "/");
+  return serviceParts[1];
+}
+
+
--- Online/Online/Saver/src/Saver.h
+++ Online/Online/Saver/src/Saver.h
@@ -5,43 +5,51 @@
 #include "GaudiKernel/Algorithm.h"
 #include "GaudiKernel/Property.h"
 #include "GaudiKernel/IHistogramSvc.h"
-#include "TH1.h"
-#include "TH2.h"
-#include "TFile.h"
-#include "TProfile.h"
 #include "GaudiKernel/IIncidentListener.h"
+#include "TFile.h"
+#include "dis.hxx"
+
 #include<time.h>
 
-/** @class Saver
-
-
-*/
-
-// Forward declarations
 class IIncidentSvc;
+class DimInfoMonObject;
+class DimInfoServerStatus;
+class MonObject;
+
 class Saver : public Algorithm, virtual public IIncidentListener {
 public:
-  /// Constructor of this form must be provided
   Saver(const std::string& name, ISvcLocator* pSvcLocator); 
-
-  /// Three mandatory member functions of any algorithm
   StatusCode initialize();
   StatusCode execute();
   StatusCode finalize();
   virtual void handle(const Incident& inc);
-private:
 
+private:
+  void WriteMonObjects();
+  void CreateDimNames(std::string nodeName, std::string taskName, std::vector<std::string> algorithmName, std::vector<std::string> objectName);
+  void CreateMonObjects(int refreshTime);
+  void DeleteMonObjects();
+  std::string getServerName(const std::string &serviceName);
+
+  // Ask Eric if this service is necessary here...
   IHistogramSvc* histoSvc;
- // TFile *f;
+  std::string   m_source;
+  std::vector<std::vector<MonObject*> >  m_monObjectVect2;
+  std::vector<std::vector<std::string> >  m_dimInfoNamesVect2;
+  std::vector<std::vector<DimInfoMonObject*> >  m_dimInfoMonObjectVect2;
   std::string m_nodeName; 
-  std::vector<std::string> m_histogramName; 
+  std::vector<std::string> m_objectName; 
   std::vector<std::string> m_algorithmName; 
+  std::vector<std::string> m_taskName;
   int m_refreshTime;
   std::string m_dimClientDns;
-  std::string m_taskName;
   std::string m_saveDir;
-  std::string command;
-  
+  std::string m_command;
+  std::string m_fileName;
+  int m_fileSize;
+  DimInfoServerStatus *m_dimInfoServerStatus;
+  std::map<std::string, bool, std::less<std::string> > *m_serverStatus;
+  std::map<std::string, bool, std::less<std::string> >::iterator  m_serverIt;
 
 };
 
