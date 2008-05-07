09e42bb7db5dd0797fa8d5f02a6ce967532bc23f
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-05-07 14:45:15 +0000

    v5r7
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@59429 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2d36c1b8cc112269e66ff4e6407ec1ded11dfa9f

--- LHCb/OT/OTDAQ/OTDAQ/OTChannelMap.h
+++ LHCb/OT/OTDAQ/OTDAQ/OTChannelMap.h
@@ -1,6 +1,7 @@
 #ifndef OTDAQ_OTCHANNELMAP_H_
 #define OTDAQ_OTCHANNELMAP_H_
 
+#include <vector>
 #include "Kernel/OTChannelID.h"
 
 class OTChannelMapTool ;
@@ -48,7 +49,7 @@ namespace OTDAQ
       // Fills the inverse map
       memset(m_strawToChannel,0,NumStraws*sizeof(unsigned char)) ;
       for(unsigned char ichan = 0; ichan < m_nchannels; ++ichan)
-	m_strawToChannel[m_channelToStraw[ichan]-OffsetStraws] = ichan ;
+        m_strawToChannel[m_channelToStraw[ichan]-OffsetStraws] = ichan ;
     }
     
     class Detector
@@ -58,38 +59,50 @@ namespace OTDAQ
       Detector() : m_modules(NumDefaultModuleLayouts) {}
       
       const Module& module(const LHCb::OTChannelID& modid) const {
-	return module(modid.station(),modid.layer(),modid.quarter(),modid.module()) ;
+        return module(modid.station(),modid.layer(),modid.quarter(),modid.module()) ;
       }
       
-      const Module& module(unsigned int station, unsigned int layer, unsigned int quadrant, unsigned int module) const {
-	return m_modules[layout(station,layer,quadrant,module)] ;
+      const Module& module( unsigned int station,  unsigned int layer,
+                            unsigned int quadrant, unsigned int module) const {
+        return m_modules[layout(station,layer,quadrant,module)] ;
       }
       ;
-      unsigned int straw(unsigned int station, unsigned int layer, unsigned int quadrant, unsigned int moduleid, unsigned int channelid ) const { 
-	return module(station,layer,quadrant,moduleid).straw(channelid) ;
+      unsigned int straw( unsigned int station,  unsigned int layer, 
+                          unsigned int quadrant, unsigned int moduleid,
+                          unsigned int channelid                        ) const {
+        return module(station,layer,quadrant,moduleid).straw(channelid) ;
       }
-      unsigned int channel(unsigned int station, unsigned int layer, unsigned int quadrant, unsigned int moduleid, unsigned int strawid ) const { 
-	return module(station,layer,quadrant,moduleid).channel(strawid) ;
+      unsigned int channel( unsigned int station,  unsigned int layer,
+                            unsigned int quadrant, unsigned int moduleid,
+                            unsigned int strawid                        ) const {
+        return module(station,layer,quadrant,moduleid).channel(strawid) ;
       }
       
-      unsigned char layout(unsigned int station, unsigned int layer, unsigned int quadrant, unsigned int module) const {
-	return m_modulepointers[station-OffsetStations][layer-OffsetLayers][quadrant-OffsetQuadrants][module-OffsetModules] ;
+      unsigned char layout( unsigned int station,  unsigned int layer,
+                            unsigned int quadrant, unsigned int module ) const {
+        return m_modulepointers[station-OffsetStations][layer-OffsetLayers][quadrant-OffsetQuadrants][module-OffsetModules] ;
       } ;
       
       unsigned char layout(const LHCb::OTChannelID& modid) const {
-	return layout( modid.station(),modid.layer(),modid.quarter(),modid.module()) ;
+        return layout( modid.station(),modid.layer(),modid.quarter(),modid.module()) ;
       } ;
       
       private:
       friend class OTChannelMapTool ;
       
-      Module& module(unsigned int station, unsigned int layer, unsigned int quadrant, unsigned int module) {
-	return m_modules[m_modulepointers[station-OffsetStations][layer-OffsetLayers][quadrant-OffsetQuadrants][module-OffsetModules]] ;
+      Module& module( unsigned int station,  unsigned int layer,
+                      unsigned int quadrant, unsigned int module) {
+        return m_modules[m_modulepointers[station-OffsetStations]
+                                         [layer-OffsetLayers]
+                                         [quadrant-OffsetQuadrants]
+                                         [module-OffsetModules]] ;
       }
       
       void setModuleLayout( const LHCb::OTChannelID& modid, unsigned char layout) {
-	m_modulepointers[modid.station()-OffsetStations]
-	  [modid.layer()-OffsetLayers][modid.quarter()-OffsetQuadrants][modid.module()-OffsetModules] = layout ;
+        m_modulepointers[modid.station()-OffsetStations]
+                        [modid.layer()-OffsetLayers]
+                        [modid.quarter()-OffsetQuadrants]
+                        [modid.module()-OffsetModules] = layout ;
       }
       
       private:  
