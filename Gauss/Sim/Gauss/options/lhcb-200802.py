893ac370b91a829de98046f0610822809c3a2fa7
Gloria Corti <gloria.corti@cern.ch>
2008-05-08 20:35:05 +0000

    use head geometry but for TT
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93357 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1c54ba7aed60e5b409b15c67218b72cf5f7b1b69

---
+++ Gauss/Sim/Gauss/options/lhcb-200802.py
@@ -0,0 +1,20 @@
+# Minimal file for running Gauss from python prompt
+# Syntax is:
+#   gaudirun.py $GAUSSOPTS/lhcb-200802.py
+#
+from Gaudi.Configuration import *
+
+#--Allow to use ns (ie. double) in options of EventClockSvc
+from Configurables import EventClockSvc, FakeEventTime
+
+ec = EventClockSvc()
+ec.EventTimeDecoder = FakeEventTime().name()
+ec.FakeEventTime = FakeEventTime()
+
+#--Geometry dependent options
+importOptions('$GAUSSOPTS/lhcb-200802.opts')
+
+#--Application name and version
+ApplicationMgr( AppName    = 'Gauss',
+                AppVersion = 'v31r0' )
+
