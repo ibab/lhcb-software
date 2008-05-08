61ab65d6cacd882e0a0c324ba36842ab054cada0
Gloria Corti <gloria.corti@cern.ch>
2008-05-08 20:41:41 +0000

    no longer supported geometry
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93360 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2d39b6f55b50883089826062c6c6d220c2e43505

--- Gauss/Sim/Gauss/options/v200601.py
+++
@@ -1,8 +0,0 @@
-from Gaudi.Configuration import *
-from DetDescSvc.DetDescSvcConf import *
-
-ec = EventClockSvc()
-ec.EventTimeDecoder = FakeEventTime().name()
-ec.FakeEventTime = FakeEventTime()
-
-importOptions("$GAUSSOPTS/v200601.opts")
