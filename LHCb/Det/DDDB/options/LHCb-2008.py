9b930b62da95f68a54359c51aeb56688190f8cb2
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-05-09 13:23:16 +0000

    file for 2008 defaults
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36514 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 79197656fd22764f1686dc87f2183d4d75697245

---
+++ LHCb/Det/DDDB/options/LHCb-2008.py
@@ -0,0 +1,17 @@
+from Gaudi.Configuration import * 
+# Defines default conditions for 2008 LHCb detector
+
+from Configurables import CondDBAccessSvc, MagneticFieldSvc
+import os
+
+# 2008 database tags
+tag = "head-20080422"
+CondDBAccessSvc("DDDB",DefaultTAG = tag)
+CondDBAccessSvc("LHCBCOND",DefaultTAG = tag)
+
+# Measured magnetic field
+MagneticFieldSvc().UseRealMap   = true
+MagneticFieldSvc().FieldMapFileQ1 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c1.vp.cdf")
+MagneticFieldSvc().FieldMapFileQ2 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c2.vp.cdf")
+MagneticFieldSvc().FieldMapFileQ3 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c3.vp.cdf")
+MagneticFieldSvc().FieldMapFileQ4 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c4.vp.cdf")
