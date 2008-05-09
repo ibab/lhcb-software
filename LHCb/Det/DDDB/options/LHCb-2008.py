eb80fc72cb750a96cf1de108f1dd5822149c10fd
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-05-09 13:58:46 +0000

    fix typo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36515 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: aedfeb46868e8e4611e26334eea30ab331e558a2

--- LHCb/Det/DDDB/options/LHCb-2008.py
+++ LHCb/Det/DDDB/options/LHCb-2008.py
@@ -10,7 +10,7 @@ CondDBAccessSvc("DDDB",DefaultTAG = tag)
 CondDBAccessSvc("LHCBCOND",DefaultTAG = tag)
 
 # Measured magnetic field
-MagneticFieldSvc().UseRealMap   = true
+MagneticFieldSvc().UseRealMap = True
 MagneticFieldSvc().FieldMapFileQ1 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c1.vp.cdf")
 MagneticFieldSvc().FieldMapFileQ2 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c2.vp.cdf")
 MagneticFieldSvc().FieldMapFileQ3 =  os.path.join(os.environ["FIELDMAPROOT"],"cdf","field048.c3.vp.cdf")
