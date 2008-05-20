88df52847e1bfb07944935edb3ddcd2b3fc12618
Stephanie Hansmann-Menzemer <menzemer@physi.uni-heidelberg.de>
2008-05-20 08:21:09 +0000

    add detailed Bethe Bloch correction tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@17828 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 50056d5763668241896cc4bdcdabfe11f88ff884

---
+++ Rec/Tr/TrackTools/src/StateDetailedBetheBlochEnergyCorrectionTool.cpp
@@ -0,0 +1,109 @@
+// Include files
+// -------------
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h" 
+#include "GaudiKernel/ParticleProperty.h"
+
+// from GSL
+#include "gsl/gsl_math.h"
+
+// from DetDesc
+#include "DetDesc/Material.h"
+
+
+// local
+#include "StateDetailedBetheBlochEnergyCorrectionTool.h"
+
+using namespace Gaudi::Units;
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : StateDetailedBetheBlochEnergyCorrectionTool
+//
+// 2006-08-18 : Eduardo Rodrigues
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( StateDetailedBetheBlochEnergyCorrectionTool );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+StateDetailedBetheBlochEnergyCorrectionTool::StateDetailedBetheBlochEnergyCorrectionTool( const std::string& type,
+                                                                                      const std::string& name,
+                                                                                      const IInterface* parent )
+  : GaudiTool ( type, name , parent )
+{
+  declareInterface<IStateCorrectionTool>(this);  
+  declareProperty( "EnergyLossFactor",  m_energyLossCorr = 1.0 );
+  declareProperty( "MaximumEnergyLoss", m_maxEnergyLoss  = 100. * MeV           );
+}
+
+//=============================================================================
+// Correct a State for dE/dx energy losses
+//=============================================================================
+void StateDetailedBetheBlochEnergyCorrectionTool::correctState( LHCb::State& state,
+                                                              const Material* material,
+                                                              double wallThickness,
+                                                              bool upstream, 
+                                                              LHCb::ParticleID pid) const
+{
+  
+  // apply correction - note for now only correct the state vector
+  Gaudi::TrackVector& tX = state.stateVector();
+  
+  if(!m_pp->findByStdHepID(pid.pid()))
+    return;
+ 
+  double mass = m_pp->findByStdHepID(pid.pid())->mass();
+  
+  double eta = 1/(tX[4]*mass);
+
+  double beta2 = (eta*eta)/(1.0+eta*eta);
+  double gamma2 = 1/((1-beta2+1e-12));
+  double me = 511000*Gaudi::Units::eV;
+
+  double x = log(eta*eta)/4.606;
+  double rho = 0;
+  
+  if (x > material->X0())
+      if (x < material->X1())
+	  rho = 4.606*x-material->C() + material->a()*pow(material->X1()-x,material->m());
+      else
+	  rho = 4.606*x-material->C();
+
+
+  double eLoss =  m_energyLossCorr*wallThickness
+     * sqrt( 1. + gsl_pow_2(state.tx()) + gsl_pow_2(state.ty()) )
+    * 30.71 * MeV*mm2/mole * material->Z() * material->density() / material->A()
+      *(log(2*me*beta2*gamma2/material->I())- rho*0.5)/beta2;
+ 
+  eLoss = GSL_MIN( m_maxEnergyLoss, eLoss );
+   if ( !upstream ) eLoss *= -1.;
+  
+  
+  if ( tX[4] > 0. ) tX[4] = 1. / ( 1./tX[4]+(eLoss) );
+  else              tX[4] = 1. / ( 1./tX[4]-(eLoss) );
+
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+StateDetailedBetheBlochEnergyCorrectionTool::~StateDetailedBetheBlochEnergyCorrectionTool() {}
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode StateDetailedBetheBlochEnergyCorrectionTool::initialize()
+{
+  StatusCode sc = GaudiTool::initialize();
+  if (sc.isFailure()) return sc;  // error already reported by base class
+  
+  m_pp = svc<IParticlePropertySvc>( "ParticlePropertySvc", true );
+ 
+  
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
---
+++ Rec/Tr/TrackTools/src/StateDetailedBetheBlochEnergyCorrectionTool.h
@@ -0,0 +1,52 @@
+#ifndef TRACKTOOLS_STATEDETAILEDBETHEBLOCHENERGYCORRECTIONTOOL_H 
+#define TRACKTOOLS_STATEDETAILEDBETHEBLOCHENERGYCORRECTIONTOOL_H 1
+
+// Include files
+// -------------
+// from Gaudi
+#include "GaudiAlg/GaudiTool.h"
+
+// from TrackInterfaces
+#include "TrackInterfaces/IStateCorrectionTool.h"
+#include "GaudiKernel/IParticlePropertySvc.h"
+
+// from TrackEvent
+#include "Event/State.h"
+
+/** @class StateDetailedBetheBlochEnergyCorrectionTool
+ *  
+ *  This state correction tool applies a dE/dx energy loss correction
+ *  with the full version of the Bethe-Bloch equation.
+ *
+ *  @author Stephanie Hansmann-Menzemer
+ *  @date   2008-05-02
+ *  
+ */
+class StateDetailedBetheBlochEnergyCorrectionTool : public GaudiTool,
+                                                  virtual public IStateCorrectionTool {
+public:
+  /// Correct a State for dE/dx energy losses with a simplified Bethe-Bloch equiaton
+  void correctState( LHCb::State& state,
+                     const Material* material,
+                     double wallThickness = 0,
+                     bool upstream = true,
+                     LHCb::ParticleID pid = LHCb::ParticleID(211) ) const ;
+  
+  /// Standard constructor
+  StateDetailedBetheBlochEnergyCorrectionTool( const std::string& type, 
+                                             const std::string& name,
+                                             const IInterface* parent);
+  
+  virtual ~StateDetailedBetheBlochEnergyCorrectionTool( ); ///< Destructor
+  
+  StatusCode initialize();
+  
+protected:
+  
+private:
+  // Job options
+  double m_energyLossCorr;      ///< tunable energy loss correction
+  double m_maxEnergyLoss;       ///< maximum energy loss in dE/dx correction
+  IParticlePropertySvc* m_pp;   /// particle property service
+};
+#endif // TRACKTOOLS_STATEDETAILEDBETHEBLOChENERGYCORRECTIONTOOL_H
