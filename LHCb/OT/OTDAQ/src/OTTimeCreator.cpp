a412ff574b5e295bb9570bd7451953056d2c3b3b
hterrier <hterrier@cern.ch>
2008-05-27 13:48:17 +0000

    made decoder private tool check on duplicate hits before filling keyed container
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@59464 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eb6c351e6da5b9222ec3b78dd0888271d5add567

--- LHCb/OT/OTDAQ/src/OTTimeCreator.cpp
+++ LHCb/OT/OTDAQ/src/OTTimeCreator.cpp
@@ -1,4 +1,4 @@
-// $Id: OTTimeCreator.cpp,v 1.21 2007-11-14 16:05:39 wouter Exp $
+// $Id: OTTimeCreator.cpp,v 1.22 2008-05-27 13:48:17 hterrier Exp $
 // Include files
 // Gaudi
 #include "GaudiKernel/AlgFactory.h"
@@ -55,7 +55,7 @@ StatusCode OTTimeCreator::initialize() {
   m_tracker = getDet<DeOTDetector>( DeOTDetectorLocation::Default );
 
   // tool handle to the ot raw bank decoder
-  m_rawBankDecoder = tool<IOTRawBankDecoder>("OTRawBankDecoder");
+  m_rawBankDecoder = tool<IOTRawBankDecoder>("OTRawBankDecoder",this);
   
   return sc;
 };
@@ -71,14 +71,23 @@ StatusCode OTTimeCreator::execute()
   // register output buffer to TES
   put(outputTimes, m_timeLocation);
 
-  // Load all modules
+  // Load all modules.
   for(DeOTDetector::Modules::const_iterator imod = m_tracker->modules().begin() ;
       imod != m_tracker->modules().end(); ++imod) {
     LHCb::OTLiteTimeRange ottimes = m_rawBankDecoder->decodeModule( (*imod)->elementID() ) ;
-    
-    for( LHCb::OTLiteTimeRange::const_iterator it = ottimes.begin() ; it != ottimes.end(); ++it) 
-      outputTimes->insert(new OTTime(it->channel(),it->calibratedTime())) ;
+    for( LHCb::OTLiteTimeRange::const_iterator it = ottimes.begin() ; it != ottimes.end(); ++it) {
+      bool found(false) ;
+      for( LHCb::OTLiteTimeRange::const_iterator jt = ottimes.begin() ; it != jt && !found; ++jt)
+	found = it->channel()==jt->channel() ;
+      if(found) 
+	warning() << "Found duplicate hit!" << it->channel() << endreq ;
+      else
+	outputTimes->insert(new OTTime(it->channel(),it->calibratedTime())) ;
+    }
   }
   
+  debug() << " OTTimeCreator created " << outputTimes->size() 
+          << " OTTime(s) at " << m_timeLocation << endmsg;
+
   return StatusCode::SUCCESS;
 }
