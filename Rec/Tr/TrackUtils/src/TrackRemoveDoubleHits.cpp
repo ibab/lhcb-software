819466ad0c3616c27b8d112c8ff28b50f9834932
lnicolas <lnicolas@cern.ch>
2008-05-28 12:33:52 +0000

    TrackRemoveDoubleHits back to where it should have stayed
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@17862 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6e731a9d28d0a37e7af2a9aec612a0de71ae025a

--- Rec/Tr/TrackUtils/src/TrackRemoveDoubleHits.cpp
+++ Rec/Tr/TrackUtils/src/TrackRemoveDoubleHits.cpp
@@ -1,4 +1,4 @@
-// $Id: TrackRemoveDoubleHits.cpp,v 1.5 2008-05-27 08:42:01 lnicolas Exp $
+// $Id: TrackRemoveDoubleHits.cpp,v 1.6 2008-05-28 12:33:52 lnicolas Exp $
 //
 
 //-----------------------------------------------------------------------------
@@ -36,21 +36,21 @@ TrackRemoveDoubleHits::TrackRemoveDoubleHits ( const std::string& name,
   this -> declareProperty ( "TTLiteClustersLocation",
                             m_ttLiteClustersPath = LHCb::STLiteClusterLocation::TTClusters );
   this -> declareProperty ( "TTClustersLocation",
-                            m_ttClustersPath = LHCb::STClusterLocation::TTClusters );
+                            m_ttClustersPath = LHCb::STClusterLocation::TTClusters         );
 
   this -> declareProperty ( "ITLiteClustersLocation",
                             m_itLiteClustersPath = LHCb::STLiteClusterLocation::ITClusters );
   this -> declareProperty ( "ITClustersLocation",
-                            m_itClustersPath = LHCb::STClusterLocation::ITClusters );
+                            m_itClustersPath = LHCb::STClusterLocation::ITClusters         );
 
   this -> declareProperty ( "OTTimesLocation",
-                            m_otTimesPath = LHCb::OTTimeLocation::Default );
+                            m_otTimesPath = LHCb::OTTimeLocation::Default                  );
   
 
   // Do we want to keep the strip with the highest charge of a double hit?
   // OR
   // Do we want to keep the strip with a high threshold of a double hit?
-  this -> declareProperty ( "KeepHighCharge", m_keepHighCharge = false );
+  this -> declareProperty ( "KeepHighCharge",    m_keepHighCharge    = false );
   this -> declareProperty ( "KeepHighThreshold", m_keepHighThreshold = false );
 }
 //===========================================================================
@@ -69,8 +69,7 @@ TrackRemoveDoubleHits::~TrackRemoveDoubleHits ( ) {
 //===========================================================================
 StatusCode TrackRemoveDoubleHits::initialize ( ) {
 
-  if ( msgLevel( MSG::DEBUG ) )
-    debug() << "==> Initialize" << endmsg;
+  debug() << "==> Initialize" << endmsg;
 
   StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
   if ( sc.isFailure() ) return Error( "Failed to initialize" );
@@ -80,29 +79,31 @@ StatusCode TrackRemoveDoubleHits::initialize ( ) {
               << endmsg
               << "User has to set one of the two properties to false!"
               << endmsg
-              << "Will remove both measurements of the track in case of a ST double hit!"
+              << "Will remove both LHCbIDs of the track in case of a ST double hit!"
               << endmsg;
     m_keepHighThreshold = false;
-    m_keepHighCharge = false;
+    m_keepHighCharge    = false;
   }
   else if ( m_keepHighThreshold )
-    info() << "Double ST hit: will remove one measurement LHCbID from the track"
+    info() << "Double ST hit: will remove one LHCbID from the track"
            << " if one of the two only has a high threshold." << endmsg;
   else if ( m_keepHighCharge )
-    info() << "Double ST hit: will remove from the track the measurement"
+    info() << "Double ST hit: will remove from the track the LHCbID"
            << " of the strip that has the lowest charge." << endmsg;
   else
-    info() << "Double ST hit: will remove both measurements from the track."
+    info() << "Double ST hit: will remove both LHCbIDs from the track."
            << endmsg;
 
-  info() << "Double OT hit: will remove both measurements from the track."
+  info() << "Double OT hit: will remove both LHCbIDs from the track."
 	 << endmsg;
 
   debug() << "TrackRemoveDoubleHits initialized successfully" << endmsg;    
 
-  m_nTTHits = 0;
-  m_nITHits = 0;
-  m_nOTHits = 0;
+  if ( msgLevel( MSG::DEBUG ) ) {
+    m_nTTHits = 0;
+    m_nITHits = 0;
+    m_nOTHits = 0;
+  }
 
   return StatusCode::SUCCESS;
 }
@@ -115,8 +116,6 @@ StatusCode TrackRemoveDoubleHits::initialize ( ) {
 StatusCode TrackRemoveDoubleHits::execute ( ) {
 
   debug() << "TrackRemoveDoubleHits starting execution" << endmsg;
-
-  std::vector<LHCb::Node*> nodesToRemove;
 
   // Get the tracks
   m_tracks = get<LHCb::Tracks>( m_tracksPath );  
@@ -135,38 +134,32 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
   for ( ; iTracks != m_tracks->end(); ++iTracks ) {
     LHCb::Track& aTrack = *(*iTracks);
 
-    nodesToRemove.clear();
+    std::vector<LHCb::LHCbID> lhcbIDsToRemove;
 
-    // Loop over the nodes to check if
+    // Loop over the lhcbIDs to check if
     // there are two of the same track in the same ST sector
-    std::vector<LHCb::Node*>::const_iterator iNodes = aTrack.nodes().begin();
-    for ( ; iNodes != aTrack.nodes().end(); ++iNodes ) {
+    std::vector<LHCb::LHCbID>::const_iterator iLHCbIDs = aTrack.lhcbIDs().begin();
+    for ( ; iLHCbIDs != aTrack.lhcbIDs().end(); ++iLHCbIDs ) {
       
-      LHCb::Node* aNode = *iNodes;
-
-      if ( !aNode->hasMeasurement() ) continue;
-
-      const LHCb::LHCbID& aLHCbID = aNode->measurement().lhcbID();
-
+      const LHCb::LHCbID& aLHCbID = *iLHCbIDs;
+      
       // Only loop on hits in ST or OT
       if ( !aLHCbID.isST() && !aLHCbID.isOT() ) continue;
 
-      if ( aLHCbID.isTT() )
-	++m_nTTHits;
-      else if ( aLHCbID.isIT() )
-	++m_nITHits;
-      else if ( aLHCbID.isOT() )
-	++m_nOTHits;
+      if ( msgLevel( MSG::DEBUG ) ) {
+	if ( aLHCbID.isTT() )
+	  ++m_nTTHits;
+	else if ( aLHCbID.isIT() )
+	  ++m_nITHits;
+	else if ( aLHCbID.isOT() )
+	  ++m_nOTHits;
+      }
       
-      std::vector<LHCb::Node*>::const_iterator iNodes2 = iNodes+1;
-      for ( ; iNodes2 != aTrack.nodes().end(); ++iNodes2 ) {
+      std::vector<LHCb::LHCbID>::const_iterator iLHCbIDs2 = iLHCbIDs+1;
+      for ( ; iLHCbIDs2 != aTrack.lhcbIDs().end(); ++iLHCbIDs2 ) {
         
-        LHCb::Node* aNode2 = *iNodes2;
-
-        if ( !aNode2->hasMeasurement() ) continue;
-
-	const LHCb::LHCbID& aLHCbID2 = aNode2->measurement().lhcbID();
-
+        const LHCb::LHCbID& aLHCbID2 = *iLHCbIDs2;
+        
         // Only loop on hits in ST or OT
 	if ( !aLHCbID2.isST() && !aLHCbID2.isOT() ) continue;
         
@@ -179,36 +172,36 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
 	  if ( aLHCbID.stID().uniqueSector() == aLHCbID2.stID().uniqueSector() ) {
 	    
 	    if ( m_keepHighThreshold ) {
-	      Warning("Found a double hit in IT. Removing from track measurement of strip with low threshold!!!",
+	      Warning("Found a double hit in IT. Removing from track LHCbID of strip with low threshold!!!",
 		      StatusCode::SUCCESS, 1);
 	      if ( isHighThreshold ( aLHCbID ) &&
 		   !isHighThreshold ( aLHCbID2 ) )
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      else if ( isHighThreshold ( aLHCbID2 ) &&
 			!isHighThreshold ( aLHCbID ) )
-		nodesToRemove.push_back( aNode );
+		lhcbIDsToRemove.push_back( aLHCbID );
 	      else {
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      }
 	    }
 	    else if ( m_keepHighCharge ) {
-	      Warning("Found a double hit in IT. Removing from track measurement of strip with less charge!!!",
+	      Warning("Found a double hit in IT. Removing from track LHCbID of strip with less charge!!!",
 		      StatusCode::SUCCESS, 1);
 	      if ( charge ( aLHCbID ) > charge ( aLHCbID2 ) )
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      else if ( charge ( aLHCbID2 ) > charge ( aLHCbID ) )
-		nodesToRemove.push_back( aNode );
+		lhcbIDsToRemove.push_back( aLHCbID );
 	      else {
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      }
 	    }
 	    else {
-	      Warning("Found a double hit in IT. Removing both measurements from track!!!",
+	      Warning("Found a double hit in IT. Removing both LHCbIDs from track!!!",
 		      StatusCode::SUCCESS, 1);
-	      nodesToRemove.push_back( aNode );
-	      nodesToRemove.push_back( aNode2 );
+	      lhcbIDsToRemove.push_back( aLHCbID );
+	      lhcbIDsToRemove.push_back( aLHCbID2 );
 	    }
 	    break;
 	  }
@@ -225,44 +218,42 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
 	       (abs(sectorDiff) < 2) ) {
 	    
 	    if ( m_keepHighThreshold ) {
-	      Warning("Found a double hit in TT. Removing from track measurement of strip with low threshold!!!",
+	      Warning("Found a double hit in TT. Removing from track LHCbID of strip with low threshold!!!",
 		      StatusCode::SUCCESS, 1);
 	      if ( isHighThreshold ( aLHCbID ) &&
 		   !isHighThreshold ( aLHCbID2 ) )
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      else if ( isHighThreshold ( aLHCbID2 ) &&
 			!isHighThreshold ( aLHCbID ) )
-		nodesToRemove.push_back( aNode );
+		lhcbIDsToRemove.push_back( aLHCbID );
 	      else {
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      }
 	    }
 	    else if ( m_keepHighCharge ) {
-	      Warning("Found a double hit in TT. Removing from track measurement of strip with less charge!!!",
+	      Warning("Found a double hit in TT. Removing from track LHCbID of strip with less charge!!!",
 		      StatusCode::SUCCESS, 1);
 	      if ( charge ( aLHCbID ) > charge ( aLHCbID2 ) )
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      else if ( charge ( aLHCbID2 ) > charge ( aLHCbID ) )
-		nodesToRemove.push_back( aNode );
+		lhcbIDsToRemove.push_back( aLHCbID );
 	      else {
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
+		lhcbIDsToRemove.push_back( aLHCbID );
+		lhcbIDsToRemove.push_back( aLHCbID2 );
 	      }
 	    }
 	    else {
-	      if ( aLHCbID.stID().sector() == aLHCbID2.stID().sector() ) {
-		Warning("Found a double hit in TT (same sector). Removing both measurements from track!!!",
+	      // Depending on the case, the warning is different.
+	      if ( aLHCbID.stID().sector() == aLHCbID2.stID().sector() )
+		Warning("Found a double hit in TT (same sector). Removing both LHCbIDs from track!!!",
 			StatusCode::SUCCESS, 1);
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
-	      }
-	      else if ( abs(sectorDiff) == 1 ) {
-		Warning("Found a double hit in TT (different sectors). Removing both measurements from track!!!",
+	      else if ( abs(sectorDiff) == 1 )
+		Warning("Found a double hit in TT (different sectors). Removing both LHCbIDs from track!!!",
 			StatusCode::SUCCESS, 1);
-		nodesToRemove.push_back( aNode );
-		nodesToRemove.push_back( aNode2 );
-	      }
+	      // But in both case we remove the two LHCbIDs
+	      lhcbIDsToRemove.push_back( aLHCbID );
+	      lhcbIDsToRemove.push_back( aLHCbID2 );
 	    }
 	    break;
 	  }
@@ -281,13 +272,13 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
 	    double tx = tState.tx();
 
 	    if ( fabs ( tx ) > 0.350 ) {
-	      Warning("Found a double hit in OT. Removing both measurements from track!!!",
+	      Warning("Found a double hit in OT. Removing both LHCbIDs from track!!!",
 		      StatusCode::SUCCESS, 1);
-	      nodesToRemove.push_back( aNode );
-	      nodesToRemove.push_back( aNode2 );
+	      lhcbIDsToRemove.push_back( aLHCbID );
+	      lhcbIDsToRemove.push_back( aLHCbID2 );
 	      break;
 	    }
-	    Warning("Found an acceptable double hit in OT. Keeping both measurements on track.",
+	    Warning("Found an acceptable double hit in OT. Keeping both LHCbIDs on track.",
 		    StatusCode::SUCCESS, 1);
 	  }
 	  //**********************************************************************
@@ -295,13 +286,9 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
       }
     }
 
-    std::vector<LHCb::Node*>::const_iterator rNodes = nodesToRemove.begin();
-    for ( ; rNodes != nodesToRemove.end(); ++rNodes ) {
-      LHCb::Measurement& theMeas = (*rNodes)->measurement();
-      (*rNodes)->removeMeasurement();
-      (*rNodes)->setType( LHCb::Node::Outlier );
-      aTrack.removeFromMeasurements( &theMeas );
-    }
+    std::vector<LHCb::LHCbID>::const_iterator rLHCbIDs = lhcbIDsToRemove.begin();
+    for ( ; rLHCbIDs != lhcbIDsToRemove.end(); ++rLHCbIDs )
+      aTrack.removeFromLhcbIDs( *rLHCbIDs );
   }
   
   return StatusCode::SUCCESS;  
@@ -314,12 +301,13 @@ StatusCode TrackRemoveDoubleHits::execute ( ) {
 //===========================================================================
 StatusCode TrackRemoveDoubleHits::finalize ( ) {
 
-  if ( msgLevel( MSG::DEBUG ) )
+  if ( msgLevel( MSG::DEBUG ) ) {
     debug() << "==> Finalize" << endmsg;
-
-  info() << "Total number of TT hits seen = " << m_nTTHits << endmsg
-	 << "Total number of IT hits seen = " << m_nITHits << endmsg
-	 << "Total number of OT hits seen = " << m_nOTHits << endmsg;
+    
+    debug() << "Total number of TT hits seen = " << m_nTTHits << endmsg
+	    << "Total number of IT hits seen = " << m_nITHits << endmsg
+	    << "Total number of OT hits seen = " << m_nOTHits << endmsg;
+  }
 
   return GaudiAlgorithm::finalize ( );
 };
