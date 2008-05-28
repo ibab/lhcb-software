2cde0e91c7cde85e71d3164a395ba13d6ad87bae
hmdegaud <hmdegaud@cern.ch>
2008-05-28 18:57:08 +0000

    added the generation of the setenv aliases
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13184 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbConfiguration/scripts/AllProjectsSetup.py
+++ LbScripts/LbConfiguration/scripts/AllProjectsSetup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from LbUtils.Script import Script
-from LbUtils.Env import Environment
+from LbUtils.Env import Environment, Aliases
 from LbConfiguration.Project import project_list, getProject
 from tempfile import mkstemp
 import sys, os
@@ -20,7 +20,8 @@ def _check_output_options_cb(option, opt_str, value, parser):
 
 class AllProjectsSetupScript(Script):
     _version = "fake version"
-    def _write_script(self,data):
+    def _write_script(self,env,aliases=None):
+        """ select the ouput stream according to the cmd line options """
         close_output = False
         if self.options.output:
             self.output_file = open(self.options.output,"w")
@@ -36,11 +37,14 @@ class AllProjectsSetupScript(Script):
             self.output_file = sys.stdout
             close_output = False
         # write the data
-        self.output_file.write(data)
+        self.output_file.write(env)
+        if aliases :
+            self.output_file.write(aliases)
         self.output_file.write("\n") # @todo: this may be avoided
         if close_output: 
             self.output_file.close()
     def defineOpts(self):
+        """ define commandline options """
         parser = self.parser
         parser.set_defaults(targetshell="csh")
         parser.add_option("--shell", action="store", type="choice", metavar="SHELL",
@@ -59,11 +63,17 @@ class AllProjectsSetupScript(Script):
     def main(self):
         opts = self.options
         ev = Environment(orig=dict())
+        al = Aliases()
         for p in project_list :
             key = "%s_release_area" % p.Name()
             value = p.ReleaseArea()
             ev[key] = value
-        self._write_script(ev.gen_script(opts.targetshell))
+        for p in project_list :
+            key = "setenv%s" % p.Name()
+            value = "setenvProject %s" % p.Name()
+            al[key] = value
+        self._write_script(ev.gen_script(opts.targetshell), 
+                           al.gen_script(opts.targetshell))
         return 0
 
 if __name__ == '__main__':
