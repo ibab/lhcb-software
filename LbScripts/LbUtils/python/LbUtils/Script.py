a570427d80315b99575ca66c1772c9655f8d3008
hmdegaud <hmdegaud@cern.ch>
2008-05-28 13:53:46 +0000

    added a default return value to the main() method. And a few doc strings.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13179 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/python/LbUtils/Script.py
+++ LbScripts/LbUtils/python/LbUtils/Script.py
@@ -7,6 +7,8 @@ import sys
 class Script():
     _version = "$Name: not supported by cvs2svn $"
     def __init__(self, usage=None, version = None):
+        """ constructor of the Script. provides default options parser 
+        and logger """
         if version is None:
             version = self._version 
         self.parser = Parser(usage=usage, version=version)
@@ -15,15 +17,17 @@ class Script():
         self.options = None
         self.args = None
     def defineOpts(self):
-        # User options
+        """ User options -- has to be overridden """
         # self.parser.add_options(...)
         pass
     def main(self):
-        # User code
-        pass
-    def parseOpts(self,args):
+        """ User code place holder -- has to be overridden """
+        return 0
+    def parseOpts(self, args):
+        """ do the parsing (behind the scene) """
         self.options, self.args = self.parser.parse_args(args)
     def run(self, args=sys.argv[1:]):
+        """ main function to be called by the user """
         self.defineOpts()
         self.parseOpts(args)
         return self.main()
