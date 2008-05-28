7f26fb72af6c32c556af73711a3b4bdfbe982751
lnicolas <lnicolas@cern.ch>
2008-05-28 12:21:21 +0000

    More alignables
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88537 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 58460e5682043c96f68f7717b528bdb7e3b91cbd

--- Alignment/Alignment/TAlignment/python/Alignables.py
+++ Alignment/Alignment/TAlignment/python/Alignables.py
@@ -3,17 +3,26 @@ import types
 class Alignables( list ):
     
     def __init__( self, elements = None, dofs = "" ) :        
-        self.m_dofs      = [ "Tx", "Ty", "Tz", "Rx", "Ry", "Rz" ]
-        self.m_velo      = "/dd/Structure/LHCb/BeforeMagnetRegion/Velo"
-        self.m_veloLeft  = "/VeloLeft"
-        self.m_veloRight = "/VeloRight"
-        self.m_tt        = "/dd/Structure/LHCb/BeforeMagnetRegion/TT"
-        self.m_it        = "/dd/Structure/LHCb/AfterMagnetRegion/T/IT"
-        self.m_ot        = "/dd/Structure/LHCb/AfterMagnetRegion/T/OT"
-        self.m_stations  = [ "/T1", "/T2", "/T3" ]
-        self.m_layers    = [ "/X1layer", "/Ulayer", "/Vlayer", "/X2layer" ]
-        self.m_quarters  = [ "/Quarter(0|2)", "/Quarter(1|3)" ]
-        self.m_modules   = [ "/Module1", "/Module2", "/Module3", "/Module4", "/Module5", "/Module6", "/Module7", "/Module8", "/Module9" ]
+        self.m_dofs       = [ "Tx", "Ty", "Tz", "Rx", "Ry", "Rz" ]
+        self.m_velo       = "/dd/Structure/LHCb/BeforeMagnetRegion/Velo"
+        self.m_veloLeft   = "/VeloLeft"
+        self.m_veloRight  = "/VeloRight"
+        
+        self.m_tt         = "/dd/Structure/LHCb/BeforeMagnetRegion/TT"
+        
+        self.m_it         = "/dd/Structure/LHCb/AfterMagnetRegion/T/IT"
+        self.m_itStations = [ "/Station1", "/Station2", "/Station3" ]
+        self.m_itBoxes    = [ "/TopBox", "/BottomBox", "/LeftBox", "/RightBox" ]
+        self.m_itLayers   = [ "/LayerX1", "/LayerU", "/LayerV", "/LayerX2" ]
+        self.m_itLadders  = [ "/Ladder1", "/Ladder2", "/Ladder3", "/Ladder4",
+                              "/Ladder5", "/Ladder6", "/Ladder7" ]
+
+        self.m_ot         = "/dd/Structure/LHCb/AfterMagnetRegion/T/OT"
+        self.m_otStations = [ "/T1", "/T2", "/T3" ]
+        self.m_otLayers   = [ "/X1layer", "/Ulayer", "/Vlayer", "/X2layer" ]
+        self.m_otQuarters = [ "/Quarter(0|2)", "/Quarter(1|3)" ]
+        self.m_otModules  = [ "/Module1", "/Module2", "/Module3", "/Module4", "/Module5",
+                              "/Module6", "/Module7", "/Module8", "/Module9" ]
         if elements and self.__validElements( elements ) :    
             self.__append( elements, dofs )
         
@@ -81,6 +90,8 @@ class Alignables( list ):
                 self.append( elements )
             
     ## Some predefined alignables
+
+    ## VELO ###############################################################################
     def Velo( self, dofs = "" ) :
         self.__append( self.m_velo, dofs )
 
@@ -99,65 +110,100 @@ class Alignables( list ):
         
     def VeloRightRSensors( self, dofs = "" ) :
         self.__append( self.m_velo + self.m_veloRight + "/Module(00|01|04|05|08|09|12|13|16|17|20|21|24|25|28|29|32|33|36|37|40|41)/RPhiPair.{1,2}/Detector-00", dofs)
-        
+
+
+    ## TT #################################################################################
     def TT( self, dofs = "" ) :
         self.__append( self.m_tt, dofs )
-            
+
+    def TTLayers( self, dofs = "" ) :
+        self.__append( self.m_tt + "/TT./.{4}Layer", dofs )
+
+    def TTHalfModules( self, dofs = "" ) :
+        self.__append( self.m_tt + "/TT./.{4}Layer/.{2}Module.{2}", dofs )
+
+
+    ## IT #################################################################################
     def IT( self, dofs = "" ) :
         self.__append( self.m_it, element )
 
-    def ITLadders( self, dofs = "" ) :
-        self.__append( self.m_it + "/Station./.+Box/Layer.{1,2}/Ladder.", dofs )
+    def ITBoxes( self, dofs = "" ) :
+        elements = []
+        for i in range( len( self.m_itStations ) ) :
+            for j in range( len( self.m_itBoxes ) ) :
+                elements.append( self.m_it + self.m_itStations[ i ] + self.m_itBoxes[ j ] )
+        self.__append( elements, dofs )
         
+    def ITLayers( self, dofs = "" ) :
+        elements = []
+        for i in range( len( self.m_itStations ) ) :
+            for j in range( len( self.m_itBoxes ) ) :
+                for k in range( len( self.m_itLayers ) ) :
+                    elements.append( self.m_it + self.m_itStations[ i ] + self.m_itBoxes[ j ] +
+                                     self.m_itLayers[ k ] )
+        self.__append( elements, dofs )
+        
+    def ITLadders( self, dofs = "" ) :
+        elements = []
+        for i in range( len( self.m_itStations ) ) :
+            for j in range( len( self.m_itBoxes ) ) :
+                for k in range( len( self.m_itLayers ) ) :
+                    for l in range( len( self.m_itLadders ) ) :
+                        elements.append( self.m_it + self.m_itStations[ i ] + self.m_itBoxes[ j ] +
+                                         self.m_itLayers[ k ] + self.m_itLadders[ l ] )
+        self.__append( elements, dofs )
+
+
+    ## OT #################################################################################
     def OT( self, dofs = "" ) :
         self.__append( self.m_ot, dofs )
     
     def OTStations( self, dofs = "" ) :
         elements = []
-        for i in range( len( self.m_stations ) ) :
-            elements.append( self.m_ot + self.m_stations[ i ] )
-            
+        for i in range( len( self.m_otStations ) ) :
+            elements.append( self.m_ot + self.m_otStations[ i ] )
         self.__append( elements, dofs )
         
                 
     def OTLayers( self, dofs = "" ) :
         elements = []
-        for i in range( len( self.m_stations ) ) :
-            for j in range( len( self.m_layers ) ) :
-                elements.append( self.m_ot + self.m_stations[ i ] + self.m_layers[j] )
-           
+        for i in range( len( self.m_otStations ) ) :
+            for j in range( len( self.m_otLayers ) ) :
+                elements.append( self.m_ot + self.m_otStations[ i ] + self.m_otLayers[ j ] )
         self.__append( elements, dofs )    
 
     def OTModules( self, dofs = "" ) :
         elements = []
         ## 3 Stations numbered from 1 to 3
-        for i in range( len( self.m_stations ) ) :
+        for i in range( len( self.m_otStations ) ) :
             ## There are four layers X1, U, V and X2
-            for j in range( len( self.m_layers ) ) :
+            for j in range( len( self.m_otLayers ) ) :
                 ## Quarters 0 and 2 (C-Side) and Quarters 1 and 3 (A-side)
-                for k in range( len( self.m_quarters ) ) :
+                for k in range( len( self.m_otQuarters ) ) :
                     ## Nine modules numbered from 1 to 9
-                    for l in range( len( self.m_modules ) ) :
-                        elements.append("Group : " + self.m_ot + self.m_stations[ i ] + self.m_layers[ j ] + self.m_quarters[ k ] + self.m_modules[ l ] )
-           
+                    for l in range( len( self.m_otModules ) ) :
+                        elements.append("Group : " + self.m_ot + self.m_otStations[ i ] +
+                                        self.m_otLayers[ j ] + self.m_otQuarters[ k ] +
+                                        self.m_otModules[ l ] )
         self.__append( elements, dofs )    
         
     def OTCFrames( self, dofs = "" ) :
         elements = []
-        for i in range( len( self.m_stations ) ) :
-            station = self.m_ot + self.m_stations[ i ]
+        for i in range( len( self.m_otStations ) ) :
+            station = self.m_ot + self.m_otStations[ i ]
             ## C-side X1 and U
-            elements.append( station + "/(X1|U)layer" + self.m_quarters[0] + "/Module.")
+            elements.append( station + "/(X1|U)layer" + self.m_otQuarters[0] + "/Module.")
             ## C-side V and X2
-            elements.append( station + "/(V|X2)layer" + self.m_quarters[0] + "/Module.")
+            elements.append( station + "/(V|X2)layer" + self.m_otQuarters[0] + "/Module.")
             ## A-side X1 and U
-            elements.append( station + "/(X1|U)layer" + self.m_quarters[1] + "/Module.")
+            elements.append( station + "/(X1|U)layer" + self.m_otQuarters[1] + "/Module.")
             ## A-side V and X2
-            elements.append( station + "/(V|X2)layer" + self.m_quarters[1] + "/Module.")
-            
+            elements.append( station + "/(V|X2)layer" + self.m_otQuarters[1] + "/Module.")
         self.__append( elements, dofs )
-            
-    def ITLadderOTModules( self, dofs = "" ) :
+
+
+    ## OTHER ##############################################################################
+    def ITLaddersOTModules( self, dofs = "" ) :
         self.ITLadders( dofs )
         self.OTModules( dofs )
         
