e2c748c94292bfd1981f5545cb0aba7b562354e7
Gloria Corti <gloria.corti@cern.ch>
2008-06-02 19:52:02 +0000

    options for latest geometry
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93403 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: de60cc1170a0516c0e0da76d9b4efa3768676fb8

---
+++ Gauss/Sim/Gauss/options/lhcb-2008.py
@@ -0,0 +1,19 @@
+# Minimal file for running Gauss from python prompt
+# Syntax is:
+#   gaudirun.py $GAUSSOPTS/lhcb-200802.py
+#
+from Gaudi.Configuration import *
+
+#--Allow to use ns (ie. double) in options of EventClockSvc
+from Configurables import EventClockSvc, FakeEventTime
+
+ec = EventClockSvc()
+ec.addTool(FakeEventTime(), name="EventTimeDecoder")
+
+#--Geometry dependent options
+importOptions('$GAUSSOPTS/lhcb-2008.opts')
+
+#--Application name and version
+ApplicationMgr( AppName    = 'Gauss',
+                AppVersion = 'v31r1' )
+
