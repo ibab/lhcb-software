706e3e00ea2d47c8f023dcae92071a308ec9d5b0
hmdegaud <hmdegaud@cern.ch>
2008-06-06 17:17:30 +0000

    added static global instance and access function
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13192 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/python/LbUtils/Cache.py
+++ LbScripts/LbUtils/python/LbUtils/Cache.py
@@ -2,15 +2,29 @@ import os
 import cPickle as pickle
 import logging
 
+_defaultcache = None
+
+
 class File(object):
-    def __init__(self, application, name):
+    def __init__(self, application=None, name=None):
         self._application = application
         self._name = name
+        self._tobeupdated = False
+        self._filename = None
+        global _defaultcache
+        if _defaultcache is not self :
+            _defaultcache = self
     def fileName(self):
-        pth = os.path.join(os.environ["HOME"], ".cache", self._application)
-        if not os.path.exists(pth):
-            os.makedirs(pth)
-        return os.path.join(pth, "%s.pkl" % self._name)
+        if not self._filename :
+            pth = os.path.join(os.environ["HOME"], ".cache")
+            if self._application :
+                pth = os.path.join(pth,self._application)
+            if not os.path.exists(pth):
+                os.makedirs(pth)
+            if not self._name :
+                self._name = "default"
+            self._filename = os.path.join(pth, "%s.pkl" % self._name) 
+        return self._filename
     def dump(self, *objs):
         log = logging.getLogger()
         f = open(self.fileName(), "w")
@@ -18,6 +32,10 @@ class File(object):
             pickle.dump(o, f)
             log.info("Cache dumping to %s" % self.fileName())
         f.close()
+    def needsUpdate(self):
+        return self._tobeupdated
+    def toBeUpdated(self):
+        self._tobeupdated = True
     def load(self):
         log = logging.getLogger()
         log.info("Cache Load from %s" % self.fileName())
@@ -28,4 +46,10 @@ class File(object):
             except EOFError:
                 break
         f.close()
-            
+
+def getDefaultCache():
+    global _defaultcache
+    if not _defaultcache :
+        _defaultcache = File()
+    return _defaultcache
+
