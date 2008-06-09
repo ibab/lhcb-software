c31e154fe9b8e1141c90ad744f5c14561d41a87c
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-06-09 16:01:01 +0000

    ! 2008-06-09 - Chris Jones  - Improve doxygen comments in FPE code + add a simple constructor
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47873 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a450b6bf917a9332c7d11451b89ec7d0900ece56

--- LHCb/Kernel/LHCbAlgs/src/FPEAuditor.cpp
+++ LHCb/Kernel/LHCbAlgs/src/FPEAuditor.cpp
@@ -1,4 +1,4 @@
-#include "FPEGuard.h"
+#include "GaudiKernel/FPEGuard.h"
 #include <vector>
 #include <string>
 #include <utility>
@@ -9,103 +9,136 @@
 #include "GaudiKernel/GaudiException.h"
 #include "GaudiKernel/MsgStream.h"
 
+/** @class FPEMaskProperty FPEAuditor.cpp
+ *
+ *  Utility class for the FPEAuditor
+ *
+ *  @author Gerhard Raven
+ *  @date   09/06/2008
+ */
 class FPEMaskProperty {
- public:
-    typedef SimpleProperty<std::vector<std::string> > property_type;
+public:
+  typedef SimpleProperty<std::vector<std::string> > property_type;
 
-     FPEMaskProperty() : m_mask(0)
-     { m_property.declareUpdateHandler(&FPEMaskProperty::updateHandler,this); }
-     FPE::Guard::mask_type value() const { return m_mask;}
-     property_type& property() { return m_property; }
-     FPEMaskProperty& set(const std::vector<std::string>& s)
-     { property().setValue(s); return *this; }
- private:
-    void updateHandler(Property&) {
-         m_mask=FPE::Guard::mask(m_property.value().begin(),m_property.value().end());
-     }
-     property_type   m_property;
-     FPE::Guard::mask_type  m_mask;
+  FPEMaskProperty() : m_mask(0)
+  { m_property.declareUpdateHandler(&FPEMaskProperty::updateHandler,this); }
+  FPE::Guard::mask_type value() const { return m_mask;}
+  property_type& property() { return m_property; }
+  FPEMaskProperty& set(const std::vector<std::string>& s)
+  { property().setValue(s); return *this; }
+private:
+  void updateHandler(Property&) {
+    m_mask=FPE::Guard::mask(m_property.value().begin(),m_property.value().end());
+  }
+  property_type   m_property;
+  FPE::Guard::mask_type  m_mask;
 };
 
-
+/** @class FPEAuditor FPEAuditor.cpp
+ *
+ *  Auditor for enabling and trapping Floating Point Exceptions (FPEs).
+ *
+ *  Configure the FPU to trap a specified set of
+ *  FPEs, and generate a SIGFPE (which leads to Gaudi
+ *  providing a traceback and aborting the job).
+ *
+ *  Example configuration options :-
+ *  @verbatim
+ *  // Enable the auditor
+ *  ApplicationMgr.ExtSvc += { "AuditorSvc" };
+ *  AuditorSvc.Auditors += { "FPEAuditor" };
+ *  // Exceptions to trap 
+ *  // Full list of possible types is "DivByZero","Overflow","Underflow","Invalid","Inexact".
+ *  // "AllExcept" turns on all exceptions.
+ *  FPEAuditor.TrapOn  = { "DivByZero","Overflow","Underflow","Invalid" };
+ *  // Processing phases to enable exceptions during.
+ *  FPEAuditor.ActivateAt = {"Execute" };
+ *  // Disable exceptions for certain problematic components
+ *  FPEAuditor.DisableTrapFor = { "Init","InitReprocSeq","BrunelInit" };
+ *  @endverbatim
+ *
+ *  @see FPE::Guard
+ *  
+ *  @author Gerhard Raven
+ *  @date   09/06/2008
+ */
 class FPEAuditor : public Auditor {
 public:
-    FPEAuditor( const std::string& name, ISvcLocator* pSvcLocator);
-    FPEAuditor::~FPEAuditor() {  }
-    StatusCode FPEAuditor::initialize() {
-        if (m_activateSuperGuard) m_superGuard.reset( new FPE::Guard( m_mask.value() ) );
-        return StatusCode::SUCCESS;
-    }
-    StatusCode FPEAuditor::finalize() {
-        m_superGuard.reset( (FPE::Guard*)0 );
-        return StatusCode::SUCCESS;
-    }
+  FPEAuditor( const std::string& name, ISvcLocator* pSvcLocator);
+  FPEAuditor::~FPEAuditor() {  }
+  StatusCode FPEAuditor::initialize() {
+    if (m_activateSuperGuard) m_superGuard.reset( new FPE::Guard( m_mask.value() ) );
+    return StatusCode::SUCCESS;
+  }
+  StatusCode FPEAuditor::finalize() {
+    m_superGuard.reset( (FPE::Guard*)0 );
+    return StatusCode::SUCCESS;
+  }
 
-    void before(StandardEventType type, INamedInterface* i) { before(type,i->name()); }
-    void before(CustomEventTypeRef   type, INamedInterface* i) { before(type,i->name()); }
-    void before(StandardEventType type, const std::string& s)
-    {  std::ostringstream t; t << type;before(t.str(),s); }
-    void before(CustomEventTypeRef eventType, const std::string& s)
-    {
-      if ( activeAt(eventType) ) {
-         bool veto = ( std::find(m_veto.begin(),m_veto.end(),s) != m_veto.end()) ;
-		 m_guards.push_back( std::make_pair( s, new FPE::Guard( m_mask.value(), veto) ) );
-      }
+  void before(StandardEventType type, INamedInterface* i) { before(type,i->name()); }
+  void before(CustomEventTypeRef   type, INamedInterface* i) { before(type,i->name()); }
+  void before(StandardEventType type, const std::string& s)
+  {  std::ostringstream t; t << type;before(t.str(),s); }
+  void before(CustomEventTypeRef eventType, const std::string& s)
+  {
+    if ( activeAt(eventType) ) {
+      bool veto = ( std::find(m_veto.begin(),m_veto.end(),s) != m_veto.end()) ;
+      m_guards.push_back( std::make_pair( s, new FPE::Guard( m_mask.value(), veto) ) );
     }
+  }
 
-    void after(StandardEventType type, INamedInterface* i, const StatusCode& sc)
-    {  std::ostringstream t; t << type;after(t.str(),i,sc); }
-    void after(CustomEventTypeRef type, INamedInterface* i, const StatusCode& sc)
-    {  if(!beforeCannotHaveBeenCalled(type,i)) after(type,i->name(),sc); }
-    void after(StandardEventType type, const std::string& s, const StatusCode& sc)
-    {  std::ostringstream t; t << type;after(t.str(),s,sc); }
-    void after(CustomEventTypeRef eventType, const std::string& s, const StatusCode&)
-    {
-     if ( activeAt(eventType) ) {
-       if (m_guards.empty()) {
+  void after(StandardEventType type, INamedInterface* i, const StatusCode& sc)
+  {  std::ostringstream t; t << type;after(t.str(),i,sc); }
+  void after(CustomEventTypeRef type, INamedInterface* i, const StatusCode& sc)
+  {  if(!beforeCannotHaveBeenCalled(type,i)) after(type,i->name(),sc); }
+  void after(StandardEventType type, const std::string& s, const StatusCode& sc)
+  {  std::ostringstream t; t << type;after(t.str(),s,sc); }
+  void after(CustomEventTypeRef eventType, const std::string& s, const StatusCode&)
+  {
+    if ( activeAt(eventType) ) {
+      if (m_guards.empty()) {
         throw GaudiException("FPEAuditor: inbalance of before/after calls...","",StatusCode::FAILURE);
-       }
-       std::pair<std::string,FPE::Guard *>& p = m_guards.back();
-       if (p.first!=s) { throw GaudiException("FPEAuditor: unexpected stack state...","",StatusCode::FAILURE); }
-       delete p.second;
-       m_guards.pop_back();
-     }
+      }
+      std::pair<std::string,FPE::Guard *>& p = m_guards.back();
+      if (p.first!=s) { throw GaudiException("FPEAuditor: unexpected stack state...","",StatusCode::FAILURE); }
+      delete p.second;
+      m_guards.pop_back();
     }
+  }
 
 
-    void beforeInitialize(INamedInterface *i) { return before(IAuditor::Initialize,i); }
-    void beforeReInitialize(INamedInterface *i) { return before(IAuditor::ReInitialize,i); }
-    void beforeExecute(INamedInterface *i) { return before(IAuditor::Execute,i); }
-    void beforeBeginRun(INamedInterface *i) { return before(IAuditor::BeginRun,i); }
-    void beforeEndRun(INamedInterface *i) { return before(IAuditor::EndRun,i); }
-    void beforeFinalize(INamedInterface *i) { return before(IAuditor::Finalize,i); }
+  void beforeInitialize(INamedInterface *i) { return before(IAuditor::Initialize,i); }
+  void beforeReInitialize(INamedInterface *i) { return before(IAuditor::ReInitialize,i); }
+  void beforeExecute(INamedInterface *i) { return before(IAuditor::Execute,i); }
+  void beforeBeginRun(INamedInterface *i) { return before(IAuditor::BeginRun,i); }
+  void beforeEndRun(INamedInterface *i) { return before(IAuditor::EndRun,i); }
+  void beforeFinalize(INamedInterface *i) { return before(IAuditor::Finalize,i); }
 
-    void afterInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Initialize,i,s); }
-    void afterReInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::ReInitialize,i,s); }
-    void afterExecute(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Execute,i,s); }
-    void afterBeginRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::BeginRun,i,s); }
-    void afterEndRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::EndRun,i,s); }
-    void afterFinalize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Finalize,i,s); }
+  void afterInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Initialize,i,s); }
+  void afterReInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::ReInitialize,i,s); }
+  void afterExecute(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Execute,i,s); }
+  void afterBeginRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::BeginRun,i,s); }
+  void afterEndRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::EndRun,i,s); }
+  void afterFinalize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Finalize,i,s); }
 
 private:
-    bool activeAt(const std::string s) { return ( std::find(m_when.begin(),m_when.end(),s)!=m_when.end() ); }
-    bool beforeCannotHaveBeenCalled(CustomEventType type, INamedInterface *i) {
-        // auditing starts 'after' AuditorSvc::initialize has been called
-        // but, at that time, the matching 'before' hasn't been called (since
-        // we were not yet activated!), so there is no state to restore, i.e.
-        // m_guards is empty. So we make an exception to the rule that m_guards
-        // cannot be empty for that particular corner case by ignoring the 'after'
-        // call (which is the right thing to do ;-)...
-        return (m_guards.empty() && type == "Initialize" && SmartIF<IAuditorSvc>(i).isValid());
-    }
+  bool activeAt(const std::string s) { return ( std::find(m_when.begin(),m_when.end(),s)!=m_when.end() ); }
+  bool beforeCannotHaveBeenCalled(CustomEventType type, INamedInterface *i) {
+    // auditing starts 'after' AuditorSvc::initialize has been called
+    // but, at that time, the matching 'before' hasn't been called (since
+    // we were not yet activated!), so there is no state to restore, i.e.
+    // m_guards is empty. So we make an exception to the rule that m_guards
+    // cannot be empty for that particular corner case by ignoring the 'after'
+    // call (which is the right thing to do ;-)...
+    return (m_guards.empty() && type == "Initialize" && SmartIF<IAuditorSvc>(i).isValid());
+  }
 
-    mutable MsgStream                                m_log;
-    std::vector<std::pair<std::string,FPE::Guard*> > m_guards;
-    std::auto_ptr<FPE::Guard>                        m_superGuard;
-    FPEMaskProperty                                  m_mask;
-    std::vector<std::string>                         m_when,
-                                                     m_veto;
-    bool                                             m_activateSuperGuard;
+  mutable MsgStream                                m_log;
+  std::vector<std::pair<std::string,FPE::Guard*> > m_guards;
+  std::auto_ptr<FPE::Guard>                        m_superGuard;
+  FPEMaskProperty                                  m_mask;
+  std::vector<std::string>                         m_when, m_veto;
+  bool                                             m_activateSuperGuard;
 };
 
 DECLARE_AUDITOR_FACTORY( FPEAuditor );
@@ -114,18 +147,18 @@ FPEAuditor::FPEAuditor( const std::string& name, ISvcLocator* pSvcLocator)
   : Auditor ( name , pSvcLocator )
   , m_log( msgSvc() , name )
 {
-    if (!FPE::Guard::has_working_implementation) { // note: this is a compile-time constant...
-        throw GaudiException("FPEAuditor: no FPE trapping support on this architecture...","",StatusCode::FAILURE);
-    }
-    declareProperty("TrapOn", m_mask.set(  boost::assign::list_of("DivByZero")
-                                                                 ("Invalid")
-                                                                 ("Overflow") ).property() );
-    declareProperty("ActivateAt", m_when = boost::assign::list_of("Initialize")
-                                                                 ("ReInitialize")
-                                                                 ("Execute")
-                                                                 ("BeginRun")
-                                                                 ("EndRun")
-                                                                 ("Finalize") );
-    declareProperty("DisableTrapFor", m_veto );
-    declareProperty("EnableGlobal", m_activateSuperGuard = false );
+  if (!FPE::Guard::has_working_implementation) { // note: this is a compile-time constant...
+    throw GaudiException("FPEAuditor: no FPE trapping support on this architecture...","",StatusCode::FAILURE);
+  }
+  declareProperty("TrapOn", m_mask.set(  boost::assign::list_of("DivByZero")
+                                         ("Invalid")
+                                         ("Overflow") ).property() );
+  declareProperty("ActivateAt", m_when = boost::assign::list_of("Initialize")
+                  ("ReInitialize")
+                  ("Execute")
+                  ("BeginRun")
+                  ("EndRun")
+                  ("Finalize") );
+  declareProperty("DisableTrapFor", m_veto );
+  declareProperty("EnableGlobal", m_activateSuperGuard = false );
 }
--- LHCb/Kernel/LHCbAlgs/src/FPEGuard.h
+++ LHCb/Kernel/LHCbAlgs/src/FPEGuard.h
@@ -7,38 +7,53 @@
 #endif
 
 #include <map>
+#include <vector>
 #include <string>
 #include <iostream>
 #include "GaudiKernel/GaudiException.h"
 #include "boost/assign/list_of.hpp"
 
-
+/** @namespace FPE
+ *
+ *  Namespace For Floating Point Exception Handling code
+ *
+ *  @author Gerhard Raven
+ *  @date   09/06/2008
+ */
 namespace FPE {
 
+  /** @namespace detail
+   *
+   *  This namespace contains machine & libc specific code
+   *  required to implement the functionality used by FPEGuard
+   *  Do not use directly.... if you do, you are on your own....
+   *
+   *  @author Gerhard Raven
+   *  @date   09/06/2008
+   */
+  namespace detail {
 
-namespace detail {
-    /// This namespace contains machine & libc specific code
-    /// required to implement the functionality used by FPEGuard
-    /// Do not use directly.... if you do, you are on your own....
 #if defined(linux) && defined(__GNUC__)
     static const bool has_working_implementation = true;
     typedef int mask_type;
     mask_type disable(mask_type mask) { return fedisableexcept(mask); }
     mask_type enable(mask_type mask)  {
-        feclearexcept(mask); // remove any 'stale' exceptions before switching on trapping
-                             // otherwise we immediately trigger an exception...
-        return feenableexcept(mask);
+      feclearexcept(mask); // remove any 'stale' exceptions before switching on trapping
+      // otherwise we immediately trigger an exception...
+      return feenableexcept(mask);
     }
     const std::map<std::string,mask_type>& map() {
-         static std::map<std::string,mask_type> m = boost::assign::map_list_of
-                   ( "Inexact"   , mask_type(FE_INEXACT)  )
-                   ( "DivByZero" , mask_type(FE_DIVBYZERO))
-                   ( "Underflow" , mask_type(FE_UNDERFLOW))
-                   ( "Overflow"  , mask_type(FE_OVERFLOW) )
-                   ( "Invalid"   , mask_type(FE_INVALID)  )
-                   ( "AllExcept" , mask_type(FE_ALL_EXCEPT));
-          return m;
+      static std::map<std::string,mask_type> m = boost::assign::map_list_of
+        ( "Inexact"   , mask_type(FE_INEXACT)  )
+        ( "DivByZero" , mask_type(FE_DIVBYZERO))
+        ( "Underflow" , mask_type(FE_UNDERFLOW))
+        ( "Overflow"  , mask_type(FE_OVERFLOW) )
+        ( "Invalid"   , mask_type(FE_INVALID)  )
+        ( "AllExcept" , mask_type(FE_ALL_EXCEPT));
+      return m;
     }
+    /// Default mask (for default FPE::Guard constructor)
+    static const mask_type s_default_guard_mask ( FE_ALL_EXCEPT );
 #elif defined(_WIN32)
     static const bool has_working_implementation = true;
     typedef unsigned int mask_type;
@@ -49,77 +64,143 @@ namespace detail {
     mask_type disable(mask_type mask) { return _controlfp(~mask,_MCW_EM);}
     mask_type enable(mask_type mask)  { return _controlfp( mask,_MCW_EM);}
     const std::map<std::string,mask_type>& map() {
-         static std::map<std::string,mask_type> m = boost::assign::map_list_of
-                   ( "Inexact"   , mask_type(EM_INEXACT)   )
-                   ( "DivByZero" , mask_type(EM_ZERODIVIDE))
-                   ( "Underflow" , mask_type(EM_UNDERFLOW) )
-                   ( "Overflow"  , mask_type(EM_OVERFLOW)  )
-                   ( "Invalid"   , mask_type(EM_INVALID)   )
-                   ( "AllExcept" , mask_type(EM_INVALID|EM_OVERFLOW|EM_UNDERFLOW|EM_INEXACT|EM_ZERODIVIDE|EM_DENORMAL));
-         return m;
+      static std::map<std::string,mask_type> m = boost::assign::map_list_of
+        ( "Inexact"   , mask_type(EM_INEXACT)   )
+        ( "DivByZero" , mask_type(EM_ZERODIVIDE))
+        ( "Underflow" , mask_type(EM_UNDERFLOW) )
+        ( "Overflow"  , mask_type(EM_OVERFLOW)  )
+        ( "Invalid"   , mask_type(EM_INVALID)   )
+        ( "AllExcept" , mask_type(EM_INVALID|EM_OVERFLOW|EM_UNDERFLOW|EM_INEXACT|EM_ZERODIVIDE|EM_DENORMAL));
+      return m;
     }
+    /// Default mask (for default FPE::Guard constructor)
+    static const mask_type s_default_guard_mask = EM_INVALID|EM_OVERFLOW|EM_UNDERFLOW|EM_INEXACT|EM_ZERODIVIDE|EM_DENORMAL;
 #else
     static const bool has_working_implementation = false;
     typedef int mask_type;
     mask_type disable(mask_type) { return 0; }
     mask_type enable(mask_type) { return 0; }
     const std::map<std::string,mask_type>& map() {
-         static std::map<std::string,mask_type> m;
-         return m;
+      static std::map<std::string,mask_type> m;
+      return m;
     }
+    /// Default mask (for default FPE::Guard constructor)
+    static const mask_type s_default_guard_mask = 0;
 #endif
-} // namespace detail
 
+  } // namespace detail
 
-class Guard {
-public:
+  /** @class Guard GaudiKernel/FPEGuard.h
+   *
+   *  Create a guard which, depending on the value of 'disable',
+   *  enables (disables) the trapping of Floating Point Exceptions
+   *  (i.e. the generation of a SIGFPE signel) according to the
+   *  specified mask, for the duration of lifetime of the guard.
+   *  The destructor will restore the state at the time of the
+   *  creation of the guard.
+   *
+   *  Example usage :-
+   *  @code
+   *  // You pass a range of strings to FPE::Guard::mask, and assign the result
+   *  // to a FPE::Guard::mask_type, i.e:
+   *  std::vector<std::string> vec = ..... ; // create a vector of valid names, such as 'Invalid', 'Overflow', etc
+   *  FPE::Guard::mask_type mask = FPE::Guard::mask( vec.begin(), vec.end() );
+   *  FPE::Guard guard( mask );
+   *
+   *  // To switch of all known exceptions
+   *  std::vector<std::string> vec = boost::assign::list_of( "AllExcept" );
+   *  FPE::Guard::mask_type mask = FPE::Guard::mask( vec.begin(), vec.end() );
+   *  bool disable = true;
+   *  FPE::Guard guard( mask , disable );
+   *  @endcode 
+   * 
+   *  @author Gerhard Raven
+   *  @date   09/06/2008
+   */
+  class Guard {
+  public:
     /// export the type of the FPE mask
     typedef FPE::detail::mask_type mask_type;
-    /// export whether a working implementation exists.
+    /// Export whether a working implementation exists.
     /// In case it doesn't, the code (silently!) defaults
     /// to no-operation.
     static const bool has_working_implementation = FPE::detail::has_working_implementation;
 
-    /// create a guard which, depending on the value of 'disable',
-    /// enables (disables) the trapping of Floating Point Exceptions
-    /// (i.e. the generation of a SIGFPE signel) according to the
-    /// specified mask, for the duration of lifetime of the guard.
-    /// The destructor will restore the state at the time of the
-    /// creation of the guard.
-    ///
-    /// Note: to create a (valid) mask, use FPE::Guard::mask
-    Guard(mask_type mask, bool disable=false)
-    : m_initial( disable ? FPE::detail::disable(mask) : FPE::detail::enable(mask) )
+    /** Constructor with explicit mask
+     *
+     *  @attention Note: to create a (valid) mask, use FPE::Guard::mask
+     *
+     *  @param The mask of exceptions to guard against
+     *  @param disable Disable or enable the given exceptions 
+     */
+    Guard( mask_type mask, 
+           bool disable   = false )
+      : m_initial( disable ? FPE::detail::disable(mask) : FPE::detail::enable(mask) )
     { }
 
+    /** Default Constructor. Activates protection for all known exceptions
+     *
+     *  @param disable Disable or enable the given exceptions 
+     */
+    explicit Guard( bool disable = false )
+      : m_initial( disable ? 
+                   FPE::detail::disable ( FPE::detail::s_default_guard_mask ) : 
+                   FPE::detail::enable  ( FPE::detail::s_default_guard_mask ) )
+    { }
+    
+    /// Destructor. Returns system to the same state as before the object was constructed
     ~Guard()
     {
-       //TODO: in disable mode, report which FPE happened between c'tor and d'tor.
-       FPE::detail::disable( ~m_initial );
-       mask_type mask = FPE::detail::enable( m_initial );
-       if (mask!=m_initial) { throw GaudiException("oops -- FPEGuard failed to restore initial state","",StatusCode::FAILURE); }
+      //TODO: in disable mode, report which FPE happened between c'tor and d'tor.
+      FPE::detail::disable( ~m_initial );
+      mask_type mask = FPE::detail::enable( m_initial );
+      if (mask!=m_initial) { throw GaudiException("oops -- FPEGuard failed to restore initial state","",StatusCode::FAILURE); }
     }
 
-    /// 'mask' will convert a range of strings (i.e. Iter::value_type
-    /// must be of type std::string) into the corresponding mask to be used
-    /// to enable (or disable) FPE trapping by the FPE::Guard.
-    /// Valid strings are "Inexact","DivByZero","Underflow","OverFlow","Invalid"
-    /// and the catch-all "AllExcept".
+    /**  Convert a range of strings (i.e. Iter::value_type
+     *   must be of type std::string) into the corresponding mask to be used
+     *   to enable (or disable) FPE trapping by the FPE::Guard.
+     *
+     *   Valid strings are "Inexact","DivByZero","Underflow","OverFlow","Invalid"
+     *   and the catch-all "AllExcept".
+     *
+     *   @param begin Begin iterator
+     *   @param end   End iterator
+     *
+     *   @return The exeption mask for the given list of exception strings
+     */
     template <typename Iter>
     static mask_type mask(Iter begin, Iter end) {
-         mask_type m=0;
-         for (;begin!=end;++begin) {
-              std::map<std::string,mask_type>::const_iterator j = FPE::detail::map().find(*begin);
-              if (j==FPE::detail::map().end()) {
-                    throw GaudiException("FPE::mask: unknown mask... ",*begin,StatusCode::FAILURE);
-              }
-              m |= j->second;
-         }
-         return m;
+      mask_type m=0;
+      for (;begin!=end;++begin) {
+        std::map<std::string,mask_type>::const_iterator j = FPE::detail::map().find(*begin);
+        if (j==FPE::detail::map().end()) {
+          throw GaudiException("FPE::mask: unknown mask... ",*begin,StatusCode::FAILURE);
+        }
+        m |= j->second;
+      }
+      return m;
     }
-private:
-    mask_type m_initial;
-};
+
+    /**  Convert a string into the corresponding mask to be used
+     *   to enable (or disable) FPE trapping by the FPE::Guard.
+     *
+     *   Valid strings are "Inexact","DivByZero","Underflow","OverFlow","Invalid"
+     *   and the catch-all "AllExcept".
+     *
+     *   @param excpt The exception type
+     *
+     *   @return The exeption mask for the given exception string
+     */
+    static mask_type mask( const std::string & excpt )
+    {
+      const std::vector<std::string> excpts(1,excpt);
+      return mask( excpts.begin(), excpts.end() );
+    }
+
+  private:
+    mask_type m_initial; ///< Saved mask
+  };
 
 } // namespace FPE
 #endif
