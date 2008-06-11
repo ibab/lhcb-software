937b53de523fd5f3153ea51aecd4a7cd077fa527
hmdegaud <hmdegaud@cern.ch>
2008-06-11 19:01:41 +0000

    added the --silent option which shows only the CRITICAL message.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13193 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/python/LbUtils/Log.py
+++ LbScripts/LbUtils/python/LbUtils/Log.py
@@ -2,17 +2,20 @@ import logging
 
 def setLogLevelCallBack(option, opt_str, value, parser):
     log = logging.getLogger()
+    if opt_str == "--silent":
+        log.setLevel(logging.CRITICAL)
+        setattr(parser.values, option.dest, "CRITICAL")    
     if opt_str == "-q" or opt_str == "--quiet":
         log.info("setting log level to ERROR")
         log.setLevel(logging.ERROR)
-        setattr(parser.values, option.dest, False)
+        setattr(parser.values, option.dest, "ERROR")
     if opt_str == "-v" or opt_str == "--verbose":
         log.setLevel(logging.INFO)
         log.info("setting log level to INFO")
-        setattr(parser.values, option.dest, True)
+        setattr(parser.values, option.dest, "INFO")
     if opt_str == "--debug":
         log.setLevel(logging.DEBUG)
-        setattr(parser.values, option.dest, True)
+        setattr(parser.values, option.dest, "DEBUG")
         log.info("setting log level to DEBUG")
         formatter = logging.Formatter("%(levelname)-8s: %(funcName)-25s %(message)s")
         for h in log.handlers :
@@ -27,22 +30,27 @@ def addDefaultLogger(parser, format=None):
     log = logging.getLogger()
     log.addHandler(console)
     log.setLevel(logging.WARNING)
-    parser.set_defaults(verbose=False)
+    parser.set_defaults(loglevel="WARNING")
     grp = parser.add_option_group("Logging")
+    grp.add_option("--silent",
+                      action="callback",
+                      callback=setLogLevelCallBack, 
+                      dest="loglevel",
+                      help="silent mode")
     grp.add_option("-q", "--quiet",
                       action="callback",
                       callback=setLogLevelCallBack, 
-                      dest="verbose",
-                      help="silent mode")
+                      dest="loglevel",
+                      help="quiet mode")
     grp.add_option("-v", "--verbose",
                       action="callback", 
                       callback=setLogLevelCallBack, 
-                      dest="verbose",
+                      dest="loglevel",
                       help="verbose mode [default: %default]")
     parser.set_defaults(debug=False)
     grp.add_option("--debug",
                       action="callback",
                       callback=setLogLevelCallBack,
-                      dest="debug",
+                      dest="loglevel",
                       help="debug mode")
     return log
