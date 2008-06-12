994871121b358a80ced5e5c7c89e9d1f868938b0
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-06-12 09:11:48 +0000

    v4r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@72109 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5db3a1ba1e77209f8de2dee80ad31f61f34db153

---
+++ Moore/Hlt/Moore/options/Moore.py
@@ -0,0 +1,8 @@
+# Minimal file for running Moore from python prompt
+# Syntax is:
+#   gaudirun.py ../options/Moore.py
+#
+from Gaudi.Configuration import *
+
+importOptions('$MOOREROOT/options/Moore.opts')
+
