2e78a4f2e187f426402541073e474f87c16d9330
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-06-13 12:52:13 +0000

    resurrect .ddl file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72124 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e0b11653b7b94eaecd167bd1fa93426f36b132cd

---
+++ Hlt/Hlt/HltL0Conf/src/HltL0Conf_dll.cpp
@@ -0,0 +1,7 @@
+// $Id: HltL0Conf_dll.cpp,v 1.3 2008-06-13 12:51:29 graven Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+LOAD_FACTORY_ENTRIES(HltL0Conf)
+
---
+++ Hlt/Hlt/HltL0MuonConf/src/HltL0MuonConf_dll.cpp
@@ -0,0 +1,9 @@
+// $Id: HltL0MuonConf_dll.cpp,v 1.3 2008-06-13 12:48:53 graven Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+
+
+LOAD_FACTORY_ENTRIES(HltL0MuonConf)
+
