76ae2f8a71a5eba248bac06f209c26877dfb0da6
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-06-17 21:42:49 +0000

    Move files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72142 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7c882c27069be3c2bc0cebf90e4b5a559869c028

--- Hlt/Hlt/HltBase/HltBase/IHltConfSummaryTool.h
+++ Hlt/Hlt/HltBase/HltBase/IHltConfSummaryTool.h
@@ -1,4 +1,4 @@
-// $Id: IHltConfSummaryTool.h,v 1.8 2008-06-06 12:53:01 hernando Exp $
+// $Id: IHltConfSummaryTool.h,v 1.9 2008-06-17 21:42:49 pkoppenb Exp $
 #ifndef HLTBASE_IHLTCONFSUMMARYTOOL_H 
 #define HLTBASE_IHLTCONFSUMMARYTOOL_H 1
 
@@ -31,68 +31,68 @@ static const InterfaceID IID_IHltConfSummaryTool ( "IHltConfSummaryTool", 1, 0 )
 class IHltConfSummaryTool : virtual public IAlgTool {
 public: 
   
-  // Return the interface ID
+  /// Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IHltConfSummaryTool; }
 
-  // returns the HLT summary
+  /// returns the HLT summary
   virtual const LHCb::HltSummary& summary() = 0;
   
-  /// returns the HLT decision
+  //// returns the HLT decision
   virtual bool decision() = 0;
   
-  /// do not use
+  //// do not use
   virtual bool decisionType(const std::string& name) = 0;
 
-  /// returns true if a given HLT selection is in the summary
+  //// returns true if a given HLT selection is in the summary
   virtual bool hasSelection(const std::string& name) = 0;
 
-  /// returns the decision of a given HLT selecion
+  //// returns the decision of a given HLT selecion
   virtual bool selectionDecision(const std::string& name) = 0;
 
-  /// do not use
+  //// do not use
   virtual bool selectionDecisionType(const std::string& name,
                                      const std::string& type) = 0;
 
-  // returns the list of selection stored in the summmary
+  /// returns the list of selection stored in the summmary
   virtual std::vector<std::string> selections() = 0;
 
-  // returns the number of candidates of a given selection
+  /// returns the number of candidates of a given selection
   virtual size_t selectionNCandidates(const std::string& name) = 0;
   
-  // returns a list of string that code the filter/cuts applied in a selection
+  /// returns a list of string that code the filter/cuts applied in a selection
   virtual std::vector<std::string> selectionFilters(const std::string& name) = 0;
 
-  // returns the type of candidates of a given selection, i.e Track, Vertex, Particle
+  /// returns the type of candidates of a given selection, i.e Track, Vertex, Particle
   virtual std::string selectionType(const std::string& name) = 0;
 
-  // returns a list of input selections required to produce this selection
+  /// returns a list of input selections required to produce this selection
   virtual std::vector<std::string> selectionInputSelections(const std::string& name) = 0;
   
-  // returns a list of track candidates for this selection
+  /// returns a list of track candidates for this selection
   virtual std::vector<LHCb::Track*> selectionTracks(const std::string& name)= 0;
-  // returns a list of vertices candidates for this selection
+  /// returns a list of vertices candidates for this selection
   virtual std::vector<LHCb::RecVertex*> selectionVertices(const std::string& name)= 0;
 
-  // returns a list of particles candidates for this selection
+  /// returns a list of particles candidates for this selection
   virtual std::vector<const LHCb::Particle*> selectionParticles(const std::string& name)= 0;
 
-  // do not use
+  /// do not use
   virtual bool isInSelection(const std::string& name,
                              const LHCb::Track& track ) = 0;
 
-  // returns all the keys stored in the HLT configuration dictionary
+  /// returns all the keys stored in the HLT configuration dictionary
   virtual std::vector<std::string> confKeys() = 0;
 
-  // returns the int value asociated to a HLT configuration key
+  /// returns the int value asociated to a HLT configuration key
   virtual int confInt(const std::string& name) = 0;
   
-  // returns the double value asociated to a HLT configuration key
+  /// returns the double value asociated to a HLT configuration key
   virtual double confDouble(const std::string& name) = 0;
   
-  // returns the string value asociated to a HLT configuration key
+  /// returns the string value asociated to a HLT configuration key
   virtual std::string confString(const std::string& name) = 0;
   
-  // returns a list of strings asociated to a HLT configuration key
+  /// returns a list of strings asociated to a HLT configuration key
   virtual std::vector<std::string> confStringVector(const std::string& name)= 0;
 
   // virtual bool isInSelection(const std::string& name,
