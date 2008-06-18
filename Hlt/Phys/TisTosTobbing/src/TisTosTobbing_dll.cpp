b05c72168228b2e9b0ba12c4245589b1cb0f55c1
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-06-18 07:30:19 +0000

    tagas v2r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72148 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e005c1497aa8d637520183927688434c8c814113

---
+++ Hlt/Phys/TisTosTobbing/src/TisTosTobbing_dll.cpp
@@ -0,0 +1,9 @@
+// $Id: TisTosTobbing_dll.cpp,v 1.3 2008-06-18 07:30:19 pkoppenb Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+//-- Just touch this file (delete this line) so that it will be saved
+
+LOAD_FACTORY_ENTRIES(TisTosTobbing)
+
