e203dec232d2d78969b2f0c8e839fb372ce410f9
Jose Angel Hernando Morata <jose.angel.hernando-morata@cern.ch>
2008-06-19 09:26:53 +0000

    ITrackView interface for drawing guided reco in Panoramix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72157 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ade9cf0811f8da0f78c4c100d754936d666081d

--- Hlt/Hlt/HltBase/HltBase/ITrackUpgrade.h
+++ Hlt/Hlt/HltBase/HltBase/ITrackUpgrade.h
@@ -1,4 +1,4 @@
-// $Id: ITrackUpgrade.h,v 1.3 2007-06-28 22:06:28 hernando Exp $
+// $Id: ITrackUpgrade.h,v 1.4 2008-06-19 09:26:53 hernando Exp $
 #ifndef HLTBASE_ITRACKUPGRADE_H 
 #define HLTBASE_ITRACKUPGRADE_H 1
 
@@ -8,6 +8,7 @@
 
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
+// #include "HltBase/ITrackView.h"
 
 //forward declarations
 namespace LHCb {
@@ -33,9 +34,25 @@ public:
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_ITrackUpgrade; }
 
+ //  // return the list of upgrade track reconstruction 
+//   virtual std::vector<std::string>& recos() = 0;
+  
+  // set this reconstruction as the default one
   virtual StatusCode setReco(const std::string& name) = 0;
-
+  
+  // upgrade a list of input tracks
   virtual StatusCode upgrade( std::vector<LHCb::Track*>& itracks,
                               std::vector<LHCb::Track*>& otracks) = 0;
+  
+//   // upgrade an input track
+//   virtual StatusCode upgrade( LHCb::Track&, 
+//                               std::vector<LHCb*>& tracks) = 0;
+  
+//   // returns the view of the seed track (the volume of search window)
+//   virtual Tf:IStationSelection& view(const LHCb::Track& seed) = 0;
+  
+//   // return the ids of the hits in the search window
+//   virtual std::vector<LHCbID> lhcbIDsInView(const LHCb::Track& seed) = 0;
+
 };
 #endif 
---
+++ Hlt/Hlt/HltBase/HltBase/ITrackView.h
@@ -0,0 +1,39 @@
+// $Id: ITrackView.h,v 1.1 2008-06-19 09:26:53 hernando Exp $
+#ifndef HLTBASE_ITRACKVIEW_H 
+#define HLTBASE_ITRACKVIEW_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+#include "TfKernel/TStationHitManager.h"
+#include "Event/LHCbID.h"
+
+static const InterfaceID IID_ITrackView ( "ITrackView", 1, 0 );
+
+/** @class ITrackView ITrackView.h HltBase/ITrackView.h
+ *  
+ *
+ *  @author Jose Angel Hernando Morata
+ *  @date   2008-06-19
+ */
+class ITrackView : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_ITrackView; }
+
+  // return view of a track (the volume search window open by this seed track)
+  virtual Tf::IStationSelector& view(const LHCb::Track& seed) = 0;
+  
+  // return the hits on the volume search windows of this seed track 
+  virtual std::vector<LHCb::LHCbID> lhcbIDsInView(const LHCb::Track& seed) = 0;
+  
+protected:
+
+private:
+
+};
+#endif // HLTBASE_ITRACKVIEW_H
