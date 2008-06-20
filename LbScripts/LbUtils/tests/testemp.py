831d39bc04e21057aa3c2bb2e2f4248608d66498
hmdegaud <hmdegaud@cern.ch>
2008-06-20 17:48:26 +0000

    added test on temporary directory destruction
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13199 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/tests/testemp.py
+++ LbScripts/LbUtils/tests/testemp.py
@@ -1,6 +1,5 @@
 from LbUtils.Temporary import TempDir
-
-
+import os
 
 import unittest
 class TestCase(unittest.TestCase):
@@ -13,9 +12,17 @@ class TestCase(unittest.TestCase):
         unittest.TestCase.tearDown(self)
         
     def testName(self):
-        name1 = self.tmpdir
+        name1 = str(self.tmpdir)
         name2 = self.tmpdir.getName()
-        print name1,name2, " ",
-#       self.assert_(name1 == name2)
+        # the next line should print twice the same thing
+        print self.tmpdir, name2, " ",
+        self.assert_(name1 == name2)
+
+    def testDestruction(self):
+        mydir = TempDir("mydir")
+        mydirname = mydir.getName()
+        del mydir
+        # the temporary directory should have been removed
+        self.assert_(not os.path.exists(mydirname))
 if __name__ == '__main__':
     unittest.main()
