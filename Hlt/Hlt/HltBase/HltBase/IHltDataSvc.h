599578dd5a731f265a7d09a111eb251b2b9a0c59
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-06-20 14:53:40 +0000

    o add IHltDataSvc
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72167 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a56f9c5ba988429ea3c71ca97aedf324baefb936

---
+++ Hlt/Hlt/HltBase/HltBase/IHltDataSvc.h
@@ -0,0 +1,35 @@
+#ifndef IHLTDATASVC_H
+#define IHLTDATASVC_H
+#include "GaudiKernel/INamedInterface.h"
+#include "GaudiKernel/StatusCode.h"
+#include "HltBase/stringKey.h"
+#include "HltData.h"
+#include "HltTypes.h"
+#include <vector>
+
+class IHltDataSvc : virtual public INamedInterface {
+
+public:
+    static const InterfaceID& interfaceID();
+    virtual ~IHltDataSvc();
+
+    virtual void resetData() = 0;
+
+    //@TODO: need to add flag to indicate ownership: if _also_ in TES, we don't own it,
+    //       and should not delete/clean the contents...
+    //@TODO: add the possibility to have the datasvc put it (also) into the TES...
+    //@TODO: have two interfaces: one to only 'query' the structure, the other to
+    //       get access to everything, the latter inheriting from the former...
+    //@TODO: extend the query capabilities: who uses what as input, who produced which output..
+    //@TODO: move the {register,retrieve}{,T}selection overhere...
+    virtual StatusCode addSelection(Hlt::Selection* sel) = 0;
+    virtual bool hasSelection(const stringKey& id) const = 0;
+    virtual Hlt::Selection& selection(const stringKey& id) =0;
+    virtual std::vector<stringKey> selectionKeys() = 0;
+    virtual void clean() =0;
+
+
+    virtual Hlt::Configuration& config() = 0;
+
+};
+#endif
