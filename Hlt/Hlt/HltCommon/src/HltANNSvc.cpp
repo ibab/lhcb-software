0f6dc4eb363cf3e93822e85fccd2d10766b2149b
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-06-25 20:16:24 +0000

    replace SelectionID by Hlt1SelectionID
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@72222 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 16a59af9ba43f6e13c4dc4f2846a079c9355b93a

--- Hlt/Hlt/HltCommon/src/HltANNSvc.cpp
+++ Hlt/Hlt/HltCommon/src/HltANNSvc.cpp
@@ -7,9 +7,9 @@ class HltANNSvc : public ANNSvc {
 public:
     HltANNSvc( const std::string& name, ISvcLocator *svcLocator )
     :ANNSvc(name, svcLocator, 
-            boost::assign::list_of( std::string("SelectionID") )
-                                  ( std::string("InfoID")      )
+            boost::assign::list_of( std::string("Hlt1SelectionID") )
                                   ( std::string("Hlt2SelectionID")   )
+                                  ( std::string("InfoID")      )
            ) 
     {
         declareProperty("allowUndefined",m_allowUndefined=true,"do we allow undefined, on-demand generated, key/value pairs?");
