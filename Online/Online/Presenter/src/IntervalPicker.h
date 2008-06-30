6eaf2d22441b656c42c7ca39d3d92dfe4d027c5c
psomogyi <psomogyi@cern.ch>
2008-06-30 16:21:30 +0000

    history mode date & time selection I
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98074 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6d5dc58deb1c69facc61ebf914232a834cabc205

--- Online/Online/Presenter/src/IntervalPicker.h
+++ Online/Online/Presenter/src/IntervalPicker.h
@@ -6,7 +6,14 @@
 
 class PresenterMainFrame;
 class Archive;
+class TGVerticalFrame;
 class TGTextButton;
+class TGRadioButton;
+class TGLabel;
+class TGTab;
+class TGCompositeFrame;
+class TGHorizontalFrame;
+class TGNumberEntry;
 
 class IntervalPicker : public TGTransientFrame
 {
@@ -16,12 +23,45 @@ public:
   void build();
   void CloseWindow();
   
+  // Slots
+  void lastMinutesRadioButtonToggled(bool on);
+  void timeIntervalRadioButtonToggled(bool on);
+  void nowButton();
+  void ok();
+  
 private:
   PresenterMainFrame* m_mainFrame;
   Archive*  m_archive;
   pres::MsgLevel  m_verbosity;
-  TGTextButton *m_cancelButton;
   
+  TGVerticalFrame*    m_mainVerticalFrame;
+  TGTextButton*       m_okButton;
+  TGTextButton*       m_cancelButton;
+  TGLabel*            m_begUserLabel;
+  TGTab*              m_mainTab;
+  TGCompositeFrame*   m_timeTabCompositeFrame;
+  TGVerticalFrame*    m_intervalTypeRadioBtnGrpVerticalFrame;
+  TGHorizontalFrame*  m_lastMinutesGrpHorizontalFr;
+  TGRadioButton*      m_lastMinutesRadioButton;
+  TGLabel*            m_fromTimeIntervalLabel;
+  TGNumberEntry*      m_startTimeNumberEntry;
+  TGNumberEntry*      m_startDateNumberEntry;
+  TGNumberEntry*      m_endTimeNumberEntry;
+  TGNumberEntry*      m_endDateNumberEntry;
+  TGNumberEntry*      m_stepTimeNumberEntry;
+  TGLabel*            m_fromTimeIntervalDateLabel;
+  TGHorizontalFrame*  m_minutesHorizontalFrame;
+  TGNumberEntry*      m_minutesEntry;
+  TGLabel*            m_minutesLabel;
+  TGNumberEntry*      m_hoursNumberEntry;
+  TGLabel*            m_hoursLabel;
+  TGLabel*            m_toLabel;
+  TGTextButton*       m_nowTextButton;
+  TGLabel*            m_timeLabel;
+  TGLabel*            m_dateLabel;
+  TGRadioButton*      m_timeIntervalRadioButton;
+  TGCompositeFrame*   m_runFillTabCompositeFrame;
+
 ClassDef(IntervalPicker, 0) 
 };
 
