335c90cdb4962c8379254ee9b8bba9f461af997f
Patrick Robbe <robbe@lal.in2p3.fr>
2008-07-01 19:29:23 +0000

    Add script to produce web page
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93514 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6d67b5e001967816f568bc4e16d6b3b1d0283f64

---
+++ Gauss/Gen/GenTests/python/GenTests/CheckAllHistograms.py
@@ -0,0 +1,54 @@
+import os
+
+GaussVersion = 'v31r1'
+
+listofhistos = {
+    '30000000': 'MinBias',
+#    '11102013': 'BdpipiCPVDecProdCut',
+    '13264001': 'BsDspiDecProdCut',
+    '12463010': 'BuD0hKpicocktail',
+    '10000000': 'InclB',
+    '20000000': 'InclD',
+    '24142001': 'InclJpsi'
+    }
+
+refCastor = '/castor/cern.ch/user/r/robbep/GaussREF/'
+pathCastor = '/castor/cern.ch/user/r/robbep/GaussGen_' + GaussVersion + '/'
+
+# copy the index page from the Web area
+webarea = os.environ['LHCBDOC'] + '/gauss/generator/validation/'
+os.system( 'cp -f '+webarea+'index.html .')
+
+for eventtype, eventname in listofhistos.iteritems():
+    # Copy CASTOR files locally
+    refNameHisto = refCastor + 'GaussHistos_' + eventtype + '.root'
+    copyRefHisto = 'rfcp ' + refNameHisto + ' /tmp/robbep'
+    refNameLog = refCastor + 'GaussOutput_' + eventtype + '.txt'
+    copyRefLog = 'rfcp ' + refNameLog + ' /tmp/robbep'
+
+    os.system( copyRefHisto )
+    os.system( copyRefLog )
+
+    nameHisto = pathCastor + 'GaussHistos_' + GaussVersion + '_' + eventtype + '.root'
+    copyHisto = 'rfcp ' + nameHisto + ' /tmp/robbep'
+    nameLog = pathCastor + 'GaussOutput_' + GaussVersion + '_' + eventtype + '.txt'
+    copyLog = 'rfcp ' + nameLog + ' /tmp/robbep'
+
+    os.system( copyHisto )
+    os.system( copyLog )
+
+    # then do the tests
+    os.system( 'python GeneratorReferencePlots.py --refhisto /tmp/robbep/GaussHistos_'+eventtype+'.root --reflog /tmp/robbep/GaussOutput_'+eventtype+'.txt --histo /tmp/robbep/GaussHistos_' + GaussVersion + '_' + eventtype + '.root --log /tmp/robbep/GaussOutput_' + GaussVersion + '_' + eventtype + '.txt --add-to-index --install' )
+
+    # move to web area
+    os.system( 'cp -rf Gauss_' + eventtype + '_*' + ' ' + webarea )   
+
+    # Do some cleanup
+    os.system( 'rm *.eps' )
+    os.system( 'rm -rf Gauss_' + eventtype + '_*' )
+    os.system( 'rm *.jpg' )
+    os.system( 'rm page.html' )
+
+os.system( 'cp -f index.html ' + webarea )
+os.system( 'rm index.html' )
+
