21814a6ccdc2a3b601a3434086ab78f6a626b883
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2008-07-04 21:40:30 +0000

    Updated interface to allow default argument
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64181 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2a01e07c5fac28142ac9ad99363d0ef0ef451f71

--- Phys/Phys/DaVinciMCKernel/Kernel/IDaVinciAssociatorsWrapper.h
+++ Phys/Phys/DaVinciMCKernel/Kernel/IDaVinciAssociatorsWrapper.h
@@ -1,4 +1,4 @@
-// $Id: IDaVinciAssociatorsWrapper.h,v 1.1 2008-06-26 23:48:19 gligorov Exp $
+// $Id: IDaVinciAssociatorsWrapper.h,v 1.2 2008-07-04 21:38:05 gligorov Exp $
 #ifndef IDAVINCIASSOCIATORSWRAPPER_H 
 #define IDAVINCIASSOCIATORSWRAPPER_H 1
 
@@ -10,12 +10,29 @@
 #include "GaudiKernel/IAlgTool.h"
 
 #include "Kernel/Particle2MCLinker.h"
+#include "Kernel/Particle2MCMethod.h"
 
 static const InterfaceID IID_IDaVinciAssociatorsWrapper ( "IDaVinciAssociatorsWrapper", 1, 0 );
 
 /** @class IDaVinciAssociatorsWrapper IDaVinciAssociatorsWrapper.h
  *  
  *  Wrapper around  Particle2MCLinker class
+ *
+ *  To use:
+ *  
+ *  Particle2MCLinker mylinker = m_myLinkerTool->linker(linkerMethod,container);
+ *
+ *  where linker Method corresponds to one of the Methods declared in Kernel/Particle2MCMethod.h,
+ *  for example Link, Composite, Chi2. Container allows you to specify a specifi location for the
+ *  particles to be associated, It defaults to 0 if omitted:
+ * 
+ *  Particle2MCLinker mylinker = m_myLinkerTool->linker(linkerMethod);
+ *
+ *  Note that you will need to declare a different instance of this tool
+ *  for each linker which you wish to use. Declare an instance in your
+ *  algorithm's initialize:
+ *
+ *  m_myLinkerTool = tool<IDaVinciAssociatorsWrapper>("DaVinciAssociatorsWrapper","exampleTool",this);
  *
  *  @author V. Gligorov (based on code by P. Koppenburg)
  *  @date   2008-06-26
@@ -26,7 +43,10 @@ public:
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IDaVinciAssociatorsWrapper; }
 
-  virtual Particle2MCLinker* linker(std::string, std::vector< std::string >) = 0 ; ///< returns a linker
+  virtual Particle2MCLinker* linker(int, 
+                                    std::vector< std::string > myLocations
+                                      = std::vector<std::string>(1,"")
+                                   ) = 0 ; ///< returns a linker
 protected:
 
 private:
