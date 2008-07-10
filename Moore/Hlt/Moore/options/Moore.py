90527ba03f352fc6a65141214dfbdc951b508e2b
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-07-10 18:15:36 +0000

    further use of Configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@72501 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 411ac7f1c5accead9be06b233ffb19b65ad4584b

--- Moore/Hlt/Moore/options/Moore.py
+++ Moore/Hlt/Moore/options/Moore.py
@@ -6,17 +6,6 @@ from Gaudi.Configuration import *
 from Moore.Configuration import *
 
 
-
-# Get the event time (for CondDb) from ODIN 
-from Configurables import EventClockSvc
-EventClockSvc().EventTimeDecoder = 'OdinTimeDecoder'
-
-# Profiling
-AuditorSvc().Auditors  = [ 'NameAuditor', 'ChronoAuditor', 'MemoryAuditor' ]
-NameAuditor().Enable   = False
-ChronoAuditor().Enable = False
-MemoryAuditor().Enable = False
-
 # General output levels 
 ToolSvc().OutputLevel                     = INFO
 from Configurables import XmlParserSvc
@@ -27,36 +16,11 @@ ApplicationMgr().OutputLevel              = ERROR
 # Print algorithm name with 40 characters
 MessageSvc().Format = '% F%40W%S%7W%R%T %0W%M'
 
-#/-----------------------------------------------------------------
-#/ Rerun L0 and HLT - Alleys
-#/-----------------------------------------------------------------
-
-
-#/ Default options to rerun L0 and run Hlt 
-importOptions('$HLTSYSROOT/options/L0.opts')
-importOptions('$HLTSYSROOT/options/HltInit.opts')
-importOptions('$HLTSYSROOT/options/Hlt1.opts')
-importOptions('$HLTSYSROOT/options/Hlt2.opts')
-
-# For DAQ studies replace the includes above for these ones:
-# importOptions('$HLTSYSROOT/options/L0DAQ.opts')
-# importOptions('$HLTSYSROOT/options/HltDAQ.opts')
-
-# include this options to get time or histogram information of alleys
-# importOptions('$HLTSYSROOT/options/HltAlleysTime.opts')
-# importOptions('$HLTSYSROOT/options/HltAlleysHistos.opts')
-
-
 #//---------------------------------------------------------------------------
 #// Histogram output
 #//---------------------------------------------------------------------------
 ApplicationMgr().HistogramPersistency = 'ROOT'
 HistogramPersistencySvc().OutputFile = 'Moore_minbias.root'
-
-for i in [ 'ToolSvc' , 'AuditorSvc' ] : ApplicationMgr().ExtSvc.append( i ) 
-ApplicationMgr().AuditAlgorithms = 1
-AuditorSvc().Auditors.append( 'TimingAuditor/TIMER' )
-
 
 #//---------------------------------------------------------------------------
 #// Number of events to process, optionally skipping some events
@@ -64,14 +28,14 @@ AuditorSvc().Auditors.append( 'TimingAuditor/TIMER' )
 ApplicationMgr().EvtMax =  10000 ;
 
 Moore().applyConf()
+print Moore()
 
-files= [ '/data/bfys/lhcb/MinBias-L0strip/MBL0-lumi2-' + str(f) +'.dst'  for f in range(1,5) ]
+# files= [ '/data/bfys/lhcb/MinBias-L0strip/MBL0-lumi2-' + str(f) +'.dst'  for f in range(1,5) ]
 
-
-#files = [ '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-1.dst',
-#          '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-2.dst',
-#          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-3.dst',
-#          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-4.dst' ]
+files = [ '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-1.dst',
+          '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-2.dst',
+          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-3.dst',
+          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-4.dst' ]
 
 EventSelector().Input =[ "DATAFILE='PFN:"+ f +"' TYP='POOL_ROOTTREE' OPT='READ'" for f in files ]
 EventSelector().PrintFreq = 100;
--- Moore/Hlt/Moore/python/Moore/Configuration.py
+++ Moore/Hlt/Moore/python/Moore/Configuration.py
@@ -1,12 +1,14 @@
 """
 High level configuration tools for Moore
 """
-__version__ = "$Id: Configuration.py,v 1.3 2008-07-10 16:16:01 graven Exp $"
+__version__ = "$Id: Configuration.py,v 1.4 2008-07-10 18:15:36 graven Exp $"
 __author__  = "Gerhard Raven <Gerhard.Raven@nikhef.nl>"
 
 from os import environ
 from Gaudi.Configuration import *
 from GaudiConf.Configuration import *
+from Configurables import ConfigFileAccessSvc, ConfigDBAccessSvc, HltConfigSvc, HltGenConfig
+from Configurables import EventClockSvc
 
 import GaudiKernel.ProcessJobOptions
 
@@ -18,6 +20,14 @@ class Moore(ConfigurableUser):
         , "IncludeHlt1":   True # include Hlt1 options
         , "IncludeHlt2":   True # include Hlt2 options
         , "RunTiming"  :   False # include Hlt1 options
+        , "UseTCK"     :   False # use TCK instead of options...
+        , "PrefetchTCK" :   []   # which TCKs to prefetch. Initial TCK used is first one...
+        , "GenerateConfig" : False # whether or not to generate a configuration
+        , "TCKDirectory" : '/user/graven/tmp/config' # where do we read TCKs from?
+        #TODO: the next can probably be done in better way...
+        , "EnableMemoryAuditor" : False
+        , "EnableChronoAuditor" : False
+        , "EnableNameAuditor" : False
         }   
                 
     def getProp(self,name):
@@ -35,23 +45,49 @@ class Moore(ConfigurableUser):
         else:
             return ApplicationMgr().getDefaultProperties()["EvtMax"]
 
+    def getConfigAccessSvc(self):
+        # TODO: avoid hardwiring ConfigAccessSvc...
+        return ConfigFileAccessSvc( Directory = self.getProp('TCKDirectory') )
+    def enableAuditor(self,x) :
+        AuditorSvc().Auditors.append( x.name() )
+        x.Enable = True
+
     def applyConf(self):
         GaudiKernel.ProcessJobOptions.printing_level += 1
         importOptions('$STDOPTS/LHCbApplication.opts')
         importOptions('$STDOPTS/DstDicts.opts')
         importOptions('$STDOPTS/DC06Conditions.opts')
-        if self.getProp("DAQStudies") :
-            if self.getProp('IncludeL0') : importOptions('$HLTSYSROOT/options/L0DAQ.opts')
-            importOptions('$HLTSYSROOT/options/HltDAQ.opts')
-        else :
-            if self.getProp('IncludeL0') : 
-                importOptions('$HLTSYSROOT/options/L0.opts')
-            importOptions('$HLTSYSROOT/options/HltInit.opts')
-            if self.getProp('IncludeHlt1') : 
-                importOptions('$HLTSYSROOT/options/Hlt1.opts')
-            if self.getProp('IncludeHlt2') : 
-                importOptions('$HLTSYSROOT/options/Hlt2.opts')
-        if self.getProp('RunTiming') :
-            importOptions('$HLTSYSROOT/options/HltAlleysTime.opts')
-            importOptions('$HLTSYSROOT/options/HltAlleysHistos.opts')
+        # Get the event time (for CondDb) from ODIN 
+        EventClockSvc().EventTimeDecoder = 'OdinTimeDecoder'
+        # Profiling
+        for i in [ 'ToolSvc' , 'AuditorSvc' ] : ApplicationMgr().ExtSvc.append( i ) 
+        ApplicationMgr().AuditAlgorithms = 1
+        AuditorSvc().Auditors.append( 'TimingAuditor/TIMER' )
+        if self.getProp('EnableNameAuditor')   : self.enableAuditor( NameAuditor() )
+        if self.getProp('EnableChronoAuditor') : self.enableAuditor( ChronoAuditor() )
+        if self.getProp('EnableMemoryAuditor') : self.enableAuditor( MemoryAuditor() )
+        # TODO: check for mutually exclusive options...
+        if self.getProp('UseTCK') :
+            cas = getConfigAccessSvc()
+            tcks = self.getProp('PrefetchTCK')
+            cfg = HltConfigSvc( prefetchTCK = tcks, initialTCK = tcks[0], ConfigAccessSvc = cas.getFullName() ) 
+            ApplicationMgr().ExtSvc.append(cfg.getFullName())
+        else:
+            if self.getProp("DAQStudies") :
+                if self.getProp('IncludeL0') : importOptions('$HLTSYSROOT/options/L0DAQ.opts')
+                importOptions('$HLTSYSROOT/options/HltDAQ.opts')
+            else :
+                if self.getProp('IncludeL0') : importOptions('$HLTSYSROOT/options/L0.opts')
+                importOptions('$HLTSYSROOT/options/HltInit.opts')
+                if self.getProp('IncludeHlt1') : importOptions('$HLTSYSROOT/options/Hlt1.opts')
+                if self.getProp('IncludeHlt2') : importOptions('$HLTSYSROOT/options/Hlt2.opts')
+            if self.getProp('RunTiming') :
+                importOptions('$HLTSYSROOT/options/HltAlleysTime.opts')
+                importOptions('$HLTSYSROOT/options/HltAlleysHistos.opts')
+        if self.getProp("GenerateConfig") :
+            gen = HltGenConfig( ConfigTop = [ i.rsplit('/')[-1] for i in ApplicationMgr().TopAlg ],
+                                ConfigSvc = [ 'ToolSvc','HltDataSvc','HltANNSvc' ],
+                                ConfigAccessSvc = getConfigAccessSvc().getName() )
+            # make sure gen is the very first Top algorithm...
+            ApplicationMgr().TopAlg = [ gen.getFullName() ] + ApplicationMgr().TopAlg
         LHCbApp().applyConf()
