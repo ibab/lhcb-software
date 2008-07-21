cafe385bfddae385192caaff89fee41c52419bab
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-07-21 15:34:40 +0000

    Remove obsolete hack
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@4915 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dd6afca6802515b23b30b5c4bb73637c47bd5fa4

--- LHCb/Det/DetDesc/dict/DetDescDict.h
+++ LHCb/Det/DetDesc/dict/DetDescDict.h
@@ -1,15 +1,7 @@
-// $Id: DetDescDict.h,v 1.5 2008-07-21 14:51:45 cattanem Exp $
+// $Id: DetDescDict.h,v 1.6 2008-07-21 15:34:40 cattanem Exp $
 // ============================================================================
 #ifndef DETSYS_DETDICT_H 
 #define DETSYS_DETDICT_H 1
-// ============================================================================
-// Hack to get round gccxml parsing problem (SEAL bug 9704)
-// ============================================================================
-#ifdef _WIN32
-#define LONG_LONG_MAX     0x7fffffffffffffffLL  /*maximum signed __int64 value */
-#define LONG_LONG_MIN     0x8000000000000000LL  /*minimum signed __int64 value */
-#define ULONG_LONG_MAX    0xffffffffffffffffLL  /*maximum unsigned __int64 value */
-#endif
 // ============================================================================
 // DetDesc 
 // ============================================================================
