edfb721ff1be04cb23340a80d3627246d39e47ec
Julien Cogan <jucogan@cern.ch>
2008-07-24 08:55:21 +0000

    Add functions useful for the monitoring
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@59749 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 21477e6efed67e5070bc7e3edf96d5b85835cbf5

---
+++ LHCb/L0/L0MuonKernel/L0MuonKernel/MonUtilities.h
@@ -0,0 +1,55 @@
+// $Id: MonUtilities.h,v 1.1 2008-07-24 08:55:21 jucogan Exp $
+#ifndef COMPONENT_MONUTILITIES_H 
+#define COMPONENT_MONUTILITIES_H 1
+
+#include <vector>
+
+#include "Kernel/MuonTileID.h"
+
+namespace L0Muon
+{
+  
+  namespace MonUtilities
+  {
+    enum Channel_type {Pad, StripH, StripV , nb_channel_types};
+    enum Stations {M1=0,M2,M3,M4,M5,NStations};
+    enum Regions  {R1=0,R2,R3,R4,NRegions};
+    enum Quarters {Q1=0,Q2,Q3,Q4,NQuarters};
+    enum Errors {Overflow=0,Internal,Decoding,Decoding_data,NErrors};
+    enum BoardTypes {Proc=0,Ctrl,NBoardTypes};
+    
+    void makePads(std::vector<LHCb::MuonTileID>& tiles, std::vector<LHCb::MuonTileID>& pads);
+    void filterTilesWithStation(std::vector<LHCb::MuonTileID>& original,
+                                std::vector<LHCb::MuonTileID>& filtered,
+                                unsigned int station);
+    void flipCoord(int &x, int&y, const int qua);
+    std::string channelTypeName(Channel_type e);
+    std::string quarterName(int qua);
+    std::string boardName(int board);
+    std::string regionName(int reg);
+    std::string stationName(int sta);
+    std::string errorName(int type);
+    std::string timeSlot(int bx);
+  
+    std::string hname_info_error(std::string toolname="");
+    std::string hname_info_bid(std::string toolname="")  ;
+    std::string hname_info_bid2(std::string toolname="") ;
+    std::string hname_info_evt(std::string toolname="") ;
+    std::string hname_pads_map(int sta,std::string toolname="") ;
+    std::string hname_pads_multiBx(int sta,std::string toolname="") ;
+    std::string hname_pads_multiBx_region(int sta, int reg,std::string toolname="") ;
+    std::string hname_error_ctrl1(int type, std::string toolname="") ;
+    std::string hname_error_ctrl2(int type, int qua, std::string toolname="") ;
+    std::string hname_error_proc(int type, int qua, int board, std::string toolname="") ;
+    std::string hname_channels_hist(int qua,Channel_type type, int sta, int reg,std::string toolname="") ;
+    std::string hname_channels_hist2D(int qua,Channel_type type, int sta, int reg,std::string toolname="") ;
+    std::string hname_channels_histBX(int qua,Channel_type type, int sta, int reg,std::string toolname="") ;
+    std::string hname_channels_histDT(int qua,Channel_type type, int sta, int reg,std::string toolname="") ;
+
+  } // namespace MonUtilities
+} // namespace L0Muon
+
+  
+L0Muon::MonUtilities::Channel_type operator++(L0Muon::MonUtilities::Channel_type &rs, int );
+
+#endif // COMPONENT_MONUTILITIES_H
---
+++ LHCb/L0/L0MuonKernel/src/MonUtilities.cpp
@@ -0,0 +1,204 @@
+// $Id: MonUtilities.cpp,v 1.1 2008-07-24 08:55:21 jucogan Exp $
+// Include files 
+
+#include <sstream>
+
+// local
+#include "L0MuonKernel/MonUtilities.h"
+
+void L0Muon::MonUtilities::makePads(std::vector<LHCb::MuonTileID>& strips,std::vector<LHCb::MuonTileID>& pads)
+{
+
+  pads.clear();
+
+  std::vector<LHCb::MuonTileID>::iterator i1;
+  std::vector<LHCb::MuonTileID>::iterator i2;
+  for (i1=strips.begin(); i1<strips.end(); ++i1){
+    if (i1->station()==0) {
+      pads.push_back(*i1);// M1 contains pads already
+    } else if ((i1->region()==0) & (i1->station()==3 || i1->station()==4)){
+      pads.push_back(*i1);// M4 and M5 contains pads already in R1
+    } else {
+      for (i2=i1+1; i2<strips.end(); ++i2){
+        LHCb::MuonTileID pad = (*i1).intercept(*i2);
+        if (pad.isValid()){
+          pads.push_back(pad);
+        }
+      }
+    }
+  }
+
+}
+
+void L0Muon::MonUtilities::filterTilesWithStation(std::vector<LHCb::MuonTileID>& original,
+                                            std::vector<LHCb::MuonTileID>& filtered, unsigned int station)
+{
+  filtered.clear();
+  
+  std::vector<LHCb::MuonTileID>::iterator it;
+  for (it=original.begin();it<original.end();++it){
+    if (it->station()==station) filtered.push_back(*it);
+  }
+
+}
+
+
+void L0Muon::MonUtilities::flipCoord(int &x, int&y, const int qua)
+{
+  switch ( qua ) {   
+  case 0: 
+    break;
+  case 1: 
+    y=-y;
+    break;
+  case 2: 
+    x=-x;
+    y=-y;
+    break;
+  case 3: 
+    x=-x;
+    break;
+  };
+}
+  
+  
+std::string  L0Muon::MonUtilities::channelTypeName(Channel_type e)
+{ 
+  switch ( e ) {   
+  case Pad    : return "Pad";
+  case StripH : return "StripH";
+  case StripV : return "StripV";
+  default : return "Unknown";
+  };
+};
+  
+std::string  L0Muon::MonUtilities::quarterName(int qua)
+{ 
+  switch ( qua ) {   
+  case 0    : return "Q1";
+  case 1    : return "Q2";
+  case 2    : return "Q3";
+  case 3    : return "Q4";
+  default : return "Unknown";
+  };
+};
+std::string  L0Muon::MonUtilities::boardName(int board)
+{
+  std::stringstream name("");
+  name<<"PB";
+  name<<board;
+  return name.str();
+};
+  
+std::string  L0Muon::MonUtilities::regionName(int reg)
+{ 
+  switch ( reg ) {   
+  case 0    : return "R1";
+  case 1    : return "R2";
+  case 2    : return "R3";
+  case 3    : return "R4";
+  default : return "Unknown";
+  };
+};
+  
+std::string  L0Muon::MonUtilities::stationName(int sta)
+{ 
+  switch ( sta ) {   
+  case 0    : return "M1";
+  case 1    : return "M2";
+  case 2    : return "M3";
+  case 3    : return "M4";
+  case 4    : return "M5";
+  default : return "Unknown";
+  };
+};
+
+std::string  L0Muon::MonUtilities::errorName(int type)
+{ 
+  switch ( type ) {   
+  case 0 : return "Overflow";
+  case 1 : return "Internal";
+  case 2 : return "Decoding";
+  case 3 : return "Decoding_data";
+  default : return "Unknown";
+  };
+};
+
+std::string  L0Muon::MonUtilities::timeSlot(int bx)
+{
+  std::string ts;
+  switch (bx) {
+  case -7 : return "Prev7/";
+  case -6 : return "Prev6/";
+  case -5 : return "Prev5/";
+  case -4 : return "Prev4/";
+  case -3 : return "Prev3/";
+  case -2 : return "Prev2/";
+  case -1 : return "Prev1/";
+  case  0 : return "";
+  case  1 : return "Next1/";
+  case  2 : return "Next2/";
+  case  3 : return "Next3/";
+  case  4 : return "Next4/";
+  case  5 : return "Next5/";
+  case  6 : return "Next6/";
+  case  7 : return "Next7/";
+  default : return "Unknown";
+  };
+};
+
+L0Muon::MonUtilities::Channel_type operator++( L0Muon::MonUtilities::Channel_type &rs, int )
+{
+  return rs = (L0Muon::MonUtilities::Channel_type)(rs + 1);
+};
+
+
+std::string L0Muon::MonUtilities::hname_info_error(std::string toolname)  {return toolname+"Error_and_overflow";}
+
+std::string L0Muon::MonUtilities::hname_info_bid(std::string toolname)  {return toolname+"L0_B_Id";}
+
+std::string L0Muon::MonUtilities::hname_info_bid2(std::string toolname)  {return toolname+"L0_B_Id_coarse";}
+
+std::string L0Muon::MonUtilities::hname_info_evt(std::string toolname)  {return toolname+"EventNumber";}
+
+std::string L0Muon::MonUtilities::hname_pads_map(int sta,std::string toolname)  {
+  return toolname+stationName(sta);
+}
+
+std::string L0Muon::MonUtilities::hname_pads_multiBx(int sta,std::string toolname)  {
+  return toolname+"multiplicity_per_time_slot_"+stationName(sta);
+}
+
+std::string L0Muon::MonUtilities::hname_pads_multiBx_region(int sta, int reg,std::string toolname)  {
+  return toolname+"multiplicity_per_time_slot_"+stationName(sta)+"_"+regionName(reg);
+}
+
+std::string L0Muon::MonUtilities::hname_error_ctrl1(int type, std::string toolname)  {
+  return toolname+errorName(type);
+}
+
+std::string L0Muon::MonUtilities::hname_error_ctrl2(int type, int qua, std::string toolname)  {
+  return toolname+errorName(type)+"_"+quarterName(qua);
+}
+
+std::string L0Muon::MonUtilities::hname_error_proc(int type, int qua, int board, std::string toolname)  {
+  return toolname+errorName(type)+"_"+quarterName(qua)+"_"+boardName(board);
+}
+
+std::string L0Muon::MonUtilities::hname_channels_hist(int qua,Channel_type type, int sta, int reg,std::string toolname)    {
+  return toolname+"Hits_"+quarterName(qua)+"_"+stationName(sta)
+    +"_"+regionName(reg)+"_"+channelTypeName(type);
+}
+
+std::string L0Muon::MonUtilities::hname_channels_hist2D(int qua,Channel_type type, int sta, int reg,std::string toolname)  {
+  return hname_channels_hist(qua,type,sta,reg,toolname)+"2D";
+}
+
+std::string L0Muon::MonUtilities::hname_channels_histBX(int qua,Channel_type type, int sta, int reg,std::string toolname)  {
+  return hname_channels_hist(qua,type,sta,reg,toolname)+"-VS-BX";
+}
+
+std::string L0Muon::MonUtilities::hname_channels_histDT(int qua,Channel_type type, int sta, int reg,std::string toolname)  {
+  return hname_channels_hist(qua,type,sta,reg,toolname)+"-wrt-Muon";
+}
+
