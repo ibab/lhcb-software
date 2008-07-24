74d736b2785dfd323af516460b3d534f397a45af
Julien Cogan <jucogan@cern.ch>
2008-07-24 09:31:38 +0000

    add implementation for the index function
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36753 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: af82e246811ffa791105af294e6f4caa86652e78

---
+++ LHCb/Event/L0Event/src/L0MuonError.cpp
@@ -0,0 +1,75 @@
+// local
+#include "Event/L0MuonError.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : L0MuonError
+//
+// 2008-07-22 : Julien Cogan
+//-----------------------------------------------------------------------------
+
+void LHCb::L0MuonError::index( int & quarter, int & board, int & pu ) const{
+
+  LHCb::MuonTileID mid=key();
+  
+
+  quarter = mid.quarter();
+  
+
+  MuonLayout lay=mid.layout();
+  
+  int xgrid=lay.xGrid();
+  
+  int ygrid=lay.yGrid();
+  
+
+  // CU & SU
+  if (xgrid==0 && ygrid==0) {
+    
+    board=15;
+    
+    pu=-1;
+    
+    return;
+    
+  }
+  
+
+  // BCSU
+  if (xgrid==1 && ygrid==1) {
+    
+    board=mid.region()*3+mid.nY()*2+mid.nX()-1;
+    
+    pu=5;
+    
+    return;
+    
+  }
+  
+
+  // PU
+  if (xgrid==2 && ygrid==2) {
+    
+    LHCb::MuonTileID mid_board=MuonLayout(1,1).contains(mid);
+    
+    board=mid_board.region()*3+mid_board.nY()*2+mid_board.nX()-1;
+    
+    pu=mid.nY()*2+mid.nX();
+    
+    return;
+    
+  }
+  
+
+  // Unknown
+  quarter=-1;
+  
+  board=-1;
+  
+  pu=-1;
+  
+
+  return;
+  
+
+
+}
