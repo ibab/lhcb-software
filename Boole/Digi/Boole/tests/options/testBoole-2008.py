d07ebe6e5a903b24df2e11ee227a0f448586718a
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-07-24 18:09:09 +0000

    add FPE test and latest reference
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@59763 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 85cc8f0d22cac6021ed2f4055c3ce9b0f93136f0

--- Boole/Digi/Boole/tests/options/testBoole-2008.py
+++ Boole/Digi/Boole/tests/options/testBoole-2008.py
@@ -9,25 +9,12 @@ Boole().useSpillover = False
 Boole().DDDBtag      = "2008-default"
 Boole().condDBtag    = "2008-default"
 Boole().writeL0ETC   = True   # Write also ETC of L0 selected events
-Boole().monitors    += ["SC"] # Add StatusCode check
+Boole().monitors    += ["FPE", "SC"] # Add Floating Point and StatusCode checks
 
 Boole().applyConf()
 
 # Do not compare times....
 TimingAuditor().OutputLevel = WARNING 
 
-datasetName = '11144103-100ev-20080613'
-EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/g/gcorti/Gauss/2008/v31r2/" + datasetName + ".sim' TYP='POOL_ROOTTREE' OPT='READ'"]
-
-# Set the property used to build other file names
-Boole().setProp( "datasetName", datasetName )
-outputName = Boole().outputName()
-
-#-- Save the monitoring histograms
-HistogramPersistencySvc().OutputFile = Boole().histosName()
-
-#-- Digi output stream
-OutputStream("DigiWriter").Output = "DATAFILE='PFN:" + outputName + ".digi' TYP='POOL_ROOTTREE' OPT='REC'"
-
-#-- ETC output stream
-TagCollectionStream( "WR" ).Output = "Collection='EVTTAGS/TagCreator/1' ADDRESS='/Event' DATAFILE='" + datasetName + "-L0ETC.root' TYP='POOL_ROOTTREE' OPT='RECREATE'"
+# Data files
+importOptions( "$BOOLEOPTS/2008-Files.py" ) 
--- Boole/Digi/Boole/tests/options/testBoole-DC06.py
+++ Boole/Digi/Boole/tests/options/testBoole-DC06.py
@@ -13,25 +13,5 @@ Boole().applyConf()
 # Do not compare times....
 TimingAuditor().OutputLevel = WARNING 
 
-#-- Main ('signal') event input
-datasetName = "00001820_00000001"
-EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001820/SIM/0000/" + datasetName + "_1.sim' TYP='POOL_ROOTTREE' OPT='READ'"]
-
-#-- Spillover events
-EventSelector("SpilloverSelector").Input = [
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001820/SIM/0000/" + datasetName + "_2.sim' TYP='POOL_ROOTTREE' OPT='READ'"
-   ,"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001820/SIM/0000/" + datasetName + "_3.sim' TYP='POOL_ROOTTREE' OPT='READ'"
-    ]
-
-# Set the property used to build other file names
-Boole().setProp( "datasetName", datasetName )
-outputName = Boole().outputName()
-
-#-- Save the monitoring histograms
-HistogramPersistencySvc().OutputFile = Boole().histosName()
-
-#-- Digi output stream
-OutputStream("DigiWriter").Output = "DATAFILE='PFN:" + outputName + ".digi' TYP='POOL_ROOTTREE' OPT='REC'"
-
-#-- ETC output stream
-TagCollectionStream( "WR" ).Output = "Collection='EVTTAGS/TagCreator/1' ADDRESS='/Event' DATAFILE='" + datasetName + "-L0ETC.root' TYP='POOL_ROOTTREE' OPT='RECREATE'"
+# Data files
+importOptions( "$BOOLEOPTS/DC06-Files.py" ) 
