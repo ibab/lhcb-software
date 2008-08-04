e88b15c6287cc6b666c570993adce2ba1ebbfa48
Matthew David Needham <mneedham@cern.ch>
2008-08-04 07:23:56 +0000

    Add more python nonsense
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@27172 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e75cbe29fbe77d729219c8678e93637765ba71ab

--- LHCb/Det/STDet/dict/STDetDict.h
+++ LHCb/Det/STDet/dict/STDetDict.h
@@ -1,4 +1,4 @@
-// $Id: STDetDict.h,v 1.2 2008-03-14 18:26:39 mneedham Exp $
+// $Id: STDetDict.h,v 1.3 2008-08-04 07:23:56 mneedham Exp $
 // ============================================================================
 #ifndef STDET_STDETDICT_H 
 #define STDET_STDETDICT_H 1
@@ -25,6 +25,19 @@
 
 // Needed to resolve forward declaration when making dictiobary
 #include "Kernel/Trajectory.h"
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::vector<DeTTStation*> m_ttStation_vector;
+    std::vector<DeITStation*> m_itStation_vector;
+    std::vector<DeITSector*> m_itsector_vector;
+    std::vector<DeITSensor*> m_itsensor_vector;
+    std::vector<DeTTSensor*> m_ttsensor_vector;
+    std::map<unsigned int,DeSTSector::Status> m_unsigned_int_Status_map; 
+    std::pair<unsigned int,DeSTSector::Status> m_unsigned_int_Status_pair;
+  } ;
+};  
 
 // ============================================================================
 #endif // STDET_STDETDICT_H
