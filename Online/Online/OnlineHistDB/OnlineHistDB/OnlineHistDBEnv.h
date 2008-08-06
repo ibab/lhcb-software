b384b00e6fbaad6cf2fcd5ceee992134c7cd96ab
Giacomo Graziani <ggiacomo@cern.ch>
2008-08-06 08:08:08 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98226 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ae1433800cbb4d744df65763e13de6ef5652731e

--- Online/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h
+++ Online/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h
@@ -1,4 +1,4 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h,v 1.26 2008-05-14 15:00:55 ggiacomo Exp $
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h,v 1.27 2008-08-06 08:08:08 ggiacomo Exp $
 #ifndef ONLINEHISTDBENV_H
 #define ONLINEHISTDBENV_H 1
 /** @class  OnlineHistDBEnv OnlineHistDBEnv.h OnlineHistDB/OnlineHistDBEnv.h
@@ -22,8 +22,8 @@ namespace OnlineHistDBEnv_constants {
   static const std::string DB="HISTDB";
   static const std::string ACCOUNT="HIST_READER";
   static const std::string PASSWORD="reader";
-  static const unsigned int DBschema = 7;
-  static const std::string version = "v4r9";
+  static const unsigned int DBschema = 8;
+  static const std::string version = "v4r10";
 
   static const int NHTYPES=6;
   static const char HistTypeName[][4] = {
