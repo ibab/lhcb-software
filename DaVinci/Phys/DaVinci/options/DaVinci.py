765429544575b016b52abd69e4107371934508fa
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-08-06 10:54:10 +0000

    new options
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@72861 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 336c0ddf0ea740fb4363347c9a1b940bf7319c69

--- DaVinci/Phys/DaVinci/options/DaVinci.py
+++ DaVinci/Phys/DaVinci/options/DaVinci.py
@@ -1,8 +1,81 @@
-# Minimal file for running DaVinci from python prompt
-# Syntax is:
-#   gaudirun.py ../options/DaVinci.py
+########################################################################
 #
+# Options for a typical DaVinci job
+#
+# @author Patrick Koppenburg
+# @date 2008-08-06
+#
+########################################################################
+from os import environ
 from Gaudi.Configuration import *
-
-importOptions('$DAVINCIROOT/options/DaVinci.opts')
+from Configurables import GaudiSequencer, PrintTree, PhysDesktop, BTagging
+########################################################################
+#
+# Standard configuration
+#
+# @todo : Replace by a python Configuration, like in Brunel
+#
+importOptions( "$DAVINCIROOT/options/DaVinciCommon.opts" )
+########################################################################
+#
+# Trigger. Uncomment what you need. Hlt1 needs L0, Hlt2 doesn't.
+#
+# @todo load a file from L0DU, not HLTSYS
+#
+importOptions( "$HLTSYSROOT/options/L0.opts" )
+importOptions( "$HLTSYSROOT/options/Hlt1.opts" )
+importOptions( "$HLTSYSROOT/options/Hlt2.opts" )
+########################################################################
+#
+# enable standard particles monitors
+#
+importOptions( "$COMMONPARTICLESROOT/options/EnableStandardParticleMonitors.opts" )
+########################################################################
+#
+# Run some DC06 preselection
+#
+importOptions( "$B2DILEPTONROOT/options/DoPreselBu2LLK.opts" )
+########################################################################
+#
+# Example of running some algorithms
+#
+ApplicationMgr().TopAlg += [ GaudiSequencer("ExampleSeq") ]
+tree = PrintTree("PrintBu2LLK")
+GaudiSequencer("ExampleSeq").Members += [ tree ]
+tree.addTool( PhysDesktop() )
+tree.PhysDesktop.InputLocations = [ "Phys/PreselBu2LLK" ]
+########################################################################
+#
+# Flavour tagging. Don't ask why you'd be tagging a B+...
+#
+# Uncommenting this does not work. Why ?
+#
+# importOptions( "$FLAVOURTAGGINGOPTS/BTagging.opts" )
+# tag = BTagging()
+# tag.addTool( PhysDesktop() ) 
+# tag.PhysDesktop.InputLocations = [ "Phys/PreselBu2LLK" ]
+########################################################################
+#
+# Histogram File
+#
+HistogramPersistencySvc().OutputFile = "DVHistos_1.root"
+########################################################################
+#
+# nTuple File
+#
+ApplicationMgr().ExtSvc += [ "NTupleSvc" ]
+NTupleSvc().Output=["FILE1 DATAFILE='DVNtuples.root' TYP='ROOT' OPT='NEW'"]
+########################################################################
+#
+# Event numbers
+#
+ApplicationMgr().EvtMax = 1000 
+EventSelector().FirstEvent = 1
+EventSelector().PrintFreq  = 100
+########################################################################
+#
+# Some test data
+#
+importOptions( "$DAVINCIROOT/options/DaVinciTestData.opts" )
+########################################################################
 
