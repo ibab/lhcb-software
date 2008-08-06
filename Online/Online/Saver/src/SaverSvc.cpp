dfe238f92130415de64699d9dcd0e7f0527e3d0d
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2008-08-06 15:37:37 +0000

    Bug fix.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98234 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 59865990b9ea9028c34a0135de6c0966a4162b0f

--- Online/Online/Saver/src/SaverSvc.cpp
+++ Online/Online/Saver/src/SaverSvc.cpp
@@ -119,24 +119,9 @@ StatusCode SaverSvc::initialize() {
 
   m_processMgr->createTimerProcess(m_refreshTime);
 
-  msg << MSG::DEBUG << "Choosing Server to get ServicesSet.........." << endreq;
-  while (true) {
-    m_processMgr->dimInfoServers()->chooseServer();
-    if ("" != m_processMgr->dimInfoServers()->serverChoosed()) {
-      msg << MSG::DEBUG << "Server Choosed = " << m_processMgr->dimInfoServers()->serverChoosed() << endreq;
-      break;
-    }
-  }
-  
-  msg << MSG::DEBUG << "Before get chooseServer............." << endreq;
-  std::string serverChoosed = m_processMgr->dimInfoServers()->serverChoosed();
-  
-  msg << MSG::DEBUG << "Befor createInfoServices............." << endreq;
-  m_processMgr->createInfoServices(serverChoosed);  
-  
   m_file="Waiting for command to save histograms............."; 
   m_fileSize=0;
-  
+
   sc = service("MonitorSvc",m_monitorSvc,true);
   if ( !sc.isSuccess() )  {
     msg << MSG::ERROR << "Cannot access monitoring service." << endmsg;
@@ -144,14 +129,7 @@ StatusCode SaverSvc::initialize() {
   }
   declareInfo("file",m_file,"Filename of latest saveset");
   declareInfo("filesize",m_fileSize,"Filesize of latest saveset");
-  
-  //msg << MSG::DEBUG << "Activing PostEvent to CreateServiceMap............." << endreq;
-  //IocSensor::instance().send(this, s_createServiceMap, this);
-  //m_processMgr->createServiceMap();
-  //m_processMgr->serviceMap()->printMap();
-  //sleep(10);
-  //msg << MSG::DEBUG << "Befor startTimer we sleep 10 seconds............." << endreq;
-  
+
   msg << MSG::DEBUG << "Activing PostEvent to StartTimer............." << endreq;
   IocSensor::instance().send(this, s_startTimer, this); //start Timer*/
   
@@ -161,34 +139,49 @@ StatusCode SaverSvc::initialize() {
 
 void SaverSvc::handle(const Event&  ev) {
   MsgStream msg(msgSvc(), name());
-  
+
   if (s_saveHistos == ev.type) {
     save().ignore();
   }
-  else if(s_updateServiceMap == ev.type) {
-    msg << MSG::INFO << " We are inside a PostEvent to UpdateServiceMap " << endreq;
-    std::map<std::string, bool, std::less<std::string> >* serverMap = (std::map<std::string, bool, std::less<std::string> >* ) ev.data;
-    m_processMgr->serviceMap()->updateMap(*serverMap);
-    m_processMgr->serviceMap()->printMap();
-  }
-  else if(s_createServiceMap == ev.type) {
-    msg << MSG::INFO << " We are inside a PostEvent to CreateServiceMap " << endreq;
-/*    msg << MSG::DEBUG << "Before createServiceMap we sleep 10 seconds. We do it because it's necessary to give " << endreq;
-    msg << MSG::DEBUG << "some time to the thread in DimInfoServices to create the ServiceSet." << endreq;
-    sleep(10);*/
-    msg << MSG::DEBUG << "*************************************************************************"<< endreq;
-    msg << MSG::DEBUG << "******************************we CreateServiceMap************************"<< endreq;
-    m_processMgr->createServiceMap();
-    m_processMgr->serviceMap()->printMap();
-    msg << MSG::DEBUG << "*************The process to CreateServiceMap is finished*****************"<< endreq;
-    msg << MSG::DEBUG << "*************************************************************************"<< endreq;
-  }
   else if(s_startTimer == ev.type) {
     msg << MSG::INFO << " We are inside a PostEvent to Start the Timer " << endreq;
-    msg << MSG::DEBUG << "Before start timer ............." << endreq;
     m_processMgr->dimTimerProcess()->start(m_refreshTime);
+    msg << MSG::INFO << " End PostEvent to Start the Timer " << endreq;
   }
-  
+  else if(s_createInfoServices == ev.type ){
+    msg << MSG::INFO << " We are inside a PostEvent to Create the DimInfoServices " << endreq;
+    msg << MSG::DEBUG << "Choosing Server to get ServicesSet.........." << endreq;
+
+    std::string serverChoosed;
+    while (true) {
+      m_processMgr->dimInfoServers()->chooseServer();
+      serverChoosed = m_processMgr->dimInfoServers()->serverChoosed();
+      if ("" != serverChoosed) {
+        msg << MSG::DEBUG << "Server Choosed = " << m_processMgr->dimInfoServers()->serverChoosed() << endreq;
+        break;
+      }
+    }
+    msg << MSG::DEBUG << "Before createInfoServices............." << endreq;
+    m_processMgr->createInfoServices(serverChoosed);
+
+   // IocSensor::instance().send(this, s_updateServiceMap, ev.data); //start Timer*/
+    msg << MSG::INFO << " End PostEvent to Create the DimInfoServices " << endreq;
+  }
+  else if(s_updateSvcMapFromInfoServer == ev.type) {
+    msg << MSG::INFO << " We are inside a PostEvent to UpdateServiceMapFromInfoServer " << endreq;
+    std::map<std::string, bool, std::less<std::string> >* serverMap = (std::map<std::string, bool, std::less<std::string> >*) ev.data;
+    m_processMgr->serviceMap()->updateMap(*serverMap);
+    m_processMgr->serviceMap()->printMap();
+    msg << MSG::INFO << " End PostEvent to UpdateServiceMap " << endreq;
+  }
+  else if(s_updateSvcMapFromInfoService == ev.type) {
+    msg << MSG::INFO << " We are inside a PostEvent to UpdateServiceMapFromInfoService " << endreq;
+    std::set<std::string>* serviceSet = (std::set<std::string>*) ev.data;
+    m_processMgr->serviceMap()->setServiceSet(*serviceSet);
+    m_processMgr->updateMap();
+    m_processMgr->serviceMap()->printMap();
+    msg << MSG::INFO << " End PostEvent to UpdateServiceMapFromInfoService " << endreq;
+  }
 }
 
 //------------------------------------------------------------------------------
