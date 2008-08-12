8ca413abde63b5e1954cf8f44361fd1d857553c9
Giacomo Graziani <ggiacomo@cern.ch>
2008-08-12 10:41:35 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98280 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c1a77e9b56d494b8716224a968d8fe200ddc88d5

--- Online/Online/OnlineHistDB/main/ChangeAlgorithmName.cpp
+++ Online/Online/OnlineHistDB/main/ChangeAlgorithmName.cpp
@@ -1,4 +1,4 @@
-// $Id: ChangeAlgorithmName.cpp,v 1.1 2008-08-12 10:37:31 ggiacomo Exp $
+// $Id: ChangeAlgorithmName.cpp,v 1.2 2008-08-12 10:41:35 ggiacomo Exp $
 #include <iostream>
 #include <OnlineHistDB/OnlineHistDB.h>
 using namespace std;
@@ -21,8 +21,8 @@ int main(int argc, char* argv[]) {
   cout << "Enter the HIST_WRITER account's password: ";
   cin >> passwd;
   cout<<endl;
-  //  OnlineHistDB *HistDB = new OnlineHistDB(passwd,"HIST_WRITER","HISTDB");
-  OnlineHistDB *HistDB = new OnlineHistDB(passwd,"LHCB_MON_GIACOMO","devdb10");
+  OnlineHistDB *HistDB = new OnlineHistDB(passwd,"HIST_WRITER","HISTDB");
+  // OnlineHistDB *HistDB = new OnlineHistDB(passwd,"LHCB_MON_GIACOMO","devdb10");
   OnlineHistTask* task= HistDB->getTask(taskName);
   if(! task) {
     cout << "Task "<<taskName<<" does not exist!"<<endl;
