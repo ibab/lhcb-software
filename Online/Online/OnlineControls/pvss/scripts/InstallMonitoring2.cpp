bff7581e83aa1a479a3070d2ccd3398c4cd3c569
Markus Frank <frankb@cern.ch>
2008-08-13 16:50:21 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98304 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b5a28cc207db8bb495b0c13b555c8a30c8dd72ed

--- Online/Online/OnlineControls/pvss/scripts/InstallMonitoring2.cpp
+++ Online/Online/OnlineControls/pvss/scripts/InstallMonitoring2.cpp
@@ -2,11 +2,31 @@
 #uses "StreamTaskMgr.cpp"
 
 main()   {
+  int num_relay_tasks = 45;
+  int num_sender_tasks = 10;
+  int num_worker_tasks = 20;
   string stream = "Monitoring";
-  string ctrl_node = "mona08";
+  string ctrl_node   = strtoupper(getHostname());
+  string storage_sys = "STORAGE:Storage";
   ctrlUtils_stopAllTree();
-  StreamTaskMgr_installStream(stream);
-  StreamTaskMgr_createAllTree(stream,45,20,"STORAGE:Storage",10);  
+  if ( ctrl_node == "MONA06" ) {
+    num_worker_tasks = 15;
+    stream = "Monitoring2";
+    StreamTaskMgr_installStream(stream);
+    StreamTaskMgr_createAllTree(stream,num_relay_tasks,num_worker_tasks,storage_sys,num_sender_tasks);  
+  }
+  else if ( ctrl_node == "MONA09" ) {
+    num_worker_tasks = 8;
+    stream = "Reconstruction";
+    StreamTaskMgr_installStream(stream);
+    StreamTaskMgr_createTree(stream,"Slice00",num_relay_tasks,num_worker_tasks,storage_sys,num_sender_tasks);
+    StreamTaskMgr_createTree(stream,"Slice01",num_relay_tasks,num_worker_tasks,storage_sys,num_sender_tasks);
+    StreamTaskMgr_createTree(stream,"Slice02",num_relay_tasks,num_worker_tasks,storage_sys,num_sender_tasks);
+  }
+  else {
+    StreamTaskMgr_installStream(stream);
+    StreamTaskMgr_createAllTree(stream,num_relay_tasks,num_worker_tasks,storage_sys,num_sender_tasks);  
+  }
   StreamTaskMgr_connectTaskManager(stream);
   // Finally generate all FSMs
   ctrlUtils_genAllFSM();
