1ad99a164e6b476ef393f352e754e7db328e0553
Markus Frank <frankb@cern.ch>
2008-08-13 15:28:17 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98301 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 028d816803ac5144f075be495c8a41964d86ec0f

--- Online/Online/OnlineControls/python/PVSS00Monitoring.py
+++ Online/Online/OnlineControls/python/PVSS00Monitoring.py
@@ -1,5 +1,6 @@
 import traceback
 import Online.Streaming
+import Online.SetupParams as Params
 
 def run():
   """
@@ -10,9 +11,10 @@ def run():
   @author M.Frank
   """
   try:
-    Online.Streaming.execute('-type Monitoring -wait'.split(' '))
+    cmd = '-name '+Params.monitor_type+' -type Monitoring -wait'
+    Online.Streaming.execute(cmd.split(' '))
   except Exception,X:
-    print 'Failed to start Monitoring streaming device.'
+    print 'Failed to start Monitoring streaming device for system:',Params.monitor_system_name,Params.monitor_type
     traceback.print_exc()
 
 if __name__ == "__main__":
