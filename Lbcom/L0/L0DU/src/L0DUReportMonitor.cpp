03e3070e6d2cf4866ff8cef2a3d9d0f3f1fccf74
Olivier Deschamps <odescham@cern.ch>
2008-08-14 09:33:41 +0000

    improve printout
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@59846 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ba0239f4c03f8a291844285a9d101128dce9b7f2

--- Lbcom/L0/L0DU/src/L0DUReportMonitor.cpp
+++ Lbcom/L0/L0DU/src/L0DUReportMonitor.cpp
@@ -1,4 +1,4 @@
-// $Id: L0DUReportMonitor.cpp,v 1.14 2008-08-14 09:30:44 odescham Exp $
+// $Id: L0DUReportMonitor.cpp,v 1.15 2008-08-14 09:33:41 odescham Exp $
 // Include files 
 #include <cmath>
 // from Gaudi
@@ -131,7 +131,7 @@ StatusCode L0DUReportMonitor::execute() {
     }
     if(m_prevTCK != -1){
       if( !m_split){
-        Warning("New configuration tck found : reset all histos",StatusCode::SUCCES).ignore();
+        Warning("New configuration tck found : reset all histos",StatusCode::SUCCESS).ignore();
         debug() << "New configuration : " << tck << " (previous was : " << m_prevTCK << ")" <<endreq;
         debug() << "Reset all histograms (number of histos : " << totalNumberOfHistos() << ")" << endreq;
         // reset all 1D histos
