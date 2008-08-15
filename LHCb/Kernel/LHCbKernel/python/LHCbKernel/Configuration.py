a3f3198faa395d6e7c7c94f801064e38a418f4d7
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-08-15 14:14:23 +0000

    ! 2008-08-15 - Chris Jones  - Add python/LHCbKernel/Configurable.py containing base class Configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47992 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 25370bd51def5cd9554673d6e4b29a6391bcdc14

---
+++ LHCb/Kernel/LHCbKernel/python/LHCbKernel/Configuration.py
@@ -0,0 +1,52 @@
+
+## @package LHCbKernel
+#  Base configurables for the LHCb software
+#  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
+#  @date   15/08/2008
+
+__version__ = "$Id: Configuration.py,v 1.1 2008-08-15 14:14:23 jonrob Exp $"
+__author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
+
+from Gaudi.Configuration import *
+
+# ----------------------------------------------------------------------------------
+
+## @class LHCbConfigurableUser
+#  Base configurable for LHCb software
+#  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
+#  @date   15/08/2008
+class LHCbConfigurableUser(ConfigurableUser):
+
+    ## @brief Returns the value of the given property
+    #  @param name The property name
+    #  @return The value of property 'name'
+    def getProp(self,name):
+        if hasattr(self,name):
+            return getattr(self,name)
+        else:
+            return self.getDefaultProperties()[name]
+
+    ## @brief Set the value of a given property
+    #  @param name The name of the property
+    def setProp(self,name,value):
+        return setattr(self,name,value)
+
+    ## @brief Set the given property in another configurable object
+    #  @param other The other configurable to set the property for
+    #  @param name  The property name
+    def setOtherProp(self,other,name):
+        # Function to propagate properties to other component, if not already set
+        if hasattr(self,name):
+            if hasattr(other,name) and len(other.getProp(name)) > 0 :
+                print "# %s().%s already defined, ignoring %s().%s"%(other.name(),name,
+                                                                     self.name(), name )
+            else:
+                other.setProp(name,self.getProp(name))
+
+    ## @brief Set the given properties in another configurable object
+    #  @param other The other configurable to set the property for
+    #  @param name  The property names
+    def setOtherProps(self,other,names):
+        for name in names:
+            self.setOtherProp(other,name)
+            
