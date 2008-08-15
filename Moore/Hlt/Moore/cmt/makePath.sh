3e705b5668a8a0b6eb4f86ebb258d2581b5286ee
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-08-15 08:50:55 +0000

    add file to generate online environment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@72989 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8105fce51568c6f822aedeadbde9272016bb646c

---
+++ Moore/Hlt/Moore/cmt/makePath.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#  create the pathsetup and pathsetup_dbg files in ../job
+
+#setup online environment
+export MYSITEROOT=/sw/lib
+export LHCBHOME=/sw/lib
+. $MYSITEROOT/scripts/ExtCMT.sh
+#export CMTPROJECTPATH=$LHCBPROJECTPATH
+#export CMTPROJECTPATH=/group/hlt/cmtuser:/home/online:/sw/lib/lhcb:/sw/lib/lcg/external
+export CMTOPT=slc4_amd64_gcc34
+
+echo "Create pathsetup using config '$CMTOPT'"
+export CMTCONFIG=$CMTOPT
+. ./setup.sh
+${CMTROOT}/mgr/cmt setup -sh > ../job/pathsetup
+
+echo "Create pathsetup_dbg using config '$CMTDEB'"
+export CMTCONFIG=$CMTDEB
+. ./setup.sh
+${CMTROOT}/mgr/cmt setup -sh > ../job/pathsetup_dbg
+
+export CMTCONFIG=$CMTOPT
+
