196af2c5830bd3536d4a84900652131e8b5b6d9f
Matthew David Needham <mneedham@cern.ch>
2008-08-17 09:06:39 +0000

    Extend IT and TT name classes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@47994 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e34ad0be2c26aea48cd44401e2f3cbf6793d87e3

--- LHCb/Kernel/LHCbKernel/src/ITNames.cpp
+++ LHCb/Kernel/LHCbKernel/src/ITNames.cpp
@@ -6,3 +6,81 @@ std::string LHCb::ITNames::UniqueSectorToString(const LHCb::STChannelID& chan) {
   std::string theString = UniqueLayerToString(chan) + "Sector" + boost::lexical_cast<std::string>(chan.sector());
   return theString;
 }
+
+std::vector<std::string> LHCb::ITNames::stations() {
+
+  GaudiUtils::VectorMap<std::string,Station>::iterator iter = s_StationTypMap().begin();
+  std::vector<std::string> stations; stations.reserve(s_StationTypMap().size());
+  for ( ;iter != s_StationTypMap().end(); ++iter ){
+    if (iter->first != "UnknownStation" ) stations.push_back(iter->first);
+  } // iter
+
+  return stations;
+}
+
+
+std::vector<std::string> LHCb::ITNames::allStations() {
+
+  return stations();
+}
+
+
+std::vector<std::string> LHCb::ITNames::allBoxes() {
+
+  typedef std::vector<std::string> Strings;
+  Strings stationVec = stations();
+  Strings boxVec = boxes(); 
+  Strings tVector; tVector.reserve(boxVec.size()*stationVec.size());
+  for (Strings::iterator iterS = stationVec.begin(); iterS != stationVec.end(); ++iterS){
+     for (Strings::iterator iterB = boxVec.begin(); iterB != boxVec.end(); ++iterB){
+       std::string temp = (*iterS) + (*iterB) ;
+       tVector.push_back(temp); 
+     } // iterB
+  } // iterS
+
+  return tVector;
+}
+
+std::vector<std::string> LHCb::ITNames::allLayers() {
+
+  typedef std::vector<std::string> Strings;
+  Strings layerVec = layers();
+  Strings boxVec = allBoxes(); 
+  Strings tVector; tVector.reserve(boxVec.size()*layerVec.size());
+  for (Strings::iterator iterB = boxVec.begin(); iterB != boxVec.end(); ++iterB){
+     for (Strings::iterator iterL = layerVec.begin(); iterL != layerVec.end(); ++iterL){
+       std::string temp = (*iterB) + (*iterL) ;
+       tVector.push_back(temp); 
+     } // iterB
+  } // iterS
+
+  return tVector;
+}
+
+
+std::vector<std::string> LHCb::ITNames::boxes() {
+
+  GaudiUtils::VectorMap<std::string,Box>::iterator iter = s_BoxTypMap().begin();
+  std::vector<std::string> boxes; boxes.reserve(s_BoxTypMap().size());
+  for ( ;iter != s_BoxTypMap().end(); ++iter ){
+    if (iter->first != "UnknownBox") boxes.push_back(iter->first);
+  } // iter
+
+  return boxes;
+}
+
+std::vector<std::string> LHCb::ITNames::layers() {
+
+  GaudiUtils::VectorMap<std::string,Layer>::iterator iter = s_LayerTypMap().begin();
+  std::vector<std::string> layers; layers.reserve(s_LayerTypMap().size());
+  for ( ;iter != s_LayerTypMap().end(); ++iter ){
+    if (iter->first != "UnknownLayer") layers.push_back(iter->first);
+  } // iter
+
+  return layers;
+}
+
+
+
+
+
--- LHCb/Kernel/LHCbKernel/src/TTNames.cpp
+++ LHCb/Kernel/LHCbKernel/src/TTNames.cpp
@@ -2,10 +2,70 @@
 #include "Kernel/STChannelID.h"
 #include "Kernel/TTNames.h"
 #include "boost/lexical_cast.hpp"
+#include <boost/assign/std/vector.hpp>
+#include <boost/assign/list_of.hpp>
 
 std::string LHCb::TTNames::UniqueSectorToString(const LHCb::STChannelID& chan) {
   std::string theString = UniqueRegionToString(chan) + "Sector" + boost::lexical_cast<std::string>(chan.sector());
   return theString;
+}
+
+std::vector<std::string> LHCb::TTNames::stations() {
+
+  GaudiUtils::VectorMap<std::string,Station>::iterator iter = s_StationTypMap().begin();
+  std::vector<std::string> stations; stations.reserve(s_StationTypMap().size());
+  for ( ;iter != s_StationTypMap().end(); ++iter ){
+    if (iter->first != "Unknownstation") stations.push_back(iter->first);
+  } // iter
+
+  return stations;
+}
+
+std::vector<std::string> LHCb::TTNames::detRegions() {
+
+  GaudiUtils::VectorMap<std::string,detRegion>::iterator iter = s_detRegionTypMap().begin();
+  std::vector<std::string> regions; regions.reserve(s_detRegionTypMap().size());
+  for ( ;iter != s_detRegionTypMap().end(); ++iter ){
+    if (iter->first != "UnknownRegion") regions.push_back(iter->first);
+  } // iter
+
+  return regions;
+}
+
+std::vector<std::string> LHCb::TTNames::layers() {
+  //messy
+  std::vector<std::string> layers = boost::assign::list_of("X")("U")("V");
+  return layers;
+}
+
+std::vector<std::string> LHCb::TTNames::allStations() {
+
+  return stations();
+}
+
+std::vector<std::string> LHCb::TTNames::allDetRegions() {
+
+  typedef std::vector<std::string> Strings;
+  Strings layers = allLayers();
+  Strings regions = detRegions(); 
+  Strings tVector; tVector.reserve(regions.size()*layers.size());
+  for (Strings::iterator iterL = layers.begin(); iterL != layers.end(); ++iterL){
+     for (Strings::iterator iterR = regions.begin(); iterR != regions.end(); ++iterR){
+       std::string temp = (*iterL) + (*iterR) ;
+       tVector.push_back(temp); 
+     } // iterB
+  } // iterS
+
+  return tVector;
+}
+
+std::vector<std::string> LHCb::TTNames::allLayers() {
+
+  typedef std::vector<std::string> Strings;
+  Strings stationVec = stations(); 
+   std::vector<std::string> layers = boost::assign::list_of(stationVec[0]+"X")(stationVec[0]+"U")
+                                    (stationVec[1]+"V")(stationVec[1]+"X");
+  return layers;
 }
 
 
