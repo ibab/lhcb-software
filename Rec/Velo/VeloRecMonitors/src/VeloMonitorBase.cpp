b0115461eb8b4579031614176340dd6e4f3a03e6
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2008-08-18 10:17:53 +0000

    First import of Velo/VeloRecMonitors
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@24571 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 222fe1529cfef53f004c4d6d87d2e36cb9d21eff

---
+++ Rec/Velo/VeloRecMonitors/src/VeloMonitorBase.cpp
@@ -0,0 +1,76 @@
+// $Id: VeloMonitorBase.cpp,v 1.1.1.1 2008-08-18 10:17:53 erodrigu Exp $
+// Include files 
+// -------------
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+// VeloDet
+#include "VeloDet/DeVelo.h"
+
+// local
+#include "VeloMonitorBase.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : VeloMonitorBase
+//
+// 2008-08-15 : Eduardo Rodrigues
+//-----------------------------------------------------------------------------
+
+namespace Velo 
+{
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+VeloMonitorBase::VeloMonitorBase( const std::string& name,
+                                  ISvcLocator* pSvcLocator)
+  : GaudiHistoAlg ( name , pSvcLocator )
+    , m_veloDet ( 0 )
+{
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+VeloMonitorBase::~VeloMonitorBase() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode VeloMonitorBase::initialize() {
+  StatusCode sc = GaudiHistoAlg::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiHistoAlg
+
+  m_debugLevel = msgLevel( MSG::DEBUG );
+
+  if ( m_debugLevel ) debug() << "==> Initialize" << endmsg;
+
+  setHistoTopDir( "Velo/" );
+
+  m_veloDet = getDet<DeVelo>( DeVeloLocation::Default );
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode VeloMonitorBase::execute() {
+
+  if ( m_debugLevel ) debug() << "==> Execute" << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode VeloMonitorBase::finalize() {
+
+  if ( m_debugLevel ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiHistoAlg::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
+} // end of Velo namespace
---
+++ Rec/Velo/VeloRecMonitors/src/VeloMonitorBase.h
@@ -0,0 +1,52 @@
+// $Id: VeloMonitorBase.h,v 1.1.1.1 2008-08-18 10:17:53 erodrigu Exp $
+#ifndef VELORECMONITORS_VELOMONITORBASE_H 
+#define VELORECMONITORS_VELOMONITORBASE_H 1
+
+// Include files
+// -------------
+// from Gaudi
+#include "GaudiAlg/GaudiHistoAlg.h"
+
+// VeloDet
+#include "VeloDet/DeVelo.h"
+
+/** @class VeloMonitorBase VeloMonitorBase.h Velo/VeloMonitorBase.h
+ *  
+ *  Header file for the VELO reconstruction monitoring
+ *  algorithm base class
+ *
+ *  @author Eduardo Rodrigues
+ *  @date   2008-08-15
+ */
+
+namespace Velo
+{
+
+  class VeloMonitorBase : public GaudiHistoAlg {
+
+  public: 
+
+    /// Standard constructor
+    VeloMonitorBase( const std::string& name, ISvcLocator* pSvcLocator );
+    
+    virtual ~VeloMonitorBase( ); ///< Destructor
+    
+    virtual StatusCode initialize();    ///< Algorithm initialization
+    virtual StatusCode execute   ();    ///< Algorithm execution
+    virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+  protected:
+
+    // pointer to the VELO detector element
+    DeVelo* m_veloDet;
+
+    // flag for DEBUG level
+    bool m_debugLevel;
+
+  private:
+
+  };
+
+} // end of Velo namespace
+
+#endif // VELORECMONITORS_VELOMONITORBASE_H
