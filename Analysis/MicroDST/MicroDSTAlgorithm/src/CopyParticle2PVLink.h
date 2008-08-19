0a4aabfceab6439ceff7ef3a9dbb4f2bb6b15209
jpalac <jpalac@cern.ch>
2008-08-19 19:33:16 +0000

    . Remove fragile       const std::string particleLocation(const std::string& tableLocation);     method   . Correct errors in doxygen documentation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@73011 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7e8963f787071ee019373d8e360178133610c336

--- Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2PVLink.h
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2PVLink.h
@@ -1,4 +1,4 @@
-// $Id: CopyParticle2PVLink.h,v 1.5 2008-03-20 14:00:06 jpalac Exp $
+// $Id: CopyParticle2PVLink.h,v 1.6 2008-08-19 19:33:16 jpalac Exp $
 #ifndef COPYPARTICLE2PVLINK_H 
 #define COPYPARTICLE2PVLINK_H 1
 
@@ -16,7 +16,7 @@
  *  a candidate LHCb::Particle and any primary vertices used to select
  *  it. 
  *  It inherits the std::string properties InputLocation and OutputPrefix from
- *  MicroDSTCommon. The LHCb::Particles are taken from the TES location 
+ *  MicroDSTCommon. The associations are taken from the TES location 
  *  defined by InputLocation, and are cloned and put in TES location 
  *  "/Event" + OutputPrefix + InputLocation. If InputLocation already contains
  *  a leading "/Event" it is removed.
@@ -24,19 +24,20 @@
  *  location like "/Event/Phys/SomeParticleMakingAlg/Particle2VertexRelations".
  *  The algorithm picks up this relations table, which relates LHCb::Particles
  *  from "/Event/Phys/SomaParticleMakingAlg/Particles" to LHCb::RecVertex.
- *  For each LHCb::Particle to LHCb::RecVertex associaiton, it picks up the 
- *  corresponding <b>previously cloned</b> LHCb::Particle from 
- *  "/Event/MyLocation/Phys/SomeParticleMakerAlg/Particles" and LHCb::RecVertex
- *  from "/Event/MyLocation/Phys/<location of associated vertices>".
+ *  For each LHCb::Particle to LHCb::RecVertex association, it picks up the 
+ *  corresponding <b>previously cloned</b> LHCb::Particle and LHCb::RecVertex 
+ *  from "/Event/MyLocation/Phys/SomeParticleMakerAlg/Particles" and
+ *  "/Event/MyLocation/Phys/<location of associated vertices>" respectively.
+ *  If these have not been previously cloned, no association is cloned or 
+ *  stored. This algorithm <b>is not resopnsible for cloning the associated
+ *  TES objects</b>
  *
  *  <b>Important conditions</b>: This algorithm relies on the LHCb::Particle
  *  and LHCb::RecVertex sets in the associations to have been previously
  *  cloned by CloneParticles and ClonePrimaryVertices. Furthermore, the
  *  <b>OutputPrefix</b> property of this algorithm and the CopyParticles and
  *  CopyPrimaryVertices used must be the same.
- *  of this algorithm. At the moment, the filling of the associations is only
- *  performed if the LHCb:: Particle creation and selection involves the
- *  primary vertex.
+ *  of this algorithm.
  *
  *  <b>Example</b>: Clone LHCb::Particle -> LHCb::recVertex associations
  *  from "/Event/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particle2VertexRelations"
@@ -67,8 +68,6 @@ protected:
 private:
 
   Particle2Vertex::Table* tableOfClones(const Particle2Vertex::Table* table);
-
-  const std::string particleLocation(const std::string& tableLocation);
   
 };
 #endif // COPYPARTICLE2PVLINK_H
