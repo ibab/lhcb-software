80a1aaca2865d6e698043ed17c41638c1ad351d2
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-08-26 19:43:17 +0000

    ! 2008-08-26 - Chris Jones  - Add python configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@9968 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 676eb54836f9ccb169a9df3f1cf3ad98e0e09d40

---
+++ Rec/Rich/RichRecQC/python/RichRecQC/Configuration.py
@@ -0,0 +1,155 @@
+
+## @package RichRecQC
+#  High level Configuration tools for RICH Data Quality monitoring
+#  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
+#  @date   15/08/2008
+
+__version__ = "$Id: Configuration.py,v 1.1 2008-08-26 19:43:17 jonrob Exp $"
+__author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
+
+from RichKernel.Configuration import *
+from Configurables import ( GaudiSequencer, MessageSvc )
+    
+# ----------------------------------------------------------------------------------
+
+## @class RichRecQCConf
+#  High level Configuration tools for RICH Data Quality monitoring
+#  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
+#  @date   15/08/2008
+class RichRecQCConf(RichConfigurableUser):
+
+    ## Steering options
+    __slots__ = {
+        "context": "Offline"  # The context within which to run
+       ,"pidMonitoring": True
+       ,"pixelMonitoring": True
+       ,"photonMonitoring": True
+       ,"tracklessRingMonitoring": False
+       ,"pidMomentumRanges": [ [2,100], [2,10], [10,70], [70,100] ]
+       ,"pidTrackTypes":  [ ["All"] ]
+       ,"recoTrackTypes": [ ["All"],["Forward"],["Match"],["KsTrack"],["VeloTT"],["Seed"] ]
+        }
+
+    ## Apply the configuration to the given sequence
+    def applyConf(self,sequence):
+
+        # Set context
+        sequence.Context = self.getProp("context")
+
+        # Suppress errors from Linker
+        MessageSvc().setFatal += [ "LinkedTo::MC/Rich/Hits2MCRichOpticalPhotons",
+                                   "LinkedTo::MC/Rich/MCPartsToMCRichTracks",
+                                   "LinkedTo::MC/Rich/MCRichHitsToOpPhotons" ]
+
+        # RICH data monitoring
+        if self.getProp("pixelMonitoring") :
+            pixSeq = GaudiSequencer("RichPixelMoni")
+            pixSeq.MeasureTime = True
+            sequence.Members += [pixSeq]
+            self.pixelPerf(pixSeq)
+         
+        # Reconstruction monitoring
+        if self.getProp("photonMonitoring") :
+            recSeq = GaudiSequencer("RichRecoMoni")
+            recSeq.MeasureTime = True
+            sequence.Members += [recSeq]
+            self.recPerf(recSeq)
+
+        # PID Performance
+        if self.getProp("pidMonitoring") :
+            pidPerf = GaudiSequencer("RichPIDMoni")
+            pidPerf.MeasureTime = True
+            sequence.Members += [pidPerf]
+            self.pidPerf(pidPerf)
+
+        # Trackless rings
+        if self.getProp("tracklessRingMonitoring") :
+            ringsMoni = GaudiSequencer("RichTracklessRingsMoni")
+            ringsMoni.MeasureTime = True
+            sequence.Members += [ringsMoni]
+            self.ringsMoni(ringsMoni)
+
+    ## standalone ring finder monitors
+    def ringsMoni(self,sequence):
+        from RichMarkovRingFinder.Configuration import RichMarkovRingFinderConf
+        from Configurables import Rich__Rec__MarkovRingFinder__MC__Moni
+        
+        conf = RichMarkovRingFinderConf()
+
+        # Activate histos in the finder algs themselves
+        conf.rich1TopFinder().HistoProduce    = True
+        conf.rich1BottomFinder().HistoProduce = True
+        conf.rich2LeftFinder().HistoProduce   = True
+        conf.rich2RightFinder().HistoProduce  = True
+
+        # Add monitors
+        allMoni  = Rich__Rec__MarkovRingFinder__MC__Moni("MarkovRingMoniAll")
+        sequence.Members += [allMoni]
+        bestMoni = Rich__Rec__MarkovRingFinder__MC__Moni("MarkovRingMoniBest")
+        bestMoni.RingLocation = "Rec/Rich/Markov/RingsBest"
+        sequence.Members += [bestMoni]
+        isoMoni = Rich__Rec__MarkovRingFinder__MC__Moni("MarkovRingMoniIsolated")
+        isoMoni.RingLocation = "Rec/Rich/Markov/RingsIsolated"
+        sequence.Members += [isoMoni]
+        
+    ## Pixel performance monitors
+    def pixelPerf(self,sequence):
+        
+        from Configurables import ( Rich__Rec__MC__PixelQC )
+
+        sequence.Members += [ Rich__Rec__MC__PixelQC("RichRecPixelQC") ]
+        
+    ## Run the PID Performance monitors
+    def pidPerf(self,sequence):
+
+        from Configurables import ( Rich__Rec__MC__PIDQC )
+
+        # Track Types
+        for trackType in self.getProp("pidTrackTypes") :
+
+            # Momentum ranges
+            for pRange in self.getProp("pidMomentumRanges") :
+
+                # Construct the name for this monitor out of the track types
+                # and momentum range
+                tkName = ""
+                for tkT in trackType : tkName += tkT
+                name = "RiPIDMon" + tkName + `pRange[0]` + "To" + `pRange[1]`
+
+                # Make a monitor alg
+                pidMon = Rich__Rec__MC__PIDQC(name)
+
+                # Trackselector
+                tkSel = RichTools().trackSelector(private=True)
+                tkSel.MinPCut = pRange[0]
+                tkSel.MaxPCut = pRange[1]
+                if trackType != ["All"] : tkSel.TrackAlgs = trackType
+                pidMon.addTool( tkSel )
+
+                # Add to sequence
+                sequence.Members += [pidMon]
+
+    ## Reconstruction performance
+    def recPerf(self,sequence):
+
+        from Configurables import ( Rich__Rec__MC__RecoQC )
+                
+        # Track Types
+        for trackType in self.getProp("recoTrackTypes") : 
+
+            # Construct the name for this monitor
+            name = "RiCKRes"
+            for tkT in trackType : name += tkT
+
+            # Make a monitor alg
+            mon = Rich__Rec__MC__RecoQC(name)
+            mon.HistoPrint = False
+          
+            # Trackselector
+            tkSel = RichTools().trackSelector(private=True)
+            if trackType != ["All"] : tkSel.TrackAlgs = trackType
+            mon.addTool( tkSel )
+
+            # Add to sequence
+            sequence.Members += [mon]
+
