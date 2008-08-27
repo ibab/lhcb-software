e8511d676627d693b4e027a98b9b99b2a90ad188
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-08-27 16:34:05 +0000

    ! 2008-08-27 - Chris Jones  - Import tool interface header files from Rich/RichMonitoringTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@9998 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3c7b7e0ab16fcbbf503a0b284901982d3a7f58da

---
+++ LHCb/Rich/RichKernel/RichKernel/IHpdUkL1DisableTool.h
@@ -0,0 +1,45 @@
+#ifndef I_HPDUKL1DISABLETOOL_H 
+#define I_HPDUKL1DISABLETOOL 1
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+#include "GaudiKernel/IIncidentListener.h"
+
+// RICH
+#include <Kernel/RichSmartID.h>
+
+static const InterfaceID IID_IHpdUkL1DisableTool ( "Rich::Mon::IHpdUkL1DisableTool", 1, 0 );
+
+/** @class IHpdUkL1DisableTool IHpdUkL1DisableTool.h IHpdUkL1DisableTool.h
+ *   
+ *
+ *  @author Ulrich Kerzel
+ *  @date   2008-06-30
+ */
+
+namespace Rich
+{
+  namespace Mon
+  {
+
+    class IHpdUkL1DisableTool : virtual public IAlgTool,
+                                virtual public IIncidentListener   {
+    public: 
+      
+      // Return the interface ID
+      static const InterfaceID& interfaceID() { return IID_IHpdUkL1DisableTool; }
+      
+      virtual StatusCode initialize         ()                                 = 0;
+      virtual StatusCode finalize           ()                                 = 0; 
+      virtual void       SetDisableThreshold(int threshold = 10)               = 0;
+      virtual void       DisableHPD         (const LHCb::RichSmartID &smartID) = 0; 
+      virtual void       ClearHPDList       ()                                 = 0;
+
+                   
+    protected:
+      
+    private:
+    }; // class
+  } // namespace Mon
+} // namespace Rich
+#endif // ICAMERATOOL_H
