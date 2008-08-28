12992b513a95d6acecd90fd02ae389ec15871d55
Marcin Kucharczyk <marcin@cern.ch>
2008-08-28 14:34:46 +0000

    Writing highest pt pion to desktop
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@73121 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dd736b070e9510143270c7d5512fb4eb60c54793

--- Hlt/Hlt/HltHidValley/options/Hlt2SelHidValley.py
+++ Hlt/Hlt/HltHidValley/options/Hlt2SelHidValley.py
@@ -3,12 +3,12 @@
 #  Hlt2 Hidden Valley selection
 #
 #  @author M. Kucharczyk Marcin.Kucharczyk@cern.ch
-#  @date 2008-07-17
+#  @date 2008-08-21
 #
 ##
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer, DeterministicPrescaler
-from Configurables import PatPV3D, PVOfflineTool, Hlt2HidValley
+from Configurables import PatPV3D, PVOfflineTool, Hlt2HidValley, PhysDesktop
 ###########################################################################
 # Define the sequencer. It must be a member of Hlt2Selections.
 # There must be a Prescale algorithm defined as well. By default
@@ -25,8 +25,13 @@ Hlt2SelHidValley = Hlt2HidValley("Hlt2SelHidValley")
 GaudiSequencer("Hlt2SelHidValleyFilterSequence").Members += [Hlt2PatPV3D, Hlt2SelHidValley]
 Hlt2PatPV3D.addTool(PVOfflineTool())
 Hlt2PatPV3D.PVOfflineTool.InputTracks = ["Hlt/Track/Velo"]
+
+# DisplacedOff vertices
 Hlt2PatPV3D.PVOfflineTool.PVFitterName = "LSAdaptPV3DFitter"
 Hlt2PatPV3D.PVOfflineTool.PVSeedingName = "PVSeed3DTool"
-Hlt2PatPV3D.OutputVerticesName = "Rec/Vertices/myVrtcsOff"
+Hlt2PatPV3D.OutputVerticesName = "Rec/Vertices/displacedOffVertices"
+
+Hlt2SelHidValley.addTool(PhysDesktop())
+Hlt2SelHidValley.PhysDesktop.InputLocations = ["Phys/Hlt2Pions"]
 #
 
