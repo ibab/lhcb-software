f6774938cb3d8c4d9c4be1d3dfda205df2c2041d
Giacomo Graziani <ggiacomo@cern.ch>
2008-08-28 08:50:01 +0000

    release v4r11
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98340 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 379efbbe4f5eb5e9199cfb938c7885db579a473b

--- Online/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h
+++ Online/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h
@@ -1,4 +1,4 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h,v 1.28 2008-08-06 08:13:07 ggiacomo Exp $
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OnlineHistDB/OnlineHistDB/OnlineHistDBEnv.h,v 1.29 2008-08-28 08:50:01 ggiacomo Exp $
 #ifndef ONLINEHISTDBENV_H
 #define ONLINEHISTDBENV_H 1
 /** @class  OnlineHistDBEnv OnlineHistDBEnv.h OnlineHistDB/OnlineHistDBEnv.h
@@ -23,7 +23,7 @@ namespace OnlineHistDBEnv_constants {
   static const std::string ACCOUNT="HIST_READER";
   static const std::string PASSWORD="reader";
   static const unsigned int DBschema = 9;
-  static const std::string version = "v4r10";
+  static const std::string version = "v4r11";
 
   static const int NHTYPES=6;
   static const char HistTypeName[][4] = {
