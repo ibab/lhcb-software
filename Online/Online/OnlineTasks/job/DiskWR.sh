8ffb8e483c8fc3b3d82fdc5046140b2b59ce8a9e
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2008-08-29 08:50:06 +0000

    OT opts commented.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98372 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c19ccfd2b5b1331e8d764d70485b0af58638577f

--- Online/Online/OnlineTasks/job/DiskWR.sh
+++ Online/Online/OnlineTasks/job/DiskWR.sh
@@ -4,4 +4,6 @@ cd job
 export DEBUGGING=YES
 . ./setupOnline.sh $*
 
-exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class2Task -main=${HLTOPTS}/Main.opts -opts=${HLTOPTS}/DiskWR${IS_TAE_RUN}.opts
+exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class2Task -main=${HLTOPTS}/Main.opts -opts=${HLTOPTS}/DiskWR${IS_TAE_RUN}.opts;
+# to write files to local diskj on farm nodes:
+# exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class2Task -main=${HLTOPTS}/Main.opts -opts=/home/online/Online_v4r12/Online/OnlineTasks/v1r13/options/OT_DiskWR.opts
--- Online/Online/OnlineTasks/job/MEPInit.sh
+++ Online/Online/OnlineTasks/job/MEPInit.sh
@@ -4,5 +4,9 @@ cd job
 export DEBUGGING=YES
 
 . ./setupOnline.sh $*
+rm -rf /dev/shm/sem.bm_*
+rm -rf /dev/shm/sem.BM_*
+rm -rf /dev/shm/sem.MEP_*
+rm -rf /dev/shm/bm_*
 
 exec -a ${UTGID} ${CLASS0_TASK} -main=${HLTOPTS}/MEPInit${IS_TAE_RUN}.opts
