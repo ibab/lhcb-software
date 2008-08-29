719b7c11da66f7ddfcc14ef15b44cd94cccbf1d1
jpalac <jpalac@cern.ch>
2008-08-29 05:50:42 +0000

    . Templated class for the simplest object cloning MicroDSTAlgorithms.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@73125 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 87ffb2e542a4bc0a6bf0f7585b65266daecfe398

---
+++ Analysis/MicroDST/MicroDSTBase/MicroDST/ObjectClonerAlg.h
@@ -0,0 +1,73 @@
+// $Id: ObjectClonerAlg.h,v 1.1 2008-08-29 05:50:42 jpalac Exp $
+#ifndef MICRODST_MICRODSTOBJECTCLONERALG_H 
+#define MICRODST_MICRODSTOBJECTCLONERALG_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+// From MicroDST
+#include "MicroDST/MicroDSTAlgorithm.h"
+
+
+/** @class ObjectClonerAlg ObjectClonerAlg.h MicroDST/ObjectClonerAlg.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2008-08-27
+ */
+namespace MicroDST 
+{
+  
+  template <typename T>
+  class ObjectClonerAlg : public MicroDSTAlgorithm {
+  public: 
+    /// Standard constructor
+    ObjectClonerAlg( const std::string& name, ISvcLocator* pSvcLocator )
+      : MicroDSTAlgorithm ( name , pSvcLocator ) 
+    {
+    }
+
+    ~ObjectClonerAlg( ) {} ///< Destructor
+
+    StatusCode initialize() 
+    {
+      StatusCode sc = MicroDSTAlgorithm::initialize();
+
+      debug() << "==> Initialize" << endmsg;
+
+      if ( sc.isFailure() ) return sc;
+
+      if (inputTESLocation()=="")  {
+        error() << "No input TES location set" 
+                << endmsg;
+        return StatusCode::FAILURE;
+      }
+      verbose() << "inputTESLocation() is " << inputTESLocation() << endmsg;
+
+      return StatusCode::SUCCESS;
+    }
+    
+    StatusCode execute() 
+    {
+        debug() << "==> Execute" << endmsg;
+
+        setFilterPassed(true);
+  
+        verbose() << "Going to object from " << inputTESLocation()
+                  << " into " << fullOutputTESLocation() << endmsg;
+        return (0!=copyAndStoreObject<T,MicroDST::BasicCopy<T> >(inputTESLocation(),
+                                                                fullOutputTESLocation() ) ) ?
+          StatusCode::SUCCESS : StatusCode::FAILURE;
+    }
+    
+    StatusCode finalize  () 
+    {
+      debug() << "==> Finalize" << endmsg;
+      return MicroDSTAlgorithm::finalize();
+    }
+    
+  };
+
+}
+
+#endif // MICRODST_MICRODSTOBJECTCLONERALG_H
