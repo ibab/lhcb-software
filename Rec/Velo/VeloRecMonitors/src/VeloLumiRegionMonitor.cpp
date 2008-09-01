970cb24855ce8d25669ae0969b78e03ed05805f0
Malcolm John <malcolm.john@cern.ch>
2008-09-01 00:18:31 +0000

    Add a liteCluster monitor
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@24606 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ce6bb50eb46605fe394d17d1084c6c2921c0cd3

--- Rec/Velo/VeloRecMonitors/src/VeloLumiRegionMonitor.cpp
+++ Rec/Velo/VeloRecMonitors/src/VeloLumiRegionMonitor.cpp
@@ -1,4 +1,4 @@
-// $Id: VeloLumiRegionMonitor.cpp,v 1.1 2008-08-31 15:52:08 krinnert Exp $
+// $Id: VeloLumiRegionMonitor.cpp,v 1.2 2008-09-01 00:18:31 mjohn Exp $
 // Include files 
 // -------------
 // from Gaudi
@@ -53,7 +53,7 @@ StatusCode Velo::VeloLumiRegionMonitor::initialize() {
   if (0==rndm) {
     return Error ("Unable to initialize IRndmGenSvc", StatusCode::FAILURE);  
   }
-  sc = m_rndm.initialize  ( rndm , Rndm::Gauss ( 29 , 0.1 ) ) ;
+  sc = m_rndm.initialize  ( rndm , Rndm::Gauss ( 29.0 , 0.5 ) ) ;
   if ( sc.isFailure() ) 
   { return Error ( "Unable to initialize Gaussian distribution" , sc ) ; }
 
@@ -98,14 +98,16 @@ void Velo::VeloLumiRegionMonitor::monitorLumiRegion() {
 
   double entry = m_rndm.shoot();
   TH1D* hX = Gaudi::Utils::Aida2ROOT::aida2root( plot1D( entry
-							, "Beam X"
-							, "X position of luminous region (currently dummy)"
-							, -0.5, 9000.5, 9001 ) );
+							, "Beam X", "X position of luminous region", -5, 30, 70 ) );
 
   if( counter( "# events" ).flag()/100.-int(counter( "# events" ).flag()/100.)==0 ){
     d_vertexDistributionMeanX = hX->GetMean();
     d_vertexDistributionSigmaX= hX->GetRMS();
-    debug() << entry << "  ==>  " << d_vertexDistributionMeanX<<"    "<<d_vertexDistributionSigmaX<< endmsg;
+    d_vertexDistributionMeanY = hX->GetMean()-27.2;
+    d_vertexDistributionSigmaY= hX->GetRMS();
+    d_vertexDistributionMeanZ = hX->GetMean()-19.0;
+    d_vertexDistributionSigmaZ= hX->GetRMS() * 20.;
+    hX->Reset();
   }
 
 }
