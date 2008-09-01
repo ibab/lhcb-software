d4c39892844fad970df0c9666fe1a708b3e4de8f
jpalac <jpalac@cern.ch>
2008-09-01 16:59:12 +0000

    . Add templated default implementation.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@73190 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5375480287d4e59cc56ba090428737a7da28b40c

--- Analysis/MicroDST/MicroDSTBase/MicroDST/BindType2ClonerDef.h
+++ Analysis/MicroDST/MicroDSTBase/MicroDST/BindType2ClonerDef.h
@@ -1,4 +1,4 @@
-// $Id: BindType2ClonerDef.h,v 1.1 2008-09-01 11:55:43 jpalac Exp $
+// $Id: BindType2ClonerDef.h,v 1.2 2008-09-01 16:59:12 jpalac Exp $
 #ifndef MICRODST_BINDTYPE2CLONERDEF_H 
 #define MICRODST_BINDTYPE2CLONERDEF_H 1
 
@@ -10,5 +10,10 @@
  *  @author Juan PALACIOS
  *  @date   2008-09-01
  */
-template <typename T> struct BindType2Cloner { };
+template <typename T> struct BindType2Cloner 
+{
+  typedef T type;
+  typedef MicroDST::BasicCopy<T> cloner;
+};
+
 #endif // MICRODST_BINDTYPE2CLONERDEF_H
