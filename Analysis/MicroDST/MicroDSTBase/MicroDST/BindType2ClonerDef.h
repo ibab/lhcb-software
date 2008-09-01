f734565a2ff562e48be02369267db67d1c3dd940
jpalac <jpalac@cern.ch>
2008-09-01 11:55:43 +0000

    . Empty definition of template <typename T> struct BindType2Cloner for client     code to specialise.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@73172 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: de3cd96cb89867b4fc498916e8cc05dec6f59c1d

---
+++ Analysis/MicroDST/MicroDSTBase/MicroDST/BindType2ClonerDef.h
@@ -0,0 +1,14 @@
+// $Id: BindType2ClonerDef.h,v 1.1 2008-09-01 11:55:43 jpalac Exp $
+#ifndef MICRODST_BINDTYPE2CLONERDEF_H 
+#define MICRODST_BINDTYPE2CLONERDEF_H 1
+
+// Include files
+
+/** @class BindType2Cloner BindType2ClonerDef.h MicroDST/BindType2ClonerDef.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2008-09-01
+ */
+template <typename T> struct BindType2Cloner { };
+#endif // MICRODST_BINDTYPE2CLONERDEF_H
