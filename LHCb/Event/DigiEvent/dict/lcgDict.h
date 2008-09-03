97883d03f151028946bc20a755fb141d199fcaea
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-09-03 08:39:52 +0000

    add custom dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36860 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ab503863c695818208d281c9e481a115d809639c

---
+++ LHCb/Event/DigiEvent/dict/lcgDict.h
@@ -0,0 +1,23 @@
+// $Id: lcgDict.h,v 1.1 2008-09-03 08:39:52 cattanem Exp $
+#ifndef DIGIEVENT_LCGDICT_H 
+#define DIGIEVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "Kernel/FastClusterContainer.h"
+#include "Event/VeloLiteCluster.h"
+#include "Event/STLiteCluster.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    FastClusterContainer<LHCb::VeloLiteCluster, int> _i1;
+    FastClusterContainer<LHCb::STLiteCluster, int>   _i2;
+    // end instantiations
+  };
+}
+
+#endif // DIGIEVENT_LCGDICT_H
