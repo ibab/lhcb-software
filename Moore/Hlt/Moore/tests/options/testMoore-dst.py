4e99056c766b140e63b5b8fafc2d7e4601e85aac
snies <snies@cern.ch>
2008-09-03 20:31:57 +0000

    adding a simple test, that checks moore reaches Event 1001
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@73218 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9478675a08d4037fe86dd960628f46023d9dd59b

---
+++ Moore/Hlt/Moore/tests/options/testMoore-dst.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env gaudirun.py
+#
+# Minimal file for running Moore from python prompt
+# Syntax is:
+#   gaudirun.py ../options/Moore.py
+# or just
+#   ../options/Moore.py
+#
+from Gaudi.Configuration import *
+from Moore.Configuration import *
+
+
+#//---------------------------------------------------------------------------
+#// Histogram output
+#//---------------------------------------------------------------------------
+ApplicationMgr().HistogramPersistency = 'ROOT'
+HistogramPersistencySvc().OutputFile = 'Moore_minbias.root'
+
+#---------------------------------------------------------------------------
+# Number of events to process, optionally skipping some events
+#---------------------------------------------------------------------------
+
+# if you want to generate a configuration, uncomment the following lines:
+#Moore().generateConfig = True
+#Moore().configLabel = 'out of the box'
+
+# if you want to run, using a (set of) TCK(s), uncomment the following two
+# lines, and input the (list of ) TCK(s)
+#Moore().useTCK = True
+#Moore().prefetchTCK = [ 0x00100000 ]
+#HltConfigSvc().OutputLevel = DEBUG
+#HltConfigSvc().checkOdin = False
+#
+#Moore().hltType = 'PHYSICS_Hlt1'
+Moore().hltType = 'PHYSICS_Hlt1+Hlt2'
+#Moore().hltType = 'DEFAULT'  # current commissioning setup
+
+#Moore().DDDBtag = '2008-default'
+#Moore().condDBtag = '2008-default'
+# importOptions( "$DDDBROOT/options/LHCb-2008.py" )
+
+#files= [ '/data/bfys/lhcb/MinBias-L0strip/DC06_L0_v1_lumi2_MuonHadron_40000ev_' + str(f) +'.mdf'  for f in range(1,3) ]
+
+#files = [ '/data/bfys/lhcb/MinBias-L0strip/MBL0-lumi2-' + str(f) +'.dst'  for f in range(1,5) ]
+
+#files = [ '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-1.dst',
+#          '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-2.dst',
+#          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-3.dst',
+#          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-4.dst' ]
+
+files = [ 'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_1.mdf',
+          'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_2.mdf' ]
+
+
+filetype = files[0][-3:].upper()
+Moore().inputType = filetype
+extensions = { 'RAW' : "' SVC='LHCb::MDFSelector'",
+               'MDF' : "' SVC='LHCb::MDFSelector'",
+               'DST' : "' TYP='POOL_ROOTTREE' OPT='READ'" }
+EventSelector().Input = [ "DATAFILE='PFN:"+ f + extensions[ filetype ] for f in files ]
+
+EventSelector().PrintFreq = 100
+
+Moore().EvtMax = 2000
+
+
+# optionally, we can enable some auditors...
+# Moore().enableAuditor = [ NameAuditor() ]
+# always, always applyConf!
+Moore().applyConf()
+
+
+#and tell the world how we are configured
+print Moore()
