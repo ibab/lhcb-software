383572f2332068e9050e8c2c392bfba9ecb248f5
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-09-04 12:42:32 +0000

    automate counter bookkeeping
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@73229 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9c6f2284f469248e774379e1e0753b8628b38e4c

--- Hlt/HltSys/options/HltLumiAlleyConfSequence.py
+++ Hlt/HltSys/options/HltLumiAlleyConfSequence.py
@@ -2,18 +2,19 @@
 #     HLT Lumi Line(s)
 ##############################
 
-# removed all "IgnoreFilterPassed = True" to get correct behaviour
-
 import GaudiPython
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer as Sequence
+from Configurables import GaudiSequencer
 from Configurables import DeterministicPrescaler as Prescale
-from Configurables import DummyLumiAlley as Dummy
+from Configurables import HltDummySelection as Dummy
 from Configurables import OdinTypesFilter 
 from Configurables import HltTrackFilter, HltSelectionFilter
 from Configurables import LumiCountVertices, LumiCountTracks, LumiFromL0DU, LumiCountHltTracks
 from Configurables import HltLumiOdinReader, LumiHistoMaker
 from Configurables import RawEventDump, HltLumiSummaryDecoder, HltLumiWriter, bankKiller
+from HltConf.HltLine import Hlt1Line   as Line
+from HltConf.HltLine import Hlt1Member as Member
 
 ####### create binders...
 def sequenceAppender( seq ) :
@@ -21,9 +22,9 @@ def sequenceAppender( seq ) :
 
 def createCounter( counterKind, seqName, seq ) : 
     return lambda name, input  :  seq.Members.append( counterKind( seqName + name
-                                                                , InputSelection = input
-                                                                , CounterName = name
-                                                                , OutputContainer='Hlt/LumiSummary' ) )
+                                                                 , InputSelection = input
+                                                                 , CounterName = name
+                                                                 , OutputContainer='Hlt/LumiSummary' ) )
 def createL0Counter( seqName, seq ) :
     return lambda name, value :  seq.Members.append(  LumiFromL0DU(seqName+name
                                                                  , InputSelection='Trig/L0/L0DUReport'
@@ -33,11 +34,11 @@ def createL0Counter( seqName, seq ) :
 ####### operator, meet arguments...
 def combine( op, arg ) :
     for key,value in arg.iteritems() : op(key,value)
+    return arg.keys()
 
 ############# start building the lumi line(s)...
 
 # HLT Reconstruction
-importOptions('$HLTSYSROOT/options/HltRecoSequence.opts')
 # init is needed for ANNSvc
 importOptions('$HLTSYSROOT/options/HltLumiInit.opts')
 
@@ -48,43 +49,29 @@ debugging=False
 # list to collect the counters created using createCounter and createL0Counter
 createdCounters = []
 
-LumiSequence = sequenceAppender( Sequence('HltLumiSequence'
-                                          , MeasureTime = True ) )
-
-LumiSequence( OdinTypesFilter( 'OdinTriggerTypes',
-                               TriggerTypes=['TimingTrigger'],  # should become 'Random', now 'TimingTrigger' or 'Reserve'
-                               BXTypes= ['ALL'] ) )
-LumiSequence( Prescale( 'HltLumiPrescaler', AcceptFraction = 1.0 ) )
-
-LumiSequence( Sequence( 'Hlt1LumiRecoSequence'
-                      , Members = [ Sequence('HltRecoSequence')
-                                    , Dummy( 'AlwaysPassReco' )         ## needed to pass this filter
-                                    ]
-                      , ModeOR = True
-                      , ShortCircuit = False
-                      , MeasureTime = True ) )
-
 seqName = 'LumiCount'
-LumiCountSequence = Sequence('Hlt'+seqName +'Sequence'
+lumiCountSequence = Sequence('Hlt'+seqName +'Sequence'
                             , ModeOR = True
                             , ShortCircuit = False
                             , MeasureTime = True)
 
-combine( createCounter( LumiCountTracks,   seqName , LumiCountSequence),
-         {   'Velo'   : 'Hlt/Track/Velo'
-         , 'RZVelo'   : 'Hlt/Track/RZVelo'
-         #, 'Muon'    : 'Rec/Tracks/Muons' 
-         } )
-createdCounters.extend(['Velo','RZVelo'])
+createdCounters.extend( 
+        combine( createCounter( LumiCountTracks,   seqName , lumiCountSequence),
+               {   'Velo'   : 'Hlt/Track/Velo'
+               , 'RZVelo'   : 'Hlt/Track/RZVelo'
+               #, 'Muon'    : 'Rec/Tracks/Muons' 
+               } ) 
+        )
 
-combine( createCounter( LumiCountVertices, seqName, LumiCountSequence ),
-       {  'PV2D'  : 'Hlt/Vertex/PV2D'
-       #,  'PV3D'  : 'Hlt/Vertex/PV3D' 
-       } )
-createdCounters.extend(['PV2D'])
+createdCounters.extend(
+        combine( createCounter( LumiCountVertices, seqName, lumiCountSequence ),
+               {  'PV2D'  : 'Hlt/Vertex/PV2D'
+               #,  'PV3D'  : 'Hlt/Vertex/PV3D' 
+               } )
+        )
 
 # filter to get backward tracks (make sure it always passes by wrapping inside a sequence)
-LumiCountSequence.Members.append( Sequence('HltRZVeloBWSequence'
+lumiCountSequence.Members.append( Sequence('HltRZVeloBWSequence'
                                           , Members  = [ HltTrackFilter('HltPrepareRZVeloBW'
                                                                        , InputSelection   = 'TES:Hlt/Track/RZVelo'
                                                                        , AddInfo = False
@@ -97,19 +84,19 @@ LumiCountSequence.Members.append( Sequence('HltRZVeloBWSequence'
                                           , ShortCircuit = False
                                            ) )
 
-combine( createCounter( LumiCountHltTracks,   seqName , LumiCountSequence),
-         { 'RZVeloBW' : 'RZVeloBW'
-         } )
-createdCounters.extend(['RZVeloBW'])
+createdCounters.extend(
+    combine( createCounter( LumiCountHltTracks,   seqName , lumiCountSequence),
+             { 'RZVeloBW' : 'RZVeloBW'
+             } )
+    )
 
-combine( createL0Counter( seqName, LumiCountSequence ),
-         { 'SPDMult'  : 'Spd(Mult)'
-         , 'PUMult'   : 'PUHits(Mult)'
-         , 'CaloEt'   : 'Sum(Et)' 
-         } )
-createdCounters.extend(['SPDMult','PUMult','CaloEt' ])
-
-LumiSequence( LumiCountSequence )
+createdCounters.extend(
+    combine( createL0Counter( seqName, lumiCountSequence ),
+             { 'SPDMult'  : 'Spd(Mult)'
+             , 'PUMult'   : 'PUHits(Mult)'
+             , 'CaloEt'   : 'Sum(Et)' 
+             } )
+    )
 
 BXMembers = []
 for i in [ 'NoBeam', 'BeamCrossing','SingleBeamRight','SingleBeamLeft'] :
@@ -128,9 +115,33 @@ BXTypes = Sequence( 'Hlt1LumiBXTypesSequence'
                   , ModeOR = True
                   , ShortCircuit = False
                   , MeasureTime = True)
-LumiSequence( BXTypes )
-LumiSequence( Prescale( 'PrescaleLumiDecision', AcceptFraction = 1.0 ) )
-LumiSequence( Dummy( 'HltLumiDecision' ) )
+
+importOptions('$HLTSYSROOT/options/HltRecoSequence.py')
+
+lumiRecoSequence  = Sequence( 'Hlt1LumiRecoSequence'
+                  , Members = [ GaudiSequencer('HltRecoSequence')
+                              , Dummy( 'AlwaysPassReco' )  ## needed to pass this filter
+                              ]
+                  , ModeOR = True
+                  , ShortCircuit = False
+                  , MeasureTime = True )
+
+
+
+lumiSequence = Line ( 'Lumi'
+                    , ODIN = 
+                    { 'TriggerTypes' : [ 'TimingTrigger']  # should become 'Random', now 'TimingTrigger' or 'Reserve'
+                    }
+                    , algos =
+                    [ lumiRecoSequence
+                    , lumiCountSequence
+                    , BXTypes
+                    , Member('Dummy','Selection',OutputSelection = '%Decision') # add a dummy selection which is used as input to the decision
+                    ] )
+
+
+
+
 
 # @todo: move writer into dedicated rawbank sequence
 if debugging: LumiSequence( RawEventDump('FullRawDataDump' ) )
@@ -138,7 +149,7 @@ if debugging: LumiSequence( RawEventDump('FullRawDataDump' ) )
 #LumiSequence( bankKiller( 'SerialKiller', DefaultIsKill=True, BankTypes=["ODIN","HltLumiSummary"],
 #                          OutputLevel=debugOPL ) )
 if debugging: LumiSequence( RawEventDump('NanoEventDump' ) )
-LumiSequence( HltLumiWriter( OutputLevel=debugOPL )  )
+#LumiSequence( HltLumiWriter( OutputLevel=debugOPL )  )
 
 # register with global decision (which is the OR of its input)
-HltSelectionFilter('Hlt1Global').InputSelections.append( 'HltLumiDecision' ) 
+#HltSelectionFilter('Hlt1Global').InputSelections.append( 'HltLumiDecision' ) 
