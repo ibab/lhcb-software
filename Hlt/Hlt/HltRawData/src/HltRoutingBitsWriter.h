f66b3a5af9dea6ae38c8d7f2d9c8ebe6e5129e27
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-09-04 13:43:05 +0000

    fix off by 8 in old version
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@73235 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9ca351249c30ecfcc2f33dc4c3ab7de676a6a598

--- Hlt/Hlt/HltRawData/src/HltRoutingBitsWriter.h
+++ Hlt/Hlt/HltRawData/src/HltRoutingBitsWriter.h
@@ -1,11 +1,12 @@
-// $Id: HltRoutingBitsWriter.h,v 1.3 2008-09-04 13:31:50 graven Exp $
+// $Id: HltRoutingBitsWriter.h,v 1.4 2008-09-04 13:43:05 graven Exp $
 #ifndef HLTCOMMON_HLTROUTINGBITSWRITER_H 
 #define HLTCOMMON_HLTROUTINGBITSWRITER_H 1
 
 // Include files
 // from Gaudi
-#include "GaudiAlg/GaudiAlgorithm.h"
-#include "HltBase/IHltDecisionPredicateFactory.h"
+#include "HltBase/HltAlgorithm.h"
+#include "HltBase/HltSelectionContainer.h"
+#include "HltBase/IHltSelectionCombinerFactory.h"
 #include "boost/array.hpp"
 #include <algorithm>
 
@@ -15,7 +16,7 @@
  *  @author Gerhard Raven
  *  @date   2008-07-29
  */
-class HltRoutingBitsWriter : public GaudiAlgorithm {
+class HltRoutingBitsWriter : public HltAlgorithm {
 public: 
   /// Standard constructor
   HltRoutingBitsWriter( const std::string& name, ISvcLocator* pSvcLocator );
@@ -27,22 +28,22 @@ public:
   virtual StatusCode finalize  ();    ///< Algorithm finalization
 private:
 
-  typedef IHltDecisionPredicateFactory::decisionPredicate combiner_t;
+  typedef IHltSelectionCombinerFactory::selectionCombiner combiner_t;
   boost::array<combiner_t*,3*sizeof(unsigned int)*8> m_evaluators;
 
-  bool evaluate(size_t i,const LHCb::HltDecReports& reports) { return m_evaluators[i]!=0 && (*m_evaluators[i])(reports); }
+  bool evaluate(size_t i) { return m_evaluators[i]!=0 && m_evaluators[i]->evaluate(); }
   std::ostream& print(std::ostream& os, size_t i) 
   { os << " " << i << " : " ; 
     return (m_evaluators[i]!=0) ? m_evaluators[i]->print(os) : (os << "[none]");}
 
   void updateEvaluators(Property&);
-  void updateEvaluators(const IHltDecisionPredicateFactory&);
+  void updateEvaluators(const IHltSelectionCombinerFactory&);
   void zeroEvaluators() { std::fill(m_evaluators.begin(),m_evaluators.end(),(combiner_t*)0); }
  
-  std::string m_location;
   std::map<unsigned int,std::string> m_specifications;
 
-  IHltDecisionPredicateFactory  *m_factory;
+  Hlt::SelectionContainer0       m_selection;
+  IHltSelectionCombinerFactory  *m_factory;
   bool                           m_updateRequired;
 
 
