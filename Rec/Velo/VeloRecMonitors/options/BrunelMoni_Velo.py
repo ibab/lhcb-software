420d3cb2d8be4f3d1f79b1446bba5525410d1f47
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-09-06 12:14:16 +0000

    Remove MONI
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@24621 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 435970b7f6148733230120c0fc347c3ce1903b92

--- Rec/Velo/VeloRecMonitors/options/BrunelMoni_Velo.py
+++ Rec/Velo/VeloRecMonitors/options/BrunelMoni_Velo.py
@@ -8,7 +8,7 @@ from Configurables import ProcessPhase, GaudiSequencer
 
 from Configurables import Velo__VeloClusterMonitor
 
-ProcessPhase( 'Moni' ).DetectorList += [ 'VELO' ]
+# ProcessPhase( 'Moni' ).DetectorList += [ 'VELO' ] # done in configurables
 
 GaudiSequencer( 'MoniVELOSeq' ).Members +=\
                 [ Velo__VeloClusterMonitor( 'VeloClusterMonitor' ) ]
