a591e4405a1db4bb84abab28826c69350c7dfc6c
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-09-09 14:48:20 +0000

    o add new experimental python configuration files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@73294 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e7fcd0403d17c43cc9b1054e1465b699b70ad744

---
+++ Hlt/Hlt/HltConf/options/Hlt1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env gaudirun.py
+# =============================================================================
+# $Id: Hlt1.py,v 1.1 2008-09-09 14:48:20 graven Exp $
+# =============================================================================
+## @file
+#  Configuration of HLT1
+#  @author Gerhard Raven Gerhard.Raven@nikhef.nl
+#  @date 2008-08-25
+# =============================================================================
+"""
+ script to configure HLT1 trigger lines
+ Note: the actual line definitions are NOT in this file, 
+ this script relies on them being available through hlt1Lines()
+"""
+# =============================================================================
+__author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.1 $"
+# =============================================================================
+
+from Gaudi.Configuration import * 
+from Configurables       import GaudiSequencer as Sequence
+from Configurables       import HltSummaryWriter
+from Configurables       import HltSelectionFilter, HltSelectionToTES
+from Configurables       import HltDecisionFilter
+from Configurables       import HltDecReportsMaker, HltDecReportsWriter
+from Configurables       import HltRoutingBitsWriter
+from HltConf.HltLine     import hlt1Lines
+from HltConf.HltLine     import hlt1Decisions
+from HltConf.HltLine     import hlt1Selections
+
+
+importOptions('$HLTCONFROOT/options/HltInit.opts')
+# importOptions('$HLTCONFROOT/options/HltMain.py')
+
+summaryWriter = HltSummaryWriter( Save = list(hlt1Selections()['All']) )
+
+Hlt1Decision =  HltDecisionFilter('Hlt1Decision'
+                                , Accept = ' | '.join( hlt1Decisions() )
+                                )
+print Hlt1Decision
+
+### TODO: check dependencies, and re-order if needed! 
+Hlt1 = Sequence('Hlt1',  ModeOR = True, ShortCircuit = False
+               , Members = [ i.sequencer() for i in  hlt1Lines() ] + [ Hlt1Decision ] 
+               )
+
+triggerBits = HltRoutingBitsWriter( routingBitDefinitions = 
+                                  { 1 : 'Hlt1Decision'
+                                  , 2 : 'Hlt1RandomDecision|Hlt1PhysicsDecision|Hlt1LumiDecision'
+                                  , 3 : 'Hlt1RandomDecision'
+                                  , 4 : 'Hlt1PhysicsDecision'
+                                  , 5 : 'Hlt1LumiDecision'
+                                  })
+print triggerBits
+
+Hlt  = Sequence('Hlt')
+Hlt.Members = [ Hlt1
+              , Sequence( 'HltEndSequence', ModeOR = True, ShortCircuit = False
+                        , Members = [ HltDecReportsWriter()
+                                    , triggerBits
+                                    ] )
+              ]
+
+print Hlt
---
+++ Hlt/Hlt/HltConf/options/HltMain.py
@@ -0,0 +1,36 @@
+from Gaudi.Configuration import *
+from Configurables import ApplicationMgr
+from Configurables import GaudiSequencer
+from Configurables import CreateHltVzero
+#/**
+# * Common configuration for Hlt1 and Hlt2. Will be included only once.
+# *
+# * Take care when changing. Make sure changes work for both trigger levels.
+# */
+#/**
+# * defines online reconstruction
+# */
+importOptions('$HLTCONFROOT/options/HltRecoSequence.py')
+#/**
+# * Add Ks 
+# * @todo Make ProtoParticlea and articles with these Ks
+# * @todo See what to do with downstream Ks
+# */
+#
+#/**
+# * HltKshort moved to HltProtoparticles to profit from clone killing
+# *
+#importOptions('$HLTKSHORTROOT/options/HltV0LL.opts')
+
+## Kzero from two long tracks
+#GaudiSequencer('HltRecoSequence').Members.append( CreateHltVzero('HltV0LL')  )
+
+
+#/**
+# * main HLT sequencer
+# */
+Hlt = GaudiSequencer('Hlt', ModeOR= True, ShortCircuit = False) 
+
+
+ApplicationMgr().TopAlg.append( Hlt )
+
