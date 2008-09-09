1bf42aff9a5752de6540116b0bd14b9f99c21dab
Gloria Corti <gloria.corti@cern.ch>
2008-09-09 16:33:56 +0000

    add GenInit
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93671 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef3e7f578364b67098e9f1f1cc6a4af3765e1547

--- Gauss/Sim/Gauss/python/Gauss/Configuration.py
+++ Gauss/Sim/Gauss/python/Gauss/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for Gauss
 """
-__version__ = "$Id: Configuration.py,v 1.1 2008-09-09 16:27:22 gcorti Exp $"
+__version__ = "$Id: Configuration.py,v 1.2 2008-09-09 16:33:56 gcorti Exp $"
 __author__  = "Gloria Corti <Gloria.Corti@cern.ch>"
 
 from Gaudi.Configuration import *
@@ -9,3 +9,4 @@ from GaudiKernel.SystemOfUnits import GeV, ns, ms
 from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime )
 from Configurables import GiGaGeo
 import GaudiKernel.ProcessJobOptions
+from Configurables import GenInit
