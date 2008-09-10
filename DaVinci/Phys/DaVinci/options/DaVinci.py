05a8d2f3a66f7c1c5304ecb9a49eeb52eee06ae1
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-09-10 16:20:55 +0000

    L0 fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@73361 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7d48cb80977e66cef75d2435a657a75111e2ed57

--- DaVinci/Phys/DaVinci/options/DaVinci.py
+++ DaVinci/Phys/DaVinci/options/DaVinci.py
@@ -20,16 +20,10 @@ importOptions( "$DAVINCIROOT/options/DaVinciCommon.opts" )
 #
 # Trigger. Uncomment what you need. Hlt1 needs L0, Hlt2 doesn't.
 #
-# importOptions( "$L0DUROOT/options/ReplaceL0DUBankWithEmulated.opts" )
+importOptions( "$L0DUROOT/options/ReplaceL0DUBankWithEmulated.opts" )
 #
-# Temporary hack to re-run L0
-#
-#importOptions('$L0DUROOT/options/L0DUBankSwap.opts')
-importOptions('$L0DUROOT/options/DefaultTCK.opts')
-#
-#
-importOptions( "$HLTSYSROOT/options/Hlt1.opts" )
-importOptions( "$HLTSYSROOT/options/Hlt2.opts" )
+importOptions( "$HLTCONFROOT/options/Hlt1.opts" )
+importOptions( "$HLTCONFROOT/options/Hlt2.opts" )
 ########################################################################
 #
 # enable standard particles monitors
