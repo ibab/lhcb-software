4ec997f0a4e6c87f8a15894339ab325dfd71b4e4
Vanya Belyaev <ivan.belyaev@cern.ch>
2008-09-17 13:45:53 +0000

    add LHCb::Math::equal_to_(u)int functions
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48020 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5ae5b4fcbdd18b031613358b1443b46a2d5afc33

--- LHCb/Kernel/LHCbMath/LHCbMath/LHCbMath.h
+++ LHCb/Kernel/LHCbMath/LHCbMath/LHCbMath.h
@@ -1,33 +1,38 @@
-
-//---------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 /** @file
  *
  *  Collection of math related functions for general use in LHCb
  *
  *  CVS Log :-
- *  $Id: LHCbMath.h,v 1.4 2008-06-04 09:51:35 mneedham Exp $
+ *  $Id: LHCbMath.h,v 1.5 2008-09-17 13:45:53 ibelyaev Exp $
  *
  *  @author Juan PALACIOS
  *  @date   2005-11-21
  */
-//---------------------------------------------------------------------------------
-
+// ----------------------------------------------------------------------------
 #ifndef LHCBMATH_LHCBMATH_H 
 #define LHCBMATH_LHCBMATH_H 1
-
+// ============================================================================
 // Include files
-
+// ============================================================================
+// STD & STL 
+// ============================================================================
 #include <cmath>
 #include <algorithm>
 #include <functional>
-#include "boost/call_traits.hpp"
-
 // ============================================================================
-
-/** @namespace LHCb::Math
- * Collection of math related functions for general use in LHCb
- */
-namespace LHCb {
+// Boost 
+// ============================================================================
+#include "boost/call_traits.hpp"
+#include "boost/integer_traits.hpp"
+#include "boost/static_assert.hpp"
+// ============================================================================
+namespace LHCb 
+{
+  // ==========================================================================
+  /** @namespace LHCb::Math
+   *  Collection of math related functions for general use in LHCb
+   */
   namespace Math 
   {
     /// Parameters for numerical calculations (M.Needham)
@@ -36,7 +41,6 @@ namespace LHCb {
     static const double looseTolerance = 1e-5;
     static const double     sqrt_12 = 3.4641016151377546; // sqrt(12.)
     static const double inv_sqrt_12 = 0.2886751345948129; // 1./sqrt(12.)
-    //--------------------------------------------------------------------------
     // ========================================================================
     /** @struct abs_less 
      *  comparison by absolute value 
@@ -47,8 +51,8 @@ namespace LHCb {
     struct abs_less : std::binary_function<TYPE,TYPE,TYPE>
     {
       inline TYPE operator() 
-      ( typename boost::call_traits<const TYPE>::param_type v1 ,
-        typename boost::call_traits<const TYPE>::param_type v2 ) const 
+        ( typename boost::call_traits<const TYPE>::param_type v1 ,
+          typename boost::call_traits<const TYPE>::param_type v2 ) const 
       {
         return m_eval ( std::fabs( v1 ) , std::fabs( v2 ) ) ; 
       }
@@ -145,7 +149,8 @@ namespace LHCb {
         return equal_to_double ( v1 , v2 , m_eps ) ;
       }
     private :
-      double m_eps ;
+      /// the precision 
+      double m_eps ;  // the precision 
     };
     // ========================================================================
     /// explicit specialization for long doubles
@@ -164,7 +169,8 @@ namespace LHCb {
             static_cast<double> ( m_eps ) ) ;
       }
     private :
-      long double m_eps ;
+      /// the precision 
+      long double m_eps ; // the precision 
     };
     // ========================================================================
     /// explicit specialization for floats
@@ -179,14 +185,14 @@ namespace LHCb {
         return equal_to_double ( v1 , v2 , m_eps ) ;
       }
     private :
-      float m_eps ;
+      /// the precision 
+      float m_eps ; // the precision 
     } ;
-
-   // ========================================================================
+    // ========================================================================
     /** Round to nearest integer. Rounds half integers to nearest even integer.
      *  @author Matt Needham 
      */
-    inline int round(const double x)  {
+    inline int round ( const double x )  {
       int i;
       LHCb::Math::Equal_To<double> equal_to(lowTolerance);
       if (x >= 0.0) {
@@ -200,11 +206,112 @@ namespace LHCb {
       }
       return i;
     }
-
-    inline int round(const float x)  {
-      return LHCb::Math::round( double(x) );
+    // ======================================================================== 
+    /** Round to nearest integer. Rounds half integers to nearest even integer.
+     *  @author Matt Needham 
+     */
+    inline int round ( const float x )  
+    {
+      return LHCb::Math::round ( double ( x ) ) ;
     }
-  }
-};
-
+    // ========================================================================
+    /** check if the double value is actually equal to the integer value  
+     *  @param val value to be compared with the integer 
+     *  @param ref the reference integer number 
+     *  @param eps1 the absolute precision
+     *  @param eps2 the relative precision
+     *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+     *  @date 2008-09-17
+     */        
+    inline bool equal_to_int 
+    ( const double val          , 
+      const int    ref          , 
+      const double eps1 = 1.e-6 , 
+      const double eps2 = 1.e-3 /
+      ( boost::integer_traits<int>::const_max - 
+        boost::integer_traits<int>::const_min ) )
+    {
+      BOOST_STATIC_ASSERT(boost::integer_traits<int>::is_specialized ) ;
+      // 1) try the straightforward comparison 
+      if ( val == ref                 ) { return true ; } // RETURN 
+      // 2) check the limits 
+      if ( val > boost::integer_traits<int>::const_max || 
+           val < boost::integer_traits<int>::const_min ) { return false ; }
+      // 3) compare the doubles 
+      if ( std::fabs ( val - ref ) <= eps1 ) { return true  ; }
+      // 3') compare the doubles  
+      LHCb::Math::Equal_To<double> cmp ( eps2 ) ;
+      return cmp ( val , ref ) ;
+    }
+    // ========================================================================
+    /** check if the double value is actually equal to the integer value  
+     *  @param ref the reference integer  number 
+     *  @param val value to be compared with the integer 
+     *  @param eps1 the absolute precision
+     *  @param eps2 the relative precision
+     *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+     *  @date 2008-09-17
+     */        
+    inline bool equal_to_int 
+    ( const int    ref          , 
+      const double val          , 
+      const double eps1 = 1.e-6 , 
+      const double eps2 = 1.e-3 /
+      ( boost::integer_traits<int>::const_max - 
+        boost::integer_traits<int>::const_min ) )
+    { 
+      BOOST_STATIC_ASSERT(boost::integer_traits<int>::is_specialized ) ;
+      return equal_to_int ( val , ref , eps1 , eps2 ) ; 
+    }
+    // ========================================================================
+    /** check if the double value is actually equal to the unsigned integer value  
+     *  @param val value to be compared with the unsigned integer 
+     *  @param ref the reference unsigned integer number 
+     *  @param eps1 the absolute precision
+     *  @param eps2 the relative precision
+     *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+     *  @date 2008-09-17
+     */        
+    inline bool equal_to_uint 
+    ( const double       val         , 
+      const unsigned int ref         , 
+      const double eps1 = 1.e-6      , 
+      const double eps2 = 1.e-3 /
+      ( boost::integer_traits<unsigned int>::const_max ) )
+    {
+      BOOST_STATIC_ASSERT(boost::integer_traits<unsigned int>::is_specialized ) ;
+      // 1) straightforward comparison 
+      if ( val == ref ) { return true ; }                             // RETURN 
+      // check the limits 
+      if ( val > boost::integer_traits<unsigned int>::const_max || 
+           val < boost::integer_traits<unsigned int>::const_min ) { return false ; }
+      // 3) compare the doubles 
+      if ( std::fabs ( val - ref ) <= eps1 ) { return true  ; }
+      // 3') compare the doubles  
+      LHCb::Math::Equal_To<double> cmp ( eps2 ) ;
+      return cmp ( val , ref ) ;
+    }
+    // ========================================================================
+    /** check if the double value is actually equal to the integer value  
+     *  @param val value to be compared with the unsigned integer 
+     *  @param ref the reference unsigned integer number 
+     *  @param eps the absolute precision 
+     *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+     *  @date 2008-09-17
+     */        
+    inline bool equal_to_uint 
+    ( const unsigned int ref          , 
+      const double       val          ,
+      const double       eps1 = 1.e-6 , 
+      const double       eps2 = 1.e-3 /
+      ( boost::integer_traits<unsigned int>::const_max ) )
+    { return equal_to_uint ( val , ref , eps1 , eps2 ) ; }
+    // ========================================================================
+  } // end of namespace LHcb::Math 
+  // ==========================================================================
+} // end of namespace LHCb 
+// ============================================================================
+// The END 
+// ============================================================================
 #endif // LHCBMATH_LHCBMATH_H
+// ============================================================================
