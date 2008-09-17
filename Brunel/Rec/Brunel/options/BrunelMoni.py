ca4205541544872e096d08b671b001382d80c661
Wouter Hulsbergen <wouterh@nikhef.nl>
2008-09-17 21:56:12 +0000

    replaces BrunelMoni.opts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30272 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0599117f11fceaceabc1a1fb0557ab0f9bcdaf03

---
+++ Brunel/Rec/Brunel/options/BrunelMoni.py
@@ -0,0 +1,34 @@
+# This file sets up the default Brunel monitoring, independent of MC Truth
+#-------------------------------------------------------------------------
+
+from GaudiKernel.ProcessJobOptions import importOptions
+from Configurables import (ProcessPhase,RootHistCnv__PersSvc,
+                           ApplicationMgr,AuditorSvc,SequencerTimerTool)
+
+ProcessPhase('Moni').Context = 'Offline'
+
+# ROOT persistency for histograms
+importOptions('$STDOPTS/RootHist.opts')
+RootHistCnv__PersSvc('RootHistCnv').ForceAlphaIds = True
+
+# Use TimingAuditor for timing, suppress printout from SequencerTimerTool
+ApplicationMgr().ExtSvc += [ 'ToolSvc', 'AuditorSvc' ]
+ApplicationMgr().AuditAlgorithms = True
+AuditorSvc().Auditors += [ 'TimingAuditor' ] 
+SequencerTimerTool().OutputLevel = 4
+
+#--------------------------------------------------------------
+# Calo Monitoring algorithms
+#--------------------------------------------------------------
+importOptions('$CALOMONIDSTOPTS/CaloMonitor.opts')
+
+#--------------------------------------------------------------
+# Velo Monitoring algorithms
+#--------------------------------------------------------------
+importOptions('$VELORECMONITORSROOT/options/BrunelMoni_Velo.py')
+
+#--------------------------------------------------------------
+# Track Monitoring algorithms
+#--------------------------------------------------------------
+from TrackMonitors.ConfiguredTrackMonitors import ConfiguredTrackMonitorSequence
+ConfiguredTrackMonitorSequence(Name='MoniTrackSeq')
