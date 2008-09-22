49195be23656c621557ccc58a5a2a1812150d7a7
Olivier Deschamps <odescham@cern.ch>
2008-09-22 07:21:12 +0000

    add
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40077 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0450fa694be4d76d519aa3297510da9e45229715

---
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/ICaloHypo2Calo.h
@@ -0,0 +1,55 @@
+// $Id: ICaloHypo2Calo.h,v 1.1 2008-09-22 07:21:12 odescham Exp $
+#ifndef ICALOHYPO2CALO_H 
+#define ICALOHYPO2CALO_H 1
+
+// Include files
+// from STL
+#include <string>
+
+#include "Kernel/CaloCellID.h"
+#include "Event/CaloDigit.h"
+#include "Event/CaloCluster.h"
+#include "Event/CaloHypo.h"
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+// forward declaration
+
+static const InterfaceID IID_ICaloHypo2Calo ( "ICaloHypo2Calo", 1, 0 );
+
+/** @class ICaloHypo2Calo ICaloHypo2Calo.h
+ *  
+ *
+ *  @author Olivier Deschamps
+ *  @date   2008-09-09
+ */
+
+
+class ICaloHypo2Calo : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_ICaloHypo2Calo; }
+
+  virtual StatusCode initialize()=0;
+  virtual std::vector<LHCb::CaloCellID> cellIDs(LHCb::CaloHypo  fromHypo, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloCellID> cellIDs(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloCellID> cellIDs()=0;
+
+  virtual std::vector<LHCb::CaloDigit*> digits(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloDigit*> digits(LHCb::CaloHypo  fromHypo, std::string toCalo)=0;
+  virtual std::vector<LHCb::CaloDigit*> digits()=0;
+
+  virtual double energy(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual double energy(LHCb::CaloHypo  fromHypo, std::string toCalo)=0;
+  virtual double energy()=0;
+
+  virtual int multiplicity(LHCb::CaloCluster  fromCluster, std::string toCalo)=0;
+  virtual int multiplicity(LHCb::CaloHypo   fromHypo     , std::string toCalo)=0;
+  virtual int multiplicity()=0;
+
+protected:
+
+private:
+
+};
+#endif // ICALOHYPO2CALO_H
