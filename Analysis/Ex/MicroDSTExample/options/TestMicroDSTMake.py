d9c2bad0a9fdccb6c065c4463fe041d229255d9a
jpalac <jpalac@cern.ch>
2008-09-23 18:01:12 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@73465 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7885b115d0a0138d0ef239f8e9ec42020ddaaa95

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMake.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMake.py
@@ -1,6 +1,28 @@
+#$Id: TestMicroDSTMake.py,v 1.2 2008-09-23 18:01:12 jpalac Exp $
 from Gaudi.Configuration import *
 from DaVinci.MicroDSTAlgorithm import *
 from Configurables import CopyRelatedMCParticles
 from Configurables import CopyParticle2PVLink
-importOptions( "$DAVINCIROOT/options/TestNewMicroDSTStuff.opts")
+from Configurables import MCParticleArrayFilterAlg
+from Configurables import FilterMCParticleArrayByDecay
+from Configurables import MCDecayFinder
+#
+AllEvents = GaudiSequencer('AllEvents')
+ApplicationMgr().TopAlg += [ AllEvents ]
+#
+filterMCDecays = MCParticleArrayFilterAlg('FilterMCDecays')
+filterMCDecays.OutputLocation = "MC/Decays"
+FilterMCParticleArrayByDecay().addTool( MCDecayFinder() )
+filterMCDecays.addTool( FilterMCParticleArrayByDecay() )
+filterMCDecays.FilterMCParticleArrayByDecay.MCDecayFinder.Decay = "[B_s0 -> (J/psi(1S) -> mu+ mu- {,gamma} {,gamma}) (phi(1020) -> K+ K-)]cc"
+filterMCDecays.OutputLevel = 4;
+#importOptions( "$DAVINCIROOT/options/TestNewMicroDSTStuff.opts")
+importOptions( "JPsiPhiData.opts")
+importOptions("$DAVINCIROOT/options/MicroDSTCommon.opts")
 
+AllEvents.Members  = [CopyRecHeader()]
+AllEvents.Members += ['FilterMCDecays']
+
+ApplicationMgr().EvtMax = 100
+EventSelector().FirstEvent = 1 
+EventSelector().PrintFreq = 10 
