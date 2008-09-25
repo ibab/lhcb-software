b11f2a3e92680f497b7d3269e5d889434f3aeb76
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-09-25 13:00:11 +0000

    add missing dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36919 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 992e3b67f65a330ee8b3af1adb0e3b53dbebadcc

--- LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
+++ LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
@@ -1,9 +1,9 @@
-// $Id: lcg_selection.h,v 1.1 2007-11-09 17:28:42 cattanem Exp $
+// $Id: lcg_selection.h,v 1.2 2008-09-25 13:00:11 cattanem Exp $
 #ifndef L0EVENT_LCGDICT_H 
 #define L0EVENT_LCGDICT_H 1
 
 // Additional classes to be added to automatically generated lcgdict
 
-// Dummy file in this case, custom dictionary just there to exclude duplicates
+// Dummy file in this case, all necessary header files already in GaudiObjDesc generated files
 
 #endif // L0EVENT_LCGDICT_H
