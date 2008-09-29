b868b42d2f97f8f48f7cbadfb233cb3f4abb9544
jpalac <jpalac@cern.ch>
2008-09-29 15:50:41 +0000

    - Backwards-incompatible changes to IRelatedPVFinder. interface now contains    three simple methods taking a particle, some primary vertices, and an    empty range of weighted Particle->VertexBase relations, so be filled    according to some "bestness" logic.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64289 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 80857149f2535194b3f4c7e72edc332cc48e7248

--- Phys/Phys/DaVinciKernel/Kernel/IRelatedPVFinder.h
+++ Phys/Phys/DaVinciKernel/Kernel/IRelatedPVFinder.h
@@ -1,4 +1,4 @@
-// $Id: IRelatedPVFinder.h,v 1.2 2007-02-06 09:57:56 pkoppenb Exp $
+// $Id: IRelatedPVFinder.h,v 1.3 2008-09-29 15:50:25 jpalac Exp $
 #ifndef KERNEL_IRELATEDPVFINDER_H 
 #define KERNEL_IRELATEDPVFINDER_H 1
 
@@ -13,16 +13,17 @@
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
 
-static const InterfaceID IID_IRelatedPVFinder ( "IRelatedPVFinder", 1, 0 );
+static const InterfaceID IID_IRelatedPVFinder ( "IRelatedPVFinder", 2, 0 );
 
 /** @class IRelatedPVFinder IRelatedPVFinder.h Kernel/IRelatedPVFinder.h
  *  
- *  Finds related PV according to some criteria
+ *  Fill a weighted relations range with the weighted relations between 
+ *  an LHCb::Particle and a container of LHCb::RecVertices.
+ *  Replaces interface by Patrick Koppenburg, 2006-10-22.
  *
- *  @todo need also to pass the weight... best is to pass the relations table altogether
+ *  @author Juan Palacios juan.palacios@nikhef.nl
  *
- *  @author Patrick Koppenburg
- *  @date   2006-10-22
+ *  @date   2008-09-29
  */
 class IRelatedPVFinder : virtual public IAlgTool {
 public: 
@@ -30,15 +31,50 @@ public:
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IRelatedPVFinder; }
 
-  /// Build relation table and store it in desktop
-  virtual StatusCode relatedPVs(const LHCb::Particle* p, 
-                                Particle2Vertex::Table* ) const =  0;
-
-  virtual StatusCode setDefaults(std::string PVloc, std::string geomTool) = 0 ;
-
-protected:
-
-private:
+  /**
+   * Return a range of weighted relations between an LHCb::Particle and a
+   * keyed container of LHCb::RecVertices
+   * 
+   * @param particle       The LHCb::Particle for which related PVs will be searched
+   * @param PVs            Container of LHCb::RecVertices to be related to particle
+   * @param relatedPVRange Range of weighted relations between particle and the vertices in PVs. 
+   * Should be empty, if not, it it cleared before filling.
+   *
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   */
+  virtual StatusCode relatedPVs(const LHCb::Particle* particle,
+                                const LHCb::RecVertex::Container& PVs,
+                                Particle2Vertex::Range& relatedPVRange)=0;
+  /**
+   * Return a range of weighted relations between an LHCb::Particle and a
+   * vector of const LHCb::RecVertex pointers
+   *
+   * @param particle       The LHCb::Particle for which related PVs will be searched
+   * @param PVs            std::vector of LHCb::RecVertex* to be related to particle
+   * @param relatedPVRange Range of weighted relations between particle and the vertices in PVs. 
+   * Should be empty, if not, it it cleared before filling.
+   *
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   */
+  virtual StatusCode relatedPVs(const LHCb::Particle* particle,
+                                const LHCb::RecVertex::ConstVector& PVs,
+                                Particle2Vertex::Range& relatedPVRange)=0;
+  /**
+   * Return a range of weighted relations between an LHCb::Particle and a
+   * keyed container of LHCb::RecVertices from a TES location
+   *
+   * @param particle       The LHCb::Particle for which related PVs will be searched
+   * @param PVLocation     String giving the TES location of a keyed container 
+   * of LHCb::RecVertices to be related to particle
+   * @param relatedPVRange Range of weighted relations between particle 
+   * and the vertices in PVs. 
+   * Should be empty, if not, it it cleared before filling.
+   *
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   */
+  virtual StatusCode relatedPVs(const LHCb::Particle* particle,
+                                const std::string& PVLocation,
+                                Particle2Vertex::Range& relatedPVRange)=0;
 
 };
 #endif // KERNEL_IRELATEDPVFINDER_H
