403b9cc627ad6b7ed7779f73917deb3a9da87e0e
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-09-30 08:05:05 +0000

    add more missing dicts
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36938 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 159c4280d42986ee4da553bbca05412fff095004

--- LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
+++ LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
@@ -1,9 +1,24 @@
-// $Id: lcg_selection.h,v 1.2 2008-09-25 13:00:11 cattanem Exp $
+// $Id: lcg_selection.h,v 1.3 2008-09-30 08:05:05 cattanem Exp $
 #ifndef L0EVENT_LCGDICT_H 
 #define L0EVENT_LCGDICT_H 1
 
 // Additional classes to be added to automatically generated lcgdict
 
-// Dummy file in this case, all necessary header files already in GaudiObjDesc generated files
+// begin include files
+#include <map>
+#include <string>
+#include <utility>
+#include "Event/L0DUChannel.h"
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::map<std::string,LHCb::L0DUChannel*> l0_1;
+    std::map<std::string,LHCb::L0DUChannel*>::iterator l0_2;
+    std::pair<const std::string,LHCb::L0DUChannel*>  l0_3;
+    // end instantiations
+  };
+}
 
 #endif // L0EVENT_LCGDICT_H
