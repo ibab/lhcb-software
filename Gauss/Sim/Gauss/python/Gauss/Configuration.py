78739ab79f8040ce93fa98e4d34be5f8ae23eb23
Gloria Corti <gloria.corti@cern.ch>
2008-10-08 16:52:38 +0000

    particle table from DDDB
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93727 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 15666bed108bb7a046c91e3afd9f81263768a557

--- Gauss/Sim/Gauss/python/Gauss/Configuration.py
+++ Gauss/Sim/Gauss/python/Gauss/Configuration.py
@@ -1,12 +1,14 @@
 """
 High level configuration tools for Gauss
 """
-__version__ = "$Id: Configuration.py,v 1.2 2008-09-09 16:33:56 gcorti Exp $"
+__version__ = "$Id: Configuration.py,v 1.3 2008-10-08 16:52:38 gcorti Exp $"
 __author__  = "Gloria Corti <Gloria.Corti@cern.ch>"
 
 from Gaudi.Configuration import *
-from GaudiKernel.SystemOfUnits import GeV, ns, ms
-from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime )
+from GaudiKernel.SystemOfUnits import GeV, ns, ms, mrad
+from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime,
+                            CondDBEntityResolver )
 from Configurables import GiGaGeo
 import GaudiKernel.ProcessJobOptions
-from Configurables import GenInit
+from Configurables import ( GenInit, Generation, MinimumBias, PythiaProduction,
+                            CollidingBeams )
