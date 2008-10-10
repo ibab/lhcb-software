1dbeb590be0de6aa39bc99ef21c1bb29f7dbb18e
Gloria Corti <gloria.corti@cern.ch>
2008-10-10 14:16:41 +0000

    new packages
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93754 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4e9e89ccff769e116995567a2f1c1e5181a00e6

--- Gauss/GaussSys/doc/MainPage.h
+++ Gauss/GaussSys/doc/MainPage.h
@@ -11,15 +11,7 @@
  *
  * <b>The HepMC doxygen pages are not included, follow the link below:</b>
  * \li: 
- * <a href="http://lcgapp.cern.ch/doxygen/HEPMC/HepMC_1_26_02/doxygen/html/index.html">HepMC 1.26.02</a>
+ * <a href="http://lcgapp.cern.ch/project/simu/HepMC/HepMC203/html/">HepMC 2.03.09</a>
  * 
- * <hr>
- * \htmlinclude new_release.notes
- *
- * <b>Top level options files</b>
- * \li v200601.opts: DC06 geometry
- * \li v200601-BOff.opts: DC06 geometry, field off
- * \li v200601-OpenVelo-BOff.opts: DC06 geometry, Velo Open and field off
- * \li Similar options for 450 GeV/c beam enery are available as v200601-450GeV-{Open,SemiOpen,Close}Velo-B{On,Off}.opts 
  *
  */
