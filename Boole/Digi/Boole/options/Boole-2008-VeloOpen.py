55344ed4300a46d832c6fe990ae5e2ec26223b65
Wouter Hulsbergen <wouterh@nikhef.nl>
2008-10-13 11:47:15 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@60009 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 727e89db52af382e2ce57bf1610bb2be3a1ea7a7

---
+++ Boole/Digi/Boole/options/Boole-2008-VeloOpen.py
@@ -0,0 +1,24 @@
+##############################################################################
+# File for running Boole with default 2008 geometry, as defined in
+#                                              $DDDBROOT/options/lhcb-2008.py
+# Syntax is:
+#   gaudirun.py Boole-2008.py <someDataFiles>.py
+##############################################################################
+
+from Boole.Configuration import *
+
+Boole().useSpillover = False
+Boole().DDDBtag      = "2008-default"
+Boole().condDBtag    = "2008-default"
+
+Boole().applyConf()
+
+#--Tell SIMCOND tag to generate Open VELO
+from DetCond.Configuration import addCondDBLayer
+simCondVelo = allConfigurables["SIMCOND"].clone("VELOCOND")
+simCondVelo.DefaultTAG = "velo-open"
+addCondDBLayer(simCondVelo)
+
+##############################################################################
+# I/O datasets are defined in a separate file, see examples in 2008-Files.py
+##############################################################################
