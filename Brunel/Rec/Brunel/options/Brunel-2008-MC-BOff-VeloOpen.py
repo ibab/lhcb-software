3351cdbcd68a2dbfc1223ea3adfc8d73d57e1dfe
Wouter Hulsbergen <wouterh@nikhef.nl>
2008-10-13 11:57:51 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30300 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5dcdb7ad32efc8ad415ea6d304aef91320984123

---
+++ Brunel/Rec/Brunel/options/Brunel-2008-MC-BOff-VeloOpen.py
@@ -0,0 +1,24 @@
+##############################################################################
+# File for running Brunel on MC data with default 2008 geometry, as defined in
+#                                              $DDDBROOT/options/lhcb-2008.py
+# Syntax is:
+#   gaudirun.py Brunel-2008-MC.py <someDataFiles>.py
+##############################################################################
+
+from Brunel.Configuration import *
+
+Brunel().inputType    = "DIGI"
+Brunel().withMC       = True
+Brunel().fieldOff     = True
+Brunel().veloOpen     = True
+Brunel().applyConf()
+
+#--Specify SIMCOND tag for Open VELO
+from DetCond.Configuration import addCondDBLayer
+simCondVelo = allConfigurables["SIMCOND"].clone("VELOCOND")
+simCondVelo.DefaultTAG = "velo-open"
+addCondDBLayer(simCondVelo)
+
+##############################################################################
+# I/O datasets are defined in a separate file, see examples in 2008-Files.py
+##############################################################################
