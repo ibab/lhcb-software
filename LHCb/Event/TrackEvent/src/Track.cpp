35e769bd09148f1afac450f56fe83b315d472303
Wouter Hulsbergen <wouterh@nikhef.nl>
2008-10-15 15:34:53 +0000

    protect Track::probChi2 against ndof = 0
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@36995 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 129d99c9d27708609d5daa178f2e8b0ada7b30e9

--- LHCb/Event/TrackEvent/src/Track.cpp
+++ LHCb/Event/TrackEvent/src/Track.cpp
@@ -26,10 +26,13 @@ using namespace LHCb;
 //=============================================================================
 double Track::probChi2() const
 {
+  double val(0) ; 
+  if (nDoF()>0) { 
     const double limit = 1e-15;
-    double chi2max = gsl_cdf_chisq_Qinv (limit, nDoF());
-    return chi2() < chi2max ? gsl_cdf_chisq_Q(chi2(),nDoF()) : 0;
-   
+    double chi2max = gsl_cdf_chisq_Qinv (limit, nDoF()); 
+    val = chi2() < chi2max ? gsl_cdf_chisq_Q(chi2(),nDoF()) : 0; 
+  } 
+  return val ;
 };
 
 //=============================================================================
