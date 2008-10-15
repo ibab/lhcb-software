2016fb407cc57b509c10dd5c255b2588356bae9f
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-10-15 13:26:30 +0000

    add file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@10210 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 59265d8e3b88f176083bc2f34dd2ad199b22fd20

---
+++ LHCb/Rich/RichKernel/RichKernel/IRichAddBackground.h
@@ -0,0 +1,64 @@
+
+//---------------------------------------------------------------------------------
+/** @file IRichAddBackground.h
+ *
+ * Header file for tool interface : Rich::IAddBackground
+ *
+ * CVS Log :-
+ * $Id: IRichAddBackground.h,v 1.1 2008-10-15 13:26:30 jonrob Exp $
+ *
+ * @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ * @date   2008-10-10
+ */
+//---------------------------------------------------------------------------------
+
+#ifndef RICHKERNEL_IRICHADDBACKGROUND_H
+#define RICHKERNEL_IRICHADDBACKGROUND_H 1
+
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+// Kernel
+#include "RichKernel/RichHashMap.h"
+
+namespace Rich
+{
+
+  /// Tool Interface ID
+  static const InterfaceID IID_IRichAddBackground ( "Rich::IAddBackground", 1, 0 );
+
+  /** @class IAddBackground RichKernel/IRichAddBackground.h
+   *
+   *  Interface for tools that generate RICH backgrounds
+   *
+   *  @author Chris Jones
+   *  @date   2008-10-10
+   */
+
+  class IAddBackground : virtual public IAlgTool
+  {
+
+  public:
+
+    /// Return the interface ID
+    static const InterfaceID& interfaceID() { return IID_IRichAddBackground; }
+
+  public:
+
+    /// Data structure for returned additional backgrounds
+    typedef Rich::HashMap<LHCb::RichSmartID,LHCb::RichSmartID::Vector> HPDBackgrounds;
+
+  public:
+
+    /// Compute a set of background hits
+    virtual StatusCode createBackgrounds( HPDBackgrounds & backgrounds,
+                                          const bool aliceMode = false ) const = 0;
+
+  };
+
+}
+
+#endif // RICHKERNEL_IRICHADDBACKGROUND_H
