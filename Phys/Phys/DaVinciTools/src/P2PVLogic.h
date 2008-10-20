262f7675ed72cf2a4df1b99ccbd72895ad532bed
jpalac <jpalac@cern.ch>
2008-10-20 15:14:26 +0000

    Structs implementing     static double weight(const LHCb::Particle*,                          const LHCb::VertexBase*,                          const IDistanceCalculator*)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64352 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e1949a99063ec3aff8dbd1689362432924135199

---
+++ Phys/Phys/DaVinciTools/src/P2PVLogic.h
@@ -0,0 +1,53 @@
+// $Id: P2PVLogic.h,v 1.1 2008-10-20 15:14:26 jpalac Exp $
+#ifndef P2PVLOGIC_H 
+#define P2PVLOGIC_H 1
+
+// Include files
+//DaVinci
+#include "Kernel/IDistanceCalculator.h"
+/** @file P2PVLogic P2PVLogic.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2008-10-20
+ */
+
+/**@struct _p2PVWithIP 
+ *
+ *  @author Juan PALACIOS
+ *  @date   2008-10-20
+ */
+
+struct _p2PVWithIP 
+{
+  static double weight(const LHCb::Particle* particle,
+                       const LHCb::VertexBase* pv,
+                       const IDistanceCalculator* distCalc) 
+  {
+    double fom(0.);
+    const StatusCode sc =  distCalc->distance(particle, pv, fom);
+    return ( (sc.isSuccess()) && fom > 0. ) ? 1./fom : 0. ;
+  }
+  
+};
+
+/**@struct _p2PVWithIPChi2
+ *
+ *  @author Juan PALACIOS
+ *  @date   2008-10-20
+ */
+
+struct _p2PVWithIPChi2
+{
+  static double weight(const LHCb::Particle* particle,
+                       const LHCb::VertexBase* pv,
+                       const IDistanceCalculator* distCalc) 
+  {
+    double fom(0.);
+    double chi2(0.);
+    const StatusCode sc = distCalc->distance(particle, pv, fom, chi2);
+    return ( (sc.isSuccess()) && chi2 > 0. ) ?  1./chi2 : 0 ;
+  }
+};
+
+#endif // P2PVLOGIC_H
