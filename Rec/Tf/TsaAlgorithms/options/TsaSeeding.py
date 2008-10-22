0141987da681209023c622661c6aaf2e84f38a9d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-10-22 09:58:57 +0000

    ! 2008-10-22 - Chris Jones  - Adapt to changes in TrackSys python configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@18396 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d8edcf461037335bb65f4ff155dc0ab39d57736f

--- Rec/Tf/TsaAlgorithms/options/TsaSeeding.py
+++ Rec/Tf/TsaAlgorithms/options/TsaSeeding.py
@@ -11,67 +11,69 @@ Tf__Tsa__SeedTrackCnv( "TsaSeedTrackCnv").outputLocation = "Rec/Track/Seed"
 Tf__Tsa__Seed("TsaSeed").addTool( Tf__Tsa__TStationHitManager("TsaDataManager") )
 Tf__Tsa__TStationHitManager("TsaDataManager").CleanITHits = True;
 Tf__Tsa__TStationHitManager("TsaDataManager").CleanOTHits = True;
+
+tsaSeed = Tf__Tsa__Seed("TsaSeed")
   
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITXSearch(), name="xSearchS0")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITXSearch(), name="xSearchS1")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITXSearch(), name="xSearchS2")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__OTXSearch(), name="xSearchS3")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__OTXSearch(), name="xSearchS4")
+tsaSeed.addTool(Tf__Tsa__ITXSearch(), name="xSearchS0")
+tsaSeed.addTool(Tf__Tsa__ITXSearch(), name="xSearchS1")
+tsaSeed.addTool(Tf__Tsa__ITXSearch(), name="xSearchS2")
+tsaSeed.addTool(Tf__Tsa__OTXSearch(), name="xSearchS3")
+tsaSeed.addTool(Tf__Tsa__OTXSearch(), name="xSearchS4")
 
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITStereoSearch(), name="stereoS0")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITStereoSearch(), name="stereoS1")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__ITStereoSearch(), name="stereoS2")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__OTStereoSearch(), name="stereoS3")
-Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__OTStereoSearch(), name="stereoS4")       
+tsaSeed.addTool(Tf__Tsa__ITStereoSearch(), name="stereoS0")
+tsaSeed.addTool(Tf__Tsa__ITStereoSearch(), name="stereoS1")
+tsaSeed.addTool(Tf__Tsa__ITStereoSearch(), name="stereoS2")
+tsaSeed.addTool(Tf__Tsa__OTStereoSearch(), name="stereoS3")
+tsaSeed.addTool(Tf__Tsa__OTStereoSearch(), name="stereoS4")       
 
-Tf__Tsa__Seed("TsaSeed").xSearchS0.sector = 0
-Tf__Tsa__Seed("TsaSeed").xSearchS1.sector = 1
-Tf__Tsa__Seed("TsaSeed").xSearchS2.sector = 2
-Tf__Tsa__Seed("TsaSeed").xSearchS3.sector = 3
-Tf__Tsa__Seed("TsaSeed").xSearchS4.sector = 4
+tsaSeed.xSearchS0.sector = 0
+tsaSeed.xSearchS1.sector = 1
+tsaSeed.xSearchS2.sector = 2
+tsaSeed.xSearchS3.sector = 3
+tsaSeed.xSearchS4.sector = 4
 
-Tf__Tsa__Seed("TsaSeed").stereoS0.sector = 0
-Tf__Tsa__Seed("TsaSeed").stereoS1.sector = 1
-Tf__Tsa__Seed("TsaSeed").stereoS2.sector = 2
-Tf__Tsa__Seed("TsaSeed").stereoS3.sector = 3
-Tf__Tsa__Seed("TsaSeed").stereoS4.sector = 4
+tsaSeed.stereoS0.sector = 0
+tsaSeed.stereoS1.sector = 1
+tsaSeed.stereoS2.sector = 2
+tsaSeed.stereoS3.sector = 3
+tsaSeed.stereoS4.sector = 4
 
-if TrackSys().getProp("fieldOff"):
+if TrackSys().fieldOff():
     Tf__Tsa__SeedTrackCnv( "TsaSeedTrackCnv").addTool( Tf__Tsa__SeedTrackCnvTool("SeedTrackCnvTool"))
     Tf__Tsa__SeedTrackCnvTool("SeedTrackCnvTool").pFromCurvature = True
     
-    Tf__Tsa__Seed("TsaSeed").xSearchS3.sxCut = 0.40;
-    Tf__Tsa__Seed("TsaSeed").xSearchS4.sxCut = 0.40;
-    Tf__Tsa__Seed("TsaSeed").xSearchS3.collectPolicy = "Linear";
-    Tf__Tsa__Seed("TsaSeed").xSearchS4.collectPolicy = "Linear";
-    Tf__Tsa__Seed("TsaSeed").xSearchS3.sx2Cut = 0.330;
-    Tf__Tsa__Seed("TsaSeed").xSearchS4.sx2Cut = 0.330;
+    tsaSeed.xSearchS3.sxCut = 0.40;
+    tsaSeed.xSearchS4.sxCut = 0.40;
+    tsaSeed.xSearchS3.collectPolicy = "Linear";
+    tsaSeed.xSearchS4.collectPolicy = "Linear";
+    tsaSeed.xSearchS3.sx2Cut = 0.330;
+    tsaSeed.xSearchS4.sx2Cut = 0.330;
     
-    Tf__Tsa__Seed("TsaSeed").xSearchS0.dthCut = 0.08;
-    Tf__Tsa__Seed("TsaSeed").xSearchS1.dthCut = 0.08;
-    Tf__Tsa__Seed("TsaSeed").xSearchS2.dthCut = 0.08;
-    Tf__Tsa__Seed("TsaSeed").xSearchS3.dthCut = 0.08;
-    Tf__Tsa__Seed("TsaSeed").xSearchS4.dthCut = 0.08;
+    tsaSeed.xSearchS0.dthCut = 0.08;
+    tsaSeed.xSearchS1.dthCut = 0.08;
+    tsaSeed.xSearchS2.dthCut = 0.08;
+    tsaSeed.xSearchS3.dthCut = 0.08;
+    tsaSeed.xSearchS4.dthCut = 0.08;
     
-    Tf__Tsa__Seed("TsaSeed").xSearchS0.x0Cut = 400.;
-    Tf__Tsa__Seed("TsaSeed").xSearchS1.x0Cut = 400.;
-    Tf__Tsa__Seed("TsaSeed").xSearchS2.x0Cut = 400.;
-    Tf__Tsa__Seed("TsaSeed").xSearchS3.x0Cut = 400.;
-    Tf__Tsa__Seed("TsaSeed").xSearchS4.x0Cut = 400.;
+    tsaSeed.xSearchS0.x0Cut = 400.;
+    tsaSeed.xSearchS1.x0Cut = 400.;
+    tsaSeed.xSearchS2.x0Cut = 400.;
+    tsaSeed.xSearchS3.x0Cut = 400.;
+    tsaSeed.xSearchS4.x0Cut = 400.;
     
-    Tf__Tsa__Seed("TsaSeed").xSearchS0.sxCut = 0.10;
-    Tf__Tsa__Seed("TsaSeed").xSearchS1.sxCut = 0.10;
-    Tf__Tsa__Seed("TsaSeed").xSearchS2.sxCut = 0.10;
-    Tf__Tsa__Seed("TsaSeed").xSearchS0.sx2Cut = 0.08; 
-    Tf__Tsa__Seed("TsaSeed").xSearchS1.sx2Cut = 0.08;
-    Tf__Tsa__Seed("TsaSeed").xSearchS2.sx2Cut = 0.08;
+    tsaSeed.xSearchS0.sxCut = 0.10;
+    tsaSeed.xSearchS1.sxCut = 0.10;
+    tsaSeed.xSearchS2.sxCut = 0.10;
+    tsaSeed.xSearchS0.sx2Cut = 0.08; 
+    tsaSeed.xSearchS1.sx2Cut = 0.08;
+    tsaSeed.xSearchS2.sx2Cut = 0.08;
     
-    Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__StubFind, "stubFind")
-    Tf__Tsa__Seed("TsaSeed").addTool(Tf__Tsa__StubExtender, "stubExtender")
-    Tf__Tsa__Seed("TsaSeed").stubFind.sxCut = 0.1;
-    Tf__Tsa__Seed("TsaSeed").stubFind.dAngle = 0.08;
-    Tf__Tsa__Seed("TsaSeed").stubExtender.y0Cut1 = 100.;
-    Tf__Tsa__Seed("TsaSeed").stubExtender.txCut = 0.1;
-    Tf__Tsa__Seed("TsaSeed").stubExtender.tyCut = 0.1;
+    tsaSeed.addTool(Tf__Tsa__StubFind, "stubFind")
+    tsaSeed.addTool(Tf__Tsa__StubExtender, "stubExtender")
+    tsaSeed.stubFind.sxCut = 0.1;
+    tsaSeed.stubFind.dAngle = 0.08;
+    tsaSeed.stubExtender.y0Cut1 = 100.;
+    tsaSeed.stubExtender.txCut = 0.1;
+    tsaSeed.stubExtender.tyCut = 0.1;
       
   
