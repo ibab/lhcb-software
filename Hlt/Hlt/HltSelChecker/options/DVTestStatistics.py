7a2e18e7bce20ef2c5d4be8f7476655e3bef8f6f
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-10-23 18:21:32 +0000

    remove file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@73829 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5b4249325b98b60ed9797e824259b0d80acc8167

--- Hlt/Hlt/HltSelChecker/options/DVTestStatistics.py
+++ Hlt/Hlt/HltSelChecker/options/DVTestStatistics.py
@@ -7,8 +7,40 @@
 from Gaudi.Configuration import *
 from Configurables import Hlt2Statistics, CheckSelResult, EventTuple, TupleToolGeneration, TupleToolTrigger, L0Filter
 
-importOptions("$HLTSELECTIONSROOT/options/DVTestHlt2.py")
-
+from Gaudi.Configuration import *
+from Configurables import HistogramPersistencySvc, ReadHltSummary
+importOptions ("$DAVINCIROOT/options/DaVinciCommon.opts")
+########################################################################
+#
+# Trigger. Uncomment what you need. Hlt1 needs L0, Hlt2 doesn't.
+#
+from HltConf.Configuration import *
+#
+## enable if you want to rerun L0
+HltConf().replaceL0BanksWithEmulated = True
+#
+## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
+#HltConf().hltType = 'Hlt1'
+#HltConf().hltType = 'Hlt2'
+#HltConf().hltType = 'Hlt1+Hlt2'
+HltConf().hltType = 'Hlt1|Hlt2'
+## don't forget to actually apply the configuration!!!
+HltConf().applyConf()
+##################################################################
+#
+# Now require L0, not Hlt1 in Hlt2
+#
+GaudiSequencer("Hlt2CheckHlt1Passed").Members =[ L0Filter() ] 
+##################################################################
+#
+# to get Hlt2 correlations
+#
+importOptions( "$HLTSELECTIONSROOT/options/Hlt2Correlations.py")
+##################################################################
+#
+# to get Hlt2 moniror plots
+#
+#importOptions( "$HLTSELECTIONSROOT/options/Hlt2MonitorPlots.py")
 # sequencer
 statseq = GaudiSequencer("StatisticsSeq")
 ApplicationMgr().TopAlg += [ statseq ]
@@ -18,30 +50,25 @@ filter = CheckSelResult("OnlyHlt2")
 filter.Algorithms = [ "Hlt2Decision" ]
 # statseq.Members += [ filter ]
 
-
 # make Hlt2 statistics
-statseq.Members += [L0Filter(), Hlt2Statistics() ]
+statseq.Members += ["L0Filter", "Hlt2Statistics" ]
 
 # add event-info tuple
 tuple = EventTuple("TriggerTuple")
 statseq.Members += [tuple ]
 tuple.ToolList = [ "TupleToolTrigger", "TupleToolEventInfo" , "TupleToolGeneration" ]
 tuple.addTool( TupleToolTrigger() )
+tuple.TupleToolTrigger.VerboseHlt1 = TRUE
 tuple.TupleToolTrigger.VerboseHlt2 = TRUE
-#tuple.TupleToolTrigger.OutputLevel = 1
 #tuple.addTool( TupleToolGeneration() )
 #tuple.TupleToolGeneration.OutputLevel = 1
 #tuple.OutputLevel = 1
 
 
 # save tuple
-NTupleSvc().Output = ["FILE1 DATAFILE='Hlt12StatsTuple.root' TYP='ROOT' OPT='NEW'"]
+NTupleSvc().Output = ["FILE1 DATAFILE='Hlt12-StatsTuple.root' TYP='ROOT' OPT='NEW'"]
 
+ApplicationMgr().EvtMax = -1
 
-importOptions("$HLTSELECTIONSROOT/options/MinimumBiasDst.py")
-
-ApplicationMgr().EvtMax = 1000
-
-EventSelector().PrintFreq = 100
-
+EventSelector().PrintFreq = 1000 
 
