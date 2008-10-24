578dca4d1944a17f805e07fd7f8558430be78f9d
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2008-10-24 15:58:58 +0000

    use HltSys
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@73844 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 80f468e768a2693ef365278d48804ca4d9086314

---
+++ DaVinci/Phys/DaVinci/options/DaVinci-2008.py
@@ -0,0 +1,12 @@
+########################################################################
+#
+# $Id: DaVinci-2008.py,v 1.1 2008-10-24 15:58:58 pkoppenb Exp $
+#
+# Options for a typical DaVinci job - this is to please Dirac
+#
+# @author Patrick Koppenburg
+# @date 2008-10-24
+#
+########################################################################
+from Gaudi.Configuration import *
+importOptions( "$DAVINCIOPTS/DaVinci.py" )
