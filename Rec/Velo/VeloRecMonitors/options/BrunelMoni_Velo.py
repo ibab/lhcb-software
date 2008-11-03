54b30c35ad7669de203fee5c22a1f812e8b687dc
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2008-11-03 14:36:47 +0000

    example Brunel options included in options file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@24690 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 28b7b1974ca1c5da6107ad676a38b95bd844a3d9

--- Rec/Velo/VeloRecMonitors/options/BrunelMoni_Velo.py
+++ Rec/Velo/VeloRecMonitors/options/BrunelMoni_Velo.py
@@ -14,4 +14,16 @@ GaudiSequencer( 'MoniVELOSeq' ).Members +=\
                   , Velo__VeloTrackMonitor  ( 'VeloTrackMonitor'   )
                   ]
 
+#TrackUser = Velo__VeloTrackMonitor( 'VeloTrackMonitor' )
+#TrackUser.TrackLocation     = "Rec/Track/Best"
+#TrackUser.OutputLevel       = 2
+#TrackUser.HitMapHistos      = True
+#TrackUser.ProfileResetEvent = 1000
+#TrackUser.UnbiasedResiduals = True
+#TrackUser.BiasedResiduals   = True
+#TrackUser.ExtraPlots        = True
+#TrackUser.EventClusterInfo  = True
+#TrackUser.ACDCGeometry      = True
+#To get unbiased residuals only, change the TrackLocation to "Rec/Track/Best"
+
 ##############################################################################
