2b070f08cc62c0ec4ed61177e4f4cb014f04a5aa
jpalac <jpalac@cern.ch>
2008-11-04 11:21:36 +0000

    Add a little bit of functionality
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@73926 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 664eddc18c542ad04971ef06e967867da7482ecd

--- DaVinci/Phys/DaVinci/python/DaVinci/Configuration.py
+++ DaVinci/Phys/DaVinci/python/DaVinci/Configuration.py
@@ -3,7 +3,7 @@ High level configuration tools for DaVinci
 At the moment this doesn't do anything. Waiting for re-structuring of
 configuratables.
 """
-__version__ = "$Id: Configuration.py,v 1.1 2008-11-04 10:24:41 jpalac Exp $"
+__version__ = "$Id: Configuration.py,v 1.2 2008-11-04 11:21:36 jpalac Exp $"
 __author__ == "Juan Palacios <juan.palacios@nikhef.nl>"
 
 from LHCbKernel.Configuration import *
@@ -11,9 +11,43 @@ from GaudiConf.Configuration import *
 
 class DaVinci(LHCbConfigurableUser) :
     __slots__ = {
-        "EvtMax"     :  -1, # Maximum number of events to process
-        "skipEvents" :   0,     # events to skip
-        "mainOpts"   : '$DAVINCIROOT/options/DaVinci.py',
-        "DDDBtag"    : 'DC06-default', #
-        "CondDBtag"  : 'DC06-default'
+        "EvtMax"          :  -1, # Maximum number of events to process
+        "skipEvents"      :   0,     # events to skip
+        "mainOpts"        : '$DAVINCIROOT/options/DaVinci.py',
+        "DDDBtag"         : 'DC06-default', #
+        "CondDBtag"       : 'DC06-default',
+        "useOracleCondDB" : False
         }
+
+    def defineEvents(self):
+        evtMax = self.getProp("EvtMax")
+        if hasattr(LHCbApp(),"EvtMax"):
+            print "# LHCbApp().EvtMax already defined, ignoring Boole().EvtMax"
+        else:
+            LHCbApp().EvtMax = evtMax
+
+        skipEvents = self.getProp("skipEvents")
+        if skipEvents > 0 :
+            if hasattr(LHCbApp(),"skipEvents"):
+                print "# LHCbApp().skipEvents already defined, ignoring Boole().skipEvents"
+            else:
+                LHCbApp().skipEvents = skipEvents
+
+    def defineDB(self):
+        # Prefer DaVinci default over LHCbApp default if not set explicitly
+        self.setProp( "condDBtag", self.getProp("condDBtag") )
+        self.setProp( "DDDBtag", self.getProp("DDDBtag") )
+        # Delegate handling to LHCbApp configurable
+        self.setOtherProps(LHCbApp(),["condDBtag","DDDBtag","useOracleCondDB"]) 
+
+    def hepMCBackComp(self)
+        # Special options for DC06 data processing
+        if LHCbApp().getProp("DDDBtag").find("DC06") != -1 :
+            ApplicationMgr().Dlls += [ "HepMCBack" ]
+
+    def applyConf(self):
+        GaudiKernel.ProcessJobOptions.PrintOff()
+        importOptions( self.getProp( "mainOptions" ) )
+        self.defineEvents()
+        self.hepMCBack()
+        LHCbApp().applyConf()
