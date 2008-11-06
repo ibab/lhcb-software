60a0123c7f9fac3ca8911b65f5e80e67da140e4d
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-11-06 14:45:05 +0000

    fix for gcc 4.3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10292 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e01e5a03736c9f2505c40e1aa6ee942d46a5fd66

--- Rec/Rich/RichGlobalPID/src/RichGlobalPIDCommonConstructors.cpp
+++ Rec/Rich/RichGlobalPID/src/RichGlobalPIDCommonConstructors.cpp
@@ -5,7 +5,7 @@
  *  Implementation of templated constructors for Rich::Rec::GlobalPID::CommonBase
  *
  *  CVS Log :-
- *  $Id: RichGlobalPIDCommonConstructors.cpp,v 1.2 2008-07-23 12:51:13 jonrob Exp $
+ *  $Id: RichGlobalPIDCommonConstructors.cpp,v 1.3 2008-11-06 14:45:05 cattanem Exp $
  *
  *  @author Chris Jones    Christopher.Rob.Jones@cern.ch
  *  @date   2004-09-18
@@ -19,42 +19,48 @@
 //=============================================================================
 // Implementation for algorithms
 //=============================================================================
-template <>
-Rich::Rec::GlobalPID::CommonBase<Rich::Rec::AlgBase>::CommonBase( const std::string& name,
-                                                                  ISvcLocator* pSvcLocator )
-  : Rich::Rec::AlgBase ( name, pSvcLocator )
-{
-  this->initConstructor();
-}
-template <>
-Rich::Rec::GlobalPID::CommonBase<Rich::Rec::AlgBase>::CommonBase( const std::string& /* type */,
-                                                                  const std::string& /* name */,
-                                                                  const IInterface* /* parent */ )
-  : Rich::Rec::AlgBase ( "ERROR", NULL )
-{
-  throw GaudiException( "Invalid Rich::Rec::GlobalPID::CommonBase<Rich::Rec::AlgBase> constructor",
-                        "Rich::Rec::GlobalPID::CommonBase", StatusCode::FAILURE );
-}
+namespace Rich {
+  namespace Rec {
+    namespace GlobalPID {
+      template <>
+      CommonBase<Rich::Rec::AlgBase>::CommonBase( const std::string& name,
+                                                  ISvcLocator* pSvcLocator )
+      : Rich::Rec::AlgBase ( name, pSvcLocator )
+      {
+        this->initConstructor();
+      }
+      template <>
+      CommonBase<Rich::Rec::AlgBase>::CommonBase( const std::string& /* type */,
+                                                  const std::string& /* name */,
+                                                  const IInterface* /* parent */ )
+        : Rich::Rec::AlgBase ( "ERROR", NULL )
+      {
+        throw GaudiException( "Invalid Rich::Rec::GlobalPID::CommonBase<Rich::Rec::AlgBase> constructor",
+                              "Rich::Rec::GlobalPID::CommonBase", StatusCode::FAILURE );
+    }
 //=============================================================================
 
 //=============================================================================
 // Implementation for tools
 //=============================================================================
-template <>
-Rich::Rec::GlobalPID::CommonBase<Rich::Rec::ToolBase>::CommonBase( const std::string& type,
-                                                                   const std::string& name,
-                                                                   const IInterface* parent )
-  : Rich::Rec::ToolBase ( type, name, parent )
-{
-  this->initConstructor();
-}
-template <>
-Rich::Rec::GlobalPID::CommonBase<Rich::Rec::ToolBase>::CommonBase( const std::string& /* name */,
-                                                                   ISvcLocator* /* pSvcLocator */ )
-  : Rich::Rec::ToolBase ( "ERROR", "ERROR", NULL )
-{
-  throw GaudiException( "Invalid Rich::Rec::GlobalPID::CommonBase<Rich::Rec::ToolBase> constructor",
-                        "Rich::Rec::GlobalPID::CommonBase", StatusCode::FAILURE );
-}
+      template <>
+      CommonBase<Rich::Rec::ToolBase>::CommonBase( const std::string& type,
+                                                   const std::string& name,
+                                                   const IInterface* parent )
+        : Rich::Rec::ToolBase ( type, name, parent )
+      {
+        this->initConstructor();
+      }
+      template <>
+      CommonBase<Rich::Rec::ToolBase>::CommonBase( const std::string& /* name */,
+                                                   ISvcLocator* /* pSvcLocator */ )
+        : Rich::Rec::ToolBase ( "ERROR", "ERROR", NULL )
+      {
+        throw GaudiException( "Invalid Rich::Rec::GlobalPID::CommonBase<Rich::Rec::ToolBase> constructor",
+                              "Rich::Rec::GlobalPID::CommonBase", StatusCode::FAILURE );
+      }
+    } // namespace GlobalPID
+  } // namespace Rec
+} // namespace Rich
 //=============================================================================
 
