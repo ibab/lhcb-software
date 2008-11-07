2c7b520b7cadac35040ddb641fdbb094c2a21376
jpalac <jpalac@cern.ch>
2008-11-07 17:04:20 +0000

    . Completely re-written in python. Only .opts used is preselection file itself.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@73948 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4723a0f7124f6cbc5ffea4772f2f90fd981dfd5

--- DaVinci/Phys/DaVinci/tests/options/DVTestB2DiMuon.py
+++ DaVinci/Phys/DaVinci/tests/options/DVTestB2DiMuon.py
@@ -1,23 +1,101 @@
 ##############################################################################
-#$Id: DVTestB2DiMuon.py,v 1.1 2008-11-05 14:49:49 jpalac Exp $
+#$Id: DVTestB2DiMuon.py,v 1.2 2008-11-07 17:04:20 jpalac Exp $
 #
 # Example Qm test option using configurables.
 #
 # Author: Juan Palacios <juan.palacios@nikhef.nl>
 #
 ##############################################################################
+from GaudiConf.Configuration import *
 from DaVinci.Configuration import DaVinciApp
-DaVinciApp().EvtMax = 500
+##############################################################################
+DaVinciApp().EvtMax = 1000
 # DaVinciApp().DDDBtag      = "DC06-default"
 # DaVinciApp().condDBtag    = "DC06-default"
 # DaVinciApp().useOracleCondDB = False
-DaVinciApp().mainOptions  = "$DAVINCIROOT/tests/options/DVTestB2DiMuon.opts"
+#DaVinciApp().mainOptions  = "$DAVINCIROOT/tests/options/DVTestB2DiMuon.opts"
+DaVinciApp().mainOptions  = "$B2DILEPTONROOT/options/DVPreselB2DiMuon.opts"
+##############################################################################
+"""
+ GAUDI data cards generated on 11/6/08 4:52 PM
+ For Event Type = 11144103 / Data type = DST 1
+     Configuration = DC06 - phys-v2-lumi2
+     DST 1 datasets output 
+     From DIGI 1 datasets produced by Boole - v12r10
+     From SIM 1 datasets produced by ANY
+     Database version = v30r14
+     Cards content = physical-physical
+     
+ Datasets replicated at CERN
+ 200 dataset(s) - NbEvents = 99391
+"""
 DaVinciApp().Input = [
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-lumi2/00001586/DST/0000/00001586_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-lumi2/00001586/DST/0000/00001586_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-lumi2/00001586/DST/0000/00001586_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-lumi2/00001586/DST/0000/00001586_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-lumi2/00001586/DST/0000/00001586_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000007_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000008_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001668/DST/0000/00001668_00000010_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+]
 ##############################################################################
 DaVinciApp().applyConf()
 ##############################################################################
+from Configurables import GaudiSequencer, PhysDesktop, MakeResonances, OldFilterDesktop, TrueMCFilterCriterion, MCDecayFinder, AlgorithmCorrelationsAlg, AlgorithmCorrelations
+
+decayDescriptor = "J/psi(1S) -> mu- mu+"
+mcDecayDescriptor = "J/psi(1S) => ^mu+ ^mu- {,gamma}{,gamma}{,gamma}{,gamma}"
+trueNoPIDsName = "SelectTrueNoPIDsDecay"
+trueLooseName = "SelectTrueLooseDecay"
+# Truth checking for Bd -> MuMuX
+ApplicationMgr().TopAlg += [ GaudiSequencer("FindTrueDecay") ]
+findTrueDecay = GaudiSequencer("FindTrueDecay")
+findTrueDecay.IgnoreFilterPassed = True
+findTrueDecay.Members += [ OldFilterDesktop("SelectTrueNoPIDsDecay") ]
+
+# Truth filter for Bd -> MuMuK* Loose PID : copy of above with NoPIDs -> Loose
+MCTruthFilter = TrueMCFilterCriterion("Filter")
+MCTruthFilter.addTool(MCDecayFinder(), name="MCDecayFinder")
+MCTruthFilter.MCDecayFinder.Decay = mcDecayDescriptor
+
+selectTrueNoPIDsDecay = OldFilterDesktop(trueNoPIDsName)
+selectTrueNoPIDsDecay.addTool( PhysDesktop() )
+selectTrueNoPIDsDecay.PhysDesktop.InputLocations = [ "Phys/StdDC06NoPIDsMuons" ]
+selectTrueNoPIDsDecay.addTool( MCTruthFilter, name="FilterCriterion" )
+
+allTrueNoPIDsDecay = MakeResonances("AllTrueNoPIDsDecay")
+allTrueNoPIDsDecay.addTool( PhysDesktop() )
+allTrueNoPIDsDecay.PhysDesktop.InputLocations = [trueNoPIDsName]
+allTrueNoPIDsDecay.DecayDescriptor = decayDescriptor
+findTrueDecay.Members += [ allTrueNoPIDsDecay ]
+
+selectTrueLooseDecay = OldFilterDesktop(trueLooseName)
+selectTrueLooseDecay.addTool( PhysDesktop() )
+selectTrueLooseDecay.PhysDesktop.InputLocations = [ "Phys/StdDC06LooseMuons" ]
+selectTrueLooseDecay.addTool( MCTruthFilter, name = "FilterCriterion" )
+findTrueDecay.Members += [ selectTrueLooseDecay ]
+
+allTrueLooseDecay = MakeResonances("AllTrueLooseDecay")
+allTrueLooseDecay.addTool( PhysDesktop() )
+allTrueLooseDecay.PhysDesktop.InputLocations = [trueLooseName]
+allTrueLooseDecay.DecayDescriptor = decayDescriptor
+findTrueDecay.Members += [ allTrueLooseDecay ]
+
+# Correlations
+testCorrelations = AlgorithmCorrelationsAlg("TestCorrelations")
+testCorrelations.addTool(AlgorithmCorrelations())
+testCorrelations.AlgorithmCorrelations.OnlyNonZero = False
+testCorrelations.Algorithms = [ trueNoPIDsName,
+                                "AllTrueNoPIDsDecay",
+                                trueLooseName,
+                                "AllTrueLooseDecay",
+                                "PreselB2DiMuon"]
+ApplicationMgr().TopAlg += [ testCorrelations ]
+##############################################################################
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+EventSelector().PrintFreq = 100
+##############################################################################
+print "How do I run this???"
