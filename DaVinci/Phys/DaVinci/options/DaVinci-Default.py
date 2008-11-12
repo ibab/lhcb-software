10a151c2d125bb7634b1ccf71f465ffbeeabd0cb
jpalac <jpalac@cern.ch>
2008-11-12 14:59:14 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@74000 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5e259b4bb992c1d5e7b121e4808792821fa2d510

--- DaVinci/Phys/DaVinci/options/DaVinci-Default.py
+++ DaVinci/Phys/DaVinci/options/DaVinci-Default.py
@@ -1,5 +1,5 @@
 ##############################################################################
-#$Id: DaVinci-Default.py,v 1.1 2008-11-05 14:50:06 jpalac Exp $
+#$Id: DaVinci-Default.py,v 1.2 2008-11-12 14:59:14 jpalac Exp $
 #
 # syntax: gaudirun.py DaVinci-Default.py
 #
@@ -17,6 +17,6 @@ DaVinciApp().EvtMax = 500
 # DaVinciApp().condDBtag    = "DC06-default"
 # DaVinciApp().useOracleCondDB = False
 # DaVinciApp().mainOptions  = "$DAVINCIROOT/options/DaVinci.opts"
-DaVinciApp().mainOptions  = "$DAVINCIROOT/tests/options/DVTestB2DiMuon.py"
+DaVinciApp().mainOptions  = "$DAVINCIROOT/tests/options/DVTestB2DiMuon.opts"
 DaVinciApp().applyConf()
 ##############################################################################
