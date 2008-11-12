d014616116efe948c220b9bd00327cc2016acd86
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-11-12 17:43:18 +0000

    updates for new configurables
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@60087 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ff26ed399061f32de833d8da0779810e5b6ae87

--- Boole/BooleSys/tests/options/testBoole.py
+++ Boole/BooleSys/tests/options/testBoole.py
@@ -5,19 +5,13 @@
 from Boole.Configuration import *
 from Configurables import CondDBAccessSvc
 
-Boole().EvtMax       = 0
-Boole().DDDBtag      = "2008-default"
-Boole().condDBtag    = "2008-default"
-Boole().writeL0ETC   = True   # Write also ETC of L0 selected events
-
-Boole().applyConf()
+Boole().EvtMax   = 0
+Boole().Outputs  = ["DIGI","MDF","L0ETC"] # Test all output types
+Boole().Histograms = "NONE"
 
 # Do not compare times....
 TimingAuditor().OutputLevel = WARNING 
 
 # Ignore changes to database tags
 CondDBAccessSvc('DDDB').OutputLevel = WARNING
-CondDBAccessSvc('SIMCOND').OutputLevel = WARNING
-
-# Switch off output
-ApplicationMgr().OutStream = []
+CondDBAccessSvc('LHCBCOND').OutputLevel = WARNING
