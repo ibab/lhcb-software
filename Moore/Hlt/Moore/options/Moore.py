190fce34abd9f791eacb94ee8a72a982a608ac65
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-11-13 14:28:43 +0000

    update Moore.py to something that works out-of-the-box at CERN
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@74016 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bb3b060ac397e5cb96a8d00f1aa5a35b65ca62c5

--- Moore/Hlt/Moore/options/Moore.py
+++ Moore/Hlt/Moore/options/Moore.py
@@ -14,8 +14,7 @@ from Moore.Configuration import *
 #// Histogram output
 #//---------------------------------------------------------------------------
 ApplicationMgr().HistogramPersistency = 'ROOT'
-HistogramPersistencySvc().OutputFile = 'Moore_minbias.root'
-
+#HistogramPersistencySvc().OutputFile = 'Moore_minbias.root'
 #---------------------------------------------------------------------------
 # Number of events to process, optionally skipping some events
 #---------------------------------------------------------------------------
@@ -24,14 +23,13 @@ HistogramPersistencySvc().OutputFile = 'Moore_minbias.root'
 #Moore().generateConfig = True
 #Moore().configLabel = 'NO prescale'
 
-#Moore().hltType = 'PA+LU+VE'
-Moore().hltType = 'PA+VE+LU+MU+HA'
-#Moore().hltType = 'PA'
+Moore().hltType = 'Hlt1'
 Moore().oldStyle = False
+Moore().verbose = True
 
 
-files= [ '/data/bfys/lhcb/MinBias-L0strip/DC06_L0_v1_lumi2_MuonHadron_40000ev_' + str(f) +'.mdf'  for f in range(1,3) ]
-files= [ '/data/bfys/lhcb/data/2008/RAW/LHCb/PHYSICS_COSMICS/27804/027804_0000063303.raw' ]
+#files= [ '/data/bfys/lhcb/MinBias-L0strip/DC06_L0_v1_lumi2_MuonHadron_40000ev_' + str(f) +'.mdf'  for f in range(1,3) ]
+#files= [ '/data/bfys/lhcb/data/2008/RAW/LHCb/PHYSICS_COSMICS/27804/027804_0000063303.raw' ]
 
 #files = [ '/data/bfys/lhcb/MinBias-L0strip/MBL0-lumi2-' + str(f) +'.dst'  for f in range(1,5) ]
 
@@ -40,8 +38,8 @@ files= [ '/data/bfys/lhcb/data/2008/RAW/LHCb/PHYSICS_COSMICS/27804/027804_000006
 #          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-3.dst',
 #          '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-4.dst' ]
 
-#files = [ 'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_1.mdf',
-#          'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_2.mdf' ]
+files = [ 'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_1.mdf',
+          'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_2.mdf' ]
 
 
 filetype = files[0][-3:].upper()
@@ -50,7 +48,6 @@ extensions = { 'RAW' : "' SVC='LHCb::MDFSelector'",
                'MDF' : "' SVC='LHCb::MDFSelector'",
                'DST' : "' TYP='POOL_ROOTTREE' OPT='READ'" }
 EventSelector().Input = [ "DATAFILE='PFN:"+ f + extensions[ filetype ] for f in files ]
-
 EventSelector().PrintFreq = 100
 
 Moore().EvtMax = 10000
