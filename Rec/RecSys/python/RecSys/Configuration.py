5283d00017179304bb9a6a900c0fc34b5fc0e4cb
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2008-11-14 17:12:05 +0000

    ! 2008-11-14 - Chris Jones  - Fixes to python Configurable for latest Gaudi changes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@30338 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5269eac62007268be3edca0f761e4db730cab884

--- Rec/RecSys/python/RecSys/Configuration.py
+++ Rec/RecSys/python/RecSys/Configuration.py
@@ -4,19 +4,22 @@
 #  @author Marco Cattaneo <Marco.Cattaneo@cern.ch>
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.3 2008-10-21 19:34:43 jonrob Exp $"
+__version__ = "$Id: Configuration.py,v 1.4 2008-11-14 17:11:19 jonrob Exp $"
 __author__  = "Marco Cattaneo <Marco.Cattaneo@cern.ch>"
 
 from LHCbKernel.Configuration import *
 from TrackSys.Configuration   import *
 from RichRecSys.Configuration import *
-from Configurables import ( ProcessPhase, PatPVOffline )
+from Configurables import ProcessPhase
 
 ## @class RecSysConf
 #  Configurable for LHCb reconstruction
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   15/08/2008
 class RecSysConf(LHCbConfigurableUser):
+
+    ## Possible used Configurables
+    __used_configurables__ = [ TrackSys, RichRecSysConf ]
 
     ## Default tracking Sub-detector processing sequence
     DefaultTrackingSubdets = ["VELO","TT","IT","OT","Tr","Vertex"]
@@ -29,52 +32,48 @@ class RecSysConf(LHCbConfigurableUser):
     
     ## Steering options
     __slots__ = {
-        "recoSequence" : []      # The Sub-detector sequencing. Default is all known
-       ,"specialData"  : []      # Various special data processing options. See KnownSpecialData for all options
-       ,"expertHistos":  False   # set to True to write out expert histos
-       ,"context":     "Offline" # The context within which to run the reco sequences
+        "RecoSequence" : []      # The Sub-detector sequencing. Default is all known
+       ,"SpecialData"  : []      # Various special data processing options. See KnownSpecialData for all options
+       ,"ExpertHistos":  False   # set to True to write out expert histos
+       ,"Context":     "Offline" # The context within which to run the reco sequences
         }
 
     ## Apply the configuration
     def applyConf(self):
 
         # Check the special data options
-        for option in self.getProp("specialData"):
+        for option in self.getProp("SpecialData"):
             if option not in self.KnownSpecialData:
                 raise RuntimeError("Unknown SpecialData option '%s'"%option)
         
         # Phases
-        if len(self.getProp("recoSequence")) == 0 :
-            if "fieldOff" in self.getProp("specialData"):
-                self.setProp("recoSequence",self.DefaultSubDetsFieldOff)
+        if len(self.getProp("RecoSequence")) == 0 :
+            if "fieldOff" in self.getProp("SpecialData"):
+                self.setProp("RecoSequence",self.DefaultSubDetsFieldOff)
             else:
-                self.setProp("recoSequence",self.DefaultSubDetsFieldOn)
-        recoSeq = self.getProp("recoSequence")
+                self.setProp("RecoSequence",self.DefaultSubDetsFieldOn)
+        recoSeq = self.getProp("RecoSequence")
         ProcessPhase("Reco").DetectorList += recoSeq
 
         # Primary Vertex
         if "Vertex" in recoSeq:
+            from Configurables import PatPVOffline
             GaudiSequencer("RecoVertexSeq").Members += [ PatPVOffline() ];
-     
+
         # Tracking (Should make it more fine grained ??)
         DoTracking = False
         for seq in self.DefaultTrackingSubdets:
             if seq in recoSeq: DoTracking = True
         if DoTracking:
             trackConf = TrackSys()
-            self.setOtherProps(trackConf,["expertHistos","specialData"])
-            trackConf.applyConf()
-            # delete config object (to allow reuse from defaults)
-            del trackConf
+            self.setOtherProps(trackConf,["ExpertHistos","SpecialData"])
 
         # RICH
         if "RICH" in recoSeq:
             richConf = RichRecSysConf()
-            self.setOtherProps(richConf,["expertHistos","specialData","context"])
-            richConf.applyConf(GaudiSequencer("RecoRICHSeq"))
-            # delete config object (to allow reuse from defaults)
-            del richConf
-
+            self.setOtherProps(richConf,["SpecialData","Context"])
+            richConf.RecoSequencer = GaudiSequencer("RecoRICHSeq")
+            
         # CALO
         if "CALO" in recoSeq:
             importOptions("$CALORECOOPTS/Brunel.opts")
