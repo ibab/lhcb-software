52f4861f0a963611d1eb90c1e1e8a5c2acf0e0b2
psomogyi <psomogyi@cern.ch>
2008-11-17 13:16:55 +0000

    grinder
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98780 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 95dae9000190e0c1b425c26afe091b7650c7a7e8

--- Online/Online/Presenter/test/grinder.sh
+++ Online/Online/Presenter/test/grinder.sh
@@ -1,1 +1,1 @@
- valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --num-callers=1000 --log-file=/tmp/presenterGrinderTrace.log $PRESENTERROOT/$CMTCONFIG/presenter.exe --login no
+ valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --num-callers=1000 --log-file=/tmp/presenterGrinderTrace.log $PRESENTERROOT/$CMTCONFIG/presenter.exe --login no -M history
