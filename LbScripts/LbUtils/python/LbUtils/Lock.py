7551a3fb6a3cfcaa8fa2145ddb8cab295f1d1383
kkruzele <kkruzele@cern.ch>
2008-11-17 12:28:13 +0000

    added optional parameter to specify the directory for lockfiles to be saved in (default: current working directory)
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13353 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbUtils/python/LbUtils/Lock.py
+++ LbScripts/LbUtils/python/LbUtils/Lock.py
@@ -11,18 +11,19 @@ def _hash(value):
     return md5.new(value).hexdigest()
 
 class Lock(object):
-    """ Lock(command, uniqId[, commonId]) -> (not important)
+    """ Lock(command, uniqId[, commonId, [lockfilePath]]) -> (not important)
 
-        Object of the class is created to run a <command> and to be sure that nobody else runs the same <command> from the same CWD at the same time.
+        Object of the class is created to run a <command> and to be sure that nobody else runs the same <command> from the same CWD (or any other, specified directory) at the same time.
         Constructor of the class has 2 obligatory parameters:
         - <command> to run
         - <uniqId> of the launcher
-        and two optional parameters:
+        and three optional parameters:
         - <commonId> which is a key to make other processes with the same <commonId> parameter wait until the first one finishes.
         If <commonId> is not given, it is created on the base of <command>
         - <environment> is a dictionary with a values to set into working environment before <command> launch
+        - <lockPath> is a directory where the lockfiles should be stored
     """
-    def __init__(self, command, uniqId, commonId=None, environment=None):
+    def __init__(self, command, uniqId, commonId=None, environment=None, lockPath=None):
         """ object constructor. Parameters described in more details in the __class__.__doc__. """
         if commonId == None: self._hashCmd = _hash(command) # check if it's None - '' is different than None so '== None' is necessary
         else: self._hashCmd = _hash(commonId)
@@ -31,6 +32,8 @@ class Lock(object):
         self._hashUniqId = _hash(uniqId)
         self._lockFileName = '.lock_' + self._hashCmd
         self._env = environment
+        if lockPath is None: self._dir = os.getcwd()
+        else: self._dir = lockPath
         while True:
             c = self._checkLock()
             if c == self._hashUniqId:
@@ -52,9 +55,9 @@ class Lock(object):
             time.sleep(5)
     def _checkLock(self):
         """ checks if lockfile for the given commands exists, and if so returns the hash value of the launcher unique id """
-        if not os.path.exists(self._lockFileName): return None
+        if not os.path.exists(os.path.join(self._dir, self._lockFileName)): return None
         else:
-            lock = file(self._lockFileName, 'r')
+            lock = file(os.path.join(self._dir, self._lockFileName), 'r')
             lockData = lock.readlines()
             if len(lockData) < 3: # file currently in use by someone else, but not saved yet
                 return 'in use'
@@ -69,12 +72,12 @@ class Lock(object):
         - launcher unique id
         - result of hash function on the launcher unique id
         """
-        lock = file(self._lockFileName, 'w')
+        lock = file(ps.path.join(self._dir, self._lockFileName), 'w')
         lock.writelines([self._command, '\n', self._uniqId, '\n', _hash(self._uniqId)])
         lock.close()
     def _removeLock(self):
         """ removes the lockfile if it exists """
         try:
-            os.remove(self._lockFileName)
+            os.remove(os.path.join(self._dir, self._lockFileName))
         except OSError:
             pass
