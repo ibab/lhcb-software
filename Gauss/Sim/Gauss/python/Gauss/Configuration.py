814485b8c75e715fda280791f0e818f0d62261cc
Gloria Corti <gloria.corti@cern.ch>
2008-11-20 16:30:11 +0000

    add configurable for geometry
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93815 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 84ebd2741b3053a37b8db1415b5596e9ce5d8e44

--- Gauss/Sim/Gauss/python/Gauss/Configuration.py
+++ Gauss/Sim/Gauss/python/Gauss/Configuration.py
@@ -1,14 +1,14 @@
 """
 High level configuration tools for Gauss
 """
-__version__ = "$Id: Configuration.py,v 1.3 2008-10-08 16:52:38 gcorti Exp $"
+__version__ = "$Id: Configuration.py,v 1.4 2008-11-20 16:30:11 gcorti Exp $"
 __author__  = "Gloria Corti <Gloria.Corti@cern.ch>"
 
 from Gaudi.Configuration import *
+import GaudiKernel.ProcessJobOptions
 from GaudiKernel.SystemOfUnits import GeV, ns, ms, mrad
 from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime,
                             CondDBEntityResolver )
-from Configurables import GiGaGeo
-import GaudiKernel.ProcessJobOptions
 from Configurables import ( GenInit, Generation, MinimumBias, PythiaProduction,
                             CollidingBeams )
+from Configurables import ( GiGaGeo, GiGaInputStream )
