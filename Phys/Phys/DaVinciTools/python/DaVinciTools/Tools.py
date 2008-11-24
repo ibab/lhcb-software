eac999ded12f72267c6bf71cbea9b31dd001b214
jpalac <jpalac@cern.ch>
2008-11-24 14:28:14 +0000

    . Short names for tool typename strings needed to retrieve instances of     P2PVWithIP, P2PVWithIP, OnlineP2PVWithIP, and OnlineP2PVWithIP from     python AppMge().toolsvc().
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64453 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0351aeb73a2bb69aaa43647cfe7ea9009a892602

---
+++ Phys/Phys/DaVinciTools/python/DaVinciTools/Tools.py
@@ -0,0 +1,9 @@
+"""
+Shortcuts for long names of templated tools
+@author Juan Palacios juan.palacios@nikhef.nl
+@date 
+"""
+P2PVWithIP = 'GenericParticle2PVRelator__p2PVWithIP_OfflineDistanceCalculatorName_'
+P2PVWithIPChi2 = 'GenericParticle2PVRelator__p2PVWithIPChi2_OfflineDistanceCalculatorName_'
+OnlineP2PVWithIP = 'GenericParticle2PVRelator__p2PVWithIP_OnlineDistanceCalculatorName_'
+OnlineP2PVWithIPChi2 = 'GenericParticle2PVRelator__p2PVWithIPChi2_OnlineDistanceCalculatorName_'
