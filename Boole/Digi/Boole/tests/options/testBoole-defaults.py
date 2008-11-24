75271680a861a4832cc77ca360e78bc97e06798c
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-11-24 14:10:03 +0000

    last? fixes to refs for v17r0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@60155 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1fb7e184f8be9bd2512451a40813befedafd01aa

--- Boole/Digi/Boole/tests/options/testBoole-defaults.py
+++ Boole/Digi/Boole/tests/options/testBoole-defaults.py
@@ -1,14 +1,6 @@
 # Default settings for all Boole tests
 from Boole.Configuration import *
-from Configurables import CondDBAccessSvc
 
 Boole().EvtMax   = 10
 Boole().Outputs  = ["DIGI","MDF","L0ETC"] # Test all output types
 Boole().Monitors = ["SC","FPE"]           # Add StatusCode and FPE checks
-
-# Do not compare times....
-TimingAuditor().OutputLevel = WARNING 
-
-# Ignore changes to database tags
-CondDBAccessSvc('DDDB').OutputLevel = WARNING
-CondDBAccessSvc('LHCBCOND').OutputLevel = WARNING
