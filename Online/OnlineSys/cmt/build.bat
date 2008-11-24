20b8332e769ca994a4c6efe95ba0ccfe7d64a2ba
Markus Frank <frankb@cern.ch>
2008-11-24 09:02:52 +0000

    no message
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98815 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bfeaf941dfe7daef6bc674c71431b0efe786d0d2

--- Online/OnlineSys/cmt/build.bat
+++ Online/OnlineSys/cmt/build.bat
@@ -4,5 +4,5 @@ cmt broadcast if exist ..\Visual rd /q/s ..\Visual
 cmt broadcast if exist ..\%CMTCONFIG% rd /q/s ..\%CMTCONFIG%
 cmt broadcast cmt config
 cmt broadcast cmt build vsnet
-cmt broadcast nmake /f nmake > build_%CMTCONFIG%.log
+cmt broadcast cmt make > build_%CMTCONFIG%.log
 
