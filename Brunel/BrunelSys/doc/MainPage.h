5469325c856df7fa0e5933b3b0e134f8a9bb2874
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-12-01 17:25:46 +0000

    Update doxygen main page
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30368 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7ce595f6f98e0127261cd06273f7f578b99d184

--- Brunel/BrunelSys/doc/MainPage.h
+++ Brunel/BrunelSys/doc/MainPage.h
@@ -7,14 +7,34 @@
  * <a href=" http://cern.ch/lhcb-release-area/DOC/brunel/">Brunel project Web pages</a>
  *
  * <hr>
+ * <h2>Top level options files</h2>
+ * <h4>Options for default settings</h4>
+ * \li Brunel-Default.py:   default setting for real data (same as Brunel-2008.py)
+ * \li Brunel-Cosmics.py:   default setting for cosmics data 
+ * \li Brunel-Rdst.py:      default setting for real data Rdst production workflow 
+ * \li Brunel-Stripping.py: default setting for real data Stripping production workflow 
+ * <h4>Options for 2008 settings</h4>
+ * \li Brunel-2008.py:      default setting for 2008 real data 
+ * \li Brunel-2008-MC.py:   default setting for 2008 simulation 
+ * \li Brunel-2008-MC-VeloOpen.py: 2008 simulation with Velo open
+ * \li Brunel-2008-MC-BOff-VeloOpen.py: 2008 simulation with Velo open and B field off
+ * <h4>Options for CCRC settings</h4>
+ * \li Brunel-CCRCStripping.py: setting for CCRC Stripping production workflow 
+ * <h4>Options for DC06 settings</h4>
+ * \li Brunel-DC06.py:          default setting for DC06 simulated data
+ * \li Brunel-DC06Rdst.py:      setting for DC06 Rdst production workflow
+ * \li Brunel-DC06L0Rdst.py:    setting for DC06 Rdst production workflow, reconstructing only L0 yes events
+ * \li Brunel-DC06Stripping.py: setting for DC06 Stripping production workflow 
+ * <h4>Examples of input data</h4>
+ * \li 2008-TED-Data.py:    example 2008 real data files (TED runs) 
+ * \li 2008-Cosmic-Data.py: example 2008 cosmics data files 
+ * \li 2008-Files.py:       example 2008 simulation files 
+ * \li DC06-Files.py:       example DC06 simulation files 
+ * <h4>Special options for Dirac (obsolete?)</h4>
+ * \li Copy.opts: To make a copy of .dst files
+
+ * <hr>
  * \htmlinclude new_release.notes
 
- * <b>Top level options files</b>
- * \li DC06.opts: DC06 geometry, rDST
- * \li DC06-ETC.opts: DC06 geometry, full DST+ETC, starting from ETC input
- * \li v200601.opts: DC06 geometry, full DST
- * \li FieldOff.opts: DC06 geometry, field off
- * \li FieldOff-VeloOpen.opts: DC06 geometry, field off, %Velo Open
- * \li Copy.opts: To make a copy of .dst files
 
  */
