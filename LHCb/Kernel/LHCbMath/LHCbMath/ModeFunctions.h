ab9c4d7956a4c5ecc573a3116f348abb271d0b1e
Matthew David Needham <mneedham@cern.ch>
2008-12-01 16:05:44 +0000

    Add robust functions to calculate the mode
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48085 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6ec792485aee65189d8b17cef1795ce7d218add9

---
+++ LHCb/Kernel/LHCbMath/LHCbMath/ModeFunctions.h
@@ -0,0 +1,118 @@
+#ifndef _ModeFunctions_H
+#define _ModeFunctions_H 1
+
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <utility>
+
+
+/** @namespace ModeFunctions ModeFunctions.h "ModeFunctions.h
+ *
+ *  namespace containing fuctions for getting robust estimate of the mode
+ *  reasoanbly generic interface......
+ *
+ *  @author Matthew Needham Matthew.Needham@cern.ch
+ */
+
+namespace ModeFunctions{
+
+  /** calculate the shorth 
+  * the mean of the half sample 
+  * @param start beginning of sequence 
+  * @param stop  end of sequence
+  * @return double shorth
+  */
+  template <typename TYPE>
+    double shorth(TYPE start, TYPE stop);
+
+  /** calculate the LMS 
+  * average of the values of the half sample 
+  * @param start beginning of sequence 
+  * @param stop  end of sequence
+  * @return double LMS
+  */
+  template <typename TYPE>
+    double LMS(TYPE start, TYPE stop);
+
+ /** calculate the halfSample
+  * value of i for which distance x_i, x_i+h is minimum
+  * where h is the half sample 
+  * @param start beginning of sequence 
+  * @param stop  end of sequence
+  * @return TYPE iterator for starting position
+  */
+  template <typename TYPE>
+    std::pair<TYPE,TYPE> halfSample(TYPE start, TYPE stop);
+
+  /** <a href = "http://en.wikipedia.org/wiki/Harmonic_mean" > Generalized Mean </a>
+   * @param start beginning of sequence 
+   * @param stop  end of sequence
+   * @param power 
+   * @return double LMS
+   */
+   template <typename TYPE>
+    double generalizedMean(TYPE start, TYPE stop, double p = -0.5);
+}
+
+template <typename TYPE>
+inline std::pair<TYPE,TYPE> ModeFunctions::halfSample(TYPE start, TYPE stop){
+ const unsigned int halfSize = (unsigned int)((stop-start)/2u); 
+ unsigned int pos = 0u;
+ unsigned int bestpos = halfSize-1u;
+ for (TYPE iter = start; iter != stop ; ++iter, ++pos){
+   if (pos < halfSize){
+    if (*(iter+halfSize) - *iter < *(start+bestpos+halfSize) - *(start+bestpos) ) { 
+       bestpos = pos; 
+     }
+   }
+ } //iter1 
+ return std::make_pair(start+bestpos, start+bestpos+halfSize);
+}
+
+template<typename TYPE>
+inline double ModeFunctions::shorth(TYPE start, TYPE stop){
+   // trivial cases
+   if (start == stop) {
+    return 0.0;
+   }
+   else if (stop - start == 1) {
+     return *start;
+   } 
+   else if (stop - start == 2){
+     return (*start + *(start+1))/2u;
+   }
+   std::pair<TYPE,TYPE> bestpos = halfSample(start,stop);
+   const unsigned int halfSize = (unsigned int)((stop-start)/2u); 
+    return std::accumulate(bestpos.first, bestpos.second,0.0)/double(halfSize);
+}
+
+template <typename TYPE>
+inline double ModeFunctions::LMS(TYPE start, TYPE stop){
+   // trivial cases
+   if (start == stop) {
+    return 0.0;
+   }
+   else if (stop - start == 1) {
+     return *start;
+   } 
+   else if (stop - start == 2){
+     return 0.5*(*start + *(start+1));
+   }
+
+   std::pair<TYPE,TYPE> bestPos = halfSample(start,stop);
+   return 0.5* (*(bestPos.first) + *(bestPos.second));
+}
+
+template <typename TYPE>
+inline double ModeFunctions::generalizedMean(TYPE start, TYPE stop, double power){
+
+  double sum = 0.0; unsigned int size = 0u;
+  for (TYPE iter = start; iter != stop; ++iter, ++size){
+    sum += pow(*iter,power);
+  } // iter
+  return pow(sum/double(size),1.0/power);
+}
+
+
+#endif
