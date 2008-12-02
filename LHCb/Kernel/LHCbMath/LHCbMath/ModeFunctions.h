293d72cb64b5dafb2fae412488923d345d25ed3c
Matthew David Needham <mneedham@cern.ch>
2008-12-02 08:23:52 +0000

    cosmetic change in generalized mean
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48096 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 37e1e684c9eaaa7712bff311b2026d95594d00be

--- LHCb/Kernel/LHCbMath/LHCbMath/ModeFunctions.h
+++ LHCb/Kernel/LHCbMath/LHCbMath/ModeFunctions.h
@@ -111,7 +111,7 @@ inline double ModeFunctions::generalizedMean(TYPE start, TYPE stop, double power
   for (TYPE iter = start; iter != stop; ++iter, ++size){
     sum += pow(*iter,power);
   } // iter
-  return start != stop ?  pow(sum/double(size),1.0/power) : 0.0;
+  return size != 0u ?  pow(sum/double(size),1.0/power) : 0.0;
 }
 
 
