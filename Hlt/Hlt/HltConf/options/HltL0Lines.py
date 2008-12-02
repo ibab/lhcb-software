b1f4ebfd9fe33c140b2de2cd08d830dd5b805930
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-12-02 12:15:17 +0000

    o add some comments...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74296 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e26602a1f6893152ade53bfaf1df5e4819d824c1

--- Hlt/Hlt/HltConf/options/HltL0Lines.py
+++ Hlt/Hlt/HltConf/options/HltL0Lines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env gaudirun.py
 # =============================================================================
-# $Id: HltL0Lines.py,v 1.2 2008-12-02 12:12:02 graven Exp $
+# $Id: HltL0Lines.py,v 1.3 2008-12-02 12:15:17 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Hlt Lines which are plain L0 lines
@@ -12,7 +12,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.2 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.3 $"
 # =============================================================================
 
 
@@ -52,6 +52,10 @@ for i in l0calolines:
        )
 
 l0muonlines = [ 'Muon','DiMuon','MuonNoGlob' ]
+##TODO: why do we have our own thresholds on these? 
+##      why not just promote everything L0 has???
+##      anybody using this can apply their own cuts
+##      before using this anyway...
 minPt = { 'Muon' : 1300.0 
         , 'MuonNoGlob' : 1500.0 }
 
