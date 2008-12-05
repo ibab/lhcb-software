6abee1aeca0be8ee3d1fc8507cb253875111d069
jpalac <jpalac@cern.ch>
2008-12-05 11:30:35 +0000

    . Do not set MainOptions. Leave default "", so script actually does nothing.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@74334 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef3c3da144f8afb9faacc4febfc6453df42ca320

--- DaVinci/Phys/DaVinci/options/DaVinci-2008.py
+++ DaVinci/Phys/DaVinci/options/DaVinci-2008.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DaVinci-2008.py,v 1.7 2008-11-28 13:07:49 jpalac Exp $
+# $Id: DaVinci-2008.py,v 1.8 2008-12-05 11:30:35 jpalac Exp $
 #
 # Options for a typical DaVinci job - this is to please Dirac
 #
@@ -20,5 +20,5 @@ DaVinci().DataType = "2008" # Default is "DC06"
 # DaVinci().Simulation   = False
 # DaVinci().Input = []
 # DaVinci().UserAlgorithms = []
-DaVinci().MainOptions  = "$DAVINCIROOT/options/DaVinci.py"
+# DaVinci().MainOptions  = "$DAVINCIROOT/options/DaVinci.py" # Default is ""
 ##############################################################################
