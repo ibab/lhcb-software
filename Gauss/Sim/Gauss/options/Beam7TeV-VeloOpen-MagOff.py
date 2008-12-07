b8402e869e4d404839cb72ed665442782260f663
Gloria Corti <gloria.corti@cern.ch>
2008-12-07 19:04:58 +0000

    config for 7 TeV
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93868 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8c022533cd053829977027faf8c9f60ec5ed5a14

---
+++ Gauss/Sim/Gauss/options/Beam7TeV-VeloOpen-MagOff.py
@@ -0,0 +1,30 @@
+# File for setting SIMCOND settings from python promt for a given
+# configuration: Beam energy, Velo Status, Magnetic Field Status 
+#
+# Beam7TeV-VeloOpen-MagOff 
+#
+# Syntax is: 
+#  gaudirun.py $GAUSSOPTS/Gauss-2008.py
+#              $GAUSSOPTS/Beam7TeV-VeloOpen-MagOff.py
+#              $DECFILESROOT/options/30000000.opts (ie. event type)
+#              $GAUSSOPTS/Gauss-Job.py (ie. job specific: random seed,
+#                                                         output file names...)
+#
+from Gauss.Configuration import *
+
+#--Tell SIMCOND tag to generate Open VELO
+from DetCond.Configuration import addCondDBLayer
+simCondVelo = allConfigurables["SIMCOND"].clone("VELOCOND")
+simCondVelo.DefaultTAG = "velo-open"
+addCondDBLayer(simCondVelo)
+
+#--Tell SIMCOND to generate Magnetic Field OFF, will be as tag "magnet-off"
+from Configurables import UpdateManagerSvc
+UpdateManagerSvc().ConditionsOverride += [
+ "Conditions/HardwareProperties/LHCb/Magnet/ScaleUp := double_v Coeffs = 0 0;",
+ "Conditions/HardwareProperties/LHCb/Magnet/ScaleDown := double_v Coeffs = 0 0;"
+]
+
+#--Starting time
+ec = EventClockSvc()
+ec.EventTimeDecoder.StartTime = 11000*SystemOfUnits.ns
---
+++ Gauss/Sim/Gauss/options/Beam7TeV-VeloOpen-MagUp.py
@@ -0,0 +1,29 @@
+# File for setting SIMCOND settings from python promt for a given
+# configuration: Beam energy, Velo Status, Magnetic Field Status 
+#
+# Beam7TeV-VeloOpen-MagUp 
+#
+# Syntax is: 
+#  gaudirun.py $GAUSSOPTS/Gauss-2008.py
+#              $GAUSSOPTS/Beam7TeV-VeloOpen-MagUp.py
+#              $DECFILESROOT/options/30000000.opts (ie. event type)
+#              $GAUSSOPTS/Gauss-Job.py (ie. job specific: random seed,
+#                                                         output file names...)
+#
+from Gauss.Configuration import *
+
+#--Tell SIMCOND tag to generate Open VELO
+from DetCond.Configuration import addCondDBLayer
+simCondVelo = allConfigurables["SIMCOND"].clone("VELOCOND")
+simCondVelo.DefaultTAG = "velo-open"
+addCondDBLayer(simCondVelo)
+
+#--Tell SIMCOND to generate Magnetic Field of positive polarity (or mag up)
+from Configurables import UpdateManagerSvc
+UpdateManagerSvc().ConditionsOverride += [
+ "Conditions/Online/LHCb/Magnet/Measured :=  int Polarity = 1;",
+]
+
+#--Starting time
+ec = EventClockSvc()
+ec.EventTimeDecoder.StartTime = 12000*SystemOfUnits.ns
