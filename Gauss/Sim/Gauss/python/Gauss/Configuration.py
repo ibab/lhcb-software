d6ca00d35a2fad06d4163642d9f93fa1c9581842
Gloria Corti <gloria.corti@cern.ch>
2008-12-07 16:30:25 +0000

    import all SystemOfUnits namespace
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93858 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 815e896939bfbb4a7313f0772114b9569c5c127e

--- Gauss/Sim/Gauss/python/Gauss/Configuration.py
+++ Gauss/Sim/Gauss/python/Gauss/Configuration.py
@@ -1,12 +1,12 @@
 """
 High level configuration tools for Gauss
 """
-__version__ = "$Id: Configuration.py,v 1.4 2008-11-20 16:30:11 gcorti Exp $"
+__version__ = "$Id: Configuration.py,v 1.5 2008-12-07 16:30:25 gcorti Exp $"
 __author__  = "Gloria Corti <Gloria.Corti@cern.ch>"
 
 from Gaudi.Configuration import *
 import GaudiKernel.ProcessJobOptions
-from GaudiKernel.SystemOfUnits import GeV, ns, ms, mrad
+from GaudiKernel import SystemOfUnits
 from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime,
                             CondDBEntityResolver )
 from Configurables import ( GenInit, Generation, MinimumBias, PythiaProduction,
