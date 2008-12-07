e01433e72e337d6e2b11c9f89de0bff7e0864897
Gloria Corti <gloria.corti@cern.ch>
2008-12-07 17:00:07 +0000

    reput previosu version accidentally overwritten
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@93864 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4916547087961bfb91c27d33ea673a358a1f1133

--- Gauss/Sim/Gauss/options/Gauss-Job.py
+++ Gauss/Sim/Gauss/options/Gauss-Job.py
@@ -4,44 +4,21 @@
 #
 
 from Gauss.Configuration import *
-import time
 
 #--Generator phase, set random numbers
 GaussGen = GenInit("GaussGen")
 GaussGen.FirstEventNumber = 1
 GaussGen.RunNumber        = 83
 
-## Generator = GaudiSequencer("Generator")
-## Generator.Members  = [GaussGen]
-
-## Generator.Members += ["Generation"]
-## Generator.Members += ["GaudiSequencer/GenMonitor"]
-
 #--Number of events
 nEvts = 5
 ApplicationMgr( EvtMax     = nEvts )
 
-#--Event type
-eventType = '11144101'
-#eventType = '51000005'
-#eventType = '60001008'
-#eventType = '60040000'
-importOptions('$DECFILESROOT/options/'+eventType+'.opts')
-
-#--Set name of output files for given job and read in options
-idFile = str(time.localtime().tm_year)
-if time.localtime().tm_mon < 10:
-    idFile += '0'
-idFile += str(time.localtime().tm_mon)
-if time.localtime().tm_mday < 10:
-    idFile += '0'
-idFile += str(time.localtime().tm_mday)
-idFile = eventType+'-'+str(nEvts)+'ev-'+idFile
-
+#--Set name of output files for given job
+idFile = 'GaussExample'
 HistogramPersistencySvc().OutputFile = idFile+'-histos.root'
 
 tape = OutputStream("GaussTape")
 tape.Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
 ApplicationMgr( OutStream = [tape] )
 
-print idFile
