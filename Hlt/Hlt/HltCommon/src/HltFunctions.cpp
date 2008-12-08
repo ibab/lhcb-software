7ed2125ac1edb906bc7027b138fa2b09f1e4df24
Vanya Belyaev <ivan.belyaev@cern.ch>
2008-12-08 09:53:56 +0000

    fix for LHCb::Track::stateAt
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74365 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c2837d3fc833e7b02b44ec980c3c471e1a467860

--- Hlt/Hlt/HltCommon/src/HltFunctions.cpp
+++ Hlt/Hlt/HltCommon/src/HltFunctions.cpp
@@ -12,12 +12,13 @@ using namespace LHCb;
 
 double Hlt::ETCalo::operator() (const Track& track)  const
 {
-  if (!track.hasStateAt(State::MidHCal)) return 1e6;
-  const State& state = track.stateAt(State::MidHCal);
-  double p = state.p();
-  double x = state.x();
-  double y = state.y();
-  double z = state.z();
+  const LHCb::State* state = track.stateAt(State::MidHCal);
+  if ( 0 == state ) { return 1e6; }
+  
+  double p = state->p();
+  double x = state->x();
+  double y = state->y();
+  double z = state->z();
   double sintheta = sqrt(x*x+y*y)/sqrt(x*x+y*y+z*z);
   double et = p*sintheta;
   // std::cout << " et " << et << " sintheta " << sintheta << " p " << p
@@ -35,9 +36,10 @@ double Hlt::DeltaP::operator() (const Track& track1, const Track& track2)  const
 
 double Hlt::DeltaE::operator() (const Track& track)  const
 {
-  if (!track.hasStateAt(State::MidHCal)) return 1e6;
+  const LHCb::State* state = track.stateAt(LHCb::State::MidHCal) ;
+  if ( 0 == state ) { return 1.e6 ; }
   double p = track.p();
-  double e = track.stateAt(State::MidHCal).p();
+  double e = state->p();
   double de = e*(sqrt( 0.60*0.60 + 0.70*0.70/e ));
   double delta = (e-p)/de;
   return delta;
