7b6b9b4d1544b8e8bcaacef7f7ca9389909514d6
Patrick Spradlin <patrick.spradlin@cern.ch>
2008-12-09 13:17:31 +0000

    Added doxygen comments to FilterByRunEvent
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48122 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0f34156a2c5194f7df9366e06e1a17d0a99c048c

--- LHCb/Kernel/LHCbAlgs/src/FilterByRunEvent.h
+++ LHCb/Kernel/LHCbAlgs/src/FilterByRunEvent.h
@@ -1,4 +1,4 @@
-// $Id: FilterByRunEvent.h,v 1.3 2008-04-25 11:58:53 graven Exp $
+// $Id: FilterByRunEvent.h,v 1.4 2008-12-09 13:17:31 spradlin Exp $
 #ifndef FILTERBYRUNEVENT_H 
 #define FILTERBYRUNEVENT_H 1
 
@@ -13,6 +13,41 @@
  *
  *  @author Patrick Spradlin
  *  @date   2007-01-18
+ *
+ *  @par Algorithm purpose
+ *    @c FilterByRunEvent filters events based on the
+ *    (run number, event number) pair of the event.
+ *
+ *  @par Algorithm properties
+ *    @c FilterByRunEvent has two properties:
+ *    - @c PassSelectedEvents:  A boolean with a default value of @c true.
+ *      If @c PassSelectedEvents is set to @c true (the default),
+ *      @c FilterByRunEvent will pass only the specified events and reject all
+ *      other events. If @c PassSelectedEvents is set to @c false,
+ *      @c FilterByRunEvent will reject the specified events and pass all
+ *      other events.
+ *    - @c RunEventNumList:  A vector of (run number, event number) pairs.
+ *      By default, @c RunEventNumList is empty.  In python configurables,
+ *      @c RunEventNumList is specified with a list of pairs (tuples of
+ *      length 2).
+ *
+ *    The default configuration of @c FilterByRunEvent is a fail-all filter.
+ *
+ *  @par Example of usage
+ * 
+ *    In order to configure a sequencer @c MySequence to skip the two events
+ *    with (run number, event number) pairs (86456, 421) and (48621, 3):
+ *    @verbatim
+.....
+MySequence = GaudiSequencer('MySequence')
+MyFilterByRunEvent = FilterByRunEvent('MyFilterByRunEvent')
+MySequence.Members += [ MyFilterByRunEvent, .... ]
+
+# Set PassSelectedEvents to false, so that it rejects the selected events
+MyFilterByRunEvent.PassSelectedEvents = 0
+# std::vector< std::pair<int, int> > configured with a list of pairs
+MyFilterByRunEvent.RunEventNumList = [ (86456, 421), (48621, 3) ]
+.... @endverbatim
  */
 class FilterByRunEvent : public GaudiAlgorithm {
 public: 
