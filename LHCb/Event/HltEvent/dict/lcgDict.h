7ab1bdfd2ab7911d377d711fdd61b1ac1776ce18
Tomasz Skwarnicki <tskwarni@syr.edu>
2008-12-11 00:43:14 +0000

    add dictionary for std::vector<const HltObjectSummary*>
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37130 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 74d9cb15e5540285c7c76baacd733f27287b17fb

---
+++ LHCb/Event/HltEvent/dict/lcgDict.h
@@ -0,0 +1,21 @@
+// $Id: lcgDict.h,v 1.1 2008-12-11 00:43:14 tskwarni Exp $
+#ifndef DICT_LCGDICT_H 
+#define DICT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "Event/HltObjectSummary.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    std::vector<const LHCb::HltObjectSummary*> _i1;
+    std::vector<LHCb::HltObjectSummary*>       _i2;
+    // end instantiations
+  };
+}
+
+#endif // DICT_LCGDICT_H
