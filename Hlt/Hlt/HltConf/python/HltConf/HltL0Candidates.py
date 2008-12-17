0b4f2fc9e1a7476aa909e860a6076be02a5983b3
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-12-17 21:36:12 +0000

    add HltL0Candidates.py as module: to be used for common conversion of L0 Candidates
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74468 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5cc648b54c2909986cc73ecbb01a726b9d029136

---
+++ Hlt/Hlt/HltConf/python/HltConf/HltL0Candidates.py
@@ -0,0 +1,31 @@
+from Gaudi.Configuration import *
+from Configurables import HltL0CaloCandidates, HltL0MuonCandidates
+from Configurables import HadronSeedTool, EcalSeedForVeloMatch
+
+def _name(i) : return 'HltL0'+i+'Candidates'
+
+_calomaker = {  'Hadron'   : HadronSeedTool( decodeCalos = False )
+             ,  'Electron' : EcalSeedForVeloMatch }
+
+_dict = dict()
+for i in [ 'Muon','DiMuon','MuonNoGlob' ] :
+    _dict.update( i, HltL0MuonCandidates(_name(i), L0Channel = i) )
+for i in [ 'Electron','Photon','Hadron' ,'LocalPi0','GlobalPi0' ] :
+    _dict.update( i, HltL0CaloCandidates(_name(i), L0Channel = i) )
+    if i in _calomaker :
+        HltL0CaloCandidates(_name(i)).addTool(_calomaker[i])
+        HltL0CaloCandidates(_name(i)).CaloMakerTool = _calomaker[i]
+
+for i in _dict.keys :
+    HltSelReportsMaker().SelectionMaxCandidates.update( { _name(i):500 } )
+    HltSelReportsMaker().SelectionMaxCandidatesDebug.update( { _name(i):500 } )
+
+def L0Channels() :
+    return _dict.keys()
+
+def convertL0Candidates(channel) :
+    return _dict[channel]
+
+def HltL0Candidates(channel) :
+    if channel not in _dict.keys() : raise KeyError('Unknown L0 Channel: %s'%channel)
+    return _name(channel)
