654df03378b2725688ccc68fcc176194389b3845
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-12-17 21:53:54 +0000

    fix oops
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74470 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e20b5cf9485882198038ff3f5c7b5ecd158edf5b

--- Hlt/Hlt/HltConf/python/HltConf/HltL0Candidates.py
+++ Hlt/Hlt/HltConf/python/HltConf/HltL0Candidates.py
@@ -1,6 +1,7 @@
 from Gaudi.Configuration import *
 from Configurables import HltL0CaloCandidates, HltL0MuonCandidates
 from Configurables import HadronSeedTool, EcalSeedForVeloMatch
+from Configurables import HltSelReportsMaker
 
 def _name(i) : return 'HltL0'+i+'Candidates'
 
@@ -9,14 +10,14 @@ _calomaker = {  'Hadron'   : HadronSeedTool( decodeCalos = False )
 
 _dict = dict()
 for i in [ 'Muon','DiMuon','MuonNoGlob' ] :
-    _dict.update( i, HltL0MuonCandidates(_name(i), L0Channel = i) )
+    _dict.update( { i: HltL0MuonCandidates(_name(i), L0Channel = i) } )
 for i in [ 'Electron','Photon','Hadron' ,'LocalPi0','GlobalPi0' ] :
-    _dict.update( i, HltL0CaloCandidates(_name(i), L0Channel = i) )
+    _dict.update( { i: HltL0CaloCandidates(_name(i), L0Channel = i) } )
     if i in _calomaker :
         HltL0CaloCandidates(_name(i)).addTool(_calomaker[i])
         HltL0CaloCandidates(_name(i)).CaloMakerTool = _calomaker[i]
 
-for i in _dict.keys :
+for i in _dict.keys() :
     HltSelReportsMaker().SelectionMaxCandidates.update( { _name(i):500 } )
     HltSelReportsMaker().SelectionMaxCandidatesDebug.update( { _name(i):500 } )
 
