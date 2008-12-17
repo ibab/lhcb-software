42346051de9ea94d7e55fd99e3d021f50ec8e5a2
Marco Cattaneo <marco.cattaneo@cern.ch>
2008-12-17 14:52:44 +0000

    fix gcc 4.3 warning
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21713 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9c90f5b2b52e82553c6262ae4d3ac1a210497bd

--- Rec/Muon/MuonPIDChecker/src/MuonPIDChecker.cpp
+++ Rec/Muon/MuonPIDChecker/src/MuonPIDChecker.cpp
@@ -398,20 +398,26 @@ int MuonPIDChecker::getTrType( const LHCb::Track *pTrack,
   StatusCode sc = checkMCAss( pTrack , mcP);
 
   // Fill Histograms
-  if ( sc.isFailure() || 0 == mcP->mother() ) {      // ghost
+  if ( sc.isFailure() || (0 == mcP->mother()) ) {      // ghost
 
     type = 0;  
 	       
   }else{                        // there is MC association
 
     m_TrMCp0 = mcP->p()/Gaudi::Units::GeV;
-    if (( abs (mcP->particleID().pid()) == 13) && ((m_TrnLinks==1 &&
-	  ( abs (mcP->mother()->particleID().pid()) == 211 || 
-	    abs (mcP->mother()->particleID().pid()) == 321 )) || 
-	    m_TrnLinks==2) ||
-	( abs (mcP->particleID().pid()) !=13 && 
-	    (m_TrzDecay > 0 && m_TrzDecay<20000.)) ){
-
+    if( ( abs( mcP->particleID().pid() ) == 13 &&
+          ( ( m_TrnLinks==1 &&( abs( mcP->mother()->particleID().pid() ) == 211 ||
+                                abs( mcP->mother()->particleID().pid() ) == 321
+                              )
+            ) ||
+            m_TrnLinks==2
+          )
+        ) ||
+        ( abs( mcP->particleID().pid() ) != 13 &&
+          m_TrzDecay > 0                       && 
+          m_TrzDecay<20000.                       )
+      )
+    {
       type = 2;  
     }else if ( abs (mcP->particleID().pid()) == 13  && m_TrnLinks == 1 ){ 
         
