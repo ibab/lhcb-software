21ff9601b0498df1a8f8ab5bd50c7918ad9a2b70
Gerhard Raven <gerhard.raven@nikhef.nl>
2008-12-18 13:45:37 +0000

    in HltElectronLines.py, use HltL0Candidates
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74500 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 098b103306620d4f1ed8997b631e4691adc8c4c1

--- Hlt/Hlt/HltConf/options/HltElectronLines.py
+++ Hlt/Hlt/HltConf/options/HltElectronLines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env gaudirun.py
 # =============================================================================
-# $Id: HltElectronLines.py,v 1.9 2008-12-09 10:51:43 witekma Exp $
+# $Id: HltElectronLines.py,v 1.10 2008-12-18 13:45:37 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Photon Lines
@@ -12,7 +12,7 @@
 '''
 # =============================================================================
 __author__  = 'Gerhard Raven Gerhard.Raven@nikhef.nl'
-__version__ = 'CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.9 $'
+__version__ = 'CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.10 $'
 # =============================================================================
 
 from Gaudi.Configuration import * 
@@ -20,29 +20,30 @@ from Gaudi.Configuration import *
 from Configurables import GaudiSequencer
 from Configurables import HltL0CaloPrepare
 from Configurables import HltTrackUpgrade
-from Configurables import EcalSeedForVeloMatch
 
 from HltConf.HltLine import Hlt1Line   as Line
 from HltConf.HltLine import Hlt1Member as Member
 from HltConf.HltLine import bindMembers
 from HltConf.HltLine import Hlt1Tool   as Tool
-from HltConf.HltLine import hlt1Lines, addHlt1Prop, rmHlt1Prop 
+from HltConf.HltL0Candidates import *
 
-HltL0CaloPrepare('L0ElectronDecision').addTool(  EcalSeedForVeloMatch('EcalSeedForVeloMatch') )
+
+L0CandidateConverter = convertL0Candidates('Electron')
+L0Candidates = HltL0Candidates('Electron')
 
 prepElectron =  bindMembers( 'PrepElectron', 
-  [ HltL0CaloPrepare('L0ElectronDecision' , CaloType = 'Electron', CaloMakerTool = 'EcalSeedForVeloMatch', MinEt = 2600.0)
+  [ L0CandidateConverter
   , GaudiSequencer('Hlt1RecoRZVeloSequence')
   , Member ('TF', 'RZVelo'  # // select RZVelo tracks with an IP and matched to L0Calo
            , InputSelection     = 'RZVelo'
-           , FilterDescriptor = [ 'Ecal2DChi2_L0ElectronDecision,<,4.' ]
+           , FilterDescriptor = [ 'Ecal2DChi2_'+L0Candidates+',<,4.' ]
            , HistogramUpdatePeriod = 0
            , HistoDescriptor = {  'Ecal2DChi2'     : ('Ecal2DChi2',0.,20.,100), 'Ecal2DChi2Best' : ('Ecal2DChi2Best',0.,20.,100) }
            )
   , Member ( 'TU', 'Velo', RecoName = 'Velo') # // Velo reconstruction of selected RZVelo
   , Member ( 'TF', 'Velo' # // Select Velo tracks with an IP and matched to L0Calo
            , HistogramUpdatePeriod = 0
-           , FilterDescriptor = [ 'IP_PV2D,||[],0.1,3.', 'Ecal3DChi2_L0ElectronDecision,<,16.' ]
+           , FilterDescriptor = [ 'IP_PV2D,||[],0.1,3.', 'Ecal3DChi2_'+L0Candidates+',<,16.' ]
            , HistoDescriptor = { 'IP' :           ('IP',-1.,3.,400), 'IPBest' :       ('IPBest',-1.,3.,400), 'Ecal3DChi2':    ('Ecal3DChi2',0.,20.,100), 'Ecal3DChi2Best':('Ecal3DChi2Best',0.,20.,100) }
            )
   , Member ( 'TU', 'Forward' # // Make forward the selected velo tracks
