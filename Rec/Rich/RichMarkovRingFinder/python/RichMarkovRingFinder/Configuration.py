71842d98089cd09249298f34d2efe0ea631b6792
ukerzel <ukerzel@cern.ch>
2009-01-07 13:31:19 +0000

    fix configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10432 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1517f6c888083ddd34d1531a0009c3e90584806d

--- Rec/Rich/RichMarkovRingFinder/python/RichMarkovRingFinder/Configuration.py
+++ Rec/Rich/RichMarkovRingFinder/python/RichMarkovRingFinder/Configuration.py
@@ -4,7 +4,7 @@
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.3 2008-11-14 17:13:17 jonrob Exp $"
+__version__ = "$Id: Configuration.py,v 1.4 2009-01-07 13:31:19 ukerzel Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from RichKernel.Configuration import *
@@ -21,88 +21,81 @@ class RichMarkovRingFinderConf(RichConfigurableUser):
 
         ## Steering options
     __slots__ = {
-        "context":    "Offline"   # The context within which to run
-       ,"panels": ["Rich1Top","Rich1Bottom","Rich2Left","Rich2Right"]
-       ,"maxHitsInPanel": 300
-       ,"associateToSegments": True
-       ,"sequencer" : None     # The sequencer to add the RICH MCMC algorithms to
+        "Context"             :    "Offline"   # The context within which to run
+       ,"Panels"              : ["Rich1Top","Rich1Bottom","Rich2Left","Rich2Right"]
+       ,"MaxHitsInPanel"      : 300
+       ,"AssociateToSegments" : True
+       ,"Sequencer"           : GaudiSequencer("RichMarkovDefaultSequencer")     # The sequencer to add the RICH MCMC algorithms to
         }
 
     ## Access the finder for RICH1 top panel
     def rich1TopFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich1TopPanel
-        cont = self.getProp("context")
+        cont = self.getProp("Context")
         return Rich__Rec__MarkovRingFinder__Rich1TopPanel(cont+"MFinderR1Top")
 
     ## Access the finder for RICH1 bottom panel
     def rich1BottomFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich1BottomPanel
-        cont = self.getProp("context")
+        cont = self.getProp("Context")
         return Rich__Rec__MarkovRingFinder__Rich1BottomPanel(cont+"MFinderR1Bottom")
 
     ## Access the finder for RICH2 left panel
     def rich2LeftFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich2LeftPanel
-        cont = self.getProp("context")
+        cont = self.getProp("Context")
         return Rich__Rec__MarkovRingFinder__Rich2LeftPanel(cont+"MFinderR2Left")
 
     ## Access the finder for RICH2 right panel
     def rich2RightFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich2RightPanel
-        cont = self.getProp("context")
+        cont = self.getProp("Context")
         return Rich__Rec__MarkovRingFinder__Rich2RightPanel(cont+"MFinderR2Right")
 
     ## @brief Apply the configuration to the given GaudiSequencer
     def applyConf(self):
 
-        sequence = self.getProp("sequencer")
-        if sequence == None :
-
-            print "RichMarkovRingFinderConf sequencer not set"
-
+        sequence = self.getProp("Sequencer")
+        if sequence.name() == "RichMarkovDefaultSequencer" :
+            raise RuntimeError("ERROR : Rich Markov Ring Finder Sequence not set")
         else :
-
-            panels = self.getProp("panels")
-
+            panels = self.getProp("Panels")
             if len(panels) == 0 :
-                
-                print "Warning : No Panels configured for ring finding"
-
+                raise RuntimeError("ERROR : No panels set to find Markov Rings")
             else:
-
                 # maximum hits in each panel for finding
-                maxhits = self.getProp("maxHitsInPanel")
+                maxhits = self.getProp("MaxHitsInPanel")
 
                 # context
-                cont = self.getProp("context")
+                cont = self.getProp("Context")
 
                 # The finders, for each HPD panel
                 if "Rich1Top" in panels :
-                    alg = self.rich1TopFinder()
+                    alg                = self.rich1TopFinder()
                     alg.MaxHitsInEvent = maxhits
-                    sequence.Members += [alg]
+                    sequence.Members  += [alg]
                     if "Rich1Bottom" in panels :
-                        alg = self.rich1BottomFinder()
+                        alg                = self.rich1BottomFinder()
                         alg.MaxHitsInEvent = maxhits
-                        sequence.Members += [alg]
+                        sequence.Members  += [alg]
                     if "Rich2Left" in panels :
-                        alg = self.rich2LeftFinder()
+                        alg                = self.rich2LeftFinder()
                         alg.MaxHitsInEvent = maxhits
-                        sequence.Members += [alg]
+                        sequence.Members  += [alg]
                     if "Rich2Right" in panels :
-                        alg = self.rich2RightFinder()
+                        alg                = self.rich2RightFinder()
                         alg.MaxHitsInEvent = maxhits
-                        sequence.Members += [alg]
+                        sequence.Members  += [alg]
 
                 # Attempt to associated rings to segments
-                if self.getProp("associateToSegments"):
+                if self.getProp("AssociateToSegments"):
                     from Configurables import Rich__Rec__TracklessRingSegmentAssociationAlg
-                    alg = Rich__Rec__TracklessRingSegmentAssociationAlg(cont+"MRingsSegAssoc")
+                    alg               = Rich__Rec__TracklessRingSegmentAssociationAlg(cont+"MRingsSegAssoc")
                     sequence.Members += [alg]
 
                 # Post finding cleaning and ring selection
-                filter = Rich__Rec__TracklessRingFilterAlg(cont+"BestMRings")
-                iso    = Rich__Rec__TracklessRingIsolationAlg(cont+"IsolatedMRings")
-                iso.InputRings = "Rec/Rich/Markov/RingsBest"
+                filter            = Rich__Rec__TracklessRingFilterAlg(cont+"BestMRings")
+                iso               = Rich__Rec__TracklessRingIsolationAlg(cont+"IsolatedMRings")
+                iso.InputRings    = "Rec/Rich/Markov/RingsBest"
                 sequence.Members += [filter,iso]
             
