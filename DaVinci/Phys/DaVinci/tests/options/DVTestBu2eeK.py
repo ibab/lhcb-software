4b21d09818c778eadb280ac06f5ca0edecc635df
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-01-07 10:49:06 +0000

    Tests adapted
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@74636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d9aa71a825d88d385b3cafa7ae77fb4724df7f1e

--- DaVinci/Phys/DaVinci/tests/options/DVTestBu2eeK.py
+++ DaVinci/Phys/DaVinci/tests/options/DVTestBu2eeK.py
@@ -1,11 +1,11 @@
-## $Id: DVTestBd2DstarPi.py,v 1.2 2009-01-07 10:49:06 pkoppenb Exp $
+## $Id: DVTestBu2eeK.py,v 1.1 2009-01-07 10:49:06 pkoppenb Exp $
 ## ============================================================================
-## CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.2 $
+## CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $
 ## ============================================================================
 """
-@file DVTestBd2DstarPi.py
+@file DVTestBu2eeK.py
 
-Test file for HLT Bd->D*pi selection
+Test file for HLT Bu->eeK selection
 
 @author J. Palacios
 @date 2007-11-12
@@ -13,7 +13,7 @@ Test file for HLT Bd->D*pi selection
 from Gaudi.Configuration import *
 ##--------------------------------------------------------------
 ##
-## Truth checking for B0 -> D*Pi
+## Truth checking for Bu->eeK
 ##
 from Configurables import GaudiSequencer
 trueSeq = GaudiSequencer("FindTrueDecay")
@@ -27,17 +27,17 @@ SelectTrueNoPIDsDecay = OldFilterDesktop("SelectTrueNoPIDsDecay")
 trueSeq.Members += [ SelectTrueNoPIDsDecay ]
 SelectTrueNoPIDsDecay.FilterCriterion = "ByPIDFilterCriterion"
 SelectTrueNoPIDsDecay.addTool(PhysDesktop())
-SelectTrueNoPIDsDecay.PhysDesktop.InputLocations = [ "StdDC06NoPIDsPions",
+SelectTrueNoPIDsDecay.PhysDesktop.InputLocations = [ "StdDC06NoPIDsElectrons",
                                                      "StdDC06NoPIDsKaons" ]
 SelectTrueNoPIDsDecay.addTool(ByPIDFilterCriterion("Filter"))
 SelectTrueNoPIDsDecay.Filter.Selections =  [ "K+ : TrueMCFilterCriterion/Decay",
-                                             "pi+ : TrueMCFilterCriterion/Decay" ]
+                                             "e+ : TrueMCFilterCriterion/Decay" ]
 ##
 ## The decay descriptor is the only part that requires some thinking...
 ##
 SelectTrueNoPIDsDecay.Filter.addTool(TrueMCFilterCriterion("Decay"))
 SelectTrueNoPIDsDecay.Filter.Decay.addTool(MCDecayFinder()) 
-SelectTrueNoPIDsDecay.Filter.Decay.MCDecayFinder.Decay = "{[[B0]nos -> ^pi+ {,gamma}{,gamma} (D*(2010)- -> ^pi- {,gamma}{,gamma} (D~0 -> ^K+ ^pi- {,gamma}{,gamma}{,gamma})) ...]cc, [[B~0]os -> ^pi+ {,gamma}{,gamma} (D*(2010)- -> ^pi- {,gamma}{,gamma} (D~0 -> ^K+ ^pi- {,gamma}{,gamma}{,gamma})) ...]cc}" 
+SelectTrueNoPIDsDecay.Filter.Decay.MCDecayFinder.Decay = "[B+ -> ^K+ ^e+ ^e- {,gamma}{,gamma}{,gamma}{,gamma}{,gamma}]cc" 
 ##
 ## Dummy MakeResonances to check that all tracks are there
 ##
@@ -47,7 +47,7 @@ trueSeq.Members += [ AllTrueNoPIDsDecay ]
 AllTrueNoPIDsDecay.MotherCut = "ALL"
 AllTrueNoPIDsDecay.addTool(PhysDesktop())
 AllTrueNoPIDsDecay.PhysDesktop.InputLocations = [ "SelectTrueNoPIDsDecay" ]
-AllTrueNoPIDsDecay.DecayDescriptor = "[B0 -> pi- K+ pi- pi+]cc"  ## all particles need to be there
+AllTrueNoPIDsDecay.DecayDescriptor = "[B+ -> e- e+ K+]cc"  ## all particles need to be there
 ##----------------------------------------------------------------------------------------------------
 ##
 ## Truth filter for Bd -> D*Pi Loose PID : copy of above with NoPIDs -> Loose 
@@ -55,7 +55,7 @@ AllTrueNoPIDsDecay.DecayDescriptor = "[B0 -> pi- K+ pi- pi+]cc"  ## all particle
 SelectTrueLooseDecay = SelectTrueNoPIDsDecay.clone("SelectTrueLooseDecay")
 trueSeq.Members += [ SelectTrueLooseDecay ]
 SelectTrueLooseDecay.addTool(PhysDesktop())
-SelectTrueLooseDecay.PhysDesktop.InputLocations = [ "StdDC06LoosePions",
+SelectTrueLooseDecay.PhysDesktop.InputLocations = [ "StdDC06LooseElectrons",
                                                     "StdDC06LooseKaons"]
 ##
 ## Dummy MakeResonances to check that all tracks are there
@@ -70,41 +70,41 @@ TestCorrelations = AlgorithmCorrelationsAlg("TestCorrelations")
 trueSeq.Members += [ TestCorrelations  ]
 TestCorrelations.addTool(AlgorithmCorrelations())
 TestCorrelations.AlgorithmCorrelations.OnlyNonZero = False
-TestCorrelations.Algorithms = [ "AllTrueNoPIDsDecay", 
+TestCorrelations.Algorithms = [ "SelectTrueNoPIDsDecay",
+                                "AllTrueNoPIDsDecay",
+                                "SelectTrueLooseDecay",
                                 "AllTrueLooseDecay",
-                                "StdDC06LooseD02KPi",
-                                "StdDC06LooseDstarWithD02KPi",
-                                "FilterDstarForBd2DstarPi", 
-                                "PreselBd2DstarPi"          ]
+                                "DiLeptonForPreselBu2LLK",
+                                "PreselBu2LLK" ]
 ###
 # DaVinci
 ###
 ## Preselection
 from Configurables import DaVinci
-DaVinci().MainOptions = "$B2DSTARXROOT/options/DoPreselBd2DstarPi.opts"
+DaVinci().MainOptions = "$B2DILEPTONROOT/options/DoPreselBu2LLK.opts"
 DaVinci().UserAlgorithms = [ trueSeq ]
 
-##-- GAUDI data cards generated on 11/12/07 6:36 PM
-##-- For Event Type = 11463000 / Data type = DST 1
+##-- GAUDI data cards generated on 11/19/07 2:55 PM
+##-- For Event Type = 12123001 / Data type = DST 1
 ##--     Configuration = DC06 - phys-v2-lumi2
 ##--     DST 1 datasets produced by Brunel - v30r14
 ##--     From DIGI 1 datasets produced by Boole - v12r10
 ##--     From SIM 1 datasets produced by Gauss - v25r8
 ##--     Database version = v30r14
 ##--     Cards content = physical-physical
-##--     
+##--
 ##-- Datasets replicated at CERN
-##-- 200 dataset(s) - NbEvents = 99411
-##-- 
+##-- 166 dataset(s) - NbEvents = 82494
+##--
 
 DaVinci().Input   = [
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001695/DST/0000/00001695_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"
-]
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001659/DST/0000/00001659_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001659/DST/0000/00001659_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001659/DST/0000/00001659_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001659/DST/0000/00001659_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v2-lumi2/00001659/DST/0000/00001659_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
+
+
 DaVinci().EvtMax = 500 
 DaVinci().SkipEvents = 0 
 DaVinci().PrintFreq = 1 
