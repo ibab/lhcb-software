3dc38860f5902cd0d4fec7214fec87f5145f06fe
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-01-07 12:30:46 +0000

    add possibility to convert all L0 muons
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74645 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 07e73c69d8f629fdd5ad57d370313c2e634094cb

--- Hlt/Hlt/HltConf/python/HltConf/HltL0Candidates.py
+++ Hlt/Hlt/HltConf/python/HltConf/HltL0Candidates.py
@@ -8,13 +8,20 @@ def _name(i) : return 'HltL0'+i+'Candidates'
 _calomaker = {  'Hadron'   : HadronSeedTool( decodeCalos = False )
              ,  'Electron' : EcalSeedForVeloMatch() }
 
+
 _dict = dict()
 for i in [ 'Muon','DiMuon','MuonNoGlob' ] :
-    _dict.update( { i: HltL0MuonCandidates(_name(i), L0Channel = i) } )
+    #note: explicitly set the OutputSelection so we can pick it up downstream...
+    _dict.update( { i: HltL0MuonCandidates(_name(i), L0Channel = i, OutputSelection = _name(i)) } )
+
+### a special instance which just convertes 'everything'...
+_dict.update( { 'AllMuon': HltL0MuonCandidates(_name('AllMuon'), L0Channel = 'Ignore', OutputSelection = _name('AllMuon')) } )
+
 for i in [ 'Electron','Photon','Hadron' ,'LocalPi0','GlobalPi0' ] :
-    _dict.update( { i: HltL0CaloCandidates(_name(i), L0Channel = i) } )
+    #note: explicitly set the OutputSelection so we can pick it up downstream...
+    _dict.update( { i: HltL0CaloCandidates(_name(i), L0Channel = i, OutputSelection = _name(i)) } )
     if i in _calomaker :
-        HltL0CaloCandidates(_name(i)).addTool(_calomaker[i])
+        HltL0CaloCandidates(_name(i)).addTool(_calomaker[i],name='CaloMakerTool')
         HltL0CaloCandidates(_name(i)).CaloMakerTool = _calomaker[i]
 
 for i in _dict.keys() :
@@ -22,7 +29,8 @@ for i in _dict.keys() :
     HltSelReportsMaker().SelectionMaxCandidatesDebug.update( { _name(i):500 } )
 
 def L0Channels() :
-    return _dict.keys()
+    #TODO: strip 'fake' 'all' entries...
+    return [ i for i in _dict.keys() if not i.startswith('All') ]
 
 def convertL0Candidates(channel) :
     return _dict[channel]
