8c71570a9598c83c81285a62e113820d3b4cf9db
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-08 15:47:17 +0000

    test for packed DST
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@60265 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 899ca29a2376ae67b2fa4aca2d5032ea18b84120

---
+++ LHCb/Ex/IOExample/tests/options/UnpackDst.py
@@ -0,0 +1,20 @@
+# Test unpacking of a packed DST. Currently only tests tracks, to be extended...
+from Gaudi.Configuration import *
+from Configurables import DumpTracks, DstConf
+
+ApplicationMgr().TopAlg += [ DumpTracks(OutputLevel = DEBUG) ]
+
+ApplicationMgr().EvtMax      = 5
+DataOnDemandSvc().OutputLevel = WARNING
+
+# Set up POOL persistency
+importOptions( "$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts")
+
+FileCatalog().Catalogs = ["xmlcatalog_file:MyCatalog.xml"]
+
+# Unpack the packed containers on demand
+ApplicationMgr().ExtSvc += [ "DataOnDemandSvc" ]
+EventDataSvc().EnableFaultHandler = True
+DstConf().EnableUnpack = True
+
+EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/c/cattanem/Brunel/v34r1/11144101-100ev-20081006.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
