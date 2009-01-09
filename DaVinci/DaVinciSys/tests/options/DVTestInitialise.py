d9799be423fa1ae3e7ca5b3e816cbd877e9f1f25
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-01-09 13:29:54 +0000

    simplify
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@74684 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1065a83a9766fbb2449f09cb72c02b8b60036d67

--- DaVinci/DaVinciSys/tests/options/DVTestInitialise.py
+++ DaVinci/DaVinciSys/tests/options/DVTestInitialise.py
@@ -1,4 +1,4 @@
-## $Id: DVTestInitialise.py,v 1.1 2009-01-07 18:26:22 pkoppenb Exp $
+## $Id: DVTestInitialise.py,v 1.2 2009-01-09 13:29:54 pkoppenb Exp $
 ## ============================================================================
 ## CVS tag $Name: not supported by cvs2svn $
 ## ============================================================================
@@ -11,24 +11,9 @@ Test for DaVinci initialisation
 @date 2009-01-07
 """
 from Gaudi.Configuration import *
-##
-## Truth checking 
-##
-from Configurables import AlgorithmCorrelationsAlg
-##
-## Correlations
-##
-from Configurables import AlgorithmCorrelationsAlg, AlgorithmCorrelations
-TestCorrelations = AlgorithmCorrelationsAlg("TestCorrelations")
-TestCorrelations.addTool(AlgorithmCorrelations())
-TestCorrelations.AlgorithmCorrelations.OnlyNonZero = False
-TestCorrelations.Algorithms = [ "Bu2LLK09_DiLepton", "Presel09Bu2LLK", "Sel09Bu2LLK" ]
-###
-# Selection
-###
 from Configurables import DaVinci
-DaVinci().MainOptions = "$B2DILEPTONROOT/options/Do09selBu2LLK.py"
-DaVinci().UserAlgorithms = [ TestCorrelations ]
+DaVinci().MainOptions = ""
+DaVinci().UserAlgorithms = [ ]
 
 DaVinci().EvtMax = 0 # No running 
 DaVinci().SkipEvents = 0 
