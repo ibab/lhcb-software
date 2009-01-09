f611b96b3350a7b19f4fc2161365a4684e36dcac
dsonnick <dsonnick@cern.ch>
2009-01-09 14:23:22 +0000

    Correction of counting and sending the events statistics, added check and counting of lumi events and added some basic logging.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98910 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b6c8f1b4d5ad27d0dd6c353ae0d2a5b37186439a

--- Online/Online/Writer/src/MDFWriterNet.cpp
+++ Online/Online/Writer/src/MDFWriterNet.cpp
@@ -5,6 +5,20 @@
 #include <ctime>
 #include <map>
 
+#include <limits.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <mqueue.h>
+
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiKernel/AlgFactory.h"
 #include "GaudiKernel/SmartDataPtr.h"
@@ -22,6 +36,9 @@ extern "C" {
 #include "dis.h"
 }
 
+#include "Writer/defs.h"
+
+
 DECLARE_NAMESPACE_ALGORITHM_FACTORY(LHCb,MDFWriterNet);
 
 using namespace LHCb;
@@ -29,7 +46,7 @@ using namespace LHCb;
 /**
  * Macro for initialising a close command.
  */
-#define INIT_CLOSE_COMMAND(h, fname, adler_32, md_5, seqno, rno, thesize, noofevents) { \
+#define INIT_CLOSE_COMMAND(h, fname, adler_32, md_5, seqno, rno, thesize, noofevents, nooflumievents) { \
     (h)->cmd = CMD_CLOSE_FILE; \
     (h)->run_no = rno;  \
     (h)->data.chunk_data.seq_num = seqno; \
@@ -37,6 +54,7 @@ using namespace LHCb;
     (md_5)->Final((h)->data.stop_data.md5_sum); \
     (h)->data.stop_data.size = thesize; \
     (h)->data.stop_data.events = noofevents; \
+    (h)->data.stop_data.lumiEvents = nooflumievents; \
     strncpy((h)->file_name, (fname), MAX_FILE_NAME); \
 }
 
@@ -74,6 +92,9 @@ void File::feedMonitor(void* tag, void** buf, int* size, int* first) {
   *buf = (void*)empty;
 }
 
+void File::feedMessageQueue(mqd_t mq) {
+}
+
 /// Constructor
 File::File(std::string fileName, unsigned int runNumber) {
   static int s_seqNo = 0;
@@ -88,6 +109,7 @@ File::File(std::string fileName, unsigned int runNumber) {
   m_mon->m_fileOpen = false;
   m_mon->m_bytesWritten = 0;
   m_mon->m_events = 0;
+  m_mon->m_lumiEvents = 0;
   m_fileName = fileName;
   m_md5 = new TMD5();
   m_prev = NULL;
@@ -119,22 +141,6 @@ MDFWriterNet::MDFWriterNet(const std::string& nam, ISvcLocator* pSvc) : MDFWrite
 /// Algorithm::execute() override, delegates to MDFWriter::execute().
 StatusCode MDFWriterNet::execute()
 {
-  // count event
-  if(m_currFile == NULL) {
-      // count into temporary variable and move this value to a file
-      // as soon as there is created one
-      m_events_tmp++;
-      *m_log << MSG::INFO
-             << "no of temporary events: "
-             << m_events_tmp
-             << endmsg;
-  } else {
-      m_currFile->incEvents();
-      *m_log << MSG::DEBUG
-             << "no of events: "
-             << m_currFile->getEvents()
-             << endmsg;
-  }
   return MDFWriter::execute();
 }
 
@@ -166,20 +172,45 @@ StatusCode MDFWriterNet::initialize(void)
     " Initializing." << endmsg;
 
   m_currFile = NULL;
-  m_events_tmp = 0;
   m_srvConnection = new Connection(m_serverAddr, m_serverPort,
 				   m_sndRcvSizes, m_log, this);
   m_rpcObj = new RPCComm(m_runDBURL.c_str());
   *m_log << MSG::INFO << "rundb url: " << m_runDBURL.c_str() <<endmsg;
   try {
-
     m_srvConnection->initialize();
-
   } catch(const std::exception& e) {
     *m_log << MSG::ERROR << "Caught Exception:" << e.what() << endmsg;
     return StatusCode::FAILURE;
   }
-
+  
+  // initialize named message queue
+  if((m_mq = mq_open("/writerqueue", O_RDWR, S_IRUSR|S_IWUSR, NULL)) == (mqd_t) -1)  {
+      *m_log << MSG::ERROR
+             << "Could not establish connection to message queue"
+             << endmsg;
+      m_mq_available = false;
+  } else {
+      *m_log << MSG::INFO
+             << "Established connection to message queue"
+             << endmsg;
+      m_mq_available = true;
+      // send message that this writer now exists
+      
+      size_t msg_size = snprintf(NULL, 0, "start%c%i", DELIMITER, getpid()) + 1;
+      char* msg = (char*) malloc(msg_size);
+      snprintf(msg, msg_size, "start%c%i", DELIMITER, getpid());
+      if(mq_send(m_mq, msg, msg_size, 0) < 0)  {
+          *m_log << MSG::ERROR
+                << "Error sending message to the queue"
+                << "deactivating message queue"
+                << endmsg;
+          m_mq_available = false; // prevent from further trying to protect writer
+      }
+      free(msg);
+      msg = NULL;
+      
+  }
+                      
   *m_log << " Writer " << getpid() << " Initialized." << endmsg;
   return StatusCode::SUCCESS;
 }
@@ -223,6 +254,33 @@ StatusCode MDFWriterNet::finalize(void)
   *m_log << MSG::INFO << WHERE
          << " Writer process finalized"
          << endmsg;
+  // log the termination of this instance of WriterNet
+  
+  if(m_mq_available) {
+      size_t msg_size = snprintf(NULL, 0, "stop%c%i", DELIMITER, getpid() ) + 1;
+      char* msg = (char*) malloc(msg_size);
+      snprintf(msg, msg_size, "stop%c%i", DELIMITER,  getpid() );
+      if(mq_send(m_mq, msg, msg_size, 0) < 0) {
+          *m_log << MSG::ERROR
+                 << "Could not send message"
+                 << "closing queue"
+                 << endmsg;
+          m_mq_available = false;
+      }
+      free(msg);
+      msg = NULL;
+  }
+  
+  // close message queue
+  
+  if(m_mq_available) {
+      // no error handling here: a not closed connection to 
+      // a message queue is nothing to worry about
+      if(mq_close(m_mq) == -1) {
+         // error closing
+      }
+      m_mq_available = false;
+  }
   return StatusCode::SUCCESS;
 }
 
@@ -240,6 +298,7 @@ std::string MDFWriterNet::createNewFile(unsigned int runNumber)
  */
 File* MDFWriterNet::createAndOpenFile(unsigned int runNumber)
 {
+  *m_log << MSG::INFO << "createAndOpenFile start" << endmsg;
   File *currFile = 0;
   struct cmd_header header;
   memset(&header, 0, sizeof(struct cmd_header));
@@ -266,10 +325,23 @@ File* MDFWriterNet::createAndOpenFile(unsigned int runNumber)
   m_srvConnection->sendCommand(&header);
   currFile->open();
   currFile->incSeqNum();
-  // it is likely that there have been events before the file was created
-  // so copy the actual value of events to the file monitor
-  // from now the events will be counted in the monitor
-  currFile->setEvents(m_events_tmp);
+
+  // log opening of file
+  if(m_mq_available) {
+      size_t msg_size = snprintf(NULL, 0, "openfile%c%i%c%s", DELIMITER, getpid(), DELIMITER, currFile->getMonitor()->m_name) + 1;
+      char* msg = (char*) malloc(msg_size);
+      snprintf(msg, msg_size, "openfile%c%i%c%s", DELIMITER, getpid(), DELIMITER, currFile->getMonitor()->m_name);
+      if(mq_send(m_mq, msg, msg_size, 0) < 0) {
+          *m_log << MSG::ERROR
+                 << "Could not send message"
+                 << "closing queue"
+                 << endmsg;
+          m_mq_available = false;
+      }
+      free(msg);
+      msg = NULL;
+  }
+  *m_log << MSG::INFO << "createAndOpenFile end" << endmsg;
   return currFile;
 }
 
@@ -288,7 +360,8 @@ void MDFWriterNet::closeFile(File *currFile)
 		     currFile->getSeqNum(),
 		     currFile->getRunNumber(),
              currFile->getBytesWritten(),
-             currFile->getEvents());
+             currFile->getEvents(),
+             currFile->getLumiEvents());
 //  *m_log << MSG::INFO << " Command: " << header.cmd << " "
 //         << "Filename: "   << header.file_name << " "
 //	 << "RunNumber: "  << header.run_no << " "
@@ -300,6 +373,23 @@ void MDFWriterNet::closeFile(File *currFile)
 //         << "Command size is: " << sizeof(header)
 //         << endmsg;
   m_srvConnection->sendCommand(&header);
+  
+  // log closing of file
+  if(m_mq_available) {
+      size_t msg_size = snprintf(NULL, 0, "closefile%c%i%c%s",  DELIMITER, getpid(), DELIMITER, currFile->getMonitor()->m_name) + 1;
+      char* msg = (char*) malloc(msg_size);
+      snprintf(msg, msg_size, "closefile%c%i%c%s", DELIMITER, getpid(), DELIMITER, currFile->getMonitor()->m_name);
+      if(mq_send(m_mq, msg, msg_size, 0) < 0) {
+          *m_log << MSG::ERROR
+                 << "Could not send message"
+                 << "closing queue"
+                 << endmsg;
+          m_mq_available = false;
+      }
+      free(msg);
+      msg = NULL;
+  } 
+  *m_log << MSG::INFO << WHERE << endmsg;
 }
 
 /* Writes out the buffer to the socket through the Connection object.
@@ -330,7 +420,7 @@ StatusCode MDFWriterNet::writeBuffer(void *const /*fd*/, const void *data, size_
            << "Very small message received, not forwarding. Length is: " << len
            << endmsg;
     return StatusCode::SUCCESS;
-  }
+  } 
 
   unsigned int runNumber = getRunNumber(data, len);
   if(m_currFile == NULL || runNumber != m_currFile->getRunNumber()) {
@@ -376,9 +466,41 @@ StatusCode MDFWriterNet::writeBuffer(void *const /*fd*/, const void *data, size_
 		     m_currFile->getSeqNum(),
 		     runNumber);
   m_srvConnection->sendCommand(&header, (void*)data);
+  // *m_log << MSG::INFO << WHERE << "elements in the queue: " << m_srvConnection->getQueueLength() << endmsg;
   size_t totalBytesWritten = m_currFile->updateWrite(data, len);
   m_currFile->incSeqNum();
 
+  // count event
+  m_currFile->incEvents();
+  
+  // physstat = (no of all events) - (lumi events)
+  // check type of event
+  if( checkForLumiEvent(data, len)) {
+      m_currFile->incLumiEvents();
+  }
+ 
+  // after every MB send statistics
+  if (m_mq_available && totalBytesWritten % 1048576 <= 1000) {
+      size_t msg_size = snprintf(NULL, 0, "log%c%i%c%s%c%s%zu",  
+      DELIMITER, getpid(), 
+      DELIMITER, m_currFile->getMonitor()->m_name,
+      DELIMITER, "bytesWritten=", totalBytesWritten) + 1;
+      char* msg = (char*) malloc(msg_size);
+      snprintf(msg, msg_size, "log%c%i%c%s%c%s%zu", 
+      DELIMITER, getpid(), 
+      DELIMITER, m_currFile->getMonitor()->m_name,
+      DELIMITER, "bytesWritten=", totalBytesWritten);
+      if(mq_send(m_mq, msg, msg_size, 0) < 0) {
+          *m_log << MSG::ERROR
+                 << "Could not send message"
+                 << "closing queue"
+                 << endmsg;
+          m_mq_available = false;
+      }
+      free(msg);
+      msg = NULL;
+  }
+  
   //How much have we written?
   if (totalBytesWritten > (m_maxFileSizeMB << 20)) {
     closeFile(m_currFile);
@@ -386,7 +508,10 @@ StatusCode MDFWriterNet::writeBuffer(void *const /*fd*/, const void *data, size_
     delete(m_currFile);
     m_currFile = NULL;
   }
-
+  
+  // WARNING: m_currFile might be NULL after the totalBytesWritten check above!
+  // -> segmentation fault
+  
   //Close it, reset counter.
   return StatusCode::SUCCESS;
 }
@@ -405,6 +530,26 @@ inline unsigned int MDFWriterNet::getRunNumber(const void *data, size_t /*len*/)
   else
     mHeader = (MDFHeader*)data;
   return mHeader->subHeader().H1->m_runNumber;
+}
+
+/** Checks if it is an lumi event (which means that bit 34 is set).
+ * @param data  The data from which MDF information may be retrieved
+ * @param len   The length of the data.
+ * @return true if it is an lumi event, false otherwise
+ */
+inline bool MDFWriterNet::checkForLumiEvent(const void *data, size_t ) {
+  MDFHeader *mHeader;
+  RawBank* b = (RawBank*)data;
+  if ( b->magic() == RawBank::MagicPattern )
+    mHeader = b->begin<MDFHeader>();
+  else
+    mHeader = (MDFHeader*)data;
+  unsigned int lumiBitMask = mHeader->subHeader().H0->m_trMask[1];
+  if(lumiBitMask & (unsigned int) 4) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 
@@ -455,12 +600,14 @@ void MDFWriterNet::notifyOpen(struct cmd_header *cmd)
  */
 void MDFWriterNet::notifyClose(struct cmd_header *cmd)
 {
+    *m_log << MSG::INFO << WHERE << " notifyClose start" << endmsg;
   try {
     m_rpcObj->confirmFile(cmd->file_name,
 			              cmd->data.stop_data.adler32_sum,
 			              cmd->data.stop_data.md5_sum,
                           cmd->data.stop_data.size,
-                          0);
+                          cmd->data.stop_data.events,
+                          cmd->data.stop_data.lumiEvents);
     *m_log << MSG::INFO << "Confirmed file " << cmd->file_name << endmsg;
   } catch(std::exception& rte) {
     char md5buf[33];
@@ -477,6 +624,7 @@ void MDFWriterNet::notifyClose(struct cmd_header *cmd)
     *m_log << " Adler32 Sum=" << cmd->data.stop_data.adler32_sum;
     *m_log << " MD5 Sum=" << md5buf << endmsg;
   }
+    *m_log << MSG::INFO << WHERE << " notifyClose end" << endmsg;
 }
 
 /** A notify listener callback, which is executed  when an error occurs.
