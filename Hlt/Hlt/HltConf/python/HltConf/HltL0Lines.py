b51bdead14a2d1fd9f312ce8db5c6d6a98ef59c2
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-01-09 19:32:52 +0000

    back out change for now
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74693 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4d568c0e3494ce62b747ab11dc3469334fc2ff82

--- Hlt/Hlt/HltConf/python/HltConf/HltL0Lines.py
+++ Hlt/Hlt/HltConf/python/HltConf/HltL0Lines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env gaudirun.py
 # =============================================================================
-# $Id: HltL0Lines.py,v 1.2 2009-01-09 12:05:40 graven Exp $
+# $Id: HltL0Lines.py,v 1.3 2009-01-09 19:32:52 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Hlt Lines which are plain L0 lines
@@ -12,28 +12,18 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.2 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.3 $"
 # =============================================================================
 
-from LHCbKernel.Configuration import *
 from HltConf.HltLine import Hlt1Line   as Line
 from HltConf.HltL0Candidates import *
 
-class HltL0LinesConf(LHCbConfigurableUser):
 # we postscale (instead of prescale!) these lines because 
 # TOS needs these candidates to exist
-   __slots__ = { 'Prescale'  : 1
-               , 'Postscale' : 0.000001
-               , 'L0Channels' : []  # if empty, use all pre-defined channels
-               }
+for i in L0Channels() :
+    Line ( 'L0' + i 
+         , L0DU  = "L0_CHANNEL('"+i+"')"
+         , algos = [ convertL0Candidates(i) ]
+         , postscale = 0.000001
+         )
 
-   def __apply_configuration__(self):
-        channels = self.getProp('L0Channels')
-        if not channels : channels = L0Channels()
-        for channel in channels :
-            Line ( 'L0' + channel 
-                 , L0DU  = "L0_CHANNEL('"+channel+"')"
-                 , prescale = self.getProp('Prescale')
-                 , algos = [ convertL0Candidates(channel) ]
-                 , postscale = self.getProp('Postscale')
-                 )
