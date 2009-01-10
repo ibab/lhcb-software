41f5883a3830418f01de783e6bc15645ced867d5
Marco Clemencic <marcocle@cern.ch>
2009-01-10 22:58:11 +0000

    Improved the test suite for VeloAlignCond to correctly test overrides and updates.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@24763 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 517d1d35302982cec208a58cd3d32d47f4a0860b

--- LHCb/Det/VeloDet/tests/scripts/VeloAlignmentTest.py
+++ LHCb/Det/VeloDet/tests/scripts/VeloAlignmentTest.py
@@ -8,24 +8,40 @@ ConditionPaths =  ["/dd/Conditions/Online/Velo/MotionSystem",
                    "/dd/Conditions/Alignment/Velo/VeloLeft",
                    "/dd/Conditions/Alignment/Velo/VeloRight"]
 
-def configure():
-    from Gaudi.Configuration import (ApplicationMgr,
-                                     MessageSvc)
-    from Configurables import DDDBConf
-    DDDBConf(DbRoot = "../data/lhcb.xml")
+use_cases = { "simple": "basic/plain geometry",
+              "override_motion_system": "change the Velo position overriding motion system data",
+              "override_alignment": "change the Velo position overriding alignment condition",
+              "override_alignment2": "override [XY]OffsetCoeffs of alignment condition",
+              }
+
+
+def configure(use_case = "simple"):
+    # Common configuration
+    import Gaudi.Configuration
+    from Configurables import (ApplicationMgr, MessageSvc,
+                               DDDBConf, CondDB, CondDBAccessSvc,
+                               UpdateManagerSvc)
+    DDDBConf() # detector description
+    localDb = CondDBAccessSvc("VeloAlignCondTestDB",
+                              ConnectionString = "sqlite_file:../data/VeloAlignCondTest.db/DDDB",
+                              DefaultTAG = "simple")
+    CondDB().addLayer(localDb) # use local DB
     
-    # This is needed to trigger the instantiation of the update manager service 
-    from Configurables import DetCondTest__TestConditionAlg
-    alg = DetCondTest__TestConditionAlg()
-    alg.Conditions = ConditionPaths
-    
-    ApplicationMgr(TopAlg = [alg], EvtSel = "NONE")
+    ApplicationMgr(TopAlg = [], EvtSel = "NONE")
     #MessageSvc(OutputLevel = 1)
     
-    # Example of how to move the Velo
-    #from Configurables import UpdateManagerSvc
-    #UpdateManagerSvc().ConditionsOverride.append("Conditions/Online/Velo/MotionSystem := double YOffset = 100")
-
+    if use_case == "simple":
+        # nothing to do more
+        return
+    elif use_case == "override_motion_system":
+        # Example of how to move the Velo
+        UpdateManagerSvc().ConditionsOverride.append("Conditions/Online/Velo/MotionSystem := double ResolPosY = 100")
+    elif use_case == "override_alignment":
+        UpdateManagerSvc().ConditionsOverride.append("Conditions/Alignment/Velo/VeloLeft := double_v dPosXYZ = 0 100 0")
+    elif use_case == "override_alignment2":
+        UpdateManagerSvc().ConditionsOverride.append("Conditions/Alignment/Velo/VeloLeft := double_v YOffsetCoeffs = 20 -5")
+        UpdateManagerSvc().ConditionsOverride.append("Conditions/Alignment/Velo/VeloRight := double_v YOffsetCoeffs = 0 1")
+    
 def datastore_walk(ds, top = "/"):
     if top == "/":
         top = ds._idm.rootName()
@@ -40,8 +56,16 @@ def datastore_walk(ds, top = "/"):
 def node_names(ds, top = "/"):
     return [ obj.registry().identifier() for obj in datastore_walk(ds,top) ]
 
-def main():
-    configure()
+def main(use_case = "simple"):
+    if use_case not in use_cases:
+        print "ERROR: uknown use case to test '%s'" % use_case
+        return 2
+    screenwidth = 120
+    print "#" * screenwidth
+    txt_use_case = "testing use case '%s': %s" % (use_case, use_cases[use_case])
+    print " " * max(0,(screenwidth - len(txt_use_case))/2) + txt_use_case
+    print "#" * screenwidth
+    configure(use_case)
     
     from Gaudi.Configuration import configurationDict
     from pprint import pprint
@@ -53,23 +77,47 @@ def main():
     app = GaudiPython.AppMgr()
     pprint(configurationDict())
     app.initialize()
+    app.start()
     
     dds = app.detsvc()
     # load everything in the store
     nodes = node_names(dds)
-
+    app.run(1)
+    
     print "=== Begin Conditions ==="
     for n in ConditionPaths:
         print n
         print dds[n]
     print "=== End Conditions ==="
-    
-    expected = { "Left": ROOT.Math.Transform3D(1, 0, 0, 30,
-                                               0, 1, 0, 1,
-                                               0, 0, 1, 0),
-                 "Right": ROOT.Math.Transform3D(1, 0, 0, -30,
-                                                0, 1, 0, 15,
-                                                0, 0, 1, 0) }
+
+    if use_case == "simple":
+        expected = { "Left": ROOT.Math.Transform3D(1, 0, 0, 30,
+                                                   0, 1, 0, 1,
+                                                   0, 0, 1, 0),
+                     "Right": ROOT.Math.Transform3D(1, 0, 0, -30,
+                                                    0, 1, 0, 15,
+                                                    0, 0, 1, 0) }
+    elif use_case == "override_motion_system":
+        expected = { "Left": ROOT.Math.Transform3D(1, 0, 0, 30,
+                                                   0, 1, 0, 100,
+                                                   0, 0, 1, 0),
+                     "Right": ROOT.Math.Transform3D(1, 0, 0, -30,
+                                                    0, 1, 0, -480,
+                                                    0, 0, 1, 0) }
+    elif use_case == "override_alignment":
+        expected = { "Left": ROOT.Math.Transform3D(1, 0, 0, 30,
+                                                   0, 1, 0, 101,
+                                                   0, 0, 1, 0),
+                     "Right": ROOT.Math.Transform3D(1, 0, 0, -30,
+                                                    0, 1, 0, 15,
+                                                    0, 0, 1, 0) }
+    elif use_case == "override_alignment2":
+        expected = { "Left": ROOT.Math.Transform3D(1, 0, 0, 30,
+                                                   0, 1, 0, 15,
+                                                   0, 0, 1, 0),
+                     "Right": ROOT.Math.Transform3D(1, 0, 0, -30,
+                                                    0, 1, 0, 1,
+                                                    0, 0, 1, 0) }
 
     result = 0 # success
     for i in expected:
@@ -78,9 +126,13 @@ def main():
         if m != expected[i]:
             print "ERROR: transforamtion for %s is not what expected" % path
             result = 1
-    
+
     return result
 
 if __name__ == '__main__':
-    from sys import exit
-    exit(main())
+    from sys import exit, argv
+    if len(argv) > 1:
+        use_case = argv[1]
+    else:
+        use_case = "simple"
+    exit(main(use_case))
