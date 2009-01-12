da1c0b5745b78243c9a8caefa3057a4b024b6fbe
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-12 12:35:13 +0000

    improve test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@60294 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6643327c3bde0afe8db1894adca30ec3333435b1

--- Boole/BooleSys/tests/options/testBoole.py
+++ Boole/BooleSys/tests/options/testBoole.py
@@ -9,8 +9,5 @@ Boole().EvtMax   = 0
 Boole().Outputs  = ["DIGI","MDF","L0ETC"] # Test all output types
 Boole().Histograms = "NONE"
 
-# Do not compare times....
-TimingAuditor().OutputLevel = WARNING 
-
 LHCbApp().CondDBtag = "default"
 LHCbApp().DDDBtag   = "default"
