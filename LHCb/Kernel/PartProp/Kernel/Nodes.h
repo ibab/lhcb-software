ac9521b9d72028c2575741f21620c546552682d7
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-12 06:35:38 +0000

    remove windows characters
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48170 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1c96c16554ac21dd5478ffc1d6411fae5536a9e7

--- LHCb/Kernel/PartProp/Kernel/Nodes.h
+++ LHCb/Kernel/PartProp/Kernel/Nodes.h
@@ -1,4 +1,4 @@
-// $Id: Nodes.h,v 1.2 2009-01-10 14:02:02 ibelyaev Exp $
+// $Id: Nodes.h,v 1.3 2009-01-12 06:35:38 cattanem Exp $
 // ============================================================================
 #ifndef DAVINCI_DECAYNODES_H 
 #define DAVINCI_DECAYNODES_H 1
