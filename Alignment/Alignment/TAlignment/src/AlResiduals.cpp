2128144f4f9554ff2d541405d2431c81f30dd191
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-01-13 15:57:24 +0000

    restructering to facilitate computing only subset of residual correlations
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88736 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 01c856833d28b0d1c746b6a0f57b44e2e06b8986

--- Alignment/Alignment/TAlignment/src/AlResiduals.cpp
+++ Alignment/Alignment/TAlignment/src/AlResiduals.cpp
@@ -1,39 +1,68 @@
+#include <sstream>
+#include <set>
 #include "AlResiduals.h"
+#include "AlignmentElement.h"
 #include "Event/Node.h"
 
 namespace Al
 {
   
-  size_t Residuals::index( const LHCb::Node& node) const 
-  {
-    size_t rc(0) ;
-    while(  rc != m_nodes.size() && m_nodes[rc] != &node ) ++rc ;
-    assert( rc < m_nodes.size() ) ;
-    //if( rc == m_nodes.size() ) throw std::runtime_error ;
-    return rc ;
+  Residual1D::Residual1D(const LHCb::Node& n, const AlignmentElement& e) :
+    m_node(&n), m_element(&e),
+    m_r(n.residual()), m_V(n.errMeasure2()), m_HCH(n.errMeasure2()-n.errResidual2()) {}
+
+  Residuals::Residuals(double chi2, size_t ndof, size_t numexthits) 
+    : m_chi2(chi2), m_nDoF(ndof), m_nExternalHits(numexthits) {}
+
+  size_t Residuals::nAlignables() const {
+    std::set<size_t> alignables ;
+    for( ResidualContainer::const_iterator inode = m_residuals.begin() ;
+	 inode != m_residuals.end(); ++inode )
+      alignables.insert( inode->element().index() ) ;
+    return alignables.size() ;
   }
 
-  Residuals::Residuals( const NodeContainer& nodes, double chisq, size_t ndof ) 
-    : m_nodes(nodes),
-      m_r(nodes.size()),
-      m_V(nodes.size()),
-      m_HCH(nodes.size(),0),
-      m_chi2(chisq),
-      m_nDoF(ndof)
+  bool Residuals::testHCH(std::string& message) const
   {
-    for(size_t index = 0 ; index != nodes.size(); ++index ) {
-      const LHCb::Node* node = nodes[index] ;
-      m_r(index+1) = node->residual() ;
-      m_V(index+1) = node->errMeasure2() ;
-      m_HCH(index+1,index+1) = node->errMeasure2() - node->errResidual2() ;
+
+   // Let's check the result. Keep track of the roots.
+    std::stringstream warningmsg ;
+    const double tol = 1e-3 ;
+    std::vector<double> HCHdiagroots ;
+    std::vector<double> Rdiagroots ;
+    bool error(false) ;
+    for(size_t irow=0; irow<size() && !error; ++irow) {
+      double HCH = m_residuals[irow].HCH() ;
+      double V   = m_residuals[irow].V() ;
+      error = ! (0<=HCH) ;
+      if(error) warningmsg << "found negative element on diagonal: " << HCH << std::endl ;
+      error = ! ( HCH<V ) ;
+      if(error) {
+	warningmsg << "found too large element on diagonal: " << HCH/V << std::endl ;
+      }
+      HCHdiagroots.push_back( std::sqrt(HCH) ) ;
+      Rdiagroots.push_back( std::sqrt(V-HCH)) ;
     }
+    bool offdiagerror(false) ;
+    for( Al::Residuals::CovarianceContainer::const_iterator it = m_HCHElements.begin() ; 
+	 it != m_HCHElements.end() && !error; ++it) {
+      size_t irow = it->row ;
+      size_t icol = it->col ;
+      double c = it->val / (HCHdiagroots[irow]*HCHdiagroots[icol]) ;
+      bool thiserror =  ! (-1-tol < c && c <1+tol) ;
+      double d = it->val / (Rdiagroots[irow]*Rdiagroots[icol]) ;
+      bool thisderror =  ! (-1-tol < d && d <1+tol) ;
+      if(thiserror||thisderror) {
+	warningmsg << "found bad element on offdiagonal: " 
+		  << irow << " " << icol << " " 
+		  << c << " " << d << std::endl ;
+      }
+      offdiagerror |= thiserror || thisderror ;
+    }
+    error = error || offdiagerror ;
+    if(error) message = warningmsg.str() ;
+    return error ;
   }
 
-  TrackResiduals::TrackResiduals(const LHCb::Track& track,
-				 const NodeContainer& nodes) 
-    : Residuals(nodes, track.chi2(), track.nDoF()),
-      m_state( nodes.front()->state() ),
-      m_residualStateCov(size()) 
-  {
-  }
+
 }
--- Alignment/Alignment/TAlignment/src/AlResiduals.h
+++ Alignment/Alignment/TAlignment/src/AlResiduals.h
@@ -1,4 +1,4 @@
-// $Id: AlResiduals.h,v 1.4 2008-07-17 09:28:07 wouter Exp $
+// $Id: AlResiduals.h,v 1.5 2009-01-13 15:57:24 wouter Exp $
 #ifndef TRACKTOOLS_TrackResiduals_H 
 #define TRACKTOOLS_TrackResiduals_H 1
 
@@ -8,51 +8,74 @@
 // from TrackEvent
 #include "Event/Track.h"
 #include "Kernel/LHCbID.h"
-
 #include <vector>
-#include "CLHEP/Matrix/SymMatrix.h"
-#include "CLHEP/Matrix/DiagMatrix.h"
-#include "CLHEP/Matrix/Matrix.h"
-#include "CLHEP/Matrix/Vector.h"
+
+// forward declarations
+namespace LHCb {
+  class Track ;
+  class Node ;
+}
+class AlignmentElement ;
 
 namespace Al
 {
+  class Residual1D
+  {
+  public:
+    Residual1D(const LHCb::Node& n, const AlignmentElement& e ) ;
+    const LHCb::Node& node() const { return *m_node ; }
+    const AlignmentElement& element() const { return *m_element ; }
+    double r() const { return m_r ; }
+    double V() const { return m_V ; }
+    double HCH() const { return m_HCH ; }
+    double R() const { return V() - HCH() ; }
+    double chi2() const { return m_r*m_r/R() ; }
+    void setHCH( double hch ) { m_HCH = hch ; }
+    void setR( double r ) { m_r = r ; }
+  private:
+    const LHCb::Node* m_node ;
+    const AlignmentElement* m_element ;
+    double m_r ;
+    double m_V ;
+    double m_HCH ;
+  } ;
+  
+  struct CovarianceElement
+  {
+    CovarianceElement(size_t r, size_t c, double v) : row(r),col(c),val(v) {}
+    size_t row ;
+    size_t col ;
+    double val ;
+    double operator*(double x) const { return val * x ; }
+    double operator+=(double x) { return val += x ; }
+  } ;
   
   class Residuals
   {
   public:
-    typedef std::vector<const LHCb::Node*> NodeContainer ;
-    Residuals(const NodeContainer&, double chisq, size_t ndof) ;
-    Residuals(size_t size = 0)  : m_nodes(size), m_r(size), m_V(size), m_HCH(size) {}
+    typedef std::vector<Residual1D>        ResidualContainer ;
+    typedef std::vector<CovarianceElement> CovarianceContainer ;
 
-    size_t size() const { return m_nodes.size() ; }
-    double r(size_t i) const { return m_r(i+1) ; }
-    double V(size_t i) const { return m_V(i+1) ; }
-    double R(size_t i) const { return V(i) - HCH(i,i) ; }
-    double V(size_t i, size_t j) const { return i==j ? V(i) : 0 ; }
-    double R(size_t i, size_t j) const { return V(i,j) - HCH(i,j) ; }
-    double HCH(size_t i, size_t j) const { return m_HCH(i+1,j+1) ; }
-    double HCH_norm(size_t i, size_t j) const { return m_HCH(i+1,j+1)/sqrt( V(i) * V(j) ) ; }
-	
-    const LHCb::Node& node(size_t i) const { return *(m_nodes[i]) ; }
-    size_t index(const LHCb::Node& node) const ;
-//     double HCH_norm(const LHCb::Node& inode, const LHCb::Node& jnode) const {
-//       size_t i(index(inode)), j(index(jnode)) ;
-//       return HCH(i,j)/sqrt( V(i) * V(j) ) ;
-//     }
-    const NodeContainer& nodes() const { return m_nodes ; }
+    Residuals(double chisq, size_t ndof, size_t numexthits=0) ;
+    
+    size_t size() const { return m_residuals.size() ; }
     double chi2() const { return m_chi2 ; }
     int nDoF() const { return m_nDoF ; }
-
+    size_t nExternalHits() const { return m_nExternalHits ; }
+    const CovarianceContainer& HCHElements() const { return m_HCHElements ; }
+    const CovarianceContainer& hch() const { return m_HCHElements ; }
+    const ResidualContainer& residuals() const { return m_residuals ; }
+    const Residual1D& residual(size_t i) const { return m_residuals[i] ; }
+    bool testHCH( std::string& message ) const ;
+    size_t nAlignables() const ;
   private:
     friend class TrackResidualTool ;
     friend class VertexResidualTool ;
-    NodeContainer m_nodes ;
-    CLHEP::HepVector     m_r ;   // vector of residuals
-    CLHEP::HepVector     m_V ;   // measurement cov matrix
-    CLHEP::HepSymMatrix  m_HCH ; // matrix HCH
+    ResidualContainer   m_residuals ;
+    CovarianceContainer m_HCHElements ; // off-diagonal elements of matrix HCH
     double m_chi2 ;
     int m_nDoF ;
+    size_t m_nExternalHits ;
   };
 
   class TrackResiduals : public Residuals
@@ -62,14 +85,12 @@ namespace Al
     typedef std::vector< Gaudi::TrackProjectionMatrix > ProjectionMatrix ;
     
     /// Standard constructor
-    TrackResiduals(const LHCb::Track& track,
-		   const NodeContainer& nodes) ;
+    TrackResiduals(const LHCb::Track& track) : Residuals(track.chi2(), track.nDoF()) {}
     const LHCb::State& state() const { return m_state ; }
     const ProjectionMatrix& residualStateCov() const { return m_residualStateCov ; }
   private:
     friend class TrackResidualTool ;
     LHCb::State m_state ;                  // most upstream state
-    //CLHEP::HepMatrix m_residualStateCov  ; // correlation matrix between that state and the residuals
     ProjectionMatrix m_residualStateCov ;
   };
   
@@ -77,10 +98,9 @@ namespace Al
   {
   public:
     /// Standard constructor
-    MultiTrackResiduals(const NodeContainer& nodes,
-			double chi2, int ndof,
+    MultiTrackResiduals(double chi2, int ndof, size_t numexthits,
 			size_t numtracks, double vertexchi2, int vertexndof) 
-      : Residuals(nodes,chi2,ndof),m_numtracks(numtracks),m_vertexchi2(vertexchi2),m_vertexndof(vertexndof) {}
+      : Residuals(chi2,ndof,numexthits),m_numtracks(numtracks),m_vertexchi2(vertexchi2),m_vertexndof(vertexndof) {}
     ~MultiTrackResiduals( ) {}; ///< Destructor
     double vertexChi2() const { return m_vertexchi2 ; }
     int    vertexNDoF() const { return m_vertexndof ; }
