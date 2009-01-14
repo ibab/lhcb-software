668134fb0c34fdfc0f8e79663adf941d503fc44e
Markus Frank <frankb@cern.ch>
2009-01-14 12:54:34 +0000

    no message
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98934 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bc53562b1207fb275fc4653688ddac89e8d1510e

--- Online/OnlineSys/cmt/build.bat
+++ Online/OnlineSys/cmt/build.bat
@@ -1,8 +1,6 @@
 if exist %CMTINSTALLAREA% rd /q/s %CMTINSTALLAREA%
 set QUIET=1
-cmt broadcast if exist ..\Visual rd /q/s ..\Visual
 cmt broadcast if exist ..\%CMTCONFIG% rd /q/s ..\%CMTCONFIG%
 cmt broadcast cmt config
-cmt broadcast cmt build vsnet
 cmt broadcast cmt make > build_%CMTCONFIG%.log
 
