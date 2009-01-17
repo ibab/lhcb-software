4395cdc8ea5b34047ea11a4f68d477040b2f43d4
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-01-17 08:46:30 +0000

    change prescale in HltL0Lines into postscale
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@74879 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 15a27cba091ff6bb4872650b3969cddf5e91e9df

--- Hlt/Hlt/HltConf/python/HltConf/HltL0Lines.py
+++ Hlt/Hlt/HltConf/python/HltConf/HltL0Lines.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# $Id: HltL0Lines.py,v 1.5 2009-01-12 12:05:16 graven Exp $
+# $Id: HltL0Lines.py,v 1.6 2009-01-17 08:46:30 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Hlt Lines which are plain L0 lines
@@ -11,7 +11,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.5 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.6 $"
 # =============================================================================
 
 from LHCbKernel.Configuration import *
@@ -20,8 +20,8 @@ from HltConf.HltL0Candidates import *
 from HltConf.HltLine     import hlt1Lines
 
 class HltL0LinesConf(LHCbConfigurableUser) :
-   __slots__ = { 'Prescale'  : 0.000001
-               , 'Postscale' : 1
+   __slots__ = { 'Prescale'  : 1 
+               , 'Postscale' : 0.000001
                , 'L0Channels' : []  # if empty, use all pre-defined channels
                }
 
