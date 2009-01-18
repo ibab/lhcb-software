052a12018bd15f122eb8d2f32e88b605eb916478
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-01-18 17:24:24 +0000

    add script to install TCK manifest in InstallArea
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@74893 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9108804478ff519cfac14c83a5ba759c28a73b84

---
+++ Moore/Hlt/Moore/scripts/install_TCK_manifest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from shutil  import copy2
+import os
+from os.path import exists, dirname
+import  subprocess
+import string
+import sys
+
+cmt = subprocess.Popen(['cmt','show','version'],stdout=subprocess.PIPE)
+version = string.strip(cmt.communicate()[0],'\n')
+
+manifest = os.environ['TCKDATAROOT'] + '/manifest/MOORE_' + version
+target_dir = dirname( sys.argv[1] )
+if exists( manifest)        : 
+    print 'copying ' + manifest + ' to ' + sys.argv[1]
+    if not exists( target_dir ) : makedirs( target_dir )
+    copy2( manifest,  sys.argv[1] )
+else:
+    print 'manifest  ' + manifest + ' does not exist'
