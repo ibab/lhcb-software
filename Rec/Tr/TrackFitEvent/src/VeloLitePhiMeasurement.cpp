97b7e7327ceaabbd04e0819ca514be50fb179683
David Hutchcroft <dhcroft@cern.ch>
2009-01-19 11:49:37 +0000

    Added Velo "Lite" measurements
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@18662 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: de2263344b079654630465c43cb7f7ca18d49418

---
+++ Rec/Tr/TrackFitEvent/src/VeloLitePhiMeasurement.cpp
@@ -0,0 +1,87 @@
+// $Id: VeloLitePhiMeasurement.cpp,v 1.1 2009-01-19 11:49:37 dhcroft Exp $
+// Include files 
+
+// From Event
+#include "Event/VeloCluster.h"
+
+// From VeloDet
+#include "VeloDet/DeVelo.h"
+
+// local
+#include "Event/VeloLitePhiMeasurement.h"
+
+using namespace LHCb;
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : VeloLitePhiMeasurement
+//
+// Author: David Hutchcroft
+// Created: 2008-11-18
+//-----------------------------------------------------------------------------
+
+/// Standard constructor, initializes variables
+VeloLitePhiMeasurement::VeloLitePhiMeasurement( const VeloLiteCluster& aCluster,
+                                        const DeVelo& det,
+                                        const IVeloClusterPosition& clusPosTool,
+                                        const LHCb::StateVector& refVector )
+  : VeloLiteMeasurement(Measurement::VeloLitePhi,aCluster)
+{
+  IVeloClusterPosition::toolInfo clusInfo = 
+    clusPosTool.position(this->cluster(),refVector.position(),
+                         std::pair<double,double>(refVector.tx(),refVector.ty())) ;
+  this->init( det, clusInfo ) ;
+}
+
+/// Standard constructor, initializes variables
+VeloLitePhiMeasurement::VeloLitePhiMeasurement( const VeloLiteCluster& aCluster,					
+                                        const DeVelo& det,
+                                        const IVeloClusterPosition& clusPosTool) 
+  : VeloLiteMeasurement(Measurement::VeloLitePhi,aCluster)
+{
+  IVeloClusterPosition::toolInfo clusInfo = clusPosTool.position(cluster());
+  this->init( det, clusInfo ) ;
+}
+
+void VeloLitePhiMeasurement::init( const DeVelo& det, const IVeloClusterPosition::toolInfo& clusInfo)
+{
+  // Fill the data members
+  const DeVeloPhiType* phiDet = det.phiSensor( channelID() );
+  m_detectorElement = phiDet ;
+  m_z = phiDet->z();
+  m_origin = phiDet -> globalOrigin();
+  
+  // Store only the 'position', which is the signed distance from strip to
+  // the origin.
+  m_measure = phiDet -> distToOrigin(  channelID().strip() );
+  // fix sign convention of d0 of strip
+  if( ! phiDet -> isDownstream() ) {
+    m_measure = -m_measure;
+  }
+  
+  m_errMeasure  = clusInfo.fractionalError *
+    phiDet -> phiPitch( clusInfo.strip.strip() );  
+  
+  m_trajectory = phiDet -> trajectory( clusInfo.strip, clusInfo.fractionalPosition );
+}
+
+double VeloLitePhiMeasurement::resolution( const Gaudi::XYZPoint& point,
+                                       const Gaudi::XYZVector& /*vec*/) const
+{
+  // Retrieve the measurement error
+  const double radius = (m_origin - point).R();
+  return radius * m_errMeasure;
+}
+
+double VeloLitePhiMeasurement::resolution2( const Gaudi::XYZPoint& point,
+                                        const Gaudi::XYZVector& /*vec*/) const
+{
+  // Retrieve the measurement error
+  const double radius = (m_origin - point).R();
+  return radius * m_errMeasure * radius * m_errMeasure;
+}
+
+const DeVeloPhiType& VeloLitePhiMeasurement::sensor() const{                                                
+  return *static_cast<const DeVeloPhiType *>(detectorElement()); 
+}                                   
+
+
---
+++ Rec/Tr/TrackFitEvent/src/VeloLiteRMeasurement.cpp
@@ -0,0 +1,65 @@
+// $Id: VeloLiteRMeasurement.cpp,v 1.1 2009-01-19 11:49:37 dhcroft Exp $
+// Include files
+
+// From VeloDet
+#include "VeloDet/DeVelo.h"
+
+// From Event
+#include "Event/VeloCluster.h"
+
+// local
+#include "Event/VeloLiteRMeasurement.h"
+
+using namespace LHCb;
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : VeloLiteRMeasurement
+//
+// Author: David Hutchcroft
+// Created: 2008-11-18
+//-----------------------------------------------------------------------------
+
+/// Standard constructor, initializes variables
+VeloLiteRMeasurement::VeloLiteRMeasurement( const VeloLiteCluster& aCluster,
+                                    const DeVelo& det,
+                                    const IVeloClusterPosition& clusPosTool,
+                                    const LHCb::StateVector& refVector )
+  : VeloLiteMeasurement(Measurement::VeloLiteR,aCluster)
+
+{
+  IVeloClusterPosition::toolInfo clusInfo = 
+    clusPosTool.position(this->cluster(),refVector.position(),
+                         std::pair<double,double>(refVector.tx(),refVector.ty())) ;
+  this->init( det, clusInfo ) ;
+}
+
+/// Standard constructor, initializes variables
+VeloLiteRMeasurement::VeloLiteRMeasurement( const VeloLiteCluster& aCluster,
+                                    const DeVelo& det, 
+                                    const IVeloClusterPosition& clusPosTool) 
+  : VeloLiteMeasurement(Measurement::VeloLiteR,aCluster)
+{
+  IVeloClusterPosition::toolInfo clusInfo = clusPosTool.position(this->cluster());
+  this->init( det, clusInfo ) ;
+}
+
+void VeloLiteRMeasurement::init( const DeVelo& det, const IVeloClusterPosition::toolInfo& clusInfo)
+{
+  // Fill the data members
+  const DeVeloRType* rDet=det.rSensor( channelID().sensor() );
+  m_detectorElement = rDet ;
+  m_z = rDet -> z();
+  
+  m_measure = rDet -> rOfStrip( clusInfo.strip.strip() ) +
+    rDet -> rPitch( clusInfo.strip.strip() ) * clusInfo.fractionalPosition;
+  m_errMeasure = rDet -> rPitch( clusInfo.strip.strip() )
+    * clusInfo.fractionalError;
+
+  m_trajectory = rDet -> trajectory( clusInfo.strip, clusInfo.fractionalPosition );
+}
+
+
+const DeVeloRType& VeloLiteRMeasurement::sensor() const{
+  return *static_cast<const DeVeloRType *>(detectorElement());
+}
+
