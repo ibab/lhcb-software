aded11579bdc5a90017d9c8870489e6a69a2a59b
Tomasz Skwarnicki <tskwarni@syr.edu>
2009-01-19 21:19:42 +0000

    adopt TupleToolTISTOS to new way of doing L0 triggers
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@74914 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2019b21611fe7c6b137407a57f61e538c1eb1b7c

--- Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.cpp
@@ -10,7 +10,6 @@
 #include "GaudiAlg/TupleObj.h"
 // local
 #include "TupleToolTISTOS.h"
-#include "Event/L0DUReport.h"
 
 #include "Event/HltSummary.h"
 
@@ -73,20 +72,25 @@ StatusCode TupleToolTISTOS::fill( const LHCb::Particle*
 
     m_TriggerTisTosTool->setOfflineInput(*P);
 
-    //Do the L0
-    m_TriggerTisTosTool->setTriggerInput("L0*Decision");
+    m_TriggerTisTosTool->setTriggerInput("Hlt1L0*Decision");
+    std::vector<std::string> vs = m_TriggerTisTosTool->triggerSelectionNames();
     //Fill the decision, tis and tos parametres for the L0 as a whole
-    m_TriggerTisTosTool->triggerTisTos(decision,tis,tos);
+    m_TriggerTisTosTool->selectionTisTos(vs,decision,tis,tos);
+    // decision here is that of Hlt1 L0 pass through lines and not of L0 itself
+    // however if any candidates were saved then L0 decision must have been true
+    decision = m_TriggerTisTosTool->hltObjectSummaries().size()!=0;
     tuple->column( head+"L0Global"+"_Dec", decision);
     tuple->column( head+"L0Global"+"_TIS", tis);
     tuple->column( head+"L0Global"+"_TOS", tos);
     //Now loop over all the subtriggers
-    std::vector<std::string> vs = m_TriggerTisTosTool->triggerSelectionNames();
     for( std::vector< std::string >::const_iterator s=vs.begin();s != vs.end();++s){
-      m_TriggerTisTosTool->triggerTisTos(*s,decision,tis,tos);
-      tuple->column( head+*s+"_Dec", decision);
-      tuple->column( head+*s+"_TIS", tis);
-      tuple->column( head+*s+"_TOS", tos);
+      m_TriggerTisTosTool->selectionTisTos(*s,decision,tis,tos);
+      // need to overwrite Hlt1L0XXXDecision 
+      decision = m_TriggerTisTosTool->hltSelectionObjectSummaries(*s).size()!=0;      
+      std::string sl0 = s->substr( s->find("L0") );      
+      tuple->column( head+sl0+"_Dec", decision);
+      tuple->column( head+sl0+"_TIS", tis);
+      tuple->column( head+sl0+"_TOS", tos);
     }
 
     //Do the Hlt1
