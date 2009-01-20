1857474c5bd8657f91cd5f099ec1829489ce7235
jpalac <jpalac@cern.ch>
2009-01-20 15:16:14 +0000

    . Namespace containing types corresponding to the Particle -> MCParticle     uni-directional weighted relationship.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64555 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fef415cc21e9ffcafb732f721c7eb18c70123646

---
+++ Phys/Phys/DaVinciMCKernel/Kernel/Particle2MCParticle.h
@@ -0,0 +1,36 @@
+// $Id: Particle2MCParticle.h,v 1.1 2009-01-20 15:16:14 jpalac Exp $
+#ifndef KERNEL_PARTICLE2MCPARTICLE_H 
+#define KERNEL_PARTICLE2MCPARTICLE_H 1
+
+// Include files
+#include "Event/MCParticle.h"
+#include "Event/Particle.h"
+#include "Relations/RelationWeighted.h"
+#include "Relations/RelationWeighted1D.h"
+
+/** @namespace Particle2MCParticle Kernel/Particle2MCParticle.h
+ *  
+ *
+ *  Namespace containing types corresponding to the Particle -> MCParticle 
+ *  uni-directional weighted relationship.
+ *
+ *  @author Juan PALACIOS
+ *  @date   2009-01-20
+ */
+namespace Particle2MCParticle {
+  typedef double                                          WeightType ;
+  typedef LHCb::RelationWeighted1D<LHCb::Particle, 
+                                   LHCb::MCParticle, 
+                                   WeightType>            Table      ;
+
+  typedef Relations::RelationWeighted<LHCb::Particle, 
+                                      LHCb::MCParticle, 
+                                      WeightType>         LightTable ;
+  typedef Table::Range                                    Range      ;
+  typedef Table::IBase::TypeTraits::Entry                 Relation   ;
+  typedef Table::IBase::TypeTraits::Entries               Relations  ;
+  typedef Table::To                                       To         ;
+  typedef Table::From                                     From       ;
+  typedef WeightType                                      Weight     ;
+}
+#endif // KERNEL_PARTICLE2MCPARTICLE_H
