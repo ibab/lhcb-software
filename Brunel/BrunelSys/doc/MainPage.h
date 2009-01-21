e6d621b646e2ffc8f68afab8f33a838f4c85512c
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-21 14:37:22 +0000

    v34r1p1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30437 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 841c8e3267da0cdf8f9fe144d0c854a51c9ea95a

--- Brunel/BrunelSys/doc/MainPage.h
+++ Brunel/BrunelSys/doc/MainPage.h
@@ -2,9 +2,9 @@
  *  \anchor bruneldoxygenmain
  *
  * This reference manual documents all %LHCb and %Gaudi classes accessible from
- * the Brunel reconstruction program environment. More information on Brunel,
- * including a list of available releases and release notes, can be found in the 
- * <a href=" http://cern.ch/lhcb-release-area/DOC/brunel/">Brunel project Web pages</a>
+ * the <a href=" http://cern.ch/lhcb-release-area/DOC/brunel/">Brunel reconstruction program</a> environment. 
+ * More information on this and other %Brunel releases can be found in the
+ * <a href="../../index.php">Brunel project Web pages</a>
  *
  * <hr>
  * <h2>Top level options files</h2>
@@ -16,15 +16,15 @@
  * <h4>Options for 2008 settings</h4>
  * \li Brunel-2008.py:      default setting for 2008 real data 
  * \li Brunel-2008-MC.py:   default setting for 2008 simulation 
- * \li Brunel-2008-MC-VeloOpen.py: 2008 simulation with Velo open
- * \li Brunel-2008-MC-BOff-VeloOpen.py: 2008 simulation with Velo open and B field off
+ * \li Brunel-2008-MC-VeloOpen.py: 2008 simulation with %Velo open
+ * \li Brunel-2008-MC-BOff-VeloOpen.py: 2008 simulation with %Velo open and B field off
  * <h4>Options for CCRC settings</h4>
  * \li Brunel-CCRCStripping.py: setting for CCRC Stripping production workflow 
- * <h4>Options for DC06 settings</h4>
- * \li Brunel-DC06.py:          default setting for DC06 simulated data
- * \li Brunel-DC06Rdst.py:      setting for DC06 Rdst production workflow
- * \li Brunel-DC06L0Rdst.py:    setting for DC06 Rdst production workflow, reconstructing only L0 yes events
- * \li Brunel-DC06Stripping.py: setting for DC06 Stripping production workflow 
+ * <h4>Options for %DC06 settings</h4>
+ * \li Brunel-DC06.py:          default setting for %DC06 simulated data
+ * \li Brunel-DC06Rdst.py:      setting for %DC06 Rdst production workflow
+ * \li Brunel-DC06L0Rdst.py:    setting for %DC06 Rdst production workflow, reconstructing only L0 yes events
+ * \li Brunel-DC06Stripping.py: setting for %DC06 Stripping production workflow 
  * <h4>Examples of input data</h4>
  * \li 2008-TED-Data.py:    example 2008 real data files (TED runs) 
  * \li 2008-Cosmic-Data.py: example 2008 cosmics data files 
