b93e99eea4583c8400c45d39aa699717ec1efd4b
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2009-01-22 14:34:21 +0000

    Change the message level to debug for MonH*F.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@98957 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 53bf1595a14d72e612c89f88a0a3a088c84e32a8

--- Online/Online/Gaucho/src/lib/MonH1F.cpp
+++ Online/Online/Gaucho/src/lib/MonH1F.cpp
@@ -4,5 +4,5 @@ MonH1F::MonH1F(IMessageSvc* msgSvc, const std::string& source, int version):
   MonH1D(msgSvc, source, version)
 {
   MsgStream msg(msgSvc, source + ":MonH1F");
-  msg << MSG::WARNING << "Histograms with floats are not implemented in Gaudi. Using doubles (MonH1D) instead." <<  endreq;
+  msg << MSG::DEBUG << "Histograms with floats are not implemented in Gaudi. Using doubles (MonH1D) instead." <<  endreq;
 }
--- Online/Online/Gaucho/src/lib/MonH2F.cpp
+++ Online/Online/Gaucho/src/lib/MonH2F.cpp
@@ -4,5 +4,5 @@ MonH2F::MonH2F(IMessageSvc* msgSvc, const std::string& source, int version):
   MonH2D(msgSvc, source, version)
 {
   MsgStream msg(msgSvc, source + ":MonH2F");
-  msg << MSG::WARNING << "Histograms with floats are not implemented in Gaudi. Using doubles (MonH2D) instead." <<  endreq;
+  msg << MSG::DEBUG << "Histograms with floats are not implemented in Gaudi. Using doubles (MonH2D) instead." <<  endreq;
 }
