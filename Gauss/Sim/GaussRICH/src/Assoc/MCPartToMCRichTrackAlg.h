4d22d83c6b7579e87c446aa669f8fac3df5c2122
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-01-26 13:45:31 +0000

    Import two new algortihms
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@10472 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cf4a06656c4b8588738880213217bdea720ff7b7

---
+++ Gauss/Sim/GaussRICH/src/Assoc/MCPartToMCRichTrackAlg.h
@@ -0,0 +1,95 @@
+
+//-----------------------------------------------------------------------------
+/** @file MCPartToMCRichTrackAlg.h
+ *
+ *  Header file for algorithm : MCPartToMCRichTrackAlg
+ *
+ *  CVS Log :-
+ *  $Id: MCPartToMCRichTrackAlg.h,v 1.1 2009-01-26 13:45:31 jonrob Exp $
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   2004-02-11
+ */
+//-----------------------------------------------------------------------------
+
+#ifndef RICHMCASSOCIATORS_MCPARTTOMCRICHTRACKALG_H
+#define RICHMCASSOCIATORS_MCPARTTOMCRICHTRACKALG_H 1
+
+// Linker
+#include "Linker/LinkerWithKey.h"
+#include "Linker/LinkerTool.h"
+#include "Linker/LinkedTo.h"
+
+// Event model
+#include "Event/MCRichTrack.h"
+#include "Event/MCParticle.h"
+
+// base class
+#include "RichKernel/RichAlgBase.h"
+
+namespace Rich
+{
+  namespace MC
+  {
+
+    //-----------------------------------------------------------------------------
+    /** @class MCPartToMCRichTrackAlg MCPartToMCRichTrackAlg.h
+     *
+     *  Builds the association tables between MCParticles and MCRichTracks
+     *
+     *  @author Chris Jones    Christopher.Rob.Jones@cern.ch
+     *  @date   2004-02-11
+     */
+    //-----------------------------------------------------------------------------
+
+    class MCPartToMCRichTrackAlg : public RichAlgBase
+    {
+
+    public:
+
+      /// Standard constructor
+      MCPartToMCRichTrackAlg( const std::string& name, ISvcLocator* pSvcLocator );
+
+      /// Destructor
+      virtual ~MCPartToMCRichTrackAlg( );
+
+      virtual StatusCode execute   ();    ///< Algorithm execution
+
+    private: // definitions
+
+      /// typedef for building linker
+      typedef LinkerWithKey<LHCb::MCRichTrack,LHCb::MCParticle> MCPartToRichTracks;
+
+    private: // methods
+
+      /// Add a given event to the linker object
+      StatusCode addEvent( const std::string & evtLoc );
+
+      /// Return a pointer to the linker for this event
+      MCPartToRichTracks * linker();
+
+      /// Reset linker object for new event
+      void resetLinker();
+
+    private: // data
+
+      /// typedef for List of event locations to process
+      typedef std::vector<std::string> EventList;
+
+      /// List of event locations to process
+      EventList m_evtLocs;
+
+      /// Pointer to linker object
+      MCPartToRichTracks * m_linker;
+
+    };
+
+    inline void MCPartToMCRichTrackAlg::resetLinker()
+    {
+      if ( m_linker ) { delete m_linker; m_linker = NULL; }
+    }
+
+  }
+}
+
+#endif // RICHMCASSOCIATORS_MCPARTTOMCRICHTRACKALG_H
---
+++ Gauss/Sim/GaussRICH/src/Assoc/MCRichHitToMCRichOpPhotAlg.cpp
@@ -0,0 +1,158 @@
+
+//-----------------------------------------------------------------------------
+/** @file MCRichHitToMCRichOpPhotAlg.cpp
+ *
+ * Implementation file for class : MCRichHitToMCRichOpPhotAlg
+ *
+ * CVS Log :-
+ * $Id: MCRichHitToMCRichOpPhotAlg.cpp,v 1.1 2009-01-26 13:45:31 jonrob Exp $
+ *
+ * @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ * @date 2004-02-11
+ */
+//-----------------------------------------------------------------------------
+
+// local
+#include "MCRichHitToMCRichOpPhotAlg.h"
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// namespace
+using namespace Rich::MC;
+
+DECLARE_ALGORITHM_FACTORY( MCRichHitToMCRichOpPhotAlg );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+MCRichHitToMCRichOpPhotAlg::MCRichHitToMCRichOpPhotAlg( const std::string& name,
+                                                        ISvcLocator* pSvcLocator)
+  : RichAlgBase ( name , pSvcLocator ),
+    m_linker    ( NULL )
+{
+
+  // Event locations to process
+  m_evtLocs.clear();
+  m_evtLocs.push_back(                    LHCb::MCRichOpticalPhotonLocation::Default );
+  m_evtLocs.push_back( "Prev/"          + LHCb::MCRichOpticalPhotonLocation::Default );
+  m_evtLocs.push_back( "PrevPrev/"      + LHCb::MCRichOpticalPhotonLocation::Default );
+  m_evtLocs.push_back( "Next/"          + LHCb::MCRichOpticalPhotonLocation::Default );
+  m_evtLocs.push_back( "NextNext/"      + LHCb::MCRichOpticalPhotonLocation::Default );
+  m_evtLocs.push_back( "LHCBackground/" + LHCb::MCRichOpticalPhotonLocation::Default );
+  declareProperty( "EventLocations", m_evtLocs );
+
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+MCRichHitToMCRichOpPhotAlg::~MCRichHitToMCRichOpPhotAlg() {};
+
+//=============================================================================
+// Initialisation
+//=============================================================================
+StatusCode MCRichHitToMCRichOpPhotAlg::initialize()
+{
+  // Sets up various tools and services
+  const StatusCode sc = RichAlgBase::initialize();
+  if ( sc.isFailure() ) return sc;
+
+  // add custom initialisations here
+
+  return sc;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode MCRichHitToMCRichOpPhotAlg::execute()
+{
+  debug() << "Execute" << endreq;
+
+  StatusCode sc = StatusCode::SUCCESS;
+
+  // Loop over all MCRichOpticalPhotons in each spillover event
+  for ( EventList::const_iterator iEvt = m_evtLocs.begin();
+        iEvt != m_evtLocs.end(); ++iEvt )
+  {
+    sc = addEvent(*iEvt);
+    if ( sc.isFailure() ) break;
+  }
+
+  // force a new linker for next event
+  resetLinker();
+
+  // return final status code
+  return sc;
+}
+
+//=============================================================================
+// Create linker object
+//=============================================================================
+MCRichHitToMCRichOpPhotAlg::MCRichHitsToPhotons *
+MCRichHitToMCRichOpPhotAlg::linker()
+{
+  if ( !m_linker )
+  {
+    // New linker object
+    m_linker =
+      new MCRichHitsToPhotons( evtSvc(), msgSvc(),
+                               LHCb::MCRichHitLocation::Default+"2MCRichOpticalPhotons" );
+    // set the ordering
+    m_linker->setDecreasingWeight();
+  }
+  return m_linker;
+}
+
+//=============================================================================
+// Process a single event location
+//=============================================================================
+StatusCode MCRichHitToMCRichOpPhotAlg::addEvent( const std::string & evtLoc )
+{
+
+  // load MCRichTracks in this event
+  SmartDataPtr<LHCb::MCRichOpticalPhotons> mcPhotons( eventSvc(), evtLoc );
+  if ( !mcPhotons )
+  {
+    if ( msgLevel(MSG::DEBUG) )
+    { debug() << "Cannot locate MCRichOpticalPhotons at " << evtLoc << endreq; }
+    return StatusCode::SUCCESS;
+  }
+  if ( msgLevel(MSG::DEBUG) )
+  { debug() << "Successfully located " << mcPhotons->size()
+            << " MCRichOpticalPhotons at " << evtLoc << endreq; }
+
+  // add links to linker
+  for ( LHCb::MCRichOpticalPhotons::const_iterator iPhot = mcPhotons->begin();
+        iPhot != mcPhotons->end(); ++iPhot )
+  {
+    const LHCb::MCRichOpticalPhoton * mcPhot = *iPhot;
+    if ( mcPhot )
+    {
+      const LHCb::MCRichHit * mchit = mcPhot->mcRichHit();
+      if ( mchit )
+      {
+        if ( msgLevel(MSG::VERBOSE) )
+        { verbose() << "Linking MCRichHit " << mchit->sensDetID()
+                    << " to MCRichOpticalPhoton " << mcPhot->key() << endreq; }
+        linker()->link( mchit, *iPhot );
+      }
+      else
+      {
+        Warning( "MCRichOpticalPhoton has NULL MCRichHit reference" );
+      }
+    }
+    else
+    {
+      Warning( "NULL MCRichOpticalPhoton in container" );
+    }
+  }
+
+  if ( msgLevel(MSG::DEBUG) )
+  { debug() << "Finished processing MCRichOpticalPhotons at " << evtLoc << endreq; }
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
