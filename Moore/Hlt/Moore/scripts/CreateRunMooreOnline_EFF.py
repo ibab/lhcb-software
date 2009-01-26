edece424550c890a3350d9aac9991a00d89f19a6
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-01-26 10:11:16 +0000

    add support for RUNTYPE=INJECTOR in (create)runMooreOnline_EFF
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@75009 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: be801e6f3ab762ca2b2db3a04b5791ad2590d2b1

--- Moore/Hlt/Moore/scripts/CreateRunMooreOnline_EFF.py
+++ Moore/Hlt/Moore/scripts/CreateRunMooreOnline_EFF.py
@@ -29,6 +29,7 @@ source ./setupOnline.sh $*
 source %(setup)s
 # and the options to be used
 export USEROPTS=%(moore)s/options/DEFAULT${IS_TAE_RUN}.opts
+[[ 'x'$RUNTYPE == 'xINJECTOR' ]] && USEROPTS=%(moore)s/options/DEFAULT_INJECTOR.opts
 export CORAL_SQLITE_TEMP_STORE_MEMORY=yes
 # and run them!
 echo exec -a ${UTGID} ${CLASS1_TASK} -opt=${USEROPTS}
