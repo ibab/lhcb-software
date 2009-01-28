09691a3e10dd8bb52b69f7dc963426b953cfc8df
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-28 12:42:58 +0000

    Add FPE test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30452 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 902ee99de1eb23257c87b528dd369a973ec28811

--- Brunel/Rec/Brunel/tests/options/testBrunel-defaults.py
+++ Brunel/Rec/Brunel/tests/options/testBrunel-defaults.py
@@ -2,4 +2,8 @@
 from Brunel.Configuration import *
 
 Brunel().EvtMax   = 10
-Brunel().Monitors = ["SC"]           # Add StatusCode checks
+Brunel().Monitors = ["SC","FPE"]           # Add StatusCode checks
+
+# Disable known FPEs...
+from Configurables import FPEAuditor
+FPEAuditor().DisableTrapFor += ['MergedPi0Rec']
