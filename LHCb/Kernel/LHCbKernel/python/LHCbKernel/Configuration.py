2aaa265ceb8ee7c5ce69cb80a26fd65fb459e7e1
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-28 16:10:59 +0000

    fixes to propagate properties
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48184 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ee64897cb3bed323b88055744fdf66bd037352a6

--- LHCb/Kernel/LHCbKernel/python/LHCbKernel/Configuration.py
+++ LHCb/Kernel/LHCbKernel/python/LHCbKernel/Configuration.py
@@ -4,7 +4,7 @@
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.8 2008-12-01 10:56:45 cattanem Exp $"
+__version__ = "$Id: Configuration.py,v 1.9 2009-01-28 16:10:59 cattanem Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from Gaudi.Configuration import *
@@ -36,21 +36,11 @@ class LHCbConfigurableUser(ConfigurableUser):
     #  @param other The other configurable to set the property for
     #  @param name  The property name
     def setOtherProp(self,other,name):
-        # If self property is set, use it
-        if hasattr(self,name) and self.getProp(name) != self.getDefaultProperties()[name]:
-            if hasattr(other,name) and other.getProp(name) != other.getDefaultProperties()[name]:
-                log.warning("Both %s().%s and %s().%s are defined, using %s().%s"%
-                            (other.name(),name, self.name(), name, self.name(), name)  )
-            other.setProp(name,self.getProp(name))
-        # If not, and other property also not set, propagate the default
-        elif not hasattr(other,name) or other.getProp(name) != other.getDefaultProperties()[name]:
-            if isinstance(other,ConfigurableUser):
-                other._properties[name].setDefault(self.getProp(name))
-            else:
-                other.setProp(name,self.getProp(name))
+        self.propagateProperty(name,other)
+
 
     ## @brief Set the given properties in another configurable object
     #  @param other The other configurable to set the property for
     #  @param names The property names
     def setOtherProps(self,other,names):
-        for name in names : self.setOtherProp(other,name)
+        self.propagateProperties(names,other)
