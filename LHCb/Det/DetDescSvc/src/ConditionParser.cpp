ee5d4b0327bd4ff5a47a31edc656a677f3d00c22
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-01-29 13:32:34 +0000

    fix the fix...
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5272 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ed20fa08362a1b4b76da5c3ad78c2bad27f86c1e

--- LHCb/Det/DetDescSvc/src/ConditionParser.cpp
+++ LHCb/Det/DetDescSvc/src/ConditionParser.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/version.hpp>
 #if BOOST_VERSION >= 103800
+#define BOOST_SPIRIT_USE_OLD_NAMESPACE
 #include <boost/spirit/include/classic_core.hpp>
 #else
 #include <boost/spirit/core.hpp>
