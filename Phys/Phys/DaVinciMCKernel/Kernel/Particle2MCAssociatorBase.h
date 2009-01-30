d7bc612094af34f52e3c2d15d38378c5216e7c10
jpalac <jpalac@cern.ch>
2009-01-30 18:33:57 +0000

    . Base class with common implementation for IParticle2MCAssociator interface.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64568 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 35bd7f3131ff5fbabf09fe9bb4b1ca2c8aef43b3

---
+++ Phys/Phys/DaVinciMCKernel/Kernel/Particle2MCAssociatorBase.h
@@ -0,0 +1,133 @@
+// $Id: Particle2MCAssociatorBase.h,v 1.1 2009-01-30 18:33:57 jpalac Exp $
+#ifndef PARTICLE2MCASSOCIATORBASE_H 
+#define PARTICLE2MCASSOCIATORBASE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTool.h"
+// LHCb
+#include "Event/MCParticle.h"
+#include "Kernel/IParticle2MCAssociator.h"            // Interface
+
+
+/** @class Particle2MCAssociatorBase Particle2MCAssociatorBase.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2009-01-30
+ */
+class Particle2MCAssociatorBase : public GaudiTool, 
+                                  virtual public IParticle2MCAssociator {
+public: 
+  /// Standard constructor
+  Particle2MCAssociatorBase( const std::string& type, 
+                             const std::string& name,
+                             const IInterface* parent);
+
+  virtual StatusCode initialize() ;
+
+  virtual StatusCode finalize() ;
+  
+  virtual ~Particle2MCAssociatorBase( );
+
+  virtual Particle2MCParticle::ToVector 
+  associate(const LHCb::Particle* particle,
+            const std::string& mcParticleLocation) const;
+
+  virtual Particle2MCParticle::LightTable 
+  relatedMCPs(const LHCb::Particle* particle,
+              const std::string& mcParticleLocation) const;
+
+  virtual Particle2MCParticle::LightTable 
+  relatedMCPs(const LHCb::Particle* particle,
+              const LHCb::MCParticle::ConstVector& mcParticles) const;
+  
+  virtual Particle2MCParticle::LightTable 
+  associationTable(const LHCb::Particle::ConstVector& particles,
+                   const std::string& mcParticleLocation) const;
+
+  
+  virtual Particle2MCParticle::LightTable 
+  associationTable(const LHCb::Particle::Container& particles,
+                   const std::string& mcParticleLocation) const;
+  
+
+  virtual Particle2MCParticle::LightTable 
+  associationTable(const LHCb::Particle::ConstVector& particles,
+                   const LHCb::MCParticle::ConstVector& mcParticles) const;
+
+  virtual Particle2MCParticle::LightTable 
+  associationTable(const LHCb::Particle::Container& particles,
+                   const LHCb::MCParticle::ConstVector& mcParticles) const;
+  
+  virtual double 
+  weight(const LHCb::Particle* particle, 
+         const LHCb::MCParticle* mcParticle) const;
+  
+private:
+
+  inline LHCb::MCParticle::Container* 
+  i_MCParticles(const std::string& location) const
+  {
+    return (exist<LHCb::MCParticle::Container>( location )) ? 
+      get<LHCb::MCParticle::Container>( location ) : 0 ;
+  }
+  
+
+  template <typename Iter> 
+  Particle2MCParticle::LightTable 
+  i_relatedMCPs(const LHCb::Particle* particle,
+                Iter begin,
+                Iter end     ) const
+  {
+    Particle2MCParticle::LightTable table;
+    if (0!=particle) {
+      for ( Iter iMCP = begin ; iMCP != end ; ++iMCP){
+        const double wt = weight(particle, *iMCP);
+        if (wt > 0. ) table.i_push(particle,*iMCP, wt );
+      }
+      table.i_sort();
+    } else {
+      Warning("No particle!").ignore();
+    }
+    return table;
+  }
+
+  template <typename pIter, typename mcPIter>
+  Particle2MCParticle::LightTable 
+  i_associationTable(const pIter pBegin, 
+                     const pIter pEnd, 
+                     const mcPIter mcBegin, 
+                     const mcPIter mcEnd) const
+  {
+
+    Particle2MCParticle::LightTable table;
+
+    for (pIter part = pBegin; part != pEnd; ++part) {
+      for ( mcPIter iMCP = mcBegin ; iMCP != mcEnd ; ++iMCP){
+        const double wt = weight(*part, *iMCP);
+        if (wt > 0. ) {
+          table.i_push( *part, *iMCP,  wt );
+        }
+      }
+    }
+    table.i_sort();
+    return table;
+  }
+  
+  template <typename Iter> 
+  inline Particle2MCParticle::LightTable 
+  i_associationTable(const Iter pBegin,
+                     const Iter pEnd,
+                     const std::string& mcParticleLocation) const
+  {
+    LHCb::MCParticle::Container* mcps = i_MCParticles(mcParticleLocation);
+    if (0!=mcps) {
+      return i_associationTable(pBegin, pEnd, mcps->begin(), mcps->end());
+    } else {
+      return Particle2MCParticle::LightTable();
+    }
+  }
+
+};
+#endif // PARTICLE2MCASSOCIATORBASE_H
---
+++ Phys/Phys/DaVinciMCKernel/src/Lib/Particle2MCAssociatorBase.cpp
@@ -0,0 +1,117 @@
+// $Id: Particle2MCAssociatorBase.cpp,v 1.1 2009-01-30 18:33:57 jpalac Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h" 
+
+// local
+#include "Kernel/Particle2MCAssociatorBase.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : Particle2MCAssociatorBase
+//
+// 2009-01-30 : Juan PALACIOS
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+Particle2MCAssociatorBase::Particle2MCAssociatorBase( const std::string& type,
+                                                      const std::string& name,
+                                                      const IInterface* parent )
+  : GaudiTool ( type, name , parent )
+{
+}
+//=============================================================================
+Particle2MCParticle::ToVector 
+Particle2MCAssociatorBase::associate(const LHCb::Particle* particle,
+                                     const std::string& mcParticleLocation) const 
+{
+  Particle2MCParticle::ToVector assocVector;
+  Particle2MCParticle::LightTable table = this->relatedMCPs(particle,
+                                                            mcParticleLocation);
+  
+
+  Particle2MCParticle::LightTable::Range r = table.i_relations(particle);
+  for (Particle2MCParticle::LightTable::Range::const_iterator i = r.begin();
+       i!=r.end(); ++i) {
+    MCAssociation myMCAssoc(i->to(), i->weight());
+    assocVector.push_back(myMCAssoc);
+  }
+  return assocVector;
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::relatedMCPs(const LHCb::Particle* particle,
+                                       const std::string& mcParticleLocation) const
+{
+  LHCb::MCParticle::Container* mcps = i_MCParticles(mcParticleLocation);
+  if (0!=mcps) {
+    return i_relatedMCPs(particle,  mcps->begin(), mcps->end());
+  } else {
+    return Particle2MCParticle::LightTable();
+  }   
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::relatedMCPs(const LHCb::Particle* particle,
+                                       const LHCb::MCParticle::ConstVector& mcParticles) const
+{
+  return i_relatedMCPs(particle,  mcParticles.begin(), mcParticles.end());
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::associationTable(const LHCb::Particle::ConstVector& particles,
+                                          const std::string& mcParticleLocation) const
+{
+  return i_associationTable(particles.begin(), particles.end(), mcParticleLocation);
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::associationTable(const LHCb::Particle::Container& particles,
+                                            const std::string& mcParticleLocation) const
+{
+  return i_associationTable(particles.begin(), particles.end(), mcParticleLocation);  
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::associationTable(const LHCb::Particle::ConstVector& particles,
+                                          const LHCb::MCParticle::ConstVector& mcParticles) const
+{
+  return i_associationTable(particles.begin(),
+                            particles.end(),
+                            mcParticles.begin(),
+                            mcParticles.end());
+  
+}
+//=============================================================================
+Particle2MCParticle::LightTable 
+Particle2MCAssociatorBase::associationTable(const LHCb::Particle::Container& particles,
+                                          const LHCb::MCParticle::ConstVector& mcParticles) const
+{
+  return i_associationTable(particles.begin(), particles.end(), mcParticles.begin(), mcParticles.end());          
+}
+//=============================================================================
+double
+Particle2MCAssociatorBase::weight(const LHCb::Particle* /*particle */, 
+                                  const LHCb::MCParticle* /* mcParticle */) const
+{
+  return 0.;
+}
+//=============================================================================
+// initialize
+//=============================================================================
+StatusCode Particle2MCAssociatorBase::initialize() {
+  return GaudiTool::initialize();
+}
+//=============================================================================
+// finalize
+//=============================================================================
+StatusCode Particle2MCAssociatorBase::finalize() {
+  return GaudiTool::finalize() ; 
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+Particle2MCAssociatorBase::~Particle2MCAssociatorBase() {}
+//=============================================================================
