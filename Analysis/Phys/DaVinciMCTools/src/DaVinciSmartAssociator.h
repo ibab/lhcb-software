374323bdc40af7d3c332ebb74ff228a79b175223
jpalac <jpalac@cern.ch>
2009-02-02 15:07:54 +0000

    . Add doxygen
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75049 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6ea6a44ac433c796584efb8c5ed749d3ceaf4e3a

--- Analysis/Phys/DaVinciMCTools/src/DaVinciSmartAssociator.h
+++ Analysis/Phys/DaVinciMCTools/src/DaVinciSmartAssociator.h
@@ -13,7 +13,7 @@
 /** @class DaVinciSmartAssociator DaVinciSmartAssociator.h
  *  
  *  A ``smart'' associator for any kind of particle. Returns a vector of
- *  MCAssociation classes, each of which represents a single associated MCParticle
+ *  MCAssociation objects, each of which represents a single associated MCParticle
  *  and its association weight. The association methods used are
  *
  *  Charged stables: Protoparticle associator
@@ -23,6 +23,15 @@
  *  For composites, there is always at most one associated MCParticle, whose
  *  association weight is set to 1 by definition
  *  
+ *  Implements IParticle2MCAssociator, but does not satisfy its rules, since the
+ *  retrieval of associations of stable particles interacts with the linker
+ *  mechanism on the TES, which can populate the TES with linker objects
+ *  when these don't already exist, and thus has direct side-effects. It also has the 
+ *  added constraint that the LHCb::MCParticles to be associated
+ *  HAVE to be on the TES always, and the LHCb::Particles have to be on the TES if they are
+ *  stable. However, the results returned to the user are self-contained and independent
+ *  of the TES.
+ * 
  *  @author V. Gligorov
  *  @date   2009-01-13
  */
