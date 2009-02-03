de11f5d98315d5e06e96b30f90b973a55b6c0946
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-02-03 13:55:38 +0000

    use FEST and LHCb partitions in CreateRunMooreOnline_EFF.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@75059 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 789dd2f539184a78514e4731a7e754ce566c8019

--- Moore/Hlt/Moore/scripts/CreateRunMooreOnline_EFF.py
+++ Moore/Hlt/Moore/scripts/CreateRunMooreOnline_EFF.py
@@ -29,7 +29,8 @@ source ./setupOnline.sh $*
 source %(setup)s
 # and the options to be used
 export USEROPTS=%(moore)s/options/DEFAULT${IS_TAE_RUN}.opts
-[[ 'x'$RUNTYPE == 'xINJECTOR' ]] && USEROPTS=%(moore)s/options/DEFAULT_INJECTOR.opts
+[[ 'x'$PARTNAME == 'xFEST' ]] && USEROPTS=%(moore)s/options/DEFAULT_FEST.opts
+[[ 'x'$PARTNAME == 'xLHCb' ]] && USEROPTS=%(moore)s/options/DEFAULT_LHCb.opts
 export CORAL_SQLITE_TEMP_STORE_MEMORY=yes
 # and run them!
 echo exec -a ${UTGID} ${CLASS1_TASK} -opt=${USEROPTS}
