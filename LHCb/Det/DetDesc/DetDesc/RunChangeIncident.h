b1c39e98f160afe9391b1b0a83cf94d5b9a35d7c
Marco Clemencic <marcocle@cern.ch>
2009-02-03 18:31:03 +0000

    Reorganized the encoding/decoding of ODIN. Main logic move away from DAQEvent.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5279 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2cd74916cbf950edf555375f36551f57625cff7

---
+++ LHCb/Det/DetDesc/DetDesc/RunChangeIncident.h
@@ -0,0 +1,9 @@
+#ifndef RUNCHANGEINCIDENT_H_
+#define RUNCHANGEINCIDENT_H_
+
+namespace IncidentType 
+{
+  const std::string RunChange = "RunChange"; ///< Change of run number
+}
+
+#endif /*RUNCHANGEINCIDENT_H_*/
