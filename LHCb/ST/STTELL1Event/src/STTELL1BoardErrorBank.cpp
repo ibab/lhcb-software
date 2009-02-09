0d18e85c5fad2d7d079cf6731ba9463dd8cb0863
Matthew David Needham <mneedham@cern.ch>
2009-02-09 10:40:44 +0000

    printout flat tell1 number in error bank streamer
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@27509 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b0fd32e8a905f922481031bb86395dc62bbc9d99

--- LHCb/ST/STTELL1Event/src/STTELL1BoardErrorBank.cpp
+++ LHCb/ST/STTELL1Event/src/STTELL1BoardErrorBank.cpp
@@ -1,6 +1,11 @@
 
 #include "Event/STTELL1BoardErrorBank.h"
 #include <iostream>
+#include <string>
+
+#include "GaudiKernel/IRegistry.h"
+
+#include "Kernel/STBoardMapping.h"
 
 //-----------------------------------------------------------------------------
 // Implementation file for class : STChannelID
@@ -18,10 +23,36 @@ double LHCb::STTELL1BoardErrorBank::fractionOK(const unsigned int pcn) const{
   return frac/m_errorInfo.size();
 }
 
+std::string LHCb::STTELL1BoardErrorBank::regName() const{
+
+  // get the name of the container in the TES
+  const ObjectContainerBase* parentCont = parent();
+  const IRegistry* reg =  parentCont->registry();
+  return reg->identifier();
+}
+
+bool LHCb::STTELL1BoardErrorBank::isIT() const {
+  const std::string name = regName();
+  return (name.find("IT") != std::string::npos ? true : false);
+}
+
+
+bool LHCb::STTELL1BoardErrorBank::isTT() const {
+  const std::string name = regName();
+  return (name.find("TT") != std::string::npos ? true : false);
+}
+
 std::ostream& LHCb::STTELL1BoardErrorBank::fillStream(std::ostream& s) const
 {
   unsigned int pp = 0;
   s << "Error bank for TELL1 board source "  << key() << std::endl;
+  if (isIT() == true) {
+    s << STBoardMapping::find(key(), STBoardMapping::ITSourceIDToNumberMap()) << std::endl;
+  } 
+  else if (isTT() == true){
+    s << STBoardMapping::find(key(), STBoardMapping::TTSourceIDToNumberMap()) << std::endl;
+  }
+
   LHCb::STTELL1BoardErrorBank::ErrorVector::const_iterator iter = m_errorInfo.begin();
   for(; iter != m_errorInfo.end(); ++iter, ++pp){
     s << "pp "  << pp<< std::endl;
