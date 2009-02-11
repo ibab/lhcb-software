92a0c301386616629e60baf93b3b75e8430e781a
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-02-11 08:18:06 +0000

    add missing dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37224 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 587f7bf4e0a20adb26641a71b1a4860f3ef20da3

---
+++ LHCb/Event/GenEvent/xml/lcgdict/lcg_selection.h
@@ -0,0 +1,19 @@
+// $Id: lcg_selection.h,v 1.1 2009-02-11 08:18:06 cattanem Exp $
+#ifndef GENEVENT_LCGDICT_H 
+#define GENEVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include "HepMC/GenVertex.h"
+
+// end include files
+
+namespace {
+  struct _Instantiations {
+    // begin instantiations
+    // end instantiations
+  };
+}
+
+#endif // GENEVENT_LCGDICT_H
