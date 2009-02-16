3b3ddef7b7f356031c7e6f5405f0d11f1e4c298f
Jaap Panman <panmanj@cern.ch>
2009-02-16 16:28:12 +0000

    first import of LumiAlgs under Rec
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@30479 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 82efeaf8521db63c6027fc53f25856a95f34fbc1

---
+++ Rec/Rec/LumiAlgs/python/LumiAlgs/Configuration.py
@@ -0,0 +1,77 @@
+
+## @package LumiAlgs
+#  High level Configuration tools for Lumi Accounting Algorithms
+#  @author Jaap Panman
+#  @date   13/02/2009
+
+__version__ = ""
+__author__  = ""
+
+from Gaudi.Configuration  import *
+from Configurables import ( GaudiSequencer, MessageSvc )
+import GaudiKernel.ProcessJobOptions
+from Configurables import ( LHCbConfigurableUser, LHCbApp, 
+                            ProcessPhase, GaudiSequencer,
+                            OdinTypesFilter, LumiAccounting, HltLumiSummaryDecoder )
+    
+# -------------------------------------------------------------------------------------------
+
+## @class LumiAlgsConf
+#  High level Configuration tools for Lumi Accounting Algorithms
+#  @author Jaap Panman
+#  @date   13/02/2009
+
+class LumiAlgsConf(LHCbConfigurableUser):
+
+    ## Possible used Configurables
+    __used_configurables__ = [ OdinTypesFilter, LumiAccounting, HltLumiSummaryDecoder ]
+
+    ## Steering options
+    __slots__ = {
+        "Context"       : "Offline"  # The context within which to run
+       ,"DataType"      : "2008" # Data type, can be ['DC06','2008']
+       ,"LumiSequencer" : None # The sequencer to add the Lumi Accounting to
+       ,"BXTypes"       : [ 'NoBeam', 'BeamCrossing','SingleBeamRight','SingleBeamLeft'] # bunch crossing types 
+       ,"HistoProduce"  : False
+        }
+
+    ## Apply the configuration to the given sequence
+    def applyConf(self):
+
+        debugOPL = INFO
+         
+        sequence = self.getProp("LumiSequencer")
+        if sequence == None : raise RuntimeError("ERROR : Lumi Sequencer not set")
+
+        # Set context
+        context = self.getProp("Context")
+
+        # Create sub-sequences according to BXTypes
+
+        crossings = self.getProp("BXTypes")
+        BXMembers = []
+        for i in crossings:
+            seqMembers=[]
+            seqMembers.append( OdinTypesFilter('Filter'+i,
+                                               TriggerTypes=['RandomTrigger'],
+                                               BXTypes=[i],
+                                               OutputLevel = debugOPL ))
+            seqMembers.append( HltLumiSummaryDecoder('LumiDecode'+i,
+                                                  OutputLevel = debugOPL ))
+            seqMembers.append( LumiAccounting('LumiCount'+i,
+                                              OutputDataContainer = "/RunRecords/EOR/LumiFSR"+i,
+                                              OutputLevel = debugOPL ))
+            
+            BXMembers.append( GaudiSequencer('Lumi'+i+'Seq', 
+                                             Members = seqMembers,
+                                             ModeOR = False,
+                                             ShortCircuit = True,
+                                             MeasureTime = True,
+                                             ))
+
+        sequence.Members = BXMembers
+        sequence.MeasureTime = True
+        sequence.ModeOR = True
+        sequence.ShortCircuit = False
+        sequence.IgnoreFilterPassed = True
+
