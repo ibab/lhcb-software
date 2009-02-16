f81e2a223e23b9009b601412e8b6522ae8450187
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-02-16 16:43:44 +0000

    ! 2009-02-16 - Chris Jones  - Add a new algorithm that dumps RICH events to a simple text file.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10542 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c8d77334a94d5ce9759ef11e383d6369b8ca5d90

---
+++ Rec/Rich/RichRecMCAlgorithms/src/DumpRichHitsToTextFileAlg.cpp
@@ -0,0 +1,113 @@
+
+/** @file DumpRichHitsToTextFileAlg.cpp
+ *
+ *  Implementation file for algorithm class : Rich::Rec::MC::DumpRichHitsToTextFileAlg
+ *
+ *  CVS Log :-
+ *  $Id: DumpRichHitsToTextFileAlg.cpp,v 1.1 2009-02-16 16:43:44 jonrob Exp $
+ *
+ *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
+ *  @date   2009-02-16
+ */
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// local
+#include "DumpRichHitsToTextFileAlg.h"
+
+//-----------------------------------------------------------------------------
+
+// namespace
+using namespace Rich::Rec::MC;
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( DumpRichHitsToTextFileAlg );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+DumpRichHitsToTextFileAlg::DumpRichHitsToTextFileAlg( const std::string& name,
+                                                      ISvcLocator* pSvcLocator)
+  : Rich::Rec::AlgBase ( name , pSvcLocator ),
+    m_truth            ( NULL ),
+    m_nEvt             ( 0    ) { }
+
+//=============================================================================
+// Destructor
+//=============================================================================
+DumpRichHitsToTextFileAlg::~DumpRichHitsToTextFileAlg() {}
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode DumpRichHitsToTextFileAlg::initialize()
+{
+  const StatusCode sc = Rich::Rec::AlgBase::initialize();
+  if ( sc.isFailure() ) return sc;
+
+  // acquire tools
+  acquireTool( "RichMCTruthTool", m_truth );
+
+  return sc;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode DumpRichHitsToTextFileAlg::execute()
+{
+  ++m_nEvt;
+  return ( dumpToTextfile(Rich::Rich1,Rich::top)    &&
+           dumpToTextfile(Rich::Rich1,Rich::bottom) &&
+           dumpToTextfile(Rich::Rich2,Rich::left)   &&
+           dumpToTextfile(Rich::Rich2,Rich::right)  );
+}
+
+StatusCode
+DumpRichHitsToTextFileAlg::dumpToTextfile( const Rich::DetectorType rich,
+                                           const Rich::Side         panel ) const
+{
+  // file name
+  std::ostringstream filename;
+  filename << Rich::text(rich) << "-" << Rich::text(rich,panel) << "_" << m_nEvt << ".txt";
+
+  // open file
+  info() << "Creating data text file : " << filename.str() << endreq;
+  std::ofstream file(filename.str().c_str(),std::ios::app);
+
+  // Iterate over pixels
+  const IPixelCreator::PixelRange range = pixelCreator()->range( rich, panel );
+  for ( LHCb::RichRecPixels::const_iterator iPix = range.begin(); iPix != range.end(); ++iPix )
+  {
+    // get X and Y
+    //const double X ( (*iPix)->radCorrLocalPositions().position(rad()).x() );
+    //const double Y ( (*iPix)->radCorrLocalPositions().position(rad()).y() );
+    const double X ( (*iPix)->localPosition().x() );
+    const double Y ( (*iPix)->localPosition().y() );
+    file << X << " " << Y << " ";
+
+    // MC history
+    typedef std::vector< const LHCb::MCRichDigitSummary * > Histories;
+    Histories histories;
+    m_truth->getMcHistories( (*iPix)->hpdPixelCluster(), histories );
+    unsigned int i(0);
+    for ( Histories::const_iterator iH = histories.begin();
+          iH != histories.end(); ++iH )
+    {
+      if ( NULL != (*iH)->mcParticle() )
+      {
+        if ( i>0 ) file << ",";
+        file << (*iH)->mcParticle()->key();
+        ++i;
+      } 
+    }
+
+    // end this line
+    file << std::endl;
+  }
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
---
+++ Rec/Rich/RichRecMCAlgorithms/src/DumpRichHitsToTextFileAlg.h
@@ -0,0 +1,75 @@
+
+/** @file DumpRichHitsToTextFileAlg.h
+ *
+ *  Header file for algorithm class : Rich::Rec::MC::DumpRichHitsToTextFileAlg
+ *
+ *  CVS Log :-
+ *  $Id: DumpRichHitsToTextFileAlg.h,v 1.1 2009-02-16 16:43:44 jonrob Exp $
+ *
+ *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
+ *  @date   2009-02-16
+ */
+
+#ifndef RICHRECMCALGORITHMS_DUMPRICHHITSTOTEXTFILEALG_H
+#define RICHRECMCALGORITHMS_DUMPRICHHITSTOTEXTFILEALG_H 1
+
+// STL
+#include <sstream>
+#include <fstream>
+
+// Base class
+#include "RichRecBase/RichRecAlgBase.h"
+
+// Interfaces
+#include "MCInterfaces/IRichMCTruthTool.h"
+
+// Event
+#include <Event/MCRichDigitSummary.h>
+
+namespace Rich
+{
+  namespace Rec
+  {
+    namespace MC
+    {
+
+      /** @class DumpRichHitsToTextFileAlg DumpRichHitsToTextFileAlg.h
+       *
+       *  Simple algorithm to dump Rich hits + minimal MC information to text files.
+       *
+       *  @author Chris Jones
+       *  @date   2009-02-16
+       */
+      class DumpRichHitsToTextFileAlg : public Rich::Rec::AlgBase
+      {
+
+      public:
+
+        /// Standard constructor
+        DumpRichHitsToTextFileAlg( const std::string& name, ISvcLocator* pSvcLocator );
+
+        virtual ~DumpRichHitsToTextFileAlg( ); ///< Destructor
+
+        virtual StatusCode initialize();    ///< Algorithm initialization
+        virtual StatusCode execute   ();    ///< Algorithm execution
+ 
+      private:
+
+        // dump the given RICH panel to text file
+        StatusCode dumpToTextfile( const Rich::DetectorType rich,
+                                   const Rich::Side         panel ) const;
+
+      private:
+
+        const Rich::MC::IMCTruthTool * m_truth;       ///< MC truth tool
+
+        /// Event number
+        unsigned int m_nEvt;
+
+      };
+
+    }
+  }
+}
+
+#endif // RICHRECMCALGORITHMS_DUMPRICHHITSTOTEXTFILEALG_H
