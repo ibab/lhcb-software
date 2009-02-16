553db8cfb10a5b15fc49f3b3a8b5a02f49fe2062
janos <janos@cern.ch>
2009-02-16 14:42:35 +0000

    o More moni
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@60419 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 44033b6a236dbd0504d779525c62d33ecc81d0c0

--- Lbcom/OT/OTMonitor/src/MCOTDepositMonitor.cpp
+++ Lbcom/OT/OTMonitor/src/MCOTDepositMonitor.cpp
@@ -1,4 +1,4 @@
-// $Id: MCOTDepositMonitor.cpp,v 1.13 2009-01-27 10:19:27 janos Exp $
+// $Id: MCOTDepositMonitor.cpp,v 1.14 2009-02-16 14:42:35 janos Exp $
 
 // from STD
 #include <algorithm>
@@ -6,6 +6,7 @@
 // Gaudi
 #include "GaudiKernel/AlgFactory.h"
 #include "GaudiKernel/SystemOfUnits.h"
+#include "GaudiKernel/IRegistry.h"
 
 // AIDA
 #include "AIDA/IHistogram1D.h"
@@ -43,12 +44,29 @@ using namespace boost;
 using namespace boost::lambda;
 using boost::lexical_cast;
 
+
 namespace MonitorHelpers {
- bool isSignal(      const LHCb::MCOTDeposit* aDeposit ) { return aDeposit->type() == LHCb::MCOTDeposit::Signal;      };   
- bool isNoise(       const LHCb::MCOTDeposit* aDeposit ) { return aDeposit->type() == LHCb::MCOTDeposit::Noise;       };
- bool isXTalk(       const LHCb::MCOTDeposit* aDeposit ) { return aDeposit->type() == LHCb::MCOTDeposit::XTalk;       };  
- bool isDoublePulse( const LHCb::MCOTDeposit* aDeposit ) { return aDeposit->type() == LHCb::MCOTDeposit::DoublePulse; };
+  const std::string& findSpill( const LHCb::MCHit* aHit ) {
+    const ObjectContainerBase* parentCont = aHit->parent();
+    const IRegistry*                  reg =  parentCont->registry();
+    return ( reg->identifier() );
+  };
+
+  bool isSpill( const std::string& spill, const LHCb::MCHit* aHit ) {
+    return ( findSpill( aHit ) == "/Event" + spill + LHCb::MCHitLocation::OT );
+  };
+  
+  bool isSpillOver( const LHCb::MCOTDeposit* aDeposit ) {
+    const LHCb::MCHit* aHit = aDeposit->mcHit();
+    return aHit ? ( !isSpill( "/", aHit ) && aDeposit->isSignal() ): false ;
+  };
+  
+  bool isCurrent( const LHCb::MCOTDeposit* aDeposit ) {
+    const LHCb::MCHit* aHit = aDeposit->mcHit();
+    return aHit ? ( isSpill( "/", aHit ) && aDeposit->isSignal() ) : false ;
+  }
 };
+
 
 /// Declaration of algorithm factory
 DECLARE_ALGORITHM_FACTORY( MCOTDepositMonitor );
@@ -67,21 +85,18 @@ MCOTDepositMonitor::~MCOTDepositMonitor() {
 StatusCode MCOTDepositMonitor::initialize() {
 
   StatusCode sc = GaudiHistoAlg::initialize();
- if (sc.isFailure()) return Error("Failed to initialize", sc);
+  if (sc.isFailure()) return Error("Failed to initialize", sc);
   
   /// set path
- if("" == histoTopDir()) setHistoTopDir("OT/");
-    
-  // Get OT Geometry from XML
-  DeOTDetector* tracker = getDet<DeOTDetector>( DeOTDetectorLocation::Default );
+  if ("" == histoTopDir() ) setHistoTopDir("OT/");
   
-  m_nStations = tracker->nStation();
-  m_firstStation = tracker->firstStation();  
-
+  // Get OT Geometry from XML
+  m_det = getDet<DeOTDetector>( DeOTDetectorLocation::Default );
+  
   // intialize histos
   initHistograms();
-
- return StatusCode::SUCCESS;
+  
+  return StatusCode::SUCCESS;
 }
 
 StatusCode MCOTDepositMonitor::execute() {
@@ -91,18 +106,31 @@ StatusCode MCOTDepositMonitor::execute() {
 
   // number of deposits
   const unsigned nTotalDeposits        = deps->size();
-  const unsigned nSignalDeposits       = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isSignal     , _1 ) );
-  const unsigned nNoiseDeposits        = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isNoise      , _1 ) );
-  const unsigned nXTalkDeposits        = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isXTalk      , _1 ) );
-  const unsigned nDoublePulseDeposits  = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isDoublePulse, _1 ) );
-  
-  plot( nTotalDeposits      , 10, "Total number of deposits"       , 0.0, 20000.0, 200 );
-  plot( nSignalDeposits     , 11, "Number of signal deposits"      , 0.0, 20000.0, 200 );
-  plot( nNoiseDeposits      , 12, "Number of noise deposits"       , 0.0, 20000.0, 200 );
-  plot( nXTalkDeposits      , 13, "Number of XTalk deposits"       , 0.0, 20000.0, 200 );
-  plot( nDoublePulseDeposits, 14, "Number of double pulse deposits", 0.0, 20000.0, 200 );
+  const unsigned nSignalDeposits       = std::count_if( deps->begin(), deps->end(), bind( &LHCb::MCOTDeposit::isSignal     , _1 ) );
+  const unsigned nNoiseDeposits        = std::count_if( deps->begin(), deps->end(), bind( &LHCb::MCOTDeposit::isNoise      , _1 ) );
+  const unsigned nXTalkDeposits        = std::count_if( deps->begin(), deps->end(), bind( &LHCb::MCOTDeposit::isXTalk      , _1 ) );
+  const unsigned nDoublePulseDeposits  = std::count_if( deps->begin(), deps->end(), bind( &LHCb::MCOTDeposit::isDoublePulse, _1 ) );
+  const unsigned nSpillOver            = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isSpillOver     , _1 ) );
+  const unsigned nCurrent              = std::count_if( deps->begin(), deps->end(), bind( &MonitorHelpers::isCurrent       , _1 ) );
 
-  // histos per deposit
+  plot( double( nTotalDeposits       ), 
+        10, "Total number of deposits"                         , -0.0005, 20000.5, 201 );
+  plot( double( nSignalDeposits      ), 
+        11, "Number of signal  deposits (including spill over)", -0.0005, 1.0005, 100 );
+  plot( double( nCurrent )   / double( nSignalDeposits ), 
+        12, "Fraction of current deposits over signal and spill"   , -0.0005, 1.0005, 100 );
+  plot( double( nSpillOver ) / double( nSignalDeposits )                                 , 
+        13, "Fraction of spill deposits over signal and spill", -0.0005, 1.0005, 100 );
+  plot( double( nNoiseDeposits       ) / double( nNoiseDeposits       + nSignalDeposits ), 
+        14, "Fraction of noise deposits"       , -0.0005,     0.4, 100 );
+  plot( double( nXTalkDeposits       ) / double( nXTalkDeposits       + nSignalDeposits ), 
+        15, "Fraction of XTalk deposits"       , -0.0005,     0.4, 100 );
+  plot( double( nDoublePulseDeposits ) / double( nDoublePulseDeposits + nSignalDeposits ), 
+        16, "Fraction of double pulse deposits", -0.0005, 0.4, 100 );
+  plot( double( nSpillOver + nNoiseDeposits + nXTalkDeposits + nDoublePulseDeposits ) / double( nTotalDeposits ), 
+        17, "Total fraction of background", -0.0005, 1.0005, 100 );
+
+  /// Histos per deposit
   std::for_each( deps->begin(), deps->end(), bind(&MCOTDepositMonitor::fillHistograms, this, _1) );
  
   return StatusCode::SUCCESS;
@@ -110,59 +138,110 @@ StatusCode MCOTDepositMonitor::execute() {
 
 void MCOTDepositMonitor::initHistograms() {
 
-  // number of deposits per layer
-  m_nHitsPerLayerHisto = book(3, "Number of deposits per layer", 9.5, 34.5, 25);
-  // drift distance
-  m_driftDistHisto = book(6, "Drift distance", 0., 5., 50);
-  
+  for ( std::vector< DeOTStation* >::const_iterator s    = ( m_det->stations() ).begin(), 
+	  sEnd = ( m_det->stations() ).end(); s != sEnd; ++s ) {
+    const unsigned    stationID       = (*s)->stationID();
+    const std::string stationToString = boost::lexical_cast<std::string>( stationID );
+    /// Deposit spectrum
+    m_spectrumHistos[ depSpectrum( stationID ) ] = book( depSpectrum( stationID ) , 
+							 "Deposit spectrum for T" + stationToString,
+							 -50.0*Gaudi::Units::ns, 
+							 200.0*Gaudi::Units::ns, 
+							 250);
+    /// noise spectrum
+    m_spectrumHistos[ noiseSpectrum( stationID ) ] = book( noiseSpectrum( stationID ) , 
+							   "Noise spectrum for T" + stationToString,
+							   -50.0*Gaudi::Units::ns, 
+							   200.0*Gaudi::Units::ns, 
+							   250);
 
-  // number of crosstalk hits in container
-  if ( fullDetail() ) {
-    // number of deposits per station
-    m_nHitsPerStationHisto = book(2, "Number of deposits per station", 0.5, 3.5, 3);
-
-    // histograms per station
-    int id;
-    AIDA::IHistogram1D* aHisto1D;
-    AIDA::IHistogram2D* aHisto2D;
-    // drift time spectra
-    for (int iStation = m_firstStation; iStation <= m_nStations; ++iStation) {
-      const std::string stationToString = boost::lexical_cast<std::string>(iStation);
-      id=100+iStation;
-      aHisto1D = book(id, "Deposit time per station "+stationToString,
-		      -50.0*Gaudi::Units::ns, 200.0*Gaudi::Units::ns, 250);
-      m_driftTimeHistos.push_back(aHisto1D);
-      
-      /// y vs x
-      id=200+iStation;
-      aHisto2D = book2D(id, "y(cm) vs x(cm) station "+stationToString, 
-			-3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 700, 
-                        -3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 700);
-      m_yvsxHistos.push_back(aHisto2D);
-    }
+    /// xtalk spectrum
+    m_spectrumHistos[ xTalkSpectrum( stationID ) ] = book( xTalkSpectrum( stationID ) , 
+							   "XTalk spectrum for T" + stationToString,
+							   -50.0*Gaudi::Units::ns, 
+							   200.0*Gaudi::Units::ns, 
+							   250);
+    /// double pulse spectrum
+    m_spectrumHistos[ dPulseSpectrum( stationID ) ] = book( dPulseSpectrum( stationID ) , 
+							    "Double pulse spectrum for T" + stationToString,
+							    -50.0*Gaudi::Units::ns, 
+							    200.0*Gaudi::Units::ns, 
+							    250);
+    
+    /// y vs x
+    m_hitDistHistos[ stationID ]     = book2D( 500 + stationID, 
+					       "Hit distribution y(cm) vs x(cm) for station " + stationToString, 
+					       -3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 700, 
+					       -3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 700 );
   }
 }
 
-void MCOTDepositMonitor::fillHistograms( LHCb::MCOTDeposit* aDeposit ) {
+void MCOTDepositMonitor::fillHistograms( const LHCb::MCOTDeposit* aDeposit ) {
   
-  // per layer
-  int iStation = (aDeposit->channel()).station();
-  int iLayer = (aDeposit->channel()).layer();
-  int iUniqueLayerNum = 10 * iStation + iLayer;
+  /// Get channel
+  const LHCb::OTChannelID& channel = aDeposit->channel();
   
-  m_nHitsPerLayerHisto->fill(double(iUniqueLayerNum));
+  /// # deposits per station 
+  unsigned stationID = channel.station();
+  plot( double( stationID ), 20, "Number of deposits per station", 0.5, 3.5, 3 );
+  /// # deposits per layer
+  unsigned layerID   = 10u*channel.station() + channel.layer();
+  plot( double( layerID )  , 21, "Number of deposits per layer"  , 9.5, 33.5, 24 );
+  /// # deposits per quarter
+  unsigned quarterID = 100u*channel.station() + 10u*channel.layer() + channel.quarter();
+  plot( double( quarterID ), 22, "Number of deposits per quarter", 99.5, 333.5, 234 );
+  /// # deposits per module
+  unsigned moduleID  = 1000u*channel.station() + 100u*channel.layer() + 10u*channel.quarter() + channel.module();
+  plot( double( moduleID ),  23, "Number of deposits per module" , 1000.5, 3339.5, 2339 );
   
-  if ( fullDetail() ) {
-    // reference to mctruth
-    const LHCb::MCHit* aHit = aDeposit->mcHit();
-   // histogram per station
-    m_nHitsPerStationHisto->fill(double(iStation));
-    m_driftTimeHistos[iStation-m_firstStation]->fill(aDeposit->time());
-    if ( 0 != aHit ) {
-      // retrieve entrance + exit points and take average
-      Gaudi::XYZPoint mcHitPoint = aHit->midPoint();
-      // fill y vs x scatter plots    
-      m_yvsxHistos[iStation-m_firstStation]->fill(mcHitPoint.x()/Gaudi::Units::cm, mcHitPoint.y()/Gaudi::Units::cm);
+
+  m_spectrumHistos[ depSpectrum( stationID ) ]->fill( aDeposit->time() );
+  if ( aDeposit->isNoise()       ) m_spectrumHistos[ noiseSpectrum( stationID )  ]->fill( aDeposit->time() );
+  if ( aDeposit->isXTalk()       ) m_spectrumHistos[ xTalkSpectrum( stationID )  ]->fill( aDeposit->time() );
+  if ( aDeposit->isDoublePulse() ) m_spectrumHistos[ dPulseSpectrum( stationID ) ]->fill( aDeposit->time() );
+
+  const LHCb::MCHit* aHit = aDeposit->mcHit();
+  if ( aHit ) {
+    /// retrieve entry + exit points and take average
+    const Gaudi::XYZPoint& mcHitPoint = aHit->midPoint();
+    //fill y vs x scatter plots    
+    m_hitDistHistos[ stationID ]->fill(mcHitPoint.x()/Gaudi::Units::cm, mcHitPoint.y()/Gaudi::Units::cm);
+    if ( layerID ==  10 ) plot2D( mcHitPoint.x()/Gaudi::Units::cm, mcHitPoint.y()/Gaudi::Units::cm, 600,
+				   "Hit distribution y(cm) vs x(cm) for T1X1", 
+				   -3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 
+				  -3500./Gaudi::Units::cm,3500./Gaudi::Units::cm, 700, 700 );
+    
+    if ( aDeposit->isSignal() ) plot( aDeposit->driftDistance(), 1, "Drift distance", -0.005, 3.0, 100 );
+    
+    /// OK so we only do this for T3, but in the end we want to do it for all stations
+    if ( stationID == 3 && aDeposit->isSignal() ) {
+      plot( aDeposit->time(), 30, "Spectrum of spills in T3", 
+	    -50.0*Gaudi::Units::ns, 
+	    200.0*Gaudi::Units::ns, 
+	    250);
+      if ( MonitorHelpers::isSpill( "/", aHit ) ) plot( aDeposit->time(), 31, "Spectrum of current spill in T3", 
+							-50.0*Gaudi::Units::ns, 
+							200.0*Gaudi::Units::ns, 
+							250);
+      else if ( MonitorHelpers::isSpill( "/PrevPrev/", aHit ) ) {
+	/// Need to split this up since we copy PrevPrev to NextNext
+	if ( aDeposit->time() < 75.0*Gaudi::Units::ns ) plot( aDeposit->time(), 32, "Spectrum of PrevPrev spill in T3", 
+							      -50.0*Gaudi::Units::ns, 
+							      200.0*Gaudi::Units::ns, 
+							      250);
+	else if ( aDeposit->time() > 75.0*Gaudi::Units::ns ) plot( aDeposit->time(), 33, "Spectrum of NextNext spill in T3", 
+								   -50.0*Gaudi::Units::ns, 
+								   200.0*Gaudi::Units::ns, 
+								   250);
+      }
+      else if ( MonitorHelpers::isSpill( "/Prev/", aHit ) ) plot( aDeposit->time(), 34, "Spectrum of Prev spill in T3", 
+								  -50.0*Gaudi::Units::ns, 
+								  200.0*Gaudi::Units::ns, 
+								  250);
+      else if ( MonitorHelpers::isSpill( "/Next/", aHit ) ) plot( aDeposit->time(), 35, "Spectrum of Next spill in T3", 
+								  -50.0*Gaudi::Units::ns, 
+								  200.0*Gaudi::Units::ns, 
+								  250);
     }
   }
 }
