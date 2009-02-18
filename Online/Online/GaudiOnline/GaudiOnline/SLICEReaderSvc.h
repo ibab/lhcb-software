8cc7d10934459148f6e776f345367c5de677f2ea
Jean-Christophe Garnier <jean-christophe.garnier@cern.ch>
2009-02-18 08:28:21 +0000

    FEST V2 file reader
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99039 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e8d8b95cff8d3bfccc342528e2f9ac5b5f005ad5

---
+++ Online/Online/GaudiOnline/GaudiOnline/SLICEReaderSvc.h
@@ -0,0 +1,147 @@
+//	============================================================
+//
+//	SLICEReaderSvc.h
+//	------------------------------------------------------------
+//
+//	Package   : GaudiOnline
+//
+//	Author    : Jean-Christophe Garnier 
+//
+//	===========================================================
+#ifndef GAUDIONLINE_SLICEREADERSVC_H
+#define GAUDIONLINE_SLICEREADERSVC_H 1
+
+#include <map>
+#include <vector>
+#include <string>
+#include <queue>
+
+#include <pthread.h>
+#include <semaphore.h>
+
+// Framework include files
+#include "Event/RawEvent.h"
+#include "GaudiKernel/Service.h"
+#include "GaudiKernel/IRunable.h"
+#include "GaudiKernel/Message.h"
+#include "GaudiKernel/IIncidentListener.h"
+#include "GaudiKernel/IMonitorSvc.h"
+#include "MBM/bmdef.h"
+#include "MBM/Producer.h"
+
+//XXX Dont think I need this to read files and put in shm or gaudi buffer or other structure
+// Online stuff
+#include "RTL/rtl.h"
+#include "RTL/types.h"
+#include "NET/IPHeader.h"
+#include "GaudiOnline/MEPHdr.h"
+#include "dis.hxx"
+
+#include <mqueue.h>
+
+// Forward declarations
+class MsgStream;
+class IIncidentSvc;
+class ISvcLocator;
+
+#define TWOMB 2097152
+#define MDFHDRSZ 48 //12 word of 32 bits, or 4 bytes
+#define BKSZMAX 65536
+
+/*
+ *  LHCb namespace declaration
+ */
+namespace LHCb  {
+
+  /** @class SLICEReaderSvc SLICEReader.h GaudiOnline/SLICEReader.h
+    *
+    * Read 2MB slices of a file, find the event and push them to a shared buffer
+    * 2 modes : bank reader, without MDFHeader nor DAQ bank, and event reader, with MDFHeader  
+    *
+    *  @author Jean-Christophe Garnier
+    *  @version 1.0
+    */
+  class SLICEReaderSvc : public Service,  
+                    virtual public IRunable,
+                    virtual public IIncidentListener 
+  {
+  public:
+    typedef std::vector<std::string>        Requirements;
+
+  protected:
+    Requirements m_Req; /* Vector of strings which tells the options to access to buffer managers,             */
+                        /* Most are useless and should be like this :                                          */
+                        /* "EvType=2;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0; */
+                        /*  MaskType=ANY;UserType=VIP;Frequency=PERC;Perc=100.0"                               */
+                        /* But I wanted it to be parameterisable                                               */
+
+    std::string m_ProcName;  /* RTL process name */
+
+    int m_PartitionID; /* Partition ID of the events, read from option file and/or from Odin */ 
+
+    std::string m_BufferName; /* Name of the buffer manager where to push events, partitionID is appended to it */ 
+    BMID m_BMID;              /* Buffer manager id*/ 
+ 
+    enum ReaderState { NOT_READY, READY, RUNNING, STOPPED }; /* FSM state of the Slice Reader Service */
+    ReaderState  m_ReaderState; /* Current state */
+  
+    bool m_LoopOverFiles; /* Once all files have been read, starts from beginning */          
+
+    int m_CurFile; /* Current file descriptor */ 
+
+    int m_SliceLen;   /* Length of the slice read, most of the time 2MB except when last slice of the file */   
+    int m_CurEvtLen;  /* Length of the current event */
+    int m_OffsetEvt;  /* To manage the slice of 2MBs, address of the beginning of an event */
+    int m_CurEvtIte;  /* To tell how many bytes were took in consideration in the current event, if it is splitted on 2 files */
+                      /* while it is not equal to CurEvtLen, we're on the same event */
+    int m_CurSliceIte;/* Current position on the slice */ 
+
+    std::vector<std::string>    m_InputFiles; /* Files to read */
+
+    char m_Buffer[TWOMB];   /* Buffer containing the chunk of 2MB read from file  */
+    char m_CurEvent[TWOMB]; /* Buffer containing the current event, XXX assuming max size is 2MB ...*/
+
+    IIncidentSvc*               m_IncidentSvc; /* Service to catch DIM commands */
+    IMonitorSvc*                m_MonSvc;      /* Service to publish information*/
+
+    int m_TotEvtsRead; /* Total of events read */
+
+    BMID getBuffer(); /* The method to connect to the buffer manager */
+  public:
+    // IInterface implementation : queryInterface
+    virtual StatusCode queryInterface(const InterfaceID& riid, void** ppvInterface); 
+    /// Standard Constructor
+    SLICEReaderSvc(const std::string& name, ISvcLocator*parent);
+    /// Standard Destructor
+    virtual ~SLICEReaderSvc(){} 
+
+    /// Service overload: initialize()
+    virtual StatusCode initialize();
+    /// Service overload: finalize()
+    virtual StatusCode finalize();
+    /// IRunable implementation: Run the object
+    virtual StatusCode run();
+    /// Handle
+    virtual void handle(const Incident& incident);
+
+    /// Decode the slice of the MDF file 
+    StatusCode decodeMDF(char *slice, int size);
+    /// Read slices of a MDF file 
+    StatusCode readFile();
+
+    /// Return FAILURE and print information 
+    StatusCode error(const std::string & msg);
+  
+    /// Push the event in the buffer manager
+    StatusCode pushEvent(char *evt, int size);
+
+    /// Setup counters for monitoring
+    int setupCounters();  
+
+  private:
+  };
+};      // End namespace LHCb
+
+#endif //  GAUDIONLINE_SLICEREADERSVC_H
+
+ 
