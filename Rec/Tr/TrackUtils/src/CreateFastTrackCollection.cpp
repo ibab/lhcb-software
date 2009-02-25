2d883d09c7c9800fb671b61667945629b6162c88
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2009-02-25 22:46:51 +0000

    added CreateFastTrackCollection algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@18775 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5c77b32efd8b56a6f9052b380c8deb3b46dedcb8

---
+++ Rec/Tr/TrackUtils/src/CreateFastTrackCollection.cpp
@@ -0,0 +1,98 @@
+// $Id: CreateFastTrackCollection.cpp,v 1.1 2009-02-25 22:46:51 mschille Exp $
+#include "GaudiKernel/AlgFactory.h"
+
+#include "CreateFastTrackCollection.h"
+
+#include "GaudiKernel/SharedObjectsContainer.h"
+#include "Event/Track.h"
+
+#include <boost/foreach.hpp>
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : CreateFastTrackCollection
+//
+// 2009-02-25 : Manuel Tobias Schiller <schiller@physi.uni-heidelberg.de>
+//-----------------------------------------------------------------------------
+
+DECLARE_ALGORITHM_FACTORY( CreateFastTrackCollection );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+CreateFastTrackCollection::CreateFastTrackCollection(const std::string& name,
+	ISvcLocator* pSvcLocator) :
+    GaudiAlgorithm(name, pSvcLocator)
+{
+    declareProperty("InputLocations", m_inputLocations);
+    declareProperty("OutputLocation", m_outputLocation);
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+CreateFastTrackCollection::~CreateFastTrackCollection() {;}
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode CreateFastTrackCollection::initialize()
+{
+    StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+    if (sc.isFailure()) return sc;  // error printed already by GaudiAlgorithm
+
+    debug() << "==> Initialize" << endmsg;
+
+    // verify job options
+    if (m_inputLocations.empty()) {
+	error() << "No input locations specified." << endmsg;
+	return StatusCode::FAILURE;
+    }
+    if (m_outputLocation.empty()) {
+	error() << "No output locations specified." << endmsg;
+	return StatusCode::FAILURE;
+    }
+    unsigned pos = 0;
+    bool allok = true;
+    BOOST_FOREACH(const std::string& src, m_inputLocations) {
+	++pos;
+	if (src.empty()) {
+	    error() << "Invalid input location specified (position " <<
+		    pos << ")." << endmsg;
+	    allok = false;
+	}
+    }
+    if (!allok)
+	return StatusCode::FAILURE;
+
+    return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode CreateFastTrackCollection::execute()
+{
+    debug() << "==> Execute" << endmsg;
+    // create output container and put it on TES
+    SharedObjectsContainer<LHCb::Track> *out =
+	new SharedObjectsContainer<LHCb::Track>;
+    put(out, m_outputLocation);
+    // get all input containers in turn and put track pointers into output
+    BOOST_FOREACH(const std::string& src, m_inputLocations) {
+	LHCb::Tracks *input = get<LHCb::Tracks>(src);
+	out->insert(input->begin(), input->end());
+    }
+
+    return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Finalize
+//=============================================================================
+StatusCode CreateFastTrackCollection::finalize()
+{
+    debug() << "==> Finalize" << endmsg;
+
+    return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
---
+++ Rec/Tr/TrackUtils/src/CreateFastTrackCollection.h
@@ -0,0 +1,35 @@
+// $Id: CreateFastTrackCollection.h,v 1.1 2009-02-25 22:46:51 mschille Exp $
+#ifndef INCLUDE_CREATEFASTTRACKCOLLECTION_H
+#define INCLUDE_CREATEFASTTRACKCOLLECTION_H 1
+
+#include <vector>
+#include <string>
+
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+/** @class CreateFastTrackCollection CreateFastTrackCollection.h
+ * given a list of input track containers, this algorithm creates a fast
+ * GaudiSharedObjectsContainer containing pointers to the tracks in the
+ * input containers given
+ *
+ * @author Manuel Tobias Schiller <schiller@physi.uni-heidelberg.de>
+ * @date   2009-02-25
+ */
+class CreateFastTrackCollection : public GaudiAlgorithm
+{
+    public:
+	/// Standard Constructor
+	CreateFastTrackCollection(const std::string& name, ISvcLocator* pSvcLocator);
+
+	virtual ~CreateFastTrackCollection(); ///< Destructor
+
+	virtual StatusCode initialize(); ///< Algorithm initialization
+	virtual StatusCode    execute(); ///< Algorithm event execution
+	virtual StatusCode   finalize(); ///< Algorithm finalize
+
+    private:
+	std::vector<std::string> m_inputLocations; ///< input locations
+	std::string m_outputLocation; ///< output location
+};
+#endif // INCLUDE_CREATEFASTTRACKCOLLECTION_H
+
