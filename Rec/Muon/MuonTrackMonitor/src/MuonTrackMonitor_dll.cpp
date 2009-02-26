c7de7f0d529b8cca89aa1481237dda26a6811b1e
Giacomo Graziani <ggiacomo@cern.ch>
2009-02-26 11:10:46 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21786 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f535c4e7ecdb594a8904a140ae5871e92d142e7f

---
+++ Rec/Muon/MuonTrackMonitor/src/MuonTrackMonitor_dll.cpp
@@ -0,0 +1,7 @@
+// $Id: MuonTrackMonitor_dll.cpp,v 1.1 2009-02-26 11:10:46 ggiacomo Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+LOAD_FACTORY_ENTRIES(MuonTrackMonitor)
+
