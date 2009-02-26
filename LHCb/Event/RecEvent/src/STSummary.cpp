a87576f87b2523cecebd9349169c757c54add544
Matthew David Needham <mneedham@cern.ch>
2009-02-26 14:43:53 +0000

    Changes in STSummary class
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37279 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 98a1ba108fdf1bb7953b2062977be2c7194217e7

--- LHCb/Event/RecEvent/src/STSummary.cpp
+++ LHCb/Event/RecEvent/src/STSummary.cpp
@@ -1,4 +1,4 @@
-// $Id: STSummary.cpp,v 1.1 2009-01-07 07:54:38 mneedham Exp $
+// $Id: STSummary.cpp,v 1.2 2009-02-26 14:43:53 mneedham Exp $
 #include "Event/STSummary.h"
 
 // fillstream method
@@ -7,37 +7,39 @@ std::ostream& LHCb::STSummary::fillStream(std::ostream& s) const
 
   std::string syncStatus = "OK";
   if (m_pcnSynch == false) syncStatus = "Not" + syncStatus; 
+  s << "Summary Block " << std::endl;
   s << "# Clusters " << m_nClusters << " PCN " << m_pcn << " Sync status " << syncStatus;
   s << std::endl;
+  s << "# bytes " << m_rawBufferSize << std::endl;
+  s << "# full banks " << m_nFullBanks << std::endl;
+  s << "# pedestal banks " << m_nPedestalBanks << std::endl;
+  s << "# error banks " << m_nErrorBanks << std::endl;
   if (m_missingBanks.empty() == false) {
     // print the missing in banks
     s << "Missing In Action";
     for (std::vector<unsigned int>::const_iterator iterM = m_missingBanks.begin();
          iterM != m_missingBanks.end(); ++iterM){
       s << "Missing: ";
-      s << std::endl;
       s << "Bank " << *iterM << std::endl;      
     } // iterM  
   }
 
-  if (m_banksWithError.empty() == false) {
+  if (m_corruptedBanks.empty() == false) {
     // print the banks in error
-    s << "Banks in error";
-    for (std::vector<unsigned int>::const_iterator iterE = m_banksWithError.begin();
-         iterE != m_banksWithError.end(); ++iterE){
-      s << "Missing: ";
-      s << std::endl;
+    s << "Corrupted banks: ";
+    for (std::vector<unsigned int>::const_iterator iterE = m_corruptedBanks.begin();
+         iterE != m_corruptedBanks.end(); ++iterE){
+      s << "Corrupted: ";
       s << "Bank " << *iterE << std::endl;      
     } // iterM  
   }
  
-  s << std::endl;
+ 
   if (m_recoveredBanks.empty() == false){
     // print the banks that were recovered
     RecoveredInfo::const_iterator iterRec = m_recoveredBanks.begin();
     s << "Recovered: ";
     for (; iterRec != m_recoveredBanks.end(); ++iterRec){
-      s << std::endl;
       s << " Bank " << iterRec->first << " fraction " << iterRec->second;
     } // iterRec
   }
