cc4c4153129b50aaff813c4e4b9d5b4e9aa0ff10
hmdegaud <hmdegaud@cern.ch>
2009-02-27 09:57:04 +0000

    fixed the argument list of the wrapper as well as the call to "python -m". Now runpy.bat is used
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13559 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbRelease/scripts/startNightlies.bat
+++ LbScripts/LbRelease/scripts/startNightlies.bat
@@ -1,5 +1,5 @@
 
 call SetupProject.bat LCGCMT lcgnightlies
 
-python -m LbRelease.Nightlies.startNightlies ${*:q} %1 %2 %3 %4 %5 %6 %7 %8 %9
+call runpy LbRelease.Nightlies.startNightlies %1 %2 %3 %4 %5 %6 %7 %8 %9
 
