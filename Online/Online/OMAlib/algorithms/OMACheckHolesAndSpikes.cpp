80757ba695fe09d016d1dfb405aa08d04c1a5b6c
Giacomo Graziani <ggiacomo@cern.ch>
2009-03-04 15:26:31 +0000

    improve OMACheckHolesAndSpikes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99137 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ebcb62f2aebce6194ac17dffb9d40e9d5656a79c

--- Online/Online/OMAlib/algorithms/OMACheckHolesAndSpikes.cpp
+++ Online/Online/OMAlib/algorithms/OMACheckHolesAndSpikes.cpp
@@ -2,6 +2,7 @@
 #include <TF1.h>
 #include "OMAlib/OMAAlgorithms.h"
 #include <sstream>
+#include <cmath>
 
 OMACheckHolesAndSpikes::OMACheckHolesAndSpikes(OMAcommon* Env) : 
   OMACheckAlg("CheckHolesAndSpikes", Env) {
@@ -17,8 +18,8 @@ OMACheckHolesAndSpikes::OMACheckHolesAndSpikes(OMAcommon* Env) :
   m_npars = 2;
   m_parnames.push_back("MinDelta"); m_parDefValues.push_back(-999999.);
   m_parnames.push_back("MaxDelta"); m_parDefValues.push_back(+999999.);
-  m_doc = "Check for holes and spikes of histogram. Check is done wrt to the ref. histogram if ";
-  m_doc +=  "Reference<0, or wrt a fitted polynomial of degree=Reference. Delta can be a limit on the ";
+  m_doc = "Check for holes and spikes of histogram. Check is done against the (normalized) ref. histogram if ";
+  m_doc +=  "Reference<0, or against a fitted polynomial of degree=Reference. Delta can be a limit on the ";
   m_doc +=  "ratio (DeltaMode=1), absolute difference (DeltaMode=2), number of sigma (DeltaMode=3).";
   m_needRef = true;
 }
@@ -32,16 +33,20 @@ void OMACheckHolesAndSpikes::exec(TH1 &Histo,
   if( warn_thresholds.size() <m_npars ||  alarm_thresholds.size() <m_npars 
       || input_pars.size() <m_ninput )
     return;
-  
+  if (Histo.GetEntries() == 0)
+    return; // do nothing for empty histograms
+  if (input_pars[0] < 0.) input_pars[0]= -99.;
   int ipoly= (int)(input_pars[0] + .1 );
   DeltaMode mode = static_cast<DeltaMode> ((int)(input_pars[1] + .1 ) );
   if (mode <= MinMode || mode>= MaxMode ) return;
   
-  std::vector<double> refValues;
+  std::vector<double> refValues,refErrors;
   int nBx = Histo.GetNbinsX();
   int nBy = Histo.GetNbinsY();
+
   refValues.resize(nBx * nBy);
-  
+  refErrors.resize(nBx * nBy);
+
   if( ipoly>=0 ) { //polynomial fit
     // only 1-dim histograms are supported for the moment
     if (Histo.GetDimension() > 1) return;
@@ -51,14 +56,19 @@ void OMACheckHolesAndSpikes::exec(TH1 &Histo,
     TF1 *fit = Histo.GetFunction(ss.str().c_str());
     for (int ih=1 ; ih<= nBx ; ih++) {
       refValues[ih-1] = fit->Eval(Histo.GetBinCenter(ih));
+      refErrors[ih-1] = 0.;
     }
   }
   else if(Ref) { // compare with reference 
+    // normalize Ref to current histogram
+    Ref->Scale(Histo.Integral() / Ref->Integral() );
     for (int ihx=1 ; ihx<= nBx ; ihx++) {
       for (int ihy=1 ; ihy<= nBy ; ihy++) {
         if( ihx <=  Ref->GetNbinsX() &&
-            ihy <=  Ref->GetNbinsY() )
+            ihy <=  Ref->GetNbinsY() ) {
           refValues[(ihx-1)*nBy + (ihy-1)] = Ref->GetBinContent(ihx,ihy);
+          refErrors[(ihx-1)*nBy + (ihy-1)] = Ref->GetBinError(ihx,ihy);
+        }
       }
     }
   }
@@ -74,6 +84,7 @@ void OMACheckHolesAndSpikes::exec(TH1 &Histo,
       double delta=0.,err=0.;
       double content = Histo.GetBinContent(ihx,ihy);
       double &xref=refValues[(ihx-1)*nBy +(ihy-1)];
+      double &xrefer=refErrors[(ihx-1)*nBy +(ihy-1)];
       switch(mode) {
       case Ratio:
         // take the ratio, except for cases where both values are very small 
@@ -95,7 +106,10 @@ void OMACheckHolesAndSpikes::exec(TH1 &Histo,
       case SigmaDiff:
         err = Histo.GetBinError(ihx,ihy);
         // assuming error=1.2 in case of 0 content 
-        delta= (err > 0.) ? (content - xref)/err : (content - xref)/1.2;
+        if (err<OMAconstants::epsilon) err=1.2;
+        // add error from reference
+        err = sqrt( pow(err, 2) + pow(xrefer,2));
+        delta= (content - xref)/err;
         break;
       default:
         break;
