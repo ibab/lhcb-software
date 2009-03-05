fb9ad8d2ec3d00ed8faa6a21406368b04e5cbcc5
Stephanie Hansmann-Menzemer <menzemer@physi.uni-heidelberg.de>
2009-03-05 09:26:39 +0000

    add doxygend documentation
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@18788 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7b3637bb302b52aeb7a3675ac186d0b0db604501

--- LHCb/Tr/TrackInterfaces/TrackInterfaces/ITrackStateInit.h
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/ITrackStateInit.h
@@ -20,9 +20,18 @@ public:
 
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_ITrackStateInit; }
-
-  virtual StatusCode fit( LHCb::Track& track, bool celarStates = true) const = 0;
+  /**
+   * remove all states on the track, apply a simple fit only based on LHCbIds
+   * and reinitalize a list of states. clearStates = false leaves the tracks
+   * untouched for the moment , more features will be added here later.
+   */   
+  virtual StatusCode fit( LHCb::Track& track, bool clearStates = true) const = 0;
+  /**
+   * check if track states are initalized at a given list of z reference positions.
+   * if list of state not complete, add additional states based on extrapolation of
+   * states given on the track
+   */
   virtual StatusCode initializeRefStates(LHCb::Track& track,
-					 LHCb::ParticleID pid = 211 ) const = 0;
+					 LHCb::ParticleID pid = LHCb::ParticleID(211) ) const = 0;
 };
 #endif
