caffb1e8e479344f8f5ee4e6efc0eea842e23621
Markus Frank <frankb@cern.ch>
2009-03-05 12:01:14 +0000

    Add tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99148 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3392fb6ed7d4a5c4517e16cf6ba14b574cf9adcd

--- Online/Online/OnlineKernel/src/RTL/TestScreen.cpp
+++ Online/Online/OnlineKernel/src/RTL/TestScreen.cpp
@@ -3,13 +3,15 @@
 #include "RTL/rtl.h"
 #include "RTL/screen.h"
 
-extern "C" int rtl_testscr(int,char **) {
+extern "C" int rtl_testscreen(int,char **) {
   char caption[255];
   initscreen();
   printxy(10, 20,"Welcome to Curses!");
   sprintf(caption, "This is just a message!");
-  printxy(11, 20,caption);
+  printxy(10, 22,caption);
+  printxy(1,  33,".....");
   refresh();
-  lib_rtl_sleep(1000*100);
+  lib_rtl_sleep(1000*20);
+  
   return(0);
 }
