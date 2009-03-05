31ebb415466d8bcc837c8ef06b28962f342282af
rlambert <rlambert@cern.ch>
2009-03-05 14:04:20 +0000

    Updated IEvtTypeScv
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48228 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a749fd07be6356331fbd86154618b3256ebac51d

--- LHCb/Kernel/LHCbKernel/Kernel/IEvtTypeSvc.h
+++ LHCb/Kernel/LHCbKernel/Kernel/IEvtTypeSvc.h
@@ -1,12 +1,21 @@
-// $Id: IEvtTypeSvc.h,v 1.1 2005-12-02 16:28:07 cattanem Exp $
+// $Id: IEvtTypeSvc.h,v 1.2 2009-03-05 14:04:20 rlambert Exp $
 #ifndef KERNEL_IEVTTYPESVC_H 
 #define KERNEL_IEVTTYPESVC_H 1
 
 // Include files
 #include "GaudiKernel/IService.h"
+#include <set>
 
 // Declaration of the interface ID (interface id, major version, minor version)
-static const InterfaceID IID_IEvtTypeSvc("IEvtTypeSvc", 1 , 0); 
+static const InterfaceID IID_IEvtTypeSvc("IEvtTypeSvc", 2 , 0); 
+
+//definition of how to store sets of events
+namespace LHCb
+{
+  typedef std::greater<long unsigned int> EventTypeComp;
+  typedef std::set<long unsigned int, EventTypeComp > EventTypeSet;
+}
+
 
 /** @class IEvtTypeSvc IEvtTypeSvc.h Kernel/IEvtTypeSvc.h
  *
@@ -14,6 +23,11 @@ static const InterfaceID IID_IEvtTypeSvc("IEvtTypeSvc", 1 , 0);
  *
  *  @author Gloria CORTI
  *  @date   2004-04-30
+ *
+ *  Version 2, R. Lambert  04/03/2009
+ *  Added allTypes method, and defined the EventTypeComp and EventTypeSet
+ *  to handle groups of Event type codes in an ordered, unique std::set
+ *
  */
 class IEvtTypeSvc : virtual public IService {
 public: 
@@ -34,6 +48,9 @@ public:
   /// Given an event code number check if it 'exist', i.e. is known by the
   /// service
   virtual bool typeExists( const int evtCode ) = 0;
+
+  /// Return all known event types in a unique and ordered std::set
+  virtual LHCb::EventTypeSet allTypes( void ) = 0;
 
 };
 #endif // KERNEL_IEVTTYPESVC_H
