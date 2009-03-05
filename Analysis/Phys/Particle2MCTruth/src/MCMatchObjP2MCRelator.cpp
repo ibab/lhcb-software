7365c0c2bb76a30e5220ab6b5fa4bd357500742c
jpalac <jpalac@cern.ch>
2009-03-05 11:27:24 +0000

    Add some LoKi MCMatch-related stuff
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75427 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c925e7a66c427dcb0a8faf915e6961b1892c7130

--- Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.cpp
+++ Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.cpp
@@ -1,9 +1,10 @@
-// $Id: MCMatchObjP2MCRelator.cpp,v 1.1.1.1 2009-03-04 14:26:10 jpalac Exp $
+// $Id: MCMatchObjP2MCRelator.cpp,v 1.2 2009-03-05 11:27:24 jpalac Exp $
 // Include files 
 
 // from Gaudi
 #include "GaudiKernel/ToolFactory.h" 
-
+// LoKi
+#include "LoKi/IReporter.h"
 // local
 #include "MCMatchObjP2MCRelator.h"
 
@@ -24,15 +25,26 @@ MCMatchObjP2MCRelator::MCMatchObjP2MCRelator( const std::string& type,
                                               const std::string& name,
                                               const IInterface* parent )
   : 
-  Particle2MCAssociatorBase( type, name , parent )
+  Particle2MCAssociatorBase( type, name , parent ),
+  m_reporter(0),
+  m_matcher(0),
+  m_PP2MC()
 {
+
+  m_PP2MC.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Charged  ) ;
+  m_PP2MC.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Upstream ) ;
+  m_PP2MC.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Neutrals ) ;
+
   declareInterface<IParticle2MCAssociator>(this);
 
 }
 //=============================================================================
 StatusCode MCMatchObjP2MCRelator::initialize()
 {
-  return Particle2MCAssociatorBase::initialize();
+  StatusCode sc =  Particle2MCAssociatorBase::initialize();
+  if (sc.isFailure()) return sc;
+  m_reporter = tool<LoKi::IReporter>( "LoKi::Reporter", this ) ;
+  return (0!=m_reporter) ? StatusCode::SUCCESS : StatusCode::FAILURE;
 }
 //=============================================================================
 StatusCode MCMatchObjP2MCRelator::finalize()
@@ -44,6 +56,23 @@ bool MCMatchObjP2MCRelator::isMatched(const LHCb::Particle* particle,
                                       const LHCb::MCParticle* mcParticle) const
 {
   return false;
+}
+//=============================================================================
+LoKi::MCMatch MCMatchObjP2MCRelator::matcher() const
+{
+
+  if (0==m_matcher) {
+    // get the reporter 
+    // create the new matcher 
+    m_matcher = new LoKi::MCMatchObj( "P2MCRelator" , m_reporter ) ;
+    // increment the reference counter 
+    m_matcher->addRef() ;
+  }
+  m_matcher->clear();
+  
+  // feed it the relations tables
+
+  return  LoKi::MCMatch( m_matcher ) ;
 }
 //=============================================================================
 // Destructor
--- Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.h
+++ Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.h
@@ -1,4 +1,4 @@
-// $Id: MCMatchObjP2MCRelator.h,v 1.1.1.1 2009-03-04 14:26:10 jpalac Exp $
+// $Id: MCMatchObjP2MCRelator.h,v 1.2 2009-03-05 11:27:24 jpalac Exp $
 #ifndef MCMATCHOBJP2MCRELATOR_H 
 #define MCMATCHOBJP2MCRELATOR_H 1
 
@@ -7,7 +7,13 @@
 #include "GaudiAlg/GaudiTool.h"
 #include "Kernel/IParticle2MCAssociator.h"            // Interface
 #include "Kernel/Particle2MCAssociatorBase.h"
-
+#include "LoKi/MCMatch.h"
+#include "LoKi/MCMatchObj.h"
+// forward declarations
+namespace LoKi 
+{
+  class IReporter;
+}
 /** @class MCMatchObjP2MCRelator MCMatchObjP2MCRelator.h
  *  
  *
@@ -32,6 +38,15 @@ public:
                          const LHCb::MCParticle* mcParticle) const ;
 
 private:
+
+  LoKi::MCMatch matcher() const;
+
+private:
+
+  LoKi::IReporter* m_reporter;
+  mutable LoKi::MCMatchObj* m_matcher;
+  typedef std::vector<std::string> Addresses ;
+  Addresses m_PP2MC;
 
 };
 #endif // MCMATCHOBJP2MCPRELATOR_H
