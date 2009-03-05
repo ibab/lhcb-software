da2666596fb8f52e047e450f943ab22988d77bdf
Olivier Deschamps <odescham@cern.ch>
2009-03-05 15:36:36 +0000

    automatic bin label for Status monitoring
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37288 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d68ce8f6af2099305c9fc1a78d68b5223c71c9df

--- LHCb/Event/DAQEvent/src/component/RawBankReadoutStatusMonitor.cpp
+++ LHCb/Event/DAQEvent/src/component/RawBankReadoutStatusMonitor.cpp
@@ -1,9 +1,9 @@
-// $Id: RawBankReadoutStatusMonitor.cpp,v 1.7 2008-09-15 17:46:55 odescham Exp $
+// $Id: RawBankReadoutStatusMonitor.cpp,v 1.8 2009-03-05 15:36:36 odescham Exp $
 // Include files 
 
 // from Gaudi
 #include "GaudiKernel/AlgFactory.h" 
-
+#include "GaudiUtils/Aida2ROOT.h"
 // local
 #include "Event/RawBank.h"
 #include "Event/RawBankReadoutStatus.h"
@@ -25,11 +25,26 @@ DECLARE_ALGORITHM_FACTORY( RawBankReadoutStatusMonitor );
 RawBankReadoutStatusMonitor::RawBankReadoutStatusMonitor( const std::string& name,
                                                           ISvcLocator* pSvcLocator)
   : GaudiHistoAlg( name , pSvcLocator )
-    ,m_counter(0)
 {
 
   declareProperty( "bankNames", m_bankTypes );
   declareProperty( "NumberOfSources", m_nSources=-1 );// Number Of Source in the StatusMap
+  declareProperty( "axisLabels", m_labels );
+  declareProperty( "xLabelOptions", m_lab ="" );
+
+  // default labelling
+  m_degree = 0;
+  unsigned int word = 1;
+  m_labels.push_back( "Counter" );
+  while( word != LHCb::RawBankReadoutStatus::Unknown ){
+    LHCb::RawBankReadoutStatus::Status stat = (LHCb::RawBankReadoutStatus::Status) word;
+    std::stringstream slabel("");
+    slabel << stat ;
+    m_labels.push_back( slabel.str() );
+    m_degree++;
+    word *= 2;
+  }
+  
 
   setHistoDir( name );
 }
@@ -47,23 +62,13 @@ StatusCode RawBankReadoutStatusMonitor::initialize() {
 
   debug() << "==> Initialize" << endmsg;
 
-
-
-  m_degree = 0;
-
-  int word = LHCb::RawBankReadoutStatus::Unknown;
-  int k=1;
-  int i=0;
-  while( word != 0 ){
-    LHCb::RawBankReadoutStatus::Status stat = (LHCb::RawBankReadoutStatus::Status) k;
-    m_stat << i << "=" << stat << "/";
-    m_degree++;
-    word = word >> 1;
-    k *= 2;
-    i++;
+  m_first = true;
+  info() << "---- Histo bins label : " << endreq  ;
+  int k = -1;
+  for( std::vector<std::string>::iterator i = m_labels.begin() ; i != m_labels.end() ; i++){
+    info() <<  " | " << k << " = '" <<  *i  <<"' | " << endreq;
+    k++;
   }
-  info()  << "RawBankReadoutStatus meaning : " << m_stat.str() << endreq;
-
   return StatusCode::SUCCESS;
 }
 
@@ -87,7 +92,10 @@ StatusCode RawBankReadoutStatusMonitor::execute() {
       base << typeName << "/" ;    
       std::stringstream tit1D("");
       tit1D << "Status summary for " << typeName << " bank " ;
-      plot1D( LHCb::RawBankReadoutStatus::MissingStatus , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree , m_degree+1);
+      AIDA::IHistogram1D* histo1D =
+        plot1D( LHCb::RawBankReadoutStatus::MissingStatus , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree, m_degree+1);
+      plot1D( -1 , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree, m_degree+1);
+      setLabels1D( histo1D  );
     }
     return StatusCode::SUCCESS;
   }
@@ -100,22 +108,21 @@ StatusCode RawBankReadoutStatusMonitor::execute() {
       base << typeName << "/" ;    
       std::stringstream tit1D("");
       tit1D << "Status summary for " << typeName << " bank " ;
-      plot1D( LHCb::RawBankReadoutStatus::MissingStatus , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree , m_degree+1);
+      AIDA::IHistogram1D* histo1D =
+        plot1D( LHCb::RawBankReadoutStatus::MissingStatus , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree, m_degree+1);
+      plot1D( -1 , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree, m_degree+1);
+      setLabels1D( histo1D  );
     }
     return StatusCode::SUCCESS;
   }
   
-  m_counter++;
   
   
-  for(LHCb::RawBankReadoutStatuss::iterator it = statuss->begin() ; it != statuss->end() ; ++it ){
-    
+  for(LHCb::RawBankReadoutStatuss::iterator it = statuss->begin() ; it != statuss->end() ; ++it ){    
     LHCb::RawBankReadoutStatus* status = *it;    
     std::map< int, long > statusMap = status->statusMap();
-    
     LHCb::RawBank::BankType type = status->key();
     std::string typeName = LHCb::RawBank::typeName( type );
-    
     bool ok = ( 0 == m_bankTypes.size() ) ? true :  false;
     for(std::vector<std::string>::iterator inam = m_bankTypes.begin() ; inam != m_bankTypes.end() ; ++inam){
       if( *inam == typeName){
@@ -123,24 +130,16 @@ StatusCode RawBankReadoutStatusMonitor::execute() {
         break;
       }  
     }
-    if( !ok ){
-      std::stringstream base("");
-      base << typeName << "/" ;    
-      std::stringstream tit1D("");
-      tit1D << "Status summary for " << typeName << " bank " ;
-      plot1D( LHCb::RawBankReadoutStatus::MissingStatus , base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree , m_degree+1);
-      continue;
-    }
-    
-        
-    
+    if( !ok )continue;
     
     std::stringstream base("");
     base << typeName << "/" ;    
     // 1D summary histogram for the whole bank
     std::stringstream tit1D("");
     tit1D << "Status summary for " << typeName << " bank " ;
-    plot1D( -1., base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree , m_degree+1);
+    AIDA::IHistogram1D* histo1D =
+      plot1D( -1., base.str() + "/1",  tit1D.str() ,  -1, (double) m_degree , m_degree+1);
+    setLabels1D( histo1D  );
     debug() << "Status " << status->status() << endreq;
     for(int i = 0; i < m_degree ; ++i ){
       int word = status->status() >> i;
@@ -167,17 +166,18 @@ StatusCode RawBankReadoutStatusMonitor::execute() {
 
     for( std::map< int, long >::iterator imap = statusMap.begin() ; imap != statusMap.end() ; ++imap ){
       double source = (double)(*imap).first;
-      plot2D(  source, -1.,  base.str() + "/2",  
-               tit2D.str() , min, max, -1., (double) m_degree+1. , bin , m_degree+2 );
+      AIDA::IHistogram2D* histo2D =
+        plot2D(  source, -1.,  base.str() + "/2",  
+                 tit2D.str() , min, max, -1., (double) m_degree , bin , m_degree+1 );
+      setLabels2D( histo2D );
       for(int i = 0; i < m_degree ; ++i ){
         int word = (*imap).second >> i;
         if( ( 0x1 & word) )plot2D(  source , (double) i , base.str() + "/2",  
                                     tit2D.str() , min, max, -1., (double) m_degree , bin , m_degree+1 );
-
-
       }
     }
   }
+  if(m_first)m_first=false;
   return StatusCode::SUCCESS;
 }
   
@@ -192,3 +192,24 @@ StatusCode RawBankReadoutStatusMonitor::finalize() {
 }
 
 //=============================================================================
+
+
+void RawBankReadoutStatusMonitor::setLabels1D( AIDA::IHistogram1D* histo ){  
+  TH1D* th1d = Gaudi::Utils::Aida2ROOT::aida2root( histo );
+  int k = 1;
+  TAxis* xAxis = th1d->GetXaxis();
+  for ( std::vector<std::string>::iterator i  = m_labels.begin() ; i != m_labels.end() ; i++) {
+    xAxis->SetBinLabel( k , (*i).c_str() );
+    k++;
+  }
+  xAxis->LabelsOption( m_lab.c_str() );
+}
+void RawBankReadoutStatusMonitor::setLabels2D( AIDA::IHistogram2D* histo ){  
+  TH2D* th2d = Gaudi::Utils::Aida2ROOT::aida2root( histo );
+  int k = 1;
+  TAxis* yAxis = th2d->GetYaxis();
+  for ( std::vector<std::string>::iterator i  = m_labels.begin() ; i != m_labels.end() ; i++) {
+    yAxis->SetBinLabel( k , (*i).c_str() );
+    k++;
+  }
+}
--- LHCb/Event/DAQEvent/src/component/RawBankReadoutStatusMonitor.h
+++ LHCb/Event/DAQEvent/src/component/RawBankReadoutStatusMonitor.h
@@ -1,11 +1,14 @@
-// $Id: RawBankReadoutStatusMonitor.h,v 1.2 2008-01-23 23:43:05 odescham Exp $
+// $Id: RawBankReadoutStatusMonitor.h,v 1.3 2009-03-05 15:36:36 odescham Exp $
 #ifndef COMPONENT_RAWBANKREADOUTSTATUSMONITOR_H 
 #define COMPONENT_RAWBANKREADOUTSTATUSMONITOR_H 1
 
 // Include files
 // from Gaudi
 #include "GaudiAlg/GaudiHistoAlg.h"
-
+// root
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TAxis.h>
 
 /** @class RawBankReadoutStatusMonitor RawBankReadoutStatusMonitor.h component/RawBankReadoutStatusMonitor.h
  *  
@@ -27,12 +30,13 @@ public:
 protected:
 
 private:
-
+  void setLabels1D( AIDA::IHistogram1D* histo1D );
+  void setLabels2D( AIDA::IHistogram2D* histo2D );
   std::vector<std::string> m_bankTypes;
-  long m_counter;
   int m_degree;
   int m_nSources;
-   std::stringstream m_stat;
-
+  std::string m_lab;
+  std::vector<std::string> m_labels;
+  bool m_first;
 };
 #endif // COMPONENT_RAWBANKREADOUTSTATUSMONITOR_H
