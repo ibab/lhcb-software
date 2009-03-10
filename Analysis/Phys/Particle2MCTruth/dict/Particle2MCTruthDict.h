b88512ac8e53a230de1e7d76871bd2b6bb3b55e2
jpalac <jpalac@cern.ch>
2009-03-10 18:11:14 +0000

    dictionaries for IP2MCP
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75468 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 079d3ce1ba9ed92378a975b61b9605bd0e716e27

---
+++ Analysis/Phys/Particle2MCTruth/dict/Particle2MCTruthDict.h
@@ -0,0 +1,14 @@
+// $Id: Particle2MCTruthDict.h,v 1.1 2009-03-10 18:11:14 jpalac Exp $
+#ifndef DICT_PARTICLE2MCTRUTHDICT_H 
+#define DICT_PARTICLE2MCTRUTHDICT_H 1
+
+// Include files
+
+/** @file dict/Particle2MCTruthDict.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2009-03-10
+ */
+#include "P2MCP/IP2MCP.h"
+#endif // DICT_PARTICLE2MCTRUTHDICT_H
