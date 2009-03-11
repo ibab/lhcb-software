420a31517d5ef07074e16ef752fc2ea05c8020a2
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-03-11 16:03:46 +0000

    improve HltFastTrackFit.py using new feature ConfiguredFastTrackFitter.ConfiguredFitters\' ConfiguredFastFitter
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@75488 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1bb52450df36c5c3024bff4a16d57690659eaade

--- Hlt/Hlt/HltConf/python/HltConf/HltFastTrackFit.py
+++ Hlt/Hlt/HltConf/python/HltConf/HltFastTrackFit.py
@@ -1,17 +1,20 @@
+from inspect import isclass
 from Gaudi import Configuration
-from Configurables import HltTrackUpgradeTool, HltTrackFit
-from Configurables import TrackHerabExtrapolator
+from Configurables import HltTrackUpgradeTool, HltTrackFit, TrackMasterFitter
 from TrackFitter.ConfiguredFitters import ConfiguredFastFitter
 
 # why is this not automatically the case if RecoName == FitTrack ???
-def setupHltFastTrackFit( fit ) :
-    fit = Configuration.allConfigurables[fit]
+def setupHltFastTrackFit( name ) :
+    fit = Configuration.allConfigurables[name]
     if fit.getType() is not 'HltTrackUpgrade' and fit.getType() is not 'HltVertexUpgrade' :
            raise AttributeError, "The argument '%s' is of type %s, should be either HltTrackUpgrade or HltVertexUpgrade"%(fit,fit.getType())
     if fit.RecoName != 'FitTrack' : 
-           raise AttributeError, "The argument '%s' has RecoName %s, should be 'FitTrack'"%(fit,fit.RecoName)
-    for i in [ HltTrackUpgradeTool(),HltTrackFit(),ConfiguredFastFitter('Fit')] :
+           raise AttributeError, "The argument '%s' has RecoName %s, should be 'FitTrack' when calling setupFastTrackFit"%(fit,fit.RecoName)
+    for i in [ HltTrackUpgradeTool, HltTrackFit ] :
         fit.addTool( i )
-        fit = getattr( fit, i.getName().split('.')[-1] )
+        fit = getattr( fit, i.__name__ )
+    fit.addTool( TrackMasterFitter, name = 'Fit')
+    ConfiguredFastFitter( getattr(fit,'Fit') )
     # for good measure, we throw this one in as well...
+    from Configurables import TrackHerabExtrapolator
     TrackHerabExtrapolator().extrapolatorID = 4
