f9f37b364839b231d71c9324f8eadf7490435ff1
jpalac <jpalac@cern.ch>
2009-03-13 18:11:01 +0000

    . Add dicts and instantiations for P2MCP::Types::FlatTree and P2MCP::Types::FlatTree
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75518 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3e8bc1045ab0e09dd6827d66a003e32c7eb5714f

--- Analysis/Phys/Particle2MCTruth/dict/Particle2MCTruthDict.h
+++ Analysis/Phys/Particle2MCTruth/dict/Particle2MCTruthDict.h
@@ -1,4 +1,4 @@
-// $Id: Particle2MCTruthDict.h,v 1.1 2009-03-10 18:11:14 jpalac Exp $
+// $Id: Particle2MCTruthDict.h,v 1.2 2009-03-13 18:11:01 jpalac Exp $
 #ifndef DICT_PARTICLE2MCTRUTHDICT_H 
 #define DICT_PARTICLE2MCTRUTHDICT_H 1
 
@@ -11,4 +11,10 @@
  *  @date   2009-03-10
  */
 #include "P2MCP/IP2MCP.h"
+#include "P2MCP/P2MCPTypes.h"
 #endif // DICT_PARTICLE2MCTRUTHDICT_H
+
+namespace
+{
+  std::vector<LHCb::MCParticle::ConstVector> __stdvector_mcpVC;
+}
