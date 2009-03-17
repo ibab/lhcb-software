d2b0b7175ea1d53434e2860254abd6381072d2a1
barandel <barandel@cern.ch>
2009-03-17 15:40:02 +0000

    updated grantPriv.sh to Online v4r20 (and LbLogin)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99192 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3e3086ca935058f62234fc5b76a730ed64c0c12

--- Online/Online/CondDBServer/cmt/grantPriv.sh
+++ Online/Online/CondDBServer/cmt/grantPriv.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-MYSITEROOT=/sw/lib
-CMTCONFIG=slc4_ia32_gcc34
-if [ -r $MYSITEROOT/scripts/ExtCMT.sh ]; then
-    export MYSITEROOT CMTCONFIG
-    . $MYSITEROOT/scripts/ExtCMT.sh >& /dev/null
+export CMTCONFIG=slc4_ia32_gcc34
+if [ -r /sw/lib/LbLogin.sh ]; then
+    . /sw/lib/LbLogin.sh >& /dev/null
 else
     echo CMT environment not found
+    exit 1
 fi
 
 # This sets ORACLE_HOME and ORACLE_SID (needed?)
 . /sw/oracle/linux/bin/oraenv
 
-SetupProject Online v4r14 --use Online/CondDBServer >& /dev/null
+SetupProject Online v4r20 --use Online/CondDBServer >& /dev/null
 
 date >> grantPriv.log
 grantPriv.py >> grantPriv.log
