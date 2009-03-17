a4eae660b45fb14e3c4151a68149f4a3cf642da4
nchiapol <nchiapol@cern.ch>
2009-03-17 10:47:58 +0000

    added correct dependencies to requirement file finished STSummaryAnalyser
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@27640 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df3fa09c272cb9d64dda995646e8e0113315ff33

--- Lbcom/ST/STMonitors/src/STSummaryAnalyser.cpp
+++ Lbcom/ST/STMonitors/src/STSummaryAnalyser.cpp
@@ -1,4 +1,4 @@
-// $Id: STSummaryAnalyser.cpp,v 1.2 2009-03-10 13:55:07 cattanem Exp $
+// $Id: STSummaryAnalyser.cpp,v 1.3 2009-03-17 10:47:58 nchiapol Exp $
 
 // Gaudi
 #include "GaudiKernel/AlgFactory.h"
@@ -37,18 +37,21 @@ DECLARE_ALGORITHM_FACTORY( STSummaryAnalyser);
 
 STSummaryAnalyser::STSummaryAnalyser( const std::string& name, ISvcLocator* pSvcLocator ) :
   GaudiHistoAlg(name, pSvcLocator),
-  m_pcnHisto("pcnEvent"),
+  //m_pcnHisto("pcnEvent"),
   c_binIDaSynch(0.5),
-  c_binIDmissing(1.5), 
-  c_binIDcorrupted(2.5)
+  c_binIDmissing(2.5), 
+  c_binIDcorrupted(1.5),
+  c_binIDrecovered(3.5)
 {
   // constructer
   declareProperty("DetType", m_detType = "TT" );
-  declareProperty("InputData", m_dataLocation = STTELL1DataLocation::TTFull );
-  declareProperty("Summary", m_summaryLocation   = STSummaryLocation::TTSummary );
+  //declareProperty("InputData", m_dataLocation = STTELL1DataLocation::TTFull );
+  //declareProperty("Summary", m_summaryLocation   = STSummaryLocation::TTSummary );
+  declareProperty("InputData", m_summaryLocation   = STSummaryLocation::TTSummary );
   declareProperty("UseSourceID", m_useSourceID = true );
-  //declareProperty("BaseNameHisto", m_basenameHisto = "pcn");
   declareProperty("SkipEvents", m_skipEvents = -1 );
+  declareProperty("SuppressMissing", m_suppressMissing = false);
+  declareProperty("SuppressRecovered", m_suppressRecovered = false);
 }
 
 STSummaryAnalyser::~STSummaryAnalyser()
@@ -64,47 +67,20 @@ StatusCode STSummaryAnalyser::initialize()
     return Error("Failed to initialize", sc);
   }
   
-  STDetSwitch::flip(m_detType,m_dataLocation);
+  STDetSwitch::flip(m_detType,m_summaryLocation);
+
+  // defining histogram ids and titles
+  m_pcnHisto[c_id]         = "pcn";
+  m_pcnHisto[c_title]      = "PCN distribution";
+  m_errorHisto[c_id]       = "errors";
+  m_errorHisto[c_title]    = "Error Info in Summary";
+  m_dataSizeHisto[c_id]    = "dataSize";
+  m_dataSizeHisto[c_title] = "Data size per event";
   
-  m_errorHisto[c_id]    = "errors";
-  m_errorHisto[c_title] = "Error Info in Summary";
-  //const Map *NumberToSourceIDMap;
-//  if (m_detType == "TT") {
-//    m_TELL1Mapping      = &TTSourceIDToNumberMap();						  ;
-//    NumberToSourceIDMap = &TTNumberToSourceIDMap();
-//  } else {
-//    m_TELL1Mapping      = &ITSourceIDToNumberMap();
-//    NumberToSourceIDMap = &ITNumberToSourceIDMap();
-//  }
-//  const std::map<unsigned int, unsigned int> *NumberToSourceIDMap;
-//  if (m_detType == "TT") {
-//    m_TELL1Mapping      = &TTSourceIDToNumberMap;						  ;
-//    NumberToSourceIDMap = &TTNumberToSourceIDMap;
-//  } else {
-//    m_TELL1Mapping      = &ITSourceIDToNumberMap;
-//    NumberToSourceIDMap = &ITNumberToSourceIDMap;
-//  }
-  
-//  m_basenameHisto += "_$tell";
   m_evtNumber      = 0;
   
-//  book(m_pcnHisto, 0, pipelineSize, pipelineSize);
-    
-//        std::map<unsigned int, unsigned int>::const_iterator iterMap = NumberToSourceIDMap->begin();
-//  const std::map<unsigned int, unsigned int>::const_iterator endMap  = NumberToSourceIDMap->end();
-//  for ( ; iterMap != endMap ; iterMap++ ) {
-//    int tellID = iterMap->first;
-//    if (m_useSourceID) {
-//      tellID = iterMap->second;
-//    }
-//    std::string title = m_basenameHisto + boost::lexical_cast<std::string>(tellID);
-//    book(title, 0, pipelineSize, pipelineSize);
-//  }
-
   return StatusCode::SUCCESS;
 }
-
-
 
 
 
@@ -119,11 +95,6 @@ StatusCode STSummaryAnalyser::execute()
     return StatusCode::SUCCESS;
   }
  
-  // Skip if there is no Tell1 data
-  //if (!exist<STTELL1Datas>(m_dataLocation)) {
-  //  debug() << "No data at given location" << endmsg;
-  //  return StatusCode::SUCCESS;
-  //}
   
   // Skip if there is no Tell1 data
   if (!exist<STSummary>(m_summaryLocation)) {
@@ -132,24 +103,18 @@ StatusCode STSummaryAnalyser::execute()
   }
 
   // Get the data
-  //const STTELL1Datas *data    = get<STTELL1Datas>(m_dataLocation);
   const STSummary    *summary = get<STSummary>(m_summaryLocation);
   // debug() << "Found " << data->size() << " boards." << endmsg;
 
-  //if (!(summary->pcnSynch())) {
-  //  error() << "PCN in event " << m_evtNumber << "do not agree!" << endmsg; 
-  //  return StatusCode::SUCCESS;
-  //}
   
-  //unsigned int pcn = summary->pcn();
+  /** 
+   * Filling the histograms
+   */
   
-  //IHistogram1D* hist = histo1D( m_pcnHisto ) ;
-  //if ( hist == 0 ) {
-  //  error() << "Histogram " << m_pcnHisto << " not found" << endmsg;
-  //}
-  //hist->fill(pcn);
-  plot1D(summary->pcn(), m_pcnHisto, "PCN", 0, pipelineSize, pipelineSize);
+  // PCN
+  plot1D(summary->pcn(), m_pcnHisto[c_id], m_pcnHisto[c_title], 0, pipelineSize, pipelineSize);
   
+  // Error Summary
   if (!(summary->pcnSynch())) {
     plot1D(c_binIDaSynch, m_errorHisto[c_id], m_errorHisto[c_title], 
 		    0, c_nErrorBins, c_nErrorBins );
@@ -157,42 +122,27 @@ StatusCode STSummaryAnalyser::execute()
   plot1D(c_binIDcorrupted, m_errorHisto[c_id], m_errorHisto[c_title], 
                     0, c_nErrorBins, c_nErrorBins, 
 		    (summary->corruptedBanks()).size());
-  plot1D(c_binIDmissing, m_errorHisto[c_id], m_errorHisto[c_title], 
-                    0, c_nErrorBins, c_nErrorBins, 
-		    (summary->missingBanks()).size());
+  if ( !m_suppressMissing ) {
+    plot1D(c_binIDmissing, m_errorHisto[c_id], m_errorHisto[c_title], 
+                      0, c_nErrorBins, c_nErrorBins, 
+        	      (summary->missingBanks()).size());
+  }
+  if ( !m_suppressRecovered ) {
+    plot1D(c_binIDrecovered, m_errorHisto[c_id], m_errorHisto[c_title], 
+                      0, c_nErrorBins, c_nErrorBins, 
+                      (summary->recoveredBanks()).size());
+  }
+
+  // Data Size
+  plot1D(summary->rawBufferSize()/1024, m_dataSizeHisto[c_id], m_dataSizeHisto[c_title], 
+                    0, 100 , 200); 
   
 
+  
   return StatusCode::SUCCESS;
 }
 
 StatusCode STSummaryAnalyser::finalize()
 {
-  //printHistos();
-  // Update al histograms at the end
-  //DataMap::const_iterator iTell = m_meanMap.begin();
-  //for( ; iTell != m_meanMap.end(); ++iTell ) { 
-  //  updateNoiseHistogram( (*iTell).first );
-  //} 
-
   return StatusCode::SUCCESS;
 }
-
-//void STSummaryAnalyser::fill(int tellID)
-//{
-  // Create a title for the histogram
-//  std::string title = m_basenameHisto + boost::lexical_cast<std::string>(tellID);
-
-//  IHistogram1D* hist = histo1D( title ) ;
-//  if ( hist == 0 ) {
-//    error() << "Histogram " << title << " not found" << endmsg;
-//    return;
-//  }
-//  hist->reset();
-
-  // Loop over strips in tell1
-//  for (unsigned int strip = 0u; strip < nStripsPerBoard; ++strip) {
-//    double rms = sqrt( m_meanSqMap[tellID][strip] 
-//                       - gsl_pow_2(m_meanMap[tellID][strip]));
-//    hist->fill(strip, rms);
-//  }
-//}
--- Lbcom/ST/STMonitors/src/STSummaryAnalyser.h
+++ Lbcom/ST/STMonitors/src/STSummaryAnalyser.h
@@ -1,4 +1,4 @@
-// $Id: STSummaryAnalyser.h,v 1.2 2009-03-10 13:55:07 cattanem Exp $
+// $Id: STSummaryAnalyser.h,v 1.3 2009-03-17 10:47:58 nchiapol Exp $
 #ifndef STSummaryAnalyser_H
 #define STSummaryAnalyser_H 1
 
@@ -36,26 +36,23 @@ private:
   //void updateNoiseHistogram(int tell1ID);
 
   //const std::string   m_basenameNoiseHisto; 
-  const std::string                m_pcnHisto;
-  std::string                    m_errorHisto[2];
-  
+  std::string         m_pcnHisto[2];
+  std::string         m_errorHisto[2];
+  std::string	      m_dataSizeHisto[2];
+
   static const int    c_id             = 0;
   static const int    c_title          = 1;
 
-  static const int    c_nErrorBins     = 3;
+  static const int    c_nErrorBins     = 4;
   const double c_binIDaSynch;
   const double c_binIDmissing; 
   const double c_binIDcorrupted;
+  const double c_binIDrecovered;
 
   /// Tell1 mapping from tell1 name to source ID.
   const std::map<unsigned int, unsigned int>* m_TELL1Mapping;
   //const STBoardMapping::Map *m_TELL1Mapping;
   
-  typedef std::map<int, std::vector<double> > DataMap;  
-  DataMap m_meanMap;            ///< Internal map for the pedestals
-  DataMap m_meanSqMap;          ///< Internal map of the pedestal^2
-  std::map<int, int> m_nEvents; ///< Internal map of number of events per tell1
-
   int                 m_evtNumber;
   
   // jobOptions
@@ -64,7 +61,7 @@ private:
   std::string m_detType;
 
   /// Location in the Event Data Store with the ADC values
-  std::string m_dataLocation;
+  //std::string m_dataLocation;
 
   /// Locatuib ub tge Event Data Store with the Summary Info
   std::string m_summaryLocation;
@@ -73,27 +70,15 @@ private:
   /// the tell1 name.
   bool m_useSourceID;
 
-  /// String used as a base for the histogram's name (_$tell + number will be 
-  /// added to the name)
-  /// if possible this might get replaced with the name of the instance running 
-  /// or removed completely
-  /// std::string m_basenameHisto; 
-
-  /// Period of the an exponential moving average.
-  /// Set to -1 to have a cumulative average.
-  //int m_followingPeriod;
-
-  /// Rate at which the histograms are updated (in number of events).
-  /// Set to -1 to update only at the end.
-  //int m_updateRate;
-  
-  /// Rate at which the counters for histograms are reset (in number of events).
-  /// Set to -1 to do no reset (default).
-  //int m_resetRate;
-  
   /// Number of events to be skipped. Useful when running over
   /// common-mode-subtracted data where the pedestals have not been calculated. 
   int m_skipEvents;
+
+  /// When set to true: do not fill the bin for missing banks 
+  bool m_suppressMissing;
+  
+  /// When set to true: do not fill the bin for recovered banks 
+  bool m_suppressRecovered;
 
 };
 
