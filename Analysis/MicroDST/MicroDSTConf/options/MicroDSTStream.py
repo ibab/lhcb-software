3bbeaed4560379bd00e5b2fa912686ad7d231e77
jpalac <jpalac@cern.ch>
2009-03-20 16:27:18 +0000

    . Remove MicroDSTStream from ApplicationMgr().OutStream.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75571 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 429b9fa59ac13f0eaa11bbf3516ed16a31b018c0

--- Analysis/MicroDST/MicroDSTConf/options/MicroDSTStream.py
+++ Analysis/MicroDST/MicroDSTConf/options/MicroDSTStream.py
@@ -1,5 +1,5 @@
 """
-$Id: MicroDSTStream.py,v 1.1.1.1 2008-11-12 12:33:48 jpalac Exp $
+$Id: MicroDSTStream.py,v 1.2 2009-03-20 16:27:18 jpalac Exp $
 Configure the MicroDST OutputStream and add it to the ApplicationMgr
 @author Juan Palacios juan.palacios@nikhef.nl
 """
@@ -9,5 +9,5 @@ from Configurables import OutputStream
 MicroDSTStream=OutputStream('MicroDSTStream')
 MicroDSTStream.OptItemList = ["/Event/microDST#99"]
 MicroDSTStream.Output = "DATAFILE='MicroDST.dst' TYP='POOL_ROOTTREE' OPT='REC'";
-ApplicationMgr().OutStream.append(MicroDSTStream)
+
 #
