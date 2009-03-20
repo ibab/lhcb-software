ec3c0e5db2787dcaa8ba1f2d83b459ee1ed5831f
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-03-20 08:49:56 +0000

    no Tuple
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@75548 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eec84768fdece6c3054faf3e96404e7f8a854976

--- Hlt/Hlt/HltSelChecker/options/DVTestStatistics.py
+++ Hlt/Hlt/HltSelChecker/options/DVTestStatistics.py
@@ -1,5 +1,3 @@
-
-
 ### @file 
  #  Test file for HLT statistics
  #
@@ -33,22 +31,22 @@ importOptions( "$HLTSELCHECKERROOT/options/Hlt2EventTuple.py" )
 ###
  # Tuple
 ###
-from Configurables import PhysDesktop, DecayTreeTuple
-importOptions( "$HLTSELCHECKERROOT/options/Hlt2DecayTreeTuple.py")
-DecayTreeTuple("Hlt2DecayTreeTuple").addTool(PhysDesktop)
-DecayTreeTuple("Hlt2DecayTreeTuple").PhysDesktop.InputLocations = ["Hlt2SelBu2LLKSignal"]
-DecayTreeTuple("Hlt2DecayTreeTuple").Decay = "[B+ -> (^J/psi(1S) => ^e+ ^e-) ^K+]cc"
+#from Configurables import PhysDesktop, DecayTreeTuple
+#importOptions( "$HLTSELCHECKERROOT/options/Hlt2DecayTreeTuple.py")
+#DecayTreeTuple("Hlt2DecayTreeTuple").addTool(PhysDesktop)
+#DecayTreeTuple("Hlt2DecayTreeTuple").PhysDesktop.InputLocations = ["Hlt2SelBu2LLKSignal"]
+#DecayTreeTuple("Hlt2DecayTreeTuple").Decay = "[B+ -> (^J/psi(1S) => ^e+ ^e-) ^K+]cc"
 # save tuple
 #
 # Configuration
 #
 from Configurables import DaVinci
-DaVinci().EvtMax = 10000
+DaVinci().EvtMax = 1000
 DaVinci().HltType = "Hlt1+Hlt2"                # Both Hlt levels
-DaVinci().Hlt2Requires = 'L0'                  # Ignore Hlt1 in 2
+DaVinci().Hlt2Requires = 'L0+Hlt1'                  # Ignore Hlt1 in 2
 DaVinci().ReplaceL0BanksWithEmulated = False   # Redo L0
 # DaVinci().DataType = "DC06" 
 DaVinci().Simulation = True 
 DaVinci().TupleFile =  "Hlt12-StatsTuple.root"
-DaVinci().HistogramFile = "DVHlt2stats.root"
+# DaVinci().HistogramFile = "DVHlt2stats.root"
 DaVinci().MoniSequence += [ moni, statseq, DecayTreeTuple("Hlt2DecayTreeTuple") ]
