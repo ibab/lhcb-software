5f0217433bd9244986e7e163fd6b80dc3b7cb32a
jpalac <jpalac@cern.ch>
2009-03-24 17:26:08 +0000

    . Use new configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75639 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9b3780c877d922efbd244d7bca965ccc9b9ee005

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
@@ -1,17 +1,20 @@
 from Gaudi.Configuration import *
+from DaVinci.Configuration import DaVinci
 from MicroDSTExample.Configuration import PhysMicroDST
-from Configurables import ApplicationMgr
 
 importOptions("$STDOPTS/PreloadUnits.opts")
 importOptions( "$CCBARROOT/options/DoDC06SelBs2Jpsi2MuMuPhi2KK_lifetime_unbiased.opts")
 conf = PhysMicroDST()
-conf.EvtMax = 1000
-conf.MicroDSTFile = "MyTestMDST_MC_testSeq2.dst"
+conf.MicroDSTFile = "MyTestMDST_MC_newConf3.dst"
 conf.MicroDSTSelectionAlg = "DC06selBs2JpsiPhi_unbiased"
-conf.UserAlgorithms = [GaudiSequencer("SeqDC06selBs2JpsiPhi")]
 conf.CopyMCTruth = True
 conf.CopyReFittedPVs = True
-conf.Input =  [
+
+dv = DaVinci()
+dv.EvtMax = 250
+seqMicroDST = conf.seqMicroDST()
+dv.UserAlgorithms = [GaudiSequencer("SeqDC06selBs2JpsiPhi")]
+dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
@@ -20,4 +23,3 @@ conf.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000007_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
-
