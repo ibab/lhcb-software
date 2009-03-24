adde04b31db0653e0b93909df4e291658dda9c61
Matthew David Needham <mneedham@cern.ch>
2009-03-24 15:08:29 +0000

    Adding STSelectChannelIDByTruth
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@27658 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ffdec8fbc38c1f85c1465e810b921efa68eb123f

---
+++ Lbcom/ST/STTools/src/STSelectChannelIDByTruth.cpp
@@ -0,0 +1,88 @@
+// $Id: STSelectChannelIDByTruth.cpp,v 1.1 2009-03-24 15:08:29 mneedham Exp $
+ 
+// Kernel
+#include "GaudiKernel/ToolFactory.h"
+ 
+#include "STSelectChannelIDByTruth.h"
+
+#include <boost/assign/list_of.hpp>
+
+#include "Kernel/STChannelID.h"
+#include "Event/MCParticle.h"
+#include "Event/STCluster.h"
+
+#include "MCInterfaces/IMCParticleSelector.h"
+
+DECLARE_TOOL_FACTORY( STSelectChannelIDByTruth );
+ 
+STSelectChannelIDByTruth::STSelectChannelIDByTruth( const std::string& type, 
+                                    const std::string& name,
+                                    const IInterface* parent ) :
+  ST::ToolBase(type, name, parent),
+  m_links(0,0,""),
+  m_configured(false)
+{
+  declareProperty("Types",m_types = boost::assign::list_of(Spillover)(Real)); 
+  declareSTConfigProperty("InputData" , m_clusterLocation  , LHCb::STClusterLocation::TTClusters);
+  declareInterface<ISTChannelIDSelector>(this);
+}
+
+STSelectChannelIDByTruth::~STSelectChannelIDByTruth()
+{
+  //destructer
+}
+
+StatusCode STSelectChannelIDByTruth::initialize() {
+  
+  StatusCode sc = ST::ToolBase::initialize();
+  if (sc.isFailure()) return Error("Failed to initialize", sc);
+
+  m_selector = tool<IMCParticleSelector>( "MCParticleSelector", 
+                                          "Selector", this );
+
+  incSvc()->addListener(this, IncidentType::BeginEvent);
+
+  return StatusCode::SUCCESS;
+}
+
+void STSelectChannelIDByTruth::handle ( const Incident& incident )
+{
+  if ( IncidentType::BeginEvent == incident.type() ) {
+    m_configured = false; 
+  }
+}
+
+bool STSelectChannelIDByTruth::select( const LHCb::STChannelID& id ) const{
+  return (*this) (id);  
+}
+  
+bool STSelectChannelIDByTruth::operator()( const LHCb::STChannelID& id) const{
+
+  // get the linker table
+  if (!m_configured){
+    m_configured = true;
+    m_links = STLinks(evtSvc(), msgSvc(),
+                      m_clusterLocation);
+    if (m_links.notFound()) {
+      throw GaudiException("no Linker", name() ,StatusCode::FAILURE);
+    }
+  }  
+
+  // spillover or not
+  int clusType = Real;
+  const LHCb::MCParticle* aParticle = m_links.first(id);
+  if (0 != aParticle) {
+    clusType = Spillover; 
+  }
+
+  // check we want this type
+  if (std::find(m_types.begin(), m_types.end(), clusType) == m_types.end()){
+    return false;
+  }
+
+  // select using the select [if there is a link]
+  return aParticle != 0 ? m_selector->accept(aParticle) : true;
+}
+
+
+
---
+++ Lbcom/ST/STTools/src/STSelectChannelIDByTruth.h
@@ -0,0 +1,91 @@
+// $Id: STSelectChannelIDByTruth.h,v 1.1 2009-03-24 15:08:29 mneedham Exp $
+#ifndef STSelectChannelIDByTruth_H
+#define STSelectChannelIDByTruth_H 1
+
+#include <vector>
+
+// ST tool base class
+#include "Kernel/STToolBase.h"
+
+// LHCbKernel
+#include "Kernel/ISTChannelIDSelector.h"
+
+/** @class STSelectChannelIDByTruth STSelectChannelIDByTruth.h
+ *
+ *  Tool for selecting clusters using a list of service boxes
+ *
+ *  @author M.Needham
+ *  @date   3/2/2009
+ */
+
+#include "Linker/LinkedTo.h"
+
+#include "GaudiKernel/IIncidentListener.h"
+#include "GaudiKernel/IIncidentSvc.h"
+
+namespace LHCb{
+  class MCParticle;
+  class STChannelID;
+  class STCluster;
+};
+
+class IMCParticleSelector;
+
+class STSelectChannelIDByTruth: public ST::ToolBase, 
+                         virtual public ISTChannelIDSelector,  
+                         virtual public IIncidentListener {
+
+ public: 
+   
+  /// constructer
+  STSelectChannelIDByTruth( const std::string& type,
+                         const std::string& name,
+                         const IInterface* parent );
+
+  /// destructer
+  virtual ~STSelectChannelIDByTruth();
+
+  /// initialize
+  virtual StatusCode initialize();
+
+  /**  @param  cluster pointer to ST cluster object to be selected 
+  *  @return true if cluster is selected
+  */
+  virtual bool select     ( const LHCb::STChannelID& id ) const ;
+  
+  /** "select"/"preselect" method (functor interface)
+   *  @param  cluster pointer to ST cluster object to be selected 
+   *  @return true if cluster is selected
+   */
+  virtual bool operator() ( const LHCb::STChannelID& id ) const  ;
+
+
+  /** Implement the handle method for the Incident service.
+  *  This is used to inform the tool of software incidents.
+  *
+  *  @param incident The incident identifier
+  */
+  void handle( const Incident& incident );
+
+ private:
+
+  ///   default  constructor  is  private 
+  STSelectChannelIDByTruth();
+  ///   copy     constructor  is  private 
+  STSelectChannelIDByTruth (const STSelectChannelIDByTruth& );
+  ///   assignement operator  is  private 
+  STSelectChannelIDByTruth& operator= (const STSelectChannelIDByTruth& );  
+
+  enum Types {Spillover = 0 , Real = 1 };
+
+  typedef LinkedTo<LHCb::MCParticle,LHCb::STCluster> STLinks;
+  mutable STLinks m_links; 
+
+  mutable bool m_configured;
+  std::string m_clusterLocation;
+  std::vector<int> m_types;
+  IMCParticleSelector* m_selector;
+
+};
+
+#endif // STSelectChannelIDByTruth_H
