a050586a8bd715a532edd746ed815b14dac86231
jpalac <jpalac@cern.ch>
2009-03-24 11:08:47 +0000

    . Re-fit PVs and copy
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9c98f217471dbef67234cc21033847e0896ae354

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
@@ -4,13 +4,13 @@ from Configurables import ApplicationMgr
 
 importOptions("$STDOPTS/PreloadUnits.opts")
 importOptions( "$CCBARROOT/options/DoDC06SelBs2Jpsi2MuMuPhi2KK_lifetime_unbiased.opts")
-#importOptions( "$MICRODSTEXAMPLEROOT/options/DoDC06SelBs2Jpsi2MuMuPhi2KK_lifetime_unbiased.py")
 conf = PhysMicroDST()
 conf.EvtMax = 1000
 conf.MicroDSTFile = "MyTestMDST_MC_testSeq2.dst"
 conf.MicroDSTSelectionAlg = "DC06selBs2JpsiPhi_unbiased"
 conf.UserAlgorithms = [GaudiSequencer("SeqDC06selBs2JpsiPhi")]
 conf.CopyMCTruth = True
+conf.CopyReFittedPVs = True
 conf.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
