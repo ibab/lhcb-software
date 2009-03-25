d52d5ffa4ece8943ec2094b6abf33847cbf58e09
hmdegaud <hmdegaud@cern.ch>
2009-03-25 20:14:16 +0000

    added missing projects and missing default volume sizes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13609 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbConfiguration/python/LbConfiguration/Project.py
+++ LbScripts/LbConfiguration/python/LbConfiguration/Project.py
@@ -4,12 +4,14 @@
 import sys
 import os
 
+# ------------------------------------------------------------------------------------
 # list of original project names
 project_names = ["Gaudi", "LHCb", "Lbcom", "Rec", "Boole", "Brunel" , 
                  "Gauss", "Phys", "Analysis", "Hlt", "Alignment", "Moore",
                  "Online", "Euler", "Geant4", "DaVinci", "Bender", "Orwell",
-                 "Panoramix", "LbScripts", "Dirac"]
+                 "Panoramix", "LbScripts", "Dirac", "LHCbGrid", "Panoptes", "Curie"]
 
+# ------------------------------------------------------------------------------------
 
 class ProjectConfException(Exception):
     pass
@@ -130,6 +132,7 @@ class ProjectConf(ProjectBaseConf):
         if self._cmtextratags:
             rep += "CMT Extra Tags\t\t: %s\n" % ", ".join(self._cmtextratags)
         return rep
+# ------------------------------------------------------------------------------------
 
 project_list = []
 for _proj in project_names:
@@ -152,8 +155,47 @@ for _pn in project_names:
     setattr(sys.modules[__name__], _pn, getProject(_pn))
 del _pn
 
+# ------------------------------------------------------------------------------------
 
-# Extra configuration for selected projects
+# Extra configuration for selected projects (in alphabetical order)
+
+# Alignment
+Alignment.setApplicationPackage("Alignment/Escher")#IGNORE:E0602
+Alignment.setAFSVolumeName("ALIGN")#IGNORE:E0602
+Alignment.setFullSize(800000)#IGNORE:E0602
+
+# Analysis
+Analysis.setAFSVolumeName("ANALY")#IGNORE:E0602
+Analysis.setFullSize(2000000)#IGNORE:E0602
+
+# Bender
+Bender.setApplicationPackage("Phys/Bender")#IGNORE:E0602
+Bender.setFullSize(500000)#IGNORE:E0602
+
+# Boole
+Boole.setApplicationPackage("Digi/Boole")#IGNORE:E0602
+Boole.setFullSize(1500000)#IGNORE:E0602
+
+# Brunel
+Brunel.setApplicationPackage("Rec/Brunel")#IGNORE:E0602
+Brunel.setFullSize(700000)#IGNORE:E0602
+
+# Curie
+Curie.setFullSize(50000)#IGNORE:E0602
+
+# DaVinci
+DaVinci.setApplicationPackage("Phys/DaVinci")#IGNORE:E0602
+DaVinci.setAFSVolumeName("DAV")#IGNORE:E0602
+DaVinci.setFullSize(800000)#IGNORE:E0602
+
+# Dirac
+Dirac.disableSetenvAlias()#IGNORE:E0602
+Dirac.setFullSize(500000)#IGNORE:E0602
+
+# Euler
+Euler.setApplicationPackage("Trig/Euler")#IGNORE:E0602
+Euler.setFullSize(6000)#IGNORE:E0602
+
 # Gaudi
 Gaudi.setSteeringPackage("GaudiRelease") #IGNORE:E0602
 Gaudi.setApplicationPackage("GaudiExamples")#IGNORE:E0602
@@ -161,56 +203,58 @@ if os.environ.has_key("GAUDISOFT") :
     Gaudi.setReleaseArea(os.environ["GAUDISOFT"])#IGNORE:E0602
 Gaudi.setAFSVolumeRoot("Gaudi") #IGNORE:E0602
 Gaudi.setAFSLibrarianGroup("gaudi") #IGNORE:E0602
-
-# LHCb
-LHCb.setApplicationPackage("Ex")#IGNORE:E0602
-
-
-# Geant4
-Geant4.setAFSVolumeName("G4")#IGNORE:E0602
+Gaudi.setFullSize(3000000)#IGNORE:E0602
 
 #Gauss
 Gauss.addCMTExtraTags("Hijing", "Herwig", #IGNORE:E0602
                       "BcVegPy", "HidValley", "AlpGen")#IGNORE:E0602
 Gauss.setApplicationPackage("Sim/Gauss")#IGNORE:E0602
+Gauss.setFullSize(700000)#IGNORE:E0602
 
+# Geant4
+Geant4.setAFSVolumeName("G4")#IGNORE:E0602
+Geant4.setFullSize(2000000)#IGNORE:E0602
 
-# Boole
-Boole.setApplicationPackage("Digi/Boole")#IGNORE:E0602
+# Hlt
+Hlt.setFullSize(2500000)#IGNORE:E0602
 
-# Brunel
-Brunel.setApplicationPackage("Rec/Brunel")#IGNORE:E0602
+# Lbcom
+Lbcom.setFullSize(1500000)#IGNORE:E0602
 
-# Analysis
-Analysis.setAFSVolumeName("ANALY")#IGNORE:E0602
+# LbScripts
+LbScripts.setAFSVolumeName("LB")#IGNORE:E0602
+LbScripts.setFullSize(10000)#IGNORE:E0602
 
-# DaVinci
-DaVinci.setApplicationPackage("Phys/DaVinci")#IGNORE:E0602
-DaVinci.setAFSVolumeName("DAV")#IGNORE:E0602
+# LHCb
+LHCb.setApplicationPackage("Ex")#IGNORE:E0602
+LHCb.setFullSize(6000000)#IGNORE:E0602
+
+# LHCbGrid
+LHCbGrid.setFullSize(100000)#IGNORE:E0602
 
 # Moore
 Moore.setApplicationPackage("Hlt/Moore")#IGNORE:E0602
+Moore.setFullSize(700000)#IGNORE:E0602
 
-# Euler
-Euler.setApplicationPackage("Trig/Euler")#IGNORE:E0602
-
-# Alignment
-Alignment.setApplicationPackage("Alignment/Escher")#IGNORE:E0602
-Alignment.setAFSVolumeName("ALIGN")#IGNORE:E0602
+# Online
+Online.setFullSize(1500000)#IGNORE:E0602
 
 # Orwell
 Orwell.setApplicationPackage("Calib/Orwell")#IGNORE:E0602
+Orwell.setFullSize(50000)#IGNORE:E0602
+
+# Panoptes
+Panoptes.setApplicationPackage("Rich/Panoptes")#IGNORE:E0602
+Panoptes.setFullSize(50000)#IGNORE:E0602
 
 # Panoramix
 Panoramix.setApplicationPackage("Vis/Panoramix")#IGNORE:E0602
 Panoramix.setAFSVolumeName("PANORA") #IGNORE:E0602
+Panoramix.setFullSize(1000000)#IGNORE:E0602
 
-# Bender
-Bender.setApplicationPackage("Phys/Bender")#IGNORE:E0602
+# Phys
+Phys.setFullSize(2000000)#IGNORE:E0602
 
+# Rec
+Rec.setFullSize(6000000)#IGNORE:E0602
 
-#LbScripts
-LbScripts.setAFSVolumeName("LB")#IGNORE:E0602
-
-#Dirac
-Dirac.disableSetenvAlias()#IGNORE:E0602
