bf9dbae473d3149619e287f0640465ae1ff53ad5
hmdegaud <hmdegaud@cern.ch>
2009-03-25 20:07:37 +0000

    initial import
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13607 4525493e-7705-40b1-a816-d608a930855b

---
+++ LbScripts/LbLegacy/python/LbLegacy/LHCb_config.py
@@ -0,0 +1,111 @@
+""" 
+ 061213 - add HidValley and Hijing in Gauss_other_executables
+ 070111 - HLT application name is Hlt/HltApp
+ 070306 - remove AcerData
+          add ANALYSIS, ORWELL and SQLDDB
+ 070329 - increase volume size of some projects
+ 070522 - add ALIGNMENT
+ 070703 - increase the AFS volume sizes for AMD64 addition
+ 070802 - added size for Online and modified a few quota
+"""
+#-----------------------------------------------------------------------------
+
+
+data_files = {'XmlDDDB':'DBASE_Det_XmlDDDB',
+                'DecFiles':'DBASE_Gen_DecFiles',
+                'FieldMap':'DBASE_FieldMap',
+                'BooleDQ':'DBASE_DQ_BooleDQ',
+                'BrunelDQ':'DBASE_DQ_BrunelDQ',
+                'ParamFiles':'PARAM_ParamFiles',
+                'XmlConditions':'DBASE_Det_XmlConditions',
+                'SQLDDDB':'DBASE_Det_SQLDDDB',
+                'BcVegPyData':'PARAM_BcVegPyData',
+                'MCatNLOData':'PARAM_MCatNLOData',
+                'LHCbBkg':'PARAM_LHCbBkg',
+                'Geant4Files':'PARAM_Geant4Files',
+                'MIBData':'PARAM_MIBData',
+                'L0TCK': 'DBASE_TCK_L0TCK',
+                'AppConfig': 'DBASE_AppConfig'}
+
+ext_lhcb = ['LCGCMT', 'GENSER', 'OpenScientist']
+
+lcg_projects =['LCGCMT', 'SEAL','POOL','GENSER','COOL','GDML','CORAL','RELAX']
+
+lhcb_projects = {'GAUDI':'Gaudi',
+                   'LHCB':'LHCb',
+                   'LBCOM':'Lbcom',
+                   'REC':'Rec',
+                   'HLT':'Hlt',
+                   'PHYS':'Phys',
+                   'ONLINE':'Online',
+                   'ALIGNMENT':'Alignment',
+                   'MOORE':'Moore',
+                   'EULER':'Euler',
+                   'VETRA':'Vetra',
+                   'GEANT4':'Geant4',
+                   'GAUSS':'Gauss',
+                   'BOOLE':'Boole',
+                   'BRUNEL':'Brunel',
+                   'DAVINCI':'DaVinci',
+                   'BENDER':'Bender',
+                   'ANALYSIS':'Analysis',
+                   'ORWELL':'Orwell',
+                   'PANORAMIX':'Panoramix',
+                   'LBSCRIPTS':'LbScripts',
+                   'CURIE':'Curie',
+                   'PANOPTES':'Panoptes',
+                   'DIRAC':'Dirac',
+                   'LHCBGRID':'LHCbGrid'}
+
+#applications = {'GAUSS':'Sim/Gauss', 
+#                'BOOLE':'Digi/Boole', 
+#                'BRUNEL':'Rec/Brunel',
+#                'DAVINCI':'Phys/DaVinci', 
+#                'MOORE':'Hlt/Moore',
+#                'EULER':'Trg/Euler', 
+#                'ALIGNMENT':'Alignment/Escher', 
+#                'ORWELL':'Calib/Orwell', 
+#                'PANOPTES': 'Rich/Panoptes', 
+#                'PANORAMIX':'Vis/Panoramix', 
+#                'BENDER':'Phys/Bender', 
+#                'LHCB':'Ex/', 
+#                'GAUDI':'GaudiExamples'}
+
+lhcb_binary = ['slc4_ia32_gcc34_dbg','slc4_ia32_gcc34',
+               'slc4_ia32_gcc345_dbg','slc4_ia32_gcc345',
+               'slc4_amd64_gcc34_dbg','slc4_amd64_gcc34',
+               'slc3_ia32_gcc323_dbg','slc3_ia32_gcc323',
+               'win32_vc71_dbg','win32_vc71',
+               'osx104_ia32_gcc401_dbg','osx104_ia32_gcc401']
+
+#lhcb_vol_size = {'GAUDI':'3000000', 
+#                 'LHCB' :'8000000', 
+#                 'REC'  :'6000000', 
+#                 'HLT'  :'2500000', 
+#                 'PHYS' :'2000000', 
+#                 'LBCOM':'1500000', 
+#                 'ALIGNMENT':'800000',
+#                 'MOORE':'700000', 
+#                 'EULER':'6000', 
+#                 'BRUNEL':'700000', 
+#                 'GAUSS' :'700000', 
+#                 'GEANT4':'2000000',
+#                 'BOOLE' :'1500000', 
+#                 'DAVINCI':'800000', 
+#                 'BENDER':'500000', 
+#                 'ANALYSIS':'2000000', 
+#                 'ORWELL':'50000', 
+#                 'CURIE':'50000', 
+#                 'PANOPTES' : '50000',
+#                 'PANORAMIX':'1000000', 
+#                 'ONLINE':'1500000', 
+#                 'DIRAC':'500000', 
+#                 'LHCbGrid': '50000'} 
+
+lhcb_base_name = {'GAUDI' : 'LCGCMT',
+                    'LHCB'  : 'GAUDI',
+                    'BENDER': 'DAVINCI',
+                    'GEANT4': 'GAUDI'   }
+                    
+
+applications_other_executables = {'GAUSS':'BcVegPy,Herwig,HidValley,Hijing'}
---
+++ LbScripts/LbLegacy/python/LbLegacy/vol_for_releases.py
@@ -0,0 +1,56 @@
+""" Script create a new afs volume for a new project version
+ 060616 - first version
+ 060623 - prepare the script to be called in mkproject.py
+ 060925 - set python_version and day
+ 061031 - fix size
+ 070306 - add ANALYSIS
+"""
+#-----------------------------------------------------------------------------
+
+from LbConfiguration.Project import getProject
+import logging
+import sys, os, commands
+
+#-----------------------------------------------------------------------------------
+def create_volume(pname,pversion,size=0):
+    log = logging.getLogger()
+
+    projconf = getProject(pname)
+
+    if not size :
+        size = projconf.FullSize()
+
+# get user name
+    MYNAME = commands.getstatusoutput('id -u -n')[1]
+
+# project directory, afs group, volume root
+    PNAME = projconf.NAME()
+    P_DIR = projconf.ReleaseArea()
+    root  = projconf.AFSVolumeRoot()
+    group = projconf.AFSLibrarianGroup()
+
+# set the volume name
+    PN = projconf.AFSVolumeName()
+    vol_name = 'q.'+root+'.'+PN+'_'+pversion
+
+# check PNAME project existence
+    if not os.path.exists(os.path.join(P_DIR,PNAME)):
+        log.error( 'The project %s does not exist on %s' % (PNAME, P_DIR))
+        sys.exit()
+    else:
+        if os.path.exists(os.path.join(P_DIR,PNAME,PNAME+'_'+pversion)):
+            log.warning('The version %s of the project %s already exists'%(pversion, projconf.Name()))
+            return
+    PPATH = os.path.join(P_DIR,PNAME,PNAME+'_'+pversion)
+
+
+# create the new volume
+    strcmd = 'afs_admin create -u '+MYNAME+' -q '+size+' '+PPATH+' '+vol_name
+    log.info(strcmd)
+    os.system(strcmd)
+
+# give write access to librarians
+    if os.path.exists(PPATH):
+        os.system('fs sa '+PPATH+' '+group+':librarians all')
+
+    return
