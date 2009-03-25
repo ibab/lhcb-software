d385143ad8850549208cc294154c47f02fed9c29
jpalac <jpalac@cern.ch>
2009-03-25 11:08:51 +0000

    Remove obsolete line and add example on how to set the output prefix.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75648 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3d8309bb0dd162e4d194139c05592feb1bd45808

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
@@ -5,6 +5,7 @@ from MicroDSTExample.Configuration import PhysMicroDST
 importOptions("$STDOPTS/PreloadUnits.opts")
 importOptions( "$CCBARROOT/options/DoDC06SelBs2Jpsi2MuMuPhi2KK_lifetime_unbiased.opts")
 conf = PhysMicroDST()
+conf.OutputPrefix = "MicroDST"
 conf.MicroDSTFile = "MyTestMDST_MC_newConf3.dst"
 conf.MicroDSTSelectionAlg = "DC06selBs2JpsiPhi_unbiased"
 conf.CopyMCTruth = True
@@ -12,7 +13,6 @@ conf.CopyReFittedPVs = True
 
 dv = DaVinci()
 dv.EvtMax = 250
-seqMicroDST = conf.seqMicroDST()
 dv.UserAlgorithms = [GaudiSequencer("SeqDC06selBs2JpsiPhi")]
 dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
