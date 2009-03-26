361b8f1fa76458e9b36a0cbb847d04509d4de33d
jpalac <jpalac@cern.ch>
2009-03-26 08:46:01 +0000

    . Now only has simple one-to-one association methods.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64727 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ad0aac235283858c071d84bcae971b5619043b53

--- Phys/Phys/DaVinciMCKernel/Kernel/IParticle2MCAssociator.h
+++ Phys/Phys/DaVinciMCKernel/Kernel/IParticle2MCAssociator.h
@@ -1,4 +1,4 @@
-// $Id: IParticle2MCAssociator.h,v 1.6 2009-03-03 16:05:38 jpalac Exp $
+// $Id: IParticle2MCAssociator.h,v 1.7 2009-03-26 08:46:01 jpalac Exp $
 #ifndef KERNEL_IPARTICLE2MCASSOCIATOR_H 
 #define KERNEL_IPARTICLE2MCASSOCIATOR_H 1
 
@@ -8,8 +8,6 @@
 
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
-// from DaVinciMCKernel
-#include "Kernel/Particle2MCParticle.h"
 
 namespace LHCb
 {
@@ -22,9 +20,9 @@ static const InterfaceID IID_IParticle2MCAssociator ( "IParticle2MCAssociator", 
 
 /** @class IParticle2MCAssociator IParticle2MCAssociator.h Kernel/IParticle2MCAssociator.h
  *  
- *  Tool interface to generate weighted associations between one or many 
- *  LHCb::Particles and return them to the user in different formats. The
- *  user is in control of the resulting associations and the invoking interface
+ *  Tool interface to generate a single association between an 
+ *  LHCb::Particle and return them to the user. The
+ *  user is in control of the resulting association and invoking the interface
  *  methods should have no side-effects. TES access should be read-only.
  *  Interface implementations should not populate the TES with their results
  *  directly or indirectly, and should not have any runtime configuration
@@ -39,196 +37,33 @@ public:
 
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IParticle2MCAssociator; }
-
   /**
-   *
-   * Calculate the weighted associations between an LHCb::Particle and
-   * and some LHCb::MCParticles from the TES. The location handling is defined
-   * by the implementation.
+   * Calculate and return the LHCb::MCParticle associated to an
+   * LHCb::Particle. The associated LHCb::Particle is selected from a
+   * container of LHCb::MCParticles from a TES locaiton.
    * @param particle LHCb::Particle* to be associated
-   * @return Particle2MCParticle::ToVector containing sorted weighted associations
+   * @return const LHCb::MCParticle* to associated LHCb::MCParticle
    *
    * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
+   * @date   2009-03-25
    *
    **/
-  virtual Particle2MCParticle::ToVector 
-  associate(const LHCb::Particle* particle) const = 0;
-  /**
-   *
-   * Calculate the weighted associations between an LHCb::Particle and
-   * and some LHCb::MCParticles from a TES locaiton.
-   * Return the result as Particle2MCParticle::ToVector
-   * @param particle LHCb::Particle* to be associated
-   * @param mcParticleLocation TES location of LHCb::MCParticles to base the
-   * association on.
-   * @return Particle2MCParticle::ToVector containing sorted weighted associations
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/
-  virtual Particle2MCParticle::ToVector 
-  associate(const LHCb::Particle* particle,
-            const std::string& mcParticleLocation) const = 0;
-
-  /**
-   * Calculate and return the weighted associations between an 
-   * LHCb::Particle and some LHCb::MCParticles from a TES locaiton.
-   * @param particle LHCb::Particle* to be associated
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * sorted weighted relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/
-  virtual Particle2MCParticle::LightTable 
-  relatedMCPs(const LHCb::Particle*) const = 0;
-
-  /**
-   *
-   * Calculate and return the weighted associations between an 
-   * LHCb::Particle and some LHCb::MCParticles from a TES locaiton.
-   * @param particle LHCb::Particle* to be associated
-   * @param mcParticleLocation TES location of LHCb::MCParticles to base the
-   * association on.
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * sorted weighted relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/
-  virtual Particle2MCParticle::LightTable 
-  relatedMCPs(const LHCb::Particle*,
-              const std::string& mcParticleLocation) const = 0;
+  virtual const LHCb::MCParticle* 
+  relatedMCP(const LHCb::Particle*) const = 0;
 
 
-  /**
-   *
-   * Calculate and return the weighted associations between a container of
-   * LHCb::Particles and a container of LHCb::MCParticles.
-   * @param particles Container of LHCb::Particles to be associated
-   * @param mcParticles Container of LHCb::MCParticles to base the
-   * association on.
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * sorted weighted relations between the LHCb::Particles and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
- virtual Particle2MCParticle::LightTable 
-  relatedMCPs(const LHCb::Particle* particles,
-              const LHCb::MCParticle::ConstVector& mcParticles) const = 0;
+  virtual const LHCb::MCParticle*
+  relatedMCP(const LHCb::Particle*,
+             const std::string& mcParticleLocation) const = 0;
 
-  /**
-   *
-   * Calculate and return the weighted associations between a container of  
-   * LHCb::Particle and a container of LHCb::MCParticles on the TES
-   * The TES location of the LHCb::MCParticles is handled by the 
-   * implementations
-   * @param particles Container of LHCb::Particles to be associated
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::ConstVector& particles) const = 0;
+  virtual const LHCb::MCParticle*
+  relatedMCP(const LHCb::Particle* particles,
+             const LHCb::MCParticle::ConstVector& mcParticles) const = 0;
 
-  /**
-   *
-   * Calculate and return the associations between a container of
-   * LHCb::Particles and a container of LHCb::MCParticles on the TES
-   * @param particles Container of LHCb::Particles to be associated
-   * @param mcParticleLocation TES location of LHCb::MCParticles to base the
-   * association on.
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particles and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::ConstVector& particles,
-               const std::string& mcParticleLocation) const = 0;
+  virtual const LHCb::MCParticle*
+  relatedMCP(const LHCb::Particle* particles,
+             const LHCb::MCParticle::Container& mcParticles) const = 0;
 
-  /**
-   *
-   * Calculate and return the associations between a container of  
-   * LHCb::Particle and a container of LHCb::MCParticles on the TES
-   * The TES location of the LHCb::MCParticles is handled by the 
-   * implementations
-   * @param particles Container of LHCb::Particles to be associated
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::ConstVector& particles,
-               const LHCb::MCParticle::ConstVector& mcParticles) const = 0;
-
-  /**
-   *
-   * Calculate and return the associations between a container of  
-   * LHCb::Particle and a container of LHCb::MCParticles on the TES
-   * The TES location of the LHCb::MCParticles is handled by the 
-   * implementations
-   * @param particles Container of LHCb::Particles to be associated
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::Container& particles) const = 0;
-
-  /**
-   *
-   * Calculate and return the associations between a container of
-   * LHCb::Particles and a container of LHCb::MCParticles on the TES
-   * @param particles Container of LHCb::Particles to be associated
-   * @param mcParticleLocation TES location of LHCb::MCParticles to base the
-   * association on.
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particles and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::Container& particles,
-               const std::string& mcParticleLocation) const = 0;
-
-  /**
-   *
-   * Calculate and return the associations between a container of  
-   * LHCb::Particle and a container of LHCb::MCParticles on the TES
-   * The TES location of the LHCb::MCParticles is handled by the 
-   * implementations
-   * @param particles Container of LHCb::Particles to be associated
-   * @return Particle2MCParticle::LightTable Relations table containing 
-   * relations between the LHCb::Particle and the LCHb::MCParticles
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/ 
-  virtual Particle2MCParticle::LightTable 
-  associations(const LHCb::Particle::Container& particles,
-               const LHCb::MCParticle::ConstVector& mcParticles) const = 0;
-  /**
-   * Calculate the association weight between an LHCb::Particle and an
-   * LHCb::MCParticle.
-   *
-   * @author Juan Palacios juan.palacios@nikhef.nl
-   * @date   2009-02-02
-   *
-   **/
-  virtual bool 
-  isMatched(const LHCb::Particle* particle, 
-            const LHCb::MCParticle* mcParticle) const = 0;
 
 };
 #endif // KERNEL_IPARTICLE2MCASSOCIATOR_H
