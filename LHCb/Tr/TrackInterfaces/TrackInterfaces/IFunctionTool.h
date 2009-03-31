4631d815860000a9ff85dd3cf01841fbebf5f9ea
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-03-31 15:40:53 +0000

    IFunctionTool: add the unique interface IDs
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@18858 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3e45cc940ac7c052a295114ce3e10fcb7c51b137

--- LHCb/Tr/TrackInterfaces/TrackInterfaces/IFunctionTool.h
+++ LHCb/Tr/TrackInterfaces/TrackInterfaces/IFunctionTool.h
@@ -1,22 +1,25 @@
-// $Id: IFunctionTool.h,v 1.1 2008-05-22 07:43:41 cattanem Exp $
+// $Id: IFunctionTool.h,v 1.2 2009-03-31 15:40:53 ibelyaev Exp $
+// ============================================================================
 #ifndef TRACKINTERFACES_IFUNCTIONTOOL_H 
 #define TRACKINTERFACES_IFUNCTIONTOOL_H 1
-
+// ============================================================================
 // Include files
-// from STL
+// ============================================================================
+// STD & STL
+// ============================================================================
 #include <string>
-
-// from Gaudi
+// ============================================================================
+// GaudiKernel
+// ============================================================================
 #include "GaudiKernel/IAlgTool.h"
-
-//forward declarations
+// ============================================================================
+/// forward declarations
+// ============================================================================
 namespace LHCb {
   class Track;
   class RecVertex;
 };
-
-static const InterfaceID IID_IFunctionTool ( "IFunctionTool", 1, 0 );
-
+// ============================================================================
 /** @class IFunctionTool IFunctionTool.h TrackInterfaces/IFunctionTool.h
  *  
  *
@@ -24,27 +27,68 @@ static const InterfaceID IID_IFunctionTool ( "IFunctionTool", 1, 0 );
  *  @date   2007-12-08
  */
 template <class T>
-class IFunctionTool : virtual public IAlgTool {
+class IFunctionTool : virtual public IAlgTool 
+{
 public: 
-
-  // Return the interface ID
-  static const InterfaceID& interfaceID() { return IID_IFunctionTool; }
-
-
+  // ==========================================================================
+  // the main method:
   virtual double function(const T& t ) = 0;
-  
+  // ==========================================================================
+public:
+  // ==========================================================================
+  /// Return the interface ID
+  static const InterfaceID& interfaceID() 
+  { 
+    static const InterfaceID s_IID ( "IFunctionTool", 2 , 0 );
+    return s_IID ; 
+  }
+  // ==========================================================================
 protected:
-
-private:
-
+  // ==========================================================================
+  /// virtual & protected destructor
+  virtual ~IFunctionTool(){}                  // virtual & protected destructor
+  // ==========================================================================
 };
-
-class ITrackFunctionTool : public IFunctionTool<LHCb::Track> {
+// ============================================================================
+class ITrackFunctionTool 
+  : virtual public IFunctionTool<LHCb::Track> 
+{
+public:
+  // ==========================================================================
+  /// Return the interface ID
+  static const InterfaceID& interfaceID() 
+  { 
+    static const InterfaceID s_IID ( "ITrackFunctionTool", 2 , 0 );
+    return s_IID ; 
+  }
+  // ==========================================================================
+protected:
+  // ==========================================================================
+  /// virtual & protected destructor, @todo move it into *.cpp
+  virtual ~ITrackFunctionTool(){}             // virtual & protected destructor
+  // ==========================================================================
 };
-
-class IRecVertexFunctionTool : public IFunctionTool<LHCb::RecVertex> {
+// ============================================================================
+class IRecVertexFunctionTool 
+  : virtual public IFunctionTool<LHCb::RecVertex> 
+{
+public:
+  // ==========================================================================
+  /// Return the interface ID
+  static const InterfaceID& interfaceID() 
+  { 
+    static const InterfaceID s_IID ( "IRecVertexFunctionTool", 2 , 0 );
+    return s_IID ; 
+  }
+  // ==========================================================================
+protected:
+  // ==========================================================================
+  /// virtual & protected destructor, @todo move it into *.cpp
+  virtual ~IRecVertexFunctionTool(){}         // virtual & protected destructor
+  // ==========================================================================
 };
-
-
-
+// ============================================================================
+// The END 
+// ============================================================================
 #endif // TRACKINTERFACES_IFUNCTIONTOOL_H
+// ============================================================================
