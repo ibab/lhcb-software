591dab0ee3b117509d3e63be81dd717509c721d4
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-04-03 12:12:04 +0000

    move explicit ROOT code to HltBase
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@75732 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 534c2997913f0a2d3e88122c1a1987cb99ba614f

--- Hlt/Hlt/HltGlobalMonitor/src/HltGlobalMonitor.cpp
+++ Hlt/Hlt/HltGlobalMonitor/src/HltGlobalMonitor.cpp
@@ -1,4 +1,4 @@
-// $Id: HltGlobalMonitor.cpp,v 1.31 2009-03-21 08:07:14 graven Exp $
+// $Id: HltGlobalMonitor.cpp,v 1.32 2009-04-03 12:12:04 graven Exp $
 // ============================================================================
 // Include files 
 // ============================================================================
@@ -22,22 +22,21 @@
 // ============================================================================
 // GaudiUtils
 // ============================================================================
-#include "GaudiUtils/Aida2ROOT.h"
 #include "GaudiUtils/HistoStats.h"
 // ============================================================================
 // Event
 // ============================================================================
 #include "Event/HltDecReports.h"
 // ============================================================================
-// ROOT
-// ============================================================================
-#include "TH1D.h"
-#include "TH2D.h"
-// ============================================================================
 // Memory Usage
 // ============================================================================
 #include "GaudiKernel/Timing.h"
 #include "GaudiKernel/Memory.h"
+// ============================================================================
+// HLT
+// ============================================================================
+#include "HltBase/HltHistogramUtilities.h"
+using namespace Hlt::HistogramUtilities;
 // ============================================================================
 // local
 // ============================================================================
@@ -57,59 +56,6 @@ DECLARE_ALGORITHM_FACTORY( HltGlobalMonitor );
 
 // utilities
 
-namespace {
-    bool setBinLabels( TAxis* axis,  const std::vector<std::pair<unsigned,std::string> >& labels ) {
-        if (axis==0) return false;
-        unsigned nbins = axis->GetNbins(); 
-        for (std::vector<std::pair<unsigned,std::string> >::const_iterator i = labels.begin();i!=labels.end();++i ) {
-            //TODO: check bin exists... 
-            if (1+i->first <= 0 ||  1+i->first > nbins ) return false;
-            // Argh... ROOT bins start counting at '1' instead of '0'
-            axis -> SetBinLabel(1 + i->first  ,i->second.c_str() );
-        }
-        return true;
-    }
-
-    bool setBinLabels( AIDA::IHistogram1D* hist, const std::vector<std::pair<unsigned,std::string> >& labels ) {
-        if (hist==0) return false;
-        TH1D *h1d = Gaudi::Utils::Aida2ROOT::aida2root( hist );  
-        if (h1d==0) return false;
-        return setBinLabels(  h1d->GetXaxis(), labels );
-    }
-
-    bool setBinLabels( AIDA::IHistogram1D* hist, const std::vector<std::string>& labels ) {
-        std::vector<std::pair<unsigned,std::string> > l;
-        for (unsigned i = 0;i<labels.size();++i) {
-            l.push_back(std::make_pair( i , labels[i] ) );
-        }
-        return setBinLabels(hist,l);
-    }
-
-    bool setBinLabels( AIDA::IHistogram2D* hist, const std::vector<std::string>& xlabels,
-                                                 const std::vector<std::string>& ylabels) {
-        if (hist==0) return false;
-        std::vector<std::pair<unsigned,std::string> > lx;
-        for (unsigned i = 0;i<xlabels.size();++i) {
-            lx.push_back(std::make_pair( i , xlabels[i] ) );
-        }
-        std::vector<std::pair<unsigned,std::string> > ly;
-        for (unsigned i = 0;i<ylabels.size();++i) {
-            ly.push_back(std::make_pair( i , ylabels[i] ) );
-        }
-        TH2D *h2d = Gaudi::Utils::Aida2ROOT::aida2root( hist );  
-        if (h2d==0) return false;
-        return setBinLabels(  h2d->GetXaxis(), lx) && setBinLabels( h2d->GetYaxis(), ly );
-    }
-
-  bool setAxisLabels( AIDA::IHistogram1D* hist, const std::string & xAxis,const std::string & yAxis  ) {
-    if (hist==0) return false;
-    TH1D *h1d = Gaudi::Utils::Aida2ROOT::aida2root( hist );  
-    if (h1d==0) return false;
-    h1d->SetXTitle(xAxis.c_str());
-    h1d->SetYTitle(yAxis.c_str());
-    return true;
-  }
-};
 
 //=============================================================================
 // Standard constructor, initializes variables
@@ -296,23 +242,16 @@ void HltGlobalMonitor::monitorL0DU(const LHCb::ODIN*,
     fill( m_L0Input  , i->second->id(), acc );
   }
   
-  //TODO: only do this once...
-  //labels  
-  if ( 0 != m_L0Input )
-  {
-    TH1D *ho = Gaudi::Utils::Aida2ROOT::aida2root( m_L0Input );
-    if ( 0 != ho) {
-      TAxis* axiso = ho->GetXaxis() ;
-      if( 0 != axiso) 
-      {
+  //TODO: only do this _once_...
+  if ( 0 != m_L0Input ) {
+        std::vector< std::pair<unsigned, std::string> > labels;
         for ( unsigned ibin = 0; ibin < repsL.size() ; ++ibin ) {
           debug() << "ibin = " << ibin << " repsL[ibin].first = " << repsL[ibin].first <<
             " repsL[ibin].second = " << repsL[ibin].second << endreq;
-          axiso-> SetBinLabel ((repsL[ibin].second)+1 , repsL[ibin].first.c_str() );
+          labels.push_back(std::make_pair( repsL[ibin].second , repsL[ibin].first));
         }
-      } 
-    } 
-  }
+        setBinLabels( m_L0Input, labels );
+   }
 };
 
 
