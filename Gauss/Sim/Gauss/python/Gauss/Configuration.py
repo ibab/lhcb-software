b11f4aa1e8be40f53cf6ff2a7664f96457faa170
Gloria Corti <gloria.corti@cern.ch>
2009-04-07 18:01:47 +0000

    add spillover functions and to define output stream
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94155 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 15e7646f019465ed2f498354439371a5ff17d43b

--- Gauss/Sim/Gauss/python/Gauss/Configuration.py
+++ Gauss/Sim/Gauss/python/Gauss/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for Gauss
 """
-__version__ = "$Id: Configuration.py,v 1.7 2009-02-10 16:41:36 gcorti Exp $"
+__version__ = "$Id: Configuration.py,v 1.8 2009-04-07 18:01:47 gcorti Exp $"
 __author__  = "Gloria Corti <Gloria.Corti@cern.ch>"
 
 from Gaudi.Configuration import *
@@ -9,15 +9,46 @@ import GaudiKernel.ProcessJobOptions
 from GaudiKernel import SystemOfUnits
 from Configurables import ( CondDBCnvSvc, EventClockSvc, FakeEventTime,
                             CondDBEntityResolver )
+from Configurables import ( RootHistCnv__PersSvc,
+                            HepRndm__Engine_CLHEP__RanluxEngine_ ) 
 from Configurables import ( GenInit, Generation, MinimumBias, Inclusive,
                             SignalPlain, SignalRepeatedHadronization,
                             SignalForcedFragmentation, StandAloneDecayTool,
                             Special,
                             PythiaProduction, HijingProduction,
                             CollidingBeams, FixedTarget,
-                            BeamSpotSmearVertex, FlatZSmearVertex )
-from Configurables import ( GiGaGeo, GiGaInputStream )
+                            BeamSpotSmearVertex, FlatZSmearVertex,
+                            EvtGenDecay,
+                            FixedLuminosity )
+from Configurables import ( SimInit, GiGaGeo, GiGaInputStream, GiGa,
+                            GiGaDataStoreAlgorithm,
+                            GiGaPhysListModular, GiGaRunActionSequence,
+                            TrCutsRunAction, GiGaRunActionCommand,
+                            GiGaEventActionSequence, GiGaMagFieldGlobal,
+                            GiGaTrackActionSequence, GaussPostTrackAction,
+                            GiGaStepActionSequence, SimulationSvc,
+                            GiGaFieldMgr, GiGaRunManager, GiGaSetSimAttributes,
+                            GiGaPhysConstructorOp, 
+                            SpdPrsSensDet, EcalSensDet, HcalSensDet,
+                            GaussSensPlaneDet )
+from Configurables import ( GenerationToSimulation, GiGaFlushAlgorithm,
+                            GiGaCheckEventStatus, SimulationToMCTruth,
+                            GiGaGetEventAlg, GiGaGetHitsAlg,
+                            GetTrackerHitsAlg, GetCaloHitsAlg, 
+                            GetMCRichHitsAlg, GetMCRichOpticalPhotonsAlg,
+                            GetMCRichSegmentsAlg, GetMCRichTracksAlg,
+                            Rich__MC__MCPartToMCRichTrackAlg,
+                            Rich__MC__MCRichHitToMCRichOpPhotAlg)
+from Configurables import ( GenMonitorAlg, MuonHitChecker, MCTruthMonitor,
+                            VeloGaussMoni, MCHitMonitor, MCCaloMonitor,
+                            DumpHepMC )
+from Configurables import ( PackMCParticle, PackMCVertex,
+                            UnpackMCParticle, UnpackMCVertex,
+                            CompareMCParticle, CompareMCVertex )
+
+
 from Configurables import LHCbApp
+
 
 ##
 ## Set value of crossing angle for all generation methods 
@@ -60,4 +91,89 @@ def setCrossingAngle(angle):
     Generation().StandAloneDecayTool.PythiaProduction.addTool(CollidingBeams,name="CollidingBeams")
     Generation().StandAloneDecayTool.PythiaProduction.CollidingBeams.HorizontalCrossingAngle = angle
 
+
 ## end setCrossingAngle
+
+##
+## Helper functions for spill-over
+def setTrackersHitsProperties( alg , det , slot , dd ):
+    if slot != '' : 
+	slot = slot + '/'
+    alg.MCHitsLocation = '/Event/' + slot + 'MC/' + det + '/Hits'
+    if det == 'PuVeto':
+	det = 'VeloPu'
+    alg.CollectionName = det + 'SDet/Hits'
+    alg.Detectors = ['/dd/Structure/LHCb/'+dd]
+    
+def tapeLocation( slot , root , item ):
+    if slot != '':
+	return '/Event/' + slot + '/' + root + '/' + item + '#1'
+    return '/Event/' + root + '/' + item + '#1'
+
+def slot_( slot ):
+    if slot != '':
+	return slot + '/'
+    return slot
+
+
+##
+## Function to define what to write on output
+## takes as input list of spill over slots ('' for main event)
+def setOutputs(spillOverSlots):
+
+    tape = OutputStream("GaussTape" , Preload = False , OutputLevel = 3 )
+    ApplicationMgr().OutStream += [ tape ]
+
+    # output content
+    for slot in spillOverSlots:
+        tape.ItemList += [ tapeLocation( slot , 'Gen' , 'Header' ) , 
+                           tapeLocation( slot , 'MC' , 'Header' ) ]
+		       
+        generatorList = [ tapeLocation( slot , 'Gen' , 'Collisions' ) ,
+                          tapeLocation( slot , 'Gen' , 'HepMCEvents' ) ]
+        if slot != '':
+            tape.OptItemList += generatorList
+        else:
+            tape.ItemList += generatorList
+    
+            simulationList = [ tapeLocation( slot , 'pSim' , 'MCParticles' ) ,
+                               tapeLocation( slot , 'pSim' , 'MCVertices' ) ,
+                               tapeLocation( slot , 'MC' , 'Velo/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'PuVeto/Hits' ) , 
+                               tapeLocation( slot , 'MC' , 'TT/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'IT/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'OT/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Rich/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Spd/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Prs/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Ecal/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Hcal/Hits' ) ,
+                               tapeLocation( slot , 'MC' , 'Muon/Hits' ) ]
+    
+            if slot != '':
+                tape.OptItemList += simulationList
+            else: 
+                tape.ItemList += simulationList
+                
+                tape.OptItemList += [ tapeLocation( slot , 'MC' , 'Rich/OpticalPhotons' ) ,
+                                      tapeLocation( slot , 'MC' , 'Rich/Tracks' ) , 
+                                      tapeLocation( slot , 'MC' , 'Rich/Segments' ) ,
+                                      tapeLocation( slot , 'Link/MC' , 'Particles2MCRichTracks' ) ,
+                                      tapeLocation( slot , 'Link/MC' , 'Rich/Hits2MCRichOpticalPhotons' ) ]
+
+
+    
+    tape.RequireAlgs = [ 'Gauss' ]
+    tape.Output = "DATAFILE='PFN:Gauss.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"
+
+    FileCatalog().Catalogs = [ "xmlcatalog_file:NewCatalog.xml" ]
+
+    HistogramPersistencySvc().OutputFile = "GaussHistos.root"
+
+
+## end of output function
+
+
+    
+    
+    
