2586db4b439a1746a1fb9e3dc9b0fdf23ee40dcd
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-04-09 13:48:29 +0000

    release notes for v34r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30590 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0340f5a5492db8c30887ce3ff71431312c3d20c9

--- Brunel/BrunelSys/doc/MainPage.h
+++ Brunel/BrunelSys/doc/MainPage.h
@@ -8,16 +8,15 @@
  *
  * <hr>
  * <h2>Top level options files</h2>
- * <h4>Options for default settings</h4>
- * \li Brunel-Default.py:   default setting for real data (same as Brunel-2008.py)
+ * <h4>Options for real data settings</h4>
+ * \li Brunel-Default.py:   default setting for real data
+ * \li Brunel-2008.py:      default setting for 2008 real data 
  * \li Brunel-Cosmics.py:   default setting for cosmics data 
  * \li Brunel-Rdst.py:      default setting for real data Rdst production workflow 
  * \li Brunel-Stripping.py: default setting for real data Stripping production workflow 
- * <h4>Options for 2008 settings</h4>
- * \li Brunel-2008.py:      default setting for 2008 real data 
- * \li Brunel-2008-MC.py:   default setting for 2008 simulation 
- * \li Brunel-2008-MC-VeloOpen.py: 2008 simulation with %Velo open
- * \li Brunel-2008-MC-BOff-VeloOpen.py: 2008 simulation with %Velo open and B field off
+ * <h4>Options for MC09 settings</h4>
+ * \li Brunel-MC09-WithTruth.py: default setting for MC09 simulation, saving MC truth 
+ * \li Brunel-MC09-NoTruth.py:   default setting for MC09 simulation, not saving MC truth 
  * <h4>Options for CCRC settings</h4>
  * \li Brunel-CCRCStripping.py: setting for CCRC Stripping production workflow 
  * <h4>Options for %DC06 settings</h4>
@@ -28,9 +27,7 @@
  * <h4>Examples of input data</h4>
  * \li 2008-TED-Data.py:    example 2008 real data files (TED runs) 
  * \li 2008-Cosmic-Data.py: example 2008 cosmics data files 
- * \li 2008-Files.py:       example 2008 simulation files 
+ * \li MC09-Files.py:       example MC09 simulation files 
  * \li DC06-Files.py:       example DC06 simulation files 
- * <h4>Special options for Dirac (obsolete?)</h4>
- * \li Copy.opts: To make a copy of .dst files
 
  */
