f4cc9bf5b8cee49f79ee1777b4cee94900218018
hmdegaud <hmdegaud@cern.ch>
2009-04-14 18:53:56 +0000

    fixed small problem in file redirection.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13659 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbConfiguration/scripts/group_shell.sh
+++ LbScripts/LbConfiguration/scripts/group_shell.sh
@@ -5,6 +5,6 @@ if [ ! -e ${HOME}/.oldLHCBLoginscript ]; then
     . /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh --silent
   else
     export LB_BANNER=`mktemp -t LbLogin_banner.XXXXXXXX`
-    . /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh --quiet >! ${LB_BANNER}
+    . /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/prod/InstallArea/scripts/LbLogin.sh --quiet > ${LB_BANNER}
   fi
 fi 
