4a3e9b78c341df3e1000313dc09fa54f70bea661
Malcolm John <malcolm.john@cern.ch>
2009-04-14 11:50:04 +0000

    add: trendplotting, slope measurement and profile vs LHC bunchID
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@24946 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 64f03886c7c26065f0970fc05b4b7a9ab5bd7c5b

--- Rec/Velo/VeloRecMonitors/src/VeloLumiRegionMonitor.h
+++ Rec/Velo/VeloRecMonitors/src/VeloLumiRegionMonitor.h
@@ -1,14 +1,19 @@
-// $Id: VeloLumiRegionMonitor.h,v 1.1 2008-08-31 15:52:08 krinnert Exp $
+// $Id: VeloLumiRegionMonitor.h,v 1.2 2009-04-14 11:50:04 mjohn Exp $
 #ifndef VELORECMONITOR_VELOLUMIREGIONMONITOR_H 
 #define VELORECMONITOR_VELOLUMIREGIONMONITOR_H 1
 
-// Include files
-// -------------
+#include <deque>
+
+// from Gaudi
 #include "GaudiKernel/RndmGenerators.h"
 #include "GaudiUtils/Aida2ROOT.h"
 
+// from LHCb
+#include "Event/ODIN.h"
+
 // ROOT
-#include "TH1F.h"
+#include "TH2F.h"
+#include "TProfile.h"
 
 // local
 #include "VeloMonitorBase.h"
@@ -30,18 +35,68 @@ namespace Velo
   protected:
     
   private:
+    LHCb::ODIN* m_odin;
 
     // Monitor the VeloLumiRegion
     void monitorLumiRegion();
+    void genExampleLumiRegion();
+    void calculateSlopesInVertexList();
+    void updateDataPoints();
+
+    TH1D* hX;
+    TH1D* hY;
+    TH1D* hZ;
+    TH1D* hNtrks;
+    TH1D* hNtrksU;
+    TH1D* hX_cumu;
+    TH1D* hY_cumu;
+    TH1D* hZ_cumu;
+    TProfile* hX_prof;
+    TProfile* hY_prof;
+    TProfile* hZ_prof;
+    TH1D* hX_hist;
+    TH1D* hY_hist;
+    TH1D* hZ_hist;
+    TH1D* hXZ_hist;
+    TH1D* hYZ_hist;
 
     double d_vertexDistributionMeanX;
     double d_vertexDistributionMeanY;
     double d_vertexDistributionMeanZ;
+    double d_vertexDistributionRMSX;
+    double d_vertexDistributionRMSY;
+    double d_vertexDistributionRMSZ;
+    double d_vertexDistributionGMeanX;
+    double d_vertexDistributionGMeanY;
+    double d_vertexDistributionGMeanZ;
     double d_vertexDistributionSigmaX;
     double d_vertexDistributionSigmaY;
     double d_vertexDistributionSigmaZ;
-    
-    Rndm::Numbers m_rndm;
+    double d_vertexDistributionSlopeXZ;
+    double d_vertexDistributionSlopeYZ;
+    double d_vertexDistributionSlopeErrorXZ;
+    double d_vertexDistributionSlopeErrorYZ;
+
+    std::vector< std::pair<Gaudi::XYZPoint,int> > vertexList;
+
+    std::string s_correspondingHltLineName;
+    std::string s_vertexReportLocation;
+    double d_generatedX;
+    double d_refreshRate;
+    int i_minTracks;
+    int n_acceptableVertices;
+    Rndm::Numbers m_rndmGaus;
+    Rndm::Numbers m_rndmExpo;
+    bool b_genData;
+
+    long i_counterEx;
+    long i_counterAl;
+
+    std::deque< std::pair<double,double> > vX_hist;
+    std::deque< std::pair<double,double> > vY_hist;
+    std::deque< std::pair<double,double> > vZ_hist;
+    std::deque< std::pair<double,double> > vXZ_hist;
+    std::deque< std::pair<double,double> > vYZ_hist;
   };
 }
 
