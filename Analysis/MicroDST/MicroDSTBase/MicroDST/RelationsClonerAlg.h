7d6f44c0c8ad519baa831eaa0ef9eb4f8e54c333
jpalac <jpalac@cern.ch>
2009-04-17 20:51:50 +0000

    Use new functors
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@75999 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 934378bba99bec06ed6bc970d1beb05270e43f72

--- Analysis/MicroDST/MicroDSTBase/MicroDST/RelationsClonerAlg.h
+++ Analysis/MicroDST/MicroDSTBase/MicroDST/RelationsClonerAlg.h
@@ -1,14 +1,17 @@
-// $Id: RelationsClonerAlg.h,v 1.5 2009-04-16 12:11:06 jpalac Exp $
+// $Id: RelationsClonerAlg.h,v 1.6 2009-04-17 20:51:50 jpalac Exp $
 #ifndef MICRODST_RELATIONSCLONERALG_H 
 #define MICRODST_RELATIONSCLONERALG_H 1
 
 // Include files
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
 // from Gaudi
 #include "GaudiKernel/AlgFactory.h" 
 // From MicroDST
 #include "MicroDST/MicroDSTAlgorithm.h"
 #include "MicroDST/BindType2ClonerDef.h"
 #include "MicroDST/Defaults.h"
+#include "MicroDST/TableExtraTraits.h"
 // from Boost
 #include <boost/type_traits/remove_pointer.hpp>
 /** @class RelationsClonerAlg RelationsClonerAlg.h MicroDST/RelationsClonerAlg.h
@@ -23,157 +26,181 @@
 namespace MicroDST 
 {
 
-template <typename TABLE>
-class RelationsClonerAlg : public MicroDSTAlgorithm {
+  template <typename TABLE>
+  class RelationsClonerAlg : public MicroDSTAlgorithm {
 
-private:
-  typedef Defaults<TABLE> DEFAULTS;
-  typedef Location<TABLE> LOCATION;
-  typedef typename BindType2Cloner<TABLE>::toCloner CLONER;
+  private:
 
+    typedef Defaults<TABLE> DEFAULTS;
+    typedef Location<TABLE> LOCATION;
+    typedef typename BindType2Cloner<TABLE>::toCloner CLONER;
+    typedef boost::function< const typename TABLE::From (const typename TABLE::From )> FromCloner;
+    typedef boost::function< const typename TABLE::To (const typename TABLE::To )> ToCloner;
 
-public:
-  //===========================================================================
-  /// Standard constructor
-  RelationsClonerAlg( const std::string& name, ISvcLocator* pSvcLocator )
-    :
-    MicroDSTAlgorithm ( name , pSvcLocator ),
-    m_cloner(0),
-    m_clonerType(DEFAULTS::clonerType)
-  {
-    declareProperty("ClonerType", m_clonerType);
-  }
-  //===========================================================================
-  ~RelationsClonerAlg( ) {} ///< Destructor
-  //===========================================================================
-  StatusCode initialize() 
-  {
-    StatusCode sc = MicroDSTAlgorithm::initialize(); // must be executed first
-
-    debug() << "==> Initialize" << endmsg;
-
-    if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
-
-    if (inputTESLocation()=="")  {
-      verbose() << "changing input TES location to " 
-                << LOCATION::Default << endmsg;
-      setInputTESLocation(LOCATION::Default);
-    }
-    verbose() << "inputTESLocation() is " << inputTESLocation() << endmsg;
-
-    m_cloner = tool<CLONER>(m_clonerType, this);
-
-    if (m_cloner) {
-      debug() << "Found cloner " << m_clonerType << endmsg;
-    } else {
-      sc = Error("Failed to find cloner " + m_clonerType,
-            StatusCode::FAILURE, 10);
-    }
-
-    return sc;
-
-  }
-  //===========================================================================
-  StatusCode execute() 
-  {
-
-    debug() << "==> Execute" << endmsg;
-    verbose() << "Going to clone relations from " 
-              << inputTESLocation()
-              << " into " << fullOutputTESLocation() << endmsg;
-
-    setFilterPassed(true);
-
-    if (exist<TABLE>(inputTESLocation()) )
+  public:
+    //===========================================================================
+    /// Standard constructor
+    RelationsClonerAlg( const std::string& name, ISvcLocator* pSvcLocator )
+      :
+      MicroDSTAlgorithm ( name , pSvcLocator ),
+      m_cloner(0),
+      m_clonerType(DEFAULTS::clonerType)
     {
-      verbose() << "Retrieving relations table from " 
-                << inputTESLocation() << endmsg;
-      const TABLE* table = get<TABLE>(inputTESLocation());
-      if (table) {
-        verbose() << "found table!" << endmsg;
-        TABLE* cloneTable = tableOfClones(table);
-        verbose() << "Going to store relations table from " 
-                  << inputTESLocation()
-                  << " into " << fullOutputTESLocation() << endmsg;
+      m_fromCloner = boost::bind(&RelationsClonerAlg<TABLE>::cloneFrom, &(*this), _1);
+      m_toCloner = boost::bind(&RelationsClonerAlg<TABLE>::cloneTo, &(*this), _1);
+      declareProperty("ClonerType", m_clonerType);
+    }
+    //===========================================================================
+    ~RelationsClonerAlg( ) {} ///< Destructor
+    //===========================================================================
+    StatusCode initialize() 
+    {
+      StatusCode sc = MicroDSTAlgorithm::initialize(); // must be executed first
+
+      debug() << "==> Initialize" << endmsg;
+
+      if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+      if (inputTESLocation()=="")  {
+        verbose() << "changing input TES location to " 
+                  << LOCATION::Default << endmsg;
+        setInputTESLocation(LOCATION::Default);
+      }
+      verbose() << "inputTESLocation() is " << inputTESLocation() << endmsg;
+
+      m_cloner = tool<CLONER>(m_clonerType, this);
+
+      if (m_cloner) {
+        debug() << "Found cloner " << m_clonerType << endmsg;
+      } else {
+        sc = Error("Failed to find cloner " + m_clonerType,
+                   StatusCode::FAILURE, 10);
+      }
+
+      return sc;
+
+    }
+    //===========================================================================
+    StatusCode execute() 
+    {
+
+      debug() << "==> Execute" << endmsg;
+      verbose() << "Going to clone relations from " 
+                << inputTESLocation()
+                << " into " << fullOutputTESLocation() << endmsg;
+
+      setFilterPassed(true);
+
+      if (exist<TABLE>(inputTESLocation()) )
+      {
+        verbose() << "Retrieving relations table from " 
+                  << inputTESLocation() << endmsg;
+        const TABLE* table = get<TABLE>(inputTESLocation());
+        if (table) {
+          verbose() << "found table!" << endmsg;
+          TABLE* cloneTable = tableOfClones(table);
+          verbose() << "Going to store relations table from " 
+                    << inputTESLocation()
+                    << " into " << fullOutputTESLocation() << endmsg;
+          put( cloneTable, fullOutputTESLocation() );
+          return StatusCode::SUCCESS;
+        }
+        return StatusCode::FAILURE;
+      } else {
+        verbose() << "Found no table at " << inputTESLocation() << ". storing empty table"<< endmsg;
+        TABLE* cloneTable = new TABLE();
         put( cloneTable, fullOutputTESLocation() );
         return StatusCode::SUCCESS;
       }
-      return StatusCode::FAILURE;
-    } else {
-      verbose() << "Found no table at " << inputTESLocation() << ". storing empty table"<< endmsg;
-      TABLE* cloneTable = new TABLE();
-      put( cloneTable, fullOutputTESLocation() );
+
       return StatusCode::SUCCESS;
+
     }
+    //===========================================================================
+    StatusCode finalize() 
+    {
+      debug() << "==> Finalize" << endmsg;
+      return MicroDSTAlgorithm::finalize();
+    }
+    //===========================================================================
 
-    return StatusCode::SUCCESS;
+  private:
 
-  }
-  //===========================================================================
-  StatusCode finalize() 
-  {
-    debug() << "==> Finalize" << endmsg;
-    return MicroDSTAlgorithm::finalize();
-  }
-  //===========================================================================
-
-private:
-
-  TABLE* tableOfClones(const TABLE* table) 
-  {
     typedef typename BindType2Cloner<TABLE>::toType TO_TYPE;
-    typedef typename boost::remove_pointer<typename TABLE::To>::type _To;
     typedef typename boost::remove_pointer<typename TABLE::From>::type _From;
 
-    TABLE* cloneTable = new TABLE();
+    TABLE* tableOfClones(const TABLE* table) 
+    {
 
-    typename TABLE::Range relations = table->relations();
-    verbose() << "Found " << relations.size() 
-              << " relations" << endmsg;
+      TABLE* cloneTable = new TABLE();
 
-    for (typename TABLE::Range::const_iterator iRel = relations.begin();
-         iRel != relations.end();
-         ++iRel ) {
-      const typename TABLE::From from = iRel->from();
-      if (from) {
-        typename TABLE::Range range = table->relations(from);
-        for (typename TABLE::Range::const_iterator iRange = range.begin();
-             iRange != range.end();
-             ++iRange) {
-          verbose() << "\nFrom " << *from 
-                    << " is related to To " 
-                    << *(iRange->to()) 
-                    << " with weight " << iRange->weight() << endmsg;
+      typename TABLE::Range relations = table->relations();
+      verbose() << "Found " << relations.size() 
+                << " relations" << endmsg;
+      typename Relations::Traits<TABLE>::EntryCloner entryCloner(m_fromCloner,
+                                                                 m_toCloner);
 
-          const typename TABLE::From clonedFrom = 
-            getStoredClone< _From >(from);
-          const typename TABLE::To storedTo = 
-            getStoredClone< TO_TYPE >( iRange->to());
-          const typename TABLE::To clonedTo = (0!=storedTo) ? storedTo : 
-            (*m_cloner)( iRange->to() );
-          if (clonedFrom&&clonedTo) {
-            cloneTable->relate(clonedFrom, clonedTo, iRange->weight());
-            verbose() << "\ncloned From "    
-                      << *clonedFrom 
-                      << " is related to cloned To " 
-                      << *clonedTo 
-                      << " with weight " << iRange->weight() << endmsg;
-          } // if cloned From AND cloned To
-        } // loop on related Tos
-      } // if From* found
-    } // loop on all relations
+      for (typename TABLE::Range::const_iterator iRel = relations.begin();
+           iRel != relations.end();
+           ++iRel ) {
+        const typename TABLE::From from = iRel->from();
+        if (from) {
+          typename TABLE::Range range = table->relations(from);
+          for (typename TABLE::Range::const_iterator iRange = range.begin();
+               iRange != range.end();
+               ++iRange) {
+            //           verbose() << "\nFrom " << *from 
+            //                     << " is related to To " 
+            //                     << *(iRange->to()) 
+            //                     << " with weight " << iRange->weight() << endmsg;
+            entryCloner(*iRange, cloneTable);
+            //           const typename TABLE::From clonedFrom = 
+            //             getStoredClone< _From >(from);
+            //           const typename TABLE::To storedTo = 
+            //             getStoredClone< TO_TYPE >( iRange->to());
+            //           const typename TABLE::To clonedTo = (0!=storedTo) ? storedTo : 
+            //             (*m_cloner)( iRange->to() );
+            //           if (clonedFrom&&clonedTo) {
+            //             cloneTable->relate(clonedFrom, clonedTo, iRange->weight());
+            //             verbose() << "\ncloned From "    
+            //                       << *clonedFrom 
+            //                       << " is related to cloned To " 
+            //                       << *clonedTo 
+            //                       << " with weight " << iRange->weight() << endmsg;
+            //           } // if cloned From AND cloned To
+          } // loop on related Tos
+        } // if From* found
+      } // loop on all relations
   
-    return cloneTable;
+      return cloneTable;
 
-  }
-  //===========================================================================
+    }
 
-private:
-
-  CLONER* m_cloner;
-  std::string m_clonerType;
+  private:
+    inline const CLONER* cloner() { return m_cloner; }
+    inline const typename TABLE::From cloneFrom(const typename TABLE::From from) 
+    {
+      return getStoredClone< _From >(from);
+    }
+    inline const typename TABLE::To cloneTo(const typename TABLE::To to) 
+    {
+      const typename TABLE::To storedTo = getStoredClone< TO_TYPE >(to);
+      return (0!=storedTo) ? storedTo : (*m_cloner)( to );
+    }
   
-};
+    //===========================================================================
+  
+  private:
+
+    CLONER* m_cloner;
+    std::string m_clonerType;
+    FromCloner m_fromCloner;
+    ToCloner m_toCloner;
+
+  };
+
+
+
  
 }
 
