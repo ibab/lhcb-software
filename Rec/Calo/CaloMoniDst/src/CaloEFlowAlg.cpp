c54fbdde40d1c854679e6a2eacf86105a1e24590
Olivier Deschamps <odescham@cern.ch>
2009-04-20 15:43:44 +0000

    new algo : CaloEFlowAlg
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@40263 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8e7cbf97a63439413f94b54cd3ea039c4b5ba0f4

---
+++ Rec/Calo/CaloMoniDst/src/CaloEFlowAlg.cpp
@@ -0,0 +1,131 @@
+// $Id: CaloEFlowAlg.cpp,v 1.1 2009-04-20 15:43:44 odescham Exp $
+// Include files 
+
+// from GaudiKernel
+#include "GaudiKernel/AlgFactory.h"
+// local
+#include "CaloEFlowAlg.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : CaloEFlowAlg
+//
+// 2009-04-08 : Aurelien Martens
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( CaloEFlowAlg );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+CaloEFlowAlg::CaloEFlowAlg( const std::string& name,
+                                              ISvcLocator* pSvcLocator)
+  : CaloMoniAlg ( name, pSvcLocator ){
+  
+  declareProperty("EnergyFilterMin"    , m_eFilterMin  = 50.*Gaudi::Units::MeV );
+  declareProperty("EtFilterMin"        , m_etFilterMin = 50.*Gaudi::Units::MeV );
+  declareProperty("EnergyFilterMax"    , m_eFilterMax  = 1.*Gaudi::Units::GeV );
+  declareProperty("EtFilterMax"        , m_etFilterMax = 1.*Gaudi::Units::GeV );
+
+  if(detData()     == "Ecal" ){setInputData( LHCb::CaloDigitLocation::Ecal );}
+  else if(detData()== "Hcal" ){setInputData( LHCb::CaloDigitLocation::Hcal );}    
+  else if(detData()== "Prs"  ){setInputData( LHCb::CaloDigitLocation::Prs  );}
+  else if(detData()== "Spd"  ){setInputData( LHCb::CaloDigitLocation::Spd  );}
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+CaloEFlowAlg::~CaloEFlowAlg() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode CaloEFlowAlg::initialize() {
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  StatusCode sc = CaloMoniAlg::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;
+
+
+  if      ( "Ecal" == detData() ) {
+    m_calo = getDet<DeCalorimeter>( DeCalorimeterLocation::Ecal );  }  
+  else if ( "Hcal" == detData() ) {
+    m_calo = getDet<DeCalorimeter>( DeCalorimeterLocation::Hcal );  }
+  else if ( "Prs"  == detData() ){
+    m_calo = getDet<DeCalorimeter>( DeCalorimeterLocation::Prs  );  }  
+  else if ( "Spd"  == detData() ) {
+    m_calo = getDet<DeCalorimeter>( DeCalorimeterLocation::Spd  );
+  }  
+  else{
+    return Error( "Unknown detector name "+detData() );
+  }
+
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode CaloEFlowAlg::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  typedef const LHCb::CaloDigit::Container Digits;
+  if ( msgLevel(MSG::DEBUG) )debug() << name() << " execute " << endreq;
+  
+  // produce histos ?
+  if ( msgLevel(MSG::DEBUG) )debug() << " Producing histo " << produceHistos() << endreq;
+  if ( !produceHistos() ) return StatusCode::SUCCESS;
+  
+  // get input data
+  if( !exist<Digits>( inputData() )){
+    debug() << "no digit container found at " << inputData() << endreq;
+    return StatusCode::SUCCESS;
+  }
+  Digits* digits = get<Digits> ( inputData() );
+  if ( digits -> empty() ){
+    if ( msgLevel(MSG::DEBUG) )debug() << "No digit found in " << inputData() << endreq;
+    return StatusCode::SUCCESS;
+  }
+  
+  for( Digits::const_iterator digit = digits->begin(); digits->end() != digit ; ++digit ){
+    if ( 0 == *digit ) continue;
+    
+    const LHCb::CaloCellID  id     = (*digit)->cellID();
+    
+    const double            e      = (*digit)->e();
+    const double            et     = e * m_calo->cellSine( id );
+
+    if ( msgLevel(MSG::VERBOSE) )
+      verbose() << " before thresholds :  cellID " << id.index() << " e " << e << " et " << et << endreq;
+    
+    if( e  < m_eFilterMin )  continue;
+    if( e  > m_eFilterMax )  continue;
+    if( et < m_etFilterMin ) continue;
+    if( et > m_etFilterMax ) continue;
+
+    if ( msgLevel(MSG::VERBOSE) )verbose() << " cellID " << id.index() << " e " << e << " et " << et << endreq;
+    
+    
+    if(doHisto("1")) fillCalo2D("1", id , 1. , detData() + " digits position 2D view");
+    if(doHisto("2")) fillCalo2D("2", id , e  , detData() + " energy weighted - digits position 2D view");
+    if(doHisto("3")) fillCalo2D("3", id , et , detData() + " Et weighted - digits position 2D view");
+  
+  }
+ 
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode CaloEFlowAlg::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return CaloMoniAlg::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Rec/Calo/CaloMoniDst/src/CaloEFlowAlg.h
@@ -0,0 +1,86 @@
+// $Id: CaloEFlowAlg.h,v 1.1 2009-04-20 15:43:44 odescham Exp $
+#ifndef CALOENERGYFLOWMONITOR_H 
+#define CALOENERGYFLOWMONITOR_H 1
+
+// Include files
+// ============================================================================
+// from Gaudi
+// ============================================================================
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiAlg/GaudiHistoAlg.h"
+#include "Kernel/CaloCellID.h"
+// ============================================================================
+// Event
+// ============================================================================
+#include  "Event/CaloDigit.h"
+// ============================================================================
+// CaloDet
+// ============================================================================
+#include "CaloDet/DeCalorimeter.h"
+// ============================================================================
+// CaloUtils
+// ============================================================================
+#include  "CaloMoniAlg.h"
+// ============================================================================
+
+
+
+/** @class CaloEFlowAlg CaloEFlowAlg.h
+ *  
+ *
+ *  The algorithm for dedicated "EnergyFlow" monitoring of "CaloDigits" containers.
+ *  The algorithm produces the following histograms:
+ *   1. CaloDigit multiplicity
+ *   2. CaloDigit ocupancy 2D plot per area
+ *   3. CaloDigit energy and transverse energy 2D plot per area
+ *  The same set of histograms, but with cut on Et (or E), is produced if specified
+ *
+ *  Histograms reside in the directory @p /stat/"Name" , where
+ *  @p "Name" is the name of the algorithm
+ *
+ *  @see   CaloMoniAlg
+ *  @see GaudiHistoAlg
+ *  @see GaudiAlgorithm
+ *  @see      Algorithm
+ *  @see     IAlgorithm
+ *
+ *  @author Aurelien Martens
+ *  @date   2009-04-08
+ */
+class CaloEFlowAlg : public CaloMoniAlg{
+public:
+  
+  /** Standard constructor
+   *  @param   name        algorithm name
+   *  @param   pSvcLocator pointer to service locator
+   */
+  CaloEFlowAlg( const std::string &name, ISvcLocator *pSvcLocator );
+  /// destructor (virtual and protected)
+  virtual ~CaloEFlowAlg();
+  
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected: 
+
+private:
+  
+  /// default  construstor  is  private
+  CaloEFlowAlg();
+  /// copy     construstor  is  private
+  CaloEFlowAlg( const CaloEFlowAlg& );
+  /// assignement operator  is  private
+  CaloEFlowAlg &operator=( const CaloEFlowAlg& );
+
+  DeCalorimeter *m_calo;
+
+  
+  double m_eFilterMin;
+  double m_etFilterMin;
+  double m_eFilterMax;
+  double m_etFilterMax;  
+
+};
+#endif // CALOENERGYFLOWMONITOR_H
+
