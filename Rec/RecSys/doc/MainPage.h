81d6d88ddf292bf77321f075790a5abc3d786069
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-04-20 07:12:02 +0000

    v6r4
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@30609 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 017d68fa3c3955e304e31fe18dbf1112359046db

--- Rec/RecSys/doc/MainPage.h
+++ Rec/RecSys/doc/MainPage.h
@@ -1,9 +1,10 @@
 /** \mainpage notitle
  *  \anchor recsysdoxygenmain
  *
- * This is the code reference manual for the %LHCb reconstruction classes. See:
- * <a href="requirements-source.html"><b>RecSys constituent packages</b></a>
+ * This is the code reference manual for the %LHCb reconstruction classes.
  * <p><b>See also:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href=" http://cern.ch/lhcb-release-area/DOC/rec/"><b>Rec project Web pages</b></a>
  * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
  * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
  * \li \ref externaldocs     "Related external libraries"
@@ -11,9 +12,4 @@
  * <hr>
  * \htmlinclude new_release.notes
  * <hr>
- *
- * <b>Additional information:</b>
- * \li <a href="../release.notes"><b>Release notes history</b></a>
- * \li <a href=" http://cern.ch/lhcb-release-area/DOC/rec/"><b>Rec project Web pages</b></a><p>
-
  */
