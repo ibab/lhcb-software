c85f006e845dccbf7ec2d2e33fc5a23d5a9c20a9
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2009-04-21 19:42:21 +0000

    more fixes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@76028 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b3a0c4005c22f6c59d664e94f75f0798323bdf0f

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Topo2Body.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Topo2Body.py
@@ -11,24 +11,17 @@ from Configurables import CombineParticles, PhysDesktop
 from Hlt2SharedParticles.GoodParticles import GoodPions
 from HltConf.HltLine import bindMembers
 
-__all__ = ( 'Topo2Body2pp','Topo2Body2mm','Topo2Body2pm' )
+__all__ = ( 'Topo2Body' )
 
-Hlt2SharedTopo2Body2pp = CombineParticles("Hlt2SharedTopo2Body2pp")
-Hlt2SharedTopo2Body2pp.addTool(PhysDesktop)
-Hlt2SharedTopo2Body2pp.PhysDesktop.InputLocations = [ "Hlt2GoodPions" ]
-Hlt2SharedTopo2Body2pp.DecayDescriptor = "K*(892)0 -> pi+ pi+" 
+Hlt2SharedTopo2Body = CombineParticles("Hlt2SharedTopo2Body")
+Hlt2SharedTopo2Body.addTool(PhysDesktop)
+Hlt2SharedTopo2Body.PhysDesktop.InputLocations = [ "Hlt2GoodPions" ]
+Hlt2SharedTopo2Body.DecayDescriptors = ["K*(892)0 -> pi+ pi+", "K*(892)0 -> pi+ pi-", "K*(892)0 -> pi- pi-"] 
 
-Hlt2SharedTopo2Body2pp.DaughtersCuts = {"pi+" : "(ALL)"} 
-Hlt2SharedTopo2Body2pp.CombinationCut = "(AMAXCHILD(PT)>1.5*GeV) & (AMINDOCA('LoKi::TrgDistanceCalculator')<0.1) & AALLSAMEBPV"
-Hlt2SharedTopo2Body2pp.MotherCut = "(BPVVD>2) & (BPVVDR > 0.2)"
+Hlt2SharedTopo2Body.DaughtersCuts = {"pi+" : "(ALL)"} 
+Hlt2SharedTopo2Body.CombinationCut = "(AMAXCHILD(PT)>1.5*GeV) & (AMINDOCA('LoKi::TrgDistanceCalculator')<0.1) & AALLSAMEBPV"
+Hlt2SharedTopo2Body.MotherCut = "(BPVVD>2) & (BPVVDR > 0.2)"
 
-Topo2Body2pp = bindMembers( None, [ GoodPions, Hlt2SharedTopo2Body2pp ] )
+Topo2Body = bindMembers( None, [ GoodPions, Hlt2SharedTopo2Body ] )
 
-Hlt2SharedTopo2Body2pm = Hlt2SharedTopo2Body2pp.clone("Hlt2SharedTopo2Body2pm")
-Hlt2SharedTopo2Body2pm.DecayDescriptor = "K*(892)0 -> pi+ pi-"
-Topo2Body2pm = bindMembers( None, [ GoodPions, Hlt2SharedTopo2Body2pm ] )
-
-Hlt2SharedTopo2Body2mm = Topo2Body2pp.clone("Hlt2SharedTopo2Body2mm")
-Hlt2SharedTopo2Body2mm.DecayDescriptor = "K*(892)0 -> pi- pi-" 
-Topo2Body2mm = bindMembers( None, [ GoodPions, Hlt2SharedTopo2Body2mm ] )
 
