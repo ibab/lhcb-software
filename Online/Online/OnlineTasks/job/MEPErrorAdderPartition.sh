b62333f511f5d579dfb2367c5cd6f2633d637e50
dsvantes <dsvantes@cern.ch>
2009-04-21 15:52:00 +0000

    New job file for MEPErrorAdder for partition sum
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99352 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d220832c7632b836c1e804fe31c98070253328fc

---
+++ Online/Online/OnlineTasks/job/MEPErrorAdderPartition.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### Script for setup and starting MEPErrorAdder for partition summing. 
+### One instance started for each partition
+###
+### author David Svantesson
+
+#cd /home/online/ONLINE/Online_v4r20/Online/OnlineTasks/job/
+cd /home/dsvantes/cmtuser/Online_v4r20/Online/OnlineTasks/job/
+
+# List of partitions to start algorithm for
+if test -n "$1" ; then
+   # list given by input argument
+   PARTLIST=$*
+else
+   # starting for all partitions at the same time
+   PARTLIST=(TRG TRG_ECS ECAL OT OTA OTC MUON MUONA MUONC FEST LHCb TDET TPU TPU_ECS TT VELO VELOA VELOC IT RICH RICH1 PRS RICH2 HCAL)
+fi
+
+export HOST=$(hostname --short | awk '{ print toupper($1) }')
+
+. ./setupOnline.sh
+
+#ONLINETASKSROOT=/home/dsvantes/cmtuser/Online_v4r20/Online/OnlineTasks
+
+#START ONE INSTANCE FOR EACH PARTITION
+for PART in ${PARTLIST[@]}
+do
+	echo "Starting algorithm for" $PART
+	#SET_PARTITION read by algorithm
+	export SET_PARTITION=${PART}
+	export UTGID=${PART}_MEPRxSTAT_1
+	exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class0Task -main=${GAUDIONLINEROOT}/options/Main.opts -opt=${ONLINETASKSROOT}/options/MEPErrorAdderPartition.opts&
+done
+
+
