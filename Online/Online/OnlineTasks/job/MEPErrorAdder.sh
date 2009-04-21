14231a86bd0b01a452c63155e45ba2103e0a5256
dsvantes <dsvantes@cern.ch>
2009-04-21 15:47:38 +0000

    New job files for MEPErrorAdder
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99351 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6374b1ad2e1d8a90043aac9979746b7b62086f70

--- Online/Online/OnlineTasks/job/MEPErrorAdder.sh
+++ Online/Online/OnlineTasks/job/MEPErrorAdder.sh
@@ -1,1 +1,24 @@
-${GAUDIKERNELROOT}/../../InstallArea/$CMTCONFIG/bin/Gaudi.exe ../options/MEPErrorAdder.opts
+#!/bin/bash
+
+### Script for setup and starting MEPErrorAdder for subfarm summing
+### 
+### To be run on the subfarm machine
+###
+### author David Svantesson
+
+#cd /home/online/ONLINE/Online_v4r20/Online/OnlineTasks/job/
+cd /home/dsvantes/cmtuser/Online_v4r20/Online/OnlineTasks/job/
+
+#if test -n "$1" ; then
+#   export UTGID=$1
+#fi
+
+# HOST is read by the algorithm to know which subfarm it sums
+export HOST=$(hostname --short | awk '{ print toupper($1) }')
+UTGID=${HOST}_MEPRxSTAT_1
+
+. ./setupOnline.sh
+
+#ONLINETASKSROOT=/home/dsvantes/cmtuser/Online_v4r20/Online/OnlineTasks
+exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class0Task -main=${GAUDIONLINEROOT}/options/Main.opts -opt=${ONLINETASKSROOT}/options/MEPErrorAdder.opts
+
