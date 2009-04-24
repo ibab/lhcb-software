a839377e1807058e179da037c7a370f9f880c358
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-04-24 16:57:54 +0000

    some mods
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@76100 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8716179492f38082ca8ad57479e524ea6fd0dee2

--- DaVinci/DaVinciSys/tests/options/DVTestHlt-2008.py
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt-2008.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DVTestHlt-2008.py,v 1.4 2009-03-24 10:43:21 pkoppenb Exp $
+# $Id: DVTestHlt-2008.py,v 1.5 2009-04-24 16:57:54 pkoppenb Exp $
 #
 # Options for a typical DaVinci job
 #
@@ -15,7 +15,7 @@ from Configurables import GaudiSequencer
 # Standard configuration
 #
 from Configurables import DaVinci
-DaVinci().EvtMax = 1000                        # Number of events
+DaVinci().EvtMax = -1                          # Number of events
 DaVinci().SkipEvents = 0                       # Events to skip
 DaVinci().PrintFreq  = 10                      # Print frequency
 DaVinci().DataType = "2008"                    # Default is "DC06"
@@ -25,14 +25,10 @@ DaVinci().Simulation   = True
 # HLT
 #
 DaVinci().Hlt2Requires = 'L0'
-DaVinci().HltType = 'Hlt2'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
-DaVinci().Input   = [
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003400/0000/00003400_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+DaVinci().HltType = 'Hlt1+Hlt2'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
+EventSelector().Input   = [
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003401/0000/00003401_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/MC/2008/DST/00003401/0000/00003401_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
 
 ########################################################################
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
