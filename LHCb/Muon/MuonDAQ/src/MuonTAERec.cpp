7693ba49666c4f2e4a211fa4b44d09dcfdbca15d
Alessia Satta <asatta@cern.ch>
2009-04-28 10:10:12 +0000

    add new algo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@21836 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4c2e5cee6225cc1b3c0ddbf074f5f84dd0a88a96

---
+++ LHCb/Muon/MuonDAQ/src/MuonTAERec.cpp
@@ -0,0 +1,393 @@
+// $Id: MuonTAERec.cpp,v 1.1 2009-04-28 10:10:12 asatta Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+#include "Event/MuonCoord.h"
+// local
+#include "MuonTAERec.h"
+using namespace LHCb;
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : MuonTAERec
+//
+// 2008-10-28 : Alessia Satta
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( MuonTAERec );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+MuonTAERec::MuonTAERec( const std::string& name,
+                        ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator ),
+   m_NStation(0), m_NRegion(0)
+{
+ declareProperty("IgnoreStrips" , m_ignoreUncrossedStrips=true) ;
+
+  m_ignoreExecution=false;
+}
+
+
+//=============================================================================
+// Destructor
+//=============================================================================
+MuonTAERec::~MuonTAERec() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode MuonTAERec::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  m_muonDetector=getDet<DeMuonDetector>
+    (DeMuonLocation::Default);
+  
+  // TDS path to the Muon system  is of the form /dd/Structure/LHCb/Muon
+  
+  if(m_muonDetector==NULL){
+    error() << 
+      "Could not read /dd/Structure/LHCb/DownstreamRegion/Muon from TDS "
+        << endreq;
+    return StatusCode::FAILURE;
+  }
+
+
+  m_NStation = 0;
+  m_NRegion = 0;
+  MuonBasicGeometry basegeometry( detSvc(),msgSvc());
+  m_NStation= basegeometry.getStations();  
+  m_NRegion = basegeometry.getRegions();  
+  if(rootInTES()!="")m_ignoreExecution=true;
+  //if TAE mode use the pribate tool to reset the meomory.
+  m_muonBuffer=tool<IMuonRawBuffer>("MuonRawBuffer",this);
+  if(!m_muonBuffer)info()<<"error retrieving the tool "<<endreq;
+  debug()<<" station number "<<m_NStation<<" "<<m_NRegion <<endreq;
+  m_offsetLoc[0]="Prev7/";
+  m_offsetLoc[1]="Prev6/";
+  m_offsetLoc[2]="Prev5/";
+  m_offsetLoc[3]="Prev4/";
+  m_offsetLoc[4]="Prev3/";
+  m_offsetLoc[5]="Prev2/";
+  m_offsetLoc[6]="Prev1/";
+  m_offsetLoc[7]="";
+  m_offsetLoc[8]="Next1/";
+  m_offsetLoc[9]="Next2/";
+  m_offsetLoc[10]="Next3/";
+  m_offsetLoc[11]="Next4/";
+  m_offsetLoc[12]="Next5/";
+  m_offsetLoc[13]="Next6/";
+  m_offsetLoc[14]="Next7/";
+
+  m_TAENum=7;
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode MuonTAERec::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;  
+  MuonCoords *coords = new MuonCoords;
+  if(!m_ignoreExecution){
+      // need to loop over input vector of MuonDigits
+  // and make output vectors of MuonCoords one for each station
+  std::vector<std::pair<LHCb::MuonTileID,unsigned int > > decoding;
+  std::vector<std::pair<LHCb::MuonTileID,unsigned int> >::iterator it;
+
+  for(int i=-m_TAENum;i<=m_TAENum;i++){
+    if (!exist<LHCb::RawEvent>(m_offsetLoc[7+i]  + LHCb::RawEventLocation::Default))
+      continue;
+  
+    
+    
+    // get tiles from the raw buffer
+    LHCb::RawEvent* raw = get<LHCb::RawEvent>(m_offsetLoc[7+i] +LHCb::RawEventLocation::Default);
+   
+    StatusCode sc=m_muonBuffer->getTileAndTDC(raw,decoding,m_offsetLoc[7+i] );
+    if(sc.isFailure()){
+      error()<<" error in decoding the muon raw data "<<endreq;
+      return sc;
+    }
+    debug()<<decoding.size()<<" digits in input "<<endreq;
+  
+    for(it = decoding.begin(); it != decoding.end(); it++){
+
+      // set the time taking into account the BX. must be unsigned...
+      std::vector< std::pair<LHCb::MuonTileID,unsigned int> >::iterator icheck;
+      bool add=true;
+     //info()<<" digit in input "<<(*it).first<<endreq;      
+      for(icheck= m_logChannels.begin();icheck!= m_logChannels.end();icheck++){
+         if ( (*it).first  ==  (*icheck).first ) {
+           verbose()<<" found double hit check time "<< (*it).second+(7+i)*16<<" "<<(*icheck).second<<endreq;
+           //take only the first one 
+           unsigned int tone=(*it).second;
+           tone=tone+(7+i)*16;
+           if(tone> (*icheck).second){
+             add =false;            
+		verbose()<<" so keep the existing one and not add the new one "<<endreq;
+             break;
+             
+           }else{
+             verbose()<<"remove the old one  "<<m_logChannels.size()<<endreq;
+             m_logChannels.erase(icheck);
+             
+           }
+           
+         }
+      }
+      if(add){
+         (*it).second += (7+i)*16;
+         std::pair<LHCb::MuonTileID,unsigned int>  ch((*it).first, (*it).second );
+         verbose()<<"adding digit  "<<(*it).first<<" to toal number "
+                  <<m_logChannels.size()<<endreq;        
+         m_logChannels.push_back(ch);
+      }
+    }
+    m_muonBuffer->forceReset();
+  }
+    std::vector< std::pair<LHCb::MuonTileID,
+                                   unsigned int> >::iterator itest;
+                                
+//for(    itest=m_logChannels.begin();itest<m_logChannels.end();itest++){
+//info()<<" lisat dei digit "<<(*itest).first<<endreq;
+//}
+  int station;
+
+  for( station = 0 ; station < m_NStation ; station++ ){
+    int region;
+    for( region = 0 ; region < m_NRegion ; region++ ){ 
+    
+      // get mapping of input to output from region
+      // in fact we are reversing the conversion done in the digitisation  
+      int NLogicalMap = m_muonDetector->getLogMapInRegion(station,region);
+      verbose()<<" station and region "<<station<<" "<<region<<" maps "<<NLogicalMap<<endreq;    
+      if(1 == NLogicalMap){
+        // straight copy of the input + making SmartRefs to the MuonDigits
+        StatusCode sc = addCoordsNoMap(coords,m_logChannels,station,region);
+        if(!sc.isSuccess()){
+          error()
+              << "Failed to map digits to coords in a one to one manner"
+              << endreq;
+          return sc;
+        }
+      }else{  
+        // need to cross the input strips to get output strips
+        StatusCode sc =
+          addCoordsCrossingMap(coords,m_logChannels,station,region);
+        if(!sc.isSuccess()){error()
+              << "Failed to map digits to coords by crossing strips"
+              << endreq;
+          return sc;
+        }
+      }
+    }
+  }
+  
+  }
+  m_logChannels.clear();
+  
+
+  put( coords, "Raw/Muon/TAECoords");
+
+  return StatusCode::SUCCESS;
+  
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode MuonTAERec::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+  release(    m_muonBuffer);
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
+//=============================================================================
+
+// Adding entries to coords 1 to 1 from digits, need to make the references
+StatusCode MuonTAERec::addCoordsNoMap(MuonCoords *coords,  
+                                   std::vector< std::pair<LHCb::MuonTileID,
+                                   unsigned int> > & digits,
+                                   const int & station,
+                                   const int & region){
+
+
+  std::vector<std::pair<LHCb::MuonTileID, unsigned int> >::iterator iD;
+  for( iD = digits.begin() ; iD != digits.end() ; iD++ ){
+    if( (iD->first).station() == static_cast<unsigned int>(station) &&
+        (iD->first).region() == static_cast<unsigned int>(region) ){
+      // make the coordinate to be added to coords
+      verbose()<<" digit tile "<<iD->first<<endreq;
+      
+      MuonCoord *current = new MuonCoord();
+      
+      current->setUncrossed(true);
+      current->setDigitTDC1(iD->second);
+      
+      // make a SmartRef to the MuonDigit
+      std::vector<MuonTileID> link;
+      link.push_back(iD->first);
+      current->setDigitTile(link);     
+      // add it to the current digit
+      
+      // need to clear the layer and readout bits
+      MuonTileID pad = iD->first;
+      verbose()<<pad<<endreq;
+      
+      // as no change between digit and coord in this mapping key is the same
+      coords->insert(current,pad);
+    }
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode MuonTAERec::addCoordsCrossingMap(MuonCoords *coords, 
+                                         std::vector<std::pair<LHCb::MuonTileID,
+                                         unsigned int > > & 
+                                         digits,
+                                         const int & station,
+                                         const int & region){
+
+  
+
+  // need to calculate the shape of the horizontal and vertical logical strips
+  //  if( 2 != m_muonDetector->getLogMapInRegion(station,region) ){
+  // log << MSG::ERROR << "There are " << pRegion->numberLogicalMap()
+  //     << " logical maps, I expect either 1 or 2" << endreq;
+  //}
+
+  // get local MuonLayouts for strips
+  MuonLayout layoutOne,layoutTwo;
+  StatusCode sc = makeStripLayouts(station,region, layoutOne,layoutTwo);
+  if(!sc.isSuccess()){
+    return sc;
+  }
+  
+  // seperate the two types of logical channel, flag if used with the pair
+  std::vector<std::pair<std::pair<MuonTileID,unsigned int>,bool> > typeOnes;
+  std::vector<std::pair<std::pair<MuonTileID,unsigned int>,bool> > typeTwos;
+  std::vector<std::pair<MuonTileID,unsigned int > >::iterator it;  
+  for( it = digits.begin() ; it != digits.end() ; it++ ){
+    if( (it->first).station() == static_cast<unsigned int>(station) &&
+        (it->first).region() == static_cast<unsigned int>(region) ){
+      if( (it->first).layout() == layoutOne ){
+        typeOnes.push_back(std::pair<std::pair<MuonTileID,unsigned int> ,bool>((*it),false));
+      }else if( (it->first).layout() == layoutTwo ){
+          typeTwos.push_back(std::pair<std::pair<MuonTileID,unsigned int> ,bool>((*it),false));
+      } else {
+        error()
+            << "MuonDigits in list are not compatable with expected shapes"
+            << (*it)<<endreq;
+      }    
+    }
+  }
+  // now cross the two sets of channels
+  // sorry about this std::pair stuff but it is the easiest way of matching
+  // a bool to each digit
+  std::vector<std::pair< std::pair<MuonTileID,unsigned int >,bool> >::iterator iOne;
+  std::vector<std::pair<std::pair<MuonTileID,unsigned int> ,bool> >::iterator iTwo;
+  for( iOne = typeOnes.begin() ; iOne != typeOnes.end() ; iOne++ ){
+    for( iTwo = typeTwos.begin() ; iTwo != typeTwos.end() ; iTwo++ ){
+      // who said C++ did not make lovely transparent code?
+      MuonTileID pad = ((iOne->first).first).intercept((iTwo->first).first);
+      if(pad.isValid()){ 
+        // have a pad to write out
+        // make the coordinate to be added to coords
+        MuonCoord *current = new MuonCoord();
+        current->setUncrossed(false);
+        std::vector<MuonTileID> link;
+        link.push_back((iOne->first).first);
+        link.push_back((iTwo->first).first);
+        current->setDigitTile(link);     
+        current->setDigitTDC1((iOne->first).second);
+        current->setDigitTDC2((iTwo->first).second);
+        verbose() << " Made an crossed pad " << pad 
+            << " from " << (iOne->first) << " and "
+            << (iTwo->first) << endreq;
+      
+        // as no change between digit and coord in this mapping key is the same
+        coords->insert(current,pad);
+
+        // set flags to used on iOne and iTwo
+        iOne->second = true;
+        iTwo->second = true;
+        
+      }
+    }
+  }
+if(!m_ignoreUncrossedStrips){
+  // now copy across directly all strips that have not yet been used
+  for( iOne = typeOnes.begin() ; iOne != typeOnes.end() ; iOne++ ){
+    if(!(iOne->second)){
+      // no crossing map here
+      MuonTileID pad = (iOne->first).first;
+
+      // make the coordinate to be added to coords
+      MuonCoord *current = new MuonCoord();
+      current->setUncrossed(true);
+
+      std::vector<MuonTileID> link;
+      link.push_back((iOne->first).first);
+      current->setDigitTile(link);
+      current->setDigitTDC1((iOne->first).second);
+
+      verbose() << " Found an uncrossed pad type 1 " << pad 
+          << endreq;
+
+      coords->insert(current,pad);
+    }
+  }
+  
+  for( iTwo = typeTwos.begin() ; iTwo != typeTwos.end() ; iTwo++ ){
+    if(!(iTwo->second)){
+      // no crossing map here
+      MuonTileID pad = (iTwo->first).first;
+
+      // make the coordinate to be added to coords
+      MuonCoord *current = new MuonCoord();
+      current->setUncrossed(true);
+
+
+      std::vector<MuonTileID> link;
+      current->setDigitTDC1((iTwo->first).second);
+      link.push_back((iTwo->first).first);
+      current->setDigitTile(link);     
+
+      verbose() << " Found an uncrossed pad type 2 " << pad 
+          << endreq;
+
+      coords->insert(current,pad);      
+
+    }
+  } 
+}
+  return StatusCode::SUCCESS;
+}
+
+
+
+StatusCode MuonTAERec::makeStripLayouts(int station, int region, 
+                                     MuonLayout &layout1,
+                                     MuonLayout &layout2){
+  
+  unsigned int x1 = m_muonDetector->getLayoutX(0,station,region);
+  unsigned int y1 = m_muonDetector->getLayoutY(0,station,region);
+  unsigned int x2 = m_muonDetector->getLayoutX(1,station,region);
+  unsigned int y2 = m_muonDetector->getLayoutY(1,station,region);
+  MuonLayout layoutOne(x1,y1);
+  MuonLayout layoutTwo(x2,y2);
+  layout1=layoutOne;
+  layout2=layoutTwo;
+  return StatusCode::SUCCESS;
+}
---
+++ LHCb/Muon/MuonDAQ/src/MuonTAERec.h
@@ -0,0 +1,68 @@
+// $Id: MuonTAERec.h,v 1.1 2009-04-28 10:10:12 asatta Exp $
+#ifndef MUONTAEREC_H 
+#define MUONTAEREC_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "MuonDet/MuonBasicGeometry.h"
+#include "MuonDet/DeMuonDetector.h"
+#include "MuonDAQ/IMuonRawBuffer.h"
+
+
+
+/** @class MuonTAERec MuonTAERec.h
+ *  
+ *
+ *  @author Alessia Satta
+ *  @date   2008-10-28
+ */
+class MuonTAERec : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  MuonTAERec( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~MuonTAERec( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+
+private:
+  /// Copy MuonTileID from digits to coords with no logical map (1:1 copy)
+  StatusCode addCoordsNoMap(LHCb::MuonCoords *coords,
+                            std::vector<std::pair<LHCb::MuonTileID ,
+                            unsigned int> > & digit,
+                            const int & station,
+                            const int & region);
+
+  /// Copy MuonTileID from digits to coord by crossing the digits
+  StatusCode addCoordsCrossingMap(LHCb::MuonCoords *coords,
+                                  std::vector<std::pair<LHCb::MuonTileID,
+                                  unsigned int> > &digit,
+                                  const int & station,
+                                  const int & region);
+
+  /// fills in the two readout layouts by querying the DeMuonRegion
+  StatusCode makeStripLayouts(int station , int region,
+                              MuonLayout &layout1,
+                              MuonLayout &layout2);
+
+bool m_ignoreUncrossedStrips;
+  // Number of stations
+  int m_NStation;
+  // Number of regions
+  int m_NRegion;
+  DeMuonDetector* m_muonDetector;
+  IMuonRawBuffer* m_muonBuffer;
+  bool m_ignoreExecution;
+  std::string m_offsetLoc[15]; 
+  int m_TAENum;
+  
+  std::vector<std::pair<LHCb::MuonTileID,unsigned int> >  m_logChannels;
+};
+
+#endif // MUONTAEREC_H
