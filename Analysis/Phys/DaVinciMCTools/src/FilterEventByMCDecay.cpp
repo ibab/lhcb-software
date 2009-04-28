8d9ab118631c22555c409d2e3012ef3de3621f53
jpalac <jpalac@cern.ch>
2009-04-28 13:57:46 +0000

    .  Algorithm to select or reject an event based on whether an event is      "signal". It uses a private instance of an IMCDecayFinder to determine      whether the MC event is consistent with being signal. In practice this      means that an MCDecayFinder (the only existing implementation of      interfaceoid IMCDecayFinder) is used, and its decay descriptor used to      determine whether the event is signal or not.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76123 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2e030684a5bcb44c8eed893809ad7be19ab534c0

---
+++ Analysis/Phys/DaVinciMCTools/src/FilterEventByMCDecay.cpp
@@ -0,0 +1,88 @@
+// $Id: FilterEventByMCDecay.cpp,v 1.1 2009-04-28 13:57:46 jpalac Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+// from LHCb
+#include "MCInterfaces/IMCDecayFinder.h"
+#include "Event/MCParticle.h"
+// local
+#include "FilterEventByMCDecay.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : FilterEventByMCDecay
+//
+// 2009-04-28 : Juan PALACIOS
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( FilterEventByMCDecay );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+FilterEventByMCDecay::FilterEventByMCDecay( const std::string& name,
+                                            ISvcLocator* pSvcLocator)
+  : 
+  GaudiAlgorithm ( name , pSvcLocator ),
+  m_select(true),
+  m_decayFinderName("MCDecayFinder"),
+  m_decayFinder(0),
+  m_inputLocation(LHCb::MCParticleLocation::Default)
+{
+  declareProperty("Select", m_select);
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+FilterEventByMCDecay::~FilterEventByMCDecay() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode FilterEventByMCDecay::initialize() {
+
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  m_decayFinder = tool<IMCDecayFinder>("MCDecayFinder", this);
+
+  return (0!=m_decayFinder) ? StatusCode::SUCCESS : StatusCode::FAILURE;
+
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode FilterEventByMCDecay::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  LHCb::MCParticle::Container* mcParts_ = 
+    get<LHCb::MCParticle::Container>(m_inputLocation);
+
+  const LHCb::MCParticle::ConstVector mcps(mcParts_->begin(),
+                                           mcParts_->end() );
+
+  const bool foundDecay = (mcParts_ != 0 ) && ( m_decayFinder->hasDecay(mcps) );
+
+  const bool pass = (m_select) ? foundDecay : !foundDecay ;
+
+  setFilterPassed(pass);
+
+  return StatusCode::SUCCESS;
+}
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode FilterEventByMCDecay::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Analysis/Phys/DaVinciMCTools/src/FilterEventByMCDecay.h
@@ -0,0 +1,58 @@
+// $Id: FilterEventByMCDecay.h,v 1.1 2009-04-28 13:57:46 jpalac Exp $
+#ifndef FILTEREVENTBYMCDECAY_H 
+#define FILTEREVENTBYMCDECAY_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+class IMCDecayFinder;
+
+/** @class FilterEventByMCDecay FilterEventByMCDecay.h
+ *  
+ *  Algorithm to select or reject an event based on whether an event is 
+ *  "signal". It uses a private instance of an IMCDecayFinder to determine 
+ *  whether the MC event is consistent with being signal. In practice this
+ *  means that an MCDecayFinder (the only existing implementation of 
+ *  interfaceoid IMCDecayFinder) is used, and its decay descriptor used to
+ *  determine whether the event is signal or not.
+ *
+ *  <b>Properties:</b>
+ *
+ *  <b>Select</b>: bool, controls whether event is accepted or rejected
+ *  if it is found to contain an MC signal.  Default <b>true</b>.
+ *
+ *  <b>Example</b>: Remove signal Bs->J/Psi(mumu)Phi(KK) events.
+ *
+ *  @code
+ *  # remove events that are signal
+ *  signalKiller = FilterEventByMCDecay()
+ *  signalKiller.Select = False
+ *  signalKiller.addTool(MCDecayFinder)
+ *  signalKiller.MCDecayFinder.Decay = '[B_s0 -> (J/psi(1S) -> mu+ mu- {,gamma} {,gamma}) (phi(1020) -> K+ K-)]cc'
+ *  # Assume TheSelSeq is a sequence that is carrying out a selection
+ *  TheSelSeq.Members += [signalKiller] # you might want to add to fron of sequence 
+ *  @endcode
+ *
+ *  @author Juan PALACIOS
+ *  @date   2009-04-28
+ */
+class FilterEventByMCDecay : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  FilterEventByMCDecay( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~FilterEventByMCDecay( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+private:
+  bool m_select;
+  std::string m_decayFinderName;
+  IMCDecayFinder* m_decayFinder;
+  std::string m_inputLocation;
+  
+};
+#endif // FILTEREVENTBYMCDECAY_H
