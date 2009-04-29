7514da6b589dfc51cc255d0a99ffde47f0d2026c
Giacomo Graziani <ggiacomo@cern.ch>
2009-04-29 08:37:08 +0000

    first release of MuonAlignmentMonitor by  Antonio Petrella and Stefania Vecchi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21838 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 10e8c7e45b019a0ef36b1c824daceda513103c5f

--- Rec/Muon/MuonTrackMonitor/options/MuonTrackMonitor.py
+++ Rec/Muon/MuonTrackMonitor/options/MuonTrackMonitor.py
@@ -1,16 +1,58 @@
+from TrackSys.Configuration import TrackSys
+# from Configurables import ( TrackEventFitter, TrackMasterFitter, TrackKalmanFilter,
+#                             TrackProjectorSelector, TrajOTProjector, TrackMasterExtrapolator,
+#                             TrackSimpleExtraSelector, TrackDistanceExtraSelector,
+#                             TrackHerabExtrapolator,
+#                             SimplifiedMaterialLocator, DetailedMaterialLocator,
+#                             MeasurementProvider)
+
+from TrackFitter.ConfiguredFitters import ( ConfiguredEventFitter )
+from Configurables import ( TrackKalmanFilter, MeasurementProvider )
+
+muonTrackFit = ConfiguredEventFitter( 'MuonTrackFitter', 'Rec/Track/Muon')
+muonTrackFit.Fitter.addTool( TrackKalmanFilter , 'NodeFitter' )
+muonTrackFit.Fitter.addTool( MeasurementProvider, name = 'MeasProvider')
+muonTrackFit.Fitter.ZPositions = [12100]
+muonTrackFit.Fitter.MeasProvider.IgnoreVelo = True 
+muonTrackFit.Fitter.MeasProvider.IgnoreTT   = True 
+muonTrackFit.Fitter.MeasProvider.IgnoreIT   = True 
+muonTrackFit.Fitter.MeasProvider.IgnoreOT   = True 
+muonTrackFit.Fitter.MeasProvider.IgnoreMuon = False 
+muonTrackFit.Fitter.NodeFitter.BiDirectionalFit = False
+muonTrackFit.Fitter.ErrorX2  = 1000000
+muonTrackFit.Fitter.ErrorY2  = 1000000
+muonTrackFit.Fitter.ErrorTx2 = 0.5
+muonTrackFit.Fitter.ErrorTy2 = 0.5
+muonTrackFit.Fitter.NumberFitIterations = 4
+muonTrackFit.Fitter.MaxNumberOutliers   = 2
+muonTrackFit.OutputLevel = 4
+
+
+
 from Configurables import GaudiSequencer,MuonTrackMonitor,MuonEfficiencyMonitor,MuonAlignmentMonitor
+
+muonMoniSeq = GaudiSequencer("MoniMUONSeq")
 
 montracks = MuonTrackMonitor( "MuonTrackMonitor",
                               HistoTopDir = "Muon/"
                               )
-GaudiSequencer("MoniMUONSeq").Members += [montracks]
+muonMoniSeq.Members += [montracks]
 
 moneffi = MuonEfficiencyMonitor( "MuonEfficiencyMonitor",
                                  HistoTopDir = "Muon/"
                                  )
-GaudiSequencer("MoniMUONSeq").Members += [moneffi]
+#muonMoniSeq.Members += [moneffi]
 
 monalig = MuonAlignmentMonitor( "MuonAlignmentMonitor",
                                  HistoTopDir = "Muon/"
                                  )
-GaudiSequencer("MoniMUONSeq").Members += [monalig]
+monalig.OutputLevel = 4
+muonMoniSeq.Members += [monalig]
+monalig.IsLongTrackState = "true"
+monalig.LongToMuonMatch = "true"
+monalig.pCut = 0
+monalig.IsCosmics = "true"
+
+# muonMoniSeq.Members = [muonTrackFit,  montracks, moneffi, monalig ]  
+muonMoniSeq.Members = [muonTrackFit, montracks, monalig]  
+ 
--- Rec/Muon/MuonTrackMonitor/src/MuonAlignmentMonitor.h
+++ Rec/Muon/MuonTrackMonitor/src/MuonAlignmentMonitor.h
@@ -1,9 +1,8 @@
-// $Id: MuonAlignmentMonitor.h,v 1.1.1.1 2009-02-26 09:29:59 ggiacomo Exp $
+// $Id: MuonAlignmentMonitor.h,v 1.2 2009-04-29 08:37:08 ggiacomo Exp $
 #ifndef MUONTRACKMONITOR_H 
 #define MUONTRACKMONITOR_H 1
 
 #include "GaudiAlg/GaudiHistoAlg.h"
-
 
 /** @class MuonAlignmentMonitor MuonAlignmentMonitor.h
  *  
@@ -11,8 +10,13 @@
  *  @author 
  *  @date   2009-02-25
  */
+class ITrackExtrapolator;
+class ITrackChi2Calculator;
+class DeMuonDetector;
+
 class MuonAlignmentMonitor : public GaudiHistoAlg {
-public: 
+
+ public: 
   MuonAlignmentMonitor( const std::string& name, ISvcLocator* pSvcLocator );
 
   virtual ~MuonAlignmentMonitor( ); ///< Destructor
@@ -21,9 +25,23 @@ public:
   virtual StatusCode execute   ();    ///< Algorithm execution
   virtual StatusCode finalize  ();    ///< Algorithm finalization
 
-protected:
+  static const int nSTATION = 5;
 
-private:
+  AIDA::IHistogram1D  *m_h_chi2, *m_h_resx_a[nSTATION], *m_h_resy_a[nSTATION], *m_h_resx_c[nSTATION], *m_h_resy_c[nSTATION], *m_h_p;
+  AIDA::IHistogram2D  *m_h_xy, *m_h_txty;
+ 
+  AIDA::IProfile1D *m_p_resxx, *m_p_resxy, *m_p_resxtx,*m_p_resxty, *m_p_restxx, *m_p_restxy, *m_p_restxtx,
+    *m_p_restxty, *m_p_resyx, *m_p_resyy, *m_p_resytx, *m_p_resyty, *m_p_restyx, *m_p_restyy, *m_p_restytx, *m_p_restyty;
+  
+  ITrackExtrapolator   *m_extrapolator; ///< extrapolator
+  ITrackChi2Calculator *m_chi2Calculator;
+  
+  DeMuonDetector*       m_muonDet; 
+  bool m_IsLongTrackState, m_LongToMuonMatch, m_IsCosmics;
+  double m_pCut;
+
+  std::string m_extrapolatorName, m_Chi2CalculatorName;
+
 
 };
 #endif // MUONTRACKMONITOR_H
