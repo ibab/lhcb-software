cc97cf63610e4c3a667fefaa9ec4f81f52a39f71
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-04-29 08:59:39 +0000

    patch
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@76139 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b4ae9d2a7c68b496cd49443c4dc586074e8ea36

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/RhoPlus.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/RhoPlus.py
@@ -11,7 +11,7 @@ from Gaudi.Configuration import *
 from Configurables import CombineParticles
 from HltConf.HltLine import bindMembers
 from Hlt2SharedParticles.GoodParticles import GoodPions, GoodKaons
-from Hlt2SharedParticles.Pi0 import AllPi0s
+from Hlt2SharedParticles.Pi0 import AllPi0s, MergedPi0s, ResolvedPi0s
 
 __all__ = ( 'RhoPlus2PiPi0','RhoPlus2KPi0')
 
@@ -44,5 +44,5 @@ Hlt2SharedRhoPlus2KPi0.DaughtersCuts.update(  { "K+" : "ALL" } )
 Hlt2SharedRhoPlus2KPi0.DaughtersCuts.pop( 'pi+' )
 
 # @todo How do I best get Merged and Resolved in here ?
-RhoPlus2KPi0 = bindMembers( None, [ GoodKaons, AllPi0s, Hlt2SharedRhoPlus2KPi0 ]) # NOT GOOD
+RhoPlus2KPi0 = bindMembers( None, [ GoodKaons, AllPi0s, Hlt2SharedRhoPlus2KPi0 ]) 
 
