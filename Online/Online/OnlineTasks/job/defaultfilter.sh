fdebd1666819d1463214f81caa55029fb85c8844
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2009-04-29 14:00:15 +0000

    Defaultfilter.sh now takes the Moore version as an argument. Remove braces from dimclientdns in Adder .opts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99396 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1ce0c6ae01529160e2a389fd4455ad984da738b5

--- Online/Online/OnlineTasks/job/defaultfilter.sh
+++ Online/Online/OnlineTasks/job/defaultfilter.sh
@@ -1,14 +1,4 @@
-#!/bin/sh
-# pick up the online setup...
-source ./setupOnline.sh $*
-# pick up 'our' setup... (which defines $MOOREROOT!)
-source /group/hlt/MOORE/Moore_v6r1/Hlt/Moore/job/SetupMoore_v6r1.sh
-# and the options to be used
-export USEROPTS=/group/hlt/MOORE/Moore_v6r1/Hlt/Moore/options/DEFAULT${IS_TAE_RUN}.opts
-[[ 'x'$PARTNAME == 'xFEST' ]] && USEROPTS=/group/hlt/MOORE/Moore_v6r1/Hlt/Moore/options/DEFAULT_FEST.opts
-[[ 'x'$PARTNAME == 'xLHCb' ]] && USEROPTS=/group/hlt/MOORE/Moore_v6r1/Hlt/Moore/options/DEFAULT_LHCb.opts
-export CORAL_SQLITE_TEMP_STORE_MEMORY=yes
-# and run them!
-echo exec -a ${UTGID} ${CLASS1_TASK} -opt=${USEROPTS}
-exec -a ${UTGID} ${CLASS1_TASK} -opt=${USEROPTS}
-      
+#!/bin/bash
+# Call Moore startup script
+# should be in /group/hlt/MOORE/Moore_version/Hlt/Moore/job/runMooreOnline_EFF.sh
+/group/hlt/MOORE/$1/Hlt/Moore/job/runMooreOnline_EFF.sh
