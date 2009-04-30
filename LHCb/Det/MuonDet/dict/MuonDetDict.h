9805955252ff07b42fc776a9990311eda0c0fba9
Alessia Satta <asatta@cern.ch>
2009-04-30 08:41:46 +0000

    update dict for new name IMuonFastPosTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37356 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e7ad325f2e30f811fc3b1ad4a545542f0bf07404

--- LHCb/Det/MuonDet/dict/MuonDetDict.h
+++ LHCb/Det/MuonDet/dict/MuonDetDict.h
@@ -1,4 +1,4 @@
-// $Id: MuonDetDict.h,v 1.2 2007-06-11 09:24:02 cattanem Exp $
+// $Id: MuonDetDict.h,v 1.3 2009-04-30 08:41:46 asatta Exp $
 // ============================================================================
 #ifndef DICT_MUONDETDICT_H 
 #define DICT_MUONDETDICT_H 1
@@ -14,7 +14,7 @@
 #include "MuonDet/MuonNamespace.h"
 #include "MuonDet/MuonODEBoard.h"
 #include "MuonDet/MuonStationCabling.h"
-#include "MuonDet/IMuonPosTool.h"
+#include "MuonDet/IMuonFastPosTool.h"
 
 // ============================================================================
 #endif // DICT_MUONDETDICT_H
