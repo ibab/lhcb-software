b2e9b12c817c40d79a38c8d0b8ab7a85a8b07788
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-05-01 17:45:25 +0000

    More tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@76200 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dc701fab26e7361b4fe2f6b1cb61201652235f4e

---
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt12-MDF.py
@@ -0,0 +1,42 @@
+########################################################################
+#
+# $Id: DVTestHlt12-MDF.py,v 1.1 2009-05-01 17:45:25 pkoppenb Exp $
+#
+# Test for Hlt1&2. Test for MDF files.
+#
+# @author Patrick Koppenburg
+# @date 2008-08-06
+#
+########################################################################
+from Gaudi.Configuration import *
+from Configurables import GaudiSequencer
+########################################################################
+#
+# Standard configuration
+#
+from Configurables import DaVinci, HltConf
+HltConf().ThresholdSettings = 'Miriam_20090430'
+DaVinci().EvtMax = 10000                       # Number of events
+DaVinci().SkipEvents = 0                       # Events to skip
+DaVinci().PrintFreq  = 100                     # Print frequency
+DaVinci().DataType = "2008"                    # Default is "DC06"
+# DaVinci().Simulation   = False 
+# DaVinci().MoniSequence += [ ReadHltSummary() ]
+########################################################################
+#
+# HLT
+#
+DaVinci().Hlt2Requires = 'L0+Hlt1'
+DaVinci().HltType = 'Hlt1+Hlt2'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
+DaVinci().InputType = "MDF"
+EventSelector().Input   = [
+    "   DATA='PFN:castor:/castor/cern.ch/grid/lhcb/test/MDF/00003083/0000/00003083_00000001_1.mdf' SVC='LHCb::MDFSelector'" ]
+
+########################################################################
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+def noInit():
+    GaudiSequencer("PhysInitSeq").Members = []
+    GaudiSequencer("AnalysisInitSeq").Members = []
+
+appendPostConfigAction(noInit)
