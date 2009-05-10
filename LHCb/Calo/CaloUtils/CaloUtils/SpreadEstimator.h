3e6b8ba94f193fea50d7371d6e3fca07810ef177
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-05-10 15:17:00 +0000

    improve the numerical stability
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40292 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7cfc0a0020c51b157f1947091cb6d1e3b752997e

--- LHCb/Calo/CaloUtils/CaloUtils/SpreadEstimator.h
+++ LHCb/Calo/CaloUtils/CaloUtils/SpreadEstimator.h
@@ -1,28 +1,30 @@
-// $Id: SpreadEstimator.h,v 1.5 2006-03-22 18:25:06 odescham Exp $
+// $Id: SpreadEstimator.h,v 1.6 2009-05-10 15:17:00 ibelyaev Exp $
 // ============================================================================
 // CVS tag $Name: not supported by cvs2svn $ 
 // ============================================================================
-// $Log: not supported by cvs2svn $
-// Revision 1.4  2005/11/07 11:57:13  odescham
-// v5r0 - Adapt to the new Track Event Model
-//
-// Revision 1.3  2004/05/27 13:40:03  cattanem
-// v4r7
-//
-// Revision 1.2  2002/11/13 20:43:36  ibelyaev
-//  few bugs are fixed
-//
-// Revision 1.1  2001/11/22 16:02:33  ibelyaev
-//  new utilities
-// 
-// ============================================================================
 #ifndef CALOUTILS_SPREADESTIMATOR_H 
 #define CALOUTILS_SPREADESTIMATOR_H 1
+// ============================================================================
 // Include files
+// ============================================================================
+// ST D& STL 
+// ============================================================================
 #include <functional> 
+// ============================================================================
+// GaudiKernel 
+// ============================================================================
 #include "GaudiKernel/StatusCode.h"
-class DeCalorimeter;    ///< from CaloDet package 
-
+#include "GaudiKernel/StatEntity.h"
+// ============================================================================
+// RecEvent
+// ============================================================================
+#include "Event/CaloDataFunctor.h"
+#include "Event/CellID.h"
+// ============================================================================
+/// forward declarations 
+// ============================================================================
+class DeCalorimeter;    // from CaloDet package 
+// ============================================================================
 /** @class SpreadEstimator SpreadEstimator.h CaloUtils/SpreadEstimator.h
  *  
  *  simple helper class for estimation of spread of 
@@ -32,22 +34,20 @@ class DeCalorimeter;    ///< from CaloDet package
  *  @author Vanya Belyaev Ivan.Belyaev@itep.ru
  *  @date   22/11/2001
  */
-
 class SpreadEstimator :
   public std::unary_function<LHCb::CaloCluster*,StatusCode>
 {
-  
+  // ==========================================================================
  public:
-  
+  // ==========================================================================  
   /** standard/default constructor 
    *  @param Det pointer to calorimeter detector
    */ 
   SpreadEstimator( const DeCalorimeter* Det = 0 );
-  
-  /** destructor 
-   */
-  virtual ~SpreadEstimator();
-  
+  // ==========================================================================  
+  /// destructor 
+  ~SpreadEstimator();
+  // ==========================================================================  
   /** calculation of cluster spread  
    *
    *  Error codes:
@@ -56,38 +56,56 @@ class SpreadEstimator :
    *  @param  cluster  pointer to cluster object 
    *  @return status code
    */
-  StatusCode operator()
-    ( LHCb::CaloCluster* cluster ) const ;
-  
+  StatusCode operator() ( LHCb::CaloCluster* cluster ) const ;
+  // ==========================================================================
   /** calculate spread for cluster
    *  @param  cluster  pointer to cluster
    *  @return status code
    */
   inline StatusCode calculateSpread 
-    ( LHCb::CaloCluster* cluster ) const
-    { return (*this)( cluster ); }
-  
+  ( LHCb::CaloCluster* cluster ) const
+  { return (*this)( cluster ); }
+  // ==========================================================================  
+public:
+  // ==========================================================================  
   /** set new value for calorimeter 
    *  @param Det pointer to calorimeter detector 
    */
-  inline void setDetector
-    ( const DeCalorimeter* Det ) 
-    { m_detector = Det ; }
-  
+  void setDetector ( const DeCalorimeter* Det ) ;
+  // ==========================================================================
   /** simple accessor to DeCalorimeter object
    *  @return pointer to detector 
    */
   inline const DeCalorimeter* detector    () const
     { return m_detector    ; }
-  
- private:
-  
-  const DeCalorimeter*  m_detector ;
-
-  mutable unsigned long m_invalid  ;
-  
+  // ==========================================================================  
+  /// get the counter for problematic cases 
+  const StatEntity& invalidRatio  () const { return m_ratio  ; }
+  /// get the counter for problematic cases 
+  const StatEntity& invalidCells  () const { return m_cells  ; }
+  /// get the counter for problematic cases 
+  const StatEntity& invalidEnergy () const { return m_energy ; }
+  // ==========================================================================  
+private:
+  // ==========================================================================
+  /// the detector elemenet 
+  const DeCalorimeter*  m_detector ;                   // the detector elemenet 
+  // ==========================================================================
+  /// counter of invalid cells 
+  mutable StatEntity m_cells  ;                    // counter for invaild cells 
+  /// counter of invalid size ratio  
+  mutable StatEntity m_ratio  ;               // counter for invaild size ratio
+  /// counter of invalid energy 
+  mutable StatEntity m_energy ;                   // counter for invaild energy 
+  // ==========================================================================  
+  typedef LHCb::CaloDataFunctor::EnergyTransverse<const LHCb::CaloCluster*,
+                                                  const DeCalorimeter*> ET ;
+  /// transverse energy estimator 
+  ET m_et ; // transverse energy estimator
+  // ==========================================================================  
 };
-
+// ============================================================================
+// The END 
 // ============================================================================
 #endif // CALOUTILS_SPREADESTIMATOR_H
 // ============================================================================
--- LHCb/Calo/CaloUtils/src/SpreadEstimator.cpp
+++ LHCb/Calo/CaloUtils/src/SpreadEstimator.cpp
@@ -1,13 +1,36 @@
-// $Id: SpreadEstimator.cpp,v 1.12 2007-08-24 21:28:04 odescham Exp $
+// $Id: SpreadEstimator.cpp,v 1.13 2009-05-10 15:17:00 ibelyaev Exp $
 // ============================================================================
 // Include files
-#include "CaloDet/DeCalorimeter.h"
-#include "Event/CaloCluster.h"
-// local
-#include "CaloUtils/SpreadEstimator.h"
-
 // ============================================================================
-/** @file SpreadEstimator.cpp 
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.13 $
+// ============================================================================
+#include <cmath>
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/GenericVectorTypes.h"
+#include "GaudiKernel/GenericMatrixTypes.h"
+#include "GaudiKernel/SymmetricMatrixTypes.h"
+#include "GaudiKernel/SystemOfUnits.h"
+// ============================================================================
+// CaloDet 
+// ============================================================================
+#include "CaloDet/DeCalorimeter.h"
+// ============================================================================
+// Event 
+// ============================================================================
+#include "Event/CaloCluster.h"
+// ============================================================================
+// LHCbMath 
+// ============================================================================
+//#include "LHCbMath/Lomont.h"
+#include "LHCbMath/LHCbMath.h"
+// ============================================================================
+// local
+// ============================================================================
+#include "CaloUtils/SpreadEstimator.h"
+// ============================================================================
+/** @file 
  * 
  *  Implementation file for class : SpreadEstimator
  * 
@@ -15,32 +38,45 @@
  *  @date 22/11/2001 
  */
 // ============================================================================
-
+namespace 
+{
+  // ==========================================================================
+  double round ( const double value , const double step  ) 
+  { 
+    const double _aux1 = value / step         ;
+    const double _aux2 = std::floor ( _aux1 ) ;
+    return ( _aux1 - _aux2 < 0.5 ) ? _aux2 * step :  ( _aux2 + 1 ) * step ;
+  } 
+  // ==========================================================================
+}
 // ============================================================================
-/** standard/default constructor 
+/*  standard/default constructor 
  *  @param Det pointer to calorimeter detector
  */ 
 // ============================================================================
 SpreadEstimator::SpreadEstimator( const DeCalorimeter* Det )
   : m_detector ( Det ) 
-  , m_invalid  ( 0   )
-{};
-
+  , m_cells    ()
+  , m_ratio    ()
+  , m_energy   ()
+  , m_et       ( Det )
+{}
 // ============================================================================
-/** destructor 
+// destructor 
+// ============================================================================
+SpreadEstimator::~SpreadEstimator() { m_detector = 0 ; } 
+// ============================================================================
+/*  set new value for calorimeter 
+ *  @param Det pointer to calorimeter detector 
  */
 // ============================================================================
-SpreadEstimator::~SpreadEstimator() 
+void SpreadEstimator::setDetector ( const DeCalorimeter* Det ) 
 { 
-  m_detector = 0 ; 
-  if( 0 < m_invalid ) 
-  {
-    std::cout << " **** File '" << __FILE__  << "'" << std::endl  
-              << " \t #of invalid covariances   " << m_invalid << std::endl;
-  }
-} ;
+  m_detector = Det ; 
+  m_et = ET ( detector () ) ;
+}
 // ============================================================================
-/** calculation of cluster spread  
+/*  calculation of cluster spread  
  *
  *  Error codes:
  *
@@ -53,8 +89,7 @@ SpreadEstimator::~SpreadEstimator()
  *  @return status code
  */
 // ============================================================================
-StatusCode SpreadEstimator::operator()
-  ( LHCb::CaloCluster* cluster ) const 
+StatusCode SpreadEstimator::operator() ( LHCb::CaloCluster* cluster ) const 
 {
   // ignore trivial cases 
   if ( 0 == cluster               ) { return StatusCode::SUCCESS ; }
@@ -65,6 +100,10 @@ StatusCode SpreadEstimator::operator()
   /// avoid long names 
   typedef LHCb::CaloCluster::Entries::const_iterator const_iterator;
   
+  const double x_prec  = 0.2 * Gaudi::Units::mm  ;
+  const double x_prec2 = x_prec * x_prec         ;
+  const double e_prec  = 0.2 * Gaudi::Units::MeV ;
+  
   ///
   double covxx         = 0 ;
   double covyx         = 0 ;
@@ -72,11 +111,15 @@ StatusCode SpreadEstimator::operator()
   double xmean         = 0 ;
   double ymean         = 0 ;
   double etot          = 0 ;
-  
-  unsigned int ncells  = 0   ;
-  
-  double cellsize      = -10 ;
  
+  double x0            = 0 ;
+  double y0            = 0 ;
+  
+  unsigned int ncells  = 0     ;
+  double eall          = 0     ;
+  
+  double cellsize      = -10   ;
+  bool   first         = true  ;
   
   const LHCb::CaloCluster::Entries& entries = cluster->entries() ;
   for ( LHCb::CaloCluster::Entries::const_iterator entry = entries.begin() ;
@@ -90,45 +133,61 @@ StatusCode SpreadEstimator::operator()
     const double energy   = digit->e() * fraction ;
     ///
     const Gaudi::XYZPoint& pos =
-      detector()->cellCenter( digit->cellID() );
+      detector()->cellCenter ( digit->cellID() );
     ///
     if ( entry->status() & LHCb::CaloDigitStatus::SeedCell )
     { cellsize = detector()->cellSize( digit->cellID() ) ; }
     ///
-    const double x =  pos.x() ;
-    const double y =  pos.y() ;
+    eall += energy ;
     ///
-    if ( energy <= 0 ) { continue ; }
-    const double weight = energy > 0.0 ? energy : 0.0 ;
+    if ( energy <= 0 ) { continue ; }                             //  CONTINUE! 
+    //
+    const double weight = energy  ;                               // ATTENTION! 
+    if ( weight <= 0 ) { continue ; }                             //  CONTINUE!  
     ///
     ++ncells ;
     ///
-    etot  +=         weight ;
-    xmean +=     x * weight ;
-    ymean +=     y * weight ;
+    const double x =  pos.x () ;
+    const double y =  pos.y () ;
+    
+    /// accumulate the energy 
+    etot  +=           weight ;
+    
+    // the first good cell? 
+    if ( first ) 
+    {
+      x0    =     x ;  // adjust the bias 
+      y0    =     y ;  // adjust the bias 
+      first = false ;  // can skip since everything propto dx and/or dy
+      continue      ;  // continue
+    }
+    
+    const double dx = x - x0 ;  // temporary shift 
+    const double dy = y - y0 ;  // temporary shift 
+    
+    xmean +=      dx * weight ;
+    ymean +=      dy * weight ;
     //
-    covxx += x * x * weight ;
-    covyx += y * x * weight ;
-    covyy += y * y * weight ;
+    covxx += dx * dx * weight ;
+    covyx += dy * dx * weight ;
+    covyy += dy * dy * weight ;
     ///
   }
-  ///
-  /// strange combinations 
-  if ( ncells    <= 0 ) {return StatusCode ( 223 ) ;  }
-  /// energy is not positive  
-  if ( etot      <= 0 ) { return StatusCode ( 224 ) ; }
-  /// seed cell was not found
-  if ( cellsize  <= 0 ) { return StatusCode ( 222 ) ; }
+  
+  // strange combinations 
+  if ( ncells    <= 0                     ) { return StatusCode ( 223 ) ; }
+  // energy is not positive  
+  if ( eall      <= 1 * Gaudi::Units::MeV ) { return StatusCode ( 224 ) ; }
+  // energy is not positive  
+  if ( etot      <= 1 * Gaudi::Units::MeV ) { return StatusCode ( 224 ) ; }
+  // seed cell was not found
+  if ( cellsize  <= 0                     ) { return StatusCode ( 222 ) ; }
+  /// 
   
   const double uniform  = cellsize * cellsize / 12.  ;
-  const double cut      = 0.5 * uniform              ;
   
   xmean /= etot ;
   ymean /= etot ;
-  
-  LHCb::CaloPosition::Center& center = cluster->position().center() ;
-  center ( LHCb::CaloPosition::X ) = xmean ;
-  center ( LHCb::CaloPosition::Y ) = ymean ;
   
   covxx /= etot ;
   covyx /= etot ;
@@ -138,41 +197,88 @@ StatusCode SpreadEstimator::operator()
   covyx -= ymean * xmean ;
   covyy -= ymean * ymean ;
   
-  /// could do nothing for 1 cell "clusters" 
+  xmean += x0   ;                      // shift back 
+  ymean += y0   ;                      // shift back 
+  
+  LHCb::CaloPosition::Center& center = cluster->position().center() ;
+  center ( LHCb::CaloPosition::X ) = xmean ;
+  center ( LHCb::CaloPosition::Y ) = ymean ;
+
+  
+  /// could do nothing else for 1 cell "clusters" 
   if ( 1 == ncells  ) 
   {
     covxx = uniform ;
     covyy = uniform ;
     covyx = 0.0     ;
   }
-  
-  //    if( 0 >= covxx || 0 >= covyy )
-  //      {
-  //        ++m_invalid ;
-  //        std::cout << " **** File '"       << __FILE__ 
-  //                  << "' : Non-positive  Cov(xx) or Cov(yy) "
-  //                  << " covxx= " << covxx  << "/" << covxx + xmean * xmean 
-  //                  << " covyx= " << covyx  << "/" << covyx + ymean * xmean 
-  //                  << " covyy= " << covyy  << "/" << covyy + ymean * ymean 
-  //                  << " etot="   << etot  
-  //                  << " ncell="  << ncells << std::endl ;
-  //        std::cout << *cluster << std::endl ;
-  //      }
-  
-  
-  if ( covxx < cut ) { covxx = uniform ; covyx = 0 ; }
-  if ( covyy < cut ) { covyy = uniform ; covyx = 0 ; }
+  else 
+  {  
+    const double trace = covxx + covyy             ;    
+    const double diff  = covxx - covyy ;
+    const double disc  = std::sqrt ( diff * diff + 4 * ( covyx * covyx ) ) ;
+    
+    // eigen values:
+    const double lambda1 = 0.5  * ( trace - disc ) ;
+    const double lambda2 = 0.5  * ( trace + disc ) ;
+    
+    // minimal eigenvalue 
+    const double lambdaMin  = std::min ( lambda1 , lambda2 ) / uniform ;
+    const double covMin     = std::min ( covxx   , covyy   ) / uniform ;
+    
+    const double       s_Cut   = 0.5 ;
+    const unsigned int s_Cells =   4 ;
+    
+    if ( s_Cells >= ncells    ||   // small amout of cells wth e>0  
+         s_Cut   >= lambdaMin ||   // small eigenvalue  
+         s_Cut   >= covMin     )   // small eigenvalue 
+    {
+      // =======================================================================
+      // the matrix must be modified a bit:
+      const double eT = round ( m_et ( cluster ) , e_prec ) ;
+      m_ratio  += (float) lambdaMin ;
+      m_energy += (float) ( eT / Gaudi::Units::GeV ) ;
+      m_cells  +=         ncells    ;
+      
+      // construct the matrix of eigen vectors
+      
+      const double diff1 = covxx - lambda1 ;
+      const double diff2 = covxx - lambda2 ;
+      
+      const LHCb::Math::abs_less<double> absLess = LHCb::Math::abs_less<double>() ;
+
+      const double phi = 
+        // the matrix is numerically diagonal? => trivial eigenvectors  
+        absLess ( covyx , x_prec2 ) ? 0.0 : 
+        // find "the best non-zero" eigenvector 
+        absLess ( diff1 , diff2   ) ? 
+        std::atan2 ( covyx ,  diff2 ) : std::atan2 ( diff1 , -covyx ) ;
+      
+      const double cphi = std::cos ( phi ) ;
+      const double sphi = std::sin ( phi ) ;
+      
+      const double s2 = sphi * sphi ;
+      const double c2 = cphi * cphi ;
+      const double cs = cphi * sphi ;
+      
+      const double newLambda1 = std::max ( lambda1 , uniform ) ;
+      const double newLambda2 = std::max ( lambda2 , uniform ) ;
+      
+      // recalculate the matrix using new eigenvalues 
+      covxx = c2 *   newLambda1 + s2 * newLambda2   ;
+      covyx = cs * ( newLambda1 -      newLambda2 ) ;
+      covyy = s2 *   newLambda1 + c2 * newLambda2   ; 
+    } 
+  }
   
   LHCb::CaloPosition::Spread& spread = cluster->position().spread() ;
   spread ( LHCb::CaloPosition::X , LHCb::CaloPosition::X ) = covxx ;
   spread ( LHCb::CaloPosition::Y , LHCb::CaloPosition::X ) = covyx ;
   spread ( LHCb::CaloPosition::Y , LHCb::CaloPosition::Y ) = covyy ;
-
+  
   ///
   return StatusCode::SUCCESS ;
-};
-// ============================================================================
-
+}
 // ============================================================================
 // The End 
 // ============================================================================
