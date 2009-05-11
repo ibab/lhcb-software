de740a5144cfce3dbc54fc4f9de483f8defcbde7
jpalac <jpalac@cern.ch>
2009-05-11 15:01:12 +0000

    . New classes to debug Particle -> MCParticle associator tool output.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76336 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e9a98272428aee243a42f62b51ad8ed17206a785

---
+++ Analysis/Ex/MicroDSTExample/python/MicroDSTExample/Debug.py
@@ -0,0 +1,61 @@
+from MicroDSTExample import Helpers
+class AssocTreeDebugger :
+    def __init__(self, assoc, particleName) :
+        self.assoc = assoc
+        self.pName = particleName
+        self.nTrees = {}
+    def __call__(self, particle) :
+        trees = self.assoc.relatedMCPs(particle)
+        if (trees != None) :
+            size = trees.size()
+            pid = Helpers.pid(particle)
+            if self.nTrees.has_key(pid) :
+                if self.nTrees[pid].has_key(size) :
+                    self.nTrees[pid][size]+=1
+                else :
+                    self.nTrees[pid][size]=1
+            else :
+                self.nTrees[pid] = { size : 1 }
+            print "Found ", size, " trees for particle ", self.pName(particle)
+            if size > 0 :
+                for tree in trees :
+                    print "\t===========TREE========"
+                    for p in tree :
+                        print "\t", self.pName(p)
+                    print "\t======================="
+        else :
+            print "Found NO trees for particle ", self.pName(particle)
+            if self.nTrees.has_key("None") :
+                self.nTrees["None"] +=1
+            else :
+                self.nTrees["None"]=1
+
+
+class WeightedDebugger :
+    def __init__(self, assoc, particleName) :
+        self.assoc = assoc
+        self.pName = particleName
+        self.nTrees = {}
+    def __call__(self, particle) :
+        mcps = self.assoc.relatedMCPs(particle)
+        if (mcps != None) :
+            size = mcps.size()
+            pid = Helpers.pid(particle)
+            if self.nTrees.has_key(pid) :
+                if self.nTrees[pid].has_key(size) :
+                    self.nTrees[pid][size]+=1
+                else :
+                    self.nTrees[pid][size]=1
+            else :
+                self.nTrees[pid] = { size : 1 }
+            if size > 1 :
+                print "Found ", size, " mcps for particle ", self.pName(particle)
+                print "\t===MCPs for Particle PID ", pid, " ==="
+                for mcp in mcps :
+                    print "\t", self.pName(mcp)
+                print "\t=================================="
+        else :
+            if self.nTrees.has_key("None") :
+                self.nTrees["None"] +=1
+            else :
+                self.nTrees["None"]=1
