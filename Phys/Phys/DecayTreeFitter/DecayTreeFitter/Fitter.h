24343d92605eade4262af34633e4e6f56a0d5d01
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-05-12 07:25:36 +0000

    moved to make public
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@64890 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f416813d64a58068073a16fd1c521e0fa2b7c9e3

---
+++ Phys/Phys/DecayTreeFitter/DecayTreeFitter/Fitter.h
@@ -0,0 +1,123 @@
+#ifndef DECAYTREEFITTER_FITTER_HH
+#define DECAYTREEFITTER_FITTER_HH
+
+#include "CLHEP/Matrix/SymMatrix.h"
+#include "CLHEP/Matrix/Vector.h"
+#include <vector>
+#include "DecayTreeFitter/VtxErrCode.h"
+#include "DecayTreeFitter/VtxFitStatus.h"
+
+class VtxDoubleErr ;
+class VtxLorentzVectorErr ;
+class VtxFitParams ;
+
+namespace LHCb
+{
+  class Particle ;
+  class VertexBase ;
+  class VtxDoubleErr ;
+  class VtxFitParams ;
+}
+
+namespace decaytreefit
+{
+
+  class DecayChain ;
+  class FitParams ;
+  class Upsilon ;
+  class ParticleBase ;
+  
+  extern int vtxverbose ;
+
+  typedef LHCb::VtxFitStatus FitStatus ;
+  typedef LHCb::VtxDoubleErr VtxDoubleErr ;
+  typedef LHCb::VtxFitParams VtxFitParams ;
+
+  class Fitter
+  {
+  public:
+    Fitter() : m_decaychain(0), m_fitparams(0) {} 
+    Fitter(const LHCb::Particle& bc, bool forceFitAll = true) ;
+    Fitter(const LHCb::Particle& bc, const LHCb::VertexBase& pv, bool forceFitAll = true) ;
+    ~Fitter() ;
+
+    // Add or remove a mass constraint
+    void setMassConstraint( const LHCb::Particle& cand, bool add=true ) ;
+    
+    // Fit the decay tree
+    void fit(int maxNumberOfIterations=10, double deltaChisquareConverged=0.01) ;
+    
+    // Fit just one step
+    void fitOneStep() ;
+
+    // Print the result of the fit
+    void print() const ;
+
+    // The top level particle that is fitted
+    const LHCb::Particle* particle() const { return m_particle ; }
+
+    // Currently the only accessor to the actual fitted data
+    VtxFitParams fitParams(const LHCb::Particle& cand) const ;
+
+    // Name of a particle in the decay tree
+    std::string name(const LHCb::Particle& cand) const ;
+    
+    // Total chisquare
+    double chiSquare() const { return m_chiSquare ; }
+
+    // Total number of DOFs
+    int nDof()      const ; 
+
+    // Status of vertex fit
+    int status() const { return m_status ; }
+
+    // Number of iterations used by vertex fit
+    int nIter() const { return m_niter ; }
+
+    // Compute the decay length sum of two particles in the decay tree (useful for e.g. B->DD)
+    VtxDoubleErr decayLengthSum(const LHCb::Particle&, const LHCb::Particle&) const ;
+
+    static void setVerbose(int i) { vtxverbose = i ; }
+    
+    // methods to retrieve the result in terms of LHCb::Particles
+    // (note: mother vertex is not updated, and decay length cannot be
+    // stored anywhere. Use fitParams instead)
+    LHCb::Particle getFitted(const LHCb::Particle& cand) const ;
+    void updateCand(LHCb::Particle& cand) const ;
+    
+  protected:
+    
+    // expert interface. not yet for real consumption
+    VtxFitParams fitParams(const ParticleBase& pb) const ;
+    VtxDoubleErr decayLengthSum(const ParticleBase&,const ParticleBase&) const ;
+
+    DecayChain* decaychain() { return m_decaychain ; }
+    FitParams* fitparams() { return m_fitparams ; }
+    const DecayChain* decaychain() const { return m_decaychain ; }
+    const FitParams* fitparams() const { return m_fitparams ; }
+
+    double globalChiSquare() const ;
+    const ErrCode& errCode() { return m_errCode ; }
+    
+    // must be moved to derived class or so ...
+    double add(const LHCb::Particle& cand) ; 
+    double remove(const LHCb::Particle& cand) ;
+    void updateIndex() ;
+
+    LHCb::Particle getFitted() const ;
+    LHCb::Particle getFittedTree() const ;
+    LHCb::Particle* fittedCand(const LHCb::Particle& cand, LHCb::Particle* headoftree) const ;
+    void updateTree(LHCb::Particle& cand) const ;
+    
+  private:
+    const LHCb::Particle* m_particle ;
+    DecayChain* m_decaychain ;
+    FitParams* m_fitparams ;
+    int m_status ;
+    double m_chiSquare ;
+    int m_niter ;
+    ErrCode m_errCode ;
+  } ;
+}
+
+#endif
