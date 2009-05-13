b18eb90f24f39a66afb2b3ac68cf5d79796b875a
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-05-13 16:30:44 +0000

    restructure tests and update references
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30676 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 694c1be3ae1b79c1f69462a5b86b006627fe493d

---
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -0,0 +1,4 @@
+from GaudiTest import LineSkipper
+from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
+
+preprocessor = LHCbPreprocessor + LineSkipper(["--- Reader", "--- CFMlpANN" ])
