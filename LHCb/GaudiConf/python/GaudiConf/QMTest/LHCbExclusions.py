0f91be09f9f690b27d2d96a6289f548ff61bcb62
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-05-13 15:45:35 +0000

    add QMTest exclusions file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5391 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bcfeb876d5bb73182fb62f3066fbbeb5a8d73b39

---
+++ LHCb/GaudiConf/python/GaudiConf/QMTest/LHCbExclusions.py
@@ -0,0 +1,11 @@
+from GaudiTest import normalizeExamples, RegexpReplacer, LineSkipper
+
+preprocessor = normalizeExamples + \
+    RegexpReplacer(when = "Connected to database",
+        orig = r"(\"sqlite_file:)(.*/)([\w.]+/[0-9A-Z_]{1,8}\")",
+        repl = r"\1\3") + \
+    LineSkipper(["/PARAM/ParamFiles/"]) + \
+    LineSkipper(["INFO Using TAG", "TimingAuditor.T", "RootDBase.open"]) + \
+    LineSkipper(["INFO Opened magnetic field file"]) + \
+    LineSkipper(["INFO Connecting to database"]) + \
+    LineSkipper(["INFO Disconnect from database after being idle"])
