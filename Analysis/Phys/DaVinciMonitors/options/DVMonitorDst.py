e67ec367319c7d5a1ba03af51891042459b2ee1c
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-05-14 11:32:54 +0000

    revert changes accidentally included in my last commit ...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76394 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9eda05eaccef197e15557f3a0d30239f6111cf4a

--- Analysis/Phys/DaVinciMonitors/options/DVMonitorDst.py
+++ Analysis/Phys/DaVinciMonitors/options/DVMonitorDst.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# $Id: DVMonitorDst.py,v 1.16 2009-05-14 11:25:57 jonrob Exp $
+# $Id: DVMonitorDst.py,v 1.17 2009-05-14 11:32:54 jonrob Exp $
 #
 # syntax: gaudirun.py $DAVINCIMONITORSROOT/options/DVMonitorDst.py
 #
@@ -27,12 +27,3 @@ DaVinci().EvtMax = 10000
 DaVinci().DataType = "2008" # Default is "DC06"
 DaVinci().Simulation = True
 
-EventSelector().Input   = [ ]
-
-#importOptions('/usera/jonesc/MyDataOptions/2008/Test3/13144002-DST-Local.py')
-
-#importOptions('/usera/jonesc/MyDataOptions/DC09/Production/MC09-MinBias-WithMC-LocalPFNs.py')
-
-importOptions('/usera/jonesc/MyDataOptions/DC09/Production/MC09-MinBias-WithMC-LFN.py')
-
-#MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
