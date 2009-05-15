9eec3ebadd02a3aeee8867f2cbbbf3ec30f14eae
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-05-15 16:07:06 +0000

    minor improvement
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10863 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8923695cceed44ec0c8b69bfff692024541adbff

--- Rec/Rich/RichRecQC/root/Run.C
+++ Rec/Rich/RichRecQC/root/Run.C
@@ -25,6 +25,15 @@
   //Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/GasP-1mbar/Brunel-histos.root", "m1mbarGasP", " | DC09 - -1mbar Expected Gas Pressure" );
 
   //Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/GasP+2mbar/Brunel-histos.root", "p2mbarGasP", " | DC09 - +2mbar Expected Gas Pressure" );
-  Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/GasP-2mbar/Brunel-histos.root", "m2mbarGasP", " | DC09 - -2mbar Expected Gas Pressure" );
+  //Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/GasP-2mbar/Brunel-histos.root", "m2mbarGasP", " | DC09 - -2mbar Expected Gas Pressure" );
+
+
+
+
+  //Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/AllHPDs/Brunel-histos.root", "noDisabledHPDs", " | DC09 | All HPDs" );
+
+  // Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/MissingHPDsTopBotRICH2/Brunel-histos.root", "missingHPDsTopBotRICH2", " | DC09 | RICH2 Top/Bot 1 HPD Rows missing" );
+
+  Rich::performance( "/usera/jonesc/NFS/RootFiles/DC09/MissingHPDsTop2Bot2RICH2/Brunel-histos.root", "missingHPDsTop2Bot2RICH2", " | DC09 | RICH2 Top/Bot 2 HPD Rows missing" );
 
 }
