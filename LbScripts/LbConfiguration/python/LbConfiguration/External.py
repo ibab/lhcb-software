1b46973dfe1c3d38348d0aeb355ce6928552abba
hmdegaud <hmdegaud@cern.ch>
2009-05-19 17:26:19 +0000

    added the version of tbroadcast
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@13727 4525493e-7705-40b1-a816-d608a930855b

--- LbScripts/LbConfiguration/python/LbConfiguration/External.py
+++ LbScripts/LbConfiguration/python/LbConfiguration/External.py
@@ -1,4 +1,5 @@
 """ General LHCb configuration for external software """
 
-Python_version = "2.5"
-CMT_version    = "v1r20p20070208"
+Python_version     = "2.5"
+CMT_version        = "v1r20p20070208"
+tbroadcast_version = "v2.0.5"
