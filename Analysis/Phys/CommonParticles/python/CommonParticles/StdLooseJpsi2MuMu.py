5443cd65ff42fa27bf1379c90ba88e2cb3fafc78
Greig Cowan <g.cowan@ed.ac.uk>
2009-05-20 15:40:03 +0000

    Added UnbiasedJpsi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76448 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 940a9930a633751d7f29205620fcc79df86ca291

--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2MuMu.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2MuMu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseJpsi2MuMu.py,v 1.3 2009-04-22 14:17:39 pkoppenb Exp $ 
+# $Id: StdLooseJpsi2MuMu.py,v 1.4 2009-05-20 15:40:03 gcowan Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseJpsi2MuMu.py
 #  configuration file for 'Standard Loose Jpsi2MuMu' 
@@ -11,10 +11,11 @@
 Configuration file for 'Standard Loose Jpsi2MuMu'
 """
 __author__  = "Patrick Koppenburg"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.3 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.4 $"
 # =============================================================================
 __all__ = (
     'StdLooseJpsi2MuMu' ,
+    'StdUnbiasedJpsi2MuMu' ,
     'locations'
     )
 # =============================================================================
@@ -31,8 +32,25 @@ StdLooseJpsi2MuMu.DecayDescriptor = "J/psi(1S) -> mu+ mu-"
 StdLooseJpsi2MuMu.CombinationCut = "ADAMASS('J/psi(1S)')<100*MeV"
 StdLooseJpsi2MuMu.MotherCut = "(VFASPF(VCHI2/VDOF)<25)"
 
+## ============================================================================
+#  Filter the loose Jpsi for the unbiased Jpsi selections
+#  @author Greig Cowan
+#  @date 2009-05-20
+StdUnbiasedJpsi2MuMu = FilterDesktop("StdUnbiasedJpsi2MuMu")
+StdUnbiasedJpsi2MuMu.InputLocations =["StdLooseJpsi2MuMu"]
+
+StdUnbiasedJpsi2MuMu.Code = "  (MINTREE('mu+'==ABSID, PT) > 500.*MeV)" \
+                            "& (MINTREE('mu+'==ABSID, P)  > 0.*MeV)" \
+                            "& (MAXTREE('mu+'==ABSID, TRCHI2DOF) < 5.0)" \
+                            "& (MINTREE('mu+'==ABSID, PIDmu) > -10.0)" \
+                            "& (MAXTREE('mu+'==ABSID, PIDK) < 10.0)" \
+                            "& (ADMASS('J/psi(1S)') < 50.*MeV)" \
+                            "& (PT > 1000.*MeV)" \
+                            "& (VFASPF(VCHI2/VDOF) < 6.0)"
+
+
 ## configure Data-On-Demand service 
-locations = updateDoD ( StdLooseJpsi2MuMu )
+locations = updateDoD ( StdLooseJpsi2MuMu, StdUnbiasedJpsi2MuMu )
 
 ## ============================================================================
 if '__main__' == __name__ :
