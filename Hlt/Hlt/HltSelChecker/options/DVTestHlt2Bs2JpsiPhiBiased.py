24f732bc86ba208f298947dd2802d334d007b28b
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-05-20 10:05:24 +0000

    new test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@76434 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4ba24938b127455377e327feac51761d78b60131

---
+++ Hlt/Hlt/HltSelChecker/options/DVTestHlt2Bs2JpsiPhiBiased.py
@@ -0,0 +1,55 @@
+### @Id: $
+ #
+ #  Test file for HLT B -> mu mu X selection
+ #
+ #  @author P. Koppenburg
+ #  @date 2007-07-20
+###
+from Gaudi.Configuration import *
+from Configurables import HltCorrelations, FilterTrueTracks, MCDecayFinder, GaudiSequencer, PhysDesktop, DecayTreeTuple, PrintHeader, CheckSelResult
+#--------------------------------------------------------------
+signal = "Bs2JpsiPhi"
+#
+# True filter criterion - will only run HLT on TRUE signal
+#
+importOptions( "$HLTSELCHECKERROOT/options/FilterTrueTracks.py")
+FilterTrueTracks().addTool(MCDecayFinder())
+FilterTrueTracks().MCDecayFinder.Decay =  "[B_s0 -> (phi(1020) -> ^K+ ^K- {,gamma}{,gamma}) (J/psi(1S) -> ^mu+ ^mu- {,gamma}{,gamma})]cc"
+#
+# Set the following to false if you want only events with a signal
+# fully reconstructed in the HLT
+#
+GaudiSequencer("SeqHlt2TruthFilter").IgnoreFilterPassed = True
+#
+# Overwrite input - uncomment to run HLT on TRUE signal only
+#
+# importOptions( "$HLTSELCHECKERROOT/options/OverwriteWithTruth.py")
+#
+# Monitoring
+#
+moni = GaudiSequencer("Hlt2MonitorSeq")
+moni.IgnoreFilterPassed = True
+moni.Context = "HLT"
+importOptions( "$HLTSELECTIONSROOT/options/Hlt2Correlations.py")
+importOptions( "$HLTSELECTIONSROOT/options/Hlt2MonitorPlots.py")
+###
+ # Tuple
+###
+importOptions( "$HLTSELCHECKERROOT/options/Hlt2DecayTreeTuple.py")
+DecayTreeTuple("Hlt2DecayTreeTuple").InputLocations = ["Hlt2Bs2JpsiPhiSignalCombineParticlesCombine"]
+DecayTreeTuple("Hlt2DecayTreeTuple").Decay = "[B_s0 -> (^J/psi(1S) => ^mu+ ^mu-) (^phi(1020) -> ^K+ ^K-)]cc"
+#
+# Configuration
+#
+from Configurables import DaVinci
+DaVinci().EvtMax = 1000
+DaVinci().HltType = "Hlt1+Hlt2"                # Both Hlt levels
+DaVinci().Hlt2Requires = 'L0'                  # Ignore Hlt1 in 2
+DaVinci().ReplaceL0BanksWithEmulated = False   # Redo L0
+DaVinci().DataType = "DC06" 
+DaVinci().Simulation = True 
+DaVinci().TupleFile =  "HLT-"+signal+".root"
+DaVinci().HistogramFile = "DVHlt2-"+signal+".root"
+DaVinci().MoniSequence += [ moni, DecayTreeTuple("Hlt2DecayTreeTuple") ]
+DaVinci().Input = [
+  "DATAFILE='PFN:castor:/castor/cern.ch/user/g/gcowan/for_HLT_physics_meet/offline_Bs2JpsiPhi_biased_latest.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
