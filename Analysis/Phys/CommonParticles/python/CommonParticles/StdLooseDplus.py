1c2bebd839a376c013b096f1acf2c9e487df414a
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2009-05-20 18:50:40 +0000

    Updated cuts for standard D+
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76454 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4147afd0aa4841c275ea702d966df8ff09fbeee7

--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseDplus.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseDplus.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseDplus.py,v 1.3 2009-04-22 14:17:39 pkoppenb Exp $ 
+# $Id: StdLooseDplus.py,v 1.4 2009-05-20 18:50:40 gligorov Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseDplus.py
 #  configuration file for 'Standard Loose Dplus' 
@@ -11,12 +11,13 @@
 Configuration file for 'Standard Loose Dplus'
 """
 __author__  = "Patrick Koppenburg"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.3 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.4 $"
 # =============================================================================
 __all__ = (
     'StdLooseDplus2KPiPi' ,
     'StdLooseDplus2KKPi' ,
     'StdLooseDplus2PiPiPi' ,
+    'StdLooseDplus2KPiPiOppSignPi' ,
     'locations'
     )
 # =============================================================================
@@ -30,11 +31,11 @@ StdLooseDplus2KPiPi = CombineParticles ( 'StdLooseDplus2KPiPi' )
 
 StdLooseDplus2KPiPi.InputLocations = [ "StdLooseKaons", "StdLoosePions" ]
 StdLooseDplus2KPiPi.DecayDescriptor = "[D+ -> K- pi+ pi+]cc" 
-StdLooseDplus2KPiPi.DaughtersCuts = { "K+"  : "ALL", "pi+" : "ALL" }
-#Note that the cut of 300 MeV around the *D_s* mass is NOT a typo, it is motivated by
-#the requirements of the B->Dh HLT2 selection
-StdLooseDplus2KPiPi.CombinationCut = "(ADAMASS('D_s+')<300*MeV) & (APT>1.5*GeV)"
-StdLooseDplus2KPiPi.MotherCut = "(VFASPF(VCHI2/VDOF) < 10 ) & (BPVDIRA > 0) " 
+StdLooseDplus2KPiPi.DaughtersCuts = { "K+"  : "((TRCHI2DOF<10) & (PT >250*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.))", "pi+" : "((TRCHI2DOF<10) & (PT >250*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.))" }
+#Need an upper mass window of 100 MeV for background estimation
+#and a lower mass window of 200 to catch the D+ and Ds
+StdLooseDplus2KPiPi.CombinationCut = "(ADAMASS('D_s+')<200*MeV) & (APT>1.*GeV)"
+StdLooseDplus2KPiPi.MotherCut = "(VFASPF(VCHI2/VDOF) < 10 ) & (M < 2070.*MeV) " 
 
 ## configure Data-On-Demand service 
 locations = updateDoD ( StdLooseDplus2KPiPi )
@@ -45,7 +46,7 @@ locations = updateDoD ( StdLooseDplus2KPiPi )
 StdLooseDplus2KKPi = StdLooseDplus2KPiPi.clone("StdLooseDplus2KKPi")
 StdLooseDplus2KKPi.DecayDescriptor = "[D+ -> K- K+ pi+]cc"
 ## configure Data-On-Demand service 
-locations = updateDoD ( StdLooseDplus2KPiPi )
+locations = updateDoD ( StdLooseDplus2KKPi )
 ##########################################################################################
 # D+ -> 3 pi is a clone of D+ -> K Pi Pi 
 #
@@ -53,7 +54,14 @@ StdLooseDplus2PiPiPi = StdLooseDplus2KPiPi.clone("StdLooseDplus2PiPiPi")
 StdLooseDplus2PiPiPi.InputLocations = [ "StdLoosePions" ]
 StdLooseDplus2PiPiPi.DecayDescriptor = "[D+ -> pi- pi+ pi+]cc"
 ## configure Data-On-Demand service 
-locations = updateDoD ( StdLooseDplus2KPiPi )
+locations = updateDoD ( StdLooseDplus2PiPiPi )
+##########################################################################################
+# Also need the opposite sign pions mode for the Ds 
+#
+StdLooseDplus2KPiPiOppSignPi = StdLooseDplus2KPiPi.clone("StdLooseDplus2KPiPiOppSignPi")
+StdLooseDplus2KPiPiOppSignPi.DecayDescriptor = "[D+ -> pi- pi+ K+]cc"
+## configure Data-On-Demand service
+locations = updateDoD ( StdLooseDplus2KPiPiOppSignPi )
 
 ## ============================================================================
 if '__main__' == __name__ :
