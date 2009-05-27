5102b586ef061cf3c76e3c6884ba3feb431fdca3
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-05-27 18:43:13 +0000

    new HepMC-decay finder tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@51032 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: efaf7440fa29c35c21dab8b49c8689cf67b52b16

---
+++ LHCb/Phys/LoKiGen/src/Components/GenDecay.cpp
@@ -0,0 +1,137 @@
+// $Id: GenDecay.cpp,v 1.1 2009-05-27 18:43:13 ibelyaev Exp $
+// ============================================================================
+// Include
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h"
+// ============================================================================
+// Event
+// ============================================================================
+#include "HepMC/GenParticle.h"
+// ============================================================================
+// LoKi 
+// ============================================================================
+#include "LoKi/IGenDecay.h"
+#include "LoKi/DecayBase.h"
+#include "LoKi/Trees.h"
+#include "LoKi/TreeHelpers.h"
+#include "LoKi/GenTreesFactory.h"
+// ============================================================================
+namespace LoKi 
+{
+  // ==========================================================================
+  /** @class GenDecay
+   *  simple implementation of interface Decays::IGenDecay
+   *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+   *  @date 2009-05-27
+   */
+  class GenDecay
+    : public              LoKi::DecayBase 
+    , public virtual Decays::IGenDecay 
+  {
+    // ========================================================================
+    /// the friend factory for instantiation
+    friend class ToolFactory<LoKi::GenDecay> ;                   // the factory
+    // ========================================================================
+  public:
+    // ========================================================================
+    /** create the decay tree from the descriptor
+     *  @param descriptor (INPUT)  the decay descriptor 
+     *  @param tree       (OUTPUT) the decay tree 
+     */
+    virtual Tree tree ( const std::string& decay ) const ;
+    // ========================================================================
+  public:
+    // ========================================================================
+    /// build the node form the node descriptor
+    virtual Decays::Node node ( const std::string& descriptor ) const 
+    { return LoKi::DecayBase::node ( descriptor ) ; }
+    // ========================================================================
+  protected:
+    // ========================================================================
+    /** constructor form the type, name and parent 
+     *  @param type the tool type (?)
+     *  @param name the instance name 
+     *  @param parent the parent 
+     */
+    GenDecay ( const std::string& type   ,                 //     the tool type  
+               const std::string& name   ,                 // the instance name 
+               const IInterface*  parent )                 //        the parent
+      : LoKi::DecayBase ( type , name , parent ) 
+    {
+      declareInterface<Decays::IGenDecay>  ( this ) ;
+      declareInterface<Decays::IDecayNode> ( this ) ;
+    }
+    /// virtual & protected destructor 
+    virtual ~GenDecay() {}                    // virtual & protected destructor 
+    // ========================================================================
+  private:
+    // ========================================================================
+    /// the default constructor is disabled 
+    GenDecay ( ) ;                       // the default constructor is disabled 
+    /// the copy constructor is disabled 
+    GenDecay ( const GenDecay& ) ;          // the copy constructor is disabled 
+    /// the assignement operator is disabled 
+    GenDecay& operator=( const GenDecay& ) ;         // no assignement operator
+    // ========================================================================
+  } ; //                                                  end of class GenDecay
+  // ==========================================================================
+} //                                                      end of namespace LoKi
+// ============================================================================
+// create the decay tree from the descriptor
+// ============================================================================
+Decays::IGenDecay::Tree 
+LoKi::GenDecay::tree ( const std::string& decay ) const 
+{
+  MsgStream& err = error() ;
+  
+  typedef Decays::Trees::Types_<const HepMC::GenParticle*>  Types ;
+  
+  std::string _decay = _makeCC ( decay ) ;
+  
+  // 1) parse the intout into "generic tree" 
+  Decays::Parsers::Tree tree ;
+  
+  StatusCode sc = _parse  ( tree , _decay ) ;
+  if ( sc.isFailure() ) 
+  {
+    Error("Error from _parse('" + _decay + "')" , sc ) ;
+    return  Types::Invalid() ;                                         // RETURN 
+  }
+  // 2) convert it into reasonable decay tree
+  Tree mctree = Types::Invalid() ;
+  
+  sc = Decays::Trees::factory ( mctree , tree , err.stream() )  ;
+  if ( sc.isFailure() ) 
+  {
+    err << endmsg ;
+    Error ( "Unable to create the tree from '" + tree.toString() + "'", sc ) ;  
+    return  Types::Invalid() ;                                        // RETURN
+  }
+  
+  /// validate the tree 
+  if ( !mctree.valid() ) 
+  {
+    sc = mctree.validate ( ppSvc () ) ;
+    if ( sc.isFailure() ) 
+    {
+      Error ( "Unable to validate the tree '" + mctree.toString() + "'" , sc ) ;  
+      return  Types::Invalid() ;                                        // RETURN
+    }
+  }
+  // finally: 
+  return mctree ;
+}
+// ============================================================================
+
+
+// ============================================================================
+//                                                the factory for instantiation 
+// ============================================================================
+DECLARE_NAMESPACE_TOOL_FACTORY(LoKi,GenDecay)
+// ============================================================================
+
+// ============================================================================
+// The END 
+// ============================================================================
