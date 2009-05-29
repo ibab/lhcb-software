13415f03f28d5747bd2947427e410c9464437322
Markus Frank <frankb@cern.ch>
2009-05-29 09:35:54 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99469 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cc529b826ffcfff19b45de38bd79e345c13b04f5

--- Online/Online/OnlineKernel/OnlineKernel/NET/DataTransfer.h
+++ Online/Online/OnlineKernel/OnlineKernel/NET/DataTransfer.h
@@ -1,6 +1,40 @@
 #ifndef ONLINEKERNEL_NET_DATATRANSFER_H
 #define ONLINEKERNEL_NET_DATATRANSFER_H
 
+extern "C" {
+  struct NET;
+  struct netheader_t;
+  struct netentry_t;
+
+  enum NetConnectionType  {
+    NET_CLIENT = 1,
+    NET_SERVER = 2
+  };
+  struct netheader_t {
+    unsigned int    size;
+    unsigned int    msg_type;
+    unsigned int    facility;
+    unsigned int    magic;
+    unsigned int    hash;
+    char            name[128];
+    //netheader_t() : size(0), msg_type(NET_MSG_DATA), facility(0), magic(NET_MAGIC), hash(0) {}
+    //~netheader_t() {}
+    //void net_to_host();
+    //void fill(const char* nam, unsigned int hash, size_t size, int fac, int mtype, const void *buf_ptr);
+  };
+
+  typedef void (*net_handler_t)(netentry_t* e, const netheader_t& hdr, void* param);
+  NET* network_init(const char* proc, NetConnectionType type=NET_SERVER);
+  void network_close(NET* net);
+  void* network_lock(NET* net);
+  void network_unlock(NET* net, void* lock);
+  int network_subscribe(NET* net, void* param, unsigned int fac, net_handler_t data, net_handler_t death);
+  int network_unsubscribe(NET* net, void* param, unsigned int fac);
+  int network_receive(NET* net, netentry_t* e, void* buff);
+  int network_send(NET* net, const void* buff, size_t size, const char* dest, unsigned int fac);
+  int network_send2(NET* net, const void* buff, size_t size, netentry_t* dest, unsigned int fac);
+}
+
 #include <string>
 
 namespace DataTransfer  {
@@ -17,35 +51,27 @@ namespace DataTransfer  {
     NET_MSG_DATA       = 16,  // Also message type
     NET_MAGIC          =  0xDEADCAFE
   };
-  enum NetConnectionType  {
-    NET_CLIENT = 1,
-    NET_SERVER = 2
-  };
-  struct NET;
-  struct netheader_t;
-  struct netentry_t;
+  using ::NET_CLIENT;
+  using ::NET_SERVER;
 
-  struct netheader_t {
-    unsigned int    size;
-    unsigned int    msg_type;
-    unsigned int    facility;
-    unsigned int    magic;
-    unsigned int    hash;
-    char            name[128];
-    netheader_t() : size(0), msg_type(NET_MSG_DATA), facility(0), magic(NET_MAGIC), hash(0) {}
-    ~netheader_t() {}
-    void net_to_host();
-    void fill(const char* nam, unsigned int hash, size_t size, int fac, int mtype, const void *buf_ptr);
-  };
-  typedef void (*net_handler_t)(netentry_t* e, const netheader_t& hdr, void* param);
-  NET* net_init(const std::string& proc, NetConnectionType type=NET_SERVER);
-  void net_close(NET* net);
-  void* net_lock(NET* net);
-  void net_unlock(NET* net, void* lock);
-  int net_subscribe(NET* net, void* param, unsigned int fac, net_handler_t data, net_handler_t death);
-  int net_unsubscribe(NET* net, void* param, unsigned int fac);
-  int net_receive(NET* net, netentry_t* e, void* buff);
-  int net_send(NET* net, const void* buff, size_t size, const std::string& dest, unsigned int fac);
-  int net_send(NET* net, const void* buff, size_t size, netentry_t* dest, unsigned int fac);
+  using ::NET;
+  using ::netheader_t;
+  using ::netentry_t;
+  using ::net_handler_t;
+  inline NET* net_init(const std::string& proc, NetConnectionType type=NET_SERVER) 
+    {   return network_init(proc.c_str(),type); }
+  inline void net_close(NET* net) {  network_close(net); }
+  inline void* net_lock(NET* net) {  return network_lock(net); }
+  inline void net_unlock(NET* net, void* lock) {  return network_unlock(net,lock); }
+  inline int net_subscribe(NET* net, void* param, unsigned int fac, net_handler_t data, net_handler_t death)
+    { return network_subscribe(net,param,fac,data,death);  }
+  inline int net_unsubscribe(NET* net, void* param, unsigned int fac)
+    { return network_unsubscribe(net,param,fac);  }
+  inline int net_receive(NET* net, netentry_t* e, void* buff)
+    { return network_receive(net,e,buff);  }
+  inline int net_send(NET* net, const void* buff, size_t size, const std::string& dest, unsigned int fac)
+    { return network_send(net,buff,size,dest.c_str(),fac);  }
+  inline int net_send(NET* net, const void* buff, size_t size, netentry_t* dest, unsigned int fac)
+    { return network_send2(net,buff,size,dest,fac);  }
 }
 #endif  /* ONLINEKERNEL_NET_DATATRANSFER_H */
