b9674d9440f1f044adc9cdaa6050aea259526577
Gloria Corti <gloria.corti@cern.ch>
2009-06-02 09:31:43 +0000

    change dbase tags
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94276 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 58ec29300557f6a060de1e034d5581edda43bd2f

--- Gauss/Sim/Gauss/options/Gauss-MC09.py
+++ Gauss/Sim/Gauss/options/Gauss-MC09.py
@@ -1,6 +1,6 @@
 from Gauss.Configuration import *
 
-LHCbApp().DDDBtag = "head-20090330"
+LHCbApp().DDDBtag = "MC09-20090526"
 LHCbApp().CondDBtag = "sim-20090402-vc-mu100"
 
 importOptions('$GAUSSOPTS/Gauss.py')
