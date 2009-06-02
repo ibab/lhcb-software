43af83742efd7e9384f54ab250ced91d4facf83a
jpalac <jpalac@cern.ch>
2009-06-02 16:11:58 +0000

    Add absolute paths to be able to run as standalone job, outside of QMT framework
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@76659 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 70a712c6ea06de86918e91a6fd40b2d57066b41c

--- DaVinci/DaVinciSys/tests/options/DVTestReadDst.py
+++ DaVinci/DaVinciSys/tests/options/DVTestReadDst.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DVTestReadDst.py,v 1.3 2009-04-29 13:27:53 pkoppenb Exp $
+# $Id: DVTestReadDst.py,v 1.4 2009-06-02 16:11:58 jpalac Exp $
 #
 # Options for a typical DaVinci job creating DSTs
 #
@@ -28,9 +28,9 @@ ph = PrintHeader()
 DaVinci().EvtMax = -1
 DaVinci().PrintFreq = 1 
 DaVinci().DataType = "2008" # Default is "DC06"
-DaVinci().Input = [ "DATAFILE='PFN:Jpsi_3050_3150.dst' TYP='POOL_ROOTTREE' OPT='READ'"
-                 ,  "DATAFILE='PFN:Jpsi_2600_3200.dst' TYP='POOL_ROOTTREE' OPT='READ'"
-                 ,  "DATAFILE='PFN:Jpsi_3000_3500.dst' TYP='POOL_ROOTTREE' OPT='READ'" 
+DaVinci().Input = [ "DATAFILE='PFN:$DAVINCISYSROOT/tests/qmtest/Jpsi_3050_3150.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+                 ,  "DATAFILE='PFN:$DAVINCISYSROOT/tests/qmtest/Jpsi_2600_3200.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+                 ,  "DATAFILE='PFN:$DAVINCISYSROOT/tests/qmtest/Jpsi_3000_3500.dst' TYP='POOL_ROOTTREE' OPT='READ'" 
                     ]
 DaVinci().UserAlgorithms = [  ph, pJpsi ]
 ########################################################################
