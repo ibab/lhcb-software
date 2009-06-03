94b010683cec66e0637051e0a985bb19775e5cf3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-06-03 09:01:41 +0000

    ! 2009-06-03 - Chris Jones  - Sychronise the configurable with the ENN Configurable.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10977 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 568254c8780f3b9b74f60126dd41367da8341889

--- Rec/Rich/RichMarkovRingFinder/python/RichMarkovRingFinder/Configuration.py
+++ Rec/Rich/RichMarkovRingFinder/python/RichMarkovRingFinder/Configuration.py
@@ -4,7 +4,7 @@
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.7 2009-05-22 15:35:45 jonrob Exp $"
+__version__ = "$Id: Configuration.py,v 1.8 2009-06-03 09:01:41 jonrob Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from RichKernel.Configuration import *
@@ -20,37 +20,46 @@ class RichMarkovRingFinderConf(RichConfigurableUser):
         ## Steering options
     __slots__ = {
         "Context"             :    "Offline"   # The context within which to run
-       ,"Panels"              : ["Rich1Top","Rich1Bottom","Rich2Left","Rich2Right"]
-       ,"MaxHitsInPanel"      : 300
-       ,"MaxHitsInHPD"        : 30
+       ,"Panels"              : [  "Rich1GasTop","Rich1GasBottom"
+                                  ,"Rich2GasLeft","Rich2GasRight"
+                                  ]
+       ,"MaxHitsInPanel"      : [ 400, 400, 400 ]
+       ,"MaxHitsInHPD"        : [ 50,  50,  50  ]
        ,"AssociateToSegments" : True
-       ,"Sequencer"           : None # The sequencer to add the RICH MCMC algorithms to
+       ,"Sequencer"           : None # The sequencer to add the RICH Markov ring finder algorithms to
        ,"OutputLevel"         : INFO
         }
 
-    ## Access the finder for RICH1 top panel
-    def rich1TopFinder(self):
+    ## Access the finder for RICH1 top panel C4F10
+    def rich1GasTopFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich1TopPanel
         cont = self.getProp("Context")
-        return Rich__Rec__MarkovRingFinder__Rich1TopPanel(cont+"MFinderR1Top")
+        return Rich__Rec__MarkovRingFinder__Rich1TopPanel(cont+"MarkovFinderR1GasTop")
 
-    ## Access the finder for RICH1 bottom panel
-    def rich1BottomFinder(self):
+    ## Access the finder for RICH1 bottom panel C4F10
+    def rich1GasBottomFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich1BottomPanel
         cont = self.getProp("Context")
-        return Rich__Rec__MarkovRingFinder__Rich1BottomPanel(cont+"MFinderR1Bottom")
+        return Rich__Rec__MarkovRingFinder__Rich1BottomPanel(cont+"MarkovFinderR1GasBottom")
 
-    ## Access the finder for RICH2 left panel
-    def rich2LeftFinder(self):
+    ## Access the finder for RICH2 left panel CF4
+    def rich2GasLeftFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich2LeftPanel
         cont = self.getProp("Context")
-        return Rich__Rec__MarkovRingFinder__Rich2LeftPanel(cont+"MFinderR2Left")
+        return Rich__Rec__MarkovRingFinder__Rich2LeftPanel(cont+"MarkovFinderR2GasLeft")
 
-    ## Access the finder for RICH2 right panel
-    def rich2RightFinder(self):
+    ## Access the finder for RICH2 right panel CF4
+    def rich2GasRightFinder(self):
         from Configurables import Rich__Rec__MarkovRingFinder__Rich2RightPanel
         cont = self.getProp("Context")
-        return Rich__Rec__MarkovRingFinder__Rich2RightPanel(cont+"MFinderR2Right")
+        return Rich__Rec__MarkovRingFinder__Rich2RightPanel(cont+"MarkovFinderR2GasRight")
+
+    ## Turn on histograming for all algs
+    def enableHistos( self, enableHistos = False ) :
+        self.rich1GasTopFinder().HistoProduce     = enableHistos
+        self.rich1GasBottomFinder().HistoProduce  = enableHistos
+        self.rich2GasLeftFinder().HistoProduce    = enableHistos
+        self.rich2GasRightFinder().HistoProduce   = enableHistos
 
     ## @brief Apply the configuration to the given GaudiSequencer
     def applyConf(self):
@@ -66,62 +75,68 @@ class RichMarkovRingFinderConf(RichConfigurableUser):
             if len(panels) == 0 :
                 raise RuntimeError("ERROR : No panels set to find Markov Rings")
             else:
+                
                 # maximum hits in each panel for finding
-                maxhits = self.getProp("MaxHitsInPanel")
+                maxhits    = self.getProp("MaxHitsInPanel")
                 maxhpdhits = self.getProp("MaxHitsInHPD")
-      
+
+                # Ring locations
+                AllRingsLoc  = "Rec/Rich/Markov/RingsAll"
+                BestRingsLoc = "Rec/Rich/Markov/RingsBest"
+                IsoRingsLoc  = "Rec/Rich/Markov/RingsIsolated"
+
                 # context
                 cont = self.getProp("Context")
 
                 setOutLevel = self.isPropertySet("OutputLevel")
                 level = self.getProp("OutputLevel")
 
-                # The finders, for each HPD panel
-                if "Rich1Top" in panels :
-                    alg                = self.rich1TopFinder()
-                    alg.MaxHitsInEvent = maxhits
-                    alg.MaxHitsInHPD   = maxhpdhits
-                    alg.RingLocation   = "Rec/Rich/Markov/RingsAll"
+                # The finders, for each HPD panel and radiator
+                if "Rich1GasTop" in panels :
+                    alg                = self.rich1GasTopFinder()
+                    alg.MaxHitsInEvent = maxhits[1]
+                    alg.MaxHitsInHPD   = maxhpdhits[1]
+                    alg.RingLocation   = AllRingsLoc
                     if setOutLevel : alg.OutputLevel = level
                     sequence.Members  += [alg]
-                if "Rich1Bottom" in panels :
-                    alg                = self.rich1BottomFinder()
-                    alg.MaxHitsInEvent = maxhits
-                    alg.MaxHitsInHPD   = maxhpdhits
-                    alg.RingLocation   = "Rec/Rich/Markov/RingsAll"
+                if "Rich1GasBottom" in panels :
+                    alg                = self.rich1GasBottomFinder()
+                    alg.MaxHitsInEvent = maxhits[1]
+                    alg.MaxHitsInHPD   = maxhpdhits[1]
+                    alg.RingLocation   = AllRingsLoc
                     if setOutLevel : alg.OutputLevel = level
                     sequence.Members  += [alg]
-                if "Rich2Left" in panels :
-                    alg                = self.rich2LeftFinder()
-                    alg.MaxHitsInEvent = maxhits
-                    alg.MaxHitsInHPD   = maxhpdhits
-                    alg.RingLocation   = "Rec/Rich/Markov/RingsAll"
+                if "Rich2GasLeft" in panels :
+                    alg                = self.rich2GasLeftFinder()
+                    alg.MaxHitsInEvent = maxhits[2]
+                    alg.MaxHitsInHPD   = maxhpdhits[2]
+                    alg.RingLocation   = AllRingsLoc
                     if setOutLevel : alg.OutputLevel = level
                     sequence.Members  += [alg]
-                if "Rich2Right" in panels :
-                    alg                = self.rich2RightFinder()
-                    alg.MaxHitsInEvent = maxhits
-                    alg.MaxHitsInHPD   = maxhpdhits
-                    alg.RingLocation   = "Rec/Rich/Markov/RingsAll"
+                if "Rich2GasRight" in panels :
+                    alg                = self.rich2GasRightFinder()
+                    alg.MaxHitsInEvent = maxhits[2]
+                    alg.MaxHitsInHPD   = maxhpdhits[2]
+                    alg.RingLocation   = AllRingsLoc
                     if setOutLevel : alg.OutputLevel = level
                     sequence.Members  += [alg]
 
                 # Attempt to associated rings to segments
                 if self.getProp("AssociateToSegments"):
                     from Configurables import Rich__Rec__TracklessRingSegmentAssociationAlg
-                    alg               = Rich__Rec__TracklessRingSegmentAssociationAlg(cont+"MRingsSegAssoc")
-                    alg.InputRings    = "Rec/Rich/Markov/RingsAll"
+                    alg               = Rich__Rec__TracklessRingSegmentAssociationAlg(cont+"MarkovRingsSegAssoc")
+                    alg.InputRings    = AllRingsLoc
                     if setOutLevel : alg.OutputLevel = level
                     sequence.Members += [alg]
 
                 # Post finding cleaning and ring selection
-                filter             = Rich__Rec__TracklessRingFilterAlg(cont+"BestMRings")
-                filter.InputRings  = "Rec/Rich/Markov/RingsAll"
-                filter.OutputRings = "Rec/Rich/Markov/RingsBest"
+                filter             = Rich__Rec__TracklessRingFilterAlg(cont+"BestMarkovRings")
+                filter.InputRings  = AllRingsLoc
+                filter.OutputRings = BestRingsLoc
                 if setOutLevel : filter.OutputLevel = level
-                iso                = Rich__Rec__TracklessRingIsolationAlg(cont+"IsolatedMRings")
-                iso.InputRings     = "Rec/Rich/Markov/RingsBest"
-                iso.OutputRings    = "Rec/Rich/Markov/RingsIsolated"
+                iso                = Rich__Rec__TracklessRingIsolationAlg(cont+"IsolatedMarkovRings")
+                iso.InputRings     = BestRingsLoc
+                iso.OutputRings    = IsoRingsLoc
                 if setOutLevel : iso.OutputLevel = level
                 sequence.Members  += [filter,iso]
             
