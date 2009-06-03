9d58190e6f19b788f5803174368c365ce787aaf1
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-06-03 06:20:31 +0000

    Fix scoping problem on Windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@27787 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9672d242427ab441bbf03e187b5b7849ebd1d520

--- Lbcom/ST/STTools/src/STSelectChannelIDByStatus.cpp
+++ Lbcom/ST/STTools/src/STSelectChannelIDByStatus.cpp
@@ -1,4 +1,4 @@
-// $Id: STSelectChannelIDByStatus.cpp,v 1.1 2009-03-14 09:16:35 mneedham Exp $
+// $Id: STSelectChannelIDByStatus.cpp,v 1.2 2009-06-03 06:20:31 cattanem Exp $
  
 // Kernel
 #include "GaudiKernel/ToolFactory.h"
@@ -36,10 +36,10 @@ StatusCode STSelectChannelIDByStatus::initialize() {
   if (sc.isFailure()) return Error("Failed to initialize", sc);
 
   // to save time convert the strings to a list of status
-  const Status::StatusToStringMap& statusMap = Status::statusDescription();
+  const ::Status::StatusToStringMap& statusMap = ::Status::statusDescription();
   BOOST_FOREACH(std::string statusString, m_statusNames) {
     // try to find the status in the map
-    Status::StatusToStringMap::const_iterator iterMap = statusMap.begin();
+	  ::Status::StatusToStringMap::const_iterator iterMap = statusMap.begin();
     while (iterMap != statusMap.end()){
       if (iterMap->second == statusString) break;
       ++iterMap;
