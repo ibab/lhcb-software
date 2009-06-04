64c5f04fca872c8f459b08a5184151bc053577e3
rlambert <rlambert@cern.ch>
2009-06-04 12:16:53 +0000

    Added small amount of documentation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76780 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e45aa9127cd6d09626d2f458684bc9c9383d9948

--- Analysis/Phys/DecayTreeTuple/src/MCTupleToolDecayType.h
+++ Analysis/Phys/DecayTreeTuple/src/MCTupleToolDecayType.h
@@ -1,4 +1,4 @@
-// $Id: MCTupleToolDecayType.h,v 1.1 2009-06-04 10:54:45 rlambert Exp $
+// $Id: MCTupleToolDecayType.h,v 1.2 2009-06-04 12:16:53 rlambert Exp $
 #ifndef TUPLETOOLMCDECAYTYPE_H
 #define TUPLETOOLMCDECAYTYPE_H 1
 
@@ -30,85 +30,171 @@
  *  There are two methods used to find the event type   (Pseudo or Slow)
  *   and several places you can look for the event type (GenHeader, FullEvent, MCAssociate)
  * 
+ *  CONFIGURATION:
+
+ *  You will also need to configure the EvtTypeSvc(), which takes the list of all possible decays from Gauss
+ 
+ *  EvtTypeSvc().EvtTypesFile='...fullpath.../table_event.txt'
+ 
+ *  Usually the file resides in $DECFILESROOT/doc/table_event.txt, copy and ship it with your job
+ 
  *  METHODS:
+ 
  *
+
  *  A) The Pseudo method: guesses the event type, by constructing the possible types
+
  *                        based on the definition of event types in the LHCb note
+
  *                        this is fast, works on all decays, but is ambiguous.
+
  *                        To look for inclusive types, this is the best method.
+
  *
+
  *  B) The Slow Method  : Finds the event type based on the decay string.
+
  *                        This is accurate, but relies on the string being correct
+
  *                        in the .dec file of this decay. Most of the time this is not the case.
+
  *                        To check the decay file you've written is correct, this is the best method.
+
  *
+
  *  WHERE TO LOOK:
+
  * 
+
  *  1) The GenHeader    : The GenHeader contains the event type which was generated.
+
  *                        If this is all you need, and you want no information on the other
+
  *                        possible decays in this event, you can stop here.
+
  *                        If all you want to do is find the heaviest quark in the event,
+
  *                        use the TupleToolGeneration.
+
  *
+
  *  2) The Full Event   : Every MCParticle in the event is checked to see how it decayed.
+
  *                        The full list of all possible event types of this event can then be found.
+
  *
+
  *  3) The MCAssociate  : The decay of the MCAssociate, or it's mother, or the top of the tree, is checked.
+
  *                        A categorisation of any fully or partially reconstructed backgrounds
+
  *                        can then be made.
+
  *                        This output is complimentary to background category and the MCHierachy and
+
  *                        can be used as a rudimentary categorisation of backgrounds.
+
  *
+
  *  For method (3) use this TupleTool. For methods (1) and (2) use the TupleToolMCEventType
+
  *
+
  *
+
  *  OPTIONS:
+
  *  
  * 
+
  *  mother         bool   Actually categorise the mc mother of this particle, not the associate itself
+
  *                        by default this is false
+
  *  top            bool   Actually categorise the ultimate mcmother of this mcparticle, not the associate itself
+
  *                        by default this is false
+
  *
+
  *  UseChi2Method  bool   assosciate using chi2
+
  *                        by default this is false
+
  *  InputLocations bool   Associator input location.
+
  *                        by default this is empty, fine for most cases
+
  * 
+
  *  fillSlowFind   bool   search through using decay strings, very slow method
+
  *                        by default this is false
+
  *  fillPseudoFind bool   construct the event types logically, much faster but less accurate
+
  *                        by default this is true
+
  *
+
  *  allEventTypes std::vector   the full list of all event types to consider. for the slow find.
+
  *                               This is ignored by the fast find.
+
  *                               By default this is the whole list in the DekFile doc
+
  *  hasEventTypes  std::vector   list of event types, the number of these types in the event
+
  *                               will appear as an entry in the ntuple
+
  *                               by default this is empty
+
  *  hasMCDecay     std::string   look for this decay string aswell
+
  *                               by default this is empty
+
  *
+
  *  ENTRIES:
+
  *
+
  *  Depending on the options, different entries will be written out.
+
  *  By default, only the Pseudo event types will be looked for
+
  *  
+
  *  _MCP_FoundTypes              (farray)            List of all found event types for the assosciate
+
  *  _MCP_FoundLen                (unsigned int)      Maximum length of this farray
+
  *  _MCP_numFoundTypes           (unsigned int)      number of types found in this case
+
  *  _MCP_MatchingTypes           (farray)            List of Event types which match those in hasEventType for the assosciate
+
  *  _MCP_MatchLen                (unsigned int)      Maximum length of this farray
+
  *  _MCP_numMatchingTypes        (unsigned int)      How many of hasEventType there are in the Event Types for the assosciate
+
  *  
+
  *  _MCP_PseudoTypes             (farray)            List of all found event types for the assosciate
+
  *  _MCP_PseudoLen               (unsigned int)      Maximum length of this farray
+
  *  _MCP_numPseudoTypes          (unsigned int)      number of pseudo types found in this case
+
  *  _MCP_MatchingPseudoTypes     (farray)            List of Pseudo Event types which match those in hasEventType for the assosciate
+
  *  _MCP_MatchPseudoLen          (unsigned int)      Maximum length of this farray
+
  *  _MCP_numMatchingPseudoTypes  (unsigned int)      How many of hasEventType there are in the Pseudo Event Types for the assosciate
+
  * 
+
  *  _MCP_hasGivenDecay           (bool)              does this MCP decay by the string in hasMCDecay?
+
  * 
  */
 
--- Analysis/Phys/DecayTreeTuple/src/MCTupleToolEventType.h
+++ Analysis/Phys/DecayTreeTuple/src/MCTupleToolEventType.h
@@ -1,4 +1,4 @@
-// $Id: MCTupleToolEventType.h,v 1.1 2009-06-04 10:54:45 rlambert Exp $
+// $Id: MCTupleToolEventType.h,v 1.2 2009-06-04 12:16:53 rlambert Exp $
 #ifndef MCTUPLETOOLEVENTTYPE_H
 #define MCTUPLETOOLEVENTTYPE_H 1
 
@@ -28,86 +28,168 @@
  *  There are two methods used to find the event type   (Pseudo or Slow)
  *   and several places you can look for the event type (GenHeader, FullEvent, MCAssociate)
  * 
+ *  CONFIGURATION:
+
+ *  You will also need to configure the EvtTypeSvc(), which takes the list of all possible decays from Gauss
+ 
+ *  EvtTypeSvc().EvtTypesFile='...fullpath.../table_event.txt'
+ 
+ *  Usually the file resides in $DECFILESROOT/doc/table_event.txt, copy and ship it with your job
+ 
+ *  
  *  METHODS:
  *
  *  A) The Pseudo method: guesses the event type, by constructing the possible types
+
  *                        based on the definition of event types in the LHCb note
+
  *                        this is fast, works on all decays, but is ambiguous.
+
  *                        To look for inclusive types, this is the best method.
  *
+
  *  B) The Slow Method  : Finds the event type based on the decay string.
+
  *                        This is accurate, but relies on the string being correct
+
  *                        in the .dec file of this decay. Most of the time this is not the case.
+
  *                        To check the decay file you've written is correct, this is the best method.
+
  *
  *  WHERE TO LOOK:
+
  * 
+
  *  1) The GenHeader    : The GenHeader contains the event type which was generated.
+
  *                        If this is all you need, and you want no information on the other
+
  *                        possible decays in this event, you can stop here.
+
  *                        If all you want to do is find the heaviest quark in the event,
+
  *                        use the TupleToolGeneration
+
  *
  *  2) The Full Event   : Every MCParticle in the event is checked to see how it decayed.
+
  *                        The full list of all possible event types of this event can then be found.
+
  *
+
  *  3) The MCAssociate  : The decay of the MCAssociate, or it's mother, or the top of the tree is checked.
+
  *                        A categorisation of any fully or partially reconstructed backgrounds
+
  *                        can then be made.
+
  *                        This output is complimentary to background category and the MCHierachy and
+
  *                        can be used as a rudimentary categorisation of backgrounds.
+
  * 
  *  To check the GenHeader and the event (1,2), use this tool.
+
  *  To check the MCAssociate (3), use TupleToolMCDecayType
+
  *
+
  *  OPTIONS:
+
  *  
  * 
+
  *  fillGenEvent   bool   should I write out the gen event header?
+
  *                        by default this is true
+
  *  findGenEvent   bool   should I look for the generated type in the event?
+
  *                        by default this is false
+
  *  fillWholeEvent bool   should I examine every MC particle in the event?
+
  *                        by default this is true
+
  * 
+
  *  fillSlowFind   bool   search through using decay strings, very slow method
+
  *                        by default this is false
+
  *  fillPseudoFind bool   construct the event types logically, much faster but less accurate
+
  *                        by default this is true
+
  *
+
  *  allEventTypes  std::vector   the full list of all event types to consider. for the slow find.
+
  *                               This is ignored by the fast find.
+
  *                               By default this is the whole list in the DekFile doc
+
  *  hasEventTypes  std::vector   list of event types, the number of these types in the event
+
  *                               will appear as an entry in the ntuple
+
  *                               by default this is empty
+
  *  hasMCDecay     std::string   look for this decay string aswell
+
  *                               by default this is empty
+
  *
+
  *  ENTRIES:
+
  *
+
  *  Depending on the options, different entries will be written out.
+
  *  By default, only the Pseudo event types will be looked for
+
  *  
+
  *  EVT_GenEvent                (long unsigned int) The event type in the GenHeader
+
  *  EVT_hasGenEvent             (bool)              Does the GenEvent decay string appear?
+
  *  EVT_hasPseudoGenEvent       (bool)              is there a pseudo decay like the one generated?
+
  *
+
  *  EVT_FoundTypes              (farray)            List of all found event types
+
  *  EVT_FoundLen                (unsigned int)      Maximum length of this farray
+
  *  EVT_numFoundTypes           (unsigned int)      number of types found in this case
+
  *  EVT_MatchingTypes           (farray)            List of Event types which match those in hasEventType
+
  *  EVT_MatchLen                (unsigned int)      Maximum length of this farray
+
  *  EVT_numMatchingTypes        (unsigned int)      How many of hasEventType there are in the Event Types
+
  *  
+
  *  EVT_PseudoTypes             (farray)            List of all found event types
+
  *  EVT_PseudoLen               (unsigned int)      Maximum length of this farray
+
  *  EVT_numPseudoTypes          (unsigned int)      number of pseudo types found in this case
+
  *  EVT_MatchingPseudoTypes     (farray)            List of Pseudo Event types which match those in hasEventType
+
  *  EVT_MatchPseudoLen          (unsigned int)      Maximum length of this farray
+
  *  EVT_numMatchingPseudoTypes  (unsigned int)      How many of hasEventType there are in the Pseudo Event Types
+
  * 
+
  *  EVT_hasGivenDecay           (bool)              does this event contain the decay in hasMCDecay
+
  * 
  * 
  */
