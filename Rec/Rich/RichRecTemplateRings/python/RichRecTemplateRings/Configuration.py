70acb8c3cb9b0f70493562fa3559481aefb689fb
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-06-04 15:26:53 +0000

    update config
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@10994 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6354e654a638188015aa601837109294f396e8f2

--- Rec/Rich/RichRecTemplateRings/python/RichRecTemplateRings/Configuration.py
+++ Rec/Rich/RichRecTemplateRings/python/RichRecTemplateRings/Configuration.py
@@ -5,7 +5,7 @@
 #  @author Sajan Easo   (Sajan.Easo@cern.ch)
 #  @date   02/03/2009
 
-__version__ = "$Id: Configuration.py,v 1.2 2009-03-19 17:50:46 seaso Exp $"
+__version__ = "$Id: Configuration.py,v 1.3 2009-06-04 15:26:53 jonrob Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from RichKernel.Configuration import *
@@ -20,17 +20,18 @@ class RichTemplateRingFinderConf(RichConfigurableUser):
 
         ## Steering options
     __slots__ = {
-        "Context"             :    "Offline"   # The context within which to run
-       ,"Sequencer"           : GaudiSequencer("RichTemplateDefaultSequencer")  # The sequencer to add the RICH MCMC algorithms to
-       ,"MakeNtuple"          : False
+        "Context"             : "Offline"   # The context within which to run
+       ,"Sequencer"           : None        # The sequencer to add the RICH MCMC algorithms to
+       ,"MakeNtuple"          : False       # Make development ntuple
         }
 
     ## @brief Apply the configuration to the given GaudiSequencer
     def __apply_configuration__(self):
 
-        sequence = self.getProp("Sequencer")
-        if sequence.name() == "RichTemplateDefaultSequencer" :
+        if not self.isPropertySet("Sequencer") :
             raise RuntimeError("ERROR : Rich Template Ring Finder Sequence not set")
+        
+        sequence = self.getProp("Sequencer")
 
         from Configurables import ( RichRingRecAlgBase,
                                     RichTargetDataConfigAlg,
@@ -47,5 +48,5 @@ class RichTemplateRingFinderConf(RichConfigurableUser):
             from Configurables import NTupleSvc
             NTupleSvc().Output = ["RICHTUPLE1 DATAFILE='Ntuple_RichTemplateRings.root' TYP='ROOT' OPT='NEW'"]
 
-            master.RichTemplateRingNtupProduce=True
-        
+            master.RichTemplateRingNtupProduce = True
+            
