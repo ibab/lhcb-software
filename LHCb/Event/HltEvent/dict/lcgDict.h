18ccf25813ad9af83387ef640f6e4f8a8cf1e14a
Tomasz Skwarnicki <tskwarni@syr.edu>
2009-06-04 20:06:25 +0000

    dictionary changes for HltSelReports and HltVertexReports; new methods in HltObjectSummary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37421 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b80294226d513d2f1a220406474efa4429c0ab22

--- LHCb/Event/HltEvent/dict/lcgDict.h
+++ LHCb/Event/HltEvent/dict/lcgDict.h
@@ -1,4 +1,4 @@
-// $Id: lcgDict.h,v 1.3 2009-06-03 20:47:23 jpalac Exp $
+// $Id: lcgDict.h,v 1.4 2009-06-04 20:06:25 tskwarni Exp $
 #ifndef DICT_LCGDICT_H 
 #define DICT_LCGDICT_H 1
 
@@ -13,9 +13,11 @@ namespace {
   struct _Instantiations {
     // begin instantiations
     GaudiUtils::VectorMap< std::string, LHCb::HltDecReport > _i0;
-    std::vector<const LHCb::HltObjectSummary*>               _i1;
-    std::vector<LHCb::HltObjectSummary*>                     _i2; 
-    LHCb::HltObjectSummary::Info                             _i3;
+    GaudiUtils::VectorMap< std::string, LHCb::HltVertexReports::HltVertexReport > _i1;
+    GaudiUtils::VectorMap< std::string, LHCb::HltObjectSummary > _i2;
+    std::vector<const LHCb::HltObjectSummary*>               _i3;
+    std::vector<LHCb::HltObjectSummary*>                     _i4; 
+    LHCb::HltObjectSummary::Info                             _i5;
     // end instantiations
   };
 }
