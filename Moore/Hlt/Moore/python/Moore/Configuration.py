00c1740b62cbe67010a19dca9c5672cdc2c5143e
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-06-08 11:48:40 +0000

    use L0 configurable start preparation for online python task
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@76803 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7e296182629f8403bd6acea600cf9c597b039bd5

--- Moore/Hlt/Moore/python/Moore/Configuration.py
+++ Moore/Hlt/Moore/python/Moore/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tool(s) for Moore
 """
-__version__ = "$Id: Configuration.py,v 1.53 2009-05-04 14:18:19 graven Exp $"
+__version__ = "$Id: Configuration.py,v 1.54 2009-06-08 11:48:40 graven Exp $"
 __author__  = "Gerhard Raven <Gerhard.Raven@nikhef.nl>"
 
 from os import environ, path
@@ -10,7 +10,7 @@ from GaudiConf.Configuration import *
 from Configurables import ConfigFileAccessSvc, ConfigDBAccessSvc, HltConfigSvc, HltGenConfig
 from Configurables import EventClockSvc
 from Configurables import GaudiSequencer
-from Configurables import HltConf, LHCbApp
+from Configurables import HltConf, LHCbApp, L0Conf
 from Configurables import L0DUMultiConfigProvider
 from Configurables import LHCb__MDFWriter as MDFWriter
 # from HltConf.Configuration import *
@@ -23,6 +23,13 @@ from  ctypes import c_uint
 
 def _ext(name) : return path.splitext(name)[-1].lstrip('.')
 
+def _datafmt(pfn) : 
+        fmt = { 'RAW' : "DATAFILE='PFN:%s' SVC='LHCb::MDFSelector'"
+              , 'MDF' : "DATAFILE='PFN:%s' SVC='LHCb::MDFSelector'"
+              , 'DST' : "DATAFILE='PFN:%s' TYP='POOL_ROOTTREE' OPT='READ'" 
+              }
+        return fmt[ _ext(pfn).upper() ] % pfn
+
 # canonicalize tck  -- eats integer + string, returns canonical string
 def _tck(x) :
     if type(x) == str and x[0:2] == '0x' : return x
@@ -31,7 +38,8 @@ def _tck(x) :
 class Moore(LHCbConfigurableUser):
     ## Possible used Configurables
     __used_configurables__ = [ HltConf
-                             , LHCbApp ]
+                             , LHCbApp
+                             , L0Conf ]
 
 
     __slots__ = {
@@ -45,27 +53,71 @@ class Moore(LHCbConfigurableUser):
         , "inputFiles" :       [ ] # input
         , "HltType" :          'Hlt1'
         , "useTCK"     :       False # use TCK instead of options...
-        , "replaceL0BanksWithEmulated" : False
+        , "L0"         :       False # run L0
+        , "ReplaceL0BanksWithEmulated" : False # rerun L0
         , "L0TCK"      :       ''  # which L0 TCKs to use for configuration
-        , "prefetchConfigDir" :''  # which configurations to prefetch.
+        , "CheckOdin"  :       False  # use TCK from ODIN
+        , "initialTCK" :'0x80450000'  # which configuration to use during initialize
+        , "prefetchConfigDir" :'MOORE_v7r0'  # which configurations to prefetch.
         , "generateConfig" :   False # whether or not to generate a configuration
         , "configLabel" :      ''    # label for generated configuration
         , "configAlgorithms" : ['Hlt']    # which algorithms to configure (automatically including their children!)...
-        , "configServices" :   ['ToolSvc','HltDataSvc','HltANNSvc','LumiANNSvc','HistogramDataSvc' ]    # which services to configure (automatically including their dependencies!)...
+        , "configServices" :   ['ToolSvc','HltDataSvc','HltANNSvc' ]    # which services to configure (automatically including their dependencies!)...
         , "TCKData" :          '$TCKDATAROOT' # where do we read/write TCK data from/to?
         , "TCKpersistency" :   'file' # which method to use for TCK data? valid is 'file' and 'sqlite'
-        , "enableAuditor" :    [ ]  # put here eg . [ NameAuditor(), ChronoAuditor(), MemoryAuditor() ]
+        , "EnableAuditor" :    [ ]  # put here eg . [ NameAuditor(), ChronoAuditor(), MemoryAuditor() ]
         , "Verbose" :          True # whether or not to print Hlt sequence
         , "ThresholdSettings" : ''
         }   
                 
+    def _configureOnline(self) :
+        import OnlineEnv as Online
+        app=ApplicationMgr()
+        EventPersistencySvc().CnvServices.append( 'LHCb::RawDataCnvSvc' )
+        mepMgr = Online.mepManager(Online.PartitionID,Online.PartitionName,['Events','SEND'],True)
+        app.Runable = Online.evtRunable(mepMgr)
+        app.ExtSvc.append(mepMgr)
+        evtMerger = Online.evtMerger(name='Output',buffer='SEND',datatype=MDF_NONE,routing=1)
+        evtMerger.DataType = MDF_BANKS
+        # append evtMerger to SendSequence, after Hlt1Global...
+        #ApplicationMgr.TopAlg               += { "Sequencer/SendSequence" };
+        #SendSequence.Members                 = {"Hlt1Global", "LHCb::RawEvent2MBMMergerAlg/Output"};
+        #SendSequence.OutputLevel             = @OnlineEnv.OutputLevel;
+        eventSelector = Online.mbmSelector(input='EVENT')
+        app.ExtSvc.append(eventSelector)
+        Online.evtDataSvc()
+        #ToolSvc.SequencerTimerTool.OutputLevel = @OnlineEnv.OutputLevel;          
+        Configs.AuditorSvc().Auditors = []
+        app.MessageSvcType = 'LHCb::FmcMessageSvc'
+        del allConfigurables['MessageSvc']
+        app.SvcOptMapping.append('LHCb::FmcMessageSvc/MessageSvc')
+        msg=Configs.LHCb__FmcMessageSvc('MessageSvc')
+        msg.LoggerOnly = True
+        #MessageSvc.LoggerOnly                = true;
+        #MessageSvc.OutputLevel               = @OnlineEnv.OutputLevel;
+        msg.fifoPath = os.environ['LOGFIFO']
+        msg.OutputLevel = WARNING
+        msg.doPrintAlways = False
+
+    def _onlineSettings(self) :
+        log.warning('overruling settings with online values')
+        self.setProp('useTCK', True)
+        HistogramPersistencySvc().Warnings = false;
+        EventLoopMgr().Warnings = false;
+        # configure services...
+        HistogramDataSvc().Input = ["CaloPIDs DATAFILE='$PARAMFILESROOT/data/CaloPIDs_DC09_v1.root' TYP='ROOT'"];
+        VFSSvc().FileAccessTools = ['FileReadTool', 'CondDBEntityResolver/CondDBEntityResolver'];
+        ParticlePropertySvc().ParticlePropertiesFile = "conddb:///param/ParticleTable.txt";
+        LHCb__ParticlePropertySvc().ParticlePropertiesFile = 'conddb:///param/ParticleTable.txt';
+
 
     def _configureInput(self):
         files = self.getProp('inputFiles')
-        extensions = { 'RAW' : "' SVC='LHCb::MDFSelector'",
-                       'MDF' : "' SVC='LHCb::MDFSelector'",
-                       'DST' : "' TYP='POOL_ROOTTREE' OPT='READ'" }
-        EventSelector().Input = [ "DATAFILE='PFN:"+ f + extensions[ _ext(f).upper() ] for f in files ]
+        if files : importOptions('$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts')
+        EventPersistencySvc().CnvServices.append( 'LHCb::RawDataCnvSvc' )
+        ApplicationMgr().ExtSvc.append( 'DataOnDemandSvc' ) 
+        importOptions('$STDOPTS/DecodeRawEvent.py')
+        EventSelector().Input = [ _datafmt(f) for f in files ]
 
     def _configureOutput(self):
         fname = self.getProp('outputFile')
@@ -102,40 +154,7 @@ class Moore(LHCbConfigurableUser):
         AuditorSvc().Auditors.append( x.name() )
         x.Enable = True
 
-
-    def _generateConfig(self) :
-        importOptions('$L0TCKROOT/options/L0DUConfig.opts')
-        # make sure we load as many L0 TCKs as possible...
-        L0DUMultiConfigProvider('L0DUConfig').Preload = True
-        svcs = self.getProp("configServices")
-        algs = self.getProp("configAlgorithms")
-        # if not algs : algs = [ i.rsplit('/')[-1] if type(i) is str else i.getName() for i in ApplicationMgr().TopAlg ] # WARNING: this doesn't work on Gaudi v20r3 and later...
-        gen = HltGenConfig( ConfigTop = [ i.rsplit('/')[-1] for i in algs ]
-                          , ConfigSvc = [ i.rsplit('/')[-1] for i in svcs ]
-                          , ConfigAccessSvc = self.getConfigAccessSvc().getName()
-                          , hltType = self.getProp('HltType')
-                          , mooreRelease = self.getRelease()
-                          , label = self.getProp('configLabel'))
-        # make sure gen is the very first Top algorithm...
-        ApplicationMgr().TopAlg = [ gen.getFullName() ] + ApplicationMgr().TopAlg
-
-    def __apply_configuration__(self):
-        GaudiKernel.ProcessJobOptions.PrintOff()
-        importOptions('$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts')
-        EventPersistencySvc().CnvServices.append( 'LHCb::RawDataCnvSvc' )
-        importOptions('$STDOPTS/DecodeRawEvent.py')
-        if self.getProp('replaceL0BanksWithEmulated') :
-            importOptions('$L0DUROOT/options/ReplaceL0BanksWithEmulated.opts')
-        ApplicationMgr().TopAlg.append( GaudiSequencer('Hlt') )
-        # needed for DecodeRawEvent 
-        for i in [  'AuditorSvc',  'DataOnDemandSvc' ] : ApplicationMgr().ExtSvc.append( i ) 
-        # forward some settings... 
-        app = LHCbApp()
-        self.setOtherProps( app, ['EvtMax','SkipEvents','Simulation', 'DataType','DDDBtag','CondDBtag' ] )
-        # Get the event time (for CondDb) from ODIN 
-        EventClockSvc().EventTimeDecoder = 'OdinTimeDecoder'
-        # make sure we don't pick up small variations of the read current
-        #MagneticFieldSvc().UseSetCurrent = True
+    def _outputLevel(self) :
         # output levels...
         ToolSvc().OutputLevel                     = INFO
         from Configurables import XmlParserSvc 
@@ -145,19 +164,72 @@ class Moore(LHCbConfigurableUser):
         SequencerTimerTool().OutputLevel          = WARNING
         # Print algorithm name with 40 characters
         MessageSvc().Format = '% F%40W%S%7W%R%T %0W%M'
+    def _profile(self) :
+        ApplicationMgr().AuditAlgorithms = 1
+        if  'AuditorSvc' not in ApplicationMgr().ExtSvc : 
+            ApplicationMgr().ExtSvc.append( 'AuditorSvc' ) 
+        AuditorSvc().Auditors.append( 'TimingAuditor/TIMER' )
+        for i in self.getProp('EnableAuditor') : self.addAuditor( i )
+
+    def _generateConfig(self) :
+        importOptions('$L0TCKROOT/options/L0DUConfig.opts')
+        # make sure we load as many L0 TCKs as possible...
+        L0DUMultiConfigProvider('L0DUConfig').Preload = True
+        svcs = self.getProp("configServices")
+        algs = self.getProp("configAlgorithms")
+        gen = HltGenConfig( ConfigTop = [ i.rsplit('/')[-1] for i in algs ]
+                          , ConfigSvc = [ i.rsplit('/')[-1] for i in svcs ]
+                          , ConfigAccessSvc = self.getConfigAccessSvc().getName()
+                          , hltType = self.getProp('HltType')
+                          , mooreRelease = self.getRelease()
+                          , label = self.getProp('configLabel'))
+        # make sure gen is the very first Top algorithm...
+        ApplicationMgr().TopAlg = [ gen.getFullName() ] + ApplicationMgr().TopAlg
+
+    def _l0(self) :
+        if ( (self.getProp("HltType")!='') and (self.getProp("DataType") == 'DC06') and (not self.getProp("L0")) ):
+            log.warning("It is mandatory to run the L0 emulation on DC06 data to get the HLT to work correctly")
+            log.warning("Will set ReplaceL0BanksWithEmulated = True")
+            self.setProp("ReplaceL0BanksWithEmulated",True)
+        if ( self.getProp("ReplaceL0BanksWithEmulated") and (not self.getProp("L0")) ):
+            log.warning("You asked to replace L0 banks with emulation. Will set L0 = True")
+            self.setProp("L0",True)
+        if ( self.getProp("L0") ):
+            l0seq = GaudiSequencer("seqL0")
+            ApplicationMgr().TopAlg += [ l0seq ]
+            L0Conf().setProp( "L0Sequencer", l0seq )
+            self.setOtherProps( L0Conf(), [ "ReplaceL0BanksWithEmulated" , "DataType" ] )
+            log.info("Will rerun L0")
+
+
+    def __apply_configuration__(self):
+        GaudiKernel.ProcessJobOptions.PrintOff()
+        self._l0()
+        ApplicationMgr().TopAlg.append( GaudiSequencer('Hlt') )
+        # forward some settings... 
+        app = LHCbApp()
+        self.setOtherProps( app, ['EvtMax','SkipEvents','Simulation', 'DataType' ] )
+        # this is a hack. Why does setOtherProps not work?
+        app.CondDBtag = self.getProp('CondDBtag')
+        app.DDDBtag   = self.getProp('DDDBtag')
+        print app
+        # Get the event time (for CondDb) from ODIN 
+        EventClockSvc().EventTimeDecoder = 'OdinTimeDecoder'
+        # make sure we don't pick up small variations of the read current
+        #MagneticFieldSvc().UseSetCurrent = True
         # Need a defined HistogramPersistency to read some calibration inputs!!!
         ApplicationMgr().HistogramPersistency = 'ROOT'
-        # Profiling
-        ApplicationMgr().AuditAlgorithms = 1
-        AuditorSvc().Auditors.append( 'TimingAuditor/TIMER' )
-        for i in self.getProp('enableAuditor') : self.addAuditor( i )
+        self._outputLevel()
+        self._profile()
         if self.getProp('useTCK') :
             if (self.getProp('L0TCK')) : raise RunTimeError( 'useTCK and L0TCK are mutually exclusive')
             # TODO: update to latest HltConfigSvc setup!!!
             tcks = [ _tck(i) for i in self.getProp('prefetchTCK') ]
-            cfg = HltConfigSvc( prefetchTCK = tcks
-                              , initialTCK = tcks[0]
+            cfg = HltConfigSvc( prefetchDir = self.getProp('prefetchConfigDir')
+                              , initialTCK = self.getProp('initialTCK')
+                              , checkOdin = self.getProp('checkOdin')
                               , ConfigAccessSvc = self.getConfigAccessSvc().getFullName() ) 
+            # TODO: make sure we are the first one...
             ApplicationMgr().ExtSvc.append(cfg.getFullName())
         else:
             hltConf = HltConf()
@@ -166,5 +238,9 @@ class Moore(LHCbConfigurableUser):
             log.info( hltConf )
             
         if self.getProp("generateConfig") : self._generateConfig()
-        self._configureInput()
-        self._configureOutput()
+        online = False
+        if online : 
+            print 'not yet done'
+        else :
+            self._configureInput()
+            self._configureOutput()
