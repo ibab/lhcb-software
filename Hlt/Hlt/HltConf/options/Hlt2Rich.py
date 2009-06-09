0add200332326c953637e28bc68daf73bd583401
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-06-09 11:17:29 +0000

    ! 2009-06-09 - Chris Jones  - Turn off trackless ring finding in RICH HLT reconstruction (Hlt2Rich.py)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@76815 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0c71910b3d0802e74068b077053d29e613610848

--- Hlt/Hlt/HltConf/options/Hlt2Rich.py
+++ Hlt/Hlt/HltConf/options/Hlt2Rich.py
@@ -1,22 +1,27 @@
 from RichRecSys.Configuration import *
 from GaudiKernel.SystemOfUnits import GeV, mm
 
-seq = GaudiSequencer("HltRICHReco")
-seq.MeasureTime = True
-#GaudiSequencer("SeqHlt2Charged").Members += [seq]
+richseq = GaudiSequencer("HltRICHReco")
+richseq.MeasureTime = True
 
+# The RICH COnfigurable
 richConf = RichRecSysConf()
 
-richConf.setProp("RecoSequencer",seq)
+# Set the sequence to run the RICH PID in
+richConf.setProp("RecoSequencer",richseq)
 
+# Configure for Fast HLT Global PID
 richConf.Context   = "HLT"
 richConf.PidConfig = "FastGlobal"
 
+# Initial pixels tracks and photons
 richConf.InitPixels  = True
 richConf.InitTracks  = True
 richConf.InitPhotons = True
 
+# Turn off ring finding
+richConf.TracklessRingAlgs = []
+
+# Turn off book keeping for pixels, for speed
 RichPixelCreatorConfig().BookKeeping  = False
 RichPixelCreatorConfig().FindClusters = False
-
-#richConf.applyConf()
