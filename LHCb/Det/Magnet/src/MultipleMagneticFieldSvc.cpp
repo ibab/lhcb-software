186e0339d5b6f88b82ece81c0a4b675c3550e93c
mlieng <mlieng@cern.ch>
2009-06-10 17:24:49 +0000

    Added MultipleMagneticFieldSvc
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37429 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 459746cde23aee07e7b9cbb46093741a2ba11222

---
+++ LHCb/Det/Magnet/src/MultipleMagneticFieldSvc.cpp
@@ -0,0 +1,172 @@
+// Include files
+#include "GaudiKernel/SvcFactory.h"
+#include "GaudiKernel/ISvcLocator.h"
+#include "GaudiKernel/MsgStream.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "MultipleMagneticFieldSvc.h"
+
+#include "GaudiKernel/Vector3DTypes.h"
+#include "GaudiKernel/Point3DTypes.h"
+
+#include <cstdlib>
+#include <fstream>
+
+/** @file MultipleMagneticFieldSvc.cpp
+ *  Implementation of MultipleMagneticFieldSvc class
+ *
+ *  @author Magnus Lieng
+ *  @date   2008-07-03
+ */
+
+DECLARE_SERVICE_FACTORY( MultipleMagneticFieldSvc );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+MultipleMagneticFieldSvc::MultipleMagneticFieldSvc( const std::string& name, 
+            ISvcLocator* svc ) : Service( name, svc ),
+                                  m_names(0),
+                                  m_magneticFieldSvcs(0)
+{
+  declareProperty( "MagneticFieldServices",    m_names );
+}
+//=============================================================================
+// Standard destructor
+//=============================================================================
+MultipleMagneticFieldSvc::~MultipleMagneticFieldSvc()
+{
+}
+
+//=============================================================================
+// Initialize
+//=============================================================================
+StatusCode MultipleMagneticFieldSvc::initialize()
+{
+  MsgStream log(msgSvc(), name());
+  StatusCode sc = Service::initialize();
+  if( sc.isFailure() ) return sc;
+
+  /// Getting the member magnetic field services
+  std::vector<std::string>::const_iterator it;
+  for ( it = m_names.begin(); m_names.end() != it; it++ ) {
+    if( !(*it).empty()) {
+      ILHCbMagnetSvc* mfSvc;
+      sc = service( (*it), mfSvc, true );
+      if( sc.isFailure() || mfSvc == 0) {
+        log << MSG::WARNING << " Member " << (*it) << " not found" << endreq;
+      }
+      else {
+        m_magneticFieldSvcs.push_back(mfSvc);
+      }
+    }
+  }
+  /*
+  /// Inintializing member fields
+  if( m_magneticFieldSvcs.size() ) {
+    std::vector<ILHCbMagnetSvc*>::iterator field;
+    for( field = m_magneticFieldSvcs.begin(); field != m_magneticFieldSvcs.end(); field++) {
+      sc = (*field)->initialize();
+      if( sc.isFailure() ){
+        log << MSG::WARNING << " Member field failed to initialize" << endreq;
+        m_magneticFieldSvcs.erase(field);
+      }
+    }
+  }
+  */
+   
+  /// Warn if no fields are set up
+  if( m_magneticFieldSvcs.size() == 0 ) {
+    log << MSG::WARNING << " No member fields configured" << endreq;
+  }
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Finalize
+//=============================================================================
+StatusCode MultipleMagneticFieldSvc::finalize()
+{   
+  MsgStream log(msgSvc(), name());
+  StatusCode sc = StatusCode::SUCCESS;
+  // release the used tools
+  if( m_magneticFieldSvcs.size() ) {
+    std::vector<ILHCbMagnetSvc*>::iterator field;
+    for( field = m_magneticFieldSvcs.begin(); field != m_magneticFieldSvcs.end(); field++) {
+      sc = (*field)->release();
+      if( sc.isFailure() ){
+        log << MSG::WARNING << " Member field failed to finalize" << endreq;
+        m_magneticFieldSvcs.erase(field);
+      }
+    }
+  }
+  return sc && Service::finalize();
+}
+
+
+//=============================================================================
+// QueryInterface
+//=============================================================================
+StatusCode MultipleMagneticFieldSvc::queryInterface( const InterfaceID& riid,
+                                             void** ppvInterface      )
+{   
+  if ( IMagneticFieldSvc::interfaceID().versionMatch(riid) ) {
+    *ppvInterface = (IMagneticFieldSvc*)this;
+    addRef();
+    return StatusCode::SUCCESS;
+  } else if ( ILHCbMagnetSvc::interfaceID().versionMatch(riid) ) {
+    *ppvInterface = (ILHCbMagnetSvc*)this;
+    addRef();
+    return StatusCode::SUCCESS;
+  }
+  return Service::queryInterface(riid,ppvInterface);
+} 
+
+//=============================================================================
+// FieldVector: find the magnetic field value at a given point in space
+//=============================================================================
+StatusCode MultipleMagneticFieldSvc::fieldVector(const Gaudi::XYZPoint&  r,
+                                               Gaudi::XYZVector& bf ) const
+{
+  /// Attaining field strength from member fields
+  bf.SetXYZ( 0.0, 0.0, 0.0 );
+  StatusCode sc;
+  Gaudi::XYZVector tempVector;
+  std::vector<ILHCbMagnetSvc*>::const_iterator field;
+  for( field = m_magneticFieldSvcs.begin(); field != m_magneticFieldSvcs.end(); field++) {
+    sc = (*field)->fieldVector(r, tempVector);
+    if( sc.isFailure() ) return sc;
+    bf+=tempVector;
+  }
+  return StatusCode::SUCCESS;      
+}
+
+//=============================================================================
+// ScaleFactor: Get the average scale factor of member fields
+//=============================================================================
+double MultipleMagneticFieldSvc::scaleFactor() const
+{
+  /// Attaining scale factor of member fields (The average)
+  double scale,sumScale = 0.0;
+  int numFields = 0;
+  std::vector<ILHCbMagnetSvc*>::const_iterator field;
+  for( field = m_magneticFieldSvcs.begin(); field != m_magneticFieldSvcs.end(); field++) {
+    sumScale += (*field)->scaleFactor();
+  }
+  if( numFields != 0 ){scale = sumScale/numFields;}
+  return scale;
+}
+
+//=============================================================================
+// UseRealMap: Check if any member fields use real maps
+//=============================================================================
+bool MultipleMagneticFieldSvc::useRealMap() const
+{
+  bool realMap = false;
+  std::vector<ILHCbMagnetSvc*>::const_iterator field;
+  for( field = m_magneticFieldSvcs.begin(); field != m_magneticFieldSvcs.end(); field++) {
+    realMap = realMap || (*field)->useRealMap();
+  }
+  return realMap;
+}
+
---
+++ LHCb/Det/Magnet/src/MultipleMagneticFieldSvc.h
@@ -0,0 +1,74 @@
+#ifndef MULTIPLEMAGNETICFIELDSVC_H
+#define MULTIPLEMAGNETICFIELDSVC_H 1
+
+// Include files
+#include "GaudiKernel/Service.h"
+#include "Kernel/ILHCbMagnetSvc.h"
+#include "GaudiKernel/Vector3DTypes.h"
+#include "GaudiKernel/Point3DTypes.h"
+
+// Forward declarations
+template <class TYPE> class SvcFactory;
+
+
+/** @class MultipleMagneticFieldSvc MultipleMagneticFieldSvc.h
+ *  A service for wrapping multiple MagneticFieldSvc
+ *
+ *  @author Magnus Lieng
+ *  @date   2008-07-03
+ */
+
+class MultipleMagneticFieldSvc : public Service,
+                         virtual public ILHCbMagnetSvc {
+
+protected:
+  
+  /// Standard Constructor.
+  /// @param  name   String with service name
+  /// @param  svc    Pointer to service locator interface
+  MultipleMagneticFieldSvc( const std::string& name, ISvcLocator* svc );
+  
+  /// Virtual destructor.
+  virtual ~MultipleMagneticFieldSvc();
+
+public:
+
+  /// Initialise the service (Inherited Service overrides) 
+  virtual StatusCode initialize();
+  
+  /// Finalise the service (Inherited Service overrides) 
+  virtual StatusCode finalize();
+    
+  /** Query the available interfaces.
+   * @param riid Requested interface ID
+   * @param ppvInterface Pointer to requested interface
+   * @return StatusCode indicating SUCCESS or FAILURE.
+   */
+  virtual StatusCode queryInterface( const InterfaceID& riid, 
+                                     void** ppvInterface      );
+  
+  /// Service type.
+  //virtual const InterfaceID& type() const { return IID_IMagneticFieldSvc; };
+
+  /** Implementation of IMagneticFieldSvc interface.
+   * @param[in]  xyz Point at which magnetic field vector will be given
+   * @param[out] fvec Magnectic field vector.
+   * @return StatusCode SUCCESS if calculation was performed.
+   */
+  virtual StatusCode fieldVector( const Gaudi::XYZPoint&  xyz, 
+                                        Gaudi::XYZVector& fvec ) const;
+
+  // These should be incorperated in a better way...
+  bool   useRealMap() const; ///< True is using real map
+  double scaleFactor() const; ///< accessor to m_scaleFactor
+  
+private:
+
+  /// Allow SvcFactory to instantiate the service.
+  friend class SvcFactory<MultipleMagneticFieldSvc>;
+
+  std::vector<std::string> m_names;
+  std::vector<ILHCbMagnetSvc*> m_magneticFieldSvcs; ///< Magnetic field services.
+};
+
+#endif  // MULTIPLEMAGNETICFIELDSVC_H
