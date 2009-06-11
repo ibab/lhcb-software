a061eb3976a0b4ef5c2afd6b7cf0d29c6b2614e2
jpalac <jpalac@cern.ch>
2009-06-11 15:23:00 +0000

    Untidy and hopefully temporary hack to get re-fitted PVs and corresponding relations tables onto TES for copying onto MicroDST.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76860 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3828c3a85c2af999624e76bb87b144334f10acc9

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
@@ -3,22 +3,38 @@ from Configurables import DaVinci, PhysMicroDST
 
 importOptions("$STDOPTS/PreloadUnits.opts")
 importOptions( "$CCBARROOT/options/DoDC06SelBs2Jpsi2MuMuPhi2KK_lifetime_unbiased.opts")
+MySelection = GaudiSequencer("SeqDC06selBs2JpsiPhi")
+
 conf = PhysMicroDST()
 conf.OutputPrefix = "MicroDST"
 conf.MicroDSTFile = "MyTestMDST_MC_newConf.dst"
 conf.MicroDSTSelectionAlg = "DC06selBs2JpsiPhi_unbiased"
-conf.CopyL0DUReport = True
+conf.CopyL0DUReport = False
 conf.CopyHltDecReports = False
 conf.CopyMCTruth = True
-# tu use the PV re-fitting, need to re-fit PVs, relate them, and pass the relations table location to the MicroDST maker.
-conf.CopyRelatedPVs = False
+# to use the PV re-fitting, need to re-fit PVs, relate them, and pass the relations table location to the MicroDST maker. Could we put this On-Demand?
+from Configurables import PVReFitterAlg, PVRelatorAlg
+mainLocation = "Phys/"+"DC06selBs2JpsiPhi_unbiased"
+PVReFitter = PVReFitterAlg("PVReFitterAlg")
+PVReFitter.ParticleInputLocation = mainLocation+"/Particles"
+PVReFitter.VertexOutputLocation = mainLocation+"/RefittedVertices"
+p2ReFitPVRelationsLoc = mainLocation+"/Particle2ReFittedVertexRelations"
+PVReFitter.P2VRelationsOutputLocation = p2ReFitPVRelationsLoc
+PVReFitter.OutputLevel=4
+PVRelator = PVRelatorAlg()
+PVRelator.OutputLevel=4
+PVRelator.P2PVRelationsInputLocation = p2ReFitPVRelationsLoc
+p2pvSortedRelationsLoc = mainLocation + "/P2ReFitPVSortedRelations"
+PVRelator.P2PVRelationsOutputLocation = p2pvSortedRelationsLoc
+MySelection.Members += [PVReFitter, PVRelator]
+conf.CopyRelatedPVs = True
 conf.P2PVRelationsSuffix = "P2ReFitPVSortedRelations"
 #
 conf.CopyBTags = True
 dv = DaVinci()
 dv.EvtMax = 500
 #dv.HistogramFile = "MyTestMDST_MC_newConf.dst"
-dv.UserAlgorithms = [GaudiSequencer("SeqDC06selBs2JpsiPhi")]
+dv.UserAlgorithms = [MySelection]
 dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
@@ -27,4 +43,6 @@ dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000007_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000011_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000013_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
