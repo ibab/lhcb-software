1c237746f249234264e9d567a9063811255ea15e
jpalac <jpalac@cern.ch>
2009-06-11 12:03:13 +0000

    - Add std::pair<std::string, T> and std::vector<std::pair<std::string,T> >    to dictionaries.  - Exlude == and < operators.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37431 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7e98cd62f3c044a1b93d00ef4574728ac1fa328b

--- LHCb/Event/HltEvent/dict/lcgDict.h
+++ LHCb/Event/HltEvent/dict/lcgDict.h
@@ -1,4 +1,4 @@
-// $Id: lcgDict.h,v 1.4 2009-06-04 20:06:25 tskwarni Exp $
+// $Id: lcgDict.h,v 1.5 2009-06-11 12:03:13 jpalac Exp $
 #ifndef DICT_LCGDICT_H 
 #define DICT_LCGDICT_H 1
 
@@ -6,18 +6,24 @@
 
 // begin include files
 #include "Event/HltObjectSummary.h"
-
+#include "Event/HltDecReports.h"
+//#include "Event/HltVertexReports.h"
 // end include files
 
 namespace {
+
   struct _Instantiations {
     // begin instantiations
-    GaudiUtils::VectorMap< std::string, LHCb::HltDecReport > _i0;
-    GaudiUtils::VectorMap< std::string, LHCb::HltVertexReports::HltVertexReport > _i1;
-    GaudiUtils::VectorMap< std::string, LHCb::HltObjectSummary > _i2;
-    std::vector<const LHCb::HltObjectSummary*>               _i3;
-    std::vector<LHCb::HltObjectSummary*>                     _i4; 
-    LHCb::HltObjectSummary::Info                             _i5;
+    std::pair< std::string, LHCb::HltDecReport >               _dr0;
+    std::vector< std::pair< std::string, LHCb::HltDecReport > > _dr1;
+    GaudiUtils::VectorMap< std::string, LHCb::HltDecReport > _dr2;
+    std::pair< std::string, LHCb::HltObjectSummary >                _ii0;
+    std::vector< std::pair< std::string, LHCb::HltObjectSummary > > _ii1;
+    GaudiUtils::VectorMap< std::string, LHCb::HltObjectSummary >    _ii2;
+    std::vector<const LHCb::HltObjectSummary*>                      _ii3;
+    std::vector<LHCb::HltObjectSummary*>                            _ii4; 
+    LHCb::HltObjectSummary::Info                                    _ii5;
+    GaudiUtils::VectorMap< std::string, LHCb::HltVertexReports::HltVertexReport > _vr0;
     // end instantiations
   };
 }
