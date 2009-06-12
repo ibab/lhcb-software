d93e1d95e0c1c7735736a23593825b67bbb66c02
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-06-12 09:10:15 +0000

    add data
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@76876 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 811b70f5fc1caf568842748fe07efa5a6288ea72

---
+++ Analysis/Tutorial/Analysis/options/MC09-InclJpsi.py
@@ -0,0 +1,16 @@
+#-- GAUDI jobOptions generated on Fri Jun 12 11:02:28 2009
+#-- Contains event types : 
+#--   24142000 - 8 files - 123545 events - 27.24 GBytes
+
+from Gaudi.Configuration import * 
+
+EventSelector().Input   = [
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000002_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000003_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000004_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000006_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000007_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000008_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000009_1.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+FileCatalog().Catalogs = [ 'xmlcatalog_file:MC09-InclJpsi.xml' ]
