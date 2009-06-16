31bb7b6a8699278892f6e08e34a81ad9427d3be6
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-06-16 13:50:23 +0000

    move suppress warnings functionality to configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30743 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0e4ffa11876c00f1b25324bd21d599c8dddc4ddd

--- Brunel/Rec/Brunel/python/Brunel/Configuration.py
+++ Brunel/Rec/Brunel/python/Brunel/Configuration.py
@@ -3,7 +3,7 @@
 #  @author Marco Cattaneo <Marco.Cattaneo@cern.ch>
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.77 2009-06-12 08:12:43 panmanj Exp $"
+__version__ = "$Id: Configuration.py,v 1.78 2009-06-16 13:50:23 cattanem Exp $"
 __author__  = "Marco Cattaneo <Marco.Cattaneo@cern.ch>"
 
 from Gaudi.Configuration  import *
@@ -412,10 +412,25 @@ class Brunel(LHCbConfigurableUser):
         ProcessPhase('Moni').Context = self.getProp("Context")
 
         # Do not print event number at every event (done already by Brunel)
-        EventSelector().PrintFreq = -1;
+        EventSelector().PrintFreq = -1
         # Modify printout defaults
         if self.getProp( "NoWarnings" ):
+            # Suppress info and below
+            msgSvc = getConfigurable("MessageSvc")
+            msgSvc.OutputLevel = WARNING
+            getConfigurable("ToolSvc").OutputLevel = WARNING
+            # Information to be kept
+            getConfigurable("BrunelInit").OutputLevel    = INFO
+            getConfigurable("EventSelector").OutputLevel = INFO
+            getConfigurable("DstWriter").OutputLevel     = INFO
+            getConfigurable("TimingAuditor").OutputLevel = INFO
+            getConfigurable("EventLoopMgr").OutputLevel  = INFO
+            getConfigurable("StatusCodeSvc").OutputLevel = INFO
+            # Suppress known warnings
             importOptions( "$BRUNELOPTS/SuppressWarnings.opts" )
+            # add a time stamp to remaining messages
+            msgSvc.Format = "%u % F%18W%S%7W%R%T %0W%M"
+            msgSvc.timeFormat = "%Y-%m-%d %H:%M:%S UTC"
 
         # Units needed in several of the monitoring options
         importOptions('$STDOPTS/PreloadUnits.opts')
