007647941031193149c6e7d7d5f9b17cd8c65e54
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-06-18 09:23:26 +0000

    Ignore changes in latest ONLINE partition name
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5445 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: acebf20a8321a6c9510df333195b062884797617

--- LHCb/GaudiConf/python/GaudiConf/QMTest/LHCbExclusions.py
+++ LHCb/GaudiConf/python/GaudiConf/QMTest/LHCbExclusions.py
@@ -4,6 +4,9 @@ preprocessor = normalizeExamples + \
     RegexpReplacer(when = "Connected to database",
         orig = r"(\"sqlite_file:)(.*/)([\w.]+/[0-9A-Z_]{1,8}\")",
         repl = r"\1\3") + \
+    RegexpReplacer(when = "Connected to database",
+        orig = "ONLINE[_-][0-9]{1,6}",
+        repl = "ONLINE_xxxxxx") + \
     LineSkipper(["/PARAM/ParamFiles/"]) + \
     LineSkipper(["INFO Using TAG", "TimingAuditor.T", "RootDBase.open"]) + \
     LineSkipper(["INFO Opened magnetic field file"]) + \
