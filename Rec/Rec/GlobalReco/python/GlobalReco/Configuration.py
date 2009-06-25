d583fe23a6ab6b9b98f8bb1f275ed26d69e55337
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-06-25 13:58:59 +0000

    ! 2009-06-25 - Chris Jones  - Add OutputLevel property to Configurable to allow the printout level of all    ProtoParticle algorithms to be controlled.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@30771 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ec89da9ca7b35ea5f44895ac9b5c418f997b779

--- Rec/Rec/GlobalReco/python/GlobalReco/Configuration.py
+++ Rec/Rec/GlobalReco/python/GlobalReco/Configuration.py
@@ -4,7 +4,7 @@
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   15/08/2008
 
-__version__ = "$Id: Configuration.py,v 1.2 2009-04-23 15:20:25 jonrob Exp $"
+__version__ = "$Id: Configuration.py,v 1.3 2009-06-25 13:58:59 jonrob Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from LHCbKernel.Configuration import *
@@ -21,6 +21,7 @@ class GlobalRecoConf(LHCbConfigurableUser):
     __slots__ = { "SpecialData"  : []
                   ,"Context":    "Offline"   # The context within which to run
                   ,"RecoSequencer" : None    # The sequencer to add the ProtoParticle reconstruction algorithms to
+                  ,"OutputLevel" : INFO     # The printout level to use
                   }
 
     ## Apply the configuration to the given sequence
@@ -28,15 +29,26 @@ class GlobalRecoConf(LHCbConfigurableUser):
 
         if not self.isPropertySet("RecoSequencer") :
             raise RuntimeError("ERROR : PROTO Sequencer not set")
-        seq = self.getProp("RecoSequencer")
-        
-        seq.Context = self.getProp("Context")
-        
-        # Charged Proto particles
-        # For time being put all protoparticles in the same container
-        seq.Members += [ "ChargedProtoPAlg" ]
-        # Fill the Combined DLL information in the charged protoparticles
-        seq.Members += [ "ChargedProtoCombineDLLsAlg" ]
 
+        from Configurables import ( ChargedProtoPAlg, ChargedProtoCombineDLLsAlg,
+                                    NeutralProtoPAlg )
+        
+        seq = self.getProp("RecoSequencer")
+        seq.Context = self.getProp("Context")
+
+        # Charged Proto particles
+        charged = ChargedProtoPAlg()
+        # Fill the Combined DLL information in the charged protoparticles
+        combine = ChargedProtoCombineDLLsAlg()
         # Neutrals
-        seq.Members += [ "NeutralProtoPAlg" ]
+        neutral = NeutralProtoPAlg()
+
+        # Set output levels
+        if self.isPropertySet("OutputLevel"):
+            level = self.getProp("OutputLevel")
+            charged.OutputLevel = level
+            combine.OutputLevel = level
+            neutral.OutputLevel = level
+
+        # Add to sequencer
+        seq.Members += [ charged,combine,neutral ]
