d528884a2d2ef57a037c83f7a6f69df41998b16f
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-06-26 07:28:02 +0000

    added dictionary files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@65000 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e45a3301c6af3c6d3997ff29837c167e8649edf0

---
+++ Phys/Phys/DecayTreeFitter/dict/DecayTreeFitterDict.h
@@ -0,0 +1,12 @@
+// $Id: DecayTreeFitterDict.h,v 1.1 2009-06-26 07:28:02 wouter Exp $
+#ifndef DECAYTREEFITTERDICT_H 
+#define DECAYTREEFITTERDICT_H 
+
+// this line is necessary to add ROOT::SMatrices to the dictionary
+#define G__DICTIONARY
+
+#include "DecayTreeFitter/Fitter.h"
+#include "DecayTreeFitter/VtxFitParams.h"
+#include "DecayTreeFitter/LorentzVectorErr.h"
+
+#endif // DECAYTREEFITTERDICT
