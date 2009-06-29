451af23f4955167f052a33b5d321bf3154655502
Tomasz Skwarnicki <tskwarni@syr.edu>
2009-06-29 12:26:39 +0000

    add HltRawData_dll.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77110 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9114b559b1432bd4008c65af9569712858ff54b5

---
+++ Hlt/Hlt/HltRawData/src/HltRawData_dll.cpp
@@ -0,0 +1,10 @@
+// $Id: HltRawData_dll.cpp,v 1.1 2009-06-29 12:26:39 tskwarni Exp $
+// Include files 
+
+#include "GaudiKernel/LoadFactoryEntries.h"
+
+//-- Just touch this file (delete this line) so that it will be saved
+
+LOAD_FACTORY_ENTRIES(HltRawData)
+
+
