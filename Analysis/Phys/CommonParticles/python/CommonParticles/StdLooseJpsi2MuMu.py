f68d837fdd115af95711dcaf6f23ea5ad9e82cb7
Greig Cowan <g.cowan@ed.ac.uk>
2009-06-29 09:16:49 +0000

    Vertex chi2/dof cut -> vertex chi2 cut.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77107 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 312b1244452099dd84d7f5c9d07af0ace1aeb0ba

--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2MuMu.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2MuMu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseJpsi2MuMu.py,v 1.8 2009-06-29 08:55:36 gcowan Exp $ 
+# $Id: StdLooseJpsi2MuMu.py,v 1.9 2009-06-29 09:16:49 gcowan Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseJpsi2MuMu.py
 #  configuration file for 'Standard Loose Jpsi2MuMu' 
@@ -11,7 +11,7 @@
 Configuration file for 'Standard Loose Jpsi2MuMu'
 """
 __author__  = "Greig Cowan"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.8 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.9 $"
 # =============================================================================
 __all__ = (
     'StdLooseJpsi2MuMu' ,
@@ -28,7 +28,7 @@ StdLooseJpsi2MuMu = CombineParticles ("StdLooseJpsi2MuMu")
 StdLooseJpsi2MuMu.InputLocations = ["StdLooseMuons"]
 StdLooseJpsi2MuMu.DecayDescriptor = "J/psi(1S) -> mu+ mu-" 
 StdLooseJpsi2MuMu.CombinationCut = "ADAMASS('J/psi(1S)')<100*MeV"
-StdLooseJpsi2MuMu.MotherCut = "(VFASPF(VCHI2/VDOF) < 25.0)"
+StdLooseJpsi2MuMu.MotherCut = "(VFASPF(VCHI2) < 25.0)"
 
 ## configure Data-On-Demand service 
 locations = updateDoD ( StdLooseJpsi2MuMu    )
--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLoosePhi.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLoosePhi.py
@@ -1,6 +1,6 @@
 ##!/usr/bin/env python
 # =============================================================================
-# $Id: StdLoosePhi.py,v 1.5 2009-06-29 08:55:36 gcowan Exp $ 
+# $Id: StdLoosePhi.py,v 1.6 2009-06-29 09:16:49 gcowan Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLoosePhi2KK.py
 #  configuration file for 'Standard Loose ' 
@@ -11,7 +11,7 @@
 Configuration file for 'Standard Loose Phi2KK'
 """
 __author__  = "Greig Cowan"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.5 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.6 $"
 # =============================================================================
 __all__ = (
     'StdLoosePhi2KK' ,
@@ -28,7 +28,7 @@ StdLoosePhi2KK = CombineParticles("StdLoosePhi2KK")
 StdLoosePhi2KK.InputLocations = ["StdLooseKaons"]
 StdLoosePhi2KK.DecayDescriptor = "phi(1020) -> K+ K-" 
 StdLoosePhi2KK.CombinationCut = "(AM < 1100.*MeV)"
-StdLoosePhi2KK.MotherCut = "(VFASPF(VCHI2/VDOF) < 25.0)"
+StdLoosePhi2KK.MotherCut = "(VFASPF(VCHI2) < 25.0)"
 
 ## configure Data-On-Demand service 
 locations = updateDoD ( StdLoosePhi2KK )
