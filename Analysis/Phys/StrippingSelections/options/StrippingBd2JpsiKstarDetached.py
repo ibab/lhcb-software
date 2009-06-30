0601c64420f05067d9ed028351972a74261390c4
Diego Martinez Santos <diego.martinez.santos@cern.ch>
2009-06-30 10:35:59 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77128 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e29c25586ec186b7a18a5d5f3f9fcfcb031b5ede

---
+++ Analysis/Phys/StrippingSelections/options/StrippingBd2JpsiKstarDetached.py
@@ -0,0 +1,50 @@
+__author__ = 'Diego Martinez Santos'
+__date__ = 'June 30, 2009'
+
+from Gaudi.Configuration import *
+from StrippingConf.StrippingLine import StrippingLine, StrippingMember
+from Configurables import FilterDesktop, CombineParticles, OfflineVertexFitter
+import GaudiKernel.SystemOfUnits as Units
+importOptions("$COMMONPARTICLESROOT/python/CommonParticles/StdLooseDetachedKstar.py")
+
+#########
+# J/psi #
+#########
+
+JpsiCuts = "(VFASPF(VCHI2/VDOF)<9) & (BPVVDCHI2>225)"
+JpsiCuts += "& (BPVVDSIGN > 0) & (ADMASS('J/psi(1S)')<60*MeV)"
+
+#########
+# Kstar #
+#########
+
+KstCuts = "(MIPCHI2DV(PRIMARY)> 12.25) & (ADMASS('K*(892)0')<40*MeV)"
+KstCuts += "& (INTREE( (ABSID=='K+') & (MIPCHI2DV(PRIMARY)  > 4.))) "
+KstCuts += "& (INTREE((ABSID=='pi+') & (MIPCHI2DV(PRIMARY)  > 4.)))"
+
+######
+# Bd #
+######
+
+Bd2JpsiKstDet = CombineParticles("StripBd2JpsiKstDet")
+Bd2JpsiKstDet.DecayDescriptor = "[B0 -> J/psi(1S) K*(892)0]cc"
+Bd2JpsiKstDet.InputLocations = ["Phys/StdLooseJpsi2MuMu", "Phys/StdLooseDetachedKst2Kpi"]
+# Set the OfflineVertexFitter to keep the 4 tracks and not the J/Psi Kstar:
+Bd2JpsiKstDet.addTool( OfflineVertexFitter() )
+Bd2JpsiKstDet.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+Bd2JpsiKstDet.OfflineVertexFitter.useResonanceVertex = False
+Bd2JpsiKstDet.ReFitPVs = True
+Bd2JpsiKstDet.DaughtersCuts = {"J/psi(1S)" : JpsiCuts, "K*(892)0" : KstCuts }
+                               
+Bd2JpsiKstDet.CombinationCut = "ADAMASS('B0') < 500.*MeV"
+Bd2JpsiKstDet.MotherCut = "(MIPCHI2DV(PRIMARY)<25)"
+
+############################################
+# Create StrippingLine with this selection #
+############################################
+
+Bd2JpsiKstDetLine = StrippingLine('Bd2JpsiKstDetLine'
+               , prescale = 1
+               , algos = [Bd2JpsiKstDet]
+               , stream = 'BExclusive'
+               )
