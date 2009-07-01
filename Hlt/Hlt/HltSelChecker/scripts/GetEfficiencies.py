9ca4ba004bb08a646494e2babf84bd8c6e3cfe54
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-07-01 18:30:45 +0000

    Better scripts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77180 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 19df5003940d83afd16f8e53eefe56a5103c02a9

--- Hlt/Hlt/HltSelChecker/scripts/GetEfficiencies.py
+++ Hlt/Hlt/HltSelChecker/scripts/GetEfficiencies.py
@@ -5,72 +5,78 @@
 ###
 import sys, os, re, glob
 ###############################################################################
-def getStats(f,signal,wiki):
+def getStats(f,signal):
 ###############################################################################
     """ returns the stats
     by Patrick Koppenburg"""
-#    print "ENTRY ", signal, sel
+    debug = False
+    if (debug): print "ENTRY ", signal, sel
     inTable = 0
-    BestSelEff = 0
-    BestHlt2Sel = 0
-    BestSel = "None"
     foundTable = 0
     Track = 0
     EqualL = 0
-    Hlt2EffAlone = 0 
+    Hlt2EffAlone = 0
+    BestSelDict = {}
+    epsilon = 0
     for line in f:
+      doPrint = False
       if inTable==0:
         if re.search("Correlation Table",line):
           inTable = 1
           foundTable = 1
       else:
-#        print inTable, line
+#        if (debug): print inTable, line
         if re.search("=======",line):
           EqualL+=1
           if (  EqualL==3 ):
             break
         else:  # in table
-          if re.search("L0",line):
-#            print "L0 line:", line
+          if (re.search("L0",line)):
+#            if (debug): print "L0 line:", line
             L0Eff = float((line.split()[2]).replace("%",""))
- #           print "L0", L0Eff
-          if re.search("Hlt1Global",line): 
- #           print line
+ #           if (debug): print "L0", L0Eff
+          elif (re.search("Hlt1Global",line)): 
+ #           if (debug): print line
             Hlt1Eff   = float((line.split()[2]).replace("%",""))
             Hlt1EffL0 = float((line.split()[4]).replace("%",""))
             Hlt1EffOn2 = float((line.split()[6]).replace("%",""))
- #           print "HltDecision", HltEff, HltEffL0
-          if ( re.search("Hlt2Global",line) and ( Hlt2EffAlone == 0 )): 
-#            print line
+ #           if (debug): print "HltDecision", HltEff, HltEffL0
+          elif ( re.search("Hlt2Global",line) and ( Hlt2EffAlone == 0 )): 
+ #           if (debug): print line
             Hlt2EffAlone = float((line.split()[4]).replace("%",""))
             Hlt2EffOn1   = float((line.split()[5]).replace("%",""))
             Trigger      = Hlt2EffAlone*Hlt1Eff/100
             HltEff       = 100.*Trigger/L0Eff
- #           print "Hlt2EffAlone ", Hlt2EffAlone, Hlt2EffOn1, Trigger, HltEff
-          if re.search("Decision",line) and re.search("Hlt2",line):
- #             print line
-              selection = (line.split()[1])
-              Hlt2Sel = float((line.split()[4]).replace("%",""))
-              if ( Hlt2Sel > BestHlt2Sel ):    # should be a dict
- #                 print selection, Hlt2Sel, "better than", BestSel
-                  BestHlt2Sel = Hlt2Sel
-                  BestSel = selection.replace("Decision","")
-                  if (( line.split()[-1]).find('#')<0) :
-                      BestSelEff = float((line.split()[-1]).replace("%",""))
-                  else: SelEff = 0
-                  #                print selection, Hlt2Sel, SelEff
-          if re.search("FilterTrueTracks",line): 
- #           print line
+ #           if (debug): print "Hlt2EffAlone ", Hlt2EffAlone, Hlt2EffOn1, Trigger, HltEff
+          elif (re.search("FilterTrueTracks",line)): 
+ #           if (debug): print line
             Track = float((line.split()[4]).replace("%",""))
- #           print "Tracking", Track
-    if foundTable == 1:
-        if ( wiki ):
- #           print Track, BestSelEff, BestHlt2Sel
- #           print Hlt2EffAlone, int(Hlt2EffAlone+0.5)
-            print "| <nox>"+signal+"  | <nox>"+BestSel+"  |  ",int(L0Eff+0.5),"% |  ",int(Hlt1EffL0+0.5),"% |   ",int(Track+0.5),"% |  ",int(BestSelEff+0.5),"% |   ",int(BestHlt2Sel+0.5),"% |   ",int(Hlt2EffAlone+0.5),"% |   ",int(Hlt2EffOn1+0.5),"% |   ",int(HltEff+0.5),"% |   ",int(Trigger+0.5),"% | "
-        else :
-            print signal.replace("_","\_"), "&", sel.replace("_","\_"), "&", int(L0Eff+0.5), "\\% &", int(Hlt1EffL0+0.5), "\\% & ", int(Track+0.5), "\\% &", int(SelEff+0.5), "\\% & ", int(Hlt2Sel+0.5), "\\% & ", int(Hlt2EffAlone+0.5), "\\% & ", int(Hlt2EffOn1+0.5), "\\% & ", int(HltEff+0.5), "\\% & ", int(Trigger+0.5), "\\% \\\\"
+ #           if (debug): print "Tracking", Track
+          elif ((re.search("Decision",line)) and (re.search("Hlt2",line))):
+            epsilon = epsilon + 0.00001
+            if (debug): print line
+            sel =  (line.split()[1]).replace("Hlt2","").replace("Decision","")
+            Hlt2Sel = float((line.split()[4]).replace("%",""))+epsilon
+            if (debug): print sel, Hlt2Sel
+            BestSelDict[sel] = Hlt2Sel # make sure they never get same efficiency
+#            if (( line.split()[-1]).find('#')<0) :
+#                SelEff = float((line.split()[-1]).replace("%",""))  # track
+    if foundTable:
+        s0,v0 = sortedDictValues(BestSelDict,0)
+        s1,v1 = sortedDictValues(BestSelDict,1)
+        s2,v2 = sortedDictValues(BestSelDict,2)
+        s3,v3 = sortedDictValues(BestSelDict,3)
+        print "| <nox>"+signal," | ",int(Trigger+0.5),"%  |  ",int(L0Eff+0.5),"% |   ",int(HltEff+0.5),"% |   ",int(Hlt1EffL0+0.5),"% |  ",int(Hlt2EffAlone+0.5),"% |   ",int(Hlt2EffOn1+0.5),"% |  ",int(Track+0.5),"% | <nox>"+s0+":",int(v0+0.5),"%  | <nox>"+s1+":",int(v1+0.5),"%  | <nox>"+s2+":",int(v2+0.5),"%  | <nox>"+s3+":",int(v3+0.5), "%  |"
 
+def sortedDictValues(adict,item):
+    array = sorted(adict.values())
+#    print array
+    val = array[-1-item]
+#    print "looking for ", val
+    for k, v in adict.iteritems():
+        if (v==val): key=k
+    return key,val
+        
 ###############################################################################
 # Main program
 ###############################################################################
@@ -79,18 +85,8 @@ def getStats(f,signal,wiki):
 #
 mainhead = "<unset>"
 wiki = True
-if ( len(sys.argv) == 3 ):
-    if ( sys.argv[1] == "--tex" ):
-        wiki = False
-        mainhead = sys.argv[2]
-    elif ( sys.argv[2] == "--tex" ):
-        wiki = False
-        mainhead = sys.argv[2]
 if ( len(sys.argv) == 2 ):
-    if ( sys.argv[1] == "--tex" ):
-        wiki = False
-    else :
-        mainhead = sys.argv[1]
+    mainhead = sys.argv[1]
 #
 #
 #
@@ -99,44 +95,23 @@ if ( mainhead == "<unset>" ):
 else :
     head = mainhead
 files = sorted(glob.glob(head+'-*'))
-if ( wiki ):
-    print "| *Signal*  | *Best Selection*  | *L0* | *Hlt1* | *Tk* | *Sel/Tk* | *Sel* | *Hlt2* | *Hlt2/1* | *Hlt* | *Trigger* |"    
-else :
-    print "\\begin{slide}{Signal efficiencies of HLT 2 after HLT 1}"
-    print "\\topleft{\\scalebox{0.5}{\\begin{tabular}{ll|CC|CCC|C|C||| LLLLLL}"
-    print "Signal & Sel & \\text{L0} & \\text{Tk} & \\text{Sel}|\\text{Tk} & \\text{Sel} & \\text{Hlt2} & \\text{Hlt2}\\left|\\right.\\text{1} & \\text{Hlt1} & \\text{Hlt} && \\text{Trigger} \\\\"
-    print "\\hline"
+print "| *Signal*  | *Trigger* | *L0* | *Hlt* | *Hlt1* | *Hlt2* | *Hlt2/1* | *Tk* | *Best Sel* | *2nd Sel* | *3rd Sel* | *4th Sel* |"    
 for f in files :
     s = f.split('-')[1]
+    #        print f, s
+    
     f  = open(f, 'r')
-    getStats(f,s,wiki)
-if ( wiki ):
-    print "   $ *Signal* : Signal sample as described in =Hlt/HltSelChecker= options.  "
-    print "   $ *Best Selection* : Most efficient Hlt2 selection. Not necessarily the one you want..."
-    print "   $ *L0* : L0 efficiency."
-    print "   $ *Hlt1* : Hlt1 efficiency."
-    print "   $ *Tk* : Hlt2 tracking efficiency."
-    print "   $ *Sel/Tk* : Efficiency of best Hlt2 selection given tracks are found."
-    print "   $ *Sel* : Hlt2 best selection efficiency, product of two above."
-    print "   $ *Hlt2* : Overall Hlt2 efficiency including all selections."
-    print "   $ *Hlt1/Hlt1* : Hlt2 efficiency given Hlt1."
-    print "   $ *Hlt* : Overall Hlt efficiency after L0."
-    print "   $ *Trigger* : Overall Trigger efficiency."
-    print " "
-    print "All efficiencies after L0 and offline (except L0 of course). Null efficiencies are due to missing or incorrect Hlt2 selections. To know what is exactly done and what offline selection is used see =HltSelChecker=. "
-    print " "
-else:
-    print "\\end{tabular}}}"
-    print "\\skipit{"
-    print "\\topright{\\smallpage{0.25}{\\begin{description}"
-    print "\\item[L0:] L0 eff."
-    print "\\item[Tk:] Total track eff (for L0 yes)."
-    print "\\item[Sel:] Eff. of dedicated Hlt2 selection."
-    print "\\item[Hlt2:] Hlt2 Eff."
-    print "\\item[Hlt1:] Hlt1 eff."
-    print "\\item[Trigger:] L0$\\times$ Hlt1$\\times$ Hlt2"
-    print "\\end{description}"
-    print "}}"
-    print "}% skipit"
-    print "\\end{slide}"
-    print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    getStats(f,s)
+    
+print "   $ *Signal* : Signal sample as described in =Hlt/HltSelChecker= options.  "
+print "   $ *Trigger* : Overall Trigger efficiency."
+print "   $ *L0* : L0 efficiency."
+print "   $ *Hlt* : Hlt efficiency on L0 events."
+print "   $ *Hlt1* : Hlt1 efficiency on L0 events."
+print "   $ *Hlt2* : Overall Hlt2 efficiency including all selections."
+print "   $ *Hlt2/Hlt1* : Hlt2 efficiency given Hlt1."
+print "   $ *Tk* : Hlt2 tracking efficiency on all tracks."
+print "   $ *Sel* : Name and efficiency  (on Hlt1 accepted events) of 4 best Hlt2 selections."
+print " "
+print "All efficiencies after L0 and offline (except L0 of course). Null efficiencies are due to missing or incorrect Hlt2 selections. To know what is exactly done and what offline selection is used see =HltSelChecker=. "
+print " "
--- Hlt/Hlt/HltSelChecker/scripts/SelsPlots.C
+++ Hlt/Hlt/HltSelChecker/scripts/SelsPlots.C
@@ -33,14 +33,31 @@ void tex_header(){
 //
 //-----------------------------------------------------------------------------
 //
-void wiki_header(){
-  std::cout << "|  *Selection*  |  *No Hlt1*  |  *Rate*  |  *Weighted*  |  *# Coll.*  |  *% b*  |" << std::endl ;
+void wiki_header(bool showHlt1){
+  if ( showHlt1){
+    std::cout << "|  *Selection*  |  *No Hlt1*  |  *Rate*  |  *Weighted*  |  *# Coll.*  |  *% b*  |" << std::endl ;
+  } else {
+    std::cout << "|  *Selection*  |  *Rate*  |  *Weighted*  |  *# Coll.*  |  *% b*  |" << std::endl ;
+  }
 };
 //
 //-----------------------------------------------------------------------------
 //
 void tex_footer(){
   std::cout << "}\\end{tabular}}}}" << std::endl ;
+};
+//
+//-----------------------------------------------------------------------------
+//
+void wiki_footer(bool showHlt1){
+  std::cout << std::endl ;
+  if (showHlt1) std::cout << "   $ *No Hlt1* : Hlt2 rate without running Hlt1 (but L0)" << std::endl ;
+  std::cout << "   $ *Rate* : Hlt2 rate after Hlt1. " << std::endl ;
+  std::cout << "   $ *Weighted* : Hlt2 rate after Hlt1 and re-weighting all events by the number of";
+  std::cout << "selections that have selected it (i.e. how much does this selection contribute to the rate?).";
+  std::cout << "The sum should be equal to the Hlt2 rate. *This is probably the number you want*." << std::endl ;
+  std::cout << "   $ *# coll* : Average number of pp collision in selected events (the lower the better)" << std::endl ;
+  std::cout << "   $ *% b* : Fraction of b quark in selected events (the higher the better)" << std::endl ;
 };
 //
 //-----------------------------------------------------------------------------
@@ -58,9 +75,8 @@ void tex_newpage(int i){
 //
 //-----------------------------------------------------------------------------
 //
-void tex_printRate(double rate, double err, bool exception, double Hard){
+void tex_printRate(double rate, double err, bool exception){
   if ( exception )   cout << "\\blue " ;
-  else if ( rate > Hard ) cout << "\\red " ;
   cout << int(rate+0.5) ;
   if (int(err+0.5)>=1) std::cout << "\\pm" << int(err+0.5)  ;
   cout << "\\:\\text{Hz}} & {" ;
@@ -68,9 +84,8 @@ void tex_printRate(double rate, double err, bool exception, double Hard){
 //
 //-----------------------------------------------------------------------------
 //
-void wiki_printRate(double rate, double err, bool exception, double Hard){
-  if ( exception )   cout << "%BLUE%" ;
-  else if ( rate > Hard ) cout << "%RED%" ;
+void wiki_printRate(double rate, double err, bool hlt1){
+  if ( hlt1 )   cout << "%BLUE%" ;
   if ( rate < 0.0001 ){
     // print nothing
   } else if ( err < 0.5 ){ // tiny rates
@@ -82,7 +97,7 @@ void wiki_printRate(double rate, double err, bool exception, double Hard){
   } else { // normal rates
     cout << int(rate+0.5) << "&plusmn;" <<int(err+0.5) << " Hz" ;
   }
-  if (( rate > Hard ) || (exception)) cout << " %ENDCOLOR%  |  " ;
+  if (hlt1) cout << " %ENDCOLOR%  |  " ;
   else cout << "  |  " ;
 }
 //
@@ -108,34 +123,20 @@ double SumWeights(TH1D* h, double& err){
 //
 //-----------------------------------------------------------------------------
 //
-double isException(TString ss, bool isHlt2Selection){
-    return( (!isHlt2Selection) || 
-            (ss.Contains("DiMuon")) || 
-            (ss.Contains("DiElectron")) ||
-            (ss.Contains("Topo")) ||
-            (ss.Contains("Inc")) ||
-            (TString("Hlt2Dstar").Contains(ss)) || 
-            (TString("Hlt2SingleMuon").Contains(ss)) || 
-            (TString("Hlt2B2MuTrack").Contains(ss)) || 
-            (TString("Hlt2UnbiasedJPsi").Contains(ss)) || 
-            (TString("Hlt2B2MuTrackNoPT").Contains(ss)));
-}
-//
-//-----------------------------------------------------------------------------
-//
 void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
 
   
-  double Limit = 10 ; // Hz
-  double ratio12 = 2. ; // not good if Hlt2 rate without 1 is too much larger
-
   double L0eff = 1.0*MB12->GetEntries("L0Decision==1")/MB12->GetEntries("L0Decision>=0") ;
   if ( L0eff < 0.95) cout << "Warning :: L0 is " << 100*L0eff << "%!\n" << endl ;
-  double Hlt1eff = 1.0*MB12->GetEntries("Hlt1Global==1")/MB12->GetEntries("L0Decision==1") ;
-  double normalisation = 1000000./MB12->GetEntries("L0Decision==1")  ;
+  int L0events = MB12->GetEntries("L0Decision==1");
+  int Hlt1events = MB12->GetEntries("Hlt1Global==1");
+  double Hlt1eff = 1.0*Hlt1events/L0events ;
+  double normalisation = 1000000./L0events  ;
+  bool showHlt1 = true ;
   if ( Hlt1eff > 0.5 ){
-    cout << "Hlt1 eff is " << 100*Hlt1eff << "%. Assuming it is already applied at 40 kHz\n" << endl ;
-    double normalisation = 40000./MB12->GetEntries("Hlt1Global==1")  ;
+    cout << "Hlt1 eff is " << 100*Hlt1eff << "%. Assuming it is already applied at " << 40000.*Hlt1eff << " kHz\n" << endl ;
+    double normalisation = 40000.*Hlt1eff/MB12->GetEntries("Hlt1Global==1")  ;
+    showHlt1 = false ;
   } else {
     cout << "Hlt1 eff is " << 100*Hlt1eff << "%. Assuming it is not already applied.\n" << endl ;
   }
@@ -143,7 +144,7 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
   if ( wiki ){
     cout << "Using " << MB12->GetEntries("L0Decision==1") << " events. Normalisation factor is " 
          << normalisation << endl ;
-    wiki_header();
+    wiki_header(showHlt1);
   } else {
     cout << "% normalisation is " << normalisation << endl ;
     tex_separator();
@@ -200,19 +201,19 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
     bool isHlt2Selection = ((ss.Contains("Hlt2")) && (ss.Contains("Decision"))) ;
     bool isHlt1Selection = ((ss.Contains("Hlt1")) && (ss.Contains("Decision"))) ;
     ss.ReplaceAll("Decision","");
-    bool exception = isException(ss, isHlt2Selection);
     if (!wiki) ss.ReplaceAll("_","\\_");
 
     double rateNo = MB12->Draw("L0Decision",FullName+"==1") ;
-
-    double errNo    = sqrt(rateNo)*normalisation;
-
-    //    cout << sels[s] << " " << rateNo << " " << errNo << endl ;
+    double rr = 1.0*rateNo/L0events ;
+    double errNo = sqrt(rr*(1-rr)/L0events)*L0events*normalisation;
+    
+    // cout << sels[s] << " " << rateNo << " " << errNo << " " << L0events << " " << normalisation << endl ;
 
     if (s==0) errNo = 0 ;
     rateNo *= normalisation ;
     double rateWithHlt1 = MB12->Draw("Hlt2nSelections >> HnHltSels",FullName+"==1 && Hlt1Global==1 && L0Decision==1");
-    double errWithHlt1  = sqrt(rateWithHlt1)*normalisation;
+    rr = 1.0*rateWithHlt1/Hlt1events ;
+    double errWithHlt1  = sqrt(rr*(1-rr)/Hlt1events)*normalisation*Hlt1events  ;
     rateWithHlt1 *= normalisation ;
     // -1 because of lumi
     if ( isHlt1Selection ) MB12->Draw("Hlt1nSelections-1 >> HnHltSels",FullName+"==1 && Hlt1IgnoringLumiDecision==1");
@@ -256,24 +257,20 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
     if ( wiki ){
       if (rateWithHlt1<0.01) {
         inactive.push_back(ss) ;
-	continue ;
+        continue ;
       }	
       std::cout << "| " ;
-      if (exception) std::cout << "%BLUE%" ;
+      if (isHlt1Selection) std::cout << "%BLUE%" ;
       std::cout << "<nox>" << ss  ; 
-      if (exception) std::cout << "%ENDCOLOR% |  " ;
+      if (isHlt1Selection) std::cout << "%ENDCOLOR% |  " ;
       else std::cout << " |  " ;
-      if ( s<2 ){
-        std::cout << "  |  " ;
-        wiki_printRate(rateNo,errNo,exception,ratio12*rateWithHlt1); // Hlt2 should not be too affected by Hlt1
-        std::cout << "  |  " ;
-      } else {
-        if (ss.Contains("Hlt2")) wiki_printRate(rateNo,errNo,exception,ratio12*rateWithHlt1);
+      if ( showHlt1 ){
+        if (ss.Contains("Hlt2")) wiki_printRate(rateNo,errNo,isHlt1Selection);
         else std::cout << "  |  " ;
-        wiki_printRate(rateWithHlt1,errWithHlt1,exception,Limit);
-        if (s>2) wiki_printRate(Weighted,errWeighted,exception,Limit);
-        else std::cout << "  |  " ;
-      } 
+      }
+      wiki_printRate(rateWithHlt1,errWithHlt1,isHlt1Selection);
+      if (s>2) wiki_printRate(Weighted,errWeighted,isHlt1Selection);
+      else std::cout << "  |  " ;
       if ( rateWithHlt1 > 0.0001 ) std::cout << (int(Hcolls->GetMean()*10.))/10. ;
       std::cout    << "  |  " ;
       if ( rateWithHlt1 > 0.0001 ) std::cout << (int(100*Hbfracs->GetMean())) << "%" ;
@@ -282,17 +279,17 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
       ss.ReplaceAll("Hlt2","\\quad ");
       if ( rateWithHlt1<0.01 ) std::cout << "% " ;
       else if ( s>2 ) nlines++ ;
-      if (exception) std::cout << "{\\bf " ;
+      if (isHlt1Selection) std::cout << "{\\bf " ;
       else std::cout << "{" ;
       std::cout << ss  << "} & {" ;
       if ( s<2 ){
         std::cout << "} & {" ;
-        tex_printRate(rateNo,errNo,exception,Limit);
+        tex_printRate(rateNo,errNo,isHlt1Selection);
         std::cout << "} & {" ;
       } else {
-        tex_printRate(rateNo,errNo,exception,Limit);
-        tex_printRate(rateWithHlt1,errWithHlt1,exception,Limit);
-        if ( s>2 ) tex_printRate(Weighted,errWeighted,exception,Limit);
+        tex_printRate(rateNo,errNo,isHlt1Selection);
+        tex_printRate(rateWithHlt1,errWithHlt1,isHlt1Selection);
+        if ( s>2 ) tex_printRate(Weighted,errWeighted,isHlt1Selection);
         else std::cout << "} & {" ;
       } 
       if ( Hcolls->GetMean()-Hcolls->GetMeanError()>avgColl_12) std::cout << "\\red " ;
@@ -313,7 +310,10 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
     tex_separator();
     cout << "% Sum of rates is " << sumRates << ", and weights is " << sumWeighted 
          <<" =? " << Hlt2Global << endl ;
+  } else {
+    wiki_footer(showHlt1) ;
   }
+  
   cout << endl ;
 
   if (!inactive.empty()){
@@ -326,7 +326,7 @@ void SelsPlots(TChain* MB12, bool wiki=true, char* DEBUG=""){
   
   if (!wiki) cout << "% Sum of rates is " << sumRates << ", and weights is " << sumWeighted 
                  <<" =? " << Hlt2Global << endl ;
-  else  cout << "Check : Sum of rates is " << sumRates << ", and weights is " << sumWeighted 
+  else  cout << "\nCheck : Sum of rates is " << sumRates << ", and weights is " << sumWeighted 
              << "Hz ( =? " << Hlt2Global << ")" << endl ;
 
   gPad->SetLogy(0);
