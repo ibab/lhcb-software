ae91eb8c95b1b54b5a4a2fc5472482d1b230ea25
Matthew David Needham <mneedham@cern.ch>
2009-07-03 13:39:08 +0000

    Add printout to STSelectChannelIDByElement
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@27813 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7cfe5eb22f20c12420fc5f0203b5d5ed941359c4

--- Lbcom/ST/STTools/src/STSelectChannelIDByElement.cpp
+++ Lbcom/ST/STTools/src/STSelectChannelIDByElement.cpp
@@ -1,4 +1,4 @@
-// $Id: STSelectChannelIDByElement.cpp,v 1.1 2009-03-14 09:16:35 mneedham Exp $
+// $Id: STSelectChannelIDByElement.cpp,v 1.2 2009-07-03 13:39:08 mneedham Exp $
  
 // Kernel
 #include "GaudiKernel/ToolFactory.h"
@@ -31,16 +31,21 @@ StatusCode STSelectChannelIDByElement::initialize() {
   StatusCode sc = ST::ToolBase::initialize();
   if (sc.isFailure()) return Error("Failed to initialize", sc);
 
+  if (m_elementNames.size() == 0){
+    info() << name() << "Empy list given: default to selecting all " << endmsg;
+  }
+
   BOOST_FOREACH(std::string name, m_elementNames) {
     DeSTBaseElement* detElement = tracker()->findTopLevelElement(name);
     if (detElement == 0){
       return Error("Failed to find detector element",StatusCode::FAILURE); 
     }
     else {
+      debug() << "Adding " <<  name << " to element list" << endmsg;     
       m_detElements.push_back(detElement);
     }
   } // for each
-
+  
   return StatusCode::SUCCESS;
 }
 
@@ -54,6 +59,8 @@ bool STSelectChannelIDByElement::select( const LHCb::STChannelID& id ) const{
 }
   
 bool STSelectChannelIDByElement::operator()( const LHCb::STChannelID& id ) const{
+
+  if (m_elementNames.size() == 0u) return true;
 
   std::vector<DeSTBaseElement*>::const_iterator iterElem = m_detElements.begin();
   for ( ; iterElem != m_detElements.end() ; ++iterElem){
