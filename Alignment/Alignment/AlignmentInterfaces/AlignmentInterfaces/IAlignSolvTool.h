e84c42c6a1ac2fe944aa280c954330da2d6aef21
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-07-06 12:51:53 +0000

    replaced SolvKernel by AlignKernel
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@88914 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 57fdc263a37dd97fb58a95543dc38180c51e89d1

--- Alignment/Alignment/AlignmentInterfaces/AlignmentInterfaces/IAlignSolvTool.h
+++ Alignment/Alignment/AlignmentInterfaces/AlignmentInterfaces/IAlignSolvTool.h
@@ -1,4 +1,4 @@
-// $Id: IAlignSolvTool.h,v 1.5 2009-05-12 18:15:24 wouter Exp $
+// $Id: IAlignSolvTool.h,v 1.6 2009-07-06 12:51:53 wouter Exp $
 #ifndef ALIGNSOLVTOOLS_IALIGNSOLVTOOL_H 
 #define ALIGNSOLVTOOLS_IALIGNSOLVTOOL_H 1
 
@@ -9,25 +9,25 @@
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
 
-// from SolvKernel
-#include "SolvKernel/AlVec.h"
-#include "SolvKernel/AlSymMat.h"
-#include "SolvKernel/AlMat.h"
+// from AlignKernel
+#include "AlignKernel/AlVec.h"
+#include "AlignKernel/AlSymMat.h"
+#include "AlignKernel/AlMat.h"
 
-static const InterfaceID IID_IAlignSolvTool ( "IAlignSolvTool", 1, 0 );
+static const InterfaceID IID_IAlignAlignTool ( "IAlignAlignTool", 1, 0 );
 
-/** @class IAlignSolvTool IAlignSolvTool.h Kernel/IAlignSolvTool.h
+/** @class IAlignAlignTool IAlignAlignTool.h Kernel/IAlignAlignTool.h
  *  
  *
  *  @author Adlene Hicheur
  *  @date   2006-09-20
  */
 
-class IAlignSolvTool : virtual public IAlgTool {
+class IAlignAlignTool : virtual public IAlgTool {
 public: 
 
   // Return the interface ID
-  static const InterfaceID& interfaceID() { return IID_IAlignSolvTool; }
+  static const InterfaceID& interfaceID() { return IID_IAlignAlignTool; }
   
   // Solves the system Ax=b for x. Returns A=A^{-1} and b=x.
   virtual bool compute(AlSymMat& A, AlVec&b) const = 0;
