e8f798e3318e65e5e0ca2315f5a3f53ada0fc24f
Angelo Carbone <angelo.carbone@bo.infn.it>
2009-07-08 08:13:57 +0000

    Add hlt2 B->hh selection
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77341 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7d8de93bd6dd89c5cff657c7a41dff658cbd5920

--- Hlt/Hlt/HltConf/python/HltConf/Configuration.py
+++ Hlt/Hlt/HltConf/python/HltConf/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for HltConf, to be invoked by Moore and DaVinci
 """
-__version__ = "$Id: Configuration.py,v 1.90 2009-06-24 09:53:35 gcowan Exp $"
+__version__ = "$Id: Configuration.py,v 1.91 2009-07-08 08:12:53 acarbone Exp $"
 __author__  = "Gerhard Raven <Gerhard.Raven@nikhef.nl>"
 
 from os import environ
@@ -33,6 +33,7 @@ from Hlt2Lines.Hlt2InclusiveMuonLines   import Hlt2InclusiveMuonLinesConf
 from Hlt2Lines.Hlt2InclusivePhiLines    import Hlt2InclusivePhiLinesConf
 from Hlt2Lines.Hlt2TopologicalLines     import Hlt2TopologicalLinesConf
 from Hlt2Lines.Hlt2XGammaLines          import Hlt2XGammaLinesConf
+from Hlt2Lines.Hlt2B2HHLines          import Hlt2B2HHLinesConf
 
 from HltConf.Hlt1             import Hlt1Conf
 from RichRecSys.Configuration import *
@@ -59,7 +60,8 @@ class HltConf(LHCbConfigurableUser):
                              , Hlt2InclusivePhiLinesConf
                              , Hlt2TopologicalLinesConf
                              , RichRecSysConf
-                             , Hlt2XGammaLinesConf  ]
+                             , Hlt2XGammaLinesConf
+                             , Hlt2B2HHLinesConf ]
     __slots__ = { "L0TCK"                      : ''
                 , "HltType"                    : 'Hlt1+Hlt2'
                 , "DataType"                   : '2009'
@@ -174,6 +176,7 @@ class HltConf(LHCbConfigurableUser):
                 Hlt2InclusivePhiLinesConf()
                 Hlt2TopologicalLinesConf()
                 Hlt2XGammaLinesConf()
+                Hlt2B2HHLinesConf()
                   
 
 
