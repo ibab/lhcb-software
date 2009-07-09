42eb2eb3c3dbe0d68a633c9f76f5e074b8aa49fd
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2009-07-09 23:47:57 +0000

    Updated and retuned B->DX stripping selection
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77404 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7ffe3d7847ce401918d4d671d7ca54e6c0698f25

--- Analysis/Phys/StrippingSelections/options/StrippingB2DPi.py
+++ Analysis/Phys/StrippingSelections/options/StrippingB2DPi.py
@@ -3,8 +3,43 @@
 # This is an example of a simple wrapper over user's own algorithm. 
 # No StrippingMember functionality is used here. 
 #
-# Still no StrippingMember functionality but being updated to do B->DX.
-# Will further update in stages.
+# Still no StrippingMember functionality now selects B->DX,
+# where X=pi,K,K*,phi, and D=D+,D0,Ds. The allowed decay modes
+# of the intermediates are K*->Kpi, phi->KK, D+->hhh (h=pi,K all combinations except KKK),
+# Ds->hhh (h=pi,K all combinations except KKK), D0->hh (h=pi,K all combinations).
+# The four body D0 decay modes are not in included for now but will be
+# included eventually. The selection is based on a combination of loose chi2 cuts
+# on the impact paramters and flight significances (wrt. the offline selections),
+# and some additional "robust" cuts which correlate with what is used in the trigger.
+# Note that since the HLT2 topological selection for these modes will use Chi2
+# cuts on the impact parameters and flight distances, there is no real motivation
+# for not including these cuts in the stripping, but they are nonetheless kept
+# loose.
+#
+# This selection deliberately uses no RICH information, since the channels in
+# the B->DX family are kinematically identical to each other and are the dominant
+# non-combinatoric backgrounds to each other.  
+#
+# A GEC is included to filter out extremely noisy events, it requires
+# the size of the Rec/Track/Best container to be less than 240.
+#
+# The selection is tuned on the L0xHLT1 sample of minbias, achieving a rate
+# of 7+-0.7 Hz for a L0xHLT1 output rate of 2kHz. Approximately 15% of this rate is
+# in the mass sidebands, with the remaining rate coming in the signal mass window.
+# The rates for other scenarios can be obtained by scaling
+# this number in an appropriate manner. Note that the expected signal rate after
+# the full offline selection(s) for the B->DX family of channels is ~0.2 Hz. 
+#
+# Further rate control is provided by the "robust" IP cut, set by default to
+# 50 microns (0.05mm) to correlate with what is done in the topological trigger.  
+#
+# The signal efficiency is tested on Bs->DsK and B0->D0K* channels. The combined
+# signal efficiency is found to be ~90%, with half of the loss coming from the GEC
+# and the other half from the "robust" cuts included to correlate with the trigger
+# which are not used in the offline selection.     
+# 
+# Will further update the selection in stages.
+#
 # Authors: J. Nardulli & V. Gligorov 
 
 from os import environ
@@ -14,25 +49,40 @@ from Configurables import CombineParticles, FilterDesktop
 from Configurables import TupleToolMCBackgroundInfo, TupleToolMCTruth, BackgroundCategory
 from StrippingConf.StrippingLine import StrippingLine
 
+# We need the Void filter for the GEC
+from Configurables import LoKi__VoidFilter as VoidFilter
+from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
+modules =  CoreFactory('CoreFactory').Modules
+for i in [ 'LoKiTrigger.decorators' ] :
+    if i not in modules : modules.append(i)
+
 ########################################################################
 #Define the shared cuts
-Dcut = "((M > 1768.*MeV) & (M < 2068.*MeV) & (BPVVDCHI2 > 36)) "
-DSignalcut = "((M > 1840.*MeV) & (M < 1895.*MeV)) | ((M > 1944.*MeV ) & (M < 1994.*MeV))"
-Dcombcut = "((APT > 1*GeV))"
+ipcut = "0.05" #robust IP cut used for rate control
 
+Dcut = "((M > 1768.*MeV) & (M < 2068.*MeV) & (BPVVDCHI2 > 36) & (BPVVD>1*mm) & (BPVVDR>0.2*mm)) "
+DSignalcut = "((M > 1840.*MeV) & (M < 1895.*MeV)) | ((M > 1944.*MeV ) & (M < 1994.*MeV))"
+
+Dptcut = "PT>1*GeV"
+Dcombcut = "(A" + Dptcut + ")"   #Common pt cut for D, K*, phi, second version to use
+                                 #before the vertexing  
 KPhiMothercut = "((VFASPF(VCHI2/VDOF) < 12) & (MIPCHI2DV(PRIMARY) > 4.))"
+
 Kstarcut = "(M > 0)"
 KstarSignalcut = "(M > 0)" #Removed K* mass cuts for B->DKpi Dalitz analysis 
-                          #funny coding for now, clean all this up 
+                           #funny coding for now, clean all this up 
+                           #this is also why we cannot use any common Kstar 
+
 Phicut = "(ADMASS('phi(1020)') < 100*MeV)"
 PhiSignalcut = "(ADMASS('phi(1020)') < 12*MeV)"
 
-Bcut = "((VFASPF(VCHI2/VDOF)<10)  & (BPVIPCHI2() < 36) & (BPVVDCHI2 > 4.) & (BPVDIRA > 0.9995) & (D2DVVDSIGN(1)>-3.0*mm))";
+Bcut = "((VFASPF(VCHI2/VDOF)<10)  & (BPVIPCHI2() < 36) & (BPVVDCHI2 > 4.) & (BPVDIRA > 0.9995))"
 Bcombcut = "((AM > 4800.*MeV) & (AM < 5900.*MeV))"
 BSignalcombcut = "((M > 5229.*MeV) & (M < 5329.*MeV)) | ((M > 5316.*MeV ) & (M < 5416.*MeV))"
 
-Daughtercut = "((TRCHI2DOF<10) & (PT >250*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.))"
-Bachelorcut = "((TRCHI2DOF<10) & (PT >350*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.))"
+Daughtercut = "((PT >250*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.) & (MIPDV(PRIMARY)>"+ipcut+"*mm))"
+Bachelorcut = "((PT >400*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMARY) > 4.) & (MIPDV(PRIMARY)>"+ipcut+"*mm))"
+PhiDaughterCut = "( 2 == NINTREE( (ABSID=='K+') & (" + Daughtercut + ")))" 
 ########################################################################
 ########################################################################
 #Now the various selections etc.
@@ -42,15 +92,16 @@ Bachelorcut = "((TRCHI2DOF<10) & (PT >350*MeV) & (P > 2*GeV) & (MIPCHI2DV(PRIMAR
 StrippingSelKstar = CombineParticles("StrippingSelKstar")
 StrippingSelKstar.InputLocations = ["StdNoPIDsPions","StdNoPIDsKaons"]
 StrippingSelKstar.DecayDescriptor = "[K*(892)0 -> K+ pi-]cc"
-StrippingSelKstar.DaughtersCuts =  { "K+"        : Daughtercut,"pi+"        : Daughtercut }
+StrippingSelKstar.DaughtersCuts =  { "K+"        : Daughtercut,
+                                     "pi+"        : Daughtercut }
 StrippingSelKstar.CombinationCut = Dcombcut
 StrippingSelKstar.MotherCut = KPhiMothercut
 ########################################################################
 # the Phi
 StrippingSelPhi = CombineParticles("StrippingSelPhi")
 StrippingSelPhi.InputLocations = ["StdNoPIDsKaons"]
-StrippingSelPhi.DecayDescriptor = "[phi(1020) -> K+ K-]cc"
-StrippingSelPhi.DaughtersCuts =  { "K+"        : Daughtercut }
+StrippingSelPhi.DecayDescriptor = "phi(1020) -> K+ K-"
+StrippingSelPhi.DaughtersCuts =  { "K+"        : Daughtercut}
 StrippingSelPhi.CombinationCut = Dcombcut
 StrippingSelPhi.MotherCut = KPhiMothercut
 ########################################################################
@@ -94,6 +145,12 @@ B2DXStrippingSel.DaughtersCuts = { "D+" : Dcut , "K*(892)0" : Kstarcut ,  "D0" :
 B2DXStrippingSel.CombinationCut = Bcombcut
 B2DXStrippingSel.MotherCut = Bcut
 ########################################################################
+# Define the GEC on the number of tracks, needed in order to control
+# the time for the combinatorics (especially once we add the 4-body D states)
+B2DXStrippingNumTracksGEC = VoidFilter('B2DXStrippingNumTracksGEC'
+                                  , Code = "TrSOURCE('Rec/Track/Best') >> (TrSIZE < 240 )"
+                                  )
+########################################################################
 B2DXStrippingSequence = GaudiSequencer("B2DXStrippingSequence")
 B2DXStrippingSequence.Members += [SubX]
 B2DXStrippingSequence.Members += [B2DXStrippingSel]
@@ -118,13 +175,16 @@ B2DXSignalStrippingSequence.Members += [B2DXSignalFilter]
 
 # Create the lines for this selection
 line = StrippingLine('B2DX'
-               , prescale = 0.01
-               ,  algos = [ B2DXStrippingSequence ]
+               , prescale = 1.0
+               ,  algos = [ B2DXStrippingNumTracksGEC, B2DXStrippingSequence ]
+               #,  algos = [ B2DXStrippingSequence ]
                ,  stream = 'BExclusive'
+               , postscale = 0.05
                )
 
 line = StrippingLine('B2DXSignal'
                , prescale = 1.
-               ,  algos = [ B2DXSignalStrippingSequence ]
+               #,  algos = [ B2DXSignalStrippingSequence ]
+               ,  algos = [ B2DXStrippingNumTracksGEC, B2DXSignalStrippingSequence ]
                ,  stream = 'BExclusive'
                )
