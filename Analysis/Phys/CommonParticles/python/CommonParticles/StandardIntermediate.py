5db773021c5a36bc80125235f5f5d27900ddd5b9
rlambert <rlambert@cern.ch>
2009-07-09 15:23:57 +0000

    added StdLooseDetachedKstar to StandardIntermediates
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77400 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f5d417e7c2a62953a118cd6d96b7938bade5a697

--- Analysis/Phys/CommonParticles/python/CommonParticles/StandardIntermediate.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StandardIntermediate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StandardIntermediate.py,v 1.4 2009-07-09 13:05:44 pkoppenb Exp $ 
+# $Id: StandardIntermediate.py,v 1.5 2009-07-09 15:23:57 rlambert Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StandardIntermediates.py
 #  configuration file for 'Standard Intermediate particles
@@ -11,7 +11,7 @@
 Configuration file for 'Standard Intermediate' particles 
 """
 __author__  = "Patrick Koppenburg"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.4 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.5 $"
 # =============================================================================
 
 _locations = {} 
@@ -32,6 +32,8 @@ _locations.update ( locations )
 from CommonParticles.StdLooseKs import *
 _locations.update ( locations )
 from CommonParticles.StdLooseKstar import *
+_locations.update ( locations )
+from CommonParticles.StdLooseDetachedKstar import *
 _locations.update ( locations )
 from CommonParticles.StdLoosePhi import *
 _locations.update ( locations )
