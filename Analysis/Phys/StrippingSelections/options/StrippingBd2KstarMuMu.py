0264c5834dde024975f216a53c4b84faaad72ba4
rlambert <rlambert@cern.ch>
2009-07-09 15:26:15 +0000

    inputlocation fix in K*MuMu
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77402 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8148565e02daca3f9281449126338a0513c22fe6

--- Analysis/Phys/StrippingSelections/options/StrippingBd2KstarMuMu.py
+++ Analysis/Phys/StrippingSelections/options/StrippingBd2KstarMuMu.py
@@ -34,18 +34,19 @@ from Configurables import CombineParticles, FilterDesktop
 # 
 Strip_loose_Bd2KstarMuMu = CombineParticles("Strip_loose_Bd2KstarMuMu")
 
-Strip_loose_Bd2KstarMuMu.InputLocations = ["StdLooseDiMuon", "StdLooseDetachedKstar"]
+Strip_loose_Bd2KstarMuMu.InputLocations = ["StdLooseDiMuon", "StdLooseDetachedKst2Kpi"]
 Strip_loose_Bd2KstarMuMu.DecayDescriptor = "[B0 -> K*(892)0 J/psi(1S)]cc"
 Strip_loose_Bd2KstarMuMu.DaughtersCuts = {
-    'K*(892)0':  "(BPVVDZ>-50) & "+
-                 "(INTREE((ABSID=='pi+') & ((PIDpi-PIDmu)>-10) & ((PIDpi-PIDK)>-10))) &"+
-                 "(INTREE((ABSID=='K+') & ((PIDK-PIDmu)>-10) & ((PIDK-PIDpi)>-5)))" ,
+    'K*(892)0':  ("(BPVVDZ>-50*mm) & "+
+                  "(INTREE((ABSID=='pi+') & ((PIDpi-PIDmu)>-10) & ((PIDpi-PIDK)>-10))) &"+
+                  "(INTREE((ABSID=='K+') & ((PIDK-PIDmu)>-10) & ((PIDK-PIDpi)>-5)))" ),
     'J/psi(1S)' : "(2 == NINTREE(((PIDmu-PIDpi)>-10) & ((PIDmu-PIDK)>-10) & (MIPCHI2DV(PRIMARY)>0)) )"
     }
 Strip_loose_Bd2KstarMuMu.CombinationCut = "(ADAMASS('B0')<300*MeV)"
-Strip_loose_Bd2KstarMuMu.MotherCut=( "(MIPDV(PRIMARY)<0.08) & (MIPCHI2DV(PRIMARY) < 150) & (PT>300*MeV) & (BPVDIRA>0.9998) & "+
+Strip_loose_Bd2KstarMuMu.MotherCut=( "(MIPDV(PRIMARY)<0.08*mm) & (MIPCHI2DV(PRIMARY) < 150) & (PT>300*MeV) & (BPVDIRA>0.9998) & "+
                                      "(VFASPF(VCHI2/VDOF)<64) & (DMASS('B0')<250*MeV) & (DMASS('B0')>-150*MeV)"
                                     )
+
 
 #
 # Filter the Bd for the ~10Hz stripping selection
@@ -58,7 +59,7 @@ filter_Bd2KstarMuMu_10Hz = StrippingMember (FilterDesktop
 		    	    		"(INTREE((ABSID=='K+') & (TRCHI2DOF < 36))) & "+
 		    	                "(INTREE((ABSID=='pi+') & (TRCHI2DOF < 36))) & "+
 		    	        	"(2 == NINTREE((ABSID=='mu+') & (MIPCHI2DV(PRIMARY)>4) & (TRCHI2DOF < 16))) & "+
-		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDZ>-50) & (BPVVDCHI2>25) & (VFASPF(VCHI2/VDOF)<36))) & "+
+		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDCHI2>25) & (VFASPF(VCHI2/VDOF)<36))) & "+
 		    	                "(PT>300*MeV) & (BPVDIRA>0.9998) & (BPVVDCHI2>36) & (VFASPF(VCHI2/VDOF)<36) "
 		             )
 
@@ -81,7 +82,7 @@ filter_Bd2KstarMuMu_2Hz = StrippingMember (FilterDesktop
 		    	    		"(INTREE((ABSID=='K+') & ((PIDK-PIDpi)>-5) & ((PIDK-PIDmu)>-5) & (TRCHI2DOF < 9))) & "+
 		    	        	"(2 == NINTREE((ABSID=='mu+') & ((PIDmu-PIDpi)>-5) & (TRCHI2DOF < 9))) & "+
 		    	                "(INTREE((ABSID=='pi+') & (TRCHI2DOF < 16))) & "+
-		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDZ>-30) & (VFASPF(VCHI2/VDOF)<16) & (PT>300*MeV))) & "+
+		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDZ>-30*mm) & (VFASPF(VCHI2/VDOF)<16) & (PT>300*MeV))) & "+
 		    	                "(INTREE((ABSID=='J/psi(1S)') & (BPVVDCHI2>25) )) & "+
 		    	                "(BPVDIRA>0.9999) & (BPVVDCHI2>64) & (MIPCHI2DV(PRIMARY) < 36) & (VFASPF(VCHI2/VDOF)<16) "
 		             )
@@ -102,9 +103,9 @@ filter_Bd2KstarMuMu_1Hz = StrippingMember (FilterDesktop
 		    	      , InputLocations = ["Strip_loose_Bd2KstarMuMu"]
 		    	      , Code = 
 		    	    		"(INTREE((ABSID=='K+') & ((PIDK-PIDpi)>-5) & ((PIDK-PIDmu)>-5))) & "+
-		    	        	"(2 == NINTREE((ABSID=='mu+') & ((PIDmu-PIDpi)>-5) & (MIPDV(PRIMARY) > 0.08))) & "+
-		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDZ>-5) & (PT>1000*MeV) & (MIPDV(PRIMARY) > 0.06))) & "+
-		    	                "(BPVDIRA>0.99995) & (VFASPF(VMINVDDV(PRIMARY))>1*mm) & (MIPDV(PRIMARY) < 0.06)"
+		    	        	"(2 == NINTREE((ABSID=='mu+') & ((PIDmu-PIDpi)>-5) & (MIPDV(PRIMARY) > 0.08*mm))) & "+
+		    	                "(INTREE((ABSID=='K*(892)0') & (BPVVDZ>-5*mm) & (PT>1000*MeV) & (MIPDV(PRIMARY) > 0.06*mm))) & "+
+		    	                "(BPVDIRA>0.99995) & (VFASPF(VMINVDDV(PRIMARY))>1*mm) & (MIPDV(PRIMARY) < 0.06*mm)"
 		             )
 #
 # Make the 1Hz Bd as a clone of 10Hz line
