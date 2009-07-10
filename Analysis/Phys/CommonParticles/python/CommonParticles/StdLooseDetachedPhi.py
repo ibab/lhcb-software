509446b39eb8d58b735e862812aa171ecdbe6f9e
Greig Cowan <g.cowan@ed.ac.uk>
2009-07-10 15:20:00 +0000

    Added vertex chi2 cut to phi of StdLooseDetachedPhi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77413 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9a732e22cb7cc401b02f7c1b8c3c82ff02617be

--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedPhi.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedPhi.py
@@ -1,6 +1,6 @@
 ##!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseDetachedPhi.py,v 1.1 2009-06-28 12:02:54 gcowan Exp $ 
+# $Id: StdLooseDetachedPhi.py,v 1.2 2009-07-10 15:19:54 gcowan Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseDetachedPhi2KK.py
 #  configuration file for 'Standard Loose Detached' 
@@ -11,7 +11,7 @@
 Configuration file for 'Standard Loose Detached (i.e., cut on K IP) Phi2KK'
 """
 __author__  = "Greig Cowan"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.2 $"
 # =============================================================================
 __all__ = (
     'StdLooseDetachedPhi2KK' ,
@@ -29,7 +29,7 @@ StdLooseDetachedPhi2KK.InputLocations = ["StdLooseKaons"]
 StdLooseDetachedPhi2KK.DecayDescriptor = "phi(1020) -> K+ K-" 
 StdLooseDetachedPhi2KK.DaughtersCuts = {"K+": "(MIPCHI2DV(PRIMARY) > 4.)"} 
 StdLooseDetachedPhi2KK.CombinationCut = "(AM < 1100.*MeV)"
-StdLooseDetachedPhi2KK.MotherCut = "ALL"
+StdLooseDetachedPhi2KK.MotherCut = "(VFASPF(VCHI2) < 25.0)"
 
 ## configure Data-On-Demand service 
 locations = updateDoD ( StdLooseDetachedPhi2KK )
