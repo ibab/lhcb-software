02be8e19240f3bdcdb5a97521ae78fa81e72d34a
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-07-14 09:09:24 +0000

    v1r10
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@25222 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b41b56b004a48cbc54bd601ba25457a807c9c352

--- Rec/Velo/VeloRecMonitors/src/VeloRecMonitors_dll.cpp
+++
@@ -1,8 +0,0 @@
-// $Id: VeloRecMonitors_dll.cpp,v 1.1 2009-06-03 13:17:37 cattanem Exp $
-// ============================================================================
-// Include files
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES( VeloRecMonitors );
-
-
