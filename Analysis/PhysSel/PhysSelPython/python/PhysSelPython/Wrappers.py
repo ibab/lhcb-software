3660678841c454fb5cb0e747b04bab883d11e365
jpalac <jpalac@cern.ch>
2009-07-14 12:30:33 +0000

    Add documentation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77440 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1076b228d88e2e3f03b88b1bf1330456e4c09390

--- Analysis/PhysSel/PhysSelPython/python/PhysSelPython/Wrappers.py
+++ Analysis/PhysSel/PhysSelPython/python/PhysSelPython/Wrappers.py
@@ -1,7 +1,38 @@
+#$Id: Wrappers.py,v 1.2 2009-07-14 12:30:33 jpalac Exp $
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer
+"""
+Wrapper classes for a DaVinci offline physics selection. The following classes
+are available:
+
+   - Selection          Wraps a selection configurable and the selections it
+                        requires
+   - SelectionSequence  Creates a sequence from a selection such that all the
+                        sub-selections required are executed in the right order
+"""
+__author__ = "Juan PALACIOS juan.palacios@nikhef.nl"
 
 class Selection :
+    """
+    Wrapper class for offline selection. Takes a top selection DVAlgorithm
+    configurable plus a list of required selection configurables. It uses
+    the required selections to set the InputLocations of the top selection.
+
+    Example: selection for A -> B(bb), C(cc)
+
+    # create and configure selection algorithm configurable:
+    A2B2bbC2cc = CombineParticles('A2B2bbC2cc')
+    A2B2bbC2cc.DecayDescriptor = ...
+    A2B2bbC2cc.MotherCut = ...
+    # now assume SelB and SelC have been defined in modules B2bb and C2cc
+    # respectively
+    from B2bb import SelB
+    from C2cc import SelC
+    # now create a Selection instance using the B and C selections:
+    from PhysSelPython.Wrappers import Selection
+    SelA2B2bbC2cc = Selection(A2B2bbC2cc, requiredSelecitons=[SelB, SelC]    
+    """
+    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
     def __init__(self, algo, requiredSelections ) :
         self.algo = algo
         self.requiredSelections = []
@@ -21,6 +52,25 @@ class Selection :
         return self.algo.name()
 
 class SelectionSequence :
+    """
+    Wrapper class for offline selection sequence. Takes a Selection object
+    corresponding to the top selection algorithm, and recursively uses
+    Selection.requiredSelections to for a GaudiSequences with all the required
+    selecitons needed to run the top selection.
+
+    Example: selection sequence for A -> B(bb), C(cc)
+
+    # Assume module A2B2bbC2cc defining a Selection object for the decay
+    # A -> B(bb), C(cc)
+    from A2B2bbC2cc import SelA2B2bbC2cc
+    from PhysSelPython.Wrappers import SelectionSequence
+    SeqA2B2bbC2cc = SelectionSequence(SelA2B2bbC2cc)
+    # use it
+    mySelSeq = SeqA2B2bbC2cc.sequence()
+    dv = DaVinci()
+    dv.UserAlgorithms = [mySelSeq]
+    """
+    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
     def __init__(self, TopSelection ) :
         self.algo = TopSelection.algorithm()
         self.seq = GaudiSequencer("Seq"+self.algo.name())
