3101132e9e7c27d926e80760444c616894f14244
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-07-14 08:48:59 +0000

    v2r22
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@19160 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0d8ea79282624d554f8cecc841af630c68234f78

--- Rec/Tr/TrackExtrapolators/src/TrackExtrapolators_dll.cpp
+++
@@ -1,8 +0,0 @@
-// $Id: TrackExtrapolators_dll.cpp,v 1.1 2005-03-10 14:41:03 hernando Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-LOAD_FACTORY_ENTRIES(TrackExtrapolators)
-
