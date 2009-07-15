2e7b7cb0455119db5a86b958b09ba4b5bb10290e
Gloria Corti <gloria.corti@cern.ch>
2009-07-15 17:54:59 +0000

    adapt to Gauss() configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94345 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6dcb50136a86a0e3519e5188cc5c95c7550a542a

--- Gauss/Sim/Gauss/options/Gauss.py
+++ Gauss/Sim/Gauss/options/Gauss.py
@@ -6,41 +6,10 @@
 # To specify an EventType you can give it as argument to follow
 #   gaudirun.py $GAUSSOPTS/Gauss.py $DECFILESROOT/options/30000000.opts
 #
-# Or for a slightly fancier version where the name of the file is automatically
-# set look at the example Gauss-Job.py and use it as
-#   gaudirun.py $GAUSSOPTS/Gauss.py $GAUSSOPTS/Gauss-Job.py
+# To set the random seeds and number of events do the following
+#   gaudirun.py $GAUSSOPTS/Gauss.py $DECFILESROOT/options/30000000.opts \
+#               $GAUSSOPTS/Gauss-Job.py
 #
 from Gauss.Configuration import *
 
-#--Allow to use ns (ie. double) in options of EventClockSvc
-ec = EventClockSvc()
-ec.addTool(FakeEventTime(), name="EventTimeDecoder")
-ec.EventTimeDecoder.StartTime = 0
-ec.EventTimeDecoder.TimeStep  = 25*SystemOfUnits.ns
-
-#--General Gauss setting and sequence
-importOptions('$GAUSSOPTS/Gauss.opts')
-
-#--Pack events and check consistency
-GaudiSequencer("Simulation").Members += [ PackMCParticle(),
-                                          PackMCVertex() ]
-testMCV = UnpackMCVertex( "TestMCVertex", OutputName= "MC/VerticesTest" )
-testMCP = UnpackMCParticle( "TestMCParticle", OutputName= "MC/ParticlesTest" )
-GaudiSequencer("Simulation").Members += [ testMCV, testMCP,
-                                         CompareMCParticle(),
-                                         CompareMCVertex() ]
-
-#--Configuration of output files and 'default' outputs files that can/should
-#--be overwritten in Gauss-Job.py
-spillOverList = ['']
-setOutputs(spillOverList)
-
-#--Geometry dependent options
-LHCbApp( Simulation = True )
-
-#--Geometry to be simulated options
-giGaGeo = GiGaGeo()
-giGaGeo.UseAlignment      = True
-giGaGeo.AlignAllDetectors = True
-importOptions('$GAUSSOPTS/SimVeloGeometry.py')  # -- To misalign VELO
-
+theApp = Gauss()
