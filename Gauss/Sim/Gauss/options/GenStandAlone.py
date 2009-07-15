7a801f51fcd680d4c6f1192879d03b610f59001e
Gloria Corti <gloria.corti@cern.ch>
2009-07-15 18:03:14 +0000

    replace opts with py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94347 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2e242a1dadb259b6885925d4017bb6f535d6fd53

---
+++ Gauss/Sim/Gauss/options/GenStandAlone.py
@@ -0,0 +1,17 @@
+## 
+##  Example on how to run only the generator phase
+##  It can be passed as additional argument to gaudirun.py directly
+##   > gaudirun.py $APPCONFIGOPTS/Gauss/MC09-b5TeV-md100.py \
+##                 $APPCONFIGOPTS/Conditions/MC09-20090602-vc-md100.py \
+##                 $DECFILESROOT/options/EVENTTYPE.opts \
+##                 $LBPYTHIAROOT/options/Pythia.opts \
+##                 $GAUSSOPTS/GenStandAlone.py \
+##                 $GAUSSOPTS/Gauss-Job.py
+##  or you can set the property in your Gauss-Job.py
+##  Port to python of GenStandAlone.opts
+## 
+
+from Gauss.Configuration import *
+
+Gauss().GenStandAlone = True
+
