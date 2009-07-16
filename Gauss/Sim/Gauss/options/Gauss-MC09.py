af1b8bef43fff228ef33b0476099c2217812b4c7
Gloria Corti <gloria.corti@cern.ch>
2009-07-16 16:06:20 +0000

    pick up conditiona s in production
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94360 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2277c7ab8c90e07717a75838d354e315a8122cfe

--- Gauss/Sim/Gauss/options/Gauss-MC09.py
+++ Gauss/Sim/Gauss/options/Gauss-MC09.py
@@ -1,4 +1,15 @@
-from Gauss.Configuration import *
+##############################################################################
+# File for running Gauss with MC09 configuration and beam conditions as in
+# production (5 TeV beams, nu=1, no spill-over)
+#
+# Syntax is:
+#   gaudirun.py Gauss-MC09.py <someInputJobConfiguration>.py
+##############################################################################
 
+from Gaudi.Configuration import *
+importOptions("$APPCONFIGOPTS/Gauss/MC09-b5TeV-md100.py")
+
+##############################################################################
+# Database tags must be set and are defined in a separate file
+##############################################################################
 importOptions('$APPCONFIGOPTS/Conditions/MC09-20090602-vc-md100.py')
-importOptions('$GAUSSOPTS/Gauss.py')
