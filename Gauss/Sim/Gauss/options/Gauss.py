0799b731b90ffe20946a15f01eb358883f9aadb8
Gloria Corti <gloria.corti@cern.ch>
2009-07-16 16:12:26 +0000

    example as template
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94361 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9da660bf151b95cc877f17d4092e52ef66bbf662

--- Gauss/Sim/Gauss/options/Gauss.py
+++ Gauss/Sim/Gauss/options/Gauss.py
@@ -1,15 +1,20 @@
-# Minimal file for running Gauss from python prompt
-#
+##############################################################################
+# File for running Gauss with default options
+##############################################################################
 # Syntax is:
-#   gaudirun.py $GAUSSOPTS/Gauss.py OtherSteerig.{opts,py}
-#
-# To specify an EventType you can give it as argument to follow
-#   gaudirun.py $GAUSSOPTS/Gauss.py $DECFILESROOT/options/30000000.opts
-#
-# To set the random seeds and number of events do the following
-#   gaudirun.py $GAUSSOPTS/Gauss.py $DECFILESROOT/options/30000000.opts \
-#               $GAUSSOPTS/Gauss-Job.py
-#
-from Gauss.Configuration import *
+#   gaudirun.py Gauss-Default.py <someInputForTheJob>.py
+##############################################################################
+
+from Configurables import Gauss, LHCbApp
+
+###############################################################################
+# Set here any steering options.
+# Available steering options and defaults are documented in
+# $GAUSSROOT/python/Gauss/Configuration.py
+###############################################################################
 
 theApp = Gauss()
+
+# You MUST also set the tag of the conditions
+#LHCbApp().DDDBtag = <yourChoice>
+#LHCbApp().ConDBtag = <yourChoice>
