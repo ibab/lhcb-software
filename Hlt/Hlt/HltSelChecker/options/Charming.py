764c067a8af3640262cd4ef8329316096cf409c7
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-07-20 17:24:07 +0000

    helper files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77553 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ef944d57e6e257f1ad57b8576917421dff4b2e3

---
+++ Hlt/Hlt/HltSelChecker/options/Charming.py
@@ -0,0 +1,8 @@
+### @file 
+ #  Append this to get appropriate scenario
+ #
+ #  @author P. Koppenburg
+ #  @date 2009-07-20
+ #
+from Configurables import DaVinci
+DaVinci().HltThresholdSettings = 'Hlt2_Charming'  # just an example
---
+++ Hlt/Hlt/HltSelChecker/options/Hadronic.py
@@ -0,0 +1,8 @@
+### @file 
+ #  Append this to get appropriate scenario
+ #
+ #  @author P. Koppenburg
+ #  @date 2009-07-20
+ #
+from Configurables import DaVinci
+DaVinci().HltThresholdSettings = 'Hlt2_Hadronic'  # just an example
