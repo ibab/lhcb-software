f2683543980f314d501ac383fddb6c954397cda3
jpalac <jpalac@cern.ch>
2009-07-27 13:33:44 +0000

    Remove interfacs from dictionaries. Now in DaVinciInterfaces.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@65105 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 88021095960c19fe92e4bfc58b934908ffdd75a5

--- Phys/Phys/DaVinciKernel/dict/DaVinciKernelDict.h
+++ Phys/Phys/DaVinciKernel/dict/DaVinciKernelDict.h
@@ -1,4 +1,4 @@
-// $Id: DaVinciKernelDict.h,v 1.14 2008-07-11 13:11:28 ibelyaev Exp $
+// $Id: DaVinciKernelDict.h,v 1.15 2009-07-27 13:33:44 jpalac Exp $
 #ifndef DICT_DAVINCIKERNELDICT_H 
 #define DICT_DAVINCIKERNELDICT_H 1
 
@@ -13,53 +13,6 @@
 // ============================================================================
 // DaVinciKernel
 // ============================================================================
-#include "Kernel/IBestParticles.h"
-#include "Kernel/IBremAdder.h"
-#include "Kernel/IBTaggingTool.h"
-#include "Kernel/ICaloParticleMaker.h"
-#include "Kernel/IChangePIDTool.h"
-#include "Kernel/ICheckOverlap.h"
-#include "Kernel/ICheckSelResults.h"
-#include "Kernel/IContextTool.h"
-#include "Kernel/IDecayFinder.h"
-#include "Kernel/IDecodeSimpleDecayString.h"
-#include "Kernel/IDirectionFit.h"
-#include "Kernel/IDistanceCalculator.h"
-#include "Kernel/IEventTupleTool.h"
-#include "Kernel/IFilterCriteriaTESTool.h"
-#include "Kernel/IFilterCriterion.h"
-#include "Kernel/IFilterParticles.h"
-#include "Kernel/IGeomDispCalculator.h"
-#include "Kernel/IJetMaker.h"
-#include "Kernel/ILifetimeFitter.h"
-#include "Kernel/IMassVertexFit.h"
-#include "Kernel/IMassFit.h"
-#include "Kernel/IOnOffline.h"
-#include "Kernel/IP2VVAngleCalculator.h"
-#include "Kernel/IP2VVPartAngleCalculator.h"
-#include "Kernel/IParticleArrayFilter.h"
-#include "Kernel/IParticle2State.h"
-#include "Kernel/IParticleCombiner.h"
-#include "Kernel/IParticleDescendants.h"
-#include "Kernel/IParticleFilter.h"
-#include "Kernel/IParticleMaker.h"
-#include "Kernel/IParticleReFitter.h"
-#include "Kernel/IParticleStuffer.h"
-#include "Kernel/IParticleTransporter.h"
-#include "Kernel/IParticleTupleTool.h"
-#include "Kernel/IPhysDesktop.h"
-#include "Kernel/IPlotTool.h"
-#include "Kernel/IProtoParticleFilter.h"
-#include "Kernel/IPVReFitter.h"
-#include "Kernel/IRelatedPVFinder.h"
-#include "Kernel/IRelatedPV.h"
-#include "Kernel/ISecondaryVertexTool.h"
-#include "Kernel/ITagger.h"
-#include "Kernel/ITriggerSelectionTisTos.h"
-#include "Kernel/ITriggerTisTos.h"
-#include "Kernel/IVertexFit.h"
-#include "Kernel/IPrintDecay.h"
-#include "Kernel/ISetInputParticles.h"
 #include "Kernel/DVAlgorithm.h" // Has to be last for compilation on Windows
 
 #endif // DICT_DAVINCIKERNELDICT_H
