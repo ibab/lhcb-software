35a7d06306eae3d0cd228f1676048bbbb961805e
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-07-31 11:41:57 +0000

    remove line
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77793 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 48a2a336f8153cba64253c541d5939aba84b8148

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2LLXLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2LLXLines.py
@@ -24,15 +24,17 @@ class Hlt2B2LLXLinesConf(HltLinesConfigurableUser) :
     selected with 100% efficiency, while 'background' areas are selected at x%
     efficiency in order to keep the output rate reasonable.
 
-    @todo Use topo 3-body robust as selection and follow with full fit 
+    The selectron strategy is to look for the presence of a dielectron or a dimuon,
+    which has a rate a a few 100 Hz. Then to make a full track fit using the settings of the
+    topological trigger and to rebuild a B->eeK candidate using the fitted tracks.
     """
 
-    __slots__ = { 'Prescale'       : { 'Hlt2Bu2eeK'           :  0.01 # full box
-                                     , 'Hlt2Bu2eeKJpsi'       :  0.1  # J/psi peak
-                                     , 'Hlt2Bu2eeKHighMass'   :  0.2  # high mass band
-                                     , 'Hlt2Bu2MuMuK'         :  0.01 # full box
-                                     , 'Hlt2Bu2MuMuKJpsi'     :  0.1  # J/psi peak
-                                     , 'Hlt2Bu2MuMuKHighMass' :  0.2  # high mass band
+    __slots__ = { 'Prescale'       : { 'Hlt2Bu2eeK'           :  1 # 0.01 # full box
+                                     , 'Hlt2Bu2eeKJpsi'       :  1 # 0.1  # J/psi peak
+                                     , 'Hlt2Bu2eeKHighMass'   :  1 # 0.2  # high mass band
+                                     , 'Hlt2Bu2MuMuK'         :  1 # 0.01 # full box
+                                     , 'Hlt2Bu2MuMuKJpsi'     :  1 # 0.1  # J/psi peak
+                                     , 'Hlt2Bu2MuMuKHighMass' :  1 # 0.2  # high mass band
                                      }
                 ,  'BFlightCHI2'        : 50        # adimentional 
                 ,  'BDIRA'              : 0.9998    # adimentional
@@ -58,95 +60,156 @@ class Hlt2B2LLXLinesConf(HltLinesConfigurableUser) :
         from Hlt2SharedParticles.GoodParticles import GoodKaons
         from Hlt2SharedParticles.DiMuon import DiMuon
         from Hlt2SharedParticles.DiElectron import DiElectron
+        
+        ###################################################################
+        # Electrons and kaons track fit
+        #
+        importOptions("$HLTCONFROOT/options/Hlt2TrackFitForTopo.py")
+        SeqHlt2TFParticlesForTopo = GaudiSequencer('SeqHlt2TFParticlesForTopo') # the sequencer that does the track fit
+        
+        ###################################################################
+        # CombineBu cuts
+        #
+        ### Kaon cut : (PT>1400*MeV) & (MIPCHI2DV(PRIMARY)>4)
+        kaoncut = "(PT> %(KaonPT)s *MeV) & (MIPCHI2DV(PRIMARY)> %(KaonIPCHI2)s )" % self.getProps()
+        #
+        ### lepton cut :(PT>1000*MeV)
+        leptoncut = "(PT> %(LeptonPT)s *MeV)" % self.getProps()
+        #
+        ### dilepton mass cut 
+        llmasscut   = "(AM < %(SignalBUpperMass)s *MeV)" % self.getProps()
+        #
+        ### dilepton cut (VFASPF(VCHI2/VDOF)<9)
+        llcut   = "(VFASPF(VCHI2/VDOF)<9)" 
+        # 
+        ### B mass cuts : Hard-coded as we _need_ the full B mass window for the final fit. Nobody dare touch that!
+        combcut = "(ADAMASS('B0')<600*MeV)"
+        # 
+        ### B candidate cuts : ((VFASPF(VCHI2/VDOF)<3) & (BPVIPCHI2()<25) & (BPVDIRA>0.9998) & (BPVVDCHI2>50))
+        bcut   = "((VFASPF(VCHI2/VDOF)< %(BVertexCHI2)s ) & (BPVIPCHI2()< %(BIPCHI2)s ) & (BPVDIRA> %(BDIRA)s ) & (BPVVDCHI2> %(BFlightCHI2)s ))" % self.getProps()
 
-        kaoncut = "(PT> %(KaonPT)s *MeV) & (MIPCHI2DV(PRIMARY)> %(KaonIPCHI2)s )" % self.getProps()   # (PT>1400*MeV) & (MIPCHI2DV(PRIMARY)>4)
-        llcut   = "(VFASPF(VCHI2/VDOF)<9) & (MINTREE((ABSID<20),PT)> %(LeptonPT)s *MeV)" % self.getProps()  # (VFASPF(VCHI2/VDOF)<9) & (MINTREE((ABSID<20),PT)>1*GeV)
-        combcut = "(ADAMASS('B0')<600*MeV)"  # Hard-coded as we _need_ the full B mass window for the final fit. Nobody dare touch that!
-        bcut   = "((VFASPF(VCHI2/VDOF)< %(BVertexCHI2)s ) & (BPVIPCHI2()< %(BIPCHI2)s ) & (BPVDIRA> %(BDIRA)s ) & (BPVVDCHI2> %(BFlightCHI2)s ))" % self.getProps() # ((VFASPF(VCHI2/VDOF)<3) & (BPVIPCHI2()<25) & (BPVDIRA>0.9998) & (BPVVDCHI2>50))
-
+        ###################################################################
+        #
+        # CombineEE : make fitted dielectron
+        #
+        combineEE = Hlt2Member( CombineParticles                               # type
+                              , "EE"                                           # name -- to be bound to the line name: Hlt2LineNameFilter
+                              , DecayDescriptor = "J/psi(1S) -> e+ e-"
+                              , DaughtersCuts = { "e-" : leptoncut }
+                              , CombinationCut = llmasscut
+                              , MotherCut = llcut
+                              , InputLocations = [ 'Hlt2TFElectronsForTopo' ]
+                              )
+        
+        ###################################################################
+        #
+        # CombineMM : make fitted dielectron
+        #
+        combineMM = Hlt2Member( CombineParticles                               # type
+                              , "MM"                                           # name -- to be bound to the line name: Hlt2LineNameFilter
+                              , DecayDescriptor = "J/psi(1S) -> mu+ mu-"
+                              , DaughtersCuts = { "mu-" : leptoncut }
+                              , CombinationCut = llmasscut
+                              , MotherCut = llcut
+                              , InputLocations = [ 'Hlt2TFMuonsForTopo' ]
+                              )
+        
+        ###################################################################
+        #
+        # CombineBu : apply hard cuts
+        #
         combineB = Hlt2Member( CombineParticles                               # type
                              , "Bu"                                           # name -- to be bound to the line name: Hlt2LineNameFilter
                              , DecayDescriptor = "[ B+ -> J/psi(1S) K+ ]cc"
-                             , DaughtersCuts = { "K+"        : kaoncut ,
-                                                 "J/psi(1S)" : llcut }
+                             , DaughtersCuts = { "K+" : kaoncut }
                              , CombinationCut = combcut
                              , MotherCut = bcut
-                             , InputLocations = [ GoodKaons, DiElectron ]
+                             , InputLocations = [ 'Hlt2TFKaonsForTopo', combineEE ]
                              )
+#        from Configurables import PrintHeader
+#        DEBUGPrintHeader1 = PrintHeader("DEBUGPrintHeader1")
+#        DEBUGPrintHeader2 = PrintHeader("DEBUGPrintHeader2")
         
-###########################################################################
-###########################################################################
+        ###########################################################################
         # Define the Hlt2 Line
         #
-        # First the 'whole' eeK line, precaled
+        # 1) The 'whole' eeK line, precaled
         #
         line_M = Hlt2Line('Bu2eeK'
                           , prescale = self.prescale
-                          ,  algos = [ DiElectron, GoodKaons , combineB ]
+# OLD                         ,  algos = [ DiElectron, GoodKaons , combineB ]
+                          ,  algos = [ DiElectron,
+                                       SeqHlt2TFParticlesForTopo,
+                                       combineEE,
+                                       combineB ]
                           )
-###########################################################################
+        ###########################################################################
         #
         # 2) Only the signal box. No precale.
+        #    That's ll mass < 3 GeV and B mass within 600 MeV
         #
-###########################################################################
+        ###########################################################################
         line_S = line_M.clone(   'Bu2eeKSignal'
                                  , prescale = 1
-                                 , Bu = {"DaughtersCuts":{ "K+":  kaoncut 
-                                                           , "J/psi(1S)" : llcut+" & (MM<3*GeV)" }   # don't change that
-                                         , "CombinationCut" :  combcut+" & (AM< %(SignalBUpperMass)s *MeV)" % self.getProps() }
+                                 , EE = { "CombinationCut" : "(AM<3*GeV)" } # don't change that (tightens combcut and hence overwrites
+                                 , Bu = { "CombinationCut" : combcut+" & (AM< %(SignalBUpperMass)s *MeV)" % self.getProps() }
                                  )
-###########################################################################
+        ###########################################################################
         #
         # 3) Only the J/psi box. Prescale by 10%
+        #    That's ll mass around the J/psi
+        #    @todo One might consider applying the SignalBUpperMass cut as well.
         #
-###########################################################################
+        ###########################################################################
         line_J = line_M.clone(   'Bu2eeKJpsi'
                                  , prescale = self.prescale
-                                 , Bu = {"DaughtersCuts":{ "K+": kaoncut 
-                                                           , "J/psi(1S)" : llcut+" & (ADMASS('J/psi(1S)')< %(JpsiMassWindow)s *MeV)" % self.getProps() }}
+                                 , EE = {  "CombinationCut" : "(ADAMASS('J/psi(1S)')< %(JpsiMassWindow)s *MeV)" % self.getProps() } 
                                  )
-###########################################################################
+        ###########################################################################
         #
         # 4) High mass combinatorics
+        #    That's the high-mass side-band for any ll mass
         #
-###########################################################################
+        ###########################################################################
         line_H = line_M.clone(   'Bu2eeKHighMass'
                                  , prescale = self.prescale
                                  , Bu = { "CombinationCut":"(AM> %(HighMassBLowerMass)s *MeV)" % self.getProps() }
                                  )
-        #
-###########################################################################
+        ###########################################################################
         #
         # 5 ) Now do MuMu. There are 4 lines as well.
         #
-###########################################################################
+        ###########################################################################
         #
         # The lines
         #
-### TODO: allow reference to entries in Hlt2Line._algos 'by name'...
-### with the above  the only real substitution would be  combineBu --> 'Bu'
-### where the 'object' is the original defined above, and the name refers to the named
-### entry (potentially modified by clone) of 'self._algos' where self is the instance of the
-### line...
+        ### TODO: allow reference to entries in Hlt2Line._algos 'by name'...
+        ### with the above  the only real substitution would be  combineB --> 'Bu'
+        ### where the 'object' is the original defined above, and the name refers to the named
+        ### entry (potentially modified by clone) of 'self._algos' where self is the instance of the
+        ### line...
+        #
         line_M.clone('Bu2MuMuK'
                      , prescale = self.prescale
-                     , algos = [ DiMuon, GoodKaons,  line_M._algos[-1] ]  # 'Bu' ]
-                     , Bu = { 'InputLocations' :  [ GoodKaons, DiMuon ]} 
+                     , algos = [ DiMuon, SeqHlt2TFParticlesForTopo, combineMM, line_M._algos[-1] ]  # 'Bu' ]
+                     , Bu = { 'InputLocations' :  [ 'Hlt2TFKaonsForTopo', combineMM ]} 
                      )
         line_S.clone('Bu2MuMuKSignal'
                      , prescale = 1
-                     , algos = [ DiMuon, GoodKaons,  line_S._algos[-1] ] # 'Bu' ]
-                     , Bu = { 'InputLocations' :  [ GoodKaons, DiMuon ]} 
+                     , algos = [ DiMuon, SeqHlt2TFParticlesForTopo, combineMM, line_S._algos[-1] ] # 'Bu' ]
+                     , MM = { "CombinationCut" : "(AM<3*GeV)" } # don't change that (tightens combcut and hence overwrites
+                     , Bu = { 'InputLocations' :  [ 'Hlt2TFKaonsForTopo', combineMM ]} 
                      )
         line_J.clone('Bu2MuMuKJpsi'
                      , prescale = self.prescale
-                     , algos = [ DiMuon, GoodKaons,  line_J._algos[-1] ]# 'Bu' ]
-                     , Bu = {'InputLocations' :  [ GoodKaons, DiMuon ]} 
+                     , algos = [ DiMuon, SeqHlt2TFParticlesForTopo, combineMM, line_J._algos[-1] ]# 'Bu' ]
+                     , MM = {  "CombinationCut" : "(ADAMASS('J/psi(1S)')< %(JpsiMassWindow)s *MeV)" % self.getProps() } 
+                     , Bu = {'InputLocations' :  [ 'Hlt2TFKaonsForTopo', combineMM ]} 
                      )
         line_H.clone('Bu2MuMuKHighMass'
                      , prescale = self.prescale
-                     , algos = [ DiMuon, GoodKaons,  line_H._algos[-1] ]# 'Bu' ]
-                     , Bu = {'InputLocations' :  [ GoodKaons, DiMuon ]} 
+                     , algos = [ DiMuon, SeqHlt2TFParticlesForTopo, combineMM, line_H._algos[-1] ]# 'Bu' ]
+                     , Bu = {'InputLocations' :  [ 'Hlt2TFKaonsForTopo', combineMM ]} 
                      )
         
         HltANNSvc().Hlt2SelectionID.update( { "Hlt2Bu2eeKDecision" : 50060 } )   # whole prescaled box
