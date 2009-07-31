d277efc329f8e34669404a3c19bcb728d357003c
Matthew David Needham <mneedham@cern.ch>
2009-07-31 14:58:10 +0000

    Fixes for windows
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@27893 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ead6b323327b60c2f1691715d38f0a2595cd6d4f

--- LHCb/ST/STKernel/src/STHistoToolBase.cpp
+++ LHCb/ST/STKernel/src/STHistoToolBase.cpp
@@ -5,12 +5,20 @@
  *  Implementation file for class : STHistoToolBase
  *
  *  CVS Log :-
- *  $Id: STHistoToolBase.cpp,v 1.1 2008-10-14 08:47:06 mneedham Exp $
+ *  $Id: STHistoToolBase.cpp,v 1.2 2009-07-31 14:58:10 mneedham Exp $
  *
  *  @author Matthew Needham    
  *  @date   2008-10-11
  */
 //-----------------------------------------------------------------------------
+
+// ============================================================================
+// Disable warning on windows about using 'this' in constructors
+#ifdef _WIN32
+#pragma warning ( disable:4355 )
+#endif
+// ============================================================================
+
 
 // local
 #include "Kernel/STHistoToolBase.h"
--- LHCb/ST/STKernel/src/STTupleAlgBase.cpp
+++ LHCb/ST/STKernel/src/STTupleAlgBase.cpp
@@ -5,7 +5,7 @@
  *  Implementation file for class : STTupleAlgBase
  *
  *  CVS Log :-
- *  $Id: STTupleAlgBase.cpp,v 1.1 2008-10-14 08:47:06 mneedham Exp $
+ *  $Id: STTupleAlgBase.cpp,v 1.2 2009-07-31 14:58:10 mneedham Exp $
  *
  *  @author Matthew Needham    
  *  @date   2008-10-11
@@ -13,6 +13,15 @@
 //-----------------------------------------------------------------------------
 
 // local
+
+// ============================================================================
+// Disable warning on windows about using 'this' in constructors
+#ifdef _WIN32
+#pragma warning ( disable:4355 )
+#endif
+// ============================================================================
+
+
 #include "Kernel/STTupleAlgBase.h"
 
 // ============================================================================
