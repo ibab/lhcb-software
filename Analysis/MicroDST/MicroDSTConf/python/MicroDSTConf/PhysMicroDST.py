c0358ba6f6a564b34a4d8f1648d353a270bdc8cc
jpalac <jpalac@cern.ch>
2009-07-31 11:35:33 +0000

    . Now possible to make many instances of a PhysMicroDST configurable.   . OutputPrefix property removed. Now taken from the name of the instance.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@77789 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0d489e7bf3cc60b189fdde5747daa5d30d7927f8

--- Analysis/MicroDST/MicroDSTConf/python/MicroDSTConf/PhysMicroDST.py
+++ Analysis/MicroDST/MicroDSTConf/python/MicroDSTConf/PhysMicroDST.py
@@ -1,7 +1,7 @@
 """
 High level configuration example for a typical physics MicroDST
 """
-__version__ = "$Id: PhysMicroDST.py,v 1.1 2009-07-23 13:25:54 jpalac Exp $"
+__version__ = "$Id: PhysMicroDST.py,v 1.2 2009-07-31 11:35:33 jpalac Exp $"
 __author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
 
 
@@ -15,7 +15,6 @@ class PhysMicroDST(LHCbConfigurableUser) :
     __slots__ = {
         "MicroDSTFile"           : "MicroDST.mdst"
         , "MicroDSTSelectionAlg" : ""
-        , "OutputPrefix"         : "microDST"
         , "CopyParticles"        : True
         , "CopyPVs"              : True
         , "CopyBTags"            : True
@@ -29,7 +28,6 @@ class PhysMicroDST(LHCbConfigurableUser) :
     _propertyDocDct = {  
         "MicroDSTFile"           : """ Write name of output MicroDST file. Default 'MicroDST.dst'"""
         , "MicroDSTSelectionAlg" : """            # Name of selection algorithm that defines data for MicroDST"""
-        , "OutputPrefix"         : """ """
         , "CopyParticles"        : """ """
         , "CopyPVs"              : """Copy Primary vertices and standard Particle->PV relaitons """
         , "CopyBTags"            : """ """
@@ -37,9 +35,12 @@ class PhysMicroDST(LHCbConfigurableUser) :
         , "CopyMCTruth"          : """ """
         }
 
+    def personaliseName(self, name) :
+        return name + "_" + self.name()
+
     def seqMicroDST(self) :
         from Configurables import GaudiSequencer
-        return GaudiSequencer('SeqMicroDST')
+        return GaudiSequencer('SeqMicroDST_'+ self.name() )
 
     def getMicroDSTSelAlg(self) :        
         seqName = self.getProp("MicroDSTSelectionAlg")
@@ -58,10 +59,12 @@ class PhysMicroDST(LHCbConfigurableUser) :
 
     def initMicroDSTStream(self) :
         from Configurables import OutputStream
-        log.info("Setting MicroDSTStream")
-        importOptions('$MICRODSTOPTS/MicroDSTStream.py')
-        stream = OutputStream('MicroDSTStream')
-        stream.OptItemList = ["/Event/"+self.outputPrefix()+"#99"]
+        streamName = 'MicroDSTStream'+ self.name()
+        log.info("Setting OutpurStream "+ streamName)
+        #importOptions('$MICRODSTOPTS/MicroDSTStream.py')
+        stream = OutputStream(streamName)
+        log.info(self.name()+ ": Will save everything under "+ "/Event/"+self.name())
+        stream.OptItemList = ["/Event/"+self.name()+"#99"]
         dstName = self.getProp("MicroDSTFile")
         if (dstName != "" ) :
             stream.Output = "DATAFILE='"+dstName +"' TYP='POOL_ROOTTREE' OPT='REC'"
@@ -71,25 +74,22 @@ class PhysMicroDST(LHCbConfigurableUser) :
         algoName = self.getMicroDSTSelAlgName()
         return 'Phys/' + algoName
 
-    def outputPrefix(self) :
-        return self.getProp("OutputPrefix")
-
     def setOutputPrefix(self, alg) :
-        alg.OutputPrefix = self.outputPrefix()
+        alg.OutputPrefix = self.name()
     
     def copyDefaultStuff(self):
         from Configurables import CopyRecHeader, CopyODIN
-        copyRecHeader = CopyRecHeader()
+        copyRecHeader = CopyRecHeader(self.personaliseName("CopyRecHeader"))
         self.setOutputPrefix(copyRecHeader)
         self.seqMicroDST().Members += [copyRecHeader]
-        copyODIN = CopyODIN()
+        copyODIN = CopyODIN(self.personaliseName("CopyODIN"))
         self.setOutputPrefix(copyODIN)
         self.seqMicroDST().Members += [copyODIN]
 
         
     def copyParticleTrees(self) :
         from Configurables import CopyParticles, VertexCloner, ProtoParticleCloner
-        copyParticles = CopyParticles('CopyParticles')
+        copyParticles = CopyParticles(self.personaliseName('CopyParticles'))
         copyParticles.InputLocation = self.mainLocation()+"/Particles"
         copyParticles.addTool(VertexCloner(), name='VertexCloner')
         copyParticles.addTool(ProtoParticleCloner(),
@@ -100,7 +100,7 @@ class PhysMicroDST(LHCbConfigurableUser) :
 
     def copyP2PVRelations(self, name, location) :
         from Configurables import CopyParticle2PVRelations
-        copyP2PVRel = CopyParticle2PVRelations(name)
+        copyP2PVRel = CopyParticle2PVRelations(self.personaliseName(name))
         copyP2PVRel.InputLocation = location
         copyP2PVRel.OutputLevel=4
         self.setOutputPrefix(copyP2PVRel)
@@ -109,7 +109,7 @@ class PhysMicroDST(LHCbConfigurableUser) :
 
     def copyPVs(self) :
         from Configurables import CopyPrimaryVertices
-        copyPV=CopyPrimaryVertices('CopyPrimaryVertices')
+        copyPV=CopyPrimaryVertices(self.personaliseName('CopyPrimaryVertices'))
         copyPV.OutputLevel = 4
         self.setOutputPrefix(copyPV)
         self.seqMicroDST().Members += [copyPV]
@@ -125,12 +125,12 @@ class PhysMicroDST(LHCbConfigurableUser) :
         from Configurables import MCParticleCloner, MCVertexCloner
         # first, get matches MCParticles for selected candidates.
         # This will make a relations table in mainLocation+"/P2MCPRelations"
-        p2mcRelator = P2MCRelatorAlg()
+        p2mcRelator = P2MCRelatorAlg(self.personaliseName('P2MCRel'))
         p2mcRelator.ParticleLocation = self.mainLocation()+'/Particles'
         p2mcRelator.OutputLevel=4
         self.seqMicroDST().Members += [p2mcRelator]
         # Now copy relations table + matched MCParticles to MicroDST
-        copyP2MCRel = CopyParticle2MCRelations()
+        copyP2MCRel = CopyParticle2MCRelations(self.personaliseName("CopyP2MCRel"))
         copyP2MCRel.addTool(MCParticleCloner)
         copyP2MCRel.MCParticleCloner.addTool(MCVertexCloner,
                                              name = 'ICloneMCVertex')
@@ -142,13 +142,12 @@ class PhysMicroDST(LHCbConfigurableUser) :
         from Configurables import BTagging, BTaggingTool
         from Configurables import CopyFlavourTag
         importOptions('$FLAVOURTAGGINGOPTS/BTaggingTool.py')
-        BTagAlgo = BTagging('BTagging')
-        BTaggingTool("BTaggingTool").OutputLevel=4
+        BTagAlgo = BTagging(self.personaliseName('BTagging'))
         BTagAlgo.InputLocations=[self.mainLocation()]
         BTagLocation = self.mainLocation()+"/Tagging"
         BTagAlgo.TagOutputLocation = BTagLocation
         self.seqMicroDST().Members += [BTagAlgo]
-        copyFlavTag = CopyFlavourTag()
+        copyFlavTag = CopyFlavourTag(self.personaliseName("CopyFlavourTag"))
         copyFlavTag.InputLocation = BTagLocation
         self.setOutputPrefix(copyFlavTag)
         self.seqMicroDST().Members += [copyFlavTag]
@@ -163,13 +162,13 @@ class PhysMicroDST(LHCbConfigurableUser) :
 
     def copyL0DUReport(self) :
         from Configurables import CopyL0DUReport
-        copyL0 = CopyL0DUReport()
+        copyL0 = CopyL0DUReport(self.personaliseName('CopyL0DUReport'))
         self.setOutputPrefix(copyL0)
         self.seqMicroDST().Members += [copyL0]
 
     def copyHltDecReports(self) :
         from Configurables import CopyHltDecReports
-        copyHlt = CopyHltDecReports()
+        copyHlt = CopyHltDecReports(self.personaliseName('CopyHltDecReports'))
         self.setOutputPrefix(copyHlt)
         self.seqMicroDST().Members += [copyHlt]
 
@@ -188,7 +187,7 @@ class PhysMicroDST(LHCbConfigurableUser) :
         ApplicationMgr().OutStream += [mdstSeq]
         selAlg = self.getMicroDSTSelAlgName()
         if type(selAlg) == str :
-            mdstSeq.Members = [LoKi__VoidFilter("MicroDSTSel",
+            mdstSeq.Members = [LoKi__VoidFilter("MicroDSTSel_"+self.name(),
                                                 Code = "ALG_PASSED('"+selAlg+"')&ALG_EXECUTED('"+selAlg+"')")]
         else :
             mdstSeq.Members = [selAlg]
