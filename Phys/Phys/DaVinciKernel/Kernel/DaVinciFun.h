c8e1bcffe5424e6d603f9ed5c320b02e88eb7a38
jpalac <jpalac@cern.ch>
2009-08-02 19:01:59 +0000

    Move implementations of non-template methods to src/Lib/DaVinciFun.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@65150 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fd86ae5e04c0857696f8df1c552bf256d44216f4

--- Phys/Phys/DaVinciKernel/Kernel/DaVinciFun.h
+++ Phys/Phys/DaVinciKernel/Kernel/DaVinciFun.h
@@ -1,4 +1,4 @@
-// $Id: DaVinciFun.h,v 1.5 2009-08-02 17:13:27 gcowan Exp $
+// $Id: DaVinciFun.h,v 1.6 2009-08-02 19:01:59 jpalac Exp $
 #ifndef KERNEL_DAVINCIFUN_H 
 #define KERNEL_DAVINCIFUN_H 1
 
@@ -6,7 +6,7 @@
 // from ROOT
 #include "Math/Boost.h"
 #include "Kernel/Particle2Vertex.h"
-
+#include "Event/RecVertex.h"
 /** @namespace DaVinci Kernel/DaVinciFun.h
  *  
  * Collection of free functions used widely in DVAlgorithm 
@@ -57,28 +57,13 @@ namespace DaVinci {
   }
 
   /// Find the position of an LHCb::VertexBase inside a range
-  inline Particle2Vertex::Range::iterator findPos(const Particle2Vertex::Range& range, 
-                                                  const LHCb::VertexBase* to )
-  {
-    for (Particle2Vertex::Range::iterator r = range.begin(); 
-         range.end()!=r; 
-         ++r) {
-      if ( to == r->to() ) { return r; }  
-    }
-    return range.end();
-  }
+  Particle2Vertex::Range::iterator findPos(const Particle2Vertex::Range& range, 
+                                           const LHCb::VertexBase* to );
+  
+  const LHCb::VertexBase* bestVertexBase(const Particle2Vertex::Range& range);
 
-  const LHCb::VertexBase* bestVertexBase(const Particle2Vertex::Range& range) 
-  {
-    if (range.empty()) return 0;
-    return range.back().to();
-  }
+  const LHCb::RecVertex* bestRecVertex(const Particle2Vertex::Range& range);
 
-  const LHCb::RecVertex* bestRecVertex(const Particle2Vertex::Range& range) 
-  {
-    if (range.empty()) return 0;
-    return dynamic_cast<const LHCb::RecVertex*>( range.back().to() );
-  }
 
   /** @namespace DaVinci::P2VVAngles Kernel/DaVinciFun.h
    *  
@@ -158,185 +143,26 @@ namespace DaVinci {
     double calculateThetaTr(const Gaudi::LorentzVector& particleA,
                             const Gaudi::LorentzVector& particleB,
                             const Gaudi::LorentzVector& particleC,
-                            const Gaudi::LorentzVector& particleD)
-    {
-      const Gaudi::LorentzVector mother1 = particleA + particleB; // i.e., Jpsi
-      const Gaudi::LorentzVector mother2 = particleC + particleD; // i.e., phi
-      const Gaudi::LorentzVector Bcand = mother1 + mother2; // B candidate
-      
-      // We need to boost everything to the B frame first
-      ROOT::Math::Boost boostToB( Bcand.BoostToCM() );
-      
-      // Now boost everything to the B frame
-      const Gaudi::LorentzVector boostedBmother1 = boostToB( mother1 );
-      const Gaudi::LorentzVector boostedBmother2 = boostToB( mother2 );
-      const Gaudi::LorentzVector boostedBpartA   = boostToB( particleA );
-      const Gaudi::LorentzVector boostedBpartB   = boostToB( particleB );
-      const Gaudi::LorentzVector boostedBpartC   = boostToB( particleC );
-      const Gaudi::LorentzVector boostedBpartD   = boostToB( particleD );
-      
-      // Now we need to boost to the mother1 frame
-      ROOT::Math::Boost boostToMother1( boostedBmother1.BoostToCM() );
-
-      // Do the boost to the mother1 frame
-      const Gaudi::LorentzVector boostedM1mother1 = boostToMother1( boostedBmother1 );
-      const Gaudi::LorentzVector boostedM1mother2 = boostToMother1( boostedBmother2 );
-      const Gaudi::LorentzVector boostedM1partA   = boostToMother1( boostedBpartA );
-      const Gaudi::LorentzVector boostedM1partB   = boostToMother1( boostedBpartB );
-      const Gaudi::LorentzVector boostedM1partC   = boostToMother1( boostedBpartC );
-      const Gaudi::LorentzVector boostedM1partD   = boostToMother1( boostedBpartD );
-      
-      // Find the vector that is normal to the plane defined by the CD system
-      const Gaudi::XYZVector vecPartA = Gaudi::XYZVector( boostedM1partA ).unit();
-      const Gaudi::XYZVector vecPartC = Gaudi::XYZVector( boostedM1partC ).unit();
-      const Gaudi::XYZVector vecPartD = Gaudi::XYZVector( boostedM1partD ).unit();
-      const Gaudi::XYZVector vecZaxis = (vecPartD.Cross( vecPartC )).unit();
-      
-      // Theta is the polar angle of A wrt this z-axis
-      double theta = acos( vecZaxis.Dot( vecPartA ) );
-      return theta;
-    }
+                            const Gaudi::LorentzVector& particleD);
 
     // Azimuthal angle
     double calculatePhiTr(const Gaudi::LorentzVector& particleA,
                           const Gaudi::LorentzVector& particleB,
                           const Gaudi::LorentzVector& particleC,
-                          const Gaudi::LorentzVector& particleD)
-    {
-      // Code here same as in thetaTr since we need to boost everything to the same 
-      // frame.
-      const Gaudi::LorentzVector mother1 = particleA + particleB; // i.e., Jpsi
-      const Gaudi::LorentzVector mother2 = particleC + particleD; // i.e., phi
-      const Gaudi::LorentzVector Bcand = mother1 + mother2;       // B candidate
-
-      // We need to boost everything to the B frame first
-      ROOT::Math::Boost boostToB( Bcand.BoostToCM() );
-      
-      // Now boost everything to the B frame
-      const Gaudi::LorentzVector boostedBmother1 = boostToB( mother1 );
-      const Gaudi::LorentzVector boostedBmother2 = boostToB( mother2 );
-      const Gaudi::LorentzVector boostedBpartA   = boostToB( particleA );
-      const Gaudi::LorentzVector boostedBpartB   = boostToB( particleB );
-      const Gaudi::LorentzVector boostedBpartC   = boostToB( particleC );
-      const Gaudi::LorentzVector boostedBpartD   = boostToB( particleD );
-      
-      // Now we need to boost to the mother1 frame
-      ROOT::Math::Boost boostToMother1( boostedBmother1.BoostToCM() );
-
-      // Do the boost to the mother1 frame
-      const Gaudi::LorentzVector boostedM1mother1 = boostToMother1( boostedBmother1 );
-      const Gaudi::LorentzVector boostedM1mother2 = boostToMother1( boostedBmother2 );
-      const Gaudi::LorentzVector boostedM1partA   = boostToMother1( boostedBpartA );
-      const Gaudi::LorentzVector boostedM1partB   = boostToMother1( boostedBpartB );
-      const Gaudi::LorentzVector boostedM1partC   = boostToMother1( boostedBpartC );
-      const Gaudi::LorentzVector boostedM1partD   = boostToMother1( boostedBpartD );
-
-      // Find the vector that is normal to the plane defined by the CD system
-      const Gaudi::XYZVector vecPartA = Gaudi::XYZVector( boostedM1partA ).unit();
-      const Gaudi::XYZVector vecPartC = Gaudi::XYZVector( boostedM1partC ).unit();
-      const Gaudi::XYZVector vecPartD = Gaudi::XYZVector( boostedM1partD ).unit();
-      const Gaudi::XYZVector vecZaxis = (vecPartD.Cross( vecPartC )).unit();
-
-      // Decompose particle A into it's perpendicular and parallel components, 
-      // where parallel is in the plane defined by the CD system in mother1's rest frame.
-      const Gaudi::XYZVector A_perp = ( vecZaxis * vecPartA.Dot( vecZaxis ) ).unit();
-      const Gaudi::XYZVector A_para = ( vecPartA - A_perp ).unit();
-      const Gaudi::XYZVector vecMother2 = Gaudi::XYZVector( boostedM1mother2 ).unit();
-
-      double phi = acos( A_para.Dot( vecMother2 ));
-        
-      // Solve sign ambiguity - make sure that the cross product of the direction of 
-      // mother2 and A_para is the same as the z-axis we have defined. 
-      const Gaudi::XYZVector checknormal = vecMother2.Cross( A_para );
-      
-      bool samedirection = ( checknormal.Dot( vecZaxis ) > 0.0);      
-      if (!samedirection) phi = -phi;
-      
-      return phi;      
-    }
+                          const Gaudi::LorentzVector& particleD);
     
     // TRANSVERSITY AND HELICITY FRAMES
     double calculateHelicityPolarAngle(const Gaudi::LorentzVector& particleA,
-                                      const Gaudi::LorentzVector& particleB,
-                                      const Gaudi::LorentzVector& particleC,
-                                      const Gaudi::LorentzVector& particleD)
-    {  
-      const Gaudi::LorentzVector mother1 = particleA + particleB; // i.e., Jpsi
-      const Gaudi::LorentzVector mother2 = particleC + particleD; // i.e., phi
-      const Gaudi::LorentzVector Bcand = mother1 + mother2;       // B candidate
-
-      // We need to boost everything to the B frame first
-      ROOT::Math::Boost boostToB( Bcand.BoostToCM() );
-
-      // Now boost everything to the B frame
-      const Gaudi::LorentzVector boostedBmother1 = boostToB( mother1 );
-      const Gaudi::LorentzVector boostedBmother2 = boostToB( mother2 );
-      const Gaudi::LorentzVector boostedBpartA   = boostToB( particleA );
-      const Gaudi::LorentzVector boostedBpartB   = boostToB( particleB );
-      const Gaudi::LorentzVector boostedBpartC   = boostToB( particleC );
-      const Gaudi::LorentzVector boostedBpartD   = boostToB( particleD );
-      
-      // Now we need to boost to the mother2 frame
-      ROOT::Math::Boost boostToMother2( boostedBmother2.BoostToCM() );
-
-      // Do the boost to the mother2 frame
-      const Gaudi::LorentzVector boostedM2mother1 = boostToMother2( boostedBmother1 );
-      const Gaudi::LorentzVector boostedM2mother2 = boostToMother2( boostedBmother2 );
-      const Gaudi::LorentzVector boostedM2partA   = boostToMother2( boostedBpartA );
-      const Gaudi::LorentzVector boostedM2partB   = boostToMother2( boostedBpartB );
-      const Gaudi::LorentzVector boostedM2partC   = boostToMother2( boostedBpartC );
-      const Gaudi::LorentzVector boostedM2partD   = boostToMother2( boostedBpartD );
-
-      // Now find the angle between particleC and the direction of 
-      // mother2 in the _B_ rest frame.
-      const Gaudi::XYZVector vecPartC = Gaudi::XYZVector( boostedM2partC ).unit();
-      const Gaudi::XYZVector vecMother2InBframe = Gaudi::XYZVector( boostedBmother2 ).unit();
- 
-      double psi = acos( vecPartC.Dot( vecMother2InBframe ) );
-      
-      return psi;
-    }
+                                       const Gaudi::LorentzVector& particleB,
+                                       const Gaudi::LorentzVector& particleC,
+                                       const Gaudi::LorentzVector& particleD);
     
     double calculatePlaneAngle( const Gaudi::LorentzVector& particleA,
                                 const Gaudi::LorentzVector& particleB,
                                 const Gaudi::LorentzVector& particleC,
-                                const Gaudi::LorentzVector& particleD ) 
-    {
-      // calculate the angle between two planes in the rest frame of the mother particle
-      const Gaudi::LorentzVector mother1 = particleA + particleB; // i.e., Jpsi
-      const Gaudi::LorentzVector mother2 = particleC + particleD; // i.e., phi
-      const Gaudi::LorentzVector Bcand = mother1 + mother2;       // B candidate
+                                const Gaudi::LorentzVector& particleD );
+    
 
-      // We need to boost everything to the B frame first
-      ROOT::Math::Boost boostToB( Bcand.BoostToCM() );
-
-      // Now boost everything to the B frame
-      const Gaudi::LorentzVector boostedBmother1 = boostToB( mother1 );
-      const Gaudi::LorentzVector boostedBmother2 = boostToB( mother2 );
-      const Gaudi::LorentzVector boostedBpartA   = boostToB( particleA );
-      const Gaudi::LorentzVector boostedBpartB   = boostToB( particleB );
-      const Gaudi::LorentzVector boostedBpartC   = boostToB( particleC );
-      const Gaudi::LorentzVector boostedBpartD   = boostToB( particleD );
-
-      // Define some vectors that we need
-      const Gaudi::XYZVector vecA = Gaudi::XYZVector( boostedBpartA ).unit();
-      const Gaudi::XYZVector vecB = Gaudi::XYZVector( boostedBpartB ).unit();
-      const Gaudi::XYZVector vecC = Gaudi::XYZVector( boostedBpartC ).unit();
-      const Gaudi::XYZVector vecD = Gaudi::XYZVector( boostedBpartD ).unit();
-
-      // Get the normals to the decay planes
-      const Gaudi::XYZVector el = ( vecA.Cross( vecB ) ).unit();      
-      const Gaudi::XYZVector ek = ( vecC.Cross( vecD ) ).unit();
-      // The direction of mother2 in the B frame
-      const Gaudi::XYZVector ez = ( Gaudi::XYZVector( boostedBmother2 ) ).unit();
-
-      double cosPhi = ( ek.Dot( el ) );      
-      double sinPhi = ( el.Cross( ek ) ).Dot( ez );
-      double phi    = acos( cosPhi ) ;
-      
-      // Resolve ambiguity
-      return ( sinPhi > 0.0 ? phi : -phi );      
-    }
   } // namespace P2VVAngles
 
 
