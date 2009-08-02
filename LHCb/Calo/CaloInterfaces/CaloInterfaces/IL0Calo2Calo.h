87eec4ed379d1d019a68d49ced0cb8779f02a737
Dmitry Golubkov <dmitry.yu.golubkov@cern.ch>
2009-08-02 18:37:23 +0000

    a new virtual method in IL0Calo2Calo.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40390 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3c7a9dc909f8e3cb3fef66f440c493218a9e3117

--- LHCb/Calo/CaloInterfaces/CaloInterfaces/IL0Calo2Calo.h
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/IL0Calo2Calo.h
@@ -12,7 +12,7 @@
 #include "Event/CaloCluster.h"
 #include "Event/L0CaloCandidate.h"
 
-static const InterfaceID IID_IL0Calo2Calo ( "IL0Calo2Calo", 1, 0 );
+static const InterfaceID IID_IL0Calo2Calo ( "IL0Calo2Calo", 1, 1 );
 
 /** @class IL0Calo2Calo IL0Calo2Calo.h CaloInterfaces/IL0Calo2Calo.h
  *  
@@ -80,6 +80,33 @@ public:
   ( std::vector<LHCb::CaloCluster*>&      clusters, 
     const LHCb::L0CaloCandidates*         candidate ) const = 0;
   // ==========================================================================
+  /** obtain CaloClusters around a CaloCellID
+   *
+   * Get a list of CaloClusters in the vicinity of the CaloCellID,
+   * if necessary invoke decoding and clusterization.
+   * 
+   * @param clusters (OUTPUT) vector of pointers of Calo clusters 
+   * @param cellID   (INPUT)  pointer to CaloCellID
+   * @param level    (INPUT)  number of neigbour levels around the cell for the ICaloClusterization tool
+   */
+  virtual StatusCode clusterize
+  ( std::vector<LHCb::CaloCluster*>&      clusters , 
+    const LHCb::CaloCellID&               cellID, 
+    const unsigned int                    level     ) const = 0;
+  // ==========================================================================
+  /** obtain CaloClusters around a CaloCellID
+   *
+   * Get a list of CaloClusters in the vicinity of the CaloCellID,
+   * if necessary invoke decoding and clusterization.
+   * 
+   * @param clusters (OUTPUT) vector of pointers of Calo clusters 
+   * @param cellID   (INPUT)  pointer to CaloCellID
+   */
+  virtual StatusCode clusterize
+  ( std::vector<LHCb::CaloCluster*>&      clusters, 
+    const LHCb::CaloCellID&               cellID   ) const = 0;
+  // ==========================================================================
+
   /** static interface identification
    *
    * \return the interface ID
