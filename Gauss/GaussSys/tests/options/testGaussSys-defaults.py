cc012c89de22a90b65ff7aba08cf4378fbf58a6a
Silvia Miglioranzi <silvia.miglioranzi@cern.ch>
2009-08-04 11:55:06 +0000

    update options gausssys.qmt with new Gaussv37r3 settings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94408 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 26d9eb32d5edb27e8a3cb9a49efefa52431b9155

--- Gauss/GaussSys/tests/options/testGaussSys-defaults.py
+++ Gauss/GaussSys/tests/options/testGaussSys-defaults.py
@@ -4,7 +4,6 @@
 #
 
 from Gauss.Configuration import *
-import time
 
 #--Generator phase, set random numbers
 GaussGen = GenInit("GaussGen")
@@ -15,33 +14,16 @@ GaussGen.RunNumber        = 1082
 nEvts = 1
 LHCbApp().EvtMax = nEvts
 
-#--Set the 'first part' of the output files names
-fName = 'Gauss'
+#--Set name of output files for given job (uncomment the lines)
+#  Note that if you do not set it Gauss will make a name based on event type,
+#  number of events and the date
+#idFile = 'GaussTest'
+#HistogramPersistencySvc().OutputFile = idFile+'-histos.root'
+#
+#OutputStream("GaussTape").Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
 
-#--Event type can either be imported here (uncomment the following lines)
-#  or given as additional argument of gaudirun.py
-#  The 'first part' of the output files names can be overwritten
-eventType = ''
-#eventType = '30000000'
-#eventType = '11144103'
-if (eventType != '') :
-    fName = fName+'-'+eventType 
-
-#--Set name of output files for given job, based on number of events and
-#  date, above the first part is the name of the application or in case
-idFile = str(time.localtime().tm_year)
-if time.localtime().tm_mon < 10:
-    idFile += '0'
-idFile += str(time.localtime().tm_mon)
-if time.localtime().tm_mday < 10:
-    idFile += '0'
-idFile += str(time.localtime().tm_mday)
-idFile = fName+'-'+str(nEvts)+'ev-'+idFile
-print idFile
-
-HistogramPersistencySvc().OutputFile = idFile+'-histos.root'
-
-tape = OutputStream("GaussTape")
-tape.Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
-ApplicationMgr( OutStream = [tape] )
+#GenMonitor = GaudiSequencer( "GenMonitor" )
+#SimMonitor = GaudiSequencer( "SimMonitor" )
+#GenMonitor.Members += [ "GaussMonitor::CheckLifeTimeHepMC/HepMCLifeTime" ]
+#SimMonitor.Members += [ "GaussMonitor::CheckLifeTimeMC/MCLifeTime" ]
 
