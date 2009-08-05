1405c7be2b7ed53428810e2dacb1d74222930a3d
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-08-05 18:59:03 +0000

    - minor changes to the traversing track trigger for Velo closing  - disable use of traversing track trigger, as it leaks memory
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@77885 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5941cf82afbf7ab851fc6016c3b7cbf58a3ba09d

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltVeloLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltVeloLines.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# $Id: HltVeloLines.py,v 1.8 2009-08-05 12:56:36 pkoppenb Exp $
+# $Id: HltVeloLines.py,v 1.9 2009-08-05 18:59:03 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Hlt Lines for the VELO closing proceure
@@ -9,7 +9,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.8 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.9 $"
 # =============================================================================
 
 #
@@ -37,6 +37,7 @@ class HltVeloLinesConf(HltLinesConfigurableUser):
         from Configurables import Tf__PatVeloSpaceTracking as PatVeloSpaceTracking
         from Configurables import Tf__PatVeloSpaceTool as PatVeloSpaceTool
         from Configurables import Tf__PatVeloGeneralTracking as PatVeloGeneralTracking
+        from Configurables import Tf__PatVeloTraversingTracking as PatVeloTraversingTracking
         from Configurables import Tf__PatVeloRHitManager as PatVeloRHitManager
         from Configurables import Tf__PatVeloPhiHitManager as PatVeloPhiHitManager
         from Configurables import PatPV3D, PVOfflineTool
@@ -88,7 +89,6 @@ class HltVeloLinesConf(HltLinesConfigurableUser):
             pv3D.PVOfflineTool.PVFitterName = "LSAdaptPV3DFitter"
             pv3D.PVOfflineTool.PVSeedingName = "PVSeed3DTool"
 
-
             from HltLine.HltDecodeRaw import DecodeVELO
             Line( 'Velo' + side
                 , ODIN = self.getProp('ODIN')
@@ -106,48 +106,34 @@ class HltVeloLinesConf(HltLinesConfigurableUser):
                 , postscale = self.postscale
                 )
 
-class HltVeloTraverserConf(HltLinesConfigurableUser):
-   __slots__ = { 'Prescale'                   : { 'Hlt1Velo.Side' : 0.0001 }
-                 , 'ODIN'                       :"( ODIN_TRGTYP != LHCb.ODIN.RandomTrigger )" # on what trigger types do we run?
-                                }
-
-   def __apply_configuration__(self):
-      # from Configurables import DecodeVeloRawBuffer
-      from Configurables import Tf__PatVeloTrackTool as PatVeloTrackTool
-      from Configurables import Tf__PatVeloGeneralTracking as PatVeloGeneralTracking
-      from Configurables import Tf__PatVeloTraversingTracking as PatVeloTraversingTracking
-      ### find traversing tracks across both sides                              
-
-      tgt = PatVeloGeneralTracking( 'Hlt1VeloGeneralTracking'
-                                    , TrackToolName = 'TrackTool'
-                                    , OutputTracksLocation = '/Event/Hlt/Track'
-                                    , OutputLevel = 3)
-      
-      PatVeloTrackTool( 'TrackTool'
-                        , TracksInHalfBoxFrame = True)
-      
-      
-      tt = PatVeloTraversingTracking('TraversingTracking'
-                                     , InputTracksLocation = '/Event/Hlt/Track'
-                                     , DistanceGuess = 0
-                                     , IncludeLuminousRegion = 'False'
-                                     , OutputTracksLocation = '/Event/Hlt/TraversingTrack'
-                                     )
-      
-      from HltLine.HltDecodeRaw import DecodeVELO
-      Line( 'VeloTrTr'
-            , ODIN = self.getProp('ODIN')
-            , prescale = self.prescale
-            , algos =
-            [ DecodeVELO, tgt, tt
-              , Member( 'TF' , 'Decision'
-                        , OutputSelection = '%Decision'
-                        , InputSelection  = 'TES:/Event/Hlt/TraversingTrack'
-                        , FilterDescriptor = ["NumberOfASideVeloHits,>,4", "NumberOfCSideVeloHits,>,4" ]
-                        , HistogramUpdatePeriod = 1
-                        #, HistoDescriptor = { "NumberOfASideVeloHits" : [ "NumberOfASideVeloHits",0.,100.,100],
-                        #                      "NumberOfCSideVeloHits" : [ "NumberOfCSideVeloHits",0.,100.,100] }
-                        )
-              ]
-            , postscale = self.postscale
-            )
+            # The following code leaks memory, and is NOT yet ready for inclusion
+            if False :
+                 ### find traversing tracks across both sides                              
+                 tgt = PatVeloGeneralTracking( 'Hlt1VeloGeneralTracking'
+                                             , TrackToolName = 'TrackTool'
+                                             , OutputTracksLocation = '/Event/Hlt/Track/VeloGeneral'
+                                             )
+                 
+                 PatVeloTrackTool( 'TrackTool', TracksInHalfBoxFrame = True)
+                 
+                 tt = PatVeloTraversingTracking('TraversingTracking'
+                                                , InputTracksLocation = tgt.OutputTracksLocation
+                                                , DistanceGuess = 0
+                                                , IncludeLuminousRegion = 'False'
+                                                , OutputTracksLocation = '/Event/Hlt/Track/TraversingTrack'
+                                                )
+                 
+                 Line( 'VeloTrTr'
+                       , ODIN = self.getProp('ODIN')
+                       , prescale = self.prescale
+                       , algos =
+                       [ DecodeVELO, tgt, tt
+                         , Member( 'TF' , 'Decision'
+                                   , OutputSelection = '%Decision'
+                                   , InputSelection  = 'TES:' + tt.OutputTracksLocation
+                                   , FilterDescriptor = ["NumberOfASideVeloHits,>,4", "NumberOfCSideVeloHits,>,4" ]
+                                   , HistogramUpdatePeriod = 1
+                                   )
+                         ]
+                       , postscale = self.postscale
+                       )
