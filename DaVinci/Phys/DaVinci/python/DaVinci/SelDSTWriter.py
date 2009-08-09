aabd1ae036063b5fa06a6bf0ff51cb2045e64e13
jpalac <jpalac@cern.ch>
2009-08-09 19:51:07 +0000

    Write a DST for a single selection sequence. Writes out the entire contents of the input DST, plus extra items found on the TES.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@77981 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3fecaa87e2081e73f66b1e15c7d359ef25ee888a

---
+++ DaVinci/Phys/DaVinci/python/DaVinci/SelDSTWriter.py
@@ -0,0 +1,64 @@
+"""
+Write a DST for a single selection sequence. Writes out the entire
+contents of the input DST
+"""∑
+__version__ = "$Id: SelDSTWriter.py,v 1.1 2009-08-09 19:51:07 jpalac Exp $"
+__author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
+
+from LHCbKernel.Configuration import *
+from GaudiConf.Configuration import *
+
+class SelDSTWriter(ConfigurableUser) :
+    """
+    Write a DST for a single selection sequence. Writes out the entire
+    contents of the input DST file, plus extra items from the TES.
+    """
+    __slots__ = {
+        "DSTFile"           : "DaVinci.dst"
+        , "SelectionSequence"    : ""
+        , "Items"                : []
+        , "SaveCandidates"       : False
+        }
+
+    _propertyDocDct = {  
+        "DSTFile"             : """ Write name of output MicroDST file. Default 'MicroDST.dst'"""
+        , "SelectionSequence" : """ Name of PhysSlePython.SelectionSequence that defines the selection"""
+        , "Items"             : """ Extra items from the TES to be copied."""
+        , "CopyCandidates"    : """ Copy selection candidates and full decay tree. NOT IMPLEMENTED."""
+        }
+    
+    def selectionSeq(self) :
+        return self.getProp('SelectionSequence')
+
+    def _streamName(self) :
+        return 'DSTStream'+ self.name()
+    
+    def _initOutStream(self) :
+        from Configurables import InputCopyStream
+        stream = InputCopyStream( self._streamName() )
+        stream.OptItemList = self.getProp("Items")
+        stream.TakeOptionalFromTES = True
+        stream.Preload = False          # True makes LoKi crash (why?)
+        stream.PreloadOptItems = False  # True makes LoKi crash (why?)
+        dstName = self.getProp('DSTFile')
+        if (dstName != "" ) :
+            stream.Output = "DATAFILE='"+dstName +"' TYP='POOL_ROOTTREE' OPT='REC'"
+        
+    def outputStream(self) :
+        return InputCopyStream(self._streamName())
+
+    def sequence(self) :
+        return self.selectionSeq().sequence()
+
+    def algorithm(self) :
+        return self.selectionSeq().algorithm()
+
+    def algName(self) :        
+        return self.algorithm().name()
+
+    def __apply_configuration__(self) :
+        """
+        SelDSTWriter configuration
+        """
+        self._initOutStream()
+        self.sequence().Members += [self.outputStream()]
