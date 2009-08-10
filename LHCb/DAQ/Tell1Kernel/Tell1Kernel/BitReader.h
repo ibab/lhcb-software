8c1b50304a2b5d8cae40fe78234219a71ca6c3a2
Tomasz Szumlak <szumlat@cern.ch>
2009-08-10 08:51:25 +0000

    BitReader helper class added
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@25306 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 822eaa0daa24321351ab520df82394eb17fc768f

---
+++ LHCb/DAQ/Tell1Kernel/Tell1Kernel/BitReader.h
@@ -0,0 +1,36 @@
+// $Id: BitReader.h,v 1.1 2009-08-10 08:51:25 szumlat Exp $
+#ifndef KERNEL_BITREADER_H 
+#define KERNEL_BITREADER_H 1
+
+// Include files
+#include <iostream>
+#include "Tell1Kernel/VeloDecodeCore.h"
+
+
+/** 
+ *  
+ *
+ *  @author Tomasz Szumlak
+ *  @date   2006-10-20
+ */
+
+namespace VeloTELL1{
+
+  template<class T>
+  void bitReader(const T& inValue)
+  {
+    const unsigned int data_size=sizeof(inValue);
+    const unsigned int bits=8;
+    const unsigned int loop_limit=data_size*bits;
+    for(unsigned int nextBit=0; nextBit<loop_limit; ++nextBit){
+      if(inValue&(1<<nextBit)){
+        std::cout<< "1";
+      }else{
+        std::cout<< "0";
+      }
+    }
+    std::cout<<std::endl;
+  }
+
+};
+#endif // KERNEL_BITREADER_H
