47b424d05faced6e51e034a9131013b4b0c12f79
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-08-10 11:52:36 +0000

    interfade polishig : ICaloGetterTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40408 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bdb0baa05eac81da0f753a60755137dc27e55873

--- LHCb/Calo/CaloInterfaces/CaloInterfaces/ICaloGetterTool.h
+++ LHCb/Calo/CaloInterfaces/CaloInterfaces/ICaloGetterTool.h
@@ -1,4 +1,4 @@
-// $Id: ICaloGetterTool.h,v 1.1 2009-04-17 11:42:32 odescham Exp $
+// $Id: ICaloGetterTool.h,v 1.2 2009-08-10 11:52:35 ibelyaev Exp $
 #ifndef ICALOGETTERTOOL_H 
 #define ICALOGETTERTOOL_H 1
 
@@ -13,7 +13,7 @@
 #include "Event/CaloHypo.h"
 class ICaloDataProvider;
 
-static const InterfaceID IID_ICaloGetterTool ( "ICaloGetterTool", 1, 0 );
+static const InterfaceID IID_ICaloGetterTool ( "ICaloGetterTool", 2, 0 );
 
 /** @class ICaloGetterTool ICaloGetterTool.h
  *  
@@ -21,21 +21,25 @@ static const InterfaceID IID_ICaloGetterTool ( "ICaloGetterTool", 1, 0 );
  *  @author Olivier Deschamps
  *  @date   2009-04-17
  */
-class ICaloGetterTool : virtual public IAlgTool {
+class ICaloGetterTool : virtual public IAlgTool 
+{
 public: 
-
+  
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_ICaloGetterTool; }
   virtual StatusCode initialize()=0;
   virtual void update()=0;
-  virtual void addToDigits  (std::string loc,bool clear )=0;
-  virtual void addToClusters(std::string loc,bool clear )=0;
-  virtual void addToHypos  (std::string loc,bool clear )=0;
-  virtual LHCb::CaloDigits*   digits(std::string loc)=0;
-  virtual LHCb::CaloClusters* clusters(std::string loc)=0;
-  virtual LHCb::CaloHypos*    hypos(std::string loc)=0;
-  virtual bool hasData(std::string det)=0;
-  virtual ICaloDataProvider* provider(std::string det)=0;
+  
+  virtual void addToDigits  ( const std::string& loc , bool clear )=0;
+  virtual void addToClusters( const std::string& loc , bool clear )=0;
+  virtual void addToHypos   ( const std::string& loc , bool clear )=0;
+  
+  virtual LHCb::CaloDigits*   digits   ( const std::string& loc)=0;
+  virtual LHCb::CaloClusters* clusters ( const std::string& loc)=0;
+  virtual LHCb::CaloHypos*    hypos    ( const std::string& loc)=0;
+  
+  virtual bool hasData ( const std::string& det )=0;
+  virtual ICaloDataProvider* provider ( const std::string& det)=0;
 protected:
 
 private:
