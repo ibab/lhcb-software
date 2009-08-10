4c267256b8eaadbd5f7c99c5c4199459da33cc6a
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-08-10 16:52:55 +0000

    fixed more tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78038 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6721bc7bb008e8649760f262e02cdcfd1c159f49

--- DaVinci/DaVinciSys/tests/options/DVTestHlt12-MC09-DEV.py
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt12-MC09-DEV.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DVTestHlt12-MC09-DEV.py,v 1.1 2009-08-10 15:08:02 pkoppenb Exp $
+# $Id: DVTestHlt12-MC09-DEV.py,v 1.2 2009-08-10 16:52:55 pkoppenb Exp $
 #
 # Test for Hlt1&2. This should be identical to Hlt1 except for the HltType.
 #
@@ -11,29 +11,10 @@
 #
 ########################################################################
 from Gaudi.Configuration import *
-from Configurables import GaudiSequencer
-########################################################################
-#
-# Standard configuration
-#
-from Configurables import DaVinci
-DaVinci().EvtMax = 100                         # Number of events
-DaVinci().SkipEvents = 0                       # Events to skip
-DaVinci().PrintFreq  = 100                     # Print frequency
-DaVinci().DataType = "MC09"                    # Pretend it's real data
-DaVinci().Simulation   = True
-# DaVinci().MoniSequence += [ ReadHltSummary() ]
+importOptions("$DAVINCISYSROOT/tests/options/DVTestHlt12-MC09.py")
 ########################################################################
 #
 # HLT
 #
-DaVinci().Hlt2Requires = 'L0'
-DaVinci().HltType = 'Hlt1+Hlt2'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
+from Configurables import DaVinci
 DaVinci().HltThresholdSettings = ''  # Explicitly default
-#-- GAUDI jobOptions generated on Mon Jun 22 16:57:21 2009
-#-- Contains event types : 
-#--   11144001 - 15 files - 215879 events - 62.23 GBytes
-EventSelector().Input   = [
-"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004875/0000/00004875_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
-#
-MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
--- DaVinci/DaVinciSys/tests/options/DVTestHlt2-MC09.py
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt2-MC09.py
@@ -1,8 +1,8 @@
 ########################################################################
 #
-# $Id: DVTestHlt1-MC09.py,v 1.3 2009-08-10 16:52:55 pkoppenb Exp $
+# $Id: DVTestHlt2-MC09.py,v 1.1 2009-08-10 16:52:55 pkoppenb Exp $
 #
-# Options for Hlt1 tests. Identical to Hlt1&2, except for type.
+# Options for Hlt2 tests. Identical to Hlt1&2, except for type.
 #
 # @author Patrick Koppenburg
 # @date 2008-08-06
@@ -15,4 +15,4 @@ importOptions("$DAVINCISYSROOT/tests/options/DVTestHlt12-MC09.py")
 # HLT
 #
 from Configurables import DaVinci
-DaVinci().HltType = 'Hlt1'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
+DaVinci().HltType = 'Hlt2'             ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
