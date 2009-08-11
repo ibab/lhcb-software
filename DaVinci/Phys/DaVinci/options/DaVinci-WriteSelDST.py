7b0da6c85d58c8a30b68879568bce64cd04b11d3
jpalac <jpalac@cern.ch>
2009-08-11 14:58:29 +0000

    Use the new SelDSTWriter to write a selection DST saving the candidate decay trees.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78129 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4c31ceac1226fdb4a601fb4550aadc427ab874a5

---
+++ DaVinci/Phys/DaVinci/options/DaVinci-WriteSelDST.py
@@ -0,0 +1,47 @@
+########################################################################
+#
+# $Id: DaVinci-WriteSelDST.py,v 1.1 2009-08-11 14:58:29 jpalac Exp $
+#
+# Options for a DaVinci job creating DSTs
+#
+# It looks for a J/psi and then saves the events in a DST
+#
+# For a more complicated example with several DSTs
+# corresponding to various mass ranges look in
+# DaVinciSys/tests/options
+#
+# @author Patrick Koppenburg
+# @date 2009-02-03
+#
+##############################################################################
+from Gaudi.Configuration import *
+from Configurables import DaVinci, DaVinciWriteDst
+##############################################################################
+from Configurables import GaudiSequencer, FilterDesktop, DeterministicPrescaler, PrintDecayTree
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+#
+# Get a J/psi
+#
+seq = GaudiSequencer("Seq_Jpsi")
+jpsi = FilterDesktop('MyJpsi')
+jpsi.InputLocations = [ 'StdLooseJpsi2MuMu' ]
+jpsi.Code = "ALL"
+det = DeterministicPrescaler("Prescale_Jpsi")
+det.AcceptFraction = 0.5                    # take 50%
+printDec = PrintDecayTree("Print_Jpsi")
+printDec.InputLocations = [ "MyJpsi" ]
+seq.Members += [ det, jpsi, printDec ]
+#
+# This the bit that declares the sequence to the Dst writer
+# seq will bee passed to DaVinci(). You do not need to pass it yourself.
+# The second line allows to save particles (not yet 100% functional)
+#
+DaVinciWriteDst().DstFiles[ "Jpsi.dst" ] = seq
+DaVinciWriteDst().Items += [ "/Event/Phys/MyJpsi#2" ] # will not save StdLooseMuos
+##############################################################################
+DaVinci().EvtMax = 100
+DaVinci().PrintFreq = 1 
+# DaVinci().SkipEvents = 0
+DaVinci().DataType = "2008" # Default is "DC06"
+# DaVinci().Simulation   = False
+##############################################################################
