39cd7dfcd27450aa061eab34b728de634b922482
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-08-12 06:55:32 +0000

    FEST
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78130 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 11f31fca3c52fb6dffa3e8fdcd5ad868911d1906

--- Hlt/Hlt/HltSelChecker/options/FEST.py
+++ Hlt/Hlt/HltSelChecker/options/FEST.py
@@ -5,4 +5,4 @@
  #  @date 2009-07-20
  #
 from Configurables import DaVinci
-DaVinci().HltThresholdSettings = 'FEST_FEST'  # just an example
+DaVinci().HltThresholdSettings = 'Effective_FEST'  # just an example
