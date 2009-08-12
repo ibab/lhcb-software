e9cbd735e699fbec62670e878437505190882eb1
jpalac <jpalac@cern.ch>
2009-08-12 07:46:31 +0000

    Add docs
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78134 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 92fe19694f7678d73679c0d259a4c2937063c73b

--- DaVinci/Phys/DaVinci/python/DaVinci/SelDSTWriter.py
+++ DaVinci/Phys/DaVinci/python/DaVinci/SelDSTWriter.py
@@ -2,13 +2,12 @@
 Write a DST for a single selection sequence. Writes out the entire
 contents of the input DST
 """
-__version__ = "$Id: SelDSTWriter.py,v 1.4 2009-08-11 14:50:24 jpalac Exp $"
+__version__ = "$Id: SelDSTWriter.py,v 1.5 2009-08-12 07:46:31 jpalac Exp $"
 __author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
 
 from LHCbKernel.Configuration import *
 from GaudiConf.Configuration import *
 
-#from AnalysisConf.BaseDSTWriter import BaseDSTWriter
 from MicroDSTConf.MicroDSTWriter import MicroDSTWriter
 
 class SelDSTWriter(MicroDSTWriter) :
@@ -24,14 +23,18 @@ class SelDSTWriter(MicroDSTWriter) :
                   "OutputPrefix"   : "Sel"
                   }
 
-    _propertyDocDct = {"SaveCandidates" : """Copy candidate decay tree"""}
+    _propertyDocDct = {"SaveCandidates" : """Copy candidate decay tree""",
+                       "CopyODIN" : """Copy ODIN bank into /Event/OutputPrefix """,
+                       "CopyRecHeader" : """Copy RecHeader bank into /Event/OutputPrefix """,
+                       "CopyPVs" : """ Copy primary vertices bank into /Event/OutputPrefix """,
+                       "CopyBTags" : """ Copy FlavourTags bank into /Event/OutputPrefix """,
+                       "OutputPrefix" : """ Prefix of TES location of candidates output: /Event/OutputPrefix/xxxx"""}
 
     def outputStreamType(self) :
         from Configurables import InputCopyStream
         return InputCopyStream
 
     def extendStream(self, stream) :
-#        stream.OptItemList = self.getProp("Items")
         stream.TakeOptionalFromTES = True
         stream.Preload = False          # True makes LoKi crash (why?)
         stream.PreloadOptItems = False  # True makes LoKi crash (why?)
