b9e92fd7c605d6112170001e3bea8269e6b15828
jpalac <jpalac@cern.ch>
2009-08-12 08:28:28 +0000

    Second try to import the correct version
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78136 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33253de01aec1dff1e63bbbb9575a19558ab38f8

--- DaVinci/Phys/DaVinci/options/DaVinci-WriteSelDST.py
+++ DaVinci/Phys/DaVinci/options/DaVinci-WriteSelDST.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DaVinci-WriteSelDST.py,v 1.1 2009-08-11 14:58:29 jpalac Exp $
+# $Id: DaVinci-WriteSelDST.py,v 1.2 2009-08-12 08:28:28 jpalac Exp $
 #
 # Options for a DaVinci job creating DSTs
 #
@@ -15,33 +15,45 @@
 #
 ##############################################################################
 from Gaudi.Configuration import *
-from Configurables import DaVinci, DaVinciWriteDst
+from Configurables import DaVinci, SelDSTWriter
 ##############################################################################
 from Configurables import GaudiSequencer, FilterDesktop, DeterministicPrescaler, PrintDecayTree
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
 #
 # Get a J/psi
 #
-seq = GaudiSequencer("Seq_Jpsi")
 jpsi = FilterDesktop('MyJpsi')
-jpsi.InputLocations = [ 'StdLooseJpsi2MuMu' ]
 jpsi.Code = "ALL"
+
+from PhysSelPython.Wrappers import Selection, DataOnDemand, SelectionSequence
+
+MyLooseJpsi = DataOnDemand('DODStdLooseJpsi2MuMu',
+                           Location = 'StdLooseJpsi2MuMu')
+
+SelJpsi = Selection('SelJpsi',
+                    Algorithm = jpsi,
+                    RequiredSelections = [MyLooseJpsi])
+
+SeqJpsi = SelectionSequence('SeqJpsi', TopSelection = SelJpsi)
+
 det = DeterministicPrescaler("Prescale_Jpsi")
 det.AcceptFraction = 0.5                    # take 50%
 printDec = PrintDecayTree("Print_Jpsi")
 printDec.InputLocations = [ "MyJpsi" ]
-seq.Members += [ det, jpsi, printDec ]
+#seq.Members += [ det, jpsi, printDec ]
 #
 # This the bit that declares the sequence to the Dst writer
 # seq will bee passed to DaVinci(). You do not need to pass it yourself.
 # The second line allows to save particles (not yet 100% functional)
 #
-DaVinciWriteDst().DstFiles[ "Jpsi.dst" ] = seq
-DaVinciWriteDst().Items += [ "/Event/Phys/MyJpsi#2" ] # will not save StdLooseMuos
+dstWriter = SelDSTWriter("JpsiDST",
+                         SelectionSequences = [SeqJpsi],
+                         SaveCandidates = True)
 ##############################################################################
 DaVinci().EvtMax = 100
-DaVinci().PrintFreq = 1 
+DaVinci().PrintFreq = 1
+DaVinci().UserAlgorithms = [ dstWriter.sequence(), printDec  ]
 # DaVinci().SkipEvents = 0
-DaVinci().DataType = "2008" # Default is "DC06"
+DaVinci().DataType = "MC09" # Default is "MC09"
 # DaVinci().Simulation   = False
 ##############################################################################
