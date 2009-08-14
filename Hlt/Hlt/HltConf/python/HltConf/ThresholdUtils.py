43005138e67e6527b5194e149877f28c435e4896
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-08-14 10:26:19 +0000

    in ThresholdUtils.py, if about to set what is already set, move on
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78185 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9b17e8cc3aa328aa2151a7b8ecd007fb91fb1d97

--- Hlt/Hlt/HltConf/python/HltConf/ThresholdUtils.py
+++ Hlt/Hlt/HltConf/python/HltConf/ThresholdUtils.py
@@ -10,24 +10,23 @@ def setThresholds(ThresholdSettings,confs):
     @date 23/7/2009 (moved)
     """
     conf = confs()  # The configurable _must_ be called even if not configured. Or it will be ignored
-    from Gaudi.Configuration import log
-    if confs in ThresholdSettings : 
-        #       print '# Found', conf.name()
-        for (k,v) in ThresholdSettings[confs].iteritems() :
-            # configurables have an exception for list and dict: 
-            #   even if not explicitly set, if you ask for them, you get one...
-            #   this is done to make foo().somelist += ... work.
-            # hence we _assume_ that, even if we have an attr, but it matches the
-            # default, it wasn't set explicitly, and we overrule it...
-            if hasattr(conf,k) and conf.getProp(k) != conf.getDefaultProperty(k) :
-                log.warning('# WARNING: %s.%s has explictly been set, NOT using requested predefined threshold %s, but keeping explicit value: %s '%(conf.name(),k,str(v),getattr(conf,k)))
+    if confs not in ThresholdSettings : return
+    for (k,v) in ThresholdSettings[confs].iteritems() :
+        if hasattr(conf,k) and v==getattr(conf,k) : continue # nothing to do, move on to next item...
+
+        # configurables have an exception for list and dict: 
+        #   even if not explicitly set, if you ask for them, you get one...
+        #   this is done to make foo().somelist += ... work.
+        # hence we _assume_ that, even if we have an attr, but it matches the
+        # default, it wasn't set explicitly, and we overrule it...
+        if hasattr(conf,k) and conf.getProp(k) != conf.getDefaultProperty(k) :
+                from Gaudi.Configuration import log
+                log.warning('%s.%s has explictly been set, NOT using requested predefined threshold %s, but keeping explicit value: %s '%(conf.name(),k,str(v),getattr(conf,k)))
+        else :
+            if ( type(v) == dict ): # special case for dictionaries (needed in topo)
+                val = conf.getProp(k)
+                val.update(v)
+                setattr(conf,k,val)
             else :
-                if ( type(v) == type({})): # special case for dictionaries (needed in topo)
-                    val = conf.getProp(k)
-                    val.update(v)                                
-                    #                    print '# SETTING dictionary', conf.name(), val
-                    setattr(conf,k,val)
-                else :
-                    #                    print '# SETTING           ', conf.name(), v
-                    setattr(conf,k,v)
+                setattr(conf,k,v)
 
