c7389067ea838fc1131adeb8ae44fc9309f7e07c
rlambert <rlambert@cern.ch>
2009-08-18 08:20:06 +0000

    Added FilterEventList
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@65215 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a6f00a85db2dabb775a46a7b29d023b08f3757c0

---
+++ Phys/Phys/DaVinciTools/src/FilterEventList.cpp
@@ -0,0 +1,119 @@
+// $Id: FilterEventList.cpp,v 1.1 2009-08-18 08:20:06 rlambert Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+#include "Event/ODIN.h" // event & run number
+#include "GaudiKernel/IRegistry.h" // IOpaqueAddress
+// local
+
+#include "FilterEventList.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : FilterEventList
+//
+// 2004-11-18 : Patrick KOPPENBURG
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory 
+DECLARE_ALGORITHM_FACTORY( FilterEventList );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+FilterEventList::FilterEventList( const std::string& name,
+                  ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator )
+  , m_list(0)
+  , m_pass(true)
+{
+  declareProperty( "EventRunList", m_list=EventRunList(0) , "Set a vector of pairs of [EventNumber, RunNumber]");
+  declareProperty( "SetFilterPassed", m_pass=true , "What to do with events in this list? Pass=true? or Fail=false?");
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+FilterEventList::~FilterEventList() {}; 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode FilterEventList::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  debug() << "==> Initialize" << endmsg;
+  
+  info() <<  "Looking for " << m_list.size() << " Events "
+	 << "starting with (" << m_list[0].first << "," << m_list[0].second 
+	 << ")" << endmsg;
+  
+  return StatusCode::SUCCESS;
+};
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode FilterEventList::execute() {
+
+  if (msgLevel(MSG::DEBUG)) debug() << "==> Execute" << endmsg;
+  
+  int run = -1;
+  int ev = -1;
+
+  LHCb::ODIN* odin(0);
+
+  if( exist<LHCb::ODIN>( LHCb::ODINLocation::Default ) )
+  {
+    odin = get<LHCb::ODIN>( LHCb::ODINLocation::Default );
+    run = odin->runNumber();
+    ev = odin->eventNumber();
+    if (msgLevel(MSG::VERBOSE)) verbose() << "Event,run (" << ev << "," << run << ")" << endmsg;
+  }
+  if( run <0 || ev < 0)
+  {
+    Error("Can't get LHCb::ODINLocation::Default (" +
+          LHCb::ODINLocation::Default + ")" );
+  }
+  
+  EventRunPair eR(ev,run);
+  
+  if(std::find(m_list.begin(), m_list.end(), eR) != m_list.end())
+  {
+    if (msgLevel(MSG::VERBOSE)) verbose() << "Event,run *is* in list (" << ev << "," << run << ")" << endmsg;
+    ++counter("EventsInList");
+    setFilterPassed(m_pass);
+    counter("EventsSelected")+=m_pass;
+    
+  }
+  else
+  {
+    counter("EventsInList")+=0;
+    setFilterPassed(!m_pass);
+    counter("EventsSelected")+=!m_pass;
+  }
+  
+  //setFilterPassed(std::find(m_list.begin(), m_list.end(), eR) != m_list.end());
+  
+  return StatusCode::SUCCESS;
+  
+};
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode FilterEventList::finalize() 
+{
+
+  debug() << "==> Finalize" << endmsg;
+  debug() << counter("EventsInList").flag() << " Events found " 
+	 << "from the list of " << m_list.size() << " Events, having read " 
+	 << counter("EventsInList").nEntries() << "Events" << endmsg;
+  
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Phys/Phys/DaVinciTools/src/FilterEventList.h
@@ -0,0 +1,48 @@
+// $Id: FilterEventList.h,v 1.1 2009-08-18 08:20:06 rlambert Exp $
+#ifndef FILTEREVENTLIST_H 
+#define FILTEREVENTLIST_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+
+/** @class FilterEventList FilterEventList.h
+ *  
+ *  Algorithm that selects events with a given list
+ *  of (event#,run#)
+ *  
+ *  OPTIONS:
+ *  - EventRunList: list of pairs (Event,Run) to look for
+ *  - SetFilterPassed: What to do with events in this list? 
+ *                     Pass=true? or Fail=false? Default true
+ *
+ *  @author Rob Lambert
+ *  @date   2009-08-14
+ */
+class FilterEventList : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  FilterEventList( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~FilterEventList( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  typedef std::pair< int, int >EventRunPair;
+  typedef std::vector< std::pair< int, int > > EventRunList;
+  
+  EventRunList m_list;
+  bool m_pass;
+  
+
+};
+#endif // FILTEREVENTLIST_H
