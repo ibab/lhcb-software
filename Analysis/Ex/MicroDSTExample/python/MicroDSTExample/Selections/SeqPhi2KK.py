350e216674b7075bc81d2cdadc11ede5a6ed388c
jpalac <jpalac@cern.ch>
2009-08-19 19:01:40 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@78260 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4c1ba01b3e920657dac7b4eed79aecd30f1a4250

--- Analysis/Ex/MicroDSTExample/python/MicroDSTExample/Selections/SeqPhi2KK.py
+++ Analysis/Ex/MicroDSTExample/python/MicroDSTExample/Selections/SeqPhi2KK.py
@@ -1,8 +1,8 @@
-# $Id: SeqPhi2KK.py,v 1.1 2009-08-18 17:05:30 jpalac Exp $
+# $Id: SeqPhi2KK.py,v 1.2 2009-08-19 19:01:40 jpalac Exp $
 
 __author__ = 'Juan Palacios'
 __date__ = 'July 2009'
-__version__ = '$Revision: 1.1 $'
+__version__ = '$Revision: 1.2 $'
 
 '''
 The roadmap Bs->JpsiPhi selection using LoKi::Hybrid and python
@@ -13,5 +13,5 @@ from Phi2KK import SelPhi2KK
 
 from PhysSelPython.Wrappers import SelectionSequence
 
-SeqPhi2KK = SelectionSequence("SeqPhi2KKxx",
+SeqPhi2KK = SelectionSequence("Seq_Phi2KK",
                               TopSelection = SelPhi2KK)
