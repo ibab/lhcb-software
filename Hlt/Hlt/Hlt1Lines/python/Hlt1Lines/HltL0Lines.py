26bc824bf8a3ccf4ee0e5bd53791ce26623dfdcd
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-08-25 09:12:42 +0000

    fix stupid omission
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78353 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 10c6a1737c44c8090f8cc353d785f65ce4418a00

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltL0Lines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltL0Lines.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# $Id: HltL0Lines.py,v 1.5 2009-08-25 06:33:46 graven Exp $
+# $Id: HltL0Lines.py,v 1.6 2009-08-25 09:12:42 graven Exp $
 # =============================================================================
 ## @file
 #  Configuration of Hlt Lines which are plain L0 lines
@@ -11,7 +11,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.5 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.6 $"
 # =============================================================================
 
 from HltLine.HltLinesConfigurableUser import *
@@ -24,7 +24,7 @@ class HltL0LinesConf(HltLinesConfigurableUser) :
    def __apply_configuration__(self):
         from HltLine.HltLine import Hlt1Line   as Line
         from HltLine.HltLine     import hlt1Lines
-        from Hlt1Lines.HltL0Candidates import convertL0Candidates
+        from Hlt1Lines.HltL0Candidates import convertL0Candidates,L0Channels
         channels = self.getProp('L0Channels')
         if not channels : channels = L0Channels()
         for channel in channels :
