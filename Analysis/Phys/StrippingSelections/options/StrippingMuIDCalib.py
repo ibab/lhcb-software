3154849552ffdac84a182fd6f64dda262726bd5b
Alessio Sarti <alessio.sarti@lnf.infn.it>
2009-08-27 13:33:50 +0000

    improved muID calib stripping selection
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@78443 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b6e00f98a254459e266b62251dbe663019f1b912

--- Analysis/Phys/StrippingSelections/options/StrippingMuIDCalib.py
+++ Analysis/Phys/StrippingSelections/options/StrippingMuIDCalib.py
@@ -13,14 +13,15 @@ from StrippingConf.StrippingLine import StrippingLine, StrippingMember
 
 # Create Jpsi -> mumu candidates out of no pid muons
 ## ############################################################
-muonscut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>500*MeV) & (TRCHI2DOF<3) & (ISLONG) & (PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1500*MeV) & (PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<6000*MeV) & (PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV) & (PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>500*MeV)'
-childcuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1))"
+muplcut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>6*GeV) & (PT>1500*MeV) & (TRCHI2DOF<3) & (ISLONG)'
+mumicut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<3) & (ISLONG) & (PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV) & (PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV) & (PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV) & (PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV)'
+childcuts = " (CHILDCUT(ISMUON,1))"
 JpsiCombine = StrippingMember( CombineParticles
                                , 'Combine'
                                , InputLocations = [ 'StdNoPIDsMuons' ]
                                , DecayDescriptor = 'J/psi(1S) -> mu+ mu-'
-                               , DaughtersCuts = { 'mu+' : muonscut
-                                                   , 'mu-'  : muonscut }
+                               , DaughtersCuts = { 'mu+' : muplcut , 
+                                                   'mu-' : mumicut }
                                , CombinationCut = "(ADAMASS('J/psi(1S)')<300*MeV)"
                                , MotherCut = "(VFASPF(VCHI2/VDOF)<6) & " + childcuts
                                )
@@ -28,29 +29,50 @@ JpsiCombine = StrippingMember( CombineParticles
 # Define the line
 ## ############################################################
 jp_line = StrippingLine('JpsiNoPID'
-                        , prescale = 0.007
+                        , prescale = 1.
                         , algos = [ JpsiCombine ]
                         , stream = 'Jpsi'
                         )
 
 
-# Create Lambda -> ppi candidates
+# Create Lambda_dd -> ppi candidates
 ## ############################################################
-LambdaCombine = StrippingMember( CombineParticles
-                                 , 'Combine'
-                                 , InputLocations = [ "StdNoPIDsDownPions","StdNoPIDsDownProtons" ]
-                                 , DecayDescriptor = "[Lambda0 -> p+ pi-]cc"
-                                 , DaughtersCuts = { "p+"  :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<3) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))",
-                                                     "pi-" :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<3) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))" }
-                                 , CombinationCut = "(ADAMASS('Lambda0')<30*MeV)"
-                                 , MotherCut = "(VFASPF(VCHI2)<6) & (500<BPVVDZ) & (BPVDIRA>0.999999) "
-                                 )
+LambdaDDCombine = StrippingMember( CombineParticles
+                                   , 'Combine'
+                                   , InputLocations = [ "StdNoPIDsDownPions","StdNoPIDsDownProtons" ]
+                                   , DecayDescriptor = "[Lambda0 -> p+ pi-]cc"
+                                   , DaughtersCuts = { "p+"  :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))",
+                                                       "pi-" :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))" }
+                                   , CombinationCut = "(ADAMASS('Lambda0')<30*MeV)"
+                                   , MotherCut = "(VFASPF(VCHI2)<6) & (500<BPVVDZ) & (2200>BPVVDZ) & (BPVDIRA>0.999999) "
+                                   )
 
 # Define the line
 # ############################################################
-lm_line = StrippingLine('LambdaNoPID'
-                        , prescale = 0.0005
-                        , algos = [ LambdaCombine ]
-                        , stream = 'Dstar'
+lm_line = StrippingLine('LambdaDDNoPID'
+                        , prescale = 1.
+                        , algos = [ LambdaDDCombine ]
+                        , stream = 'Lambda_dd'
+                    )
+
+
+# Create Lambda_LL -> ppi candidates
+## ############################################################
+LambdaLLCombine = StrippingMember( CombineParticles
+                                   , 'Combine'
+                                   , InputLocations = [ "StdNoPIDsPions","StdNoPIDsProtons" ]
+                                   , DecayDescriptor = "[Lambda0 -> p+ pi-]cc"
+                                   , DaughtersCuts = { "p+"  :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISLONG) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))",
+                                                       "pi-" :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISLONG) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))" }
+                                   , CombinationCut = "(ADAMASS('Lambda0')<30*MeV)"
+                                   , MotherCut = "(VFASPF(VCHI2)<6) & (50<BPVVDZ) & (600>BPVVDZ) & (BPVDIRA>0.9999995) "
+                                   )
+
+# Define the line
+# ############################################################
+lm_line = StrippingLine('LambdaLLNoPID'
+                        , prescale = 1.
+                        , algos = [ LambdaDDCombine ]
+                        , stream = 'Lambda_ll'
                     )
 
