f7829704441906abd5f92283116fd788e56bef7f
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-09-01 15:13:58 +0000

    added access to field grid
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48413 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4be8b2e640982a922c1b9fef46ba5a1238415762

--- LHCb/Kernel/LHCbKernel/Kernel/ILHCbMagnetSvc.h
+++ LHCb/Kernel/LHCbKernel/Kernel/ILHCbMagnetSvc.h
@@ -1,4 +1,4 @@
-// $Id: ILHCbMagnetSvc.h,v 1.1 2008-07-18 16:12:10 cattanem Exp $
+// $Id: ILHCbMagnetSvc.h,v 1.2 2009-09-01 15:13:58 wouter Exp $
 #ifndef KERNEL_ILHCBMAGNETSVC_H 
 #define KERNEL_ILHCBMAGNETSVC_H 1
 
@@ -9,6 +9,10 @@
 // from Gaudi
 #include "GaudiKernel/IAlgTool.h"
 #include "GaudiKernel/IMagneticFieldSvc.h"
+
+namespace LHCb {
+  class MagneticFieldGrid ;
+}
 
 static const InterfaceID IID_ILHCbMagnetSvc ( "ILHCbMagnetSvc", 1, 0 );
 
@@ -27,6 +31,24 @@ public:
 
   virtual double scaleFactor() const = 0; ///< Return the field scaling factor
   virtual bool   useRealMap()  const = 0; ///< True if using measured map
+
+
+  /** Implementation of IMagneticFieldSvc interface.
+   * @param[in]  xyz Point at which magnetic field vector will be given
+   * @param[out] fvec Magnectic field vector.
+   * @return StatusCode SUCCESS if calculation was performed.
+   */
+  virtual StatusCode fieldVector( const ROOT::Math::XYZPoint& xyz,
+                                  ROOT::Math::XYZVector& fvec ) const = 0;
+
+  /** Implementation of IMagneticFieldSvc interface.
+   * @param[in]  xyz Point at which magnetic field vector will be given
+   * @return fvec Magnectic field vector.
+   */
+  virtual ROOT::Math::XYZVector fieldVector( const ROOT::Math::XYZPoint& xyz ) const = 0;
+
+  /// Get direct access to the field grid
+  virtual const LHCb::MagneticFieldGrid* fieldGrid() const = 0 ;
 
 protected:
 
