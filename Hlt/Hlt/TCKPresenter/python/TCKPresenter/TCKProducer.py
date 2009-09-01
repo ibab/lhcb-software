a292cde3bfbe2278ba533313526ab11fc2d7fe30
kneumann <kneumann@cern.ch>
2009-09-01 12:42:19 +0000

    the svg images are wrapped with html code now before they are read in bye the http server this is important for usage in all browsers
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78535 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7622999d1a83ad1f6249bbfc4b1bad64e00eb31e

--- Hlt/Hlt/TCKPresenter/python/TCKPresenter/TCKProducer.py
+++ Hlt/Hlt/TCKPresenter/python/TCKPresenter/TCKProducer.py
@@ -1,12 +1,24 @@
 
+################################################
+## basic script for the TCK-Presenter Project ##
+## Klaus Neumann                              ##
+## 17.07.09                                   ##
+## CERN-PH-LHCb                               ##
+################################################
+
+## this script is used to read in all the tcks and produce one after the other.
+## there is a possibility to overwrite existing tcks or not depends on the choosen option
+
 import os
 import sys
 from TCKUtils.utils import *
 
+## the path where to put the tcks is mentioned here 
 Path = '/afs/cern.ch/user/k/kneumann/tcktestpage/'
 
 TCKs = getTCKList()
 
+## read in the parameters
 if len(sys.argv) == 3:
     Path = sys.argv[1]
     Overwrite = sys.argv[2]
@@ -17,8 +29,10 @@ else:
 print "number of TCKs: ", len(TCKs)
 
 for TCK in TCKs:
+    ## investigate if a tcks should be overwritten or not
     if not os.path.exists(Path + TCK + '/') or Overwrite=='1':
         print 'create TCK: ' + TCK
+        ##TCKPresenter script is the main script to produce tcks
         os.system('python TCKPresenter.py ' + TCK + ' ' + Path)
     else:
         print 'TCK exist already: ' + TCK
