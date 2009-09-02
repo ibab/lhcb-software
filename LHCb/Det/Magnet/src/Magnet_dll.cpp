f493f8148fd5f5c8b4ad258535f5bdddd5340078
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-09-02 12:59:48 +0000

    remove obsolete file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37571 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d37ada1252ab4030c594b7701c649d5d0dc0eab0

--- LHCb/Det/Magnet/src/Magnet_dll.cpp
+++
@@ -1,6 +0,0 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Det/Magnet/src/Magnet_dll.cpp,v 1.1.1.1 2002-05-21 09:12:33 gcorti Exp $
-
-// Include Files
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES(Magnet)
