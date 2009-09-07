3a26f211e853b5a2fab387719d1e28976e2cc0eb
rogers <rogers@cern.ch>
2009-09-07 10:01:47 +0000

    Now takes multiple CAMERA server in the form 'host:port host:port ...'  after the comment. All are options and defaults to localhost:12346.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99747 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 39869dfc55165c00e35db367e77384fbeda2b501

--- Online/Online/Camera/scripts/commentCamera.sh
+++ Online/Online/Camera/scripts/commentCamera.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
-# Sends a comment to the CAMERA server.
+# Sends a comment to the CAMERA servers.
 #
 # $1 the comment to send. Must be in "" if spaces are used e.g. "My comment" not My comment.
-# $2 optional argument with give host:port, where :port is optional.
+# $2 optional argument with give host:port, where :port is optional. To send commet to more 
+#    than one server the servers must be separated by a space and must be enclosed in "" or '',
+#     Ex.: ./commentCamera.sh comment "host1:port1 host2:port2" 
 #
 # The comment will use the user name as its group and it will have
 # the info level. The script will use the localhost by default
