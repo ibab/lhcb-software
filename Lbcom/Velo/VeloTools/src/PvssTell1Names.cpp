20e52af72545af6fd249144c107b2af8b8fc799e
Kurt Rinnert <kurt.rinnert@cern.ch>
2009-09-08 16:44:19 +0000

    - added new tool providing PVSS TELL1 names
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@25399 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e26709dac642da94289b297bb4c31f9e4279642c

---
+++ Lbcom/Velo/VeloTools/src/PvssTell1Names.cpp
@@ -0,0 +1,64 @@
+// $Id: PvssTell1Names.cpp,v 1.1 2009-09-08 16:44:19 krinnert Exp $
+#include <sstream>
+
+#include <time.h>
+
+#include "GaudiKernel/ToolFactory.h" 
+
+#include "PvssTell1Names.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : PvssTell1Names
+//
+// 2008-09-08 : Kurt Rinnert
+//-----------------------------------------------------------------------------
+
+namespace Velo
+{
+  
+  DECLARE_TOOL_FACTORY( PvssTell1Names );
+  
+  //=============================================================================
+  // Standard constructor
+  //=============================================================================
+  PvssTell1Names::PvssTell1Names( const std::string& type,
+      const std::string& name,
+      const IInterface* parent )
+    : GaudiTool ( type, name , parent )
+    {
+      declareInterface<Velo::IPvssTell1Names>(this);
+    }
+
+  //=============================================================================
+  // Destructor
+  //=============================================================================
+  PvssTell1Names::~PvssTell1Names() {}
+
+  //=============================================================================
+  // Initializes init time stamp
+  //=============================================================================
+  StatusCode PvssTell1Names::initialize()
+  {
+    StatusCode sc = GaudiTool::initialize();
+    if ( !sc ) return sc;
+
+    return StatusCode::SUCCESS;
+  }
+
+  //=============================================================================
+  // Finalize
+  //=============================================================================
+  StatusCode PvssTell1Names::finalize()
+  {
+    return GaudiTool::finalize();
+  }
+
+  //=============================================================================
+  // Access to PVSS TELL1 name by sensor number
+  //=============================================================================
+  const std::string& PvssTell1Names::pvssName( unsigned int sensorNumber )
+  {
+    return m_pvssNames.pvssName(sensorNumber);
+  }
+
+}
---
+++ Lbcom/Velo/VeloTools/src/PvssTell1Names.h
@@ -0,0 +1,51 @@
+// $Id: PvssTell1Names.h,v 1.1 2009-09-08 16:44:19 krinnert Exp $
+#ifndef PVSSTELL1NAMES_H 
+#define PVSSTELL1NAMES_H 1
+
+#include <map>
+#include <vector>
+
+#include "GaudiAlg/GaudiTool.h"
+#include "Tell1Kernel/IPvssTell1Names.h" 
+#include "Tell1Kernel/PvssTell1NameMap.h" 
+
+
+namespace Velo
+{
+  /** @class PvssTell1Names PvssTell1Names.h
+   *
+   *  Implementation of PvssTell1Map interface.
+   *
+   *  Provides access to PVSS TELL1 names from sensor
+   *  numbers.
+   *
+   *  @author Kurt Rinnert
+   *  @date   2009-09-08
+   */
+  class PvssTell1Names : public GaudiTool, virtual public Velo::IPvssTell1Names {
+
+    private:
+
+      friend class ToolFactory<Velo::PvssTell1Names>;
+
+    public: 
+      /// Standard constructor
+      PvssTell1Names( const std::string& type, 
+          const std::string& name,
+          const IInterface* parent);
+
+      virtual ~PvssTell1Names( ); ///< Destructor
+
+      virtual StatusCode initialize(); 
+      virtual StatusCode finalize(); 
+
+      virtual const std::string& pvssName( unsigned int sensorNumber ); ///< retrive PVSS TELL1 name
+
+    private:
+
+      // time stamps
+      Velo::PvssTell1NameMap m_pvssNames; 
+
+  };
+}
+#endif // PVSSTELL1NAMES_H 
