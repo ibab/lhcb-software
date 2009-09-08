a345ca208dcdff1e7b32533994c1e09db4a4f442
snies <snies@cern.ch>
2009-09-08 16:12:28 +0000

    first check in of HltCosmics und Hlt
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78618 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 862a25996fd385566436e490ec07fb75167a5f8b

---
+++ Hlt/Hlt/HltCosmics/src/HltCosmicsOT.cpp
@@ -0,0 +1,120 @@
+// $Id: HltCosmicsOT.cpp,v 1.1.1.1 2009-09-08 16:12:28 snies Exp $
+#include "GaudiKernel/DeclareFactoryEntries.h"
+//#include "GaudiKernel/AlgFactory.h"
+
+#include "HltCosmicsOT.h"
+#include "Event/OTLiteTime.h"
+
+#include <boost/foreach.hpp>
+
+namespace LHCb { class RawEvent; }
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : HltCosmicsOT
+//
+// 2009-07-16 : Manuel Tobias Schiller <schiller@physi.uni-heidelberg.de>
+//
+// 2009-09-08 : adapted by Stephan Nies <snies@cern.ch> 
+//                     and Jesko Merkel <jmerkel@cern.ch>
+//-----------------------------------------------------------------------------
+
+DECLARE_ALGORITHM_FACTORY( HltCosmicsOT );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+    HltCosmicsOT::HltCosmicsOT(const std::string& name, ISvcLocator* pSvcLocator)
+: HltAlgorithm(name, pSvcLocator)
+, m_selections(*this)
+{
+    declareProperty("OTDecoderName", m_otdecodername = "OTRawBankDecoder");
+    declareProperty("MinNumberOfHits", m_minhits = 6);
+    declareProperty("TolStraws", m_tolstraws = 3);
+    m_selections.declarePropties();
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+HltCosmicsOT::~HltCosmicsOT() {;}
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode HltCosmicsOT::initialize()
+{
+    //StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+
+
+    StatusCode sc = HltAlgorithm::initialize();
+    if (sc.isFailure()) return sc;  // error printed already by GaudiAlgorithm
+    //m_selections.retrieveSelections();
+    m_selections.registerSelection();
+
+    debug() << "==> Initialize" << endmsg;
+
+    m_otdecoder = tool<IOTRawBankDecoder>(m_otdecodername);
+
+    return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode HltCosmicsOT::execute()
+{
+    if (msgLevel(MSG::DEBUG))
+	debug() << "==> Execute" << endmsg;
+
+    if (m_otdecoder->totalNumberOfHits() >= m_minhits) {
+	// loop over all modules, decode them, and return early if we find
+	// hits in two neighbouring straws of a module
+	// (reasoning: decoding modules without hits is cheap, and we stop
+	// at the first one with hits which satisfies the neighbouring
+	// straws criterion... if we have lots of noise, we may have to
+	// reconsider the strategy)
+	for (unsigned i = 0; i < 432; ++i) {
+	    // covert i to quadruple (station, layer, quadrant, module):
+	    // 1st 2 bits of i are lay, 2nd 2 bits are qua, remaining bits
+	    // are sta + 3 * module (starting at LSB)
+	    // numbering conventions are such that sta and mod start at 1
+	    LHCb::OTLiteTimeRange hits = m_otdecoder->decodeModule(
+		    LHCb::OTChannelID(1 + ((i >> 4) % 3), i & 3,
+			(i >> 2) & 3, 1 + (i / (4 * 4 * 3)), 0));
+	    // check for at least 2 hits
+	    if (hits.begin() + 1 >= hits.end()) continue;
+	    // calculate sum of straw numbers
+	    unsigned long sum = 0ul;
+	    BOOST_FOREACH(const LHCb::OTLiteTime& hit, hits)
+		sum += hit.channel().straw();
+	    sum &= 0x7ful; // remainder when dividing by 128
+	    if (sum < m_tolstraws || 0x80ul - sum < m_tolstraws) {
+		// we have hits in two neighbouring straws, return early
+		setFilterPassed(true);
+                m_selections.output()->setDecision(true);
+   		if (msgLevel(MSG::DEBUG))
+		    debug() << "OT activity found!" << endreq;
+		return StatusCode::SUCCESS;
+	    }
+	}
+    }
+
+    // default: we don't pass the filter
+    setFilterPassed(false);
+    m_selections.output()->setDecision(false);
+    return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Finalize
+//=============================================================================
+StatusCode HltCosmicsOT::finalize()
+{
+    debug() << "==> Finalize" << endmsg;
+
+
+    return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+// vim: sw=4:tw=78:ft=cpp
---
+++ Hlt/Hlt/HltCosmics/src/HltCosmicsOT.h
@@ -0,0 +1,66 @@
+// $Id: HltCosmicsOT.h,v 1.1.1.1 2009-09-08 16:12:28 snies Exp $
+#ifndef INCLUDE_HLTCOSMICSOT_H
+#define INCLUDE_HLTCOSMICSOT_H 1
+
+#include <string>
+
+//#include "GaudiAlg/GaudiAlgorithm.h"
+#include "HltBase/HltAlgorithm.h"
+#include "OTDAQ/IOTRawBankDecoder.h"
+
+
+/** @class HltCosmicsOT HltCosmicsOT.h
+ *
+ * trigger on OT Activity (idea based on a suggestion by Dirk Wiedner)
+ *
+ * starting from a minimum number of hits in OT, we look for neighbouring
+ * hits inside a module. if we find any, we accept the event and set a flag
+ * that the event passed this algorithm (using the setFilterPassed(true) method,
+ * otherwise, we use setFilterPassed(false))
+ *
+ * @author Manuel Tobias Schiller <schiller@physi.uni-heidelberg.de>
+ * @date   2009-07-16
+ *
+ * 2009-09-08 : adapted by Stephan Nies <snies@cern.ch> 
+ *                   and Jesko Merkel <jmerkel@cern.ch>
+ *
+ */
+
+
+//class HltCosmicsOT : public GaudiAlgorithm {
+
+#include "HltBase/HltSelectionContainer.h"
+
+class HltCosmicsOT : public HltAlgorithm {
+
+    public:
+
+	/// Standard Constructor
+	HltCosmicsOT(const std::string& name, ISvcLocator* pSvcLocator);
+
+	virtual ~HltCosmicsOT(); ///< Destructor
+
+	virtual StatusCode initialize(); ///< Algorithm initialization
+	virtual StatusCode    execute(); ///< Algorithm event execution
+	virtual StatusCode   finalize(); ///< Algorithm finalize
+
+    protected:
+
+
+    private:
+        Hlt::SelectionContainer0 m_selections;
+
+	/// decoder so that we can ask quickly if there were any hits
+	IOTRawBankDecoder* m_otdecoder;
+	/// name of raw bank decoder to use
+	std::string m_otdecodername;
+	/// minimum number of hits in OT before we start decoding
+	unsigned m_minhits;
+	/// tolerance of straw sum test
+	unsigned m_tolstraws;
+
+
+};
+#endif // INCLUDE_HLTCOSMICSOT_H
+
+// vim: sw=4:tw=78:ft=cpp
