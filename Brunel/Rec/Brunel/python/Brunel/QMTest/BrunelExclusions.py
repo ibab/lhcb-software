a57e6ff0aef7226128587f7341037e3b51a3d3d8
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-09-09 14:07:37 +0000

    fix TransportSvc exclusions
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@30925 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 145a83565529029ffdbffdc6ca192dea74518bfb

--- Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -2,4 +2,8 @@ from GaudiTest import LineSkipper
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
-               LineSkipper(["--- Reader", "--- CFMlpANN", "INFO MuonIDAlg v" ])
+  LineSkipper(["--- Reader", "--- CFMlpANN", "INFO MuonIDAlg v" ]) + \
+  LineSkipper(["AfterMagnetRegion/T/lvT"]) + \
+  LineSkipper(["BeforeMagnetRegion/Velo2Rich1/lvVelo2Rich1"]) +\
+  LineSkipper(["BeforeMagnetRegion/Velo/RFFoil/lvRFDownStreamSection"]) +\
+  LineSkipper(["BeforeMagnetRegion/Velo/PipeSections/lvVeloPipe"])
