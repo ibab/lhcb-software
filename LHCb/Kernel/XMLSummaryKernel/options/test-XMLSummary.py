cc1af340e4d5f5c0869e79715bdf43fcc5474b0d
rlambert <rlambert@cern.ch>
2009-09-11 15:34:57 +0000

    finished test .py file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48437 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 50148a013e8c741c93e8d82723f090461fdc2f2e

--- LHCb/Kernel/XMLSummaryKernel/options/test-XMLSummary.py
+++ LHCb/Kernel/XMLSummaryKernel/options/test-XMLSummary.py
@@ -1,42 +1,22 @@
+# Test, will read and write a Dst file
+# Will fill some counters, and output the summary.xml
 from Gaudi.Configuration import *
 
 from Configurables import GaudiSequencer, EventCountAlg
 from Configurables import XMLSummarySvc
 from Configurables import LHCbApp
 
-#Set up POOL persistency
-importOptions("$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts")
-
-#// The input catalog describes the input file.
-#// In this example it is the catalog produced by WriteMin.opts
-#FileCatalog().Catalogs = ["xmlcatalog_file:MyCatalog.xml"]
-
-#// Unpack the packed containers on demand
-ApplicationMgr().ExtSvc += ["DataOnDemandSvc"]
-EventDataSvc().EnableFaultHandler = True 
-importOptions("$STDOPTS/UnpackDigi.opts")
-
-#importOptions("$IOEXAMPLEROOT/options/ReadDst.opts")
+#Run this other example, which reads/copies a DST
+importOptions("$IOEXAMPLEROOT/tests/options/WriteDst.opts")
 
 xmlsummary=XMLSummarySvc("CounterSummarySvc")
-#xmlsummary.xmlfile=""
-#xmlsummary.xmlfile="summary.xml"
 xmlsummary.OutputLevel=1
-#xmlsummary.CatalogType="Gaudi::XMLFileCatalog/XMLFileCatalog"
-#xmlsummary.CounterList=[".*RWL.*"]
 
 EvtAlg=EventCountAlg("TestCounter")
 
 ApplicationMgr().TopAlg+=[EvtAlg]
-ApplicationMgr().ExtSvc+=[xmlsummary]
-#ApplicationMgr().ExtSvc+=["IncidentSvc"]
-#ApplicationMgr().ExtSvc+=["Gaudi::IODataManager/IODataManager"]
 
 
-#LHCbApp().XMLSummary="summary.xml"
+LHCbApp().XMLSummary="summary.xml"
 LHCbApp().DataType="DC06"
 LHCbApp().Simulation=True
-LHCbApp().EvtMax=100
-EventSelector().Input   = [
-"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002142/DST/0000/00002142_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"
-]
