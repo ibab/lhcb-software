4669c49e125c8e28c37c2d0fd2d8a616374f4100
Jean-Christophe Garnier <jean-christophe.garnier@cern.ch>
2009-09-16 06:41:21 +0000

    Script for MEPInjector tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99804 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2bb678a08398cf63d4d693c8591bd15752d3eb3e

---
+++ Online/Online/OnlineTasks/job/MEPInjectorTest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if test -n "$1" ; then
+   export UTGID=$1
+fi
+
+
+if [ -z $DIM_DNS_NODE ]; then
+        echo "Please define DIM_DNS_NODE"
+        exit 1
+fi
+
+${GAUDIKERNELROOT}/../InstallArea/$CMTCONFIG/bin/Gaudi.exe ../options/MEPInjectorTest.opts
+
+#cd /home/online/ONLINE/Online_v4r20/Online/OnlineTasks/job
+
+#HOST=$(hostname --short | awk '{ print toupper($1) }')
+
+#. ./setupOnline.sh $1
+
+#exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class1Task -main=${GAUDIONLINEROOT}/options/Main.opts -opt=${ONLINETASKSROOT}/options/MEPInjector.opts&
