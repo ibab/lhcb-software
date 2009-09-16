9cdc1aa75dc1a368f7cda0ee6a9fe7add072e0d8
rlambert <rlambert@cern.ch>
2009-09-16 08:00:18 +0000

    added XMLSummary configuration
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5513 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 48112135dc89aac3bc4a70f6f06a981f111c7c1a

--- LHCb/GaudiConf/python/GaudiConf/Configuration.py
+++ LHCb/GaudiConf/python/GaudiConf/Configuration.py
@@ -1,13 +1,14 @@
 """
 High level configuration tools for LHCb applications
 """
-__version__ = "$Id: Configuration.py,v 1.22 2009-07-07 08:58:39 cattanem Exp $"
+__version__ = "$Id: Configuration.py,v 1.23 2009-09-16 08:00:18 rlambert Exp $"
 __author__  = "Marco Cattaneo <Marco.Cattaneo@cern.ch>"
 
 from os import environ
 from Gaudi.Configuration import *
 from LHCbKernel.Configuration import *
 from Configurables import ( DDDBConf )
+from Configurables import ( XMLSummarySvc )
 
 class LHCbApp(LHCbConfigurableUser):
     __slots__ = {
@@ -20,8 +21,9 @@ class LHCbApp(LHCbConfigurableUser):
        ,"Monitors"    : []
        ,"OutputLevel" : INFO 
        ,"TimeStamp"   : False
+       ,"XMLSummary"   : None
         }
-
+    
     _propertyDocDct = { 
         'EvtMax'      : """ Maximum number of events to process """
        ,'SkipEvents'  : """ Number of events to skip """
@@ -32,16 +34,17 @@ class LHCbApp(LHCbConfigurableUser):
        ,'Monitors'    : """ List of monitors to execute """
        ,'OutputLevel' : """ The printout level to use (default INFO) """
        ,'TimeStamp'   : """ Flag to add a time stamp to messages (default False) """
+       ,'XMLSummary'   : """ Add an XML summary file, default None """
        }
-
-    __used_configurables__ = [ DDDBConf ]
-
+    
+    __used_configurables__ = [ DDDBConf, XMLSummarySvc ]
+    
     def knownMonitors(self):
         return ["SC", "FPE"]
-
+    
     def knownAuditors(self):
         return ["NameAuditor","MemoryAuditor","ChronoAuditor"]
-
+    
     def defineDB(self):
         # Delegate handling of properties to DDDBConf
         self.setOtherProps( DDDBConf(), ["Simulation", "DataType" ] )
@@ -52,7 +55,7 @@ class LHCbApp(LHCbConfigurableUser):
         if hasattr( self, "CondDBtag" ):
             CondDB().Tags [ "LHCBCOND" ] = self.getProp("CondDBtag")
             CondDB().Tags [ "SIMCOND"  ] = self.getProp("CondDBtag")
-            
+    
     def defineEvents(self):
         # Set up transient store and data on demand service
         EventDataSvc( ForceLeaves        = True,
@@ -67,19 +70,19 @@ class LHCbApp(LHCbConfigurableUser):
 
         # Delegate handling to ApplicationMgr configurable
         self.setOtherProps(ApplicationMgr(),["EvtMax"])
-
+    
     def evtMax(self):
         if hasattr(ApplicationMgr(),"EvtMax") and not hasattr(self,"EvtMax"):
             return ApplicationMgr().getProp("EvtMax")
         else:
             return self.getProp("EvtMax")
-
+    
     def skipEvents(self):
         if hasattr(EventSelector(),"FirstEvent") and not hasattr(self,"SkipEvents"):
             return EventSelector().getProp("FirstEvent") - 1
         else:
             return self.getProp("SkipEvents")
-
+    
     def defineMonitors(self):
         for prop in self.getProp("Monitors"):
             if prop not in self.knownMonitors():
@@ -96,7 +99,14 @@ class LHCbApp(LHCbConfigurableUser):
             getConfigurable("StatusCodeSvc").OutputLevel = INFO
         if "FPE" in self.getProp("Monitors"):
             importOptions( "$STDOPTS/FPEAudit.opts" )
-
+    
+    def defineXMLSum(self):
+        if hasattr( self, "XMLSummary" ):
+            if self.getProp("XMLSummary") is None: return
+            xmlSum=XMLSummarySvc("CounterSummarySvc")
+            xmlSum.xmlfile=self.getProp("XMLSummary")
+            ApplicationMgr().ExtSvc+=[ xmlSum ]
+    
     def defineOutput(self):
         # Message service
         msgSvc = getConfigurable("MessageSvc")
@@ -116,9 +126,10 @@ class LHCbApp(LHCbConfigurableUser):
             # add a time stamp to remaining messages
             msgSvc.Format = "%u % F%18W%S%7W%R%T %0W%M"
             msgSvc.timeFormat = "%Y-%m-%d %H:%M:%S UTC"
-
+    
     def __apply_configuration__(self):
         self.defineDB()
         self.defineEvents()
         self.defineMonitors()
+        self.defineXMLSum()
         self.defineOutput()
