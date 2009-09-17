3f75ba66d23418616a6646590d0d6101708d6575
psomogyi <psomogyi@cern.ch>
2009-09-17 14:13:05 +0000

    stub for trend baseline
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@99808 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f0c8008bb01acd1da2e8ee2f7903a895c91f5aca

--- Online/Online/Presenter/test/startHltTest.sh
+++ Online/Online/Presenter/test/startHltTest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-. startgauditest.sh HLTA0102_GauchoJob_2 &
+source startgauditest.sh HLTA0102_GauchoJob_2 &
+#setenv  UTGID "HLTA0102_Adder_2";
 export UTGID="HLTA0102_Adder_2";
-. Adder.sh&
+source Adder.sh&
---
+++ Online/Online/Presenter/test/trendRefStub.C
@@ -0,0 +1,29 @@
+// GauchoJob/MonitorSvc/monRate/Counternumber2
+// GauchoJob/MonitorSvc/monRate/Eventsaccepted-flag
+// GauchoJob/MonitorSvc/monRate/Eventsaccepted-nEntries
+// GauchoJob/MonitorSvc/monRate/NUMBER_OF_PROCESSES
+// GauchoJob/MonitorSvc/monRate/Counternumber1
+// GauchoJob/MonitorSvc/monRate/NUMBER_OF_PROCESSES
+
+void trendRefStub() {
+TFile *refFile = new TFile("TCK_1.root","recreate");
+
+// create a subdirectory for each algo ...for "new" fileformat
+TDirectory *algo1= refFile->mkdir("GauchoJob");
+ algo1->cd(); 
+TDirectory *algo2= algo1->mkdir("MonitorSvc");
+ algo2->cd(); 
+TDirectory *algo3= algo2->mkdir("monRate");
+ algo3->cd(); 
+
+const Int_t nbins = 200;
+const Double_t refValue1 = 1.5;
+TH1D* h1 = new TH1D("Counternumber1","GauchoJob/MonitorSvc/monRate/Counternumber1",nbins,0,nbins);
+Int_t i;
+for (i=1 ; i < nbins; i++) {
+  h1->SetBinContent(refValue1, 1);
+}   
+refFile->Write();
+delete refFile;
+}
+
