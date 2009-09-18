efdb4244377e68115ef995a36278c44be0a90c72
Gloria Corti <gloria.corti@cern.ch>
2009-09-18 16:29:13 +0000

    add python options for production
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94478 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 84ff39821cee26cb14ce0f63ec85961b5f40046c

---
+++ Gauss/Gen/LbPythia/options/Pythia.py
@@ -0,0 +1,8 @@
+##  Empty Pythia options to be included in production to generate
+##  standard events as to havethe same structure for all generators
+##
+##  author G.Corti
+##  date 2009-09-18
+##
+
+
