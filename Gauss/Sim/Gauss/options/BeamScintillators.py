b50bbfe95454e35749a86aafdb1da603bd96b31e
Gloria Corti <gloria.corti@cern.ch>
2009-09-18 17:30:17 +0000

    replace old BeamScintillators.opts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94483 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 302f3b144ad7965a2644e0588ada18ef843d1eb8

---
+++ Gauss/Sim/Gauss/options/BeamScintillators.py
@@ -0,0 +1,28 @@
+## /** @file
+##  *
+##  *  File containing options to simulate geometry upstream of VELO and
+##  *  produce hits in Scintillator planes just at the wall.
+##  *
+##  *  @author G.Corti
+##  *  @date 2008-05-06
+##  */
+
+from Gaudi.Configuration import *
+
+from Configurables import GetTrackerHitsAlg
+
+## Switch on geometry upstream
+importOptions('$GAUSSOPTS/BeforeVeloGeometry.py')
+
+## Retrieve hits from Scintillator planes
+getScintillatorHits.CollectionName = "VPlaneSDet/Hits"
+getScintillatorHits.MCHitsLocation = "/Event/MC/Scintillators/Hits"
+getScintillatorHits.Detectors = [
+  "/dd/Structure/LHCb/BeforeMagnetRegion/BeforeVelo/VPlane1", 
+  "/dd/Structure/LHCb/BeforeMagnetRegion/BeforeVelo/VPlane2" 
+]
+getScintillatorHits.ExtendedInfo = True
+GaudiSequencer("DetectorsHits").Members += [ getScintillatorHits ]
+
+## and save them on sim file
+OutputStream("GaussTape").ItemList += [ "/Event/MC/Scintillators/Hits#1" ]
