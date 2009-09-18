272f5fc4d5c258941f1051b6ca488cea10a185e1
jpalac <jpalac@cern.ch>
2009-09-18 17:41:02 +0000

    Required new input. Now use evt type  10000000, DC06-Sim + DC06-Reco_v30 + DC06-Stripping_v31
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78693 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b30714d7f178ad850885fa1c0a91431a24403c2b

--- DaVinci/DaVinciSys/tests/options/DVTestHlt-DC06-Effective.py
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt-DC06-Effective.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DVTestHlt-DC06-Effective.py,v 1.2 2009-09-03 10:38:04 pkoppenb Exp $
+# $Id: DVTestHlt-DC06-Effective.py,v 1.3 2009-09-18 17:41:02 jpalac Exp $
 #
 # Options for a typical DaVinci job
 #
@@ -33,11 +33,23 @@ DaVinci().ReplaceL0BanksWithEmulated = True    ## enable if you want to rerun L0
 DaVinci().Hlt2Requires = 'L0'                  ## Run Hlt1 and 2
 DaVinci().HltType = 'Hlt1+Hlt2'                ## pick one of 'Hlt1', 'Hlt2', or 'Hlt1+Hlt2'
 DaVinci().HltThresholdSettings = 'Effective_Nominal'
-#
-# DC06 stripped BB in order to get some statistics
-#
+
+#-- GAUDI jobOptions generated on Fri Sep 18 18:52:16 2009
+#-- Contains event types : 
+#--   10000000 - 10 files - 17852 events - 11.10 GBytes
+#-- DC06-Sim + DC06-Reco_v30 + DC06-Stripping_v31
 EventSelector().Input   = [
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v3-lumi2/00001883/DST/0000/00001883_00002264_2.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000857_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000858_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000859_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000860_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000861_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000862_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000867_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000874_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000924_3.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/production/DC06/v1r0/00002000/DST/0000/00002000_00000926_3.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+FileCatalog().Catalogs = [ 'xmlcatalog_file:DC06-Stripped-inclb-unbiased.xml' ]
 
 ########################################################################
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
