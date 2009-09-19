1087e25be9e879dd33481128d81f22e95d20f350
Patrick Robbe <robbe@lal.in2p3.fr>
2009-09-19 16:38:48 +0000

    Add new decay models for B->Xll (MS)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94499 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d33436ecf67a167cb6b0789e080eb46ed7dc2f8b

--- Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
+++ Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
@@ -1,12 +1,14 @@
 #include "EvtGenModels/EvtModelRegExtras.hh"
 
 #include "EvtGenModels/EvtBToKStarllDurham07.hh"
+#include "EvtGenModels/EvtbTosllMS.hh"
 
 //we use auto_ptr here to show explicit transfer of ownership
 std::auto_ptr<const EvtModelList> EvtModelRegExtras::getModels(){
 	
 	EvtModelList* models = new EvtModelList();
 	models->push_back(new EvtBToKStarllDurham07);
+  models -> push_back( new EvtbTosllMS() ) ;
 	return std::auto_ptr<const EvtModelList>(models);
 	
 }
