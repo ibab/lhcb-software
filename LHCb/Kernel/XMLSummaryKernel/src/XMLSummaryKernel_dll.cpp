b1db6397e9143d45860bdf4810d11e051cb10a08
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-09-20 10:16:33 +0000

    remove unneccesary file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48466 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 68809b0eb9f5d3288036bf285aa819c6e0cdf860

--- LHCb/Kernel/XMLSummaryKernel/src/XMLSummaryKernel_dll.cpp
+++
@@ -1,5 +0,0 @@
-// $Id: XMLSummaryKernel_dll.cpp,v 1.1.1.1 2009-09-11 10:52:12 rlambert Exp $
-
-// DllMain entry point
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES(XMLSummaryKernel)
