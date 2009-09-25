28bcddc316d954de9464221b60c5add938b96a0a
Jeroen Van Tilburg <jeroen.van.tilburg@cern.ch>
2009-09-25 16:39:14 +0000

    Added new algorithm: TTGenericTracking. See include file for documentation.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@19335 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3a5da36a3e68f745ccbc6ce956c0e08016a47ed5

---
+++ Rec/Tf/TsaAlgorithms/src/TTGenericTracking.cpp
@@ -0,0 +1,343 @@
+// $Id: TTGenericTracking.cpp,v 1.1 2009-09-25 16:39:14 jvantilb Exp $
+
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// from DigiEvent and TrackEvent
+#include "Event/STCluster.h"
+#include "Event/Track.h"
+#include "Event/StateParameters.h"
+
+// TfKernel
+#include "TfKernel/STHit.h"
+
+// local
+#include "TTGenericTracking.h"
+
+// boost
+#include <boost/assign/list_of.hpp> // for 'vector_list_of()'
+
+using namespace LHCb;
+using namespace Tf;
+using namespace Gaudi::Units;
+using namespace boost::assign;
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( TTGenericTracking );
+
+//--------------------------------------------------------------------
+//
+//  TTGenericTracking : TT Generic track finding algorithm
+//
+//        More details can be found in the TTGenericTracking.h
+//
+//--------------------------------------------------------------------
+
+TTGenericTracking::TTGenericTracking( const std::string& name, 
+                                      ISvcLocator* pSvcLocator ) :
+  GaudiAlgorithm(name, pSvcLocator )
+{
+  // constructer
+  declareProperty("InputData", m_clusterLocation=STClusterLocation::TTClusters);
+  declareProperty("OutputLocation", m_outputLocation = "Rec/Track/TT" );
+  declareProperty("MaxNumClusters", m_maxNumClusters  = 200      );
+  declareProperty("ChargeCut",      m_chargeCut       = 15.0     );// ADC counts
+  declareProperty("ToleranceY",     m_toleranceY      = 1.0 * mm );
+  declareProperty("DistCut",        m_distCut         = 1.0 * mm );
+  declareProperty("WindowCenter",   m_winCenter       = list_of(0.0)(0.0)(0.0));
+  declareProperty("HalfWindowXSize",m_windowX         =-1.0 * mm );
+  declareProperty("HalfWindowYSize",m_windowY         =-1.0 * mm );
+  declareProperty("AddRich1State",  m_addRich1State   = true     );  
+}
+
+StatusCode TTGenericTracking::initialize()
+{
+  StatusCode sc = GaudiAlgorithm::initialize();
+  if (sc.isFailure()) return Error("Failed to initialize", sc);
+
+  if( m_winCenter.size() != 3 ) {
+    sc = StatusCode::FAILURE;
+    return Error("WindowCenter does not have 3 coordinates.", sc);
+  } 
+
+  // Internal counters
+  m_nTrackCandidates = 0;
+  m_nTriggered = 0;
+  m_nEvent = 0;
+  
+  // Get the detector element
+  m_tracker = getDet<DeSTDetector>(DeSTDetLocation::location("TT"));
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TTGenericTracking::finalize()
+{
+  info() << "Events with tracks " << m_nTriggered 
+         << ", TT tracks " << m_nTrackCandidates << endmsg;
+  return StatusCode::SUCCESS;
+}
+
+StatusCode TTGenericTracking::execute()
+{
+  // Initialize the event
+  setFilterPassed(false);
+  ++m_nEvent;
+
+  // Register the tracks
+  Tracks* tracks = new Tracks();
+  tracks->reserve(10);
+  put( tracks, m_outputLocation);
+
+  // Retrieve the clusters
+  const STClusters* clusterCont = get<STClusters>(m_clusterLocation);  
+  if (clusterCont->size() > m_maxNumClusters) {
+    info() << "Number of clusters too high: " << clusterCont->size() 
+           << " (cut set at " << m_maxNumClusters << ") -> Skip this event." 
+           << endmsg;  
+    return StatusCode::SUCCESS;
+  }
+  
+  // make the ST hits (stores geometrical information)
+  STHits hits; hits.reserve(clusterCont->size());
+  STClusters::const_iterator iter = clusterCont->begin();
+  for( ; iter != clusterCont->end(); ++iter ) {
+
+    // Select clusters above a certain threshold
+    if ((*iter)->totalCharge() < m_chargeCut ) continue;
+
+    // Ignore bad channels (e.g. broken bonds)
+    const DeSTSector* aSector = m_tracker->findSector((*iter)->channelID()); 
+    if( !aSector->isOKStrip((*iter)->channelID()) ) continue;
+
+    // Create a Tf::STHit and add it to the container
+    STHit* newHit = new STHit(*aSector,(*iter)->liteCluster());
+    hits.push_back(newHit);
+  }
+
+  // Step 1: Make all possible combinations with 4 hits in 4 different layers
+  findTTtracks( tracks, hits );
+  
+  // Skip the rest if there are no tracks
+  if( tracks->size() == 0 ) return StatusCode::SUCCESS;
+  setFilterPassed(true);
+
+  // Step 2: Add additional hits to the track
+  addAdditionalHits( tracks, hits );
+
+  // Step 3: Remove clones
+  removeClones( tracks );  
+
+  // Update counters
+  m_nTrackCandidates += tracks->size();
+  ++m_nTriggered;
+  
+  // Print number of track candidates in event
+  info() << "Event " << m_nEvent << " has " << tracks->size() 
+         << " track candidate" << ((tracks->size()!=1) ? "s." : ".") << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+
+// Make all possible combinations with 4 hits in 4 different layers
+void TTGenericTracking::findTTtracks(Tracks* tracks, STHits& hits )
+{
+  // Arrange the hits by layer 
+  STHits x1Hits; x1Hits.reserve(hits.size());
+  STHits x2Hits; x2Hits.reserve(hits.size());
+  STHits uHits;  uHits.reserve(hits.size());
+  STHits vHits;  vHits.reserve(hits.size());
+  STHits::const_iterator iHit = hits.begin();
+  for( ; iHit != hits.end(); ++iHit ) {
+    if( (*iHit)->station() == 0  ) {
+      ((*iHit)->layer()==0) ? x1Hits.push_back(*iHit) : uHits.push_back(*iHit);
+    } else {
+      ((*iHit)->layer()==0) ? vHits.push_back(*iHit) : x2Hits.push_back(*iHit);
+    }
+  }
+
+  // Loop over the hits in the first layer (X1 layer)
+  STHits::const_iterator iX1Hit = x1Hits.begin();
+  for( ; iX1Hit != x1Hits.end(); ++iX1Hit ) {
+
+    // Get the coordinate of the X1 hit
+    double x = (*iX1Hit)->x();
+    double z = (*iX1Hit)->z();
+
+  // Loop over the hits in the last layer (X2 layer)
+    STHits::const_iterator iX2Hit = x2Hits.begin();
+    for( ; iX2Hit != x2Hits.end(); ++iX2Hit ) {
+      
+      // Get the slope through both hits
+      double zX2 = (*iX2Hit)->z();
+      double tx  = ( (*iX2Hit)->x() - x )/( zX2 - z );
+
+      // Apply window cuts in x if necessary
+      if( m_windowX > 0.0 ) {
+        double xAtWindow = x + tx*(m_winCenter[2] - z);
+        if( fabs(xAtWindow - m_winCenter[0]) > m_windowX ) continue;        
+      }
+
+      // Preliminary check on the y window using the endpoints of the strips
+      if( m_windowY > 0.0 ) {
+        double minTy = ( (*iX2Hit)->yMin() - (*iX1Hit)->yMax() )/( zX2 - z ) ;
+        double maxTy = ( (*iX2Hit)->yMax() - (*iX1Hit)->yMin() )/( zX2 - z ) ;
+        double minYAtWindow = (*iX1Hit)->yMax() + minTy*(m_winCenter[2] - z);
+        double maxYAtWindow = (*iX1Hit)->yMin() + maxTy*(m_winCenter[2] - z);
+        if( minYAtWindow - m_winCenter[1] > m_windowY && 
+            maxYAtWindow - m_winCenter[1] > m_windowY ) continue;
+        if( minYAtWindow - m_winCenter[1] < -m_windowY && 
+            maxYAtWindow - m_winCenter[1] < -m_windowY ) continue;
+      }
+
+      // Add u-layer hits
+      STHits::const_iterator iUHit = uHits.begin();
+      for( ; iUHit != uHits.end(); ++iUHit ) {
+
+        // Calculate intersection and check if compatible with u-layer hit
+        double zU = (*iUHit)->z();
+        double xU = x + tx*(zU - z);
+        double yU = ( xU - (*iUHit)->x() )/(*iUHit)->dxDy();
+        if( !(*iUHit)->isYCompatible( yU, m_toleranceY ) ) continue;
+
+        // Preliminary check if intersection is compatible with x-layer hits
+        double minTx = ( (*iX2Hit)->yMin() - m_toleranceY - yU )/( zX2 - zU ) ;
+        double maxTx = ( (*iX2Hit)->yMax() + m_toleranceY - yU )/( zX2 - zU ) ;
+        double minYatX1 = yU + maxTx*(z - zU);
+        double maxYatX1 = yU + minTx*(z - zU);
+        if( !(*iX1Hit)->isYCompatible( minYatX1 , m_toleranceY ) &&
+            !(*iX1Hit)->isYCompatible( maxYatX1 , m_toleranceY ) ) continue;
+        
+        // Another preliminary check on the y window using the endpoints of 
+        // the strip in the X2 layer and the intersection y-point.
+        if( m_windowY > 0.0 ) {
+          double minTy = ( (*iX2Hit)->yMin() - yU )/( zX2 - zU ) ;
+          double maxTy = ( (*iX2Hit)->yMax() - yU )/( zX2 - zU ) ;
+          double minYAtWindow = yU + minTy*(m_winCenter[2] - zU);
+          double maxYAtWindow = yU + maxTy*(m_winCenter[2] - zU);
+          if( minYAtWindow - m_winCenter[1] > m_windowY && 
+              maxYAtWindow - m_winCenter[1] > m_windowY ) continue;
+          if( minYAtWindow - m_winCenter[1] < -m_windowY && 
+              maxYAtWindow - m_winCenter[1] < -m_windowY ) continue;
+        }
+
+        // Add v-layer hits
+        STHits::const_iterator iVHit = vHits.begin();
+        for( ; iVHit != vHits.end(); ++iVHit ) {
+          
+          // Calculate intersection and check if intersection is compatible
+          double zV = (*iVHit)->z();
+          double xV = x + tx*(zV - z);
+          double yV = ( xV - (*iVHit)->x() )/(*iVHit)->dxDy();
+          if( !(*iVHit)->isYCompatible( yV, m_toleranceY ) ) continue;
+
+          // Check if x hits are compatible in y 
+          double ty = (yV - yU)/( zV - zU );
+          double y  = yU + ty*(z - zU);
+          double yX2 = yV + ty*(zX2 - zV);
+          if( !(*iX1Hit)->isYCompatible( y  , m_toleranceY ) ) continue;
+          if( !(*iX2Hit)->isYCompatible( yX2, m_toleranceY ) ) continue;
+
+          // Apply final window cut in y
+          if( m_windowY > 0.0 ) {
+            double yAtWindow = y + ty*(m_winCenter[2] - z);
+            if( fabs(yAtWindow - m_winCenter[1]) > m_windowY ) continue;        
+          }
+
+          // Add state at first measurement
+          LHCb::State firstState;
+          firstState.setState( x, y, z, tx, ty, 1./(10.0*GeV) );
+          firstState.setLocation( State::FirstMeasurement );
+          // Make a vector with the lhcbIDs
+          std::vector<LHCbID> ids = list_of((*iX1Hit)->lhcbID())
+            ((*iUHit )->lhcbID())((*iVHit )->lhcbID())((*iX2Hit)->lhcbID());
+          // Make a track
+          Track* track = new Track(Track::PatVeloTT, Track::Upstream, 
+                                   Track::PatRecIDs, ids, firstState );
+          // Add state at start of RICH1
+          if( m_addRich1State ) {
+            LHCb::State rich1State;
+            double zRich1 = StateParameters::ZBegRich1;
+            rich1State.setState( x + tx*(zRich1-z), y + ty*(zRich1-z), zRich1,
+                                 tx, ty, 1./(10.0*GeV) );
+            rich1State.setLocation( State::BegRich1 );
+            track->addToStates( rich1State );
+          }
+          tracks->add( track );
+
+          // Found track
+          debug() << "Found track, event " << m_nEvent << endmsg;
+          debug() << "State: " << x << ", " << y << ", " 
+                  << z << ", " << tx << ", " << ty << endmsg;
+        }        
+      }
+    }
+  }
+  return;
+}
+
+
+// Add additional hits to the track
+void TTGenericTracking::addAdditionalHits( Tracks* tracks, STHits& hits )
+{
+  Tracks::iterator iTrack = tracks->begin();
+  for( ; iTrack != tracks->end(); ++iTrack ) {
+
+    // Loop over all TT clusters
+    STHits::const_iterator iHit = hits.begin();
+    for( ; iHit != hits.end(); ++iHit ) {
+
+      // Check if cluster not already on sector that was hit
+      bool found = false;
+      std::vector<LHCbID>::const_iterator id = ((*iTrack)->lhcbIDs()).begin();
+      while( id != ((*iTrack)->lhcbIDs()).end() && !found ) {
+        if( (*iHit)->uniqueSector() == (*id).stID().uniqueSector() ) found=true;
+        ++id;
+      }
+      if( found ) continue;
+
+      // Check if hit compatible with track
+      State myState = (*iTrack)->firstState();
+      double hitX = myState.x()+myState.tx()*((*iHit)->zMid()-myState.z());
+      double hitY = myState.y()+myState.ty()*((*iHit)->zMid()-myState.z());
+      double b = - (*iHit)->dxDy();
+      double dist = fabs(hitX + hitY*b - (*iHit)->x())/sqrt(1+b*b);
+      if( dist > m_distCut ) continue;
+
+      // Check if hit within its physical boundaries
+      if( !(*iHit)->isYCompatible( hitY, m_toleranceY ) ) continue;
+
+      // Add the hit to the track
+      (*iTrack)->addToLhcbIDs( (*iHit)->lhcbID() );
+      debug() << "Found additional hit: distance = " << dist << endmsg;
+    }
+  }
+  return;
+}
+
+
+// Find and remove the clones in the container of tracks
+void TTGenericTracking::removeClones( Tracks* tracks ) 
+{
+  Tracks::iterator iTrack = tracks->begin();
+  for( ; iTrack != tracks->end(); ++iTrack ) {
+    Tracks::iterator jTrack = iTrack;
+    ++jTrack;
+    for( ; jTrack != tracks->end(); ++jTrack ) {
+      // Check this
+      unsigned int nCommon = (*iTrack)->nCommonLhcbIDs(*(*jTrack));
+      if( nCommon == (*jTrack)->nLHCbIDs() ) {
+        debug() << "Found clone. Remove track " << jTrack-tracks->begin()
+                << " from second loop." << endmsg;
+        tracks->erase(jTrack--);
+      } else if( nCommon == (*iTrack)->nLHCbIDs() ) {
+        debug() << "Found clone. Remove track " << iTrack-tracks->begin()
+                << " from first loop." << endmsg;
+        tracks->erase(iTrack--);
+        break;
+      }
+    }
+  }
+  return;
+}
---
+++ Rec/Tf/TsaAlgorithms/src/TTGenericTracking.h
@@ -0,0 +1,109 @@
+// $Id: TTGenericTracking.h,v 1.1 2009-09-25 16:39:14 jvantilb Exp $
+#ifndef TTGenericTracking_H
+#define TTGenericTracking_H 1
+
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "STDet/DeSTDetector.h"
+
+/** @class TTGenericTracking TTGenericTracking.h
+ *
+ *  Algorithm to find tracks in TT. This algorithm is mainly useful for finding
+ *  cosmic tracks, since for regular collision data the occupancy is too high.
+ *  Keep in mind that the TT has only 4 layers, so a typical TT track will have
+ *  only 4 hits, having 0 degrees of freedom left for the linear track fit (4 
+ *  parameters).
+ *  The algorithm has three stages:
+ *  - <b>Track finding</b>: Loops over the hits in the x layers to make all
+ *    possible combinations of 2d tracks out of a hit in an x1 layer and a
+ *    hit in an x2 layer. Then, it searches for hits in the u and v layers to
+ *    include information on the y position. The intersection of the 2d track
+ *    with the stereo hit has to be compatible in y with the begin and end
+ *    points of the strip. When it has found two stereo hits, it also checks if
+ *    the 3d track is compatible in y with the x hits.
+ *  - <b>Adding additional hits</b>: Due to the staggered layout of a
+ *    layer, there are overlaps between the sensors within a single layer.
+ *    Therefore there can sometimes be more than 4 hits on a track. In this
+ *    step, a search is performed on additional hits in these staggered
+ *    sensors.
+ *  - <b>Removing clones</b>: Also due to these overlaps, there can be clones
+ *    (tracks that share most of their hits). In this step all tracks that are a
+ *    subset of another track are removed.
+ *  
+ *  These are the options that can be set:
+ *  - <b>ClusterLocation</b>: Input location in the TES of the TT clusters.
+ *  - <b>OutputLocation</b>: Output location in the TES of the TT tracks.
+ *  - <b>MaxNumClusters</b>: Cut on busy events. Very useful, since for some
+ *    (busy) events the required CPU time can be too high. Default value is now
+ *    set to 1000 (somewhat arbitrary).
+ *  - <b>ChargeCut</b>: Cut on the total charge (in ADC counts) of the
+ *    STClusters.
+ *  - <b>ToleranceY</b>: Tolerance for compatibility in y of the hit with the
+ *    strip.
+ *  - <b>DistCut</b>: Maximum distance in mm of the additional hits to the
+ *     track.
+ *  - <b>WindowCenter</b>: Vector of three doubles that represents the central
+ *    3d point (in mm) of the window through which the tracks are required to
+ *    pass.
+ *  - <b>HalfWindowXSize and HalfWindowYSize</b>: Size in mm of the half window
+ *    in x and y through which the tracks are required to pass. For example,
+ *    when the WindowCenter is set to (0,0,880) and the HalfWindowXSize is set
+ *    to 500 mm, the tracks are first extrapolated to z=880. Only tracks that
+ *    are then within a window of +- 500 mm in x are kept.
+ *  - <b>AddRich1State</b>: Boolean flag to determine whether a state at the
+ *    entrance of RICH1 is added to the track.
+ *
+ *  @author J. van Tilburg
+ *  @date   25/09/2009
+ */
+
+class TTGenericTracking : public GaudiAlgorithm {
+
+public:
+ 
+  /// constructer
+  TTGenericTracking( const std::string& name, ISvcLocator *svcloc );
+
+  /// initialize
+  StatusCode initialize();
+
+  /// execute
+  StatusCode execute();
+
+  /// finalize
+  StatusCode finalize();
+
+private:
+
+  /// Make all possible combinations with 4 hits in 4 different layers
+  void findTTtracks(LHCb::Tracks* tracks, Tf::STHits& hits );
+
+  /// Add additional hits to the track
+  void addAdditionalHits( LHCb::Tracks* tracks, Tf::STHits& hits );
+
+  /// Find and remove the clones in the container of tracks
+  void removeClones( LHCb::Tracks* tracks );
+  
+
+  // Internal counters
+  int m_nEvent;           ///< Counts the number of events
+  int m_nTriggered;       ///< Counts the number of events with TT tracks
+  int m_nTrackCandidates; ///< Counts the total number of TT tracks
+
+  /// Pointer to the TT detector element
+  DeSTDetector* m_tracker;
+  
+  // job options
+  std::string m_clusterLocation;  ///< Location of the TT clusters
+  std::string m_outputLocation;   ///< Location of the TT tracks
+  unsigned int m_maxNumClusters;  ///< Cut on busy events
+  double m_chargeCut;             ///< Cut on the total charge of the STClusters
+  double m_toleranceY;            ///< Tolerance for hit compatibility in y
+  double m_distCut;               ///< Cut on distance track to additional hits
+  std::vector<double> m_winCenter;///< Central point (x,y,z) of allowed window
+  double m_windowX;               ///< Half the size of the window in x
+  double m_windowY;               ///< Half the size of the window in y
+  bool m_addRich1State;           ///< Flag to add a state at RICH1 entrance
+
+};
+
+#endif // TTGenericTracking_H
