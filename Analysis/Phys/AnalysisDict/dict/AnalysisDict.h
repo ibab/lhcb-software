efb8c4617dd61f885ddd8de03fd6f8ddfa749c2d
jpalac <jpalac@cern.ch>
2009-09-28 16:11:25 +0000

    First import of Phys/AnalysisDict
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@78772 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b375462146b1e1cfdce1cd7805a9f402bb04053e

---
+++ Analysis/Phys/AnalysisDict/dict/AnalysisDict.h
@@ -0,0 +1,35 @@
+// $Id: AnalysisDict.h,v 1.1.1.1 2009-09-28 16:11:25 jpalac Exp $
+#ifndef DICT_DAVINCIMCKERNELDICT_H 
+#define DICT_DAVINCIMCKERNELDICT_H 1
+
+// Include files
+
+/** @file DaVinciMCKernelDict DaVinciMCKernelDict.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2006-10-02
+ */
+// ============================================================================
+// DaVinciMCKernel
+// ============================================================================
+#include "Kernel/IBackgroundCategory.h"
+#include "Kernel/ICheatedLifetimeFitter.h"
+#include "Kernel/IDaVinciAssociatorsWrapper.h"
+#include "Kernel/IFilterMCParticles.h"
+#include "Kernel/IHepMC2MC.h"
+#include "Kernel/IMCParticleArrayFilter.h"
+#include "Kernel/IMC2Collision.h"
+#include "Kernel/IParticle2MCAssociator.h"
+#include "Kernel/IParticle2MCWeightedAssociator.h"
+#include "Kernel/IP2VVMCPartAngleCalculator.h"
+#include "Kernel/IPrintDecayTreeTool.h"
+#include "Kernel/IPV2MC.h"
+#include "Kernel/MCAssociation.h"
+#include "Kernel/Particle2MCParticle.h"
+namespace
+{
+  std::vector<MCAssociation> __stdvector_MCAssoc;
+}
+
+#endif // DICT_DAVINCIMCKERNELDICT_H
