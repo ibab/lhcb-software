1fb13b7c709807e291e4ae227fc1972da4707ad3
Tomasz Skwarnicki <tskwarni@syr.edu>
2009-10-02 11:00:54 +0000

    adopt options/Gauss-Upgrade.py to changes in AppConfig
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94528 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0275ed0b13cb65bc5341a39ffa869d9e8e0d3f9f

--- Gauss/Sim/Gauss/options/Gauss-Upgrade.py
+++ Gauss/Sim/Gauss/options/Gauss-Upgrade.py
@@ -6,14 +6,17 @@
 ##############################################################################
 
 from Gaudi.Configuration import *
-#           select beam conditions
-importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b7TeV-md100-nu6,8-25ns.py")
-# importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b5TeV-md100.py")
-# 2x10^32 importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b7TeV-md100-nu0,7-25ns.py")
-# 5x10^32 importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b7TeV-md100-nu1,7-25ns.py")
-# 1x10^33 importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b7TeV-md100-nu3,4-25ns.py")
-# 2x10^33 importOptions("$APPCONFIGOPTS/Gauss/Upgrade-b7TeV-md100-nu6,8-25ns.py")
 
+#           tell Gauss these are upgrade simulations
+importOptions("$APPCONFIGOPTS/Gauss/Upgrade.py")
+
+#           select beam conditions
+importOptions("$APPCONFIGOPTS/Gauss/MC09-b7TeV-md100-nu6,8-25ns.py")
+# importOptions("$APPCONFIGOPTS/Gauss/MC09-b5TeV-md100.py")
+# 2x10^32 importOptions("$APPCONFIGOPTS/Gauss/MC09-b7TeV-md100-nu0,7-25ns.py")
+# 5x10^32 importOptions("$APPCONFIGOPTS/Gauss/MC09-b7TeV-md100-nu1,7-25ns.py")
+# 1x10^33 importOptions("$APPCONFIGOPTS/Gauss/MC09-b7TeV-md100-nu3,4-25ns.py")
+# 2x10^33 importOptions("$APPCONFIGOPTS/Gauss/MC09-b7TeV-md100-nu6,8-25ns.py")
 
 ##############################################################################
 # Database tags must be set and are defined in a separate file
