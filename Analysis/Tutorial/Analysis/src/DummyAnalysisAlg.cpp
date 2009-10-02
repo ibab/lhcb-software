57818dda79514d4168359712bcec849dcffc9590
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-10-02 07:24:53 +0000

    dummy alg to allow creation of library from empty src directory
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@78838 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6a4f7dce462eb069396be9ea5c3113538ebb3a6a

---
+++ Analysis/Tutorial/Analysis/src/DummyAnalysisAlg.cpp
@@ -0,0 +1,68 @@
+// $Id: DummyAnalysisAlg.cpp,v 1.1 2009-10-02 07:24:53 cattanem Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+// local
+#include "DummyAnalysisAlg.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : DummyAnalysisAlg
+//
+// 2009-10-02 : Marco Cattaneo
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( DummyAnalysisAlg );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+DummyAnalysisAlg::DummyAnalysisAlg( const std::string& name,
+                                    ISvcLocator* pSvcLocator)
+  : DVAlgorithm ( name , pSvcLocator )
+{
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+DummyAnalysisAlg::~DummyAnalysisAlg() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode DummyAnalysisAlg::initialize() {
+  StatusCode sc = DVAlgorithm::initialize(); 
+  if ( sc.isFailure() ) return sc;
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode DummyAnalysisAlg::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  // code goes here  
+
+  setFilterPassed(true);  // Mandatory. Set to true if event is accepted. 
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode DummyAnalysisAlg::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return DVAlgorithm::finalize();
+}
+//=============================================================================
---
+++ Analysis/Tutorial/Analysis/src/DummyAnalysisAlg.h
@@ -0,0 +1,32 @@
+// $Id: DummyAnalysisAlg.h,v 1.1 2009-10-02 07:24:53 cattanem Exp $
+#ifndef DUMMYANALYSISALG_H 
+#define DUMMYANALYSISALG_H 1
+
+// Include files
+// from DaVinci, this is a specialized GaudiAlgorithm
+#include "Kernel/DVAlgorithm.h"
+
+
+/** @class DummyAnalysisAlg DummyAnalysisAlg.h
+ *  Dummy algorithm to allow creation of empty library in Tutorial example
+ *
+ *  @author Marco Cattaneo
+ *  @date   2009-10-02
+ */
+class DummyAnalysisAlg : public DVAlgorithm {
+public: 
+  /// Standard constructor
+  DummyAnalysisAlg( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~DummyAnalysisAlg( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+
+};
+#endif // DUMMYANALYSISALG_H
