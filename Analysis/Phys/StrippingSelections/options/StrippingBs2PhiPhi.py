727656c2e40d86660c28eec7a82059f1d8a0b052
schleich <schleich@cern.ch>
2009-10-05 15:20:47 +0000

    Relaxed PID cut in Bs2PhiPhi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@78895 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ea80b47c124ea11ea277367f28e888e53ae53306

--- Analysis/Phys/StrippingSelections/options/StrippingBs2PhiPhi.py
+++ Analysis/Phys/StrippingSelections/options/StrippingBs2PhiPhi.py
@@ -1,43 +1,54 @@
-# $Id: StrippingBs2PhiPhi.py,v 1.3 2009-09-15 18:02:49 schleich Exp $
+# $Id: StrippingBs2PhiPhi.py,v 1.4 2009-10-05 15:20:47 schleich Exp $
 
 __author__ = 'Sebastian Schleich'
-__date__ = '06/08/2009'
-__version__ = '$Revision: 1.3 $'
+__date__ = '05/10/2009'
+__version__ = '$Revision: 1.4 $'
 
 '''
-Bs->PhiPhi stripping selection test
+Bs->PhiPhi stripping selection
 '''
 
 from Gaudi.Configuration import *
 from StrippingConf.StrippingLine import StrippingLine, StrippingMember, StrippingTool
-from Configurables import FilterDesktop, CombineParticles, OfflineVertexFitter, PhysDesktop
+from Configurables import FilterDesktop, CombineParticles #, OfflineVertexFitter, PhysDesktop
 import GaudiKernel.SystemOfUnits as Units
 
 
-StrippingPhi2KK = StrippingMember ( FilterDesktop
-                   , "StrippingPhi2KK"
-                   , InputLocations = ["StdLooseDetachedPhi2KK"]
-                   , Code = "(ADMASS('phi(1020)') < 20.*MeV)" 
-                   )
+Bs2PhiPhiLooseDetachedPhi2KK = StrippingMember ( CombineParticles
+                          , "Bs2PhiPhiLooseDetachedPhi2KK"
+                          , InputLocations = ["StdNoPIDsKaons"]
+                          , DecayDescriptor = "phi(1020) -> K+ K-"
+                          , DaughtersCuts = {"K+": "((PIDK-PIDpi)>-7.0)&(MIPCHI2DV(PRIMARY)>4)"}
+                          , CombinationCut = "(AM < 1100.*MeV)"
+                          , MotherCut = "(VFASPF(VCHI2) < 25.0)"
+                          )
 
-BsCombine = StrippingMember( CombineParticles
-          , "BsCombine"
+Bs2PhiPhiPhi2KK = StrippingMember ( FilterDesktop
+                , "Bs2PhiPhiPhi2KK"
+                , InputLocations = ["%Bs2PhiPhiLooseDetachedPhi2KK"]
+                , Code = "(ADMASS('phi(1020)') < 20.*MeV)"
+                )
+
+Bs2PhiPhi = StrippingMember( CombineParticles
+          , "Bs2PhiPhi"
           , DecayDescriptor =  "[B_s0 -> phi(1020) phi(1020)]cc"
-          , InputLocations  = [ "%StrippingPhi2KK" ]
-          , CombinationCut  = "(ADAMASS('B_s0')<300*MeV)&(ACHILD(PT,1)*ACHILD(PT,2)>3*GeV*GeV)"
+          , InputLocations  = [ "%Bs2PhiPhiPhi2KK" ]
+          , DaughtersCuts   = {"phi(1020)" : "PT>650*MeV"}
+          , CombinationCut  = "(ADAMASS('B_s0')<300*MeV)&(ACHILD(PT,1)*ACHILD(PT,2)>3.2*GeV*GeV)"
           , MotherCut       = "(VFASPF(VCHI2)<40)"
+          , ReFitPVs        = True
           )
 
 Bs2PhiPhiLine = StrippingLine('Bs2PhiPhiLine'
-               , prescale = 1 
-               , postscale = 1
-               , algos = [StrippingPhi2KK, BsCombine]
-               , stream = 'Hadron'
-               )
+              , prescale = 1
+              , postscale = 1
+              , algos = [Bs2PhiPhiLooseDetachedPhi2KK, Bs2PhiPhiPhi2KK, Bs2PhiPhi]
+              , stream = 'Hadron'
+              )
 
-Bs2PhiPhiLineWide = Bs2PhiPhiLine.clone('Bs2PhiPhiLineWide'
-               , prescale = 0.1 
-               , postscale = 1
-               , FilterDesktopStrippingPhi2KK = {"Code": "MM < 1100.*MeV"}
-               )
+Bs2PhiPhiWideLine = Bs2PhiPhiLine.clone('Bs2PhiPhiWideLine'
+                  , prescale = 0.05
+                  , postscale = 1
+                  , FilterDesktopBs2PhiPhiPhi2KK = {"Code": "MM < 1100.*MeV"}
+                  )
 
