8e7ad15125050c821282ecab6485ed595cefea51
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-10-07 10:52:58 +0000

    clean up some includes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@78928 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 237fe831ab7fa510cdce40821d3e73a648fa774b

--- Hlt/Hlt/HltBase/src/HltSelection.cpp
+++ Hlt/Hlt/HltBase/src/HltSelection.cpp
@@ -1,12 +1,6 @@
 #include "HltBase/HltSelection.h"
-#include "Event/Track.h"
-#include "Event/RecVertex.h"
 #include "Event/L0CaloCandidate.h"
 #include "Event/L0MuonCandidate.h"
-#include "Event/Particle.h"
-#include "boost/lambda/lambda.hpp"
-#include "boost/lambda/construct.hpp"
-#include <algorithm>
 
 
 
@@ -20,7 +14,6 @@ void Hlt::TSelection<T>::clean()
 template <typename T>
 Hlt::TSelection<T>::~TSelection()
 {
-   // assert(m_candidates.empty());
 }
 
 // explicityly instantiate allowed instances...
