57e83334883e36eb19c95bc5e4ffe99c37ca8075
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-10-08 11:18:26 +0000

    MemoryTool : two new counters & two new histograms
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48495 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f422cd1b4234cc1f9bf263d7cf5efedbfa0d48dc

--- LHCb/Kernel/LHCbAlgs/src/MemoryTool.cpp
+++ LHCb/Kernel/LHCbAlgs/src/MemoryTool.cpp
@@ -1,9 +1,11 @@
-// $Id: MemoryTool.cpp,v 1.5 2008-04-16 08:11:25 cattanem Exp $
+// $Id: MemoryTool.cpp,v 1.6 2009-10-08 11:18:26 ibelyaev Exp $
+// =============================================================================
 // Include files 
-
+// =============================================================================
 // from Gaudi
 #include "GaudiKernel/ToolFactory.h" 
 #include "GaudiKernel/Memory.h"
+// =============================================================================
 
 // local
 #include "MemoryTool.h"
@@ -23,16 +25,43 @@ DECLARE_TOOL_FACTORY( MemoryTool );
 MemoryTool::MemoryTool( const std::string& type,
                         const std::string& name,
                         const IInterface* parent )
-  : GaudiHistoTool ( type, name , parent ), m_counter(0), m_bins(0)
+  : GaudiHistoTool ( type, name , parent )
+  , m_counter(0)
+  , m_bins(0)
+  // 
+  , m_skip   ( 10     )
+  , m_prev   ( -1.e-6 )
+  , m_histo1 ( "Total Memory [MB]" ,   0 , 2000 ) 
+  , m_histo2 ( "Delta Memory [MB]" , -25 ,   25 ) 
+                      //
+  , m_totMem ( 0      )
+  , m_delMem ( 0      )
+  , m_plot1  ( 0      )
+  , m_plot2  ( 0      )
 {
-  declareProperty( "HistoSize", m_bins = 500 );
-  setProperty( "HistoPrint", false );
+  declareProperty( "HistoSize"  , m_bins = 500 );
+  //
+  declareProperty 
+    ( "SkipEvents" , 
+      m_skip       , 
+      "Skip the first N events from delta memory counter " );
+  declareProperty 
+    ( "TotalMemoryHisto" , 
+      m_histo1      , 
+      "The parameters of 'total memory' histogram" ) ;
+  declareProperty 
+    ( "DeltaMemoryHisto" , 
+      m_histo2      , 
+      "The parameters of 'delta memory' histogram" ) ;
+  //
+  setProperty ( "HistoPrint", false );
   declareInterface<IGenericTool>(this);
 }
 //=============================================================================
 // Destructor
 //=============================================================================
-MemoryTool::~MemoryTool() {}; 
+MemoryTool::~MemoryTool() {}
+// ============================================================================
 
 //=============================================================================
 // Plot the memory usage
@@ -40,7 +69,41 @@ MemoryTool::~MemoryTool() {};
 void MemoryTool::execute() {
 
   double mem = (double)System::virtualMemory();
-
+  
+  static const std::string s_TotalMemory = "Total Memory/MB" ;
+  static const std::string s_DeltaMemory = "Delta Memory/MB" ;
+  
+  if ( 0 == m_totMem                   ) { m_totMem = &counter( s_TotalMemory ) ; }
+  if ( 0 == m_delMem                   ) { m_delMem = &counter( s_DeltaMemory ) ; }
+  if ( 0 == m_plot1 && produceHistos() ) { m_plot1  = book    ( m_histo1      ) ; }
+  if ( 0 == m_plot2 && produceHistos() ) { m_plot2  = book    ( m_histo2      ) ; }
+  
+  // memory in megabytes 
+  const double memMB = mem / 1000 ;
+  // reset the counter (if required ) 
+  if      ( 0 < m_skip && (unsigned long) m_skip == m_counter && 0 != m_delMem ) 
+  {
+    // NB: I hate StatEntity::reset, use == instead!
+    *m_delMem = StatEntity() ;
+    debug()  << "Reset Delta Virtual Memory counter" << endmsg ;
+  } 
+  // Fill the counter for "valid" previous measurements
+  if   ( 0 != m_totMem ) { *m_totMem += memMB ; }
+  // Fill the plot
+  if   ( 0 != m_plot1  ) { fill ( m_plot1 , memMB , 1 , m_histo1.title() ) ; }
+  // Fill the counter for "valid" previous measurements
+  if   ( 0 <= m_prev   )
+  { 
+    const double delta = memMB - m_prev ;
+    // fill the counter 
+    if ( 0 != m_delMem ) { *m_delMem += delta  ; }
+    // fill the counter 
+    if ( 0 != m_plot2  ) { fill ( m_plot2 , delta , 1 , m_histo2.title() ) ; }
+  } 
+  
+  // set "previous" measurement 
+  m_prev = memMB    ;                                        // memory in MB
+  
   if( m_bins > m_counter ) {
     plot( m_counter+1, "Virtual mem, all entries", "Virtual memory (kB), first 'HistoSize' entries",
           0.5, m_bins+0.5, m_bins, mem );
--- LHCb/Kernel/LHCbAlgs/src/MemoryTool.h
+++ LHCb/Kernel/LHCbAlgs/src/MemoryTool.h
@@ -1,13 +1,19 @@
-// $Id: MemoryTool.h,v 1.2 2008-07-21 14:45:52 cattanem Exp $
+// $Id: MemoryTool.h,v 1.3 2009-10-08 11:18:26 ibelyaev Exp $
+// ============================================================================
 #ifndef MEMORYTOOL_H 
 #define MEMORYTOOL_H 1
-
+// ============================================================================
 // Include files
-// from Gaudi
+// ============================================================================
+//  GaudiKernel
+// ============================================================================
+#include "GaudiKernel/HistoDef.h"
+// ============================================================================
+// GaudiAlg 
+// ============================================================================
 #include "GaudiAlg/GaudiHistoTool.h"
 #include "GaudiAlg/IGenericTool.h"            // Interface
-
-
+// ============================================================================
 /** @class MemoryTool MemoryTool.h
  *  Tool to plot memory usage of the application at each call
  *
@@ -23,12 +29,48 @@ public:
 
   virtual ~MemoryTool( ); ///< Destructor
 
-  void execute(); ///< Plot the current memory usage 
+  void execute(); ///< Plot the current memory usage
+  
+  StatusCode finalize () 
+  {
+    m_totMem = 0 ;
+    m_delMem = 0 ;
+    m_plot1  = 0 ;
+    m_plot2  = 0 ;
+    return GaudiHistoTool::finalize (); 
+  }
 
 protected:
 
 private:
-  unsigned int m_counter; ///< Counter of calls to the tool
-  unsigned int m_bins;    ///< Number of bins of histogram (Property HistoSize)  
+  // ==========================================================================
+  unsigned long m_counter; ///< Counter of calls to the tool
+  unsigned int  m_bins;    ///< Number of bins of histogram (Property HistoSize)  
+  // ==========================================================================
+  /// flag to skip/reset events for memory measurements  
+  int         m_skip   ;   // flag to skip/reset events for memory measurements
+  /// the previosu measurement of virtual memory 
+  double      m_prev   ;          // the previosu measurement of virtual memory
+  // ==========================================================================
+  /// the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histo1 ;         // the histogram definition (as property) 
+  /// the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histo2 ;         // the histogram definition (as property) 
+  // ==========================================================================
+private:
+  // ==========================================================================
+  /// the counetr for total memory 
+  StatEntity* m_totMem ;                       // the counter for total memory 
+  /// the counetr for delta memory 
+  StatEntity* m_delMem ;                       // the counter for delta memory
+  /// the histogram of total memory 
+  AIDA::IHistogram1D* m_plot1 ;                // the histogram of total memory 
+  /// the histogram of delta memory 
+  AIDA::IHistogram1D* m_plot2 ;                // the histogram of delta memory 
+  // ==========================================================================
 };
+// ============================================================================
+// The END 
+// ============================================================================
 #endif // MEMORYTOOL_H
+// ============================================================================
