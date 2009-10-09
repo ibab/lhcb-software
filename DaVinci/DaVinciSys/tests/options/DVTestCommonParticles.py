6f9c869987645797488ff60542314e1ee274c3d2
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-10-09 12:36:41 +0000

    Added CommonParticles test
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@78967 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0b6918085ce764b30cbe076ace17e172cb86fc69

---
+++ DaVinci/DaVinciSys/tests/options/DVTestCommonParticles.py
@@ -0,0 +1,40 @@
+##############################################################################
+# $Id: DVTestCommonParticles.py,v 1.1 2009-10-09 12:36:41 pkoppenb Exp $
+#
+# syntax: gaudirun.py $DAVINCIMONITORSROOT/options/DVMonitorDst.py
+#
+# Author: Patrick Koppenburg <patrick.koppenburg@cern.ch>
+#
+##############################################################################
+from DaVinci.Configuration import *
+from Gaudi.Configuration import *
+##############################################################################
+#
+# CommonParticles
+# Thanks to Vanya for this trick
+#
+from CommonParticles import StandardBasic
+from CommonParticles import StandardIntermediate
+from CommonParticles.Utils import particles  # particles() method returns DoD dictionary
+List = []
+for a,b in particles().iteritems():
+    List.append(b)
+##############################################################################
+#
+# Histograms
+#
+DaVinci().HistogramFile = "DVMonitors.root"
+##############################################################################
+#
+# Most of this will be configured from Dirac
+#
+##############################################################################
+DaVinci().UserAlgorithms = List
+DaVinci().EvtMax = 1000
+DaVinci().DataType = "MC09" # Default is "DC06"
+DaVinci().Simulation = True
+DaVinci().InputType = "RDST"
+EventSelector().Input   = [
+    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/data/2009/RDST/00005288/0000/00005288_00000001_1.rdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+    
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
