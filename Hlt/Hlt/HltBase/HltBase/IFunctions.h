749c29ff5f9887edbc005696094c331b1a543505
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-10-12 20:14:39 +0000

    remove unneeded/unused typedefs
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79010 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6dd1300f8f149bc98fb1d5121c24dfe6601b6e82

--- Hlt/Hlt/HltBase/HltBase/IFunctions.h
+++ Hlt/Hlt/HltBase/HltBase/IFunctions.h
@@ -1,65 +1,53 @@
-// $Id: IFunctions.h,v 1.2 2009-10-12 18:48:04 graven Exp $
+// $Id: IFunctions.h,v 1.3 2009-10-12 20:14:39 graven Exp $
 #ifndef HLTBASE_OPER_H 
 #define HLTBASE_OPER_H 1
 
 #include <functional>
 
-/** Namespace for Hlt template base classes
+/** Template base class to define functions and filters and operations
+   *  between them
  *  
  *  @author Jose Angel Hernando Morata
  *          based on LoKi ideas by I. Belyaev
  *  @date   2006-07-18
  */
 
-namespace zen
-{// Include file
+namespace zen {
 
-  /** Template base class to define functions and filters and operations
-   *  between them
-   *  
-   *  @author Jose Angel Hernando Morata
-   *          based on LoKi ideas by I. Belyaev
-   *  @date   2006-07-18
-   */
-  
   template <class T>
   class function : public std::unary_function<const T,double> 
   {
   public:
-    typedef zen::function<T> Self;
     virtual ~function() {}
     virtual double operator() (const T& t) const = 0;
-    virtual Self* clone() const = 0;
+    virtual zen::function<T>* clone() const = 0;
   };
 
   template <class T>
   class filter : public std::unary_function<const T,bool> 
   {
   public:
-    typedef zen::filter<T> Self;
     virtual ~filter() {}
     virtual bool operator() (const T& t) const  = 0;
-    virtual Self* clone() const = 0;
+    virtual zen::filter<T>* clone() const = 0;
   };
 
   template <class T1, class T2>
   class bifunction : public std::binary_function<const T1, const T2, double> 
   {
   public:
-    typedef zen::bifunction<T1,T2> Self;
     virtual ~bifunction() {}
     virtual double operator() (const T1& t, const T2& t2) const = 0;
-    virtual Self* clone() const = 0;
+    virtual zen::bifunction<T1,T2>* clone() const = 0;
   };
 
   template <class T1, class T2>
   class bifilter : public std::binary_function<const T1, const T2, bool> 
   {
   public:
-    typedef zen::bifilter<T1,T2> BiFilter;
     virtual ~bifilter() {}
     virtual bool operator() (const T1& t, const T2& t2) const = 0;
-    virtual BiFilter* clone() const = 0;
+    virtual zen::bifilter<T1,T2>* clone() const = 0;
   };
 }
 #endif
