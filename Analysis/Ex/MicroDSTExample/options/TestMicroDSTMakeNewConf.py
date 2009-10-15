3a88c209ae3c42025a533ff8cf76cdcd63721afa
jpalac <jpalac@cern.ch>
2009-10-15 10:29:26 +0000

    Clean up.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@79083 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e0bd346521ef45d2625ae55052c8420d4ba9f1ec

--- Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
+++ Analysis/Ex/MicroDSTExample/options/TestMicroDSTMakeNewConf.py
@@ -16,30 +16,9 @@ conf.CopyMCTruth = True
 conf.CopyBTags = True
 microDST0Seq = conf.sequence()
 
-# to use the PV re-fitting, need to re-fit PVs, relate them, and pass the relations table location to the MicroDST maker. Could we put this On-Demand?
-'''
-from Configurables import PVReFitterAlg, PVRelatorAlg
-mainLocation = "Phys/" + selSequence.topAlgName()
-PVReFitter = PVReFitterAlg("PVReFitterAlg")
-PVReFitter.ParticleInputLocation = mainLocation+"/Particles"
-PVReFitter.VertexOutputLocation = mainLocation+"/RefittedVertices"
-p2ReFitPVRelationsLoc = mainLocation+"/Particle2ReFittedVertexRelations"
-PVReFitter.P2VRelationsOutputLocation = p2ReFitPVRelationsLoc
-PVReFitter.OutputLevel=4
-PVRelator = PVRelatorAlg()
-PVRelator.OutputLevel=4
-PVRelator.P2PVRelationsInputLocation = p2ReFitPVRelationsLoc
-p2pvSortedRelationsLoc = mainLocation + "/P2ReFitPVSortedRelations"
-PVRelator.P2PVRelationsOutputLocation = p2pvSortedRelationsLoc
-MySelection.Members += [PVReFitter, PVRelator]
-conf.CopyRelatedPVs = True
-conf.P2PVRelationsSuffix = "P2ReFitPVSortedRelations"
-'''
-#
-
 dv = DaVinci()
 dv.DataType = 'DC06'
-dv.EvtMax = 500
+dv.EvtMax = 495
 dv.UserAlgorithms = [microDST0Seq]
 dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
@@ -52,5 +31,3 @@ dv.Input =  [
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000011_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000013_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
-
-#print conf.name(), " sequence ", conf.seqMicroDST(), " ", conf.seqMicroDST().Members
