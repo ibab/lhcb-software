6fa54b6fae7f16ad0c8bd727c67e8b5a741aebcf
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-10-17 16:57:30 +0000

    fix unknown member in HltCosmicLines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79159 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bce8fe54e6524654793bf27243b8bad243fc4bb

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltCosmicLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/HltCosmicLines.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# $Id: HltCosmicLines.py,v 1.1 2009-09-10 15:12:35 snies Exp $
+# $Id: HltCosmicLines.py,v 1.2 2009-10-17 16:57:30 graven Exp $
 # =============================================================================
 ## @file
 #  Cosmics 
@@ -11,7 +11,7 @@
 """
 # =============================================================================
 __author__  = "Stephan Nies & Jesko Merkel " 
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.1 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.2 $"
 # =============================================================================
 
 from HltLine.HltLinesConfigurableUser import *
@@ -19,28 +19,16 @@ from HltLine.HltLinesConfigurableUser import *
 
 class HltCosmicLinesConf(HltLinesConfigurableUser):
  
-    __slots__ = { 'Prescale' : { 'Hlt1OTCosmic'  : 1.0
-                               }
+    __slots__ = { 'Prescale' : { }
                 }
     def __apply_configuration__(self):
 
-        from HltLine.HltLine import Hlt1Member as Member 
         from HltLine.HltDecodeRaw import DecodeOT
         from HltLine.HltLine import Hlt1Line as Line
-        Line('OTCosmic'  
+        from Configurables import HltCosmicsOT
+        Line('CosmicOT'  
             , prescale = self.prescale
             , postscale = self.postscale
             , L0DU = "L0_ALL"
-            , algos = [ DecodeOT, Member('CosmicsOT', 'CosmicsOT', OutputSelection = '%Decision') ]
+            , algos = [ DecodeOT, HltCosmicsOT('Hlt1CosmicOTDecision', OutputSelection = '%Decision') ]
             )
-        #from Configurables import FilterByBankType
-        #Line('Tell1Error'
-        #    , ODIN='ODIN_ALL'
-        #    , algos = [ FilterByBankType('Hlt1Tell1ErrorDecision' 
-        #                                , PassSelectedEvents = True
-        #                                , BankNames = [ ".*Error" ] 
-        #                                )
-        #              ]
-        #    , prescale = self.prescale
-        #    , postscale = self.postscale
-        #    )
