ca7c810b2fda3853216aa3e510d3c077fce0c63d
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-10-19 06:56:23 +0000

    update example options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@79216 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9e79c116c2dbf0945ec282a0476b68f357ae919

--- Moore/Hlt/Moore/options/Moore.py
+++ Moore/Hlt/Moore/options/Moore.py
@@ -6,49 +6,33 @@
 # or just
 #   ../options/Moore.py
 #
-from Gaudi.Configuration import *
-from Moore.Configuration import *
+import Gaudi.Configuration
+from Moore.Configuration import Moore
+
 
 # if you want to generate a configuration, uncomment the following lines:
 #Moore().generateConfig = True
-#Moore().configLabel = 'NO prescale'
+#Moore().configLabel = 'ODINRandom accept=1, TELL1 Error accept=0'
 
-Moore().HltType = 'Hlt1' # +Hlt2'
-Moore().ThresholdSettings = 'Effective_Nominal'
+#Moore().ThresholdSettings = 'Physics_320Vis_300L0_10Hlt1_Aug09'
+#Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_LeptonicHlt2_Jul09'
+#Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_CharmingHlt2_Jul09'
+Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_EffectiveHlt2_Jul09'
+#Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_Apr09'
+#Moore().ThresholdSettings = 'Commissioning_PassThrough'
+
 Moore().Verbose = True
+Moore().EvtMax = 10000
 
-#Moore().Simulation = False
-#Moore().DataType = '2008'
-#Moore().inputFiles= [ '/data/bfys/lhcb/data/2008/RAW/LHCb/PHYSICS_COSMICS/27804/027804_0000063303.raw' ]
 
-#Moore().Simulation = True
-#Moore().DataType = 'DC06'
-#Moore().inputFiles = [ '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-1.dst'
-#                     , '/afs/cern.ch/lhcb/group/trigger/vol1/dijkstra/Selections/MBL0-lumi2-2.dst'
-#                     , '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-3.dst'
-#                     , '/afs/cern.ch/lhcb/group/trigger/vol3/dijkstra/Selections/MBL0-lumi2-4.dst' ]
-
-#Moore().Simulation = True
-#Moore().DataType = 'DC06'
-#Moore().inputFiles = [ '/data/bfys/lhcb/MinBias-L0strip/DC06_L0_v1_lumi2_MuonHadron_40000ev_' + str(f) +'.mdf'  for f in range(1,3) ]
-#Moore().inputFiles = [ 'castor:/castor/cern.ch/user/s/snies/mdf/DC06_L0_v1_lumi2_MuonHadron_40000ev_'+str(f)+'.mdf' for f in range(1,3) ]
-#Moore().inputFiles = [ '/data/bfys/lhcb/MinBias-L0strip/MBL0-lumi2-' + str(f) +'.dst'  for f in range(1,5) ]
-
+Moore().DDDBtag   = 'MC09-20090602'
+Moore().CondDBtag = 'sim-20090402-vc-md100'
 Moore().Simulation = True
 Moore().DataType   = '2009'
-#Moore().inputFiles = [ '/data/bfys/lhcb/test/MDF/00003083/0000/00003083_%08d_1.mdf'%f  for f in range(1,4) ]
+#Moore().inputFiles = [ '/data/bfys/lhcb/MinBias-L0xHlt1strip/Sim03Reco02-Mbias_%d.mdf'%(i) for i in [ 1, 2 ]  ]
+#Moore().inputFiles = [ '/data/bfys/lhcb/data/2009/RAW/FULL/FEST/FEST/56721/056721_0000000001.raw' ]
+Moore().inputFiles = [ 'castor:/castor/cern.ch/grid/lhcb/data/2009/RAW/FULL/FEST/FEST/56721/056721_%010d.raw'%(i) for i in [1,2,3] ]
 
-#Moore().inputFiles = [ 'castor:/castor/cern.ch/grid/lhcb/test/MDF/00003083/0000/00003083_%08d_1.mdf'%f  for f in range(1,4) ]
-#Moore().inputFiles = [ 'castor:/castor/cern.ch/user/s/snies/mdf/00003083_%08d_1.mdf'%f for f in range(1,3) ]
-Moore().inputFiles = [ 'castor:/castor/cern.ch/user/d/dijkstra/Stripped-L0-MC09/Sim03Reco02-Mbias_%d.mdf'%f for f in range(1,5) ]  
-
+from Configurables import EventSelector
 EventSelector().PrintFreq = 100
-Moore().EvtMax = 1000
 
-# Moore().outputFile = '/tmp/foo.raw'
-
-# optionally, we can enable some auditors...
-# Moore().enableAuditor = [ NameAuditor() ]
-
-#and tell the world how we are configured
-print Moore()
