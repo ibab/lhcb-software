bb938b8032ec84cf08dbf2f37c060d1a5560b4ab
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-10-20 15:13:32 +0000

    v1r8
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@21998 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e3ced770b9b969dbfbe9888aa59f21ee0661edf7

--- Rec/Muon/MuonTrackAlign/src/MuonTrackAlign_dll.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: MuonTrackAlign_dll.cpp,v 1.1.1.1 2007-07-23 07:14:36 asatta Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-
-LOAD_FACTORY_ENTRIES(MuonTrackAlign)
-
