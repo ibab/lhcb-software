21f4d794fc04e8390b00ae3e6e1b735f962e3557
Julien Cogan <jucogan@cern.ch>
2009-10-20 13:20:16 +0000

    Turn the package into a linker library
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@61104 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b59039d5257982e02007ecaab0916f81dbde981f

--- LHCb/L0/L0Base/L0Base/L0AlgBase.h
+++ LHCb/L0/L0Base/L0Base/L0AlgBase.h
@@ -1,24 +1,24 @@
-// $Id: L0FromRawBase.h,v 1.1.1.1 2009-10-20 12:43:08 jucogan Exp $
-#ifndef L0FROMRAWBASE_H 
-#define L0FROMRAWBASE_H 1
+// $Id: L0AlgBase.h,v 1.1 2009-10-20 13:20:16 jucogan Exp $
+#ifndef L0ALGBASE_H 
+#define L0ALGBASE_H 1
 
 // Include files
 // from Gaudi
 #include "GaudiAlg/GaudiAlgorithm.h"
 
 
-/** @class L0FromRawBase L0FromRawBase.h
- *  Base class for the L0 banks decoding algorithms
+/** @class L0AlgBase L0AlgBase.h
+ *  
  *
  *  @author Julien Cogan
  *  @date   2009-10-20
  */
-class L0FromRawBase : public GaudiAlgorithm {
+class L0AlgBase : public GaudiAlgorithm {
 public: 
   /// Standard constructor
-  L0FromRawBase( const std::string& name, ISvcLocator* pSvcLocator );
+  L0AlgBase( const std::string& name, ISvcLocator* pSvcLocator );
 
-  virtual ~L0FromRawBase( ); ///< Destructor
+  virtual ~L0AlgBase( ); ///< Destructor
 
   virtual StatusCode initialize();    ///< Algorithm initialization
   virtual StatusCode execute   ();    ///< Algorithm execution
@@ -26,12 +26,11 @@ public:
 
 protected:
 
-  bool m_writeProcData;    ///< Flag to activate the writing of the raw banks
+  bool m_writeBanks;       ///< Flag to activate the writing of the raw banks
   bool m_writeOnTES;       ///< Flag to activate the writing of the Candidates/Report on the TES
   std::string m_l0context; ///< Suffix appended to the default location in TES
-
 
 private:
 
 };
-#endif // L0FROMRAWBASE_H
+#endif // L0ALGBASE_H
