cd35a7527d4bef12ed5626882674af00702b9a3b
Alessio Sarti <alessio.sarti@lnf.infn.it>
2009-10-23 17:26:43 +0000

    Added min ip chi2 cut also for DD selection
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@79308 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f4191fe7b86400c5c441f19fdb5b267b47985c8d

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingMuIDCalib.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingMuIDCalib.py
@@ -96,7 +96,8 @@ LambdaDDCombine = StrippingMember( CombineParticles
                                    , DaughtersCuts = { "p+"  :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))",
                                                        "pi-" :  "(P>3000*MeV) & (PT>100*MeV) & (TRCHI2DOF<2) & (ISDOWN) & (0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1))" }
                                    , CombinationCut = "(ADAMASS('Lambda0')<20*MeV)"
-                                   , MotherCut = " (500<BPVVDZ) & (2200>BPVVDZ) & (BPVDIRA>0.999999)  & ( (CHILDCUT((PT>0.4*GeV),1)) | (CHILDCUT((PT>0.4*GeV),2)) )"
+                                   , MotherCut = " (500<BPVVDZ) & (2200>BPVVDZ) & (BPVDIRA>0.999999)  & ( (CHILDCUT((PT>0.4*GeV),1)) | (CHILDCUT((PT>0.4*GeV),2)) & " +
+                                   " ((CHILDCUT((MIPCHI2DV(PRIMARY)>5),1)) & (CHILDCUT((MIPCHI2DV(PRIMARY)>5),2)))"
                                    )
 
 # Define the line
