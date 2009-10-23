38ff0a20c89b44a7c6e272b3eae789441f944665
jpalac <jpalac@cern.ch>
2009-10-23 08:54:43 +0000

    Obsolete. Superceded by CopyHltDecReports.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@79294 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 73a31f6a672326c0ef9367f1803adf91c9a41c51

--- Analysis/MicroDST/MicroDSTAlgorithm/src/CopyHltSummary.cpp
+++
@@ -1,5 +0,0 @@
-// $Id: CopyHltSummary.cpp,v 1.2 2008-08-29 07:08:56 jpalac Exp $
-// Include files 
-// local
-#include "CopyHltSummary.h"
-//-----------------------------------------------------------------------------
