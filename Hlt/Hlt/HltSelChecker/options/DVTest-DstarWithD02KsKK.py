2d617ab9ccdb8ff58a5d5cb65a1549eb44dc6d81
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-10-23 16:41:06 +0000

    Add flavour!
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79303 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 97a3c4aff6dc5f75914226a53f37483bed05c999

--- Hlt/Hlt/HltSelChecker/options/DVTest-DstarWithD02KsKK.py
+++ Hlt/Hlt/HltSelChecker/options/DVTest-DstarWithD02KsKK.py
@@ -11,7 +11,7 @@ from HltSelChecker.CheckerSeq import *
 #--------------------------------------------------------------
 CS = CheckerSeq( DV=DaVinci()
                , Signal="DstarWithD02KsKK"
-               , Decay = "{[D*(2010)+ -> (D0 -> ( KS0 -> ^pi+ ^pi- {,gamma}{,gamma} ) ^K+ ^K- {,gamma}{,gamma}) ^pi+  {,gamma}{,gamma}]cc}"
+               , Decay = "[D*(2010)+ ->  ^pi+ (D0 -> ( KS0 -> ^pi+ ^pi- {,gamma}{,gamma} ) ^K+ ^K- {,gamma}{,gamma}) {,gamma}]cc"
                # Mat Charles, https://twiki.cern.ch/twiki/bin/view/LHCb/NewCpOfflineSelected
                , Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/user/m/mcharles/128/outputdata/dstar.dst' TYP='POOL_ROOTTREE' OPT='READ'" ])
 CS.configure()
--- Hlt/Hlt/HltSelChecker/options/DVTest-Z0.py
+++ Hlt/Hlt/HltSelChecker/options/DVTest-Z0.py
@@ -1,18 +1,17 @@
 ### @file DVTest-Z0.py
  #
- #  Test file for HLT on selected  events
+ #  Test file for HLT on selected Z0 events
  #
  #  @author P. Koppenburg
- #  @date 2009-10-22
+ #  @date 2009-10-23
  #/
 from Gaudi.Configuration import *
 from Configurables import DaVinci
 from HltSelChecker.CheckerSeq import *
 #--------------------------------------------------------------
 CS = CheckerSeq( DV=DaVinci()
-               , Signal=""
-               , Decay = ""
-               # 
-               , Input = [ "DATAFILE='PFN:castor:' TYP='POOL_ROOTTREE' OPT='READ'" ])
+               , Signal="Z0"
+               , Decay = "Z0 -> ^mu+ ^mu- {,gamma}{,gamma}"
+               # James Keaveney , https://twiki.cern.ch/twiki/bin/view/LHCb/FlavourPhysics#MC09_samples_of_offline_selected
+               , Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/user/k/keaveney/MC09/Z/Z02MuMuMC09_SELECTED.dst' TYP='POOL_ROOTTREE' OPT='READ'" ])
 CS.configure()
-
