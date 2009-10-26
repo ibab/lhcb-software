c85b6b08ee23513b3af6b9d5a895fbd7f8c9d586
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-10-26 08:10:42 +0000

    update example input
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@79328 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6bb45e9815e59b9b0b3eb21a1407f3fb8e165681

--- Moore/Hlt/Moore/options/Moore.py
+++ Moore/Hlt/Moore/options/Moore.py
@@ -11,8 +11,8 @@ from Moore.Configuration import Moore
 
 
 # if you want to generate a configuration, uncomment the following lines:
-Moore().generateConfig = True
-Moore().configLabel = 'Default'
+#Moore().generateConfig = True
+#Moore().configLabel = 'Default'
 
 #Moore().ThresholdSettings = 'Commissioning_PassThrough'
 #Moore().configLabel = 'ODINRandom acc=0, TELL1Error acc=1'
@@ -21,8 +21,8 @@ Moore().configLabel = 'Default'
 #Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_LeptonicHlt2_Jul09'
 #Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_CharmingHlt2_Jul09'
 #Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_EffectiveHlt2_Jul09'
-Moore().configLabel = 'ODINRandom=0.000001, ODINNonRandom=0.000001'
 Moore().ThresholdSettings = 'Physics_10000Vis_1000L0_40Hlt1_Apr09'
+#Moore().ThresholdSettings = 'Commissioning_PassThrough'
 
 Moore().Verbose = True
 Moore().EvtMax = 10000
@@ -32,9 +32,11 @@ Moore().DDDBtag   = 'MC09-20090602'
 Moore().CondDBtag = 'sim-20090402-vc-md100'
 Moore().Simulation = True
 Moore().DataType   = '2009'
-Moore().inputFiles = [ '/data/bfys/lhcb/MinBias-L0xHlt1strip/Sim03Reco02-Mbias_%d.mdf'%(i) for i in [ 1, 2 ]  ]
+#Moore().inputFiles = [ '/data/bfys/lhcb/MinBias-L0xHlt1strip/Sim03Reco02-Mbias_%d.mdf'%(i) for i in [ 1, 2 ]  ]
+Moore().inputFiles = [ 'castor:/castor/cern.ch/user/d/dijkstra/Stripped-L0-MC09/Sim03Reco02-Mbias_%d.mdf'%f for f in range(1,5) ]  
 #Moore().inputFiles = [ '/data/bfys/lhcb/data/2009/RAW/FULL/FEST/FEST/56721/056721_0000000001.raw' ]
 #Moore().inputFiles = [ 'castor:/castor/cern.ch/grid/lhcb/data/2009/RAW/FULL/FEST/FEST/56721/056721_%010d.raw'%(i) for i in [1,2,3] ]
+
 
 from Configurables import EventSelector
 EventSelector().PrintFreq = 100
