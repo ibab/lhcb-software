2ac8b0250a5b54aafcdb84700c1cf55d59836578
Olivier Deschamps <odescham@cern.ch>
2009-10-26 10:51:26 +0000

    fix onDemand configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@40598 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 71f78557b479608b0a7c6cd09a20c79b06bd027c

--- Lbcom/Calo/CaloAssociators/python/CaloAssociators/MCTruthOnDemand.py
+++ Lbcom/Calo/CaloAssociators/python/CaloAssociators/MCTruthOnDemand.py
@@ -12,7 +12,7 @@ Configuration file to run Calorimeter Monte Carlo Truth Associations on-demand
 """
 # =============================================================================
 __author__  = "Vanya BELYAEV Ivan.Belyaev@nikhef.nl"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.2 $"
 # =============================================================================
 # nothing to be imported 
 __all__     = (
@@ -23,16 +23,27 @@ from Gaudi.Configuration   import *
 from Configurables         import ( CaloDigit2MCLinks2Table ,
                                     CaloClusterMCTruth      )
 
+from Configurables         import ( CaloZSupAlg       ,
+                                    CaloDigitsFromRaw ) 
+
 from CaloKernel.ConfUtils  import ( onDemand       ,
                                     printOnDemand  ,
                                     getAlgo        ) 
 
 # =============================================================================
-linker2table = getAlgo ( CaloDigit2MCLinks2Table       , 
-                         'CaloDigit2MCLinks2Table'     ,
+linker2table = getAlgo ( GaudiSequencer                , 
+                         'CaloDigit2MC'                ,
                          'Offline'                     ,
                          'Relations/Raw/Calo/Digits'   ,
-                         True                          ) 
+                         True                          )
+
+linker2table.Members += [CaloZSupAlg       ( "EcalZSup"        ) ,
+                         CaloZSupAlg       ( "HcalZSup"        ) ,
+                         CaloDigitsFromRaw ( "PrsFromRaw"      ) ,
+                         CaloDigitsFromRaw ( "SpdFromRaw"      ) ,
+                         CaloDigit2MCLinks2Table( "CaloDigit2MCLinks" )
+                         ]
+
 
 for context in ( 'Offline' ,
                  'HLT'     ) : 
