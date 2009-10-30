fe30b9066c70f256bda821171e4df5763ace8d57
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2009-10-30 14:18:35 +0000

    Debug thresholds to run only the Hlt1 hadron lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79524 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 36f7512a5e0eb362554d43568480a4b116077ea6

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Debug_320Vis_300L0_Hadron_Hlt1_Oct09.py
@@ -0,0 +1,70 @@
+class Debug_320Vis_300L0_Hadron_Hlt1_Oct09 :
+    """
+    Threshold settings for testing the Hlt1 hadron lines, 2010 conditions
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author V. Gligorov
+    @date 2009-10-30
+    """
+    
+    __all__ = ( 'ActiveHlt1Lines', 'ActiveHlt2Lines', 'Thresholds','L0TCK' )
+
+    def verifyType(self,ref) :
+        # verify self.ActiveLines is still consistent with
+        # our types self.ActiveHlt2Lines and self.ActiveHlt1Lines...
+        # so we can force that classes which inherit from us
+        # and overrule either ActiveHlt.Lines also overrule
+        # HltType...
+        if self.ActiveHlt1Lines() != ref.ActiveHlt1Lines(self)  or self.ActiveHlt2Lines() != ref.ActiveHlt2Lines(self)  :
+            raise RuntimeError('Must update HltType when modifying ActiveHlt.Lines()')
+
+    
+    def L0TCK(self) :
+        return '0xFF68'
+
+    def HltType(self) :
+        self.verifyType( VavaDebug )
+        return          'VavaDebug'
+
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        from Hlt1Lines.HltCommissioningLines  import HltCommissioningLinesConf
+        from Hlt1Lines.HltMuonLines           import HltMuonLinesConf
+        from Hlt1Lines.HltHadronLines         import HltHadronLinesConf
+        from Hlt1Lines.HltElectronLines       import HltElectronLinesConf
+        from Hlt1Lines.HltPhotonLines         import HltPhotonLinesConf
+        from Hlt1Lines.HltExpressLines        import HltExpressLinesConf
+        from Hlt1Lines.HltVeloLines           import HltVeloLinesConf
+        from Hlt2Lines.Hlt2CommissioningLines  import Hlt2CommissioningLinesConf
+
+        return { HltHadronLinesConf : {  'SingleHadron_PTCut' : 4800
+                                         , 'HadSingle_IPCut' : 0.06 
+                                         , 'HadMain_PTCut' : 250
+                                         , 'HadCompanion_DZCut' : 1.83
+                                         , 'HadDi_IPCut' : 0.05
+                                         , 'HadCompanion_PTCut' : 750
+                                         , 'HadCompanion_PointingCut' : 0.39
+                                        }
+                 , HltExpressLinesConf : {  'Prescale' : { '.*' : 0.0  } }
+
+                 }
+    
+    def ActiveHlt2Lines(self) :
+        """
+        Returns a list of active lines
+        """
+        return [ ]
+
+        
+    def ActiveHlt1Lines(self) :
+        """
+        Returns a list of active lines
+        """
+        return [ 'Hlt1SingleHadron'
+                 ,'Hlt1DiHadron'
+               ]
+    
+    
