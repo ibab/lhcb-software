df722d707a6714577b142893541b6b0d104b89ad
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-10-30 09:52:09 +0000

    suppress warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@40617 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 994c77e9aee55ecc254a242dffcaaacaf695b423

--- Rec/Calo/CaloPIDs/src/EcalEnergyForTrack.cpp
+++ Rec/Calo/CaloPIDs/src/EcalEnergyForTrack.cpp
@@ -1,20 +1,6 @@
-// $Id: EcalEnergyForTrack.cpp,v 1.5 2009-03-11 19:05:40 vegorych Exp $
+// $Id: EcalEnergyForTrack.cpp,v 1.6 2009-10-30 09:52:09 ibelyaev Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.5 $
-// ============================================================================
-// $Log: not supported by cvs2svn $
-// Revision 1.4  2008/05/13 12:25:51  odescham
-// restore _setProperty()
-//
-// Revision 1.3  2008/01/24 10:22:42  vegorych
-// _setProperty was removed, AddNeigbours warning was solved
-//
-// Revision 1.2  2007/08/24 21:25:18  odescham
-// fix uncheck. StatusCodes
-//
-// Revision 1.1  2006/06/18 18:35:28  ibelyaev
-//  the firstcommmit for DC06 branch
-//
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.6 $
 // ============================================================================
 // local
 // ============================================================================
@@ -28,36 +14,40 @@
 // ============================================================================
 class EcalEnergyForTrack : public CaloEnergyForTrack 
 {
+  // ==========================================================================
   // friend factory for istantiation
   friend class ToolFactory<EcalEnergyForTrack>;
+  // ==========================================================================
 protected:
+  // ==========================================================================
   EcalEnergyForTrack
   ( const std::string& type   , // ? 
     const std::string& name   , 
     const IInterface*  parent ) 
     : CaloEnergyForTrack ( type , name , parent ) 
   {
-    _setProperty ( "DataAddress"   , LHCb::CaloDigitLocation::Ecal ) ;
-    _setProperty ( "MorePlanes"    , "3"    ) ; /// 3 additional planes 
-    _setProperty ( "AddNeigbours"  , "false" ) ; 
-    _setProperty ( "Tolerance"     , "5" ) ; /// 5 * Gaudi::Units::mm 
-    _setProperty ( "Calorimeter"   , DeCalorimeterLocation::Ecal   ) ;
-  } ;
+    setProperty ( "DataAddress"   , LHCb::CaloDigitLocation::Ecal ) ;
+    setProperty ( "MorePlanes"    , 3    ) ; /// 3 additional planes 
+    setProperty ( "AddNeigbours"  , 0    ) ; 
+    setProperty ( "Tolerance"     , 5 * Gaudi::Units::mm  ) ;
+    setProperty ( "Calorimeter"   , DeCalorimeterLocation::Ecal   ) ;
+  } 
   virtual ~EcalEnergyForTrack() {}
+  // ==========================================================================
 private:
+  // ==========================================================================
   // default constructor is disabled 
   EcalEnergyForTrack() ;
   // copy constructor is disabled 
   EcalEnergyForTrack           ( const  EcalEnergyForTrack& ) ;
   // assignement is disabled 
   EcalEnergyForTrack& operator=( const EcalEnergyForTrack& ) ;
+  // ==========================================================================
 } ;
 // ============================================================================
-/// Factory
+// Factory
 // ============================================================================
-DECLARE_TOOL_FACTORY(EcalEnergyForTrack) ;
-// ============================================================================
-
+DECLARE_TOOL_FACTORY(EcalEnergyForTrack) 
 // ============================================================================
 // The END 
 // ============================================================================
--- Rec/Calo/CaloPIDs/src/HcalEnergyForTrack.cpp
+++ Rec/Calo/CaloPIDs/src/HcalEnergyForTrack.cpp
@@ -1,8 +1,11 @@
-// $Id: HcalEnergyForTrack.cpp,v 1.5 2009-03-11 19:05:40 vegorych Exp $
+// $Id: HcalEnergyForTrack.cpp,v 1.6 2009-10-30 09:52:09 ibelyaev Exp $
 // ============================================================================
-// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.5 $
+// CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.6 $
 // ============================================================================
 // $Log: not supported by cvs2svn $
+// Revision 1.5  2009/03/11 19:05:40  vegorych
+// new PIDs PDF for DC09
+//
 // Revision 1.4  2008/05/13 12:25:51  odescham
 // restore _setProperty()
 //
@@ -28,36 +31,42 @@
 // ============================================================================
 class HcalEnergyForTrack : public CaloEnergyForTrack 
 {
-  // friend factory for istantiation
+  // ==========================================================================
+  /// friend factory for istantiation
   friend class ToolFactory<HcalEnergyForTrack>;
+  // ==========================================================================
 protected:
+  // ==========================================================================
   HcalEnergyForTrack
   ( const std::string& type   , // ? 
     const std::string& name   , 
     const IInterface*  parent ) 
     : CaloEnergyForTrack ( type , name , parent ) 
   {
-    _setProperty ( "DataAddress"   , LHCb::CaloDigitLocation::Hcal ) ;
-    _setProperty ( "MorePlanes"    , "5"    ) ; ///  3 additional planes 
-    _setProperty ( "AddNeigbours"  , "false" ) ;  
-    _setProperty ( "Tolerance"     , "15"   ) ; /// 15 * Gaudi::Units::mm  
-    _setProperty ( "Calorimeter"   , DeCalorimeterLocation::Hcal   ) ;
-  } ;
+    setProperty ( "DataAddress"    , LHCb::CaloDigitLocation::Hcal ).ignore() ;
+    setProperty ( "Calorimeter"    , DeCalorimeterLocation::Hcal   ).ignore() ;
+    //
+    setProperty ( "MorePlanes"     , 5                             ).ignore() ; 
+    setProperty ( "AddNeigbours"   , 0                             ).ignore() ;  
+    setProperty ( "Tolerance"      , 15.0 * Gaudi::Units::mm       ).ignore() ;
+  }
+  // ==========================================================================
   virtual ~HcalEnergyForTrack() {}
+  // ==========================================================================
 private:
+  // ==========================================================================
   // default constructor is disabled 
   HcalEnergyForTrack() ;
   // copy constructor is disabled 
   HcalEnergyForTrack           ( const  HcalEnergyForTrack& ) ;
   // assignement is disabled 
   HcalEnergyForTrack& operator=( const HcalEnergyForTrack& ) ;
+  // ==========================================================================
 } ;
 // ============================================================================
-/// Factory
+// Factory
 // ============================================================================
-DECLARE_TOOL_FACTORY(HcalEnergyForTrack) ;
-// ============================================================================
-
+DECLARE_TOOL_FACTORY(HcalEnergyForTrack) 
 // ============================================================================
 // The END 
 // ============================================================================
