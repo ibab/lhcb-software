c6b787f45c91fb53160a9fe268bcceea7d1456d1
powell <powell@cern.ch>
2009-10-30 17:54:41 +0000

    python/RichPIDQC/DstarToDzeroPi.py updated to use CombineParticles
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@11690 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3c0c1250c3633c8f5188b9480158cafb6fc5388e

--- Analysis/Rich/RichPIDQC/python/RichPIDQC/DstarToDzeroPi.py
+++ Analysis/Rich/RichPIDQC/python/RichPIDQC/DstarToDzeroPi.py
@@ -4,13 +4,11 @@
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
 #  @date   10/02/2009
 
-__version__ = "$Id: DstarToDzeroPi.py,v 1.4 2009-10-28 10:17:57 jpalac Exp $"
+__version__ = "$Id: DstarToDzeroPi.py,v 1.5 2009-10-30 17:54:36 powell Exp $"
 __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from LHCbKernel.Configuration import *
-#from Configurables import RichDstarToD0PiSelConf
-
-                
+              
 ## @class DstarToDzeroPiConf
 #  Configurable for RICH D* -> D0(Kpi) pi PID monitoring
 #  @author Chris Jones  (Christopher.Rob.Jones@cern.ch)
@@ -19,7 +17,7 @@ from LHCbKernel.Configuration import *
 class DstarToDzeroPiConf(LHCbConfigurableUser) :
 
     ## Possible used Configurables
-    __used_configurables__ = [ 'RichDstarToD0PiSelConf' ]
+    __used_configurables__ = [ ]
     
     ## Steering options
     __slots__ = {
@@ -30,25 +28,62 @@ class DstarToDzeroPiConf(LHCbConfigurableUser) :
 
     ## Configure Ds -> Phi Pi selection
     def __apply_configuration__(self) :
-        from DstCalibSel.Configuration import RichDstarToD0PiSelConf
+
+        from Configurables import ( GaudiSequencer,
+                                    CombineParticles, FilterDesktop )
+
         if not self.isPropertySet("Sequencer") :
             raise RuntimeError("ERROR : Sequence not set")
         seq = self.getProp("Sequencer")
-  
-        # The selection algs
-        from Configurables import GaudiSequencer
-        name = RichDstarToD0PiSelConf().selName()
-        selAlgs = GaudiSequencer(name+"SelSeq")
-        selAlgs.MeasureTime = True
-        RichDstarToD0PiSelConf().setProp("Sequencer",selAlgs)
-        seq.Members += [ selAlgs ]
+      
+        # Filter Pi/K Tracks
+        trackfilterName = "RichDStSelTrackFilter"
+        trackfilter = FilterDesktop(trackfilterName)
+        trackfilter.InputLocations = [ "StdNoPIDsPions", "StdNoPIDsKaons" ]
+        trackfilter.Code = "(PT > 0.4*GeV) & (P > 2.0*GeV) & (MIPCHI2DV(PRIMARY) > 6) & (TRCHI2DOF < 10)"
+
+        # Filter Pi Tracks
+        pionfilterName = "RichDStSelPiFilter"
+        pionfilter = FilterDesktop(pionfilterName)
+        pionfilter.InputLocations = [ "StdNoPIDsPions" ]
+        pionfilter.Code = "(PT > 110*MeV) & (MIPCHI2DV(PRIMARY) > 2)"
+
+        # Make the D0
+        d02kpiName = "RichD0ToKPiSel"
+        d02kpi = CombineParticles(d02kpiName)
+        d02kpi.InputLocations = [ trackfilterName ]
+        d02kpi.DecayDescriptor = "[ D0 -> K- pi+ ]cc"
+        d02kpi.CombinationCut = "(ADAMASS('D0') < 100*MeV)"
+        d02kpi.MotherCut = "(ADMASS('D0') < 50.0*MeV)"\
+                           "& (PT > 1.00*GeV)"\
+                           "& (VFASPF(VCHI2/VDOF)< 10.0)"\
+                           "& (BPVDIRA > 0.9999)"\
+                           "& (BPVVDCHI2 > 12)"
+
+        # Make the D*
+        dstar2d0piName = "RichDstarToD0PiSel"
+        dstar2d0pi = CombineParticles(dstar2d0piName)
+        dstar2d0pi.InputLocations = [ d02kpiName, pionfilterName ]
+        dstar2d0pi.DecayDescriptor = "[ D*(2010)+ -> D0 pi+ ]cc"
+        dstar2d0pi.CombinationCut = "(ADAMASS('D*(2010)+') < 100*MeV)"
+        dstar2d0pi.MotherCut = "(ADMASS('D*(2010)+') < 50.0*MeV)"\
+                               "& (PT > 2.2*GeV)"\
+                               "& (VFASPF(VCHI2/VDOF)< 15.0)"\
+                               "& (M-MAXTREE('D0'==ABSID,M)<155.5)"
+
+        # Add selection algs to the sequence
+        seq.Members += [trackfilter,pionfilter,d02kpi,dstar2d0pi]
 
         # Particle Monitoring plots
-        from Configurables import ( ParticleMonitor )
-        plotter =  ParticleMonitor(name+"Plots")
-        plotter.InputLocations = [ "Phys/"+name ]
-        plotter.PeakCut     = "(ADMASS('D_s+')<100*MeV)" # CRJ : Guess
-        plotter.SideBandCut = "(ADMASS('D_s+')>100*MeV)" # CRJ : Guess
+        from Configurables import ( ParticleMonitor, PhysDesktop )
+        plotter =  ParticleMonitor(dstar2d0piName+"Plots")
+        plotter.InputLocations = [ "Phys/"+dstar2d0piName ]
+        plotter.PeakCut     = "(M-MAXTREE('D0'==ABSID,M)<147.43) "\
+                              "& (M-MAXTREE('D0'==ABSID,M)>143.43) "\
+                              "& (INTREE((ABSID=='D0') & (ADMASS('D0') < 15*MeV) ))"
+        plotter.SideBandCut = "(M-MAXTREE('D0'==ABSID,M)>147.43) "\
+                              "& (M-MAXTREE('D0'==ABSID,M)<143.43) "\
+                              "& (INTREE((ABSID=='D0') & (ADMASS('D0') > 15*MeV) ))"
         plotter.PlotTools = [ "MassPlotTool","MomentumPlotTool",
                               "CombinedPidPlotTool",
                               "RichPlotTool","CaloPlotTool","MuonPlotTool" ]
@@ -58,8 +93,8 @@ class DstarToDzeroPiConf(LHCbConfigurableUser) :
         if self.getProp("MCChecks") :
 
             from Configurables import ParticleEffPurMoni
-            mcPerf = ParticleEffPurMoni(name+"MCPerf")
-            mcPerf.InputLocations = ["Phys/"+name]
+            mcPerf = ParticleEffPurMoni(dstar2d0piName+"MCPerf")
+            mcPerf.InputLocations = ["Phys/"+dstar2d0piName]
             seq.Members += [mcPerf]
     
         # Ntuple ?
