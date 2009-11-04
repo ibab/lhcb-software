85f61c1e80e1c0919642230ab06ebf487801434d
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-11-04 15:58:13 +0000

    - fixed OTBankDecoder for cosmics in OT monitoring algos - added track-calo match algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@19493 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a48bbef86098cd8fcba74ea107a4042e830f4aca

--- Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
+++ Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
@@ -1,6 +1,6 @@
 from Configurables import (GaudiSequencer, TrackMonitor, TrackVertexMonitor,
                            TrackFitMatchMonitor,TrackV0Monitor,TrackDiMuonMonitor,
-                           OTTrackMonitor, OTHitEfficiencyMonitor)
+                           OTTrackMonitor, OTHitEfficiencyMonitor,TrackCaloMatchMonitor)
 
 def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
                                    HistoPrint = False ):
@@ -10,12 +10,23 @@ def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
     seq.Members.append( TrackFitMatchMonitor(HistoPrint=HistoPrint) )
     seq.Members.append( TrackV0Monitor(HistoPrint=HistoPrint) )
     seq.Members.append( TrackDiMuonMonitor(HistoPrint=HistoPrint) )
-    return seq
+    from Configurables import RecSysConf
+    if "CALO" in RecSysConf().RecoSequence:
+        seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", HistoPrint=HistoPrint) )
+        #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
+        seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd', HistoPrint=HistoPrint))
+        seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs', HistoPrint=HistoPrint))
 
+    return seq
+    
 def ConfiguredOTMonitorSequence(Name = "MoniOTSeq",
                                 HistoPrint = False ):
     seq = GaudiSequencer(Name)
     seq.Members.append( OTTrackMonitor(HistoPrint=HistoPrint) )
     seq.Members.append( OTHitEfficiencyMonitor(HistoPrint=HistoPrint) )
+    from Configurables import TrackSys
+    if TrackSys().cosmics():
+        OTTrackMonitor().RawBankDecoder = 'OTMultiBXRawBankDecoder'
+        OTHitEfficiencyMonitor().RawBankDecoder = 'OTMultiBXRawBankDecoder'
     return seq
 
