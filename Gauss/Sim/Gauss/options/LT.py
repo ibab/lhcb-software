80216d1c24d16f02b1026cea1869091a0f36b569
Silvia Miglioranzi <silvia.miglioranzi@cern.ch>
2009-11-05 18:27:57 +0000

    added option to lower the kinetic cuts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94680 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 64143bd2f34570d08fe63316018471046903ba1f

---
+++ Gauss/Sim/Gauss/options/LT.py
@@ -0,0 +1,20 @@
+##
+##  File containing options to lower kinetic energies cuts
+##  Options provided by M.Needham
+##
+
+from Gaudi.Configuration import *
+from GaudiKernel import SystemOfUnits
+
+from Configurables import ( GiGa, GiGaRunActionSequence, TrCutsRunAction )
+giga = GiGa()
+giga.addTool( GiGaRunActionSequence("RunSeq") , name="RunSeq" )
+giga.RunSeq.addTool( TrCutsRunAction("TrCuts") , name = "TrCuts" )
+giga.RunSeq.TrCuts.MuonTrCut     = 1.0*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.pKpiCut       = 1.0*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.NeutrinoTrCut = 0.0*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.NeutronTrCut  = 0.0*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.GammaTrCut    = 0.1*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.ElectronTrCut = 0.1*SystemOfUnits.MeV;
+giga.RunSeq.TrCuts.OtherTrCut    = 0.0*SystemOfUnits.MeV;
+
