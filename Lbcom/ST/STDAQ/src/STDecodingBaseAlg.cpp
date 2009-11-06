3b35712fa761561998606422bda73412442915df
Matthew David Needham <mneedham@cern.ch>
2009-11-06 12:32:31 +0000

    suppress warnings about lost banks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@28002 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 55295ed2930dee9ceb5ec4264e75fc1c6437e736

--- Lbcom/ST/STDAQ/src/STDecodingBaseAlg.cpp
+++ Lbcom/ST/STDAQ/src/STDecodingBaseAlg.cpp
@@ -1,4 +1,4 @@
-// $Id: STDecodingBaseAlg.cpp,v 1.28 2009-02-27 08:27:03 mneedham Exp $
+// $Id: STDecodingBaseAlg.cpp,v 1.29 2009-11-06 12:32:31 mneedham Exp $
 
 #include <algorithm>
 
@@ -227,7 +227,7 @@ std::vector<unsigned int> STDecodingBaseAlg::missingInAction(const std::vector<R
         missing.push_back((unsigned int)testID);
         std::string lostBank = "lost bank "+
 	 boost::lexical_cast<std::string>(testID);  
-        Warning(lostBank, StatusCode::SUCCESS);
+        Warning(lostBank, StatusCode::SUCCESS,0);
       }
     } // iBoard
   }
