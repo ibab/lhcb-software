43a906403c221fb6cdaa2520d3de25538e0f7b2f
jpalac <jpalac@cern.ch>
2009-11-06 16:31:09 +0000

    . Re-written with selection "framework" classes.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@79629 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 18a437ef84dff282f9270609a30771cbcd523d3c

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingJpsiInc.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingJpsiInc.py
@@ -5,28 +5,44 @@
 # @date 2009-Sept-1
 # 
 ## #####################################################################
+
+name = "JpsiInc"
+
+__all__ = ('name', 'Jpsi', 'sequence')
+
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer, CombineParticles, FilterDesktop
 from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-
+from PhysSelPython.Wrappers import DataOnDemand, Selection, SelectionSequence
 
 # Create Jpsi -> mumu candidates out of std loose muons
 ## ############################################################
-mucut = '(PT>500*MeV) & (TRCHI2DOF<3) & (ISLONG) '
-JpsiInclusive = StrippingMember( CombineParticles
-                                 , 'Combine'
-                                 , InputLocations = [ 'StdLooseMuons' ]
-                                 , DecayDescriptor = 'J/psi(1S) -> mu+ mu-'
-                                 , DaughtersCuts = { 'mu+' : mucut , 
-                                                     'mu-' : mucut }
-                                 , CombinationCut = " (AM>2900*MeV) "
-                                 , MotherCut = "(VFASPF(VCHI2/VDOF)<20) "
-                                 )
+_muons =  DataOnDemand('stdLooseMuons', Location = 'Phys/StdLooseMuons')
 
+mucut = '(PT>500*MeV) & (TRCHI2DOF<3) & (ISLONG) '
+
+_Jpsi = CombineParticles(name,
+                         DecayDescriptor = 'J/psi(1S) -> mu+ mu-',
+                         DaughtersCuts = { 'mu+' : mucut , 
+                                             'mu-' : mucut },
+                         CombinationCut = " (AM>2900*MeV) ",
+                         MotherCut = "(VFASPF(VCHI2/VDOF)<20) ",
+                         WriteP2PVRelations = False
+                         )
+                         
+Jpsi = Selection( "Sel"+name,
+                  Algorithm = _Jpsi,
+                  RequiredSelections = [_muons]
+                  )
+
+# build the SelectionSequence
+sequence = SelectionSequence("Seq"+name,
+                             TopSelection = Jpsi
+                             )
 # Define the line
 ## ############################################################
 line = StrippingLine('JpsiInclusive'
                            , prescale = 1.
-                           , algos = [ JpsiInclusive ]
+                           , algos = [ sequence ]
                            )
 
