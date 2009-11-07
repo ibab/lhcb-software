f6ba92d55542a125b844f2d74157af39bec54f81
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2009-11-07 11:45:35 +0000

    ! 2009-11-07 - Chris Jones  - Add more const'ness to StandardPacker.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48543 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a76dd5263b2551a0694a2b55f0816a1d406cb5e0

--- LHCb/Kernel/LHCbKernel/Kernel/StandardPacker.h
+++ LHCb/Kernel/LHCbKernel/Kernel/StandardPacker.h
@@ -1,4 +1,4 @@
-// $Id: StandardPacker.h,v 1.7 2009-10-22 09:17:34 jonrob Exp $
+// $Id: StandardPacker.h,v 1.8 2009-11-07 11:45:35 jonrob Exp $
 #ifndef KERNEL_STANDARDPACKER_H
 #define KERNEL_STANDARDPACKER_H 1
 
@@ -32,32 +32,32 @@ public:
   ~StandardPacker( ) {}; ///< Destructor
 
   /** returns an int for a double energy */
-  int energy( double e ) const {
+  int energy( const double e ) const {
     return packDouble( e * Packer::ENERGY_SCALE );
   }
 
   /** returns an int for a double position */
-  int position( double x ) const {
+  int position( const double x ) const {
     return packDouble( x * Packer::POSITION_SCALE );
   }
 
   /** returns an int for a double slope */
-  int slope( double x ) const {
+  int slope( const double x ) const {
     return packDouble( x * Packer::SLOPE_SCALE );
   }
 
   /** returns an short int for a double fraction, for covariance matrix */
-  short int fraction( double f ) const {
+  short int fraction( const double f ) const {
     return shortPackDouble( f * Packer::FRACTION_SCALE );
   }
 
   /** returns an int for a double time (TOF) value */
-  int time( double x ) const {
+  int time( const double x ) const {
     return packDouble( x * Packer::TIME_SCALE );
   }
 
   /** returns an int containing the float value of the double */
-  int fltPacked( double x  ) const {
+  int fltPacked( const double x  ) const {
     union fltInt { int i; float f; } convert;
     convert.f = (float)x;
     return convert.i;
@@ -68,7 +68,8 @@ public:
    *  @arg  parent : Pointer to the parent container of the SmartRef, method ->parent()
    *  @arg  key    : returned by the method .linkID() of the SmartRef
    */
-  int reference( DataObject* out, const DataObject* parent, int key ) const {
+  int reference( DataObject* out, const DataObject* parent, const int key ) const 
+  {
     LinkManager::Link* myLink = out->linkMgr()->link( parent );
     if ( NULL == myLink ) {
       out->linkMgr()->addLink( parent->registry()->identifier(),
@@ -81,29 +82,32 @@ public:
     return key + myLinkID;
   }
 
-  void hintAndKey( int data, DataObject* source, DataObject* target, int& hint, int& key ) const {
-    int indx = data >> 28;
+  void hintAndKey( const int data, 
+                   const DataObject* source, 
+                   DataObject* target, 
+                   int& hint, int& key ) const {
+    const int indx = data >> 28;
     key = data & 0x0FFFFFFF;
     hint = target->linkMgr()->addLink( source->linkMgr()->link( indx )->path(), 0 );
   }
 
   /** returns the energy as double from the int value */
-  double energy( int k )         const { return double(k) / Packer::ENERGY_SCALE; }
+  double energy( const int k )         const { return double(k) / Packer::ENERGY_SCALE; }
 
   /** returns the position as double from the int value */
-  double position( int k )       const { return double(k) / Packer::POSITION_SCALE; }
+  double position( const int k )       const { return double(k) / Packer::POSITION_SCALE; }
 
   /** returns the slope as double from the int value */
-  double slope( int k )          const { return double(k) / Packer::SLOPE_SCALE; }
+  double slope( const int k )          const { return double(k) / Packer::SLOPE_SCALE; }
 
   /** returns the fraction as double from the short int value */
-  double fraction( short int k ) const { return double(k) / Packer::FRACTION_SCALE; }
+  double fraction( const short int k ) const { return double(k) / Packer::FRACTION_SCALE; }
 
   /** returns the time as double from the int value */
-  double time( int k )           const { return double(k) / Packer::TIME_SCALE; }
+  double time( const int k )           const { return double(k) / Packer::TIME_SCALE; }
 
   /** returns an double from a int containing in fact a float */
-  double fltPacked( int k  ) const {
+  double fltPacked( const int k  ) const {
     union fltInt { int i; float f; } convert;
     convert.i = k;
     return double(convert.f);
