8881881abe2e9aeedc2ed3277dbf6de704908ea3
Silvia Miglioranzi <silvia.miglioranzi@cern.ch>
2009-11-07 11:55:57 +0000

    added option to run with latest geometry conditions
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94690 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b4ce6dddb5f3e68999140c1690632d817f157c8d

---
+++ Gauss/Sim/Gauss/options/Gauss-DEV.py
@@ -0,0 +1,18 @@
+##############################################################################
+# File for running Gauss with postMC09 configuration
+# (3.5 TeV beams, nu=1, no spill-over)
+#
+# Syntax is:
+#   gaudirun.py Gauss-DEV.py <someInputJobConfiguration>.py
+##############################################################################
+
+from Gaudi.Configuration import *
+importOptions("$APPCONFIGOPTS/Gauss/MC09-b3,5TeV-md100-nu1.py")
+
+##############################################################################
+# latest database tags 
+##############################################################################
+from Configurables import LHCbApp
+LHCbApp().DDDBtag   = "head-20091007"
+LHCbApp().CondDBtag = "sim-20090928-vc-md100"
+
