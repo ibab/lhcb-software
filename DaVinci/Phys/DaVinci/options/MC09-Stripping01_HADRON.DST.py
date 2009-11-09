2302fcfe2cd0c028df2911134afa7c518481d293
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-11-09 10:20:58 +0000

    new test files
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@79638 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bd8db0f6af4de5e8692938f92bee4c79c719409f

---
+++ DaVinci/Phys/DaVinci/options/MC09-Stripping01_HADRON.DST.py
@@ -0,0 +1,12 @@
+#-- GAUDI jobOptions generated on Mon Nov  9 11:12:48 2009
+#-- Contains event types : 
+#--   30000000 - 5 files - 12051 events - 1.19 GBytes
+
+from Gaudi.Configuration import * 
+
+EventSelector().Input   = [
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/HADRON.DST/00005437/0000/00005437_00000002_1.hadron.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/HADRON.DST/00005437/0000/00005437_00000003_1.hadron.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/HADRON.DST/00005437/0000/00005437_00000004_1.hadron.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/HADRON.DST/00005437/0000/00005437_00000006_1.hadron.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/HADRON.DST/00005437/0000/00005437_00000007_1.hadron.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
---
+++ DaVinci/Phys/DaVinci/options/MC09-Stripping01_JPSI.DST.py
@@ -0,0 +1,11 @@
+#-- GAUDI jobOptions generated on Mon Nov  9 11:13:22 2009
+#-- Contains event types : 
+#--   30000000 - 4 files - 5256 events - 0.38 GBytes
+
+from Gaudi.Configuration import * 
+
+EventSelector().Input   = [
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/JPSI.DST/00005438/0000/00005438_00000001_1.jpsi.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/JPSI.DST/00005438/0000/00005438_00000002_1.jpsi.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/JPSI.DST/00005438/0000/00005438_00000004_1.jpsi.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/MC09/JPSI.DST/00005438/0000/00005438_00000005_1.jpsi.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
