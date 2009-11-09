0f34c9b413011d5d6a0912349c8dd796f04e71d8
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-11-09 15:49:32 +0000

    3D PV in HLT
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@65437 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: be0b6512000a735dc2cc07c80728df15ac712c94

--- Phys/Phys/DaVinciTools/src/OnOfflineTool.cpp
+++ Phys/Phys/DaVinciTools/src/OnOfflineTool.cpp
@@ -1,4 +1,4 @@
-// $Id: OnOfflineTool.cpp,v 1.21 2009-08-13 10:48:16 ibelyaev Exp $
+// $Id: OnOfflineTool.cpp,v 1.22 2009-11-09 15:49:32 pkoppenb Exp $
 // Include files 
 
 // from Gaudi
@@ -28,7 +28,7 @@ OnOfflineTool::OnOfflineTool( const std::string& type,
                               const std::string& name,
                               const IInterface* parent )
   : GaudiTool ( type, name , parent )
-  , m_onlinePVLocation("Hlt/Vertex/PV2D")
+  , m_onlinePVLocation(LHCb::RecVertexLocation::Velo3D)
   , m_offlinePVLocation(LHCb::RecVertexLocation::Primary)
   , m_online(false)
   , m_offlineDistTool("LoKi::DistanceCalculator")
