539029f7d0857016712a0ed27a1d5ccf304a4555
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-11-11 17:23:42 +0000

    fix typo in previous commit
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5570 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fe3330b93933c48bb3ea1d757dac44533ac8ad95

--- LHCb/GaudiConf/python/GaudiConf/DstConf.py
+++ LHCb/GaudiConf/python/GaudiConf/DstConf.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for LHCb applications
 """
-__version__ = "$Id: DstConf.py,v 1.31 2009-11-11 16:58:24 cattanem Exp $"
+__version__ = "$Id: DstConf.py,v 1.32 2009-11-11 17:23:42 cattanem Exp $"
 __author__  = "Marco Cattaneo <Marco.Cattaneo@cern.ch>"
 
 __all__ = [
@@ -87,7 +87,7 @@ class DstConf(LHCbConfigurableUser):
         else:
             recDir = "pRec"
             if sType != "None":
-                DigiConf().setProp("EnablePack", False)
+                DigiConf().setProp("EnablePack", True)
                 SimConf().setProp("EnablePack", DigiConf().getProp("EnablePack") )
             if not hasattr( self, "PackSequencer" ):
                 raise TypeError( "Packing requested but PackSequencer not defined" )
