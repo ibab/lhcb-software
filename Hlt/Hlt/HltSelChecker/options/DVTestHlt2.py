516eb93c2a03df8cb0f1e2a6d56a764bf8f733e3
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-11-11 10:15:23 +0000

    no additions; lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79661 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 738fca52b2085e0a297f358ddf0f07bacae018ee

--- Hlt/Hlt/HltSelChecker/options/DVTestHlt2.py
+++ Hlt/Hlt/HltSelChecker/options/DVTestHlt2.py
@@ -1,4 +1,4 @@
-### $Id: DVTestHlt2.py,v 1.3 2009-11-11 10:15:09 pkoppenb Exp $
+### $Id: DVTestHlt2.py,v 1.4 2009-11-11 10:15:23 pkoppenb Exp $
 #
 #   Hlt 2 selections test job
 #
@@ -42,7 +42,7 @@ DaVinci().DataType = "MC09"                    # Default is "DC06"
 DaVinci().Simulation   = True                  # It is MC
 DaVinci().MoniSequence = [ moni ]              # Declare moni sequence
 DaVinci().Hlt = True                # Both Hlt levels
-DaVinci().HltThresholdSettings = 'Physics_320Vis_300L0_10Hlt1_Aug09'
+DaVinci().HltThresholdSettings = 'Physics_320Vis_300L0_10Hlt1_Hlt2_Oct09'
 # DaVinci().Hlt2Requires = 'L0'                  # Ignore Hlt1 in 2
 DaVinci().ReplaceL0BanksWithEmulated = False   # Redo L0
 DaVinci().HistogramFile = "DVTestHlt.root"     # Histogram file
