922a3897f1ed673a6f5e5f5748f68d5005097752
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-11-13 08:07:45 +0000

    no partial matches
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79701 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 92036b7e946a5b788df2fb7a700cf7cf8c523f25

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1VeloLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1VeloLines.py
@@ -7,7 +7,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.2 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.3 $"
 # =============================================================================
 
 #
@@ -16,7 +16,7 @@ __version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.2 $"
 from HltLine.HltLinesConfigurableUser import *
 
 class Hlt1VeloLinesConf(HltLinesConfigurableUser):
-   __slots__ = { 'Prescale'                   : { 'Hlt1Velo(.Side|TrTr)' : 0.0001 } 
+   __slots__ = { 'Prescale'                   : { 'Hlt1Velo(.Side|TrTr).*' : 0.0001 } 
                , 'MinimumNumberOfRClusters'   : 12 # 4 tracks with 3 hits
                , 'MinimumNumberOfPhiClusters' : 12 # 4 tracks with 3 hits
                , 'MaxNumberOfClusters'        : 450 # 0.5% occupancy
