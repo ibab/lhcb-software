a4fd5d785e7dd0353e944fdafd9807ae5c4d69b4
Gerhard Raven <gerhard.raven@nikhef.nl>
2009-11-13 19:58:15 +0000

    add Physics_MinBiasL0_PassThroughHlt_Nov09 setting
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@79780 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5642156289a3f2cfd154fae6a409329bca8a42d3

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Physics_MinBiasL0_PassThroughHlt_Nov09.py
@@ -0,0 +1,54 @@
+class Physics_MinBiasL0_PassThroughHlt_Nov09 :
+    """
+    Threshold settings for Hlt1 and 2 in nominal conditions (list of commissioning)
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author P.Koppenburg, G. Raven
+    @date 2009-08-11
+    """
+    
+    
+    def __init__(self) :
+        self.StripEndSequence = False
+
+    def HltType(self) :
+        return 'Physics_MinBiasL0_PassThroughHlt_Nov09'
+
+    def L0TCK(self) :
+        return "0x2009"
+
+    def ActiveHlt2Lines(self) :
+        """
+        Returns a list of active lines
+        """
+        return  [ ]
+   
+    def ActiveHlt1Lines(self) :
+        """
+        Returns a list of active lines
+        """
+        return [ 'Hlt1ODINPhysics','Hlt1ODINTechnical','Hlt1Tell1Error'
+               , 'Hlt1L0Any'
+               , 'Hlt1LumiNoBeam','Hlt1LumiBeamCrossing','Hlt1LumiBeam1','Hlt1LumiBeam2'
+               , 'Hlt1VeloASide','Hlt1VeloCSide'
+               ]
+
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        from Hlt1Lines.Hlt1CommissioningLines  import Hlt1CommissioningLinesConf
+        from Hlt1Lines.Hlt1LumiLines  import Hlt1LumiLinesConf
+        from Hlt1Lines.Hlt1VeloLines  import Hlt1VeloLinesConf
+        return { Hlt1CommissioningLinesConf : { 'Prescale' : { 'Hlt1ODINPhysics'  : 1.
+                                                             , 'Hlt1ODINTechnical'     : 0. # @OnlineEnv.AcceptRate
+                                                             , 'Hlt1Tell1Error'     : 1.
+                                                             } 
+                                              , 'TechnicalTrigger' : 'TechnicalTrigger'
+                                              }
+               , Hlt1LumiLinesConf : { 'EnableReco' : False, 'TriggerType' : 'LumiTrigger'  }
+               , Hlt1VeloLinesConf : { 'ODIN' : "( ODIN_TRGTYP != LHCb.ODIN.TechnicalTrigger )"
+                                     , 'Prescale' : { '.*' : 0 } 
+                                     }
+               }
