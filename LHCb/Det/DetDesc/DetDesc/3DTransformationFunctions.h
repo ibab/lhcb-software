c59853be2dec804092d5b88f09231a55d5a55a46
Wouter Hulsbergen <wouterh@nikhef.nl>
2009-11-13 10:07:31 +0000

    added getTransformJacobian
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@5572 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 775d34ca28acced11c13ed1c9ac2df2ad78cda6b

--- LHCb/Det/DetDesc/DetDesc/3DTransformationFunctions.h
+++ LHCb/Det/DetDesc/DetDesc/3DTransformationFunctions.h
@@ -1,4 +1,4 @@
-// $Id: 3DTransformationFunctions.h,v 1.5 2008-01-14 10:09:54 jpalac Exp $
+// $Id: 3DTransformationFunctions.h,v 1.6 2009-11-13 10:07:31 wouter Exp $
 #ifndef DETDESC_3DTRANSFORMATIONFUNCTIONS_H 
 #define DETDESC_3DTRANSFORMATIONFUNCTIONS_H 1
 
@@ -6,6 +6,7 @@
 #include "GaudiKernel/Transform3DTypes.h"
 #include "Math/Translation3D.h"
 #include "Math/RotationZYX.h"
+#include "Math/SMatrix.h"
 #include <vector>
 
 namespace DetDesc {
@@ -74,6 +75,36 @@ namespace DetDesc {
     euler321.GetComponents(rotZ, rotY, rotX); 
   }
   
+   /**
+    * Returns a Jacobian for a given transform. This you need to
+    * transform alignment derivatives from one frame to
+    * another. Suppose f( x ) is the transform corresponding to 6
+    * alignment parameters (e.g. deltatranslations and rotations).
+    * This routine returns the jacobian for a transformation of f: if
+    * x' is the delta in frame 0 and x is the delta in frame 1 and A
+    * is the transform from frame 0 to frame 1, then this returns the
+    * matrix Mij = dx_i / dx'_j, by taking the derivative of
+    *  
+    *   f'(x') = A * f(x) * A^{-1}
+    *
+    * Currently the application is transforming the alignment
+    * derivatives computed by TrackProjector. Suppose that T is the
+    * local-global transform. Suppose the derivative in the globa
+    * frame is D_global which is a 1x6 matrix. To get the derivative
+    * in the local frame, you do
+    *
+    * Transform3D localToGlobal = elements->geometry()->toGlobalMatrix()
+    * Matri6x6    jacobian      = getTransformJacobian( localToGlobal ) ;
+    * Matrix1x6   D_local       = D_global * jacobian
+    * 
+    * This is how this is used in the alignment and monitoring.
+    *
+    * @author Wouter Hulsbergen @nikhef.nl
+    * 
+    * @param CDM 3D transformation 
+    * @return The 6x6 jacobian
+    */ 
+  ROOT::Math::SMatrix<double,6,6> getTransformJacobian( const Gaudi::Transform3D& transform ) ;
 }
 
 #endif // DETDESC_3DTRANSFORMATIONFUNCTIONS_H
--- LHCb/Det/DetDesc/src/Lib/3DTransformationFunctions.cpp
+++ LHCb/Det/DetDesc/src/Lib/3DTransformationFunctions.cpp
@@ -1,4 +1,4 @@
-// $Id: 3DTransformationFunctions.cpp,v 1.5 2008-01-15 14:45:25 jpalac Exp $
+// $Id: 3DTransformationFunctions.cpp,v 1.6 2009-11-13 10:07:31 wouter Exp $
 // Include files 
 
 
@@ -50,6 +50,77 @@ void getZYXTransformParameters(const Gaudi::Transform3D& CDM,
   newTrans.GetComponents( translationParams.begin(), translationParams.end() );
   
 }
+
+ROOT::Math::SMatrix<double,6,6> getTransformJacobian( const Gaudi::Transform3D& transform ) 
+{
+
+  // Suppose f( x ) is the transform corresponding to parameters x
+  // (see routine AlParameters::transform).  This routine returns the
+  // jacobian for a transformation of f: if x' is the delta in frame 0
+  // and x is the delta in frame 1 and A is the transform from frame 0
+  // to frame 1, then this returns the matrix Mij = dx_i / dx'_j, by taking the
+  // derivative of
+  //   f'(x')   =    A * f(x) * A^{-1}
+  //
+
+  typedef ROOT::Math::SMatrix<double, 3, 3> Matrix3x3 ;
+  typedef ROOT::Math::SMatrix<double, 6, 6> Matrix6x6 ;
+  typedef ROOT::Math::SVector<double, 3>    Vector3;
+
+  // first decompose the transform. cannot do this quicker because of old ROOT version.
+  Matrix3x3 R ;
+  Vector3 T ;
+  transform.GetComponents(R(0,0),R(0,1),R(0,2),T(0),
+                          R(1,0),R(1,1),R(1,2),T(1),
+                          R(2,0),R(2,1),R(2,2),T(2)) ;
+
+  // We also need the inverse of R, which is of course just its transpose:
+  Matrix3x3 Rinv = ROOT::Math::Transpose(R) ;
+  //invR.Invert() ;
+  
+  // const matrices for derivative of rotations to rotation pars
+  Matrix3x3 drot[3] ;
+  drot[0](2,1) = drot[1](0,2) = drot[2](1,0) = 1 ;
+  drot[0](1,2) = drot[1](2,0) = drot[2](0,1) = -1 ;
+  
+  // R ( deps ) R^{-1} for each three components
+  Matrix3x3 RdrotR[3] ;
+  for(int i=0; i<3; ++i) RdrotR[i] = R * (drot[i] * Rinv) ;
+  
+  // now construct all components of the jacobian
+  Matrix6x6 jacobian ;
+  
+  // the derivative of the rotation to the translation (deps/deta') is zero.
+
+  // the derivative of the translation to the translation (deta/deta')
+  jacobian.Place_at( R, 0, 0) ;
+  
+  // the derivatives to the rotation are a bit more complicated. let's
+  // first do the rotation to the rotation:
+  for(int i=0; i<3; ++i) {
+    jacobian(3,3+i) = RdrotR[i](2,1) ;
+    jacobian(4,3+i) = RdrotR[i](0,2) ;
+    jacobian(5,3+i) = RdrotR[i](1,0) ;
+  }
+
+  // SO, if you just think about it for a minute, you realize that
+  // also the delta-rotation should transform with the rotation:
+  // //jacobian.Place_at( R, 3, 3) ;
+
+  // the only thing left is the derivative of the translation to the
+  // rotation. it's the only part that involves T:
+  for(int i=0; i<3; ++i) {
+    Vector3 tmp = RdrotR[i] * T ;
+    for(int j=0; j<3; ++j) jacobian(j,i+3) = - tmp(j) ;
+  }
+  
+  // Het staat er nu zo kort, dat je geen idee meer hebt hoeveel werk
+  // de algebra was ...
+  return jacobian ;
+}
+
+
+
   
 } // namespace DetDesc
 //=============================================================================
