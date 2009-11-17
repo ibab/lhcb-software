de621408890eb435ee36b1178a3ef3f530b284ef
jpalac <jpalac@cern.ch>
2009-11-17 15:52:36 +0000

    New interface for cloning of LHCb::Tagger objects.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@79838 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0db86391b671a01824b44b065fa1b9daaf52b01d

---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneTagger.h
@@ -0,0 +1,26 @@
+// $Id: ICloneTagger.h,v 1.1 2009-11-17 15:52:36 jpalac Exp $
+#ifndef MICRODST_ICLONETAGGER_H 
+#define MICRODST_ICLONETAGGER_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from MicroDST
+#include "MicroDST/ICloner.h"
+namespace LHCb 
+{
+  class Tagger;
+}
+
+/** @class ICloneTagger ICloneTagger.h MicroDST/ICloneTagger.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2009-11-17
+ */
+class GAUDI_API ICloneTagger : virtual public MicroDST::ICloner<LHCb::Tagger> {
+public: 
+  DeclareInterfaceID(ICloneTagger, 1, 0 );
+};
+#endif // MICRODST_ICLONETAGGER_H
