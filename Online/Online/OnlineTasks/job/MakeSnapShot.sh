c25744728a8f1a24b1030a7aa856af06fc3e39b3
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2009-11-20 10:33:33 +0000

    Add date option to ensure we get the latest conditions for a given tag.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100031 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2cd0d90048ce7d1449b8498016ffce4a23cceff

--- Online/Online/OnlineTasks/job/MakeSnapShot.sh
+++ Online/Online/OnlineTasks/job/MakeSnapShot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # argument 1=Tag
 # argument 2=output db filename
+# we need the $DATE argument to ensure we get the latest conditions
 
 if test -n "$1" ; then export TAG=$1
    else echo "Script cannot be called without explicit tag. Call MakeSnapshot.py.";exit 1
@@ -8,11 +9,14 @@ fi
 
 . $LHCBSCRIPTS/SetupProject.sh LHCb
 
+DATE=`date +%Y"-"%m"-"%d`
+
 for partition in LHCBCOND DDDB SIMCOND; do
    if test -n "$2"; then export DB=$2
       else export DB=/group/online/hlt/conditions/${partition}_$TAG.db
    fi   
-   CondDBAdmin_MakeSnapshot.py -T $TAG $partition sqlite_file:$DB/$partition
+   echo " request snapshot of partition " $partition " using tag " $TAG " for date "   $DATE
+   CondDBAdmin_MakeSnapshot.py -T $TAG -s $DATE $partition sqlite_file:$DB/$partition
 done      
  
 
