cc5382403c40f7edd0d75817a2604769e230bae0
rlambert <rlambert@cern.ch>
2009-11-20 16:08:06 +0000

    extra tests for integration purposes, see savannah task 12602
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@79910 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 42da9905a1b587846604e7995b381e4df6aad702

---
+++ DaVinci/DaVinciSys/tests/options/SimpleMuSelection.py
@@ -0,0 +1,31 @@
+"""
+  @ Created by R. LAmbert
+  @ date 2009-11-20
+  Mu selection for tests
+  Long track muons with PT > 1 GeV
+"""
+
+__author__ = 'R. Lambert'
+__date__ = 'November 2009'
+__version__ = '$Revision: 1.1 $'
+
+import GaudiKernel.SystemOfUnits as Units
+from Gaudi.Configuration import *
+from Configurables import FilterDesktop, DaVinci
+from PhysSelPython.Wrappers import Selection, DataOnDemand, SelectionSequence
+
+MuForTests = FilterDesktop("BachelorMuForTests")
+MuForTests.Code = "((ISLONG) & (PT > 250.*MeV))"
+
+from PhysSelPython.Wrappers import Selection, DataOnDemand
+MyStdMuons = DataOnDemand('DODStdLooseMuons', Location = 'StdLooseMuons')
+
+SelMuForTests = Selection("SelMuForTests", Algorithm=MuForTests, RequiredSelections = [MyStdMuons])
+from Configurables import CheckPV
+checkPV = CheckPV('TestCheckPV')
+TestSequence = SelectionSequence("TestSeq",
+                                 TopSelection = SelMuForTests,
+                                 EventPreSelector = [checkPV])
+
+dv=DaVinci()
+DaVinci().UserAlgorithms=[ TestSequence.sequence() ]
