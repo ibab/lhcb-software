648909549e41153f1da955f723368eadea78a861
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-11-23 09:15:45 +0000

    update tests for v35r8 release
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31117 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: da7b71df4e74550a0786cf96d52dc949344e81bc

--- Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -3,6 +3,7 @@ from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
   LineSkipper(["INFO MuonIDAlg v"]) + \
+  LineSkipper(["Delta Memory for the event exceeds 3*sigma"]) + \
   LineSkipper(["AfterMagnetRegion/T/lvT"]) + \
   LineSkipper(["BeforeMagnetRegion/Rich1/lvRich1AerogelContainerRight"]) +\
   LineSkipper(["BeforeMagnetRegion/Velo2Rich1/lvVelo2Rich1"]) +\
