14e3ff2144a184f620718caa0338d802d00d7550
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-11-23 10:09:52 +0000

    changes for v35r9
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31118 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4352931628ccbdcfd634c012c14fd25ed85c5158

---
+++ Brunel/Rec/Brunel/options/2009-BeamGas.py
@@ -0,0 +1,25 @@
+# Example 2009 Beam gas options for Brunel
+
+# Syntax is:
+#   gaudirun.py 2009-BeamGas.py
+#
+from GaudiKernel.ProcessJobOptions import importOptions
+importOptions("$APPCONFIGOPTS/Brunel/moff.py")
+importOptions("$APPCONFIGOPTS/Brunel/veloOpen.py")
+importOptions("$APPCONFIGOPTS/Brunel/beamGas.py")
+
+from Gaudi.Configuration import FileCatalog, EventSelector
+from Configurables import Brunel
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
+
+#-- Use latest 2009 database tags for real data
+Brunel().DataType = "2009"
+
+# Latest cosmic run, with CALO, OT and (!!) RICH2 (35569 events)
+datasetName = "062509_0000000001"
+EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/data/2009/RAW/FULL/LHCb/BEAM1/62509/" + datasetName + ".raw'  SVC='LHCb::MDFSelector'"]
+
+# Default output files names are set up using value Brunel().DatasetName property
+Brunel().DatasetName = datasetName
