fd3548d04c3cf4f7da822b2bf35cc15dada0c7d1
Vanya Belyaev <ivan.belyaev@cern.ch>
2009-11-25 11:55:35 +0000

    add new example to deal with real data
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@89096 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fe5621626d8dbb4ef8bd8f245717516eda4eecb5

---
+++ Bender/Ex/BenderExample/python/BenderExample/FirstRealData.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+# =============================================================================
+# $Id: FirstRealData.py,v 1.1 2009-11-25 11:54:38 ibelyaev Exp $
+# =============================================================================
+# @file BenderExampele/FirstRealData.py
+# Bender-based version of the original
+#  GaudiPython/AnalysisPython-based script written
+# by Juan Palacious for analysis of real data from the first
+# collsions in November 2k+9
+# @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+# @date 2009-11-25
+# ============================================================================
+"""
+
+Bender-based version of the original GaudiPython/AnalysisPython-based
+script written by Juan Palacious for analysis of real data from the first
+collsions in November 2k+9
+
+"""
+# ============================================================================
+__author__  = "Vanya BELYAEV Ivan.Belyaev@nikhef.nl"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+# ============================================================================
+## import all needed stuff:
+import ROOT                           ## needed to produce/visualize the histograms
+
+from   Bender.Main         import *   ## import the ba
+import LHCbMath.Types                 ## easy access to various geometry routines 
+
+from   Gaudi.Configuration import *   ## needed for job configuration
+
+from   GaudiKernel.SystemOfUnits import GeV
+from   LoKiCore.functions        import doubles ## list -> std::vector<double>
+
+## namespace Gaudi
+Gaudi = cpp.Gaudi
+
+# =============================================================================
+## @class RealData
+#  Simple Bender-based algorithm tp 'analyse' the first real data
+#  the idea has been stollen from Juan Palacious
+#  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+#  @date 2009-11-25
+class RealData(Algo) :
+    """
+    Simple Bender-based algorithm tp 'analyse' the first real data
+    the idea has been stollen from Juan Palacious
+    """
+
+    def initialize ( self ) :
+
+        
+        sc = Algo.initialize ( self )
+        if sc.isFailure() : return sc 
+
+        self.beamLine = Gaudi.Math.XYZLine ( Gaudi.Math.XYZPoint (0,0,0) ,
+                                             Gaudi.Math.XYZVector(0,0,1) )
+
+        return SUCCESS
+
+    
+    ## the major analysis method 
+    def analyse  (self ) :
+        """
+        The major analysis method
+        """
+
+
+        tup = self.nTuple ( 'RealData')
+
+        
+        ## get tracks and count them!
+        
+        tracks = self.get('Rec/Track/Best')
+        cnt    = self.counter('#tracks')
+        cnt   += tracks.size()
+        
+        self.plot ( tracks.size() , '#tracks' , 0 , 50 )
+        
+        tup.column ( 'nTracks' , tracks.size() )
+        
+
+        zs = [] 
+        ## loop over all tracks
+        for track in tracks :
+            
+            trackLine = Gaudi.Math.XYZLine( track.position() , track.slopes() )
+
+            doca = trackLine.distance( self.beamLine )
+
+            self.plot ( doca , 'distance to beamline' , 0 , 500 ) 
+
+            trackPoint, beamPoint , flag = trackLine.closestPoints ( self.beamLine )
+            if flag :
+                self.plot ( trackPoint.z() , 'Z of track at POCA', -5000, 5000, 200)
+                zs += [ trackPoint.z() ] 
+
+        tup.farray ( 'zPoca' , doubles( zs ) , 'nT' , 200 )
+
+        
+        ## get Calo info:
+        caloE  = {}  ## energy in Calorimeter            (in GeV) 
+        caloET = {}  ## transverse energy in calorimeter (in GeV) 
+        caloN  = {}  ## number of digits 
+        
+        for det in ( 'Spd' , 'Prs' , 'Ecal' , 'Hcal' ) :
+
+            # get digits and count them 
+            digits = self.get( 'Raw/' + det + '/Digits' )
+
+            dd = self.getDet('/dd/Structure/LHCb/DownstreamRegion/' + det )
+            
+            e  = 0 
+            eT = 0 
+            for d in digits :
+                e  += d.e() / GeV 
+                eT += d.e() * dd.cellSine( d.cellID() ) / GeV 
+
+            e  = e 
+            eT = eT  
+            caloE  [ det ] = e
+            caloET [ det ] = eT 
+            caloN  [ det ] = digits.size()
+
+            
+        for det in ( 'Spd' , 'Prs' , 'Ecal' , 'Hcal' ) :
+
+            n  = int   ( caloN  [ det ] ) 
+            e  = float ( caloE  [ det ] ) 
+            eT = float ( caloET [ det ] ) 
+            
+            cnt  = self.counter('#  ' + det )
+            cnt += n
+            
+            cnt  = self.counter('e  ' + det )
+            cnt += e
+            
+            cnt  = self.counter('eT ' + det )
+            cnt += eT 
+            
+            tup.column ('e'  + det , e  ) 
+            tup.column ('eT' + det , eT ) 
+            tup.column ('n'  + det , n  )
+            
+            
+        tup.write()
+        
+        if caloET[ 'Ecal' ] > 3 :
+            
+            for z in zs : self.plot ( z , 'zPoca for eT(Ecal)>3 GeV ' , -5000 , 5000 , 200 )
+            
+        else :
+            
+            for z in zs : self.plot ( z , 'zPoca for eT(Ecal)<3 GeV' , -5000 , 5000 , 200 )
+            
+        
+        return SUCCESS 
+
+
+# =============================================================================
+## the actual job configuration
+def configure ( datafiles ) :
+    """
+    The actual job configuration
+    """
+    from Configurables           import DaVinci       ## needed for job configuration
+    from Configurables           import EventSelector ## needed for job configuration 
+    from GaudiConf.Configuration import FileCatalog   ## needed for job configuration 
+    from GaudiConf.Configuration import NTupleSvc     ## needed for job configuration 
+    
+    # the main configurable 
+    davinci = DaVinci (
+        DataType   = '2009' ,             ## NB:    2k+9
+        Simulation = False  ,             ## NB:   *REAL DATA*
+        EvtMax     = -1     ,             ## all eventts in the input files 
+        )
+    
+    EventSelector (
+        PrintFreq = 10        , 
+        Input     = datafiles 
+        )
+    
+    FileCatalog (
+        Catalogs = [ 'xmlcatalog_file:FirstBeamData2009.xml' ]
+        )
+    
+    NTupleSvc (
+        Output = [
+        "FRD DATAFILE='FirstRealData.root' TYPE='ROOT' OPT='NEW'"
+        ]
+        )
+ 
+    
+    ## end of static configurtaion
+    
+    ## instantiate application manager
+    gaudi = appMgr()
+
+    
+    ## start of dynamic configuration 
+    
+    ## instantiate my algorithm
+    alg = RealData(
+        'RealData'        ,
+        NTupleLUN = 'FRD' 
+        )
+    
+    gaudi.setAlgorithms ( [ alg ] ) 
+    
+
+    return SUCCESS 
+
+
+# =============================================================================
+## the actual job steering     
+if '__main__' == __name__ :
+    
+    print __doc__
+    print __author__
+    print __version__
+    
+    beamgas = [
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000002_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000003_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000005_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000006_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000007_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000008_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000009_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+        "   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000010_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'"
+        ]
+
+    collisions = [
+        "DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000011_1.dst?svcClass=lhcbmdst&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'"
+        ]
+
+    configure ( collisions )
+    ## configure ( beamgas )
+
+    gaudi = appMgr()
+
+    run ( -1 )
+
+    import GaudiPython.HistoUtils
+    
+    alg = gaudi.algorithm ( 'RealData' )
+    
+    histos = alg.Histos()
+    for key in histos :
+        histo = histos[key]
+        if hasattr ( histo , 'dump' ) :
+            print histo.dump( 40 , 20 )
+        if hasattr ( histo , 'Draw' ) :  
+            histo.Draw(  histo.title() + '.gif' )
+            histo.Draw(  histo.title() + '.eps' )
+            
+    
+# ============================================================================
+# The END 
+# ============================================================================
---
+++ Bender/Ex/BenderExample/python/BenderExample/FirstRealData_Tuples.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# =============================================================================
+# $Id: FirstRealData_Tuples.py,v 1.1 2009-11-25 11:54:38 ibelyaev Exp $
+# =============================================================================
+## @file BenderExample/FirstRealData_Tuples.py
+#  simple example to procss tuples, prepared by FirstRealData example.
+#  It is assumed that one has produces two tuples
+#  - The firts  one for data with collisions
+#  - The second one for data with beam-gas only
+#  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+#  @date   2009-11-25
+# =============================================================================
+"""
+Simple example to procss tuples, prepared by FirstRealData example.
+It is assumed that one has produces two tuples
+- The firts  one for data with collisions
+- The second one for data with beam-gas only
+"""
+# =============================================================================
+__author__  = "Vanya BELYAEV Ivan.Belyaev@nikhef.nl"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+# ============================================================================
+import ROOT
+
+
+collisions = 'FirstRealData_BX.root'
+beamgas    = 'FirstRealData_BG.root'
+
+file1 = ROOT.TFile( collisions )
+file2 = ROOT.TFile( beamgas    )
+
+tree1 = file1.Get('RealData/RealData')
+tree2 = file2.Get('RealData/RealData')
+
+print tree1
+print tree2
+
+h1 = ROOT.TH1D('h1', 'zPoca for collisions'                  , 100 , -5000 , 5000 )
+h2 = ROOT.TH1D('h2', 'zPoca for beamgas'                     , 100 , -5000 , 5000 )
+
+h3 = ROOT.TH1D('h3', 'zPoca for collisions Et(Ecal)>3 GeV'   , 100 , -5000 , 5000 )
+h4 = ROOT.TH1D('h4', 'zPoca for beamgas    Et(Ecal)>3 GeV'   , 100 , -5000 , 5000 )
+
+h5 = ROOT.TH1D('h5', 'zPoca for collisions Et(Ecal)<1 GeV'   , 100 , -5000 , 5000 )
+h6 = ROOT.TH1D('h6', 'zPoca for beamgas    Et(Ecal)<1 GeV'   , 100 , -5000 , 5000 )
+
+h7 = ROOT.TH1D('h7', 'zPoca for collisions 1<Et(Ecal)<3 GeV' , 100 , -5000 , 5000 )
+h8 = ROOT.TH1D('h8', 'zPoca for beamgas    1<Et(Ecal)<3 GeV' , 100 , -5000 , 5000 )
+
+tree1.Draw('zPoca >> h1 ')
+tree1.Draw('zPoca >> h3 ' , 'eTEcal > 3 ')
+tree1.Draw('zPoca >> h5 ' , 'eTEcal < 1 ')
+tree1.Draw('zPoca >> h7 ' , 'eTEcal > 1 && eTEcal <3 ')
+
+tree2.Draw('zPoca >> h2 ')
+tree2.Draw('zPoca >> h4 ' , 'eTEcal > 3 ')
+tree2.Draw('zPoca >> h6 ' , 'eTEcal < 1 ')
+tree2.Draw('zPoca >> h8 ' , 'eTEcal > 1 && eTEcal <3 ')
+
+c = ROOT.TCanvas()
+
+h1.Draw()
+h2.Draw('same')
+
+c.Print('no-cuts.gif') 
+c.Print('no-cuts.eps') 
+
+h3.Draw()
+h4.Draw('same')
+
+c.Print('etEcal>3.gif') 
+c.Print('etEcal>3.eps') 
+
+h5.Draw()
+h6.Draw('same')
+
+c.Print('etEcal<1.gif') 
+c.Print('etEcal<1.eps') 
+
+h7.Draw()
+h8.Draw('same')
+
+c.Print('1<etEcal<3.gif') 
+c.Print('1<etEcal<3.eps') 
+
+
+
+
+
+# =============================================================================
+# The END 
+# =============================================================================
+
