9c4eded74425e8190048bd7cede0c1d914e07ade
Marco Gersabeck <marco.gersabeck@cern.ch>
2009-11-26 22:40:48 +0000

    Updated un-tagged prompt charm selection.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80033 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fe922a87395565876d3b2e3eff34984aef6e46df

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingD2hhNoPID.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingD2hhNoPID.py
@@ -1,14 +1,12 @@
 ############################################################################
 #
 # Stripping selection for prompt D->hh events, mass window includes D->K+K- and D->K+pi- (+cc)
-# Current default selection is very loose
-# _tight_sel = True switches to a selection close to the offline one
 #
 ############################################################################
 
 __author__ = ['Marco Gersabeck']
 __date__ = '03/11/2009'
-__version__ = '$Revision: 1.2 $'
+__version__ = '$Revision: 1.3 $'
 
 __all__ = ('name', 'D0', 'sequence')
 
@@ -19,16 +17,9 @@ from StrippingConf.StrippingLine import StrippingLine, StrippingMember
 
 name = "D2hhNoPID"
 
-_tight_sel = False
-
-_D0Cuts = "(MIPDV(PRIMARY)<0.3) & (BPVDIRA>0.999)"
-_D0DauCuts = { "K+": "(PT>400) & (MIPDV(PRIMARY)>0.04)" }
-_D0CombCut = "(ADAMASS(1975*MeV)<175*MeV) & (APT>1200)"
-
-if _tight_sel:
-    _D0Cuts = "(BPVIPCHI2()<25) & (BPVDIRA>0.9995)"
-    _D0DauCuts = { "K+": "(PT>500) & (MIPCHI2DV(PRIMARY)>4)" }
-    _D0CombCut = "(ADAMASS(1975*MeV)<175*MeV) & (APT>1500)"
+_D0Cuts = "(BPVVDCHI2>9) & (BPVIPCHI2()<9) & (BPVDIRA>0.99975)"
+_D0DauCuts = { "K+": "(PT>500) & (MIPCHI2DV(PRIMARY)>9) & (TRCHI2DOF<9)" }
+_D0CombCut = "(ADAMASS(1940*MeV)<125*MeV) & (APT>1500)"
 
 _D0 = CombineParticles( name,
                         DecayDescriptor = "D0 -> K- K+",
@@ -44,14 +35,24 @@ D0 = Selection ( "Sel"+name,
 
 sequence = SelectionSequence("Seq"+name, TopSelection = D0)
 
+# Global event cuts
+from Configurables import LoKi__VoidFilter as VoidFilter
+from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
+modules = CoreFactory('CoreFactory').Modules
+for i in [ 'LoKiTrigger.decorators' ]:
+    if i not in modules: modules.append( i )
+gec_cuts = VoidFilter('gec_cuts',
+                      Code = "( VSOURCE('Rec/Vertex/Primary', NTRACKS >= 10) >> (VSIZE == 1) )\
+                              & ( TrSOURCE('Rec/Track/Best') >> (TrSIZE < 150 ) )",
+                      Preambulo = [ "from LoKiPhys.decorators import *",
+                                    "from LoKiCore.functions import *" ]
+                     )
+
 ############################################
 # Create StrippingLine with this selection #
 ############################################
-#from Configurables import LoKi__HDRFilter as HDRFilter
-#HDRFilter( 'NoLumiFilter', Code="HLT_PASS_RE('Hlt1(?!Lumi).*Decision')" )
-
 line = StrippingLine(name+"Line"
                           , prescale = 1.
-                          , algos = [ sequence ]
+                          , algos = [ gec_cuts, sequence ]
 #                          , HLT = HDRFilter( 'NoLumiFilter', Code="HLT_PASS_RE('Hlt1(?!Lumi).*Decision')" )
                           )
