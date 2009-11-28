a82451c8f350f8581bf8734897f5098e54b37d34
Victor Coco <victor.coco@cern.ch>
2009-11-28 07:53:54 +0000

    add paranthesis in VeloPixLiteCluster to remove warning with gcc43
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37835 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1f9515c2acc04c891e0733f540087989e23cb75d

--- LHCb/Event/DigiEvent/dict/lcgDict.h
+++ LHCb/Event/DigiEvent/dict/lcgDict.h
@@ -1,4 +1,4 @@
-// $Id: lcgDict.h,v 1.1 2008-09-03 08:39:52 cattanem Exp $
+// $Id: lcgDict.h,v 1.2 2009-11-28 07:53:54 cocov Exp $
 #ifndef DIGIEVENT_LCGDICT_H 
 #define DIGIEVENT_LCGDICT_H 1
 
@@ -7,6 +7,7 @@
 // begin include files
 #include "Kernel/FastClusterContainer.h"
 #include "Event/VeloLiteCluster.h"
+#include "Event/VeloPixLiteCluster.h"
 #include "Event/STLiteCluster.h"
 
 // end include files
@@ -16,6 +17,7 @@ namespace {
     // begin instantiations
     FastClusterContainer<LHCb::VeloLiteCluster, int> _i1;
     FastClusterContainer<LHCb::STLiteCluster, int>   _i2;
+    FastClusterContainer<LHCb::VeloPixLiteCluster, ulonglong> _i3;
     // end instantiations
   };
 }
