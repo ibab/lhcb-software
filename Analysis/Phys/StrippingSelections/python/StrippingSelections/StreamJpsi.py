c3c5631861f44c5a006630b91e2b5bf47ab27b37
jpalac <jpalac@cern.ch>
2009-11-30 11:42:35 +0000

    . Use StrippingDiMuon instead of StrippingJpsiInc.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80120 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e7b2bfee390cc5646d50ea3d7a6224f9170319fd

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamJpsi.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamJpsi.py
@@ -7,12 +7,12 @@
 from Gaudi.Configuration import *
 from StrippingConf.StrippingStream import StrippingStream
 
-from StrippingSelections import StrippingJpsiInc
+from StrippingSelections import StrippingDiMuon
 from StrippingSelections import StrippingMuIDCalib
 
 stream = StrippingStream("Jpsi")
 stream.appendLines( [ 
-		        StrippingJpsiInc.line, 
+		        StrippingDiMuon.line, 
 		        StrippingMuIDCalib.jp_line, 
 		        StrippingMuIDCalib.b_line 
 		    ] )
