f06c6451c931e0daec24aed73e62d23ecae3b3d9
jpalac <jpalac@cern.ch>
2009-11-30 13:02:52 +0000

    Fix algo output location
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@80128 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3f86cafb1df366745d87e6d7242b5a43be3aed92

--- DaVinci/DaVinciSys/tests/options/SimpleMuSelection.py
+++ DaVinci/DaVinciSys/tests/options/SimpleMuSelection.py
@@ -7,20 +7,22 @@
 
 __author__ = 'R. Lambert'
 __date__ = 'November 2009'
-__version__ = '$Revision: 1.1 $'
+__version__ = '$Revision: 1.2 $'
 
 import GaudiKernel.SystemOfUnits as Units
 from Gaudi.Configuration import *
 from Configurables import FilterDesktop, DaVinci
 from PhysSelPython.Wrappers import Selection, DataOnDemand, SelectionSequence
 
-MuForTests = FilterDesktop("BachelorMuForTests")
+MuForTests = FilterDesktop("_bachelorMu")
 MuForTests.Code = "((ISLONG) & (PT > 250.*MeV))"
 
 from PhysSelPython.Wrappers import Selection, DataOnDemand
 MyStdMuons = DataOnDemand('DODStdLooseMuons', Location = 'StdLooseMuons')
 
-SelMuForTests = Selection("SelMuForTests", Algorithm=MuForTests, RequiredSelections = [MyStdMuons])
+SelMuForTests = Selection("BachelorMuForTests",
+                          Algorithm=MuForTests,
+                          RequiredSelections = [MyStdMuons])
 from Configurables import CheckPV
 checkPV = CheckPV('TestCheckPV')
 TestSequence = SelectionSequence("TestSeq",
