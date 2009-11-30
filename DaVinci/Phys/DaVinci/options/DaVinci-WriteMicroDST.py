c7e40c18e101fe69f5c782ed1c27813da176f17d
jpalac <jpalac@cern.ch>
2009-11-30 18:26:17 +0000

    Remove redundant lines.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@80170 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 63e02c3e5147be6db74b3b1177bea99a550d7cd2

--- DaVinci/Phys/DaVinci/options/DaVinci-WriteMicroDST.py
+++ DaVinci/Phys/DaVinci/options/DaVinci-WriteMicroDST.py
@@ -1,5 +1,5 @@
 from Gaudi.Configuration import *
-from Configurables import DaVinci, PhysMicroDST, MicroDSTWriter
+from Configurables import DaVinci, MicroDSTWriter
 
 importOptions("$STDOPTS/PreloadUnits.opts")
 
@@ -16,52 +16,25 @@ conf.CopyMCTruth = True
 conf.CopyBTags = True
 microDST0Seq = conf.sequence()
 
-'''
-from MicroDSTExample.Selections import Phi2KK
-conf1 = MicroDSTWriter("MicroDST1")
-conf1.MicroDSTFile = "MyTestMDST1_MC_newConfWithAlgo2.mdst"
-#con1f.MicroDSTSelectionAlg = selSequence.topAlgName()
-conf1.MicroSelectionAlg = Phi2KK.SelPhi2KK.algo
-conf1.CopyL0DUReport = False
-conf1.CopyHltDecReports = False
-conf1.CopyMCTruth = True
-conf1.CopyBTags = False
-'''
-# to use the PV re-fitting, need to re-fit PVs, relate them, and pass the relations table location to the MicroDST maker. Could we put this On-Demand?
-'''
-from Configurables import PVReFitterAlg, PVRelatorAlg
-mainLocation = "Phys/" + selSequence.topAlgName()
-PVReFitter = PVReFitterAlg("PVReFitterAlg")
-PVReFitter.ParticleInputLocation = mainLocation+"/Particles"
-PVReFitter.VertexOutputLocation = mainLocation+"/RefittedVertices"
-p2ReFitPVRelationsLoc = mainLocation+"/Particle2ReFittedVertexRelations"
-PVReFitter.P2VRelationsOutputLocation = p2ReFitPVRelationsLoc
-PVReFitter.OutputLevel=4
-PVRelator = PVRelatorAlg()
-PVRelator.OutputLevel=4
-PVRelator.P2PVRelationsInputLocation = p2ReFitPVRelationsLoc
-p2pvSortedRelationsLoc = mainLocation + "/P2ReFitPVSortedRelations"
-PVRelator.P2PVRelationsOutputLocation = p2pvSortedRelationsLoc
-MySelection.Members += [PVReFitter, PVRelator]
-conf.CopyRelatedPVs = True
-conf.P2PVRelationsSuffix = "P2ReFitPVSortedRelations"
-'''
 #
 
 dv = DaVinci()
-dv.DataType = 'DC06'
+dv.DataType = 'MC09'
 dv.EvtMax = 100
 dv.UserAlgorithms = [microDST0Seq]
 dv.Input =  [
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000001_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000002_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000003_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000004_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000005_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000006_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000007_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000009_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000011_5.dst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/production/DC06/phys-v4-lumi2/00002146/DST/0000/00002146_00000013_5.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
-
-#print conf.name(), " sequence ", conf.seqMicroDST(), " ", conf.seqMicroDST().Members
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000002_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000003_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000004_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000005_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000006_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000007_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000008_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000009_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000010_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000011_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000012_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000013_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004879/0000/00004879_00000014_1.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+]
