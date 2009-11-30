caaa2c2f9c1e8d7440c5f36097584f0e1ebe7c60
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2009-11-30 04:57:08 +0000

    Algorithm to create a relations table between Particles and BackgroundCategories
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80063 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 22530ce9e7ac404153b6743c6582bcdbc66fb46b

---
+++ Analysis/Phys/DaVinciMCTools/src/Particle2BackgroundCategoryRelationsAlg.cpp
@@ -0,0 +1,95 @@
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+// local
+#include "Particle2BackgroundCategoryRelationsAlg.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : Particle2BackgroundCategoryRelationsAlg
+//
+// 2009-04-28 : Juan PALACIOS
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( Particle2BackgroundCategoryRelationsAlg );
+
+using namespace LHCb;
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+Particle2BackgroundCategoryRelationsAlg::Particle2BackgroundCategoryRelationsAlg( const std::string& name,
+                                            ISvcLocator* pSvcLocator)
+  : 
+  GaudiAlgorithm ( name , pSvcLocator ),
+  m_bkg(0)  
+{
+  declareProperty("InputParticles", m_particleLocation, "");
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+Particle2BackgroundCategoryRelationsAlg::~Particle2BackgroundCategoryRelationsAlg() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode Particle2BackgroundCategoryRelationsAlg::initialize() {
+
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  m_bkg = tool<IBackgroundCategory>( "BackgroundCategory", this );
+  m_desktop = tool<IPhysDesktop>("PhysDesktop",this) ;
+
+  return (0!=m_bkg) ? StatusCode::SUCCESS : StatusCode::FAILURE;
+
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode Particle2BackgroundCategoryRelationsAlg::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  //Check that we have an input location
+  if (m_particleLocation == "") {
+    Error ( "No particle location provided" ).ignore() ;
+    return StatusCode::FAILURE;
+  }
+  //Get the input particles
+  LHCb::Particle::Container* myParticles = get<LHCb::Particle::Container>(m_particleLocation);
+  //Check that this returns something 
+  if (!myParticles) {
+    Error ( "No particles at the location provided" ).ignore() ;
+    return StatusCode::FAILURE;
+  }
+
+  //Make the relations table
+  LHCb::Relation1D<const LHCb::Particle*,IBackgroundCategory::categories>* m_catRelations = new LHCb::Relation1D<const LHCb::Particle*,IBackgroundCategory::categories>;
+
+  for(LHCb::Particle::Container::const_iterator iP = myParticles->begin(); iP != myParticles->end(); ++iP ){
+    IBackgroundCategory::categories thisCat = m_bkg->category(*iP);
+    m_catRelations->relate(*iP,thisCat);
+  }
+
+  put(m_catRelations,m_particleLocation+"/P2BCRelations");
+ 
+  return StatusCode::SUCCESS;
+}
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode Particle2BackgroundCategoryRelationsAlg::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Analysis/Phys/DaVinciMCTools/src/Particle2BackgroundCategoryRelationsAlg.h
@@ -0,0 +1,39 @@
+#ifndef PARTICLE2BACKGROUNDCATEGORYRELATIONSALG_H 
+#define PARTICLE2BACKGROUNDCATEGORYRELATIONSALG_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "Kernel/IBackgroundCategory.h"          // Interface
+#include "Relations/Relations.h"
+#include "Kernel/IPhysDesktop.h"
+
+/** @class Particle2BackgroundCategoryRelationsAlg Particle2BackgroundCategoryRelationsAlg.h
+ *  
+ *  Algorithm which takes as input a TES container of Particles, gets the BackgroundCategory
+ *  for each one, and writes a Relations table between these Particles and their
+ *  BackgroundCategories to the TES. 
+ *
+ *  @author V. Gligorov
+ *  @date   2009-11-30
+ */
+
+class Particle2BackgroundCategoryRelationsAlg : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  Particle2BackgroundCategoryRelationsAlg( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~Particle2BackgroundCategoryRelationsAlg( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+private:
+
+  std::string m_particleLocation;
+
+  mutable IPhysDesktop* m_desktop;
+  IBackgroundCategory* m_bkg;  
+};
+#endif // PARTICLE2BACKGROUNDCATEGORYRELATIONSALG_H
