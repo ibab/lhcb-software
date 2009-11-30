44f4c78d0887c7a2052aae4a77f8f67fa4c00fba
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-11-30 08:59:02 +0000

    remove obsolete files
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40674 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 882580c88275d974a259a2f5479620e41416b90e

--- LHCb/Calo/CaloUtils/CaloUtils/dirHbookName.h
+++
@@ -1,8 +0,0 @@
-// $Id: dirHbookName.h,v 1.4 2004-10-27 12:47:08 cattanem Exp $ 
-#ifndef CALOUTILS_DIRHBOOKNAME_H 
-#define CALOUTILS_DIRHBOOKNAME_H 1
-
-// Dummy file for backward compatibility
-#include "GaudiAlg/HbookName.h"
-
-#endif ///< CALOUTILS_DIRHBOOKNAME_H
--- LHCb/Calo/CaloUtils/CaloUtils/histoHbookName.h
+++
@@ -1,8 +0,0 @@
-// $Id: histoHbookName.h,v 1.6 2004-10-27 12:47:08 cattanem Exp $ 
-#ifndef CALOUTILS_HISTOHBOOKNAME_H 
-#define CALOUTILS_HISTOHBOOKNAME_H 1
-
-// Dummy file for backward compatibility
-#include "GaudiAlg/HbookName.h"
-
-#endif ///< CALOUTILS_HISTOHBOOKNAME_H
