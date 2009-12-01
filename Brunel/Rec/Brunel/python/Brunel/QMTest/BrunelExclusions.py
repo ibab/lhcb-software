d8a4aa83f46f62f64115497a5f3c47391b7b9567
Marco Cattaneo <marco.cattaneo@cern.ch>
2009-12-01 15:27:16 +0000

    do not check geometry overlap coordinates
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31147 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 38b3761853a99125745c17e7b05b8869d2c1787a

--- Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -4,11 +4,7 @@ from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 preprocessor = LHCbPreprocessor + \
   LineSkipper(["INFO MuonIDAlg v"]) + \
   LineSkipper(["Delta Memory for the event exceeds 3*sigma"]) + \
-  LineSkipper(["AfterMagnetRegion/T/lvT"]) + \
-  LineSkipper(["BeforeMagnetRegion/Rich1/lvRich1AerogelContainerRight"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo2Rich1/lvVelo2Rich1"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo/RFFoil/lvRFDSSlope"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo/RFFoil/lvRFSlope"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo/RFFoil/lvRFDownStreamSection"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo/lvVelo"]) +\
-  LineSkipper(["BeforeMagnetRegion/Velo/PipeSections/lvVeloPipe"])
+  LineSkipper(["AfterMagnetRegion/"]) + \
+  LineSkipper(["MagnetRegion/"]) +\
+  LineSkipper(["BeforeMagnetRegion/"]) +\
+  LineSkipper(["DownstreamRegion"])
