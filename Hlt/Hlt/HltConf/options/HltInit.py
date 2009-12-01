2b302cc2b56794242e4941c2e8b8e43e7febbd03
Tomasz Skwarnicki <tskwarni@syr.edu>
2009-12-01 10:46:58 +0000

    add ID for PV3D in HltInit.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@80185 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 11cfe193a43f29824399c206705271bbfec24da9

--- Hlt/Hlt/HltConf/options/HltInit.py
+++ Hlt/Hlt/HltConf/options/HltInit.py
@@ -113,6 +113,7 @@ HltANNSvc().Hlt1SelectionID = {
     'RZVelo'                                :10100,
     'PV2D'                                  :10101,
     'Forward1'                              :10102,
+    'PV3D'                                  :10103,
     #
     # 11K - 20K : added automatically by configuration...
     # 50K+ : used by Hlt2
