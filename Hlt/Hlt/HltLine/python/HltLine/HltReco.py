45ae143d23cf6e63855dee0d7f7756d40fcb50df
Johannes Albrecht <johannes.albrecht@cern.ch>
2009-12-02 13:40:15 +0000

    add dummy vertex position filters for monitoring
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@80220 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f688ebca859b35646050470f54740744e1f7e217

--- Hlt/Hlt/HltLine/python/HltLine/HltReco.py
+++ Hlt/Hlt/HltLine/python/HltLine/HltReco.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: HltReco.py,v 1.12 2009-11-25 15:21:54 pkoppenb Exp $
+# $Id: HltReco.py,v 1.13 2009-12-02 13:40:15 albrecht Exp $
 # =============================================================================
 ## @file HltLine/HltReco.py
 #  Collection of predefined algorithms to perform reconstruction
@@ -184,6 +184,12 @@ recoFwd = HltTrackUpgrade( 'Hlt1RecoForward'
 preparePV2D = HltVertexFilter( 'Hlt1PreparePV2D'
                              , InputSelection = "TES:" + PatPV2DFit3D('HltRecoPV2D').OutputVerticesName
                              , RequirePositiveInputs = False
+                             , FilterDescriptor = ["VertexZPosition,>,-5000","VertexTransversePosition,>,-1"]
+                             , HistoDescriptor = {'VertexZPosition': ( 'PV2D: VertexZPosition',-200.,200.,200),
+                                                  'VertexZPositionBest': ( 'PV2D: Highest VertexZPosition',-20,200.,100),
+                                                  'VertexTransversePosition': ( 'PV2D: VertexTransversePosition',0,1,50),
+                                                  'VertexTransversePositionBest': ( 'PV2D: Highest VertexTransversePosition',0,1,50)
+                                                    }
                              , OutputSelection   = "PV2D" )
 
 
