2d44ec904479c7c9d5e31863c144a83a95655e7c
Gloria Corti <gloria.corti@cern.ch>
2009-12-02 09:04:41 +0000

    replace obsolte HidValley.opts with empty .py for production
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94761 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 573014dc6d7fd26365746028af754a2d99024bed

---
+++ Gauss/Gen/LbHidValley/options/HidValley.py
@@ -0,0 +1,8 @@
+##  Empty HiddenValley options to be included in production to generate
+##  events as to have the same structure for all generators
+##
+##  author G.Corti
+##  date 2009-09-18
+##
+
+
