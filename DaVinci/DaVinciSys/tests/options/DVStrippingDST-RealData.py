2998c568419a7519e2bb5e1d287d0f6c53441795
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2009-12-07 20:37:52 +0000

    real data updated
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@80307 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3f47b772f4ae4541e418bde6f9e4333af1a330d

--- DaVinci/DaVinciSys/tests/options/DVStrippingDST-RealData.py
+++ DaVinci/DaVinciSys/tests/options/DVStrippingDST-RealData.py
@@ -52,20 +52,14 @@ DaVinci().EvtMax = -1
 # Real Data
 #
 DaVinci().Input = [
-    # 21/11 Beam1 events
-    "   DATAFILE='castor:/castor/cern.ch/grid/lhcb/data/2009/DST/00005617/0000/00005617_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ,
-    "   DATAFILE='castor:/castor/cern.ch/grid/lhcb/data/2009/DST/00005617/0000/00005617_00000002_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ,
-    # 23/11 Collision data
-    "   DATAFILE='castor:/castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000010_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ,
-    "   DATAFILE='castor:/castor/cern.ch/grid/lhcb/data/2009/DST/00005616/0000/00005616_00000011_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ,
-    # add as much a you like
-     ]
+    # 6/12 Collision data
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/data/2009/DST/00005705/0000/00005705_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/data/2009/DST/00005705/0000/00005705_00000006_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/data/2009/DST/00005705/0000/00005705_00000008_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='PFN:/castor/cern.ch/grid/lhcb/data/2009/DST/00005705/0000/00005705_00000011_1.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
 ##############################################################################
 #
 # There's no field (temporary hack)
 #
-from Configurables import MagneticFieldSvc
-MagneticFieldSvc().UseConstantField = True
-MagneticFieldSvc().UseConditions    = False
 
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
