8341aa06e7f6744f6d06e74cf18b3ea68d4765ee
Albert Puig Navarro <albert.puig@epfl.ch>
2009-12-16 14:48:19 +0000

    Replaced GetNTuples by GetLFNs with improved functionality
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80479 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c71f52278fdaf2224dab9c4dbdcc94c24f6006a9

---
+++ Analysis/Phys/KaliCalo/grid/GetLFNs.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import re
+from optparse import OptionParser
+
+def getLFNs(job, regex):
+  """Generate a list of the LFNs of the ROOT Output Data of the given job
+  by selecting them using the given regular expression.
+
+  Example:
+  >> getTupleLFNs(job, 'fmDST')
+  ['LFN:/grid/KaliCalo.fmDST']
+  
+  @param job: job to get the output data from
+  @type job: GangaJob
+  @param regex: regular expression to use
+  @type regex: string
+  
+  @return: list of strings
+  
+  """
+  lfnList = []
+  myre = re.compile(regex)
+  subjobs = job.subjobs.select(status='completed')
+  for subjob in subjobs:
+      for output in subjob.backend.getOutputDataLFNs():
+          if myre.search(output.name):
+              lfnList.append('LFN:%s' %output.name)
+  return lfnList
+  
+if __name__=='__main__':  
+  usage = "usage: ganga %prog [options] output"
+  parser = OptionParser(usage=usage)
+  parser.add_option("-j", "--jobid", action="store", type="int", dest="jobid", default=-1, help="jobID to take the output data from")
+  parser.add_option("-r", "--regex", action="store", type="str", dest="regex", default=".*", help="regular expression to filter outputdata results")
+  (options, args) = parser.parse_args()
+
+  if options.jobid not in jobs.ids():
+    print "Unknown jobID %s" %options.jobid
+    sys.exit()
+  if len(args) != 1:
+    print "Wrong number of arguments"
+    print usage
+    sys.exit()
+  list = getLFNs(jobs(options.jobid), options.regex)
+  f = open(args[0], 'w')
+  for line in list:
+    f.write(line + '\n')
+  f.close()
