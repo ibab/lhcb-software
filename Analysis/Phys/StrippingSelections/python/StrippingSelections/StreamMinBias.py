5f7bec69d30fc12b46864e85a515112f9e7d6f17
Anton Poluektov <anton.poluektov@cern.ch>
2009-12-18 13:24:01 +0000

    Added pass-through lines: StrippingPass
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80504 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d5544d20271527b1084cfdbeabe100be459c7954

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamMinBias.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamMinBias.py
@@ -8,16 +8,18 @@ from Gaudi.Configuration import *
 from StrippingConf.StrippingStream import StrippingStream
 
 from StrippingSelections import StrippingMiniBias 
-
 from StrippingSelections import StrippingNoBias 
+from StrippingSelections.StrippingPass import StrippingPassConf
 
 stream = StrippingStream("MinBias")
 
 stream.appendLines( [ StrippingMiniBias.TrackingRequirementLine, 
                       StrippingMiniBias.L0MinBiasLine , 
+                      StrippingMiniBias.TrackLine , 
+                      StrippingMiniBias.VertexLine , 
                       StrippingNoBias.RandomBeam1Line,
                       StrippingNoBias.RandomBeam2Line,
                       StrippingNoBias.RandomNoBeamLine,
-                      StrippingNoBias.RandomBeamCrossingLine  ])
-
-
+                      StrippingNoBias.RandomBeamCrossingLine, 
+                      StrippingPassConf().passLine(),
+                      StrippingPassConf().physLine() ] )
---
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingPass.py
@@ -0,0 +1,58 @@
+####################################################################################
+#
+# Pass-through lines with configurable prescale: 
+#    PassLine:  selects all events 
+#    PhysLine:  selects all non-lumi events
+#
+# @author Anton Poluektov
+# @date 2009-12-18
+#
+####################################################################################       
+
+from Gaudi.Configuration import *
+from LHCbKernel.Configuration import *
+
+class StrippingPassConf(LHCbConfigurableUser):
+
+    __slots__ = { 
+                   'PassPrescale'       : 1.       # adimentional 
+                ,  'PhysPrescale'       : 1.       # adimentional
+                   }
+
+    def passLine( self ) : 
+    
+	props = self.getProps()
+
+	print "PassLine props:"
+	print props
+
+	from StrippingConf.StrippingLine import StrippingLine
+	return StrippingLine( "PassLine" , checkPV = False 
+                                     , prescale = props['PassPrescale'] )
+
+
+    def physLine( self ) : 
+	from Configurables import HltRoutingBitsFilter
+
+	# Filter lumi events
+	physFilter = HltRoutingBitsFilter( "PhysFilter", RequireMask = [ 0x0, 0x4, 0x0 ] ) 
+
+	props = self.getProps()
+	
+	print "PhysLine props:"
+	print props
+
+	from StrippingConf.StrippingLine import StrippingLine
+	return StrippingLine( "PhysLine" , checkPV = False
+				     , prescale = props['PhysPrescale']
+				     , algos = [ physFilter ]  )
+
+    def getProps(self) :
+        """
+	From HltLinesConfigurableUser
+	@todo Should be shared between Hlt and stripping
+	"""
+	d = dict()
+	for (k,v) in self.getDefaultProperties().iteritems() :
+    	    d[k] = getattr(self,k) if hasattr(self,k) else v
+	return d
