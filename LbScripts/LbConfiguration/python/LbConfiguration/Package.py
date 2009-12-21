7fc28d211b357ab9f36fa323765d44860e6c5d39
hmdegaud <hmdegaud@cern.ch>
2009-12-21 19:29:30 +0000

    - python/LbConfiguration/Package.py: New file. New module which contains the data package configurations. - tests/PackageTest.py: New file. Test file for Package.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@14019 4525493e-7705-40b1-a816-d608a930855b

---
+++ LbScripts/LbConfiguration/python/LbConfiguration/Package.py
@@ -0,0 +1,148 @@
+""" general configuration for data packages """
+#@PydevCodeAnalysisIgnore
+
+import logging
+import sys
+
+# ------------------------------------------------------------------------------------
+# list of unversioned data projects
+project_names = ["DBASE", "PARAM" ]
+
+
+# ------------------------------------------------------------------------------------
+# list of original package names
+package_names = ['XmlDDDB',
+                 'DecFiles',
+                 'FieldMap',
+                 'BooleDQ',
+                 'BrunelDQ',
+                 'ParamFiles',
+                 'XmlConditions',
+                 'SQLDDDB',
+                 'BcVegPyData',
+                 'MCatNLOData',
+                 'LHCbBkg',
+                 'Geant4Files',
+                 'MIBData',
+                 'L0TCK',
+                 'HltTCK',
+                 'AppConfig']
+
+class PackageConfException(Exception): 
+    pass
+
+class PackageConf(object):
+    def __init__(self, packagename):
+        self._name = packagename
+        self._hat = None
+        self._project = None
+        self._tarballname = None
+    def Name(self):
+        return self._name
+    def name(self):
+        return self._name.lower()
+    def NAME(self):
+        return self._name.upper()
+    def FullName(self):
+        if self._hat :
+            return "/".join([self._hat, self._name])
+        else :
+            return self._name
+    def setHat(self, hat):
+        self._hat = hat
+    def hat(self):
+        return self._hat
+    def setProject(self, project):
+        if project in project_names :
+            self._project = project
+        else :
+            raise PackageConfException
+    def project(self):
+        return self._project
+    def tarBallName(self, version=None, full=False):
+        tbname = "_".join([self._project, self.FullName().replace("/", "_")])
+        if version :
+            tbname += "_%s" % version
+            if full :
+                tbname += ".tar.gz"
+        return tbname
+    
+package_list = []
+for _pak in package_names:
+    package_list.append(PackageConf(_pak))
+del _pak#IGNORE:W0631
+
+def getPackage(packagename):
+    """ return the static instance of the package configuration by name """
+    pj = None
+    for p in package_list:
+        if p.name() == packagename.lower():
+            pj = p
+    if pj :
+        return pj
+    else:
+        raise PackageConfException, "No such package configuration"
+
+def getPackageList(projectname=None, hatname=None):
+    plist = package_list
+    if projectname :
+        plist = [ p for p in plist if p.project() == projectname ]
+    if hatname :
+        plist = [ p for p in plist if p.hat() == hatname ]
+    return plist
+        
+def getTarBallName(packagename, version):
+    """ contruct the tarball name from the components """
+    filename = None
+    log = logging.getLogger()
+    pak = getPackage(packagename)
+    filename = pak.tarBallName(version, full=True)
+    if filename :
+        log.debug("The tarball name is %s" % filename)
+    return filename
+
+# create static instance of the package configuration with the correct name
+for _pn in package_names:
+    setattr(sys.modules[__name__], _pn, getPackage(_pn))
+del _pn#IGNORE:W0631
+
+XmlDDDB.setHat("Det")#IGNORE:E0602
+XmlDDDB.setProject("DBASE")#IGNORE:E0602
+
+DecFiles.setHat("Gen")#IGNORE:E0602
+DecFiles.setProject("DBASE")#IGNORE:E0602
+
+FieldMap.setProject("DBASE")#IGNORE:E0602
+
+BooleDQ.setHat("DQ")#IGNORE:E0602
+BooleDQ.setProject("DBASE")#IGNORE:E0602
+
+BrunelDQ.setHat("DQ")#IGNORE:E0602
+BrunelDQ.setProject("DBASE")#IGNORE:E0602
+
+ParamFiles.setProject("PARAM")#IGNORE:E0602
+
+XmlConditions.setHat("Det")#IGNORE:E0602
+XmlConditions.setProject("DBASE")#IGNORE:E0602
+
+SQLDDDB.setHat("Det")#IGNORE:E0602
+SQLDDDB.setProject("DBASE")#IGNORE:E0602
+
+BcVegPyData.setProject("PARAM")#IGNORE:E0602
+
+MCatNLOData.setProject("PARAM")#IGNORE:E0602
+
+LHCbBkg.setProject("PARAM")#IGNORE:E0602
+
+Geant4Files.setProject("PARAM")#IGNORE:E0602
+
+MIBData.setProject("PARAM")#IGNORE:E0602
+
+L0TCK.setHat("TCK")#IGNORE:E0602
+L0TCK.setProject("DBASE")#IGNORE:E0602
+
+HltTCK.setHat("TCK")#IGNORE:E0602
+HltTCK.setProject("DBASE")#IGNORE:E0602
+
+AppConfig.setProject("DBASE")#IGNORE:E0602
+
---
+++ LbScripts/LbConfiguration/tests/PackageTest.py
@@ -0,0 +1,43 @@
+from LbConfiguration.Package import getPackage, DecFiles, AppConfig
+
+import logging
+import unittest
+
+class PackageTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        
+    def testPackage(self):
+        m1 = getPackage("DecFiles")
+        self.assertEqual(m1, DecFiles)
+        self.assertEqual(m1.Name(), "DecFiles")
+        self.assertEqual(m1.hat(), "Gen")
+        self.assertEqual(m1.FullName(), "Gen/DecFiles")
+        self.assertEqual(m1.project(), "DBASE")
+        
+        m2 = getPackage("AppConfig")
+        self.assertEqual(m2, AppConfig)
+        self.assertNotEqual(m2, DecFiles)
+        self.assertEqual(m2.Name(), "AppConfig")
+        self.assertEqual(m2.hat(), None)
+        self.assertEqual(m2.FullName(), "AppConfig")
+        self.assertEqual(m1.project(), "DBASE")
+        
+    def testTarBall(self):
+        m1 = getPackage("DecFiles")
+        
+        m2 = getPackage("AppConfig")
+        
+        
+        
+if __name__ == '__main__':
+    log = logging.getLogger()
+    log.setLevel(logging.CRITICAL)
+    console = logging.StreamHandler()
+    console.setFormatter(logging.Formatter("%(levelname)-8s: %(message)s"))
+    log.addHandler(console)
+    unittest.main()
