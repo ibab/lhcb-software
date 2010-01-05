6ef1f1d07bea5529cd04bde4c6942786fdcfa209
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-01-05 13:21:14 +0000

    add file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80614 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 05984bea613f0b12dbc4f413850db0fb98b5d8ad

---
+++ Analysis/Tutorial/Analysis/options/MC09-Bs2JpsiPhi.py
@@ -0,0 +1,15 @@
+#-- GAUDI jobOptions generated on Tue Jan  5 12:07:28 2010
+#-- Contains event types : 
+#--   13144002 - 335 files - 5833615 events - 1675.97 GBytes
+
+from Gaudi.Configuration import * 
+
+EventSelector().Input   = [
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000001_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000002_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000003_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000004_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000005_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000006_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000007_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'",
+"   DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/MC09/DST/00005077/0000/00005077_00000010_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'"]
