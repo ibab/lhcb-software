3547dd09d248397cd19863fedb2378e3eb7baa62
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-01-05 11:11:16 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@19800 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bc1030974ae3662bd08c3cba04cdea58eafe33c

---
+++ Rec/Tr/TrackUtils/src/TrackListMerger.cpp
@@ -0,0 +1,68 @@
+// $Id: TrackListMerger.cpp,v 1.1 2010-01-05 11:11:16 wouter Exp $
+
+
+/** @class TrackListMerger TrackListMerger.h
+ *
+ *  Merge different track lists.
+ *
+ *  @author Wouter Hulsbergen
+ *  @date   05/01/2010
+ */
+
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/AlgFactory.h"
+#include "GaudiKernel/SharedObjectsContainer.h"
+#include <string>
+#include <vector>
+#include "Event/Track.h"
+
+
+class TrackListMerger: public GaudiAlgorithm {
+
+public:
+
+  // Constructors and destructor
+  TrackListMerger(const std::string& name,
+              ISvcLocator* pSvcLocator);
+  virtual ~TrackListMerger();
+  virtual StatusCode execute();
+
+private:
+  std::vector<std::string> m_inputLocations;
+  std::string m_outputLocation;
+};
+
+
+DECLARE_ALGORITHM_FACTORY( TrackListMerger );
+
+TrackListMerger::TrackListMerger(const std::string& name,
+                       ISvcLocator* pSvcLocator):
+  GaudiAlgorithm(name, pSvcLocator)
+{
+  // constructor
+  declareProperty( "inputLocations",  m_inputLocations) ;
+  declareProperty( "outputLocation", m_outputLocation) ;
+}
+
+TrackListMerger::~TrackListMerger()
+{
+  // destructor
+}
+
+StatusCode TrackListMerger::execute()
+{
+  LHCb::Track::Selection* tracksout = new LHCb::Track::Selection() ;
+  put( tracksout, m_outputLocation) ;
+  
+  // loop 
+  for( std::vector<std::string>::const_iterator ilist = m_inputLocations.begin() ;
+       ilist != m_inputLocations.end(); ++ilist) {
+    LHCb::Track::Range tracksin  = get<LHCb::Track::Range>(*ilist) ;
+    for (Gaudi::Range_<LHCb::Track::ConstVector>::const_iterator itrack = tracksin.begin(); 
+	 itrack != tracksin.end(); ++itrack) 
+      // make sure the track is not yet there!
+      if( std::find( tracksout->begin(),tracksout->end(),*itrack) == tracksout->end() )
+	tracksout->insert( *itrack ) ;
+  }
+  return StatusCode::SUCCESS;
+}
---
+++ Rec/Tr/TrackUtils/src/TrackListRefiner.cpp
@@ -0,0 +1,102 @@
+// $Id: TrackListRefiner.cpp,v 1.1 2010-01-05 11:11:16 wouter Exp $
+
+
+/** @class TrackListRefiner TrackListRefiner.h
+ *
+ *  Make a subselection of a track list
+ *
+ *  @author Wouter Hulsbergen
+ *  @date   05/01/2010
+ */
+
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiKernel/ToolHandle.h"
+#include "TrackInterfaces/ITrackSelector.h"
+#include "GaudiKernel/SharedObjectsContainer.h"
+#include <string>
+#include "GaudiKernel/AlgFactory.h"
+#include "Event/Track.h"
+
+
+class TrackListRefiner: public GaudiAlgorithm {
+
+public:
+
+  // Constructors and destructor
+  TrackListRefiner(const std::string& name,
+              ISvcLocator* pSvcLocator);
+  virtual ~TrackListRefiner();
+
+  virtual StatusCode initialize();
+  virtual StatusCode finalize();
+
+  virtual StatusCode execute();
+
+private:
+  std::string m_inputLocation;
+  std::string m_outputLocation;
+  ToolHandle<ITrackSelector> m_selector;
+};
+
+
+DECLARE_ALGORITHM_FACTORY( TrackListRefiner );
+
+TrackListRefiner::TrackListRefiner(const std::string& name,
+                       ISvcLocator* pSvcLocator):
+  GaudiAlgorithm(name, pSvcLocator),
+  m_selector("None",this)
+{
+  // constructor
+  declareProperty( "inputLocation",  m_inputLocation ) ;
+  declareProperty( "outputLocation", m_outputLocation ) ;
+  declareProperty( "Selector", m_selector );
+}
+
+TrackListRefiner::~TrackListRefiner()
+{
+  // destructor
+}
+
+StatusCode TrackListRefiner::initialize()
+{
+  // Initializes TsaInitialization at the begin of program execution.
+  
+  StatusCode sc = GaudiAlgorithm::initialize();
+  if (sc.isFailure()){
+    return Error("Failed to initialize");
+  }
+  
+  // retrieve the selector if it is set
+  if ( !m_selector.empty() ) {
+    sc = m_selector.retrieve() ;
+    if(sc.isFailure())
+      error() << "Failed to retrieve selector." << endmsg ;
+  }
+  
+  return sc ;
+}
+
+StatusCode TrackListRefiner::finalize()
+{
+  if ( !m_selector.empty() ) m_selector.release().ignore() ;
+  return GaudiAlgorithm::finalize() ;
+}
+
+StatusCode TrackListRefiner::execute()
+{
+  LHCb::Track::Range tracksin  = get<LHCb::Track::Range>(m_inputLocation) ;
+  LHCb::Track::Selection* tracksout = new LHCb::Track::Selection() ;
+  put( tracksout, m_outputLocation) ;
+  
+  // loop 
+  for (LHCb::Track::Range::const_iterator itrack = tracksin.begin(); 
+       itrack != tracksin.end(); ++itrack)  {
+    bool accept = true ;
+    if ( !m_selector.empty() )
+      accept = m_selector->accept( **itrack ) ;
+    if( accept) tracksout->insert( *itrack ) ;
+  }
+  
+  return StatusCode::SUCCESS;
+};
+
