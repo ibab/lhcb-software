fe0048274c8f8f5128e57d459e2d134e0edebdfc
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-01-05 11:53:09 +0000

    v8r3p2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80611 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 448900b0976d5374317ff3fcb81b02362374e015

--- Analysis/Tutorial/Analysis/options/MC09-InclJpsi.py
+++ Analysis/Tutorial/Analysis/options/MC09-InclJpsi.py
@@ -13,4 +13,3 @@ EventSelector().Input   = [
 "   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000007_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000008_1.dst' TYP='POOL_ROOTTREE' OPT='READ'",
 "   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DST/00004831/0000/00004831_00000009_1.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
-FileCatalog().Catalogs = [ 'xmlcatalog_file:MC09-InclJpsi.xml' ]
