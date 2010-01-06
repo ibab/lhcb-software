5cebd8160c2d9737bbaf50dd11f205641798173d
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-01-06 11:04:12 +0000

    Task 13084: eta
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 997817a853de876d3b7180bfa71296b87c5a412a

--- Analysis/Phys/CommonParticles/python/CommonParticles/StandardEta.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StandardEta.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StandardBasicNeutral.py,v 1.3 2010-01-06 11:04:12 pkoppenb Exp $ 
+# $Id: StandardEta.py,v 1.1 2010-01-06 11:04:12 pkoppenb Exp $ 
 # =============================================================================
-## @file  CommonParticles/StandardBasicNeutral.py
-#  configuration file for 'Standard Basic Neutral' 
+## @file  CommonParticles/StandardEta.py
+#  configuration file for 'Standard Eta' 
 #  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
 #  @date 2009-01-14
 # =============================================================================
 """
-Configuration file for 'Standard Basic Neutral' particles 
+Configuration file for 'Standard Eta'
 """
-__author__  = "Vanya BELYAEV Ivan.Belyaev@nikhef.nl"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.3 $"
+__author__  = "Patrick Koppenburg Patrick.Koppenburg@nikhef.nl"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
 # =============================================================================
 
 _locations = {} 
 
-from CommonParticles.StandardPhotons import *
+from CommonParticles.StdLooseResolvedEta import *
 _locations.update ( locations )
 
-from CommonParticles.StandardPi0 import *
-_locations.update ( locations )
-
-from CommonParticles.StandardEta import *
-_locations.update ( locations )
 
 # redefine the locations 
 locations = _locations
@@ -34,6 +29,7 @@ if '__main__' == __name__ :
     print __doc__
     print __author__
     print __version__
+    
     from CommonParticles.Utils import locationsDoD
     print locationsDoD ( locations )
     
---
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseResolvedEta.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# =============================================================================
+# $Id: StdLooseResolvedEta.py,v 1.1 2010-01-06 11:04:12 pkoppenb Exp $ 
+# =============================================================================
+## @file  CommonParticles/StdLooseResolvedEta.py
+#  configuration file for 'Standard Loose Pions' 
+#  @author Patrick Koppenburg
+#  @date 2009-02-18
+# =============================================================================
+"""
+Configuration file for 'Standard Loose ResolvedEta'
+"""
+__author__  = "Patrick Koppenburg"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+# =============================================================================
+__all__ = (
+    'StdLooseResolvedEta' ,
+    'locations'
+    )
+# =============================================================================
+from Gaudi.Configuration import *
+from Configurables       import ResolvedPi0Maker, PhotonMaker
+
+from GaudiKernel.SystemOfUnits import MeV
+
+from CommonParticles.Utils import *
+
+
+## create the algorithm 
+algorithm =  ResolvedPi0Maker ( 'StdLooseResolvedEta'         ,
+                                DecayDescriptor = 'Eta',
+                                MassWindow = 50.* MeV,
+                                Particle = 'eta')
+algorithm.addTool(PhotonMaker)
+algorithm.PhotonMaker.PtCut = 200.*MeV
+
+## configure Data-On-Demand service 
+locations = updateDoD ( algorithm )
+
+## finally: define the symbol 
+StdLooseResolvedEta = algorithm 
+
+## ============================================================================
+if '__main__' == __name__ :
+
+    print __doc__
+    print __author__
+    print __version__
+    print locationsDoD ( locations ) 
+
+# =============================================================================
+# The END 
+# =============================================================================
