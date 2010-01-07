3843d926e7bfda823a52459e123f9caee7959e44
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-01-07 14:55:46 +0000

    fix the fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@19809 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 310b32a672ac5df451b92d97186a79d16d376afb

--- Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
+++ Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
@@ -19,7 +19,7 @@ def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
         isMip = TrackSys().cosmics()
         seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal',
                                                   UseGeometricZ=isMip, HistoPrint=HistoPrint) )
-        seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
+        #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
         seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
         seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
 
