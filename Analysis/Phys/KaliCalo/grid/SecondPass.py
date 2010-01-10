c8fd5828ee884f38120c8e2ac2d29383f566f689
Albert Puig Navarro <albert.puig@epfl.ch>
2010-01-10 19:12:59 +0000

    Made SecondPass work, also with external coefficients
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80682 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0d45cd452a0a4c3c230b049e5eea17a86411f0a3

--- Analysis/Phys/KaliCalo/grid/SecondPass.py
+++ Analysis/Phys/KaliCalo/grid/SecondPass.py
@@ -14,6 +14,7 @@ from optparse import OptionParser
 # Hack
 sys.path.append('.')
 from JobTemplate import createJob
+from GetLFNs import getLFNs
 
 def getFmDSTLFNs(job):
   """Generate a list of the LFNs of the fmDST Output Data of the given job.
@@ -38,19 +39,30 @@ if __name__=='__main__':
   parser.add_option("-n", "--name", action="store", type="string", dest="name", default="fmDSTSecondPass", help="name of the job")
   parser.add_option("-j", "--jobid", action="store", type="int", dest="jobid", default=-1, help="jobID to take the input data from")
   parser.add_option("-f", "--fmDST", action="store_true", dest="fmDST", default=False, help="keep the fmDST in the outputdata")
+  parser.add_option("-c", "--coefficients", action="store", type="string", dest="coef", default=None, help="filename to import coefficients from")
   parser.add_option("-s", "--submit", action="store_true", dest="submit", default=False, help="submit job after creating it")
   (options, args) = parser.parse_args()
   
   print "Creating job with name %s" %options.name
   gridProxy.renew()
-  if not options.jobid in jobs.ids():
-    print "Job %s is unknown" %options.jobid
+  jobID = options.jobid
+  if not jobID in jobs.ids():
+    print "Job %s is unknown" %jobID
     sys.exit()
 
   j = createJob(options.name)
   oldjob = jobs(jobID)
-  j.inputdata = LHCbDataset(oldjob)
-  j.application.extraopts = "kali.FirstPass = False"
+  outputData = getLFNs(oldjob, "fmDST")
+  j.inputdata = LHCbDataset(outputData)
+  j.application.extraopts = "kali.FirstPass=False;"
+  if options.coef:
+    f = open(options.coef)
+    coef = {}
+    for line in f.readlines():
+      cell, value = line.split()
+      coef[int(cell)] = float(value)
+    f.close()
+    j.application.extraopts += "kali.Coefficients=%s" %coef
   if options.fmDST:
     j.outputdata.files.append('KaliPi0.fmDST')
   if options.submit:
