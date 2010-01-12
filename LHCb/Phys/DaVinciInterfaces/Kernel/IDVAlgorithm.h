a820dbb46b9ef75471af9c69416086af9fde5928
jpalac <jpalac@cern.ch>
2010-01-12 10:55:46 +0000

    typos.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@61370 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 17a3ab6aa1cc79659c89ed1328267a36125f6f08

--- LHCb/Phys/DaVinciInterfaces/Kernel/IDVAlgorithm.h
+++ LHCb/Phys/DaVinciInterfaces/Kernel/IDVAlgorithm.h
@@ -1,4 +1,4 @@
-// $Id: IDVAlgorithm.h,v 1.4 2010-01-12 10:42:29 jpalac Exp $
+// $Id: IDVAlgorithm.h,v 1.5 2010-01-12 10:55:46 jpalac Exp $
 #ifndef KERNEL_IDVALGORITHM_H 
 #define KERNEL_IDVALGORITHM_H 1
 
@@ -44,18 +44,18 @@ public:
    **/  
   virtual const GaudiAlgorithm* gaudiAlg() const = 0;
 
-  virtual const IDistanceCalculator* distanceCalculator(const string& name = "") const = 0;
+  virtual const IDistanceCalculator* distanceCalculator(const std::string& name = "") const = 0;
 
-  virtual const ILifetimeFitter* lifetimeFitter(const string& name = "") const = 0;
+  virtual const ILifetimeFitter* lifetimeFitter(const std::string& name = "") const = 0;
   
-  virtual const IVertexFit* vertexFitter(const string& name = "") const = 0;
+  virtual const IVertexFit* vertexFitter(const std::string& name = "") const = 0;
 
   /**
    *
    * Return a container of local LHCb::Particle*
    *
    **/  
-  virtual const LHCb::Particle::ConstVector& particles() const = 0;
+  virtual const LHCb::Particle::ConstVector* particles() const = 0;
 
   /**
    *
