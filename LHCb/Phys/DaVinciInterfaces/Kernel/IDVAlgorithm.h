40ab48dd22abc47841871004f5c52fa779eee3ed
jpalac <jpalac@cern.ch>
2010-01-12 10:37:13 +0000

    Add some doxygen comments.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@61366 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f8a184ee42ff1b1959bd82997229e5aedc953a47

--- LHCb/Phys/DaVinciInterfaces/Kernel/IDVAlgorithm.h
+++ LHCb/Phys/DaVinciInterfaces/Kernel/IDVAlgorithm.h
@@ -1,4 +1,4 @@
-// $Id: IDVAlgorithm.h,v 1.1 2010-01-12 10:31:54 jpalac Exp $
+// $Id: IDVAlgorithm.h,v 1.2 2010-01-12 10:37:13 jpalac Exp $
 #ifndef KERNEL_IDVALGORITHM_H 
 #define KERNEL_IDVALGORITHM_H 1
 
@@ -33,19 +33,40 @@ class GAUDI_API IDVAlgorithm : virtual public IInterface {
 public: 
 
   DeclareInterfaceID(IDVAlgorithm, 1, 0);
-  
+
+  /**
+   *
+   * Return a pointer to the parent GaudiAlgorithm, if in the
+   * implementation's inheritance tree. Return 0 otherwise.
+   *
+   **/  
   virtual const GaudiAlgorithm* gaudiAlg() const = 0;
-  
+
   virtual const IDistanceCalculator* distanceCalculator() const = 0;
 
   virtual const ILifetimeFitter* lifetimeFitter() const = 0;
   
   virtual const IVertexFit* vertexFitter() const = 0;
+
+  /**
+   *
+   * Return a container of local LHCb::Particle*
+   *
+   **/  
+  virtual const LHCb::Particle::ConstVector& particles() const = 0;
+  /**
+   *
+   * Return a container of local LHCb::Vertex*, containing LHCb::Particle's
+   * decay vertices
+   *
+   **/
+  virtual const LHCb::Vertex::ConstVector& vertices() const = 0;
   
-  virtual const LHCb::Particle::ConstVector* particles() const = 0;
-  
-  virtual const LHCb::Vertex::ConstVector* vertices() const = 0;
-  
+  /**
+   *
+   * Return the best primary vertex for a given LHCb::Particle.
+   *
+   **/
   virtual const LHCb::VertexBase* bestVertex(const LHCb::Particle*) const = 0;
   
 
