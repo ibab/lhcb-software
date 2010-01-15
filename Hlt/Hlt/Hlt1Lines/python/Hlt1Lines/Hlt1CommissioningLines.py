4e104b7065a603d1247aa633de69ce753a8f6880
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-01-15 12:18:41 +0000

    update commissioning lines configuration
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@80849 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fd6796f4ef04025945d8c1ea3b2e283510357617

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1CommissioningLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1CommissioningLines.py
@@ -9,7 +9,7 @@
 """
 # =============================================================================
 __author__  = "Gerhard Raven Gerhard.Raven@nikhef.nl"
-__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.5 $"
+__version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.6 $"
 # =============================================================================
 
 from HltLine.HltLinesConfigurableUser import *
@@ -21,14 +21,16 @@ class Hlt1CommissioningLinesConf(HltLinesConfigurableUser):
                               , 'Hlt1ODINTechnical'  : 0.000001 # @OnlineEnv.AcceptRate
                               , 'Hlt1Tell1Error'     : 0
                               }
+               , 'ODINPhysics'   : '( ODIN_TRGTYP == LHCb.ODIN.PhysicsTrigger )'
+               , 'ODINTechnical' : '( ODIN_TRGTYP == LHCb.ODIN.TechnicalTrigger )'
                }
    def __apply_configuration__(self):
         from HltLine.HltLine import Hlt1Line   as Line
-        Line('ODINPhysics',   ODIN = '( ODIN_TRGTYP == LHCb.ODIN.PhysicsTrigger ) | ( ODIN_TRGTYP == LHCb.ODIN.TimingTrigger )'
+        Line('ODINPhysics',   ODIN = self.getProp('ODINPhysics')
             , prescale = self.prescale
             , postscale = self.postscale
             )
-        Line('ODINTechnical', ODIN = '( ODIN_TRGTYP == LHCb.ODIN.TechnicalTrigger ) | ( ODIN_TRGTYP == LHCb.ODIN.AuxiliaryTrigger ) | ( ODIN_TRGTYP == LHCb.ODIN.NonZSupTrigger ) | ( ODIN_TRGTYP == LHCb.ODIN.CalibrationTrigger )'
+        Line('ODINTechnical', ODIN = self.getProp('ODINTechnical')
             , prescale = self.prescale
             , postscale = self.postscale
             )
