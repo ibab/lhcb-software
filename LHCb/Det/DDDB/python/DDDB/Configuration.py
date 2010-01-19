934c960076873548c2c6d6725589f63ad5ac9fdf
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-01-19 12:31:19 +0000

    change default tags
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@37907 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9bd26a8d399ab4429ffd897fd55c103c5ea93473

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -117,9 +117,9 @@ class DDDBConf(ConfigurableUser):
         Default configuration for 2009 data and corresponding MonteCarlo
         """
         # Set the tags
-        self.__set_tag__(["DDDB"], "head-20091120")
-        self.__set_tag__(["LHCBCOND"], "head-20091211")
-        self.__set_tag__(["SIMCOND"], "sim-20091112-vc-md100")
+        self.__set_tag__(["DDDB"], "head-20100119")
+        self.__set_tag__(["LHCBCOND"], "head-20100119")
+        self.__set_tag__(["SIMCOND"], "sim-20100119-vc-md100")
     
     def __2008_conf__(self):
         """
