0f15a90ad1cd34aa0e85d71eafe011e4aa2cec2a
Anton Poluektov <anton.poluektov@cern.ch>
2010-01-21 08:22:51 +0000

    Rearranged streams
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80938 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: baafbe53687424ed5822bdc64f87951c2f3c3cbe

---
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamCalibration.py
@@ -0,0 +1,19 @@
+#
+#   Calibration stripping selections 
+#
+#   @author A. Poluektov
+#   @date 2009-05-15
+#
+from Gaudi.Configuration import *
+from StrippingConf.StrippingStream import StrippingStream
+
+from StrippingSelections import StrippingLambdaNoPID
+from StrippingSelections import StrippingMuIDCalib
+
+stream = StrippingStream("Calibration")
+stream.appendLines( [ 
+		        StrippingMuIDCalib.jp_line, 
+		        StrippingMuIDCalib.b_line 
+		        StrippingLambdaNoPID.line_lambda_dd, 
+		        StrippingLambdaNoPID.line_lambda_ll 
+		    ] )
