6acd4794a41b6fbe6e3b144bf3f34f76142fa549
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-01-21 13:03:48 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@89222 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ec7c8a88f6734fd01d9c223d6c5c8d43f7a62f63

---
+++ Alignment/Alignment/TAlignment/src/IAlignChisqConstraintTool.h
@@ -0,0 +1,35 @@
+#ifndef TALIGNMENT_IALIGNCHISQCONSTRAINTTOOL_H
+#define TALIGNMENT_IALIGNCHISQCONSTRAINTTOOL_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include "IGetElementsToBeAligned.h"
+#include "AlignKernel/AlEquations.h"
+#include "AlignKernel/AlVec.h"
+#include "AlignKernel/AlSymMat.h"
+#include "Event/ChiSquare.h"
+
+namespace Al
+{
+  /// Static ID object
+  static const InterfaceID IID_IAlignChisqConstraintTool( "Al::IAlignChisqConstraintTool", 0, 0 );
+  
+  class IAlignChisqConstraintTool : virtual public IAlgTool
+  {
+  public:
+    typedef IGetElementsToBeAligned::Elements Elements ;
+
+    // Retrieve interface ID
+    static const InterfaceID& interfaceID() { return IID_IAlignChisqConstraintTool ; }
+    virtual LHCb::ChiSquare addConstraints(const Elements& elements,
+					   AlVec& halfDChi2DAlpha, AlSymMat& halfD2Chi2DAlpha2,
+					   std::ostream& logmessage) const = 0 ;
+    virtual LHCb::ChiSquare addConstraints(const Elements& inputelements,
+					   Al::Equations& equations,
+					   std::ostream& logmessage) const = 0 ;
+    virtual LHCb::ChiSquare chiSquare( const AlignmentElement& element, bool activeonly=true) const = 0 ;
+  } ;
+}
+
+#endif
+
+//  LocalWords:  TALIGNMENT
