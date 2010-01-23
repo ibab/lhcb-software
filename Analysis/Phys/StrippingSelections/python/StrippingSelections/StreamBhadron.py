148636594d312b5da2127b3bb3e39c204a85e1b6
Susan Carol Haines <haines@hep.phy.cam.ac.uk>
2010-01-23 12:17:47 +0000

    New and replaced B->DK lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@80986 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d112dee6856c5aea95194c0f2317651241c477e2

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamBhadron.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamBhadron.py
@@ -15,7 +15,8 @@ from StrippingConf.StrippingStream import StrippingStream
 from StrippingSelections import StrippingB2Charged2Body
 from StrippingSelections import StrippingB2DPi
 from StrippingSelections.StrippingBs2PhiPhi import StrippingBs2PhiPhiConf #from StrippingSelections import StrippingBs2PhiPhi
-from StrippingSelections import StrippingBu2D0K_D02KSPiPi
+from StrippingSelections.StrippingBu2D0K_D02KSPiPi import StrippingBu2D0K_D02KSPiPiConf
+from StrippingSelections.StrippingBu2D0h_D02KShh_NoPIDNoErrors import StrippingBu2D0h_D02KShh_NoPIDNoErrorsConf
 from StrippingSelections import StrippingBu2hhh
 from StrippingSelections import StrippingBu2hhh_1e4
 from StrippingSelections import StrippingTopo
@@ -28,9 +29,11 @@ stream.appendLines( [
 		        StrippingB2DPi.line2,  
 			StrippingBs2PhiPhiConf().normal(),  #StrippingBs2PhiPhi.line1, 
 			StrippingBs2PhiPhiConf().wide(),    #StrippingBs2PhiPhi.line2, 
-		        StrippingBu2D0K_D02KSPiPi.line1,  
-		        StrippingBu2D0K_D02KSPiPi.line2,  
-		        StrippingBu2hhh.line,  
+		        StrippingBu2D0K_D02KSPiPiConf().Bu2D0K_KSLL(), #StrippingBu2D0K_D02KSPiPi.line1,  
+		        StrippingBu2D0K_D02KSPiPiConf().Bu2D0K_KSDD(), #StrippingBu2D0K_D02KSPiPi.line2,
+                        StrippingBu2D0h_D02KShh_NoPIDNoErrorsConf().Bu2D0h_KSLL(), #StrippingBu2D0h_D02KShh_NoPIDNoErrors.line1,  
+		        StrippingBu2D0h_D02KShh_NoPIDNoErrorsConf().Bu2D0h_KSDD(), #StrippingBu2D0h_D02KShh_NoPIDNoErrors.line2,
+     		        StrippingBu2hhh.line,  
 		        StrippingBu2hhh_1e4.line,  
 		        StrippingTopo.line1,
                         StrippingTopo.line2, 
--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBu2D0K_D02KSPiPi.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBu2D0K_D02KSPiPi.py
@@ -1,133 +1,266 @@
 
 
 __author__ = 'Susan Haines'
-__date__ = '20/08/2009'
-__version__ = '$Revision: 1.1 $'
+__date__ = '22/01/2010'
+__version__ = '$Revision: 1.2 $'
 
 '''
 Bu->D0(KSPiPi)K stripping selection using LoKi::Hybrid and python
-configurables.
+configurables for new MC09 offline selection.
 '''
 
 from Gaudi.Configuration import *
-from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-from Configurables import FilterDesktop, CombineParticles
-import GaudiKernel.SystemOfUnits as Units
+from LHCbKernel.Configuration import *
 
-###########################
-# KS, long long daughters #
-###########################
-KSLLForBu2D0K_D02KSPiPi = FilterDesktop("StripKSLLForBu2D0K_D02KSPiPi")
-KSLLForBu2D0K_D02KSPiPi.InputLocations = ["StdLooseKsLL"]
-KSLLForBu2D0K_D02KSPiPi.Code = "(MINTREE( ('pi+'==ABSID), P) >2.*GeV)"\
-                             "& (MINTREE( ('pi+'==ABSID), MIPDV(PRIMARY))>0.15)"\
-                             "& (MAXTREE( ('pi+'==ABSID), TRCHI2DOF) <7.)"\
-                             "& (MINTREE( ('pi+'==ABSID), PIDp) >-1000.)"\
-                             "& (MINTREE( ('pi+'==ABSID), PIDK) >-1000.)"\
-                             "& (VFASPF(VCHI2/VDOF)<17.5)"\
-                             "& (BPVVD> 17.5)"\
-                             "& (ADMASS('KS0') < 35.*MeV)"\
-                             "& (2==NINGENERATION(('pi+'==ABSID)&((PIDp < -1e-10)|(PIDp > 1e-10)),1))"\
-                             "& (2==NINGENERATION(('pi+'==ABSID)&((PIDK < -1e-10)|(PIDK > 1e-10)),1))"
+class StrippingBu2D0K_D02KSPiPiConf(LHCbConfigurableUser):
+    __slots__ = { 
+        'BFlightCHI2_LL'        : 144.  
+        ,  'BDIRA_LL'           : 0.99985     
+        ,  'BIPCHI2_LL'         : 16.         
+        ,  'BVertexCHI2_LL'     : 9.
+        ,  'BachKIPCHI2_LL'     : 9.
+        ,  'BachKPt_LL'         : 0.9  
+        ,  'DVertexCHI2_LL'     : 6.25     
+        ,  'DdaughterPiIPCHI2_LL' : 9.
+        ,  'KSFlightCHI2_LL'    : 4.
+        ,  'KSVertexCHI2_LL'    : 20.25
+        ,  'KSdaughterPiIPCHI2_LL' : 4.
+        ,  'BFlightCHI2_DD'     : 81.
+        ,  'BDIRA_DD'           : 0.9998    
+        ,  'BIPCHI2_DD'         : 25.         
+        ,  'BVertexCHI2_DD'     : 12.25
+        ,  'BachKIPCHI2_DD'     : 4.
+        ,  'BachKPt_DD'         : 0.4  
+        ,  'DVertexCHI2_DD'     : 16.        
+        ,  'DdaughterPiIPCHI2_DD' : 4.
+        ,  'KSFlightCHI2_DD'    : 4.
+        ,  'KSVertexCHI2_DD'    : 20.25
+        ,  'KSdaughterPiIPCHI2_DD' : 4.
+        }
 
-
-#######################################
-# KS, downstream downstream daughters #
-#######################################
-KSDDForBu2D0K_D02KSPiPi = FilterDesktop("StripKSDDForBu2D0K_D02KSPiPi")
-KSDDForBu2D0K_D02KSPiPi.InputLocations = ["StdLooseKsDD"]
-KSDDForBu2D0K_D02KSPiPi.Code = "(MINTREE( ('pi+'==ABSID), P) >2.*GeV)"\
-                             "& (MINTREE( ('pi+'==ABSID), MIPDV(PRIMARY))>0.8)"\
-                             "& (MAXTREE( ('pi+'==ABSID), TRCHI2DOF) <7.)"\
-                             "& (MINTREE( ('pi+'==ABSID), PIDp) >-1000.)"\
-                             "& (MINTREE( ('pi+'==ABSID), PIDK) >-1000.)"\
-                             "& (VFASPF(VCHI2/VDOF)<17.5)"\
-                             "& (BPVVD> 175.)"\
-                             "& (ADMASS('KS0') < 64.*MeV)"\
-                             "& (2==NINGENERATION(('pi+'==ABSID)&((PIDp < -1e-10)|(PIDp > 1e-10)),1))"\
-                             "& (2==NINGENERATION(('pi+'==ABSID)&((PIDK < -1e-10)|(PIDK > 1e-10)),1))"
-
-
-###########################
-# D0, with KS LL daughter #
-###########################
-DForBu2D0K_D02KSPiPi_KSLL = CombineParticles("StripDForBu2D0K_D02KSPiPi_KSLL")
-DForBu2D0K_D02KSPiPi_KSLL.DecayDescriptor = "[D0 -> KS0 pi+ pi-]cc"
-
-DForBu2D0K_D02KSPiPi_KSLL.InputLocations =  ["StdLoosePions",
-                                             "StripKSLLForBu2D0K_D02KSPiPi"
-                                             ]
-
-DForBu2D0K_D02KSPiPi_KSLL.DaughtersCuts  = {"pi+" : "(PT>350.*MeV) & ((PIDK <-1e-10)|(PIDK >1e-10)) & ((PIDp <-1e-10)|(PIDp > 1e-10)) & (PIDK > -1000.0) & (PIDp > -1000.0) & (TRCHI2DOF<7.)" 
-                                           ,"KS0" : "(PT > 1.*GeV)"
-                                           }
-DForBu2D0K_D02KSPiPi_KSLL.CombinationCut =  "(ADAMASS('D0')<100.*MeV) & (APT>900.*MeV)" 
-DForBu2D0K_D02KSPiPi_KSLL.MotherCut      =  "(ADMASS('D0')<80.*MeV) & (VFASPF(VCHI2/VDOF)<9.) & (PT>1.8*GeV) & (MIPDV(PRIMARY)>0.05)"
+    _propertyDocDct = {
+        'BFlightCHI2_LL'     : """ LL B flight distance chi2 cut """   
+        , 'BDIRA_LL'            : """ LL B cos angle cut         """
+        ,  'BIPCHI2_LL'         : """ LL B IP chi2 cut         """       
+        ,  'BVertexCHI2_LL'     : """ LL B vertex chi2 cut         """
+        ,  'BachKIPCHI2_LL'     : """ LL Bachelor kaon IP chi2 cut         """
+        ,  'BachKPt_LL'         : """ LL Bachelor kaon Pt cut         """
+        ,  'DVertexCHI2_LL'     : """ LL D0 vertex chi2 cut         """
+        ,  'DdaughterPiIPCHI2_LL'     : """ LL D daughter pion IP chi2 cut         """
+        ,  'KSFlightCHI2_LL'    : """ LL KS flight distance chi2 cut     """
+        ,  'KSVertexCHI2_LL'    : """ LL KS vertex chi2 cut     """
+        ,  'KSdaughterPiIPCHI2_LL'    : """ LL KS daughter pion IP chi2 cut         """
+        ,  'BFlightCHI2_DD'     : """ DD B flight distance chi2 cut """   
+        ,  'BDIRA_DD'           : """ DD B cos angle cut         """
+        ,  'BIPCHI2_DD'         : """ DD B IP chi2 cut         """       
+        ,  'BVertexCHI2_DD'     : """ DD B vertex chi2 cut         """
+        ,  'BachKIPCHI2_DD'     : """ DD Bachelor kaon IP chi2 cut         """
+        ,  'BachKPt_DD'         : """ DD Bachelor kaon Pt cut         """
+        ,  'DVertexCHI2_DD'     : """ DD D0 vertex chi2 cut         """
+        ,  'DdaughterPiIPCHI2_DD'     : """ DD D daughter pion IP chi2 cut         """
+        ,  'KSFlightCHI2_DD'    : """ DD KS flight distance chi2 cut     """
+        ,  'KSVertexCHI2_DD'    : """ DD KS vertex chi2 cut     """
+        ,  'KSdaughterPiIPCHI2_DD'    : """ DD KS daughter pion IP chi2 cut         """
 
 
+        }
 
-###########################
-# D0, with KS DD daughter #
-###########################
-DForBu2D0K_D02KSPiPi_KSDD = CombineParticles("StripDForBu2D0K_D02KSPiPi_KSDD")
-DForBu2D0K_D02KSPiPi_KSDD.DecayDescriptor = "[D0 -> KS0 pi+ pi-]cc"
-
-DForBu2D0K_D02KSPiPi_KSDD.InputLocations =  ["StdLoosePions",
-                                            "StripKSDDForBu2D0K_D02KSPiPi"
-                                            ]
-
-DForBu2D0K_D02KSPiPi_KSDD.DaughtersCuts  = {"pi+" : "(PT>350.*MeV) & ((PIDK <-1e-10)|(PIDK >1e-10)) & ((PIDp <-1e-10)|(PIDp > 1e-10)) & (PIDK > -1000.0) & (PIDp > -1000.0) & (TRCHI2DOF<7.)" 
-                                           ,"KS0" : "(PT > 1.*GeV)"
-                                           }
-DForBu2D0K_D02KSPiPi_KSDD.CombinationCut =  "(ADAMASS('D0')<100.*MeV) & (APT>900.*MeV)" 
-DForBu2D0K_D02KSPiPi_KSDD.MotherCut      =  "(ADMASS('D0')<80.*MeV) & (VFASPF(VCHI2/VDOF)<9.) & (PT>1.8*GeV) & (MIPDV(PRIMARY)>0.05)"
-
-
-################
-# B, LL KS     #
-################
-Bu2D0K_KSLL = CombineParticles("StripBu2D0K_KSLL")
-Bu2D0K_KSLL.DecayDescriptor = "[B+ -> D~0 K+]cc"
-Bu2D0K_KSLL.InputLocations = ["StripDForBu2D0K_D02KSPiPi_KSLL",
-                              "StdLooseKaons"]
-
-
-Bu2D0K_KSLL.DaughtersCuts = { "D0" : "ALL",
-                              "K+" : "(PT>0.6*GeV) & (MIPDV(PRIMARY)>0.05) & ((PIDK < -1e-10)|(PIDK > 1e-10)) & ((PIDp < -1e-10)|(PIDp > 1e-10)) & (PIDK > -1000.0) & (PIDp > -1000.0)  & (TRCHI2DOF<7.)"
-                            }
- 
-Bu2D0K_KSLL.CombinationCut =  "(ADAMASS('B+')<150.*MeV)"
-Bu2D0K_KSLL.MotherCut = "(ADMASS('B+')<130.*MeV)  & (VFASPF(VCHI2/VDOF)<9.) & (BPVDIRA>0.99975) & (MIPDV(PRIMARY)< 0.1) & (BPVVD>1.7)"
+    ###############################################
+    # Create StrippingLines with these selections #
+    ###############################################
+    def Bu2D0K_KSLL( self ) :
+        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
+        LLPionFilterForBu2D0K_D02KSPiPi = self.LLPionFilterForBu2D0K_D02KSPiPiAlg()
+        KSLLForBu2D0K_D02KSPiPi = self.KSLLForBu2D0K_D02KSPiPiAlg()
+        DForBu2D0K_D02KSPiPi_KSLL = self.DForBu2D0K_D02KSPiPi_KSLLAlg()
+        Bu2D0K_KSLL = self.Bu2D0K_KSLLAlg()
+        return StrippingLine('Bu2D0K_D02KSPiPi_KSLL'
+                             , prescale = 1
+                             , algos = [LLPionFilterForBu2D0K_D02KSPiPi, KSLLForBu2D0K_D02KSPiPi, DForBu2D0K_D02KSPiPi_KSLL, Bu2D0K_KSLL]
+                             , postscale = 1
+                             )
+    
+    def Bu2D0K_KSDD( self ) :
+        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
+        DDPionFilterForBu2D0K_D02KSPiPi = self.DDPionFilterForBu2D0K_D02KSPiPiAlg()
+        KSDDForBu2D0K_D02KSPiPi = self.KSDDForBu2D0K_D02KSPiPiAlg()
+        DForBu2D0K_D02KSPiPi_KSDD = self.DForBu2D0K_D02KSPiPi_KSDDAlg()
+        Bu2D0K_KSDD = self.Bu2D0K_KSDDAlg()
+        return StrippingLine('Bu2D0K_D02KSPiPi_KSDD'
+                             , prescale = 1
+                             , algos = [DDPionFilterForBu2D0K_D02KSPiPi, KSDDForBu2D0K_D02KSPiPi, DForBu2D0K_D02KSPiPi_KSDD, Bu2D0K_KSDD]
+                             , postscale = 1
+                             )
 
 
-################
-# B, DD KS     #
-################
-Bu2D0K_KSDD = CombineParticles("StripBu2D0K_KSDD")
-Bu2D0K_KSDD.DecayDescriptor = "[B+ -> D~0 K+]cc"
-Bu2D0K_KSDD.InputLocations = ["StripDForBu2D0K_D02KSPiPi_KSDD",
-                             "StdLooseKaons"]
+
+        ###########################
+        # KS daughters, long long # 
+        ###########################
+    def LLPionFilterForBu2D0K_D02KSPiPiAlg(self):
+        from Configurables import FilterDesktop
+        import GaudiKernel.SystemOfUnits as Units
+
+        LLPi_FilterCut = "(BPVIPCHI2() > %(KSdaughterPiIPCHI2_LL)s) & (P < 100.*GeV)"% self.getProps() 
+        
+        LLPionFilterForBu2D0K_D02KSPiPi = FilterDesktop("StripLLPionFilterForBu2D0K_D02KSPiPi")
+        LLPionFilterForBu2D0K_D02KSPiPi.InputLocations = ["StdLoosePions"]
+        LLPionFilterForBu2D0K_D02KSPiPi.Code = LLPi_FilterCut
+        return LLPionFilterForBu2D0K_D02KSPiPi
+    
+        #######################################
+        # KS daughters, downstream downstream #
+        #######################################
+    def DDPionFilterForBu2D0K_D02KSPiPiAlg(self):
+        from Configurables import FilterDesktop
+        import GaudiKernel.SystemOfUnits as Units
+
+        DDPi_FilterCut = "(ISDOWN) & (BPVIPCHI2() > %(KSdaughterPiIPCHI2_DD)s) & (P < 100.*GeV) & ((-PIDK) > 0.)"% self.getProps()
+        
+        DDPionFilterForBu2D0K_D02KSPiPi = FilterDesktop("StripDDPionFilterForBu2D0K_D02KSPiPi")
+        DDPionFilterForBu2D0K_D02KSPiPi.InputLocations = ["StdNoPIDsDownPions"]
+        DDPionFilterForBu2D0K_D02KSPiPi.Code = DDPi_FilterCut
+        return DDPionFilterForBu2D0K_D02KSPiPi
+        
+        ###########################
+        # KS, long long daughters #
+        ###########################
+    def KSLLForBu2D0K_D02KSPiPiAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units
+
+        KSLL_CombCut = "(ADAMASS('KS0') < 40.*MeV)"
+        KSLL_MotherCut = "(ADMASS('KS0') < 30.*MeV) & (VFASPF(VCHI2/VDOF) < %(KSVertexCHI2_LL)s) & (BPVVDCHI2 > %(KSFlightCHI2_LL)s)"% self.getProps()
+
+        KSLLForBu2D0K_D02KSPiPi = CombineParticles("StripKSLLForBu2D0K_D02KSPiPi")
+        KSLLForBu2D0K_D02KSPiPi.InputLocations = ["StripLLPionFilterForBu2D0K_D02KSPiPi"]
+        KSLLForBu2D0K_D02KSPiPi.DecayDescriptor = "KS0 -> pi+ pi-"
+        KSLLForBu2D0K_D02KSPiPi.DaughtersCuts = {"pi+": "ALL"}
+        KSLLForBu2D0K_D02KSPiPi.CombinationCut = KSLL_CombCut
+        KSLLForBu2D0K_D02KSPiPi.MotherCut = KSLL_MotherCut
+        return KSLLForBu2D0K_D02KSPiPi
+        
+        #######################################
+        # KS, downstream downstream daughters #
+        #######################################
+    def KSDDForBu2D0K_D02KSPiPiAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units
+
+        KSDD_CombCut = "(ADAMASS('KS0') < 50.*MeV)"
+        KSDD_MotherCut = "(ADMASS('KS0') < 42.*MeV) & (VFASPF(VCHI2/VDOF) < %(KSVertexCHI2_DD)s) & (BPVVDCHI2 > %(KSFlightCHI2_DD)s)"% self.getProps()
+        
+        KSDDForBu2D0K_D02KSPiPi = CombineParticles("StripKSDDForBu2D0K_D02KSPiPi")
+        KSDDForBu2D0K_D02KSPiPi.InputLocations = ["StripDDPionFilterForBu2D0K_D02KSPiPi"]
+        KSDDForBu2D0K_D02KSPiPi.DecayDescriptor = "KS0 -> pi+ pi-" 
+        KSDDForBu2D0K_D02KSPiPi.DaughtersCuts = {"pi+" : "ALL"}
+        KSDDForBu2D0K_D02KSPiPi.CombinationCut = KSDD_CombCut
+        KSDDForBu2D0K_D02KSPiPi.MotherCut = KSDD_MotherCut
+        return KSDDForBu2D0K_D02KSPiPi
+
+        ###########################
+        # D0, with KS LL daughter #
+        ###########################
+    def DForBu2D0K_D02KSPiPi_KSLLAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units
+
+        D_LL_DaughterPiCut = "(BPVIPCHI2() > %(DdaughterPiIPCHI2_LL)s) & (P < 100.*GeV) & ((-PIDK) > -11.)"% self.getProps()
+        D_LL_CombCut = "(ADAMASS('D0') < 60.*MeV)" 
+        D_LL_MotherCut = "(ADMASS('D0') < 50.*MeV) & (VFASPF(VCHI2/VDOF) < %(DVertexCHI2_LL)s)"% self.getProps()
+        
+        DForBu2D0K_D02KSPiPi_KSLL = CombineParticles("StripDForBu2D0K_D02KSPiPi_KSLL")
+        DForBu2D0K_D02KSPiPi_KSLL.DecayDescriptor = "[D0 -> KS0 pi+ pi-]cc"
+        DForBu2D0K_D02KSPiPi_KSLL.InputLocations =  ["StdLoosePions",
+                                                     "StripKSLLForBu2D0K_D02KSPiPi"
+                                                     ]
+
+        DForBu2D0K_D02KSPiPi_KSLL.DaughtersCuts  = {"pi+" : D_LL_DaughterPiCut
+                                                   ,"KS0" : "ALL"
+                                                   }
+        DForBu2D0K_D02KSPiPi_KSLL.CombinationCut =  D_LL_CombCut
+        DForBu2D0K_D02KSPiPi_KSLL.MotherCut      =  D_LL_MotherCut
+        return DForBu2D0K_D02KSPiPi_KSLL
 
 
-Bu2D0K_KSDD.DaughtersCuts = { "D0" : "ALL",
-                              "K+" : "(PT>0.6*GeV) & (MIPDV(PRIMARY)>0.05) & ((PIDK < -1e-10)|(PIDK > 1e-10)) & ((PIDp < -1e-10)|(PIDp > 1e-10)) & (PIDK > -1000.0) & (PIDp > -1000.0)  & (TRCHI2DOF<7.)"
-                           }
- 
-Bu2D0K_KSDD.CombinationCut =  "(ADAMASS('B+')<160.*MeV)"
-Bu2D0K_KSDD.MotherCut = "(ADMASS('B+')<145.*MeV)  & (VFASPF(VCHI2/VDOF)<9.) & (BPVDIRA>0.99975) & (MIPDV(PRIMARY)< 0.1) & (BPVVD>1.)"
 
 
-############################################
-# Create StrippingLine with this selection #
-############################################
-line1 = StrippingLine('Bu2D0K_D02KSPiPi_KSLL'
-               , prescale = 1
-               , algos = [KSLLForBu2D0K_D02KSPiPi, DForBu2D0K_D02KSPiPi_KSLL, Bu2D0K_KSLL]
-               , postscale = 1
-               )
+        ###########################
+        # D0, with KS DD daughter #
+        ###########################
+    def DForBu2D0K_D02KSPiPi_KSDDAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units
 
-line2 = StrippingLine('Bu2D0K_D02KSPiPi_KSDD'
-               , prescale = 1
-               , algos = [KSDDForBu2D0K_D02KSPiPi, DForBu2D0K_D02KSPiPi_KSDD, Bu2D0K_KSDD]
-               , postscale = 1
-               )
+        D_DD_DaughterPiCut = "(BPVIPCHI2() > %(DdaughterPiIPCHI2_DD)s) & (P < 100.*GeV) & ((-PIDK) > 0.)"% self.getProps()
+        D_DD_CombCut = "(ADAMASS('D0') < 70.*MeV)" 
+        D_DD_MotherCut = "(ADMASS('D0') < 60.*MeV) & (VFASPF(VCHI2/VDOF) < %(DVertexCHI2_DD)s)"% self.getProps()
+        
+        DForBu2D0K_D02KSPiPi_KSDD = CombineParticles("StripDForBu2D0K_D02KSPiPi_KSDD")
+        DForBu2D0K_D02KSPiPi_KSDD.DecayDescriptor = "[D0 -> KS0 pi+ pi-]cc"
+        DForBu2D0K_D02KSPiPi_KSDD.InputLocations =  ["StdLoosePions",
+                                                    "StripKSDDForBu2D0K_D02KSPiPi"
+                                                    ]
+
+        DForBu2D0K_D02KSPiPi_KSDD.DaughtersCuts  = {"pi+" : D_DD_DaughterPiCut 
+                                                   ,"KS0" : "ALL"
+                                                   }
+        DForBu2D0K_D02KSPiPi_KSDD.CombinationCut =  D_DD_CombCut
+        DForBu2D0K_D02KSPiPi_KSDD.MotherCut      =  D_DD_MotherCut
+        return DForBu2D0K_D02KSPiPi_KSDD
+
+        ################
+        # B, LL KS     #
+        ################
+    def Bu2D0K_KSLLAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units    
+
+        B_LL_BachKCut = "(PT > %(BachKPt_LL)s *GeV ) & (BPVIPCHI2() > %(BachKIPCHI2_LL)s ) & (P < 100.*GeV) & (PIDK > -4.) & ((PIDK - PIDp) > -11.)"% self.getProps()
+        B_LL_CombCut = "(ADAMASS('B+') < 600.*MeV)"
+        B_LL_MotherCut = "(ADMASS('B+') < 500.*MeV)  & (VFASPF(VCHI2/VDOF)<%(BVertexCHI2_LL)s) & (BPVIPCHI2() < %(BIPCHI2_LL)s)  & (BPVDIRA > %(BDIRA_LL)s) & (BPVVDCHI2 > %(BFlightCHI2_LL)s)" % self.getProps()
+
+        Bu2D0K_KSLL = CombineParticles("StripBu2D0K_KSLL")
+        Bu2D0K_KSLL.DecayDescriptor = "[B+ -> D~0 K+]cc"
+        Bu2D0K_KSLL.InputLocations = ["StripDForBu2D0K_D02KSPiPi_KSLL",
+                                      "StdLooseKaons"]
+
+        Bu2D0K_KSLL.DaughtersCuts = { "D0" : "ALL",
+                                      "K+" : B_LL_BachKCut
+                                    }
+        Bu2D0K_KSLL.CombinationCut =  B_LL_CombCut
+        Bu2D0K_KSLL.MotherCut = B_LL_MotherCut 
+        return Bu2D0K_KSLL
+
+
+        ################
+        # B, DD KS     #
+        ################
+    def Bu2D0K_KSDDAlg(self):
+        from Configurables import CombineParticles
+        import GaudiKernel.SystemOfUnits as Units
+
+        B_DD_BachKCut = "(PT > %(BachKPt_DD)s *GeV) & (BPVIPCHI2() > %(BachKIPCHI2_DD)s) & (P < 100.*GeV) & (PIDK > -2.5) & ((PIDK - PIDp) > -7.5)"% self.getProps()
+        B_DD_CombCut = "(ADAMASS('B+') < 600.*MeV)"
+        B_DD_MotherCut = "(ADMASS('B+') < 500.*MeV)  & (VFASPF(VCHI2/VDOF) < %(BVertexCHI2_DD)s) & (BPVIPCHI2() < %(BIPCHI2_DD)s) & (BPVDIRA > %(BDIRA_DD)s) & (BPVVDCHI2 > %(BFlightCHI2_DD)s)"% self.getProps()
+
+
+        Bu2D0K_KSDD = CombineParticles("StripBu2D0K_KSDD")
+        Bu2D0K_KSDD.DecayDescriptor = "[B+ -> D~0 K+]cc"
+        Bu2D0K_KSDD.InputLocations = ["StripDForBu2D0K_D02KSPiPi_KSDD",
+                                     "StdLooseKaons"]
+
+        Bu2D0K_KSDD.DaughtersCuts = { "D0" : "ALL",
+                                      "K+" : B_DD_BachKCut
+                                      }
+        Bu2D0K_KSDD.CombinationCut = B_DD_CombCut
+        Bu2D0K_KSDD.MotherCut = B_DD_MotherCut
+        return Bu2D0K_KSDD
+
+
+
+    def getProps(self) :
+        d = dict()
+        for (k,v) in self.getDefaultProperties().iteritems() :
+            d[k] = getattr(self,k) if hasattr(self,k) else v
+        return d
