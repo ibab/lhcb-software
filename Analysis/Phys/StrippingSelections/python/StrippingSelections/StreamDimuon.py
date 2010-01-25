493acac94c6819e161e247d84837c4d3f5a0d073
Jibo He <jibo.he@cern.ch>
2010-01-25 00:57:19 +0000

    Three news lines added for Bc2JpsiMuX and Bc2JpsiH
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81003 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a805fe0d34da8b871f9e00facf334399d832ec7f

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamDimuon.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StreamDimuon.py
@@ -63,5 +63,5 @@ stream.appendLines( [
 		        StrippingBu2LLKConf().mmK(),
                         StrippingBc2JpsiMuXConf().MuMu(),
                         StrippingBc2JpsiHConf().MuMu(),
-                        StrippingBc2JpsiHDetachedConf(),MuMu()
+                        StrippingBc2JpsiHDetachedConf().MuMu()
 		    ] )
