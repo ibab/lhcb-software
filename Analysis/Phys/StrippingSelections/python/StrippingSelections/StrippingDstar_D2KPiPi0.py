8c92f630f3e9bdc49a9dbd2d211fe6273e29a94c
Andrea Contu <andrea.contu@cern.ch>
2010-01-26 17:09:38 +0000

    Modified sequencers name in StrippingDstar_D2KPiPi0.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81081 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 89356521b14e5cedec6a0b3eb80c4635f92c67c2

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstar_D2KPiPi0.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstar_D2KPiPi0.py
@@ -7,15 +7,15 @@
 #
 ########################################################################                   
 __author__ = 'Andrea Contu'
-__date__ = '22/01/2010'
-__version__ = '$Revision: 1.2 $'
+__date__ = '26/01/2010'
+__version__ = '$Revision: 1.3 $'
 
 from Configurables import GaudiSequencer, CombineParticles
 from StrippingConf.StrippingLine import StrippingLine
 
-seq=GaudiSequencer("seq")
-seqRS=GaudiSequencer("seqRS")
-seqWS=GaudiSequencer("seqWS")
+StrippingDstar_D2KPiPi0seq=GaudiSequencer("StrippingDstar_D2KPiPi0seq")
+StrippingDstar_D2KPiPi0seqRS=GaudiSequencer("StrippingDstar_D2KPiPi0seqRS")
+StrippingDstar_D2KPiPi0seqWS=GaudiSequencer("StrippingDstar_D2KPiPi0seqWS")
 
 #D0 Cuts
 str_D0_ChargedDau="(PT>500*MeV) & (P>5*GeV) & (MIPCHI2DV(PRIMARY)>4) & (TRCHI2DOF<10)"
@@ -39,16 +39,16 @@ D02KPiPi0_RS.DaughtersCuts = { "K-" : str_D0_ChargedDau,
                                     "pi0" : str_D0_Pi0}
 D02KPiPi0_RS.CombinationCut = str_D0_CombCuts
 D02KPiPi0_RS.MotherCut = str_D0_MotherCuts
-seq.Members.append(D02KPiPi0_RS)
+StrippingDstar_D2KPiPi0seq.Members.append(D02KPiPi0_RS)
 
 from Configurables import ConjugateNeutralPID
 #make D0 WS
 D02KPiPi0_WS=ConjugateNeutralPID("D02KPiPi0_WS")
 D02KPiPi0_WS.InputLocations = [ "D02KPiPi0_RS" ]
-seq.Members.append(D02KPiPi0_WS)
+StrippingDstar_D2KPiPi0seq.Members.append(D02KPiPi0_WS)
 
-seqRS.Members.append(seq)
-seqWS.Members.append(seq)
+StrippingDstar_D2KPiPi0seqRS.Members.append(StrippingDstar_D2KPiPi0seq)
+StrippingDstar_D2KPiPi0seqWS.Members.append(StrippingDstar_D2KPiPi0seq)
 #make D* RS
 Strip_DStar2D0Pi_D02KPiPi0_RS = CombineParticles ( "DStar2D0Pi_D02KPiPi0_RS" )
 Strip_DStar2D0Pi_D02KPiPi0_RS.InputLocations = [ "D02KPiPi0_RS", "StdNoPIDsPions" ]
@@ -57,7 +57,7 @@ Strip_DStar2D0Pi_D02KPiPi0_RS.DaughtersCuts = { "D0" : "(ALL)",
                                     "pi+" : str_DStar_SlowPiCuts}
 Strip_DStar2D0Pi_D02KPiPi0_RS.CombinationCut = str_DStar_CombCuts
 Strip_DStar2D0Pi_D02KPiPi0_RS.MotherCut = str_DStar_MotherCuts
-seqRS.Members.append(Strip_DStar2D0Pi_D02KPiPi0_RS)
+StrippingDstar_D2KPiPi0seqRS.Members.append(Strip_DStar2D0Pi_D02KPiPi0_RS)
 
 #make D* WS
 Strip_DStar2D0Pi_D02KPiPi0_WS = CombineParticles ( "DStar2D0Pi_D02KPiPi0_WS" )
@@ -68,15 +68,15 @@ Strip_DStar2D0Pi_D02KPiPi0_WS.DaughtersCuts = { "D0" : "(ALL)",
 Strip_DStar2D0Pi_D02KPiPi0_WS.CombinationCut = str_DStar_CombCuts
 Strip_DStar2D0Pi_D02KPiPi0_WS.MotherCut = str_DStar_MotherCuts
 
-seqWS.Members.append(Strip_DStar2D0Pi_D02KPiPi0_WS)
+StrippingDstar_D2KPiPi0seqWS.Members.append(Strip_DStar2D0Pi_D02KPiPi0_WS)
 
 # Create 2 distinct lines for WS/RS
 lineRS = StrippingLine('Dstar_D2KPiPi0RS_Line'
                , prescale = 1
-               ,  algos = [ seqRS ]
+               ,  algos = [ StrippingDstar_D2KPiPi0seqRS ]
                )
 							 
 lineWS = StrippingLine('Dstar_D2KPiPi0WS_Line'
                , prescale = 1  #if we really need to prescale, prescale this one first
-               ,  algos = [ seqWS ]
+               ,  algos = [ StrippingDstar_D2KPiPi0seqWS ]
                )
