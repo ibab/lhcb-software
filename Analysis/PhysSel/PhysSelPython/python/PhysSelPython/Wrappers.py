9fa15015aaea2792bd9bc0ea14399c88bf2e5dda
jpalac <jpalac@cern.ch>
2010-01-27 16:16:13 +0000

    Allow loading in the absence of Gaudi.Configuration.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81114 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 217ddd9aa8a1506ad97af2af03ae71c46a0cfe5b

--- Analysis/PhysSel/PhysSelPython/python/PhysSelPython/Wrappers.py
+++ Analysis/PhysSel/PhysSelPython/python/PhysSelPython/Wrappers.py
@@ -1,143 +1,47 @@
-#$Id: Wrappers.py,v 1.25 2010-01-27 13:26:23 jpalac Exp $
+#$Id: Wrappers.py,v 1.26 2010-01-27 16:16:13 jpalac Exp $
 """
 Wrapper classes for a DaVinci offline physics selection. The following classes
 are available:
    - Selection          Wraps a selection configurable and the selections it
                         requires
    - DataOnDemand       Wraps a string TES location to make it look like a Seleciton
+   - AutomaticData      Wraps a string TES location to make it look like a Seleciton
    - SelectionSequence  Creates a sequence from a selection such that all the
                         sub-selections required are executed in the right order
 """
 __author__ = "Juan PALACIOS juan.palacios@nikhef.nl"
 
-__all__ = ('DataOnDemand', 'AutomaticData', 'Selection', 'SelectionSequence')
+__all__ = ('DataOnDemand',
+           'AutomaticData',
+           'Selection',
+           'SelectionSequence',
+           'NameError',
+           'SelSequence')
 
-from copy import copy
 
-class DataOnDemand(object) :
+
+__sequencerType = None
+
+try :
+    from Gaudi.Configuation import *
+    from Configurables import GaudiSequencer
+    __sequencerType = GaudiSequencer
+except :
+    print 'WARNING: Gaudi Configurables not found. No default sequencer type defined.'
+
+from selection import ( Selection,
+                        FlatSelectionListBuilder,
+                        DataOnDemand,
+                        AutomaticData,
+                        NameError,
+                        update_overlap,
+                        SelSequence                )
+
+
+def SelectionSequence(*args, **kwargs) :
     """
-    Simple wrapper for a data location. To be used for locations
-    that are guaranteed to be populated. This could be a location
-    on a DST or a location registered to the DataOnDemandSvc.
-    Returns output location via outputLocation() method.
-    Can be used as a Selection in RequiredSelections field of other
-    Selections.
-
-    Example: wrap StdLoosePions
-
-    SelStdLoosePions = DataOnDemand('SelStdLoosePions',
-                                    Location = 'Phys/StdLoosePions')
-    """
-
-    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
-    
-    def __init__ (self,
-                  name,
-                  Location = "", 
-                  RequiredSelections = [] ) :
-        self._name = name
-        self.requiredSelections = []
-        self._location = Location
-
-    def name(self) :
-        return self._name
-
-    def algorithm(self) :
-        return None
-
-    def __apply_configuration__(self) :
-        print self.name(), ".__apply_configuration(): NOT A CONFIGURABLE."
-        
-    def outputLocation(self) :
-        return self._location
-
-    def algName(self) :
-        loc = self.outputLocation()
-        loc = loc[loc.rfind("/")+1:] # grab the last string after the last '/'
-        return loc
-
-AutomaticData = DataOnDemand
-    
-class Selection(object) :
-    """
-    Wrapper class for offline selection. Takes a top selection DVAlgorithm
-    configurable plus a list of required selection configurables. It uses
-    the required selections to set the InputLocations of the top selection.
-    Makes the output location of the data available via outputLocation(),
-    a concatenation of the OutputBranch and Algorithm's name.
-
-    Example: selection for A -> B(bb), C(cc)
-
-    # create and configure selection algorithm configurable:
-    A2B2bbC2cc = CombineParticles('A2B2bbC2cc')
-    A2B2bbC2cc.DecayDescriptor = ...
-    A2B2bbC2cc.MotherCut = ...
-    # now assume SelB and SelC have been defined in modules B2bb and C2cc
-    # respectively
-    from B2bb import SelB
-    from C2cc import SelC
-    # now create a Selection instance using the B and C selections.
-    # Output will go to 'Phys' + A2B2bbC2cc.name()
-    from PhysSelPython.Wrappers import Selection
-    SelA2B2bbC2cc = Selection('SelA2B2bbC2cc',
-                              Algorithm = A2B2bbC2cc,
-                              RequiredSelections = [SelB, SelC],
-                              OutputBranch = 'Phys')
-    """
-    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
-
-    __used_names = []
-
-    def __init__(self,
-                 name,
-                 Algorithm,
-                 RequiredSelections = [],
-                 OutputBranch = "Phys") :
-
-        if name in Selection.__used_names :
-            raise NameError('Selection name ' + name + ' has already been used. Pick a new one.')
-        Selection.__used_names.append(name)
-        self.__ctor_dict__ = copy(locals())
-        del self.__ctor_dict__['self']
-        del self.__ctor_dict__['name']
-        
-        self.requiredSelections = []
-        print "Instantiating Selection", name
-        for sel in RequiredSelections :
-            print "Selection: Adding Required Selection ", sel.name()
-            self.requiredSelections.append(sel)
-        self._name = name
-        self.alg = Algorithm.clone(self._name, InputLocations = [])
-        print "Selection: cloned", type(self.alg) , Algorithm.name(), "to", self.alg.name()
-        self._outputBranch = OutputBranch
-        
-        for sel in self.requiredSelections :
-            print "\tAlgo ", self.algName(),  ": adding InputLocation ", sel.outputLocation()
-            self.algorithm().InputLocations += [sel.outputLocation()]
-        print self._name, "Required Selection Algorithms: ", self.requiredSelections
-
-    def name(self) :
-        return self._name
-
-    def __apply_configuration__(self) :
-        print self.name(), ".__apply_configuration(): NOT A CONFIGURABLE."
-
-    def algorithm(self) :
-        return self.alg
-
-    def algName(self) :
-        return self.algorithm().name()
-
-    def outputLocation(self) :
-        return self._outputBranch + "/" + self.algName()
-
-    def clone(self, name, **args) :
-        new_dict = update_overlap(self.__ctor_dict__, args)
-        return Selection(name, **new_dict)
-    
-class SelectionSequence(object) :
-    """
-    Wrapper class for offline selection sequence. Takes a Selection object
+    Wrapper function for offline selection sequence creation.
+    Takes a Selection object
     corresponding to the top selection algorithm, and recursively uses
     Selection.requiredSelections to for a GaudiSequences with all the required
     selecitons needed to run the top selection. Can add list of event selection
@@ -159,84 +63,11 @@ class SelectionSequence(object) :
     mySelSeq = SeqA2B2bbC2cc.sequence()
     dv = DaVinci()
     dv.UserAlgorithms = [mySelSeq]
+
+    Uses selection.SelSequence and selection.FlatSelectionListBuilder
     """
     __author__ = "Juan Palacios juan.palacios@nikhef.nl"
 
-    from Configurables import GaudiSequencer
-
-    __used_names = []
-
-    def __init__(self,
-                 name,
-                 TopSelection,
-                 EventPreSelector = [],
-                 PostSelectionAlgs = [],
-                 SequencerType = GaudiSequencer) :
-
-        if name in SelectionSequence.__used_names :
-            raise NameError('SelectionSequence name ' + name + ' has already been used. Pick a new one.')
-        SelectionSequence.__used_names.append(name)
-        self.__ctor_dict__ = copy(locals())
-        del self.__ctor_dict__['self']
-        del self.__ctor_dict__['name']
-
-        self._name = name
-        self._topSelection = TopSelection
-        self._seqType = SequencerType
-        self.algos = copy(EventPreSelector)
-        self.alg = self._topSelection.algorithm()
-        self.sels = [self.alg]
-        if (self.alg != None) :
-            self.buildSelectionList( self._topSelection.requiredSelections )
-        self.gaudiseq = None
-        self.algos += self.sels
-        self.algos += PostSelectionAlgs
-
-        
-    def name(self) :
-        return self._name
-
-    def __apply_configuration__(self) :
-        print self.name(), ".__apply_configuration(): NOT A CONFIGURABLE."
-
-    def algorithm(self) :
-        return self.alg
-        
-    def sequence(self) :
-        if self.gaudiseq == None :
-            self.gaudiseq = self._seqType(self.name(), Members = self.algos)
-        return self.gaudiseq
-
-    def algName(self) :
-        return self.algorithm().name()
-
-    def outputLocation(self) :
-        return self._topSelection.outputLocation()
-
-    def outputLocations(self) :
-        return [self.outputLocation()]
-
-    def buildSelectionList(self, selections) :
-        for sel in selections :
-            if type(sel) == DataOnDemand :
-                print "DataOnDemand: do nothing"
-            else :
-                self.sels.insert(0, sel.algorithm())
-                self.buildSelectionList( sel.requiredSelections )
-
-    def clone(self, name, **args) :
-        new_dict = update_overlap(self.__ctor_dict__, args)
-        return SelectionSequence(name, **new_dict)
-    
-def update_overlap(dict0, dict1) :
-    '''
-    Replace entries from dict0 with those from dict1 that have
-    keys present in dict0.
-    '''
-    overlap_keys = filter(dict1.has_key, dict0.keys())
-    result = copy(dict0)
-    for key in overlap_keys : result[key] = dict1[key]
-    return result
-
-class NameError(Exception) :
-    pass
+    if not 'SequencerType' in kwargs.keys() :
+        kwargs['SequencerType'] = __sequencerType
+    return SelSequence(*args, **kwargs)
