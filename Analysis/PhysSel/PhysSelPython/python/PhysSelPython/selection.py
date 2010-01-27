ce7b8f0fb5b8d69f66585cc772dc88bc01cd5c44
jpalac <jpalac@cern.ch>
2010-01-27 15:02:51 +0000

    All important fragments foe Selection classes moved here from Wrappers.py.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81109 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 06c12c5fb316fb620b31b026d82a9880d9fb4514

---
+++ Analysis/PhysSel/PhysSelPython/python/PhysSelPython/selection.py
@@ -0,0 +1,208 @@
+#$Id: selection.py,v 1.1 2010-01-27 15:02:51 jpalac Exp $
+"""
+Classes for a DaVinci offline physics selection. The following classes
+are available:
+   - Selection          Wraps a selection configurable and the selections it
+                        requires
+   - DataOnDemand       Wraps a string TES location to make it look like a Seleciton
+   - SelectionSequence  Creates a sequence from a selection such that all the
+                        sub-selections required are executed in the right order
+"""
+__author__ = "Juan PALACIOS juan.palacios@nikhef.nl"
+
+__all__ = ('DataOnDemand',
+           'AutomaticData',
+           'Selection',
+           'SelectionSequenceBuilder',
+           'NameError',
+           'update_overlap')
+
+from copy import copy
+
+class DataOnDemand(object) :
+    """
+    Simple wrapper for a data location. To be used for locations
+    that are guaranteed to be populated. This could be a location
+    on a DST or a location registered to the DataOnDemandSvc.
+    Returns output location via outputLocation() method.
+    Can be used as a Selection in RequiredSelections field of other
+    Selections.
+
+    Example: wrap StdLoosePions
+
+    SelStdLoosePions = DataOnDemand('SelStdLoosePions',
+                                    Location = 'Phys/StdLoosePions')
+    """
+
+    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
+    
+    def __init__ (self,
+                  name,
+                  Location = "", 
+                  RequiredSelections = [] ) :
+        self._name = name
+        self.requiredSelections = []
+        self._location = Location
+
+    def name(self) :
+        return self._name
+
+    def algorithm(self) :
+        return None
+
+    def __apply_configuration__(self) :
+        print self.name(), ".__apply_configuration(): NOT A CONFIGURABLE."
+        
+    def outputLocation(self) :
+        return self._location
+
+    def algName(self) :
+        loc = self.outputLocation()
+        loc = loc[loc.rfind("/")+1:] # grab the last string after the last '/'
+        return loc
+
+AutomaticData = DataOnDemand
+    
+class Selection(object) :
+    """
+    Wrapper class for offline selection. Takes a top selection DVAlgorithm
+    configurable plus a list of required selection configurables. It uses
+    the required selections to set the InputLocations of the top selection.
+    Makes the output location of the data available via outputLocation(),
+    a concatenation of the OutputBranch and Algorithm's name.
+
+    Example: selection for A -> B(bb), C(cc)
+
+    # create and configure selection algorithm configurable:
+    A2B2bbC2cc = CombineParticles('A2B2bbC2cc')
+    A2B2bbC2cc.DecayDescriptor = ...
+    A2B2bbC2cc.MotherCut = ...
+    # now assume SelB and SelC have been defined in modules B2bb and C2cc
+    # respectively
+    from B2bb import SelB
+    from C2cc import SelC
+    # now create a Selection instance using the B and C selections.
+    # Output will go to 'Phys' + A2B2bbC2cc.name()
+    from PhysSelPython.Wrappers import Selection
+    SelA2B2bbC2cc = Selection('SelA2B2bbC2cc',
+                              Algorithm = A2B2bbC2cc,
+                              RequiredSelections = [SelB, SelC],
+                              OutputBranch = 'Phys')
+    """
+    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
+
+    __used_names = []
+
+    def __init__(self,
+                 name,
+                 Algorithm,
+                 RequiredSelections = [],
+                 OutputBranch = "Phys") :
+
+        if name in Selection.__used_names :
+            raise NameError('Selection name ' + name + ' has already been used. Pick a new one.')
+        Selection.__used_names.append(name)
+        self.__ctor_dict__ = copy(locals())
+        del self.__ctor_dict__['self']
+        del self.__ctor_dict__['name']
+        
+        self.requiredSelections = []
+        print "Instantiating Selection", name
+        for sel in RequiredSelections :
+            print "Selection: Adding Required Selection ", sel.name()
+            self.requiredSelections.append(sel)
+        self._name = name
+        self.alg = Algorithm.clone(self._name, InputLocations = [])
+        print "Selection: cloned", type(self.alg) , Algorithm.name(), "to", self.alg.name()
+        self._outputBranch = OutputBranch
+        
+        for sel in self.requiredSelections :
+            print "\tAlgo ", self.algName(),  ": adding InputLocation ", sel.outputLocation()
+            self.algorithm().InputLocations += [sel.outputLocation()]
+        print self._name, "Required Selection Algorithms: ", self.requiredSelections
+
+    def name(self) :
+        return self._name
+
+    def __apply_configuration__(self) :
+        print self.name(), ".__apply_configuration(): NOT A CONFIGURABLE."
+
+    def algorithm(self) :
+        return self.alg
+
+    def algName(self) :
+        return self.algorithm().name()
+
+    def outputLocation(self) :
+        return self._outputBranch + "/" + self.algName()
+
+    def clone(self, name, **args) :
+        new_dict = update_overlap(self.__ctor_dict__, args)
+        return Selection(name, **new_dict)
+    
+class SelectionSequenceBuilder(object) :
+    """
+    To buils a flat selection sequence. Takes a Selection object
+    corresponding to the top selection algorithm, and recursively uses
+    Selection.requiredSelections to make a flat list with all the required
+    selecitons needed to run the top selection. Can add list of event selection
+    algorithms to be added at the beginning of the sequence, and a list of
+    algorithms to be run straight after the selection algoritms.
+
+    Example: selection sequence for A -> B(bb), C(cc). Add pre-selectors alg0
+             and alg1, and counter counter0.
+
+    # Assume module A2B2bbC2cc defining a Selection object for the decay
+    # A -> B(bb), C(cc)
+    from A2B2bbC2cc import SelA2B2bbC2cc
+    from PhysSelPython.Wrappers import SelectionSequence
+    SeqA2B2bbC2cc = SelectionSequence( 'SeqA2B2bbC2cc',
+                                       TopSelection = SelA2B2bbC2cc,
+                                       EventPreSelector = [alg0, alg1],
+                                       PostSelectionAlgs = [counter0]   )
+    # use it
+    mySelList = SeqA2B2bbC2cc.flatSelectionList()
+    print mySelList
+    """
+    __author__ = "Juan Palacios juan.palacios@nikhef.nl"
+
+    def __init__(self,
+                 TopSelection,
+                 EventPreSelector = [],
+                 PostSelectionAlgs = []) :
+
+        self._topSelection = TopSelection
+        self.algos = copy(EventPreSelector)
+        self.alg = self._topSelection.algorithm()
+        self.sels = [self.alg]
+        if (self.alg != None) :
+            self.buildSelectionList( self._topSelection.requiredSelections )
+        self.gaudiseq = None
+        self.algos += self.sels
+        self.algos += PostSelectionAlgs
+
+    def flatSelectionList(self) :
+        return self.algos
+
+    def buildSelectionList(self, selections) :
+        for sel in selections :
+            if type(sel) == DataOnDemand :
+                print "DataOnDemand: do nothing"
+            else :
+                self.sels.insert(0, sel.algorithm())
+                self.buildSelectionList( sel.requiredSelections )
+
+
+
+def update_overlap(dict0, dict1) :
+    '''
+    Replace entries from dict0 with those from dict1 that have
+    keys present in dict0.
+    '''
+    overlap_keys = filter(dict1.has_key, dict0.keys())
+    result = copy(dict0)
+    for key in overlap_keys : result[key] = dict1[key]
+    return result
+
+class NameError(Exception) :
+    pass
