d980df5c6998820c8b23652228d15002c712e425
Jaap Panman <panmanj@cern.ch>
2010-01-29 13:38:12 +0000

    add lumi method flag
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81171 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 14545d78111881fd183a33c933723648c874d95d

---
+++ Hlt/Hlt/HltLuminosity/src/LumiFlagMethod.h
@@ -0,0 +1,37 @@
+// $Id: LumiFlagMethod.h,v 1.1 2010-01-29 13:38:12 panmanj Exp $
+#ifndef LUMIFLAGMETHOD_H 
+#define LUMIFLAGMETHOD_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+/** @class LumiFlagMethod LumiFlagMethod.h
+ *  
+ *
+ *  @author Jaap Panman
+ *  @date   2008-07-21
+ */
+class LumiFlagMethod : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  LumiFlagMethod( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~LumiFlagMethod( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+  std::string m_OutputContainerName;
+
+  std::string m_ValueName;
+  std::string m_CounterName;
+  int m_Counter;
+  int m_Value;
+
+private:
+
+};
+#endif // LUMIFLAGMETHOD_H
