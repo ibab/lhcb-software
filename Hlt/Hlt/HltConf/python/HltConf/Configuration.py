7ed4ffd02ab0a6be1b0ee021863c274eb162f7f6
kvervink <kvervink@cern.ch>
2010-01-29 15:31:05 +0000

    Added Hlt2 and Histo of lines per alley
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81177 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 708b5afeb7cfce57459c2b8ee11bfad8aa24e9f1

--- Hlt/Hlt/HltConf/python/HltConf/Configuration.py
+++ Hlt/Hlt/HltConf/python/HltConf/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for HltConf, to be invoked by Moore and DaVinci
 """
-__version__ = "$Id: Configuration.py,v 1.146 2010-01-28 15:43:30 graven Exp $"
+__version__ = "$Id: Configuration.py,v 1.147 2010-01-29 15:31:05 kvervink Exp $"
 __author__  = "Gerhard Raven <Gerhard.Raven@nikhef.nl>"
 
 from os import environ
@@ -298,7 +298,7 @@ class HltConf(LHCbConfigurableUser):
 
 
 ##################################################################################
-    def configureHltMonitoring(self, lines) :
+    def configureHltMonitoring(self, lines1, lines2) :
         """
         Hlt Monitoring
         @todo That's all Hlt1. Shouldn't it go there ?
@@ -306,7 +306,8 @@ class HltConf(LHCbConfigurableUser):
         import re
         ## and tell the monitoring what it should expect..
         from Configurables import HltGlobalMonitor
-        HltGlobalMonitor().Hlt1Decisions = [ i.decision() for i in lines ]
+        HltGlobalMonitor().Hlt1Decisions = [ i.decision() for i in lines1 ]
+        HltGlobalMonitor().Hlt2Decisions = [ i.decision() for i in lines2 ]
        
         # the keys are the Labels for the Histograms in the GUI
         # the values are the Pattern Rules to for the Decisions contributing 
@@ -315,12 +316,14 @@ class HltConf(LHCbConfigurableUser):
                                   ("Velo"       , "Hlt1Velo.*Decision"),
                                   ("XPress"     , "Hlt1.*XPress.*Decision"),
                                   ("Hadron"     , "Hlt1.*Hadron.*Decision"),
-                                  ("SingleMuon" , "Hlt1.*SingleMuon.*Decision"),
+                                  ("SingleMuon" , "Hlt1.*(SingleMuon|MuTrack).*Decision"),
                                   ("DiMuon"     , "Hlt1.*DiMuon.*Decision"),
-                                  ("MuTrack"    , "Hlt1.*MuTrack.*Decision"),
                                   ("Electron"   , "Hlt1.*Electron.*Decision"),
                                   ("Photon"     , "Hlt1.*Pho.*Decision"),
-                                  ("PA"         , "Hlt1(ODIN.*|Tell1Error)Decision"),
+                                  ("PA"         , "Hlt1(ODIN.*|Tell1Error|Incident)Decision"),
+                                  ("BeamGas"      , "Hlt1BeamGas.*Decision"),
+                                  ("MinBias"      , "Hlt1.*Bias.*Decision"),
+
                                   ("Global"     , ".*Global.*"),
                                   ("Other"      , ".*") # add a 'catch all' term to pick up all remaining decisions...
                                 ]
@@ -345,6 +348,46 @@ class HltConf(LHCbConfigurableUser):
         HltGlobalMonitor().DecToGroup  = decision_group_map
         HltGlobalMonitor().GroupLabels = group_labels 
 
+        # For the HLT2
+
+        # the keys are the Labels for the Histograms in the GUI
+        # the values are the Pattern Rules to for the Decisions contributing 
+
+        alley_string_patterns2 = [ ("Topological"    , "Hlt2Topo.*Decision"),
+                                   ("InclusivePhi"   , "Hlt2IncPhi.*Decision"),
+                                   ("SingleMuon"     , "Hlt2(Single|IncMuTrack).*Decision"),
+                                   ("DiMuon"         , "Hlt2.*DiMuon.*Decision"),
+                                   ("B2DX"           , "Hlt2B2D2.*Decision"),
+                                   ("B2XGamma"       , "Hlt2.*Gamma.*Decision"),
+                                   ("B2JpsiX"        , "Hlt2B.*2Jpsi.*Decision"),
+                                   ("B2XPhi"         , "Hlt2B.*2Phi.*Decision"),
+                                   ("B2HH"           , "Hlt2B2HH.*Decision"),
+                                   ("Express"        , "Hlt2(ExpressJPsi|BeamHalo).*Decision"),
+                                   ("Commissioning"  , "Hlt2(PassThrough|Transparent|Forward|DebugEvent).*Decision"),
+                                   ("DisplVertices"  , "Hlt2DisplVertices.*Decision"),
+                                   ("Global"         , ".*Global.*"),
+                                   ("Other"          , ".*") # add a 'catch all' term to pick up all remaining decisions...
+                                  ]
+
+        # prepare compiled regex patterns         
+        # and a list of names for the Labels  
+        patterns2 = []
+        group_labels2 = []
+        for pos in range(len(alley_string_patterns2)):
+          (name, pattern_string) = alley_string_patterns2[pos]
+          patterns2.append(re.compile(pattern_string))
+          group_labels2.append(name) 
+
+        # prepare the group map dictionary 
+        decision_group_map2 = {}
+        for decision_name2 in HltGlobalMonitor().Hlt2Decisions: 
+          for pos in range(len(patterns2)):
+            m = patterns2[pos].match(decision_name2)
+            if m and not decision_name2 in decision_group_map2:
+                decision_group_map2[decision_name2]=pos
+
+        HltGlobalMonitor().DecToGroup2  = decision_group_map2
+        HltGlobalMonitor().GroupLabels2 = group_labels2 
 
         def _disableHistograms(c,filter = lambda x : True) :
             if 'HistoProduce' in c.getDefaultProperties() and filter(c):
@@ -428,7 +471,7 @@ class HltConf(LHCbConfigurableUser):
             for i in hlt1Lines()+hlt2Lines() :
                 i.configurable().AcceptIfSlow = True 
         
-        self.configureHltMonitoring(lines1)
+        self.configureHltMonitoring(lines1, lines2)
         self.configureVertexPersistence( [ i.name() for i in lines1 ] ) # TODO: add lines2...
         self.configureANNSelections()
 
