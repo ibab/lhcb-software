25b4b3eb6961fd0e0fb4f1758b46e83649ee79ae
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-01-30 16:06:04 +0000

    - remove LumiLow from Physics_MinBiasL0_PassThroughHlt_Nov09.py and    Physics_MinBiasL0_PassThroughHlt_09Dec09.py which have been used    for taking data, and didn't include those lines then...  - rename Physics_MinBiasL0_PassThroughHlt_Nov09_with_MiniBias    into Physics_MinBiasL0_PassThroughHlt_Jan10
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81186 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a6e8433195b9cee7eb900a4e2a5ad5b41e8b8876

--- Hlt/Hlt/HltSettings/python/HltSettings/Physics_MinBiasL0_PassThroughHlt_09Dec09.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Physics_MinBiasL0_PassThroughHlt_09Dec09.py
@@ -31,7 +31,6 @@ class Physics_MinBiasL0_PassThroughHlt_09Dec09 :
         return [ 'Hlt1ODINPhysics','Hlt1ODINTechnical','Hlt1Tell1Error'
                , 'Hlt1L0Any', 'Hlt1L0MUON','Hlt1L0CALO'
                , 'Hlt1LumiNoBeam','Hlt1LumiBeamCrossing','Hlt1LumiBeam1','Hlt1LumiBeam2'
-               , 'Hlt1LumiLowNoBeam','Hlt1LumiLowBeamCrossing','Hlt1LumiLowBeam1','Hlt1LumiLowBeam2'
                , 'Hlt1VeloASide','Hlt1VeloCSide'
                ]
 
