0d7cb1e2d64bc7eb74c1117adff61dd7e8389baf
jpalac <jpalac@cern.ch>
2010-02-02 17:30:43 +0000

    Simplify. Only build GaudiSequencer if asked.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81308 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 24ff50e6d92967b4c2a3affc990ef2fc47b2b315

--- Analysis/Phys/DSTWriters/python/DSTWriters/dstwriters.py
+++ Analysis/Phys/DSTWriters/python/DSTWriters/dstwriters.py
@@ -1,42 +1,72 @@
-#$Id: dstwriters.py,v 1.1 2010-02-01 21:06:05 jpalac Exp $
-from Configurables import GaudiSequencer, InputCopyStream
+''' '''
+#$Id: dstwriters.py,v 1.2 2010-02-02 17:30:43 jpalac Exp $
+
+
+__author__ = "Juan PALACIOS juan.palacios@nikhef.nl"
+
+__version__ = '$Revision: 1.2 $'
+
+
+from Configurables import GaudiSequencer, InputCopyStream, OutputStream
+
+from streamconf import OutputStreamConf
 
 from copy import copy
+        
+baseDSTWriterConf = OutputStreamConf(streamType = InputCopyStream)
 
-class OutputStreamConf(object) :
-    pass
+microDSTWriterConf = OutputStreamConf(streamType = OutputStream,
+                                      fileExtension = '.mdst',
+                                      extraItems = ['/Event/DAQ/ODIN#1',
+                                                    '/Event/Rec/Header#1'])
 
-baseDSTWriterConf = OutputStreamConf()
-baseDSTWriterConf.streamType = InputCopyStream
-baseDSTWriterConf.filePrefix = 'Sel'
-baseDSTWriterConf.extraItems = []
+selDSTWriterConf = OutputStreamConf(streamType = InputCopyStream)
 
-class SelectionSequenceDSTWriter(object) :
+class DSTWriterSelectionSequence(object) :
     '''
     Extends a SelectionSequence to write a DST.
+    Takes a SelectionSequence as an input. Other arguments include
+    the output stream configuration and a MicroDSTExtras, which
+    has a list of callables that populate the relevant MicroDST
+    locations plut their output location trunk.
     '''
     def __init__(self,
                  selSequence,
                  outputStreamConfiguration = baseDSTWriterConf,
-                 sequencerType = GaudiSequencer) :
+                 extras = None ) :
 
-        self.seqType = sequencerType
         outputStreamConfiguration.name=selSequence.name()
         stream = outputStream(outputStreamConfiguration)
-        self.selSeq = selSequence.clone("."+selSequence.name(),
-                                        PostSelectionAlgs = [stream])
-    def sequence(self) :
-        return self.selSeq.sequence()
+        self.algos = copy(selSequence.algos)
+        self.name = "DSTWriter"+selSequence.name()
+        self.mainSeq = None
+        if None != extras :
+            for _algs in [x(selSequence) for x in extras] :
+                self.algos += _algs
+            stream.OptItemList += [extras.output]
+        self.algos.append(stream)
+
+    def sequence(self, sequencerType = GaudiSequencer) :
+        if self.mainSeq == None :
+            self.mainSeq = sequencerType(self.name, Members = copy(self.algos))
+        return self.mainSeq
 
 def outputStream(conf) :
-    stream = conf.streamType(_streamName(conf.name),
+    '''
+    Create an output stream object based in a stream configuration input.
+    '''
+    stream = conf.streamType('OStream'+conf.name,
                              OptItemList = conf.extraItems,
-                             Output = _outputFileName(conf.filePrefix + conf.name + '.dst'))
+                             Output = _poolFileName(conf.filePrefix + conf.name + conf.extension))
+    try :
+        stream.TakeOptionalFromTES = True
+    except AttributeError :
+        pass
     return stream
 
-def _streamName(name) :
-    return 'OStream' + name
-
-def _outputFileName(name) :
+def _poolFileName(name) :
+    '''
+    Add necessary elements to simple filename to make it a valid Pool output name.
+    '''
     if name == "" : name = 'Output'
     return "DATAFILE='" + name + "' TYP='POOL_ROOTTREE' OPT='REC' "
