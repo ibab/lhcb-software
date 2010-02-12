58cc9e92fb607cbcbb8ed83ce7c8fd3b8ae17845
Gloria Corti <gloria.corti@cern.ch>
2010-02-12 14:00:30 +0000

    add cpp file to have in library LbAppInit factory
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48644 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 57529d15bd5d8bc220e59f5f5b0baa2a6c096266

---
+++ LHCb/Kernel/LHCbAlgs/src/LbAppInitFactory.cpp
@@ -0,0 +1,11 @@
+// $Id: LbAppInitFactory.cpp,v 1.1 2010-02-12 14:00:30 gcorti Exp $
+// Include files 
+#include "Kernel/LbAppInit.h"
+#include "GaudiKernel/LoadFactoryEntries.h"
+#include "GaudiKernel/AlgFactory.h"
+
+// Put LbAppInit base class factory in this library, to avoid having it in
+// LHCbKernel component library. 
+// It must be present otherwise the class cannot be instanciated
+DECLARE_ALGORITHM_FACTORY( LbAppInit );
+
