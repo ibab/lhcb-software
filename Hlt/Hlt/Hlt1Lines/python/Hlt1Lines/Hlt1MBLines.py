596c997bbe90b1ac6bc023a6d34ea3a834d75a30
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-02-12 20:22:27 +0000

    - rename Hlt1MiniBias and Hlt1NoBias into Hlt1MBMiniBias and Hlt1MBNoBias  - combine Hlt1MiniBias and Hlt1NoBias configuration into a single configurable
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81492 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1bd1706a2d757e8a0d676d9693d4d83c65358797

---
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1MBLines.py
@@ -0,0 +1,42 @@
+##############################
+#     HLT MiniBias Line(s)
+##############################
+
+from HltLine.HltLinesConfigurableUser import *
+from HltLine.HltLine import Hlt1Line as Line
+
+############# start building the MB  line(s)...
+class Hlt1MBLinesConf(HltLinesConfigurableUser) :
+
+    __slots__ = { 'MiniBiasL0Channels'     : ['CALO'] #'Hadron'
+                , 'NoBiasTriggerType'      : 'LumiTrigger'  # ODIN trigger type accepted
+                , 'BXTypes'                : ['NoBeam', 'BeamCrossing','Beam1','Beam2']
+                }
+
+    def __create_nobias_line__(self, BXType):
+        '''
+        returns an Hlt1 "Line" including input and output filter
+        '''
+        return Line ( 'MBNoBias'+BXType
+                    , prescale = self.prescale
+                    , ODIN = '( ODIN_BXTYP == LHCb.ODIN.%s ) & (ODIN_TRGTYP == LHCb.ODIN.%s)' % ( BXType, self.getProp('NoBiasTriggerType') )
+                    , postscale = self.postscale
+                    ) 
+    def __create_minibias_line__(self, BXType):
+        '''
+        returns an Hlt1 "Line" including input and output filter
+        '''
+        return Line ( 'MBMiniBias'+BXType
+                    , prescale = self.prescale
+                    , ODIN = '( ODIN_BXTYP == LHCb.ODIN.%s )' % ( BXType )
+                    , L0DU  = ' | '.join( [ "L0_CHANNEL('%s') "%(x) for x in  self.getProp('MiniBiasL0Channels') ] )
+                    , postscale = self.postscale
+                    ) 
+
+    def __apply_configuration__(self) :
+        '''
+        creates parallel HLT1 Lines for each beam crossing type
+        '''
+        for i in self.getProp('BXTypes') :
+            self.__create_minibias_line__(i)
+            self.__create_nobias_line__(i)
