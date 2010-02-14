ae72d4265eee8df199a06b1200353b0e0d086a03
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-02-14 10:49:22 +0000

    updates to TarFileAccess
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81534 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 57151273c13a05ebfbeb20f5f284b595bbe8e525

--- Hlt/Hlt/HltCommon/src/TarFileAccess.h
+++ Hlt/Hlt/HltCommon/src/TarFileAccess.h
@@ -1,6 +1,7 @@
 #ifndef TARFILEACCESS_H
 #define TARFILEACCESS_H
 #include "GaudiKernel/IFileAccess.h"
+#include "IDirectoryAccess.h"
 #include <string>
 #include <memory>
 #include <iostream>
@@ -8,13 +9,15 @@
 
 namespace TarFileAccess_details { class TarFile; };
 
-class TarFileAccess : public IFileAccess {
+class TarFileAccess : public IFileAccess, IDirectoryAccess {
 public:
     TarFileAccess();
     ~TarFileAccess();
     std::auto_ptr<std::istream> open(const std::string &url);
+    std::vector<std::string> listdir(const std::string &url);
     const std::vector<std::string>& protocols() const;
 private:
+    std::pair<TarFileAccess_details::TarFile*,std::string> resolve(const std::string& url);
     typedef std::map<std::string,TarFileAccess_details::TarFile*> container_t;
     container_t m_tarFiles;
 };
