30919a16a69dbde229718018b59791e5a672a196
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-02-18 16:40:43 +0000

    add example options for 2010 simulation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@61632 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4435d08e0cc826ad4c1a8c4cef5a93c05423e285

---
+++ Boole/Digi/Boole/options/Boole-2010.py
@@ -0,0 +1,15 @@
+##############################################################################
+# File for running Boole with 2010 settings (full Digi output)
+#
+# Syntax is:
+# gaudirun.py Boole-2010.py <someDataFiles>.py
+##############################################################################
+
+from Gaudi.Configuration import importOptions
+
+importOptions("$APPCONFIGOPTS/Boole/Default.py")
+importOptions("$APPCONFIGOPTS/Boole/DataType-2010.py")
+importOptions("$APPCONFIGOPTS/Boole/IgnoreFlatSpillover.py")
+
+importOptions("$APPCONFIGOPTS/L0/L0TCK-0x1810.py")
+importOptions("$APPCONFIGOPTS/L0/L0Muon-FOI-2009.py")
