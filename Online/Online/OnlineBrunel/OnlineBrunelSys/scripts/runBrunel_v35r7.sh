049215a7442374c8961f4c5b46bbae73736ffc82
Markus Frank <frankb@cern.ch>
2010-02-19 16:32:33 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100311 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 980b7ea27c06daa13caa1abaef495561974c801c

---
+++ Online/Online/OnlineBrunel/OnlineBrunelSys/scripts/runBrunel_v35r7.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export UTGID
+export LOGFIFO
+cd `dirname $0`/../cmt
+#. setup.slc4_amd64_gcc34.vars
+. setup.x86_64-slc5-gcc43-opt.vars
+BASE=`cd ..;dirname $PWD`
+
+export PYTHONPATH=/group/online/dataflow/options/${PARTITION}/RECONSTRUCTION:${BASE}/InstallArea/python:${PYTHONPATH}
+export DATAINTERFACE=`python /group/online/dataflow/scripts/getDataInterface.py`
+TASK_CLASS_TYPE=${2}
+TASK_TYPE=${3}
+export TAN_PORT=YES
+export TAN_NODE=${DATAINTERFACE}
+export DIM_DNS_NODE=${4}
+export ONLINETASKS=/group/online/dataflow/templates
+###export SQLITEDBPATH=/dev/shm/SQL
+
+echo "[INFO] +++ Starting BRUNEL ${UTGID} of class ${TASKCLASS} ${TASK_TYPE} with DNS:${DIM_DNS_NODE}"
+exec -a ${UTGID} \
+    $GAUDIONLINEROOT/$CMTCONFIG/Gaudi.exe libGaudiOnline.so OnlineTask \
+    -msgsvc=LHCb::FmcMessageSvc \
+    -tasktype=LHCb::${TASK_CLASS_TYPE}Task \
+    -main=$ONLINETASKS/options/Main.opts \
+    -opt=command="\
+import Gaudi,GaudiKernel.ProcessJobOptions;\
+from Gaudi.Configuration import importOptions;\
+GaudiKernel.ProcessJobOptions.printing_level=999;\
+importOptions('../python/BrunelOnline.py');"
+
