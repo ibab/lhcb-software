8d61a64ca7c9743c0f90a592000522c8f034e764
rlambert <rlambert@cern.ch>
2010-02-23 13:23:23 +0000

    added VeloRecMonitors configurable to control histogramming for online and offline modes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@25882 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7bce1bf671aa8e86d6899d8124388c18299a1674

---
+++ Rec/Velo/VeloRecMonitors/python/VeloRecMonitors/Configuration.py
@@ -0,0 +1,44 @@
+"""
+Configuration of the Velo Monitoring Histograms
+"""
+__version__ = "$Id: Configuration.py,v 1.1 2010-02-23 13:23:23 rlambert Exp $"
+__author__  = "Rob Lambert"
+
+from Gaudi.Configuration import *
+
+class VeloRecMonitors(ConfigurableUser):
+    __slots__ = {
+        "Histograms"   : "OfflineFull"
+        , "MoniSequence": None
+        , "KnownHistograms" : ['OfflineFull', 'OfflineExpress', 'Expert', 'Online', 'None']
+        }
+    
+    _propertyDocDct = { 
+       'Histograms'       : """ What level of Histogramming to write """
+       , "MoniSequence"   : """ The sequence to which to append the monitors"""
+       , "KnownHistograms": """what Histograms settings are known """
+       }
+    
+    def __apply_configuration__(self):
+        '''choose to add the monitors to the sequence or not'''
+        if hasattr( self, "Histograms" ):
+            
+            if self.getProp("Histograms") not in self.getProp("KnownHistograms") :
+                #this is an error, perhaps it should throw an exception
+                return
+            
+            #no histograms to be added in this mode
+            if self.getProp("Histograms") in ['Online', 'None']: return
+            
+            #must be 'OfflineFull', 'OfflineExpress' or 'Expert'
+            #histograms to be added in this mode
+            from Configurables import ( VeloTrackMonitor, VeloClusterMonitor )
+            
+            if hasattr( self, "MoniSequence" ):
+                theseq=self.getProp("MoniSequence")
+                if theseq is None:
+                    #this is an error, I should throw an exception
+                    return
+                theseq.Members+=[ VeloTrackMonitor(), VeloClusterMonitor() ]
+        
+        return
