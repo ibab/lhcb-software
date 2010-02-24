95014970a919e0bcce778ba0eb766238e7a9b7d1
jpalac <jpalac@cern.ch>
2010-02-24 15:36:23 +0000

    MicroDSTAlgorithm to clone a Particle->backgroundCategory (int) 1D relations table.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81877 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a5258c43dcc034aaa3b2e89ed85b95a590513be7

---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2BackgroundCategory.cpp
@@ -0,0 +1,3 @@
+// $Id: CopyParticle2BackgroundCategory.cpp,v 1.1 2010-02-24 15:36:23 jpalac Exp $
+// Include files 
+#include "CopyParticle2BackgroundCategory.h"
---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2BackgroundCategory.h
@@ -0,0 +1,57 @@
+// $Id: CopyParticle2BackgroundCategory.h,v 1.1 2010-02-24 15:36:23 jpalac Exp $
+#ifndef COPYPARTICLE2BACKGROUNDCATEGORY_H 
+#define COPYPARTICLE2BACKGROUNDCATEGORY_H 1
+
+// Include files
+// from LHCb
+#include <Relations/Relation1D.h>
+// from MicroDST
+#include <MicroDST/ICloneParticle.h>
+#include "MicroDST/RelationsFromClonerAlg.h"
+#include "MicroDST/BindType2ClonerDef.h"
+// from DaVinci
+#include "Event/Particle.h"
+/** @class CopyParticle2BackgroundCategory CopyParticle2BackgroundCategory.h
+ *  
+ *  MicroDSTAlgorithm to clone the associations between an LHCb::Particle
+ *  and a BackgroundCategory. 
+ *  It inherits the std::string properties InputLocation and OutputPrefix from
+ *  MicroDSTCommon. The associations are taken from the TES location 
+ *  defined by InputLocation, and are cloned and put in TES location 
+ *  "/Event" + OutputPrefix + InputLocation. If InputLocation already contains
+ *  a leading "/Event" it is removed.
+ *
+ *  <b>Important conditions</b>: This algorithm relies on the LHCb::Particle
+ *  set in the associations to have been previously cloned by CopyParticles,
+ *  and the existence of the LHCb::Relation1D<const LHCb::Particle*,int> associating the origin
+ *  LHCb::Particles to BackgroundCategories (ints)
+ *
+ *  <b>Example</b>: Clone LHCb::Particle -> BackgroundCategory associations
+ *  from "/Event/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/P2BCRelations"
+ *  to "/Event/MyLocation/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particle2VertexRelations"
+ *  @code
+ *  mySeq = GaudiSequencer('SeqDC06selBd2Jpsi2MuMu_Kst2KPi')
+ *  copyP2BC = CopyParticle2BackgroundCategory()
+ *  mySeq.Members += [copyP2BC]
+ *  copyP2BC.InputLocation = "Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/P2BCRelations";
+ *  copyP2BC.OutputPrefix = "MyLocation";
+ *  @endcode
+ *
+ *  @author Juan PALACIOS juan.palacios@nikhef.nl
+ *  @date   2010-02-24
+ */
+//=============================================================================
+namespace Particle2BackCat {
+  
+  typedef LHCb::Relation1D<const LHCb::Particle*,int> Table;
+}
+//=============================================================================
+template<> struct Location<Particle2BackCat::Table>
+{
+  const static std::string Default;
+};
+const std::string Location<Particle2BackCat::Table>::Default = "NO DEFAULT LOCATION";
+//=============================================================================
+typedef MicroDST::RelationsFromClonerAlg<Particle2BackCat::Table> CopyParticle2BackgroundCategory;
+DECLARE_NAMED_ALGORITHM_FACTORY( CopyParticle2BackgroundCategory, CopyParticle2BackgroundCategory )
+#endif // COPYPARTICLE2BACKGROUNDCATEGORY_H
