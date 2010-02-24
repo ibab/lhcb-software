7c389f4e5583fb4233afc93b7036f01755a7893a
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2010-02-24 04:17:35 +0000

    First commit of new file to store shared track naming conventions across the Hlt
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81852 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5a4119e5311165e05489541bd4f17d2722f0e8fc

---
+++ Hlt/Hlt/HltLine/python/HltLine/HltTrackNames.py
@@ -0,0 +1,53 @@
+# A module to hold the hardcoded names of tracks in the Hlt
+# and rules for deriving containers from them  
+
+# Tracks
+
+HltSharedTracksPrefix = "Hlt"
+
+HltSharedRZVeloTracksName = "RZVelo"
+
+Hlt1TracksPrefix = "Hlt1"
+
+Hlt2TracksPrefix = "Hlt2"
+
+Hlt2VeloTracksName = "Velo"
+
+Hlt2ForwardTracksName = "Forward"
+
+Hlt2MatchTracksName = "Match"
+
+Hlt2LongTracksName = "Long"
+
+Hlt2SeedingTracksName = "Seeding"
+
+Hlt2DownstreamTracksName = "SeedTT"
+
+HltBiDirectionalKalmanFitSuffix = "BiKalmanFitted"
+
+HltUniDirectionalKalmanFitSuffix = "UniKalmanFitted"  
+
+# ProtoParticles
+# These share "prefixes" with the tracks
+
+Hlt2SharedProtoParticleSuffix = "ProtoP"
+
+Hlt2ChargedProtoParticleSuffix = "Charged"
+
+Hlt2NeutralProtoParticleSuffix = "Neutral" 
+
+
+__all__ = (	HltSharedTracksPrefix, 
+           	Hlt1TracksPrefix, 
+           	Hlt2TracksPrefix, 
+		Hlt2ForwardTracksName, 
+		Hlt2MatchTracksName, 
+		Hlt2LongTracksName, 
+		Hlt2SeedingTracksName, 
+		Hlt2DownstreamTracksName, 
+		HltBiDirectionalKalmanFitSuffix, 
+		HltUniDirectionalKalmanFitSuffix, 
+		Hlt2SharedProtoParticleSuffix, 
+		Hlt2ChargedProtoParticleSuffix, 
+		Hlt2NeutralProtoParticleSuffix
+	  ) 
