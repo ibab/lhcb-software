15da1b492f52a99295b68a86824200f816247910
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-02-24 16:45:24 +0000

    remove broken if
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@25893 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0e5d90411a2c519d404c7159afcf9a38f0b447f3

--- Rec/Velo/VeloRecMonitors/python/VeloRecMonitors/Configuration.py
+++ Rec/Velo/VeloRecMonitors/python/VeloRecMonitors/Configuration.py
@@ -1,7 +1,7 @@
 """
 Configuration of the Velo Monitoring Histograms
 """
-__version__ = "$Id: Configuration.py,v 1.5 2010-02-24 15:26:57 erodrigu Exp $"
+__version__ = "$Id: Configuration.py,v 1.6 2010-02-24 16:45:24 cattanem Exp $"
 __author__  = "Rob Lambert"
 
 from Gaudi.Configuration import *
@@ -23,33 +23,28 @@ class VeloRecMonitors(ConfigurableUser):
     
     def __apply_configuration__(self):
         '''choose to add the monitors to the sequence or not'''
-        if hasattr( self, "Histograms" ):
+        if self.getProp("Histograms") not in self.getProp("KnownHistograms") :
+            raise RuntimeError("Unknown Histograms option '%s'"%self.getProp("Histograms"))
             
-            if self.getProp("Histograms") not in self.getProp("KnownHistograms") :
-                RuntimeError("Unknown Histograms option '%s'"%self.getProp("Histograms"))
-                #this is an error, perhaps it should throw an exception
-                return
+        #no histograms to be added in this mode
+        if self.getProp("Histograms") in ['Online', 'None']: return
             
-            #no histograms to be added in this mode
-            if self.getProp("Histograms") in ['Online', 'None']: return
+        #must be 'OfflineFull', 'OfflineExpress' or 'Expert'
+        #histograms to be added in this mode            
+        from Configurables import Velo__VeloClusterMonitor
+        from Configurables import Velo__VeloTrackMonitor
+        if hasattr( self, "MoniSequence" ):
+            # set by RecMoniConf to GaudiSequencer('MoniVELOSeq')
+            theseq=self.getProp("MoniSequence")
+            if theseq is None:
+                #this is an error, I should throw an exception
+                raise RuntimeError("Monitoring sequence not set")
+            VCM = Velo__VeloClusterMonitor( 'VeloClusterMonitor' )
+            VTM = Velo__VeloTrackMonitor  ( 'VeloTrackMonitor'   )
+            VCM.OutputLevel     = self.getProp( 'OutputLevel' )
+            VTM.OutputLevel     = self.getProp( 'OutputLevel' )
+            VCM.PropertiesPrint = False
+            VTM.PropertiesPrint = False
+            theseq.Members += [ VCM, VTM ]
             
-            #must be 'OfflineFull', 'OfflineExpress' or 'Expert'
-            #histograms to be added in this mode            
-            from Configurables import Velo__VeloClusterMonitor
-            from Configurables import Velo__VeloTrackMonitor
-            if hasattr( self, "MoniSequence" ):
-                # set by RecMoniConf to GaudiSequencer('MoniVELOSeq')
-                theseq=self.getProp("MoniSequence")
-                if theseq is None:
-                    #this is an error, I should throw an exception
-                    raise RuntimeError("Monitoring sequence not set")
-                    return
-                VCM = Velo__VeloClusterMonitor( 'VeloClusterMonitor' )
-                VTM = Velo__VeloTrackMonitor  ( 'VeloTrackMonitor'   )
-                VCM.OutputLevel     = self.getProp( 'OutputLevel' )
-                VTM.OutputLevel     = self.getProp( 'OutputLevel' )
-                VCM.PropertiesPrint = False
-                VTM.PropertiesPrint = False
-                theseq.Members += [ VCM, VTM ]
-        
         return
