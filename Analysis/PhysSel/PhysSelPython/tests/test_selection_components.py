a2e8e926764fbbcf6c014c042f1b8b541eef109e
jpalac <jpalac@cern.ch>
2010-02-25 14:13:33 +0000

    Make executable.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81907 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 25c49a26d856e5811ce0af047cc33131d619d261

--- Analysis/PhysSel/PhysSelPython/tests/test_selection_components.py
+++ Analysis/PhysSel/PhysSelPython/tests/test_selection_components.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#$Id: test_selection_components.py,v 1.1 2010-02-25 14:08:47 jpalac Exp $
+#$Id: test_selection_components.py,v 1.2 2010-02-25 14:13:33 jpalac Exp $
 '''
 Test suite for selection module.
 '''
@@ -99,3 +99,35 @@ def test_FlatSelectionListBuilder_removes_duplicates() :
     assert alg_names.index('1.10000') < alg_names.index('2.10000')
     assert alg_names.index('1.10001') < alg_names.index('2.10000')
 
+if '__main__' == __name__ :
+
+    import sys
+
+    test_names = filter(lambda k : k.count('test_') > 0, locals().keys())
+
+    __tests = filter( lambda x : x[0] in test_names, locals().items())
+    
+
+    message = ''
+    summary = '\n'
+    length = len(sorted(test_names,
+                        cmp = lambda x,y : cmp(len(y),len(x)))[0]) +2
+    
+    for test in __tests :
+        try :
+            test[1]()
+            message = 'PASS'
+        except :
+            message = "FAIL"
+        summary += test[0].ljust(length) + ':' + message.rjust(10) + '\n'
+
+    if summary.count('FAIL') > 0 :
+        message = 'FAIL'
+        wr = sys.stderr.write
+    else :
+        message = 'PASS'
+        wr = sys.stdout.write
+
+    summary += 'Global'.ljust(length) + ':' + message.rjust(10) + '\n\n'
+    wr(summary)
+        
