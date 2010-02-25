13d5beab92e2ab12a8d2ee7046a5705dab841d57
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-02-25 14:12:24 +0000

    changes for v36r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31352 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 16186d0935b0fa653a584ebf51eb11366d946318

--- Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -2,6 +2,8 @@ from GaudiTest import LineSkipper
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
+  LineSkipper(["MagneticFieldSvc     INFO Map scaled by factor"]) + \
+  LineSkipper(["MagneticFieldSvc     INFO Opened magnetic field file"]) + \
   LineSkipper(["INFO MuonIDAlg v"]) + \
   LineSkipper(["Delta Memory for the event exceeds 3*sigma"]) + \
   LineSkipper(["AfterMagnetRegion/"]) + \
