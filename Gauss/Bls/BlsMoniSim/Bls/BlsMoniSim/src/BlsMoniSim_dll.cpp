801a17079495138931547a610c506c79be140d34
Marco Clemencic <marcocle@cern.ch>
2010-02-25 10:18:47 +0000

    Removed pacakges Bls/BlsMoniSim and Gen/LbMIB (not migrated yet)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@12094 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dc065a5f8322c78175256ae7f5751843a2adc4fc

--- Gauss/Bls/BlsMoniSim/Bls/BlsMoniSim/src/BlsMoniSim_dll.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: BlsMoniSim_dll.cpp,v 1.1.1.1 2009/10/30 08:30:10 vtalanov Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES ( BlsMoniSim )
-
--- Gauss/Gen/LbMIB/options/MIB.py
+++
@@ -1,6 +0,0 @@
-##
-## Steer Gauss not to use standard physics generation algorihtm but the
-## one for MIB: 'MIBackground'
-##
-from Gauss.Configuration import *
-Gauss().Production = 'MIB'
