8fd47d2f2591282e6c1516ef6354ee26a04b0dab
jpalac <jpalac@cern.ch>
2010-02-25 14:13:58 +0000

    Remove duplicaiton test. In test_selection_components.py.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@81908 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e801eb159db6e2b450087cd2d0a6e63e4b1fa568

--- Analysis/PhysSel/PhysSelPython/tests/test_selection_sequence.py
+++ Analysis/PhysSel/PhysSelPython/tests/test_selection_sequence.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#$Id: test_selection_sequence.py,v 1.13 2010-02-25 14:09:30 jpalac Exp $
+#$Id: test_selection_sequence.py,v 1.14 2010-02-25 14:13:58 jpalac Exp $
 '''
 Test suite for SelectionSequence class.
 '''
@@ -144,9 +144,6 @@ def test_clone_sequence() :
 
     for sel in [sel01, sel02, sel03]:
         assert sel.algorithm() in ref_algos[len(presels):len(ref_algos)-len(postsels)]
-
-def test_duplicated_entries_removed() :
-    pass
 
 if '__main__' == __name__ :
 
