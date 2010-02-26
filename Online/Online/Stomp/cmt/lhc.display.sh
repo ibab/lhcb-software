62fd9dca12548d581f1e36fc213f4e73e5f86da5
Markus Frank <frankb@cern.ch>
2010-02-26 15:49:27 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100334 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c883d1072e718f8ae3a24294bf2652095a49dd48

--- Online/Online/Stomp/cmt/lhc.display.sh
+++ Online/Online/Stomp/cmt/lhc.display.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd /home/frankm/JS
+##cd /home/frankm/JS
 export UTGID=LHC_`hostname -s`
 echo "Starting UTGID=${UTGID}"
-exec -a ${UTGID} /usr/bin/java -jar env-js.jar <<EOF
+exec -a ${UTGID} /usr/bin/java -jar ../web/env-js.jar <<EOF
 var loc = 'http://ecs03.lbdaq.cern.ch:8000/static/';
-load(loc+'env.rhino.cpp');
+var loc = 'http://lbcomet.cern.ch/static/';
+load('http://frankm.web.cern.ch/frankm/Online/env.rhino.cpp');
 function alert(msg) {   print(msg);   }
 window.onload=function() {  print('================= Document '+window.location+' loaded');  }
 window.location=loc+'RunStatus/display.htm?${1}';
