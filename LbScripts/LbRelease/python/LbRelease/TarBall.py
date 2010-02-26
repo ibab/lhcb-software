d7215087ca05ee9e456925b1851145101e785390
hmdegaud <hmdegaud@cern.ch>
2010-02-26 19:29:18 +0000

    new script/module to create LHCb tarballs.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@14143 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6dc1414b71ee1b1629c6c3134780b2471a9d4a7c

--- LbScripts/LbRelease/python/LbRelease/TarBall.py
+++ LbScripts/LbRelease/python/LbRelease/TarBall.py
@@ -1,9 +1,17 @@
 """
 Generic Tar Ball module
 """
-
-import os.path
+from LbConfiguration.Project import getProject
+from LbConfiguration.Platform import pathBinaryMatch, pathSharedMatch
+from LbConfiguration.Platform import binary_list
+from LbUtils.Tar import createTarBallFromFilter
 from LbUtils.Temporary import TempDir
+
+from fnmatch import fnmatch
+
+import logging
+
+import os, sys
 import tarfile
 import shutil
 
@@ -60,6 +68,72 @@ class BinaryTarBall(TarBall):
         self._binaries = [binary]
     def addBinary(self, binary):
         self._binaries += binary
+
+def projectFilter(path, cmtconfig=None):
+    exclude_file_list = ["install.history", "core.*", "*.pyc", "*.pyo", ".glimpse_*",
+                         "*.o", "lib*.a", "*.obj" ]
+    exclude_dir_list = ["html", "Visal"]
+
+    exclude_list = [ os.path.join("*", x) for x in exclude_file_list]
+    exclude_list += [ os.path.join("*", x, "*") for x in exclude_dir_list]
+    for e in exclude_list :
+        if fnmatch(path, e) :
+            return False
+    
+    if cmtconfig :
+        return pathBinaryMatch(path, cmtconfig)
+    else :
+        return pathSharedMatch(path)
+
+
+def buildTar(project, version, cmtconfig=None, 
+             top_dir=None, output_dir=None, 
+             overwrite=False, overwrite_shared=False, update=False):
+    log = logging.getLogger()
+    prj_conf = getProject(project)
+    if not top_dir :
+        top_dir = prj_conf.ReleaseArea()
+    if not output_dir :
+        output_dir = prj_conf.DistLocation()
+    if not cmtconfig :
+        cmtconfig = binary_list
+        if overwrite :
+            overwrite_shared = True
+    prefix = prj_conf.releasePrefix(version)
+    srcdirs = [os.path.join(top_dir, prefix)]
+    for c in cmtconfig :
+        filename = os.path.join(output_dir, prj_conf.tarBallName(version, c, full=True))
+        if os.path.exists(filename) and not (overwrite or update) :
+            sys.exit("The file %s already exists. Please remove it first." % filename)
+        pathfilter = lambda x : projectFilter(x, c)
+        prefix = prj_conf.releasePrefix(version)
+        if update :
+            log.info("Updating %s with %s" % (filename, ", ".join(srcdirs)) )
+        else :
+            log.info("Creating %s with %s" % (filename, ", ".join(srcdirs)) )        
+        createTarBallFromFilter(srcdirs, filename, pathfilter,
+                                prefix=prefix, dereference=False, update=update)
+
+    filename = os.path.join(output_dir, prj_conf.tarBallName(version, cmtconfig=None, full=True))
+    pathfilter = projectFilter
+    if overwrite_shared :
+        if os.path.exists(filename) :
+            os.remove(filename)
+            log.info("Replacing %s with %s" % (filename, ", ".join(srcdirs)) )
+        else :
+            log.info("Creating %s with %s" % (filename, ", ".join(srcdirs)) )            
+        createTarBallFromFilter(srcdirs, filename, pathfilter,
+                            prefix=prefix, dereference=False, update=False)        
+    else :
+        if os.path.exists(filename) :
+            log.info("Updating %s with %s" % (filename, ", ".join(srcdirs)) )
+        else :
+            log.info("Creating %s with %s" % (filename, ", ".join(srcdirs)) )
+        createTarBallFromFilter(srcdirs, filename, pathfilter,
+                            prefix=prefix, dereference=False, update=True)
+    
+
+
 
 if __name__ == '__main__':
     pass
