9151342c1d05df26658bd793f9a62dd92524efca
rlambert <rlambert@cern.ch>
2010-02-27 15:53:11 +0000

    sort configurables in test
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@61688 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 05418a5e5cec7d2c9769fb58fd309f08f7b2a72c

--- LHCb/LHCbSys/tests/options/configurables.py
+++ LHCb/LHCbSys/tests/options/configurables.py
@@ -2,9 +2,8 @@ import Configurables
 
 if len(Configurables.__all__) <400:
     print "Not enough configurables found, should be >400"
-    print Configurables.__all__
 
-for conf in Configurables.__all__:
+for conf in sorted(Configurables.__all__):
     print conf
     exec("Configurables.%s()" % conf)
 
