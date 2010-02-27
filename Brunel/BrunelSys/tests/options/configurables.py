00a7adf652e57564d29e4a17d70e08a8fd4d9198
rlambert <rlambert@cern.ch>
2010-02-27 15:46:45 +0000

    rlambert_20100227
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31375 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef14ae4d5656a5f2195321667575b01455d4197a

--- Brunel/BrunelSys/tests/options/configurables.py
+++ Brunel/BrunelSys/tests/options/configurables.py
@@ -2,9 +2,8 @@ import Configurables
 
 if len(Configurables.__all__) <1000:
     print "Not enough configurables found, should be >1000"
-    print Configurables.__all__
 
-for conf in Configurables.__all__:
+for conf in sorted(Configurables.__all__):
     print conf
     exec("Configurables.%s()" % conf)
 
