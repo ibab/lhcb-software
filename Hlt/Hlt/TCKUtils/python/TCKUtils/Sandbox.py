95850c4e5efd72c153e36b3ae0f741ff940992f1
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-03-01 20:38:34 +0000

    transmit exceptions out of sandbox, to the parent
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@81993 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a0facaf759ac2a3bc43d1993f73900250b313b59

--- Hlt/Hlt/TCKUtils/python/TCKUtils/Sandbox.py
+++ Hlt/Hlt/TCKUtils/python/TCKUtils/Sandbox.py
@@ -1,17 +1,21 @@
 
 def execInSandbox(fun,*args,**kwargs) :
-    from processing import Pipe, Process
+    from processing import Pipe, Process, Condition
     (r,w) = Pipe()
 
     # bind worker to the 'write' side of the pipe, and to 'fun'
     def worker( *args, **kwargs) :
-        w.send(fun(*args,**kwargs))
-
+            try :
+                # TODO: catch errors, and signal parent about them...
+                w.send(fun(*args,**kwargs))
+            except Exception, e :
+                w.send(e)
+             
     p = Process(target=worker,args=args,kwargs=kwargs)
     p.start()
     result = r.recv()
-    p.join()
+    if type(result) is Exception :
+        raise result
+    # check if child exited OK..    .
     return result
     
-
-
