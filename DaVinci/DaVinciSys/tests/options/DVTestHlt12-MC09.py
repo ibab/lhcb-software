bff8a98674c6ea563f08bde6bf8d1c75c7f93b0f
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-03-05 10:58:05 +0000

    Fix tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@82070 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b99481fa93c053e1390a257b573110244657506d

--- DaVinci/DaVinciSys/tests/options/DVTestHlt12-MC09.py
+++ DaVinci/DaVinciSys/tests/options/DVTestHlt12-MC09.py
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: DVTestHlt12-MC09.py,v 1.9 2010-02-09 15:25:42 pkoppenb Exp $
+# $Id: DVTestHlt12-MC09.py,v 1.10 2010-03-05 10:58:05 pkoppenb Exp $
 #
 # Test for Hlt1&2. This should be identical to Hlt1 except for the Hlt settings
 #
@@ -33,9 +33,10 @@ DaVinci().Hlt = True
 DaVinci().HltThresholdSettings = 'Physics_320Vis_300L0_10Hlt1_Hlt2_Feb10'   # Hlt2 for MC09
 DaVinci().ReplaceL0BanksWithEmulated = True 
 #
-# a lambda stream file from the first MC09 MB stripping 
+# a D* stream file from the first MC09 MB stripping 
 #
 EventSelector().Input   = [
 "   DATAFILE='castor://castorlhcb.cern.ch:9002/?svcClass=lhcbdata&castorVersion=2&path=/castor/cern.ch/grid/lhcb/MC/MC09/DSTAR.DST/00005436/0000/00005436_00000003_1.dstar.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
 #
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
