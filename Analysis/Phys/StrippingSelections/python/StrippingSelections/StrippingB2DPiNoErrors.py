0b904ecad9da3fe57c3f43517e4ff21c244aa66d
Jacopo Nardulli <jacopo.nardulli@cern.ch>
2010-03-08 09:43:26 +0000

    Change names in python/StrippingSelections/StrippingB2DPiNoErrors.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@82088 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b2cbb0ca829830fa2450ce8cf3304034e797960

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2DPiNoErrors.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2DPiNoErrors.py
@@ -82,136 +82,136 @@ PhiDaughterCut = "( 2 == NINTREE( (ABSID=='K+') & (" + Daughtercut + ")))"
 ########################################################################
 ########################################################################
 # the K*
-StrippingSelKstar = CombineParticles("StrippingSelKstar")
-StrippingSelKstar.InputLocations = ["StdNoPIDsPions","StdNoPIDsKaons"]
-StrippingSelKstar.DecayDescriptors = ["[K*(892)0 -> K+ pi-]cc","[K*(892)0 -> pi+ pi-]cc"]
-StrippingSelKstar.DaughtersCuts =  { "K+"        : Daughtercut,
+StrippingSelKstarNoErrors = CombineParticles("StrippingSelKstarNoErrors")
+StrippingSelKstarNoErrors.InputLocations = ["StdNoPIDsPions","StdNoPIDsKaons"]
+StrippingSelKstarNoErrors.DecayDescriptors = ["[K*(892)0 -> K+ pi-]cc","[K*(892)0 -> pi+ pi-]cc"]
+StrippingSelKstarNoErrors.DaughtersCuts =  { "K+"        : Daughtercut,
                                      "pi+"        : Daughtercut }
-StrippingSelKstar.CombinationCut = Dcombcut
-StrippingSelKstar.MotherCut = KPhiMothercut
+StrippingSelKstarNoErrors.CombinationCut = Dcombcut
+StrippingSelKstarNoErrors.MotherCut = KPhiMothercut
 ########################################################################
 # the Phi
-StrippingSelPhi = CombineParticles("StrippingSelPhi")
-StrippingSelPhi.InputLocations = ["StdNoPIDsKaons"]
-StrippingSelPhi.DecayDescriptor = "phi(1020) -> K+ K-"
-StrippingSelPhi.DaughtersCuts =  { "K+"        : Daughtercut}
-StrippingSelPhi.CombinationCut = Dcombcut
-StrippingSelPhi.MotherCut = KPhiMothercut
+StrippingSelPhiNoErrors = CombineParticles("StrippingSelPhiNoErrors")
+StrippingSelPhiNoErrors.InputLocations = ["StdNoPIDsKaons"]
+StrippingSelPhiNoErrors.DecayDescriptor = "phi(1020) -> K+ K-"
+StrippingSelPhiNoErrors.DaughtersCuts =  { "K+"        : Daughtercut}
+StrippingSelPhiNoErrors.CombinationCut = Dcombcut
+StrippingSelPhiNoErrors.MotherCut = KPhiMothercut
 ########################################################################
 #The bachelor
-StrippingBachelor = FilterDesktop("StrippingBachelor")
-StrippingBachelor.InputLocations  = [ "StdNoPIDsPions","StdNoPIDsKaons" ]
-StrippingBachelor.Code = Bachelorcut
+StrippingBachelorNoErrors = FilterDesktop("StrippingBachelorNoErrors")
+StrippingBachelorNoErrors.InputLocations  = [ "StdNoPIDsPions","StdNoPIDsKaons" ]
+StrippingBachelorNoErrors.Code = Bachelorcut
 ########################################################################
-SubX = GaudiSequencer("SubX")
-SubX.ModeOR = 1
-SubX.IgnoreFilterPassed = True
-SubX.Members += [StrippingSelKstar]
-SubX.Members += [StrippingSelPhi]
-SubX.Members += [StrippingBachelor]
+SubXNoErrors = GaudiSequencer("SubXNoErrors")
+SubXNoErrors.ModeOR = 1
+SubXNoErrors.IgnoreFilterPassed = True
+SubXNoErrors.Members += [StrippingSelKstarNoErrors]
+SubXNoErrors.Members += [StrippingSelPhiNoErrors]
+SubXNoErrors.Members += [StrippingBachelorNoErrors]
 #######################################################################
 # All possible D decays starting from the D->hh
-StrippingSelD0 = CombineParticles("StrippingSelD0")
-StrippingSelD0.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
-StrippingSelD0.DecayDescriptors = ["[D0 -> K- pi+]cc","[D0 -> pi- pi+]cc","[D0 -> K- K+]cc","[D0 -> K+ pi-]cc"]
-StrippingSelD0.DaughtersCuts =  { "K+"        : Daughtercut,"pi+"        : Daughtercut }
-StrippingSelD0.CombinationCut = Dcombcut
-StrippingSelD0.MotherCut = Dcut
+StrippingSelD0NoErrors = CombineParticles("StrippingSelD0NoErrors")
+StrippingSelD0NoErrors.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
+StrippingSelD0NoErrors.DecayDescriptors = ["[D0 -> K- pi+]cc","[D0 -> pi- pi+]cc","[D0 -> K- K+]cc","[D0 -> K+ pi-]cc"]
+StrippingSelD0NoErrors.DaughtersCuts =  { "K+"        : Daughtercut,"pi+"        : Daughtercut }
+StrippingSelD0NoErrors.CombinationCut = Dcombcut
+StrippingSelD0NoErrors.MotherCut = Dcut
 #now the D ->hhh
-StrippingSelD2hhh = CombineParticles("StrippingSelD2hhh")
-StrippingSelD2hhh.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
-StrippingSelD2hhh.DecayDescriptors = ["[D+ -> K- pi+ pi+]cc","[D+ -> K- K+ pi+]cc","[D+ -> pi- pi+ pi+]cc","[D+ -> pi- pi+ K+]cc"]
-StrippingSelD2hhh.DaughtersCuts =  { "K+"        : Daughtercut,"pi+"        : Daughtercut }
-StrippingSelD2hhh.CombinationCut = Dcombcut
-StrippingSelD2hhh.MotherCut = Dcut
+StrippingSelD2hhhNoErrors = CombineParticles("StrippingSelD2hhhNoErrors")
+StrippingSelD2hhhNoErrors.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
+StrippingSelD2hhhNoErrors.DecayDescriptors = ["[D+ -> K- pi+ pi+]cc","[D+ -> K- K+ pi+]cc","[D+ -> pi- pi+ pi+]cc","[D+ -> pi- pi+ K+]cc"]
+StrippingSelD2hhhNoErrors.DaughtersCuts =  { "K+"        : Daughtercut,"pi+"        : Daughtercut }
+StrippingSelD2hhhNoErrors.CombinationCut = Dcombcut
+StrippingSelD2hhhNoErrors.MotherCut = Dcut
 
 # D->KPiPi0
-StripSelD2KPiPi0 = CombineParticles("StripSelD2KPiPi0")
-StripSelD2KPiPi0.InputLocations  = [ "StdLooseResolvedPi0","StdLooseMergedPi0","StdNoPIDsPions", "StdNoPIDsKaons"]
-StripSelD2KPiPi0.DecayDescriptor = "[D0 -> K- pi+ pi0]cc"
-StripSelD2KPiPi0.DaughtersCuts = { "K-"        : Daughtercut + "& (PT>400*MeV)",
-                                   "pi+"        : Daughtercut + "& (PT>400*MeV)",
-                                   "pi0": "(PT>800*MeV) & (P>4500*MeV)"}
-StripSelD2KPiPi0.CombinationCut =  "(APT>2000*MeV)"
-StripSelD2KPiPi0.MotherCut = Dcut+" & (BPVDIRA>0) & (P>3000*MeV)"
+StripSelD2KPiPi0NoErrors = CombineParticles("StripSelD2KPiPi0NoErrors")
+StripSelD2KPiPi0NoErrors.InputLocations  = [ "StdLooseResolvedPi0","StdLooseMergedPi0","StdNoPIDsPions", "StdNoPIDsKaons"]
+StripSelD2KPiPi0NoErrors.DecayDescriptor = "[D0 -> K- pi+ pi0]cc"
+StripSelD2KPiPi0NoErrors.DaughtersCuts = { "K-"        : Daughtercut + "& (PT>400*MeV)",
+                                           "pi+"        : Daughtercut + "& (PT>400*MeV)",
+                                           "pi0": "(PT>800*MeV) & (P>4500*MeV)"}
+StripSelD2KPiPi0NoErrors.CombinationCut =  "(APT>2000*MeV)"
+StripSelD2KPiPi0NoErrors.MotherCut = Dcut+" & (BPVDIRA>0) & (P>3000*MeV)"
 
 # D->hhhhh
-StrippingSelD2hhhh = CombineParticles("StrippingSelD2hhhh")
-StrippingSelD2hhhh.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
-StrippingSelD2hhhh.DecayDescriptors  = ["[D0 -> K- pi+ pi- pi+]cc","[D0 -> K+ pi- pi- pi+]cc","[D0 -> K+ K- pi+ pi-]cc"]
-StrippingSelD2hhhh.DaughtersCuts = { "K+"        : Daughtercut+ "& (PT>400*MeV)","pi+"        : Daughtercut+ "& (PT>400*MeV)"}
-StrippingSelD2hhhh.CombinationCut =  Dcombcut
-StrippingSelD2hhhh.MotherCut = Dcut+" & (BPVDIRA>0) & (P>3000*MeV)"
+StrippingSelD2hhhhNoErrors = CombineParticles("StrippingSelD2hhhhNoErrors")
+StrippingSelD2hhhhNoErrors.InputLocations = ["StdNoPIDsKaons", "StdNoPIDsPions"]
+StrippingSelD2hhhhNoErrors.DecayDescriptors  = ["[D0 -> K- pi+ pi- pi+]cc","[D0 -> K+ pi- pi- pi+]cc","[D0 -> K+ K- pi+ pi-]cc"]
+StrippingSelD2hhhhNoErrors.DaughtersCuts = { "K+"        : Daughtercut+ "& (PT>400*MeV)","pi+"        : Daughtercut+ "& (PT>400*MeV)"}
+StrippingSelD2hhhhNoErrors.CombinationCut =  Dcombcut
+StrippingSelD2hhhhNoErrors.MotherCut = Dcut+" & (BPVDIRA>0) & (P>3000*MeV)"
 
 #Kshorts
-LooseKsDD = CombineParticles("LooseKsDD")
-LooseKsDD.InputLocations = [ "StdNoPIDsDownPions" ]
-LooseKsDD.DecayDescriptor = "KS0 -> pi+ pi-"
-LooseKsDD.DaughtersCuts = { "pi+" : Daughtercut }
-LooseKsDD.CombinationCut = "(ADAMASS('KS0')<50*MeV)"
-LooseKsDD.MotherCut = "(ADMASS('KS0')<30.0*MeV) & (VFASPF(VCHI2/VDOF)<16.0)& (BPVVD>1*mm)"
-LooseKsLL = LooseKsDD.clone("LooseKsLL")
-LooseKsLL.InputLocations = [ "StdNoPIDsPions" ]
-SubKshorts = GaudiSequencer("SubKshorts")
-SubKshorts.ModeOR = 1
-SubKshorts.IgnoreFilterPassed = True
-SubKshorts.Members += [LooseKsDD]
-SubKshorts.Members += [LooseKsLL]
+LooseKsDDNoErrors = CombineParticles("LooseKsDDNoErrors")
+LooseKsDDNoErrors.InputLocations = [ "StdNoPIDsDownPions" ]
+LooseKsDDNoErrors.DecayDescriptor = "KS0 -> pi+ pi-"
+LooseKsDDNoErrors.DaughtersCuts = { "pi+" : Daughtercut }
+LooseKsDDNoErrors.CombinationCut = "(ADAMASS('KS0')<50*MeV)"
+LooseKsDDNoErrors.MotherCut = "(ADMASS('KS0')<30.0*MeV) & (VFASPF(VCHI2/VDOF)<16.0)& (BPVVD>1*mm)"
+LooseKsLLNoErrors = LooseKsDDNoErrors.clone("LooseKsLLNoErrors")
+LooseKsLLNoErrors.InputLocations = [ "StdNoPIDsPions" ]
+SubKshortsNoErrors = GaudiSequencer("SubKshortsNoErrors")
+SubKshortsNoErrors.ModeOR = 1
+SubKshortsNoErrors.IgnoreFilterPassed = True
+SubKshortsNoErrors.Members += [LooseKsDDNoErrors]
+SubKshortsNoErrors.Members += [LooseKsLLNoErrors]
 
 #D->kspipi
-StrippingSelD2kshh = CombineParticles("StrippingSelD2kshh")
-StrippingSelD2kshh.InputLocations = ["LooseKsDD", "LooseKsLL","StdNoPIDsPions","StdNoPIDsKaons"]
-StrippingSelD2kshh.DecayDescriptors = ["[D0 -> KS0 pi+ pi-]cc","[D0 -> KS0 K+ K-]cc","[D0 -> KS0 K+ pi-]cc","[D0 -> KS0 pi+ K-]cc"]
-StrippingSelD2kshh.DaughtersCuts = { "pi+" : Daughtercut,"K+" : Daughtercut, "KS0" : "ALL" }
-StrippingSelD2kshh.CombinationCut = Dcombcut
-StrippingSelD2kshh.MotherCut = Dcut
+StrippingSelD2kshhNoErrors = CombineParticles("StrippingSelD2kshhNoErrors")
+StrippingSelD2kshhNoErrors.InputLocations = ["LooseKsDDNoErrors", "LooseKsLLNoErrors","StdNoPIDsPions","StdNoPIDsKaons"]
+StrippingSelD2kshhNoErrors.DecayDescriptors = ["[D0 -> KS0 pi+ pi-]cc","[D0 -> KS0 K+ K-]cc","[D0 -> KS0 K+ pi-]cc","[D0 -> KS0 pi+ K-]cc"]
+StrippingSelD2kshhNoErrors.DaughtersCuts = { "pi+" : Daughtercut,"K+" : Daughtercut, "KS0" : "ALL" }
+StrippingSelD2kshhNoErrors.CombinationCut = Dcombcut
+StrippingSelD2kshhNoErrors.MotherCut = Dcut
 
 # D as a or of all possible Ds
-SubD = GaudiSequencer("SubD")
-SubD.ModeOR = 1
-SubD.IgnoreFilterPassed = True
-SubD.Members += [StrippingSelD0]
-SubD.Members += [StrippingSelD2hhh]
-SubD.Members += [StripSelD2KPiPi0]
-SubD.Members += [StrippingSelD2hhhh]
-SubD.Members += [StrippingSelD2kshh]
+SubDNoErrors = GaudiSequencer("SubDNoErrors")
+SubDNoErrors.ModeOR = 1
+SubDNoErrors.IgnoreFilterPassed = True
+SubDNoErrors.Members += [StrippingSelD0NoErrors]
+SubDNoErrors.Members += [StrippingSelD2hhhNoErrors]
+SubDNoErrors.Members += [StripSelD2KPiPi0NoErrors]
+SubDNoErrors.Members += [StrippingSelD2hhhNoErrors]
+SubDNoErrors.Members += [StrippingSelD2kshhNoErrors]
 ########################################################################
-B2DXStrippingSel = CombineParticles("B2DXStrippingSel")
-B2DXStrippingSel.InputLocations  = ["StrippingSelD0",
-                                    "StrippingSelD2hhh",
-                                    "StripSelD2KPiPi0",
-                                    "StrippingSelD2hhhh",
-                                    "StrippingSelD2kshh",
-                                    "StrippingSelKstar",
-                                    "StrippingSelPhi",
-                                    "StrippingBachelor"]
+B2DXStrippingSelNoErrors = CombineParticles("B2DXStrippingSelNoErrors")
+B2DXStrippingSelNoErrors.InputLocations  = ["StrippingSelD0NoErrors",
+                                            "StrippingSelD2hhhNoErrors",
+                                            "StripSelD2KPiPi0NoErrors",
+                                            "StrippingSelD2hhhhNoErrors",
+                                            "StrippingSelD2kshhNoErrors",
+                                            "StrippingSelKstarNoErrors",
+                                            "StrippingSelPhiNoErrors",
+                                            "StrippingBachelorNoErrors"]
 
-B2DXStrippingSel.DecayDescriptors  = ["[B+ -> D~0 K+]cc",
-                                      "[B+ -> D~0 pi+]cc",
-                                      "[B+ -> D0 K+]cc",
-                                      "[B+ -> D0 pi+]cc",
-                                      "[B0 -> D~0 K*(892)0]cc",
-                                      "[B0 -> D0 K*(892)0]cc",
-                                      "[B0 -> D~0 phi(1020)]cc",
-                                      "[B0 -> D0 phi(1020)]cc",
-                                      "[B0 -> D- pi+]cc",
-                                      "[B0 -> D- K+]cc"] 
+B2DXStrippingSelNoErrors.DecayDescriptors  = ["[B+ -> D~0 K+]cc",
+                                              "[B+ -> D~0 pi+]cc",
+                                              "[B+ -> D0 K+]cc",
+                                              "[B+ -> D0 pi+]cc",
+                                              "[B0 -> D~0 K*(892)0]cc",
+                                              "[B0 -> D0 K*(892)0]cc",
+                                              "[B0 -> D~0 phi(1020)]cc",
+                                              "[B0 -> D0 phi(1020)]cc",
+                                              "[B0 -> D- pi+]cc",
+                                              "[B0 -> D- K+]cc"] 
 
-B2DXStrippingSel.DaughtersCuts = { "D+" : Dcut , "K*(892)0" : Kstarcut ,  "D0" : Dcut , "phi(1020)": Phicut, "pi+" : Bachelorcut, "K+" : Bachelorcut}
-B2DXStrippingSel.CombinationCut = Bcombcut
-B2DXStrippingSel.MotherCut = Bcut
+B2DXStrippingSelNoErrors.DaughtersCuts = { "D+" : Dcut , "K*(892)0" : Kstarcut ,  "D0" : Dcut , "phi(1020)": Phicut, "pi+" : Bachelorcut, "K+" : Bachelorcut}
+B2DXStrippingSelNoErrors.CombinationCut = Bcombcut
+B2DXStrippingSelNoErrors.MotherCut = Bcut
 ########################################################################
 # Define the GEC on the number of tracks, needed in order to control
 # the time for the combinatorics (especially once we add the 4-body D states)
-B2DXStrippingNumTracksGEC = VoidFilter('B2DXStrippingNumTracksGEC'
-                                  , Code = "TrSOURCE('Rec/Track/Best') >> (TrSIZE < 240 )"
-                                  )
+B2DXStrippingNumTracksGECNoErrors = VoidFilter('B2DXStrippingNumTracksGECNoErrors'
+                                               , Code = "TrSOURCE('Rec/Track/Best') >> (TrSIZE < 240 )"
+                                               )
 ########################################################################
-B2DXStrippingSequence = GaudiSequencer("B2DXStrippingSequence")
-B2DXStrippingSequence.Members += [SubKshorts]
-B2DXStrippingSequence.Members += [SubX]
-B2DXStrippingSequence.Members += [SubD]
-B2DXStrippingSequence.Members += [B2DXStrippingSel]
+B2DXStrippingSequenceNoErrors = GaudiSequencer("B2DXStrippingSequenceNoErrors")
+B2DXStrippingSequenceNoErrors.Members += [SubKshortsNoErrors]
+B2DXStrippingSequenceNoErrors.Members += [SubXNoErrors]
+B2DXStrippingSequenceNoErrors.Members += [SubDNoErrors]
+B2DXStrippingSequenceNoErrors.Members += [B2DXStrippingSelNoErrors]
 ########################################################################
 ########################################################################
 #Now we define a second sequence, for the signal box
@@ -221,28 +221,28 @@ B2DXStrippingSequence.Members += [B2DXStrippingSel]
 #For the B and the D the signal mass windows include both the B/Bs and D/Ds masses 
 ########################################################################
 ########################################################################
-B2DXSignalFilter = FilterDesktop("B2DXSignalFilter")
-B2DXSignalFilter.InputLocations  = [ "B2DXStrippingSel" ]
-B2DXSignalFilter.Code = BSignalcombcut + "&" + "INTREE(((ABSID == 'D0') | (ABSID == 'D+')) & " + DSignalcut + ")" + "&" + "(INTREE((ABSID == 'K*(892)0') &" + KstarSignalcut + ") | INTREE((ABSID == 'phi(1020)') &" + PhiSignalcut + ") | INGENERATION(ABSID == 'K+',1) | INGENERATION(ABSID == 'pi+',1))"
+B2DXSignalFilterNoErrors = FilterDesktop("B2DXSignalFilterNoErrors")
+B2DXSignalFilterNoErrors.InputLocations  = [ "B2DXStrippingSelNoErrors" ]
+B2DXSignalFilterNoErrors.Code = BSignalcombcut + "&" + "INTREE(((ABSID == 'D0') | (ABSID == 'D+')) & " + DSignalcut + ")" + "&" + "(INTREE((ABSID == 'K*(892)0') &" + KstarSignalcut + ") | INTREE((ABSID == 'phi(1020)') &" + PhiSignalcut + ") | INGENERATION(ABSID == 'K+',1) | INGENERATION(ABSID == 'pi+',1))"
 
-B2DXSignalStrippingSequence = GaudiSequencer("B2DXSignalStrippingSequence")
-B2DXSignalStrippingSequence.Members += [SubKshorts]
-B2DXSignalStrippingSequence.Members += [SubX]
-B2DXSignalStrippingSequence.Members += [SubD]
-B2DXSignalStrippingSequence.Members += [B2DXStrippingSel]
-B2DXSignalStrippingSequence.Members += [B2DXSignalFilter]
+B2DXSignalStrippingSequenceNoErrors = GaudiSequencer("B2DXSignalStrippingSequenceNoErrors")
+B2DXSignalStrippingSequenceNoErrors.Members += [SubKshortsNoErrors]
+B2DXSignalStrippingSequenceNoErrors.Members += [SubXNoErrors]
+B2DXSignalStrippingSequenceNoErrors.Members += [SubDNoErrors]
+B2DXSignalStrippingSequenceNoErrors.Members += [B2DXStrippingSelNoErrors]
+B2DXSignalStrippingSequenceNoErrors.Members += [B2DXSignalFilterNoErrors]
 ########################################################################
 
 # Create the lines for this selection
-line1 = StrippingLine('B2DX'
-               , prescale = 1.0
-               ,  algos = [ B2DXStrippingNumTracksGEC, B2DXStrippingSequence ]
-               #,  algos = [ B2DXStrippingSequence ]
-               , postscale = 0.05
-               )
+line1 = StrippingLine('B2DXNoErrors'
+                      , prescale = 1.0
+                      ,  algos = [ B2DXStrippingNumTracksGECNoErrors, B2DXStrippingSequenceNoErrors ]
+                      #,  algos = [ B2DXStrippingSequenceNoErrors ]
+                      , postscale = 0.05
+                      )
 
-line2 = StrippingLine('B2DXSignal'
-               , prescale = 1.
-               #,  algos = [ B2DXSignalStrippingSequence ]
-               ,  algos = [ B2DXStrippingNumTracksGEC, B2DXSignalStrippingSequence ]
-               )
+line2 = StrippingLine('B2DXSignalNoErrors'
+                      , prescale = 1.
+                      #,  algos = [ B2DXSignalStrippingSequenceNoErrors ]
+                      ,  algos = [ B2DXStrippingNumTracksGECNoErrors, B2DXSignalStrippingSequenceNoErrors ]
+                      )
