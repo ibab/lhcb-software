94c4429de99f30c84683dcd2ba3ebe8668abb341
Markus Frank <frankb@cern.ch>
2010-03-08 12:46:18 +0000

    Remove warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100377 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 85491563e90217a20e85c3550cbd5b4be2cffe38

--- Online/Online/RawBankSizes/src/MakeOptions.cpp
+++ Online/Online/RawBankSizes/src/MakeOptions.cpp
@@ -1,13 +1,11 @@
 #include "Event/RawEvent.h"
-#include "stdio.h"
-main(int argc, char *argv[])
+#include "cstdio"
+int main(int /* argc */, char** /* argv */)
 {
-  int i;
   std::string bname;
   printf("======== Running MakeOptions...\n");
-  FILE *f;
-  f = fopen("/tmp/banks.in","w");
-  for(i = 0 ; i != (int) LHCb::RawBank::LastType; i++)
+  FILE *f = fopen("/tmp/banks.in","w");
+  for(int i = 0 ; i != (int) LHCb::RawBank::LastType; i++)
   {
     bname = LHCb::RawBank::typeName( (LHCb::RawBank::BankType) i );
     fprintf(f,"%s \n",bname.c_str());
