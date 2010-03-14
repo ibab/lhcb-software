f8047d072a760ab18f46e8a2f91080f605f37b63
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-03-14 16:29:21 +0000

    strip debug velo setting
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@82251 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ae0282529590c0875f3549f0e234e5f646b88236

--- Hlt/Hlt/HltSettings/python/HltSettings/Debug_10000Vis_1000L0_Velo_Hlt1_Oct09.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Debug_10000Vis_1000L0_Velo_Hlt1_Oct09.py
@@ -24,7 +24,7 @@ class Debug_10000Vis_1000L0_Velo_Hlt1_Oct09 :
             raise RuntimeError('Must update HltType when modifying ActiveHlt.Lines()')
 
     def L0TCK(self) :
-        return '0xDC09'
+        return None
 
     def HltType(self) :
         self.verifyType( Debug_10000Vis_1000L0_Velo_Hlt1_Oct09 )
@@ -34,8 +34,7 @@ class Debug_10000Vis_1000L0_Velo_Hlt1_Oct09 :
         """
         Returns a list of active Hlt2 lines
         """
-        # return [ 'Hlt2PassThrough', 'Hlt2Transparent','Hlt2Forward','Hlt2DebugEvent']
-        return [ 'Hlt2PassThrough', 'Hlt2Transparent','Hlt2DebugEvent']
+        return [ 'Hlt2Transparent']
         
     def ActiveHlt1Lines(self) :
         """
@@ -50,62 +49,18 @@ class Debug_10000Vis_1000L0_Velo_Hlt1_Oct09 :
         """
         Returns a dictionary of cuts
         """
-        from Hlt1Lines.Hlt1CommissioningLines  import Hlt1CommissioningLinesConf
-        from Hlt1Lines.Hlt1MuonLines           import Hlt1MuonLinesConf
-        from Hlt1Lines.Hlt1HadronLines         import Hlt1HadronLinesConf
-        from Hlt1Lines.Hlt1ElectronLines       import Hlt1ElectronLinesConf
-        from Hlt1Lines.Hlt1PhotonLines         import Hlt1PhotonLinesConf
         from Hlt1Lines.Hlt1VeloLines           import Hlt1VeloLinesConf
         from Hlt2Lines.Hlt2CommissioningLines import Hlt2CommissioningLinesConf
     
-        return { Hlt1HadronLinesConf : { 'SingleHadron_PTCut' : 4450
-                                        , 'HadSingle_IPCut'      : 0.09
-                                        , 'HadMain_PTCut'      : 1600
-                                        , 'HadCompanion_DZCut' : 0.05
-                                        , 'HadDi_IPCut' : 0.13
-                                        , 'HadCompanion_PTCut' : 950
-                                        , 'HadCompanion_PointingCut' : 1.0 
-                                         }
-                 , Hlt1VeloLinesConf : { 'Prescale' : { 'Hlt1Velo(.Side|TrTr)' : 1 }
+        return {  Hlt1VeloLinesConf : { 'Prescale' : { 'Hlt1Velo(.Side|TrTr)' : 1 }
                                          , 'MinimumNumberOfRClusters'   : 12 # 4 tracks with 3 hits
                                          , 'MinimumNumberOfPhiClusters' : 12 # 4 tracks with 3 hits
                                          , 'MaxNumberOfClusters'        : 450 # 0.5% occupancy
                                          , 'MinTrksPerVtx'              : 4   # 4 is the minumum value you can put here 
-                                         , 'ODIN'                       :"( ODIN_TRGTYP != LHCb.ODIN.LumiTrigger )" # on what trigger types do we run?
+                                         , 'ODIN'                       :  None
+                                         , 'L0DU'                       : "L0_CHANNEL('CALO')" # warning: this generarates a dependency on the L0 configuration!
                                          }
-                 , Hlt1PhotonLinesConf : { 'Pho_EtCut'         : 2800.0
-                                          , 'Pho_IsPho'        : -0.082 
-                                          , 'Track_IPCut'      : 0.15
-                                          , 'Track_PtCut'      : 1250.
-                                          , 'DiTrack_PtCut'    : 650
-                                          }
-                 , Hlt1ElectronLinesConf : { 'EleIP_PtCut'             : 2820.0
-                                            , 'EleIP_IPCut'            : 0.13 
-                                            , 'EleIPCompanion_PtCut'   : 2750
-                                            , 'EleCompanion_PtCut'     : 1000
-                                            }
-                 , Hlt1MuonLinesConf : { 'DiMuon_SumPtCut'    : 1480.0
-                                        , 'Muon_PtCut'         : 6200
-                                        , 'MuonIP_PtCut'       : 1800
-                                        , 'Muon_IPMinCut'      : 0.07
-                                        , 'DiMuon_DOCACut'     : 1.6
-                                        , 'DiMuon_MassCut'     : 2500
-                                        , 'DiMuonIP_MassCut'   : 300
-                                        , 'DiMuon_IPCut'       : 0.08
-                                        , 'Prescale' : { 'Hlt1MuTrack4JPsi' : 0. }
-                                        , 'MuTrackMuPt'        : 950
-                                        , 'MuTrackMuIP'        : 0.07
-                                        , 'MuTrackTrPt'        : 550
-                                        , 'MuTrackTrIP'        : 0.1
-                                        , 'MuTrackDoca'        : 0.2
-                                        , 'MuTrackDZ'          : 2.0
-                                        , 'MuTrackDimuMass'    : 1000
-                                        , 'MuTrackPoint'       : 0.4
-                                        , 'MuTrackTrChi2'      : 10 
-                                        }
-                 , Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.
-                                                               , 'Hlt2Forward'      : 0. # first needs a check that /Hlt/Track/Long actually exists..
-                                                               , 'Hlt2DebugEvent'   : 0.0001
-                                                }              }
-                 }
+               ,  Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.  }
+                                               }
+               }
     
