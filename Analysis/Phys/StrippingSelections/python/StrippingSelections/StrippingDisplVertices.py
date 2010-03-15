3b121a6482982037ad673d92259b96d461cccf74
ngueissa <ngueissa@cern.ch>
2010-03-15 15:30:57 +0000

    Adapt StrippingDisplVertices.py to bug-fixed DisplVertices.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@82286 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 172cc71f24235df70d004bc4383e8401d642bc04

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDisplVertices.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDisplVertices.py
@@ -15,10 +15,14 @@ from CommonParticles.Utils import *
 from Configurables import PatPV3D,PVOfflineTool,PVSeed3DTool,LSAdaptPV3DFitter
 from Configurables import RecVertices2Particles,DisplVertices
 from StrippingConf.StrippingLine import StrippingLine
+
+#####################################################################
 ## Charged pions
-ChargedPion = BestPIDParticleMaker("ChargedPartBest", Particle = 'pion' )
-selector = trackSelector(ChargedPion)
+BestPIDParts = BestPIDParticleMaker("ChargedPartBest", Particle = 'pion' )
+selector = trackSelector(BestPIDParts)
 selector.TrackTypes = ['Velo','Long','Upstream']
+
+#####################################################################
 ## PatPV3D
 DisplPatPV3D = PatPV3D("DisplPatPV3D")
 DisplPatPV3D.OutputVerticesName = "Rec/Vertices/DisplRV"
@@ -33,6 +37,8 @@ DisplPatPV3D.PVOfflineTool.PVSeed3DTool.zMaxSpread = 1*mm
 DisplPatPV3D.PVOfflineTool.addTool(LSAdaptPV3DFitter())
 DisplPatPV3D.PVOfflineTool.LSAdaptPV3DFitter.maxIP2PV = 2*mm
 DisplPatPV3D.PVOfflineTool.LSAdaptPV3DFitter.MinTracks = 4
+
+#####################################################################
 ## Preselection
 PreselDisplVertices = RecVertices2Particles("PreselDisplVertices")
 PreselDisplVertices.InputLocations = ["Phys/ChargedPartBest"]
@@ -40,42 +46,37 @@ PreselDisplVertices.RecVerticesLocation = ["Rec/Vertices/DisplRV"]
 PreselDisplVertices.PreyMinMass = 1.0*GeV
 PreselDisplVertices.RMin = 0.3*mm
 PreselDisplVertices.NbTracks = 3 
-PreselDisplVertices.IsolationDistance = 0
-PreselDisplVertices.KeepLowestZ = False
-PreselDisplVertices.RemVtxFromDet = 0*mm
+PreselDisplVertices.RemVtxFromDet = 0
 PreselDisplVertices.RCutMethod = "FromUpstreamPV"
+
+#####################################################################
 ## Single DisplVertex selection
 SingleDV = DisplVertices("SingleDV")
 SingleDV.InputLocations = ["Phys/PreselDisplVertices"]
 SingleDV.RCutMethod = "FromUpstreamPV"
-SingleDV.MC = False
-SingleDV.SaveTuple = False
-SingleDV.SaveTrigInfos = False
-SingleDV.RemVtxFromDet = 0*mm
+SingleDV.RemVtxFromDet = 0
 SingleDV.NbTracks = 5
 SingleDV.PreyMinMass = 6.2*GeV
 SingleDV.PreyMinSumpt = 6.0*GeV
 SingleDV.RMin = 0.3*mm
+
+#####################################################################
 ## Double DisplVertex selection
 DoubleDV = DisplVertices("DoubleDV")
 DoubleDV.InputLocations = ["Phys/PreselDisplVertices"]
 DoubleDV.RCutMethod = "FromUpstreamPV"
-DoubleDV.MC = False
-DoubleDV.SaveTuple = False
-DoubleDV.SaveTrigInfos = False
-DoubleDV.RemVtxFromDet = 0*mm
+DoubleDV.RemVtxFromDet = 0
 DoubleDV.MinNBCands = 2
 DoubleDV.NbTracks = 4
-DoubleDV.PreyMinMass = 3.5*GeV
-DoubleDV.PreyMinSumpt = 2.5*GeV
+DoubleDV.PreyMinMass = 2*GeV
+DoubleDV.PreyMinSumpt = 1.5*GeV
 DoubleDV.RMin = 0.3*mm
+
+#####################################################################
 ## Background study selection
 SelBckgrd = DisplVertices("SelBckgrd")
 SelBckgrd.InputLocations = ["Phys/PreselDisplVertices"]
 SelBckgrd.RCutMethod = "FromUpstreamPV"
-SelBckgrd.MC = False
-SelBckgrd.SaveTuple = False
-SelBckgrd.SaveTrigInfos = False
 SelBckgrd.NbTracks = 5
 SelBckgrd.PreyMinMass = 6*GeV
 SelBckgrd.PreyMinSumpt = 5*GeV
@@ -84,18 +85,18 @@ SelBckgrd.RMin = 3*mm
 # Create line for single DisplVertex selection
 line1 = StrippingLine('SingleDisplVtx'
              , prescale = 1.0
-             , algos = [ChargedPion,DisplPatPV3D,PreselDisplVertices,SingleDV]
+             , algos = [BestPIDParts,DisplPatPV3D,PreselDisplVertices,SingleDV]
              )
 # Create line for double DisplVertex selection
 line2 = StrippingLine('DoubleDisplVtx'
              , prescale = 1.0
-             , algos = [ChargedPion,DisplPatPV3D,PreselDisplVertices,DoubleDV]
+             , algos = [BestPIDParts,DisplPatPV3D,PreselDisplVertices,DoubleDV]
              )
 # Create line for background study selection
-line3 = StrippingLine('BckgrdDisplVtx'
-             , prescale = 1.0
-             , algos = [ChargedPion,DisplPatPV3D,PreselDisplVertices,SelBckgrd]
-             )
+## line3 = StrippingLine('BckgrdDisplVtx'
+##              , prescale = 1.0
+##              , algos = [ChargedPion,DisplPatPV3D,PreselDisplVertices,SelBckgrd]
+##              )
 
 
 
