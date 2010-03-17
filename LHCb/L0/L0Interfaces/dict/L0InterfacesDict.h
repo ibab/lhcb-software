0bba5ade786745c3aef71404c4e35d64eac2caf5
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-03-17 17:23:00 +0000

    add new interface to dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@61774 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fad1893b8b4a59d14a7ba7f0f31d9ef7ad826096

--- LHCb/L0/L0Interfaces/dict/L0InterfacesDict.h
+++ LHCb/L0/L0Interfaces/dict/L0InterfacesDict.h
@@ -6,6 +6,7 @@
 #include "L0Interfaces/IL0DUConfigProvider.h" 
 #include "L0Interfaces/IL0DUEmulatorTool.h" 
 #include "L0Interfaces/IL0DUFromRawTool.h" 
+#include "L0Interfaces/IL0MuonOverflowTool.h" 
 #include "L0Interfaces/IL0ProcessorDataDecoder.h" 
 
 #endif //DICT_L0INTERFACESDICT_H 
