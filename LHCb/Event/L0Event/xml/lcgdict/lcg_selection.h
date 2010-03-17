bed0dd5441755d56b43342fa072c1511a5f1c0d2
Roel Aaij <roel.aaij@cern.ch>
2010-03-17 21:30:50 +0000

    Fix warnings on write to file of L0DUConfig or L0DUReport
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38027 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1b4709d88ca09d4bdf0269e32c81d6e8d085afbe

--- LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
+++ LHCb/Event/L0Event/xml/lcgdict/lcg_selection.h
@@ -1,4 +1,4 @@
-// $Id: lcg_selection.h,v 1.3 2008-09-30 08:05:05 cattanem Exp $
+// $Id: lcg_selection.h,v 1.4 2010-03-17 21:30:50 raaij Exp $
 #ifndef L0EVENT_LCGDICT_H 
 #define L0EVENT_LCGDICT_H 1
 
@@ -11,14 +11,10 @@
 #include "Event/L0DUChannel.h"
 // end include files
 
-namespace {
-  struct _Instantiations {
-    // begin instantiations
-    std::map<std::string,LHCb::L0DUChannel*> l0_1;
-    std::map<std::string,LHCb::L0DUChannel*>::iterator l0_2;
-    std::pair<const std::string,LHCb::L0DUChannel*>  l0_3;
-    // end instantiations
-  };
-}
+template std::map<int,unsigned int>;
+template std::map<std::string,LHCb::L0DUChannel*>;
+template std::map<std::string,LHCb::L0DUTrigger*>;
+template std::map<std::string,LHCb::L0DUChannel*>::iterator;
+template std::pair<const std::string,LHCb::L0DUChannel*>;
 
 #endif // L0EVENT_LCGDICT_H
