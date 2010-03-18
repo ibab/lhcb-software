b74190eb127066cf559cf6613e61afa19ac8a3af
Patrick Robbe <robbe@lal.in2p3.fr>
2010-03-18 20:17:47 +0000

    Use web scripts for shift db interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100408 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6232993d00601bdf63674bb724517b8a35148de9

--- Online/Online/Presenter/src/ShiftDB.cpp
+++ Online/Online/Presenter/src/ShiftDB.cpp
@@ -1,4 +1,4 @@
-// $Id: ShiftDB.cpp,v 1.1 2010-03-10 11:01:49 robbep Exp $
+// $Id: ShiftDB.cpp,v 1.2 2010-03-18 20:17:47 robbep Exp $
 // Include files
 
 // local
@@ -6,14 +6,15 @@
 
 // STL
 #include <iostream>
-#include <ctime>
 
 // Boost
 #include "boost/date_time/posix_time/posix_time_types.hpp"
 #include "boost/date_time/gregorian/formatters.hpp"
-
-// Oracle
-#include "occiControl.h" 
+#include <boost/asio.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/find.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 //-----------------------------------------------------------------------------
 // Implementation file for class : ShiftDB
@@ -24,41 +25,105 @@
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-ShiftDB::ShiftDB(  ) : m_env( 0 ) , m_conn( 0 ) {
-  // Create environment
-  m_env = oracle::occi::Environment::createEnvironment() ;
-  
-  // Create connection
-  try {
-    m_conn = m_env -> createConnection( "shiftdb_reader" , "reader" , "shiftdb" ) ;
-  } catch ( std::exception & exc ) {
-    std::cerr << "Error opening database : " << exc.what() << std::endl ;
-    m_conn = 0 ;
-  }
+ShiftDB::ShiftDB(  ) : m_open( false ) {
+  m_shifters.clear() ;
 }
 
 //=============================================================================
 // Destructor
 //=============================================================================
-ShiftDB::~ShiftDB() {
-  // Close connection
-  if ( 0 != m_conn ) m_env -> terminateConnection( m_conn ) ;
+ShiftDB::~ShiftDB() { }
 
-  // Terminate environment
-  oracle::occi::Environment::terminateEnvironment( m_env ) ;
+//=============================================================================
+// Get content of web page
+//=============================================================================
+void ShiftDB::readWebFile( const boost::gregorian::date * day ) {
+  m_shifters.clear() ;
+
+  boost::asio::ip::tcp::iostream 
+    web_stream( "shiftdb.lbdaq.cern.ch" , "http" ) ;
+
+  if ( ! web_stream ) {
+    std::cerr << "Cannot open shiftdb script via the web" << std::endl ;
+    m_open = false ;
+    return ;
+  }
+
+  // Send HTTP request to web server
+  web_stream << "GET /shiftdb_report.py?format=twiki&"
+	     << "sDate=" << boost::gregorian::to_iso_string( *day ) 
+	     << " HTTP/1.0\r\n" 
+	     << "Host:shiftdb.lbdaq.cern.ch\r\n" 
+	     << "\r\n" << std::flush ;
+
+  std::string line ;
+
+  // Check that the web server answers correctly
+  std::getline( web_stream , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) {
+    std::cerr << "ShiftDB server does not reply" << std::endl ;
+    m_open = false ;
+    return ;
+  }
+
+  // Parse the answer of the web server with | as separator
+  boost::char_separator< char > sep( "|" , "" , boost::keep_empty_tokens ) ;
+  boost::tokenizer< boost::char_separator< char > > tok( line , sep ) ;
+  boost::tokenizer< boost::char_separator< char > >::iterator it ;
+
+  std::string pattern ;
+  pattern = "\\N{vertical-line}(.*)" ;
+  pattern += "\\N{vertical-line}(.*)" ;
+  pattern += "\\N{vertical-line}(.*)" ;
+  pattern += "\\N{vertical-line}(.*)" ; 
+  pattern += "\\N{vertical-line}(.*)" ;
+  pattern += "\\N{vertical-line}(.*)" ;
+  pattern += "\\N{vertical-line}" ;
+
+  const boost::regex e( pattern ) ;
+
+  while( std::getline( web_stream , line ) ) {
+    // Removes spaces at the beginning of the line
+    boost::algorithm::trim_left_if( line , boost::algorithm::is_space() ) ;
+    // Use only lines with correct format
+    if ( ! boost::regex_match( line , e ) ) continue ;
+    // Separate fields
+    tok.assign( line ) ;
+    it = tok.begin() ; ++it ; ++it ;
+    std::string function = (*it) ; ++it ;
+    // Remove spaces before and after
+    boost::algorithm::trim_left_if( function , boost::algorithm::is_space() ) ;
+    boost::algorithm::trim_right_if( function , 
+				     boost::algorithm::is_space() ) ;
+    // Get Names
+    std::vector< std::string > vec ;
+    vec.push_back( *(++it ) ) ;
+    vec.push_back( *(++it ) ) ;
+    vec.push_back( *(++it ) ) ;
+    
+    m_shifters[ function ] = vec ;
+  }
+
+  m_open = true ;
+}
+
+//=============================================================================
+// Get name of the person on shift for a given function and a given slot 
+//=============================================================================
+std::string ShiftDB::getShifter( const std::string & function , 
+				 const int slot ) { 
+  if ( ! m_open ) return function ;
+
+  if ( m_shifters.find( function ) == m_shifters.end() ) 
+    return function ;
+
+  return m_shifters[ function ][ slot ] ;  
 }
 
 //=============================================================================
 // Name of data manager on shift
 //=============================================================================
 std::string ShiftDB::getCurrentDataManager( ) {
-  if ( 0 == m_conn ) return "Data Manager" ;
-
-  std::stringstream sql_statement ;
-  std::string result ;
-  sql_statement << "SELECT PEOPLE.FIRST_NAME, PEOPLE.NAME, SHIFTS.FN_SLOT " ;
-  sql_statement << "FROM SHIFTS, PEOPLE " ;
-  sql_statement << "WHERE (SHIFTS.START_TIME=TO_CHAR(TO_DATE('" ;
 
   boost::posix_time::ptime now = 
     boost::posix_time::second_clock::local_time() ;
@@ -68,29 +133,13 @@ std::string ShiftDB::getCurrentDataManager( ) {
 
   if ( now.time_of_day().hours() <= 7 ) {
     now_day = now.date() - boost::gregorian::date_duration( 1 ) ;
-    slot = 4 ;
-  } else if ( now.time_of_day().hours() <= 15 ) slot = 1 ;
-  else if ( now.time_of_day().hours() <= 23 ) slot = 2 ;
-  else slot = 4 ;
+    slot = 2 ;
+  } else if ( now.time_of_day().hours() <= 15 ) slot = 0 ;
+  else if ( now.time_of_day().hours() <= 23 ) slot = 1 ;
+  else slot = 2 ;
 
-  sql_statement << boost::gregorian::to_simple_string( now_day ) ;
+  std::map< std::string , std::vector< std::string > > shifters ;
+  readWebFile( &now_day ) ;
 
-  sql_statement << "','YYYY-Mon-DD')) AND SHIFTS.PID=PEOPLE.ID " 
-		<< "AND SHIFTS.FN_ID=2 AND SHIFTS.FN_SLOT="
-		<< slot << ")" ;
-  
-  try {
-    oracle::occi::Statement * stmt = 
-      m_conn -> createStatement( sql_statement.str().c_str() ) ; 
-    oracle::occi::ResultSet * rs = stmt -> executeQuery() ;
-    rs -> next() ;
-    result = ( rs -> getString( 1 ) + " " + rs -> getString( 2 ) ) ; 
-    stmt -> closeResultSet( rs ) ;
-    m_conn -> terminateStatement( stmt ) ;
-  } catch ( std::exception & exc ) {
-    std::cerr << "Error in SQL statement : " << exc.what() << std::endl ;
-    return "Data Manager" ;
-  }
-
-  return result ;
+  return getShifter( "Data Manager" , slot ) ;
 }
--- Online/Online/Presenter/src/ShiftDB.h
+++ Online/Online/Presenter/src/ShiftDB.h
@@ -1,15 +1,16 @@
-// $Id: ShiftDB.h,v 1.1 2010-03-10 11:01:49 robbep Exp $
+// $Id: ShiftDB.h,v 1.2 2010-03-18 20:17:47 robbep Exp $
 #ifndef SHIFTDB_H 
 #define SHIFTDB_H 1
 
 // Include files
 #include <string>
+#include <map>
+#include <vector>
 
 // Forward declarations
-namespace oracle {
-  namespace occi {
-    class Environment ;
-    class Connection ;
+namespace boost {
+  namespace gregorian {
+    class date ;
   };
 };
 
@@ -29,12 +30,22 @@ class ShiftDB {
   virtual ~ShiftDB( ); ///< Destructor
 
   /// Name of the data manager on shift now
-  std::string getCurrentDataManager() ; 
+  std::string getCurrentDataManager() ;
 
 protected:
 
 private:
-  oracle::occi::Environment * m_env ;
-  oracle::occi::Connection  * m_conn ;
+  /// Get name of people on shift in a given slot
+  std::string getShifter( const std::string & function , 
+			  const int slot ) ;
+
+  /// Read and interpret web file obtained from ShiftDB web server 
+  void readWebFile( const boost::gregorian::date * day ) ;
+
+  /// Pointer to shift db web script file
+  bool m_open ;
+
+  /// Map of people on shift
+  std::map< std::string , std::vector< std::string > > m_shifters ;
 };
 #endif // SHIFTDB_H
