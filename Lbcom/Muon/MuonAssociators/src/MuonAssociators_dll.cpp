c5909cfa9c974e04586772f5efef0ac794721d04
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-03-18 14:29:04 +0000

    v3r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@22171 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 57514a81d848d0da27dc7d4cfd97dcdb151dc57e

--- Lbcom/Muon/MuonAssociators/src/MuonAssociators_dll.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: MuonAssociators_dll.cpp,v 1.1.1.1 2002-06-28 09:59:33 dhcroft Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES(MuonAssociators)
-
