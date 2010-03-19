6f92073b147ecb773b420fd84d5495cd60a7e8cd
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-03-19 12:52:56 +0000

    RealPi0: add examples with recalibration of Ecal and reading of femto-DST
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@89401 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 921d4d6c25d3d66a6547ae0296e06257f57490f2

--- Bender/Ex/BenderExample/python/BenderExample/RealPi0_Re.py
+++ Bender/Ex/BenderExample/python/BenderExample/RealPi0_Re.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: RealPi0.py,v 1.6 2010-03-19 12:52:56 ibelyaev Exp $
+# $Id: RealPi0_Re.py,v 1.1 2010-03-19 12:52:56 ibelyaev Exp $
 # =============================================================================
 # @file BenderExampele/RealPi0.py
 #
-# Attempt to recontruct pi0 on real data
+# Attempt to recontruct pi0 on real data using 
+# private recalibration of Ecal/Prs
+#
+#  - Prs-coefficients are taken from Olivier Deschamps
+#  - Ecal coefficients are extracted using Kali
+# 
 #
 #  This file is a part of 
 #  <a href="http://cern.ch/lhcb-comp/Analysis/Bender/index.html">Bender project</a>
@@ -27,6 +32,11 @@
 """
 
 Attempt to recontruct pi0 on real data
+using private recalibration of Ecal/Prs:
+
+- Prs-coefficients are taken from Olivier Deschamps
+- Ecal coefficients are extracted using Kali
+
 
 This file is a part of BENDER project:
 ``Python-based Interactive Environment for Smart and Friendly Physics Analysis''
@@ -45,12 +55,14 @@ with the campain of Dr.O.Callot et al.:
 # ============================================================================
 __author__  = " Vanya BELYAEV Ivan.Belyaev@nikhef.nl "
 __date__    = " 2009-11-25 "
-__version__ = " CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.6 $ "
+__version__ = " CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $ "
 # ============================================================================
 ## import all needed stuff:
 import ROOT                           ## needed to produce/visualize the histograms
+
 from   Bender.Main         import *   ## import the ba
 import LHCbMath.Types                 ## easy access to various geometry routines 
+
 from   Gaudi.Configuration import *   ## needed for job configuration
 
 from   GaudiKernel.SystemOfUnits import GeV, MeV
@@ -61,114 +73,9 @@ Gaudi = cpp.Gaudi
 
 Photons = LoKi.Photons
 
-fitPi0 = None
-getPi0 = None
+## import the algorithm 
+from BenderExample.RealPi0 import RealPi0 
 
-try :
-    
-    from KaliCalo.Pi0HistoFit import fitPi0, getPi0Params
-    
-except ImportError :
-    
-    fitPi0 = None
-    getPi0 = None
-
-    
-# =============================================================================
-## @class RealPi0
-#  Simple Bender-based algorithm to reconstruct pi0 on real data 
-#  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
-#  @date 2009-11-25
-class RealPi0(Algo) :
-    """
-    Simple Bender-based algorithm to reconstruct pi0 on real data 
-    """
-
-    def initialize ( self ) :
-        
-        sc = Algo.initialize ( self )
-        if sc.isFailure() : return sc 
-
-        self.beamLine = Gaudi.Math.XYZLine ( Gaudi.Math.XYZPoint (0,0,0) ,
-                                             Gaudi.Math.XYZVector(0,0,1) )
-        return SUCCESS
-    
-    ## the major analysis method 
-    def analyse  (self ) :
-        """
-        The major analysis method
-        """
-        
-        clusters = self.get ( 'Rec/Calo/EcalClusters' )
-        photons  = self.get ( 'Rec/Calo/Photons'      )
-        protos   = self.get ( 'Rec/ProtoP/Neutrals'   )
-
-        gammas = self.select('g' , 'gamma' == ID  )
-
-        tup = self.nTuple('RealPi0')
-        
-        pi0s = self.loop( ' g g ', 'pi0')
-        for pi0 in pi0s :
-
-            m12 = pi0.mass(1,2) 
-            if m12 > 1.0 * GeV : continue
-
-            if m12 < 500 * MeV :
-                self.plot ( m12 , 'gamma gamma 2' , 0 , 500 * MeV , 250 )
-            if m12 < 250 * MeV :
-                self.plot ( m12 , 'gamma gamma 3' , 0 , 250 * MeV , 250 )
-                    
-            g1 = pi0(1)
-            g2 = pi0(2)
-
-            prs1 = Photons.energyFrom     ( g1 , 'Prs') 
-            prs2 = Photons.energyFrom     ( g2 , 'Prs') 
-
-            spd1 = Photons.seedEnergyFrom ( g1 , 'Spd') 
-            spd2 = Photons.seedEnergyFrom ( g2 , 'Spd') 
-
-            p = pi0.momentum(1,2)
-            
-            tup.column ( 'm12'  , m12           )
-            tup.column ( 'p'    , p             )
-            tup.column ( 'g1'   , g1.momentum() )
-            tup.column ( 'g2'   , g2.momentum() )
-            tup.column ( 'pt1'  , PT ( g1 ) )
-            tup.column ( 'pt2'  , PT ( g2 ) )
-
-            tup.column ( 'prs1' , prs1 )
-            tup.column ( 'prs2' , prs2 )
-            
-            tup.column ( 'spd1' , spd1 )
-            tup.column ( 'spd2' , spd2 )
-
-            tup.write()
-            
-            if   m12 < 250 * MeV  and max ( prs1 ,prs2 ) < 10 :
-                self.plot ( m12 , 'gamma gamma 4 - LL' , 0 , 250 *MeV  , 250 )
-            elif m12 < 250 * MeV  and min ( prs1 ,prs2 ) > 10 :
-                self.plot ( m12 , 'gamma gamma 4 - GG' , 0 , 250 *MeV  , 250 )
-            elif m12 < 250 * MeV  :
-                self.plot ( m12 , 'gamma gamma 4 - LG' , 0 , 250 *MeV  , 250 )
-          
-        return SUCCESS 
-
-    ## finalize & print histos 
-    def finalize ( self ) :
-        """
-        Finalize & print histos         
-        """
-        histos = self.Histos()
-        for key in histos :
-            h = histos[key]
-            if hasattr ( h , 'dump' ) :
-                print h.dump(50,30,True)
-                if fitPi0 and getPi0Params : 
-                    fitPi0( h )
-                    print 'FIT pi0:' , getPi0Params ( h )
-                    
-        return Algo.finalize ( self )
-  
 # =============================================================================
 ## the actual job configuration
 def configure ( datafiles , catalogs = [] ) :
@@ -186,9 +93,41 @@ def configure ( datafiles , catalogs = [] ) :
         DataType     = '2009' ,             ## NB:    2k+9
         Simulation   = False  ,             ## NB:   *REAL DATA*
         HistogramFile = 'RealPi0_Histos.root' ,
-        ##
+        EnableUnpack = False ,
+        ## 
         )
+
+    ## apply calibration coefficients to Ecal+Prs 
+    import shelve
+    db_location = '/afs/cern.ch/lhcb/group/calo/ecal/vol10/ECAL-2k+9/'
+    dbase = shelve.open( db_location + 'Ecal_2k+10_Mar_18_db','r')
+    ecal = dbase['Ecal-2k+9']
+    prs  = dbase['Prs-2k+9' ]
+
+    for c in prs  : print c , prs [c]
+    for c in ecal : print c , ecal[c]
     
+    from Configurables import Kali__MisCalibrateCalo
+    davinci.UserAlgorithms += [
+        Kali__MisCalibrateCalo ( 'KaliPrs'  , Coefficients = prs  ) ,
+        Kali__MisCalibrateCalo ( 'KaliEcal' , Coefficients = ecal ) 
+        ]
+    
+    ## re-reconstruct Photons 
+    from Configurables import OffLineCaloRecoConf
+    calo = OffLineCaloRecoConf ()
+    calo.RecList += [ "Photons" ]
+    davinci.UserAlgorithms += [ calo.photons() ] 
+    
+    ## re-reconstruct NeutralPtotoP
+    from Configurables           import NeutralProtoPAlg
+    davinci.UserAlgorithms += [
+        NeutralProtoPAlg (
+        "NeutralProtoPMaker"  , 
+        ## LightMode      = True ,               ## "light-mode", no PIDs
+        HyposLocations = [ 'Rec/Calo/Photons']   ## only single photons 
+        )
+        ]
     
     from GaudiConf.Configuration import NTupleSvc  
     NTupleSvc (
@@ -211,11 +150,11 @@ def configure ( datafiles , catalogs = [] ) :
     alg = RealPi0(
         'RealPi0'        ,
         NTupleLUN = 'PI0' ,
-        InputLocations = [ 'StdLooseAllPhotons' ] ,
-        ##
+        InputLocations = [ 'StdLooseAllPhotons' ] 
         )
     
-    gaudi.setAlgorithms ( [ alg ] ) 
+    ##gaudi.setAlgorithms ( [ alg ] ) 
+    gaudi.addAlgorithm ( alg  ) 
     
     return SUCCESS 
 
@@ -232,7 +171,7 @@ if '__main__' == __name__ :
     print ' Date    : %s ' %   __date__
     print ' dir(%s) : %s ' % ( __name__    , dir() )
     print '*'*120  
-
+    
     import BenderExample.Data2009Reco07
     
     ##
@@ -256,7 +195,7 @@ if '__main__' == __name__ :
 ##     files = [ prefix + f for f in _files ]
 ##     configure  (files ) 
     
-    run ( 1000 )
+    run ( 5000 )
     
 # ============================================================================
 # The END 
