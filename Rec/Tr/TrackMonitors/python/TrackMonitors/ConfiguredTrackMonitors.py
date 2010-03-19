210eb2b3fdeeae29270ea23f3c51d1d799031867
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-03-19 15:22:08 +0000

    - add TTTrackMonitor - add ITTrackMonitor - add TrackITOverlapMonitor - remove all algorithms for "ONLINE" except TrackMonitor and TrackDiMuonMonitor
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@20103 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33acbd994b2521beaf523b8b3cb35e515f3739ca

--- Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
+++ Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
@@ -2,36 +2,44 @@ from Configurables import (GaudiSequencer, TrackMonitor, TrackVertexMonitor,
                            TrackAlignMonitor,
                            TrackFitMatchMonitor,TrackV0Monitor,TrackDiMuonMonitor,
                            OTTrackMonitor, OTHitEfficiencyMonitor,OTTimeMonitor,
-                           TrackCaloMatchMonitor,TrackMuonMatchMonitor)
-from Configurables import (RecSysConf, TrackSys)
+                           TrackCaloMatchMonitor,TrackMuonMatchMonitor,
+                           TrackITOverlapMonitor,
+                           TTTrackMonitor, ITTrackMonitor)
+from Configurables import (RecSysConf, RecMoniConf, TrackSys)
 
 def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
                                    HistoPrint = False ):
     seq = GaudiSequencer(Name)
     seq.Members.append( TrackMonitor(HistoPrint=HistoPrint) )
-    seq.Members.append( TrackVertexMonitor(HistoPrint=HistoPrint) )
-    seq.Members.append( TrackFitMatchMonitor(HistoPrint=HistoPrint) )
-    seq.Members.append( TrackV0Monitor(HistoPrint=HistoPrint) )
     seq.Members.append( TrackDiMuonMonitor(HistoPrint=HistoPrint) )
-    seq.Members.append( TrackAlignMonitor(HistoPrint=HistoPrint) )
 
-    if "CALO" in RecSysConf().RecoSequence:
-        seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal', HistoPrint=HistoPrint) )
-        #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
-        seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
-        seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
+    if not RecMoniConf().getProp("Histograms") is "Online":
+        seq.Members.append( TrackVertexMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( TrackV0Monitor(HistoPrint=HistoPrint) )
+        seq.Members.append( TrackFitMatchMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( TrackAlignMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( TrackITOverlapMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( TTTrackMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( ITTrackMonitor(HistoPrint=HistoPrint) )
 
-    if "MUON" in RecSysConf().RecoSequence:
-        seq.Members.append(TrackMuonMatchMonitor("TrackMuonMatchMonitor", HistoPrint=HistoPrint))
+        if "CALO" in RecSysConf().RecoSequence:
+            seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal', HistoPrint=HistoPrint) )
+            #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
+            seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
+            seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
+
+        if "MUON" in RecSysConf().RecoSequence:
+            seq.Members.append(TrackMuonMatchMonitor("TrackMuonMatchMonitor", HistoPrint=HistoPrint))
 
     return seq
     
 def ConfiguredOTMonitorSequence(Name = "MoniOTSeq",
                                 HistoPrint = False ):
     seq = GaudiSequencer(Name)
-    seq.Members.append( OTTrackMonitor(HistoPrint=HistoPrint) )
-    seq.Members.append( OTHitEfficiencyMonitor(HistoPrint=HistoPrint) )
     seq.Members.append( OTTimeMonitor(HistoPrint=HistoPrint) )
+    if not RecMoniConf().getProp("Histograms") is "Online":
+        seq.Members.append( OTTrackMonitor(HistoPrint=HistoPrint) )
+        seq.Members.append( OTHitEfficiencyMonitor(HistoPrint=HistoPrint) )
 
     from Configurables import TrackSys
     if TrackSys().cosmics():
