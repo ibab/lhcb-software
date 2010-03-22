e8bd5831ce817bf1169ceec95cbb2a3e87d44f5d
Jaap Panman <panmanj@cern.ch>
2010-03-22 14:38:36 +0000

    add qmtests for xml summary - new files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@31435 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ebbb97983c550aca4e2be0d89b12fc07b893131d

---
+++ Rec/Rec/LumiAlgs/job/LumiFsrIntegrator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env gaudirun.py
+#
+# Minimal file for running LumiFsrIntegrator from python prompt
+# Syntax is:
+#   gaudirun.py ../job/LumiFsrIntegrator.py
+# or just
+#   ../job/LumiFsrIntegrator.py
+#
+import os
+from Gaudi.Configuration import *
+
+#--- switch on xml summary
+from Configurables import LHCbApp
+LHCbApp().XMLSummary = 'summary.xml'
+
+#--- determine application to run
+from Configurables import LumiAlgsConf
+
+# standard sequence from configurable
+LumiAlgsConf().LumiSequencer = GaudiSequencer("LumiSeq", ShortCircuit = False )
+LumiAlgsConf().InputType = 'DST'
+LumiAlgsConf().OutputLevel =  INFO
+
+#-- main
+ApplicationMgr( TopAlg = [ GaudiSequencer( "LumiSeq", OutputLevel = INFO ), ], HistogramPersistency = 'NONE' )
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
+#-- Test input
+files = [ "DATAFILE='PFN:testFSRout.dst' TYP='POOL_ROOTTREE'  OPT='REC' " ]
+#-- input options
+ApplicationMgr().EvtMax =  1000
+importOptions("$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts")
+EventSelector( OutputLevel  = INFO, 
+               PrintFreq    = 1000,
+               FirstEvent   = 1,
+               Input        = files
+               )
