19b20446ebe6b95d8e987fb449d717c2477f44e2
jpalac <jpalac@cern.ch>
2010-03-23 17:18:10 +0000

    Add Lesser GPL information.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@12569 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 25c68034eae962df7f435d6839568ca78ca14601

--- Analysis/Analysis/PyAna/python/PyAna/pyntuple/write.py
+++ Analysis/Analysis/PyAna/python/PyAna/pyntuple/write.py
@@ -1,5 +1,22 @@
 '''
+Helper functions for writing and opening ntuples.
+
+Copyright (c) 2010 Juan Palacios juan.palacios.puyana@gmail.com
+Subject to the Lesser GNU Public License - see < http://www.gnu.org/licenses/lgpl.html>
 '''
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Juan Palacios <juan.palacios@nikhef.nl>'
 __version__ = '1.0'
