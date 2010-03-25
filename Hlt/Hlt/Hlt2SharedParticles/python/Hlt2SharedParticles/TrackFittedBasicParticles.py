edc1b709367bde25c377ba4ffc03e111d5933658
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-03-25 21:43:53 +0000

    - set WriteP2PVRelations to False for ParticleMakers to avoid accessing a PV   which may or may not be present...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@82470 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9c1c9fce4529bb6deb11c22a2b2ea9db913cedb0

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/TrackFittedBasicParticles.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/TrackFittedBasicParticles.py
@@ -59,6 +59,7 @@ Hlt2BiKalmanFittedMuons.addTool(ProtoParticleMUONFilter('Muon'))
 Hlt2BiKalmanFittedMuons.Input 				=  BiKalmanFittedMuonProtoMaker.outputSelection()
 Hlt2BiKalmanFittedMuons.Particle 			= "muon"
 Hlt2BiKalmanFittedMuons.Muon.Selection 			= ["RequiresDet='MUON' IsMuon=True"]
+Hlt2BiKalmanFittedMuons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the pions
@@ -66,6 +67,7 @@ Hlt2BiKalmanFittedMuons.Muon.Selection 			= ["RequiresDet='MUON' IsMuon=True"]
 Hlt2BiKalmanFittedPions 				= NoPIDsParticleMaker("Hlt2BiKalmanFittedPions")
 Hlt2BiKalmanFittedPions.Particle 			=  "pion"
 Hlt2BiKalmanFittedPions.Input 				=  BiKalmanFittedChargedProtoMaker.outputSelection()
+Hlt2BiKalmanFittedPions.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the downstream pions
@@ -73,6 +75,7 @@ Hlt2BiKalmanFittedPions.Input 				=  BiKalmanFittedChargedProtoMaker.outputSelec
 Hlt2BiKalmanFittedDownPions 				= NoPIDsParticleMaker("Hlt2BiKalmanFittedDownPions")
 Hlt2BiKalmanFittedDownPions.Particle 			=  "pion"
 Hlt2BiKalmanFittedDownPions.Input 			=  BiKalmanFittedChargedDownProtoMaker.outputSelection()
+Hlt2BiKalmanFittedDownPions.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the kaons
@@ -80,6 +83,7 @@ Hlt2BiKalmanFittedDownPions.Input 			=  BiKalmanFittedChargedDownProtoMaker.outp
 Hlt2BiKalmanFittedKaons 				= NoPIDsParticleMaker("Hlt2BiKalmanFittedKaons")
 Hlt2BiKalmanFittedKaons.Particle 			=  "kaon"
 Hlt2BiKalmanFittedKaons.Input 				=  BiKalmanFittedChargedProtoMaker.outputSelection()
+Hlt2BiKalmanFittedKaons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the protons
@@ -87,6 +91,7 @@ Hlt2BiKalmanFittedKaons.Input 				=  BiKalmanFittedChargedProtoMaker.outputSelec
 Hlt2BiKalmanFittedProtons 				= NoPIDsParticleMaker("Hlt2BiKalmanFittedProtons")
 Hlt2BiKalmanFittedProtons.Particle 			=  "proton"
 Hlt2BiKalmanFittedProtons.Input 			=  BiKalmanFittedChargedProtoMaker.outputSelection()
+Hlt2BiKalmanFittedProtons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the downstream protons
@@ -94,6 +99,7 @@ Hlt2BiKalmanFittedProtons.Input 			=  BiKalmanFittedChargedProtoMaker.outputSele
 Hlt2BiKalmanFittedDownProtons 				= NoPIDsParticleMaker("Hlt2BiKalmanFittedDownProtons")
 Hlt2BiKalmanFittedDownProtons.Particle 			=  "proton"
 Hlt2BiKalmanFittedDownProtons.Input 			=  BiKalmanFittedChargedDownProtoMaker.outputSelection()
+Hlt2BiKalmanFittedDownProtons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the Rich kaons 
@@ -101,6 +107,7 @@ Hlt2BiKalmanFittedDownProtons.Input 			=  BiKalmanFittedChargedDownProtoMaker.ou
 Hlt2BiKalmanFittedRichKaons 				= CombinedParticleMaker("Hlt2BiKalmanFittedRichKaons")
 Hlt2BiKalmanFittedRichKaons.Particle 			=  "kaon"
 Hlt2BiKalmanFittedRichKaons.Input 			= BiKalmanFittedChargedRichHadronProtoMaker.outputSelection() 
+Hlt2BiKalmanFittedRichKaons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # Make the electrons
@@ -110,6 +117,7 @@ Hlt2BiKalmanFittedElectrons.addTool(ProtoParticleCALOFilter('Electron'))
 Hlt2BiKalmanFittedElectrons.Particle 			=  "electron"
 Hlt2BiKalmanFittedElectrons.Input 			=  BiKalmanFittedChargedCaloProtoMaker.outputSelection()
 Hlt2BiKalmanFittedElectrons.Electron.Selection 		= ["RequiresDet='CALO' CombDLL(e-pi)>'-2.0'"]
+Hlt2BiKalmanFittedElectrons.WriteP2PVRelations 	=  False
 ##########################################################################
 #
 # define exported symbols -- these are for available
