e2052adf4154184d617c2f7878abd455830c390d
Gloria Corti <gloria.corti@cern.ch>
2010-03-25 18:20:29 +0000

    use latest tag for 2009 data
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@94922 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df3b7c5a735cc5cd1356ab43bbd999de42b5315a

--- Gauss/Sim/Gauss/options/Gauss-MC-2009.py
+++ Gauss/Sim/Gauss/options/Gauss-MC-2009.py
@@ -14,5 +14,6 @@ importOptions("$APPCONFIGOPTS/Gauss/Beam450GeV-md100-fix1.py")
 ##############################################################################
 from Configurables import LHCbApp
 LHCbApp().DDDBtag   = "head-20100119"
-LHCbApp().CondDBtag = "MC-20100205-vc15mm-md100"
+#LHCbApp().CondDBtag = "MC-20100205-vc15mm-md100"  # as in gauss v38r3
+LHCbApp().CondDBtag = "MC-2010321-vc15mm-md100"  # latest simcond for 2009
 
