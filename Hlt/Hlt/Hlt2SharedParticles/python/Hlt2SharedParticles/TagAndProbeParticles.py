ecf2c4b2cd63ad68fb3676513bfdd88f34dcd171
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2010-03-25 18:48:04 +0000

    Add tag-and-probe particles for Muon ID
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@82458 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5965226aced985c72bf2bb2afb16549fd63d7859

---
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/TagAndProbeParticles.py
@@ -0,0 +1,56 @@
+### @file
+#
+#  Charged particles for the tag-and-probe muon lines
+#
+#  @author V. Gligorov vladimir.gligorov@cern.ch
+#  @date 2010-03-25
+#
+##
+# =============================================================================
+__author__  = "V. Gligorov vladimir.gligorov@cern.ch"
+# =============================================================================
+from Gaudi.Configuration import *
+from HltLine.HltLine import bindMembers
+from Configurables import CombinedParticleMaker
+from Configurables import ProtoParticleMUONFilter
+from Configurables import GaudiSequencer
+from GaudiKernel.SystemOfUnits import MeV
+#
+# These are all based on unfitted tracks
+#
+from HltTracking.Hlt2TrackingConfigurations import Hlt2UnfittedForwardTracking
+Hlt2UnfittedForwardTracking = Hlt2UnfittedForwardTracking()
+##########################################################################
+#
+# Charged protoparticles -> pulls all the pid
+#
+caloProtos 		= Hlt2UnfittedForwardTracking.hlt2ChargedCaloProtos( )
+muonWithCaloProtos 	= Hlt2UnfittedForwardTracking.hlt2ChargedMuonWithCaloProtos( )
+##########################################################################
+# Make the pions
+#
+Hlt2TagAndProbePions = CombinedParticleMaker("Hlt2TagAndProbePions")
+Hlt2TagAndProbePions.Input =  caloProtos.outputSelection()
+Hlt2TagAndProbePions.Particle =  "pion" 
+Hlt2TagAndProbePions.WriteP2PVRelations = False
+##########################################################################
+# Make the Muons
+#
+Hlt2TagAndProbeMuons = CombinedParticleMaker("Hlt2TagAndProbeMuons")
+Hlt2TagAndProbeMuons.Particle = "muon" 
+Hlt2TagAndProbeMuons.addTool(ProtoParticleMUONFilter('Muon'))
+Hlt2TagAndProbeMuons.Muon.Selection = ["RequiresDet='MUON' IsMuon=True" ]
+Hlt2TagAndProbeMuons.Input =  muonWithCaloProtos.outputSelection()
+Hlt2TagAndProbeMuons.WriteP2PVRelations = False
+##########################################################################
+#
+# define exported symbols -- these are for available
+# for use in Hlt2 by adding:
+#
+# from Hlt2SharedParticles.TagAndProbeParticles import TagAndProbeMuons
+#
+
+__all__ = ( 'TagAndProbePions', 'TagAndProbeMuons' )
+
+TagAndProbePions   = bindMembers( None, [ caloProtos		,	Hlt2TagAndProbePions 	] )
+TagAndProbeMuons   = bindMembers( None, [ muonWithCaloProtos	, 	Hlt2TagAndProbeMuons	] )
