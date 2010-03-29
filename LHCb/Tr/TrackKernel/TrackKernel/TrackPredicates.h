7dcddd6e18c642144e935e8a81070726e8fb5814
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-03-29 09:19:00 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@20169 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 754edaa0e5fd7508410c16e8c876e42a8c3efe9d

---
+++ LHCb/Tr/TrackKernel/TrackKernel/TrackPredicates.h
@@ -0,0 +1,37 @@
+#include "Event/Track.h"
+
+namespace TrackPredicates
+{
+  struct Type
+  {
+    int m_type ;
+    Type(int atype) : m_type(atype) {} 
+    bool operator()(const LHCb::Track* track) const { return track->type()==m_type ; }
+  } ;
+
+  struct Flag
+  {
+    LHCb::Track::Flags m_flag ;
+    bool m_positive ;
+    Flag(LHCb::Track::Flags flag, bool positive=true) : m_flag(flag)
+								    , m_positive(positive) {} 
+    bool operator()(const LHCb::Track* track) const { 
+      return m_positive ? track->checkFlag(m_flag) : !track->checkFlag(m_flag) ; }
+  } ;
+
+  struct VeloSide
+  {
+    int m_sign ;
+    VeloSide(int asign) : m_sign(asign) {}
+    bool operator()(const LHCb::Track* track) const { 
+      return track->firstState().tx()*m_sign*(track->checkFlag(LHCb::Track::Backward)? -1 : 1 )>0 ; }
+  } ;
+
+  struct MaxChisqPerDoF
+  {
+    double m_maxchisq ;
+    MaxChisqPerDoF(double maxChisqPerDof) : m_maxchisq(maxChisqPerDof) {}
+    bool operator()(const LHCb::Track* track) const { 
+      return track->chi2PerDoF() <m_maxchisq  ; }
+  } ;
+};
