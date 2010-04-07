1ee423af280e200a93dbffa913f54250a3b06eb5
Sascha Stahl <sascha.stahl@cern.ch>
2010-04-07 14:44:18 +0000

    modified early data tuning for PatDownstream
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@20189 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5fd19673ba30642f089f60de9ec36083badc2aca

--- Rec/Tf/PatAlgorithms/options/PatDownstream.py
+++ Rec/Tf/PatAlgorithms/options/PatDownstream.py
@@ -4,9 +4,9 @@ from Configurables import  PatDownstream
 
 if TrackSys().earlyData():       
     PatDownstream().xPredTol2 = 20.0
-    PatDownstream().TolMatch = 3.5
-    PatDownstream().TolUV = 4.0
-    PatDownstream().maxWindowSize = 25.0
+    PatDownstream().TolMatch = 1.5
+    PatDownstream().TolUV = 2.0
+    PatDownstream().maxWindowSize = 10.0
     PatDownstream().MaxChisq  = 20.0
     PatDownstream().MaxDistance = 0.3
     PatDownstream().deltaP = 2.0
