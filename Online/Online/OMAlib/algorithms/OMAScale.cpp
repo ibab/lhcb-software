ed5fe2741803e117d7039c1fb73be63625672686
Giacomo Graziani <ggiacomo@cern.ch>
2010-04-12 16:00:25 +0000

    bug fix in OMAScale for 2D histograms
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100496 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f75984a92b36aa22646883d0995e6d0c0de9211a

--- Online/Online/OMAlib/algorithms/OMAScale.cpp
+++ Online/Online/OMAlib/algorithms/OMAScale.cpp
@@ -1,4 +1,4 @@
-// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OMAlib/algorithms/OMAScale.cpp,v 1.7 2010-02-12 14:25:39 ggiacomo Exp $
+// $Header: /afs/cern.ch/project/cvs/reps/lhcb/Online/OMAlib/algorithms/OMAScale.cpp,v 1.8 2010-04-12 16:00:25 ggiacomo Exp $
 #include <TH1F.h>
 #include <TH2F.h>
 #include "OMAlib/OMAAlgorithms.h"
@@ -32,14 +32,14 @@ TH1* OMAScale::exec( const std::vector<TH1*> *sources,
   TH1* H = sources->at(0);
   TH1* scalefactorH = sources->at(1);
   TH1* scaledHist=existingHisto;
-  bool copied=false;
+  bool cloned= false;
   int shift=0;
+  }
   if(!scaledHist) {
-    if( std::string(scalefactorH->GetName()) != std::string(H->GetName()) ||
-       (dynamic_cast<TH2*>(H) != NULL)) {
+    if( ( std::string(scalefactorH->GetName()) != std::string(H->GetName()) ) || (dynamic_cast<TH2*>(H) != NULL) ) {
       scaledHist=(TH1*) H->Clone();
       scaledHist->SetNameTitle(outName.data(),outTitle.data() );
-      copied = true;
+      cloned = true;
     }
     else {
       scaledHist= (TH1*) ( new TH1D 
@@ -50,14 +50,15 @@ TH1* OMAScale::exec( const std::vector<TH1*> *sources,
       scaledHist->Sumw2();
     }
   }  
-  if (scaledHist && !copied) {
+  if (scaledHist && !cloned) {
     shift=H->GetNbinsX()-scaledHist->GetNbinsX();
-    for (int ib=1+shift; ib<=H->GetNbinsX(); ib++) {
-      scaledHist->SetBinContent(ib-shift,  H->GetBinContent(ib));
+    for (int ix=1+shift; ix<= H->GetNbinsX(); ix++) {
+      for (int iy=1 ; iy<= H->GetNbinsY(); iy++) {
+        scaledHist->SetBinContent(ix-shift, iy, H->GetBinContent(ix,iy));
+      }
     }
   }
   double normK= scalefactorH->GetBinContent(1) * k;
-  
   if (scaledHist && normK > 0.)
     scaledHist->Scale(1./normK);
   
