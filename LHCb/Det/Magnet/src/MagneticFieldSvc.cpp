918e6fb10b2d5b67d16583a19a6e5654e6514da0
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-04-13 11:40:03 +0000

    restored v1.48
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38055 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ef324a9adf6f5b203afdae35a0b6c74f4f45dc9

--- LHCb/Det/Magnet/src/MagneticFieldSvc.cpp
+++ LHCb/Det/Magnet/src/MagneticFieldSvc.cpp
@@ -1,4 +1,4 @@
-// $Id: MagneticFieldSvc.cpp,v 1.49 2010-04-13 11:36:00 wouter Exp $
+// $Id: MagneticFieldSvc.cpp,v 1.50 2010-04-13 11:40:03 wouter Exp $
 
 // Include files
 #include "GaudiKernel/SvcFactory.h"
@@ -168,10 +168,6 @@ StatusCode MagneticFieldSvc::initializeWithCondDB()
         << "Requested condDB but using manually set field map file name(s) = "
         << m_mapFileNames << endmsg;
     m_mapFromOptions = true;
-    StatusCode sc = m_mapFileNames.size() == 1 ? 
-      m_magFieldGridReader.readDC06File( m_mapFileNames.front(), m_magFieldGrid ) : 
-      m_magFieldGridReader.readFiles( m_mapFileNames, m_magFieldGrid ) ; 
-    if( !sc.isSuccess() ) return sc ;
   }
   else {
     m_updMgrSvc->registerCondition( this, MagnetCondLocations::FieldMapFilesUp,
