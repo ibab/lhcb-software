66d6d8f94c39f576132af2e91edd21fd81731135
Anton Poluektov <anton.poluektov@cern.ch>
2010-04-15 15:35:09 +0000

    Fixed Bs2PhiPhi line
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@82998 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 339e4ed8e0048703f912671bee962e4dbf24ae40

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2PhiPhi.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2PhiPhi.py
@@ -1,8 +1,8 @@
-# $Id: StrippingBs2PhiPhi.py,v 1.4 2010-01-22 13:04:07 schleich Exp $
+# $Id: StrippingBs2PhiPhi.py,v 1.5 2010-04-15 15:35:09 poluekt Exp $
 
 __author__ = 'Sebastian Schleich'
 __date__ = '2010/01/22'
-__version__ = '$Revision: 1.4 $'
+__version__ = '$Revision: 1.5 $'
 
 '''
 Bs->PhiPhi stripping selection
@@ -29,6 +29,8 @@ class StrippingBs2PhiPhiConf(LHCbConfigurableUser):
                 , 'BsMassWindow'        : 300      # MeV
                 , 'WidePrescale'        : 0.06     # adimensional
                 }
+                
+    _BsPhiPhiLine = None
 
 
     def normal( self ) :
@@ -51,32 +53,36 @@ class StrippingBs2PhiPhiConf(LHCbConfigurableUser):
 
 
     def BsPhiPhiLine( self ) :
-        from StrippingConf.StrippingLine import StrippingLine, StrippingMember, StrippingTool
-        from Configurables import FilterDesktop, CombineParticles #, OfflineVertexFitter, PhysDesktop
-        import GaudiKernel.SystemOfUnits as Units
+    
+	if StrippingBs2PhiPhiConf._BsPhiPhiLine == None : 
+	    print "Bs2PhiPhiLine not created yet"
+    
+    	    from StrippingConf.StrippingLine import StrippingLine, StrippingMember, StrippingTool
+    	    from Configurables import FilterDesktop, CombineParticles #, OfflineVertexFitter, PhysDesktop
+    	    import GaudiKernel.SystemOfUnits as Units
 
-        Phi2KK_DC = "(MIPDV(PRIMARY)> %(KaonIP)s )&(PT> %(KaonPT)s *MeV)" % self.getProps()
-        Phi2KK_CC = "(AMAXDOCA('') < %(PhiDOCA)s *mm)&(AM < %(PhiMassMax)s *MeV)" % self.getProps()
-        Phi2KK_MC = "ALL"
-        PhiFilter = "(ADMASS('phi(1020)') < %(PhiMassWindow)s*MeV)" % self.getProps()
-        Bs2PhiPhi_DC = "(PT> %(PhiPT)s *MeV)" % self.getProps()
-        Bs2PhiPhi_CC = "(ADAMASS('B_s0')< %(BsMassWindow)s *MeV)&(ACHILD(PT,1)*ACHILD(PT,2)> %(PhiPTsq)s *GeV*GeV)" % self.getProps()
-        #Bs2PhiPhi_MC = ""
-        #Add error estimate cuts:
-        Phi2KK_DC = "(MIPCHI2DV(PRIMARY)> %(KaonIPCHI2)s )&"% self.getProps()+ Phi2KK_DC
-        Phi2KK_MC = "(VFASPF(VCHI2/VDOF)< %(PhiVertexCHI2pDOF)s )" % self.getProps()
-        Bs2PhiPhi_MC = "(VFASPF(VCHI2/VDOF)< %(BsVertexCHI2pDOF)s )" % self.getProps()
-        print "Cuts for Bs2PhiPhi  Stripping line"
-        print Phi2KK_DC
-        print Phi2KK_CC
-        print Phi2KK_MC
-        print PhiFilter
-        print Bs2PhiPhi_DC
-        print Bs2PhiPhi_CC
-        print Bs2PhiPhi_MC
+    	    Phi2KK_DC = "(MIPDV(PRIMARY)> %(KaonIP)s )&(PT> %(KaonPT)s *MeV)" % self.getProps()
+    	    Phi2KK_CC = "(AMAXDOCA('') < %(PhiDOCA)s *mm)&(AM < %(PhiMassMax)s *MeV)" % self.getProps()
+    	    Phi2KK_MC = "ALL"
+    	    PhiFilter = "(ADMASS('phi(1020)') < %(PhiMassWindow)s*MeV)" % self.getProps()
+    	    Bs2PhiPhi_DC = "(PT> %(PhiPT)s *MeV)" % self.getProps()
+    	    Bs2PhiPhi_CC = "(ADAMASS('B_s0')< %(BsMassWindow)s *MeV)&(ACHILD(PT,1)*ACHILD(PT,2)> %(PhiPTsq)s *GeV*GeV)" % self.getProps()
+    	    #Bs2PhiPhi_MC = ""
+    	    #Add error estimate cuts:
+    	    Phi2KK_DC = "(MIPCHI2DV(PRIMARY)> %(KaonIPCHI2)s )&"% self.getProps()+ Phi2KK_DC
+    	    Phi2KK_MC = "(VFASPF(VCHI2/VDOF)< %(PhiVertexCHI2pDOF)s )" % self.getProps()
+    	    Bs2PhiPhi_MC = "(VFASPF(VCHI2/VDOF)< %(BsVertexCHI2pDOF)s )" % self.getProps()
+    	    print "Cuts for Bs2PhiPhi  Stripping line"
+    	    print Phi2KK_DC
+    	    print Phi2KK_CC
+    	    print Phi2KK_MC
+    	    print PhiFilter
+    	    print Bs2PhiPhi_DC
+    	    print Bs2PhiPhi_CC
+    	    print Bs2PhiPhi_MC
         
 
-        Bs2PhiPhiLooseDetachedPhi2KK = StrippingMember ( CombineParticles
+    	    Bs2PhiPhiLooseDetachedPhi2KK = StrippingMember ( CombineParticles
                           , "Bs2PhiPhiLooseDetachedPhi2KK"
                           , InputLocations = ["StdNoPIDsKaons"]
                           , DecayDescriptor = "phi(1020) -> K+ K-"
@@ -85,29 +91,31 @@ class StrippingBs2PhiPhiConf(LHCbConfigurableUser):
                           , MotherCut = Phi2KK_MC
                           )
 
-        Bs2PhiPhiPhi2KK = StrippingMember ( FilterDesktop
+    	    Bs2PhiPhiPhi2KK = StrippingMember ( FilterDesktop
                 , "Bs2PhiPhiPhi2KK"
                 , InputLocations = ["%Bs2PhiPhiLooseDetachedPhi2KK"]
                 , Code = PhiFilter
                 )
 
-        Bs2PhiPhi = StrippingMember( CombineParticles
-          , "Bs2PhiPhi"
-          , DecayDescriptor =  "B_s0 -> phi(1020) phi(1020)"
-          , InputLocations  = [ "%Bs2PhiPhiPhi2KK" ]
-          , DaughtersCuts   = {"phi(1020)" : Bs2PhiPhi_DC}
-          , CombinationCut  = Bs2PhiPhi_CC
-          , MotherCut       = Bs2PhiPhi_MC 
-          , ReFitPVs        = True
-          )
+    	    Bs2PhiPhi = StrippingMember( CombineParticles
+            , "Bs2PhiPhi"
+            , DecayDescriptor =  "B_s0 -> phi(1020) phi(1020)"
+            , InputLocations  = [ "%Bs2PhiPhiPhi2KK" ]
+    	    , DaughtersCuts   = {"phi(1020)" : Bs2PhiPhi_DC}
+            , CombinationCut  = Bs2PhiPhi_CC
+            , MotherCut       = Bs2PhiPhi_MC 
+            , ReFitPVs        = True
+            )
 
-        line = StrippingLine('Bs2PhiPhiLine'
+    	    StrippingBs2PhiPhiConf._BsPhiPhiLine = StrippingLine('Bs2PhiPhiLine'
               , prescale = 1
               , postscale = 1
               , algos = [Bs2PhiPhiLooseDetachedPhi2KK, Bs2PhiPhiPhi2KK, Bs2PhiPhi]
               )
+        else : 
+    	    print "Bs2PhiPhiLine already created"
 
-        return line
+        return StrippingBs2PhiPhiConf._BsPhiPhiLine
 
 
     def getProps(self) :
