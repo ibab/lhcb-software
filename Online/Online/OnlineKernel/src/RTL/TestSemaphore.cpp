f6041c404cda9f7d608bae5acca3dd1d9bee5b2c
Markus Frank <frankb@cern.ch>
2010-04-15 15:36:38 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100526 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b3dab6f942de960fff15bea3a97d055d33522d8f

--- Online/Online/OnlineKernel/src/RTL/TestSemaphore.cpp
+++ Online/Online/OnlineKernel/src/RTL/TestSemaphore.cpp
@@ -3,7 +3,7 @@
 #include "RTL/Lock.h"
 
 extern "C" int rtl_testSemaphoreSubProcess(int argc,char** argv) {
-  int msecs = 1000;
+  int msecs = 2000;
   int turns = 10;
   lib_rtl_lock_t id;
   std::string proc = argc>1 ? std::string(argv[1]) : RTL::processName();
@@ -30,6 +30,6 @@ extern "C" int rtl_testSemaphoreSubProcess(int argc,char** argv) {
 
 extern "C" int rtl_testsemaphore(int,char**) {
   system("gentest.exe libOnlineKernel.so rtl_testSemaphoreSubProcess  Process__1&");
-  lib_rtl_sleep(700);
+  lib_rtl_sleep(1000);
   return system("gentest.exe libOnlineKernel.so rtl_testSemaphoreSubProcess Process__2");
 }
