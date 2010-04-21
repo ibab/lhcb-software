79eda730f5cf09fc85c82d4193883ef6f6f0c5f1
jpalac <jpalac@cern.ch>
2010-04-21 06:37:11 +0000

    Simple test to check that streams instantiate OK.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83154 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f9e1399465931746c7099a56fb8d0e31acffa775

---
+++ Analysis/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+'''
+ Test that Stripping Streams can be instantiated.
+ This is the most basic test that should be run when modifying or
+ adding Stripping Streams to StrippingSelections.Streams.allStreams.
+'''
+__author__ = 'Juan Palacios'
+
+from StrippingConf.Configuration import StrippingConf
+from StrippingSelections.Streams import allStreams
+
+sc = StrippingConf (Streams = allStreams)
