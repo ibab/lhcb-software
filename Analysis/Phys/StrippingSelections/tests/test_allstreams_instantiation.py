ae61a6e684ccab77ca7469e3490b39acbe0d5b69
jpalac <jpalac@cern.ch>
2010-04-21 06:45:29 +0000

    Add author.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83155 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d392e3067a909ffe0a29cbe20296df5ff862e963

--- Analysis/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
+++ Analysis/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
@@ -4,7 +4,7 @@
  This is the most basic test that should be run when modifying or
  adding Stripping Streams to StrippingSelections.Streams.allStreams.
 '''
-__author__ = 'Juan Palacios'
+__author__ = 'Juan Palacios juan.palacios@nikhef.nl'
 
 from StrippingConf.Configuration import StrippingConf
 from StrippingSelections.Streams import allStreams
