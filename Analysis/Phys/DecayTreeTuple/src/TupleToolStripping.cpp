8e29c434c2120f0171289831a16fb31f889971e1
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-04-26 12:49:59 +0000

    add stripping tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83309 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f672ecdb058d593e97560110e689db595626692e

---
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolStripping.cpp
@@ -0,0 +1,85 @@
+// $Id: TupleToolStripping.cpp,v 1.1 2010-04-26 12:49:59 pkoppenb Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h" 
+
+// local
+#include "TupleToolStripping.h"
+#include "Event/HltDecReports.h"
+#include "Event/HltDecReport.h"
+#include "GaudiAlg/Tuple.h"
+#include "GaudiAlg/TupleObj.h"
+
+// gaudirun.py ~/cmtuser/Saved-options/DVTestStripping-ReadDST.py ~/cmtuser/Saved-options/DiMuon_100419.py | tee test
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : TupleToolStripping
+//
+// 2010-04-23 : Patrick Koppenburg
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( TupleToolStripping );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+TupleToolStripping::TupleToolStripping( const std::string& type,
+                                        const std::string& name,
+                                        const IInterface* parent )
+  : TupleToolBase ( type, name , parent )
+{
+  declareInterface<IEventTupleTool>(this);
+  /// @todochange with rootontes
+  declareProperty("StrippigReportsLocations", m_location = "/Event/Strip/Phys/DecReports" ); 
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+TupleToolStripping::~TupleToolStripping() {} 
+//=============================================================================
+// Destructor
+//=============================================================================
+StatusCode TupleToolStripping::initialize() {
+  StatusCode sc = TupleToolBase::initialize();
+
+  return sc ;
+
+} 
+//=============================================================================
+//=============================================================================
+
+StatusCode TupleToolStripping::fill( Tuples::Tuple& tuple ){
+
+  if (exist<LHCb::HltDecReports>(m_location)){
+    LHCb::HltDecReports* dr = get<LHCb::HltDecReports>(m_location);
+    if(msgLevel(MSG::DEBUG)) debug() << "There are " << dr->size() << " DecReports at " << m_location << endmsg ;
+    std::vector<std::string> names = dr->decisionNames() ;
+    if(msgLevel(MSG::VERBOSE)) verbose() << "NAMES: " << names << endmsg ;
+    unsigned int i = 0 ;
+    for ( std::vector<std::string>::const_iterator s = names.begin() ; 
+          s != names.end() ; ++s){
+      if(msgLevel(MSG::VERBOSE)) verbose() << "Trying " << i << " " << *s << endmsg ;
+      if ( dr->hasDecisionName(*s) ){
+        const LHCb::HltDecReport* report = dr->decReport(*s);
+        if ( 0==report ) Exception("Cannot find report "+*s);
+        if(msgLevel(MSG::VERBOSE)) verbose() << *s << " says " << report->decision() << endmsg ;
+        if ( !tuple->column( *s, report->decision() ) ) return StatusCode::FAILURE;
+      } else {
+        Exception("Don't have report name "+*s);
+      }
+      i++ ;
+    }
+  } else {
+    Warning("No DecReports at "+m_location, StatusCode::SUCCESS, 1);
+    return StatusCode::SUCCESS ;
+  }
+
+  return StatusCode::SUCCESS ;
+  
+}
+ 
+//=============================================================================
---
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolStripping.h
@@ -0,0 +1,35 @@
+// $Id: TupleToolStripping.h,v 1.1 2010-04-26 12:49:59 pkoppenb Exp $
+#ifndef TUPLETOOLSTRIPPING_H 
+#define TUPLETOOLSTRIPPING_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTool.h"
+
+#include "TupleToolBase.h"
+#include "Kernel/IEventTupleTool.h"            // Interface
+
+/** @class TupleToolStripping TupleToolStripping.h
+ *  
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2010-04-23
+ */
+class TupleToolStripping : public TupleToolBase, virtual public IEventTupleTool {
+public: 
+  /// Standard constructor
+  TupleToolStripping( const std::string& type, 
+                      const std::string& name,
+                      const IInterface* parent);
+
+  virtual ~TupleToolStripping( ); ///< Destructor
+  virtual StatusCode initialize();
+
+  virtual StatusCode fill( Tuples::Tuple& );
+
+protected:
+
+private:
+  std::string m_location ; ///< HDR location
+};
+#endif // TUPLETOOLSTRIPPING_H
