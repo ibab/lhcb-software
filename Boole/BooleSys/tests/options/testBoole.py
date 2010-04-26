9591efdb2b800b7114973ae2f861e5b695d78467
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-04-26 15:41:38 +0000

    update database tags
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@61896 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: db7f7be12ec0e7be8673bf6232a65d0b0de41c96

--- Boole/BooleSys/tests/options/testBoole.py
+++ Boole/BooleSys/tests/options/testBoole.py
@@ -9,5 +9,5 @@ Boole().EvtMax   = 0
 Boole().Outputs  = ["DIGI","MDF","L0ETC"] # Test all output types
 Boole().Histograms = "NONE"
 
-LHCbApp().CondDBtag = "sim-20100119-vc-md100"
-LHCbApp().DDDBtag   = "head-20100119"
+LHCbApp().CondDBtag = "sim-20100412-vc-md100"
+LHCbApp().DDDBtag   = "head-20100407"
