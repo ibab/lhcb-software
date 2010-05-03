e340d72a070e9bc3f8f6aafb5ec7a771f563b80e
rlambert <rlambert@cern.ch>
2010-05-03 08:29:05 +0000

    added PVConf.py to replace PVLoose.py and PVVeryLoose.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@20425 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 32136f80ffdd8130c842e8e53847501be5bba1c6

---
+++ Rec/Tr/PatPV/python/PatPV/PVConf.py
@@ -0,0 +1,51 @@
+
+from Configurables import PatPVOffline, PVSeed3DOfflineTool, PVSeedTool
+from Configurables import PVOfflineTool, SimplePVSeedTool, SimplePVFitter, LSAdaptPVFitter
+
+class LoosePV(object):
+    '''Simple python class to configure loose PV settings for reconstruction''' 
+    def configureAlg(self, PVAlg = PatPVOffline("PatPVOffline")):
+        '''pass in an alg to configure'''
+        
+        self.PVAlg.addTool(PVOfflineTool, "PVOfflineTool")
+        
+        self.configureTool(self.PVAlg.PVOfflineTool)
+    
+    def configureTool(self, PVTool):
+        '''pass in a tool method to configure'''
+        PVTool.addTool(PVSeedTool, "PVSeedTool")
+        PVTool.PVSeedTool.minClusterMult = 2
+        PVTool.PVSeedTool.minCloseTracksInCluster = 2
+        PVTool.PVSeedTool.ratioSig2HighMult = 1
+        PVTool.PVSeedTool.ratioSig2LowMult =1
+        
+        PVTool.addTool(LSAdaptPVFitter,'LSAdaptPVFitter')
+        PVTool.LSAdaptPVFitter.MinTracks = 2
+        
+
+class VLoosePV(object):
+    '''Simple python class to configure Very loose PV settings for reconstruction'''
+    def configureAlg(self, PVAlg = PatPVOffline("PatPVOffline")):
+        '''pass in an alg to configure'''
+        
+        self.PVAlg.addTool(PVOfflineTool, "PVOfflineTool")
+        
+        self.configureTool(self.PVAlg.PVOfflineTool)
+    
+    def configureTool(self, PVTool):
+        '''pass in a tool method to configure'''
+        PVTool.addTool(PVSeedTool, "PVSeedTool")
+        PVTool.PVSeedTool.minClusterMult = 2
+        PVTool.PVSeedTool.minCloseTracksInCluster = 2
+        PVTool.PVSeedTool.ratioSig2HighMult = 1
+        PVTool.PVSeedTool.ratioSig2LowMult =1
+        
+        PVTool.addTool(LSAdaptPVFitter,'LSAdaptPVFitter')
+        PVTool.LSAdaptPVFitter.MinTracks = 2
+        PVTool.LSAdaptPVFitter.MinTracks    = 3
+        PVTool.LSAdaptPVFitter.acceptTrack  = 0.000000000001
+        PVTool.LSAdaptPVFitter.trackMaxChi2 = 25.
+        PVTool.LSAdaptPVFitter.zMaxSpread   = 1000.
+        PVTool.LSAdaptPVFitter.minIter      = 3
+        
+
