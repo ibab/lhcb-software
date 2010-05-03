707fea05842317a86a3a7dfd27777993bf502af8
rlambert <rlambert@cern.ch>
2010-05-03 10:27:27 +0000

    Copy configuration options files into a set of python classes PatAlgConf.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@20430 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3831589ba3f51af56fa0aa23bce119b1c17b0bdb

--- Rec/Tf/PatAlgorithms/python/PatAlgorithms/PatAlgConf.py
+++ Rec/Tf/PatAlgorithms/python/PatAlgorithms/PatAlgConf.py
@@ -71,25 +71,25 @@ class CosmicConf(object):
   def configureTool(self, SeedTool):
     #no magnetic field
     #options to tune PatSeeding for tracking with B field off
-    SeedTool.xMagTol = 4e2;
-    SeedTool.zMagnet = 0.;
-    SeedTool.FieldOff = true;
-    SeedTool.MinMomentum = 5e4;
+    SeedTool.xMagTol = 4e2
+    SeedTool.zMagnet = 0.
+    SeedTool.FieldOff = true
+    SeedTool.MinMomentum = 5e4
     #relax requirements on number of hits/planes
     #(this may need tweaking if some C frames are in "open" position)
-    SeedTool.MinXPlanes = 4;
-    SeedTool.MinTotalPlanes = 8;
-    SeedTool.OTNHitsLowThresh = 9;
-    SeedTool.MaxMisses = 2;
+    SeedTool.MinXPlanes = 4
+    SeedTool.MinTotalPlanes = 8
+    SeedTool.OTNHitsLowThresh = 9
+    SeedTool.MaxMisses = 2
     #relax pointing requirement (no PV for cosmics!)
-    SeedTool.MaxYAtOrigin = 4e5;
-    SeedTool.MaxYAtOriginLowQual = 8e5;
-    SeedTool.xMagTol = 4e5;
-    SeedTool.MinMomentum = 1e-4; #pointing requirement in disguise!
+    SeedTool.MaxYAtOrigin = 4e5
+    SeedTool.MaxYAtOriginLowQual = 8e5
+    SeedTool.xMagTol = 4e5
+    SeedTool.MinMomentum = 1e-4 #pointing requirement in disguise!
     #cosmic reconstruction
-    SeedTool.Cosmics = true;
+    SeedTool.Cosmics = true
     #detector not yet aligned - chi^2 should not contribute to track quality
-    SeedTool.QualityWeights        = { 1.0, 0.0 };
+    SeedTool.QualityWeights        = [ 1.0, 0.0 ]
     
     #depending on misalignments/usage of IT, one may need to tweak
     #the chi^2 cuts and/or CurveTol, TolCollect{IT,OT} or MaxRange{IT,OT}
@@ -97,4 +97,4 @@ class CosmicConf(object):
     #look)
     
     #switch off drift times for the pattern recognition
-    OTHitCreator("ToolSvc.OTHitCreator").NoDriftTimes = true;
+    OTHitCreator("ToolSvc.OTHitCreator").NoDriftTimes = true
