01cb62c7913b0bb1979303f41b1e8036b84ba0cb
rlambert <rlambert@cern.ch>
2010-05-04 05:44:38 +0000

    added TsaAlgConf.py to replace the options file TsaSeeding
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@20454 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 34894aa0fd864c3089f2da5b9a6d4f388d156ffc

---
+++ Rec/Tf/TsaAlgorithms/python/TsaAlgorithms/TsaAlgConf.py
@@ -0,0 +1,182 @@
+from TrackSys.Configuration import *
+
+from Configurables import ( Tf__Tsa__Seed, Tf__Tsa__SeedTrackCnv,
+                            Tf__Tsa__ITXSearch, Tf__Tsa__OTXSearch,
+                            Tf__Tsa__ITStereoSearch, Tf__Tsa__OTStereoSearch,
+                            Tf__Tsa__TStationHitManager, Tf__Tsa__SeedTrackCnvTool,
+                            Tf__Tsa__StubFind, Tf__Tsa__StubExtender,
+                            Tf__Tsa__SeedAddHits, Tf__Tsa__Likelihood,Tf__Tsa__StubLinker, Tf__Tsa__StubExtender, Tf__Tsa__StubFind
+                           )
+
+class TsaSeedConf(object):
+    '''Apply configuration to a TsaSeed algorithm'''
+    def configureAlg(self, tsaSeed = Tf__Tsa__Seed("TsaSeed"), tsaConv=Tf__Tsa__SeedTrackCnv( "TsaSeedTrackCnv")):
+        
+        tsaConv.outputLocation = "Rec/Track/Seed"
+        ## Hit cleaning options
+        tsaSeed.addTool( Tf__Tsa__TStationHitManager,"TsaDataManager" )
+        tsaSeed.TsaDataManager.CleanITHits = True
+        tsaSeed.TsaDataManager.CleanOTHits = True
+        
+        tsaSeed.addTool(Tf__Tsa__ITXSearch, name="xSearchS0")
+        tsaSeed.addTool(Tf__Tsa__ITXSearch, name="xSearchS1")
+        tsaSeed.addTool(Tf__Tsa__ITXSearch, name="xSearchS2")
+        tsaSeed.addTool(Tf__Tsa__OTXSearch, name="xSearchS3")
+        tsaSeed.addTool(Tf__Tsa__OTXSearch, name="xSearchS4")
+        
+        tsaSeed.addTool(Tf__Tsa__ITStereoSearch, name="stereoS0")
+        tsaSeed.addTool(Tf__Tsa__ITStereoSearch, name="stereoS1")
+        tsaSeed.addTool(Tf__Tsa__ITStereoSearch, name="stereoS2")
+        tsaSeed.addTool(Tf__Tsa__OTStereoSearch, name="stereoS3")
+        tsaSeed.addTool(Tf__Tsa__OTStereoSearch, name="stereoS4")       
+
+        self.configureTools([tsaSeed.xSearchS0,tsaSeed.xSearchS1,
+                             tsaSeed.xSearchS2,tsaSeed.xSearchS3,tsaSeed.xSearchS4],
+                            [tsaSeed.stereoS0, tsaSeed.stereoS1, tsaSeed.stereoS2,
+                             tsaSeed.stereoS3, tsaSeed.stereoS4])
+        
+        tsaSeed.addTool(Tf__Tsa__SeedAddHits, name="SeedAddHits")
+        tsaSeed.SeedAddHits.OnlyUnusedHits = OnlyUnusedHits
+        tsaSeed.addTool(Tf__Tsa__StubLinker, name = "stubLinker")
+        tsaSeed.addTool(Tf__Tsa__StubExtender, "stubExtender")
+        tsaSeed.addTool(Tf__Tsa__StubFind, "stubFinder")
+        
+        if TrackSys().fieldOff():
+            tsaConv.addTool( Tf__Tsa__SeedTrackCnvTool("SeedTrackCnvTool"))
+            tsaConv.SeedTrackCnvTool.pFromCurvature = True
+            
+            tsaSeed.addTool(Tf__Tsa__StubFind, "stubFind")
+            tsaSeed.addTool(Tf__Tsa__StubExtender, "stubExtender")
+            tsaSeed.stubFind.sxCut = 0.1;
+            tsaSeed.stubFind.dAngle = 0.08;
+            tsaSeed.stubExtender.y0Cut1 = 100.;
+            tsaSeed.stubExtender.txCut = 0.1;
+            tsaSeed.stubExtender.tyCut = 0.1;
+            
+        if TrackSys().noDrifttimes():
+            tsaSeed.calcLikelihood = False
+
+        
+        if  TrackSys().earlyData():
+            tsaSeed.SeedAddHits.dCut = 0.7
+            tsaSeed.SeedAddHits.tol = 7.0
+            tsaSeed.SeedAddHits.outlierCutParabola = 6.0
+            tsaSeed.SeedAddHits.outlierCutLine = 6.0
+            tsaSeed.stubLinker.outlierCutParabola = 6.0
+            tsaSeed.stubLinker.deltaXCon = 0.08
+            tsaSeed.stubLinker.deltaSx = 0.03
+            tsaSeed.stubLinker.outlierCutLine = 6.0
+            tsaSeed.stubExtender.outlierCutParabola = 6.0
+            tsaSeed.stubExtender.outlierCutLine = 6.0
+            tsaSeed.stubExtender.dxCut = 1.5
+            tsaSeed.stubExtender.dyCut = 40.
+            tsaSeed.stubFinder.yCut = 5
+            tsaSeed.stubFinder.yTol = 10
+            tsaSeed.stubFinder.dAngle = 0.35
+        
+            
+    def configureTools(self, xTools, stereoTools):
+
+        if len(xTools)<5 or len(stereoTools)<5:
+            raise ValueError, "TsaAlgTools, you must provide 5 xTools and 5 stereoTools to configure"
+        
+        OnlyUnusedHits = False
+        
+        for xtool in xTools:
+            xtool.sector = xTools.index(xtool)
+            xtool.OnlyUnusedHits = OnlyUnusedHits
+                
+        for stool in stereoTools:
+            stool.sector = stereoTools.index(stool)
+            stool.OnlyUnusedHits = OnlyUnusedHits
+        
+        
+        
+        
+        if TrackSys().fieldOff():
+            
+            xtool[3].sxCut = 0.40;
+            xtool[4].sxCut = 0.40;
+            xtool[3].collectPolicy = "Linear";
+            xtool[4].collectPolicy = "Linear";
+            xtool[3].sx2Cut = 0.330;
+            xtool[4].sx2Cut = 0.330;
+            
+            for xtool in xTools:
+                xtool.dthCut = 0.08;
+                xtool.x0Cut = 400.;
+                xtool.sxCut = 0.10;
+            
+            
+        if TrackSys().noDrifttimes():
+                
+            xTools[3].outlierCut = 21
+            xTools[4].outlierCut = 21
+            stereoTools[3].outlierCut = 21
+            stereoTools[4].outlierCut = 21
+            xTools[3].deltaX = 2.7
+            xTools[4].deltaX = 2.7
+            xTools[3].x0Cut = 800
+            xTools[4].x0Cut = 800
+            stereoTools[3].syCut = 0.08 
+            stereoTools[4].syCut = 0.08
+            stereoTools[3].yTol = 25
+            stereoTools[4].yTol = 25
+            stereoTools[3].yTol2 = 20
+            stereoTools[4].yTol2 = 20
+            
+        if  TrackSys().earlyData():
+            tsaSeed.addTool(Tf__Tsa__Likelihood(), name = "likelihood")
+            tsaSeed.likelihood.LikCut = -40
+            tsaSeed.likelihood.outlierCut = 6.0
+            tsaSeed.likelihood.OTEff = 0.85
+            Tf__Tsa__SeedTrackCnv("TsaSeedTrackCnv").LikCut = -40
+            xTools[0].outlierCut = 6.0
+            xTools[1].outlierCut = 6.0
+            xTools[2].outlierCut = 6.0
+            xTools[3].outlierCut = 6.0
+            xTools[4].outlierCut = 6.0
+            stereoTools[0].outlierCut = 6.0
+            stereoTools[1].outlierCut = 6.0
+            stereoTools[2].outlierCut = 6.0
+            stereoTools[3].outlierCut = 6.0
+            stereoTools[4].outlierCut = 6.0
+            xTools[0].deltaX = 0.7
+            xTools[1].deltaX = 0.7
+            xTools[2].deltaX = 0.7
+            xTools[0].x0Cut = 2000.
+            xTools[1].x0Cut = 2000.
+            xTools[2].x0Cut = 2000.
+            xTools[3].deltaX = 1.2
+            xTools[4].deltaX = 1.2
+            xTools[0].dthCut = 0.45
+            xTools[1].dthCut = 0.45
+            xTools[2].dthCut = 0.45
+            xTools[3].dthCut = 0.75
+            xTools[4].dthCut = 0.75
+            stereoTools[0].syCut = 0.02
+            stereoTools[1].syCut = 0.02
+            stereoTools[2].syCut = 0.02
+            stereoTools[0].yTol = 10.0
+            stereoTools[1].yTol = 10.0
+            stereoTools[2].yTol = 10.0
+            stereoTools[3].nHit = 14
+            stereoTools[4].nHit = 14
+            stereoTools[3].syCut = 0.1
+            stereoTools[4].syCut = 0.1
+            stereoTools[3].yTol2 = 11.0
+            stereoTools[4].yTol2 = 11.0
+            stereoTools[3].maxDriftRadius = 3.
+            xTools[3].maxDriftRadius = 3.
+            stereoTools[4].maxDriftRadius = 3.
+            xTools[4].maxDriftRadius = 3.
+
+
+
+    
+
+
+    
+
+    
+    
