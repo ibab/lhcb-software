16e78bc019cb8885bbf0f43c7b28a9394a25f61a
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-05-05 14:36:39 +0000

    add items to dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@48713 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 687c7970c7de764f3ab53434defc7cd54ec610dd

--- LHCb/Kernel/HltInterfaces/dict/HltInterfacesDict.h
+++ LHCb/Kernel/HltInterfaces/dict/HltInterfacesDict.h
@@ -3,6 +3,24 @@
 
 #include "Kernel/IANNSvc.h"
 #include "Kernel/IANSvc.h"
+#include "Kernel/IANSvc.h"
+#include "Kernel/ConfigTreeNodeAlias.h"
+#include "Kernel/ConfigTreeNode.h"
+#include "Kernel/PropertyConfig.h"
+#include "Kernel/IPropertyConfigSvc.h"
+#include "Kernel/IConfigAccessSvc.h"
+
+namespace {
+
+  struct _hltinterfaces_instantiations
+  {
+    std::vector<ConfigTreeNodeAlias> py_hlt_07;
+    boost::optional<ConfigTreeNode> py_hlt_08;
+    boost::optional<PropertyConfig> py_hlt_09;
+    _hltinterfaces_instantiations(){}
+  };
+}
+
 
 #endif // DICT_HLTINTERFACESDICT_H
 
