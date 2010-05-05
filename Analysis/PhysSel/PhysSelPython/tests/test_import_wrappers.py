6072341e89f0f738679a763ce39423ba7e8f9675
jpalac <jpalac@cern.ch>
2010-05-05 12:21:23 +0000

    Test imports of PhysSelPython.Wrappers module.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83522 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 558dc0a56537890b557d95fa6dcb07efda6b275a

---
+++ Analysis/PhysSel/PhysSelPython/tests/test_import_wrappers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+'''
+Test loading of modules
+'''
+
+import sys
+sys.path.append('../python')
+
+def test_import_wrappers_star() :
+    from PhysSelPython.Wrappers import *
+
+def test_import_physselpython_wrappers() :
+    import PhysSelPython.Wrappers
+
+if '__main__' == __name__ :
+
+    import sys
+
+    test_names = filter(lambda k : k.count('test_') > 0, locals().keys())
+
+    __tests = filter( lambda x : x[0] in test_names, locals().items())
+    
+
+    message = ''
+    summary = '\n'
+    length = len(sorted(test_names,
+                        cmp = lambda x,y : cmp(len(y),len(x)))[0]) +2
+    
+    for test in __tests :
+        try :
+            test[1]()
+            message = 'PASS'
+        except :
+            message = "FAIL"
+        summary += test[0].ljust(length) + ':' + message.rjust(10) + '\n'
+
+    if summary.count('FAIL') > 0 :
+        message = 'FAIL'
+        wr = sys.stderr.write
+    else :
+        message = 'PASS'
+        wr = sys.stdout.write
+
+    summary += 'Global'.ljust(length) + ':' + message.rjust(10) + '\n\n'
+    wr(summary)
+        
