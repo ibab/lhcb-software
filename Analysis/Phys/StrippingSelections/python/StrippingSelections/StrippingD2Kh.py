afb3e971fb820641dc2ed8ac8a32a6eab5e693c5
Harry Victor Cliff <harry.victor.cliff@cern.ch>
2010-05-07 16:20:32 +0000

    *** empty log message ***
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83576 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7740f81dba447181fc98dc6188bc03d99d60c420

---
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingD2Kh.py
@@ -0,0 +1,55 @@
+############################################################################
+#
+# Stripping selection for prompt D->Kh events, reconstructed as D0->K+K-
+#
+############################################################################
+
+__author__ = ['Harry Cliff']
+__date__ = '20/04/2010'
+__version__ = '$Revision: 1.1 $'
+
+'''
+D0->Kh stripping selection.
+'''
+
+
+from Gaudi.Configuration import *
+from Configurables import CombineParticles
+from PhysSelPython.Wrappers import Selection, SelectionSequence, DataOnDemand
+from StrippingConf.StrippingLine import StrippingLine, StrippingMember
+
+#----------------------------------------
+# 1) Make a preselection
+#----------------------------------------
+DKhPresel = CombineParticles("DKhPresel")
+
+DKhPresel.DecayDescriptor = "D0 -> K+ K-"
+DKhPresel.DaughtersCuts = { "K+" : "ISLONG & (PT>500) & (TRCHI2DOF<9)" } 
+DKhPresel.CombinationCut  = "(AM>1815) & (AM<2065) & (AP>5000)"
+DKhPresel.MotherCut       = "(BPVVDCHI2>35)  & (BPVIPCHI2()<9) & (VFASPF(VCHI2/VDOF)<10) & (BPVDIRA>0.9999) & (MAXTREE('K+'==ABSID,PIDK)>5.0)"
+
+DKhPreselOutput = "Phys/"
+DKhPreselOutput += "DKhPresel"
+
+#------------------------------------------
+# 2) Wrap StdNoPIDsPions and StdNoPIDsKaons
+#------------------------------------------
+
+SelStdNoPIDsKaons = DataOnDemand('SelStdNoPIDsKaons', Location = 'Phys/StdNoPIDsKaons')
+
+#----------------------------------------
+# 3) Make a selection sequence
+#----------------------------------------
+
+SelDKhPresel = Selection("SelDKhPresel",  Algorithm = DKhPresel, RequiredSelections = [ SelStdNoPIDsKaons])
+
+SelSeqDKhPresel = SelectionSequence("SeqDKhPresel", TopSelection = SelDKhPresel)
+
+
+#--------------------------------------------
+# 4) Create StrippingLine with this selection 
+#--------------------------------------------
+DKhPreselLine = StrippingLine("DKhPreselLine"
+                          , prescale = 1.
+                          , algos = [ SelSeqDKhPresel ]
+                          )
