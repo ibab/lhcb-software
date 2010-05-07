999b4d0d1d36cd0202250c99d33ba0dd00b18fe2
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-05-07 19:22:21 +0000

    add test for bug #67111
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31583 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 830d0baa6a55a70fd6abd229753c6152ffd9f92c

--- Brunel/Rec/Brunel/tests/options/test-big.py
+++ Brunel/Rec/Brunel/tests/options/test-big.py
@@ -9,4 +9,5 @@ Brunel().DataType = "2010"
 # file of selected large events
 EventSelector().Input = [
   "DATAFILE='castor:/castor/cern.ch/user/c/cattanem/testFiles/run_69669_large_2ev.mdf' SVC='LHCb::MDFSelector'"
+, "DATAFILE='castor:/castor/cern.ch/user/c/cattanem/testFiles/run_70122_large_1ev.mdf' SVC='LHCb::MDFSelector'"
     ]
