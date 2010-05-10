9d6eac06f86fdc1e75b0be42307c72b29f3dbe1a
jpalac <jpalac@cern.ch>
2010-05-10 09:26:31 +0000

    For CondDB tag date hack.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@83608 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4b52d1f7fe18746caaf2f27d3233b1e0fc6ef4ac

---
+++ DaVinci/DaVinciSys/tests/test_conddb_tag_date.py
@@ -0,0 +1,32 @@
+from DaVinci.Configuration import isNewCondDBTag
+
+def test_new_tags() :
+    tags = ["sim-20100510-vc10mm-mu100",
+            "sim-20100510-vc10mm-md100",
+            "sim-20100510-vc-mu100",
+            "sim-20100510-vc-moff",
+            "sim-20100510-vc-md100",
+            "hlt-20100509",
+            "head-20100504",
+            "MC09-20100430-vc-md100",
+            "MC09-20100430-vc-moff",
+            "MC09-20100430-vc-mu100",
+            "MC09-20100430-vo-mu100",
+            "MC09-20100430-vo-moff",
+            "MC09-20100430-vo-md100",
+            "hlt-20100430",
+            "head-20100430",
+            "sim-20100429-vc-md100",
+            "sim-20090501"]
+
+    for tag in tags :
+        assert isNewCondDBTag(tag) == True
+
+def test_old_tags() :
+    tags = ["hlt-20100414",
+            "head-20100414",
+            "MC-20100412-vc15mm-md100",
+            "sim-20100412-vc-mu100"]
+
+    for tag in tags :
+        assert isNewCondDBTag(tag) == False
