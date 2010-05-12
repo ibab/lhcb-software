59327763561462859309f7760cfd485322fd2f30
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-05-12 15:18:20 +0000

    adapt thresholds, add same sign dimuon, remove obsolete line
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@83659 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f83f9a38ac274c054f15a73147404defb241e863

--- Hlt/Hlt/HltSettings/python/HltSettings/DiMuonLines.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/DiMuonLines.py
@@ -37,11 +37,53 @@ class DiMuonLines :
             'Hlt2BiasedDiMuonRefined',
             'Hlt2BiasedDiMuonMass',
             'Hlt2BiasedDiMuonIP',
-            'Hlt2DiMuonUnbiasedJPsiLow',
+#            'Hlt2DiMuonUnbiasedJPsiLow',
             'Hlt2UnbiasedDiMuon',  
-            'Hlt2UnbiasedDiMuonLowMass', 
+            'Hlt2UnbiasedDiMuonLowMass',
+            'Hlt2DiMuonSameSign',
+          
             ]
             
         return lines
 
    
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        
+        # keep pass through thresholds
+        d = { }
+
+
+        from Hlt2Lines.Hlt2InclusiveDiMuonLines  import Hlt2InclusiveDiMuonLinesConf
+        d.update( { Hlt2InclusiveDiMuonLinesConf : { 
+            ## Cut values
+            'UnbiasedDiMuonPt'         : 0.0      # in MeV
+            ,'UnbiasedJPsiPt'          : 0.0      # MeV
+            ,'UnbiasedJPsiMassWindow'  : 120      # MeV
+            ,'UnbiasedPsi2SPt'         : 0.0      # MeV
+            ,'UnbiasedPsi2SMuPt'       : 500      # MeV
+            ,'UnbiasedPsi2SMassWindow' : 120      # MeV
+            ,'UnbiasedBmmMinMass'      : 5000     # MeV
+            ,'DYPt'                    : 0.0      # MeV
+            ,'DY1MinMass'              : 2500      # MeV
+            ,'DY1MaxMass'              : 6000      # MeV
+            ,'DY2MinMass'              : 5000      # MeV
+            ,'DY2MaxMass'              :12000      # MeV
+            ,'DY3MinMass'              :10000      # MeV
+            ,'DY3MaxMass'              :25000      # MeV
+            ,'DY4MinMass'              :20000      # MeV
+            ,'DY4MaxMass'              :50000      # MeV
+            ,'UnbiasedZmmMinMass'      :40000      # MeV
+            ,'UnbiasedZmmPt'           :  0.0      # MeV
+            , 'Prescale'   : { 'Hlt2UnbiasedDiMuon'          : 1.00
+                               , 'Hlt2UnbiasedDiMuonLowMass' : 0.1
+                               ,'Hlt2DiMuonDY1'              :  1
+                               ,'Hlt2DiMuonDY2'              :  1
+                               ,'Hlt2DiMuonSameSign'         :  1
+                               }           
+            }}
+                  )
+
+        return d
