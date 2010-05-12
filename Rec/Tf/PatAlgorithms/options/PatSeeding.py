f386434cc402f6d9a61149d6816a989fbda47b13
rlambert <rlambert@cern.ch>
2010-05-12 11:15:10 +0000

    Remove obsolete options files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@22478 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 107ba68819c98a88a2354f0e71d9d0a2c6891ff3

--- Rec/Tf/PatAlgorithms/options/PatSeeding.py
+++
@@ -1,9 +0,0 @@
-from TrackSys.Configuration import *
-from Configurables import (PatSeeding, PatSeedingTool)
-
-if TrackSys().fieldOff():
-   PatSeeding("PatSeeding").addTool(PatSeedingTool, name="PatSeedingTool")
-   PatSeeding("PatSeeding").PatSeedingTool.xMagTol = 4e2;
-   PatSeeding("PatSeeding").PatSeedingTool.zMagnet = 0.;
-   PatSeeding("PatSeeding").PatSeedingTool.FieldOff = True;
-   PatSeeding("PatSeeding").PatSeedingTool.MinMomentum = 5e4;
