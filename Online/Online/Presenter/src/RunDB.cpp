3ba811d7d483ae8be67b276fa4c4d04398009427
Patrick Robbe <robbe@lal.in2p3.fr>
2010-05-15 16:07:03 +0000

    New files for RunDB interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@100609 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7c392b5e568ec569e7792215093b7de5de15bd9

---
+++ Online/Online/Presenter/src/RunDB.cpp
@@ -0,0 +1,426 @@
+// $Id: RunDB.cpp,v 1.1 2010-05-15 16:07:03 robbep Exp $
+
+// local 
+#include "RunDB.h"
+
+// STL
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : RunDB
+//
+// 2010-05-10 : Patrick Robbe
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+RunDB::RunDB( std::string address ) : 
+  m_currentRunNumber( 0 ) , 
+  m_currentRunStartTime( "" ) , 
+  m_currentRunDuration( "" ) ,
+  m_partition( "LHCb" ) ,
+  m_destination( "OFFLINE" ) 
+{
+  m_address = address;
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+RunDB::~RunDB() {} 
+
+//=============================================================================
+// Get last run 
+//=============================================================================
+int RunDB::getLastRun( ) {
+
+  int lastRun = 0 ;
+
+  boost::asio::ip::tcp::iostream webStream( m_address , "http" ) ;
+
+  if ( ! webStream ) {
+    std::cerr << "Cannot open the RunDb Database at " << m_address  
+	      << std::endl ;
+    return lastRun ;
+  }
+
+  // Send HTTP request to web server
+  webStream << "GET /api/search/?rows=1" ;
+  if ( getPartition() != "" ) webStream << "&partition=" << getPartition() ;
+  if ( getDestination() != "" ) webStream << "&destination=" << getDestination() ;
+  webStream << " HTTP/1.0\r\n"
+	    << "Host:" << m_address << "\r\n"
+	    << "\r\n" << std::flush ;
+
+  std::string line ;
+
+  // Check that the web server answers correctly
+  std::getline( webStream , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) {
+    std::cerr << "RunDB server does not reply" << std::endl ;
+    return lastRun ;
+  }
+  
+  // Parse the web server answers
+  std::string pattern ;
+  pattern = "\\N{left-curly-bracket}(.*)" ;
+  pattern += "\\N{right-curly-bracket}" ;
+  
+  const boost::regex e( pattern ) ;
+
+  boost::property_tree::ptree run_tree ;
+
+  while ( std::getline( webStream , line ) ) {
+    // Check that the answer has the correct format (JSON format)
+    if ( boost::regex_match( line , e ) ) {
+      // Now parse each line (there should be actually only one line
+      // in the server answer, otherwise it is over-written
+      std::istringstream is( line ) ;
+
+      try {
+	boost::property_tree::json_parser::read_json( is , run_tree ) ;
+	
+	BOOST_FOREACH( boost::property_tree::ptree::value_type &v,
+		       run_tree.get_child( "runs" ) ) {
+	  lastRun = v.second.get< int >( "runid" ) ;
+	  m_currentRunNumber = lastRun ;
+	  std::string start = v.second.get< std::string >( "starttime" ) ;
+	  std::string end   = v.second.get< std::string >( "endtime" ) ;
+	  boost::algorithm::erase_all( start , "-" ) ;
+	  boost::algorithm::erase_all( start , ":" ) ;
+	  boost::algorithm::erase_all( end , "-" ) ;
+	  boost::algorithm::erase_all( end , ":" ) ;
+	  boost::posix_time::ptime st = boost::posix_time::from_iso_string( start ) ;
+	  m_currentRunStartTime = boost::posix_time::to_simple_string( st ) ;
+	  boost::posix_time::ptime et = boost::posix_time::from_iso_string( end ) ;
+	  m_currentRunEndTime = boost::posix_time::to_simple_string ( et ) ;
+	  
+	  boost::posix_time::time_duration dur = et - st ;
+	  m_currentRunDuration = boost::posix_time::to_simple_string( dur ) ;
+	  return lastRun ;
+	}
+      } 
+      catch (...) {
+	std::cerr << "Error in the problem db server response" << std::endl ;
+	return lastRun ;
+      }
+    }
+  }
+
+  return lastRun ;
+}
+
+//=============================================================================
+// Get next run
+//=============================================================================
+int RunDB::getNextRun( ) {
+
+  int nextRun = 0 ;
+
+  boost::asio::ip::tcp::iostream webStream( m_address , "http" ) ;
+
+  if ( ! webStream ) {
+    std::cerr << "Cannot open the RunDb Database at " << m_address  
+	      << std::endl ;
+    return nextRun ;
+  }
+
+  // Send HTTP request to web server
+  webStream << "GET /api/search/?rows=1" ;
+  if ( getPartition() != "" ) webStream << "&partition=" << getPartition() ;
+  if ( getDestination() != "" ) webStream << "&destination=" << getDestination() ;
+  webStream << "&starttime=" 
+	    << boost::posix_time::to_iso_extended_string( boost::posix_time::time_from_string( m_currentRunEndTime ) ) ; 
+  webStream << " HTTP/1.0\r\n"
+	    << "Host:" << m_address << "\r\n"
+	    << "\r\n" << std::flush ;
+
+  std::string line ;
+
+  // Check that the web server answers correctly
+  std::getline( webStream , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) {
+    std::cerr << "RunDB server does not reply" << std::endl ;
+    return nextRun ;
+  }
+  
+  // Parse the web server answers
+  std::string pattern ;
+  pattern = "\\N{left-curly-bracket}(.*)" ;
+  pattern += "\\N{right-curly-bracket}" ;
+  
+  const boost::regex e( pattern ) ;
+
+  boost::property_tree::ptree run_tree ;
+
+  int total = 0 ;
+  // Check first the number of candidate run:
+  while ( std::getline( webStream , line ) ) {
+    // Check that the answer has the correct format (JSON format)
+    if ( boost::regex_match( line , e ) ) {
+      // Now parse each line (there should be actually only one line
+      // in the server answer, otherwise it is over-written
+      std::istringstream is( line ) ;
+
+      try {
+	boost::property_tree::json_parser::read_json( is , run_tree ) ;
+	
+	total = run_tree.get< int >( "totalResults" ) ;
+
+	if ( 0 >= total ) 
+	  // No next run has been found
+	  return nextRun ;
+      } 
+      catch (...) {
+	std::cerr << "Error in the problem db server response" << std::endl ;
+	return nextRun ;
+      }
+    }
+  }
+  webStream.close() ;
+
+  boost::asio::ip::tcp::iostream webStream2( m_address , "http" ) ;
+
+  if ( ! webStream2 ) {
+    std::cerr << "Cannot open the RunDb Database at " << m_address  
+	      << std::endl ;
+    return nextRun ;
+  }  
+
+  // Call again web server to get only next run
+  // Send HTTP request to web server
+  if ( total > 1 ) total = total - 1 ; 
+  webStream2 << "GET /api/search/?rows=" << total << "&start=" << (total-1) ;
+  if ( getPartition() != "" ) webStream2 << "&partition=" << getPartition() ;
+  if ( getDestination() != "" ) webStream2 << "&destination=" << getDestination() ;
+  webStream2 << "&starttime=" 
+	     << boost::posix_time::to_iso_extended_string( boost::posix_time::time_from_string( m_currentRunEndTime ) ) ; 
+  webStream2 << " HTTP/1.0\r\n"
+	     << "Host:" << m_address << "\r\n"
+	     << "\r\n" << std::flush ;
+  
+  // Check that the web server answers correctly
+  std::getline( webStream2 , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) {
+    std::cerr << "RunDB server does not reply" << std::endl ;
+    return nextRun ;
+  }
+
+  while ( std::getline( webStream2 , line ) ) {
+    // Check that the answer has the correct format (JSON format)
+    if ( boost::regex_match( line , e ) ) {
+      // Now parse each line (there should be actually only one line
+      // in the server answer, otherwise it is over-written
+      std::istringstream is( line ) ;
+
+      try {
+	boost::property_tree::json_parser::read_json( is , run_tree ) ;
+	
+	BOOST_FOREACH( boost::property_tree::ptree::value_type &v,
+		       run_tree.get_child( "runs" ) ) {
+	  nextRun = v.second.get< int >( "runid" ) ;
+	  if ( nextRun == m_currentRunNumber ) return 0 ;
+	  m_currentRunNumber = nextRun ;
+	  std::string start = v.second.get< std::string >( "starttime" ) ;
+	  std::string end   = v.second.get< std::string >( "endtime" ) ;
+	  boost::algorithm::erase_all( start , "-" ) ;
+	  boost::algorithm::erase_all( start , ":" ) ;
+	  boost::algorithm::erase_all( end , "-" ) ;
+	  boost::algorithm::erase_all( end , ":" ) ;
+	  boost::posix_time::ptime st = boost::posix_time::from_iso_string( start ) ;
+	  m_currentRunStartTime = boost::posix_time::to_simple_string( st ) ;
+	  boost::posix_time::ptime et = boost::posix_time::from_iso_string( end ) ;
+	  m_currentRunEndTime = boost::posix_time::to_simple_string( et ) ;
+
+	  boost::posix_time::time_duration dur = et - st ;
+	  m_currentRunDuration = boost::posix_time::to_simple_string( dur ) ;
+	  return nextRun ;
+	}
+      } 
+      catch (...) {
+	std::cerr << "Error in the problem db server response" << std::endl ;
+	return nextRun ;
+      }
+    }
+  }
+  
+  return nextRun ;
+}
+
+//=============================================================================
+// Get next run
+//=============================================================================
+int RunDB::getPreviousRun( ) {
+
+  int previousRun = 0 ;
+
+  boost::asio::ip::tcp::iostream webStream( m_address , "http" ) ;
+
+  if ( ! webStream ) {
+    std::cerr << "Cannot open the RunDb Database at " << m_address  
+	      << std::endl ;
+    return previousRun ;
+  }
+    
+  // Send HTTP request to web server
+  webStream << "GET /api/search/?rows=2&start=1" ;
+  if ( getPartition() != "" ) webStream << "&partition=" << getPartition() ;
+  if ( getDestination() != "" ) webStream << "&destination=" << getDestination() ;
+  webStream << "&endtime=" 
+	    << boost::posix_time::to_iso_extended_string( boost::posix_time::time_from_string( m_currentRunStartTime ) ) ; 
+  webStream << " HTTP/1.0\r\n"
+	    << "Host:" << m_address << "\r\n"
+	    << "\r\n" << std::flush ;
+
+  std::string line ;
+
+  // Check that the web server answers correctly
+  std::getline( webStream , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) {
+    std::cerr << "RunDB server does not reply" << std::endl ;
+    return previousRun ;
+  }
+  
+  // Parse the web server answers
+  std::string pattern ;
+  pattern = "\\N{left-curly-bracket}(.*)" ;
+  pattern += "\\N{right-curly-bracket}" ;
+  
+  const boost::regex e( pattern ) ;
+
+  boost::property_tree::ptree run_tree ;
+
+  // Check first the number of candidate run:
+  while ( std::getline( webStream , line ) ) {
+    // Check that the answer has the correct format (JSON format)
+    if ( boost::regex_match( line , e ) ) {
+      // Now parse each line (there should be actually only one line
+      // in the server answer, otherwise it is over-written
+      std::istringstream is( line ) ;
+
+      try {
+	boost::property_tree::json_parser::read_json( is , run_tree ) ;
+
+	BOOST_FOREACH( boost::property_tree::ptree::value_type &v,
+		       run_tree.get_child( "runs" ) ) {
+	  previousRun = v.second.get< int >( "runid" ) ;
+	  if ( previousRun == m_currentRunNumber ) return 0 ;
+	  m_currentRunNumber = previousRun ;
+	  std::string start = v.second.get< std::string >( "starttime" ) ;
+	  std::string end   = v.second.get< std::string >( "endtime" ) ;
+	  boost::algorithm::erase_all( start , "-" ) ;
+	  boost::algorithm::erase_all( start , ":" ) ;
+	  boost::algorithm::erase_all( end , "-" ) ;
+	  boost::algorithm::erase_all( end , ":" ) ;
+	  boost::posix_time::ptime st = boost::posix_time::from_iso_string( start ) ;
+	  m_currentRunStartTime = boost::posix_time::to_simple_string( st ) ;
+	  boost::posix_time::ptime et = boost::posix_time::from_iso_string( end ) ;
+	  m_currentRunEndTime   = boost::posix_time::to_simple_string( et ) ;
+
+	  boost::posix_time::time_duration dur = et - st ;
+	  m_currentRunDuration = boost::posix_time::to_simple_string( dur ) ;
+	  return previousRun ;
+	}
+      } 
+      catch (...) {
+	std::cerr << "Error in the problem db server response" << std::endl ;
+	return previousRun ;
+      }
+    }
+  }
+  webStream.close() ;
+
+  return previousRun ;
+}
+
+//=============================================================================
+// Get a list of runs from the run db
+// starting for a given run number with +/-10 runs
+//=============================================================================
+std::vector< int > RunDB::getListOfRuns( int /*runNumber*/ ) {
+  return std::vector< int >() ;
+}
+
+//=============================================================================
+// Check run
+//=============================================================================
+bool RunDB::checkRun( int runNumber ) {
+  if ( runNumber == getCurrentRunNumber() ) return true ;
+
+  boost::asio::ip::tcp::iostream webStream( m_address , "http" ) ;
+
+  if ( ! webStream ) {
+    std::cerr << "Cannot open the RunDb Database at " << m_address  
+	      << std::endl ;
+    return false ;
+  }
+
+  // Send HTTP request to web server
+  webStream << "GET /api/run/" << runNumber << "/" ;
+  webStream << " HTTP/1.0\r\n"
+	    << "Host:" << m_address << "\r\n"
+	    << "\r\n" << std::flush ;
+
+  std::string line ;
+
+  // Check that the web server answers correctly
+  std::getline( webStream , line ) ;
+  if ( ! boost::algorithm::find_first( line , "200 OK" ) ) return false ;
+  
+  // Parse the web server answers
+  std::string pattern ;
+  pattern = "\\N{left-curly-bracket}(.*)" ;
+  pattern += "\\N{right-curly-bracket}" ;
+  
+  const boost::regex e( pattern ) ;
+
+  boost::property_tree::ptree run_tree ;
+
+  // Check destination and partition match
+  while ( std::getline( webStream , line ) ) {
+    // Check that the answer has the correct format (JSON format)
+    if ( boost::regex_match( line , e ) ) {
+      // Now parse each line (there should be actually only one line
+      // in the server answer, otherwise it is over-written
+      std::istringstream is( line ) ;
+      
+      try {
+	boost::property_tree::json_parser::read_json( is , run_tree ) ;
+	
+	std::string dest = run_tree.get< std::string >( "destination" ) ;
+	std::string part = run_tree.get< std::string >( "partitionname" ) ;
+
+	std::string start = run_tree.get< std::string >( "starttime" ) ;
+	std::string end   = run_tree.get< std::string >( "endtime" ) ;
+	boost::algorithm::erase_all( start , "-" ) ;
+	boost::algorithm::erase_all( start , ":" ) ;
+	boost::algorithm::erase_all( end , "-" ) ;
+	boost::algorithm::erase_all( end , ":" ) ;
+	boost::posix_time::ptime st = boost::posix_time::from_iso_string( start ) ;
+	m_currentRunStartTime = boost::posix_time::to_simple_string( st ) ;
+	boost::posix_time::ptime et = boost::posix_time::from_iso_string( end ) ;
+	m_currentRunEndTime = boost::posix_time::to_simple_string( et ) ;
+
+	boost::posix_time::time_duration dur = et - st ;
+	m_currentRunDuration = boost::posix_time::to_simple_string( dur ) ;
+
+	if ( ( dest == getDestination() ) && ( part == getPartition() ) ) {
+	  m_currentRunNumber = runNumber ;
+	  return true ;
+	}
+      } 
+      catch (...) {
+	std::cerr << "Error in the problem db server response" << std::endl ;
+	return false ;
+      }
+    }
+  }  
+  
+  return false ;
+}
---
+++ Online/Online/Presenter/src/RunDB.h
@@ -0,0 +1,84 @@
+// $Id: RunDB.h,v 1.1 2010-05-15 16:07:03 robbep Exp $
+#ifndef PRESENTER_RUNDB_H 
+#define PRESENTER_RUNDB_H 1
+
+// Include files
+#include <string>
+#include <vector>
+
+/** @class RunDB RunDB.h
+ *  
+ *  Interface to run db for presenter
+ *
+ *  @author Patrick Robbe
+ *  @date   2010-05-10
+ */
+class RunDB {
+public: 
+  /// Standard constructor
+  RunDB( std::string address );
+
+  virtual ~RunDB( ); ///< Destructor
+
+  /// Get last run number (for all destination or all partition, give empty string
+  int getLastRun( ) ;
+
+  /// Get next run number (compared to current run number) 
+  int getNextRun( ) ;
+
+  /// Get previous run number (compared to current run number) 
+  int getPreviousRun( ) ;
+
+  /// Get list of runs for a given partition (+/- 10 runs)
+  std::vector< int > getListOfRuns( int runNumber ) ;
+
+  /// Get current run's start time
+  std::string getCurrentStartTime( ) { return m_currentRunStartTime ; } ;
+  
+  /// Get current run's duration
+  std::string getCurrentRunDuration( ) { return m_currentRunDuration ; } ;
+
+  /// Get current run number (cached)
+  int getCurrentRunNumber( ) { return m_currentRunNumber ; } ;
+
+  /// Check if this run exists
+  bool checkRun( int runNumber ) ;
+
+  /// Get partition
+  std::string getPartition() { return m_partition ; } ;
+    
+  /// Set Partition
+  void setPartition( const std::string & partition ) {
+    m_partition = partition ; } ;
+
+  /// Get destination
+  std::string getDestination() { return m_destination ; } ;
+
+  /// Set Destination
+  void setDestination( const std::string & destination ) {
+    m_destination = destination ; } ;
+
+private:
+  /// RunDB Web server address
+  std::string m_address;
+
+  /// Current (cached) run number
+  int m_currentRunNumber ;
+
+  /// Current run's start time
+  std::string m_currentRunStartTime ;
+
+  /// Current run's end time
+  std::string m_currentRunEndTime ;
+
+  /// Current run's duration 
+  std::string m_currentRunDuration ;
+
+  /// Partition
+  std::string m_partition ;
+
+  /// Destination
+  std::string m_destination ;
+
+};
+#endif // PRESENTER_RUNDB_H
