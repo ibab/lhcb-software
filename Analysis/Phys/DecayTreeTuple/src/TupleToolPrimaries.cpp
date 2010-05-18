024e473fc424e08f552979a2edf241a8077165c4
rlambert <rlambert@cern.ch>
2010-05-18 16:53:20 +0000

    Rewrite TupleToolPrimaries to make more sence to me.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83746 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0dee1b41371308bf58272275e6d76ffdb58c7177

--- Analysis/Phys/DecayTreeTuple/src/TupleToolPrimaries.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolPrimaries.cpp
@@ -1,4 +1,4 @@
-// $Id: TupleToolPrimaries.cpp,v 1.8 2010-03-01 11:47:34 rlambert Exp $
+// $Id: TupleToolPrimaries.cpp,v 1.9 2010-05-18 16:53:20 rlambert Exp $
 // Include files
 
 // from Gaudi
@@ -15,7 +15,7 @@
 #include <Kernel/DVAlgorithm.h>
 #include <Event/RecVertex.h>
 
-#include <functional>
+//#include <functional>
 
 //-----------------------------------------------------------------------------
 // Implementation file for class : TupleToolPrimaries
@@ -28,44 +28,6 @@
 DECLARE_TOOL_FACTORY( TupleToolPrimaries );
 
 using namespace LHCb;
-
-namespace {
-
-  class flat {
-  public:
-    flat( const RecVertex & r ) 
-      : pos( r.position() )
-      , mchi2( r.chi2() )
-      , mndof( r.nDoF() )
-      , ntracks( r.tracks().size() )
-    {
-      const Gaudi::SymMatrix3x3 & e = r.covMatrix ();
-      err = Gaudi::XYZPoint( std::sqrt( e(0,0) )
-			     ,std::sqrt( e(1,1) )
-			     ,std::sqrt( e(2,2) ) );
-    }
-    
-    double x() const { return pos.x(); }
-    double y() const { return pos.y(); }
-    double z() const { return pos.z(); }
-    
-    double ex() const { return err.x(); }
-    double ey() const { return err.y(); }
-    double ez() const { return err.z(); }
-    
-    double chi2() const { return mchi2; }
-    int ndof() const { return mndof; }
-
-    int nTracks() const { return ntracks; }
-
-    flat(){};
-  private:
-    Gaudi::XYZPoint pos,err;
-    double mchi2;
-    int mndof, ntracks;
-  };
-
-}
 
 //=============================================================================
 // Standard constructor, initializes variables
@@ -106,52 +68,50 @@ StatusCode TupleToolPrimaries::fill( Tuples::Tuple& tuple )
 {
   const std::string prefix=fullName();
   
-  std::vector<flat> pvs;
-  std::vector<const flat*> refPvs;
-
+  std::vector<double>  pvx, pvy, pvz;
+  std::vector<double>  epvx, epvy, epvz;
+  std::vector<double>  pvchi2, pvndof, pvntracks;
+  
   const RecVertex::Container* PV = 0 ;
   //if ( ""==m_pvLocation ) PV = m_dva->primaryVertices();   // default
-  if (exist<RecVertex::Container>(m_pvLocation)){     // user given
+  if (exist<RecVertex::Container>(m_pvLocation))
+  {     // user given
     PV = get<RecVertex::Container>(m_pvLocation);
   }
-  if (0!=PV){
-    unsigned int size = std::min<unsigned int>( PV->size(), 40 );  
-    pvs.reserve( size );
-    refPvs.reserve( size );
+  if (0!=PV)
+  {
     
-    RecVertex::Container::const_iterator it = PV->begin();
-    for( ; (PV->end()!=it && int(it-PV->begin()) < int(size) ) ; ++it ){
-      pvs.push_back( **it ); // doing this avoid using delete at the end...
-      refPvs.push_back( &(pvs.back()) ); // to use mem_fun
+    for(RecVertex::Container::const_iterator i = PV->begin() ; PV->end()!=i ; ++i )
+    {
+      pvx.push_back( (*i)->position().X() );
+      pvy.push_back( (*i)->position().Y() );
+      pvz.push_back( (*i)->position().Z() );
+      epvx.push_back( std::sqrt((*i)->covMatrix()(0,0)) );
+      epvy.push_back( std::sqrt((*i)->covMatrix()(1,1)) );
+      epvz.push_back( std::sqrt((*i)->covMatrix()(2,2)) );
+      pvchi2.push_back((*i)->chi2());
+      pvndof.push_back((*i)->nDoF());
+      pvntracks.push_back((*i)->tracks().size());
     }
   
-    if( PV->size() > 40 ){
+    if( PV->size() > 40 )
+    {
       Warning("More than 40 primaries, some PVs will not be stored.");
     }
   }
   
-  StatusCode test;
-  test = tuple->farray( prefix+"PVX", std::mem_fun( &flat::x ),
-			prefix+"PVY", std::mem_fun( &flat::y ),
-			prefix+"PVZ", std::mem_fun( &flat::z ),
-			refPvs.begin(), refPvs.end(),
-			prefix+"nPV", 40 );
-  if( !test.isSuccess() ) return StatusCode::FAILURE;
+  bool test=true;
+  test &= tuple->farray( prefix+"PVX", pvx, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVY", pvy, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVZ", pvz, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVXERR", epvx, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVYERR", epvy, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVZERR", epvz, prefix+"nPV",  40 );
   
+  test &= tuple->farray( prefix+"PVCHI2", pvchi2, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVNDOF", pvndof, prefix+"nPV",  40 );
+  test &= tuple->farray( prefix+"PVNTRACKS", pvntracks, prefix+"nPV",  40 );
   
-  test = tuple->farray( prefix+"PVXERR", std::mem_fun( &flat::ex ),
-			prefix+"PVYERR", std::mem_fun( &flat::ey ),
-			prefix+"PVZERR", std::mem_fun( &flat::ez ),
-			refPvs.begin(), refPvs.end(),
-			prefix+"nPV", 40 );
-  if( !test.isSuccess() ) return StatusCode::FAILURE;
-  
-  test = tuple->farray( prefix+"PVCHI2", std::mem_fun( &flat::chi2 ),
-			prefix+"PVNDOF", std::mem_fun( &flat::ndof ),
-			prefix+"PVNTRACKS", std::mem_fun( &flat::nTracks ),
-			refPvs.begin(), refPvs.end(),
-			prefix+"nPV", 40 );
-
   return StatusCode(test);
   //  return StatusCode::SUCCESS;
 }
