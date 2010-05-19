46e82aa5e7c7cff811b1675564517291a05fa559
jpalac <jpalac@cern.ch>
2010-05-19 13:29:49 +0000

    add DecReport checking script.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@26322 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7547078ff675d5bcc62498ea693f947c872b1e0e

---
+++ Analysis/Phys/DSTWriterTests/scripts/check_decreports.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+'''
+Check the DecReports in a DST and print a summary of the number of events selected by each line.
+Useage:
+./check_decreports.py --input <some file name> --location <TES location of DecReports - Default /Event/Strip/Phys/DecReports>
+'''
+
+__author__ = "Juan PALACIOS juan.palacios@cern.ch"
+
+import sys, getopt
+from GaudiConf.Configuration import *
+from AnalysisPython import Dir, Functors
+from GaudiPython.Bindings import gbl, AppMgr, Helper
+
+decReportsLocation = '/Event/Strip/Phys/DecReports'
+filename = ''
+
+opts, args = getopt.getopt(sys.argv[1:], "l:i", ["input=", "locaiton="])
+
+for o, a in opts:
+    if o in ("-i", "--input"):
+        filename = a
+    elif o in ('-l', '--location') :
+        decReportsLocation = a
+
+assert(filename != '')
+
+lhcbApp = LHCbApp(DDDBtag = 'default',
+                  CondDBtag = 'default',
+                  DataType = '2010',
+                  Simulation = True)
+
+
+appMgr = AppMgr(outputlevel=4)
+appMgr.config( files = ['$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts'])
+appMgr.initialize()
+
+import atexit
+atexit.register(appMgr.exit)
+
+evtSvc = appMgr.evtSvc()
+
+evtSel = appMgr.evtSel()
+
+nextEvent = Functors.NextEvent(appMgr)
+
+evtSel.open(filename)
+
+nEvents=0
+nDecReports = 0
+decReportSummary = {}
+
+def addEntry(self, key, value) :
+    if self.has_key(key) :
+        self[key] += value
+    else :
+        self[key] = value
+
+while ( nextEvent() ) :
+   nEvents+=1
+   decReports = evtSvc[decReportsLocation]
+   if decReports :
+       nDecReports += 1
+       for decName in decReports.decisionNames() :
+           decision = decReports.decReport(decName)
+           if decision :
+               addEntry(decReportSummary, decName, decision.decision())
+
+print '======================================================================================'
+print 'Found', nDecReports, 'DecReports in', nEvents, 'events'
+print '======================================================================================'
+for decs in decReportSummary.iteritems() :
+    print decs[0], ":", decs[1]
+print '======================================================================================'
