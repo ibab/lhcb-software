4114298dae6f1b66e614b6f7b588976bb2bda43e
Kazu Carvalho Akiba <kazuyoshi.akiba@cern.ch>
2010-05-19 06:26:34 +0000

    Fix MDFWriterLite
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38103 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f161c495447279b451fbc52e9c701a74f28ff1b6

--- LHCb/DAQ/MDF/MDF/MDFWriterLite.h
+++ LHCb/DAQ/MDF/MDF/MDFWriterLite.h
@@ -1,4 +1,4 @@
-// $Id: MDFWriterLite.h,v 1.9 2008-04-10 09:58:42 cattanem Exp $
+// $Id: MDFWriterLite.h,v 1.10 2010-05-19 06:26:34 kakiba Exp $
 //  ====================================================================
 //  MDFWriterLite.h
 //  --------------------------------------------------------------------
@@ -9,12 +9,7 @@
 #ifndef MDF_MDFWRITERLITE_H
 #define MDF_MDFWRITERLITE_H
 
-#include "GaudiKernel/StreamBuffer.h"
-#include "GaudiKernel/Algorithm.h"
-#include "MDF/StreamDescriptor.h"
-#include "MDF/MDFIO.h"
-
-class TMD5;
+#include "MDF/MDFWriter.h"
 
 /*
  *    LHCb namespace
@@ -25,80 +20,29 @@ namespace LHCb    {
     *
     *
     * @author:  S. Suman & N. Neufeld
+    * @author   M.Frank
     * @version: 1.0
     */
-  class MDFWriterLite : public Algorithm, protected MDFIO   {
-
-  private:
+  class MDFWriterLite : public MDFWriter   {
+  protected:
     /// Number of bytes written out to the file so far.
     size_t m_bytesWritten;
     /// Number of events written out to the file so far. 
     size_t m_eventsWritten;
-  protected:
-    typedef LHCb::StreamDescriptor   Descriptor;
-    typedef Descriptor::Access       Access;
-
-    /// Stream descriptor (Initializes networking)
-    Access        m_connection;
-    /// Input parameters for connection parameters
-    std::string   m_connectParams;
-    /// Compression algorithm identifier
-    int           m_compress;
-    /// Flag to create checksum
-    int           m_genChecksum;
-    /// Flag to create MD5 checksum
-    bool          m_genMD5;
-    /// Streambuffer to hold uncompressed data
-    StreamBuffer  m_data;
-    /// Pointer to checksum object
-    TMD5*         m_md5;
     /// Maximum size of each file (after which a new file will be written)
-    size_t    m_maxFileSizeKB;
+    size_t m_maxFileSizeKB;
     /// Maximum number of events in a file (after which a new file will be written)
-    size_t        m_maxFileEvents;
-    /// Location of the raw banks in the TES
-    std::string    m_bankLocation;
+    size_t m_maxFileEvents;
 
-
-    /// Internal setup (may not be called from sub-classes!
-    void construct();
-
-    /** Allocate space for IO buffer
-      * @param[in] ioDesc Output IO descriptor       
-      * @param[in] len    Total length of the data buffer
-      *
-      * @return  Pointer to allocated memory space
-      */
-    virtual std::pair<char*,int> getDataSpace(void* const /* ioDesc */, size_t len)  {
-      m_data.reserve(len);
-      return std::pair<char*,int>(m_data.data(), m_data.size());
-    }
-
-    /** Write byte buffer to output stream
-      * @param[in] ioDesc Output IO descriptor       
-      * @param[in] data   Data buffer to be streamed
-      * @param[in] len    Length of the data buffer
-      *
-      * @return  Status code indicating success or failure.
-      */
-    virtual StatusCode writeBuffer(void* const ioDesc, const void* data, size_t len);
-
+    /// Transform file name in presence of catalogs
+    virtual std::string getConnection(const std::string& org_conn);
+    
   public:
-
     /// Standard algorithm constructor
     MDFWriterLite(const std::string& name, ISvcLocator* pSvcLocator);
 
-    /// Extended algorithm constructor
-    MDFWriterLite(MDFIO::Writer_t typ, const std::string& name, ISvcLocator* pSvcLocator);
-
     /// Standard Destructor
     virtual ~MDFWriterLite();
-
-    /// Algoritm overload: Initialize
-    virtual StatusCode initialize();
-
-    /// Algoritm overload: Finalize
-    virtual StatusCode finalize();
 
     /// Algoritm overload: Execute procedure
     virtual StatusCode execute();
--- LHCb/DAQ/MDF/components/MDFWriterLite.cpp
+++ LHCb/DAQ/MDF/components/MDFWriterLite.cpp
@@ -1,4 +1,4 @@
-// $Id: MDFWriterLite.cpp,v 1.1 2009-02-06 09:37:57 frankb Exp $
+// $Id: MDFWriterLite.cpp,v 1.2 2010-05-19 06:26:34 kakiba Exp $
 //  ====================================================================
 //  MDFWriterLite.cpp
 //  --------------------------------------------------------------------
@@ -7,122 +7,61 @@
 //
 //  ====================================================================
 #include "GaudiKernel/AlgFactory.h"
-#include "GaudiKernel/SmartDataPtr.h"
 #include "GaudiKernel/MsgStream.h"
-#include "MDF/RawEventHelpers.h"
+#include "GaudiUtils/IIODataManager.h"
+#include "MDF/RawDataConnection.h"
 #include "MDF/MDFWriterLite.h"
-#include "MDF/MDFHeader.h"
-#include "Event/RawEvent.h"
-#include "TMD5.h"
 
 #include <ctime>
 #include <clocale>
 
 DECLARE_NAMESPACE_ALGORITHM_FACTORY(LHCb,MDFWriterLite)
 
-namespace {  // Don't clutter global namespace!
-  void getDateStr(char *datestring, int strlen)   {
-    ::setlocale(LC_ALL, "");
-    time_t ctime = ::time(NULL);
-    struct tm *currtime = ::localtime(&ctime);
-    ::strftime (datestring, strlen, "%EY.%0m.%0d-%0H.%0M.%0S.mdf", currtime);
-    return;
-  }
-}
-
-/// Extended algorithm constructor
-LHCb::MDFWriterLite::MDFWriterLite(MDFIO::Writer_t typ, const std::string& nam, ISvcLocator* pSvc)
-: Algorithm(nam, pSvc), MDFIO(typ, nam)
-{
-  construct();
-  m_bytesWritten = m_eventsWritten = 0;
-}
+using namespace std;
+using namespace LHCb;
+using namespace Gaudi;
 
 /// Standard algorithm constructor
-LHCb::MDFWriterLite::MDFWriterLite(const std::string& nam, ISvcLocator* pSvc)
-: Algorithm(nam, pSvc), MDFIO(MDFIO::MDF_RECORDS, nam)
+MDFWriterLite::MDFWriterLite(const string& nam, ISvcLocator* pSvc)
+: MDFWriter(nam, pSvc)
 {
-  construct();
-}
-
-/// Internal setup (may not be called from sub-classes!
-void LHCb::MDFWriterLite::construct()   {
-  m_md5 = new TMD5();
-  m_data.reserve(1024*64);
-  declareProperty("Connection",     m_connectParams="");
-  declareProperty("Compress",       m_compress=2);        // File compression
-  declareProperty("ChecksumType",   m_genChecksum=1);     // Generate checksum
-  declareProperty("GenerateMD5",    m_genMD5=false);      // Generate MD5 checksum
-  declareProperty("DataType",       m_dataType=MDFIO::MDF_NONE); // Input data type
   declareProperty("MaxFileSizeKB",  m_maxFileSizeKB=2000000);
   declareProperty("MaxFileEvents",  m_maxFileEvents=1000000);
-  declareProperty("BankLocation",    m_bankLocation=LHCb::RawEventLocation::Default);  // Location of the banks in the TES
 }
 
-LHCb::MDFWriterLite::~MDFWriterLite()   {
-  if ( m_md5 ) delete m_md5;
-}
+MDFWriterLite::~MDFWriterLite()   {}
 
-/// Initialize
-StatusCode LHCb::MDFWriterLite::initialize()   {
+/// Transform file name in presence of catalogs
+std::string MDFWriterLite::getConnection(const std::string& org_conn)  {
   char dateStr[40];
+  ::setlocale(LC_ALL, "");
+  time_t ctime = ::time(NULL);
+  struct tm *currtime = ::localtime(&ctime);
+  ::strftime (dateStr,sizeof(dateStr), "%EY.%0m.%0d-%0H.%0M.%0S.mdf",currtime);
+  string con = org_conn+dateStr;
   MsgStream log(msgSvc(), name());
-  getDateStr(dateStr, sizeof(dateStr));
-  m_connection = Descriptor::connect(m_connectParams + dateStr);
-  if ( m_connection.ioDesc > 0 )  {
-    log << MSG::INFO << "Received event request connection." << endmsg;
-  }
-  else {
-    log << MSG::ERROR << "FAILED receiving event request connection." << endmsg;
-  }
-  return (m_connection.ioDesc > 0) ? StatusCode::SUCCESS : StatusCode::FAILURE;
-}
-
-/// Finalize
-StatusCode LHCb::MDFWriterLite::finalize() {
-  if ( m_genMD5 )  {
-    MsgStream log(msgSvc(), name());
-    m_md5->Final();
-    log << MSG::INFO << "Output:" << m_connectParams
-      << " MD5 sum:" << m_md5->AsString() << endmsg;
-  }
-  Descriptor::close(m_connection);
-  return StatusCode::SUCCESS;
+  log << MSG::ALWAYS << "New file name:" << con << endmsg;
+  return con;
 }
 
 /// Execute procedure
-StatusCode LHCb::MDFWriterLite::execute()    {
-  if (((m_bytesWritten>>10) > m_maxFileSizeKB) || (m_eventsWritten >= m_maxFileEvents)) {
-    Descriptor::close(m_connection);
-    char dateStr[40];
-    getDateStr(dateStr, sizeof(dateStr));
-    ////Time for a new file. Let's create it.
-    m_connection = Descriptor::connect(m_connectParams + dateStr); 
+StatusCode MDFWriterLite::execute()    {
+  if (((m_bytesWritten>>10) > m_maxFileSizeKB) || (m_eventsWritten >= m_maxFileEvents)) { 
+    MsgStream log(msgSvc(), name());
+    string con = getConnection(m_connectParams);
+    m_ioMgr->disconnect(m_connection).ignore();
+    //delete m_connection;
+    m_connection = new RawDataConnection(this,con);
+    StatusCode status = m_ioMgr->connectWrite(m_connection,IDataConnection::RECREATE,"MDF");
+    status.ignore();
+    if ( m_connection->isConnected() ) {
+      log << MSG::INFO << "Received event request connection." << endmsg;
+    }
+    else {
+      log << MSG::ERROR << "FAILED receiving event request connection." << endmsg;
+      return StatusCode::FAILURE;
+    }
     m_bytesWritten = m_eventsWritten = 0;
   }
-  setupMDFIO(msgSvc(),eventSvc());
-  switch(m_dataType) {
-    case MDF_NONE:
-      return commitRawBanks(m_compress, m_genChecksum, &m_connection, m_bankLocation);
-    case MDF_BANKS:
-    case MDF_RECORDS:
-      return commitRawBuffer(m_dataType, m_compress, m_genChecksum, &m_connection);
-    default:
-      break;
-  }
-  return StatusCode::FAILURE;
-}
-
-/// Write byte buffer to output stream
-StatusCode LHCb::MDFWriterLite::writeBuffer(void* const /* ioDesc */, const void* data, size_t len)    {
-  int res = Descriptor::write(m_connection, data, len);
-  if ( res )  {
-    if ( m_genMD5 )  {
-      m_md5->Update((const unsigned char*)data, len);
-    }
-    m_bytesWritten += len;
-    m_eventsWritten++;
-    return StatusCode::SUCCESS;
-  }
-  return StatusCode::FAILURE;
+  return MDFWriter::execute();
 }
