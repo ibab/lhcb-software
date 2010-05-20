98f730f06b83317a752f2cee26a69d15678631a1
Olivier Deschamps <odescham@cern.ch>
2010-05-20 08:41:52 +0000

    protect against dereferenced smartref to caloHypo
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@38109 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1723363a5732140eafd2f5eecfa6d86f084fe1c3

--- LHCb/Event/RecEvent/src/ProtoParticle.cpp
+++ LHCb/Event/RecEvent/src/ProtoParticle.cpp
@@ -1,4 +1,4 @@
-// $Id: ProtoParticle.cpp,v 1.11 2010-03-16 08:41:28 jonrob Exp $
+// $Id: ProtoParticle.cpp,v 1.12 2010-05-20 08:41:52 odescham Exp $
 
 // STL
 #include <algorithm>
@@ -41,15 +41,25 @@ LHCb::ProtoParticle::clearCalo( const LHCb::CaloHypo::Hypothesis & hypo )
   // number of hypos removed
   LHCb::ProtoParticle::ExtraInfo::size_type removed = 0;
 
+
+  
+  const SmartRefVector<LHCb::CaloHypo>::iterator it = std::remove ( m_calo.begin(), m_calo.end(), NULL );
+
+  // remove selected hypos
+  if ( m_calo.end() != it ){
+    removed += m_calo.end() - it;
+    m_calo.erase ( it , m_calo.end() );
+  }
+
+
   // Find hypos to remove
   const SmartRefVector<LHCb::CaloHypo>::iterator iHypoRemove =
     std::remove_if ( m_calo.begin(), m_calo.end(),
                      bind( &LHCb::CaloHypo::hypothesis, _1 ) == hypo );
 
   // remove selected hypos
-  if ( m_calo.end() != iHypoRemove )
-  {
-    removed = m_calo.end() - iHypoRemove;
+  if ( m_calo.end() != iHypoRemove ){
+    removed += m_calo.end() - iHypoRemove;
     m_calo.erase ( iHypoRemove , m_calo.end() );
   }
 
