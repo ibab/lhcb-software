167963e8070171bbbff2cd57a0372393e060875c
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-05-20 13:18:23 +0000

    looser J/psi->eee
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83837 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c55036d5bad2fda694dbaca4db60bde1bd0ec361

--- Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2ee.py
+++ Analysis/Phys/CommonParticles/python/CommonParticles/StdLooseJpsi2ee.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseJpsi2ee.py,v 1.3 2009-04-22 14:17:39 pkoppenb Exp $ 
+# $Id: StdLooseJpsi2ee.py,v 1.4 2010-05-20 13:18:23 pkoppenb Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseJpsi2ee.py
 #  configuration file for 'Standard Loose Jpsi2ee' 
@@ -11,7 +11,7 @@
 Configuration file for 'Standard Loose Jpsi2ee'
 """
 __author__  = "Patrick Koppenburg"
-__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.3 $"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.4 $"
 # =============================================================================
 __all__ = (
     'StdLooseJpsi2ee' ,
@@ -25,10 +25,10 @@ from CommonParticles.Utils import *
 ## ============================================================================
 ## create the algorithm 
 StdLooseJpsi2ee = CombineParticles ( 'StdLooseJpsi2ee' )
-StdLooseJpsi2ee.InputLocations = [ "StdLooseElectron" ]
+StdLooseJpsi2ee.InputLocations = [ "StdLooseElectrons" ]
 StdLooseJpsi2ee.DecayDescriptor = "J/psi(1S) -> e+ e-" 
 
-StdLooseJpsi2ee.CombinationCut = "ADAMASS('J/psi(1S)')<100*MeV"
+StdLooseJpsi2ee.CombinationCut = "AM>1500*MeV && AM<3800*MeV"
 StdLooseJpsi2ee.MotherCut = "(VFASPF(VCHI2/VDOF)<25)"
 
 ## configure Data-On-Demand service 
