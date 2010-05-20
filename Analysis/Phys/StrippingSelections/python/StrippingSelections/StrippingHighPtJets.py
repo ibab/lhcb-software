893692777f5d6526205be857aa0ae493225ecaf2
Marcin Kucharczyk <marcin@cern.ch>
2010-05-20 12:46:20 +0000

    Add new propery in StrippingHighPtJets
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83825 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 561c892f3a10d8389ba1afae4d8ab1d32f58b088

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingHighPtJets.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingHighPtJets.py
@@ -14,20 +14,21 @@ from Configurables import GaudiSequencer
 from GaudiKernel.SystemOfUnits import *
 from CommonParticles.Utils import *
 from Configurables import PatPV3D
-from Configurables import HighPtJetFinder
+from Configurables import TrackClusterFinder
 from StrippingConf.StrippingLine import StrippingLine
 
 PatPV = PatPV3D("PatPV")
-JetFinder = HighPtJetFinder("JetFinder")
-JetFinder.MinWeight = 6
-JetFinder.MaxDeltaPhi = 8.5
-JetFinder.MaxDeltaEta = 0.16
-JetFinder.JetMult = 1
-JetFinder.Min1stJetPt = 3.0
-JetFinder.Min2ndJetPt = 0.0
+TrkClusterFinder = TrackClusterFinder("TrkClusterFinder")
+TrkClusterFinder.MinWeightSeed = 6
+TrkClusterFinder.ConeExtFactor = 4.0
+TrkClusterFinder.MaxDeltaPhi   = 8.5
+TrkClusterFinder.MaxDeltaEta   = 0.16
+TrkClusterFinder.Min1stJetPt   = 3.0
+TrkClusterFinder.Min2ndJetPt   = 0.0
+TrkClusterFinder.JetMult       = 1
 
 # Create line for high-pt jets selection
 line1 = StrippingLine('HighPtJetsSel'
                       , prescale = 1.0
-                      , algos = [PatPV,JetFinder]
+                      , algos = [PatPV,TrkClusterFinder]
                      )
