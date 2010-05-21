773ed34ed475842d9321fe071fd6b5fdefac99f4
rlambert <rlambert@cern.ch>
2010-05-21 04:59:48 +0000

    Adding package RecConf, to hold the RecSys configurables
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@31604 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 85d5a0f4ea18f20f82b75cab7808973445e80cec

---
+++ Rec/Rec/RecConf/doc/MainPage.h
@@ -0,0 +1,15 @@
+/** \mainpage notitle
+ *  \anchor recsysdoxygenmain
+ *
+ * This is the code reference manual for the %LHCb reconstruction classes.
+ * <p><b>See also:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href=" http://cern.ch/lhcb-release-area/DOC/rec/"><b>Rec project Web pages</b></a>
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core classes)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
+ * \li \ref externaldocs     "Related external libraries"
+
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ */
