ea59ff6815e07ee1e7699fbec5ded92ef072300d
jpalac <jpalac@cern.ch>
2010-05-21 15:39:08 +0000

    Add nested list flattening.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@83871 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f9393cc1d0011ece9ccfcfb84d56fdd27553d4dc

--- Analysis/Phys/DSTWriters/python/DSTWriters/dstwriterutils.py
+++ Analysis/Phys/DSTWriters/python/DSTWriters/dstwriterutils.py
@@ -4,6 +4,24 @@ __author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
 
 __all__ = ('ConfigurableList')
 
+def isIterable(obj) :
+    '''
+    Test if an object is iterable but not a string type.
+    '''
+    return hasattr(el, "__iter__") and not isinstance(obj, basestring) 
+
+def flattenList(sequence) :
+    """flattenList(sequence) -> flat list
+    Return a flat list containing all the non-iterable elements retrieved sequence and sequence's sub-sequences. 
+    """
+    flatList = []
+    for member in sequence :
+        if isIterable(member) :
+            flatList.extend(flattenList(member))
+        else :
+            flattenList.append(member)
+
+
 class ConfigurableList(object) :
     '''
     Return a list of plain configurables from a SelectionSequence style object.
@@ -20,6 +38,7 @@ class ConfigurableList(object) :
         return type(self._sel).__name__
     def flatList(self) :
         if self.name() in ConfigurableList._algos.keys() :
-            return ConfigurableList._algos[self.name()](self._sel)
+            return flattenList(ConfigurableList._algos[self.name()](self._sel))
         else :
             return ConfigurableList._algos['DEFAULT'](self._sel)
+
