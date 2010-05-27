46a6b0035fa9e8205758a49df3958b2582f9ea8c
Albert Bursche <albert.frithjof.bursche@cern.ch>
2010-05-27 11:21:43 +0000

    Added plots.py
    Now plots.py contains the plotting while dump_align_db.py is only little restructurised to allow insertion of a callback funktion in the main recursive part.
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@89587 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f2e1862d17bbf4def988a1c647300d27d48f8974

--- Alignment/Alignment/Escher/scripts/dump_align_db.py
+++ Alignment/Alignment/Escher/scripts/dump_align_db.py
@@ -15,132 +15,7 @@ from GaudiPython.Bindings import gbl
 
 from Gaudi.Configuration import *
 
-
-class DetectorObject:
-    def __init__(self,name,x,y,z,dx,dy,dz):
-
-        self.name = name
-
-        self.X  =  x
-        self.Y  =  y
-        self.Z  =  z
-        
-        self.TX = dx
-        self.TY = dy
-        self.TZ = dz
-
-    def phi(self):
-        """
-        polar angle
-        """
-        if (self.X!=0):
-            return math.atan(self.Y/self.X)
-        else:
-            return 0.0
-
-    def rho(self):
-        """
-        distance from z axis
-        """
-        return math.sqrt(self.X**2+self.Y**2)
-
-    def theta(self):
-        if self.Z!=0:
-            return math.atan(self.rho()/self.Z)
-
-    def eta(self):
-        """
-        pseudorapidity
-        """
-        return -math.log(math.tan(self.theta()/2))
-
-    def r(self):
-        """
-        distance to origin        
-        """
-        return math.sqrt(self.X**2+self.Y**2+self.Z**2)
-
-    
-class Plot:
-    def __init__(self,name,RegExpression,Fun,selector = lambda x: True,graph = None ):
-        import re
-        self.name = name
-        self.re =re.compile(RegExpression)
-        if verbose:
-            self.reFull = RegExpression
-            print RegExpression
-        self.functor = Fun
-        if graph:
-            self.Graph = graph
-        else:
-            self.graph = TGraph()
-            self.graph.SetMarkerSize(5)
-            self.graph.SetMarkerStyle(6)
-            self.graph.SetTitle(RegExpression)
-        self.current = 0
-        self.selector = selector
-        #self.initialiseAxes()
-        
-    def Fill(self,DetObj):
-        if self.re.match(DetObj.name):
-            if verbose:
-                print DetObj.name , "match", self.reFull
-            if self.selector(DetObj):
-                [x,y] = self.functor(DetObj)
-                if self.graph.GetN() < self.current:
-                    self.graph.Set(self.graph.GetN()+1000)
-                self.graph.SetPoint(self.current,x,y)
-                self.current += 1
-                self.initialiseAxes()
-        else:
-            if verbose:
-                print DetObj.name , "no match" , self.reFull
-
-    def initialiseAxes(self):
-        #find out whicht variables are plottet
-
-
-        #are all values different in do?
-        #this is nessecary to assign the axisses unabiguesly...
-
-        do = DetectorObject("dummy",1.,2.,3.,4.,5.,6.) 
-        
-        values = {do.X:"x",
-                  do.Y:"y",
-                  do.Z:"z",
-                  do.TX:"Tx",
-                  do.TY:"Ty",
-                  do.TZ:"Tz",
-                  do.phi():"#phi",
-                  do.eta():"#eta",
-                  do.theta():"#theta",
-                  do.rho():"#rho"}
-
-                
-        self.graph.Draw("A")
-        xaxis = self.graph.GetXaxis()
-        yaxis = self.graph.GetYaxis()
-        [x,y] = self.functor(do)
-
-        for (val,axis) in [(x,xaxis),(y,yaxis)]:
-            axis.SetTitle(values[val])
-            if verbose:
-                print values[val]
-        self.initialised = True
-    def __del__(self):
-        if self.current==0:
-            return
-        #self.initialiseAxes()
-        #print self.name
-        self.graph.Set(self.current)
-        #print self.name
-        self.graph.Draw("AP")
-        #print self.name
-        gPad.Print(self.name) 
-
-
-
-def dumpToFile( element, output ) :
+def dumpToFile( element, output ,callback = lambda a,c,b,d:0) :
     geometryinfo = element.geometry()
     if geometryinfo:
         aligncondition = geometryinfo.alignmentCondition()
@@ -153,14 +28,6 @@ def dumpToFile( element, output ) :
             globalcenter = globalnominaltransform * globalorigin
             globalnominaltranslation = gbl.Gaudi.Transform3D(globalnominaltransform.Translation())
             
-            ex =  gbl.Gaudi.XYZPoint(1,0,0)
-            #mex = gbl.Gaudi.XYZPoint(-1,0,0)
-            #print ex.Phi() #prints 0 
-            #print mex.Phi() #prints pi
-            discr = (geometryinfo.toGlobalMatrixNominal().Rotation()*ex).Phi()/math.pi
-            #print [fabs (discr-x)<0.001 for x in [0,1,-0.0277777777778, 0.972222222222,-0.972222401409,-1,0.0277775985909]],discr, element.name()
-            #print fabs (discr) <0.3 ,0.7 < discr < 1.3,-0.7 > discr > -1.3,discr, element.name() 
-
             # compute the total delta in the global frame 
             globaltransform        = geometryinfo.toGlobalMatrix()
             globalnominaltransform = geometryinfo.toGlobalMatrixNominal()
@@ -182,7 +49,20 @@ def dumpToFile( element, output ) :
             localtranslations = GaudiPython.gbl.vector('double')(3)
             localrotations = GaudiPython.gbl.vector('double')(3)
             gbl.DetDesc.getZYXTransformParameters(locallocaldelta, localtranslations, localrotations)
-            
+             
+            ex =  gbl.Gaudi.XYZPoint(1,0,0)
+            #mex = gbl.Gaudi.XYZPoint(-1,0,0)
+            #print ex.Phi() #prints 0 
+            #print mex.Phi() #prints pi            
+            discr = (geometryinfo.toGlobalMatrixNominal().Rotation()*ex).Phi()/math.pi
+            rotations = [0,-0.0277777777778,0.0277775985909, 0.972222222222,-0.972222401409,1,-1]
+            pattern = [fabs (discr-x)<0.001 for x in rotations]
+            if True not in pattern:
+                print "Error: unknown rotation" , discr 
+            if True in pattern[3:7]:
+                RotZ180deg =  ROOT.Math.Transform3D(ROOT.Math.RotationZ(math.pi))
+                localdelta = localdelta * RotZ180deg
+
             line = element.name() 
             line += " : " + aligncondition.name() + " : "
             line += str( globalcenter.x() ) + " " +  str( globalcenter.y() ) + " " +  str( globalcenter.z() ) + " : "
@@ -192,14 +72,15 @@ def dumpToFile( element, output ) :
             line += str( localrotations[0] ) + " " + str( localrotations[1] ) + " "+ str( localrotations[2] )
             line += '\n'
             output.write( line )
-            if plots != None:
-                DetObj = DetectorObject(aligncondition.name(),
-                                        globalcenter.x(),globalcenter.y(),globalcenter.z(),
-                                        totaltranslations[0],totaltranslations[1],totaltranslations[2])
-                map(lambda x:x.Fill(DetObj),plots)
-                
+
+#            if plots != None:
+#                DetObj = DetectorObject(aligncondition.name(),
+#                                        globalcenter.x(),globalcenter.y(),globalcenter.z(),
+#                                        totaltranslations[0],totaltranslations[1],totaltranslations[2])
+#                map(lambda x:x.Fill(DetObj),plots)
+            callback(aligncondition.name(),globalcenter,totaltranslations,localdelta)
     for daughter in element.childIDetectorElements() :
-        dumpToFile( daughter, output )
+        dumpToFile( daughter, output ,callback)
  
 
 def main():
@@ -223,75 +104,18 @@ def main():
                       default=-1)
     parser.add_option("-d", "--aligndb",action="append", dest="aligndb",help="path to file with alignment database layer. you can use this option multiple times")
     parser.add_option("-p","--plot",type="string",default=None,dest="plot",help="generate a lot of plots.")
-    parser.add_option("-v","--verbose",action="store_true",dest="verbose",default=False,
-                      help="make a lot o noise!")
+    #parser.add_option("-v","--verbose",action="store_true",dest="verbose",default=False,
+    #                  help="make a lot o noise!")
 
     (opts, args) = parser.parse_args()
     global plots
     plots =  None
-    global verbose
-    verbose = opts.verbose
-    if opts.plot:
-        plots = [Plot("TT-ZX"+opts.plot,"/TT.*",lambda do:[do.Z,do.X],lambda do:do.Z < 3000),
-                 Plot("all-ZX"+opts.plot,".*",lambda do:[do.Z,do.X]),
-
-                 Plot("TT-XTX"+opts.plot,"/TT.*",lambda do:[do.X,do.TX],lambda do:do.Z < 3000),
-                 Plot("TT-YTX"+opts.plot,"/TT.*",lambda do:[do.Y,do.TX],lambda do:do.Z < 3000),
-                 Plot("TT-ZTX"+opts.plot,"/TT.*",lambda do:[do.Z,do.TX],lambda do:do.Z < 3000),
-                 
-                 Plot("TT-XTY"+opts.plot,"/TT.*",lambda do:[do.X,do.TY],lambda do:do.Z < 3000),
-                 Plot("TT-YTY"+opts.plot,"/TT.*",lambda do:[do.Y,do.TY],lambda do:do.Z < 3000),
-                 Plot("TT-ZTY"+opts.plot,"/TT.*",lambda do:[do.Z,do.TY],lambda do:do.Z < 3000),
-                 
-                 Plot("TT-XTZ"+opts.plot,"/TT.*",lambda do:[do.X,do.TZ],lambda do:do.Z < 3000),
-                 Plot("TT-YTZ"+opts.plot,"/TT.*",lambda do:[do.Y,do.TZ],lambda do:do.Z < 3000),
-                 Plot("TT-ZTZ"+opts.plot,"/TT.*",lambda do:[do.Z,do.TZ],lambda do:do.Z < 3000),
-
-                 Plot("TTaX-XTX"+opts.plot,"/TT.*",lambda do:[do.X,do.TX],lambda do:do.Z < 2340 and do.Z>2300),
-                 Plot("TTaU-XTX"+opts.plot,"/TT.*",lambda do:[do.X,do.TX],lambda do:do.Z < 2400 and do.Z>2360),
-                 Plot("TTbV-XTX"+opts.plot,"/TT.*",lambda do:[do.X,do.TX],lambda do:do.Z < 2625 and do.Z>2550),
-                 Plot("TTbX-XTX"+opts.plot,"/TT.*",lambda do:[do.X,do.TX],lambda do:do.Z < 2670 and do.Z>2625),
-                 Plot("TTaX-XTY"+opts.plot,"/TT.*",lambda do:[do.X,do.TY],lambda do:do.Z < 2340 and do.Z>2300),
-                 Plot("TTaU-XTY"+opts.plot,"/TT.*",lambda do:[do.X,do.TY],lambda do:do.Z < 2400 and do.Z>2360),
-                 Plot("TTbV-XTY"+opts.plot,"/TT.*",lambda do:[do.X,do.TY],lambda do:do.Z < 2625 and do.Z>2550),
-                 Plot("TTbX-XTY"+opts.plot,"/TT.*",lambda do:[do.X,do.TY],lambda do:do.Z < 2670 and do.Z>2625),
-                 Plot("TTaX-XTZ"+opts.plot,"/TT.*",lambda do:[do.X,do.TZ],lambda do:do.Z < 2340 and do.Z>2300),
-                 Plot("TTaU-XTZ"+opts.plot,"/TT.*",lambda do:[do.X,do.TZ],lambda do:do.Z < 2400 and do.Z>2360),
-                 Plot("TTbV-XTZ"+opts.plot,"/TT.*",lambda do:[do.X,do.TZ],lambda do:do.Z < 2625 and do.Z>2550),
-                 Plot("TTbX-XTZ"+opts.plot,"/TT.*",lambda do:[do.X,do.TZ],lambda do:do.Z < 2670 and do.Z>2625),
-                 Plot("IT-ZY"+opts.plot,".*",lambda do:[do.Z,do.Y],lambda do:do.Z>3000 and math.fabs(do.rho()<500)),
-                 Plot("IT-ZX"+opts.plot,".*",lambda do:[do.Z,do.X],lambda do:do.Z>3000 and math.fabs(do.rho()<500)),
-
-                 Plot("IT-XTX"+opts.plot,".*",lambda do:[do.X,do.TX],lambda do:do.Z>3000 and math.fabs(do.rho()<500)),
-
-
-
-
-                 Plot("IT-RhoPhi"+opts.plot,".*",lambda do:[do.rho(),do.phi()],lambda do:do.Z>3000 and math.fabs(do.rho()<500)),
-                 Plot("all-ZY"+opts.plot,".*",lambda do:[do.Z,do.Y]),
-                 Plot("all-RhoPhi"+opts.plot,".*",lambda do:[do.rho(),do.phi()]) ]
-
-        for s in ["TTbV","TTbX","TTaX","TTaU",
-                  "ITT1CSide","ITT1ASide","ITT1BottomLayer","ITT1TopLayer",
-                  "ITT2CSide","ITT2ASide","ITT2BottomLayer","ITT2TopLayer",
-                  "ITT3CSide","ITT3ASide","ITT3BottomLayer","ITT3TopLayer",
-                  "ITT"]:
-            for (i,function) in enumerate([lambda do:[do.X,do.TX],
-                                           lambda do:[do.X,do.TY],
-                                           lambda do:[do.X,do.TZ],
-
-                                           lambda do:[do.Y,do.TX],
-                                           lambda do:[do.Y,do.TY],
-                                           lambda do:[do.Y,do.TZ],
-                                           
-                                           lambda do:[do.Z,do.TX],
-                                           lambda do:[do.Z,do.TY],
-                                           lambda do:[do.Z,do.TZ]]):
-
-                plots.append(Plot(s+"XYZ"[i//3]+"T"+"XYZ"[i%3]+opts.plot,"/"+s+"(?!.*Sensor.*)",function))
-        
+    #global verbose
+    #verbose = opts.verbose
     outputfilename = args[0]
+    CallDumpToFile(outputfilename,opts)
 
+def CallDumpToFile(outputfilename,opts,callback = lambda a,c,b,d:0):
     # we'll count everything in seconds
     nsPerSecond  = 1000000000
     from PyCool import cool
@@ -318,7 +142,7 @@ def main():
 
     appConf = ApplicationMgr( OutputLevel = INFO, AppName = 'myBrunel' )
     appMgr = GaudiPython.AppMgr()
-
+    print "started AppMgr"
     parentelements = [
         '/dd/Structure/LHCb/BeforeMagnetRegion/Velo',
         '/dd/Structure/LHCb/BeforeMagnetRegion/TT',
@@ -331,9 +155,13 @@ def main():
 
     for parentelementname in parentelements:
         parentelement = appMgr.detSvc()[parentelementname]
-        dumpToFile( parentelement, outputfile )
-
+        dumpToFile( parentelement, outputfile ,callback)
     outputfile.close()
+
+
+#    if plots:
+#        map(lambda p:p.save(),plots)
+#    outputfile.close()
     
 if __name__ == '__main__':
     main()
