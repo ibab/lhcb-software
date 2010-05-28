6ad53de29f069663b9e76873dab2c99cd17ad6d9
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-05-28 09:47:02 +0000

    fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@51659 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 12d889f46c47701f8939d72e6a1fb59646e5ba00

---
+++ Phys/Phys/LoKiUtils/python/LoKiUtils/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# =============================================================================
+# $Id: __init__.py,v 1.1 2010-05-28 09:47:02 ibelyaev Exp $
+# =============================================================================
+## @file  LoKiUtils/__init__.py
+#  Helper file to manage LoKiUtils package
+#
+#        This file is a part of LoKi project - 
+#    "C++ ToolKit  for Smart and Friendly Physics Analysis"
+#
+#  The package has been designed with the kind help from
+#  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+#  contributions and advices from G.Raven, J.van Tilburg, 
+#  A.Golutvin, P.Koppenburg have been used in the design.
+#
+#  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+#  @date 2010-05-28
+# =============================================================================
+"""
+Helper file to manage LoKiUtils package
+
+      This file is a part of LoKi project - 
+'C++ ToolKit  for Smart and Friendly Physics Analysis'
+
+The package has been designed with the kind help from
+Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+contributions and advices from G.Raven, J.van Tilburg, 
+A.Golutvin, P.Koppenburg have been used in the design.
+"""
+# =============================================================================
+__author__  = "Vanya BELYAEV Ivan.BElyaev@nikhef.nl"
+__date__    = "2010-05-28"
+__version__ = "CVS tag $Name: not supported by cvs2svn $, version $Revision: 1.1 $"
+
+# =============================================================================
+if '__main__' == __name__ :
+    print 80*'*'
+    print __doc__
+    print ' Author  : ', __author__
+    print ' Date    : ', __date__
+    print ' Version : ', __version__
+    print 80*'*'
+
+# =============================================================================
+# The END 
+# =============================================================================
