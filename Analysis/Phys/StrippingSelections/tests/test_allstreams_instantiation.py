fcf98a1e0e898541d43af2f650f4abce19155bb0
jpalac <jpalac@cern.ch>
2010-05-29 16:07:44 +0000

    make into valid QM test.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84193 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e8ee519888b2cc8abcb97d39b5eb352d36e7079e

--- Analysis/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
+++ Analysis/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-'''
- Test that Stripping Streams can be instantiated.
- This is the most basic test that should be run when modifying or
- adding Stripping Streams to StrippingSelections.Streams.allStreams.
-'''
+'''Tests of StrippingStreams instantiation.'''
+
 __author__ = 'Juan Palacios juan.palacios@nikhef.nl'
 
-from StrippingConf.Configuration import StrippingConf
-from StrippingSelections.Streams import allStreams
+def test_instantiate_StrippingConf_with_all_streams() :
+    '''
+    Test that Stripping Streams can be instantiated.
+    This is the most basic test that should be run when modifying or
+    adding Stripping Streams to StrippingSelections.Streams.allStreams.
+    '''
+    from StrippingConf.Configuration import StrippingConf
+    from StrippingSelections.Streams import allStreams
 
-sc = StrippingConf (Streams = allStreams)
+    sc = StrippingConf (Streams = allStreams)
+
+if __name__ == '__main__' :
+    test_instantiate_StrippingConf_with_all_streams()
