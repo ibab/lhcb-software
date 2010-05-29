bc8775ce497cbb1f54bdc466c13c6b36aa7723dc
jpalac <jpalac@cern.ch>
2010-05-29 15:26:58 +0000

    Tydy up.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@31905 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ddd1f662dc5faeeef9678b78f6535e1c578a8270

--- Analysis/Phys/DSTWriterTests/scripts/check_dst_container_size.py
+++ Analysis/Phys/DSTWriterTests/scripts/check_dst_container_size.py
@@ -11,18 +11,26 @@ import sys, getopt
 from GaudiConf.Configuration import *
 from ROOT import TFile, TTree, TObject, TList
 
-def printTree( filename, outputfilename = 'out.txt'  ) :
+class TreeInfo(object) :
+    def __init__(self, tree) :
+        self.location = tree.GetName().replace('_', '/')
+        self.sizeInBytes = tree.GetTotBytes()
+        self.entries = tree.GetEntries()
+
+def getTreeInfo( filename, outputfilename = 'out.txt'  ) :
 
     file = TFile.Open(filename)
-
+    summary = []
     if file :
         for treeobj in file.GetListOfKeys() :
             tree = file.Get( treeobj.GetName() )
-            if tree :  
-                print treeobj.GetName().replace('_', '/'), ':', tree.GetTotBytes(), 'Bytes', tree.GetEntries(), 'entries'
+            if tree :
+                summary.append( TreeInfo( tree ) )
     else :
         print 'ERROR: Could not open file', filename
-            
+    return summary
+
+   
 if __name__ == '__main__' :
     filename = ''
     output = 'out.txt'
@@ -36,4 +44,12 @@ if __name__ == '__main__' :
 
     assert(filename != '')
 
-    printTree(filename, output)
+    stats = getTreeInfo(filename, output)
+
+    length = len(sorted([s.location for s in stats], cmp = lambda x,y : cmp(len(y), len(x)))[0])+2
+    print '----------------------------------------------------------------------------------'
+    for s in stats :
+        message = s.location.ljust(length) + str(str(s.sizeInBytes)+' / '+ str(s.entries)+'     Bytes/Entries.').rjust(10)
+        print message
+    print '----------------------------------------------------------------------------------'
+
