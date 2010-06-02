c5ce222b0e3d947adb4b6bd0e4b398fef27dc45d
Greig Cowan <g.cowan@ed.ac.uk>
2010-06-02 14:18:29 +0000

    Added options for redoing the stripping
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84260 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5403e7f8b6767b8e3e1381eb5340d845a8b856ff

---
+++ Analysis/Phys/StrippingSettings/options/RecoStripping-04.py
@@ -0,0 +1,44 @@
+from Gaudi.Configuration import *
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+#
+# DaVinci Monitoring is done first
+#
+importOptions( "$DAVINCIMONITORSROOT/options/DaVinciMonitoring.py") 
+
+#
+# Stripping job configuration
+#
+from StrippingSelections.Streams import allStreams
+from StrippingConf.Configuration import StrippingConf
+sc = StrippingConf( Streams = allStreams )
+
+from Configurables import SelDSTWriter
+dstWriter = SelDSTWriter("MyDSTWriter",
+	SelectionSequences = sc.activeStreams(),
+        OutputPrefix = 'Strip',
+	OutputFileSuffix = '000000')
+dstWriter.ExtraItems += ['/Event/DAQ/RawEvent#1', '/Event/Strip/Phys/DecReports#1']
+
+#
+# DaVinci configuration
+#
+from Configurables import DaVinci
+DaVinci().DataType = "2010"
+DaVinci().InputType = 'SDST'
+DaVinci().EvtMax = 10000 
+DaVinci().appendToMainSequence( [ sc.sequence() ] )
+DaVinci().appendToMainSequence( [ dstWriter.sequence() ] )
+DaVinci().HistogramFile = "DVHistos.root"
+
+#
+# Additional options for accessing Oracle conditions DB and
+# to disable the usage of LFC to get the credentials to access Oracle servers
+#
+from Configurables import CondDB
+CondDB(UseOracle = True)
+
+from Configurables import COOLConfSvc
+def disableLFC():
+    COOLConfSvc(UseLFCReplicaSvc = False)
+appendPostConfigAction(disableLFC)
