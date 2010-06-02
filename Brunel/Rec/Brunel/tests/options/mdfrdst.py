cdd288d5ff7af44fefb64d6e18df1f07bec762fa
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-06-02 08:22:53 +0000

    remove duplicate test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@31994 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b25b847b59b3b9819e22e1441bf074b062fa566f

--- Brunel/Rec/Brunel/tests/options/mdfrdst.py
+++
@@ -1,7 +0,0 @@
-# Options to set up MDF RDST configuration test for Brunel
-from Brunel.Configuration import *
-
-Brunel().OutputType = 'RDST'
-Brunel().PackType   = 'MDF'
-
-Brunel().Monitors  = ["FPE","SC"]
