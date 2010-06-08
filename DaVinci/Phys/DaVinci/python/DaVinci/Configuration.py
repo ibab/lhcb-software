c168a4cd0dc61d7388fc374fca289066f6942787
jpalac <jpalac@cern.ch>
2010-06-08 12:05:07 +0000

    Un-do last red-herring motivated change.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@84369 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2c61d805626a9166dcd15a3c2823f952eb5cc97c

--- DaVinci/Phys/DaVinci/python/DaVinci/Configuration.py
+++ DaVinci/Phys/DaVinci/python/DaVinci/Configuration.py
@@ -1,7 +1,7 @@
 """
 High level configuration tools for DaVinci
 """
-__version__ = "$Id: Configuration.py,v 1.111 2010-06-08 08:42:30 jpalac Exp $"
+__version__ = "$Id: Configuration.py,v 1.112 2010-06-08 12:05:07 jpalac Exp $"
 __author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
 
 from LHCbKernel.Configuration import *
@@ -501,10 +501,7 @@ class DaVinci(LHCbConfigurableUser) :
         '''
         FileRecordDataSvc().ForceLeaves         = True
         FileRecordDataSvc().RootCLID            = 1
-        '''
         FileRecordDataSvc().PersistencySvc      = "PersistencySvc/FileRecordPersistencySvc"
-
-        '''
         ApplicationMgr().ExtSvc += [ PoolDbCnvSvc("FileRecordCnvSvc",
                                                   DbType = "POOL_ROOTTREE",
                                                   ShareFiles = "YES" )
