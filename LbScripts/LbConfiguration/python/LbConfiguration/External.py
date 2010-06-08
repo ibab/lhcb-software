2540f7717abf70ae431a8afe897ba0e229b601b9
hmdegaud <hmdegaud@cern.ch>
2010-06-08 18:43:13 +0000

    * scripts/doxygen: New file. Wrapper script to call the version of doxygen specified in the LbConfiguration.External module. The wrapper takes 2 options 	  --lcgcmt-version : specifiy the interface version (ex 58c) 	  --doxygen-version : override the default version * scripts/getExtVersion: New file. Script to extract the external version from the LbConfiguration.External module * python/LbConfiguration/External.py:   - added the getExternalVersion function.   - added the doxygen_version.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@38244 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9f94af4bbaf6d62d6b9ebe40cb78642ea6992a3

--- LbScripts/LbConfiguration/python/LbConfiguration/External.py
+++ LbScripts/LbConfiguration/python/LbConfiguration/External.py
@@ -1,8 +1,18 @@
 """ General LHCb configuration for external software """
 
+import sys
+
+
 Python_version     = "2.5"
 CMT_version        = "v1r20p20090520"
 tbroadcast_version = "v2.0.5"
+doxygen_version    = "1.6.2"
+
 
 external_projects  = ['LCGCMT', 'GENSER', 'OpenScientist']
 lcg_projects       = ['LCGCMT', 'SEAL','POOL','GENSER','COOL','GDML','CORAL','RELAX']
+
+def getExternalVersion(ext_name):
+    vers_attr = "%s_version" % ext_name
+    if hasattr(sys.modules[__name__], vers_attr) :
+        return getattr(sys.modules[__name__], vers_attr)
