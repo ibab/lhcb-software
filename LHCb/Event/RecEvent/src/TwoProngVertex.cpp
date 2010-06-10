21b15b954f55b205f100c0f923d6b99fc8d5aa9e
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-06-10 15:20:55 +0000

    added p3 and cov6 methods to TwoProngVertex
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@40902 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3d140473de5e8b0f95bc810214c9487edf42c911

--- LHCb/Event/RecEvent/src/TwoProngVertex.cpp
+++ LHCb/Event/RecEvent/src/TwoProngVertex.cpp
@@ -44,9 +44,25 @@ namespace LHCb
        return;
      }
                   
+  
+  Gaudi::XYZVector TwoProngVertex::p3A() const
+  {
+    double tx = m_momA(0) ;
+    double ty = m_momA(1) ;
+    double p  = 1/std::abs( m_momA(2) ) ;
+    double pz = p/std::sqrt(1 + tx*tx + ty*ty ) ;
+    return Gaudi::XYZVector( pz*tx, pz*ty, pz) ; 
+  }
 
-
- 
+  Gaudi::XYZVector TwoProngVertex::p3B() const
+  {
+    double tx = m_momB(0) ;
+    double ty = m_momB(1) ;
+    double p  = 1/std::abs( m_momB(2) ) ;
+    double pz = p/std::sqrt(1 + tx*tx + ty*ty ) ;
+    return Gaudi::XYZVector( pz*tx, pz*ty, pz) ; 
+  }
+   
   const Gaudi::SymMatrix9x9 TwoProngVertex::covMatrix9x9() const
   {
     ///< Return the covariance matrix for position and track 1 & track 2 momenta
@@ -112,6 +128,71 @@ namespace LHCb
     theMatrix.Place_at(covmommom, 3,3);
     // off diagonal elements
     for(int i = 0 ; i <4; ++i)
+      for(int j =0;j<3;++j)
+        theMatrix(i+3,j) = covmompos(i,j);
+    
+    return theMatrix;
+  }
+  
+  namespace {
+    inline void  JacobdP3dMom 
+    ( const ROOT::Math::SVector<double,3>& mom  ,
+      ROOT::Math::SMatrix<double,3,3>&     J    )
+    {
+      double tx = mom(0) ;
+      double ty = mom(1) ;
+      double qop = mom(2) ;
+      double p  = 1/std::abs(qop) ;
+      double n2 = 1 + tx*tx + ty*ty ;
+      double n  = std::sqrt(n2) ;
+      double n3 = n2*n ;
+      double pz = p/n ;
+      double px = pz*tx ;
+      double py = pz*ty ;
+      J(0,0) = p * (1+ty*ty)/n3 ; // dpx/dtx
+      J(0,1) = p * tx * -ty/n3  ; // dpx/dty
+      J(0,2) = -px/qop ;          // dpx/dqop
+
+      J(1,0) = p * ty * -tx/n3  ; // dpy/dtx
+      J(1,1) = p * (1+tx*tx)/n3 ; // dpy/dty
+      J(1,2) = -py/qop ;          // dpy/dqop
+
+      J(2,0) = pz * -tx/n2 ;      // dpz/dtx
+      J(2,1) = pz * -ty/n2 ;      // dpz/dtx
+      J(2,2) = -pz/qop ;          // dpz/dqop
+    }
+  }
+
+  Gaudi::SymMatrix6x6 TwoProngVertex::covMatrix6x6() const
+  {
+    //< Return the covariance matrix for the V0
+    
+    //the Jacobians to change momenta of daughters from tx,ty,qop to PxPyPz space
+    Gaudi::Matrix3x3 JacobP1;
+    Gaudi::Matrix3x3 JacobP2;
+    JacobdP3dMom( m_momA, JacobP1);
+    JacobdP3dMom( m_momB, JacobP2);
+    
+    //covmompos contains the transformed covariance matrix for the off diagonal part (position and momentum)
+    Gaudi::Matrix3x3 covmompos;
+    covmompos =  JacobP1*  m_momposcovA;
+    covmompos += JacobP2 * m_momposcovB; 
+    
+    //covmommom contains the V0 symmetric covariance for the  momentum part
+    Gaudi::Matrix3x3 covmommom12 = JacobP1 * m_mommomcov * ROOT::Math::Transpose(JacobP2) ;
+    Gaudi::SymMatrix3x3 covmommom ;
+    ROOT::Math::AssignSym::Evaluate(covmommom, covmommom12 + ROOT::Math::Transpose(covmommom12)) ;
+    covmommom+=ROOT::Math::Similarity(JacobP1, m_momcovA); //J * C * JT
+    covmommom+=ROOT::Math::Similarity(JacobP2, m_momcovB);
+    
+    // finally, fill the returned matrix
+    Gaudi::SymMatrix6x6 theMatrix;
+    // position part from rec vertex
+    theMatrix.Place_at(LHCb::VertexBase::covMatrix(), 0,0);
+    // momentum part
+    theMatrix.Place_at(covmommom, 3,3);
+    // off diagonal elements
+    for(int i = 0 ; i<3; ++i)
       for(int j =0;j<3;++j)
         theMatrix(i+3,j) = covmompos(i,j);
     
