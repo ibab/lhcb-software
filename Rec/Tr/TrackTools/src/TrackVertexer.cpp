3c2ad85ab7bcfeb681888e4070ed9601b810aa85
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-06-12 07:36:20 +0000

    added computeDecayLength
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@40967 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b7500210ae9f0669640c499b0fe782f23f49e85b

--- Rec/Tr/TrackTools/src/TrackVertexer.cpp
+++ Rec/Tr/TrackTools/src/TrackVertexer.cpp
@@ -111,3 +111,70 @@ TrackVertexer::fit(const TrackContainer& tracks) const
   
   return recvertex ;
 }
+
+namespace {
+  inline Gaudi::Vector3 transform( const Gaudi::XYZVector& vec)
+  {
+    return Gaudi::Vector3(vec.X(),vec.Y(),vec.Z()) ;
+  }
+}
+
+bool
+TrackVertexer::computeDecayLength(const LHCb::TwoProngVertex& vertex,
+				  const LHCb::RecVertex& pv,
+				  double& chi2,
+				  double& decaylength,
+				  double& decaylengtherr) const
+{
+  // This calculation is basically a 1-iteration beamspot fit. The
+  // constraint is 
+  //
+  //    r = x - lambda p/|p| - xbs
+  // 
+  // where x and p are the position of the decay vertex of the
+  // candidate and its momentum, lambda is the decaylength and xbs
+  // the position of the beamspot. The covariance in the constraint
+  // is
+  //
+  //    V = Vbs + Vxx - a * Vxp - a Vxp^T + a^2 * Vpp
+  //
+  // where a=lambda/|p|^2. It needs an initial estimate for the
+  // flightlength, for which we simply take the projection of deltaX
+  // on the direction. We now minimize  the chisquare contribution 
+  // 
+  //     chi^2 = r^T V^{-1} r
+  //
+  // for lambda.
+
+
+  const Gaudi::XYZPoint pos = vertex.position() ;
+  // I really only need p3, not p4
+  Gaudi::SymMatrix6x6 cov6  = vertex.covMatrix6x6() ;
+  Gaudi::XYZVector p3 = vertex.p3() ;
+  Gaudi::Vector3 dx = transform(pos - pv.position()) ;
+  double p3mag = p3.R() ; 
+  Gaudi::Vector3 dir = transform(p3.Unit()) ;
+  Gaudi::SymMatrix3x3 W = pv.covMatrix() ;
+    
+   //double a = (ROOT::Math::Transpose(dir)*dx)/p3mag  ;
+  double a = ROOT::Math::Dot(dir,dx)/p3mag  ;
+  for(size_t row=0; row<3; ++row)
+    for(size_t col=0; col<=row; ++col)
+      W(row,col) += 
+	cov6(row,col)
+	+ a*a * cov6(row+3,col+3)
+	- a * (cov6(row+3,col) + cov6(col+3,row)) ;
+  
+  int OK = W.InvertChol() ;
+  if( !OK ) info() << "inversion error in fitDecayLength" << endmsg ;
+  
+  double halfdChi2dLam2 = ROOT::Math::Similarity(W,dir) ;
+  decaylength    = ROOT::Math::Dot(dir,W*dx)/halfdChi2dLam2 ;
+  decaylengtherr = std::sqrt(1/halfdChi2dLam2) ;
+  
+  Gaudi::Vector3 res = dx - decaylength * dir ;
+  
+  chi2 = ROOT::Math::Similarity(W,res) ;
+
+  return OK ;
+}
