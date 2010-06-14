787e06a31c4e98c7013b151ab31a2a63777dae12
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2010-06-14 23:04:39 +0000

    Update for new routing bits etc.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@84439 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6623f8c90ee19ece24fe333fdd24802c38073544

--- Hlt/Hlt/HltSelChecker/scripts/GetEfficiencies.py
+++ Hlt/Hlt/HltSelChecker/scripts/GetEfficiencies.py
@@ -10,72 +10,62 @@ def getStats(f,signal):
     """ returns the stats
     by Patrick Koppenburg"""
     debug = False
-    if (debug): print "ENTRY ", signal, sel
-    inTable = 0
+    if (debug): print "ENTRY ", signal
     foundTable = 0
-    Track = 0
-    EqualL = 0
-    Hlt2EffAlone = 0
     BestSelDict = {}
     epsilon = 0
+    L0Eff = 0.
+    Hlt1EffL0 = 0.
+    Hlt2EffOn1 = 0.
+    Trigger = 0.
+    EqualL = 0
+    foundTable = False
+    numL0 = 0.
+    numHlt1 = 0.
     for line in f:
-      doPrint = False
-      if inTable==0:
-        if re.search("Correlation Table",line):
-          inTable = 1
-          foundTable = 1
-      else:
-#        if (debug): print inTable, line
+        if (re.search("11:L0",line)):
+          if (debug): print "L0 line:", line
+          numL0 = float(line.split('|')[5].rstrip().lstrip())
+          L0Eff = 100.*float(line.split('|')[6].rstrip().lstrip())
+          if (debug): print "L0", L0Eff
+        elif (re.search("46:HLT",line)): # Hlt1-Physics
+          if (debug): print line
+          numHlt1 = float(line.split('|')[3].rstrip().lstrip())  
+          Hlt1EffL0 = 100.*numHlt1/numL0
+          if (debug): print "Hlt1 Physics", Hlt1EffL0
+        elif ( re.search("77:HLT",line)): # Hlt2-Physics
+          if (debug): print line
+          Hlt2EffOn1   = 100.*float(line.split('|')[3].rstrip().lstrip())/numHlt1
+          Trigger      = 100.*float(line.split('|')[3].rstrip().lstrip())/float(line.split('|')[2].rstrip().lstrip())
+          if (debug): print "Hlt2 Physics", Hlt2EffOn1
+          if (debug): print "Trigger efficiency", Trigger
+        if not re.search("Correlation Table",line) and not foundTable: continue
+        foundTable = True
         if re.search("=======",line):
           EqualL+=1
-          if (  EqualL==3 ):
-            break
-        else:  # in table
-          if (re.search("L0",line)):
-#            if (debug): print "L0 line:", line
-            L0Eff = float((line.split()[2]).replace("%",""))
- #           if (debug): print "L0", L0Eff
-          elif (re.search("Bit46",line)): # Hlt1-Physics
- #           if (debug): print line
-            Hlt1Eff   = float((line.split()[2]).replace("%",""))
-            Hlt1EffL0 = float((line.split()[4]).replace("%",""))
-            Hlt1EffOn2 = float((line.split()[6]).replace("%",""))
- #           if (debug): print "HltDecision", HltEff, HltEffL0
-          elif ( re.search("Bit66",line) and ( Hlt2EffAlone == 0 )): # Hlt2-Physics
- #           if (debug): print line
-            Hlt2EffAlone = float((line.split()[4]).replace("%",""))
-            Hlt2EffOn1   = float((line.split()[5]).replace("%",""))
-            Trigger      = Hlt2EffAlone*Hlt1Eff/100
-            HltEff       = 100.*Trigger/L0Eff
- #           if (debug): print "Hlt2EffAlone ", Hlt2EffAlone, Hlt2EffOn1, Trigger, HltEff
-          elif (re.search("FilterTrueTracks",line)): 
- #           if (debug): print line
-            Track = float((line.split()[4]).replace("%",""))
- #           if (debug): print "Tracking", Track
-          elif ((re.search("Decision",line)) and (re.search("Hlt2",line)) and not (re.search("PassThrough",line))):
-            epsilon = epsilon + 0.00001
-            if (debug): print line
-            sel =  (line.split()[1]).replace("Hlt2","").replace("Decision","")
-            Hlt2Sel = float((line.split()[5]).replace("%",""))+epsilon   # on Hlt1
-            if (debug): print sel, Hlt2Sel
-            BestSelDict[sel] = Hlt2Sel # make sure they never get same efficiency
-#            if (( line.split()[-1]).find('#')<0) :
-#                SelEff = float((line.split()[-1]).replace("%",""))  # track
-    if foundTable:
-        s0,v0 = sortedDictValues(BestSelDict,0)
-        s1,v1 = sortedDictValues(BestSelDict,1)
-        s2,v2 = sortedDictValues(BestSelDict,2)
-        s3,v3 = sortedDictValues(BestSelDict,3)
-        print "| <nox>"+signal," | ",int(Trigger+0.5),"%  |  ",int(L0Eff+0.5),"% |   ",int(HltEff+0.5),"% |   ",int(Hlt1EffL0+0.5),"% |  ",int(Hlt2EffAlone+0.5),"% |   ",int(Hlt2EffOn1+0.5),"% |  ",int(Track+0.5),"% | <nox>"+s0+":",int(v0+0.5),"%  | <nox>"+s1+":",int(v1+0.5),"%  | <nox>"+s2+":",int(v2+0.5),"%  | <nox>"+s3+":",int(v3+0.5), "%  |"
+          if (  EqualL==3 ): break  
+        elif ((re.search("Decision",line)) and (re.search("Hlt2",line)) and not (re.search("PassThrough",line))):
+          epsilon = epsilon + 0.00001
+          if (debug): print line
+          sel =  (line.split()[1]).replace("Hlt2","").replace("Decision","")
+          Hlt2Sel = float((line.split()[5]).replace("%",""))+epsilon   # on Hlt1
+          if (debug): print sel, Hlt2Sel
+          BestSelDict[sel] = Hlt2Sel # make sure they never get same efficiency
+    if foundTable :
+        s0,v0,tos0,tis0 = sortedDictValues(BestSelDict,0)
+        s1,v1,tos1,tis1 = sortedDictValues(BestSelDict,1)
+        print "| <nox>"+signal," | ",int(Trigger+0.5),"%  |  ",int(L0Eff+0.5),"% |  ",int(Hlt1EffL0+0.5),"% |  ",int(Hlt2EffOn1+0.5),"% | <nox>"+s0+":",int(v0+0.5),"%/",int(tos0+0.5),"%/",int(tis0+0.5),"%  | <nox>"+s1+":",int(v1+0.5),"%/",int(tos1+0.5),"%/",int(tis1+0.5),"%  |"
 
 def sortedDictValues(adict,item):
     array = sorted(adict.values())
 #    print array
     val = array[-1-item]
+    tos = -100
+    tis = -100
 #    print "looking for ", val
     for k, v in adict.iteritems():
         if (v==val): key=k
-    return key,val
+    return key,val,tos,tis
         
 ###############################################################################
 # Main program
@@ -93,7 +83,7 @@ else :
 
 files = sorted(glob.glob(head+'-*'))
 print "\n\n---++ Signal efficiencies\n"
-print "| *Signal*  | *Trigger* | *L0* | *Hlt* | *Hlt1* | *Hlt2* | *Hlt2/1* | *Tk* | *Best Sel* | *2nd Sel* | *3rd Sel* | *4th Sel* |"    
+print "| *Signal*  | *Trigger* | *L0* | *Hlt* | *Hlt1* | *Hlt2* | *Hlt2/1* | *Tk* | *Best Sel (All/TOS/TIS)* | *2nd Sel (All/TOS/TIS)* |"    
 for f in files :
     s = f.split('-')[1]
     #        print f, s
