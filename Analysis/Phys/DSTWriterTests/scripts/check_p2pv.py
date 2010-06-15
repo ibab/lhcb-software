f3c4b3a4b9a9489dc508ac5ecec25b3b6ad5e234
jpalac <jpalac@cern.ch>
2010-06-15 08:30:47 +0000

    . Generalise to scan all tree nodes ending in "/Particles", then look for     related PVs through relations tables, and print summary with location     of the related PVs.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@41020 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 96e92f030f5468689243d30554a4e864e2c6b71a

--- Analysis/Phys/DSTWriterTests/scripts/check_p2pv.py
+++ Analysis/Phys/DSTWriterTests/scripts/check_p2pv.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 '''
-Check some TES locations in a DST and print a summary of the number of events selected by each line.
+Check some TES locations with Particles in a DST and print a summary of the number of related PVs and their location. Uses stored Particle->PV relations tables..
 Useage:
-./check_dst_contents.py --input <some file name> --root <location of trunk - defailt "/Event"> --location <location of selection on top of root>
+./check_p2pv.py --input <some file name> --location <location of trunk - default "/Event">
 '''
-from __future__ import division
 
 __author__ = "Juan PALACIOS juan.palacios@cern.ch"
-
-from AnalysisPython.Helpers import deSmartRef
 
 def addEntry(self, key, value) :
     if self.has_key(key) :
@@ -16,20 +13,12 @@ def addEntry(self, key, value) :
     else :
         self[key] = value
 
-def particleLoop(particles, evtSvc, p2pvmap) :
-    if particles :
-        p2pv = evtSvc[location+'/Particle2VertexRelations']
-        for p in particles :
-            p = deSmartRef(p)
-            loc =  p.parent().registry().identifier()
-            nRels = 0
-            if p2pv :
-                rng = p2pv.relations(p)
-                nRels = rng.size()
-            addEntry(p2pvmap,loc, nRels)
-            particleLoop(p.daughters(), evtSvc, p2pvmap)
-
-
+class P2PVSummaryInfo(object) :
+    def __init__(self, entries) :
+        self.entries = entries
+        self.p2pvmap = {}
+    def addEntry(self, key, value) :
+        addEntry(self.p2pvmap, key, value)
 
 if __name__ == '__main__' :
 
@@ -40,18 +29,14 @@ if __name__ == '__main__' :
     import sys, getopt
 
     filename = ''
-    root = '/Event'
-    opts, args = getopt.getopt(sys.argv[1:], "l:i:r", ["input=", "location=", "root="])
+    location = '/Event'
+    opts, args = getopt.getopt(sys.argv[1:], "l:i", ["input=", "location="])
 
     for o, a in opts:
         if o in ("-i", "--input"):
             filename = a
         if o in ("-l", "--location") :
             location = a
-        if o in ("-r", "--root") :
-            root = a
-
-    location = root + '/' + location
             
     assert(filename != '')
 
@@ -74,28 +59,36 @@ if __name__ == '__main__' :
     evtSel.open(filename)
 
     nEvents=0
-    nCandidates = {}
-    p2pvmap = {}
-    nMoreThanOnePV = 0
-    particleLocation = location+'/Particles'
+    nSelEvents = {}
+    nObjects = {}
+    p2pvMap = {}
+    p2pvSummaries = {}
+    
     while ( nextEvent() ) :
         nEvents+=1
-        particles = evtSvc[particleLocation]
-        if particles :
-            particleLoop(particles, evtSvc, p2pvmap)
-            pvs = evtSvc[root+'/Rec/Vertex/Primary']
-            if pvs and pvs.size() > 1:
-                nMoreThanOnePV += 1
-            for loc in p2pvmap.keys() :
-                addEntry(nCandidates, loc, evtSvc[loc].size()) 
-    length = len(sorted(p2pvmap.keys(), cmp = lambda x,y : cmp(len(y), len(x)))[0])+2
-    print '----------------------------------------------------------------------------------'
-    print "Analysed ", nEvents, " events"
-#    print "Found", nCandidates, "candidates in", particleLocation
-    print "Found", nMoreThanOnePV, "events with more than one PV in events with candidates"
-    for loc in p2pvmap.keys() :
-        message = loc.ljust(length) + str('Particle->PV relations/Candidates: '+ str(p2pvmap[loc])+'/'+ str(nCandidates[loc])).rjust(10)
-        print message
-    print '----------------------------------------------------------------------------------'
-
- 
+        leaves = evtSvc.nodes(node = location, forceload = True)
+        leaves = filter(lambda s : s.endswith('/Particles'), leaves)
+        for leaf in leaves :
+            particles = evtSvc[leaf]
+            if particles :
+                p2pvSummaries[leaf] = P2PVSummaryInfo(particles.size())
+                p2pvloc = leaf.replace('/Particles',
+                                       '/Particle2VertexRelations')
+                p2pv = evtSvc[p2pvloc]
+                else :
+                    for p in particles :
+                        rng = p2pv.relations(p)
+                        if not rng.empty():
+                            for rel in rng :
+                                pv = rel.to()
+                                pvLoc = pv.parent().registry().identifier()
+                                p2pvSummaries[leaf].addEntry(pvLoc, 1)
+    print '==================================================================='
+    print 'Analysed', nEvents, 'in location', location
+    for particleloc, value in p2pvSummaries.iteritems() :
+        if value.p2pvmap :
+            print '-----------------------------------------------------------------'
+            print particleloc, ':', value.entries, 'particles'
+            for loc, entries in value.p2pvmap.iteritems():
+                print '\t\t', loc, ':', entries , 'related PVs'
+    print '-----------------------------------------------------------------'
