20b27794026cd36804f302721a36457fa0cb73c8
akozlins <akozlins@cern.ch>
2010-06-17 10:29:02 +0000

    Loosened cuts on D0 vertex chi2 and life time fit chi2 and D* vertex chi2 in line StrippingDstarPromptWithD02HHNoP
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84461 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d81c305bd5ec691f260ec7521a886cc982752dfd

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHNoPt.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHNoPt.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Alexandr Kozlinskiy'
 __date__ = '22/05/2010'
-__version__ = '$Revision: 1.2 $'
+__version__ = '$Revision: 1.3 $'
 
 from PhysSelPython.Wrappers import DataOnDemand
 pions = DataOnDemand(Location = 'Phys/StdNoPIDsPions')
@@ -15,7 +15,7 @@ D0Combine.DaughtersCuts = {
   "K+" : "BPVIPCHI2() > 6.25"
 }
 D0Combine.CombinationCut = "(ADAMASS('D0') < 100 * MeV)"
-D0Combine.MotherCut = "(VFASPF(VCHI2/VDOF) < 9) & (BPVLTFITCHI2() < 9) & (BPVLTIME() > 0.3 * ps)"
+D0Combine.MotherCut = "(VFASPF(VCHI2/VDOF) < 16) & (BPVLTFITCHI2() < 25) & (BPVLTIME() > 0.3 * ps)"
 
 DstarCombine = CombineParticles( "Dstar2D0PiWithD02PiPiKPiKK" )
 DstarCombine.DecayDescriptor = '[D*(2010)+ -> D0 pi+]cc'
@@ -23,7 +23,7 @@ DstarCombine.DaughtersCuts = {
   'pi+' : "BPVIPCHI2() < 100"
 }
 DstarCombine.CombinationCut = "(ADAMASS('D*(2010)+') < 500 * MeV)"
-DstarCombine.MotherCut = "(VFASPF(VCHI2/VDOF) < 16) & ((M - MAXTREE('D0' == ABSID, M)) < 160 * MeV)"
+DstarCombine.MotherCut = "(VFASPF(VCHI2/VDOF) < 25) & ((M - MAXTREE('D0' == ABSID, M)) < 160 * MeV)"
 
 from PhysSelPython.Wrappers import Selection
 D0Selection = Selection("SelectionD02PiPiKPiKK", Algorithm = D0Combine, RequiredSelections = [ pions, kaons ])
