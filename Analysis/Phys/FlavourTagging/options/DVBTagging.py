463d1653f56910a1071c6e395ddc02155a948fcf
Marc Grabalosa Gandara <marc.grabalosa.gandara@cern.ch>
2010-06-17 18:05:51 +0000

    tag Opts MC2010
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84466 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e47a4f9991d9919a6f58097f80159631c7921219

--- Analysis/Phys/FlavourTagging/options/DVBTagging.py
+++ Analysis/Phys/FlavourTagging/options/DVBTagging.py
@@ -2,52 +2,93 @@
 #
 # Example Options for BTagging algorithm
 #
-# @author Marco Musy
-# @date 2009-02-19
+# @author Marc Grabalosa
+# @date 2010-02-20
 #
 ########################################################################
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer, DaVinci
 
+######################################################################\
+##
+# CheatedSelection.
+
+from Configurables import CheatedSelection, PhysDesktop
+
+cheatsel = CheatedSelection("CheatedSelection")
+cheatsel.InputLocations = [ "Phys/TaggingPions" ]
+cheatsel.AssociatorInputData = [ "Phys/CheatedSelection/Particles" ]
+cheatsel.OutputLevel = 4
+
+
 ########################################################################
 #
 # Flavour tagging. 
 #
-from Configurables import BTagging, BTaggingChecker
+from Configurables import BTagging, BTaggingTool, BTaggingChecker, TriggerTisTos, TaggingUtils, MCMatchObjP2MCRelator, BackgroundCategory
 
-location = "/Event/Strip/Phys/StripBu2eeK"
+location = "Phys/CheatedSelection"
 
 tag = BTagging("BTagging")
 tag.InputLocations = [ location ]
-tag.OutputLevel = 2
 
+tag.OutputLevel    = 3
+tag.addTool( PhysDesktop )
+tag.PhysDesktop.OutputLevel = 4
+tag.addTool( TriggerTisTos )
+tag.TriggerTisTos.OutputLevel = 4
+tag.addTool( TaggingUtils )
+tag.TaggingUtils.OutputLevel = 4
+tag.addTool( MCMatchObjP2MCRelator )
+tag.MCMatchObjP2MCRelator.OutputLevel = 4
+tag.addTool( BackgroundCategory )
+tag.BackgroundCategory.OutputLevel = 4
+
+tag.addTool( BTaggingTool )
+tag.BTaggingTool.ChoosePVCriterium = "PVbyIP"#needed by CheatedSel
+tag.BTaggingTool.CombineTaggersName = "CombineTaggersNN"#combine taggers with NN
+tag.BTaggingTool.OutputLevel    = 2
+#tag.BTaggingTool.ChoosePVCriterium = "RefitPV"
+#tag.BTaggingTool.UseReFitPV = True
+
+
+######################################################################\
+##
 # Flavour tagging Checker:
+
 tagcheck = BTaggingChecker("BTaggingChecker")
 tagcheck.InputLocations = [ location ]
 tagcheck.TagsLocation = location+"/FlavourTags"
+tagcheck.OutputLevel = 4
 
 
-MessageSvc().Format = "% F%30W%S%7W%R%T %0W%M"
 ########################################################################
 #
 # Standard configuration
 #
+MessageSvc().Format = "% F%30W%S%7W%R%T %0W%M"
+
 DaVinci().EvtMax     = 200                         # Number of events
 DaVinci().SkipEvents = 0                           # Events to skip
 DaVinci().PrintFreq  = 1
-DaVinci().DataType   = "MC09" 
+DaVinci().DataType   = "2010" 
 DaVinci().Simulation = True
+
+DaVinci().DDDBtag    = "head-20100119"
+DaVinci().CondDBtag  = "sim-20100222-vc-md100"
 
 ########################################################################
 
-DaVinci().MoniSequence = [  tag,
+DaVinci().MoniSequence = [  cheatsel,
+                            tag,
                             tagcheck
                          ]  # The algorithms
 
 ########################################################################
 #/releases/DAVINCI/DAVINCI_v24r7/DaVinciSys/tests/options/DVTestTagging.py
 #
-# example data file
+# example data file (Bs2DsPi)
 #
-DaVinci().Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/user/p/pkoppenb/MC09-Bu2eeK/Sel.Belectron-440-0.dst' TYP='POOL_ROOTTREE' OPT='READ'"  ]
+
+EventSelector().Input = [ "DATAFILE='castor://castorlhcb.cern.ch:9002//castor/cern.ch/grid/lhcb/MC/2010/DST/00005967/0000/00005967_00000001_1.dst?svcClass=lhcbdata&castorVersion=2' TYP='POOL_ROOTTREE' OPT='READ'" ]
 
