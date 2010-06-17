3bf392af8263c10b963a07f1e748e2213274ca53
jpalac <jpalac@cern.ch>
2010-06-17 12:47:54 +0000

    - python/PyAna/pyhistogram/histogram.py   . Add Bin.sumWeight2 attribute. Holds sum of squares of bin weights.  - python/PyAna/pyhistogram/histostats.py   . Add poissonSigma function acting on bins.     Returns sqrt( sum of squares of bin weights)  - python/PyAna/pyhistoplots.py   . Add errorFunction to histo_plot. For function acting on bins and     returning bin error.   . ntuple_column_histo makes Histogram then plot, to access stats and errors.  - cmt/requirements   . Increase to v1r2.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@48773 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 349d1af7f091110f622a4a323c59f0724c52db43

--- Analysis/Analysis/PyAna/tests/test_bin.py
+++ Analysis/Analysis/PyAna/tests/test_bin.py
@@ -14,19 +14,22 @@ def test_bin_contents() :
     bin = Bin(entries = 100, height = 75.)
     assert bin.entries == 100
     assert bin.height == 75.
-
+    assert bin.sumWeight2 == 75.*75.
+    
 def test_bin_addition() :
     bin0 = Bin(entries = 50, height = 75.)
     bin1 = Bin(entries = 50, height = 75.)
     bin2 = bin0+bin1
     assert bin2.entries == 100 and bin2.height == 150.
-
+    assert bin2.sumWeight2 == bin0.sumWeight2 + bin1.sumWeight2 == 2*pow(75.,2)
+    
 def test_bin_subtraction() :
     bin0 = Bin(entries = 100, height = 200.)
     bin1 = Bin(entries = 50, height = 75.)
     bin2 = bin0-bin1
     assert bin2.entries == 50 and bin2.height == 125.
-
+    assert bin2.sumWeight2 == bin0.sumWeight2 + bin1.sumWeight2
+    
 def test_bin_division() :
     bin0 = Bin(entries = 100, height = 200.)
     bin1 = Bin(entries = 2, height = 100.)
