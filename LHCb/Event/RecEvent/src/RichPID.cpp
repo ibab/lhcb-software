5979bf046ec9636ece95e23d6e00f6d18a456d0a
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-06-20 16:01:40 +0000

    Fix threshold setting methods in RichPID
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@51879 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7da04824e7966b3e94d03fa176dd4d7ee97b4b7e

--- LHCb/Event/RecEvent/src/RichPID.cpp
+++ LHCb/Event/RecEvent/src/RichPID.cpp
@@ -4,9 +4,6 @@
  *
  *  Implementation file for class : RichPID
  *
- *  CVS Log :-
- *  $Id: RichPID.cpp,v 1.6 2010-03-09 18:39:17 jonrob Exp $
- *
  *  @author  Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date    2002-06-10
  */
@@ -33,43 +30,32 @@ std::string LHCb::RichPID::pidType() const
   return hist;
 }
 
-bool LHCb::RichPID::isAboveThreshold(const Rich::ParticleIDType type) const
+bool LHCb::RichPID::isAboveThreshold( const Rich::ParticleIDType type ) const
 {
-  switch ( type )
-  {
-  case Rich::Pion           :  return this->pionHypoAboveThres();
-  case Rich::Electron       :  return this->electronHypoAboveThres();
-  case Rich::Muon           :  return this->muonHypoAboveThres();
-  case Rich::Kaon           :  return this->kaonHypoAboveThres();
-  case Rich::Proton         :  return this->protonHypoAboveThres();
-  case Rich::BelowThreshold :  return false;
-  default                   :  return false;
-  }
+  return ( Rich::Electron == type ? this->electronHypoAboveThres() :
+           Rich::Muon     == type ? this->muonHypoAboveThres()     :
+           Rich::Pion     == type ? this->pionHypoAboveThres()     :
+           Rich::Kaon     == type ? this->kaonHypoAboveThres()     :
+           Rich::Proton   == type ? this->protonHypoAboveThres()   :
+           false );
 }
 
-void LHCb::RichPID::setAboveThreshold(const Rich::ParticleIDType type,
-                                      const bool flag)
+void LHCb::RichPID::setAboveThreshold( const Rich::ParticleIDType type,
+                                       const bool flag )
 {
-  switch ( type )
-  {
-  case Rich::Pion        :  this->setPionHypoAboveThres(flag);
-  case Rich::Electron    :  this->setElectronHypoAboveThres(flag);
-  case Rich::Muon        :  this->setMuonHypoAboveThres(flag);
-  case Rich::Kaon        :  this->setKaonHypoAboveThres(flag);
-  case Rich::Proton      :  this->setProtonHypoAboveThres(flag);
-  default                :  return; // Covers Rich::BelowThreshold
-  }
+  if      ( Rich::Electron == type ) { this->setElectronHypoAboveThres(flag); }
+  else if ( Rich::Muon     == type ) { this->setMuonHypoAboveThres(flag);     }
+  else if ( Rich::Pion     == type ) { this->setPionHypoAboveThres(flag);     }
+  else if ( Rich::Kaon     == type ) { this->setKaonHypoAboveThres(flag);     }
+  else if ( Rich::Proton   == type ) { this->setProtonHypoAboveThres(flag);   }
 }
 
 bool LHCb::RichPID::traversedRadiator(const Rich::RadiatorType radiator) const
 {
-  switch ( radiator )
-  {
-  case Rich::Aerogel  : return this->usedAerogel();
-  case Rich::Rich1Gas : return this->usedRich1Gas();
-  case Rich::Rich2Gas : return this->usedRich2Gas();
-  default             : return false;
-  }
+  return ( Rich::Aerogel  == radiator ? this->usedAerogel()  :
+           Rich::Rich1Gas == radiator ? this->usedRich1Gas() :
+           Rich::Rich2Gas == radiator ? this->usedRich2Gas() :
+           false );
 }
 
 std::ostream& LHCb::RichPID::fillStream( std::ostream& s ) const
