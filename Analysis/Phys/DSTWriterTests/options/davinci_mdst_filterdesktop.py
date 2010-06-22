f85b5e17e6347ef3ecf4e98cb6fa733e56e8d6d3
jpalac <jpalac@cern.ch>
2010-06-22 13:31:44 +0000

    Test new PhysSelPython functionality.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@52056 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ea6556e5772f1828136b47e67e46ce35436e67c

---
+++ Analysis/Phys/DSTWriterTests/options/davinci_mdst_filterdesktop.py
@@ -0,0 +1,39 @@
+'''
+Create a bunch of FilterDesktop Selections and test that they get written to a MicroDST. This requires
+that the MicroDSTWriter sets their CloneFilteredParticles properties to True.
+'''
+
+from Gaudi.Configuration import *
+from Configurables import DaVinci, MicroDSTWriter
+
+from PhysSelPython.Wrappers import AutomaticData, SelectionSequence, Selection
+from Configurables import FilterDesktop
+
+sels = [AutomaticData(Location = 'Phys/StdTightPions'),
+        AutomaticData(Location = 'Phys/StdTightMuons'),
+        AutomaticData(Location = 'Phys/StdTightKaons'),
+        AutomaticData(Location = 'Phys/StdLooseJpsi2MuMu') ]
+
+filterSeqs = []
+for sel in sels :
+    filter = FilterDesktop('_'+sel.name(), Code = 'ALL')
+    selection  = Selection('Sel'+sel.name(), 
+                           Algorithm = filter, 
+                           RequiredSelections = [sel])
+    filterSeqs.append(SelectionSequence('Seq'+selection.name(), TopSelection = selection))
+
+conf = MicroDSTWriter("MicroDST0", 
+                      OutputPrefix = 'SequenceName',
+                      OutputFileSuffix = "TestFilterDesktop",
+                      SelectionSequences = filterSeqs,
+                      CopyBTags = False)
+
+selDST0Seq = conf.sequence()
+
+dv = DaVinci()
+dv.DataType = '2010'
+dv.InputType = 'SDST'
+dv.EvtMax = -1
+dv.UserAlgorithms = [selDST0Seq]
+EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/c/cattanem/testFiles/Brunel-v37r1-069857_0000000006-1000ev.sdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+FileCatalog().Catalogs =['xmlcatalog_file:TestSDSTCatalog.xml']
---
+++ Analysis/Phys/DSTWriterTests/options/davinci_mdst_mergedselection.py
@@ -0,0 +1,38 @@
+from Gaudi.Configuration import *
+from Configurables import DaVinci, MicroDSTWriter
+
+from PhysSelPython.Wrappers import AutomaticData, SelectionSequence, MergedSelection, MultiSelectionSequence
+from Configurables import TESCheck
+
+pions = AutomaticData(Location = 'Phys/StdTightPions')
+muons = AutomaticData(Location = 'Phys/StdTightMuons')
+kaons = AutomaticData(Location = 'Phys/StdTightKaons')
+jpsis = AutomaticData(Location = 'Phys/StdLooseJpsi2MuMu')
+
+allSelection = MergedSelection('MergeAll', RequiredSelections = [pions, kaons, muons, jpsis] )
+
+pionSeq = SelectionSequence('SeqPions', pions )
+muonSeq = SelectionSequence('SeqMuons', muons )
+
+kaonSeq =  SelectionSequence('SeqKaons', kaons)
+jpsiSeq =  SelectionSequence('SeqJpsi', jpsis )
+
+mergeSeq = SelectionSequence('SeqMerge', TopSelection = allSelection )
+
+multiSeq = MultiSelectionSequence('CocktailB', Sequences = [kaonSeq, pionSeq, muonSeq, jpsiSeq, mergeSeq])
+
+conf = MicroDSTWriter("MicroDST0", 
+                      OutputPrefix = 'SequenceName',
+                      OutputFileSuffix = "Test",
+                      SelectionSequences = [multiSeq],
+                      CopyFlavourTags = False)
+
+selDST0Seq = conf.sequence()
+
+dv = DaVinci()
+dv.DataType = '2010'
+dv.InputType = 'SDST'
+dv.EvtMax = -1
+dv.UserAlgorithms = [selDST0Seq]
+EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/c/cattanem/testFiles/Brunel-v37r1-069857_0000000006-1000ev.sdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+FileCatalog().Catalogs =['xmlcatalog_file:TestSDSTCatalog.xml']
