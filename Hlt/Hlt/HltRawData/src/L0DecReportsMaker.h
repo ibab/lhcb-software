5090a1e31da08a2393358833d6669915fcc71757
Tomasz Skwarnicki <tskwarni@syr.edu>
2010-06-23 22:50:27 +0000

    add L0SelReportsMaker,L0DecReportsMaker
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@84538 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 408d54b4d6551e9eb234d76383d0f73119f98e9a

---
+++ Hlt/Hlt/HltRawData/src/L0DecReportsMaker.h
@@ -0,0 +1,57 @@
+// $Id: L0DecReportsMaker.h,v 1.1 2010-06-23 22:50:27 tskwarni Exp $
+#ifndef L0DECREPORTSMAKER_H 
+#define L0DECREPORTSMAKER_H 1
+
+// Include files
+#include <string>
+// from Gaudi
+#include "GaudiKernel/VectorMap.h"
+#include "Event/HltObjectSummary.h"
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+
+namespace LHCb {
+  class L0MuonCandidate;  
+  class L0CaloCandidate;  
+};
+
+
+/** @class L0DecReportsMaker L0DecReportsMaker.h
+ *  
+ *  @author Tomasz Skwarnicki
+ *  @date   2010-06-23
+ *
+ *  Algorithm to translate L0DO report into HltDecReport
+ *
+ */
+class L0DecReportsMaker : public GaudiAlgorithm {
+
+public:
+
+  
+
+  /// Standard constructor
+  L0DecReportsMaker( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~L0DecReportsMaker( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+
+
+private:
+
+  
+  // ----------------------- data members 
+
+  /// location of input L0 DU Report
+  StringProperty m_inputL0DUReportLocation;
+
+  /// location of output Hlt Summary
+  StringProperty m_outputHltDecReportsLocation;
+
+
+
+};
+
+#endif // L0SELREPORTSMAKER_H
