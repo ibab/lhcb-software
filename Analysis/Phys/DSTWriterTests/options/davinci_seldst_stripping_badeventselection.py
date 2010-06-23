be2f43de4253f2b950b6bcc1dc592d00795b078c
jpalac <jpalac@cern.ch>
2010-06-23 16:59:15 +0000

    Simplify.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@52114 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 724042e513074a9e8d8e09fca6ac6a8e1347df8d

--- Analysis/Phys/DSTWriterTests/options/davinci_seldst_stripping_badeventselection.py
+++ Analysis/Phys/DSTWriterTests/options/davinci_seldst_stripping_badeventselection.py
@@ -1,32 +1,30 @@
 from Gaudi.Configuration import *
-from Configurables import DaVinci, SelDSTWriter, DeterministicPrescaler
+from Configurables import DaVinci, SelDSTWriter, DeterministicPrescaler, FilterDesktop
 from StrippingConf.Configuration import StrippingConf, StrippingStream, StrippingLine
-from StrippingSelections.StrippingBd2KstarMuMu import StrippingBd2KstarMuMuConf
-from PhysSelPython.Wrappers import AutomaticData
+from PhysSelPython.Wrappers import AutomaticData, SelectionSequence, Selection, MergedSelection
 
 pions = AutomaticData(Location = 'Phys/StdLoosePions')
-
 eventFilter = DeterministicPrescaler('BadEventFilter', AcceptFraction = 0.5)
-line = StrippingLine('PionLine', algos = [pions])
-line1 = StrippingLine('PionLine2', algos = [pions])
 
-streamDefault = StrippingStream("PionDefault", Lines = [line])
-streamDefault1 = StrippingStream("PionDefault1", Lines = [line1])
-streamBadEvent = StrippingStream("PionBadEvent", Lines = [line], BadEventSelection = eventFilter)
+lineX = StrippingLine('PionLineX', algos = [pions])
+lineY = StrippingLine('PionLineY', algos = [pions])
 
+streamX = StrippingStream("StreamPionX", Lines = [lineX])
+streamXBadEvent = StrippingStream("StreamPionXBadEvent", Lines = [lineX], BadEventSelection = eventFilter)
+streamYBadEvent = StrippingStream("StreamPionYBadEvent", Lines = [lineY], BadEventSelection = eventFilter)
 
-sc = StrippingConf( Streams = [streamDefault, streamDefault1, streamBadEvent] ) 
+sc = StrippingConf( Streams = [streamX, streamYBadEvent, streamXBadEvent] ) 
 
 dstWriter = SelDSTWriter("SelDST0", 
                          OutputPrefix = 'SequenceName',
-                         OutputFileSuffix = "Test2",
+                         OutputFileSuffix = "Test3",
                          SelectionSequences = sc.activeStreams())
 
 dv = DaVinci()
 dv.DataType = '2010'
 dv.InputType = 'SDST'
 dv.EvtMax = -1
-dv.appendToMainSequence( [ sc.sequence() ] )
+#dv.appendToMainSequence( [ sc.sequence() ] )
 dv.appendToMainSequence( [ dstWriter.sequence() ] )
 EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/c/cattanem/testFiles/Brunel-v37r1-069857_0000000006-1000ev.sdst' TYP='POOL_ROOTTREE' OPT='READ'"]
 FileCatalog().Catalogs =['xmlcatalog_file:TestSDSTCatalog.xml']
