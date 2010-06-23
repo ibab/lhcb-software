e5eee18f810728574360bca0789abc45a6dcbe8a
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-06-23 08:16:14 +0000

    v1r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@62115 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b7e919756cf7454118b11f2aec75744a024794d9

--- Boole/Digi/BooleUMC/src/BooleUMC_dll.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: BooleUMC_dll.cpp,v 1.1.1.1 2009-09-19 23:12:04 tskwarni Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-//-- Just touch this file (delete this line) so that it will be saved
-
-LOAD_FACTORY_ENTRIES(BooleUMC)
-
