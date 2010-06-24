baa5de82037d5dd42ef7f08f6b65cf1fe4ddc534
Anton Poluektov <anton.poluektov@cern.ch>
2010-06-24 11:57:00 +0000

    Added ProcStatusCheck
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@52136 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e7e25cf0834bf72d62396829be3ce6214d14dff1

---
+++ Analysis/Phys/StrippingAlgs/src/ProcStatusCheck.cpp
@@ -0,0 +1,91 @@
+#include "GaudiKernel/AlgFactory.h" 
+#include "ProcStatusCheck.h"
+
+#include <Event/ProcStatus.h>
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : ProcStatusCheck
+// 
+// A simple algorithm that checks for the events aborted by reconstruction 
+// algorithms. 
+// 
+// Returns True if at least one record is present in ProcStatus structure. 
+// 
+// If OutputLevel == DEBUG, prints out the names and status of the failed 
+// algorithms 
+//
+// 2010-06-24 : Anton Poluektov
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( ProcStatusCheck );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+ProcStatusCheck::ProcStatusCheck( const std::string& name,
+                                  ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator )
+{
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+ProcStatusCheck::~ProcStatusCheck() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode ProcStatusCheck::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode ProcStatusCheck::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  bool selected = false;
+  
+  LHCb::ProcStatus* procStat = get<LHCb::ProcStatus*>( LHCb::ProcStatusLocation::Default ); 
+  if (procStat) {
+    int procStatSize = procStat->algs().size();
+    if ( msgLevel(MSG::DEBUG) ) debug() << " ProcStat size = " << procStatSize << endmsg;
+    if( 0 < procStatSize ) {
+      // event failed some alg in Brunel
+      
+      if ( msgLevel(MSG::DEBUG) ) { 
+        for (int i = 0; i < procStatSize; i++) {
+          debug() << "   Algorithm " << i+1 << " name " << 
+                     procStat->algs()[i].first << ", status = " << 
+                     procStat->algs()[i].second << endmsg;
+        }
+      }
+
+      selected = true;
+    }
+  }
+      
+  setFilterPassed( selected );
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode ProcStatusCheck::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Analysis/Phys/StrippingAlgs/src/ProcStatusCheck.h
@@ -0,0 +1,37 @@
+#ifndef PROCSTATUSCHECK_H 
+#define PROCSTATUSCHECK_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+/** @class ProcStatusCheck ProcStatusCheck.h
+ *  
+ * A simple algorithm that checks for the events aborted by reconstruction 
+ * algorithms. 
+ * 
+ * Returns True if at least one record is present in ProcStatus structure. 
+ * 
+ * If OutputLevel == DEBUG, prints out the names and status of the failed 
+ * algorithms 
+ *
+ *  @author Anton Poluektov
+ *  @date   2010-06-24
+ */
+class ProcStatusCheck : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  ProcStatusCheck( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~ProcStatusCheck( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+
+};
+#endif // PROCSTATUSCHECK_H
