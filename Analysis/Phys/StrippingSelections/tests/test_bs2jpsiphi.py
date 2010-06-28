59fd05d0828cf617d703783b78eeaa5ac71bcc89
jpalac <jpalac@cern.ch>
2010-06-28 20:06:22 +0000

    Print out line name and locations. Build stream.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84610 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 67412938be56b1e888fa36263cfe59c6597d6182

--- Analysis/Phys/StrippingSelections/tests/test_bs2jpsiphi.py
+++ Analysis/Phys/StrippingSelections/tests/test_bs2jpsiphi.py
@@ -3,21 +3,24 @@ from StrippingConf.StrippingStream import StrippingStream
 from StrippingConf.Configuration import StrippingConf
 from StrippingSelections import StrippingBs2JpsiPhi_dev as stripping
 
-config_params =  { "MuonTRCHI2Loose"	: 10.0
-                   ,	"JpsiMassWinLoose"	: 80.0
-                   ,	"JpsiVCHI2Loose"	: 20.0
-                   ,	"KaonTRCHI2Loose"	: 10.0
-                   ,	"KaonPIDK"	: -5.0
-                   ,	"PhiMassWin"	: 15.0
-                   ,	"PhiMassWinLoose"	: 30.0
-                   ,	"PhiPT" 	: 1000.0
-                   ,	"PhiPTLoose" 	: 500.0
-                   ,	"PhiVCHI2" 	: 20.0
-                   ,	"PhiVCHI2Loose" 	: 20.0
-                   ,	"BsMassWin"	: 300.0
-                   ,	"BsMassWinLoose"	: 300.0
-                   ,	"BsVCHI2" 	: 5.0
-                   ,	"BsVCHI2Loose" 	: 20.0
+config_params =  { "MuonTRCHI2Loose"	: 10.0,
+                   "JpsiMassWinLoose"	: 80.0,
+                   "JpsiVCHI2Loose"	: 20.0,
+                   "KaonTRCHI2Loose"	: 10.0,
+                   "KaonPIDK"	        : -5.0,
+                   "PhiMassWin"	        : 15.0,
+                   "PhiMassWinLoose"	: 30.0,
+                   "PhiPT" 	        : 1000.0,
+                   "PhiPTLoose" 	: 500.0,
+                   "PhiVCHI2" 	        : 20.0,
+                   "PhiVCHI2Loose" 	: 20.0,
+                   "BsMassWin"	        : 300.0,
+                   "BsMassWinLoose"	: 300.0,
+                   "BsVCHI2" 	        : 5.0,
+                   "BsVCHI2Loose" 	: 20.0
                    }
 
-bs2jpsiPhi = stripping.StrippingBs2JpsiPhiConf(config_params)
+bs2jpsiphi = stripping.StrippingBs2JpsiPhiConf(config_params)
+bs2JpsiPhiStream = StrippingStream('StreamBs2JpsiPhi', Lines = bs2jpsiphi.lines())
+for line in bs2JpsiPhiStream.lines :
+    print 'Line', line.name(), 'Location', line.outputLocation()
