c6017cd4a9aae77414c958d826dc82bf0f6767ff
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-06-28 06:54:02 +0000

    Add a new method to ProcStat
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@52268 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6b8b343d83f5d2b07a2b498e84084c1326ddfc29

---
+++ LHCb/Event/RecEvent/src/ProcStatus.cpp
@@ -0,0 +1,67 @@
+
+// local
+#include "Event/ProcStatus.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : ProcStatus
+//
+// 2010-06-25 : Chris Jones
+//-----------------------------------------------------------------------------
+
+void LHCb::ProcStatus::addAlgorithmStatus(const std::string& name,
+                                          int status)
+{
+  bool found = false;
+  for ( LHCb::ProcStatus::AlgStatusVector::iterator iS = m_algs.begin();
+        iS != m_algs.end(); ++iS )
+  {
+    if ( iS->first == name )
+    {
+      iS->second = status;
+      found = true;
+      break;
+    }
+  }
+  if ( !found ) { m_algs.push_back( AlgStatus(name,status) ); }
+}
+
+void LHCb::ProcStatus::addAlgorithmStatus(const std::string& algName,
+                                          const std::string& subsystem,
+                                          const std::string& reason,
+                                          const int status,
+                                          const bool eventAborted)
+{
+  // Add abort or OK string
+  std::string message = ( eventAborted ? "ABORTED:" : "OK:" );
+
+  // Add sub-system
+  message += subsystem + ":";
+
+  // Add reason for the message
+  message += reason + ":";
+
+  // Finally, the algorithm name
+  message += algName;
+
+  // Set the full message in the algs list
+  addAlgorithmStatus(message,status);
+
+  // if event was aborted, set this as well.
+  // (Don't set if false, since this will overwrite pre-existing aborts
+  if (eventAborted) setAborted(true);
+}
+
+int LHCb::ProcStatus::algorithmStatus(const std::string& name)
+{
+  int status = 0;
+  for ( LHCb::ProcStatus::AlgStatusVector::const_iterator iS = m_algs.begin();
+        iS != m_algs.end(); ++iS )
+  {
+    if ( iS->first == name )
+    {
+      status = iS->second;
+      break;
+    }
+  }
+  return status;
+}
