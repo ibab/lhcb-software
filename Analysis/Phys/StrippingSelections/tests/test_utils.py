660f6a95dfcaeffda7e3a2c839cf577786b8c8cd
jpalac <jpalac@cern.ch>
2010-06-30 09:19:36 +0000

    Tests for StrippingSelections.Utils module.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84627 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3f3c35340de24f27cd7a073cc676ff5c9ac64b85

---
+++ Analysis/Phys/StrippingSelections/tests/test_utils.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from py.test import raises
+
+def test_checkConfig_raises_KeyError_for_missing_parameter() :
+    from StrippingSelections.Utils import checkConfig
+    
+    ref_params = ('A', 'B', 'C', 'D')
+
+    config = { 'A' :1, 'B' : 2, 'C' : 3}
+
+    raises(KeyError, checkConfig, ref_params, config)
+
+def test_checkConfig_raises_KeyError_for_too_many_parameters() :
+    from StrippingSelections.Utils import checkConfig
+    
+    ref_params = ('A', 'B', 'C', 'D')
+
+    config = { 'A' :1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5}
+
+    raises(KeyError, checkConfig, ref_params, config)
+
+if __name__ == '__main__' :
+
+    import sys
+
+    test_names = filter(lambda k : k.count('test_') > 0, locals().keys())
+
+    __tests = filter( lambda x : x[0] in test_names, locals().items())
+    
+
+    message = ''
+    summary = '\n'
+    length = len(sorted(test_names,
+                        cmp = lambda x,y : cmp(len(y),len(x)))[0]) +2
+    
+    for test in __tests :
+        try :
+            test[1]()
+            message = 'PASS'
+        except :
+            message = "FAIL"
+        summary += test[0].ljust(length) + ':' + message.rjust(10) + '\n'
+
+    if summary.count('FAIL') > 0 :
+        message = 'FAIL'
+        wr = sys.stderr.write
+    else :
+        message = 'PASS'
+        wr = sys.stdout.write
+
+    summary += 'Global'.ljust(length) + ':' + message.rjust(10) + '\n\n'
+    wr(summary)
+        
