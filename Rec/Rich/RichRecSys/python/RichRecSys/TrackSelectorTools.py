c854bf2a7b6a2a6f3a686048e8dbf912d59be52d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-07-09 18:38:43 +0000

    Add missing python file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@52513 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 814ca6c99c43b92f9f0d8055a4a42ff4295e6639

---
+++ Rec/Rich/RichRecSys/python/RichRecSys/TrackSelectorTools.py
@@ -0,0 +1,23 @@
+
+from Configurables import ( Rich__Rec__ForwardTrackSelector,
+                            Rich__Rec__MatchTrackSelector,
+                            Rich__Rec__VeloTTTrackSelector,
+                            Rich__Rec__SeedTrackSelector,
+                            Rich__Rec__KsTrackSelector,
+                            Rich__Rec__BaseTrackSelector )
+
+# Method to just set the cuts in a track selector object
+def configureTrackSelectorCuts(selector,cuts):
+    for type in cuts.keys() :
+        if type == "Forward" : selector.addTool( Rich__Rec__ForwardTrackSelector, type )
+        if type == "Match"   : selector.addTool( Rich__Rec__MatchTrackSelector, type )
+        if type == "VeloTT"  : selector.addTool( Rich__Rec__VeloTTTrackSelector, type )
+        if type == "Seed"    : selector.addTool( Rich__Rec__SeedTrackSelector, type )
+        if type == "KsTrack" : selector.addTool( Rich__Rec__KsTrackSelector, type )
+        if type == "Velo" or type == "Trigger" or type == "MCRichTrack" :
+            selector.addTool( BaseTrackSelector, type )
+        ts = getattr(selector,type)
+        for name,cut in cuts[type].iteritems() :
+            ts.setProp("Min"+name,cut[0])
+            ts.setProp("Max"+name,cut[1])
+
