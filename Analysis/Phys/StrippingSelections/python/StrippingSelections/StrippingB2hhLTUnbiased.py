d76cd4fbd6d93d27739e30216b705f5e13ddc3d6
ukerzel <ukerzel@cern.ch>
2010-07-13 13:51:17 +0000

    update cuts for B2hhLTUnbiased
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@84865 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8e7b6ffa1b68cde7e766ae81313f5a8fa32d3984

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2hhLTUnbiased.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2hhLTUnbiased.py
@@ -1,6 +1,6 @@
 __author__ = 'Ulrich Kerzel'
 __date__ = '03/06/2010'
-__version__ = '$Revision: 1.2 $'
+__version__ = '$Revision: 1.3 $'
 
 """
 B->hh selection, unbiased lifetime
@@ -11,29 +11,38 @@ from LHCbKernel.Configuration import *
 class StrippingB2hhLTUnbiasedConf(LHCbConfigurableUser):
     """
     Definition of lifetime unbiased B->hh  stripping
+    The Stripping is aimed at an unbiased measurement of the Bs->KK lifetme.
+    Two approaches are followed: An unbiased trigger for B->hh and a measurement of the
+    BsKK lifetime relative to the (known) BdPiK lifetime.
+    In order to efficiently select both the BsKK and BdPiK decay mode, only one kaon
+    is required.
+
+    The code ((PIDK < -0.1) | (PIDK > 0.1)) avoids the big peak at zero when pions and kaons
+    cannot be separated from each other.
     """
     __slots__ = { 
-        'TrackChi2'             :  10
-        , 'DaughterPtMin'         :   1.0 # GeV
-        , 'DaughterPtMax'         :   2.0 # GeV
-        , 'DaughterPtMinLoose'    :   1.0 # GeV
-        , 'DaughterPtMaxLoose'    :   1.1 # GeV
-        , 'DaughterPIDKMin'       :  -2.0
-        , 'DaughterPIDKMax'       :  -2.0
-        , 'DaughterPIDKMinLoose'  :  -2.0
-        , 'DaughterPIDKMaxLoose'  :  -2.0
-        , 'BMassMin'              :   5.0 # GeV
-        , 'BMassMax'              :   5.9 # GeV
-        , 'BMassMinLoose'         :   4.5 # GeV
-        , 'BMassMaxLoose'         :   6.0 # GeV
-        , 'DOCA'                  :   0.2
-        , 'DOCALoose'             :   0.5
-        , 'VertexChi2'            :  25.0
+        'TrackChi2'               :  10
+        , 'DaughterPtMin'         :    2.5 # GeV
+        , 'DaughterPtMax'         :    3.0 # GeV
+        , 'DaughterPMin'          :   10.0 # GeV  
+        , 'DaughterPtMinLoose'    :    1.5 # GeV
+        , 'DaughterPtMaxLoose'    :    2.0 # GeV
+        , 'DaughterPIDKMin'       : -999.0
+        , 'DaughterPIDKMax'       :    0.1
+        , 'DaughterPIDKMinLoose'  : -999.0
+        , 'DaughterPIDKMaxLoose'  :    0.1
+        , 'BMassMin'              :    5.0 # GeV
+        , 'BMassMax'              :    5.9 # GeV
+        , 'BMassMinLoose'         :    4.8 # GeV
+        , 'BMassMaxLoose'         :    6.0 # GeV
+        , 'DOCA'                  :    0.07
+        , 'DOCALoose'             :    0.15
+        , 'VertexChi2'            :   25.0
                    }
 
 
     #
-    # default line
+    # loose line
     #
     def StripB2hhLTUnbiasedLoose( self ) :
         from StrippingConf.StrippingLine import StrippingLine, StrippingMember
@@ -44,17 +53,17 @@ class StrippingB2hhLTUnbiasedConf(LHCbConfigurableUser):
         #StripB2hhLTUnbiasedLoose.ReFitPVs        = True
         #StripB2hhLTUnbiased.OutputLevel    =   1
         StripB2hhLTUnbiasedLoose.DecayDescriptor = "B_s0 -> K+ K-"
-        StripB2hhLTUnbiasedLoose.DaughtersCuts   = { "K+" : "ISLONG & (TRCHI2DOF < %(TrackChi2)s) & (PT > %(DaughterPtMinLoose)s *GeV) & (PIDK > %(DaughterPIDKMinLoose)s)"% self.getProps() }
+        StripB2hhLTUnbiasedLoose.DaughtersCuts   = { "K+" : "ISLONG & (TRCHI2DOF < %(TrackChi2)s) & (PT > %(DaughterPtMinLoose)s *GeV) & (P> %(DaughterPMin)s *GeV) & (PIDK > %(DaughterPIDKMinLoose)s)"% self.getProps() }
         StripB2hhLTUnbiasedLoose.CombinationCut  = "((AM > %(BMassMinLoose)s *GeV) & (AM < %(BMassMaxLoose)s *GeV) & (AMAXDOCA('LoKi::DistanceCalculator') < %(DOCALoose)s))" % self.getProps()
         StripB2hhLTUnbiasedLoose.MotherCut       = "(VFASPF(VCHI2/VDOF) < %(VertexChi2)s) & (MAXTREE(('K+'==ABSID) ,PT) > %(DaughterPtMaxLoose)s*GeV) & (MAXTREE(('K+'==ABSID) , PIDK) > %(DaughterPIDKMaxLoose)s )" % self.getProps()
         
 
         StripB2hhLTUnbiasedLoose.InputLocations = [ 'Phys/StdNoPIDsKaons' ]
-        return StrippingLine('B2hhLTUnbiasedLoose', prescale = 1, algos = [ StripB2hhLTUnbiasedLoose ] )
+        return StrippingLine('B2hhLTUnbiasedLoose', prescale = 100, algos = [ StripB2hhLTUnbiasedLoose ] )
 
 
     #
-    # loose version
+    # default version
     #
     def StripB2hhLTUnbiased( self ) :
         from StrippingConf.StrippingLine import StrippingLine, StrippingMember
@@ -65,7 +74,7 @@ class StrippingB2hhLTUnbiasedConf(LHCbConfigurableUser):
         #StripB2hhLTUnbiased.ReFitPVs        = True
         #StripB2hhLTUnbiased.OutputLevel    =   1
         StripB2hhLTUnbiased.DecayDescriptor = "B_s0 -> K+ K-"
-        StripB2hhLTUnbiased.DaughtersCuts   = { "K+" : "ISLONG & (TRCHI2DOF < %(TrackChi2)s) & (PT > %(DaughterPtMin)s *GeV) & (PIDK > %(DaughterPIDKMin)s)"% self.getProps() }
+        StripB2hhLTUnbiased.DaughtersCuts   = { "K+" : "ISLONG & (TRCHI2DOF < %(TrackChi2)s) & (PT > %(DaughterPtMin)s *GeV) & (P> %(DaughterPMin)s *GeV) & ((PIDK < -0.1) | (PIDK > 0.1)) & (PIDK > %(DaughterPIDKMin)s)"% self.getProps() }
         StripB2hhLTUnbiased.CombinationCut  = "((AM > %(BMassMin)s *GeV) & (AM < %(BMassMax)s *GeV) & (AMAXDOCA('LoKi::DistanceCalculator') < %(DOCA)s))" % self.getProps()
         StripB2hhLTUnbiased.MotherCut       = "(VFASPF(VCHI2/VDOF) < %(VertexChi2)s) & (MAXTREE(('K+'==ABSID) ,PT) > %(DaughterPtMax)s*GeV) & (MAXTREE(('K+'==ABSID) , PIDK) > %(DaughterPIDKMax)s )" % self.getProps()
         
@@ -93,8 +102,9 @@ class StrippingB2hhLTUnbiasedConf(LHCbConfigurableUser):
         StripB2hhLTUnbiasedTrigger.InputLocations = [ 'Phys/StdNoPIDsKaons' ]
         return StrippingLine('B2hhLTUnbiasedTrigger',
                              prescale = 1,
-                             HLT = "HLT_PASS_RE('Hlt1DiHadronLTUnbiasedDecision')" ,
-                             algos = [ StripB2hhLTUnbiasedTrigger ] )    
+                             HLT = "HLT_PASS('Hlt1DiHadronLTUnbiasedDecision')" ,
+                             algos = [ StripB2hhLTUnbiasedTrigger ] )
+          
 
     def getProps(self) :
         """
