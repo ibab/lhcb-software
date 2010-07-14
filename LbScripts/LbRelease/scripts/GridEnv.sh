60e364c03745bb38079bc9a1d482cccae5841dcd
Joel Closier <joel.closier@cern.ch>
2010-07-14 15:19:18 +0000

    Add GridEnv
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@52631 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 905deac018635f454b2b250b238e71cb9007330a

---
+++ LbScripts/LbRelease/scripts/GridEnv.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+rh=`echo "${CMTCONFIG}" | awk -F_ '{print $1 "_" $2}'`
+
+if [ $# -eq 1 ]; then
+     if [ $1='-v' ]; then
+         echo "Setting up environment for the grid on a $rh type machine"
+     fi
+fi
+# until further notice the script is valid on slc3 and slc4
+if [ x"$rh" != "x" ]; then
+  if [ x"$X509_USER_PROXY" != "x" ]; then
+     SAVE_PROXY=$X509_USER_PROXY
+  fi
+  if [ "$rh" = "slc4_ia32" ]; then
+      . /afs/cern.ch/project/gd/LCG-share/sl4/etc/profile.d/grid_env.sh
+  elif [ "$rh" = "slc4_amd64" ]; then
+      . /afs/cern.ch/project/gd/LCG-share/sl4/etc/profile.d/grid_env.sh
+  elif [ "$rh" = "x86_64-slc5-gcc43-opt" ]; then
+      . /afs/cern.ch/project/gd/LCG-share/sl5/etc/profile.d/grid_env.sh
+  else
+      . /afs/cern.ch/project/gd/LCG-share/sl5/etc/profile.d/grid-env.sh
+  fi
+  if [ x"$SAVE_PROXY" != "x" ]; then
+     X509_USER_PROXY=$SAVE_PROXY
+     unset SAVE_PROXY
+  fi
+fi
