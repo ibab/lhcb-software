5749813dab908b044d1ddbbb3177dd13ebf90540
hmdegaud <hmdegaud@cern.ch>
2010-07-15 14:11:03 +0000

    fixed indentation
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@52685 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 24bc76386fd9b5663ccb41c82547fa95d621c7ec

--- LbScripts/LbRelease/python/LbRelease/AfsProjectSpace.py
+++ LbScripts/LbRelease/python/LbRelease/AfsProjectSpace.py
@@ -24,12 +24,12 @@ def check_space(thelog):
         if nb_line < 6 :
             member = line.split()
             if member[0] in ('lhcb', 'lhcb_q','lhcb_s'):
-               free_space = str( int(member[1]) - int(member[2]) )
-               space_left = str( int(free_space)  * 100 / int(member[1]))
-               if int(space_left) < 10 :
-                   size_GB = int(member[1]) / 100000
-                   free_GB = int(free_space) / 100000
-                   thelog.info('The space left for %s represents %s percent \n (Total is %s GB and free is %s GB)' %( member[0], space_left, size_GB, free_GB))
+                free_space = str( int(member[1]) - int(member[2]) )
+                space_left = str( int(free_space)  * 100 / int(member[1]))
+                if int(space_left) < 10 :
+                    size_GB = int(member[1]) / 100000
+                    free_GB = int(free_space) / 100000
+                    thelog.info('The space left for %s represents %s percent \n (Total is %s GB and free is %s GB)' %( member[0], space_left, size_GB, free_GB))
         else:
             break
 
