6ec42501566cc8a8d5aa834bde82bfa5bea1085e
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-07-19 13:54:31 +0000

    change default LHCBCOND tag
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@52769 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 135e3a386fd534cdc5af30155323242e98a28d73

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -118,7 +118,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20100624")
-        self.__set_tag__(["LHCBCOND"], "head-20100624")
+        self.__set_tag__(["LHCBCOND"], "head-20100715")
     
     def __2009_conf__(self):
         """
@@ -126,7 +126,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20100624")
-        self.__set_tag__(["LHCBCOND"], "head-20100624")
+        self.__set_tag__(["LHCBCOND"], "head-20100715")
         self.__set_tag__(["SIMCOND"], "MC-20100412-vc15mm-md100")
     
     def __2008_conf__(self):
