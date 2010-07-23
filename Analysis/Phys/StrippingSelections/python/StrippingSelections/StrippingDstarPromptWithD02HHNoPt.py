3885a1941c30d0394ceeaaadeae40fb3d0d00a81
akozlins <akozlins@cern.ch>
2010-07-23 14:59:42 +0000

    removed selection sequence, tightened all cuts, added filter on number of primaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85036 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f1872ffb13cd534b15ef8b7569caa6a9090b7ffe

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHNoPt.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHNoPt.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 __author__ = 'Alexandr Kozlinskiy'
+__email__ = 'akozlins@cern.ch'
 __date__ = '22/05/2010'
-__version__ = '$Revision: 1.5 $'
+__version__ = '$Revision: 1.6 $'
 
 """
 
@@ -11,51 +12,55 @@ This stripping line selects prompt D* in decay modes
 
 Cuts:
   * Particles input: StdNoPIDsPions, StdNoPIDsKaons
-  * IP-chi2 of the D0 daughters > 9
-  * D0 mass window 100 MeV
-  * chi2 of the D0 vertex < 16
-  * chi2 of the D0 life time fit < 100 (loose cut for study non-propmpt D*)
-  * D0 life time > 0.2 ps
-  * M(D*) - M(D0) < 160 MeV
-  * chi2 of the D* vertex < 100
+  * Track-Chi2/NDOF of the D0 daughters < 10
+  * IP-Chi2 of the D0 daughters > 9
+  * D0 mass window 75 MeV
+  * Chi2 of the D0 vertex < 9
+  * Chi2 of the D0 life time fit < 36
+  * D0 life time > 0.3 ps
+  * Track-Chi2/NDOF of the soft pion from D* < 10
+  * M(D*) - M(D0) < 155 MeV
+  * Chi2 of the D* vertex < 100
 
 """
 
 from PhysSelPython.Wrappers import DataOnDemand
-pions = DataOnDemand(Location = 'Phys/StdNoPIDsPions')
-kaons = DataOnDemand(Location = 'Phys/StdNoPIDsKaons')
+pions = DataOnDemand(Location = "Phys/StdNoPIDsPions")
+kaons = DataOnDemand(Location = "Phys/StdNoPIDsKaons")
 
 # D0 combine
 from Configurables import CombineParticles
 D0Combine = CombineParticles( "CombD02HHForDstarPromptNoPt" )
-D0Combine.DecayDescriptors = [ "[D0 -> pi+ pi-]cc", "[D0 -> K- pi+]cc", "[D0 -> K+ pi-]cc", "[D0 -> K+ K-]cc" ]
+D0Combine.DecayDescriptors = [ "D0 -> pi+ pi-", "D0 -> K- pi+", "D0 -> K+ pi-", "D0 -> K+ K-" ]
 D0Combine.DaughtersCuts = {
-  "pi+" : "BPVIPCHI2() > 9",
-  "K+" : "BPVIPCHI2() > 9"
+  "pi+" : "(TRCHI2DOF < 10) & (BPVIPCHI2() > 9)",
+  "K+" : "(TRCHI2DOF < 10) & (BPVIPCHI2() > 9)"
 }
-D0Combine.CombinationCut = "(ADAMASS('D0') < 110 * MeV)"
-D0Combine.MotherCut = "(VFASPF(VCHI2) < 16) & (DMASS('D0') < 100 * MeV) & (BPVLTFITCHI2() < 100) & (BPVLTIME() > 0.2 * ps)"
+D0Combine.CombinationCut = "(ADAMASS('D0') < 80 * MeV)"
+D0Combine.MotherCut = "(VFASPF(VCHI2) < 9) & (DMASS('D0') < 75 * MeV) & (BPVLTFITCHI2() < 36) & (BPVLTIME() > 0.3 * ps)"
 
 # D* combine
 DstarCombine = CombineParticles( "CombDstarPromptWithD02HHNoPt" )
-DstarCombine.DecayDescriptor = '[D*(2010)+ -> D0 pi+]cc'
-DstarCombine.CombinationCut = "AM - AM1 < 170 * MeV"
-DstarCombine.MotherCut = "(VFASPF(VCHI2) < 100) & ((M - M1) < 160 * MeV)"
+DstarCombine.DecayDescriptors = [ "D*(2010)+ -> D0 pi+", "D*(2010)- -> D0 pi-" ]
+DstarCombine.DaughtersCuts = {
+  "pi+" : "(TRCHI2DOF < 10)"
+}
+DstarCombine.CombinationCut = "AM - AM1 < 160 * MeV"
+DstarCombine.MotherCut = "(VFASPF(VCHI2) < 100) & ((M - M1) < 155 * MeV)"
 
 # D0 and D* selections
 from PhysSelPython.Wrappers import Selection
 D0Selection = Selection("SelD02HHForDstarPromptNoPt", Algorithm = D0Combine, RequiredSelections = [ pions, kaons ])
 DstarSelection = Selection("SelDstarPromptWithD02HHNoPt", Algorithm = DstarCombine, RequiredSelections = [ pions, kaons, D0Selection ])
 
-# D* sequence
-from PhysSelPython.Wrappers import SelectionSequence
-DstarSequence = SelectionSequence("SeqDstarPromptWithD02HHNoPt", TopSelection = DstarSelection)
-
 # D* stripping line
 from StrippingConf.StrippingLine import StrippingLine
-lineDstarPromptWithD02HHNoPt = StrippingLine('DstarPromptWithD02HHNoPt', prescale = 1, algos = [ DstarSequence ] )
-
-
+from Configurables import LoKi__VoidFilter
+lineDstarPromptWithD02HHNoPt = StrippingLine("DstarPromptWithD02HHNoPt",
+                                             algos = [
+                                               LoKi__VoidFilter("NPVFilter", Code = "CONTAINS('Rec/Vertex/Primary') < 3.5"),
+                                               DstarSelection ],
+                                             prescale = 1)
 
 if '__main__' == __name__ :
 
