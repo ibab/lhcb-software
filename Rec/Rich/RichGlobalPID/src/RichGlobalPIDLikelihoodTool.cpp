63de236df579bbe0fcecb2783178c1b0d8ffd2bf
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-07-27 17:17:36 +0000

    Tidy up a little the previous commit.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@53095 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a515366d5a52c9a8df7828e6088bcc3a8f0c91b4

--- Rec/Rich/RichGlobalPID/src/RichGlobalPIDLikelihoodTool.cpp
+++ Rec/Rich/RichGlobalPID/src/RichGlobalPIDLikelihoodTool.cpp
@@ -20,6 +20,15 @@ using namespace Rich::Rec::GlobalPID;
 // Declaration of the Tool Factory
 DECLARE_TOOL_FACTORY( LikelihoodTool );
 
+// anon namespace for a few constants
+namespace
+{
+  const double limitA = 0.001;
+  const double limitB = 0.01;
+  const double limitC = 0.1;
+  const double limitD = 1.0;
+}
+
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
@@ -87,6 +96,12 @@ StatusCode LikelihoodTool::initialize()
   pixelCreator();
   photonCreator();
   statusCreator();
+
+  // Check if power series expansions are OK
+  if ( m_minSig < 0.999*limitA )
+  {
+    return Warning( "Power series interpolations for log(exp(x)-1) need retuning" );
+  }
 
   // Initialise parameters
   m_logMinSig = logExp(m_minSig);
@@ -199,7 +214,7 @@ void LikelihoodTool::pids( const LHCb::RichGlobalPIDTrack::Vector & tracks ) con
 
         if ( nRetries == m_maxItRetries && nChange > 0 )
         {
-          Warning( "Maximum number of iteration re-tries reached", 
+          Warning( "Maximum number of iteration re-tries reached",
                    StatusCode::SUCCESS ).ignore();
         }
 
@@ -306,72 +321,72 @@ unsigned int LikelihoodTool::initBestLogLikelihood() const
   // Loop over all tracks and find best hypothesis for each track
   {for ( LHCb::RichGlobalPIDTrack::Vector::const_iterator track = m_gpidTracksToPID->begin();
          track != m_gpidTracksToPID->end(); ++track )
-  {
-    LHCb::RichRecTrack * rRTrack = (*track)->richRecTrack();
-
-    // skip frozen tracks
-    if ( (*track)->frozenHypo() )
     {
-      if ( msgLevel(MSG::DEBUG) )
-      { debug() << "  -> Skipping globally frozen track " << (*track)->key() << endmsg; }
-      continue;
-    }
-    // Initialise starting values
-    double mindeltaLL = 99999.;
-    Rich::ParticleIDType minHypo = rRTrack->currentHypothesis();
+      LHCb::RichRecTrack * rRTrack = (*track)->richRecTrack();
 
-    // Loop over all particle codes
-    for ( Rich::Particles::const_iterator hypo = pidTypes().begin();
-          hypo != pidTypes().end(); ++hypo )
-    {
-
-      // Skip analysing starting hypothesis
-      if ( *hypo == rRTrack->currentHypothesis() ) continue;
-
-      // calculate delta logLikelihood for event with new track hypothesis
-      const double deltaLogL = deltaLogLikelihood( rRTrack, *hypo );
-
-      // Set the value for deltaLL for this hypothesis
-      (*track)->globalPID()->setParticleDeltaLL( *hypo, static_cast<float>(deltaLogL) );
-
-      // Set new minimum if lower logLikelihood is achieved
-      if ( deltaLogL < mindeltaLL )
+      // skip frozen tracks
+      if ( (*track)->frozenHypo() )
       {
-        if ( 0 != deltaLogL ) mindeltaLL = deltaLogL;
-        if ( deltaLogL < m_epsilon ) minHypo = *hypo;
+        if ( msgLevel(MSG::DEBUG) )
+        { debug() << "  -> Skipping globally frozen track " << (*track)->key() << endmsg; }
+        continue;
       }
+      // Initialise starting values
+      double mindeltaLL = 99999.;
+      Rich::ParticleIDType minHypo = rRTrack->currentHypothesis();
 
-      // In case the threshold is reached, skip other hypotheses
-      bool threshold = true;
-      for ( Rich::Particles::const_iterator hypo2 = hypo; hypo2 != pidTypes().end(); ++hypo2 )
+      // Loop over all particle codes
+      for ( Rich::Particles::const_iterator hypo = pidTypes().begin();
+            hypo != pidTypes().end(); ++hypo )
       {
-        if ( m_tkSignal->nDetectablePhotons( rRTrack, *hypo2 ) > 0 )
+
+        // Skip analysing starting hypothesis
+        if ( *hypo == rRTrack->currentHypothesis() ) continue;
+
+        // calculate delta logLikelihood for event with new track hypothesis
+        const double deltaLogL = deltaLogLikelihood( rRTrack, *hypo );
+
+        // Set the value for deltaLL for this hypothesis
+        (*track)->globalPID()->setParticleDeltaLL( *hypo, static_cast<float>(deltaLogL) );
+
+        // Set new minimum if lower logLikelihood is achieved
+        if ( deltaLogL < mindeltaLL )
         {
-          threshold = false; break;
+          if ( 0 != deltaLogL ) mindeltaLL = deltaLogL;
+          if ( deltaLogL < m_epsilon ) minHypo = *hypo;
         }
-      }
-      if ( threshold )
-      {
-        for ( Rich::Particles::const_iterator hypo3 = hypo;
-              hypo3 != pidTypes().end(); ++hypo3 )
-        {
-          (*track)->globalPID()->setParticleDeltaLL( *hypo3, static_cast<float>(deltaLogL) );
-        }
-        break;
-      }
 
-    } // end particle for loop
+        // In case the threshold is reached, skip other hypotheses
+        bool threshold = true;
+        for ( Rich::Particles::const_iterator hypo2 = hypo; hypo2 != pidTypes().end(); ++hypo2 )
+        {
+          if ( m_tkSignal->nDetectablePhotons( rRTrack, *hypo2 ) > 0 )
+          {
+            threshold = false; break;
+          }
+        }
+        if ( threshold )
+        {
+          for ( Rich::Particles::const_iterator hypo3 = hypo;
+                hypo3 != pidTypes().end(); ++hypo3 )
+          {
+            (*track)->globalPID()->setParticleDeltaLL( *hypo3, static_cast<float>(deltaLogL) );
+          }
+          break;
+        }
+
+      } // end particle for loop
 
       // Save info on tracks that have a better minimum hypothesis
-    if ( minHypo != rRTrack->currentHypothesis() )
-    {
-      minTrackData.push_back( InitTrackInfo(*track,minHypo,mindeltaLL) );
-    }
+      if ( minHypo != rRTrack->currentHypothesis() )
+      {
+        minTrackData.push_back( InitTrackInfo(*track,minHypo,mindeltaLL) );
+      }
 
-    // Add this track / deltaLL to the track list
-    m_trackList.push_back( TrackPair(mindeltaLL, *track) );
+      // Add this track / deltaLL to the track list
+      m_trackList.push_back( TrackPair(mindeltaLL, *track) );
 
-  }} // end track for loop
+    }} // end track for loop
 
   // Finally, set all track hypotheses to their minimum
   for ( InitTrackInfo::Vector::const_iterator iT = minTrackData.begin();
@@ -698,7 +713,7 @@ double LikelihoodTool::logLikelihood() const
         {
           verbose() << "  -> Using photon : track=" << rRTrack->key()
                     << " pixel=" << pixel->key()
-                    << " bkg=" << pixel->currentBackground() 
+                    << " bkg=" << pixel->currentBackground()
                     << " sig=" << m_photonSig->predictedPixelSignal( *iPhoton,
                                                                      rRTrack->currentHypothesis() )
                     << endmsg;
@@ -727,4 +742,57 @@ double LikelihoodTool::logLikelihood() const
 
   // return overall LL
   return ( trackLL + pixelLL );
+}
+
+double LikelihoodTool::logExp( const double x ) const
+{
+  double res(0);
+  if      ( x > limitD )
+  {
+    // Very very rarely called in this regime, so just use the full fat version
+    res = std::log( std::exp(x) - 1.0 );
+  }
+  else 
+  {
+    // A collection of rational power series covering the important range
+    // note by construction this function is never called for x<0.001
+
+    const double xx    = x*x;
+    const double xxx   = xx*x;
+    const double xxxx  = xxx*x;
+    const double xxxxx = xxxx*x;
+    if ( x > limitC )
+    {
+      res = (-5.751779337152293 - 261.58791552313113*x -
+             1610.1902353909695*xx - 291.61172549536417*xxx +
+             3733.957211885683*xxxx + 1224.2104172168554*xxxxx)/
+        (1.0 + 79.52981108433892*x + 953.4570349071099*xx +
+         2638.609797400796*xxx + 1506.9612115322623*xxxx -
+         27.33558114045007*xxxxx);
+    }
+    else if ( x > limitB )
+    {
+      res = (-7.845788509794026 - 3428.7804135353526*x -
+             228752.20145929293*xx - 3.082032088759535e6*xxx -
+             3.836270197409883e6*xxxx + 1.2251900378118051e7*xxxxx)/
+        (1.0 + 638.7306815040638*x + 60430.91709817034*xx +
+         1.315432074531156e6*xxx + 6.373056770682967e6*xxxx +
+         3.3914176474223877e6*xxxxx);
+    }
+    else if ( x > limitA )
+    {
+      res = (-10.160864268729455 - 49897.23275778952*x -
+             3.855669108991894e7*xx - 6.777802095268419e9*xxx -
+             2.421987003565588e11*xxxx - 3.5610129242332263e11*xxxxx)/
+        (1.0 + 6487.897657865318*x + 6.294785881144457e6*xx +
+         1.4333658673633337e9*xxx + 7.670700007081306e10*xxxx +
+         6.06654149712832e11*xxxxx);
+    }
+    else
+    {
+      // should never get here. But just in case ...
+      res = std::log( std::exp(x) - 1.0 );
+    }
+  }
+  return res;
 }
