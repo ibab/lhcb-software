d5398fb4c1cd9160f82db6135e545c741ff5d53e
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-07-28 16:44:41 +0000

    Added variables to RecoStats
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85100 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 05331e224bc1e69501d0f838cd45e44ed36d506f

--- Analysis/Phys/DecayTreeTuple/src/TupleToolRecoStats.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolRecoStats.cpp
@@ -1,4 +1,4 @@
-// $Id: TupleToolRecoStats.cpp,v 1.2 2010-01-26 15:39:26 rlambert Exp $
+// $Id: TupleToolRecoStats.cpp,v 1.3 2010-07-28 16:44:41 pkoppenb Exp $
 // Include files 
 
 // from Gaudi
@@ -7,10 +7,12 @@
 // local
 #include "TupleToolRecoStats.h"
 #include "Event/ProtoParticle.h"
-#include "Event/Track.h"
 
 #include "GaudiAlg/Tuple.h"
 #include "GaudiAlg/TupleObj.h"
+
+#include "Event/STCluster.h"
+
 //-----------------------------------------------------------------------------
 // Implementation file for class : TupleToolRecoStats
 //
@@ -30,7 +32,7 @@ TupleToolRecoStats::TupleToolRecoStats( const std::string& type,
   : TupleToolBase ( type, name , parent )
 {
   declareInterface<IEventTupleTool>(this);
-
+  declareProperty("fillVerbose", fillVerbose=true );
 }
 //=============================================================================
 // Destructor
@@ -38,8 +40,26 @@ TupleToolRecoStats::TupleToolRecoStats( const std::string& type,
 TupleToolRecoStats::~TupleToolRecoStats() {} 
 
 //=============================================================================
-//=============================================================================
-// Fill
+StatusCode TupleToolRecoStats::initialize() {
+  if( ! TupleToolBase::initialize() ) return StatusCode::FAILURE;
+
+  m_rawBankDecoder = tool<IOTRawBankDecoder>("OTRawBankDecoder");
+  if(!m_rawBankDecoder){
+    Error("unable to get the OTRawBankDecoder tool");
+    return StatusCode::FAILURE;
+  }
+  
+  m_l0BankDecoder = tool<IL0DUFromRawTool>("L0DUFromRawTool");
+  //declareProperty( "L0DUFromRawTool"   , m_fromRawTool = "L0DUFromRawTool" );
+  //m_l0BankDecoder = tool<IL0DUFromRawTool>( m_fromRawTool , m_fromRawTool,this );
+  if(!m_l0BankDecoder){
+    Error("unable to get the L0DUFromRawTool");
+    return StatusCode::FAILURE;
+  }
+
+  return StatusCode::SUCCESS;
+}
+
 //=============================================================================
 StatusCode TupleToolRecoStats::fill( Tuples::Tuple& tup) 
 {
@@ -49,7 +69,35 @@ StatusCode TupleToolRecoStats::fill( Tuples::Tuple& tup)
   test &= tup->column(prefix+"ChargedProtos",number<LHCb::ProtoParticles>(LHCb::ProtoParticleLocation::Charged));
   test &= tup->column(prefix+"NeutralProtos",number<LHCb::ProtoParticles>(LHCb::ProtoParticleLocation::Neutrals));
   test &= tup->column(prefix+"BestTracks",number<LHCb::Tracks>(LHCb::TrackLocation::Default));
-  test &= tup->column(prefix+"MuonTracks",number<LHCb::Tracks>(LHCb::TrackLocation::Muon));
-  return StatusCode(test) ;
+
+  if (isVerbose()){
+    test &= tup->column(prefix+"MuonTracks",number<LHCb::Tracks>(LHCb::TrackLocation::Muon));
+    test &= tup->column(prefix+"ITClusters",number<LHCb::STClusters>(LHCb::STClusterLocation::ITClusters));  
+    const unsigned int nHitsInOT = m_rawBankDecoder->totalNumberOfHits();
+    test &= tup->column(prefix+"OTClusters", nHitsInOT);
+    
+    m_l0BankDecoder->fillDataMap();
+    bool l0BankDecoderOK = m_l0BankDecoder->decodeBank();
+    if(!l0BankDecoderOK ){
+      Error("Readout error : unable to monitor the L0DU rawBank", StatusCode::SUCCESS,StatusCode::SUCCESS).ignore();
+      return StatusCode::SUCCESS;
+    }
+    int nSpd = m_l0BankDecoder->data("Spd(Mult)");
+    test &= tup->column(prefix+"spdMult", nSpd);
+    
+    const LHCb::Track::Container* tracks =  get<LHCb::Track::Container> ( LHCb::TrackLocation::Default ) ;
+    unsigned int nBack = 0;
+    LHCb::Tracks::const_iterator iterT = tracks->begin();
+    for(; iterT != tracks->end() ;++iterT) {
+      if ((*iterT)->checkFlag( LHCb::Track::Backward) == true) ++nBack;
+    }
+    test &= tup->column(prefix+"backwardTracks", nBack);
+    
+    int veloTracks = 0;
+    veloTracks = nVelo(tracks);
+    test &= tup->column(prefix+"veloTracks", veloTracks);
+  }
   
-} 
+  return StatusCode(test) ;
+    
+  } 
--- Analysis/Phys/DecayTreeTuple/src/TupleToolRecoStats.h
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolRecoStats.h
@@ -1,4 +1,4 @@
-// $Id: TupleToolRecoStats.h,v 1.3 2010-01-26 15:39:26 rlambert Exp $
+// $Id: TupleToolRecoStats.h,v 1.4 2010-07-28 16:44:41 pkoppenb Exp $
 #ifndef TUPLETOOLRECOSTATS_H 
 #define TUPLETOOLRECOSTATS_H 1
 
@@ -7,11 +7,28 @@
 #include "TupleToolBase.h"
 #include "Kernel/IEventTupleTool.h"            // Interface
 
+#include "OTDAQ/IOTRawBankDecoder.h"
+#include "L0Interfaces/IL0DUFromRawTool.h"
+#include "Event/Track.h"
+
+#include "Kernel/LHCbID.h"
+#include "LoKi/select.h"
+#include <algorithm>
+//for velo count 
+#include <boost/lambda/bind.hpp>
+#include <boost/lambda/lambda.hpp>
+ 
 /** @class TupleToolRecoStats TupleToolRecoStats.h
  *  
- *  Fill number of protoparticles, tracks, ...
+ *  Fill number of protoparticles, tracks.
+ *  If isVerbose, also fill muon tracks, IT and OT clusters, SPD multiplicity, backward tracks, velo tracks
+ *  for IT clusters, one needs to run 
+ *  \verbatim
+from Configurables import RawBankToSTClusterAlg 
+createITClusters = RawBankToSTClusterAlg("CreateITClusters") 
+createITClusters.DetType     = "IT"\endverbatim 
  *
- *  @author Patrick Koppenburg
+ *  @author Patrick Koppenburg, Fatima Soomro, Jibo He
  *  @date   2009-02-11
  */
 class TupleToolRecoStats : public TupleToolBase, virtual public IEventTupleTool {
@@ -22,14 +39,63 @@ public:
                       const IInterface* parent);
 
   virtual ~TupleToolRecoStats( ); ///< Destructor
+  virtual StatusCode initialize();
+  
   virtual StatusCode fill( Tuples::Tuple& );///< Fill tuple
 
 protected:
+// velo count
+inline bool inCloneContainer(const std::vector<LHCb::Track*>& keepCont, const std::vector<LHCb::LHCbID>& vids) {
+  bool keep = true;
+  std::vector<LHCb::Track*>::const_iterator iter = keepCont.begin();
+  for (;iter != keepCont.end(); ++iter ){
+    const std::vector<LHCb::LHCbID>& vids2 = (*iter)->lhcbIDs(); 
+    std::vector<LHCb::LHCbID> veloHits2; veloHits2.reserve(vids.size());
+    LoKi::select(vids2.begin(), vids2.end(), std::back_inserter(veloHits2), 
+                 boost::lambda::bind(&LHCb::LHCbID::isVelo,boost::lambda::_1));
+    if (std::equal(vids.begin(), vids.end(), veloHits2.begin())){
+      keep = false;
+      break;
+    }
+  }
+  return keep;
+}
+  
+  inline unsigned nVelo(const LHCb::Tracks* tracks) {
+    
+    std::vector<LHCb::Track*> tmpCont;
+    LHCb::Tracks::const_iterator iterT = tracks->begin();
+    for(; iterT != tracks->end() ;++iterT) {
+      if ((*iterT)->hasVelo() == true) tmpCont.push_back(*iterT); 
+    }
+    
+    std::vector<LHCb::Track*> keepCont;
+    keepCont.push_back(tmpCont.front());
+    
+    std::vector<LHCb::Track*>::const_iterator iterT2 = tmpCont.begin();
+    for (;iterT2 != tmpCont.end(); ++iterT2 ){
+      const std::vector<LHCb::LHCbID>& vids = (*iterT2)->lhcbIDs();
+      std::vector<LHCb::LHCbID> veloHits; veloHits.reserve(vids.size());
+      LoKi::select(vids.begin(), vids.end(), std::back_inserter(veloHits), 
+                   boost::lambda::bind(&LHCb::LHCbID::isVelo,boost::lambda::_1));
+      
+      if (inCloneContainer(keepCont,veloHits) == false){
+        // nothing
+      } 
+      else {
+        keepCont.push_back(*iterT2);   
+      }
+    } // iterT2
+    return keepCont.size();
+  }
 
 private:
 #include "Number.icpp"
-
-
+  const IOTRawBankDecoder* m_rawBankDecoder;
+  std::string m_fromRawTool;
+  IL0DUFromRawTool* m_l0BankDecoder;
+  bool fillVerbose;
+ 
 };
 #endif // TUPLETOOLRECOSTATS_H
 
