092561c41e0739d2503c14df521ba3d8366c9d17
Greig Cowan <g.cowan@ed.ac.uk>
2010-07-29 15:37:42 +0000

    Added new data to the tests directory which is more representative of current LHC conditions
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85108 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 49b5f8bac9ab291d5d687ca0005abc1044a08256

--- Analysis/Phys/StrippingSelections/tests/TestMyStrippingLineOnRealData.py
+++ Analysis/Phys/StrippingSelections/tests/TestMyStrippingLineOnRealData.py
@@ -19,18 +19,8 @@ dvinit.Members.insert(0, veloNZSKiller() )
 
 DaVinci().PrintFreq = 100
 DaVinci().HistogramFile = 'DV_stripping_histos.root'
-DaVinci().EvtMax = 100000 
+DaVinci().EvtMax = 10 
 DaVinci().appendToMainSequence( [ sc.sequence() ] )
 DaVinci().DataType = "2010"
 DaVinci().InputType = 'SDST'
-DaVinci().Input = [
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002181_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'", 
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002182_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002183_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'", 
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002199_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'", 
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002294_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'", 
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002308_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'", 
-"DATAFILE='LFN:/lhcb/data/2010/SDST/00006563/0000/00006563_00002347_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'" 
-]
-FileCatalog().Catalogs= ["xmlcatalog_file:pool_xml_catalog.xml"]
-
+importOptions("$STRIPPINGSELECTIONSROOT/tests/Reco04-Stripping07-SDSTs.py")
