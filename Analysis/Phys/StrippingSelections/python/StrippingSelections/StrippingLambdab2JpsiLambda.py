7c8e6bd086a84854bb840eea23f19bacb164e619
Yasmine Sara Amhis <yasmine.sara.amhis@cern.ch>
2010-07-29 16:45:20 +0000

    update correct version of StrippingLambdab2JpsiLambda
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85114 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2c37b54e0a47180b72e2140de24550208d96bc31

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingLambdab2JpsiLambda.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingLambdab2JpsiLambda.py
@@ -1,10 +1,11 @@
+# $Id: StrippingLambdab2JpsiLambda.py,v 1.2 2010-07-29 16:45:20 yamhis Exp $
 
 __author__ = ['Yasmine Amhis']
 __date__ = '29/07/2010'
-__version__ = '$Revision: 1.1 $'
+__version__ = '$Revision: 1.2 $'
 
 '''
-Strippping Selections for Lambdab ->Jpsi Lambda (LL and DD)
+StrippingSelections for Lambda_b0-> Jpsi Lambda ( LL and DD )
 '''
 
 from Gaudi.Configuration import *
@@ -12,40 +13,41 @@ from LHCbKernel.Configuration import *
 from Configurables import FilterDesktop, CombineParticles, OfflineVertexFitter	
 from PhysSelPython.Wrappers import Selection, DataOnDemand
 
+
+
 class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
     """
-    Definition of nominal Lamdba_b-> Jpsi Lambda stripping. 
+    Definition  Lambad_b-> Jpsi Lambda stripping. 
     """
     __slots__ = {
         
         #---------------------------------------------------------
         #Jpsi cuts
         #---------------------------------------------------------
-                "MuonTRCHI2Loose"	 : 10.0	# adimensional 
-        ,	"JpsiMassWinLoose"	: 800.0	# MeV
-        ,	"JpsiVCHI2Loose"	: 25.0	# adimensional
+                "MuonTRCHI2"	: 20.0	# adimensional 
+        ,	"JpsiMassWin"	: 100.0	# MeV
+        ,	"JpsiVCHI2"	: 25.0	# adimensional
         #---------------------------------------------------------
         #Lambda cuts
         #---------------------------------------------------------
-        ,       "ProtonTRCHI2Loose"	: 10.0	# adimensional  
-        ,	"ProtonPIDp"	: -5.0	# adimensional  
-        ,	"LambdaMassWin"	: 15.0	# MeV
-        ,	"LambdaMassWinLoose"	: 30.0	# MeV
-        ,	"LambdaPT" 	: 1000.0# MeV
-        ,	"LambdaPTLoose" 	: 500.0	# MeV
-        ,	"LambdaVCHI2" 	: 20.0	# adimensional
-        ,	"LambdaVCHI2Loose" 	: 20.0	# adimensional
+        ,       "ProtonTRCHI2"	: 20.0	# adimensional
+        ,       "PionTRCHI2"    : 20.0  #adimensional  
+      
+        ,       "ProtonPT" 	: 100.0 # MeV                     
+        ,       "PionPT" 	: 100.0 # MeV 
+
+        ,	"LambdaMassWin"	: 30.0	# MeV
+        ,	"LambdaVCHI2" 	: 25.0	# adimensional
         #---------------------------------------------------------
         #Lambda_b cuts
         #---------------------------------------------------------         
-        ,	"LambdabMassWin"	: 300.0	# MeV
-        ,	"LambdabWinLoose"	: 300.0	# MeV
-        ,	"LambdabVCHI2" 	: 5.0	# adimensional
-        ,	"LambdabVCHI2Loose" 	: 20.0	# adimensional
+        ,	"LambdabMassWin"     : 500.0	# MeV
+        ,	"LambdabVCHI2" 	: 9.0	# adimensional
+        ,       "LambdabDirection": 1  # adimentional only used to make sure that the PV-refit is done        
         }
     
     
-    
+    _Jpsi2MuMuSel = None
     def line_LL( self ):
         from StrippingConf.StrippingLine import StrippingLine
 	Lambdab2JpsiLambdaSel = self.Lambdab2JpsiLambdaLL()
@@ -56,49 +58,30 @@ class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
 	Lambdab2JpsiLambdaSel = self.Lambdab2JpsiLambdaDD()
 	return StrippingLine('Lambdab2JpsiLambdaDDLine', prescale = 1, algos = [Lambdab2JpsiLambdaSel])   
     #---------------------------------------------------------
-    def Jpsi2MuMuLoose( self ):
-       StdVeryLooseJpsi2MuMu = DataOnDemand(Location = "Phys/StdVeryLooseJpsi2MuMu")
-       _JpsiFilter = FilterDesktop("JpsiFilterForLambdab2JpsiLambda")
-       _JpsiFilter.Code = "  (MAXTREE('mu+'==ABSID, TRCHI2DOF) < %(MuonTRCHI2Loose)s)" \
-                          "& (ADMASS('J/psi(1S)') < %(JpsiMassWinLoose)s *MeV)" \
-                          "& (VFASPF(VCHI2/VDOF) < %(JpsiVCHI2Loose)s)" % self.getProps()
-       
-       Jpsi = Selection("Jpsi2MuMuForLambdab2JpsiLambda",
-                        Algorithm = _JpsiFilter,
-                        RequiredSelections = [StdVeryLooseJpsi2MuMu])
-       return Jpsi
+    def Jpsi2MuMu( self ):
+        if StrippingLambdab2JpsiLambdaConf._Jpsi2MuMuSel == None:
+            StdVeryLooseJpsi2MuMu = DataOnDemand(Location = "Phys/StdVeryLooseJpsi2MuMu")
+            _JpsiFilter = FilterDesktop("JpsiFilterForLambdab2JpsiLambda")
+            _JpsiFilter.Code = "  (MAXTREE('mu+'==ABSID, TRCHI2DOF) < %(MuonTRCHI2)s)" \
+                               "& (ADMASS('J/psi(1S)') < %(JpsiMassWin)s *MeV)" \
+                               "& (VFASPF(VCHI2/VDOF) < %(JpsiVCHI2)s)" % self.getProps()
+            
+            StrippingLambdab2JpsiLambdaConf._Jpsi2MuMuSel = Selection("Jpsi2MuMuForLambdab2JpsiLambda",
+                             Algorithm = _JpsiFilter,
+                             RequiredSelections = [StdVeryLooseJpsi2MuMu])
+        return StrippingLambdab2JpsiLambdaConf._Jpsi2MuMuSel 
    
-    def Jpsi2MuMuForLL( self ):
-       StdVeryLooseJpsi2MuMu = DataOnDemand(Location = "Phys/StdVeryLooseJpsi2MuMu")
-       _JpsiFilterLL = FilterDesktop("JpsiFilterForLambdab2JpsiLambdaLL")
-       _JpsiFilterLL.Code = "  (MAXTREE('mu+'==ABSID, TRCHI2DOF) < %(MuonTRCHI2Loose)s)" \
-                          "& (ADMASS('J/psi(1S)') < %(JpsiMassWinLoose)s *MeV)" \
-                          "& (VFASPF(VCHI2/VDOF) < %(JpsiVCHI2Loose)s)" % self.getProps()
-       
-       JpsiLL = Selection("Jpsi2MuMuForLambdab2JpsiLambdaLL",
-                        Algorithm = _JpsiFilterLL,
-                        RequiredSelections = [StdVeryLooseJpsi2MuMu])
-       return JpsiLL
-    def Jpsi2MuMuForDD( self ):
-       StdVeryLooseJpsi2MuMu = DataOnDemand(Location = "Phys/StdVeryLooseJpsi2MuMu")
-       _JpsiFilterDD = FilterDesktop("JpsiFilterForLambdab2JpsiLambdaDD")
-       _JpsiFilterDD.Code = "  (MAXTREE('mu+'==ABSID, TRCHI2DOF) < %(MuonTRCHI2Loose)s)" \
-                          "& (ADMASS('J/psi(1S)') < %(JpsiMassWinLoose)s *MeV)" \
-                          "& (VFASPF(VCHI2/VDOF) < %(JpsiVCHI2Loose)s)" % self.getProps()
-       
-       JpsiDD = Selection("Jpsi2MuMuForLambdab2JpsiLambdaDD",
-                        Algorithm = _JpsiFilterDD,
-                        RequiredSelections = [StdVeryLooseJpsi2MuMu])
-       return JpsiDD
+
      #---------------------------------------------------------
     def Lambda2ppiLL( self ):
         StdLooseLambdaLL = DataOnDemand(Location = "Phys/StdLooseLambdaLL")
         _LambdaFilter = FilterDesktop("LambdaFilterForLambdab2JpsiLambda")
-        _LambdaFilter.Code = "  (MAXTREE('p+'==ABSID, TRCHI2DOF) < %(ProtonTRCHI2Loose)s)" \
-                             "& (ADMASS('Lambda0') < %(LambdaMassWinLoose)s *MeV)" \
-                             "& (PT > %(LambdaPTLoose)s *MeV)" \
-                             "& (VFASPF(VCHI2/VDOF) < %(LambdaVCHI2Loose)s)" % self.getProps()
-        
+        _LambdaFilter.Code = "  (MAXTREE('p+'==ABSID, TRCHI2DOF) < %(ProtonTRCHI2)s)" \
+                             "&  (MAXTREE('p+'==ABSID, PT) > %(ProtonPT)s)"\
+                             "&  (MAXTREE('pi-'==ABSID, TRCHI2DOF) < %(PionTRCHI2)s)" \
+                             "&  (MAXTREE('pi-'==ABSID, PT) > %(PionPT)s)"\
+                             "& (ADMASS('Lambda0') < %(LambdaMassWin)s *MeV)" \
+                             "& (VFASPF(VCHI2/VDOF) < %(LambdaVCHI2)s)" % self.getProps()        
         Lambda =  Selection ("LambdaLLForLambdab2JpsiLambdaLL",
                              Algorithm = _LambdaFilter,
 
@@ -109,10 +92,12 @@ class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
     def Lambda2ppiDD( self ):
          StdLooseLambdaDD = DataOnDemand(Location = "Phys/StdLooseLambdaDD")
          _LambdaFilter = FilterDesktop("LambdaFilterForLambdab2JpsiLambdaDD")
-         _LambdaFilter.Code = "  (MAXTREE('p+'==ABSID, TRCHI2DOF) < %(ProtonTRCHI2Loose)s)" \
-                              "& (ADMASS('Lambda0') < %(LambdaMassWinLoose)s *MeV)" \
-                              "& (PT > %(LambdaPTLoose)s *MeV)" \
-                              "& (VFASPF(VCHI2/VDOF) < %(LambdaVCHI2Loose)s)" % self.getProps()
+         _LambdaFilter.Code = "  (MAXTREE('p+'==ABSID, TRCHI2DOF) < %(ProtonTRCHI2)s)" \
+                              "&  (MAXTREE('p+'==ABSID, PT) > %(ProtonPT)s)"\
+                              "&  (MAXTREE('pi-'==ABSID, TRCHI2DOF) < %(PionTRCHI2)s)" \
+                              "&  (MAXTREE('pi-'==ABSID, PT) > %(PionPT)s)"\
+                              "& (ADMASS('Lambda0') < %(LambdaMassWin)s *MeV)" \
+                              "& (VFASPF(VCHI2/VDOF) < %(LambdaVCHI2)s)" % self.getProps()
          
          Lambda =  Selection ("LambdaDDForLambdab2JpsiLambdaDD",
                               Algorithm = _LambdaFilter,
@@ -122,12 +107,15 @@ class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
      
      #---------------------------------------------------------
     def Lambdab2JpsiLambdaLL( self ):
-        JpsiLL   = self.Jpsi2MuMuForLL()
+        Jpsi   = self.Jpsi2MuMu()
         Lambda = self.Lambda2ppiLL()
         _Lambdab = CombineParticles("Lambdab2JpsiLambdaLL")
         _Lambdab.DecayDescriptor = "[Lambda_b0 -> Lambda0 J/psi(1S) ]cc"
         _Lambdab.CombinationCut = "ADAMASS('Lambda_b0') < %(LambdabMassWin)s *MeV" % self.getProps()
-        _Lambdab.MotherCut = "(VFASPF(VCHI2/VDOF) < %(LambdabVCHI2Loose)s)" % self.getProps()
+        
+        _Lambdab.MotherCut = "(VFASPF(VCHI2/VDOF) < %(LambdabVCHI2)s)"\
+                             "& (BPVDIRA < %(LambdabDirection)s)" % self.getProps()
+         
         _Lambdab.ReFitPVs = True
         # Set the OfflineVertexFitter to keep the 4 tracks and not the J/Psi Lambda
         _Lambdab.addTool( OfflineVertexFitter() )
@@ -136,17 +124,19 @@ class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
         
         Lambdab = Selection("SelLambdab2JpsiLambdaLL",
                             Algorithm = _Lambdab,
-                            RequiredSelections = [JpsiLL, Lambda])
+                            RequiredSelections = [Jpsi, Lambda])
         
         return Lambdab
     #---------------------------------------------------------
     def Lambdab2JpsiLambdaDD ( self ):
-        JpsiDD   = self.Jpsi2MuMuForDD()
+        Jpsi   = self.Jpsi2MuMu()
         Lambda = self.Lambda2ppiDD()
         _Lambdab = CombineParticles("Lambdab2JpsiLambdaDD")
         _Lambdab.DecayDescriptor = "[Lambda_b0 -> Lambda0 J/psi(1S) ]cc"
         _Lambdab.CombinationCut = "ADAMASS('Lambda_b0') < %(LambdabMassWin)s *MeV" % self.getProps()
-        _Lambdab.MotherCut = "(VFASPF(VCHI2/VDOF) < %(LambdabVCHI2Loose)s)" % self.getProps()
+        _Lambdab.MotherCut = "(VFASPF(VCHI2/VDOF) < %(LambdabVCHI2)s)"\
+                             "&(BPVDIRA < %(LambdabDirection)s)" % self.getProps()
+
         _Lambdab.ReFitPVs = True
         # Set the OfflineVertexFitter to keep the 4 tracks and not the J/Psi Lambda
         _Lambdab.addTool( OfflineVertexFitter() )
@@ -155,7 +145,7 @@ class StrippingLambdab2JpsiLambdaConf(LHCbConfigurableUser):
         
         Lambdab = Selection("SelLambdab2JpsiLambdaDD",
                             Algorithm = _Lambdab,
-                            RequiredSelections = [JpsiDD, Lambda])
+                            RequiredSelections = [Jpsi, Lambda])
         
         return Lambdab
     
