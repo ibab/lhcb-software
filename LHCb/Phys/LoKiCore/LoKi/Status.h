e60506eddbba1324d17c82490eedba684f2c9d74
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-08-02 10:54:02 +0000

    Add new functors for Global Event cuts in Hlt, see task #16675
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@53181 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9de5fe07b097c854726c6ca56a508640c5b0e856

---
+++ LHCb/Phys/LoKiCore/LoKi/Status.h
@@ -0,0 +1,145 @@
+// $Id: $
+// ============================================================================
+#ifndef LOKI_STATUS_H 
+#define LOKI_STATUS_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// LHCbKernel
+// ============================================================================
+#include "Kernel/IAccept.h"
+#include "Kernel/ICheckTool.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/BasicFunctors.h"
+#include "LoKi/Interface.h"
+// ============================================================================
+/** @file Loki/Status.h
+ *  collection of "checkers" deaing with generic tools
+ *  @see ICheckTool 
+ *  @see IAcceptTool
+ *
+ *  This file is part of LoKi project: 
+ *   ``C++ ToolKit for Smart and Friendly Physics Analysis''
+ * 
+ *  By usage of this code one clearly states the disagreement 
+ *  with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@nikhef.nl
+ *  @date   2010-08-02 
+ */
+// ============================================================================
+namespace LoKi 
+{
+  // ==========================================================================
+  /** @namespace LoKi::Status 
+   *  Collection of "checkers"-functors dealing with generic tools 
+   *  @author Vanya Belyaev Ivan.Belyaev@nikhef.nl
+   *  @date   2010-08-02
+   */
+  namespace Status 
+  {
+    // ========================================================================
+    /** @class Check     LoKi/Status.h
+     *  Simple checker to "check" using ICheckTool 
+     *  @see ICheckTool
+     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
+     *  @date 2010-08-03
+     */
+    class Check : public LoKi::Functor<void,bool>
+    {
+    public:
+      // ======================================================================
+      /// constructor from tool nickname 
+      Check ( const std::string& nikname ) ;
+      /// MANDATORY: virtual destructor 
+      virtual ~Check() ;
+      /// MANDATORY: clone method ("virtual constructor")
+      virtual  Check* clone() const ;
+      /// MANDATORY: the only one essential method 
+      virtual  result_type operator() ( /* argument v */ ) const ;
+      /// OPTIONAL: nice printout 
+      virtual std::ostream& fillStream ( std::ostream& s ) const ;
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// the default constructor is disabled 
+      Check () ;                         // the default constructor is disabled 
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// tool nickname 
+      std::string                     m_nickname ; // the tool nickname  
+      /// the checker 
+      mutable LoKi::Interface<ICheckTool> m_tool;  // the checker 
+      // ======================================================================
+    } ;
+    // ========================================================================
+    /** @class Accept LoKi/Status.h
+     *  Simple checker to "check" using IAccept
+     *  @see IAccept
+     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
+     *  @date 2010-08-03
+     */
+    class Accept : public LoKi::Functor<void,bool>
+    {
+    public:
+      // ======================================================================
+      /// constructor from tool nickname 
+      Accept ( const std::string& nikname ) ;
+      /// MANDATORY: virtual destructor 
+      virtual ~Accept () ;
+      /// MANDATORY: clone method ("virtual constructor")
+      virtual  Accept* clone() const ;
+      /// MANDATORY: the only one essential method 
+      virtual  result_type operator() ( /* argument v */ ) const ;
+      /// OPTIONAL: nice printout 
+      virtual std::ostream& fillStream ( std::ostream& s ) const ;
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// the default constructor is disabled 
+      Accept () ;                        // the default constructor is disabled 
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// tool nickname 
+      std::string                      m_nickname ;  // the tool nickname  
+      /// the checker 
+      mutable LoKi::Interface<IAccept> m_tool     ;  // the checker 
+      // ======================================================================
+    } ;
+    // ========================================================================
+  } //                                            end of namespace LoKi::Status 
+  // ==========================================================================
+  namespace Cuts 
+  {
+    // ========================================================================
+    /** @typedef ACCEPT
+     *  Trivial checker/function to "check"
+     *  @see IAccept 
+     *  @see LoKi::Status::Accept
+     *  @author Vanya BELYAEV  Ivan.BElyaev@nikhef.nl
+     *  @date 2010-08-03
+     */
+    typedef LoKi::Status::Accept                                       ACCEPT ;
+    // ========================================================================
+    /** @typedef CHECK
+     *  Trivial checker/function to "check"
+     *  @see ICheckTool 
+     *  @see LoKi::Status::Check
+     *  @author Vanya BELYAEV  Ivan.BElyaev@nikhef.nl
+     *  @date 2010-08-03
+     */
+    typedef LoKi::Status::Check                                         CHECK ;
+    // ========================================================================
+  } //                                             end of namespace LoKi::Cuts 
+  // ==========================================================================
+} //                                                      end of namespace LoKi
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // LOKI_STATUS_H
+// ============================================================================
---
+++ LHCb/Phys/LoKiCore/src/Status.cpp
@@ -0,0 +1,141 @@
+// $Id: $
+// ============================================================================
+// Include files 
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/IAlgContextSvc.h"
+#include "GaudiKernel/IToolSvc.h"
+#include "GaudiKernel/SmartIF.h"
+#include "GaudiKernel/ToStream.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/Status.h"
+#include "LoKi/ILoKiSvc.h"
+// ============================================================================
+/** @file 
+ *  Implementation of functors deaing with generic tools
+ *  @see ICheckTool 
+ *  @see IAcceptTool
+ *
+ *  This file is part of LoKi project: 
+ *   ``C++ ToolKit for Smart and Friendly Physics Analysis''
+ * 
+ *  By usage of this code one clearly states the disagreement 
+ *  with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *  @author Vanya Belyaev Ivan.Belyaev@nikhef.nl
+ *  @date   2010-08-02 
+ */
+// ============================================================================
+// constructor from tool nickname 
+// ============================================================================
+LoKi::Status::Check::Check
+( const std::string& nickname ) 
+  : LoKi::Functor<void,bool>()
+  , m_nickname ( nickname ) 
+  , m_tool     () 
+{
+  //
+  ILoKiSvc* l = lokiSvc() ;
+  Assert ( 0 != l   , "ILoKiSvc* points to NULL!" ) ;
+  SmartIF<IToolSvc> tsvc ( l ) ;
+  Assert ( !(!tsvc) , "IToolSvc* points to NULL!" ) ;
+  // aquire tool 
+  ICheckTool*       ctool  = 0  ;
+  const IInterface* parent = 0 ;
+  StatusCode sc = tsvc->retrieveTool ( nickname , ctool , parent , true ) ;
+  Assert ( sc.isSuccess() , "Unable to aquire the tool '" + nickname + "'" , sc );
+  m_tool = ctool ;
+  Assert ( !(!m_tool)     , "Unable to aquire the tool '" + nickname + "'" );
+}
+// ============================================================================
+// MANDATORY: virtual destructor 
+// ============================================================================
+LoKi::Status::Check::~Check(){  m_tool.release() ; }
+// ============================================================================
+// MANDATORY: clone method ("virtual constructor")
+// ============================================================================
+LoKi::Status::Check* LoKi::Status::Check::clone() const 
+{ return new LoKi::Status::Check ( *this ) ; }
+// ============================================================================
+// MANDATORY: the only one essential method 
+// ============================================================================
+LoKi::Status::Check::result_type 
+LoKi::Status::Check::operator() ( /* LoKi::Status::Check::argument */ ) const 
+{
+  Assert ( !(!m_tool) , "Invalid ICheckTool-interface" ) ;
+  //
+  return m_tool->check().isSuccess() ;
+}
+// ============================================================================
+// OPTIONAL: nice printout 
+// ============================================================================
+std::ostream& 
+LoKi::Status::Check::fillStream ( std::ostream& s ) const 
+{
+  s << " CHECK("   ;
+  Gaudi::Utils::toStream ( m_nickname , s ) ;
+  return s << ") " ;
+}
+// ============================================================================
+
+// ============================================================================
+// constructor from tool nickname 
+// ============================================================================
+LoKi::Status::Accept::Accept
+( const std::string& nickname ) 
+  : LoKi::Functor<void,bool>()
+  , m_nickname ( nickname ) 
+  , m_tool     () 
+{
+  //
+  ILoKiSvc* l = lokiSvc() ;
+  Assert ( 0 != l   , "ILoKiSvc* points to NULL!" ) ;
+  SmartIF<IToolSvc> tsvc ( l ) ;
+  Assert ( !(!tsvc) , "IToolSvc* points to NULL!" ) ;
+  // aquire tool 
+  IAccept*       ctool  = 0  ;
+  const IInterface* parent = 0 ;
+  StatusCode sc = tsvc->retrieveTool ( nickname , ctool , parent , true ) ;
+  Assert ( sc.isSuccess() , "Unable to aquire the tool '" + nickname + "'" , sc );
+  m_tool = ctool ;
+  Assert ( !(!m_tool)     , "Unable to aquire the tool '" + nickname + "'" );
+}
+// ============================================================================
+// MANDATORY: virtual destructor 
+// ============================================================================
+LoKi::Status::Accept::~Accept(){  m_tool.release() ; }
+// ============================================================================
+// MANDATORY: clone method ("virtual constructor")
+// ============================================================================
+LoKi::Status::Accept* LoKi::Status::Accept::clone() const 
+{ return new LoKi::Status::Accept ( *this ) ; }
+// ============================================================================
+// MANDATORY: the only one essential method 
+// ============================================================================
+LoKi::Status::Accept::result_type 
+LoKi::Status::Accept::operator() ( /* LoKi::Status::Check::argument */ ) const 
+{
+  Assert ( !(!m_tool) , "Invalid IAccept-interface" ) ;
+  //
+  return m_tool->accept() ;
+}
+// ============================================================================
+// OPTIONAL: nice printout 
+// ============================================================================
+std::ostream& 
+LoKi::Status::Accept::fillStream ( std::ostream& s ) const 
+{
+  s << " ACCEPT("   ;
+  Gaudi::Utils::toStream ( m_nickname , s ) ;
+  return s << ") " ;
+}
+// ============================================================================
+
+
+// ============================================================================
+// The END 
+// ============================================================================
