ab22a89cff83d794a82d97aff357bfa1af830b78
hmdegaud <hmdegaud@cern.ch>
2010-08-03 15:47:41 +0000

    fixed the version of LbScripts to v5r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@53226 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b9035b41d3057214962385838fabcbf6f547dd0

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -14,10 +14,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from shutil import rmtree
 
-script_version = '100802'
+script_version = '100803'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v5r2"
+lbscripts_version = "v5r3"
 compat_version = None
 #-----------------------------------------------------------------------------------
 
