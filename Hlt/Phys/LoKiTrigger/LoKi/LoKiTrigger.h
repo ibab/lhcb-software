d1ebfb006551458515cc0c04098359540e4dd5f7
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-08-06 09:19:54 +0000

    remove missing include
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@53315 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: acd9b7ab19f95e2c49a3d525ee9f2de0667dd45e

--- Hlt/Phys/LoKiTrigger/LoKi/LoKiTrigger.h
+++ Hlt/Phys/LoKiTrigger/LoKi/LoKiTrigger.h
@@ -9,6 +9,21 @@
 // ============================================================================
 // LoKi
 // ============================================================================
+/** @file
+ *  This file is part of LoKi project: 
+ *   ``C++ ToolKit for Smart and Friendly Physics Analysis''
+ * 
+ *  By usage of this code one clearly states the disagreement 
+ *  with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *  
+ *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+ *
+ *  $Revision$
+ *  Last Modification $Date:$ 
+ *                 by $Author$ 
+ */
+// ============================================================================
 #include "LoKi/TrackTypes.h"
 #include "LoKi/Tracks.h"
 #include "LoKi/TrackCuts.h"
@@ -18,7 +33,7 @@
 #include "LoKi/RecVertexCuts.h"
 #include "LoKi/TrSources.h"
 // ============================================================================
-#include "LoKi/ITrHybridFactory.h"
+#include "LoKi/ITrgFunctorFactory.h"
 // ============================================================================
 // The END 
 // ============================================================================
