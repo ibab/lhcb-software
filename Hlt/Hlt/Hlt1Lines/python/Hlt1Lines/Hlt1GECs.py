5325c225c7b7ce01c8400a6e6d8f5fa83af0c530
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2010-08-07 04:15:39 +0000

    New Global Event Cut machinery for HLT1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@85233 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0ec74a9d835cb688a4f59745acee61c2262278b0

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
@@ -33,3 +33,27 @@ def Hlt1_Velo_GEC( logic ):
                                    ) 
 
   return bindMembers( None, [ Hlt1_Velo_GEC_Filter ] )
+
+def Hlt1_OT_GEC( logic ):
+  from Configurables import LoKi__VoidFilter as VoidFilter
+  from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
+  from HltLine.HltLine import bindMembers
+
+  modules =  CoreFactory('CoreFactory').Modules
+  for i in [ 'LoKiTrigger.decorators' ] : 
+    if i not in modules : modules.append(i)
+
+  # The OT Void filter
+  Hlt1_OT_GEC_Code = ""
+  if   (logic == "<") : Hlt1_OT_GEC_Code = "ACCEPT('Hlt::GEC/MyGEC')"
+  elif (logic == ">") : Hlt1_OT_GEC_Code = "~ACCEPT('Hlt::GEC/MyGEC')" 
+  else : return 0 
+
+  Hlt1_OT_GEC_Filter = VoidFilter('Hlt1_OT_GEC'
+                                   , Code = Hlt1_OT_GEC_Code
+                                   )
+  from Configurables import Hlt__GEC as GEC 
+  Hlt1_OT_GEC_Filter.addTool ( GEC , "MyGEC" )
+  Hlt1_OT_GEC_Filter.MyGEC.MaxOTHits = 10000
+
+  return bindMembers( None, [ Hlt1_OT_GEC_Filter ] )
