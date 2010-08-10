0d6c640ac0d9840b9818020fd0770d43df52b19a
jpalac <jpalac@cern.ch>
2010-08-10 10:13:01 +0000

    . OutputStreamConf is now a simple ConfigurableUser. Its params() method     returns an OutputStreamConfParams, which is equivalent to old OutputStreamConf.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85253 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ae3d010800e953344fb5af6ed1e80606196b1356

--- Analysis/Phys/DSTWriters/python/DSTWriters/streamconf.py
+++ Analysis/Phys/DSTWriters/python/DSTWriters/streamconf.py
@@ -2,13 +2,28 @@
 
 __author__ = "Juan PALACIOS juan.palacios@nikhef.nl"
 
-__version__ = '$Revision: 1.2 $'
+__version__ = '$Revision: 1.3 $'
 
-__all__ = ('OutputStreamConf')
+__all__ = ('OutputStreamConf', 'OutputStreamConfParams')
 
 from copy import copy
 
-class OutputStreamConf(object) :
+from GaudiConf.Configuration import ConfigurableUser
+
+class OutputStreamConf(ConfigurableUser) :
+        
+    __slots__ = { 'streamType' : None,
+                  'filePrefix' : 'Sel',
+                  'fileExtension' : '.dst',
+                  'extraItems' :  []}
+
+    def params(self) :
+        return OutputStreamConfParams(streamType = self.getProp('streamType'),
+                                      filePrefix = self.getProp('filePrefix'),
+                                      fileExtension = self.getProp('fileExtension'),
+                                      extraItems = copy(self.getProp('extraItems'))  )
+
+class OutputStreamConfParams(object) :
     def __init__(self,
                  streamType = None,
                  filePrefix = 'Sel',
@@ -18,5 +33,14 @@ class OutputStreamConf(object) :
         self.filePrefix = filePrefix
         self.extension = fileExtension
         self.extraItems = copy(extraItems)
-        self.name = ''
+        self.name = ''    
+
+    def __str__(self) :
+        output = '\nOutputStreamConfXX\n'
+        output += 'name       : '+ self.name + '\n'
+        output += 'streamType : '+ str(self.streamType) + '\n'
+        output += 'filePrefix : '+ str(self.filePrefix) + '\n'
+        output += 'extension  : '+ str(self.extension) + '\n'
+        output += 'extraItems : '+ str(self.extraItems) + '\n'
+        return output
 
