f74823679c9e561620c421b0418f68f3d9a47fc5
jpalac <jpalac@cern.ch>
2010-08-12 15:54:53 +0000

    New interfaces. To be used for deep cloning of ProtoParticles.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85332 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 03f3f48c76e8adfcd55c6e643761a7dd02ce28cc

---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneCaloHypo.h
@@ -0,0 +1,25 @@
+// $Id: ICloneCaloHypo.h,v 1.1 2010-08-12 15:54:53 jpalac Exp $
+#ifndef MICRODST_ICLONECALOHYPO_H 
+#define MICRODST_ICLONECALOHYPO_H 1
+// from MicroDST
+#include "MicroDST/ICloner.h"
+
+// Forward declarations
+namespace LHCb 
+{
+  class CaloHypo;
+}
+
+/** @class ICloneCaloHypo MicroDST/ICloneCaloHypo.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2010-08-12
+ */
+class GAUDI_API ICloneCaloHypo : virtual public MicroDST::ICloner<LHCb::CaloHypo> {
+public: 
+
+  DeclareInterfaceID(ICloneCaloHypo, 1, 0 );
+
+};
+#endif // MICRODST_ICLONECALOHYPO_H
---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneMuonPID.h
@@ -0,0 +1,25 @@
+// $Id: ICloneMuonPID.h,v 1.1 2010-08-12 15:54:53 jpalac Exp $
+#ifndef MICRODST_ICLONEMUONPID_H 
+#define MICRODST_ICLONEMUONPID_H 1
+// from MicroDST
+#include "MicroDST/ICloner.h"
+
+// Forward declarations
+namespace LHCb 
+{
+  class MuonPID;
+}
+
+/** @class ICloneMuonPID MicroDST/ICloneMuonPID.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2010-08-12
+ */
+class GAUDI_API ICloneMuonPID : virtual public MicroDST::ICloner<LHCb::MuonPID> {
+public: 
+
+  DeclareInterfaceID(ICloneMuonPID, 1, 0 );
+
+};
+#endif // MICRODST_ICLONEMUONPID_H
