ibab/lhcb-software

commit 60be1b0cb93977e837eef65b02071463b1078d17

set filtered states in FitNode
git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@52947 4525493e-7705-40b1-a816-d608a930855b

Former-commit-id: e390c8058f20475296da639b158b1293ae66a0ac

Rec/Tr/TrackFitter/doc/release.notes
@@ -3,6 +3,9 @@
 ! Responsible : Wouter Hulsbergen
 !----------------------------------------------------------------------------
 
+! 2010-07-23 - Wouter Hulsbergen
+ - set filtered states in FitNode
+
 !=========================== TrackFitter v2r49 2010-07-20 ===================
 ! 2010-07-07 - Wouter Hulsbergen
  - Fixed memory leak affecting HLT2

Rec/Tr/TrackFitter/src/TrackKalmanFilter.cpp
@@ -123,6 +123,7 @@ StatusCode TrackKalmanFilter::fit( LHCb::Track& track, NodeRange& nodes,
 
     // save filtered state
     node.setState( state );
+    node.setFilteredStateForward( state ) ;
 
   } // end of prediction and filter
 
@@ -179,6 +180,9 @@ StatusCode TrackKalmanFilter::fit( LHCb::Track& track, NodeRange& nodes,
         chisqreverse += node.chi2() ;
         node.setDeltaChi2Backward( node.chi2() );
       }
+
+      // save the filtered state
+      node.setFilteredStateBackward( state ) ;
       
       // save filtered state, but not for the first node. we also
       // don't smooth for the first state.
@@ -278,8 +282,11 @@ StatusCode TrackKalmanFilter::fit( Track& track ) const
       sc = predictReverseFit( *prevNode, *node, state );
       if(!sc.isSuccess()) Warning("Unable to predict reverse fit node", StatusCode::SUCCESS,1).ignore();
       node->setState(state) ;
-      node->setPredictedStateForward( LHCb::State() );
-      node->setPredictedStateBackward( LHCb::State() );
+      node->setPredictedStateBackward( state );
+      node->setFilteredStateBackward( state );
+      state.setCovariance( seedCov ) ;
+      node->setPredictedStateForward( state );
+      node->setFilteredStateForward( state );
       node->setDeltaChi2Forward(0);
       node->setDeltaChi2Backward(0);
       updateResidual(*node) ;
@@ -294,8 +301,11 @@ StatusCode TrackKalmanFilter::fit( Track& track ) const
       sc = predict( *node, state );
       if(!sc.isSuccess()) Warning("Unable to predict reverse fit node", StatusCode::SUCCESS,1).ignore();
       node->setState(state) ;
-      node->setPredictedStateForward( LHCb::State() );
-      node->setPredictedStateBackward( LHCb::State() );
+      node->setPredictedStateForward( state );
+      node->setFilteredStateForward( state );
+      state.setCovariance( seedCov ) ;
+      node->setPredictedStateBackward( state );
+      node->setFilteredStateBackward( state );
       node->setDeltaChi2Forward(0);
       node->setDeltaChi2Backward(0);
       updateResidual(*node) ;

commit 649d8e6d12c9e4e8e118935868eb24497283ad7a

fix bug in TrackKalmanFilter introduced in r52947
git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@53505 4525493e-7705-40b1-a816-d608a930855b

Former-commit-id: d0264667f16f823e5ab73ab7580d7894c3a522dc

Rec/Tr/TrackFitter/doc/release.notes
@@ -2,6 +2,11 @@
 ! Package     : Tr/TrackFitter
 ! Responsible : Wouter Hulsbergen
 !----------------------------------------------------------------------------
+
+! 2010-08-13 - Wouter Hulsbergen
+ - restore TrackKalmanFilter with bug fixed
+
+
 ! 2010-08-07 - Stephanie Hansmann-Menzemer
  - revert changes from WH as they screw up track calo matching
 

Rec/Tr/TrackFitter/src/TrackKalmanFilter.cpp
@@ -123,6 +123,7 @@ StatusCode TrackKalmanFilter::fit( LHCb::Track& track, NodeRange& nodes,
 
     // save filtered state
     node.setState( state );
+    node.setFilteredStateForward( state ) ;
 
   } // end of prediction and filter
 
@@ -179,6 +180,9 @@ StatusCode TrackKalmanFilter::fit( LHCb::Track& track, NodeRange& nodes,
         chisqreverse += node.chi2() ;
         node.setDeltaChi2Backward( node.chi2() );
       }
+
+      // save the filtered state
+      node.setFilteredStateBackward( state ) ;
       
       // save filtered state, but not for the first node. we also
       // don't smooth for the first state.
@@ -278,8 +282,12 @@ StatusCode TrackKalmanFilter::fit( Track& track ) const
       sc = predictReverseFit( *prevNode, *node, state );
       if(!sc.isSuccess()) Warning("Unable to predict reverse fit node", StatusCode::SUCCESS,1).ignore();
       node->setState(state) ;
-      node->setPredictedStateForward( LHCb::State() );
-      node->setPredictedStateBackward( LHCb::State() );
+      node->setPredictedStateBackward( state );
+      node->setFilteredStateBackward( state );
+      LHCb::State revstate = state ;
+      revstate.setCovariance( seedCov ) ;
+      node->setPredictedStateForward( revstate );
+      node->setFilteredStateForward( revstate );
       node->setDeltaChi2Forward(0);
       node->setDeltaChi2Backward(0);
       updateResidual(*node) ;
@@ -294,8 +302,12 @@ StatusCode TrackKalmanFilter::fit( Track& track ) const
       sc = predict( *node, state );
       if(!sc.isSuccess()) Warning("Unable to predict reverse fit node", StatusCode::SUCCESS,1).ignore();
       node->setState(state) ;
-      node->setPredictedStateForward( LHCb::State() );
-      node->setPredictedStateBackward( LHCb::State() );
+      node->setPredictedStateForward( state );
+      node->setFilteredStateForward( state );
+      LHCb::State revstate = state ;
+      revstate.setCovariance( seedCov ) ;
+      node->setPredictedStateBackward( revstate );
+      node->setFilteredStateBackward( revstate );
       node->setDeltaChi2Forward(0);
       node->setDeltaChi2Backward(0);
       updateResidual(*node) ;