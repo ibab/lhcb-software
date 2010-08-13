864cfa75da57d643a17f25fe438a5832dbbe75f5
jpalac <jpalac@cern.ch>
2010-08-13 11:25:05 +0000

    For TisTos.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85342 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 00abb354e8c61b760df73815bb7cb3914066127a

---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneCaloCluster.h
@@ -0,0 +1,25 @@
+// $Id: ICloneCaloCluster.h,v 1.1 2010-08-13 11:25:05 jpalac Exp $
+#ifndef MICRODST_ICLONECALOCLUSTER_H 
+#define MICRODST_ICLONECALOCLUSTER_H 1
+// from MicroDST
+#include "MicroDST/ICloner.h"
+
+// Forward declarations
+namespace LHCb 
+{
+  class CaloCluster;
+}
+
+/** @class ICloneCaloCluster MicroDST/ICloneCaloCluster.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2010-08-13
+ */
+class GAUDI_API ICloneCaloCluster : virtual public MicroDST::ICloner<LHCb::CaloCluster> {
+public: 
+
+  DeclareInterfaceID(ICloneCaloCluster, 1, 0 );
+
+};
+#endif // MICRODST_ICLONECALOCLUSTER_H
---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneCaloDigit.h
@@ -0,0 +1,25 @@
+// $Id: ICloneCaloDigit.h,v 1.1 2010-08-13 11:25:05 jpalac Exp $
+#ifndef MICRODST_ICLONECALODIGIT_H 
+#define MICRODST_ICLONECALODIGIT_H 1
+// from MicroDST
+#include "MicroDST/ICloner.h"
+
+// Forward declarations
+namespace LHCb 
+{
+  class CaloDigit;
+}
+
+/** @class ICloneCaloDigit MicroDST/ICloneCaloDigit.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2010-08-13
+ */
+class GAUDI_API ICloneCaloDigit : virtual public MicroDST::ICloner<LHCb::CaloDigit> {
+public: 
+
+  DeclareInterfaceID(ICloneCaloDigit, 1, 0 );
+
+};
+#endif // MICRODST_ICLONECALODIGIT_H
