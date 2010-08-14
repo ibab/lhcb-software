b8b2f25961b5bd782268f32f9e2d8b606607f746
Angelo Carbone <angelo.carbone@bo.infn.it>
2010-08-14 13:36:09 +0000

    remove high mass lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85377 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f64055a4ad58ab034c769ef2a3f5ca4cb26ae962

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2Charged2Body.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2Charged2Body.py
@@ -18,21 +18,6 @@ PreselB2Charged2Body.DaughtersCuts = { "pi+" : "(PT>700*MeV) & (MIPDV(PRIMARY)>0
 PreselB2Charged2Body.MotherCut = "(M>4800*MeV) & (M<5800*MeV) & (BPVIP()<0.06) & (BPVVDSIGN>1.8) & (INTREE ( (ABSID=='pi+') & (PT>2400*MeV))) & (INTREE ( (ABSID=='pi+') & (MIPDV(PRIMARY)>0.2)))"
 
 
-PreselB2Charged2Body1000 = CombineParticles("PreselB2Charged2Body1000")
-PreselB2Charged2Body1000.InputLocations  = ["StdLoosePions"]
-PreselB2Charged2Body1000.DecayDescriptor = "B0 -> pi+ pi-"
-PreselB2Charged2Body1000.CombinationCut = "(AMAXDOCA('') < 0.15*mm)"
-PreselB2Charged2Body1000.DaughtersCuts = { "pi+" : "(PT>500*MeV) & (MIPDV(PRIMARY)>0.05) & (TRCHI2DOF<5)"}
-PreselB2Charged2Body1000.MotherCut = "(M>1000*MeV) & (BPVIP()<0.08) & (BPVVDSIGN>1.2) & (INTREE ( (ABSID=='pi+') & (PT>1500*MeV))) & (INTREE ( (ABSID=='pi+') & (MIPDV(PRIMARY)>0.12)))"
-
-PreselB2Charged2Body4000 = CombineParticles("PreselB2Charged2Body4000")
-PreselB2Charged2Body4000.InputLocations  = ["StdLoosePions"]
-PreselB2Charged2Body4000.DecayDescriptor = "B0 -> pi+ pi-"
-PreselB2Charged2Body4000.CombinationCut = "(AMAXDOCA('') < 0.15*mm)"
-PreselB2Charged2Body4000.DaughtersCuts = { "pi+" : "(PT>500*MeV) & (MIPDV(PRIMARY)>0.05) & (TRCHI2DOF<5)"}
-PreselB2Charged2Body4000.MotherCut = "(M>4000*MeV) & (BPVIP()<0.1) & (BPVVDSIGN>1.0)"
-
-
 
 
 # Create the line for this selection
@@ -41,12 +26,3 @@ line = StrippingLine('B2Charged2Body'
                      , algos = [ PreselB2Charged2Body ]
                      )
 
-line1 = StrippingLine('B2Charged2Body4000'
-                     , prescale = 1
-                     , algos = [ PreselB2Charged2Body4000 ]
-                     )
-
-line2 = StrippingLine('B2Charged2Body1000'
-                     , prescale = 1
-                      , algos = [ PreselB2Charged2Body1000 ]
-                      )
