f06256e705824bc962531c4c727c445deca1f432
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-08-19 09:42:26 +0000

    New algorithm TimingTuple
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@53633 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3dd05d2b715d922581fbeb470c34e802c385e0c6

---
+++ Rec/Rec/RecAlgs/src/TimingTuple.cpp
@@ -0,0 +1,192 @@
+// $Id: $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+#include "GaudiAlg/ISequencerTimerTool.h"
+
+// local
+#include "TimingTuple.h"
+// from DAQEvent
+#include "Event/RawEvent.h"
+#include "Event/RawBank.h"
+#include "Event/ODIN.h"
+#include "GaudiKernel/Memory.h"
+#include "Event/STCluster.h"
+#include "Event/VeloCluster.h"
+#include "Event/Track.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : TimingTuple
+//
+// 2010-08-18 : Patrick Koppenburg
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( TimingTuple );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+TimingTuple::TimingTuple( const std::string& name,
+                          ISvcLocator* pSvcLocator)
+  : GaudiTupleAlg ( name , pSvcLocator ), 
+    m_timerTool(0),
+    m_timer(0),
+    m_fromRawTool(),
+    m_l0BankDecoder(),
+    m_evtCounter(0),
+    m_richTool()
+{
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+TimingTuple::~TimingTuple() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode TimingTuple::initialize() {
+  StatusCode sc = GaudiTupleAlg::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiTupleAlg
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  m_rawBankDecoder = tool<IOTRawBankDecoder>("OTRawBankDecoder");
+  m_l0BankDecoder = tool<IL0DUFromRawTool>("L0DUFromRawTool");
+  m_richTool = tool<Rich::DAQ::IRawBufferToSmartIDsTool>("Rich::DAQ::RawBufferToSmartIDsTool");
+  m_timerTool = tool<ISequencerTimerTool>( "SequencerTimerTool" ); //global tool
+  m_timer = m_timerTool->addTimer( name() );
+  m_timerTool->start(m_timer) ; /// start it now
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode TimingTuple::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+  m_evtCounter++;
+
+  double t =  m_timerTool->stop(m_timer) ; /// stop
+  if (msgLevel(MSG::DEBUG)) debug() << "Time is " << t  << endmsg ;
+  m_timerTool->start(m_timer) ; /// start again
+
+  Tuple tuple = nTuple( "TimingTuple", "TimingTuple" );
+  LHCb::ODIN* odin = get<LHCb::ODIN> ( LHCb::ODINLocation::Default );
+  fillTuple(tuple,  "EventInSequence",m_evtCounter);
+  fillTuple(tuple,  "RunNumber", odin->runNumber());
+  fillTuple(tuple,  "EvtNumber", (int)odin->eventNumber());
+  fillTuple(tuple,  "Memory", (double)System::virtualMemory());
+  fillTuple(tuple,  "Time", t);
+
+  fillTuple(tuple, "VeloClusters",number<LHCb::VeloClusters>(LHCb::VeloClusterLocation::Default));  
+  fillTuple(tuple, "ITClusters",number<LHCb::STClusters>(LHCb::STClusterLocation::ITClusters));  
+  const unsigned int nHitsInOT = m_rawBankDecoder->totalNumberOfHits();
+  fillTuple(tuple, "OTClusters", nHitsInOT);
+  m_l0BankDecoder->fillDataMap();
+  bool l0BankDecoderOK = m_l0BankDecoder->decodeBank();
+  if(!l0BankDecoderOK ){
+    Error("Readout error : unable to monitor the L0DU rawBank", StatusCode::SUCCESS,StatusCode::SUCCESS).ignore();
+    return StatusCode::SUCCESS;
+  }
+  int nSpd = m_l0BankDecoder->data("Spd(Mult)");
+  fillTuple(tuple, "spdMult", nSpd);
+  
+  const LHCb::Track::Container* tracks =  get<LHCb::Track::Container> ( LHCb::TrackLocation::Default ) ;
+  
+  unsigned int nBack = 0;
+  int veloTracks = 0;
+  
+  if (msgLevel(MSG::VERBOSE)) verbose() << "Number of Tracks " << tracks->size() << endmsg ;
+  
+  // Protection from empty track container 
+  if( tracks!=0 && !tracks->empty() ) {
+    LHCb::Tracks::const_iterator iterT = tracks->begin();
+    for(; iterT != tracks->end() ;++iterT) {
+      if ((*iterT)->checkFlag( LHCb::Track::Backward) == true) ++nBack;
+    } 
+    veloTracks = nVelo(tracks);
+  }
+    
+  fillTuple(tuple, "backwardTracks", nBack);
+  fillTuple(tuple, "veloTracks", veloTracks);
+  fillTuple(tuple, "BestTracks",number<LHCb::Tracks>(LHCb::TrackLocation::Default));
+  fillTuple(tuple, "HPDHits", nRichHits());
+  
+  return tuple->write();
+
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode TimingTuple::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiTupleAlg::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
+// nVelo by Matt, with a protection by PK
+//============================================================================
+unsigned TimingTuple::nVelo(const LHCb::Tracks* tracks) {
+
+  if (msgLevel(MSG::VERBOSE)) verbose() << "nVelo" << endmsg ;
+    std::vector<LHCb::Track*> tmpCont;
+    LHCb::Tracks::const_iterator iterT = tracks->begin();
+    for(; iterT != tracks->end() ;++iterT) {
+      if ((*iterT)->hasVelo() == true) tmpCont.push_back(*iterT); 
+    }
+    if (msgLevel(MSG::VERBOSE)) verbose() << "tmpCont " << tmpCont.size() << endmsg ;
+    if (tmpCont.empty()) return 0;
+    std::vector<LHCb::Track*> keepCont;
+    keepCont.push_back(tmpCont.front());
+    
+    std::vector<LHCb::Track*>::const_iterator iterT2 = tmpCont.begin();
+    for (;iterT2 != tmpCont.end(); ++iterT2 ){
+      const std::vector<LHCb::LHCbID>& vids = (*iterT2)->lhcbIDs();
+      std::vector<LHCb::LHCbID> veloHits; veloHits.reserve(vids.size());
+      LoKi::select(vids.begin(), vids.end(), std::back_inserter(veloHits), boost::bind(&LHCb::LHCbID::isVelo,_1));
+      
+      if (inCloneContainer(keepCont,veloHits) == false){
+        // nothing
+      } 
+      else {
+        keepCont.push_back(*iterT2);   
+      }
+    } // iterT2
+    if (msgLevel(MSG::VERBOSE)) verbose() << "keepCont " << keepCont.size() << endmsg ;
+    return keepCont.size();
+}
+
+//=============================================================================
+// nRichHits by Chris
+//============================================================================
+unsigned int TimingTuple::nRichHits(){
+
+  unsigned int nHits(0);
+  Rich::DAQ::L1Map data  = m_richTool->allRichSmartIDs();
+  for ( Rich::DAQ::L1Map::const_iterator iL1 = data.begin();
+        iL1 != data.end(); ++iL1 ){
+    // loop over ingresses for this L1 board
+    for ( Rich::DAQ::IngressMap::const_iterator iIn = 
+            (*iL1).second.begin();
+          iIn != (*iL1).second.end(); ++iIn )
+    {
+      // Loop over HPDs in this ingress
+      for ( Rich::DAQ::HPDMap::const_iterator iHPD = 
+              (*iIn).second.hpdData().begin();
+            iHPD != (*iIn).second.hpdData().end(); ++iHPD )
+      {
+        nHits += (*iHPD).second.smartIDs().size() ;
+      }
+    }
+  }
+  return nHits;
+}
+
---
+++ Rec/Rec/RecAlgs/src/TimingTuple.h
@@ -0,0 +1,104 @@
+// $Id: $
+#ifndef TIMINGTUPLE_H 
+#define TIMINGTUPLE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiTupleAlg.h"
+#include "OTDAQ/IOTRawBankDecoder.h"
+#include "L0Interfaces/IL0DUFromRawTool.h"
+#include "Kernel/LHCbID.h"
+#include "LoKi/select.h"
+#include <algorithm>
+#include "Event/Track.h"
+#include "RichKernel/IRichRawBufferToSmartIDsTool.h"
+//for velo count 
+#include "boost/bind.hpp"
+
+class ISequencerTimerTool ;
+
+/** @class TimingTuple TimingTuple.h
+ *  
+ *  Fill a Tuple with timing, memory and some event variables
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2010-08-18
+ */
+class TimingTuple : public GaudiTupleAlg {
+public: 
+  /// Standard constructor
+  TimingTuple( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~TimingTuple( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+  unsigned int nVelo(const LHCb::Tracks* tracks); ///< extract and count velo tracks
+  unsigned int nRichHits(); ///< Rich hits
+
+private:
+  ISequencerTimerTool* m_timerTool ; ///< timer tool
+  int m_timer ; ///< timer index
+  const IOTRawBankDecoder* m_rawBankDecoder;
+  std::string m_fromRawTool;
+  IL0DUFromRawTool* m_l0BankDecoder;
+  unsigned int m_evtCounter ;
+  Rich::DAQ::IRawBufferToSmartIDsTool* m_richTool ;
+  
+  void fillTuple(Tuple& tuple, std::string var, int number){ 
+    if (msgLevel(MSG::VERBOSE)) verbose() << "Filling " << var << " with " << number << endmsg ;
+    tuple->column( var , number);
+  }
+  
+  void fillTuple(Tuple& tuple, std::string var, unsigned int number){ 
+    if (msgLevel(MSG::VERBOSE)) verbose() << "Filling " << var << " with " << number << endmsg ;
+    tuple->column( var , number);
+  }
+  
+  void fillTuple(Tuple& tuple, std::string var, double number){ 
+    if (msgLevel(MSG::VERBOSE)) verbose() << "Filling " << var << " with " << number << endmsg ;
+    tuple->column( var , number);
+  }
+  
+
+  template <class TYPE>
+  bool safeExist  ( const std::string& location ) 
+  {
+    DataObject* obj = 0 ;
+    StatusCode sc = evtSvc()->findObject( location , obj ) ;
+    return sc.isSuccess() && 0 != obj && 0 != dynamic_cast<TYPE*>( obj ) ;
+  }
+  /// generic templated method to extract the number of entries in a given location.
+  /// usage int n = number<LHCb::Particles>('/Event/Phys/MyParts/Particles')
+  template<class CLASS> 
+  int number( const std::string& location){
+    if (safeExist<CLASS>(location)){
+      return (get<CLASS>(location))->size() ;
+    } else return -1 ;
+  }
+
+  // velo count from Matthew David Needham
+  inline bool inCloneContainer(const std::vector<LHCb::Track*>& keepCont, 
+                      const std::vector<LHCb::LHCbID>& vids) {
+
+    if (msgLevel(MSG::VERBOSE)) verbose() << "inClone " << vids.size() << endmsg ;
+
+    bool keep = true;
+    std::vector<LHCb::Track*>::const_iterator iter = keepCont.begin();
+    for (;iter != keepCont.end(); ++iter ){
+      const std::vector<LHCb::LHCbID>& vids2 = (*iter)->lhcbIDs(); 
+      std::vector<LHCb::LHCbID> veloHits2; veloHits2.reserve(vids.size());
+      LoKi::select(vids2.begin(), vids2.end(), std::back_inserter(veloHits2),boost::bind(&LHCb::LHCbID::isVelo,_1));
+      if (std::equal(vids.begin(), vids.end(), veloHits2.begin())){
+        keep = false;
+        break;
+      }
+    }
+    return keep;
+  }  
+
+};
+#endif // TIMINGTUPLE_H
