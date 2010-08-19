176065c8b9c936ccf90385032369db35abeb6920
jpalac <jpalac@cern.ch>
2010-08-19 09:18:26 +0000

    - Attemt at genereting dictionaries for Particle2LHCbIDVectorMap.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@66022 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5e9cdfc881fe0b40e26b69597c4d162284d34332

--- Phys/Phys/PhysDict/dict/PhysDict.h
+++ Phys/Phys/PhysDict/dict/PhysDict.h
@@ -1,4 +1,4 @@
-// $Id: PhysDict.h,v 1.10 2010-02-17 14:35:54 jpalac Exp $
+// $Id: PhysDict.h,v 1.11 2010-08-19 09:18:26 jpalac Exp $
 #ifndef DICT_PHYSDICT_H 
 #define DICT_PHYSDICT_H 1
 
@@ -12,6 +12,9 @@
  */
 #include "Event/HltDecReports.h"
 #include "Event/RecVertex.h"
+#include "Event/Particle.h"
+#include "Kernel/LHCbID.h"
+#include "MicroDST/Particle2LHCbIDVectorMap.h"
 namespace 
 {
 
@@ -21,6 +24,8 @@ namespace
     std::vector< std::pair< std::string, LHCb::HltDecReport > > _i2;
     GaudiUtils::VectorMap< std::string, LHCb::HltDecReport >   _i3;
     std::vector<LHCb::RecVertex> _i4;
+    std::vector<LHCb::LHCbID> _i5;
+    GaudiUtils::VectorMap< const LHCb::Particle*, std::vector<LHCb::LHCbID> >   _i6;
   };
   
 }
