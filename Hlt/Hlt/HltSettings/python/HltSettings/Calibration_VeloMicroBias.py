37bd149daeae149f23e2f62bdb92f58b6e9c07bb
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-08-19 10:08:05 +0000

    initial release
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@85486 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fd2fed6a13add5e8534ea529a83d14c3aefd768b

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Calibration_VeloMicroBias.py
@@ -0,0 +1,43 @@
+class Calibration_VeloMicroBias :
+    """
+    Threshold settings for Hlt1 and 2 in nominal conditions (list of commissioning)
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author J. Albrecht
+    @date 2010-08-09
+    """
+    
+    
+    def __init__(self) :
+        self.StripEndSequence = ['']
+
+    def HltType(self) :
+        return 'Calibration_VeloMicroBias'
+
+    def L0TCK(self) :
+        return None
+
+    def ActiveHlt2Lines(self) :
+        """
+        Returns a list of active Hlt2 lines
+        """
+        return  []
+   
+    def ActiveHlt1Lines(self) :
+        """
+        Returns a list of active Hlt1 lines
+        """
+        return [ 'Hlt1MBMicroBiasRZVelo' ]
+
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        from Hlt1Lines.Hlt1MBLines             import Hlt1MBLinesConf
+        return {
+            Hlt1MBLinesConf :      { 'Prescale' : { 'Hlt1MBMicroBias.*' : 1. } 
+                                          , 'Postscale': { 'Hlt1MBMicroBias.*' : 1. }
+                                     }
+        
+            }
