de85e07b0c9b35890bc20253c0676e640faf2166
jpalac <jpalac@cern.ch>
2010-08-25 16:28:03 +0000

    Temporary clone of BestPVAlg, writing out a DaVinci::Map::Particle2VertexBase.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@66040 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 154e757ac5b956aaeb4d57da0aed2bc23a3fae17

---
+++ Phys/Phys/DaVinciTools/src/BestPVAlg2.cpp
@@ -0,0 +1,236 @@
+// $Id: BestPVAlg2.cpp,v 1.1 2010-08-25 16:28:03 jpalac Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+// from LHCb
+#include "Kernel/IOnOffline.h"
+#include "Kernel/IRelatedPVFinder.h"
+#include "Event/RecVertex.h"
+#include "Event/Particle.h"
+#include "Kernel/Particle2Vertex.h"
+#include "Relations/Get.h"
+// DaVinci
+#include "Kernel/DaVinciStringUtils.h"
+// local
+#include "BestPVAlg2.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : BestPVAlg2
+//
+// 2010-07-26 : Juan Palacios
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( BestPVAlg2 );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+BestPVAlg2::BestPVAlg2( const std::string& name,
+                        ISvcLocator* pSvcLocator)
+  : 
+  GaudiAlgorithm ( name , pSvcLocator ),
+  m_PVInputLocation(LHCb::RecVertexLocation::Primary),
+  m_particleInputLocations(),
+  m_P2PVInputLocations(),
+  m_useTables(false),
+  m_OnOffline(0),
+  m_pvRelator(0)
+{
+  declareProperty("PrimaryVertexInputLocation",  m_PVInputLocation);
+  declareProperty("ParticleInputLocations",  m_particleInputLocations);
+  declareProperty("P2PVRelationsInputLocations",  m_P2PVInputLocations);
+  
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+BestPVAlg2::~BestPVAlg2() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode BestPVAlg2::initialize() {
+
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  if (!m_P2PVInputLocations.empty()) m_useTables=true;
+  
+  if ( m_useTables ) { 
+    if ( (!m_particleInputLocations.empty()) ||
+         (m_PVInputLocation!=LHCb::RecVertexLocation::Primary) )  {
+      return Error("You have set P2PVRelationsLocations and one of ParticleInputLocations and PrimaryVertexInputLocation.",
+                   StatusCode::FAILURE);
+    }
+  }
+  
+  m_OnOffline = tool<IOnOffline>("OnOfflineTool",this);
+
+  if (0==m_OnOffline) return Error("Failed to get IOnOffline tool");
+  
+  m_pvRelator = tool<IRelatedPVFinder>(m_OnOffline->relatedPVFinderType(), 
+                                       this);
+ 
+  if (0==m_pvRelator) return Error("Failed to get IRelatedPVFinder tool");
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode BestPVAlg2::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+  if (m_useTables) {
+    this->tablesFromTables();
+  } else {
+    if (exist< LHCb::RecVertex::Range>(m_PVInputLocation) && 
+        !get<LHCb::RecVertex::Range>(m_PVInputLocation).empty()) {
+      this->tables();
+    }    
+  }
+
+  return StatusCode::SUCCESS;
+}
+//=============================================================================
+void BestPVAlg2::tables() const
+{
+
+  typedef LHCb::Particle::Range Particles;
+  typedef LHCb::RecVertex::Range Vertices;
+
+  typedef DaVinci::Map::Particle2VertexBase P2PVMap;
+
+  std::vector<std::string>::const_iterator iLoc = m_particleInputLocations.begin();
+  std::vector<std::string>::const_iterator locEnd = m_particleInputLocations.end();
+
+  for ( ; iLoc != locEnd; ++iLoc) {
+
+    if ( exist< Particles >(*iLoc) &&  
+         ! get< Particles >(*iLoc).empty()) {
+
+      P2PVMap* table = new P2PVMap();
+      const std::string tableLoc = tableLocation(*iLoc);
+      put( table, tableLoc );
+
+      Particles particles = get<Particles>(*iLoc);
+      Vertices vertices = get<Vertices>(m_PVInputLocation);
+
+      for (Particles::const_iterator iPart = particles.begin();
+           iPart != particles.end(); ++ iPart) {
+        const LHCb::VertexBase* vtx = 
+          m_pvRelator->relatedPV(*iPart, 
+                                 LHCb::VertexBase::ConstVector(vertices.begin(), 
+                                                               vertices.end()));
+        table->insert(*iPart, vtx);
+
+      }
+      checkTable(table, tableLoc);
+    }
+    
+  }
+
+}
+
+//=============================================================================
+void BestPVAlg2::tablesFromTables() const
+{
+  typedef LHCb::Particle::ConstVector Particles;
+  typedef LHCb::VertexBase::ConstVector Vertices;
+  typedef LHCb::RecVertex::ConstVector PVs;
+  typedef DaVinci::Map::Particle2VertexBase P2PVMap;
+  typedef Particle2Vertex::Table Table;
+  typedef Particle2Vertex::Table::InvType InvTable;
+ 
+  std::vector<std::string>::const_iterator iLoc = m_P2PVInputLocations.begin();
+  std::vector<std::string>::const_iterator locEnd = m_P2PVInputLocations.end();
+ 
+  for ( ; iLoc != locEnd ; ++iLoc) {
+
+    const Table* inputTable = i_get<Table>(*iLoc);
+
+    if (inputTable) {
+
+      P2PVMap* table = new P2PVMap();
+      const std::string tableLoc = tableLocation(*iLoc);
+      put( table,  tableLoc );
+
+      const InvTable invTable(*inputTable, 1);
+
+      InvTable::Range invRange = invTable.relations();
+
+      Particles particles;
+
+      Relations::getUniqueTo(invRange.begin(), invRange.end(), particles);
+
+      if ( msgLevel(MSG::VERBOSE) ) {
+        verbose() << "tableFromTable found " 
+                  << particles.size() << " Particles" 
+                  << endmsg;
+      }
+  
+      for (Particles::const_iterator iPart = particles.begin();
+           iPart != particles.end(); ++iPart) {
+
+        const Table::Range range = inputTable->relations(*iPart);
+
+        Vertices vertices;
+
+        Relations::getUniqueTo(range.begin(), range.end(), vertices);
+
+        if ( msgLevel(MSG::VERBOSE) ) verbose() << "tableFromTable found " 
+                                                << vertices.size() 
+                                                << " related vertices" << endmsg;
+
+        const LHCb::VertexBase* vtx = m_pvRelator->relatedPV(*iPart, 
+                                                             LHCb::VertexBase::ConstVector(vertices.begin(), 
+                                                                                           vertices.end()));
+
+        table->insert(*iPart, vtx);
+      }
+      checkTable(table, tableLoc);
+    } // if inputTable 
+
+  } // location loop
+  
+
+}
+//=============================================================================
+std::string BestPVAlg2::tableLocation(const std::string& location) const
+{
+  std::string tableLocation(location);
+  DaVinci::StringUtils::removeEnding(tableLocation, "/");
+  return tableLocation + "/" + this->name() + "_P2PV";
+}
+//=============================================================================
+void BestPVAlg2::checkTable(const DaVinci::Map::Particle2VertexBase* table,
+                            const std::string& tableLoc) const
+{
+  
+  if (0!=table) {
+    if ( msgLevel(MSG::VERBOSE) ) {
+      verbose() << "Checking relations table in " 
+                << tableLoc << endmsg;
+      verbose() << "Table size " << table->size() << endmsg;
+    }
+  } else {
+    Error("No table created!",10).ignore();
+  }
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode BestPVAlg2::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+//=============================================================================
---
+++ Phys/Phys/DaVinciTools/src/BestPVAlg2.h
@@ -0,0 +1,64 @@
+// $Id: BestPVAlg2.h,v 1.1 2010-08-25 16:28:03 jpalac Exp $
+#ifndef BESTPVALG2_H 
+#define BESTPVALG2_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+// from DaVinci
+#include "Kernel/Particle2VertexBase.h"
+
+class IOnOffline;
+class IRelatedPVFinder;
+
+/** @class BestPVAlg2 BestPVAlg2.h
+ *  
+ * Temporary test!
+ *
+ *  @author Juan Palacios
+ *  @date   2010-08-25
+ */
+class BestPVAlg2 : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  BestPVAlg2( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~BestPVAlg2( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+private:
+
+  
+
+  template <typename T>
+  inline T* 
+  i_get(const std::string& location) const
+  {
+    return exist<T>(location) ? get<T>( location ) : 0 ;
+  }
+
+  void tables() const;
+  
+  void tablesFromTables() const;
+
+  std::string tableLocation(const std::string& location) const;
+  
+  void checkTable(const DaVinci::Map::Particle2VertexBase* table,
+                  const std::string& tableLoc) const;
+
+private:
+
+  std::string m_PVInputLocation;
+  std::vector<std::string> m_particleInputLocations;
+  std::vector<std::string> m_P2PVInputLocations;
+  bool m_useTables;
+  IOnOffline* m_OnOffline ; ///< context switch tool. To be deprecated.
+  IRelatedPVFinder* m_pvRelator ; ///< Tool that relates the Particle to a PV
+
+
+};
+#endif // PVRELATORALG2_H
