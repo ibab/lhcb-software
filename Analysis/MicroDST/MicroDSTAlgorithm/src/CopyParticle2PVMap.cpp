8b7c9f78ab649f38d995cf4ec93125c379fe24af
jpalac <jpalac@cern.ch>
2010-08-26 17:06:45 +0000

    Copies DaVinci::Map::Particle2VertexBase.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85704 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 04952c2e12f1a8f5fb34aed6def46f3755864380

---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2PVMap.cpp
@@ -0,0 +1,169 @@
+// $Id: CopyParticle2PVMap.cpp,v 1.1 2010-08-26 17:06:44 jpalac Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+#include <GaudiKernel/ObjectVector.h>
+// LHCb 
+#include "Kernel/ITriggerTisTos.h" 
+#include "Event/RecVertex.h"
+#include "Event/Particle.h"
+
+// MicroDST
+#include "MicroDST/ICloneVertexBase.h"
+
+// DaVinci
+#include "Kernel/DaVinciStringUtils.h"
+#include "Kernel/Particle2VertexBase.h"
+// local
+#include "CopyParticle2PVMap.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : CopyParticle2PVMap
+//
+// 2010-08-18 : Juan Palacios
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( CopyParticle2PVMap );
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+CopyParticle2PVMap::CopyParticle2PVMap( const std::string& name,
+                                        ISvcLocator* pSvcLocator)
+  : MicroDSTAlgorithm ( name , pSvcLocator )
+{
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+CopyParticle2PVMap::~CopyParticle2PVMap() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode CopyParticle2PVMap::initialize() {
+  StatusCode sc = MicroDSTAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  m_toCloner = tool<ICloneVertexBase>("VertexBaseFromRecVertexCloner", this);
+
+  return m_toCloner ? StatusCode::SUCCESS : StatusCode::FAILURE;
+
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode CopyParticle2PVMap::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+
+
+  typedef std::vector<std::string>::const_iterator stringIter;
+  stringIter locBegin = this->inputTESLocations().begin();
+  stringIter locEnd = this->inputTESLocations().end();
+
+      
+  for (stringIter iLoc = locBegin; iLoc != locEnd; ++iLoc) {
+    
+    const std::string inputLocation = MicroDST::niceLocationName(*iLoc);
+
+    executeLocation(inputLocation);
+
+  }
+    
+  return StatusCode::SUCCESS;
+
+}
+//=============================================================================
+void CopyParticle2PVMap::executeLocation(const std::string& inputLocation) 
+{
+
+  typedef DaVinci::Map::Particle2VertexBase Map;
+
+  const std::string outputLocation = 
+    this->outputTESLocation( inputLocation );
+  
+  if ( msgLevel(MSG::VERBOSE) ) {
+    verbose() << "Going to clone relations from " 
+              << inputLocation
+              << " into " << outputLocation << endmsg;
+  }
+
+  if (exist<Map>(outputLocation) )
+  {
+    this->Warning("Object "+ outputLocation + " already exists. Not cloning.", 
+                  StatusCode::SUCCESS, 0).ignore() ;
+    return;
+  }
+
+  if (exist<Map>(inputLocation) )
+  {
+    if ( msgLevel(MSG::VERBOSE) ) {
+      verbose() << "Retrieving relations table from " 
+                << inputLocation << endmsg;
+    }
+    const Map* p2pv = get<Map>(inputLocation);
+    if ( p2pv && !p2pv->empty() ) {
+          
+      if ( msgLevel(MSG::VERBOSE) ) {
+        verbose() << "found P->PV map with "<< p2pv->size() 
+                  << " entries!" << endmsg;
+      }
+      Map* p2pvClone = new Map();
+      Map::const_iterator iP2PV = p2pv->begin();
+      Map::const_iterator iP2PVEnd = p2pv->end();
+
+      for (; iP2PV != iP2PVEnd; ++ iP2PV) {
+        const LHCb::Particle* part = (*iP2PV).first;
+        const LHCb::VertexBase* vtx = (*iP2PV).second;
+        if (part && vtx) {
+          const LHCb::VertexBase* cloneVtx = (*m_toCloner)(vtx);
+          const LHCb::Particle* clonePart = getStoredClone<LHCb::Particle>(part);
+          if (cloneVtx && clonePart) {
+            p2pvClone->insert(clonePart, cloneVtx);
+          }
+        }
+      }
+      
+      if ( msgLevel(MSG::VERBOSE) ) {
+        verbose() << "Going to store relations table from " 
+                  << inputLocation
+                  << " into " << outputLocation << endmsg;
+        verbose() << "Number of relations in cloned table: "
+                  << p2pvClone->size() << endmsg;
+      }
+      if (!p2pvClone->empty()) {
+        put( p2pvClone, outputLocation );
+      }
+    } else {
+      if ( msgLevel(MSG::VERBOSE) ) {
+        this->Warning("Found no table at "+inputLocation,
+                      StatusCode::FAILURE).ignore();  
+      }
+    }
+        
+  } else {
+    if ( msgLevel(MSG::VERBOSE) ) {
+      this->Warning("Found no table at "+inputLocation,
+                    StatusCode::FAILURE).ignore();
+    }
+  }
+  
+}
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode CopyParticle2PVMap::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2PVMap.h
@@ -0,0 +1,37 @@
+// $Id: CopyParticle2PVMap.h,v 1.1 2010-08-26 17:06:45 jpalac Exp $
+#ifndef COPYPARTICLE2PVMAP_H 
+#define COPYPARTICLE2PVMAP_H 1
+
+// Include files
+// from Gaudi
+#include "MicroDST/MicroDSTAlgorithm.h"
+
+class ICloneVertexBase;
+
+/** @class CopyParticle2PVMap CopyParticle2PVMap.h
+ *  
+ *
+ *  @author Juan Palacios
+ *  @date   2010-08-26
+ */
+class CopyParticle2PVMap : public MicroDSTAlgorithm {
+public: 
+  /// Standard constructor
+  CopyParticle2PVMap( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~CopyParticle2PVMap( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+private:
+
+  void executeLocation(const std::string& particleLocation);
+
+private:
+
+  ICloneVertexBase* m_toCloner;
+
+};
+#endif // COPYPARTICLE2PVMAP_H
