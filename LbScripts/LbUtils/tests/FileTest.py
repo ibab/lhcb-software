90a64cc4c9b6f4c443ad254602b15fdb8aa384c9
hmdegaud <hmdegaud@cern.ch>
2010-08-30 11:32:40 +0000

    added test for the double quoting in the template file.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@54130 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d8da9e152a8a2b5d43816729eb60f83cd1e70224

--- LbScripts/LbUtils/tests/FileTest.py
+++ LbScripts/LbUtils/tests/FileTest.py
@@ -33,6 +33,16 @@ class FileTest(unittest.TestCase):
         genTemplateFile(t.name, tmpl_dict, "myfile.txt")
         self.assertEqual(open("myfile.txt").read(),"wdht ddd blah\n" )
         os.remove("myfile.txt")
+    def testTemplate3(self):
+        t = TempFile()
+        fdt = open(t.name, "w")
+        fdt.write("""wdht ddd "%(tobereplaced)s"\n""")
+        fdt.close()
+        tmpl_dict = {}
+        tmpl_dict["tobereplaced"] = "blah"
+        genTemplateFile(t.name, tmpl_dict, "myfile.txt")
+        self.assertEqual(open("myfile.txt").read(),"""wdht ddd "blah"\n""" )
+        os.remove("myfile.txt")
         
 if __name__ == '__main__':
     unittest.main()
