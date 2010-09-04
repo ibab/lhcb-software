ddc9fed5973842ce1aaa963a06621f7e4330013a
Plamen Hristov Hopchev <plamen.hristov.hopchev@cern.ch>
2010-09-04 15:01:01 +0000

    Add a prescale for StrippingBeamGas
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85873 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fc34bc5a50b1320b521e7b2e97a8be102e19f03c

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBeamGas.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBeamGas.py
@@ -5,7 +5,7 @@ from StrippingConf.StrippingLine import StrippingLine
 # The Hlt requirement coincides with the definition of the BeamGas routing bit
 #-----------------------------------------------------------------------------
 lineUnbiased = StrippingLine( "BeamGasUnbiased"
-                            , prescale = 1
+                            , prescale = 0.05
                             , HLT = "HLT_PASS_SUBSTR('Hlt1BeamGas')"
                             , checkPV = False
                             , postscale = 1
