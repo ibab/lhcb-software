113c9364a5a14f7638600887a433d0883c1236a5
Stefano Perazzini <stefano.perazzini@cern.ch>
2010-09-04 14:07:53 +0000

    tuning of StrippingB2Charged2Body for CPU time consuming
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85869 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1289c3cc4f7cca07c1582230c7cc9f3823076c24

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2Charged2Body.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingB2Charged2Body.py
@@ -3,7 +3,8 @@
 # Options for pre-selecting B->hh' and Bs->mu+ mu- events
 #
 # @author Angelo Carbone
-# @date 2010-05-21
+# @rivisited by Stefano Perazzini
+# @date 2010-00-04
 #
 ########################################################################                   
 
@@ -13,12 +14,8 @@ from StrippingConf.StrippingLine import StrippingLine
 PreselB2Charged2Body = CombineParticles("PreselB2Charged2Body")
 PreselB2Charged2Body.InputLocations  = ["StdLoosePions"]
 PreselB2Charged2Body.DecayDescriptor = "B0 -> pi+ pi-"
-PreselB2Charged2Body.CombinationCut = "(AMAXDOCA('') < 0.08*mm)"
-PreselB2Charged2Body.DaughtersCuts = { "pi+" : "(PT>700*MeV) & (MIPDV(PRIMARY)>0.08) & (TRCHI2DOF<5)"}
-PreselB2Charged2Body.MotherCut = "(M>4800*MeV) & (M<5800*MeV) & (BPVIP()<0.06) & (BPVVDSIGN>1.8) & (INTREE ( (ABSID=='pi+') & (PT>2400*MeV))) & (INTREE ( (ABSID=='pi+') & (MIPDV(PRIMARY)>0.2)))"
-
-
-
+PreselB2Charged2Body.CombinationCut = "(AM > 4500*MeV) & (AM < 6500*MeV) & (AMAXDOCA('') < 0.08*mm) & (AMAXCHILD(MAXTREE('pi+'==ABSID,PT))>2400) & (AMAXCHILD(MAXTREE('pi+'==ABSID,MIPDV(PRIMARY)))>0.2)"
+PreselB2Charged2Body.MotherCut = "(M>4800*MeV) & (M<5800*MeV) & (BPVIP()<0.06) & (BPVVDSIGN>1.8)"
 
 # Create the line for this selection
 line = StrippingLine('B2Charged2Body'
