5ef73245ce27d1f935449f91fbe9f983eea7683b
Rafael Silva Coutinho <rafael.silva.coutinho@cern.ch>
2010-09-05 22:10:36 +0000

    Updated Line for better CPU Timing
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@85913 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1337ba77bc4eb0ad91f5a8bb9dde1996a2f7c85a

--- Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBu2hhh.py
+++ Analysis/Phys/StrippingSelections/python/StrippingSelections/StrippingBu2hhh.py
@@ -3,7 +3,7 @@
 # Options for pre-selecting B->hhh' events
 #
 # @author Jussara  Miranda, Alvaro Gomes, Rafael Coutinho
-# @date 2010-07-26
+# @date 2010-09-05
 #
 ########################################################################                   
 
@@ -26,8 +26,8 @@ StripBu2hhh = CombineParticles("StripBu2hhh")
 StripBu2hhh.InputLocations  = ["StdLoosePions"]
 StripBu2hhh.DecayDescriptor = "[B+ -> pi+ pi+ pi-]cc"
 
-StripBu2hhh.CombinationCut = "(AM<6000*MeV) & (AM>4000*MeV) & ((APT1+APT2+APT3)>4500*MeV) & (AVAL_MAX(MIPDV(PRIMARY), PT)>0.05) & (ANUM(PT>900*MeV)>=2) & ((ACHILD(TRCHI2DOF,1)+ACHILD(TRCHI2DOF,2)+ACHILD(TRCHI2DOF,3))<4) & ((ACHILD(MIPCHI2DV(PRIMARY),1)+ACHILD(MIPCHI2DV(PRIMARY),2)+ACHILD(MIPCHI2DV(PRIMARY),3))>200)&(AMAXDOCA('LoKi::TrgDistanceCalculator') < .3)"
-StripBu2hhh.DaughtersCuts = {"pi+" : "(MIPDV(PRIMARY)>0.005)"}
+StripBu2hhh.CombinationCut = "(AM<6000*MeV) & (AM>4000*MeV) & ((APT1+APT2+APT3)>4500*MeV) & (AVAL_MAX(MIPDV(PRIMARY), PT)>0.05) & (ANUM(PT>900*MeV)>=2) & ((ACHILD(MIPCHI2DV(PRIMARY),1)+ACHILD(MIPCHI2DV(PRIMARY),2)+ACHILD(MIPCHI2DV(PRIMARY),3))>200)&(AMAXDOCA('LoKi::TrgDistanceCalculator') < .3)"
+StripBu2hhh.DaughtersCuts = {"pi+" : "(TRCHI2DOF<8)"}
 StripBu2hhh.MotherCut = "(BPVDIRA>0.999) & (VFASPF(VMINVDDV(PRIMARY))>3) & (VFASPF(VCHI2)<8) & (MAXTREE((('pi+'==ABSID)) ,PT)>1500*MeV) & (MIPDV(PRIMARY)<0.04) & (PT>1000*MeV) & (BPVTRGPOINTINGWPT<.1)"
 
 # Create the line for this selection
