870647b3d9abc2c319778187ee7df02127000534
phunt <phunt@cern.ch>
2010-09-07 17:01:14 +0000

    Removing Phys/Bu2D0X_D02HHHH from incorrect package
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@66341 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 55387d5c5e9adbfbe149c693c27f8499f1d16445

--- Analysis/Phys/Bu2D0X_D02HHHH/options/DV-FilterInBeamCrossing.py
+++
@@ -1,8 +0,0 @@
-from Gaudi.Configuration import *
-from Configurables import DaVinci
-from Configurables import LoKi__ODINFilter  as ODINFilter
-odinFilter=ODINFilter( 'FilterInBeamCrossing'
-                       , Code = " ODIN_BXTYP == LHCb.ODIN.BeamCrossing " )
-#DaVinci().appendToMainSequence(odinFilter)
-useralgs=DaVinci().UserAlgorithms
-DaVinci().UserAlgorithms=[odinFilter]+useralgs
