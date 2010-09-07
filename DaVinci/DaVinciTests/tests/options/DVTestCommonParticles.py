eea73a5cad47f9613b278cf15f6b2df1eedaca1b
jpalac <jpalac@cern.ch>
2010-09-07 14:41:42 +0000

    Use real data.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@66324 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d48340d04cb8b43b55c1a1a2bbd85cea77e77ab7

--- DaVinci/DaVinciTests/tests/options/DVTestCommonParticles.py
+++ DaVinci/DaVinciTests/tests/options/DVTestCommonParticles.py
@@ -38,10 +38,16 @@ DaVinci().HistogramFile = "DVMonitors.root"
 # DaVinci().UserAlgorithms = List   # explicitly run them
 DaVinci().UserAlgorithms = [ CP ]   # count them all
 DaVinci().EvtMax = 1000
-DaVinci().DataType = "MC09" # Default is "DC06"
-DaVinci().Simulation = True
-DaVinci().InputType = "RDST"
+DaVinci().DataType = "2010" # Default is "DC06"
+DaVinci().Simulation = False
+DaVinci().InputType = "DST"
 EventSelector().Input   = [
-    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/data/2009/RDST/00004937/0000/00004937_00000001_1.rdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00000996_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00001004_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00001008_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00001009_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+
+#EventSelector().Input   = [
+#    "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/data/2009/RDST/00004937/0000/00004937_00000001_1.rdst' TYP='POOL_ROOTTREE' OPT='READ'"]
     
 MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
