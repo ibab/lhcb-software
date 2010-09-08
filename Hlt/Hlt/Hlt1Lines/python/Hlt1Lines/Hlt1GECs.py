9e25f36a561a2cfc0046db5ff66cf15515d220dd
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-09-08 14:08:47 +0000

    revert changes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@86068 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 66a7e5d00d5e1e95ea4d4395aa2e5c1b5e568f05

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
@@ -1,11 +1,11 @@
 
-def Hlt1GEC( reject = True, nMaxVeloHits = 3000 ):
+def Hlt1GEC( reject = True ):
     from Configurables import Hlt__GEC
     from HltLine.HltDecodeRaw import DecodeIT,DecodeVELO
     from HltLine.HltLine import bindMembers, Hlt1Tool
     from Configurables import LoKi__VoidFilter as VoidFilter
     
-    tool = Hlt1Tool( Hlt__GEC, "ActualGECs", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = nMaxVeloHits )
+    tool = Hlt1Tool( Hlt__GEC, "ActualGECs", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 3000 )
 
     rej  = VoidFilter("Hlt1GECReject")
     rej.Code = "ACCEPT('%s')" % tool.createConfigurable(rej).getFullName()
@@ -14,6 +14,6 @@ def Hlt1GEC( reject = True, nMaxVeloHits = 3000 ):
     acc.Code = "~ ACCEPT('%s')" % tool.createConfigurable(acc).getFullName()
 
     return bindMembers(None, [ DecodeIT, DecodeVELO, rej if reject else acc  ] )
-    
+
 
     
