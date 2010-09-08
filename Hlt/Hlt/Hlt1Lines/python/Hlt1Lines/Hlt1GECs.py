82cfc413e2c5e223c4fb39a3a5b201fceb049ab7
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-09-08 14:13:33 +0000

    add possibility to do loose cuts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@86070 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 08685b841e35ccde458483a52ff0cf968be74f40

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
@@ -17,3 +17,20 @@ def Hlt1GEC( reject = True ):
 
 
     
+def Hlt1GECLoose( reject = True ):
+    from Configurables import Hlt__GEC
+    from HltLine.HltDecodeRaw import DecodeIT,DecodeVELO
+    from HltLine.HltLine import bindMembers, Hlt1Tool
+    from Configurables import LoKi__VoidFilter as VoidFilter
+    
+    tool = Hlt1Tool( Hlt__GEC, "ActualGECsLoose", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 4000 )
+
+    rej  = VoidFilter("Hlt1GECRejectLoose")
+    rej.Code = "ACCEPT('%s')" % tool.createConfigurable(rej).getFullName()
+
+    acc  = VoidFilter("Hlt1GECAcceptLoose")
+    acc.Code = "~ ACCEPT('%s')" % tool.createConfigurable(acc).getFullName()
+
+    return bindMembers(None, [ DecodeIT, DecodeVELO, rej if reject else acc  ] )
+    
+
