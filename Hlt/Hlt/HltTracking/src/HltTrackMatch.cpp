12fd6cd5e694f2d03ca325ca18b0a643e69433a2
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-09-08 12:55:25 +0000

    change matching such that there are no hidden properties
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@86053 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6cfdb66caa56ed298bafb142dad7559f162ee17a

--- Hlt/Hlt/HltTracking/src/HltTrackMatch.cpp
+++ Hlt/Hlt/HltTracking/src/HltTrackMatch.cpp
@@ -1,4 +1,4 @@
-// $Id: HltTrackMatch.cpp,v 1.22 2010-08-17 08:41:18 graven Exp $
+// $Id: HltTrackMatch.cpp,v 1.23 2010-09-08 12:55:25 graven Exp $
 // Include files 
 
 // from Gaudi
@@ -29,23 +29,29 @@ HltTrackMatch::HltTrackMatch( const std::string& name,
   : HltAlgorithm ( name , pSvcLocator )
   , m_selections(*this)
   , m_tool(0)
-  , m_qualityID(0)
-  , m_quality2ID(0)
   , m_qualityHisto(0)
   , m_quality2Histo(0)
   , m_qualityHistoBest(0)
   , m_quality2HistoBest(0)
+  , m_qualityID(0)
+  , m_quality2ID(0)
 {
-  declareProperty("MatchName",m_matchName = "");
   
   declareProperty("MaxQuality",m_maxQuality = 1e6);
   declareProperty("MaxQuality2",m_maxQuality2 = 1e6);
+  declareProperty("Tool", m_toolName);
+  declareProperty("RecoID",m_recoID); 
+  declareProperty("TESOutput",  m_TESOutput);
+  declareProperty("TrackType",  m_trackType);
+  declareProperty("TransferIDs",  m_transferIDs);
+  declareProperty("TransferAncestor",  m_transferAncestor);
+  declareProperty("TransferInfo",  m_transferInfo);
+  declareProperty("Quality",  m_qualityName = "<UNKNOWN>");
+  declareProperty("Quality2",  m_quality2Name  = "<UNKNOWN>");
+
 
   m_selections.declareProperties();
 
-  m_TESOutput = "";
-  m_qualityName = "UNKNOWN";
-  m_quality2Name = "UNKNOWN2";
 }
 //=============================================================================
 // Destructor
@@ -63,12 +69,26 @@ StatusCode HltTrackMatch::initialize() {
 
   m_selections.retrieveSelections();
   m_selections.registerSelection();
-  
-  recoConfiguration();
-  
-  sc = setReco(m_matchName);
-  if ( sc.isFailure() ) return sc;
 
+  m_qualityID = 0;
+  if (!m_qualityName.empty()) m_qualityID = hltInfoID(m_qualityName);
+  m_quality2ID = 0;
+  if (!m_quality2Name.empty()) m_quality2ID = hltInfoID(m_quality2Name);
+  
+  info() << " Reco: " << name() 
+         << " Tool: " << m_toolName
+         << " Output: " << m_TESOutput << endmsg;
+
+  debug() << " reco ID " << m_recoID
+          << " transfers IDs " << m_transferIDs 
+          << " transfer ancestor " << m_transferAncestor
+          << " track type " << m_trackType 
+          << " quality [1] " << m_qualityName << " " << m_qualityID
+          << " quality [2] " << m_quality2Name << " " << m_quality2ID
+          << endmsg;
+  
+  m_tool = tool<ITrackMatch>(m_toolName,this);
+  
   if (produceHistos()){
     m_qualityHisto = initializeHisto(m_qualityName,-50,50,1000);
     m_qualityHistoBest = initializeHisto(m_qualityName+"Best",-50,50,1000);
@@ -82,80 +102,6 @@ StatusCode HltTrackMatch::initialize() {
   return StatusCode::SUCCESS;
 }
 
-void HltTrackMatch::recoConfiguration() {
-  // configure
-  m_recoConf.add("VeloCalo/Tool",std::string("HltVeloTCaloMatch"));
-  m_recoConf.add("VeloCalo/RecoID", (int) 100); //TODO
-  m_recoConf.add("VeloCalo/TransferIDs",true);
-  m_recoConf.add("VeloCalo/TransferAncestor",true);
-  m_recoConf.add("VeloCalo/TransferInfo",false);
-  m_recoConf.add("VeloCalo/TrackType", (int) LHCb::Track::Velo);
-  m_recoConf.add("VeloCalo/TESOutput", std::string("Hlt1/Track/VeloCalo"));
-  m_recoConf.add("VeloCalo/Quality",std::string("Chi2Y"));
-  m_recoConf.add("VeloCalo/Quality2",std::string("Chi2X"));
-  
-  m_recoConf.add("VeloTDist/Tool",std::string("HltMatchTVeloTracks"));
-  m_recoConf.add("VeloTDist/RecoID", (int) 101); //TODO
-  m_recoConf.add("VeloTDist/TransferIDs",true);
-  m_recoConf.add("VeloTDist/TransferAncestor",true);
-  m_recoConf.add("VeloTDist/TransferInfo",false);
-  m_recoConf.add("VeloTDist/TrackType", (int) LHCb::Track::Long);
-  m_recoConf.add("VeloTDist/TESOutput", std::string("Hlt1/Track/VeloTDist"));
-  m_recoConf.add("VeloTDist/Quality",std::string("deltaX"));
-  m_recoConf.add("VeloTDist/Quality2",std::string("deltaY"));
-
-  m_recoConf.add("VeloT/Tool",std::string("PatMatchTool"));
-  m_recoConf.add("VeloT/RecoID", (int) 102); //TODO
-  m_recoConf.add("VeloT/TransferIDs",true);
-  m_recoConf.add("VeloT/TransferAncestor",true);
-  m_recoConf.add("VeloT/TransferInfo",true);
-  m_recoConf.add("VeloT/TrackType", (int) LHCb::Track::Long);
-  m_recoConf.add("VeloT/TESOutput", std::string("Hlt1/Track/VeloT")); //TODO
-  m_recoConf.add("VeloT/Quality",std::string("chi2_PatMatch"));
-  m_recoConf.add("VeloT/Quality2",std::string(""));
-}
-
-
-StatusCode HltTrackMatch::setReco(const std::string& key) 
-{
-  m_matchName = key;
-  debug() << " InitReco " << endmsg;
-  bool ok = m_recoConf.has_key(m_matchName+"/Tool");
-  Assert(ok," no reconstruction with name "+m_matchName);
-  
-  std::string toolName = m_recoConf.retrieve<std::string>(m_matchName+"/Tool");
-  m_recoID   = m_recoConf.retrieve<int>(m_matchName+"/RecoID");
-  m_TESOutput = m_recoConf.retrieve<std::string>(m_matchName+"/TESOutput");
-  m_trackType = m_recoConf.retrieve<int>(m_matchName+"/TrackType");
-  m_transferIDs = m_recoConf.retrieve<bool>(m_matchName+"/TransferIDs");
-  m_transferAncestor = 
-    m_recoConf.retrieve<bool>(m_matchName+"/TransferAncestor");
-  m_transferInfo = 
-    m_recoConf.retrieve<bool>(m_matchName+"/TransferInfo");
-
-  m_qualityName = m_recoConf.retrieve<std::string>(m_matchName+"/Quality");
-  m_qualityID = 0;
-  if (!m_qualityName.empty()) m_qualityID = hltInfoID(m_qualityName);
-  m_quality2Name = m_recoConf.retrieve<std::string>(m_matchName+"/Quality2");
-  m_quality2ID = 0;
-  if (!m_quality2Name.empty()) m_quality2ID = hltInfoID(m_quality2Name);
-  
-  info() << " Reco: " << m_matchName 
-         << " Tool: " << toolName
-         << " Output: " << m_TESOutput << endmsg;
-
-  debug() << " reco ID " << m_recoID
-          << " transfers IDs " << m_transferIDs 
-          << " transfer ancestor " << m_transferAncestor
-          << " track type " << m_trackType 
-          << " quality [1] " << m_qualityName << " " << m_qualityID
-          << " quality [2] " << m_quality2Name << " " << m_quality2ID
-          << endmsg;
-  
-  m_tool = tool<ITrackMatch>(toolName,this);
-
-  return StatusCode::SUCCESS;
-}
 
 
 //=============================================================================
@@ -166,8 +112,12 @@ StatusCode HltTrackMatch::execute() {
   debug() << "==> Execute" << endmsg;
   StatusCode sc = StatusCode::SUCCESS;
 
-  Tracks* otracks = 
-    getOrCreate<LHCb::Tracks,LHCb::Tracks>(m_TESOutput);
+  //TODO: must move this into a layer of public tool, like is done in TrackUpgrade...
+  //      in order to guarantee that there is a single (tool) instance corresponding
+  //      to each TES location
+  // Fortunately, things are not so bad here, as the TES is not used to cache information,
+  // just to dump/transfer ownership into....
+  Tracks* otracks = getOrCreate<LHCb::Tracks,LHCb::Tracks>(m_TESOutput);
 
   BOOST_FOREACH(const LHCb::Track* track1, *m_selections.input<1>()) {
     if (msgLevel(MSG::VERBOSE)) printInfo(" track [1] ",*track1);
--- Hlt/Hlt/HltTracking/src/HltTrackMatch.h
+++ Hlt/Hlt/HltTracking/src/HltTrackMatch.h
@@ -1,10 +1,9 @@
-// $Id: HltTrackMatch.h,v 1.8 2009-12-23 10:50:34 graven Exp $
+// $Id: HltTrackMatch.h,v 1.9 2010-09-08 12:55:25 graven Exp $
 #ifndef HLTTRACKING_HLTTRACKMATCH_H 
 #define HLTTRACKING_HLTTRACKMATCH_H 1
 
 // Include files
 // from Gaudi
-#include "EDictionary.h"
 #include "HltBase/HltSelectionContainer.h"
 #include "HltBase/HltAlgorithm.h"
 #include "TrackInterfaces/ITrackMatch.h"
@@ -29,51 +28,33 @@ public:
 
 private:
 
-  void recoConfiguration();
-
-  StatusCode setReco(const std::string& recoName);
-
-
   // 1 output: track
   // 2 inputs: track, track
   // hence: container3<track,track,track> (output, first input, second input)
   Hlt::SelectionContainer3<LHCb::Track,LHCb::Track,LHCb::Track> m_selections;
 
-  std::string m_matchName;
   ITrackMatch* m_tool;
+  AIDA::IHistogram1D* m_qualityHisto;
+  AIDA::IHistogram1D* m_quality2Histo;
+  AIDA::IHistogram1D* m_qualityHistoBest;
+  AIDA::IHistogram1D* m_quality2HistoBest;
 
   std::string m_TESOutput;
+  std::string m_toolName;
+  std::string m_qualityName;
+  std::string m_quality2Name;
   
   double m_maxQuality;
   double m_maxQuality2;
 
-  template <class T>
-  void recoregister(const std::string& name, const std::string& prop,
-                    const T& t) {
-    std::string key = name+"/"+prop;m_recoConf.add(key,t);
-    info() << " reco ["<<key<<"] = " << t <<endreq;
-  }
-
-  zen::dictionary m_recoConf;
-
-
-  std::string m_qualityName;
-  std::string m_quality2Name;
-  
   int m_qualityID;
   int m_quality2ID;
-
   int m_recoID;
-  bool m_owner;
   int m_trackType;
 
   bool m_transferInfo;
   bool m_transferIDs;
   bool m_transferAncestor;
 
-  AIDA::IHistogram1D* m_qualityHisto;
-  AIDA::IHistogram1D* m_quality2Histo;
-  AIDA::IHistogram1D* m_qualityHistoBest;
-  AIDA::IHistogram1D* m_quality2HistoBest;
 };
 #endif // HLTTRACKMATCH_H
