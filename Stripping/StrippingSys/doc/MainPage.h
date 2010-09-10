5e8cf43e26f059e90194d3b2685d57df515f2487
rlambert <rlambert@cern.ch>
2010-09-10 10:19:53 +0000

    first import of StrippingSys under Stripping
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@66447 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 13512d4d00d24818c3475ce8829d1f1d5713cfa3

---
+++ Stripping/StrippingSys/doc/MainPage.h
@@ -0,0 +1,24 @@
+/** \mainpage notitle
+ *  \anchor strippingsysdoxygenmain
+ *
+ * This is the code reference manual for the Physics Stripping classes.
+
+ * These pages have been generated directly from the code and reflect the exact 
+ * state of the software for this version of the StrippingSys packages. More
+ * information is available from the 
+ * <a href="http://cern.ch/lhcb-release-area/DOC/davinci/">web pages</a>
+ * of the DaVinci project
+ *
+ * \sa
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation"
+ * \li \ref gaudidoxygenmain "Gaudi documentation"
+ * \li \ref externaldocs     "Related external libraries"
+ * \li <a href="https://twiki.cern.ch/twiki/bin/view/LHCb/DaVinci">DaVinci wiki page</a>
+ 
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+ * <b>Additional information:</b>
+ * \li <a href="../release.notes"><b>Release notes history</b></a>
+ * \li <a href="http://cern.ch/lhcb-release-area/DOC/stripping/"><b>Stripping project Web pages</b></a><p>
+ */
