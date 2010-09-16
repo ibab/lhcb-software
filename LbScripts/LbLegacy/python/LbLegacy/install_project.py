e4e884a97a8884f72c6fc2422ce28ce57a4d381a
hmdegaud <hmdegaud@cern.ch>
2010-09-16 11:33:32 +0000

    release of LbScripts v5r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@86274 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b1d04d1baa376c69c524feed406de60f67c4f83

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -14,10 +14,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from shutil import rmtree
 
-script_version = '100823'
+script_version = '100916'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v5r4p1"
+lbscripts_version = "v5r5"
 compat_version = None
 #-----------------------------------------------------------------------------------
 
