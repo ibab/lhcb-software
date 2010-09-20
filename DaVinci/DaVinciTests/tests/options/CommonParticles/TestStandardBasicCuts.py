731795791af07bc575504bd1852a985917c88e86
jpalac <jpalac@cern.ch>
2010-09-20 06:59:21 +0000

    New tests for cut validity.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@86343 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1051fde67a2d1f36a7a8aad372668d8a718c25ce

---
+++ DaVinci/DaVinciTests/tests/options/CommonParticles/TestStandardBasicCuts.py
@@ -0,0 +1,30 @@
+"""Test validisy of CommonParticles.StandardBasic cuts.
+
+   syntax: gaudirun.py $DAVINCIMONITORSROOT/options/TestStandardBasicCuts.py
+
+   Author: Juan Palacios <palacios@physik.uzh.ch>
+
+"""
+##############################################################################
+from DaVinci.Configuration import DaVinci
+from Gaudi.Configuration import *
+#
+from CommonParticles import StandardBasic
+
+List = []
+
+for a,b in StandardBasic.locations.iteritems():
+    print "DVTestCommonParticles adding location", a
+    List.append(b)                      # ist of algorithms
+
+DaVinci( UserAlgorithms = List,
+         EvtMax = 1,
+         DataType = "2010",
+         Simulation = False,
+         InputType = "DST"       )
+
+EventSelector().Input   = [
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00000996_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'"
+    ]
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
---
+++ DaVinci/DaVinciTests/tests/options/CommonParticles/TestStandardIntermetiateCuts.py
@@ -0,0 +1,31 @@
+"""Test validisy of CommonParticles.StandardIntermediate cuts.
+
+   syntax: gaudirun.py $DAVINCIMONITORSROOT/options/TestStandardIntermediateCuts.py
+
+   Author: Juan Palacios <palacios@physik.uzh.ch>
+
+"""
+##############################################################################
+from DaVinci.Configuration import DaVinci
+from Gaudi.Configuration import *
+#
+from CommonParticles import StandardBasic
+from CommonParticles import StandardIntermediate
+
+List = []
+
+for a,b in StandardIntermediate.locations.iteritems():
+    print "DVTestCommonParticles adding location", a
+    List.append(b)                      # ist of algorithms
+
+DaVinci( UserAlgorithms = List,
+         EvtMax = 1,
+         DataType = "2010",
+         Simulation = False,
+         InputType = "DST"       )
+
+EventSelector().Input   = [
+    "   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007557/0000/00007557_00000996_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'"
+    ]
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
