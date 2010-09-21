aa8fdb11973b1ebbfd13d7061c583a08df29afb8
jpalac <jpalac@cern.ch>
2010-09-21 16:23:02 +0000

    First import of DaVinciUtils, light-weight package with DaVinci-independent functions, guards, functors that only know about Gaudi stuff.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@86442 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 47f9b6e3167d5668fa327c92270508a26815a0eb

---
+++ Phys/Phys/DaVinciUtils/DaVinciUtils/Functions.h
@@ -0,0 +1,30 @@
+#ifndef DAVINCIUTILS_FUNCTIONS_H 
+#define DAVINCIUTILS_FUNCTIONS_H 1
+
+/** @namespace DaVinci::Utils DaVinciUtils/Functions.h
+ *  
+ *  General helper functions for use in DaVinci.
+ *  These should only know about Gaudi classes and patterns.
+ *  No knowledge of DaVinci specific code should be assumed.
+ * 
+ *  @author Juan PALACIOS palacios@uzh.uzh.ch
+ *  @date   2010-09-21
+ */
+
+namespace DaVinci 
+{
+  
+namespace Utils
+{
+
+  ///  Is obj in TES
+  template <class T>
+  inline bool inTES(const T* obj) {
+    return ( 0!=oby && 0!=obj->parent()) ;
+  }
+
+} // namespace Utils
+  
+} // namespace DaVinci
+
+#endif // DAVINCIUTILS_FUNCTIONS_H 
---
+++ Phys/Phys/DaVinciUtils/DaVinciUtils/Guards.h
@@ -0,0 +1,102 @@
+#ifndef DAVINCIUTILS_GUARDS_H 
+#define DAVINCIUTILS_GUARDS_H 1
+
+/** @namespace DaVinci::Utils DaVinciUtils/Guards.h
+ *  
+ *  General helper functions and classes for use in DaVinci.
+ *  These should only know about Gaudi classes and patterns.
+ *  No knowledge of DaVinci specific code should be assumed.
+ * 
+ *  @author Juan PALACIOS palacios@uzh.uzh.ch
+ *  @date   2010-09-21
+ */
+
+namespace DaVinci 
+{
+  
+namespace Utils
+{
+  
+  /**
+   *
+   * Guard for container of newed pointers. Delete each item in a container of 
+   * newed pointers.
+   *
+   * T must be a const_iterator defined, begin(), end() and clear() methods,
+   * and holding pointers.
+   *
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   * @date 31/08/2009
+   *
+   **/
+  template <class T>
+  class PointerContainerGuard {
+  public: 
+
+    explicit PointerContainerGuard(T& container )
+      :
+      m_container(container)
+    {
+    }
+  
+
+    ~PointerContainerGuard( ) 
+    {
+      for (typename T::const_iterator iObj = m_container.begin();
+           iObj != m_container.end(); ++iObj) delete *iObj;
+    }
+
+  private:
+    PointerContainerGuard();
+
+  private:
+
+    T& m_container;
+
+  };
+
+  /**
+   *
+   * Guard for container of newed pointers. Delete each item in a container of 
+   * newed pointers unless they are also in the TES.
+   *
+   * T must be a const_iterator defined, begin(), end() and clear() methods,
+   * and holding pointers.
+   *
+   *
+   * @author Juan Palacios juan.palacios@nikhef.nl
+   * @date 01/09/2009
+   *
+   **/
+  template <class T>
+  class OrphanPointerContainerGuard
+  {
+  public:
+
+    explicit OrphanPointerContainerGuard(T& container)
+      :
+      m_container(container)
+    {
+    }
+
+    ~OrphanPointerContainerGuard( ) 
+    {
+      for (typename T::const_iterator iObj = m_container.begin();
+           iObj != m_container.end(); ++iObj) {
+        if( !DaVinci::Utils::inTES(*iObj) ) delete *iObj;
+      }
+
+    }
+  private:
+    OrphanPointerContainerGuard();
+
+  private:
+    
+    T& m_container;
+  };
+ 
+} // namespace Utils
+  
+} // namespace DaVinci
+
+#endif // KERNEL_DAVINCIGUARDS_H
