0c74cb928c886de62cf7d5393f2d017b471b2599
Victor Coco <victor.coco@cern.ch>
2010-09-22 16:18:08 +0000

    add upgrade configurables and remove useless classes/options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@86485 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 89d5d38d3275fef3749792f197d37f2b4db347e5

---
+++ Rec/Rec/RecoUpgrade/python/RecoUpgrade/RecoTrackingUpgrade.py
@@ -0,0 +1,268 @@
+from os import environ
+from Gaudi.Configuration import *
+import GaudiKernel.ProcessJobOptions
+from TrackSys.Configuration import *
+from GaudiKernel.SystemOfUnits import mm
+
+from TrackFitter.ConfiguredFitters import *
+
+from Configurables import TrackSys
+from Configurables import RecSysConf
+
+
+from Configurables import (  ProcessPhase,DecodeVeloRawBuffer,MuonIDAlg,
+                             VeloPixRawBankToLiteCluster,VeloPixRawBankToPartialCluster,
+                             PatLHCbIDUp2MCParticle,PatLHCbIDUp2MCHit,CheatedVeloPixPat,Tf__Tsa__Seed,
+                             TrackStateInitAlg,TrackStateInitTool,TrackStateVeloPixInitTool,
+                             TrackEventCloneKiller, TrackPrepareVelo, VeloPixLiteMeasurementProvider,
+                             TrackAddLikelihood,PatForward,PatSeedFit,PatSeeding,PatVeloTT,PatVeloTTTool,
+                             TrackMatchVeloSeed, PatDownstream,PatMatch,Tf__PatVeloPixFitLHCbIDs,Tf__PatVeloFitLHCbIDs  )
+
+def ConfiguredFitVeloPix( Name = "FitVeloPix",
+                       TracksInContainer = "Rec/Track/PreparedVeloPix"):
+    eventfitter = ConfiguredEventFitter(Name,TracksInContainer,
+                                        FieldOff = True,
+                                        SimplifiedGeometry = False,
+                                        NoDriftTimes       = None,
+                                        KalmanSmoother     = True,
+                                        LiteClusters = True,
+                                        ApplyMaterialCorrections = True,
+                                        StateAtBeamLine = True,
+                                        MaxNumberOutliers = 3)
+    
+    eventfitter.Fitter.addTool( MeasurementProvider(), name = "MeasProvider")
+    eventfitter.Fitter.MeasProvider.VeloPixProvider = VeloPixLiteMeasurementProvider()
+    eventfitter.Fitter.MeasProvider.IgnoreVelo = True
+    eventfitter.Fitter.MeasProvider.IgnoreVeloPix = False
+    return eventfitter
+
+def ConfiguredFitVeloPixTT( Name = "FitVeloPixTT",
+                       TracksInContainer = "Rec/Track/VeloPixTT"):
+    # note that we ignore curvatue in velo. in the end that seems the
+    # most sensible thing to do.
+    eventfitter = ConfiguredEventFitter(Name,TracksInContainer,
+                                        FieldOff = True,
+                                        SimplifiedGeometry = False,
+                                        NoDriftTimes       = None,
+                                        KalmanSmoother     = True,
+                                        LiteClusters = True,
+                                        ApplyMaterialCorrections = True,
+                                        StateAtBeamLine = True,
+                                        MaxNumberOutliers = 3)
+    
+    eventfitter.Fitter.addTool( MeasurementProvider(), name = "MeasProvider")
+    eventfitter.Fitter.MeasProvider.VeloPixProvider = VeloPixLiteMeasurementProvider()
+    eventfitter.Fitter.MeasProvider.IgnoreVelo = True
+    eventfitter.Fitter.MeasProvider.IgnoreVeloPix = False
+    return eventfitter
+
+
+def ConfigVeloPixProvider(fitter):
+    fitter.addTool( MeasurementProvider(), name = "MeasProvider")
+    fitter.MeasProvider.VeloPixProvider = VeloPixLiteMeasurementProvider()
+    fitter.MeasProvider.IgnoreVelo = True
+    fitter.MeasProvider.IgnoreVeloPix = False
+
+def ConfigureTrackStateInitTool(nameTSInit,thebeamState = False):
+    myTool = TrackStateInitAlg(nameTSInit).StateInitTool
+    myTool.VeloFitterName="Tf::PatVeloPixFitLHCbIDs/FitVeloPix"
+    myTool.addTool(Tf__PatVeloPixFitLHCbIDs,name="FitVeloPix")
+
+
+
+def RecoTrackingUpgrade(exclude=[]):
+    
+   ## --------------------------------------------------------------------
+   ## Pattern Recognition and Fitting
+   ## --------------------------------------------------------------------
+   Tf__Tsa__Seed("TsaSeed").maxOTHits = 100000
+   Tf__Tsa__Seed("TsaSeed").maxITHits = 30000
+    
+   
+   # Which algs to run ?
+   trackAlgs = TrackSys().getProp("TrackPatRecAlgorithms")
+   cloneKiller = TrackEventCloneKiller()
+
+   ########## Replace/insert decoding of velopart
+   if "VeloPix" in trackAlgs :
+       veloUPLiteClusters = VeloPixRawBankToLiteCluster("VeloPixRawBankToLiteCluster")
+       veloUPLiteClusters.ClusterLocation = "Raw/VeloPix/LiteClusters"
+       veloUPClusters = VeloPixRawBankToPartialCluster("VeloPixRawBankToPartialCluster")
+       veloUPClusters.ClusterLocation = "Raw/VeloPix/Clusters"
+       if DecodeVeloRawBuffer("DecodeVeloClusters") in GaudiSequencer("RecoDecodingSeq").Members :
+           GaudiSequencer("RecoDecodingSeq").remove(DecodeVeloRawBuffer("DecodeVeloClusters"))
+       GaudiSequencer("RecoDecodingSeq").Members.insert(0,veloUPLiteClusters)
+       GaudiSequencer("RecoDecodingSeq").Members.insert(1,veloUPClusters)
+
+   ## Do what is needed for Dst
+   #from GaudiConf.DstConf import DummyWriter
+   
+   if "VeloPix" in trackAlgs :
+       from Configurables import DigiConf
+       DigiConf().Detectors.insert(DigiConf().Detectors.index("Velo"),"VeloPix")
+       DigiConf().Detectors.remove("Velo")
+       DigiConf().Detectors.remove("L0")
+   
+
+   ######### Definition of cheated Pattern Recognition
+   if "VeloPix" in trackAlgs :
+       if not TrackSys().getProp( "WithMC" ):
+           raise RuntimeError("Only Cheated Pattern recognition is implemented for VeloPix, MC is needed")
+       if "Velo" in trackAlgs :
+           GaudiSequencer("RecoVELOSeq").removeAll() ## Is there a better way to remove it if it exists
+       
+       ## IF ASSOCIATION WITH MCHIT IS NEEDED, IS A DataOnDemandSvc    
+       from Configurables import MCParticle2MCHitAlg, TrackerMCParticle2MCHitAlg, DataOnDemandSvc
+
+       velopixAssoc = MCParticle2MCHitAlg( "MCP2VeloPixMCHitAlg", MCHitPath = "MC/VeloPix/Hits", OutputData = "/Event/MC/Particles2MCVeloPixHits" )
+       trackerAssoc = TrackerMCParticle2MCHitAlg( "MCP2TrackerHitAlg", OutputData = "/Event/MC/Particles2MCTrackerHits" )
+       
+       ## Create the different linkers
+       DataOnDemandSvc().AlgMap[ "/Event/Link/MC/Particles2MCVeloPixHits" ]    = velopixAssoc
+       DataOnDemandSvc().AlgMap[ "/Event/Link/MC/Particles2MCTrackerHits" ] = trackerAssoc
+       DataOnDemandSvc().NodeMap[ "/Event/Link" ]    = "DataObject"
+       DataOnDemandSvc().NodeMap[ "/Event/Link/MC" ] = "DataObject"
+
+       ## Cheated Pattern recognition
+       GaudiSequencer("RecoVELOPIXSeq").Members += [PatLHCbIDUp2MCParticle("PatLHCbID2MCParticleVeloPix"),
+                                                    CheatedVeloPixPat("CheatedPatVeloPixTracking")]
+       patLHCbID2MCP = PatLHCbIDUp2MCParticle("PatLHCbID2MCParticleVeloPix")
+       patLHCbID2MCP.LinkVELO = False
+       patLHCbID2MCP.LinkTT = False
+       patLHCbID2MCP.LinkIT = False
+       patLHCbID2MCP.LinkOT = False
+       patLHCbID2MCP.LinkVELOPIX = True
+       
+       cheatPat = CheatedVeloPixPat("CheatedPatVeloPixTracking")
+       cheatPat.MinimalMCHitForTrack = 2
+
+
+   ######### Definition/Replacement of Tracking
+   track = ProcessPhase("Track")
+   GaudiSequencer("TrackAddExtraInfoSeq").Members.remove(TrackAddLikelihood())
+   
+   if "VeloPix" in trackAlgs :
+       track.DetectorList += [ "VeloPixFit"]
+       GaudiSequencer("TrackVeloPixFitSeq").Members = [ TrackPrepareVelo("TrackPrepareVeloPix")]
+       TrackPrepareVelo("TrackPrepareVeloPix").inputLocation = "/Event/Rec/Track/VeloPix"
+       TrackPrepareVelo("TrackPrepareVeloPix").outputLocation = "/Event/Rec/Track/PreparedVeloPix"
+       
+       ## Fit the velo tracks
+       fitter = ConfiguredFitVeloPix("FitVeloPix","Rec/Track/PreparedVeloPix")
+       GaudiSequencer("TrackVeloPixFitSeq").Members += [fitter]
+       
+       ## copy the velo tracks to the "best" container (except in RDST case)
+       if TrackSys().getProp( "OutputType" ).upper() != "RDST":
+           from Configurables import TrackContainerCopy
+           copyVeloPix = TrackContainerCopy( "CopyVeloPix" )
+           copyVeloPix.inputLocation = "Rec/Track/PreparedVeloPix";
+           GaudiSequencer("TrackVeloPixFitSeq").Members += [ copyVeloPix ]
+
+
+       ## Modify the Forward Pattern reco
+       if "ForwardPat" in  track.DetectorList :
+           PatForward("PatForward").InputTracksName = "/Event/Rec/Track/VeloPix"
+       ## Modify the Forward fit
+       if "ForwardFit" in track.DetectorList :
+           ConfigureTrackStateInitTool("InitForwardFit")          
+           fitter = TrackEventFitter("FitForward") 
+           ConfigVeloPixProvider(fitter.Fitter)
+           GaudiSequencer("TrackForwardFitSeq").Members += [ fitter ]
+           
+       ## Modify the Seed fit    
+       if "SeedFit" in track.DetectorList :
+           ConfigureTrackStateInitTool("InitSeedFit")
+           
+           if "Match" not in trackAlgs :
+               fitter = TrackEventFitter("FitSeed")
+               ConfigVeloPixProvider(fitter.Fitter)
+           else :
+               fitter = TrackEventFitter("FitSeedForMatch")
+               ConfigVeloPixProvider(fitter.Fitter)
+                   
+      ## Modify the MatchPat
+       if "MatchPat" in track.DetectorList :
+           trmatchVSeed = TrackMatchVeloSeed("TrackMatch")
+           ConfigVeloPixProvider(trmatchVSeed)
+           trmatchVSeed.InputVeloTracks =  "/Event/Rec/Track/VeloPix"
+           
+      ## Modify the MatchFit
+       if "MatchFit" in track.DetectorList :
+           ConfigureTrackStateInitTool("InitMatchFit")
+           fitter = TrackEventFitter("FitMatch")
+           ConfigVeloPixProvider(fitter.Fitter)
+
+       if "DownstreamFit"in track.DetectorList :
+           ConfigureTrackStateInitTool("InitDownstreamFit")
+           fitter = TrackEventFitter("FitDownstream")
+           ConfigVeloPixProvider(fitter.Fitter)
+           
+       if "SeedRefit"in track.DetectorList : 
+           ConfigureTrackStateInitTool("InitSeedRefit")
+           fitter = TrackEventFitter("RefitSeed")
+           ConfigVeloPixProvider(fitter.Fitter)  
+
+       if "Fit"in track.DetectorList :
+           ConfigureTrackStateInitTool("InitBestFit")
+           fitter = TrackEventFitter("FitBest")
+           ConfigVeloPixProvider(fitter.Fitter)
+           
+         
+    ## Velo-TT pattern
+   if "VeloPixTT" in trackAlgs :
+       track.DetectorList += ["VeloPixTTPat"]
+       patVPixTT = PatVeloTT("PatVeloPixTT")
+       patVPixTT.InputTracksName  = "Rec/Track/VeloPix"
+       patVPixTT.OutputTracksName = "Rec/Track/VeloPixTT"
+       GaudiSequencer("TrackVeloPixTTPatSeq").Members += [ patVPixTT ]
+
+       patVPixTT.InputUsedTracksNames = ["Rec/Track/Forward","Rec/Track/Match"]
+       patVPixTT.addTool(ConfiguredMasterFitter( "Fitter",
+                                                 FieldOff = None,
+                                                 SimplifiedGeometry = False,
+                                                 NoDriftTimes       = None,
+                                                 KalmanSmoother     = None,
+                                                 LiteClusters       = True,
+                                                 ApplyMaterialCorrections = None,
+                                                 StateAtBeamLine = True,
+                                                 MaxNumberOutliers = 2 ))
+       patVPixTT.Fitter.NumberFitIterations = 1
+       patVPixTT.Fitter.MaxNumberOutliers = 1
+       patVPixTT.Fitter.Extrapolator.ExtraSelector = "TrackSimpleExtraSelector"
+       patVPixTT.Fitter.Extrapolator.addTool(TrackSimpleExtraSelector, "ExtraSelector")
+       patVPixTT.Fitter.NodeFitter.addTool(TrackMasterExtrapolator, "Extrapolator")
+       patVPixTT.Fitter.NodeFitter.Extrapolator.ExtraSelector = "TrackSimpleExtraSelector";
+       patVPixTT.Fitter.StateAtBeamLine = False
+       
+       ConfigVeloPixProvider(patVPixTT.Fitter)
+       
+       patVPixTT.addTool(PatVeloTTTool, name="PatVeloTTTool")
+       if TrackSys().fieldOff():
+           patVPixTT.PatVeloTTTool.minMomentum = 5000.
+           patVPixTT.PatVeloTTTool.maxPseudoChi2 = 256
+           patVPixTT.maxChi2 = 256.
+           patVPixTT.PatVeloTTTool.DxGroupFactor = 0.0
+           patVPixTT.fitTracks = False
+       else:
+           patVPixTT.PatVeloTTTool.minMomentum = 800.
+           patVPixTT.PatVeloTTTool.maxPseudoChi2 = 10000.
+           patVPixTT.maxChi2 = 5.
+       
+       #from PatVeloTT import PatVeloConf
+       #PatVeloConf.PatVeloTTConf().configureAlg(patVPixTT)
+       ##cloneKiller.TracksInContainers += ["Rec/Track/VeloPixTT"]
+       if "fastSequence" not in TrackSys().getProp("ExpertTracking") :
+           track.DetectorList += ["VeloPixTTFit"]
+           GaudiSequencer("TrackVeloPixTTFitSeq").Members += [TrackStateInitAlg("InitVeloPixTTFit")]
+           ConfigureTrackStateInitTool("InitVeloPixTTFit")
+           TrackStateInitAlg("InitVeloPixTTFit").TrackLocation = "Rec/Track/VeloPixTT"
+           fitter = ConfiguredFitVeloPixTT(  "FitVeloPixTT", "Rec/Track/VeloPixTT")
+           GaudiSequencer("TrackVeloPixTTFitSeq").Members += [ fitter ]
+
+
+       
+
+
+
+
+
