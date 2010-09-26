a7cbcd9496721afd2ba217050a4499f25100bdc5
Walter Marcello Bonivento <bonivent@cern.ch>
2010-09-26 13:59:37 +0000

    updated StrippingDstarD02xx.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@86654 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4a0775dbeebccab8a82227c8c883e55385c48d3

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarD02xx.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarD02xx.py
@@ -16,34 +16,32 @@ class StrippingDstarD02xxConf(LHCbConfigurableUser):
     Definition of  D*(2010)+ -> pi+ (D0->xx) stripping
     """
     __slots__ = { 'PrescalepipiBox'     : 1.
-                  , 'PrescalepipiD0SB'  : 1.
-                  , 'PrescalepipiDMSB'  : 1.
-                  , 'PrescalepipiSignal'  : 1.
                   , 'PrescalemumuBox'     : 1.
-                  , 'PrescalemumuD0SB'  : 1.
-                  , 'PrescalemumuDMSB'  : 1.
-                  , 'PrescalemumuSignal'  : 1.
                   , 'PrescaleKpiBox'    : 1.
-                  , 'PrescalemueBox'    : 1.
-                  , 'PrescaleeeBox'    : 1.
+                  , 'PrescaleemuBox'    : 1.
+                  , 'PrescaleeKBox'    : 1.
+                  , 'PrescaleepiBox'    : 1.
                   , 'PrescalepimuBox'    : 1.
                   , 'PrescaleKmuBox'    : 1.
-                  ,'DMassWin'           : 100.       # MeV
-                  ,'DVtxChi2'           :  20.       # adimensional
-                  ,'doca'               : 7.0        # mm
+                  ,'DMassWin'           : 70.       # MeV
+                  ,'doca'               : 0.1        # mm
                   ,'XminPT'             : 750.       # MeV
-                  ,'XmaxPT'             : 1100.       # MeV
-                  ,'XminP'              : 3000.      # MeV
-                  ,'XminIPCHI2'         : 1          # adimensional
-                  ,'DDira'              : 0.999     # adimensional
+                  ,'XmaxPT'             : 1100.      # MeV
+                  ,'XminP'              : 4000.      # MeV
+                  ,'XTrackChi2'         : 5.        # adimensional
+                  ,'XTrackChi2Pi'         : 7.        # adimensional
+                  ,'XminIPChi2'         : 3        # adimensional
+                  ,'XmaxIPChi2'         : 8        # adimensional
+                  ,'DMinFlightChi2'    :  20.
+                  ,'DDira'              : 0.9997     # adimensional
+                  ,'D0MinPT'            : 1800.      # MeV
+                  ,'DMaxIPChi2'        :15.
+                  ,'DVChi2'            :10. 
                   ,'PiMinPT'            : 110.       # MeV
                   ,'PiMaxIPCHI2'        : 10.         # adimensional
-                  ,'D0MinPT'            : 1800.      # MeV
-                  ,'D0MinD'             : 0.         # mm
-                  ,'D0MaxIPCHI2'        : 10.       # adimensional
                   ,'DstMassWin'         : 110.       # MeV
                   ,'DstD0DMWin'         : 10.        # MeV
-##                  ,'RequireHlt'         : 1          # bool
+                  ,'RequireHlt'         : 1          # 
                   }
 
 
@@ -63,11 +61,10 @@ class StrippingDstarD02xxConf(LHCbConfigurableUser):
     ####### Template for combine particles for D0 -> x+ y-  ######
     def combinetwobody(self, xplus, xminus) :
         from Configurables import CombineParticles
-        d0comb_combcut =  " (ADAMASS('D0')< %(DMassWin)s *MeV) & (AMAXCHILD(PT)>%(XmaxPT)s *MeV)"
+        d0comb_combcut =       "(AMAXDOCA('')< %(doca)s *mm) & (ADAMASS('D0')< %(DMassWin)s *MeV) & (AMAXCHILD(PT)>%(XmaxPT)s *MeV) & (APT> %(D0MinPT)s)"
 
-
-        d0comb_childcut = "(PT> %(XminPT)s *MeV) & (P>%(XminP)s *MeV) & (BPVIPCHI2()> %(XminIPCHI2)s)" 
-        d0comb_d0cut = "(BPVDIRA> %(DDira)s) & (VFASPF(VCHI2/VDOF)< %(DVtxChi2)s )"
+        d0comb_childcut = "(PT> %(XminPT)s *MeV) & (P>%(XminP)s *MeV) & (TRCHI2DOF<%(XTrackChi2)s) & (MIPCHI2DV(PRIMARY)> %(XminIPChi2)s) " 
+        d0comb_d0cut = "(BPVDIRA> %(DDira)s) & (INGENERATION( (BPVIPCHI2()>%(XmaxIPChi2)s),1 ) ) & (BPVVDCHI2> %(DMinFlightChi2)s) & (MIPCHI2DV(PRIMARY)< %(DMaxIPChi2)s) & (VFASPF(VCHI2/VDOF)< %(DVChi2)s)"
         name = "D02"+xplus+xminus
         tbline = CombineParticles( name )
         inputLoc = {
@@ -94,10 +91,10 @@ class StrippingDstarD02xxConf(LHCbConfigurableUser):
     ####### Template for combine particles for D* -> D0 pi
     def combineDstar(self) :
         from Configurables import CombineParticles
-        dstcomb_dstcut       =  "(abs(M-MAXTREE('D0'==ABSID,M)-145.42) < %(DstD0DMWin)s )"
+        dstcomb_dstcut       =  "(abs(M-MAXTREE('D0'==ABSID,M)-145.42) < %(DstD0DMWin)s ) & (VFASPF(VCHI2/VDOF)< %(DVChi2)s)"
         dstcomb_combcut =  "(ADAMASS('D*(2010)+')<%(DstMassWin)s * MeV)"
-        dstcomb_picut = "(PT> %(PiMinPT)s * MeV) &  (BPVIPCHI2() < %(PiMaxIPCHI2)s)"
-        dstcomb_d0cut = "(PT> %(D0MinPT)s * MeV) & (BPVVD > %(D0MinD)s ) & (BPVIPCHI2() < %(D0MaxIPCHI2)s )"
+        dstcomb_picut = "(PT> %(PiMinPT)s * MeV) &  ( MIPCHI2DV(PRIMARY)< %(PiMaxIPCHI2)s) & (TRCHI2DOF<%(XTrackChi2Pi)s) "
+        dstcomb_d0cut = "PT>0"
         
         dstar = CombineParticles( "combine" )
         #dstar.DecayDescriptors = ['[D*(2010)+ -> D0 pi+]cc']
@@ -123,13 +120,11 @@ class StrippingDstarD02xxConf(LHCbConfigurableUser):
         # Capitalize particle names to match Hlt2 D*->pi D0-> xx lines
         Xplus  = xplus[0].upper() + xplus[1:]    
         Xminus = xminus[0].upper() + xminus[1:]
-        #RequireHlt = 1
-
         hltname = "Hlt2Dst2PiD02"+Xplus+Xminus+"*Decision"  # * matches Signal, Sidebands and Box lines
-        
         line_box = StrippingLine("Dst2PiD02"+combname+"Box",
                                  HLT = "HLT_PASS_RE('"+hltname+"')",
                                  algos = [ seq_box ], prescale = self.getProps()[ pres ])
+
         return line_box
     
     
@@ -145,14 +140,15 @@ class StrippingDstarD02xxConf(LHCbConfigurableUser):
         line_pipi_box = self.baseLine("pi", "pi")
         line_mumu_box = self.baseLine("mu", "mu")
         line_Kpi_box  = self.baseLine("K",  "pi")
-        line_ee_box   = self.baseLine("e",   "e")
-        line_mue_box  = self.baseLine("mu",  "e")
+        line_eK_box   = self.baseLine("e",   "K")
+        line_epi_box   = self.baseLine("e",   "pi")
+        line_mue_box  = self.baseLine("e",  "mu")
         line_pimu_box = self.baseLine("pi", "mu")
         line_Kmu_box  = self.baseLine("K",  "mu")
         
         
         
-        lines = [line_pipi_box, line_mumu_box,line_pimu_box, line_Kpi_box, line_mue_box, line_ee_box, line_Kmu_box]
+        lines = [line_pipi_box, line_mumu_box,line_pimu_box, line_Kpi_box, line_mue_box, line_eK_box,line_epi_box, line_Kmu_box]
         
         return lines
     
