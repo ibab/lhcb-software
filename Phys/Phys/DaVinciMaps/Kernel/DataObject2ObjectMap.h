df11c9d4387731070d485dc5cfb7124052004943
jpalac <jpalac@cern.ch>
2010-09-28 05:52:34 +0000

    Added new classes. Had forgotten in previous commit.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@86801 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fc9f482ce6e76629e5cb221f5fa8b16dfacd5a49

---
+++ Phys/Phys/DaVinciMaps/Kernel/DataObject2ObjectMap.h
@@ -0,0 +1,139 @@
+// $Id: DataObject2ObjectVector.h,v 1.1 2010/08/19 15:09:43 jpalac Exp $
+#ifndef DATAOBJECT2OBJECTMAP_H 
+#define DATAOBJECT2OBJECTMAP_H 1
+
+// Include files
+#include "GaudiKernel/DataObject.h"
+#include "GaudiKernel/VectorMap.h"
+#include "GaudiKernel/SmartRef.h"
+#include "Event/Particle.h"
+#include "Kernel/LHCbID.h"
+
+namespace DaVinci{
+
+  namespace Map {
+    
+
+    /** @class DataObject2ObjectMap DataObject2ObjectMap.h
+     *  
+     *
+     *  @author Juan Palacios
+     *  @date   2010-08-18
+     */
+
+    template <class From, class To>
+    class DataObject2ObjectMap : public DataObject
+    {
+
+    public:
+
+      typedef GaudiUtils::VectorMap<SmartRef<From>, To > Map;
+      typedef typename Map::iterator iterator;
+      typedef typename Map::const_iterator const_iterator;
+      typedef typename Map::reverse_iterator reverse_iterator;
+      typedef typename Map::const_reverse_iterator const_reverse_iterator;
+      typedef typename Map::mapped_type mapped_type;
+      typedef typename Map::result_type result_type;
+      typedef typename Map::value_type value_type;
+      typedef typename Map::size_type size_type;
+      typedef const From* key_type;
+  
+
+    public: 
+      /// Standard constructor
+      DataObject2ObjectMap( )
+        :
+        DataObject(),
+        m_map()
+      {
+      }
+      
+        ~DataObject2ObjectMap( ) 
+      {
+        m_map.clear();
+      }
+      
+    public :
+
+      inline iterator find(const From* key) const
+      {
+        return m_map.find(key);
+      }
+
+      inline const mapped_type& operator()(const From* key) const
+      {
+        return m_map(key);
+      }
+  
+      inline const mapped_type& operator[](const From* key) const
+      {
+        return m_map[key];    
+      }
+
+      inline const mapped_type& at(const From* key) const
+      {
+        return m_map.at(key); 
+      }
+
+      inline size_type count(const key_type key) const 
+      {
+        return m_map.count(key);
+      }
+
+      inline result_type insert(const From* key,
+                                mapped_type mapped) 
+      {
+        return m_map.insert(key, mapped);
+      }
+  
+      inline iterator begin  () const 
+      {
+        return m_map.begin  () ;
+      }
+  
+      inline iterator end() const 
+      {
+        return m_map.end    () ;
+      }
+
+      inline reverse_iterator rbegin  () const
+      {
+        return m_map.rbegin  () ;    
+      }
+
+      inline reverse_iterator rend() const
+      { 
+        return m_map.rend    () ; 
+      }
+  
+      inline size_type size() const
+      {
+        return m_map.size();
+      }
+  
+      inline bool empty() const 
+      {
+        return m_map.empty();
+      }
+
+      inline key_type key_at(const size_t index) const 
+      {
+        return m_map.key_at(index);
+      }
+
+      inline const mapped_type& value_at(const size_t index) const 
+      {
+        return m_map.value_at(index);
+      }
+
+    private :
+      Map m_map;
+
+    };
+
+  } // namespace Map
+  
+} // namespace DaVinci
+
+  
+#endif // DATAOBJECT2OBJECTMAP_H
