cf3995eab5ad74461225f5fd0d87970cf870cecf
jpalac <jpalac@cern.ch>
2010-09-29 15:05:31 +0000

    Fix initialization test.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@86957 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4add6189a3436bceca0de5e71f9eafd34ed76f3c

--- DaVinci/DaVinciTests/tests/options/DVTestTagging.py
+++ DaVinci/DaVinciTests/tests/options/DVTestTagging.py
@@ -41,13 +41,11 @@ from Configurables import DaVinci
 DaVinci().EvtMax     = 100                         # Number of events
 DaVinci().SkipEvents = 0                           # Events to skip
 DaVinci().PrintFreq  = 1
-DaVinci().DataType = "MC09"
+DaVinci().DataType = "2010"
 DaVinci().Simulation    = True
-# DaVinci().MainOptions = "$DAVINCITESTSROOT/tests/options/Do09selBu2LLK.py"
 from Configurables import StoreExplorerAlg, PrintDecayTree
 PrintDecayTree().InputLocations = [ location ] 
 DaVinci().MoniSequence = [   PrintDecayTree()
-                            #, StoreExplorerAlg() ] #
                             ,tag
                             , tagcheck
                             ]  # The algorithms
@@ -57,7 +55,7 @@ DaVinci().MoniSequence = [   PrintDecayTree()
 #
 # example data file
 #
-DaVinci().Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/user/p/pkoppenb/MC09-Bu2eeK/Sel.Belectron-440-0.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
+DaVinci().Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/MC/2010/DST/00006522/0000/00006522_00000001_1.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
 
 MessageSvc().Format = "% F%40W%S%7W%R%T %0W%M"
 
