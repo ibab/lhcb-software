88018d202881f645061aba43fbac95aae3ec7484
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-09-29 12:14:56 +0000

    now that there are Hlt2BeamGasLines, remove the Hlt1 beamgas passthrough
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@86916 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 68082d923142aeec430d4cb21b193f9821fb291f

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2BeamGasLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2BeamGasLines.py
@@ -19,8 +19,8 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
                 , 'AbsZPosMax'             : '1300.'
                 , 'VertexMaxChi2'          : '9999.'
                 , 'InputRZTracksLocation'  : 'Hlt1/Track/RZVeloBeamGas'
-                , 'Output3DTracksLocation' : 'Hlt/Track/BeamGasVelo3D'
-                , 'OutputVerticesLocation' : 'Hlt/Vertex/PVBeamGas'
+                , 'Output3DTracksLocation' : 'Hlt2/Track/VeloSpaceBeamGas'
+                , 'OutputVerticesLocation' : 'Hlt2/Vertex/PVBeamGas'
                 , 'InputRateLimitNonBBLine': 100.
                 , 'InputRateLimitBBLine'   : 200.
                 , 'Prescale'               : { 'Hlt2BeamGasNonBBCrossing' : 1.0
@@ -42,20 +42,22 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
 
         ## Create filters on the produced vertices: 2 different filters for the non-beam-beam and the beam-beam crossings
         from Configurables import LoKi__VoidFilter
-        vtxCut_MinNTracks = "(NTRACKS > "+self.getProp('VertexMinNTracks')+")"
-        vtxCut_AbsZPosMax = "(abs(VZ) < "+self.getProp('AbsZPosMax')+" * mm)"
-        vtxCut_AbsZPosMin = "(abs(VZ) > "+self.getProp('AbsZPosMin')+" * mm)"
-        vtxCut_MaxChi2    = "(VCHI2 < "+self.getProp('VertexMaxChi2')+")"
+        vtxSource =         "VSOURCE('%(OutputVerticesLocation)s')" % self.getProps()
+        vtxCut_MaxChi2    = "(VCHI2 < %(VertexMaxChi2)s)" % self.getProps()
+        vtxCut_MinNTracks = "(NTRACKS > %(VertexMinNTracks)s)" % self.getProps()
+        vtxCut_AbsZPosMax = "(abs(VZ) < %(AbsZPosMax)s*mm)" % self.getProps()
+        vtxCut_AbsZPosMin = "(abs(VZ) > %(AbsZPosMin)s*mm)" % self.getProps()
+        vtxSink   =         "~VEMPTY"
 
         ## The vertex filters will be the last algorithms inside the Hlt2 lines so they need to have the names of the lines
         fltrVerticesBE = LoKi__VoidFilter( 'Hlt2BeamGasNonBBCrossingDecision',
                                            Preambulo = ['from LoKiPhys.decorators import *'],
-                                           Code = "VSOURCE('"+self.getProp('OutputVerticesLocation')+"') >> "+vtxCut_MaxChi2+" >> "+vtxCut_MinNTracks+" >> "+vtxCut_AbsZPosMax+" >> ~VEMPTY"
+                                           Code = " >> ".join( [ vtxSource, vtxCut_MaxChi2, vtxCut_MinNTracks, vtxCut_AbsZPosMax, vtxSink ] )
                                          )
 
         fltrVerticesBB = LoKi__VoidFilter( 'Hlt2BeamGasBBCrossingDecision',
                                            Preambulo = ['from LoKiPhys.decorators import *'],
-                                           Code = "VSOURCE('"+self.getProp('OutputVerticesLocation')+"') >> "+vtxCut_MaxChi2+" >> "+vtxCut_MinNTracks+" >> "+vtxCut_AbsZPosMin+" >> "+vtxCut_AbsZPosMax+" >> ~VEMPTY"
+                                           Code = " >> ".join( [ vtxSource, vtxCut_MaxChi2, vtxCut_MinNTracks, vtxCut_AbsZPosMin, vtxCut_AbsZPosMax, vtxSink ] ) 
                                          )
 
 
@@ -69,6 +71,7 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
                             , prescale = self.prescale
                             , ODIN = 'ODIN_BXTYP != LHCb.ODIN.BeamCrossing'
                             , HLT = 'scale( %s, RATE(%s) )' %(hltFilterNonBB, inpRateLimitNonBB)
+                            , VoidFilter = '' # make sure we DO NOT get a filter thrown on top of us!!!
                             , algos = recoSeq+[fltrVerticesBE]
                             , postscale = self.postscale
                             )
@@ -77,6 +80,7 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
                          , prescale = self.prescale
                          , ODIN = 'ODIN_BXTYP == LHCb.ODIN.BeamCrossing'
                          , HLT = 'scale( %s, RATE(%s) )' %(hltFilterBB, inpRateLimitBB)
+                         , VoidFilter = '' # make sure we DO NOT get a filter thrown on top of us!!!
                          , algos = recoSeq+[fltrVerticesBB]
                          , postscale = self.postscale
                          )
@@ -88,16 +92,15 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
 
     def configureMyReco(self):
 
-        bgTracksLocation_RZ = self.getProp('InputRZTracksLocation')
         bgTracksLocation_3D = self.getProp('Output3DTracksLocation')
 
         ### 1) Create a filter to process only events with more than X "BeamGas" RZ VELO tracks
         from Configurables import LoKi__VoidFilter as VoidFilter
-        fltrInpTr = VoidFilter('BGFltr_InputTracks', Code = "CONTAINS('%s') > %d" %(bgTracksLocation_RZ, int(self.getProp('VertexMinNTracks'))) )
+        fltrInpTr = VoidFilter('BGFltr_InputTracks', Code = "CONTAINS('%(InputRZTracksLocation)s') > %(VertexMinNTracks)s" % self.getProps() )
 
         ## 2) Do Space Tracking on top of the RZ tracks produced in the Hlt1BeamGas lines
         from Configurables import Tf__PatVeloSpaceTracking, Tf__PatVeloSpaceTool
-        mySpaceTracking = Tf__PatVeloSpaceTracking( 'SpaceTrackingBeamGas', InputTracksName  = bgTracksLocation_RZ, OutputTracksName = bgTracksLocation_3D )
+        mySpaceTracking = Tf__PatVeloSpaceTracking( 'Hlt2BeamGasVeloSpaceTracking', InputTracksName  = self.getProp('InputRZTracksLocation'), OutputTracksName = bgTracksLocation_3D )
         mySpaceTracking.addTool( Tf__PatVeloSpaceTool(), name="PatVeloSpaceTool" )
         mySpaceTracking.PatVeloSpaceTool.MarkClustersUsed=False
         
@@ -105,20 +108,14 @@ class Hlt2BeamGasLinesConf(HltLinesConfigurableUser) :
         ### 3) Configure the vertex fitter
         from Configurables import PatPV3D, PVOfflineTool,PVSeed3DTool,LSAdaptPV3DFitter
 
-        recoPV3D =  PatPV3D('HltPV3DBeamGas' )
+        recoPV3D =  PatPV3D('Hlt2BeamGasPV3D' )
         recoPV3D.addTool(PVOfflineTool,'PVOfflineTool')
-
         recoPV3D.PVOfflineTool.PVSeedingName = 'PVSeed3DTool'
-
         recoPV3D.PVOfflineTool.addTool(LSAdaptPV3DFitter, 'LSAdaptPV3DFitter')
         recoPV3D.PVOfflineTool.PVFitterName = 'LSAdaptPV3DFitter'
         recoPV3D.PVOfflineTool.LSAdaptPV3DFitter.TrackErrorScaleFactor = 2.
-
         recoPV3D.PVOfflineTool.InputTracks = [bgTracksLocation_3D]
         recoPV3D.OutputVerticesName = self.getProp('OutputVerticesLocation')
 
         ### 4) return a list of the reconstruction algorithms
         return [fltrInpTr, mySpaceTracking, recoPV3D]
-
-
-        
