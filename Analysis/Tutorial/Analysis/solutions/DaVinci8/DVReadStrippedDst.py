e933eacf33945af5bdbe8ff541f695ea72266459
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2010-10-01 09:50:18 +0000

    update tutorial 8
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@87091 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c100cb5ea672df44e2b8cbf5d6d54f8e26c25dc5

--- Analysis/Tutorial/Analysis/solutions/DaVinci8/DVReadStrippedDst.py
+++ Analysis/Tutorial/Analysis/solutions/DaVinci8/DVReadStrippedDst.py
@@ -9,91 +9,72 @@
 
 from Gaudi.Configuration import *
 
-location = '/Event/Radiative/Phys/SelBd2KstGamma' 
+line = 'DiMuonIncLine'
+location = '/Event/Dimuon/Phys/'+line 
 
 MessageSvc().Format = "% F%80W%S%7W%R%T %0W%M"
 from Configurables import DaVinci, PrintDecayTree, GaudiSequencer
 from Configurables import LoKi__HDRFilter   as StripFilter
 MySequencer = GaudiSequencer('Sequence')
-sf = StripFilter( 'StripPassFilter', Code="HLT_PASS('StrippingBd2KstGammaDecision')", Location="/Event/Strip/Phys/DecReports" )
 pt = PrintDecayTree(InputLocations = [ location ])
 
 # get classes to build the SelectionSequence
 from PhysSelPython.Wrappers import AutomaticData, Selection, SelectionSequence
 # Get the Candidates from the DST.
 # Treat particles already on the DST as data-on-demand, but give the full path.
-KstgammaSel = AutomaticData(Location = location)
+JpsiSel = AutomaticData(Location = location)
 # Filter the Candidate. Replace 'ALL' by some cuts.
 from Configurables import FilterDesktop
-_kstgammaFilter = FilterDesktop('kstgammaFilter', Code = 'ALL')
+_jpsiFilter = FilterDesktop('jpsiFilter', Code = 'MM<4000')
 
 # make a Selection
-KstgammaFilterSel = Selection(name = 'KstgammaFilterSel',
-                          Algorithm = _kstgammaFilter,
-                          RequiredSelections = [ KstgammaSel])
+JpsiFilterSel = Selection(name = 'JpsiFilterSel',
+                          Algorithm = _jpsiFilter,
+                          RequiredSelections = [ JpsiSel ])
 
 # build the SelectionSequence
-KstgammaSeq = SelectionSequence('SeqKstgamma',
-                                TopSelection = KstgammaFilterSel,
-                                EventPreSelector = [ sf ])
+JpsiSeq = SelectionSequence('SeqJpsi',
+                             TopSelection = JpsiFilterSel,
+                            )
 
 ############# DecayTreeTuple
 from Configurables import DecayTreeTuple, TupleToolTrigger, TupleToolDecay, TupleToolTISTOS
-tuple = DecayTreeTuple("KstarGamma_Tuple")
+tuple = DecayTreeTuple("Jpsi_Tuple")
 tuple.ToolList +=  [
     "TupleToolGeometry"
     , "TupleToolKinematic"
     , "TupleToolPrimaries"
     , "TupleToolEventInfo"
     , "TupleToolTrackInfo"
-    , "TupleToolTrigger"
     , "TupleToolTISTOS"
     , "TupleToolAngles"
     , "TupleToolPid"
     , "TupleToolPropertime"
     ]
-tuple.Decay = "[B0 -> (^K*(892)0 -> ^K+ ^pi-) ^gamma]cc"
-tuple.InputLocations = [ 'KstgammaFilterSel' ]
-tuple.addTool(TupleToolTrigger)
-tuple.TupleToolTrigger.VerboseL0 = True
-tuple.TupleToolTrigger.VerboseHlt1 = True
-tuple.TupleToolTrigger.UseAutomaticTriggerList = True
+tuple.Decay = "J/psi(1S) -> ^mu+ ^mu-"
+tuple.InputLocations = [ pt, JpsiSeq.outputLocation() ]
 tuple.addTool(TupleToolTISTOS)
-tuple.TupleToolTISTOS.TriggerList = [ "Hlt1DiHadronDecision", "Hlt1PhotonDiTrackDecision", "Hlt1SingleHadronDecision" ]
-tuple.TupleToolTISTOS.VerboseHlt1 = True
+tuple.TupleToolTISTOS.TriggerList = [ "Hlt2DiMuonUnbiasedJPsiDecision" ]
+tuple.TupleToolTISTOS.VerboseHlt2 = True
 ##################
 
-DaVinci().appendToMainSequence( [ KstgammaSeq.sequence(), tuple ] ) # pt
+DaVinci().appendToMainSequence( [ JpsiSeq.sequence(), tuple ] ) # pt
 EventSelector().Input   = [
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000001_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000004_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000005_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000007_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000008_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000009_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000017_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000019_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000022_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000028_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000029_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000034_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000036_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000039_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000043_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000044_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000045_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000050_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000052_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000054_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'",
-"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/RADIATIVE.DST/00006591/0000/00006591_00000055_1.radiative.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'"]
+"   DATAFILE='root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2010/DIMUON.DST/00007956/0000/00007956_00000294_1.dimuon.dst?svcClass=lhcbmdst' TYP='POOL_ROOTTREE' OPT='READ'"]
 
 DaVinci().DataType = "2010"
 DaVinci().EvtMax = -1
-DaVinci().PrintFreq = 10
-DaVinci().TupleFile = "KstGamma.root"
+DaVinci().PrintFreq = 100
+DaVinci().TupleFile = "Jpsi.root"
 
 """
-TTree* T = _file0->Get("KstarGamma_Tuple/DecayTree")
-SandB(T,T,"log(Kplus_IP_OWNPV)/log(10.)","KplusHlt1SingleHadronDecision_TOS","KplusHlt1SingleHadronDecision_TIS","Hlt1SingleHadronDecision")
-SandB(T,T,"max(Kplus_PT,piminus_PT)","B0Hlt1SingleHadronDecision_TOS","B0Hlt1SingleHadronDecision_TOS==0","B0Hlt1SingleHadronDecision_TOS || B0Hlt1SingleHadronDecision_TIS")
+TTree* T = _file0->Get("Jpsi_Tuple/DecayTree")
+T->Show(0)
+T->Draw("J_psi_1S_MM")
+SandB(T,0,"J_psi_1S_MM","J_psi_1SHlt2DiMuonUnbiasedJPsiDecision_TOS","1","J_psi_1S_MM<3500","","Psi_Tos")
+TH1D hh("hh","MuMu Mass",100,2900,3400)
+T->Draw("J_psi_1S_MM >> hh")
+NiceRooPlot(hh,"gp")
+
 """
+
