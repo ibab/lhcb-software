39ecc966e88ea83111fc5b970538a65a588219a7
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-10-02 20:28:15 +0000

    Add new script to create mirror alignment DBs for different IOVs
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@87161 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3cfb349f6b432ca34b384853a497fa00b339fa65

---
+++ Rec/Rich/RichAlignment/scripts/RefractAndHPDJobs/CreateMirrorCondDBLayer.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import CondDBUI
+from PyCool import cool
+import os
+import datetime
+
+def getUNIXTime(dtime):
+    import time
+    t = time.mktime(dtime.timetuple())
+    return int( t * 1e9 )
+
+def genXML(root,cond):
+    file = open(root+cond)
+    data = ""
+    for line in file : data += line
+    file.close()
+    return data
+
+def addToDB(startTime,rootToFiles,condPath,db):
+    start = getUNIXTime(startTime) 
+    stop  = cool.ValidityKeyMax
+    db.storeXMLString( condPath, genXML(rootToFiles,condPath), start, stop )
+    
+# field Up Alignment
+upAlign = "/usera/jonesc/AFS/mcoombes/public/RICHMirrorAlignment27-09-10/RichMirrAlign-HPDNewCentres-MagUp"
+dnAlign = "/usera/jonesc/AFS/mcoombes/public/RICHMirrorAlignment27-09-10/RichMirrAlign-HPDNewCentres-MagDown"
+
+# Hardcode the field changes. Format is date of change and the new polarity
+field = { }
+#                          Year   Month  Day   Hour  Min  Sec
+field[ datetime.datetime(  2010,  4,     5,    2,    10,   8 ) ] = upAlign
+field[ datetime.datetime(  2010,  4,     6,    3,     8,  49 ) ] = dnAlign
+field[ datetime.datetime(  2010,  5,     8,   22,    53,  53 ) ] = upAlign
+field[ datetime.datetime(  2010,  5,    14,   13,     7,  21 ) ] = dnAlign
+field[ datetime.datetime(  2010,  5,    15,   17,    20,  36 ) ] = upAlign
+field[ datetime.datetime(  2010,  5,    19,    6,    40,   8 ) ] = dnAlign
+field[ datetime.datetime(  2010,  7,    13,    5,    18,  49 ) ] = upAlign
+field[ datetime.datetime(  2010,  7,    28,   22,    37,  11 ) ] = dnAlign
+field[ datetime.datetime(  2010,  8,    18,    9,    21,  32 ) ] = upAlign
+field[ datetime.datetime(  2010,  8,    29,   18,    26,   8 ) ] = dnAlign
+field[ datetime.datetime(  2010,  9,    22,   17,     1,  39 ) ] = upAlign
+
+condPaths = [ "/Conditions/Rich1/Alignment/Mirrors.xml",
+              "/Conditions/Rich2/Alignment/Mirrors.xml" ]
+
+# Open a new DB file
+dbFileName = "NewMirrorAlignFieldPolarity.db"
+if os.path.exists(dbFileName) : os.remove(dbFileName)
+db = CondDBUI.CondDB( "sqlite_file:"+dbFileName+"/LHCBCOND",
+                      create_new_db=True, readOnly=False )
+print "Created DB", dbFileName
+for path in condPaths: db.createNode(path)
+
+for start in sorted(field.keys()):
+    align = field[start]
+    print start, align
+    for path in condPaths: addToDB(start,align,path,db)
+
