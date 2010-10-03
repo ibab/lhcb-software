41569d9590e1ada6ca9c132c8ed63ffb71fb4301
jpalac <jpalac@cern.ch>
2010-10-03 14:13:30 +0000

    For trivial cloning of LHCb::WeightsVector.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@87175 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6db9badd4267380a7bb21b7fa296f75ba60326ab

---
+++ Analysis/MicroDST/MicroDSTTools/src/WeightsVectorCloner.cpp
@@ -0,0 +1,64 @@
+// $Id: WeightsVectorCloner.cpp,v 1.4 2010-08-11 12:52:52 jpalac Exp $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h" 
+
+// from LHCb
+#include "Event/WeightsVector.h"
+
+// local
+#include "WeightsVectorCloner.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : WeightsVectorCloner
+//
+// 2008-04-01 : Juan PALACIOS
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( WeightsVectorCloner );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+WeightsVectorCloner::WeightsVectorCloner( const std::string& type,
+                          const std::string& name,
+                          const IInterface* parent )
+  : 
+  base_class ( type, name , parent )
+{
+}
+//=============================================================================
+StatusCode WeightsVectorCloner::initialize() 
+{
+
+  debug() << "==> Initialize" << endmsg;
+
+  StatusCode sc = base_class::initialize();
+  
+  if (! sc.isSuccess() ) return sc;
+  
+  return StatusCode::SUCCESS;
+}
+//=============================================================================
+LHCb::WeightsVector* WeightsVectorCloner::operator() (const LHCb::WeightsVector* track)
+{
+  return this->clone(track);
+}
+//=============================================================================
+LHCb::WeightsVector* WeightsVectorCloner::clone(const LHCb::WeightsVector* track)
+{
+  LHCb::WeightsVector* trackClone = 
+    cloneKeyedContainerItem<BasicWeightsVectorCloner>(track);
+
+  return trackClone;
+  
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+WeightsVectorCloner::~WeightsVectorCloner() {} 
+
+//=============================================================================
---
+++ Analysis/MicroDST/MicroDSTTools/src/WeightsVectorCloner.h
@@ -0,0 +1,42 @@
+// $Id: $
+#ifndef MICRODST_WEIGHTSVECTORCLONER_H 
+#define MICRODST_WEIGHTSVECTORCLONER_H 1
+
+// Include files
+// from Gaudi
+#include <MicroDST/MicroDSTTool.h>
+#include <MicroDST/ICloneWeightsVector.h>            // Interface
+#include <MicroDST/Functors.hpp>
+
+/** @class WeightsVectorCloner WeightsVectorCloner.h src/WeightsVectorCloner.h
+ *  
+ *  Clone an LHCb::WeightsVector. At the moment this does nothing other than 
+ *  performing a simple clone and storing the clone in the appropriate
+ *  TES location.
+ *  
+ *  @author Juan PALACIOS
+ *  @date   2010-10-03
+ */
+class WeightsVectorCloner : public extends1<MicroDSTTool, ICloneWeightsVector> {
+public: 
+  /// Standard constructor
+  WeightsVectorCloner( const std::string& type, 
+                       const std::string& name,
+                       const IInterface* parent);
+
+  virtual ~WeightsVectorCloner( ); ///< Destructor
+
+  StatusCode initialize();
+
+  virtual LHCb::WeightsVector* operator() (const LHCb::WeightsVector* track);
+
+private:
+
+  LHCb::WeightsVector* clone(const LHCb::WeightsVector* track);
+
+private:
+
+  typedef MicroDST::BasicCopy<LHCb::WeightsVector> BasicWeightsVectorCloner;
+
+};
+#endif // MICRODST_WEIGHTSVECTORCLONER_H
