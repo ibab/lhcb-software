ec39fc3692b70357e16562d4b44c4977c563840b
jpalac <jpalac@cern.ch>
2010-10-03 13:29:25 +0000

    - Add ICloneWeightsVector  - Increase to v1r6
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@87169 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f7581243ac275fb835f602c65bbf8ec588bb74f0

---
+++ Analysis/MicroDST/MicroDSTInterfaces/MicroDST/ICloneWeightsVector.h
@@ -0,0 +1,25 @@
+// $Id: $
+#ifndef MICRODST_ICLONEWEIGHTSVECTOR_H 
+#define MICRODST_ICLONEWEIGHTSVECTOR_H 1
+// from MicroDST
+#include "MicroDST/ICloner.h"
+
+// Forward declarations
+namespace LHCb 
+{
+  class WeightsVector;
+}
+
+/** @class ICloneWeightsVector MicroDST/ICloneWeightsVector.h
+ *  
+ *
+ *  @author Juan PALACIOS
+ *  @date   2010-10-03
+ */
+class GAUDI_API ICloneWeightsVector : virtual public MicroDST::ICloner<LHCb::WeightsVector> {
+public: 
+
+  DeclareInterfaceID(ICloneWeightsVector, 1, 0 );
+
+};
+#endif // MICRODST_ICLONEWEIGHTSVECTOR_H
