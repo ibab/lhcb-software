0487652fb3446791aeab472f7add962cee1c433d
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-10-05 14:42:26 +0000

    correct db tag
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@87389 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f64367009443016da102197da37556890e982c76

---
+++ Moore/Hlt/Moore/job/runMooreFromTCK_MC.py
@@ -0,0 +1,46 @@
+from Configurables import Moore
+
+###
+# Configure Moore to run from TCK.
+# MAKE SURE THE TCK FITS THE MOORE VERSION YOU USE.
+#
+Moore().UseTCK = True
+Moore().InitialTCK = '0x0023002b'
+
+Moore().L0 = True
+Moore().ReplaceL0BanksWithEmulated = True
+Moore().UseDBSnapshot = False
+Moore().EnableRunChangeHandler = False
+Moore().CheckOdin = False
+Moore().Simulation = True
+Moore().DataType = '2010'
+
+Moore().EvtMax = 1000
+
+###
+# what to write and where? everything?
+#
+Moore().WriterRequires = []
+#Moore().WriterRequires = ['Hlt1MuTrackPostScaler']
+Moore().outputFile = 'tmp.dst'
+
+
+
+###
+# DB tags. MAKE SURE YOUR TAGS ARE RIGHT
+#
+#this should work (2010-10-05) on all MC2010
+Moore().DDDBtag = 'head-20100624'
+Moore().CondDBtag = 'sim-20100831-vc-md100'
+
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
+#layer needed to run on simulation
+CondDB().addLayer(dbFile = "/afs/cern.ch/user/g/graven/public/simcond-RunParameters-layer.db", dbName = "SIMCOND")
+
+###
+# some input data. this is Bs2mm signal MC
+#
+Moore().inputFiles = ['/castor/cern.ch/grid/lhcb/MC/2010/DST/00007617/0000/00007617_00000029_1.dst']
+
+
