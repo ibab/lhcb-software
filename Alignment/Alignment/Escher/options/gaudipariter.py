7489285083aa8b579e2edc9916860dcdff2ab0c1
Wouter Hulsbergen <wouterh@nikhef.nl>
2010-10-08 07:47:50 +0000

    add option to specify name of histogram file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@89768 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c3f145065a512b53f3bd98273a8fda8eeab765ed

--- Alignment/Alignment/Escher/options/gaudipariter.py
+++ Alignment/Alignment/Escher/options/gaudipariter.py
@@ -8,6 +8,7 @@ parser.add_option("-f","--firstiter",type="int", dest="firstiter",help="first it
 parser.add_option("-e","--numevents",type="int", dest="numevents",help="number of events", default=1000)
 parser.add_option("-p","--numprocesses",type="int", dest="numprocs",help="number of processes", default=8)
 parser.add_option("-d", "--aligndb", action = 'append', dest="aligndb",help="path to file with alignment database layer for first iteration")
+parser.add_option("-r", "--roothistofile",dest="histofile",help="name of histogram file",default = "histograms.root")
 (opts, args) = parser.parse_args()
 
 import os
@@ -25,7 +26,7 @@ for i in range(opts.firstiter,opts.numiter) :
     os.chdir ( iterdir )
 
     # beyond the first iteration, add the input database as an option
-    theseoptions = ' --numevents ' + str(opts.numevents) + ' --numprocesses ' + str(opts.numprocs) + ' --iter ' + str(i)
+    theseoptions = ' --numevents ' + str(opts.numevents) + ' --numprocesses ' + str(opts.numprocs) + ' --iter ' + str(i) + ' -r ' + opts.histofile + ' '
     if i>0 :
         previterdb = '../Iter' + str( i-1 ) + '/Alignment.db'
         theseoptions += ' --aligndb ' + previterdb
@@ -37,14 +38,15 @@ for i in range(opts.firstiter,opts.numiter) :
     # add the remaining options
     for a in args:
         theseoptions += ' ' + a
-            
+        os.system( ' cp -f ' + a + ' .' )
+
     # run the job
     thiscommand = '$ESCHEROPTS/gaudipar.py' + theseoptions + '>& logfile.txt'
     print 'command: ', thiscommand
     os.system( thiscommand )
     os.system( 'gzip logfile.txt' )
     # keep only the last version of the derivatives. they take too much space.
-    os.system( 'mv -f myderivatives.dat ..')
+    # os.system( 'mv -f myderivatives.dat ..')
     os.chdir(rundir)
 
 # create a single alignlog file
