9bc74cfae03faf5f2937bbcf88af03f528f661dd
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2010-10-11 18:34:58 +0000

    improve doxygen comments for new interface methods
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@87593 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4b61331863dfd731769f813e9e20ef44f2459a9

--- LHCb/Rich/RichKernel/RichKernel/IRichSnellsLawRefraction.h
+++ LHCb/Rich/RichKernel/RichKernel/IRichSnellsLawRefraction.h
@@ -32,7 +32,7 @@ namespace Rich
 {
 
   //-----------------------------------------------------------------------------
-  /** @class ISnellsLawRefraction IRichSnellsLawRefraction.h RichKernel/IRichSnellsLawRefraction.h
+  /** @class ISnellsLawRefraction RichKernel/IRichSnellsLawRefraction.h
    *
    *  Interface to a tool which implements snells law refraction at Aerogel / Rich1Gas
    *  boundary.
@@ -52,40 +52,48 @@ namespace Rich
      */
     static const InterfaceID& interfaceID() { return IID_IRichSnellsLawRefraction; }
 
-    /** Correct the direction vector and start point for refraction in going from Aerogel to Rich1Gas
+    /** Correct the direction vector and start point for refraction in going from Aerogel to Rich1Gas.
+     *
+     *  Uses the effective refractive index seen by the track segment in question.
      *
      *  @param[in]     startPoint The starting point
      *  @param[in,out] dir   The direction to update (WARNING Must be a unit vector)
-     *  @param[in]     trSeg The aerogel track segment assoicated to the photon direction to update
+     *  @param[in]     trSeg The aerogel track segment associated to the photon direction to update
      */
     virtual void aerogelToGas( Gaudi::XYZPoint & startPoint,
-                               Gaudi::XYZVector &    dir,
+                               Gaudi::XYZVector & dir,
                                const LHCb::RichTrackSegment& trSeg ) const = 0;
 
     /** Correct the direction vector and start point for refraction in going from Aerogel to Rich1Gas
      *
+     *  Uses a simple average of the refractive index for the radiator volumes
+     *
      *  @param[in]     startPoint The starting point
      *  @param[in,out] dir        The direction to update (WARNING Must be a unit vector)
      *  @param[in]     photonEnergy The energy of the photon (used to get the correct refractive indices)
      */
     virtual void aerogelToGas( Gaudi::XYZPoint & startPoint,
-                               Gaudi::XYZVector &     dir,
+                               Gaudi::XYZVector & dir,
                                const double photonEnergy ) const = 0;
 
     /** Correct the direction vector for refraction in going from Rich1Gas to aerogel
      *
+     *  Uses the effective refractive index seen by the track segment in question.
+     *
      *  @param[in,out] dir   The direction to update (WARNING Must be a unit vector)
-     *  @param[in]     trSeg The aerogel track segment assoicated to the photon direction to update
+     *  @param[in]     trSeg The aerogel track segment associated to the photon direction to update
      */
-    virtual void gasToAerogel( Gaudi::XYZVector &     dir,
-                               const LHCb::RichTrackSegment& trSeg  ) const = 0;
+    virtual void gasToAerogel( Gaudi::XYZVector & dir,
+                               const LHCb::RichTrackSegment& trSeg ) const = 0;
 
     /** Correct the direction vector for refraction in going from Rich1Gas to aerogel
+     *
+     *  Uses a simple average of the refractive index for the radiator volumes
      *
      *  @param[in,out] dir        The direction to update (WARNING Must be a unit vector)
      *  @param[in]     photonEnergy The energy of the photon (used to get the correct refractive indices)
      */
-    virtual void gasToAerogel( Gaudi::XYZVector &     dir,
+    virtual void gasToAerogel( Gaudi::XYZVector & dir,
                                const double photonEnergy ) const = 0;
 
   };
