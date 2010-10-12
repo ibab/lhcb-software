d7fc536eefba4dadb47068a9ade101357cf0f6a9
Gerhard Raven <gerhard.raven@nikhef.nl>
2010-10-12 15:05:26 +0000

    - fix GEC configuration: the code assumed a private tool, when it actually     is a public one...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@89829 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 00b0c39638121af76d55d9c6a6c3a578e57b3f59

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1GECs.py
@@ -5,13 +5,14 @@ def Hlt1GEC( reject = True ):
     from HltLine.HltLine import bindMembers, Hlt1Tool
     from Configurables import LoKi__VoidFilter as VoidFilter
     
-    tool = Hlt1Tool( Hlt__GEC, "ActualGECs", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 3000 )
+    # tool = Hlt1Tool( Hlt__GEC, "ActualGECs", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 3000 )
+    tool = Hlt__GEC("ActualGECs", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 3000 )
 
     rej  = VoidFilter("Hlt1GECReject")
-    rej.Code = "ACCEPT('%s')" % tool.createConfigurable(rej).getFullName()
+    rej.Code = "ACCEPT('%s')" % tool.getFullName()
 
     acc  = VoidFilter("Hlt1GECAccept")
-    acc.Code = "~ ACCEPT('%s')" % tool.createConfigurable(acc).getFullName()
+    acc.Code = "~ ACCEPT('%s')" % tool.getFullName()
 
     return bindMembers(None, [ DecodeIT, DecodeVELO, rej if reject else acc  ] )
 
@@ -23,13 +24,14 @@ def Hlt1GECLoose( reject = True ):
     from HltLine.HltLine import bindMembers, Hlt1Tool
     from Configurables import LoKi__VoidFilter as VoidFilter
     
-    tool = Hlt1Tool( Hlt__GEC, "ActualGECsLoose", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 4000 )
+    #tool = Hlt1Tool( Hlt__GEC, "ActualGECsLoose", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 4000 )
+    Hlt__GEC( "ActualGECsLoose", MaxOTHits = 10000, MaxITHits = 3000, MaxVeloHits = 4000 )
 
     rej  = VoidFilter("Hlt1GECRejectLoose")
-    rej.Code = "ACCEPT('%s')" % tool.createConfigurable(rej).getFullName()
+    rej.Code = "ACCEPT('%s')" % tool.getFullName()
 
     acc  = VoidFilter("Hlt1GECAcceptLoose")
-    acc.Code = "~ ACCEPT('%s')" % tool.createConfigurable(acc).getFullName()
+    acc.Code = "~ ACCEPT('%s')" % tool.getFullName()
 
     return bindMembers(None, [ DecodeIT, DecodeVELO, rej if reject else acc  ] )
     
