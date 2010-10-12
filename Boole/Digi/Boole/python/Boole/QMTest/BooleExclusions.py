82b1f18820d458328894fe27f94aa4ae1d338836
Marco Cattaneo <marco.cattaneo@cern.ch>
2010-10-12 15:38:04 +0000

    add test for 2010 latest tags
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@89831 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a0d0a24c50175122b407ce9fb3093b026e1b408e

--- Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
+++ Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
@@ -2,6 +2,8 @@ from GaudiTest import RegexpReplacer
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
+    LineSkipper(["SIMCOND_"]) + \
+    LineSkipper(["DDDB_"]) + \
     RegexpReplacer(when = "XmlParserSvc        ERROR DOM",
         orig = r'entity .*conddb:',
         repl = r'entity conddb:')
