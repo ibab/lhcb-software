d8c92e481231a7aa5cebcb65b3533582ff72ede4
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-10-12 07:50:29 +0000

    make use of Lomont from GaudiKernel, remove local
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@89801 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c379b772c5e94c3f0b896db0c698ee842e0127fb

--- LHCb/Kernel/LHCbMath/LHCbMath/Lomont.h
+++ LHCb/Kernel/LHCbMath/LHCbMath/Lomont.h
@@ -3,124 +3,24 @@
 #ifndef LHCBMATH_LOMONT_H 
 #define LHCBMATH_LOMONT_H 1
 // ============================================================================
+// Incldue files 
+// ============================================================================
+#include "GaudiKernel/Lomont.h"
+// ============================================================================
 namespace LHCb
 {
+  // ==========================================================================
   namespace Math 
   {
     // ========================================================================
-    /** equality comparison of float numbers using as the metric the maximal 
-     *  number of Units in the Last Place (ULP).
-     *  It is a slightly modified version of very efficient implementation 
-     *  of the initial Bruce Dawson's algorithm by Chris Lomont.
-     *
-     *  @see www.lomont.org 
-     *  @see http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
-     *
-     *  C.Lomont claims the algorithm is factor 2-10 more efficient 
-     *  with respect to  Knuth's algorithm fomr comparions of floating number 
-     *  using the relative precision.
-     *
-     *  The effective relative difference depends on the choice of 
-     *   <c>maxULPS</c>:
-     *  - For the case of maxULPs=1, (of cource it is totally unphysical case!!!)
-     *  the effective relative precision r = |a-b|/(|a|+|b|)is 
-     *  between 3.5e-8 and 5.5e-8 for |a|,|b|>1.e-37, and 
-     *  then it quickly goes to ~1 
-     *  - For the case of maxULPS=10 
-     *  the effective relative precision is 
-     *  between 3e-8 and 6e-7 for |a|,|b|>1.e-37, and 
-     *  then it quickly goes to ~1 
-     *  - For the case of maxULPS=100 
-     *  the effective relative precision is 
-     *  around ~6e-6 for |a|,|b|>1.e-37, and 
-     *  then it quickly goes to ~1 
-     *  - For the case of maxULPS=1000 
-     *  the effective relative precision is 
-     *  around ~6e-5 for |a|,|b|>1.e-37, and 
-     *  then it quickly goes to ~1 
-     *  
-     *  @param  af the first number 
-     *  @param  bf the second number 
-     *  @param  maxULPs the maximal metric deciation in the terms of 
-     *                 maximal number of units in the last place
-     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
-     *  @date 2008-11-08
-     */
-    bool lomont_compare_float
-    ( const float          af            , 
-      const float          bf            , 
-      const unsigned short maxULPs = 100 ) ;
+    using Gaudi::Math::lomont_compare_float  ;
+    using Gaudi::Math::lomont_compare_double ;
+    using Gaudi::Math::next_float            ;
+    using Gaudi::Math::next_double           ;
     // ========================================================================
-    /** equality comparison of float numbers using as the metric the maximal 
-     *  number of Units in the Last Place (ULP).
-     *  It is a slightly modified version of very efficient implementation 
-     *  of the initial Bruce Dawson's algorithm by Chris Lomont.
-     *
-     *  @see www.lomont.org 
-     *  @see http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
-     *
-     *  C.Lomont claims the algorithm is factor 2-10 more efficient 
-     *  with respect to  Knuth's algorithm fomr comparions of floating number 
-     *  using the relative precision.
-     *
-     *  The effective relative difference depends on the choice of 
-     *   <c>maxULPS</c>:
-     *  - For the case of maxULPs=1, (of cource it is totally unphysical case!!!)
-     *  the effective relative precision r = |a-b|/(|a|+|b|)is 
-     *  ~6e-16 for |a|,|b|>1.e-304, and 
-     *  then it quickly goes to ~1 
-     *  
-     *  @param  af the first number 
-     *  @param  bf the second number 
-     *  @param  maxULPs the maximal metric deciation in the terms of 
-     *                 maximal number of units in the last place
-     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
-     *  @date 2008-11-08
-     */
-    bool lomont_compare_double
-    ( const double         af      , 
-      const double         bf      , 
-      const unsigned int   maxULPs ) ;
-    // ========================================================================
-    /** Get the floating number that representation 
-     *  is different with respect  to the argument for 
-     *  the certain number of "Units in the Last Position".
-     *  For ulps=1, it is just next float number, for ulps=-1 is is the 
-     *  previous one.
-     *
-     *  This routine is very convinient to test the parameter maxULPS for
-     *  the routine LHCb::Math::lomont_compare_float 
-     *
-     *  @see LHCb:Math::lomont_compare
-     *  @param af the reference number 
-     *  @param ulps the bias 
-     *  @return the biased float number (on distance "ulps")
-     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
-     *  @date 2008-11-08
-     */  
-    float next_float ( const float af , const short ulps ) ;
-    // ========================================================================
-    /** Get the floating number that representation 
-     *  is different with respect  to the argument for 
-     *  the certain number of "Units in the Last Position".
-     *  For ulps=1, it is just next float number, for ulps=-1 is is the 
-     *  previous one.
-     *
-     *  This routine is very convinient to test the parameter maxULPS for
-     *  the routine LHCb::Math::lomont_compare_float 
-     *
-     *  @see LHCb:Math::lomont_compare
-     *  @param af the reference number 
-     *  @param ulps the bias 
-     *  @return the biased float number (on distance "ulps")
-     *  @author Vanya BELYAEV  Ivan.Belyaev@nikhef.nl
-     *  @date 2008-11-08
-     */  
-    double next_double ( const double  af , const short ulps ) ;
-    // ========================================================================
-  } // end of namespace LHCb::Math
+  } //                                              end of namespace LHCb::Math
   // ==========================================================================
-} // end of namespace LHCb
+} //                                                      end of namespace LHCb
 // ============================================================================
 // The END 
 // ============================================================================
