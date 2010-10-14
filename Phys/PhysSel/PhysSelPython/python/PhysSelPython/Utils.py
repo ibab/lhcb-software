88d11714eade179bf759002a5cd03d7e136dbac8
jpalac <jpalac@cern.ch>
2010-10-14 16:14:42 +0000

    - python/PhysSelPython/Wrappers.py   . New module PhysSelPython.Utils. Contains classes to transform selection-like     objects into selections with an output location completely bound to a new     name. Needed for stripping, where the stripping line is created from a selection     object.  - tests/test_utils.py   . Add tests for new functionality.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@89940 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 85f0175f329da2200888bc5c63e9dacb2a686934

---
+++ Phys/PhysSel/PhysSelPython/python/PhysSelPython/Utils.py
@@ -0,0 +1,91 @@
+"""Set of utilities for handling and acting upon selection-type objects.
+
+"""
+
+__author__ = 'Juan Palacios palacios@physik.uzh.ch'
+
+from copy import copy
+
+from SelPy.selection import FlatSelectionListBuilder   
+from PhysSelPython.Wrappers import MergedSelection
+
+def _getAlgoList(sel) :
+    return copy(FlatSelectionListBuilder(sel).selectionList)
+
+def _cloneSelection(sel, name) :
+    return sel.clone(name)
+
+def _mergedSelection(sel, name) :
+    return MergedSelection(name, RequiredSelections = [sel])
+
+def _eventSelection(sel, name) :
+    if len(sel.outputLocation) == 0 :
+        return sel
+    else :
+        return _mergedSelection(sel, name)
+    
+
+class dummy(object) :
+    '''
+    This class should return a list of configurables and an output location.
+    '''
+
+    _getMemberAlgs = {'MergedSelection' : lambda x : [x.algorithm()]   }
+
+    _getSelectionAlgs = { 'Selection'      : _cloneSelection,
+                          'MergedSelection': _cloneSelection,
+                          'EventSelection' : _eventSelection,
+                          'AutomaticData'  : _mergedSelection  }
+    
+    
+    def __init__(self, name, selection) :
+
+        self._sel = self.getSelection(selection, name)
+        self._members = self.getMembers(self._sel)
+        self._outputLocation = self._sel.outputLocation()
+
+    def getMembers(self, selection) :
+        selTypeName = type(selection).__name__
+        return dummy._getMemberAlgs.get(selTypeName, _getAlgoList)(selection)
+
+    def getSelection(self, selection, name) :
+        selTypeName = type(selection).__name__
+        return dummy._getSelectionAlgs.get(selTypeName, lambda x : x)(selection, name)
+    
+    def outputLocation(self) :
+        return self._outputLocation
+    
+    def members(self) :
+        return copy(self._members)
+
+    def selection(self) :
+        return self._sel
+
+def _getAlgoAsList(sel) :
+    return [sel.algorithm()]
+
+def _handleGenericSelectionType(self, name, sel) :
+    self._members += _getAlgoList(sel)
+    loc = sel.outputLocation()
+    self._outputsel = loc
+    self._outputloc = loc
+    
+def _handle_Selection(self, name, alg) :
+    sel = alg.clone(name)
+    self._handleGenericSelectionType( name, sel )
+
+def _handle_EventSelection(self, name, alg) :
+    sel = MergedSelection(name, RequiredSelections = [alg])    
+    self._handleGenericSelectionType( name, sel )
+
+def _handle_MergedSelection(self, name, alg) :
+    sel = alg.clone(name)
+    self._members += _getAlgoAsList(sel)
+    loc = sel.outputLocation()
+    self._outputsel = loc
+    self._outputloc = loc
+    
+def _handle_AutomaticData(self, name, alg) :
+    sel = MergedSelection(name, RequiredSelections = [alg])
+    self._handleGenericSelectionType( name, sel )
+                                                                                                                                                                                                                                        
---
+++ Phys/PhysSel/PhysSelPython/tests/test_utils.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#
+# test Selection-like classes
+#
+import sys
+from py.test import raises
+sys.path.append('../python')
+from PhysSelPython.Wrappers import ( Selection,
+                                     AutomaticData,
+                                     MergedSelection,
+                                     EventSelection,
+                                     NameError,
+                                     NonEmptyInputLocations,
+                                     IncompatibleInputLocations )
+from PhysSelPython.Utils import dummy
+
+from SelPy.configurabloids import DummyAlgorithm, DummySequencer
+
+def test_dummy_automaticdata() :
+    data = AutomaticData(Location='Phys/Data0')
+    newData = dummy('NewData0', data)
+    assert newData.selection().name() == 'NewData0'
+    assert newData.outputLocation() == newData.selection().outputLocation() == 'Phys/NewData0'
+    assert len(newData.members()) == 1
+    print newData.members()[0].Members
+    assert len(newData.members()[0].Members) == 2
+    
+def test_dummy_mergedselection() :
+    data0 = AutomaticData(Location='Phys/Data0')
+    data1 = AutomaticData(Location='Phys/Data1')
+    mergedData = MergedSelection('MergedData01', RequiredSelections = [data0, data1])
+    newData = dummy('NewMergedData', mergedData)
+    assert newData.selection().name() == 'NewMergedData'
+    assert newData.outputLocation() == newData.selection().outputLocation() == 'Phys/NewMergedData'
+    assert len(newData.members()) == 1
+    assert len(newData.members()[0].Members) == 3
+    
+def test_dummy_selection() :
+    data0 = AutomaticData(Location='Phys/Data0')
+    data1 = AutomaticData(Location='Phys/Data1')
+    algo = DummyAlgorithm('_sel01')
+    sel = Selection('Sel01', Algorithm = algo, RequiredSelections = [data0, data1])
+    newData = dummy('NewSel01', sel)
+    assert newData.selection().name() == 'NewSel01'
+    assert newData.outputLocation() == newData.selection().outputLocation() == 'Phys/NewSel01'
+    assert len(newData.members()) == 3
+
+def test_eventSelection():
+    alg = DummyAlgorithm('evtSel')
+    evtSel = EventSelection(alg)
+    newSel = dummy('NewEvtSel', evtSel)
+    assert newSel.selection().name() == 'NewEvtSel'
+    assert newSel.outputLocation() == ''
+    assert len(newSel.members()) == 1
+
+
+
+if '__main__' == __name__ :
+
+    import sys
+
+    test_names = filter(lambda k : k.count('test_') > 0, locals().keys())
+
+    __tests = filter( lambda x : x[0] in test_names, locals().items())
+    
+
+    message = ''
+    summary = '\n'
+    length = len(sorted(test_names,
+                        cmp = lambda x,y : cmp(len(y),len(x)))[0]) +2
+    
+    for test in __tests :
+        try :
+            test[1]()
+            message = 'PASS'
+        except :
+            message = "FAIL"
+        summary += test[0].ljust(length) + ':' + message.rjust(10) + '\n'
+
+    if summary.count('FAIL') > 0 :
+        message = 'FAIL'
+        wr = sys.stderr.write
+    else :
+        message = 'PASS'
+        wr = sys.stdout.write
+
+    summary += 'Global'.ljust(length) + ':' + message.rjust(10) + '\n\n'
+    wr(summary)
+        
