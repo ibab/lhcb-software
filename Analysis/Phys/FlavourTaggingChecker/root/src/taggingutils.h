2f9bfb5ea8193c8ba0dfedaab9da9bbe7b55dffd
musy <musy@cern.ch>
2010-10-24 22:53:16 +0000

    IncludeFile fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@95108 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 32b0ba89f76ad1507dad7049b2e2a19c92b8944b

--- Analysis/Phys/FlavourTaggingChecker/root/src/taggingutils.h
+++ Analysis/Phys/FlavourTaggingChecker/root/src/taggingutils.h
@@ -32,7 +32,9 @@ typedef std::map<const TString, const TString > propertyStringMap;
 bool isinTextFile(int run, int evt, TString& s);
 bool isinTextFile(int run, int evt);
 
-TString readString(TString varname);
+int getwordnr(TString line, TString tok=" ,;:");
+TString getword(int i, TString line, TString tok=" ,;:");
+TString readString(TString varname,TString optsfilename="tag.opts");
 double  read(TString varname) ;
 double  read(const char* varname  ) ;
 
