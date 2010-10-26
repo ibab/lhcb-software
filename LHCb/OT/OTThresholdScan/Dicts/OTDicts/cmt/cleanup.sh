e43549c37bc5fce0043e8e7bcf771dedabe69bd8
dvaneijk <dvaneijk@cern.ch>
2010-10-26 16:07:30 +0000

    delete OTThresholdScan, shouldn't be in LHCb
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@95267 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e74fe9fa99b63c903e5d15f791a19739980522b9

--- LHCb/OT/OTThresholdScan/Dicts/OTDicts/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/cern.ch/sw/contrib/CMT/v1r20p20090520; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=OTDicts -version=v1r0 -path=/afs/cern.ch/user/d/dvaneijk/cmtuser/Brunel_v37r4/Dicts $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
