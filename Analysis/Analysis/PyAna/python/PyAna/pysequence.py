764674634df72620f4a9b6417ed61c2ccba73a17
jpalac <jpalac@cern.ch>
2010-10-28 07:02:09 +0000

    - Added PyAna.pysequence module   . Contains helpers for iterable objects.  - Increase to v1r3
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@95379 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2e6fcc53efbc6ec38e9028cdd4086b97fb9939d7

---
+++ Analysis/Analysis/PyAna/python/PyAna/pysequence.py
@@ -0,0 +1,24 @@
+''' Helpers functions and classes for sequence manipulation.
+'''
+__author__ = "Juan Palacios <juan.palacios@nikhef.nl>"
+
+__all__ = ('isiterable',
+           'flattenList')
+def isIterable(obj) :
+    '''
+    Test if an object is iterable but not a string type.
+    '''
+    return hasattr(obj, "__iter__") and not isinstance(obj, basestring)
+
+def flattenList(tree) :
+    """flattenList(tree) -> flat list
+    Return a flat list containing all the non-iterable elements retrieved tree and tree's sub-trees.
+    """
+    flatList = []
+    for member in tree :
+        if isIterable(member) :
+            flatList.extend(flattenList(member))
+        else :
+            flatList.append(member)
+        return flatList
+                                                        
