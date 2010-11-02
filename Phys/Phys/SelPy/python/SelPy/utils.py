a172017828a6d1894be37295a7592bc95429de73
jpalac <jpalac@cern.ch>
2010-11-02 22:44:36 +0000

    Add utilities for creating flat lists of selections from dependency trees, removing duplicates, etc.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@95560 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b9eb1ba5e3d625af8e47150b27cf5dc958368e72

---
+++ Phys/Phys/SelPy/python/SelPy/utils.py
@@ -0,0 +1,53 @@
+"""
+General helpers for maniplating selections and lists.
+"""
+__author__ = "Juan PALACIOS palacios@physik.uzh.ch"
+
+__all__ = ( 'flatSelectionList',
+            'treeSelectionList',
+            'flattenList',
+            'isIterable',
+            'removeDuplicates'   )
+
+def treeSelectionList(selection) :
+    """
+    Return a nested list with all the selections needed by a selection, including itself.
+    """
+    _selList = [treeSelectionList(sel) for sel in selection.requiredSelections] + [selection]
+    return _selList
+
+def isIterable(obj) :
+    '''
+    Test if an object is iterable but not a string type.
+    '''
+    return hasattr(obj, "__iter__") and not isinstance(obj, basestring)
+
+def flattenList(tree) :
+    """flattenList(tree) -> flat list
+    Return a flat list containing all the non-iterable elements retrieved tree and tree's sub-trees.
+    """
+    flatList = []
+    for member in tree :
+        if isIterable(member) :
+            flatList.extend(flattenList(member))
+        else :
+            flatList.append(member)
+    return flatList
+
+def flatSelectionList(selection) :
+    """
+    Return a flat selection list containing all the selections required
+    by selection, in the correct order of execution.
+    """
+    return flattenList(treeSelectionList(selection))
+                                                                        
+def removeDuplicates(obj_list) :
+    """
+    Remove all but the first instance of an object from the a list.
+    """
+    clean_list = []
+    for obj in obj_list :
+        if obj not in clean_list :
+            clean_list.append(obj)
+    return clean_list
+                                
