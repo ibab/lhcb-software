9f4775644c34c3540d7b360f6c9556d8463ce5d1
rlambert <rlambert@cern.ch>
2010-11-12 14:24:50 +0000

    Remove print statement bug #75257
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@101226 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6bc0f1ce29419d63d9262b970e4b66f312edfa66

--- LHCb/Phys/LoKiGen/python/LoKiGen/trees.py
+++ LHCb/Phys/LoKiGen/python/LoKiGen/trees.py
@@ -4,12 +4,12 @@
 #  The set of basic decorator for Tree-objects from LoKiPhys library
 #  The file is a part of LoKi and Bender projects
 #
-#        This file is a part of LoKi project - 
+#        This file is a part of LoKi project -
 #    "C++ ToolKit  for Smart and Friendly Physics Analysis"
 #
 #  The package has been designed with the kind help from
-#  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
-#  contributions and advices from G.Raven, J.van Tilburg, 
+#  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas,
+#  contributions and advices from G.Raven, J.van Tilburg,
 #  A.Golutvin, P.Koppenburg have been used in the design.
 #
 #  @author Vanya BELYAEV ibelyaev@physics.syr.edu
@@ -17,16 +17,16 @@
 """
 The set of basic decorators for Tree-objects from LoKiGen library
 
-      This file is a part of LoKi project - 
+      This file is a part of LoKi project -
 'C++ ToolKit  for Smart and Friendly Physics Analysis'
 
 The package has been designed with the kind help from
-Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
-contributions and advices from G.Raven, J.van Tilburg, 
+Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas,
+contributions and advices from G.Raven, J.van Tilburg,
 A.Golutvin, P.Koppenburg have been used in the design.
 """
 # =============================================================================
-__author__ = "Vanya BELYAEV ibelyaev@physics.syr.edu" 
+__author__ = "Vanya BELYAEV ibelyaev@physics.syr.edu"
 # =============================================================================
 
 import LoKiCore.decorators    as      _LoKiCore
@@ -49,14 +49,14 @@ _decorated = _LoKiCore.decorateTrees (
       Decays.Trees.GenPhotos     ,
       Decays.Trees.GenPhotosOptional ) , LoKi.Dicts.TreeOps( _GP ) )
 
-print dir( LoKi.Dicts.TreeOps(_GP) )
+#print dir( LoKi.Dicts.TreeOps(_GP) )
 
 ## decay trees
 iGTree             = Decays.iTree_        ( _GP )
 GTree              = Decays.Tree_         ( _GP )
 GAny               = Decays.Trees.Any_    ( _GP ) () ## instance!!!
 GNone              = Decays.Trees.None_   ( _GP ) () ## instance!!!
-GStable            = Decays.Trees.Stable_ ( _GP ) 
+GStable            = Decays.Trees.Stable_ ( _GP )
 GenExclusive       = Decays.Trees.GenExclusive
 GenInclusive       = Decays.Trees.GenInclusive
 GenOptional        = Decays.Trees.GenOptional
@@ -68,12 +68,12 @@ IGenDecay = Decays.IGenDecay
 GenFinder = _LoKiCore.decorateFinder (
     IGenDecay.Finder               ,
     LoKi.Dicts.FinderDicts_(_GP)
-    ) 
+    )
 
 # =============================================================================
-if '__main__' == __name__ :
-    for o in dir() : print o        
-    
+#if '__main__' == __name__ :
+#    for o in dir() : print o
+
 # =============================================================================
-# The END 
+# The END
 # =============================================================================
