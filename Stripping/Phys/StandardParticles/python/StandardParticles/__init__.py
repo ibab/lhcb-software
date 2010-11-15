aefbced93f950cbf2ca73e737fd47771bc16a566
jpalac <jpalac@cern.ch>
2010-11-15 15:44:57 +0000

    Add some printout.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@102767 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4728048ce4a643099038dd587b7ea0a53c41fd9

--- Stripping/Phys/StandardParticles/python/StandardParticles/__init__.py
+++ Stripping/Phys/StandardParticles/python/StandardParticles/__init__.py
@@ -1,20 +1,32 @@
-"""Module containing AutomaticDatas corresponding to common particles from CommonParticles.StandardBacis and CommonParticles.StandardIntermediate.
+"""Module containing AutomaticDatas corresponding to common particles from CommonParticles.StandardBasic.
 """
 
 __author__ = 'Juan Palacios palacios@physik.uzh.ch'
 
+__all__ = ['addParticleModule',
+           'selections']
 
 from sys import modules
 _this = modules[__name__]
 from CommonParticles import StandardBasic
 from PhysSelPython.Wrappers import AutomaticData
 
-def addModuleToSelections(particleModule) :
+selections = []
+
+def addParticleModule(particleModule) :
     """
     Add all the common particles in particleModule so Selections module.
     """
     for loc in particleModule.locations.keys() :
         name = loc.split('/')[-1]
-        setattr(_this,  name, AutomaticData(Location =  loc))
+        global selections
+        sel =  AutomaticData(Location =  loc)
+        selections += [sel]
+        setattr(_this,  name, sel)
 
-addModuleToSelections(StandardBasic)
+addParticleModule(StandardBasic)
+
+if __name__ == '__main__' :
+    print '\nStandardParticles: available Selections:\n'
+    for sel in selections :
+        print '\t', sel.name()
