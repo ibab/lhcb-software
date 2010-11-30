ade8108e1b16e4a9d0161d5459edea89f4bde5d4
jpalac <jpalac@cern.ch>
2010-11-30 14:42:46 +0000

    Add .py files for merge-v0 and merge-radiative tests so that they can be run without QM.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@104298 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 36b4b45e4e50e450c9d81229bbcae06174c91e60

---
+++ DaVinci/DaVinciTests/tests/options/fsrs/davinci-merge-radiative.py
@@ -0,0 +1,11 @@
+from GaudiConf.Configuration import *
+importOptions('$DAVINCIROOT/options/DaVinci-Default.py')
+importOptions('$APPCONFIGOPTS/Merging/DVMergeDST.py')
+importOptions('$APPCONFIGOPTS/DaVinci/DataType-2010.py')
+importOptions('$APPCONFIGOPTS/Merging/WriteFSR.py')
+importOptions('$DAVINCITESTSROOT/tests/options/fsrs/davinci-merge.py')
+from Configurables import DaVinci
+DaVinci().InputType="DST"
+EventSelector().Input = ["DATAFILE='PFN:MagUp.Radiative.dst' TYP='POOL_ROOTTREE' OPT='READ'", "DATAFILE='PFN:MagDown.Radiative.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+
+IODataManager().AgeLimit=2
