0225bd99264079bc304e8ec576585115a3d3da34
Vanya Belyaev <ivan.belyaev@cern.ch>
2010-12-03 08:52:36 +0000

    first import of KalmanFilter
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@114330 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6e2ee1bc6b99b8290a1aac1b1b2933be06a1df48

---
+++ LHCb/Phys/KalmanFilter/KalmanFilter/ParticleTypes.h
@@ -0,0 +1,76 @@
+// $Id: $
+// ============================================================================
+#ifndef KALMANFILTER_PARTICLETYPES_H 
+#define KALMANFILTER_PARTICLETYPES_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// STD & STL 
+// ============================================================================
+#include <string>
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/Kernel.h"
+// ============================================================================
+/** @file 
+ *
+ *  This file is a part of 
+ *  <a href="http://cern.ch/lhcb-comp/Analysis/LoKi/index.html">LoKi project:</a>
+ *  ``C++ ToolKit for Smart and Friendly Physics Analysis''
+ *
+ *  This file is a part of LoKi project - 
+ *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
+ *
+ *  The package has been designed with the kind help from
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+ *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  A.Golutvin, P.Koppenburg have been used in the design.
+ *
+ *   By usage of this code one clearly states the disagreement 
+ *    with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+ *  @date   2006-04-12
+ *  
+ *                    $Revision$
+ *  Last modification $Date$
+ *                 by $Author$
+ * 
+ */
+// ============================================================================
+namespace LoKi
+{
+  // ==========================================================================
+  namespace KalmanFilter
+  {
+    // ========================================================================
+    /** @enum ParticleType 
+     *  the particle types for Kalman Filter-based vertex fitter
+     *  @author Vanya BELYAEV Ivan.Belyaev@nikhef.nl
+     *  @date 2010-01-04
+     */
+    enum ParticleType {
+      // ======================================================================
+      /// Unknown/unspecified particle type 
+      UnspecifiedParticle = 0 ,            // Unknown/unspecified particle type 
+      /// Short-lived (verticing) particles
+      ShortLivedParticle      ,                  //        Short-lived particle
+      /// Long-lived/stable particle
+      LongLivedParticle       ,                  //  Long-lived/stable particle
+      /// Photon-like 
+      GammaLikeParticle       ,                  //        Photon-like particle 
+      /// Di-Photon-like 
+      DiGammaLikeParticle                        //     Di-Photon-like particle 
+      // ======================================================================
+    } ;
+    // ========================================================================
+  } ; //                                    end of namespace LoKi::KalmanFilter 
+  // ==========================================================================
+} //                                                      end of namespace LoKi
+// ============================================================================
+//                                                                      The END 
+// ============================================================================
+#endif // KALMANFILTER_PARTICLETYPES_H
+// ============================================================================
