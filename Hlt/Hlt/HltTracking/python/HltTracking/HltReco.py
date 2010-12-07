392d7bb287bb8251e4b1ab22341d8c99107d86c7
Johannes Albrecht <johannes.albrecht@cern.ch>
2010-12-07 16:16:57 +0000

    cleanup
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@114471 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f722fe41bcde55451deb0f1b7ac27f48ad1a465a

--- Hlt/Hlt/HltTracking/python/HltTracking/HltReco.py
+++ Hlt/Hlt/HltTracking/python/HltTracking/HltReco.py
@@ -90,9 +90,6 @@ from Configurables import Tf__PatVeloSpaceTracking, Tf__PatVeloSpaceTool
 patVeloR = Tf__PatVeloRTracking('HltRecoRZVelo', OutputTracksName = _baseTrackLocation(HltSharedTracksPrefix,HltSharedRZVeloTracksName) ) 
 
 if useFastVelo :
-    #recoVelo = FastVeloTracking( 'FastVeloHlt', OutputTracksName = "Hlt/Track/Velo" )
-    #recoVelo.MinRSensor = 4
-    #recoVelo.BestEfficiency = False
     recoVelo = FastVeloTracking( 'FastVeloHlt', OutputTracksName = "Hlt/Track/Velo" )
     recoVelo.HLT1Only = True
    
