7fcec41ca55861d069ba9cd8e46a87171979473e
Matthew David Needham <mneedham@cern.ch>
2010-12-07 10:11:08 +0000

    Update XComp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@114450 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e97b1ddd04038b5415debf59932a6cbdaf0e26cb

--- Analysis/Phys/XNote/XComp.C
+++ Analysis/Phys/XNote/XComp.C
@@ -42,16 +42,35 @@ void XComp(){
   Errors[nov-1] = sqrt(1/sumWeight); 
   Values[nov-1]= avg;  
 
-  std::cout << "Average mass " << avg << " +/- " << sqrt(1/sumWeight) << std::endl;
+  char cvalue[100];
+  float e = sqrt(1/sumWeight);
+  sprintf(cvalue, "Average %.2f", avg);
+
+  std::string header(cvalue);
+
+  sprintf(cvalue, "%.2f", e);
+  std::string footer(cvalue);
+
+  std::string averageString = header + " #pm " + footer;
+
+
+  //std::cout << aString << endl;
+
+  //sprintf(cvalue, "%f", avg);
+  //  std::cout << cvalue << std::endl;
+  //  std::cout << "Average mass " << avg << " +/- " << sqrt(1/sumWeight) << std::endl;
 
   Int_t Colors[nov]={1,1,1,1,1,1};
   char* Labels[nov] = {"CDF", "Babar B^{+}", "Babar B^{0}", "D0", "Belle", "Avg"};
+  Labels[nov-1] = averageString.c_str(); 
  
   float MinX = 3860;
-  float MaxX = 3880;
+  float MaxX = 3878;
   float MinY = 0.;
   float MaxY = nov + 1;
 
+  float xMinAvg = avg - e;
+  float xMaxAvg = avg + e;
   
   TH2F* frame = new TH2F("frame","",8, MinX,MaxX,10,MinY,MaxY);   
   frame->SetStats(0);
@@ -60,14 +79,20 @@ void XComp(){
   frame->GetXaxis()->SetLabelFont(132);
   frame->GetXaxis()->SetTitle("X(3872) Mass [MeV/c^{2}]");
   frame->GetXaxis()->SetTitleFont(132);
+  frame->GetXaxis()->SetLabelFont(132);
 
   //  frame->Draw("axissame");
   TLatex label;
   label.SetTextAlign(22);
-  label.SetTextSize(0.027);
+  label.SetTextFont(132);
+  label.SetTextSize(0.04);
+  TBox* pav = new TBox(xMinAvg,MinY,xMaxAvg , MaxY);   
+  pav->SetLineColor(0);
+  pav->SetFillColor(31);
+  pav->Draw();
 
   
-  for (int i=0;i<nov;i++){
+  for (int i=0;i<nov ;i++){
      Float_t x1=Values[i]-Errors[i];
      Float_t xc=Values[i];
      Float_t x2=Values[i]+Errors[i];
@@ -84,13 +109,13 @@ void XComp(){
      pav->SetLineColor(0);
      pav->SetFillColor(91);
      if (i == nov-1) pav->SetFillColor(99);
-     pav->Draw();
+     //pav->Draw();
      l->Draw();
      pt->Draw();
      g1->Draw();
      //     pav->Draw();
      g2->Draw();
-     label.DrawLatex(MinX+(MaxX-MinX)*0.1,y,Labels[i]);
+     label.DrawLatex(MinX+(MaxX-MinX)*0.18,y,Labels[i]);
   }
 
   frame->Draw("axissame");
