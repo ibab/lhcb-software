9e3008b7a7aacc7c8a494c329b19b0c01c9eaae9
jpalac <jpalac@cern.ch>
2011-01-10 17:47:25 +0000

    - DaVinciUtils/Functors.h   . New file containing some unary predicates that check if a ContainedObject     is in the TES.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@115206 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2b5defefc88c50e8a27105925bd3466b98e51428

---
+++ Phys/Phys/DaVinciUtils/DaVinciUtils/Functors.h
@@ -0,0 +1,65 @@
+// $Id: $
+#ifndef DAVINCIUTILS_FUNCTORS_H 
+#define DAVINCIUTILS_FUNCTORS_H 1
+
+// Include files
+
+/** @class Functors Functors.h DaVinciUtils/Functors.h
+ *  
+ *  General helper functions for use in DaVinci.
+ *  These should only know about Gaudi classes and patterns.
+ *  No knowledge of DaVinci specific code should be assumed.
+ * 
+ *
+ *  @author Juan Palacios
+ *  @date   2011-01-10
+ */
+
+namespace DaVinci 
+{
+
+namespace Utils
+{
+
+  typedef std::unary_function<const ContainedObject*, bool> ContainedObjectPredicate;
+  
+
+  ///  Functor to check if a DataObject is in the TES.
+  //  template <class T> 
+  struct InTES : public std::unary_function<const ContainedObject*, bool> 
+  {
+
+    inline bool operator() (const ContainedObject* obj) const    
+    {
+      std::cout << "DaVinci::Utils::InTES!!!!" << std::endl;
+      return ( 0!=obj && 0!=obj->parent()) ;
+    }
+    
+  };
+
+  ///  Return true for all objects.
+  template <class T>
+  struct TRUE : public std::unary_function<const T*, bool> {
+
+    inline bool operator() (const T*) {
+      return true;
+    }
+    
+  };
+
+  ///  Return false for all objects.
+  template <class T>
+  struct FALSE : public std::unary_function<const T*, bool> {
+
+    inline bool operator() (const T*) {
+      return false;
+    }
+    
+  };
+
+
+} // namespace Utils
+  
+} // namespace DaVinci
+
+#endif // DAVINCIUTILS_FUNCTORS_H
