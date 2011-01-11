fdc7fcdd07aeb176be06e6c3fc97180e9771100f
jpalac <jpalac@cern.ch>
2011-01-11 10:45:46 +0000

    - Adapt to changes in DVAlgorithm protected methods.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@115233 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 766b3fd8d808288971bc5d4c4bdfb7d8242f995a

--- Phys/Phys/LoKiAlgo/src/Algo.cpp
+++ Phys/Phys/LoKiAlgo/src/Algo.cpp
@@ -368,7 +368,7 @@ StatusCode LoKi::Algo::save
   if( 0 == particle ) 
   { return Error("save('"+tag+"'): invalid particle could not be saved!") ; }
   
-  const LHCb::Particle* saved = this->cloneAndMark( particle );
+  const LHCb::Particle* saved = this->cloneAndMarkTree( particle );
   
   if( 0 == saved ) { return Error("Particle '"+tag+"' is not kept! " ) ; }
   
