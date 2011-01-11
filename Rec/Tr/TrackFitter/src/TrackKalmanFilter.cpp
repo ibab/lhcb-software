9ed664e276c2ef0e12fc8d81a252daee98e8b8ea
Victor Coco <victor.coco@cern.ch>
2011-01-11 15:01:38 +0000

    get the inverse matrix as reference in predictReverseFit in TrackKalmanFilter
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@115244 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b41ffa79dd979432bb4645405c0e5c1fadf0a38

--- Rec/Tr/TrackFitter/src/TrackKalmanFilter.cpp
+++ Rec/Tr/TrackFitter/src/TrackKalmanFilter.cpp
@@ -356,7 +356,7 @@ StatusCode TrackKalmanFilter::predictReverseFit(const FitNode& prevNode,
   double z = aNode.z() ;
   //assert(fabs( prevNode.z() - aState.z()) < TrackParameters::propagationTolerance) ;
   if( fabs(z - aState.z()) > TrackParameters::propagationTolerance ) {
-    TrackMatrix invF = prevNode.invertTransportMatrix();
+    const TrackMatrix& invF = prevNode.invertTransportMatrix();
     if ( ! prevNode.transportIsInverted())
       return failure( "unable to invert matrix in predictReverseFit" );
     
