25c44ac42bd08e832e5e5c01599a548bc682cf64
jpalac <jpalac@cern.ch>
2011-01-19 19:24:11 +0000

    Use __dev__ dst writers.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@115585 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ff6df73cd76a4c584d26790085e58925bdfcf2de

--- Analysis/Phys/DSTWriterTests/options/davinci_mdst_mergedselection.py
+++ Analysis/Phys/DSTWriterTests/options/davinci_mdst_mergedselection.py
@@ -1,5 +1,6 @@
 from Gaudi.Configuration import *
-from Configurables import DaVinci, MicroDSTWriter
+from Configurables import DaVinci #, MicroDSTWriter
+from DSTWriters.__dev__.Configuration import MicroDSTWriter
 
 from PhysSelPython.Wrappers import AutomaticData, SelectionSequence, MergedSelection, MultiSelectionSequence
 from Configurables import TESCheck
@@ -11,27 +12,32 @@ jpsis = AutomaticData(Location = 'Phys/StdLooseJpsi2MuMu')
 
 allSelection = MergedSelection('MergeAll', RequiredSelections = [pions, kaons, muons, jpsis] )
 
-pionSeq = SelectionSequence('SeqPions', pions )
-muonSeq = SelectionSequence('SeqMuons', muons )
+pionSeq = SelectionSequence('SeqPions', TopSelection = pions )
+muonSeq = SelectionSequence('SeqMuons', TopSelection = muons )
 
-kaonSeq =  SelectionSequence('SeqKaons', kaons)
-jpsiSeq =  SelectionSequence('SeqJpsi', jpsis )
+kaonSeq =  SelectionSequence('SeqKaons', TopSelection = kaons)
+jpsiSeq =  SelectionSequence('SeqJpsi', TopSelection = jpsis )
 
 mergeSeq = SelectionSequence('SeqMerge', TopSelection = allSelection )
+
+#mergeSeq.algorithm().OutputLevel=1
+
+mergeAlg = getConfigurable("MergeAll")
+mergeAlg.OutputLevel=1
+mergeAlg.ForceP2PVBuild = True
 
 multiSeq = MultiSelectionSequence('CocktailB', Sequences = [kaonSeq, pionSeq, muonSeq, jpsiSeq, mergeSeq])
 
 conf = MicroDSTWriter("MicroDST0", 
-                      OutputFileSuffix = "Test",
-                      SelectionSequences = [multiSeq],
-                      CopyFlavourTags = False)
+                      OutputFileSuffix = "Test4",
+                      SelectionSequences = [multiSeq])
 
-selDST0Seq = conf.sequence()
+#selDST0Seq = conf.sequence()
 
 dv = DaVinci()
 dv.DataType = '2010'
 dv.InputType = 'SDST'
 dv.EvtMax = -1
-dv.UserAlgorithms = [selDST0Seq]
+dv.UserAlgorithms = [conf.sequence()]
 EventSelector().Input = ["DATAFILE='PFN:castor:/castor/cern.ch/user/c/cattanem/testFiles/Brunel-v37r1-069857_0000000006-1000ev.sdst' TYP='POOL_ROOTTREE' OPT='READ'"]
 FileCatalog().Catalogs =['xmlcatalog_file:TestSDSTCatalog.xml']
