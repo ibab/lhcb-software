736c099378815a2498099219811f1b0923fbbfb1
Patrick Robbe <robbe@lal.in2p3.fr>
2011-01-20 16:17:49 +0000

    Add options in python format
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@115692 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1a9b858caa1fa86f474520ad3e4dc4a2ea8b954e

---
+++ Gauss/Gen/LbBcVegPy/options/BcVegPy.py
@@ -0,0 +1,23 @@
+# $Id$ 
+
+from Configurables import Generation, Special, BcVegPyProduction
+
+BcVegPyListOfCommands = [
+  "mixevnt imix 1" ,
+  "mixevnt imixtype 1" ,
+  "counter ibcstate 1",
+  "vegasinf nitmx 20",
+  "vegasinf number 1000000",
+  "loggrade ivegasopen 0",
+  "loggrade igrade 1",
+  "upcom ecm 7000.0"
+]
+
+gen = Generation()
+gen.addTool( Special , name = "Special" )
+gen.Special.ProductionTool = "BcVegPyProduction"
+gen.Special.addTool( BcVegPyProduction , name = "BcVegPyProduction" )
+gen.Special.BcVegPyProduction.Commands += BcVegPyListOfCommands
+gen.PileUpTool = "FixedLuminosityForRareProcess";
+
+
