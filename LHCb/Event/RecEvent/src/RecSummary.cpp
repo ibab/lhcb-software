884b037cad5a9f019b21f3024d45c61782415016
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-01-20 09:20:10 +0000

    ! 2011-01-19 - Chris Jones  - Add new LHCb::RecSummary class, to store some basic summary information from    the reconstruction.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@115632 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 30d9c893fec6296d57cecea1e9083dd52417b315

---
+++ LHCb/Event/RecEvent/src/RecSummary.cpp
@@ -0,0 +1,24 @@
+// $Id: $
+
+#include "Event/RecSummary.h"
+
+//-----------------------------------------------------------------------------
+
+std::ostream& LHCb::RecSummary::fillStream(std::ostream& s) const
+{
+  s << "{ Summary Info :";
+  if ( hasInfo(LHCb::RecSummary::nPVs) )
+    s << " nPVs = " << info( LHCb::RecSummary::nPVs, 0 );
+  if ( hasInfo(LHCb::RecSummary::nLongTracks) )
+    s << " nLongTracks = " << info( LHCb::RecSummary::nLongTracks, 0 );
+  if ( hasInfo(LHCb::RecSummary::nDownstreamTracks) )
+    s << " nDownstreamTracks = " << info( LHCb::RecSummary::nDownstreamTracks, 0 );
+  if ( hasInfo(LHCb::RecSummary::nUpstreamTracks) )
+    s << " nUpstreamTracks = " << info( LHCb::RecSummary::nUpstreamTracks, 0 );
+  if ( hasInfo(LHCb::RecSummary::nVeloTracks) )
+    s << " nVeloTracks = " << info( LHCb::RecSummary::nVeloTracks, 0 );
+  if ( hasInfo(LHCb::RecSummary::nTTracks) )
+    s << " nTTracks = " << info( LHCb::RecSummary::nTTracks, 0 );
+  return s << " }";
+}
+
