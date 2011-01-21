7a721a2558bc9cea110ee89ea77eec44836b8a46
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-01-21 12:15:15 +0000

    ! 2011-01-21 - Chris Jones  - Convert reconstruction summary data object to internally use a map<int,int>    instead of a map<int,double>.  - Add various new summary values (IT,OT,SPD,Velo hits + Velo backward tracks).
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@115784 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ccb5ce2ad755f829a966288aa0a18b336df86994

--- LHCb/Event/RecEvent/src/RecSummary.cpp
+++ LHCb/Event/RecEvent/src/RecSummary.cpp
@@ -7,8 +7,10 @@
 std::ostream& LHCb::RecSummary::fillStream(std::ostream& s) const
 {
   s << "{ Summary Info :";
+
   if ( hasInfo(LHCb::RecSummary::nPVs) )
     s << " nPVs = " << info( LHCb::RecSummary::nPVs, 0 );
+
   if ( hasInfo(LHCb::RecSummary::nLongTracks) )
     s << " nLongTracks = " << info( LHCb::RecSummary::nLongTracks, 0 );
   if ( hasInfo(LHCb::RecSummary::nDownstreamTracks) )
@@ -17,12 +19,28 @@ std::ostream& LHCb::RecSummary::fillStream(std::ostream& s) const
     s << " nUpstreamTracks = " << info( LHCb::RecSummary::nUpstreamTracks, 0 );
   if ( hasInfo(LHCb::RecSummary::nVeloTracks) )
     s << " nVeloTracks = " << info( LHCb::RecSummary::nVeloTracks, 0 );
+  if ( hasInfo(LHCb::RecSummary::nBackTracks) )
+    s << " nBackTracks = " << info( LHCb::RecSummary::nBackTracks, 0 );
   if ( hasInfo(LHCb::RecSummary::nTTracks) )
     s << " nTTracks = " << info( LHCb::RecSummary::nTTracks, 0 );
+
   if ( hasInfo(LHCb::RecSummary::nRich1Hits) )
     s << " nRich1Hits = " << info( LHCb::RecSummary::nRich1Hits, 0 );
   if ( hasInfo(LHCb::RecSummary::nRich2Hits) )
     s << " nRich2Hits = " << info( LHCb::RecSummary::nRich2Hits, 0 );
+
+  if ( hasInfo(LHCb::RecSummary::nVeloClusters) )
+    s << " nVeloClusters = " << info( LHCb::RecSummary::nVeloClusters, 0 );
+
+  if ( hasInfo(LHCb::RecSummary::nITClusters) )
+    s << " nITClusters = " << info( LHCb::RecSummary::nITClusters, 0 );
+
+  if ( hasInfo(LHCb::RecSummary::nOTClusters) )
+    s << " nOTClusters = " << info( LHCb::RecSummary::nOTClusters, 0 );
+
+  if ( hasInfo(LHCb::RecSummary::nSPDhits) )
+    s << " nSPDhits = " << info( LHCb::RecSummary::nSPDhits, 0 );
+
   return s << " }";
 }
 
