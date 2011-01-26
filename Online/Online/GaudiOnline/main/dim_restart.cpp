37d6caf8d254f1c31c8a751c78291c8d31366844
Markus Frank <frankb@cern.ch>
2011-01-26 18:10:56 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@116052 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 1a89bba1efe7915085e31c5b9824e28592a1a225

---
+++ Online/Online/GaudiOnline/main/dim_restart.cpp
@@ -0,0 +1,38 @@
+#include <cerrno>
+#include <cstdio>
+#include <unistd.h>
+extern "C" {
+#include "dis.h"
+}
+
+int main(int /* argc */, char** /* argv */) {
+  static int run = 0;
+  int id = ::dis_add_service((char*)"Initial_ID/run",(char*)"I",&run,sizeof(run),0,0);
+  ::dis_start_serving((char*)"Initial_ID");
+  printf("Dim started with ID:Initial_ID and service created. Wait for 10 secs.\n");
+  ::sleep(10);
+  ::dis_remove_service(id);
+  ::dis_stop_serving();
+  ::dim_stop();
+  printf("Dim is now stopped. Wait for 10 secs.\n");
+  ::sleep(10);
+
+  id = ::dis_add_service((char*)"Updated_ID/run",(char*)"I",&run,sizeof(run),0,0);
+  ::dim_init();
+  ::dis_start_serving((char*)"Updated_ID");
+  printf("Dim started with ID:Updated_ID and service created. Wait......\n");
+  while(1) ::sleep(1);
+  return 1;
+}
+
+
+/*
+
+g++ -o xxxx -I../../DIM/dim -ldim -lrt -pthread ../test/dim_restart.cpp
+
+[frankm@plus04 cmt]$ ./xxxx
+PID:20407 Register Child to DNS....
+PID 20407 - Fri Dec  3 22:01:57 2010 - (FATAL) Child: Some Services already known to DNS
+PID:20404 RE-Register Parent to DNS....
+
+*/
