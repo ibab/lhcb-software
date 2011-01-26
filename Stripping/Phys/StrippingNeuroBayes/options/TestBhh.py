27849d089c84d75afae76dac729caea9ba20a034
ukerzel <ukerzel@cern.ch>
2011-01-26 12:33:46 +0000

    test file for Bhh stripping
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@116034 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0e026197a7f3620aef7ced6107b7030c8e933671

---
+++ Stripping/Phys/StrippingNeuroBayes/options/TestBhh.py
@@ -0,0 +1,131 @@
+from os import environ
+from Gaudi.Configuration import *
+from Configurables import CombineParticles
+from Configurables import DaVinci
+
+from GaudiKernel.SystemOfUnits import MeV, GeV
+
+
+########################################################################
+#
+# Bs -> KK
+#
+from Configurables import CombineParticles
+Bhh                  = CombineParticles("Bhh")
+Bhh.DecayDescriptor  = "B_s0 -> K+ K-"
+Bhh.DaughtersCuts    = {"K+" : "PPCUT(PP_RICHTHRES_K) & ISLONG & (TRCHI2DOF < 5) & (PT > 1.5 *GeV) & (P> 10.0*GeV) & ((PIDK < -0.1) | (PIDK > 0.1))" }
+Bhh.CombinationCut   = "((AM> 5.0*GeV) & (AM< 5.9*GeV) & (AMAXDOCA('LoKi::DistanceCalculator')<0.1))"
+Bhh.MotherCut        = "(VFASPF(VCHI2/VDOF) < 25) & (MAXTREE(('K+'==ABSID) ,PT) > 2.0*GeV) & (MAXTREE(('K+'==ABSID) , PIDK) > 0.1 )"
+
+
+########################################################################
+#
+# NeuroBayes
+#
+from Configurables import StrippingNBBhh
+BhhNB                    = StrippingNBBhh("BhhNB")
+#BhhNB.OutputLevel        = 2
+BhhNB.PlotHisto          = True
+BhhNB.PlotMassMin        =  5.0
+BhhNB.PlotMassMax        =  5.6
+BhhNB.PlotNBins          = 120
+BhhNB.Expertise          = 'bshhet.nb' 
+BhhNB.NetworkVersion     = "TuneMC10"
+BhhNB.NetworkCut         = -1.0
+
+
+########################################################################
+#
+# Selections
+#
+from PhysSelPython.Wrappers import Selection, DataOnDemand
+from PhysSelPython.Wrappers import MergedSelection
+from PhysSelPython.Wrappers import SelectionSequence
+
+noPIDsKaons = DataOnDemand(Location = 'Phys/StdNoPIDsKaons')
+
+BhhSel       = Selection("BhhSel"       , Algorithm = Bhh       , RequiredSelections = [noPIDsKaons])
+BhhNBSel     = Selection("BhhNBSel"     , Algorithm = BhhNB     , RequiredSelections = [ BhhSel ] )
+
+SelSeqBhh    = SelectionSequence("SeqBhh"     , TopSelection = BhhNBSel)
+SeqBhh       = SelSeqBhh.sequence()
+
+########################################################################
+#
+# NTupling
+#
+from Configurables import DecayTreeTuple
+tupleBhh                =  DecayTreeTuple("tupleBhh")
+tupleBhh.InputLocations = [SelSeqBhh.outputLocation()]
+tupleBhh.Decay          = "B_s0 -> K+ K-"
+tupleBhh.ToolList      += [
+    "TupleToolGeometry"
+    , "TupleToolKinematic"
+    , "TupleToolPrimaries"
+    , "TupleToolTrackInfo"
+    , "TupleToolPid"
+    , "TupleToolEventInfo"
+    , "TupleToolPropertime"
+    ,"LoKi::Hybrid::TupleTool/LoKiTupleBhh" 
+     ]
+from Configurables import LoKi__Hybrid__TupleTool
+LoKiTupleBhh = LoKi__Hybrid__TupleTool("LoKiTupleBhh")
+LoKiTupleBhh.Variables = {
+    "Charge"  : "Q" ,
+    "DOCA"    : "DOCA(1,2)",
+    "DOCAMAX" : "DOCAMAX",
+    "NetOut"  : "INFO(LHCb.Particle.LastGlobal+1, -999)",
+    "NetProb" : "INFO(LHCb.Particle.LastGlobal+2, -999)"
+    }
+tupleMuMu.addTool(LoKiTupleMuMu)
+
+########################################################################
+#
+# DaVinci
+#
+
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
+#CondDB(UseOracle = True)
+#importOptions("$APPCONFIGOPTS/DisableLFC.py")
+
+
+# Remove the microbias and beam gas etc events before doing the tagging step
+regexp = "HLT_PASS_RE('Hlt1(?!ODIN)(?!L0)(?!Lumi)(?!Tell1)(?!MB)(?!NZS)(?!Velo)(?!BeamGas)(?!Incident).*Decision')"
+from Configurables import LoKi__HDRFilter
+filterHLT = LoKi__HDRFilter("FilterHLT",Code = regexp )
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+
+from Configurables import AuditorSvc, ChronoAuditor
+AuditorSvc().Auditors.append( ChronoAuditor("Chrono") )
+
+DaVinci().HistogramFile              = "DVHistosBhhNB.root"
+DaVinci().TupleFile                  = "DVTupleBhhNB.root"
+DaVinci().EventPreFilters            = [ filterHLT ]
+DaVinci().EvtMax                     = 5000;
+#DaVinci().EvtMax                     = -1;
+DaVinci().DataType                   = "2010"
+
+DaVinci().Simulation                 = False
+DaVinci().UserAlgorithms             = [ SeqBhh, tupleBhh]
+
+DaVinci().DataType                   = "2010"
+#DaVinci().InputType                  = 'SDST'
+DaVinci().InputType                  = 'DST'
+
+DaVinci().DDDBtag                    = "head-20101003" #"head-20100518" # "head-20100407"
+DaVinci().CondDBtag                  = "head-20101010" #"head-20100730" # "head-20100509"
+
+#DaVinci().SkipEvents = 20100
+EventSelector().Input   = [
+    "DATAFILE='PFN:/work/kerzel/00007574_00000133_1.minibias.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+    ]
+
+#EventSelector().Input   = [
+#    "   DATAFILE='LFN:/lhcb/data/2010/SDST/00007951/0000/00007951_00001135_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'",
+#    "   DATAFILE='LFN:/lhcb/data/2010/SDST/00007951/0000/00007951_00001136_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'",
+#    "   DATAFILE='LFN:/lhcb/data/2010/SDST/00007951/0000/00007951_00001137_1.sdst' TYP='POOL_ROOTTREE' OPT='READ'"
+#]
+#FileCatalog().Catalogs = [ 'xmlcatalog_file:$STRIPPINGSELECTIONSROOT/tests/data/pool_xml_catalog_run79646_mu2.23_tck1F0029.xml' ]
