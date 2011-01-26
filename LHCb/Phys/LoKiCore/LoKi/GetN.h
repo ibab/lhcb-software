c230d800f368578a744cb74facd581af4af2409d
Vanya Belyaev <ivan.belyaev@cern.ch>
2011-01-26 19:09:34 +0000

    GetN_ : new utility to get all N-body combinations form some container
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@116057 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b2e219e42557df1bfa0b4bb82ec532535cb30efb

---
+++ LHCb/Phys/LoKiCore/LoKi/GetN.h
@@ -0,0 +1,94 @@
+// $Id: $
+// ============================================================================
+#ifndef LOKI_GETN_H 
+#define LOKI_GETN_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/Combiner.h"
+// ============================================================================
+namespace LoKi 
+{
+  // ==========================================================================
+  /** @class GetN GetN.h LoKi/GetN.h
+   *
+   *  Simple wrapper over LoKi::Combiner class 
+   *  that allows to get certain elements combinations
+   *
+   *  @author Vanya Belyaev Ivan.Belyaev@cern.ch
+   *  @date   2011-01-26
+   */
+  template <class CONTAINER, class UNIQUE>
+  class GetN_
+  {
+  public:
+    // =======================================================================
+    GetN_ ( const          std::size_t                        N     , 
+            const typename LoKi::Combiner_<CONTAINER>::Range& range )
+      : m_combiner () 
+      , m_unique   () 
+    {
+      for ( std::size_t n = 0 ; n < N ; ++n ) 
+      { m_combiner.add( range ) ; }
+    }  
+    // =======================================================================
+    template <class INPUT> 
+    GetN_ ( const std::size_t N     , 
+            INPUT             first , 
+            INPUT             last  ) 
+      : m_combiner () 
+      , m_unique   () 
+    {
+      for ( std::size_t n = 0 ; n < N ; ++n ) 
+      { m_combiner.add ( typename LoKi::Combiner_<CONTAINER>::Range ( first , last ) ) ; }
+    }  
+    // =======================================================================
+  public:
+    // =======================================================================
+    /// get the actual combination 
+    template <class OUTPUT> 
+    std::size_t next ( OUTPUT out ) 
+    {
+      //
+      if ( !m_combiner.valid() ) { return 0 ; }
+      //
+      if (  m_combiner.valid() && !m_combiner.unique ( m_unique ) ) 
+      { 
+        ++m_combiner ;
+        return next ( out ) ;
+      }
+      //
+      typedef typename LoKi::Combiner_<CONTAINER>::Select   Select   ;
+      typedef typename Select::const_iterator               iterator ;
+      //
+      const Select&  sel = m_combiner.current() ; 
+      for ( iterator ic  = sel.begin() ; sel.end() != ic ; ++ic )
+      {  *out = **ic ; ++out ; }  
+      //
+      ++m_combiner ;
+      //
+      return sel.size() ;
+    }
+    // =======================================================================
+  private :
+    // =======================================================================
+    /// the default constructor is disabled ;
+    GetN_ () ;                        // the default constructor is disabled ;    
+    // =======================================================================
+  private :
+    // =======================================================================
+    /// the actual combiner 
+    LoKi::Combiner_<CONTAINER> m_combiner ;             // the actual combiner 
+    /// the unique selector 
+    UNIQUE                     m_unique   ;             // the unique selector
+    // =======================================================================
+  } ;
+  // ==========================================================================  
+} //                                                      end of namespace LoKi 
+// ============================================================================
+//                                                                      The END 
+// ============================================================================
+#endif // LOKI_GETN_H
+// ============================================================================
