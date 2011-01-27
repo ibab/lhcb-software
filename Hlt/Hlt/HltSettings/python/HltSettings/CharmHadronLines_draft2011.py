5b30abac1e4de2abf10843f20df258ac5004a32c
Patrick Spradlin <patrick.spradlin@cern.ch>
2011-01-27 19:59:01 +0000

    Updated draft threshold settings for some charm line renaming.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@116186 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 571341a3a57874e1c5966d6425034f05d571275f

--- Hlt/Hlt/HltSettings/python/HltSettings/CharmHadronLines_draft2011.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/CharmHadronLines_draft2011.py
@@ -11,20 +11,27 @@ class CharmHadronLines :
     __all__ = ( 'ActiveHlt2Lines' )
     
     
-    def ActiveHlt2Lines(self) :
+    def ActiveHlt2Lines(self) : # {
         """
         Returns a list of active lines
         """
 
         lines = [ ## from Hlt2CharmHadD02HHLines
-                  'Hlt2Charm2Body'
+                  #'Hlt2CharmD02KK'
+                  #, 'Hlt2CharmD02KPi'
+                  #, 'Hlt2CharmD02PiPi'
+                  ## Inclusive lines
                   #, 'Hlt2Charm2BodyInc'
-                  , 'Hlt2Charm2BodyWideMass'
+                  ## Wide mass lines
+                  #, 'Hlt2CharmD02KKWideMass'
+                  #, 'Hlt2CharmD02KPiWideMass'
+                  #, 'Hlt2CharmD02PiPiWideMass'
              ]
         
         return lines
+    # }
 
-    def Thresholds(self) :
+    def Thresholds(self) : # {
         """
         Returns a dictionary of cuts
         """
@@ -32,38 +39,46 @@ class CharmHadronLines :
 
         from Hlt2Lines.Hlt2CharmHadD02HHLines import Hlt2CharmHadD02HHLinesConf
         d.update( { Hlt2CharmHadD02HHLinesConf       : {
-                        ## Cut values for one stage track-fit (OSTF) lines.
-                        ## 2Body
-                          'OSTFAllTrkPtLL_2Body'          : 800.0      # in MeV
-                        , 'OSTFAllTrkPLL_2Body'           : 5000.0     # in MeV
-                        , 'OSTFAllTrkPVIPChi2LL_2Body'    : 2.0        # unitless
-                        , 'OSTFAllTrkChi2UL_2Body'        : 5.0        # unitless
-                        , 'OSTFPairMinDocaUL_2Body'       : 0.10       # in mm
-                        , 'OSTFTrkMaxPtLL_2Body'          : 1500.0     # in MeV
-                        , 'OSTFVtxPVDispChi2LL_2Body'     : 25.0       # unitless
-                        , 'OSTFVtxChi2UL_2Body'           : 10.0       # unitless
-                        , 'OSTFPointLL_2Body'             : 0.99985    # unitless
+                        ## Cut values for basic D0 -> hh signal lines
+                          'Trk_PT_MIN'               : 800.0      # MeV
+                        , 'Trk_P_MIN'                : 5000.0     # MeV
+                        , 'Trk_MIPCHI2DV_MIN'        : 2.0        # neuter
+                        , 'Trk_TRCHI2DOF_MAX'        : 5.0        # neuter
+                        , 'Pair_AMINDOCA_MAX'        : 0.10       # mm
+                        , 'Trk_Max_APT_MIN'          : 1500.0     # MeV
+                        , 'D0_BPVVDCHI2_MIN'         : 25.0       # neuter
+                        , 'D0_BPVDIRA_MIN'           : 0.99985    # neuter
+                        , 'D0_VCHI2PDOF_MAX'         : 10.0       # neuter
+                        , 'D0_PT_MIN'                : 2000.0     # MeV
+                        , 'Sig_M_MIN'                : 1839.0     # MeV
+                        , 'Sig_M_MAX'                : 1889.0     # MeV
+                        , 'WideMass_M_MIN'           : 1700.0     # MeV
+                        , 'WideMass_M_MAX'           : 2100.0     # MeV
                         ## 2-body inclusive!
-		                , 'OSTFAllTrkPtLL_2BodyInc'          : 500.0      # in MeV
-		                , 'OSTFAllTrkPLL_2BodyInc'           : 5000.0     # in MeV
-		                , 'OSTFAllTrkPVIPChi2LL_2BodyInc'    : 16.0        # unitless
-		                , 'OSTFAllTrkChi2UL_2BodyInc'        : 3.0        # unitless
-		                , 'OSTFPairMinDocaUL_2BodyInc'       : 0.15       # in mm
-                        , 'OSTFVtxPVDispChi2LL_2BodyInc'     : 100.       # unitless  
-		                , 'OSTFTrkMaxPtLL_2BodyInc'          : 1000.0     # in MeV
-		                , 'OSTFDPtLL_2BodyInc'               : 1500.0     # in MeV
-		                , 'OSTFDSumPtLL_2BodyInc'            : 1800.0     # in MeV
-		                , 'OSTFDIPChi2LL_2BodyInc'           : 2.0       # unitless
-		                , 'OSTFMCOR_MIN_2BodyInc'            : 300.        # MeV
-		                , 'OSTFMCOR_MAX_2BodyInc'            : 3200.        # MeV
-                        , 'Prescale'                : {
-                                  'Hlt2CharmOSTF2BodyWideMass' : 0.01
-                                }
-                        , 'Postscale'               : { }
-                    
+                        , 'Inc_Trk_PT_MIN'           : 500.0      # MeV
+                        , 'Inc_Trk_P_MIN'            : 5000.0     # MeV
+                        , 'Inc_Trk_MIPCHI2DV_MIN'    : 16.0       # neuter
+                        , 'Inc_Trk_TRCHI2DOF_MAX'    : 3.0        # neuter
+                        , 'Inc_Pair_AMINDOCA_MAX'    : 0.15       # in mm
+                        , 'Inc_D0_BPVVDCHI2_MIN'     : 100.0      # neuter
+                        , 'Inc_Trk_Max_APT_MIN'      : 1000.0     # MeV
+                        , 'Inc_D0_PT_MIN'            : 1500.0     # MeV
+                        , 'Inc_D0_SUMPT_MIN'         : 1800.0     # MeV
+                        , 'Inc_D0_BPVIPCHI2_MIN'     : 2.0        # neuter
+                        , 'Inc_D0_MCOR_MIN'          : 300.       # MeV
+                        , 'Inc_D0_MCOR_MAX'          : 3200.      # MeV
+                        ## GEC
+                        , 'GEC_Filter_NTRACK'        : True       # 
+                        , 'GEC_NTRACK_MAX'           : 120        # 
+                        , 'Postscale' : { 
+                                          'Hlt2CharmD02KKWideMass'     : 0.05
+                                         , 'Hlt2CharmD02KPiWideMass'  : 0.05
+                                         , 'Hlt2CharmD02PiPiWideMass' : 0.05
+                                        }
                     }
                   } )
 
         return d
+    # }
             
    
