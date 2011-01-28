c71aa3b4a5154bbbc3e203f89da827d2c503f74a
Patrick Spradlin <patrick.spradlin@cern.ch>
2011-01-28 15:40:18 +0000

    Added configuration for Hlt2CharmHadD2HHHLines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@116281 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 82b6d00dd6077b5ca7ea52f3e21dd93d2db1e1de

--- Hlt/Hlt/HltSettings/python/HltSettings/CharmHadronLines_draft2011.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/CharmHadronLines_draft2011.py
@@ -16,16 +16,21 @@ class CharmHadronLines :
         Returns a list of active lines
         """
 
-        lines = [ ## from Hlt2CharmHadD02HHLines
-                  #'Hlt2CharmD02KK'
-                  #, 'Hlt2CharmD02KPi'
-                  #, 'Hlt2CharmD02PiPi'
+        lines = [ ### from Hlt2CharmHadD02HHLines
+                  #'Hlt2CharmHadD02KK'
+                  #, 'Hlt2CharmHadD02KPi'
+                  #, 'Hlt2CharmHadD02PiPi'
                   ## Inclusive lines
-                  #, 'Hlt2Charm2BodyInc'
+                  #, 'Hlt2CharmHad2BodyInc'
                   ## Wide mass lines
-                  #, 'Hlt2CharmD02KKWideMass'
-                  #, 'Hlt2CharmD02KPiWideMass'
-                  #, 'Hlt2CharmD02PiPiWideMass'
+                  #, 'Hlt2CharmHadD02KKWideMass'
+                  #, 'Hlt2CharmHadD02KPiWideMass'
+                  #, 'Hlt2CharmHadD02PiPiWideMass'
+
+                  ### from Hlt2CharmHadD2HHHLines
+                  #, 'Hlt2CharmHad2BodyForD2HHH'
+                    'Hlt2CharmHadD2HHH'
+                  , 'Hlt2CharmHadD2HHHWideMass'
              ]
         
         return lines
@@ -38,6 +43,7 @@ class CharmHadronLines :
         d = { }
 
         from Hlt2Lines.Hlt2CharmHadD02HHLines import Hlt2CharmHadD02HHLinesConf
+        from Hlt2Lines.Hlt2CharmHadD2HHHLines import Hlt2CharmHadD2HHHLinesConf
         d.update( { Hlt2CharmHadD02HHLinesConf       : {
                         ## Cut values for basic D0 -> hh signal lines
                           'Trk_PT_MIN'               : 800.0      # MeV
@@ -50,10 +56,10 @@ class CharmHadronLines :
                         , 'D0_BPVDIRA_MIN'           : 0.99985    # neuter
                         , 'D0_VCHI2PDOF_MAX'         : 10.0       # neuter
                         , 'D0_PT_MIN'                : 2000.0     # MeV
-                        , 'Sig_M_MIN'                : 1839.0     # MeV
-                        , 'Sig_M_MAX'                : 1889.0     # MeV
-                        , 'WideMass_M_MIN'           : 1700.0     # MeV
-                        , 'WideMass_M_MAX'           : 2100.0     # MeV
+                        , 'Sig_M_MIN'                : 1815.0     # MeV
+                        , 'Sig_M_MAX'                : 1915.0     # MeV
+                        , 'WideMass_M_MIN'           : 1715.0     # MeV
+                        , 'WideMass_M_MAX'           : 2015.0     # MeV
                         ## 2-body inclusive!
                         , 'Inc_Trk_PT_MIN'           : 500.0      # MeV
                         , 'Inc_Trk_P_MIN'            : 5000.0     # MeV
@@ -71,11 +77,40 @@ class CharmHadronLines :
                         , 'GEC_Filter_NTRACK'        : True       # 
                         , 'GEC_NTRACK_MAX'           : 120        # 
                         , 'Postscale' : { 
-                                          'Hlt2CharmD02KKWideMass'     : 0.05
-                                         , 'Hlt2CharmD02KPiWideMass'  : 0.05
-                                         , 'Hlt2CharmD02PiPiWideMass' : 0.05
+                                          'Hlt2CharmHadD02KKWideMass'    : 0.05
+                                         , 'Hlt2CharmHadD02KPiWideMass'  : 0.05
+                                         , 'Hlt2CharmHadD02PiPiWideMass' : 0.05
                                         }
                     }
+
+                    , Hlt2CharmHadD2HHHLinesConf : {
+                        ## 3Body
+                          'TrkPt_3Body'                 : 250.0    # MeV
+                        , 'TrkP_3Body'                  : 2000.0   # MeV
+                        , 'TrkPVIPChi2_3Body'           : 2.0      # unitless
+                        , 'TrkChi2_3Body'               : 5.0      # unitless
+                        , 'PairMinDoca_3Body'           : 0.10     # mm
+                        , 'VtxPVDispChi2_3Body'         : 100.0    # unitless
+                        , 'VtxChi2_3Body'               : 20.0     # unitless
+                        , 'DIPChi2_3Body'               : 25.0     # unitless
+                        , 'DSumPt_3Body'                : 2000.0   # MeV
+                        , 'MCOR_MAX_3Body'              : 3500.    # MeV
+                        ## 2-body Input for 3Body
+                        , 'TrkPt_2BodyFor3Body'         : 500.0    # MeV
+                        , 'TrkP_2BodyFor3Body'          : 5000.0   # MeV
+                        , 'TrkPVIPChi2_2BodyFor3Body'   : 7.0      # unitless
+                        , 'TrkChi2_2BodyFor3Body'       : 5.0      # unitless
+                        , 'Doca_2BodyFor3Body'          : 0.085    # mm
+                        , 'VtxPVDispChi2_2BodyFor3Body' : 40.      # unitless
+                        , 'VtxPVDisp_2BodyFor3Body'     : 3.0      # mm
+                        , 'DSumPt_2BodyFor3Body'        : 1400.0   # MeV
+                        , 'MCOR_MAX_2BodyFor3Body'      : 3500.    # MeV
+                        # prescales
+                        , 'Prescale'                    : {
+                                'Hlt2CharmHadD2HHHWideMass'    : 0.1
+                           }
+                    }
+
                   } )
 
         return d
