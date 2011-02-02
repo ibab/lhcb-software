0c9fdbec032fc45decf517939076eb367a866c4c
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-02 18:30:07 +0000

    ! 2011-02-02 - Chris Jones  - Adapt to removal of depreciated RichRadIntersection typedef.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@116847 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2823d8fa4524dfacafedb61ed8fd5b63939607c0

--- Rec/Rich/RichRecMonitors/src/RichPhotonRecoTestAlg.cpp
+++ Rec/Rich/RichRecMonitors/src/RichPhotonRecoTestAlg.cpp
@@ -4,8 +4,6 @@
  *
  *  Implementation file for algorithm class : RichPhotonRecoTestAlg
  *
- *  $Id: RichPhotonRecoTestAlg.cpp,v 1.9 2009-07-30 11:18:33 jonrob Exp $
- *
  *  @author Chris Jones       Christopher.Rob.Jones@cern.ch
  *  @date   21/02/2006
  */
@@ -88,15 +86,15 @@ StatusCode PhotonRecoTestAlg::execute()
   LHCb::RichTrackSegment * seg(NULL);
 
   TrackSegments Segs;
-  RichRadIntersection::Vector intersects;
+  Rich::RadIntersection::Vector intersects;
 
   // Aerogel segments
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(1.24572,99.0629,1110),
-                                             Vector(8.27581,284.16,3423.45),
-                                             Point(1.37191,103.213,1160),
-                                             Vector(9.00563,284.158,3423.45),
-                                             m_radiators[Rich::Aerogel] ) );
+  intersects.push_back( Rich::RadIntersection( Point(1.24572,99.0629,1110),
+                                               Vector(8.27581,284.16,3423.45),
+                                               Point(1.37191,103.213,1160),
+                                               Vector(9.00563,284.158,3423.45),
+                                               m_radiators[Rich::Aerogel] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Rich::Aerogel, Rich::Rich1,
@@ -104,11 +102,11 @@ StatusCode PhotonRecoTestAlg::execute()
   Segs.push_back( new LHCb::RichRecSegment(seg,&trk,1,0,1) );
 
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(-279.149,-45.9607,1110),
-                                             Vector(-534.221,-86.3879,2269.82),
-                                             Point(-290.927,-47.8635,1160),
-                                             Vector(-535.06,-86.3626,2269.62),
-                                             m_radiators[Rich::Aerogel] ) );
+  intersects.push_back( Rich::RadIntersection( Point(-279.149,-45.9607,1110),
+                                               Vector(-534.221,-86.3879,2269.82),
+                                               Point(-290.927,-47.8635,1160),
+                                               Vector(-535.06,-86.3626,2269.62),
+                                               m_radiators[Rich::Aerogel] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Rich::Aerogel, Rich::Rich1,
@@ -117,11 +115,11 @@ StatusCode PhotonRecoTestAlg::execute()
 
   // C4f10 segments
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(12.1593,43.7352,990),
-                                             Vector(200.427,725.766,17990.2),
-                                             Point(22.1956,80.5884,1907.12),
-                                             Vector(190.165,723.361,17986.1),
-                                             m_radiators[Rich::Rich1Gas] ) );
+  intersects.push_back( Rich::RadIntersection( Point(12.1593,43.7352,990),
+                                               Vector(200.427,725.766,17990.2),
+                                               Point(22.1956,80.5884,1907.12),
+                                               Vector(190.165,723.361,17986.1),
+                                               m_radiators[Rich::Rich1Gas] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Point(17.183,62.2349,1448.56),
@@ -131,11 +129,11 @@ StatusCode PhotonRecoTestAlg::execute()
   Segs.push_back( new LHCb::RichRecSegment(seg,&trk,1,0,1) );
 
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(-37.1749,-8.16514,990),
-                                             Vector(-658.922,-144.142,19349),
-                                             Point(-67.6491,-14.2807,1886.02),
-                                             Vector(-649.358,-132.183,19353.4),
-                                             m_radiators[Rich::Rich1Gas] ) );
+  intersects.push_back( Rich::RadIntersection( Point(-37.1749,-8.16514,990),
+                                               Vector(-658.922,-144.142,19349),
+                                               Point(-67.6491,-14.2807,1886.02),
+                                               Vector(-649.358,-132.183,19353.4),
+                                               m_radiators[Rich::Rich1Gas] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Point(-52.3554,-11.5027,1438.01),
@@ -146,11 +144,11 @@ StatusCode PhotonRecoTestAlg::execute()
 
   // Cf4 segments
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(-178.735,385.802,9501.4),
-                                             Vector(-1021.86,719.726,17948.8),
-                                             Point(-284.777,459.695,11344.3),
-                                             Vector(-1038.67,719.252,17940.1),
-                                             m_radiators[Rich::Rich2Gas] ) );
+  intersects.push_back( Rich::RadIntersection( Point(-178.735,385.802,9501.4),
+                                               Vector(-1021.86,719.726,17948.8),
+                                               Point(-284.777,459.695,11344.3),
+                                               Vector(-1038.67,719.252,17940.1),
+                                               m_radiators[Rich::Rich2Gas] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Point(-231.545,422.75,10422.8),
@@ -160,11 +158,11 @@ StatusCode PhotonRecoTestAlg::execute()
   Segs.push_back( new LHCb::RichRecSegment(seg,&trk,1,0,1) );
 
   intersects.clear();
-  intersects.push_back( RichRadIntersection( Point(160.598,47.8836,9500.18),
-                                             Vector(1278.51,154.37,35339.7),
-                                             Point(227.559,55.8973,11334.7),
-                                             Vector(1295.61,154.249,35312.6),
-                                             m_radiators[Rich::Rich2Gas] ) );
+  intersects.push_back( Rich::RadIntersection( Point(160.598,47.8836,9500.18),
+                                               Vector(1278.51,154.37,35339.7),
+                                               Point(227.559,55.8973,11334.7),
+                                               Vector(1295.61,154.249,35312.6),
+                                               m_radiators[Rich::Rich2Gas] ) );
   seg = new LHCb::RichTrackSegment( LHCb::RichTrackSegment::UseAllStateVectors,
                                     intersects,
                                     Point(193.967,51.8904,10417.4),
