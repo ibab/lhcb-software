52c3f398dbf36d7dad026089a7a137012fb5eeee
Olivier Deschamps <odescham@cern.ch>
2011-02-08 08:18:44 +0000

    add missing interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@117203 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2d0aaffe8ac96a599b77a2428ce3c01647417ff2

---
+++ LHCb/Calo/CaloDAQ/CaloDAQ/ICaloDigitFilterTool.h
@@ -0,0 +1,41 @@
+// $Id: $
+#ifndef ICALODIGITFILTERTOOL_H 
+#define ICALODIGITFILTERTOOL_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "Event/CaloDigit.h"
+#include "GaudiKernel/IAlgTool.h"
+
+static const InterfaceID IID_ICaloDigitFilterTool ( "ICaloDigitFilterTool", 1, 0 );
+
+/** @class ICaloDigitFilterTool ICaloDigitFilterTool.h
+ *  
+ *
+ *  @author Olivier Deschamps
+ *  @date   2010-12-13
+ */
+class ICaloDigitFilterTool : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_ICaloDigitFilterTool; }
+
+  virtual int getScale()=0;
+  virtual bool cleanDigits(std::string det, bool substr=true, bool mask = true)=0;
+  virtual int method(std::string det)=0;
+  virtual double offset(LHCb::CaloCellID id)=0;    
+  virtual double offsetRMS(LHCb::CaloCellID id)=0;    
+
+
+protected:
+
+private:
+
+
+
+};
+#endif // ICALODIGITFILTERTOOL_H
