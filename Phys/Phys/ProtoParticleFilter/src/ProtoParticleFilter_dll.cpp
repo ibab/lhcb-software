5a82890a9256544135c31d7cec75a682bb5f6797
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-09 10:49:43 +0000

    remove ProtoParticleFilter_dll.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@117330 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f78fe7970d09cc3499d5eb4e01cda8f4a1a3f8fe

--- Phys/Phys/ProtoParticleFilter/src/ProtoParticleFilter_dll.cpp
+++
@@ -1,6 +0,0 @@
-// $Id: ProtoParticleFilter_dll.cpp,v 1.1.1.1 2006-06-18 14:23:44 jonrob Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES ( ProtoParticleFilter )
