0b9b00bb8dad661f8d35580944b905e39c6bab82
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-10 14:08:44 +0000

    ! 2011-02-10 - Chris Jones  - Fix a warning with the icc compiler in LinkedTo.h - Replace use of sprintf    with an std::ostringstream object.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@117443 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b5458eee88e5f2462007ebab0f42d20a810caa85

--- LHCb/Event/LinkerEvent/Linker/LinkedTo.h
+++ LHCb/Event/LinkerEvent/Linker/LinkedTo.h
@@ -2,6 +2,9 @@
 #ifndef LINKER_LINKEDTO_H 
 #define LINKER_LINKEDTO_H 1
 
+// STL
+#include <sstream>
+
 // Include files
 #include "GaudiKernel/IDataProviderSvc.h"
 #include "GaudiKernel/LinkManager.h"
@@ -46,18 +49,16 @@ public:
       //== Check proper template, only if specified. 
       if ( links->sourceClassID() != SOURCE::classID() &&
            CLID_ContainedObject   != SOURCE::classID()  ) {
-        char message[200];
-        sprintf( message, " : Template classID is %4d expected %4d",SOURCE::classID(), links->sourceClassID());
-        std::string added( message );
-        throw GaudiException( "Incompatible SOURCE type for location " + containerName + added,
-                              "LinkedTo", StatusCode::FAILURE);
+        std::ostringstream message;
+        message << "Incompatible SOURCE type for location " << containerName
+                << " : Template classID is " << SOURCE::classID() << " expected " << links->sourceClassID();
+        throw GaudiException( message.str(), "LinkedTo", StatusCode::FAILURE);
       }
       if ( links->targetClassID() != TARGET::classID() ) {
-        char message[200];
-        sprintf( message, " : Template classID is %4d expected %4d", TARGET::classID(), links->targetClassID());
-        std::string added( message );
-        throw GaudiException( "Incompatible TARGET type for location " + containerName + added,
-                              "LinkedTo", StatusCode::FAILURE);
+        std::ostringstream message;
+        message << "Incompatible TARGET type for location " << containerName 
+                << " : Template classID is " << TARGET::classID() << " expected " << links->targetClassID();
+        throw GaudiException( message.str(), "LinkedTo", StatusCode::FAILURE);
       }
     }
     m_links = links;
