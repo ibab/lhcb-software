7c0497f592475a2b24d10aac213c98ebab7a3328
jpalac <jpalac@cern.ch>
2011-02-11 10:10:29 +0000

    - Add StrippingSelections.lineBuilders(), function returning all the line    builders available in the StrippingSelections module.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@117565 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 922983186be959349c2975caf7007daeed232eaf

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/__init__.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/__init__.py
@@ -1,5 +1,33 @@
+"""
+Module with stripping selection line builder modules.
+All line builders available via function lineBuilders().
+"""
+
+__author__ = 'Juan Palacios palacios@physik.uzh.ch'
+
 import StrippingB2XGamma
 import StrippingBs2JpsiPhiPrescaledAndDetatched
 import StrippingB2hhLTUnbiased
 import StrippingNeuroBayesMuMu
 import StrippingB2nbody
+
+from sys import modules as _modules
+_this = _modules[__name__]
+
+_strippingKeys = filter ( lambda x : x[:9]=='Stripping',
+                          locals().keys())
+
+_strippingModules = [getattr(_this, _k) for _k in _strippingKeys]
+
+from StrippingUtils.Utils import getLineBuildersFromModule as _getter
+
+_lineBuilders = {}
+
+for _sm in _strippingModules :
+    _lineBuilders.update(_getter(_sm))
+
+def lineBuilders() :
+    """
+    Return all the line builders in the module.
+    """
+    return dict(_lineBuilders)
