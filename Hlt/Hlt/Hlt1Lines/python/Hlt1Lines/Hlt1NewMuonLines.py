18ccf36031412ca6c76d572a365bee0aafa45d36
Roel Aaij <roel.aaij@cern.ch>
2011-02-11 18:08:47 +0000

    Added additional new muon lines.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@117633 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dbf334fa76e45081f5bafca8824ab41e99518886

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1NewMuonLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1NewMuonLines.py
@@ -5,39 +5,59 @@ class Hlt1NewMuonLinesConf( HltLinesConfigurableUser ):
     # steering variables
     __slots__ = { 
         #  Muon Lines
-         'L0SingleMuon'             :"Muon"
-        ,'L0SingleMuonGEC'          :"Muon"
-        ,'DiMuon_VxDOCA'            :  0.2
-        ,'DiMuon_VxChi2'            :   25
-        ,'DiMuon_P'                 : 6000
-        ,'DiMuon_PT'                :  500
-        ,'DiMuon_TrChi2'            :    4
+         'DiMuonLowMass_VxDOCA'     :  0.2
+        ,'DiMuonLowMass_VxChi2'     :   25
+        ,'DiMuonLowMass_P'          : 6000
+        ,'DiMuonLowMass_PT'         :  500
+        ,'DiMuonLowMass_TrChi2'     :    4
+        ,'DiMuonLowMass_M'          : 1000
+        ,'DiMuonHighMass_VxDOCA'    :  0.2
+        ,'DiMuonHighMass_VxChi2'    :   25
+        ,'DiMuonHighMass_P'         : 6000
+        ,'DiMuonHighMass_PT'        :  500
+        ,'DiMuonHighMass_TrChi2'    :    4
+        ,'DiMuonHighMass_M'         : 2900
+        ,'SingleMuonHighPT_P'       : 6000
+        ,'SingleMuonHighPT_PT'      :  500
+        ,'SingleMuonHighPT_TrChi2'  :    4
+        ,'SingleMuonNoIP_P'         : 6000
+        ,'SingleMuonNoIP_PT'        :  500
+        ,'SingleMuonNoIP_TrChi2'    :    4
+        ,'channels'                 : { 'DiMuonLowMass'    : ( 'Muon', 'DiMuon' ),
+                                        'DiMuonHighMass'   : ( 'Muon', 'DiMuon' ),
+                                        'SingleMuonHighPT' : ( 'MuonHigh', ),
+                                        'SingleMuonNoIP'   : ( 'Muon', ) }
         }
+    
+    def localise_props( self, prefix ):
+        ps = self.getProps()
+        props = dict( [ ( key.split( '_' )[ 1 ], ps[ key ] ) for key in ps if key.find( prefix ) != -1 ] )
+        props[ 'name' ] = prefix
+        return props
 
-    def preambulo( self ):
+    def preambulo( self, properties ):
         from HltTracking.Hlt1Streamers import ( VeloCandidates,
                                                 MatchVeloMuon, IsMuon,
                                                 LooseForward, FitTrack )
         ## define some "common" preambulo 
-        Preambulo = [ VeloCandidates( "DiMuon" ),
+        Preambulo = [ VeloCandidates( properties[ 'name' ] ),
                       MatchVeloMuon,
                       LooseForward,
                       FitTrack,
-                      IsMuon,
-                      ## helpers
-                      "VertexConf = LoKi.Hlt1.VxMakerConf( %(DiMuon_VxDOCA)f * mm, \
-                                      %(DiMuon_VxChi2)f )" % self.getProps(),
-                      "MakeDiMuons = TC_VXMAKE4( '', VertexConf )",
-                      "from LoKiPhys.decorators import RV_MASS"
-                      ]
+                      IsMuon ]
         return Preambulo
 
-    def dimuon_streamer( self ):
+    def diMuon_streamer( self, properties ):
         from Configurables import LoKi__HltUnit as HltUnit
-        dimuon_unit = HltUnit(
-            'DiMuonStreamer',
+        extra_pre = [
+            "VertexConf = LoKi.Hlt1.VxMakerConf( %(VxDOCA)f * mm, %(VxChi2)f )" % properties,
+            "MakeDiMuons = TC_VXMAKE4( '', VertexConf )",
+            "from LoKiPhys.decorators import RV_MASS" ]
+
+        diMuon_unit = HltUnit(
+            '%(name)sStreamer' % properties,
             ##OutputLevel = 1 ,
-            Preambulo = self.preambulo(),
+            Preambulo = self.preambulo( properties ) + extra_pre,
             Code = """
             VeloCandidates
             >>  execute( 'MuonRec' )
@@ -48,35 +68,71 @@ class Hlt1NewMuonLinesConf( HltLinesConfigurableUser ):
             >>  LooseForward
             >>  tee  ( monitor( TC_SIZE > 0, '# pass forward', LoKi.Monitoring.ContextSvc ) )
             >>  tee  ( monitor( TC_SIZE , 'nForward' , LoKi.Monitoring.ContextSvc ) )
-            >>  ( ( TrPT > %(DiMuon_PT)s * MeV ) & ( TrP  > %(DiMuon_P)s  * MeV ) )
+            >>  ( ( TrPT > %(PT)s * MeV ) & ( TrP  > %(P)s  * MeV ) )
             >>  FitTrack
             >>  tee  ( monitor( TC_SIZE > 0, '# pass fit', LoKi.Monitoring.ContextSvc ) )
             >>  tee  ( monitor( TC_SIZE , 'nFitted' , LoKi.Monitoring.ContextSvc ) )
-            >>  ( TrCHI2PDOF < %(DiMuon_TrChi2)s )
+            >>  ( TrCHI2PDOF < %(TrChi2)s )
             >>  IsMuon
             >>  tee  ( monitor( TC_SIZE > 0, '# pass IsMuon', LoKi.Monitoring.ContextSvc ) )
             >>  tee  ( monitor( TC_SIZE , 'nIsMuon' , LoKi.Monitoring.ContextSvc ) )
             >>  MakeDiMuons
             >>  tee  ( monitor( TC_SIZE > 0, '# pass vertex', LoKi.Monitoring.ContextSvc ) )
             >>  tee  ( monitor( TC_SIZE , 'nVertices' , LoKi.Monitoring.ContextSvc ) )
-            >>  ( RV_MASS ( 'mu+' , 'mu-' ) > 1000 ) 
+            >>  ( RV_MASS ( 'mu+' , 'mu-' ) > %(M)s * MeV ) 
             >>  tee  ( monitor( TC_SIZE > 0, '# pass mass', LoKi.Monitoring.ContextSvc ) )
             >>  tee  ( monitor( TC_SIZE , 'nDiMuons' , LoKi.Monitoring.ContextSvc ) )
-            >>  SINK( 'Hlt1NewDiMuonDecision' )
+            >>  SINK( 'Hlt1%(name)sDecision' )
             >> ~TC_EMPTY
-            """ % self.getProps()
+            """ % properties
             )
-        return dimuon_unit
-    
-    def __apply_configuration__( self ) : 
-         ## Create the DiMuon line
+        return diMuon_unit
+
+    def singleMuon_streamer( self, properties ):
+        from Configurables import LoKi__HltUnit as HltUnit
+        singleMuon_unit = HltUnit(
+            '%(name)sStreamer' % properties,
+            ##OutputLevel = 1 ,
+            Preambulo = self.preambulo( properties ),
+            Code = """
+            VeloCandidates
+            >>  execute( 'MuonRec' )
+            >>  MatchVeloMuon
+            >>  tee  ( monitor( TC_SIZE > 0, '# pass match', LoKi.Monitoring.ContextSvc ) )
+            >>  tee  ( monitor( TC_SIZE    , 'nMatched' , LoKi.Monitoring.ContextSvc ) )
+            >>  execute( decodeIT )
+            >>  TightForward
+            >>  tee  ( monitor( TC_SIZE > 0, '# pass forward', LoKi.Monitoring.ContextSvc ) )
+            >>  tee  ( monitor( TC_SIZE , 'nForward' , LoKi.Monitoring.ContextSvc ) )
+            >>  ( ( TrPT > %(PT)s * MeV ) & ( TrP  > %(P)s  * MeV ) )
+            >>  FitTrack
+            >>  tee  ( monitor( TC_SIZE > 0, '# pass fit', LoKi.Monitoring.ContextSvc ) )
+            >>  tee  ( monitor( TC_SIZE , 'nFitted' , LoKi.Monitoring.ContextSvc ) )
+            >>  ( TrCHI2PDOF < %(TrChi2)s )
+            >>  IsMuon
+            >>  tee  ( monitor( TC_SIZE > 0, '# pass IsMuon', LoKi.Monitoring.ContextSvc ) )
+            >>  tee  ( monitor( TC_SIZE , 'nIsMuon' , LoKi.Monitoring.ContextSvc ) )
+            >>  SINK( 'Hlt1%(name)sDecision' )
+            >> ~TC_EMPTY
+            """ % properties
+            )
+        return singleMuon_unit
+
+    def build_line( self, name, streamer ):
         from HltLine.HltLine import Hlt1Line
-        Hlt1Line(
-            'NewDiMuon',
-            ##
+        line = Hlt1Line(
+            name,
             prescale  = self.prescale,
             postscale = self.prescale,
-            L0DU = "L0_CHANNEL( 'Muon' ) | L0_CHANNEL( 'DiMuon' )",
-            ##
-            algos     = [ self.dimuon_streamer() ] 
+            L0DU = "|".join( [ "L0_CHANNEL('%s')" % l0 for l0 in self.channels[ name ] ] ) ,
+            algos = [ streamer( self.localise_props( name ) ) ] 
             )
+
+    def __apply_configuration__( self ) : 
+         ## Create the lines
+        to_build = [ ( 'DiMuonLowMass',    self.diMuon_streamer ),
+                     ( 'DiMuonHighMass',   self.diMuon_streamer ),
+                     ( 'SingleMuonHighPT', self.singleMuon_streamer ),
+                     ( 'SingleMuonNoIP',   self.singleMuon_streamer ) ]
+        for line, streamer in to_build:
+            self.build_line( line, streamer )
