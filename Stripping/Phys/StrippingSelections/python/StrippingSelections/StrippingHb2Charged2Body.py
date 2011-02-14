62a8f6ab5b9cf117203ebbe7e1c1d159e8b92f83
Stefano Perazzini <stefano.perazzini@cern.ch>
2011-02-14 09:31:44 +0000

    Update of StrippingHb2Charged2Body lines to new framework
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@117737 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2f32c1dee6f128367c81ac7fdf827d6d0c9fc85a

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingHb2Charged2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingHb2Charged2Body.py
@@ -14,101 +14,144 @@ stream.appendLines( Hb2Charged2BodyLines().lines() )
 """
 
 ########################################################################
+uthor__ = ['Stefano Perazzini','Angelo Carbone','Eduardo Rodrigues']
+__date__ = '07/02/2011'
+__version__ = '$Revision: 1.1 $'
 
-from Configurables import CombineParticles
+__all__ = ('Hb2Charged2BodyLines',
+           'makeB2Charged2Body',
+           'makeB2PPbar')
 
-from PhysSelPython.Wrappers      import Selection, DataOnDemand
+from Gaudi.Configuration import *
+
+from GaudiConfUtils.ConfigurableGenerators import CombineParticles
+from StandardParticles import StdNoPIDsPions, StdLooseProtons 
+
+from PhysSelPython.Wrappers      import Selection
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingSelections.Utils   import checkConfig 
+from StrippingUtils.Utils import LineBuilder
 
+name = "Presel"
 
+config = { 'PrescaleB2Charged2Body' : 1, 'PrescaleB2PPbar' : 1,
+           'MinPTB2Charged2Body' : 700,  'MinPTB2PPbar' : 900,
+           'MinIP' : 0.08,               'MinIPChi2' : 9,
+           'TrChi2' : 5,
+                                         'PIDppi' : 0, 
+                                         'PIDpk' : -2,
+           'MaxPTB2Charged2Body' : 2400, 'MaxPTB2PPbar' : 2500,
+           'MaxIP' : 0.2,
+           'CombMassLow' : 4500, 
+           'CombMassHigh' : 6500,        'CombMassWindow' : 200,
+           'BPT' : 1000,
+           'BTAU' : 0.0005,              'VertexChi2' : 16,
+                                         'BIPChi2' : 36, 
+                                         'BDIRA' : 0.9995,
+           'MassLow' : 4800,
+           'MassHigh' : 5800
+         }
 
-class Hb2Charged2BodyLines( object ) :
+class Hb2Charged2BodyLines( LineBuilder ) :
     """Class defining the Hb -> hh stripping lines"""
-    def __init__( self,
-                  moduleName = 'Hb2Charged2BodyLines',
-                  config = {
-                    'Prescales' : {   # Prescale fractions for individual lines
-	                 'B2Charged2Body' : 1.,
-                         'B2PPbarLine'    : 1.
-                    }, 
-                    'CheckPV' : True  # checkPV condition for all lines 
-                  }
-                ) :
+   
+    __configuration_keys__ = ( 'PrescaleB2Charged2Body', 'PrescaleB2PPbar',
+                               'MinPTB2Charged2Body',    'MinPTB2PPbar',
+                               'MinIP',                  'MinIPChi2',
+                               'TrChi2', 
+                                                         'PIDppi', 
+                                                         'PIDpk',
+                               'MaxPTB2Charged2Body',    'MaxPTB2PPbar', 
+                               'MaxIP',
+                               'CombMassLow', 
+                               'CombMassHigh',           'CombMassWindow',
+                               'BPT',        
+                               'BTAU',                   'VertexChi2',
+                                                         'BIPChi2',
+                                                         'BDIRA',      
+                               'MassLow',  
+                               'MassHigh'
+                             )
+
+    def __init__( self,name,config ) :
         
-        # variable storing the list of stripping lines
-        self.__lines = []
-        
-        # check the class configuration
-        __configuration_keys__ = ( 'Prescales', 'CheckPV' )
-        
-        checkConfig( __configuration_keys__, config )
-        
+
+        LineBuilder.__init__(self, name, config)
+
+        B2Charged2BodyName = name+"B2Charged2Body"
+        B2PPbarName = name+"B2PPbar"
+
         # make the various stripping selections
-        B2Charged2Body = makeB2Charged2Body( moduleName )        
-        B2PPbar        = makeB2PPbar       ( moduleName )
+        self.B2Charged2Body = makeB2Charged2Body( B2Charged2BodyName,
+                                                  config['MinPTB2Charged2Body'],
+                                                  config['MinIP'],
+                                                  config['TrChi2'],
+                                                  config['MaxPTB2Charged2Body'],
+                                                  config['MaxIP'],
+                                                  config['CombMassLow'],
+                                                  config['CombMassHigh'],
+                                                  config['BPT'],
+                                                  config['BTAU'],
+                                                  config['MassLow'],
+                                                  config['MassHigh'] )
+
+        self.B2PPbar = makeB2PPbar( B2PPbarName,
+                                    config['MinPTB2PPbar'],
+                                    config['TrChi2'],
+                                    config['PIDppi'],
+                                    config['PIDpk'],
+                                    config['MinIPChi2'],
+                                    config['CombMassWindow'],
+                                    config['MaxPTB2PPbar'], 
+                                    config['VertexChi2'], 
+                                    config['BIPChi2'], 
+                                    config['BDIRA'] )                            
         
-        Hb2Charged2BodySelections = {
-            'B2Charged2Body' : B2Charged2Body,
-            'B2PPbarLine'    : B2PPbar
-            }
+        self.lineB2Charged2Body = StrippingLine( B2Charged2BodyName+"Line",
+                                                 prescale  = config['PrescaleB2Charged2Body'],
+                                                 selection = self.B2Charged2Body )
 
-        # make the list of stripping lines
-        for name, sel in Hb2Charged2BodySelections.iteritems() :
-            line = StrippingLine( name,
-                                  prescale = config[ 'Prescales' ][ name ],
-                                  algos    = [ sel ],
-                                  checkPV  = config[ 'CheckPV' ]
-                                  )
-            self.__lines.append( line )
+        self.lineB2PPbar = StrippingLine( B2PPbarName+"Line",
+                                          prescale = ['PrescaleB2PPbar'],
+                                          selection = self.B2PPbar )
 
-    def lines( self ) :
-        """Get the list of the stripping lines instances"""
-        return self.__lines
-    
-    def lineNames( self ) :
-        """Retrieve the names of all stripping lines"""
-        return [ line.name() for line in self.__lines ]
-    
-    def line( self, line_name ) :
-        """Get the instance of a given stripping line"""
-        stripping_line = [ line for line in self.__lines if line.name() == line_name ]
-        if stripping_line == [] :
-            raise ValueError( 'No such stripping line with name "%s"!' % line_name )
-        return stripping_line
+        self.registerLine(self.lineB2Charged2Body)
+        self.registerLine(self.lineB2PPbar)    
 
+def makeB2Charged2Body( name, 
+                        minPT,minIP,trChi2,
+                        maxPT,maxIP,combMassLow,combMassHigh,
+                        bPT,bTAU,massLow,massHigh ) :
+    
+    _daughters_cuts = "(PT > %(minPT)s * MeV) & (MIPDV(PRIMARY) > %(minIP)s ) & (TRCHI2DOF < %(trChi2)s)" %locals()
+    _combination_cuts = "(AM > %(combMassLow)s * MeV) & (AM < %(combMassHigh)s * MeV) & (AMAXCHILD(MAXTREE('pi+'==ABSID,PT)) > %(maxPT)s ) & (AMAXCHILD(MAXTREE('pi+'==ABSID,MIPDV(PRIMARY))) > %(maxIP)s)" %locals()
+    _mother_cuts = "(M > %(massLow)s * MeV) & (M < %(massHigh)s * MeV) & (PT > %(bPT)s * MeV) & (BPVLTIME('PropertimeFitter/properTime:PUBLIC') > %(bTAU)s )" %locals()
 
-def makeB2Charged2Body( moduleName ) :
-    """Make a SelPy.selection Hb -> h h Selection object for a given stripping module name.
-    Example module: Hb2Charged2BodyLines"""
-    stdLoosePions = DataOnDemand( Location = 'Phys/StdLoosePions' )
+    CombineHb2Charged2Body = CombineParticles( DecayDescriptor = 'B0 -> pi+ pi-',
+                                               DaughtersCuts = { "pi+" : _daughters_cuts },
+                                               CombinationCut = _combination_cuts,
+                                               MotherCut = _mother_cuts )
     
-    PreselHb2Charged2Body = CombineParticles( 'PreselHb2Charged2Body' )
-    PreselHb2Charged2Body.DecayDescriptor = 'B0 -> pi+ pi-'
-    PreselHb2Charged2Body.DaughtersCuts = { "pi+" : "(PT>700*MeV) & (MIPDV(PRIMARY)>0.08) & (TRCHI2DOF<5)"}
-    PreselHb2Charged2Body.CombinationCut = "(AM > 4500*MeV) & (AM < 6500*MeV) & (AMAXDOCA('') < 0.08*mm) & (AMAXCHILD(MAXTREE('pi+'==ABSID,PT))>2400) & (AMAXCHILD(MAXTREE('pi+'==ABSID,MIPDV(PRIMARY)))>0.2)"
-    PreselHb2Charged2Body.MotherCut = "(M>4800*MeV) & (M<5800*MeV) & (BPVIP()<0.06) & (BPVVDSIGN>1.8)"
-    
-    return Selection( 'SelHb2Charged2BodyFor' + moduleName,
-                      Algorithm = PreselHb2Charged2Body,
-                      RequiredSelections = [ stdLoosePions ] )    
+    return Selection( name,
+                      Algorithm = CombineHb2Charged2Body,
+                      RequiredSelections = [ StdNoPIDsPions ] )    
 
+def makeB2PPbar( name,
+                 minPT, trChi2, pidPPi, pidPK, minIPChi2,
+                 combMassWindow, maxPT,
+                 vertexChi2, bIPChi2, bDIRA ) :
 
-def makeB2PPbar( moduleName ) :
-    """Make a SelPy.selection B -> p pbar Selection object for a given stripping module name.
-    Example module: Hb2Charged2BodyLines"""
-    
-    stdLooseProtons = DataOnDemand( Location = 'Phys/StdLooseProtons' )
-    
-    PreselB2PPbar = CombineParticles( 'PreselB2PPbar' )
-    PreselB2PPbar.DecayDescriptor = 'B0 -> p+ p~-'
-    PreselB2PPbar.DaughtersCuts = { 'p+' : '(PT>900*MeV) & (TRCHI2DOF<5) & ((PIDp-PIDpi)>0) & ( (PIDp-PIDK) > -2 ) & (MIPCHI2DV(PRIMARY)>9)' }
-    PreselB2PPbar.CombinationCut = "(ADAMASS('B0') < 200*MeV) & ( AMAXCHILD(MAXTREE('p+'==ABSID,PT)) > 2500*MeV )"
-    PreselB2PPbar.MotherCut = '( VFASPF(VCHI2PDOF) < 16 ) & ( BPVIPCHI2() < 36 ) & ( BPVDIRA > 0.9995 )'
-    
-    return Selection( 'SelB2PPbarFor' + moduleName,
-                      Algorithm = PreselB2PPbar,
-                      RequiredSelections = [ stdLooseProtons ] )
+    _daughters_cuts = "(PT > %(minPT)s * MeV) & (TRCHI2DOF < %(trChi2)s) & ((PIDp-PIDpi) > %(pidPPi)s) & ( (PIDp-PIDK) > %(pidPK)s ) & (MIPCHI2DV(PRIMARY) > %(minIPChi2)s)" %locals()
+    _combination_cuts = "(ADAMASS('B0') < %(combMassWindow)s * MeV) & ( AMAXCHILD(MAXTREE('p+'==ABSID,PT)) > %(maxPT)s * MeV )" %locals()
+    _mother_cuts = "( VFASPF(VCHI2PDOF) < %(vertexChi2)s ) & ( BPVIPCHI2() < %(bIPChi2)s ) & ( BPVDIRA > %(bDIRA)s )" %locals()
 
+    CombineB2PPbar = CombineParticles( DecayDescriptor = 'B0 -> p+ p~-',
+                                       DaughtersCuts = { "p+" : _daughters_cuts },
+                                       CombinationCut = _combination_cuts,
+                                       MotherCut = _mother_cuts )
+
+    return Selection( name,
+                      Algorithm = CombineB2PPbar,
+                      RequiredSelections = [ StdLooseProtons ] )
 
 ########################################################################  
