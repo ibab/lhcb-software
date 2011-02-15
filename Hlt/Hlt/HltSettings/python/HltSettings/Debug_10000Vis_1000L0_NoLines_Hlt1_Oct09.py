cd8d8d9527e82c6d81009320246103c60bf9f7bc
Gerhard Raven <gerhard.raven@nikhef.nl>
2011-02-15 21:53:40 +0000

    remove any reference to old Hlt1 lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@117889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 61b74088967fc43b39976428dcaffacdade6c908

--- Hlt/Hlt/HltSettings/python/HltSettings/Debug_10000Vis_1000L0_NoLines_Hlt1_Oct09.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Debug_10000Vis_1000L0_NoLines_Hlt1_Oct09.py
@@ -43,54 +43,9 @@ class Debug_10000Vis_1000L0_NoLines_Hlt1_Oct09 :
         """
         Returns a dictionary of cuts
         """
-        from Hlt1Lines.Hlt1CommissioningLines  import Hlt1CommissioningLinesConf
-        from Hlt1Lines.Hlt1MuonLines           import Hlt1MuonLinesConf
-        from Hlt1Lines.Hlt1HadronLines         import Hlt1HadronLinesConf
-        from Hlt1Lines.Hlt1ElectronLines       import Hlt1ElectronLinesConf
-        from Hlt1Lines.Hlt1PhotonLines         import Hlt1PhotonLinesConf
-        from Hlt1Lines.Hlt1VeloLines           import Hlt1VeloLinesConf
         from Hlt2Lines.Hlt2CommissioningLines import Hlt2CommissioningLinesConf
     
-        return { Hlt1HadronLinesConf : {  'SingleHadron_PTCut' : 4450
-                                         , 'HadSingle_IPChi2Cut' : 25.0
-                                         , 'HadMain_PTCut' : 1600
-                                         , 'HadCompanion_DZCut' : 2.0
-                                         , 'HadDi_VDChi2Cut' : 50.0
-                                         , 'HadCompanion_PTCut' : 950
-                                         , 'HadCompanion_PointingCut' : 1.0
-                                         , 'HadMain_TrackFitChi2Cut' :  10.
-                                        }         
-                 , Hlt1PhotonLinesConf : { 'Pho_EtCut'         : 2800.0
-                                          , 'Pho_IsPho'        : -0.082 
-                                          , 'Track_IPCut'      : 0.15
-                                          , 'Track_PtCut'      : 1250.
-                                          , 'DiTrack_PtCut'    : 650
-                                          }
-                 , Hlt1ElectronLinesConf : { 'EleIP_PtCut'             : 2820.0
-                                            , 'EleIP_IPCut'            : 0.13 
-                                            , 'EleIPCompanion_PtCut'   : 2750
-                                            , 'EleCompanion_PtCut'     : 1000
-                                            }
-                 , Hlt1MuonLinesConf : { 'DiMuon_SumPtCut'    : 1480.0
-                                        , 'Muon_PtCut'         : 6200
-                                        , 'MuonIP_PtCut'       : 1800
-                                        , 'Muon_IPMinCut'      : 0.07
-                                        , 'DiMuon_DOCACut'     : 1.6
-                                        , 'DiMuon_MassCut'     : 2500
-                                        , 'DiMuonIP_MassCut'   : 300
-                                        , 'DiMuon_IPCut'       : 0.08
-                                        #, 'Prescale' : { 'Hlt1MuTrack4JPsi' : 0. }
-                                        , 'MuTrackMuPt'        : 950
-                                        , 'MuTrackMuIP'        : 0.07
-                                        , 'MuTrackTrPt'        : 550
-                                        , 'MuTrackTrIP'        : 0.1
-                                        , 'MuTrackDoca'        : 0.2
-                                        , 'MuTrackDZ'          : 2.0
-                                        , 'MuTrackDimuMass'    : 1000
-                                        , 'MuTrackPoint'       : 0.4
-                                        , 'MuTrackTrChi2'      : 10 
-                                        }
-                 , Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.
+        return { Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.
                                                                , 'Hlt2Forward'      : 0. # first needs a check that /Hlt/Track/Long actually exists..
                                                                , 'Hlt2DebugEvent'   : 0.0001
                                                 }              }
--- Hlt/Hlt/HltSettings/python/HltSettings/Debug_320Vis_300L0_NoLines_Hlt1_Aug09.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Debug_320Vis_300L0_NoLines_Hlt1_Aug09.py
@@ -32,54 +32,9 @@ class Debug_320Vis_300L0_NoLines_Hlt1_Aug09 :
         """
         Returns a dictionary of cuts
         """
-        from Hlt1Lines.Hlt1CommissioningLines  import Hlt1CommissioningLinesConf
-        from Hlt1Lines.Hlt1MuonLines           import Hlt1MuonLinesConf
-        from Hlt1Lines.Hlt1HadronLines         import Hlt1HadronLinesConf
-        from Hlt1Lines.Hlt1ElectronLines       import Hlt1ElectronLinesConf
-        from Hlt1Lines.Hlt1PhotonLines         import Hlt1PhotonLinesConf
-        from Hlt1Lines.Hlt1VeloLines           import Hlt1VeloLinesConf
-        from Hlt2Lines.Hlt2CommissioningLines  import Hlt2CommissioningLinesConf
+        from HLt2Lines.HLt2CommissioningLines  import HLt2CommissioningLinesConf
         
-        return { Hlt1HadronLinesConf : {  'SingleHadron_PTCut' : 4800
-                                         , 'HadSingle_IPChi2Cut' : 25.0
-                                         , 'HadMain_PTCut' : 250
-                                         , 'HadCompanion_DZCut' : 2.0
-                                         , 'HadDi_VDChi2Cut' : 50.0
-                                         , 'HadCompanion_PTCut' : 750
-                                         , 'HadCompanion_PointingCut' : 0.39
-                                         , 'HadMain_TrackFitChi2Cut' :  10.
-                                        } 
-                 , Hlt1PhotonLinesConf : { 'Pho_EtCut'         : 2800.0
-                                          , 'Pho_IsPho'        : -0.082 
-                                          , 'Track_IPCut'      : 0.15
-                                          , 'Track_PtCut'      : 1250.
-                                          , 'DiTrack_PtCut'    : 650
-                                          }
-                 , Hlt1ElectronLinesConf : { 'EleIP_PtCut'             : 2820.0
-                                            , 'EleIP_IPCut'            : 0.13 
-                                            , 'EleIPCompanion_PtCut'   : 2750
-                                            , 'EleCompanion_PtCut'     : 1000
-                                            }
-                 , Hlt1MuonLinesConf : {  'DiMuon_SumPtCut' : 120.0
-                                          , 'Muon_PtCut' : 1000
-                                          , 'MuonIP_PtCut' : 1000
-                                          , 'Muon_IPMinCut' : 0.05
-                                          , 'DiMuon_DOCACut' : 1.0
-                                          , 'DiMuon_MassCut' : 2500
-                                          , 'DiMuonIP_MassCut' : 300
-                                          , 'DiMuon_IPCut' : 0.08
-                                          #, 'Prescale' : { 'Hlt1MuTrack4JPsi' : 0. }
-                                          , 'MuTrackMuPt' : 500
-                                          , 'MuTrackMuIP' : 0.07
-                                          , 'MuTrackTrPt' : 550
-                                          , 'MuTrackTrIP' : 0.1
-                                          , 'MuTrackDoca' : 0.2
-                                          , 'MuTrackDZ' : 2.0
-                                          , 'MuTrackDimuMass' : 1000
-                                          , 'MuTrackPoint' : 0.4
-                                          , 'MuTrackTrChi2' : 10
-                                          }
-                 , Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.
+        return {   Hlt2CommissioningLinesConf : { 'Prescale' : { 'Hlt2PassThrough'  : 1.
                                                                  , 'Hlt2Forward'      : 0. # first needs a check that /Hlt/Track/Long actually exists..
                                                                  , 'Hlt2DebugEvent'   : 0.0001
                                                                  }              }
