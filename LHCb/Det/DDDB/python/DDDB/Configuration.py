f5d09f683362dc500ef47c0d7d9b7587b20b6a43
Illya Shapoval <illya.shapoval@cern.ch>
2011-02-16 12:14:28 +0000

    Defaults for DQFLAGS db tag are added.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@117916 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a216b89f5cd14f90bc1bac4a07d7c19220ad85de

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -19,14 +19,14 @@ class DDDBConf(ConfigurableUser):
                   "DataType"  : "2011",
                   "Simulation": False,
                    }
-    _propertyDocDct = { 
+    _propertyDocDct = {
                        'DbRoot' : """ Root file of the detector description """,
                        'DataType' : """ Symbolic name for the data type. Allowed values: ["2011", "2010", "2009","2008","MC09","DC06","Upgrade"] """,
                        'Simulation' : """ Boolean flag to select the simulation or real-data configuration """,
                        }
-    
+
     __used_configurables__ = [ CondDB ]
-    
+
     def __apply_configuration__(self):
         """
         Converts the high-level information passed as properties into low-level configuration.
@@ -38,13 +38,13 @@ class DDDBConf(ConfigurableUser):
         #   - DetDbLocation must use '/' because it is a URL (never use '\')
         #   - DetStorageType:
         #     + 7 => XML
-        #     + 9 => pure CondDB (remove "conddb:" from URL) 
+        #     + 9 => pure CondDB (remove "conddb:" from URL)
         ##########################################################################
         detDataSvc = DetectorDataSvc( UsePersistency = True,
                                       DetDbRootName = "dd",
                                       DetDbLocation = self.getProp("DbRoot"),
                                       DetStorageType = 7 )
-        
+
         ##########################################################################
         # XML Conversion Service configuration
         ##########################################################################
@@ -54,34 +54,34 @@ class DDDBConf(ConfigurableUser):
         #     defined detector element converters are somehow not available.
         ##########################################################################
         xmlCnvSvc = XmlCnvSvc(AllowGenericConversion = True)
-        
-        xmlParserSvc = XmlParserSvc( 
+
+        xmlParserSvc = XmlParserSvc(
                                     # Set the maximum number of files to be put in the
                                     # cache of the XmlCnvSvc
                                     MaxDocNbInCache = 10,
-                                    
+
                                     # Change the behavior of the cache. A 0 value makes
                                     # it FIFO while a bigger value tends to make it keep
                                     # only reused files.
                                     CacheBehavior = 3,
-                                    
+
                                     # Tell to the XmlParserSvc how to resolve entities
                                     # in the CondDB
                                     EntityResolver = CondDBEntityResolver()
                                     )
-        
+
         ##########################################################################
         # Use the XmlCnvSvc in the persistency service for the detector description
         DetectorPersistencySvc( CnvServices = [ xmlCnvSvc ] )
-        
-        
+
+
         ##########################################################################
         # DetetectorDataSvc must be external, to finalize after ToolSvc
         ApplicationMgr().ExtSvc.insert(0, detDataSvc)
-        
+
         # Propagate the properties to the used Configurables
         self.propagateProperties()
-        
+
         dataType = self.getProp("DataType")
         if not dataType in self.__data_types_handlers__:
             raise ValueError("Invalid value %r for property DetDesc().DataType."
@@ -91,7 +91,7 @@ class DDDBConf(ConfigurableUser):
             sim = self.getProp("Simulation")
             if not sim:
                 log.info("%s data is _always_ simulation", dataType )
-        
+
         # calls the specific configuration function for the requested data type
         self.__data_types_handlers__[dataType](self)
 
@@ -101,7 +101,7 @@ class DDDBConf(ConfigurableUser):
         # New LHCb implementation
         from Configurables import LHCb__ParticlePropertySvc
         LHCb__ParticlePropertySvc( ParticlePropertiesFile = 'conddb:///param/ParticleTable.txt' )
-            
+
     def __set_tag__(self, partitions, tag):
         cdb = CondDB()
         for p in partitions:
@@ -119,7 +119,8 @@ class DDDBConf(ConfigurableUser):
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20101206")
         self.__set_tag__(["LHCBCOND"], "head-20101112")
-    
+        self.__set_tag__(["DQFLAGS"], "tt-20110126")
+
     def __2010_conf__(self):
         """
         Default configuration for 2010 data (and MonteCarlo for DDDB)
@@ -127,7 +128,8 @@ class DDDBConf(ConfigurableUser):
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20101206")
         self.__set_tag__(["LHCBCOND"], "head-20101112")
-    
+        self.__set_tag__(["DQFLAGS"], "tt-20110126")
+
     def __2009_conf__(self):
         """
         Default configuration for 2009 data and corresponding MonteCarlo
@@ -136,7 +138,8 @@ class DDDBConf(ConfigurableUser):
         self.__set_tag__(["DDDB"], "head-20101206")
         self.__set_tag__(["LHCBCOND"], "head-20101112")
         self.__set_tag__(["SIMCOND"], "MC-20101026-vc15mm-md100")
-    
+        self.__set_tag__(["DQFLAGS"], "tt-20110126")
+
     def __2008_conf__(self):
         """
         Default configuration for 2008 data and corresponding MonteCarlo
@@ -145,7 +148,8 @@ class DDDBConf(ConfigurableUser):
         self.__set_tag__(["DDDB"], "head-20101003")
         self.__set_tag__(["LHCBCOND"], "head-20101010")
         self.__set_tag__(["SIMCOND"], "sim-20090212")
-    
+        self.__set_tag__(["DQFLAGS"], "tt-20110126")
+
     def __MC09_conf__(self):
         """
         Default configuration for MC09 MonteCarlo production and analysis
@@ -153,14 +157,14 @@ class DDDBConf(ConfigurableUser):
         # Set the tags
         self.__set_tag__(["DDDB"], "MC09-20090602")
         self.__set_tag__(["SIMCOND"], "MC09-20100921-vc-md100")
-    
+
     def __Upgrade_conf__(self):
         """
         Default database for Upgrade MonteCarlo production and analysis
         """
         # Need also to change connection string to DDDB
         CondDB().PartitionConnectionString = { "DDDB":"sqlite_file:$SQLITEDBPATH/DDDB_upgrade.db/DDDB"}
-    
+
     def __DC06_conf__(self):
         """
         Default configuration for DC06 MonteCarlo production and analysis
@@ -168,25 +172,25 @@ class DDDBConf(ConfigurableUser):
         # Set the tags
         self.__set_tag__(["DDDB"],     "DC06-20090902")
         self.__set_tag__(["LHCBCOND"], "DC06-20081002")
-        
+
         # Specials for DC06
 
-        # The configuration of the database is like the one for real data (no SIMCOND) 
+        # The configuration of the database is like the one for real data (no SIMCOND)
         CondDB().Simulation = False
-        
+
         # Force negative polarity (i.e. don't take it from ONLINE)
         from Configurables import MagneticFieldSvc
         MagneticFieldSvc(ForceToUseDownMap = True,
                          OutputLevel = ERROR)
-        
+
         # Set initial event time to something close to 0 to avoid to use the current
         # time (causes failures when the ONLINE snapshots are updated).
         from Configurables import EventClockSvc
         EventClockSvc(InitialTime = 1)
-        
+
         # Backward compatibility Dll to read HepMC 1 record
         ApplicationMgr().Dlls += [ "HepMCBack" ]
-    
+
     __data_types_handlers__ =  { "2011": __2011_conf__,
                                  "2010": __2010_conf__,
                                  "2009": __2009_conf__,
