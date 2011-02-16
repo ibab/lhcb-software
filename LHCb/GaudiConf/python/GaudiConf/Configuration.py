8722a62ae1bf0a3536220586db2bc3e289c4af21
Illya Shapoval <illya.shapoval@cern.ch>
2011-02-16 09:24:56 +0000

    'DQFLAGStag' property added to the LHCbApp configurable.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@117900 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4e38ac04ef2565030393215543489a9b10c49360

--- LHCb/GaudiConf/python/GaudiConf/Configuration.py
+++ LHCb/GaudiConf/python/GaudiConf/Configuration.py
@@ -17,34 +17,36 @@ class LHCbApp(LHCbConfigurableUser):
        ,"DataType"    : "2009"
        ,"DDDBtag"     : ""
        ,"CondDBtag"   : ""
+       ,"DQFLAGStag"  : ""
        ,"Simulation"  : False
        ,"Monitors"    : []
-       ,"OutputLevel" : INFO 
+       ,"OutputLevel" : INFO
        ,"TimeStamp"   : False
        ,"XMLSummary"   : None
         }
-    
-    _propertyDocDct = { 
+
+    _propertyDocDct = {
         'EvtMax'      : """ Maximum number of events to process """
        ,'SkipEvents'  : """ Number of events to skip """
        ,'DataType'    : """ Data type, see DDDBConf for allowed values. Default '2009' """
        ,'DDDBtag'     : """ Tag for DDDB. Default as set in DDDBConf for DataType """
        ,'CondDBtag'   : """ Tag for CondDB. Default as set in DDDBConf for DataType """
+       ,"DQFLAGStag"  : """ Tag for DQFLAGS. Default as set in DDDBConf for DataType """
        ,'Simulation'  : """ Flag to indicate usage of simulation conditions """
        ,'Monitors'    : """ List of monitors to execute """
        ,'OutputLevel' : """ The printout level to use (default INFO) """
        ,'TimeStamp'   : """ Flag to add a time stamp to messages (default False) """
        ,'XMLSummary'   : """ Add an XML summary file, default None """
        }
-    
+
     __used_configurables__ = [ DDDBConf, XMLSummary ]
-    
+
     def knownMonitors(self):
         return ["SC", "FPE"]
-    
+
     def knownAuditors(self):
         return ["NameAuditor","MemoryAuditor","ChronoAuditor"]
-    
+
     def defineDB(self):
         # Delegate handling of properties to DDDBConf
         self.setOtherProps( DDDBConf(), ["Simulation", "DataType" ] )
@@ -55,7 +57,9 @@ class LHCbApp(LHCbConfigurableUser):
         if hasattr( self, "CondDBtag" ):
             CondDB().Tags [ "LHCBCOND" ] = self.getProp("CondDBtag")
             CondDB().Tags [ "SIMCOND"  ] = self.getProp("CondDBtag")
-    
+        if hasattr( self, "DQFLAGStag" ):
+            CondDB().Tags [ "DQFLAGS" ] = self.getProp("DQFLAGStag")
+
     def defineEvents(self):
         # Set up transient store and data on demand service
         EventDataSvc( ForceLeaves        = True,
@@ -70,19 +74,19 @@ class LHCbApp(LHCbConfigurableUser):
 
         # Delegate handling to ApplicationMgr configurable
         self.setOtherProps(ApplicationMgr(),["EvtMax"])
-    
+
     def evtMax(self):
         if hasattr(ApplicationMgr(),"EvtMax") and not hasattr(self,"EvtMax"):
             return ApplicationMgr().getProp("EvtMax")
         else:
             return self.getProp("EvtMax")
-    
+
     def skipEvents(self):
         if hasattr(EventSelector(),"FirstEvent") and not hasattr(self,"SkipEvents"):
             return EventSelector().getProp("FirstEvent") - 1
         else:
             return self.getProp("SkipEvents")
-    
+
     def defineMonitors(self):
         for prop in self.getProp("Monitors"):
             if prop not in self.knownMonitors():
@@ -99,15 +103,15 @@ class LHCbApp(LHCbConfigurableUser):
             getConfigurable("StatusCodeSvc").OutputLevel = INFO
         if "FPE" in self.getProp("Monitors"):
             importOptions( "$STDOPTS/FPEAudit.opts" )
-    
+
     def defineXMLSum(self):
         if hasattr( self, "XMLSummary" ):
             self.setOtherProps( XMLSummary(), ["XMLSummary" ] )
-            
+
     def defineOutput(self):
         # Message service
         msgSvc = getConfigurable("MessageSvc")
-        
+
         # Modify printout defaults
         if self.isPropertySet("OutputLevel"):
             level = self.getProp("OutputLevel")
@@ -123,7 +127,7 @@ class LHCbApp(LHCbConfigurableUser):
             # add a time stamp to remaining messages
             msgSvc.Format = "%u % F%18W%S%7W%R%T %0W%M"
             msgSvc.timeFormat = "%Y-%m-%d %H:%M:%S UTC"
-    
+
     def __apply_configuration__(self):
         self.defineDB()
         self.defineEvents()
