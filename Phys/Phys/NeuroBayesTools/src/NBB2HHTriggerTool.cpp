5ade5007ba11fab46aa5be57bffae8192173dde5
ukerzel <ukerzel@cern.ch>
2011-02-18 13:54:38 +0000

    first import of NeuroBayesTools under Phys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@118168 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ebd2b881f4016e550f383215297f7d2f25688c8

---
+++ Phys/Phys/NeuroBayesTools/src/NBB2HHTriggerTool.cpp
@@ -0,0 +1,346 @@
+// $Id: $
+// Include files 
+#include <cmath>
+#include <algorithm>
+
+#include "Event/RecVertex.h"
+#include "GaudiAlg/CheckForNaN.h"
+#include <Event/MuonPID.h>
+
+//LoKi
+#include <LoKi/ParticleCuts.h>
+
+// local
+#include "NBB2HHTriggerTool.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : NBB2HHTriggerTool
+//
+// 2011-02-16 : Ulrich Kerzel
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( NBB2HHTriggerTool )
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+NBB2HHTriggerTool::NBB2HHTriggerTool( const std::string& type,
+                                      const std::string& name,
+                                      const IInterface* parent ):
+  base_class(type,name,parent),
+  m_NetworkCut (       -1.0         ),
+  m_DoPrecuts  (       false        ),
+  m_nVar       (       16           )
+{
+  declareProperty("Expertise"    , m_Expertise        , "NeuroBayes Expertise");
+  declareProperty("NetworkCut"   , m_NetworkCut       , "Cut on Network output");
+  declareProperty("DoPrecuts"    , m_DoPrecuts        , "Apply pre-cuts");
+}
+
+// ===========================================================================
+StatusCode NBB2HHTriggerTool::initialize() {
+
+  if ( msgLevel(MSG::DEBUG) )
+    debug() << "==> Initialize" << endmsg;
+
+
+
+  // initialize the base class  (the first action)
+  StatusCode sc = GaudiTool::initialize();
+  if(sc.isFailure()) return sc;
+
+  // get tools and algs
+  m_DistCalc   = tool<IDistanceCalculator>("LoKi::DistanceCalculator",this);
+  if (!m_DistCalc) {
+    Error("Could not get DistanceCalculator");
+    return StatusCode::FAILURE;
+  }
+
+  m_HistoTool = tool<IHistoTool>("HistoTool", this); 
+  if (!m_HistoTool){
+    Error("Could not get Histogramming tool");
+    return StatusCode::FAILURE;
+  }
+
+  //
+  // print settings
+  //
+  verbose() <<  "Expertise      " <<  m_Expertise     << endmsg;
+  debug()   <<  "NetworkCut     " <<  m_NetworkCut    << endmsg;
+  debug()   <<  "Apply precuts  " <<  m_DoPrecuts     << endmsg;
+ 
+  //
+  // setup NeuroBayes
+  //
+#ifdef __GNUC__
+
+  m_NBExpert = new Expert(&m_Expertise.front());
+
+  // prepare arry of input variables, use max. allowed length
+  // (easier for switching between different networks)
+  m_inArray = new float[NB_MAXNODE];
+
+
+#endif
+
+
+  return StatusCode::SUCCESS;
+} //initialise
+// ============================================================================
+bool NBB2HHTriggerTool::operator()(const LHCb::Particle* p) const {
+
+  if(0 == p) {
+    Error("LHCb::Particle* points to NULL, return false");
+    return false ;
+  } // null
+
+  // safe side: reject everything by default  
+  bool returnValue = false;
+
+  bool filledInputVar =  NBB2HHTriggerTool::getInputVar(p);
+
+  if (filledInputVar) {
+
+    if ( msgLevel(MSG::VERBOSE) ) {
+      for (int i = 0; i< m_nVar; i++) {
+        debug() << "input var [" << i << "] = " << m_inArray[  i ] << endmsg;
+      } //for i
+    } // if msg
+
+    const double netOut = m_NBExpert->nb_expert(m_inArray);
+    const double prob   = (1.0 + netOut)*0.5;
+
+    m_HistoTool->plot1D(netOut, "BhhNet" , "NeuroBayes Bhh network output ", -1.0  , 1.0 ,  120);
+
+
+    if ( msgLevel(MSG::DEBUG) ) {
+      debug() << "Bhh cand: Network output " << netOut
+              << " probability " << prob << endmsg;
+    }
+
+
+    // accept candidate?
+    if (prob > m_NetworkCut) {
+      if ( msgLevel(MSG::DEBUG) )
+        debug() << "Bhh cand pass cut "  << m_NetworkCut << endmsg;
+
+      // accept candidate
+      returnValue = true;
+    } // if prob
+  } // if sc
+
+
+
+
+  return returnValue;
+
+} //bool operator
+
+// ============================================================================
+bool NBB2HHTriggerTool::getInputVar(const LHCb::Particle* particle) const {
+
+#ifdef __GNUC__
+  // get daughters
+  const SmartRefVector< LHCb::Particle > &daughters = particle->endVertex()->outgoingParticles();
+
+  if (daughters.size() != 2) {
+    warning() << "number of daughters at vertex, skip candidate " << daughters.size() << endmsg;
+    return false;
+  }//if #Kaons
+
+  const LHCb::Particle* hPlus  = NULL;
+  const LHCb::Particle* hMinus = NULL;
+  SmartRefVector< LHCb::Particle >::const_iterator iDaughter;
+  SmartRefVector< LHCb::Particle >::const_iterator iDaughterBegin = daughters.begin();
+  SmartRefVector< LHCb::Particle >::const_iterator iDaughterEnd   = daughters.end();
+
+  if ( msgLevel(MSG::VERBOSE) )
+    verbose() << " h 1 charge " << (*daughters[0]).charge()
+              << " h 2 charge " << (*daughters[1]).charge()
+              << endmsg;
+
+  for (iDaughter = iDaughterBegin; iDaughter != iDaughterEnd; iDaughter++){
+    if ( (*iDaughter)->charge() > 0) {
+      hPlus  = *iDaughter;
+    } else {
+      hMinus = *iDaughter;
+    }// if
+  }//for iKaon
+
+  // sanity check
+  if (!hPlus || !hMinus) {
+    warning() << " Daughters obtained from not valid, skip candidate" << endmsg;
+    return false;
+  }// if
+
+  //
+  // get variables needed for pre-cuts
+  //
+  double hPlusPt         = hPlus->pt()/Gaudi::Units::GeV;
+  double hMinusPt        = hMinus->pt()/Gaudi::Units::GeV;
+  double hPlusP          = hPlus->p()/Gaudi::Units::GeV;
+  double hMinusP         = hMinus->p()/Gaudi::Units::GeV;
+  double hPlusDllK       = hPlus ->proto()->info(LHCb::ProtoParticle::CombDLLk,-999);
+  double hMinusDllK      = hMinus->proto()->info(LHCb::ProtoParticle::CombDLLk,-999);
+  double hPlusTrackChi2  = hPlus ->proto()->track()->probChi2();
+  double hMinusTrackChi2 = hMinus->proto()->track()->probChi2();
+
+  double chi2B           = particle->endVertex()->chi2();
+
+  double     docaB    = -999;
+  double     docaChi2 = -999;
+  StatusCode sc2;
+  sc2 = m_DistCalc->distance(hPlus, hMinus, docaB, docaChi2);
+  if (sc2 != StatusCode::SUCCESS) {
+    docaB     = -999;
+    docaChi2  = -999;
+  } //if
+
+  double minP            = std::min(hPlusP    , hMinusP);
+  //double maxP            = std::max(hPlusP    , hMinusP);
+
+  double minPt           = std::min(hPlusPt    , hMinusPt);
+  double maxPt           = std::max(hPlusPt    , hMinusPt);
+
+  //double minDllK         = std::min(hPlusDllK  , hMinusDllK);
+  double maxDllK         = std::max(hPlusDllK  , hMinusDllK);
+
+
+  //
+  // appply pre-cuts
+  //
+  if (m_DoPrecuts) {
+    double mB              = particle->measuredMass()/Gaudi::Units::GeV;
+
+    bool hPlusAboveThresholdK  = false;
+    bool hMinusAboveThresholdK = false;
+    if (hPlus ->proto()->richPID()) {
+      hPlusAboveThresholdK   = hPlus ->proto()->richPID()->kaonHypoAboveThres();
+    }
+    if (hMinus->proto()->richPID()) {
+      hMinusAboveThresholdK = hMinus ->proto()->richPID()->kaonHypoAboveThres();
+    }
+
+
+    if (mB               < 5.0      ||
+        mB               > 5.8      ||
+        !hPlusAboveThresholdK       ||
+        !hMinusAboveThresholdK      ||
+        hPlusTrackChi2   >  5       ||
+        hMinusTrackChi2  >  5       ||
+        minP             < 10.0     ||
+        minPt            <  1.5     ||
+        (hPlusDllK   > -0.1 && hPlusDllK  < 0.1) ||
+        (hMinusDllK  > -0.1 && hMinusDllK < 0.1) ||
+        docaB            > 0.10     ||
+        chi2B            > 25.0     ||
+        maxPt            <  2.0     ||
+        maxDllK          <  0.1)
+      return false;
+  } //if doPrecut
+
+
+  //
+  // get remaining variables
+  //
+
+  // not accessible at trigger level 
+  //double hPlusCaloE      = hPlus ->proto()->info(LHCb::ProtoParticle::CaloEcalE, -999);
+  //double hMinusCaloE     = hMinus->proto()->info(LHCb::ProtoParticle::CaloEcalE, -999);
+  //double hPlusDllP       = hPlus ->proto()->info(LHCb::ProtoParticle::CombDLLp,-999);
+  //double hMinusDllP      = hMinus->proto()->info(LHCb::ProtoParticle::CombDLLp,-999);
+  //double hPlusDllMu      = hPlus ->proto()->info(LHCb::ProtoParticle::CombDLLmu,-999);
+  //double hMinusDllMu     = hMinus->proto()->info(LHCb::ProtoParticle::CombDLLmu,-999);
+  //double hPlusGhost      = hPlus ->proto()->track()->ghostProbability();
+  //double hMinusGhost     = hMinus->proto()->track()->ghostProbability();
+
+  double hPlusIsMuon     = 0;
+  double hMinusIsMuon    = 0;
+  const LHCb::MuonPID *hPlusMuonPID  = hPlus ->proto()->muonPID();
+  const LHCb::MuonPID *hMinusMuonPID = hMinus->proto()->muonPID();
+  if (hPlusMuonPID)
+    hPlusIsMuon = hPlusMuonPID->IsMuon();
+  if (hMinusMuonPID)
+    hMinusIsMuon = hMinusMuonPID->IsMuon();
+  double mErrB           = particle->measuredMassErr()/Gaudi::Units::GeV;
+  
+  double ptB             = particle->pt()/Gaudi::Units::GeV;
+  double pB              = particle->p() /Gaudi::Units::GeV;
+
+
+  double cosTheta        = LoKi::Cuts::LV01(particle);
+  if (lnan(cosTheta))
+    cosTheta = -999;
+
+
+
+
+
+  //
+  // move delta functions to -999
+  //
+
+  //if (hPlusCaloE > -0.01 && hPlusCaloE < 0.01)
+  //  hPlusCaloE = -999;
+  //if (hMinusCaloE > -0.01 && hMinusCaloE < 0.01)
+  //  hMinusCaloE = -999;
+  //if (hPlusCaloE < -999)
+  //  hPlusCaloE = -999;
+  //if (hMinusCaloE < -999)
+  //  hMinusCaloE = -999;
+
+  if (hPlusDllK > -0.01 && hPlusDllK < 0.01)
+    hPlusDllK = -999;
+  if (hMinusDllK > -0.01 && hMinusDllK < 0.01)
+    hMinusDllK = -999;
+
+
+  //if (hPlusDllP > -0.01 && hPlusDllP < 0.01)
+  //  hPlusDllP = -999;
+  //if (hMinusDllP > -0.01 && hMinusDllP < 0.01)
+  //  hMinusDllP = -999;
+  //if (hPlusDllP > -0.01 && hPlusDllP < 0.01)
+  //  hPlusDllP = -999;
+  //if (hMinusDllP > -0.01 && hMinusDllP < 0.01)
+  //  hMinusDllP = -999;
+  //
+  //
+  //if (hPlusDllMu > -0.1 && hPlusDllMu < 0.01)
+  //  hPlusDllK = -999;
+  //if (hMinusDllMu > -0.1 && hMinusDllMu < 0.01)
+  //  hMinusDllMu = -999;
+  //
+  //if (hPlusGhost < 0)
+  //  hPlusGhost = -999;
+  //
+  //if (hMinusGhost < 0)
+  //  hMinusGhost = -999;
+
+
+  //
+  // fill input array
+  //
+  m_inArray[  0 ] =  hPlusIsMuon;
+  m_inArray[  1 ] =  hMinusIsMuon;
+  m_inArray[  2 ] =  hPlusDllK;
+  m_inArray[  3 ] =  hMinusDllK;
+  m_inArray[  4 ] =  minPt;
+  m_inArray[  5 ] =  maxPt;
+  m_inArray[  6 ] =  hPlusP;
+  m_inArray[  7 ] =  hMinusP;
+  m_inArray[  8 ] =  chi2B;
+  m_inArray[  9 ] =  mErrB;
+  m_inArray[ 10 ] =  pB;
+  m_inArray[ 11 ] =  docaB;
+  m_inArray[ 12 ] =  hPlusTrackChi2;
+  m_inArray[ 13 ] =  hMinusTrackChi2;
+  m_inArray[ 14 ] =  cosTheta;
+  m_inArray[ 15 ] =  ptB; 
+
+
+#endif
+
+  return true;
+} //getInputVar
