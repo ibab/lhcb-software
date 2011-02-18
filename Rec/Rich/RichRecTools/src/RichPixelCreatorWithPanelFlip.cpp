e393a4cf3ad849dc6b713c09d8fa4155d478361e
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-18 18:38:27 +0000

    ! 2011-02-18 - Chris Jones  - Add new tool PixelCreatorWithPanelFlip   + Does the same as the default PixelCreatorFromRawBuffer but flips the panel     information. So left <-> right in RICH2, top <-> bottom in RICH1. Useful     for background studies.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@118201 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d54c34ac848c7648bae7defdce0d3a0f5db4d63a

---
+++ Rec/Rich/RichRecTools/src/RichPixelCreatorWithPanelFlip.cpp
@@ -0,0 +1,91 @@
+
+//---------------------------------------------------------------------------------
+/** @file RichPixelCreatorWithPanelFlip.cpp
+ *
+ *  Implementation file for tool : Rich::Rec::PixelCreatorWithPanelFlip
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   23/11/2006
+ */
+//---------------------------------------------------------------------------------
+
+// local
+#include "RichPixelCreatorWithPanelFlip.h"
+
+// All code is in general Rich reconstruction namespace
+using namespace Rich::Rec;
+
+//---------------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( PixelCreatorWithPanelFlip )
+
+// Standard constructor
+PixelCreatorWithPanelFlip::
+PixelCreatorWithPanelFlip( const std::string& type,
+                           const std::string& name,
+                           const IInterface* parent )
+  : PixelCreatorFromRawBuffer ( type, name, parent  ),
+    m_flipRICH                ( Rich::NRiches, true )
+{
+  declareProperty ( "FlipRICHes", m_flipRICH );
+}
+
+StatusCode PixelCreatorWithPanelFlip::initialize()
+{
+  // base class initilize
+  const StatusCode sc = PixelCreatorFromRawBuffer::initialize();
+  if ( sc.isFailure() ) { return sc; }
+
+  info() << "Flipping RICH hits for RICHes(1/2) : " << m_flipRICH << endmsg;
+
+  return sc;
+}
+
+LHCb::RichSmartID 
+PixelCreatorWithPanelFlip::flip( const LHCb::RichSmartID & id ) const
+{
+  // make a temp copy of the ID
+  LHCb::RichSmartID flipID(id);
+
+  // Flip the appropriate panels
+  if      ( Rich::Rich1 == id.rich() && m_flipRICH[Rich::Rich1] )
+  {
+    flipID.setPanel( Rich::top  == id.panel() ? Rich::bottom : Rich::top  );     
+  }
+  else if ( Rich::Rich2 == id.rich() && m_flipRICH[Rich::Rich2] )
+  {
+    flipID.setPanel( Rich::left == id.panel() ? Rich::right  : Rich::left );     
+  }
+
+  // return the flipped ID
+  return flipID;
+}
+
+LHCb::RichRecPixel *
+PixelCreatorWithPanelFlip::buildPixel( const Rich::HPDPixelCluster& cluster ) const
+{
+  // Make new container for flipped RichSmartIDs
+  HPDPixelCluster::SmartIDVector flipIDs;
+  flipIDs.reserve( cluster.smartIDs().size() );
+  
+  // Loop over each RichSmartID in this cluster, flip it and save in the new list
+  for ( HPDPixelCluster::SmartIDVector::const_iterator ID = cluster.smartIDs().begin();
+        ID != cluster.smartIDs().end(); ++ID )
+  { 
+    flipIDs.push_back( flip(*ID) ); 
+  }
+  
+  // create a new cluster
+  const HPDPixelCluster flipCluster(flipIDs);
+  
+  // reconstruct with the new cluster
+  return PixelCreatorFromRawBuffer::buildPixel(flipCluster);
+}
+
+LHCb::RichRecPixel *
+PixelCreatorWithPanelFlip::buildPixel( const LHCb::RichSmartID & id ) const
+{
+  // reconstruct with the flipped ID
+  return PixelCreatorFromRawBuffer::buildPixel( flip(id) );
+}
---
+++ Rec/Rich/RichRecTools/src/RichPixelCreatorWithPanelFlip.h
@@ -0,0 +1,80 @@
+
+//---------------------------------------------------------------------------------
+/** @file RichPixelCreatorWithPanelFlip.h
+ *
+ *  Header file for tool : Rich::Rec::PixelCreatorWithPanelFlip
+ *
+ *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+ *  @date   18/02/2011
+ */
+//---------------------------------------------------------------------------------
+
+#ifndef RICHRECTOOLS_RichPixelCreatorWithPanelFlip_H
+#define RICHRECTOOLS_RichPixelCreatorWithPanelFlip_H 1
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h"
+#include "GaudiKernel/RndmGenerators.h"
+#include "GaudiKernel/IRndmGenSvc.h"
+
+// base class
+#include "RichPixelCreatorFromRawBuffer.h"
+
+namespace Rich
+{
+  namespace Rec
+  {
+
+    //---------------------------------------------------------------------------------
+    /** @class PixelCreatorWithPanelFlip RichPixelCreatorWithPanelFlip.h
+     *
+     *  Tool for the creation and book-keeping of RichRecPixel objects.
+     *
+     *  Inherits from RichPixelCreatorFromRawBuffer, but flips the HPD panel
+     *  information ( So left <-> right in RICH2, up <-> down in RICH1 )
+     *
+     *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
+     *  @date   18/02/2011
+     */
+    //---------------------------------------------------------------------------------
+
+    class PixelCreatorWithPanelFlip : public PixelCreatorFromRawBuffer
+    {
+
+    public: // Methods for Gaudi Framework
+
+      /// Standard constructor
+      PixelCreatorWithPanelFlip( const std::string& type,
+                                 const std::string& name,
+                                 const IInterface* parent );
+
+      /// Destructor
+      virtual ~PixelCreatorWithPanelFlip() {}
+
+      // Initialize method
+      virtual StatusCode initialize();
+
+    protected: // methods
+
+      /// Build a new RichRecPixel from a Rich::HPDPixelCluster
+      virtual LHCb::RichRecPixel * buildPixel ( const Rich::HPDPixelCluster& cluster ) const;
+
+      /// Build a new RichRecPixel a single LHCb::RichSmartID
+      virtual LHCb::RichRecPixel * buildPixel ( const LHCb::RichSmartID & id ) const;
+
+    private: // methods
+
+      /// Flip the given RichSmartID
+      LHCb::RichSmartID flip( const LHCb::RichSmartID & id ) const;
+
+    private: // data
+
+      /// Flags to turn on the flipping for RICH1 and RICH2
+      std::vector<bool> m_flipRICH;
+
+    };
+
+  }
+}
+
+#endif // RICHRECTOOLS_RichPixelCreatorWithPanelFlip_H
