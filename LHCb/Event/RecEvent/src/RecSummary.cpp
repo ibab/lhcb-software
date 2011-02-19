3ea7305c5351377b2f08a10a2ba53ed70ad6f62d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-19 12:12:01 +0000

    always print all members of RecSummary, even if zero
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@118210 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c6ee4ff33ebd5ebbeb012c622f532601f8bc594e

--- LHCb/Event/RecEvent/src/RecSummary.cpp
+++ LHCb/Event/RecEvent/src/RecSummary.cpp
@@ -17,51 +17,32 @@ std::ostream& LHCb::RecSummary::fillStream(std::ostream& s) const
 {
   s << "{ Summary Info :";
 
-  if ( hasInfo(LHCb::RecSummary::nPVs) )
-    s << " nPVs = " << info( LHCb::RecSummary::nPVs, 0 );
+  s << " nPVs = " << info( LHCb::RecSummary::nPVs, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nLongTracks) )
-    s << " nLongTracks = " << info( LHCb::RecSummary::nLongTracks, 0 );
-  if ( hasInfo(LHCb::RecSummary::nDownstreamTracks) )
-    s << " nDownstreamTracks = " << info( LHCb::RecSummary::nDownstreamTracks, 0 );
-  if ( hasInfo(LHCb::RecSummary::nUpstreamTracks) )
-    s << " nUpstreamTracks = " << info( LHCb::RecSummary::nUpstreamTracks, 0 );
-  if ( hasInfo(LHCb::RecSummary::nVeloTracks) )
-    s << " nVeloTracks = " << info( LHCb::RecSummary::nVeloTracks, 0 );
-  if ( hasInfo(LHCb::RecSummary::nBackTracks) )
-    s << " nBackTracks = " << info( LHCb::RecSummary::nBackTracks, 0 );
-  if ( hasInfo(LHCb::RecSummary::nTTracks) )
-    s << " nTTracks = " << info( LHCb::RecSummary::nTTracks, 0 );
+  s << " nLongTracks = " << info( LHCb::RecSummary::nLongTracks, 0 );
+  s << " nDownstreamTracks = " << info( LHCb::RecSummary::nDownstreamTracks, 0 );
+  s << " nUpstreamTracks = " << info( LHCb::RecSummary::nUpstreamTracks, 0 );
+  s << " nVeloTracks = " << info( LHCb::RecSummary::nVeloTracks, 0 );
+  s << " nBackTracks = " << info( LHCb::RecSummary::nBackTracks, 0 );
+  s << " nTTracks = " << info( LHCb::RecSummary::nTTracks, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nRich1Hits) )
-    s << " nRich1Hits = " << info( LHCb::RecSummary::nRich1Hits, 0 );
-  if ( hasInfo(LHCb::RecSummary::nRich2Hits) )
-    s << " nRich2Hits = " << info( LHCb::RecSummary::nRich2Hits, 0 );
+  s << " nRich1Hits = " << info( LHCb::RecSummary::nRich1Hits, 0 );
+  s << " nRich2Hits = " << info( LHCb::RecSummary::nRich2Hits, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nVeloClusters) )
-    s << " nVeloClusters = " << info( LHCb::RecSummary::nVeloClusters, 0 );
+  s << " nVeloClusters = " << info( LHCb::RecSummary::nVeloClusters, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nITClusters) )
-    s << " nITClusters = " << info( LHCb::RecSummary::nITClusters, 0 );
+  s << " nITClusters = " << info( LHCb::RecSummary::nITClusters, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nOTClusters) )
-    s << " nOTClusters = " << info( LHCb::RecSummary::nOTClusters, 0 );
+  s << " nOTClusters = " << info( LHCb::RecSummary::nOTClusters, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nSPDhits) )
-    s << " nSPDhits = " << info( LHCb::RecSummary::nSPDhits, 0 );
+  s << " nSPDhits = " << info( LHCb::RecSummary::nSPDhits, 0 );
 
-  if ( hasInfo(LHCb::RecSummary::nMuonCoordsS0) )
-    s << " nMuonCoordsS0 = " << info( LHCb::RecSummary::nMuonCoordsS0, 0 );
-  if ( hasInfo(LHCb::RecSummary::nMuonCoordsS1) )
-    s << " nMuonCoordsS1 = " << info( LHCb::RecSummary::nMuonCoordsS1, 0 );
-  if ( hasInfo(LHCb::RecSummary::nMuonCoordsS2) )
-    s << " nMuonCoordsS2 = " << info( LHCb::RecSummary::nMuonCoordsS2, 0 );
-  if ( hasInfo(LHCb::RecSummary::nMuonCoordsS3) )
-    s << " nMuonCoordsS3 = " << info( LHCb::RecSummary::nMuonCoordsS3, 0 );
-  if ( hasInfo(LHCb::RecSummary::nMuonCoordsS4) )
-    s << " nMuonCoordsS4 = " << info( LHCb::RecSummary::nMuonCoordsS4, 0 );
-  if ( hasInfo(LHCb::RecSummary::nMuonTracks) )
-    s << " nMuonTracks = "   << info( LHCb::RecSummary::nMuonTracks, 0 );
+  s << " nMuonCoordsS0 = " << info( LHCb::RecSummary::nMuonCoordsS0, 0 );
+  s << " nMuonCoordsS1 = " << info( LHCb::RecSummary::nMuonCoordsS1, 0 );
+  s << " nMuonCoordsS2 = " << info( LHCb::RecSummary::nMuonCoordsS2, 0 );
+  s << " nMuonCoordsS3 = " << info( LHCb::RecSummary::nMuonCoordsS3, 0 );
+  s << " nMuonCoordsS4 = " << info( LHCb::RecSummary::nMuonCoordsS4, 0 );
+  s << " nMuonTracks = "   << info( LHCb::RecSummary::nMuonTracks, 0 );
 
   return s << " }";
 }
