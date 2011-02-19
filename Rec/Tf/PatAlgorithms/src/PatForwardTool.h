f421eeaba93f4324166b6a02e3cd1e4dc9b154e3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-02-19 18:54:53 +0000

    Fix coverity performance warning in PatForwardTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@118220 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 97bd9d286d8acb5443d7fca3fb3617a8e78a357f

--- Rec/Tf/PatAlgorithms/src/PatForwardTool.h
+++ Rec/Tf/PatAlgorithms/src/PatForwardTool.h
@@ -26,128 +26,129 @@
 static const InterfaceID IID_PatForwardTool ( "PatForwardTool", 1, 0 );
 
 
-  /** @class PatForwardTool PatForwardTool.h
-   *  Tool to extend one Velo (VeloTT) track through the magnet
-   *
-   *  @author Olivier Callot
-   *  @date   2005-10-04 Initial version
-   *  @date   2007-08-20 Update for A-Team framework
-   */
+/** @class PatForwardTool PatForwardTool.h
+ *  Tool to extend one Velo (VeloTT) track through the magnet
+ *
+ *  @author Olivier Callot
+ *  @date   2005-10-04 Initial version
+ *  @date   2007-08-20 Update for A-Team framework
+ */
 
-  class PatForwardTool : public GaudiTool, virtual public IPatForwardTool,
-                         virtual public ITracksFromTrack{
-  public:
+class PatForwardTool : public GaudiTool, virtual public IPatForwardTool,
+                       virtual public ITracksFromTrack{
+public:
 
-    // Return the interface ID
-    static const InterfaceID& interfaceID() { return IID_PatForwardTool; }
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_PatForwardTool; }
 
-    /// Standard constructor
-    PatForwardTool( const std::string& type,
-                    const std::string& name,
-                    const IInterface* parent);
+  /// Standard constructor
+  PatForwardTool( const std::string& type,
+                  const std::string& name,
+                  const IInterface* parent);
 
-    virtual ~PatForwardTool( ); ///< Destructor
+  virtual ~PatForwardTool( ); ///< Destructor
 
-    virtual void forwardTrack( const LHCb::Track* track, LHCb::Tracks* output );
+  virtual void forwardTrack( const LHCb::Track* track, LHCb::Tracks* output );
 
-    virtual StatusCode tracksFromTrack( const LHCb::Track& seed,
-                                        std::vector<LHCb::Track*>& tracks );
+  virtual StatusCode tracksFromTrack( const LHCb::Track& seed,
+                                      std::vector<LHCb::Track*>& tracks );
 
-    virtual StatusCode initialize();
+  virtual StatusCode initialize();
 
-    // added for NNTools
-    void setNNSwitch( bool nnSwitch)  { m_nnSwitch = nnSwitch;}
-    bool nnSwitch()       const       { return m_nnSwitch;}
+  // added for NNTools
+  void setNNSwitch( bool nnSwitch)  { m_nnSwitch = nnSwitch;}
+  bool nnSwitch()       const       { return m_nnSwitch;}
 
-    struct sortQuality{
-      bool operator()( PatFwdTrackCandidate first, PatFwdTrackCandidate second)
-      {
-	bool sortDecision = ( first.quality() < second.quality());
-	if(first.quality() == second.quality()){
-	  sortDecision = ( first.chi2PerDoF() < second.chi2PerDoF());
-	  if(first.chi2PerDoF() == second.chi2PerDoF()){
-	    sortDecision = ( first.qOverP() < second.qOverP());
-	  }
-	}
-	return sortDecision;
+  struct sortQuality{
+    bool operator()( const PatFwdTrackCandidate& first, 
+                     const PatFwdTrackCandidate& second )
+    {
+      bool sortDecision = ( first.quality() < second.quality());
+      if(first.quality() == second.quality()){
+        sortDecision = ( first.chi2PerDoF() < second.chi2PerDoF());
+        if(first.chi2PerDoF() == second.chi2PerDoF()){
+          sortDecision = ( first.qOverP() < second.qOverP());
+        }
       }
-    };
-
-
-  protected:
-
-    void buildXCandidatesList( PatFwdTrackCandidate& track );
-
-    void fillXList( PatFwdTrackCandidate& track, double xMin, double xMax );
-
-    bool fillStereoList( PatFwdTrackCandidate& track, double tol );
-
-    void debugFwdHits( PatFwdTrackCandidate& track );
-
-    void debugFwdHits( PatFwdTrackCandidate& track, MsgStream& msg );
-
-  private:
-
-    PatFwdTool*                                 m_fwdTool;        ///< Tool to compute extrapolations of tracks
-    Tf::TStationHitManager <PatForwardHit> *   m_tHitManager;    ///< Tool to provide hits
-    IAddTTClusterTool*                          m_addTTClusterTool;
-    std::string                                 m_addTtToolName;
-
-      
-
-    //== Parameters of the algorithm
-    bool   m_secondLoop;
-    double m_zAfterVelo;
-    double m_yCompatibleTol;
-    double m_yCompatibleTolFinal;
-    double m_minOTDrift;
-    double m_maxOTDrift;
-    double m_maxSpreadX;
-    double m_maxSpreadY;
-    double m_maxSpreadSlopeX;
-    double m_maxSpreadSlopeY;
-    int    m_minXPlanes;
-    int    m_minPlanes;
-    double m_minPt;
-    double m_minMomentum;
-    double m_maxChi2;
-    double m_maxChi2Track;
-    int    m_minHits;
-    int    m_minOTHits;
-    double m_centerOTYSize;
-    double m_maxDeltaY;
-    double m_maxDeltaYSlope;
-    int    m_maxXCandidateSize;
-
-    double m_rangePerMeV;
-    double m_minRange;
-    double m_rangeErrorFraction;
-
-
-    // setting the cov matrix of the state
-    double m_stateErrorX2;
-    double m_stateErrorY2;
-    double m_stateErrorTX2;
-    double m_stateErrorTY2;
-    double m_stateErrorP;
-
-    PatFwdHits  m_xHitsAtReference;
-    PatFwdHits  m_liste;
-    PatFwdHits  m_liste1;
-    PatFwdHits  m_liste2;
-
-    std::vector<PatFwdTrackCandidate> m_candidates;
-
-    bool   m_withoutBField;
-
-    static const unsigned int m_nSta = Tf::RegionID::OTIndex::kNStations;
-    static const unsigned int m_nLay = Tf::RegionID::OTIndex::kNLayers;
-    static const unsigned int m_nReg = Tf::RegionID::OTIndex::kNRegions +
-    Tf::RegionID::ITIndex::kNRegions;
-    static const unsigned int m_nOTReg = Tf::RegionID::OTIndex::kNRegions;
-    static const unsigned int m_nITReg = Tf::RegionID::ITIndex::kNRegions;
-
-    bool   m_nnSwitch;                   // switch on or off NN var. writing
+      return sortDecision;
+    }
   };
+
+
+protected:
+
+  void buildXCandidatesList( PatFwdTrackCandidate& track );
+
+  void fillXList( PatFwdTrackCandidate& track, double xMin, double xMax );
+
+  bool fillStereoList( PatFwdTrackCandidate& track, double tol );
+
+  void debugFwdHits( PatFwdTrackCandidate& track );
+
+  void debugFwdHits( PatFwdTrackCandidate& track, MsgStream& msg );
+
+private:
+
+  PatFwdTool*                                 m_fwdTool;        ///< Tool to compute extrapolations of tracks
+  Tf::TStationHitManager <PatForwardHit> *   m_tHitManager;    ///< Tool to provide hits
+  IAddTTClusterTool*                          m_addTTClusterTool;
+  std::string                                 m_addTtToolName;
+
+
+
+  //== Parameters of the algorithm
+  bool   m_secondLoop;
+  double m_zAfterVelo;
+  double m_yCompatibleTol;
+  double m_yCompatibleTolFinal;
+  double m_minOTDrift;
+  double m_maxOTDrift;
+  double m_maxSpreadX;
+  double m_maxSpreadY;
+  double m_maxSpreadSlopeX;
+  double m_maxSpreadSlopeY;
+  int    m_minXPlanes;
+  int    m_minPlanes;
+  double m_minPt;
+  double m_minMomentum;
+  double m_maxChi2;
+  double m_maxChi2Track;
+  int    m_minHits;
+  int    m_minOTHits;
+  double m_centerOTYSize;
+  double m_maxDeltaY;
+  double m_maxDeltaYSlope;
+  int    m_maxXCandidateSize;
+
+  double m_rangePerMeV;
+  double m_minRange;
+  double m_rangeErrorFraction;
+
+
+  // setting the cov matrix of the state
+  double m_stateErrorX2;
+  double m_stateErrorY2;
+  double m_stateErrorTX2;
+  double m_stateErrorTY2;
+  double m_stateErrorP;
+
+  PatFwdHits  m_xHitsAtReference;
+  PatFwdHits  m_liste;
+  PatFwdHits  m_liste1;
+  PatFwdHits  m_liste2;
+
+  std::vector<PatFwdTrackCandidate> m_candidates;
+
+  bool   m_withoutBField;
+
+  static const unsigned int m_nSta = Tf::RegionID::OTIndex::kNStations;
+  static const unsigned int m_nLay = Tf::RegionID::OTIndex::kNLayers;
+  static const unsigned int m_nReg = Tf::RegionID::OTIndex::kNRegions +
+    Tf::RegionID::ITIndex::kNRegions;
+  static const unsigned int m_nOTReg = Tf::RegionID::OTIndex::kNRegions;
+  static const unsigned int m_nITReg = Tf::RegionID::ITIndex::kNRegions;
+
+  bool   m_nnSwitch;                   // switch on or off NN var. writing
+};
 
 #endif // PATFORWARDTOOL_H
