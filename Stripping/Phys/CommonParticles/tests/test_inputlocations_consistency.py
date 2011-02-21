0b506a5b823a1f4ebab3a52e91862e579c34e75a
jpalac <jpalac@cern.ch>
2011-02-21 11:27:34 +0000

    - Add QM test testing that all InputLocations are among CommonParticles    output locations.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118353 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 38f396858355f73fd5a3b7eb8da6461ae2deebfa

---
+++ Stripping/Phys/CommonParticles/tests/test_inputlocations_consistency.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#$Id: test_load_modules.py,v 1.1 2010-04-22 14:44:53 jpalac Exp $
+'''
+Test loading various CommonParticles in the Analysis environment. Needs to be extended.
+'''
+__author__ = 'Juan Palacios juan.palacios@nikhef.nl'
+
+
+import sys
+sys.path.append('../python')
+
+from CommonParticles import StandardBasic, StandardIntermediate, StdLooseKs
+
+outputLocations = list(StandardBasic.locations.keys()) + list(StandardIntermediate.locations.keys())
+
+algs = list(StandardBasic.locations.values()) + list(StandardIntermediate.locations.values())
+
+errors = {}
+
+message = ""
+
+for alg in algs :
+    locs = alg.InputLocations
+    for loc in locs :
+        if not loc in outputLocations :
+            message+= "ERROR: Algorithm "+alg.name()+".InputLocation "+loc+" not in output locations\n"
+            errors[alg.name()]=loc
+
+
+if len(errors)>0 :
+    sys.stderr.write('test_inputlocations_consistency: FAIL\n')
+    sys.stderr.write(message)
+else :
+    sys.stdout.write('test_inputlocations_consistency: PASS\n')
