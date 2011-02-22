4030b75b4ef00b24dec19e06186f4bb1705f5d94
Beat Jost <beat.jost@cern.ch>
2011-02-22 10:42:28 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@118436 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 63fc5ab8258c241de070a7029893652a67b161db

---
+++ Online/Online/Gaucho/src/CounterAdder.cpp
@@ -0,0 +1,651 @@
+#include "Gaucho/CounterAdder.h"
+#include "Gaucho/ObjService.h"
+#include "Gaucho/MonServer.h"
+#include "Gaucho/ObjRPC.h"
+#include "Gaucho/MonHist.h"
+#include "Gaucho/RootHists.h"
+#include "Gaucho/RateService.h"
+#include "dis.hxx"
+#include "TDirectory.h"
+#include <vector>
+#include "string.h"
+#include "Gaucho/Utilities.h"
+static int mpty;
+typedef std::pair<std::string, void*> MonPair;
+
+
+//DimInfo *CounterAdder::gg_DNSInfo =0;
+//std::vector<MonAdder*> CounterAdder::gg_AdderList;
+//HAdderTaskInfoHandler CounterAdder::gg_TaskHandler;
+//HAdderServInfoHandler CounterAdder::gg_ServHandler;
+
+
+
+extern "C"
+{
+  void BufferAdder(void *tis, void *buff, int siz, MonInfo *h);
+}
+CounterAdder::CounterAdder(char * taskname, char *myName, char *serviceName)
+{
+  std::string svc_prefix("/Histos/");
+  m_taskname = std::string(taskname);
+  m_buffersize = 0;
+  m_buffer = 0;
+  m_usedSize = 0;
+  m_reference =-1;
+  m_serviceName = svc_prefix +std::string(serviceName);
+  m_expandRate = false;
+  m_srcnode = "";
+  m_outdns = "localhost";
+  m_MyName = myName;
+  m_taskname = taskname;
+  m_lockid = 0;
+  m_maplock = 0;
+  m_IsEOR = false;
+//  g_DNSInfo = 0;
+//  m_TaskHandler = new HAdderTaskInfoHandler();
+//  m_ServHandler = new HAdderServInfoHandler();
+  m_oldProf = 0;
+  m_added = 0;
+  m_noRPC = false;
+  gg_AdderList.push_back(this);
+  m_type = ADD_COUNTER;
+}
+CounterAdder::~CounterAdder()
+{
+  if (gg_DNSInfo != 0)
+  {
+    delete gg_DNSInfo;
+    gg_DNSInfo = 0;
+  }
+  for (TaskServIter i = m_TaskServiceMap.begin();i!= m_TaskServiceMap.end();i++)
+  {
+    delete i->second->m_diminfo;
+    delete i->second;
+  }
+  m_TaskServiceMap.clear();
+  for (INServIter i = m_inputServicemap.begin();i!= m_inputServicemap.end();i++)
+  {
+    delete i->second->m_Info;
+    delete i->second;
+  }
+  m_inputServicemap.clear();
+
+  if (m_outservice  != 0)
+  {
+    delete m_outservice;
+  }
+  if (m_ser !=0 )
+  {
+    delete m_ser;
+  }
+  if (m_rpc !=0 )
+  {
+    delete m_rpc;
+  }
+
+}
+void CounterAdder::Configure()
+{
+  std::string nodename;
+  nodename = RTL::nodeNameShort();
+  if (nodename != "")
+  {
+    toLowerCase(nodename);
+    m_name= std::string("MON_");
+    //if (strlen(myName) != 0)
+    {
+      m_name = m_name + std::string(m_MyName);//+ std::string("_");
+    }
+    //else
+    //{
+    //  m_name = m_name + std::string("_");
+    //}
+//    std::string tsknam = m_taskname.substr(m_taskname.find("_")+1);
+//    m_name = m_name + tsknam+ std::string("_0");
+  }
+  if (m_srcnode == "")
+  {
+    if (nodename.find("hlt"))
+    {
+      std::string rackrow = nodename.substr(3,1); //size of "hlt"
+      std::string rack_node = nodename.substr(4);
+      if (rack_node.size() == 4)
+      {
+        m_srcnode = nodename;
+      }
+      else if (rack_node.size() == 2 )
+      {
+        m_srcnode = nodename;
+      }
+      else
+      {
+        m_srcnode = nodename.substr(0,3);
+      }
+    }
+  }
+  m_serviceexp = boost::regex(m_servicePattern.c_str(),boost::regex_constants::icase);
+  m_taskexp = boost::regex(m_taskPattern.c_str(),boost::regex_constants::icase);
+  if (gg_DNSInfo ==0) gg_DNSInfo = new DimInfo((char*)"DIS_DNS/SERVER_LIST",(char*)"DEAD", &gg_TaskHandler);
+//  DimServer::setDnsNode(m_outdns.c_str());
+  m_outsvcname = m_name+m_serviceName;
+  lib_rtl_create_lock(0,&m_maplock);
+//  printf("================Out Service Name %s %s %s\n",m_name.c_str(),m_taskname.c_str(),m_outsvcname.c_str());
+//  m_outservice = new ObjService(m_outsvcname.c_str(),(char*)"C",(void*)&mpty, 4, &m_buffer, &m_usedSize);
+  std::string nam = m_name+std::string("/Histos/HistCommand");
+  m_ser = new AddSerializer((ObjMap*)&m_hmap);
+  m_rpc = 0;
+  if (!m_noRPC)
+  {
+    m_rpc = new ObjRPC(m_ser,(char*)nam.c_str(), (char*)"I:1;C",(char*)"C", this->m_maplock, 0/*this->m_lockid*/);
+  }
+}
+void CounterAdder::ServiceHandler(DimInfo *myInfo, char *input,int )
+{
+  if (strcmp(input,"DEAD") == 0)
+  {
+    return;
+  }
+//  printf("New Message from Service List Service %s data:\n%s\n",myInfo->getName(),input);
+  bool Newe;
+  Newe = false;
+  char *sinput=input;
+  if ((input[0] == '+') || (input[0] == '-') || (input[0] == '!'))
+  {
+    Newe = (input[0] == '+');
+    sinput = &input[1];
+  }
+  else
+  {
+    Newe = true;
+  }
+  dyn_string *service_list = Strsplit(sinput,(char*)"\n");
+  for (unsigned int j=0;j<service_list->size();j++)
+  {
+    std::string service_line(service_list->at(j));
+    dyn_string *service = Strsplit((char*)service_line.c_str(),(char*)"|");
+    bool status;
+//    printf("comparing %s to pattern %s\n",service->at(0).c_str(),this->m_servicePattern.c_str());
+    status = boost::regex_search(service->at(0),m_serviceexp);
+    if (status)
+    {
+      INServIter i;
+      i = m_inputServicemap.find(service->at(0));
+      if (Newe)
+      {
+        if (i == m_inputServicemap.end())
+        {
+//          servc = svc->at(0);
+          TaskServIter k;
+          std::string taskn=std::string(myInfo->getName());
+          taskn = taskn.substr(0,taskn.find("/SERVICE_LIST"));
+          k = m_TaskServiceMap.find(taskn);
+          if (k != m_TaskServiceMap.end())
+          {
+            k->second->m_svcname = service->at(0);
+          }
+          INServiceDescr *sd = new INServiceDescr((char*)(service->at(0).c_str()),
+          new MonInfo((char*)(service->at(0).c_str()),&BufferAdder,this));
+//            printf("Service: %s\n",service->at(0).c_str());
+          m_inputServicemap.insert(INServicePair(service->at(0).c_str(),sd));
+//            printf("%s: Adding service @%s@\n",m_name.c_str(),service->at(0).c_str());
+          if (m_inputServicemap.size() == 1)
+          {
+//              printf ("First client for adding... Creating our output service...\n");
+            m_outservice = new ObjService(m_ser,m_outsvcname.c_str(),(char*)"C",(void*)&mpty, 4, &m_buffer, &m_usedSize);
+            DimServer::start();
+          }
+        }
+        else
+        {
+            printf("Duplicate service %s. DNS restart? \n",service->at(0).c_str());
+        }
+      }
+      else
+      {
+        if (i != m_inputServicemap.end())
+        {
+          TaskServIter k;
+          std::string taskn=std::string(myInfo->getName());
+          taskn = taskn.substr(0,taskn.find("/SERVICE_LIST"));
+          k = m_TaskServiceMap.find(taskn);
+          if (k != m_TaskServiceMap.end())
+          {
+            k->second->m_svcname = "";
+          }
+          INServiceDescr *sd = i->second;
+          delete sd->m_Info;
+          delete sd;
+          m_inputServicemap.erase(i);
+    //      printf("%s: Removing service %s\n",m_name.c_str(),servc.c_str());
+          if (m_inputServicemap.empty())
+          {
+//            printf ("No more client... Deleteing our output service...\n");
+            delete m_outservice;
+            m_outservice = 0;
+          }
+        }
+      }
+    }
+  }
+}
+void CounterAdder::TaskHandler(char *input, int )
+{
+//  char *input;
+//  int siz;
+//  input = (char*)m_DNSInfo->getData();
+//  siz = m_DNSInfo->getSize();
+  if (strcmp(input,"DEAD") == 0)
+  {
+//    printf("DNS Down!!!!\n");
+    return;
+  }
+//  printf("New Message from DNS %s\n",input);
+  bool Newe;
+  Newe = true;
+  char *sinput=input;
+  if ((input[0] == '+') || (input[0] == '-') || (input[0] == '!'))
+  {
+    Newe = (input[0] == '+');
+    sinput = &input[1];
+  }
+  dyn_string *server_list = Strsplit(sinput,(char*)"|");
+
+  dyn_string::iterator i;
+  for (i=server_list->begin();i!=server_list->end();i++)
+  {
+    std::string server = *i;
+    std::string tskname,tgen;
+    std::string servc;
+    TaskName(server,tskname,tgen);
+    if (tskname != "")
+    {
+      if (Newe)
+      {
+        TaskServIter i;
+        i = m_TaskServiceMap.find(tskname);
+        if (i == m_TaskServiceMap.end())
+        {
+          servc = tskname+std::string("/SERVICE_LIST");
+          DimInfo *slist = new DimInfo(servc.c_str(),(char *)"DEAD",&gg_ServHandler);
+          TaskSvcDescr *ts = new TaskSvcDescr(tskname,slist);
+          m_TaskServiceMap.insert(TaskServicePair(tskname,ts));
+//          printf("Listening to Task %s\n",tskname.c_str());
+        }
+        else
+        {
+//          printf("Duplicate Task %s. DNS restart? \n",tskname.c_str());
+        }
+      }
+      else
+      {
+        TaskServIter i;
+        i = m_TaskServiceMap.find(tskname);
+        if (i != m_TaskServiceMap.end())
+        {
+          std::string service;
+          INServIter k,l;
+          service = i->second->m_svcname;
+  //        printf("Task %s died. Removing service @%s@\n",tskname.c_str(),service.c_str());
+          k = m_inputServicemap.find(service);
+          if (k != m_inputServicemap.end())
+          {
+            delete k->second->m_Info;
+            delete k->second;
+            m_inputServicemap.erase(k);
+            if (m_inputServicemap.empty())
+            {
+  //            printf ("No more client... Deleteing our output service...\n");
+              delete m_outservice;
+              m_outservice = 0;
+            }
+          }
+          else
+          {
+          }
+          TaskSvcDescr *ts = i->second;
+          delete ts->m_diminfo;
+          delete ts;
+          m_TaskServiceMap.erase(i);
+        }
+        else
+        {
+  //        printf("Task %s not found in map\n",tskname.c_str());
+        }
+        INServIter k;
+        std::string srvc=tskname+m_serviceName;
+        k = m_inputServicemap.find(srvc);
+        if (k != m_inputServicemap.end())
+        {
+          INServiceDescr *is = k->second;
+          m_inputServicemap.erase(k);
+          delete is->m_Info;
+          delete is;
+          if (m_inputServicemap.empty())
+          {
+  //          printf ("No more client... Deleteing our output service...\n");
+            delete m_outservice;
+            m_outservice = 0;
+          }
+        }
+      }
+    }
+  }
+}
+
+/*
+
+ void HAdderServInfoHandler::infoHandler(void)
+{
+  char *input;
+  int siz;
+  input = (char*)this->itsService->getData();
+  siz = this->itsService->getSize();
+//  printf("Service Info Handler %s. Input :\n%s\n",this->itsService->getName(),input);
+  std::vector<MonAdder*>::iterator i;
+  for (i=MonAdder::gg_AdderList.begin();i!= MonAdder::gg_AdderList.end();i++)
+  {
+    CounterAdder *p;
+    p = (CounterAdder*)*i;
+    p->ServiceHandler(itsService,input,siz);
+  }
+//    m_Hadder->TaskHandler();
+//    m_Hadder->ServiceHandler(itsService);
+}
+
+void HAdderTaskInfoHandler::infoHandler(void)
+{
+  char *input;
+  int siz;
+  input = (char*)this->itsService->getData();
+//  printf("Server Info Handler. Input :%s\n",input);
+  siz = this->itsService->getSize();
+  std::vector<MonAdder*>::iterator i;
+  for (i=MonAdder::gg_AdderList.begin();i!=MonAdder::gg_AdderList.end();i++)
+  {
+    CounterAdder *p;
+    p = (CounterAdder*)*i;
+    p->TaskHandler(input,siz);
+  }
+//    m_Hadder->TaskHandler();
+}
+*/
+void CounterAdder::add(void *buff, int siz, MonInfo *h)
+{
+  void *p;
+  int n1d,n2d,nprof,nrate;
+  n1d=0;
+  n2d=0;
+  nprof=0;
+  nrate=0;
+  if (siz == 4)
+  {
+//    printf("No Link from %s. Update counts....\n",h->m_TargetService.c_str());
+    m_received++;
+    return;
+  }
+  if (((SerialHeader*)buff)->m_magic != SERIAL_MAGIC)
+  {
+    printf("========> [ERROR] Serial Magic Word Missing  from connection %s\n",h->m_TargetService.c_str());
+    m_received++;
+    return;
+  }
+  m_expected = m_inputServicemap.size();
+  long long cltime;
+  SerialHeader* header= ((SerialHeader*)buff);
+  cltime = header->ser_tim;
+//  printf("Received size %d Header size %d\n",siz,header->buffersize);
+  long long runno = ((SerialHeader*)buff)->run_number;
+  long long current;
+  if (m_IsEOR)
+  {
+    current = runno;
+  }
+  else
+  {
+    current = cltime;
+  }
+//  printf("%s Last update %lld Client Time %lld run number %lld %d %d\n",h->m_TargetService.c_str(),m_reference,cltime,runno,m_received,m_expected);
+  m_RateBuff = 0;
+  if (m_reference < current)
+  {
+    m_added++;
+    m_received++;// = 1;
+    if (m_isSaver)
+    {
+      if (m_received == 1)
+      {
+        Lock();
+      }
+    }
+    //printf ("New cycle %s... %d\n",h->m_TargetService.c_str(),m_received);
+    p = Allocate(siz);
+    m_reference = current;
+    memset(m_buffer,0,m_buffersize);
+    m_hmap.clear();
+    memcpy(p,buff,siz);
+    m_usedSize = siz;
+    void *bend = AddPtr(p,siz);
+    void *hstart = AddPtr(p,sizeof(SerialHeader));
+    DimHistbuff1 *pp = (DimHistbuff1*)hstart;
+    while (pp<bend)
+    {
+      char *nam = (char*)AddPtr(pp,pp->nameoff);
+//      printf("Histogram Name: %s\n",nam);
+      std::string nams =nam;
+      LockMap();
+      m_hmap.insert(MonPair(nams,pp));
+      UnLockMap();
+      pp=(DimHistbuff1*)AddPtr(pp,pp->reclen);
+    }
+  }
+  else if (m_reference == current)
+  {
+//    printf("Adding %s\n",h->m_TargetService.c_str());
+    MonMap hmap;
+    hmap.clear();
+    void *bend = AddPtr(buff,siz);
+    void *hstart = AddPtr(buff,sizeof(SerialHeader));
+    DimHistbuff1 *pp = (DimHistbuff1*)hstart;
+    while (pp<bend)
+    {
+      char *nam = (char*)AddPtr(pp,pp->nameoff);
+      hmap.insert(MonPair(std::string(nam),pp));
+      pp=(DimHistbuff1*)AddPtr(pp,pp->reclen);
+    }
+    for (MonIter i=hmap.begin();i!=hmap.end();i++)
+    {
+      MonIter j = m_hmap.find(i->first);
+      if (j!=m_hmap.end())
+      {
+        LockMap();
+        double *ps, *ph;
+        DimHistbuff1 *sumh = (DimHistbuff1*)(j->second);
+        DimHistbuff1 *srch = (DimHistbuff1*)(i->second);
+        ps = (double*)AddPtr(sumh,sumh->dataoff);
+        ph = (double*)AddPtr(srch,srch->dataoff);
+        switch(srch->type)
+        {
+        case C_INT:
+          {
+            *(int *)sumh += *(int*)srch;
+            break;
+          }
+        case C_LONGLONG:
+          {
+            *(long long*)sumh += *(long long*)srch;
+            break;
+          }
+        case C_FLOAT:
+          {
+            *(float*)sumh += *(float*)srch;
+            break;
+          }
+        case C_DOUBLE:
+          {
+            *(double*)sumh += *(double*)srch;
+            break;
+          }
+        default:
+          {
+            break;
+          }
+        }
+        UnLockMap();
+      }
+      else
+      {
+//        printf("New Histogram... re-allocating...\n");
+        int csiz = m_usedSize;
+        int hsiz;
+        DimHistbuff1 *srch = (DimHistbuff1*)(i->second);
+        void *p;
+        hsiz = srch->reclen;
+        LockMap();
+        p = ReAllocate(hsiz);
+        if (p!=0)
+        {
+          p = AddPtr(p,csiz);
+          memcpy(p,srch,hsiz);
+          m_usedSize += hsiz;
+          char *nam = (char*)AddPtr(srch,srch->nameoff);
+          m_hmap.insert(MonPair(std::string(nam),p));
+        }
+        UnLockMap();
+      }
+    }
+    m_received++;
+    m_added++;
+    //printf ("New Source %s... %d\n",h->m_TargetService.c_str(),m_received);
+  }
+  else
+  {
+//    printf("late update from %s\n expected %lli received %lli\n",h->m_TargetService.c_str(),m_reference,current);
+    m_received++;
+  }
+  if (m_received >= m_expected)
+  {
+//    printf("Finished one cycle. Updating our service... %d %d\n", m_received,m_expected);
+    if (m_isSaver)
+    {
+      UnLock();
+    }
+    m_received = 0;
+    //printf(" %d %d\n", m_received,m_expected);
+    if (m_outservice != 0)
+    {
+      m_outservice->Serialize();
+      m_outservice->Update();
+    }
+    if (m_expandRate)
+    {
+#define MAX_SERVICE_SIZE 128
+      std::string nnam;
+      if (m_RateBuff != 0)
+      {
+        MonHist h;
+//        MyTProfile *p;
+//        if (m_isSaver)
+//        {
+//          Lock();
+//        }
+//        p = (MyTProfile*)h.de_serialize(m_RateBuff,"RATE");
+//        if (m_isSaver)
+//        {
+//          UnLock();
+//        }
+//        printf("Array Size %d Block Size %d",p->fN,h.BlockSize());
+//        p->Print("range");
+        DimHistbuff1 *pdiff;
+
+        if (m_oldProf !=0)
+        {
+          int ndble = (m_RateBuff->nxbin+2);
+          pdiff = (DimHistbuff1*)malloc(m_RateBuff->reclen);
+          double *obincont  = (double*)AddPtr(AddPtr(m_oldProf,m_oldProf->dataoff),0*(m_oldProf->nxbin+2)*sizeof(double));
+          double *nbincont  = (double*)AddPtr(AddPtr(m_RateBuff,m_RateBuff->dataoff),0*(m_RateBuff->nxbin+2)*sizeof(double));
+          double *dbincont  = (double*)AddPtr(AddPtr(pdiff,m_RateBuff->dataoff),0*(m_RateBuff->nxbin+2)*sizeof(double));
+          double *ocont  = (double*)AddPtr(AddPtr(m_oldProf,m_oldProf->dataoff),1*(m_oldProf->nxbin+2)*sizeof(double));
+          double *ncont  = (double*)AddPtr(AddPtr(m_RateBuff,m_RateBuff->dataoff),1*(m_RateBuff->nxbin+2)*sizeof(double));
+          double *dcont  = (double*)AddPtr(AddPtr(pdiff,m_RateBuff->dataoff),1*(m_RateBuff->nxbin+2)*sizeof(double));
+          std::vector<char*> labvec;
+          labvec.clear();
+          labvec.push_back(0);
+          {
+            int i;
+            int nbin=m_oldProf->nxbin;
+            char *labs = (char*)AddPtr(m_oldProf,m_oldProf->xlaboff);
+            char *lab = labs;
+            for (i = 1; i < (nbin+1) ; ++i)
+            {
+              labvec.push_back(lab);
+              lab = (char*)AddPtr(lab,strlen(lab)+1);
+            }
+          }
+          for (int i = 9;i<ndble;i++)
+          {
+            dbincont[i] = nbincont[i]-obincont[i];
+            dcont[i] = ncont[i]-ocont[i];
+//            printf("%d\t%f = %f - %f\n\t%f = %f - %f\n",i,dbincont[i], nbincont[i], obincont[i],dcont[i], ncont[i], ocont[i]);
+          }
+          OUTServiceDescr *outs;
+          std::string nams;
+          int i;
+          int dim;
+          double rate;
+          double denom;
+//          printf("Time Bin %f\n",p->GetBinContent(8));
+//          denom  = p->GetBinContent(8)/1.0e09;
+//          denom = dbincont[8] == 0 ? 0 : dcont[8]/dbincont[8]/1.0e09;
+          denom = ncont[8]/m_added/1.0e09;
+          dim = m_RateBuff->nxbin;
+          std::string sname;
+          for (i=9;i<dim;i++)
+          {
+            nams  = labvec[i];
+            outs = findOUTService(nams);
+            RateService *s;
+            if (outs != 0)
+            {
+              s = (RateService*)outs->m_svc;
+            }
+            else
+            {
+              DimServer::autoStartOn();
+              sname = m_NamePrefix+nams;
+              if (sname.size() < MAX_SERVICE_SIZE)
+              {
+                s= new RateService(sname.c_str(),(char*)"D:2;C");
+                outs = new OUTServiceDescr((char*)nams.c_str(),(void*)s);
+                this->m_outputServicemap.insert(OUTServicePair(nams,outs));
+              }
+              else
+              {
+                continue;
+              }
+            }
+//            rate = dbincont[i] == 0 ? 0 : dcont[i]/dbincont[i]/denom;
+            double c = dcont[i];
+            rate = c/denom;
+//            printf("%d %d %s %f %f\n",dim,i,nams.c_str(), pdiff->GetBinContent(i),rate);
+            s->m_data.d1 = rate;
+            s->m_data.d2 = c;
+            int upsiz = 2*sizeof(s->m_data.d1)+strlen(s->m_data.c);
+            s->Updater(upsiz);
+          }
+          delete pdiff;
+        }
+        else
+        {
+          m_oldProf = (DimHistbuff1*)malloc(m_RateBuff->reclen);
+        }
+      }
+      m_added = 0;
+      memcpy(m_oldProf,m_RateBuff,m_RateBuff->reclen);
+    }
+    if (CycleFn!= 0)
+    {
+      (*CycleFn)(CycleCBarg, m_buffer, m_buffersize, &m_hmap, this);
+    }
+  }
+}
