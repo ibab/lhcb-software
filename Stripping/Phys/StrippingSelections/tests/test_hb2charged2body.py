f3a6aec83c34519a65393b425bde8e305ff97e13
jpalac <jpalac@cern.ch>
2011-02-23 19:44:34 +0000

    New test, failing because dictionary doesn't match builder.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118773 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 592362c7892fa73b1bb4316bc4b7164aceb3d1fa

---
+++ Stripping/Phys/StrippingSelections/tests/test_hb2charged2body.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from StrippingUtils import LineBuilderTests
+from StrippingSelections.StrippingHb2Charged2Body import Hb2Charged2BodyLines as builder
+from StrippingSelections.StrippingHb2Charged2Body import configHb2hh as config_params
+
+config_params = { 'PrescaleB2Charged2Body'   : 1,
+                  'PrescaleB2PPbar'          : 1,
+                  'MinPTB2Charged2Body'      : 1100,
+                  'MinPTB2PPbar'             : 900,
+                  'MinIPB2Charged2Body'      : 0.15,
+                  'MinIPChi2B2Charged2Body'  : 100,
+                  'MinIPChi2B2PPbar'         : 9,
+                  'TrChi2'                   : 5,
+                  'PIDppi'                   : 0,
+                  'PIDpk'                    : -2,
+                  'MaxPTB2Charged2Body'      : 2700,
+                  'MaxPTB2PPbar'             : 2500,
+                  'MaxIPB2Charged2Body'      : 0.3,
+                  'MaxIPChi2B2Charged2Body'  : 200,
+                  'CombMassLow'              : 4500,
+                  'CombMassHigh'             : 6100,
+                  'CombMassWindow'           : 200,
+                  'DOCA'                     : 0.08,
+                  'VertexChi2B2PPbar'        : 16,
+                  'BPT'                      : 1200,
+                  'BIP'                      : 0.06,
+                  'BIPChi2B2Charged2Body'    : 12,
+                  'BIPChi2B2PPbar'           : 36,
+                  'BDIRA'                    : 0.9995,
+                  'BTAU'                     : 0.0009,
+                  'MassLow'                  : 4800,
+                  'MassHigh'                 : 5800
+                  }
+
+
+def test_line_builder() :
+    LineBuilderTests.test_line_builder(builder, config_params)
+
+if __name__ == '__main__' :
+    
+    test_line_builder()
