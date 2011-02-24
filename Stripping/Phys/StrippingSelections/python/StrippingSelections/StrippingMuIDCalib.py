d01a54c8c8d44022ccd333b3417601d33daa344f
Diego Milanes Carreno <diego.milanes@lpnhe.in2p3.fr>
2011-02-24 12:08:52 +0000

    Update to requirements for stripping13. Also line for Jpsi detached noMIP with an associated K added
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118814 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1e466a2e93a58088f49c3d85c4ba24a6fc2a1fed

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingMuIDCalib.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingMuIDCalib.py
@@ -1,114 +1,163 @@
 ## #####################################################################
 # A stripping selection for muon identification calibration trough
-# the study of J/psi(1S) -> mu+ mu- and Lambda to p pi decays
-#
-# @authors S.Furcas, G.Lanfranchi, M.Palutan, A.Sarti 
-# @date 2009-July-30
-# 
+# the study of J/psi(1S) -> mu+ mu-
 ## #####################################################################
+
+__author__  = [ 'S.Furcas', 'G.Lanfranchi', 'M.Palutan', 'A.Sarti', 'MuID Team' ]
+__date__    = '02/14/2011'
+__version__ = '$Revision: 1.2 $'
+__all__     = ( 'MuIDCalibConf',
+                'makePromptSelection',
+                'makeDetachedSelection',
+                'makeDetachedNoMIPSelection', 
+                'makeDetachedNoMIPKSelection'
+                )
+
 from Gaudi.Configuration import *
-from Configurables import GaudiSequencer, CombineParticles, FilterDesktop
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
 from StrippingConf.StrippingLine import StrippingLine, StrippingMember
+from PhysSelPython.Wrappers import DataOnDemand, Selection, SelectionSequence, MergedSelection
+from Configurables import LoKi__VoidFilter as VoidFilter
+from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
+from StrippingUtils.Utils import LineBuilder
+from StandardParticles import StdNoPIDsKaons, StdNoPIDsMuons
+
+name = 'MuIDCalib'
+class MuIDCalibConf( LineBuilder ):
+    __configuration_keys__ = ('PromptPrescale',       #0.08
+                              'DetachedPrescale',     #1.
+                              'DetachedNoMIPPrescale', #0.25
+                              'DetachedNoMIPKPrescale', #1.
+                              )
+
+    def __init__( self, name, config ) :
+        LineBuilder.__init__( self, name, config )
 
 
-# Create Jpsi -> mumu candidates out of no pid muons
-## ############################################################
-#Muons common cut
-mucocut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<5) & (ISLONG)'
-#Tag and probe cuts: 
-#   TAG:: IsMuon and P>6Gev and Pt>1.5 GeV
-#   PROBE:: Ecal (-10,1000) and Hcal (1000,4000) energy filtered
+        self.selStdNoPIDMuons = StdNoPIDsMuons #DataOnDemand( Location = "Phys/StdNoPIDsMuons" )
+        self.selStdNoPIDKaons = StdNoPIDsKaons #DataOnDemand( Location = "Phys/StdNoPIDsKaons" )
 
-tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) "
-tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) "
-
-probe2cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),2)) ) "
-probe1cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),1)) )  "
-
-child1cuts = tag1cuts + " & " + probe2cuts
-
-child2cuts = tag2cuts + " & " + probe1cuts
-
-JpsiCombine = StrippingMember( CombineParticles
-                               , 'Combine'
-                               , InputLocations = [ 'StdNoPIDsMuons' ]
-                               , DecayDescriptor = 'J/psi(1S) -> mu+ mu-'
-                               , DaughtersCuts = { 'mu+' : mucocut , 
-                                                   'mu-' : mucocut }
-                               , CombinationCut = "(ADAMASS('J/psi(1S)')<200*MeV)"
-                               , MotherCut = "(VFASPF(VCHI2/VDOF)<10) & ( " + child1cuts + " | " + child2cuts + " ) "
-                               )
-
-# Define the line
-## ############################################################
-jp_line = StrippingLine('JpsiNoPID'
-                        , prescale = 0.1
-                        , algos = [ JpsiCombine ]
-                        )
+        #GhostLines to isolate cpu time
+        #self.line_mu = StrippingLine( 'Mu', prescale = 1, selection = self.selStdNoPIDMuons ) 
+        #self.line_K  = StrippingLine( 'K' , prescale = 1, selection = self.selStdNoPIDKaons ) 
 
 
-# Create b -> Jpsi -> mumu candidates out of no pid muons
-## ############################################################
-#Muons common cut
-mucocut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<3) & (ISLONG) &(MIPDV(PRIMARY)>0.050)'
-#Tag and probe cuts: 
-#   TAG:: IsMuon and P>6Gev and Pt>1.5 GeV
-#   PROBE:: Ecal (-10,1000) and Hcal (1000,4000) energy filtered
-tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) & (CHILDCUT((MIPDV(PRIMARY)>0.12),1)) "
-tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) & (CHILDCUT((MIPDV(PRIMARY)>0.12),2)) "
+        self.sel_Prompt  = makePromptSelection( "Combine", self.selStdNoPIDMuons )
+        self.line_Prompt = StrippingLine( 'JpsiNoPID', prescale = config[ 'PromptPrescale' ], selection = self.sel_Prompt ) 
 
-probe2cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),2)) ) "
-probe1cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),1)) )  "
+        self.sel_Detached  = makeDetachedSelection( "FromBCombine", self.selStdNoPIDMuons )
+        self.line_Detached = StrippingLine( 'JpsiFromBNoPID', prescale = config[ 'DetachedPrescale' ], selection = self.sel_Detached ) 
 
-child1cuts = tag1cuts + " & " + probe2cuts
+        self.sel_DetachedNoMIP  = makeDetachedNoMIPSelection( "FromBNoMipCombine", self.selStdNoPIDMuons )
+        self.line_DetachedNoMIP = StrippingLine( 'JpsiFromBNoPIDNoMip', prescale = config[ 'DetachedNoMIPPrescale' ], selection = self.sel_DetachedNoMIP ) 
+      
+        self.sel_DetachedNoMIPK  = makeDetachedNoMIPKSelection( "FromBNoMipWithKCombine", self.sel_DetachedNoMIP, self.selStdNoPIDKaons )
+        self.line_DetachedNoMIPK = StrippingLine( 'JpsiKFromBNoPIDNoMip', prescale = config[ 'DetachedNoMIPKPrescale' ], selection = self.sel_DetachedNoMIPK ) 
+  
 
-child2cuts = tag2cuts + " & " + probe1cuts
+        #self.registerLine( self.line_mu ) #GhostLines
+        #self.registerLine( self.line_K )  #GhostLines
+
+        self.registerLine( self.line_Prompt )
+        self.registerLine( self.line_Detached )
+        self.registerLine( self.line_DetachedNoMIP )
+        self.registerLine( self.line_DetachedNoMIPK )
 
 
-JpsiFromBCombine = StrippingMember( CombineParticles
-                                    , 'FromBCombine'
-                                    , InputLocations = [ 'StdNoPIDsMuons' ]
-                                    , DecayDescriptor = 'J/psi(1S) -> mu+ mu-'
-                                    , DaughtersCuts = { 'mu+' : mucocut , 
-                                                        'mu-' : mucocut }
-                                    , CombinationCut = "(ADAMASS('J/psi(1S)')<200*MeV)"
-                                    , MotherCut = "(VFASPF(VCHI2/VDOF)<10) & (BPVVDCHI2 > 225) & ( ( " + child1cuts + " ) | (" 
-                                                                                  + child2cuts + " ) ) "
+
+
+'''
+Selection Makers
+'''
+def makePromptSelection( name, muons ):
+    '''
+    Create Jpsi -> mumu candidates out of no pid muons
+    TAG:: IsMuon and P>6Gev and Pt>1.5 GeV
+    PROBE:: Ecal (-10,1000) and Hcal (1000,4000) energy filtered
+    '''
+    mucocut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<3) & (ISLONG)'
+    tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) "
+    tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) "
+    probe2cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),2)) ) "
+    probe1cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),1)) )  "
+    child1cuts = tag1cuts + " & " + probe2cuts + " & " + probe1cuts
+    child2cuts = tag2cuts + " & " + probe1cuts + " & " + probe2cuts
+
+    combination = CombineParticles( DecayDescriptor = 'J/psi(1S) -> mu+ mu-',
+                                    DaughtersCuts = { 'mu+' : mucocut , 'mu-' : mucocut },
+                                    CombinationCut = "(ADAMASS('J/psi(1S)')<150*MeV)",
+                                    MotherCut = "(VFASPF(VCHI2/VDOF)<10) & ( " + child1cuts + " | " + child2cuts + " ) "
                                     )
+    return Selection( name, 
+                      Algorithm = combination,
+                      RequiredSelections = [muons] )
 
 
-# Define the line
-## ############################################################
-b_line = StrippingLine('JpsiFromBNoPID'
-                        , prescale = 1.
-                        , algos = [ JpsiFromBCombine ]
-                        )
+def makeDetachedSelection( name, muons ):
+    '''
+    Create b -> Jpsi -> mumu candidates out of no pid muons
+    TAG:: IsMuon and P>6Gev and Pt>1.5 GeV
+    PROBE:: Ecal (-10,1000) and Hcal (1000,4000) energy filtered
+    '''
+    mucocut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<3) & (ISLONG) &(MIPDV(PRIMARY)>0.050) & (MIPCHI2DV(PRIMARY)>25)'
+    tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) & (CHILDCUT((MIPDV(PRIMARY)>0.12),1)) "
+    tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) & (CHILDCUT((MIPDV(PRIMARY)>0.12),2)) "
+    probe2cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),2)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),2)) ) "
+    probe1cuts = " ( (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)<1000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)<4000*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloEcalE,-10000)>-10*MeV),1)) & (CHILDCUT((PPINFO(LHCb.ProtoParticle.CaloHcalE,-10000)>1000*MeV),1)) )  "
+    child1cuts = tag1cuts + " & " + probe2cuts 
+    child2cuts = tag2cuts + " & " + probe1cuts 
 
-
-# Create b -> Jpsi -> mumu candidates out of no pid muons without mip cuts
-## ############################################################
-#Muons common cut
-mucocut = '(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) & (P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<5) & (ISLONG) & (MIPCHI2DV(PRIMARY)>4)'
-#Tag and probe cuts: 
-#   TAG:: IsMuon and P>6Gev and Pt>1.5 GeV & IpChi2>10
-
-tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) & (CHILDCUT((MIPCHI2DV(PRIMARY)>10),1)) "
-tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) & (CHILDCUT((MIPCHI2DV(PRIMARY)>10),2)) "
-
-JpsiFromBNoMipCombine = StrippingMember( CombineParticles
-                                    , 'FromBNoMipCombine'
-                                    , InputLocations = [ 'StdNoPIDsMuons' ]
-                                    , DecayDescriptor = 'J/psi(1S) -> mu+ mu-'
-                                    , DaughtersCuts = { 'mu+' : mucocut ,
-                                                        'mu-' : mucocut }
-                                    , CombinationCut = "(ADAMASS('J/psi(1S)')<150*MeV)"
-                                    , MotherCut = "(VFASPF(VCHI2/VDOF)<8) & (BPVVDCHI2 > 225) & ( ( " + tag1cuts + " ) | ("
-                                                                                  + tag2cuts + " ) ) "
+    combination = CombineParticles( DecayDescriptor = 'J/psi(1S) -> mu+ mu-',
+                                    DaughtersCuts = { 'mu+' : mucocut , 'mu-' : mucocut },
+                                    CombinationCut = "(ADAMASS('J/psi(1S)')<150*MeV)",
+                                    MotherCut = "(VFASPF(VCHI2/VDOF)<10) & (BPVVDCHI2 > 225) & ( ( " + child1cuts + " ) | (" + child2cuts + " ) ) "
                                     )
-# Define the line
-## ############################################################
-bnomip_line = StrippingLine('JpsiFromBNoPIDNoMip'
-                        , prescale = 0.25
-                        , algos = [ JpsiFromBNoMipCombine ]
-                        )
+    return Selection( name, 
+                      Algorithm = combination,
+                      RequiredSelections = [muons] )
+
+
+def makeDetachedNoMIPSelection( name, muons ):
+    '''
+    Create b -> Jpsi -> mumu candidates out of no pid muons without mip cuts
+    TAG:: IsMuon and P>6Gev and Pt>1.5 GeV & IpChi2>10
+    '''
+    #(0.5<PPINFO(LHCb.ProtoParticle.InAccMuon,-1)) &
+    mucocut = '(P>3*GeV) & (PT>800*MeV) & (TRCHI2DOF<3) & (ISLONG) & (MIPCHI2DV(PRIMARY)>25)'
+    tag1cuts = " (CHILDCUT(ISMUON,1)) & (CHILDCUT((P>6*GeV),1)) & (CHILDCUT((PT>1.5*GeV),1)) & (CHILDCUT((MIPCHI2DV(PRIMARY)>10),1)) "
+    tag2cuts = " (CHILDCUT(ISMUON,2)) & (CHILDCUT((P>6*GeV),2)) & (CHILDCUT((PT>1.5*GeV),2)) & (CHILDCUT((MIPCHI2DV(PRIMARY)>10),2)) "
+
+    combination = CombineParticles( DecayDescriptor = 'J/psi(1S) -> mu+ mu-',
+                                    DaughtersCuts = { 'mu+' : mucocut , 'mu-' : mucocut },
+                                    CombinationCut = "(ADAMASS('J/psi(1S)')<200*MeV)",
+                                    MotherCut = "(VFASPF(VCHI2/VDOF)<8) & (BPVVDCHI2 > 225) & ( ( " + tag1cuts + " ) | (" + tag2cuts + " ) ) "
+                                    )
+    return Selection( name, 
+                      Algorithm = combination,
+                      RequiredSelections = [muons] )
+
+
+def makeDetachedNoMIPKSelection( name, jpsis, kaons ):
+    jpsi_cuts = "(VFASPF(VCHI2/VDOF)<15) &  (MIPCHI2DV(PRIMARY)>25)"
+    combination = CombineParticles( DecayDescriptor = '[B+ -> J/psi(1S) K+]cc',
+                                    DaughtersCuts = { "K+": "(MIPCHI2DV(PRIMARY)>25)" , 'J/psi(1S)' : jpsi_cuts },
+                                    CombinationCut = "ADAMASS('B+') < 500.*MeV",
+                                    MotherCut = "(BPVIPCHI2()<25)"
+                                    )
+    return Selection( name, 
+                      Algorithm = combination,
+                      RequiredSelections = [ jpsis, kaons ] )
+
+
+
+
+
+
+
+
+
+
+
+
+
 
