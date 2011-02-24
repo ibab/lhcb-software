848b4e5c22afd3d3c22642784f361e0b185ca5ac
Frederic Henry Blanc <fred.blanc@epfl.ch>
2011-02-24 22:13:28 +0000

    update StrippingB2CharmlessQuasi2Body to use LineBuilder, and tighten selection cuts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5d8416ad39c9588df38009f77143ea207ea1d433

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
@@ -1,88 +1,125 @@
 '''
-Module for construction of B->(pipi)(pipi) to use for quasi-two body
-B decays involving two light resonances, such as eta, etaprime, omega, Kstar, phi
+B->charmless quasi 2-body selection
 '''
 
 __author__ = ['Fred Blanc']
-__date__ = '25/10/2010'
-__version__ = '$Revision: 1.0 $'
+__date__ = '25/02/2011'
+__version__ = '$Revision: 2.0 $'
 
+__all__ = ( 'B2Quasi2Body4piConf',
+            'makeDiTrackList',
+            'makeB2Q2B4pi' )
 
-# Need __all__ for ???
-__all__ = ('Lines')
+config_params = {'Q2BPrescale'     : 1.,
+                 'Q2BResMinPT'     : 1500.,
+                 'Q2BResMinP'      : 10.,
+                 'Q2BResMaxMass'   : 900.,
+                 'Q2BResVtxChiDOF' : 4.,
+                 'Q2BBMinM'        : 2800.,
+                 'Q2BBMaxM'        : 5700.,
+                 'Q2BBVtxChi2DOF'  : 4.,
+                 'Q2BBMinTrkPT'    : 1500.,
+                 'Q2BBPVVDZ'       : 2. }
+
 
 from Gaudi.Configuration import *
-from Configurables import FilterDesktop, CombineParticles, OfflineVertexFitter
-from PhysSelPython.Wrappers import Selection, DataOnDemand, MergedSelection
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
+from PhysSelPython.Wrappers import Selection, DataOnDemand
 from StrippingConf.StrippingLine import StrippingLine
-from StrippingSelections.Utils import checkConfig
-from GaudiKernel.SystemOfUnits import MeV
+from StrippingUtils.Utils import LineBuilder
+from StandardParticles import StdNoPIDsPions
 
-##############################################################################
-## This is the common Jpsi that is used in all of the subsequent selections ##
-##############################################################################
-# a few functions that we will use. are there alternatives for this in Juan's framework?
-B2Quasi2Body4piLines = []
+name = "B2Quasi2Body4pi"
 
-# create a selection using a FilterDesktop
-def createSubSel( OutputList, InputList, Cuts ) :
-    # create a unique filterdesktop
-    filterName = "FilterDesktopFor" + OutputList
-    if allConfigurables.get( filterName ) :
-        raise ValueError, 'FilterDesktop instance with name '+filterName+' already exists'
-    filter = FilterDesktop(filterName, Code = Cuts)
-    return Selection( OutputList,
-                      Algorithm = filter,
-                      RequiredSelections = [ InputList ] )
+class B2Quasi2Body4piConf(LineBuilder) :
+    """
+    Builder for B2Quasi2Body4pi
+    """
 
-# create a selection using a ParticleCombiner
-def createCombinationSel( OutputList,
-                          DecayDescriptor,
-                          DaughterLists,
-                          DaughterCuts = {} ,
-                          PreVertexCuts = "ALL",
-                          PostVertexCuts = "ALL" ) :
-    combinerName = "CombinerParticlesFor" + OutputList
-    if allConfigurables.get( combinerName ) :
-        raise ValueError, 'CombineParticles instance with name '+ combinerName+' already exists'
-    combiner = CombineParticles( combinerName,
-                                 DecayDescriptor = DecayDescriptor,
-                                 DaughtersCuts = DaughterCuts,
-                                 MotherCut = PostVertexCuts,
-                                 CombinationCut = PreVertexCuts,
-                                 ReFitPVs = True)
-    return Selection ( OutputList,
-                       Algorithm = combiner,
-                       RequiredSelections = DaughterLists)
+    __configuration_keys__ = ( 'Q2BPrescale',
+                               'Q2BResMinPT',
+                               'Q2BResMinP',
+                               'Q2BResMaxMass',
+                               'Q2BResVtxChiDOF',
+                               'Q2BBMinM',
+                               'Q2BBMaxM',
+                               'Q2BBVtxChi2DOF',
+                               'Q2BBMinTrkPT',
+                               'Q2BBPVVDZ' )
 
+    def __init__(self, name, config) :
 
-# define input daughter lists
-TrackList = createSubSel( OutputList = "TracksForCharmlessB",
-                          InputList = DataOnDemand(Location = "Phys/StdNoPIDsPions"),
-                          Cuts = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 16) & (PT > 250*MeV)" )
+        LineBuilder.__init__(self, name, config)
 
-DiTrackList = createCombinationSel( OutputList = "DiTracksForCharmlessB",
-                                    DaughterLists = [ TrackList ],
-                                    DaughterCuts = { },
-                                    DecayDescriptor = "rho(770)0 -> pi+ pi-",
-                                    PreVertexCuts = "(APT>1000*MeV) & (AP>10*GeV) & (AM<1000*MeV)",
-                                    PostVertexCuts = "(VFASPF(VCHI2/VDOF) < 4)" )
+        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 25) & (PT > 250*MeV)")
+        self.TrackList = Selection( 'TrackList',
+                                    Algorithm = _trkFilter,
+                                    RequiredSelections = [StdNoPIDsPions])
 
+        self.DiTrackList = makeDiTrackList( name="DiTracksForCharmlessB",
+                                            trkList=self.TrackList,
+                                            MinPTCut = config['Q2BResMinPT'],
+                                            MinPCut = config['Q2BResMinP'],
+                                            MaxMassCut = config['Q2BResMaxMass'],
+                                            VtxChi2DOFCut = config['Q2BResVtxChiDOF'] )
 
-#######################
-### B->(pipi)(pipi) ###
-#######################
-B2Q2B = createCombinationSel( OutputList = "B2Q2B",
-                              DecayDescriptor = "B0 -> rho(770)0 rho(770)0",
-                              DaughterLists = [ DiTrackList ],
-                              DaughterCuts  = {},
-                              PreVertexCuts = "(APT>1000*MeV) & in_range(2500,AM,5700)",
-                              PostVertexCuts = "(VFASPF(VCHI2/VDOF) < 4)" )
+        B2Q2BName = name+"Selection"
+        self.B2CharmlessQ2B4pi = makeB2Q2B4pi( B2Q2BName,
+                                               diTrkList=self.DiTrackList,
+                                               MinMassCut = config['Q2BBMinM'],
+                                               MaxMassCut = config['Q2BBMaxM'],
+                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'],
+                                               MinTrkPTCut = config['Q2BBMinTrkPT'],
+                                               MinPVVDZCut = config['Q2BBPVVDZ'] )
 
-B2Quasi2Body4piLine = StrippingLine("B2Quasi2Body4piLine",
-                                    algos = [ createSubSel( InputList = B2Q2B,
-                                                            OutputList = B2Q2B.name() + "Prescaled",
-                                                            Cuts = "(MAXTREE('pi+'==ABSID, PT) > 1500*MeV)" ) ] ,
-                                    prescale = 0.2)
+        self.Q2B4piLine = StrippingLine( B2Q2BName+"Line",
+                                         prescale = config['Q2BPrescale'],
+                                         selection = self.B2CharmlessQ2B4pi )
 
-B2Quasi2Body4piLines += [ B2Quasi2Body4piLine ]
+        self.registerLine(self.Q2B4piLine)
+
+def makeDiTrackList( name,
+                     trkList,
+                     MinPTCut,
+                     MinPCut,
+                     MaxMassCut,
+                     VtxChi2DOFCut ) :
+    """
+    Di-track selection
+    """
+    _diTrackPreVertexCuts = "(APT> %(MinPTCut)s *MeV) & (AP> %(MinPCut)s *GeV) & (AM< %(MaxMassCut)s *MeV)" % locals()
+    _diTrackPostVertexCuts = "(VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s)" % locals()
+#    _diTrackPreVertexCuts = "(APT>1500*MeV) & (AP>10*GeV) & (AM<900*MeV)"
+#    _diTrackPostVertexCuts = "(VFASPF(VCHI2/VDOF) < 4)"
+
+    _combineDiTrack = CombineParticles( DecayDescriptor="rho(770)0 -> pi+ pi-",
+                                        CombinationCut = _diTrackPreVertexCuts,
+                                        MotherCut = _diTrackPostVertexCuts )
+
+    return Selection(name,
+                     Algorithm = _combineDiTrack,
+                     RequiredSelections = [ trkList ] )
+
+def makeB2Q2B4pi( name,
+                  diTrkList,
+                  MinMassCut,
+                  MaxMassCut,
+                  VtxChi2DOFCut,
+                  MinTrkPTCut,
+                  MinPVVDZCut ) :
+    """
+    Charmless Q2B selection
+    """
+
+    _B2Q2BPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" % locals()
+    _B2Q2BPostVertexCuts = "(VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s ) & (MAXTREE('pi+'==ABSID, PT) > %(MinTrkPTCut)s *MeV) & (BPVVDZ > %(MinPVVDZCut)s *mm)" % locals()
+#    _B2Q2BPreVertexCuts = "in_range(2800,AM,5700)"
+#    _B2Q2BPostVertexCuts = "(VFASPF(VCHI2/VDOF) < 4) & (MAXTREE('pi+'==ABSID, PT) > 1500*MeV) & (BPVVDZ > 2*mm)"
+
+    _combineB2Q2B = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
+                                      MotherCut = _B2Q2BPostVertexCuts,
+                                      CombinationCut = _B2Q2BPreVertexCuts )
+
+    return Selection(name,
+                     Algorithm = _combineB2Q2B,
+                     RequiredSelections = [ diTrkList ] )
