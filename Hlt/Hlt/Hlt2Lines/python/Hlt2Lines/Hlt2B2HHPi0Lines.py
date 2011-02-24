42156d0c36d7f27b6b09442756c38c737cf33f37
Olivier Deschamps <odescham@cern.ch>
2011-02-24 10:09:31 +0000

    update Hlt2B2HHPi0.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@118794 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 804b738431c1a577538f0bf87effca752e6d8e0a

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2HHPi0Lines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2HHPi0Lines.py
@@ -5,21 +5,26 @@ class Hlt2B2HHPi0LinesConf(HltLinesConfigurableUser) :
     __slots__ = {
                     'PiMinPT'               : 500       # MeV
                     ,'PiMinP'               : 5000      # MeV
-                    ,'TrackMaxChi2Ndof'     : 5         # unitless
+                    ,'TrackMaxChi2Ndof'     : 4         # unitless
+                    ,'MaxMinTrackChi2Ndof'  : 2.4       # unitless 
                     ,'PiMinIPChi2'          : 9         # unitless
 
-                    ,'BMinVtxProb'          : 0.0001    # unitless
-                    ,'BMinVVDChi2'          : 64        # unitless
+                    ,'MaxDOCA'              : 0.2       # mm
 
-                    ,'Pi0MinPT_M'           : 1200      # MeV
-                    ,'Pi0MinPT_R'           : 1200      # MeV
+                    ,'BMaxVtxChi2'          : 10        # unitless
+                    ,'BMinVVDChi2'          : 100       # unitless
 
-                    ,'BMinM_M'              : 4000      # MeV
-                    ,'BMaxM_M'              : 6600      # MeV
-                    ,'BMinM_R'              : 4000      # MeV
-                    ,'BMaxM_R'              : 6600      # MeV
-                    ,'BMinPT_M'             : 2000      # MeV
-                    ,'BMinPT_R'             : 2000      # MeV
+                    ,'PiMinIPChi2_R'        : 16        # unitless
+
+                    ,'Pi0MinPT_M'           : 2500      # MeV
+                    ,'Pi0MinPT_R'           : 1500      # MeV
+
+                    ,'BMinM_M'              : 4200      # MeV
+                    ,'BMaxM_M'              : 6400      # MeV
+                    ,'BMinM_R'              : 4200      # MeV
+                    ,'BMaxM_R'              : 6400      # MeV
+                    ,'BMinPT_M'             : 3000      # MeV
+                    ,'BMinPT_R'             : 2500      # MeV
                     
                     ,'BMaxIPChi2_M'         : 25        # unitless
                     ,'BMaxIPChi2_R'         : 25        # unitless
@@ -58,10 +63,11 @@ class Hlt2B2HHPi0LinesConf(HltLinesConfigurableUser) :
             "pi-"  : "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRCHI2DOF<%(TrackMaxChi2Ndof)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % self.getProps()
             ,"pi+" : "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRCHI2DOF<%(TrackMaxChi2Ndof)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % self.getProps()   
                                  } 
-            , CombinationCut = "AALL" % self.getProps()
-            , MotherCut      = "(VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s)" % self.getProps()
+            #, CombinationCut = "AALL" % self.getProps()
+            , CombinationCut = "(AMAXDOCA('LoKi::DistanceCalculator')<%(MaxDOCA)s *mm)& (AALLSAMEBPV) & (AMINCHILD(MINTREE('pi+'==ABSID,TRCHI2DOF))<%(MaxMinTrackChi2Ndof)s)" % self.getProps()
+            , MotherCut      = "(VFASPF(VCHI2)<%(BMaxVtxChi2)s) & (BPVVDCHI2>%(BMinVVDChi2)s)" % self.getProps()
             , ParticleCombiners = {'' : 'TrgVertexFitter'}
-            , Inputs = [ BiKalmanFittedPions ] )
+            , InputLocations = [ BiKalmanFittedPions ] )
 
        ###########################################################################
         Hlt2B2HHPi0 = Hlt2Member(
@@ -71,10 +77,10 @@ class Hlt2B2HHPi0LinesConf(HltLinesConfigurableUser) :
             "rho(770)0" : "ALL" % self.getProps()
             ,"pi0"      : "(PT>%(Pi0MinPT_M)s *MeV)" % self.getProps()   
             } 
-            , CombinationCut = "in_range(%(BMinM_M)s *MeV, AM, %(BMaxM_M)s *MeV)" % self.getProps()
+            , CombinationCut = "(in_range(%(BMinM_M)s *MeV, AM, %(BMaxM_M)s *MeV))" % self.getProps()
             , MotherCut      = "(PT>%(BMinPT_M)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2_M)s) & (BPVDIRA>%(BMinDIRA_M)s)" % self.getProps()
             , ParticleCombiners = {'' : 'TrgVertexFitter'}
-            , Inputs = [ Hlt2Rho4HHPi0, MergedPi0s ])
+            , InputLocations = [ Hlt2Rho4HHPi0, MergedPi0s ])
         
         
         ############################################################################
@@ -104,9 +110,9 @@ class Hlt2B2HHPi0LinesConf(HltLinesConfigurableUser) :
             "rho(770)0" : "ALL" % self.getProps()
             ,"pi0"      : "(PT>%(Pi0MinPT_R)s *MeV)" % self.getProps()   
             }
-            , "CombinationCut" : "in_range(%(BMinM_R)s *MeV, AM, %(BMaxM_R)s *MeV)" % self.getProps()
+            , "CombinationCut" : "(in_range(%(BMinM_R)s *MeV, AM, %(BMaxM_R)s *MeV)) & (AMINCHILD(MINTREE('pi+'==ABSID,MIPCHI2DV(PRIMARY)))>%(PiMinIPChi2_R)s)" % self.getProps()
             , "MotherCut"      : "(PT>%(BMinPT_R)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2_R)s) & (BPVDIRA>%(BMinDIRA_R)s)" % self.getProps()
-            , "Inputs" : [ Hlt2Rho4HHPi0, ResolvedPi0s ]
+            , "InputLocations" : [ Hlt2Rho4HHPi0, ResolvedPi0s ]
             }
                    , postscale = self.postscale
                    )
