5d5256563c10dc3aa6e85535c80258a0b7fa1cc9
Artur Ukleja <artur.ukleja@cern.ch>
2011-02-24 15:25:32 +0000

    refined Bs2JpsieePhi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118836 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3e82c94651086dd6eb4a562e9d5ce212a7ec9cc4

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2JpsieePhi.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2JpsieePhi.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Artur Ukleja, Jibo He'
-__date__ = '2010/09/23'
+__date__ = '2011/02/24'
 __version__ = '$Revision: 1.5 $'
 
 '''
@@ -16,37 +16,43 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
     Definition of Bs->JpsieePhi stripping
     """
     __slots__ = { 
-                  'ElectronPTLoose'          :   700.    # MeV
-                , 'JpsiVertexCHI2pDOFLoose'  :    16.    # adimensional
-                , 'JpsiMassMinLoose'         :  2500.    # MeV
-                , 'JpsiMassMaxLoose'         :  3300.    # MeV
-                , 'PhiPTLoose'               :  1000.    # MeV
-                , 'PhiVertexCHI2pDOFLoose'   :    16.    # adimensional
-                , 'PhiMassMinLoose'          :   995.    # MeV
-                , 'PhiMassMaxLoose'          :  1045.    # MeV
-                , 'BsVertexCHI2pDOFLoose'    :    16.    # adimensional
-                , 'BsMassMinLoose'           :  3500.    # MeV
-                , 'BsMassMaxLoose'           :  6500.    # MeV
+                  'ElectronPTLoose'            :   800.    # MeV
+                , 'ElectronTrackCHI2pDOFLoose' :    10.    # adimensional
+                , 'JpsiVertexCHI2pDOFLoose'    :    15.    # adimensional
+                , 'JpsiMassMinLoose'           :  2700.    # MeV
+                , 'JpsiMassMaxLoose'           :  3300.    # MeV
+                , 'KaonTrackCHI2pDOFLoose'     :    10.    # adimensional
+                , 'PhiPTLoose'                 :  1000.    # MeV
+                , 'PhiVertexCHI2pDOFLoose'     :    15.    # adimensional
+                , 'PhiMassMinLoose'            :   990.    # MeV
+                , 'PhiMassMaxLoose'            :  1050.    # MeV
+                , 'BsVertexCHI2pDOFLoose'      :    10.    # adimensional
+                , 'BsMassMinLoose'             :  4500.    # MeV
+                , 'BsMassMaxLoose'             :  6000.    # MeV
+                , 'BsDIRALoose'                :     0.99  # adimensional
 
-                , 'ElectronPT'          :   800.    # MeV
-                , 'ElectronPID'         :     2.    # adimensional
-                , 'JpsiVertexCHI2pDOF'  :    15.    # adimensional
-                , 'JpsiMassMin'         :  2500.    # MeV
-                , 'JpsiMassMax'         :  3300.    # MeV
-                , 'PhiPT'               :  1000.    # MeV
-                , 'PhiVertexCHI2pDOF'   :    15.    # adimensional
-                , 'PhiMassMin'          :  1005.    # MeV
-                , 'PhiMassMax'          :  1035.    # MeV
-                , 'BsVertexCHI2pDOF'    :     6.    # adimensional
-                , 'BsMassMin'           :  4000.    # MeV
-                , 'BsMassMax'           :  6000.    # MeV
-                , 'BsDIRA'              :     0.99  # adimensional
+                , 'ElectronPT'            :   800.    # MeV
+                , 'ElectronPID'           :     2.    # adimensional
+                , 'ElectronTrackCHI2pDOF' :     5.    # adimensional
+                , 'JpsiVertexCHI2pDOF'    :    15.    # adimensional
+                , 'JpsiMassMin'           :  2700.    # MeV
+                , 'JpsiMassMax'           :  3300.    # MeV
+                , 'KaonTrackCHI2pDOF'     :     5.    # adimensional
+                , 'KaonPID'               :     0.    # adimensional
+                , 'PhiPT'                 :  1000.    # MeV
+                , 'PhiVertexCHI2pDOF'     :    15.    # adimensional
+                , 'PhiMassMin'            :  1005.    # MeV
+                , 'PhiMassMax'            :  1035.    # MeV
+                , 'BsVertexCHI2pDOF'      :     6.    # adimensional
+                , 'BsMassMin'             :  4500.    # MeV
+                , 'BsMassMax'             :  6000.    # MeV
+                , 'BsDIRA'                :     0.99  # adimensional
                 }
 
 
     def UnbiasedPT_line( self ) :
         from StrippingConf.StrippingLine import StrippingLine
-        return StrippingLine('Bs2JpsieePhiLoose', prescale = 0.01, algos = [ self.Bs2JpsieePhiLoose() ])
+        return StrippingLine('Bs2JpsieePhiLoose', prescale = 1., algos = [ self.Bs2JpsieePhiLoose() ])
 
 
     def Signalbox_line( self ) :
@@ -55,9 +61,10 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
       
 
     def Jpsi2eeLoose( self ):
-        _stdJpsi = DataOnDemand( Location="Phys/StdLooseJpsi2ee" )
+        _stdJpsi = DataOnDemand( Location="Phys/StdLooseJpsi2ee/Particles" )
         _jpsi = FilterDesktop("Jpsi2eeFilterForBs2JpsieePhiLoose",
                               Code = "   (MINTREE('e+'==ABSID,PT) > %(ElectronPTLoose)s *MeV)" \
+                                     " & (MAXTREE('e+'==ABSID,TRCHI2DOF) < %(ElectronTrackCHI2pDOFLoose)s)" \
                                      " & (VFASPF(VCHI2/VDOF) < %(JpsiVertexCHI2pDOFLoose)s)" \
                                      " & (MM > %(JpsiMassMinLoose)s *MeV)" \
                                      " & (MM < %(JpsiMassMaxLoose)s *MeV)" % self.getProps()
@@ -69,10 +76,11 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
     
 
     def Jpsi2ee( self ):
-        _stdJpsi = DataOnDemand( Location="Phys/StdLooseJpsi2ee" )
+        _stdJpsi = DataOnDemand( Location="Phys/StdLooseJpsi2ee/Particles" )
         _jpsi = FilterDesktop("Jpsi2eeFilterForBs2JpsieePhi",
                               Code = "   (MINTREE('e+'==ABSID,PT) > %(ElectronPT)s *MeV)" \
                                      " & (MINTREE('e+'==ABSID,PIDe-PIDpi) > %(ElectronPID)s )" \
+                                     " & (MAXTREE('e+'==ABSID,TRCHI2DOF) < %(ElectronTrackCHI2pDOF)s)" \
                                      " & (VFASPF(VCHI2/VDOF) < %(JpsiVertexCHI2pDOF)s)" \
                                      " & (MM > %(JpsiMassMin)s *MeV)" \
                                      " & (MM < %(JpsiMassMax)s *MeV)" % self.getProps()
@@ -85,9 +93,10 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
 
 
     def Phi2KKLoose( self ):
-        _stdPhi = DataOnDemand(Location="Phys/StdLoosePhi2KK")
+        _stdPhi = DataOnDemand(Location="Phys/StdLoosePhi2KK/Particles")
         _phi = FilterDesktop("Phi2KKFilterForBs2JpsieePhiLoose",
-                             Code = "   (VFASPF(VCHI2/VDOF) < %(PhiVertexCHI2pDOFLoose)s)" \
+                             Code = "   (MAXTREE('K+'==ABSID,TRCHI2DOF) < %(KaonTrackCHI2pDOFLoose)s)" \
+                                    " & (VFASPF(VCHI2/VDOF) < %(PhiVertexCHI2pDOFLoose)s)" \
                                     " & (PT > %(PhiPTLoose)s *MeV)"\
                                     " & (MM > %(PhiMassMinLoose)s *MeV)" \
                                     " & (MM < %(PhiMassMaxLoose)s *MeV)" % self.getProps()
@@ -99,9 +108,11 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
     
 
     def Phi2KK( self ):
-        _stdPhi = DataOnDemand(Location="Phys/StdLoosePhi2KK")  
+        _stdPhi = DataOnDemand(Location="Phys/StdLoosePhi2KK/Particles")  
         _phi = FilterDesktop("Phi2KKFilterForBs2JpsieePhi",
-                             Code = "   (VFASPF(VCHI2/VDOF) < %(PhiVertexCHI2pDOF)s)" \
+                             Code = "   (MINTREE('K+'==ABSID,PIDK-PIDpi) > %(KaonPID)s )" \
+                                    " & (MAXTREE('K+'==ABSID,TRCHI2DOF) < %(KaonTrackCHI2pDOF)s)" \
+                                    " & (VFASPF(VCHI2/VDOF) < %(PhiVertexCHI2pDOF)s)" \
                                     " & (PT > %(PhiPT)s *MeV)"\
                                     " & (MM > %(PhiMassMin)s *MeV)" \
                                     " & (MM < %(PhiMassMax)s *MeV)" % self.getProps()
@@ -117,7 +128,7 @@ class StrippingBs2JpsieePhiConf(LHCbConfigurableUser):
         Jpsi = self.Jpsi2eeLoose()
         Phi = self.Phi2KKLoose()
         CC = "(AM > %(BsMassMinLoose)s *MeV) & (AM < %(BsMassMaxLoose)s *MeV)" % self.getProps()
-        MC = "(VFASPF(VCHI2/VDOF) < %(BsVertexCHI2pDOFLoose)s)" % self.getProps()
+        MC = "(VFASPF(VCHI2/VDOF) < %(BsVertexCHI2pDOFLoose)s) & (BPVDIRA > %(BsDIRALoose)s)" % self.getProps()
         _Bs = CombineParticles("Bs2JpsieePhiLoose",
                                DecayDescriptor = "B_s0 -> J/psi(1S) phi(1020)",
                                CombinationCut = CC , 
