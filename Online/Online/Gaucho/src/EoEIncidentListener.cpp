481f1b2666b007d89500f0393b9119a0c22f5380
Beat Jost <beat.jost@cern.ch>
2011-02-24 12:15:26 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@118815 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 1ce7c7a8fb75f943223a8045cfe0f0c7cdf99e93

--- Online/Online/Gaucho/src/EoEIncidentListener.cpp
+++ Online/Online/Gaucho/src/EoEIncidentListener.cpp
@@ -35,7 +35,7 @@ void EoEIncidentListener::handle(const Incident &i)
   }
   else if (i.type() == "DAQ_BEGIN_EVENT")
   {
-    m_MonSvc->UnLock();
+    m_MonSvc->Lock();
 //    printf("-----------------EoE Handler Monitor System UN-LockED\n");
   }
 }
