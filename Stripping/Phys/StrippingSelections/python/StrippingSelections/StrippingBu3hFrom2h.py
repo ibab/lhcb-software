d078264f5960f16269f4ee2870f65862198e3f81
Jussara Marques De Miranda <jussara.miranda@cern.ch>
2011-02-24 18:50:06 +0000

    StrippingBu3hFrom2h for stripping13
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118880 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 966e3e235d8d69113ef8905f37a64aa778011dfa

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBu3hFrom2h.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBu3hFrom2h.py
@@ -1,87 +1,333 @@
-########################################################################
-#
-#   Module for construction of B->hhh (h=K,pi,p) StrippingLines.
-#
-#
-#   This Lines follow the "Topological" idea in which we may apply
-#   PT and IP Cuts in three bodies and can recover from the 2 Body line.
-#
-#   __author__ = 'Rafael Coutinho'
-#   __date__ = '22/10/2010'
-#   __version__ = '$ 1.0 $'
-#
-########################################################################
-                                                                                                                                                                                  
-from os import environ
+ # $Id: StrippingBu3hFrom2h.py,v 2.0 2011-02-15 samba team $
+'''
+Module for construction of Bu->hhh from:   
+   two-body (allowing an extra low momentum third hadron afterwards)
+   three-body
+
+Exported symbols (use python help!):
+   - Bu3hFrom2hBuilder
+   - make2hLoose  
+   - make2h
+   - make3h
+
+Parameters:
+
+    	'MaxTrSIZE':  None,         ## GEC  maximim Rec/Track/Best TrSIZE
+        '_h_PT': 700. ,             ## tracks min PT
+        '_h_P': 5000. ,             ## tracks min P  
+        '_h_IPCHI2': 16. ,          ## min tracks PI wrt OWNPV
+        '_h_TRCHI2DOF': 3.0 ,       ## max tracks CHI2DOF
+        '_2hLoose_DOCA': .12 ,      ## max two track DOCA
+        '_2h_PTmax': 2000. ,        ## min PT of the 2h higests PT track 
+        '_2h_PTsum':4000.  ,        ## min of 2h tracks PT sum 
+        '_2h_PVIPCHI2sum': 100. ,   ## min of the 2h tracks IP wrt best 2h PV
+        '_2h_FDCHI2': 150. ,        ## min 2h FDCHI2 wrt best 2h PV 
+        '_2h_DIRA': 0.99 ,          ## min cos angle between 2h momentum and PV decayV  direction
+        '_2h_CORRMmax': 6000. ,     ## max corrected mass for 2h candidate
+        '_2h_CORRMmin': 4000. ,     ## min corrected mass for 2h candidate 
+        '_2h_CharmVetoIPCHI2': 16. ,## charm veto: (Mhh>_2h_CharmVetoM||IP>_2h_CharmVetoIPCHI2)
+        '_2h_CharmVetoM': 2500. ,   ##
+	'_2h_TRKCHIDOFmin': 3. ,    ## max tracks CHI2DOF for the track with smalest CHI2DOF
+        '_2h_Mmin': 3500. ,         ## min 2h mass
+        '_2h_PVDOCAmin': 1.0 ,      ## min value of the 2h doca wrt any PV
+        '_2h_CHI2': 14.0 ,          ## max 2h vertex CHI2
+        '_2h_PTRANS': 4000. ,       ## 2h transverse mom wrt the PV decayV  direction
+        '_3h_DOCA': .14 ,           ## max DOCA between h and 2h 
+        '_3h_PTmax': 2000 ,         ## min PT of the 3h higests PT track
+        '_3h_DIRA': .9998 ,         ## min cos angle between 3h momentum and PV decayV  direction   
+        '_3h_FDCHI2': 150. ,        ## min 3h FDCHI2 wrt best 3h PV  
+        '_3h_PVDOCAmin': 1.0 ,      ## min value of the 3h doca wrt any PV
+        '_3h_CHI2': 14.0 ,          ## max 3h vertex CHI2 
+        '_3h_IP': 0.1 ,             ## max 3h IP wrt best 3h PV
+        '_3h_PT': 1000. ,           ## min 3h PT   
+        '_3h_PTsum':4250.  ,        ## min of 3h tracks PT sum 
+        '_3h_PVIPCHI2sum': 150. ,   ## min of the 3h tracks IP wrt best 3h PV
+        '_3h_Charge': 1 ,           ## 3h tracks charge sum ==+-1
+        '_3h_CORRMmax': 7000. ,     ## max corrected mass for 3h candidate  
+        '_3h_CORRMmin': 4000. ,     ## min corrected mass for 3h candidate   
+        '_3h_Mmax': 6000. ,         ## max 3h mass        
+        '_3h_Mmin': 4000. ,         ## min 3h mass     
+        '2hLinePrescale': 1. ,
+        '2hLinePostscale': 1 ,
+        '3hLinePrescale': 1 ,
+        '3hLinePostscale': 1}
+
+'''
+
+__author__ = ['Jussara Miranda, Juan Otalora and Andre Massafferri']
+__date__ = '15/02/2011'
+__version__ = '$Revision: 1.0 $'
+
+__all__ = ('Bu3hFrom2hBuilder',
+           'make2hLoose', 
+           'make2h', 
+           'make3h')
+
 from Gaudi.Configuration import *
-from Configurables import CombineParticles, FilterDesktop
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
+from StandardParticles                     import StdLoosePions
+
+from PhysSelPython.Wrappers import Selection
 from StrippingConf.StrippingLine import StrippingLine
-from Configurables import OfflineVertexFitter
+from StrippingUtils.Utils import LineBuilder
 from Configurables import LoKi__VoidFilter as VoidFilter
-from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
-modules =  CoreFactory('CoreFactory').Modules
-for i in [ 'LoKiTrigger.decorators' ] :
-  if i not in modules : modules.append(i)
 
-StrippingKillTooManyTopoIP = VoidFilter('StrippingKillTooManyTopoIP' 
-                                        , Code = "TrSOURCE('Rec/Track/Best') >> (TrSIZE < 300 )")
+name = "Bu3hFrom2hBuilder"
 
-#  Two-body Line
-Bu3hFrom2h_2Body = CombineParticles("Bu3hFrom2h_2Body")
-Bu3hFrom2h_2Body.InputLocations = ["StdLoosePions"]
-Bu3hFrom2h_2Body.DecayDescriptors = ["rho(770)0 -> pi+ pi+","rho(770)0 -> pi+ pi-","rho(770)0 -> pi- pi-"]
-Bu3hFrom2h_2Body.DaughtersCuts = {"pi+" : "(PT > 500.*MeV) & (P>5*GeV) & (MIPCHI2DV(PRIMARY)>16) & (TRCHI2DOF<5)","pi-" : "(PT > 500.*MeV) & (P>5*GeV) & (MIPCHI2DV(PRIMARY)>16) & (TRCHI2DOF<5)"}
-Bu3hFrom2h_2Body.CombinationCut = "(AMINDOCA('LoKi::TrgDistanceCalculator')<0.15)"
-Bu3hFrom2h_2Body.MotherCut = "(MAXTREE((('pi+'==ABSID) | ('pi-'==ABSID))  ,PT)>1.5*GeV) & (BPVVDCHI2>64) & (BPVDIRA>0.)"
+class Bu3hFrom2hBuilder(LineBuilder) :
+    __configuration_keys__ = ('MaxTrSIZE',
+                              '_h_PT',             
+                              '_h_P',               
+                              '_h_IPCHI2',         
+                              '_h_TRCHI2DOF',       
+                              '_2hLoose_DOCA',      
+                              '_2h_PTmax',         
+                              '_2h_PTsum',         
+                              '_2h_PVIPCHI2sum',    
+                              '_2h_FDCHI2',         
+                              '_2h_DIRA',           
+                              '_2h_CORRMmax',       
+                              '_2h_CORRMmin',       
+                              '_2h_CharmVetoIPCHI2',
+                              '_2h_CharmVetoM',
+			      '_2h_TRKCHIDOFmin',
+                              '_2h_Mmin', 
+                              '_2h_PVDOCAmin',
+                              '_2h_CHI2',          
+                              '_2h_PTRANS',          
+                              '_3h_DOCA',            
+                              '_3h_PTmax',          
+                              '_3h_DIRA',            
+                              '_3h_FDCHI2',         
+                              '_3h_PVDOCAmin',      
+                              '_3h_CHI2',           
+                              '_3h_IP',             
+                              '_3h_PT',              
+                              '_3h_PTsum',              
+                              '_3h_PVIPCHI2sum',              
+                              '_3h_Charge',         
+                              '_3h_CORRMmax',       
+                              '_3h_CORRMmin',       
+                              '_3h_Mmax',                 
+                              '_3h_Mmin',             
+                              '2hLinePrescale',
+                              '2hLinePostscale',
+                              '3hLinePrescale',
+                              '3hLinePostscale')
 
-# Three-body Line
-Bu3hFrom2h_3Body= CombineParticles("Bu3hFrom2h_3Body")
-Bu3hFrom2h_3Body.InputLocations  = ["StdLoosePions", "Bu3hFrom2h_2Body"]
-Bu3hFrom2h_3Body.DecayDescriptors = ["B+ -> rho(770)0 pi+","B+ -> rho(770)0 pi-"]
-Bu3hFrom2h_3Body.DaughtersCuts = {"pi+" : "(PT > 500.*MeV) & (MIPCHI2DV(PRIMARY)>16)","pi-" : "(PT > 500.*MeV) & (MIPCHI2DV(PRIMARY)>16)",}
-Bu3hFrom2h_3Body.CombinationCut = "(AMAXDOCA('LoKi::TrgDistanceCalculator')<0.3)"
-Bu3hFrom2h_3Body.MotherCut = "(MAXTREE((('pi+'==ABSID) | ('pi-'==ABSID)),PT)>1.5*GeV) & (BPVDIRA>0.99998) & (VFASPF(VMINVDDV(PRIMARY))>3) & (VFASPF(VCHI2)<12) & (MIPDV(PRIMARY)<0.04) & (PT>1000*MeV) & (abs(CHILD(1,SUMQ) + CHILD(2,Q))==1)"
-Bu3hFrom2h_3Body.addTool( OfflineVertexFitter() )
-Bu3hFrom2h_3Body.VertexFitters.update( { "" : "OfflineVertexFitter"} )
-Bu3hFrom2h_3Body.OfflineVertexFitter.useResonanceVertex = False
+    def __init__(self, name, config) :
 
-# Filters to apply Mass and MCor cuts
-# Two-body Line
-preambulo = ["PTRANS = P*sqrt( 1-BPVDIRA**2 )",
-             "MCOR = sqrt(M**2 + PTRANS**2) + PTRANS"]
+        LineBuilder.__init__(self, name, config)
 
-filterTopoStripping2Body = FilterDesktop('filterTopoStripping2Body'
-                                         , InputLocations = ["Bu3hFrom2h_2Body"]
-                                         , Code = "(M >4000*MeV) & (MCOR < 7000*MeV)"
-                                         , Preambulo=preambulo
-                                         )
-               
-#The 'robust' 3-body
-filterTopoStripping3Body = FilterDesktop('filterTopoStripping3Body'
-                                         , InputLocations = ["Bu3hFrom2h_3Body"]
-                                         , Code = "(M >4000*MeV) & (MCOR < 7000*MeV)"
-                                         , Preambulo=preambulo
-                                         )
+        _2hName = name + '2h'
+        _3hName = name + '3h'
+	
+	self.sel2hLoose = make2hLoose('2hLooseFor' + _2hName + 'Sel', 
+                                      _h_PT         = config['_h_PT'],
+                                      _h_P          = config['_h_P'],
+                                      _h_IPCHI2     = config['_h_IPCHI2'],
+                                      _h_TRCHI2DOF  = config['_h_TRCHI2DOF'], 
+                                      _2hLoose_DOCA = config['_2hLoose_DOCA'])
+				      
+        self.sel2h = make2h('2hFor' + _2hName + 'Sel', 
+                             Sel2hLoose = self.sel2hLoose, 
+                             _2h_PTmax           = config['_2h_PTmax'],
+                             _2h_PTsum           = config['_2h_PTsum'],
+                             _2h_PVIPCHI2sum     = config['_2h_PVIPCHI2sum'],
+                             _2h_FDCHI2          = config['_2h_FDCHI2'],
+                             _2h_DIRA            = config['_2h_DIRA'],
+                             _2h_CORRMmax        = config['_2h_CORRMmax'],
+                             _2h_CORRMmin        = config['_2h_CORRMmin'],
+                             _2h_CharmVetoIPCHI2 = config['_2h_CharmVetoIPCHI2'],
+                             _2h_CharmVetoM      = config['_2h_CharmVetoM'],
+                             _2h_TRKCHIDOFmin    = config['_2h_TRKCHIDOFmin'],
+                             _2h_Mmin            = config['_2h_Mmin'],
+                             _2h_PVDOCAmin       = config['_2h_PVDOCAmin'],
+                             _2h_CHI2            = config['_2h_CHI2'],       
+                             _2h_PTRANS          = config['_2h_PTRANS'])       
+
+	self.sel3h = make3h( '3hFor' + _3hName + 'Sel', 
+                             Sel2hLoose = self.sel2hLoose,
+                             _h_PT           = config['_h_PT'],
+                             _h_P            = config['_h_P'],
+                             _h_IPCHI2       = config['_h_IPCHI2'],
+                             _h_TRCHI2DOF    = config['_h_TRCHI2DOF'], 
+                             _3h_DOCA        = config['_3h_DOCA'],
+                             _3h_PTmax       = config['_3h_PTmax'],
+                             _3h_DIRA        = config['_3h_DIRA'],
+                             _3h_FDCHI2      = config['_3h_FDCHI2'],
+                             _3h_PVDOCAmin   = config['_3h_PVDOCAmin'],
+                             _3h_CHI2        = config['_3h_CHI2'],
+                             _3h_IP          = config['_3h_IP'],
+                             _3h_PT          = config['_3h_PT'],
+                             _3h_PTsum       = config['_3h_PTsum'],
+                             _3h_PVIPCHI2sum = config['_3h_PVIPCHI2sum'],
+                             _3h_Charge      = config['_3h_Charge'],
+                             _3h_CORRMmax    = config['_3h_CORRMmax'],
+                             _3h_CORRMmin    = config['_3h_CORRMmin'],
+                             _3h_Mmin        = config['_3h_Mmin'],
+                             _3h_Mmax        = config['_3h_Mmax'])
+
+	self.gECFilter = globalEventCutFilter(name + 'GlobalEventCutFilter', MaxTrSIZE = config['MaxTrSIZE'])
+	
+        self.algos2h = []
+        if self.gECFilter != None  : self.algos2h.append(self.gECFilter)
+
+	self.algos2h.append(self.sel2h)
+	
+        self.algos3h = []
+        if self.gECFilter != None : self.algos3h.append(self.gECFilter)
+
+        self.algos3h.append(self.sel3h)	
+	
+        self.line2h = StrippingLine( _2hName + 'Line',
+                                     prescale   = config['2hLinePrescale'],
+                                     postscale  = config['2hLinePostscale'],
+                                     algos      = self.algos2h)
+        
+	self.line3h = StrippingLine( _3hName + 'Line',
+                                     prescale   = config['3hLinePrescale'],
+                                     postscale  = config['3hLinePostscale'],
+                                     algos      = self.algos3h)
+
+        self.registerLine(self.line2h)
+        self.registerLine(self.line3h)
+
+def make2hLoose(name, 
+                _h_PT,
+                _h_P,
+                _h_IPCHI2,
+                _h_TRCHI2DOF,
+                _2hLoose_DOCA) :
+    _motherCut = "ALL" % locals()
+    _combinationCut = "(AMINDOCA('LoKi::TrgDistanceCalculator') < %(_2hLoose_DOCA)s)" % locals()
+    _daughtersCuts = {"pi+" : "(PT > %(_h_PT)s*MeV) \
+                             & (P > %(_h_P)s*MeV) \
+                             & (MIPCHI2DV(PRIMARY) > %(_h_IPCHI2)s) \
+                             & (TRCHI2DOF < %(_h_TRCHI2DOF)s)" % locals()}
+    
+    
+    _2hLoose=CombineParticles()
+    _2hLoose.DecayDescriptors  = ["rho(770)0 -> pi+ pi+","rho(770)0 -> pi+ pi-","rho(770)0 -> pi- pi-"]
+    _2hLoose.DaughtersCuts     = _daughtersCuts
+    _2hLoose.CombinationCut    = _combinationCut
+    _2hLoose.MotherCut         = _motherCut
+
+    return Selection ( name,
+                       Algorithm = _2hLoose,
+                       RequiredSelections = [StdLoosePions])
+
+def make2h(name, 
+           Sel2hLoose, 
+           _2h_PTmax,
+           _2h_PTsum,
+           _2h_PVIPCHI2sum,
+           _2h_FDCHI2,
+           _2h_DIRA,
+           _2h_CORRMmax,
+           _2h_CORRMmin,
+           _2h_CharmVetoIPCHI2,
+           _2h_CharmVetoM,
+	   _2h_TRKCHIDOFmin,
+           _2h_Mmin,
+	   _2h_PVDOCAmin,
+	   _2h_CHI2,
+	   _2h_PTRANS) :
+ 
+    _tightCutFor2h = " (M > %(_2h_Mmin)s*MeV) \
+                     & (MAXTREE((('pi+'==ABSID) | ('pi-'==ABSID)),PT) > %(_2h_PTmax)s*MeV) \
+		     & (SUMTREE(PT,(('pi+'==ABSID) | ('pi-'==ABSID)),0.0) > %(_2h_PTsum)s*MeV) \
+		     & (SUMTREE(MIPCHI2DV(PRIMARY),(('pi+'==ABSID) | ('pi-'==ABSID)),0.0) > %(_2h_PVIPCHI2sum)s) \
+		     & (BPVVDCHI2 > %(_2h_FDCHI2)s) \
+		     & (BPVDIRA > %(_2h_DIRA)s) \
+		     & (BPVCORRM < %(_2h_CORRMmax)s*MeV) \
+		     & (BPVCORRM > %(_2h_CORRMmin)s*MeV) \
+		     & ((M > %(_2h_CharmVetoM)s*MeV) | (BPVIPCHI2() > %(_2h_CharmVetoIPCHI2)s)) \
+		     & (MINTREE((('pi+'==ABSID) | ('pi-'==ABSID)),TRCHI2DOF) < %(_2h_TRKCHIDOFmin)s) \
+                     & (VFASPF(VMINVDDV(PRIMARY)) > %(_2h_PVDOCAmin)s) \
+                     & (BPVPTFLIGHT < %(_2h_PTRANS)s*MeV) \
+		     & (VFASPF(VCHI2) < %(_2h_CHI2)s) " % locals()
+    
+    
+    _2hFilter = FilterDesktop(Code = _tightCutFor2h)
+
+    return Selection (name,
+                      Algorithm = _2hFilter,
+                      RequiredSelections = [Sel2hLoose])
+
+def make3h(name,
+           Sel2hLoose,
+           _h_PT,
+           _h_P,
+           _h_IPCHI2,
+           _h_TRCHI2DOF,
+           _3h_DOCA,
+           _3h_PTmax,
+           _3h_DIRA,
+           _3h_FDCHI2,
+           _3h_PVDOCAmin,
+           _3h_CHI2,
+           _3h_IP,
+           _3h_PT,
+           _3h_PTsum,
+           _3h_PVIPCHI2sum,
+           _3h_Charge,
+           _3h_CORRMmax,
+           _3h_CORRMmin,
+           _3h_Mmin,
+           _3h_Mmax) :
+
+    _daughtersCuts = {"pi+" : "(PT > %(_h_PT)s*MeV) \
+                             & (P > %(_h_P)s*MeV) \
+                             & (MIPCHI2DV(PRIMARY) > %(_h_IPCHI2)s) \
+                             & (TRCHI2DOF < %(_h_TRCHI2DOF)s)" % locals()}
+    _combinationCut = "(AM < %(_3h_Mmax)s*MeV) \
+                     & (AM > %(_3h_Mmin)s*MeV) \
+		     & (AMAXDOCA('LoKi::TrgDistanceCalculator') < %(_3h_DOCA)s)" % locals()
+    _motherCut = "(MAXTREE((('pi+'==ABSID) | ('pi-'==ABSID)),PT) > %(_3h_PTmax)s*MeV) \
+                & (BPVDIRA > %(_3h_DIRA)s) \
+		& (BPVVDCHI2 > %(_3h_FDCHI2)s) \
+		& (VFASPF(VMINVDDV(PRIMARY)) > %(_3h_PVDOCAmin)s) \
+		& (VFASPF(VCHI2) < %(_3h_CHI2)s) \
+		& (MIPDV(PRIMARY) < %(_3h_IP)s) \
+		& (PT > %(_3h_PT)s*MeV) \
+		& (SUMTREE(PT,(('pi+'==ABSID) | ('pi-'==ABSID)),0.0) > %(_3h_PTsum)s*MeV) \
+		& (SUMTREE(MIPCHI2DV(PRIMARY),(('pi+'==ABSID) | ('pi-'==ABSID)),0.0) > %(_3h_PVIPCHI2sum)s) \
+		& (abs(CHILD(1,SUMQ) + CHILD(2,Q))== %(_3h_Charge)s ) \
+		& (BPVCORRM < %(_3h_CORRMmax)s * MeV)& (BPVCORRM > %(_3h_CORRMmin)s*MeV)" % locals()
+
+   
+    _3h=CombineParticles()
+    _3h.DecayDescriptors  = ["B+ -> rho(770)0 pi+","B+ -> rho(770)0 pi-"]
+    _3h.MotherCut         = _motherCut
+    _3h.CombinationCut    = _combinationCut
+    _3h.DaughtersCuts     = _daughtersCuts
 
 
-# Sequence for Two-body Line
-Bu3hFrom2h_2BodySeq = GaudiSequencer("Bu3hFrom2h_2BodySeq")
-Bu3hFrom2h_2BodySeq.Members += [Bu3hFrom2h_2Body]
-Bu3hFrom2h_2BodySeq.Members += [filterTopoStripping2Body]
-# Sequence for Three-body Line
-Bu3hFrom2h_3BodySeq = GaudiSequencer("Bu3hFrom2h_3BodySeq")
-Bu3hFrom2h_3BodySeq.Members += [Bu3hFrom2h_2Body]
-Bu3hFrom2h_3BodySeq.Members += [Bu3hFrom2h_3Body]
-Bu3hFrom2h_3BodySeq.Members += [filterTopoStripping3Body]
+    return Selection ( name,
+                       Algorithm = _3h,
+                       RequiredSelections = [Sel2hLoose, StdLoosePions])
+
+def globalEventCutFilter(name, 
+	                 MaxTrSIZE = None 
+	                 ) :
+  
+  if MaxTrSIZE == None : return None
+  
+  from Configurables import LoKi__VoidFilter as VoidFilter
+  from Configurables import LoKi__Hybrid__CoreFactory as CoreFactory
+  modules = CoreFactory('CoreFactory').Modules
+  for i in ['LoKiTrigger.decorators']:
+     if i not in modules : modules.append(i)
+  
+  _code = ""
+  if MaxTrSIZE != None : _code += "TrSOURCE('Rec/Track/Best') >> (TrSIZE < %(MaxTrSIZE)s )" % locals()
+  globalFilter= VoidFilter(name)
+  globalFilter.Code = _code
+  
+  return globalFilter
 
 
-# The 2-body Line
-line1 = StrippingLine('Bu3hFrom2h_2BodyLine'
-               , prescale = 0.05
-               , algos = [StrippingKillTooManyTopoIP, Bu3hFrom2h_2BodySeq]
-               )
-# The 3-body Line 
-line2 = StrippingLine('Bu3hFrom2h_3BodyLine'
-               , prescale = 1
-               , algos = [StrippingKillTooManyTopoIP, Bu3hFrom2h_3BodySeq]
-               )
+
+
+
