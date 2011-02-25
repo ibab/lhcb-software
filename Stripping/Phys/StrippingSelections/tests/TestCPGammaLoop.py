2160bcfea49b29073534f63594997468953b00ff
ukerzel <ukerzel@cern.ch>
2011-02-25 07:49:25 +0000

    new line definition for CP WG Gamma/Loop
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118896 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 664d2d92f254e86e9a66666c35cc5f39a101c978

--- Stripping/Phys/StrippingSelections/tests/TestCPGammaLoop.py
+++ Stripping/Phys/StrippingSelections/tests/TestCPGammaLoop.py
@@ -11,69 +11,116 @@
 from Gaudi.Configuration import *
 from Configurables import SelDSTWriter, DaVinci
 from StrippingConf.Configuration import StrippingConf
+from StrippingSettings.Utils import lineBuilderConfiguration
+
+# Now build the stream
+from StrippingConf.StrippingStream import StrippingStream
+stream = StrippingStream("Test")
 
 #
 # Import your stripping lines
 #
 
-# B-> hh (lifetime unbiased, mass hypothesis Bs->KK)
-from StrippingSelections.StrippingB2hhLTUnbiased import StrippingB2hhLTUnbiasedConf as B2hhLTUnbiased
 
-# B-> hh (lifetime biased, mass hypothesis Bd->pi pi),  B-> pp
+# B->hh (lifetime biased)
 from StrippingSelections.StrippingHb2Charged2Body import Hb2Charged2BodyLines
+paramBhh = { 'PrescaleB2Charged2Body'   : 1,      'PrescaleB2PPbar' : 1,
+             'MinPTB2Charged2Body'      : 1100,    'MinPTB2PPbar' : 900,
+             'MinIPB2Charged2Body'      : 0.15,
+             'MinIPChi2B2Charged2Body'  : 100,     'MinIPChi2B2PPbar' : 16,
+             'TrChi2'                   : 5,
+             'PIDppi'                   : 0,
+             'PIDpk'                    : -2,
+             'MaxPTB2Charged2Body'      : 2700,   'MaxPTB2PPbar' : 2400,
+             'MaxIPB2Charged2Body'      : 0.3,
+             'MaxIPChi2B2Charged2Body'  : 200,    'MaxIPChi2B2PPbar' : 36,
+             'CombMassLow'              : 4800,
+             'CombMassHigh'             : 5800,   'CombMassWindow' : 200,
+             'DOCA'                     : 0.08,   'VertexChi2B2PPbar' : 16,
+             'BPT'                      : 1200,   'BPTB2PPbar' : 1000,
+             'BIP'                      : 0.06,
+             'BIPChi2B2Charged2Body'    : 12,     'BIPChi2B2PPbar' : 36,
+             'BDIRA'                    : 0.9995,
+             'BTAU'                     : 0.0009,
+             'MassLow'                  : 4800,
+             'MassHigh'                 : 5800
+             }
+confBhh          = Hb2Charged2BodyLines("Bhh", paramBhh)
+stream.appendLines(confBhh.lines())
 
-# B-> hhh
-from StrippingSelections import StrippingBu2hhh
-from StrippingSelections.StrippingB2KShh import StrippingB2KShhConf
+# B->hh (lifetime unbiased)
+from StrippingSelections import StrippingB2hhLTUnbiased
+paramBhhUnbiased = lineBuilderConfiguration('StrippingDev', 'B2hhLTUnbiased' )['CONFIG']
+paramBhhUnbiased['NetCut'] = (0.85 + 1.0)/2.0
+print "NetCut %lf" % paramBhhUnbiased['NetCut']
 
-# B-> hhh (for testing)
-from StrippingSelections import StrippingBu3hFrom2h
+
+confBhhUnbiased  = StrippingB2hhLTUnbiased.StrippingB2hhLTUnbiasedConf("BhhLTUnbiased", paramBhhUnbiased)
+stream.appendLines(confBhhUnbiased.lines())
 
 
-# B -> hhpi0
-from StrippingSelections.StrippingB2HHPi0 import StrippingB2HHPi0Conf
+# B->hhh
+from StrippingSelections.StrippingBu3hFrom2h import Bu3hFrom2hBuilder
+paramsBu3hFrom2h = {
+    'MaxTrSIZE':  None,
+    '_h_PT': 700. ,
+    '_h_P': 5000. ,
+    '_h_IPCHI2': 16. ,
+    '_h_TRCHI2DOF': 3.0 ,
+    '_2hLoose_DOCA': .12 ,
+    '_2h_PTmax': 2000. ,
+    '_2h_PTsum':4000.  ,
+    '_2h_PVIPCHI2sum': 100. ,
+    '_2h_FDCHI2': 150. ,
+    '_2h_DIRA': 0.99 ,
+    '_2h_CORRMmax': 6000. ,
+    '_2h_CORRMmin': 4000. ,
+    '_2h_CharmVetoIPCHI2': 16. ,
+    '_2h_CharmVetoM': 2500. ,
+    '_2h_TRKCHIDOFmin': 3. ,
+    '_2h_Mmin': 3500. ,
+    '_2h_PVDOCAmin': 1.0 ,
+    '_2h_CHI2': 14.0 ,
+    '_2h_PTRANS': 4000. ,
+    '_3h_DOCA': .14 ,
+    '_3h_PTmax': 2000 ,
+    '_3h_DIRA': .9998 ,
+    '_3h_FDCHI2': 150. ,
+    '_3h_PVDOCAmin': 1.0 ,
+    '_3h_CHI2': 14.0 ,
+    '_3h_IP': 0.1 ,
+    '_3h_PT': 1000. ,
+    '_3h_PTsum':4250.  ,
+    '_3h_PVIPCHI2sum': 150. ,
+    '_3h_Charge': 1 ,
+    '_3h_CORRMmax': 7000. ,
+    '_3h_CORRMmin': 4000. ,
+    '_3h_Mmax': 6000. ,
+    '_3h_Mmin': 4000. ,
+    '2hLinePrescale': 1. ,
+    '2hLinePostscale': 1 ,
+    '3hLinePrescale': 1 ,
+    '3hLinePostscale': 1}
+
+bu3hfrom2hBuilder = Bu3hFrom2hBuilder("bu3hfrom2hBuilder", paramsBu3hFrom2h)
+stream.appendLines( bu3hfrom2hBuilder.lines() )
+
 
 #
-# Now build the stream
+# remaining config
 #
-
-# for the BHadron stream
-from StrippingConf.StrippingStream import StrippingStream
-stream = StrippingStream("Test")
-
-stream.appendLines( Hb2Charged2BodyLines().lines() )
-
-stream.appendLines( [StrippingB2KShhConf().B2KSLLhh(),
-                     StrippingB2KShhConf().B2KSDDhh(),
-                     StrippingBu2hhh.line
-                     ])
-
-stream.appendLines( [StrippingBu3hFrom2h.line1,
-                     StrippingBu3hFrom2h.line2
-                     ])
-
-stream.appendLines( [B2hhLTUnbiased().StripB2hhLTUnbiasedLoose(),
-                     B2hhLTUnbiased().StripB2hhLTUnbiased(),
-                     B2hhLTUnbiased().StripB2hhLTUnbiasedTrigger()] )
-
-stream.appendLines( [StrippingB2HHPi0Conf().B2HHPi0_Merged(),
-                     StrippingB2HHPi0Conf().B2HHPi0_Resolved()
-                     ])
-
 from Configurables import  ProcStatusCheck
 filterBadEvents =  ProcStatusCheck()
 
 # Configure the stripping using the same options as in Reco06-Stripping10
 sc = StrippingConf( Streams = [ stream ],
-                    MaxCandidates = 2000,
+                    MaxCandidates = 50000,
                     AcceptBadEvents = False,
                     BadEventSelection = filterBadEvents )
 sc.OutputType = "ETC"                    # Can be either "ETC" or "DST"
 
-
-# Mimic the new default tracking cuts
-from CommonParticles.Utils import DefaultTrackingCuts
-DefaultTrackingCuts().Cuts = { "Chi2Cut" : [0,5] }
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
 
 # Configure the ETC writing step
 from Configurables import EventTuple, TupleToolSelResults
@@ -101,24 +148,23 @@ AuditorSvc().Auditors.append( ChronoAuditor("Chrono") )
 from Configurables import StrippingReport
 sr = StrippingReport(Selections = sc.selections())
 
-from Configurables import CondDB
-CondDB().IgnoreHeartBeat = True
-
-# correlation plot
 from Configurables import AlgorithmCorrelationsAlg
 ac = AlgorithmCorrelationsAlg(Algorithms = sc.selections())
 
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
 
-DaVinci().PrintFreq = 500
+
+
+DaVinci().PrintFreq = 2000
 DaVinci().HistogramFile = 'DV_stripping_histos.root'
 DaVinci().ETCFile = "etc.root"
-DaVinci().EvtMax = 500000
+DaVinci().EvtMax = 50000
 DaVinci().EventPreFilters = [ filterHLT ]
 DaVinci().appendToMainSequence( [ sc.sequence() ] )
 DaVinci().appendToMainSequence( [ sr ] )
 DaVinci().appendToMainSequence( [ ac ] )
-DaVinci().MoniSequence += [ seq ]                     # Append the TagCreator to DaVinci
+DaVinci().MoniSequence += [ seq ]            # Append the TagCreator to DaVinci
 DaVinci().DataType = "2010"
 DaVinci().InputType = 'SDST'
-importOptions("$STRIPPINGSELECTIONSROOT/tests/data/RUN_79646_RealData+Reco06-Stripping10_90000000_SDST.py")
-
+importOptions("$STRIPPINGSELECTIONSROOT/tests/data/RUN_81430_RealData+Reco08-Stripping12_90000000_SDST.py")
