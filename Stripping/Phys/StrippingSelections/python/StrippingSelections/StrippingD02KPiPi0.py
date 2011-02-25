1639ce7b47006b8cb44968f8056e504c8d1cd86a
Olivier Deschamps <odescham@cern.ch>
2011-02-25 16:36:29 +0000

    Update D02KPiPi0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118986 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 84e0fc419a30bafba44dc786b02c88ec8d12841a

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '03/09/2010'
-__version__ = '$Revision: 1.1 $'
+__date__ = '2011.02.24'
+__version__ = '$Revision: 1.2 $'
 
 '''
 Stripping selection for D0 -> K pi pi0 
@@ -12,106 +12,144 @@ Stripping selection for D0 -> K pi pi0
 ####################################################################
 
 from Gaudi.Configuration import *
-from LHCbKernel.Configuration import *
+from GaudiConfUtils.ConfigurableGenerators import CombineParticles
+from PhysSelPython.Wrappers import Selection, DataOnDemand
+from StrippingConf.StrippingLine import StrippingLine
+from StrippingUtils.Utils import LineBuilder
+from StandardParticles import StdTightPions,StdTightKaons,StdLooseMergedPi0,StdLooseResolvedPi0
 
-class StrippingD02KPiPi0Conf(LHCbConfigurableUser):
-    __slots__ = {
-        'TrackMinPT'          : 300       # MeV
-        ,'TrackMinPT_R'       : 600       # MeV  (>= TrackMinPT else no effect)
-        ,'TrackMinTrackProb'  : 0.000001  # unitless
-        ,'TrackMinIPChi2'     : 16        # unitless
-        ,'Pi0MinPT_M'         : 2000      # MeV
-        ,'Pi0MinPT_R'         : 1000      # MeV
-        ,'ResPi0MinGamCL'     : 0.2       # unitless
-        ,'D0MinM'             : 1300      # MeV
-        ,'D0MaxM'             : 2400      # MeV
-        ,'D0MinVtxProb'       : 0.001     # unitless
-        ,'D0MaxIPChi2'        : 9         # unitless
-        ,'D0MinDIRA'          : 0.9999    # unitless
-        ,'D0MinVVDChi2'       : 64        # unitless
-         }
+class StrippingD02KPiPi0Conf(LineBuilder) :
 
-    ###############################################
-    # Create StrippingLines with these selections #
-    ###############################################
-    def D02KPiPi0_Merged( self ) :
-        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-        D02KPiPi0_Ks    = self.D02KPiPi0_Ks_Alg()
-        D02KPiPi0_Merged = self.D02KPiPi0_Merged_Alg()
-        return StrippingLine('D02KPiPi0_Merged_line'
-                             , prescale = 1
-                             , algos = [D02KPiPi0_Ks, D02KPiPi0_Merged]
-                             , postscale = 1
+    __configuration_keys__ = ( 'TrackMinPT'
+                               ,'TrackMinPT_R'
+                               ,'TrackMinTrackProb'
+                               ,'TrackMinIPChi2'
+                               ,'Pi0MinPT_M'
+                               ,'Pi0MinPT_R'
+                               ,'ResPi0MinGamCL'
+                               ,'D0MinM'
+                               ,'D0MaxM'
+                               ,'D0MinVtxProb'
+                               ,'D0MaxIPChi2'
+                               ,'D0MinDIRA'
+                               ,'D0MinVVDChi2'
+                               ,'MergedLinePrescale'
+                               ,'MergedLinePostscale'
+                               ,'ResolvedLinePrescale'
+                               ,'ResolvedLinePostscale'
+                               ,'prefix'
+                               )
+
+##############################################################
+    def __init__(self, name, config) :
+
+        LineBuilder.__init__(self, name, config)
+
+        myPions       = StdTightPions
+        myKaons       = StdTightKaons
+	myMergedPi0   = StdLooseMergedPi0
+	myResolvedPi0 = StdLooseResolvedPi0
+        #---------------------------------------
+        # hh selection
+        self.selkstar = makeD02KPiPi0kstar( 'D02KPiPi0_kstar',
+                                      config,
+                                      DecayDescriptor = '[K*(892)~0 -> K- pi+]cc',
+				      inputSel = [myPions,myKaons]
+                                      )
+        #---------------------------------------
+        # B -> HHPi0 selections
+	self.selresolved = makeD02KPiPi0R( 'D02KPiPi0R',
+                                           config,
+                                           DecayDescriptor = '[D0 -> K*(892)~0 pi0]cc',
+                                           inputSel = [self.selkstar, myResolvedPi0]
+                                         )				       
+	self.selmerged = makeD02KPiPi0M( 'D02KPiPi0M',
+                                         config,
+                                         DecayDescriptor = '[D0 -> K*(892)~0 pi0]cc',
+                                         inputSel = [self.selkstar, myMergedPi0]
+                                         )
+        #---------------------------------------
+        # Stripping lines
+        self.D02KPiPi0R_line = StrippingLine("%(prefix)s_R" %locals()['config'],
+                                             prescale = config['ResolvedLinePrescale'],
+                                             postscale = config['ResolvedLinePostscale'],
+                                             selection = self.selresolved
+                                             )
+        self.D02KPiPi0M_line = StrippingLine("%(prefix)s_M" %locals()['config'],
+                                             prescale = config['MergedLinePrescale'],
+                                             postscale = config['MergedLinePostscale'],
+                                             selection = self.selmerged
+                                             )
+        # register lines
+        self.registerLine(self.D02KPiPi0R_line)
+        self.registerLine(self.D02KPiPi0M_line)
+
+##############################################################
+def makeD02KPiPi0kstar(name,
+                   config,
+                   DecayDescriptor,
+                   inputSel
+                   ) :
+
+    _TrackCuts ="(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']
+    _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts }
+    _combCuts = "(AALL)"  % locals()['config']
+    _motherCuts = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s)" % locals()['config']
+
+    _kstar = CombineParticles( DecayDescriptor = DecayDescriptor,
+                             MotherCut = _motherCuts,
+                             CombinationCut = _combCuts,
+                             DaughtersCuts = _daughterCuts
                              )
 
-    def D02KPiPi0_Resolved( self ) :
-        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-        D02KPiPi0_Ks      = self.D02KPiPi0_Ks_Alg()
-        D02KPiPi0_Resolved = self.D02KPiPi0_Resolved_Alg()
-        return StrippingLine('D02KPiPi0_Resolved_line'
-                             , prescale = 1
-                             , algos = [D02KPiPi0_Ks, D02KPiPi0_Resolved]
-                             , postscale = 1
-                             )
+    return Selection ( name+'Sel',
+                       Algorithm = _kstar,
+                       RequiredSelections = inputSel )
 
-    ################
-    # K- pi+ comb. # 
-    ################
+##############################################################
+def makeD02KPiPi0R( name,
+                  config,
+                  DecayDescriptor,
+                  inputSel
+                  ) :
 
-    def D02KPiPi0_Ks_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	D02KPiPi0_Ks=CombineParticles("StripD02KPiPi0_Ks")
-	D02KPiPi0_Ks.InputLocations = ["StdTightPions", "StdTightKaons"]
-	D02KPiPi0_Ks.DecayDescriptor = "[K*(892)~0 -> K- pi+]cc"
-	D02KPiPi0_Ks.DaughtersCuts = {
-            "K-"   : "(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)"  % self.getProps()
-            ,"pi+" : "(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" % self.getProps()   
-            }
-	D02KPiPi0_Ks.CombinationCut = "AALL" % self.getProps()
-	D02KPiPi0_Ks.MotherCut = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s)" % self.getProps()
-        return D02KPiPi0_Ks
+    _kstarCuts = "(CHILD(PT,1)>%(TrackMinPT_R)s *MeV) & (CHILD(PT,2)>%(TrackMinPT_R)s *MeV)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" %locals()['config']
+    _daughterCuts = { 'K*(892)~0' : _kstarCuts, 'pi0' : _pi0Cuts }
+    _combCuts = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
+    _motherCuts = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
 
-    ##########
-    # Merged # 
-    ##########
-    
-    def D02KPiPi0_Merged_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	D02KPiPi0M=CombineParticles("StripD02KPiPi0M")
-	D02KPiPi0M.InputLocations = ["StripD02KPiPi0_Ks", "StdLooseMergedPi0"]
-	D02KPiPi0M.DecayDescriptor = "[D0 -> K*(892)~0 pi0]cc"
-	D02KPiPi0M.DaughtersCuts = {
-            "K*(892)~0" : "ALL" % self.getProps()
-            ,"pi0"      : "(PT>%(Pi0MinPT_M)s *MeV)" % self.getProps()   
-            }
-	D02KPiPi0M.CombinationCut = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % self.getProps()
-	D02KPiPi0M.MotherCut = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % self.getProps()
-        return D02KPiPi0M
+    _D = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts
+                           )
 
-    ############
-    # Resolved # 
-    ############
-    def D02KPiPi0_Resolved_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	D02KPiPi0R=CombineParticles("StripD02KPiPi0R")
-	D02KPiPi0R.InputLocations = ["StripD02KPiPi0_Ks", "StdLooseResolvedPi0"]
-	D02KPiPi0R.DecayDescriptor = "[D0 -> K*(892)~0 pi0]cc"
-	D02KPiPi0R.DaughtersCuts = {
-            "K*(892)~0" : "(CHILD(PT,1)>%(TrackMinPT_R)s *MeV) & (CHILD(PT,2)>%(TrackMinPT_R)s *MeV)" % self.getProps()     
-            ,"pi0"      : "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % self.getProps()
-            }
-	D02KPiPi0R.CombinationCut = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % self.getProps()
-	D02KPiPi0R.MotherCut = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % self.getProps()
-        return D02KPiPi0R
+    return Selection( name+'Sel',
+                      Algorithm = _D,
+                      RequiredSelections = inputSel
+                      )
+##############################################################
+def makeD02KPiPi0M( name,
+                  config,
+                  DecayDescriptor,
+                  inputSel
+                  ) :
 
-    ######
-    ######
-    ######
-    def getProps(self) :
-        d = dict()
-        for (k,v) in self.getDefaultProperties().iteritems() :
-            d[k] = getattr(self,k) if hasattr(self,k) else v
-        return d
+    _kstarCuts = "(ALL)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_M)s *MeV)" %locals()['config']
+    _daughterCuts = { 'K*(892)~0' : _kstarCuts, 'pi0' : _pi0Cuts }
+    _combCuts = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
+    _motherCuts = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
+
+    _B = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts
+                           )
+
+    return Selection( name+'Sel',
+                      Algorithm = _B,
+                      RequiredSelections = inputSel
+                      )
+##############################################################
