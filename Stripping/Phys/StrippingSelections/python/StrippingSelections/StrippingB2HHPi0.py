eead6eee057d18fb5c6ffb9b3e619592e5d55aa7
Olivier Deschamps <odescham@cern.ch>
2011-02-25 14:24:37 +0000

    update B2HHPi0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@118957 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 68d1a148e5dc6a7ad3f48c7075cda1fd778383b3

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '31/08/2010'
-__version__ = '$Revision: 1.2 $'
+__date__ = '2011.02.24'
+__version__ = '$Revision: 1.3 $'
 
 '''
 Stripping selection for B -> h h pi0
@@ -13,107 +13,170 @@ Stripping selection for B -> h h pi0
 #################################################################
 
 from Gaudi.Configuration import *
-from LHCbKernel.Configuration import *
+from GaudiConfUtils.ConfigurableGenerators import CombineParticles
+from PhysSelPython.Wrappers import Selection, DataOnDemand
+from StrippingConf.StrippingLine import StrippingLine
+from StrippingUtils.Utils import LineBuilder
+from StandardParticles import StdNoPIDsPions,StdLooseMergedPi0,StdLooseResolvedPi0
 
-class StrippingB2HHPi0Conf(LHCbConfigurableUser):
-    __slots__ = {
-        'PiMinPT'               : 500       # MeV
-        ,'PiMinP'               : 5000      # MeV
-        ,'PiMinTrackProb'       : 0.000001  # unitless
-        ,'PiMinIPChi2'          : 25        # unitless
-        ,'Pi0MinPT_M'           : 2500      # MeV
-        ,'Pi0MinPT_R'           : 1500      # MeV
-        ,'ResPi0MinMM'          : 100       # MeV  # no effect if bellow 105
-        ,'ResPi0MaxMM'          : 170       # MeV  # no effect if above  165
-        ,'ResPi0MinGamCL'       : 0.2       # unitless
-        ,'BMinM'                : 4200      # MeV
-        ,'BMaxM'                : 6400      # MeV
-        ,'BMinPT_M'             : 3000      # MeV
-        ,'BMinPT_R'             : 2500      # MeV
-        ,'BMinVtxProb'          : 0.001     # unitless
-        ,'BMaxIPChi2'           : 9         # unitless
-        ,'BMinDIRA'             : 0.99995   # unitless
-        ,'BMinVVDChi2'          : 64        # unitless
-         }
+default_config = {   'PiMinPT'              : 500       # MeV
+		    ,'PiMinP'               : 5000      # MeV
+		    ,'PiMinTrackProb'       : 0.000001  # unitless
+		    ,'PiMinIPChi2'          : 25        # unitless
+		    ,'Pi0MinPT_M'           : 2500      # MeV
+		    ,'Pi0MinPT_R'           : 1500      # MeV
+		    ,'ResPi0MinMM'          : 100       # MeV  # no effect if bellow 105
+		    ,'ResPi0MaxMM'          : 170       # MeV  # no effect if above  165
+		    ,'ResPi0MinGamCL'       : 0.2       # unitless
+		    ,'BMinM'                : 4200      # MeV
+		    ,'BMaxM'                : 6400      # MeV
+		    ,'BMinPT_M'             : 3000      # MeV
+		    ,'BMinPT_R'             : 2500      # MeV
+		    ,'BMinVtxProb'          : 0.001     # unitless
+		    ,'BMaxIPChi2'           : 9         # unitless
+		    ,'BMinDIRA'             : 0.99995   # unitless
+		    ,'BMinVVDChi2'          : 64        # unitless
+		    ,'MergedLinePrescale'   : 1.        # unitless
+		    ,'MergedLinePostscale'  : 1.        # unitless
+		    ,'ResolvedLinePrescale' : 1.        # unitless
+		    ,'ResolvedLinePostscale': 1.        # unitless
+		    ,'prefix'               : 'StripB2HHPi0'
+		 }
 
-    ###############################################
-    # Create StrippingLines with these selections #
-    ###############################################
-    def B2HHPi0_Merged( self ) :
-        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-        B2HHPi0_rho    = self.B2HHPi0_rho_Alg()
-        B2HHPi0_Merged = self.B2HHPi0_Merged_Alg()
-        return StrippingLine('B2HHPi0_Merged_line'
-                             , prescale = 1
-                             , algos = [B2HHPi0_rho, B2HHPi0_Merged]
-                             , postscale = 1
+class StrippingB2HHPi0Conf(LineBuilder) :
+
+    __configuration_keys__ = ( 'PiMinPT'
+                               ,'PiMinP'
+                               ,'PiMinTrackProb'
+			       ,'PiMinIPChi2'
+			       ,'Pi0MinPT_M'
+			       ,'Pi0MinPT_R'
+			       ,'ResPi0MinMM'
+			       ,'ResPi0MaxMM'
+			       ,'ResPi0MinGamCL'
+			       ,'BMinM'
+			       ,'BMaxM'
+			       ,'BMinPT_M'
+			       ,'BMinPT_R'
+			       ,'BMinVtxProb'
+			       ,'BMaxIPChi2'
+			       ,'BMinDIRA'
+			       ,'BMinVVDChi2'
+			       ,'MergedLinePrescale'
+			       ,'MergedLinePostscale'
+			       ,'ResolvedLinePrescale'
+			       ,'ResolvedLinePostscale'
+			       ,'prefix'
+                               )
+
+##############################################################
+    def __init__(self, name, config) :
+
+        LineBuilder.__init__(self, name, config)
+
+        myPions       = StdNoPIDsPions
+	myMergedPi0   = StdLooseMergedPi0
+	myResolvedPi0 = StdLooseResolvedPi0
+        #---------------------------------------
+        # hh selection
+        self.selrho = makeB2HHPi0rho( 'B2HHPi0_rho',
+                                      config,
+                                      DecayDescriptor = 'rho(770)0 -> pi+ pi-',
+				      inputSel = [myPions]
+                                      )
+        #---------------------------------------
+        # B -> HHPi0 selections
+	self.selresolved = makeB2HHPi0R( 'B2HHPi0R',
+	                                 config,
+					 DecayDescriptor = 'B0 -> rho(770)0 pi0',
+					 inputSel = [self.selrho, myResolvedPi0]
+                                         )				       
+	self.selmerged = makeB2HHPi0M( 'B2HHPi0M',
+	                               config,
+				       DecayDescriptor = 'B0 -> rho(770)0 pi0',
+				       inputSel = [self.selrho, myMergedPi0]
+                                       )
+        #---------------------------------------
+        # Stripping lines
+        self.B2HHPi0R_line = StrippingLine("%(prefix)s_R" %locals()['config'],
+                                           prescale = config['ResolvedLinePrescale'],
+                                           postscale = config['ResolvedLinePostscale'],
+                                           selection = self.selresolved
+                                           )
+        self.B2HHPi0M_line = StrippingLine("%(prefix)s_M" %locals()['config'],
+                                           prescale = config['MergedLinePrescale'],
+                                           postscale = config['MergedLinePostscale'],
+                                           selection = self.selmerged
+                                           )
+        # register lines
+        self.registerLine(self.B2HHPi0R_line)
+        self.registerLine(self.B2HHPi0M_line)
+
+##############################################################
+def makeB2HHPi0rho(name,
+                   config,
+                   DecayDescriptor,
+                   inputSel
+                   ) :
+
+    _piCuts ="(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" %locals()['config']
+    _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts }
+    _combCuts = "(AALL)"  % locals()['config']
+    _motherCuts = "(VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s)" % locals()['config']
+
+    _rho = CombineParticles( DecayDescriptor = DecayDescriptor,
+                             MotherCut = _motherCuts,
+                             CombinationCut = _combCuts,
+                             DaughtersCuts = _daughterCuts
                              )
 
-    def B2HHPi0_Resolved( self ) :
-        from StrippingConf.StrippingLine import StrippingLine, StrippingMember
-        B2HHPi0_rho      = self.B2HHPi0_rho_Alg()
-        B2HHPi0_Resolved = self.B2HHPi0_Resolved_Alg()
-        return StrippingLine('B2HHPi0_Resolved_line'
-                             , prescale = 1
-                             , algos = [B2HHPi0_rho, B2HHPi0_Resolved]
-                             , postscale = 1
-                             )
+    return Selection ( name+'Sel',
+                       Algorithm = _rho,
+                       RequiredSelections = inputSel )
+##############################################################
+def makeB2HHPi0R( name,
+                  config,
+                  DecayDescriptor,
+                  inputSel
+                  ) :
 
-    ##########
-    # Merged # 
-    ##########
+    _rhoCuts = "(ALL)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_R)s *MeV) & (MM>%(ResPi0MinMM)s *MeV) & (MM<%(ResPi0MaxMM)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" %locals()['config']
+    _daughterCuts = { 'rho(770)0' : _rhoCuts, 'pi0' : _pi0Cuts }
+    _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
+    _motherCuts = "(PT>%(BMinPT_R)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
 
-    def B2HHPi0_rho_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	B2HHPi0_rho=CombineParticles("StripB2HHPi0_rho")
-	B2HHPi0_rho.InputLocations = ["StdLoosePions"]
-	B2HHPi0_rho.DecayDescriptor = "rho(770)0 -> pi+ pi-"
-	B2HHPi0_rho.DaughtersCuts = {
-            "pi-"  : "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % self.getProps()
-            ,"pi+" : "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % self.getProps()   
-            }
-	B2HHPi0_rho.CombinationCut = "AALL" % self.getProps()
-	B2HHPi0_rho.MotherCut = "(VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s)" % self.getProps()
-        return B2HHPi0_rho
-    
-    def B2HHPi0_Merged_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	B2HHPi0M=CombineParticles("StripB2HHPi0M")
-	B2HHPi0M.InputLocations = ["StripB2HHPi0_rho", "StdLooseMergedPi0"]
-	B2HHPi0M.DecayDescriptor = "B0 -> rho(770)0 pi0"
-	B2HHPi0M.DaughtersCuts = {
-            "rho(770)0" : "ALL" % self.getProps()
-            ,"pi0" : "(PT>%(Pi0MinPT_M)s *MeV)" % self.getProps()   
-            }
-	B2HHPi0M.CombinationCut = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % self.getProps()
-	B2HHPi0M.MotherCut = "(PT>%(BMinPT_M)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % self.getProps()
-        return B2HHPi0M
+    _B = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts
+                           )
 
-    ############
-    # Resolved # 
-    ############
-    def B2HHPi0_Resolved_Alg(self):
-        from Configurables import CombineParticles
-        import GaudiKernel.SystemOfUnits as Units    
-	B2HHPi0R=CombineParticles("StripB2HHPi0R")
-	B2HHPi0R.InputLocations = ["StripB2HHPi0_rho", "StdLooseResolvedPi0"]
-	B2HHPi0R.DecayDescriptor = "B0 -> rho(770)0 pi0"
-	B2HHPi0R.DaughtersCuts = {
-            "rho(770)0" : "ALL" % self.getProps()     
-            ,"pi0" : "(PT>%(Pi0MinPT_R)s *MeV) & (MM>%(ResPi0MinMM)s *MeV) & (MM<%(ResPi0MaxMM)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % self.getProps()   
-            }
-	B2HHPi0R.CombinationCut = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % self.getProps()
-	B2HHPi0R.MotherCut = "(PT>%(BMinPT_R)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % self.getProps()
-        return B2HHPi0R
+    return Selection( name+'Sel',
+                      Algorithm = _B,
+                      RequiredSelections = inputSel
+                      )
+##############################################################
+def makeB2HHPi0M( name,
+                  config,
+                  DecayDescriptor,
+                  inputSel
+                  ) :
 
+    _rhoCuts = "(ALL)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_M)s *MeV)" %locals()['config']
+    _daughterCuts = { 'rho(770)0' : _rhoCuts, 'pi0' : _pi0Cuts }
+    _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
+    _motherCuts = "(PT>%(BMinPT_M)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
 
-    ######
-    ######
-    ######
-    def getProps(self) :
-        d = dict()
-        for (k,v) in self.getDefaultProperties().iteritems() :
-            d[k] = getattr(self,k) if hasattr(self,k) else v
-        return d
+    _B = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts
+                           )
+
+    return Selection( name+'Sel',
+                      Algorithm = _B,
+                      RequiredSelections = inputSel
+                      )
+##############################################################
