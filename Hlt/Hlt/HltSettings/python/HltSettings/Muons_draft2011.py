50ae508cadc8978651ac6dfc4cb0833dff1ea428
Johannes Albrecht <johannes.albrecht@cern.ch>
2011-03-01 18:40:35 +0000

    add D2mm lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@119342 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 42682f402ed188d290190f1e0bc8682c69b612ba

--- Hlt/Hlt/HltSettings/python/HltSettings/Muons_draft2011.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Muons_draft2011.py
@@ -54,13 +54,17 @@ class Muons_draft2011 :
             #'Hlt2MuTrack2',
             #'Hlt2MuTrack3',
              
-            'Hlt2Dst2PiD02MuMu',         
-            #'Hlt2Dst2PiD02EMu', 
-            'Hlt2CharmRareDecayD02MuMu',
-            #'Hlt2CharmRareDecayD02PiPiForD02MuMu',
-
             'Hlt2TriMuonsDetached',
-            'Hlt2TriMuonsTau'
+            'Hlt2TriMuonsTau',
+      
+            'Hlt2CharmRareDecayD02MuMu',    
+            "Hlt2Dst2PiD02PiPi",            
+            "Hlt2Dst2PiD02KPi",             
+            "Hlt2Dst2PiD02MuMu",            
+            "Hlt2Dst2PiD02KMu",             
+            
+            'Hlt2TFBc2JpsiMuX',
+            'Hlt2TFBc2JpsiMuXSignal'
             ]
             
         return lines
@@ -78,6 +82,8 @@ class Muons_draft2011 :
         from Hlt2Lines.Hlt2InclusiveDiMuonLines  import Hlt2InclusiveDiMuonLinesConf
         from Hlt2Lines.Hlt2MuNTrackLines  import Hlt2MuNTrackLinesConf
         from Hlt2Lines.Hlt2CharmRareDecayLines  import Hlt2CharmRareDecayLinesConf
+        from Hlt2Lines.Hlt2Dst2D2XXLines import Hlt2Dst2D2XXLinesConf
+
         d.update( { Hlt2InclusiveDiMuonLinesConf : { 
             ## Cut values
             'UnbiasedDiMuonMinMass'    : 2900      # MeV
@@ -104,9 +110,6 @@ class Muons_draft2011 :
             ,'DetachedJPsiDLS'           : 3
             ,'DetachedJPsiMassWindow'    : 120 #MeV 
 
-            
-            #,'DYPt'                    : 0.0      # MeV
-
             , 'Prescale'   : { 'Hlt2DiMuon'          : 0.01
                                , 'Hlt2DiMuonLowMass' : 0.002
                                , 'Hlt2DiMuonDY1'              :  0.05
@@ -127,51 +130,22 @@ class Muons_draft2011 :
                               }
             }}
                   )
-        
-        d.update( { Hlt2MuNTrackLinesConf : {
-            'L0FILTER'            : "L0_CHANNEL_RE('.*Muon')"
-            ,'HLT1FILTER'         : ""#"HLT_PASS_RE('Hlt1TrackMuon.*Decision')"
-            #mu + n tracks filter cuts
-            ,'MCOR_MAX'           : 7000.0  # MeV
-            ,'MCOR_MIN'           : 0.0     # MeV 
-            ,'MCOR_NTR_MIN'       : 4000.0  # MeV 
-            ,'SUM_PT_1TR_MIN'     : 2500.0  # MeV
-            ,'SUM_PT_2TR_MIN'     : 2500.0  # MeV
-            ,'SUM_PT_3TR_MIN'     : 2600.0  # MeV
-            ,'MAX_PT_MIN'         : 1500.0  # MeV 
-            ,'MAX_PT_NTR_MIN'     : 1500.0  # MeV 
-            ,'SUM_IPCHI2_1TR_MIN' : 50      # unitless
-            ,'SUM_IPCHI2_2TR_MIN' : 75      # unitless
-            ,'SUM_IPCHI2_3TR_MIN' : 100     # unitless
-            ,'BPVVDCHI2_MIN'      : 150.0    # unitless
-            ,'MIN_TRCHI2DOF_MAX'  : 2       # unitless
-            #combination cuts
-            ,'AMAXDOCA_MAX'       : 0.12    # mm 
-            ,'AMAXDOCA_MIN'       : 0.12    # mm 
-            ,'DIRA_LOOSE_MIN'     : 0.99    # rad
-            ,'DIRA_TIGHT_MIN'     : 0.995   # rad
-            # mother cuts
-            ,'MASS_1TR_VETO'      : 2000.0  # MeV 
-            ,'MASS_2TR_VETO'      : 2000.0  # MeV 
-            ,'MASS_3TR_VETO'      : 2000.0  # MeV 
-            # cuts on input particles
-            ,'ALL_MIPCHI2DV_MIN'  : 16.0    # unitless
-            ,'ALL_TRCHI2DOF_MAX'  : 5.0     # unitless
-            ,'ALL_MU_PT_MIN'      : 1000.0   # MeV
-            ,'ALL_TR_PT_MIN'      : 600.0   # MeV
-            ,'ALL_P_MIN'          : 5000.0  # MeV
-            ,'Prescale'   : {  'Hlt2MuTrack1'  : 1.0
-                              ,'Hlt2MuTrack2'  : 1.0
-                              ,'Hlt2MuTrack3'  : 0.0
-                              }
-            }})
+           
         d.update({Hlt2CharmRareDecayLinesConf : { 
                           'D02MuMuMinDaughterPt'            : 1.0 # GeV
                         , 'D02MuMuMinLifeTime'              : 0.1 # ps
                         , 'D02PiPiForD02MuMuMinLifeTime'    : 0.3 # ps
                         , 'Prescale'                        : { 
                             'Hlt2CharmRareDecayD02MuMu'           : 1.0 
-                          , 'Hlt2CharmRareDecayD02PiPiForD02MuMu' : 0.1 
+                            }
+            }})
+
+        d.update({Hlt2Dst2D2XXLinesConf : { 
+                    'Prescale'                        : { 
+                        'Hlt2Dst2PiD02PiPi'           : 0.006 
+                        , 'Hlt2Dst2PiD02KPi'          : 0.002
+                        , 'Hlt2Dst2PiD02MuMu'         : 1
+                        , 'Hlt2Dst2PiD02KMu'          : 0.03
                           }
             }})
         
