0e96d96de41d91a926fa3f011b1e1b32a7981d09
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-03-01 10:07:47 +0000

    Improvements to test application
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@119259 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c3fc275f00fc84a33ed3069a5c0e024cee5de1d5

--- LHCb/Kernel/LHCbKernel/tests/RichSmartIDTest/main.cpp
+++ LHCb/Kernel/LHCbKernel/tests/RichSmartIDTest/main.cpp
@@ -1,36 +1,35 @@
 
 // Include files
-#include <cmath>
-#include <vector>
+//#include <cmath>
+//#include <vector>
 #include <iostream>
-#include <iomanip>
-#include <float.h>
+//#include <iomanip>
+//#include <float.h>
 
 #include "Kernel/RichSmartID.h"
 
 using namespace std;
 
-//--- Example main program
 int main ( int /*argc*/, char** /*argv*/ )
 {
 
   bool OK = true;
 
-  for ( unsigned int irich=0; irich<2; ++irich )
+  for ( unsigned int irich = 0; irich <= LHCb::RichSmartID::HPD::MaxRich; ++irich )
   {
     const Rich::DetectorType rich = (Rich::DetectorType)irich;
-    for ( unsigned int ipanel=0; ipanel<2; ++ipanel )
+    for ( unsigned int ipanel = 0; ipanel <= LHCb::RichSmartID::HPD::MaxPanel; ++ipanel )
     {
       const Rich::Side panel = (Rich::Side)ipanel;
-      for ( unsigned int pdrow=0; pdrow<LHCb::RichSmartID::HPD::MaxPDNumInCol; ++pdrow )
+      for ( unsigned int pdrow = 0; pdrow <= LHCb::RichSmartID::HPD::MaxPDNumInCol; ++pdrow )
       {
-        for ( unsigned int pdcol=0; pdcol<LHCb::RichSmartID::HPD::MaxPDCol; ++pdcol )
+        for ( unsigned int pdcol = 0; pdcol <= LHCb::RichSmartID::HPD::MaxPDCol; ++pdcol )
         {
-          for ( unsigned int pixrow=0; pixrow<LHCb::RichSmartID::HPD::MaxPixelRow; ++pixrow )
+          for ( unsigned int pixrow = 0; pixrow <= LHCb::RichSmartID::HPD::MaxPixelRow; ++pixrow )
           {
-            for ( unsigned int pixcol=0; pixcol<LHCb::RichSmartID::HPD::MaxPixelCol; ++pixcol )
+            for ( unsigned int pixcol = 0; pixcol <= LHCb::RichSmartID::HPD::MaxPixelCol; ++pixcol )
             {
-              for ( unsigned int subpix=0; subpix<LHCb::RichSmartID::HPD::MaxPixelSubRow; ++subpix )
+              for ( unsigned int subpix = 0; subpix <= LHCb::RichSmartID::HPD::MaxPixelSubRow; ++subpix )
               {
 
                 // Make a test smart ID
@@ -60,19 +59,19 @@ int main ( int /*argc*/, char** /*argv*/ )
     }
   }
 
-  for ( unsigned int irich=0; irich<2; ++irich )
+  for ( unsigned int irich = 0; irich <= LHCb::RichSmartID::MaPMT::MaxRich; ++irich )
   {
     const Rich::DetectorType rich = (Rich::DetectorType)irich;
-    for ( unsigned int ipanel=0; ipanel<2; ++ipanel )
+    for ( unsigned int ipanel = 0; ipanel <= LHCb::RichSmartID::MaPMT::MaxPanel; ++ipanel )
     {
       const Rich::Side panel = (Rich::Side)ipanel;
-      for ( unsigned int pdrow=0; pdrow<LHCb::RichSmartID::MaPMT::MaxPDNumInCol; ++pdrow )
+      for ( unsigned int pdrow = 0; pdrow <= LHCb::RichSmartID::MaPMT::MaxPDNumInCol; ++pdrow )
       {
-        for ( unsigned int pdcol=0; pdcol<LHCb::RichSmartID::MaPMT::MaxPDCol; ++pdcol )
+        for ( unsigned int pdcol = 0; pdcol <= LHCb::RichSmartID::MaPMT::MaxPDCol; ++pdcol )
         {
-          for ( unsigned int pixrow=0; pixrow<LHCb::RichSmartID::MaPMT::MaxPixelRow; ++pixrow )
+          for ( unsigned int pixrow = 0; pixrow <= LHCb::RichSmartID::MaPMT::MaxPixelRow; ++pixrow )
           {
-            for ( unsigned int pixcol=0; pixcol<LHCb::RichSmartID::MaPMT::MaxPixelCol; ++pixcol )
+            for ( unsigned int pixcol = 0; pixcol <= LHCb::RichSmartID::MaPMT::MaxPixelCol; ++pixcol )
             {
 
               // Make a test smart ID
