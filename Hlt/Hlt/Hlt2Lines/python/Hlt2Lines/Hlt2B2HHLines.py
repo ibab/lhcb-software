16a6288474c844f4faf495e06465915e3aa25157
Stefano Perazzini <stefano.perazzini@cern.ch>
2011-03-01 11:32:32 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@119272 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: b9706cfa24da0667cb201f79b51c319c76e65c36

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2HHLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2B2HHLines.py
@@ -3,15 +3,15 @@ from HltLine.HltLinesConfigurableUser import HltLinesConfigurableUser
 
 
 class Hlt2B2HHLinesConf(HltLinesConfigurableUser) :
-    __slots__ = {  'BMassWinLow'         : 4800      # MeV
-                   ,'BMassWinHigh'       : 5800      # MeV
-                   ,'doca'               : 0.1       # MeV
-                   ,'PionPTmin'          : 700       # MeV
-                   ,'PionPTmax'          : 2400      # MeV
-                   ,'PionIPmin'          : 0.08      # mm
-                   ,'PionIPmax'          : 0.24      # mm
-                   ,'BIP'                : 0.1       # mm
-                   ,'BSignDistFlight'    : 2.2       # mm
+    __slots__ = {  'BMassWinLow'         : 4700   # MeV
+                   ,'BMassWinHigh'       : 5900   # MeV
+                   ,'TrChi2'             : 5      # 
+                   ,'PionPT'             : 1000   # MeV
+                   ,'PionIP'             : 0.12   # mm
+                   ,'doca'               : 0.1    # mm
+                   ,'BIP'                : 0.12   # mm
+                   ,'BPT'                : 1200   # MeV/c
+                   ,'BTAU'               : 0.0006 # mm
                    }
     
 
@@ -24,14 +24,20 @@ class Hlt2B2HHLinesConf(HltLinesConfigurableUser) :
         from Configurables import FilterDesktop
         from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedPions        
         
+        _daughters_cut = "( TRCHI2DOF < %(TrChi2)s ) & ( PT > %(PionPT)s *MeV ) & ( MIPDV(PRIMARY) > %(PionIP)s )" % self.getProps()
+
+        _combination_cut = "(AM>%(BMassWinLow)s*MeV) & (AM<%(BMassWinHigh)s*MeV) & ( AMAXDOCA('') < %(doca)s )" % self.getProps()
+
+        _mother_cut = "( PT > %(BPT)s ) & (BPVIP() < %(BIP)s ) & ( BPVLTIME('PropertimeFitter/properTime:PUBLIC') > %(BTAU)s )" % self.getProps()
+
 
         Hlt2B2HH = Hlt2Member( CombineParticles 
                                , "Combine"     
                                , DecayDescriptor = "B0 -> pi+ pi-"
-                               , CombinationCut = "(in_range(%(BMassWinLow)s *MeV, AM, %(BMassWinHigh)s *MeV) & (AMAXDOCA('LoKi::TrgDistanceCalculator')< %(doca)s ))" % self.getProps()
-                               , DaughtersCuts = { "pi+" : "(PT> %(PionPTmin)s *MeV) & (MIPDV(PRIMARY)> %(PionIPmin)s )" % self.getProps() }
-                               , MotherCut = "(BPVIP()< %(BIP)s ) & (BPVVDSIGN> %(BSignDistFlight)s ) & (INTREE ( (ABSID=='pi+') & (PT> %(PionPTmax)s *MeV))) & ( INTREE(( ABSID=='pi+') & (MIPDV(PRIMARY)> %(PionIPmax)s )))" % self.getProps()
-                               , Inputs = [ BiKalmanFittedPions ])
+                               , DaughtersCuts = { 'pi+' : _daughters_cut }
+                               , CombinationCut = _combination_cut
+                               , MotherCut = _mother_cut
+                               , Inputs = [ BiKalmanFittedPions.outputSelection() ])
         ###########################################################################
         # Define the Hlt2 Line
         #
