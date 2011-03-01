4406f970359604bfa2973ed89ebc1c243c196320
Roel Aaij <roel.aaij@cern.ch>
2011-03-01 13:37:03 +0000

    Align cuts for Hlt1MuonLines to PT > 500 in LooseForward
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@119298 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3dc7e1c7d88e28042195dcf413dfd2306b260a3e

--- Hlt/Hlt/HltSettings/python/HltSettings/Physics_draft2011.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Physics_draft2011.py
@@ -80,18 +80,18 @@ class Physics_draft2011( object ):
                                              ,'DiMuonLowMass_VxDOCA'     :  0.2
                                              ,'DiMuonLowMass_VxChi2'     :   25
                                              ,'DiMuonLowMass_P'          : 6000
-                                             ,'DiMuonLowMass_PT'         :  600
+                                             ,'DiMuonLowMass_PT'         :  500
                                              ,'DiMuonLowMass_TrChi2'     :    4
                                              ,'DiMuonLowMass_M'          : 1000
                                              ,'DiMuonLowMass_IPChi2'     :    4
                                              ,'DiMuonHighMass_VxDOCA'    :  0.2
                                              ,'DiMuonHighMass_VxChi2'    :   25
                                              ,'DiMuonHighMass_P'         : 6000
-                                             ,'DiMuonHighMass_PT'        :  600
+                                             ,'DiMuonHighMass_PT'        :  500
                                              ,'DiMuonHighMass_TrChi2'    :    4
                                              ,'DiMuonHighMass_M'         : 2900
                                              ,'MultiMuonNoIP_P'          : 6000
-                                             ,'MultiMuonNoIP_PT'         :  600
+                                             ,'MultiMuonNoIP_PT'         :  500
                                              ,'MultiMuonNoIP_TrChi2'     :    4
                                              ,'MultiMuonNoIP_GT'         :  2.5
                                              ,'Prescale'                 : { 'Hlt1SingleMuonNoIP' : 0.1,
