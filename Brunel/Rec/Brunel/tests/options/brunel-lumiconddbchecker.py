9e2afe6691813a06561a30e80445a454db6dbc78
Jaap Panman <panmanj@cern.ch>
2011-03-02 15:15:44 +0000

    brunel-lumiconddbchecker to test online partition usage for lumi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@119437 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 19038bc8c6b1fa92aa9601d8df923a10d4764068

---
+++ Brunel/Rec/Brunel/tests/options/brunel-lumiconddbchecker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env gaudirun.py
+#
+import os
+from Gaudi.Configuration import *
+
+#--- switch on xml summary
+from Configurables import LHCbApp
+from Configurables import CondDB
+
+#-- set explicit CondDB tag
+LHCbApp().CondDBtag = 'head-20110131'
+
+#--- determine application to run
+from Configurables import LumiAlgsConf, LumiCheckCondDB
+
+# standard sequence from configurable
+sequence = GaudiSequencer("CheckDB")
+
+# normalization of BeamCrossing
+seqMembers=[]
+seqMembers.append( LumiCheckCondDB('CheckCondDB', UseOnline=True, ) )
+sequence.Members = seqMembers
+sequence.MeasureTime = True
+sequence.ModeOR = False
+sequence.ShortCircuit = True
+sequence.IgnoreFilterPassed = False
+
+#-- main
+ApplicationMgr( TopAlg = [ GaudiSequencer( "CheckDB"),
+                           ], HistogramPersistency = 'NONE' )
+
+#-- Test input not needed
+files = [ ]
+
+#-- input options
+ApplicationMgr().EvtMax =  -1
+importOptions("$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts")
+EventSelector( OutputLevel  = INFO, 
+               PrintFreq    = 1000,
+               FirstEvent   = 1,
+               Input        = files
+               )
+
