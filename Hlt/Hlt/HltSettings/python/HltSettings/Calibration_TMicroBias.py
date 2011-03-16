d9c99b57b452dd260d9d36f61ff86b94dc51ffc9
Johannes Albrecht <johannes.albrecht@cern.ch>
2011-03-16 15:01:09 +0000

    initial revision
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@120276 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f66504664d458ea780afe5dcea451ca6c5c6d29f

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Calibration_TMicroBias.py
@@ -0,0 +1,46 @@
+class Calibration_TMicroBias :
+    """
+    Threshold settings for T-Station micro bias 
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author J. Albrecht
+    @date 2010-08-09
+    """
+    
+    
+    def __init__(self) :
+        self.StripEndSequence = ['']
+
+    def HltType(self) :
+        return 'Calibration_TMicroBias'
+
+    def L0TCK(self) :
+        return '0x1710'
+
+    def ActiveHlt2Lines(self) :
+        """
+        Returns a list of active Hlt2 lines
+        """
+        return  []
+   
+    def ActiveHlt1Lines(self) :
+        """
+        Returns a list of active Hlt1 lines
+        """
+        return [ 'Hlt1MBMicroBiasTStation' ]
+
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        from Hlt1Lines.Hlt1MBLines             import Hlt1MBLinesConf
+        d= { Hlt1MBLinesConf : { 'MicroBiasOdin' : '( (ODIN_TRGTYP == LHCb.ODIN.PhysicsTrigger) | (ODIN_TRGTYP == LHCb.ODIN.TimingTrigger) )'
+                                 ,'Prescale' : { 'Hlt1MBMicroBias.*' : 0.01 } 
+#                                   , 'Postscale': { 'Hlt1MBMicroBias.*' : 'RATE(5000)' }
+                                   }
+             }
+        
+
+
+        return d
