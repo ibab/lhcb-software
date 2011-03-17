5672ddfc3be9c46d932da0527c5ae42573dced67
Wenbin Qian <wenbin.qian@cern.ch>
2011-03-17 14:41:27 +0000

    add some xx.h files for the change
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@120361 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 94409b039448d68766b28dfbb95fbfb04d0bc8e6

---
+++ Rec/Rec/RecoUpgradeMC/src/PatCheckerUpgrade.h
@@ -0,0 +1,75 @@
+// $Id: PatCheckerUpgrade.h,v 1.3 2008-06-01 17:28:53 mjohn Exp $
+#ifndef PATCHECKERUPGRADE_H 
+#define PATCHECKERUPGRADE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "GaudiAlg/ISequencerTimerTool.h"
+
+// local
+#include "PatCounterUpgrade.h"
+#include "PatTTCounter.h"
+
+/** @class PatCheckerUpgrade PatCheckerUpgrade.h
+ *  Check the quality of the pattern, by comparing to MC information
+ *
+ *  @author Olivier Callot
+ *  @date   2005-03-29
+ * Modified by Wenbin Qian for VeloPix
+ *  @adapted to A-Team framework 2007-08-20 SHM
+ */
+  
+  class PatCheckerUpgrade : public GaudiAlgorithm {
+  public: 
+    /// Standard constructor
+    PatCheckerUpgrade( const std::string& name, ISvcLocator* pSvcLocator );
+    
+    virtual ~PatCheckerUpgrade( ); ///< Destructor
+    
+    virtual StatusCode initialize();    ///< Algorithm initialization
+    virtual StatusCode execute   ();    ///< Algorithm execution
+    virtual StatusCode finalize  ();    ///< Algorithm finalization
+    
+  protected:
+    
+    
+  private:
+    bool m_level1;
+    bool m_checkMissedVeloPix;
+    bool m_checkMissedForward;
+    bool m_checkMissedSeed;
+    bool m_checkMissedDownstream;
+    bool m_checkMatchNotForward;
+    
+    PatCounterUpgrade* m_veloPix;
+    PatCounterUpgrade* m_veloTT;
+    PatCounterUpgrade* m_forward; 
+    PatCounterUpgrade* m_tsa;
+    PatCounterUpgrade* m_tTrack;
+    PatCounterUpgrade* m_match;
+    PatCounterUpgrade* m_downst;
+    PatCounterUpgrade* m_kSNew;
+    PatCounterUpgrade* m_best;
+    
+    PatTTCounter* m_ttForward;
+    PatTTCounter* m_ttMatch;
+    PatTTCounter* m_ttDownst;
+    
+    //== Vector of the counters
+    std::vector<PatCounterUpgrade*> m_allCounters;
+    
+    std::vector<PatTTCounter*> m_allTTCounters;
+
+    bool m_measureTime;
+    ISequencerTimerTool* m_timer;
+    int m_initTime;
+    int m_partTime;
+
+    std::string veloPixName;
+    std::string m_hltVeloPixLocation;
+    std::string m_hltVeloTTLocation;
+    std::string m_hltForwardLocation;
+  }; 
+
+#endif // PATCHECKERUPGRADE_H
