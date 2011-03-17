e0d854395921361d3c1248a6ee32033768f99132
rlambert <rlambert@cern.ch>
2011-03-17 11:03:42 +0000

    fixed debug printout
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@120326 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 43e5b3895af903622e642d6eb03b2851639fbf22

--- Rec/Muon/MuonID/src/MuonIDAlg.cpp
+++ Rec/Muon/MuonID/src/MuonIDAlg.cpp
@@ -1507,11 +1507,11 @@ StatusCode MuonIDAlg::calcMuonLL_tanhdist_landau(LHCb::MuonPID * pMuid, const do
 
   // Using histograms it's not unexpected that some bins are empty. Use very small prob as a protection for the log
   if(ProbMu<1.e-20) {
-    debug() << "calcMuonLL_tanhdist_landau: Found null ProbMu: " << ProbMu << endmsg;
+    if (msgLevel(MSG::DEBUG) ) debug() << "calcMuonLL_tanhdist_landau: Found null ProbMu: " << ProbMu << endmsg;
     ProbMu = 1.e-6;
   }
   if(ProbNonMu<1.e-20) {
-    debug() << "calcMuonLL_tanhdist_landau: Found null ProbNonMu: " << ProbNonMu << endmsg;
+    if (msgLevel(MSG::DEBUG) ) debug() << "calcMuonLL_tanhdist_landau: Found null ProbNonMu: " << ProbNonMu << endmsg;
     ProbNonMu = 1.e-6;
   }
 
