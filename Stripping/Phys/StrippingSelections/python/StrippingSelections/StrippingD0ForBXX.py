ed4c7f15b665498a5a47cb8ecb3db4e92bbb22c8
Liming Zhang <liming.zhang@cern.ch>
2011-03-17 22:35:55 +0000

    modify strippingD0forBXX.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@120431 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 67a2ceedc98790770b0ddebc47cb33a86a17e993

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD0ForBXX.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD0ForBXX.py
@@ -16,10 +16,13 @@ from StandardParticles import StdLoosePions, StdLooseKaons, StdLooseProtons
 name = 'confD0forBXX'
 
 __all__ = ('D0forBXXConf',
+           'makeDst2D0Pi',
            'confdict')
 
 confdict =  {
     "PreScale"      : 0.03   # adimensiional        
+    ,"PreScaleRS"   : 1.0    # adimensiional
+    ,"PreScaleWS"   : 0.2    # adimensiional
     ,"MINIPCHI2"     : 9.0   # adimensiional
     ,"TRCHI2"        : 5.0   # adimensiional
     ,"KaonPIDK"      : 4.0   # adimensiional
@@ -41,6 +44,8 @@ class D0forBXXLinesConf(LineBuilder):
 
     __configuration_keys__ = (
          "PreScale"     
+        ,"PreScaleRS"
+        ,"PreScaleWS" 
         ,"MINIPCHI2"    
         ,"TRCHI2"       
         ,"KaonPIDK"     
@@ -75,6 +80,19 @@ class D0forBXXLinesConf(LineBuilder):
         self.seld02k3pi = Selection("D02K3Pifor" + name,
                                    Algorithm = self._D02K3Pi(),
                                    RequiredSelections = [ StdLooseKaons, StdLoosePions ])
+
+        self.seld02k2pi = Selection("D02K2Pifor" + name,
+                                    Algorithm = self._D02K2Pi(),
+                                    RequiredSelections = [ StdLooseKaons, StdLoosePions ])
+
+        self.seldstrs = makeDst2D0Pi("DstRSwD0K2Pi" + name,
+                                  DecayDescriptor = '[D*(2010)+ -> D0 pi+]cc',
+                                  DaughterLists = [ self.seld02k2pi, StdLoosePions ])
+
+        self.seldstws = makeDst2D0Pi("DstWSwD0K2Pi" + name,
+                                     DecayDescriptor = '[D*(2010)- -> D0 pi-]cc',
+                                     DaughterLists = [ self.seld02k2pi, StdLoosePions ])
+        
         self.D02KPiLine = StrippingLine('D02KPi' + name + 'Line'
                                         , prescale = config['PreScale']
                                         , selection = self.seld02kpi
@@ -84,8 +102,20 @@ class D0forBXXLinesConf(LineBuilder):
                                          , selection = self.seld02k3pi
                                         )
 
+        self.DstRSLine = StrippingLine('DstRSwD02K2Pi' + name + 'Line'
+                                       , prescale = config['PreScaleRS']
+                                       , selection = self.seldstrs
+                                       )
+        
+        self.DstWSLine = StrippingLine('DstWSwD02K2Pi' + name + 'Line'
+                                       , prescale = config['PreScaleWS']
+                                       , selection = self.seldstws
+                                       )        
+        
         self.registerLine(self.D02KPiLine)
         self.registerLine(self.D02K3PiLine)
+        self.registerLine(self.DstRSLine)
+        self.registerLine(self.DstWSLine)
         
     def _D02KPi( self ):
         _decayDescriptor = '[D0 -> pi+ K-]cc'
@@ -99,7 +129,20 @@ class D0forBXXLinesConf(LineBuilder):
                                 CombinationCut = _combinationCut,
                                 MotherCut = _motherCut,
                                 )
-    
+    def _D02K2Pi( self ):
+        _decayDescriptor = '[D0 -> pi+ pi- K-]cc'
+        _combinationCut = "(AM>1.4*GeV) & (AM<1.7*GeV) & (AM12>575.5*MeV) & (AM12<975.5*MeV) & (APT > 3000*MeV) & (ADOCACHI2CUT( %(DDocaChi2Max)s, ''))" % self.__confdict__
+        _daughtersCuts = {  "pi+" : "(TRCHI2DOF < %(TRCHI2)s) & (P>2.0*GeV) & (PT > 400 *MeV)"\
+                     "& (MIPCHI2DV(PRIMARY)> 4.0) &  (PIDK< %(PionPIDK)s) & (PIDmu< 10)" % self.__confdict__,
+                            "K+" :  "(TRCHI2DOF < %(TRCHI2)s) & (PT > 400 *MeV) & (P>2.0*GeV) "\
+                    "& (MIPCHI2DV(PRIMARY)> 4.0)  &  (PIDK> %(KaonPIDK)s)" % self.__confdict__  }
+        _motherCut = "(VFASPF(VCHI2/VDOF)<6.0) & (BPVVDCHI2>100) & (BPVDIRA>0.999) & (BPVIPCHI2()<25.)"
+        return CombineParticles(DecayDescriptor = _decayDescriptor,
+                                DaughtersCuts = _daughtersCuts,
+                                CombinationCut = _combinationCut,
+                                MotherCut = _motherCut,
+                                )
+
     def _D02K3Pi( self ):
         _decayDescriptor = '[D0 -> K- pi+ pi- pi+]cc' 
         _combinationCut = "(ADAMASS('D0') < %(D0AMassWin)s *MeV) & (APT > 1500.*MeV) &  (ADOCACHI2CUT( %(DDocaChi2Max)s, ''))" % self.__confdict__
@@ -114,5 +157,21 @@ class D0forBXXLinesConf(LineBuilder):
                                  CombinationCut = _combinationCut,
                                  MotherCut = _motherCut)
 
-        
+
+def makeDst2D0Pi(name,
+                 DecayDescriptor,
+                 DaughterLists):
+    #    _decayDescriptor = '[D*(2010)+ -> D0 pi+]cc'
+    _combinationCut = "(AM-AM1-139.57*MeV<40.0*MeV) & (APT>3.0*GeV)"
+    _daughtersCuts = { "pi+" : "(PT > 250*MeV) & (TRCHI2DOF < 5.0) & (MIPDV(PRIMARY)< 1.0*mm) & (MIPCHI2DV(PRIMARY)<4.0) & (PIDK < 10)"}
+    _motherCut = "(VFASPF(VCHI2/VDOF)<10.0) & (BPVVDCHI2 < 25) & (BPVIPCHI2() < 25.)"
+    _dst = CombineParticles(DecayDescriptor = DecayDescriptor,
+                            DaughtersCuts = _daughtersCuts,
+                            CombinationCut = _combinationCut,
+                            MotherCut = _motherCut,
+                            )        
+    return Selection(name,
+                     Algorithm = _dst,
+                     RequiredSelections = DaughterLists)
+
     
