f2d4ae154283ffd3aefd62753a38f0e6fe19dba2
Vanya Belyaev <ivan.belyaev@cern.ch>
2011-03-25 21:04:16 +0000

    add missing files
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@120810 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7097ac3b02f447ad4a793d2133e529ea6d66d3eb

---
+++ LHCb/Phys/LoKiCore/LoKi/RecStat.h
@@ -0,0 +1,171 @@
+// $Id: $
+// ============================================================================
+#ifndef LOKI_RECSTAT_H 
+#define LOKI_RECSTAT_H 1
+// ============================================================================
+// Include files
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/Kernel.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/TES.h"
+// ============================================================================
+/** @file
+ *
+ *  This file is a part of LoKi project - 
+ *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
+ *
+ *  The package has been designed with the kind help from
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+ *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  A.Golutvin, P.Koppenburg have been used in the design.
+ *
+ *  By usage of this code one clearly states the disagreement 
+ *  with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *                    $Revision:$
+ *  Last modification $Date:$
+ *                 by $Author:$
+ */
+// ============================================================================
+namespace LoKi 
+{
+  // ==========================================================================
+  namespace TES
+  {
+    // ========================================================================
+    /** @class HasRecSummary
+     *  Check the existence of certain record in RecSummary object
+     *  @see LHCb::RecSummary
+     *  @see LHCb::RecSummary::hasInfo
+     *  The functor has been asked by Sophie Redford and Andy Powell
+     *  @author Vanya BELYAEV Ivan.Belyaev@cern.ch
+     *  @daet 2011-03-24
+     */
+    class GAUDI_API HasRecSummary : public LoKi::TES::Exists
+    {
+    public:
+      // ======================================================================
+      /** Standard constructor from the key, location and 'rootInTES'
+       *  @param key          the key to use 
+       *  @param location     TES location of LHCb::RecSummary obejct
+       *  @param useRootInTES flato to use/ignore root-in-tes 
+       */
+      HasRecSummary 
+      ( const int          key                 , 
+        const std::string& location     = ""   , 
+        const bool         useRootInTES = true ) ;
+      /// MANDATORY: virtual destructor 
+      virtual ~HasRecSummary () ;
+      /// MANDATORY: clone method ("virtual constructor")
+      virtual  HasRecSummary* clone () const ;
+      /// MANDATORY: the only one essential method 
+      virtual  result_type operator() ( /* argument v */ ) const ;
+      /// OPTIONAL: nice printout 
+      virtual std::ostream& fillStream ( std::ostream& s ) const ;
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// default constructor is disabled
+      HasRecSummary () ;                    // default constructor is disabled
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// the key to be used for LHCb::RecSummary::hasInfo method 
+      int m_key ; // the key to be used for LHCb::RecSummary::hasInfo method
+      // ======================================================================
+    } ;
+    // ========================================================================
+    /** @class RecSummary
+     *  Check the content of certain record in RecSummary object
+     *  @see LHCb::RecSummary
+     *  @see LHCb::RecSummary::info
+     *  The functor has been asked by Sophie Redford and Andy Powell
+     *  @author Vanya BELYAEV Ivan.Belyaev@cern.ch
+     *  @daet 2011-03-24
+     */
+    class GAUDI_API RecSummary : public LoKi::TES::Contains
+    {
+    public:
+      // ======================================================================
+      /** Standard constructor from the key, location and 'rootInTES'
+       *  @param key          the key to use 
+       *  @param bad          bad value  
+       *  @param location     TES location of LHCb::RecSummary obejct
+       *  @param useRootInTES flato to use/ignore root-in-tes 
+       */
+      RecSummary 
+      ( const int          key                  , 
+        const int          bad                  ,  
+        const std::string& location     = ""    , 
+        const bool         useRootInTES = true  ) ;
+      /// MANDATORY: virtual destructor 
+      virtual ~RecSummary () ;
+      /// MANDATORY: clone method ("virtual constructor")
+      virtual  RecSummary* clone () const ;
+      /// MANDATORY: the only one essential method 
+      virtual  result_type operator() ( /* argument v */ ) const ;
+      /// OPTIONAL: nice printout 
+      virtual std::ostream& fillStream ( std::ostream& s ) const ;
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// default constructor is disabled
+      RecSummary () ;                       // default constructor is disabled
+      // ======================================================================
+    private:
+      // ======================================================================
+      /// the key to be used for LHCb::RecSummary::info method 
+      int m_key ; // the key to be used for LHCb::RecSummary::info method
+      /// the value to be used for LHCb::RecSummary::info method 
+      int m_bad ; // the value to be used for LHCb::RecSummary::info method
+      // ======================================================================
+    } ;
+    // ========================================================================
+  } //                                               end of namespace LoKi::TES 
+  // ==========================================================================
+  namespace Cuts 
+  {
+    // ========================================================================
+    /** @typedef HASRECSUMMARY 
+     *  Check the record in LHCb::RecSummary
+     *  The functors is designed for Sophie Redford and Andy Powell
+     *
+     *  @code
+     * 
+     *     HASRECSUMMARY ( 10 ) 
+     *
+     *  @endcode 
+     *  @see LoKi::TES::HasRecSummary
+     *  @author Vanya BELYAEV  Ivan.BElyaev@cern.ch
+     *  @date 2011-03-24
+     */
+    typedef LoKi::TES::HasRecSummary                            HASRECSUMMARY ;
+    // ========================================================================
+    /** @typedef RECSUMMARY 
+     *  Get the record from LHCb::RecSummary
+     *  The functors is designed for Sophie Redford and Andy Powell
+     *
+     *  @code
+     * 
+     *     RECSUMMARY ( 10 , -1000 ) 
+     *
+     *  @endcode 
+     *  @see LoKi::TES::RecSummary
+     *  @author Vanya BELYAEV  Ivan.BElyaev@cern.ch
+     *  @date 2011-03-24
+     */
+    typedef LoKi::TES::RecSummary                                  RECSUMMARY ;
+    // ========================================================================
+  } //                                             end of namespace LoKi::Cuts 
+  // ==========================================================================
+} //                                                      end of namespace LoKi 
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // LOKI_RECSTAT_H
+// ============================================================================
---
+++ LHCb/Phys/LoKiCore/src/RecStat.cpp
@@ -0,0 +1,172 @@
+// $Id: $
+// ============================================================================
+// Include files 
+// ============================================================================
+// Event 
+// ============================================================================
+#include "Event/RecSummary.h"
+// ============================================================================
+// LoKi
+// ============================================================================
+#include "LoKi/RecStat.h"
+// ============================================================================
+/** @file
+ *  Implementation file for 
+ *  class LoKi::TES::HasRecSummary and 
+ *  class LoKi::TES::RecSummary 
+ *  @see LoKi::TES::HasRecSummary and 
+ *  @see LoKi::TES::RecSummary 
+ *
+ *  This file is a part of LoKi project - 
+ *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
+ *
+ *  The package has been designed with the kind help from
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+ *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  A.Golutvin, P.Koppenburg have been used in the design.
+ *
+ *  By usage of this code one clearly states the disagreement 
+ *  with the campain of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *                    $Revision:$
+ *  Last modification $Date:$
+ *                 by $Author:$
+ */
+// ============================================================================
+/*  Standard constructor from the key, location and 'rootInTES'
+ *  @param key          the key to use 
+ *  @param location     TES location of LHCb::RecSummary obejct
+ *  @param useRootInTES flato to use/ignore root-in-tes 
+ */
+// ============================================================================
+LoKi::TES::HasRecSummary::HasRecSummary 
+( const int          key          , 
+  const std::string& location     , 
+  const bool         useRootInTES ) 
+  : LoKi::TES::Exists ( location , useRootInTES ) 
+  , m_key ( key )
+{
+  if  ( location.empty () ) 
+  { setLocation( LHCb::RecSummaryLocation::Default ) ; }
+}
+// ============================================================================
+// MANDATORY: virtual destructor 
+// ============================================================================
+LoKi::TES::HasRecSummary::~HasRecSummary () {}
+// ============================================================================
+// MANDATORY: clone method ("virtual constructor")
+// ============================================================================
+LoKi::TES::HasRecSummary*
+LoKi::TES::HasRecSummary::clone () const
+{ return new LoKi::TES::HasRecSummary ( *this ) ; }
+// ============================================================================
+// MANDATORY: the only one essential method 
+// ============================================================================
+LoKi::TES::HasRecSummary::result_type 
+LoKi::TES::HasRecSummary::operator() 
+  ( /* LoKi::TES::HasRecSummary::argument a */ ) const 
+{
+  // check the algorithm
+  Assert ( !(!m_algorithm) , "GaudiAlgorithm* points to NULL" ) ;
+  //
+  if ( !m_algorithm->exist<LHCb::RecSummary> ( location     ()  , 
+                                               useRootInTes () ) )
+  {
+    Error ( "No LHCb::RecSummary is available at '" + location () + "'") ;
+    return false ;
+  }
+  //
+  const LHCb::RecSummary* summary = 
+    m_algorithm->get<LHCb::RecSummary> ( location () , useRootInTes () ) ;
+  //
+  return 0 != summary && summary -> hasInfo ( m_key ) ;
+}    
+// ============================================================================
+// OPTIONAL: nice printout 
+// ============================================================================
+std::ostream& 
+LoKi::TES::HasRecSummary::fillStream ( std::ostream& s ) const 
+{
+  //
+  s << " HASRECSUMMARY( " << m_key ;
+  if ( !useRootInTes() || LHCb::RecSummaryLocation::Default != location() ) 
+  { s << ",'" << location() << "'" ; }
+  if ( !useRootInTes() ) { s << ", False " ; }
+  //
+  return s << " ) " ;
+}
+// ============================================================================
+/*  Standard constructor from the key, location and 'rootInTES'
+ *  @param key          the key to use 
+ *  @param bad          the bad value 
+ *  @param location     TES location of LHCb::RecSummary obejct
+ *  @param useRootInTES flato to use/ignore root-in-tes 
+ */
+// ============================================================================
+LoKi::TES::RecSummary::RecSummary 
+( const int          key          , 
+  const int          bad         , 
+  const std::string& location     , 
+  const bool         useRootInTES ) 
+  : LoKi::TES::Contains ( location , useRootInTES ) 
+  , m_key ( key )
+  , m_bad ( bad )
+{
+  if  ( location.empty () ) 
+  { setLocation( LHCb::RecSummaryLocation::Default ) ; }
+}
+// ============================================================================
+// MANDATORY: virtual destructor 
+// ============================================================================
+LoKi::TES::RecSummary::~RecSummary () {}
+// ============================================================================
+// MANDATORY: clone method ("virtual constructor")
+// ============================================================================
+LoKi::TES::RecSummary*
+LoKi::TES::RecSummary::clone () const
+{ return new LoKi::TES::RecSummary ( *this ) ; }
+// ============================================================================
+// MANDATORY: the only one essential method 
+// ============================================================================
+LoKi::TES::RecSummary::result_type 
+LoKi::TES::RecSummary::operator() 
+  ( /* LoKi::TES::HasRecSummary::argument a */ ) const 
+{
+  // check the algorithm
+  Assert ( !(!m_algorithm) , "GaudiAlgorithm* points to NULL" ) ;
+  //
+  if ( !m_algorithm->exist<LHCb::RecSummary> ( location     ()  , 
+                                               useRootInTes () ) )
+  {
+    Error ( "No LHCb::RecSummary is available at '" + location () + "'") ;
+    return m_bad ;
+  }
+  //
+  const LHCb::RecSummary* summary = 
+    m_algorithm->get<LHCb::RecSummary> ( location () , useRootInTes () ) ;
+  //
+  if ( 0 == summary ) { return m_bad ; }
+  //
+  return summary -> info ( m_key , m_bad ) ;
+}    
+// ============================================================================
+// OPTIONAL: nice printout 
+// ============================================================================
+std::ostream& 
+LoKi::TES::RecSummary::fillStream ( std::ostream& s ) const 
+{
+  s << " RECSUMMARY( " << m_key << "," << m_bad ;
+  if ( !useRootInTes() || LHCb::RecSummaryLocation::Default != location() ) 
+  { s << ",'" << location() << "'" ; }
+  if ( !useRootInTes() ) { s << ", False " ; }
+  //
+  return s << " ) " ;
+}
+// ============================================================================
+
+
+
+// ============================================================================
+// The END 
+// ============================================================================
