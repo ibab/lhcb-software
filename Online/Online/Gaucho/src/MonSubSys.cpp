63937254ab2a3688b67843cf77e7d10d20990ceb
Beat Jost <beat.jost@cern.ch>
2011-03-28 12:09:13 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@120843 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 3aa4d6920ce1580f58f1134c33319c59e5a734a7

--- Online/Online/Gaucho/src/MonSubSys.cpp
+++ Online/Online/Gaucho/src/MonSubSys.cpp
@@ -182,7 +182,7 @@ void MonSubSys::setup(char *n, bool expandnames)
       }
     }
   }
-  if (m_rpc == 0) m_rpc = new ObjRPC(m_ser, (char*)nam.c_str(), (char*)"I:1;C",(char*)"C");
+//  if (m_rpc == 0) m_rpc = new ObjRPC(m_ser, (char*)nam.c_str(), (char*)"I:1;C",(char*)"C");
   nam = /*nodename+"_*/std::string("MON_")+m_pname+"/"+m_name+"/Data";
   if ( m_genSrv == 0) m_genSrv = new ObjService(m_ser,(char*)nam.c_str(),(char*)"C",(void*)&mpty, 4);
   if (m_expandnames)
