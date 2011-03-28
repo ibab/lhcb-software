3c8c78d1f807a4314a545c6b2e98df72d1292539
hmdegaud <hmdegaud@cern.ch>
2011-03-28 08:42:18 +0000

    Incomplete - task : Implement the selfextract script creator  added a few needed options to the specifications.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@120821 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bbdb9755b34447bf429d190963bb993c55255982

--- LbScripts/LbUtils/python/LbUtils/SelfExtract.py
+++ LbScripts/LbUtils/python/LbUtils/SelfExtract.py
@@ -1,3 +1,9 @@
+# $Id$
+# module to create self extracting script. Based on the shell
+# makeself application (http://megastep.org/makeself)
+#
+# @author: Hubert Degaudenzi
+
 from LbUtils.Tar import createTarBall
 from LbUtils.Temporary import TempFile
 from LbUtils.File import genTemplateFile
@@ -17,7 +23,22 @@ default_template = os.path.join(os.environ.get("LBUTILSROOT", ""), "data", "Self
 
 def createSelfExtractFile(input_dir, target_filename, description, script=None, script_args=None,
                           compression_type="gzip", target_dir=None, use_temp_dir=True,
-                          template=None):
+                          template=None, update_url=None, script_version=None):
+    """ function to create a self extracting script.
+    @return: status code for the whole process 0: success other:failed
+    @param input_dir: directory to be tarred and placed at the end of the script as a string
+    @param target_filename: name for the self extracting script
+    @param description: short description of the self extracting script
+    @param script: internal script to be called after the untar. the path must be relative to the
+                   root of the input_dir
+    @param script_args: arguments to the internal scripts
+    @param compression_type: type of compression to be used for the tar payload
+    @param target_dir: name of the directory where the untarring is done
+    @param use_temp_dir: usage of a temporary directory or not.
+    @param template: template file to be used as a base for the self extracting script
+    @param update_url: url to self update the self extracting script
+    @param script_version: sets the version of the self extracting script
+    """
     status = 0
     if not template :
         template = default_template
