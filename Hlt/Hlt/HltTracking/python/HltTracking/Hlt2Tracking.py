02e7de05c5f9e63f77f2fb1c525296cd69f9ac72
Patrick Spradlin <patrick.spradlin@cern.ch>
2011-03-29 20:09:01 +0000

    Renamed some Hlt2Charm configurables to conform to a naming convention.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@121027 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dcc71fd4e60ceb6c399050eafb734e2e8af7505f

--- Hlt/Hlt/HltTracking/python/HltTracking/Hlt2Tracking.py
+++ Hlt/Hlt/HltTracking/python/HltTracking/Hlt2Tracking.py
@@ -46,15 +46,15 @@ from Hlt2Lines.Hlt2B2XGammaLines          import Hlt2B2XGammaLinesConf
 from Hlt2Lines.Hlt2B2HHLines              import Hlt2B2HHLinesConf
 from Hlt2Lines.Hlt2B2LLXLines             import Hlt2B2LLXLinesConf
 from Hlt2Lines.Hlt2CharmHadD02HHLines     import Hlt2CharmHadD02HHLinesConf
-from Hlt2Lines.Hlt2CharmSemilepD02HMuNuLines           import Hlt2CharmSemilepD02HMuNuLinesConf
 from Hlt2Lines.Hlt2CharmHadTwoBodyForMultiBody     import Hlt2CharmHadTwoBodyForMultiBodyConf
-from Hlt2Lines.Hlt2CharmHadTwoMuonForMuMuHad import Hlt2CharmHadTwoMuonForMuMuHadConf
 from Hlt2Lines.Hlt2CharmHadD2HHHLines     import Hlt2CharmHadD2HHHLinesConf
 from Hlt2Lines.Hlt2CharmHadD02HHHHLines   import Hlt2CharmHadD02HHHHLinesConf
-from Hlt2Lines.Hlt2CharmHadD02HHmumuLines   import Hlt2CharmHadD02HHmumuLinesConf
-from Hlt2Lines.Hlt2CharmHadD2HmumuLines   import Hlt2CharmHadD2HmumuLinesConf
+from Hlt2Lines.Hlt2CharmSemilepD02HMuNuLines     import Hlt2CharmSemilepD02HMuNuLinesConf
+from Hlt2Lines.Hlt2CharmSemilepTwoMuonForMuMuHad import Hlt2CharmSemilepTwoMuonForMuMuHadConf
+from Hlt2Lines.Hlt2CharmSemilepD02HHMuMuLines   import Hlt2CharmSemilepD02HHMuMuLinesConf
+from Hlt2Lines.Hlt2CharmSemilepD2HMuMuLines   import Hlt2CharmSemilepD2HMuMuLinesConf
 from Hlt2Lines.Hlt2CharmRareDecayLines    import Hlt2CharmRareDecayLinesConf
-from Hlt2Lines.Hlt2D2KS0HLines            import Hlt2CharmHadD2KS0HLinesConf
+from Hlt2Lines.Hlt2CharmHadD2KS0HLines    import Hlt2CharmHadD2KS0HLinesConf
 from Hlt2Lines.Hlt2CharmHadD02HHKsLines   import Hlt2CharmHadD02HHKsLinesConf  
 from Hlt2Lines.Hlt2DisplVerticesLines     import Hlt2DisplVerticesLinesConf
 from Hlt2Lines.Hlt2CommissioningLines     import Hlt2CommissioningLinesConf
@@ -89,15 +89,15 @@ class Hlt2Tracking(LHCbConfigurableUser):
                              , Hlt2TopologicalLinesConf
                              , Hlt2B2DXLinesConf
                              , Hlt2CharmHadD02HHLinesConf
-                             , Hlt2CharmSemilepD02HMuNuLinesConf
                              , Hlt2CharmHadD2HHHLinesConf
                              , Hlt2CharmHadD2KS0HLinesConf 
                              , Hlt2CharmHadD02HHKsLinesConf
                              , Hlt2CharmHadD02HHHHLinesConf
-                             , Hlt2CharmHadD02HHmumuLinesConf
-                             , Hlt2CharmHadD2HmumuLinesConf
                              , Hlt2CharmHadTwoBodyForMultiBodyConf
-                             , Hlt2CharmHadTwoMuonForMuMuHadConf
+                             , Hlt2CharmSemilepD02HMuNuLinesConf
+                             , Hlt2CharmSemilepD02HHMuMuLinesConf
+                             , Hlt2CharmSemilepD2HMuMuLinesConf
+                             , Hlt2CharmSemilepTwoMuonForMuMuHadConf
                              , Hlt2CharmRareDecayLinesConf
                              , Hlt2InclusiveDiMuonLinesConf
                              , Hlt2InclusiveMuonLinesConf
