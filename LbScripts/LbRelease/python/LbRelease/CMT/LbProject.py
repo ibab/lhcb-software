195489cf055e45935114bea12bf55cffaa9109c9
hmdegaud <hmdegaud@cern.ch>
2011-03-30 14:48:50 +0000

    Complete - task : Implement LbWhich - fixed usage of the User release area in LbCMTWhich.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@121127 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6113b6f310c60ba7f50a9254dcef18e442e3e838

--- LbScripts/LbRelease/python/LbRelease/CMT/LbProject.py
+++ LbScripts/LbRelease/python/LbRelease/CMT/LbProject.py
@@ -2,7 +2,7 @@ from LbUtils.CMT import Project
 from LbUtils.CMT import CMTWhich
 from LbPackage import LbPackage
 from LbConfiguration.Project import getProject
-from LbConfiguration.Version import LHCb2CMT
+from LbConfiguration.Version import LHCb2CMT, sortStrings
 
 import logging
 
@@ -31,6 +31,8 @@ def LbCMTWhich(project, package=None, version=None, all_occurences=False,
                casesense=True, with_user_area=True):
     log = logging.getLogger()
 
+    result = None
+
     if os.environ.has_key("LHCBPROJECTPATH") :
         log.debug("Using LHCBPROJECTPATH (%s)" % os.environ["LHCBPROJECTPATH"])
         os.environ["CMTPROJECTPATH"] = os.environ["LHCBPROJECTPATH"]
@@ -42,17 +44,48 @@ def LbCMTWhich(project, package=None, version=None, all_occurences=False,
             log.fatal("Not fallback CMTPROJECTPATH in the environment")
             return 1
 
-    if version :
-        proj, ver = LHCb2CMT(project, version)
-        result = CMTWhich(proj, package, ver, all_occurences, casesense)
-    else :
-        proj, pack = LHCb2CMT(project, package)
-        result = CMTWhich(proj, pack, version, all_occurences, casesense)
+    if with_user_area and os.environ.has_key("User_release_area") :
+        log.debug("adding the User release area %s to CMTPROJECTPATH" % os.environ["User_release_area"])
+        os.environ["CMTPROJECTPATH"] = os.pathsep.join([os.environ["User_release_area"], os.environ["CMTPROJECTPATH"]])
 
-    if not result :
-        result = CMTWhich(proj, package, version, all_occurences, casesense)
+    log.debug("CMTPROJECTPATH used: %s" % os.environ["CMTPROJECTPATH"])
+
+    if with_user_area and os.environ.has_key("User_release_area") :
+        if version :
+            proj = "%s_%s" % (project, version)
+            if os.path.exists(os.path.join(os.environ["User_release_area"], proj)) :
+                result = CMTWhich(proj, package, None, all_occurences, casesense)
+        else :
+            proj = "%s_%s" % (project, package)
+            if os.path.exists(os.path.join(os.environ["User_release_area"], proj)) :
+                result = CMTWhich(proj, None, None, all_occurences, casesense)
+
+        if not result and not version :
+            prjlist = []
+            for l in os.listdir(os.environ["User_release_area"]) :
+                if l.startswith("%s_" % project) :
+                    prjlist.append(l)
+            prjlist = sortStrings(prjlist, versiontype=None, reverse=True)
+            prjlist.insert(0, "%s_HEAD" % project)
+            for p in prjlist :
+                if os.path.exists(os.path.join(os.environ["User_release_area"], p)) :
+                    result = CMTWhich(p, package , None, all_occurences, casesense)
+                    if result :
+                        break
 
     if not result :
         result = CMTWhich(project, package, version, all_occurences, casesense)
 
+    if not result :
+        if version :
+            proj, ver = LHCb2CMT(project, version)
+            result = CMTWhich(proj, package, ver, all_occurences, casesense)
+        else :
+            proj, pack = LHCb2CMT(project, package)
+            result = CMTWhich(proj, pack, version, all_occurences, casesense)
+
+    if not result :
+        result = CMTWhich(proj, package, version, all_occurences, casesense)
+
     return result
+
