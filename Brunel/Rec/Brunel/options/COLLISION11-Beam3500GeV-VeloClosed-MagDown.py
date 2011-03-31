f0cf5588d9edce51eef4e099dc07fc768e972da9
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-03-31 16:02:02 +0000

    updates to references, add a test for 2011
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@121171 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8f8dc694bbaafa5f00a4380df625e3fe56fb9866

---
+++ Brunel/Rec/Brunel/options/COLLISION11-Beam3500GeV-VeloClosed-MagDown.py
@@ -0,0 +1,18 @@
+# Example 2011 collisions options for Brunel
+
+# Syntax is:
+#   gaudirun.py COLLISION11-Beam3500GeV-VeloClosed-MagDown.py
+#
+from Gaudi.Configuration import FileCatalog, EventSelector
+from Configurables import Brunel
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
+
+#-- Use latest 2011 database tags for real data
+Brunel().DataType = "2011"
+
+# Events from run 87968 from fill 1647, on 24th March 2011
+EventSelector().Input = [
+  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/87968/087968_0000000001.raw?svcClass=lhcbdst' SVC='LHCb::MDFSelector'"
+    ]
