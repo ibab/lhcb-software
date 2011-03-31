5e262be3949fe743281ada0894be4e56de2dd5db
Vanya Belyaev <ivan.belyaev@cern.ch>
2011-03-31 09:54:20 +0000

    add 'ondemand'-property to dir/ls methods
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@121156 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 41b2c08976f8694dc4099402e467c2ec88bb8184

--- Analysis/Phys/AnalysisPython/python/AnalysisPython/Dir.py
+++ Analysis/Phys/AnalysisPython/python/AnalysisPython/Dir.py
@@ -1,7 +1,7 @@
 from GaudiPython.Bindings import iDataSvc, SUCCESS, FAILURE
  
 
-def _dir_ ( self , node = None , forceload = False, level = -1 ) :
+def _dir_ ( self , node = None , forceload = False, level = -1 , ondemand = False ) :
 
     """
 
@@ -19,36 +19,77 @@ def _dir_ ( self , node = None , forceload = False, level = -1 ) :
 
     if 0 == level          : return SUCCESS
     if   str is type(node) : node = self.retrieveObject( node )
-    elif  not node         : return self.dir('', forceload, level )
-    
+    elif  not node         : return self.dir('', forceload, level , ondemand )
+
     if not node            : return FAILURE
     if hasattr ( node , 'registry'   ) : node = node.registry()
-    if hasattr ( node , 'identifier' ) :
-        obj = node.object ()
-        if not obj :  print  node.identifier()
-        else :
-            if hasattr( obj , 'size'    ) :
-                size = obj.size()
-                if 0 == size : print "%s (empty) %s" % (  node.identifier() , type( obj ) )
-                elif hasattr ( obj , 'containedObjects' ) :
-                    c = obj.containedObjects()[0]
-                    print "%s container of %d %s objects " % ( node.identifier() , obj.size() , type(c))
-                else :
-                    print  "%s %s " % (  node.identifier() , type( node.object()) )
-            else :
-                print  "%s %s " % (  node.identifier() , type( node.object()) )
 
-    else : print "The node has no 'identifier'"
+    if hasattr ( node , 'identifier' ) :
+        id  = node.identifier () 
+        obj = node.object     ()
+        if not obj or not type( obj ) : 
+            print  " %-60s " % id 
+        else :
+            if hasattr ( obj , 'containedObjects' ) and not obj.empty()  :
+                c = obj.containedObjects()[0]
+                print " %-60s container of %-5d %s objects " % ( id , obj.size() , type(c).__name__ )
+            elif hasattr( obj , 'size'    ) :
+                size = obj.size()
+                if 0 == size : print " %-60s (empty)  \t%s " % ( id , type ( obj ) )
+                else         : print " %-60s #=%-5d   \t%s " % ( id , obj.size() , type ( obj ).__name__ )  
+            else :
+                print " %-60s \t\t%s " % ( id , type( obj ).__name__ )
+
+
+    else : print " The node has no 'identifier'"
     # start the recursion
 
+        
     for l in self.leaves( node ) :
         if forceload :
             try :
                 temp = self[l.identifier()]
             except :
                 temp = self[l.identifier()]
-        if l : self.dir( l , forceload, level - 1 ) 
+        if l : self.dir( l , forceload, level - 1 , ondemand ) 
 
+    if ondemand and hasattr ( node , 'identifier' ) :
+
+        s   = set()
+        id  = node.identifier()
+        id1 = id.split('/')
+        
+        from Configurables import DataOnDemandSvc as DoD
+        dod = DoD () 
+
+        dmap = dod.NodeMap
+        for key_ in dmap :
+            key = key_ if 0 == key_.find('/Event/') else '/Event/' + key_
+            if 0 == key.find ( id ) :
+                nkey = '/'.join(key.split('/')[:-1])
+                nkey = key.split('/')
+                if len ( nkey ) > len( id1 ) :
+                    nkey = nkey[:len(id1)+1]                
+                nkey = '/'.join(nkey)
+                if id == '/'.join(key.split('/')[:-1]) :
+                    if not nkey in s : 
+                        print "*%-60s \t\t%s"  % ( nkey , 'Directory')
+                        s.add( nkey )
+                        
+        amap = dod.AlgMap
+        for key_ in amap :
+            key = key_ if 0 == key_.find('/Event/') else '/Event/' + key_
+            if 0 == key.find ( id ) :
+                nkey  = key.split('/')
+                if len ( nkey ) > len( id1 ) :
+                    nkey = nkey[:len(id1)+1]
+                nkey  = '/'.join(nkey)
+                alg = amap[key_]
+                if hasattr ( alg , 'getFullName' ) : alg = alg.getFullName()
+                if not nkey in s : 
+                    print "*%-60s \t\t%s"  % ( nkey , alg )
+                    s.add ( nkey )
+                        
     return SUCCESS
 
 def _nodes_(self, forceload = False, node = '') :
