0bd339d3def9f312da840a363f2f8abb8d1a9907
Gerhard Raven <gerhard.raven@nikhef.nl>
2011-04-05 18:51:47 +0000

    fix old qmtest
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@121536 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: da097dc3f0fcce3fc0585108ca51778875d721d1

--- Moore/Hlt/Moore/tests/options/testMoore-FEST-raw.py
+++ Moore/Hlt/Moore/tests/options/testMoore-FEST-raw.py
@@ -2,13 +2,26 @@
 from Gaudi.Configuration import *
 from Moore.Configuration import *
 
-Moore().HltType = 'Hlt1' # +Hlt2'
 Moore().EvtMax = 2000
-Moore().Simulation = True
-Moore().DataType = '2008'
-Moore().inputFiles = [ 'castor:/castor/cern.ch/grid/lhcb/test/MDF/00003083/0000/00003083_%08d_1.mdf'%f  for f in range(1,4) ]
+Moore().Simulation = False
+Moore().DataType = '2010'
+Moore().UseDBSnapshot = False
+Moore().DDDBtag    = 'head-20110302'
+Moore().CondDBtag  = 'head-20110318'
 
-LHCbApp().DDDBtag   = "default"
-LHCbApp().CondDBtag = "default"
+Moore().inputFiles  = [ 'castor:/castor/cern.ch/user/e/evh/81349/81349_0x0032_MBNB_L0Phys.raw'
+                      , 'castor:/castor/cern.ch/user/e/evh/80881/80881_0x0032_MBNB_L0Phys.raw'
+                      , 'castor:/castor/cern.ch/user/e/evh/79646/79646_0x0032_MBNB_L0Phys.raw'
+                      , 'castor:/castor/cern.ch/user/e/evh/79647/79647_0x0032_MBNB_L0Phys.raw'
+                      ]
+#Moore().inputFiles  = [ '/data/bfys/graven/81349_0x0033_MBNB_L0Phys.raw'
+#                      , '/data/bfys/graven/80881_0x0033_MBNB_L0Phys.raw'
+#                      , '/data/bfys/graven/79646_0x0033_MBNB_L0Phys.raw'
+#                      , '/data/bfys/graven/79647_0x0033_MBNB_L0Phys.raw'
+#                      ]
+
+
+Moore().ForceSingleL0Configuration = False
+
 
 EventSelector().PrintFreq = 100
