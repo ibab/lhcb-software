ef2298eb672e551239792ac77fd59079bb5d51d9
hmdegaud <hmdegaud@cern.ch>
2011-04-07 12:47:13 +0000

    release of version v6r1p2
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@121636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a03b5106a22bea3a2631621c68f47af7bc4df7d0

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -21,10 +21,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from shutil import rmtree
 
-script_version = '110501'
+script_version = '110407'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v6r1p1"
+lbscripts_version = "v6r1p2"
 compat_version = None
 line_size = 120
 #-----------------------------------------------------------------------------------
