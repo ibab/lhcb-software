ba48a69cc823c274669375c2b2f0ec49dd047b15
Gloria Corti <gloria.corti@cern.ch>
2011-04-13 18:11:09 +0000

    new options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@122006 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ba202643ed520d6f56731ae5decf5aadbb72d5ed

--- Gauss/Sim/Gauss/options/Gauss-DEV.py
+++ Gauss/Sim/Gauss/options/Gauss-DEV.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# File for running Gauss with postMC09 configuration
+# File for running Gauss with latest tags
 # (3.5 TeV beams, nu=1, no spill-over)
 #
 # Syntax is:
@@ -15,9 +15,9 @@ importOptions("$APPCONFIGOPTS/Gauss/Beam3500GeV-md100-nu1.py")
 from Configurables import DDDBConf, CondDB, LHCbApp
 
 #CondDB().Simulation = True             # Already taken care of in Gauss()
-LHCbApp().DDDBtag   = "head-20101206"   # Must be set for selecting VELO
+LHCbApp().DDDBtag   = "head-20110303"   # Must be set for selecting VELO
                                         # geometry list in Gauss()
 
-DDDBConf(DataType = "2010")
-CondDB().UseLatestTags = ["2010"]
+DDDBConf(DataType = "2011")
+CondDB().UseLatestTags = ["2011"]
 
