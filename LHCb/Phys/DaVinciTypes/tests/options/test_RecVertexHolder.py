a6669a98827ee639059ada195df9af9c639fa55f
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-04-21 19:26:17 +0000

    Phys/DaVinciTypes moved to Phys
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@122364 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 80604de8c1d9864701d74d855eb1efbe8b42cf28

--- LHCb/Phys/DaVinciTypes/tests/options/test_RecVertexHolder.py
+++
@@ -1,6 +0,0 @@
-from Configurables import LHCbApp, ApplicationMgr, TestRecVertexHolder
-
-LHCbApp(EvtMax=10)
-
-ApplicationMgr(TopAlg = [TestRecVertexHolder(OutputLevel=3)],
-               EvtSel = 'NONE')
