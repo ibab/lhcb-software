c00feb3ba6c552b785bcf4d931d5a90fd434cfe5
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-04-27 12:10:04 +0000

    Add LHCb::RichSmartID::Vector to python dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@122490 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 28416120aad090074aa50721deac162307c94869

--- LHCb/Kernel/LHCbKernel/dict/LHCbKernelDict.h
+++ LHCb/Kernel/LHCbKernel/dict/LHCbKernelDict.h
@@ -88,9 +88,9 @@ namespace {
     Rich::Particles                                     m_RICH_Particles;
     Rich::RichSideVector                                m_RICH_Side_V;
     Rich::Sides                                         m_RICH_Sides;
-    //
-    std::pair<std::string,double>     m_pair_1 ;
-    std::pair<std::string,StatEntity> m_pair_2 ;
+    LHCb::RichSmartID::Vector                           m_RICH_SmartID_V;
+    std::pair<std::string,double>                       m_pair_1 ;
+    std::pair<std::string,StatEntity>                   m_pair_2 ;
     // end instantiations
   };
 }
