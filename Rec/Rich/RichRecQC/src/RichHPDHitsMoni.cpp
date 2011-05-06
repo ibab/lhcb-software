912d6bed037b36e4a89d31e62a773d857e3ca57a
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-05-06 12:40:26 +0000

    Small changes to the HPD plots made by HPDHitsMoni
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@122924 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4ea158b204b69421f4753f810d13d9e088a318a8

--- Rec/Rich/RichRecQC/src/RichHPDHitsMoni.cpp
+++ Rec/Rich/RichRecQC/src/RichHPDHitsMoni.cpp
@@ -55,9 +55,6 @@ StatusCode HPDHitsMoni::initialize()
 StatusCode HPDHitsMoni::execute()
 {
 
-  // Histogramming
-  const Rich::HistoID hid;
-
   // Obtain RichSmartIDs from raw decoding
   const DAQ::L1Map & data = m_decoder->allRichSmartIDs();
 
@@ -78,32 +75,30 @@ StatusCode HPDHitsMoni::execute()
         // inhibited HPD ?
         if ( (*iHPD).second.header().inhibit() ) { continue; }
 
-        // ZS or not ?
-        const std::string& ZS = ( (*iHPD).second.header().zeroSuppressed() ?
-                                  "/ZS/" : "/NonZS/" );
-
         // HPD info
         const LHCb::RichSmartID hpd     = (*iHPD).second.hpdID();
         const DAQ::HPDHardwareID hardID = m_richSys->hardwareID(hpd);
         const DAQ::Level0ID l0ID        = m_richSys->level0ID(hpd);
         const Rich::DetectorType rich   = hpd.rich();
+        const DAQ::HPDCopyNumber copyN  = m_richSys->copyNumber(hpd);
 
         // Vector of SmartIDs
         const LHCb::RichSmartID::Vector & rawIDs = (*iHPD).second.smartIDs();
 
         // create histo title
-        std::ostringstream HPD1,HPD2;
-        HPD1 << "Number hits in HPD " << hpd << " L0ID=" << l0ID << " hardID=" << hardID;
-        HPD2 << "Hit Map for HPD " << hpd << " L0ID=" << l0ID << " hardID=" << hardID;
+        std::ostringstream HPD1,HPD2,hpd1,hpd2;
+        HPD1 << "Number hits in HPD " << hpd << " CopyN=" << copyN << " L0ID=" << l0ID << " hardID=" << hardID;
+        HPD2 << "Hit Map for HPD "    << hpd << " CopyN=" << copyN << " L0ID=" << l0ID << " hardID=" << hardID;
+        hpd1 << "NumHits_HPDCopyN_" << copyN;
+        hpd2 << "HitMaps_HPDCopyN_" << copyN;
 
         // Alice or LHCb mode ? Use to define number of pixel rows for plots
         const int nPixRows = ( (*iHPD).second.header().aliceMode() ? 
                                Rich::DAQ::MaxDataSizeALICE : Rich::DAQ::MaxDataSize );
 
         // number of hits plot
-        plot1D( (double)rawIDs.size(),
-                hid(rich,"NumHits"+ZS+(std::string)l0ID), HPD1.str(),
-                -0.5,100.5,101 );
+        richHisto1D( HID(hpd1.str(),rich), HPD1.str(),
+                     -0.5,100.5,101 ) -> fill( (double)rawIDs.size() );
 
         // Loop over raw RichSmartIDs
         for ( LHCb::RichSmartID::Vector::const_iterator iR = rawIDs.begin();
@@ -116,19 +111,12 @@ StatusCode HPDHitsMoni::execute()
                             (*iR).pixelRow() );
 
           // HPD hit map
-          plot2D( (*iR).pixelCol(), row,
-                  hid(rich,"HitMaps"+ZS+(std::string)l0ID), HPD2.str(),
-                  -0.5,Rich::DAQ::NumPixelColumns-0.5,
-                  -0.5,nPixRows-0.5,
-                  Rich::DAQ::NumPixelColumns,nPixRows );
-
-          // Combined hit map
-          plot2D( (*iR).pixelCol(), row,
-                  hid(rich,"HitMaps"+ZS+"Combined"), "Combined HPD Hit Map",
-                  -0.5,Rich::DAQ::NumPixelColumns-0.5,
-                  -0.5,nPixRows-0.5,
-                  Rich::DAQ::NumPixelColumns,nPixRows );
-          
+          richHisto2D( HID(hpd2.str(),rich), HPD2.str(),
+                       -0.5,Rich::DAQ::NumPixelColumns-0.5,
+                       Rich::DAQ::NumPixelColumns,
+                       -0.5,nPixRows-0.5,
+                       nPixRows ) -> fill( (*iR).pixelCol(), row );     
+     
         } // raw channel ids
         
       } // loop over HPDs
