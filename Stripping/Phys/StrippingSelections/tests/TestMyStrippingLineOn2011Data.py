fbe90de5d1ee5a7505faf2bebffa014a0e5f34ab
Thomas Blake <thomas.blake@cern.ch>
2011-05-06 12:04:23 +0000

    Added new test script for 2011 data
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@122922 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ac85b06887440a419a2b4f0ecac2cb3c0e5f0498

---
+++ Stripping/Phys/StrippingSelections/tests/TestMyStrippingLineOn2011Data.py
@@ -0,0 +1,82 @@
+# $Id: $
+# Test your line(s) of the stripping
+
+from Gaudi.Configuration import *
+from Configurables import SelDSTWriter, DaVinci
+from StrippingConf.Configuration import StrippingConf
+
+# Now build the stream
+from StrippingConf.StrippingStream import StrippingStream
+stream = StrippingStream("Test")
+
+# Import your stripping lines. Replace the lines below appropriately.
+
+from StrippingSelections.StrippingB2XMuMu import B2XMuMuConf as builder
+
+config_params =  {
+    'BVXCHI2NDOF'        : 4.0         
+    , 'BIPCHI2'            : 9.0       
+    , 'BDIRA'              : 0.999968  
+    , 'BFDCHI2'            : 100.0     
+    , 'KpiMINIPCHI2'       : 9.0       
+    , 'KpiTRACKCHI2'       : 4.0       
+    , 'KpiVXCHI2NDOF'      : 9.0       
+    , 'MuonMINIPCHI2'      : 16.0      
+    , 'MuonTRACKCHI2'      : 4.0       
+    , 'MuonPID'            : 0.0       
+    , 'DimuonVXCHI2NDOF'   : 9.0       
+    , 'DimuonUPPERMASS'    : 5050.0    
+    , 'Pi0MINPT'           : 800.0     
+    , 'DplusLOWERMASS'     : 1600.0    
+    , 'DplusUPPERMASS'     : 2300.0    
+    , 'KstarplusWINDOW'    : 300.0     
+    }
+
+lb = builder('B2XMuMu',config_params)
+
+stream.appendLines( lb.lines() )
+
+from Configurables import  ProcStatusCheck
+filterBadEvents =  ProcStatusCheck()
+
+# Configure the stripping using the same options as in Reco06-Stripping10
+sc = StrippingConf( Streams = [ stream ],
+                    MaxCandidates = 50000,
+                    AcceptBadEvents = False,
+                    BadEventSelection = filterBadEvents )
+
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
+
+# Remove the microbias, nobias and beam gas etc events before doing the tagging step. Remove these lines if inapropriate for your analysis
+regexp = "HLT_PASS_RE('Hlt1(?!ODIN)(?!L0)(?!Lumi)(?!Tell1)(?!MB)(?!NoBias)(?!NZS)(?!Velo)(?!BeamGas)(?!Incident).*Decision')"
+from Configurables import LoKi__HDRFilter
+filterHLT = LoKi__HDRFilter("FilterHLT",Code = regexp )
+
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+from Configurables import AuditorSvc, ChronoAuditor
+AuditorSvc().Auditors.append( ChronoAuditor("Chrono") )
+
+from Configurables import StrippingReport
+sr = StrippingReport(Selections = sc.selections())
+
+from Configurables import AlgorithmCorrelationsAlg
+ac = AlgorithmCorrelationsAlg(Algorithms = sc.selections())
+
+from Configurables import CondDB
+CondDB().IgnoreHeartBeat = True
+
+DaVinci().PrintFreq = 2000
+DaVinci().HistogramFile = 'DV_stripping_histos.root'
+DaVinci().EvtMax = 50000
+DaVinci().EventPreFilters = [ filterHLT ]
+DaVinci().appendToMainSequence( [ sc.sequence() ] )
+DaVinci().appendToMainSequence( [ sr ] )
+DaVinci().appendToMainSequence( [ ac ] )
+DaVinci().DataType  = "2011"
+DaVinci().InputType = "SDST"
+
+
+importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco09-Stripping13_SDSTs.py")
