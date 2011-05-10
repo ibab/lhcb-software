43eba5eadeb3fce35478d6577846cefba2913e04
Wenbin Qian <wenbin.qian@cern.ch>
2011-05-10 14:29:59 +0000

    modified input range for B2SameChargeMuon as suggested
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123031 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a823ed6b143f8aefc999ef65a29954a888c87f3f

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2SameChargeMuon.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2SameChargeMuon.py
@@ -52,13 +52,13 @@ class StrippingB2SameChargeMuonConf(LineBuilder):
 
         self.DsList = self._DsMaker([self.KaonList,self.PionList])
 
-        self._makeB2JpsiKPiPi([self.KaonList,self.PionList,self.JpsiList])
+        self._makeB2JpsiKPiPi([self.JpsiList,self.KaonList,self.PionList])
 
-        self._makeB2D0MuMuPi([self.MuonList,self.PionList,self.D0List])
+        self._makeB2D0MuMuPi([self.D0List,self.MuonList,self.PionList])
 
-        self._makeB2DpMuMu([self.MuonList,self.DpList])
+        self._makeB2DpMuMu([self.DpList,self.MuonList])
 
-        self._makeB2DsMuMu([self.MuonList,self.DsList])
+        self._makeB2DsMuMu([self.DsList,self.MuonList])
     
     def _MuonFilter( self ):       
         Muoncut = "(TRCHI2DOF < 5.0) & (PIDmu > 0.0)  & (MIPCHI2DV(PRIMARY) > 4.0) & (PT > 300*MeV)"
