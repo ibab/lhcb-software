400521dd124207ea184d18d90b1ad150968062ce
Sajan Easo <sajan.easo@cern.ch>
2011-05-10 16:38:50 +0000

    commit
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@123044 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 52a3d2ee8d8a79f0422d3f0dbd0f47db34a10d5b

---
+++ Gauss/Sim/GaussCherenkov/src/CherenkovMisc/CherenkovG4PmtReflTag.cpp
@@ -0,0 +1,47 @@
+// $Id: $
+// Include files 
+#include "GaussTools/GaussTrackInformation.h"
+#include "RichInfo.h"
+#include "RichPhotInfo.h"
+#include <bitset> 
+
+
+
+// local
+#include "CherenkovG4PmtReflTag.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : CherenkovG4PmtReflTag
+//
+// 2011-04-19 : Sajan Easo
+//-----------------------------------------------------------------------------
+
+extern void   RichG4PmtQWIncidentTag(const G4Track& aPhotonTk, const G4ThreeVector & aPmtQWPoint ){
+
+   G4VUserTrackInformation* aUserTrackInfo=aPhotonTk.GetUserInformation();
+   GaussTrackInformation* aRichPhotTrackInfo
+    = (GaussTrackInformation*)aUserTrackInfo;
+   if( aRichPhotTrackInfo) {
+    if( aRichPhotTrackInfo->detInfo() ){
+      RichInfo* aRichTypeInfo =
+        ( RichInfo*) (aRichPhotTrackInfo->detInfo());
+
+      if( aRichTypeInfo && aRichTypeInfo->HasUserPhotInfo() ){
+        RichPhotInfo* aRichPhotInfo =
+          aRichTypeInfo-> RichPhotInformation();
+        if( aRichPhotInfo ) {
+          // now add the info
+          if( aRichPhotInfo -> VerbosePhotTagFlag() ) {
+           
+	    aRichPhotInfo ->setHpdQWPhotIncidentPosition(aPmtQWPoint);
+
+	  }
+	}
+      }
+    }
+  }
+ 
+   
+}
+
+
