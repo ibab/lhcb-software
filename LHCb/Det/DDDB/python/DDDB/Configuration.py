adb4f0d48730a4457947e999b0a63134e3f5039d
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-05-14 13:16:46 +0000

    update default LHCBCOND tag to head-20110512 for 2009,2010,2011, to follow SQLDDDB v6r9
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@123315 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2db2a74a5509c10708ac6c9c8a2ff3ab06674a41

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -118,7 +118,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20110302")
-        self.__set_tag__(["LHCBCOND"], "head-20110407")
+        self.__set_tag__(["LHCBCOND"], "head-20110512")
         self.__set_tag__(["DQFLAGS"], "tt-20110126")
 
     def __2010_conf__(self):
@@ -127,7 +127,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20110303")
-        self.__set_tag__(["LHCBCOND"], "head-20110407")
+        self.__set_tag__(["LHCBCOND"], "head-20110512")
         self.__set_tag__(["DQFLAGS"], "tt-20110126")
 
     def __2009_conf__(self):
@@ -136,7 +136,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "head-20101206")
-        self.__set_tag__(["LHCBCOND"], "head-20110131")
+        self.__set_tag__(["LHCBCOND"], "head-20110512")
         self.__set_tag__(["SIMCOND"], "MC-20101026-vc15mm-md100")
         self.__set_tag__(["DQFLAGS"], "tt-20110126")
 
