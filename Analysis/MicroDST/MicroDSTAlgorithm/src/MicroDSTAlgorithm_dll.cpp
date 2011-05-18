1218c0a3203ae5b0b2d5510bbc59cf414022f102
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-05-18 12:12:02 +0000

    Change dependency from Phys/DaVinciMaps to Event/MicroDst
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@123481 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: afc4db11c46b2cc113763fa3283c25cc958c3da5

--- Analysis/MicroDST/MicroDSTAlgorithm/src/MicroDSTAlgorithm_dll.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: MicroDSTAlgorithm_dll.cpp,v 1.2 2008-08-29 06:00:01 jpalac Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES(MicroDSTAlgorithm)
-
