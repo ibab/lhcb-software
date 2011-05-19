6a58ac967a3fd5755e674eac357c9ecca8a96068
Thomas Blake <thomas.blake@cern.ch>
2011-05-19 14:38:29 +0000

    Updated makeDB.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123539 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c1599630a045b34058ffc97f1bff3add53ba9271

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/makeDB.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/makeDB.py
@@ -6,52 +6,33 @@ Generate a dbase module from a set of stripping line config
 dictionaries.
 
 Usage: 
-  > python makeDB python/StrippingSettings/StrippingDev 
+  > python makeDB <Stripping> 
 '''
 
-def usage( sys.argv ):
-   print "Usage:\n \t python " + sys.argv[0] + " <relative>/<path>" 
+import string
 
-def generate_dbase( rel_path ):
-   full_path = os.path.abspath( rel_path )
-   if os.path.exists( full_path ):
-      sys.path.append( full_path )
+def my_import(name):
+    mod = __import__(name)
+    components = string.split(name, '.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
 
-      print "Appending path to python path"
-      
-      from StrippingSettings.Utils import addModuleToDbase
-      
-      import LineConfigDictionaries_CharmXsec
-      import LineConfigDictionaries_GTWG    
-      import LineConfigDictionaries_Semileptonic
-      import LineConfigDictionaries_BetaS 
-      import LineConfigDictionaries_EWWG  
-      import LineConfigDictionaries_MiniBias  
-      import LineConfigDictionaries_Calib 
-      import LineConfigDictionaries_FWG       
-      import LineConfigDictionaries_Charm 
-      import LineConfigDictionaries_GLWG
-      import LineConfigDictionaries_RDWG          
-      
-      configs = (
-	 LineConfigDictionaries_CharmXsec
-         ,LineConfigDictionaries_GTWG    
-         ,LineConfigDictionaries_Semileptonic
-         ,LineConfigDictionaries_BetaS 
-         ,LineConfigDictionaries_EWWG      
-         ,LineConfigDictionaries_MiniBias    
-         ,LineConfigDictionaries_Calib 
-         ,LineConfigDictionaries_FWG      
-         ,LineConfigDictionaries_Charm 
-         ,LineConfigDictionaries_GLWG
-         ,LineConfigDictionaries_RDWG 
-         )
-      
-      for config in configs :
-         addModuleToDbase('stripping.tmp', config, verbose=True)
+def generate_dbase( config ):
+   
+   _strippingmod = my_import( 'StrippingSettings.'+config )
+   _modulenames  = _strippingmod.__all__
 
+   _configs  = [] 
+   
+   for _name in _modulenames:
+      _configs += [ my_import( 'StrippingSettings.'+config + '.'+_name ) ]
+         
+   from StrippingSettings.Utils import addModuleToDbase
 
+   for _config in _configs :
+      addModuleToDbase('stripping.tmp', _config, verbose=True)
+      
+  
 if 2 == len(sys.argv):
    generate_dbase( sys.argv[1] )
-else:
-   usage( sys.argv )
