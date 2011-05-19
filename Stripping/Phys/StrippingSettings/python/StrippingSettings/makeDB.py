0dd923aba77aab6772d8ac85c0394a0b5f1f2625
Thomas Blake <thomas.blake@cern.ch>
2011-05-19 13:01:58 +0000

    update makeDB.py to allow relative paths
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123532 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1dc49979523553c91610656ef155f5edf288a3da

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/makeDB.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/makeDB.py
@@ -1,32 +1,57 @@
-from StrippingSettings.Utils import *
+import os
+import sys
 
-import LineConfigDictionaries_CharmXsec
-import LineConfigDictionaries_GTWG    
-import LineConfigDictionaries_Semileptonic
-import LineConfigDictionaries_BetaS 
-import LineConfigDictionaries_EWWG  
-import LineConfigDictionaries_MiniBias  
-import LineConfigDictionaries_Calib 
-import LineConfigDictionaries_FWG       
-import LineConfigDictionaries_Charm 
-import LineConfigDictionaries_GLWG
-import LineConfigDictionaries_RDWG    
+'''
+Generate a dbase module from a set of stripping line config
+dictionaries.
 
-configs = (
+Usage: 
+  > python makeDB python/StrippingSettings/StrippingDev 
+'''
+
+def usage( sys.argv ):
+   print "Usage:\n \t python " + sys.argv[0] + " <relative>/<path>" 
+
+def generate_dbase( rel_path ):
+   full_path = os.path.abspath( rel_path )
+   if os.path.exists( full_path ):
+      sys.path.append( full_path )
+
+      print "Appending path to python path"
+      
+      from StrippingSettings.Utils import addModuleToDbase
+      
+      import LineConfigDictionaries_CharmXsec
+      import LineConfigDictionaries_GTWG    
+      import LineConfigDictionaries_Semileptonic
+      import LineConfigDictionaries_BetaS 
+      import LineConfigDictionaries_EWWG  
+      import LineConfigDictionaries_MiniBias  
+      import LineConfigDictionaries_Calib 
+      import LineConfigDictionaries_FWG       
+      import LineConfigDictionaries_Charm 
+      import LineConfigDictionaries_GLWG
+      import LineConfigDictionaries_RDWG          
+      
+      configs = (
 	 LineConfigDictionaries_CharmXsec
-	,LineConfigDictionaries_GTWG    
-	,LineConfigDictionaries_Semileptonic
-	,LineConfigDictionaries_BetaS 
-	,LineConfigDictionaries_EWWG      
-	,LineConfigDictionaries_MiniBias    
-	,LineConfigDictionaries_Calib 
-	,LineConfigDictionaries_FWG      
-	,LineConfigDictionaries_Charm 
-        ,LineConfigDictionaries_GLWG
-	,LineConfigDictionaries_RDWG 
-	)
+         ,LineConfigDictionaries_GTWG    
+         ,LineConfigDictionaries_Semileptonic
+         ,LineConfigDictionaries_BetaS 
+         ,LineConfigDictionaries_EWWG      
+         ,LineConfigDictionaries_MiniBias    
+         ,LineConfigDictionaries_Calib 
+         ,LineConfigDictionaries_FWG      
+         ,LineConfigDictionaries_Charm 
+         ,LineConfigDictionaries_GLWG
+         ,LineConfigDictionaries_RDWG 
+         )
+      
+      for config in configs :
+         addModuleToDbase('stripping.tmp', config, verbose=True)
 
-for config in configs :
-   addModuleToDbase('stripping.tmp', config, verbose=True)
 
-
+if 2 == len(sys.argv):
+   generate_dbase( sys.argv[1] )
+else:
+   usage( sys.argv )
