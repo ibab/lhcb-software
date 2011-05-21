5e29c0374790943e6c54e39a688ae0e1aca18779
Thomas Blake <thomas.blake@cern.ch>
2011-05-21 17:38:23 +0000

    Updated Stripping14 dbase to add betas, FWG, GT and GL lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1e658c9b8902352489abd2d5f99f70748083bd7f

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping14/__init__.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping14/__init__.py
@@ -2,4 +2,8 @@ __all__ = [ "LineConfigDictionaries_Charm",
             "LineConfigDictionaries_Calib",
             "LineConfigDictionaries_EW",
             "LineConfigDictionaries_RDWG",
-            "LineConfigDictionaries_Semileptonic" ]
+            "LineConfigDictionaries_Semileptonic",
+            "LineConfigDictionaries_BetaS",
+            "LineConfigDictionaries_GTWG",
+            "LineConfigDictionaries_GLWG",
+            "LineConfigDictionaries_FWG" ]
