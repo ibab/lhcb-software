9b622fcd31c4d8dd86113da2002212de577afc59
gmcgrego <gmcgrego@cern.ch>
2011-05-26 13:15:49 +0000

    Addition of a few cuts to DStarD02KMuNu line to reduce rate.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123839 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 16f1d2ff55eb115a6ad1a11fee6d772a2556097e

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarD02KMuNu.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarD02KMuNu.py
@@ -16,7 +16,7 @@ Exported symbols (use python help!):
 
 __author__ = ['Grant McGregor']
 __date__ = '17/02/2010'
-__version__ = '$Revision: 1.0 $'
+__version__ = '$Revision: 2.0 $'
 
 __all__ = ('DstarD02KMuNuConf',
            'makeMuons',
@@ -39,18 +39,19 @@ default_config = {'TRACK_Chi2' : 3.,
     'KAON_MinPT' : 600.,
     'MUON_MinPT' : 800.,
     'PION_PIDmu' : -1.,
-    'PION_MaxP' : 25000.,
+    'PAIR_SumPTMin' : 2800.,
     'D0_MassMax' : 1950.,
     'D0_DOCA' : 0.07,
-    'D0_MinP' : 18000.,
+    'D0_MinP' : 20000.,
+    'D0_FD'  :  4.0,
+    'D0_MCORR_MIN'  : 1400.,
+    'D0_MCORR_MAX'  : 2700.,
     'D0_BPVVDZ' : 0.,
     'D0_VtxChi2' : 9.,
     'Dstar_MassMin' : 800.,
     'Dstar_DOCA' : 0.4,
     'Dstar_VtxChi2' : 9.,
     'Dstar_DeltaMass' : 200.,
-    # Trigger
-    'HLT'          : " HLT_PASS_RE('Hlt2CharmSemilepD02HMuNu.*Decision') " ,
     #Pre and postscale
     'PrescaledLinePrescale' : 1.0,
     'PrescaledLinePostscale' : 1.0
@@ -65,10 +66,10 @@ class DstarD02KMuNuConf(LineBuilder) :
     Constructs D* -> D0 Pi Selections and StrippingLines from a configuration dictionary.
     Usage:
     >>> from StrippingSelections.StrippingDstarD02KMuNu import DstarD02KMuNuConf
-    >>> config_params = { 'TRACK_Chi2' :  4., .... etc }
-    >>> DstarD02KMuNuConf = DstarD02KMuNuConf('DstarD02KMuNuTest',config_params)
-    >>> DstarLines = DstarD02KMuNuConf.lines()
-    >>> for line in DstarLines :
+    >>> from StrippingSelections.StrippingDstarD02KMuNu  import default_config as DstarD02KMuNu_config
+    >>> DstarD02KMuNuConf = DstarD02KMuNuConf('DstarD02KMuNu', DstarD02KMuNu_config)
+    >>> DstarD02KMuNuLines = DstarD02KMuNuConf.lines()
+    >>> for line in DstarD02KMuNuLines :
     >>>  print line.name(), line.outputLocation()
     The lines can be used directly to build a StrippingStream object.
 
@@ -92,17 +93,19 @@ class DstarD02KMuNuConf(LineBuilder) :
                               'KAON_MinPT',
                               'MUON_MinPT',
 			      'PION_PIDmu',
-			      'PION_MaxP',
+                              'PAIR_SumPTMin',
 			      'D0_MassMax',
 			      'D0_DOCA',
 			      'D0_MinP',
+			      'D0_FD',
+			      'D0_MCORR_MIN',
+			      'D0_MCORR_MAX',
 			      'D0_BPVVDZ',
 			      'D0_VtxChi2',
 			      'Dstar_MassMin',
 			      'Dstar_DOCA',
 			      'Dstar_VtxChi2',
 			      'Dstar_DeltaMass',
-			      'HLT',
                               'PrescaledLinePrescale',
                               'PrescaledLinePostscale'
                               )
@@ -124,24 +127,31 @@ class DstarD02KMuNuConf(LineBuilder) :
 
         self.selPions = makePions( 'PionsFor'+prescaled_name, 
                                      TRACK_Chi2 = config['TRACK_Chi2'],
-				     PION_PIDmu = config['PION_PIDmu'], 
-				     PION_MaxP = config['PION_MaxP'])
+				     PION_PIDmu = config['PION_PIDmu'])
 
         self.selD02KMuNuRS = makeD02KMuNuRS('D0RSFor'+prescaled_name,  
                                             muonSel = self.selMuons, 
                                             kaonSel = self.selKaons,
+					    PAIR_SumPTMin = config['PAIR_SumPTMin'],
                                             D0_MassMax = config['D0_MassMax'],
                                             D0_DOCA = config['D0_DOCA'],
                                             D0_MinP = config['D0_MinP'],
+					    D0_FD = config['D0_FD'],
+					    D0_MCORR_MIN = config['D0_MCORR_MIN'],
+					    D0_MCORR_MAX = config['D0_MCORR_MAX'],
                                             D0_BPVVDZ = config['D0_BPVVDZ'],
                                             D0_VtxChi2 = config['D0_VtxChi2'])
 
         self.selD02KMuNuWS = makeD02KMuNuWS('D0WSFor'+prescaled_name,  
                                             muonSel = self.selMuons, 
                                             kaonSel = self.selKaons,
+					    PAIR_SumPTMin = config['PAIR_SumPTMin'],
                                             D0_MassMax = config['D0_MassMax'],
                                             D0_DOCA = config['D0_DOCA'],
                                             D0_MinP = config['D0_MinP'],
+					    D0_FD = config['D0_FD'],
+					    D0_MCORR_MIN = config['D0_MCORR_MIN'],
+					    D0_MCORR_MAX = config['D0_MCORR_MAX'],
                                             D0_BPVVDZ = config['D0_BPVVDZ'],
                                             D0_VtxChi2 = config['D0_VtxChi2'])
 
@@ -163,13 +173,11 @@ class DstarD02KMuNuConf(LineBuilder) :
                                             Dstar_DeltaMass = config['Dstar_DeltaMass'])
 
 	self.prescaled_lineRS = StrippingLine(prescaled_name+"RSLine",
-		HLT      = config['HLT'],
 		prescale = config['PrescaledLinePrescale'],
 		postscale = config['PrescaledLinePostscale'],
 		selection = self.selDstarD02KMuNuRS
 		)
 	self.prescaled_lineWS = StrippingLine(prescaled_name+"WSLine",
-		HLT      = config['HLT'],
 		prescale = config['PrescaledLinePrescale'],
 		postscale = config['PrescaledLinePostscale'],
 		selection = self.selDstarD02KMuNuWS
@@ -211,14 +219,13 @@ def makeKaons(name,
 
 def makePions(name, 
           TRACK_Chi2, 
-	  PION_PIDmu, 
-	  PION_MaxP) :
+	  PION_PIDmu) :
     """
     Create pions for D*->D0 pi
     Starts from StandardParticles/StdLoosePions
     name     : name of the Selection.
     """
-    _code = "(TRCHI2DOF < %(TRACK_Chi2)s ) & ( %(PION_PIDmu)s < PIDpi - PIDmu) & (P < %(PION_MaxP)s *MeV) " % locals()
+    _code = "(TRCHI2DOF < %(TRACK_Chi2)s ) & ( %(PION_PIDmu)s < PIDpi - PIDmu) " % locals()
     _pionFilter = FilterDesktop (Code = _code ) 
     return Selection (name,
 	              Algorithm = _pionFilter,
@@ -227,9 +234,13 @@ def makePions(name,
 def makeD02KMuNuRS(name,
                    muonSel,
 		   kaonSel,
+		   PAIR_SumPTMin,
 		   D0_MassMax,
 		   D0_DOCA,
 		   D0_MinP,
+		   D0_FD,
+		   D0_MCORR_MIN,
+		   D0_MCORR_MAX,
 		   D0_BPVVDZ,
 		   D0_VtxChi2) :
     """
@@ -238,8 +249,11 @@ def makeD02KMuNuRS(name,
     name             : name of the Selection.
     """
     _combinationCuts = "(AM < %(D0_MassMax)s *MeV) "\
+    "& ((APT1+APT2) > %(PAIR_SumPTMin)s *MeV) "\
     "& (AMAXDOCA('') < %(D0_DOCA)s *mm )" % locals()
     _motherCuts = "(P > %(D0_MinP)s *MeV)  "\
+    "& (BPVVD > %(D0_FD)s *mm) "\
+    "& (in_range(%(D0_MCORR_MIN)s *MeV ,BPVCORRM,%(D0_MCORR_MAX)s *MeV)) "\
     "& (BPVVDZ > %(D0_BPVVDZ)s *mm) "\
     "& (VFASPF(VCHI2/VDOF)<%(D0_VtxChi2)s) " % locals()
     _D02KMuNuRS = CombineParticles( DecayDescriptor = "[D0 -> mu+ K-]cc",
@@ -253,10 +267,14 @@ def makeD02KMuNuRS(name,
 def makeD02KMuNuWS(name,
                    muonSel,
 		   kaonSel,
+		   PAIR_SumPTMin,
 		   D0_MassMax,
 		   D0_DOCA,
 		   D0_MinP,
+		   D0_MCORR_MIN,
+		   D0_MCORR_MAX,
 		   D0_BPVVDZ,
+		   D0_FD,
 		   D0_VtxChi2) :
     """
     Create and return a wrong sign D0 -> KMuNu selection object.
@@ -264,8 +282,11 @@ def makeD02KMuNuWS(name,
     name             : name of the Selection.
     """
     _combinationCuts = "(AM < %(D0_MassMax)s *MeV) "\
+    "& ((APT1+APT2) > %(PAIR_SumPTMin)s *MeV) "\
     "& (AMAXDOCA('') < %(D0_DOCA)s *mm )" % locals()
     _motherCuts = "(P > %(D0_MinP)s *MeV)  "\
+    "& (BPVVD > %(D0_FD)s *mm) "\
+    "& (in_range(%(D0_MCORR_MIN)s *MeV ,BPVCORRM,%(D0_MCORR_MAX)s *MeV)) "\
     "& (BPVVDZ > %(D0_BPVVDZ)s *mm) "\
     "& (VFASPF(VCHI2/VDOF)<%(D0_VtxChi2)s)  " % locals()
     _D02KMuNuWS = CombineParticles( DecayDescriptor = "[D0 -> mu+ K+]cc",
@@ -319,8 +340,7 @@ def makeDstarD02KMuNuWS(name,
     """
     _combinationCuts = "(AM > %(Dstar_MassMin)s *MeV) "\
     "& (AMAXDOCA('') < %(Dstar_DOCA)s *mm)" % locals()
-    _motherCuts = " (VFASPF(VCHI2/VDOF)< %(Dstar_VtxChi2)s ) "\
-    "& ( ( M - CHILD(M,1) ) < %(Dstar_DeltaMass)s *MeV )  " % locals()
+    _motherCuts = " (VFASPF(VCHI2/VDOF)< %(Dstar_VtxChi2)s ) & ( ( M - CHILD(M,1) ) < %(Dstar_DeltaMass)s *MeV )  " % locals()
 
     _DstarD02KMuNuWS = CombineParticles( DecayDescriptors = [
                                          " [D*(2010)+ -> D0 pi+]cc" ,
