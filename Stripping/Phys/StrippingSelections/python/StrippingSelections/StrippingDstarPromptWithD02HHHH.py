d5b629ca9f4c746a448d50cf7ece51f3eebd5223
phunt <phunt@cern.ch>
2011-05-27 16:06:36 +0000

    Fixed application of GECs in StrippingDstarPromptWithD02HHHH.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@123932 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ff3d2499d6f8bd301deb95c21efef0f106900502

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHHH.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingDstarPromptWithD02HHHH.py
@@ -148,25 +148,25 @@ class DstarPromptWithD02HHHHConf(LineBuilder):
       if nLong is not None:
         if _filter != "":
           _filter+=" & "
-          _filter += "(TrSOURCE('Rec/Track/Best') >> TrLONG >> (TrSIZE < %s))" %nLong
+        _filter += "(TrSOURCE('Rec/Track/Best') >> TrLONG >> (TrSIZE < %s))" %nLong
           
       nDigits = config["MaxSpdDigits"]
       if nDigits is not None:
         if _filter != "":
           _filter+=" & "
-          _filter += "(CONTAINS('Raw/Spd/Digits') < %s)" %nDigits
+        _filter += "(CONTAINS('Raw/Spd/Digits') < %s)" %nDigits
 
       nClusters = config["MaxITClusters"]
       if nClusters is not None:
         if _filter != "":
           _filter+=" & "
-          _filter += "(CONTAINS('Raw/IT/Clusters') < %s)" %nClusters
+        _filter += "(CONTAINS('Raw/IT/Clusters') < %s)" %nClusters
 
       nVELO = config["MaxVeloTracks"]
       if nVELO is not None:
         if _filter != "":
           _filter+=" & "
-          _filter += "(TrSOURCE('Rec/Track/Best') >> TrVELO >> (TrSIZE < %s))" %nVELO
+        _filter += "(TrSOURCE('Rec/Track/Best') >> TrVELO >> (TrSIZE < %s))" %nVELO
       if _filter=="": _filter=None # no filters
            
     self.line_tagged_d02hhhh = StrippingLine(
