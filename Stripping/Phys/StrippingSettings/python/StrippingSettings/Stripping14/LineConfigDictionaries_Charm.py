d39cf6d3dd2efc17dc2291899cc0a2a927a13182
Patrick Spradlin <patrick.spradlin@cern.ch>
2011-06-02 12:55:51 +0000

    Updated StrippingDev dictionaries for the Charm WG
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@124193 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bfac92309ec4b023955369cc053a577aba0455e

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping14/LineConfigDictionaries_Charm.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping14/LineConfigDictionaries_Charm.py
@@ -22,81 +22,6 @@ CharmedAndCharmedStrangeSpectroscopy = {
     'STREAMS' : [ 'Charm' ]
     }
 
-DstarD2KShh = {
-    'BUILDERTYPE' : 'DstarD2KShhBuilder',
-    'CONFIG'      : {
-    'LongTrackGEC'          : 150      ## Global event cut on number of long tracks
-    ,'KSLLCutDIRA'          : 0.9997   ## unitless
-    ,'KSDDCutDIRA'          : 0.99995  ## unitless
-    ,'KSLLCutMass'          : 11.4     ## in MeV
-    ,'KSDDCutMass'          : 24.9     ## in MeV
-    ,'KSLLCutFDChi2'        : 100      ## unitless
-    ,'KSDDCutFDChi2'        : 100      ## unitless
-    ,'trackFromDCutP'       : 1500.0   ## in MeV
-    ,'trackFromDCutPIDe'    : 10.0     ## unitless -- note that this means (DLLe - DLLx) < 10 for x=K,pi
-    ,'trackFromDCutPIDp'    : 15.0     ## unitless -- note that this means (DLLp - DLLx) < 15 for x=K,pi
-    ,'pionFromDCutPIDK'     : -5.0     ## unitless -- note that this means (DLLpi - DLLK) < -5, i.e. (DLLK-DLLpi) > +5
-    ,'kaonFromDCutPIDpi'    : 0.0      ## unitless -- note that this means (DLLK - DLLpi) < 0
-    ,'pionFromDCutTRCHI2DOF': 4.0      ## unitless
-    ,'kaonFromDCutTRCHI2DOF': 5.0      ## unitless
-    ,'DCutDIRA'             : 0.0      ## unitless -- remove "backwards-going" D0
-    ,'DCutTau'              : 0.3      ## ps
-    ,'DCutVtxChi2_KK'       : 21.0     ## unitless
-    ,'DCutVtxChi2_KP'       : 15.0     ## unitless
-    ,'DCutVtxChi2_PP'       : 13.0     ## unitless
-    ,'preFitDCutPT'         : 1500.0   ## in MeV
-    ,'preFitDMassCut_LL'    : 130      ## in MeV
-    ,'preFitDMassCut_DD'    : 270      ## in MeV
-    ,'wideDMassCut_KKLL'    : 57.2     ## in MeV
-    ,'wideDMassCut_KPLL'    : 92.0     ## in MeV
-    ,'wideDMassCut_PPLL'    : 110.0    ## in MeV
-    ,'wideDMassCut_KKDD'    : 72.0     ## in MeV
-    ,'wideDMassCut_KPDD'    : 120.0    ## in MeV
-    ,'wideDMassCut_PPDD'    : 140.0    ## in MeV
-    ,'preFitDstarMassCut'   : 200.0    ## in MeV; make sure it's well above the largest D mass window
-    ,'wideDMCutLower'       : 0.0      ## in MeV
-    ,'wideDMCutUpper'       : 15.0     ## in MeV
-    ,'DstarCutPT_KK'        : 2500.0   ## in MeV
-    ,'DstarCutPT_KP'        : 3000.0   ## in MeV
-    ,'DstarCutPT_PP'        : 3000.0   ## in MeV
-    ,'DstarCutChi2NDOF_KK'  : 60.0     ## unitless
-    ,'DstarCutChi2NDOF_KP'  : 20.0     ## unitless
-    ,'DstarCutChi2NDOF_PP'  : 20.0     ## unitless
-    ,'SoftPionCutPIDe'      : 5.0      ## unitless
-    ,'KKLLPrescale'         : 1.0
-    ,'KKLLPostscale'        : 1.0
-    ,'KPLLPrescale'         : 1.0
-    ,'KPLLPostscale'        : 1.0
-    ,'PPLLPrescale'         : 1.0
-    ,'PPLLPostscale'        : 1.0
-    ,'KKDDPrescale'         : 1.0
-    ,'KKDDPostscale'        : 1.0
-    ,'KPDDPrescale'         : 1.0
-    ,'KPDDPostscale'        : 1.0
-    ,'PPDDPrescale'         : 1.0
-    ,'PPDDPostscale'        : 1.0
-    },
-    'WGs' : ['Charm'],
-    'STREAMS' : [ 'CharmCompleteEvent' ]
-    }
-
-
-# Mat Charles, Micro-DST
-# Test: Yes. "name" already decalred at top of file. Renamed to default_name to avoid clash
-
-Xicc = {
-    'BUILDERTYPE' : 'XiccBuilder',
-    'CONFIG'      : {
-    'LongTrackGEC' : 150
-    ,'controlPrescaleLc' : 0.05
-    ,'controlPrescaleXic' : 1.0
-    ,'signalPrescaleViaLc' : 1.0
-    ,'signalPrescaleViaXic' : 1.0
-    },
-    'WGs' : [ 'Charm' ],
-    'STREAMS' : [ 'Charm' ] 
-    }
-
 #Micro-DST, Vanya BELYAEV
 #Test: Yes. Only the lambdac line is needed.
 PromptCharm = {
@@ -188,6 +113,24 @@ D2XMuMu = {
     'STREAMS' : [ 'CharmCompleteEvent' ]
     }
 
+
+# Mat Charles, Micro-DST
+# Test: Yes. "name" already decalred at top of file. Renamed to default_name to avoid clash
+
+Xicc = {
+    'BUILDERTYPE' : 'XiccBuilder',
+    'CONFIG'      : {
+    'LongTrackGEC' : 150
+    ,'controlPrescaleLc' : 0.05
+    ,'controlPrescaleXic' : 1.0
+    ,'signalPrescaleViaLc' : 1.0
+    ,'signalPrescaleViaXic' : 1.0
+    },
+    'WGs' : [ 'Charm' ],
+    'STREAMS' : [ 'Charm' ] 
+    }
+
+
 # Benoit Vaud, Full ? Micro ? 
 # Test : Yes 
 
@@ -268,4 +211,64 @@ DstarD02KKpipi = {
     'WGs' : ['Charm'],
     'STREAMS' : [ 'Charm' ]
     }
-    
+
+
+DstarD2KShh = {
+    'BUILDERTYPE' : 'DstarD2KShhBuilder',
+    'CONFIG'      : {
+    'LongTrackGEC'          : 150      ## Global event cut on number of long tracks
+    ,'KSLLCutDIRA'          : 0.9997   ## unitless
+    ,'KSDDCutDIRA'          : 0.99995  ## unitless
+    ,'KSLLCutMass'          : 11.4     ## in MeV
+    ,'KSDDCutMass'          : 24.9     ## in MeV
+    ,'KSLLCutFDChi2'        : 100      ## unitless
+    ,'KSDDCutFDChi2'        : 100      ## unitless
+    ,'trackFromDCutP'       : 1500.0   ## in MeV
+    ,'trackFromDCutPIDe'    : 10.0     ## unitless -- note that this means (DLLe - DLLx) < 10 for x=K,pi
+    ,'trackFromDCutPIDp'    : 15.0     ## unitless -- note that this means (DLLp - DLLx) < 15 for x=K,pi
+    ,'pionFromDCutPIDK'     : -5.0     ## unitless -- note that this means (DLLpi - DLLK) < -5, i.e. (DLLK-DLLpi) > +5
+    ,'kaonFromDCutPIDpi'    : 0.0      ## unitless -- note that this means (DLLK - DLLpi) < 0
+    ,'pionFromDCutTRCHI2DOF': 4.0      ## unitless
+    ,'kaonFromDCutTRCHI2DOF': 5.0      ## unitless
+    ,'DCutDIRA'             : 0.0      ## unitless -- remove "backwards-going" D0
+    ,'DCutTau'              : 0.3      ## ps
+    ,'DCutVtxChi2_KK'       : 21.0     ## unitless
+    ,'DCutVtxChi2_KP'       : 15.0     ## unitless
+    ,'DCutVtxChi2_PP'       : 13.0     ## unitless
+    ,'preFitDCutPT'         : 1500.0   ## in MeV
+    ,'preFitDMassCut_LL'    : 130      ## in MeV
+    ,'preFitDMassCut_DD'    : 270      ## in MeV
+    ,'wideDMassCut_KKLL'    : 57.2     ## in MeV
+    ,'wideDMassCut_KPLL'    : 92.0     ## in MeV
+    ,'wideDMassCut_PPLL'    : 110.0    ## in MeV
+    ,'wideDMassCut_KKDD'    : 72.0     ## in MeV
+    ,'wideDMassCut_KPDD'    : 120.0    ## in MeV
+    ,'wideDMassCut_PPDD'    : 140.0    ## in MeV
+    ,'preFitDstarMassCut'   : 200.0    ## in MeV; make sure it's well above the largest D mass window
+    ,'wideDMCutLower'       : 0.0      ## in MeV
+    ,'wideDMCutUpper'       : 15.0     ## in MeV
+    ,'DstarCutPT_KK'        : 2500.0   ## in MeV
+    ,'DstarCutPT_KP'        : 3000.0   ## in MeV
+    ,'DstarCutPT_PP'        : 3000.0   ## in MeV
+    ,'DstarCutChi2NDOF_KK'  : 60.0     ## unitless
+    ,'DstarCutChi2NDOF_KP'  : 20.0     ## unitless
+    ,'DstarCutChi2NDOF_PP'  : 20.0     ## unitless
+    ,'SoftPionCutPIDe'      : 5.0      ## unitless
+    ,'KKLLPrescale'         : 1.0
+    ,'KKLLPostscale'        : 1.0
+    ,'KPLLPrescale'         : 1.0
+    ,'KPLLPostscale'        : 1.0
+    ,'PPLLPrescale'         : 1.0
+    ,'PPLLPostscale'        : 1.0
+    ,'KKDDPrescale'         : 1.0
+    ,'KKDDPostscale'        : 1.0
+    ,'KPDDPrescale'         : 1.0
+    ,'KPDDPostscale'        : 1.0
+    ,'PPDDPrescale'         : 1.0
+    ,'PPDDPostscale'        : 1.0
+    },
+    'WGs' : ['Charm'],
+    'STREAMS' : [ 'CharmCompleteEvent' ]
+    }
+
+
