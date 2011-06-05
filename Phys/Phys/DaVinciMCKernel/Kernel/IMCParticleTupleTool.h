df910d79b397427a7d1e66ed5c6a542f6bf39ff5
Vanya Belyaev <ivan.belyaev@cern.ch>
2011-06-05 16:03:42 +0000

    fix ICC warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@124246 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5bb3a2cb05c0b29bb4e39c1962f3bd614f40d8f0

--- Phys/Phys/DaVinciMCKernel/Kernel/IMCParticleTupleTool.h
+++ Phys/Phys/DaVinciMCKernel/Kernel/IMCParticleTupleTool.h
@@ -1,7 +1,8 @@
 // $Id$
+// ============================================================================
 #ifndef DECAYTUPLE_IMCPARTICLETUPLETOOL_H
 #define DECAYTUPLE_IMCPARTICLETUPLETOOL_H  1
-
+// ============================================================================
 // Include files
 // from STL
 #include <string>
@@ -13,11 +14,10 @@ static const InterfaceID IID_IMCParticleTupleTool ( "IMCParticleTupleTool", 1, 0
 
 namespace Tuples{
   class Tuple;
-};
+}
 namespace LHCb {
   class MCParticle;
-};
-
+}
 /** @class IMCParticleTupleTool
  * 
  * Fill some mc-particle related variables into a tuple.
@@ -28,22 +28,23 @@ namespace LHCb {
  * @author patrick.koppenburg@cern.ch
  * @date 19 January 2009
  */
-class IMCParticleTupleTool : virtual public IAlgTool {
- public:
+class IMCParticleTupleTool : virtual public IAlgTool 
+{
+public:
   virtual ~IMCParticleTupleTool(){};
   // Return the interface ID
   static const InterfaceID& interfaceID() { return IID_IMCParticleTupleTool; }
-
+  
   //! Fill the tuple. 
-
+  
   //! - \b top  : may be used to provide additional information about \b part, like the top particle of the decay.
   //! - \b part : the particle about which some info are filled.
   //! - \b head : prefix for the tuple column name.
   //! - \b tuple: the tuple to fill
-  virtual StatusCode fill( const LHCb::MCParticle* top
-			   , const LHCb::MCParticle* part
-			   , const std::string& head
-			   , Tuples::Tuple& tuple )=0;
+  virtual StatusCode fill( const LHCb::MCParticle* top   , 
+                           const LHCb::MCParticle* part  , 
+                           const std::string&      head  , 
+                           Tuples::Tuple&          tuple ) = 0 ;
 };
 
 #endif // DECAYTUPLE_IMCPARTICLETUPLETOOL_H
