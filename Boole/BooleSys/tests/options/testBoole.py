446333fb68b5054a10accab2428cc56ff5aa2af3
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-06-07 15:24:56 +0000

    remove support for L0ETC
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@124404 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: add443daf83a5f2bc54805cfb644ae3259d67fdd

--- Boole/BooleSys/tests/options/testBoole.py
+++ Boole/BooleSys/tests/options/testBoole.py
@@ -6,7 +6,7 @@ from Boole.Configuration import *
 from Configurables import CondDBAccessSvc, LHCbApp
 
 Boole().EvtMax   = 0
-Boole().Outputs  = ["DIGI","MDF","L0ETC"] # Test all output types
+Boole().Outputs  = ["DIGI","MDF"] # Test all output types
 Boole().Histograms = "NONE"
 
 LHCbApp().CondDBtag = "sim-20100412-vc-md100"
