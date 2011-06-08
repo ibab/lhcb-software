32bd69b0dbfb203a7490df27d78ec860f15abe08
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-06-08 11:16:18 +0000

    minor fix to allow for yearly ONLINE snapshots
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@124422 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e45faf61371261d948e1d5588ae901bd659cf839

--- LHCb/GaudiConf/python/GaudiConf/QMTest/LHCbExclusions.py
+++ LHCb/GaudiConf/python/GaudiConf/QMTest/LHCbExclusions.py
@@ -2,7 +2,7 @@ from GaudiTest import normalizeExamples, RegexpReplacer, LineSkipper
 
 preprocessor = normalizeExamples + \
     RegexpReplacer(when = "Connected to database",
-        orig = r'ONLINE[_-][0-9]{1,6}(.*)"[^"]*/([0-9A-Z_]{1,8})"',
+        orig = r'ONLINE[_-][0-9 ]{1,6}(.*)"[^"]*/([0-9A-Z_]{1,8})"',
         repl = r'ONLINE_xxxxxx\1"\2"') + \
     RegexpReplacer(when = "Connected to database",
         orig = r'"[^"]*/([0-9A-Z_]{1,8})"',
