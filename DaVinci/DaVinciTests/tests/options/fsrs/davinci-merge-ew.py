900912dcd314c818586b2e803d45470c66dd856c
rlambert <rlambert@cern.ch>
2011-06-09 12:35:52 +0000

    updated fsr test suite
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@124471 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 79ece4f70c48e0a038b62a831f7b648ef66a0e1c

--- DaVinci/DaVinciTests/tests/options/fsrs/davinci-merge-ew.py
+++ DaVinci/DaVinciTests/tests/options/fsrs/davinci-merge-ew.py
@@ -2,4 +2,4 @@ from GaudiConf.Configuration import *
 importOptions('$APPCONFIGOPTS/Merging/DV-Stripping12-Merging.py')
 importOptions('$APPCONFIGOPTS/Merging/WriteFSR.py')
 importOptions('$DAVINCITESTSROOT/tests/options/fsrs/davinci-merge.py')
-EventSelector().Input = ["DATAFILE='PFN:MagUp.V0.dst' TYP='POOL_ROOTTREE' OPT='READ'", "DATAFILE='PFN:MagDown.V0.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
+EventSelector().Input = ["DATAFILE='PFN:MagUp.EW.dst' TYP='POOL_ROOTTREE' OPT='READ'", "DATAFILE='PFN:MagDown.EW.dst' TYP='POOL_ROOTTREE' OPT='READ'"]
---
+++ DaVinci/DaVinciTests/tests/options/mc09_testdata.py
@@ -0,0 +1,5 @@
+from Gaudi.Configuration import EventSelector
+#event-type 24142000 
+EventSelector().Input   = [
+"   DATAFILE='castor:/castor.cern.ch/user/r/rlambert/testfiles/110012001_MC09_Data_MC09.SeqB0q2DplusMuX.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+]
