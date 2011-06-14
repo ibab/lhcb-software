0fb3c25b4fd21a8c1b132f7324d58f768a3d3c52
rlambert <rlambert@cern.ch>
2011-06-14 15:05:25 +0000

    Support MDFWriter in IOHelper.outputAlgs, Add IOExtension class, holding useful functions when the file type needs to be determined from the file extension
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@124677 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b9893134e5c26736e245314fedeaf81b5ecaf9cd

---
+++ LHCb/GaudiConf/tests/test_ioextension.py
@@ -0,0 +1,67 @@
+from GaudiConf import (IOHelper, IOExtension)
+
+exts=['.RAW','.mdf','.digi','.sim','.sdst','.xdst','.mdst','.dst']
+
+def testthisiox(iox):
+    print "- test file detection and dressing"
+    inputs=[]
+    basename='eggs'
+    for ext in exts:
+        basename+='.spam_'
+        file = basename+ext
+        inputs.append(file)
+        print iox.detectFileType(file)
+        print iox.detectMinType(inputs)
+        for IO in ['I','O']:
+            print iox.dressFile(file,IO)
+
+    print '- check you take the last extension'
+    print iox.detectFileType(''.join(exts))
+    print iox.detectFileType(''.join(exts+[exts[0]]))
+    
+    print "- test getting ioh"
+    
+    for ext in exts:
+        ioh=iox.getIOHelper(basename+ext)
+        print ioh
+        for oext in exts:
+            ioh=iox.getCompleteIOHelper([basename+ext],basename+oext)
+            print ioh
+    
+    print "- test inputting files"
+
+    print inputs
+    iox.inputFiles(inputs)
+    
+    from Gaudi.Configuration import EventSelector
+    print EventSelector().Input
+    
+    print "- test outputting files"
+    
+    for ext in exts:
+        iox.outputAlgs(basename+ext,writer='OutputStream/Alg'+ext.replace('.',''))
+        iox.outStream(basename+ext,writer='OutputStream/Str'+ext.replace('.',''))
+    
+    streams=IOHelper().activeStreams()
+    print streams
+    for stream in streams:
+        print stream.Output
+    
+    print "- test file extensions which should not be known"
+    print [iox.detectFileType('ni'+file) for file in ext]
+    
+
+for persistency in [None,'POOL','ROOT']:
+    print '============================='
+    print persistency
+    print '============================='
+
+    print "- input conversion tests "
+    #preload with MDF
+    
+    iox=IOExtension(persistency)
+    
+    from Gaudi.Configuration import EventSelector
+    EventSelector().Input=[]
+    
+    testthisiox(iox)
