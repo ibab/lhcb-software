31bb4e91b97737660f52118aa987dd59712c77fc
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2011-06-15 14:39:49 +0000

    add proc status algo
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@124805 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d5edd816caa5764658d9cc5a3d50cfe66895d828

---
+++ Rec/Rec/RecAlgs/src/AddToProcStatus.cpp
@@ -0,0 +1,94 @@
+// $Id: $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h" 
+
+// local
+#include "Event/ProcStatus.h"
+#include "AddToProcStatus.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : AddToProcStatus
+//
+// 2011-06-15 : Patrick Koppenburg
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( AddToProcStatus );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+AddToProcStatus::AddToProcStatus( const std::string& name,
+                                  ISvcLocator* pSvcLocator)
+  : GaudiAlgorithm ( name , pSvcLocator )
+{
+  //  addAlgorithmStatus (const std::string &algName, const std::string &subsystem, 
+  //                    const std::string &reason, const int status, const bool eventAborted)
+  declareProperty("Subsystem", m_subsystem = "", "Subsystem that has aborted");  
+  declareProperty("Reason", m_reason = "", "Reason");
+  declareProperty("Status", m_status = -3, "Status Code");
+  declareProperty("Abort", m_abort = true, "Abort Event?");
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+AddToProcStatus::~AddToProcStatus() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode AddToProcStatus::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+  if ( ""==m_reason) {
+    Error("Please provide a Reason");
+    return StatusCode::FAILURE;
+  }
+  if ( ""==m_subsystem) {
+    Error("Please provide a Subsystem");
+    return StatusCode::FAILURE;
+  }
+
+  info() << "Whenever called will add the following to ProcStatus:" << endmsg;
+  info() << "  Algorithm: " << name() << ", Subsystem: " << m_subsystem 
+         << ", Status: " << m_status << ", Abort: " << m_abort << endmsg ;
+  
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode AddToProcStatus::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+    
+  LHCb::ProcStatus* procStat =
+    getOrCreate<LHCb::ProcStatus,LHCb::ProcStatus>(LHCb::ProcStatusLocation::Default);
+    
+  // give some indication that we had to skip this event
+  // (ProcStatus returns zero status for us in cases where we don't
+  // explicitly add a status code)
+  procStat->addAlgorithmStatus(name(), m_subsystem, m_reason, m_status , m_abort );
+  return Warning("Processing aborted", StatusCode::SUCCESS, 0);
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode AddToProcStatus::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiAlgorithm::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Rec/Rec/RecAlgs/src/AddToProcStatus.h
@@ -0,0 +1,36 @@
+// $Id: $
+#ifndef ADDTOPROCSTATUS_H 
+#define ADDTOPROCSTATUS_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+
+/** @class AddToProcStatus AddToProcStatus.h
+ * 
+ *  Add an entry to ProcStatus if some sequeence failed
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2011-06-15
+ */
+class AddToProcStatus : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  AddToProcStatus( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~AddToProcStatus( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  std::string m_subsystem ; ///< subsystem
+  std::string m_reason ; ///< Reason for error
+  int m_status ; ///< return code
+  bool m_abort ; ///< should processing be aborted?
+};
+#endif // ADDTOPROCSTATUS_H
