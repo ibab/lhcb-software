bc1f1382b17f296f755133904d8dd367c578ae61
Anton Poluektov <anton.poluektov@cern.ch>
2011-06-15 15:20:25 +0000

    Updated StrippingB2Kshh.py\
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@124822 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ded87b6b4776d76bcf21e441fe4651c3c6ede883

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2KShh.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2KShh.py
@@ -10,9 +10,8 @@ Exported symbols (use python help!):
 """
 
 __author__ = ['Thomas Latham','David Dossett','Jussara Miranda']
-__date__ = '27/05/2011'
-__version__ = 'Stripping15'
-
+__date__ = '15/06/2011'
+__version__ = 'Stripping15-1'
 __all__ = 'B2KShhConf'
 
 from Gaudi.Configuration import *
@@ -34,16 +33,18 @@ default_config = {'Trk_Chi2'         : 4.0,
 		  'B_Mhigh'          : 280.0,
 		  'BDaug_MedPT_PT'   : 800.0,
 		  'BDaug_MaxPT_IP'   : 0.05,
-		  'hh_DOCA'          : 0.09,
-		  'BDaug_DD_PTsum'   : 4500.0,
+		  'hh_DOCA'          : 0.06,
+		  'BDaug_DD_PTsum'   : 4800.0,
 		  'BDaug_LL_PTsum'   : 4500.0,
 		  'B_VtxChi2'        : 12.0,
 		  'B_Dira'           : 0.9999,
 		  'B_DD_IPwrtPV'     : 0.08,
 		  'B_LL_IPwrtPV'     : 0.06,
+		  'BDaug_LL_IPChi2sum'	: 15.0,
+		  'BDaug_DD_IPChi2sum'	: 15.0,
 		  'B_FDwrtPV'        : 1.0,
-		  'B_DD_FDChi2'      : 15.0,
-		  'B_LL_FDChi2'      : 15.0,
+		  'B_DD_FDChi2'      : 50.0,
+		  'B_LL_FDChi2'      : 50.0,
 		  'GEC_MaxTracks'    : 250,
 		  'Prescale'         : 1.0,
 		  'Postscale'        : 1.0
@@ -92,6 +93,8 @@ class B2KShhConf(LineBuilder) :
                               'B_VtxChi2',
                               'B_Dira',
                               'B_DD_IPwrtPV',
+			      'BDaug_LL_IPChi2sum',
+			      'BDaug_DD_IPChi2sum',
                               'B_LL_IPwrtPV',
                               'B_FDwrtPV',
                               'B_DD_FDChi2',
@@ -185,8 +188,9 @@ class B2KShhConf(LineBuilder) :
 	_daugMaxPtIPCut = "(AVAL_MAX(MIPDV(PRIMARY),PT)>%s)" % config['BDaug_MaxPT_IP']
 	_hhDocaCut      = "(ADOCA(1,2)<%s)"                  % config['hh_DOCA']
 	_daugPtSumCut   = "((APT1+APT2+APT3)>%s*MeV)"        % config['BDaug_DD_PTsum']
+	_daugIPChi2SumCut  = "((ACHILD(MIPCHI2DV(PRIMARY),1)+ACHILD(MIPCHI2DV(PRIMARY),2))>%s)"	% config['BDaug_DD_IPChi2sum']
 
-	_combCuts = _massCutLow+'&'+_massCutHigh+'&'+_daugMedPtCut+'&'+_daugMaxPtIPCut+'&'+_hhDocaCut+'&'+_daugPtSumCut
+	_combCuts = _massCutLow+'&'+_massCutHigh+'&'+_daugMedPtCut+'&'+_daugMaxPtIPCut+'&'+_hhDocaCut+'&'+_daugPtSumCut+'&'+_daugIPChi2SumCut
 
 	_vtxChi2Cut = "(VFASPF(VCHI2)<%s)"             % config['B_VtxChi2']
 	_diraCut    = "(BPVDIRA>%s)"                   % config['B_Dira']
@@ -220,8 +224,9 @@ class B2KShhConf(LineBuilder) :
 	_daugMaxPtIPCut = "(AVAL_MAX(MIPDV(PRIMARY),PT)>%s)" % config['BDaug_MaxPT_IP']
 	_hhDocaCut      = "(ADOCA(1,2)<%s)"                  % config['hh_DOCA']
 	_daugPtSumCut   = "((APT1+APT2+APT3)>%s*MeV)"        % config['BDaug_LL_PTsum']
+	_daugIPChi2SumCut  = "((ACHILD(MIPCHI2DV(PRIMARY),1)+ACHILD(MIPCHI2DV(PRIMARY),2))>%s)"	% config['BDaug_LL_IPChi2sum']
 
-	_combCuts = _massCutLow+'&'+_massCutHigh+'&'+_daugMedPtCut+'&'+_daugMaxPtIPCut+'&'+_hhDocaCut+'&'+_daugPtSumCut
+	_combCuts = _massCutLow+'&'+_massCutHigh+'&'+_daugMedPtCut+'&'+_daugMaxPtIPCut+'&'+_hhDocaCut+'&'+_daugPtSumCut+'&'+_daugIPChi2SumCut
 
 	_vtxChi2Cut = "(VFASPF(VCHI2)<%s)"             % config['B_VtxChi2']
 	_diraCut    = "(BPVDIRA>%s)"                   % config['B_Dira']
@@ -240,4 +245,3 @@ class B2KShhConf(LineBuilder) :
 	_B.MotherCut = _motherCuts
 
         self.selB2KSLLhh = Selection (name, Algorithm = _B, RequiredSelections = [ self.selKS2LL, self.pions, self.kaons ])
-
