7a07f4b07ad63e2415d26d8779257ec411842f82
Markus Frank <frankb@cern.ch>
2011-06-16 02:41:42 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@124873 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 134f396b8a3f2fdbc1938b28a967a5787f57329b

--- Online/Online/OnlineKernel/OnlineKernel/SCR/scrcc.h
+++
@@ -1,3 +0,0 @@
-#include "SCR/scr.h"
-extern "C" SCR::PasteboardContext scrcc_create_pasteboard(const char* device);
-extern "C" SCR::Display* scrcc_create_display(int rows, int cols, int attr, SCR::flag border,const char* title);
--- Online/Online/OnlineKernel/src/RTL/TestScreen.cpp
+++ Online/Online/OnlineKernel/src/RTL/TestScreen.cpp
@@ -3,15 +3,31 @@
 #include "RTL/rtl.h"
 #include "RTL/strdef.h"
 #include "RTL/screen.h"
+#include "SCR/scr.h"
 
 extern "C" int rtl_testscreen(int argc,char **) {
   char caption[255];
+  ::snprintf(caption, sizeof(caption), "This is just a message!");
+#if 0
   initscreen();
   printxy(10, 20,"Welcome to Curses!");
-  snprintf(caption, sizeof(caption), "This is just a message!");
   printxy(10, 22,caption);
   printxy(1,  33,".....");
   refresh();
+#endif
+  using namespace SCR;
+  Pasteboard* pasteboard;
+  Display*    display;
+  int height, width;
+  ::scrc_create_pasteboard (&pasteboard, 0, &height, &width);
+  ::scrc_create_display (&display, height-2, width-2, NORMAL, ON, "SCR test program");
+  ::scrc_paste_display  (display, pasteboard, 2, 2);
+  ::scrc_put_chars(display,caption, NORMAL, 2, 2, 0);
+  ::scrc_end_pasteboard_update(pasteboard);
+  ::scrc_fflush(pasteboard);
+  ::scrc_set_cursor(display, 2, 10);
+  ::scrc_cursor_off(pasteboard);
+
   if ( argc == 0 ) lib_rtl_sleep(1000*20);
   return 0;
 }
