8e1f1030553cd1b79a7c265dd3676f5552325348
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2011-06-23 16:31:07 +0000

    add new settings for summer 2011 running
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@125304 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7f7a39474c8d13cd72a95a8343688ac26191e8a

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Phi_July2011.py
@@ -0,0 +1,57 @@
+#
+# High cuts (for hadronic)
+#
+
+# the list of lines is the same 
+
+__phiLines__ = [ 'Hlt2IncPhi',
+                 'Hlt2IncPhiSidebands' 
+                ]
+#
+# Low cuts (for charming and leptonic)
+#
+
+class Phi_July2011 :
+    """
+    Draft settings for 2011 running    
+
+    (see presentation on Trigger & Physics meeting Jan 17th 2011)
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author V. Gligorov, A. Puig 
+    @date 2011-01-19
+    """
+    
+    __all__ = ( 'ActiveHlt2Lines', 'Thresholds' )
+    
+    def Thresholds(self) :
+        """
+        Returns a dictionary of cuts
+        """
+        
+        from Hlt2Lines.Hlt2InclusivePhiLines import Hlt2InclusivePhiLinesConf
+        
+        return { Hlt2InclusivePhiLinesConf   : {
+             'KaonPT'             : 800      # MeV
+            ,'PhiMassWin'         : 20       # MeV
+            ,'PhiPT'              : 1800     # MeV
+            ,'PhiDOCA'            : 0.2      # mm
+            ,'KaonIPS'            : 6        # dimensionless
+            ,'PhiVCHI2'           : 20       # dimensionless
+            ,'KaonRichPID'        : 0        # dimensionless
+            ,'PhiMassWinSB'       : 30       # MeV            
+            ,'Prescale'           : {'Hlt2IncPhiSidebands'   : 1.0
+                                     ,'Hlt2IncPhi'           : 1.0
+                                     }
+            ,'Postscale'          : {'Hlt2IncPhiSidebands'  : 0.05
+                                     ,'Hlt2IncPhi'          : 1.0
+                                     }
+            } }
+    
+    def ActiveHlt2Lines(self) :
+        """
+        Returns a list of active lines
+        """
+        
+        return __phiLines__
