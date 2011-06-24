2fb6c676c9a2a2248cd6e11e422004582b70573f
Veerle Heijne <veerle.heijne@cern.ch>
2011-06-24 20:33:52 +0000

    Tighten cuts and redefinition PS lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@125397 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ce50e5f8949afcf1ce62d69b4b836fc16ecb6c0a

--- Hlt/Hlt/HltSettings/python/HltSettings/DV_draft2011.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/DV_draft2011.py
@@ -16,12 +16,16 @@ class DV_draft2011 :
 
         lines = [
             # The topological
+            'Hlt2DisplVerticesSingle',
             'Hlt2DisplVerticesDouble', 
-            'Hlt2DisplVerticesLowMassSingle',
             'Hlt2DisplVerticesHighMassSingle',
             'Hlt2DisplVerticesHighFDSingle',
             'Hlt2DisplVerticesSinglePostScaled',
-            'Hlt2DisplVerticesSingleDown'
+            'Hlt2DisplVerticesSingleDown',
+            'Hlt2DisplVerticesDoublePostScaled',
+            'Hlt2DisplVerticesSingleHighMassPostScaled',
+            'Hlt2DisplVerticesSingleHighFDPostScaled',
+            'Hlt2DisplVerticesSingleMVPostScaled'
             ]
 
         return lines
@@ -31,74 +35,96 @@ class DV_draft2011 :
         d = { }
         ##########################################################################
         #
-        #  For the Bandwidth division, variable for each line that might be tunned
-        #       line Hlt2DisplVerticesLowMassSingle:  slot ["MinAngle"]['Hlt2SingleLonglivedLowMass'] can vary in range [ 0. ; 0.035 ] 
-        #       line Hlt2DisplVerticesHighMassSingle: ["RMin"]['Hlt2SingleLonglivedHighMass'] can vary in range [ 0. ; 0.7 ] 
-        #       line Hlt2DisplVerticesHighFDSingle:   ["MinFD"]['Hlt2SingleLonglivedHighFD'] can vary in range [ 15. ; 20. ] 
-        #       line Hlt2DisplVerticesDouble:   ["MinMass"]['Hlt2DoubleLonglived'] can vary in range [ 2000. ; 3000. ]  
-        #       line Hlt2DisplVerticesSinglePostScaled:   ["PostScale"]["Hlt2SinglePSLonglived"] can vary to the requiered value to have 1Hz
-        #
+        # For tuning:
+        # Prescale only the PS lines:
+        # Hlt2SinglePSLonglived
+        # Hlt2DoublePSLonglived
+        # Hlt2SinglePSLonglivedHighMass
+        # Hlt2SinglePSLonglivedHighFD
+        # Hlt2SinglePSLonglivedMV
         ##########################################################################
         
-
         from Hlt2Lines.Hlt2DisplVerticesLines  import Hlt2DisplVerticesLinesConf
         d.update( { Hlt2DisplVerticesLinesConf : {
             "MinNbTracks"  : { 'Hlt2RV2P' : 4
+                               , 'Hlt2SingleLonglived' : 5
                                , 'Hlt2SingleDown' : 4
-                               , 'Hlt2SingleLonglivedHighFD' : 5
-                               , 'Hlt2SingleLonglivedLowMass' : 6
+                               , 'Hlt2SingleLonglivedHighFD' :  5
                                , 'Hlt2SingleLonglivedHighMass' : 6
                                , 'Hlt2DoubleLonglived' : 4
                                , 'Hlt2SinglePSLonglived' : 4
+                               , 'Hlt2DoublePSLonglived' : 4
+                               , 'Hlt2SinglePSLonglivedHighMass' : 4
+                               , 'Hlt2SinglePSLonglivedHighFD' : 5
+                               , 'Hlt2SinglePSLonglivedMV' : 4
                                }
             ,  "RCutMethod"   : "FromBeamSpot"
-            ,  "RMin"         : {   'Hlt2RV2P' : 0.3
-                                  , 'Hlt2SingleDown' : 0.5
-                                  , 'Hlt2SingleLonglivedHighFD' : 0.7
-                                  , 'Hlt2SingleLonglivedLowMass' : 0.3
-                                  , 'Hlt2SingleLonglivedHighMass' : 0.3
-                                  , 'Hlt2DoubleLonglived' : 0.3
-                                  , 'Hlt2SinglePSLonglived' : 0.3
+            ,  "RMin"         : {   'Hlt2RV2P' : 0.4
+                                    , 'Hlt2SingleLonglived' : 2.5
+                                    , 'Hlt2SingleDown' : 2.0
+                                    , 'Hlt2SingleLonglivedHighFD' : 4.0
+                                    , 'Hlt2SingleLonglivedHighMass' : 0.4
+                                    , 'Hlt2DoubleLonglived' : 0.4 
+                                    , 'Hlt2SinglePSLonglived' : 0.4
+                                    , 'Hlt2DoublePSLonglived' : 0.4
+                                    , 'Hlt2SinglePSLonglivedHighMass' : 0.4
+                                    , 'Hlt2SinglePSLonglivedHighFD' : 2.0
+                                    , 'Hlt2SinglePSLonglivedMV' : 0.4  
                                   }
-            ,  "MinMass"      : {   'Hlt2RV2P' : 2000.
-                                  , 'Hlt2SingleDown' : 3000.
-                                  , 'Hlt2SingleLonglivedHighFD' : 4500.
-                                  , 'Hlt2SingleLonglivedLowMass' : 4500.
-                                  , 'Hlt2SingleLonglivedHighMass' : 9000.
-                                  , 'Hlt2DoubleLonglived' : 2000.
-                                  , 'Hlt2SinglePSLonglived' : 2000.
+            ,  "MinMass"      : {   'Hlt2RV2P' : 3000.
+                                    , 'Hlt2SingleLonglived' : 7000.
+                                    , 'Hlt2SingleDown' : 3000
+                                    , 'Hlt2SingleLonglivedHighFD' : 4500.
+                                    , 'Hlt2SingleLonglivedHighMass' : 10000.
+                                    , 'Hlt2DoubleLonglived' : 3000.
+                                    , 'Hlt2SinglePSLonglived' : 3000.
+                                    , 'Hlt2DoublePSLonglived' : 3000.
+                                    , 'Hlt2SinglePSLonglivedHighMass' : 10000.
+                                    , 'Hlt2SinglePSLonglivedHighFD' : 4500.
+                                    , 'Hlt2SinglePSLonglivedMV' :3000.
                                   }
+            ,  "HighestMass"  : {   'Hlt2DoubleLonglived' : 4500.
+                                    ,'Hlt2DoublePSLonglived' : 3000.
+                                    }
             ,  "MinSumpt"     :{ 'Hlt2RV2P' : 3000.
+                                 , 'Hlt2SingleLonglived' : 3000.
                                  , 'Hlt2SingleDown' : 3000.
-                                 , 'Hlt2SingleLonglivedHighFD' : 4500.
-                                 , 'Hlt2SingleLonglivedLowMass' : 4500.
-                                 , 'Hlt2SingleLonglivedHighMass' : 10000.
+                                 , 'Hlt2SingleLonglivedHighFD' : 3000.
+                                 , 'Hlt2SingleLonglivedHighMass' : 3000
                                  , 'Hlt2DoubleLonglived' : 3000.
                                  , 'Hlt2SinglePSLonglived' : 3000.
+                                 , 'Hlt2DoublePSLonglived' : 3000.
+                                 , 'Hlt2SinglePSLonglivedHighMass' : 3000.
+                                 , 'Hlt2SinglePSLonglivedHighFD' : 3000.
+                                 , 'Hlt2SinglePSLonglivedMV' :3000.
                                  } 
             ,  "RemVtxFromDet" : { 'Hlt2RV2P' : 0
+                                 , 'Hlt2SingleLonglived' : 5
                                  , 'Hlt2SingleLonglivedHighFD' : 5
-                                 , 'Hlt2SingleLonglivedLowMass' : 5
                                  , 'Hlt2SingleLonglivedHighMass' : 5
-                                 , 'Hlt2DoubleLonglived' : 0
+                                 , 'Hlt2DoubleLonglived' : 5
+                                 , 'Hlt2SingleDown' : 0
                                  , 'Hlt2SinglePSLonglived' : 0
+                                 , 'Hlt2DoublePSLonglived' : 0
+                                 , 'Hlt2SinglePSLonglivedHighMass' : 5
+                                 , 'Hlt2SinglePSLonglivedHighFD' : 5
+                                 , 'Hlt2SinglePSLonglivedMV' : 5
                                    }
-            ,  "MinAngle" : { 'Hlt2RV2P' : 0.
-                                 , 'Hlt2SingleLonglivedHighFD' : 0.035
-                                 , 'Hlt2SingleLonglivedLowMass' : 0.0
-                                 , 'Hlt2SingleLonglivedHighMass' : 0.
-                                 , 'Hlt2DoubleLonglived' : 0.0
-                                 , 'Hlt2SinglePSLonglived' : 0.0
-                                   }
-            ,  "MinFD" : {  'Hlt2SingleLonglivedHighFD' : 15.
-                                   }
+            ,  "AllOutDet"  : {  'Hlt2DoubleLonglived' : False
+                                 ,'Hlt2DoublePSLonglived' : False
+                                 }
             , "PostScale" : { 'Hlt2RV2P' : 1.
+                              , 'Hlt2SingleLonglived' : 1.
                               , 'Hlt2SingleDown' : 1.
                               , 'Hlt2SingleLonglivedHighFD' : 1.
-                              , 'Hlt2SingleLonglivedLowMass' : 1.
                               , 'Hlt2SingleLonglivedHighMass' : 1.
                               , 'Hlt2DoubleLonglived' : 1.
-                              , 'Hlt2SinglePSLonglived' : 0.0006
+                              , 'Hlt2SinglePSLonglived' : 1.
+                              , 'Hlt2DoublePSLonglived' : 1.
+                              , 'Hlt2SinglePSLonglivedHighMass' : 1.
+                              , 'Hlt2SinglePSLonglivedHighFD' : 1.
+                              , 'Hlt2SinglePSLonglivedMV' : 1.
+
                               }             
             # pre and post scales
             }})
