2fd395199bab88e5bd2fe392ef38981d43a48c1c
Gerhard Raven <gerhard.raven@nikhef.nl>
2011-06-26 20:56:59 +0000

    fix ValidateWithTT configuration
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@125457 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4e71a51d865ff1cc9212e4c35869f024d9e74fa8

--- Hlt/Hlt/HltTracking/python/HltTracking/Hlt1TrackFilterConf.py
+++ Hlt/Hlt/HltTracking/python/HltTracking/Hlt1TrackFilterConf.py
@@ -3,20 +3,20 @@ __all__ = ( 'ValidateWithTT' )
 ## Symbols for streamer users
 # =============================================================================
 import HltLine.HltDecodeRaw
-from Configurables import ToolSvc
-from HltLine.HltLine import Hlt1Tool
 
-def ConfiguredTrackFilter( toolName, **args ):
-    parent = ToolSvc()
+def ConfiguredTrackFilter( toolName, parent = None,  **args ):
+    from Configurables import ToolSvc
+    if not parent : parent = ToolSvc()
+    t,n = toolName.split( '/' )
     import Configurables
-    toolType = getattr( Configurables, toolName.split( '/' )[ 0 ] )
-    return Hlt1Tool( toolType
-                     , toolName
-                     , **args ).createConfigurable( parent )
+    t = getattr( Configurables, t )
+    from HltLine.HltLine import Hlt1Tool
+    return Hlt1Tool( t, n , **args ).createConfigurable( parent )
 
 ## Configure tool, set the match chi2
 validate = "FastTTValidationTool/ValidateWithTT"
-ConfiguredTrackFilter( validate, MaxTTProj = 3 )
+x = ConfiguredTrackFilter( validate, MaxTTProj = 3 )
+print x
 
 ## Streamer symbol
 ValidateWithTT = "ValidateWithTT = execute( decodeTT ) * TC_FILTER_TR( '', '%s' )" \
