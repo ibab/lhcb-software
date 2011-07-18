052e11e8f21f5183de6fe75d8d75b32ecc0d24fc
Beat Jost <beat.jost@cern.ch>
2011-07-18 14:19:25 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@126129 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 3ccf2b444af92a46f05d24d1b57bf7c4ef7c82cc

--- Online/Online/Gaucho/src/components/GenStatSvc.h
+++ Online/Online/Gaucho/src/components/GenStatSvc.h
@@ -35,6 +35,34 @@ public:
     delete m_serv;
   }
 };
+
+template <> COutService<double>::COutService(std::string nam) : COutServiceBase(nam)
+{
+  m_data = -1;
+  m_serv = new DimService(m_nam.c_str(),(float&)m_data);
+}
+
+template <> COutService<long long>::COutService(std::string nam) : COutServiceBase(nam)
+{
+  m_data = -1;
+  m_serv = new DimService(m_nam.c_str(),"I",0,0);
+}
+
+template <> void COutService<double>::Update(double &d)
+{
+  float dat;
+  dat = (float)d;
+  m_serv->updateService(dat);
+}
+
+template <> void COutService<long long>::Update(long long &d)
+{
+  unsigned int dat[2];
+  dat[0] = d&0xffffffff;
+  dat[1] = d>>32;
+  m_serv->updateService(dat,sizeof(dat));
+}
+
 typedef std::map<std::string,COutServiceBase*> OUTS;
 class GenStatSvc : public PubSvc
 {
