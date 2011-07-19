4f3249775693af4a89f9c9e1f65fcef2690fd3f6
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2011-07-19 01:21:49 +0000

    add kshh and pi0hh d decays
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126154 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c9836933c47d257aa885d1e25774eb3da91b16ac

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBeauty2Charm.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBeauty2Charm.py
@@ -12,10 +12,11 @@ moduleName = 'Beauty2Charm'
 
 from Gaudi.Configuration import *
 from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
-from PhysSelPython.Wrappers import Selection
+from PhysSelPython.Wrappers import Selection, DataOnDemand, MergedSelection
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
 from StandardParticles import StdNoPIDsPions, StdNoPIDsKaons, StdNoPIDsProtons
+from StandardParticles import StdLooseResolvedPi0,StdLooseMergedPi0
 #from StandardParticles import StdAllNoPIDsPions, StdAllNoPIDsKaons, \
 #     StdAllNoPIDsProtons
 from Beauty2Charm_DBuilder import *
@@ -30,6 +31,17 @@ config = {
     'PT_MIN'        : '100*MeV',
     'P_MIN'         : '1000*MeV',
     'MIPCHI2DV_MIN' : 4
+    },
+    "KS0" : {
+    'PT_MIN'        : '250*MeV',
+    'MIPCHI2DV_MIN' : 4,
+    'MASS_MIN'      : '467.*MeV',
+    'MASS_MAX'      : '527.*MeV'
+    },
+    "Pi0" : {
+    'PT_MIN'        : '500*MeV',
+    'Pi0CL_1_MIN'   : 0.25,
+    'Pi0CL_2_MIN'   : 0.25
     },
     "D2X" : {              # Cuts made on all D's used in all lines 
     'ASUMPT_MIN'    : '1500*MeV',
@@ -63,7 +75,11 @@ config = {
     "D2HHHTIS"     : 1., 
     "D2HHHWS"      : 0.1,
     "D2HHHH"       : 1,
-    "D2HHHHTIS"    : 1
+    "D2HHHHTIS"    : 1,
+    "D2KSHLLWS"    : 0.1,
+    "D2KSHDDWS"    : 0.1,
+    "D2Pi0HHHMergedWS" : 0.1,
+    "D2Pi0HHHResolvedWS" : 0.1,
     },
     'GECNTrkMax'   : 500
     }
@@ -71,7 +87,7 @@ config = {
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\#
 
 class Beauty2CharmConf(LineBuilder):
-    __configuration_keys__ = ('ALL','D2X','B2X','Dstar','Prescales',
+    __configuration_keys__ = ('ALL','KS0','Pi0','D2X','B2X','Dstar','Prescales',
                               'GECNTrkMax')
  
     def __init__(self, moduleName, config) :
@@ -83,13 +99,20 @@ class Beauty2CharmConf(LineBuilder):
         # pre-filter all inputs (nothing is looser than this)
         pions = filterInputs('Pi',[StdNoPIDsPions],config['ALL'])
         kaons = filterInputs('K',[StdNoPIDsKaons],config['ALL'])
+        ks_dd = filterInputs('KS0_DD',[DataOnDemand(Location = "Phys/StdLooseKsDD/Particles")],config['KS0']) 
+        ks_ll = filterInputs('KS0_LL',[DataOnDemand(Location = "Phys/StdLooseKsLL/Particles")],config['KS0'])
+        pi0_merged   = filterInputs('Pi0_Merged',[StdLooseMergedPi0],config['Pi0'])
+        pi0_resolved = filterInputs('Pi0_Resolved',[StdLooseResolvedPi0],config['Pi0'])
 
         # pre-filter hard inputs (these could have been used in HLT2)
         topoPions = topoInputs('Pi',[pions])
         topoKaons = topoInputs('K',[kaons])
 
         # make D->X, etc. inputs
-        d = DBuilder(pions,config['D2X'])
+        d = DBuilder(   pions, #Regular charged tracks
+                        {"DD":[ks_dd],"LL":[ks_ll]}, #To keep DD and LL Kshorts separate
+                        {"Merged":[pi0_merged],"Resolved":[pi0_resolved]}, #To keep Merged/Resolved Pi0 separate
+                        config['D2X'])
         dst = DstarBuilder(d.hh,pions,config['Dstar'])
 
         # make B->DX
@@ -101,12 +124,13 @@ class Beauty2CharmConf(LineBuilder):
                            Algorithm=FilterDesktop(Code='ALL'),
                            RequiredSelections=[sel])
         filter = "TrSOURCE('Rec/Track/Best') >> TrLONG >> (TrSIZE < %s )" \
-                 % config['GECNTrkMax']        
+                 % config['GECNTrkMax']
+        prescale = 1. if not key in config['Prescales'].keys() else config['Prescales'][key]
         line = StrippingLine(sel.name()+'Line',
-                             prescale=config['Prescales'][key],
+                             prescale=prescale,
                              selection=tmpSel,
                              checkPV=True,FILTER=filter,
-                             HLT="HLT_PASS_RE('Hlt2Topo.*Decision')")
+                             HLT="HLT_PASS_RE('Hlt2(Topo|IncPhi).*Decision')")
         self.registerLine(line)
 
     def _makeLines(self,sels,config):
