45eb0c3784ed2698cda852e0b6e325f30d14c737
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2011-07-20 00:00:07 +0000

    update
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126197 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: de17a6ffae3a95d7612551a2b9c623d2560ebc1b

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/Beauty2Charm_HHBuilder.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/Beauty2Charm_HHBuilder.py
@@ -21,16 +21,22 @@ class HHBuilder(object):
         self.pi0   = pi0
         self.config = config
         self.pipi = self._makePiPi()
+        self.kspi = self._makeKsPi()
+        self.kpi0 = self._makeKPi0()
+        self.pipi0 = self._makePiPi0()
         self.kpi = self._makeKPi()
         self.kk = self._makeKK()
         self.rho0 = self._makeRho0([self.pipi])
+        self.rhoplus = self._makeRhoPlus([self.pipi0])
+        self.kstplus_kspi = self._makeKstarPlus_KSpi([self.kspi])
+        self.kstplus_kpi0 = self._makeKstarPlus_KPi0([self.kpi0])
         self.kstar0 = self._makeKstar0([self.kpi])
         self.phi = self._makePhi([self.kk])
         self.ppi = self._makePPi()
         self.pk = self._makePK()
 
     def _makeX2HH(self,name,decays,amass,config,inputs):
-        ''' Makes all X -> HH selections.'''
+        ''' Makes all X -> HH selections with charged tracks only.'''
         comboCuts = [LoKiCuts(['ASUMPT'],config).code(),amass,hasTopoChild()]
         # require at least one daughter to IP chi2 > D min FD chi2
         #comboCuts.append("AHASCHILD(MIPCHI2DV(PRIMARY) > %s)" \
@@ -38,6 +44,20 @@ class HHBuilder(object):
         comboCuts.append(LoKiCuts(['AMAXDOCA'],config).code())
         comboCuts = LoKiCuts.combine(comboCuts)
         momCuts = LoKiCuts(['VCHI2DOF','BPVVDCHI2','BPVDIRA'],config).code()
+        cp = CombineParticles(CombinationCut=comboCuts,MotherCut=momCuts,
+                              DecayDescriptors=decays)
+        print 'makeX2HH',name,'combo:',comboCuts,'mom:',momCuts
+        return Selection(name+'Beauty2Charm',Algorithm=cp,
+                         RequiredSelections=inputs)
+
+    def _makeXPLUS2HH(self,name,decays,amass,config,inputs):
+        ''' Makes all X+ -> HH selections involving neutrals.'''
+        comboCuts = [LoKiCuts(['ASUMPT'],config).code(),amass,hasTopoChild()]
+        # require at least one daughter to IP chi2 > D min FD chi2
+        #comboCuts.append("AHASCHILD(MIPCHI2DV(PRIMARY) > %s)" \
+        #                 % config['BPVVDCHI2_MIN'])
+        comboCuts = LoKiCuts.combine(comboCuts)
+        momCuts = "ALL" #LoKiCuts(['BPVDIRA'],config).code()
         cp = CombineParticles(CombinationCut=comboCuts,MotherCut=momCuts,
                               DecayDescriptors=decays)
         print 'makeX2HH',name,'combo:',comboCuts,'mom:',momCuts
@@ -63,11 +83,37 @@ class HHBuilder(object):
         return self._makeX2HH('X2KK',['phi(1020) -> K+ K-'],
                               '(AM < 6*GeV)',self.config,[self.kaons])
 
+    def _makeKsPi(self):
+        '''Makes X -> Ks0pi- + c.c.'''
+        return self._makeXPLUS2HH('X2KsPi',['[K*(892)+ -> KS0 pi+]cc'],
+                              '(AM < 6*GeV)',self.config,
+                              self.ks["DD"]+self.ks["LL"]+[self.pions]) 
+
+    def _makeKPi0(self):
+        '''Makes X -> K+pi0 + c.c.'''
+        return self._makeXPLUS2HH('X2KPi0',['[K*(892)+ -> K+ pi0]cc'],
+                              '(AM < 6*GeV)',self.config,
+                              self.pi0["Merged"]+self.pi0["Resolved"]+[self.kaons])
+
+    def _makePiPi0(self):
+        '''Makes X -> pi+pi0'''
+        return self._makeXPLUS2HH('X2PiPi0',['[rho(770)+ -> pi+ pi0]cc'],
+                              '(AM < 6*GeV)',self.config,[self.pions]+self.pi0["Merged"]+self.pi0["Resolved"])
+
+    def _makeRhoPlus(self,pipi0):
+        return filterSelection('RHO+',self._massWindow('RHO','rho(770)+'),pipi0)
+
+    def _makeKstarPlus_KSpi(self,kspi):
+        return filterSelection('KST_KSPI',self._massWindow('KST','K*(892)+'),kspi)
+
+    def _makeKstarPlus_KPi0(self,kpi0):
+        return filterSelection('KST_KPI0',self._massWindow('KST','K*(892)+'),kpi0)
+
     def _makeRho0(self,pipi):
         return filterSelection('RHO0',self._massWindow('RHO','rho(770)0'),pipi)
 
     def _makeKstar0(self,kpi):
-        return filterSelection('K*0',self._massWindow('KST','kstar(892)0'),kpi)
+        return filterSelection('K*0',self._massWindow('KST','K*(892)0'),kpi)
 
     def _makePhi(self,kk):
         return filterSelection('PHI',self._massWindow('PHI','phi(1020)'),kk)
