13de6dbbb0ec42a239a5a4d54d8d1fa96830a18e
nmangiaf <nmangiaf@cern.ch>
2011-07-21 12:06:05 +0000

    StrippingBuToKX3872: filtered vertices, StdAllLoosePions switch, tightened selection.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126352 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8db4268a7b122bff7acd60a91ba8b65466ee6e24

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBuToKX3872.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBuToKX3872.py
@@ -34,12 +34,12 @@ Stripping_BuToKX3872_TestDictonary = {
     'Bu_Comb_MassWindowLoose'    : 500.0,
     'Bu_MassWindow'              : 400.0,
     'Bu_MassWindowLoose'         : 400.0,
-    'Bu_VertexCHI2'              :   5.0,
-    'Bu_VertexCHI2Loose'         :   7.0,
+    'Bu_VertexCHI2'              :   4.0,
+    'Bu_VertexCHI2Loose'         :   5.0,
     'Bu_IPCHI2'                  :  15.0,
     'Bu_IPCHI2Loose'             :  20.0,
-    'Bu_FlightCHI2'              :  30.0,
-    'Bu_FlightCHI2Loose'         :  20.0,
+    'Bu_FlightCHI2'              :  40.0,
+    'Bu_FlightCHI2Loose'         :  30.0,
     'Bu_DIRA'                    :   0.9995,
     'Bu_DIRALoose'               :   0.9995,
     
@@ -55,12 +55,14 @@ Stripping_BuToKX3872_TestDictonary = {
     'Track_CHI2nDOF'       : 4.0,
 
     # Kaon cuts
-    'Kaon_MinIPCHI2'       : 4.5,
-    'Kaon_MinIPCHI2Loose'  : 4.0,
+    'Kaon_MinIPCHI2'       : 6.0,
+    'Kaon_MinIPCHI2Loose'  : 5.0,
+    'Kaon_PT'              : 200.0,
+    'Kaon_PTLoose'         : 150.0,
 
     # Pion cuts
-    'Pion_MinIPCHI2'       : 4.5,
-    'Pion_MinIPCHI2Loose'  : 4.0,
+    'Pion_MinIPCHI2'       : 6.0,
+    'Pion_MinIPCHI2Loose'  : 5.0,
 
     # JPsi cuts
     'JPsi_MassWindow'       :  70.0,
@@ -69,8 +71,8 @@ Stripping_BuToKX3872_TestDictonary = {
     'JPsi_VertexCHI2Loose'  :  10.0,
 
     # Muon cuts
-    'Muon_MinIPCHI2'        :   1.5,
-    'Muon_MinIPCHI2Loose'   :   1.5,
+    'Muon_MinIPCHI2'        :   3.0,
+    'Muon_MinIPCHI2Loose'   :   2.0,
     'Muon_PT'               : 500.0,
     'Muon_IsMuon'           : True
     }
@@ -120,6 +122,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
     # Kaon cuts
     'Kaon_MinIPCHI2',     
     'Kaon_MinIPCHI2Loose',
+    'Kaon_PT',     
+    'Kaon_PTLoose',
 
     # Pion cuts
     'Pion_MinIPCHI2',      
@@ -151,6 +155,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
         self.BuToKPsi2SLineName = self.name + "_BuToKPsi2S"
         self.BuToKX3872LooseLineName = self.name + "_BuToKX3872Loose"
         self.BuToKPsi2SLooseLineName = self.name + "_BuToKPsi2SLoose"
+        self.__PVOutputLocation__ = "Rec/Vertex/"+name+"_ModPrimary"
+        #self.__PVOutputLocation__ = "Rec/Vertex/Primary"
 
         ############################
         ## Define the cut strings ##
@@ -191,8 +197,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         TrackCut = "(TRCHI2DOF < %(Track_CHI2nDOF)s)" %config
 
-        self.KaonCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Kaon_MinIPCHI2)s)" %config
-        self.KaonLooseCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Kaon_MinIPCHI2Loose)s)" %config
+        self.KaonCut = TrackCut + " & (PT > %(Kaon_PT)s * MeV) & (MIPCHI2DV(PRIMARY) > %(Kaon_MinIPCHI2)s)" %config
+        self.KaonLooseCut = TrackCut + "  & (PT > %(Kaon_PTLoose)s * MeV) & (MIPCHI2DV(PRIMARY) > %(Kaon_MinIPCHI2Loose)s)" %config
 
         self.PionCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Pion_MinIPCHI2)s)" %config
         self.PionLooseCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Pion_MinIPCHI2Loose)s)" %config
@@ -201,7 +207,13 @@ class StrippingBu2KX3872Conf(LineBuilder):
         if(config["Muon_IsMuon"]):
             MuonCut += " & (ISMUON)"
             
-        MuonLooseCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Muon_MinIPCHI2Loose)s)" %config
+        MuonLooseCut = TrackCut + " & (MIPCHI2DV(PRIMARY) > %(Muon_MinIPCHI2Loose)s) & (PT > %(Muon_PT)s * MeV)" %config
+        if(config["Muon_IsMuon"]):
+            MuonLooseCut += " & (ISMUON)"
+
+        MuonPVCut = TrackCut + " & (PT > %(Muon_PT)s * MeV)" %config
+        if(config["Muon_IsMuon"]):
+            MuonPVCut += " & (ISMUON)"
 
         ##################
         ##   Rho cuts   ##
@@ -234,7 +246,7 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         Sel_BuX3872KLoose = self.__CreateSelectionBu__(self.BuToKX3872LooseLineName, self.BuCombLooseCut, self.BuLooseCut, [Sel_X3872Loose, Sel_KaonLoose])
         Sel_BuPsi2SKLoose = self.__CreateSelectionBu__(self.BuToKPsi2SLooseLineName, self.BuCombLooseCut, self.BuLooseCut, [Sel_Psi2SLoose, Sel_KaonLoose])
-        
+
         ## tight selections
         Sel_JPsi = self.__FilterSelectionJPsi__(self.name, self.JPsiCuts)
         Sel_Rho = self.__FilterSelectionRho__(self.name, self.RhoCuts, [Sel_RhoLoose])
@@ -253,11 +265,15 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         from StrippingConf.StrippingLine import StrippingLine
 
+        ## Define PV filter
+        MyFilterPVs = {'Code' : "VSOURCE('Rec/Vertex/Primary') >> (NTRACKS > 6) >> RV_SINKTES('"+self.__PVOutputLocation__+"') >> ~VEMPTY", 'Preambulo' : ["from LoKiPhys.decorators import *"]}
+
         ## --- B+ -> X3872 K+ loose line ---
         Line_BuToX3872Loose_Name = self.BuToKX3872LooseLineName + "Line"
         Line_BuToX3872Loose = StrippingLine( Line_BuToX3872Loose_Name,
                                              prescale = config['Prescale_BuToKX3872Loose'],
                                              postscale = config['Postscale_BuToKX3872Loose'],
+                                             FILTER = MyFilterPVs,
                                              selection = Sel_BuX3872KLoose)
         self.registerLine(Line_BuToX3872Loose)
 
@@ -290,7 +306,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
     def printCuts(self):
 
         '''Print the compiled cut values'''
-
+        
+        print " "
         print "-------------------------------------------"
         print "-- B+ -> K X3872 etc stripping line cuts --"
         print "-------------------------------------------"
@@ -353,11 +370,13 @@ class StrippingBu2KX3872Conf(LineBuilder):
         
         from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
         FilterKaon = FilterDesktop()
+        FilterKaon.InputPrimaryVertices = self.__PVOutputLocation__
         FilterKaon.Code = KaonCut
 
         from PhysSelPython.Wrappers import Selection        
-        SelKaon = Selection("SelFilter_" + lName + "_Kaon", Algorithm = FilterKaon, RequiredSelections = [StdLooseKaons])
-        
+        SelKaon = Selection("SelFilter_" + lName + "_Kaon", Algorithm = FilterKaon,
+                            RequiredSelections = [StdLooseKaons])
+
         return SelKaon
 
     #########################
@@ -366,13 +385,14 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
     def __CreateSelectionRho__(self, lName, RhoCombCut, RhoCut, PionCut):
         '''
-        rho(770)0 -> pi+ pi- selection (from StdLoosePions)
+        rho(770)0 -> pi+ pi- selection (from StdAllLoosePions)
         '''
 
-        from StandardParticles import StdLoosePions
+        from StandardParticles import StdAllLoosePions
 
         from GaudiConfUtils.ConfigurableGenerators import CombineParticles
         CombineRho = CombineParticles()
+        CombineRho.InputPrimaryVertices = self.__PVOutputLocation__
         
         CombineRho.DecayDescriptor = "rho(770)0 -> pi+ pi-"
 
@@ -383,7 +403,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
         CombineRho.DaughtersCuts = { "pi+" : PionCut }
 
         from PhysSelPython.Wrappers import Selection
-        SelRho = Selection("SelBuild_" + lName + "_Rho", Algorithm = CombineRho, RequiredSelections = [StdLoosePions])
+        SelRho = Selection("SelBuild_" + lName + "_Rho", Algorithm = CombineRho,
+                           RequiredSelections = [StdAllLoosePions])
 
         return SelRho
 
@@ -394,10 +415,12 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
         FilterRho = FilterDesktop()
+        FilterRho.InputPrimaryVertices = self.__PVOutputLocation__
         FilterRho.Code = RhoCuts
 
         from PhysSelPython.Wrappers import Selection        
-        SelRho = Selection("SelFilter_" + lName + "_Rho", Algorithm = FilterRho, RequiredSelections = InputSelections)
+        SelRho = Selection("SelFilter_" + lName + "_Rho", Algorithm = FilterRho,
+                           RequiredSelections = InputSelections)
         
         return SelRho
 
@@ -415,10 +438,12 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
         FilterJPsi = FilterDesktop()
+        FilterJPsi.InputPrimaryVertices = self.__PVOutputLocation__
         FilterJPsi.Code = JPsiCuts
 
         from PhysSelPython.Wrappers import Selection        
-        SelJPsi = Selection("SelFilter_" + lName + "_JPsi", Algorithm = FilterJPsi, RequiredSelections = [StdJPsi])
+        SelJPsi = Selection("SelFilter_" + lName + "_JPsi", Algorithm = FilterJPsi,
+                            RequiredSelections = [StdJPsi])
         
         return SelJPsi
 
@@ -433,6 +458,7 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         from GaudiConfUtils.ConfigurableGenerators import CombineParticles
         CombineX3872 = CombineParticles()
+        CombineX3872.InputPrimaryVertices = self.__PVOutputLocation__
 
         CombineX3872.DecayDescriptor = "X_1(3872) -> J/psi(1S) rho(770)0"
 
@@ -440,7 +466,8 @@ class StrippingBu2KX3872Conf(LineBuilder):
         CombineX3872.MotherCut = MotherCut
 
         from PhysSelPython.Wrappers import Selection
-        SelX3872 = Selection("Sel_" + lName + "_X3872", Algorithm = CombineX3872, RequiredSelections = InputSelections)
+        SelX3872 = Selection("Sel_" + lName + "_X3872", Algorithm = CombineX3872,
+                             RequiredSelections = InputSelections)
         
         return SelX3872
 
@@ -455,14 +482,16 @@ class StrippingBu2KX3872Conf(LineBuilder):
 
         from GaudiConfUtils.ConfigurableGenerators import CombineParticles
         CombineBu = CombineParticles()
-
+        CombineBu.InputPrimaryVertices = self.__PVOutputLocation__
+        
         CombineBu.DecayDescriptors = ["B+ -> X_1(3872) K+", "B- -> X_1(3872) K-"]
 
         CombineBu.CombinationCut = CombCut
         CombineBu.MotherCut = MotherCut
 
         from PhysSelPython.Wrappers import Selection
-        SelBu = Selection("Sel_" + lName + "_Bu", Algorithm = CombineBu, RequiredSelections = InputSelections)
+        SelBu = Selection("Sel_" + lName + "_Bu", Algorithm = CombineBu,
+                          RequiredSelections = InputSelections)
         
         return SelBu
-    
+
