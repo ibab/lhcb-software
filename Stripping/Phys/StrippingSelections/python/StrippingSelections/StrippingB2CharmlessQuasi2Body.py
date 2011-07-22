319d014faf459beccaa53d99bce24eddb0b98987
Frederic Henry Blanc <fred.blanc@epfl.ch>
2011-07-22 10:17:16 +0000

    update StrippingB2CharmlessQuasi2Body with tighter cuts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126393 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 82b0fad146ddb5dc2b0f54ee4ea72ef934b1728f

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
@@ -3,8 +3,8 @@ B->charmless quasi 2-body selection
 '''
 
 __author__ = ['Fred Blanc']
-__date__ = '15/03/2011'
-__version__ = '$Revision: 2.1 $'
+__date__ = '22/07/2011'
+__version__ = '$Revision: 2.2 $'
 
 __all__ = ( 'B2Quasi2BodyConf',
             'makeDiTrackList',
@@ -14,15 +14,15 @@ __all__ = ( 'B2Quasi2BodyConf',
 config_params = {'Q2BPrescale'     : 1.,
                  'Q2BResMinPT'     : 1000.,
                  'Q2BResMinP'      : 10.,
-                 'Q2BResMaxMass'   : 1000.,
+                 'Q2BResMaxMass'   : 1100.,
                  'Q2BResVtxChiDOF' : 9.,
-                 'Q2BBMinM'        : 2500.,
+                 'Q2BBMinM4pi'     : 2500.,
+                 'Q2BBMinM3pi'     : 4000.,
                  'Q2BBMaxM'        : 5700.,
                  'Q2BBMinCorrM4pi' : 4000.,
                  'Q2BBMinCorrM3pi' : 4600.,
                  'Q2BBMaxCorrM'    : 7000.,
-                 'Q2BBVtxChi2DOF'  : 9.,
-                 'Q2BBMinTrkPT'    : 1000.}
+                 'Q2BBVtxChi2DOF'  : 9.}
 
 
 from Gaudi.Configuration import *
@@ -44,24 +44,24 @@ class B2Quasi2BodyConf(LineBuilder) :
                                'Q2BResMinP',
                                'Q2BResMaxMass',
                                'Q2BResVtxChiDOF',
-                               'Q2BBMinM',
+                               'Q2BBMinM4pi',
+                               'Q2BBMinM3pi',
                                'Q2BBMaxM',
                                'Q2BBMinCorrM4pi',
                                'Q2BBMinCorrM3pi',
                                'Q2BBMaxCorrM',
-                               'Q2BBVtxChi2DOF',
-                               'Q2BBMinTrkPT')
+                               'Q2BBVtxChi2DOF')
 
     def __init__(self, name, config) :
 	self.name = name
         LineBuilder.__init__(self, name, config)
 
-        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 25) & (PT > 250*MeV)")
+        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 25)")
         self.TrackList = Selection( 'TrackList' + self.name,
                                     Algorithm = _trkFilter,
                                     RequiredSelections = [StdNoPIDsPions])
 
-        _trkFilter_HiP = FilterDesktop(Code = "(PT > 2500*MeV) & (P > 10*GeV) & (MIPCHI2DV(PRIMARY) > 36)")
+        _trkFilter_HiP = FilterDesktop(Code = "(PT > 2500*MeV) & (P > 10*GeV)")
         self.TrackList_HiP = Selection( 'TrackList_HiP' + self.name,
                                          Algorithm = _trkFilter_HiP,
                                          RequiredSelections = [self.TrackList])
@@ -76,32 +76,28 @@ class B2Quasi2BodyConf(LineBuilder) :
         B2Q2B4piName = self.name + "4piSelection"
         self.B2CharmlessQ2B4pi = makeB2Q2B4pi( B2Q2B4piName,
                                                diTrkList=self.DiTrackList,
-                                               MinMassCut = config['Q2BBMinM'],
+                                               MinMassCut = config['Q2BBMinM4pi'],
                                                MaxMassCut = config['Q2BBMaxM'],
                                                MinCorrMCut = config['Q2BBMinCorrM4pi'],
                                                MaxCorrMCut = config['Q2BBMaxCorrM'],
-                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'],
-                                               MinTrkPTCut = config['Q2BBMinTrkPT'] )
+                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
 
         self.Q2B4piLine = StrippingLine( B2Q2B4piName+"Line",
                                          prescale = config['Q2BPrescale'],
-#                                         HLT = "(HLT_PASS_RE('Hlt1Track.*Decision'))",
                                          selection = self.B2CharmlessQ2B4pi )
 
         B2Q2B3piName = self.name + "3piSelection"
         self.B2CharmlessQ2B3pi = makeB2Q2B3pi( B2Q2B3piName,
                                                trkList = self.TrackList_HiP,
                                                diTrkList=self.DiTrackList,
-                                               MinMassCut = config['Q2BBMinM'],
+                                               MinMassCut = config['Q2BBMinM3pi'],
                                                MaxMassCut = config['Q2BBMaxM'],
                                                MinCorrMCut = config['Q2BBMinCorrM3pi'],
                                                MaxCorrMCut = config['Q2BBMaxCorrM'],
-                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'],
-                                               MinTrkPTCut = config['Q2BBMinTrkPT'] )
+                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
 
         self.Q2B3piLine = StrippingLine( B2Q2B3piName+"Line",
                                          prescale = config['Q2BPrescale'],
-#                                         HLT = "(HLT_PASS_RE('Hlt1Track.*Decision'))",
                                          selection = self.B2CharmlessQ2B3pi )
 
         self.registerLine(self.Q2B4piLine)
@@ -133,17 +129,16 @@ def makeB2Q2B4pi( name,
                   MaxMassCut,
                   MinCorrMCut,
                   MaxCorrMCut,
-                  VtxChi2DOFCut,
-                  MinTrkPTCut ) :
+                  VtxChi2DOFCut ) :
     """
     Charmless Q2B to 4pi selection with missing mass
     """
 
     _B2Q2B4piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
+    _B2Q2B4piPreVertexCuts += " & ( ACHILD(MM,1) + ACHILD(MM,2) < 1100. + 0.2*(AM-3500.) )" %locals()
 
     _B2Q2B4piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
     _B2Q2B4piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" %locals()
-    _B2Q2B4piPostVertexCuts += " & (MAXTREE('pi+'==ABSID, PT) > %(MinTrkPTCut)s *MeV)" %locals()
 
     _combineB2Q2B4pi = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
                                       MotherCut = _B2Q2B4piPostVertexCuts,
@@ -161,16 +156,15 @@ def makeB2Q2B3pi( name,
                   MaxMassCut,
                   MinCorrMCut,
                   MaxCorrMCut,
-                  VtxChi2DOFCut,
-                  MinTrkPTCut ) :
+                  VtxChi2DOFCut ) :
     """
     Charmless Q2B to 3pi selection with missing mass
     """
 
     _B2Q2B3piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
+
     _B2Q2B3piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
     _B2Q2B3piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s ) " %locals()
-    _B2Q2B3piPostVertexCuts += " & CHILDCUT((MAXTREE('pi+'==ABSID, PT) > %(MinTrkPTCut)s *MeV) , 1) " % locals()
 
     _combineB2Q2B3pi = CombineParticles( DecayDescriptor="B+ -> rho(770)0 pi+",
                                          MotherCut = _B2Q2B3piPostVertexCuts,
