a7be98dc37ba6194c0b75e010f01329d0d8fb99d
Sean Benson <sean.benson@cern.ch>
2011-07-23 12:31:06 +0000

    Removed cuts included in std particles
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126453 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 867ff67d6da8696c545d50c7990665037ba9f540

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2Q2B.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBs2Q2B.py
@@ -2,11 +2,15 @@
 Bs->quasi 2-body selection
 Note: This script borrows heavily from that constructed by
 Fred Blanc in the B2CharmlessQuasi2Body selection
+
+Track PT cuts removed as these are in the standard 
+particles now anyway. Also corrected mass cuts
+removed.
 '''
 
 __author__ = ['Sean Benson']
-__date__ = '20/05/2011'
-__version__ = '1.0'
+__date__ = '22/07/2011'
+__version__ = '1.1'
 
 __all__ = ( 'Bs2Q2Body4piConf',
             'mkDiTrackList',
@@ -19,15 +23,12 @@ config_params = {'Q2BPrescale'     : 1.,
                  'Q2BResVtxChiDOF' : 9.,
                  'Q2BBMinM'        : 4500.,
                  'Q2BBMaxM'        : 5700.,
-                 'Q2BBMinCorrM'    : 3500.,
-                 'Q2BBMaxCorrM'    : 7000.,
                  'Q2BBVtxChi2DOF'  : 9.,
-                 'Q2BBMinTrkPT'    : 250.0,
                  'Q2BIPCHI2' : 25}
 
 
 from Gaudi.Configuration import *
-from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles#, OfflineVertexFitter
 from PhysSelPython.Wrappers import Selection, DataOnDemand
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
@@ -47,17 +48,14 @@ class Bs2Q2Body4piConf(LineBuilder) :
                                'Q2BResVtxChiDOF',
                                'Q2BBMinM',
                                'Q2BBMaxM',
-                               'Q2BBMinCorrM',
-                               'Q2BBMaxCorrM',
                                'Q2BBVtxChi2DOF',
-                               'Q2BBMinTrkPT',
                                'Q2BIPCHI2')
 
     def __init__(self, name, config) :
 	self.name = name
         LineBuilder.__init__(self, name, config)
 
-        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 16) & (PT > 250*MeV)")
+        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 16)")
         self.TrackList = Selection( 'TrackList' + self.name,
                                     Algorithm = _trkFilter,
                                     RequiredSelections = [StdNoPIDsPions])
@@ -74,10 +72,7 @@ class Bs2Q2Body4piConf(LineBuilder) :
                                                diTrkList=self.DiTrackList,
                                                MinMassCut = config['Q2BBMinM'],
                                                MaxMassCut = config['Q2BBMaxM'],
-                                               MinCorrMCut = config['Q2BBMinCorrM'],
-                                               MaxCorrMCut = config['Q2BBMaxCorrM'],
                                                VtxChi2DOFCut = config['Q2BBVtxChi2DOF'],
-                                               MinTrkPTCut = config['Q2BBMinTrkPT'], 
                                               BIPchi2Cut = config['Q2BIPCHI2'])
 
         self.Q2B4piLine = StrippingLine( Bs2Q2BName+"Line",
@@ -97,7 +92,7 @@ def mkDiTrackList( name,
     """
     _diTrackPreVertexCuts = "(APT> %(MinPTCut)s *MeV) & (AP> %(MinPCut)s *GeV) & (AM< %(MaxMassCut)s *MeV)" % locals()
     _diTrackPostVertexCuts = "(VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s)" % locals()
-
+    
     _combineDiTrack = CombineParticles( DecayDescriptor="rho(770)0 -> pi+ pi-",
                                         CombinationCut = _diTrackPreVertexCuts,
                                         MotherCut = _diTrackPostVertexCuts )
@@ -110,20 +105,18 @@ def mkBs2Q2B4pi( name,
                   diTrkList,
                   MinMassCut,
                   MaxMassCut,
-                  MinCorrMCut,
-                  MaxCorrMCut,
                   VtxChi2DOFCut,
-                  MinTrkPTCut,
                  BIPchi2Cut) :
     """
     Charmless Q2B selection
     """
     _B2Q2BPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" % locals()
-    _B2Q2BPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s ) & (MIPCHI2DV(PRIMARY) < %(BIPchi2Cut)s) &  (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s ) & (MAXTREE('pi+'==ABSID, PT) > %(MinTrkPTCut)s *MeV) " % locals()
+    _B2Q2BPostVertexCuts = "(MIPCHI2DV(PRIMARY) < %(BIPchi2Cut)s) &  (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" % locals()
 
     _combineB2Q2B = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
                                       MotherCut = _B2Q2BPostVertexCuts,
                                       CombinationCut = _B2Q2BPreVertexCuts )
+
 
     return Selection(name,
                      Algorithm = _combineB2Q2B,
