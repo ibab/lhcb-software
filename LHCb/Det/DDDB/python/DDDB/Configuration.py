71b1b0c0a966aee4090bf417ff1edfb3408ca24a
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-07-25 12:13:00 +0000

    update database tags for SQLDDDB v6r14
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@126526 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3f5b9902b755e00b8111609605ed1b7898bce97

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -117,8 +117,8 @@ class DDDBConf(ConfigurableUser):
         Default configuration for 2011 data (and MonteCarlo for DDDB)
         """
         # Set the tags
-        self.__set_tag__(["DDDB"], "head-20110302")
-        self.__set_tag__(["LHCBCOND"], "head-20110622")
+        self.__set_tag__(["DDDB"], "head-20110722")
+        self.__set_tag__(["LHCBCOND"], "head-20110722")
         self.__set_tag__(["DQFLAGS"], "tt-20110126")
 
     def __2010_conf__(self):
@@ -126,7 +126,7 @@ class DDDBConf(ConfigurableUser):
         Default configuration for 2010 data (and MonteCarlo for DDDB)
         """
         # Set the tags
-        self.__set_tag__(["DDDB"], "head-20110303")
+        self.__set_tag__(["DDDB"], "head-20110721")
         self.__set_tag__(["LHCBCOND"], "head-20110614")
         self.__set_tag__(["DQFLAGS"], "tt-20110126")
 
