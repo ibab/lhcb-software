d9d28540e1ef9f4c6f8fa5d6599d47e90b66a155
Thomas Blake <thomas.blake@cern.ch>
2011-07-25 10:20:24 +0000

    Updated RD lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@126520 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 65502388567f8495c21e4845fc2477e3654f3a3c

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping16/LineConfigDictionaries_RDWG.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping16/LineConfigDictionaries_RDWG.py
@@ -50,7 +50,8 @@ Bd2KstarMuMu = {
     'Track_CHI2nDOF'      :    5.0,
     'Hadron_MinIPCHI2'    :    9.0, 
     'Muon_MinIPCHI2'      :    9.0,
-    'Muon_IsMuon'         :   False
+    'Muon_IsMuon'         :   False,
+    'MuonNoPIDs_PIDmu'    :    0.0
     },
     'WGs' : [ 'RD' ] ,
     'STREAMS' : [ 'Dimuon' ]
@@ -321,41 +322,42 @@ Bs2PhiMuMu = {
 
 B2XGamma = {
     'BUILDERTYPE' : 'StrippingB2XGammaConf',
-    'CONFIG' : {'TrIPchi2Phi'          : 10.       # Dimensionless
-                ,'TrIPchi2Kst'         : 10.       # Dimensionless
-                ,'PhiMassWin'          : 15.       # MeV
-                ,'KstMassWin'          : 150.      # MeV
-                ,'KstMassWinSB'        : 200.      # MeV
-                ,'BsMassWin'           : 1000.     # MeV
-                ,'B0MassWin'           : 1000.     # MeV
-                ,'BMassWinSB'          : 2000.     # MeV
-                ,'BsDirAngle'          : 0.02      # radians
-                ,'B0DirAngle'          : 0.02      # radians
-                ,'BDirAngleMoni'       : 0.06      # radians
-                ,'BsPVIPchi2'          : 15.       # Dimensionless
-                ,'B0PVIPchi2'          : 15.       # Dimensionless
-                ,'photonPT'            : 2600.     # MeV
-                ,'PhiVCHI2'            : 15.       # dimensionless
-                ,'KstVCHI2'            : 15.       # dimensionless
-                ,'TrChi2'              : 5.        # dimensionless
-                # Pre- and postscales
-                ,'Bs2PhiGammaWideBMassPreScale'      : 0.1
-                ,'Bs2PhiGammaWideBMassPostScale'     : 1.0
-                ,'Bs2PhiGammaLooseDiraPreScale'      : 0.1
-                ,'Bs2PhiGammaLooseDiraPostScale'     : 1.0
-                ,'Bs2PhiGammaPreScale'               : 1.0
-                ,'Bs2PhiGammaPostScale'              : 1.0
-                ,'Bd2KstGammaWideBMassPreScale'      : 0.05
-                ,'Bd2KstGammaWideBMassPostScale'     : 1.0
-                ,'Bd2KstGammaLooseDiraPreScale'      : 0.05
-                ,'Bd2KstGammaLooseDiraPostScale'     : 1.0
-                ,'Bd2KstGammaWideKstMassPreScale'    : 0.05
-                ,'Bd2KstGammaWideKstMassPostScale'   : 1.0
-                ,'Bd2KstGammaWidePreScale'           : 0.05
-                ,'Bd2KstGammaWidePostScale'          : 1.0
-                ,'Bd2KstGammaPreScale'               : 1.0
-                ,'Bd2KstGammaPostScale'              : 1.0
-                },
+    'CONFIG' : {
+    'TrIPchi2Phi'         : 15.       # Dimensionless
+    ,'TrIPchi2Kst'         : 15.       # Dimensionless
+    ,'PhiMassWin'          : 15.       # MeV
+    ,'KstMassWin'          : 100.      # MeV
+    ,'KstMassWinSB'        : 150.      # MeV
+    ,'BsMassWin'           : 1000.     # MeV
+    ,'B0MassWin'           : 1000.     # MeV
+    ,'BMassWinSB'          : 2000.     # MeV
+    ,'BsDirAngle'          : 0.02      # radians
+    ,'B0DirAngle'          : 0.02      # radians
+    ,'BDirAngleMoni'       : 0.06      # radians
+    ,'BsPVIPchi2'          : 15.       # Dimensionless
+    ,'B0PVIPchi2'          : 15.       # Dimensionless
+    ,'photonPT'            : 2600.     # MeV
+    ,'PhiVCHI2'            : 15.       # dimensionless
+    ,'KstVCHI2'            : 15.       # dimensionless
+    ,'TrChi2'              : 5.        # dimensionless
+    # Pre- and postscales
+    ,'Bs2PhiGammaWideBMassPreScale'      : 0.1
+    ,'Bs2PhiGammaWideBMassPostScale'     : 1.0
+    ,'Bs2PhiGammaLooseDiraPreScale'      : 0.1
+    ,'Bs2PhiGammaLooseDiraPostScale'     : 1.0
+    ,'Bs2PhiGammaPreScale'               : 1.0
+    ,'Bs2PhiGammaPostScale'              : 1.0
+    ,'Bd2KstGammaWideBMassPreScale'      : 0.05
+    ,'Bd2KstGammaWideBMassPostScale'     : 1.0
+    ,'Bd2KstGammaLooseDiraPreScale'      : 0.05
+    ,'Bd2KstGammaLooseDiraPostScale'     : 1.0
+    ,'Bd2KstGammaWideKstMassPreScale'    : 0.05
+    ,'Bd2KstGammaWideKstMassPostScale'   : 1.0
+    ,'Bd2KstGammaWidePreScale'           : 0.05
+    ,'Bd2KstGammaWidePostScale'          : 1.0
+    ,'Bd2KstGammaPreScale'               : 1.0
+    ,'Bd2KstGammaPostScale'              : 1.0
+    },
     'WGs'    : ['RD'],
     'STREAMS' : [ 'Radiative' ] 
     }
