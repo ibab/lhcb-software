66229585d69357f5640be35eeba50b471941ee2e
Beat Jost <beat.jost@cern.ch>
2011-07-25 09:48:45 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@126512 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 82268b625c1de3397f3760fe4b3523979039e086

--- Online/Online/Gaucho/src/components/GenStatSvc.h
+++ Online/Online/Gaucho/src/components/GenStatSvc.h
@@ -39,12 +39,14 @@ public:
 template <> COutService<double>::COutService(std::string nam) : COutServiceBase(nam)
 {
   m_data = -1;
+  printf("New Output Service %s of type double\n",nam.c_str());
   m_serv = new DimService(m_nam.c_str(),(float&)m_data);
 }
 
 template <> COutService<long long>::COutService(std::string nam) : COutServiceBase(nam)
 {
   m_data = -1;
+  printf("New Output Service %s of type long long\n", nam.c_str());
   m_serv = new DimService(m_nam.c_str(),"I",0,0);
 }
 
@@ -52,11 +54,13 @@ template <> void COutService<double>::Update(double &d)
 {
   float dat;
   dat = (float)d;
+  printf("Update Output Service %s of type double\n", m_nam.c_str());
   m_serv->updateService(dat);
 }
 
 template <> void COutService<long long>::Update(long long &d)
 {
+  printf("Update Output Service %s of type long long\n", m_nam.c_str());
   unsigned int dat[2];
   dat[0] = d&0xffffffff;
   dat[1] = d>>32;
