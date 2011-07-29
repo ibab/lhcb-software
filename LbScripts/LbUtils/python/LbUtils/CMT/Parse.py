6eba9ed56880f889f2c2175e38475bc3e1d185b2
rlambert <rlambert@cern.ch>
2011-07-29 15:02:44 +0000

    added Thread and Parse modules, see bug #83487
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@127030 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5ddb5d4699af446e7ce3c0616db9f61330913deb

---
+++ LbScripts/LbUtils/python/LbUtils/CMT/Parse.py
@@ -0,0 +1,46 @@
+"""
+Module to parse files used by or created by CMT
+"""
+
+def parseReq(requirements, token='use', default_version="*"):
+    '''Takes a requirements file or project.cmt-like string,
+       and returns a dictionary of {project:version}
+       of the use statements in the file
+
+       options:
+       token: steer which cmt token to look for, usually "use"
+       default_version:  steer what to have as the default version
+       '''
+    requirements=requirements.strip().split('\n')
+    projver={}
+    for req in requirements:
+        aproj=''
+        aver=star
+        req=req.strip()
+        #ignore lines
+        if 'use' not in req:
+            continue
+        #remove comments
+        if '#' in req:
+            if req.find('#')<req.find('use'):
+                continue
+            req=req.split('#')[0]
+        #remove repeated whitespace
+        while '\t' in req or '  ' in req:
+            req=req.replace('\t',' ')
+            req=req.replace('  ',' ')
+        #tokenize
+        req=req.strip()
+        tokens=req.split(' ')
+        if tokens[0]!=token:
+            continue
+        if len(tokens)<2:
+            raise TypeError, ('The requirements file has "use" with no package')
+        aproj=tokens[1]
+        if len(tokens)>2:
+            #if ('*') not in tokens[2]:
+            aver=tokens[2]
+        if len(tokens)>3:
+            aproj=tokens[3]+'/'+aproj
+        projver[aproj]=aver
+    return projver
---
+++ LbScripts/LbUtils/python/LbUtils/Thread.py
@@ -0,0 +1,43 @@
+"""
+Module for handy threading classes
+"""
+import threading, thread, string, sys, Queue
+
+class LockOnPrintThread(threading.Thread):
+    """
+    A threading class which locks before printing a result,
+    method should be replaced with your own method returning a
+    string for printing
+    """
+    def __init__(self, pool, lock):
+        """
+        pool should be a Queue.Queue object
+        lock, a thread.lock object
+        """
+        threading.Thread.__init__(self)
+        self.lock=lock
+        self.pool=pool
+        self.done=False
+
+    def run(self):
+        """
+        The main sequence, method, lock, print, unlock
+        """
+        while not self.done:
+            try:
+                item=self.pool.get_nowait()
+                if item is not None:
+                    result=self.method(self,item)
+                    self.lock.acquire()
+                    print result.strip()
+                    self.lock.release()
+            except Queue.Empty:
+                self.done=True
+    
+    def method(self,item):
+        """
+        A const method on any class variables, must return a string
+        or an exception
+        """
+        raise ImportError, "do not call the baseclass method!"
+
