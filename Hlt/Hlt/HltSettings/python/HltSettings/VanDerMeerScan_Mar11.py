e8a6c3bdcc729f804909e539a969483acfcc6a62
Gerhard Raven <gerhard.raven@nikhef.nl>
2011-08-02 15:04:56 +0000

    update VdM config
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@127122 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e4923ee83d76743df67e21a5b04f1a2da4d8de5f

--- Hlt/Hlt/HltSettings/python/HltSettings/VanDerMeerScan_Mar11.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/VanDerMeerScan_Mar11.py
@@ -60,9 +60,11 @@ class VanDerMeerScan_Mar11 :
         
         thresholds = {
                        Hlt1L0LinesConf  : { 'Prescale'   : { 'Hlt1L0CALO' : 1.
-                                                          , 'Hlt1L0PU'   : 1. }
+                                                           , 'Hlt1L0PU'   : 1. }
                                           , 'Postscale'  : { 'Hlt1L0CALO' : 'RATE(25)' 
-                                                          , 'Hlt1L0PU'   : 'RATE(25)' } }
+                                                           , 'Hlt1L0PU'   : 'RATE(25)' } 
+                                          , 'L0Channels' : [ 'PU', 'CALO' ]
+                                          }
                      , Hlt1LumiLinesConf : { 'MaxRate'    : 40000000 #take all
                                            , 'Prescale'   : { 'Hlt1Lumi' :  1. }
                                            , 'Postscale'  : { 'Hlt1Lumi' :  1. }
