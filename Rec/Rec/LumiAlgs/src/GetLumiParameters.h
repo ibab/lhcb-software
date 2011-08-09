5bd749ba5713fcece2102c9fb48c2a192f074530
Jaap Panman <panmanj@cern.ch>
2011-08-09 11:10:09 +0000

    revert correction
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@127359 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 469b774edfd553ae68502a63d06618f924b03b95

--- Rec/Rec/LumiAlgs/src/GetLumiParameters.h
+++ Rec/Rec/LumiAlgs/src/GetLumiParameters.h
@@ -5,7 +5,7 @@
 // Include files
 // from Gaudi
 #include "GaudiAlg/GaudiTool.h"
-#include "IGetLumiParameters.h"            // Interface
+#include "Kernel/IGetLumiParameters.h"            // Interface
 
 // CondDB
 #include "DetDesc/Condition.h"
