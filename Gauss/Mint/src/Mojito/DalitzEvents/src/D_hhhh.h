10e001d847438dba45bbee8992cd35d4d24c4568
Matt Coombes <matt.coombes@cern.ch>
2011-08-10 08:49:01 +0000

    Updated requirements
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@127393 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c34425d5a496d63104521e0f8350fd1462a1e709

---
+++ Gauss/Mint/src/Mojito/DalitzEvents/src/D_hhhh.h
@@ -0,0 +1,57 @@
+/*
+ * MintGen.h
+ *
+ *  Created on: May 11, 2011
+ *      Author: mcoombes
+ *
+ *      Purpose: Interface between Gauss model and MINT
+ */
+
+#ifndef GENERATOR_H_
+#define GENERATOR_H_
+
+//C++
+#include <string>
+#include <iostream>
+#include <vector>
+
+//MINT
+#include "SignalGenerator.h"
+#include "IDalitzEvent.h"
+
+//ROOT
+#include "TVector3.h"
+
+namespace MINT {
+	class MintGen {
+	public:
+		MintGen();
+		virtual ~MintGen();
+		void SetInputTextFile(std::string inputFile);
+
+		// Decay Event in parent Rest Frame
+		std::vector<TLorentzVector> DecayEventRFVec();
+
+		const TLorentzVector& getDaugtherMom(unsigned int i);
+
+		//PGDID
+		void SetDaughters_pgdID(std::vector<int> pgd_id);
+		std::vector<int> GetDaughters_pgdID();
+		void SetMothers_pgdID(std::vector<int>);
+		int GetMothers_pgdID();
+
+		//DalitzEvent
+
+		void SetDalitzEvent(IDalitzEvent*);
+
+		void Initalize(std::vector<int>);
+
+	
+	private:
+		std::string m_inputFileName;
+		IDalitzEvent* m_dE;
+		SignalGenerator* m_sg;
+	};
+}
+
+#endif /* MintGen_H_ */
