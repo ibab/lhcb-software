c34475f5bf58471550fad0040a50d4a94a381689
Patrick Robbe <robbe@lal.in2p3.fr>
2011-08-10 13:54:19 +0000

    New monitoring algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@127418 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 08b78d20c9a0c43f757ed21f90306167b1e2fcfe

---
+++ Lbcom/L0/L0Calo/src/L0CaloPRSResetMonitor.cpp
@@ -0,0 +1,139 @@
+// $Id: $
+
+// local
+#include "L0CaloPRSResetMonitor.h"
+
+// Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// Calo
+#include "CaloDet/DeCalorimeter.h"
+
+// Event
+#include "Event/L0CaloCandidate.h"
+#include "Event/L0DUBase.h"
+
+// Declare Algorithm
+DECLARE_ALGORITHM_FACTORY( L0CaloPRSResetMonitor );
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : L0CaloPRSResetMonirot
+//
+// 10/08/2011 : Patrick Robbe
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Standard constructor
+//=============================================================================
+L0CaloPRSResetMonitor::L0CaloPRSResetMonitor( const std::string& name, 
+                                              ISvcLocator* pSvcLocator ) : 
+  GaudiHistoAlg ( name , pSvcLocator ),
+  m_nEvents( 0 ) , 
+  m_alarm  ( 0 ) { 
+  declareProperty( "InputData"       , 
+                   m_inputData  = LHCb::L0CaloCandidateLocation::Full ) ;  
+  declareProperty( "UpdateFrequency" , m_updateFrequency = 1000 )  ;
+
+  // define alarm published service
+  declareInfo( "L0CaloPRSReset" , m_alarm ,
+               "Empty board in L0 Electron, PRS Reset needed." ) ;
+}
+
+//=============================================================================
+// Standard destructor
+//=============================================================================
+L0CaloPRSResetMonitor::~L0CaloPRSResetMonitor() { };
+
+//=============================================================================
+// Initialisation. 
+//=============================================================================
+StatusCode L0CaloPRSResetMonitor::initialize() {
+
+  StatusCode sc = GaudiHistoAlg::initialize();
+
+  if ( sc.isFailure() ) return sc;
+
+  debug() << "==> Initialize" << endmsg;
+
+  // Initialize event counters
+  m_nEvents = 0 ;
+
+  // Reset alarm
+  m_alarm = 0 ;
+
+  // Retrieve the ECAL detector element, build cards
+  m_ecal = getDet<DeCalorimeter>( DeCalorimeterLocation::Ecal );
+
+  // Initialize the counter
+  int eCard( 0 );
+  for ( eCard = 0 ; eCard < m_ecal -> nCards() ; ++eCard ) {
+    if ( ! m_ecal -> isPinCard( eCard ) ) 
+      m_electronCounter[ eCard ] = 0 ;
+  }
+ 
+  return StatusCode::SUCCESS; 
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode L0CaloPRSResetMonitor::execute() {
+
+  // Read input data specified by options
+  debug() << "Execute will read " << m_inputData << endmsg ;
+
+  if ( ! exist< LHCb::L0CaloCandidates >( m_inputData ) ) {
+    Warning( "No data at " + m_inputData ).ignore() ;
+    return StatusCode::SUCCESS ;
+  }
+
+  LHCb::L0CaloCandidates * candidates = 
+    get<LHCb::L0CaloCandidates>( m_inputData );
+  LHCb::L0CaloCandidates::iterator cand ;
+
+  if ( 0 == candidates ) return StatusCode::SUCCESS ;
+
+  m_nEvents++ ;
+
+  int card( 0 ) ;
+
+  // Loop over the candidates
+  for ( cand = candidates -> begin() ; candidates -> end() != cand ; 
+        ++cand ) { 
+    LHCb::L0CaloCandidate * theCand = (*cand) ;
+
+    // Use only electron candidates
+    if ( theCand -> type() != L0DUBase::CaloType::Electron ) 
+      continue ;
+    
+    // Find the candidate board number
+    card = m_ecal -> cardNumber( theCand -> id() ) ;
+    if ( 0 != m_electronCounter.count( card ) ) 
+      m_electronCounter[ card ] = m_electronCounter[ card ] + 1 ;
+  }
+
+  // Analyse counters at the requested frequency
+  if ( m_nEvents % m_updateFrequency == 0 ) {
+    bool found = false ;
+    std::map< int , int >::iterator it = m_electronCounter.begin() ;
+    while ( it != m_electronCounter.end() ) {
+      found = ( it -> second == 0 ) ;
+      if ( found ) break ;
+      ++it ;
+    }
+    if ( found ) { 
+      m_alarm = 1 ;
+      Error( "No L0Electron candidates, reset of PRS will be done automatically" , 
+             StatusCode::SUCCESS , 20 ).ignore() ;
+    } else m_alarm = 0 ;
+
+    // Reset the counters
+    int eCard( 0 );
+    for ( eCard = 0 ; eCard < m_ecal -> nCards() ; ++eCard ) {
+      if ( ! m_ecal -> isPinCard( eCard ) ) 
+        m_electronCounter[ eCard ] = 0 ;
+    }
+  }
+
+  return StatusCode::SUCCESS; 
+}
