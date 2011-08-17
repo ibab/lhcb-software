c82deb5bbfe02d1274b785283761a9b8e5030da4
Matt Coombes <matt.coombes@cern.ch>
2011-08-17 12:01:28 +0000

    Added External interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@127647 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 48e5143125ee72ea44a964f3f8207e3711eb0bf6

---
+++ Gauss/Gen/Mint/Mint/IMintGen.h
@@ -0,0 +1,18 @@
+#ifndef IMINTGEN_HH
+#define IMINTGEN_HH
+// author: Matt Coombes
+// status:  17 Aug 2011
+
+namespace MINT{
+class IMintGen{
+ public:
+  virtual void SetInputTextFile(std::string inputFile) = 0;
+  virtual void Initalize(std::vector<int>) = 0;
+
+	// Decay Event in parent Rest Frame
+  virtual std::vector<std::vector<double> > DecayEventRFVec() = 0;
+};
+}// namespace MINT
+
+#endif
+//
