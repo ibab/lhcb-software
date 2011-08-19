332c2174d1285ce283c3243c8e81974deff3292f
Albert Puig Navarro <albert.puig@epfl.ch>
2011-08-19 15:19:34 +0000

    Added small script to combine lambda maps.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@127777 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 50c4c83adde04c4286ef534fe46b94279704155e

---
+++ Analysis/Phys/KaliCalo/python/KaliCalo/CandidateMinimization/CombineLambdaMaps.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# =============================================================================
+# @file CombineLambdaMaps.py
+# @author Albert Puig Navarro (albert.puig@cern.ch)
+# @date 2011-08-19
+# =============================================================================
+"""
+"""
+
+__author__ = "Albert Puig (albert.puig@cern.ch)"
+
+from optparse import OptionParser
+import os
+
+from KaliCalo.Kali.LambdaMap import LambdaMap
+
+def combineLambdaMaps(*lMaps):
+  newLambdaMap = LambdaMap()
+  for lMap in lMaps:
+    for key in lMap:
+      if not newLambdaMap.has_key(key):
+        newLambdaMap._lambdas[key] = []
+      newLambdaMap._lambdas[key].extend(lMap[key])
+  return newLambdaMap
+
+if __name__ == '__main__':
+  parser = OptionParser()
+  parser.add_option("-o", "--outputFile",
+                    dest="outputFile", action="store", type="string", default=None,
+                    help="join specified constants in FILE", metavar="FILE")
+  (options, args) = parser.parse_args()
+  newL = combineLambdaMaps([arg for arg in args if os.path.exists(arg)])
+  if options.outputFile:
+    newL.save(options.outputFile)
+
+# EOF
