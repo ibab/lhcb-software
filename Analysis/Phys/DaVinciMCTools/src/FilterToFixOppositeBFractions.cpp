7b50d0ed4615d049bec045acb3d0b318a6f4b667
Alex Grecu <alex.grecu@cern.ch>
2011-08-24 11:19:33 +0000

    substituted missing FourVector.mag() method with HepMC 2.06 equivalent
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@127935 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f3eb7d13d6254a22f386dfec9f6d18119bc7cb82

--- Analysis/Phys/DaVinciMCTools/src/FilterToFixOppositeBFractions.cpp
+++ Analysis/Phys/DaVinciMCTools/src/FilterToFixOppositeBFractions.cpp
@@ -342,7 +342,7 @@ MCParticle* FilterToFixOppositeBFractions::associatedofHEP(HepMC::GenParticle* h
 
   MCParticles* mcpart = get<MCParticles>(MCParticleLocation::Default);
   int mid = hepmcp->pdg_id();
-  double mothmom = hepmcp->momentum().mag();
+  double mothmom = hepmcp->momentum().rho();
   double moththeta = hepmcp->momentum().theta();
   MCParticles::const_iterator imc;
   for ( imc = mcpart->begin(); imc != mcpart->end(); ++imc ) {
