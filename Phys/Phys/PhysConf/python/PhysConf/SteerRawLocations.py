bb8fa9e046b1fe47b7f2cda1d5cd3e572dcf010e
rlambert <rlambert@cern.ch>
2011-08-25 18:03:56 +0000

    added steering raw event for trigger from task #21716
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@128035 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8d9db530eb2eed31795079a1e16029564288444b

---
+++ Phys/Phys/PhysConf/python/PhysConf/SteerRawLocations.py
@@ -0,0 +1,27 @@
+"""Simple functions to steer raw event locations
+"""
+
+def setTriggerRawEventLocation(location="/Event/Trigger/RawEvent"):
+    """
+    After Brunel v41r0, there is a new smaller raw event with the
+    trigger information, '/Event/Trigger/RawEvent'
+    see task #217176
+    This will be there on SDST and MicroDST
+    """
+    from Configurables import OdinTimeDecoder, ODINDecodeTool, HltDecReportsDecoder, HltSelReportsDecoder, L0CaloCandidatesFromRaw, L0DUFromRawAlg, L0DUFromRawTool, L0MuonCandidatesFromRaw, L0MuonOutputs, DecodePileUpData
+    
+    OdinTimeDecoder().addTool( ODINDecodeTool, 'ODINDecodeTool' )
+    OdinTimeDecoder().ODINDecodeTool.RawEventLocation = location
+    
+    HltDecReportsDecoder().InputRawEventLocation = location
+    HltSelReportsDecoder().InputRawEventLocation = location
+    L0CaloCandidatesFromRaw().InputRawEventLocation = location
+    
+    L0DUFromRawAlg().addTool( L0DUFromRawTool, 'L0DUFromRawTool' )
+    L0DUFromRawAlg().L0DUFromRawTool.RawLocation = location
+    
+    L0MuonCandidatesFromRaw().addTool( L0MuonOutputs, 'L0MuonOutputs' )
+    L0MuonCandidatesFromRaw().L0MuonOutputs.InputRawEventLocation = location
+    
+    DecodePileUpData().RawEventLocation = location
+
