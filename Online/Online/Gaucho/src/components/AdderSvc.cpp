d850606bb70bb6f4fc2dc9101fa8fba1d640032f
Beat Jost <beat.jost@cern.ch>
2011-08-26 15:42:28 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@128108 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 77e164caecb01c703579e85763fd734dd99f0dd8

--- Online/Online/Gaucho/src/components/AdderSvc.cpp
+++ Online/Online/Gaucho/src/components/AdderSvc.cpp
@@ -94,6 +94,7 @@ StatusCode AdderSvc::initialize()
   StatusCode sc = Service::initialize();
   MsgStream msg( msgSvc(), name() );
   dis_set_debug_on();
+  dic_set_debug_on();
   if( !sc.isSuccess() )
   {
     return sc;
