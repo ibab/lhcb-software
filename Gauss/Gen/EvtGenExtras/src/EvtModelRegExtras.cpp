da1d8a8f83c65e9093aa9d189b37d8243a168d7b
Matt Coombes <matt.coombes@cern.ch>
2011-08-28 20:03:51 +0000

    Removed MINT depent component until MINT is included in the nightly builds
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@128154 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8f1d14c2ff3338fae3db665b3e11ba5eff0f2611

--- Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
+++ Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
@@ -3,16 +3,16 @@
 #include "EvtGenModels/EvtBToKStarllDurham07.hh"
 #include "EvtGenModels/EvtbTosllMS.hh"
 #include "EvtGenModels/Evtbs2llGammaMNT.hh"
-#include "EvtGenModels/EvtDTohhhh.hh"
+//#include "EvtGenModels/EvtDTohhhh.hh"
 
 //we use auto_ptr here to show explicit transfer of ownership
 std::auto_ptr<const EvtModelList> EvtModelRegExtras::getModels(){
-	
+
 	EvtModelList* models = new EvtModelList();
 	models->push_back(new EvtBToKStarllDurham07);
         models -> push_back( new EvtbTosllMS() );
         models -> push_back( new Evtbs2llGammaMNT() );
-        models -> push_back( new EvtDTohhhh() );
+//        models -> push_back( new EvtDTohhhh() );
 	return std::auto_ptr<const EvtModelList>(models);
 	
 }
