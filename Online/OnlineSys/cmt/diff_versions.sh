0627a1ca06a59ff7ed0a8ba6e9a438ecfbaa5415
Markus Frank <frankb@cern.ch>
2011-09-01 15:35:04 +0000

    v4r57
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@128467 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 073404b740316c7d678b06fb24a663d99d15306c

--- Online/OnlineSys/cmt/diff_versions.sh
+++ Online/OnlineSys/cmt/diff_versions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-diff -r --brief ONLINE_HEAD ONLINE_v4r56 | grep -v ".svn" | grep -v "setup." | grep -v "cleanup." | grep -v ".history" | grep -v "x86_64-slc5-gcc43"
+diff -r --brief ONLINE_${1} ONLINE_${2} | grep -v ".svn" | grep -v "setup." | grep -v "cleanup." | grep -v ".history" | grep -v "x86_64-slc5-gcc43"
