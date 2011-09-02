90fdee26833cb75ac670441b0354ac8dff2f84a6
Beat Jost <beat.jost@cern.ch>
2011-09-02 13:15:53 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@128519 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: f541c27850c48f26790e17fab07b71609d7a26ef

--- Online/Online/Gaucho/src/COutService.cpp
+++ Online/Online/Gaucho/src/COutService.cpp
@@ -8,26 +8,26 @@
 #include "Gaucho/COutService.h"
 template <typename T> COutService<T>::COutService(std::string nam):COutServiceBase(nam)
 {
-  printf("New Standard Output Service %s of type %s\n",nam.c_str(),typeid(T).name());
+//  printf("New Standard Output Service %s of type %s\n",nam.c_str(),typeid(T).name());
   m_data = -1;
   m_serv = new DimService(m_nam.c_str(),m_data);
 }
 template <typename T> void COutService<T>::Update(T &dat)
 {
-  printf("Update Service %s of type %s\n",m_nam.c_str(),typeid(T).name());
+//  printf("Update Service %s of type %s\n",m_nam.c_str(),typeid(T).name());
   m_serv->updateService(dat);
 }
 template <> COutService<double>::COutService(std::string nam) : COutServiceBase(nam)
 {
   m_data = -1;
-  printf("New SPECIAL Output Service %s of type double\n",nam.c_str());
+//  printf("New SPECIAL Output Service %s of type double\n",nam.c_str());
   m_serv = new DimService(m_nam.c_str(),"F",0,0);//(float&)m_data);
 }
 
 template <> COutService<long long>::COutService(std::string nam) : COutServiceBase(nam)
 {
   m_data = -1;
-  printf("New SPECIAL Output Service %s of type long long\n", nam.c_str());
+//  printf("New SPECIAL Output Service %s of type long long\n", nam.c_str());
   m_serv = new DimService(m_nam.c_str(),"I",0,0);
 }
 
@@ -35,13 +35,13 @@ template <> void COutService<double>::Update(double &d)
 {
   float dat;
   dat = (float)d;
-  printf("SPECIAL Update Output Service %s of type double\n", m_nam.c_str());
+//  printf("SPECIAL Update Output Service %s of type double\n", m_nam.c_str());
   m_serv->updateService(&dat,sizeof(float));
 }
 
 template <> void COutService<long long>::Update(long long &d)
 {
-  printf("SPECIAL Update Output Service %s of type long long\n", m_nam.c_str());
+//  printf("SPECIAL Update Output Service %s of type long long\n", m_nam.c_str());
   unsigned int dat[2];
   dat[0] = d&0xffffffff;
   dat[1] = d>>32;
