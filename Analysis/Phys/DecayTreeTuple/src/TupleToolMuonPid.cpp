06c073cdbcdd4f2096b24e84e09ff2699ab6807f
Kazu Carvalho Akiba <kazuyoshi.akiba@cern.ch>
2011-09-02 03:41:16 +0000

    adding tuple tool Muon Pid
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@128486 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4124554f3fdc7cb7288acae10cff1bfc4d3a8156

--- Analysis/Phys/DecayTreeTuple/src/TupleToolMuonPid.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolMuonPid.cpp
@@ -1,11 +1,11 @@
-// $Id: TupleToolPid.cpp,v 1.6 2010-03-04 14:02:03 xieyu Exp $
+// $Id: TupleToolMuonPid.cpp,v 1.6 2011-09-01 14:02:03 kakiba Exp $
 // Include files
 
 // from Gaudi
 #include "GaudiKernel/ToolFactory.h"
 
 // local
-#include "TupleToolPid.h"
+#include "TupleToolMuonPid.h"
 
 #include "GaudiAlg/Tuple.h"
 #include "GaudiAlg/TupleObj.h"
@@ -19,13 +19,13 @@
 
 // Declaration of the Tool Factory
 // actually acts as a using namespace TupleTool
-DECLARE_TOOL_FACTORY( TupleToolPid );
+DECLARE_TOOL_FACTORY( TupleToolMuonPid );
 
 using namespace LHCb;
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-TupleToolPid::TupleToolPid( const std::string& type,
+TupleToolMuonPid::TupleToolMuonPid( const std::string& type,
     const std::string& name,
     const IInterface* parent )
 : TupleToolBase ( type, name , parent )
@@ -35,75 +35,48 @@ TupleToolPid::TupleToolPid( const std::string& type,
 
 //=============================================================================
 
-StatusCode TupleToolPid::fill( const Particle*
+StatusCode TupleToolMuonPid::fill( const Particle*
     , const Particle* P
     , const std::string& head
     , Tuples::Tuple& tuple )
 {
   const std::string prefix=fullName(head);
 
-
   if( P ){
     bool test = true;
     test &= tuple->column( prefix+"_ID", P->particleID().pid() );
-
     if( !P->isBasicParticle() ) return StatusCode(test); // no rich info for composite!
     if( isPureNeutralCalo(P)) return StatusCode(test); // no rich information for calo neutrals
-
     const ProtoParticle* proto = P->proto();
     if( proto ){
-
       // Combined DLLs
-      test &= tuple->column( prefix+"_PIDe"
-          ,proto->info(LHCb::ProtoParticle::CombDLLe,-1000));
-
-      test &= tuple->column( prefix+"_PIDmu"
+      test &= tuple->column( prefix+"_CombmuID"
           ,proto->info(LHCb::ProtoParticle::CombDLLmu,-1000));
-
-      test &= tuple->column( prefix+"_PIDK"
-          ,proto->info(LHCb::ProtoParticle::CombDLLk,-1000));
-      test &= tuple->column( prefix+"_PIDp"
-          ,proto->info(LHCb::ProtoParticle::CombDLLp,-1000));
-
-      //The NeuroBays PID probabilities
-      //There is one for each hypothesis
-      test &= tuple->column( prefix+"_ProbNNe"
-          ,proto->info(LHCb::ProtoParticle::ProbNNe,-1000));
-      test &= tuple->column( prefix+"_ProbNNk"
-          ,proto->info(LHCb::ProtoParticle::ProbNNk,-1000));
-      test &= tuple->column( prefix+"_ProbNNp"
-          ,proto->info(LHCb::ProtoParticle::ProbNNp,-1000));
-      test &= tuple->column( prefix+"_ProbNNpi"
-          ,proto->info(LHCb::ProtoParticle::ProbNNpi,-1000));
       test &= tuple->column( prefix+"_ProbNNmu"
           ,proto->info(LHCb::ProtoParticle::ProbNNmu,-1000));
       test &= tuple->column( prefix+"_ProbNNghost"
           ,proto->info(LHCb::ProtoParticle::ProbNNghost,-1000));
-
-      // Calo variables
-      test &= tuple->column( prefix+"_CaloEcalE"
-          ,proto->info(LHCb::ProtoParticle::CaloEcalE,-10000.));
-      test &= tuple->column( prefix+"_CaloHcalE"
-          ,proto->info(LHCb::ProtoParticle::CaloHcalE,-10000.));
-
       bool hasMuon = false;
       bool isMuon = false;
+      bool isMuonLoose = false;
+      bool isMuonTight = false;
+      double muonLLmu;
+      double muonLLbg;
       const MuonPID * muonPID = proto->muonPID();
+      int NShared  = -1;
       if(muonPID) {
         hasMuon = true;
         isMuon = muonPID->IsMuon();
+        isMuonLoose = muonPID->IsMuonLoose();
+        //isMuonTight = muonPID->IsMuonTight();
+        NShared = muonPID->nShared();
+        muonLLmu= muonPID->MuonLLMu();
+        muonLLbg= muonPID->MuonLLBg();
       }
       test &= tuple->column( prefix+"_hasMuon", hasMuon);
       test &= tuple->column( prefix+"_isMuon", isMuon);
-
-      bool hasRich = false;
-      if(proto->richPID()) hasRich = true;
-      test &= tuple->column( prefix+"_hasRich", hasRich);
-
-      bool hasCalo = false;
-      if(proto->calo().size()>0) hasCalo = true;
-      test &= tuple->column( prefix+"_hasCalo", hasCalo);
-
+      test &= tuple->column( prefix+"_isMuonLoose", isMuonLoose);
+      //test &= tuple->column( prefix+"_isMuonTight", isMuonTight);
 
       return StatusCode(test);
     }
