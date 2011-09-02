a2b9767d22789810dd0679db5ac6b4d92e58537a
Albert Bursche <albert.frithjof.bursche@cern.ch>
2011-09-02 14:34:26 +0000

    Added TupleToolJets a trivial tool derrived from TupleToolJetsBase
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@128524 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a831f4d4faa65572cf9f1e63e3656c132f9d2b8a

---
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolJets.cpp
@@ -0,0 +1,47 @@
+#include "TupleToolJets.h"
+#include "GaudiKernel/ToolFactory.h"
+#include <math.h>
+#include <boost/bind.hpp>
+#include "boost/lexical_cast.hpp"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : TupleToolJets
+// Autor: Albert Bursche
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( TupleToolJets )
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+TupleToolJets::TupleToolJets( const std::string& type,
+					      const std::string& name,
+					      const IInterface* parent )
+: TupleToolJetsBase ( type, name , parent )
+   
+{
+  declareInterface<IParticleTupleTool>(this);
+}
+
+//=============================================================================
+StatusCode TupleToolJets::initialize()
+{
+  if( ! TupleToolJetsBase::initialize() )
+    return StatusCode::FAILURE;
+  return StatusCode::SUCCESS ;
+}
+StatusCode TupleToolJets::fill( const LHCb::Particle* /* top */,
+                                  const LHCb::Particle* p,
+                                  const std::string& head ,
+                                  Tuples::Tuple& tuple )
+{
+  m_tuple = &tuple;
+  bool test = true;
+  m_p = p;
+  m_head = m_extraName+head; 
+  test &= WriteJetToTuple(p,m_head);
+  return StatusCode(test);
+}
+
+
