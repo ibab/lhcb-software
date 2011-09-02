9d8ebeba7f88d749bc5f5e9d6fa2881cf42bc7e0
Roel Aaij <roel.aaij@cern.ch>
2011-09-02 06:10:25 +0000

    Allow configuration of Hlt1L0.* lines, but do not create them by default.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@128491 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a6bf775475e624f8aad94174bcdb6384f2918800

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1L0Lines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1L0Lines.py
@@ -22,25 +22,25 @@ class Hlt1L0LinesConf(HltLinesConfigurableUser) :
                , 'Prescale'  : { 'Hlt1L0AnyNoSPD'            : 0.001
                                , 'Hlt1L0.*(?<!RateLimited)$' : 0.000001  
                                }
-               , 'L0Channels' : []  # if empty, use all pre-defined channels
+               , 'L0Channels' : [] # empty by default so lines don't get needlessly created.
                }
 
    def __apply_configuration__(self):
         from HltLine.HltLine import Hlt1Line   as Line
         from HltLine.HltLine     import hlt1Lines
-        #from Hlt1Lines.HltL0Candidates import convertL0Candidates,L0Channels, L0Mask, L0Mask2ODINPredicate
-        #channels = self.getProp('L0Channels')
-        #if not channels : channels = L0Channels()
-        #for channel in channels :
-        #    converter = convertL0Candidates(channel) 
-        #    mask = L0Mask(channel)
-        #    Line ( 'L0' + channel 
-        #         , prescale = self.prescale
-        #         , ODIN  = L0Mask2ODINPredicate(mask) if mask else None 
-        #         , L0DU  = "L0_CHANNEL('%s')" % channel
-        #         , algos = [ converter ] if converter else []
-        #         , postscale = self.postscale
-        #         )
+        from Hlt1Lines.HltL0Candidates import (convertL0Candidates, L0Mask,
+                                               L0Mask2ODINPredicate )
+        channels = self.getProp('L0Channels')
+        for channel in channels :
+           converter = convertL0Candidates(channel) 
+           mask = L0Mask(channel)
+           Line ( 'L0' + channel 
+                , prescale = self.prescale
+                , ODIN  = L0Mask2ODINPredicate(mask) if mask else None 
+                , L0DU  = "L0_CHANNEL('%s')" % channel
+                , algos = [ converter ] if converter else []
+                , postscale = self.postscale
+                )
         ##  How to deal with the MASKing ???
         #  Actually, we don't have to -- ODIN will do this 'upstream' of us ;-)
 
