71f70e31724979600f7e1f32e145db8a5ec259c9
Wouter Hulsbergen <wouterh@nikhef.nl>
2011-09-05 09:15:37 +0000

    added some histograms
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@128555 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df50cfb26b9553deb63a2ddf3f9e2394eb62646f

--- Rec/Tr/TrackMonitors/src/TrackITOverlapMonitor.cpp
+++ Rec/Tr/TrackMonitors/src/TrackITOverlapMonitor.cpp
@@ -16,6 +16,7 @@
 
 // AIDA
 #include "AIDA/IHistogram1D.h"
+#include "AIDA/IHistogram2D.h"
 
 #include "TfKernel/IITHitCreator.h"
 #include "LHCbMath/GeomFun.h"
@@ -33,8 +34,9 @@ namespace Al {
 class IHistogram1D ;
 class IHistogram2D ;
 
-/** @class AlignAlgorithm AlignAlgorithm.h
+/** @class TrackITOverlapMonitor TrackITOverlapMonitor.cpp
 *
+* Algorithm to monitor overlap between IT boxes and between IT and OT
 *
 *  @author Wouter Hulsbergen
 *  @date   2008-09-07
@@ -46,14 +48,24 @@ namespace {
     AIDA::IHistogram1D* resh1 ;
     AIDA::IHistogram1D* hotresh1 ;
     AIDA::IHistogram1D* dyh1 ;
+    AIDA::IHistogram1D* hotdyh1 ;
+    AIDA::IHistogram2D* resVsLayerH1 ;
+    AIDA::IHistogram2D* resVsLadderH1 ;
+    
     DeltaHistogrammer( const std::string& tracktype, 
 		       const std::string& boxtype,
 		       GaudiHistoAlg& parent ) 
     {
       resh1 = parent.book1D(tracktype + "/" + boxtype + "/hitres", boxtype + " dx", -5,5,100) ;
       hotresh1 = parent.book1D(tracktype + "/" + boxtype + "/hotres", boxtype + " dx", -5,5,100) ;
-      dyh1 = parent.book1D(tracktype + "/" + boxtype + "/y", 
+      dyh1 = parent.book1D(tracktype + "/" + boxtype + "/hity", 
 			   boxtype + " y position wrt to sensor edge", -50,250,300) ;
+      hotdyh1 = parent.book1D(tracktype + "/" + boxtype + "/hoty", 
+			      boxtype + " hot y position wrt to sensor edge", -50,250,300) ;
+      resVsLayerH1 = parent.book2D(tracktype + "/" + boxtype + "/hitresVsLayer", boxtype + " dx",
+				   0.5,4.5,4,-5,5,100) ;
+      resVsLadderH1 = parent.book2D(tracktype + "/" + boxtype + "/hitresVsLadder", boxtype + " dx",
+				    0.5,28.5,28,-5,5,100) ;
     }
   } ;
 }
@@ -69,7 +81,7 @@ public:
   virtual StatusCode execute();    ///< Algorithm execution
 
 private:
-  enum TrackType {OTASide=0, OTCSide, ITASide, ITCSide, ITTop, ITBottom} ;
+  enum TrackType {OTASide=0, OTCSide, ITASide, ITCSide, ITTop, ITBottom, OtherTTrack, NoTTrack } ;
   int trackType( const LHCb::Track& track ) const ;
   DeltaHistogrammer* getHistogrammer( size_t itstation,
 				      size_t itbox,
@@ -79,7 +91,7 @@ private:
       if(m_histograms.size()<id+1) m_histograms.resize(id+1) ;
       const char stations[3][256] = { "IT1","IT2","IT3" } ;
       const char boxes[4][256]    = { "CSideBox","ASideBox","BottomBox","TopBox"} ;
-      const char types[6][256]    = { "OTASideTrack", "OTCSideTrack", "ITASideTrack", "ITCSideTrack", "ITTopTrack", "ITBottomTrack" } ;
+      const char types[7][256]    = { "OTASideTrack", "OTCSideTrack", "ITASideTrack", "ITCSideTrack", "ITTopTrack", "ITBottomTrack", "Other" } ;
       char boxname[256] ;
       sprintf(boxname,"%s%s",stations[itstation],boxes[itbox]) ;
       setHistoTopDir("Track/");
@@ -87,7 +99,7 @@ private:
     }
     return m_histograms[id] ;
   }
-  
+
   size_t accumulate( const LHCb::Track& track ) ;
 
   size_t accumulate( const Tf::STHitRange& hits, 
@@ -95,6 +107,8 @@ private:
 		     const LHCb::Track& track) ;
 private:
   std::vector<DeltaHistogrammer*> m_histograms ;
+  AIDA::IHistogram1D* m_tracktypeh1 ;
+  AIDA::IHistogram1D* m_numregionsh1 ;
   ToolHandle<Tf::IITHitCreator> m_ithitcreator ;
   std::string m_trackLocation ;
   double m_maxDistance ;
@@ -108,12 +122,12 @@ DECLARE_ALGORITHM_FACTORY( TrackITOverlapMonitor );
 TrackITOverlapMonitor::TrackITOverlapMonitor( const std::string& name,
 				ISvcLocator* pSvcLocator)
   : GaudiHistoAlg( name , pSvcLocator ),
-    m_ithitcreator("Tf::STHitCreator<Tf::IT>/Tf::ITHitCreator"),
+    m_ithitcreator("Tf::STHitCreator<Tf::IT>/ITHitCreator"),
     m_maxDistance(10.* Gaudi::Units::mm ),
     m_maxTrackError(1.0 * Gaudi::Units::mm )
 {
   declareProperty("TrackLocation", m_trackLocation = LHCb::TrackLocation::Default  ); 
-  declareProperty("MaxTolY", m_maxTolY = 50.0 ) ; 
+  declareProperty("MaxTolY", m_maxTolY = 50.0 ) ;
 }
 
 TrackITOverlapMonitor::~TrackITOverlapMonitor() {}
@@ -123,8 +137,11 @@ StatusCode TrackITOverlapMonitor::initialize() {
   if ( !GaudiHistoAlg::initialize().isSuccess() ) return StatusCode::FAILURE ;
   
   if(!m_ithitcreator.retrieve().isSuccess()) 
-    return Error("==> Failed to retrieve IT hit creator",StatusCode::FAILURE);
-  
+    return Error("==> Failed to retrieve IT hit creator",StatusCode::FAILURE); 
+  setHistoTopDir("Track/");
+  m_tracktypeh1 = book1D("tracktype","tracktype",-0.5,7.5,8) ;
+  m_numregionsh1 = book1D("numregions","number of traversed regions",-0.5,24.5,25) ;
+
   return StatusCode::SUCCESS;
 }
 
@@ -154,13 +171,14 @@ int TrackITOverlapMonitor::trackType( const LHCb::Track& track) const
 	++numOT[1] ;
     }
   }
-  int rc = -1 ;
+  int rc = NoTTrack ;
   if(      numIT[0] >= 8 ) rc = ITCSide ;
   else if( numIT[1] >= 8 ) rc = ITASide ;
   else if( numIT[2] >= 8 ) rc = ITBottom ;
   else if( numIT[3] >= 8 ) rc = ITTop ;
   else if( numOT[0] >= 16 ) rc = OTASide ;
   else if( numOT[1] >= 16 ) rc = OTCSide ;
+  else if( numIT[0] +  numIT[1] + numIT[2] +  numIT[3] + numOT[0] +  numOT[1] >= 8 ) rc = OtherTTrack ;
   return rc ;
 }
 
@@ -173,22 +191,34 @@ StatusCode TrackITOverlapMonitor::execute()
   // For some reason this doesn't work if I don't just first get the
   // hits. This must be a bug in the STHitCreator.
   m_ithitcreator->hits() ;
-
+    
   // loop over all tracks
   BOOST_FOREACH( const LHCb::Track* track, tracks) {
 
+    // get the nodes for later use
+    LHCb::Track::ConstNodeRange nodes = track->nodes() ;
+
+    // make sure that there are sufficient active nodes behind the
+    // magnet. we want at least 8 active nodes.
+    size_t numnodes(0) ;
+    BOOST_FOREACH( const LHCb::Node* node, nodes) 
+      if(node->type() == LHCb::Node::HitOnTrack) {
+	double z = node->z() ;
+	if( 7500 < z && z < 9500 ) ++numnodes ;
+      }
+
     // get the track type.
     int tracktype = trackType(*track) ;
+    m_tracktypeh1->fill( tracktype ) ;
     
     // only process tracks that are classified.
-    if( tracktype>=0 ) {
+    if( tracktype < NoTTrack && numnodes >= 8 ) {
 
-      size_t numcompatible(0) ;
       // create a tracktraj
       LHCb::TrackTraj tracktraj(*track) ;
 
-      // get the nodes for later use
-      LHCb::Track::ConstNodeRange nodes = track->nodes() ;
+      // keep some statistics about how many regions the track crosses
+      size_t numtraversedregions(0), numcompatible(0) ;
 
       // loop over all regions
       for(size_t istation=0; istation<3; ++istation)
@@ -204,15 +234,18 @@ StatusCode TrackITOverlapMonitor::execute()
 	    LHCb::State refstate = tracktraj.state( regionz ) ;
 	    // make sure the track is reasonably well known
 	    if( refstate.covariance()(0,0) < m_maxTrackError * m_maxTrackError ) {
-	      
+
 	      double statex = refstate.x() + (regionz - refstate.z())*refstate.tx() ;
 	      double statey = refstate.y() + (regionz - refstate.z())*refstate.ty() ;
 	      double tolx = 10*m_maxDistance/region->cosT() ;
 	      double toly = m_maxTolY ;
-	      
+
 	      if( region->isXCompatible( statex, tolx ) &&
 		  region->isYCompatible( statey, toly ) ) {
-		
+
+		// update the number of traversed regions
+		++numtraversedregions ;
+
 		// most efficient: use local x-coordinate. hope this works:-)
 		double localx = region->xLocal( statex, statey ) ;
 		Tf::STHitRange thesehits = m_ithitcreator->hitsLocalXRange( istation, ilayer, iregion, 
@@ -253,9 +286,15 @@ StatusCode TrackITOverlapMonitor::execute()
 		      // make sure all contributions get the same
 		      // sign. (some IT layers are rotated)
 		      int sign = hit->yBegin() < hit->yEnd() ? 1 : -1 ;
+		      double dy = ytrk - std::min(hit->yBegin(),hit->yEnd())  ;
 		      histos->resh1->fill( sign * distance ) ;
-		      histos->dyh1->fill( ytrk - std::min(hit->yBegin(),hit->yEnd()) ) ;
-		      if( isOnTrack ) histos->hotresh1->fill( sign * distance ) ;
+		      histos->resVsLayerH1->fill( stid.layer(), sign * distance ) ;
+		      histos->resVsLadderH1->fill( (stid.layer()-1)*7 + stid.sector(), sign * distance ) ;
+		      histos->dyh1->fill( dy ) ;
+		      if( isOnTrack ) {
+			histos->hotresh1->fill( sign * distance ) ;
+			histos->hotdyh1->fill( dy ) ;
+		      }
 		      ++numcompatible ;
 		    }
 		  }
@@ -263,6 +302,7 @@ StatusCode TrackITOverlapMonitor::execute()
 	      }
 	    }
 	  }
+      m_numregionsh1->fill(numtraversedregions) ;
     }
   }
   return StatusCode::SUCCESS ;
