e9676bac89182b684c72f7c57ec88629d752e02c
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-09-15 15:03:35 +0000

    fix reference comparisons also on i686
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@129068 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 945d4adda35eb8385974f575e58d73aa2d943898

--- Rec/Rec/LumiAlgs/python/LumiAlgs/QMTest/LumiAlgsExclusions.py
+++ Rec/Rec/LumiAlgs/python/LumiAlgs/QMTest/LumiAlgsExclusions.py
@@ -3,5 +3,6 @@ from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
   LineSkipper(["FileRecordCnvSvc     INFO File sharing enabled. Do not retire files"]) + \
+  LineSkipper(["DEBUG Releasing service 'UpdateManagerSvc'"]) + \
   LineSkipper(["LHCBCOND_"]) + \
   LineSkipper(["DDDB_"])
