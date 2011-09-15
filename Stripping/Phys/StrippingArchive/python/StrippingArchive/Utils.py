82484910a750a626366d96ec8580297d2514b1e1
rlambert <rlambert@cern.ch>
2011-09-15 12:27:11 +0000

    Add WG and Stream dictionary
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@129043 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1bc8dbfd9e711d130a339aa28a1ae3be9f1285aa

--- Stripping/Phys/StrippingArchive/python/StrippingArchive/Utils.py
+++ Stripping/Phys/StrippingArchive/python/StrippingArchive/Utils.py
@@ -10,6 +10,17 @@ lb=lineBuilder("Stripping13","B0q2DplusMuX")
 This can be used to get the outputLocation of a given line
 
 lb.lines()[0].outputLocation()
+
+But, depending on the exact setup, the stream name will not appear in the outputLocation.
+
+To find the true outputLocation as it was in the Stripping, it's better to use the outputLocations function:
+
+locations=outputLocations('Stripping13','B0q2DplusMuX')
+
+or usually if you need also to instantiate the line itself:
+
+lb=lineBuilder("Stripping13","B0q2DplusMuX")
+locations=outputLocations('Stripping13','B0q2DplusMuX',lines=lb.lines())
 '''
 __author__ = "Rob Lambert rob.lambert@cern.ch"
 __date__ = "30/06/2010"
@@ -18,6 +29,7 @@ __all__ = ('lineBuilder', 'buildStream','streamNames','cloneLinesFromStream','cl
 
 from StrippingSettings.Utils import lineBuilderConfiguration as _lbConf
 from StrippingSettings.Utils import strippingConfiguration as _sConf
+from StrippingUtils.Utils import lineFromName as _lfName
 
 def lineBuilder(stripping, lineBuilderName, archive=None) :
     """
@@ -37,6 +49,37 @@ def lineBuilder(stripping, lineBuilderName, archive=None) :
 
     return archive.lineBuilders()[_config['BUILDERTYPE']](lineBuilderName,
                                                           _config['CONFIG'])
+
+
+def addBuilderToStreamGrouping( streams, config, lb ):
+    """
+    Append lines from a line builder to a dictionary
+    mapping lines to stream names.
+    """
+
+    for stream in config['STREAMS']:
+        if stream in streams:
+            if isinstance(config['STREAMS'],dict):
+                for linename in config['STREAMS'][stream]:
+                    line = _lfName( lb,   linename )
+                    if line: streams[stream] += [ line ]
+            elif isinstance(config['STREAMS'],list):
+                streams[stream] += [ line for line in lb.lines() ]
+            else:
+                raise Exception( 'Unsupported type, expected list' +
+                                 'or dict for line-to-STREAM mapping' )
+        else:
+            if isinstance(config['STREAMS'],dict):
+                for linename in config['STREAMS'][stream]:
+                    line = lineFromName( lb,   linename )
+                    if line: streams[stream] = [ line ]
+            elif isinstance(config['STREAMS'],list):
+                streams[stream] = [ line for line in lb.lines() ]
+            else:
+                raise Exception( 'Unsupported type, expected list' +
+                                 'or dict for line-to-STREAM mapping' )
+    return
+
 
 def lineBuilderAndConf(stripping, lineBuilderName, archive=None):
     """
@@ -59,7 +102,7 @@ def lineBuilderAndConf(stripping, lineBuilderName, archive=None):
     return (archive.lineBuilders()[_config['BUILDERTYPE']], _config['CONFIG'])
 
 
-def buildStream(stripping, streamName = '', archive=None):
+def buildStream(stripping, streamName = '', archive=None, WGs = None ):
     """
     Create a StrippingStream from the lineBuilder database
     Usage:
@@ -67,24 +110,36 @@ def buildStream(stripping, streamName = '', archive=None):
     or:
     >>> conf = strippingConfiguration('Stripping13')
     >>> streamDimuon = strippingStream(conf,'Dimuon')
-    
+
     """
 
     from StrippingConf.StrippingStream import StrippingStream
-    
+
     stream = StrippingStream( streamName )
-    
+
     if isinstance(stripping, basestring) :
         _db = _sConf( stripping )
         from StrippingArchive import strippingArchive
         archive=strippingArchive(stripping)
     else :
         _db = stripping
-    
+
     for key in _db.keys():
         _conf = _db[key]
         if stream.name() in _conf['STREAMS']:
+            if WGs and not any( WG for WG in _conf['WGs'] if WG in WGs): continue
             _lb = archive.lineBuilders()[_conf['BUILDERTYPE']](key,_conf['CONFIG'])
+
+            if isinstance(_conf['STREAMS'],dict):
+                for linename in _conf['STREAMS'][stream.name()]:
+                    line = _lfName( _lb, linename )
+                    if line: stream.appendLines( [ line ] )
+            elif isinstance(_conf['STREAMS'],list):
+                stream.appendLines( _lb.lines() )
+            else:
+                raise Exception( 'Unsupported type, expected list ' +
+                                 'or dict for line-to-STREAM mapping' )
+
             stream.appendLines( _lb.lines() )
     return stream
 
@@ -126,7 +181,7 @@ def cloneStream( stream, newStreamName = 'Full', linePrefix = 'Full', prescale =
 	stream.appendLines( lines )
 	return stream
 
-def buildStreams(stripping,archive=None) :
+def buildStreams(stripping,archive=None, WGs = None) :
     """
     Build and return a set of StrippingStreams for a given stripping
     configuration.
@@ -145,16 +200,23 @@ def buildStreams(stripping,archive=None) :
 
     """
     from StrippingConf.StrippingStream import StrippingStream
-    
+
     streams = {}
+
     if isinstance(stripping, basestring) :
         scdb = _sConf(stripping)
         from StrippingArchive import strippingArchive
         archive=strippingArchive(stripping)
     else :
         scdb = stripping
-    
+
     for k, v in scdb.iteritems() :
+        if 'STREAMS' in v.keys() and not WGs or 'WGs' in v.keys():
+            if not WGs or any( WG for WG in v['WGs'] if WG in WGs ):
+                lb = archive.lineBuilders()[v['BUILDERTYPE']](k,v['CONFIG'])
+                addBuilderToStreamGrouping( streams, v, lb )
+        else:
+            raise Exception('Config',k,'missing either STREAM or WG data data.')
         if 'STREAMS' in v.keys() :
             for stream in v['STREAMS'] :
                 if stream in streams.keys() :
@@ -166,14 +228,15 @@ def buildStreams(stripping,archive=None) :
     builderMap = {}
     for builder in scdb.keys() :
         builderMap[builder] = lineBuilder(scdb, builder, archive)
-    
+
+
     strippingStreams=[]
-    for stream, builderNames in streams.iteritems() :
-        lines=[]
-        for b in builderNames :
-            lines +=  [line for line in builderMap[b].lines()]
-        print 'Creating steam', stream, 'with lines', lines
-        strippingStreams.append(StrippingStream(stream, Lines = lines))
+    for stream in streams:
+        lines = [ line for line in streams[stream] ]
+        print ( 'Creating ' + stream + ' stream with '
+                + str( len(lines) ) + ' lines' )
+        strippingStreams.append( StrippingStream( stream, Lines = lines ) )
+
     return strippingStreams
 
 def lineBuilderClone(stripping, line=None, prefix='Clone', prescale=1.0, archive=None):
@@ -189,35 +252,59 @@ def outputLocations(stripping, lineName, lines=[], archive=None):
     Return the output location(s) [as a list] of a given line in a given stripping
     if lines is an empty list, outputLocations will instantiate the lines itself
     or else it is assumed they are proper line objects with an outputLocation method
-    
+
     e.g.:
-    
+
     line=lineBuilder('Stripping15','B0q2DplusMuX')
     oneline=line.lines()[0]
-    
+
     location=outputLocations('Stripping15', 'B0q2DplusMuX', [oneline])
-    
+
     Essentially it returns /Event/<stream>/Phys/<line>/Particles, avoiding the need for
     the user to construct this manually
     '''
-        
+
     if isinstance(stripping, basestring) :
         _db = _sConf( stripping )
         from StrippingArchive import strippingArchive
         archive=strippingArchive(stripping)
     else :
         _db = stripping
-    
+
     if lineName not in _db:
         raise KeyError, "the line "+lineName+" does not exist in the chosen stripping"
-    
+
     if len(lines)==0:
         _line=lineBuilder(_db, lineName, archive)
         lines=_line.lines()
-    
+
+    _conf=_db[lineName]
+
     retlist=[]
-    for aline in lines:
-        for stream in _db[lineName]['STREAMS']:
-            retlist.append('/Event/'+stream+'/'+aline.outputLocation().replace('/Event','').strip('/'))
-    
+    #add individual lines if the lines are a dictionary
+    if isinstance(_conf['STREAMS'],dict):
+            for stream in _conf['STREAMS']:
+                for linename in _conf['STREAMS'][streams]:
+                    line = lineFromList( _line, lines )
+                    if line: retlist.append('/Event/'+stream+'/'+line.outputLocation().replace('/Event','').strip('/'))
+    #add whole list if the streams are a list
+    elif isinstance(_conf['STREAMS'],list):
+        for aline in lines:
+            for stream in _conf['STREAMS']:
+                retlist.append('/Event/'+stream+'/'+aline.outputLocation().replace('/Event','').strip('/'))
+
+    else:
+        raise Exception( 'Unsupported type, expected list ' +
+                         'or dict for line-to-STREAM mapping' )
     return retlist
+
+
+def lineFromList(lines, lineName) :
+    """
+    Get the line that has a given name. If not available, return None.
+    """
+    rlines = filter(lambda x: x.name()==lineName , lines)
+    if len(rlines)>1 :
+        raise Exception("More than one line with name ", lineName)
+    if len(rlines) == 0 : return None
+    return rlines[0]
