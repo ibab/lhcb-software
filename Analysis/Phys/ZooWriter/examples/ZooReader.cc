f4450397f46456ad470338c4c45e1afbd66625b0
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2011-09-29 23:58:46 +0000

    added/updated documentation, contribute a ZooReader in python
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@129611 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b5221ede8be3a4e489a9071c4291b2e5a4ecb83f

--- Analysis/Phys/ZooWriter/examples/ZooReader.cc
+++ Analysis/Phys/ZooWriter/examples/ZooReader.cc
@@ -1,166 +1,123 @@
-/*
- * Attempt to save Decays into ROOT Tree.
+/** @file ZooReader
  *
- * Use example
+ * read Zoo tuples and make a few plots (very simple example)
  *
+ * see ../options/standalone-simple.py for an example of an option file
+ * to go with this example
  *
- * AZ SHM, PI 2008
+ * @author Manuel Schiller <manuel.schiller@nikhef.nl>
+ * @date 2011-09-30
  */
 
+#include <cmath>
 #include <iostream>
+
 #include <boost/shared_ptr.hpp>
 
-#include <TFile.h>
-#include <TRandom.h>
-#include <TTree.h>
-#include <TClassTable.h>
-#include <TH1F.h>
-#include <TSystem.h>
-#include <TCanvas.h>
 #include <TChain.h>
+#include <TTree.h>
+#include <TH1D.h>
+#include <TCanvas.h>
 #include <TROOT.h>
 #include <TStyle.h>
 
-
-
-#include "./Zoo/Zoo.h"
-
-typedef ROOT::Math::SVector<double, 7> Vec7;
-typedef ROOT::Math::SVector<double, 3> Vec3;
-
-bool selection(const ZooP* p)
-{
-    //Ks and Lambda mostly decay in two particles, so lets filter out anything
-    //else
-    if(p->NCh()!=2) return false;
-    //now get the daughters
-    const ZooP* Ch1 = (*p)[0];
-    const ZooP* Ch2 = (*p)[1];
-    //We want a momentum cut to filter out low momentum stuff
-    if(Ch1->momentum().P() < 2000. || Ch2->momentum().P() < 2000.)
-	return false;
-    //Same for transverse momentum
-    if(Ch1->momentum().Pt() < 50. || Ch2->momentum().Pt() < 50.)
-	return false;
-    //We want the particle to have a good vertex
-    if(p->chi2()/p->nDoF() > 15.) return false;
-    //And a reasonable flight distance
-    if(p->fDistance() < 10.) return false;
-    //We want the particle to have a good vertex
-    if(Ch1->TrInfo()->chi2()/Ch1->TrInfo()->chi2() > 15. ) return false;
-    if(Ch2->TrInfo()->chi2()/Ch2->TrInfo()->chi2() > 15. ) return false;
-    
-    return true;
-}
+#include "Zoo/Zoo.h"
 
 int main(int argc, char** argv)
 {
-
-
-    //Define TChain that will later contain all the tuple files. By adding more
-    //tuple files it is possible to read in more than one file, e.g. from
-    //splitted sub-jobs
+    // Define TChain that will later contain all the tuple files. By adding more
+    // tuple files it is possible to read in more than one file, e.g. from
+    // splitted sub-jobs
     boost::shared_ptr<TChain> chain(new TChain("Forest"));
-    chain->Add("$ZOOROOT/options/Data.root");
+    chain->Add("MC.root");
 
-    //Define Zoo objects that will later contain the event and the particle
-    //branches. 
-    //NB: Pointers have to be created as NULL pointers!
-    ZooEv *pev=0;
-    ZooObjRefArray<ZooP> *myKs=0;
-    ZooObjRefArray<ZooP> *myLambda=0;
+    // Define Zoo objects that will later contain the event and the particle
+    // branches. 
+    // NB: Pointers have to be created as NULL pointers!
+    ZooEv *pev = 0;
+    ZooObjRefArray<ZooP> *myBs = 0;
+    ZooObjRefArray<ZooP> *myDs = 0;
+    ZooObjRefArray<ZooP> *myKaons = 0;
 
-    //Assigning branch names in the tuple to the pointers
-    chain->SetBranchAddress("Event",&pev);
-    chain->SetBranchAddress("MyKaons",&myKs);
-    chain->SetBranchAddress("MyLambdas",&myLambda);
+    // Assigning branch names in the tuple to the pointers
+    chain->SetBranchAddress("Event", &pev);
+    chain->SetBranchAddress("MyKaons", &myKaons);
+    chain->SetBranchAddress("MyBs", &myBs);
+    chain->SetBranchAddress("MyDs", &myDs);
 
     TH1::AddDirectory(kFALSE);
-    //define some aethetic options
 
+    // define some aethetic options
     gROOT->SetStyle("Plain");
     gStyle->SetOptStat(1111111);
     gStyle->SetOptFit(1111);
     gStyle->SetPalette(1);
     gStyle->SetHistLineWidth(2);
 
-    //define the histos to be filled
-    TH1F *preselKs = new TH1F("ks_mass_presel","Ks mass after preselection;candidates;mass [MeV]",100,396.,596.);
-    TH1F *preselL = new TH1F("lambda_mass_presel","Ks mass after preselection;candidates;mass [MeV]",100,1015,1215);
-    TH1F *selKs = new TH1F("ks_mass_sel","Ks mass after preselection;candidates;mass [MeV]",100,396.,596.);
-    TH1F *selL = new TH1F("lambda_mass_sel","Ks mass after preselection;candidates;mass [MeV]",100,1015,1215);
-    TH1F *mcKs = new TH1F("ks_mass_mc","Ks mass after preselection;candidates;mass [MeV]",100,396.,596.);
-    TH1F *mcL = new TH1F("lambda_mass_mc","Ks mass after preselection;candidates;mass [MeV]",100,1015,1215);
+    // define the histos to be filled
+    boost::shared_ptr<TH1D> mDs(new TH1D("mDs", "D_{s} mass;m [MeV];candidates", 300, 1800., 2100.));
+    boost::shared_ptr<TH1D> pDs(new TH1D("pDs", "p of D_{s};p [GeV];candidates", 100, 0., 300.));
+    boost::shared_ptr<TH1D> ptDs(new TH1D("ptDs", "p_{T} of D_{s};p_{T} [GeV];candidates", 100, 0., 50.));
+    boost::shared_ptr<TH1D> mBs(new TH1D("mBs", "B_{s} mass;m [MeV];candidates", 300, 5000., 5600.));
+    boost::shared_ptr<TH1D> pBs(new TH1D("pBs", "p of B_{s};p [GeV];candidates", 100, 0., 300.));
+    boost::shared_ptr<TH1D> ptBs(new TH1D("ptBs", "p_{T} of B_{s};p_{T} [GeV];candidates", 100, 0., 50.));
+    boost::shared_ptr<TH1D> KDLLKPi(new TH1D("KDLLKPi", "Kaon DLL(K-#pi);DLL(K-#pi);entries", 100, -100., 100.));
 
+    // count Bs in MC
+    unsigned long nTrueBs = 0;
 
-    int nMCKs = 0;
-    int nMCLambdas = 0;
-    //do we run over Monte Carlo, i.e., we want to try truth matching
-    bool mc = false;
-
-    //loop over all entries of our chain, i.e. over all events
-    for(int i=0;i<chain->GetEntries();i++){
-	//get event number i
+    // loop over all entries of our chain, i.e. over all events
+    const unsigned long nevents = chain->GetEntries();
+    for(unsigned long i = 0; i < nevents; ++i) {
+	// get event number i
 	chain->GetEntry(i);
+	if (0ul == (i % 1000ul))
+	    std::cout << "Read event " << i << "/" << nevents << std::endl;
 
-	//loop over all MC particles to find out how many there are
-	for (int m=0; m < pev->NMCP(); m++){
-	    const ZooMCP *mcp = pev->MCP(m);
-	    if(mcp->pid()==310) nMCKs++;
-	    if(mcp->pid()==3122) nMCLambdas++;
+	// loop over all MC particles to find out how many there are
+	for (unsigned j = 0; j < pev->NMCP(); ++j) {
+	    const ZooMCP *mcp = pev->MCP(j);
+	    if (531 == std::abs(mcp->pid())) ++nTrueBs;
 	}
 
-
-	//loop over all entries in our myKs branch, i.e. all candidates which
-	//come from the corresponding selection in the current event
-	for(int m=0;m<myKs->size();m++){
-	    //get Ks candidate
-	    const ZooP *candidate=(*myKs)[m];
-
-	    if(mc) {
-		//get the MC associated particle if there is one
-		const ZooMCP* mcks =  candidate->assocMCP();
-		//if the ks is associated, plot the mass in a special histogram
-		if (mcks && mcks->pid() == 310){
-		    mcKs->Fill(candidate->momentum().M());
-		}
-	    }
-
-	    //plot the mass of all candidates
-	    preselKs->Fill(candidate->momentum().M());
-	    //apply a simple selection to reduce background a little
-	    if(selection(candidate)) selKs->Fill(candidate->momentum().M());
+	// loop over Bs to do a mass, p, pt plots
+	for (unsigned j = 0; j < myBs->size(); ++j) {
+	    const ZooP *bs = (*myBs)[j];
+	    mBs->Fill(bs->measuredMass());
+	    ROOT::Math::PxPyPzEVector p = bs->momentum();
+	    pBs->Fill(p.P() / 1e3);
+	    ptBs->Fill(p.Pt() / 1e3);
 	}
 
-	//Do the same for the lambdas
-	for(int m=0;m<myLambda->size();m++){
-	    
-	    const ZooP *candidate=(*myLambda)[m];
-
-	    if(mc) {
-		const ZooMCP* mcl =  candidate->assocMCP();
-		if (mcl && mcl->pid() == 3122){
-		    mcL->Fill(candidate->momentum().M());
-		}
-	    }
-
-	    preselL->Fill(candidate->momentum().M());
-	    if(selection(candidate)) selL->Fill(candidate->momentum().M());
+	// loop over Ds to do a mass, p, pt plots
+	for (unsigned j = 0; j < myDs->size(); ++j) {
+	    const ZooP *ds = (*myDs)[j];
+	    mDs->Fill(ds->measuredMass());
+	    ROOT::Math::PxPyPzEVector p = ds->momentum();
+	    pDs->Fill(p.P() / 1e3);
+	    ptDs->Fill(p.Pt() / 1e3);
 	}
 
-    }
+	// loop over all Kaons to make a DLL(K-pi) plot
+	for (unsigned j = 0; j < myKaons->size(); ++j) {
+	    const ZooP *k = (*myKaons)[j];
+	    const ZooDLL *dll = k->DLL();
+	    if (0 != dll)
+		KDLLKPi->Fill(dll->DLLk());
+	}
+    } // end of loop over events
 
-    TCanvas *c1 = new TCanvas("c1","Sample Plots",200,10,700,900);
-    c1->Divide(2,2);
-    c1->cd(1);
-    preselKs->Draw();
-    c1->cd(2);
-    preselL->Draw();
-    c1->cd(3);
-    selKs->Draw();
-    c1->cd(4); 
-    selL->Draw();
-    c1->Print("plots.eps");
+    std::cout << nTrueBs << " true Bs found in MC truth." << std::endl;
+
+    // plot and save to eps
+    boost::shared_ptr<TCanvas> c(new TCanvas);
+    mBs->Draw("E1"); c->Print("mBs.eps");
+    pBs->Draw("E1"); c->Print("pBs.eps");
+    ptBs->Draw("E1"); c->Print("ptBs.eps");
+    mDs->Draw("E1"); c->Print("mDs.eps");
+    pDs->Draw("E1"); c->Print("pDs.eps");
+    ptDs->Draw("E1"); c->Print("ptDs.eps");
+    KDLLKPi->Draw("E1"); c->Print("KDLLKPi.eps");
 
     return 0;
 }
