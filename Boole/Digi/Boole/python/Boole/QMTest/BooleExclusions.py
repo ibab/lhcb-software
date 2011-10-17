c9e782cb08247a32592448b1a5002f2a3cba8e6b
Marco Cattaneo <marco.cattaneo@cern.ch>
2011-10-17 13:54:07 +0000

    clean up tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@130096 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ca63c5381d6f817e710c552fbdaee5be73fbdf6d

--- Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
+++ Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
@@ -1,10 +1,7 @@
-from GaudiTest import RegexpReplacer, LineSkipper
+from GaudiTest import LineSkipper
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
     LineSkipper(["SIMCOND_"]) + \
-    LineSkipper(["DDDB_"]) + \
-    RegexpReplacer(when = "XmlParserSvc        ERROR DOM",
-        orig = r'entity .*conddb:',
-        repl = r'entity conddb:')
+    LineSkipper(["DDDB_"])
  
