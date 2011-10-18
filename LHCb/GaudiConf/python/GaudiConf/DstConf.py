0b1587b92415c7599fb46a3f5cce157b634401d0
Helder Lopes <lopes@cern.ch>
2011-10-18 12:42:55 +0000

    Set the AncestorFor property of UnpackMuonTracks in DstConf.py to the same path as InputName, to allow the ancestor track association.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@130141 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 89ad404be56251926b647355bff3be0000f20bf5

--- LHCb/GaudiConf/python/GaudiConf/DstConf.py
+++ LHCb/GaudiConf/python/GaudiConf/DstConf.py
@@ -413,7 +413,8 @@ class DstConf(LHCbConfigurableUser):
         if self.getProp( "DstType" ).upper() != "RDST":
             unpackMuons = UnpackTrack( name       = "UnpackMuonTracks",
                                        OutputName = "/Event/Rec/Track/Muon",
-                                       InputName  = "/Event/pRec/Track/Muon" )
+                                       InputName  = "/Event/pRec/Track/Muon",
+                                       AncestorFor= "/Event/pRec/Track/Muon" )
             DataOnDemandSvc().AlgMap[ "/Event/Rec/Track/Muon" ] = unpackMuons
 
     def _unpackMuonPIDs(self):
