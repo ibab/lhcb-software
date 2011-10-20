0ead02af340a4800726a3fb8bc11cfa827c072b4
Patrick Robbe <robbe@lal.in2p3.fr>
2011-10-20 12:55:28 +0000

    Add new options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@130304 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b035cfa98b74107005a1a5d1fc9b6d1497d5541

---
+++ Gauss/Sim/Gauss/tests/options/testGauss-gen-1000evts-defaults.py
@@ -0,0 +1,25 @@
+#
+# Options specific for a given job
+# ie. setting of random number seed and name of output files
+#
+
+from Gauss.Configuration import *
+
+#--Generator phase, set random numbers
+GaussGen = GenInit("GaussGen")
+GaussGen.FirstEventNumber = 1
+GaussGen.RunNumber        = 1082
+
+#--Number of events
+nEvts = 1000
+LHCbApp().EvtMax = nEvts
+
+#--Set name of output files for given job (uncomment the lines)
+#  Note that if you do not set it Gauss will make a name based on event type,
+#  number of events and the date
+#idFile = 'GaussTest'
+#HistogramPersistencySvc().OutputFile = idFile+'-histos.root'
+#
+#OutputStream("GaussTape").Output = "DATAFILE='PFN:%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'"%idFile
+
+
