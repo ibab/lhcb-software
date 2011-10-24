23f83f77b28e033149448a7ef4d1986a5440920b
Nikolai Nikitin <nnikitin@cern.ch>
2011-10-24 16:21:23 +0000

    Added new QED-based model for the B(d,s) -> 4 leptons decays (BQTOLLLL)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@130469 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2f33f025be492baac47caae7a7a89f68b9c1d2e8

--- Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
+++ Gauss/Gen/EvtGenExtras/src/EvtModelRegExtras.cpp
@@ -4,6 +4,7 @@
 #include "EvtGenModels/EvtbTosllMS.hh"
 #include "EvtGenModels/Evtbs2llGammaMNT.hh"
 #include "EvtGenModels/EvtDTohhhh.hh"
+#include "EvtGenModels/EvtbsToLLLL.hh"
 
 //we use auto_ptr here to show explicit transfer of ownership
 std::auto_ptr<const EvtModelList> EvtModelRegExtras::getModels(){
@@ -13,6 +14,7 @@ std::auto_ptr<const EvtModelList> EvtModelRegExtras::getModels(){
         models -> push_back( new EvtbTosllMS() );
         models -> push_back( new Evtbs2llGammaMNT() );
         models -> push_back( new EvtDTohhhh() );
+        models -> push_back( new EvtbsToLLLL() );
 	return std::auto_ptr<const EvtModelList>(models);
 	
 }
