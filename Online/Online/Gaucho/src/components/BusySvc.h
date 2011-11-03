1a525f78522d7f3f411e79d436e53ab34bd090a5
Beat Jost <beat.jost@cern.ch>
2011-11-03 16:08:23 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@130886 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 865573902a10f8bb4c573662e7b5aca5acaaadf0

---
+++ Online/Online/Gaucho/src/components/BusySvc.h
@@ -0,0 +1,40 @@
+#ifndef BUSYSVC_H
+#define BUSYSVC_H
+
+#include "GaudiKernel/Service.h"
+#include "Gaucho/GenTimer.h"
+class IGauchoMonitorSvc;
+class BusySvc;
+class IMonitorSvc;
+class IdleTimer : public GenTimer
+{
+public:
+  BusySvc *m_bsysvc;
+  IdleTimer(BusySvc *master, int period = 5000) : GenTimer(master,period)
+  {
+    m_bsysvc = master;
+  };
+  virtual ~IdleTimer();
+  void timerHandler(void);
+};
+
+class BusySvc : public Service
+{
+public:
+  long m_times_old[7];
+  double m_idlebogus;
+  double m_busybogus;
+  double m_bogus;
+  bool m_first;
+  IMonitorSvc *m_pMonitorSvc;
+  IdleTimer *m_timer;
+  BusySvc(const std::string& name, ISvcLocator* sl);
+  virtual ~BusySvc();
+  //IInterface pure virtual member functions
+//  StatusCode queryInterface(const InterfaceID& riid, void** ppvIF);
+  StatusCode start();
+  StatusCode initialize();
+  void calcIdle();
+  void getBogus(double &bogus);
+};
+#endif // BUSYSVC_H
