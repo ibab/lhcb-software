d3a97e4f66b309253042447b29bef374aed9138b
Markus Frank <frankb@cern.ch>
2011-11-09 15:07:13 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@131218 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: b111bfcd0174ae60b56dafee488e56cd28ab1dc3

--- Online/Online/RootCnv/components/Components.cpp
+++ Online/Online/RootCnv/components/Components.cpp
@@ -1,12 +1,7 @@
-#include "GaudiKernel/AlgFactory.h"
 #include "GaudiKernel/SvcFactory.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
 
 #include "RootCnv/RootCnvSvc.h"
 DECLARE_NAMESPACE_SERVICE_FACTORY(Gaudi,RootCnvSvc)
 
 #include "RootCnv/RootEvtSelector.h"
 DECLARE_NAMESPACE_SERVICE_FACTORY(Gaudi,RootEvtSelector)
-
-#include "RootCnv/RootOutputStream.h"
-DECLARE_NAMESPACE_ALGORITHM_FACTORY(Gaudi,RootOutputStream)
