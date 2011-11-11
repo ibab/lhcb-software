16af9859f9871aa4d7c84565ab48d58fb0f462c6
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-11-11 14:56:37 +0000

    ! 2011-11-11 - Chris Jones  - Update MCMatchObjP2MCRelator to make it more inteligent in where it    looks for the relations tables. Will now try to look in the same place in    the TES location as the input Particle container, which helps when runnin    on uDSTs.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@131388 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d892fd5aab546587683e14174e2cf8a71baf723d

--- Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.cpp
+++ Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.cpp
@@ -1,14 +1,16 @@
 // $Id: MCMatchObjP2MCRelator.cpp,v 1.15 2009-10-27 14:08:49 ibelyaev Exp $
-// Include files 
+// Include files
 
 // from Gaudi
-#include "GaudiKernel/ToolFactory.h" 
+#include "GaudiKernel/ToolFactory.h"
 // LoKi
 #include "LoKi/IReporter.h"
 // P2MCP
 #include "P2MCP/P2MCPFunctors.h"
 // local
 #include "MCMatchObjP2MCRelator.h"
+// boost
+#include "boost/algorithm/string.hpp"
 
 //-----------------------------------------------------------------------------
 // Implementation file for class : MCMatchObjP2MCRelator
@@ -30,12 +32,12 @@ MCMatchObjP2MCRelator::MCMatchObjP2MCRelator( const std::string& type,
   , m_reporter (0)
   , m_matcher  (0)
   , m_tables   ( )
-  , m_loaded   ( false ) 
+  , m_loaded   ( false )
 {
   m_tables.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Charged  ) ;
   m_tables.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Upstream ) ;
   m_tables.push_back ( "Relations/" + LHCb::ProtoParticleLocation::Neutrals ) ;
-  
+
   declareProperty    ( "RelTableLocations",  m_tables ) ;
 }
 //=============================================================================
@@ -55,79 +57,140 @@ StatusCode MCMatchObjP2MCRelator::initialize()
   m_incSvc = svc<IIncidentSvc>("IncidentSvc", true);
   if (0!=m_incSvc) m_incSvc->addListener( this, IncidentType::EndEvent);
 
-  // create the new matcher 
+  // create the new matcher
   m_matcher = new LoKi::MCMatchObj( "P2MCRelator" , m_reporter ) ;
 
   if (0==m_matcher) return StatusCode::SUCCESS;
 
-  // increment the reference counter 
+  // increment the reference counter
   m_matcher->addRef() ;
 
   m_loaded = false ;
-  
+  m_pLocDoneMap.clear();
+
   return (0!=m_reporter && 0!=m_matcher && 0!= m_incSvc) ? StatusCode::SUCCESS : StatusCode::FAILURE;
 }
 //=============================================================================
 StatusCode MCMatchObjP2MCRelator::finalize()
 {
   m_incSvc=0;
-  
-  if ( 0 != m_matcher ) 
-  { 
-    m_matcher -> clear () ; 
+
+  if ( 0 != m_matcher )
+  {
+    m_matcher -> clear () ;
     long count = m_matcher->refCount();
     while ( 0 < count ) { count = m_matcher->release() ; }
-    m_matcher = 0 ; 
-  }  
-  
+    m_matcher = 0 ;
+  }
+
   m_loaded = false ;
+  m_pLocDoneMap.clear();
   return base_class::finalize();
 }
 //=============================================================================
-bool MCMatchObjP2MCRelator::isMatched(const LHCb::Particle* particle, 
+bool MCMatchObjP2MCRelator::isMatched(const LHCb::Particle* particle,
                                       const LHCb::MCParticle* mcParticle) const
 {
-  
-  if ( !m_loaded ) { addTables ( m_matcher ) ; m_loaded = true ; }
-  
-  const bool match = matcher()->match(particle, mcParticle);
+  if ( !particle ) { mcParticle = NULL; return false; }
 
-  if (msgLevel(MSG::VERBOSE)) {
-    if (match) {
-      verbose() << "match Particle PID " << particle->particleID().pid() 
-                << " to MCParticle PID " << mcParticle->particleID().pid() 
+  if ( !m_loaded ) { addTables() ; m_loaded = true ; }
+
+  bool match = matcher()->match( particle, mcParticle );
+
+  if ( !match )
+  {
+    // No match found the 'normal' way. Try the Particle location dependent (uDST) way.
+    addTable( particle );
+    match = matcher()->match( particle, mcParticle );
+  }
+
+  if ( msgLevel(MSG::VERBOSE) )
+  {
+    if ( match )
+    {
+      verbose() << "match Particle PID " << particle->particleID().pid()
+                << " to MCParticle PID " << mcParticle->particleID().pid()
                 << endmsg;
-    } else {
-      verbose() << "No match for Particle PID " 
-                << particle->particleID().pid() 
+    }
+    else
+    {
+      verbose() << "No match for Particle PID "
+                << particle->particleID().pid()
                 << endmsg;
     }
   }
-  
+
   return match;
 }
 //=============================================================================
-void MCMatchObjP2MCRelator::addTables(LoKi::MCMatchObj* matcher) const 
+void MCMatchObjP2MCRelator::addTable(const LHCb::Particle* particle) const
 {
-  for (Addresses::const_iterator item = m_tables.begin(); item!=m_tables.end(); ++item) {
-    const std::string& address = *item;
-    if (exist<LoKi::Types::TablePP2MC>(address) ) {
-      LoKi::Types::TablePP2MC* table = get<LoKi::Types::TablePP2MC>(address);
-      matcher->addMatchInfo(table);
-    } else if (exist<LoKi::Types::TableP2MC>(address) ) {
-      LoKi::Types::TableP2MC* table = get<LoKi::Types::TableP2MC>(address);
-      matcher->addMatchInfo(table);
-    } else {
-      Error ( " There is no valid data at '" + address + "'" ).ignore() ; 
-    }
+  // Try Particle specific locations for relations tables
+  std::string pLoc = objectLocation( particle->parent() );
+  if ( !m_pLocDoneMap[pLoc] )
+  {
+    m_pLocDoneMap[pLoc] = true;
+
+    // Form the relations TES location for this Particles location
+    boost::replace_all( pLoc, "/Particles", "/P2MCPRelations" );
+
+    // load the table
+    loadTable(pLoc);
+  }
+}
+//=============================================================================
+void MCMatchObjP2MCRelator::addTables() const
+{
+  for ( Addresses::const_iterator item = m_tables.begin(); 
+        item != m_tables.end(); ++item )
+  {
+    loadTable( *item );
   }
   m_loaded = true ;
 }
 //=============================================================================
-void MCMatchObjP2MCRelator::handle(const Incident&) 
+void MCMatchObjP2MCRelator::loadTable(const std::string & address) const
+{
+
+  // First try respecting the RootInTES settings.
+  if      (exist<LoKi::Types::TablePP2MC>(address) )
+  {
+    LoKi::Types::TablePP2MC* table = get<LoKi::Types::TablePP2MC>(address);
+    m_matcher->addMatchInfo(table);
+  }
+  else if (exist<LoKi::Types::TableP2MC>(address) )
+  {
+    LoKi::Types::TableP2MC* table = get<LoKi::Types::TableP2MC>(address);
+    m_matcher->addMatchInfo(table);
+  }
+
+  // If that fails, try without RootInTES
+  else if (exist<LoKi::Types::TablePP2MC>(address,false) )
+  {
+    LoKi::Types::TablePP2MC* table = get<LoKi::Types::TablePP2MC>(address,false);
+    m_matcher->addMatchInfo(table);
+  }
+  else if (exist<LoKi::Types::TableP2MC>(address,false) )
+  {
+    LoKi::Types::TableP2MC* table = get<LoKi::Types::TableP2MC>(address,false);
+    m_matcher->addMatchInfo(table);
+  }
+
+  // No good ...
+
+  else
+  {
+    Warning ( " There is no valid data at '" + address + "'", 
+              StatusCode::FAILURE, 0 ).ignore() ;
+  }
+
+}
+//=============================================================================
+void MCMatchObjP2MCRelator::handle(const Incident&)
 {
   if ( 0 != m_matcher ) { m_matcher->clear(); }
   m_loaded = false ;
+  m_pLocDoneMap.clear();
 }
 //=============================================================================
 // Destructor
--- Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.h
+++ Analysis/Phys/Particle2MCTruth/src/MCMatchObjP2MCRelator.h
@@ -1,9 +1,12 @@
 // $Id: MCMatchObjP2MCRelator.h,v 1.16 2009-10-27 14:08:49 ibelyaev Exp $
-#ifndef MCMATCHOBJP2MCRELATOR_H 
+#ifndef MCMATCHOBJP2MCRELATOR_H
 #define MCMATCHOBJP2MCRELATOR_H 1
 
 // Include files
 #include <functional>
+#include <map>
+#include <string>
+
 // from Gaudi
 #include <GaudiKernel/IIncidentListener.h>
 #include <GaudiKernel/IIncidentSvc.h>
@@ -12,22 +15,24 @@
 #include "LoKi/MCMatchObj.h"
 #include "LoKi/MCParticles.h"
 // forward declarations
-namespace LoKi 
+namespace LoKi
 {
   class IReporter;
 }
+
 /** @class MCMatchObjP2MCRelator MCMatchObjP2MCRelator.h
- *  
+ *
  *
  *  @author Juan PALACIOS
  *  @date   2009-03-04
  */
 class MCMatchObjP2MCRelator : public extends1<P2MCPBase, IIncidentListener>
-
 {
-public: 
+
+public:
+
   /// Standard constructor
-  MCMatchObjP2MCRelator( const std::string& type, 
+  MCMatchObjP2MCRelator( const std::string& type,
                          const std::string& name,
                          const IInterface* parent);
 
@@ -37,30 +42,47 @@ public:
 
   virtual StatusCode finalize() ;
 
-  virtual bool isMatched(const LHCb::Particle* particle, 
+  virtual bool isMatched(const LHCb::Particle* particle,
                          const LHCb::MCParticle* mcParticle) const ;
 
 public :
+
   virtual void handle( const Incident& );
 
 private:
 
-  inline LoKi::MCMatch matcher() const 
+  inline LoKi::MCMatch matcher() const
   {
     return LoKi::MCMatch( m_matcher );
   }
 
-  void addTables(LoKi::MCMatchObj* matcher) const;
-  
+  void addTables() const;
+
+  void addTable(const LHCb::Particle* particle) const;
+
+  void loadTable(const std::string & address) const;
+
   void printMCPIDs(const LHCb::MCParticle::ConstVector& mcps) const
   {
     always() << "==================================" << endmsg;
     for (LHCb::MCParticle::ConstVector::const_iterator mcp = mcps.begin();
-         mcp!=mcps.end(); ++mcp) {
+         mcp!=mcps.end(); ++mcp) 
+    {
       always() << "\tMCP PID " << (*mcp)->particleID().pid() << endmsg;
     }
   }
   
+  /** Returns the full location of the given object in the Data Store
+   *
+   *  @param pObj Data object
+   *
+   *  @return Location of given data object
+   */
+  inline std::string objectLocation( const DataObject * pObj ) const
+  {
+    return ( !pObj ? "Null DataObject !" :
+             (pObj->registry() ? pObj->registry()->identifier() : "UnRegistered") );
+  }
 
 private:
 
@@ -71,7 +93,9 @@ private:
   IIncidentSvc* m_incSvc;
 
   mutable bool m_loaded ;
-};
 
+  mutable std::map<std::string,bool> m_pLocDoneMap;
+
+};
 
 #endif // MCMATCHOBJP2MCPRELATOR_H
