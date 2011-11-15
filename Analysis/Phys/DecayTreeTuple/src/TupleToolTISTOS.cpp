6c696da96680a7ed98492920875c9c3dcad7bd72
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-11-15 10:32:41 +0000

    Tidy up the TisTos tool a bit
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@131599 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 61cfcdc46f2d3de337b7817ca54dbf625a6a7c40

--- Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.cpp
@@ -2,9 +2,9 @@
 #include "boost/regex.hpp"
 
 // from Gaudi
-#include "GaudiKernel/ToolFactory.h" 
+#include "GaudiKernel/ToolFactory.h"
 
-#include "Event/Particle.h"     
+#include "Event/Particle.h"
 // kernel
 #include "Kernel/IANNSvc.h"
 
@@ -24,79 +24,62 @@ using namespace LHCb;
 //-----------------------------------------------------------------------------
 
 // Declaration of the Tool Factory
-DECLARE_TOOL_FACTORY( TupleToolTISTOS );
-
+DECLARE_TOOL_FACTORY( TupleToolTISTOS )
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
 TupleToolTISTOS::TupleToolTISTOS( const std::string& type,
-                              const std::string& name,
-                              const IInterface* parent )
-  : TupleToolTriggerBase ( type, name , parent )
-    , m_TriggerTisTosTool()
-    , m_L0TriggerTisTosTool()
+                                  const std::string& name,
+                                  const IInterface* parent )
+  : TupleToolTriggerBase  ( type, name , parent )
+  , m_TriggerTisTosName   ( "TriggerTisTos"     )
+  , m_L0TriggerTisTosName ( "L0TriggerTisTos"   )
+  , m_TriggerTisTosTool   ( NULL                )
+  , m_L0TriggerTisTosTool ( NULL                )
 {
   declareInterface<IParticleTupleTool>(this);
-  declareProperty("TriggerTisTosName",m_TriggerTisTosName="");  
-  declareProperty("L0TriggerTisTosName",m_L0TriggerTisTosName="");  
-
+  declareProperty("TriggerTisTosName",m_TriggerTisTosName);
+  declareProperty("L0TriggerTisTosName",m_L0TriggerTisTosName);
 }
+
 //=============================================================================
 // Destructor
 //=============================================================================
-TupleToolTISTOS::~TupleToolTISTOS() {} 
+TupleToolTISTOS::~TupleToolTISTOS() {}
 
 //=============================================================================
 
 //=========================================================================
 //  initialize
 //=========================================================================
-StatusCode TupleToolTISTOS::initialize( ) 
+StatusCode TupleToolTISTOS::initialize( )
 {
-  
-  StatusCode sc = TupleToolTriggerBase::initialize();
+  const StatusCode sc = TupleToolTriggerBase::initialize();
   if (!sc) return sc;
-  if( m_TriggerTisTosName != "" ){
-    m_TriggerTisTosTool = tool<ITriggerTisTos>( m_TriggerTisTosName,this);
-  } else {
-    m_TriggerTisTosTool = tool<ITriggerTisTos>( "TriggerTisTos",this);
-  }
 
-  if( m_L0TriggerTisTosName != "" ){
-    m_L0TriggerTisTosTool = tool<ITriggerTisTos>( m_L0TriggerTisTosName,this);
-  } else {
-    m_L0TriggerTisTosTool = tool<ITriggerTisTos>( "L0TriggerTisTos",this);
-  }
-  if( !m_TriggerTisTosTool )  return StatusCode::FAILURE;
-  if( !m_L0TriggerTisTosTool )  return StatusCode::FAILURE;
-  
+  m_TriggerTisTosTool = tool<ITriggerTisTos>( m_TriggerTisTosName,"TriggerTisTos",this );
+  m_L0TriggerTisTosTool = tool<ITriggerTisTos>( m_L0TriggerTisTosName,"L0TriggerTisTos",this);
+
   return sc;
-}
-
-//=========================================================================
-//  finalize
-//=========================================================================
-StatusCode TupleToolTISTOS::finalize( ) 
-{
-  return TupleToolTriggerBase::finalize();
 }
 
 //=========================================================================
 //  Fill
 //=========================================================================
-StatusCode TupleToolTISTOS::fillBasic( const LHCb::Particle* 
-				   , const LHCb::Particle* P
-				   , const std::string& head
-				   , Tuples::Tuple& tuple )
+
+StatusCode TupleToolTISTOS::fillBasic( const LHCb::Particle*
+                                       , const LHCb::Particle* P
+                                       , const std::string& head
+                                       , Tuples::Tuple& tuple )
 {
   const std::string prefix=fullName(head);
- 
+
   ITriggerTisTos::TisTosDecision classifiedDec;
-  
+
   if(m_doL0)
   {
-    
+
     m_L0TriggerTisTosTool->setOfflineInput(*P);
     m_L0TriggerTisTosTool->setTriggerInput("L0.*Decision");
     classifiedDec = m_L0TriggerTisTosTool->triggerTisTos();
@@ -104,74 +87,74 @@ StatusCode TupleToolTISTOS::fillBasic( const LHCb::Particle*
     tuple->column( prefix+"L0Global"+"_TIS", classifiedDec.tis());
     tuple->column( prefix+"L0Global"+"_TOS", classifiedDec.tos());
   }
-  
- 
+
+
   m_TriggerTisTosTool->setOfflineInput(*P);
 
   if(m_doHlt1)
   {
-    
+
     //Do the Hlt1
     m_TriggerTisTosTool->setTriggerInput("Hlt1.*Decision");
-    //Fill the decision, tis and tos parametres for the Hlt1 as a whole   
+    //Fill the decision, tis and tos parametres for the Hlt1 as a whole
     classifiedDec = m_TriggerTisTosTool->triggerTisTos();
     tuple->column( prefix+"Hlt1Global"+"_Dec", classifiedDec.decision());
     tuple->column( prefix+"Hlt1Global"+"_TIS", classifiedDec.tis());
     tuple->column( prefix+"Hlt1Global"+"_TOS", classifiedDec.tos());
   }
-  
+
 
   if(m_doHlt2)
   {
-    
+
     //Do the Hlt2
     m_TriggerTisTosTool->setTriggerInput("Hlt2.*Decision");
-    //Fill the decision, tis and tos parametres for the Hlt2 as a whole   
+    //Fill the decision, tis and tos parametres for the Hlt2 as a whole
     classifiedDec = m_TriggerTisTosTool->triggerTisTos();
     tuple->column( prefix+"Hlt2Global"+"_Dec", classifiedDec.decision());
     tuple->column( prefix+"Hlt2Global"+"_TIS", classifiedDec.tis());
     tuple->column( prefix+"Hlt2Global"+"_TOS", classifiedDec.tos());
   }
-  
+
   if(m_doHlt2)
   {
-    
+
     //Do the Hlt2
     m_TriggerTisTosTool->setTriggerInput("Stripping.*Decision");
-    //Fill the decision, tis and tos parametres for the Hlt2 as a whole   
+    //Fill the decision, tis and tos parametres for the Hlt2 as a whole
     classifiedDec = m_TriggerTisTosTool->triggerTisTos();
     tuple->column( prefix+"StrippingGlobal"+"_Dec", classifiedDec.decision());
     tuple->column( prefix+"StrippingGlobal"+"_TIS", classifiedDec.tis());
     tuple->column( prefix+"StrippingGlobal"+"_TOS", classifiedDec.tos());
   }
-  
-  
+
+
   return StatusCode::SUCCESS;
 }
 
-StatusCode TupleToolTISTOS::fillVerbose( const LHCb::Particle* 
-				   , const LHCb::Particle* P
-				   , const std::string& head
-				   , Tuples::Tuple& tuple )
+StatusCode TupleToolTISTOS::fillVerbose( const LHCb::Particle*
+                                         , const LHCb::Particle* P
+                                         , const std::string& head
+                                         , Tuples::Tuple& tuple )
 {
   const std::string prefix=fullName(head);
- 
+
   bool decision = false;
   bool tis = 0;
   bool tos = 0;
-  
-  
+
+
   if( m_verboseL0 )
-  {      
+  {
     m_L0TriggerTisTosTool->setOfflineInput(*P);
     //Now loop over all the subtriggers
     for( std::vector< std::string >::const_iterator s=m_l0.begin();s!= m_l0.end();++s){
-        if (msgLevel(MSG::VERBOSE)) verbose() << "Selection " << *s << endmsg  ;
-        m_L0TriggerTisTosTool->triggerTisTos(*s,decision,tis,tos);
-        tuple->column( prefix+*s+"_Dec", decision);
-        tuple->column( prefix+*s+"_TIS", tis);
-        tuple->column( prefix+*s+"_TOS", tos);
-      }
+      if (msgLevel(MSG::VERBOSE)) verbose() << "Selection " << *s << endmsg  ;
+      m_L0TriggerTisTosTool->triggerTisTos(*s,decision,tis,tos);
+      tuple->column( prefix+*s+"_Dec", decision);
+      tuple->column( prefix+*s+"_TIS", tis);
+      tuple->column( prefix+*s+"_TOS", tos);
+    }
   }
 
   m_TriggerTisTosTool->setOfflineInput(*P);
@@ -179,22 +162,22 @@ StatusCode TupleToolTISTOS::fillVerbose( const LHCb::Particle*
   {
     //Now loop over all the subtriggers
     for( std::vector< std::string >::const_iterator s=m_hlt1.begin();s != m_hlt1.end();++s){
-        if (msgLevel(MSG::VERBOSE)) verbose() << "Selection " << *s << endmsg  ;
-        if( s->find("Hlt1L0") == 0 ){
-          // assume that we want to look at L0 rather than Hlt1L0 decisions
-          m_TriggerTisTosTool->selectionTisTos(*s,decision,tis,tos);
-          decision = m_TriggerTisTosTool->hltSelectionObjectSummaries(*s).size()!=0;      
-        } else {
-          m_TriggerTisTosTool->triggerTisTos(*s,decision,tis,tos);
-        }        
-        tuple->column( prefix+*s+"_Dec", decision);
-        tuple->column( prefix+*s+"_TIS", tis);
-        tuple->column( prefix+*s+"_TOS", tos);
+      if (msgLevel(MSG::VERBOSE)) verbose() << "Selection " << *s << endmsg  ;
+      if( s->find("Hlt1L0") == 0 ){
+        // assume that we want to look at L0 rather than Hlt1L0 decisions
+        m_TriggerTisTosTool->selectionTisTos(*s,decision,tis,tos);
+        decision = m_TriggerTisTosTool->hltSelectionObjectSummaries(*s).size()!=0;
+      } else {
+        m_TriggerTisTosTool->triggerTisTos(*s,decision,tis,tos);
       }
+      tuple->column( prefix+*s+"_Dec", decision);
+      tuple->column( prefix+*s+"_TIS", tis);
+      tuple->column( prefix+*s+"_TOS", tos);
+    }
   }
-  
+
   if( m_verboseHlt2 )
-  {      
+  {
     //Now loop over all the subtriggers
     for( std::vector< std::string >::const_iterator s=m_hlt2.begin();s != m_hlt2.end();++s)
     {
@@ -205,9 +188,9 @@ StatusCode TupleToolTISTOS::fillVerbose( const LHCb::Particle*
       tuple->column( prefix+*s+"_TOS", tos);
     }
   }
-  
+
   if( m_verboseStripping )
-  {      
+  {
     //Now loop over all the subtriggers
     for( std::vector< std::string >::const_iterator s=m_stripping.begin();s != m_stripping.end();++s)
     {
--- Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.h
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolTISTOS.h
@@ -5,86 +5,81 @@
 // from Gaudi
 #include "TupleToolTriggerBase.h"
 #include "Kernel/IParticleTupleTool.h"            // Interface
-#include "Kernel/ITriggerTisTos.h" //
+#include "Kernel/ITriggerTisTos.h"
+
 /** @class TupleToolTISTOS TupleToolTISTOS.h
  *
  *  This is an adaptation of the TISTOS example by Tomasz
- *  Skwarnicki for the DecayTreeTuple framework  
+ *  Skwarnicki for the DecayTreeTuple framework
  *
- *  It saves the trigger TIS/TOS decisions for each 
+ *  It saves the trigger TIS/TOS decisions for each
  *  particle for the L0, and each HLT Alley and HltSelection
  *
- *  Saved values for each trigger:  
+ *  Saved values for each trigger:
  *
  *  Decision : the trigger decision, 1 is pass, 0 is fail
- *  TIS      : was the event tis? 1 is yes, 0 is no.  
+ *  TIS      : was the event tis? 1 is yes, 0 is no.
  *  TOS      : was the event tos? 1 is yes, 0 is no.
  *
  * If verbose is true the tool needs somehow to find a list of triggers to fill.
- * In this case it uses the base class TupleToolTriggerBase to sort everything out. 
+ * In this case it uses the base class TupleToolTriggerBase to sort everything out.
  *
  * If \b VerboseL0 = true
  * L0Decision_xxx : LHCb::L0DUReport->channelDecisionByName(xxx)
- *  
+ *
  * If \b VerboseHlt1 = true
  * Hlt1_xxx_Decision : filled
- * 
+ *
  * If \b VerboseHlt2 = true
  * Hlt2_xxx_Decision : filled
- * 
+ *
  * Verbose flag is a shortcut to turn all verbosity on.
- *  
+ *
  *  @author V. Gligorov
  *  @date   2008-04-09
  */
 class IP2VVPartAngleCalculator;
 
-class TupleToolTISTOS : public TupleToolTriggerBase, virtual public IParticleTupleTool {
-public: 
+class TupleToolTISTOS : public TupleToolTriggerBase,
+                        virtual public IParticleTupleTool
+{
+
+public:
+
   /// Standard constructor
-  TupleToolTISTOS( const std::string& type, 
-                 const std::string& name,
-                 const IInterface* parent);
+  TupleToolTISTOS( const std::string& type,
+                   const std::string& name,
+                   const IInterface* parent);
 
   virtual ~TupleToolTISTOS( ); ///< Destructor
 
   StatusCode initialize() ;
-  
-  StatusCode finalize() ;
-
-  //implemented in TupleToolTriggerBase
-  virtual StatusCode fill( const LHCb::Particle* M
-		   , const LHCb::Particle* P
-			   , const std::string& head
-			   , Tuples::Tuple& tuple)
-  {
-    return TupleToolTriggerBase::fill(M, P, head, tuple);
-  };
 
 protected:
-  
+
   /// to make it possible to replace standard TisTos tools.
   std::string m_TriggerTisTosName;
   std::string m_L0TriggerTisTosName;
-  
 
 private:
 
   ITriggerTisTos* m_TriggerTisTosTool;
   ITriggerTisTos* m_L0TriggerTisTosTool;
 
-  StatusCode fillBasic( const LHCb::Particle* 
-                               , const LHCb::Particle* P
-                               , const std::string& head
-                               , Tuples::Tuple& tuple );
-  
-  
-  
-  StatusCode fillVerbose( const LHCb::Particle* 
-                   , const LHCb::Particle* P
-                   , const std::string& head
-                   , Tuples::Tuple& tuple );
-  
-  
+private:
+
+  StatusCode fillBasic( const LHCb::Particle*
+                        , const LHCb::Particle* P
+                        , const std::string& head
+                        , Tuples::Tuple& tuple );
+
+
+
+  StatusCode fillVerbose( const LHCb::Particle*
+                          , const LHCb::Particle* P
+                          , const std::string& head
+                          , Tuples::Tuple& tuple );
+
+
 };
 #endif // TUPLETOOLTISTOS_H
