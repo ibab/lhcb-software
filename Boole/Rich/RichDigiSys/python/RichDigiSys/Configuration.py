d5d9de058ea111a5dd493247a398dfd68f3748f7
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2011-11-18 17:04:53 +0000

    ! 2011-11-18 - Chris Jones  - Small improvements, to allow more sorts of digitisation sequences to    be set up
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@131898 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 354eaf62787439276b09de73efe1e7ca40a9dec0

--- Boole/Rich/RichDigiSys/python/RichDigiSys/Configuration.py
+++ Boole/Rich/RichDigiSys/python/RichDigiSys/Configuration.py
@@ -9,6 +9,7 @@ __author__  = "Chris Jones <Christopher.Rob.Jones@cern.ch>"
 
 from LHCbKernel.Configuration import *
 from Configurables import ( Rich__MC__Digi__Signal,
+                            Rich__MC__Digi__CopySummedDepositsToDigits,
                             Rich__MC__Digi__SimpleChargeSharing,
                             Rich__MC__Digi__DetailedFrontEndResponse,
                             Rich__MC__Digi__SimpleFrontEndResponse,
@@ -28,17 +29,26 @@ class RichDigiSysConf(LHCbConfigurableUser):
         "UseSpillover"         : True
        ,"UseLHCBackground"     : False
        ,"ChargeShareFraction"  : 0.025
-       ,"HpdResponseModel"     : "Detailed"
+       ,"ResponseModel"        : "Detailed"
        ,"ChargeShareModel"     : "Simple"
        ,"RawDataFormatVersion" : 129
+       ,"Sequencer"            : None
+       ,"OutputLevel"          : INFO
         }
+
+    def makeComponent(self, type, name):
+        c = type(name)
+        if self.isPropertySet("OutputLevel") : c.OutputLevel = self.getProp("OutputLevel")
+        return c
 
     ## @brief Apply the configuration to the given GaudiSequencer
     #  @param sequence The GaudiSequencer to add the RICH digitisation to
-    def applyConf(self,sequence) :
+    def applyConf(self) :
+
+        sequence = self.getProp("Sequencer")
 
         # Process the raw hit signals.
-        pdSignals = Rich__MC__Digi__Signal("RichPDSignal")
+        pdSignals = self.makeComponent(Rich__MC__Digi__Signal,"RichPDSignal")
         pdSignals.UseSpillover     = self.getProp("UseSpillover")
         pdSignals.UseLHCBackground = self.getProp("UseLHCBackground")
         sequence.Members += [pdSignals]
@@ -46,28 +56,31 @@ class RichDigiSysConf(LHCbConfigurableUser):
         # Charge Sharing
         chargeShareModel = self.getProp("ChargeShareModel")
         if chargeShareModel == "Simple" :
-            chargeShare = Rich__MC__Digi__SimpleChargeSharing("RichChargeShare")
+            chargeShare = self.makeComponent(Rich__MC__Digi__SimpleChargeSharing,"RichChargeShare")
             chargeShare.ChargeShareFraction = self.getProp("ChargeShareFraction")
+            sequence.Members += [chargeShare]
+        elif chargeShareModel == "None" :
+            pass
         else :
             raise RuntimeError("ERROR : Unknown HPD charge sharing model '%s'"%chargeShareModel)
-        sequence.Members += [chargeShare]
 
         # deposit summation
-        sequence.Members += [ Rich__MC__Digi__SummedDeposits("RichSummedDeposits") ]
+        sequence.Members += [ self.makeComponent(Rich__MC__Digi__SummedDeposits,"RichSummedDeposits") ]
 
         # HPD response
-        hpdModel = self.getProp("HpdResponseModel")
+        hpdModel = self.getProp("ResponseModel")
         if hpdModel == "Detailed" :
-            response = Rich__MC__Digi__DetailedFrontEndResponse("RichPDResponse")
+            response = self.makeComponent(Rich__MC__Digi__DetailedFrontEndResponse,"RichPDResponse")
             response.TimeCalib = [ 10, 6 ]
         elif hpdModel == "Simple" :
-            response = Rich__MC__Digi__SimpleFrontEndResponse("RichPDResponse")
+            response = self.makeComponent(Rich__MC__Digi__SimpleFrontEndResponse,"RichPDResponse")
+        elif hpdModel == "Copy" :
+            response = self.makeComponent(Rich__MC__Digi__CopySummedDepositsToDigits,"RichPDResponse")
         else :
             raise RuntimeError("ERROR : Unknown HPD Response model '%s'"%hpdModel)
         sequence.Members += [response]
 
         # Fill the Raw Event
-        rawEvtFill = Rich__MC__Digi__MCRichDigitsToRawBufferAlg("RichFillRawBuffer")
+        rawEvtFill = self.makeComponent(Rich__MC__Digi__MCRichDigitsToRawBufferAlg,"RichFillRawBuffer")
         rawEvtFill.DataVersion = self.getProp("RawDataFormatVersion")
         sequence.Members += [rawEvtFill]
-
