9775ee51d16ea76f6fe52e161c1f2e107c78387f
Marcin Kucharczyk <marcin@cern.ch>
2011-11-25 11:11:43 +0000

    Add Hlt2HighPtJetsLines.py lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@132221 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3cd4a4b09c5ae26a9b719b2f404a297b645f694a

---
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2HighPtJetsLines.py
@@ -0,0 +1,66 @@
+"""
+Module for high-pt jets selection
+"""
+
+__author__  = ['Marcin Kucharczyk']
+__date__    = '17/09/2011'
+__version__ = '$Revision: 1.0$'
+__all__ = ('Hlt2HighPtJetsLinesConf')
+
+from Gaudi.Configuration import *
+from HltLine.HltLinesConfigurableUser import HltLinesConfigurableUser
+import GaudiKernel.SystemOfUnits as units
+
+class Hlt2HighPtJetsLinesConf(HltLinesConfigurableUser) :
+    
+    __slots__ = {            
+           'MinWeightSeedCut' : 5
+        ,  'ConeExtFactorCut' : 4.0
+        ,  'MaxDeltaPhiCut'   : 8.0
+        ,  'MaxDeltaEtaCut'   : 0.5
+        ,  'Min1stJetPtCut'   : 7.0
+        ,  'Min2ndJetPtCut'   : 0.0
+        ,  'JetMultCut'       : 1
+        ,  'NrPVsCut'         : 5
+        ,  'MinSumEtCut'      : 15.0
+        ,  'PostScale'        : 1.0
+    }
+                
+    def __apply_configuration__(self) :
+
+        from HltLine.HltLine import Hlt2Line
+        from Configurables import GaudiSequencer, HltANNSvc
+        from HltTracking.HltPVs import PV3D
+        from HltTracking.Hlt2TrackingConfigurations import Hlt2BiKalmanFittedForwardTracking
+
+        #######################################################################
+        DVSeq = []
+        hlt2Tracking = Hlt2BiKalmanFittedForwardTracking().hlt2PrepareTracks()
+        DVSeq.extend([PV3D(),hlt2Tracking])
+        
+        ####################################################################### 
+        from Configurables import TrackClusterFinder
+        Hlt2TrkFinder = TrackClusterFinder("Hlt2TrkFinder")
+        DVSeq.append(Hlt2TrkFinder)
+        Hlt2TrkFinder.InputTracks = [hlt2Tracking.outputSelection()]
+        Hlt2TrkFinder.MinWeightSeed = self.getProp('MinWeightSeedCut')
+        Hlt2TrkFinder.ConeExtFactor = self.getProp('ConeExtFactorCut')
+        Hlt2TrkFinder.MaxDeltaPhi = self.getProp('MaxDeltaPhiCut')
+        Hlt2TrkFinder.MaxDeltaEta = self.getProp('MaxDeltaEtaCut')
+        Hlt2TrkFinder.Min1stJetPt = self.getProp('Min1stJetPtCut')
+        Hlt2TrkFinder.Min2ndJetPt = self.getProp('Min2ndJetPtCut')
+        Hlt2TrkFinder.JetMult = self.getProp('JetMultCut')
+        Hlt2TrkFinder.NrPVs = self.getProp('NrPVsCut')
+        Hlt2TrkFinder.MinSumEt = self.getProp('MinSumEtCut')
+
+        Hlt1HighPtJets = "HLT_PASS_RE('Hlt1L0HighSumETJet.*Decision')"
+        
+        line = Hlt2Line('HighPtJets'
+                        , prescale = self.prescale
+                        , HLT = Hlt1HighPtJets
+                        , algos = DVSeq + [Hlt2TrkFinder]
+                        , postscale = self.getProp('PostScale')
+                        )
+
+        HltANNSvc().Hlt2SelectionID.update( { "Hlt2HighPtJetsDecision" : 50295 } )
+        
