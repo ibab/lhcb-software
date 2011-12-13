fb2df34f43498ed6e330edd60a000464eda74a2d
Markus Frank <frankb@cern.ch>
2011-12-13 10:19:20 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@132846 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 2f1470d6fe0c0cbec147f6d2f95b4c9feffbca26

--- Online/Online/GaudiOnline/src/DelaySleepAlg.cpp
+++ Online/Online/GaudiOnline/src/DelaySleepAlg.cpp
@@ -16,16 +16,19 @@ namespace LHCb  {
   *  @date   2005-10-13
   */
   class DelaySleepAlg : public Algorithm {
-    int m_delay;
+    int m_delay, m_muDelay;
   public: 
     /// Standard constructor
     DelaySleepAlg(const std::string& nam,ISvcLocator* pSvc) : Algorithm(nam,pSvc)
-    {  declareProperty("DelayTime",m_delay=1);          }
+    {
+      declareProperty("DelayTime",m_delay=1);          
+      declareProperty("MicroDelayTime",m_muDelay=0);          
+    }
     /// Destructor
     virtual ~DelaySleepAlg()  {} 
     /// Main execution
     virtual StatusCode execute()  {
-      if ( m_delay > 0 ) ::lib_rtl_sleep(m_delay);
+      if ( m_delay > 0 || m_muDelay > 0 ) ::lib_rtl_usleep(m_delay*1000+m_muDelay);
       return StatusCode::SUCCESS;
     }
   };
