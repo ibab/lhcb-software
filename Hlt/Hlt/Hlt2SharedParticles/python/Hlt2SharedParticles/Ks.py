528eab94ca2415df7852f72f9cf1768ea8ee2e29
sgregson <sgregson@cern.ch>
2012-01-05 16:27:23 +0000

    Updated shared Ks. Removed "Good Pions" KsLL variant and edited KsLL/DD cuts to reduce combinatoric timing
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@133587 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 337e6f49380ed14afec6936c7659700217f8b0a6

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Ks.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Ks.py
@@ -7,29 +7,19 @@
 #
 ##
 from Gaudi.Configuration import *
-from Hlt2SharedParticles.GoodParticles import GoodPions
 from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedDownPions, BiKalmanFittedPions
 from Configurables import CombineParticles
 from HltLine.HltLine import bindMembers, Hlt2Member
 
 __all__ = ( 'KsLL', 'KsDD' )
 
-Hlt2SharedKsLL = Hlt2Member( CombineParticles, "KsLL"
-                           , DecayDescriptor = "KS0 -> pi+ pi-" 
-                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<20)" } 
-                           , CombinationCut = "(ADAMASS('KS0')<50*MeV)"
-                           , MotherCut = "(ADMASS('KS0')<35*MeV) & (VFASPF(VCHI2PDOF)<30)"
-                           , Inputs = [ GoodPions ]
-                           )
-
-KsLL = bindMembers( "SharedKsLL", [ GoodPions, Hlt2SharedKsLL ] )
 
 # The LL K shorts, track fitted
 Hlt2SharedKsLLTF = Hlt2Member( CombineParticles, "KsLLTF"
                            , DecayDescriptor = "KS0 -> pi+ pi-" 
-                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<20)" } 
-                           , CombinationCut = "(ADAMASS('KS0')<50*MeV)"
-                           , MotherCut = "(ADMASS('KS0')<35*MeV) & (VFASPF(VCHI2PDOF)<30)"
+                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV) & (MIPCHI2DV(PRIMARY)>25)" } 
+                           , CombinationCut = "(ADAMASS('KS0')<50*MeV) & (APT>700*MeV) & (ADOCACHI2CUT(30, ''))"
+                           , MotherCut = "(ADMASS('KS0')<35*MeV) & (PT>700*MeV) & (VFASPF(VCHI2PDOF)<30)"
                            , Inputs = [ BiKalmanFittedPions ]
                            )
 
@@ -38,9 +28,9 @@ KsLLTF = bindMembers( "SharedKsLLTF", [BiKalmanFittedPions , Hlt2SharedKsLLTF ] 
 # Now the downstream K shorts, requires fitted tracks!
 Hlt2SharedKsDD = Hlt2Member( CombineParticles, "KsDD"
                            , DecayDescriptor = "KS0 -> pi+ pi-"
-                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<20)" }
-                           , CombinationCut = "(ADAMASS('KS0')<80*MeV)"
-                           , MotherCut = "(ADMASS('KS0')<64*MeV) & (VFASPF(VCHI2PDOF)<30)"
+                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV)" }
+                           , CombinationCut = "(ADAMASS('KS0')<80*MeV) & (APT>700*MeV) & (ADOCACHI2CUT(30, ''))"
+                           , MotherCut = "(ADMASS('KS0')<64*MeV) & (PT>700*MeV) & (VFASPF(VCHI2PDOF)<30)"
                            , Inputs = [ BiKalmanFittedDownPions ]
                            )
 
