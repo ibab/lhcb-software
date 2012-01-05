131319b5dfae7646aa63e043072dcfc81fcd34fb
Frederic Henry Blanc <fred.blanc@epfl.ch>
2012-01-05 20:31:24 +0000

    update StrippingB2CharmlessQuasi2Body for Stripping17b
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@133593 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8ed38660415f6f4c3a0eb03211bda1b0b50a3ab4

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
@@ -3,13 +3,12 @@ B->charmless quasi 2-body selection
 '''
 
 __author__ = ['Fred Blanc']
-__date__ = '11/08/2011'
-__version__ = '$Revision: 2.3 $'
+__date__ = '06/01/2012'
+__version__ = '$Revision: 2.4 $'
 
 __all__ = ( 'B2Quasi2BodyConf',
             'makeDiTrackList',
-            'makeB2Q2B4pi1n',
-            'makeB2Q2B4pi2n',
+            'makeB2Q2B4pi1',
             'makeB2Q2B3pi' )
 
 config_params = {'Q2BPrescale'     : 1.,
@@ -17,12 +16,12 @@ config_params = {'Q2BPrescale'     : 1.,
                  'Q2BResMinP'      : 10.,
                  'Q2BResMaxMass'   : 1000.,
                  'Q2BResVtxChiDOF' : 9.,
-                 'Q2BBMinM4pi'     : 2500.,
-                 'Q2BBMinM3pi'     : 4000.,
+                 'Q2BBMinM4pi'     : 3800.,
+                 'Q2BBMinM3pi'     : 4400.,
                  'Q2BBMaxM'        : 5700.,
-                 'Q2BBMinCorrM4pi' : 4000.,
-                 'Q2BBMinCorrM3pi' : 4600.,
-                 'Q2BBMaxCorrM'    : 7000.,
+                 'Q2BBMinCorrM4pi' : 0.,
+                 'Q2BBMinCorrM3pi' : 0.,
+                 'Q2BBMaxCorrM'    : 6000.,
                  'Q2BBVtxChi2DOF'  : 9.}
 
 
@@ -62,7 +61,7 @@ class B2Quasi2BodyConf(LineBuilder) :
                                     Algorithm = _trkFilter,
                                     RequiredSelections = [StdNoPIDsPions])
 
-        _trkFilter_HiP = FilterDesktop(Code = "(PT > 2500*MeV) & (P > 10*GeV)")
+        _trkFilter_HiP = FilterDesktop(Code = "(PT > 1000*MeV) & (P > 10*GeV)")
         self.TrackList_HiP = Selection( 'TrackList_HiP' + self.name,
                                          Algorithm = _trkFilter_HiP,
                                          RequiredSelections = [self.TrackList])
@@ -74,26 +73,13 @@ class B2Quasi2BodyConf(LineBuilder) :
                                             MaxMassCut = config['Q2BResMaxMass'],
                                             VtxChi2DOFCut = config['Q2BResVtxChiDOF'] )
 
-        _diTrackFilter_HiPt = FilterDesktop(Code = "(PT > 1000*MeV)")
+        _diTrackFilter_HiPt = FilterDesktop(Code = "(PT > 2000*MeV)")
         self.DiTrackList_HiPt = Selection( 'DiTracksHiPtForCharmlessB' + self.name,
                                            Algorithm = _diTrackFilter_HiPt,
                                            RequiredSelections = [self.DiTrackList])
 
-        B2Q2B4pi1nName = self.name + "4pi1nSelection"
-        self.B2CharmlessQ2B4pi1n = makeB2Q2B4pi1n( B2Q2B4pi1nName,
-                                                   diTrkList=self.DiTrackList_HiPt,
-                                                   MinMassCut = config['Q2BBMinM4pi'],
-                                                   MaxMassCut = config['Q2BBMaxM'],
-                                                   MinCorrMCut = config['Q2BBMinCorrM4pi'],
-                                                   MaxCorrMCut = config['Q2BBMaxCorrM'],
-                                                   VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
-
-        self.Q2B4pi1nLine = StrippingLine( B2Q2B4pi1nName+"Line",
-                                           prescale = config['Q2BPrescale'],
-                                           selection = self.B2CharmlessQ2B4pi1n )
-
-        B2Q2B4pi2nName = self.name + "4pi2nSelection"
-        self.B2CharmlessQ2B4pi2n = makeB2Q2B4pi2n( B2Q2B4pi2nName,
+        B2Q2B4piName = self.name + "4piSelection"
+        self.B2CharmlessQ2B4pi = makeB2Q2B4pi( B2Q2B4piName,
                                                    diTrkList=self.DiTrackList,
                                                    MinMassCut = config['Q2BBMinM4pi'],
                                                    MaxMassCut = config['Q2BBMaxM'],
@@ -101,9 +87,9 @@ class B2Quasi2BodyConf(LineBuilder) :
                                                    MaxCorrMCut = config['Q2BBMaxCorrM'],
                                                    VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
 
-        self.Q2B4pi2nLine = StrippingLine( B2Q2B4pi2nName+"Line",
+        self.Q2B4piLine = StrippingLine( B2Q2B4piName+"Line",
                                            prescale = config['Q2BPrescale'],
-                                           selection = self.B2CharmlessQ2B4pi2n )
+                                           selection = self.B2CharmlessQ2B4pi )
 
         B2Q2B3piName = self.name + "3piSelection"
         self.B2CharmlessQ2B3pi = makeB2Q2B3pi( B2Q2B3piName,
@@ -119,8 +105,7 @@ class B2Quasi2BodyConf(LineBuilder) :
                                          prescale = config['Q2BPrescale'],
                                          selection = self.B2CharmlessQ2B3pi )
 
-        self.registerLine(self.Q2B4pi1nLine)
-        self.registerLine(self.Q2B4pi2nLine)
+        self.registerLine(self.Q2B4piLine)
         self.registerLine(self.Q2B3piLine)
 
 def makeDiTrackList( name,
@@ -143,7 +128,7 @@ def makeDiTrackList( name,
                      Algorithm = _combineDiTrack,
                      RequiredSelections = [ trkList ] )
 
-def makeB2Q2B4pi1n( name,
+def makeB2Q2B4pi( name,
                     diTrkList,
                     MinMassCut,
                     MaxMassCut,
@@ -151,50 +136,21 @@ def makeB2Q2B4pi1n( name,
                     MaxCorrMCut,
                     VtxChi2DOFCut ) :
     """
-    Charmless Q2B to 4pi selection with missing mass from 1 (neutral) particle
+    Charmless Q2B to 4pi selection with missing mass
     """
 
-    _B2Q2B4pi1nPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
-    _B2Q2B4pi1nPreVertexCuts += " & ( ACHILD(MM,1) + ACHILD(MM,2) < 1100. + 0.2*(AM-3500.) )" %locals()
+    _B2Q2B4piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
+    _B2Q2B4piPreVertexCuts += " & ( APT > 1000 )" %locals()
 
-    _B2Q2B4pi1nPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
-    _B2Q2B4pi1nPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" %locals()
+    _B2Q2B4piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
+    _B2Q2B4piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" %locals()
 
-    _combineB2Q2B4pi1n = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
-                                        MotherCut = _B2Q2B4pi1nPostVertexCuts,
-                                        CombinationCut = _B2Q2B4pi1nPreVertexCuts )
+    _combineB2Q2B4pi = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
+                                        MotherCut = _B2Q2B4piPostVertexCuts,
+                                        CombinationCut = _B2Q2B4piPreVertexCuts )
 
     return Selection(name,
-                     Algorithm = _combineB2Q2B4pi1n,
-                     RequiredSelections = [ diTrkList ] )
-
-
-def makeB2Q2B4pi2n( name,
-                    diTrkList,
-                    MinMassCut,
-                    MaxMassCut,
-                    MinCorrMCut,
-                    MaxCorrMCut,
-                    VtxChi2DOFCut ) :
-    """
-    Charmless Q2B to 4pi selection with missing mass from 2 (neutral) particles
-    """
-
-    _B2Q2B4pi2nPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
-    _B2Q2B4pi2nPreVertexCuts += " & ( ACHILD(MM,1) + ACHILD(MM,2) < 1300. )" %locals()
-    _B2Q2B4pi2nPreVertexCuts += " & ( ACHILD(MM,1) + ACHILD(MM,2) < 500.+1.6*(AM-2500.) )" %locals()
-    _B2Q2B4pi2nPreVertexCuts += " & ( ACHILD(PT,1) + ACHILD(PT,2) < 3000.+AM )" %locals()
-
-    _B2Q2B4pi2nPostVertexCuts = "(VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" %locals()
-    _B2Q2B4pi2nPostVertexCuts += " & ( BPVCORRM < 6000. )" %locals()
-    _B2Q2B4pi2nPostVertexCuts += " & ( MINTREE('rho(770)0'==ABSID, MM) <450.*MeV)" %locals()
-
-    _combineB2Q2B4pi2n = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
-                                        MotherCut = _B2Q2B4pi2nPostVertexCuts,
-                                        CombinationCut = _B2Q2B4pi2nPreVertexCuts )
-
-    return Selection(name,
-                     Algorithm = _combineB2Q2B4pi2n,
+                     Algorithm = _combineB2Q2B4pi,
                      RequiredSelections = [ diTrkList ] )
 
 
@@ -211,11 +167,12 @@ def makeB2Q2B3pi( name,
     """
 
     _B2Q2B3piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
+    _B2Q2B3piPreVertexCuts += " & ( APT > 1000 )" %locals()
 
     _B2Q2B3piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
     _B2Q2B3piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s ) " %locals()
 
-    _combineB2Q2B3pi = CombineParticles( DecayDescriptor="B+ -> rho(770)0 pi+",
+    _combineB2Q2B3pi = CombineParticles( DecayDescriptor="[B+ -> rho(770)0 pi+]cc",
                                          MotherCut = _B2Q2B3piPostVertexCuts,
                                          CombinationCut = _B2Q2B3piPreVertexCuts )
 
