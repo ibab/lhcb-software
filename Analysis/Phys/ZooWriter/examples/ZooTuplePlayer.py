7c9ff49fd8211b4b46ebbe2f1df041420fb39063
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2012-01-19 22:48:53 +0000

    modularise ZooTuplePlayer.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@133930 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e753142bc336da6b1744500b3d47893df513e4ee

--- Analysis/Phys/ZooWriter/examples/ZooTuplePlayer.py
+++ Analysis/Phys/ZooWriter/examples/ZooTuplePlayer.py
@@ -4,6 +4,10 @@
 implement something similar to the Draw() method of TTree for Zoo tuples
 to facilitate a quick look at a Zoo tuple
 
+since the framework for plotting is rather useful, we use the same code which
+checks if a candidate is accepted for plotting to implement a ZooTuple
+stripping utility
+
 @author Manuel Schiller <manuel.schiller@nikhef.nl>
 @date 2011-09-27
 
@@ -11,6 +15,7 @@ to facilitate a quick look at a Zoo tuple
 
 import ZooPy
 import ROOT
+import sys
 
 class ZooTuplePlayer:
     """
@@ -33,6 +38,169 @@ class ZooTuplePlayer:
 	"""
 	self.tree = tree
 	self.histo = None
+
+    def runOverZooP(self, collection, cuts, maxevts, startevt,
+	    candaction = None, evtaction = None):
+	"""
+	runOverZooP runs over a given collection of a ZooTuple, applying the
+	given cuts to each of the candidates of the collection; for each
+	accepted candidate, a user-defined action can be executed, and for
+	each event with accepted candidates, a user-defined per-event action
+	can be executed
+
+	parameters are:
+	collection	name of the collection to read candidates from
+    	cuts            list of zero or more functions or lambda expressions
+    			which evaluate to True of False; a candidate is
+    			accepted as passing all the cuts if all expressions
+    			evaluate to True
+    	maxevents       maximum number of events to process
+    	startevt	event from which to start plotting
+	candaction	function or lambda expression to execute in case of an
+			accepted candidate, or None if no action is to be
+			executed for an accepted candidates
+			(default: None)
+	evtaction	function or lambda expression to be executed for an
+			event with accepted candidates, or None if no action
+			is to be executed for events with accepted candidates
+			(default: None)
+	
+	The function or lambda expression passed for candaction is called with
+	two parameters, the candidate and the Zoo event structure; the
+	function or lambda expression passed for evtaction is called with a
+	single parameter, the Zoo event structure.
+
+	The list of cuts is defined an used analogously to that of the
+	DrawZooP method, see the documentation there to get an idea of how
+	this is supposed to work.
+	"""
+	zoops = ROOT.ZooObjRefArray(ROOT.ZooP)()
+	zooev = ROOT.ZooEv()
+	self.tree.SetBranchAddress(collection, ROOT.AddressOf(zoops))
+	self.tree.SetBranchAddress("Event", ROOT.AddressOf(zooev))
+	# prepare to loop over events in Zoo tuple
+	nevents = self.tree.GetEntries()
+	if startevt >= nevents:
+	    print 'runOverZooP: no events left!'
+	    return None
+	if maxevts > 0 and startevt + maxevts < nevents:
+	    nevents = startevt + maxevts
+	self.tree.SetCacheLearnEntries(1000)
+	nbytes = 0
+	for i in range(startevt, nevents):
+	    # read next event
+	    self.tree.LoadTree(i)
+	    nbytes += self.tree.GetEntry(i)
+	    acceptevt = False
+	    # loop over candidates in collection
+	    for j in range(0, zoops.size()):
+		zp = zoops[j]
+		# work out if the candidate satisfies the cuts
+		accept = True
+		for cut in cuts:
+		    if not cut(zp, zooev):
+			accept = False
+			break
+		if not accept:
+		    continue
+		# candidate accepted, event accepted
+		acceptevt = True
+		# perform per candidate action
+		if candaction != None:
+		    candaction(zp, zooev)
+	    if (i - startevt) % 1024 == 0:
+		print ("\r% 9d/% 9d events read (%9.3f GBytes)" % \
+			tuple([ (i - startevt), nevents, nbytes / 1024. / 1024. / 1024. ]) ),
+		sys.stdout.flush()
+	    # if at least one candidate in the event was accepted, perform the
+	    # per-event action
+	    if acceptevt and evtaction != None:
+		evtaction(zooev)
+	# free branches
+	self.tree.SetBranchAddress(collection, ROOT.MakeNullPointer())
+	self.tree.SetBranchAddress("Event", ROOT.MakeNullPointer())
+	print ("\r% 9d/% 9d events read (%9.3f GBytes)" % \
+		tuple([ nevents, nevents, nbytes / 1024. / 1024. / 1024. ]) )
+	return nevents
+
+    def runOverZooMCP(self, cuts, maxevts, startevt,
+	    mcpaction = None, evtaction = None):
+	"""
+	runOverZooP runs over all MC particles of a ZooTuple, applying the
+	given cuts to each of them; for each accepted MC particle, a
+	user-defined action can be executed, and for each event with accepted
+	MC particles, a user-defined per-event action can be executed
+
+	parameters are:
+    	cuts            list of zero or more functions or lambda expressions
+    			which evaluate to True of False; a MC particle is
+    			accepted as passing all the cuts if all expressions
+    			evaluate to True
+    	maxevents       maximum number of events to process
+    	startevt	event from which to start plotting
+	mcpaction	function or lambda expression to execute in case of an
+			accepted MC particle, or None if no action is to be
+			executed for an accepted MC particle
+			(default: None)
+	evtaction	function or lambda expression to be executed for an
+			event with accepted MC particles, or None if no action
+			is to be executed for events with accepted MC
+			particles
+			(default: None)
+	
+	The function or lambda expression passed for candaction is called with
+	two parameters, the candidate and the Zoo event structure; the
+	function or lambda expression passed for evtaction is called with a
+	single parameter, the Zoo event structure.
+
+	The list of cuts is defined an used analogously to that of the
+	DrawZooP method, see the documentation there to get an idea of how
+	this is supposed to work.
+	"""
+	# initialise branches
+	zooev = ROOT.ZooEv()
+	self.tree.SetBranchAddress("Event", ROOT.AddressOf(zooev))
+	# prepare to loop over events in Zoo tuple
+	nevents = self.tree.GetEntries()
+	if startevt >= nevents:
+	    print 'runOverZooMCP: no events left!'
+	    return None
+	if maxevts > 0 and startevt + maxevts < nevents:
+	    nevents = startevt + maxevts
+	self.tree.SetCacheLearnEntries(1000)
+	nbytes = 0
+	for i in range(startevt, nevents):
+	    # read next event
+	    self.tree.LoadTree(i)
+	    nbytes += self.tree.GetEntry(i)
+	    # loop over list of ZooMCPs
+	    zoomcps = zooev.GetMCP()
+	    acceptevt = False
+	    for j in range(0, zoomcps.size()):
+		zmcp = zoomcps[j]
+		# work out if the candidate satisfies the cuts
+		accept = True
+                for cut in cuts:
+                    if not cut(zmcp, zooev):
+                        accept = False
+                        break
+                if not accept:
+                    continue
+		# MC particle accepted
+		acceptevt = True
+		if None != mcpaction:
+		    mcpaction(zmcp, zooev)
+	    if (i - startevt) % 1024 == 0:
+		print ("\r% 9d/% 9d events read (%9.3f GBytes)" % \
+			tuple([ (i - startevt), nevents, nbytes / 1024. / 1024. / 1024. ]) ),
+		sys.stdout.flush()
+	    if acceptevt and None != evtaction:
+		evtaction(zooev)
+	print ("\r% 9d/% 9d events read (%9.3f GBytes)" % \
+		tuple([ nevents, nevents, nbytes / 1024. / 1024. / 1024. ]) )
+        # free branches
+        self.tree.SetBranchAddress("Event", ROOT.MakeNullPointer())
+	return nevents
 
     def DrawZooP(self, collection, title, variables = [], cuts = [],
 	    weight = lambda zp, zev: 1.0, maxevts = -1, startevent = 0,
@@ -106,41 +274,15 @@ class ZooTuplePlayer:
 		    ROOT.TH3D('DrawZooP_' + collection, title, 100, 0, 0, 100, 0, 0, 100, 0, 0)
 	# append weight to variable array
 	variables.append(weight)
-	# initialise branches
-	zoops = ROOT.ZooObjRefArray(ROOT.ZooP)()
-	zooev = ROOT.ZooEv()
-	self.tree.SetBranchAddress(collection, ROOT.AddressOf(zoops))
-	self.tree.SetBranchAddress("Event", ROOT.AddressOf(zooev))
-	# prepare to loop over events in Zoo tuple
-	nevents = self.tree.GetEntries()
-	if startevent >= nevents:
-	    print 'DrawZooP: no events left!'
+	# use the runOverZooP method to do all the tedious cut evaluation work
+	# for us, passing a per-candidate action which fills the histogram
+	# with the required variables for an accepted candidate
+	retVal = self.runOverZooP(collection, cuts, maxevts, startevent,
+		lambda zp, zev: self.histo.Fill(*[ f(zp, zev) for f in variables ]))
+	if None == retVal:
 	    return None
-	if maxevts > 0 and startevent + maxevts < nevents:
-	    nevents = startevent + maxevts
-	self.tree.SetCacheLearnEntries(1000)
-	for i in range(startevent, nevents):
-	    # read next event
-	    self.tree.LoadTree(i)
-	    self.tree.GetEntry(i)
-	    # loop over candidates in collection
-	    for j in range(0, zoops.size()):
-		zp = zoops[j]
-		# work out if the candidate satisfies the cuts
-		accept = True
-		for cut in cuts:
-		    if not cut(zp, zooev):
-			accept = False
-			break
-		if not accept:
-		    continue
-		# yes, fill histogram
-		self.histo.Fill(*[ f(zp, zooev) for f in variables ])
-	# draw histogram
+	## initialise branche	# draw histogram
 	self.histo.Draw(drawopts)
-	# free branches
-	self.tree.SetBranchAddress(collection, ROOT.MakeNullPointer())
-	self.tree.SetBranchAddress("Event", ROOT.MakeNullPointer())
 	# return histogram
 	return self.histo
 
@@ -215,39 +357,15 @@ class ZooTuplePlayer:
 		    ROOT.TH3D('DrawZooMCP', title, 100, 0, 0, 100, 0, 0, 100, 0, 0)
 	# append weight to variable array
 	variables.append(weight)
-	# initialise branches
-	zooev = ROOT.ZooEv()
-	self.tree.SetBranchAddress("Event", ROOT.AddressOf(zooev))
-	# prepare to loop over events in Zoo tuple
-	nevents = self.tree.GetEntries()
-	if startevent >= nevents:
-	    print 'DrawZooP: no events left!'
+	# use the runOverZooMCP method to do all the tedious cut evaluation
+	# work for us, passing a per-MC particle action which fills the
+	# histogram with the required variables for an accepted MC particle
+	retVal = self.runOverZooMCP(cuts, maxevts, startevent,
+		lambda zp, zev: self.histo.Fill(*[ f(zp, zev) for f in variables ]))
+	if None == retVal:
 	    return None
-	if maxevts > 0 and startevent + maxevts < nevents:
-	    nevents = startevent + maxevts
-	self.tree.SetCacheLearnEntries(1000)
-	for i in range(startevent, nevents):
-	    # read next event
-	    self.tree.LoadTree(i)
-	    self.tree.GetEntry(i)
-	    # loop over list of ZooMCPs
-	    zoomcps = zooev.GetMCP()
-	    for j in range(0, zoomcps.size()):
-		zmcp = zoomcps[j]
-		# work out if the candidate satisfies the cuts
-		accept = True
-                for cut in cuts:
-                    if not cut(zmcp, zooev):
-                        accept = False
-                        break
-                if not accept:
-                    continue
-                # yes, fill histogram
-                self.histo.Fill(*[ f(zmcp, zooev) for f in variables ])
         # draw histogram
         self.histo.Draw(drawopts)
-        # free branches
-        self.tree.SetBranchAddress("Event", ROOT.MakeNullPointer())
         # return histogram
         return self.histo
 
