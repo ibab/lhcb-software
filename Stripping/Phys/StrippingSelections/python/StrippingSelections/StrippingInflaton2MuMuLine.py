ebc4a67952f63aa9f1f94d3f339388f485a3a1a5
Michel De Cian <michel.de.cian@cern.ch>
2012-01-19 10:25:10 +0000

    Added 3 new lines for (light) inflaton search in StrippingInflaton2MuMuLine
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@133887 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 20e63a514313071019d0dccf91a14bf511500883

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingInflaton2MuMuLine.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingInflaton2MuMuLine.py
@@ -40,7 +40,13 @@ class StrippingInflaton2MuMuConf(LineBuilder) :
     __configuration_keys__ = ('Inflaton2MuMuLongPrescale',
                               'Inflaton2MuMuDownstreamPrescale',
                               'Inflaton2MuMuLongPostscale',
-                              'Inflaton2MuMuDownstreamPostscale'
+                              'Inflaton2MuMuDownstreamPostscale',
+                              'Bu2InflatonKPrescale', 
+                              'Bu2InflatonKPostscale', 
+                              'Bs2InflatonPhiPrescale', 
+                              'Bs2InflatonPhiPostscale', 
+                              'Bd2InflatonKstPrescale', 
+                              'Bd2InflatonKstPostscale'
                               )
     
     #### This is the dictionary of all tunable cuts ########
@@ -49,6 +55,12 @@ class StrippingInflaton2MuMuConf(LineBuilder) :
         'Inflaton2MuMuDownstreamPrescale'    : 1,
         'Inflaton2MuMuLongPostscale'    : 1,
         'Inflaton2MuMuDownstreamPostscale'    : 1,
+        'Bu2InflatonKPrescale' : 1, 
+        'Bu2InflatonKPostscale' : 1, 
+        'Bs2InflatonPhiPrescale' : 1, 
+        'Bs2InflatonPhiPostscale' : 1, 
+        'Bd2InflatonKstPrescale' : 1, 
+        'Bd2InflatonKstPostscale' : 1
         }                
     
     
@@ -62,27 +74,56 @@ class StrippingInflaton2MuMuConf(LineBuilder) :
 
         default_name=name
 
-        self.selLong = makeLong(default_name+"long")
-        self.selDownstream = makeDownstream(default_name+"downstream")
+        self.selLongLoose = makeLong(default_name+"_longLoose", 16, 5)
+        self.selLongTight = makeLong(default_name+"_longTight", 625, 90)
+        self.selDownstream = makeDownstream(default_name+"_downstream")
+        
+        self.selBu2InflatonK = makeBu2InflatonK(default_name+"_Bu2InflatonKBuilder", self.selLongLoose)
+        self.selBs2InflatonPhi = makeBs2InflatonPhi(default_name+"_Bs2InflatonPhiBuilder", self.selLongLoose)
+        self.selBd2InflatonKst = makeBd2InflatonKst(default_name+"_Bd2InflatonKstBuilder", self.selLongLoose)
+
+
 
         self.longLine = StrippingLine(default_name+"LongLine",
                                             prescale = config['Inflaton2MuMuLongPrescale'],
                                             postscale = config['Inflaton2MuMuLongPostscale'],
-                                            algos = [ self.selLong ]
+                                            algos = [ self.selLongTight ]
                                             )
 
         self.downstreamLine = StrippingLine(default_name+"DownstreamLine",
                                             prescale = config['Inflaton2MuMuDownstreamPrescale'],
                                             postscale = config['Inflaton2MuMuDownstreamPostscale'],
                                             algos = [ self.selDownstream ]
-                                      )
+                                            )
+
+        self.Bu2InflatonKLine = StrippingLine(default_name+"_Bu2InflatonK", 
+                                              prescale = config['Bu2InflatonKPrescale'],
+                                              postscale = config['Bu2InflatonKPostscale'],
+                                              algos = [ self.selBu2InflatonK ]
+                                              )
+
+        self.Bs2InflatonPhiLine = StrippingLine(default_name+"_Bs2InflatonPhi", 
+                                              prescale = config['Bs2InflatonPhiPrescale'],
+                                              postscale = config['Bs2InflatonPhiPostscale'],
+                                              algos = [ self.selBs2InflatonPhi ]
+                                              )
+        
+        self.Bd2InflatonKstLine = StrippingLine(default_name+"_Bd2InflatonKst", 
+                                              prescale = config['Bd2InflatonKstPrescale'],
+                                              postscale = config['Bd2InflatonKstPostscale'],
+                                              algos = [ self.selBd2InflatonKst ]
+                                              )
+
       
         self.registerLine( self.longLine )
+        self.registerLine( self.Bu2InflatonKLine )
+        self.registerLine( self.Bs2InflatonPhiLine )
+        self.registerLine( self.Bd2InflatonKstLine )
         #self.registerLine( self.downstreamLine )
         
 
 
-def makeLong(name) :
+def makeLong(name, vertexDistChi2, vertexDist) :
     """
     very very detached dimuon selection with long tracks
     """
@@ -103,8 +144,8 @@ def makeLong(name) :
     Detached2mu.MotherCut = "(VFASPF(VCHI2/VDOF)<10) "\
                             "& (M>250)"\
                             "& (BPVDIRA > 0) "\
-                            "& (BPVVDCHI2>625)"\
-                            "& (BPVVD>90)" 
+                            "& (BPVVDCHI2>  %(vertexDistChi2)s)"\
+                            "& (BPVVD> %(vertexDist)s)" % locals() 
              
 
            
@@ -150,3 +191,91 @@ def makeDownstream(name) :
     return Selection (name,
                       Algorithm = Detached2mu,
                       RequiredSelections = [ _stdLooseDownMuons ])
+
+
+
+def makeBu2InflatonK(name, SelInflatonLong) :
+    """
+    detached Bu -> Inflaton K selection.
+
+    Arguments:
+    name        : name of the Selection.
+    """
+
+    
+    from Configurables import OfflineVertexFitter
+   
+    Bu2InflatonK = CombineParticles("Combine"+name)
+    Bu2InflatonK.DecayDescriptor =  " [B+ -> KS0 K+]cc ";
+    Bu2InflatonK.addTool( OfflineVertexFitter() )
+    Bu2InflatonK.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+    Bu2InflatonK.OfflineVertexFitter.useResonanceVertex = False
+    Bu2InflatonK.ReFitPVs = True
+    Bu2InflatonK.DaughtersCuts = { "K+" : "(ISLONG) & (TRCHI2DOF < 5 ) &(MIPCHI2DV(PRIMARY)>25)& (PT>250*MeV) "}
+    Bu2InflatonK.CombinationCut = "(ADAMASS('B+') < 500*MeV)"
+    Bu2InflatonK.MotherCut = "(BPVIPCHI2()< 50)& (VFASPF(VCHI2)<100)& (D2DVVDDOT(1) > 0.3*mm)"
+
+    _kaons = DataOnDemand(Location='Phys/StdLooseKaons/Particles')
+
+    return Selection( "Sel"+name,
+                      Algorithm = Bu2InflatonK,
+                      RequiredSelections=[SelInflatonLong,_kaons] )
+
+
+def makeBs2InflatonPhi(name, SelInflatonLong) :
+    """
+    detached Bs -> Inflaton Phi selection. 
+
+    Arguments:
+    name        : name of the Selection.
+    """
+
+    
+    from Configurables import OfflineVertexFitter
+   
+    _phi = DataOnDemand(Location='Phys/StdLooseDetachedPhi2KK/Particles')
+
+    Bs2InflatonPhi = CombineParticles("Combine"+name)
+    Bs2InflatonPhi.DecayDescriptor = "B_s0 -> KS0 phi(1020)"
+    Bs2InflatonPhi.addTool( OfflineVertexFitter() )
+    Bs2InflatonPhi.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+    Bs2InflatonPhi.OfflineVertexFitter.useResonanceVertex = False
+    Bs2InflatonPhi.ReFitPVs = True
+    Bs2InflatonPhi.DaughtersCuts = {"phi(1020)" : "MIPCHI2DV(PRIMARY)> 25."}
+    Bs2InflatonPhi.CombinationCut = "(ADAMASS('B_s0') < 500*MeV)"
+    Bs2InflatonPhi.MotherCut = "(BPVIPCHI2()< 50)& (VFASPF(VCHI2)<100)& (D2DVVDDOT(1) > 0.3*mm)"
+    
+    return  Selection( "Sel"+name,
+                       Algorithm = Bs2InflatonPhi,
+                       RequiredSelections=[SelInflatonLong, _phi] )
+
+
+
+def makeBd2InflatonKst(name, SelInflatonLong) :
+    """
+    detached Bd -> Inflaton K* selection.
+
+    Arguments:
+    name        : name of the Selection.
+    """
+
+    
+    from Configurables import OfflineVertexFitter
+   
+    
+    _kstar = DataOnDemand(Location='Phys/StdLooseDetachedKst2Kpi/Particles')
+    
+    Bd2InflatonKst = CombineParticles("Combine"+name)
+    Bd2InflatonKst.DecayDescriptor = "[B0 -> KS0 K*(892)0]cc"
+    Bd2InflatonKst.addTool( OfflineVertexFitter() )
+    Bd2InflatonKst.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+    Bd2InflatonKst.OfflineVertexFitter.useResonanceVertex = False
+    Bd2InflatonKst.ReFitPVs = True
+    Bd2InflatonKst.DaughtersCuts = { "K*(892)0" : "MIPCHI2DV(PRIMARY)> 25."}
+    Bd2InflatonKst.CombinationCut = "(ADAMASS('B0') < 500*MeV)"
+    Bd2InflatonKst.MotherCut = "(BPVIPCHI2()< 50) & (VFASPF(VCHI2)<100)& (D2DVVDDOT(1) > 0.3*mm)"
+
+    return Selection( "Sel"+name,
+                      Algorithm = Bd2InflatonKst,
+                      RequiredSelections=[SelInflatonLong, _kstar] )
+
