489b48c7d73d865a7f6403e81e26dfcaf5f14513
Patrick Spradlin <patrick.spradlin@cern.ch>
2012-01-20 13:26:37 +0000

    Updated StrippingXicc.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@133940 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 69786388131d53510b767d1b8cd688056c9452b5

--- Stripping/Phys/StrippingSelections/tests/test_Xicc.py
+++ Stripping/Phys/StrippingSelections/tests/test_Xicc.py
@@ -6,8 +6,15 @@ config_params = {
      'LongTrackGEC' : 150
     ,'controlPrescaleLc' : 0.05
     ,'controlPrescaleXic' : 1.0
-    ,'signalPrescaleViaLc' : 1.0
-    ,'signalPrescaleViaXic' : 1.0
+    , 'controlPrescaleDp' : 0.001
+    , 'controlPrescaleD0' : 0.001
+    , 'controlPrescaleDsp' : 0.00
+    , 'signalPrescaleViaLc' : 1.0
+    , 'signalPrescaleViaXic' : 1.0
+    , 'signalPrescaleViaDp'  : 1.0
+    , 'signalPrescaleViaDsp' : 0.0
+    , 'signalPrescaleViaD0'  : 1.0
+    , 'Hlt2TisTosSpec' : { 'Hlt2CharmHad.*Decision%TOS' : 0, 'Hlt1TrackAllL0Decision%TOS' : 0, 'Hlt2Global%TIS' : 0 }
     }
 
 
