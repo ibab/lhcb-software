c9cfe8da87d5175f2696efed49f9fb36a527b853
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-01-23 12:12:59 +0000

    ! 2012-01-23 - Chris Jones  - Some improvements to TrackToDST   + Fully support stripping of Muon Tracks   + Strip Track::Velo tracks in the same way as Track::VeloR
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@133987 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bd732873907267edc594913224f3098f85f04b5

--- Rec/Tr/TrackUtils/src/TrackToDST.cpp
+++ Rec/Tr/TrackUtils/src/TrackToDST.cpp
@@ -13,42 +13,42 @@ using namespace boost::assign; // bring 'map_list_of()' into scope
 
 DECLARE_ALGORITHM_FACTORY( TrackToDST )
 
-TrackToDST::TrackToDST(const std::string& name,
-                       ISvcLocator* pSvcLocator):
-  GaudiAlgorithm(name, pSvcLocator)
+  TrackToDST::TrackToDST(const std::string& name,
+                         ISvcLocator* pSvcLocator):
+    GaudiAlgorithm(name, pSvcLocator)
 {
   // constructor
   this->declareProperty( "TracksInContainer",
                          m_inputLocation = TrackLocation::Default );
   this->declareProperty( "StoreAllStates", m_storeAllStates = false );
 
-  m_theMap = map_list_of("ClosestToBeam", State::ClosestToBeam) 
-                        ("FirstMeasurement", State::FirstMeasurement )
-                        ("EndVelo",State::EndVelo)
-                        ("AtTT",State::AtTT)
-                        ("AtT",State::AtT)
-                        ("BegRich1",State::BegRich1)
-                        ("EndRich1",State::EndRich1)
-                        ("BegRich2",State::BegRich2)
-                        ("EndRich2",State::EndRich2)
-                        ("Spd",State::Spd)
-                        ("Prs",State::Prs)
-                        ("BegECal", State::BegECal)
-                        ("ECalShowerMax", State::ECalShowerMax)
-                        ("EndECal",State::EndECal)
-                        ("BegHCal", State::BegHCal)
-                        ("MidHCal",State::MidHCal)
-                        ("EndHCal",State::EndHCal)
-                        ("Muon",State::Muon)
-                        ("V0Vertex",State::V0Vertex);
+  m_theMap = map_list_of("ClosestToBeam", State::ClosestToBeam)
+    ("FirstMeasurement", State::FirstMeasurement )
+    ("EndVelo",State::EndVelo)
+    ("AtTT",State::AtTT)
+    ("AtT",State::AtT)
+    ("BegRich1",State::BegRich1)
+    ("EndRich1",State::EndRich1)
+    ("BegRich2",State::BegRich2)
+    ("EndRich2",State::EndRich2)
+    ("Spd",State::Spd)
+    ("Prs",State::Prs)
+    ("BegECal", State::BegECal)
+    ("ECalShowerMax", State::ECalShowerMax)
+    ("EndECal",State::EndECal)
+    ("BegHCal", State::BegHCal)
+    ("MidHCal",State::MidHCal)
+    ("EndHCal",State::EndHCal)
+    ("Muon",State::Muon)
+    ("V0Vertex",State::V0Vertex);
 
 
   declareProperty("veloStates", m_veloStrings);
-  declareProperty("longStates", m_longStrings); 
+  declareProperty("longStates", m_longStrings);
   declareProperty("TTrackStates", m_tStrings);
   declareProperty("downstreamStates", m_downstreamStrings);
   declareProperty("upstreamStates", m_upstreamStrings);
-  declareProperty("muonState", m_muonStrings);
+  declareProperty("muonStates", m_muonStrings);
 
   m_veloStrings = list_of("ClosestToBeam");
   m_longStrings =  list_of("ClosestToBeam")("FirstMeasurement")("BegRich1")("BegRich2")("V0Vertex");
@@ -68,9 +68,9 @@ StatusCode TrackToDST::initialize()
 {
   // Initializes TsaInitialization at the begin of program execution.
 
-  StatusCode sc = GaudiAlgorithm::initialize();
+  const StatusCode sc = GaudiAlgorithm::initialize();
   if (sc.isFailure()){
-     return Error("Failed to initialize");
+    return Error("Failed to initialize",sc);
   }
 
   stringToLoc(m_veloStrings,m_veloStates);
@@ -80,24 +80,29 @@ StatusCode TrackToDST::initialize()
   stringToLoc(m_upstreamStrings,m_upstreamStates);
   stringToLoc(m_muonStrings,m_muonStates);
 
-  return StatusCode::SUCCESS;
+  return sc;
 }
 
-StatusCode TrackToDST::execute(){
-
+StatusCode TrackToDST::execute()
+{
   Tracks* inCont = get<Tracks>(m_inputLocation);
 
-  // loop 
-  for (Tracks::iterator iterT = inCont->begin(); 
-       iterT != inCont->end(); ++iterT){
- 
+  // loop
+  for ( Tracks::iterator iterT = inCont->begin();
+        iterT != inCont->end(); ++iterT )
+  {
+    
     // remove the necessary States on the Track
-    if (m_storeAllStates == false){
-      // done in an ugly way for now - will be easier with the new 
+    if ( !m_storeAllStates )
+    {
+      // done in an ugly way for now - will be easier with the new
       // jobOptions parser
-      unsigned int type = (*iterT)->type();
-      switch (type){
+      const LHCb::Track::Types type = (*iterT)->type();
+      switch (type)
+      {
       case Track::Velo:
+        cleanStates(*iterT, m_veloStates);
+        break;
       case Track::VeloR:
         cleanStates(*iterT, m_veloStates);
         break;
@@ -113,6 +118,9 @@ StatusCode TrackToDST::execute(){
       case Track::Ttrack:
         cleanStates(*iterT, m_tStates);
         break;
+      case Track::Muon:
+        cleanStates(*iterT, m_muonStates);
+        break;
       default:
         Warning( format("Unknown track type %i",type) , StatusCode::SUCCESS, 1 ).ignore();
         break;
@@ -120,51 +128,68 @@ StatusCode TrackToDST::execute(){
     } // if
     // set the appropriate flag!
     (*iterT) -> setPatRecStatus( Track::PatRecIDs );
-  
+
   } // iterT
-   
+
   return StatusCode::SUCCESS;
 }
 
-
-void TrackToDST::cleanStates(Track* aTrack, const SLocations& loc) const{
+void TrackToDST::cleanStates(Track* aTrack, const SLocations& loc) const
+{
+  if ( msgLevel(MSG::VERBOSE) )
+  {
+    verbose() << "Analysing Track key=" << aTrack->key()
+              << " type=" << aTrack->type()
+              << " : " << aTrack->states().size() << " States at : z =";
+    for ( std::vector<LHCb::State*>::const_iterator iS = aTrack->states().begin();
+          iS != aTrack->states().end(); ++iS )
+    {
+      if (*iS) verbose() << " (" << (*iS)->z() << " " << (*iS)->location() << ")";
+    }
+    verbose() << endmsg;
+  }
 
   std::vector<State*> tempCont;
-  SLocations::const_iterator iterL = loc.begin();
-  for (; iterL != loc.end(); ++iterL){
+  for ( SLocations::const_iterator iterL = loc.begin();
+        iterL != loc.end(); ++iterL )
+  {
     const State* state = aTrack->stateAt(*iterL);
-    if(state) 
+    if ( state )
+    {
       tempCont.push_back(state->clone());
-    else if( *iterL!= LHCb::State::V0Vertex) {
+    }
+    else if ( *iterL != LHCb::State::V0Vertex )
+    {
       Warning("Failed to find state - more info in DEBUG",StatusCode::SUCCESS,1).ignore();
-      if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) 
-        debug() << "Missing state at " << *iterL << " on track " << aTrack->key() 
+      if( UNLIKELY( msgLevel(MSG::DEBUG) ) )
+      {
+        debug() << "Missing state at " << *iterL << " on track " << aTrack->key()
                 << " of type " << aTrack->type() << endmsg;
+      }
     }
   } // loca
 
   aTrack->clearStates();
-
-  std::vector<State*>::iterator iterS = tempCont.begin(); 
-  for (; iterS != tempCont.end(); ++iterS){ 
+  for ( std::vector<State*>::iterator iterS = tempCont.begin();
+        iterS != tempCont.end(); ++iterS )
+  {
     aTrack->addToStates(**iterS);
     delete *iterS;
   }
-} 
+}
 
-void TrackToDST::stringToLoc(const Strings& sCont, SLocations& loc) const{
-
+void TrackToDST::stringToLoc(const Strings& sCont, SLocations& loc) const
+{
   loc.reserve(sCont.size());
 
   Strings::const_iterator iterS = sCont.begin();
   for (; iterS != sCont.end(); ++iterS){
-    StateMap::const_iterator mIter = m_theMap.find(*iterS); 
+    StateMap::const_iterator mIter = m_theMap.find(*iterS);
     if (mIter != m_theMap.end()){
       loc.push_back(mIter->second);
     }
     else {
       error() << "attempt to convert unknown state" << endmsg;
     }
-  } // iter   
-
+  } // iter
 }
