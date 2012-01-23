b0dd29f09bc49b17642643ce782816bfb0e536ce
sgregson <sgregson@cern.ch>
2012-01-23 17:43:11 +0000

    Updated cuts in Hlt2 shared Ks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@134009 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f851d7f064ee677c2389b098be19f7ca9e32c311

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Ks.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Ks.py
@@ -17,9 +17,9 @@ __all__ = ( 'KsLLTF', 'KsDD' )
 # The LL K shorts, track fitted
 Hlt2SharedKsLLTF = Hlt2Member( CombineParticles, "KsLLTF"
                            , DecayDescriptor = "KS0 -> pi+ pi-" 
-                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV) & (MIPCHI2DV(PRIMARY)>25)" } 
-                           , CombinationCut = "(ADAMASS('KS0')<50*MeV) & (APT>700*MeV) & (ADOCACHI2CUT(30, ''))"
-                           , MotherCut = "(ADMASS('KS0')<35*MeV) & (PT>700*MeV) & (VFASPF(VCHI2PDOF)<30)"
+                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV) & (MIPCHI2DV(PRIMARY)>36)" } 
+                           , CombinationCut = "(ADAMASS('KS0')<50*MeV) & (ADOCACHI2CUT(30, ''))"
+                           , MotherCut = "(ADMASS('KS0')<35*MeV) & (VFASPF(VCHI2PDOF)<30) & (BPVLTIME() > 2.0*ps) "
                            , Inputs = [ BiKalmanFittedPions ]
                            )
 
@@ -28,9 +28,9 @@ KsLLTF = bindMembers( "SharedKsLLTF", [BiKalmanFittedPions , Hlt2SharedKsLLTF ] 
 # Now the downstream K shorts, requires fitted tracks!
 Hlt2SharedKsDD = Hlt2Member( CombineParticles, "KsDD"
                            , DecayDescriptor = "KS0 -> pi+ pi-"
-                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV)" }
-                           , CombinationCut = "(ADAMASS('KS0')<80*MeV) & (APT>700*MeV) & (ADOCACHI2CUT(30, ''))"
-                           , MotherCut = "(ADMASS('KS0')<64*MeV) & (PT>700*MeV) & (VFASPF(VCHI2PDOF)<30)"
+                           , DaughtersCuts = { "pi+" : "(TRCHI2DOF<4) & (P>3000*MeV) & (PT>300*MeV) & (MIPCHI2DV(PRIMARY)>9)" }
+                           , CombinationCut = "(ADAMASS('KS0')<80*MeV) & (ADOCACHI2CUT(30, ''))"
+                           , MotherCut = "(ADMASS('KS0')<64*MeV) & (VFASPF(VCHI2PDOF)<30)  & (BPVVDZ > 400)"
                            , Inputs = [ BiKalmanFittedDownPions ]
                            )
 
