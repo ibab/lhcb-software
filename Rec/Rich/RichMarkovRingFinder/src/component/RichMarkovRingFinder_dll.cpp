14212f8bb0d113f07f23a7f85b00de337851b4b3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-01-28 22:47:45 +0000

    remove obsolete _dll.cpp files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@134312 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b752d669612a2b12a19cc8adde58fa607332527c

--- Rec/Rich/RichMarkovRingFinder/src/component/RichMarkovRingFinder_dll.cpp
+++
@@ -1,7 +0,0 @@
-// $Id: RichMarkovRingFinder_dll.cpp,v 1.2 2004-06-01 12:14:02 jonesc Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-LOAD_FACTORY_ENTRIES( RichMarkovRingFinder )
-
