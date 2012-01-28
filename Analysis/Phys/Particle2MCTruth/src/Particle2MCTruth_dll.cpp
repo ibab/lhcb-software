7697bde529c259cb5ebe26df3a62d2b3e684decd
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-01-28 22:45:13 +0000

    remove obsolete _dll.cpp files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@134311 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1eaf30d50e73af1a9e58118c325ee9eeb4b564bf

--- Analysis/Phys/Particle2MCTruth/src/Particle2MCTruth_dll.cpp
+++
@@ -1,8 +0,0 @@
-// $Id: Particle2MCTruth_dll.cpp,v 1.1.1.1 2009-03-04 14:26:10 jpalac Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-
-LOAD_FACTORY_ENTRIES(Particle2MCTruth)
-
