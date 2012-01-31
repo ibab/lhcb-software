f23a7986fa4469f53993f5725062b35aa91ff37a
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2012-01-31 15:16:42 +0000

    modify PatMatchTool to have all three states in T for fits in HLT, some code cleanups
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@134488 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4daea985d6986167619cc824529ce245e424879

--- Rec/Tf/PatAlgorithms/src/PatMatchTool.cpp
+++ Rec/Tf/PatAlgorithms/src/PatMatchTool.cpp
@@ -1,7 +1,18 @@
-// Include files 
+// Include files
+#include <limits>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+#include <boost/foreach.hpp>
 
 // from Gaudi
-#include "GaudiKernel/ToolFactory.h" 
+#include "GaudiKernel/ToolFactory.h"
+
+#include "Event/StateParameters.h"
+
+#include "TrackInterfaces/IAddTTClusterTool.h"
+#include "TrackInterfaces/ITrackMomentumEstimate.h"
 
 // local
 #include "PatMatchTool.h"
@@ -13,260 +24,264 @@
 //-----------------------------------------------------------------------------
 
 // Declaration of the Tool Factory
-DECLARE_TOOL_FACTORY( PatMatchTool )
-
+DECLARE_TOOL_FACTORY(PatMatchTool)
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-PatMatchTool::PatMatchTool( const std::string& type,
-                            const std::string& name,
-                            const IInterface* parent )
-  : GaudiTool ( type, name , parent )
+PatMatchTool::PatMatchTool(const std::string& type,
+	const std::string& name, const IInterface* parent) :
+    GaudiTool(type, name , parent),
+    m_fastMomentumTool(0), m_addTTClusterTool(0)
 {
-  declareInterface<IMatchTool>(this);
-  declareInterface<ITrackMatch>(this);
- 
-  declareProperty( "zMagnet"         , m_zMagnet       = 5200. * Gaudi::Units::mm   );
-  declareProperty( "zMagnetTx2"      , m_zMagnetTx2    =-1700. * Gaudi::Units::mm   );
-  declareProperty( "zMagnetDsl2"     , m_zMagnetDsl2   =  500. * Gaudi::Units::mm   );
-  declareProperty( "zMatchY"         , m_zMatchY       = 8420. * Gaudi::Units::mm   );
-  declareProperty( "dxTol"           , m_dxTol         =    8. * Gaudi::Units::mm   );
-  declareProperty( "dxTolSlope"      , m_dxTolSlope    =   80. * Gaudi::Units::mm   );
-  declareProperty( "dyTol"           , m_dyTol         =    6. * Gaudi::Units::mm   );
-  declareProperty( "dyTolSlope"      , m_dyTolSlope    =  300. * Gaudi::Units::mm   );
-  declareProperty( "MagnetBend"      , m_magnetBend    =-1000. * Gaudi::Units::mm   );
-  declareProperty( "maxMatchChi2"    , m_maxChi2       = 4                          );
-  declareProperty( "FastMomentumToolName",	m_fastMomentumToolName	= "FastMomentumEstimate" );
-  declareProperty( "AddTTClusters"   , m_addTT            = true                    );
-  declareProperty( "writeNNVariables", m_writeNNVariables = true                    );
+    declareInterface<IMatchTool>(this);
+    declareInterface<ITrackMatch>(this);
+
+    declareProperty("zMagnet"         , m_zMagnet       = 5200. * Gaudi::Units::mm);
+    declareProperty("zMagnetTx2"      , m_zMagnetTx2    =-1700. * Gaudi::Units::mm);
+    declareProperty("zMagnetDsl2"     , m_zMagnetDsl2   =  500. * Gaudi::Units::mm);
+    declareProperty("zMatchY"         , m_zMatchY       = 8420. * Gaudi::Units::mm);
+    declareProperty("dxTol"           , m_dxTol         =    8. * Gaudi::Units::mm);
+    declareProperty("dxTolSlope"      , m_dxTolSlope    =   80. * Gaudi::Units::mm);
+    declareProperty("dyTol"           , m_dyTol         =    6. * Gaudi::Units::mm);
+    declareProperty("dyTolSlope"      , m_dyTolSlope    =  300. * Gaudi::Units::mm);
+    declareProperty("MagnetBend"      , m_magnetBend    =-1000. * Gaudi::Units::mm);
+    declareProperty("maxMatchChi2"    , m_maxChi2       = 4);
+    declareProperty("FastMomentumToolName",
+					m_fastMomentumToolName = "FastMomentumEstimate");
+    declareProperty("AddTTClusters"   , m_addTT            = true);
+    declareProperty("writeNNVariables", m_writeNNVariables = true);
 
 }
 //=============================================================================
 // Destructor
 //=============================================================================
-PatMatchTool::~PatMatchTool() {} 
+PatMatchTool::~PatMatchTool() {}
 
 //=============================================================================
 
 //=============================================================================
 // Initialization
 //=============================================================================
-StatusCode PatMatchTool::initialize() {
-  StatusCode sc = GaudiTool::initialize();
-  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
+StatusCode PatMatchTool::initialize()
+{
+    StatusCode sc = GaudiTool::initialize();
+    if (sc.isFailure()) return sc;    // error printed already by GaudiAlgorithm
 
-  if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) debug() << "==> Initialize" << endmsg;
+    if (UNLIKELY(msgLevel(MSG::DEBUG)))
+	debug() << "==> Initialize" << endmsg;
 
-  m_addTTClusterTool = tool<IAddTTClusterTool>( "PatAddTTCoord" );
+    m_addTTClusterTool = tool<IAddTTClusterTool>("PatAddTTCoord");
 
-  m_fastMomentumTool = tool<ITrackMomentumEstimate>( m_fastMomentumToolName );
+    m_fastMomentumTool = tool<ITrackMomentumEstimate>(m_fastMomentumToolName);
 
-  return StatusCode::SUCCESS;
+    return StatusCode::SUCCESS;
 }
 
 StatusCode PatMatchTool::matchSingle(const LHCb::Track& velo,
-                                     const LHCb::Track& seed , 
-                                     LHCb::Track& output,
-                                     double& chi2 )
+	const LHCb::Track& seed , LHCb::Track& output, double& chi2)
 {
-  
-  //calculate quality of match between tracks
-  chi2 = getChi2Match(velo, seed );
-  
-  if ( m_maxChi2 > chi2 ) {
-    
-    //set states and flags of output track 
-    makeTrack(velo, seed, output, chi2 );
-    if(m_addTT) m_addTTClusterTool->addTTClusters( output );
-    return StatusCode::SUCCESS;
-  }
-  
-  return StatusCode::FAILURE;
-  
+    // calculate quality of match between tracks
+    chi2 = getChi2Match(velo, seed);
+
+    if (m_maxChi2 > chi2) {
+	// set states and flags of output track
+	makeTrack(velo, seed, output, chi2);
+	if (m_addTT) m_addTTClusterTool->addTTClusters(output);
+	return StatusCode::SUCCESS;
+    }
+
+    return StatusCode::FAILURE;
 }
-
-
 
 //=============================================================================
 // Main execution
 //=============================================================================
 StatusCode PatMatchTool::match(const LHCb::Tracks& velos,
-                               const LHCb::Tracks& seeds,
-                               LHCb::Tracks& matchs ) {
-  
-  std::vector<MatchCandidate> cand;
-
-  LHCb::Tracks::const_iterator itV, itS;
-
-  for ( itV = velos.begin(); velos.end() != itV; ++itV ) {
-    if ( (*itV)->checkFlag( LHCb::Track::Backward ) ) continue;
-    if ( (*itV)->checkFlag( LHCb::Track::Invalid  ) ) continue;
-   
-    for ( itS = seeds.begin(); seeds.end() != itS; ++itS ) {
-     
-      double dist = getChi2Match(*(*itV), *(*itS) );
-
-      if ( m_maxChi2 > dist ) {
-        MatchCandidate candidate( *itV, *itS, dist );
-        cand.push_back( candidate );
-      }
+	const LHCb::Tracks& seeds, LHCb::Tracks& matchs)
+{
+    // build a match-chi^2 sorted table of velo-seed matches
+    std::vector<MatchCandidate> cands;
+    cands.reserve(4 * std::max(velos.size(), seeds.size()));
+    BOOST_FOREACH(const LHCb::Track* vTr, velos) {
+	if (vTr->checkFlag(LHCb::Track::Backward)) continue;
+	if (vTr->checkFlag(LHCb::Track::Invalid)) continue;
+	BOOST_FOREACH(const LHCb::Track* sTr, seeds) {
+	    if (sTr->checkFlag(LHCb::Track::Invalid)) continue;
+	    const double dist = getChi2Match(*vTr, *sTr);
+	    if (m_maxChi2 > dist) {
+		cands.push_back(MatchCandidate(vTr, sTr, dist));
+	    }
+	}
     }
-  }
-  std::sort( cand.begin(), cand.end(), MatchCandidate::lowerByChi2() );
+    std::sort(cands.begin(), cands.end(), MatchCandidate::lowerByChi2());
 
-  // for each track's key, tag if used or not.
-  std::map<int,bool> veloUsed;
-  std::map<int,bool> seedUsed;
-  for ( itV = velos.begin(); velos.end() != itV; ++itV ) veloUsed[(*itV)->key()] = false;
-  for ( itS = seeds.begin(); seeds.end() != itS; ++itS ) seedUsed[(*itS)->key()] = false;
-  
-  for ( std::vector<MatchCandidate>::iterator itM = cand.begin(); cand.end() != itM; ++itM ) {
-    LHCb::Track* vTr = (*itM).vTr();
-    LHCb::Track* sTr = (*itM).sTr();
-    
-    if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) 
-      debug() << "Candidate Velo " << vTr->key() << " used " << veloUsed[ vTr->key()] 
-              << " Seed " << sTr->key() << " used " << seedUsed[ sTr->key()] 
-              << " dist " << (*itM).dist() << endmsg;
-    
-    if ( veloUsed[ vTr->key()] ) continue;
-    if ( seedUsed[ sTr->key()] ) continue;
-    
-    veloUsed[ vTr->key()] = true;
-    seedUsed[ sTr->key()] = true;
+    // for each track, tag if used or not.
+    std::map<const LHCb::Track*,bool> used;
+    // convert unused match candidates to tracks
+    BOOST_FOREACH(const MatchCandidate& cand, cands) {
+	const LHCb::Track* vTr = cand.vTr();
+	const LHCb::Track* sTr = cand.sTr();
 
-    LHCb::Track* match = new LHCb::Track();
-    
-    makeTrack(*vTr, *sTr, *match, (*itM).dist() );
+	if (UNLIKELY(msgLevel(MSG::DEBUG))) {
+	    debug() << "Candidate Velo " << vTr->key() << " used " << used[ vTr ]
+		<< " Seed " << sTr->key() << " used " << used[ sTr ]
+		<< " dist " << cand.dist() << endmsg;
+	}
 
-    if(m_addTT){
-      StatusCode sc = m_addTTClusterTool->addTTClusters( *match );
-      if ( sc.isFailure() ) 
-        Warning("adding TT clusters failed!",sc).ignore();
-    }
+	bool& vUsed = used[vTr];
+	if (vUsed) continue;
+	bool& sUsed = used[sTr];
+	if (sUsed) continue;
 
-    // added for NNTools -- check how many tracks have common hits
-    // This is always 1.0 as we require that velo and seed tracks are used only once.
-    if( m_writeNNVariables == true) match->addInfo(LHCb::Track::NCandCommonHits, 1.0);
+	vUsed = sUsed = true;
 
-    matchs.insert( match);
+	LHCb::Track* match = new LHCb::Track();
+	matchs.insert(match);
 
-  }//end loop match cands
+	makeTrack(*vTr, *sTr, *match, cand.dist());
 
-  return StatusCode::SUCCESS;
+	if (m_addTT) {
+	    StatusCode sc = m_addTTClusterTool->addTTClusters(*match);
+	    if (sc.isFailure())
+		Warning("adding TT clusters failed!",sc).ignore();
+	}
+
+	// added for NNTools -- check how many tracks have common hits
+	// This is always 1.0 as we require that velo and seed tracks are used only once.
+	if (m_writeNNVariables)
+	    match->addInfo(LHCb::Track::NCandCommonHits, 1.0);
+    }//end loop match cands
+
+    return StatusCode::SUCCESS;
 }
 
 //=============================================================================
 //  Finalize
 //=============================================================================
-StatusCode PatMatchTool::finalize() {
+StatusCode PatMatchTool::finalize()
+{
+    if (UNLIKELY(msgLevel(MSG::DEBUG))) debug() << "==> Finalize" << endmsg;
 
-  if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) debug() << "==> Finalize" << endmsg;
-  
-  return GaudiTool::finalize();
+    return GaudiTool::finalize();
 }
 
 //=============================================================================
 
 double PatMatchTool::getChi2Match(const LHCb::Track& velo,
-                                  const LHCb::Track& seed ) 
+	const LHCb::Track& seed) const
 {
-  
-  double chi2 = 1e6;
-  if ( velo.checkFlag( LHCb::Track::Invalid  ) ) return chi2;
-  if ( velo.checkFlag( LHCb::Track::Backward ) ) return chi2;
-  if ( seed.checkFlag( LHCb::Track::Invalid  ) ) return chi2;
-  
-  const LHCb::State& vState = velo.closestState( 0.);
-  const LHCb::State& sState = seed.closestState( m_zMatchY );
 
-  double dSlope = vState.tx() - sState.tx();
-  double zForX = m_zMagnet 
-    + m_zMagnetTx2 * vState.tx() * vState.tx()
-    + m_zMagnetDsl2 * dSlope * dSlope;
-  double dxTol2 = m_dxTol * m_dxTol;
-  double dxTolSlope2 = m_dxTolSlope * m_dxTolSlope;
-  
-  double xV = vState.x() + ( zForX     - vState.z() ) * vState.tx();
-  double yV = vState.y() + ( m_zMatchY - vState.z() ) * vState.ty();
-  yV += vState.ty() * dSlope * dSlope * m_magnetBend;
+    double chi2 = std::numeric_limits<double>::max();
+    if (velo.checkFlag(LHCb::Track::Invalid)) return chi2;
+    if (velo.checkFlag(LHCb::Track::Backward)) return chi2;
+    if (seed.checkFlag(LHCb::Track::Invalid)) return chi2;
 
-  double xS = sState.x() + ( zForX     - sState.z() ) * sState.tx();
-  double yS = sState.y() + ( m_zMatchY - sState.z() ) * sState.ty();
-  
-  double distX = xS - xV;
-  double distY = yS - yV;
-  double dslx  = vState.tx() - sState.tx();
-  double tolX = dxTol2 + dslx * dslx *  dxTolSlope2;
-  double teta2 = vState.tx() * vState.tx() + vState.ty() * vState.ty();
-  double tolY  = m_dyTol * m_dyTol + teta2 * m_dyTolSlope * m_dyTolSlope;  
+    const LHCb::State& vState = velo.closestState(0.);
+    const LHCb::State& sState = seed.closestState(m_zMatchY);
 
-  chi2 = distX * distX / tolX + distY * distY /tolY;
+    const double dSlope = vState.tx() - sState.tx();
+    const double zForX = m_zMagnet
+	+ m_zMagnetTx2 * vState.tx() * vState.tx()
+	+ m_zMagnetDsl2 * dSlope * dSlope;
+    const double dxTol2 = m_dxTol * m_dxTol;
+    const double dxTolSlope2 = m_dxTolSlope * m_dxTolSlope;
 
-  double dSlY = sState.ty() - vState.ty();
-  chi2 += dSlY * dSlY / sState.errTy2() / 16;
-  
-  return chi2;
+    const double xV = vState.x() + (zForX     - vState.z()) * vState.tx();
+    const double yV = vState.y() + (m_zMatchY - vState.z()) * vState.ty() +
+	vState.ty() * dSlope * dSlope * m_magnetBend;
+
+    const double xS = sState.x() + (zForX     - sState.z()) * sState.tx();
+    const double yS = sState.y() + (m_zMatchY - sState.z()) * sState.ty();
+
+    const double distX = xS - xV;
+    const double distY = yS - yV;
+    const double dslx  = vState.tx() - sState.tx();
+    const double tolX = dxTol2 + dslx * dslx *  dxTolSlope2;
+    const double teta2 = vState.tx() * vState.tx() + vState.ty() * vState.ty();
+    const double tolY  = m_dyTol * m_dyTol + teta2 * m_dyTolSlope * m_dyTolSlope;
+
+    chi2 = distX * distX / tolX + distY * distY /tolY;
+
+    const double dSlY = sState.ty() - vState.ty();
+    chi2 += dSlY * dSlY / sState.errTy2() / 16.;
+
+    return chi2;
 }
 
 //=============================================================================
-void PatMatchTool::makeTrack( const LHCb::Track& velo,
-                              const LHCb::Track& seed,
-                              LHCb::Track& output,
-                              double chi2              )
+void PatMatchTool::makeTrack(const LHCb::Track& velo,
+	const LHCb::Track& seed, LHCb::Track& output, double chi2) const
 {
-
-  
-  const LHCb::State& vState = velo.closestState( 0.);
-  const LHCb::State& sState =seed.closestState( m_zMatchY );
-  //== Store the track
-  output.addToAncestors( velo );                // set the velo as ancestor
-  output.addToAncestors( seed );                // set the seed as ancestor
-  output.addToStates( vState );   // copy the state of the Velo
-  output.addToStates( sState );   // copy the state of the seed
-
-  //get closest state at 0 and set q over p
-  LHCb::State& oState = output.closestState( 0.); 
-  
-  double qOverP, sigmaQOverP;
-  //FIXME true or false??  
-  StatusCode sc = m_fastMomentumTool->calculate(&vState,&sState, 
-                                                qOverP, sigmaQOverP, true);
-
-  if(sc.isFailure()) Warning("momentum determination failed!",sc).ignore();
-  
-  Gaudi::TrackSymMatrix& cov = oState.covariance();
-  cov(4,4) = sigmaQOverP * sigmaQOverP;
-  oState.setQOverP(qOverP);
-  
-  //== Adjust flags
-  output.setType(         LHCb::Track::Long  );
-  output.setHistory(      LHCb::Track::PatMatch );
-  output.setPatRecStatus( LHCb::Track::PatRecIDs   );
-  output.setLhcbIDs(  velo.lhcbIDs()      );   // copy those from the Seed
-  output.addInfo(LHCb::Track::MatchChi2, chi2);
-  
-
-  //== add new LHCbIDs
-  for ( std::vector<LHCb::LHCbID>::const_iterator itH = seed.lhcbIDs().begin(); 
-        seed.lhcbIDs().end() != itH; ++itH ){
-    output.addToLhcbIDs( *itH );
-  }      
-        
-  return;
+    //== add ancestors
+    output.addToAncestors(velo);
+    output.addToAncestors(seed);
+    //== Adjust flags
+    output.setType(LHCb::Track::Long);
+    output.setHistory(LHCb::Track::PatMatch);
+    output.setPatRecStatus(LHCb::Track::PatRecIDs);
+    output.addInfo(LHCb::Track::MatchChi2, chi2);
+    //== copy LHCbIDs
+    output.addSortedToLhcbIDs(velo.lhcbIDs());
+    output.addSortedToLhcbIDs(seed.lhcbIDs());
+    //== copy Velo and T states at the usual pattern reco positions
+    std::vector<LHCb::State*> newstates;
+    newstates.reserve(6);
+    if (velo.hasStateAt(LHCb::State::ClosestToBeam))
+	newstates.push_back(velo.stateAt(LHCb::State::ClosestToBeam)->clone());
+    if (velo.hasStateAt(LHCb::State::FirstMeasurement))
+	newstates.push_back(velo.stateAt(LHCb::State::FirstMeasurement)->clone());
+    if (velo.hasStateAt(LHCb::State::EndVelo))
+	newstates.push_back(velo.stateAt(LHCb::State::EndVelo)->clone());
+    newstates.push_back(seed.closestState(StateParameters::ZBegT).clone());
+    newstates.push_back(seed.closestState(StateParameters::ZMidT).clone());
+    // make sure we don't include same state twice
+    if (std::abs(newstates[newstates.size() - 2]->z() -
+		newstates.back()->z()) < 300.) {
+	delete newstates.back();
+	newstates.pop_back();
+    }
+    newstates.push_back(seed.closestState(StateParameters::ZEndT).clone());
+    // make sure we don't include same state twice
+    if (std::abs(newstates[newstates.size() - 2]->z() -
+		newstates.back()->z()) < 300.) {
+	delete newstates.back();
+	newstates.pop_back();
+    }
+    //== estimate q/p
+    double qOverP, sigmaQOverP;
+    bool const cubicFit = seed.checkHistory(LHCb::Track::PatSeeding);
+    const LHCb::State& vState = velo.closestState(0.);
+    const LHCb::State& sState =seed.closestState(m_zMatchY);
+    StatusCode sc = m_fastMomentumTool->calculate(&vState,&sState,
+	    qOverP, sigmaQOverP, cubicFit);
+    if (sc.isFailure()) {
+	Warning("momentum determination failed!",sc).ignore();
+	// assume the Velo/T station standalone reco do something reasonable
+    } else {
+	// adjust q/p and its uncertainty
+	sigmaQOverP = sigmaQOverP * sigmaQOverP;
+	BOOST_FOREACH(LHCb::State* st, newstates) {
+	    st->covariance()(4,4) = sigmaQOverP;
+	    st->setQOverP(qOverP);
+	}
+    }
+    //== add copied states to output track
+    output.addToStates(newstates);
 }
 
 //=============================================================================
-  StatusCode PatMatchTool::match( const LHCb::Track& veloTrack,
-                                  const LHCb::Track& tTrack,
-                                  LHCb::Track& matchedTrack,
-                                  double& quality, double& quality2 ) {
-
+StatusCode PatMatchTool::match(const LHCb::Track& veloTrack,
+	const LHCb::Track& tTrack,
+	LHCb::Track& matchedTrack,
+	double& quality, double& quality2)
+{
     quality2=0;
-    StatusCode sc = this->matchSingle( veloTrack, tTrack, matchedTrack, quality );
-    if(sc.isFailure()) 
-      if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) debug()<<"matching failed !"<<endmsg;
+    StatusCode sc = this->matchSingle(veloTrack, tTrack, matchedTrack, quality);
+    if (sc.isFailure())
+	if (UNLIKELY(msgLevel(MSG::DEBUG))) debug()<<"matching failed !"<<endmsg;
 
     return sc;
-  }
+}
+
+// vim: sw=4:tw=78:ft=cpp
--- Rec/Tf/PatAlgorithms/src/PatMatchTool.h
+++ Rec/Tf/PatAlgorithms/src/PatMatchTool.h
@@ -1,5 +1,5 @@
 // $Id: PatMatchTool.h,v 1.6 2009-02-09 14:33:10 albrecht Exp $
-#ifndef PATMATCHTOOL_H 
+#ifndef PATMATCHTOOL_H
 #define PATMATCHTOOL_H 1
 
 // Include files
@@ -10,124 +10,136 @@
 
 #include "Event/Track.h"
 #include "GaudiKernel/IRegistry.h"
-#include "TrackInterfaces/IAddTTClusterTool.h"
 
 #include "TrackInterfaces/IMatchTool.h"            // Interface
 #include "TrackInterfaces/ITrackMatch.h"            // Interface for Hlt
-#include "TrackInterfaces/ITrackMomentumEstimate.h"
 
 
+// forward decls
+class ITrackMomentumEstimate;
+class IAddTTClusterTool;
 
 /** @class PatMatchTool PatMatchTool.h
- *  
- *  Match Velo and Seed tracks 
  *
- *  Johannes Albrecht: reorganize match as tool
- *  2008-04-25
+ * Match Velo and Seed tracks
  *
+ * @author Manuel Schiller
+ * @date 2012-01-31
+ *	code cleanup, make sure all states are there for fit in HLT
  *
- *  @author Olivier Callot
- *  @date   2007-02-07 
+ * @author Johannes Albrecht
+ * @date 2008-04-25
+ * 	reorganize match as tool
+ *
+ * @author Olivier Callot
+ * @date   2007-02-07
+ *	initial implementation
  */
 
-class PatMatchTool : public GaudiTool, 
-                     virtual public ITrackMatch,
-                     virtual public IMatchTool{
-public: 
-  /// Standard constructor
-  PatMatchTool( const std::string& type, 
-                const std::string& name,
-                const IInterface* parent);
-  
-  virtual ~PatMatchTool( ); ///< Destructor
-
-  virtual StatusCode initialize();    ///< Algorithm initialization
-  virtual StatusCode finalize  ();    ///< Algorithm finalization
-  
-  virtual StatusCode match(const LHCb::Tracks& velos,
-                           const LHCb::Tracks& seeds,
-                           LHCb::Tracks& matchs );
-
-  virtual StatusCode matchSingle(const LHCb::Track& velo,
-                                 const LHCb::Track& seed, 
-                                 LHCb::Track& output ,
-                                 double& chi2 );
-
-  /// Function for matching in Hlt
-  StatusCode match(const LHCb::Track& veloTrack,
-                   const LHCb::Track& tTrack,
-                   LHCb::Track& matchedTrack,
-                   double& quality, double& quality2);
-  
-public:
-
-  /** @class MatchCandidate PatMatch.h
-   *
-   *  Match candidate for Tf::PatMatch algorithm
-   *
-   *  @author Olivier Callot
-   *  @date   2007-02-07 
-   */
-  class MatchCandidate {
-  public:
-    MatchCandidate( LHCb::Track* vTr, LHCb::Track* sTr, double dist ) {
-      m_vTr = vTr;
-      m_sTr = sTr;
-      m_dist = dist;
-    };
-
-    ~MatchCandidate() { };
-
-    LHCb::Track* vTr() { return m_vTr; }
-    LHCb::Track* sTr() { return m_sTr; }
-    double dist()      { return m_dist; }
-
-    class lowerByChi2 {
+class PatMatchTool : public GaudiTool,
+    virtual public ITrackMatch,
+    virtual public IMatchTool
+{
     public:
-      bool operator() ( MatchCandidate first, MatchCandidate second ) const {
-        return first.dist() < second.dist();
-      }
-    };
+	/// Standard constructor
+	PatMatchTool(const std::string& type, const std::string& name,
+		const IInterface* parent);
+	/// Destructor
+	virtual ~PatMatchTool();
+	/// Algorithm initialization
+	virtual StatusCode initialize();
+	/// Algorithm finalization
+	virtual StatusCode finalize();
 
-  private:
-    LHCb::Track* m_vTr;
-    LHCb::Track* m_sTr;
-    double m_dist;
-  };
+	/// create match tracks from containers of velo and seed tracks
+	virtual StatusCode match(const LHCb::Tracks& velos,
+		const LHCb::Tracks& seeds,
+		LHCb::Tracks& matchs);
 
+	/// create a match track from a velo and a seed track
+	virtual StatusCode matchSingle(const LHCb::Track& velo,
+		const LHCb::Track& seed,
+		LHCb::Track& output ,
+		double& chi2);
 
+	/// Function for matching in Hlt
+	StatusCode match(const LHCb::Track& veloTrack,
+		const LHCb::Track& tTrack,
+		LHCb::Track& matchedTrack,
+		double& quality, double& quality2);
 
-private:
+    public:
 
-  double getChi2Match(const LHCb::Track& velo,
-                      const LHCb::Track& seed );
-  
-  void makeTrack( const LHCb::Track& velo,
-                  const LHCb::Track& seed,
-                  LHCb::Track& output,
-                  double chi2                 );
-  
-  
-  // use tool for momentum parametrisation
-  std::string m_fastMomentumToolName;
-  bool m_addTT;
-  bool m_writeNNVariables;
-  
-  ITrackMomentumEstimate *m_fastMomentumTool;
-  
-  IAddTTClusterTool*   m_addTTClusterTool;
-  
-  double m_zMagnet;
-  double m_zMagnetTx2;
-  double m_zMagnetDsl2;
-  double m_zMatchY;
-  double m_dxTol;
-  double m_dxTolSlope;
-  double m_dyTol;
-  double m_dyTolSlope;
-  double m_magnetBend;
-  double m_momentumScale;
-  double m_maxChi2;
-  
+	/** @class MatchCandidate PatMatch.h
+	 *
+	 * Match candidate for Tf::PatMatch algorithm
+	 *
+	 * @author Manuel Schiller
+	 * @date 2012-01-31
+	 * 	code cleanups
+	 *
+	 * @author Olivier Callot
+	 * @date   2007-02-07
+	 * 	initial implementation
+	 */
+	class MatchCandidate
+	{
+	    public:
+		MatchCandidate(const LHCb::Track* vTr,
+			const LHCb::Track* sTr, double dist) :
+		    m_vTr(vTr), m_sTr(sTr), m_dist(dist)
+	        { }
+
+		~MatchCandidate() { }
+
+		const LHCb::Track* vTr() const { return m_vTr; }
+		const LHCb::Track* sTr() const { return m_sTr; }
+		double dist() const { return m_dist; }
+
+		struct lowerByChi2 {
+		    bool operator()(const MatchCandidate& first,
+			    const MatchCandidate& second) const
+		    { return first.dist() < second.dist(); }
+		};
+
+	    private:
+		const LHCb::Track* m_vTr;
+		const LHCb::Track* m_sTr;
+		double m_dist;
+	};
+
+    private:
+	/// calculate matching chi^2
+	double getChi2Match(const LHCb::Track& velo,
+		const LHCb::Track& seed) const;
+
+	/// merge velo and seed segment to output track
+	void makeTrack(const LHCb::Track& velo,
+		const LHCb::Track& seed, LHCb::Track& output,
+		double chi2) const;
+
+	// use tool for momentum parametrisation
+	std::string m_fastMomentumToolName;
+	bool m_addTT;
+	bool m_writeNNVariables;
+
+	ITrackMomentumEstimate* m_fastMomentumTool;
+
+	IAddTTClusterTool*   m_addTTClusterTool;
+
+	double m_zMagnet;
+	double m_zMagnetTx2;
+	double m_zMagnetDsl2;
+	double m_zMatchY;
+	double m_dxTol;
+	double m_dxTolSlope;
+	double m_dyTol;
+	double m_dyTolSlope;
+	double m_magnetBend;
+	double m_momentumScale;
+	double m_maxChi2;
 };
+
 #endif // PATMATCHTOOL_H
+
+// vim: sw=4:tw=78:ft=cpp
