ece9a8da24cabee78aedf0ebeba3f7ed34455ff5
J Michael Williams <mwill@mit.edu>
2012-02-02 23:27:14 +0000

    B2C update
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@134679 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 975ad158078fa96de49eaf8eda8e6d3e88797bf5

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBeauty2Charm.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBeauty2Charm.py
@@ -14,8 +14,10 @@ import re
 from Gaudi.Configuration import *
 from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
 from PhysSelPython.Wrappers import Selection, DataOnDemand, MergedSelection
+from PhysSelPython.Wrappers import EventSelection
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
+from Configurables import LoKi__VoidFilter
 #from StandardParticles import StdNoPIDsPions, StdNoPIDsKaons, StdNoPIDsProtons
 from StandardParticles import StdLooseResolvedPi0,StdLooseMergedPi0
 from StandardParticles import StdAllNoPIDsPions, StdAllNoPIDsKaons, \
@@ -107,7 +109,7 @@ config = {
     },
     "Prescales" : { # Prescales for individual lines
     'RUN_BY_DEFAULT' : False, # False = lines off by default
-    'RUN_RE'         : ['.*KS.*','.*Lb2LcD.*'],  
+    'RUN_RE'         : ['.*KS.*','.*Lb2LcD.*','.*DoubleTopo.*'],  
     # Defaults are defined in, eg, Beauty2Charm_B2DXBuilder.py.  Put the full
     # line name here to override. E.g. 'B2D0HD2HHBeauty2CharmTOSLine':0.5.
     'B02D0PiPiD2HHBeauty2CharmLine'      : 1.0,
@@ -190,7 +192,20 @@ class Beauty2CharmConf(LineBuilder):
         # Unbiased lines
         ltub = LTUnbiasedBuilder(d,config['B2X'])
         self._makeLines(ltub.lines,config)
-        
+
+        # Double Topo line
+        code = "ACCEPT('DoubleTopoTool/DT')"
+        sel = EventSelection('DoubleTopo',
+                             Algorithm=LoKi__VoidFilter('DoubleTopoFilter',Code=code))
+        # This doesn't work for some reason ... FIX ME!!!!
+        from Configurables import DoubleTopoTool as DT
+        dt = DT('DoubleTopo.DT')
+        dt.minAngle = 5/57.#config['B2CBBDT_MIN']
+        dt.minMass = 40000
+        hlt = "HLT_PASS_RE('Hlt2Topo.*Decision')"
+        sline = StrippingLine('DoubleTopoLine',1.0,selection=sel,HLT=hlt)
+        self.registerLine(sline)
+
     def _makeLine(self,protoLine,config):
         tag = 'B2CBBDTBeauty2CharmFilter'
         default = config['Prescales']['RUN_BY_DEFAULT']
