4e6ecf4ce743250579b8a18f74383692879fd83f
Markus Frank <frankb@cern.ch>
2012-02-09 20:23:42 +0000

    Move to proper placements
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@135077 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9431b8f093cb9837180a6591a2cbadb31601e6f6

--- Online/Online/GeoTest/src/DetDesc/detector/DiskTracker_geo.cpp
+++ Online/Online/GeoTest/src/DetDesc/detector/DiskTracker_geo.cpp
@@ -22,52 +22,53 @@ namespace DetDesc { namespace Geometry {
     string     det_name  = x_det.nameStr();
     string     det_type  = x_det.typeStr();
     bool       reflect   = x_det.reflect();
-    DetElement sdet      (lcdd,det_name,det_type,x_det.id());
+    DetElement sdet (lcdd,det_name,det_type,x_det.id());
     Volume     motherVol = lcdd.pickMotherVolume(sdet);
-    int n = 0;
+    int l_num = 0;
 
-    for(xml_coll_t i(x_det,_X(layer)); i; ++i, ++n)  {
+    for(xml_coll_t i(x_det,_X(layer)); i; ++i, ++l_num)  {
       xml_comp_t x_layer = i;
-      string l_name = det_name+_toString(n,"_layer%d");
+      string l_nam = det_name+_toString(l_num,"_layer%d");
       double  zmin = x_layer.inner_z();
       double  rmin = x_layer.inner_r();
       double  rmax = x_layer.outer_r();
       double  z    = zmin, layerWidth = 0.;
-      Tube    l_tub(lcdd,l_name,rmin,rmax,2.*z,2.*M_PI);
-      Volume  l_vol(lcdd,l_name+"_volume",l_tub,air);
-      int m = 0;
+      Tube    l_tub(lcdd,l_nam,rmin,rmax,2*z,2*M_PI);
+      Volume  l_vol(lcdd,l_nam+"_volume",l_tub,air);
+      int  s_num = 0;
 
       l_vol.setVisAttributes(lcdd,x_layer.visStr());
 
       for(xml_coll_t j(x_layer,_X(slice)); j; ++j)  {
-	double w = xml_comp_t(j).thickness();
-	layerWidth += w;
+	double thickness = xml_comp_t(j).thickness();
+	layerWidth += thickness;
       }
-      for(xml_coll_t j(x_layer,_X(slice)); j; ++j, ++m)  {
+      for(xml_coll_t j(x_layer,_X(slice)); j; ++j, ++s_num)  {
 	xml_comp_t x_slice = j;
-	double     w = x_slice.thickness();
+	double thick = x_slice.thickness();
 	Material mat = lcdd.material(x_slice.materialStr());
-	string s_name = l_name+_toString(m,"_slice%d");
-	Tube   s_tub(lcdd,s_name,rmin,rmax,2*M_PI);
-	Volume s_vol(lcdd,s_name+"_volume", s_tub, mat);
+	string s_nam = l_nam+_toString(s_num,"_slice%d");
+	Tube   s_tub(lcdd,s_nam,rmin,rmax,2*M_PI);
+	Volume s_vol(lcdd,s_nam+"_volume", s_tub, mat);
 
 	if ( x_slice.isSensitive() ) s_vol.setSensitiveDetector(sens);
 	s_vol.setAttributes(lcdd,x_slice.regionStr(),x_slice.limitsStr(),x_slice.visStr());
 
-	PlacedVolume spv = l_vol.placeVolume(s_vol,Position(0,0,z-zmin-layerWidth/2+w/2));
-	spv.addPhysVolID(_X(layer),n);
+	PlacedVolume spv = l_vol.placeVolume(s_vol,Position(0,0,z-zmin-layerWidth/2+thick/2));
+	spv.addPhysVolID(_X(layer),l_num);
+	spv.addPhysVolID(_X(slice),s_num);
       }
 
       PlacedVolume lpv = motherVol.placeVolume(l_vol,Position(0,0,zmin+layerWidth/2.));
       lpv.addPhysVolID(_X(system),sdet.id());
       lpv.addPhysVolID(_X(barrel),1);
-      DetElement layer(lcdd,l_name,det_type+"/Layer",n);
+      DetElement layer(lcdd,l_nam,det_type+"/Layer",l_num);
       sdet.add(layer.setPlacement(lpv));
       if ( reflect )  {
 	PlacedVolume lpvR = motherVol.placeVolume(l_vol,Position(0,0,-zmin-layerWidth/2),ReflectRot());
 	lpvR.addPhysVolID(_X(system),sdet.id());
 	lpvR.addPhysVolID(_X(barrel),2);
-	DetElement layerR(lcdd,l_name+"_reflect",det_type+"/Layer",n);
+	DetElement layerR(lcdd,l_nam+"_reflect",det_type+"/Layer",l_num);
 	sdet.add(layerR.setPlacement(lpvR));
       }
     }
