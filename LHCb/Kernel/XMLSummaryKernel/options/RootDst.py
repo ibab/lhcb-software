a03de571f42f4693ea0365baa83ec0dd90cd1ca3
Marco Cattaneo <marco.cattaneo@cern.ch>
2012-02-14 17:43:48 +0000

    use xrootd to access test file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@135338 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 655f752d7c5d50b40974895cbc5d7d79fd68c389

--- LHCb/Kernel/XMLSummaryKernel/options/RootDst.py
+++ LHCb/Kernel/XMLSummaryKernel/options/RootDst.py
@@ -1,7 +1,7 @@
 from Gaudi.Configuration import *
 from GaudiConf import IOHelper
 IOHelper().inputFiles( [
-  'PFN:rfio:/castor/cern.ch/user/c/cattanem/Brunel/v34r5/00001820_00000001.dst'
+  'PFN:root://castorlhcb.cern.ch//castor/cern.ch/user/c/cattanem/Brunel/v34r5/00001820_00000001.dst?svcClass=default'
 ], clear=True)
 
 IOHelper().outStream("RootDst.root","InputCopyStream")
