f9f9dcf73dfd3781fa0a8343630c31cf84e8afb0
Marcin Kucharczyk <marcin@cern.ch>
2012-02-15 13:56:14 +0000

    Rename Hlt1SinglePV into Hlt1HighPtJetsSinglePV
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@135385 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e63b0bab1de636248fb404940ccedb9f5f145b07

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1HighPtJetsSinglePVLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1HighPtJetsSinglePVLines.py
@@ -11,7 +11,7 @@ __version__ = "CVS Tag $Name: not supported by cvs2svn $, $Revision: 1.0 $"
 from Gaudi.Configuration import *
 from HltLine.HltLinesConfigurableUser import *
 
-class Hlt1SinglePVLinesConf(HltLinesConfigurableUser):
+class Hlt1HighPtJetsSinglePVLinesConf(HltLinesConfigurableUser):
     __slots__ = {
          'L0Channel'  : 'HighSumETJet'
         }
@@ -24,7 +24,7 @@ class Hlt1SinglePVLinesConf(HltLinesConfigurableUser):
         from Configurables import LoKi__VoidFilter as VoidFilter
         algCheckVtcs = VoidFilter('SinglePVCont', Code = "CONTAINS('SinglePVCont') == 1" )
         channel = self.getProp('L0Channel')
-        Hlt1Line('SinglePV',
+        Hlt1Line('HighPtJetsSinglePV',
                  prescale  = self.prescale,
                  postscale = self.postscale,
                  L0DU = "L0_CHANNEL('%s')" % channel,
