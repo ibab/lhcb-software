fc729cd14b6e4968967f698eaf349ff73b8f80b9
powell <powell@cern.ch>
2012-02-21 09:25:16 +0000

    RICH & MUON PID Lines added to LineConfigDictionaries_Calib.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@135601 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3d7228445c04a8041c56d36ac77da5eda4f5e53d

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/PreStripping18/LineConfigDictionaries_Calib.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/PreStripping18/LineConfigDictionaries_Calib.py
@@ -4,16 +4,101 @@ MuIDCalib = {
     'STREAMS'     : [ 'PID' ],
     'CONFIG'      : {
     'PromptPrescale'           : 0.0,
-    'DetachedPrescale'         : 0.0,
+    'DetachedPrescale'         : 1.0,
     'DetachedNoMIPPrescale'    : 1.0,
     'DetachedNoMIPHiPPrescale' : 1.0,
     'DetachedNoMIPKPrescale'   : 1.0,
-    'FromLambdacPrescale'      : 0.0,
-    'KFromLambdacPrescale'     : 0.0,
-    'PiFromLambdacPrescale'    : 0.0,
-    'PFromLambdacPrescale'     : 0.0,
-    'KISMUONFromLambdacPrescale' : 0.0,
-    'PiISMUONFromLambdacPrescale': 0.0,
-    'PISMUONFromLambdacPrescale' : 0.0 
+    'FromLambdacPrescale'      : 1.0,
+    'KFromLambdacPrescale'     : 1.0,
+    'PiFromLambdacPrescale'    : 1.0,
+    'PFromLambdacPrescale'     : 1.0,
+    'KISMUONFromLambdacPrescale' : 1.0,
+    'PiISMUONFromLambdacPrescale': 1.0,
+    'PISMUONFromLambdacPrescale' : 1.0 
     }
+    }
+
+NoPIDDstarWithD02RSKPi = {
+    'BUILDERTYPE' : 'NoPIDDstarWithD02RSKPiConf',
+    'STREAMS'     : [ 'PID' ],
+    'WGs'         : [ 'ALL' ],
+    'CONFIG'      : {
+    'DaugPt'             : 250 * MeV      ## GeV
+    , 'DaugP'            : 2.0 * GeV      ## GeV
+    , 'DaugIPChi2'       : 16             ## unitless
+    , 'DaugTrkChi2'      : 5              ## unitless
+    , 'D0MassWin'        : 75 * MeV       ## MeV
+    , 'D0Pt'             : 1.5 * GeV      ## GeV
+    , 'D0VtxChi2Ndof'    : 13             ## unitless
+    , 'D0FDChi2'         : 49             ## unitless
+    , 'D0BPVDira'        : 0.9999         ## unitless
+    , 'D0IPChi2'         : 30             ## unit
+    , 'SlowPiPt'         : 150 * MeV      ## MeV
+    , 'SlowPiTrkChi2'    : 5              ## unitless
+    , 'DstarPt'          : 2.2 * GeV      ## GeV
+    , 'DstarVtxChi2Ndof' : 13             ## unitless
+    , 'DeltaM_Min'       : 130 * MeV      ## MeV
+    , 'DeltaM_Max'       : 155 * MeV      ## MeV
+    ##
+    , 'DCS_WrongMass'    : 25 * MeV       ## MeV (3 sigma veto)
+    , 'KK_WrongMass'     : 25 * MeV       ## MeV (3 sigma veto)
+    , 'PiPi_WrongMass'   : 25 * MeV       ## MeV (3 sigma veto)
+    ##
+    , 'Prescale'         : 0.482          ## unitless
+    , 'Postscale'        : 1.00           ## unitless
+    ##
+    , 'Monitor'          : False           ## Activate the monitoring?
+    }
+    }
+
+V0ForPID = {
+    'BUILDERTYPE' : 'StrippingV0ForPIDConf',
+    'STREAMS'     : [ 'PID' ],
+    'WGs'         : [ 'ALL' ],
+    'CONFIG'      : {
+    'TrackChi2'            :  5       ,          ## Track Chi2/ndof quality 
+    'VertexChi2'           :  16      ,          ## Cut on Vertex chi2-quality
+    'DeltaMassK0S'         :  50 * MeV,          ## Mass-window (half)-width for K0S 
+    'DeltaMassLambda'      :  25 * MeV,          ## Mass-window (half)-width for Lambda 
+    'MaxZ'                 : 220 * cm ,          ## Maximal allowed vertex Z-position
+    'DaughtersIPChi2'      :  25      ,          ## minimal IP-chi2 for daughter particles  
+    'LTimeFitChi2'         :  49      ,          ## Chi2 from LifeTime Fitter
+    ##
+    'WrongMassK0S'         :   9 * MeV,          ## Mass-window (half)-width for ppi hypthesis
+    'WrongMassK0S_DD'      :  18 * MeV,          ## Mass-window (half)-width for ppi hypthesis (DD-case)
+    ##
+    'WrongMassLambda'      :  20 * MeV,          ## Mass-window (half)-width for pipi hypthesis
+    'WrongMassLambda_DD'   :  40 * MeV,          ## Mass-window (half)-width for pipi hypthesis (DD-case)
+    ##
+    'CTauK0S'              :   1 * mm ,          ## Cut on c*tau for K0S 
+    'CTauK0S_DD'           :  10 * mm ,          ## Cut on c*tau for K0S (DD-case)
+    ##
+    'CTauLambda0'          :   5 * mm ,          ## Cut on c*tau for Lambda0
+    'CTauLambda0_DD'       :  20 * mm ,          ## Cut on c*tau for Lambda0 (DD-case)
+    ##
+    'Proton_IsMUONCut'     :  "(INTREE( (ABSID=='p+') & ISMUON ) )" , 
+    ## 
+    # Technicalities:
+    ##
+    'Monitor'              :  False    ,          ## Activate the monitoring ?
+    ##
+    "HLT"                  : "HLT_PASS_RE('Hlt1MB.*Decision')" ,  ## HLT-cut
+    ##
+    'Preambulo'       : [
+    ## import c_light
+    "from GaudiKernel.PhysicalConstants import c_light" ,
+    ## define DD-category of K0S 
+    "DD =    CHILDCUT ( ISDOWN , 1 ) & CHILDCUT ( ISDOWN , 2 ) " ,
+    ## define LL-category of K0S 
+    "LL =    CHILDCUT ( ISLONG , 1 ) & CHILDCUT ( ISLONG , 2 ) "
+    ] ,
+    'KS0LL_Prescale'           : 0.024 ,
+    'KS0DD_Prescale'           : 0.026 , 
+    'LamLL_Prescale_LoP'       : 0.151 , 
+    'LamLL_Prescale_HiP'       : 1.000 ,
+    'LamDD_Prescale'           : 0.099 , 
+    'LamLLIsMUON_Prescale_LoP' : 1.000 ,
+    'LamLLIsMUON_Prescale_HiP' : 1.000 ,
+    'LamDDIsMUON_Prescale'     : 1.000   
+     }
     }
