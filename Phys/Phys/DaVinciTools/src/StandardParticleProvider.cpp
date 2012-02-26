6fea904695db8ae2326e19c43bc86586d1727c51
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-02-26 13:48:41 +0000

    ! 2012-02-26 - Chris Jones  - Add a new tool StandardParticleProvider that returns a pointer to the    'standard' Particle for a given ProtoParticle and PID hypothesis
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@135776 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 17a3e81cac5507bbf810a1c2a39c7a3951af6ebe

---
+++ Phys/Phys/DaVinciTools/src/StandardParticleProvider.cpp
@@ -0,0 +1,130 @@
+
+// local
+#include "StandardParticleProvider.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : StandardParticleProvider
+//
+// 2012-02-26 : Chris Jones
+//-----------------------------------------------------------------------------
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( StandardParticleProvider )
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+  StandardParticleProvider::StandardParticleProvider( const std::string& type,
+                                                      const std::string& name,
+                                                      const IInterface* parent )
+    : GaudiTool ( type, name , parent )
+{
+  declareInterface<IStandardParticleProvider>(this);
+  m_pidToTESMap = boost::assign::map_list_of
+    ( 11,   "Phys/StdAllNoPIDsElectrons/Particles" )
+    ( 13,   "Phys/StdAllNoPIDsMuons/Particles"     )
+    ( 211,  "Phys/StdAllNoPIDsPions/Particles"     )
+    ( 321,  "Phys/StdAllNoPIDsKaons/Particles"     )
+    ( 2212, "Phys/StdAllNoPIDsProtons/Particles"   );
+  declareProperty( "ParticleLocations", m_pidToTESMap );
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+StandardParticleProvider::~StandardParticleProvider() {}
+
+//=============================================================================
+
+StatusCode StandardParticleProvider::initialize()
+{
+  // Initialise base class
+  const StatusCode sc = GaudiTool::initialize();
+  if ( sc.isFailure() ) return sc;
+
+  // Setup incident services
+  incSvc()->addListener( this, IncidentType::BeginEvent );
+
+  return sc;
+}
+
+//=============================================================================
+
+// Method that handles various Gaudi "software events"
+void StandardParticleProvider::handle ( const Incident& )
+{
+  // only one incident type subscribed to, so no need to check type
+  m_pidToParts.clear();
+}
+
+//=============================================================================
+
+const LHCb::Particle * 
+StandardParticleProvider::particle( const LHCb::ProtoParticle * proto,
+                                    const LHCb::ParticleID& pid ) const
+{
+  const LHCb::Particle * part = NULL;
+  if ( !proto ) return part;
+
+  /// Find or build the map for the given PID type
+  const ProtoToPartMap * pptop = NULL;
+  PIDToPartsMap::const_iterator iP = m_pidToParts.find( pid.abspid() );
+  if ( UNLIKELY( iP == m_pidToParts.end() ) )
+  {
+    if ( fillParticleMap(pid.abspid()) )
+    {
+      pptop = &(m_pidToParts[pid.abspid()]);
+    } 
+  }
+  else
+  {
+    pptop = &(iP->second);
+  }
+
+  // Find the Particle in the Map
+  if ( pptop )
+  {
+    ProtoToPartMap::const_iterator iPPToP = pptop->find(proto);
+    if ( iPPToP != pptop->end() )
+    {
+      part = iPPToP->second;
+    }
+  }
+
+  return part;
+}
+
+//=============================================================================
+
+bool StandardParticleProvider::fillParticleMap( const unsigned int pid ) const
+{
+  bool OK = false;
+
+  // Find the Particles for this PID type
+  const LHCb::Particles * parts(NULL);
+  PIDToTESMap::const_iterator iPartLoc = m_pidToTESMap.find(pid);
+  if ( iPartLoc != m_pidToTESMap.end() &&
+       exist<LHCb::Particles>(iPartLoc->second) )
+  {
+    parts = get<LHCb::Particles>(iPartLoc->second);
+  }
+
+  // If found, fill the map
+  if ( parts )
+  {
+    ProtoToPartMap & pptop = m_pidToParts[pid];
+    for ( LHCb::Particles::const_iterator iP = parts->begin();
+          iP != parts->end(); ++iP )
+    {
+      if ( *iP && (*iP)->proto() )
+      {
+        pptop[(*iP)->proto()] = *iP;
+      }
+    }
+    OK = true;
+  }
+
+  return OK;
+}
+
+//=============================================================================
---
+++ Phys/Phys/DaVinciTools/src/StandardParticleProvider.h
@@ -0,0 +1,87 @@
+// $Id: $
+#ifndef STANDARDPARTICLEPROVIDER_H 
+#define STANDARDPARTICLEPROVIDER_H 1
+
+#include <map>
+
+// from Gaudi
+#include "GaudiKernel/ToolFactory.h"
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiKernel/IIncidentListener.h"
+#include "GaudiKernel/IIncidentSvc.h"
+#include "GaudiKernel/HashMap.h"
+
+// Interface
+#include "Kernel/IStandardParticleProvider.h"
+
+// Event model
+#include "Event/ProtoParticle.h"
+#include "Event/Particle.h"
+
+// BOOST
+#include <boost/assign/list_of.hpp>
+
+/** @class StandardParticleProvider StandardParticleProvider.h
+ *  
+ *  Tool that provides pointer to 'standard' Particles
+ *  for a given ProtoParticle and PID hypothesis
+ *  
+ *  @author Chris Jones
+ *  @date   2012-02-26
+ */
+class StandardParticleProvider : public GaudiTool, 
+                                 virtual public IStandardParticleProvider,
+                                 virtual public IIncidentListener
+{
+
+public: 
+
+  /// Standard constructor
+  StandardParticleProvider( const std::string& type, 
+                            const std::string& name,
+                            const IInterface* parent);
+
+  /// Destructor
+  virtual ~StandardParticleProvider( );
+
+  // Initialization of the tool after creation
+  StatusCode initialize();
+
+public:
+
+  /** Implement the handle method for the Incident service.
+   *  This is used to inform the tool of software incidents.
+   *
+   *  @param incident The incident identifier
+   */
+  void handle( const Incident& incident );
+  
+public:
+  
+  // Get a Particle for a given ProtoParticle and PID
+  virtual const LHCb::Particle * particle( const LHCb::ProtoParticle * proto,
+                                           const LHCb::ParticleID& pid ) const;
+  
+private:
+
+  typedef std::map<unsigned int,std::string> PIDToTESMap;
+
+  typedef std::map<const LHCb::ProtoParticle*,const LHCb::Particle*> ProtoToPartMap;
+
+  typedef std::map<int,ProtoToPartMap> PIDToPartsMap;
+
+private:
+
+  bool fillParticleMap( const unsigned int pid ) const;
+
+private:
+
+  /// Mapping between PID types and TES locations for the standard Particles
+  PIDToTESMap m_pidToTESMap;
+
+  /// Local map to store association between ProtoParticles and Particles of a certain PID
+  mutable PIDToPartsMap m_pidToParts;
+
+};
+
+#endif // STANDARDPARTICLEPROVIDER_H
