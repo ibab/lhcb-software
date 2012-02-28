572cfce829bba8fd27c7f252fe90fa8cd63a7dce
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-02-28 21:07:01 +0000

    update of ITTrackSelector needed for alignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@135996 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 84b3d1a77717decb33eb79d7d311a2aa1602888a

--- Rec/Tr/TrackTools/src/ITTrackSelector.cpp
+++ Rec/Tr/TrackTools/src/ITTrackSelector.cpp
@@ -6,9 +6,8 @@
  *  CVS Log :-
  *  $Id: ITTrackSelector.cpp,v 1.1 2010-04-15 09:26:43 wouter Exp $
  *
- *  @author M.Needham Matt.Needham@cern.ch
- *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
- *  @date   30/12/2005
+ *  @author Wouter Hulsbergen
+ *  @date   2010
  */
 //-----------------------------------------------------------------------------
 
@@ -44,14 +43,16 @@ public:
    */
   virtual bool accept ( const LHCb::Track & aTrack ) const;
 
+protected:
+  bool computeModuleOverlap( const LHCb::Track & aTrack ) const;
+
 private:
   size_t m_minHitsTop ;
   size_t m_minHitsBottom ;
   size_t m_minHitsA ;
   size_t m_minHitsC ;
-  bool   m_requireOverlap ;
-  bool   m_requireOTOverlap ;
-  bool   m_requireITOverlap ;
+  bool   m_requireBoxOverlap ;
+  bool   m_requireModuleOverlap ;
 };
 
 DECLARE_TOOL_FACTORY( ITTrackSelector )
@@ -67,9 +68,8 @@ ITTrackSelector::ITTrackSelector( const std::string& type,
   declareProperty( "MinHitsBottom" , m_minHitsBottom = 0 ) ;
   declareProperty( "MinHitsA",       m_minHitsA = 0 ) ;
   declareProperty( "MinHitsC" ,      m_minHitsC = 0 ) ;
-  declareProperty( "RequireOverlap" , m_requireOverlap = false ) ;
-  declareProperty( "RequireITOverlap" , m_requireITOverlap = false ) ;
-  declareProperty( "RequireOTOverlap" , m_requireOTOverlap = false ) ;
+  declareProperty( "RequireOverlap" , m_requireBoxOverlap = false ) ;
+  declareProperty( "RequireModuleOverlap" , m_requireModuleOverlap = false ) ;
 }
 
 ITTrackSelector::~ITTrackSelector() { }
@@ -79,7 +79,7 @@ bool ITTrackSelector::accept ( const LHCb::Track& aTrack ) const
   size_t numIT[4] = {0,0,0,0} ;
   size_t numOT[2] = {0,0} ;
   for(int i=0; i<4; ++i) numIT[i] = 0 ;
-  for( std::vector<LHCb::LHCbID>::const_iterator 
+  for( std::vector<LHCb::LHCbID>::const_iterator
 	 it = aTrack.lhcbIDs().begin() ;
        it != aTrack.lhcbIDs().end(); ++it ) 
     if( it->isIT() ) {
@@ -92,22 +92,47 @@ bool ITTrackSelector::accept ( const LHCb::Track& aTrack ) const
   size_t sumit = numIT[0] + numIT[1] + numIT[2] + numIT[3] ;
   size_t sumot = numOT[0] + numOT[1] ;
   size_t sum   = sumot + sumit ;
-  bool acceptitoverlap = !m_requireITOverlap ||
-    (sumit > numIT[0] && sumit > numIT[1] && sumit > numIT[2] && sumit > numIT[3] ) ;
-  bool acceptotoverlap = !m_requireOTOverlap ||
-    (sumot > numOT[0] && sumot > numOT[1]) ;
-  bool acceptoverlap = !m_requireOverlap ||
+  //bool acceptitoverlap = !m_requireITOverlap ||
+  //  (sumit > numIT[0] && sumit > numIT[1] && sumit > numIT[2] && sumit > numIT[3] ) ;
+  //bool acceptotoverlap = !m_requireOTOverlap ||
+  //  (sumot > numOT[0] && sumot > numOT[1]) ;
+  bool acceptBoxOverlap = 
     (sum > numOT[0] && sum > numOT[1] && 
      sum > numIT[0] && sum > numIT[1] && sum > numIT[2] && sum > numIT[3]) ;
+  bool acceptModuleOverlap = m_requireModuleOverlap ? computeModuleOverlap( aTrack) : false ;
   
   return 
-    acceptoverlap &&
-    acceptitoverlap &&
-    acceptotoverlap &&
     numIT[3] >= m_minHitsTop &&
     numIT[2] >= m_minHitsBottom &&
     numIT[1] >= m_minHitsA &&
-    numIT[0] >= m_minHitsC 
-    && TrackSelector::accept( aTrack );
+    numIT[0] >= m_minHitsC &&
+    ( (m_requireBoxOverlap && acceptBoxOverlap ) ||
+      (m_requireModuleOverlap && acceptModuleOverlap) ||
+      (!m_requireBoxOverlap && !m_requireModuleOverlap ) ) &&
+    TrackSelector::accept( aTrack );
 }
 
+bool ITTrackSelector::computeModuleOverlap ( const LHCb::Track& aTrack ) const
+{
+  // this routine checks if this track has hits in two neighbouring
+  // modules (to select module overlaps for alignment)
+
+  // this can be written a lot faster
+  std::vector< LHCb::STChannelID > ithits ;
+  for( std::vector<LHCb::LHCbID>::const_iterator it = aTrack.lhcbIDs().begin() ;
+       it != aTrack.lhcbIDs().end(); ++it ) 
+    if( it->isIT() ) ithits.push_back( it->stID() ) ;
+  
+  bool foundoverlap(false) ;
+  for( std::vector< LHCb::STChannelID >::const_iterator it = ithits.begin() ;
+       it != ithits.end() && !foundoverlap; ++it) {
+    unsigned int iuniquelayer = it->detRegion()-1 + 4*( it->layer() -1 + 4* (it->station()-1) ) ;
+    for( std::vector< LHCb::STChannelID >::const_iterator jt = ithits.begin() ;
+	 jt != it && !foundoverlap ; ++jt ) {
+      unsigned int juniquelayer = jt->detRegion()-1 + 4*( jt->layer() -1 + 4* (jt->station()-1) ) ;
+      foundoverlap = foundoverlap ||
+	( iuniquelayer == juniquelayer &&  it->sector() != jt->sector() ) ;
+    }
+  }
+  return foundoverlap ;
+}
