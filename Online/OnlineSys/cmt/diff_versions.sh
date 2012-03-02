89fe2ae08b0a1014c4fcc39a93c7a13871edb965
Markus Frank <frankb@cern.ch>
2012-03-02 10:17:24 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@136215 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 03eebd1486bb66d9d4ccde51ba07703ba8abba38

--- Online/OnlineSys/cmt/diff_versions.sh
+++ Online/OnlineSys/cmt/diff_versions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-diff -r --brief ONLINE_${1} ONLINE_${2} | grep -v ".svn" | grep -v "setup." | grep -v "cleanup." | grep -v ".history" | grep -v "x86_64-slc5-gcc43" | grep -v ".pyc" | grep -v ".ph"
+diff -r --brief ONLINE_${1} ONLINE_${2} | grep -v ".svn" | grep -v "setup." | grep -v "cleanup." | grep -v ".history" | grep -v "x86_64-slc5-gcc43" | grep -v ".pyc" | grep -v ".ph" | grep -v ".d differ"
+
