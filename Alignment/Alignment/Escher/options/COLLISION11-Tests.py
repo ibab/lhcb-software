f67beb358dbceab30875618a69d3cfeb62704737
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-03-06 22:25:19 +0000

    data file for qmtests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@136397 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 59aa870861e1a94598b756a125d33abfeed9c9c2

---
+++ Alignment/Alignment/Escher/options/COLLISION11-Tests.py
@@ -0,0 +1,21 @@
+# Example 2011 collisions options for Brunel
+
+# Syntax is:
+#   gaudirun.py COLLISION11-Beam3500GeV-VeloClosed-MagDown.py
+#
+from Gaudi.Configuration import FileCatalog, EventSelector
+from Configurables import Escher
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
+
+#-- Use latest 2011 database tags for real data
+Escher().DataType = "2011"
+
+# Events from run 87968 from fill 1647, on 24th March 2011
+EventSelector().Input = [
+  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/87968/087968_0000000001.raw?svcClass=lhcbdisk' SVC='LHCb::MDFSelector'"
+    ]
+EventSelector().Input = [
+  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11/97121/097121_0000000003.raw?svcClass=lhcbdisk' SVC='LHCb::MDFSelector'"
+    ]
