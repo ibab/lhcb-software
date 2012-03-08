f98005f43af93da16b0c0e4e771ccb96814ccecf
William James Barter <william.barter@cern.ch>
2012-03-08 16:07:46 +0000

    Updates from S. Bifani to reduce rate of StrippingWMu.py and StrippingSingleTrackTIS.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@136505 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6705aa8fba0496870370da58b7ee5418bce083b2

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingSingleTrackTIS.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingSingleTrackTIS.py
@@ -1,111 +1,129 @@
-#Stripping Lines for Study of Background to Electroweak Processes
-#Electroweak Group (Convener: Tara Shears)
-#Line designed by Simone Bifani and William Barter
-
-# Accepts events with at least one noPID track which is TIS wrt Hlt1 and Hlt2 lines
-# Low and high Pt lines implemented with different prescales
+# Stripping Lines for study of background to Electroweak processes
+# Electroweak Group (Convenor: S.Bifani, J.Anderson)
+#
+# Line designed by S.Bifani and W.Barter
+#
+# (2012-02-28) S.Bifani
+# StdAllNoPIDsMuons, pT>20GeV & TTHits & Hlt1xHlt2 TIS (10% PRESCALE)
+# StdAllNoPIDsMuons, pT>15GeV & TTHits & Hlt1xHlt2 TIS ( 1% PRESCALE)
 
 from Gaudi.Configuration import *
 from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
-from PhysSelPython.Wrappers import Selection, DataOnDemand
+from PhysSelPython.Wrappers import Selection
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
 from StandardParticles import StdAllNoPIDsMuons
 
-confdict_SingleTrackTIS = { 'LowPtPrescale'     : .001
-                            , 'LowPtPostscale'  : 1.0
-                            , 'HighPtPrescale'  : .1
-                            , 'HighPtPostscale' : 1.0
-                            , 'LowPtCut'  : 5.
-                            , 'HighPtCut' : 15.
+confdict_SingleTrackTIS = { 'SingleTrackTIS_Prescale'    : 0.10,
+                            'SingleTrackTISLow_Prescale' : 0.01,
+                            'SingleTrackTIS_Postscale'   : 1.00,
+                            'pT'    : 20.,
+                            'pTlow' : 15.
                             }
 
-name = "SingleTrackTIS"
+name = 'SingleTrackTIS'
 
-class SingleTrackTISConf(LineBuilder) :
+class SingleTrackTISConf( LineBuilder ) :
 
-    __configuration_keys__ = ( 'LowPtPrescale'
-                               , 'LowPtPostscale'
-                               , 'HighPtPrescale'
-                               , 'HighPtPostscale'
-                               , 'LowPtCut'
-                               , 'HighPtCut'
+    __configuration_keys__ = ( 'SingleTrackTIS_Prescale',
+                               'SingleTrackTISLow_Prescale',
+                               'SingleTrackTIS_Postscale',
+                               'pT',
+                               'pTlow'
                                )
     
-    def __init__(self, name, config) :
-        LineBuilder.__init__(self, name, config)
+    def __init__( self, name, config ) :
 
-        # Requiring at least one TT hits removes ghost-tracks
-        _LowPtCutTight  = "(PT>%(LowPtCut)s*GeV) & (HASTRACK & TRCUT(0<TrIDC('isTT')))"%config
-        _HighPtCutTight = "(PT>%(HighPtCut)s*GeV) & (HASTRACK & TRCUT(0<TrIDC('isTT')))"%config
-        
+        LineBuilder.__init__( self, name, config )
+
         self._myname = name
 
 
-        # Low Pt line
-        self.selLowPtNoPIDs = makeFilter( self._myname + "LowPtNoPIDs"
-                                          , StdAllNoPIDsMuons
-                                          , "from LoKiTracks.decorators import *"
-                                          , _LowPtCutTight
+        # Define the cuts
+
+        _cut    = "(PT>%(pT)s*GeV) & (HASTRACK & TRCUT(0<TrIDC('isTT')))"%config
+        _cutlow = "(PT>%(pTlow)s*GeV) & (HASTRACK & TRCUT(0<TrIDC('isTT')))"%config
+        
+
+        # StdAllNoPIDsMuons, pT>20GeV & TTHits & Hlt1xHlt2 TIS
+
+        self.sel_NoPIDs = makeFilter( self._myname + 'NoPIDs',
+                                      StdAllNoPIDsMuons,
+                                      "from LoKiTracks.decorators import *",
+                                      _cut
+                                      )
+
+        self.sel_Hlt1TIS = makeTISTOS( self._myname + 'Hlt1TIS',
+                                       self.sel_NoPIDs,
+                                       "Hlt1.*Decision%TIS"
+                                       )
+
+        self.sel_Hlt2TIS = makeTISTOS( self._myname + 'Hlt2TIS',
+                                       self.sel_Hlt1TIS,
+                                       "Hlt2.*Decision%TIS"
+                                       )
+
+        self.line_SingleTrackTIS = StrippingLine( self._myname + 'Line',
+                                                  prescale  = config[ 'SingleTrackTIS_Prescale' ],
+                                                  postscale = config[ 'SingleTrackTIS_Postscale' ],
+                                                  checkPV   = False,
+                                                  selection = self.sel_Hlt2TIS
+                                                  )
+
+	self.registerLine( self.line_SingleTrackTIS )
+
+
+        # StdAllNoPIDsMuons, pT>15GeV & TTHits & Hlt1xHlt2 TIS PRESCALED
+
+        self.sel_NoPIDsLow = makeFilter( self._myname + 'NoPIDsLow',
+                                         StdAllNoPIDsMuons,
+                                         "from LoKiTracks.decorators import *",
+                                         _cutlow
+                                         )
+
+        self.sel_Hlt1TISLow = makeTISTOS( self._myname + 'Hlt1TISLow',
+                                          self.sel_NoPIDsLow,
+                                          "Hlt1.*Decision%TIS"
                                           )
-        self.selLowPtHlt1TIS = makeTISTOS( self._myname + "LowPtHlt1TIS"
-                                           , self.selLowPtNoPIDs
-                                           , "Hlt1.*Decision%TIS"
-                                           )
-        self.selLowPtHlt2TIS = makeTISTOS( self._myname + "LowPtHlt2TIS"
-                                           , self.selLowPtHlt1TIS
-                                           , "Hlt2.*Decision%TIS"
-                                           )
-        self.lineSingleTrackTISLowPt = StrippingLine( self._myname + "LowPt"
-                                                      , prescale = config[ "LowPtPrescale" ]
-                                                      , postscale = config[ "LowPtPostscale" ]
-                                                      , checkPV = False
-                                                      , selection = self.selLowPtHlt2TIS
-                                                      )
-	self.registerLine(self.lineSingleTrackTISLowPt)	
 
+        self.sel_Hlt2TISLow = makeTISTOS( self._myname + 'Hlt2TISLow',
+                                          self.sel_Hlt1TISLow,
+                                          "Hlt2.*Decision%TIS"
+                                          )
 
-        # High Pt line
-        self.selHighPtNoPIDs = makeFilter( self._myname + "HighPtNoPIDs"
-                                           , StdAllNoPIDsMuons
-                                           , "from LoKiTracks.decorators import *"
-                                           , _HighPtCutTight
-                                           )
-        self.selHighPtHlt1TIS = makeTISTOS( self._myname + "HighPtHlt1TIS"
-                                            , self.selHighPtNoPIDs
-                                            , "Hlt1.*Decision%TIS"
-                                            )
-        self.selHighPtHlt2TIS = makeTISTOS( self._myname + "HighPtHlt2TIS"
-                                            , self.selHighPtHlt1TIS
-                                            , "Hlt2.*Decision%TIS"
-                                            )
-        self.lineSingleTrackTISHighPt = StrippingLine( self._myname + "HighPt"
-                                                       , prescale = config[ "HighPtPrescale" ]
-                                                       , postscale = config[ "HighPtPostscale" ]
-                                                       , checkPV = False
-                                                       , selection = self.selHighPtHlt2TIS
-                                                       )
-	self.registerLine(self.lineSingleTrackTISHighPt)
+        self.line_SingleTrackTISLow = StrippingLine( self._myname + 'LowLine',
+                                                     prescale  = config[ 'SingleTrackTISLow_Prescale' ],
+                                                     postscale = config[ 'SingleTrackTIS_Postscale' ],
+                                                     checkPV   = False,
+                                                     selection = self.sel_Hlt2TISLow
+                                                     )
+
+	self.registerLine( self.line_SingleTrackTISLow )
 
 
 def makeFilter( name, _input, _preambulo, _code ) :
-    _filter = FilterDesktop( Preambulo = [ _preambulo ]
-                             , Code = _code
+
+    _filter = FilterDesktop( Preambulo = [ _preambulo ],
+                             Code      = _code
                              )
-    return Selection ( name
-                       , Algorithm = _filter
-                       , RequiredSelections = [ _input ]
+
+    return Selection ( name,
+                       Algorithm          = _filter,
+                       RequiredSelections = [ _input ]
                        )
 
+
 def makeTISTOS( name, _input, _trigger ) :
+
     from Configurables import TisTosParticleTagger
-    _tisTosFilter = TisTosParticleTagger( name + "Tagger" )
+    _tisTosFilter = TisTosParticleTagger( name + 'Tagger' )
     _tisTosFilter.TisTosSpecs = { _trigger : 0 }
     #_tisTosFilter.ProjectTracksToCalo = False
     #_tisTosFilter.CaloClustForCharged = False
     #_tisTosFilter.CaloClustForNeutral = False
     #_tisTosFilter.TOSFrac = { 4:0.0, 5:0.0 }
-    return Selection( name
-                      , Algorithm = _tisTosFilter
-                      , RequiredSelections = [ _input ]
+
+    return Selection( name,
+                      Algorithm          = _tisTosFilter,
+                      RequiredSelections = [ _input ]
                       )
--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingWMu.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingWMu.py
@@ -1,176 +1,117 @@
-#Stripping Lines for W->MuNu and Studies of their Background.
-#Electroweak Group (Convenor: Tara Shears)
-#Adaptation of lines (to use line builders) originally written by James Keaveney by Will Barter
-
-# Lines needed:
-
-# W prescaled 0.1. normal cut. stdveryloosemuons
-# W prescaled 0.1. low pT window. stdloosemuons.
-# W prescaled 0.1. very tight cut. stdnopids muons.
-# W not prescaled. normal cut. stdloosemuons
-# W not prescaled. tight cut. stdveryloosemuons
-# no prescale, low pt cut, used to study background. Ensure absence of bias by only taking events that trigger min bias in HLT.
+# Stripping Lines for W->MuNu and studies of their background
+# Electroweak Group (Convenor: S.Bifani, J.Anderson)
+#
+# Adaptation of lines originally written by J.Keaveney and W.Barter
+#
+# (2012-02-28) S.Bifani
+# WMu signal:     StdAllLooseMuons,  pT>20GeV
+# WMu control:    StdAllLooseMuons,  pT>15GeV                    (10% PRESCALE)
+# WMu background: StdAllNoPIDsMuons, pT>5GeV  & Hlt1MBNoBias Dec (RATE LIMITED)
 
 from Gaudi.Configuration import *
-from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop
 from PhysSelPython.Wrappers import Selection
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
-from StandardParticles import StdVeryLooseMuons, StdAllNoPIDsMuons, StdAllLooseMuons
+from StandardParticles import StdAllLooseMuons, StdAllNoPIDsMuons
 
-confdict_WMu={
-    
-    'WMuLinePrescale_ps'    : .1
-    ,  'WMuLinePrescale_hps': .05
-    ,  'WMuLinePrescale'    : 1.0
-    ,  'WMuLineLoosePrescale'    : 1.0
-    ,  'WMuLineNoPIDsPrescale'    : 0.1
-    ,  'WMuLinePostscale'   : 1.0
-    ,  'mucuttight' : 20.
-    ,  'mucut' : 15.
-    ,  'mucut_hps' : 10. 
-    ,  'mucutlow' : 5.
-    ,  'trkpchi2' : 0.001
- }
+confdict_WMu = { 'WMu_Prescale'    : 1.0,
+                 'WMuLow_Prescale' : 0.1,
+                 'WMu_Postscale'   : 1.0,
+                 'pT'     : 20.,
+                 'pTlow'  : 15.,
+                 'pTvlow' :  5.
+                 }
 
-default_name = "W2Mu"
+default_name = 'WMu'
 
-class WMuConf(LineBuilder) :
+class WMuConf( LineBuilder ) :
 
-    __configuration_keys__ = ('mucuttight',
-                              'mucut',
-                              'mucut_hps',
-                              'WMuLinePrescale_hps',
-                              'WMuLineLoosePrescale',
-                              'WMuLineNoPIDsPrescale', 
-                              'mucutlow',
-                              'trkpchi2',
-                              'WMuLinePrescale_ps',
-                              'WMuLinePrescale',
-                              'WMuLinePostscale'                           
-                              )
-    
-    def __init__(self, name, config) :
-        LineBuilder.__init__(self, name, config)
+    __configuration_keys__ = ( 'WMu_Prescale',
+                               'WMuLow_Prescale',
+                               'WMu_Postscale',
+                               'pT',
+                               'pTlow',
+                               'pTvlow'
+                               )
+
+    def __init__( self, name, config ) :
+
+        LineBuilder.__init__( self, name, config )
 
         self._myname = name
-        
-        #Define the cuts
-        _mucut='(PT>%(mucut)s*GeV)'%config
-        _mucuttight = '(PT>%(mucuttight)s*GeV)'%config
-        _mucutverytight = '(PT>%(mucuttight)s*GeV)&(TRPCHI2>%(trkpchi2)s)'%config
-        _mucutlow='(PT>%(mucutlow)s*GeV)'%config
-        _mucuthps = '(PT>%(mucut_hps)s*GeV)'%config
-
-        # First Make the PrescaledLines
 
 
-        # StdLooseMuons, Between 10 GeV and 15GeV
-        # will prescale to keep rate low...
-        self.selWMu_hps = makeWMu(self._myname+'WMu_hps', 
-                                  _mucuthps)
-     
-        self.WMu_linehps = StrippingLine(self._myname+"LowPTLine",
-                                             prescale = config['WMuLinePrescale_hps'],
-                                             postscale = config['WMuLinePostscale'],
-                                             checkPV = False,
-                                             selection = self.selWMu_hps
-                                             )
-        self.registerLine(self.WMu_linehps)
+        # Define the cuts
 
-        # Prescaled Line, StdVeryLooseMuons, Cut at 15GeV
-        self.selWMu_looseps = makeWMuLoose(self._myname+'WMu_looseps', 
-                                           _mucut)
-     
-        self.WMu_linelooseps = StrippingLine(self._myname+"Line_looseps",
-                                             prescale = config['WMuLinePrescale_ps'],
-                                             postscale = config['WMuLinePostscale'],
-                                             checkPV = False,
-                                             selection = self.selWMu_looseps
-                                             )
-        self.registerLine(self.WMu_linelooseps)
-
-        # Prescaled Line, StdNoPIDsMuons, Cut at 20GeV. 
-        self.selWMu_NoPIDsps = makeWMuNoPIDs(self._myname+'WMu_NoPIDsps', 
-                                             _mucutverytight)
-     
-        self.WMu_lineNoPIDsps = StrippingLine(self._myname+"Line_NoPIDsps",
-                                              prescale = config['WMuLineNoPIDsPrescale'],
-                                              postscale = config['WMuLinePostscale'],
-                                              checkPV = False,
-                                              selection = self.selWMu_NoPIDsps
-                                              )
-        self.registerLine(self.WMu_lineNoPIDsps)
+        _pT     = "(PT>%(pT)s*GeV)"%config
+        #_pT     = "(PT>%(pT)s*GeV) & (HASTRACK & TRCUT(0<TrIDC('isTT')))"%config
+        _pTlow  = "(PT>%(pTlow)s*GeV)"%config
+        _pTvlow = "(PT>%(pTvlow)s*GeV)"%config
 
 
+        # WMu signal: StdAllLooseMuons, pT>20GeV
+
+        self.sel_WMu = makeFilter( self._myname + 'WMu',
+                                   StdAllLooseMuons,
+                                   "from LoKiTracks.decorators import *",
+                                   _pT
+                                   )
+
+        self.line_WMu = StrippingLine( self._myname + 'Line',
+                                       prescale  = config[ 'WMu_Prescale' ],
+                                       postscale = config[ 'WMu_Postscale' ],
+                                       checkPV   = False,
+                                       selection = self.sel_WMu
+                                       )
+
+        self.registerLine( self.line_WMu )
 
 
+        # WMu control: StdAllLooseMuons, pT>15GeV (10% PRESCALE)
 
-        # Now the tighter, non pre-scaled lines
-        # StdLooseMuons, Cut at 15GeV
-        self.selWMu = makeWMu(self._myname+'WMu', 
-                              _mucut)
-     
-        self.WMu_line = StrippingLine(self._myname+"Line",
-                                      prescale = config['WMuLinePrescale'],
-                                      postscale = config['WMuLinePostscale'],
-                                      checkPV = False,
-                                      selection = self.selWMu
+        self.sel_WMuLow = makeFilter( self._myname + 'WMuLow',
+                                      StdAllLooseMuons,
+                                      "from LoKiTracks.decorators import *",
+                                      _pTlow
                                       )
-        self.registerLine(self.WMu_line)
 
-        # StdVeryLooseMuons, Cut at 20GeV
-        self.selWMu_loose = makeWMuLoose(self._myname+'WMu_loose', 
-                              _mucuttight)
-     
-        self.WMu_lineloose = StrippingLine(self._myname+"Line_loose",
-                                           prescale = config['WMuLineLoosePrescale'],
-                                           postscale = config['WMuLinePostscale'],
-                                           checkPV = False,
-                                           selection = self.selWMu_loose
-                                           )
-        self.registerLine(self.WMu_lineloose)
+        self.line_WMuLow = StrippingLine( self._myname + 'LowLine',
+                                          prescale  = config[ 'WMuLow_Prescale' ],
+                                          postscale = config[ 'WMu_Postscale' ],
+                                          checkPV   = False,
+                                          selection = self.sel_WMuLow
+                                          )
+
+        self.registerLine( self.line_WMuLow )
 
 
-        #background line, require minbias triggers to have fired.
-        self.selNoPIDsPTCut = makeNoPIDs(self._myname+'NoPIDs', 
-                                         _mucutlow)  
-        self.line_SingleTrackNoBias = StrippingLine(self._myname+"SingleTrackNoBias",
-                                               prescale = config['WMuLinePrescale'],
-                                               postscale = config['WMuLinePostscale'],
-                                               checkPV = False,
-                                               HLT = "HLT_PASS('Hlt1MBNoBiasDecision')",
-                                               selection = self.selNoPIDsPTCut
-                                               )
-        self.registerLine(self.line_SingleTrackNoBias)
+        # WMu background: StdAllNoPIDsMuons, pT>5GeV & Hlt1MBNoBias Dec (RATE LIMITED)
 
-       
-def makeWMu(name, _muCut) :
-    _W = FilterDesktop(Code = _muCut)
-    _stdallloosemuons = StdAllLooseMuons
+        self.sel_SingleTrackNoBias = makeFilter( self._myname + 'SingleTrackNoBias',
+                                                 StdAllNoPIDsMuons,
+                                                 "from LoKiTracks.decorators import *",
+                                                 _pTvlow
+                                                 )  
+
+        self.line_SingleTrackNoBias = StrippingLine( self._myname + 'SingleTrackNoBiasLine',
+                                                     prescale  = config[ 'WMu_Prescale' ],
+                                                     postscale = config[ 'WMu_Postscale' ],
+                                                     checkPV   = False,
+                                                     HLT       = "HLT_PASS( 'Hlt1MBNoBiasDecision' )",
+                                                     selection = self.sel_SingleTrackNoBias
+                                                     )
+
+        self.registerLine( self.line_SingleTrackNoBias )
+
+
+def makeFilter( name, _input, _preambulo, _code ) :
+
+    _filter = FilterDesktop( Preambulo = [ _preambulo ],
+                             Code      = _code
+                             )
+
     return Selection ( name,
-                       Algorithm = _W,
-                       RequiredSelections = [_stdallloosemuons])
-
-
-def makeWMuLoose(name, _muCut) :
-    _WLoose = FilterDesktop(Code = _muCut)                            
-    _stdveryloosemuons = StdVeryLooseMuons
-    return Selection ( name,
-                       Algorithm = _WLoose,
-                       RequiredSelections = [_stdveryloosemuons])
-
-
-def makeWMuNoPIDs(name, _muCut) :
-    _WNoPIDs = FilterDesktop(Code = _muCut)
-    _AllNoPIDsmuons = StdAllNoPIDsMuons
-    return Selection ( name,
-                       Algorithm = _WNoPIDs,
-                       RequiredSelections = [_AllNoPIDsmuons])
-
-def makeNoPIDs(name, _muCut) :
-    _NoPIDsSelection = FilterDesktop(Code = _muCut)
-    _AllNoPIDsmuons = StdAllNoPIDsMuons
-    return Selection ( name,
-                       Algorithm = _NoPIDsSelection,
-                       RequiredSelections = [_AllNoPIDsmuons])
+                       Algorithm          = _filter,
+                       RequiredSelections = [ _input ]
+                       )
