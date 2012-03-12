90db8932d3bba9d3baa6d537f7ab47bca32b6449
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-03-12 22:01:40 +0000

    - some changes in names - added selection for favourite cocktail of tracks - add flag such that selections can be applied before fitting - added selection with sequence for reconstructing HLT tracks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@136713 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3a9d1d3f262074338605262cfda927b9c0942cf1

--- Alignment/Alignment/TAlignment/python/TAlignment/TrackSelections.py
+++ Alignment/Alignment/TAlignment/python/TAlignment/TrackSelections.py
@@ -1,35 +1,32 @@
 # Base class for track selections. Each selection holds a sequence and
 # an outputllistname
 
-class AlignTrackSelection :
+class TrackSelection :
     __slots__ = {
         "_name" : ""   # Name of this selection"
         }
     
-    def __init__( self, Name ) :
+    def __init__( self, Name ) : 
         self._name = Name
 
-    def name( self ):
-        return self._name 
-
-    def algorithm( self ):
-        return None
-
-    def location( self ):
-        return 'Rec/Track/' + self.name()
+    def name( self ): return self._name 
+    def algorithm( self ): return None
+    def location( self ): return 'Rec/Track/' + self.name()
 
 def BestTracks() :
-    return AlignTrackSelection("Best")
+    return TrackSelection("Best")
 
 # Base class for selectors that use a simple refiner
-class AlignTrackRefiner(  AlignTrackSelection ):
+class TrackRefiner(  TrackSelection ):
     __slots__ = {
         "_inputLocation" : "" # Location of input track list"
+        ,"_fitted" : True
         }
 
-    def __init__( self, Name , InputLocation = "Rec/Track/Best" ) :
-        AlignTrackSelection.__init__(self,Name = Name)
+    def __init__( self, Name , InputLocation = "Rec/Track/Best", Fitted=True ) :
+        TrackSelection.__init__(self,Name = Name)
         self._inputLocation = InputLocation
+        self._fitted = Fitted
         
     def algorithm( self ):
         from Configurables import TrackListRefiner
@@ -43,90 +40,178 @@ class AlignTrackRefiner(  AlignTrackSelection ):
         print 'Base classes must overload configureSelector'
        
 # Selection for good long tracks for alignment
-class GoodLongTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "GoodLongTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation)
+class GoodLongTracks( TrackRefiner ):
+    def __init__( self, Name = "GoodLongTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation,
+                                   Fitted = Fitted)
     def configureSelector( self, a ):
         print 'inGoodLongTracks.configureSelector'
         from Configurables import TrackSelector
         a.Selector = TrackSelector()
-        a.Selector.MaxChi2Cut = 5
-        a.Selector.MaxChi2PerDoFMatch = 5
-        a.Selector.MaxChi2PerDoFVelo = 5
-        a.Selector.MaxChi2PerDoFDownstream = 5
         a.Selector.MinPCut  =   5000
         a.Selector.MaxPCut  = 200000
-        a.Selector.MinPtCut = 2000
+        a.Selector.MinPtCut =    200
         a.Selector.TrackTypes = ["Long"]
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
+            a.Selector.MaxChi2PerDoFMatch = 5
+            a.Selector.MaxChi2PerDoFVelo = 5
+            a.Selector.MaxChi2PerDoFDownstream = 5
+
 
 # selection for box overlap tracks in IT
-class ITBoxOverlapTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "ITBoxOverlapTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name, InputLocation)
+class ITBoxOverlapTracks( TrackRefiner ):
+    def __init__( self, Name = "ITBoxOverlapTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name, InputLocation, Fitted)
     def configureSelector( self, a ):
         from Configurables import ITTrackSelector
         a.Selector = ITTrackSelector()
         a.Selector.RequireOverlap = True
         a.Selector.MinPCut =  10000
         a.Selector.MinPtCut = 500
-        a.Selector.MaxChi2Cut = 5
-        a.Selector.MaxChi2PerDoFMatch = 5
-        a.Selector.MaxChi2PerDoFVelo = 5
-        a.Selector.MaxChi2PerDoFDownstream = 5
         a.Selector.TrackTypes = ["Long"]
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
+            a.Selector.MaxChi2PerDoFMatch = 5
+            a.Selector.MaxChi2PerDoFVelo = 5
+            a.Selector.MaxChi2PerDoFDownstream = 5
+
+# selection for box overlap tracks in IT
+class ITBoxOverlapTracksNoFit( TrackRefiner ):
+    def __init__( self, Name = "ITBoxOverlapTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name, InputLocation, Fitted)
+    def configureSelector( self, a ):
+        from Configurables import ITTrackSelector
+        a.Selector = ITTrackSelector()
+        a.Selector.TrackTypes = ["Long"]
+        a.Selector.RequireOverlap = True
+        a.Selector.MinPCut =  10000
+        a.Selector.MinPtCut = 500
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
+            a.Selector.MaxChi2PerDoFMatch = 5
+            a.Selector.MaxChi2PerDoFVelo = 5
+            a.Selector.MaxChi2PerDoFDownstream = 5
+
 
 # selection for module overlap tracks in IT
-class ITModuleOverlapTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "GoodLongTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation)
+class ITModuleOverlapTracks( TrackRefiner ):
+    def __init__( self, Name = "GoodLongTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation, Fitted = Fitted)
     def configureSelector( self, a ):
         from Configurables import ITTrackSelector
         a.Selector = ITTrackSelector()
         a.Selector.RequireModuleOverlap = True
         a.Selector.MinPCut =  10000
         a.Selector.MinPtCut = 100
-        a.Selector.MaxChi2Cut = 5
-        a.Selector.MaxChi2PerDoFMatch = 5
-        a.Selector.MaxChi2PerDoFVelo = 5
-        a.Selector.MaxChi2PerDoFDownstream = 5
         a.Selector.TrackTypes = ["Long"]
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
+            a.Selector.MaxChi2PerDoFMatch = 5
+            a.Selector.MaxChi2PerDoFVelo = 5
+            a.Selector.MaxChi2PerDoFDownstream = 5
 
 # selection of high momentum TTracks
-class HighMomentumTTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "HighMomentumTTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation)
+class HighMomentumTTracks( TrackRefiner ):
+    def __init__( self, Name = "HighMomentumTTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation, Fitted = Fitted)
     def configureSelector( self, a ):
         from Configurables import TrackSelector
         a.Selector = TrackSelector()
         a.Selector.MinPCut = 50000
-        a.Selector.MaxChi2Cut = 5
         a.Selector.TrackTypes = ["Ttrack"]
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
 
 # selection of Velo backward tracks
-class VeloBackwardTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "VeloBackwardTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation)
+class VeloBackwardTracks( TrackRefiner ):
+    def __init__( self, Name = "VeloBackwardTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation, Fitted = Fitted)
     def configureSelector( self, a ):
         from Configurables import TrackSelector
         a.Selector = TrackSelector()
-        a.Selector.MaxChi2Cut = 5
         a.Selector.TrackTypes = ["Backward"]
         a.Selector.MinNVeloRHits = 4
         a.Selector.MinNVeloPhiHits = 4
         a.Selector.MaxNVeloHoles = 0
+        if self._fitted:
+            a.Selector.MaxChi2Cut = 5
+
 
 # selection of Velo overlap tracks
-class VeloOverlapTracks( AlignTrackRefiner ):
-    def __init__( self, Name = "VeloOverlapTracks", InputLocation = "Rec/Track/Best" ) :
-        AlignTrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation)
+class VeloOverlapTracks( TrackRefiner ):
+    def __init__( self, Name = "VeloOverlapTracks", InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackRefiner.__init__(self, Name = Name, InputLocation = InputLocation, Fitted = Fitted)
     def configureSelector( self, a ):
         from Configurables import VeloTrackSelector
         a.Selector = VeloTrackSelector()
-        a.Selector.MaxChi2PerDoFVelo = 10
-        a.Selector.MaxChi2Cut = 5
         a.Selector.MinHitsASide = 1
         a.Selector.MinHitsCSide = 1
         a.Selector.MinNVeloRHits = 4
         a.Selector.MinNVeloPhiHits = 4
         a.Selector.MaxNVeloHoles = 0
+        a.Selector.TrackTypes = ["Long"]
+        if self._fitted:
+            a.Selector.MaxChi2PerDoFVelo = 10
+            a.Selector.MaxChi2Cut = 5
 
+
+# a favourite cocktail of rare tracks
+class FavouriteTrackCocktail(TrackSelection): 
+    __slots__ = {
+        "_inputLocation" : "" # Location of input track list"
+        , "_fitted" : True
+        }
+    def __init__( self, Name, InputLocation = "Rec/Track/Best", Fitted = True ) :
+        TrackSelection.__init__(self, Name )
+        self._inputLocation = InputLocation
+        self._fitted = Fitted
+    def algorithm( self ):
+        from Configurables import GaudiSequencer, TrackListMerger
+        Name = self._name
+        loc = self._inputLocation
+        b = self._fitted
+        seq = GaudiSequencer( Name + "SelectionSeq" )
+        selections = [ VeloOverlapTracks(Name = Name + "VeloOverlap",
+                                         InputLocation = loc,Fitted = b),
+                       VeloBackwardTracks(Name = Name + "VeloBackward",
+                                          InputLocation = loc,Fitted= b),
+                       ITBoxOverlapTracks(Name = Name + "ITBoxOverlap",
+                                          InputLocation = loc,Fitted = b),
+                       GoodLongTracks(Name = Name + "GoodLong",
+                                      InputLocation = loc,Fitted = b) ]
+        seq.Members += [ i.algorithm() for i in selections ]
+        merger = TrackListMerger( Name + "Merger" )
+        merger.inputLocations = [ i.location() for i in selections ]
+        merger.outputLocation = self.location()
+        seq.Members.append( merger )
+        return seq 
+
+# a complete sequence to get the tracks from the HLT event. put them
+# in a different location than usual.  it seems that the Hlt2Global
+# decision is the most useful, because it has TT hits on the
+# tracks. adding Hlt1Global does rather little. if you do, make sure
+# to put it after Hlt2Global, since the clone rejection takes the
+# first track.
+class NoPIDTracksFromHlt(TrackSelection):
+    def __init__( self ) :
+        TrackSelection.__init__(self, 'NoPIDTracksFromHlt')
+
+    def algorithm( self ):
+        from Configurables import GaudiSequencer, HltTrackConverter, TrackContainerCopy
+        from TAlignment.Utils import configuredFitAndHitAdderSequence
+        seq = GaudiSequencer(self.name() + "Seq")
+        hltTrackConvAll = HltTrackConverter("HltTrackConvAll",TrackDestignation = 'Rec/Track/NoPIDBest',
+                                            HltLinesToUse = ['Hlt2Global'])
+        seq.Members.append( hltTrackConvAll ) 
+        tracksel = FavouriteTrackCocktail(Name ='NoPIDForAlignment',
+                                          InputLocation = 'Rec/Track/NoPIDBest',
+                                          Fitted = False)
+        seq.Members += [ tracksel.algorithm(),
+                         TrackContainerCopy('NoPIDForAlignmentCopy',
+                                            inputLocation = tracksel.location(),
+                                            outputLocation = tracksel.location() + 'Owned'),
+                         configuredFitAndHitAdderSequence('NoPIDFit',
+                                                          InputLocation = tracksel.location() + 'Owned',
+                                                          OutputLocation = self.location() ) ]
+        return seq
