52ff7050b0582aca6dc81bac1fc7b726d2ae6dab
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-03-13 11:50:20 +0000

    added velo sensor alignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@136737 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3b3e668572f5f89f4be57e6caa13d5ac042d5e3

--- Alignment/Alignment/TAlignment/python/TAlignment/AlignmentScenarios.py
+++ Alignment/Alignment/TAlignment/python/TAlignment/AlignmentScenarios.py
@@ -15,7 +15,7 @@ from Configurables import TAlignment
         
 # scenario used for prompt alignment
 
-def configurePromptAlignment() :
+def configurePromptAlignment( fixQOverPBias = True ) :
     TAlignment().WriteCondSubDetList += ['Velo','TT','IT','OT','MUON']
   
     # define the alignment elements
@@ -37,9 +37,11 @@ def configurePromptAlignment() :
 
     # make sure that the velo stays where it was
     TAlignment().Constraints = constraints = []
-    constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx : total")
-    # fix the q/p scale by not moving T in X
-    constraints.append("OT3X : OT/T3X1U.Side : Tx")
+    constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx ")
+    # fix the q/p scale by not moving T in X. note that you do not
+    # want to do this if you use D0 in the alignment
+    if fixQOverPBias:
+        constraints.append("OT3X : OT/T3X1U.Side : Tx")
  
     # tweak the survey a little bit to fix the z-scale to survey
     surveyconstraints = SurveyConstraints()
@@ -49,7 +51,7 @@ def configurePromptAlignment() :
 
 
 # define additional scenarios below
-def configureVeloHalfAlignment():
+def configureVeloHalfAlignment(): 
     TAlignment().WriteCondSubDetList  += ['Velo']
   
     # define the alignment elements
@@ -61,9 +63,47 @@ def configureVeloHalfAlignment():
 
     # make sure that the velo stays where it was
     TAlignment().Constraints = constraints = []
-    constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx : total")
+    constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx ")
  
     # tweak the survey a little bit to fix the z-scale to survey
     surveyconstraints = SurveyConstraints()
     surveyconstraints.All()
+
+
+def configureVeloSensorAlignment():
+    TAlignment().WriteCondSubDetList  += ['Velo']
+
+    # To align at the sensor level, we align 'modules' and
+    #'phi-sensors'. The R-sensor is always equal to the modules. (We
+    #keep its 'deltas' equal to zero.) However, because of a feature
+    #in the way we keep track of mother-daughter relations, it is
+    #important to create alignables for the R-sensors as well: use
+    #"None" to specify an alignable without active degrees of
+    #freedom.
+
+    # Note also that it doesn't make sense to align R modules for
+    # rotations around the z-axis. Rather than aligning the phi sensor
+    # for this rotation, we put the rotation at the module level.
+
+    elements = Alignables()
+    elements.Velo("None")
+    elements.VeloRight("TxTy")
+    elements.VeloLeft("TxTy")
+    elements.VeloModules("TxTyTzRz")
+    elements.VeloPhiSensors("TxTyTz")
+    elements.VeloRSensors("None")
+    TAlignment().ElementsToAlign += list(elements)
+
+    # make sure that the velo stays where it was. Important note: the
+    # dofs here must match the ones that we actually align for. If you
+    # specify too many, things will go rather wrong.
+    TAlignment().Constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx Ty")
+    
+    # now constrain the total shearing and z-scale in each Velo half
+    # to get the z-axis nominal. we'll do this by tightening the
+    # survey errors:
+    surveyconstraints = SurveyConstraints()
+    surveyconstraints.All()
+    surveyconstraints.XmlUncertainties += [ 
+        'Velo/Velo(Left|Right)/Module(00|01|40|41) : 0.0001 0.0001 0.0001 0.000001 0.000001 0.000001' ]
 
