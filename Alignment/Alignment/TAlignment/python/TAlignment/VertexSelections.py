0b6f5c7a7db6b586ca4d2f744218029758fd8033
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-03-13 11:43:10 +0000

    first import
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@136734 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 696b56e18df082fbf028283c03aedcb61280de18

---
+++ Alignment/Alignment/TAlignment/python/TAlignment/VertexSelections.py
@@ -0,0 +1,35 @@
+class VertexSelection:
+    def __init__( self, Name, Location, Algorithm = None ) :
+        self._name = Name
+        self._location = Location
+        self._algorithm = Algorithm
+
+    def algorithm(self) : return self._algorithm
+    def location(self) : return self._location
+    def name(self) : return self._name
+
+def configuredPVSelection():
+    # we assume that the PV reconstruction has already been performed.
+    # (can always add later)
+    inputlocation = "Rec/Vertex/Primary"
+    outputlocation = "Rec/Vertex/AlignPrimaryVertices"
+    from Configurables import TrackPVRefitter,VertexListRefiner,GaudiSequencer
+    pvrefitter = TrackPVRefitter("AlignPVRefitter",
+                                 PVContainer =  "Rec/Vertex/Primary")
+    pvselector = VertexListRefiner("AlignPVSelector",
+                                   InputLocation = "Rec/Vertex/Primary",
+                                   OutputLocation = "Rec/Vertex/AlignPrimaryVertices",
+                                   MaxChi2PerDoF = 5,
+                                   MinNumTracks = 10,
+                                   MinNumLongTracks = 2 )
+    seq = GaudiSequencer("AlignPVSelSeq")
+    seq.Members += [ pvrefitter, pvselector ]
+    sel = VertexSelection(Name = "DefaultPVSelection",
+                          Location = "Rec/Vertex/AlignPrimaryVertices",
+                          Algorithm = seq)
+    return sel
+
+def configuredPVSelectionFromDST():
+    print "I still need to implement this. "\
+        "Use the PV Refitter. It can also refit the tracks"
+    return None
