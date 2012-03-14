9741c0aba2eaf606ec77db7cf11e823178e0d9aa
Jibo He <jibo.he@cern.ch>
2012-03-14 17:43:19 +0000

    Indrek  added D->4 mu line
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@136851 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 184034858f1d387a63a3b22c545b95dac006ab89

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2MuMuMuMuLines.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2MuMuMuMuLines.py
@@ -47,13 +47,17 @@ class B2MuMuMuMuLinesConf(LineBuilder) :
     """
 
     __configuration_keys__ = ('B2MuMuMuMuLinePrescale',
-                              'B2MuMuMuMuLinePostscale'
+                              'B2MuMuMuMuLinePostscale',
+                              'D2MuMuMuMuLinePrescale',
+                              'D2MuMuMuMuLinePostscale'
                               )
     
     #### This is the dictionary of all tunable cuts ########
     config_default={
         'B2MuMuMuMuLinePrescale'    : 1,
         'B2MuMuMuMuLinePostscale'   : 1,
+        'D2MuMuMuMuLinePrescale'    : 1,
+        'D2MuMuMuMuLinePostscale'   : 1,
         }                
     
     
@@ -68,16 +72,29 @@ class B2MuMuMuMuLinesConf(LineBuilder) :
 
         default_name=name
 
+        D_name='D2MuMuMuMu'
+
         self.selDefault = makeDefault(default_name)
+
+        self.selD2MuMuMuMu = makeD2MuMuMuMu(D_name)
+
 
         self.defaultLine = StrippingLine(default_name+"Line",
                                             prescale = config['B2MuMuMuMuLinePrescale'],
                                             postscale = config['B2MuMuMuMuLinePostscale'],
                                             algos = [ self.selDefault ]
                                             )
+
+        self.D2MuMuMuMuLine = StrippingLine(D_name+"Line",
+                                            prescale = config['D2MuMuMuMuLinePrescale'],
+                                            postscale = config['D2MuMuMuMuLinePostscale'],
+                                            algos = [ self.selD2MuMuMuMu ]
+                                            )
         
       
         self.registerLine( self.defaultLine )
+
+        self.registerLine( self.D2MuMuMuMuLine )
 
 
 
@@ -113,3 +130,36 @@ def makeDefault(name) :
                       RequiredSelections = [ _stdLooseMuons ])
 
 
+def makeD2MuMuMuMu(name) :
+    """
+    D --> 4 mu selection
+    should become     inclusive bb-->4 mu selection  ??
+    """
+    from Configurables import OfflineVertexFitter
+    D2MuMuMuMu = CombineParticles("Combine"+name)
+    D2MuMuMuMu.DecayDescriptor = "D0 -> mu+ mu- mu+ mu-"
+    # Set the OfflineVertexFitter to keep the 4 tracks and not the J/Psi Kstar:
+    D2MuMuMuMu.addTool( OfflineVertexFitter() )
+    D2MuMuMuMu.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+    D2MuMuMuMu.OfflineVertexFitter.useResonanceVertex = False
+    D2MuMuMuMu.ReFitPVs = True
+    D2MuMuMuMu.DaughtersCuts = { "mu+" : "(TRCHI2DOF < 5 ) "\
+                                  " & (MIPCHI2DV(PRIMARY)> 9.)"}
+
+    D2MuMuMuMu.CombinationCut =  "(ADAMASS('D0')<1000*MeV) "\
+                                 "& (AMAXDOCA('')<0.3*mm) "
+
+ 
+    D2MuMuMuMu.MotherCut = "(VFASPF(VCHI2/VDOF)<10) "\
+                              "& (BPVDIRA > 0.9998) "\
+                              "& (BPVVDCHI2>48.)"\
+                              " & (M>864.83) & (M<2868.47)"\
+                              "& (BPVIPCHI2()< 30) "
+    
+
+    _stdLooseMuons = DataOnDemand(Location = "Phys/StdLooseMuons/Particles")
+
+
+    return Selection (name,
+                      Algorithm = D2MuMuMuMu,
+                      RequiredSelections = [ _stdLooseMuons ])
