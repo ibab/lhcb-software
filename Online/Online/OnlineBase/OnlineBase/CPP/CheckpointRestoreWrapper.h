7bb6fa28c4b9529ce7a039d38fd976f6f1fb953f
Markus Frank <frankb@cern.ch>
2012-03-15 22:46:35 +0000

    Fixes for checkpointing with gcc46
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@136912 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 92e867ec9af716278f229fe36efc77f30d538f38

--- Online/Online/OnlineBase/OnlineBase/CPP/CheckpointRestoreWrapper.h
+++ Online/Online/OnlineBase/OnlineBase/CPP/CheckpointRestoreWrapper.h
@@ -24,34 +24,6 @@ class CheckpointRestoreWrapper  {
  public:
   /// Initialization callback with startup arguments
   virtual void init_instance(int argc, char** argv, char** environ) = 0;
-  /// Setup process UTGID if availible
-  virtual int setUTGID(const char* new_utgid)  = 0;
-  /// Force process UTGID/argv[0] if availible
-  virtual int forceUTGID(const char* new_utgid)  = 0;
-  /// Set the printout level for the checkpoint/restore mechanism
-  virtual int setPrint(int new_level) = 0;
-  /// Give access to the restart type after a call to "checkpoint". 0==checkpointing, 1=restart from checkpoint
-  virtual int restartType() const = 0;
-  /// Stop all threads to allow producing a consistent checkpoint
-  virtual int stop()            = 0;
-  /// Resume all threads after the checkpoint was produced
-  virtual int resume()          = 0;
-  /// Fork child instance
-  virtual int forkInstance()    = 0;
-  /// Child fork: Re-establish child to running state
-  virtual int startChild()      = 0;
-  /// After a restart allow to set environment etc. from stdin.
-  virtual int updateEnv() = 0;
-
-  /// Create a checkpoint file by writing to file descriptor fd
-  virtual int checkpoint(int fd)= 0;
-
-  /// Save file descriptor information to opaque memory block (allocated inside!)
-  virtual int getFileDescriptors(void** ptr) = 0;
-  /// Restore file descriptors from opaque memory block
-  virtual int setFileDescriptors(void* ptr)  = 0;
-  /// For debugging: Dump file descriptors to stdout
-  virtual int dumpFileDescriptors(void* ptr) = 0;
 };
 
 void CheckpointRestoreWrapper__init_instance(int argc, char** argv);
