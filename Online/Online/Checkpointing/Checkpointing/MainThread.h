b870f91f0abe168f1674796636a6e9df2ef0c180
Markus Frank <frankb@cern.ch>
2012-03-15 22:46:51 +0000

    Fixes for checkpointing with gcc46
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@136913 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 287f2b9f1bcb48e4fc2e6738f7298133427e89e9

--- Online/Online/Checkpointing/Checkpointing/MainThread.h
+++ Online/Online/Checkpointing/Checkpointing/MainThread.h
@@ -5,6 +5,29 @@
 
 // Forward declarations
 struct user_desc;
+/* @class CheckpointRestoreWrapper CheckpointRestoreWrapper.h CPP/CheckpointRestoreWrapper.h
+ *
+ * Basic interface to interface to checkpoint / restore mechanisms for fast program startup.
+ *
+ * NOTE:
+ * This header structure is explicitly copied to the checkpoint/restart package(s)
+ * To decouple them entirely from the application
+ *
+ * !!!! DO NOT EVEN THINK ABOUT CHANGING IT WITHOUT KNOWNING WHAT YOU DO !!!!
+ *
+ * @author  M.Frank
+ * @version 1.0
+ */
+class CheckpointRestoreWrapper  {
+ protected:
+  /// Standard constructor
+  CheckpointRestoreWrapper() {}
+  /// Standard destructor
+  virtual ~CheckpointRestoreWrapper() {}
+ public:
+  /// Initialization callback with startup arguments
+  virtual void init_instance(int argc, char** argv, char** environ) = 0;
+};
 
 /*
  * Checkpointing namespace declaration
@@ -19,7 +42,7 @@ namespace CHECKPOINTING_NAMESPACE {
    * @author  M.Frank
    * @version 1.0
    */
-  class MainThread  {
+  class MainThread  : public    CheckpointRestoreWrapper  {
     /// Standard constructor
     MainThread();
   public:
