f470e9cbf0b3e2b64ac2afc11faac2e526ace417
Albert Puig Navarro <albert.puig@epfl.ch>
2012-03-16 00:17:07 +0000

    New version of cut-based radiative topo lines.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@136924 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 290f7abbc3f305dd1aabcba2e31746b4f07e33a5

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2RadiativeTopoLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2RadiativeTopoLines.py
@@ -16,52 +16,58 @@ from HltLine.HltLinesConfigurableUser import HltLinesConfigurableUser
 from HltLine.HltLine import Hlt2Member, bindMembers
 from Configurables import CombineParticles
 
-
-
 class Hlt2RadiativeTopoConf(HltLinesConfigurableUser):
   __slots__ = {# Track cuts
-               'Track_PT_MIN'       : 700.0  , # MeV
+               'Track_PT_MIN'       : 500.0  , # MeV
                'Track_P_MIN'        : 5000.0 , # MeV
                'Track_IPCHI2_MIN'   : 10.0   , # Dimensionless
+               'Track_HARD_IPCHI2_MIN' : 16.0   , # Dimensionless
                'Track_CHI2_MAX'     : 5    , # Dimensionless
+               # KS cuts
+               'USE_KS'            : True,
+               'KS_IPCHI2DV_MIN'   : 16.0,    # unitless
+               'KS_TRACK_PT_MIN'   : 300.0  , # MeV
+               'KS_TRACK_P_MIN'    : 3000.0 , # MeV
+               'KS_BPVVDCHI2_MIN'  : 1000.0,  # unitless 
+               'KS_MASS_WINDOW'    : 30.0,    # MeV
                # Track combination cuts
                'DITRACK_AMAXDOCA_MAX'      : 0.15   , # mm        
                'DITRACK_MINTRCHI2DOF_MAX'  : 3      , # Dimensionless 
                'DITRACK_VTXCHI2_MAX'       : 10     , # Dimensionless
                'DITRACK_M_MAX'             : 2000   , # MeV
                'DITRACK_PT_MIN'            : 1500   , # MeV
+               'DITRACK_HARD_TRACKMAX_PT_MIN'   : 3000  , # MeV
+               'DITRACK_SOFT_TRACKMAX_PT_MIN'   : 1200  , # MeV
                # Photon
-               'photon_PT_MIN'      : 2500.0 , # MeV
+               'photon_SOFT_PT_MIN'      : 2000.0 , # MeV
+               'photon_HARD_PT_MIN'      : 2500.0 , # MeV
                # Track + photon combination cuts
                'B_SUMPT_MIN'        : 5000   , # MeV
                'B_FDCHI2_MIN'       : 64     , # Dimensionless
-               'B_PT_MIN'           : 1000   , # MeV
-               #'B_MASS_MAX'         : 7000   , # MeV
-               #'B_MASS_MIN'         : 4000   , # MeV
-               'B_CORRM_MAX'        : 7000  , # MeV
+               'B_PT_MIN'           : 2000   , # MeV
+               'B_CORRM_MAX'        : 7000   , # MeV
                'B_CORRM_MIN'        : 4000   , # MeV
                # GEC
-               'GEC_USE'            : True   ,
-               'GEC_NTRACK_MAX'     : 180    , # max number of tracks
+               'USE_GEC'            : True   ,
+               'GEC_NTRACK_MAX'     : 120    , # max number of tracks
                # L0
-               'L0Filter'      : "|".join( [ "L0_CHANNEL('%s')" % channel for channel in ['Photon','Electron'] ] ),
+               'L0FilterPhoton'     : "|".join( [ "L0_CHANNEL('%s')" % channel for channel in ['Photon','Electron'] ] ),
+               'L0FilterTrack'      : "|".join( [ "L0_CHANNEL('%s')" % channel for channel in ['Hadron'] ] ),
                # HLT1
                'HLT1Filter'    : "HLT_PASS_RE('Hlt1(?!ODIN)(?!L0)(?!Lumi)(?!Tell1)(?!MB)(?!Velo)(?!BeamGas)(?!Incident).*Decision')" ,
                # TOS
-               'TisTosParticleTaggerSpecs': { "Hlt1Track.*Decision%TOS":0 } ,
+               'USE_TRACKTOS'       : True   ,
+               'TisTosParticleTaggerSpecs': { "Hlt1TrackAllL0Decision%TOS": 0, "Hlt1TrackPhotonDecision%TOS":0 } ,
                # IDs
-               'HltANNSvcID'   : {'RadiativeTopoTrackTOS': 50515, 
-                                  'RadiativeTopoPhotonL0': 50516} ,
+               'HltANNSvcID'   : {'RadiativeTopoTrack' : 50515, 
+                                  'RadiativeTopoPhoton': 50516} ,
                }
                
-  def __combineTracks(self, name, inputParticles, decayDescriptor):
+  def __combineTracks(self, name, inputParticles):
     from HltLine.Hlt2Monitoring import Hlt2MonitorMinMax
     props = self.getProps()
-    trackCuts = """(TRCHI2DOF < %(Track_CHI2_MAX)s) & (MIPCHI2DV(PRIMARY) > %(Track_IPCHI2_MIN)s) & (PT > %(Track_PT_MIN)s *MeV) & (P > %(Track_P_MIN)s *MeV)""" % props
-    #trackMonitor = Hlt2MonitorMinMax("TRCHI2DOF", "Track chi2/dof", 0, 10, nbins=100) + '&' +\
-    #               Hlt2MonitorMinMax("MIPCHI2DV(PRIMARY)", "Track IP chi2", 0, 200, nbins=100) + '&' +\
-    #               Hlt2MonitorMinMax("PT", "Track p_{T}", 0, 3000, nbins=50)
-    comboCuts = """(AALLSAMEBPV) &
+    #trackCuts = """(MIPCHI2DV(PRIMARY) > %(Track_IPCHI2_MIN)s)""" % props
+    comboCuts = """((AALLSAMEBPV) | (AMINCHILD(MIPCHI2DV(PRIMARY)) > %(Track_HARD_IPCHI2_MIN)s )) &
                    (AM < 5000*MeV) &
                    (AMAXDOCA('LoKi::DistanceCalculator') < %(DITRACK_AMAXDOCA_MAX)s)""" % props
     motherCuts = """(BPVDIRA > 0) &
@@ -69,23 +75,13 @@ class Hlt2RadiativeTopoConf(HltLinesConfigurableUser):
                     (PT > %(DITRACK_PT_MIN)s * MeV) &
                     (VFASPF(VCHI2) < %(DITRACK_VTXCHI2_MAX)s) &
                     (MINTREE(HASTRACK, TRCHI2DOF) < %(DITRACK_MINTRCHI2DOF_MAX)s)""" % props
-    #motherMonitor = Hlt2MonitorMinMax("BPVVDCHI2", "FD chi2", 0 , 200, nbins=100) + '&' +\
-    #                Hlt2MonitorMinMax("MINTREE(HASTRACK, TRCHI2DOF)", "Best track chi2/dof", 0, 25, nbins=50) + '&' +\
-    #                Hlt2MonitorMinMax("BPVIPCHI2()", "vtx IP #chi^2", 0, 300, nbins=60) + '&' +\
-    #                Hlt2MonitorMinMax("SUMTREE(MIPCHI2DV(PRIMARY), M<4000, 0.0)", "#sum_{IP chi^{2}}", 0, 500, nbins=50) + ' & ' +\
-    #                Hlt2MonitorMinMax("VFASPF(VCHI2)", "Vertex chi2/dof", 0, 16, nbins=100) + '&' +\
-    #                Hlt2MonitorMinMax("M", "M(2Track)", 0, 6000, nbins=60) + '&' +\
-    #                Hlt2MonitorMinMax("BPVCORRM", "CorrM(2Track)", 0, 10000, nbins=100)
     combination = Hlt2Member(CombineParticles                                  ,
                              'CombineTracks'                                   ,
-                             DecayDescriptors=decayDescriptor                  ,
+                             DecayDescriptors=["K*(892)0 -> K+ K+","K*(892)0 -> K+ K-","K*(892)0 -> K- K-"] ,
                              Inputs=inputParticles                             ,
-                             DaughtersCuts={"K+": trackCuts, "K-": trackCuts } ,
-    #                         DaughtersMonitors={"K+": trackMonitor, "K-": trackMonitor} ,
+                             #DaughtersCuts={"K+": trackCuts, "K-": trackCuts } ,
                              CombinationCut=comboCuts                          ,
-                             # CombinationMonitor=comboMonitor                   ,
                              MotherCut=motherCuts                              ,
-    #                         MotherMonitor=motherMonitor
                              )
     return bindMembers(name, inputParticles+[combination])
 
@@ -110,45 +106,111 @@ class Hlt2RadiativeTopoConf(HltLinesConfigurableUser):
   def __filterHlt1TOS(self, name, inputComb) :
     from HltLine.HltLine import Hlt2Member, bindMembers
     from Configurables import TisTosParticleTagger
-    filterTOS = Hlt2Member(TisTosParticleTagger                                  ,
-                           'Hlt1TOSFilter'                                       ,
-                           Inputs=[inputComb.outputSelection()]                  ,
-                           TisTosSpecs=self.getProp('TisTosParticleTaggerSpecs') )
-    return bindMembers(name, [inputComb, filterTOS])
+    from HltLine.HltLine import Hlt2Member,bindMembers
+    TOSParticleTagger = TisTosParticleTagger(name+"Tagger")
+    TOSParticleTagger.TisTosSpecs = self.getProp('TisTosParticleTaggerSpecs')
+    TOSParticleTagger.NoRegex = True
+    TOSParticleTagger.ProjectTracksToCalo = False
+    TOSParticleTagger.CaloClustForCharged = False
+    TOSParticleTagger.CaloClustForNeutral = False
+    TOSParticleTagger.TOSFrac = {4:0.0,5:0.0 }
+    TOSParticleTagger.Inputs = [inputComb.outputSelection()]
+    TOSParticleTagger.Output = 'Hlt2/%s/Particles'%(name+'Tagger')
+    return bindMembers(name+'ParticleFilter', [inputComb,TOSParticleTagger])
 
-  def __buildInputTracks(self):
-    from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedKaons  
-    decays = ["K*(892)0 -> K+ K+", "K*(892)0 -> K+ K-", "K*(892)0 -> K- K-"]
-    return self.__combineTracks("DiTrack", [BiKalmanFittedKaons], decays)
-    
-  def __buildPhotons(self):
-    from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedPhotonsFromL0 as photons
-    return photons
-
-  def __combineTracksAndPhoton(self, name, inputTracks, inputPhotons):
-    from HltLine.Hlt2Monitoring import Hlt2MonitorMinMax, Hlt2Monitor
+  def __filterMaxTrackPt(self, name, inputTracks, cutProp):
+    from HltLine.HltLine import Hlt2Member, bindMembers
+    from Configurables import FilterDesktop
     props = self.getProps()
-    inputParticles = [inputTracks, inputPhotons]
+    # Cut on the max PT of the physical tracks
+    filterCut = "MAXTREE(HASTRACK, PT)>%s" % self.getProp(cutProp)
+    __filter = Hlt2Member( FilterDesktop          ,
+                           name+'MaxTrackPtFilter'   ,
+                           Inputs=[inputTracks] ,
+                           Code=filterCut)
+    return bindMembers(name, [inputTracks, __filter])
+
+  def __buildInputTracks(self, name, inputContainers, isKS):
+    """Filters input tracks for radiative topological."""
+    from HltLine.HltLine import Hlt2Member, bindMembers
+    from Configurables import FilterDesktop
+    props = self.getProps()
+    cuts = "(TRCHI2DOF < %(Track_CHI2_MAX)s) &" % props
+    if isKS:
+      cuts += """(PT > %(KS_TRACK_PT_MIN)s*MeV) &
+                 (P > %(KS_TRACK_P_MIN)s*MeV) &
+                 (MIPCHI2DV(PRIMARY) > %(KS_IPCHI2DV_MIN)s)""" % props
+    else:
+      cuts += """(PT > %(Track_PT_MIN)s*MeV) &
+                 (P > %(Track_P_MIN)s*MeV) &
+                 (MIPCHI2DV(PRIMARY) > %(Track_IPCHI2_MIN)s)""" % props    
+    __filter = Hlt2Member(FilterDesktop,
+                          'Filter',
+                          Inputs=inputContainers,
+                          Code=cuts)
+    return bindMembers(name, inputContainers + [__filter])
+    
+  def __combineKS(self, name, inputTracks, mass):
+    """Make KSs amd Lambdas."""
+    from HltLine.HltLine import Hlt2Member, bindMembers
+    from Configurables import CombineParticles
+    props = self.getProps()
+    dm = float(props['KS_MASS_WINDOW'])
+    comboCuts = "in_range(%.3f*MeV, AM, %.3f*MeV)" % (mass-1.5*dm, mass+1.5*dm)
+    momCuts = """in_range(%.3f*MeV, M, %.3f*MeV) &
+                 (VFASPF(VCHI2)<%s) & 
+                 (BPVDIRA > 0) &
+                 (BPVVDCHI2 > %s)""" % (mass-dm,mass+dm, props['DITRACK_VTXCHI2_MAX'], props['KS_BPVVDCHI2_MIN'])
+    createKS = Hlt2Member(CombineParticles,
+                          name+"Combine",
+                          DecayDescriptors=["K+ -> pi+ pi-"], 
+                          CombinationCut=comboCuts,
+                          MotherCut = momCuts,
+                          Inputs=inputTracks)
+    return bindMembers(name, inputTracks + [createKS])
+
+  def __joinInputContainers(self, kaons, kshorts, tag):
+      """Puts Ks and KSs into one container w/ same ID."""
+      from HltLine.HltLine import Hlt2Member, bindMembers
+      from Configurables import GaudiSequencer, FilterDesktop
+      inputK = GaudiSequencer("InputK",Members=kaons.members())
+      inputKS = GaudiSequencer("InputKS", Members=kshorts.members())
+      inputs = [inputK, inputKS]
+      finputs = [kaons, kshorts]
+      inputAll = GaudiSequencer("InputRadTopoALL",
+                                Members=inputs,
+                                ModeOR=True,
+                                ShortCircuit=False)
+      __filter = Hlt2Member(FilterDesktop,
+                            'FilterRadTopoAllInput',
+                            Inputs=finputs,
+                            Code='ALL')
+      return bindMembers("InputRadTopoALL" + tag, [inputAll, __filter])
+
+  def __buildPhotons(self, name, inputTracks, cutProp):
+    from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedPhotonsFromL0 as photons
+    from HltLine.HltLine import Hlt2Member, bindMembers
+    from Configurables import FilterDesktop
+    filterCut = "PT>%s*MeV" % self.getProps()[cutProp]
+    __filter = Hlt2Member( FilterDesktop       ,
+                           name+'Filter'       ,
+                           Inputs=[photons] ,
+                           Code=filterCut)
+    return bindMembers(name, [inputTracks, photons, __filter])
+
+  def __combineTracksAndPhoton(self, name, *inputParticles):
+    from HltLine.Hlt2Monitoring import Hlt2MonitorMinMax, Hlt2Monitor
+    inputParticles = list(inputParticles)
+    props = self.getProps()
     motherCuts = """(PT > %(B_PT_MIN)s) &
                     (((BPVCORRM > %(B_CORRM_MIN)s) & (BPVCORRM < %(B_CORRM_MAX)s))) &
                     (BPVVDSIGN > 0) &
                     (BPVVDCHI2 > %(B_FDCHI2_MIN)s) &
-                    (SUMTREE(PT, (('K+'==ABSID)|('gamma'==ABSID)) , 0.0) > %(B_SUMPT_MIN)s*MeV)""" % props
-    #motherMonitor = Hlt2MonitorMinMax("BPVDIRA", "B DIRA", 0.999, 1, nbins=100) + ' & ' +\
-    #                Hlt2MonitorMinMax("BPVCORRM", "CorrM (2track + #gamma)", 3500, 10000, nbins=50) + ' & ' +\
-    #                Hlt2MonitorMinMax("BPVVDCHI2", "Flight distance chi2", 0 , 250, nbins=100) + '&' +\
-    #                Hlt2MonitorMinMax("BPVIPCHI2()", "VTX IP chi2", 0 , 100, nbins=100) + '&' +\
-    #                Hlt2MonitorMinMax("M", "Mass (2track + #gamma)", 2000, 8000, nbins=60) + ' & ' +\
-    #                Hlt2MonitorMinMax("SUMTREE(MIPCHI2DV(PRIMARY), M<4000, 0.0)", "#sum_{IP chi^{2}}", 0, 500, nbins=50) + ' & ' +\
-    #                Hlt2MonitorMinMax("SUMTREE(PT, M<4000, 0.0)", "#sum p_{T}", 0, 10000, nbins=100)
-    #gammaMonitor = Hlt2MonitorMinMax("PT", "#gamma p_{T}", 0, 5000, nbins=50)
+                    (SUMTREE(PT, (('K+'==ABSID)|('gamma'==ABSID)|('KS0'==ABSID)) , 0.0) > %(B_SUMPT_MIN)s*MeV)""" % props
     combination = Hlt2Member(CombineParticles                                         ,
-                             "CombineTracksAndPhoton"                                 ,
+                             "CombineTracksAndPhoton"+name                            ,
                              DecayDescriptors=["[B0 -> K*(892)0  gamma]cc"]           ,
-                             DaughtersCuts={"gamma": "(PT>%(photon_PT_MIN)s*MeV)" % props},
-   #                          DaughtersMonitors={"gamma": gammaMonitor},
                              MotherCut=motherCuts                                     ,
-   #                          MotherMonitor=motherMonitor                              ,
                              Preambulo=["from math import cos"]                       ,
                              Inputs=inputParticles)
     return bindMembers(name, inputParticles+[combination])    
@@ -157,46 +219,65 @@ class Hlt2RadiativeTopoConf(HltLinesConfigurableUser):
     from HltLine.HltLine import Hlt2Line
     from HltTracking.HltPVs import PV3D
     from Configurables import HltANNSvc
-    from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedKaons  
+    from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedKaons
+    from Hlt2SharedParticles.GoodParticles import GoodPions
     from HltLine.HltDecodeRaw import DecodeL0CALO
-    
-    l0filter = self.getProp("L0Filter")
-    if l0filter == "":
-      l0filter = None
+    # Configure trigger requirements
+    l0filterphoton = self.getProp("L0FilterPhoton")
+    if l0filterphoton == "":
+      l0filterphoton = None
+    l0filtertrack = self.getProp("L0FilterTrack")
+    if l0filtertrack == "":
+      l0filtertrack = None
     hltfilter = self.getProp("HLT1Filter")
     if hltfilter == "" :
       hltfilter = None
-
+    # Decide if we need V0
+    useKS = self.getProps()['USE_KS']
+    # Initialize algorithm list
     algoList = []
     # GECs?
-    if self.getProp('GEC_USE'):
+    if self.getProp('USE_GEC'):
       gec = self.__seqGEC()
       algoList.append(gec)
-    # Build the pieces
-    tracks  = self.__buildInputTracks()
-    tracksTOS = self.__filterHlt1TOS('DiTrackTOS', tracks)
-    photons = self.__buildPhotons()
+    # Build the di-track
+    tracks  = self.__buildInputTracks('InputKaons', [BiKalmanFittedKaons], False)
+    if useKS:
+        # Build KS
+        pionsForKS = self.__buildInputTracks('InputPionsForKS', [GoodPions], True)
+        inputKS = self.__combineKS('InputKS', [pionsForKS], 497.648)
+        tracks = self.__joinInputContainers(tracks, inputKS, 'All')
+    ditracks = self.__combineTracks('DiTrack', [tracks])
+    if self.getProp('USE_TRACKTOS'):
+      ditracks = self.__filterHlt1TOS('DiTrackTOS', ditracks)
+    # Prepare two sets of tracks
+    ditracksSoft = self.__filterMaxTrackPt('DiTrackSoftPt', ditracks, 'DITRACK_SOFT_TRACKMAX_PT_MIN')
+    ditracksHard = self.__filterMaxTrackPt('DiTrackHardPt', ditracks, 'DITRACK_HARD_TRACKMAX_PT_MIN')
+    # Build photons
+    photonsSoft = self.__buildPhotons('SoftPhotons', ditracksHard, 'photon_SOFT_PT_MIN')
+    photonsHard = self.__buildPhotons('HardPhotons', ditracksSoft, 'photon_HARD_PT_MIN')
     # Build combinations
-    tracksAndPhoton = self.__combineTracksAndPhoton('PhotonL0', tracks, photons)
-    tracksTOSAndPhoton = self.__combineTracksAndPhoton('TrackTOS', tracksTOS, photons)
+    softTracksAndHardPhoton = self.__combineTracksAndPhoton('Photon', ditracksSoft, photonsHard)
+    hardTracksAndSoftPhoton = self.__combineTracksAndPhoton('Track', ditracksHard, photonsSoft)
     # Create the line
-    algoListTOS = algoList[:]
-    algoListL0  = algoList[:]
-    algoListTOS.extend([PV3D(), BiKalmanFittedKaons, tracksTOS, DecodeL0CALO, photons, tracksTOSAndPhoton])
-    algoListL0.extend([PV3D(), BiKalmanFittedKaons, tracks, DecodeL0CALO, photons, tracksAndPhoton])
-    lineTOS = Hlt2Line('RadiativeTopoTrackTOS' ,
+    algoListTrack = algoList[:]
+    algoListTrack.extend([PV3D(), BiKalmanFittedKaons, ditracksHard, DecodeL0CALO, photonsSoft, hardTracksAndSoftPhoton])
+    algoListPhoton  = algoList[:]
+    algoListPhoton.extend([PV3D(), BiKalmanFittedKaons, ditracksSoft, DecodeL0CALO, photonsHard, softTracksAndHardPhoton])
+    lineTOS = Hlt2Line('RadiativeTopoTrack' ,
+                       L0DU=l0filtertrack      ,
                        HLT=hltfilter           ,
                        prescale=1.0            ,
-                       algos=algoListTOS       ,
+                       algos=algoListTrack       ,
                        postscale=1.0  )
-    lineL0 = Hlt2Line('RadiativeTopoPhotonL0'  ,
-                       L0DU=l0filter           ,
+    lineL0 = Hlt2Line('RadiativeTopoPhoton'  ,
+                       L0DU=l0filterphoton     ,
                        HLT=hltfilter           ,
                        prescale=1.0            ,
-                       algos=algoListL0        ,
+                       algos=algoListPhoton        ,
                        postscale=1.0  )
-    HltANNSvc().Hlt2SelectionID.update( { "Hlt2RadiativeTopoTrackTOSDecision" : self.getProp('HltANNSvcID')['RadiativeTopoTrackTOS']} )
-    HltANNSvc().Hlt2SelectionID.update( { "Hlt2RadiativeTopoPhotonL0Decision" : self.getProp('HltANNSvcID')['RadiativeTopoPhotonL0']} )
+    HltANNSvc().Hlt2SelectionID.update( { "Hlt2RadiativeTopoTrackDecision" : self.getProp('HltANNSvcID')['RadiativeTopoTrack']} )
+    HltANNSvc().Hlt2SelectionID.update( { "Hlt2RadiativeTopoPhotonDecision" : self.getProp('HltANNSvcID')['RadiativeTopoPhoton']} )
       
 # EOF
 
