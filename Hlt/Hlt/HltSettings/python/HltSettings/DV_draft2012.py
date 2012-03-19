0c29b261fad2d43849fd3a0004ddf99ea04a6a6b
Victor Coco <victor.coco@cern.ch>
2012-03-19 18:29:10 +0000

    DV_draft2012: update the cuts to meet retention requirements
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@137073 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 62709863cea57b3f8f0af4a2b44eb75a56a62a5f

--- Hlt/Hlt/HltSettings/python/HltSettings/DV_draft2012.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/DV_draft2012.py
@@ -46,63 +46,70 @@ class DV_draft2012 :
         
         from Hlt2Lines.Hlt2DisplVerticesLines  import Hlt2DisplVerticesLinesConf
         d.update( { Hlt2DisplVerticesLinesConf : {
-            "FilterVelo" : False ,
-            "VeloIP" : 0.05 ,
-            "UseVeloTracks": True,
-            "FracE1Track": 1000.,
-            "FracTrackBefV": 1000.,
-            "MaxChi2Long": 1000.,
+            "FilterVelo" : True ,
+            "VeloIP" : -0.05 ,
+            "UseVeloTracks": False,
+            "FracE1Track": 0.85,
+            "FracTrackBefV": 0.49,
+            "MaxChi2Long": 5.,
+            "MaxChi2Down": 10.,
             "MinNbTracks"  : { 'Hlt2RV2P' : 4
-                               , 'Hlt2SingleLonglived' : 5
+                               , 'Hlt2SingleLonglived' : 4
                                , 'Hlt2SingleDown' : 4
+                               , 'Hlt2PreSelSingleDown' : 5
                                , 'Hlt2SingleLonglivedHighFD' :  5
                                , 'Hlt2SingleLonglivedHighMass' : 6
                                , 'Hlt2DoubleLonglived' : 4
                                , 'Hlt2SinglePSLonglived' : 4
                                , 'Hlt2DoublePSLonglived' : 4
-                               , 'Hlt2SinglePSLonglivedHighMass' : 4
+                               , 'Hlt2SinglePSLonglivedHighMass' : 5
                                , 'Hlt2SinglePSLonglivedHighFD' : 5
                                , 'Hlt2SinglePSLonglivedMV' : 4
                                }
             ,  "RMin"         : {   'Hlt2RV2P' : 0.4
-                                    , 'Hlt2SingleLonglived' : 2.5
-                                    , 'Hlt2SingleDown' : 2.0
-                                    , 'Hlt2SingleLonglivedHighFD' : 4.0
-                                    , 'Hlt2SingleLonglivedHighMass' : 0.4
+                                    , 'Hlt2SingleLonglived' : 2.0
+                                    , 'Hlt2SingleDown' : 2.0 ##2.5 dans '3'
+                                    , 'Hlt2PreSelSingleDown' : 0.4
+                                    , 'Hlt2SingleLonglivedHighFD' : 4.
+                                    , 'Hlt2SingleLonglivedHighMass' : 0.7
                                     , 'Hlt2DoubleLonglived' : 0.4 
                                     , 'Hlt2SinglePSLonglived' : 0.4
                                     , 'Hlt2DoublePSLonglived' : 0.4
-                                    , 'Hlt2SinglePSLonglivedHighMass' : 0.4
-                                    , 'Hlt2SinglePSLonglivedHighFD' : 2.0
+                                    , 'Hlt2SinglePSLonglivedHighMass' : 0.65
+                                    , 'Hlt2SinglePSLonglivedHighFD' : 2.5
                                     , 'Hlt2SinglePSLonglivedMV' : 0.4  
                                   }
-            ,  "MinMass"      : {   'Hlt2RV2P' : 3000.
-                                    , 'Hlt2SingleLonglived' : 7000
+            ,  "MinMass"      : {   'Hlt2RV2P' : 2800.
+                                    , 'Hlt2SingleLonglived' : 4000.
                                     , 'Hlt2SingleDown' : 3000
-                                    , 'Hlt2SingleLonglivedHighFD' : 4500.
-                                    , 'Hlt2SingleLonglivedHighMass' : 10000.
-                                    , 'Hlt2DoubleLonglived' : 3000.
-                                    , 'Hlt2SinglePSLonglived' : 3000.
-                                    , 'Hlt2DoublePSLonglived' : 3000.
-                                    , 'Hlt2SinglePSLonglivedHighMass' : 10000.
-                                    , 'Hlt2SinglePSLonglivedHighFD' : 4500.
+                                    , 'Hlt2PreSelSingleDown' : 4500.
+                                    , 'Hlt2SingleLonglivedHighFD' : 3000.
+                                    , 'Hlt2SingleLonglivedHighMass' : 8700.
+                                    , 'Hlt2DoubleLonglived' : 2800.
+                                    , 'Hlt2SinglePSLonglived' : 2800.
+                                    , 'Hlt2DoublePSLonglived' : 2800.
+                                    , 'Hlt2SinglePSLonglivedHighMass' : 8700.
+                                    , 'Hlt2SinglePSLonglivedHighFD' : 3000.
                                     , 'Hlt2SinglePSLonglivedMV' :3000.
+
                                   }
-            ,  "HighestMass"  : {   'Hlt2DoubleLonglived' : 4500.
-                                    ,'Hlt2DoublePSLonglived' : 3000.
+            ,  "HighestMass"  : {   'Hlt2DoubleLonglived' : 3300.
+                                    ,'Hlt2DoublePSLonglived' : 2800.
                                     }
-            ,  "MinSumpt"     :{ 'Hlt2RV2P' : 3000.
-                                 , 'Hlt2SingleLonglived' : 3000.
-                                 , 'Hlt2SingleDown' : 3000.
-                                 , 'Hlt2SingleLonglivedHighFD' : 3000.
-                                 , 'Hlt2SingleLonglivedHighMass' : 3000
-                                 , 'Hlt2DoubleLonglived' : 3000.
-                                 , 'Hlt2SinglePSLonglived' : 3000.
-                                 , 'Hlt2DoublePSLonglived' : 3000.
-                                 , 'Hlt2SinglePSLonglivedHighMass' : 3000.
-                                 , 'Hlt2SinglePSLonglivedHighFD' : 3000.
-                                 , 'Hlt2SinglePSLonglivedMV' :3000.
-                                 } 
+            ,  "MinSumpt"     :{ 'Hlt2RV2P' : 0.
+                                 , 'Hlt2SingleLonglived' : 2600.
+                                 , 'Hlt2SingleDown' : 2600.
+                                 , 'Hlt2PreSelSingleDown' : 2600.
+                                 , 'Hlt2SingleLonglivedHighFD' : 2600.
+                                 , 'Hlt2SingleLonglivedHighMass' : 2600
+                                 , 'Hlt2DoubleLonglived' : 2600.
+                                 , 'Hlt2SinglePSLonglived' : 0.
+                                 , 'Hlt2DoublePSLonglived' : 2600.
+                                 , 'Hlt2SinglePSLonglivedHighMass' : 2600.
+                                 , 'Hlt2SinglePSLonglivedHighFD' : 2600.
+                                 , 'Hlt2SinglePSLonglivedMV' :0.
+                                 }
+            
             ,  "ComputeMatterVeto" : { 'Hlt2RV2P' : True
                                  , 'Hlt2SingleLonglived' : 5
                                  , 'Hlt2SingleLonglivedHighFD' : 5
@@ -124,13 +131,14 @@ class DV_draft2012 :
                               , 'Hlt2SingleLonglivedHighFD' : 1.
                               , 'Hlt2SingleLonglivedHighMass' : 1.
                               , 'Hlt2DoubleLonglived' : 1.
-                              , 'Hlt2SinglePSLonglived' : 0.0001
+                              , 'Hlt2SinglePSLonglived' : 0.0005
                               , 'Hlt2DoublePSLonglived' : 0.01
                               , 'Hlt2SinglePSLonglivedHighMass' : 1.
                               , 'Hlt2SinglePSLonglivedHighFD' : 1.
-                              , 'Hlt2SinglePSLonglivedMV' : 0.0006
+                              , 'Hlt2SinglePSLonglivedMV' : 0.0005
                               }  
-            }})
+            }}   
+                  )
 
         return d
     
