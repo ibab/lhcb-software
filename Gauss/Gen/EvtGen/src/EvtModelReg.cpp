654b33f00da1fc83775e029f3712dcf024626a20
John James Back <j.j.back@warwick.ac.uk>
2012-03-23 15:33:54 +0000

    Added EvtEta2MuMuGamma model from Marcin Chrzaszcz.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@137273 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 648f2b155926b0343b18258f4132371e06888ee8

--- Gauss/Gen/EvtGen/src/EvtModelReg.cpp
+++ Gauss/Gen/EvtGen/src/EvtModelReg.cpp
@@ -139,6 +139,7 @@
 #include "EvtGenModels/EvtDMix.hh"
 #include "EvtGenModels/EvtD0mixDalitz.hh"
 #include "EvtGenModels/EvtD0gammaDalitz.hh"
+#include "EvtGenModels/EvtEta2MuMuGamma.hh"
 
 #include "EvtGenModels/EvtBcToNPi.hh"
 #include "EvtGenModels/EvtBcPsiNPi.hh"
@@ -262,6 +263,7 @@ EvtModelReg::EvtModelReg(const std::list<EvtDecayBase*>* extraModels)
     modelist.registerModel(new EvtDMix);
     modelist.registerModel(new EvtD0mixDalitz);
     modelist.registerModel(new EvtD0gammaDalitz);
+    modelist.registerModel(new EvtEta2MuMuGamma);
 
     modelist.registerModel(new EvtbTosllAli);
     modelist.registerModel(new EvtBaryonPCR);
