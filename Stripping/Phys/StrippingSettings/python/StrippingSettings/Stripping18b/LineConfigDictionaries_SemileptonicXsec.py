930b06a305f762a4691fb6c50e000026ce82af0b
Jibo He <jibo.he@cern.ch>
2012-04-01 20:41:14 +0000

    Copied to Stripping18b
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@137791 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 39c16d1cad2f47eca3268e1924ce57e6565aeafe

---
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping18b/LineConfigDictionaries_SemileptonicXsec.py
@@ -0,0 +1,28 @@
+CrossSectionB2DMuNuX = {
+    'BUILDERTYPE' : 'CrossSectionB2DMuNuXAllLinesConf',
+    'CONFIG' :  {
+     "MINIPCHI2"     : 9.0    # adimensiional
+    ,"TRCHI2"        : 4.0    # adimensiional
+    ,"TRCHI2Loose"   : 5.0    # adimensiional    
+    ,"KaonPIDK"      : 4.0    # adimensiional
+    ,"PionPIDK"      : 10.0   # adimensiional
+    ,"MuonIPCHI2"    : 4.00   # adimensiional
+    ,"MuonPT"        : 800.0  # MeV
+    ,"KPiPT"         : 300.0  # MeV
+    ,"DsDIRA"        : 0.99   # adimensiional
+    ,"DsFDCHI2"      : 100.0  # adimensiional
+    ,"DsMassWin"     : 80.0   # MeV
+    ,"DsAMassWin"    : 100.0  # MeV
+    ,"DsIP"          : 7.4    #mm
+    ,"DsVCHI2DOF"    : 6.0    # adimensiional
+    ,"PIDmu"         : -0.0   # adimensiional
+    ,"BDIRA"         : 0.999  #adimensiional
+    ,"BVCHI2DOF"     : 6.0    # adimensiional
+    ,"DZ"            : 0.0    #mm
+    ,"DDocaChi2Max"  : 20     #adimensiional
+    ,"MINIPCHI2Loose" : 4.0   #adimensiional
+    ,"KaonPIDKloose" : -5     #adimensiional
+     },
+    'STREAMS' : ['ICHEP'] ,
+    'WGs'    : ['Semileptonic']
+    }
