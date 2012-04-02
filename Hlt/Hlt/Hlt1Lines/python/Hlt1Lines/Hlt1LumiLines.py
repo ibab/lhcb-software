87438c606782f93f2c578d04ab3a47b350074f3b
Eric Van Herwijnen <eric.van.herwijnen@cern.ch>
2012-04-02 12:54:45 +0000

    remove rate limit on Hlt1LumiLines.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@137860 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f2d6fb6d329acc5cbf41ac1bdaa3f828393e9642

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1LumiLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1LumiLines.py
@@ -281,7 +281,7 @@ class Hlt1LumiLinesConf(HltLinesConfigurableUser) :
             if i not in HltFactory('ToolSvc.HltFactory').Modules : HltFactory('ToolSvc.HltFactory').Modules += [ i ]
         return Line ( 'Lumi'
                     , prescale = self.prescale
-                    , ODIN = 'scale( ODIN_TRGTYP == LHCb.ODIN.%s , RATE(%s,LoKi.Scalers.RandomPhasePeriodicLimiter)) ' % ( self.getProp('TriggerType'), self.getProp('MaxRate') )
+                    , ODIN = '( ODIN_TRGTYP == LHCb.ODIN.%s ) ' % self.getProp('TriggerType')
                     , algos = self.__create_lumi_algos__( '' )
                     , postscale = self.postscale
                     ) 
