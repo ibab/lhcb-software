8ec206d132b25d1f67af2e800d3a9b2ce795cb68
ocallot <ocallot@cern.ch>
2012-04-02 17:00:48 +0000

    Add BuildMCTrackINfoUpgrade algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@137897 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bdc2429a512525bcc96d32f486d64c6b4d157602

---
+++ Boole/Digi/BooleUMC/src/BuildMCTrackInfoUpgrade.h
@@ -0,0 +1,93 @@
+// $Id: $
+#ifndef BUILDMCTRACKINFOUPGRADE_H 
+#define BUILDMCTRACKINFOUPGRADE_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+#include "Event/MCTrackInfo.h"
+
+class DeVelo;
+class DeVeloPix;
+class DeSTDetector;
+class DeOTStation;
+
+/** @class BuildMCTrackInfoUpgrade BuildMCTrackInfoUpgrade.h
+ *  Build the Reconstructable MCProperty table.
+ *
+ *  @author Olivier Callot
+ *  @date   2012-04-02 : Updated version for upgrade: IT, OT and FT optional, Pixel/normal Velo
+ */
+class BuildMCTrackInfoUpgrade : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  BuildMCTrackInfoUpgrade( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~BuildMCTrackInfoUpgrade( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+
+protected:
+
+  void updateBit( int& result, int sta, bool isX ) {
+    if ( 0 == sta ) {
+      result |= MCTrackInfo::maskTT1;
+    } else if ( 1 == sta ) {
+      result |= MCTrackInfo::maskTT2;
+    } else if ( 2 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskT1X;
+      else       result |= MCTrackInfo::maskT1S;
+    } else if ( 3 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskT2X;
+      else       result |= MCTrackInfo::maskT2S;
+    } else if ( 4 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskT3X;
+      else       result |= MCTrackInfo::maskT3S;
+    }
+  }
+  
+
+  void updateAccBit( int& result, int sta, bool isX ) {
+    if ( 0 == sta ) {
+      result |= MCTrackInfo::maskAccTT1;
+    } else if ( 1 == sta ) {
+      result |= MCTrackInfo::maskAccTT2;
+    } else if ( 2 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskAccT1X;
+      else       result |= MCTrackInfo::maskAccT1S;
+    } else if ( 3 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskAccT2X;
+      else       result |= MCTrackInfo::maskAccT2S;
+    } else if ( 4 == sta ) {
+      if ( isX ) result |= MCTrackInfo::maskAccT3X;
+      else       result |= MCTrackInfo::maskAccT3S;
+    }
+  }
+  
+  void computeAcceptance ( std::vector<int>& station ) ;
+
+  ///< method for sorting the VeloPix clusters. Should be in the VeloPixCluster class!
+  struct increasingSensor {
+     bool operator() ( LHCb::VeloPixCluster* clust1, LHCb::VeloPixCluster* clust2 ) const{
+       return clust1->channelID().sensor() < clust2->channelID().sensor();
+     }
+  };
+
+private:
+  bool m_withPixel;
+  bool m_withIT;
+  bool m_withOT;
+  bool m_withFT;
+
+  DeVelo*       m_velo;
+  DeVeloPix*    m_veloPix;
+
+  DeSTDetector* m_ttDet;
+  DeSTDetector* m_itDet;
+
+  // OT Detector information
+  DeOTDetector* m_otDet;
+};
+#endif // BUILDMCTRACKINFOUPGRADE_H
