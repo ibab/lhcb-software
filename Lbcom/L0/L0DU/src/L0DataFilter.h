e478cf04cabecdb6bbeaf7f3988d56fa71546fbb
Olivier Deschamps <odescham@cern.ch>
2012-04-10 16:45:17 +0000

    new algorithms
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@138284 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 148b41d00c03705f0c3ef4848ca4052cec77a8d2

---
+++ Lbcom/L0/L0DU/src/L0DataFilter.h
@@ -0,0 +1,34 @@
+// $Id: L0DataFilter.h,v 1.6 2010-05-26 10:46:21 odescham Exp $
+#ifndef L0DATAFILTER_H 
+#define L0DATAFILTER_H 1
+
+// Include files
+#include "L0Base/L0AlgBase.h"
+
+
+/** @class L0DataFilter L0DataFilter.h
+ *  
+ *
+ *  @author Olivier Deschamps
+ *  @date   2012-04-10
+ */
+class L0DataFilter : public L0AlgBase {
+public: 
+  /// Standard constructor
+  L0DataFilter( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~L0DataFilter( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  std::string m_l0Location;
+  std::string m_logical;
+  std::map<std::string,std::vector<std::string> > m_selection ; 
+  bool m_revert;
+};
+#endif // L0DATAFILTER_H
