efaa2f455a75eaaa2c119665f7dbcb092efce281
Marco Cattaneo <marco.cattaneo@cern.ch>
2012-04-13 14:18:50 +0000

    v42r2p2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@138393 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef54a230c3cbf3924267680a9127c8f21e2e3ea1

---
+++ Brunel/Rec/Brunel/tests/options/2012-crashes.py
@@ -0,0 +1,16 @@
+# Pathological events from 2012 that caused Brunel crashes or huge processing times.
+from Gaudi.Configuration import FileCatalog
+from Configurables import Brunel
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
+
+#-- Use latest 2012 database tags for real data
+Brunel().DataType = "2012"
+
+# Three events with uge processing times, fixed in Brunel v41r2p2
+from GaudiConf.IOHelper import IOHelper
+IOHelper("MDF").inputFiles( [
+  "DATAFILE='root:root://castorlhcb.cern.ch//castor/cern.ch/user/c/cattanem/testFiles/111473_0000000110_evt11603.raw?svcClass=default'"
+, "DATAFILE='root:root://castorlhcb.cern.ch//castor/cern.ch/user/c/cattanem/testFiles/111702_0000000001_evt110102.raw?svcClass=default'"
+, "DATAFILE='root:root://castorlhcb.cern.ch//castor/cern.ch/user/c/cattanem/testFiles/111800_0000000026_evt16533.raw?svcClass=default'" ] )
