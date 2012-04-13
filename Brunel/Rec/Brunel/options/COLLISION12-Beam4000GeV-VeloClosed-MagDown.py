51ae7c1d27b6508a1f20c5f0c3489edc1401f102
Marco Cattaneo <marco.cattaneo@cern.ch>
2012-04-13 15:26:15 +0000

    use a 2012 file for the 2012 test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@138402 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 23127d1e41c5552a04a9a0435abf7c5868ff6887

--- Brunel/Rec/Brunel/options/COLLISION12-Beam4000GeV-VeloClosed-MagDown.py
+++ Brunel/Rec/Brunel/options/COLLISION12-Beam4000GeV-VeloClosed-MagDown.py
@@ -12,10 +12,6 @@ FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml" ]
 #-- Use latest 2012 database tags for real data
 Brunel().DataType = "2012"
 
-# BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE
-# For time being this is a 2011 file, until we get 2012 data!
-# Events from run 87968 from fill 1647, on 24th March 2011
-# BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE BEWARE
 EventSelector().Input = [
-  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/87968/087968_0000000001.raw?svcClass=lhcbdisk' SVC='LHCb::MDFSelector'"
+  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2012/RAW/FULL/LHCb/COLLISION12/112181/112181_0000000182.raw?svcClass=lhcbdisk' SVC='LHCb::MDFSelector'"
     ]
