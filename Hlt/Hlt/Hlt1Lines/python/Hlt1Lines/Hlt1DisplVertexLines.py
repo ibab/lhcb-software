4cb0158daa0b1ff1107843e5f097db9a0af056bb
Pieter David <pieter.david@cern.ch>
2012-04-15 19:48:24 +0000

    Add protection against large combinatorics to displaced vertex line
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@138421 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: abc3a10ba22ef55945360f294dd893ffcc948083

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1DisplVertexLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1DisplVertexLines.py
@@ -4,27 +4,41 @@
 #  @@author Pieter David pieter.david@cern.ch
 #  @@date 2012-03-15
 # =============================================================================
-from Gaudi.Configuration import * 
+from Gaudi.Configuration import *
 from HltLine.HltLinesConfigurableUser import *
 
 class Hlt1DisplVertexLinesConf( HltLinesConfigurableUser ):
     # steering variables
-    __slots__ = { 
+    __slots__ = {
         # Displaced Vertex line
-        "DoTiming"                      : False
-        , "TrackDisplVertex_GEC"        : "Loose"
-        , "VertexDisplVertex_DOCABL"    : "2.0*mm"
-        , "VertexDisplVertex_VELO_NSP"  : "3"
-        , "VertexDisplVertex_VELO_NCSP" : "2"
-        , "VertexDisplVertex_VELOTrChi2": "2.5"
-        , "VertexDisplVertex_VX_DOCA"   : "0.3*mm"
-        , "VertexDisplVertex_VX_CHI2"   : "1000000000.0"
-        , "VertexDisplVertex_VX_RHO"    : "12.0*mm"
-        , "VertexDisplVertex_Tr_P"      : "10.0*GeV"
-        , "VertexDisplVertex_Tr_PT"     : "1.7*GeV"
-        , "VertexDisplVertex_Tr_CHI2"   : "2.5"
-        , "VertexDisplVertex_GEC"       : "Loose"
-        , "Priorities"                  : { "VertexDisplVertex"  : 128 }
+        "DoTiming"                          : False
+        , "VertexDisplVertex_DOCABL"        : "2.0*mm"
+        , "VertexDisplVertex_VELO_NSP"      : "3"
+        , "VertexDisplVertex_VELO_NCSP"     : "2"
+        , "VertexDisplVertex_VELOTrChi2"    : "2.5"
+        , "VertexDisplVertex_VX_DOCA"       : "0.3*mm"
+        , "VertexDisplVertex_VX_CHI2"       : "1000000000.0"
+        , "VertexDisplVertex_VX_RHO"        : "12.0*mm"
+        , "VertexDisplVertex_Tr_P"          : "10.0*GeV"
+        , "VertexDisplVertex_Tr_PT"         : "1.7*GeV"
+        , "VertexDisplVertex_Tr_CHI2"       : "2.5"
+        , "VertexDisplVertex_GEC"           : "Loose"
+        , "VertexDisplVertex_TracksForVx"   : "50"
+        ## Prescaled line without cut on the number of tracks before vertexing
+        , "VertexDisplVertexPS_DOCABL"      : "2.0*mm"
+        , "VertexDisplVertexPS_VELO_NSP"    : "3"
+        , "VertexDisplVertexPS_VELO_NCSP"   : "2"
+        , "VertexDisplVertexPS_VELOTrChi2"  : "2.5"
+        , "VertexDisplVertexPS_VX_DOCA"     : "0.3*mm"
+        , "VertexDisplVertexPS_VX_CHI2"     : "1000000000.0"
+        , "VertexDisplVertexPS_VX_RHO"      : "12.0*mm"
+        , "VertexDisplVertexPS_Tr_P"        : "10.0*GeV"
+        , "VertexDisplVertexPS_Tr_PT"       : "1.7*GeV"
+        , "VertexDisplVertexPS_Tr_CHI2"     : "2.5"
+        , "VertexDisplVertexPS_GEC"         : "Loose"
+        , "Priorities"                    : { "VertexDisplVertex"   : 128
+                                            , "VertexDisplVertexPS" : 128 }
+        , "Prescale"                      : { "VertexDisplVertexPS" : 0.001 }
         }
 
     def localise_props( self, prefix ):
@@ -51,6 +65,24 @@ class Hlt1DisplVertexLinesConf( HltLinesConfigurableUser ):
         unit.Code = code
         return unit
 
+    def vertexDisplVertex_trackPreambulo( self, properties ):
+        from HltTracking.Hlt1TrackUpgradeConf import VeloCandidates
+        return [ VeloCandidates( properties["name"] ) ]
+
+    def vertexDisplVertex_vertexPreambulo( self, properties ):
+        from HltTracking.Hlt1VertexUpgradeConf import ( LooseForward
+                                                      , FitTracks
+                                                      , VeloOnlyFitTracks
+                                                      )
+
+        preambulo = [ LooseForward(allowFail=True)
+                    , FitTracks(allowFail=True)
+                    , VeloOnlyFitTracks(allowFail=True)
+                    , "MakeDisplVertices = TC_VXMAKE4   ( '', LoKi.Hlt1.VxMakerConf( %(VX_DOCA)s, %(VX_CHI2)s ) )" % properties
+                    ]
+
+        return preambulo
+
     def vertexDisplVertex_preambulo( self, properties ):
         from HltTracking.Hlt1TrackUpgradeConf import VeloCandidates
         from HltTracking.Hlt1VertexUpgradeConf import ( LooseForward
@@ -70,53 +102,73 @@ class Hlt1DisplVertexLinesConf( HltLinesConfigurableUser ):
     def vertexDisplVertex_streamer( self, properties ):
         from Hlt1Lines.Hlt1GECs import Hlt1GECUnit
         from Configurables import LoKi__HltUnit as HltUnit
-        unit = HltUnit(
-                "Hlt1%(name)sStreamer" % properties
-                ##, OutputLevel = 1
-                , Preambulo = self.vertexDisplVertex_preambulo( properties )
-                , Code = """
-                VeloCandidates
-                >>  tee ( monitor( TC_SIZE>0, '# events pass Velo candidates', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo candidates', LoKi.Monitoring.ContextSvc ) )
-                >>  ( ( monitor( TrNCONSECVELOSPACEPOINTS , Gaudi.Histo1DDef( 'Number of consecutive Velo spacepoints on track', 0., 10., 10 ) , 'hNConsecVelo' ) > %(VELO_NCSP)s ) & ( monitor( TrNVELOSPACEPOINTS , Gaudi.Histo1DDef( 'Number of Velo spacepoints on track', 0., 10., 10 ) , 'hNVelo' ) > %(VELO_NSP)s ) )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass Velo spacepoints cuts', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo spacepoints cuts', LoKi.Monitoring.ContextSvc ) )
-                >>  ( monitor( Tr_FASTDOCATOBEAMLINE(5.) , Gaudi.Histo1DDef( 'DOCA to beamline', 0., 30.*mm, 300 ) , 'hDOCABL' ) > %(DOCABL)s )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass DOCABL cut', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass DOCABL cut', LoKi.Monitoring.ContextSvc ) )
-                >>  MakeDisplVertices
-                >>  tee ( monitor( TC_SIZE>0, '# events pass displaced vertex making', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass displaced vertex making', LoKi.Monitoring.ContextSvc ) )
-                >>  ( monitor( VX_BEAMSPOTRHO(5.) , Gaudi.Histo1DDef( 'RHO to beamline for all created vertices', 0., 30.*mm, 300 ), 'hRHO_all' ) > %(VX_RHO)s )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass RHO cut', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass RHO cut', LoKi.Monitoring.ContextSvc ) )
-                >>  LooseForward
-                >>  tee ( monitor( TC_SIZE>0, '# events pass forward', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass forward', LoKi.Monitoring.ContextSvc ) )
-                >>  HLT_TCASRVC_ANY( ( monitor( TrPT , Gaudi.Histo1DDef( 'Transverse momentum', 0., 100.*GeV, 1000 ) , 'hPT' ) > %(Tr_PT)s ) & ( TrP > %(Tr_P)s), True )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass P, PT cut', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass P, PT cut', LoKi.Monitoring.ContextSvc ) )
-                >>  FitTracks
-                >>  tee ( monitor( TC_SIZE>0, '# events pass track fit', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass track fit', LoKi.Monitoring.ContextSvc ) )
-                >>  HLT_TCASRVC_ANY( ( monitor( TrCHI2PDOF , Gaudi.Histo1DDef( 'Track Chi2 per degree of freedom', 0., 10., 100 ) , 'hTrChi2' ) < %(Tr_CHI2)s ), True )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass track CHI2 cut', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass track CHI2', LoKi.Monitoring.ContextSvc ) )
-                >>  SINK ( 'Hlt1%(name)sDecision' )
-                >> ~TC_EMPTY
-                  """ % properties
-                )
-        """     # Velo-only track fit for before PatForward
-                >>  VeloOnlyFitTracks
-                >>  tee ( monitor( TC_SIZE>0, '# events pass Velo-only track fit', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo-only track fit', LoKi.Monitoring.ContextSvc ) )
-                >>  HLT_TCASRVC_ANY( ( monitor( TrCHI2PDOF , Gaudi.Histo1DDef( 'Velo-only track Chi2 per degree of freedom', 0., 10., 100 ) , 'hVeloOnlyTrChi2' ) < %(VELOTrChi2)s ), True )
-                >>  tee ( monitor( TC_SIZE>0, '# events pass Velo-only track chi2 cut', LoKi.Monitoring.ContextSvc ) )
-                >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo-only track chi2 cut', LoKi.Monitoring.ContextSvc ) )
-                >>  Dump ( 'Before PatForward' , 2 )
-        """
+        from HltLine.HltLine import bindMembers
+
+        algos = []
+
         gec = properties["GEC"]
-        return [ Hlt1GECUnit( gec ), unit ]
+        algos.append( Hlt1GECUnit( properties["GEC"] ) )
+
+        trackUnit = HltUnit(
+            "Hlt1%(name)sTrackStreamer" % properties
+          , Preambulo = self.vertexDisplVertex_trackPreambulo( properties )
+          , Code = """
+              VeloCandidates
+              >>  tee ( monitor( TC_SIZE>0, '# events pass Velo candidates', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo candidates', LoKi.Monitoring.ContextSvc ) )
+              >>  ( ( TrNCONSECVELOSPACEPOINTS > %(VELO_NCSP)s ) & ( TrNVELOSPACEPOINTS > %(VELO_NSP)s ) )
+              >>  tee ( monitor( TC_SIZE>0, '# events pass Velo spacepoints cuts', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass Velo spacepoints cuts', LoKi.Monitoring.ContextSvc ) )
+              >>  ( Tr_FASTDOCATOBEAMLINE(5.) > %(DOCABL)s )
+              >>  tee ( monitor( TC_SIZE>0, '# events pass DOCABL cut', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass DOCABL cut', LoKi.Monitoring.ContextSvc ) )
+              >>  SINK ( 'Hlt1%(name)sTracks' )
+              >> ~TC_EMPTY
+              """ % properties
+          )
+        algos.append( trackUnit )
+
+        if "TracksForVx" in properties:
+            combinatoricsGECUnit = HltUnit(
+                "Hlt1%(name)sCombinatoricsCut" % properties
+              , Code = """
+                  SELECTION('Hlt1%(name)sTracks')
+                  >> monitor( TC_SIZE , Gaudi.Histo1DDef('# tracks for vertexing', 0., 100., 100 ), 'hNTracksForVertexing%(name)s' ) < %(TracksForVx)s
+                  """ % properties
+            )
+            algos.append( combinatoricsGECUnit )
+
+        vertexUnit = HltUnit(
+            "Hlt1%(name)sStreamer" % properties
+          , Preambulo = self.vertexDisplVertex_vertexPreambulo( properties )
+          , Code = """
+              SELECTION ( 'Hlt1%(name)sTracks' )
+              >>  MakeDisplVertices
+              >>  tee ( monitor( TC_SIZE>0, '# events pass displaced vertex making', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass displaced vertex making', LoKi.Monitoring.ContextSvc ) )
+              >>  ( VX_BEAMSPOTRHO(5.) > %(VX_RHO)s )
+              >>  tee ( monitor( TC_SIZE>0, '# events pass RHO cut', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass RHO cut', LoKi.Monitoring.ContextSvc ) )
+              >>  LooseForward
+              >>  tee ( monitor( TC_SIZE>0, '# events pass forward', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass forward', LoKi.Monitoring.ContextSvc ) )
+              >>  HLT_TCASRVC_ANY( ( TrPT > %(Tr_PT)s ) & ( TrP > %(Tr_P)s), True )
+              >>  tee ( monitor( TC_SIZE>0, '# events pass P, PT cut', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass P, PT cut', LoKi.Monitoring.ContextSvc ) )
+              >>  FitTracks
+              >>  tee ( monitor( TC_SIZE>0, '# events pass track fit', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass track fit', LoKi.Monitoring.ContextSvc ) )
+              >>  HLT_TCASRVC_ANY( ( TrCHI2PDOF < %(Tr_CHI2)s ), True )
+              >>  tee ( monitor( TC_SIZE>0, '# events pass track CHI2 cut', LoKi.Monitoring.ContextSvc ) )
+              >>  tee ( monitor( TC_SIZE  , '# candidates pass track CHI2', LoKi.Monitoring.ContextSvc ) )
+              >>  SINK ( 'Hlt1%(name)sDecision' )
+              >> ~TC_EMPTY
+              """ % properties
+          )
+
+        algos.append( vertexUnit )
+
+        return bindMembers( "Hlt1%(name)sAlgos" % properties, algos ).setOutputSelection( "Hlt1%(name)sDecision" )
 
     def build_line( self, name, streamer ):
         from HltLine.HltLine import Hlt1Line
@@ -130,9 +182,10 @@ class Hlt1DisplVertexLinesConf( HltLinesConfigurableUser ):
                        , L0DU      = "L0_DECISION_PHYSICS"
                        , algos     = algos
                        )
-    
+
     def __apply_configuration__( self ):
         ## create the lines
-        to_build = [ ( "VertexDisplVertex", self.vertexDisplVertex_streamer ) ]
+        to_build = [ ( "VertexDisplVertex"  , self.vertexDisplVertex_streamer )
+                   , ( "VertexDisplVertexPS", self.vertexDisplVertex_streamer ) ]
         for line, streamer in to_build:
             self.build_line( line, streamer )
