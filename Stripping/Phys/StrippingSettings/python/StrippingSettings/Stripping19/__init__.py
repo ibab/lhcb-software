f284d3aa016ae29935e2b4bc10ed34fca740f79c
Jibo He <jibo.he@cern.ch>
2012-04-19 10:04:10 +0000

    Copied Stripping18 to Stripping19
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@138586 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f1e4771ee91ceb039ad21008400a94b512f814bc

---
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping19/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [ 'LineConfigDictionaries_B2CC',
+            'LineConfigDictionaries_B2OC',
+            'LineConfigDictionaries_BandQ',
+            'LineConfigDictionaries_Calib',
+            'LineConfigDictionaries_Charmless',
+            'LineConfigDictionaries_Charm',
+            'LineConfigDictionaries_MiniBias',
+            'LineConfigDictionaries_QEE',
+            'LineConfigDictionaries_RD',
+            'LineConfigDictionaries_Semileptonic',
+            'LineConfigDictionaries_CharmXsec',
+            'LineConfigDictionaries_BandQXsec',
+            'LineConfigDictionaries_SemileptonicXsec'
+            ]
+
