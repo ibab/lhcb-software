1840e222a075edc10544175c010f52f514f54c04
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2012-04-23 15:48:08 +0000

    EventTimeMonitor
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@138776 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 203bcb50783d8cbd1a879e8e57bcf65881d9ef90

---
+++ Rec/Rec/RecAlgs/src/EventTimeMonitor.cpp
@@ -0,0 +1,107 @@
+// $Id: $
+// Include files 
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+// local
+#include "EventTimeMonitor.h"
+#include "Event/ODIN.h" // event & run number
+#include <boost/assign/list_of.hpp>
+#include "AIDA/IHistogram1D.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : EventTimeMonitor
+//
+// 2012-04-19 : Patrick Koppenburg
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( EventTimeMonitor );
+
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+EventTimeMonitor::EventTimeMonitor( const std::string& name,
+                                    ISvcLocator* pSvcLocator)
+  : GaudiHistoAlg ( name , pSvcLocator )
+  , m_histoS ( "GPS Seconds" ,  0 , 3600., 3600 ) 
+  , m_histoH ( "GPS Hours" , -0.5 , 23.5,   24 ) 
+  , m_histoD ( "GPS Days" ,  0.5 , 365.5, 366 ) 
+  , m_histoY ( "GPS Year" , 2008.5 , 2028.5, 20 ) 
+  , m_plotS()
+  , m_plotH()
+  , m_plotD()
+  , m_plotY()
+{
+  declareProperty 
+    ( "SecondsPlot" , 
+      m_histoS , 
+      "The parameters of 'delta memory' histogram" ) ;
+
+}
+//=============================================================================
+// Destructor
+//=============================================================================
+EventTimeMonitor::~EventTimeMonitor() {} 
+
+//=============================================================================
+// Initialization
+//=============================================================================
+StatusCode EventTimeMonitor::initialize() {
+  StatusCode sc = GaudiHistoAlg::initialize(); // must be executed first
+  if ( sc.isFailure() ) return sc;  // error printed already by GaudiHistoAlg
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  if ( produceHistos() ) { 
+    m_plotS  = book    ( m_histoS      ) ; 
+    m_plotH  = book    ( m_histoH      ) ; 
+    m_plotD  = book    ( m_histoD      ) ; 
+    m_plotY  = book    ( m_histoY      ) ; 
+  }
+  m_moffsets = boost::assign::list_of(0)(21)(60)(91)(121)(152)(182)(213)(244)(274)(305)(335);
+  
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode EventTimeMonitor::execute() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
+  // Load the ODIN
+  LHCb::ODIN* odin(NULL);
+  if( exist<LHCb::ODIN>( LHCb::ODINLocation::Default ) )
+  {
+    odin = get<LHCb::ODIN>( LHCb::ODINLocation::Default );
+  }
+  else if( exist<LHCb::ODIN>( LHCb::ODINLocation::Default, false ) )
+  {
+    odin = get<LHCb::ODIN>( LHCb::ODINLocation::Default, false );
+  } 
+  else 
+  {
+    // should always be available ...
+    return Error( "Cannot load the ODIN data object", StatusCode::SUCCESS );
+  }
+  const Gaudi::Time gtime = odin->eventTime();
+  m_plotY->fill(gtime.year(false));
+  m_plotD->fill(m_moffsets[gtime.month(false)]+gtime.day(false));
+  m_plotH->fill(gtime.hour(false));
+  m_plotS->fill(60*gtime.minute(false)+gtime.second(false)+gtime.nsecond()/1000000000.);
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
+//  Finalize
+//=============================================================================
+StatusCode EventTimeMonitor::finalize() {
+
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
+
+  return GaudiHistoAlg::finalize();  // must be called after all other actions
+}
+
+//=============================================================================
---
+++ Rec/Rec/RecAlgs/src/EventTimeMonitor.h
@@ -0,0 +1,42 @@
+// $Id: $
+#ifndef EVENTTIMEMONITOR_H 
+#define EVENTTIMEMONITOR_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiHistoAlg.h"
+
+
+/** @class EventTimeMonitor EventTimeMonitor.h
+ *  
+ *  Creates histograms of event time
+ *
+ *  @author Patrick Koppenburg
+ *  @date   2012-04-19
+ */
+class EventTimeMonitor : public GaudiHistoAlg {
+public: 
+  /// Standard constructor
+  EventTimeMonitor( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~EventTimeMonitor( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+private:
+  /// the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histoS ;         // the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histoH ;         // the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histoD ;         // the histogram definition (as property) 
+  Gaudi::Histo1DDef m_histoY ;         // the histogram definition (as property) 
+  AIDA::IHistogram1D* m_plotS ;                // the histogram of seconds
+  AIDA::IHistogram1D* m_plotH ;                // the histogram of hours
+  AIDA::IHistogram1D* m_plotD ;                // the histogram of day of year
+  AIDA::IHistogram1D* m_plotY ;                // the histogram of year
+  std::vector<unsigned int> m_moffsets ; // month offsets. Isn't theer a method doing that?
+};
+#endif // EVENTTIMEMONITOR_H
