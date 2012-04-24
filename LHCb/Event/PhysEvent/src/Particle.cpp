b910067dcc916578c8db2242e4aff6ade942a5c3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-04-24 21:34:29 +0000

    Change default initialisation value for the reference point in the    Particle class from (0,0,-1km) to (0,0,-100m), to be within the range the    Particle length scale packing can handle.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@138953 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5265862a6c458a56b88a5c300815b6108eede425

--- LHCb/Event/PhysEvent/src/Particle.cpp
+++ LHCb/Event/PhysEvent/src/Particle.cpp
@@ -1,5 +1,5 @@
 // $Id: Particle.cpp,v 1.24 2006-07-28 06:55:48 jpalac Exp $
-// Include files 
+// Include files
 
 // STD and STL
 #include <iostream> // from Gaudi
@@ -10,25 +10,12 @@
 #include "LHCbMath/MatrixManip.h"
 
 //-----------------------------------------------------------------------------
-// Implementation file for class : Particle
-//                                 derived from class KeyedObjeect
-//
-// 19/03/2002 : Gloria Corti
-// 25/07/2005 : P. Koppenburg
-// 28/07/2006 : Juan Palacios
-// 
-// WARNING: AS OF 28/07/2006 THE CODE BELOW CAUSES DICTIONARY GENERATION
-//          PROBLEMS ON WINDOWS WHEN INCLUDED IN THE Particle.h FILE.
-//          DO NOT REMOVE THIS FILE UNLESS YOU ARE SURE THIS PROBLEM HAS BEEN
-//          FIXED. J. Palacios 28/07/2006 
-//-----------------------------------------------------------------------------
 
 //=============================================================================
 // Create and return the covariance matrix
 //=============================================================================
-Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const 
+Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const
 {
-
   Gaudi::Matrix7x7 full;
 
   full.Place_at(m_posCovMatrix,0,0);
@@ -36,7 +23,7 @@ Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const
   full.Place_at(m_posMomCovMatrix,3,0);
   full.Place_at(ROOT::Math::Transpose(m_posMomCovMatrix), 0,3);
 
-   return Gaudi::SymMatrix7x7( full.LowerBlock() );
-	
+  return Gaudi::SymMatrix7x7( full.LowerBlock() );
 }
+
 //=============================================================================
