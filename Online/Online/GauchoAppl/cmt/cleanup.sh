ae63181d55522e6d7f6ee8a3b3e3509e0efcaf2f
Beat Jost <beat.jost@cern.ch>
2012-04-24 09:02:43 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@138864 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: cc11cfea57712656f470212d91935600e64f6bde

--- Online/Online/GauchoAppl/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/sw/lib/contrib/CMT/v1r20p20090520; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=GauchoAppl -version=head -path=/home/beat/cmtuser/myOnline/Online $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
