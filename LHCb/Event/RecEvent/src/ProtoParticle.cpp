f250729a7c1df03a0b202b300752f0c112980ae1
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-04-26 09:34:52 +0000

    ! 2012-04-26 - Chris Jones  - Improvements to the fillStream methods for VertexBase  - Change the default initialisation for VertexBase::position from    (0,0,-1*km) to (0,0,-100*m) to avoid packing truncation issues.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@139068 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5fb9a55510576b1433553b2c38ab9f3fb40a2597

--- LHCb/Event/RecEvent/src/ProtoParticle.cpp
+++ LHCb/Event/RecEvent/src/ProtoParticle.cpp
@@ -24,8 +24,8 @@ std::ostream& LHCb::ProtoParticle::fillStream( std::ostream & s ) const
     << " RichPID "   << this->richPID()
     << " MuonPID "   << this->muonPID()
     << " ExtraInfo [";
-  for ( ExtraInfo::const_iterator i = this->extraInfo().begin();
-        i != this->extraInfo().end(); ++i )
+  for ( ExtraInfo::const_iterator i = extraInfo().begin();
+        i != extraInfo().end(); ++i )
   {
     const LHCb::ProtoParticle::additionalInfo info =
       static_cast<LHCb::ProtoParticle::additionalInfo>(i->first);
@@ -42,24 +42,21 @@ LHCb::ProtoParticle::clearCalo( const LHCb::CaloHypo::Hypothesis & hypo )
   // number of hypos removed
   LHCb::ProtoParticle::ExtraInfo::size_type removed = 0;
 
-
-  
-  const SmartRefVector<LHCb::CaloHypo>::iterator it = std::remove ( m_calo.begin(), m_calo.end(), 0 );
-
-  // remove selected hypos
-  if ( m_calo.end() != it ){
+  // remove NULL hypos
+  const SmartRefVector<LHCb::CaloHypo>::iterator it = 
+    std::remove ( m_calo.begin(), m_calo.end(), 0 );
+  if ( m_calo.end() != it )
+  {
     removed += m_calo.end() - it;
     m_calo.erase ( it , m_calo.end() );
   }
-
 
   // Find hypos to remove
   const SmartRefVector<LHCb::CaloHypo>::iterator iHypoRemove =
     std::remove_if ( m_calo.begin(), m_calo.end(),
                      bind( &LHCb::CaloHypo::hypothesis, _1 ) == hypo );
-
-  // remove selected hypos
-  if ( m_calo.end() != iHypoRemove ){
+  if ( m_calo.end() != iHypoRemove )
+  {
     removed += m_calo.end() - iHypoRemove;
     m_calo.erase ( iHypoRemove , m_calo.end() );
   }
---
+++ LHCb/Event/RecEvent/src/VertexBase.cpp
@@ -0,0 +1,25 @@
+// $Id: $
+
+#include "Event/VertexBase.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : VertexBase
+//
+// 2012-04-26 : Chris Jones
+//-----------------------------------------------------------------------------
+
+std::ostream& LHCb::VertexBase::fillStream(std::ostream& s) const
+{
+  s << "{ "
+    << "position  : " << m_position << std::endl
+    << "covMatrix :	" << std::endl << m_covMatrix << std::endl
+    << "chi2      : " << m_chi2 << std::endl
+    << "nDoF      : " << m_nDoF << std::endl
+    << "extraInfo :	[";
+  for ( ExtraInfo::const_iterator i = extraInfo().begin();
+        i != extraInfo().end(); ++i )
+  {
+    s << " " << i->first << "=" << i->second;
+  }
+  return s << " ] }";
+}
