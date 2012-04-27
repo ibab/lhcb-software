3977a620e3a8b586022399c30732a1747231c6e8
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-04-27 19:25:09 +0000

    fix Particle printout for Particles with extra info entries with keys > 10000
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@139252 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4d9abd4b2184e43e43ef8131e8af07b66b2d3a39

--- LHCb/Event/PhysEvent/src/Particle.cpp
+++ LHCb/Event/PhysEvent/src/Particle.cpp
@@ -1,19 +1,14 @@
-// $Id: Particle.cpp,v 1.24 2006-07-28 06:55:48 jpalac Exp $
-// Include files
 
-// STD and STL
-#include <iostream> // from Gaudi
+// STD
+#include <iostream>
 
 // local
 #include "Event/Particle.h"
 
 #include "LHCbMath/MatrixManip.h"
 
-//-----------------------------------------------------------------------------
+//=============================================================================
 
-//=============================================================================
-// Create and return the covariance matrix
-//=============================================================================
 Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const
 {
   Gaudi::Matrix7x7 full;
@@ -21,7 +16,7 @@ Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const
   full.Place_at(m_posCovMatrix,0,0);
   full.Place_at(m_momCovMatrix,3,3);
   full.Place_at(m_posMomCovMatrix,3,0);
-  full.Place_at(ROOT::Math::Transpose(m_posMomCovMatrix), 0,3);
+  full.Place_at(ROOT::Math::Transpose(m_posMomCovMatrix),0,3);
 
   return Gaudi::SymMatrix7x7( full.LowerBlock() );
 }
@@ -30,7 +25,7 @@ Gaudi::SymMatrix7x7 LHCb::Particle::covMatrix() const
 
 std::ostream& LHCb::Particle::fillStream(std::ostream& s) const
 {
-  s << "{ " 
+  s << "{ "
     << "particleID      : " << m_particleID << std::endl
     << "measuredMass    : " << m_measuredMass << std::endl
     << "measuredMassErr : " << m_measuredMassErr << std::endl
@@ -39,13 +34,22 @@ std::ostream& LHCb::Particle::fillStream(std::ostream& s) const
     << "momCovMatrix    : " << std::endl << m_momCovMatrix << std::endl
     << "posCovMatrix    : " << std::endl << m_posCovMatrix << std::endl
     << "posMomCovMatrix : " << std::endl << m_posMomCovMatrix << std::endl
-    << "extraInfo       : ["; 
+    << "extraInfo       : [";
   for ( ExtraInfo::const_iterator i = extraInfo().begin();
         i != extraInfo().end(); ++i )
   {
-    const LHCb::Particle::additionalInfo info =
-      static_cast<LHCb::Particle::additionalInfo>(i->first);
-    s << " " << info << "=" << i->second;
+    if ( (*i).first < (int)LHCb::Particle::LastGlobal )
+    {
+      const LHCb::Particle::additionalInfo info =
+        static_cast<LHCb::Particle::additionalInfo>((*i).first);
+      s << " " << info << "=" << (*i).second;
+    }
+    else
+    {
+      s << " " << (*i).first << "=" << (*i).second;
+    }
   }
   return s << " ] }";
 }
+
+//=============================================================================
