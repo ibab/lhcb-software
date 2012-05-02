fce418c6301ccdf2bf418f5da507108fb44f8921
Vanya Belyaev <ivan.belyaev@cern.ch>
2012-05-02 19:10:35 +0000

    move microDST to BenderTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@139523 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 927a3b87f19b5a01e51318bc058217ec9201356b

--- Bender/Phys/Bender/python/Bender/MicroDST.py
+++ Bender/Phys/Bender/python/Bender/MicroDST.py
@@ -48,84 +48,17 @@ with the smear campaign of Dr.O.Callot et al.:
 __author__  = "Vanya BELYAEV Ivan.Belyaev@itep.ru"
 __date__    = "2011-10-05"
 __version__ = "$Revision$"
-__all__     = ( 'trunkConf' , )
+__all__     = ( 'uDstConf' , )
 # =============================================================================
 ## logging
 # =============================================================================
 from Bender.Logger import getLogger 
 logger = getLogger( __name__ )
 # =============================================================================
-## configure variopsi uDst settings from RootInTES 
-def uDstConf ( rootInTes ) :
-    """
-    Various settings for mDst
-    
-    """
-    #
-    ## pick up all goodies from Juan
-    #
-    import MicroDSTConf.TriggerConfUtils as TCU
-    #
-    #
-    ## 'rootify'
-    #
-    if 0 != rootInTes.find ( '/Event/') : rootInTes = '/Event/' + rootInTes 
+logger.warnig('Use BenderTools.MicroDST!')
+# =============================================================================
+from BenderTools.MicroDST impiort uDstConf
 
-    rootInTes = TCU.fixTrunk ( rootInTes )
-    
-    logger.info("Reconfigure uDST to use    RootInTES='%s'" % rootInTes )
-    
-    #
-    ## configure L0,Hlt,e tc using Juan's functions:
-    #
-    TCU.configureL0AndHltDecoding ( rootInTes )
-    logger.info("Configure L0&HLT decoding  RootInTES='%s'" % rootInTes )
-
-    from Configurables import DataOnDemandSvc
-    dod = DataOnDemandSvc( Dump = True )
-    #
-    ## extra configuration:
-    #   it is more relevant for Stripping-17 data,
-    #   however it is backard compatible with other uDsts 
-    #
-
-    #
-    ## 1. Copy/Link ODIN, RawEvent and RecSummary
-    #
-    from Configurables import Gaudi__DataLink as Link
-    rawEvt  = Link ( 'LinkRawEvent'                      ,
-                     What   =  '/Event/Trigger/RawEvent' ,
-                     Target = rootInTes + 'DAQ/RawEvent' )
-    odin    = Link ( 'LinkODIN'                          ,
-                     What   =          '/Event/DAQ/ODIN' ,
-                     Target = rootInTes +     'DAQ/ODIN' )
-    summary = Link ( 'LinkSummary'                       ,
-                     What   =      '/Event/Rec/Summary'  ,
-                     Target = rootInTes + 'Rec/Summary'  )    
-    header  = Link ( 'LinkHeader'                        ,
-                     What   =       '/Event/Rec/Header'  ,
-                     Target = rootInTes +  'Rec/Header'  )
-    
-    dod.AlgMap [ rawEvt  . Target ] = rawEvt 
-    dod.AlgMap [ odin    . Target ] = odin
-    dod.AlgMap [ summary . Target ] = summary
-    dod.AlgMap [ header  . Target ] = header
-    
-    logger.info("Configure ODIN,Raw&Summary RootInTES='%s'" % rootInTes )
-    #
-    ##
-    #
-    from Configurables import OdinTimeDecoder, ODINDecodeTool
-    odinDec = OdinTimeDecoder ( )
-    odinDec.addTool( ODINDecodeTool, 'ODINDecodeTool' )
-    odinDec.ODINDecodeTool.RawEventLocation = rootInTes + 'DAQ/RawEvent'
-    
-    from Configurables import DecodePileUpData
-    pileupDec = DecodePileUpData()
-    pileupDec.RawEventLocation              = rootInTes + 'DAQ/RawEvent' 
-
-    
-    
 # =============================================================================
 if __name__ == '__main__' :
     print '*'*120
