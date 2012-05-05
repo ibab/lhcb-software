07c3f11d2b94557e4e951e57bf41526166df3d66
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-05-05 14:29:27 +0000

    Downgrade info message to debug (for stripping).
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@139746 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 963858c26854b021377f39b637f8e420dc18e1a2

--- Rec/Muon/MuonTools/src/MuonFastPosTool.cpp
+++ Rec/Muon/MuonTools/src/MuonFastPosTool.cpp
@@ -18,32 +18,31 @@ DECLARE_TOOL_FACTORY( MuonFastPosTool )
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-MuonFastPosTool::MuonFastPosTool( const std::string& type,
-                          const std::string& name,
-                          const IInterface* parent )
-  : GaudiTool ( type, name , parent ) 
+  MuonFastPosTool::MuonFastPosTool( const std::string& type,
+                                    const std::string& name,
+                                    const IInterface* parent )
+    : GaudiTool ( type, name, parent )
     , m_stationNumber(0)
     , m_regionNumber(0)
-    //, m_DDS()
-    ,m_debug(false){
+    , m_debug(false)
+{
   declareInterface<IMuonFastPosTool>(this);
-  return ;
 }
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
 StatusCode MuonFastPosTool::initialize() {
-  
+
   StatusCode sc = GaudiTool::initialize() ;
   if (sc.isFailure()) return sc;
 
-  info() << "Initialising the muon get info tool" << endmsg;
+  debug() << "Initialising the muon get info tool" << endmsg;
 
   m_muonDetector=getDet<DeMuonDetector>
-	("/dd/Structure/LHCb/DownstreamRegion/Muon");
+    ("/dd/Structure/LHCb/DownstreamRegion/Muon");
 
   /// get tile tool
-  MuonBasicGeometry basegeometry( detSvc(),msgSvc()); 
+  MuonBasicGeometry basegeometry( detSvc(),msgSvc());
   m_stationNumber=basegeometry.getStations();
   m_regionNumber=basegeometry.getRegions();
 
@@ -52,58 +51,58 @@ StatusCode MuonFastPosTool::initialize() {
 
   m_padSizeX.resize(20);
   m_padSizeY.resize(20);
-  
+
   for (int i=0;i<5;i++){
     int channels=48*(m_padGridX[i]*m_padGridY[i]);
     m_pos[i].resize(channels);
   }
 
   LHCb::MuonTileID tile;
-  for(int station=0;station<m_stationNumber;station++){    
+  for(int station=0;station<m_stationNumber;station++){
     tile.setStation(station);
     tile.setLayout( MuonLayout(m_padGridX[station],m_padGridY[station]) );
     int index=0;
     for(int region=0;region<m_regionNumber;region++){
       tile.setRegion(region);
-      for(int quarter=0;quarter<4;quarter++){        
+      for(int quarter=0;quarter<4;quarter++){
         tile.setQuarter(quarter);
-        for(unsigned y=0;y<m_padGridY[station];y++){         
-          tile.setY(y);   
+        for(unsigned y=0;y<m_padGridY[station];y++){
+          tile.setY(y);
           for (unsigned x=m_padGridX[station];x<2*m_padGridX[station];x++){
             tile.setX(x);
-            
+
             double xp,dx,yp,dy,zp,dz;
             sc =  m_muonDetector->Tile2XYZ(tile,xp,dx,yp,dy,zp,dz);
             if (sc.isFailure() ) return sc;
             (m_pos[station])[index++]=Gaudi::XYZPoint(xp,yp,zp);
             m_padSizeX[station*4+region]=(float) dx;
             m_padSizeY[station*4+region]=(float) dy;
-          }          
+          }
         }
-        for(unsigned y=m_padGridY[station];y<2*m_padGridY[station];y++){         
-          tile.setY(y);            
+        for(unsigned y=m_padGridY[station];y<2*m_padGridY[station];y++){
+          tile.setY(y);
           for (unsigned x=0;x<2*m_padGridX[station];x++){
             tile.setX(x);
             double xp,dx,yp,dy,zp,dz;
             sc = m_muonDetector->Tile2XYZ(tile,xp,dx,yp,dy,zp,dz);
             if (sc.isFailure()) return sc;
             (m_pos[station])[index++]=Gaudi::XYZPoint(xp,yp,zp);
-          }          
-        }        
-      }      
-    }    
+          }
+        }
+      }
+    }
   }
   return StatusCode::SUCCESS ;
 }
 
 
 
-StatusCode MuonFastPosTool::calcTilePos(const LHCb::MuonTileID& tile, 
-                                    double& x, double& deltax,
-                                    double& y, double& deltay,
-                                    double& z, double& /*deltaz*/)
+StatusCode MuonFastPosTool::calcTilePos(const LHCb::MuonTileID& tile,
+                                        double& x, double& deltax,
+                                        double& y, double& deltay,
+                                        double& z, double& /*deltaz*/)
 {
-     
+
   int station=tile.station();
   int region=tile.region();
   int quarter=tile.quarter();
@@ -111,14 +110,14 @@ StatusCode MuonFastPosTool::calcTilePos(const LHCb::MuonTileID& tile,
   unsigned int xpad=tile.nX();
   unsigned int ypad=tile.nY();
   unsigned int index=(region*4+quarter)*perQuarter;
-     
+
   if(ypad<m_padGridY[station]){
     index=index+m_padGridX[station]*ypad+xpad-m_padGridX[station];
   }else{
     index=index+m_padGridX[station]*m_padGridY[station]+
       2*m_padGridX[station]*(ypad-m_padGridY[station])+xpad;
   }
-     
+
   const Gaudi::XYZPoint& p = m_pos[station][index];
   x = p.x();
   y = p.y();
