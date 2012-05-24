71efe04d6dd9df417f01adc2f2815c7afc0f93a2
Vanya Belyaev <ivan.belyaev@cern.ch>
2012-05-24 17:46:30 +0000

    improve decorators
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@140474 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 229314a14eda3d78af29a5c1688a6bb15bb5ebd5

---
+++ Phys/Phys/LoKiPhys/src/PhysStreamers.cpp
@@ -0,0 +1,418 @@
+// $Id: $
+// ============================================================================
+// Include files 
+// ============================================================================
+// STD & STL 
+// ============================================================================
+#include <set>
+// ============================================================================
+// Local
+// ============================================================================
+#include "LoKi/PhysTypes.h"
+#include "LoKi/PhysRangeTypes.h"
+#include "LoKi/PhysExtract.h"
+#include "LoKi/apply.h"
+#include "LoKi/select.h"
+#include "LoKi/Streamers.h"
+// ============================================================================
+/** @file 
+ *
+ *  implementation of some streamers 
+ *
+ *  This file is a part of LoKi project - 
+ *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
+ *
+ *  The package has been designed with the kind help from
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
+ *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  A.Golutvin, P.Koppenburg have been used in the design.
+ *
+ *  By usage of this code one clearly states the disagreement 
+ *  with the smear campaign of Dr.O.Callot et al.: 
+ *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+ *
+ *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+ *  @date 2012-05-24 
+ *
+ *                    $Revision: 124207 $
+ *  Last modification $Date: 2011-06-02 18:05:29 +0200 (Thu, 02 Jun 2011) $
+ *                 by $Author: ibelyaev $
+ */
+// ============================================================================
+namespace 
+{
+  // ==========================================================================
+  template <class PARTICLE>
+  inline
+  LHCb::Particle::ConstVector
+  _get_ ( PARTICLE                 first , 
+          PARTICLE                 last  , 
+          const LoKi::Types::Cuts& cuts  )
+  {
+    //
+    LHCb::Particle::ConstVector r ; r.reserve ( last - first  ) ;
+    // 
+    LoKi::apply_filter ( first , last , cuts , std::back_inserter ( r ) ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================
+  template <class PARTICLES>
+  inline
+  LHCb::Particle::ConstVector
+  _get_ ( const PARTICLES*         parts , 
+          const LoKi::Types::Cuts& cuts  )
+  {
+    //
+    LHCb::Particle::ConstVector r ; 
+    if ( 0 == parts || parts->empty()  ) { return r ; }  // RETURN 
+    //
+    r.reserve ( parts->size() ) ;
+    // 
+    LoKi::apply_filter ( parts -> begin ()  , 
+                         parts -> end   ()  , cuts , std::back_inserter ( r ) ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================  
+  template <class PARTICLE, class TYPE>
+  inline
+  std::vector<double>
+  _get_ ( PARTICLE                          first , 
+          PARTICLE                          last  , 
+          const LoKi::Functor<TYPE,double>& func  )
+  {
+    //
+    std::vector<double> r ; r.reserve ( last - first  ) ;
+    // 
+    LoKi::apply ( first , last , func , std::back_inserter ( r ) ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================
+  template <class PARTICLES, class TYPE>
+  inline
+  std::vector<double>
+  _get_ ( const PARTICLES*                  parts , 
+          const LoKi::Functor<TYPE,double>& func  )
+  {
+    //
+    std::vector<double> r ;
+    if ( 0 == parts || parts->empty()  ) { return r ; }  // RETURN 
+    //
+    r.reserve ( parts->size() ) ;
+    // 
+    LoKi::apply ( parts -> begin ()  , 
+                  parts -> end   ()  , func , std::back_inserter ( r ) ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================  
+  class _VRTX_ : 
+    public std::unary_function<const LHCb::Particle*,bool>
+  {
+  public:
+    // ========================================================================
+    _VRTX_ ( const LoKi::Types::VCuts& cuts ) : m_cuts ( cuts ) {}
+    // ========================================================================
+    inline bool operator() ( const LHCb::Particle* p ) const 
+    {
+      static const LHCb::VertexBase* s_v ;
+      return ( 0 == p ) ? m_cuts ( s_v ) : m_cuts ( p->endVertex() ) ;
+    }
+    // ========================================================================
+  private:
+    // ========================================================================
+    _VRTX_ () ;
+    _VRTX_            ( const _VRTX_& ) ;
+    _VRTX_& operator= ( const _VRTX_& ) ;
+    // ========================================================================
+  private:
+    // ========================================================================
+    const LoKi::Types::VCuts& m_cuts  ;
+    // ========================================================================    
+  } ;
+  // ==========================================================================
+  template <class PARTICLE>
+  inline
+  LHCb::Particle::ConstVector
+  _wget_ ( PARTICLE                  first , 
+           PARTICLE                  last  , 
+           const LoKi::Types::VCuts& cuts  )
+  {
+    //
+    LHCb::Particle::ConstVector r ; r.reserve ( last - first  ) ;
+    // 
+    _VRTX_ vx ( cuts ) ;
+    LoKi::select ( first , last , std::back_inserter ( r ) , vx ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================
+  template <class PARTICLES>
+  inline
+  LHCb::Particle::ConstVector
+  _wget_ ( const PARTICLES*          parts ,
+           const LoKi::Types::VCuts& cuts  )
+  {
+    //
+    LHCb::Particle::ConstVector r ; 
+    if ( 0 == parts || parts -> empty() ) { return r ; }
+    //
+    r.reserve ( parts -> size()  ) ;
+    // 
+    _VRTX_ vx ( cuts ) ;
+    LoKi::select ( parts -> begin () ,
+                   parts -> end   () , std::back_inserter ( r ) , vx ) ;
+    //
+    return r ;
+  }
+  // ==========================================================================
+}
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::ConstVector&    input , 
+  const LoKi::Types::Cuts&              cuts  ) 
+{ return _get_ ( input.begin() , input.end() , cuts ) ; } 
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Range&          input , 
+  const LoKi::Types::Cuts&              cuts  ) 
+{ return _get_ ( input.begin() , input.end() , cuts ) ; } 
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Container*      input , 
+  const LoKi::Types::Cuts&              cuts  ) 
+{ return _get_ ( input , cuts ) ; } 
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Selection*      input , 
+  const LoKi::Types::Cuts&              cuts  ) 
+{ return _get_ ( input , cuts ) ; } 
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const SmartRefVector<LHCb::Particle>& input , 
+  const LoKi::Types::Cuts&              cuts  ) 
+{ return _get_ ( input.begin() , input.end() , cuts ) ; } 
+// ============================================================================
+// Vertices 
+// ============================================================================
+LHCb::VertexBase::ConstVector
+operator>> 
+( const LoKi::Types::VRange&            input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::VertexBase::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::VertexBase::ConstVector
+operator>> 
+( const LHCb::VertexBase::ConstVector&  input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::VertexBase::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::VertexBase::ConstVector
+operator>> 
+( const LHCb::Vertex::ConstVector&      input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::VertexBase::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::RecVertex::ConstVector
+operator>> 
+( const LHCb::RecVertex::ConstVector&   input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::RecVertex::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::VertexBase::ConstVector
+operator>> 
+( const LHCb::Vertex::Container*        input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::VertexBase::ConstVector r ; 
+  if ( 0 == input || input->empty() ) { return r ; }
+  r.reserve ( input->size() ) ;
+  LoKi::apply_filter ( input -> begin ()  , 
+                       input -> end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::RecVertex::ConstVector
+operator>> 
+( const LHCb::RecVertex::Container*     input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::RecVertex::ConstVector r ; 
+  if ( 0 == input || input->empty() ) { return r ; }
+  r.reserve ( input->size() ) ;
+  LoKi::apply_filter ( input -> begin ()  , 
+                       input -> end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::VertexBase::ConstVector
+operator>> 
+( const SmartRefVector<LHCb::Vertex>&   input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{
+  LHCb::VertexBase::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+LHCb::RecVertex::ConstVector
+operator>> 
+( const SmartRefVector<LHCb::RecVertex>& input , 
+  const LoKi::Types::VCuts&              cuts  ) 
+{
+  LHCb::RecVertex::ConstVector r ; 
+  r.reserve ( input.size() ) ;
+  LoKi::apply_filter ( input . begin ()  , 
+                       input . end   ()  , cuts , std::back_inserter ( r ) ) ;
+  return r ;
+}
+// ============================================================================
+// Functions 
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Particle::ConstVector&    input , 
+  const LoKi::Types::Func&              cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Particle::Range&          input , 
+  const LoKi::Types::Func&              cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Particle::Container*      input , 
+  const LoKi::Types::Func&              cuts  ) 
+{ return _get_ ( input , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Particle::Selection*      input , 
+  const LoKi::Types::Func&              cuts  ) 
+{ return _get_ ( input , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const SmartRefVector<LHCb::Particle>& input , 
+  const LoKi::Types::Func&              cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+// Vertex functions  
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::VertexBase::ConstVector& input , 
+  const LoKi::Types::VFunc&            cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Vertex::ConstVector&     input , 
+  const LoKi::Types::VFunc&            cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::RecVertex::ConstVector&   input , 
+  const LoKi::Types::VFunc&             cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::Vertex::Container*        input , 
+  const LoKi::Types::VFunc&             cuts  ) 
+{ return _get_ ( input , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const LHCb::RecVertex::Container*     input , 
+  const LoKi::Types::VFunc&             cuts  ) 
+{ return _get_ ( input , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const SmartRefVector<LHCb::Vertex>&   input , 
+  const LoKi::Types::VFunc&             cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+std::vector<double>
+operator>> 
+( const SmartRefVector<LHCb::RecVertex>&input , 
+  const LoKi::Types::VFunc&             cuts  ) 
+{ return _get_ ( input.begin () , input.end () , cuts ) ; }
+// ============================================================================
+// Some weird stuff 
+// ============================================================================
+#include "LoKi/Particles7.h"
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::ConstVector&    input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{ return _wget_ ( input.begin() , input.end() , cuts ) ; }
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Container*      input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{ return _wget_ ( input , cuts ) ; }
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Selection*      input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{ return _wget_ ( input , cuts ) ; }
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const SmartRefVector<LHCb::Particle>& input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{ return _wget_ ( input.begin() , input.end() , cuts ) ; }
+// ============================================================================
+LHCb::Particle::ConstVector
+operator>> 
+( const LHCb::Particle::Range&          input , 
+  const LoKi::Types::VCuts&             cuts  ) 
+{ return _wget_ ( input.begin() , input.end() , cuts ) ; }
+// ============================================================================
+
+
+
+// ============================================================================
+// The END 
+// ============================================================================
