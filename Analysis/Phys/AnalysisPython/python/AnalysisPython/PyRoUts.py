3fb973d3782f9c35b0ef9612715987d067bae223
Vanya Belyaev <ivan.belyaev@cern.ch>
2012-05-28 19:40:28 +0000

    add hfits
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@140506 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a4c78a8714b4710936445dea6f83afd8a6ac7301

--- Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
+++ Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
@@ -3049,6 +3049,82 @@ def ve_adjust ( ve , mn = 0 , mx = 1.0 ) :
     #
     return ve
 
+
+# =============================================================================
+## fit the histogram by sum of components
+def _h_Fit_ ( self                              ,
+              components                        ,
+              draw = False                      ,
+              interpolate = True                ,
+              selector    = lambda i,x,y : True ) :
+    """
+    (Chi_2)-fit the histogram with the set of ``components''
+    
+    The ``components'' could be histograms, functions and other
+    callable object :
+    
+    >>> h0 = ...
+    >>> h .hFit ( h0 )
+    
+    >>> h0 = ...
+    >>> h1 = ...
+    >>> h .hFit ( [ h0 , h1 ] )
+    
+    """
+    DATA =   VE.Vector
+    CMPS = DATA.Vector
+
+    if   isinstance ( components , ROOT.TH1D ) : components = [ components ]
+    elif isinstance ( components , ROOT.TH1F ) : components = [ components ]
+    elif not isinstance ( components , ( tuple , list ) ) :
+        components = [ components ]
+
+    data = DATA ()
+    cmps = CMPS ()
+    
+    while len ( cmps ) < len ( components )  :
+        cmps.push_back( DATA() )
+
+    for i,x,y in self.iteritems () :
+
+        if not selector ( i , x , y ) : continue
+        
+        dp = VE ( y )
+        data.push_back ( dp )
+        
+        for j in range ( 0 , len ( components ) ) :
+
+            cmp = components[j]
+            if isinstance ( cmp , ( ROOT.TH1F , ROOT.TH1D )) :
+                cp = cmp ( x , interpolate   )
+            else :
+                cp =  VE ( cmp ( x.value() ) )  ## CALLABLE !!! 
+            
+            cmps[ j ].push_back ( cp ) 
+            
+
+    _c2Fit = cpp.Gaudi.Math.Chi2Fit ( data , cmps )
+
+    if draw :
+        
+        if not hasattr ( self , '_histos_' ) :
+            self._histos_ = []
+            
+        self.Draw( 'e1' ) 
+        for j in range ( 0 , len ( components ) ) :
+            cmpj = components [j]
+            if not isinstance ( cmpj , ( ROOT.TH1F , ROOT.TH1D ) ) : continue
+            sc = _c2Fit[j].value ()
+            nh = cmpj * sc 
+            nh.Draw ( 'same' )
+            self._histos_.append ( nh ) 
+
+    return _c2Fit
+
+
+ROOT.TH1F. hFit = _h_Fit_ 
+ROOT.TH1D. hFit = _h_Fit_ 
+
 # =============================================================================
 logger.info ( 'Some useful decorations for RooFit objects')
 # =============================================================================
