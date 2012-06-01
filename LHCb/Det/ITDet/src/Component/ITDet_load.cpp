d7fb3c211a3db1b3fc6faa925b1ca4f839bf38b5
Marco Clemencic <marcocle@cern.ch>
2012-06-01 12:49:12 +0000

    move_package: moved the Det/ITDet package to the obsolete project
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@140619 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 77eb94eb88537ad57f80dd64501429ce4bbd9d32

--- LHCb/Det/ITDet/src/Component/ITDet_load.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "GaudiKernel/DeclareFactoryEntries.h"
-
-
-DECLARE_FACTORY_ENTRIES(ITDet) {
-  DECLARE_CONVERTER( XmlITDetectorCnv );
-}
--- LHCb/Det/ITDet/src/Component/XmlITDetectorCnv.cpp
+++
@@ -1,9 +0,0 @@
-
-/// DetDesc
-#include "DetDescCnv/XmlUserDetElemCnv.h"
-/// ITDet 
-#include "ITDet/DeITDetector.h"
-
-typedef XmlUserDetElemCnv<DeITDetector>       XmlITDetectorCnv;
-static CnvFactory<XmlITDetectorCnv>           s_XmlITDetectorCnvFactory ;
-const ICnvFactory&  XmlITDetectorCnvFactory = s_XmlITDetectorCnvFactory ;
