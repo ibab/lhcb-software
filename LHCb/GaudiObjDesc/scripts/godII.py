08d6013635550dd7233cff5b55a08ee0b133cc8a
Marco Clemencic <marcocle@cern.ch>
2012-06-04 12:16:21 +0000

    Fixed a minor mistake in the new godII.py script.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@140669 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3b4563fb9b7feaab40b6be9dc0ed4b08effbbf72

--- LHCb/GaudiObjDesc/scripts/godII.py
+++ LHCb/GaudiObjDesc/scripts/godII.py
@@ -3,7 +3,7 @@ import os
 import sys
 # Ensure we can find the GaudiObjDesc module even if the PYTHONPATH does not
 # have it. (Assume the standard relative locations of the modules)
-sys.path.append(os.path.join(os.path.dirname(__file__), 'python'))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'python'))
 
 from GaudiObjDesc.godII import godII
 
