2610c31265cf25f01c46e42908905c1ffd63c602
Ben Couturier <lben@cern.ch>
2012-06-06 14:59:17 +0000

    Release of v4r7p7
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@140768 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9362cc1864bc79adcfa09433f13ea49dcf7bd7d6

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -21,10 +21,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from tempfile import mkdtemp
 
-script_version = '120508'
+script_version = '120606'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v6r7p6"
+lbscripts_version = "v6r7p7"
 compat_version = None
 line_size = 120
 #-----------------------------------------------------------------------------------
