9b0af3d5a27280cc622a4a1c06169045b94e1d90
Illya Shapoval <illya.shapoval@cern.ch>
2012-06-12 18:06:01 +0000

    A method is added to the SAX Release Notes Handler
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@140970 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 197bd2c7b40aff9c67a7151da74e0991039d3a52

--- LHCb/Tools/CondDBUI/python/CondDBUI/Admin/TagsFilter.py
+++ LHCb/Tools/CondDBUI/python/CondDBUI/Admin/TagsFilter.py
@@ -60,22 +60,22 @@ class ReleaseNotesHandler(ContentHandler):
         """Performs analysis of .xml file elements content.
 
         Depending on the request, this function fills:
-        1) The list of all global tags wich correspond to the given partition
+        1) The list of all global tags which correspond to the given partition
         and data type.
         2) The tuple of the most recent global tag and all subsequent local tags for it.
         """
 
-        # Cancellation of whitespaces processing by characters()
+        # Cancellation of white-spaces processing by characters()
         if ch in ("\n", "\t", "\t\t", "\t\t\t"): return 0
 
-        # Local tag entry treating. Grabing marked elemets values.
+        # Local tag entry treating. Grabbing marked elements values.
         if self.found_lt:
             if self.found_lt_Partition:
                 self.lt_Partition = ch
             elif self.found_lt_Name:
                 self.lt_Name = ch
             elif self.found_lt_DataType:
-                # Choosing among all mentioned datatypes the one requested
+                # Choosing among all mentioned data types the one requested
                 if ch == self.requested_datatype:
                     self.lt_DataType = ch
 
@@ -84,12 +84,12 @@ class ReleaseNotesHandler(ContentHandler):
                     if self.lt_Name not in self.local_tags:
                         self.local_tags.append(str(self.lt_Name))
 
-        # Global tag entry treating.  Grabing marked elemets values.
+        # Global tag entry treating.  Grabbing marked elements values.
         elif self.found_gt:
             if self.found_gt_Name:
                 self.gt_Name = ch
             elif self.found_gt_DataType:
-                # Choosing among all mentioned datatypes the one requested
+                # Choosing among all mentioned data types the one requested
                 if ch == self.requested_datatype:
                     self.gt_DataType = ch
             elif self.found_gt_Partition:
@@ -104,7 +104,7 @@ class ReleaseNotesHandler(ContentHandler):
                         self.global_tags.append(str(self.gt_Name))
 
     def endElement(self, name):
-        """Performs unsetting the flag_*_* variables, when processed element is left."""
+        """Performs resetting the flag_*_* variables, when processed element is left."""
 
         # Local tag entry treating.
         if self.found_lt:
@@ -158,6 +158,28 @@ def all_gts(partition, datatype, rel_notes = None):
         parser.parse(open(rel_notes))
         if len(handler.global_tags) != 0:
             return handler.global_tags
+        else:
+            return None
+    except IOError:
+        print "\nSorry.. There is no such file or IO problem has been met.\
+        \nYou were using: %s \
+        \nPlease check the path to 'release_notes.xml' or just try again later."\
+        % rel_notes
+        return 1
+
+def all_lts(partition, datatype, rel_notes = None):
+    """Returns for the given partition and datatype the list of all local tags.
+
+    The output is in the form of a list. The fist one global tag is the most recent one.
+    """
+
+    parser, handler = init_finder(partition, datatype, False)
+    if not rel_notes:
+        rel_notes = os.path.join(os.environ["SQLITEDBPATH"], "..", "doc", "release_notes.xml")
+    try:
+        parser.parse(open(rel_notes))
+        if len(handler.local_tags) != 0:
+            return handler.local_tags
         else:
             return None
     except IOError:
