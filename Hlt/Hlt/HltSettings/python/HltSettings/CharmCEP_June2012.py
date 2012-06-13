0bcc050547c8113f758e97c13e12e23e2915bd9e
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2012-06-13 15:10:15 +0000

    add CEP lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@141014 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8d7427fc49e1632214d922f4a64205ee00923349

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/CharmCEP_June2012.py
@@ -0,0 +1,32 @@
+class CharmCEP_June2012 :
+    """
+    Threshold settings for Hlt1 and 2 in nominal conditions (list of exclusive)
+    
+    WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
+    
+    @author P.Koppenburg, M. Gandelman, G. Raven, V. Gligorov, P. Spradlin
+    @date 2009-08-05
+    """
+    
+    __all__ = ( 'ActiveHlt2Lines' )
+    
+    
+    def ActiveHlt2Lines(self) : 
+        """
+        Returns a list of active lines
+        """
+
+        lines = [   'Hlt2LowMultD2KPi'       
+                  , 'Hlt2LowMultD2KPiPi'   
+                  , 'Hlt2LowMultD2K3Pi'    
+                  , 'Hlt2LowMultChiC'      
+                  , 'Hlt2LowMultD2KPiWS'   
+                  , 'Hlt2LowMultD2KPiPiWS' 
+                  , 'Hlt2LowMultD2K3PiWS'  
+                  , 'Hlt2LowMultChiCWS'    
+             ]
+        
+        return lines
+
+            
+   
