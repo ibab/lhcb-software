91d7c259f5096f45161161be38f5de52bb067bf2
cmthomas <cmthomas@cern.ch>
2012-06-13 21:40:20 +0000

    updated CharmCEP with two more lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@141037 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d9f9f7e771a6ece682852d2071d690dbb559e0ed

--- Hlt/Hlt/HltSettings/python/HltSettings/CharmCEP_June2012.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/CharmCEP_June2012.py
@@ -15,16 +15,18 @@ class CharmCEP_June2012 :
         """
         Returns a list of active lines
         """
-
-        lines = [   'Hlt2LowMultD2KPi'       
+        
+        lines = [ 'Hlt2LowMultD2KPi'       
                   , 'Hlt2LowMultD2KPiPi'   
                   , 'Hlt2LowMultD2K3Pi'    
-                  , 'Hlt2LowMultChiC'      
+                  , 'Hlt2LowMultChiC2HH'
+                  , 'Hlt2LowMultChiC2HHHH'      
                   , 'Hlt2LowMultD2KPiWS'   
                   , 'Hlt2LowMultD2KPiPiWS' 
                   , 'Hlt2LowMultD2K3PiWS'  
-                  , 'Hlt2LowMultChiCWS'    
-             ]
+                  , 'Hlt2LowMultChiC2HHWS'
+                  , 'Hlt2LowMultChiC2HHHHWS'    
+                  ]
         
         return lines
 
