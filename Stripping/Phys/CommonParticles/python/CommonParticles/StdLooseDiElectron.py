65de2709b58ca82d7ae05e861aba0f1eaaae1d31
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2012-06-18 13:20:51 +0000

    StdLooseDiElectron : Make a DiElectron for B->eeK(*) using DiElectronMaker  called StdDiElectronFromTracks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@141187 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5f86e337a4a367b07d44cce02e3d32d4b58f144b

--- Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDiElectron.py
+++ Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDiElectron.py
@@ -14,7 +14,9 @@ import GaudiKernel.SystemOfUnits as Units
 ###########################
 # StdLooseDiElectron #
 ###########################
-
+"""
+Standard Di-Electron maker from 2 electrons
+"""
 StdLooseDiElectron = CombineParticles("StdLooseDiElectron")
 StdLooseDiElectron.DecayDescriptor = "J/psi(1S) -> e+ e-"
 StdLooseDiElectron.Inputs = ["Phys/StdAllLooseElectrons/Particles"]
@@ -22,6 +24,23 @@ StdLooseDiElectron.DaughtersCuts = { "e+" : "(PT>500*MeV)" }
 StdLooseDiElectron.CombinationCut = "(AM>30*MeV) & (ADOCACHI2CUT(30,''))"
 StdLooseDiElectron.MotherCut = "(VFASPF(VCHI2)<25)"
 locations = updateDoD(StdLooseDiElectron)
+
+"""
+Using Olivier's Di-Electron Particle Maker
+"""
+###--- From Olivier's ParticleMaker
+from Configurables import DiElectronMaker,ProtoParticleCALOFilter
+from GaudiKernel.SystemOfUnits import *
+dieLL = DiElectronMaker('StdDiElectronFromTracks')
+dieLL.Particle = "J/psi(1S)"
+selector = trackSelector ( dieLL , trackTypes = [ "Long"]) 
+dieLL.addTool( ProtoParticleCALOFilter('Electron'))
+dieLL.Electron.Selection = ["RequiresDet='CALO' CombDLL(e-pi)>'-2.0'"]
+dieLL.DiElectronMassMax = 5000.*MeV
+dieLL.DiElectronMassMin = 0.*MeV
+dieLL.DiElectronPtMin = 200.*MeV
+locations = updateDoD ( dieLL )
+StdDiElectronFromTracks = dieLL
 
 ## ============================================================================
 if '__main__' == __name__ :
