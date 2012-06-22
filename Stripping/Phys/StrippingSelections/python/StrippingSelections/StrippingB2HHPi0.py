38d7ec4ae8f99f9e91ce34779914e1ff361dac08
Olivier Deschamps <odescham@cern.ch>
2012-06-22 17:45:07 +0000

    update D02KPiPi0 & B2HHPi0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@141460 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 831b1db1a4dcf2068e39edeabcf8c456bf29f098

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '2012.04.20'
-__version__ = '$Revision: 1.4 $'
+__date__ = '2012.05.02'
+__version__ = '$Revision: 1.5 $'
 
 '''
 Stripping selection for B -> h h pi0
@@ -54,33 +54,16 @@ class StrippingB2HHPi0Conf(LineBuilder) :
 	myResolvedPi0 = StdLooseResolvedPi0
 
         #---------------------------------------
-        # pi0 selection
-        self.selpi0r = makeB2HHPi0pi0r( name + '_pi0r',
-                                        config,
-                                        inputSel = [myResolvedPi0]
-                                        )
-        self.selpi0m = makeB2HHPi0pi0m( name + '_pi0m',
-                                        config,
-                                        inputSel = [myMergedPi0]
-                                        )        
-        #---------------------------------------
-        # hh selection
-        self.selrho = makeB2HHPi0rho( name + '_rho',
-                                      config,
-                                      DecayDescriptor = 'rho(770)0 -> pi+ pi-',
-				      inputSel = [myPions]
-                                      )
-        #---------------------------------------
         # B -> HHPi0 selections
 	self.selresolved = makeB2HHPi0R( name + 'R',
 	                                 config,
-					 DecayDescriptor = 'B0 -> rho(770)0 pi0',
-					 inputSel = [self.selpi0r, self.selrho]
+                                         DecayDescriptor = 'B0 -> pi+ pi- pi0',
+					 inputSel = [myPions, myResolvedPi0]
                                          )				       
         self.selmerged = makeB2HHPi0M( name + 'M',
 	                               config,
-				       DecayDescriptor = 'B0 -> rho(770)0 pi0',
-				       inputSel = [self.selpi0m, self.selrho ]
+                                       DecayDescriptor = 'B0 -> pi+ pi- pi0',
+                                       inputSel = [myPions, myMergedPi0]
                                        )
         #---------------------------------------
         # Stripping lines
@@ -99,65 +82,17 @@ class StrippingB2HHPi0Conf(LineBuilder) :
         self.registerLine(self.B2HHPi0M_line)
 
 ##############################################################
-def makeB2HHPi0pi0r(name,
-                   config,
-                   inputSel
-                   ) :
-
-    _pi0r = "(PT>%(Pi0MinPT_R)s *MeV) & (MM>%(ResPi0MinMM)s *MeV) & (MM<%(ResPi0MaxMM)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % locals()['config']
-
-    _pi0rFilter = FilterDesktop(Code=_pi0r)
-
-    return Selection ( name+'Sel',
-                       Algorithm = _pi0rFilter,
-                       RequiredSelections = inputSel )
-
-##############################################################
-def makeB2HHPi0pi0m(name,
-                   config,
-                   inputSel
-                   ) :
-
-    _pi0m = "(PT>%(Pi0MinPT_M)s *MeV)" % locals()['config']
-    
-    _pi0mFilter = FilterDesktop(Code=_pi0m)
-
-    return Selection ( name+'Sel',
-                       Algorithm = _pi0mFilter,
-                       RequiredSelections = inputSel )
-##############################################################
-def makeB2HHPi0rho(name,
-                   config,
-                   DecayDescriptor,
-                   inputSel
-                   ) :
-
-    _piCuts ="(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
-    _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts }
-    _combCuts = "(AALL)"  % locals()['config']
-    _motherCuts = "(VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s)" % locals()['config']
-
-    _rho = CombineParticles( DecayDescriptor = DecayDescriptor,
-                             MotherCut = _motherCuts,
-                             CombinationCut = _combCuts,
-                             DaughtersCuts = _daughterCuts
-                             )
-
-    return Selection ( name+'Sel',
-                       Algorithm = _rho,
-                       RequiredSelections = inputSel )
-##############################################################
 def makeB2HHPi0R( name,
                   config,
                   DecayDescriptor,
                   inputSel
                   ) :
 
-    _rhoCuts = "(ALL)" % locals()['config']
-    _pi0Cuts = "(ALL)" % locals()['config']
-    _daughterCuts = { 'rho(770)0' : _rhoCuts, 'pi0' : _pi0Cuts }
+    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_R)s *MeV) & (MM>%(ResPi0MinMM)s *MeV) & (MM<%(ResPi0MaxMM)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % locals()['config']
+    _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts, 'pi0' : _pi0Cuts }
     _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
-    _motherCuts = "(PT>%(BMinPT_R)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
+    _motherCuts = "(PT>%(BMinPT_R)s *MeV) & (VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
 
     _B = CombineParticles( DecayDescriptor = DecayDescriptor,
                            MotherCut = _motherCuts,
@@ -176,11 +111,11 @@ def makeB2HHPi0M( name,
                   inputSel
                   ) :
 
-    _rhoCuts = "(ALL)" % locals()['config']
-    _pi0Cuts = "(ALL)" % locals()['config']
-    _daughterCuts = { 'rho(770)0' : _rhoCuts, 'pi0' : _pi0Cuts }
+    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_M)s *MeV)" % locals()['config']
+    _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts, 'pi0' : _pi0Cuts }
     _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
-    _motherCuts = "(PT>%(BMinPT_M)s *MeV) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
+    _motherCuts = "(PT>%(BMinPT_M)s *MeV) & (VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
 
     _B = CombineParticles( DecayDescriptor = DecayDescriptor,
                            MotherCut = _motherCuts,
--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '2012.04.23'
-__version__ = '$Revision: 1.3 $'
+__date__ = '2012.05.02'
+__version__ = '$Revision: 1.4 $'
 
 '''
 Stripping selection for D0 -> K pi pi0 
@@ -50,38 +50,16 @@ class StrippingD02KPiPi0Conf(LineBuilder) :
 	myResolvedPi0 = StdLooseResolvedPi0
 
         #---------------------------------------
-        # pi0 selection
-        self.selpi0r = makeD02KPiPi0pi0r( name + '_pi0r',
-                                          config,
-                                          inputSel = [myResolvedPi0]
-                                          )
-        self.selpi0m = makeD02KPiPi0pi0m( name + '_pi0m',
-                                          config,
-                                          inputSel = [myMergedPi0]
-                                          )        
-        #---------------------------------------
-        # hh selection
-        self.selkstar_forRes = makeD02KPiPi0kstar_forRes( name + '_kstar_forRes',
-                                                          config,
-                                                          DecayDescriptor = '[K*(892)~0 -> K- pi+]cc',
-                                                          inputSel = [myPions,myKaons]
-                                                          )
-        self.selkstar_forMer = makeD02KPiPi0kstar_forMer( name + '_kstar_forMer',
-                                                          config,
-                                                          DecayDescriptor = '[K*(892)~0 -> K- pi+]cc',
-                                                          inputSel = [myPions,myKaons]
-                                                          )
-        #---------------------------------------
         # B -> HHPi0 selections
 	self.selresolved = makeD02KPiPi0R( name + 'R',
                                            config,
-                                           DecayDescriptor = '[D0 -> K*(892)~0 pi0]cc',
-                                           inputSel = [self.selpi0r, self.selkstar_forRes]
+                                           DecayDescriptor = '[D0 -> K- pi+ pi0]cc',
+                                           inputSel = [myKaons, myPions, myResolvedPi0]
                                          )				       
 	self.selmerged = makeD02KPiPi0M( name + 'M',
                                          config,
-                                         DecayDescriptor = '[D0 -> K*(892)~0 pi0]cc',
-                                         inputSel = [self.selpi0m, self.selkstar_forMer]
+                                         DecayDescriptor = '[D0 -> K- pi+ pi0]cc',
+                                         inputSel = [myKaons, myPions, myMergedPi0]
                                          )
         #---------------------------------------
         # Stripping lines
@@ -100,90 +78,17 @@ class StrippingD02KPiPi0Conf(LineBuilder) :
         self.registerLine(self.D02KPiPi0M_line)
 
 ##############################################################
-def makeD02KPiPi0pi0r(name,
-                      config,
-                      inputSel
-                      ) :
-
-    _pi0r = "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % locals()['config']
-
-    _pi0rFilter = FilterDesktop(Code=_pi0r)
-
-    return Selection ( name+'Sel',
-                       Algorithm = _pi0rFilter,
-                       RequiredSelections = inputSel )
-
-##############################################################
-def makeD02KPiPi0pi0m(name,
-                      config,
-                      inputSel
-                      ) :
-
-    _pi0m = "(PT>%(Pi0MinPT_M)s *MeV)" % locals()['config']
-
-    _pi0mFilter = FilterDesktop(Code=_pi0m)
-
-    return Selection ( name+'Sel',
-                       Algorithm = _pi0mFilter,
-                       RequiredSelections = inputSel )
-
-##############################################################
-def makeD02KPiPi0kstar_forRes(name,
-                              config,
-                              DecayDescriptor,
-                              inputSel
-                              ) :
-
-    _TrackCuts ="(PT>%(TrackMinPT_R)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']
-    _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts }
-    _combCuts = "(AALL)"  % locals()['config']
-    _motherCuts = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s)" % locals()['config']
-
-    _kstar = CombineParticles( DecayDescriptor = DecayDescriptor,
-                             MotherCut = _motherCuts,
-                             CombinationCut = _combCuts,
-                             DaughtersCuts = _daughterCuts
-                             )
-
-    return Selection ( name+'Sel',
-                       Algorithm = _kstar,
-                       
-                       RequiredSelections = inputSel )
-
-##############################################################
-def makeD02KPiPi0kstar_forMer(name,
-                              config,
-                              DecayDescriptor,
-                              inputSel
-                              ) :
-
-    _TrackCuts ="(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']
-    _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts }
-    _combCuts = "(AALL)"  % locals()['config']
-    _motherCuts = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s)" % locals()['config']
-
-    _kstar = CombineParticles( DecayDescriptor = DecayDescriptor,
-                             MotherCut = _motherCuts,
-                             CombinationCut = _combCuts,
-                             DaughtersCuts = _daughterCuts
-                             )
-
-    return Selection ( name+'Sel',
-                       Algorithm = _kstar,
-                       RequiredSelections = inputSel )
-
-##############################################################
 def makeD02KPiPi0R( name,
                   config,
                   DecayDescriptor,
                   inputSel
                   ) :
 
-    _kstarCuts = "(ALL)" % locals()['config']
-    _pi0Cuts = "(ALL)" %locals()['config']
-    _daughterCuts = { 'K*(892)~0' : _kstarCuts, 'pi0' : _pi0Cuts }
-    _combCuts = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
-    _motherCuts = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
+    _TrackCuts    = "(PT>%(TrackMinPT_R)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
+    _pi0Cuts      = "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" %locals()['config']
+    _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts, 'pi0' : _pi0Cuts }
+    _combCuts     = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
+    _motherCuts   = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s) & (BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
 
     _D = CombineParticles( DecayDescriptor = DecayDescriptor,
                            MotherCut = _motherCuts,
@@ -202,11 +107,11 @@ def makeD02KPiPi0M( name,
                   inputSel
                   ) :
 
-    _kstarCuts = "(ALL)" % locals()['config']
-    _pi0Cuts = "(ALL)" %locals()['config']
-    _daughterCuts = { 'K*(892)~0' : _kstarCuts, 'pi0' : _pi0Cuts }
-    _combCuts = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
-    _motherCuts = "(BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
+    _TrackCuts    = "(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
+    _pi0Cuts      = "(PT>%(Pi0MinPT_M)s *MeV)" %locals()['config']
+    _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts, 'pi0' : _pi0Cuts }
+    _combCuts     = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
+    _motherCuts   = "(VFASPF(VPCHI2)>%(D0MinVtxProb)s) & (BPVVDCHI2>%(D0MinVVDChi2)s) & (BPVIPCHI2()<%(D0MaxIPChi2)s) & (BPVDIRA>%(D0MinDIRA)s)" % locals()['config']
 
     _D = CombineParticles( DecayDescriptor = DecayDescriptor,
                            MotherCut = _motherCuts,
