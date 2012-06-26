cc7654936ac5f5a954b292c1abaa9b3ebf13f977
William James Barter <william.barter@cern.ch>
2012-06-26 09:12:07 +0000

    Update QEE dictionary for Stripping19b.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@141763 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 94c38cfa108897a2a9eaba6cafd8e87bd01adfff

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping19b/LineConfigDictionaries_QEE.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping19b/LineConfigDictionaries_QEE.py
@@ -35,9 +35,13 @@ WMu = {
     'WMu_Prescale'    : 1.0,
     'WMuLow_Prescale' : 0.1,
     'WMu_Postscale'   : 1.0,
+    'SingMuon10_Prescale' : 0.01,
+    'SingMuon48_Prescale' : 0.2,
     'pT'     : 20.,
     'pTlow'  : 15.,
-    'pTvlow' :  5.
+    'pTvlow' :  5.,
+    'SingMuon10_pT': 10.,
+    'SingMuon48_pT': 4.8
     },
     'WGs' : [ 'QEE' ],
     'STREAMS' : [ 'EW' ] 
@@ -201,10 +205,70 @@ MuMuSS = {
 LowMult = {
     'BUILDERTYPE'  : 'LowMultConf',
     'CONFIG'       : {
-    'LowMultPrescale'    : 1.0 
-    ,  'LowMultPostscale'   : 1.0
-    ,  'LowMultPrescale_ps'    : 0.005
-    ,  'LowMultNoFilterPrescale':0.1
+    'LowMultPrescale'           : 1.0
+    , 'LowMultWSPrescale'       : 0.1
+    , 'LowMultHHIncPrescale'    : 0.1
+    , 'LowMultPrescaleHalf'     : 0.5
+    , 'LowMultPrescale_ps'      : 0.005
+    , 'LowMultNoFilterPrescale' : 0.1
+    , 'LowMultPostscale'        : 1.0
+    # Final-state particles
+    , 'H_PTmin'         : 100.0 * MeV
+    , 'H_Pmin'          : 5000.0 * MeV
+    , 'H_TrkChi2max'    : 3.0
+    , 'K_PIDKmin'       : 0.0
+    , 'P_PIDPmin'       : 0.0
+    , 'KKInc_K_Pmin'    : 10000.0 * MeV
+    , 'KKInc_K_PIDKmin' : 5.0
+    # D0 -> KPi
+    , 'D2KPi_APTmin'        : 0.0 * MeV
+    , 'D2KPi_ADAMASSmax'    : 80.0 * MeV
+    , 'D2KPi_ADOCAmax'      : 0.5 * mm
+    , 'D2KPi_APmin'         : 10000.0 * MeV
+    , 'D2KPi_SSumPTmin'     : 200.0 * MeV
+    , 'D2KPi_VtxChi2DoFmax' : 15.0
+    # D+- -> KPiPi
+    , 'D2KPiPi_APTmin'        : 0.0 * MeV
+    , 'D2KPiPi_ADAMASSmax'    : 80.0 * MeV
+    , 'D2KPiPi_ADOCAmax'      : 0.5 * mm
+    , 'D2KPiPi_APmin'         : 10000.0 * MeV
+    , 'D2KPiPi_SSumPTmin'     : 300.0 * MeV
+    , 'D2KPiPi_VtxChi2DoFmax' : 15.0
+    # D0 -> K3Pi
+    , 'D2K3Pi_APTmin'        : 0.0 * MeV
+    , 'D2K3Pi_ADAMASSmax'    : 80.0 * MeV
+    , 'D2K3Pi_ADOCAmax'      : 0.7 * mm
+    , 'D2K3Pi_APmin'         : 10000.0 * MeV
+    , 'D2K3Pi_SSumPTmin'     : 400.0 * MeV
+    , 'D2K3Pi_VtxChi2DoFmax' : 15.0
+    # 'ChiC' -> HH (H = K, Pi)
+    , 'ChiC2HH_APTmin'        : 0.0 * MeV
+    , 'ChiC2HH_APTmax'        : 5000.0 * MeV
+    , 'ChiC2HH_AMmin'         : 3300.0 * MeV
+    , 'ChiC2HH_AMmax'         : 3600.0 * MeV
+    , 'ChiC2HH_ADOCAmax'      : 0.5 * mm
+    , 'ChiC2HH_APmin'         : 10000.0 * MeV
+    , 'ChiC2HH_SSumPTmin'     : 200.0 * MeV
+    , 'ChiC2HH_VtxChi2DoFmax' : 15.0
+    # 'ChiC' -> PP
+    , 'ChiC2PP_APTmin'        : 0.0 * MeV
+    , 'ChiC2PP_APTmax'        : 5000.0 * MeV
+    , 'ChiC2PP_AMmin'         : 2850.0 * MeV
+    , 'ChiC2PP_AMmax'         : 3650.0 * MeV
+    , 'ChiC2PP_ADOCAmax'      : 0.5 * mm
+    , 'ChiC2PP_APmin'         : 10000.0 * MeV
+    , 'ChiC2PP_SSumPTmin'     : 200.0 * MeV
+    , 'ChiC2PP_VtxChi2DoFmax' : 15.0
+    # 'ChiC' -> HHHH (H = K, Pi)
+    , 'ChiC2HHHH_APTmin'        : 0.0 * MeV
+    , 'ChiC2HHHH_APTmax'        : 5000.0 * MeV
+    , 'ChiC2HHHH_AMmin'         : 2850.0 * MeV
+    , 'ChiC2HHHH_AMmid'         : 3650.0 * MeV
+    , 'ChiC2HHHH_AMmax'         : 4500.0 * MeV
+    , 'ChiC2HHHH_ADOCAmax'      : 0.7 * mm
+    , 'ChiC2HHHH_APmin'         : 10000.0 * MeV
+    , 'ChiC2HHHH_SSumPTmin'     : 400.0 * MeV
+    , 'ChiC2HHHH_VtxChi2DoFmax' : 15.0
     },
     'WGs' : [ 'QEE' ],
     'STREAMS' : [ 'EW' ]
