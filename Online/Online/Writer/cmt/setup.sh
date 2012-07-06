1af2830879d125c6892c2a9b7c520ccd24309a84
vsubbiah <vsubbiah@cern.ch>
2012-07-06 07:56:07 +0000

    Change version number to v1r41 for tagging.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@142385 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e6b1de7aa08f33f3d839a3e709b0c6c77b500d6b

--- Online/Online/Writer/cmt/setup.sh
+++ Online/Online/Writer/cmt/setup.sh
@@ -1,4 +1,4 @@
-# echo "Setting Writer v1r40 in /home/vijay/cmtuser/Online_v4r64/Online"
+# echo "Setting Writer v1r40 in /home/vijay/cmtuser/Online_v4r66/Online"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/sw/lib/contrib/CMT/v1r20p20090520; export CMTROOT
@@ -7,6 +7,6 @@ fi
 
 tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt setup -sh -pack=Writer -version=v1r40 -path=/home/vijay/cmtuser/Online_v4r64/Online  -no_cleanup $* >${tempfile}; . ${tempfile}
+${CMTROOT}/mgr/cmt setup -sh -pack=Writer -version=v1r40 -path=/home/vijay/cmtuser/Online_v4r66/Online  -no_cleanup $* >${tempfile}; . ${tempfile}
 /bin/rm -f ${tempfile}
 
