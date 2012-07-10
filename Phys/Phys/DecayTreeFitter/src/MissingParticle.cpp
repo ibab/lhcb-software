0965d7a92987f0a49bc7cd6447428cf4e87915ad
Wouter Hulsbergen <wouterh@nikhef.nl>
2012-07-10 19:55:41 +0000

    revived MissingParticle
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@142567 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3a0d43a0c9ba8f7ef84f9408de4459b3f024a476

--- Phys/Phys/DecayTreeFitter/src/MissingParticle.cpp
+++ Phys/Phys/DecayTreeFitter/src/MissingParticle.cpp
@@ -1,37 +1,44 @@
-// #include "FitParams.h"
-// #include "MissingParticle.h"
-// #include "Event/Particle.h"
+/**
+ To finish the implementation, this is what we need to do: We would like to make the dimension of the missing particle depend on
+ the mass constraint. That means that any call to 'setMassConstraint' should invalidate the 'indices'. The creation of indices is
+ done with 'DecayChain::updateIndex'. We could call this from 'initPar' (or from any other place that initialized the
+ constraints. That also means that 'setMassConstraint' should change Fitter.m_fitStatus to Unfitted'. Finally, to make sure that
+ we don't break anything, we need a test case.
 
-// namespace DecayTreeFitter
-// {
+*/
 
-//   extern int vtxverbose ;
+#include "FitParams.h"
+#include "MissingParticle.h"
+#include "Event/Particle.h"
 
-//   MissingParticle::MissingParticle(const LHCb::Particle* bc, const ParticleBase* mother)
-//     : ParticleBase(bc,mother),m_constrainMass(false)
-//   {
-//     // this will be one of the very few particles for which we adjust
-//     // the dimension if there is a constraint
-//     // copy constraints
-//     // m_constrainMass = bc && bc->constraint(BtaConstraint::Mass) ;
-//   }
+namespace DecayTreeFitter
+{
 
-//   MissingParticle::~MissingParticle() {}
+  extern int vtxverbose ;
 
-//   ErrCode MissingParticle::initPar1(FitParams* fitpar)
-//   {
-//     // take them from the bc
-//     Gaudi::LorentzVector p4 = bc() ? bc()->momentum() : Gaudi::LorentzVector(0,0,1,1) ;
-//     int momindex = momIndex();
-//     fitpar->par()(momindex+1) = p4.x() ;
-//     fitpar->par()(momindex+2) = p4.y() ;
-//     fitpar->par()(momindex+3) = p4.z() ;
-//     if(hasEnergy()) fitpar->par()(momindex+4) = p4.t() ;
-//     return ErrCode() ;
-//   }
-  
-//   std::string MissingParticle::parname(int index) const
-//   {
-//     return ParticleBase::parname(index+4) ;
-//   }
-// }
+  MissingParticle::MissingParticle(const LHCb::Particle& bc, const ParticleBase* mother)
+    : ParticleBase(bc,mother)
+  {
+    // this will be one of the very few particles for which we adjust
+    // the dimension if there is a constraint
+  }
+
+  MissingParticle::~MissingParticle() {}
+
+  ErrCode MissingParticle::initPar1(FitParams* fitpar)
+  {
+    // take them from the bc
+    Gaudi::LorentzVector p4 = particle().momentum() ;
+    int momindex = momIndex();
+    fitpar->par()(momindex+1) = p4.x() ;
+    fitpar->par()(momindex+2) = p4.y() ;
+    fitpar->par()(momindex+3) = p4.z() ;
+    if(hasEnergy()) fitpar->par()(momindex+4) = p4.t() ;
+    return ErrCode() ;
+  }
+
+  std::string MissingParticle::parname(int index) const
+  {
+    return ParticleBase::parname(index+4) ;
+  }
+}
--- Phys/Phys/DecayTreeFitter/src/MissingParticle.h
+++ Phys/Phys/DecayTreeFitter/src/MissingParticle.h
@@ -16,15 +16,11 @@ namespace DecayTreeFitter
     virtual ErrCode initPar2(FitParams*) { return ErrCode::success ; }
 
     virtual std::string parname(int index) const ;
-    virtual int dim() const  { return m_constrainMass ? 3 : 4; }   //(px,py,pz,E)
+    virtual int dim() const  { return hasMassConstraint() ? 3 : 4 ; }
     virtual int momIndex() const { return index() ; }
-    virtual bool hasEnergy() const { return m_constrainMass ? false : true ; }
+    virtual bool hasEnergy() const { return hasMassConstraint() ? false : true ; }
     virtual int type() const { return kMissingParticle ; }
-    virtual void addToConstraintList(constraintlist& /*alist*/, int /*depth*/) const {
-      //if(m_constrainMass) alist.push_back(Constraint(this,Constraint::mass,depth)) ; 
-    }
-  private:
-    bool m_constrainMass ;
+    virtual void addToConstraintList(constraintlist& /*alist*/, int /*depth*/) const {}
   } ;
 
 }
