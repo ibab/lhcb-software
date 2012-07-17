40005b017293f289828376aa8c88d7119e8fd998
Vanya Belyaev <ivan.belyaev@cern.ch>
2012-07-17 11:54:24 +0000

    add toHepDATA''
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@142818 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bdcc7bc54e8b6ff5f94c55b92b97e6cd4223230a

--- Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
+++ Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
@@ -3682,6 +3682,145 @@ ROOT.RooRealVar   . ve    = lambda s :      s.as_VE  ()
 ## further decoration
 import GaudiPython.HistoUtils
 logger.info ( 'Histogram utilities from GaudiPython.HistoUtils' )
+
+# =============================================================================
+_HS = cpp.Gaudi.Utils.Histos.HistoStrings
+# =============================================================================
+## convert histogram to string
+def _h_toString_ ( h , asXml = False  ) :
+    """
+    Convert histogram to string (or XML)
+
+    >>> h = ... # the histo
+    >>> s = h.toString()
+    >>> print s 
+    
+    """
+    return _HS.toString( h , asXml )
+
+_h_toString_ . __doc__  += '\n' + _HS.toString. __doc__
+
+# =============================================================================
+## convert histogram to XML
+def _h_toXml_ ( h ) :
+    """
+    Convert histogram to XML
+
+    >>> h = ... # the histo
+    >>> s = h.toXml()
+    >>> print s 
+    
+    """
+    return _HS.toXml( h )
+
+_h_toXml_ . __doc__  += '\n' + _HS.toXml. __doc__
+
+# =============================================================================
+## convert XML to histogram 
+def _h_fromString_ ( h , input  ) :
+    """
+    Convert string (or XLM) to histogram 
+
+    >>> input  = ... # the string(or XML)
+    >>> histo  = ... # book some the histo
+    >>> status = histo.fromString ( input ) 
+
+    """
+    return _HS.fromString ( h , input )
+
+_h_fromString_ . __doc__  += '\n' + _HS.fromString. __doc__
+
+# =============================================================================
+## convert XML to histogram 
+def _h_fromXml_ ( h , xml ) :
+    """
+    Convert XML to histogram 
+
+    >>> xml   = ... # the XML-string
+    >>> histo = ... # book some the histo
+    >>> status = histo.fromXml ( xml ) 
+
+    """
+    return _HS.fromXml ( h , xml )
+
+_h_fromXml_ . __doc__  += '\n' + _HS.fromXml. __doc__
+
+
+for t in ( ROOT.TH1D       ,
+           ROOT.TH1F       ,
+           ROOT.TH2D       ,
+           ROOT.TH2F       ,     
+           ROOT.TH3D       ,     
+           ROOT.TProfile   ,
+           ROOT.TProfile2D ) : 
+    
+    t.toString   = _h_toString_
+    t.fromString = _h_fromString_
+    t.toXml      = _h_toXml_
+    t.fromXml    = _h_fromXml_
+    
+# =============================================================================
+## HEPDATA format
+# 
+# @code 
+# The format we accept data in is very wide and generally we require only
+# a flat file containing the numerical values.
+# Postscript and pdf figures are not suitable.
+# 
+# Ideally the format should be:
+#
+# xlow xhigh y +stat -stat +sys1 -sys1 +sys2 -sys2 ......
+# where:
+#  xlow and xhigh are the xbin edges
+#  y is the measured quantity
+# +stat and -stat are the positive and negative statistical errors (could also be +-stat)
+# +sysn and -sysn are any number of positive and negative systematic errors (again could be +-sysn)
+# 
+# =============================================================================
+def _h1_hepdata_ ( h1 , fmt = " %13.6g %-13.6g   %13.6g +-%-12.5g \n" ) : 
+    """
+    Dump histogram in HepData -compatible format
+    
+    '''
+    The format we accept data in is very wide and generally we require only
+    a flat file containing the numerical values.
+    Postscript and pdf figures are not suitable.
+    
+    Ideally the format should be:
+    
+    xlow xhigh y +stat -stat +sys1 -sys1 +sys2 -sys2 ......
+    where:
+    xlow and xhigh are the xbin edges
+    y is the measured quantity
+    +stat and -stat are the positive and negative statistical errors (could also be +-stat)
+    +sysn and -sysn are any number of positive and negative systematic errors (again could be +-sysn)
+    '''
+    
+    >>> h = ... # the histogram
+    >>> data = h.toHepDATA ()
+    
+    """
+    data = ''
+    for  i in h1.iteritems () :
+        
+        x = i[1]
+        y = i[2]
+        
+        x_low  = x.value() - x.error()
+        x_high = x.value() - x.error()
+        
+        data += fmt  % ( x_low      ,
+                         x_high     ,
+                         y.value () ,
+                         y.error () )
+        
+    return data
+
+
+ROOT.TH1D.toHepDATA = _h1_hepdata_
+ROOT.TH1D.toHepData = _h1_hepdata_
+
+    
 # =============================================================================
 if '__main__' == __name__ :
     
