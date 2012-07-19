5114d486f620180f57506da10d7aa6b4a0952b38
Gloria Corti <gloria.corti@cern.ch>
2012-07-19 20:04:14 +0000

    restore factory declaration for PythiaProduction
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@142968 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e92895445dac723ed41967448b7b71da65d4adf8

---
+++ Gauss/Gen/LbPythia/src/component/PythiaProductionFactory.cpp
@@ -0,0 +1,9 @@
+// Include files
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "LbPythia/PythiaProduction.h"
+
+// It must be present otherwise the class cannot be instanciated, since the
+// Factory is in the linker library since it can (and is) specialized by 
+// other production tools based on Pythia
+DECLARE_TOOL_FACTORY( PythiaProduction );
+
