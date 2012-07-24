cab99ec6ab675cc63e8ab63506d2c93ebf5521cc
Matt Coombes <matt.coombes@cern.ch>
2012-07-24 08:32:54 +0000

    Added Grid submission tool in Users/Matt
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@143176 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 080105436951b6bb6c56930e06bc793387384f91

---
+++ Gauss/Gen/Mint/src/Users/Matt/ModelBuilderGridSubmission/SetupUpModelBuilder.py
@@ -0,0 +1,16 @@
+import os
+#from ModelFinder import *
+#gangaArea = "/afs/cern.ch/work/m/mcoombes/Ganga/ganga_workspace1/workspace/mcoombes/LocalXML"
+modelTextFile = "Models.txt" # output models to this text file
+#Chi2TextFile = "Chi2.txt"
+max_iterations  = 5
+MintDir = "/afs/cern.ch/work/m/mcoombes/cmtuser/Gen/Mint/src/"
+UserArea = "Users/Matt/FourPiModelBuilder/" #AmpFit Users Area
+
+inputLFNs = ['LFN:/lhcb/user/m/mcoombes/FourPiModelFit/BinningFile_D0.root',
+             'LFN:/lhcb/user/m/mcoombes/FourPiModelFit/NocutsMC.root']
+
+ModelBuilderExe = MintDir+"Users/Matt/ModelCreator/ModelBuilder"
+
+SubmitModelBuilder = MintDir+'Users/mcoombes/ModelBuilderSubmission/ModelSubmission.py'
+
