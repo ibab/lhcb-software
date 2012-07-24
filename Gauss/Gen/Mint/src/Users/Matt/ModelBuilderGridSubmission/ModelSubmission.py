d39f3629429b5abcd51d87e4dc589f3354eee18c
Matt Coombes <matt.coombes@cern.ch>
2012-07-24 09:48:47 +0000

    Added BaseDir and Grid Submission
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@143182 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8465587bfa9bbf1a8739f7a770d8546a9d8d3658

--- Gauss/Gen/Mint/src/Users/Matt/ModelBuilderGridSubmission/ModelSubmission.py
+++ Gauss/Gen/Mint/src/Users/Matt/ModelBuilderGridSubmission/ModelSubmission.py
@@ -2,8 +2,80 @@
 import os
 import time
 
-from ModelCreator import *
+class ModelCreator:
+    constDecays = ['D0->a(1)(1260)+[D](->rho(770)0(->pi+,pi-),pi+),pi-',
+                  'D0->a(1)(1260)+(->sigma10(->pi+,pi-),pi+),pi-',
+                  'D0->rho(770)0(->pi+,pi-),rho(770)0(->pi+,pi-)',
+                  'D0[P]->rho(770)0(->pi+,pi-),rho(770)0(->pi+,pi-)',
+                  'D0[D]->rho(770)0(->pi+,pi-),rho(770)0(->pi+,pi-)']
+    
+    optionalDecays = ['FocusFlatteD0->NonResVbar0(->pi+,pi-),f(0)(980)0(->pi+,pi-)',
+                      'D0->sigma10(->pi+,pi-),pi+,pi-']
+    modelsUsed = []
+    model = []
+    fixedDecay = 'D0->a(1)(1260)+(->rho(770)0(->pi+,pi-),pi+),pi-'
+    def __init__(self):
+        for decay in self.constDecays:
+            self.modelsUsed.append(decay)
+#        for decay in self.optionalDecays:
+#            self.modelsUsed.append(decay)
+    
+    def MakeModel(self):
+        self.model.append('"'+self.fixedDecay+'_Amp"    1  30.1  0.1  0.0  0.0\n')
+        self.model.append('"'+self.fixedDecay+'_Phase"    1  100  0.5  0.0  0.0\n\n')
+        for model in self.modelsUsed:
+            self.model.append('"'+model+'_Amp"    0  0.1  0.1  0.0  0.0\n')
+            self.model.append('"'+model+'_Phase"  0  100  0.5  0.0  0.0\n')
+            self.model.append('*\n')
+        return model
+    
+    def Write(self,fileName):
+        file = open(fileName,"w")
+        
+        #Write Fixed Decay
+        file.writelines('"'+self.fixedDecay+'_Amp"    1  30.1  0.1  0.0  0.0\n')
+        file.writelines('"'+self.fixedDecay+'_Phase"    1  100  0.5  0.0  0.0\n\n')
 
+        for model in self.modelsUsed:
+            file.writelines('"'+model+'_Amp"    0  0.1  0.1  0.0  0.0\n')
+            file.writelines('"'+model+'_Phase"  0  100  0.5  0.0  0.0\n')
+            file.writelines('*\n')
+        file.close()
+    
+    def MakeInputFile(self,inputBaseFile,outputFile):
+        self.MakeModel()
+        file = open(inputBaseFile)
+        outputlines=[]
+        while 1:
+            line = file.readline()# read file
+            if not line:
+                    break
+            pass
+            outputlines.append(line)
+        file.close()
+        
+        for decay in self.model:
+            outputlines.append(decay)
+        
+        outFile = open(outputFile,"w")
+        outFile.writelines(outputlines)
+        outFile.close()
+        
+        
+"""
+Example :
+           
+modelsForIt = open(modelTextFile,"w")
+decs = ModelCreator()
+optionalDecays = decs.optionalDecays
+for j in range(0,len(optionalDecays)):
+    myModel = ModelCreator()
+    myModel.modelsUsed.append(optionalDecays[j])
+    myModel.MakeInputFile("D_4pi_Base.txt", "D_4pi_"+str(j)+".txt")
+    modelsForIt.writelines("D_4pi_"+str(j)+".txt\n")
+modelsForIt.close()
+
+"""
 class Models:
     inputfile = ''
     def __init__(self,file='FileOfModels.txt'):
@@ -48,7 +120,7 @@ class ModelFinderJob:
     def SetupJob(self):
         app = GaudiPython(project='DaVinci', version='v29r2') #v25r0  
         self.j.application = app
-        script=self.userarea+"Grid/GangaSubmit.py"
+        script=self.basedir+"Users/Matt/ModelBuilderGridSubmission/GangaSubmit.py"
         exe=self.userarea+"ampFit"
         input_mass=self.ParticleProp+"mass_width.cvs"
         input_parts=self.ParticleProp+"MintDalitzSpecialParticles.cvs"
@@ -115,21 +187,23 @@ class ModelFinderJob:
 
 
 def RunModelFinder(MintDir,UserArea,inputLFNs,modelTextFile,ModelBuilderExe,max_iterations):
-    print "GERE"
+    useModelCreator = True
     for i in range (0,max_iterations):
         print i 
         if (i == 0):
             print "Run jacks code first iteration"
+            print "Will create an empty file to feed to Jack for iteration 0"
             modelsForIt = open(modelTextFile,"w")
-            decs = ModelCreator()
-            optionalDecays = decs.optionalDecays
-            for j in range(0,len(optionalDecays)):
-                myModel = ModelCreator()
-                myModel.modelsUsed.append(optionalDecays[j])
-                myModel.MakeInputFile("D_4pi_Base.txt", "FourPiModel_"+str(j)+".txt")
-                modelsForIt.writelines("FourPiModel_"+str(j)+".txt\n")
+            if (useModelCreator):
+                decs = ModelCreator()
+                optionalDecays = decs.optionalDecays
+                for j in range(0,len(optionalDecays)):
+                    myModel = ModelCreator()
+                    myModel.modelsUsed.append(optionalDecays[j])
+                    myModel.MakeInputFile(MintDir+"Users/Matt/ModelBuilderGridSubmission/D_4pi_Base.txt", "FourPiModel_"+str(j)+".txt")
+                    modelsForIt.writelines("FourPiModel_"+str(j)+".txt\n")
             modelsForIt.close()
-#            os.system('./'+ModelBuilderExe+' > modelbuilder.txt') # Ask Jack about iteration 0
+            os.system('./'+ModelBuilderExe+' '+str(modelTextFile)+' > modelbuilder.txt') # Ask Jack about iteration 0
         else:
             print "Run iteration ", i, " of Jacks code"
             print "Input ", modelTextFile
--- Gauss/Gen/Mint/src/Users/Matt/ModelBuilderGridSubmission/SetupUpModelBuilder.py
+++ Gauss/Gen/Mint/src/Users/Matt/ModelBuilderGridSubmission/SetupUpModelBuilder.py
@@ -4,13 +4,28 @@ import os
 modelTextFile = "Models.txt" # output models to this text file
 #Chi2TextFile = "Chi2.txt"
 max_iterations  = 5
+
+
 MintDir = "/afs/cern.ch/work/m/mcoombes/cmtuser/Gen/Mint/src/"
+
+#Users area directory where ampFit is.
 UserArea = "Users/Matt/FourPiModelBuilder/" #AmpFit Users Area
 
 inputLFNs = ['LFN:/lhcb/user/m/mcoombes/FourPiModelFit/BinningFile_D0.root',
              'LFN:/lhcb/user/m/mcoombes/FourPiModelFit/NocutsMC.root']
 
+#Where is the executable for the ModelBuilder.
 ModelBuilderExe = MintDir+"Users/Matt/ModelCreator/ModelBuilder"
 
-SubmitModelBuilder = MintDir+'Users/Matt/ModelBuilderSubmission/ModelSubmission.py'
+#Keep as this directory. Points to where the submission script is
+SubmitModelBuilder = MintDir+'Users/Matt/ModelBuilderGridSubmission/ModelSubmission.py'
 
+
+print "Before submitting make sure you have compiled ampFit with DaVinci v29r2 in order to get the correct ROOT version"
+print "\n"
+
+if ( not( os.path.exists("BG4piModel.txt") ) ) :
+    print "ERROR: file ", MintDir+UserArea+"BG4piModel.txt does not exist"
+    print "You must have the background model file BG4piModel.txt in userarea\n\n"
+    
+print "To run type in the Ganga prompt execfile(SubmitModelBuilder)"
