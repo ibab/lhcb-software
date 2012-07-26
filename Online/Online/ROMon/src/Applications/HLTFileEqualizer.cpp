35ebf9ad1f7296530b4132e5e85735464d106589
Beat Jost <beat.jost@cern.ch>
2012-07-26 14:50:45 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@143417 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: c63abde025b33104a32e6161e3decdcdb4749b15

--- Online/Online/ROMon/src/Applications/HLTFileEqualizer.cpp
+++ Online/Online/ROMon/src/Applications/HLTFileEqualizer.cpp
@@ -338,7 +338,7 @@ void HLTFileEqualizer::Dump()
     sscanf(nod->m_name.substr(6,2).c_str(),"%d",&indx);
     char nfil[10];
     sprintf(nfil,"%4d",nod->m_nofiles);
-    line.replace(10+indx*5,4,nfil);
+    line.replace(10+(indx-1)*5,4,nfil);
   }
   fflush(outf);
 };
