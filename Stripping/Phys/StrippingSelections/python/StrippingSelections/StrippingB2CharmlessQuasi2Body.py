26094a1c690298680126ad616106194eb19bb765
Frederic Henry Blanc <fred.blanc@epfl.ch>
2012-08-02 14:24:19 +0000

    modified StrippingB2CharmlessQuasi2Body lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@143758 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3cc4c600f66076360abaa34e6766122b05ba8a42

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CharmlessQuasi2Body.py
@@ -2,9 +2,9 @@
 B->charmless quasi 2-body selection
 '''
 
-__author__ = ['Fred Blanc']
-__date__ = '06/01/2012'
-__version__ = '$Revision: 2.4 $'
+__author__ = ['Fred Blanc', 'Albert Puig']
+__date__ = '02/08/2012'
+__version__ = '$Revision: 2.5 $'
 
 __all__ = ( 'B2Quasi2BodyConf',
             'makeDiTrackList',
@@ -13,16 +13,14 @@ __all__ = ( 'B2Quasi2BodyConf',
 
 config_params = {'Q2BPrescale'     : 1.,
                  'Q2BResMinPT'     : 600.,
-                 'Q2BResMinP'      : 10.,
                  'Q2BResMaxMass'   : 1000.,
-                 'Q2BResVtxChiDOF' : 9.,
+                 'Q2BResVtxChi2DOF': 9.,
+                 'Q2BBMinPT'       : 2500.,
                  'Q2BBMinM4pi'     : 3800.,
                  'Q2BBMinM3pi'     : 4400.,
                  'Q2BBMaxM'        : 5700.,
-                 'Q2BBMinCorrM4pi' : 0.,
-                 'Q2BBMinCorrM3pi' : 0.,
                  'Q2BBMaxCorrM'    : 6000.,
-                 'Q2BBVtxChi2DOF'  : 9.}
+                 'Q2BBVtxChi2DOF'  : 6.}
 
 
 from Gaudi.Configuration import *
@@ -41,14 +39,12 @@ class B2Quasi2BodyConf(LineBuilder) :
 
     __configuration_keys__ = ( 'Q2BPrescale',
                                'Q2BResMinPT',
-                               'Q2BResMinP',
                                'Q2BResMaxMass',
-                               'Q2BResVtxChiDOF',
+                               'Q2BResVtxChi2DOF',
+                               'Q2BBMinPT',
                                'Q2BBMinM4pi',
                                'Q2BBMinM3pi',
                                'Q2BBMaxM',
-                               'Q2BBMinCorrM4pi',
-                               'Q2BBMinCorrM3pi',
                                'Q2BBMaxCorrM',
                                'Q2BBVtxChi2DOF')
 
@@ -56,12 +52,12 @@ class B2Quasi2BodyConf(LineBuilder) :
 	self.name = name
         LineBuilder.__init__(self, name, config)
 
-        _trkFilter = FilterDesktop(Code = "(TRCHI2DOF < 4) & (MIPCHI2DV(PRIMARY) > 25)")
+        _trkFilter = FilterDesktop(Code = "(MIPCHI2DV(PRIMARY) > 25)")
         self.TrackList = Selection( 'TrackList' + self.name,
                                     Algorithm = _trkFilter,
                                     RequiredSelections = [StdNoPIDsPions])
 
-        _trkFilter_HiP = FilterDesktop(Code = "(PT > 1000*MeV) & (P > 10*GeV)")
+        _trkFilter_HiP = FilterDesktop(Code = "(PT > 1000)")
         self.TrackList_HiP = Selection( 'TrackList_HiP' + self.name,
                                          Algorithm = _trkFilter_HiP,
                                          RequiredSelections = [self.TrackList])
@@ -69,41 +65,40 @@ class B2Quasi2BodyConf(LineBuilder) :
         self.DiTrackList = makeDiTrackList( name="DiTracksForCharmlessB" + self.name,
                                             trkList=self.TrackList,
                                             MinPTCut = config['Q2BResMinPT'],
-                                            MinPCut = config['Q2BResMinP'],
                                             MaxMassCut = config['Q2BResMaxMass'],
-                                            VtxChi2DOFCut = config['Q2BResVtxChiDOF'] )
+                                            VtxChi2DOFCut = config['Q2BResVtxChi2DOF'] )
 
-        _diTrackFilter_HiPt = FilterDesktop(Code = "(PT > 2000*MeV)")
+        _diTrackFilter_HiPt = FilterDesktop(Code = "(PT > 1000)")
         self.DiTrackList_HiPt = Selection( 'DiTracksHiPtForCharmlessB' + self.name,
                                            Algorithm = _diTrackFilter_HiPt,
                                            RequiredSelections = [self.DiTrackList])
 
         B2Q2B4piName = self.name + "4piSelection"
         self.B2CharmlessQ2B4pi = makeB2Q2B4pi( B2Q2B4piName,
-                                                   diTrkList=self.DiTrackList,
-                                                   MinMassCut = config['Q2BBMinM4pi'],
-                                                   MaxMassCut = config['Q2BBMaxM'],
-                                                   MinCorrMCut = config['Q2BBMinCorrM4pi'],
-                                                   MaxCorrMCut = config['Q2BBMaxCorrM'],
-                                                   VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
+                                               diTrkList=self.DiTrackList,
+                                               MinPTCut = config['Q2BBMinPT'],
+                                               MinMassCut = config['Q2BBMinM4pi'],
+                                               MaxMassCut = config['Q2BBMaxM'],
+                                               MaxCorrMCut = config['Q2BBMaxCorrM'],
+                                               VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
 
         self.Q2B4piLine = StrippingLine( B2Q2B4piName+"Line",
-                                           prescale = config['Q2BPrescale'],
-                                           selection = self.B2CharmlessQ2B4pi )
+                                         prescale = config['Q2BPrescale'],
+                                         selection = tisTosSelection(self.B2CharmlessQ2B4pi) )
 
         B2Q2B3piName = self.name + "3piSelection"
         self.B2CharmlessQ2B3pi = makeB2Q2B3pi( B2Q2B3piName,
                                                trkList = self.TrackList_HiP,
                                                diTrkList=self.DiTrackList_HiPt,
+                                               MinPTCut = config['Q2BBMinPT'],
                                                MinMassCut = config['Q2BBMinM3pi'],
                                                MaxMassCut = config['Q2BBMaxM'],
-                                               MinCorrMCut = config['Q2BBMinCorrM3pi'],
                                                MaxCorrMCut = config['Q2BBMaxCorrM'],
                                                VtxChi2DOFCut = config['Q2BBVtxChi2DOF'] )
 
         self.Q2B3piLine = StrippingLine( B2Q2B3piName+"Line",
                                          prescale = config['Q2BPrescale'],
-                                         selection = self.B2CharmlessQ2B3pi )
+                                         selection = tisTosSelection(self.B2CharmlessQ2B3pi) )
 
         self.registerLine(self.Q2B4piLine)
         self.registerLine(self.Q2B3piLine)
@@ -111,13 +106,12 @@ class B2Quasi2BodyConf(LineBuilder) :
 def makeDiTrackList( name,
                      trkList,
                      MinPTCut,
-                     MinPCut,
                      MaxMassCut,
                      VtxChi2DOFCut ) :
     """
     Di-track selection
     """
-    _diTrackPreVertexCuts = "(APT> %(MinPTCut)s *MeV) & (AP> %(MinPCut)s *GeV) & (AM< %(MaxMassCut)s *MeV)" %locals()
+    _diTrackPreVertexCuts = "(APT> %(MinPTCut)s) & (AM< %(MaxMassCut)s)" %locals()
     _diTrackPostVertexCuts = "(VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s)" %locals()
 
     _combineDiTrack = CombineParticles( DecayDescriptor="rho(770)0 -> pi+ pi-",
@@ -129,20 +123,20 @@ def makeDiTrackList( name,
                      RequiredSelections = [ trkList ] )
 
 def makeB2Q2B4pi( name,
-                    diTrkList,
-                    MinMassCut,
-                    MaxMassCut,
-                    MinCorrMCut,
-                    MaxCorrMCut,
-                    VtxChi2DOFCut ) :
+                  diTrkList,
+                  MinPTCut,
+                  MinMassCut,
+                  MaxMassCut,
+                  MaxCorrMCut,
+                  VtxChi2DOFCut ) :
     """
     Charmless Q2B to 4pi selection with missing mass
     """
 
     _B2Q2B4piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
-    _B2Q2B4piPreVertexCuts += " & ( APT > 1000 )" %locals()
+    _B2Q2B4piPreVertexCuts += " & ( APT > %(MinPTCut)s )" %locals()
 
-    _B2Q2B4piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
+    _B2Q2B4piPostVertexCuts = "( BPVCORRM < %(MaxCorrMCut)s )" %locals()
     _B2Q2B4piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s )" %locals()
 
     _combineB2Q2B4pi = CombineParticles( DecayDescriptor="B0 -> rho(770)0 rho(770)0",
@@ -157,9 +151,9 @@ def makeB2Q2B4pi( name,
 def makeB2Q2B3pi( name,
                   trkList,
                   diTrkList,
+                  MinPTCut,
                   MinMassCut,
                   MaxMassCut,
-                  MinCorrMCut,
                   MaxCorrMCut,
                   VtxChi2DOFCut ) :
     """
@@ -167,9 +161,9 @@ def makeB2Q2B3pi( name,
     """
 
     _B2Q2B3piPreVertexCuts = "in_range( %(MinMassCut)s ,AM, %(MaxMassCut)s )" %locals()
-    _B2Q2B3piPreVertexCuts += " & ( APT > 1000 )" %locals()
+    _B2Q2B3piPreVertexCuts += " & ( APT > %(MinPTCut)s )" %locals()
 
-    _B2Q2B3piPostVertexCuts = "in_range( %(MinCorrMCut)s ,BPVCORRM, %(MaxCorrMCut)s )" %locals()
+    _B2Q2B3piPostVertexCuts = "( BPVCORRM < %(MaxCorrMCut)s )" %locals()
     _B2Q2B3piPostVertexCuts += " & (VFASPF(VCHI2/VDOF) < %(VtxChi2DOFCut)s ) " %locals()
 
     _combineB2Q2B3pi = CombineParticles( DecayDescriptor="[B+ -> rho(770)0 pi+]cc",
@@ -179,3 +173,21 @@ def makeB2Q2B3pi( name,
     return Selection(name,
                      Algorithm = _combineB2Q2B3pi,
                      RequiredSelections = [ diTrkList, trkList ] )
+
+
+def makeTISTOSFilter(name):
+    specs = {'Hlt1Track.*Decision%TOS':0}
+    from Configurables import TisTosParticleTagger
+    tisTosFilter = TisTosParticleTagger(name+'TISTOSFilter')
+    tisTosFilter.TisTosSpecs = specs
+    tisTosFilter.ProjectTracksToCalo = False
+    tisTosFilter.CaloClustForCharged = False
+    tisTosFilter.CaloClustForNeutral = False
+    tisTosFilter.TOSFrac = {4:0.0, 5:0.0}
+    return tisTosFilter
+
+def tisTosSelection(sel):
+    '''Filters Selection sel to be TOS OR TIS.'''
+    tisTosFilter = makeTISTOSFilter(sel.name())
+    return Selection(sel.name()+'TISTOS', Algorithm=tisTosFilter,
+                     RequiredSelections=[sel])
