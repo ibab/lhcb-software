f23898556c942370011c4bab844dac3c998271d7
Marco Clemencic <marcocle@cern.ch>
2012-08-09 14:45:05 +0000

    Workaround for bug #96673.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@143953 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c6eb327bf2f6ed4c42815977a222b27831839719

---
+++ LHCb/Det/DetCond/python/DetCond/__init__.py
@@ -0,0 +1,4 @@
+# Hack to prevent spurious warnings in GenConfUser.
+# See bug #96673, https://savannah.cern.ch/bugs/index.php?96673
+import os,sys
+__path__ = filter(os.path.exists, [os.path.join(d, __name__) for d in sys.path if d])
