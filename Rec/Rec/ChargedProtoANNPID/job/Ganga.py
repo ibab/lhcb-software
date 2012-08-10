cdb3e302eee0f00ebc6985eba177131575983f58
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-08-10 13:22:21 +0000

    Update the default tuning for 2012 and 2011 to MC12TuneV1, trained on a    preliminary sample of 2012 B inclusive MC processed with Reco13d.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@144000 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 709a635f10a331c57d98b03ca98d6cdd69ec352c

--- Rec/Rec/ChargedProtoANNPID/job/Ganga.py
+++ Rec/Rec/ChargedProtoANNPID/job/Ganga.py
@@ -1,18 +1,19 @@
 
 for polarity in ["MagUp","MagDown"]:
 
-    reco="Reco13d"
-    strp="Stripping19b"
+    reco="Reco13a"
+    strp="Stripping19a"
 
-    j = Job( application = DaVinci( version = 'v32r1p1' ) )
-    j.name = "MC2012-ANNPID-"+polarity+"-"+reco+"-"+strp
+    j = Job( application = DaVinci( version = 'v32r1p2' ) )
+    j.name = "MC2012-ANNPID-"+polarity+"-"+reco+"-"+strp+"-TEST"
 
     datapath = "/MC/DEV/Beam4000GeV-MayJune2012-"+polarity+"-Nu2.5-EmNoCuts/Sim06a/Trig0x0097003dFlagged/"+reco+"/"+strp+"NoPrescalingFlagged/10000000/ALLSTREAMS.DST"
 
     # Main options
     j.application.optsfile = [ File('options.py') ]
 
-    j.splitter = SplitByFiles ( filesPerJob = 3, maxFiles = 999999 )
+    j.splitter = DiracSplitter ( filesPerJob = 2, maxFiles = 999999 )
+    #j.splitter = SplitByFiles ( filesPerJob = 2, maxFiles = 6 )
 
     rootfiles = [ 'ProtoPIDANN.tuples.root' ]
 
--- Rec/Rec/ChargedProtoANNPID/python/ChargedProtoANNPID/Configuration.py
+++ Rec/Rec/ChargedProtoANNPID/python/ChargedProtoANNPID/Configuration.py
@@ -21,8 +21,8 @@ class ChargedProtoANNPIDConf(LHCbConfigurableUser):
                   ,"RecoSequencer" : None    # The sequencer to use
                   ,"OutputLevel" : INFO      # The printout level to use
                   ,"ProtoParticlesLocation" : None
-                  ,"NetworkVersions" : { "2012" : "MC11aTuneV3", 
-                                         "2011" : "MC11aTuneV3", 
+                  ,"NetworkVersions" : { "2012" : "MC12TuneV1", 
+                                         "2011" : "MC12TuneV1", 
                                          "2010" : "MC11aTuneV3",
                                          "2009" : "MC10TuneV1",
                                          "2008" : "MC10TuneV1",
