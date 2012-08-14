2c62377b47f92db2748b912143230468f73c0558
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-08-14 15:24:20 +0000

    update ROOT scripts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@144074 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 26e480a73759edec47eed7a89e906ce56d4e2da5

--- Rec/Rec/GlobalReco/root/MakeRichPlots.C
+++ Rec/Rec/GlobalReco/root/MakeRichPlots.C
@@ -10,15 +10,13 @@
 
   TCut trackSel = "TrackType == 3 && TrackP > 10000 && TrackP < 100000";
 
-  TCut kaonDllCut = "RichDLLk>0";
-
   TCut realK  = "abs(MCParticleType) == 321";
   TCut fakeK  = "abs(MCParticleType) != 321";
 
   TCut realPi = "abs(MCParticleType) == 11";
   TCut fakePi = "abs(MCParticleType) != 11";
 
-  TCanvas * c = new TCanvas();
+  TCanvas * c = new TCanvas( "RICH", "RICH", 1400, 1000 );
 
   std::vector<double> cuts;
   cuts.push_back(-5);
@@ -32,7 +30,7 @@
     cC << *iC;
 
     // DLL>0
-    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>kIDEff").c_str(), detOK && realK && trackSel, "prof" );
+    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>kIDEff").c_str(),     detOK && realK  && trackSel, "prof" );
     tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>piMisIDEff").c_str(), detOK && realPi && trackSel, "prof" );
 
     kIDEff->SetTitle( ("DLLk>"+cC.str()).c_str() );
@@ -45,6 +43,7 @@
     piMisIDEff->SetLineColor(kBlue);
 
     c->SaveAs( ("KaonID-DLL"+cC.str()+".png").c_str() );
+
   }
 
 }
