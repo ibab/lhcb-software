01dcaa443c83a3e74fcfb70d24691563716343a6
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-08-15 10:38:59 +0000

    fix typo
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@144093 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4691a8cb194ae987bf7abcbbe120af4b4b81c6fd

--- Rec/Rec/GlobalReco/root/MakeRichPlots.C
+++ Rec/Rec/GlobalReco/root/MakeRichPlots.C
@@ -2,19 +2,22 @@
 {
 
   // load the file
-  TFile * f = new TFile("/afs/cern.ch/user/s/seaso/public/Rich/Upgrade/RichLumi/NtupleTest/protoparticles.tuples.root");
+  TFile * f = new TFile("protoparticles.tuples.root");
 
   TTree * tree = (TTree*)gDirectory->Get("ChargedProtoTuple/protoPtuple");
 
-  TCut detOK = "RichUsedAero || RichUsedR1Gas || RichUsedR2Gas";
+  TCut detOK = "RichUsedAero == 1 || RichUsedR1Gas == 1 || RichUsedR2Gas == 1";
 
-  TCut trackSel = "TrackType == 3 && TrackP > 10000 && TrackP < 100000";
+  TCut trackSel = "TrackType == 3 && TrackP > 10000 && TrackP < 100000 && TrackPt > 1000 ";
 
   TCut realK  = "abs(MCParticleType) == 321";
   TCut fakeK  = "abs(MCParticleType) != 321";
 
-  TCut realPi = "abs(MCParticleType) == 11";
-  TCut fakePi = "abs(MCParticleType) != 11";
+  TCut realPi = "abs(MCParticleType) == 211";
+  TCut fakePi = "abs(MCParticleType) != 211";
+
+  TCut kAboveThres  = "RichAboveKaThres == 1";
+  TCut piAboveThres = "RichAbovePiThres == 1";
 
   TCanvas * c = new TCanvas( "RICH", "RICH", 1400, 1000 );
 
@@ -29,8 +32,10 @@
     std::ostringstream cC; 
     cC << *iC;
 
-    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>kIDEff").c_str(),     detOK && realK  && trackSel, "prof" );
-    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>piMisIDEff").c_str(), detOK && realPi && trackSel, "prof" );
+    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>kIDEff").c_str(),     
+                detOK && realK  && trackSel && kAboveThres, "prof" );
+    tree->Draw( ("(RichDLLk>"+cC.str()+"?100:0):TrackP>>piMisIDEff").c_str(), 
+                detOK && realPi && trackSel && piAboveThres, "prof" );
 
     kIDEff->SetTitle( ("DLLk>"+cC.str()).c_str() );
     kIDEff->Draw();
