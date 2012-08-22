59c3fcaa72f4bc154ccdabd6b2e830e7a72c06cd
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-08-22 12:24:15 +0000

    Remove the temporary hack in the last release, to rerun the Tracking    Ghost Probability algorithm
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@144249 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 495e84061e762076c47268b53fdccd7083f3fc56

--- Rec/Rec/RecConf/python/RecConf/Configuration.py
+++ Rec/Rec/RecConf/python/RecConf/Configuration.py
@@ -187,17 +187,9 @@ class RecSysConf(LHCbConfigurableUser):
 
         # PROTO
         if "PROTO" in recoSeq:
-            protoSeq = GaudiSequencer("ProtoSeq")
-            GaudiSequencer("RecoPROTOSeq").Members += [protoSeq]
             self.setOtherProps(GlobalRecoConf(),["DataType","SpecialData",
                                                  "Context","OutputLevel"])
-            GlobalRecoConf().RecoSequencer = protoSeq
-            # hack - Rerun Tracking Ghost prob with new settings
-            from Configurables import TrackAddNNGhostId, TrackNNGhostId
-            nn = TrackAddNNGhostId("TrackAddNewNNGhostId")
-            nn.addTool( TrackNNGhostId, name = 'TrackNNGhostId' )
-            nn.TrackNNGhostId.IsMC2012Tuning = True
-            GaudiSequencer("RecoPROTOSeq").Members += [nn]
+            GlobalRecoConf().RecoSequencer = GaudiSequencer("RecoPROTOSeq")
             
 
         # SUMMARY
