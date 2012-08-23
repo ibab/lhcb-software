6f8c93dfc09754a6726c3899727660ea07c1cc3c
Stefano Perazzini <stefano.perazzini@cern.ch>
2012-08-23 07:49:19 +0000

    Changes to StrippingHb2Charged2Body.py and creation of StrippingB2HHBkg.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@144288 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0a9afdd30f1323a60cddd678e294bcd885410f0c

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingHb2Charged2Body.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingHb2Charged2Body.py
@@ -1,12 +1,12 @@
 """
-Stripping options for (pre-)selecting B -> hh', B -> p pbar
-and Bs -> mu mu events.
-Authors: Angelo Carboni, Lars Eklumd, Vava Gligorov, Stefano Perazzini, Eduardo Rodrigues
+Stripping options for (pre-)selecting B -> hh'
+
+Authors: Stefano Perazzini, Lars Eklumd
 """
 
 ########################################################################
-__author__ = ['Stefano Perazzini','Angelo Carbone','Eduardo Rodrigues', 'Lars Eklund', 'Vava Gligorov']
-__date__ = '25/01/2012'
+__author__ = ['Stefano Perazzini', 'Lars Eklund']
+__date__ = '21/08/2012'
 __version__ = '$Revision: 1.5 $'
 
 __all__ = ('Hb2Charged2BodyLines',
@@ -24,24 +24,23 @@ from Configurables               import SubstitutePID, FilterDesktop
 
 
 default_config = {
-           'PrescaleB2Charged2Body'   : 1,      'PrescaleBs2KK_NoIPCutOnB' : 1,
+           'PrescaleB2Charged2Body'   : 1,
            'MinPTB2Charged2Body'      : 1100,
-           'MinIPB2Charged2Body'      : 0.12,
+           'MinIPB2Charged2Body'      : 0.15,
            'MinIPChi2B2Charged2Body'  : 100, 
-           'TrChi2'                   : 5,
-           'MaxPTB2Charged2Body'      : 2500, 
-           'MaxIPB2Charged2Body'      : 0.25,
+           'TrChi2'                   : 3,
+           'MaxPTB2Charged2Body'      : 2700, 
+           'MaxIPB2Charged2Body'      : 0.27,
            'MaxIPChi2B2Charged2Body'  : 200,  
-           'CombMassLow'              : 4800,
-           'CombMassHigh'             : 5800,
+           'CombMassLow'              : 4600,
+           'CombMassHigh'             : 6000,
            'DOCA'                     : 0.08,
            'BPT'                      : 1200,
-           'BIP'                      : 0.1,
+           'BIP'                      : 0.08,
            'BIPChi2B2Charged2Body'    : 12,  
            'BTAU'                     : 0.0006,
            'MassLow'                  : 4800,
-           'MassHigh'                 : 5800,
-           'PIDKBs2KK_NoIPCutOnB'     : 0.
+           'MassHigh'                 : 5800
            }
 
 ## Change decay descriptor and re-fit decay tree
@@ -66,7 +65,7 @@ def subPID(name, input, mother, plusD, minusD):
 class Hb2Charged2BodyLines( LineBuilder ) :
     """Class defining the Hb -> hh stripping lines"""
     
-    __configuration_keys__ = ( 'PrescaleB2Charged2Body',  'PrescaleBs2KK_NoIPCutOnB',
+    __configuration_keys__ = ( 'PrescaleB2Charged2Body',
                                'MinPTB2Charged2Body',    
                                'MinIPB2Charged2Body',
                                'MinIPChi2B2Charged2Body',
@@ -82,8 +81,7 @@ class Hb2Charged2BodyLines( LineBuilder ) :
                                'BIPChi2B2Charged2Body', 
                                'BTAU',
                                'MassLow',
-                               'MassHigh',
-                               'PIDKBs2KK_NoIPCutOnB' 
+                               'MassHigh'
                                )
     
     def __init__( self,name,config ) :        
@@ -98,7 +96,6 @@ class Hb2Charged2BodyLines( LineBuilder ) :
         Lb2PiPlusPMinusName  = name + "Lb2PiPlusPMinus"
         Lb2PPlusKMinusName   = name + "Lb2PPlusKMinus"
         Lb2KPlusPMinusName   = name + "Lb2KPlusPMinus"
-        Bs2KK_NoIPCutOnBName = name + "Bs2KK_NoIPCutOnB"
         
         # make the various stripping selections
         self.B2Charged2Body = makeB2Charged2Body( B2Charged2BodyName,
@@ -141,24 +138,6 @@ class Hb2Charged2BodyLines( LineBuilder ) :
         self.Lb2KPlusPMinus  = subPID(Lb2KPlusPMinusName, self.B2Charged2Body,
                                       "Lambda_b0", "K+", "p~-" )
         
-        self.Bs2KK_NoIPCutOnB = makeBs2KK_NoIPCutOnB( Bs2KK_NoIPCutOnBName,
-                                                  config['PIDKBs2KK_NoIPCutOnB'],
-                                                  config['TrChi2'],
-                                                  config['MinPTB2Charged2Body'],
-                                                  config['MinIPB2Charged2Body'],
-                                                  config['MinIPChi2B2Charged2Body'],
-                                                  config['MaxPTB2Charged2Body'],
-                                                  config['MaxIPB2Charged2Body'],
-                                                  config['MaxIPChi2B2Charged2Body'],
-                                                  config['CombMassLow'],
-                                                  config['CombMassHigh'],
-                                                  config['DOCA'],
-                                                  config['BPT'],
-                                                  config['BTAU'],
-                                                  config['MassLow'],
-                                                  config['MassHigh']
-                                                ) 
-
         self.lineB2Charged2Body = StrippingLine( B2Charged2BodyName+"Line",
                                                  prescale  = config['PrescaleB2Charged2Body'],
                                                  selection = self.B2Charged2Body )
@@ -191,10 +170,6 @@ class Hb2Charged2BodyLines( LineBuilder ) :
                                                   prescale  = config['PrescaleB2Charged2Body'],
                                                   selection = self.Lb2KPlusPMinus )
         
-        self.lineBs2KK_NoIPCutOnB = StrippingLine( Bs2KK_NoIPCutOnBName+"Line",
-                                                   prescale = config['PrescaleBs2KK_NoIPCutOnB'],
-                                                   selection = self.Bs2KK_NoIPCutOnB)      
- 
         self.registerLine(self.lineB2Charged2Body)
         self.registerLine(self.lineB2KPlusPiMinus)
         self.registerLine(self.lineB2PiPlusKMinus)
@@ -203,7 +178,6 @@ class Hb2Charged2BodyLines( LineBuilder ) :
         self.registerLine(self.lineLb2PiPlusPMinus)
         self.registerLine(self.lineLb2PPlusKMinus)
         self.registerLine(self.lineLb2KPlusPMinus)
-        self.registerLine(self.lineBs2KK_NoIPCutOnB)
 
 def makeB2Charged2Body( name, 
                         trChi2,minPT,minIP,minIPChi2,
@@ -226,20 +200,5 @@ def makeB2Charged2Body( name,
                       RequiredSelections = [ StdNoPIDsPions ] )    
 
 
-def makeBs2KK_NoIPCutOnB( name, pidk,trChi2,minPT,minIP,minIPChi2,
-                        maxPT,maxIP,maxIPChi2,combMassLow,combMassHigh,doca,
-                        bPT,bTAU,massLow,massHigh ) : 
-                        
-    _daughters_cuts = "(PIDK > %(pidk)s) & (TRCHI2DOF < %(trChi2)s) & (PT > %(minPT)s * MeV) & ( (MIPDV(PRIMARY) > %(minIP)s ) | ( MIPCHI2DV(PRIMARY) > %(minIPChi2)s  ) )" %locals()    
-    _combination_cuts = "(AMAXCHILD(MAXTREE('K+'==ABSID,PT)) > %(maxPT)s ) & ( (AMAXCHILD(MAXTREE('K+'==ABSID,MIPDV(PRIMARY))) > %(maxIP)s) | (AMAXCHILD(MAXTREE('K+'==ABSID,MIPCHI2DV(PRIMARY))) > %(maxIPChi2)s ) ) & (AMAXDOCA('') < %(doca)s ) & (AM > %(combMassLow)s * MeV) & (AM < %(combMassHigh)s * MeV)" %locals()    
-    _mother_cuts = "(PT > %(bPT)s * MeV) & (M > %(massLow)s * MeV) & (M < %(massHigh)s * MeV) & (BPVLTIME('PropertimeFitter/properTime:PUBLIC') > %(bTAU)s )" %locals()    
-    CombineHb2Charged2Body = CombineParticles( DecayDescriptor = 'B0 -> K+ K-',
-                                               DaughtersCuts = { "K+" : _daughters_cuts },
-                                               CombinationCut = _combination_cuts,
-                                               MotherCut = _mother_cuts )
-                                               
-    return Selection( name,
-                      Algorithm = CombineHb2Charged2Body,
-                      RequiredSelections = [ StdLooseKaons ] )
 ########################################################################  
 
--- Stripping/Phys/StrippingSelections/tests/TestCharmlessLines.py
+++ Stripping/Phys/StrippingSelections/tests/TestCharmlessLines.py
@@ -1,16 +1,11 @@
-# $Id: $
-# Test your line(s) of the stripping
-
-#
-# for Physics WG CP Gamma/Loop
-#
-# contact: V. Vagnoni, U. Kerzel
-#
-
 from Gaudi.Configuration import *
-from Configurables import SelDSTWriter, DaVinci
+from Configurables import DaVinci
 from StrippingConf.Configuration import StrippingConf
-from StrippingSettings.Utils import lineBuilderConfiguration
+
+# Tighten Trk Chi2 to <3
+from CommonParticles.Utils import DefaultTrackingCuts
+DefaultTrackingCuts().Cuts  = { "Chi2Cut" : [ 0, 3 ],
+                                "CloneDistCut" : [5000, 9e+99 ] }
 
 # Now build the stream
 from StrippingConf.StrippingStream import StrippingStream
@@ -23,15 +18,25 @@ stream = StrippingStream("Test")
 ##
 ## B->hh (lifetime biased)
 ##
-from StrippingSelections.StrippingB2PPbar import B2PPbarLines
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2PPbar
-confB2PPbar          = B2PPbarLines("Hb2Charged2Body", B2PPbar['CONFIG'])
+from StrippingSelections.StrippingB2TwoBaryons import B2TwoBaryonLines
+from StrippingSelections.StrippingB2TwoBaryons import default_config as B2PPbar
+confB2PPbar          = B2TwoBaryonLines("B2TwoBaryons", B2PPbar)
 stream.appendLines(confB2PPbar.lines())
 
+from StrippingSelections.StrippingBu2KSh import Bu2KShConf
+from StrippingSelections.StrippingBu2KSh import default_config as B2KSh
+confB2KSh = Bu2KShConf("B2KSh",B2KSh)
+stream.appendLines(confB2KSh.lines())
+
 from StrippingSelections.StrippingHb2Charged2Body import Hb2Charged2BodyLines
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import Hb2Charged2Body
-confBhh          = Hb2Charged2BodyLines("Hb2Charged2Body", Hb2Charged2Body['CONFIG'])
+from StrippingSelections.StrippingHb2Charged2Body import default_config as Hb2Charged2Body
+confBhh          = Hb2Charged2BodyLines("Hb2Charged2Body", Hb2Charged2Body)
 stream.appendLines(confBhh.lines())
+
+from StrippingSelections.StrippingB2HHBkg import B2HHBkgLines
+from StrippingSelections.StrippingB2HHBkg import default_config as B2HHBkg
+confBhhBkg          = B2HHBkgLines("B2HHBkg", B2HHBkg)
+stream.appendLines(confBhhBkg.lines())
 
 from StrippingSelections.StrippingB2hhLTUnbiased import StrippingB2hhLTUnbiasedConf
 from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2hhLTUnbiased, B2hhLTUnbiasedDetached
@@ -41,9 +46,14 @@ stream.appendLines(confBhhLTUnbiased.lines())
 stream.appendLines(confBhhLTUnbiasedDetached.lines())
 
 from StrippingSelections.StrippingBu2hhh import Bu2hhhBuilder
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2hhh
-confB2hhh = Bu2hhhBuilder("Bu2hhh",B2hhh['CONFIG'])
+from StrippingSelections.StrippingBu2hhh import config_params as B2hhh
+confB2hhh = Bu2hhhBuilder("Bu2hhh",B2hhh)
 stream.appendLines(confB2hhh.lines())
+
+from StrippingSelections.StrippingD02KPiPi0 import StrippingD02KPiPi0Conf
+from StrippingSettings.Stripping19a.LineConfigDictionaries_Calib import D02KPiPi0
+confD02KPiPi0 = StrippingD02KPiPi0Conf(D02KPiPi0['BUILDERTYPE'],D02KPiPi0['CONFIG'])
+stream.appendLines(confD02KPiPi0.lines())
 
 from StrippingSelections.StrippingB2HHPi0 import StrippingB2HHPi0Conf
 from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2HHPi0
@@ -51,88 +61,80 @@ confBHHPI0 = StrippingB2HHPi0Conf("B2HHPI0",B2HHPi0['CONFIG'])
 stream.appendLines(confBHHPI0.lines())
 
 from StrippingSelections.StrippingB2KShh import B2KShhConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2KShh
-confB2KShh = B2KShhConf("B2KShh",B2KShh['CONFIG'])
+from StrippingSelections.StrippingB2KShh import default_config as B2KShh
+confB2KShh = B2KShhConf("B2KShh",B2KShh)
 stream.appendLines(confB2KShh.lines())
 
 # Added section for new Lb2V0hhConf line - commented out for now
 from StrippingSelections.StrippingLb2V0hh import Lb2V0hhConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import Lb2V0hh
-confLb2V0hh = Lb2V0hhConf("Lb2V0hh",Lb2V0hh['CONFIG'])
+from StrippingSelections.StrippingLb2V0hh import default_config as Lb2V0hh
+confLb2V0hh = Lb2V0hhConf("Lb2V0hh",Lb2V0hh)
 stream.appendLines(confLb2V0hh.lines())
 
 from StrippingSelections.StrippingB2Kphiphi import B2KPhiPhiConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import B2KPhiPhi
-confB2Kphiphi = B2KPhiPhiConf("B2KPhiPhi",B2KPhiPhi['CONFIG'])
+from StrippingSelections.StrippingB2Kphiphi import config_params as B2KPhiPhi
+confB2Kphiphi = B2KPhiPhiConf("B2KPhiPhi",B2KPhiPhi)
 stream.appendLines(confB2Kphiphi.lines())
 
 from StrippingSelections.StrippingB2CharmlessQuasi2Body import B2Quasi2BodyConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSQ2B
-confB2CharmlessQuasi2Body = B2Quasi2BodyConf("B2Quasi2Body",BetaSQ2B['CONFIG'])
+from StrippingSelections.StrippingB2CharmlessQuasi2Body import config_params as BetaSQ2B
+confB2CharmlessQuasi2Body = B2Quasi2BodyConf("B2Quasi2Body",BetaSQ2B)
 stream.appendLines(confB2CharmlessQuasi2Body.lines())
 
 from StrippingSelections.StrippingBs2PhiKst0 import StrippingBs2PhiKstConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSBs2PhiKst
+from StrippingSettings.Stripping19a.LineConfigDictionaries_Charmless import BetaSBs2PhiKst
 confBs2PhiKst = StrippingBs2PhiKstConf("Bs2PhiKst",BetaSBs2PhiKst['CONFIG'])
 stream.appendLines(confBs2PhiKst.lines())
 
 from StrippingSelections.StrippingBs2Kst0Kst0 import StrippingBs2KstKstConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSBs2KstKst
-confBs2KstKst = StrippingBs2KstKstConf("Bs2KstKst",BetaSBs2KstKst['CONFIG'])
+from StrippingSelections.StrippingBs2Kst0Kst0 import __config_default__ as BetaSBs2KstKst
+confBs2KstKst = StrippingBs2KstKstConf("Bs2KstKst",BetaSBs2KstKst)
 stream.appendLines(confBs2KstKst.lines())
 
 from StrippingSelections.StrippingBs2Kst_0Kst_0 import StrippingBs2Kst_0Kst_0Conf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSBs2K0stK0st
-confBs2Kst_0Kst_0 = StrippingBs2Kst_0Kst_0Conf("Bs2Kst_0Kst_0",BetaSBs2K0stK0st["CONFIG"])
+from StrippingSelections.StrippingBs2Kst_0Kst_0 import __config_default__ as BetaSBs2K0stK0st
+confBs2Kst_0Kst_0 = StrippingBs2Kst_0Kst_0Conf("Bs2Kst_0Kst_0",BetaSBs2K0stK0st)
 stream.appendLines(confBs2Kst_0Kst_0.lines())
 
 from StrippingSelections.StrippingBs2Q2B import Bs2Q2Body4piConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSBs2Q2Body4pi
-confBs2Q2Body = Bs2Q2Body4piConf("Bs2Q2Body4pi",BetaSBs2Q2Body4pi['CONFIG'])
+from StrippingSelections.StrippingBs2Q2B import config_params as BetaSBs2Q2Body4pi
+confBs2Q2Body = Bs2Q2Body4piConf("Bs2Q2Body4pi",BetaSBs2Q2Body4pi)
 stream.appendLines(confBs2Q2Body.lines())
 
 from StrippingSelections.StrippingBs2KKhh import BsPhiRhoConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BsPhiRho
-confBsPhiRho = BsPhiRhoConf("BsPhiRho",BsPhiRho['CONFIG'])
+from StrippingSelections.StrippingBs2KKhh import config_params as BsPhiRho
+confBsPhiRho = BsPhiRhoConf("BsPhiRho",BsPhiRho)
 stream.appendLines(confBsPhiRho.lines())
 
 from StrippingSelections.StrippingB2FourKHigh import Bs2Q2Body4KConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import Bs2Q2Body4K
-confBs2Q2Body4K = Bs2Q2Body4KConf("Bs2Q2Body4K",Bs2Q2Body4K['CONFIG'])
+from StrippingSelections.StrippingB2FourKHigh import config_params as Bs2Q2Body4K
+confBs2Q2Body4K = Bs2Q2Body4KConf("Bs2Q2Body4K",Bs2Q2Body4K)
 stream.appendLines(confBs2Q2Body4K.lines())
 
 from StrippingSelections.StrippingBs2PhiPhi import StrippingBs2PhiPhiConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import BetaSBs2PhiPhi
-confBs2PhiPhi = StrippingBs2PhiPhiConf("Bs2PhiPhi",BetaSBs2PhiPhi['CONFIG'])
+from StrippingSelections.StrippingBs2PhiPhi import config_params as BetaSBs2PhiPhi
+confBs2PhiPhi = StrippingBs2PhiPhiConf("Bs2PhiPhi",BetaSBs2PhiPhi)
 stream.appendLines(confBs2PhiPhi.lines())
 
 from StrippingSelections.StrippingBs2KSKS import Bs2KSKSConf
-from StrippingSettings.Stripping18.LineConfigDictionaries_Charmless import Bs2KSKS
-confBs2KSKS = Bs2KSKSConf("Bs2KSKS",Bs2KSKS['CONFIG'])
+from StrippingSelections.StrippingBs2KSKS import default_config as Bs2KSKS
+confBs2KSKS = Bs2KSKSConf("Bs2KSKS",Bs2KSKS)
 stream.appendLines(confBs2KSKS.lines())
 
-#
-# remaining config
-#
+from StrippingSelections.StrippingB2XEta import B2XEtaConf
+from StrippingSelections.StrippingB2XEta import default_config as B2XEta
+confB2XEta = B2XEtaConf("B2XEta",B2XEta)
+stream.appendLines(confB2XEta.lines())
+
 from Configurables import  ProcStatusCheck
 filterBadEvents =  ProcStatusCheck()
 
-# Configure the stripping using the same options as in Reco06-Stripping10
 sc = StrippingConf( Streams = [ stream ],
-                    MaxCandidates = 50000,
+                    MaxCandidates = 2000,
                     AcceptBadEvents = False,
-                    BadEventSelection = filterBadEvents )
-#sc.OutputType = "ETC"                    # Can be either "ETC" or "DST"
-
-from Configurables import CondDB
-CondDB().IgnoreHeartBeat = True
-
-# Remove the microbias and beam gas etc events before doing the tagging step
-regexp = "HLT_PASS_RE('Hlt1(?!ODIN)(?!L0)(?!Lumi)(?!Tell1)(?!MB)(?!NZS)(?!Velo)(?!BeamGas)(?!Incident).*Decision')"
-from Configurables import LoKi__HDRFilter
-filterHLT = LoKi__HDRFilter("FilterHLT",Code = regexp )
-
-MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+                    BadEventSelection = filterBadEvents,
+                    TESPrefix = 'Strip'
+                    )
 
 from Configurables import AuditorSvc, ChronoAuditor
 AuditorSvc().Auditors.append( ChronoAuditor("Chrono") )
@@ -143,20 +145,26 @@ sr = StrippingReport(Selections = sc.selections())
 from Configurables import AlgorithmCorrelationsAlg
 ac = AlgorithmCorrelationsAlg(Algorithms = sc.selections())
 
-from Configurables import CondDB
-#CondDB(UseOracle = True)
-CondDB().IgnoreHeartBeat = True
-DaVinci().DDDBtag  = "head-20110722"
-DaVinci().CondDBtag = "head-20110722"
-
-DaVinci().PrintFreq = 2000
 DaVinci().HistogramFile = 'DV_stripping_histos.root'
 DaVinci().EvtMax = 100000
-DaVinci().EventPreFilters = [ filterHLT ]
+DaVinci().PrintFreq = 2000
 DaVinci().appendToMainSequence( [ sc.sequence() ] )
 DaVinci().appendToMainSequence( [ sr ] )
 DaVinci().appendToMainSequence( [ ac ] )
-DaVinci().DataType  = "2011"
-DaVinci().InputType = "SDST"
+DaVinci().DataType  = "2012"
+DaVinci().InputType = "DST"
 
-importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco12_Run97120_SDST.py")
+# change the column size of timing table
+from Configurables import TimingAuditor, SequencerTimerTool
+TimingAuditor().addTool(SequencerTimerTool,name="TIMER")
+TimingAuditor().TIMER.NameSize = 60
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+# database
+DaVinci().DDDBtag  = "head-20120413"
+DaVinci().CondDBtag = "cond-20120710"
+
+# input file
+importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco13c_Run124134.py")
+
