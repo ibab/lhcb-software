0a5b365cdf3d01edc9807df493d214f979299de9
cmthomas <cmthomas@cern.ch>
2012-08-29 19:42:43 +0000

    added ChiC2PP and low-mass resonance lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@144552 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ef47c8f006c75aaae5fb36c021b9b09c5a1fec4a

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2CEPLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Hlt2CEPLines.py
@@ -22,6 +22,8 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                                  , 'Hlt2LowMultChiC2HHHHWS' : 0.1
                                  , 'Hlt2LowMultDDIncCP'     : 1.0
                                  , 'Hlt2LowMultDDIncVF'     : 1.0
+                                 , 'Hlt2LowMulChiC2PP'      : 1.0
+                                 , 'Hlt2LowMultLMR2HH'      : 0.05
                                  }
                   , 'Postscale' : { 'Hlt2LowMultD2KPi'         : 1.0
                                     , 'Hlt2LowMultD2KPiPi'     : 1.0
@@ -35,12 +37,15 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                                     , 'Hlt2LowMultChiC2HHHHWS' : 1.0
                                     , 'Hlt2LowMultDDIncCP'     : 1.0
                                     , 'Hlt2LowMultDDIncVF'     : 1.0
+                                    , 'Hlt2LowMulChiC2PP'      : 1.0
+                                    , 'Hlt2LowMultLMR2HH'      : 1.0
                                     }
                   # Final-state particles
                   , 'H_PTmin'         : 100.0 * MeV
                   , 'H_Pmin'          : 5000.0 * MeV
                   , 'H_TrkChi2max'    : 3.0
                   , 'K_PIDKmin'       : 0.0
+                  , 'P_PIDpmin'       : 0.0
                   , 'DDInc_H_Pmin'    : 10000.0 * MeV
                   , 'DDInc_K_PIDKmin' : 5.0
                   # D0 -> KPi
@@ -48,30 +53,26 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                   , 'D2KPi_ADAMASSmax'    : 80.0 * MeV
                   , 'D2KPi_ADOCAmax'      : 0.5 * mm
                   , 'D2KPi_APmin'         : 10000.0 * MeV
-                  , 'D2KPi_SSumPTmin'     : 200.0 * MeV
                   , 'D2KPi_VtxChi2DoFmax' : 15.0
                   # D+- -> KPiPi
                   , 'D2KPiPi_APTmin'        : 0.0 * MeV
                   , 'D2KPiPi_ADAMASSmax'    : 80.0 * MeV
                   , 'D2KPiPi_ADOCAmax'      : 0.5 * mm
                   , 'D2KPiPi_APmin'         : 10000.0 * MeV
-                  , 'D2KPiPi_SSumPTmin'     : 300.0 * MeV
                   , 'D2KPiPi_VtxChi2DoFmax' : 15.0
                   # D0 -> K3Pi
                   , 'D2K3Pi_APTmin'        : 0.0 * MeV
                   , 'D2K3Pi_ADAMASSmax'    : 80.0 * MeV
                   , 'D2K3Pi_ADOCAmax'      : 0.7 * mm
                   , 'D2K3Pi_APmin'         : 10000.0 * MeV
-                  , 'D2K3Pi_SSumPTmin'     : 400.0 * MeV
                   , 'D2K3Pi_VtxChi2DoFmax' : 15.0
                   # ChiC -> HH
                   , 'ChiC2HH_APTmin'        : 0.0 * MeV
                   , 'ChiC2HH_APTmax'        : 5000.0 * MeV
-                  , 'ChiC2HH_AMmin'         : 3300.0 * MeV
+                  , 'ChiC2HH_AMmin'         : 2850.0 * MeV
                   , 'ChiC2HH_AMmax'         : 3600.0 * MeV
                   , 'ChiC2HH_ADOCAmax'      : 0.5 * mm
                   , 'ChiC2HH_APmin'         : 10000.0 * MeV
-                  , 'ChiC2HH_SSumPTmin'     : 200.0 * MeV
                   , 'ChiC2HH_VtxChi2DoFmax' : 15.0
                   # ChiC -> HHHH
                   , 'ChiC2HHHH_APTmin'        : 0.0 * MeV
@@ -80,8 +81,23 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                   , 'ChiC2HHHH_AMmax'         : 4500.0 * MeV
                   , 'ChiC2HHHH_ADOCAmax'      : 0.7 * mm
                   , 'ChiC2HHHH_APmin'         : 10000.0 * MeV
-                  , 'ChiC2HHHH_SSumPTmin'     : 400.0 * MeV
                   , 'ChiC2HHHH_VtxChi2DoFmax' : 15.0
+                  # ChiC -> PP
+                  , 'ChiC2PP_APTmin'        : 0.0 * MeV
+                  , 'ChiC2PP_APTmax'        : 5000.0 * MeV
+                  , 'ChiC2PP_AMmin'         : 2850.0 * MeV
+                  , 'ChiC2PP_AMmax'         : 3600.0 * MeV
+                  , 'ChiC2PP_ADOCAmax'      : 0.5 * mm
+                  , 'ChiC2PP_APmin'         : 10000.0 * MeV
+                  , 'ChiC2PP_VtxChi2DoFmax' : 15.0
+                  # Low-mass resonance -> HH
+                  , 'LMR2HH_APTmin'        : 0.0 * MeV
+                  , 'LMR2HH_APTmax'        : 5000.0 * MeV
+                  , 'LMR2HH_AMmin'         : 400.0 * MeV
+                  , 'LMR2HH_AMmax'         : 1600.0 * MeV
+                  , 'LMR2HH_ADOCAmax'      : 0.2 * mm
+                  , 'LMR2HH_APmin'         : 15000.0 * MeV
+                  , 'LMR2HH_VtxChi2DoFmax' : 4.0
                   # ANNSvc IDs
                   , 'HltANNSvcID' : { "D2KPi"         : 50401
                                       , "D2KPiPi"     : 50402
@@ -95,13 +111,15 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                                       , "ChiC2HHHHWS" : 50410
                                       , "DDIncCP"     : 50411
                                       , "DDIncVF"     : 50412
+                                      , "ChiC2PP"     : 50413
+                                      , "LMR2HH"      : 50414
                                       }
                   } 
 
     def __apply_configuration__(self) :
         from HltLine.HltLine import Hlt2Line, Hlt2Member, bindMembers
         from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedKaons, BiKalmanFittedPions, BiKalmanFittedRichKaons
-        from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedRichLowPTKaons 
+        from Hlt2SharedParticles.TrackFittedBasicParticles import BiKalmanFittedRichLowPTKaons, BiKalmanFittedRichLowPTProtons
         from HltTracking.Hlt2TrackingConfigurations import Hlt2BiKalmanFittedForwardTracking
         from HltLine.HltDecodeRaw import DecodeL0CALO
         from Configurables import HltANNSvc
@@ -110,6 +128,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
 
         Kaons = BiKalmanFittedRichLowPTKaons
         Pions = BiKalmanFittedPions
+        Protons = BiKalmanFittedRichLowPTProtons
 
         #
         #=== Final-state particles ===#
@@ -118,8 +137,9 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         K_cut        = "(PT > %(H_PTmin)s) & (P > %(H_Pmin)s) & (TRCHI2DOF < %(H_TrkChi2max)s) & (PIDK > %(K_PIDKmin)s)" % self.getProps()
         DDInc_K_cut  = "(PT > %(H_PTmin)s) & (P > %(DDInc_H_Pmin)s) & (TRCHI2DOF < %(H_TrkChi2max)s) & (PIDK > %(DDInc_K_PIDKmin)s)" % self.getProps()
         Pi_cut       = "(PT > %(H_PTmin)s) & (P > %(H_Pmin)s) & (TRCHI2DOF < %(H_TrkChi2max)s)" % self.getProps()
+        P_cut        = "(PT > %(H_PTmin)s) & (P > %(H_Pmin)s) & (TRCHI2DOF < %(H_TrkChi2max)s) & (PIDp > %(P_PIDpmin)s)" % self.getProps()
 
-        Daug_cuts = {"K+" : K_cut, "K-" : K_cut, "pi+" : Pi_cut, "pi-" : Pi_cut}
+        Daug_cuts = {"K+" : K_cut, "K-" : K_cut, "pi+" : Pi_cut, "pi-" : Pi_cut, "p+" : P_cut, "p~-" : P_cut}
         DDInc_Daug_cuts = {"K+" : DDInc_K_cut, "K-" : DDInc_K_cut}
         
         #
@@ -127,7 +147,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #
 
         D2KPi_Comb_cut   = "(APT > %(D2KPi_APTmin)s) & (ADAMASS('D0') < %(D2KPi_ADAMASSmax)s) & (ADOCAMAX('LoKi::DistanceCalculator') < %(D2KPi_ADOCAmax)s) & " \
-                           "(AP > %(D2KPi_APmin)s) & (ACHILD(PT, 1) + ACHILD(PT, 2) > %(D2KPi_SSumPTmin)s)" % self.getProps()
+                           "(AP > %(D2KPi_APmin)s)" % self.getProps()
         D2KPi_Mother_cut = "(VFASPF(VCHI2PDOF) < %(D2KPi_VtxChi2DoFmax)s)" % self.getProps()
         
         CombineD2KPi = Hlt2Member(CombineParticles
@@ -157,7 +177,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #
 
         D2KPiPi_Comb_cut   = "(APT > %(D2KPiPi_APTmin)s) & (ADAMASS('D+') < %(D2KPiPi_ADAMASSmax)s) & (ADOCAMAX('LoKi::DistanceCalculator') < %(D2KPiPi_ADOCAmax)s) & " \
-                             "(AP > %(D2KPiPi_APmin)s) & (ACHILD(PT, 1) + ACHILD(PT, 2) + ACHILD(PT, 3) > %(D2KPiPi_SSumPTmin)s)" % self.getProps()
+                             "(AP > %(D2KPiPi_APmin)s)" % self.getProps()
         D2KPiPi_Mother_cut = "(VFASPF(VCHI2PDOF) < %(D2KPiPi_VtxChi2DoFmax)s)" % self.getProps()
         
         CombineD2KPiPi = Hlt2Member(CombineParticles
@@ -188,7 +208,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #
 
         D2K3Pi_Comb_cut   = "(APT > %(D2K3Pi_APTmin)s) & (ADAMASS('D0') < %(D2K3Pi_ADAMASSmax)s) & (ADOCAMAX('LoKi::DistanceCalculator') < %(D2K3Pi_ADOCAmax)s) & " \
-                            "(AP > %(D2K3Pi_APmin)s) & (ACHILD(PT, 1) + ACHILD(PT, 2) + ACHILD(PT, 3) + ACHILD(PT, 4) > %(D2K3Pi_SSumPTmin)s)" % self.getProps()
+                            "(AP > %(D2K3Pi_APmin)s)" % self.getProps()
         D2K3Pi_Mother_cut = "(VFASPF(VCHI2PDOF) < %(D2K3Pi_VtxChi2DoFmax)s)" % self.getProps()
         
         CombineD2K3Pi = Hlt2Member(CombineParticles
@@ -220,8 +240,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #
 
         ChiC2HH_Comb_cut   = "(APT > %(ChiC2HH_APTmin)s) & (APT < %(ChiC2HH_APTmax)s) & (AM > %(ChiC2HH_AMmin)s) & (AM < %(ChiC2HH_AMmax)s) & " \
-                             "(ADOCAMAX('LoKi::DistanceCalculator') < %(ChiC2HH_ADOCAmax)s) & " \
-                             "(AP > %(ChiC2HH_APmin)s) & (ACHILD(PT, 1) + ACHILD(PT, 2) > %(ChiC2HH_SSumPTmin)s)" % self.getProps()
+                             "(ADOCAMAX('LoKi::DistanceCalculator') < %(ChiC2HH_ADOCAmax)s) & (AP > %(ChiC2HH_APmin)s)" % self.getProps()
         ChiC2HH_Mother_cut = "(VFASPF(VCHI2PDOF) < %(ChiC2HH_VtxChi2DoFmax)s)" % self.getProps()
         
         CombineChiC2HH = Hlt2Member(CombineParticles
@@ -253,8 +272,7 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #
 
         ChiC2HHHH_Comb_cut   = "(APT > %(ChiC2HHHH_APTmin)s) & (APT < %(ChiC2HHHH_APTmax)s) & (AM > %(ChiC2HHHH_AMmin)s) & (AM < %(ChiC2HHHH_AMmax)s) & " \
-                               "(ADOCAMAX('LoKi::DistanceCalculator') < %(ChiC2HHHH_ADOCAmax)s) & " \
-                               "(AP > %(ChiC2HHHH_APmin)s) & (ACHILD(PT, 1) + ACHILD(PT, 2) + ACHILD(PT, 3) + ACHILD(PT, 4) > %(ChiC2HHHH_SSumPTmin)s)" % self.getProps()
+                               "(ADOCAMAX('LoKi::DistanceCalculator') < %(ChiC2HHHH_ADOCAmax)s) & (AP > %(ChiC2HHHH_APmin)s)" % self.getProps()
         ChiC2HHHH_Mother_cut = "(VFASPF(VCHI2PDOF) < %(ChiC2HHHH_VtxChi2DoFmax)s)" % self.getProps()
         
         CombineChiC2HHHH = Hlt2Member(CombineParticles
@@ -287,6 +305,25 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                                         )
         
         ChiC2HHHHWS = bindMembers("ChiC2HHHHWS", [ Kaons ] + [ Pions ] + [ CombineChiC2HHHHWS ])
+
+        #
+        #=== 'chi_c' -> PPbar ===#
+        #
+
+        ChiC2PP_Comb_cut   = "(APT > %(ChiC2PP_APTmin)s) & (APT < %(ChiC2PP_APTmax)s) & (AM > %(ChiC2PP_AMmin)s) & (AM < %(ChiC2PP_AMmax)s) & " \
+                             "(ADOCAMAX('LoKi::DistanceCalculator') < %(ChiC2PP_ADOCAmax)s) & (AP > %(ChiC2PP_APmin)s)" % self.getProps()
+        ChiC2PP_Mother_cut = "(VFASPF(VCHI2PDOF) < %(ChiC2PP_VtxChi2DoFmax)s)" % self.getProps()
+        
+        CombineChiC2PP = Hlt2Member(CombineParticles
+                                    , "Combine"
+                                    , DecayDescriptors = [ "chi_c1(1P) -> p+ p~-" ]
+                                    , DaughtersCuts = Daug_cuts
+                                    , CombinationCut = ChiC2PP_Comb_cut
+                                    , MotherCut = ChiC2PP_Mother_cut
+                                    , Inputs = [ Protons ]
+                                    )
+        
+        ChiC2PP = bindMembers("ChiC2PP", [ Protons ] + [ CombineChiC2PP ])
 
         #
         #=== Inclusive DD using CombineParticles ===#
@@ -339,6 +376,27 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         #vfcode_Qm = "CONTAINS('" + bindDDInc_Qm.outputSelection() + "') > 0"
         #KCount_Qm = VoidFilter("Hlt2CEPKCount_Qm", Code = vfcode_Qm)
         #DDIncVF = bindMembers("DDIncVF", [ Kaons ] + [ bindDDInc_Qp ] + [ KCount_Qp ] + [ bindDDInc_Qm ] + [ KCount_Qm ])
+
+        #
+        #=== Low-mass resonance ===#
+        #
+
+        LMR2HH_Comb_cut   = "(APT > %(LMR2HH_APTmin)s) & (APT < %(LMR2HH_APTmax)s) & (AM > %(LMR2HH_AMmin)s) & (AM < %(LMR2HH_AMmax)s) & " \
+                            "(ADOCAMAX('LoKi::DistanceCalculator') < %(LMR2HH_ADOCAmax)s) & (AP > %(LMR2HH_APmin)s)" % self.getProps()
+        LMR2HH_Mother_cut = "(VFASPF(VCHI2PDOF) < %(LMR2HH_VtxChi2DoFmax)s)" % self.getProps()
+        
+        CombineLMR2HH = Hlt2Member(CombineParticles
+                                   , "Combine"
+                                   , DecayDescriptors = [ "phi(1020) -> K+ K-",
+                                                          "[phi(1020) -> K+ pi-]cc",
+                                                          "phi(1020) -> pi+ pi-" ]
+                                   , DaughtersCuts = Daug_cuts
+                                   , CombinationCut = LMR2HH_Comb_cut
+                                   , MotherCut = LMR2HH_Mother_cut
+                                   , Inputs = [ Kaons, Pions ]
+                                   )
+        
+        LMR2HH = bindMembers("LMR2HH", [ Kaons ] + [ Pions ] + [ CombineLMR2HH ])
         
         #
         #=== Filtering algorithms ===#
@@ -400,6 +458,14 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                          , postscale = self.postscale
                          )
 
+        line = Hlt2Line( 'LowMultChiC2PP'
+                         , prescale = self.prescale
+                         , HLT = HLTreq    
+                         , L0DU = L0req
+                         , algos = [ velotracks, FilterNumVeloTracksChiC2HH, FilterNumBackTracks, Protons, ChiC2PP ]
+                         , postscale = self.postscale
+                         )
+
         line = Hlt2Line( 'LowMultD2KPiWS'
                          , prescale = self.prescale
                          , HLT = HLTreq
@@ -456,6 +522,14 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
                          , postscale = self.postscale
                          )
 
+        line = Hlt2Line( 'LowMultLMR2HH'
+                         , prescale = self.prescale
+                         , HLT = HLTreq    
+                         , L0DU = L0req
+                         , algos = [ velotracks, FilterNumVeloTracksChiC2HH, FilterNumBackTracks, Kaons, Pions, LMR2HH ]
+                         , postscale = self.postscale
+                         )
+
         #
         #=== Register with ANNSvc ===#
         #
@@ -472,4 +546,5 @@ class Hlt2CEPLinesConf(HltLinesConfigurableUser) :
         HltANNSvc().Hlt2SelectionID.update( { "Hlt2LowMultChiC2HHHHWSDecision" : self.getProp('HltANNSvcID')['ChiC2HHHHWS'] } )
         HltANNSvc().Hlt2SelectionID.update( { "Hlt2LowMultDDIncCPDecision"     : self.getProp('HltANNSvcID')['DDIncCP'] } )
         HltANNSvc().Hlt2SelectionID.update( { "Hlt2LowMultDDIncVFDecision"     : self.getProp('HltANNSvcID')['DDIncVF'] } )
-
+        HltANNSvc().Hlt2SelectionID.update( { "Hlt2LowMultChiC2PPDecision"     : self.getProp('HltANNSvcID')['ChiC2PP'] } )
+        HltANNSvc().Hlt2SelectionID.update( { "Hlt2LowMultLMR2HHDecision"      : self.getProp('HltANNSvcID')['LMR2HH'] } )
