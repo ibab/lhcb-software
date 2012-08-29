fd33790a7534ce8ec47d2b29de1ff721a8251f21
Anton Poluektov <anton.poluektov@cern.ch>
2012-08-29 15:42:45 +0000

    Replace VertexFitters in B2C_Utils
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@144540 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0cf28f8693d217a08fff7b2bb5f777133ee6a607

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/Beauty2Charm_Utils.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/Beauty2Charm_Utils.py
@@ -113,7 +113,7 @@ def makeB2X(name,decay,inputs,config,useIP=True,resVert=True):
     if not resVert:
         b2x = b2x.configurable(name+'Beauty2CharmVertexFitter')
         b2x.addTool(OfflineVertexFitter)
-        b2x.VertexFitters.update( { "" : "OfflineVertexFitter"} )
+        b2x.ParticleCombiners.update( { "" : "OfflineVertexFitter"} )
         b2x.OfflineVertexFitter.useResonanceVertex = False
     return Selection(name,Algorithm=b2x,RequiredSelections=inputs)
 
