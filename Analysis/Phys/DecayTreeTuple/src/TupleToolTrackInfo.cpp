9c72cdcbcdc8e6b6982243a6d045558ae5b690c5
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-09-03 09:14:28 +0000

    clean up
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@144777 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33dbffe07947bda42854f9b5fb32d7d1380c0656

--- Analysis/Phys/DecayTreeTuple/src/TupleToolTrackInfo.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolTrackInfo.cpp
@@ -118,15 +118,11 @@ StatusCode TupleToolTrackInfo::fill( const LHCb::Particle*
   }
 
   //}
-  test &= tuple->column( prefix+"_TRACK_MatchCHI2", track->info(LHCb::Track::FitMatchChi2,-1) );
-  double ghostProbability = -1.0;
-  //  if (track->ghostProbability() != 0)
-  ghostProbability = track->ghostProbability();
 
-  test &= tuple->column( prefix+"_TRACK_GhostProb",ghostProbability);
+  test &= tuple->column( prefix+"_TRACK_MatchCHI2", track->info(LHCb::Track::FitMatchChi2,-1) );
+  test &= tuple->column( prefix+"_TRACK_GhostProb", track->ghostProbability() );
   test &= tuple->column( prefix+"_TRACK_CloneDist", track->info(LHCb::Track::CloneDist, -1.) );
   test &= tuple->column( prefix+"_TRACK_Likelihood", track->likelihood() );
-
 
   return StatusCode(test);
 }
