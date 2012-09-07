9e149105b22dfb0e56397537d79a40205a5b9fbf
Olivier Deschamps <odescham@cern.ch>
2012-09-07 10:49:23 +0000

    update B2HHPi0 & D02KPiPi0
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@144966 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d04a8a81fe48be9656ed3a943309d86b04f84ce3

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2HHPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '2012.05.02'
-__version__ = '$Revision: 1.5 $'
+__date__ = '2012.09.05'
+__version__ = '$Revision: 1.6 $'
 
 '''
 Stripping selection for B -> h h pi0
@@ -24,11 +24,10 @@ class StrippingB2HHPi0Conf(LineBuilder) :
     __configuration_keys__ = ( 'PiMinPT'
                                ,'PiMinP'
                                ,'PiMinTrackProb'
+                               ,'PiMaxGhostProb'
 			       ,'PiMinIPChi2'
 			       ,'Pi0MinPT_M'
 			       ,'Pi0MinPT_R'
-			       ,'ResPi0MinMM'
-			       ,'ResPi0MaxMM'
 			       ,'ResPi0MinGamCL'
 			       ,'BMinM'
 			       ,'BMaxM'
@@ -88,8 +87,8 @@ def makeB2HHPi0R( name,
                   inputSel
                   ) :
 
-    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
-    _pi0Cuts = "(PT>%(Pi0MinPT_R)s *MeV) & (MM>%(ResPi0MinMM)s *MeV) & (MM<%(ResPi0MaxMM)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % locals()['config']
+    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (TRGHOSTPROB<%(PiMaxGhostProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
+    _pi0Cuts = "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" % locals()['config']
     _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts, 'pi0' : _pi0Cuts }
     _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
     _motherCuts = "(PT>%(BMinPT_R)s *MeV) & (VFASPF(VPCHI2)>%(BMinVtxProb)s) & (BPVVDCHI2>%(BMinVVDChi2)s) & (BPVIPCHI2()<%(BMaxIPChi2)s) & (BPVDIRA>%(BMinDIRA)s)" % locals()['config']
@@ -111,7 +110,7 @@ def makeB2HHPi0M( name,
                   inputSel
                   ) :
 
-    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
+    _piCuts = "(PT>%(PiMinPT)s *MeV) & (P>%(PiMinP)s *MeV) & (TRPCHI2>%(PiMinTrackProb)s) & (TRGHOSTPROB<%(PiMaxGhostProb)s) & (MIPCHI2DV(PRIMARY)>%(PiMinIPChi2)s)" % locals()['config']
     _pi0Cuts = "(PT>%(Pi0MinPT_M)s *MeV)" % locals()['config']
     _daughterCuts = { 'pi+' : _piCuts, 'pi-' : _piCuts, 'pi0' : _pi0Cuts }
     _combCuts = "(AM>%(BMinM)s *MeV) & (AM<%(BMaxM)s *MeV)" % locals()['config']
--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingD02KPiPi0.py
@@ -1,7 +1,7 @@
 
 __author__ = 'Regis Lefevre'
-__date__ = '2012.05.02'
-__version__ = '$Revision: 1.4 $'
+__date__ = '2012.09.05'
+__version__ = '$Revision: 1.5 $'
 
 '''
 Stripping selection for D0 -> K pi pi0 
@@ -20,9 +20,10 @@ from StandardParticles import StdTightPions,StdTightKaons,StdLooseMergedPi0,StdL
 
 class StrippingD02KPiPi0Conf(LineBuilder) :
 
-    __configuration_keys__ = ( 'TrackMinPT'
+    __configuration_keys__ = (  'TrackMinPT_M'
                                ,'TrackMinPT_R'
                                ,'TrackMinTrackProb'
+                               ,'TrackMaxGhostProb'
                                ,'TrackMinIPChi2'
                                ,'Pi0MinPT_M'
                                ,'Pi0MinPT_R'
@@ -84,7 +85,7 @@ def makeD02KPiPi0R( name,
                   inputSel
                   ) :
 
-    _TrackCuts    = "(PT>%(TrackMinPT_R)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
+    _TrackCuts    = "(PT>%(TrackMinPT_R)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (TRGHOSTPROB<%(TrackMaxGhostProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
     _pi0Cuts      = "(PT>%(Pi0MinPT_R)s *MeV) & (CHILD(CL,1)>%(ResPi0MinGamCL)s) & (CHILD(CL,2)>%(ResPi0MinGamCL)s)" %locals()['config']
     _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts, 'pi0' : _pi0Cuts }
     _combCuts     = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
@@ -107,7 +108,7 @@ def makeD02KPiPi0M( name,
                   inputSel
                   ) :
 
-    _TrackCuts    = "(PT>%(TrackMinPT)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
+    _TrackCuts    = "(PT>%(TrackMinPT_M)s *MeV) & (TRPCHI2>%(TrackMinTrackProb)s) & (TRGHOSTPROB<%(TrackMaxGhostProb)s) & (MIPCHI2DV(PRIMARY)>%(TrackMinIPChi2)s)" %locals()['config']    
     _pi0Cuts      = "(PT>%(Pi0MinPT_M)s *MeV)" %locals()['config']
     _daughterCuts = { 'K-' : _TrackCuts, 'pi+' : _TrackCuts, 'pi0' : _pi0Cuts }
     _combCuts     = "(AM>%(D0MinM)s *MeV) & (AM<%(D0MaxM)s *MeV)" % locals()['config']
