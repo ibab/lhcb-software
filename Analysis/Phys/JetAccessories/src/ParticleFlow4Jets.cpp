431b4a9283a96a486f260070b5f653e0988b1f83
Murilo Santana Rangel <murilo.rangel@cern.ch>
2012-09-11 13:48:23 +0000

    fix the fix about velo tracks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@145119 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d94907b008cc86be3e04e4491dc31ee9dae179e5

--- Analysis/Phys/JetAccessories/src/ParticleFlow4Jets.cpp
+++ Analysis/Phys/JetAccessories/src/ParticleFlow4Jets.cpp
@@ -1045,10 +1045,8 @@ StatusCode ParticleFlow4Jets::loadDatas() {
   }
   
   if (m_useVelo){
-    if( exist<LHCb::ProtoParticles>("Rec/ProtoP/VeloProtoPMaker") ){
-      return Warning("No charged protopacticles at Rec/ProtoP/VeloProtoPMaker");
-    }
-    m_protoParticles["Velo"] = get<LHCb::ProtoParticles*>("Rec/ProtoP/VeloProtoPMaker");
+    if( exist<LHCb::ProtoParticles>("Rec/ProtoP/VeloProtoPMaker") )
+       m_protoParticles["Velo"] = get<LHCb::ProtoParticles*>("Rec/ProtoP/VeloProtoPMaker");
   }
   // Get ECAL Clusters container
   if(exist<LHCb::CaloClusters>( "Rec/Calo/EcalClusters" ))
