8be2503f3de5229f7da8c63e165602500d5fa003
Markus Frank <frankb@cern.ch>
2012-09-12 13:00:05 +0000

    New version for release v4r67
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@145203 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6ec0fc969a62b3ef2f4bc8eec1b29e4b44fb4169

--- Online/Online/OnlineTasks/job/MEPInit.sh
+++ Online/Online/OnlineTasks/job/MEPInit.sh
@@ -12,9 +12,6 @@ rm -f /dev/shm/bm_*_RESULT
 if test -n "`grep Activity /group/online/dataflow/options/${PARTNAME}/HLT/OnlineEnvBase.py|grep EOF_CALIB`";
 then
     exec -a ${UTGID} ${CLASS0_TASK} -main=${HLTOPTS}/MEPInit_EoFCalib.opts
-    exec -a ${UTGID} ${gaudi_exe} -tasktype=LHCb::Class2Task -main=/group/online/dataflow/templates/options/Main.opts -opt=${HLTOPTS}/MEPRxSvc_EoFCalib.opts
 else
     exec -a ${UTGID} ${CLASS0_TASK} -main=${HLTOPTS}/MEPInit${IS_TAE_RUN}.opts
 fi;
-
-exec -a ${UTGID} ${CLASS0_TASK} -main=${HLTOPTS}/MEPInit${IS_TAE_RUN}.opts
