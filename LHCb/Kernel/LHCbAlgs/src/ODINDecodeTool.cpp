6ca030617057a33a5188b3dea3a80f7fc835e337
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-09-17 10:46:15 +0000

    Use getIfExists in ODINDecodeTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@145489 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c569df6c7a52cf42820f6669f1bb889e4841aa26

--- LHCb/Kernel/LHCbAlgs/src/ODINDecodeTool.cpp
+++ LHCb/Kernel/LHCbAlgs/src/ODINDecodeTool.cpp
@@ -57,21 +57,21 @@ DECLARE_TOOL_FACTORY( ODINDecodeTool )
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-ODINDecodeTool::ODINDecodeTool( const std::string& type,
-                                const std::string& name,
-                                const IInterface* parent )
-  : ODINCodecBaseTool(type, name, parent) {
-  declareProperty("ODINLocation", m_odinLocation = "",
-                  "Location of the ODIN object in the transient store. By "
-                  "default is the content of LHCb::ODINLocation::Default.");
-  declareProperty("RawEventLocation", m_rawEventLocation = "",
-                  "Location of the RawEvent object in the transient store. By "
-                  "default is the content of LHCb::RawEventLocation::Default.");
-  declareProperty("RawEventLocations", m_rawEventLocations,
-                  "List of possible locations of the RawEvent object in the"
-                  " transient store. By default it is LHCb::RawEventLocation::Copied,"
-                  " LHCb::RawEventLocation::Default.");
-}
+  ODINDecodeTool::ODINDecodeTool( const std::string& type,
+                                  const std::string& name,
+                                  const IInterface* parent )
+    : ODINCodecBaseTool(type, name, parent) {
+    declareProperty("ODINLocation", m_odinLocation = "",
+                    "Location of the ODIN object in the transient store. By "
+                    "default is the content of LHCb::ODINLocation::Default.");
+    declareProperty("RawEventLocation", m_rawEventLocation = "",
+                    "Location of the RawEvent object in the transient store. By "
+                    "default is the content of LHCb::RawEventLocation::Default.");
+    declareProperty("RawEventLocations", m_rawEventLocations,
+                    "List of possible locations of the RawEvent object in the"
+                    " transient store. By default it is LHCb::RawEventLocation::Copied,"
+                    " LHCb::RawEventLocation::Default.");
+  }
 //=============================================================================
 // Destructor
 //=============================================================================
@@ -80,66 +80,81 @@ ODINDecodeTool::~ODINDecodeTool() {
 //=============================================================================
 // Initialize
 //=============================================================================
-StatusCode ODINDecodeTool::initialize() {
+StatusCode ODINDecodeTool::initialize()
+{
   StatusCode sc = ODINCodecBaseTool::initialize(); // always first
   if (sc.isFailure()) return sc; // error message already printed
 
-  if (m_odinLocation.empty()) {
+  if (m_odinLocation.empty()) 
+  {
     // use the default
     m_odinLocation = LHCb::ODINLocation::Default;
-  } else {
+  } 
+  else 
+  {
     info() << "Using '" << m_odinLocation << "' as location of the ODIN object" << endmsg;
   }
-
+  
   bool usingDefaultLocation = m_rawEventLocations.empty() && m_rawEventLocation.empty();
-  if (! m_rawEventLocation.empty()) {
+  if ( !m_rawEventLocation.empty() ) 
+  {
     warning() << "The RawEventLocation property is obsolete, use RawEventLocations instead" << endmsg;
     m_rawEventLocations.insert(m_rawEventLocations.begin(), m_rawEventLocation);
   }
 
-  if (std::find(m_rawEventLocations.begin(), m_rawEventLocations.end(), LHCb::RawEventLocation::Default)
-      == m_rawEventLocations.end()) {
+  if ( std::find( m_rawEventLocations.begin(), 
+                  m_rawEventLocations.end(), 
+                  LHCb::RawEventLocation::Default)
+       == m_rawEventLocations.end() ) 
+  {
     // append the defaults to the search path
     m_rawEventLocations.push_back(LHCb::RawEventLocation::Trigger);
     m_rawEventLocations.push_back(LHCb::RawEventLocation::Default);
   }
 
-  if (!usingDefaultLocation) {
+  if (!usingDefaultLocation) 
+  {
     info() << "Using '" << m_rawEventLocations << "' as search path for the RawEvent object" << endmsg;
   }
+
   return sc;
 }
 //=============================================================================
 // Main function
 //=============================================================================
-void ODINDecodeTool::execute() {
-
-  LHCb::ODIN* odin = 0;
+void ODINDecodeTool::execute() 
+{
+  LHCb::ODIN* odin = NULL;
 
   // Check if there is already an ODIN object
-  if (exist<LHCb::ODIN>(m_odinLocation)) {
-    if (m_force) {
+  if (exist<LHCb::ODIN>(m_odinLocation)) 
+  {
+    if (m_force) 
+    {
       // Use the already registered object
       if( UNLIKELY( msgLevel(MSG::DEBUG) ) )
         debug() << "Modify existing ODIN object" << endmsg;
       odin = get<LHCb::ODIN>(m_odinLocation);
-    } else {
+    } 
+    else 
+    {
       // ODIN already there and we are not supposed to touch it
       if( UNLIKELY( msgLevel(MSG::DEBUG) ) )
         debug() << "Keep existing ODIN object" << endmsg;
       return;
     }
   }
-
+  
   if( UNLIKELY( msgLevel(MSG::DEBUG) ) ) debug() << "Getting RawEvent" << endmsg;
   LHCb::RawEvent* rawEvent = NULL;
-  for (std::vector<std::string>::const_iterator p = m_rawEventLocations.begin(); p != m_rawEventLocations.end(); ++p) {
-    if (exist<LHCb::RawEvent>(*p)){
-      rawEvent = get<LHCb::RawEvent>(*p);
-      break;
-    }
+  for (std::vector<std::string>::const_iterator p = m_rawEventLocations.begin();
+       p != m_rawEventLocations.end(); ++p )
+  {
+    rawEvent = getIfExists<LHCb::RawEvent>( evtSvc(), *p );
+    if ( rawEvent ) { break; }
   }
-  if (!rawEvent) {
+  if (!rawEvent) 
+  {
     using namespace GaudiUtils;
     // Throw a meaningful exception it the bank is not found;
     std::ostringstream out;
@@ -149,13 +164,15 @@ void ODINDecodeTool::execute() {
 
   // Check if have an ODIN bank...
   const std::vector<LHCb::RawBank*>& odinBanks = rawEvent->banks(LHCb::RawBank::ODIN);
-  if(odinBanks.size()){ // ... good, we can decode it
+  if ( !odinBanks.empty() ) 
+  { // ... good, we can decode it
     odin = this->i_decode(*odinBanks.begin(), odin);
     if (odin && (!odin->registry())){ // register ODIN object if valid and not yet registered
       put(odin, m_odinLocation);
     }
   }
-  else {
+  else
+  {
     Warning("Cannot find ODIN bank in RawEvent");
   }
 }
