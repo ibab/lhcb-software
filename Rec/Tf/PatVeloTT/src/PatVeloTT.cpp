97b69d18e8c97170ccf012620aa9a854a4ffcc65
szczypka <szczypka@cern.ch>
2012-09-21 07:55:30 +0000

    Rename of VeloPix to VP
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@145703 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9ce844042dc68a80bf7e27ae6aea6103646842a1

--- Rec/Tf/PatVeloTT/src/PatVeloTT.cpp
+++ Rec/Tf/PatVeloTT/src/PatVeloTT.cpp
@@ -271,17 +271,17 @@ bool PatVeloTT::matchingTracks( LHCb::Track* vttcand, LHCb::Track* trackused)
   int nvelo1 = 0;
   int nvelo2 = 0;
   for ( unsigned int i1 = 0; i1 < ids1.size(); ++i1 ) {
-    if(ids1[i1].checkDetectorType(LHCb::LHCbID::Velo) || ids1[i1].checkDetectorType(LHCb::LHCbID::VeloPix) ) nvelo1++;
+    if(ids1[i1].checkDetectorType(LHCb::LHCbID::Velo) || ids1[i1].checkDetectorType(LHCb::LHCbID::VP) ) nvelo1++;
   }
   for ( unsigned int i2 = 0; i2 < ids2.size(); ++i2 ) {
-    if(ids2[i2].checkDetectorType(LHCb::LHCbID::Velo)|| ids2[i2].checkDetectorType(LHCb::LHCbID::VeloPix) ) nvelo2++;
+    if(ids2[i2].checkDetectorType(LHCb::LHCbID::Velo)|| ids2[i2].checkDetectorType(LHCb::LHCbID::VP) ) nvelo2++;
   }
 
   // Calculate the number of common LHCbIDs
   for ( unsigned int i1 = 0; i1 < ids1.size(); ++i1 ) {
-    if(!ids1[i1].checkDetectorType(LHCb::LHCbID::Velo) && !ids1[i1].checkDetectorType(LHCb::LHCbID::VeloPix)) continue;
+    if(!ids1[i1].checkDetectorType(LHCb::LHCbID::Velo) && !ids1[i1].checkDetectorType(LHCb::LHCbID::VP)) continue;
     for ( unsigned int i2 = 0; i2 < ids2.size(); ++i2 ) {
-      if(!ids2[i2].checkDetectorType(LHCb::LHCbID::Velo) && !ids2[i2].checkDetectorType(LHCb::LHCbID::VeloPix)) continue;
+      if(!ids2[i2].checkDetectorType(LHCb::LHCbID::Velo) && !ids2[i2].checkDetectorType(LHCb::LHCbID::VP)) continue;
 
       if ( ids1[i1].channelID() == ids2[i2].channelID()  ) {
         ++nCommon;
