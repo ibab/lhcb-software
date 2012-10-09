b2c5343855ce03b660eac3feaa1d0dcbad35a209
Alex Grecu <alex.grecu@cern.ch>
2012-10-09 13:05:07 +0000

    Added QMTests for the package
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@146672 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9ce70dbd1471611297f3e8534fa4b6b703993e68

---
+++ Gauss/Gen/GenTune/tests/options/MC_Generics.py
@@ -0,0 +1,41 @@
+#Configuration file for MC generic RIVET analyses run on LHCb MB events
+from Gaudi.Configuration import *
+from LHCbKernel.Configuration import *
+from Gauss.Configuration import *
+from Configurables import Generation
+from Configurables import RivetAnalysisHandler
+#Needed to add local directory path to RivetAnalysisHandler options
+import os
+importOptions("$GAUSSOPTS/Gauss-DEV.py")
+importOptions("$DECFILESROOT/options/30000000.py")
+importOptions("$LBPYTHIAROOT/options/Pythia.py")
+importOptions("$GAUSSOPTS/GenStandAlone.py")
+#importOptions("$DECFILESROOT/options/SwitchOffEvtGen.py")
+importOptions("$GAUSSOPTS/Gauss-Job.py")
+
+#GaussGen = GenInit("GaussGen")
+#GaussGen.RunNumber = 1082
+LHCbApp().EvtMax = 1000
+LHCbApp().OutputLevel = WARNING
+Gauss().Histograms = "NONE"
+Gauss().Output = "NONE"
+Gauss().DatasetName = "GaussTestMC"
+#force head on collisions:
+#Gauss().BeamCrossingAngle = 0.0
+#Gauss().BeamBetaStar = 0.0
+
+GenMonitor = GaudiSequencer( "GenMonitor" )
+GenMonitor.Members += [ "RivetAnalysisHandler" ]
+rivet = RivetAnalysisHandler()
+rivet.BaseFileName = "LHCb_MC_hist"
+rivet.RunName = "LHCbMBPy6"
+rivet.Analyses = ["MC_GENERIC","MC_IDENTIFIED"]
+rivet.StreamName = "/Rivet"
+#to search Rivet plugins in current directory
+#rivet.AnalysisPath += [os.path.abspath('.'),]
+rivet.xSectionNeeded = False
+rivet.xSectionValue = 32.123
+rivet.CorrectStatusID = True
+rivet.CorrectCrossingAngles = True
+
+HistogramPersistencySvc().OutputFile = ''
