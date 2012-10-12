f4309527f5d5d3dbb67bf7d31311abea68cfff21
Marco Clemencic <marcocle@cern.ch>
2012-10-12 08:30:00 +0000

    Fixed warning: extra ';' [-pedantic].
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@146825 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3b0d2d97bce73a0f958e1d7e0c30551305acc9f9

--- LHCb/Event/GenEvent/GenEvent/HepMCUtils.h
+++ LHCb/Event/GenEvent/GenEvent/HepMCUtils.h
@@ -1,5 +1,5 @@
 // $Id: HepMCUtils.h,v 1.1 2008-07-23 17:14:24 cattanem Exp $
-#ifndef GENEVENT_HEPMCUTILS_H 
+#ifndef GENEVENT_HEPMCUTILS_H
 #define GENEVENT_HEPMCUTILS_H 1
 
 // Include files
@@ -9,7 +9,7 @@
 #include "Event/HepMCEvent.h"
 
 /** @namespace HepMCUtils HepMCUtils.h GenEvent/HepMCUtils.h
- *  
+ *
  *  Utility functions to use HepMC Events
  *
  *  @author Patrick Robbe
@@ -18,11 +18,11 @@
 
 namespace HepMCUtils {
   /// Returns true if trees of vertices V1 and V2 belong to the same tree
-  bool commonTrees( HepMC::GenVertex * V1 , 
+  bool commonTrees( HepMC::GenVertex * V1 ,
                     const HepMC::GenVertex * V2 ) ;
 
   /// Compare 2 HepMC GenParticle according to their barcode
-  bool compareHepMCParticles( const HepMC::GenParticle * part1 , 
+  bool compareHepMCParticles( const HepMC::GenParticle * part1 ,
                               const HepMC::GenParticle * part2 ) ;
 
   /** Check if a particle is before or after oscillation.
@@ -46,7 +46,7 @@ namespace HepMCUtils {
 
   /// Type of HepMC particles container ordered with barcodes
   typedef std::set< HepMC::GenParticle * , particleOrder > ParticleSet ;
-};
+}
 
 //=============================================================================
 // Inline functions
@@ -55,13 +55,13 @@ namespace HepMCUtils {
 //=============================================================================
 // Function to test if vertices are in the same decay family
 //=============================================================================
-inline bool HepMCUtils::commonTrees( HepMC::GenVertex * V1 , 
+inline bool HepMCUtils::commonTrees( HepMC::GenVertex * V1 ,
                                      const HepMC::GenVertex * V2 ) {
   if ( 0 == V2 ) return false ;
   if ( 0 == V1 ) return false ;
   if ( V1 == V2 ) return true ;
   HepMC::GenVertex::vertex_iterator iter ;
-  for ( iter = V1 -> vertices_begin( HepMC::descendants ) ; 
+  for ( iter = V1 -> vertices_begin( HepMC::descendants ) ;
         iter != V1 -> vertices_end( HepMC::descendants ) ; ++iter ) {
     if ( V2 == (*iter) ) return true ;
   }
@@ -75,7 +75,7 @@ inline bool HepMCUtils::commonTrees( HepMC::GenVertex * V1 ,
 //=============================================================================
 // Function to sort HepMC::GenParticles according to their barcode
 //=============================================================================
-inline bool 
+inline bool
 HepMCUtils::compareHepMCParticles( const HepMC::GenParticle * part1 ,
                                    const HepMC::GenParticle * part2 ) {
   return ( part1->barcode() < part2->barcode() ) ;
@@ -85,14 +85,14 @@ HepMCUtils::compareHepMCParticles( const HepMC::GenParticle * part1 ,
 // Returns true if B is first B (removing oscillation B) and false
 // if the B is the B after oscillation
 //=============================================================================
-inline bool 
+inline bool
 HepMCUtils::IsBAtProduction( const HepMC::GenParticle * thePart ) {
-  if ( ( abs( thePart -> pdg_id() ) != 511 ) && 
+  if ( ( abs( thePart -> pdg_id() ) != 511 ) &&
        ( abs( thePart -> pdg_id() ) != 531 ) ) return true ;
   if ( 0 == thePart -> production_vertex() ) return true ;
   HepMC::GenVertex * theVertex = thePart -> production_vertex() ;
   if ( 1 != theVertex -> particles_in_size() ) return true ;
-  HepMC::GenParticle * theMother = 
+  HepMC::GenParticle * theMother =
     (* theVertex -> particles_in_const_begin() ) ;
   if ( theMother -> pdg_id() == - thePart -> pdg_id() ) return false ;
   return true ;
@@ -101,33 +101,33 @@ HepMCUtils::IsBAtProduction( const HepMC::GenParticle * thePart ) {
 //=============================================================================
 // Erase the daughters of one particle
 //=============================================================================
-inline 
+inline
 void HepMCUtils::RemoveDaughters( HepMC::GenParticle * theParticle ) {
   if ( 0 == theParticle ) return ;
-  
+
   HepMC::GenVertex * EV = theParticle -> end_vertex() ;
-  
+
   if ( 0 == EV ) return ;
-  
+
   theParticle -> set_status( LHCb::HepMCEvent::StableInProdGen ) ;
   HepMC::GenEvent * theEvent = theParticle -> parent_event() ;
 
-  std::vector< HepMC::GenVertex * > tempList ; 
+  std::vector< HepMC::GenVertex * > tempList ;
   HepMC::GenVertex::particle_iterator iterDes ;
-  
+
   tempList.push_back( EV ) ;
 
-  for ( iterDes = EV -> particles_begin( HepMC::descendants ) ; 
+  for ( iterDes = EV -> particles_begin( HepMC::descendants ) ;
         iterDes != EV -> particles_end( HepMC::descendants ) ; ++iterDes ) {
-    if ( 0 != (*iterDes) -> end_vertex() ) 
+    if ( 0 != (*iterDes) -> end_vertex() )
       tempList.push_back( (*iterDes) -> end_vertex() ) ;
-  } 
-  
+  }
+
   std::vector< HepMC::GenVertex * >::iterator iter ;
   for ( iter = tempList.begin() ; iter != tempList.end() ; ++iter ) {
     theEvent -> remove_vertex( *iter ) ;
     delete (*iter) ;
   }
-} 
+}
 
 #endif // GENEVENT_HEPMCUTILS_H
