0ba4f8b32342069806cd7750737b3a45eb805dcd
Marco Clemencic <marcocle@cern.ch>
2012-10-12 12:54:03 +0000

    Fixed warning: virtual function is hidden.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@146852 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ea7b82b53a91fb49da70ce87779274d72c95dc6

--- LHCb/Ex/DetCondExample/src/XmlDummyDECnv.h
+++ LHCb/Ex/DetCondExample/src/XmlDummyDECnv.h
@@ -1,5 +1,5 @@
 // $Id: XmlDummyDECnv.h,v 1.1 2005-04-22 15:17:54 marcocle Exp $
-#ifndef XMLDUMMYDECNV_H 
+#ifndef XMLDUMMYDECNV_H
 #define XMLDUMMYDECNV_H 1
 
 // Include files
@@ -7,28 +7,22 @@
 #include "DummyDetectorElement.h"
 
 /** @class XmlDummyDECnv XmlDummyDECnv.h
- *  
+ *
  *
  *  @author Marco CLEMENCIC
  *  @date   2005-04-11
  */
 class XmlDummyDECnv: public XmlUserDetElemCnv<DummyDetectorElement>{
-public: 
+public:
   /// Standard constructor
-  XmlDummyDECnv(ISvcLocator* svc); 
+  XmlDummyDECnv(ISvcLocator* svc);
 
   virtual ~XmlDummyDECnv(); ///< Destructor
 
-#ifdef __INTEL_COMPILER         // Disable ICC warning
-  #pragma warning(disable:1125) // virtual function is hidden, override intended?
-  #pragma warning(push)
-#endif
+  using XmlUserDetElemCnv<DummyDetectorElement>::i_fillSpecificObj;
   virtual StatusCode i_fillSpecificObj (xercesc::DOMElement* childElement,
                                         DummyDetectorElement* dataObj,
                                         IOpaqueAddress* address);
-#ifdef __INTEL_COMPILER // Re-enable ICC warning
-  #pragma warning(pop)
-#endif
 
 protected:
 
