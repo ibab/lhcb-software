e443c4fb93d273c243e97b07701602a5ffd5295a
Marco Clemencic <marcocle@cern.ch>
2012-10-12 09:01:20 +0000

    Fixed warning: extra ';' [-pedantic].
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@146831 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1ceb4d59aae1fd08bf81bbc5172fd6e315e970b2

--- LHCb/ST/STKernel/Kernel/ISTClusterSelector.h
+++ LHCb/ST/STKernel/Kernel/ISTClusterSelector.h
@@ -1,23 +1,23 @@
 // $Id: ISTClusterSelector.h,v 1.1 2009-02-05 11:57:10 mneedham Exp $
 // ============================================================================
-#ifndef STKERNEL_ISTCLUSTERSELECTOR_H 
+#ifndef STKERNEL_ISTCLUSTERSELECTOR_H
 #define STKERNEL_ISTCLUSTERSELECTOR_H 1
 // Include files
-// STD & STL 
+// STD & STL
 #include <functional>
 // GaudiKernel
 #include "GaudiKernel/IAlgTool.h"
 // forward declaration
 namespace LHCb{
-  class  STCluster     ;    
-};
+  class  STCluster     ;
+}
 static const InterfaceID IID_ISTClusterSelector ( "ISTClusterSelector" , 1 , 0 );
 
 /** @class ISTClusterSelector ISTClusterSelector.h
- *  
- *  An abstract interface for cluster "Selector"/"Preselector" 
- *  tools, which allow a fast and efficient selection/preselection 
- *  of clusters 
+ *
+ *  An abstract interface for cluster "Selector"/"Preselector"
+ *  tools, which allow a fast and efficient selection/preselection
+ *  of clusters
  *
  *
  *  @author M Needham
@@ -26,32 +26,32 @@ static const InterfaceID IID_ISTClusterSelector ( "ISTClusterSelector" , 1 , 0 )
 
 class ISTClusterSelector :
   virtual public IAlgTool                           ,
-  public std::unary_function<const LHCb::STCluster*,bool> 
+  public std::unary_function<const LHCb::STCluster*,bool>
 {
 public:
-  
+
   /** static interface identification
    *  @see IInterface
    *  @see IID_ISTClusterSelector
    *  @return the unique interface identifier
    */
   static const InterfaceID& interfaceID() { return IID_ISTClusterSelector; }
-  
-  
-  /** "select"/"preselect" method 
-   *  @param  cluster pointer to calo cluster object to be selected 
+
+
+  /** "select"/"preselect" method
+   *  @param  cluster pointer to calo cluster object to be selected
    *  @return true if cluster is selected
    */
   virtual bool select     ( const LHCb::STCluster* cluster ) const = 0 ;
-  
+
   /** "select"/"preselect" method (functor interface)
-   *  @param  cluster pointer to calo cluster object to be selected 
+   *  @param  cluster pointer to calo cluster object to be selected
    *  @return true if cluster is selected
    */
   virtual bool operator() ( const LHCb::STCluster* cluster ) const = 0 ;
-  
+
 };
-  
+
 // ============================================================================
 #endif // ST_ISTCLUSTERSELECTOR_H
 // ============================================================================
--- LHCb/ST/STKernel/Kernel/ISTRawADCInfo.h
+++ LHCb/ST/STKernel/Kernel/ISTRawADCInfo.h
@@ -1,22 +1,22 @@
 // $Id: ISTRawADCInfo.h,v 1.1 2009-08-20 07:58:46 mneedham Exp $
 // ============================================================================
-#ifndef STKERNEL_ISTRAWADCINFO_H 
+#ifndef STKERNEL_ISTRAWADCINFO_H
 #define STKERNEL_ISTRAWADCINFO 1
 // Include files
-// STD & STL 
+// STD & STL
 #include <string>
 #include <vector>
 // GaudiKernel
 #include "GaudiKernel/IAlgTool.h"
 // forward declaration
 namespace LHCb{
-  class  STChannelID;    
-};
+  class  STChannelID;
+}
 static const InterfaceID IID_ISTRawADCInfo ( "ISTRawADCInfo" , 1 , 0 );
 
 /** @class ISTRawADCInfo ISTRawADCInfo.h
- *  
- *  An abstract interface for linking channels to RAW info 
+ *
+ *  An abstract interface for linking channels to RAW info
  *
  *
  *  @author M Needham
@@ -26,7 +26,7 @@ static const InterfaceID IID_ISTRawADCInfo ( "ISTRawADCInfo" , 1 , 0 );
 class ISTRawADCInfo :
   virtual public IAlgTool {
 public:
-  
+
   /** static interface identification
    *  @see IInterface
    *  @see IID_ISTRawADCInfo
@@ -34,32 +34,32 @@ public:
    */
   static const InterfaceID& interfaceID() { return IID_ISTRawADCInfo; }
 
-  /** link to RAW adc info 
-  * @param LHCb::STChannelID chan  
+  /** link to RAW adc info
+  * @param LHCb::STChannelID chan
   * @param adc value
-  * @return StatusCode   
+  * @return StatusCode
   */
   virtual StatusCode link(const LHCb::STChannelID& chan, unsigned int& value) const =0;
 
   /** link to RAW adc info for a list of channels
-  * @param std::vector<LHCb::STChannelID> chan  
+  * @param std::vector<LHCb::STChannelID> chan
   * @param adc value
-  * @return StatusCode   
+  * @return StatusCode
   */
-  virtual StatusCode link(const std::vector<LHCb::STChannelID>& chans, 
+  virtual StatusCode link(const std::vector<LHCb::STChannelID>& chans,
                   std::vector<unsigned int>& values) const =0;
 
   /** link to RAW adc info for a list of channels in a window
-  * @param std::vector<LHCb::STChannelID> chan  
+  * @param std::vector<LHCb::STChannelID> chan
   * @param adc value
-  * @return StatusCode   
+  * @return StatusCode
   */
-  virtual StatusCode link(const LHCb::STChannelID& chan, 
+  virtual StatusCode link(const LHCb::STChannelID& chan,
                   const unsigned int window,
                   std::vector<unsigned int>& values) const = 0;
 
 };
-  
+
 // ============================================================================
 #endif // ST_ISTRawADCInfo
 // ============================================================================
