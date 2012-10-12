350d51197c0dc8c2c095efc91f97591ca089dcef
Marco Clemencic <marcocle@cern.ch>
2012-10-12 10:01:29 +0000

    Fixed warning 'virtual function override'.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@146837 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ec61851a54083648b4fb78f9af6e017d84d2c57e

--- LHCb/Kernel/LHCbAlgs/src/FPEAuditor.cpp
+++ LHCb/Kernel/LHCbAlgs/src/FPEAuditor.cpp
@@ -109,28 +109,6 @@ public:
     }
   }
 
-
-  void beforeInitialize(INamedInterface *i) { return before(IAuditor::Initialize,i); }
-  void beforeReInitialize(INamedInterface *i) { return before(IAuditor::ReInitialize,i); }
-  void beforeExecute(INamedInterface *i) { return before(IAuditor::Execute,i); }
-  void beforeBeginRun(INamedInterface *i) { return before(IAuditor::BeginRun,i); }
-  void beforeEndRun(INamedInterface *i) { return before(IAuditor::EndRun,i); }
-  void beforeFinalize(INamedInterface *i) { return before(IAuditor::Finalize,i); }
-
-#ifdef __INTEL_COMPILER         // Disable ICC warning
-  #pragma warning(disable:1125) // IAuditor::* function is hidden, virtual function override intended?
-  #pragma warning(push)
-#endif
-  void afterInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Initialize,i,s); }
-  void afterBeginRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::BeginRun,i,s); }
-  void afterEndRun(INamedInterface *i, const StatusCode& s) { return after(IAuditor::EndRun,i,s); }
-  void afterFinalize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Finalize,i,s); }
-#ifdef __INTEL_COMPILER         // Re-enable ICC warning 1125
-  #pragma warning(push)
-#endif
-  void afterReInitialize(INamedInterface *i, const StatusCode& s) { return after(IAuditor::ReInitialize,i,s); }
-  void afterExecute(INamedInterface *i, const StatusCode& s) { return after(IAuditor::Execute,i,s); }
-
 private:
   bool activeAt(const std::string s) { return ( std::find(m_when.begin(),m_when.end(),s)!=m_when.end() ); }
   bool beforeCannotHaveBeenCalled(CustomEventType type, INamedInterface *i) {
