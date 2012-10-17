23eb7c26dbb0908f4e4a0a625311f2646dbfade9
Marco Clemencic <marcocle@cern.ch>
2012-10-17 15:49:05 +0000

    imported the shell wrapper for the LCG compilers from RPM
    Imported and modified the shell wrapper for the LCG compilers from the
    package cern-lxdistcc-wrappers-1-11.slc6.x86_64.rpm, with some changes,
    and created the symlinks for a few known versions.
    
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@147010 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 002f938b51080fa66998f6c7d471287ba9ecefdd

---
+++ LbScripts/LbUtils/scripts/cern-lxdistcc-wrappers.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# cern-lxdistcc-wrappers.sh: v1 KELEMEN Peter <lxdistcc-admins@cern.ch>
+# 
+# 2012-10-17: Modified by Marco Clemencic
+#
+_self=${0##*/}
+
+setup_gcc () {
+	_version=${_self##*-}
+	_platform=${_platform:-x86_64-slc6}
+        _external=${LCG_external:-/afs/cern.ch/sw/lcg/external}
+	
+	_prefix="${_external}/gcc/${_version}"
+	_bin="${_prefix}/${_platform}/bin"
+	_lib="${_prefix}/${_platform}/lib64"
+	LD_LIBRARY_PATH="${_lib}${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
+	PATH="${_bin}${PATH:+:}${PATH}"
+	COMPILER_PATH="${_prefix}/${_platform}/lib/gcc/x86_64-unknown-linux-gnu/${_version}"
+
+	export LD_LIBRARY_PATH
+	export PATH
+	export COMPILER_PATH
+}
+
+case ${_self} in
+
+        lcg-*-4.3.5|lcg-*-4.3.6|lcg-*-4.5.3|lcg-*-4.6.2|lcg-*-4.7.2)
+		
+                setup_gcc
+                ;;
+
+	*)
+		echo "E: Unsupported compiler '${_self}', please contact <marco.clemencic@cern.ch>"
+		exit 100
+		;;
+esac
+
+exec ${_bin}/${_self} "$@"
+
+# End of file.
