3286f4fb032f6ce1f8f166f0ea462287b7b0d96f
Gloria Corti <gloria.corti@cern.ch>
2012-10-19 15:08:35 +0000

    remove obsolete files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@147163 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 52576b0f3c295e9db8892a4c95378ddd5d7dcb1d

--- Gauss/Sim/GaussUMC/src/GaussUMC_dll.cpp
+++
@@ -1,9 +0,0 @@
-// $Id: GaussUMC_dll.cpp,v 1.1.1.1 2009-10-05 09:44:32 cocov Exp $
-// Include files 
-
-#include "GaudiKernel/LoadFactoryEntries.h"
-
-//-- Just touch this file (delete this line) so that it will be saved
-
-LOAD_FACTORY_ENTRIES(GaussUMC)
-
