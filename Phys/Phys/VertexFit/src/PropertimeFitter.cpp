6ed06057455560bcf6e2738f316bbc768953bdca
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-10-29 13:44:01 +0000

    Minor CPU optimisations in PropertimeFitter
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@147483 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b9f01f5bc16c88bf7b3ad2666a348f1e7b7016da

--- Phys/Phys/VertexFit/src/PropertimeFitter.cpp
+++ Phys/Phys/VertexFit/src/PropertimeFitter.cpp
@@ -66,18 +66,21 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
                                   const LHCb::Particle& B,
                                   double& propertime, 
                                   double& error,
-                                  double& chisq) const
+                                  double& chisq ) const
 {
 
-  Gaudi::XYZPoint PVposition = PV.position();
-  Gaudi::SymMatrix3x3 CovPV = PV.covMatrix();
+  const Gaudi::XYZPoint& PVposition = PV.position();
+  const Gaudi::SymMatrix3x3& CovPV  = PV.covMatrix();
 
-  Gaudi::XYZPoint BPosition= B.referencePoint();
-  Gaudi::LorentzVector BLmom= B.momentum();
+  const Gaudi::XYZPoint& BPosition  = B.referencePoint();
+  const Gaudi::LorentzVector& BLmom = B.momentum();
 
-  double BMeasuredMass = std::sqrt(BLmom.T()*BLmom.T()-BLmom.X()*BLmom.X()-BLmom.Y()*BLmom.Y()-BLmom.Z()*BLmom.Z());
+  const double BMeasuredMass = std::sqrt( BLmom.T()*BLmom.T() - 
+                                          BLmom.X()*BLmom.X() - 
+                                          BLmom.Y()*BLmom.Y() - 
+                                          BLmom.Z()*BLmom.Z() );
 
-  Gaudi::SymMatrix7x7 CovB = B.covMatrix();
+  const Gaudi::SymMatrix7x7& CovB = B.covMatrix();
 
   Gaudi::Matrix7x7 Te2m = ROOT::Math::SMatrixIdentity();
   Te2m(6,3) = -BLmom.X()/BMeasuredMass;
@@ -85,7 +88,7 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
   Te2m(6,5) = -BLmom.Z()/BMeasuredMass;
   Te2m(6,6) =  BLmom.T()/BMeasuredMass;
 
-  Gaudi::SymMatrix7x7 CovBm = ROOT::Math::Similarity<double,7,7>(Te2m, CovB);
+  const Gaudi::SymMatrix7x7 CovBm = ROOT::Math::Similarity<double,7,7>(Te2m, CovB);
 
   ROOT::Math::SVector<double, 10> X;
   X[0] = PVposition.x();
@@ -101,30 +104,30 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
 
   SymMatrix10x10 Cx;
 
-  for(int l1=0;l1<=2; l1++)
-    for(int l2=0;l2<=l1; l2++) Cx(l1,l2) = CovPV(l1,l2);
+  for ( int l1 = 0; l1 <= 2; ++l1 )
+    for ( int l2 = 0; l2<=l1; ++l2 ) Cx(l1,l2) = CovPV(l1,l2);
 
-  for(int l1=0;l1<=6; l1++)
-    for(int l2=0;l2<=l1; l2++) Cx(l1+3,l2+3) = CovBm(l1,l2);
+  for ( int l1 = 0; l1 <= 6; ++l1 )
+    for ( int l2 = 0; l2<=l1; ++l2 ) Cx(l1+3,l2+3) = CovBm(l1,l2);
 
-  for(int l1=3;l1<=9; l1++)
-    for(int l2=0;l2<=2; l2++) Cx(l1,l2) = 0.0;
+  for ( int l1 = 3; l1 <= 9; ++l1 )
+    for ( int l2 = 0; l2 <= 2; ++l2 ) Cx(l1,l2) = 0.0;
 
   // xpv, ypv, zpv, xb, yb, zb, pxb, pyb, pzb, mb
-  ROOT::Math::SVector<double, 10> vfit=X;
+  ROOT::Math::SVector<double, 10> vfit = X;
 
   SymMatrix10x10 cfit = Cx;
 
-  bool converged=false;
-  int iter=0;
+  bool converged = false;
+  int iter       = 0;
 
-  double chi2PreviousFit=9999.;
-  double chi2Fit=999.;
+  double chi2PreviousFit = 9999.;
+  double chi2Fit         = 999.;
 
-  while(!converged && iter< m_maxIter) 
+  while ( !converged && iter< m_maxIter ) 
   {
     ++iter;
-
+    
     if ( msgLevel(MSG::VERBOSE) )
       verbose() << ":-) Iteration   " << iter << endreq;
 
@@ -132,8 +135,8 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
     //f(1)=(yb-ypv)*pzb-(zb-zpv)*pyb
 
     ROOT::Math::SVector<double, 2> f;
-    f(0)= (vfit[3]-vfit[0])*vfit[8] - (vfit[5]-vfit[2])*vfit[6];
-    f(1)= (vfit[4]-vfit[1])*vfit[8] - (vfit[5]-vfit[2])*vfit[7];
+    f(0) = (vfit[3]-vfit[0])*vfit[8] - (vfit[5]-vfit[2])*vfit[6];
+    f(1) = (vfit[4]-vfit[1])*vfit[8] - (vfit[5]-vfit[2])*vfit[7];
 
     //D is the derivative matrix
     ROOT::Math::SMatrix<double, 2, 10> D;
@@ -160,51 +163,57 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
     D(1,8) = (vfit[4]-vfit[1]);
     D(1,9) = 0.0;
 
-    ROOT::Math::SVector<double, 2> d = f - D*vfit;
+    const ROOT::Math::SVector<double, 2> d = f - D*vfit;
 
-    Gaudi::SymMatrix2x2 VD=ROOT::Math::Similarity<double,2,10>(D, Cx);
-    if(!VD.Invert())
+    Gaudi::SymMatrix2x2 VD = ROOT::Math::Similarity<double,2,10>(D, Cx);
+    if ( !VD.Invert() )
     {
       if ( msgLevel(MSG::DEBUG) )
         debug() << "could not invert matrix VD in fit! " <<endreq;
       return StatusCode::FAILURE;
     }
 
-    ROOT::Math::SVector<double, 2> alpha=D*X+d;
+    const ROOT::Math::SVector<double, 2> alpha  = D*X+d;
 
-    ROOT::Math::SVector<double, 2> lambda=VD*alpha;
+    const ROOT::Math::SVector<double, 2> lambda = VD*alpha;
 
-    ROOT::Math::SMatrix<double, 10,2> DT = ROOT::Math::Transpose(D);
+    const ROOT::Math::SMatrix<double, 10,2> DT  = ROOT::Math::Transpose(D);
 
-    vfit=X-Cx*DT*lambda;
+    vfit = X-Cx*DT*lambda;
 
-    SymMatrix10x10 delataC1=ROOT::Math::Similarity<double,10,2>(DT, VD);
+    const SymMatrix10x10 delataC1 = ROOT::Math::Similarity<double,10,2>(DT, VD);
 
-    SymMatrix10x10 delataC2=ROOT::Math::Similarity<double,10,10>(Cx, delataC1);
+    const SymMatrix10x10 delataC2 = ROOT::Math::Similarity<double,10,10>(Cx, delataC1);
 
-    cfit=Cx -delataC2;
+    cfit = Cx -delataC2;
 
-    chi2Fit=ROOT::Math::Dot(alpha,lambda);
+    chi2Fit = ROOT::Math::Dot(alpha,lambda);
     //chi2Fit+= 2*ROOT::Math::Dot(lambda,f);
 
-    if(fabs(chi2Fit-chi2PreviousFit)<m_maxDeltaChi2) {
-      converged=true;
-    } else {
-      chi2PreviousFit=chi2Fit;
+    if ( fabs(chi2Fit-chi2PreviousFit) < m_maxDeltaChi2 ) 
+    {
+      converged = true;
+    } 
+    else 
+    {
+      chi2PreviousFit = chi2Fit;
     }
   }  // end chi2 minimization iteration
 
-  if(!converged)  return StatusCode::FAILURE;
+  if ( !converged ) return StatusCode::FAILURE;
 
-  if(m_applyBMassConstraint) {
-    //int Bpid = B.particleID().pid();
-    //ParticleProperty*  partProp = m_ppSvc->findByStdHepID(Bpid  );
-    const LHCb::ParticleProperty*  partProp = m_ppSvc->find(B.particleID());
-    double nominalBMass = partProp->mass();
+  if ( m_applyBMassConstraint ) 
+  {
+    const LHCb::ParticleProperty* partProp = m_ppSvc->find(B.particleID());
+    if ( !partProp )
+    {
+      return Error( "Failed to find ParticleProperty" );
+    }
+    const double nominalBMass = partProp->mass();
     ROOT::Math::SMatrix<double,1,10> DD;
     Gaudi::Vector1 dd;
-    DD(0,9)=1.;
-    dd[0]=-nominalBMass;
+    DD(0,9) = 1.;
+    dd[0] = -nominalBMass;
     Gaudi::SymMatrix1x1 Cd = ROOT::Math::Similarity<double, 1, 10>(DD,cfit);
     if ( !Cd.Invert() )
     {
@@ -212,17 +221,17 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
         debug() << "could not invert matrix Cd in fit" << endmsg;
       return StatusCode::FAILURE;
     }
-    vfit-= cfit*ROOT::Math::Transpose(DD)*Cd*(DD*vfit+dd);
-    SymMatrix10x10 deltacfit1=ROOT::Math::SimilarityT<double,1,10>(DD,Cd);
-    SymMatrix10x10 deltacfit2=ROOT::Math::Similarity<double,10,10>(cfit,deltacfit1);
+    vfit -= cfit*ROOT::Math::Transpose(DD)*Cd*(DD*vfit+dd);
+    const SymMatrix10x10 deltacfit1 = ROOT::Math::SimilarityT<double,1,10>(DD,Cd);
+    const SymMatrix10x10 deltacfit2 = ROOT::Math::Similarity<double,10,10>(cfit,deltacfit1);
     cfit -= deltacfit2;
 
-    for(int i=0;i<=9;i++) cfit(9,i)=0.0;
+    for ( int i = 0; i<=9; ++i ) { cfit(9,i) = 0.0; }
 
   }
 
   //propertime = (xb-xpv)*mb/pxb ;
-  propertime =  (vfit[3]-vfit[0])*vfit[9]/vfit[6];
+  propertime = (vfit[3]-vfit[0])*vfit[9]/vfit[6];
 
   ROOT::Math::SMatrix<double, 1, 10> JA;
   JA(0,0) = -vfit[9]/vfit[6];
@@ -236,15 +245,15 @@ StatusCode PropertimeFitter::fit( const LHCb::VertexBase& PV,
   JA(0,8) = 0.0;
   JA(0,9) = (vfit[3]-vfit[0])/vfit[6];
 
-  Gaudi::SymMatrix1x1 CovTau = ROOT::Math::Similarity<double,1,10>(JA, cfit);
+  const Gaudi::SymMatrix1x1 CovTau = ROOT::Math::Similarity<double,1,10>(JA, cfit);
 
   error = std::sqrt(CovTau(0,0));
 
   // convert c*tau to tau
-  propertime /=  c_light;
-  error /= c_light;
+  propertime /= c_light;
+  error      /= c_light;
 
-  chisq =  chi2Fit;
+  chisq = chi2Fit;
 
   return StatusCode::SUCCESS ;
 }
