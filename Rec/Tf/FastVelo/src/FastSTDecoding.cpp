97984d95eb47659ed712046f066bc6dde2377001
ocallot <ocallot@cern.ch>
2012-11-02 14:00:19 +0000

    Fast ST decoding too
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@147595 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3de4c9821f23e885998ff1050c469a5f5ee1bda4

--- Rec/Tf/FastVelo/src/FastSTDecoding.cpp
+++ Rec/Tf/FastVelo/src/FastSTDecoding.cpp
@@ -2,31 +2,36 @@
 
 // from Gaudi
 #include "GaudiKernel/AlgFactory.h"
-#include "Event/VeloLiteCluster.h"
+#include "Event/STLiteCluster.h"
 #include "Event/RawEvent.h"
+#include "Kernel/STTell1Board.h"
 // local
-#include "FastVeloDecoding.h"
+#include "FastSTDecoding.h"
 
 //-----------------------------------------------------------------------------
-// Implementation file for class : FastVeloDecoding
+// Implementation file for class : FastSTDecoding
 //
 // 2012-10-15 : Olivier Callot
 //-----------------------------------------------------------------------------
 
 // Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( FastVeloDecoding )
+DECLARE_ALGORITHM_FACTORY( FastSTDecoding )
 
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-  FastVeloDecoding::FastVeloDecoding( const std::string& name,
+  FastSTDecoding::FastSTDecoding( const std::string& name,
                                       ISvcLocator* pSvcLocator)
     : GaudiAlgorithm ( name , pSvcLocator )
-    , m_velo(NULL)
+    , m_stDet( NULL )
+    , m_readoutTool( NULL )
+    , m_bankType( LHCb::RawBank::IT )
+    , m_compareLocation("")
+    , m_outputIndex(0)
 {
-  declareProperty( "OutputLocation",    m_outputLocation  = LHCb::VeloLiteClusterLocation::Default );
-  declareProperty( "MaxVeloClusters",   m_maxVeloClusters = 10000  );
+  declareProperty( "DetectorName",      m_detectorName    = "IT"   );
+  declareProperty( "OutputLocation",    m_outputLocation  = LHCb::STLiteClusterLocation::ITClusters );
   declareProperty( "ErrorCount",        m_errorCount      = 0      );
   declareProperty( "IgnoreErrors",      m_ignoreErrors    = false  );
   declareProperty( "CompareResult",     m_compareResult   = false  );
@@ -34,18 +39,37 @@ DECLARE_ALGORITHM_FACTORY( FastVeloDecoding )
 //=============================================================================
 // Destructor
 //=============================================================================
-FastVeloDecoding::~FastVeloDecoding() {}
+FastSTDecoding::~FastSTDecoding() {}
 
 //=============================================================================
 // Initialization
 //=============================================================================
-StatusCode FastVeloDecoding::initialize() {
+StatusCode FastSTDecoding::initialize() {
   StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
   if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
 
   if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
 
-  m_velo = getDet<DeVelo>( DeVeloLocation::Default );
+  if ( "TT" == m_detectorName ) {
+    m_bankType       = LHCb::RawBank::TT;
+    m_outputLocation = LHCb::STLiteClusterLocation::TTClusters;
+  } else if ( "IT" == m_detectorName ) {
+    m_bankType       = LHCb::RawBank::IT;
+    m_outputLocation = LHCb::STLiteClusterLocation::ITClusters;
+  }
+  if ( m_compareResult ) {
+    m_compareLocation = m_outputLocation;
+    m_outputLocation  = m_outputLocation + "Test";
+  }
+  
+  m_stDet = getDet<DeSTDetector>( DeSTDetLocation::location(m_detectorName) );
+
+  std::string readoutToolName = m_detectorName + "ReadoutTool";
+  m_readoutTool = tool<ISTReadoutTool>( readoutToolName );
+
+  info() << "Context = " << context() << endmsg;
+
+  if ( "FastTES" == context() ) m_outputIndex = containerIndex( m_outputLocation );
 
   return StatusCode::SUCCESS;
 }
@@ -53,81 +77,82 @@ StatusCode FastVeloDecoding::initialize() {
 //=============================================================================
 // Main execution
 //=============================================================================
-StatusCode FastVeloDecoding::execute() {
+StatusCode FastSTDecoding::execute() {
 
   if ( msgLevel(MSG::DEBUG) ) debug() << "==> Execute" << endmsg;
 
   LHCb::RawEvent* rawEvent = get<LHCb::RawEvent>(LHCb::RawEventLocation::Default );
-  const std::vector<LHCb::RawBank*>& banks = rawEvent->banks( LHCb::RawBank::Velo );
+  const std::vector<LHCb::RawBank*>& banks = rawEvent->banks( m_bankType );
 
-  LHCb::VeloLiteCluster::FastContainer* fastCont = new LHCb::VeloLiteCluster::FastContainer();
-  put( fastCont, m_outputLocation);
-
-  debug() << "Number of Velo banks : " << banks.size() << endmsg;
+  LHCb::STLiteCluster::FastContainer* fastCont = new LHCb::STLiteCluster::FastContainer();
+  if ( "FastTES" == context() ) {
+    put( fastCont, m_outputIndex );
+  } else {
+    put( fastCont, m_outputLocation );
+  }
+  
+  debug() << "Number of ST banks : " << banks.size() << endmsg;
 
   for (std::vector<LHCb::RawBank*>::const_iterator bi = banks.begin(); bi != banks.end(); ++bi) {
     const LHCb::RawBank* rb = *bi;
 
     //== protect against corrupted banks
-    if(LHCb::RawBank::MagicPattern!=rb->magic()) {
+    if( LHCb::RawBank::MagicPattern != rb->magic() ) {
       failEvent(format("Bad magic pattern in bank source ID %i", rb->sourceID()),
-                "CorruptVeloBuffer",CorruptVeloBuffer,false);
+                "CorruptSTBuffer",CorruptSTBuffer,false);
       continue;
     }
+    if ( 3 != rb->version() ) {
+      failEvent( format( "Unsupported bank version %2d", rb->version() ), 
+                "CorruptSTBuffer",CorruptSTBuffer,false);
+      continue;
+    }
+    STDAQ::version bankVersion = STDAQ::v4;
 
-    //== Get the sensor, in fact the source id is the neumber...
-    const DeVeloSensor* sensor = m_velo->sensorByTell1Id(static_cast<unsigned int>(rb->sourceID()));
-    if (!sensor) {
-      failEvent( format("Could not map source ID %i to sensor number!", rb->sourceID()),
-                 "BadTELL1IDMapping",BadTELL1IDMapping,false);
+   // get the board and data
+    STTell1Board* aBoard = m_readoutTool->findByBoardID( STTell1ID( rb->sourceID() ) );
+    if (!aBoard ){
+      failEvent( format( "Invalid source ID %4d ->skip bank", rb->sourceID() ), 
+                 "CorruptSTBuffer", CorruptSTBuffer, false );
       continue;
-    }
-    sensor->tell1EventInfo().setHasError(false);
-    unsigned int sensorNb = sensor->sensorNumber();
+    }    
 
     const unsigned int* data = rb->data();
 
     unsigned int nClus = (*data) & 0xFFFF;
-    //unsigned int pcn   = ( (*data) & 0xFF0000 ) >> 16;
     unsigned int error = ( (*data) & 0x1000000 ) >> 24;
     if ( !m_ignoreErrors && 0 != error ) continue;
-    if ( msgLevel( MSG::DEBUG ) ) {
-      debug() << "Process source id " << rb->sourceID() << " that is sensor " << sensorNb
-              << " data " << (*data) << " nData " << nClus << endmsg;
-    }
+    debug() << "Process source id " << rb->sourceID()
+            << " data " << (*data) << " nData " << nClus << endmsg;
+
     unsigned short* word = (unsigned short*) (data+1);
     for (  ; 0 < nClus; nClus--, ++word ) {
-      unsigned int frac    = (*word) & 0x7;
-      unsigned int channel = ( (*word) & 0x3FF8 ) >> 3;
+      unsigned int frac    = (*word) & 0x3;
+      unsigned int channel = ( (*word) & 0x3FFC ) >> 2;
       unsigned int size    = ( (*word) & 0x4000 ) >> 14;
       bool high            = ( ( (*word) & 0x8000 ) >> 15) != 0;
-      fastCont->push_back( LHCb::VeloLiteCluster( frac, size, high,
-                                                  LHCb::VeloChannelID( sensorNb, channel)));
+
+      const std::pair<LHCb::STChannelID,int> chan = aBoard->DAQToOffline( frac, bankVersion, 
+                                                                          STDAQ::StripRepresentation( channel ));
+      LHCb::STLiteCluster liteCluster( chan.second, size, high, chan.first );
+      fastCont->push_back( liteCluster );
     }
-    sensor->tell1EventInfo().setWasDecoded(true);
   }
 
-  if( fastCont->size() > m_maxVeloClusters){
-    fastCont->clear();
-    failEvent( format("Deleted all lite VELO clusters as more than limit %i in the event",
-                      m_maxVeloClusters),
-              "TooManyClusters", TooManyClusters, true );
-  }
-
-
-  std::sort( fastCont->begin(), fastCont->end(), SiDataFunctor::Less_by_Channel< LHCb::VeloLiteCluster >() );
+  std::sort( fastCont->begin(), fastCont->end(), SiDataFunctor::Less_by_Channel< LHCb::STLiteCluster >() );
 
   //== Test if the result is equal to another decoding in the default location
 
   if ( m_compareResult ) {
-    LHCb::VeloLiteCluster::FastContainer* old;
-    old = get<LHCb::VeloLiteCluster::FastContainer>(LHCb::VeloLiteClusterLocation::Default );
+    LHCb::STLiteCluster::FastContainer* old;
+    old = get<LHCb::STLiteCluster::FastContainer>( m_compareLocation );
 
     if ( old->size() != fastCont->size() ) {
       info() << "=== Wrong size: old " << old->size() << " new " << fastCont->size() << endmsg;
+      return StatusCode::FAILURE;
     } else {
-      LHCb::VeloLiteCluster::FastContainer::iterator itOld = old->begin();
-      LHCb::VeloLiteCluster::FastContainer::iterator itNew = fastCont->begin();
+      LHCb::STLiteCluster::FastContainer::iterator itOld = old->begin();
+      LHCb::STLiteCluster::FastContainer::iterator itNew = fastCont->begin();
       for ( ;old->end() != itOld; ++itOld, ++itNew ) {
         if ( (*itOld).channelID()          != (*itNew).channelID()          ||
              (*itOld).pseudoSize()         != (*itNew).pseudoSize()         ||
@@ -150,7 +175,7 @@ StatusCode FastVeloDecoding::execute() {
 //=============================================================================
 //  Finalize
 //=============================================================================
-StatusCode FastVeloDecoding::finalize() {
+StatusCode FastSTDecoding::finalize() {
 
   if ( msgLevel(MSG::DEBUG) ) debug() << "==> Finalize" << endmsg;
 
@@ -160,13 +185,13 @@ StatusCode FastVeloDecoding::finalize() {
 //=========================================================================
 //  Print and store failure information
 //=========================================================================
-void FastVeloDecoding::failEvent( const std::string &ErrorText,
+void FastSTDecoding::failEvent( const std::string &ErrorText,
                                   const std::string &ProcText,
                                   AlgStatusType status,
                                   bool procAborted){
-  // set ProcStat for this event to failed in DecodeVeloRawBuffer
+  // set ProcStat for this event to failed in DecodeSTRawBuffer
   LHCb::ProcStatus *pStat = getOrCreate<LHCb::ProcStatus,LHCb::ProcStatus>(LHCb::ProcStatusLocation::Default);
-  pStat->addAlgorithmStatus("DecodeVeloRawBuffer", "VELO", ProcText, status, procAborted);
+  pStat->addAlgorithmStatus("FastSTDecoding", "ST", ProcText, status, procAborted);
   if( m_errorCount > 0 || msgLevel(MSG::DEBUG) ) {
     unsigned int msgCount = m_errorCount;
     if ( msgLevel(MSG::DEBUG) ) msgCount += 10;
---
+++ Rec/Tf/FastVelo/src/FastSTDecoding.h
@@ -0,0 +1,56 @@
+#ifndef FASTSTDECODING_H 
+#define FASTSTDECODING_H 1
+
+// Include files
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+#include "STDet/DeSTDetector.h"
+#include "Kernel/ISTReadoutTool.h"
+#include "Event/ProcStatus.h"
+
+/** @class FastSTDecoding FastSTDecoding.h
+ *   
+ *
+ *  @author Olivier Callot
+ *  @date   2012-10-15
+ */
+class FastSTDecoding : public GaudiAlgorithm {
+public: 
+  /// Standard constructor
+  FastSTDecoding( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~FastSTDecoding( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+  enum AlgStatusType{
+    OK = 0,
+    BadTELL1IDMapping = 1,
+    UnsupportedBufferVersion = 2,
+    CorruptSTBuffer = 3,
+    TooManyClusters = 4,
+    HeaderErrorBit = 5,
+    Other = 99
+  };
+
+protected:
+  void failEvent( const std::string &ErrorText,
+                  const std::string &ProcText,
+                  AlgStatusType status,
+                  bool procAborted );
+
+private:
+  std::string   m_outputLocation;
+  bool          m_ignoreErrors;
+  bool          m_compareResult;
+  DeSTDetector* m_stDet;
+  ISTReadoutTool* m_readoutTool;
+  std::string   m_detectorName;
+  LHCb::RawBank::BankType m_bankType;
+  std::string   m_compareLocation;
+  unsigned int  m_errorCount;  ///< number of errors to print
+  unsigned int  m_outputIndex;
+};
+#endif // FASTSTDECODING_H
