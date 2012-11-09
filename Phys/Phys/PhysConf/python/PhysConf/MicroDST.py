92f8cfee6b063b5a8bbc841aca3e8b5c934ea1a9
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-11-09 16:35:36 +0000

    Improvements to the MicroDST module
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@147803 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7b8b1ff92a65d0307def3d37508f632f25a651f4

--- Phys/Phys/PhysConf/python/PhysConf/MicroDST.py
+++ Phys/Phys/PhysConf/python/PhysConf/MicroDST.py
@@ -1,56 +1,7 @@
-#!/usr/bin/env python
-# =============================================================================
-# @file Bender/MicroDST.py
-#
-# Helper module to deal with uDST, mainly TisTos'sing for uDst 
-#
-#  This file is a part of 
-#  <a href="http://cern.ch/lhcb-comp/Analysis/Bender/index.html">Bender project</a>
-#  <b>``Python-based Interactive Environment for Smart and Friendly 
-#   Physics Analysis''</b>
-#
-#  The package has been designed with the kind help from
-#  Pere MATO and Andrey TSAREGORODTSEV. 
-#  And it is based on the 
-#  <a href="http://cern.ch/lhcb-comp/Analysis/LoKi/index.html">LoKi project:</a>
-#  ``C++ ToolKit for Smart and Friendly Physics Analysis''
-#
-#  By usage of this code one clearly states the disagreement 
-#  with the smear campaign of Dr.O.Callot et al.: 
-#  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
-#
-# @author VanyaBELYAEV Ivan.Belyaev@nikhef.nl
-# @date   2011-10-05
-#
-#                    $Revision: 129470 $
-#  Last modification $Date: 2011-09-26 12:40:16 +0200 (Mon, 26 Sep 2011) $
-#                 by $Author: ibelyaev $ 
 # =============================================================================
 """
-
-Helper module to deal with uDST, mainly TisTos'sing for uDst 
-
-This file is a part of BENDER project:
-``Python-based Interactive Environment for Smart and Friendly Physics Analysis''
-
-The project has been designed with the kind help from
-Pere MATO and Andrey TSAREGORODTSEV. 
-
-And it is based on the 
-LoKi project: ``C++ ToolKit for Smart and Friendly Physics Analysis''
-
-By usage of this code one clearly states the disagreement 
-with the smear campaign of Dr.O.Callot et al.: 
-``No Vanya's lines are allowed in LHCb/Gaudi software.''
-
+Helper module to deal with uDST
 """
-# =============================================================================
-__author__  = "Vanya BELYAEV Ivan.Belyaev@itep.ru"
-__date__    = "2011-10-05"
-__version__ = "$Revision: 129470 $"
-__all__     = ( 'trunkConf' , )
-# =============================================================================
-
 # =============================================================================
 ## configure variopsi uDst settings from RootInTES 
 def uDstConf ( rootInTes ) :
@@ -66,8 +17,7 @@ def uDstConf ( rootInTes ) :
     #
     ## 'rootify'
     #
-    if 0 != rootInTes.find ( '/Event/') : rootInTes = '/Event/' + rootInTes 
-
+    if 0 != rootInTes.find ( '/Event/') : rootInTes = '/Event/' + rootInTes
     rootInTes = TCU.fixTrunk ( rootInTes )
 
     #
@@ -84,12 +34,12 @@ def uDstConf ( rootInTes ) :
     #
 
     #
-    ## 0. Kill Event/DAQ
+    ## 0. Kill Event/DAQ and Event/pRec
     #
-    def killDAQ ()  : 
+    def killDAQAndpRec ()  : 
         from Configurables import EventNodeKiller
-        killer = EventNodeKiller ( "KillDAQ" )
-        killer.Nodes += [ '/Event/DAQ' ]
+        killer = EventNodeKiller ( "KillDAQAndpRecNodes" )
+        killer.Nodes += [ '/Event/DAQ', '/Event/pRec' ]
         try : 
             from Gaudi.Configuration import getConfigurable
             conf   = getConfigurable ( 'DaVinciEventInitSeq' ) 
@@ -98,7 +48,7 @@ def uDstConf ( rootInTes ) :
             print "Failed to add killer agent for", killer.Nodes
             
     from Gaudi.Configuration import appendPostConfigAction
-    appendPostConfigAction ( killDAQ )
+    appendPostConfigAction ( killDAQAndpRec )
  
     #
     ## 1. Copy/Link ODIN, RawEvent and RecSummary
@@ -116,36 +66,24 @@ def uDstConf ( rootInTes ) :
     header  = Link ( 'LinkHeader'                        ,
                      What   =       '/Event/Rec/Header'  ,
                      Target = rootInTes +  'Rec/Header'  )
-    
     dod.AlgMap [ rawEvt  . Target ] = rawEvt 
     dod.AlgMap [ odin    . Target ] = odin
     dod.AlgMap [ summary . Target ] = summary
     dod.AlgMap [ header  . Target ] = header
 
     #
-    ##
+    ## CRJ No longer needed.
     #
-    from Configurables import OdinTimeDecoder, ODINDecodeTool
-    odinDec = OdinTimeDecoder ( )
-    odinDec.addTool( ODINDecodeTool, 'ODINDecodeTool' )
-    odinDec.ODINDecodeTool.RawEventLocations = [
-        rootInTes + 'DAQ/RawEvent' ] + odinDec.ODINDecodeTool.RawEventLocations
+    #from Configurables import OdinTimeDecoder, ODINDecodeTool
+    #odinDec = OdinTimeDecoder ( )
+    #odinDec.addTool( ODINDecodeTool, 'ODINDecodeTool' )
+    #odinDec.ODINDecodeTool.RawEventLocations = [
+    #    rootInTes + 'DAQ/RawEvent' ] + odinDec.ODINDecodeTool.RawEventLocations
     
     from Configurables import DecodePileUpData
     pileupDec = DecodePileUpData()
     pileupDec.RawEventLocation = rootInTes + 'DAQ/RawEvent' 
     
-    
-# =============================================================================
-if __name__ == '__main__' :
-    print '*'*120
-    print                      __doc__
-    print ' Author  : %s ' %   __author__    
-    print ' Version : %s ' %   __version__
-    print ' Date    : %s ' %   __date__
-    print '*'*120
-
-
 # =============================================================================
 # The END 
 # =============================================================================
