afa111c225fc3dafe2aa2ca1dec4d027930cc7c4
rlambert <rlambert@cern.ch>
2012-11-12 09:53:07 +0000

    remember to add lcgdict
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@147842 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8059900716137f3186cb53e4fec2c90933f309c4

---
+++ LHCb/Event/FSREvent/xml/lcgdict/lcg_selection.h
@@ -0,0 +1,15 @@
+// $Id: lcg_selection.h,v 1.4 2010-03-17 21:30:50 raaij Exp $
+#ifndef FSREVENT_LCGDICT_H 
+#define FSREVENT_LCGDICT_H 1
+
+// Additional classes to be added to automatically generated lcgdict
+
+// begin include files
+#include <map>
+#include <string>
+#include <utility>
+// end include files
+
+template std::map<std::string, unsigned long long>;
+
+#endif // FSREVENT_LCGDICT_H
