9af9a4ccfe8f5f2bca37569ba9cd8e1662ce012e
Marco Clemencic <marcocle@cern.ch>
2012-11-13 17:10:49 +0000

    improvements/fixes to the generic CMake makefile
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@147910 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ee4c3437054d22bc70c432ff0cf4892bf4996fd9

--- LbScripts/LbUtils/data/Makefile-cmake.mk
+++ LbScripts/LbUtils/data/Makefile-cmake.mk
@@ -1,6 +1,7 @@
 ################################################################################
 #
-# Generic Makefile to simplify the use of CMake projects.
+# Generic Makefile to simplify the use of CMake projects
+# ------------------------------------------------------ 
 #
 # This simple Makefile is meant to provide a simplified entry point for the
 # configuration and build of CMake-based projects that use a default toolchain
@@ -23,33 +24,38 @@
 #     clean
 #         remove build products from the build directory
 #
-#     purge [*]
+#     purge [*]_
 #         deep clean of the build, including InstallArea
 #         (requires re-configuration)
 #
 #     help
 #         print the list of available targets
 #
-#     configure [*]
+#     configure [*]_
 #         alias to CMake 'rebuild_cache' target
 #
-# .. [*]: targets defined by this Makefile. 
+#     tests [*]_
+#         backward-compatibility target for the CMT generic Makefile
+#
+# :Author: Marco Clemencic
+#
+# .. [*] Targets defined by this Makefile. 
+#
 ################################################################################
 
 # settings
-PROJDIR := $(shell pwd)
-ifneq ($(wildcard $(PROJDIR)/toolchain.cmake),)
-override CMAKEFLAGS += -DCMAKE_TOOLCHAIN_FILE=$(PROJDIR)/toolchain.cmake
+ifneq ($(wildcard $(CURDIR)/toolchain.cmake),)
+override CMAKEFLAGS += -DCMAKE_TOOLCHAIN_FILE=$(CURDIR)/toolchain.cmake
 endif
-BUILDDIR := $(PROJDIR)/build.$(CMTCONFIG)
+BUILDDIR := $(CURDIR)/build.$(CMTCONFIG)
 
 # default target
 all:
 
 # deep clean
 purge:
-	$(RM) -r $(BUILDDIR) $(PROJDIR)/InstallArea/$(CMTCONFIG)
-	find $(PROJDIR) -name "*.pyc" -exec $(RM) -v \{} \;
+	$(RM) -r $(BUILDDIR) $(CURDIR)/InstallArea/$(CMTCONFIG)
+	find $(CURDIR) -name "*.pyc" -exec $(RM) -v \{} \;
 
 # delegate any target to the build directory (except 'purge')
 ifneq ($(MAKECMDGOALS),purge)
@@ -58,14 +64,25 @@ ifneq ($(MAKECMDGOALS),purge)
 endif
 
 # aliases
+.PHONY: configure tests
 ifneq ($(wildcard $(BUILDDIR)/Makefile),)
 configure: rebuild_cache
 else
-configure:
+configure: $(BUILDDIR)/Makefile
 endif
+	@ # do not delegate further
+
+tests: all
+	-$(MAKE) -C build.$(CMTCONFIG) test
+	$(MAKE) -C build.$(CMTCONFIG) QMTestSummary
+
+# Special trick to allow a non-standard makefile name
+#  If the makefile is not called 'Makefile', we get its update delegated to
+#  cmake, unless we block the delegation.
+$(lastword $(MAKEFILE_LIST)):
 	@ # do not delegate further
 
 # trigger CMake configuration
 $(BUILDDIR)/Makefile:
 	mkdir -p $(BUILDDIR)
-	cd $(BUILDDIR) && cmake $(CMAKEFLAGS) $(PROJDIR)
+	cd $(BUILDDIR) && cmake $(CMAKEFLAGS) $(CURDIR)
