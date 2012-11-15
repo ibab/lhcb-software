0820cc16ef4212ea367d2e89f2979faf4c6a0f70
Marco Clemencic <marcocle@cern.ch>
2012-11-15 20:07:29 +0000

    Added CMake configuration files.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@148028 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 02632a9b9230761afe7b1f7da458c17bc766e31f

---
+++ LHCb/toolchain.cmake
@@ -0,0 +1,22 @@
+# Special wrapper to load the declared version of the heptools toolchain.
+set(heptools_version 64a)
+
+# Remove the reference to this file from the cache.
+unset(CMAKE_TOOLCHAIN_FILE CACHE)
+
+# Find the actual toolchain file.
+find_file(CMAKE_TOOLCHAIN_FILE
+          NAMES heptools-${heptools_version}.cmake
+          HINTS ENV CMTPROJECTPATH
+          PATHS ${CMAKE_CURRENT_LIST_DIR}/cmake/toolchain
+          PATH_SUFFIXES toolchain)
+
+if(NOT CMAKE_TOOLCHAIN_FILE)
+  message(FATAL_ERROR "Cannot find heptools-${heptools_version}.cmake.")
+endif()
+
+# Reset the cache variable to have proper documentation.
+set(CMAKE_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE}
+    CACHE FILEPATH "The CMake toolchain file" FORCE)
+
+include(${CMAKE_TOOLCHAIN_FILE})
