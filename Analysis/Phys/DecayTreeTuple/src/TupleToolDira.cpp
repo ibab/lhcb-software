401c74bfd0bb3a443cedf039f971735438afefaa
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-11-16 18:24:30 +0000

    Minor clean up of TupleToolDira
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@148073 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1cdd8b5dfd138b94ed0829e48ade50942dceeb4e

--- Analysis/Phys/DecayTreeTuple/src/TupleToolDira.cpp
+++ Analysis/Phys/DecayTreeTuple/src/TupleToolDira.cpp
@@ -49,28 +49,34 @@ StatusCode TupleToolDira::initialize()
   if ( sc.isFailure() ) return sc;
 
   m_context = Gaudi::Utils::getIDVAlgorithm ( contextSvc(), this ) ;
-  if (0==m_context) return Error("Couldn't get parent DVAlgorithm",
+  if ( !m_context ) return Error("Could not get parent DVAlgorithm",
                                  StatusCode::FAILURE);
 
   return sc;
 }
 
 //=============================================================================
+
 StatusCode TupleToolDira::fill( const Particle* mother
                                 , const Particle* P
                                 , const std::string& head
-                                , Tuples::Tuple& tuple ){
+                                , Tuples::Tuple& tuple )
+{
   Assert( P && mother && m_context
           , "This should not happen, you are inside TupleToolDira.cpp :(" );
 
-  if( isPureNeutralCalo(P) )
-    return Warning("Will not fill geometry tuple for calo neutrals. No worry.", StatusCode::SUCCESS, 10);
-  if ( mother != P || P->isBasicParticle() ) return StatusCode::SUCCESS ;
+  if ( isPureNeutralCalo(P) )
+    return Warning( "Will not fill geometry tuple for calo neutrals. No worry.", 
+                    StatusCode::SUCCESS, 10 );
+  if ( mother != P || P->isBasicParticle() ) return StatusCode::SUCCESS;
 
   const VertexBase* motherPV = m_context->bestVertex ( mother );
-  if(motherPV==NULL) { info()<<"could not find related PV for the Bs"<<endmsg;  return StatusCode::FAILURE; }
-  StatusCode sc = fillDIRAError( motherPV, mother, head, tuple );
-  return sc ;
+  if ( !motherPV ) 
+  { 
+    return Warning( "Could not find related PV" ); 
+  }
+
+  return fillDIRAError( motherPV, mother, head, tuple );
 }
 
 //=========================================================================
@@ -79,30 +85,35 @@ StatusCode TupleToolDira::fill( const Particle* mother
 StatusCode TupleToolDira::fillDIRAError( const VertexBase* primVtx
                                          ,const Particle* P
                                          , const std::string head
-                                         , Tuples::Tuple& tuple ) const {
-  bool test = true ;
+                                         , Tuples::Tuple& tuple ) const 
+{
 
   const VertexBase* evtx = P->endVertex();
 
-  if( !evtx ){
-    Error("Can't retrieve the end vertex for " + head );
-    return StatusCode::FAILURE;
+  if( !evtx )
+  {
+    return Error( "Cannot retrieve the end vertex for " + head );
   }
 
 
-  double diraerr, dira ;
+  double diraerr(0), dira(0);
   calculateDIRAError( P ,primVtx , dira, diraerr );
 
-  test &= tuple->column( head + "_DiraAngleError", diraerr/(sqrt(1-dira*dira))) ;
+  bool test = true;
+  test &= tuple->column( head + "_DiraAngleError", diraerr/(std::sqrt(1-dira*dira))) ;
   test &= tuple->column( head + "_DiraCosError", diraerr );
-  test &= tuple->column( head + "_DiraAngle", acos(dira)) ;
+  test &= tuple->column( head + "_DiraAngle", std::acos(dira)) ;
   test &= tuple->column( head + "_DiraCos", dira );
-  // --------------------------------------------------
-  return StatusCode(test) ;
+
+  return StatusCode(test);
 }
 
 
-void TupleToolDira::calculateDIRAError(const Particle* P,  const VertexBase* primVtx, double & dira, double & diraerr  ) const {
+void TupleToolDira::calculateDIRAError( const Particle* P, 
+                                        const VertexBase* primVtx, 
+                                        double & dira, 
+                                        double & diraerr  ) const
+{
 
 
   const Gaudi::SymMatrix7x7 & covMother = P->covMatrix ();
@@ -110,40 +121,49 @@ void TupleToolDira::calculateDIRAError(const Particle* P,  const VertexBase* pri
   SymMatrix10x10 Cov;
 
   for(int i=0; i<7; ++i)
+  {
     for(int j=0; j<7; ++j)
+    {
       Cov(i,j) = covMother(i,j);
-
+    }
+  }
 
   for(int i=7; i<10; ++i)
+  {
     for(int j=7; j<10; ++j)
+    {
       Cov(i,j) = covPrim(i-7,j-7);
+    }
+  }
 
-  for(int i=7; i<10; ++i){
-    for(int j=0; j<7; ++j) {
+  for(int i=7; i<10; ++i)
+  {
+    for(int j=0; j<7; ++j) 
+    {
       Cov(i,j) = 0.0;
       Cov(j,i) = 0.0;
     }
   }
 
-
-
-  Gaudi::XYZVector vectord = P->endVertex()->position() -  primVtx->position();
-  Gaudi::XYZVector vectorp = P->momentum().Vect() ;
-
+  const Gaudi::XYZVector  vectord = P->endVertex()->position() -  primVtx->position();
+  const Gaudi::XYZVector& vectorp = P->momentum().Vect() ;
+  
   double vecd[3] = { vectord.x(), vectord.y(), vectord.z()};
   double vecp[3] = { vectorp.x(), vectorp.y(), vectorp.z()};
-
-  double magd = sqrt( vectord.Mag2() ) ;
-  double magp = sqrt( vectorp.Mag2() );
+  
+  const double magd = std::sqrt( vectord.Mag2() ) ;
+  const double magp = std::sqrt( vectorp.Mag2() );
 
   ROOT::Math::SMatrix<double,10, 1> gradVec;
   gradVec(6,0) = 0.0;
 
-  for ( int i = 0; i < 3; ++i ){
+  for ( int i = 0; i < 3; ++i )
+  {
     double gp = vecd[i]*magp*magp;
     double gd = vecp[i]*magd*magd;
 
-    for(int j=0; j<3; ++j){
+    for ( int j = 0; j < 3; ++j )
+    {
       gp -= vecd[j]*vecp[j]*vecp[i];
       gd -= vecp[j]*vecd[j]*vecd[i];
     }
@@ -154,8 +174,8 @@ void TupleToolDira::calculateDIRAError(const Particle* P,  const VertexBase* pri
 
   Gaudi::SymMatrix1x1 diraErrSq = ROOT::Math::SimilarityT<double, 10, 1>(gradVec, Cov);
 
-  diraerr = sqrt(diraErrSq(0,0));
-
+  diraerr = std::sqrt(diraErrSq(0,0));
+  
   dira = (vectord.Dot(vectorp))/ (magp*magd);
 
   return;
