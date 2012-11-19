f2cd587efb5446f35682e604256e79c60700b9c2
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2012-11-19 12:39:15 +0000

    remove spaces
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@148097 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 41d89147c44af48cd11b67ecd56562b8f93ee361

--- Phys/Phys/DaVinciKernel/src/Lib/DVAlgorithm.cpp
+++ Phys/Phys/DaVinciKernel/src/Lib/DVAlgorithm.cpp
@@ -10,14 +10,14 @@ StatusCode DVAlgorithm::initialize ()
   if ( sc.isFailure() ) return sc;
   warning() << "======================================================================================"
             << endmsg
-            << " TAKE NOTE - '" << System::typeinfoName(typeid(*this)) << "'"
+            << "TAKE NOTE - '" << System::typeinfoName(typeid(*this)) << "'"
             << " is using the depreciated DVAlgorithm base class."
             << endmsg
             << "Please move to DaVinciAlgorithm, DaVinciHistoAlgorithm or DaVinciTupleAlgorithm."
             << endmsg
             << "Eventually the DVAlgorithm base class will be REMOVED, at which point this class"
             << endmsg
-            << " will NO LONGER COMPILE."
+            << "will NO LONGER COMPILE."
             << endmsg
             << "======================================================================================"
             << endmsg;
