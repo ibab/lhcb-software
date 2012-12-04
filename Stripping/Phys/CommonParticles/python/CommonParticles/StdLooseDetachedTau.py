339cae896fc9a6ebf9cb4296897ee90fdc571a9f
Donal Hill <donal.hill@cern.ch>
2012-12-04 11:03:28 +0000

    Commited changes to common particles StdLooseDetachedTauNonPhys.py and StdLooseDetachedTau.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@149398 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5b6f743892f6fa840aaecebeb4eb0e3c8a92b2b3

--- Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedTau.py
+++ Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedTau.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseDetachedTau.py,v 1.0 2012-08-07 14:44:42 elsasser Exp $ 
+# $Id: StdLooseDetachedTau.py,v 1.0 2012-12-04 14:44:42 elsasser Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseDetachedTau.py
 #  configuration file for 'Standard Loose Detached Tau' 
 #  @author Donal Hill, Conor Fitzpatrick, Christian Elsasser
-#  @date 2012-08-07
+#  @date 2012-12-02
 # =============================================================================
 """
 Configuration file for 'Standard Loose Detached Tau'
@@ -27,20 +27,20 @@ from CommonParticles.Utils import *
 ## create the algorithm - physical tau+ -> pi+ pi- pi+
 StdLooseDetachedTau3pi = CombineParticles ( 'StdLooseDetachedTau3pi' )
 
-StdLooseDetachedTau3pi.Inputs = [ "Phys/StdLooseDetachedDipion/Particles",
-                                  "Phys/StdLoosePions/Particles" ]
+StdLooseDetachedTau3pi.Inputs = [ "Phys/StdLoosePions/Particles" ]
 
 #Build physical tau and tau+++
-StdLooseDetachedTau3pi.DecayDescriptors = ["[tau+ -> rho(770)0 pi+]cc"]
+StdLooseDetachedTau3pi.DecayDescriptors = ["[tau+ -> pi+ pi- pi+]cc"]
 StdLooseDetachedTau3pi.DaughtersCuts = {
-    "rho(770)0"  : "ALL",
-    "pi+"        : "(PT>150.*MeV) & (MIPCHI2DV(PRIMARY) > 4.0) & (TRCHI2DOF<3) & (TRGHOSTPROB<0.4) & (PIDK < 8)"
+    "pi+"        : "(PT>150.*MeV) & (MIPCHI2DV(PRIMARY) > 4.0) & (TRCHI2DOF<3) & (TRGHOSTPROB<0.4) & (PIDK < 8)",
+    "pi-"        : "(PT>150.*MeV) & (MIPCHI2DV(PRIMARY) > 4.0) & (TRCHI2DOF<3) & (TRGHOSTPROB<0.4) & (PIDK < 8)"
+    
     }
 
 #Need an upper mass window of 100 MeV for background estimation
 #and a lower mass window of 200 to catch the D+ and Ds
 
-StdLooseDetachedTau3pi.CombinationCut = "((AM>400.*MeV) & (AM<3500.*MeV)) & (ADOCAMAX('')<0.15*mm)"
+StdLooseDetachedTau3pi.CombinationCut = "((AM>400.*MeV) & (AM<3500.*MeV)) & (ADOCAMAX('')<0.15*mm) & ((AM12<1670.*MeV) or (AM23<1670.*MeV)) & (ANUM(PT < 300*MeV) <= 1) & (1<ANUM( ('pi+'==ABSID) & (MIPCHI2DV(PRIMARY)>5)))"
 StdLooseDetachedTau3pi.MotherCut = "(M>400.*MeV) & (M < 3500.*MeV) & (BPVDIRA>0.99) & (VFASPF(VCHI2) < 25 )"
 
 ## configure Data-On-Demand service 
--- Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedTauNonPhys.py
+++ Stripping/Phys/CommonParticles/python/CommonParticles/StdLooseDetachedTauNonPhys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # =============================================================================
-# $Id: StdLooseDetachedTau.py,v 1.0 2012-08-07 14:44:42 elsasser Exp $ 
+# $Id: StdLooseDetachedTau.py,v 1.0 2012-12-04 14:44:42 elsasser Exp $ 
 # =============================================================================
 ## @file  CommonParticles/StdLooseDetachedTau.py
 #  configuration file for 'Standard Loose Detached Tau' 
@@ -28,20 +28,18 @@ from CommonParticles.Utils import *
 StdLooseDetachedTau3piNonPhys = CombineParticles ( 'StdLooseDetachedTau3piNonPhys' )
 
 
-StdLooseDetachedTau3piNonPhys.Inputs = [ "Phys/StdLooseDetachedDipion/Particles",
-                                  "Phys/StdLoosePions/Particles" ]
+StdLooseDetachedTau3piNonPhys.Inputs = [ "Phys/StdLoosePions/Particles" ]
 
 #Build physical tau and tau+++
-StdLooseDetachedTau3piNonPhys.DecayDescriptors = ["[tau+ -> rho(770)+ pi+]cc"]
+StdLooseDetachedTau3piNonPhys.DecayDescriptors = ["[tau+ -> pi+ pi+ pi+]cc"]
 StdLooseDetachedTau3piNonPhys.DaughtersCuts = {
-    "rho(770)+"  : "ALL",
     "pi+"        : "(PT>150.*MeV) & (MIPCHI2DV(PRIMARY) > 4.0) & (TRCHI2DOF<3) & (TRGHOSTPROB<0.4) & (PIDK < 8)"
     }
 
 #Need an upper mass window of 100 MeV for background estimation
 #and a lower mass window of 200 to catch the D+ and Ds
 
-StdLooseDetachedTau3piNonPhys.CombinationCut = "((AM>400.*MeV) & (AM<3500.*MeV)) & (ADOCAMAX('')<0.15*mm)"
+StdLooseDetachedTau3piNonPhys.CombinationCut = "((AM>400.*MeV) & (AM<3500.*MeV)) & (ADOCAMAX('')<0.15*mm) & ((AM12<1670.*MeV) or (AM23<1670.*MeV)) & (ANUM(PT < 300*MeV) <= 1) & (1<ANUM( ('pi+'==ABSID) & (MIPCHI2DV(PRIMARY)>5)))"
 StdLooseDetachedTau3piNonPhys.MotherCut = "(M>400.*MeV) & (M < 3500.*MeV) & (BPVDIRA>0.99) & (VFASPF(VCHI2) < 25 )"
 
 
