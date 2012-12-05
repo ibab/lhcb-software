7803858ab7c7485d5e13d192bc9a0a755a4087af
amartens <amartens@cern.ch>
2012-12-05 15:49:33 +0000

    fix options for new pdf
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@149500 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0a7d34e749f61e9a638a74632321dd344f656e5e

--- Gauss/Gen/LbPythia8/options/Pythia8_CT09MCS.py
+++ Gauss/Gen/LbPythia8/options/Pythia8_CT09MCS.py
@@ -8,25 +8,25 @@ gen = Generation()
 gen.addTool( MinimumBias , name = "MinimumBias" )
 gen.MinimumBias.ProductionTool = "Pythia8Production"
 gen.MinimumBias.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.MinimumBias.Pythia8Production.Tuning = "LHCbDefault.cmd"
+gen.MinimumBias.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
 
 gen.addTool( Inclusive , name = "Inclusive" )
 gen.Inclusive.ProductionTool = "Pythia8Production"
 gen.Inclusive.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.Inclusive.Pythia8Production.Tuning = "LHCbDefault.cmd"
+gen.Inclusive.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
 
 gen.addTool( SignalPlain , name = "SignalPlain" )
 gen.SignalPlain.ProductionTool = "Pythia8Production"
 gen.SignalPlain.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.SignalPlain.Pythia8Production.Tuning = "LHCbDefault.cmd"
+gen.SignalPlain.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
 
 gen.addTool( SignalRepeatedHadronization , name = "SignalRepeatedHadronization" )
 gen.SignalRepeatedHadronization.ProductionTool = "Pythia8Production"
 gen.SignalRepeatedHadronization.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.SignalRepeatedHadronization.Pythia8Production.Tuning = "LHCbDefault.cmd"
+gen.SignalRepeatedHadronization.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
 gen.SignalRepeatedHadronization.Pythia8Production.Commands += Pythia8TurnOffFragmentation
 
 gen.addTool( Special , name = "Special" )
 gen.Special.ProductionTool = "Pythia8Production"
 gen.Special.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.Special.Pythia8Production.Tuning = "LHCbDefault.cmd"
+gen.Special.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
