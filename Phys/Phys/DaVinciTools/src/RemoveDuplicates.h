bb0d069650be49c64f289745d64162b13368c23e
Christian Urs Elsasser <che@physik.uzh.ch>
2012-12-14 10:20:50 +0000

    Tool to remove duplicates based on the LHCb-ID hash
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@149940 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 609b04959c1e11e118cda961dcdf89efb6ab5587

---
+++ Phys/Phys/DaVinciTools/src/RemoveDuplicates.h
@@ -0,0 +1,54 @@
+// $Id: RemoveDuplicates.h,v 1.1 2012-10-11 11:56:34 elsasser Exp $
+#ifndef REMOVEDUPLICATES_H 
+#define REMOVEDUPLICATES_H 1
+
+// Include files
+// from DaVinci, this is a specialized GaudiAlgorithm
+#include "Kernel/DVAlgorithm.h"
+
+/** @class RemoveDuplicates RemoveDuplicates.h
+ *  
+ *  Filter the local storage to remove all duplicated candidates
+ *
+ *  @author Christian U. Elsasser
+ *  @date   2012-10-11
+ * 
+ * 
+ */
+
+
+//===========================================================================
+///A public DVAlgorithm using the LHCb-ID hash to remove duplicates.
+/** RemoveDuplicates is a useful tool to be called on stripped candidates.
+ * The Tool removes duplicates (built from the same final state particles) 
+ * within stripped candidates based on LHCb-ID hash.
+ * If the candidates have the same hash only one is kept.
+ * Output container for further processing is Phys/<toolname>/Particles.
+ */
+//===========================================================================
+
+class RemoveDuplicates : public DVAlgorithm {
+
+public: 
+  /// Standard constructor
+  RemoveDuplicates( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~RemoveDuplicates( ); ///< Destructor
+
+  virtual StatusCode initialize();    ///< Algorithm initialization
+  virtual StatusCode execute   ();    ///< Algorithm execution
+  virtual StatusCode finalize  ();    ///< Algorithm finalization
+
+protected:
+
+  
+private:
+  // globals
+
+  int m_NpartIn; ///<number of particles so far sent to the alg
+  int m_NpartOut; ///<number of unique particles so far written out by the alg
+  
+  
+
+};
+#endif // REMOVEDUPLICATES_H
