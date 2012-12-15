6703dd4f0c50efe70d695baa32817fcb63ccf413
powell <powell@cern.ch>
2012-12-15 12:54:11 +0000

    Updates to Stripping20r1/LineConfigDictionaries_Calib.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@150016 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0049d868878380cf877ed4c8c3169577d45bd4a4

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1/LineConfigDictionaries_Calib.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1/LineConfigDictionaries_Calib.py
@@ -79,7 +79,7 @@ NoPIDDstarWithD02RSKPi = {
     , 'KK_WrongMass'     : 25 * MeV       ## MeV (3 sigma veto)
     , 'PiPi_WrongMass'   : 25 * MeV       ## MeV (3 sigma veto)
     ##
-    , 'Prescale'         : 0.80           ## unitless
+    , 'Prescale'         : 0.89           ## unitless
     , 'Postscale'        : 1.00           ## unitless
     ##
     , 'Monitor'          : False           ## Activate the monitoring?
@@ -127,11 +127,11 @@ V0ForPID = {
     ## define LL-category of K0S 
     "LL =    CHILDCUT ( ISLONG , 1 ) & CHILDCUT ( ISLONG , 2 ) "
     ] ,
-    'KS0LL_Prescale'           : 0.020 ,
-    'KS0DD_Prescale'           : 0.024 , 
-    'LamLL_Prescale_LoP'       : 0.140 , 
+    'KS0LL_Prescale'           : 0.022 ,
+    'KS0DD_Prescale'           : 0.027 , 
+    'LamLL_Prescale_LoP'       : 0.180 , 
     'LamLL_Prescale_HiP'       : 1.000 ,
-    'LamDD_Prescale'           : 0.100 , 
+    'LamDD_Prescale'           : 0.140 , 
     'LamLLIsMUON_Prescale_LoP' : 1.000 ,
     'LamLLIsMUON_Prescale_HiP' : 1.000 ,
     'LamDDIsMUON_Prescale'     : 1.000   
