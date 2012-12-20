f2c0e219de821077618d1ea2965b0e3dd0a97d20
Anton Poluektov <anton.poluektov@cern.ch>
2012-12-20 20:47:41 +0000

    Update MiniBias
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@150391 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0564f7f95e6285b7223adf5f98841a78b680e92d

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1/LineConfigDictionaries_MiniBias.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1/LineConfigDictionaries_MiniBias.py
@@ -4,17 +4,17 @@ Line config dictionaries for MiniBias.
 
 
 MiniBias = {
-    'BUILDERTYPE'	:	'MiniBiasConf',
+    'BUILDERTYPE' : 'MiniBiasConf',
     'CONFIG'   :{ "NoBiasLine_RE"       : "(HLT_PASS('Hlt1MBNoBiasDecision'))",
-                  "NoBiasLine_Rate" : 1,
-                  "NoBiasLine_Limiter" : "Hlt1MBNoBiasODINFilter",
-                  "L0AnyRLLine_RE"        : "(HLT_PASS('Hlt1L0AnyRateLimitedDecision'))",
-                  "L0AnyRLLine_Rate"  : 1,
+                  "NoBiasLine_Rate"     : 1,
+                  "NoBiasLine_Limiter"  : "Hlt1MBNoBiasODINFilter",
+                  "NoBiasLine_CondDB"   : False,
+                  "L0AnyRLLine_RE"      : "(HLT_PASS('Hlt1L0AnyRateLimitedDecision'))",
+                  "L0AnyRLLine_Rate"    : 1,
                   "L0AnyRLLine_Limiter" : "Hlt1L0AnyRateLimitedPostScaler",
                   "L0AnyPSLine_RE"      : "(HLT_PASS_RE('Hlt1L0Any.*Decision'))",
-                  "L0AnyPSLine_PS"  : 0.1,
-                  }, 
+                  "L0AnyPSLine_PS"      : 0.01, # changed too
+                }, 
     'WGs' : [ 'ALL' ],
     'STREAMS' : [ 'MiniBias' ]
     }
-
