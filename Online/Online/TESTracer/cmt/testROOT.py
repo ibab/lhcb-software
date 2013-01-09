f05f68d244c35263dad4abb43a934895b0baab86
Markus Frank <frankb@cern.ch>
2013-01-09 13:37:33 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@150623 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 600013d4634a70486aac28dd1b3a3738c510a251

--- Online/Online/TESTracer/cmt/testROOT.py
+++ Online/Online/TESTracer/cmt/testROOT.py
@@ -1,13 +1,16 @@
 from Gaudi.Configuration import *
 
 #importOptions("$APPCONFIGOPTS/Brunel/DataType-2010.py")
-importOptions("$APPCONFIGOPTS/Brunel/DataType-2011.py")
+#importOptions("$APPCONFIGOPTS/Brunel/DataType-2011.py")
+importOptions("$APPCONFIGOPTS/Brunel/DataType-2012.py")
 
 from GaudiConf import IOHelper;
 ##IOHelper('MDF').inputFiles(['/afs/cern.ch/user/r/rlambert/public/forMarkus/run_69669_large_2ev.mdf']);
 ##IOHelper('MDF').inputFiles(['castor://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11/102907/102907_0000000002.raw']);
+##IOHelper('MDF').inputFiles(['castor://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11/97114/097114_0000000004.raw?svcClass=lhcbtape'])
+##IOHelper('MDF').inputFiles(['/castorfs/cern.ch/grid/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11/97114/097114_0000000004.raw'])
 
-IOHelper('MDF').inputFiles(['castor://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11/97114/097114_0000000004.raw?svcClass=lhcbtape'])
+IOHelper('MDF').inputFiles(['/castorfs/cern.ch/grid/lhcb/data/2012/RAW/FULL/LHCb/COLLISION12/133586/133586_0000000309.raw'])
 
 from Configurables import Brunel, AuditorSvc, OutputStream;
 ApplicationMgr().ExtSvc.insert(0,AuditorSvc())
@@ -17,7 +20,7 @@ Brunel().Persistency='ROOT';
 Brunel().OutputLevel = 4;
 MessageSvc().OutputLevel = 4;
 AuditorSvc().Auditors.append("TES::TraceAuditor")
-OutputStream('DstWriter').Output = "DATAFILE='PFN:someFile.sdst' TYP='ROOT' OPT='REC'"
+OutputStream('DstWriter').Output = "DATAFILE='PFN:someFile.dst' TYP='ROOT' OPT='REC'"
 
 def fixstuff():
     for stream in IOHelper().activeStreams():
