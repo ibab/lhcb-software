138c8b014a9d8643cb6728c6901490411e375c98
Andrey Ustyuzhanin <andrey.ustyuzhanin@cern.ch>
2013-01-29 14:41:57 +0000

    Changes by Antonio
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@151250 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ca45eee123c59554c80b81b638e8874a8c9e75fd

--- LHCb/Tools/EventIndexer/python/EventIndexer/indexer.py
+++ LHCb/Tools/EventIndexer/python/EventIndexer/indexer.py
@@ -1,12 +1,14 @@
+import sys
+del sys.modules["GaudiPython"]
 from GaudiPython.Bindings import AppMgr
 from GaudiConf             import IOHelper
-from Bender.Main import appMgr, get, SUCCESS 
+from Bender.Main import appMgr, get, SUCCESS, FAILURE, Algo
 from array import array
 import ROOT
     
 
 # some global variables
-last_open_lfn = "LFN:/lhcb/LHCb/Collision12/BHADRONCOMPLETEEVENT.DST/00020241/0000/00020241_00000015_1.bhadroncompleteevent.dst"
+last_open_lfn = ""
 fh = None
 tree = None
 data_fields = {
@@ -18,27 +20,28 @@ data_fields = {
 }
 # end of global variables
 
+evtCounter = -1
+class FillIndexer(Algo):
+    def analyse(self):
+        global evtCounter,last_open_lfn
+        evtCounter += 1
+
+        rc = get('/Event/Rec/Header')
+        if not rc :
+            return FAILURE
+        indexer_put(tree, {
+            'lfn/C': last_open_lfn,
+            'position/I': evtCounter,
+            'eventNumber/I': rc.evtNumber(),
+            'runNumber/I': rc.runNumber()
+        })
+
+        return SUCCESS
+
 # should be called from 'new file open' incident handler
 def update_last_lfn(lfn):
     global last_open_lfn
     last_open_lfn = lfn
-
-# basic routine
-def run ( numevents ) :
-    gaudi = appMgr()
-    for i in range ( 0 , numevents ) :
-        gaudi.run(1) 
-        rc = get('/Event/Rec/Header')
-        if not rc :
-            print 'RC !!!! '
-            break
-        indexer_put(tree, {
-            'lfn/C': last_open_lfn,
-            'position/I': i,
-            'eventNumber/I': rc.evtNumber(),
-            'runNumber/I': rc.runNumber()
-        })
-    indexer_finish()
 
 def indexer_prepare(stripping, outputfilename):
     global fh, tree
@@ -71,8 +74,14 @@ def indexer_put(tree, data):
             data_fields[k][0][0] = v
     tree.Fill()
 
+def fill_input(self, filename=None, GUID=None, status=None, addevents=0):
+    global last_open_lfn 
+    last_open_lfn   = filename
+     return self._fill_input(self, filename, GUID, status, addevents)
+
     
 def configure( datafiles         ,
+               XMLSummaryFile    ,
                catalogs  = []    ,
                castor    = False ,
                params    = {}    ) :
@@ -80,7 +89,7 @@ def configure( datafiles         ,
     
     from Configurables         import LHCbApp
     
-    LHCbApp()
+    LHCbApp(XMLSummary = XMLSummaryFile)
     
     from Bender.Main import setData, appMgr, SUCCESS 
     stripping = params['stripping'] if 'stripping' in params else 'strNone'
@@ -88,48 +97,66 @@ def configure( datafiles         ,
 
     setData ( datafiles , catalogs , castor )
 
+    gaudi = appMgr()
+    alg = FillIndexer('FillIndexer')
+    gaudi.setAlgorithms([alg])
+    indexer_prepare(stripping, outputfilename)
+    return SUCCESS 
+
+
+def main():
+
+    '''   
+    from Gaudi.Configuration import * 
+    if os.path.exists('prodConf.py'):
+        from prodconf import ProdConf
+        importOptions('prodConf.py')
+        datafiles = Prodconf().InputFiles
+        allConfigurables.clear()
+    else:
+        pass
+    '''
+   
+    import os
+    for file in os.listdir("."):
+       if (file.startswith("prodConf") and file.endswith(".py")):
+           prodConf_file = file
+
+
+ 
+    import ProdConf
+    data = {}
+    ProdConf.ProdConf = lambda **kwargs: data.update(kwargs)
+    from Gaudi.Configuration import importOptions
+    importOptions(prodConf_file)
+    
+    
+    datafiles = data['InputFiles']
+    
+    XMLSummaryFile = data['XMLSummaryFile']
+
+    s = datafiles[0]
+    ss = s.split(".")
+    stripping = ss[len(ss)-2]  
+    
+    catalogs = 'xmlcatalog_file:' + data['XMLFileCatalog']
+
+    outputFileName = data['OutputFilePrefix']+"."+data['OutputFileTypes'][0] 
+  
+    import XMLSummaryBase.summary
+    
+    XMLSummaryBase.summary.Summary._fill_input = XMLSummaryBase.summary.Summary.fill_input
+    XMLSummaryBase.summary.Summary.fill_input = fill_input
+
+    configure ( datafiles , XMLSummaryFile , catalogs , True, {
+        'outputfilename':outputFileName,
+        'stripping'     :stripping,
+    }) 
     
     gaudi = appMgr()
-    gaudi.setAlgorithms([])
-    indexer_prepare(stripping, outputfilename)
-    
-    return SUCCESS 
-##
+    gaudi.run(-1)
+    indexer_finish()
+    sys.exit(0)
 
-
-if '__main__' == __name__ :
-    
-    from Gaudi.Configuration import * 
-    datafiles = [
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000007_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000020_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000033_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000046_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000059_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000069_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000076_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000090_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000097_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000105_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000118_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000120_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000122_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000124_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000126_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000132_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000138_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000143_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000144_1.dimuon.dst',
-        '/lhcb/LHCb/Collision12/DIMUON.DST/00020241/0000/00020241_00000148_1.dimuon.dst'
-        ]
-    
-    catalogs = [ 'xmlcatalog_file:qqq.xml' ]
-    
-    
-    configure ( datafiles , catalogs , True, {
-        'outputfilename':'i.root',
-        'stripping'     :'DIMUON',
-    }) 
-    run(10)
-    
-    
+main()
+        
