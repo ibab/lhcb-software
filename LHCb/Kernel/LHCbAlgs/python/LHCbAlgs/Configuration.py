745d9a5ba729610c0f61aeb52e5cd9ce750a20dc
szczypka <szczypka@cern.ch>
2013-02-04 20:01:48 +0000

    Added derivation of DB dataTypes in Configuration.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@151623 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3777529d30b515026bd5c7b83224fb86a5c6dc4

--- LHCb/Kernel/LHCbAlgs/python/LHCbAlgs/Configuration.py
+++ LHCb/Kernel/LHCbAlgs/python/LHCbAlgs/Configuration.py
@@ -15,6 +15,8 @@ class LHCbApp(LHCbConfigurableUser):
         "EvtMax"        : -1
        ,"SkipEvents"    : 0
        ,"DataType"      : "2012"
+       ,"Detectors"     : []
+       ,"LocalDataTypes": []
        ,"DDDBtag"       : ""
        ,"CondDBtag"     : ""
        ,"DQFLAGStag"    : ""
@@ -31,6 +33,8 @@ class LHCbApp(LHCbConfigurableUser):
         'EvtMax'      : """ Maximum number of events to process """
        ,'SkipEvents'  : """ Number of events to skip """
        ,'DataType'    : """ Data type, see DDDBConf for allowed values. Default '2009' """
+       ,"Detectors"   : """ Maximal list of detector components used in the simulation """
+       ,"LocalDataTypes" : """ Set of Local DataTypes uses to set DB """
        ,'DDDBtag'     : """ Tag for DDDB. Default as set in DDDBConf for DataType """
        ,'CondDBtag'   : """ Tag for CondDB. Default as set in DDDBConf for DataType """
        ,"DQFLAGStag"  : """ Tag for DQFLAGS. Default as set in DDDBConf for DataType """
@@ -44,13 +48,173 @@ class LHCbApp(LHCbConfigurableUser):
        }
     
     __used_configurables__ = [ DDDBConf, XMLSummary ]
+
+    __dtMapping = {
+        "velo"     : "Velo",
+        "muon"     : "Muon",
+        "muonnom1" : "MuonNoM1",
+        "tt"       : "TT",
+        "ut"       : "UT",
+        "ft"       : "FT",
+        "vp"       : "VP",
+        "vl"       : "VL",
+        "rich1"    : "Rich1",
+        "rich2"    : "Rich2",
+        "rich1pmt" : "Rich1Pmt",
+        "rich2pmt" : "Rich2Pmt",
+        "rich1horiz" : "Rich1Horiz"
+        }
+
+
+    # Following the xml structure
+    __DTRegion = {
+        "UpstreamRegion"     : {},
+        "BeforeMagnetRegion" : {
+            "veloRegion"   : [],
+            "ttRegion"     : [],
+            "rich1Region"  : []
+            },
+        "MagnetRegion"       : {
+            "magnetRegion" : []
+            },
+        "AfterMagnetRegion"  : {
+            "itRegion"     : [],
+            "otRegion"     : [],
+            "ftRegion"     : [],
+            "rich2Region"  : []
+            },
+        "DownstreamRegion"   : {
+            "muonRegion"   : []
+            }
+        }
+
+    # Following the xml structure
+    __sectionOrder = {
+        "UpstreamRegion" : {
+            "regions" : [],
+            "ignore" : []
+            },
+        "BeforeMagnetRegion" : {
+            "regions" : ['veloRegion', 'rich1Region', 'ttRegion'],
+            "ignore" : ['rich1', 'rich1pmt']
+            },
+        "MagnetRegion" : {
+            "regions" : ['magnetRegion'],
+            "ignore" : ['magnet']
+            },
+        "AfterMagnetRegion" : {
+            "regions" : ['itRegion', 'otRegion', 'ftRegion', 'rich2Region'],
+            "ignore" : ['rich2', 'rich2pmt']
+            },
+        "DownstreamRegion" : {
+            "regions" : ['muonRegion'],
+            "ignore" : ['muon']
+            }
+        }
     
+    __possDTRegion = {
+        "UpstreamRegion"     : {},
+        "BeforeMagnetRegion" : {
+            "veloRegion"  : ["velo", "vl", "vp"],
+            "rich1Region" : ["rich1", "rich1pmt", "rich", "rich1horiz"],
+            "ttRegion"    : ["tt", "ut"]
+            },
+        "MagnetRegion"       : {
+            "magnetRegion" : ["magnet"]
+            },
+        "AfterMagnetRegion"  : {
+            "itRegion"    : ["it", "ot", "ft"],
+            "rich2Region" : ["rich2", "rich2pmt", "rich", "superrich"]
+            },
+        "DownstreamRegion"   : {
+            "muonRegion" : ["muon", "muonnom1"]
+            }
+        }
+
+
+    def createDetList(self):
+        detList = []
+        if hasattr(self, "Detectors"):
+            import string
+            for det in self.getProp("Detectors"):
+                detList.append( det.lower() )
+        return detList
+
     def knownMonitors(self):
         return ["SC", "FPE"]
     
     def knownAuditors(self):
         return ["NameAuditor","MemoryAuditor","ChronoAuditor"]
     
+    def checkIncompatibleDetectors(self):
+
+        myDetList = self.createDetList();
+    
+        _veloRegion   = [ det for det in myDetList if det in self.__possDTRegion['BeforeMagnetRegion']['veloRegion']]
+        _muonRegion   = [ det for det in myDetList if det in self.__possDTRegion['DownstreamRegion']['muonRegion']]
+        _track1Region = [ det for det in myDetList if det in self.__possDTRegion['BeforeMagnetRegion']['ttRegion']]
+        _track2Region = [ det for det in myDetList if det in self.__possDTRegion['AfterMagnetRegion']['itRegion']]
+        _rich1Region  = [ det for det in myDetList if det in self.__possDTRegion['BeforeMagnetRegion']['rich1Region']]
+        _rich2Region  = [ det for det in myDetList if det in self.__possDTRegion['AfterMagnetRegion']['rich2Region']]
+
+        # check for only one velo
+        if len(_veloRegion) > 1:
+            raise RuntimeError("FATAL: Incompatible Velo detectors defined in LHCbApp().Detectors! '%s'" %(_veloRegion))
+        elif _veloRegion:
+            if (
+                ("vp" in _veloRegion) or ("vl" in _veloRegion)
+                ) and ("puveto" in myDetList):
+                raise RuntimeError("FATAL: Incompatible Velo detectors defined in LHCbApp().Detectors! '%s' and '%s'" %(_veloRegion, "PuVeto"))
+            self.__DTRegion["BeforeMagnetRegion"]["veloRegion"] += _veloRegion
+        else:
+            pass
+            
+        # check for detectors in muon region
+        if len(_muonRegion) > 1:
+            raise RuntimeError("FATAL: Incompatible Muon detectors defined in LHCbApp().Detectors! '%s'" %(_muonRegion))
+        elif _muonRegion:
+            self.__DTRegion["DownstreamRegion"]["muonRegion"] += _muonRegion
+        else:
+            pass
+
+        # check for detectors in tt/ut region
+        if len(_track1Region) > 1:
+            raise RuntimeError("FATAL: Incompatible tracking detectors defined in LHCbApp().Detectors! '%s'" %(_track1Region))
+        elif _track1Region:
+            self.__DTRegion["BeforeMagnetRegion"]["ttRegion"] = _track1Region
+        else:
+            pass
+
+        # check for detectors in it/ot/ft region
+        if len(_track2Region) > 2:
+            raise RuntimeError("FATAL: Incompatible tracking detectors defined in LHCbApp().Detectors! '%s'" %(_track2Region))
+        elif len(_track2Region) > 1:
+            if "ft" in _track2Region:
+                raise RuntimeError("FATAL: Incompatible tracking detectors defined in LHCbApp().Detectors! '%s'" %(_track2Region))
+        elif _track2Region:
+            self.__DTRegion["AfterMagnetRegion"]["itRegion"] = _track2Region
+        else:
+            pass
+
+        # check for detectors in rich1 region
+        if len(_rich1Region) > 1:
+            raise RuntimeError("FATAL: Incompatible tracking detectors defined in LHCbApp().Detectors! '%s'" %(_rich1Region))
+        elif _rich1Region:
+            self.__DTRegion["BeforeMagnetRegion"]["rich1Region"] = _rich1Region
+        # No dataTypes needed
+        else:
+            pass
+
+        # check for detectors in rich2 region
+        if len(_rich2Region) > 1:
+            raise RuntimeError("FATAL: Incompatible tracking detectors defined in LHCbApp().Detectors! '%s'" %(_rich2Region))
+        elif _rich2Region:
+            self.__DTRegion["AfterMagnetRegion"]["rich2Region"] = _rich2Region
+        else:
+            pass
+
+
+
     def defineDB(self):
         # Delegate handling of properties to DDDBConf
         self.setOtherProps( DDDBConf(), ["Simulation", "DataType" ] )
@@ -63,6 +227,31 @@ class LHCbApp(LHCbConfigurableUser):
             CondDB().Tags [ "SIMCOND"  ] = self.getProp("CondDBtag")
         if hasattr( self, "DQFLAGStag" ):
             CondDB().Tags [ "DQFLAGS" ] = self.getProp("DQFLAGStag")
+
+
+    def defineDBDataTypes(self):
+        self.checkIncompatibleDetectors()
+        myDataTypes = []
+        for section in self.__DTRegion:
+            myTag = ""
+            joinText = "" 
+            for step in self.__sectionOrder[section]["regions"]:
+                for det in  self.__DTRegion[section][step]:
+                    if det not in self.__sectionOrder[section]["ignore"]:
+                        if not joinText:
+                            myTag += "%s" %(self.__dtMapping[det])
+                            joinText = "+"
+                        else:
+                            myTag = "%s%s%s" %(myTag, joinText, self.__dtMapping[det])
+            if myTag:
+                myDataTypes.append(myTag)
+
+        if myDataTypes:
+            from Configurables import CondDB
+            if hasattr(CondDB(), "AllLocalTagsByDataType"):
+                if not CondDB().AllLocalTagsByDataType:
+                    CondDB().AllLocalTagsByDataType = myDataTypes
+        return
     
     def defineEvents(self):
         SkipEvents = self.getProp("SkipEvents")
@@ -145,6 +334,7 @@ class LHCbApp(LHCbConfigurableUser):
     
     def __apply_configuration__(self):
         self.defineDB()
+        self.defineDBDataTypes()
         self.defineEvents()
         self.defineMonitors()
         self.defineXMLSum()
