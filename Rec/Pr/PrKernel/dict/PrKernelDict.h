201133f7151437da76861556aca384dbdb2d725d
Sascha Stahl <sascha.stahl@cern.ch>
2013-02-06 11:09:12 +0000

    Added dictionary files.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@151735 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c654724e8d6e73b6825e4591eda52975a459e2f0

---
+++ Rec/Pr/PrKernel/dict/PrKernelDict.h
@@ -0,0 +1,9 @@
+#ifndef DICT_PRKERNELDICT_H 
+#define DICT_PRKERNELDICT_H 1
+
+#include "PrKernel/IPrDebugTool.h"
+#include "PrKernel/IPrDebugUTTool.h"
+#include "PrKernel/IPrUTCounter.h"
+
+#endif // DICT_PRKERNELDICT_H
+
