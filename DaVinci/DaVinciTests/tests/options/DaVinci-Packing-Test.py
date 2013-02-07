4f8fb482117a5ddf5e4feb1320a7a4d6fcfe102b
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-02-07 16:14:27 +0000

    Add a new packing test
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@151804 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8c0c7a49652877bcbb490315edcef35b33491471

---
+++ DaVinci/DaVinciTests/tests/options/DaVinci-Packing-Test.py
@@ -0,0 +1,59 @@
+
+########################################################################
+from Gaudi.Configuration import *
+from Configurables import GaudiSequencer
+from Configurables import DaVinci, PhysConf
+########################################################################
+
+DaVinci().EvtMax    = 100                   # Number of events
+DaVinci().PrintFreq = 10                    # Events to skip
+DaVinci().DataType  = "2012"                # Must be given
+DaVinci().Simulation    = False
+
+########################################################################
+
+from Configurables import DataObjectVersionFilter
+DaVinci().UserAlgorithms += [
+    DataObjectVersionFilter( "CheckTracks",
+                             DataObjectLocation = "/Event/Bhadron/Rec/Track/Best",
+                             MaxVersion = 999 ),
+    DataObjectVersionFilter( "CheckDecReports",
+                             DataObjectLocation = "/Event/Strip/Phys/DecReports",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckVeloClusters",
+                             DataObjectLocation = "/Event/Bhadron/Raw/Velo/Clusters",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckCaloElectrons",
+                             DataObjectLocation = "/Event/Bhadron/Rec/Calo/Electrons",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckCaloPhotons",
+                             DataObjectLocation = "/Event/Bhadron/Rec/Calo/Photons",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckLiteTTClusters",
+                             DataObjectLocation = "/Event/Bhadron/Raw/TT/LiteClusters",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckLiteITClusters",
+                             DataObjectLocation = "/Event/Bhadron/Raw/IT/LiteClusters",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckLiteVeloClusters",
+                             DataObjectLocation = "/Event/Bhadron/Raw/Velo/LiteClusters",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckBhadronKaons",
+                             DataObjectLocation = "/Event/Bhadron/Phys/StdAllNoPIDsKaons/Particles",
+                             MaxVersion = 999,
+                             OutputLevel = 3 ),
+    DataObjectVersionFilter( "CheckPVs",
+                             DataObjectLocation = "/Event/Bhadron/Rec/Vertex/Primary",
+                             MaxVersion = 999,
+                             OutputLevel = 3 )
+    ]
+
+from GaudiConf import IOHelper
+IOHelper().inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/LHCb/Collision12/BHADRON.MDST/00021211/0000/00021211_00001955_1.bhadron.mdst'])
