9532c35492f437726af5168f421640c8de91537b
Gloria Corti <gloria.corti@cern.ch>
2013-02-07 20:37:17 +0000

    remove dependency from upgrade
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@151843 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c0f4b6b4046ac78354c57f56451fcfbdfdad0f70

--- Gauss/Sim/Gauss/options/CaloOnly.py
+++ Gauss/Sim/Gauss/options/CaloOnly.py
@@ -7,15 +7,10 @@
 
 
 from Gaudi.Configuration import *
-
-
 from Configurables import CondDB
-
-CondDB().Upgrade     = True
 
 
 from Configurables import Gauss
-
 Gauss().DetectorGeo  = { "Detectors": ['Spd', 'Prs', 'Ecal', 'Hcal', 'Magnet' ] }
 Gauss().DetectorSim  = { "Detectors": ['Spd', 'Prs', 'Ecal', 'Hcal', 'Magnet' ] }
 Gauss().DetectorMoni = { "Detectors": ['Spd', 'Prs', 'Ecal', 'Hcal', 'Magnet' ] }
