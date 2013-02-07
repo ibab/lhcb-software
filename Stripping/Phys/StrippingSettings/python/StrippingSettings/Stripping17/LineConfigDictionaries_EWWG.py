1234d7894a87bc5152aed19cf0b6361eb204e326
Anton Poluektov <anton.poluektov@cern.ch>
2013-02-07 23:38:09 +0000

    Remove non-ascii chars
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@151861 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 054ffabaf122fd1b58d840c719bde9def16ed920

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping17/LineConfigDictionaries_EWWG.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping17/LineConfigDictionaries_EWWG.py
@@ -108,10 +108,10 @@ Z02TauTauProng = {
     'Z2TauTau_Prong_LinePrescale'  : 1.0,
     'Z2TauTau_Prong_LinePostscale' : 1.0,
     ###
-    'TAU_MASS_LOW'                 : '400.0',    # MeV/c²
-    'TAU_MASS_HIGH'                : '1777.0',   # MeV/c²
+    'TAU_MASS_LOW'                 : '400.0',    # MeV/c2
+    'TAU_MASS_HIGH'                : '1777.0',   # MeV/c2
     ###
-    'Z_MASS_LOW'                   : '10000.0',  # MeV/c²
+    'Z_MASS_LOW'                   : '10000.0',  # MeV/c2
     #'Z_MASS_HIGH'                 : not set
     'VCHI2_Z_MAX'                  : '100',       # dl
     ###
