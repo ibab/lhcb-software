8177a6e5c140b52a384bb84af7b841d7f88f69dd
Wouter Hulsbergen <wouterh@nikhef.nl>
2013-02-08 10:41:09 +0000

    added loose PV selection for use when aligning with beamgas
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@151865 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1067109f8f8aa1f070ae445e4ea534cc9bda1be6

--- Alignment/Alignment/TAlignment/python/TAlignment/VertexSelections.py
+++ Alignment/Alignment/TAlignment/python/TAlignment/VertexSelections.py
@@ -29,6 +29,27 @@ def configuredPVSelection():
                           Algorithm = seq)
     return sel
 
+def configuredLoosePVSelection():
+    # we assume that the PV reconstruction has already been performed.
+    # (can always add later)
+    inputlocation = "Rec/Vertex/Primary"
+    outputlocation = "Rec/Vertex/AlignPrimaryVertices"
+    from Configurables import TrackPVRefitter,VertexListRefiner,GaudiSequencer
+    pvrefitter = TrackPVRefitter("AlignPVRefitter",
+                                 PVContainer =  "Rec/Vertex/Primary")
+    pvselector = VertexListRefiner("AlignPVSelector",
+                                   InputLocation = "Rec/Vertex/Primary",
+                                   OutputLocation = "Rec/Vertex/AlignPrimaryVertices",
+                                   MaxChi2PerDoF = 5,
+                                   MinNumTracks = 4,
+                                   MinNumLongTracks = 0 )
+    seq = GaudiSequencer("AlignPVSelSeq")
+    seq.Members += [ pvrefitter, pvselector ]
+    sel = VertexSelection(Name = "DefaultPVSelection",
+                          Location = "Rec/Vertex/AlignPrimaryVertices",
+                          Algorithm = seq)
+    return sel
+
 def configuredPVSelectionFromDST():
     print "I still need to implement this. "\
         "Use the PV Refitter. It can also refit the tracks"
