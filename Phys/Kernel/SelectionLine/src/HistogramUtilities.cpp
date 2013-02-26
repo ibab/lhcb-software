60ba3c745e95b1701c5fcda85833d5957da72518
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-02-26 10:31:36 +0000

    fix -pedandic warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@152876 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 341d9d511194351ea81b279f7e504931a3e80f9b

--- Phys/Kernel/SelectionLine/src/HistogramUtilities.cpp
+++ Phys/Kernel/SelectionLine/src/HistogramUtilities.cpp
@@ -7,44 +7,44 @@
 #include "TH2D.h"
 #include "TProfile.h"
 
-namespace { 
-    template <typename R, typename A> 
-    bool setAxisLabels_( A* aida, const std::string& xAxis, const std::string& yAxis ) {
-        if (aida==0) return false;
-        R *root = Gaudi::Utils::Aida2ROOT::aida2root( aida );  
-        if (root==0) return false;
-        root->SetXTitle(xAxis.c_str());
-        root->SetYTitle(yAxis.c_str());
-        return true;
-    }
+namespace {
+  template <typename R, typename A>
+  bool setAxisLabels_( A* aida, const std::string& xAxis, const std::string& yAxis ) {
+    if (aida==0) return false;
+    R *root = Gaudi::Utils::Aida2ROOT::aida2root( aida );
+    if (root==0) return false;
+    root->SetXTitle(xAxis.c_str());
+    root->SetYTitle(yAxis.c_str());
+    return true;
+  }
 
-    bool setBinLabels_( TAxis* axis,  const std::vector<std::pair<unsigned,std::string> >& labels ) {
-      if (axis==0) return false;
-      unsigned nbins = axis->GetNbins(); 
-      for (std::vector<std::pair<unsigned,std::string> >::const_iterator i = labels.begin();i!=labels.end();++i ) {
-        //TODO: check bin exists... 
-        if (1+i->first <= 0 ||  1+i->first > nbins ) return false;
-        // Argh... ROOT bins start counting at '1' instead of '0'
-        axis -> SetBinLabel(1 + i->first  ,i->second.c_str() );
-      }
-      return true;
+  bool setBinLabels_( TAxis* axis,  const std::vector<std::pair<unsigned,std::string> >& labels ) {
+    if (axis==0) return false;
+    unsigned nbins = axis->GetNbins();
+    for (std::vector<std::pair<unsigned,std::string> >::const_iterator i = labels.begin();i!=labels.end();++i ) {
+      //TODO: check bin exists...
+      if (1+i->first <= 0 ||  1+i->first > nbins ) return false;
+      // Argh... ROOT bins start counting at '1' instead of '0'
+      axis -> SetBinLabel(1 + i->first  ,i->second.c_str() );
     }
+    return true;
+  }
 
-    template <typename R, typename A>
-    bool setBinLabels_( A* aida, const std::vector<std::pair<unsigned,std::string> >& labels ) {
-        if (aida==0) return false;
-        R *root = Gaudi::Utils::Aida2ROOT::aida2root( aida );  
-        if (root==0) return false;
-        return setBinLabels_( root->GetXaxis(), labels );
-    }
+  template <typename R, typename A>
+  bool setBinLabels_( A* aida, const std::vector<std::pair<unsigned,std::string> >& labels ) {
+    if (aida==0) return false;
+    R *root = Gaudi::Utils::Aida2ROOT::aida2root( aida );
+    if (root==0) return false;
+    return setBinLabels_( root->GetXaxis(), labels );
+  }
 
-    template <typename Histogram>
-    bool setBinLabels_( Histogram* hist, const std::vector<std::string>& labels ) {
-        std::vector<std::pair<unsigned,std::string> > l;
-        for (unsigned i = 0;i<labels.size();++i) l.push_back(std::make_pair( i , labels[i] ) );
-        return HistogramUtilities::setBinLabels(hist,l);
-    }
-};
+  template <typename Histogram>
+  bool setBinLabels_( Histogram* hist, const std::vector<std::string>& labels ) {
+    std::vector<std::pair<unsigned,std::string> > l;
+    for (unsigned i = 0;i<labels.size();++i) l.push_back(std::make_pair( i , labels[i] ) );
+    return HistogramUtilities::setBinLabels(hist,l);
+  }
+}
 
 namespace HistogramUtilities {
 
@@ -61,8 +61,8 @@ namespace HistogramUtilities {
   bool setBinLabels( AIDA::IProfile1D* hist, const std::vector<std::string>& labels ) {
     return setBinLabels_(hist,labels);
   }
-  
-  
+
+
   bool setBinLabels( AIDA::IHistogram2D* hist, const std::vector<std::string>& xlabels,
                      const std::vector<std::string>& ylabels) {
     if (hist==0) return false;
@@ -74,11 +74,11 @@ namespace HistogramUtilities {
     for (unsigned i = 0;i<ylabels.size();++i) {
       ly.push_back(std::make_pair( i , ylabels[i] ) );
     }
-    TH2D *h2d = Gaudi::Utils::Aida2ROOT::aida2root( hist );  
+    TH2D *h2d = Gaudi::Utils::Aida2ROOT::aida2root( hist );
     if (h2d==0) return false;
     return setBinLabels_(  h2d->GetXaxis(), lx) && setBinLabels_( h2d->GetYaxis(), ly );
   }
-  
+
   bool setAxisLabels( AIDA::IHistogram1D* hist, const std::string & xAxis,const std::string & yAxis  ) {
     return setAxisLabels_<TH1D>( hist, xAxis, yAxis );
   }
@@ -86,16 +86,14 @@ namespace HistogramUtilities {
   bool setAxisLabels( AIDA::IProfile1D* hist, const std::string & xAxis,const std::string & yAxis  ) {
     return setAxisLabels_<TProfile>( hist, xAxis, yAxis );
   }
-  
-  
 
 
 #if 0
   bool shiftBinsLeft( AIDA::IProfile1D* profile ) {
     if (profile==0) return false;
-    TProfile *prof = Gaudi::Utils::Aida2ROOT::aida2root( profile );  
+    TProfile *prof = Gaudi::Utils::Aida2ROOT::aida2root( profile );
     if (prof==0) return false;
   }
 #endif
-  
-};
+
+}
--- Phys/Kernel/SelectionLine/src/HistogramUtilities.h
+++ Phys/Kernel/SelectionLine/src/HistogramUtilities.h
@@ -1,25 +1,36 @@
 #include <vector>
 #include <string>
 #include <utility>
-namespace AIDA { 
+
+namespace AIDA 
+{
   class IHistogram1D;
   class IHistogram2D;
   class IProfile1D;
-};
+}
 
-namespace HistogramUtilities {
+namespace HistogramUtilities 
+{
 
+  bool setBinLabels( AIDA::IHistogram1D* hist,
+                     const std::vector<std::string>& labels ) ;
+  bool setBinLabels( AIDA::IProfile1D*   hist, 
+                     const std::vector<std::string>& labels ) ;
 
-  bool setBinLabels( AIDA::IHistogram1D* hist, const std::vector<std::string>& labels ) ;
-  bool setBinLabels( AIDA::IProfile1D*   hist, const std::vector<std::string>& labels ) ;
+  bool setBinLabels( AIDA::IHistogram1D* hist, 
+                     const std::vector<std::pair<unsigned,std::string> >& labels ) ;
+  bool setBinLabels( AIDA::IProfile1D*   hist,
+                     const std::vector<std::pair<unsigned,std::string> >& labels ) ;
 
-  bool setBinLabels( AIDA::IHistogram1D* hist, const std::vector<std::pair<unsigned,std::string> >& labels ) ;
-  bool setBinLabels( AIDA::IProfile1D*   hist, const std::vector<std::pair<unsigned,std::string> >& labels ) ;
+  bool setBinLabels( AIDA::IHistogram2D* hist, 
+                     const std::vector<std::string>& xlabels,
+                     const std::vector<std::string>& ylabels) ;
 
-  bool setBinLabels( AIDA::IHistogram2D* hist, const std::vector<std::string>& xlabels,
-                                               const std::vector<std::string>& ylabels) ;
-  
-  bool setAxisLabels( AIDA::IHistogram1D* hist, const std::string & xAxis,const std::string & yAxis  ) ;
-  bool setAxisLabels( AIDA::IProfile1D* prof, const std::string & xAxis,const std::string & yAxis  ) ;
-  
-};
+  bool setAxisLabels( AIDA::IHistogram1D* hist, 
+                      const std::string & xAxis,
+                      const std::string & yAxis  ) ;
+  bool setAxisLabels( AIDA::IProfile1D* prof, 
+                      const std::string & xAxis,
+                      const std::string & yAxis  ) ;
+
+}
