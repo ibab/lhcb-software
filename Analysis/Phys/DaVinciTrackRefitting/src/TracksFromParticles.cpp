a49e76fc8eebfe8859d66bc80ee128789090b302
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-02-27 16:00:49 +0000

    Fix -pedantic compilation warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@152940 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1243c602b911a3c637573ec743a85dc957c011bd

--- Analysis/Phys/DaVinciTrackRefitting/src/TracksFromParticles.cpp
+++ Analysis/Phys/DaVinciTrackRefitting/src/TracksFromParticles.cpp
@@ -7,54 +7,53 @@
 //-----------------------------------------------------------------------------
 // Implementation file for class : TracksFromParticles
 //
-// 2012-10-27 : Matthew Needham   
+// 2012-10-27 : Matthew Needham
 //-----------------------------------------------------------------------------
 
 
-DECLARE_ALGORITHM_FACTORY( TracksFromParticles );
+DECLARE_ALGORITHM_FACTORY( TracksFromParticles )
 
-TracksFromParticles::TracksFromParticles(const std::string& name, ISvcLocator* pSvc)
-  :DaVinciAlgorithm(name,pSvc)
+  TracksFromParticles::TracksFromParticles(const std::string& name, ISvcLocator* pSvc)
+    : DaVinciAlgorithm(name,pSvc)
 {
   declareProperty("inputLocation",m_outputLocation = "/Event/Rec/MyTracks");
-///@fixme: "inputLocation" as property for m_outputLocation ???
+  ///@fixme: "inputLocation" as property for m_outputLocation ???
 }
-
 
 TracksFromParticles::~TracksFromParticles() {}
 
 StatusCode TracksFromParticles::execute(){
 
   // get the input particles
- const LHCb::Particle::Range inputParticles = particles();
- std::vector<LHCb::Track*> tracks;
- LoKi::Extract::getTracks(inputParticles.begin(), inputParticles.end(), std::back_inserter(tracks));
- 
-
- if ( UNLIKELY( msgLevel(MSG::VERBOSE) ) ) {
-   verbose() << " Found  " << tracks.size() << endmsg;
- }
-
- // now clone the tracks and output
- LHCb::Tracks* outputCont = new LHCb::Tracks();
- std::vector<LHCb::Track*>::iterator iterT = tracks.begin();
- for (; iterT != tracks.end(); ++iterT ){
-   if (!outputCont->object((*iterT)->key())) {
-     // clone track only if it's not a duplicate from another candidate
-     LHCb::Track* newTrack = (*iterT)->cloneWithKey();
-     outputCont->insert(newTrack);
-   }
- } // iterT
-
- put(outputCont,m_outputLocation); 
+  const LHCb::Particle::Range inputParticles = particles();
+  std::vector<LHCb::Track*> tracks;
+  LoKi::Extract::getTracks(inputParticles.begin(), inputParticles.end(), std::back_inserter(tracks));
 
 
- setFilterPassed(true);
-  
- return StatusCode::SUCCESS;
- 
+  if ( UNLIKELY( msgLevel(MSG::VERBOSE) ) ) {
+    verbose() << " Found  " << tracks.size() << endmsg;
+  }
+
+  // now clone the tracks and output
+  LHCb::Tracks* outputCont = new LHCb::Tracks();
+  std::vector<LHCb::Track*>::iterator iterT = tracks.begin();
+  for (; iterT != tracks.end(); ++iterT ){
+    if (!outputCont->object((*iterT)->key())) {
+      // clone track only if it's not a duplicate from another candidate
+      LHCb::Track* newTrack = (*iterT)->cloneWithKey();
+      outputCont->insert(newTrack);
+    }
+  } // iterT
+
+  put(outputCont,m_outputLocation);
+
+
+  setFilterPassed(true);
+
+  return StatusCode::SUCCESS;
+
 }
 
 
 
-  
+
