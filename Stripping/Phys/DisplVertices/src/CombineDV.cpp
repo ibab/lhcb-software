5560db6dba0b6f81d5e506d39b8841bf2eab2ef3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-02-28 09:27:55 +0000

    Fix -pedantic compilation warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@153009 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e9b680adfcdefc8f7f94cd2a83ca583be278c2f5

--- Stripping/Phys/DisplVertices/src/CombineDV.cpp
+++ Stripping/Phys/DisplVertices/src/CombineDV.cpp
@@ -1,8 +1,8 @@
 // $Id$
-// Include files 
+// Include files
 
 // from Gaudi
-#include "GaudiKernel/AlgFactory.h" 
+#include "GaudiKernel/AlgFactory.h"
 #include "Kernel/IParticleCombiner.h"
 
 #include "GaudiAlg/GaudiAlgorithm.h"
@@ -18,24 +18,23 @@
 //-----------------------------------------------------------------------------
 
 // Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( CombineDV );
-
+DECLARE_ALGORITHM_FACTORY( CombineDV )
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-CombineDV::CombineDV( const std::string& name,
+  CombineDV::CombineDV( const std::string& name,
                         ISvcLocator* pSvcLocator)
-  : GaudiAlgorithm ( name , pSvcLocator )
+    : GaudiAlgorithm ( name , pSvcLocator )
 {
   declareProperty( "Inputs", m_candidateLocation ) ;
   declareProperty( "Output", m_outputLocation );
-  declareProperty( "ParticleCombinerName" ,m_combinerName = "MomentumCombiner");  
+  declareProperty( "ParticleCombinerName" ,m_combinerName = "MomentumCombiner");
 }
 //=============================================================================
 // Destructor
 //=============================================================================
-CombineDV::~CombineDV() {} 
+CombineDV::~CombineDV() {}
 
 //=============================================================================
 // Initialization
@@ -44,8 +43,8 @@ StatusCode CombineDV::initialize() {
   StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
   if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
 
-  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg; 
-  m_combiner = tool<IParticleCombiner> ( m_combinerName , this ) ; 
+  if ( msgLevel(MSG::DEBUG) ) debug() << "==> Initialize" << endmsg;
+  m_combiner = tool<IParticleCombiner> ( m_combinerName , this ) ;
 
   return StatusCode::SUCCESS;
 }
@@ -60,13 +59,13 @@ StatusCode CombineDV::execute() {
   LHCb::Particles* CombDVs = new LHCb::Particles();
   put( CombDVs , m_outputLocation );
 
-  if(!exist<LHCb::Particles>(m_candidateLocation)) 
+  if(!exist<LHCb::Particles>(m_candidateLocation))
     return Warning( m_candidateLocation +" not found", StatusCode::SUCCESS, 0);
   const LHCb::Particles * myCand = get<LHCb::Particles>(m_candidateLocation);
   LHCb::Particle::ConstVector daughters ;
 
   // loop on candidates to merge:
-  for ( LHCb::Particles::const_iterator i = myCand->begin() ; myCand->end() != i ; ++i ) 
+  for ( LHCb::Particles::const_iterator i = myCand->begin() ; myCand->end() != i ; ++i )
   {
     const LHCb::Particle* p = *i ;
     daughters.push_back ( p ) ;
@@ -76,9 +75,9 @@ StatusCode CombineDV::execute() {
   LHCb::Vertex* newvertex= new LHCb::Vertex();
 
   StatusCode sc = m_combiner->combine ( daughters , *newparticle , *newvertex ) ;
-  if ( sc.isFailure() ) 
+  if ( sc.isFailure() )
   {
-    std::cout<< "Error from momentum combiner"<<std::endl;   
+    std::cout<< "Error from momentum combiner"<<std::endl;
     delete newvertex;
     delete newparticle;
     return StatusCode::SUCCESS;
