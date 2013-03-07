fe58c9a5a6599872289ed2e2ae13cea74f839e71
Gloria Corti <gloria.corti@cern.ch>
2013-03-07 20:07:05 +0000

    revert changes for VL path as the old one require LHCB and DB not yet released
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@153275 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d2e5aefa0a8156eea9bdb1a11002d56412066f22

--- Gauss/Sim/Gauss/tests/options/testGauss-Upgrade-VL-DBTags.py
+++ Gauss/Sim/Gauss/tests/options/testGauss-Upgrade-VL-DBTags.py
@@ -6,7 +6,8 @@ nEvts = 1
 LHCbApp().EvtMax = nEvts
 
 from Configurables import LHCbApp, CondDB
-LHCbApp().DDDBtag    = "dddb-20130214"
+#LHCbApp().DDDBtag    = "dddb-20130214"
+LHCbApp().DDDBtag    = "dddb-20130124"
 LHCbApp().CondDBtag  = "simcond-20121001-vc-md100"
 
 
