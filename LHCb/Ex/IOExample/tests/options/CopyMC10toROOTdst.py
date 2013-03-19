767f234a9eff2e6359607c3d3f29732d593601d5
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-03-19 13:16:53 +0000

    follow move of MC10 input file to eos
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@153676 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f4d64047e62bcb050c26bb422072f8e51f5f6e0f

--- LHCb/Ex/IOExample/tests/options/CopyMC10toROOTdst.py
+++ LHCb/Ex/IOExample/tests/options/CopyMC10toROOTdst.py
@@ -6,5 +6,5 @@ IOTest( EvtMax = 5, WithMC = True, LoadAll = True )
 
 ioh = IOHelper('POOL','ROOT')
 ioh.setupServices()
-ioh.inputFiles(['PFN:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/MC/MC10/ALLSTREAMS.DST/00008898/0000/00008898_00000002_1.allstreams.dst?svcClass=lhcbdisk'])
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/MC/MC10/ALLSTREAMS.DST/00008898/0000/00008898_00000002_1.allstreams.dst'])
 ioh.outStream('PFN:ROOT-MC10.dst',"InputCopyStream")
