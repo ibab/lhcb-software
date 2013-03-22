173681efef5a1c992a2fe058b611abcb4c46562e
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-03-22 08:46:11 +0000

    One more file moved to EOS
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@153876 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a455814699ad93c45daca56337d71c507e9943d3

--- LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTdst.py
+++ LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTdst.py
@@ -6,5 +6,5 @@ IOTest( EvtMax = 5, LoadAll = True )
 
 ioh = IOHelper()
 ioh.setupServices()
-ioh.inputFiles(['PFN:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/LHCb/Collision12/DIMUON.DST/00020198/0000/00020198_00000763_1.dimuon.dst?svcClass=lhcbdisk'])
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/LHCb/Collision12/DIMUON.DST/00020198/0000/00020198_00000763_1.dimuon.dst'])
 ioh.outStream('PFN:Reco14-Stripping20.dst',"InputCopyStream")
--- LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTmdst.py
+++ LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTmdst.py
@@ -6,5 +6,5 @@ IOTest( EvtMax = 5, LoadAll = True, DataContent = 'MDST', KillDAQ = True )
 
 ioh = IOHelper()
 ioh.setupServices()
-ioh.inputFiles(['PFN:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/LHCb/Collision12/BHADRON.MDST/00020198/0000/00020198_00000758_1.bhadron.mdst?svcClass=lhcbdisk'])
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/LHCb/Collision12/BHADRON.MDST/00020198/0000/00020198_00000758_1.bhadron.mdst'])
 ioh.outStream('PFN:Reco14-Stripping20.mdst',"InputCopyStream")
