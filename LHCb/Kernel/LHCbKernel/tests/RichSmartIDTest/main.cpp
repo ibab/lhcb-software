37600f5b66c9e53d2b5eef4d1592bed7de20e144
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-03-26 12:26:20 +0000

    Extend RichSmartID test application to test conversion to and from an LHCbID
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@154167 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ee0083ad350602609762e5d8043464d60c117c46

--- LHCb/Kernel/LHCbKernel/tests/RichSmartIDTest/main.cpp
+++ LHCb/Kernel/LHCbKernel/tests/RichSmartIDTest/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include "Kernel/RichSmartID.h"
+#include "Kernel/LHCbID.h"
 
 using namespace std;
 
@@ -41,7 +42,24 @@ int main ( int /*argc*/, char** /*argv*/ )
                      pixcol != id.pixelCol()   ||
                      subpix != id.pixelSubRow() )
                 {
-                  cout << "Problem with RichSmartID " << id << endl;
+                  cerr << "Problem with RichSmartID " << id << endl;
+                  OK = false;
+                }
+
+                // Test conversion to and from an LHCbID
+                const LHCb::LHCbID lID( id );
+                const LHCb::RichSmartID nid( lID.richID() );
+                if ( nid.rich()        != id.rich()       ||
+                     nid.panel()       != id.panel()      ||
+                     nid.pdNumInCol()  != id.pdNumInCol() ||
+                     nid.pdCol()       != id.pdCol()      ||
+                     nid.pixelRow()    != id.pixelRow()   ||
+                     nid.pixelCol()    != id.pixelCol()   ||
+                     nid.pixelSubRow() != id.pixelSubRow() )
+                {
+                  cerr << "Problem with RichSmartID <-> LHCbID conversion" << endl;
+                  cerr << "  Original  ID : " << id  << endl;
+                  cerr << "  Converted ID : " << nid << endl;
                   OK = false;
                 }
 
@@ -82,9 +100,26 @@ int main ( int /*argc*/, char** /*argv*/ )
                    pixrow != id.pixelRow()   ||
                    pixcol != id.pixelCol()    )
               {
-                cout << "Problem with RichSmartID " << id << endl;
+                cerr << "Problem with RichSmartID " << id << endl;
                 OK = false;
               }
+
+              // // Test conversion to and from an LHCbID
+              // const LHCb::LHCbID lID( id );
+              // const LHCb::RichSmartID nid( lID.richID() );
+              // if ( nid.rich()        != id.rich()       ||
+              //      nid.panel()       != id.panel()      ||
+              //      nid.pdNumInCol()  != id.pdNumInCol() ||
+              //      nid.pdCol()       != id.pdCol()      ||
+              //      nid.pixelRow()    != id.pixelRow()   ||
+              //      nid.pixelCol()    != id.pixelCol()   ||
+              //      nid.pixelSubRow() != id.pixelSubRow() )
+              // {
+              //   cerr << "Problem with RichSmartID <-> LHCbID conversion" << endl;
+              //   cerr << "  Original  ID : " << id  << endl;
+              //   cerr << "  Converted ID : " << nid << endl;
+              //   OK = false;
+              // }
 
             }
           }
