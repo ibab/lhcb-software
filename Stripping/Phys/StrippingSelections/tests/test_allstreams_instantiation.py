6c55c04821b2e08956b2e00e6068506b7650415f
Anton Poluektov <anton.poluektov@cern.ch>
2013-03-26 12:38:30 +0000

    Fix typo in __init__, update tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@154169 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9e77fabda8f196fb638d5fcd17d24bb85acf293a

--- Stripping/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
+++ Stripping/Phys/StrippingSelections/tests/test_allstreams_instantiation.py
@@ -13,7 +13,7 @@ def test_instantiate_StrippingConf_with_all_streams() :
     from StrippingConf.Configuration import StrippingConf
     from StrippingSelections.Utils import buildStreams
 
-    streams = buildStreams('stripping20r1')
+    streams = buildStreams('stripping20r0p1')
 
     sc = StrippingConf (Streams = streams)
 
