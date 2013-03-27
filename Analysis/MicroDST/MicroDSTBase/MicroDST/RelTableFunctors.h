2d684d7d731780a06b2a963d3d5cbfc105db4339
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-03-27 22:06:23 +0000

    Fix -std=c++11 compilation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@154280 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d6b6084f474684d668f3634a035d2faffd692a59

--- Analysis/MicroDST/MicroDSTBase/MicroDST/RelTableFunctors.h
+++ Analysis/MicroDST/MicroDSTBase/MicroDST/RelTableFunctors.h
@@ -16,23 +16,21 @@ namespace MicroDST
   template <class TABLE>
   struct Cloners
   {
-    typedef boost::function< typename TABLE::From (typename TABLE::From )> From;
-    typedef boost::function< typename TABLE::To (typename TABLE::To )> To;
+    typedef boost::function< typename TABLE::From (typename TABLE::From ) > From;
+    typedef boost::function< typename TABLE::To   (typename TABLE::To   ) > To;
   };
 
   template <class TABLE>
   struct EntryClonerBase
   {
-    EntryClonerBase(const typename Cloners<TABLE>::From& fromCloner,
-                    const typename Cloners<TABLE>::To& toCloner)
-      :
-      m_fromCloner(fromCloner),
-      m_toCloner(toCloner)
-    {
-    }
+    EntryClonerBase( const typename Cloners<TABLE>::From& fromCloner,
+                     const typename Cloners<TABLE>::To& toCloner )
+      : m_fromCloner(fromCloner),
+        m_toCloner(toCloner)
+    { }
   protected:
     typename Cloners<TABLE>::From m_fromCloner;
-    typename Cloners<TABLE>::To m_toCloner;
+    typename Cloners<TABLE>::To   m_toCloner;
   private:
     EntryClonerBase() {}
   };
@@ -41,60 +39,55 @@ namespace MicroDST
   struct EntryCloner : public EntryClonerBase<TABLE>
   {
 
-    EntryCloner(const typename Cloners<TABLE>::From& fromCloner,
-                const typename Cloners<TABLE>::To& toCloner)
-      :
-      EntryClonerBase<TABLE>(fromCloner, toCloner)
-    {
-    }
+    EntryCloner( const typename Cloners<TABLE>::From& fromCloner,
+                 const typename Cloners<TABLE>::To& toCloner )
+      : EntryClonerBase<TABLE>(fromCloner, toCloner)
+    { }
     typename TABLE::Entry operator() (const typename TABLE::Entry& entry) const
     {
-      const typename TABLE::From clonedFrom = m_fromCloner(entry.from());
-      const typename TABLE::To clonedTo = m_toCloner( entry.to() );
-      return typename TABLE::Entry(clonedFrom, clonedTo, entry.weight());
+      const typename TABLE::From clonedFrom = this->m_fromCloner( entry.from() );
+      const typename TABLE::To clonedTo     = this->m_toCloner  ( entry.to()   );
+      return typename TABLE::Entry( clonedFrom, clonedTo, entry.weight() );
     }
   };
 
   template <class TABLE>
   struct EntryCloner<TABLE, false> : public EntryClonerBase<TABLE>
   {
-    EntryCloner(const typename Cloners<TABLE>::From& fromCloner,
-                const typename Cloners<TABLE>::To& toCloner)
-      :
-      EntryClonerBase<TABLE>(fromCloner, toCloner)
+    EntryCloner( const typename Cloners<TABLE>::From& fromCloner,
+                 const typename Cloners<TABLE>::To& toCloner )
+      : EntryClonerBase<TABLE>( fromCloner, toCloner )
+    { }
+    typename TABLE::Entry operator() ( const typename TABLE::Entry& entry ) const
     {
-    }
-    typename TABLE::Entry operator() (const typename TABLE::Entry& entry) const
-    {
-      const typename TABLE::From clonedFrom = m_fromCloner(entry.from());
-      const typename TABLE::To clonedTo = m_toCloner( entry.to() );
-      return typename TABLE::Entry(clonedFrom, clonedTo);
+      const typename TABLE::From clonedFrom = this->m_fromCloner ( entry.from() );
+      const typename TABLE::To   clonedTo   = this->m_toCloner   ( entry.to()   );
+      return typename TABLE::Entry( clonedFrom, clonedTo );
     }
   };
 
   template <class TABLE>
   struct TableCloner
   {
-    TableCloner(const typename Cloners<TABLE>::From& fromCloner,
-                const typename Cloners<TABLE>::To& toCloner)
-      :
-      m_cloner(fromCloner, toCloner)
-    {
-    }
-    TABLE* operator() (const TABLE* table)
+    TableCloner( const typename Cloners<TABLE>::From& fromCloner,
+                 const typename Cloners<TABLE>::To& toCloner )
+      : m_cloner(fromCloner, toCloner)
+    { }
+    TABLE* operator() ( const TABLE* table )
     {
       TABLE* cloneTable = new TABLE();
 
       typename TABLE::Range relations = table->relations();
       for ( typename TABLE::Range::const_iterator iRel = relations.begin();
-            iRel != relations.end(); ++iRel ) 
+            iRel != relations.end(); ++iRel )
       {
         typename TABLE::Entry entryClone = m_cloner(*iRel);
-        if (isValid(entryClone.from()) && isValid(entryClone.to()) ) {
+        if ( isValid(entryClone.from()) && isValid(entryClone.to()) )
+        {
           cloneTable->add(entryClone);
         }
       } // loop on all relations
-      
+
       return cloneTable;
     }
   protected:
