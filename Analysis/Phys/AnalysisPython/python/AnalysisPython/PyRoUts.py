240e6f466f6cb822f97945ef35faf72283a808da
Vanya Belyaev <ivan.belyaev@cern.ch>
2013-04-01 15:36:24 +0000

    PyRoUts.py : more decorators
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@154418 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c577bd938523c39d5da894ce611401bd96d096f1

--- Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
+++ Analysis/Phys/AnalysisPython/python/AnalysisPython/PyRoUts.py
@@ -4438,6 +4438,64 @@ ROOT.TMinuit . __len__      = lambda s : s.GetNumPars()
 ROOT.TMinuit . par         = _mn_par_
 ROOT.TMinuit . parameter   = _mn_par_
 ROOT.TMinuit . __getitem__ = _mn_par_
+ROOT.TMinuit . __call__    = _mn_par_
+
+# =============================================================================
+## iterator over TMinuit indices 
+def _mn_iter_ ( self ) :
+    """
+    Iterator for TMinuit indices:
+
+    >>> m = ... #TMinuit object
+    >>> for i in m : print m[i]
+    
+    """
+    i = 0
+    while i < len ( m )  :
+        yield i
+        i += 1
+
+ROOT.TMinuit . __iter__ = _mn_iter_
+
+# =============================================================================
+## excute MINUIT command
+#  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+#  @date   2013-04-01
+def _mn_exec_ ( self , command , *args ) :
+    """
+    Execute MINUIT  command
+    """
+    if not args :
+        args = [0]
+        logger.warning ( 'TMinuit::execute: empty vector replaced with  %s ' % args ) 
+        
+    import array
+    arglist = array.array ( 'd' , [ i for i in args ]  )
+    ierr    = ROOT.Long   ( 0 )
+    #        
+    self.mnexcm ( command , arglist , len(arglist) , ierr )
+    #
+    return ierr
+
+_mn_exec_ . __doc__  += '\n' + ROOT.TMinuit.mnexcm . __doc__
+
+ROOT.TMinuit.execute = _mn_exec_
+
+# =============================================================================
+## excute MINUIT "SHOW" command
+#  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+#  @date   2013-04-01
+def _mn_show_ ( self , what , *args ) :
+    """
+    Execute MINUIT  command
+    """
+    if not args : args = [ 0 ]
+    ##
+    what = what.upper()
+    whar = what.replace ( 'SHOW',' ' )
+    return _mn_exec_ ( self , 'SHOW ' + what , *args )
+
+ROOT.TMinuit.show = _mn_show_
 
 # =============================================================================
 ## set the parameter 
@@ -4452,15 +4510,9 @@ def _mn_set_par_ ( self , i , val , fix = False ) :
     ip = ROOT.Long ( i )
     if hasattr ( val , 'value' ) : val = val.value()
     #
-    import array
-    arglist = array.array('d',2*[0.])
-    ierr    = ROOT.Long  ( -1 )
+    ierr =  _mn_exec_ ( self , "SET PAR" , i + 1 , val )
     #
-    arglist[0] =  i + 1 
-    arglist[1] = val
-    self.mnexcm ( "SET PAR" , arglist , 2 , ierr )
-    #
-    if fix : self.FixParameter ( ip ) 
+    if fix : self.FixParameter ( ROOT.Long ( ip ) ) 
     #
     return ierr 
 
@@ -4470,7 +4522,27 @@ ROOT.TMinuit . setParameter = _mn_set_par_
 ROOT.TMinuit . fixPar       = lambda s,i,v: _mn_set_par_ ( s , i , v , True )
 ROOT.TMinuit . fixParameter = lambda s,i,v: _mn_set_par_ ( s , i , v , True )
 
+
 ROOT.TMinuit . __setitem__ = _mn_set_par_ 
+
+
+# =============================================================================
+## release the parameter 
+#  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+#  @date   2012-09-28
+def _mn_rel_par_ ( self , i ) :
+    """
+    Release MINUIT parameter for some value
+
+    >>> mn = ... # TMinuit  obejct
+    >>> mn.release ( 1 ) 
+    """
+    if not i in self : raise IndexError
+    #
+    return _mn_exec_ ( self , "REL" , i + 1 )
+    #
+
+ROOT.TMinuit . release = _mn_rel_par_ 
 
 # ===========================================================
 ## set the parameter 
@@ -4490,20 +4562,17 @@ def _mn_min_ ( self                  ,
     
     """
     #
-    import array
-    arglist = array.array ( 'd' , 2*[0.] )
-    ierr    = ROOT.Long   (  -1          )
-    #
-    arglist[0] = maxcalls
-    arglist[1] = tolerance
-    #
-    self.mnexcm ( method , arglist , 2 , ierr )
-    #
-    return ierr
+    return _mn_exec_ ( self , method , maxcalls , tolerance ) 
 
 ROOT.TMinuit . migrade  = _mn_min_
+ROOT.TMinuit . migrad   = _mn_min_
 ROOT.TMinuit . fit      = _mn_min_
-       
+
+ROOT.TMinuit . hesse    = lambda s : _mn_exec_ ( s , 'HESSE'   , 0 )
+ROOT.TMinuit . minimize = lambda s : _mn_exec_ ( s , 'MIN'     , 0 )
+ROOT.TMinuit . seek     = lambda s : _mn_exec_ ( s , 'SEEK'    , 0 )
+ROOT.TMinuit . simplex  = lambda s : _mn_exec_ ( s , 'SIMPLEX' , 0 )
+
 # =============================================================================
 ## set the parameter 
 #  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
@@ -4589,7 +4658,7 @@ ROOT.TMinuit .   minErrs = _mn_minerr_
 ROOT.TMinuit . minosErrs = _mn_minerr_ 
 
 # =============================================================================
-## Run MINOS
+## run MINOS
 #  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
 #  @date   2012-09-28 
 def _mn_minos_ ( self , *args ) :
@@ -4597,24 +4666,206 @@ def _mn_minos_ ( self , *args ) :
     Get MINOS errors for parameter:
     
     >>> m = ...       # TMinuit object
-    >>> pos,neg = m.minosErr( 0 )
+    >>> result = m.minos( 1 , 2  )
     
     """
     ipars  = []
     for i in args :
         if not i in self : raise IndexError
         ipars.append ( i )
-    #
-    import array
-    arglist = array.array ( 'd' , [ 200 ] + ipars  )
-    ierr    = ROOT.Long   ( 0 )
-    #
-    self.mnexcm ( "MINOS" , arglist , len(arglist) , ierr )
-    #
-    return ierr
+
+    return _mn_exec_ ( self , 'MINOS' , 200 , *tuple(ipars) ) 
 
 ROOT.TMinuit . minos = _mn_minos_
+# =============================================================================
 
+# =============================================================================
+## get current Minuit statistics 
+#  @author Vanya BELYAEV Ivan.Belyaev@cern.ch
+#  @date   2013-04-01
+def _mn_stat_  ( self ) :
+    """
+    Get current Minuit status
+
+    >>> mn   = ... # TMoniut object
+    >>> stat = mn.stat()
+    
+    
+    Returns concerning the current status of the minimization
+    *-*      =========================================================
+    *-*       User-called
+    *-*          Namely, it returns:
+    *-*        FMIN: the best function value found so far
+    *-*        FEDM: the estimated vertical distance remaining to minimum
+    *-*        ERRDEF: the value of UP defining parameter uncertainties
+    *-*        NPARI: the number of currently variable parameters
+    *-*        NPARX: the highest (external) parameter number defined by user
+    *-*        ISTAT: a status integer indicating how good is the covariance
+    *-*           matrix:  0= not calculated at all
+    *-*                    1= approximation only, not accurate
+    *-*                    2= full matrix, but forced positive-definite
+    *-*                    3= full accurate covariance matrix
+    *
+    
+    """
+    fmin    = ROOT.Double ( )
+    fedm    = ROOT.Double ( )
+    errdef  = ROOT.Double ( )
+    npari   = ROOT.Long   (1)
+    nparx   = ROOT.Long   (2)
+    istat   = ROOT.Long   (0)
+    #
+    self . mnstat( fmin, fedm, errdef, npari , nparx , istat )
+    #
+    return { 'FMIN'   : float( fmin   ) ,
+             'FEDM'   : float( fmin   ) ,
+             'ERRDEF' : float( errdef ) ,
+             'NPARI'  : int  ( npari  ) ,
+             'NPARX'  : int  ( nparx  ) ,
+             'ISTAT'  : int  ( nparx  ) } 
+
+_mn_stat_ . __doc__  += '\n' + ROOT.TMinuit.mnstat . __doc__
+
+ROOT.TMinuit.stat    = _mn_stat_ 
+# =============================================================================
+## get UP-parameter for err-def 
+#  @author Vanya BELYAEV Ivan.Belyaev@cern.ch
+#  @date   2013-04-01
+def _mn_get_errdef_ ( self ) :
+    """
+    Get UP-parameter used to define the uncertainties
+
+    >>> mn = ... # TMoniut object
+    >>> up  = mn.GetErrorDef()
+    
+    """
+    return _mn_stat_ ( self ) ['ERRDEF']
+
+ROOT.TMinuit.errDef      = _mn_get_errdef_ 
+ROOT.TMinuit.GetErrorDef = _mn_get_errdef_ 
+
+# =============================================================================
+## create N-sigma contour 
+#  @author Vanya BELYAEV Ivan.Belyaev@cern.ch
+#  @date   2011-06-07
+def _mn_contour_ ( self , npoint , par1  , par2 , nsigma = 1 ) :
+    """
+    Create n-sigma contour for par1 vs par2
+
+    >>> mn = ... # TMinuit object
+    >>> graph = mn.contour( 100 , 1 , 2 )
+    
+    """
+    if     npoint < 4   : raise ValueError ( 'contour: npoint (%s) must be >= 4'  % npoint )
+    if not par1 in self : raise ValueError ( 'contour: par1(%s) is not in Minuit' % par1   )
+    if not par2 in self : raise ValueError ( 'contour: par2(%s) is not in Minuit' % par2   )
+    if     par1 == par2 : raise ValueError ( 'contour: par1 == par2(%s) '         % par2   )
+    #
+    ## save old error defintion
+    #
+    old_err_def = self.GetErrorDef()
+    #
+    ## set new error definition
+    #
+    self.SetErrorDef ( nsigma * nsigma )
+    
+    graph  = self.Contour ( npoint , par1 , par2 )
+
+    #
+    ## restore old error defininion
+    #
+    status = self.GetStatus()
+    self.SetErrorDef ( old_err_def ) 
+    #
+    if graph and 0 == status : return graph
+    logger.error ( 'TMinuit::Contour: status %i' % status ) 
+    return graph 
+
+_mn_contour_ . __doc__ += '\n' + ROOT.TMinuit.Contour . __doc__ 
+
+ROOT.TMinuit . contour = _mn_contour_
+
+# =============================================================================
+## get the covariance matrix from TMinuit
+def _mn_cov_ ( self , size = -1 ) :
+    """
+    Get the covariance matrix from TMinuit
+
+    >>> mn  = ... # TMinuit object
+    >>> cov = mn.cov() 
+    
+    """
+    #
+    if size <= 0 : size = len ( self )
+    size = min ( size , len ( self ) ) 
+    #
+    import array
+    matrix = array.array ( 'd' , [ 0 for i in range(0, size * size) ]  )
+    self.mnemat ( matrix , size )
+    #
+    if   1 == size : mtrx = cpp.Gaudi.Math.SymMatrix1x1 ()
+    elif 2 == size : mtrx = cpp.Gaudi.Math.SymMatrix2x2 ()
+    elif 3 == size : mtrx = cpp.Gaudi.Math.SymMatrix3x3 ()
+    elif 4 == size : mtrx = cpp.Gaudi.Math.SymMatrix4x4 ()
+    elif 5 == size : mtrx = cpp.Gaudi.Math.SymMatrix5x5 ()
+    elif 6 == size : mtrx = cpp.Gaudi.Math.SymMatrix6x6 ()
+    elif 7 == size : mtrx = cpp.Gaudi.Math.SymMatrix7x7 ()
+    ## no 8x8! 
+    elif 9 == size : mtrx = cpp.Gaudi.Math.SymMatrix9x9 ()
+    else :
+        t_line = cpp.std.vector('double')
+        t_mtrx = cpp.std.vector( t_line )
+        mtrx   = t_mtrx() 
+        for i in range ( 0 , size ) : mtrx.push_back ( t_line( size ) )
+        #
+        for i in range ( 0 , size ) :
+            for j in range ( 0 , size ) :
+                mtrx [i][j] = matrix [ i * size + j ]
+                
+        return mtrx  ## RETURN 
+        
+    for i in range ( 0 , size ) :
+        for j in range ( i , size ) :
+            mtrx [i,j] = matrix [ i * size + j ]
+            
+    return mtrx
+
+# =============================================================================
+## get the correlation matrix from TMinuit
+def _mn_cor_ ( self , size = -1 ) :
+    """
+    Get the correlation matrix from TMinuit
+
+    >>> mn  = ... # TMinuit object
+    >>> cor = mn.cor() 
+        
+    """
+    #
+    cov = self.cov ( size )
+    #
+    from math import sqrt
+    #
+    # matrix?
+    if hasattr ( cov , 'kRows' ) and hasattr ( cov , 'kCols' ) :
+        ## copy 
+        cor = cov.__class__ () 
+        for i in range(0, cov.kRows) :
+            d_i = cov(i,i)
+            cor[i,i] = 1 if 0 < d_i else 0 
+            for j in range ( i + 1 , cov.kRows ) :
+                d_j = cov(j,j)
+                if 0 != cov(i,j) and 0 < d_i and 0 < d_j  :
+                    cor[i,j] = cov(i,j) / sqrt ( d_i * d_j )
+                else : 
+                    cor[i,j] = 0 
+        return cor
+            
+_mn_cor_ . __doc__ += '\n' + ROOT.TMinuit.mnemat . __doc__ 
+
+
+ROOT.TMinuit . cov  = _mn_cov_
+ROOT.TMinuit . cor  = _mn_cor_
+ROOT.TMinuit . corr = _mn_cor_
 
 # =============================================================================
 logger.info ( 'Some useful decorations for RooFit objects')
