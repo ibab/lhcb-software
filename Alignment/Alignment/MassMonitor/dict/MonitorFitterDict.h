1f2de699490110646dfd6fcef8646fc1da726793
Jack Wimberley <jack.wimberley@cern.ch>
2013-04-04 20:49:15 +0000

    Remove unnecessary dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@154525 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9d243232c451dd59d684b3c4307e5353683837ab

--- Alignment/Alignment/MassMonitor/dict/MonitorFitterDict.h
+++
@@ -1,7 +0,0 @@
-#ifndef DICT_MONITORFITTERDICT_H 
-#define DICT_MONITORFITTERDICT_H 1
-
-#include "MonitorFitter/MonitorFitter.hh"
-#include "MonitorFitter/RooCruijff.hh"
-
-#endif // DICT_MONITORFITTERDICT_H
