41dc3d7d78f1755dda6db06397d3a35c762f8965
Wouter Hulsbergen <wouterh@nikhef.nl>
2013-04-04 14:30:14 +0000

    change TrackBestTrackCreator to use TrackKernel/TrackCloneData
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@154504 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 517915f1fc801bf74bd7ba59c298aabdbc3d0e62

--- Rec/Tr/TrackUtils/src/TrackBestTrackCreator.cpp
+++ Rec/Tr/TrackUtils/src/TrackBestTrackCreator.cpp
@@ -30,84 +30,28 @@
 #include "TrackInterfaces/ITrackCloneFinder.h"
 #include "TrackInterfaces/ITrackFitter.h"
 #include "TrackInterfaces/ITrackStateInit.h"
-#include "DetDesc/StaticArray.h"
+#include "TrackKernel/TrackCloneData.h"
 
 namespace  {
   // structure to save some data for each track
-  class TrackData
+  class TrackData : public LHCb::TrackCloneData
   {
-    // we can probably do this with ranges since the lhcbids are
-    // ordered by detector type. for now, let's just create containers
-  public:
-    //typedef Range_<LHCb::Track::LHCbIDContainer> LHCbIDs ;
-    //typedef StaticArray<LHCb::LHCbID,128> LHCbIDs ;
-    typedef std::vector<LHCb::LHCbID> LHCbIDs;
-    LHCbIDs veloIds, veloRIds,veloPhiIds ;
-    LHCbIDs TTIds ;
-    LHCbIDs TIds ;
   private:
-    LHCb::Track* m_track ;
-    unsigned long int m_weight ;
     double m_qOverP ;
     bool m_owntrack ;
-
-    // don't allow copies, because ownership too complicated
-    TrackData( const TrackData& ) {}
   public:
-    TrackData() : m_track(NULL), m_weight(0), m_qOverP(0.), m_owntrack(false) {}
-    TrackData(LHCb::Track* atrack, bool owntrack=true)
-      : m_track(atrack),
-        m_weight(0), 
-        m_qOverP(atrack->firstState().qOverP()),
-        m_owntrack(owntrack)
-    {
-      veloIds.reserve(48); veloRIds.reserve(24); veloPhiIds.reserve(24);
-      TTIds.reserve(6);
-      TIds.reserve(24);
-      const LHCb::Track::LHCbIDContainer& ids = atrack->lhcbIDs() ;
-      for( LHCb::Track::LHCbIDContainer::const_iterator id = ids.begin() ;
-           id != ids.end(); ++id ) {
-        switch( id->detectorType() ) {
-        case LHCb::LHCbID::Velo: 
-          veloIds.push_back(*id) ; 
-          if( id->isVeloR() ) veloRIds.push_back(*id) ;
-          else veloPhiIds.push_back(*id) ;
-          break ;
-        case LHCb::LHCbID::TT: TTIds.push_back(*id) ; break ;
-        case LHCb::LHCbID::OT: 
-        case LHCb::LHCbID::IT: TIds.push_back(*id) ; break ;
-        default: break ;
-        }
-      }
-      // compute a weight for sorting.
-      LHCb::HitPattern hp(ids) ;
-      // first sort by type
-      int weightFromType[10] = {0,11,10,20,15,16,12,2,3} ; 
-      m_weight = 100000 * weightFromType[atrack->type()] ;
-      // then sort by the number of TT planes
-      m_weight += 10000*hp.tt().count() ;
-      // then sort by number of T layers / velo _clusters_
-      m_weight += 100*hp.numTLayers() ;
-      m_weight += 100*( (hp.veloRA()|hp.veloRC()) & (hp.veloPhiA()|hp.veloPhiC()) ).count() ;
-      // only finally sort by total number of hits
-      m_weight += ids.size() ;      
-    }
-
+    double qOverP() const { return m_qOverP ; }
+    TrackData( const LHCb::Track& track, bool owntrack=true ) : 
+      LHCb::TrackCloneData(track), 
+      m_qOverP( track.firstState().qOverP()),
+      m_owntrack(owntrack){} ;
     // destructor
     ~TrackData() {
-      if( m_owntrack ) delete m_track ;
+      if( m_owntrack ) delete trackp() ;
     }
-
-    // return the track
-    LHCb::Track* track() const { return m_track ; }
     // release ownership of the track
-    LHCb::Track* release() { m_owntrack = false ; return m_track ; }
-    // sort by weight
-    bool operator<( const TrackData& rhs) const { return m_weight > rhs.m_weight ; }
-    // get q/p (set by stateinittool)
-    double qOverP() const { return m_qOverP ; }
-    // return the weight used for sorting
-    unsigned long int weight() const { return m_weight; }
+    LHCb::Track* release() { m_owntrack = false ; return const_cast<LHCb::Track*>(trackp()) ; }
+    LHCb::Track& nonconsttrack() { return const_cast<LHCb::Track&>(track()) ; }
   } ;
 
   struct TrackDataSorter
@@ -249,39 +193,11 @@ StatusCode TrackBestTrackCreator::initialize()
   return sc ;
 }
 
-
-namespace {
-  template <class Container>
-  size_t nCommon(const Container& lhs, const Container& rhs) {
-    size_t rc(0) ;
-    typename Container::const_iterator first1 = lhs.begin() ;
-    typename Container::const_iterator last1  = lhs.end() ;
-    typename Container::const_iterator first2 = rhs.begin() ;
-    typename Container::const_iterator last2  = rhs.end() ;
-    while (first1 != last1 && first2 != last2)
-      if (*first1 < *first2)
-	++first1;
-      else if (*first2 < *first1)
-	++first2;
-      else {
-	++first1;
-	++first2;	++rc ;
-      }
-    return rc ;
-  }
-  
-  template <class Container>
-  double overlapFraction(const Container& lhs, const Container& rhs) {
-    size_t minsize = std::min( lhs.size(), rhs.size()) ;
-    return minsize>0 ? nCommon(lhs,rhs) / double(minsize) : 0 ;
-  }
-}
-
 bool TrackBestTrackCreator::veloClones(const TrackData& lhs,
 				       const TrackData& rhs) const
 {
-  double fR = overlapFraction(lhs.veloRIds, rhs.veloRIds) ;
-  double fPhi = overlapFraction(lhs.veloPhiIds,rhs.veloPhiIds) ;
+  double fR   = lhs.overlapFraction(rhs, TrackData::VeloR) ;
+  double fPhi = lhs.overlapFraction(rhs, TrackData::VeloPhi) ;
 #ifdef DEBUGHISTOGRAMS
   if(fR>0) plot1D(fR,"veloROverlapFractionH1",0,1) ;
   if(fPhi>0) plot1D(fPhi,"veloPhiOverlapFractionH1",0,1) ;
@@ -290,17 +206,21 @@ bool TrackBestTrackCreator::veloClones(const TrackData& lhs,
 }
 
 bool TrackBestTrackCreator::veloOrClones(const TrackData& lhs,
-					    const TrackData& rhs) const
+					 const TrackData& rhs) const
 {
-  double fR = overlapFraction(lhs.veloRIds, rhs.veloRIds) ;
-  double fPhi = overlapFraction(lhs.veloPhiIds,rhs.veloPhiIds) ;
+  double fR   = lhs.overlapFraction(rhs, TrackData::VeloR) ;
+  double fPhi = lhs.overlapFraction(rhs, TrackData::VeloPhi) ;
+#ifdef DEBUGHISTOGRAMS
+  if(fR>0) plot1D(fR,"veloROverlapFractionH1",0,1) ;
+  if(fPhi>0) plot1D(fPhi,"veloPhiOverlapFractionH1",0,1) ;
+#endif
   return fR>m_maxOverlapFracVelo || fPhi>m_maxOverlapFracVelo ;
 }
 
 bool TrackBestTrackCreator::TClones(const TrackData& lhs,
-				       const TrackData& rhs) const
+				    const TrackData& rhs) const
 {
-  double f = overlapFraction(lhs.TIds,rhs.TIds); 
+  double f = lhs.overlapFraction(rhs,TrackData::T) ;
 #ifdef DEBUGHISTOGRAMS
   if(f>0) plot1D(f,"TOverlapFractionH1",0,1) ;
 #endif
@@ -308,9 +228,9 @@ bool TrackBestTrackCreator::TClones(const TrackData& lhs,
 }
 
 bool TrackBestTrackCreator::TTClones(const TrackData& lhs,
-					const TrackData& rhs) const
+				     const TrackData& rhs) const
 {
-  double f = overlapFraction(lhs.TTIds,rhs.TTIds) ;
+  double f = lhs.overlapFraction(rhs,TrackData::TT) ;
 #ifdef DEBUGHISTOGRAMS
   if(f>0) plot1D(f,"TTOverlapFractionH1",0,1) ;
 #endif
@@ -337,7 +257,7 @@ StatusCode TrackBestTrackCreator::execute()
       if( m_initTrackStates )
         sc = m_stateinittool->fit(*track,true);
       if( sc.isSuccess() ) 
-        alltracks.push_back(new TrackData(track)) ;
+        alltracks.push_back(new TrackData(*track)) ;
       else {
         delete track ;
         Warning("TrackStateInitTool fit failed", sc, 0 ).ignore() ;
@@ -348,7 +268,7 @@ StatusCode TrackBestTrackCreator::execute()
   // sort them
   std::stable_sort( alltracks.begin(), alltracks.end(), TrackDataSorter() ) ;
 
-   // loop over all tracks and add them to output container if they
+  // loop over all tracks and add them to output container if they
   // pass selector and are not yet there.
   std::vector<TrackData*> selectedtracks ;
   selectedtracks.reserve( alltracks.size() ) ;
@@ -361,8 +281,8 @@ StatusCode TrackBestTrackCreator::execute()
       // rely on the fact that tracks were sorted Long, Downstream, Upstream, Velo, T
       // note: 'clone' can also mean that either of them has no hits in this region
 
-      const LHCb::Track* itrack = (*it)->track() ;
-      const LHCb::Track* jtrack = (*jt)->track() ;
+      const LHCb::Track* itrack = (*it)->trackp() ;
+      const LHCb::Track* jtrack = (*jt)->trackp() ;
       int itype = itrack->type() ;
       int jtype = jtrack->type() ;
       double dqop = (*it)->qOverP()-(*jt)->qOverP() ;
@@ -431,7 +351,7 @@ StatusCode TrackBestTrackCreator::execute()
     }
     // if it isn't there yet, clone, fit, apply selection
     if(!found) {
-      bool success = fitAndSelect(*((*it)->track())) ;
+      bool success = fitAndSelect((*it)->nonconsttrack()) ;
       if( success )
 	selectedtracks.push_back( *it ) ;
     }
