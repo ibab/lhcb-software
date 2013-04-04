1b14cb7ce63eff48a7e94c5f2c5c6231040edcd0
Wouter Hulsbergen <wouterh@nikhef.nl>
2013-04-04 13:56:01 +0000

    bug fix in TrackCloneData::overlapFraction
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@154500 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 125d44a45bd3d220389f1543855d5b2163163855

--- LHCb/Tr/TrackKernel/TrackKernel/TrackCloneData.h
+++ LHCb/Tr/TrackKernel/TrackKernel/TrackCloneData.h
@@ -14,17 +14,28 @@ namespace  LHCb
   {
   public:
     enum HitType { T, TT, VeloR, VeloPhi, Velo, MaxType } ;
+    typedef unsigned long WeightType ;
+    typedef StaticArray<LHCb::LHCbID,64> LHCbIDs ;
     // constructor
     TrackCloneData(const LHCb::Track& track) ;
 
     // sort by number of LHCbIDs
     bool operator<( const TrackCloneData& rhs) const { return m_weight > rhs.m_weight ; }
-    //
+
+    // fraction of common hits
     double overlapFraction( const TrackCloneData& rhs, HitType type ) const ;
+
+    // number of common hits
     size_t nCommon( const TrackCloneData& rhs, HitType type ) const ;
 
+    // LHCb IDs of a certain type
+    const LHCbIDs& lhcbIDs( HitType type) const { return m_ids[type] ; }
+
     // set the weight for ordering
-    void setWeight( double w ) { m_weight = w ; }
+    void setWeight( WeightType w ) { m_weight = w ; }
+
+    // return the weight
+    WeightType weight() const { return m_weight ; }
 
     // return reference to track
     const LHCb::Track& track() const { return *m_track ; }
@@ -36,10 +47,9 @@ namespace  LHCb
     // we can probably do this with ranges since the lhcbids are
     // ordered by detector type. for now, let's just create containers
     //typedef Range_<LHCb::Track::LHCbIDContainer> LHCbIDs ;
-    typedef StaticArray<LHCb::LHCbID,64> LHCbIDs ;
     LHCbIDs m_ids[MaxType] ;
     const LHCb::Track* m_track ;
-    double m_weight ;
+    WeightType m_weight ;
   } ;
 }
   
