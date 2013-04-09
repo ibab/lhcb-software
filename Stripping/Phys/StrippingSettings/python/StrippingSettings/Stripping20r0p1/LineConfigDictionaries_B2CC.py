4591efc11ef03e4eb00de1d1beba8c6244b7030f
Francesca Dordei <francesca.dordei@cern.ch>
2013-04-09 17:55:22 +0000

    update LineConfigDictionaries_B2CC
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@154816 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: feeeda452d37ed1e6297dbd0f3241853c645b962

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p1/LineConfigDictionaries_B2CC.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p1/LineConfigDictionaries_B2CC.py
@@ -1,5 +1,5 @@
 # Line config dictionaries for B2CC WG for 2012 restripping
-BetaS = {         
+BetaS = {        
     'WGs'               : ['B2CC'],
     'BUILDERTYPE'       : 'B2JpsiXforBeta_sConf',
     'CONFIG'    : {
@@ -7,43 +7,43 @@ BetaS = {
                  ,       'BPVLTIME'                  :       0.2
                  ,       'DaughterPT'                :       1000
                  ,       'VCHI2PDOF'                 :       10
-                 ,       'Jpsi2MuMuPrescale'         :       0.1   #0.01
-		 ,	 'Jpsi2MuMuDetachedPrescale' :       1.0
-                 ,       'Bu2JpsiKPrescale'          :       0.45  #0.05
-                 ,       'Bd2JpsiKstarPrescale'      :       0.45   #0.03
-                 ,       'Bd2JpsiKsPrescale'         :       1.0   #0.3
-                 ,       'Bs2JpsiPhiPrescale'        :       1.0    #0.5 #0.15
-		 ,       'Bs2Jpsif0Prescale'         :       0.3   
-                 ,       'Bs2JpsiEtaPrescale'        :       1.0 
-		 ,       'Bs2JpsiEtapPrescale'       :       0.3 
-		 ,       'Bs2JpsiPi0Prescale'        :       0.8
-		 ,       'Bs2JpsiRho0Prescale'       :       0.3 
+                 ,       'Jpsi2MuMuPrescale'         :       0.04   #0.01
+                 ,     'Jpsi2MuMuDetachedPrescale'   :       1.0
+                 ,       'Bu2JpsiKPrescale'          :       0.2  #0.05
+                 ,       'Bd2JpsiKstarPrescale'      :       0.2   #0.03
+                 ,       'Bd2JpsiKsPrescale'         :       0.5   #0.3
+                 ,       'Bs2JpsiPhiPrescale'        :       0.5 #0.15
+                 ,       'Bs2Jpsif0Prescale'         :       0.15  
+                 ,       'Bs2JpsiEtaPrescale'        :       0.5
+                 ,       'Bs2JpsiEtapPrescale'       :       0.15
+                 ,       'Bs2JpsiPi0Prescale'        :       0.4
+                 ,       'Bs2JpsiRho0Prescale'       :       0.15
                          },
-	'STREAMS' : { 
-		'Dimuon' : [
-		'StrippingBetaSJpsi2MuMuLine',
- 		'StrippingBetaSJpsi2MuMuDetachedLine', 
- 		'StrippingBetaSBu2JpsiKPrescaledLine',
- 		'StrippingBetaSBs2JpsiPhiPrescaledLine', 
- 		'StrippingBetaSBs2JpsiPhiDetachedLine',
- 		'StrippingBetaSBd2JpsiKstarPrescaledLine', 
- 		'StrippingBetaSBd2JpsiKstarDetachedLine',
- 		'StrippingBetaSBd2JpsiKsPrescaledLine',
- 		'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
- 		'StrippingBetaSBs2Jpsif0NoIPDetachedLine',
- 		'StrippingBetaSBs2Jpsi4PiLine',
- 		'StrippingBetaSBs2JpsiEtaPrescaledLine',
- 		'StrippingBetaSBs2JpsiEtaDetachedLine',
- 		'StrippingBetaSBs2JpsiEtapPrescaledLine',
- 		'StrippingBetaSBs2JpsiEtapDetachedLine',
- 		'StrippingBetaSBd2JpsiPi0PrescaledLine',
- 		'StrippingBetaSBd2JpsiRho0PrescaledLine',
- 		'StrippingBetaSBd2JpsiRho0DetachedLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+        'StrippingBetaSJpsi2MuMuLine',
+         'StrippingBetaSJpsi2MuMuDetachedLine',
+         'StrippingBetaSBu2JpsiKPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiDetachedLine',
+         'StrippingBetaSBd2JpsiKstarPrescaledLine',
+         'StrippingBetaSBd2JpsiKstarDetachedLine',
+         'StrippingBetaSBd2JpsiKsPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPDetachedLine',
+         'StrippingBetaSBs2Jpsi4PiLine',
+         'StrippingBetaSBs2JpsiEtaPrescaledLine',
+         'StrippingBetaSBs2JpsiEtaDetachedLine',
+         'StrippingBetaSBs2JpsiEtapPrescaledLine',
+         'StrippingBetaSBs2JpsiEtapDetachedLine',
+         'StrippingBetaSBd2JpsiPi0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0DetachedLine'
+        ]
+    }   
     }
 
-# Neus 
+# Neus
 BetaSPsi2S = {
     'WGs'               : ['B2CC'],
     'BUILDERTYPE'   : 'B2Psi2SXConf',
@@ -66,13 +66,13 @@ BetaSPsi2S = {
                  'KstMassDown':  826,
                  'KstMassUp': 966,
                  'KstAPT':1500, #before 500
-                 'KstVFASPF':16, 
+                 'KstVFASPF':16,
                  'KstTRCHI2DOF':4,
                  'KstPIDK': 0, #before -2
                  'KstPIDpi': 10, #new 10
                  'KsVFASPF':20,
                  'KsBPVDLS':5,
-                 'incl_LinePrescale':0.1, #0.1 #before 0.5 #avui 0.2 
+                 'incl_LinePrescale':0.1, #0.1 #before 0.5 #avui 0.2
                  'incl_LinePostscale':1,
                  'BPVLTIME_detatched':0.15,
                  'BsMassCutDownPre':5000,     #ATTENTION before 5050
@@ -85,21 +85,21 @@ BetaSPsi2S = {
                  'ChKPT': 500 ,
                  'K_LinePrescale': 1, #0.5
                  'K_LinePostscale': 1,
-                 'Kstar_LinePrescale': 1, #0.5 #before 1 #ara 0.5 
+                 'Kstar_LinePrescale': 1, #0.5 #before 1 #ara 0.5
                  'Kstar_LinePostscale': 1,
                  'MINTREEPT2' : 1000,
                  'BKsVCHI2PDOF': 10,
                  'Ks_LinePrescale': 1,
                  'Ks_LinePostscale':1
                 },
-	'STREAMS' : { 
-		'Dimuon' : [
- 		'StrippingBetaSPsi2S_Bs2Psi2SPhiJpsiPiPiLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+         'StrippingBetaSPsi2S_Bs2Psi2SPhiJpsiPiPiLine'
+        ]
+    }   
     }
 
-#Neus 
+#Neus
 BetaSPsi2SMuMu = {
     'WGs'           : ['B2CC'],
     'BUILDERTYPE'   : 'Bs2Psi2SPhiMuMuConf',
@@ -164,14 +164,13 @@ BetaSPsi2SMuMu = {
 #                 'Ks_UnbiasedLinePrescale':1,
 #                 'Ks_UnbiasedLinePostscale':1
                 },
-	'STREAMS' : { 
-		'Dimuon' : [
- 		'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuDetatchedLine',
- 		'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuPrescaledLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+         'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuDetatchedLine',
+         'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuPrescaledLine'
+        ]
+    }   
     }
-
 
 
 
--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1p1/LineConfigDictionaries_B2CC.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1p1/LineConfigDictionaries_B2CC.py
@@ -1,6 +1,6 @@
 # Line config dictionaries for B2CC WG 2011 config dictionary
 
-BetaS = {         
+BetaS = {        
     'WGs'               : ['B2CC'],
     'BUILDERTYPE'       : 'B2JpsiXforBeta_sConf',
     'CONFIG'    : {
@@ -8,44 +8,44 @@ BetaS = {
                  ,       'BPVLTIME'                  :       0.2
                  ,       'DaughterPT'                :       1000
                  ,       'VCHI2PDOF'                 :       10
-                 ,       'Jpsi2MuMuPrescale'         :       0.02   #0.01
-		 ,	 'Jpsi2MuMuDetachedPrescale' :       1.0
-                 ,       'Bu2JpsiKPrescale'          :       0.08  #0.05
-                 ,       'Bd2JpsiKstarPrescale'      :       0.08   #0.03
-                 ,       'Bd2JpsiKsPrescale'         :       1.0   #0.3
-                 ,       'Bs2JpsiPhiPrescale'        :       0.15   #0.5 #0.15
-		 ,       'Bs2Jpsif0Prescale'         :       0.07   
-                 ,       'Bs2JpsiEtaPrescale'        :       0.22 
-		 ,       'Bs2JpsiEtapPrescale'       :       0.08 
-		 ,       'Bs2JpsiPi0Prescale'        :       0.2
-		 ,       'Bs2JpsiRho0Prescale'       :       0.05 
+                 ,       'Jpsi2MuMuPrescale'         :       0.008   #0.01
+         	 ,     'Jpsi2MuMuDetachedPrescale'   :       1.0
+                 ,       'Bu2JpsiKPrescale'          :       0.03  #0.05
+                 ,       'Bd2JpsiKstarPrescale'      :       0.03   #0.03
+                 ,       'Bd2JpsiKsPrescale'         :       0.5   #0.3
+                 ,       'Bs2JpsiPhiPrescale'        :       0.06   #0.5 #0.15
+                 ,       'Bs2Jpsif0Prescale'         :       0.02  
+                 ,       'Bs2JpsiEtaPrescale'        :       0.10
+                 ,       'Bs2JpsiEtapPrescale'       :       0.04
+                 ,       'Bs2JpsiPi0Prescale'        :       0.1
+                 ,       'Bs2JpsiRho0Prescale'       :       0.02
                          },
-	'STREAMS' : { 
-		'Dimuon' : [
-		'StrippingBetaSJpsi2MuMuLine',
- 		'StrippingBetaSJpsi2MuMuDetachedLine', 
- 		'StrippingBetaSBu2JpsiKPrescaledLine',
- 		'StrippingBetaSBs2JpsiPhiPrescaledLine', 
- 		'StrippingBetaSBs2JpsiPhiDetachedLine',
- 		'StrippingBetaSBd2JpsiKstarPrescaledLine', 
- 		'StrippingBetaSBd2JpsiKstarDetachedLine',
- 		'StrippingBetaSBd2JpsiKsPrescaledLine',
- 		'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
- 		'StrippingBetaSBs2Jpsif0NoIPDetachedLine',
- 		'StrippingBetaSBs2Jpsi4PiLine',
- 		'StrippingBetaSBs2JpsiEtaPrescaledLine',
- 		'StrippingBetaSBs2JpsiEtaDetachedLine',
- 		'StrippingBetaSBs2JpsiEtapPrescaledLine',
- 		'StrippingBetaSBs2JpsiEtapDetachedLine',
- 		'StrippingBetaSBd2JpsiPi0PrescaledLine',
- 		'StrippingBetaSBd2JpsiRho0PrescaledLine',
- 		'StrippingBetaSBd2JpsiRho0DetachedLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+        'StrippingBetaSJpsi2MuMuLine',
+         'StrippingBetaSJpsi2MuMuDetachedLine',
+         'StrippingBetaSBu2JpsiKPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiDetachedLine',
+         'StrippingBetaSBd2JpsiKstarPrescaledLine',
+         'StrippingBetaSBd2JpsiKstarDetachedLine',
+         'StrippingBetaSBd2JpsiKsPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPDetachedLine',
+         'StrippingBetaSBs2Jpsi4PiLine',
+         'StrippingBetaSBs2JpsiEtaPrescaledLine',
+         'StrippingBetaSBs2JpsiEtaDetachedLine',
+         'StrippingBetaSBs2JpsiEtapPrescaledLine',
+         'StrippingBetaSBs2JpsiEtapDetachedLine',
+         'StrippingBetaSBd2JpsiPi0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0DetachedLine'
+        ]
+    }   
     }
 
 
-# Neus 
+# Neus
 BetaSPsi2S = {
     'WGs'               : ['B2CC'],
     'BUILDERTYPE'   : 'B2Psi2SXConf',
@@ -68,13 +68,13 @@ BetaSPsi2S = {
                  'KstMassDown':  826,
                  'KstMassUp': 966,
                  'KstAPT':1500, #before 500
-                 'KstVFASPF':16, 
+                 'KstVFASPF':16,
                  'KstTRCHI2DOF':4,
                  'KstPIDK': 0, #before -2
                  'KstPIDpi': 10, #new 10
                  'KsVFASPF':20,
                  'KsBPVDLS':5,
-                 'incl_LinePrescale':0.1, #0.1 #before 0.5 #avui 0.2 
+                 'incl_LinePrescale':0.1, #0.1 #before 0.5 #avui 0.2
                  'incl_LinePostscale':1,
                  'BPVLTIME_detatched':0.15,
                  'BsMassCutDownPre':5000,     #ATTENTION before 5050
@@ -87,21 +87,21 @@ BetaSPsi2S = {
                  'ChKPT': 500 ,
                  'K_LinePrescale': 1, #0.5
                  'K_LinePostscale': 1,
-                 'Kstar_LinePrescale': 1, #0.5 #before 1 #ara 0.5 
+                 'Kstar_LinePrescale': 1, #0.5 #before 1 #ara 0.5
                  'Kstar_LinePostscale': 1,
                  'MINTREEPT2' : 1000,
                  'BKsVCHI2PDOF': 10,
                  'Ks_LinePrescale': 1,
                  'Ks_LinePostscale':1
                 },
-	'STREAMS' : { 
-		'Dimuon' : [
- 		'StrippingBetaSPsi2S_Bs2Psi2SPhiJpsiPiPiLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+         'StrippingBetaSPsi2S_Bs2Psi2SPhiJpsiPiPiLine'
+        ]
+    }   
     }
 
-#Neus 
+#Neus
 BetaSPsi2SMuMu = {
     'WGs'           : ['B2CC'],
     'BUILDERTYPE'   : 'Bs2Psi2SPhiMuMuConf',
@@ -166,12 +166,12 @@ BetaSPsi2SMuMu = {
 #                 'Ks_UnbiasedLinePrescale':1,
 #                 'Ks_UnbiasedLinePostscale':1
                 },
-	'STREAMS' : { 
-		'Dimuon' : [
- 		'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuDetatchedLine',
- 		'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuPrescaledLine'
-		]
-	}	
+    'STREAMS' : {
+        'Dimuon' : [
+         'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuDetatchedLine',
+         'StrippingBetaSPsi2SMuMu_Bs2Psi2SPhiMuMuPrescaledLine'
+        ]
+    }   
     }
 
 
