8aa9a4d0886e901b39a422f354727d53dfc88a89
Vanya Belyaev <ivan.belyaev@cern.ch>
2013-04-13 11:18:12 +0000

    prepare v22r5
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@154951 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8b9012a214c4f09a9c7a6e38d77ddb31e6f3abbb

--- Bender/Phys/BenderTools/python/BenderTools/MicroDST.py
+++ Bender/Phys/BenderTools/python/BenderTools/MicroDST.py
@@ -61,194 +61,41 @@ logger = getLogger( __name__ )
 ## Configure node killing agents for uDST processing
 #  @author VanyaBELYAEV Ivan.Belyaev@itep.ru
 #  @date   2011-10-05
-def killDAQ ( nodes = [ '/Event/DAQ' , '/Event/pRec' ] )  :
+def killDAQ ( nodes = [ '/Event/DAQ' , '/Event/pRec' ] , logger_ = None )  :
     """
     Configure node killing agents for uDST processing
     """
-    from Configurables import EventNodeKiller
-    killer = EventNodeKiller ( "KillDAQ" )
-    killer.Nodes += nodes 
-    try : 
-        from Gaudi.Configuration import getConfigurable
-        conf   = getConfigurable ( 'DaVinciEventInitSeq' ) 
-        conf.Members.insert ( 0 , killer ) 
-        logger.info  ( "Add           killer agent for: %s" % killer.Nodes )
-    except:
-        logger.error ( "Failed to add killer agent for: %s" % killer.Nodes )
+    #
+    from PhysConf.MicroDst import killDAQ as _killDAQ
+    #
+    return _killDAQ ( nodes , logger ) 
 
 # ======================================================================================
 ## configure various uDst settings from RootInTES 
-def uDstConf ( rootInTes ) :
+def uDstConf ( rootInTes           ,
+               nodes     = [ '/Event/DAQ'  ,
+                             '/Event/pRec' ] ,
+               logger_   = None )  :
     """
     Various settings for mDst
     
     """
+    from PhysConf.MicroDst import uDstConf as _uDstConf 
     #
-    ## pick up all goodies from Juan
-    #
-    import MicroDSTConf.TriggerConfUtils as TCU
-    #
-    #
-    ## 'rootify'
-    #
-    if 0 != rootInTes.find ( '/Event/') : rootInTes = '/Event/' + rootInTes 
-    
-    rootInTes = TCU.fixTrunk ( rootInTes )
-    
-    logger.info("Reconfigure uDST to use    RootInTES='%s'" % rootInTes )
-    
-    #
-    ## configure L0,Hlt, etc using Juan's functions:
-    #
-    TCU.configureL0AndHltDecoding ( rootInTes )
-    logger.info("Configure L0&HLT decoding  RootInTES='%s'" % rootInTes )
-
-    from Configurables import DataOnDemandSvc
-    dod = DataOnDemandSvc( Dump = True )
-    #
-    ## extra configuration:
-    #
-    #   it is more relevant for Stripping-17 data,
-    #   however it is backard compatible with other uDsts 
-    #
-    
-    #
-    ## 0. Kill Event/DAQ
-    #
-    from Gaudi.Configuration import appendPostConfigAction
-    appendPostConfigAction ( killDAQ )
-    logger.info ( "Configure DAQ-killer agent" ) 
-    
-    # 
-    #
-    ## 1. Copy/Link ODIN, RawEvent and RecSummary
-    #
-    from Configurables import Gaudi__DataLink as Link
-    rawEvt  = Link ( 'LinkRawEvent'                      ,
-                     What   =  '/Event/Trigger/RawEvent' ,
-                     Target = rootInTes + 'DAQ/RawEvent' )
-    odin    = Link ( 'LinkODIN'                          ,
-                     What   =          '/Event/DAQ/ODIN' ,
-                     Target = rootInTes +     'DAQ/ODIN' )
-    summary = Link ( 'LinkSummary'                       ,
-                     What   =      '/Event/Rec/Summary'  ,
-                     Target = rootInTes + 'Rec/Summary'  )    
-    header  = Link ( 'LinkHeader'                        ,
-                     What   =       '/Event/Rec/Header'  ,
-                     Target = rootInTes +  'Rec/Header'  )
-    
-    dod.AlgMap [ rawEvt  . Target ] = rawEvt 
-    dod.AlgMap [ odin    . Target ] = odin
-    dod.AlgMap [ summary . Target ] = summary
-    dod.AlgMap [ header  . Target ] = header
-    
-    logger.info("Configure ODIN,Raw&Summary RootInTES='%s'" % rootInTes )
-    #
-    ##
-    #
-    from Configurables import OdinTimeDecoder, ODINDecodeTool
-    odinDec = OdinTimeDecoder ( )
-    odinDec.addTool( ODINDecodeTool, 'ODINDecodeTool' )
-    odinDec.ODINDecodeTool.RawEventLocations = [
-        rootInTes + 'DAQ/RawEvent' ] + odinDec.ODINDecodeTool.RawEventLocations
-
-    from Configurables import DecodePileUpData
-    pileupDec = DecodePileUpData()
-    pileupDec.RawEventLocation              = rootInTes + 'DAQ/RawEvent' 
-
-
+    return _uDstConf ( rootInTes , nodes , logger ) 
 
 # ======================================================================================
 ## configure various uDst settings from RootInTES (verision for Stripping13b processing)
 #  @attention it is valid only for Stripping13b
-def uDstConf13b ( rootInTes ) :
+def uDstConf13b ( rootInTes , logger_ = None ) :
     """
     Various settings for mDst (version for Stirpping13b processing)
     
     """
+    from PhysConf.MicroDst import uDstConf as _uDstConf13b 
     #
-    ## warning message
-    # 
-    logger.warnig("The configuration for procession of Stripping13b data is activated!")
-    #
-    
-    ## pick up all goodies from Juan
-    #
-    import MicroDSTConf.TriggerConfUtils as TCU
-    #
-    #
-    ## 'rootify'
-    #
-    if 0 != rootInTes.find ( '/Event/') : rootInTes = '/Event/' + rootInTes 
-    
-    rootInTes = TCU.fixTrunk ( rootInTes )
-    
-    logger.info("Reconfigure uDST to use    RootInTES='%s'" % rootInTes )
-    
-    #
-    ## configure L0,Hlt, etc using Juan's functions:
-    #
-    TCU.configureL0AndHltDecoding ( rootInTes )
-    logger.info("Configure L0&HLT decoding  RootInTES='%s'" % rootInTes )
+    return _uDstConf13b ( rootInTes , nodes , logger ) 
 
-    from Configurables import DataOnDemandSvc
-    dod = DataOnDemandSvc( Dump = True )
-    #
-    ## extra configuration:
-    #
-    #   it is more relevant for Stripping-17 data,
-    #   however it is backard compatible with other uDsts 
-    #
-    
-    #
-    ## 0. Kill Event/DAQ
-    #
-    def _killDAQ_13b_ ( ) :
-        return  killDAQ ( nodes = [ '/Event/pRec' ] ) 
-            
-    from Gaudi.Configuration import appendPostConfigAction
-    appendPostConfigAction ( _killDAQ_13b_ )
-    logger.info ( "Configure DAQ-killer agent" ) 
-    
-    # 
-    #
-    ## 1. Copy/Link ODIN, RawEvent and RecSummary
-    #     note the swap for   Target <--> What
-    from Configurables import Gaudi__DataLink as Link
-    rawEvt  = Link ( 'LinkRawEvent'                        ,
-                     Target   =  '/Event/Trigger/RawEvent' ,
-                     What     = rootInTes + 'DAQ/RawEvent' )
-    odin    = Link ( 'LinkODIN'                            ,
-                     Target   =          '/Event/DAQ/ODIN' ,
-                     Root     = rootInTes +     'DAQ/ODIN' )
-    summary = Link ( 'LinkSummary'                         ,
-                     Target   =      '/Event/Rec/Summary'  ,
-                     What     = rootInTes + 'Rec/Summary'  )    
-    header  = Link ( 'LinkHeader'                          ,
-                     Target   =       '/Event/Rec/Header'  ,
-                     Root     = rootInTes +  'Rec/Header'  )
-    
-    dod.AlgMap [ rawEvt  . Target ] = rawEvt 
-    dod.AlgMap [ odin    . Target ] = odin
-    dod.AlgMap [ summary . Target ] = summary
-    dod.AlgMap [ header  . Target ] = header
-
-    #
-    logger.info("Configure ODIN,Raw&Summary RootInTES='%s'" % rootInTes )
-    #
-    ##
-    #
-    from Configurables import OdinTimeDecoder, ODINDecodeTool
-    odinDec = OdinTimeDecoder ( )
-    odinDec.addTool( ODINDecodeTool, 'ODINDecodeTool' )
-    odinDec.ODINDecodeTool.RawEventLocations = [
-        rootInTes + 'DAQ/RawEvent' ] + odinDec.ODINDecodeTool.RawEventLocations
-    
-    from Configurables import DecodePileUpData
-    pileupDec = DecodePileUpData()
-    pileupDec.RawEventLocation              = rootInTes + 'DAQ/RawEvent' 
-
-        
 # =============================================================================
 if __name__ == '__main__' :
     print '*'*120
