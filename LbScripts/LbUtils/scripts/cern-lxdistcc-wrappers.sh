7907879d4517fedf1380544c73d31648095c1aeb
Marco Clemencic <marcocle@cern.ch>
2013-04-16 08:05:54 +0000

    added support for gcc 4.8.0 and clang 3.2
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@155053 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8eeafd20162dac86bf7d24520deb92b286a3098a

--- LbScripts/LbUtils/scripts/cern-lxdistcc-wrappers.sh
+++ LbScripts/LbUtils/scripts/cern-lxdistcc-wrappers.sh
@@ -8,46 +8,76 @@ _self=${0##*/}
 hostos() {
     arch=$(uname -i)
     case $(lsb_release -si) in
-	ScientificCERNSLC)
-	    os=slc
-	    vers=$(lsb_release -sr | cut -d. -f1)
-	    ;;
-	*)
-	    os=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
-	    vers=$(lsb_release -sr)
+        ScientificCERNSLC)
+            os=slc
+            vers=$(lsb_release -sr | cut -d. -f1)
+            ;;
+        *)
+            os=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
+            vers=$(lsb_release -sr)
     esac
     echo $arch-$os$vers 
 }
 
 setup_gcc() {
-	_version=${_self##*-}
-	_platform=${LCG_hostos:-$(hostos)}
+        _version=$1
+        _platform=${LCG_hostos:-$(hostos)}
         _external=${LCG_external_area:-/afs/cern.ch/sw/lcg/external}
-	
-	_prefix="${_external}/gcc/${_version}"
-	_bin="${_prefix}/${_platform}/bin"
-	_lib="${_prefix}/${_platform}/lib64"
-	LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed 's-[^:]*/gcc/[^:]*:\?--g')
-	LD_LIBRARY_PATH="${_lib}${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
-	PATH="${_bin}${PATH:+:}${PATH}"
-	COMPILER_PATH="${_prefix}/${_platform}/lib/gcc/x86_64-unknown-linux-gnu/${_version}"
 
-	export LD_LIBRARY_PATH
-	export PATH
-	export COMPILER_PATH
+        _prefix="${_external}/gcc/${_version}"
+        _bin="${_prefix}/${_platform}/bin"
+        _lib="${_prefix}/${_platform}/lib64"
+        LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed 's-[^:]*/gcc/[^:]*:\?--g')
+        LD_LIBRARY_PATH="${_lib}${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
+        PATH="${_bin}${PATH:+:}${PATH}"
+        COMPILER_PATH="${_prefix}/${_platform}/lib/gcc/x86_64-unknown-linux-gnu/${_version}"
+
+        export LD_LIBRARY_PATH
+        export PATH
+        export COMPILER_PATH
+}
+
+setup_clang() {
+        setup_gcc 4.6.3
+
+        _version=$1
+        _platform=${LCG_hostos:-$(hostos)}
+        _external=${LCG_external_area:-/afs/cern.ch/sw/lcg/external}
+
+        _prefix="${_external}/llvm/${_version}"
+        _bin="${_prefix}/${_platform}/bin"
+        _lib="${_prefix}/${_platform}/lib64"
+        LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed 's-[^:]*/llvm/[^:]*:\?--g')
+        LD_LIBRARY_PATH="${_lib}${LD_LIBRARY_PATH:+:}${LD_LIBRARY_PATH}"
+        PATH="${_bin}${PATH:+:}${PATH}"
+
+        # clang does not have the nice lcg-* links
+        _self=${_self#*-}
+        _self=${_self%-*}
+
+        export LD_LIBRARY_PATH
+        export PATH
+        export COMPILER_PATH
 }
 
 case ${_self} in
 
-        lcg-*-4.3.5|lcg-*-4.3.6|lcg-*-4.5.3|lcg-*-4.6.2|lcg-*-4.7.2)
-		
-                setup_gcc
+        lcg-[cg]++-[0-9].[0-9].[0-9]|lcg-gcc-[0-9].[0-9].[0-9])
+
+                _version=${_self##*-}
+                setup_gcc $_version
                 ;;
 
-	*)
-		echo "E: Unsupported compiler '${_self}', please contact <marco.clemencic@cern.ch>"
-		exit 100
-		;;
+        lcg-clang-*|lcg-clang++-*)
+
+                _version=${_self##*-}
+                setup_clang $_version
+                ;;
+
+        *)
+                echo "E: Unsupported compiler '${_self}', please contact <marco.clemencic@cern.ch>"
+                exit 100
+                ;;
 esac
 
 exec ${_bin}/${_self} "$@"
