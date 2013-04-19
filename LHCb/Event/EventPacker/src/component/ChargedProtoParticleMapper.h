7c2c93b39e55122ef1528f494666f91bdae88268
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-04-19 11:21:45 +0000

    ! 2013-04-19 - Chris Jones  - Add a new mapper tool, ChargedProtoParticleMapper, for charged    ProtoParticles that implements recalibration of the PID information    on the fly.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@155222 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 754691324977fe72e14ed4cca03126f6da2248f6

--- LHCb/Event/EventPacker/src/component/ChargedProtoParticleMapper.h
+++ LHCb/Event/EventPacker/src/component/ChargedProtoParticleMapper.h
@@ -1,5 +1,6 @@
-#ifndef SRC_TrackClustersMapper_H
-#define SRC_TrackClustersMapper_H 1
+
+#ifndef SRC_ChargedProtoParticleMapper_H
+#define SRC_ChargedProtoParticleMapper_H 1
 
 #include <string>
 #include <map>
@@ -8,29 +9,32 @@
 #include "MapperToolBase.h"
 
 // Event model
-#include "Event/PackedCluster.h"
+#include "Event/PackedProtoParticle.h"
+
+// Boost
+#include <boost/assign/list_of.hpp>
 
 class IJobOptionsSvc;
 
-/** @class TrackClustersMapper TrackClustersMapper.h
+/** @class ChargedProtoParticleMapper ChargedProtoParticleMapper.h
  *
- *  Tool for mappings for Track clusters
+ *  Tool for mappings for charged ProtoParticles
  *
  * @author Chris Jones
- * @date 26/03/2012
+ * @date 16/04/2013
  */
-class TrackClustersMapper : public MapperToolBase
+class ChargedProtoParticleMapper : public MapperToolBase
 {
 
 public:
 
   /// Standard constructor
-  TrackClustersMapper( const std::string& type,
-                       const std::string& name,
-                       const IInterface* parent );
+  ChargedProtoParticleMapper( const std::string& type,
+                              const std::string& name,
+                              const IInterface* parent );
 
   /// Destructor
-  virtual ~TrackClustersMapper();
+  virtual ~ChargedProtoParticleMapper();
 
 public:
 
@@ -58,14 +62,20 @@ private:
     NodeTypeMap::const_iterator it = m_nodeTypeMap.find( fixPath(path) );
     return ( it != m_nodeTypeMap.end() );
   }
-  
+
   /// Check the node mappings are updated for the given path stream
   void updateNodeTypeMap( const std::string & path );
 
-  /// Get the location of the packed clusters for the given stream
-  std::string packedClustersLocation( const std::string & stream ) const
+  /// Get the location of the packed ProtoParticles for the given stream
+  std::string packedProtoLocation( const std::string & stream ) const
   {
-    return stream + "/" + LHCb::PackedClusterLocation::Default;
+    return stream + "/" + LHCb::PackedProtoParticleLocation::Charged;
+  }
+
+  /// Get the location of the packed ProtoParticles for the given stream
+  std::string protoLocation( const std::string & stream ) const
+  {
+    return stream + "/" + LHCb::ProtoParticleLocation::Charged;
   }
 
 private:
@@ -77,12 +87,21 @@ private:
   typedef std::map<std::string,std::string> NodeTypeMap;
   NodeTypeMap m_nodeTypeMap;
 
-  /// Unpacker class type
-  std::string m_unpackerType;
-  
+  /// List of track types for PID recalibration
+  std::vector<std::string> m_tkTypes;
+
+  /// List of hypo types for PID recalibration
+  std::vector<std::string> m_pidTypes;
+
+  /// ANN PID recalibration tuning
+  std::string m_pidTune;
+
+  /// String match regex for the Brunel application version for rerunning the ANNPID
+  std::string m_regex;
+
   /// Outputlevel for unpackers created
   int m_unpackersOutputLevel;
 
 };
 
-#endif // SRC_TrackClustersMapper_H
+#endif // SRC_ChargedProtoParticleMapper_H
