89215b5dd136b66d5963f8a9c7e62b90e213cb91
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-04-19 11:26:33 +0000

    Add a new simple little algorithm, ApplicationVersionFilter, that    selects/rejects events based on a regex filter on the application version.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@155224 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5a23657b6b123def0e0b38bc549b534990a01081

---
+++ LHCb/Kernel/LHCbAlgs/src/ApplicationVersionFilter.cpp
@@ -0,0 +1,69 @@
+// $Id: ApplicationVersionFilter.cpp,v 1.3 2009-11-10 09:27:51 jonrob Exp $
+// Include files
+
+// from Gaudi
+#include "GaudiKernel/AlgFactory.h"
+
+// local
+#include "ApplicationVersionFilter.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : ApplicationVersionFilter
+//
+// 2009-11-06 : Chris Jones
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( ApplicationVersionFilter )
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+  ApplicationVersionFilter::ApplicationVersionFilter( const std::string& name,
+                                                      ISvcLocator* pSvcLocator)
+    : GaudiAlgorithm ( name , pSvcLocator )
+{
+  declareProperty( "VersionRegex",   m_regex = "" );
+  declareProperty( "HeaderLocation", m_loc = LHCb::ProcessHeaderLocation::Rec );
+}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+ApplicationVersionFilter::~ApplicationVersionFilter() {}
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode ApplicationVersionFilter::execute()
+{
+  bool OK = true;
+
+  try
+  {
+    if ( !m_regex.empty() )
+    {
+      const LHCb::ProcessHeader * header = getIfExists<LHCb::ProcessHeader>(m_loc);
+      if ( header )
+      {
+        // Apply the regex to the application version
+        OK = boost::regex_match( header->applicationVersion().c_str(),
+                                 boost::regex(m_regex) );
+      }
+      else
+      {
+        OK = false;
+      }
+    }
+  }
+  catch ( const GaudiException & )
+  {
+    OK = false;
+  }
+  
+  setFilterPassed(OK);
+
+  return StatusCode::SUCCESS;
+}
+
+//=============================================================================
---
+++ LHCb/Kernel/LHCbAlgs/src/ApplicationVersionFilter.h
@@ -0,0 +1,41 @@
+// $Id: ApplicationVersionFilter.h,v 1.3 2009-11-10 09:27:51 jonrob Exp $
+#ifndef ApplicationVersionFilter_H 
+#define ApplicationVersionFilter_H 1
+
+// from Gaudi
+#include "GaudiAlg/GaudiAlgorithm.h"
+
+// Event
+#include "Event/ProcessHeader.h"
+
+// boost
+#include <boost/regex.hpp>
+
+/** @class ApplicationVersionFilter ApplicationVersionFilter.h
+ *  
+ *  Checks the application version from the given ProcessHeader,
+ *  comparing it to a given regex pattern to pass/reject the event.
+ *
+ *  @author Chris Jones
+ *  @date   18-04-2013
+ */
+class ApplicationVersionFilter : public GaudiAlgorithm 
+{
+
+public: 
+
+  /// Standard constructor
+  ApplicationVersionFilter( const std::string& name, ISvcLocator* pSvcLocator );
+
+  virtual ~ApplicationVersionFilter( ); ///< Destructor
+
+  virtual StatusCode execute();    ///< Algorithm execution
+
+private:
+
+  std::string m_regex;   ///< String match regex
+  std::string m_loc;     ///< DataObject location in TES
+
+};
+
+#endif // DATAOBJECTVERSIONFILTER_H
