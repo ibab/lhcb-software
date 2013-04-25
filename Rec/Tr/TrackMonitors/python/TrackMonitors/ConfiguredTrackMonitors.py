96015243a544a74bc8439bf5111f81e1038a3611
Sascha Stahl <sascha.stahl@cern.ch>
2013-04-25 16:33:04 +0000

    Added protections to ConfiguredTrackMonitors in case detector does not exist.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@155635 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 582ff2e19d8730dfea71a81cb6b776a213be9ea7

--- Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
+++ Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
@@ -11,7 +11,13 @@ from Configurables import (RecSysConf, RecMoniConf, TrackSys)
 
 def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
                                    HistoPrint = False):
+
+    # figure out detectors
     seq = GaudiSequencer(Name)
+    from Configurables import LHCbApp
+    if hasattr(LHCbApp(),"Detectors"):
+        if LHCbApp().isPropertySet("Detectors"):
+            subDets = LHCbApp().getProp("Detectors")
     seq.Members.append( TrackMonitor(HistoPrint=HistoPrint) )
     seq.Members.append( TrackDiMuonMonitor(HistoPrint=HistoPrint) )
     seq.Members.append( TrackVertexMonitor(HistoPrint=HistoPrint) )
@@ -24,22 +30,29 @@ def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
         seq.Members.append( TrackV0Monitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackFitMatchMonitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackAlignMonitor(HistoPrint=HistoPrint) )
-        seq.Members.append( TrackITOverlapMonitor(HistoPrint=HistoPrint) )
-        seq.Members.append( TrackVeloOverlapMonitor(HistoPrint=HistoPrint) )
-        seq.Members.append( TTTrackMonitor(HistoPrint=HistoPrint) )
-        seq.Members.append( ITTrackMonitor(HistoPrint=HistoPrint) )
+        if ("IT" in subDets):   seq.Members.append( TrackITOverlapMonitor(HistoPrint=HistoPrint) )
+        if ("Velo" in subDets): seq.Members.append( TrackVeloOverlapMonitor(HistoPrint=HistoPrint) )
+        if ("TT" in subDets):   seq.Members.append( TTTrackMonitor(HistoPrint=HistoPrint) )
+        if ("IT" in subDets):   seq.Members.append( ITTrackMonitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackPV2HalfAlignMonitor(HistoPrint=HistoPrint) )
-	if RecMoniConf().expertHistos() : 
-            seq.Members.append( HitEffPlotter(HistoPrint=HistoPrint) )
-
+	if RecMoniConf().expertHistos() :
+            if [det for det in ['Velo', 'TT', 'IT', 'OT'] if det in subDets]:
+                seq.Members.append( HitEffPlotter(HistoPrint=HistoPrint) )
+            #else:
+            #    log.warning("HitEffPlotter not defined for upgrade")
         if "CALO" in RecSysConf().RecoSequence:
-            seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal', HistoPrint=HistoPrint) )
-            #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
-            seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
-            seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
+            if ("Ecal" in subDets):
+                seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal', HistoPrint=HistoPrint) )
+            #if ("Hcal" in subDets):
+                #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
+            if ("Spd" in subDets):
+                seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
+            if ("Prs" in subDets):
+                seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
 
         if "MUON" in RecSysConf().RecoSequence:
-            seq.Members.append(TrackMuonMatchMonitor("TrackMuonMatchMonitor", HistoPrint=HistoPrint))
+            if ("Muon" in subDets):
+                seq.Members.append(TrackMuonMatchMonitor("TrackMuonMatchMonitor", HistoPrint=HistoPrint))
 
     return seq
     
