9d557ca5630a6435ceb6431826784681ec1e0e00
Lucio Anderlini <lucio.anderlini@cern.ch>
2013-05-07 07:00:26 +0000

    Fixed 2 BandQ problems in Stripping20r(0,1)p1: zero-retention Betac and HeavyBaryon(s) name.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@156247 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e9627e1fe25182e96c3be38ce5d89c0e49dc1e36

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p1/LineConfigDictionaries_BandQ.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p1/LineConfigDictionaries_BandQ.py
@@ -5,7 +5,7 @@
 
 from GaudiKernel.SystemOfUnits import *
 
-HeavyBaryons = {
+HeavyBaryon = {
     'BUILDERTYPE'  : 'HeavyBaryonsConf',
     'CONFIG'       : {    'TRCHI2DOF'             :       4.,
                           'PionPIDK'              :       5.,
@@ -62,30 +62,60 @@ ChiCJPsiGammaConv = {
     }                                                  
 
 
+# OLD DICTIONARY USED ON STRIPPING20r1p1, TYPO IN Hlt2IncPhiDecision
+#Betac2PhiP = {
+#    'BUILDERTYPE'  : 'StrippingBetac2PhiPConf',
+#    'CONFIG'       : {
+#                    'Daug_TRCHI2DOF_MAX'        : 10.     
+#                  , 'Daug_P_MIN'                : 1200.0*MeV 
+#                  , 'Daug_PT_MIN'               : 200.0*MeV
+#                  , 'Daug_MIPDV'                : 0.0      # mm
+#                  , 'Proton_PIDpi_MIN'          : 10.0
+#                  , 'Proton_PIDK_MIN'           :  0.0 
+#                  , 'Phi_WIN'                   : 20.0 * MeV
+#                  , 'Phi_PT'                    : 1700.0*MeV         
+#                  , 'Betac_AM_MIN'              : 1950.0 * MeV 
+#                  , 'Betac_AM_MAX'              : 3300.0 * MeV 
+#                  , 'Betac_BPVDIRA_MIN'         : 0.999   
+#                  , 'Betac_VCHI2VDOF_MAX'       : 10.0   
+#                  , 'Betac_BPVLTIME_MIN'        : 0.0 * ns
+#                  , 'Betac_BPVLTIME_MAX'        : 0.06 * ns
+#                  , 'Hlt2IncPhiDecision'        : "HLT_PASS_RE('Hlt2IncPhiDecsion')"
+#                  , 'PrescaleBetac2PhiP'        : 1.0
+#                  , 'PostscaleBetac2PhiP'       : 1.0
+#                 },
+#'STREAMS' : ['CharmCompleteEvent'],
+#'WGs' : ['BandQ']
+#}
+#
+
 Betac2PhiP = {
     'BUILDERTYPE'  : 'StrippingBetac2PhiPConf',
     'CONFIG'       : {
                     'Daug_TRCHI2DOF_MAX'        : 10.     
-                  , 'Daug_P_MIN'                : 1200.0*MeV 
-                  , 'Daug_PT_MIN'               : 200.0*MeV
-                  , 'Daug_MIPDV'                : 0.0      # mm
-                  , 'Proton_PIDpi_MIN'          : 10.0
-                  , 'Proton_PIDK_MIN'           :  0.0 
-                  , 'Phi_WIN'                   : 20.0 * MeV
-                  , 'Phi_PT'                    : 1700.0*MeV         
+                  , 'Daug_P_MIN'                : 10000.0 * MeV 
+                  , 'Daug_PT_MIN'               : 400.0 * MeV
+                  , 'Daug_MIPDV'                : 0.05 * mm
+                  , 'Proton_PIDpi_MIN'          : 20.0
+                  , 'Proton_PIDK_MIN'           : 10.0 
+                  , 'Phi_WIN'                   : 10.0 * MeV
+                  , 'Phi_PT'                    : 0.0 * MeV         
                   , 'Betac_AM_MIN'              : 1950.0 * MeV 
-                  , 'Betac_AM_MAX'              : 3300.0 * MeV 
-                  , 'Betac_BPVDIRA_MIN'         : 0.999   
+                  , 'Betac_AM_MAX'              : 2800.0 * MeV 
+                  , 'Betac_BPVDIRA_MIN'         : 0.9999
                   , 'Betac_VCHI2VDOF_MAX'       : 10.0   
                   , 'Betac_BPVLTIME_MIN'        : 0.0 * ns
-                  , 'Betac_BPVLTIME_MAX'        : 0.06 * ns
-                  , 'Hlt2IncPhiDecision'        : "HLT_PASS_RE('Hlt2IncPhiDecsion')"
+                  , 'Betac_BPVLTIME_MAX'        : 0.006 * ns
+                  , 'Hlt2IncPhiDecision'        : "HLT_PASS_RE('Hlt2IncPhiDecision')"
                   , 'PrescaleBetac2PhiP'        : 1.0
                   , 'PostscaleBetac2PhiP'       : 1.0
                  },
 'STREAMS' : ['CharmCompleteEvent'],
 'WGs' : ['BandQ']
 }
+
+
+
 
 
 BB2DMuNuX = {
