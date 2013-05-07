c21cf4f4f731252587a0243461a6e2f711ff3636
Paul Seyfert <paul.seyfert@mib.infn.it>
2013-05-07 12:10:13 +0000

    remove code which was commented out
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@156279 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a9fffcd6dca62363ae60e3c2cbd08e06559af4ae

--- Rec/Tf/TrackSys/python/TrackSys/RecoTracking.py
+++ Rec/Tf/TrackSys/python/TrackSys/RecoTracking.py
@@ -323,8 +323,6 @@ def RecoTracking(exclude=[]):
       if "GhostProbability" in extraInfos :
          from Configurables import TrackAddNNGhostId, TrackNNGhostId
          nn = TrackAddNNGhostId()
-         #nn.addTool( TrackNNGhostId, name='TrackNNGhostId' )
-         #nn.TrackNNGhostId.IsMC2012Tuning = True
          GaudiSequencer("TrackAddExtraInfoSeq").Members += [ nn ]
          
    track.DetectorList += ["EraseExtraInformation"]
