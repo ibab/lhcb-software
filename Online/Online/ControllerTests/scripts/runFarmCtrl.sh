c66103400cf54fec9699b3a83d4525031c5a122f
Markus Frank <frankb@cern.ch>
2013-05-13 19:26:56 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@156701 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 3d8bb0e7662221613db8639578a807a64f895ccb

---
+++ Online/Online/ControllerTests/scripts/runFarmCtrl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+. ../cmt/setup.x86_64-slc5-gcc46-dbg.vars
+echo "UTGID=${UTGID}"
+export UTGID=${UTGID}
+exec -a ${UTGID} ${CONTROLLERROOT}/${CMTCONFIG}/fsm_ctrl.exe $*
--- Online/Online/ControllerTests/scripts/runFarmTask.sh
+++ Online/Online/ControllerTests/scripts/runFarmTask.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #
+echo `pwd`
+echo `pwd`
+echo `pwd`  $*
 . ../cmt/setup.x86_64-slc5-gcc46-dbg.vars
-. preamble.sh
+. ./preamble.sh
 export UTGID=${UTGID}
 ##echo $UTGID -- $* Invoking script: ${2}.sh
 . ./${2}.sh
