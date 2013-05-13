a9b9627f488fcdd9c2903895a6c97aa373bda806
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-05-13 22:50:52 +0000

    - TupleToolVELOClusters   . Fix inappropriate return values causing compilation warnings with icc11.   . Do not load the RecSummary object, as not actually used.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@156703 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5080b262e8dfa50c903e4fd06ffdabe20642370a

--- Analysis/Phys/DecayTreeTupleReco/src/TupleToolVELOClusters.cpp
+++ Analysis/Phys/DecayTreeTupleReco/src/TupleToolVELOClusters.cpp
@@ -11,17 +11,13 @@
 #include "TupleToolVELOClusters.h"
 
 //-----------------------------------------------------------------------------
-// Implementation file for class : TupleToolVELOClusters
-//
-// 2009-02-11 : Patrick Koppenburg
-//-----------------------------------------------------------------------------
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
 TupleToolVELOClusters::TupleToolVELOClusters( const std::string& type,
-                                        const std::string& name,
-                                        const IInterface* parent )
+                                              const std::string& name,
+                                              const IInterface* parent )
   : TupleToolBase ( type, name, parent )
 {
   declareInterface<IEventTupleTool>(this);
@@ -37,13 +33,7 @@ TupleToolVELOClusters::~TupleToolVELOClusters() {}
 StatusCode TupleToolVELOClusters::fill( Tuples::Tuple& tup )
 {
   const std::string prefix = fullName();
-  // Load the RecSummary object
-  const LHCb::RecSummary * rS = 
-    getIfExists<LHCb::RecSummary>(evtSvc(),LHCb::RecSummaryLocation::Default);
-  if ( !rS ) 
-  {
-    rS = getIfExists<LHCb::RecSummary>(evtSvc(),LHCb::RecSummaryLocation::Default,false); 
-  }
+
   // Fill the tuple
   bool test = true;
   std::vector<double> stations;
@@ -51,51 +41,57 @@ StatusCode TupleToolVELOClusters::fill( Tuples::Tuple& tup )
 
   std::vector<int> vids; vids.reserve(1000);
   LHCb::Tracks* tracksCont = get<LHCb::Tracks>( LHCb::TrackLocation::Default );
-  if( tracksCont == NULL || tracksCont->size()==0 ){
-    return -20000;
+  if ( !tracksCont || tracksCont->empty() )
+  {
+    return StatusCode::FAILURE;
   }
-  for (LHCb::Tracks::iterator iter = tracksCont->begin(); 
+  for (LHCb::Tracks::iterator iter = tracksCont->begin();
        iter != tracksCont->end(); ++iter){
     const std::vector<LHCb::LHCbID>& ids = (*iter)->lhcbIDs();
-    for ( std::vector<LHCb::LHCbID>::const_iterator it = ids.begin(); it != ids.end(); ++it ){
-      if (it->isVelo() == true) vids.push_back(it->veloID()); 
+    for ( std::vector<LHCb::LHCbID>::const_iterator it = ids.begin(); it != ids.end(); ++it )
+    {
+      if ( it->isVelo() ) vids.push_back(it->veloID());
     } // lhcbids
   } // tracks
-  
+
   std::sort(vids.begin(), vids.end());
-  std::vector<int>::iterator u =  std::unique(vids.begin(), vids.end());
+  std::vector<int>::iterator u = std::unique(vids.begin(), vids.end());
   vids.resize(u - vids.begin());
 
   LHCb::VeloClusters* clusters = get<LHCb::VeloClusters>( LHCb::VeloClusterLocation::Default );
-  if( clusters == NULL || clusters->size()==0 ){
-    return -10000;
+  if ( !clusters || clusters->empty() )
+  {
+    return StatusCode::FAILURE;
   }
   // MDN loop over the clusters
-  for ( LHCb::VeloClusters::iterator iterV = clusters->begin(); iterV != clusters->end(); ++iterV) {
+  for ( LHCb::VeloClusters::iterator iterV = clusters->begin(); iterV != clusters->end(); ++iterV )
+  {
     // check if used on track
-    if (std::find(vids.begin(), vids.end(),(*iterV)->channelID()) == vids.end()){
-        debug() << "hit is not on track " 
-          << (*iterV)->isRType() <<  " "<< (*iterV)->isPhiType() <<  " " << (*iterV)->isPileUp() << std::endl; 
+    if (std::find(vids.begin(), vids.end(),(*iterV)->channelID()) == vids.end())
+    {
 
-      int sensor = (*iterV)->channelID().sensor();
-      double mysensor = sensor;
+      if ( msgLevel(MSG::DEBUG) )
+        debug() << "hit is not on track "
+                << (*iterV)->isRType() <<  " "<< (*iterV)->isPhiType() <<  " "
+                << (*iterV)->isPileUp() << endmsg;
+
+      const double mysensor = (double)(*iterV)->channelID().sensor();
       stations.push_back(mysensor);
-      double adc =(*iterV)->totalCharge();
+      const double adc = (*iterV)->totalCharge();
       stationADC.push_back(adc);
 
     }
   }
-  int unusedvelo  = clusters->size() - vids.size() ;
+
+  const int unusedvelo = clusters->size() - vids.size() ;
+
   //info() << " n unused =  " << unusedvelo << endmsg;
   test &= tup->column( prefix+"nUnusedVeloClusters", unusedvelo);
   test &= tup->farray( prefix+"UNUSED_VELO_STATION_ADC", stationADC.begin(), stationADC.end(), "nClusters" , 100);
   test &= tup->farray( prefix+"UNUSED_VELO_STATION_VECTOR", stations.begin(), stations.end(), "nClusters", 100);
+
   return StatusCode(test);
-
-
 }
-
-
 
 
 // Declaration of the Tool Factory
--- Analysis/Phys/DecayTreeTupleReco/src/TupleToolVELOClusters.h
+++ Analysis/Phys/DecayTreeTupleReco/src/TupleToolVELOClusters.h
@@ -7,29 +7,25 @@
 #include "DecayTreeTupleBase/TupleToolBase.h"
 
 #include "Event/Track.h"
-#include "Event/RecSummary.h"
 
 /** @class TupleToolVELOClusters TupleToolVELOClusters.h
  *
- *  Fills Reco stats, from RecSummary
- *
- *  @author Patrick Koppenburg, Fatima Soomro, Jibo He
- *  @date   2009-02-11
+ *  Fills VELO information
  */
-class TupleToolVELOClusters : public TupleToolBase, 
-                           virtual public IEventTupleTool
+class TupleToolVELOClusters : public TupleToolBase,
+                              virtual public IEventTupleTool
 {
 
 public:
 
   /// Standard constructor
   TupleToolVELOClusters( const std::string& type,
-                      const std::string& name,
-                      const IInterface* parent);
+                         const std::string& name,
+                         const IInterface* parent);
 
   virtual ~TupleToolVELOClusters( ); ///< Destructor
   virtual StatusCode fill( Tuples::Tuple& );///< Fill tuple
-//  int unusedVelo();
+  //  int unusedVelo();
 
 };
 
