2fc42980012c10a9ea641b9a27c928a684ea326d
rlambert <rlambert@cern.ch>
2013-05-13 09:58:52 +0000

    update readStrip20Data.py test
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@156638 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5acf43c53dc5206267dffbaede03958fa73694a4

--- Moore/Hlt/Moore/tests/options/readStrip20Data.py
+++ Moore/Hlt/Moore/tests/options/readStrip20Data.py
@@ -7,19 +7,21 @@ Moore().UseDBSnapshot= False;
 Moore().Persistency= 'ROOT';
 Moore().outputFile= 'SwimmingTest.dst';
 Moore().Verbose= False;
-Moore().DataType= '2011';
-Moore().DDDBtag= 'head-20110722';
+Moore().DataType= '2012';
+Moore().DDDBtag= 'head-20120413'; #from RunDB
 Moore().EnableRunChangeHandler= False;
 Moore().WriterRequires= [];
 Moore().WriteFSR= True;
 Moore().EnableDataOnDemand= True;
-Moore().L0= True;
 Moore().CheckOdin= False;
 Moore().InitialTCK= '0x00790038';
 Moore().Simulation= False;
-Moore().CondDBtag= 'head-20110901';
-Moore().ReplaceL0BanksWithEmulated= False;
-Moore().UseTCK= True;
+Moore().CondDBtag= 'head-20120420'; #from RunDB
+Moore().ThresholdSettings="Physics_September2012"
+Moore().ForceSingleL0Configuration = False
+
+from Configurables import L0Conf
+L0Conf().EnsureKnownTCK=False
 
 ############# prepend a "killHltSeq" ############
 
@@ -38,94 +40,18 @@ class Deathstar(object):
         ApplicationMgr().TopAlg.insert(0, self._seq)
 
 d = Deathstar(deathstar)
+appendPostConfigAction(d.witnessTheFirepowerOfThisFullyArmedAndOperationalBattleStation)
 
 ############# prepend a "resurectRawEventSeq"  ############
 
-from Configurables import RawEventMapCombiner, RawEventSimpleCombiner, DataOnDemandSvc
+from Configurables import RecombineRawEvent
+RecombineRawEvent()
 
-#### Try simple thingy ... something wierd happens #####
-myCombiner=RawEventSimpleCombiner("resurectRawEvent")
-myCombiner.InputRawEventLocations=[
-    "Trigger/RawEvent",
-    "Other/RawEvent",
-    "Rich/RawEvent",
-    "Muon/RawEvent",
-    "Calo/RawEvent"#,
-    #"Other/RawEvent" #can't use Other/RawEvent
-    ] 
-myCombiner.OutputLevel=VERBOSE
-
-#### Try complicated thingy .. works so long as some banks are left alone #####
-
-myMapCombiner=RawEventMapCombiner("resurectRawEventMap")
-
-myMapCombiner.RawBanksToCopy={ 
-    'ODIN': "Trigger/RawEvent",
-    'L0DU': "Trigger/RawEvent",
-    'L0Calo': "Trigger/RawEvent",
-    'L0CaloFull': "Trigger/RawEvent",
-    'L0DU': "Trigger/RawEvent",
-    'L0Muon': "Trigger/RawEvent",
-    'L0MuonProcCand': "Trigger/RawEvent",
-    'L0PU': "Trigger/RawEvent",
-    'HltSelReports': "Trigger/RawEvent",
-    'HltDecReports': "Trigger/RawEvent",
-    'HltRoutingBits': "Trigger/RawEvent",
-    'HltVertexReports': "Trigger/RawEvent",
-    'Rich': "Rich/RawEvent",
-    'Muon': "Muon/RawEvent",
-    'PrsE': "Calo/RawEvent",
-    'EcalE': "Calo/RawEvent",
-    'HcalE': "Calo/RawEvent",
-    'PrsTrig': "Calo/RawEvent",
-    'EcalTrig': "Calo/RawEvent",
-    'HcalTrig': "Calo/RawEvent",
-    'EcalPacked': "Calo/RawEvent",
-    'HcalPacked': "Calo/RawEvent",
-    'PrsPacked': "Calo/RawEvent",
-    'EcalPackedError': "Calo/RawEvent",
-    'HcalPackedError': "Calo/RawEvent",
-    'PrsPackedError': "Calo/RawEvent"
-##     'Velo' : "Other/RawEvent",
-##     'TT' : "Other/RawEvent",
-##     'IT' : "Other/RawEvent",
-##     'OT' : "Other/RawEvent"
-    }
-
-myMapCombiner.OutputLevel=VERBOSE
-
-
-necromancer               = GaudiSequencer("resurectRawEventSeq")
-
-#try and use SimpleCombiner...
-necromancer.Members       = [myCombiner]
-
-#try and use MapCombiner
-#necromancer.Members       = [myMapCombiner]
-
-ApplicationMgr().ExtSvc = [ "DataOnDemandSvc" ]+ApplicationMgr().ExtSvc
-DataOnDemandSvc().AlgMap["DAQ/RawEvent"]=myCombiner
-DataOnDemandSvc().NodeMap[ "DAQ" ]    = "DataObject" #Do I need this?
-#DataOnDemandSvc().PreInitialize=True #Do I need this?
-
-from Configurables import ODINDecodeTool
-
-#configure this public tool
-#ODINDecodeTool().RawEventLocation="Trigger/RawEvent"
-#ODINDecodeTool("ToolSvc.OdinTimeDecoder.ODINDecodeTool").RawEventLocation="Trigger/RawEvent"
-
-class Necromancer(object):
-    def __init__(self, seq):
-        self._seq = seq
-    def raiseFromTheDead(self):
-        ApplicationMgr().TopAlg.insert(0, self._seq)
-
-
-n = Necromancer(necromancer)
-#appendPostConfigAction(n.raiseFromTheDead)
 
 ################ dataset #################
 # Input data
 from GaudiConf import IOHelper
 IOHelper().inputFiles(['/castor/cern.ch/user/n/ntorr/test/00022726_00111043_1.CharmToBeSwum.dst'], clear=True)
+
+
 
