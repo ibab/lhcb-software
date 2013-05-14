3a8f9dc674d1b87182fd9a01ca7aa3c108127c22
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-05-14 13:46:44 +0000

    Patch TupleToolGeneration to not throw an exception if a NULL LHCb::HepMCEvent    pointer is found, as this is now normal in MC12 data (and thus this tool    currently does not work for these MC samples, and needs updating...).
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@156808 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 42a628c580145fa0baed7335461e861b4456e4b4

--- Analysis/Phys/DecayTreeTupleMC/src/TupleToolGeneration.cpp
+++ Analysis/Phys/DecayTreeTupleMC/src/TupleToolGeneration.cpp
@@ -21,19 +21,13 @@
 // 2008-07-01 : Patrick Koppenburg
 //-----------------------------------------------------------------------------
 
-using namespace Gaudi;
-using namespace LHCb;
-
-// Declaration of the Tool Factory
-DECLARE_TOOL_FACTORY( TupleToolGeneration )
-
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-  TupleToolGeneration::TupleToolGeneration( const std::string& type,
-                                            const std::string& name,
-                                            const IInterface* parent )
-    : TupleToolBase ( type, name , parent )
+TupleToolGeneration::TupleToolGeneration( const std::string& type,
+                                          const std::string& name,
+                                          const IInterface* parent )
+  : TupleToolBase ( type, name , parent )
 {
   declareInterface<IEventTupleTool>(this);
 }
@@ -43,14 +37,15 @@ DECLARE_TOOL_FACTORY( TupleToolGeneration )
 StatusCode TupleToolGeneration::fill( Tuples::Tuple& tuple )
 {
 
-  const std::string prefix=fullName();
+  const std::string prefix = fullName();
 
   if (msgLevel(MSG::DEBUG)) debug() << "TupleToolGeneration" << endmsg  ;
 
-  if (!exist<LHCb::GenHeader>(LHCb::GenHeaderLocation::Default))
+  LHCb::GenHeader * mch = getIfExists<LHCb::GenHeader>(LHCb::GenHeaderLocation::Default);
+  if ( !mch )
+  {
     return Warning("NoGenHeader. You probably don't need this tool.",1,StatusCode::SUCCESS);
-  LHCb::GenHeader* mch = get<LHCb::GenHeader>(LHCb::GenHeaderLocation::Default);
-
+  }
   if (msgLevel(MSG::DEBUG)) debug() << mch->numOfCollisions() << " collisions" << endmsg  ;
 
   std::vector<unsigned int> heaviestQuark ;
@@ -58,27 +53,51 @@ StatusCode TupleToolGeneration::fill( Tuples::Tuple& tuple )
   unsigned int hqEvent = 0 ;
 
   // quarks
-  for ( SmartRefVector< LHCb::GenCollision >::const_iterator ic = mch->collisions().begin() ;
-        ic != mch->collisions().end() ; ++ic){
-    if (0==*ic) Exception("Null collision pointer");
-    const LHCb::HepMCEvent* gene = (*ic)->event() ;
-    if (!gene) Exception("Null event pointer");
-    processType.push_back((*ic)->processType());
+  processType.reserve( mch->collisions().size() );
+  for ( SmartRefVector<LHCb::GenCollision>::const_iterator ic = mch->collisions().begin();
+        ic != mch->collisions().end(); ++ic )
+  {
+    if ( 0 == *ic ) { Exception("NULL LHCb::GenCollision pointer"); }
+    const LHCb::HepMCEvent * gene = (*ic)->event() ;
+    if ( !gene ) 
+    {
+      Warning("NULL LHCb::HepMCEvent pointer -> This tool cannot work on this MC data",
+              StatusCode::SUCCESS).ignore();
+      continue;
+    }
+
+    processType.push_back( (*ic)->processType() );
     unsigned int hq = 2 ;
     for ( HepMC::GenEvent::particle_const_iterator p = gene->pGenEvt()->particles_begin();
-          p != gene->pGenEvt()->particles_end();   ++p ) {
-      LHCb::ParticleID pid( (*p)->pdg_id()) ;
+          p != gene->pGenEvt()->particles_end(); ++p )
+    {
+      const LHCb::ParticleID pid( (*p)->pdg_id() ) ;
       if (msgLevel(MSG::VERBOSE)) verbose() << "Gen particle " << (*p)->pdg_id() << " "
                                             << pid.hasQuark(LHCb::ParticleID::bottom)
-                                            << " " << pid.hasQuark(LHCb::ParticleID::charm) << endmsg ;
-      if ((hq<LHCb::ParticleID::top) &&  pid.hasQuark(LHCb::ParticleID::top)) { hq = LHCb::ParticleID::top; break ;}
-      else if ((hq<LHCb::ParticleID::bottom) &&  pid.hasQuark(LHCb::ParticleID::bottom)) hq = LHCb::ParticleID::bottom;
-      else if ((hq<LHCb::ParticleID::charm) &&  pid.hasQuark(LHCb::ParticleID::charm)) hq = LHCb::ParticleID::charm;
-      else if ((hq<LHCb::ParticleID::strange) &&  pid.hasQuark(LHCb::ParticleID::strange)) hq = LHCb::ParticleID::strange;
+                                            << " " << pid.hasQuark(LHCb::ParticleID::charm)
+                                            << endmsg ;
+      if ( (hq<LHCb::ParticleID::top) && pid.hasQuark(LHCb::ParticleID::top) )
+      {
+        hq = LHCb::ParticleID::top; break ;
+      }
+      else if (( hq<LHCb::ParticleID::bottom) &&  pid.hasQuark(LHCb::ParticleID::bottom) )
+      {
+        hq = LHCb::ParticleID::bottom;
+      }
+      else if ((hq<LHCb::ParticleID::charm) &&  pid.hasQuark(LHCb::ParticleID::charm) )
+      {
+        hq = LHCb::ParticleID::charm;
+      }
+      else if ((hq<LHCb::ParticleID::strange) &&  pid.hasQuark(LHCb::ParticleID::strange) )
+      {
+        hq = LHCb::ParticleID::strange;
+      }
     }
-    heaviestQuark.push_back(hq);
-    if ( hq > hqEvent) hqEvent = hq ;
-    if (msgLevel(MSG::VERBOSE)) verbose() << "Process type is " << (*ic)->processType() << ". Heaviest quark " << hq << endmsg ;
+    heaviestQuark.push_back( hq );
+    if ( hq > hqEvent ) hqEvent = hq ;
+    if (msgLevel(MSG::VERBOSE))
+      verbose() << "Process type is " << (*ic)->processType() << ". Heaviest quark " << hq
+                << endmsg ;
   }
 
   bool test = true;
@@ -88,3 +107,6 @@ StatusCode TupleToolGeneration::fill( Tuples::Tuple& tuple )
 
   return StatusCode(test);
 }
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( TupleToolGeneration )
