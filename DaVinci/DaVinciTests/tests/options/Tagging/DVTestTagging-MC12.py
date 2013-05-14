31c5e74aa1415c1b45577f1affb13970cc43ed2d
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-05-14 11:49:46 +0000

    Add MC12 based flavour tagging test
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@156766 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 53f41b010f8956e778b79f12bc4797d399b4ba05

--- DaVinci/DaVinciTests/tests/options/Tagging/DVTestTagging-MC12.py
+++ DaVinci/DaVinciTests/tests/options/Tagging/DVTestTagging-MC12.py
@@ -8,30 +8,36 @@
 ########################################################################
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer
-
 ########################################################################
 #
 # Some selections. 
 #
-from Configurables import CheatedSelection
+from Configurables import CheatedSelection, BDecayTool, MCDecayFinder
+
+myDecay = '[B+ -> (J/psi(1S) => mu+ mu- ) K+]cc'
 
 cheatsel = CheatedSelection("CheatedSelection")
 cheatsel.Inputs = [ "Phys/TaggingPions" ]
 cheatsel.AssociatorInputData = [ "Phys/CheatedSelection/Particles" ]
-cheatsel.OutputLevel = 6
+cheatsel.OutputLevel = 5
 
+cheatsel.addTool( BDecayTool )
+cheatsel.BDecayTool.addTool( MCDecayFinder )
+cheatsel.BDecayTool.MCDecayFinder.Decay = myDecay 
 ########################################################################
 #
 # Flavour tagging. 
 #
 location = "Phys/CheatedSelection"
 
-from Configurables import BTagging, BTaggingTool, BTaggingChecker
+from Configurables import BTagging, BTaggingTool, BTaggingChecker, BDecayTool, MCDecayFinder
 
 tag = BTagging("BTagging")
 tag.Inputs = [ location ]
-tag.OutputLevel = 3
-
+#tag.OutputLevel = 5
+tag.addTool( BDecayTool )
+tag.BDecayTool.addTool( MCDecayFinder )
+tag.BDecayTool.MCDecayFinder.Decay = myDecay
 tag.addTool( BTaggingTool )
 tag.BTaggingTool.ChoosePVCriterium = "PVbyIP" #needed by CheatedSel
 
@@ -40,19 +46,22 @@ tag.BTaggingTool.ChoosePVCriterium = "PVbyIP" #needed by CheatedSel
 tagcheck = BTaggingChecker("BTaggingChecker")
 tagcheck.Inputs = [ location ]
 tagcheck.TagsLocation = location+"/FlavourTags"
-
+#tagcheck.OutputLevel = 1
+tagcheck.addTool( BDecayTool )
+tagcheck.BDecayTool.addTool( MCDecayFinder )
+tagcheck.BDecayTool.MCDecayFinder.Decay = myDecay
 MessageSvc().Format = "% F%40W%S%7W%R%T %0W%M"
 ########################################################################
 #
 # Standard configuration
 #
 from Configurables import DaVinci
-DaVinci().EvtMax     = 500                       # Number of events
+DaVinci().EvtMax     = 1000                       # Number of events
 DaVinci().SkipEvents = 0                          # Events to skip
-DaVinci().PrintFreq  = 1
-DaVinci().DataType = "2010"
-DaVinci().CondDBtag = 'sim-20101210-vc-md100'
-DaVinci().DDDBtag = "head-20101206"
+DaVinci().PrintFreq  = 100
+DaVinci().DataType = "2012"
+DaVinci().CondDBtag = 'sim-20121025-vc-mu100'
+DaVinci().DDDBtag = "dddb-20120831"
 DaVinci().Simulation    = True
 from Configurables import StoreExplorerAlg, PrintDecayTree
 PrintDecayTree().Inputs = [ location ] 
@@ -66,8 +75,6 @@ DaVinci().MoniSequence = [   #PrintDecayTree()
 ########################################################################
 #
 # example data file
-# bsjpsiphi
-#DaVinci().Input = [ "DATAFILE='PFN:/usera/jonesc/NFS/data/MC/MC10/DST/BtaggingTest/00008922_00000001_1.allstreams.dst' TYP='POOL_ROOTTREE' OPT='READ'" ]
-DaVinci().Input = [ "DATAFILE='PFN:castor:/castor/cern.ch/grid/lhcb/freezer/lhcb/MC/MC10/ALLSTREAMS.DST/00008922/0000/00008922_00000001_1.allstreams.dst?svcClass=lhcbdisk' TYP='POOL_ROOTTREE' OPT='READ'" ]
-#from Configurables import CondDB
-#CondDB().LocalTags['SIMCOND'] = ['sim-20100429-vc-md100']
+
+from GaudiConf import IOHelper
+IOHelper().inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/MC/2012/ALLSTREAMS.DST/00021062/0000/00021062_00000001_1.allstreams.dst']) # Bu2JpsiK
