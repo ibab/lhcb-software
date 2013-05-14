173abb1e5f0f0907e10336e4b28de04a0e818328
Thomas Ruf <thomas.ruf@cern.ch>
2013-05-14 08:40:01 +0000

    Added support for Upgrade cluster classes
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@156712 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c5a18d55039801be179c4c666794bbe149e615bc

--- LHCb/Event/LinkerInstances/dict/linker_instances.h
+++ LHCb/Event/LinkerInstances/dict/linker_instances.h
@@ -4,6 +4,9 @@
 #include "Event/MCParticle.h"
 #include "Event/MCHit.h"
 #include "Event/VeloCluster.h"
+#include "Event/VLCluster.h"
+#include "Event/VPCluster.h"
+#include "Event/FTCluster.h"
 #include "Event/STCluster.h"
 #include "Kernel/OTChannelID.h"
 #include "Event/MuonCoord.h"
@@ -13,6 +16,9 @@
 #include "Event/Particle.h"
 namespace {
   LinkedTo<LHCb::MCParticle,LHCb::VeloCluster> i1 (0,0,"");
+  LinkedTo<LHCb::MCParticle,LHCb::VLCluster> i21 (0,0,"");
+  LinkedTo<LHCb::MCParticle,LHCb::VPCluster> i31 (0,0,"");
+  LinkedTo<LHCb::MCParticle,LHCb::FTCluster>   i41 (0,0,"");
   LinkedTo<LHCb::MCParticle,LHCb::STCluster>   i2 (0,0,"");
   LinkedTo<LHCb::MCParticle,ContainedObject>      i3 (0,0,"");
   LinkedTo<LHCb::MCParticle,LHCb::MuonCoord>   i4 (0,0,"");
@@ -24,6 +30,9 @@ namespace {
   LinkedTo<LHCb::MCParticle>                   i9 (0,0,"");
 
   LinkedFrom<LHCb::VeloCluster,LHCb::MCParticle> i101 (0,0,"");
+  LinkedFrom<LHCb::VLCluster,LHCb::MCParticle> i201 (0,0,"");
+  LinkedFrom<LHCb::VPCluster,LHCb::MCParticle> i301 (0,0,"");
+  LinkedFrom<LHCb::FTCluster,LHCb::MCParticle> i401 (0,0,"");
   LinkedFrom<LHCb::STCluster,LHCb::MCParticle>   i102 (0,0,"");
   LinkedFrom<ContainedObject,LHCb::MCParticle>      i103 (0,0,"");
   LinkedFrom<LHCb::MuonCoord,LHCb::MCParticle>   i104 (0,0,"");
