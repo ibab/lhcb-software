fa462674753e1ccfed255a2ea078021cccc557af
Markus Frank <frankb@cern.ch>
2013-05-22 13:25:21 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@157139 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: fb2167ccb2c37ccc7e64e02c74ed05bed5f399c1

--- Online/Online/ControllerTests/scripts/runFarmTask.sh
+++ Online/Online/ControllerTests/scripts/runFarmTask.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #
-echo `pwd`
-echo `pwd`
-echo `pwd`  $*
 . ../cmt/setup.x86_64-slc5-gcc46-dbg.vars
-. ./preamble.sh
+. preamble.sh
 export UTGID=${UTGID}
 ##echo $UTGID -- $* Invoking script: ${2}.sh
 . ./${2}.sh
