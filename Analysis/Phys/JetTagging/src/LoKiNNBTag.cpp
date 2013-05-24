bad4db835ea85e39e78b54e700495d56e965c2b4
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-05-24 13:35:25 +0000

    Check values with lfin as well as lnan
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@157240 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 06cd8a53abb7a64f23a38821656231aeb60b54a1

--- Analysis/Phys/JetTagging/src/LoKiNNBTag.cpp
+++ Analysis/Phys/JetTagging/src/LoKiNNBTag.cpp
@@ -7,12 +7,11 @@
 #include "LoKiNNBTag.h"
 #include "GaudiAlg/CheckForNaN.h"
 
-
 DECLARE_NAMESPACE_TOOL_FACTORY(LoKi,NNBTag)
 
 
 LoKi::NNBTag::NNBTag(const std::string& type, const std::string& name, const IInterface* parent)
-  : GaudiTool(type, name, parent)
+: GaudiTool(type, name, parent)
   , m_distCalc(0)
   , m_jet(0)
   , m_bTrackExpertForOdd(0)
@@ -23,25 +22,25 @@ LoKi::NNBTag::NNBTag(const std::string& type, const std::string& name, const IIn
   declareInterface<IJetTagTool>(this);
 
   declareProperty("TrackNetMinIP",m_trackNetMinIP=0.08
-		  ,"NeuroBayes b-track network min. IP precut in [mm], should be consistent with training.");
+                  ,"NeuroBayes b-track network min. IP precut in [mm], should be consistent with training.");
 
   declareProperty("TrackNetMaxIP",m_trackNetMaxIP=3.0
-		  ,"NeuroBayes b-track network max. IP precut in [mm], should be consistent with training.");
+                  ,"NeuroBayes b-track network max. IP precut in [mm], should be consistent with training.");
 
   declareProperty("TrackNetMinIPSig",m_trackNetMinIPSig=0.5
-		  ,"NeuroBayes b-track network min. IP significance precut, should be consistent with training.");
+                  ,"NeuroBayes b-track network min. IP significance precut, should be consistent with training.");
 
   declareProperty("TrackNetPtCut",m_trackNetMinPt=500.0
-		  ,"NeuroBayes b-track network p_T precut in [MeV], should be consistent with training.");
+                  ,"NeuroBayes b-track network p_T precut in [MeV], should be consistent with training.");
 
   declareProperty("RefitTracksAndPVs",m_refitTracksAndPVs=false
-		  ,"Set this to 'true' if you want to refit tracks and primary vertices.");
+                  ,"Set this to 'true' if you want to refit tracks and primary vertices.");
 
   declareProperty("NBExpertiseDirectory", m_nbExpertiseDirectory=""
-		  ,"Custom NB expert directory. Defaults to $JETNNBTAGPARAMROOT/data");
+                  ,"Custom NB expert directory. Defaults to $JETNNBTAGPARAMROOT/data");
 
   declareProperty("NBExpertiseVersion", m_nbExpertiseVersion="7TeV_v1"
-		  ,"NB expert version subdirectory.");
+                  ,"NB expert version subdirectory.");
   return;
 }
 
@@ -81,7 +80,7 @@ StatusCode LoKi::NNBTag::initialize()
   std::string expertiseDirectory;
 
   if ( m_nbExpertiseDirectory.empty() ) { //use expertises from JetNNBTagParam
-      const char* jetNNBTagParamRoot = getenv("JETTAGGINGROOT");
+    const char* jetNNBTagParamRoot = getenv("JETTAGGINGROOT");
 
     if ( 0 == jetNNBTagParamRoot ) {
       error() << "$JETTAGGINGROOT is not set! Aborting." << endmsg;
@@ -91,15 +90,15 @@ StatusCode LoKi::NNBTag::initialize()
     expertiseDirectory = std::string(jetNNBTagParamRoot) + "/data/nn_configs";
 
   } else { //use custom expertise directory
-      expertiseDirectory = m_nbExpertiseDirectory;
+    expertiseDirectory = m_nbExpertiseDirectory;
   }
 
   //  assemble full file path for all expertises
-    std::vector< std::string > expertisePaths;
-  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/btrack_expert_for_odd.nb"); 
-  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/btrack_expert_for_even.nb"); 
-  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/bjet_expert_for_odd.nb"); 
-  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/bjet_expert_for_even.nb"); 
+  std::vector< std::string > expertisePaths;
+  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/btrack_expert_for_odd.nb");
+  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/btrack_expert_for_even.nb");
+  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/bjet_expert_for_odd.nb");
+  expertisePaths.push_back(expertiseDirectory + "/" + m_nbExpertiseVersion + "/bjet_expert_for_even.nb");
 
   // check whether files are readable
   for ( unsigned int i=0; i<expertisePaths.size(); ++i ) {
@@ -113,13 +112,13 @@ StatusCode LoKi::NNBTag::initialize()
       fileTest.close();
     }
   }
-  
+
   // create track and jet experts
   m_bTrackExpertForOdd  = new Expert(expertisePaths[0].c_str());
   m_bTrackExpertForEven = new Expert(expertisePaths[1].c_str());
   m_bJetExpertForOdd    = new Expert(expertisePaths[2].c_str());
   m_bJetExpertForEven   = new Expert(expertisePaths[3].c_str());
-  
+
 #else
   error() << "NeuroBayes is not supported on this platform! Aborting." << endmsg;
   sc = StatusCode::FAILURE;
@@ -142,17 +141,17 @@ StatusCode LoKi::NNBTag::finalize()
 
 
 // ===========================================================================
-// Calculate Jet Properties 
+// Calculate Jet Properties
 bool LoKi::NNBTag::calculateJetProperty
 (const LHCb::Particle *jet,   std::map <std::string,double> &jetProps)
 {
   StatusCode sc = StatusCode::SUCCESS;
 
 
-  // ODIN event number, relevant for odd/even NB training  
+  // ODIN event number, relevant for odd/even NB training
   LHCb::ODIN* odin_bank = getOdinBank();
   if ( 0 != odin_bank ) {
-    // we don't care about the number, only odd or even classification. 
+    // we don't care about the number, only odd or even classification.
     m_oddEvent = 1 == odin_bank->eventNumber()%2;
   } else {
     // we can't reliably use NeuroBayes without knowing which training to choose
@@ -171,7 +170,7 @@ bool LoKi::NNBTag::calculateJetProperty
   float nb_jet_net = applyJetNet();
 
 
-  
+
   jetProps["j_max_trk_netout"] = m_nb_jet_vars[0];
   jetProps["j_sum_trk_netout"] = m_sum_trk_netout;
   jetProps["j_pt"] = m_nb_jet_vars[1];
@@ -190,7 +189,7 @@ bool LoKi::NNBTag::calculateJetProperty
 
   jetProps["extraInfo"] = 5300;
 
- 
+
   if (jetProps["j_nb_net"]>0)
     return true;
   else
@@ -205,7 +204,7 @@ LHCb::ODIN* LoKi::NNBTag::getOdinBank()
   if (!exist<LHCb::ODIN>(LHCb::ODINLocation::Default)) {
     if (UNLIKELY(msgLevel(MSG::DEBUG))) {
       debug() << "No ODIN bank found. Can't retrieve event numbers. (Relevant for odd/even NB training only)"
-	      << endmsg;
+              << endmsg;
     }
   } else {
     odin = get<LHCb::ODIN>(LHCb::ODINLocation::Default);
@@ -268,7 +267,7 @@ void LoKi::NNBTag::getPrimaries()
       m_refittedPVsPtrs.push_back(&(*irfpv));
     }
     m_PVs = LHCb::RecVertex::Range(m_refittedPVsPtrs.begin(),m_refittedPVsPtrs.end());
-  } 
+  }
 
   return;
 }
@@ -283,9 +282,9 @@ void LoKi::NNBTag::extractTracks()
   std::sort(m_jet_all_parts.begin(),m_jet_all_parts.end(),ParticlePtrComparePt());
 
   // find all constituents with tracks
-  for ( std::vector < const LHCb::Particle* >::const_iterator ci = m_jet_all_parts.begin(); 
-	ci != m_jet_all_parts.end(); 
-	++ci ) {
+  for ( std::vector < const LHCb::Particle* >::const_iterator ci = m_jet_all_parts.begin();
+        ci != m_jet_all_parts.end();
+        ++ci ) {
 
     // only consider basic particles for tracks
     if ( ! (*ci)->isBasicParticle() ) continue;
@@ -297,7 +296,7 @@ void LoKi::NNBTag::extractTracks()
     if  ( !tracks.empty() ) {
       m_jet_tracks.push_back(tracks.front());
       m_jet_trk_parts.push_back(*ci);
-    } 
+    }
   }
 
   return;
@@ -306,13 +305,13 @@ void LoKi::NNBTag::extractTracks()
 // ===========================================================================
 // calculate IP and IP significance
 void LoKi::NNBTag::calculateIP(const LHCb::Particle* p,
-			       const Gaudi::XYZPoint& v,
-			       double& ip,
-			       double& ipsig)   
+                               const Gaudi::XYZPoint& v,
+                               double& ip,
+                               double& ipsig)
 {
 
   double ipC=0.0, ipChi2=0.0;
-  
+
   StatusCode sc = m_distCalc->distance(p, v, ipC, ipChi2);
   ipC = std::fabs(ipC);
 
@@ -329,43 +328,47 @@ void LoKi::NNBTag::calculateIP(const LHCb::Particle* p,
 void LoKi::NNBTag::calculateTrackVariables()
 {
 #ifdef __GNUC__
-  std::vector<LHCb::Track*>::const_iterator itrk = m_jet_tracks.begin();
-  std::vector<const LHCb::Particle*>::const_iterator ipart = m_jet_trk_parts.begin();
 
-  for ( ; itrk != m_jet_tracks.end(); ++itrk, ++ipart){
+  std::vector<const LHCb::Particle*>::const_iterator ipart = m_jet_trk_parts.begin();
+  for ( std::vector<LHCb::Track*>::const_iterator itrk = m_jet_tracks.begin(); 
+        itrk != m_jet_tracks.end(); ++itrk, ++ipart)
+  {
     const LHCb::Particle* part = *ipart;
     LHCb::Track*         track = *itrk;
 
     // get the best PV associated with this particle
     // and calculate IP. There might be no PV associated
-    // and the IP calculation might fail. 
+    // and the IP calculation might fail.
     double ip     = -999.0;
     double ip_sig = -999.0;
     double ip_err = -999.0;
 
     if ( ! m_PVs.empty() )
+    {
+      std::vector<const LHCb::Track*> excludeTracks;
+      excludeTracks.push_back(track);
+
+      double ip_tmp     =  std::numeric_limits<double>::max();
+      double ip_sig_tmp =  std::numeric_limits<double>::max();
+
+      for ( std::vector<const LHCb::RecVertex*>::const_iterator ipv = m_PVs.begin(); 
+            m_PVs.end()!=ipv; ++ipv) 
       {
-	std::vector<const LHCb::Track*> excludeTracks;
-	excludeTracks.push_back(track);
+        const LHCb::RecVertex* biasedPV = (*ipv);
+        LHCb::RecVertex unbiasedPV;
+        m_pvTool->removeTracksAndRecalculatePV (biasedPV,excludeTracks,unbiasedPV);
 
-	double ip_tmp     =  std::numeric_limits<double>::max();
-	double ip_sig_tmp =  std::numeric_limits<double>::max();
-
-	std::vector<const LHCb::RecVertex*>::const_iterator ipv = m_PVs.begin();
-	for ( ; m_PVs.end()!=ipv; ++ipv) {
-
-	  const LHCb::RecVertex* biasedPV = (*ipv);
-	  LHCb::RecVertex unbiasedPV;
-	  m_pvTool->removeTracksAndRecalculatePV (biasedPV,excludeTracks,unbiasedPV);
-
-	  calculateIP(part, unbiasedPV.position(), ip_tmp, ip_sig_tmp);
-	  if ( std::fabs(ip_tmp) < std::fabs(ip) && !lnan(ip_tmp) && !lnan(ip_sig_tmp) ) {
-	    ip = ip_tmp; 
-	    ip_sig = ip_sig_tmp; 
-	    ip_err = ip_tmp/ip_sig_tmp;
-	  }
-	}
+        calculateIP(part, unbiasedPV.position(), ip_tmp, ip_sig_tmp);
+        if ( std::fabs(ip_tmp) < std::fabs(ip) &&
+             !lnan(ip_tmp) && !lnan(ip_sig_tmp) &&
+             lfin(ip_tmp)  && lfin(ip_sig_tmp) )
+        {
+          ip = ip_tmp;
+          ip_sig = ip_sig_tmp;
+          ip_err = ip_tmp/ip_sig_tmp;
+        }
       }
+    }
 
     m_trk_ip.push_back(ip);
     m_trk_ipsig.push_back(ip_sig);
@@ -375,7 +378,7 @@ void LoKi::NNBTag::calculateTrackVariables()
     if ( m_refitTracksAndPVs ) { // after refit use tracks as they are more accurate...
       pt = track->pt();
       p  = track->p ();
-    } else { // ...otherwise use the particle properties as they are better 
+    } else { // ...otherwise use the particle properties as they are better
       pt = part->pt();
       p  = part->p ();
     }
@@ -388,11 +391,11 @@ void LoKi::NNBTag::calculateTrackVariables()
     m_nb_track_vars[3] = static_cast<float>(pt);
     m_nb_track_vars[4] = static_cast<float>(eta);
 
-    double nb_track_bprob = -999.0; 
+    double nb_track_bprob = -999.0;
     if (   ip > m_trackNetMinIP
-	   && ip < m_trackNetMaxIP 
-	   && ip_sig > m_trackNetMinIPSig 
-	   && pt > m_trackNetMinPt) {
+           && ip < m_trackNetMaxIP
+           && ip_sig > m_trackNetMinIPSig
+           && pt > m_trackNetMinPt) {
       ++m_ntrk_bprob;
       nb_track_bprob = applyTrackNet();
     }
@@ -405,7 +408,7 @@ void LoKi::NNBTag::calculateTrackVariables()
 }
 
 // ===========================================================================
-// calculate per jet variables 
+// calculate per jet variables
 void LoKi::NNBTag::calculateJetVariables()
 {
 
@@ -426,7 +429,7 @@ void LoKi::NNBTag::calculateJetVariables()
     if(m_trk_bprob[itrk]>-10.0)
       m_sum_trk_netout +=  m_trk_bprob[itrk];
     if ( m_trk_bprob[itrk] > max_trk_b_prob )  max_trk_b_prob = m_trk_bprob[itrk];
-  
+
     float ip     = m_trk_ip   [itrk];
     float ip_sig = m_trk_ipsig[itrk];
     if ( ip > 0 ) {
@@ -439,7 +442,7 @@ void LoKi::NNBTag::calculateJetVariables()
     }
 
     Gaudi::LorentzVector ptrk = m_jet_trk_parts[itrk]->momentum();
-    Gaudi::LorentzVector pjet = m_jet->momentum(); 
+    Gaudi::LorentzVector pjet = m_jet->momentum();
     Gaudi::XYZVector vtrk(ptrk.x()/ptrk.r(),ptrk.y()/ptrk.r(),ptrk.z()/ptrk.r());
     Gaudi::XYZVector vjet(pjet.x()/pjet.r(),pjet.y()/pjet.r(),pjet.z()/pjet.r());
     double angle = std::acos(vtrk.Dot(vjet));
@@ -492,7 +495,7 @@ void LoKi::NNBTag::calculateJetVariables()
   m_nb_jet_vars[var++] = static_cast<float>(m_jet->pt());
 
   // highest track pt
-  m_nb_jet_vars[var++] = static_cast<float>(m_jet_tracks.front()->pt()); 
+  m_nb_jet_vars[var++] = static_cast<float>(m_jet_tracks.front()->pt());
 
   // maximum track ip sig
   m_nb_jet_vars[var++] = static_cast<float>(max_trk_ip_sig);
@@ -501,7 +504,7 @@ void LoKi::NNBTag::calculateJetVariables()
   m_nb_jet_vars[var++] = static_cast<float>(avg_trk_ip);
 
   // kt sum of all tracks with respect to jet
-  m_nb_jet_vars[var++] = static_cast<float>(trk_kt_sum); 
+  m_nb_jet_vars[var++] = static_cast<float>(trk_kt_sum);
 
   // signed ip tag sum
   m_nb_jet_vars[var++] = static_cast<float>(ip_tag_sum);
