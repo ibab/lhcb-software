6e7c0b4f2cecfe8045eecd0f2409714a6f2a7591
Marcin Kucharczyk <marcin@cern.ch>
2013-05-24 10:28:40 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@157227 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: b2098a13c8a385b665c5f7923ee357e10e3127a6

---
+++ Gauss/Gen/LbPowheg/LbPowheg/ReadLHE.h
@@ -0,0 +1,87 @@
+// $Id: ReadLHE.h,v 1.2 2007-07-31 12:49:53 ibelyaev Exp $
+// ============================================================================
+#ifndef LBPYTHIA_READLHE_H
+#define LBPYTHIA_READLHE_H 1 
+// ============================================================================
+// Include files 
+// ============================================================================
+// Gaudi
+// ============================================================================
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/ToolFactory.h"
+// ============================================================================
+// Generators  
+// ============================================================================
+#include "Generators/F77Utils.h"
+// ============================================================================
+// LbPythia
+// ============================================================================
+#include "LbPythia/Pypars.h"
+#include "LbPythia/Pythia.h"
+#include "LbPythia/PythiaProduction.h"
+// ============================================================================
+// Local 
+// ============================================================================
+#include "LbPythia/ReadFile.h"
+// ============================================================================
+namespace LbPythia 
+{
+  // ==========================================================================
+  /** @class ReadLHE RadLHE.h ReadLHE.cpp
+   *
+   *  Simple production tool which reads parton level 
+   *  LHE file (Les Houches Events). The LHE standard 
+   *  in described in hep-ph/0609017 CERN-LCGAPP-2006-03
+   *
+   *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
+   *  @date 2006-10-03
+   */
+  class ReadLHE: public LbPythia::ReadFile
+  {
+    // ========================================================================
+    /// friend factory for instantiation
+    friend class ToolFactory<LbPythia::ReadLHE> ;
+    // ========================================================================
+  public:
+    // ========================================================================
+    /// tool initialization 
+    virtual StatusCode initialize () ;
+    /// tool finalization 
+    virtual StatusCode finalize   () ;
+    // ========================================================================
+  protected:
+    // ========================================================================
+    /** standard constructor 
+     *  @param type tool type (?)
+     *  @param name tool name 
+     *  @param parent tool parent 
+     */
+    ReadLHE
+    ( const std::string& type,
+      const std::string& name,
+      const IInterface* parent ) ;
+    // destructor
+    virtual ~ReadLHE () ;
+    // ========================================================================
+  private:
+    // ========================================================================
+    // the default constructor is disabled 
+    ReadLHE () ;
+    // the copy constructor is disabled 
+    ReadLHE( const ReadLHE& ) ;
+    // assignement operator is disabled 
+    ReadLHE& operator=( const ReadLHE& ) ;
+    // ========================================================================
+  private:
+    // ========================================================================
+    /// logical file unit  (F77)
+    int         m_LUN     ;                         // logical file unit  (F77)
+    // ========================================================================
+  } ;
+  // ==========================================================================
+} // end of namespace LbPythia
+// ============================================================================
+// The END 
+// ============================================================================
+#endif // LBPYTHIA_READLHE_H 1 
+// ============================================================================
