22c95e8d5fdf6bf542c68bef448ddde43ef1a1d9
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-05-27 05:58:42 +0000

    move_package: moved the Tf/PatKernel package to the Rec project
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@157312 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eadcb65de1d487111f25ec871f3b95ab369de0b3

--- LHCb/Tf/PatKernel/dict/PatKernelDict.h
+++
@@ -1,9 +0,0 @@
-#ifndef DICT_PATKERNELDICT_H 
-#define DICT_PATKERNELDICT_H 1
-
-#include "PatKernel/IPatDebugTool.h"
-#include "PatKernel/IPatDebugTTTool.h"
-#include "PatKernel/IPatTTCounter.h"
-
-#endif // DICT_PATKERNELDICT_H
-
