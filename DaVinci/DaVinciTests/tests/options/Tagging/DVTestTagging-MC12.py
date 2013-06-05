5815b3ca257cd7c5691e02fa3dfed2ad4aa56629
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-06-05 14:44:14 +0000

    Take FT data options from PRConfig
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@157913 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e4650c43d4678d2ba82d140082938f21fef417f5

--- DaVinci/DaVinciTests/tests/options/Tagging/DVTestTagging-MC12.py
+++ DaVinci/DaVinciTests/tests/options/Tagging/DVTestTagging-MC12.py
@@ -71,10 +71,3 @@ DaVinci().MoniSequence = [   #PrintDecayTree()
                             ,tagcheck
                             ]  # The algorithms
 
-
-########################################################################
-#
-# example data file
-
-from GaudiConf import IOHelper
-IOHelper().inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/freezer/lhcb/MC/2012/ALLSTREAMS.DST/00021062/0000/00021062_00000001_1.allstreams.dst']) # Bu2JpsiK
