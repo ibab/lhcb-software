70388091c91c734e93a8dec6e762657ff5509138
phunt <phunt@cern.ch>
2013-06-07 13:18:02 +0000

    Added PID calibration lines for kaons, pion, protons, muons and electrons to the Stripping20r3 dictionary.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@158030 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9368b6633373981808109ce9253090eb74070598

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r3/LineConfigDictionaries_ALL.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r3/LineConfigDictionaries_ALL.py
@@ -782,3 +782,172 @@ D02HHForXSec = {
     'STREAMS' : [ 'ALL' ] 
     }
 
+
+#------------------------------------------------------------------------
+#
+# PID calibration lines (excludes the recently added Lambda_c proton
+# calibration lines
+#
+#-----------------------------------------------------------------------
+
+# PID: J/Psi -> mu mu
+MuIDCalib = {
+    'BUILDERTYPE' : 'MuIDCalibConf',
+    'WGs'         : [ 'ALL' ],
+    'STREAMS'     : [ 'ALL' ],
+    'CONFIG'      : {
+    'PromptPrescale'           : 0.0,
+    'DetachedPrescale'         : 1.0,
+    'DetachedNoMIPPrescale'    : 1.0,
+    'DetachedNoMIPHiPPrescale' : 0.0,
+    'DetachedNoMIPKPrescale'   : 1.0,
+    'FromLambdacPrescale'      : 1.0,
+    'KFromLambdacPrescale'     : 0.0,
+    'PiFromLambdacPrescale'    : 0.0,
+    'PFromLambdacPrescale'     : 0.0,
+    'KISMUONFromLambdacPrescale' : 0.0,
+    'PiISMUONFromLambdacPrescale': 0.0,
+    'PISMUONFromLambdacPrescale' : 0.0
+    }
+    }
+
+# PID: D* tagged D0 -> K- pi+
+NoPIDDstarWithD02RSKPi = {
+    'BUILDERTYPE' : 'NoPIDDstarWithD02RSKPiConf',
+    'STREAMS'     : [ 'ALL' ],
+    'WGs'         : [ 'ALL' ],
+    'CONFIG'      : {
+    'DaugPt'             : 250 * MeV      ## GeV
+    , 'DaugP'            : 2.0 * GeV      ## GeV
+    , 'DaugIPChi2'       : 16             ## unitless
+    , 'DaugTrkChi2'      : 5              ## unitless
+    , 'D0MassWin'        : 75 * MeV       ## MeV
+    , 'D0Pt'             : 1.5 * GeV      ## GeV
+    , 'D0VtxChi2Ndof'    : 13             ## unitless
+    , 'D0FDChi2'         : 49             ## unitless
+    , 'D0BPVDira'        : 0.9999         ## unitless
+    , 'D0IPChi2'         : 30             ## unit
+    , 'SlowPiPt'         : 150 * MeV      ## MeV
+    , 'SlowPiTrkChi2'    : 5              ## unitless
+    , 'DstarPt'          : 2.2 * GeV      ## GeV
+    , 'DstarVtxChi2Ndof' : 13             ## unitless
+    , 'DeltaM_Min'       : 130 * MeV      ## MeV
+    , 'DeltaM_Max'       : 155 * MeV      ## MeV
+    ##
+    , 'DCS_WrongMass'    : 25 * MeV       ## MeV (3 sigma veto)
+    , 'KK_WrongMass'     : 25 * MeV       ## MeV (3 sigma veto)
+    , 'PiPi_WrongMass'   : 25 * MeV       ## MeV (3 sigma veto)
+    ##
+    , 'Prescale'         : 0.89           ## unitless
+    , 'Postscale'        : 1.00           ## unitless
+    ##
+    , 'Monitor'          : False           ## Activate the monitoring?
+    }
+    }
+
+# PID: Lambda0 -> p pi- and KS0 -> pi+ pi-
+V0ForPID = {
+    'BUILDERTYPE' : 'StrippingV0ForPIDConf',
+    'STREAMS'     : [ 'ALL' ],
+    'WGs'         : [ 'ALL' ],
+    'CONFIG'      : {
+    'TrackChi2'            :  5       ,          ## Track Chi2/ndof quality 
+    'VertexChi2'           :  16      ,          ## Cut on Vertex chi2-quality
+    'DeltaMassK0S'         :  50 * MeV,          ## Mass-window (half)-width for K0S 
+    'DeltaMassLambda'      :  25 * MeV,          ## Mass-window (half)-width for Lambda 
+    'MaxZ'                 : 220 * cm ,          ## Maximal allowed vertex Z-position
+    'DaughtersIPChi2'      :  25      ,          ## minimal IP-chi2 for daughter particles  
+    'LTimeFitChi2'         :  49      ,          ## Chi2 from LifeTime Fitter
+    ##
+    'WrongMassK0S'         :   9 * MeV,          ## Mass-window (half)-width for ppi hypthesis
+    'WrongMassK0S_DD'      :  18 * MeV,          ## Mass-window (half)-width for ppi hypthesis (DD-case)
+    ##
+    'WrongMassLambda'      :  20 * MeV,          ## Mass-window (half)-width for pipi hypthesis
+    'WrongMassLambda_DD'   :  40 * MeV,          ## Mass-window (half)-width for pipi hypthesis (DD-case)
+    ##
+    'CTauK0S'              :   1 * mm ,          ## Cut on c*tau for K0S 
+    'CTauK0S_DD'           :  10 * mm ,          ## Cut on c*tau for K0S (DD-case)
+    ##
+    'CTauLambda0'          :   5 * mm ,          ## Cut on c*tau for Lambda0
+    'CTauLambda0_DD'       :  20 * mm ,          ## Cut on c*tau for Lambda0 (DD-case)
+    ##
+    'Proton_IsMUONCut'     :  "(INTREE( (ABSID=='p+') & ISMUON ) )" , 
+    ## 
+    # Technicalities:
+    ##
+    'Monitor'              :  False    ,          ## Activate the monitoring ?
+    ##
+    "HLT"                  : "HLT_PASS_RE('Hlt1MB.*Decision')" ,  ## HLT-cut
+    ##
+    'Preambulo'       : [
+    ## import c_light
+    "from GaudiKernel.PhysicalConstants import c_light" ,
+    ## define DD-category of K0S 
+    "DD =    CHILDCUT ( ISDOWN , 1 ) & CHILDCUT ( ISDOWN , 2 ) " ,
+    ## define LL-category of K0S 
+    "LL =    CHILDCUT ( ISLONG , 1 ) & CHILDCUT ( ISLONG , 2 ) "
+    ] ,
+    'KS0LL_Prescale'           : 0.022 ,
+    'KS0DD_Prescale'           : 0.027 , 
+    'LamLL_Prescale_LoP'       : 0.180 , 
+    'LamLL_Prescale_HiP'       : 1.000 ,
+    'LamDD_Prescale'           : 0.140 , 
+    'LamLLIsMUON_Prescale_LoP' : 1.000 ,
+    'LamLLIsMUON_Prescale_HiP' : 1.000 ,
+    'LamDDIsMUON_Prescale'     : 1.000   
+     }
+    }
+
+# PID: J/Psi -> e+ e-
+Jpsi2eeForElectronID = {
+    'BUILDERTYPE'       : 'ElectronIDConf',
+    'CONFIG'    : {
+        'JpsiLinePrescale'        :   0.5   ,
+        'JpsiLineHltFilter'       : None    ,
+
+        'Both_PT'                 :  250.   ,  # MeV
+        'Both_P'                  : 2000.   ,  # MeV
+        'Both_TRCHI2DOF'          :    5.   ,
+        'Both_MIPCHI2'            :    9.   ,
+        
+        'Tag_PT'                  : 1500.   ,  # MeV
+        'Tag_P'                   : 6000.   ,  # MeV
+        'Tag_PIDe'                :    5.   ,
+        'Tag_MIPCHI2'             :    9.   ,
+
+        'Probe_PT'                :  250.   ,  # MeV
+        'Probe_P'                 : 2000.   ,  # MeV
+        'Probe_MIPCHI2'           :    9.   ,
+        
+        'eeCombMinMass'           : 2100.   ,  # MeV         
+        'eeCombMaxMass'           : 4300.   ,  # MeV   
+        'eeVCHI2PDOF'             :    9.   ,  
+        'eeMinMass'               : 2200.   ,  # MeV 
+        'eeMaxMass'               : 4200.   ,  # MeV
+
+        'JpsiLineCut'             : "(PT>2.*GeV) & (BPVDLS>50) ",      
+        
+        'Bu2JpsieeKLine_Prescale'  :  1,
+        'Bu2JpsieeKLine_HltFilter' : None,
+        'Bu2JpsieeKLine_KaonCut'   : "(TRCHI2DOF<4) & (TRGHOSTPROB<0.5) & (PT>1.0*GeV) & (PIDK >0) & (BPVIPCHI2()>9)",
+        'Bu2JpsieeKLine_JpsiCut'   : "(BPVDLS>5)",
+        'Bu2JpsieeKLine_BuComCut'  : "in_range(4.1*GeV,AM,6.1*GeV)",
+        'Bu2JpsieeKLine_BuMomCut'  : "in_range(4.2*GeV,M, 6.0*GeV) & (VFASPF(VCHI2PDOF)<9)"
+        },
+    'STREAMS' : [ 'ALL' ],
+    'WGs'    : ['ALL']
+    }
+
+# PID: inclusive phi -> K+ K-
+InclPhi={
+    'BUILDERTYPE' : 'InclPhiConf',
+    'CONFIG' : {  'KaonPT'              : 500      # MeV
+                , 'KaonDLL'             : 15       # adimensional
+                , 'PhiMassWindow'       : 30       # MeV
+                , 'HighPtPrescale'      : 0.0030   # adimensional 
+                , 'LowPtPrescale'       : 0.0040   # adimensional 
+                , 'LDPrescale'          : 0.0002   # adimensional 
+                },
+    'STREAMS' : [ 'ALL' ],
+    'WGs'    : ['ALL']
+    }
