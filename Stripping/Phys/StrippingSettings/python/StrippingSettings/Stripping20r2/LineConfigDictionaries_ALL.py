30b15b5173306a7ff2b0dce18ed9524e3793f612
Lucio Anderlini <lucio.anderlini@cern.ch>
2013-06-12 17:40:38 +0000

    Added DiMuonForXsection stripping line for Stripping20r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@158253 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9bc8d4b042d3395363f80222bbb1e7da7d0dc955

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r2/LineConfigDictionaries_ALL.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r2/LineConfigDictionaries_ALL.py
@@ -762,17 +762,18 @@ FullDSTDiMuon = {
     }
 
 
-#DiMuonForXsection = {
-#    'BUILDERTYPE' : 'DiMuonForXsectionConf',
-#    'CONFIG' : {
-#    'TotalCuts'  :  """
-#    (MINTREE('mu+'==ABSID,PT)>0.65*GeV)
-#    & (VFASPF(VCHI2PDOF)<20)
-#    & ((ADMASS('J/psi(1S)')<120*MeV) | (ADMASS('psi(2S)')<120*MeV) | (MM>8.5*GeV))
-#    """,
-#    'Prescale'   :  1.,
-#    'CheckPV'    :  False
-#    },
-#    'STREAMS' : [ 'ALL' ] ,
-#    'WGs'    : [ 'BandQ' ]
-#    }
+DiMuonForXsection = {
+    'BUILDERTYPE' : 'DiMuonForXsectionConf',
+    'CONFIG' : {
+    'TotalCuts'  :  """
+     (MINTREE('mu+'==ABSID,PT)>0.5*GeV)
+      & (VFASPF(VCHI2PDOF)<20)
+      & (MM>2.9*GeV)
+    """,
+    'Prescale'   :  1.,
+    'CheckPV'    :  False
+    },
+    'STREAMS' : [ 'ALL' ] ,
+    'WGs'    : [ 'BandQ' ]
+    }
+
