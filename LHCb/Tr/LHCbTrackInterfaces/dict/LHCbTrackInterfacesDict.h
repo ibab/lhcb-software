aa3435fabdc0143292cd6e89faf934fa8562732d
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-06-18 13:26:35 +0000

    first import of LHCbTrackInterfaces under Tr
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@158537 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3ef6efc88a28daf456a5817ebe893273f6177e4b

---
+++ LHCb/Tr/LHCbTrackInterfaces/dict/LHCbTrackInterfacesDict.h
@@ -0,0 +1,13 @@
+#ifndef DICT_LHCBTRACKINTERFACESDICT_H 
+#define DICT_LHCBTRACKINTERFACESDICT_H 1
+
+#include "Kernel/STChannelID.h"
+#include "TrackInterfaces/ISTClusterPosition.h"
+#include "Kernel/VeloChannelID.h"
+#include "TrackInterfaces/IVeloClusterPosition.h"
+#include "Kernel/VLChannelID.h"
+#include "TrackInterfaces/IVLClusterPosition.h"
+#include "Kernel/VPChannelID.h"
+#include "TrackInterfaces/IVPClusterPosition.h"
+#endif // DICT_LHCBTRACKINTERFACESDICT_H
+
