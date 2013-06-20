0e82d7133baa67f78ce9b266e8e9dacd58c5ad7d
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-06-20 16:09:17 +0000

    Fix broken QMtest comparison with reference
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@158636 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 069cdc54a3da75a64ca773e33ab43785158a8187

--- Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
+++ Brunel/Rec/Brunel/python/Brunel/QMTest/BrunelExclusions.py
@@ -16,12 +16,12 @@ preprocessor = LHCbPreprocessor + \
   LineSkipper(["Memory for the event exceeds 3*sigma"]) + \
   LineSkipper(["Mean 'delta-memory' exceeds 3*sigma"]) + \
   LineSkipper(["BrunelInit.Brun...SUCCESS Exceptions/Errors/Warnings/Infos Statistics : 0/0/"]) + \
-  LineSkipper(["AfterMagnetRegion/"]) + \
-  LineSkipper(["MagnetRegion/"]) +\
-  LineSkipper(["BeforeMagnetRegion/"]) +\
-  LineSkipper(["DownstreamRegion"]) +\
+  LineSkipper(["| AfterMagnetRegion/"]) + \
+  LineSkipper(["| MagnetRegion/"]) +\
+  LineSkipper(["| BeforeMagnetRegion/"]) +\
+  LineSkipper(["| DownstreamRegion/"]) +\
   LineSkipper(["MD5 sum:"]) + \
-  LineSkipper("INFO ANNPID : Tune=MC12TuneV2    TrackType") + \
+  LineSkipper(["INFO ANNPID : Tune=MC12TuneV2    TrackType"]) + \
   RegexpReplacer(when = "RichHotPixels     WARNING Rich::HPDAnalysisAlg::",
                  orig = r'[01 ]{33}', repl = r'') +\
   RegexpReplacer(when = "RichHotPixels     SUCCESS  #WARNINGS   =",
