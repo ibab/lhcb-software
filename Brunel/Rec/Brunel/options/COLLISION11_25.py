810ef341d996c2451ef5734b6eea64009522f110
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-06-21 09:47:08 +0000

    Pick up input files from CERN-SWTEST (EOS)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@158659 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d4207e2afe5fe217fe7983bc942de5b221ce1e56

--- Brunel/Rec/Brunel/options/COLLISION11_25.py
+++ Brunel/Rec/Brunel/options/COLLISION11_25.py
@@ -15,5 +15,5 @@ importOptions("$APPCONFIGOPTS/Brunel/2011-25ns.py")
 
 # Events from run 103053, fill 2186 on 2011-10-07, 25ns bunch spacing
 EventSelector().Input = [
-  "DATAFILE='mdf:root://castorlhcb.cern.ch//castor/cern.ch/grid/lhcb/freezer/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11_25/103053/103053_0000000035.raw?svcClass=lhcbdisk' SVC='LHCb::MDFSelector'"
+  "DATAFILE='mdf:root://eoslhcb.cern.ch//eos/lhcb/swtest/lhcb/data/2011/RAW/FULL/LHCb/COLLISION11_25/103053/103053_0000000035.raw' SVC='LHCb::MDFSelector'"
     ]
