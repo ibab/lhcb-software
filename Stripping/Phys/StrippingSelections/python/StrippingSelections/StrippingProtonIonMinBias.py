9417d3591e8ce844e37788dd936371c8b26b2ce0
Ronan James Wallace <ronan.wallace@cern.ch>
2013-06-27 10:18:11 +0000

    Added new lines to StrippingProtonIonMinBias.py. Prescales depend on run.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@158883 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c94e32a3060403a954c4deba37db4273429c1d4a

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingProtonIonMinBias.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingProtonIonMinBias.py
@@ -1,47 +1,93 @@
-__author__ = 'Marco Meissner'
-__date__ = '08/05/2013'
-__version__ = '$Revision: 1 $'
+__author__ = 'Marco Meissner and Ronan Wallace'
+__date__ = '24/06/2013'
+__version__ = '$Revision: 2 $'
+
 
 """
 Minimum Bias Stripping selections for ProtonIon and IonProton physics
 """
 
 # Begin StrippingProtonIonMinBias.py
-
 config_params = { "ProtonIonMinBiasLine_RE"       : "(HLT_PASS('Hlt2PassThroughDecision'))",
-                  "ProtonIonMinBiasLine_Prescale" : 0.10,
+                  "ProtonIonMagDownMinBiasLine_RunNumbers"            :  "(ODIN_RUNNUMBER(135576,136212))",
+                  "ProtonIonMagUpMinBiasLine_RunNumbers"            :  "(ODIN_RUNNUMBER(136237,136341))",
+                  "IonProtonMagDownMinBiasLine_RunNumbers"            :  "(ODIN_RUNNUMBER(136799,137045))",
+                  "IonProtonMagUpMinBiasLine_RunNumbers"            :  "(ODIN_RUNNUMBER(136417,136758))",
+                  "ProtonIonMagDownMinBiasLine_Prescale" : 0.025,
+                  "ProtonIonMagUpMinBiasLine_Prescale" : 0.11,
+                  "IonProtonMagDownMinBiasLine_Prescale" : 0.08,
+                  "IonProtonMagUpMinBiasLine_Prescale" : 0.09,
                   "ProtonIonMinBiasLine_CondDB" : True ,
                   }
 
+
 __all__ = ('ProtonIonMinBiasConf' )
 
+#from LoKiHlt import *
 from StrippingConf.StrippingLine import StrippingLine
 from StrippingUtils.Utils import LineBuilder
 from PhysSelPython.Wrappers import Selection, DataOnDemand
+from Gaudi.Configuration import *
 
 class ProtonIonMinBiasConf(LineBuilder) :
     """
-    Builder for pA - Ap minimum bias line
+    Builder for pA - Ap minimum bias lines
     """
-    ProtonIonMinBiasLine = None
+    MinBiasLine = None
 
     __configuration_keys__ = ( 
         "ProtonIonMinBiasLine_RE",
-        "ProtonIonMinBiasLine_Prescale",
+        "ProtonIonMagDownMinBiasLine_RunNumbers",
+        "ProtonIonMagUpMinBiasLine_RunNumbers",
+        "IonProtonMagDownMinBiasLine_RunNumbers",
+        "IonProtonMagUpMinBiasLine_RunNumbers",
+        "ProtonIonMagDownMinBiasLine_Prescale",
+        "ProtonIonMagUpMinBiasLine_Prescale",
+        "IonProtonMagDownMinBiasLine_Prescale",
+        "IonProtonMagUpMinBiasLine_Prescale",
         "ProtonIonMinBiasLine_CondDB",
        )
 
     def __init__(self, name, config):
       LineBuilder.__init__(self, name, config)
-      self.ProtonIonMinBiasLine = self._makeLine("ProtonIonMinBias",
-                                       config["ProtonIonMinBiasLine_RE"],
-                                       config["ProtonIonMinBiasLine_Prescale"],
-                                       UseConditionDB=config["ProtonIonMinBiasLine_CondDB"] )
       
-    def _makeLine(self,name,RE,Prescale,UseConditionDB=False):
+      self._myname = name
+      
+      self.ProtonIonMagDownMinBias_line = self._makeLine("ProtonIonMagDownMinBiasLine",
+                                              config["ProtonIonMinBiasLine_RE"],
+                                              config["ProtonIonMagDownMinBiasLine_RunNumbers"],
+                                              config["ProtonIonMagDownMinBiasLine_Prescale"],
+                                              UseConditionDB=config["ProtonIonMinBiasLine_CondDB"] )
 
+
+      self.ProtonIonMagUpMinBias_line = self._makeLine("ProtonIonMagUpMinBiasLine",
+                                             config["ProtonIonMinBiasLine_RE"],
+                                             config["ProtonIonMagUpMinBiasLine_RunNumbers"],
+                                             config["ProtonIonMagUpMinBiasLine_Prescale"],
+                                             UseConditionDB=config["ProtonIonMinBiasLine_CondDB"] )
+
+
+      self.IonProtonMagDownMinBias_line = self._makeLine("IonProtonMagDownMinBiasLine",
+                                               config["ProtonIonMinBiasLine_RE"],
+                                               config["IonProtonMagDownMinBiasLine_RunNumbers"],
+                                               config["IonProtonMagDownMinBiasLine_Prescale"],
+                                               UseConditionDB=config["ProtonIonMinBiasLine_CondDB"] )
+
+
+      self.IonProtonMagUpMinBias_line = self._makeLine("IonProtonMagUpMinBiasLine",
+                                             config["ProtonIonMinBiasLine_RE"],
+                                             config["IonProtonMagUpMinBiasLine_RunNumbers"],
+                                             config["IonProtonMagUpMinBiasLine_Prescale"],
+                                             UseConditionDB=config["ProtonIonMinBiasLine_CondDB"] )
+
+
+
+
+
+    def _makeLine(self,name,RE,RunNumbers,Prescale,UseConditionDB=False):
       line = StrippingLine(name
                            , HLT =  RE
+                           , ODIN = RunNumbers
                            , checkPV = False 
                            , prescale = Prescale )
 
