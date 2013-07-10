08741b79d88691db6cba4c2e924328acfefae7bc
rlambert <rlambert@cern.ch>
2013-07-10 09:47:01 +0000

    added manipulation module, for adding functions for manipulating configurables. Simplifies IOHelper and will enable production outputlevel global settings.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@159584 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: bf9b0947dbd7fce178522da99931ce6d9a411fe0

---
+++ LHCb/GaudiConf/python/GaudiConf/Manipulations.py
@@ -0,0 +1,166 @@
+"""
+Simple python functions for setting common options to all configurables.
+Uses elements from HLTConf and GaudiConf
+
+Usage, something like:
+
+forAllConf(ApplicationMgr().TopAlg,{'OutputLevel':3})
+or
+forAllConf(fullNameConfigurables().keys(),{'OutputLevel':3})
+
+or same with postConfForAll...
+
+"""
+# Copied from GaudiConf, IOHelper. Manipulators of configurables.
+
+
+def fullNameConfigurables():
+    """
+    Simple function to find all defined configurables
+    and make a dictionary of {fullName : conf}
+    """
+    import Gaudi.Configuration as GaudiConfigurables
+    retdict={}
+    for key in GaudiConfigurables.allConfigurables:
+        retdict[GaudiConfigurables.allConfigurables[key].getFullName()]=GaudiConfigurables.allConfigurables[key]
+    return retdict
+    
+def nameFromConfigurable(conf):
+    """
+    Safe function to get the full name of a configurable
+    """
+    if type(conf) is str:
+        return conf
+    elif hasattr(conf, "getFullName"):
+        return conf.getFullName()
+    return None
+
+def configurableInstanceFromString(config):
+    '''Get a configurable instance given only the string
+    Uses  fullNameConfigurables() to get all defined configurables
+    If this configurable has not been instantiated, I instantiate it'''
+    
+    import Gaudi.Configuration as GaudiConfigurables
+    
+    #if it's in Gaudi.Configuration with this name, return it
+    
+    if config in fullNameConfigurables():
+        return fullNameConfigurables()[config]
+
+    #might be using a short name instead:
+    if config in GaudiConfigurables.allConfigurables:
+        return GaudiConfigurables.allConfigurables[config]
+    
+    config=config.replace('::','__')
+    
+    #Try again with __ instead of :: to cope with namespaces!
+    
+    if config in fullNameConfigurables():
+        return fullNameConfigurables()[config]
+    
+    if config in GaudiConfigurables.allConfigurables:
+        return GaudiConfigurables.allConfigurables[config]
+    
+    #Since I didn't find it, I need to create it:
+    wclass=None
+
+    if hasattr(GaudiConfigurables, config.split('/')[0]):
+        wclass = getattr(GaudiConfigurables,config.split('/')[0])
+    else:
+        import Configurables
+        wclass = getattr(Configurables,config.split('/')[0])
+        #otherwise it must be a configurable
+        
+    #check if it has an instance name
+    if '/' not in config:
+        return wclass()
+    else:
+        return wclass(name=config.split('/')[-1])
+
+# Copied from HLT Conf, setting given configurables with a dictionary:
+
+def forAllConf( head, prop_value_dict, types=[], force=False ) :
+    """ Find all configurable algorithms and set certain properties
+    
+    head_conf: can be a sequence or a list, or a configurable to start with.
+    prop_value_dict: A dictionary of Property: Value, e.g. {'OutputLevel':3}
+    types: A list of types to check against, e.g. ['FilterDesktop','CombineParticles','DVAlgorithm'...], default empty list, doesn't check for types
+    force: Overwrite properties even if they are already set, default False
+    
+    To obtain all configurables try something like:
+    forAllConf(ApplicationMgr().TopAlg,{'OutputLevel':3})
+    or:
+    forAllConf(fullNameConfigurables(),keys(),{'OutputLevel':3})
+    """
+    if type(prop_value_dict) is not dict:
+        raise TypeError("Hey, you need to give me a dictionary, you passed me a, "+str(type(prop_value_dict)))
+    
+    #recurse over lists
+    if type(head) is list:
+        for i in head: forAllConf(i,prop_value_dict,types,force)
+    
+    #interpret strings:
+    if type(head) is str:
+        try:
+            head=configurableInstanceFromString(head)
+        except:
+            #I cannot find the configurable, skip it
+            return
+    
+    if head is None:
+        return
+    #print "attempting",  nameFromConfigurable(head)
+    if (not len(types)) or head.getType() in types:
+        for prop in prop_value_dict:
+            if hasattr(head,prop) or (hasattr(head,"properties") and prop in head.properties()) or(hasattr(head,"__slots__") and prop in head.__slots__):
+                if force or (not head.isPropertySet(prop)):
+                    head.setProp(prop,prop_value_dict[prop])
+                    #print "set", nameFromConfigurable(head)
+    
+    #iterate/recurse over all subparts
+    for p in [ 'Members','Filter0','Filter1','TopAlg' ] :
+        if not hasattr(head,p) : continue
+        seq = getattr(head,p)
+        forAllConf(seq,prop_value_dict,types,force) 
+
+def postConfigCallable(*args,**kwargs):
+    """
+    Setup a post comfig action for any function requiring arguments
+    
+    usage:
+    
+    postConfigCallable(<afunction>,<arg1>,...,<kw1>=<kwarg1>,...)
+    
+    first unnamed argument MUST be a function to call in postConfig
+    
+    all other named or unnamed arguments are passed into the function
+    
+    """
+    from Gaudi.Configuration import appendPostConfigAction
+    import collections
+    
+    class dummyPostConf(object):
+        """dummy class to wrap function call"""
+        def __init__(self,thefunction,theargs,thekwargs):
+            self.thefunction=thefunction
+            self.theargs=theargs
+            self.thekwargs=thekwargs
+        def method(self):
+            return self.thefunction(*self.theargs,**self.thekwargs)
+    
+    if not len(args):
+        raise ValueError("You must pass at least one nameless arg, the function to call!")
+    if not isinstance(args[0], collections.Callable):
+        raise TypeError("The first nameless argument must be a callable function!")
+    mydummy=None
+    if len(args)==1:
+        mydummy=dummyPostConf(args[0],kwargs)
+    else:
+        mydummy=dummyPostConf(args[0],args[1:],kwargs)
+    appendPostConfigAction(mydummy.method)
+
+def postConfForAll(head, prop_value_dict, types=[], force=False ) :
+    """postConfigCallable with signature for forAllConf
+    Append postConfigAction or forAllConf"""
+    postConfigCallable(forAllConf,head,prop_value_dict, types=[], force=False)
+
---
+++ LHCb/GaudiConf/tests/options/test-manipulations.py
@@ -0,0 +1,61 @@
+#test the import
+from GaudiConf.Manipulations import *
+
+#create some algs from Gaudi, ready to have their properties set
+from Configurables import InputCopyStream, EventCounter
+ics=InputCopyStream(name="I_Created_This")
+ec=EventCounter(name="I_Also_Created_This")
+
+if nameFromConfigurable(ics)!="InputCopyStream/I_Created_This":
+    raise ValueError("Name found is not correct",nameFromConfigurable(ics))
+
+
+if configurableInstanceFromString(nameFromConfigurable(ics))!=ics:
+    raise ValueError("the configurable instance wasn't the same as I created")
+
+ops="OutputStream/You_Created_This"
+
+if nameFromConfigurable(configurableInstanceFromString(ops))!=ops:
+    raise ValueError("I wasn't able to create the configurable requested")
+
+if (ops not in fullNameConfigurables()) or (nameFromConfigurable(ics) not in fullNameConfigurables()) or (nameFromConfigurable(ec) not in fullNameConfigurables()):
+    raise ValueError("I couldn't find the configurables we just created, "+fullNameConfigurables().__str__())
+
+ics.OutputLevel=2
+
+forAllConf(fullNameConfigurables().keys(),{'OutputLevel':3,"OutputFile":'ASpamName'})
+
+if not ec.isPropertySet("OutputLevel"):
+    raise AttributeError("I didn't set the OutputLevel of the EventCounter you made")
+if ics.getProp("OutputLevel")!=2:
+    raise AttributeError("I set the output level of ics when it was already set elsewhere")
+for conf in [configurableInstanceFromString(ops),ics]:
+    if (not conf.isPropertySet("OutputFile")) or conf.getProp("OutputFile")!='ASpamName':
+        raise ("I didn't correctly set the property of output file for "+nameFromConfigurable(conf))
+
+fdict={'OutputLevel':7,"OutputFile":'AnotherSpamName'}
+
+forAllConf(fullNameConfigurables().keys(),fdict, force=True)
+
+for conf in [configurableInstanceFromString(ops),ics,ec]:
+    for key in fdict:
+        if hasattr(conf,key) or (hasattr(conf,"properties") and key in conf.properties()) or(hasattr(conf,"__slots__") and key in conf.__slots__):
+            if (not conf.isPropertySet(key)) or conf.getProp(key)!=fdict[key]:
+                raise ("I didn't correctly set the property of" +key+" for "+nameFromConfigurable(conf))
+
+#check that I follow down sequencers!
+gs=configurableInstanceFromString("GaudiSequencer/testSeq")
+gs.Members=["EventCounter/NewCounter","InputCopyStream/newStream","OutputStream/spamStream"]
+
+forAllConf(fullNameConfigurables().keys(),fdict, force=True)
+for conf in gs.Members:
+    conf=configurableInstanceFromString(conf)
+    for key in fdict:
+        if hasattr(conf,key) or (hasattr(conf,"properties") and key in conf.properties()) or(hasattr(conf,"__slots__") and key in conf.__slots__):
+            if (not conf.isPropertySet(key)) or conf.getProp(key)!=fdict[key]:
+                raise ("I didn't correctly set the property of" +key+" for "+nameFromConfigurable(conf))
+
+#test that I successfully can add a postConfigAction
+postConfForAll(fullNameConfigurables().keys(),fdict, force=True)
+
+print "PASS"
