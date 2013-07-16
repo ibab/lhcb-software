da90338a3788725c77fb324585d17ec50baf7c08
Patrick Robbe <robbe@lal.in2p3.fr>
2013-07-16 16:14:36 +0000

    Modifications for LbPowheg
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@159866 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2b4c475369621957b6ef12af836911c4a6ee51fa

--- Gauss/Gen/LbPythia/src/component/PythiaProductionFactory.cpp
+++ Gauss/Gen/LbPythia/src/component/PythiaProductionFactory.cpp
@@ -1,9 +1,11 @@
 // Include files
 #include "GaudiKernel/ToolFactory.h"
 #include "LbPythia/PythiaProduction.h"
+#include "LbPythia/ReadLHE.h"
 
 // It must be present otherwise the class cannot be instanciated, since the
 // Factory is in the linker library since it can (and is) specialized by 
 // other production tools based on Pythia
 DECLARE_TOOL_FACTORY( PythiaProduction )
+DECLARE_NAMESPACE_TOOL_FACTORY(LbPythia,ReadLHE)
 
