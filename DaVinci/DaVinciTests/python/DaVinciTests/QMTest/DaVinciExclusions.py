87140cff3eb6688dd43bd2dde49b273e0f08ccbb
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-07-22 14:30:36 +0000

    turn off tag exclusions
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@160215 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 173c7fcefca495b7f882417bc28cfbc39c33163c

--- DaVinci/DaVinciTests/python/DaVinciTests/QMTest/DaVinciExclusions.py
+++ DaVinci/DaVinciTests/python/DaVinciTests/QMTest/DaVinciExclusions.py
@@ -7,9 +7,9 @@ preprocessor = LHCbPreprocessor + \
   LineSkipper(["MagneticFieldSvc                 INFO Map scaled by factor"]) + \
   LineSkipper(["MagneticFieldSvc                 INFO Opened magnetic field file"]) + \
   LineSkipper(["ConfigTarFileAccessSvc           INFO  opening "]) + \
-  LineSkipper(["LHCBCOND_"]) + \
-  LineSkipper(["SIMCOND_"]) + \
-  LineSkipper(["DDDB_"]) + \
+  #LineSkipper(["LHCBCOND_"]) + \
+  #LineSkipper(["SIMCOND_"]) + \
+  #LineSkipper(["DDDB_"]) + \
   LineSkipper(["INFO MuonIDAlg v"]) + \
   LineSkipper(["Memory for the event exceeds 3*sigma"]) + \
   LineSkipper(["Mean 'delta-memory' exceeds 3*sigma"]) + \
