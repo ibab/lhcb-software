a81f58e302b090f40b0144c217462b4a5e0f425f
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-07-22 14:36:37 +0000

    turn off tag exclusions - typo fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@160219 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 51c2c9c436494911317c0790eebadc8622d77d75

--- DaVinci/DaVinciTests/python/DaVinciTests/QMTest/DaVinciExclusions.py
+++ DaVinci/DaVinciTests/python/DaVinciTests/QMTest/DaVinciExclusions.py
@@ -7,14 +7,15 @@ preprocessor = LHCbPreprocessor + \
   LineSkipper(["MagneticFieldSvc                 INFO Map scaled by factor"]) + \
   LineSkipper(["MagneticFieldSvc                 INFO Opened magnetic field file"]) + \
   LineSkipper(["ConfigTarFileAccessSvc           INFO  opening "]) + \
-  #LineSkipper(["LHCBCOND_"]) + \
-  #LineSkipper(["SIMCOND_"]) + \
-  #LineSkipper(["DDDB_"]) + \
   LineSkipper(["INFO MuonIDAlg v"]) + \
   LineSkipper(["Memory for the event exceeds 3*sigma"]) + \
   LineSkipper(["Mean 'delta-memory' exceeds 3*sigma"]) + \
   LineSkipper(["AfterMagnetRegion/"]) + \
-  LineSkipper(["MagnetRegion/"]) +\
-  LineSkipper(["BeforeMagnetRegion/"]) +\
-  LineSkipper(["DownstreamRegion"]) +\
+  LineSkipper(["MagnetRegion/"]) + \
+  LineSkipper(["BeforeMagnetRegion/"]) + \
+  LineSkipper(["DownstreamRegion"]) + \
   LineSkipper(["MD5 sum:"]) 
+
+#LineSkipper(["LHCBCOND_"]) + \
+#LineSkipper(["SIMCOND_"]) + \
+#LineSkipper(["DDDB_"]) + \
