6658bcdf280f0f57046eacdfcc24009f373f6c23
Francesco Polci <francesco.polci@cern.ch>
2013-07-24 11:24:44 +0000

    switch old/new FTDet in PrFTHiManager
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@160301 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e13a6700dec7103c166bd8aba56683ef63d45283

--- Rec/Pr/PrAlgorithms/src/PrFTHitManager.cpp
+++ Rec/Pr/PrAlgorithms/src/PrFTHitManager.cpp
@@ -7,6 +7,8 @@
 // local
 #include "PrFTHitManager.h"
 
+//#include  "Kernel/FTChannelID.h"
+
 //-----------------------------------------------------------------------------
 // Implementation file for class : PrFTHitManager
 //
@@ -37,24 +39,32 @@ PrFTHitManager::~PrFTHitManager() {}
 //=========================================================================
 //  Create the zones in the hit manager, 'zone' is a method of the base class
 //=========================================================================
-void PrFTHitManager::buildGeometry ( ) {
+void PrFTHitManager::buildGeometry ( ) {  
   IRndmGenSvc* randSvc;
   service( "RndmGenSvc", randSvc );
   m_gauss.initialize( randSvc, Rndm::Gauss( 0., 1. ) );
   m_ftDet = getDet<DeFTDetector>( DeFTDetectorLocation::Default );
-  for ( std::vector<DeFTLayer*>::const_iterator itL = m_ftDet->layers().begin();
-        m_ftDet->layers().end() != itL; ++itL ) {
-    int id = (*itL)->layerID();
-    LHCb::FTChannelID tmp( id, 0, 0, 0 );
-    DetectorSegment seg = (*itL)->createDetSegment( tmp, 0. );
-    zone( 2*id   )->setGeometry( seg );
-    zone( 2*id+1 )->setGeometry( seg );
-    zone( 2*id   )->setBoundaries( -4090., 4090., -3030., 50. );
-    zone( 2*id+1 )->setBoundaries( -4090., 4090., -50., 3030. );    
+  DetectorSegment seg;
+
+  for ( std::vector<DeFTLayer*>::const_iterator itL = m_ftDet->layers().begin();  //loop over layers
+	m_ftDet->layers().end() != itL; ++itL ) {
+    int id = (*itL)->params()->param<int>("layerID");  //ask layerID
+    LHCb::FTChannelID tmp( id, 0, 0, 0 );  
+    if(m_ftDet->version() != 20) {
+      seg = (*itL)->createDetSegment( tmp, 0. );   //create detector segment 
+    }else{
+      const DeFTFibreMat* ftMat = m_ftDet->findFibreMat(tmp);
+      seg = ftMat->createDetSegment( tmp, 0. ); 
+    }
+    zone( 2*id   )->setGeometry( seg ); // ex:layers 0 down
+    zone( 2*id+1 )->setGeometry( seg ); // ex:layers 0 up 
+    zone( 2*id   )->setBoundaries( -4090., 4090., -3030., 50. ); //check this boudaries values for zone down 
+    zone( 2*id+1 )->setBoundaries( -4090., 4090., -50., 3030. ); //check this boudaries values for zone up   
     debug() << "Zone " << id << " z " << zone(2*id)->z() << " angle " << zone(2*id)->dxDy() << endmsg;
   }
   info() << "XSmearing " << m_xSmearing << " ZSmearing " << m_zSmearing << endmsg;
 }
+
 
 //=========================================================================
 //  Decode an event.
@@ -64,65 +74,138 @@ void PrFTHitManager::decodeData ( ) {
   typedef FastClusterContainer<LHCb::FTRawCluster,int> FTRawClusters;
   FTRawClusters* clus = get<FTRawClusters>( LHCb::FTRawClusterLocation::Default );
   DeFTLayer* myLayer = NULL;
+  const DeFTFibreMat* ftMat = NULL;
   unsigned int oldLayer = 1000;
+  unsigned int oldFibreMat = -1000;
+
   debug() << "Retrieved " << clus->size() << " clusters" << endmsg;
 
   float zDisplacement = 0.;
   unsigned int oldBiLayer = 1000;
 
-  for ( FTRawClusters::iterator itC = clus->begin(); clus->end() != itC; ++itC ) {
-    if( (*itC).channelID().layer() != oldLayer ) {
-      oldLayer =(*itC).channelID().layer();
-      myLayer = m_ftDet->layers()[oldLayer];
-      if ( NULL == myLayer ) {
-        info() << "Layer not found for FT channelID " << (*itC).channelID() << endmsg;    
-        oldLayer = 1000;
+  DetectorSegment seg ; 
+  if(m_ftDet->version() != 20) { //OLD DETECTOR VERSION
+    debug() << "I HAVE THE OLD DETECTOR VERSION" << endmsg;
+    for ( FTRawClusters::iterator itC = clus->begin(); clus->end() != itC; ++itC ) {
+      if( (*itC).channelID().layer() != oldLayer ) {  
+	oldLayer =(*itC).channelID().layer();  
+	myLayer = m_ftDet->layers()[oldLayer];      
+	if ( NULL == myLayer ) {
+	  info() << "Layer not found for FT channelID " << (*itC).channelID() << endmsg;    
+	  oldLayer = 1000;
+	}
+	unsigned int biLayer = (*itC).channelID().layer()/2;  
+	if ( m_zSmearing > 0. && biLayer != oldBiLayer ) {
+	  oldBiLayer = biLayer;
+	  zDisplacement = m_zSmearing * m_gauss();
+	  if ( msgLevel( MSG::DEBUG ) ) {
+	    debug () << "Bilayer " << biLayer << " z displacement " << zDisplacement << endmsg;
+	  }
+	}
       }
-      unsigned int biLayer = (*itC).channelID().layer()/2;
-      if ( m_zSmearing > 0. && biLayer != oldBiLayer ) {
-        oldBiLayer = biLayer;
-        zDisplacement = m_zSmearing * m_gauss();
-        if ( msgLevel( MSG::DEBUG ) ) {
-          debug () << "Bilayer " << biLayer << " z displacement " << zDisplacement << endmsg;
-        }
-      }
-    }
-    float fraction = (*itC).fraction() + 0.125;   // Truncated to 4 bits = 0.25. Add half of it
-    LHCb::FTChannelID id = (*itC).channelID();
-    DetectorSegment seg = myLayer->createDetSegment( id, fraction );
-
-    //== Add some smearing if needed
-    if ( m_xSmearing > 0. || m_zSmearing > 0. ) {
-      float x0 = seg.x(0.);
-      float z0 = seg.z(0.);
-      float dxDy = 0.001 * ( seg.x(1000.)-seg.x(0.) );
-      float dzDy = 0.001 * ( seg.z(1000.)-seg.z(0.) );
-      float yMin = seg.yMin();
-      float yMax = seg.yMax();
+      float fraction = (*itC).fraction() + 0.125;   // Truncated to 4 bits = 0.25. Add half of it
+      LHCb::FTChannelID id = (*itC).channelID();
+      seg = myLayer->createDetSegment( id, fraction );   //create detector segment  
       
-      if ( m_xSmearing > 0. ) x0 = x0 + m_xSmearing * m_gauss();
-      if ( m_zSmearing > 0. ) z0 = z0 + zDisplacement;
-      DetectorSegment tmp( x0, z0, dxDy, dzDy, yMin, yMax );
-      seg = tmp;
+      //== Add some smearing if needed
+      if ( m_xSmearing > 0. || m_zSmearing > 0. ) {
+	float x0 = seg.x(0.);
+	float z0 = seg.z(0.);
+	float dxDy = 0.001 * ( seg.x(1000.)-seg.x(0.) );
+	float dzDy = 0.001 * ( seg.z(1000.)-seg.z(0.) );
+	float yMin = seg.yMin();
+	float yMax = seg.yMax();
+	
+	if ( m_xSmearing > 0. ) x0 = x0 + m_xSmearing * m_gauss();
+	if ( m_zSmearing > 0. ) z0 = z0 + zDisplacement;
+	DetectorSegment tmp( x0, z0, dxDy, dzDy, yMin, yMax );
+	seg = tmp;
+      }
+      
+      int lay  = (*itC).channelID().layer();  
+      int zone = ( (*itC).channelID().quarter() > 1 ) ? 1 : 0; 
+      int code = 2*lay + zone;
+      PrHit* aHit = newHitInZone( code );
+      float errX = 0.05 + .03 * (*itC).size();
+      aHit->setHit( LHCb::LHCbID( (*itC).channelID() ), (*itC).size(), (*itC).charge(), seg, errX , zone, lay );
+      debug() << " .. hit " << (*itC).channelID() << " zone " << zone << " x " << seg.x(0.) << endmsg;
+    }
+    /*  
+    //== Remove a bilayer.
+    zone(0 ) ->hits().clear();
+    zone(1  )->hits().clear();
+    zone(2  )->hits().clear();
+    zone(3  )->hits().clear();
+    */
+    for ( unsigned int lay = 0; nbZones() > lay ; ++lay ) {
+      std::sort( zone(lay)->hits().begin(), zone(lay)->hits().end(), PrHit::LowerByCoord() );
+    }
+    
+  } else { //NEW DETECTOR
+   debug() << "I HAVE THE NEW DETECTOR VERSION" << endmsg;
+    for ( FTRawClusters::iterator itC = clus->begin(); clus->end() != itC; ++itC ) {
+      always() << "IN THE LOOP CHANNELID LAYER" << (*itC).channelID().layer() << endmsg;
+      ftMat = m_ftDet->findFibreMat( (*itC).channelID() );
+      debug() << "MYFIBERMAT " << ftMat->FibreMatID() << endmsg;
+      if( ftMat->FibreMatID() != oldFibreMat ) {  
+	//oldFibreMat =(*itC).channelID().layer();
+	//giveMeFibreMat((*itC).channelID(),myFibreMat);  
+	oldFibreMat= ftMat->FibreMatID();
+	debug() << "FIBERMATID = " <<ftMat->FibreMatID() << endmsg;
+	if ( NULL == ftMat ) {
+	  info() << "FiberMat not found for FT channelID " << (*itC).channelID() << endmsg;    
+	  oldFibreMat = -1000;
+	}
+	////SMEARING OF BILAYER NOW SHOULD BE DONE AT WHICH LEVEL???
+	// unsigned int biLayer = (*itC).channelID().layer()/2;  
+	// if ( m_zSmearing > 0. && biLayer != oldBiLayer ) {
+	//   oldBiLayer = biLayer;
+	//   zDisplacement = m_zSmearing * m_gauss();
+	//   if ( msgLevel( MSG::DEBUG ) ) {
+	//     debug () << "Bilayer " << biLayer << " z displacement " << zDisplacement << endmsg;
+	//   }
+	//}
+      }
+      float fraction = (*itC).fraction() + 0.125;   // Truncated to 4 bits = 0.25. Add half of it
+      LHCb::FTChannelID id = (*itC).channelID();
+      seg = ftMat->createDetSegment( id, fraction );   //create detector segment  
+    
+      //== Add some smearing if needed
+      if ( m_xSmearing > 0. || m_zSmearing > 0. ) {
+	float x0 = seg.x(0.);
+	float z0 = seg.z(0.);
+	float dxDy = 0.001 * ( seg.x(1000.)-seg.x(0.) );
+	float dzDy = 0.001 * ( seg.z(1000.)-seg.z(0.) );
+	float yMin = seg.yMin();
+	float yMax = seg.yMax();
+	
+	if ( m_xSmearing > 0. ) x0 = x0 + m_xSmearing * m_gauss();
+	if ( m_zSmearing > 0. ) z0 = z0 + zDisplacement;
+	DetectorSegment tmp( x0, z0, dxDy, dzDy, yMin, yMax );
+	seg = tmp;
+      }
+
+      int lay  = (*itC).channelID().layer();   
+      int zone = ( (*itC).channelID().quarter() > 1 ) ? 1 : 0; 
+      int code = 2*lay + zone;
+      PrHit* aHit = newHitInZone( code );
+      float errX = 0.05 + .03 * (*itC).size();
+      aHit->setHit( LHCb::LHCbID( (*itC).channelID() ), (*itC).size(), (*itC).charge(), seg, errX , zone, lay );
+      debug() << " .. hit " << (*itC).channelID() << " zone " << zone << " x " << seg.x(0.) << endmsg;
     }
 
-    int lay  = (*itC).channelID().layer();
-    int zone = ( (*itC).channelID().quarter() > 1 ) ? 1 : 0;
-    int code = 2*lay + zone;
-    PrHit* aHit = newHitInZone( code );
-    float errX = 0.05 + .03 * (*itC).size();
-    aHit->setHit( LHCb::LHCbID( (*itC).channelID() ), (*itC).size(), (*itC).charge(), seg, errX , zone, lay );
-    debug() << " .. hit " << (*itC).channelID() << " zone " << zone << " x " << seg.x(0.) << endmsg;
-  }
-  /*  
-  //== Remove a bilayer.
-  zone(0 ) ->hits().clear();
-  zone(1  )->hits().clear();
-  zone(2  )->hits().clear();
-  zone(3  )->hits().clear();
-  */
-  for ( unsigned int lay = 0; nbZones() > lay ; ++lay ) {
-    std::sort( zone(lay)->hits().begin(), zone(lay)->hits().end(), PrHit::LowerByCoord() );
-  }
+    /*  
+    //== Remove a bilayer.
+    zone(0 ) ->hits().clear();
+    zone(1  )->hits().clear();
+    zone(2  )->hits().clear();
+    zone(3  )->hits().clear();
+    */
+    for ( unsigned int lay = 0; nbZones() > lay ; ++lay ) {
+      std::sort( zone(lay)->hits().begin(), zone(lay)->hits().end(), PrHit::LowerByCoord() );
+    }
+    
+  }  
+  
 }
 //=============================================================================
