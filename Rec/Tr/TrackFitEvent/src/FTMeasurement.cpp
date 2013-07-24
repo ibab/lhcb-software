bc4e9b34fed5e2db0ceaba5757642aa5a2cb53f5
Francesco Polci <francesco.polci@cern.ch>
2013-07-24 11:27:47 +0000

    switch old/new FTDet in FTMeasurement.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@160303 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 654d0d7c624683ce68c74f097176709d06b99cf1

--- Rec/Tr/TrackFitEvent/src/FTMeasurement.cpp
+++ Rec/Tr/TrackFitEvent/src/FTMeasurement.cpp
@@ -17,18 +17,36 @@ using namespace LHCb;
 //  2012-11-27 Olivier Callot, from STMeasurement
 //-----------------------------------------------------------------------------
 void FTMeasurement::init( const DeFTDetector& geom ) {
- 
   // Get the corresponding layer
-  const DeFTLayer* ftLayer = geom.findLayer( m_cluster.channelID() );
-  m_detectorElement = ftLayer;
-  DetectorSegment mySeg = ftLayer->createDetSegment( m_cluster.channelID(), m_cluster.fraction() );
-  Gaudi::XYZPoint begPoint( mySeg.x( mySeg.yMin() ), mySeg.yMin(), mySeg.z( mySeg.yMin() ) );
-  Gaudi::XYZPoint endPoint( mySeg.x( mySeg.yMax() ), mySeg.yMax(), mySeg.z( mySeg.yMax() ) );
-  
-  m_size = m_cluster.size();
-  m_errMeasure = 0.050 + .030 * m_size;
-  LHCb::LineTraj* traj = new LHCb::LineTraj( begPoint, endPoint );
-  m_trajectory = std::auto_ptr<LHCb::Trajectory>( traj );
-  m_z = ftLayer->layerCenterZ();
-  m_measure = mySeg.x( 0. );
+  if( geom.version() != 20) { 
+    const DeFTLayer* ftLayer = geom.findLayer( m_cluster.channelID() );
+    m_detectorElement = ftLayer;
+    DetectorSegment mySeg = ftLayer->createDetSegment( m_cluster.channelID(), m_cluster.fraction() );
+    
+    Gaudi::XYZPoint begPoint( mySeg.x( mySeg.yMin() ), mySeg.yMin(), mySeg.z( mySeg.yMin() ) );
+    Gaudi::XYZPoint endPoint( mySeg.x( mySeg.yMax() ), mySeg.yMax(), mySeg.z( mySeg.yMax() ) );
+    
+    m_size = m_cluster.size();
+    m_errMeasure = 0.050 + .030 * m_size;
+    LHCb::LineTraj* traj = new LHCb::LineTraj( begPoint, endPoint );
+    m_trajectory = std::auto_ptr<LHCb::Trajectory>( traj );
+    m_z = ftLayer->layerCenterZ();
+    m_measure = mySeg.x( 0. );
+    
+  } else {  
+    const DeFTFibreMat* ftMat = geom.findFibreMat( m_cluster.channelID() );
+    m_detectorElement = ftMat;
+    DetectorSegment mySeg = ftMat->createDetSegment( m_cluster.channelID(), m_cluster.fraction() );
+    Gaudi::XYZPoint begPoint( mySeg.x( mySeg.yMin() ), mySeg.yMin(), mySeg.z( mySeg.yMin() ) );
+    Gaudi::XYZPoint endPoint( mySeg.x( mySeg.yMax() ), mySeg.yMax(), mySeg.z( mySeg.yMax() ) );
+    
+    m_size = m_cluster.size();
+    m_errMeasure = 0.050 + .030 * m_size;
+    LHCb::LineTraj* traj = new LHCb::LineTraj( begPoint, endPoint );
+    m_trajectory = std::auto_ptr<LHCb::Trajectory>( traj );
+    m_z = ftMat->layerCenterZ();
+    m_measure = mySeg.x( 0. );
+    
+  }
 }
+
