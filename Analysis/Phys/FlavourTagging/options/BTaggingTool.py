b5b5826916600ad25978c6de7ae508409095c838
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-07-25 14:36:24 +0000

    ! 2013-07-25 - Chris Jones  - Add a new python Configurable, to replace the job of      importOptions("$FLAVOURTAGGINGROOT/options/BTaggingTool.py")    which is to be depreciated. The above file still exists, but now prints    a large depreciation warning, and then just imports the new Configurable.    Should be removed at some point.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@160473 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 857bb15e4e61d02ea01d69b5fc433b9fbd1f40c9

--- Analysis/Phys/FlavourTagging/options/BTaggingTool.py
+++ Analysis/Phys/FlavourTagging/options/BTaggingTool.py
@@ -1,48 +1,29 @@
+
 ##############################################################################
 #
 # Tagging options
 #
-#
 ##############################################################################
-from Gaudi.Configuration import *
-from Configurables import ( CombinedParticleMaker, ProtoParticleMUONFilter,
-                            ProtoParticleCALOFilter, ProtoParticleRICHFilter,
-                            FilterDesktop )
 
-mName = "TaggingMuons"
-eName = "TaggingElectrons"
-pName = "TaggingPions"
-tagName = 'TaggingParticles'
+print " "
+print "==========================================================================="
+print "  WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING"
+print " "
+print "    The syntax :-"
+print " "
+print """      importOptions("$FLAVOURTAGGINGROOT/options/BTaggingTool.py")"""
+print " "
+print "    is OBSOLETE. Please instead use :-"
+print " "
+print "      from Configurables import FlavourTaggingConf"
+print "      ftConf = FlavourTaggingConf()"
+print " "
+print "  WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING"
+print "==========================================================================="
+print " "
 
-hat = 'Phys/'
-leaf = '/Particles'
-
-mLoc, eLoc, pLoc, tagLoc = [ hat+name+leaf for name in mName, eName, pName, tagName]
-
-taggerMuons = CombinedParticleMaker(mName)
-taggerMuons.Particle = 'Muon'
-taggerMuons.addTool(ProtoParticleMUONFilter, name="Muon")
-taggerMuons.Muon.Selection = [ "RequiresDet='MUON' CombDLL(mu-pi)>'0.0'" ]
-
-taggerElectrons = CombinedParticleMaker(eName)
-taggerElectrons.Particle = 'Electron'
-taggerElectrons.addTool(ProtoParticleCALOFilter, name="Electron")
-taggerElectrons.Electron.Selection = [ "RequiresDet='CALO' CombDLL(e-pi)>'4.0'" ]
-
-taggerPions = CombinedParticleMaker(pName)
-taggerPions.Particle = 'Pion'
-
-
-taggingParticles = FilterDesktop(tagName)
-taggingParticles.Inputs = [ eLoc, mLoc, pLoc ]
-taggingParticles.Code = "(P>2.0*GeV)"    
-
-
-
-dod = DataOnDemandSvc()
-dod.AlgMap.update( { mLoc   : 'CombinedParticleMaker/'+mName } )
-dod.AlgMap.update( { eLoc   : 'CombinedParticleMaker/'+eName } )
-dod.AlgMap.update( { pLoc   : 'CombinedParticleMaker/'+pName } )
-dod.AlgMap.update( { tagLoc : 'FilterDesktop/'+tagName       } )
+from Configurables import FlavourTaggingConf
+# Just create an instance. Nothing to configure as of yet
+ftConf = FlavourTaggingConf()
 
 ##############################################################################
