10c6b956ad931e4bcd813b03920a56a0331be165
Sebastian Neubert <sebastian.neubert@cern.ch>
2013-07-30 15:31:19 +0000

    added IParticleDictTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@160594 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dd962389fe21374b8a8480ff6674c175632ffc4f

---
+++ Phys/Phys/DaVinciInterfaces/Kernel/IParticleDictTool.h
@@ -0,0 +1,41 @@
+#ifndef IPARTICLEDICTTOOL_H 
+#define IPARTICLEDICTTOOL_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+#include "GaudiKernel/VectorMap.h"
+#include "LoKi/PhysTypes.h"
+
+static const InterfaceID IID_IParticleDictTool ( "IParticleDictTool", 1, 0 );
+
+/** @class IParticleDictTool IParticleDictTool.h
+ *  returns a dictionary of variable-names and their values
+ *
+ *  @author Sebastian Neubert
+ *  @date   2013-07-08
+ *
+ */
+
+class IParticleDictTool : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IParticleDictTool; }
+
+  typedef GaudiUtils::VectorMap<std::string,double> DICT;
+  
+  /** fill the dictionary: 
+   *  @param p (INPUT) the particle
+   *  @param head (INPUT) prefix for ditionary variables
+   *  @param dict (UPDATE) the dictionary to be filled  
+   */
+  virtual StatusCode  fill ( const LHCb::Particle* p , const std::string& head, DICT& dict ) const =0;
+
+
+
+};
+#endif // IPARTICLEDICTTOOL_H
