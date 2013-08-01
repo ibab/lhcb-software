e8afbfb16e0ef1cc3e51f4f0a2f9e6532e3c4116
rlambert <rlambert@cern.ch>
2013-08-01 09:50:21 +0000

    - Fix uninitialized variable in ConfiguredTrackMonitors  - Run all monitors if the LHCbApp property isn't set
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@160705 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4e9130d831678e4766924364bbf1e486d3c2dd58

--- Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
+++ Rec/Tr/TrackMonitors/python/TrackMonitors/ConfiguredTrackMonitors.py
@@ -14,6 +14,7 @@ def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
 
     # figure out detectors
     seq = GaudiSequencer(Name)
+    subDets=None #default, all monitors
     from Configurables import LHCbApp
     if hasattr(LHCbApp(),"Detectors"):
         if LHCbApp().isPropertySet("Detectors"):
@@ -30,28 +31,28 @@ def ConfiguredTrackMonitorSequence(Name = "TrackMonitorSequence",
         seq.Members.append( TrackV0Monitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackFitMatchMonitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackAlignMonitor(HistoPrint=HistoPrint) )
-        if ("IT" in subDets):   seq.Members.append( TrackITOverlapMonitor(HistoPrint=HistoPrint) )
-        if ("Velo" in subDets): seq.Members.append( TrackVeloOverlapMonitor(HistoPrint=HistoPrint) )
-        if ("TT" in subDets):   seq.Members.append( TTTrackMonitor(HistoPrint=HistoPrint) )
-        if ("IT" in subDets):   seq.Members.append( ITTrackMonitor(HistoPrint=HistoPrint) )
+        if (subDets is None or "IT" in subDets):   seq.Members.append( TrackITOverlapMonitor(HistoPrint=HistoPrint) )
+        if (subDets is None or "Velo" in subDets): seq.Members.append( TrackVeloOverlapMonitor(HistoPrint=HistoPrint) )
+        if (subDets is None or "TT" in subDets):   seq.Members.append( TTTrackMonitor(HistoPrint=HistoPrint) )
+        if (subDets is None or "IT" in subDets):   seq.Members.append( ITTrackMonitor(HistoPrint=HistoPrint) )
         seq.Members.append( TrackPV2HalfAlignMonitor(HistoPrint=HistoPrint) )
 	if RecMoniConf().expertHistos() :
-            if [det for det in ['Velo', 'TT', 'IT', 'OT'] if det in subDets]:
+            if [det for det in ['Velo', 'TT', 'IT', 'OT'] if (subDets is None or det in subDets)]:
                 seq.Members.append( HitEffPlotter(HistoPrint=HistoPrint) )
             #else:
             #    log.warning("HitEffPlotter not defined for upgrade")
         if "CALO" in RecSysConf().RecoSequence:
-            if ("Ecal" in subDets):
+            if (subDets is None or "Ecal" in subDets):
                 seq.Members.append( TrackCaloMatchMonitor("TrackEcalMatchMonitor", CaloSystem='Ecal', HistoPrint=HistoPrint) )
             #if ("Hcal" in subDets):
                 #seq.Members.append(TrackCaloMatchMonitor("TrackHcalMatchMonitor", CaloSystem='Hcal', HistoPrint=HistoPrint))
-            if ("Spd" in subDets):
+            if (subDets is None or "Spd" in subDets):
                 seq.Members.append(TrackCaloMatchMonitor("TrackSpdMatchMonitor", CaloSystem='Spd',HistoPrint=HistoPrint))
-            if ("Prs" in subDets):
+            if (subDets is None or "Prs" in subDets):
                 seq.Members.append(TrackCaloMatchMonitor("TrackPrsMatchMonitor", CaloSystem='Prs',HistoPrint=HistoPrint))
 
         if "MUON" in RecSysConf().RecoSequence:
-            if ("Muon" in subDets):
+            if (subDets is None or "Muon" in subDets):
                 seq.Members.append(TrackMuonMatchMonitor("TrackMuonMatchMonitor", HistoPrint=HistoPrint))
 
     return seq
