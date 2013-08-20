cf44242617224bae3b00a3d2932c7d80fc89ab43
Ronan James Wallace <ronan.wallace@cern.ch>
2013-08-20 07:49:45 +0000

    S20r0p2 restripping of TrackEffMuonTT Z lines config dictionary added.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@161510 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0e2cc0ac2035f7919bbb064643bcd0a16b6cbce4

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_Calib.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_Calib.py
@@ -10,4 +10,52 @@ StrippingSelections file containing the line builder instance.
 
       StrippingNoPIDDstarWithD02RSKPi = { ... }
 """
+from GaudiKernel.SystemOfUnits import MeV, GeV, cm, mm
+
+StrippingTrackEffMuonTT = {
+    'BUILDERTYPE' : 'StrippingTrackEffMuonTTConf',
+    'WGs' : [ 'ALL' ],
+    'STREAMS' : [ 'Calibration' ],
+    'CONFIG' : {
+    'JpsiMassWin'                 : 500,
+    'UpsilonMassWin'              : 1500,
+    'ZMassWin'                    : 40000,
+    'BMassWin'                    : 500,
+    'JpsiMuonTTPT'                : 0,
+    'UpsilonMuonTTPT'             : 500,
+    'ZMuonTTPT'                   : 500,
+    'JpsiLongPT'                  : 1300,
+    'UpsilonLongPT'               : 1000,
+    'ZLongPT'                     : 10000,
+    'JpsiPT'                      : 1000,
+    'UpsilonPT'                   : 0,
+    'ZPT'                         : 0,
+    'JpsiLongMuonMinIP'           : 0.5,
+    'UpsilonLongMuonMinIP'        : 0,
+    'ZLongMuonMinIP'              : 0,
+    'JpsiMINIP'                   : 3,
+    'UpsilonMINIP'                : 10000, #this is a dummy
+    'ZMINIP'                      : 10000, #this is a dummy
+    'BJpsiKMINIP'                 : 10000, #this is a dummy
+    'JpsiLongMuonTrackCHI2'       : 5,
+    'UpsilonLongMuonTrackCHI2'    : 5,
+    'ZLongMuonTrackCHI2'          : 5,
+    'VertexChi2'                  : 5,
+    'LongMuonPID'                 : 2,
+    'JpsiHlt1Triggers'            :  { "Hlt1TrackMuonDecision%TOS" : 0},
+    'UpsilonHlt1Triggers'         :  { "Hlt1SingleMuonHighPTDecision%TOS" : 0},
+    'ZHlt1Triggers'               :  { "Hlt1SingleMuonHighPTDecision%TOS" : 0},
+    'JpsiHlt2Triggers'            :  { "Hlt2SingleMuon.*Decision%TOS" : 0},
+    'UpsilonHlt2Triggers'         :  { "Hlt2SingleMuonLowPTDecision%TOS" : 0},
+    'ZHlt2Triggers'               :  { "Hlt2SingleMuonHighPTDecision%TOS" : 0},
+    'BJpsiKHlt2TriggersTUS'       :  { "Hlt2TopoMu2BodyBBDTDecision%TUS" : 0},
+    'BJpsiKHlt2TriggersTOS'       :  { "Hlt2TopoMu2BodyBBDTDecision%TOS" : 0},
+    'JpsiPrescale'                : 0, 
+    'UpsilonPrescale'             : 0,
+    'ZPrescale'                   : 1,   
+    'BJpsiKPrescale'              : 0,
+    'Postscale'                   : 1 
+    }
+    }
+
 
