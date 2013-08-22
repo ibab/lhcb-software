932bf7280279fa03957858557610e5522a3548a4
Francesca Dordei <francesca.dordei@cern.ch>
2013-08-22 15:25:57 +0000

    First version of Stripping20r{0,1}p2 settings for B2CC WG
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@161654 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ba40b35ab4c9b5ec341df724e43329f948c26c7f

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_B2CC.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_B2CC.py
@@ -1,4 +1,43 @@
 # Line config dictionaries for B2CC WG for 2012 restripping
 
+BetaS = {        
+    'WGs'               : ['B2CC'],
+    'BUILDERTYPE'       : 'B2JpsiXforBeta_sConf',
+    'CONFIG'    : {
+                         'TRCHI2DOF'                 :       5
+                 ,       'BPVLTIME'                  :       0.2
+                 ,       'DaughterPT'                :       1000
+                 ,       'VCHI2PDOF'                 :       10
+                 ,       'Jpsi2MuMuPrescale'         :       0.04   #0.01
+                 ,       'Jpsi2MuMuDetachedPrescale' :       1.0
+                 ,       'Bu2JpsiKPrescale'          :       0.2  #0.05
+                 ,       'Bd2JpsiKstarPrescale'      :       0.2   #0.03
+                 ,       'Bd2JpsiKsPrescale'         :       0.5   #0.3
+                 ,       'Bs2JpsiPhiPrescale'        :       0.5 #0.15
+                 ,       'Bs2Jpsif0Prescale'         :       0.15  
+                 ,       'Bs2JpsiEtaPrescale'        :       0.5
+                 ,       'Bs2JpsiEtapPrescale'       :       0.15
+                 ,       'Bs2JpsiPi0Prescale'        :       0.4
+                 ,       'Bs2JpsiRho0Prescale'       :       0.15
+                         },
+    'STREAMS' : {
+        'Leptonic' : [
+         'StrippingBetaSBs2JpsiPhiMicroLine'
+        ],
+        'Dimuon' : [
+         'StrippingBetaSJpsi2MuMuLine',
+         'StrippingBetaSBs2JpsiPhiDetachedLine',
+         'StrippingBetaSBu2JpsiKPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiPrescaledLine',
+         'StrippingBetaSBd2JpsiKstarPrescaledLine',
+         'StrippingBetaSBd2JpsiKsPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
+         'StrippingBetaSBs2JpsiEtaPrescaledLine',
+         'StrippingBetaSBs2JpsiEtapPrescaledLine',
+         'StrippingBetaSBd2JpsiPi0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0PrescaledLine'
+        ]
+    }   
+    }
 
 
--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1p2/LineConfigDictionaries_B2CC.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r1p2/LineConfigDictionaries_B2CC.py
@@ -1,5 +1,44 @@
 # Line config dictionaries for B2CC WG 2011 config dictionary
 
+BetaS = {        
+    'WGs'               : ['B2CC'],
+    'BUILDERTYPE'       : 'B2JpsiXforBeta_sConf',
+    'CONFIG'    : {
+                         'TRCHI2DOF'                 :       5
+                 ,       'BPVLTIME'                  :       0.2
+                 ,       'DaughterPT'                :       1000
+                 ,       'VCHI2PDOF'                 :       10
+                 ,       'Jpsi2MuMuPrescale'         :       0.008   #0.01
+         	 ,     'Jpsi2MuMuDetachedPrescale'   :       1.0
+                 ,       'Bu2JpsiKPrescale'          :       0.03  #0.05
+                 ,       'Bd2JpsiKstarPrescale'      :       0.03   #0.03
+                 ,       'Bd2JpsiKsPrescale'         :       0.5   #0.3
+                 ,       'Bs2JpsiPhiPrescale'        :       0.06   #0.5 #0.15
+                 ,       'Bs2Jpsif0Prescale'         :       0.02  
+                 ,       'Bs2JpsiEtaPrescale'        :       0.10
+                 ,       'Bs2JpsiEtapPrescale'       :       0.04
+                 ,       'Bs2JpsiPi0Prescale'        :       0.1
+                 ,       'Bs2JpsiRho0Prescale'       :       0.02
+                         },
+    'STREAMS' : {
+        'Leptonic' : [
+         'StrippingBetaSBs2JpsiPhiMicroLine'
+        ],
+        'Dimuon' : [
+         'StrippingBetaSJpsi2MuMuLine',
+         'StrippingBetaSBu2JpsiKPrescaledLine',
+         'StrippingBetaSBs2JpsiPhiPrescaledLine',
+         'StrippingBetaSBd2JpsiKstarPrescaledLine',
+         'StrippingBetaSBd2JpsiKsPrescaledLine',
+         'StrippingBetaSBs2Jpsif0NoIPPrescaledLine',
+         'StrippingBetaSBs2JpsiEtaPrescaledLine',
+         'StrippingBetaSBs2JpsiEtapPrescaledLine',
+         'StrippingBetaSBd2JpsiPi0PrescaledLine',
+         'StrippingBetaSBd2JpsiRho0PrescaledLine'
+        ]
+    }   
+    }
+
 
 
 
