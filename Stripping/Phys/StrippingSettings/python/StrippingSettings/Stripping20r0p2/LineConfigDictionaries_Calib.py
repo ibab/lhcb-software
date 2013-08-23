f6c3eba4f32453c5eae2c1cd8939b65510834417
phunt <phunt@cern.ch>
2013-08-23 14:31:45 +0000

    Added B2LcpXLc2Kpi lines (for proton detection asymmetry studies) to the Stripping20r{0,1}p2 Calibration line config dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@161690 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 86bc861a5cfca680e0fab91030de5f77583a880d

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_Calib.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p2/LineConfigDictionaries_Calib.py
@@ -4,15 +4,35 @@ Line config dictionaries for the PID and Calibration streams
 (all WGs, 2012 incremental restripping).
 
 Dictionary names should have the same name as the corresponding
-StrippingSelections file containing the line builder instance.
+StrippingSelections file containing the line builder instance,
+without the preceeding 'Stripping'.
 
    e.g  StrippingNoPIDDstarWithD02RSKPi linebuilder should be added as:
 
-      StrippingNoPIDDstarWithD02RSKPi = { ... }
+      NoPIDDstarWithD02RSKPi = { ... }
 """
+
 from GaudiKernel.SystemOfUnits import MeV, GeV, cm, mm
 
-StrippingTrackEffMuonTT = {
+
+
+
+#### Modified lines ####
+
+##############################################################################
+##
+## Module for construction of Jpsi -> mu mu /  Upsilon -> mu mu /
+## Z -> mu mu / B -> J/psi K to be used to measure tracking efficiency.
+## Provides functions to build a Jpsi / Upsilon / Z / B selection
+## with a long track and a muonTT track.
+##
+## Seeding station for Z candidates has been changed from M3 to M5
+##
+## Responsible: Ronan Wallace
+## Test: Yes
+##
+##############################################################################
+TrackEffMuonTT = {
     'BUILDERTYPE' : 'StrippingTrackEffMuonTTConf',
     'WGs' : [ 'ALL' ],
     'STREAMS' : [ 'Calibration' ],
@@ -58,4 +78,64 @@ StrippingTrackEffMuonTT = {
     }
     }
 
+#### NEW lines ####
 
+##############################################################################
+##
+## Stripping line for selection of
+##    B0 -> Lambda_c~- (-> p+ K- pi+) p+ pi- pi+
+##    B+ -> Lambda_c~- (-> p+ K- pi+) p+ pi- pi+ pi+
+## for proton detection asymmetry measurement.
+##
+## Responsible: Alex Pearce
+## Test: Yes
+##
+##############################################################################
+B2LcpXLc2Kpi = {
+    'BUILDERTYPE' : 'B2LcpXLc2KpiBuilder',
+    'WGs' : [ 'ALL' ],
+    'STREAMS' : [ 'Calibration' ],
+    'CONFIG' : {
+    # Fraction of candidates to randomly throw away before stripping
+    "Prescale": 1.0,
+    # Fraction of candidates to randomly throw away after stripping
+    "Postscale": 1.0,
+    # B0 partial mass window
+    "BMassMin": 2400*MeV,
+    "BMassMax": 4400*MeV,
+    # B0 mass minus Lambda_c partial mass window
+    "BLcDeltaMassMin": 1500*MeV,
+    "BLcDeltaMassMax": 3750*MeV,
+    # Maximum B0 pairwise daughter distance of closest approach
+    "BDOCACHI2": 50.0,
+    # Minimum Sum of Lambda_c daughters' tranverse momentum
+    "BPTSum": 5000.0*MeV,
+    # Maximum B0 vertex fit quality per DOF
+    "BVCHI2DOF": 15.0,
+    # Minimum cosine of B0 pointing angle difference
+    "BDIRA": 0.999,
+    # Lambda_c mass window
+    "LcPartialMassMin": 800.0*MeV,
+    "LcPartialMassMax": 1350.0*MeV,
+    # Maximum Lambda_c pairwise daughter distance of closest approach
+    "LcPartialDOCACHI2": 10.0,
+    # Minimum Sum of Lambda_c daughters' tranverse momentum
+    "LcPartialPTSum": 1500.0*MeV,
+    # Maximum Lambda_c vertex fit quality per DOF
+    "LcPartialVCHI2DOF": 3.0,
+    # Minimum Lambda_c flight distance fit quality per DOF
+    "LcPartialFDCHI2": 20.0,
+    # Minimum Lambda_c daughter momentum
+    "TrackP": 2000.0*MeV,
+    # Minimum final state track transverse momentum
+    "TrackPT": 500.0*MeV,
+    # Maximum final state track impact parameter chi^2
+    "TrackIPCHI2": 20.0,
+    # Minimum Kaon ANN PID
+    "ProbNNk": 0.1,
+    # Minimum Proton ANN PID
+    "ProbNNp": 0.1,
+    # Minimum Pion ANN PID
+    "ProbNNpi": 0.1
+    }
+    }
