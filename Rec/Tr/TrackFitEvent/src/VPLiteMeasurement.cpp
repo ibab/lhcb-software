81e74143f5bd99362e0c3cd52a97cf5a23deef7c
Heinrich Schindler <heinrich.schindler@cern.ch>
2013-08-25 20:28:28 +0000

    replace direct calls to DeVPSquareType in VPLiteMeasurement
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@161738 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4c020d984a04781a286c293fb26d7101e2c9fff

--- Rec/Tr/TrackFitEvent/src/VPLiteMeasurement.cpp
+++ Rec/Tr/TrackFitEvent/src/VPLiteMeasurement.cpp
@@ -1,11 +1,8 @@
-// $Id: VPLiteMeasurement.cpp,v 1.1 2010-04-13 08:58:10 cocov Exp $
-// Include files 
-
-// local
-#include "Event/VPLiteMeasurement.h"
 #include "VPDet/DeVP.h"
 #include "Kernel/LineTraj.h"
 #include "LHCbMath/LHCbMath.h"
+
+#include "Event/VPLiteMeasurement.h"
 
 using namespace LHCb;
 
@@ -17,82 +14,62 @@ using namespace LHCb;
 // Created: 12-02-2010
 //-----------------------------------------------------------------------------
 
-
-/// Constructor out of the VPLite cluster, dx, dy are the error on the cluster...
-VPLiteMeasurement::VPLiteMeasurement( const DeVP& vpdet,const VPLiteCluster& acluster,
-                                                const IVPClusterPosition& clusPosTool, 
-                                                VPLiteMeasurement::VPLiteMeasurementType& XY)
-  : Measurement(Measurement::VPLite,acluster.channelID(),0),
-    m_vPProjection(XY),
-    m_cluster(&acluster)
-{
-  IVPClusterPosition::toolInfo clusInfo = 
-    clusPosTool.position(this->cluster()) ; 
-  this->init( vpdet, clusInfo ) ;
+/// Constructor from VPLiteCluster
+VPLiteMeasurement::VPLiteMeasurement(const DeVP& det,const VPLiteCluster& cluster,
+                                     const IVPClusterPosition& clusPosTool, 
+                                     VPLiteMeasurement::VPLiteMeasurementType& xy) :
+    Measurement(Measurement::VPLite, cluster.channelID(), 0),
+    m_projection(xy),
+    m_cluster(&cluster) {
+  IVPClusterPosition::toolInfo info = clusPosTool.position(this->cluster()); 
+  init(det, info);
   
 }
-/// Constructor out of the VPLite cluster knowing the ref track state, dx, dy are the error on the cluster...
-VPLiteMeasurement::VPLiteMeasurement( const DeVP& vpdet,const VPLiteCluster& acluster,
-                                                const IVPClusterPosition& clusPosTool, 
-                                                VPLiteMeasurement::VPLiteMeasurementType& XY,
-                                                const LHCb::StateVector& refVector)
-  : Measurement(Measurement::VPLite,acluster.channelID(),0),
-  m_vPProjection(XY),
-    m_cluster(&acluster)
-{
-  IVPClusterPosition::toolInfo clusInfo = 
-    clusPosTool.position(this->cluster(),refVector.position(),
-                         std::pair<double,double>(refVector.tx(),refVector.ty())) ; 
-  this->init( vpdet, clusInfo ) ;  
+
+/// Constructor from VPLiteCluster and known track state
+VPLiteMeasurement::VPLiteMeasurement(const DeVP& det,const VPLiteCluster& cluster,
+                                     const IVPClusterPosition& clusPosTool, 
+                                     VPLiteMeasurement::VPLiteMeasurementType& xy,
+                                     const LHCb::StateVector& ref) :
+    Measurement(Measurement::VPLite, cluster.channelID(), 0),
+    m_projection(xy),
+    m_cluster(&cluster) {
+
+  IVPClusterPosition::toolInfo info = 
+    clusPosTool.position(this->cluster(), ref.position(),
+                         std::pair<double, double>(ref.tx(), ref.ty())); 
+  init(det, info);  
+
 }
 
-void VPLiteMeasurement::init( const DeVP& det, const IVPClusterPosition::toolInfo& clusInfo )
-{
-  m_detectorElement = det.squareSensor(clusInfo.pixel.sensor() );
-  const DeVPSquareType& sqDet=this->sensor();
-  //std::pair<double,double> thePixPoint = sqDet.globalXYZ(clusInfo.pixel.pixel(),clusInfo.fractionalPosition) ;
-  Gaudi::XYZPoint position =  sqDet.globalXYZ(clusInfo.pixel.pixel(),clusInfo.fractionalPosition) ;
+void VPLiteMeasurement::init(const DeVP& det, const IVPClusterPosition::toolInfo& info) {
 
-  std::pair<double,double> pixSize = sqDet.PixelSize(clusInfo.pixel.pixel());
-
-  //// CHEATED..................................
-  //if(position.x()>0 && clusInfo.pixel.chip()%2!=0 )position.SetX(position.x()+pixSize.first);
-  //else if(position.x()<0 && clusInfo.pixel.chip()%2!=0 ) position.SetX(position.x()-pixSize.first);
-  //// CHEATED..................................END
-
-  m_position = position;
-  double dx = pixSize.first*clusInfo.fractionalError.first ;
-  //// CHEATED..................................
-  //if (sqDet.isLong(clusInfo.pixel.pixel())) dx = 0.1 ;
-  //// CHEATED..................................END
-  double dy = pixSize.second*clusInfo.fractionalError.second;
-  setZ(position.z());
-  // Case of the X projection
-  if(m_vPProjection == X){
-    setMeasure(position.x());
+  const DeVPSensor* sensor = det.sensor(info.pixel.sensor()); 
+  const unsigned long pixel = info.pixel.pixel(); 
+  m_position = sensor->globalXYZ(pixel, info.fractionalPosition);
+  std::pair<double, double> pixelSize = sensor->pixelSize(pixel);
+  double dx = pixelSize.first * info.fractionalError.first ;
+  double dy = pixelSize.second * info.fractionalError.second;
+  setZ(m_position.z());
+  if (m_projection == X) {
+    setMeasure(m_position.x());
     setErrMeasure(dx);
-    Gaudi::XYZVector y_dir(0,1,0); 
-    std::pair<double,double> y_range(-dy,dy);
-    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, y_dir, y_range, true));
-
-  }
-  // Case of the Y projection
-  else{
-    setMeasure(position.y());
+    Gaudi::XYZVector yDir(0, 1, 0); 
+    std::pair<double, double> yRange(-dy, dy);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(m_position, yDir, yRange, true));
+  } else {
+    setMeasure(m_position.y());
     setErrMeasure(dy);
-    Gaudi::XYZVector x_dir(1,0,0); 
-    std::pair<double,double> x_range(-dx,dx);
-    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, x_dir, x_range, true));  
-
+    Gaudi::XYZVector xDir(1, 0, 0); 
+    std::pair<double, double> xRange(-dx, dx);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(m_position, xDir, xRange, true));  
   }
+
 }
 
 /// Copy constructor
-VPLiteMeasurement::VPLiteMeasurement( const VPLiteMeasurement& other ) 
-  : Measurement(other),m_vPProjection(other.m_vPProjection), m_cluster(other.m_cluster) {
+VPLiteMeasurement::VPLiteMeasurement(const VPLiteMeasurement& other) : 
+    Measurement(other), m_projection(other.m_projection), m_cluster(other.m_cluster) {
+
 }
 
-
-const DeVPSquareType& VPLiteMeasurement::sensor() const{
-  return *static_cast<const DeVPSquareType *>(detectorElement());
-}
