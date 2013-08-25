8b0e8031d925ec304cbaed9e7bb1a04d974176c6
Heinrich Schindler <heinrich.schindler@cern.ch>
2013-08-25 20:25:22 +0000

    remove dependency on det element, various cleanups
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@161735 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6dad5a81a9c9f38174d258390f6cc223e9a65f41

--- Gauss/Sim/GaussUMC/src/VPGaussMoni.cpp
+++ Gauss/Sim/GaussUMC/src/VPGaussMoni.cpp
@@ -1,15 +1,12 @@
-// from Gaudi
+// Gaudi
 #include "GaudiKernel/DeclareFactoryEntries.h"
 #include "GaudiAlg/Tuples.h"
 #include "GaudiKernel/Vector4DTypes.h"
 
-// from LHCb
+// LHCb
 #include "Event/MCParticle.h"
 
-// velo
-#include "VPDet/DeVP.h"
-
-// local
+// Local
 #include "VPGaussMoni.h"
 
 //-----------------------------------------------------------------------------
@@ -18,284 +15,163 @@
 // 2009-06-05 : Victor Coco, based on VeloGaussMoni
 //-----------------------------------------------------------------------------
 
-// Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( VPGaussMoni );
-
+DECLARE_ALGORITHM_FACTORY(VPGaussMoni);
 
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-VPGaussMoni::VPGaussMoni( const std::string& name,
-                              ISvcLocator* pSvcLocator)
-  : GaudiTupleAlg ( name , pSvcLocator ),
-    m_vpDetLocation ( DeVPLocation::Default ),
-    m_vpMCHits ( ),
-    m_print ( 0 ),
-    m_printInfo (false),
-    m_detailedMonitor ( true ),
-    m_testMCHit ( true ),
-    m_nMCH ( 0. ),
-    m_nMCH2 ( 0. ),
-    m_nEvent ( 0 )
-{
-  declareProperty("Print", m_print);
-  declareProperty("PrintInfo", m_printInfo);
-  declareProperty("TestMCHit", m_testMCHit);
-  declareProperty("DetailedMonitor", m_detailedMonitor);
-  declareProperty("VPDetLocation", m_vpDetLocation);
-  declareProperty("VPMCHits" ,
-                  m_vpMCHitsLocation = LHCb::MCHitLocation::VP /* "MC/VP/Hits" */ ) ;
+VPGaussMoni::VPGaussMoni(const std::string& name,
+                         ISvcLocator* pSvcLocator) :
+    GaudiTupleAlg(name, pSvcLocator),
+    m_hits(),
+    m_nMCH(0.),
+    m_nMCH2(0.),
+    m_nEvent(0) {
+
+  declareProperty("PrintInfo", m_printInfo = false);
+  declareProperty("DetailedMonitor", m_detailedMonitor = true);
+  // Location of MCHits ("MC/VP/Hits")
+  declareProperty("HitLocation", m_hitsLocation = LHCb::MCHitLocation::VP);
+
 }
+
 //=============================================================================
 // Destructor
 //=============================================================================
-VPGaussMoni::~VPGaussMoni() {}; 
+VPGaussMoni::~VPGaussMoni() {
+
+}
 
 //=============================================================================
 // Initialization
 //=============================================================================
 StatusCode VPGaussMoni::initialize() {
-  StatusCode sc = GaudiAlgorithm::initialize(); // must be executed first
-  if ( sc.isFailure() ) return sc;  // error printed already by GaudiAlgorithm
-
-  debug() << "==> Initialize" << endmsg;
-  //
-  m_vpDet=( getDet<DeVP>(m_vpDetLocation ) );
+  StatusCode sc = GaudiAlgorithm::initialize();
+  if (sc.isFailure()) return sc;
   setHistoTopDir("VP/");
-  //
   return StatusCode::SUCCESS;
-};
+}
 
 //=============================================================================
 // Main execution
 //=============================================================================
 StatusCode VPGaussMoni::execute() {
 
-  debug() << "==> Execute" << endmsg;
-  //
-  m_nEvent++;
-  getData();
-  if(m_testMCHit) vpMCHitMonitor();
-  if(!m_detailedMonitor) basicMonitor();
-  checkTests();
-  //
+  ++m_nEvent;
+  // Get data.
+  m_hits = getIfExists<LHCb::MCHits>(m_hitsLocation);
+  if (!m_hits) {
+    error() << "No MCHits at " << m_hitsLocation << endmsg;
+  } else {
+    if (m_printInfo) {
+      info() << "--------------------------------------------------" << endmsg;
+      info() << " ==> Found " << m_hits->size() << " VP MCHits" << endmsg;
+      info() << "--------------------------------------------------" << endmsg;
+    }
+    monitor();
+  }
   return StatusCode::SUCCESS;
-};
+
+}
 
 //=============================================================================
 //  Finalize
 //=============================================================================
 StatusCode VPGaussMoni::finalize() {
 
-  debug() << "==> Finalize" << endmsg;  
-  m_nMCH/=m_nEvent;
-  m_nMCH2/=m_nEvent;
-  double err_nMCH=sqrt((m_nMCH2-(m_nMCH*m_nMCH))/m_nEvent);
+  m_nMCH /= m_nEvent;
+  m_nMCH2 /= m_nEvent;
+  const double err = sqrt((m_nMCH2 - (m_nMCH * m_nMCH)) / m_nEvent);
 
-  //
-  info()<< "------------------------------------------------------" <<endmsg;
-  info()<< "                - VPGaussMoni table -                 " <<endmsg;
-  info()<< "------------------------------------------------------" <<endmsg;
-  if(m_nMCH>0){
-    info()<< "| Number of MCHits/Event:       " << m_nMCH << "+/-"
-          << err_nMCH <<endmsg;
-  }else{
-    info()<< "| ==> No MCHits found! " <<endmsg;
+  info() << "------------------------------------------------------" << endmsg;
+  info() << "                - VPGaussMoni table -                 " << endmsg;
+  info() << "------------------------------------------------------" << endmsg;
+  if (m_nMCH > 0) {
+    info() << " Number of MCHits/Event: " << m_nMCH << "+/-" << err << endmsg;
+  } else {
+    info() << " ==> No MCHits found! " << endmsg;
   }
-  info()<< "------------------------------------------------------" <<endmsg;
-  //
+  info() << "------------------------------------------------------" << endmsg;
   return GaudiAlgorithm::finalize();  // must be called after all other actions
 }
-//
-StatusCode VPGaussMoni::getData()
-{
-  debug()<< " ==> VPGaussMoni::getData" <<endmsg;
 
-  if(!exist<LHCb::MCHits>( m_vpMCHitsLocation )){
-    error()<< "There is no MCHits at "<<m_vpMCHitsLocation<<" in TES!" <<endmsg;
-    m_testMCHit=false;
-  }else{
-    m_vpMCHits=get<LHCb::MCHits>( m_vpMCHitsLocation );
-  }
-  // printf("VPGaussMoni::getData(): %d MC hits\n", m_vpMCHits->size() );
-  //  
-  if(m_printInfo){
-    info()<< "----------------------------------------------------" <<endmsg;
-    if(m_testMCHit)
-      info()<< " ==> Number of MCHits found in VP Detector: "
-            << m_vpMCHits->size() <<endmsg;
-    info()<< "---------------------------------------------------" <<endmsg;
-  }
-  //
-  return StatusCode::SUCCESS;
-}
-//
-StatusCode VPGaussMoni::vpMCHitMonitor()
-{
-  debug()<< " ==> VPGaussMoni::VeloMCHitMonitor " <<endmsg;
-  //
-  double size=m_vpMCHits->size();
-  m_nMCH+=size;
-  m_nMCH2+=size*size;
-  //
+void VPGaussMoni::monitor() {
+
+  const double size = m_hits->size();
+  m_nMCH += size;
+  m_nMCH2 += size * size;
+
   plot(size, "nMCHits", 
        "Number of hits in VP per event",
        0., 3000., 100);
-  //
-  LHCb::MCHits::iterator It;
-
-  // loop over all MCHits stored into the container
-  for(It=m_vpMCHits->begin(); It!=m_vpMCHits->end(); It++){
-    if(m_printInfo){
-      info()<< " ==> Test MCHit: \n"
-            << " sensor: " << ((*It)->sensDetID())
-            << "\nentry: x= " << ((*It)->entry()).x()/Gaudi::Units::mm << " mm"
-            << "\nentry: y= " << ((*It)->entry()).y()/Gaudi::Units::mm << " mm"
-            << "\nentry: z= " << ((*It)->entry()).z()/Gaudi::Units::mm << " mm"
-            << "\nexit: x= " << ((*It)->exit()).x()/Gaudi::Units::mm << " mm"
-            << "\nexit: y= " << ((*It)->exit()).y()/Gaudi::Units::mm << " mm"
-            << "\nexit: z= " << ((*It)->exit()).z()/Gaudi::Units::mm << " mm"
-            << "energy: " << ((*It)->energy())/Gaudi::Units::eV << " eV"
-            << "TOF: " << ((*It)->time())/Gaudi::Units::ns << " ns"
-            <<endmsg;
+  if (!m_detailedMonitor) return;
+  // Loop over all MCHits in the container.
+  LHCb::MCHits::iterator it;
+  for (it = m_hits->begin(); it != m_hits->end(); ++it) {
+    if (m_printInfo) {
+      info() << " ==> Test MCHit:\n"
+             << " sensor: " << ((*it)->sensDetID())
+             << "\nentry: x= " << ((*it)->entry()).x()/Gaudi::Units::mm << " mm"
+             << "\nentry: y= " << ((*it)->entry()).y()/Gaudi::Units::mm << " mm"
+             << "\nentry: z= " << ((*it)->entry()).z()/Gaudi::Units::mm << " mm"
+             << "\nexit: x= " << ((*it)->exit()).x()/Gaudi::Units::mm << " mm"
+             << "\nexit: y= " << ((*it)->exit()).y()/Gaudi::Units::mm << " mm"
+             << "\nexit: z= " << ((*it)->exit()).z()/Gaudi::Units::mm << " mm"
+             << "energy: " << ((*it)->energy())/Gaudi::Units::eV << " eV"
+             << "TOF: " << ((*it)->time())/Gaudi::Units::ns << " ns"
+             << endmsg;
     }
-    
-    //
-    if(m_detailedMonitor){
-      plot((*It)->energy()/Gaudi::Units::eV, "eDepSi",
-           "Energy deposited in Si [eV]",
-           0., 300000., 100);
-      plot2D((*It)->entry().z()/Gaudi::Units::cm,
-             (*It)->entry().x()/Gaudi::Units::cm, "entryZX",
-             "Particle entry point in Si [cm] - ZX plane",
-             -20., 80., -10., 10., 1000, 50);
-      plot2D((*It)->entry().x()/Gaudi::Units::cm,
-             (*It)->entry().y()/Gaudi::Units::cm, "entryXY",
-             "Particle entry point in Si [cm] - XY plane",
-             -10., 10., -10., 10., 500, 500);
-      plot2D((*It)->exit().z()/Gaudi::Units::cm,
-             (*It)->exit().x()/Gaudi::Units::cm, "exitZX",
-             "Particle exit point in Si [cm] - ZX plane",
-             -20., 80., -10., 10., 1000, 50);
-      plot2D((*It)->exit().x()/Gaudi::Units::cm,
-             (*It)->exit().y()/Gaudi::Units::cm, "exitXY",
-             "Particle exit point in Si [cm] - XY plane",
-             -10., 10., -10., 10., 500, 500);
-      plot((*It)->time()/Gaudi::Units::ns, "TOF",
-           "Time Of Flight [ns]", 
-           0., 50., 100);
-      //
-      const DeVPSensor* sensor=m_vpDet->sensor((*It)->sensDetID());
-      double x=(*It)->entry().x()/Gaudi::Units::cm;
-      double y=(*It)->entry().y()/Gaudi::Units::cm;
-      double z=(*It)->entry().z()/Gaudi::Units::cm;
-      //
-      if(sensor->isRight()){
-        if(sensor->isX()){
-          plot2D(x, y, "entryRX_XY",
-                 "Particle entry point in XRight [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryRX_ZX",
-                 "Particle entry point in XRight [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }else if(sensor->isY()){
-          plot2D(x, y, "entryRY_XY",
-                 "Particle entry point in YRight [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryRY_ZX",
-                 "Particle entry point in YRight [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }else if(sensor->isSquare()){
-          plot2D(x, y, "entryRY_XY",
-                 "Particle entry point in SquareRight [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryRY_ZX",
-                 "Particle entry point in SquareRight [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }
-      }else{
-        if(sensor->isX()){
-          plot2D(x, y, "entryLX_XY",
-                 "Particle entry point in XLeft [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryLX_ZX",
-                 "Particle entry point in XLeft [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }else if(sensor->isY()){
-          plot2D(x, y, "entryLY_XY",
-                 "Particle entry point in YLeft [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryLY_ZX",
-                 "Particle entry point in YLeft [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }else if(sensor->isSquare()){
-          plot2D(x, y, "entryLY_XY",
-                 "Particle entry point in SquareLeft [cm] - XY plane",
-                 -10., 10., -10., 10., 50, 50);
-          plot2D(z, x, "entryLY_ZX",
-                 "Particle entry point in SquareLeft [cm] - ZX plane",
-                 -20., 80., -10., 10., 1000, 50);
-        }
-      }
-    }
-    // get access to the MCParticle which made the hit, and write out 4-mom
-    const LHCb::MCParticle* myMCParticle=(*It)->mcParticle();
-    if(0!=myMCParticle){
-      Gaudi::LorentzVector fMom=myMCParticle->momentum();
-      plot(fMom.e()/Gaudi::Units::GeV, "eMCPart",
-           "Particle energy [GeV]",
-           0., 50., 100);       
-      //Get the production vertex position
-      const LHCb::MCVertex * 	myMVVert = myMCParticle->originVertex () ;
-      //      const LHCb::MCVertex * 	myPVVert = myMCParticle->primaryVertex () ;
-      
-      plot2D(myMVVert->position().x(),myMVVert->position().y(), "MCVertexPosXY",
-             "Position of production MC Vertex of MCParticles giving hits - XY [cm]",
-             -10., 10., -10., 10., 500, 500);      
-      plot2D(myMVVert->position().z(),myMVVert->position().y(), "MCVertexPosZY",
-             "Position of production MC Vertex of MCParticles giving hits - ZY [cm]",
-             -20., 80., -10., 10., 500, 500); 
-      if(m_printInfo){
-        info()<< " ==> MCHit - MCParticle: "
-              << "\np_x = " << fMom.px()/Gaudi::Units::GeV
-              << "\np_y = " << fMom.py()/Gaudi::Units::GeV
-              << "\np_z = " << fMom.pz()/Gaudi::Units::GeV
-              <<endmsg;
-      }
+    plot((*it)->energy() / Gaudi::Units::eV, "eDepSi",
+         "Energy deposited in Si [eV]",
+         0., 300000., 100);
+    double x = (*it)->entry().x() / Gaudi::Units::cm;
+    double y = (*it)->entry().y() / Gaudi::Units::cm;
+    double z = (*it)->entry().z() / Gaudi::Units::cm;
+    plot2D(z, x, "entryZX",
+           "Particle entry point in Si [cm] - ZX plane",
+           -20., 80., -10., 10., 1000, 50);
+    plot2D(x, y, "entryXY",
+           "Particle entry point in Si [cm] - XY plane",
+           -10., 10., -10., 10., 500, 500);
+    x = (*it)->exit().x() / Gaudi::Units::cm;
+    y = (*it)->exit().y() / Gaudi::Units::cm;
+    z = (*it)->exit().z() / Gaudi::Units::cm;
+    plot2D(z, x, "exitZX",
+           "Particle exit point in Si [cm] - ZX plane",
+           -20., 80., -10., 10., 1000, 50);
+    plot2D(x, y, "exitXY",
+           "Particle exit point in Si [cm] - XY plane",
+           -10., 10., -10., 10., 500, 500);
+    plot((*it)->time() / Gaudi::Units::ns, "TOF",
+         "Time Of Flight [ns]", 
+         0., 50., 100);
+    // Get access to the MCParticle which made the hit, and write out 4-mom
+    const LHCb::MCParticle* particle = (*it)->mcParticle();
+    if (!particle) continue;
+    Gaudi::LorentzVector fMom = particle->momentum();
+    plot(fMom.e() /Gaudi::Units::GeV, "eMCPart",
+         "Particle energy [GeV]",
+         0., 50., 100);       
+    // Get the production vertex position
+    const LHCb::MCVertex* vert = particle->originVertex();
+    x = vert->position().x() / Gaudi::Units::cm;
+    y = vert->position().y() / Gaudi::Units::cm;
+    z = vert->position().z() / Gaudi::Units::cm;
+    plot2D(x, y, "MCVertexPosXY",
+           "Position of production MC Vertex of MCParticles giving hits - XY [cm]",
+           -10., 10., -10., 10., 500, 500);      
+    plot2D(z, y, "MCVertexPosZX",
+           "Position of production MC Vertex of MCParticles giving hits - ZY [cm]",
+           -20., 80., -10., 10., 500, 500); 
+    plot2D(z, y, "MCVertexPosZY",
+           "Position of production MC Vertex of MCParticles giving hits - ZY [cm]",
+           -20., 80., -10., 10., 500, 500); 
+    if (m_printInfo) {
+      info() << " ==> MCHit - MCParticle: "
+             << "\np_x = " << fMom.px() / Gaudi::Units::GeV
+             << "\np_y = " << fMom.py() / Gaudi::Units::GeV
+             << "\np_z = " << fMom.pz() / Gaudi::Units::GeV
+             << endmsg;
     }
   }
-  return StatusCode::SUCCESS;
 }
-//
-StatusCode VPGaussMoni::basicMonitor()
-{
-  debug()<< " ==> VPGaussMoni::basicMonitor " <<endmsg;
-  
-  double size=0.;
-  if(m_testMCHit){
-    if(0==m_vpMCHits){
-      error()<< " -- No MCHit container retrieved! -- " <<endmsg;
-      return StatusCode::FAILURE;
-    }else{
-      debug()<< " -- Retrieved MCHit container --" <<endmsg;
-    }
-    size=m_vpMCHits->size();    
-    m_nMCH+=size;
-    m_nMCH2+=size*size;
-    plot(size, "nMCHits", "Number of MCHits per event" , 0., 3000., 100);
-  }
-  //
-  //
-  return StatusCode::SUCCESS;
-}
-//
-StatusCode VPGaussMoni::checkTests()
-{
-  if(!(m_testMCHit)){
-    error()<< " ==> VPGaussMoni asked to monitor nothing! " <<endmsg;
-    return StatusCode::FAILURE;
-  }
-  //
-  return StatusCode::SUCCESS;
-}   
-//=============================================================================
--- Gauss/Sim/GaussUMC/src/VPGaussMoni.h
+++ Gauss/Sim/GaussUMC/src/VPGaussMoni.h
@@ -1,9 +1,9 @@
 #ifndef VPGAUSSMONI_H 
 #define VPGAUSSMONI_H 1
 
-// Include files
-// from Gaudi
+// Gaudi
 #include "GaudiAlg/GaudiTupleAlg.h"
+// LHCb
 #include "Event/MCHit.h"
 
 /** @class VPGaussMoni VPGaussMoni.h
@@ -13,42 +13,27 @@
  *  @date   2009-06-05
  */
 
-class DeVP;
-class DeVPXType;
-class DeVPYType;
-class DeVPSquareType;
-
 class VPGaussMoni : public GaudiTupleAlg {
 public: 
   /// Standard constructor
-  VPGaussMoni( const std::string& name, ISvcLocator* pSvcLocator );
-
-  virtual ~VPGaussMoni( ); ///< Destructor
+  VPGaussMoni(const std::string& name, ISvcLocator* pSvcLocator);
+  virtual ~VPGaussMoni(); ///< Destructor
 
   virtual StatusCode initialize();    ///< Algorithm initialization
   virtual StatusCode execute   ();    ///< Algorithm execution
   virtual StatusCode finalize  ();    ///< Algorithm finalization
 
-protected:
-
-  StatusCode checkTests();
-  StatusCode getData();
-  StatusCode vpMCHitMonitor();
-  StatusCode basicMonitor();
-
 private:
 
-  std::string m_vpDetLocation;
-  DeVP* m_vpDet;
-  LHCb::MCHits* m_vpMCHits;
-  int m_print;
+  void monitor();
+
+  std::string m_hitsLocation;
+  LHCb::MCHits* m_hits;
   bool m_printInfo;
   bool m_detailedMonitor;
-  bool m_testMCHit;
   double m_nMCH;
   double m_nMCH2;
-  int m_nEvent;
-  /// Location of Velo MCHits 
-  std::string m_vpMCHitsLocation ;
+  unsigned int m_nEvent;
+
 };
 #endif // VPGAUSSMONI_H
