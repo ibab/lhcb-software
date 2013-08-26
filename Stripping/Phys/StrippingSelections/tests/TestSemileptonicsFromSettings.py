8c178bbab750830d46cf6aa2ccf9040654f72d9d
Mika Anton Vesterinen <mika.vesterinen@cern.ch>
2013-08-26 13:07:42 +0000

    Update to TestSemileptonicsFromSettings.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@161787 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 00d98234db95f176b3dacc5e4c414014572f422d

--- Stripping/Phys/StrippingSelections/tests/TestSemileptonicsFromSettings.py
+++ Stripping/Phys/StrippingSelections/tests/TestSemileptonicsFromSettings.py
@@ -1,6 +1,8 @@
 # $Id: $
 # Test your line(s) of the stripping
 
+### Setup for Stripping20rXp2 (incremental restripping starting August 2013)
+
 from Gaudi.Configuration import *
 from Configurables import SelDSTWriter, DaVinci
 from StrippingConf.Configuration import StrippingConf
@@ -11,180 +13,68 @@ DefaultTrackingCuts().Cuts  = { "Chi2Cut" : [ 0, 3 ],
                                 "CloneDistCut" : [5000, 9e+99 ] }
 # Now build the stream
 from StrippingConf.StrippingStream import StrippingStream
-Semileptonic_stream = StrippingStream("_Semileptonic")
-Dimuon_stream = StrippingStream("_Dimuon")
-BhadronCompleteEvent_stream = StrippingStream("_BHad")
-PID_stream = StrippingStream("_PID")
-Charm_stream = StrippingStream("_Charm")
-CharmComplete_stream = StrippingStream("_CharmComplete")
 
-##### For the Stripping 20r0p1 and 20r1p1 incremental restrippings.
-
-from StrippingSelections import StrippingCharmFromBSemiForHadronAsy
-from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import CharmFromBSemiForHadronAsy
-confCharmFromBSemiForHadronAsy = StrippingCharmFromBSemiForHadronAsy.CharmFromBSemiForHadronAsyAllLinesConf("CharmFromBSemiForHadronAsy", CharmFromBSemiForHadronAsy['CONFIG'])
-Semileptonic_stream.appendLines( confCharmFromBSemiForHadronAsy.lines() )
-
-B2DMuForTauMu = {
-    'BUILDERTYPE'       : 'B2DMuForTauMuconf',
-    'CONFIG'    :{
-    "MINIPCHI2"     : 45.0    # adimensiional
-    ,"GhostProb"     : 0.5     # adimensional
-    ,"KaonPIDK"      : 4.0    # adimensiional
-    ,"PionPIDKTight" : 2.0    # adimensiional
-    ,"MuonIPCHI2"    : 45.00   # adimensiional
-    ,"KPiPT"         : 300.0  # MeV
-    ,"DSumPT"        : 1400.0 # MeV
-    ,"DsDIRA"        : 0.9998   # adimensiional
-    ,"DsFDCHI2"      : 250.0  # adimensiional
-    ,"DsMassWin"     : 80.0   # MeV
-    ,"DsAMassWin"    : 100.0  # MeV
-    ,"DsVCHI2DOF"    : 4.0    # adimensiional
-    ,"PIDmu"         : 2.0   # adimensiional
-    ,"BDIRA"         : 0.9995  #adimensiional
-    ,"BVCHI2DOF"     : 6.0    # adimensiional
-    ,"SPDmax"        : 600    # adimensiional
-    },
-    'STREAMS' : {
-    'Semileptonic' : ['Strippingb2D0MuXFakeB2DMuForTauMuLine']
-    },
-    'WGs'    : ['Semileptonic']
+Streams = {
+    "Semileptonic" : StrippingStream("_Semileptonic"),
+    "Dimuon" : StrippingStream("_Dimuon"),
+    "Bhadron" : StrippingStream("_Bhadron"),
+    "BhadronCompleteEvent" : StrippingStream("_BhadronCompleteEvent"),
+    "Leptonic" : StrippingStream("_Leptonic"),
+    "PID" : StrippingStream("_PID"),
+    "Radiative" : StrippingStream("_Radiative"),
+    "Charm" : StrippingStream("_Charm"),
+    "CharmCompleteEvent" : StrippingStream("_CharmCompleteEvent"),
+    "EW" : StrippingStream("_EW")
     }
 
-from StrippingSelections import StrippingB2DMuForTauMu
-confB2DMuForTauMu = StrippingB2DMuForTauMu.B2DMuForTauMuconf("B2DMuForTauMu",B2DMuForTauMu['CONFIG'])
-for l in confB2DMuForTauMu.lines():
-    if l.name() in B2DMuForTauMu['STREAMS']['Semileptonic']:
-        Semileptonic_stream.appendLines([l])
 
+def AddLines(_Streams,_conf,_settings):
+    print _settings['STREAMS']
+    print '%s' %len(_settings['STREAMS'])
+    if len(_settings['STREAMS']) == 1:
+        print 'adding just to %s' %_settings['STREAMS']
+        _Streams[ _settings['STREAMS'][0]].appendLines(_conf.lines())
+    else:
+        for STREAM in _settings['STREAMS']:
+            for line in _settings['STREAMS'][STREAM]:
+                _Streams[STREAM].appendLines([ l for l in _conf.lines() if l.name() == line])
 
-from StrippingSelections import StrippingB2XTauNu
-from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import B2XTauNu
-confB2XTauNu = StrippingB2XTauNu.B2XTauNuAllLinesConf("B2XTauNu",B2XTauNu['CONFIG'])
-for l in confB2XTauNu.lines():
-    if l.name() in B2XTauNu['STREAMS']['BhadronCompleteEvent']:
-        BhadronCompleteEvent_stream.appendLines([l])
+### modules that will be run
+### specific lines may have been switched off in the settings
 
-from StrippingSelections import StrippingB2DHHHForBXX
-#from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import B2DHHHForBXX
-confB2DHHHForBXX =  StrippingB2DHHHForBXX.B2DHHHForBXXLinesConf("B2DHHHForBXX",StrippingB2DHHHForBXX.confdict) #B2DHHHForBXX['CONFIG'])
-BhadronCompleteEvent_stream.appendLines( confB2DHHHForBXX.lines() )
+from StrippingSelections import StrippingB2DMuNuX
+from StrippingSettings.Stripping20r0p2.LineConfigDictionaries_Semileptonic import B2DMuNuX
+confB2DMuNuX = StrippingB2DMuNuX.B2DMuNuXAllLinesConf("B2DMuNuX", B2DMuNuX['CONFIG'])
+AddLines(Streams,confB2DMuNuX,B2DMuNuX)
 
+from StrippingSelections import StrippingPhiToKSKS
+from StrippingSettings.Stripping20r0p2.LineConfigDictionaries_Semileptonic import PhiToKSKS
+confPhiToKSKS = StrippingPhiToKSKS.PhiToKSKSAllLinesConf("PhiToKSKS", PhiToKSKS['CONFIG'])
+AddLines(Streams,confPhiToKSKS,PhiToKSKS)
 
+from StrippingSelections import StrippingB2XuMuNu
+from StrippingSettings.Stripping20r0p2.LineConfigDictionaries_Semileptonic import B2XuMuNu
+confB2XuMuNu = StrippingB2XuMuNu.B2XuMuNuBuilder("B2XuMuNu", B2XuMuNu['CONFIG'])
+AddLines(Streams,confB2XuMuNu,B2XuMuNu)
 
-# CharmFromBSemi Lines
-# Micro-DST, Mika Vesterinen
-# Moved to this file in Stripping20
-# Lives in StrippingCharmFromBSemi.py
-CharmFromBSemi = {
-    'BUILDERTYPE' : 'CharmFromBSemiAllLinesConf',
-    'CONFIG' :  {
-    "GEC_nLongTrk"   : 250    # adimensional
-    ,"TrGhostProbMax": 0.5    # adimensional
-    ,"MinBMass"      : 2500   # MeV
-    ,"MaxBMass"      : 6000   # MeV
-    ,"MinBMassTight" : 2800   # MeV
-    ,"MaxBMassTight" : 5100   # MeV
-    ,"MINIPCHI2"     : 9.0    # adimensiional
-    ,"TRCHI2"        : 4.0    # adimensiional
-    ,"KaonPIDK"      : 4.0    # adimensiional
-    ,"PionPIDK"      : 10.0   # adimensiional
-    ,"PionPIDKTight" : 4.0    # adimensiional
-    ,"MuonIPCHI2"    : 4.0    # adimensiional
-    ,"MuonPT"        : 800.0  # MeV
-    ,"KPiPT"         : 300.0  # MeV
-    ,"DsDIRA"        : 0.99   # adimensiional
-    ,"DsFDCHI2"      : 100.0  # adimensiional
-    ,"DsMassWin"     : 80.0   # MeV
-    ,"DsAMassWin"    : 100.0  # MeV
-    ,"Dto4h_MassWin" : 40.0   # MeV
-    ,"Dto4h_AMassWin": 42.0   # MeV
-    ,"DsIP"          : 7.4    # mm
-    ,"DsVCHI2DOF"    : 6.0    # adimensiional
-    ,"PIDmu"         : -0.0   # adimensiional
-    ,"BDIRA"         : 0.999  # adimensiional
-    ,"BVCHI2DOF"     : 6.0    # adimensiional
-    ,"DZ"            : 0      #mm
-    ,"DDocaChi2Max"  : 20     #adimensiional
-    ,"MINIPCHI2Loose": 4.0   #adimensiional
-    ,"KaonPIDKloose" : -5     #adimensiional
-    ,'KSCutZFDFromD' :   10.0  #mm
-    ,'KSLLPMin'         : 2000  ## MeV
-    ,'KSLLPTMin'        : 400   ## MeV
-    ,'KSDDPMin'         : 3000  ## MeV
-    ,'KSDDPTMin'        : 500   ## MeV
-    ,'KSLLCutMass'      : 30    ## MeV
-    ,'KSDDCutMass'      : 30    ## MeV
-    ,'KSLLCutFDChi2'    : 100   ## unitless
-    ,'KSDDCutFDChi2'    : 100   ## unitless
-    ,'KSDaugTrackChi2'  : 4     ## max chi2/ndf for Ks daughter tracks
-    ,'KSVertexChi2'     : 6     ## max chi2/ndf for Ks vertex
-    ,'KSCutDIRA'        : 0.99  ## unitless
-    ,'LambdaLLPMin'       : 2000 ## MeV
-    ,'LambdaLLPTMin'      : 500  ## MeV
-    ,'LambdaLLCutMass'    : 30   ## MeV
-    ,'LambdaLLCutFDChi2'  : 100  ## unitless
-    ,'LambdaDDPMin'       : 3000 ## MeV
-    ,'LambdaDDPTMin'      : 800  ## MeV
-    ,'LambdaDDCutMass'    : 30   ## MeV
-    ,'LambdaDDCutFDChi2'  : 100  ## unitless
-    ,'LambdaCutDIRA'      : 0.99 ## unitless
-    ,'LambdaDaugTrackChi2': 4    ## unitless
-    ,'LambdaVertexChi2'   : 6    ## max chi2/ndf for Lambda0 vertex
-    ,"Pi0PtMinLoose"    : 1000    # Minimum Pt of pi0 (MeV)
-    ,"Pi0PtMin"         : 1300   # Minimum Pt of pi0 (MeV)
-    ,"Pi0PMin"          : 3000   # Minimum P of pi0 (MeV)
-    ,"PhotonCL"         : 0.25   # Confidence level for Pi0 photons
-    ,"D02HHPi0AMassWin" : 220  # MeV (mass window for combination)
-    ,"D02HHPi0MassWin"  : 210  # MeV (mass window after vertex fit)
-    ,"D02HHPi0DocaCut"  : 6.0    # mm
-    ,"Dstar_Chi2"       :  8.0 ## unitless
-    ,"Dstar_SoftPion_PIDe" : 2. ## unitless
-    ,"Dstar_SoftPion_PT" : 180. ## MeV ### 
-    ,"Dstar_wideDMCutLower" : 0. ## MeV
-    ,"Dstar_wideDMCutUpper" : 170. ## MeV
-    ,"PTSUMLoose"  : 1400. ## MeV
-    ,"PTSUM"       : 1800. ## MeV
-    ,"PTSUM_HHPi0" : 1800. ## MeV
-    },
-    'STREAMS' : {
-    'Charm' : ['Strippingb2DstarMuXKsKPiLLCharmFromBSemiLine',
-               'Strippingb2DstarMuXKsKPiDDCharmFromBSemiLine',
-               'Strippingb2DstarMuXKsPiPiLLCharmFromBSemiLine',
-               'Strippingb2DstarMuXKsPiPiDDCharmFromBSemiLine',
-               'Strippingb2DstarMuXKsKKLLCharmFromBSemiLine',
-               'Strippingb2DstarMuXKsKKDDCharmFromBSemiLine',
-               'Strippingb2D0MuXKsKPiLLCharmFromBSemiLine',
-               'Strippingb2D0MuXKsKPiDDCharmFromBSemiLine',
-               'Strippingb2D0MuXKsPiPiLLCharmFromBSemiLine',
-               'Strippingb2D0MuXKsPiPiDDCharmFromBSemiLine',
-               'Strippingb2D0MuXKsKKLLCharmFromBSemiLine',
-               'Strippingb2D0MuXKsKKDDCharmFromBSemiLine',
-               'Strippingb2MuXLc2pKsLLCharmFromBSemiLine',
-               'Strippingb2MuXLc2pKsDDCharmFromBSemiLine']},
-    'WGs'    : ['Charm']
-    }
-
-from StrippingSelections import StrippingCharmFromBSemi
-#from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import CharmFromBSemi
-confCharmFromBSemi = StrippingCharmFromBSemi.CharmFromBSemiAllLinesConf("CharmFromBSemi", CharmFromBSemi['CONFIG'])
-for l in confCharmFromBSemi.lines():
-    if l.name() in CharmFromBSemi['STREAMS']['Charm']:
-        Charm_stream.appendLines([l])
-
-### run from selections
-from StrippingSelections import StrippingLb2pMuNu
-confLb2pMuNu = StrippingLb2pMuNu.Lb2pMuNuBuilder("Lb2pMuNu", StrippingLb2pMuNu.confdict)
-Charm_stream.appendLines(confLb2pMuNu.lines())
+#from StrippingSelections import StrippingB2XuMuNu
+#from StrippingSettings.Stripping20r0p2.LineConfigDictionaries_Semileptonic import B2XuMuNu
+#confB2XuMuNu1 = StrippingB2XuMuNu.B2XuMuNuBuilder("B2XuMuNu1", StrippingB2XuMuNu.confdict)
+#Streams["Semileptonic"].appendLines(confB2XuMuNu1.lines())
+#AddLines(Streams,confB2XuMuNu,B2XuMuNu)
+#confB2XuMuNu2 = StrippingB2XuMuNu.B2XuMuNuBuilder("B2XuMuNu2", StrippingB2XuMuNu.confdict)
+#Streams["Semileptonic"].appendLines(confB2XuMuNu2.lines())
 
 ######################################
         
 from Configurables import  ProcStatusCheck
 filterBadEvents =  ProcStatusCheck()
 
-# Configure the stripping using the same options as in Reco06-Stripping10
-#sc = StrippingConf( Streams = [ Semileptonic_stream,Dimuon_stream,PID_stream , Charm_stream],
-sc = StrippingConf( Streams = [ Semileptonic_stream,Dimuon_stream,Charm_stream,BhadronCompleteEvent_stream],
+ActiveStreams = []
+for STREAM in Streams:
+    if len(Streams[STREAM].lines) > 0:
+        ActiveStreams.append(Streams[STREAM])
+sc = StrippingConf( Streams = ActiveStreams,
                     MaxCandidates = 2000,
                     AcceptBadEvents = False,
                     BadEventSelection = filterBadEvents )
@@ -251,12 +141,9 @@ DaVinci().appendToMainSequence( [ sr ] )
 #DaVinci().appendToMainSequence( [ ac ] )
 DaVinci().DataType = "2012"
 DaVinci().InputType = 'SDST'
-#DaVinci().DDDBtag  = "head-20120413"
-#DaVinci().CondDBtag = "head-20120420"
 DaVinci().DDDBtag  = "dddb-20120831"
 DaVinci().CondDBtag = "cond-20121008"
 
-#importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_2011Data_MagDn.py")
 importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_Run125113.py")
 
 ########################################################################
@@ -322,3 +209,44 @@ importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_Run125113.py")
 #from StrippingSelections import StrippingMuIDCalib
 #confMuIDCalib = StrippingMuIDCalib.MuIDCalibConf("MuIDCalib",StrippingMuIDCalib.config_params)
 #PID_stream.appendLines([confMuIDCalib.line_DetachedNoMIP])
+
+#for l in confCharmFromBSemi.lines():
+#    if l.name() in CharmFromBSemi['STREAMS']['Charm']:
+#        Charm_stream.appendLines([l])
+
+
+##### For the Stripping 20r0p1 and 20r1p1 incremental restrippings.
+#from StrippingSelections import StrippingCharmFromBSemiForHadronAsy
+#from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import CharmFromBSemiForHadronAsy
+#confCharmFromBSemiForHadronAsy = StrippingCharmFromBSemiForHadronAsy.CharmFromBSemiForHadronAsyAllLinesConf("CharmFromBSemiForHadronAsy", CharmFromBSemiForHadronAsy['CONFIG']#)
+#Semileptonic_stream.appendLines( confCharmFromBSemiForHadronAsy.lines() )
+
+#from StrippingSelections import StrippingB2DMuForTauMu
+#confB2DMuForTauMu = StrippingB2DMuForTauMu.B2DMuForTauMuconf("B2DMuForTauMu",B2DMuForTauMu['CONFIG'])
+#for l in confB2DMuForTauMu.lines():
+#    if l.name() in B2DMuForTauMu['STREAMS']['Semileptonic']:
+#        Semileptonic_stream.appendLines([l])
+
+#from StrippingSelections import StrippingB2XTauNu
+#from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import B2XTauNu
+#confB2XTauNu = StrippingB2XTauNu.B2XTauNuAllLinesConf("B2XTauNu",B2XTauNu['CONFIG'])
+#for l in confB2XTauNu.lines():
+#    if l.name() in B2XTauNu['STREAMS']['BhadronCompleteEvent']:
+#        BhadronCompleteEvent_stream.appendLines([l])
+
+#from StrippingSelections import StrippingB2DHHHForBXX
+#from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import B2DHHHForBXX
+#confB2DHHHForBXX =  StrippingB2DHHHForBXX.B2DHHHForBXXLinesConf("B2DHHHForBXX",StrippingB2DHHHForBXX.confdict) #B2DHHHForBXX['CONFIG'])
+#BhadronCompleteEvent_stream.appendLines( confB2DHHHForBXX.lines() )
+
+#from StrippingSelections import StrippingCharmFromBSemi
+##from StrippingSettings.Stripping20r0p1.LineConfigDictionaries_Semileptonic import CharmFromBSemi
+#confCharmFromBSemi = StrippingCharmFromBSemi.CharmFromBSemiAllLinesConf("CharmFromBSemi", CharmFromBSemi['CONFIG'])
+#for l in confCharmFromBSemi.lines():
+#    if l.name() in CharmFromBSemi['STREAMS']['Charm']:
+#        Charm_stream.appendLines([l])
+
+### run from selections
+#from StrippingSelections import StrippingLb2pMuNu
+#confLb2pMuNu = StrippingLb2pMuNu.Lb2pMuNuBuilder("Lb2pMuNu", StrippingLb2pMuNu.confdict)
+#Charm_stream.appendLines(confLb2pMuNu.lines())
