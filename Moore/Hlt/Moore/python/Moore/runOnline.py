0191e7503544949b706d9ef5eb4f83fa28433d97
Gerhard Raven <gerhard.raven@nikhef.nl>
2013-08-30 14:29:11 +0000

    move generation of online scripts from Moore to MooreSys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@161962 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 429e7fe91dfc4bf37cc6583d9395779e2e001068

--- Moore/Hlt/Moore/python/Moore/runOnline.py
+++ Moore/Hlt/Moore/python/Moore/runOnline.py
@@ -15,34 +15,36 @@ PrintOff(999)
 InstallRootLoggingHandler(level = logging.CRITICAL)
 
 
-def start(NbOfSlaves) :
+def start(**kwargs) :
     from Moore.Configuration import Moore
     import OnlineEnv 
 
-    Moore().RunOnline = True
-    Moore().NbOfSlaves = NbOfSlaves
+    moore = Moore()
+
+    moore.RunOnline = True
 
     ### default database setup -- require an explit tag when running in the LHCb or FEST partitions...
-    Moore().Simulation = False
-    Moore().DDDBtag    = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
-    Moore().CondDBtag  = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
+    moore.Simulation = False
+    moore.DDDBtag    = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
+    moore.CondDBtag  = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
 
-    Moore().UseDBSnapshot = True
-    Moore().IgnoreDBHeartBeat = True
-    Moore().EnableRunChangeHandler = ( OnlineEnv.HLTType not in ['PA','PassThrough' ] )
-    Moore().PartitionName = OnlineEnv.PartitionName
-    Moore().InitialTCK = OnlineEnv.InitialTCK
-    Moore().REQ1 = "EvType=1;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
+    moore.UseDBSnapshot = True
+    moore.IgnoreDBHeartBeat = True
+    moore.EnableRunChangeHandler = ( OnlineEnv.HLTType not in ['PA','PassThrough' ] )
+    moore.PartitionName = OnlineEnv.PartitionName
+    moore.InitialTCK = OnlineEnv.InitialTCK
+    moore.REQ1 = "EvType=1;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
 
+    for (k,v) in kwargs : setattr(moore,k,v)
 
     ## TODO: is this still needed???
     #if OnlineEnv.PartitionName == 'FEST' : Moore().EnableRunChangeHandler = False
 
     ### pick up requested DB tags
-    if hasattr(OnlineEnv,'CondDBTag') and OnlineEnv.CondDBTag : Moore().CondDBtag = OnlineEnv.CondDBTag
-    if hasattr(OnlineEnv,'DDDBTag')   and OnlineEnv.DDDBTag   : Moore().DDDBtag   = OnlineEnv.DDDBTag
+    if hasattr(OnlineEnv,'CondDBTag') and OnlineEnv.CondDBTag : moore.CondDBtag = OnlineEnv.CondDBTag
+    if hasattr(OnlineEnv,'DDDBTag')   and OnlineEnv.DDDBTag   : moore.DDDBtag   = OnlineEnv.DDDBTag
 
-    # Forward all attributes of 'OnlineEnv' to the job options service...
+    # Forward all attributes of 'OnlineEnv' python module to the job options service (using the OnlineEnv configurable)...
     from GaudiKernel.Proxy.Configurable import ConfigurableGeneric
     c = ConfigurableGeneric("OnlineEnv")
     #[ setattr(c,k,v) for (k,v) in OnlineEnv.__dict__.items() if k not in OnlineConfig.__dict__ ]
