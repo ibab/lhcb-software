a42ff32a54c061fe65fd986509be1d05db6a19f8
Jason Emory Andrews <jea@umd.edu>
2013-09-02 04:02:21 +0000

    Actually added tentative version of StrippingBu2Kpi0.py...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@162013 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 715d7b208621553e75faee927e71b6fae4be3f88

---
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBu2Kpi0.py
@@ -0,0 +1,143 @@
+"""
+Stripping lines for B -> h h pi0
+both resolved and merged pi0's
+modification of StrippingB2HHPi0.py by Regis Lefevre
+"""
+
+__author__  = "Jason E Andrews"
+__date__    = "29/08/2013"
+__version__ = "1.0"
+__all__     = ( "StrippingBu2Kpi0Conf",
+                "makeBu2Kpi0Resolved",
+                "makeBu2Kpi0Merged")
+
+from Gaudi.Configuration import *
+
+from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
+from StandardParticles                     import StdNoPIDsKaons, StdLooseMergedPi0, StdLooseResolvedPi0
+
+from PhysSelPython.Wrappers      import Selection
+from StrippingConf.StrippingLine import StrippingLine
+from StrippingUtils.Utils        import LineBuilder
+
+
+default_config = {
+    "RKuMIPCHI2DV"   : 25,
+    "KuMinPROBNNK"   : 0.8,
+    "KuMaxTRCHI2DOF" : 1.5,
+    "Rpi0MinP"       : 10000,
+    "Rpi0MinPT"      : 1600,
+    "Rpi0MinCL"      : 0.2,
+    "RBuMinM"        : 4550,
+    "BuMaxM"         : 6050,
+    "RBuMinPT"       : 0,
+    "PrescaleBu2Kpi0Resolved" : 1.0,
+    "MKuMIPCHI2DV"   : 49,
+    "Mpi0MinP"       : 10000,
+    "Mpi0MinPT"      : 2600,
+    "Mpi0MinCL"      : 0.9,
+    "MBuMinM"        : 4200,
+    "MBuMinPT"       : 1500,
+    "PrescaleBu2Kpi0Merged"   : 1.0
+    }
+
+class StrippingBu2Kpi0Conf(LineBuilder) :
+
+    __configuration_keys__ = ( "RKuMIPCHI2DV",
+                               "KuMinPROBNNK",
+                               "KuMaxTRCHI2DOF",
+			       "Rpi0MinP",
+			       "Rpi0MinPT",
+			       "Rpi0MinCL",
+			       "RBuMinM",
+			       "BuMaxM",
+                               "RBuMinPT",
+			       "PrescaleBu2Kpi0Resolved",
+			       "MKuMIPCHI2DV",
+			       "Mpi0MinP",
+			       "Mpi0MinPT",
+			       "Mpi0MinCL",
+                               "MBuMinM",
+                               "MBuMinPT",
+                               "PrescaleBu2Kpi0Merged"
+                               )
+
+    def __init__(self, name, config) :
+
+        LineBuilder.__init__(self, name, config)
+
+        Bu2Kpi0Name = "Bu2Kpi0"
+
+	self.selResolved = makeBu2Kpi0Resolved( Bu2Kpi0Name + "Resolved",
+                                                config,
+                                                DecayDescriptor = "[B+ -> K+ pi0]cc",
+                                                inputSel = [ StdNoPIDsKaons, StdLooseResolvedPi0 ]
+                                                )				       
+
+        self.selMerged = makeBu2Kpi0Merged( Bu2Kpi0Name + "Merged",
+                                            config,
+                                            DecayDescriptor = "[B+ -> K+ pi0]cc",
+                                            inputSel = [ StdNoPIDsKaons, StdLooseMergedPi0 ]
+                                            )
+
+        self.Bu2Kpi0Resolved_line = StrippingLine(Bu2Kpi0Name + "ResolvedLine",
+                                                  prescale = config["PrescaleBu2Kpi0Resolved"],
+                                                  checkPV = True,
+                                                  algos = [ self.selResolved ]
+                                                  )
+        self.Bu2Kpi0Merged_line = StrippingLine(Bu2Kpi0Name + "MergedLine",
+                                                prescale = config["PrescaleBu2Kpi0Merged"],
+                                                selection = self.selMerged
+                                                )
+
+        self.registerLine(self.Bu2Kpi0Resolved_line)
+        self.registerLine(self.Bu2Kpi0Merged_line)
+
+
+def makeBu2Kpi0Resolved( name,
+                         config,
+                         DecayDescriptor,
+                         inputSel
+                         ) :
+
+    _KuCuts = "(PROBNNK>%(KuMinPROBNNK)s) & (MIPCHI2DV(PRIMARY)>%(RKuMIPCHI2DV)s) & (TRCHI2DOF<%(KuMaxTRCHI2DOF)s)" % locals()["config"]
+    _pi0Cuts = "(P>%(Rpi0MinP)s *MeV) & (PT>%(Rpi0MinPT)s *MeV) & (CL>%(Rpi0MinCL)s)" % locals()["config"]
+    _daughterCuts = { "K+" : _KuCuts, "pi0" : _pi0Cuts }
+    _combCuts = "(AM>%(RBuMinM)s *MeV) & (AM<%(BuMaxM)s *MeV)" % locals()["config"]
+    _motherCuts = "(PT>%(RBuMinPT)s *MeV)" % locals()["config"]
+
+    _B = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts,
+                           ParticleCombiners = { "" : "ParticleAdder" }
+                           )
+
+    return Selection( name + "Sel",
+                      Algorithm = _B,
+                      RequiredSelections = inputSel
+                      )
+
+def makeBu2Kpi0Merged( name,
+                       config,
+                       DecayDescriptor,
+                       inputSel
+                       ) :
+
+    _KuCuts = "(PROBNNK>%(KuMinPROBNNK)s) & (MIPCHI2DV(PRIMARY)>%(MKuMIPCHI2DV)s) & (TRCHI2DOF<%(KuMaxTRCHI2DOF)s)" % locals()["config"]
+    _pi0Cuts = "(P>%(Mpi0MinP)s *MeV) & (PT>%(Mpi0MinPT)s *MeV) & (CL>%(Mpi0MinCL)s)" % locals()["config"]
+    _daughterCuts = { "K+" : _KuCuts, "pi0" : _pi0Cuts }
+    _combCuts = "(AM>%(MBuMinM)s *MeV) & (AM<%(BuMaxM)s *MeV)" % locals()["config"]
+    _motherCuts = "(PT>%(MBuMinPT)s *MeV)" % locals()["config"]
+
+    _B = CombineParticles( DecayDescriptor = DecayDescriptor,
+                           MotherCut = _motherCuts,
+                           CombinationCut = _combCuts,
+                           DaughtersCuts = _daughterCuts,
+                           ParticleCombiners = { "" : "ParticleAdder" }
+                           )
+
+    return Selection( name + "Sel",
+                      Algorithm = _B,
+                      RequiredSelections = inputSel
+                      )
