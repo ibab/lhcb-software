cdc27c10fcaa67547c2e898ccbd84f7e9a98f42a
rlambert <rlambert@cern.ch>
2013-09-04 16:36:52 +0000

    Moore().Split = 'Hlt1Hlt2' into a test... not sure if it works yet...
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@162105 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ca1f20744bc9e22d9a2594619af9c4c3402d3d55

---
+++ Moore/Hlt/Moore/tests/options/Moore_NoTosDeferral.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env gaudirun.py
+#
+# Minimal file for running Moore from python prompt
+# Syntax is:
+#   gaudirun.py ../options/Moore.py
+# or just
+#   ../options/Moore.py
+#
+import Gaudi.Configuration
+from Moore.Configuration import Moore
+
+#use new splitting of Hlt2
+Moore().Split='Hlt1Hlt2'
