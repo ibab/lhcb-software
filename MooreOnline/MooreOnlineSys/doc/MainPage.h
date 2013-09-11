875e1a3e0230993a96aa5236e28eb07709575cbd
rlambert <rlambert@cern.ch>
2013-09-11 09:31:50 +0000

    Creation of new project, MooreOnline.
    git-svn-id: http://svn.cern.ch/guest/lhcb/MooreOnline/trunk@162250 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e60730c3c1ba5c8d2d8a208b22bc8755c2c58c6e

---
+++ MooreOnline/MooreOnlineSys/doc/MainPage.h
@@ -0,0 +1,23 @@
+/** \mainpage notitle
+ *  \anchor mooredoxygenmain
+ *
+ * <b>The classes used by Moore are documented in the following reference manuals:</b>
+ *
+ * \li \ref hltsysdoxygenmain "HltSys documentation (LHCb High Level Trigger classes)"
+ * \li \ref physsysdoxygenmain "PhysSys documentation (LHCb physics packages)"
+ * \li \ref recsysdoxygenmain "RecSys documentation (LHCb reconstruction packages)"
+ * \li \ref lbcomdoxygenmain "LbcomSys documentation (LHCb packages shared with Digitization)"
+ * \li \ref lhcbdoxygenmain  "LHCbSys documentation (LHCb core packages)"
+ * \li \ref gaudidoxygenmain "Gaudi documentation (Framework packages)"
+ * \li \ref externaldocs     "Related external libraries"
+ *
+ * <hr>
+ * \htmlinclude new_release.notes
+ * <hr>
+
+ * <b>Additional information:</b>
+ * \li <a href="http://cern.ch/lhcb-trig/HLT/"><b>High Level Trigger Web pages</b></a>
+ * \li <a href="http://cern.ch/lhcb-reconstruction/"><b>Reconstruction project Web pages</b></a>
+ * \li <a href="../release.notes"><b>Release notes history</b></a><p>
+
+ */
