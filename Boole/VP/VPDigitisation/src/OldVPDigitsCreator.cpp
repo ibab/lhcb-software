0aa46eede970a08ed0cb0aa7f4d601b572a48058
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-09-27 13:02:33 +0000

    Remove extra ';' to fix CMake warnings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@162848 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 63f0dfd3c5f483007c8f54483d3af63e53e3f011

--- Boole/VP/VPDigitisation/src/OldVPDigitsCreator.cpp
+++ Boole/VP/VPDigitisation/src/OldVPDigitsCreator.cpp
@@ -15,7 +15,7 @@ using namespace LHCb;
 //-----------------------------------------------------------------------------
 
 // Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( OldVPDigitsCreator );
+DECLARE_ALGORITHM_FACTORY( OldVPDigitsCreator )
 
 
 //=============================================================================
