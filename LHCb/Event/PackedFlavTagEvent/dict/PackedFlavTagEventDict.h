4f57755146db5cc568c91289b5f5675231124d34
rlambert <rlambert@cern.ch>
2013-10-08 12:14:49 +0000

    simplify dictionaries
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@163456 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b2965fa481e1163520a96c9e0f8a28c65555288c

---
+++ LHCb/Event/PackedFlavTagEvent/dict/PackedFlavTagEventDict.h
@@ -0,0 +1,8 @@
+// $Id: PackedEventDict.h,v 1.6 2010-05-18 09:04:35 jonrob Exp $
+#ifndef DICT_PACKEDFLAVTAGEVENTDICT_H
+#define DICT_PACKEDFLAVTAGEVENTDICT_H 1
+
+// Include files for LCG dictionary
+#include "Event/PackedFlavourTag.h"
+
+#endif // DICT_PACKEDFLAVTAGEVENTDICT_H
