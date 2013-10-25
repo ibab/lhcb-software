cf407cf9c58533b52e1e2128600160235e8b0ac1
Sebastian Neubert <sebastian.neubert@cern.ch>
2013-10-25 11:02:20 +0000

    added release notes for compact configuration. added python helper to create mva tuple tool chain
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@164340 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eb0c2ac41cb41adb23bd594eccc6261b08b96d2d

--- Analysis/Phys/MVADictTools/python/MVADictHelpers.py
+++ Analysis/Phys/MVADictTools/python/MVADictHelpers.py
@@ -23,3 +23,25 @@ def addTMVAclassifierValue(Component, XMLFile, Variables, Name) :
     MVAResponse.TMVA.MVAdict.Variables = Variables
     # end of function 
   
+# TMVA tuple (to be added to the HybridTupleTool) 
+def addTMVAclassifierTuple(Component, XMLFile, Variables, Name) :
+    from Configurables import LoKi__Hybrid__Dict2Tuple as Dict2Tuple
+    Component.addTool(Dict2Tuple,Name)
+    MVAResponse = getattr(Component,Name)
+    Key = "BDT"
+    MVAResponse.Key = Key
+    MVAResponse.Source = "LoKi__Hybrid__DictTransform_TMVATransform_/TMVA"
+    Options = {
+        "XMLFile"    : XMLFile,
+        "Name"       : Key, 
+        "KeepVars"   : "0"}
+    
+    from Configurables import LoKi__Hybrid__DictTransform_TMVATransform_ as TMVAtransform
+    from Configurables import LoKi__Hybrid__DictOfFunctors
+    
+    MVAResponse.addTool(TMVAtransform,"TMVA")
+    MVAResponse.TMVA.Options = Options
+    MVAResponse.TMVA.Source = "LoKi__Hybrid__DictOfFunctors/MVAdict"
+    MVAResponse.TMVA.addTool(LoKi__Hybrid__DictOfFunctors,"MVAdict")
+    MVAResponse.TMVA.MVAdict.Variables = Variables
+    # end of function 
