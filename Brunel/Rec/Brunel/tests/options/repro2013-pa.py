b644b51bf99d54eb6bc28f8201726b902f4bf9f6
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-11-28 11:59:00 +0000

    Add test and options for reprocessing of 2013 pA data without GECs
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@165327 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c7e0af433929fb47e36dd8227fd784d30fc5e8ec

---
+++ Brunel/Rec/Brunel/tests/options/repro2013-pa.py
@@ -0,0 +1,25 @@
+from Gaudi.Configuration import *
+
+#-- File catalogs. First one is read-write
+FileCatalog().Catalogs = [ "xmlcatalog_file:MyCatalog.xml",
+                           "xmlcatalog_file:$BRUNELROOT/job/NewCatalog.xml" ]
+
+
+from Configurables import Brunel
+Brunel().DataType =  "2013"
+Brunel().InputType = "DST"
+file = "$QMTESTFILESROOT/data/136237_10ev_bug_svn164539.dst"
+EventSelector().Input.append(file)
+
+from Configurables import TrackSys
+
+TrackSys().GlobalCuts = { 'Velo':80000, 'IT':999999, 'OT':999999 }
+
+from Configurables import RichRecSysConf
+rConf = RichRecSysConf("RichOfflineRec")
+rConf.pixelConfig().MaxPixels      = 30000
+rConf.trackConfig().MaxInputTracks = 99999
+rConf.trackConfig().MaxUsedTracks  = 10000 # this used to be 1000
+rConf.photonConfig().MaxPhotons    = 900000 # this used to be 250000
+rConf.gpidConfig().MaxUsedPixels   = 90000 # this used to be 30000
+rConf.gpidConfig().MaxUsedTracks   = 10000 # this used to be 1000
