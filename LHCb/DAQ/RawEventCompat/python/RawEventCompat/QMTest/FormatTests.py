899310673e6ae34a1f013abf27085ffd6ed5fff4
rlambert <rlambert@cern.ch>
2013-12-02 16:43:33 +0000

    Add a test validator which can interpret the contents of DSTs and work out if the required locations are there or not
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@165530 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1d5c0c35406c9bc8b2b834a124b8a54171604b82

---
+++ LHCb/DAQ/RawEventCompat/python/RawEventCompat/QMTest/FormatTests.py
@@ -0,0 +1,53 @@
+"""
+QMTest validator expension module, for checking the split RawEvent file format
+"""
+
+def checkIsSplit(afile, version, causes, result, testname):
+    """
+    Check the split version of a file, compare the branches of 'Event' with the
+    expected contents from the database
+    """
+    from RawEventCompat.Configuration import RawEventFormatConf, _checkv, _replaceWrap
+    RawEventFormatConf().loadIfRequired()
+    from RawEventCompat.Configuration import ReverseDict
+    from RawEventFormat import Raw_location_db
+    version=_checkv(version)
+    required=[_replaceWrap(r).lstrip("/").rstrip("/") for r in ReverseDict(version).keys()]
+    shouldbegone=[]
+    otherversions=[v for v in Raw_location_db if v!=version]
+    for other in otherversions:
+        for loc in ReverseDict(other).keys():
+            loc=loc.lstrip("/").rstrip("/")
+            if loc not in shouldbegone and loc not in required:
+                shouldbegone.append(loc)
+    required=[("Event/"+r).replace("Event/Event/","Event/") for r in required]
+    shouldbegone=[("Event/"+r).replace("Event/Event/","Event/") for r in shouldbegone]
+    import ROOT
+    import os
+    if not os.path.exists(afile):
+        causes.append("File was not found "+afile)
+        result[testname]=result.Quote("File was not found "+afile)
+        return
+    f=ROOT.TFile.Open(afile)
+    if not f:
+        causes.append("File was not openable by ROOT "+afile)
+        result[testname]=result.Quote("File was not openable by ROOT "+afile)
+        return
+    names=[b.GetName().replace("_","/").lstrip("/").rstrip("/").rstrip(".") for b in f.Event.GetListOfBranches()]
+    #print names
+    f.Close()
+    missing=[r for r in required if r not in names]
+    therewrongly=[s for s in shouldbegone if s in names]
+    if len(missing)+len(therewrongly)==0:
+        return
+    causes.append("File was not of the correct split format")
+    quote=""
+    if len(missing):
+        quote=quote+"Expected the following locations: "+missing.__str__()
+    if len(therewrongly):
+        if len(quote):
+            quote=quote+"\n"
+        quote=quote+"Locations which should not be there: "+therewrongly.__str__()
+    result[testname]=result.Quote(quote)
+    return
+
