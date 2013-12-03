aeed724a20441ff8757d287386222511aac22f53
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-12-03 07:30:01 +0000

    Add exclusion for memory change warnings from BooleInit
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@165569 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 02718a4e2a210a8307734baadad95363bd77a8ca

--- Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
+++ Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
@@ -2,6 +2,7 @@ from GaudiTest import LineSkipper
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
+    LineSkipper(["BooleInit.Boole...SUCCESS Exceptions/Errors/Warnings/Infos Statistics : 0/0/"]) + \
     LineSkipper(["SIMCOND_"]) + \
     LineSkipper(["DDDB_"])
  
