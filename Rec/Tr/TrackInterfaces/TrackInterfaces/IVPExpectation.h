20d6c9110f99248d17121675971bd982b2b12316
Thomas Bird <thomas.bird@cern.ch>
2013-12-04 10:54:12 +0000

    adding VP Expectation tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@165660 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4845dd7be7bcabeb323437d52159e72005ad07ab

---
+++ Rec/Tr/TrackInterfaces/TrackInterfaces/IVPExpectation.h
@@ -0,0 +1,132 @@
+// $Id: IVPExpectation.h,v 1.5 2009-08-13 14:58:04 smenzeme Exp $
+#ifndef _IVPExpectation_H
+#define _IVPExpectation_H
+
+#include "GaudiKernel/IAlgTool.h"
+#include <bitset>
+
+static const InterfaceID IID_IVPExpectation( "IVPExpectation", 0, 0 );
+
+/** @class IVPExpectation IVPExpectation.h TrackInterfaces/IVPExpectation.h
+ *
+ *  interface for selecting tracks....
+ *
+ *  @author M.Needham
+ *  @date   11/03/2007
+ */
+
+namespace LHCb{
+ class Track;
+}
+
+class IVPExpectation: virtual public IAlgTool{
+
+public:
+
+
+  /** Helper struct
+  * n - Number of hits
+  */
+  struct Info {
+    unsigned int n;
+    double firstR;
+    double firstX;
+    double firstY;
+    double firstZ;
+    std::vector<double> expectedZ;
+  };
+
+
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() { return IID_IVPExpectation ; }
+
+  /** Returns number of hits expected, from zFirst to endVelo
+   *
+   *  @param aTrack Reference to the Track to test
+   *
+   *  @return number of hits expected
+   */
+  virtual int nExpected ( const LHCb::Track& aTrack ) const = 0;
+
+
+  /** Returns info on expected hits and fill bitmap for hit pattern
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @return Info
+   */
+  virtual IVPExpectation::Info expectedInfo ( const LHCb::Track& aTrack, std::bitset<30> velo[4] ) const = 0;
+
+
+  /** Returns info on expected hits
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @return Info
+   */
+  virtual IVPExpectation::Info expectedInfo ( const LHCb::Track& aTrack ) const = 0;
+
+
+  /** Returns number of hits expected, from zStart to zStop
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @param zStart --> start of scan range
+   *  @param zStop --> end of scan range
+   *  @return Info
+   */
+  virtual int nExpected ( const LHCb::Track& aTrack , const double zStart, const double zStop) const = 0;
+
+
+  /** Returns expected hits info, from zStart to zStop
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @param zStart --> start of scan range
+   *  @param zStop --> end of scan range
+   *  @return Info
+   */
+  virtual IVPExpectation::Info expectedInfo ( const LHCb::Track& aTrack , const double zStart, const double zStop) const = 0;
+
+
+   /** Returns expected hits info, from zStart to zStop and hit pattern
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @param zStart --> start of scan range
+   *  @param zStop --> end of scan range
+   *  @return Info
+   */
+  virtual IVPExpectation::Info expectedInfo ( const LHCb::Track& aTrack , const double zStart, const double zStop,
+                                                std::bitset<30> velo[4]) const = 0;
+
+
+  /** Returns number of hits missed, from zBeamLine to firstHit
+   *
+   *  @param aTrack Reference to the Track to test
+   *
+   *  @return number of hits missed before first hit
+   */
+  virtual int nMissed( const LHCb::Track& aTrack ) const = 0;
+
+
+  /** Returns number of hits missed, from z to firstHit
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @param z --> z to start from
+   *
+   *  @return number of hits missed before first hit
+   */
+  virtual int nMissed( const LHCb::Track& aTrack, const double z) const = 0;
+
+
+  /** Returns true if track passses through a working strip in the sensor
+   *
+   *
+   *  @param aTrack Reference to the Track to test
+   *  @param sensorNum ---> sensor number
+   *
+   *  @return true if crosses a working strip
+   */
+  virtual bool isInside(const LHCb::Track& aTrack,
+                const unsigned int sensorNum) const = 0;
+
+
+};
+
+#endif
