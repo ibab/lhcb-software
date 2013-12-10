50bdd361051d080977c69acb2e2d60aad7649762
Marco Cattaneo <marco.cattaneo@cern.ch>
2013-12-10 13:06:55 +0000

    update pA test to use AppConfig pa-GECs,py options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@165999 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1c8f8ab8b02824312c5b26a305ec32b6c1806f09

--- Brunel/Rec/Brunel/tests/options/repro2013-pa.py
+++ Brunel/Rec/Brunel/tests/options/repro2013-pa.py
@@ -12,15 +12,5 @@ file = "$QMTESTFILESROOT/data/136237_10ev_bug_svn164539.dst"
 EventSelector().Input.append(file)
 Brunel().SplitRawEventInput=2.0  # Stripping20
 
-from Configurables import TrackSys
-
-TrackSys().GlobalCuts = { 'Velo':80000, 'IT':999999, 'OT':999999 }
-
-from Configurables import RichRecSysConf
-rConf = RichRecSysConf("RichOfflineRec")
-rConf.pixelConfig().MaxPixels      = 30000
-rConf.trackConfig().MaxInputTracks = 99999
-rConf.trackConfig().MaxUsedTracks  = 10000 # this used to be 1000
-rConf.photonConfig().MaxPhotons    = 900000 # this used to be 250000
-rConf.gpidConfig().MaxUsedPixels   = 90000 # this used to be 30000
-rConf.gpidConfig().MaxUsedTracks   = 10000 # this used to be 1000
+# Suppress GECs for pA data
+importOptions("$APPCONFIGOPTS/Brunel/pA-GECs.py")
