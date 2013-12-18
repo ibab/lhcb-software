f71488ea3fdcc840c28fff43dc77c6a9065de214
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2013-12-18 14:58:04 +0000

    Update stripping references to adapt to new messages from the decoders.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@166295 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a674379f259f5d3c542c905dd60ead3d6c328560

--- DaVinci/DaVinciTests/tests/options/DaVinci-1000events.py
+++ DaVinci/DaVinciTests/tests/options/DaVinci-1000events.py
@@ -4,3 +4,8 @@ from Configurables import DaVinci
 # Number of events etc.
 DaVinci().EvtMax    = 1000
 DaVinci().PrintFreq = 10
+
+# Temporary. Force a particular CondDB tag
+#from Configurables import LHCbApp
+#LHCbApp().DDDBtag="head-20120413"
+#LHCbApp().CondDBtag="cond-20131028"
