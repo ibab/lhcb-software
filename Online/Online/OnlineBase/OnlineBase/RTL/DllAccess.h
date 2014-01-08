05513726f76cc9c11599dbe9c94e5bbfa1df9d6b
Marco Clemencic <marcocle@cern.ch>
2014-01-08 11:26:16 +0000

    added a couple of utility functions to RTL/DllAccess.h
     * OnlineBase::FuncPtrCast
       wrapper to cast void* to function pointers and vice versa
    
     * OnlineBase::currentCommand()
       return a string containing the full path to the current executable
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@166535 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2699a565b750caad0d751e5c1f34cd199064c03c

--- Online/Online/OnlineBase/OnlineBase/RTL/DllAccess.h
+++ Online/Online/OnlineBase/OnlineBase/RTL/DllAccess.h
@@ -1,7 +1,7 @@
 #ifndef ONLINEKERNEL_RTL_ALLACCESS_H
 #define ONLINEKERNEL_RTL_ALLACCESS_H
 
-typedef long (*func)(int, char**);
+typedef int (*func)(int, char**);
 namespace {
   union Function {
     explicit Function(void* p) { ptr = p; }
@@ -35,11 +35,34 @@ namespace {
 #else
   #include <dlfcn.h>
   #include <unistd.h>
+  #include <limits.h>
   //#define LOAD_LIB(x)  ::dlopen( x , RTLD_NOW)
   inline void* LOAD_LIB(const std::string& x) {   return ::dlopen(x.c_str(),RTLD_NOW);  }
   inline void* LOAD_LIB2(const std::string& x) {  return LOAD_LIB("lib"+x+".so"); }
   inline void* GETPROC(void* h, const char* x) {  return ::dlsym(h,x); }
   #define DLERROR      ::dlerror()
 #endif
+
+namespace OnlineBase {
+  inline std::string currentCommand() {
+    // see http://stackoverflow.com/questions/1023306/finding-current-executables-path-without-proc-self-exe
+    char buf[PATH_MAX] = {0}; // initialize buffer to 0s (see man 2 readlink)
+    ssize_t status = readlink("/proc/self/exe", buf, PATH_MAX);
+    if (status == -1) {
+      return "";
+    }
+    return buf;
+  }
+  // Imported from GaudiKernel
+  template <typename DESTPTR, typename SRCPTR>
+  inline DESTPTR FuncPtrCast(SRCPTR ptr) {
+    union {
+      SRCPTR src;
+      DESTPTR dst;
+    } p2p;
+    p2p.src = ptr;
+    return p2p.dst;
+  }
+}
 
 #endif // ONLINEKERNEL_RTL_ALLACCESS_H
