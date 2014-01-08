96870c2e4a6d52486f1bcbfdb7581e7b1836cf8d
Marco Clemencic <marcocle@cern.ch>
2014-01-08 14:16:32 +0000

    fixed compilation flags (CMake) and relocatability of libCheckpointing.so in test_save.sh
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@166553 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2ee89e6f6c9c557912203d0cb4ae0992225e0018

--- Online/Online/Checkpointing/tests/test_save.sh
+++ Online/Online/Checkpointing/tests/test_save.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "CMTCONFIG=$CMTCONFIG "
 #. setup.sh
-export LD_PRELOAD=${CHECKPOINTINGROOT}/${CMTCONFIG}/libCheckpointing.so
+export LD_PRELOAD=libCheckpointing.so
 #echo ${LD_LIBRARY_PATH}
 #echo ${PATH}
 #echo ${LD_PRELOAD}
