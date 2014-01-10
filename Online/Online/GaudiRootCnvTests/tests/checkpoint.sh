581ff866e30124a5c7bfb349303741da7edde1e9
Markus Frank <frankb@cern.ch>
2014-01-10 20:45:02 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@166684 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: a6b10e4823229b58816ed9a8ce7d0f1b5ed60df0

--- Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
+++ Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export DIM_DNS_NODE=blabla.cern.ch;
 export UTGID=CollRDCheckpoint;
-export LD_PRELOAD=${CHECKPOINTINGROOT}/${CMTCONFIG}/libCheckpointing.so;
+export LD_PRELOAD=libCheckpointing.so;
 `which GaudiCheckpoint.exe` libGaudiKernel.so GaudiMain $* 2>&1
