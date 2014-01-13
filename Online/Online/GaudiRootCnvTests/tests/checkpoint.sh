8bc58986b99d34ca270d0be75a3b5a4a8f44513c
Markus Frank <frankb@cern.ch>
2014-01-13 15:16:51 +0000

    Fixes for cmake build
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@166743 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c226f989c932d2110b5fea49373cf0da8b5c98b7

--- Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
+++ Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
@@ -2,4 +2,4 @@
 export DIM_DNS_NODE=blabla.cern.ch;
 export UTGID=CollRDCheckpoint;
 export LD_PRELOAD=libCheckpointing.so;
-`which GaudiCheckpoint.exe` libGaudiKernel.so GaudiMain $* 2>&1
+GaudiCheckpoint.exe libGaudiKernel.so GaudiMain $* 2>&1
--- Online/Online/GaudiRootCnvTests/tests/restore.sh
+++ Online/Online/GaudiRootCnvTests/tests/restore.sh
@@ -2,4 +2,4 @@
 export DIM_DNS_NODE=blabla.cern.ch;
 unset LD_PRELOAD;
 unset LD_LIBRARY_PATH;
-`which restore.exe` $* 2>&1;
+restore.exe $* 2>&1;
