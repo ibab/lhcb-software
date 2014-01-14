3b3134ed2b37a3bf600d357686dfdf4e0ab100db
Markus Frank <frankb@cern.ch>
2014-01-14 14:40:07 +0000

    Fix tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@166777 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9d89b73a47c29d9045470a9fd21ab0d9fcfc2a6c

--- Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
+++ Online/Online/GaudiRootCnvTests/tests/checkpoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DIM_DNS_NODE=blabla.cern.ch;
+export DIM_DNS_NODE=`hostname -s`;
 export UTGID=CollRDCheckpoint;
 export LD_PRELOAD=libCheckpointing.so;
 GaudiCheckpoint.exe libGaudiKernel.so GaudiMain $* 2>&1
--- Online/Online/GaudiRootCnvTests/tests/restore.sh
+++ Online/Online/GaudiRootCnvTests/tests/restore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DIM_DNS_NODE=blabla.cern.ch;
+export DIM_DNS_NODE=`hostname -s`;
 unset LD_PRELOAD;
 unset LD_LIBRARY_PATH;
 restore.exe $* 2>&1;
