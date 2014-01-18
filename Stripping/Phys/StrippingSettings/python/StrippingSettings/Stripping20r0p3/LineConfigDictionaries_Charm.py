3265bc29064d521ed67b8d92fbc37e862c21eeb5
Matthew John Charles <matthew.john.charles@cern.ch>
2014-01-18 00:19:07 +0000

    Mat: Add lines for Xicc to Charm for Stripping20r0p3 (but NOT Stripping20r1p3)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@166954 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 239048266d0f32233dcdd065ca10f7cf05b589bf

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p3/LineConfigDictionaries_Charm.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping20r0p3/LineConfigDictionaries_Charm.py
@@ -1,1 +1,39 @@
+"""
+Line config dictionaries for the Charm WG
+"""
+
+
+from GaudiKernel.SystemOfUnits import *
+
+# Re-run a subset of the Xicc lines which include a hyperon
+# in the final state to take proper advantage of triggers
+# introduced in 2012.
+#
+# Responsible: Mat Charles
+# Output to micro-DST
+
+Xicc = {
+    'BUILDERTYPE' : 'XiccBuilder',
+    'CONFIG'      : {
+    'LongTrackGEC'            : 150
+    ,'controlPrescaleLc'       : 0.0
+    ,'controlPrescaleXic'      : 0.1
+    , 'controlPrescaleDp'      : 0.0
+    , 'controlPrescaleD0'      : 0.0
+    , 'controlPrescaleDsp'     : 0.0
+    , 'signalPrescaleViaLc'    : 0.0
+    , 'signalPrescaleViaXic'   : 1.0
+    , 'signalPrescaleViaDp'    : 0.0
+    , 'signalPrescaleViaD0'    : 0.0
+    , 'signalPrescaleViaLcWC'  : 0.0
+    , 'signalPrescaleViaLcDCS' : 0.0
+    , 'signalPrescaleViaXicWC' : 1.0
+    , 'signalPrescaleViaDpWC'  : 0.0
+    , 'signalPrescaleViaD0WC'  : 0.0
+    , 'signalPrescaleViaD0DCS' : 0.0
+    , 'Hlt2TisTosSpec' : { 'Hlt2.*Decision%TOS' : 0 }
+    },
+    'WGs' : [ 'Charm' ],
+    'STREAMS' : [ 'Charm' ]
+    }
 
