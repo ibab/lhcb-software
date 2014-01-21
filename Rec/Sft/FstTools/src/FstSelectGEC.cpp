57217d1f5910d1181d3f0a45177e98abb1481a5f
Johannes Albrecht <johannes.albrecht@cern.ch>
2014-01-21 08:23:44 +0000

    adjust to cut on sum
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@167105 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6818e423f4b032eb6afb6dc168f9c5046767f87a

--- Rec/Sft/FstTools/src/FstSelectGEC.cpp
+++ Rec/Sft/FstTools/src/FstSelectGEC.cpp
@@ -1,11 +1,12 @@
 /**
  * Implementation of an algorithm that performs multiplicity cuts.
- * At this point, however, it only prints 'Hello, world!'
+ * It requires Patrick Robbe's LLTCaloAlg to have run beforehands.
  *
  * @author: Kevin Dungs
- * @date:   2014-01-10
+ * @date:   2014-01-19
  */
 
+#include "Event/L0CaloCandidate.h"
 #include "GaudiKernel/AlgFactory.h"
 
 #include "FstSelectGEC.h"
@@ -14,21 +15,28 @@
 DECLARE_ALGORITHM_FACTORY(FstSelectGEC)
 
 FstSelectGEC::FstSelectGEC(const std::string &name, ISvcLocator *loc)
-    : GaudiAlgorithm(name, loc) {
-    declareProperty("DoNothing", m_doNothing = false);
+: GaudiHistoAlg(name, loc) {
+    declareProperty("MultiplicityCutECAL", m_multiplicityCutECAL=1000);
+    declareProperty("MultiplicityCutHCAL", m_multiplicityCutHCAL=1000);
+    declareProperty("DoNothing", m_doNothing=false);
 }
 
 StatusCode FstSelectGEC::initialize() {
-    StatusCode sc = GaudiAlgorithm::initialize();
+    StatusCode sc = GaudiHistoAlg::initialize();
     if (sc.isFailure()) {
         return sc;
     }
+
+    m_nCaloMult=book(7 , "ECal and HCal multiplicity", 0.,12000.,400);
 
     if (m_doNothing) {
         info() << "This algorithm is turned off via the DoNothing property."
                << endmsg;
         return StatusCode::SUCCESS;
     }
+
+    info() << "MultiplicityCutECAL = " << m_multiplicityCutECAL << endmsg
+           << "MultiplicityCutHCAL = " << m_multiplicityCutHCAL << endmsg;
 
     return StatusCode::SUCCESS;
 }
@@ -39,15 +47,41 @@ StatusCode FstSelectGEC::execute() {
         return StatusCode::SUCCESS;
     }
 
-    always() << "Hello, world!" << endmsg;
-    setFilterPassed(true);
+    int multECAL = 0,
+        multHCAL = 0;
+
+    LHCb::L0CaloCandidates *cs = get<LHCb::L0CaloCandidates>("LLT/Calo");
+    for (
+        LHCb::L0CaloCandidates::iterator it = cs->begin();
+        it != cs->end();
+        ++it
+    ) {
+        LHCb::L0CaloCandidate *candidate = *it;
+        if (candidate->type() == 6) {  // We have a multiplicity cut.
+            // Personally, I'm not so very sure if using a double for such a
+            // switch is a good idea.
+            if (candidate->et() == 0.) {  // ECAL
+                multECAL = candidate->etCode();
+            } else if (candidate->et() == 1.) {  // HCAL
+                multHCAL = candidate->etCode();
+            } else { // This should not happen.
+                fatal() << "Multiplicity is neither ECAL nor HCAL!" << endmsg;
+            }
+        }
+    }
+
+    m_nCaloMult->fill(multECAL+multHCAL);
+    setFilterPassed(
+  	    (multECAL+multHCAL) < (m_multiplicityCutECAL+m_multiplicityCutHCAL)   //     multHCAL < m_multiplicityCutHCAL
+    );
+
     return StatusCode::SUCCESS;
 }
 
 StatusCode FstSelectGEC::finalize() {
     if (m_doNothing) {
-        return GaudiAlgorithm::finalize();
+        return GaudiHistoAlg::finalize();
     }
 
-    return GaudiAlgorithm::finalize();
+    return GaudiHistoAlg::finalize();
 }
--- Rec/Sft/FstTools/src/FstSelectGEC.h
+++ Rec/Sft/FstTools/src/FstSelectGEC.h
@@ -1,27 +1,33 @@
 /**
  * Defines an algorithm that performs multiplicity cuts.
- * Based on LLTCaloAlg by Patrick Robbe.
+ * It requires Patrick Robbe's LLTCaloAlg to have run beforehands.
  *
  * @author: Kevin Dungs
- * @date:   2014-01-10
+ * @date:   2014-01-19
  */
 
 #ifndef __FSTSELECTGEC_H__
 #define __FSTSELECTGEC_H__
 
 #include <string>
+#include "GaudiAlg/GaudiHistoAlg.h"  //MB                                                                                                                                                                                      
 
-#include "GaudiAlg/GaudiAlgorithm.h"
+
+// from AIDA Histogramming  //MB                                                                                                                                                                                               
+#include "AIDA/IHistogram1D.h"  //MB                                                                                                                                                                                           
+#include "AIDA/IHistogram2D.h"  //MB   
+//#include "GaudiAlg/GaudiAlgorithm.h"
 #include "TrackInterfaces/ITrackSelector.h"
 
 
 /** @class FstSelectGEC FstSelectGEC.h
  * An algorithm that performs multiplicity cuts.
+ * It depends on LLTCaloAlg having run before.
  *
  * @author Kevin Dungs
- * @date   2014-01-10
+ * @date   2014-01-17
  */
-class FstSelectGEC : public GaudiAlgorithm {
+class FstSelectGEC : public GaudiHistoAlg {
     public:
         FstSelectGEC(const std::string&, ISvcLocator*);
         StatusCode initialize();
@@ -29,7 +35,11 @@ class FstSelectGEC : public GaudiAlgorithm {
         StatusCode finalize();
 
     private:
+        int m_multiplicityCutECAL,  // above these values, events won't pass
+            m_multiplicityCutHCAL;
         bool m_doNothing;  // flag that turns off the algorithm
+	AIDA::IHistogram1D * m_nCaloMult;
+	
 };
 
 
