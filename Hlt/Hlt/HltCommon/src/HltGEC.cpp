e1d35614ef4c5e0e2c568bfe81f380fbf8605a9f
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-01-25 14:45:45 +0000

    simplify HltGEC
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@167448 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c2671598ae6fc9638ed4da84a07f13b845087bff

--- Hlt/Hlt/HltCommon/src/HltGEC.cpp
+++ Hlt/Hlt/HltCommon/src/HltGEC.cpp
@@ -27,16 +27,16 @@ Hlt::GEC::GEC( const std::string& type,   // the tool type
                const std::string& name,   // the tool instance name
                const IInterface* parent ) // the tool parent
     : base_class( type, name, parent ),
-      m_maxVeloBalance( -1 ),
-      m_maxOTHits( 10000 ),
-      m_maxITHits( 3000 ),
-      m_maxVeloHits( 3000 ),
-      m_minOTHits( 50 ),
-      m_minITHits( 50 ),
-      m_minVeloHits( 50 ),
-      m_rawBankDecoder( 0 ),
-      m_veloDet( 0 ),
-      m_isActivity( false )
+      m_maxVeloBalance{ -1 },
+      m_maxOTHits{ 10000 },
+      m_maxITHits{ 3000 },
+      m_maxVeloHits{ 3000 },
+      m_minOTHits{ 50 },
+      m_minITHits{ 50 },
+      m_minVeloHits{ 50 },
+      m_rawBankDecoder{ nullptr },
+      m_veloDet{ nullptr },
+      m_isActivity{ false }
 {
     declareProperty( "MaxOTHits", m_maxOTHits, "Maximum number of OT-hits" );
     declareProperty( "MaxITHits", m_maxITHits, "Maximum number of IT-hits" );
@@ -59,13 +59,9 @@ Hlt::GEC::~GEC()
 StatusCode Hlt::GEC::initialize()
 {
     StatusCode sc = GaudiTool::initialize();
-    if ( sc.isFailure() ) {
-        return sc;
-    }
-    //
+    if ( sc.isFailure() ) return sc;
     m_rawBankDecoder = tool<IOTRawBankDecoder>( "OTRawBankDecoder" );
     m_veloDet = getDet<DeVelo>( DeVeloLocation::Default );
-    //
     return sc;
 }
 // ============================================================================
@@ -73,7 +69,7 @@ StatusCode Hlt::GEC::initialize()
 // ============================================================================
 StatusCode Hlt::GEC::finalize()
 {
-    m_rawBankDecoder = 0;
+    m_rawBankDecoder = nullptr;
     return GaudiTool::finalize();
 }
 // ============================================================================
@@ -81,28 +77,24 @@ StatusCode Hlt::GEC::finalize()
 // ============================================================================
 bool Hlt::GEC::accept() const
 {
+    auto nOTHits = [&]() { return (int)m_rawBankDecoder->totalNumberOfHits(); };
+    auto nITHits = [&]() {
+        return (int)get<LHCb::STLiteCluster::STLiteClusters>(
+            LHCb::STLiteClusterLocation::ITClusters )->size();
+    };
+    auto nVeloHits = [&]() {
+        return (int)get<LHCb::VeloLiteCluster::VeloLiteClusters>(
+            LHCb::VeloLiteClusterLocation::Default )->size();
+    };
     if ( m_isActivity ) {
         // always()<<"run activity trigger"<<endmsg;
-        return ( m_minOTHits < 0 ||
-                 (int)m_rawBankDecoder->totalNumberOfHits() > m_minOTHits ) ||
-               ( m_minITHits < 0 || (int)get<LHCb::STLiteCluster::STLiteClusters>(
-                                        LHCb::STLiteClusterLocation::ITClusters )
-                                            ->size() > m_minITHits ) ||
-               ( m_minVeloHits < 0 ||
-                 (int)get<LHCb::VeloLiteCluster::VeloLiteClusters>(
-                     LHCb::VeloLiteClusterLocation::Default )->size() >
-                     m_minVeloHits );
-
+        return ( m_minOTHits < 0 || nOTHits() > m_minOTHits ) ||
+               ( m_minITHits < 0 || nITHits() > m_minITHits ) ||
+               ( m_minVeloHits < 0 || nVeloHits() > m_minVeloHits );
     } else {
-        return ( m_maxOTHits < 0 ||
-                 (int)m_rawBankDecoder->totalNumberOfHits() < m_maxOTHits ) &&
-               ( m_maxITHits < 0 || (int)get<LHCb::STLiteCluster::STLiteClusters>(
-                                        LHCb::STLiteClusterLocation::ITClusters )
-                                            ->size() < m_maxITHits ) &&
-               ( m_maxVeloHits < 0 ||
-                 (int)get<LHCb::VeloLiteCluster::VeloLiteClusters>(
-                     LHCb::VeloLiteClusterLocation::Default )->size() <
-                     m_maxVeloHits ) &&
+        return ( m_maxOTHits < 0 || nOTHits() < m_maxOTHits ) &&
+               ( m_maxITHits < 0 || nITHits() < m_maxITHits ) &&
+               ( m_maxVeloHits < 0 || nVeloHits() < m_maxVeloHits ) &&
                ( m_maxVeloBalance < 0 || veloBalance() < m_maxVeloBalance );
     }
 }
@@ -126,23 +118,21 @@ StatusCode Hlt::GEC::check()
 double Hlt::GEC::veloBalance() const
 {
     // Loop over velo clusters and compute the vector sum of all phi vectors
-    typedef LHCb::VeloLiteCluster::VeloLiteClusters container;
-    const container* clusters =
-        get<container>( LHCb::VeloLiteClusterLocation::Default );
+    const auto* clusters = get<LHCb::VeloLiteCluster::VeloLiteClusters>(
+        LHCb::VeloLiteClusterLocation::Default );
     assert( clusters != 0 );
     double sumx( 0 ), sumy( 0 );
     size_t nphi( 0 );
-    for ( container::const_iterator c = clusters->begin(); c != clusters->end();
-          ++c ) {
-        if ( !c->isPhiType() ) continue;
+    for ( const LHCb::VeloLiteCluster& c : *clusters ) {
+        if ( !c.isPhiType() ) continue;
         // navigate to the phi sensor
-        LHCb::VeloChannelID channelID = c->channelID();
+        LHCb::VeloChannelID channelID = c.channelID();
         int sensorID = channelID.sensor();
-        const DeVeloPhiType* phiSensor = m_veloDet->phiSensor( sensorID );
+        const DeVeloPhiType* sensor = m_veloDet->phiSensor( sensorID );
 
         // get phi
         unsigned int stripID = channelID.strip();
-        double phi = phiSensor->globalPhiOfStrip( stripID );
+        double phi = sensor->globalPhiOfStrip( stripID );
 
         // add to the vector sum
         sumx += std::cos( phi );
@@ -166,6 +156,6 @@ double Hlt::GEC::veloBalance() const
 // The Factory
 // ============================================================================
 DECLARE_NAMESPACE_TOOL_FACTORY( Hlt, GEC )
-    // ============================================================================
-    // The END
-    // ============================================================================
+// ============================================================================
+// The END
+// ============================================================================
