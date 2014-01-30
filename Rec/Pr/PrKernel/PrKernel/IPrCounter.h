0046be9fe85d907d5d83868b0eb2b7aa4124d8db
Michel De Cian <michel.de.cian@cern.ch>
2014-01-30 16:23:11 +0000

    Add IPrCounter as interface for PrCounter and PrUTCounter
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@167744 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0aac18a437938337f1055b3309b5c5b0e43b035e

---
+++ Rec/Pr/PrKernel/PrKernel/IPrCounter.h
@@ -0,0 +1,76 @@
+#ifndef PRKERNEL_IPRCOUNTER_H 
+#define PRKERNEL_IPRCOUNTER_H 1
+
+// Include files
+// from STL
+#include <string>
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+#include "Event/Track.h"
+
+
+namespace LHCb{
+  class MCParticle;
+  class LHCbID;
+}
+
+class IHistoTool;
+
+
+static const InterfaceID IID_IPrCounter ( "IPrCounter", 1, 0 );
+
+/** @class IPrCounter IPrCounter.h PrKernel/IPrCounter.h
+ *  
+ *
+ *  @author Michel De Cian & Paolo Gandini & Thomas Nikodem
+ *  @date   2013-12-13
+ */
+class IPrCounter : virtual public IAlgTool {
+public: 
+
+  // Return the interface ID
+  static const InterfaceID& interfaceID() { return IID_IPrCounter; }
+  
+  virtual StatusCode finalize() = 0;  
+  
+  virtual void initEvent(const IHistoTool* htool = NULL, const int nPV = 0) = 0;
+
+  virtual int  countAndPlot(const IHistoTool* htool,
+                            const LHCb::MCParticle* part, 
+                            std::vector<bool> flags,
+                            std::vector<LHCb::LHCbID>& ids, 
+                            const int nPV) = 0;
+
+  virtual void addSelection ( std::string name, bool writeHisto ) = 0;
+
+  void setContainer( std::string name )  { m_container = name; };
+  void setWriteHistos(int write){ m_writeHistos = write; };
+  void setUseEta25Cut(bool cut){ m_eta25cut = cut;};
+  void setTriggerNumbers(bool numbers){ m_triggerNumbers = numbers;};
+  void setSelectId( int data )   { m_selectId = data; };
+  void setTrackType( LHCb::Track::Types type)  { m_trackType = type; };
+
+  //virtual void setUse3MCLayerCut( bool cut) = 0;
+    
+  virtual void printStatistics() = 0;
+
+
+protected:
+
+  int          m_writeHistos;
+  bool         m_eta25cut;
+  bool         m_triggerNumbers;
+  int          m_selectId;
+  std::string  m_container;
+
+
+  LHCb::Track::Types m_trackType;
+
+private:
+
+
+
+};
+#endif // PRKERNEL_IPRCOUNTER_H
+
--- Rec/Pr/PrKernel/dict/PrKernelDict.h
+++ Rec/Pr/PrKernel/dict/PrKernelDict.h
@@ -4,6 +4,8 @@
 #include "PrKernel/IPrDebugTool.h"
 #include "PrKernel/IPrDebugUTTool.h"
 #include "PrKernel/IPrUTCounter.h"
+#include "PrKernel/IPrCounter.h"
+
 
 #endif // DICT_PRKERNELDICT_H
 
