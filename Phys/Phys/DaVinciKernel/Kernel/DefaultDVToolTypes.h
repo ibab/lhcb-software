6785bc61d90ef5d44c3057abb5c87e2576bf22f2
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-02-06 13:08:51 +0000

    revert the last change
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@167996 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ba8a6ce18b42460d727448010eaa8352e88ab388

--- Phys/Phys/DaVinciKernel/Kernel/DefaultDVToolTypes.h
+++ Phys/Phys/DaVinciKernel/Kernel/DefaultDVToolTypes.h
@@ -10,8 +10,8 @@ namespace DaVinci
     static const std::string Distance         = "LoKi::DistanceCalculator:PUBLIC";
     static const std::string VertexFitter     = "OfflineVertexFitter:PUBLIC";
     static const std::string ParticleCombiner = "OfflineVertexFitter:PUBLIC";
-    //static const std::string PVRelator = "GenericParticle2PVRelator__p2PVWithIPChi2_OfflineDistanceCalculatorName_/P2PVWithIPChi2:PUBLIC";
-    static const std::string PVRelator = "GenericParticle2PVRelator<_p2PVWithIPChi2, OfflineDistanceCalculatorName>/P2PVWithIPChi2:PUBLIC";
+    static const std::string PVRelator = "GenericParticle2PVRelator__p2PVWithIPChi2_OfflineDistanceCalculatorName_/P2PVWithIPChi2:PUBLIC";
+    //static const std::string PVRelator = "GenericParticle2PVRelator<_p2PVWithIPChi2, OfflineDistanceCalculatorName>/P2PVWithIPChi2:PUBLIC";
   }
 }
 
