a102f7ad3c39af0ffa7c46fb287d654773eb230d
rlambert <rlambert@cern.ch>
2014-02-06 13:36:21 +0000

    Significantly adapt MooreScripts CreateSetup.py to change the behaviour for stripping paths, now make sure we do leave the online satelite and local MooreOnline in the paths even if others are stripped out.
    git-svn-id: http://svn.cern.ch/guest/lhcb/MooreOnline/trunk@167998 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 667ef58929cef3b3368f79e253d2d5ca57fbca5e

--- MooreOnline/MooreScripts/python/MooreScripts/CreateSetup.py
+++ MooreOnline/MooreScripts/python/MooreScripts/CreateSetup.py
@@ -8,11 +8,17 @@ from zipfile import is_zipfile
 import fnmatch
 
 
-def StripPath(path,predicate=None):
+def StripPath(path,predicate=None,exceptions=["MooreOnline_","Moore_","Online_"]):
     collected = []
     for p in path.split(pathsep):
+        #print "checking", p
         rp = realpath(p)
-        if exists(rp) :
+        if len([True for m in exceptions if m in p]):
+            #print p, "in exceptions"
+            if p not in collected:
+                collected.append(p)
+        elif exists(rp) :
+            #print p, "exists"
             if ( isdir(rp) and len(listdir(rp))!=0 ) or is_zipfile(rp) :
                 if p not in collected and ( not predicate or predicate(p) ): collected.append(p)
     return pathsep.join(collected)
@@ -24,10 +30,29 @@ def ContainsFNmatch(dir,matches) :
         dirs.extend([ f for f in files if islink(f) and isdir(f)])
     return False
 #
-
-def StripSetupMoore( output ):
+def ReFactorPath(path,order=["MooreOnline_","MOOREONLINE_","Moore_","MOORE_","Online_","ONLINE_"]):
+    "re-order the path into the order specified, if more than one match to the ordering, the original order is preserverd for those matches"
+    collected=[]
+    #do all in the match statement
+    for m in order:
+        for p in path.split(pathsep):
+            if m in p and p not in collected:
+                collected.append(p)
+    #move Gaudi after LHCb
+    gaudi=[p for p in path.split(pathsep) if "GAUDI_" in p]
+    lhcb=[p for p in path.split(pathsep) if "LHCB_" in p]
+    for p in path.split(pathsep):
+        if p in collected:
+            continue
+        if  p not in gaudi and p not in lhcb:
+            collected.append(p)
+        if p in lhcb:
+            collected=collected+lhcb+gaudi
+    return pathsep.join(collected)
+#
+def FixSetupMoore( output ):
     """
-    Edit the output of SetupProject to remove certain rubbish
+    Edit the output of SetupProject to fix certain rubbish
     """
     if exists(output) :
 
@@ -44,23 +69,26 @@ def StripSetupMoore( output ):
                     if name == 'LD_LIBRARY_PATH':
                         # value = StripPath(value,lambda x: not re.search('lcg/external/Grid',x))
                         value = StripPath(value,lambda x: ContainsFNmatch(x,['*.so']))
+                        value=ReFactorPath(value)
                     if name == 'PYTHONPATH' :  # TODO: deal with python.zip files...
                         value = StripPath(value)
+                        value=ReFactorPath(value)
                         #value = StripPath(value,lambda x: ContainsFNmatch(x,['*.py','*.pyc']))
+                        
                     line = 'export %s="%s"'%(name,value)
                 if input!=line : f.write('#ORIG: %s\n'%input)
                 f.write(line+'\n')        
     else :
         print 'SetupProject did not generate %s... this is fine during a release build...' % output
 
-def CreateSetupMoore( output, version , setupProjectOps=[]) :
-
-    target_dir = dirname( output )
-    if not exists( target_dir ) : makedirs( target_dir )
-
-    from LbConfiguration.SetupProject import SetupProject
-
-    print 'generating %s assuming version %s'%(output,version)
-    SetupProject().main( [ '--dev-dir=/home/online/ONLINE','--shell=sh','--output='+output,'MooreOnline',version]+setupProjectOps )
-    StripSetupMoore(output)
+#def CreateSetupMoore( output, version , setupProjectOps=[]) :
+#
+#    target_dir = dirname( output )
+#    if not exists( target_dir ) : makedirs( target_dir )
+#
+#    from LbConfiguration.SetupProject import SetupProject
+#
+#    print 'generating %s assuming version %s'%(output,version)
+#    SetupProject().main( [ '--dev-dir=/home/online/ONLINE','--shell=sh','--output='+output,'MooreOnline',version]+setupProjectOps )
+#    StripSetupMoore(output)
 
