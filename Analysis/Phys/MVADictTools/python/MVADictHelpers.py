47ed914558454ff6d3e09867406e3a79f52f5b34
Andrey Ustyuzhanin <andrey.ustyuzhanin@cern.ch>
2014-02-12 10:59:44 +0000

    Added support for Matrixnet models
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@168243 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 74acee6931eaa94f8e1dac399ed0ca2d296838bf

--- Analysis/Phys/MVADictTools/python/MVADictHelpers.py
+++ Analysis/Phys/MVADictTools/python/MVADictHelpers.py
@@ -48,3 +48,53 @@ def addTMVAclassifierTuple(Branch, XMLFile, Variables,
     MVAResponse.TMVA.MVAdict.Preambulo = Preambulo
     MVAResponse.TMVA.MVAdict.Variables = Variables
     # end of function
+
+# Matrixnet Value (to be used for cutting)
+def addMatrixnetclassifierValue(Component, MatrixnetFile, Variables, ToolName) :
+    from Configurables import LoKi__Hybrid__DictValue as DictValue
+    Component.addTool(DictValue,ToolName)
+    MVAResponse = getattr(Component,ToolName)
+    Key = "BDT"
+    MVAResponse.Key = Key
+    MVAResponse.Source = "LoKi__Hybrid__DictTransform_MatrixnetTransform_/Matrixnet"
+    Options = {
+        "MatrixnetFile"    : MatrixnetFile,
+        "Name"       : Key,
+        "KeepVars"   : "0"}
+
+    from Configurables import LoKi__Hybrid__DictTransform_MatrixnetTransform_ as Matrixnettransform
+    from Configurables import LoKi__Hybrid__DictOfFunctors
+
+    MVAResponse.addTool(Matrixnettransform,"Matrixnet")
+    MVAResponse.Matrixnet.Options = Options
+    MVAResponse.Matrixnet.Source = "LoKi__Hybrid__DictOfFunctors/MVAdict"
+    MVAResponse.Matrixnet.addTool(LoKi__Hybrid__DictOfFunctors,"MVAdict")
+    MVAResponse.Matrixnet.MVAdict.Variables = Variables
+    # end of function
+
+# Matrixnet tuple (to be added to the HybridTupleTool)
+def addMatrixnetclassifierTuple(Branch, MatrixnetFile, Variables,
+                           Name="BDT", Keep=False, Preambulo=[]) :
+    from Configurables import LoKi__Hybrid__Dict2Tuple as Dict2Tuple
+    Branch.addTupleTool(Dict2Tuple,Name)
+    MVAResponse = getattr(Branch,Name)
+    #Key = "BDT"
+    #MVAResponse.Key = Key
+
+    MVAResponse.Source = "LoKi__Hybrid__DictTransform_MatrixnetTransform_/Matrixnet"
+    Options = {
+        "MatrixnetFile"    : MatrixnetFile,
+        "Name"       : Name,
+        "KeepVars"   : "1" if Keep else "0"}
+
+    from Configurables import LoKi__Hybrid__DictTransform_MatrixnetTransform_ as Matrixnettransform
+    from Configurables import LoKi__Hybrid__DictOfFunctors
+
+    MVAResponse.addTool(Matrixnettransform,"Matrixnet")
+    MVAResponse.Matrixnet.Options = Options
+    MVAResponse.Matrixnet.Source = "LoKi__Hybrid__DictOfFunctors/MVAdict"
+    MVAResponse.Matrixnet.addTool(LoKi__Hybrid__DictOfFunctors,"MVAdict")
+    MVAResponse.Matrixnet.MVAdict.Preambulo = Preambulo
+    MVAResponse.Matrixnet.MVAdict.Variables = Variables
+    # end of function
+
---
+++ Analysis/Phys/MVADictTools/src/MVADictTools/MatrixnetTransform.h
@@ -0,0 +1,54 @@
+#ifndef LIB_MATRIXNETTRANSFORM_H 
+#define LIB_MATRIXNETTRANSFORM_H 1
+
+
+#include "GaudiKernel/MsgStream.h"
+#include <iostream>
+#include "Kernel/IParticleDictTool.h"
+#include <string>
+#include <vector>
+#include <map>
+
+#include "Options.h"
+
+// typedef std::map<std::string, float> DICT;
+typedef IParticleDictTool::DICT DICT;
+/** @class MatrixnetTransform
+ *  Policy class to be used by the DictTransform template
+ *  Implementing the Matrixnet Reader backend
+ *
+ *  @author Andrey Ustyuzhanin, Egor Khairullin
+ *  @date   2014-01-31
+ */
+
+class MatrixnetTransform {
+public:
+  typedef std::map<std::string, std::string> optmap;
+
+private:
+  bool m_setup_success;
+  bool m_keep_all_vars;
+  std::string m_matrixnet_file;
+  std::string m_name;
+  std::string m_default_path;
+
+  // Matrixnet bits
+  std::string m_formula;
+
+  // Helper Functions
+  void readWeightsFile(std::ostream&);
+  bool parseOpts(optmap&, std::ostream&);
+
+public:
+  std::vector<std::string> m_variables; /// variables needed by the classifier
+  MatrixnetTransform();
+  ~MatrixnetTransform();
+
+  bool checkWeightsFile(std::ostream& info);
+
+  bool Init(optmap options, std::ostream& info);
+
+  bool operator()(const DICT& in, DICT& out) const;
+};
+
+#endif // LIB_MATRIXNETTRANSFORM_H
