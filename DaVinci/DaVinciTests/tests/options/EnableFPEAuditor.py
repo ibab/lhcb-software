4aa7577420c446a6f5886d69e4af56a2eb0b77c9
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-02-13 16:03:55 +0000

    Enabled FPE checks during initialize() as well
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@168309 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ca3534963ac00d427c2dc8fdfecbcb93eae7278f

--- DaVinci/DaVinciTests/tests/options/EnableFPEAuditor.py
+++ DaVinci/DaVinciTests/tests/options/EnableFPEAuditor.py
@@ -4,4 +4,4 @@ ApplicationMgr().ExtSvc + [ "AuditorSvc" ]
 AuditorSvc().Auditors += [ "FPEAuditor" ]
 from Configurables import FPEAuditor
 #FPEAuditor().TrapOn = [ "DivByZero", "Overflow", "Underflow" ]
-FPEAuditor().ActivateAt = ["Execute"]
+#FPEAuditor().ActivateAt = ["Execute"]
