f5f6712c3a1a71a393a1cd2e8ec2cd5fc1c396b9
rlambert <rlambert@cern.ch>
2014-02-16 12:50:19 +0000

    move test to use PRConfig
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@168439 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: efc88272e21fa45ca079dbd89c9386a11f5e8bd0

--- LHCb/Kernel/FSRAlgs/options/merge-small-files-test.py
+++ LHCb/Kernel/FSRAlgs/options/merge-small-files-test.py
@@ -2,23 +2,16 @@
 # Define InputData
 ###################
 
-InputData=[]
-
-for i in range(94):
-    InputData.append("castor:/castor/cern.ch/user/r/rlambert/smallfiles/R08S14EW/R08S14EW_small_1_#.dst".replace("#",str(i+1)))
-
-RecoDict= {"Reco" : "R08S14EW"}
+from PRConfig import TestFileDB
+TestFileDB["R08S14_smallfiles"].run()
 
 from Gaudi.Configuration import *
 from GaudiConf import IOHelper
 
-id=InputData
 
 ######################
 #choose number of files
 ######################
-
-IOHelper().inputFiles(id[:5])
 
 IOHelper().outStream("merged.dst","InputCopyStream")
 
