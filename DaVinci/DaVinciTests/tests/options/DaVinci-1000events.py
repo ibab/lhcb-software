aa755c5c2554e63a84108596ccd988be05e68efd
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-02-16 10:40:36 +0000

    - Add Stripping20rXp3 tests.  - Enable the FPE Auditor in all stripping, pid and packing tests.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@168435 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ee0f03ac010d6a30d72f03663441b0b8377fd7e9

--- DaVinci/DaVinciTests/tests/options/DaVinci-1000events.py
+++ DaVinci/DaVinciTests/tests/options/DaVinci-1000events.py
@@ -5,6 +5,8 @@ from Configurables import DaVinci
 DaVinci().EvtMax    = 1000
 DaVinci().PrintFreq = 10
 
+#DaVinci().SkipEvents = 17100
+
 # Temporary. Force a particular CondDB tag
 #from Configurables import LHCbApp
 #LHCbApp().DDDBtag="head-20120413"
