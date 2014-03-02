87ddf1f43c037d150581a287bca98e7a718e32f3
Stefano Perazzini <stefano.perazzini@cern.ch>
2014-03-02 17:24:28 +0000

    Updated qmtests for commonparticles
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@169479 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6fbbc9657ec5ac344c2acb05b0019ce05dfda042

--- DaVinci/DaVinciTests/tests/options/dimuon_testfile.py
+++ DaVinci/DaVinciTests/tests/options/dimuon_testfile.py
@@ -1,5 +1,5 @@
 from Gaudi.Configuration import EventSelector
 
 EventSelector().Input   = [
-    "DATAFILE='PFN:root://castorlhcb.cern.ch//castor/cern.ch/user/r/rlambert/testfiles/2010_DiMuon.dst' TYP='POOL_ROOTTREE' OPT='READ'"
+    "DATAFILE='PFN:root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/freezer/lhcb/LHCb/Collision12/FULL.DST/00020330/0004/00020330_00047243_1.full.dst' TYP='POOL_ROOTTREE' OPT='READ'"
     ]
