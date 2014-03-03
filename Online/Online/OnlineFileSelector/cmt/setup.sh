cd2dce817e3d6d4398d51b28aae4c23704496643
ichalkia <ichalkia@cern.ch>
2014-03-03 14:00:36 +0000

    No connection bug fixed
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@169545 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 14735e3fcef77344910c05f91548dd05ba2940cc

--- Online/Online/OnlineFileSelector/cmt/setup.sh
+++ Online/Online/OnlineFileSelector/cmt/setup.sh
@@ -1,12 +1,12 @@
-# echo "Setting OnlineFileSelector head in /afs/cern.ch/user/i/ichalkia/cmtuser/Online_v5r6/Online"
+# echo "Setting OnlineFileSelector v0r1 in /home/ichalkia/cmtuser/Online_v5r6/Online"
 
 if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/cern.ch/sw/contrib/CMT/v1r20p20090520; export CMTROOT
+  CMTROOT=/cvmfs/lhcb.cern.ch/lib/contrib/CMT/v1r20p20090520; export CMTROOT
 fi
 . ${CMTROOT}/mgr/setup.sh
 
 tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt setup -sh -pack=OnlineFileSelector -version=head -path=/afs/cern.ch/user/i/ichalkia/cmtuser/Online_v5r6/Online  -no_cleanup $* >${tempfile}; . ${tempfile}
+${CMTROOT}/mgr/cmt setup -sh -pack=OnlineFileSelector -version=v0r1 -path=/home/ichalkia/cmtuser/Online_v5r6/Online  -no_cleanup $* >${tempfile}; . ${tempfile}
 /bin/rm -f ${tempfile}
 
