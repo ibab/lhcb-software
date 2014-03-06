bb9aebd348dd5685293c23a53fc3b1f06d2a192b
Liang Sun <liang.sun@cern.ch>
2014-03-06 18:13:08 +0000

    Add a script to set up test environment for the split HLT scenario
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@169744 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7f37128f9efb46732dda935db8d73ef198eddd6c

---
+++ LHCb/Ex/DetCondExample/python/SplitHlt/testsplit.py
@@ -0,0 +1,28 @@
+from Gaudi.Configuration import *
+from Configurables import DetCondTest__TestConditionAlg as TestAlg
+from Configurables import LHCbAlgsTests__TestTimeDecoderOdin as TimeDecoder
+from Configurables import ( EventClockSvc, RunChangeHandlerSvc, XmlCnvSvc )
+from Configurables import LoadDDDB, DDDBConf, CondDB
+
+import sys
+sys.path.append('.')
+
+from rchoption_withhlt2 import calibconds
+
+DDDBConf(DbRoot = "/scratch/lsun/myDDDB/lhcb.xml") # Configure the detector description
+CondDB(IgnoreHeartBeat = True)
+
+EventClockSvc().addTool(TimeDecoder,"EventTimeDecoder")
+
+
+testAlg = TestAlg("TestCond")
+testAlg.Conditions = []
+testAlg.Conditions += calibconds # Load the conditions in rch for testing
+
+app = ApplicationMgr()
+app.TopAlg = [ TestAlg("TestCond") ]
+
+app.EvtSel = "NONE"
+app.EvtMax = 2
+
+EventDataSvc(ForceLeaves = True)
