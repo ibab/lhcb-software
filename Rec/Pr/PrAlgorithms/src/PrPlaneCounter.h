aa3eb946b63f2f9ac3e71800b3d46ec33b897969
Michel De Cian <michel.de.cian@cern.ch>
2014-03-10 17:22:04 +0000

    Changes in PrPlaneCounter which make it faster
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@169882 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b0d89212fc6d23151a2ed88308d126a182d46fa9

--- Rec/Pr/PrAlgorithms/src/PrPlaneCounter.h
+++ Rec/Pr/PrAlgorithms/src/PrPlaneCounter.h
@@ -16,45 +16,84 @@ public:
 
   /// Standard constructor
   PrPlaneCounter( )
-    : m_nbDifferent(0) 
+    : m_nbVals( {0,0} ),
+      m_planeList( {0,0,0,0,0,0,0,0,0,0,0,0 })
+                    
   {
-    m_planeList.resize( 12, 0 );
   }
 
   virtual ~PrPlaneCounter( ) {} ///< Destructor
 
   /// set the counters from the specified range.
-  void set( PrHits::const_iterator itBeg, PrHits::const_iterator itEnd )  {
-    m_nbDifferent = 0;
-    std::fill( m_planeList.begin(), m_planeList.end(), 0 );
+  void set( PrHits::const_iterator itBeg, PrHits::const_iterator itEnd, const bool fill = true)  {
+    
+    if(fill){
+      m_planeList.fill(0);
+      m_nbVals.fill(0);
+    }
+    
     for ( PrHits::const_iterator itH = itBeg; itEnd != itH; ++itH) {
-      addHit( *itH );
+      if ( (*itH)->isUsed() ) continue;
+      if ( 0 == m_planeList[(*itH)->planeCode()]++ ){
+        ++m_nbVals[0];
+        ++m_nbVals[1];
+      }else{
+        if ( 2 ==  m_planeList[(*itH)->planeCode()]){
+          --m_nbVals[1];
+        }
+      }
     }
   }
 
   /// add a hit to be counted
   int addHit( const PrHit* hit ) {
-    if ( hit->isUsed() ) return m_nbDifferent;
-    if ( 0 == m_planeList[hit->planeCode()]++ ) ++m_nbDifferent;
-    return m_nbDifferent;
+    if ( hit->isUsed() ) return m_nbVals[0];
+    if ( 0 == m_planeList[hit->planeCode()]++ ){
+      ++m_nbVals[0];
+      ++m_nbVals[1];
+    }else{
+      if ( 2 ==  m_planeList[hit->planeCode()]){
+        --m_nbVals[1];
+      }    
+    }
+    return m_nbVals[0];
   }
 
   /// remove a hit
   int removeHit( const PrHit* hit ) {
-    if ( hit->isUsed() ) return m_nbDifferent;
-    if ( 0 == --m_planeList[hit->planeCode()] ) --m_nbDifferent;
-    return m_nbDifferent;
+    if ( hit->isUsed() ) return m_nbVals[0];
+    if ( 0 == --m_planeList[hit->planeCode()] ){
+      --m_nbVals[0];
+      ++m_nbVals[1];
+    }else{
+      if( 0 == m_planeList[hit->planeCode()]){
+        --m_nbVals[1];
+      }
+    }
+    return m_nbVals[0];
   }
 
   /// returns number of different planes
-  unsigned int nbDifferent() const { return m_nbDifferent; }
+  unsigned int nbDifferent() const { return m_nbVals[0]; }
 
   /// returns number of hits in specified plane
-  int nbInPlane( int plane ) const { return m_planeList[plane]; }
+  int nbInPlane( const int plane ) const { return m_planeList[plane]; }
+
+  /// returns number of single planes
+  unsigned int nbSingle() const { return m_nbVals[1];}
+  
+  /// clear list with hits in planes and number of different planes / single-hit planes 
+  void clear(){
+    m_planeList.fill(0);
+    m_nbVals.fill(0);
+  }
+  
 
 private:
-  std::vector<int> m_planeList;
-  unsigned int m_nbDifferent;
+  
+  /// array for: number of different plane and number of planes with single hit 
+  std::array<unsigned int,2> m_nbVals;
+  std::array<int,12> m_planeList;
 };
 
 #endif // PRPLANECOUNTER_H
