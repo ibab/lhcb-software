6f7a699a57e34639ee25c18615ef32a36796cee8
David Hutchcroft <dhcroft@cern.ch>
2014-03-26 17:02:13 +0000

    Moved args from boole-sim08-xdst.qmt to tests/options/boole-sim08-xdst.py as failed in nightlies
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@170565 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c3bbb0d3dde11277b81f5dc1c8e99b29f479427f

---
+++ Boole/Digi/Boole/tests/options/boole-sim08-xdst.py
@@ -0,0 +1,4 @@
+from Boole.Configuration import *
+from PRConfig import TestFileDB
+TestFileDB.test_file_db["Brunel-SIM08-MinBias-Beam6500GeV-md100-nu4.8.xdst"].run()
+Boole().SiG4EnergyDeposit=True
