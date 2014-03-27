8fc1284cda061694f7bb697e99f842c6b4a77a3f
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-03-27 23:52:59 +0000

    PatFwdTrackCandidate: use default copy ctor, dtor, assignement, move ctor/assignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@170674 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 69cbf16d1ca024e8b23c23feb753775da0debd20

--- Rec/Tf/PatAlgorithms/src/PatFwdTrackCandidate.h
+++ Rec/Tf/PatAlgorithms/src/PatFwdTrackCandidate.h
@@ -19,10 +19,10 @@
 
 class PatFwdTrackCandidate final {
 public:
-  PatFwdTrackCandidate(const LHCb::Track *tr, PatFwdHits coords )
-      : m_track{tr}, m_coords{std::move(coords)} {
-    const LHCb::State* state = m_track->stateAt(LHCb::State::EndVelo);
-    assert( state != nullptr );
+  PatFwdTrackCandidate(const LHCb::Track *tr, PatFwdHits coords)
+      : m_track{ tr }, m_coords{ std::move(coords) } {
+    const LHCb::State *state = m_track->stateAt(LHCb::State::EndVelo);
+    assert(state != nullptr);
     m_x0 = state->x();
     m_y0 = state->y();
     m_z0 = state->z();
@@ -32,56 +32,23 @@ public:
     m_qOverP = state->qOverP();
   }
 
-  PatFwdTrackCandidate(const PatFwdTrackCandidate& )  = default;
-
-  PatFwdTrackCandidate(PatFwdTrackCandidate&& other) noexcept :
-      m_track ( other.m_track ),
-      m_x0 ( other.m_x0 ),
-      m_y0 ( other.m_y0 ),
-      m_z0 ( other.m_z0 ),
-      m_tx ( other.m_tx ),
-      m_ty ( other.m_ty ),
-      m_qOverP ( other.m_qOverP ),
-      m_coords( std::move( other.m_coords ) ), // this is the only 'move' that really matters...
-      m_x ( std::move(other.m_x)),
-      m_y ( std::move(other.m_y)),
-      m_quality ( other.m_quality),
-      m_candquality (std::move( other.m_candquality) ),
-      m_nb ( std::move(other.m_nb)),
-      m_chi2PerDof ( std::move(other.m_chi2PerDof)),
-      m_fitted ( other.m_fitted)
-  {
+  PatFwdTrackCandidate(const LHCb::Track *tr)
+      : PatFwdTrackCandidate(tr, PatFwdHits{}) {
+    m_coords.reserve(32);
   }
 
-  PatFwdTrackCandidate& operator=(const PatFwdTrackCandidate& )  = default;
+  template <typename I>
+  PatFwdTrackCandidate(const LHCb::Track *tr, I first, I last)
+      : PatFwdTrackCandidate(
+            tr, PatFwdHits{ std::forward<I>(first), std::forward<I>(last) }) {}
 
-  PatFwdTrackCandidate& operator=(PatFwdTrackCandidate&& other) noexcept {
-      m_track = other.m_track ;
-      m_x0 = other.m_x0 ;
-      m_y0 = other.m_y0 ;
-      m_z0 = other.m_z0 ;
-      m_tx = other.m_tx ;
-      m_ty = other.m_ty ;
-      m_qOverP = other.m_qOverP ;
-      m_coords = std::move( other.m_coords ) ; // this is the only 'move' that really matters...
-      m_x = std::move(other.m_x);
-      m_y = std::move(other.m_y);
-      m_quality = other.m_quality;
-      m_candquality =std::move( other.m_candquality);
-      m_nb = std::move(other.m_nb);
-      m_chi2PerDof = std::move(other.m_chi2PerDof);
-      m_fitted = other.m_fitted;
-      return *this;
-  }
+  PatFwdTrackCandidate(const PatFwdTrackCandidate &) = default;
+  PatFwdTrackCandidate(PatFwdTrackCandidate &&) = default;
 
-  PatFwdTrackCandidate(const LHCb::Track *tr ) : 
-      PatFwdTrackCandidate(tr,PatFwdHits{}) 
-  { m_coords.reserve(32); }
+  ~PatFwdTrackCandidate() = default;
 
-  template <typename I> 
-  PatFwdTrackCandidate(const LHCb::Track *tr, I first, I last) : 
-      PatFwdTrackCandidate(tr,PatFwdHits{std::forward<I>(first),std::forward<I>(last)}) 
-  {}
+  PatFwdTrackCandidate &operator=(const PatFwdTrackCandidate &) = default;
+  PatFwdTrackCandidate &operator=(PatFwdTrackCandidate &&) = default;
 
   void cleanCoords() {
     m_coords.erase(
@@ -90,9 +57,6 @@ public:
         std::end(m_coords));
   }
 
-  ~PatFwdTrackCandidate() {};
-
-
   const LHCb::Track *track() const { return m_track; }
   double xStraight(double z) const { return m_x0 + m_tx * (z - m_z0); }
   double yStraight(double z) const { return m_y0 + m_ty * (z - m_z0); }
@@ -100,12 +64,12 @@ public:
 
   double slX() const { return m_tx; }
   double slY() const { return m_ty; }
-  double slX2() const { return m_tx*m_tx; }
-  double slY2() const { return m_ty*m_ty; }
+  double slX2() const { return m_tx * m_tx; }
+  double slY2() const { return m_ty * m_ty; }
 
   double sinTrack() const { return sqrt(1. - 1. / (1. + slX2() + slY2())); }
 
-  PatFwdHits::const_iterator coordBegin()  const{ return m_coords.begin(); }
+  PatFwdHits::const_iterator coordBegin() const { return m_coords.begin(); }
   PatFwdHits::const_iterator coordEnd() const { return m_coords.end(); }
 
   PatFwdHits::iterator coordBegin() { return m_coords.begin(); }
@@ -113,11 +77,13 @@ public:
 
   PatFwdHits::iterator begin() { return m_coords.begin(); }
   PatFwdHits::iterator end() { return m_coords.end(); }
- 
+
   PatFwdHits::const_iterator begin() const { return m_coords.begin(); }
   PatFwdHits::const_iterator end() const { return m_coords.end(); }
 
-  PatFwdHits::const_reverse_iterator crbegin() const { return m_coords.crbegin(); }
+  PatFwdHits::const_reverse_iterator crbegin() const {
+    return m_coords.crbegin();
+  }
   PatFwdHits::const_reverse_iterator crend() const { return m_coords.crend(); }
 
   void addCoord(PatFwdHit *coord) { m_coords.push_back(coord); }
@@ -125,9 +91,8 @@ public:
     addCoords(std::begin(coords), std::end(coords));
   }
   template <typename I> void addCoords(I first, I last) {
-    m_coords.reserve(m_coords.size() + std::distance(first,last));
-    m_coords.insert(m_coords.end(), first, last );
-
+    m_coords.reserve(m_coords.size() + std::distance(first, last));
+    m_coords.insert(m_coords.end(), first, last);
   }
   const PatFwdHits &coords() const { return m_coords; }
   PatFwdHits &coords() { return m_coords; }
@@ -156,7 +121,6 @@ public:
 
     m_y[0] += day;
     m_y[1] += dby;
-
   }
 
   double x(double dz) const {
@@ -172,7 +136,7 @@ public:
 
   double xMagnet(double dz) const { return m_x[0] + dz * m_x[1]; }
 
-  double cosAfter() const { return  1. / sqrt(1. + m_x[1] * m_x[1]); }
+  double cosAfter() const { return 1. / sqrt(1. + m_x[1] * m_x[1]); }
 
   double dSlope() const { return m_x[1] - m_tx; }
 
@@ -189,25 +153,29 @@ public:
   void setQuality(double quality) { m_quality = quality; }
   double quality() const { return m_quality; }
 
-  void setChi2PerDoF(double chi2, int nDof) { m_chi2PerDof.first = chi2;m_chi2PerDof.second = nDof; }
+  void setChi2PerDoF(double chi2, int nDof) {
+    m_chi2PerDof.first = chi2;
+    m_chi2PerDof.second = nDof;
+  }
   double chi2PerDoF() const { return m_chi2PerDof.first; }
   int nDoF() const { return m_chi2PerDof.second; }
 
   double bx() const { return m_x[1]; }
 
-  int nbIT() const { return m_nb[0]; }
-  int nbOT() const { return m_nb[1]; }
   void setNbIT(int nb) { m_nb[0] = nb; }
   void setNbOT(int nb) { m_nb[1] = nb; }
+
+  int nbIT() const { return m_nb[0]; }
+  int nbOT() const { return m_nb[1]; }
 
   void setCand1stQuality(double cand1stquality) {
     m_candquality[0] = cand1stquality;
   }
-  double cand1stquality() const { return m_candquality[0]; }
-
   void setCand2ndQuality(double cand2ndquality) {
     m_candquality[1] = cand2ndquality;
   }
+
+  double cand1stquality() const { return m_candquality[0]; }
   double cand2ndquality() const { return m_candquality[1]; }
 
 private:
@@ -222,13 +190,13 @@ private:
 
   PatFwdHits m_coords;
 
-  std::array<double,4>  m_x{{ 0., 0., 0., 0. }};
-  std::array<double,2>  m_y{{ 0., 0. }};
+  std::array<double, 4> m_x{ { 0., 0., 0., 0. } };
+  std::array<double, 2> m_y{ { 0., 0. } };
 
   double m_quality{ 0. };
-  std::array<double,2> m_candquality{{0.,0.}};
-  std::array<int,2> m_nb{{ 0, 0 }}; // IT, OT
-  std::pair<double,int> m_chi2PerDof{ 0.,0 };
+  std::array<double, 2> m_candquality{ { 0., 0. } };
+  std::array<int, 2> m_nb{ { 0, 0 } }; // IT, OT
+  std::pair<double, int> m_chi2PerDof{ 0., 0 };
   bool m_fitted{ false };
 };
 #endif // PATFWDTRACKCANDIDATE_H
