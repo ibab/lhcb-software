dd7bcbd880395108d9a160adfa9ab996d7c9cbd5
Gloria Corti <gloria.corti@cern.ch>
2014-03-31 10:44:28 +0000

    fix bug so that pt0 setting is for all production mechanism
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@170734 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 391975bc3799e78f4214977a876d67b60405b359

--- Gauss/Gen/LbPythia8/options/Pythia8_Pt0Ref2.6_CT09MCS.py
+++ Gauss/Gen/LbPythia8/options/Pythia8_Pt0Ref2.6_CT09MCS.py
@@ -8,22 +8,23 @@ gen = Generation()
 gen.addTool( MinimumBias , name = "MinimumBias" )
 gen.MinimumBias.ProductionTool = "Pythia8Production"
 gen.MinimumBias.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.MinimumBias.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
+gen.MinimumBias.Pythia8Production.Tuning = "LHCbpT0Ref2.6_CT09MCS.cmd"
 
 gen.addTool( Inclusive , name = "Inclusive" )
 gen.Inclusive.ProductionTool = "Pythia8Production"
 gen.Inclusive.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.Inclusive.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
+gen.Inclusive.Pythia8Production.Tuning = "LHCbpT0Ref2.6_CT09MCS.cmd"
+
 
 gen.addTool( SignalPlain , name = "SignalPlain" )
 gen.SignalPlain.ProductionTool = "Pythia8Production"
 gen.SignalPlain.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.SignalPlain.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
+gen.SignalPlain.Pythia8Production.Tuning = "LHCbpT0Ref2.6_CT09MCS.cmd"
 
 gen.addTool( SignalRepeatedHadronization , name = "SignalRepeatedHadronization" )
 gen.SignalRepeatedHadronization.ProductionTool = "Pythia8Production"
 gen.SignalRepeatedHadronization.addTool( Pythia8Production , name = "Pythia8Production" )
-gen.SignalRepeatedHadronization.Pythia8Production.Tuning = "LHCbDefault_CT09MCS.cmd"
+gen.SignalRepeatedHadronization.Pythia8Production.Tuning = "LHCbpT0Ref2.6_CT09MCS.cmd"
 gen.SignalRepeatedHadronization.Pythia8Production.Commands += Pythia8TurnOffFragmentation
 
 gen.addTool( Special , name = "Special" )
