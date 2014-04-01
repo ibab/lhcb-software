200880e8f6b61bf68bd6c6ac56e56f93c1c7f335
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-04-01 11:21:33 +0000

    remove unneccessary files
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@170817 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 720af91164ae6c08dd2b704c3b3ed6f7b4948e95

--- Analysis/Phys/DecayTreeTupleANNPID/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/cvmfs/lhcb.cern.ch/lib/contrib/CMT/v1r20p20090520; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=DecayTreeTupleANNPID -version=v1r0 -path=/var/clus/usera/jonesc/cmtuser/DaVinci_v34r0/Phys $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
