831f377b93757b61c61478cec3d5372ee30b3c6f
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-04-02 09:31:51 +0000

    Remove functions using VPLiteCluster from VPClusterPosition interface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@170934 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c4688682b3761b0018ddab2cdb9d3d75450ee1c5

--- LHCb/Tr/LHCbTrackInterfaces/TrackInterfaces/IVPClusterPosition.h
+++ LHCb/Tr/LHCbTrackInterfaces/TrackInterfaces/IVPClusterPosition.h
@@ -1,15 +1,14 @@
 #ifndef KERNEL_IVPCLUSTERPOSITION_H 
 #define KERNEL_IVPCLUSTERPOSITION_H 1
 
-// Include files
-
-// from Gaudi
+// Gaudi
 #include "GaudiKernel/IAlgTool.h"
+// LHCb
 #include "Kernel/PixelPositionInfo.h"
 
-static const InterfaceID IID_IVPClusterPosition ("IVPClusterPosition", 1, 0);
+static const InterfaceID IID_IVPClusterPosition("IVPClusterPosition", 1, 0);
 
-/** @class IVPClusterPosition IVPClusterPosition.h Kernel/IVPClusterPosition.h
+/** @class IVPClusterPosition IVPClusterPosition.h TrackInterfaces/IVPClusterPosition.h
  *  
  *
  *  @author Victor Coco
@@ -18,32 +17,20 @@ static const InterfaceID IID_IVPClusterPosition ("IVPClusterPosition", 1, 0);
 
 namespace LHCb {
   class VPCluster;
-  class VPLiteCluster;
   class VPChannelID;
   class StateVector;
 }
 
-class IVPClusterPosition : virtual public IAlgTool{
+class IVPClusterPosition : virtual public IAlgTool {
 public:
 
   // a new tool interface, for VP, can be used in other pixel detector
-  // (defined in Kernel/PixelPositonInfo.h)
+  // (defined in Kernel/PixelPositionInfo.h)
   typedef LHCb::PixelPositionInfo<LHCb::VPChannelID> toolInfo;
   typedef std::pair<double, double> Direction;
   
   // Return the interface ID
   static const InterfaceID& interfaceID() {return IID_IVPClusterPosition;}
-
- /** calculate position of the VPLiteCluster
-  * @param aCluster pointer to a VPLiteCLuster
-  * @return toolInfo (templated struct) 
-  * <br> pixel = channel ID of a nearest pixel
-  * <br> fractionalPosition = fractional position in the pixel (better precision)
-  * within range (0-1,0-1)
-  * <br> error = estimate of the error (in fraction of pixel)
-  * The returned error estimate depends only on pixel size
-  */
-  virtual toolInfo position(const LHCb::VPLiteCluster* aCluster) const=0;
 
  /** Calculate position of a given VPCluster
   * @param cluster pointer to a VPCluster
@@ -54,24 +41,6 @@ public:
   * The returned error estimate depends only on pixel size
   */
   virtual toolInfo position(const LHCb::VPCluster* cluster) const = 0;
-
- /** calculate position of the VPLiteCluster
-  * @param aCluster pointer to a VPLiteCLuster cluster
-  * @param aPoint const reference to XYZPoint - XYZ position of track  
-  * in the sensor in global reference frame. 
-  * @param aDirection const reference to pair of doubles - directions of 
-  *        track xz slope and yz slope
-  * @return toolInfo (templated struct) 
-  * <br> pixel = channel ID of a nearest pixel
-  * <br> fractionalPosition = fractional position in the pixel (better precision)
-  * within range (0-1,0-1)
-  * <br> error = estimate of the error (in fraction of pixel)
-  * The returned error estimate depends both on pixel size and 
-  * projected angle of a track
-  */
-  virtual toolInfo position(const LHCb::VPLiteCluster* aCluster,
-                            const Gaudi::XYZPoint& aPoint,
-                            const Direction& aDirection) const=0;
 
  /** Calculate position of a given VPCluster
   * @param cluster pointer to a VPCluster
@@ -87,21 +56,6 @@ public:
   virtual toolInfo position(const LHCb::VPCluster* cluster,
                             const Gaudi::XYZPoint& point,
                             const Direction& direction) const = 0;
-
- /** calculate position of the VPLiteCluster
-  * @param aCluster pointer to a VPLiteCluster cluster
-  * @param aState const reference to VectorState object
-  * @return toolInfo (templated struct) 
-  * <br> pixel = channel ID of a nearest pixel
-  * <br> fractionalPosition = fractional position in the pixel (better precision)
-  * within range (0-1,0-1)
-  * <br> error = estimate of the error (in fraction of pixel)
-  * The returned error estimate depends both on pixel size and 
-  * projected angle of a track
-  */
-
-  virtual toolInfo position(const LHCb::VPLiteCluster* aCluster,
-                            const LHCb::StateVector& aState) const=0;
 
 };
 #endif // KERNEL_IVPCLUSTERPOSITION_H 
