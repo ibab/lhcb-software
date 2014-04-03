999a865f52098ff8d913ed7f3fd745bbfda312db
rlambert <rlambert@cern.ch>
2014-04-03 15:12:29 +0000

    consolodate configurables tests into a single module, task #50698
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@171122 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 68de9dd1720274fb6192ad8a010636157a07ed72

---
+++ LHCb/GaudiConf/python/GaudiConf/QMTest/ConfigurablesTest.py
@@ -0,0 +1,223 @@
+###################################################
+# Helper functions, shared code, used in the tests
+###################################################
+def generateList(packages=[]):
+    """
+    Return a dictionary of all configurables, {package: {configurable: True/False/None} }
+    input a list of packages to restrict the packages examined
+    True: exists in Configurables and is instantiated OK
+    False: exists in Configurables but fails to instantiate
+    None: Exists in confDB but not in Configurables?
+    """
+    from GaudiKernel.ConfigurableDb import cfgDb, loadConfigurableDb
+    import Configurables
+    #package : conf : Pass/Fail (True/False)
+    retdict={}
+    loadConfigurableDb()
+    for name,conf in cfgDb.iteritems():
+        mypack=conf['package']
+        if len(packages) and mypack not in packages:
+            continue
+        if mypack not in retdict:
+            retdict[mypack]={}
+        retdict[mypack][name]=None
+        
+        if hasattr(Configurables, name):
+            try:
+                aConf=getattr(Configurables,name)
+                aConf()
+                retdict[mypack][name]=True
+            except Exception,e:
+                retdict[mypack][name]=False
+                print >> sys.stderr, 'ERROR, cannot import/instantiate configurable', name, '\n-------\n', e.__class__, '\n-------'
+                traceback.print_exc()
+    return retdict
+
+def saveTo(ofile,thingy):
+    f=open(ofile,"w")
+    f.write(thingy.__str__())
+    f.close()
+
+def evalProject():
+    """
+    guess project name from directory
+    """
+    import os
+    cwd=os.getcwd()
+    s=[s for s in cwd.split(os.sep) if "_" in s][-1]
+    proj=s.split("_")[0]
+    ver=s.split("_")[-1]
+    return proj,ver
+
+def moduleList():
+    """
+    Determine a list of modules I expected to see
+    """
+    proj,ver=evalProject()
+    import commands
+    stat,out=commands.getstatusoutput("svnProjectDeps "+proj+" "+ver)
+    if stat:
+        print out
+        raise ValueError("Couldn't call svnProjectDeps :S ")
+    out=out.replace("\t"," ")
+    while "  " in out:
+        out=out.replace("  "," ")
+    proj_pack_list=[l.strip().split()[:-1] for l in out.split("\n")]
+    proj_pack={}
+    for proj_pack_i in proj_pack_list:
+        if proj_pack_i[0] not in proj_pack:
+            proj_pack[proj_pack_i[0]]=[proj_pack_i[2].split('/')[-1]]
+        else:
+            proj_pack[proj_pack_i[0]].append(proj_pack_i[2].split('/')[-1])
+    return proj_pack
+
+def dict_from_file(fname):
+    """
+    read a dictionary from a file into a variable to return
+    """
+    if not os.path.exists(fname):
+        raise IOError(fname + " does not exist, so I cannot read from it")
+    f=open(fname)
+    fr=f.read()
+    f.close()
+    exec("retdict = "+fr.replace("\n"," "))
+    if type(retdict) is not dict:
+        raise TypeError(fname+" did not contain a dictionary!")
+    return retdict
+
+#########################################
+# The test functions, run one of these!
+#   python -c "from GaudiConf.QMTest.ConfigurablesTest import test...; test...(outputfile);"
+#########################################
+
+
+def testThisProjectOnly(output,project=None):
+    if project is None:
+        project,dummy=evalProject()
+    if project is None:
+        raise ValueError("could not determine project automatically, please supply a string")
+    proj_pack=moduleList()
+    findproj=[p for p in proj_pack if p==me or p.lower()==me.lower()][0]
+    clist=generateList(proj_pack[findproj])
+    saveTo(output,clist)
+    return
+
+def testThisPackOnly(output,package):
+    clist=generateList([package.split('/')[-1]])
+    saveTo(output,clist)
+    return
+
+def testAllConfigurables(output):
+    clist=generateList()
+    saveTo(output,clist)
+    return
+
+
+##############################################
+# Evaluate the results, comparing a ref file dictionary with a test output
+##############################################
+
+def compareConfigurables(reference, testoutput, causes, result, testname, packages=[]):
+    """
+    Compare two sets of configurables
+    reference: compare against these configurables, a filename
+    testoutput: the result of the current list, a dictionary or filename
+    package: a list of packages to check, empty means everything
+    causes: the usual QMTest causes
+    result: the usual QMTest result
+    testname: the usual QMTestname
+    """
+    rfile=reference
+    reference=dict_from_file(reference)
+    if type(testoutput) is str:            
+        testoutput=dict_from_file(testoutput)
+    if type(reference) is not dict:
+        raise TypeError("expected reference dictionary, got instead "+str(type(reference)))
+    if type(testoutput) is not dict:
+        raise TypeError("expected testoutput dictionary, got instead "+str(type(testoutput)))
+    if reference==testoutput:
+        return
+    saveTo(rfile+".new",reference)
+    missingpackages=[]
+    addedpackages=[]
+    failedpackages=[]
+    brokenpackages=[]
+    mixedpackages=[]
+    extraconfigurables=[]
+    missingconfigurables=[]
+    failedconfigurables=[]
+    brokenconfigurables=[]
+    if not len(packages):
+        packages=reference.keys()+testoutput.keys()
+    if len(packages):
+        missingpackages=[m for m in reference if m not in testoutput and m in packages]
+        addedpackages=[m for m in testoutput if m not in reference and m in packages]
+        #all None
+        brokenpackages=[m for m in testoutput if  m in packages and [None]*len(testoutput[m])==[testoutput[m][v] for v in testoutput[m]] ]
+        #all False
+        failedpackages=[m for m in testoutput if  m in packages and [False]*len(testoutput[m])==[testoutput[m][v] for v in testoutput[m]] ]
+        #Nothing True
+        mixedpackages=[m for m in testoutput if  m in packages and True not in [testoutput[m][v] for v in testoutput[m]] ]
+        #misc not all False/None
+        for p in packages:
+            if p not in missingpackages and p not in brokenpackages and p not in failedpackages and p not in mixedpackages:
+                if p in testoutput:
+                    cs=testoutput[p]
+                    for c,v in cs.iteritems():
+                        if v is False:
+                            failedconfigurables.append((c,p))
+                        elif v is None:
+                            brokenconfigurables.append((c,p))
+                        elif c not in reference[p]:
+                            extraconfigurables.append((c,p))
+                if p in reference:
+                    cs=reference[p]
+                    for c,v in cs.iteritems():
+                        if c not in testoutput[p]:
+                            missingconfigurables.append((c,p))
+    
+    #if there is a missing package, test if I expected it to be gone!
+    movedpackages=[]
+    if len(missingpackages):
+        expected=[]
+        proj_pack=moduleList()
+        for k,v in proj_pack.iteritems():
+            expected+=v
+        movedpackages=[m for m in missingpackages if m not in expected]
+        missingpackages=[m for m in missingpackages if m in expected]
+    #reasons to fail
+    if len(missingpackages):
+        causes.append("Entire Package Missing")
+        result[testname+".missingpack"]=result.Quote("Completely cannot see the following packages, even though they were there in svnProjectDeps, indicates some major problem with svnProjectDeps or that the package failed to build: "+missingpackages.__str__())
+    if len(brokenpackages):
+        causes.append("Entire package broken")
+        result[testname+".brokenpack"]=result.Quote("Package existed in confDB, but had no entries in Configurables module? Perhaps the python code was broken in this package : "+brokenpackages.__str__())
+    if len(failedpackages):
+        causes.append("Entire package failed")
+        result[testname+".failedpack"]=result.Quote("All configurables in a package failed to instantiate. Check stderr, definitely a problem with the configurables themselves. : "+failedpackages.__str__())
+    if len(mixedpackages):
+        causes.append("Entire package broken or failed")
+        result[testname+".mixedpack"]=result.Quote("All configurables either did not make it to Configurables module or failed to instantiate, probably the module is broken : "+mixedpackages.__str__())
+    if len(failedconfigurables):
+        causes.append("Configurables failed to instantiate")
+        result[testname+".failed"]=result.Quote("Failed to instantiate : "+mixedpackages.__str__())
+    if len(brokenconfigurables):
+        causes.append("Configurables which didn't make if to Configurables module")
+        result[testname+".broken"]=result.Quote("Failed to make it to Configurables module : "+mixedpackages.__str__())
+    #only fail if there are a lot of modifications
+    if len(movedpackages+addedpackages)>20:
+        causes.append("More than 20 svn moves have taken place, please update the reference! ")
+    if len(extraconfigurables+missingconfigurables)>50:
+        causes.append("More than 50 extra/lost configurables, this is suspicious!")
+    #not of themselves a problem
+    if len(movedpackages):
+        result[testname+".movedpack"]=result.Quote("Moved packages in svn : "+movedpackages.__str__())
+    #not of themselves a problem
+    if len(addedpackages):
+        result[testname+".addedpack"]=result.Quote("Added packages in svn : "+addedpackages.__str__())
+    #not of themselves a problem
+    if len(missingconfigurables):
+        result[testname+".missing"]=result.Quote("Missing configurables : "+missingconfigurables.__str__())
+    if len(extraconfigurables):
+        result[testname+".added"]=result.Quote("Added configurables : "+addedconfigurables.__str__())
+    
