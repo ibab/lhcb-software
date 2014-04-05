8bf402cf48391c1a14b50aadcd21f7610486d7f6
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-04-05 11:51:35 +0000

    VPClusterLinker: add temporary check if table already exists
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@171198 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a0f40ca1412934013f02edc86a1cdf985317e126

--- Lbcom/VP/VPAssociators/src/VPClusterLinker.cpp
+++ Lbcom/VP/VPAssociators/src/VPClusterLinker.cpp
@@ -47,6 +47,17 @@ VPClusterLinker::~VPClusterLinker() {}
 //=============================================================================
 StatusCode VPClusterLinker::execute() {
 
+  // Check if there is already an existing linker table.
+  // TODO: to be removed once support for old-style raw banks is dropped.
+  SmartDataPtr<LHCb::LinksByKey> links(evtSvc(), "Link/" + m_clusterLocation);
+  if (links) {
+    // Table exists. Nothing to do.
+    if (msgLevel(MSG::DEBUG)) {
+      debug() << "Found association table." << endmsg;
+    } 
+    return StatusCode::SUCCESS;
+  }
+
   // Get clusters.
   const VPClusters* clusters = getIfExists<VPClusters>(m_clusterLocation);
   if (!clusters) {
