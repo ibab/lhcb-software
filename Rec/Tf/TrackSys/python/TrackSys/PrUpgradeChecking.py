b18f3e64efc242f96a3a4fbfdc55d0d687fa8d2d
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-04-10 14:05:45 +0000

    Run VPClusterLinker only if VP is in the list of subdetectors.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@171340 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 10bba510dadf895865466ab527b4d350de0e34cc

--- Rec/Tf/TrackSys/python/TrackSys/PrUpgradeChecking.py
+++ Rec/Tf/TrackSys/python/TrackSys/PrUpgradeChecking.py
@@ -6,13 +6,23 @@ def PrUpgradeChecking():
    ### match hits and tracks
    log.warning("Run upgrade checkers.")
    from Configurables import UnpackMCParticle, UnpackMCVertex, PrLHCbID2MCParticle
-   from Configurables import VPClusterLinker
+   # Check if VP is part of the list of detectors.
+   from Configurables import LHCbApp, VPClusterLinker
+   withVP = False
+   if hasattr(LHCbApp(), "Detectors"):
+      if LHCbApp().isPropertySet("Detectors"):
+         if 'VP' in LHCbApp().upgradeDetectors(): withVP = True
    trackTypes = TrackSys().getProp("TrackTypes")
    if "Truth" in trackTypes :
       truthSeq = GaudiSequencer("RecoTruthSeq")
-      truthSeq.Members = [UnpackMCParticle(), UnpackMCVertex(), VPClusterLinker(), PrLHCbID2MCParticle()]
+      truthSeq.Members = [UnpackMCParticle(), UnpackMCVertex()]
+      if withVP: truthSeq.Members += [VPClusterLinker()]
+      truthSeq.Members += [PrLHCbID2MCParticle()]
    else:
-      GaudiSequencer("MCLinksTrSeq").Members = [VPClusterLinker(), PrLHCbID2MCParticle() ]
+      if withVP:
+        GaudiSequencer("MCLinksTrSeq").Members = [VPClusterLinker(), PrLHCbID2MCParticle()]
+      else:
+        GaudiSequencer("MCLinksTrSeq").Members = [PrLHCbID2MCParticle()]
    
    from Configurables import PrTrackAssociator
    GaudiSequencer("MCLinksTrSeq").Members += [ PrTrackAssociator() ]
