7273544fbcca362b2e5cc4a1f0a8c48c61c8f05c
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-04-28 14:04:01 +0000

    - never instantiate HltConf online...  - institutionalize forwarding from OnlineEnv to various other places  - forward RunChangeHandlerConditions
    git-svn-id: http://svn.cern.ch/guest/lhcb/MooreOnline/trunk@172034 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3dc1ba56e74c1bb201476db925ad9ae39693d043

--- MooreOnline/MooreScripts/python/MooreScripts/runOnline.py
+++ MooreOnline/MooreScripts/python/MooreScripts/runOnline.py
@@ -30,18 +30,34 @@ def start(**kwargs) :
     moore.DDDBtag    = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
     moore.CondDBtag  = 'unknown-please-specify-in-PVSS-RunInfo' if OnlineEnv.PartitionName in [ 'LHCb', 'FEST' ] else 'hlt-20100906'
 
+    
+    ### pick up requested DB tags
+    def fwdOnlineEnv(attr,cfg,attr2 = None) :
+        if hasattr(OnlineEnv,attr) and getattr(OnlineEnv,attr) : setattr(cfg,attr2 if attr2 else attr,getattr(OnlineEnv,attr))
+
+    fwdOnlineEnv( 'CondDBTag', moore, 'CondDBtag' )
+    fwdOnlineEnv( 'DDDBTag',   moore, 'DDDBtag' )
+    fwdOnlineEnv( 'RunChangeHandlerConditions', mooreOnline )
+
     #default is OK moore.UseDBSnapshot = True
     #default is OK moore.IgnoreDBHeartBeat = True
     mooreOnline.EnableRunChangeHandler = ( OnlineEnv.HLTType not in ['PA','PassThrough' ] )
     mooreOnline.PartitionName = OnlineEnv.PartitionName
+    
+    ## is this still needed??? YES -- the run_XYZ.xml files are not generated for FEST...
+    if OnlineEnv.PartitionName == 'FEST' : mooreOnline.EnableRunChangeHandler = False
+    ### TODO: pick up which calibrations to 'register' for the run change handler
+
+    if OnlineEnv.PartitionName == 'TEST' : mooreOnline.CheckOdin = False
+
     #not needed any longer? MooreOnline already does this...
     moore.InitialTCK = OnlineEnv.InitialTCK
     mooreOnline.REQ1 = "EvType=1;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
-    from Configurables import MooreExpert, HltConf
+    from Configurables import MooreExpert
     for k,v in kwargs.iteritems() :
         #iterate through the available configurables to set required properties
         found=False
-        for conf in [mooreOnline, moore, MooreExpert(), HltConf()]:
+        for conf in [mooreOnline, moore, MooreExpert()]:
             if k in conf.__slots__ or hasattr(conf,k):
                 setattr(conf,k,v)
                 found=True
@@ -50,13 +66,6 @@ def start(**kwargs) :
         if not found:
             print "# WARNING: skipping setting '"+k+":"+v+"' because no configurable has that option"
     
-    
-    ## TODO: is this still needed???
-    #if OnlineEnv.PartitionName == 'FEST' : Moore().EnableRunChangeHandler = False
-    
-    ### pick up requested DB tags
-    if hasattr(OnlineEnv,'CondDBTag') and OnlineEnv.CondDBTag : moore.CondDBtag = OnlineEnv.CondDBTag
-    if hasattr(OnlineEnv,'DDDBTag')   and OnlineEnv.DDDBTag   : moore.DDDBtag   = OnlineEnv.DDDBTag
 
     # Forward all attributes of 'OnlineEnv' python module to the job options service (using the OnlineEnv configurable)...
     from GaudiKernel.Proxy.Configurable import ConfigurableGeneric
