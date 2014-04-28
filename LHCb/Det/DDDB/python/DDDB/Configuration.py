41cee32e7d34cdd14f3ecb319b3118db8f361303
Marco Cattaneo <marco.cattaneo@cern.ch>
2014-04-28 15:30:20 +0000

    Update default database tags to the latest ones: LHCBCOND, 2012, 2011 : cond-20140425
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@172051 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fd0e89ed0a977d84569fe258b89c8de67a738617

--- LHCb/Det/DDDB/python/DDDB/Configuration.py
+++ LHCb/Det/DDDB/python/DDDB/Configuration.py
@@ -190,7 +190,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"],     "dddb-20130503-1" )
-        self.__set_tag__(["LHCBCOND"], "cond-20131128"   )
+        self.__set_tag__(["LHCBCOND"], "cond-20140425"   )
         self.__set_tag__(["DQFLAGS"],  "dq-20130806-1"   )
         if not self.getProp("Simulation"):
             self.__set_init_time__(datetime.utcfromtimestamp(1355694590)) # End of fill 3453
@@ -201,7 +201,7 @@ class DDDBConf(ConfigurableUser):
         """
         # Set the tags
         self.__set_tag__(["DDDB"], "dddb-20130503")
-        self.__set_tag__(["LHCBCOND"], "cond-20130522")
+        self.__set_tag__(["LHCBCOND"], "cond-20140425")
         self.__set_tag__(["DQFLAGS"], "dq-20130806-1")
         if not self.getProp("Simulation"):
             self.__set_init_time__(datetime.utcfromtimestamp(1319991087)) # End of fill 2267
