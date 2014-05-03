46e9a17611a746b7b32658954abd83cd3f925ccd
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-05-03 20:45:50 +0000

    add file VPConstants.h
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@172318 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fa7d7ae2f74e47f46740f080198623f4305e34ff

---
+++ LHCb/Kernel/LHCbKernel/Kernel/VPConstants.h
@@ -0,0 +1,16 @@
+#ifndef KERNEL_VPCONSTANTS_H
+#define KERNEL_VPCONSTANTS_H 1
+namespace VP {
+
+static const unsigned int NModules = 52;
+static const unsigned int NSensorsPerModule = 4;
+static const unsigned int NSensors = NModules * NSensorsPerModule;
+static const unsigned int NChipsPerSensor = 3;
+static const unsigned int NRows = 256;
+static const unsigned int NColumns = 256;
+static const unsigned int NSensorColumns = NColumns * NChipsPerSensor; 
+static const unsigned int NPixelsPerSensor = NSensorColumns * NRows;
+
+}
+
+#endif
