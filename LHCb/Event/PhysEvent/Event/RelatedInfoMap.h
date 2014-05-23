2534290afd2f9cd8eaf0a871112c389bc4748cc7
Anton Poluektov <anton.poluektov@cern.ch>
2014-05-23 09:05:25 +0000

    Add RelatedInfoMap class
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@173139 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 57741c6ddc98354be19d9ee04cf9a61af2383798

---
+++ LHCb/Event/PhysEvent/Event/RelatedInfoMap.h
@@ -0,0 +1,14 @@
+#ifndef EVENT_RELATEDINFOMAP_H 
+#define EVENT_RELATEDINFOMAP_H 1
+
+// from Gaudi
+#include "GaudiKernel/VectorMap.h"
+
+class RelatedInfoMap : public GaudiUtils::VectorMap<short int, float> {
+  public:
+    typedef GaudiUtils::VectorMap<short,float>             MAP  ;
+    typedef GaudiUtils::VectorMap<short,float>::_vector    VECT ;
+    typedef GaudiUtils::VectorMap<short,float>::value_type VAL  ;
+}; 
+
+#endif // KERNEL_IRELATEDINFOTOOL_H
