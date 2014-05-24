caa4ecfa30799ee5caf78b926e9a650fb2f3d0c9
Anton Poluektov <anton.poluektov@cern.ch>
2014-05-24 15:19:24 +0000

    Add CopyParticle2RelatedInfo
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@173166 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a7d318a18de0fbacd82530e247145c9d64c83281

---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2RelatedInfo.cpp
@@ -0,0 +1,3 @@
+// $Id: CopyParticle2RelatedInfo.cpp,v 1.1 2010-02-24 15:36:23 jpalac Exp $
+// Include files 
+#include "CopyParticle2RelatedInfo.h"
---
+++ Analysis/MicroDST/MicroDSTAlgorithm/src/CopyParticle2RelatedInfo.h
@@ -0,0 +1,65 @@
+// $Id: CopyParticle2RelatedInfo.h,v 1.2 2010-02-24 17:02:09 jpalac Exp $
+#ifndef COPYPARTICLE2RELATEDINFO_H 
+#define COPYPARTICLE2RELATEDINFO_H 1
+
+// Include files
+// from LHCb
+#include <Relations/Relation1D.h>
+// from MicroDST
+#include <MicroDST/ICloneParticle.h>
+#include "MicroDST/RelationsFromParticleLocationsFromClonerAlg.h"
+#include "MicroDST/BindType2ClonerDef.h"
+// from DaVinci
+#include "Event/Particle.h"
+#include "Event/RelatedInfoMap.h"
+
+/** @class CopyParticle2RelatedInfo CopyParticle2RelatedInfo.h
+ *  
+ *  MicroDSTAlgorithm to clone the associations between an LHCb::Particle
+ *  and a RelatedInfo. 
+ *  It inherits the std::string properties InputLocation and OutputPrefix from
+ *  MicroDSTCommon. The associations are taken from the TES location 
+ *  defined by InputLocation, and are cloned and put in TES location 
+ *  "/Event" + OutputPrefix + InputLocation. If InputLocation already contains
+ *  a leading "/Event" it is removed.
+ *
+ *  <b>Important conditions</b>: This algorithm relies on the LHCb::Particle
+ *  set in the associations to have been previously cloned by CopyParticles,
+ *  and the existence of the LHCb::Relation1D<const LHCb::Particle*,int> associating the origin
+ *  LHCb::Particles to BackgroundCategories (ints)
+ *
+ *  <b>Example</b>: Clone LHCb::Particle -> RelatedInfo associations
+ *  from "/Event/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/P2BCRelations"
+ *  to "/Event/MyLocation/Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/Particle2VertexRelations"
+ *  @code
+ *  mySeq = GaudiSequencer('SeqDC06selBd2Jpsi2MuMu_Kst2KPi')
+ *  copyP2BC = CopyParticle2RelatedInfo()
+ *  mySeq.Members += [copyP2BC]
+ *  copyP2BC.InputLocation = "Phys/DC06selBd2Jpsi2MuMu_Kst2KPi/P2BCRelations";
+ *  copyP2BC.OutputPrefix = "MyLocation";
+ *  @endcode
+ *
+ *  @author Juan PALACIOS juan.palacios@nikhef.nl
+ *  @date   2010-02-24
+ */
+//=============================================================================
+namespace Particle2RelatedInfo
+{  
+  typedef LHCb::Relation1D<LHCb::Particle, RelatedInfoMap > Table;
+}
+//=============================================================================
+template<> struct Defaults<Particle2RelatedInfo::Table>
+{
+  const static std::string relationsName;
+};
+const std::string Defaults<Particle2RelatedInfo::Table>::relationsName = "/Particle2CV1Relations";
+//=============================================================================
+template<> struct Location<Particle2RelatedInfo::Table>
+{
+  const static std::string Default;
+};
+const std::string Location<Particle2RelatedInfo::Table>::Default = "NO DEFAULT LOCATION";
+//=============================================================================
+typedef MicroDST::RelationsFromParticleLocationsFromClonerAlg<Particle2RelatedInfo::Table> CopyParticle2RelatedInfo;
+DECLARE_NAMED_ALGORITHM_FACTORY( CopyParticle2RelatedInfo, CopyParticle2RelatedInfo )
+#endif // COPYPARTICLE2RELATEDINFO_H
