a42a3c0e34b0d91725475ca4d9d0b016b4d9aa06
Anton Poluektov <anton.poluektov@cern.ch>
2014-05-24 18:16:29 +0000

    Add IRelatedInfoTool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@173170 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 100c040a6c6a1a73bc1765d15c04814652005868

---
+++ Phys/Phys/DaVinciInterfaces/Kernel/IRelatedInfoTool.h
@@ -0,0 +1,42 @@
+#ifndef KERNEL_IRELATEDINFOTOOL_H 
+#define KERNEL_IRELATEDINFOTOOL_H 1
+
+// Include files
+#include "Event/Particle.h"
+#include "Event/RelatedInfoMap.h"
+
+// from Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+/**
+ *  @class IRelatedInfoTool IRelatedInfoTool.h Kernel/IRelatedInfoTool.h
+ *  
+ *  Abstract Interface for the tool calculating extra particle info 
+ *  to be stored into DST via a Particle to VectorMap relation
+ *  
+ *  @author Anton Poluektov a.o.poluektov@warwick.ac.uk
+ *  @date   2012-02-18
+ */
+
+class GAUDI_API IRelatedInfoTool : virtual public IAlgTool {
+
+public:
+
+  DeclareInterfaceID( IRelatedInfoTool, 1, 0 );
+
+  /// Method to calculate extra information 
+  virtual StatusCode calculateRelatedInfo(const LHCb::Particle* top, 
+                                          const LHCb::Particle* part) = 0;
+
+  /// Get the value of calculated parameters
+  virtual RelatedInfoMap* getInfo(void) = 0;
+  
+  virtual std::string infoPath(void) = 0; 
+
+//protected:
+
+//  virtual ~IRelatedInfoTool();
+
+};
+
+#endif // KERNEL_IRELATEDINFOTOOL_H
