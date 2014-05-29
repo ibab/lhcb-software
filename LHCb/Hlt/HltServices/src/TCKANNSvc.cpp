01cc71a15a04ec0c84b47bb737f39a03acc35c56
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-05-29 20:13:52 +0000

    add caching to TCKANNSvc
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@173292 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fc7a7eebec93c629e0e22c22a2e793ec1f44a1c0

--- LHCb/Hlt/HltServices/src/TCKANNSvc.cpp
+++ LHCb/Hlt/HltServices/src/TCKANNSvc.cpp
@@ -26,6 +26,7 @@ private:
   mutable IPropertyConfigSvc * m_propertyConfigSvc;
   std::string       m_propertyConfigSvcName;
   std::string       m_instanceName;
+  mutable std::map<TCK,const PropertyConfig*> m_cache; // TODO: flush cache if m_instanceName changes
 };
 
 #include "GaudiKernel/SvcFactory.h"
@@ -53,7 +54,6 @@ TCKANNSvc::initialize()
 
   if( msgLevel(MSG::VERBOSE) ) verbose() << "==> Initialize" << endmsg;
 
-  // grab IConfigAccessSvc
   if (!service( m_propertyConfigSvcName, m_propertyConfigSvc).isSuccess()) {
     fatal() << "TCKANNSvc failed to get the IConfigAccessSvc." << endmsg;
     return StatusCode::FAILURE;
@@ -69,41 +69,64 @@ TCKANNSvc::finalize(  )
 }
 
 
-GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const {
-
+GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> 
+TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const 
+{
     TCK _tck(tck); _tck.normalize();
-    // grab properties of child from config database...
-    const ConfigTreeNode* tree = m_propertyConfigSvc->resolveConfigTreeNode( ConfigTreeNodeAlias::alias_type{ std::string("TCK/") +  _tck.str()  } );
-    if (!tree) {
-      //If we could not resolve the (non-zero) TCK we have a problem
-      error() << "Requested TCK " << _tck <<  " could not resolved. Returning an empty map... " << endmsg;
-      return {};
+    auto entry = m_cache.find(_tck);
+    if (entry == std::end(m_cache) ) {
+        // grab properties of child from config database...
+        const ConfigTreeNode* tree = m_propertyConfigSvc->resolveConfigTreeNode( ConfigTreeNodeAlias::alias_type{ std::string("TCK/") +  _tck.str()  } );
+        if (!tree) {
+          //If we could not resolve the (non-zero) TCK we have a problem
+          error() << "Requested TCK " << _tck <<  " could not resolved. Returning an empty map... " << endmsg;
+          return {};
+        }
+        PropertyConfig::digest_type child = m_propertyConfigSvc->findInTree(tree->digest(), m_instanceName);
+        if (child.invalid()) {
+          error() << "Error finding configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+          return {};
+        }
+        const PropertyConfig *config = m_propertyConfigSvc->resolvePropertyConfig(child);
+        if (!config) { 
+          error() << "Error reading configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+          return {};
+        }
+        auto status = m_cache.insert( { _tck, config } );
+        if (!status.second) {
+          error() << "Error updating cache for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+          return {};
+        }
+        entry = status.first;
     }
-    PropertyConfig::digest_type child = m_propertyConfigSvc->findInTree(tree->digest(), m_instanceName);
-    if (child.invalid()) {
-      error() << "Error finding configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
-      return {};
-    }
-    const PropertyConfig *config = m_propertyConfigSvc->resolvePropertyConfig(child);
-    if (!config) { 
-      error() << "Error reading configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
-      return {};
-    }
-    auto prop = std::find_if( std::begin(config->properties()), std::end( config->properties() ), 
+    auto prop = std::find_if( std::begin(entry->second->properties()), std::end( entry->second->properties() ), 
                               [&](const std::pair<std::string,std::string>& p ) { return major.str() == p.first ; } );
-    if (prop == std::end(config->properties())) {
+    if (prop == std::end(entry->second->properties())) {
       error() << "Error finding requested major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
       return {};
     }
     // now use the property parser to do the hard work of converting the string rep into a map...
     std::map<std::string,int> map;
+    // or extend the PropertyConfig interface so it takes a property, invokes 'fromString' on it, and returns StatusCode ?
+    // or an templated interface, which wraps a PropertyWithValue around it, then invokes the above, and drops the PropertyWithValue?
+    // boost::optional< map<string,int> > = config->assign< map<string,int> >( major );
+    // PropertyWithValue<std::map<std::string,int>>{  major.str(), &map, false }.fromString( prop->second );
+    // auto status = prp.fromString( prop->second );
     auto status = Gaudi::Parsers::parse(map , prop->second);
     if (status.isFailure()) {
       error() << "Error interpreting requested major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
       return {};
     }
     GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> result;
-    for ( const auto& item : map ) result.insert( item.second, item.first ); 
+    bool ok = true;
+    for ( const auto& item : map )  {
+        ok = ok && result.insert( item.second, item.first ).second;
+    }
+    if (!ok) {
+      error() << "Duplicate entries in map for  major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+      return {};
+
+    }
     return result;
 }
 
