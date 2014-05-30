39127ed2f8f61fe1c617cd22cc1262477ede2473
Ben Couturier <lben@cern.ch>
2014-05-30 13:17:34 +0000

    LBCORE-502 Release of LbLegacy v6r0
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@173334 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 42b1534045306823e01edbe224f245aa05dba1d5

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -21,10 +21,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from tempfile import mkdtemp
 
-script_version = '140324'
+script_version = '140530'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v7r10p4"
+lbscripts_version = "v8r0"
 compat_version = None
 line_size = 120
 #-----------------------------------------------------------------------------------
