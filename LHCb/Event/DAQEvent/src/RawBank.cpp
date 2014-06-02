a34d603c1e900142cc4d49bc4632df1c4c12538f
Marco Cattaneo <marco.cattaneo@cern.ch>
2014-06-02 12:12:21 +0000

    Remove definition of (obsolete) VPDigit RawBank
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@173392 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 48eae00377ebcc0f4ab406659db41b3e20bde9f3

--- LHCb/Event/DAQEvent/src/RawBank.cpp
+++ LHCb/Event/DAQEvent/src/RawBank.cpp
@@ -86,8 +86,8 @@ std::string LHCb::RawBank::typeName(LHCb::RawBank::BankType e){
   case LHCb::RawBank::UTFull          : return  "UTFull";
   case LHCb::RawBank::UTError         : return  "UTError";
   case LHCb::RawBank::UTPedestal      : return  "UTPedestal";
-  case LHCb::RawBank::VPDigit	      : return  "VPDigit";
-  case LHCb::RawBank::HltTrackReports   : return  "HltTrackReports";    
+    //  case LHCb::RawBank::VPDigit	      : return  "VPDigit";
+  case LHCb::RawBank::HltTrackReports : return  "HltTrackReports";    
   default                             : return  "Undefined_name";
   };
 }  
