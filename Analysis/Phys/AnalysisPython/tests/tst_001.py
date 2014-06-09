a1883da13ed6824be1a76ca0abc67d6b29b8d0fb
Vanya Belyaev <ivan.belyaev@cern.ch>
2014-06-09 06:28:28 +0000

    fix qmtest
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@173556 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 994e6942bbc7eaeb849a2c559542a8458310cb35

--- Analysis/Phys/AnalysisPython/tests/tst_001.py
+++ Analysis/Phys/AnalysisPython/tests/tst_001.py
@@ -6,11 +6,12 @@ from   LoKiCore.basic import cpp
 mass = ROOT.RooRealVar('mass','mass',0 ,1)
 tau  = ROOT.RooRealVar('tau' ,'tau' ,-1,1)
 phi  = ROOT.RooRealVar('phi' ,'phi' ,-4,4)
-
+phis = ROOT.RooArgList( phi ) 
 # ===================================================================================================================
 ## THIS FAILS FOR ROOT6
-# =================================================================================================================== 
-pdf = cpp.Analysis.Models.ExpoPositive ( 'Model' ,'model' , mass , tau , phi , mass.getMin() , mass.getMax() )
+# ===================================================================================================================
+
+pdf = cpp.Analysis.Models.ExpoPositive ( 'Model' ,'model' , mass , tau , phis , mass.getMin() , mass.getMax() )
 
 print 'PDF:', pdf
 
