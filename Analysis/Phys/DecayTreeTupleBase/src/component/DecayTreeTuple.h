ab547bb7c088c728ea9e7c82e0e016b3910a3826
Patrick Koppenburg <patrick.koppenburg@cern.ch>
2014-06-10 10:30:50 +0000

    fix doc
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@173662 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d24b74a456d57a58d546ced29eee1dd6edf1a7b3

--- Analysis/Phys/DecayTreeTupleBase/src/component/DecayTreeTuple.h
+++ Analysis/Phys/DecayTreeTupleBase/src/component/DecayTreeTuple.h
@@ -15,28 +15,10 @@
  * 
  * DecayTreeTuple has a decay descriptor (given by the Decay
  * property). One line of the Tuple if filled for each reconstructed
- * candidate. This algorithm is NOT backward compatible with
- * DecayChainNTuple. Yet the base syntax is quite close.
+ * candidate. 
  *
- * Here is a minimalist but working example:
- * \verbatim
- from Configurables import DecayTreeTuple, PhysDesktop, GaudiSequencer
- MyDecayTreeTuple = DecayTreeTuple("MyDecayTreeTuple")
- GaudiSequencer("MySeq").Members =+ [ MyDecayTreeTuple]
- MyDecayTreeTuple.addTool(PhysDesktop())
- MyDecayTreeTuple.PhysDesktop.InputLocations = ["SomeSelection"]
- MyDecayTreeTuple.Decay = "B0 -> ^pi- ^pi+"
- MyDecayTreeTuple.TupleName = "MyTuple"\endverbatim
- *
- * Do not forget to give the context if you want to use Hlt2 particles:
- *\verbatim
-MyDecayTreeTuple.PhysDesktop.InputLocations = ["Hlt2SelB2HH"]
-MyDecayTreeTuple.Context = "HLT" \endverbatim       
- *
- * \note Any particle to be stored in the Tuple has to be flagged with
- * '^' (an exception here is the decay head which cannot be flagged as
- * DecayFinder will refuse it. Top level particle are therefore always
- * stored).
+ * For examples on how to use it, refer to Tutorial 8. 
+ * See https://twiki.cern.ch/twiki/bin/view/LHCb/DaVinciTutorial
  * 
  * \section principle How it works:
  *
@@ -49,17 +31,6 @@ MyDecayTreeTuple.Context = "HLT" \endverbatim
  *
  * - Once for the candidate for the IEventTupleTool instances
  *
- * The instantiated tool list is defined by the \b ToolList property,
- * a vector of strings. Hence any tool can be added and manipulated as
- * usual, doing things like
- *
- * \verbatim
- MyDecayTreeTuple.ToolList = [ "TupleToolMCTruth", TupleToolTrigger" ]
- MyDecayTreeTuple.ToolList += [ "TupleToolPid/genericPID" ]
- from Configurables import TupleTooPid
- MyDecayTreeTuple.addTool(TupleTooPid("genericPID"))
- MyDecayTreeTuple.genericPID.SomeProperty = True\endverbatim
- *
  * \subsection branches Fine tuning of the branches:
  *
  * The \b Branches property, is a map<string,string>. Each of its
@@ -71,35 +42,6 @@ MyDecayTreeTuple.Context = "HLT" \endverbatim
  * associated decay AND the main decay. Specific tools have their own
  * properties, whether or not the tool already exists in the generic
  * tool list.
- *
- * Specific tools are instance of TupleToolDecay, they have a few properties:
- *
- * - ToolList, a vector of string, defining the tool list obviously.
- *
- * - InheritTools, whether or not generic tools should also be applied
- *   to this sub-decay.
- *
- * Here is an example with Bs->Ds h, h as K or pi, Ds => K K pi (a
- * good occasion to remind you the DecayFinder syntax):
- *
- * \verbatim
- Dtt = DecayTreeTuple("Dtt")
- GaudiSequencer("MySeq").Members = [ Dtt ]
- Dtt.Decay = "[[B_s0]cc -> (^D_s- => ^K+ ^K- ^pi-) {^pi+, ^K+}]cc"
- Dtt.Branches = {
-    "bachelor" : " [[B_s0]cc -> D_s- {^pi+, ^K+}]cc "
-    ,"Ds" : " [ [B_s0]cc -> ^D_s- {pi+,K+} ]cc "
-    ,"head" : " [B_s0]cc : [[B_s0]cc -> (D_s- => K+ K- pi- ) pi+ ]cc"
- }
-
- Dtt.head.ToolList = [ "TupleToolPropertime" ]
- Dtt.head.InheritTools = False
- from Configurables import TupleToolMCTruth, TupleToolDecay
- Dtt.addTool(TupleToolDecay("bachelor"))
- Dtt.bachelor.addTool(TupleToolMCTruth())
- Dtt.bachelor.ToolList = [ "TupleToolMCTruth" ]
-
- Dtt.bachelor.TupleToolMCTruch.UseChi2Method = True\endverbatim
  *
  * \note Notice the way the decay head is matched as it is a bit unusual.
  *
