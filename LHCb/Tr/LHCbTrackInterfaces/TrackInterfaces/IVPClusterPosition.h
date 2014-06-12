05dc9888d7b70597a42e585d25756bf814d6f0b0
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-06-12 07:17:51 +0000

    Adapt IVPClusterInterface to modified PixelPositionInfo format (r173748).
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@173749 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 66dd5ab19af56df27e9952de6c287fd6f1c8a684

--- LHCb/Tr/LHCbTrackInterfaces/TrackInterfaces/IVPClusterPosition.h
+++ LHCb/Tr/LHCbTrackInterfaces/TrackInterfaces/IVPClusterPosition.h
@@ -1,61 +1,50 @@
-#ifndef KERNEL_IVPCLUSTERPOSITION_H 
+#ifndef KERNEL_IVPCLUSTERPOSITION_H
 #define KERNEL_IVPCLUSTERPOSITION_H 1
 
 // Gaudi
 #include "GaudiKernel/IAlgTool.h"
-// LHCb
+// Kernel/LHCbKernel
 #include "Kernel/PixelPositionInfo.h"
 
 static const InterfaceID IID_IVPClusterPosition("IVPClusterPosition", 1, 0);
 
-/** @class IVPClusterPosition IVPClusterPosition.h TrackInterfaces/IVPClusterPosition.h
- *  
+/** @class IVPClusterPosition IVPClusterPosition.h
+ *TrackInterfaces/IVPClusterPosition.h
+ *
  *
  *  @author Victor Coco
  *  @date   2010-02-02
  */
 
 namespace LHCb {
-  class VPCluster;
-  class VPChannelID;
-  class StateVector;
+class VPCluster;
 }
 
 class IVPClusterPosition : virtual public IAlgTool {
-public:
-
-  // a new tool interface, for VP, can be used in other pixel detector
-  // (defined in Kernel/PixelPositionInfo.h)
-  typedef LHCb::PixelPositionInfo<LHCb::VPChannelID> toolInfo;
-  typedef std::pair<double, double> Direction;
-  
+ public:
   // Return the interface ID
-  static const InterfaceID& interfaceID() {return IID_IVPClusterPosition;}
+  static const InterfaceID& interfaceID() { return IID_IVPClusterPosition; }
 
- /** Calculate position of a given VPCluster
-  * @param cluster pointer to a VPCluster
-  * @return toolInfo (templated struct) 
-  * <br> pixel = channel ID of the cluster
-  * <br> fractionalPosition = inter-pixel fraction (within range 0-1)
-  * <br> error = estimate of the error (in fraction of pixel pitch)
-  * The returned error estimate depends only on pixel size
-  */
-  virtual toolInfo position(const LHCb::VPCluster* cluster) const = 0;
+  /** Calculate position of a given VPCluster
+   * @param cluster pointer to a VPCluster
+   * @return struct containing coordinates and errors
+   * The returned error estimate depends only on the pixel size.
+   */
+  virtual LHCb::VPPositionInfo position(const LHCb::VPCluster* cluster)
+      const = 0;
 
- /** Calculate position of a given VPCluster
-  * @param cluster pointer to a VPCluster
-  * @param point const reference to XYZ coordinates of track intercept 
-  * @param direction const reference to pair of doubles (track xz and yz slope)
-  * @return toolInfo (templated struct) 
-  * <br> pixel = channel ID of the cluster
-  * <br> fractionalPosition = inter-pixel fraction (within range 0-1)
-  * <br> error = estimate of the error (in fraction of pixel pitch)
-  * The returned error estimate depends both on pixel size and 
-  * projected angle of a track
-  */
-  virtual toolInfo position(const LHCb::VPCluster* cluster,
-                            const Gaudi::XYZPoint& point,
-                            const Direction& direction) const = 0;
-
+  /** Calculate position of a given VPCluster
+   * @param cluster pointer to a VPCluster
+   * @param point const reference to XYZ coordinates of track intercept
+   * @param tx track xz slope
+   * @param ty track yz slope
+   * @return struct containing coordinates and errors
+   * The returned error estimate depends both on the pixel size and
+   * the projected angle of a track.
+   */
+  virtual LHCb::VPPositionInfo position(const LHCb::VPCluster* cluster,
+                                        const Gaudi::XYZPoint& point,
+                                        const double& tx,
+                                        const double& ty) const = 0;
 };
-#endif // KERNEL_IVPCLUSTERPOSITION_H 
+#endif  // KERNEL_IVPCLUSTERPOSITION_H
