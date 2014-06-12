c2b9ea06996e3de1f54de6c06c626ab331a0775f
Markus Frank <frankb@cern.ch>
2014-06-12 09:20:29 +0000

    v5r9
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@173758 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: aff06a64874ef35254f025ad5470c450a451c6f8

--- Online/OnlineSys/cmt/release_utils.sh
+++ Online/OnlineSys/cmt/release_utils.sh
@@ -41,3 +41,42 @@ make_tag()
     tag_package Online/$1 $2;
     cd ${ONLINESYSROOT}/cmt;
 }
+
+remove_tag()
+{
+    if test "$1" != "";
+    then
+	echo "+++ Package name = $1";
+    else
+	echo "+++ The first argument to make_tag must be the package name";
+    fi;
+    if test "$2" != "";
+    then
+	echo "+++ Package version = $2";
+    else
+	echo "+++ The second argument to make_tag must be the package version";
+    fi;
+#
+#
+#
+    if test "$ONLINESYSROOT" = "";
+    then
+	eval `cmt run printenv|grep ONLINESYSROOT`;
+    fi;
+#
+#
+#
+    while true; do
+	read -p "+++ Is this correct? Y/N Cr=[Y]" yn;
+	case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) return 0;;
+            * ) break;;
+	esac;
+    done;
+#
+#
+#
+    echo "+++ Removing tag from package: Online/$1 $2 ";
+    svn rm -m "Need to remove tag Online/$1 version $2" svn+ssh://svn.cern.ch/reps/lhcb/Online/tags/Online/${1}/${2};
+}
