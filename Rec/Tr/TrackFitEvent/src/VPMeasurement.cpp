28658de403bac917be1a918082bc4160a66cc89c
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-06-12 07:28:32 +0000

    VPMeasurement: modify constructor, passing PixelPositionInfo directly instead of cluster position tool
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@173751 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 61cf7d7fa326989c97c77977f2bdc6639b094dc3

--- Rec/Tr/TrackFitEvent/src/VPMeasurement.cpp
+++ Rec/Tr/TrackFitEvent/src/VPMeasurement.cpp
@@ -1,71 +1,45 @@
 #include "VPDet/DeVP.h"
 #include "Kernel/LineTraj.h"
-#include "LHCbMath/LHCbMath.h"
 
 #include "Event/VPMeasurement.h"
 
 using namespace LHCb;
 
-//-----------------------------------------------------------------------------
-// Implementation file for class : VPMeasurement
-//
-//-----------------------------------------------------------------------------
-
-/// Constructor from VPCluster
-VPMeasurement::VPMeasurement(const DeVP& det,const VPCluster& cluster,
-                             const IVPClusterPosition& clusPosTool, 
+//=============================================================================
+// Constructor
+//=============================================================================
+VPMeasurement::VPMeasurement(const VPCluster& cluster,
+                             const VPPositionInfo& info, 
                              VPMeasurement::VPMeasurementType& xy) :
     Measurement(Measurement::VP, cluster.channelID(), 0),
     m_projection(xy),
     m_cluster(&cluster) {
-  IVPClusterPosition::toolInfo info = clusPosTool.position(this->cluster()); 
-  init(det, info);
   
-}
-
-/// Constructor from VPCluster and known track state
-VPMeasurement::VPMeasurement(const DeVP& det,const VPCluster& cluster,
-                             const IVPClusterPosition& clusPosTool, 
-                             VPMeasurement::VPMeasurementType& xy,
-                             const LHCb::StateVector& ref) :
-    Measurement(Measurement::VP, cluster.channelID(), 0),
-    m_projection(xy),
-    m_cluster(&cluster) {
-
-  IVPClusterPosition::toolInfo info = 
-    clusPosTool.position(this->cluster(), ref.position(),
-                         std::pair<double, double>(ref.tx(), ref.ty())); 
-  init(det, info);  
-
-}
-
-void VPMeasurement::init(const DeVP& det, const IVPClusterPosition::toolInfo& info) {
-
-  const DeVPSensor* sensor = det.sensorOfChannel(info.pixel); 
-  m_position = sensor->channelToPoint(info.pixel, info.fractionalPosition);
-  std::pair<double, double> pixelSize = sensor->pixelSize(info.pixel);
-  double dx = pixelSize.first * info.fractionalError.first ;
-  double dy = pixelSize.second * info.fractionalError.second;
-  setZ(m_position.z());
+  Gaudi::XYZPoint position(info.x, info.y, m_cluster->z());
+  setZ(m_cluster->z());
   if (m_projection == X) {
-    setMeasure(m_position.x());
-    setErrMeasure(dx);
+    setMeasure(info.x);
+    setErrMeasure(info.dx);
     Gaudi::XYZVector yDir(0, 1, 0); 
-    std::pair<double, double> yRange(-dy, dy);
-    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(m_position, yDir, yRange, true));
+    std::pair<double, double> yRange(-info.dy, info.dy);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, yDir, yRange, true));
   } else {
-    setMeasure(m_position.y());
-    setErrMeasure(dy);
+    setMeasure(info.y);
+    setErrMeasure(info.dy);
     Gaudi::XYZVector xDir(1, 0, 0); 
-    std::pair<double, double> xRange(-dx, dx);
-    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(m_position, xDir, xRange, true));  
+    std::pair<double, double> xRange(-info.dx, info.dx);
+    m_trajectory = std::auto_ptr<Trajectory>(new LineTraj(position, xDir, xRange, true));  
   }
 
 }
 
-/// Copy constructor
+//=============================================================================
+// Copy constructor
+//=============================================================================
 VPMeasurement::VPMeasurement(const VPMeasurement& other) : 
-    Measurement(other), m_projection(other.m_projection), m_cluster(other.m_cluster) {
+    Measurement(other), 
+    m_projection(other.m_projection), 
+    m_cluster(other.m_cluster) {
 
 }
 
