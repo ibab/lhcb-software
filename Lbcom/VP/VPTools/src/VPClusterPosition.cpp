8c40dc349ae2efe800e89433910c6ce77240964b
Heinrich Schindler <heinrich.schindler@cern.ch>
2014-06-12 07:19:24 +0000

    Simplified error parameterisation based on cluster size.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@173750 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 487c7c33d7e68f3884012c36daca8b9b99cbf1d3

--- Lbcom/VP/VPTools/src/VPClusterPosition.cpp
+++ Lbcom/VP/VPTools/src/VPClusterPosition.cpp
@@ -1,13 +1,9 @@
-#include <vector>
-
 // Gaudi
 #include "GaudiKernel/ToolFactory.h"
 #include "GaudiKernel/SystemOfUnits.h"
 #include "GaudiKernel/Point3DTypes.h"
 
 // LHCb
-// Kernel/LHCbMath
-#include "LHCbMath/LHCbMath.h"
 // Det/VPDet
 #include "VPDet/DeVP.h"
 // Event/DigiEvent
@@ -16,159 +12,110 @@
 // Local
 #include "VPClusterPosition.h"
 
-//-----------------------------------------------------------------------------
-// Implementation file for class : VPClusterPosition
-//
-// 2010-02-02 : Victor Coco
-// 
-//-----------------------------------------------------------------------------
-
 DECLARE_TOOL_FACTORY(VPClusterPosition)
 
-typedef IVPClusterPosition::toolInfo toolInfo;
-typedef IVPClusterPosition::Direction Direction;
-typedef std::pair<double, double> Pair;
-
 //=============================================================================
-/// Standard constructor, initializes variables
+// Standard constructor, initializes variables
 //=============================================================================
 VPClusterPosition::VPClusterPosition(const std::string& type,
                                      const std::string& name,
-                                     const IInterface* parent) :
-    GaudiTool(type, name, parent),
-    m_det(NULL) {
+                                     const IInterface* parent)
+    : GaudiTool(type, name, parent), m_det(NULL) {
 
   declareInterface<IVPClusterPosition>(this);
-  m_p.reserve(7);
+
+  declareProperty("ErrorSinglePixel", 
+                  m_errorSinglePixel = 12.5 * Gaudi::Units::micrometer); 
+  declareProperty("ErrorTwoPixel", 
+                  m_errorTwoPixel = 8. * Gaudi::Units::micrometer);
+  declareProperty("ErrorAverage", 
+                  m_errorAverage = 12. * Gaudi::Units::micrometer);
 
 }
 
 //=============================================================================
-/// Destructor
+// Destructor
 //=============================================================================
-VPClusterPosition::~VPClusterPosition() {} 
+VPClusterPosition::~VPClusterPosition() {}
 
 //=============================================================================
-/// Initialisation 
+// Initialisation
 //=============================================================================
 StatusCode VPClusterPosition::initialize() {
 
   StatusCode sc = GaudiTool::initialize();
   if (sc.isFailure()) return sc;
   m_det = getDet<DeVP>(DeVPLocation::Default);
-  // Set parameters for polynomial fit of resolution as function of angle.
-  m_p.resize(7);
-  m_p[0] = 11.0187;
-  m_p[1] = -0.0487707;
-  m_p[2] = -0.0930352;
-  m_p[3] = 0.000938413;
-  m_p[4] = 0.000709917;
-  m_p[5] = -3.85402e-05;
-  m_p[6] = 5.72957e-07; 
-
   return StatusCode::SUCCESS;
-
 }
 
 //=========================================================================
-/// Position given cluster only
+// Position and error given cluster only
 //=========================================================================
-toolInfo VPClusterPosition::position(const LHCb::VPCluster* cluster) const {
+LHCb::VPPositionInfo VPClusterPosition::position(const LHCb::VPCluster* cluster)
+    const {
 
-  toolInfo info;
-  const DeVPSensor* sensor = m_det->sensorOfChannel(cluster->channelID());
-  Gaudi::XYZPoint point = sensor->channelToPoint(cluster->channelID(), 
-                                                 cluster->fraction());
-  Direction direction;
-  direction.first = point.x() / point.z();
-  direction.second = point.y() / point.z();
-
-  return position(cluster->channelID(), cluster->fraction(), point, direction);
-
+  Gaudi::XYZPoint point(cluster->x(), cluster->y(), cluster->z());
+  // Assume that the track originates from the nominal interaction point.
+  const double tx = point.x() / point.z();
+  const double ty = point.y() / point.z();
+  return position(cluster, point, tx, ty);
 }
 
 //=========================================================================
-/// Position given cluster, point and direction
+// Position and error given cluster, point and direction
 //=========================================================================
-toolInfo VPClusterPosition::position(const LHCb::VPCluster* cluster,
-                                     const Gaudi::XYZPoint& point,
-                                     const Direction& direction) const {
+LHCb::VPPositionInfo VPClusterPosition::position(const LHCb::VPCluster* cluster,
+                                                 const Gaudi::XYZPoint& /*p*/,
+                                                 const double& /*tx*/,
+                                                 const double& /*ty*/) const {
 
-  return position(cluster->channelID(), cluster->fraction(), point, direction);
-
-}
-
-//=========================================================================
-/// Calculate position and error
-//=========================================================================
-toolInfo VPClusterPosition::position(const LHCb::VPChannelID& channel,
-                                     const Pair& fraction,
-                                     const Gaudi::XYZPoint& point,
-                                     const Direction& direction) const {
-
-  toolInfo info;
+  // TODO: include track information, parameterise error as function of angle.
+  LHCb::VPPositionInfo info;
+  // Get the position directly from the cluster.
+  info.x = cluster->x();
+  info.y = cluster->y();
+  // Get the sensor.
+  const LHCb::VPChannelID channel = cluster->channelID();
   const DeVPSensor* sensor = m_det->sensorOfChannel(channel);
   if (!sensor) {
     Error("No valid pointer to sensor");
-    info.pixel = LHCb::VPChannelID(0);
-    info.fractionalPosition = std::make_pair(0., 0.);
-    info.fractionalError = std::make_pair(0., 0.);
+    info.dx = 0.;
+    info.dy = 0.;
     return info;
   }
 
-  info.pixel = channel;
-  info.fractionalPosition = fraction;
-  Gaudi::XYZVector track(direction.first, direction.second, 1.);
-  // Get the projected angles between track direction and sensor
-  Pair angles = projectedAngles(sensor, track, point);
-  Pair pixelSize = sensor->pixelSize(channel);
-  // Error estimate
-  info.fractionalError = errorEstimate(angles, pixelSize);
+  // Initialise the local error estimate.
+  double dx = m_errorAverage;
+  double dy = m_errorAverage;
+  // Get the inter-pixel fraction.
+  const double fx = cluster->fraction().first;
+  const double fy = cluster->fraction().second;
+  // Get the cluster size.
+  const unsigned int nPixels = cluster->pixels().size();
+  if (nPixels == 1) {
+    // Single-pixel clusters
+    dx = m_errorSinglePixel;
+    dy = m_errorSinglePixel;
+    if (sensor->isLong(channel)) dx *= 2.;
+  } else if (nPixels == 2) {
+    // Two-pixel clusters
+    if (fx < 0.1) {
+      dx = m_errorSinglePixel;
+      if (sensor->isLong(channel)) dx *= 2.;
+    } else {
+      dx = m_errorTwoPixel;
+      dx *= sensor->xPitch()[channel.scol()] / sensor->xPitch()[0];
+    }
+    dy = fy < 0.1 ? m_errorSinglePixel : m_errorTwoPixel;
+  } else if (nPixels >= 9) {
+    dx = 20. * Gaudi::Units::micrometer;
+    dy = 20. * Gaudi::Units::micrometer;
+  }
+  // Transform the error estimate to the global frame.
+  Gaudi::XYZVector err = sensor->geometry()->toGlobal(Gaudi::XYZVector(dx, dy, 0.));
+  info.dx = err.x();
+  info.dy = err.y();
   return info;
-
-}
-
-//============================================================================
-/// Calculate the projected angles (in rad)
-//============================================================================
-Pair VPClusterPosition::projectedAngles(const DeVPSensor* sensor, 
-                                        Gaudi::XYZVector track, 
-                                        Gaudi::XYZPoint globalPoint) const {
-
-  // Transform global point to the local reference frame.
-  Gaudi::XYZPoint localPoint = sensor->globalToLocal(globalPoint);
-  Gaudi::XYZPoint vGlobal(globalPoint.x() - track.x(),
-                          globalPoint.y() - track.y(),
-                          globalPoint.z() - track.z());
-  Gaudi::XYZPoint vLocal = sensor->globalToLocal(vGlobal);
-  const double dx = localPoint.x() - vLocal.x();
-  const double dy = localPoint.y() - vLocal.y();
-  const double dz = localPoint.z() - vLocal.z();
-  return std::make_pair(atan(dx / dz), atan(dy / dz));
-
-}
-
-//=========================================================================
-/// Projected angles can be the angles between track and sensor
-//=========================================================================
-Pair VPClusterPosition::errorEstimate(const Pair angles, const Pair pixelSize) const {
-
-  // Convert angles to degrees.
-  double thetax = fabs(angles.first / Gaudi::Units::degree);
-  double thetay = fabs(angles.second / Gaudi::Units::degree);
-  if (thetax > 25.) thetax = 25.;
-  if (thetay > 25.) thetay = 25.;
-  // Calculate resolution in micron (based on fit to testbeam data).
-  // TODO: to be improved!
-  double errx = m_p[0] + thetax * m_p[1] + thetax * thetax * m_p[2] +
-                pow(thetax, 3) * m_p[3] + pow(thetax, 4) * m_p[4] +
-                pow(thetax, 5) * m_p[5] + pow(thetax, 6) * m_p[6];
-  double erry = m_p[0] + thetay * m_p[1] + thetay * thetay * m_p[2] +
-                pow(thetay, 3) * m_p[3] + pow(thetay, 4) * m_p[4] +
-                pow(thetay, 5) * m_p[5] + pow(thetay, 6) * m_p[6];
-  errx *= 1.e-3 / pixelSize.first;
-  erry *= 1.e-3 / pixelSize.second;
-  return std::make_pair(errx, erry);
-
 }
 
--- Lbcom/VP/VPTools/src/VPClusterPosition.h
+++ Lbcom/VP/VPTools/src/VPClusterPosition.h
@@ -1,65 +1,48 @@
-#ifndef VPCLUSTERPOSITION_H 
+#ifndef VPCLUSTERPOSITION_H
 #define VPCLUSTERPOSITION_H 1
 
 // Gaudi
 #include "GaudiAlg/GaudiTool.h"
-#include "GaudiMath/GaudiMath.h"
 
 // LHCb
-#include "Kernel/VPChannelID.h"
-#include "Kernel/PixelPositionInfo.h"
-#include "Event/StateVector.h"
 #include "TrackInterfaces/IVPClusterPosition.h"
 
 /** @class VPClusterPosition VPClusterPosition.h
- *  
- *  @author Victor Coco (based on  Tomasz Szumlak VeloClusterPosition)
+ *
+ *  @author Victor Coco
  *  @date   2010-02-02
  */
 
 class DeVP;
-class DeVPSensor;
 
-class VPClusterPosition: public GaudiTool, virtual public IVPClusterPosition {
+class VPClusterPosition : public GaudiTool, virtual public IVPClusterPosition {
 
-public:
+ public:
   /// Constructor
-  VPClusterPosition(const std::string& type, 
-                    const std::string& name,
+  VPClusterPosition(const std::string& type, const std::string& name,
                     const IInterface* parent);
   /// Destructor
   virtual ~VPClusterPosition();
 
-  // Throughout the code LA stands for Linear Approximation
-  // typedefs for object returned by tool
-  typedef IVPClusterPosition::toolInfo toolInfo;
-  typedef IVPClusterPosition::Direction Direction;
-  typedef std::pair<double, double> Pair;
-
   virtual StatusCode initialize();
 
-  virtual toolInfo position(const LHCb::VPCluster* cluster) const;
-  virtual toolInfo position(const LHCb::VPCluster* cluster,
-                            const Gaudi::XYZPoint& point,
-                            const Direction& direction) const;
+  virtual LHCb::VPPositionInfo position(const LHCb::VPCluster* cluster) const;
+  virtual LHCb::VPPositionInfo position(const LHCb::VPCluster* cluster,
+                                        const Gaudi::XYZPoint& point,
+                                        const double& tx,
+                                        const double& ty) const;
 
-private:
-
-  toolInfo position(const LHCb::VPChannelID& channel, const Pair& frac,
-                    const Gaudi::XYZPoint& point,
-                    const Direction& direction) const;
-
-  // Error estimate when full state is available
-  Pair errorEstimate(const Pair projAngle, const Pair pixelSize) const;
-  // Calculate the projected angles
-  Pair projectedAngles(const DeVPSensor* sensor, Gaudi::XYZVector track,
-                       Gaudi::XYZPoint globalPoint) const;
-  
-  /// Detector element
+ private:
+  /// Pointer to detector element
   DeVP* m_det;
-  /// Parameters for polynomial fit of resolution as function of proj. angle
-  std::vector<double> m_p; 
-    
+
+  /// Average error for single-pixel clusters
+  double m_errorSinglePixel;
+  /// Average error for two-pixel clusters
+  double m_errorTwoPixel;
+  /// Average error (all cluster sizes)
+  double m_errorAverage;
+
 };
 
 #endif
