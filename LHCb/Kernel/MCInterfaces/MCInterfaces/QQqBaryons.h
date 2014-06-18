43437d2ee35dba92d446014bfebf5313bfac4d81
Gloria Corti <gloria.corti@cern.ch>
2014-06-18 16:23:30 +0000

    Move header to MCInterfaces since it is needed by GenCuts
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@174040 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fa6de629c0a5d53efa262446130a5b4d8d3980b1

---
+++ LHCb/Kernel/MCInterfaces/MCInterfaces/QQqBaryons.h
@@ -0,0 +1,35 @@
+// $Id: $
+#ifndef LBGENXICC_QQQBARYONS_H 
+#define LBGENXICC_QQQBARYONS_H 1
+
+#include <string>
+#include <map>
+// Include files
+
+/** @class QQqBaryons QQqBaryons.h LbGenXicc/QQqBaryons.h
+ *  
+ *
+ *  @author Giacomo GRAZIANI
+ *  @date   2012-02-16
+ */
+class QQqBaryons {
+public:
+  QQqBaryons() {initQQqBaryons();}
+  virtual ~QQqBaryons() {};
+protected:
+  enum QQqState { Undef=0,Xiccpp, Xiccp, Xibcp, Xibc0, Xibb0, Xibbm, Omegaccp, Omegabc0, Omegabbm};
+  std::map<std::string, QQqState> m_mapBaryon;
+  std::map<std::string, int> m_mapBaryonPID;
+  void initQQqBaryons() {
+    m_mapBaryon["Xi_cc++"]=Xiccpp;           m_mapBaryonPID["Xi_cc++"]=4422;   
+    m_mapBaryon["Xi_cc+"]=Xiccp;             m_mapBaryonPID["Xi_cc+"] =4412; 
+    m_mapBaryon["Xi_bc+"]=Xibcp;             m_mapBaryonPID["Xi_bc+"] =5242;
+    m_mapBaryon["Xi_bc0"]=Xibc0;             m_mapBaryonPID["Xi_bc0"] =5142;
+    m_mapBaryon["Xi_bb0"]=Xibb0;             m_mapBaryonPID["Xi_bb0"] =5522;
+    m_mapBaryon["Xi_bb-"]=Xibbm;             m_mapBaryonPID["Xi_bb-"] =5512;
+    m_mapBaryon["Omega_cc+"]=Omegaccp;       m_mapBaryonPID["Omega_cc+"]=4432;
+    m_mapBaryon["Omega_bc0"]=Omegabc0;       m_mapBaryonPID["Omega_bc0"]=5342;
+    m_mapBaryon["Omega_bb-"]=Omegabbm;       m_mapBaryonPID["Omega_bb-"]=5532;
+  }
+};
+#endif // LBGENXICC_QQQBARYONS_H
