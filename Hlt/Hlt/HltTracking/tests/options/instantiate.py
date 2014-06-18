63d5961851ce6f7377b1325f3419c75d994c4e60
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-06-18 12:15:58 +0000

    remove use of importOptions($STDOPTS/DecodeRawEvent.py)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@174027 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 005a4501c256af4ed021bcada368a5a645184af6

--- Hlt/Hlt/HltTracking/tests/options/instantiate.py
+++ Hlt/Hlt/HltTracking/tests/options/instantiate.py
@@ -17,8 +17,6 @@ for name,conf in cfgDb.iteritems():
         print >> sys.stderr, 'ERROR, cannot import/instantiate configurable', name, '\n-------\n', e.__class__, '\n-------'
         traceback.print_exc()
 
-importOptions("$STDOPTS/DecodeRawEvent.py")
-
 # =========== Auto-generated, import all python modules ====
 import glob, os, sys, traceback
 modules=glob.glob('../../python/HltTracking/*.py')
