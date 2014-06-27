569361295f85c167b92b1d934f1c46eb7da5be92
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-06-27 13:57:23 +0000

    Add a new TupleTool that fills the ANNPID variables for a given list    of MVA tunes.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@174302 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ab83d942f6651ef6e98eb9f9498a1de254527a7e

---
+++ Analysis/Phys/DecayTreeTupleANNPID/src/TupleToolANNPID.cpp
@@ -0,0 +1,71 @@
+// $Id: $
+
+#include "TupleToolANNPID.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : TupleToolANNPID
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Standard constructor, initializes variables
+//=============================================================================
+TupleToolANNPID::TupleToolANNPID( const std::string& type,
+                                  const std::string& name,
+                                  const IInterface* parent )
+  : TupleToolBase ( type, name, parent ),
+    m_pidTool     ( NULL )
+{
+  declareInterface<IParticleTupleTool>(this);
+  declareProperty( "ANNPIDTunes", m_pidTunes = {"MC12TuneV2","MC12TuneV3"} );
+}
+
+//=============================================================================
+
+StatusCode TupleToolANNPID::initialize()
+{
+  const StatusCode sc = TupleToolBase::initialize();
+  if ( sc.isFailure() ) return sc;
+
+  m_pidTool =
+    tool<ANNGlobalPID::IChargedProtoANNPIDTool>( "ANNGlobalPID::ChargedProtoANNPIDTool",
+                                                 "ANNPID", this );
+
+  return sc;
+}
+
+//=============================================================================
+
+StatusCode TupleToolANNPID::fill( const LHCb::Particle*
+                                  , const LHCb::Particle* P
+                                  , const std::string& head
+                                  , Tuples::Tuple& tuple )
+{
+  StatusCode sc = StatusCode::SUCCESS;
+
+  if ( !P || !P->proto() ) return sc;
+
+  const std::string prefix = fullName(head);
+
+  // Loop over PID tunes
+  for ( const auto& pidTune : m_pidTunes )
+  {
+    // Fill the ANNPID variables
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNe", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(11),pidTune) );
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNmu", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(13),pidTune) );
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNpi", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(211),pidTune) );
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNk", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(321),pidTune) );
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNp", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(2212),pidTune) );
+    sc = sc && tuple->column( prefix+"_"+pidTune+"_ProbNNghost", 
+                              m_pidTool->annPID(P->proto(),LHCb::ParticleID(0),pidTune) ); 
+  }
+  
+  return sc;
+}
+
+// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( TupleToolANNPID )
---
+++ Analysis/Phys/DecayTreeTupleANNPID/src/TupleToolANNPID.h
@@ -0,0 +1,53 @@
+// $Id: TupleToolProtopinfo.h,v 1.1 2010-05-21 17:03:11 odescham Exp $
+#ifndef _TupleToolANNPID_H
+#define _TupleToolANNPID_H 1
+
+// STL
+#include <string>
+#include <vector>
+
+// Gaudi
+#include "GaudiKernel/ToolFactory.h"
+
+// Base class
+#include "DecayTreeTupleBase/TupleToolBase.h"
+
+// Interfaces
+#include "Kernel/IParticleTupleTool.h"
+#include "RecInterfaces/IChargedProtoANNPIDTool.h"
+
+//============================================================================
+
+class TupleToolANNPID : public TupleToolBase,
+                        virtual public IParticleTupleTool
+{
+
+public:
+
+  // Standard constructor
+  TupleToolANNPID( const std::string& type,
+                   const std::string& name,
+                   const IInterface* parent);
+  
+  virtual ~TupleToolANNPID() {}
+
+  StatusCode initialize(); ///< Initialise
+
+public:
+
+  virtual StatusCode fill( const LHCb::Particle*,
+                           const LHCb::Particle*,
+                           const std::string& ,
+                           Tuples::Tuple& );
+
+private:
+
+  /// Pointer to the ANNPID tool
+  const ANNGlobalPID::IChargedProtoANNPIDTool * m_pidTool;
+
+  /// The ANNPID tunes to fill
+  std::vector<std::string> m_pidTunes;
+  
+};
+
+#endif // _TupleToolANNPID_H
