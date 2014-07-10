84768a8c148413d1a609648902be8d605c1bd259
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-07-10 10:42:14 +0000

    - fix off-by-one beyond-bounds bug when zero-ing the indices (identified    very clearly by clang!)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@174786 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3d0712e8c065d7e353c2215c5c8fd77b268f650f

--- Rec/Tf/TfKernel/TfKernel/IndexedHitContainer.h
+++ Rec/Tf/TfKernel/TfKernel/IndexedHitContainer.h
@@ -1,4 +1,4 @@
-#ifndef  INDEXEDHITCONTAINER_H 
+#ifndef  INDEXEDHITCONTAINER_H
 #define  INDEXEDHITCONTAINER_H 1
 
 #include <assert.h>
@@ -17,25 +17,28 @@ class IndexedHitContainer {
 
 #ifndef __GCCXML__
 
-    std::pair<size_t,size_t> offset( unsigned station ) const {
+    inline std::pair<size_t,size_t> offset( unsigned station ) const {
         return { m_s_offsets[station], m_s_offsets[station+1] };
     }
-    std::pair<size_t,size_t> offset( unsigned station, unsigned layer ) const {
+    inline std::pair<size_t,size_t> offset( unsigned station, unsigned layer ) const {
         auto i = m_s_offsets[station] ;
         return { i + m_l_offsets[station][layer], i + m_l_offsets[station][layer+1] };
     }
-    std::pair<size_t,size_t> offset( unsigned station, unsigned layer, unsigned region ) const {
+    inline std::pair<size_t,size_t> offset( unsigned station, unsigned layer, unsigned region ) const {
         auto i = m_s_offsets[station] + m_l_offsets[station][layer];
         return { i + m_r_offsets[station][layer][region], i + m_r_offsets[station][layer][region+1] };
     }
+    inline void clearOffsets() {
+        std::fill( std::begin(m_s_offsets),       std::end(m_s_offsets),                                   0u );
+        std::fill( std::begin(m_l_offsets[0]),    std::end(m_l_offsets[NStations-1]),                      0u );
+        std::fill( std::begin(m_r_offsets[0][0]), std::end(m_r_offsets[NStations-1][NLayersPerStation-1]), 0u );
+    }
 
 public:
-    IndexedHitContainer( size_t reserve = 0) 
+    IndexedHitContainer( size_t reserve = 0 )
     {
-        if (reserve>0) m_hits.reserve( reserve );
-        std::fill( std::begin(m_s_offsets),       std::end(m_s_offsets),                               0u );
-        std::fill( std::begin(m_l_offsets[0]),    std::end(m_l_offsets[NStations]),                    0u );
-        std::fill( std::begin(m_r_offsets[0][0]), std::end(m_r_offsets[NStations][NLayersPerStation]), 0u );
+        if (reserve!=0) m_hits.reserve( reserve );
+        clearOffsets();
     }
 #endif
 
@@ -55,9 +58,7 @@ public:
 #ifndef __GCCXML__
     void clear() {
         m_hits.clear();
-        std::fill( std::begin(m_s_offsets),       std::end(m_s_offsets),                               0u );
-        std::fill( std::begin(m_l_offsets[0]),    std::end(m_l_offsets[NStations]),                    0u );
-        std::fill( std::begin(m_r_offsets[0][0]), std::end(m_r_offsets[NStations][NLayersPerStation]), 0u );
+        clearOffsets();
     }
 
     std::pair<iterator,iterator> range_( unsigned station, unsigned layer, unsigned region ) {
@@ -85,24 +86,24 @@ public:
       return { std::begin(m_hits), std::end(m_hits) };
     }
 
-    size_t size(unsigned station)                                  const { return m_s_offsets[station+1] - m_s_offsets[station]; }
-    size_t size(unsigned station, unsigned layer)                  const { return m_l_offsets[station][layer+1]-m_l_offsets[station][layer]; }
-    size_t size(unsigned station, unsigned layer, unsigned region) const { return m_r_offsets[station][layer][region+1] - m_r_offsets[station][layer][region] ; } 
+    size_t size( unsigned station )                                  const { return m_s_offsets[station+1] - m_s_offsets[station]; }
+    size_t size( unsigned station, unsigned layer )                  const { return m_l_offsets[station][layer+1]-m_l_offsets[station][layer]; }
+    size_t size( unsigned station, unsigned layer, unsigned region ) const { return m_r_offsets[station][layer][region+1] - m_r_offsets[station][layer][region] ; }
 
-    bool empty(unsigned station)                                const { return  m_s_offsets[station+1] == m_s_offsets[station]; }
-    bool empty(unsigned station, unsigned layer)                const { return  m_l_offsets[station][layer+1] == m_l_offsets[station][layer] ; }
-    bool empty(unsigned station, unsigned layer, size_t region) const { return  m_r_offsets[station][layer][region+1] == m_r_offsets[station][layer][region] ; }
+    bool empty( unsigned station )                                const { return  m_s_offsets[station+1] == m_s_offsets[station]; }
+    bool empty( unsigned station, unsigned layer )                const { return  m_l_offsets[station][layer+1] == m_l_offsets[station][layer] ; }
+    bool empty( unsigned station, unsigned layer, size_t region ) const { return  m_r_offsets[station][layer][region+1] == m_r_offsets[station][layer][region] ; }
 
     // empirically, in Brunel ALL inserts happen at the 'end' of m_hits...
     template <typename I>
-    void insert( unsigned station, unsigned layer, unsigned region, I&& b, I&& e) {
+    void insert( unsigned station, unsigned layer, unsigned region, I&& b, I&& e ) {
           auto n = std::distance(b,e);
           // assert( m_hits.size() + n < std::numeric_limits<offset_t>::max()  );
           // std::cout << " size: " << m_hits.size() << " capacity: " << m_hits.capacity() << " growing to " << m_hits.size()+n << " requires realloc: " << ( m_hits.size()+n<= m_hits.capacity() ? " no " : " yes " ) << std::endl;
           // std::cout << " insertion of " << n << " entries at offset " << std::distance(std::begin(m_hits),begin(station,layer,region)) << " atEnd: " << ( begin(station,layer,region)==std::end(m_hits) ? " yes " : " no " )<< std::endl;
           m_hits.reserve( m_hits.size() + n );
-          m_hits.insert( std::next( std::begin(m_hits), 
-                                    m_s_offsets[station] + m_l_offsets[station][layer] + m_r_offsets[station][layer][region]) , 
+          m_hits.insert( std::next( std::begin(m_hits),
+                                    m_s_offsets[station] + m_l_offsets[station][layer] + m_r_offsets[station][layer][region]) ,
                          std::forward<I>(b), std::forward<I>(e) );
           // adjust everything 'behind' the insertion point...
           auto add_n = [n](offset_t& offset) { offset+=n; };
@@ -111,9 +112,8 @@ public:
           // subsequent layers in current station
           std::for_each( std::next(m_l_offsets[station],layer+1), std::end(m_l_offsets[station]), add_n );
           // subsequent regions in current layer
-          std::for_each( std::next(m_r_offsets[station][layer],region+1), std::end(m_r_offsets[station][layer]), add_n);
+          std::for_each( std::next(m_r_offsets[station][layer],region+1), std::end(m_r_offsets[station][layer]), add_n );
     }
 #endif
 };
-
 #endif
