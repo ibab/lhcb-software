75fbd6802b46b03ad9d0e31d297939828a0299f1
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-07-13 11:35:44 +0000

    replace hitpointer containers and bool arrays in UT station manager
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@174876 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fd08064f5562d996e2ee956c1a1d82d193970203

--- Rec/Tf/TfKernel/TfKernel/UTStationHitManager.h
+++ Rec/Tf/TfKernel/TfKernel/UTStationHitManager.h
@@ -15,6 +15,8 @@
 // from Gaudi
 #include "GaudiAlg/GaudiTool.h"
 #include "GaudiKernel/ToolHandle.h"
+#include "TfKernel/IndexedHitContainer.h"
+#include "TfKernel/IndexedBitArray.h"
 #include "TfKernel/IUTHitCreator.h"
 #include "TfKernel/LineHit.h"
 #include "TfKernel/HitExtension.h"
@@ -27,8 +29,7 @@
 #include "GaudiKernel/IIncidentSvc.h"
 
 // boost
-//#include <boost/lambda/lambda.hpp>
-//#include <boost/lambda/bind.hpp>
+#include "boost/iterator/transform_iterator.hpp"
 
 /// Static interface ID
 static const InterfaceID IID_UTStationHitManager ( "UTStationHitManager", 1, 0 );
@@ -104,10 +105,13 @@ namespace Tf
   public:
 
     /// Type for container of Hits
-    typedef std::vector< Hit* > Hits;
+    typedef  IndexedHitContainer< Tf::RegionID::UTIndex::kNStations, 
+                                  Tf::RegionID::UTIndex::kNLayers, 
+                                  Tf::RegionID::UTIndex::kNRegions,
+                                  Hit* > Hits;
 
     /// range object for Hits
-    typedef Gaudi::Range_<Hits> HitRange;
+    typedef typename Hits::HitRange HitRange;
 
   public:
 
@@ -172,8 +176,7 @@ namespace Tf
                           const UTRegionID region ) const
     {
       if (!allHitsPrepared(sta,lay,region)) prepareHits(sta,lay,region);
-      return HitRange( m_hits[sta][lay][region].begin(),
-                       m_hits[sta][lay][region].end() );
+      return m_hits.range(sta,lay,region);
     }
 
     /** Load the hits for a given region of interest
@@ -187,8 +190,7 @@ namespace Tf
                           const UTLayerID lay ) const
     {
       if (!allHitsPrepared(sta,lay)) prepareHits(sta,lay);
-      return HitRange( m_hits_layers[sta][lay].begin(),
-                       m_hits_layers[sta][lay].end() );
+      return m_hits.range(sta,lay);
     }
 
     /** Load the hits for a given region of interest
@@ -200,8 +202,7 @@ namespace Tf
     inline HitRange hits( const UTStationID sta ) const
     {
       if (!allHitsPrepared(sta)) prepareHits(sta);
-      return HitRange( m_hits_stations[sta].begin(),
-                       m_hits_stations[sta].end() );
+      return m_hits.range(sta);
     }
 
     /** Load the all hits
@@ -210,8 +211,7 @@ namespace Tf
     inline HitRange hits( ) const
     {
       if (!allHitsPrepared()) prepareHits();
-      return HitRange( m_hits_all.begin(),
-                       m_hits_all.end() );
+      return m_hits.range();
     }
 
     /** Load the hits for a given region of interest
@@ -230,11 +230,12 @@ namespace Tf
                                   const UTRegionID region ) const
     {
       if (!allHitsPrepared(sta,lay,region)) prepareHits(sta,lay,region);
-      return HitRange( std::lower_bound( m_hits[sta][lay][region].begin(),
-                                         m_hits[sta][lay][region].end(),
+      HitRange range = m_hits.range(sta,lay,region);
+      return HitRange( std::lower_bound( range.begin(),
+                                         range.end(),
                                          xMin,
                                          Tf::compByX_LB< Hit >() ),
-                       m_hits[sta][lay][region].end() );
+                       range.end() );
     }
 
     /** Retrieve the STRegion for a certain UT region ID. The region
@@ -265,68 +266,23 @@ namespace Tf
                           const UTLayerID lay,
                           const UTRegionID region )
     {
-      std::sort ( m_hits[sta][lay][region].begin(), m_hits[sta][lay][region].end(), SORTER() );
-    }
-
-    /** Sort the hits in a given station and layer
-     *  @param[in] sta    Station ID
-     *  @param[in] lay    Station layer ID
-     */
-    template < typename SORTER >
-    inline void sortHits( const UTStationID sta,
-                          const UTLayerID lay )
-    {
-      std::sort ( m_hits_layers[sta][lay].begin(), m_hits_layers[sta][lay].end(), SORTER() );
-    }
-
-    /** Sort the hits in a given station
-     *  @param[in] sta    Station ID
-     */
-    template < typename SORTER >
-    inline void sortHits( const UTStationID sta )
-    {
-      std::sort ( m_hits_stations[sta].begin(), m_hits_stations[sta].end(), SORTER() );
-    }
-
-    /** Sort the all the hits
-     */
-    template < typename SORTER >
-    inline void sortHits( )
-    {
-      std::sort ( m_hits_all.begin(), m_hits_all.end(), SORTER() );
+      std::pair<typename Hits::iterator,typename Hits::iterator>  rng = m_hits.range_(sta,lay,region);
+      std::sort ( rng.first, rng.second, SORTER() );
     }
 
     /// Maximum number of stations
-    inline UTStationID maxStations() const { return UTStationID(m_nSta); }
+    inline UTStationID maxStations() const { return UTStationID(Tf::RegionID::UTIndex::kNStations); }
 
     /// Maximum number of layers
-    inline UTLayerID maxLayers()     const { return UTLayerID(m_nLay); }
+    inline UTLayerID maxLayers()     const { return UTLayerID(Tf::RegionID::UTIndex::kNLayers); }
 
     /// Maximum number of regions
-    inline UTRegionID maxRegions()   const { return UTRegionID(m_nReg); }
+    inline UTRegionID maxRegions()   const { return UTRegionID(Tf::RegionID::UTIndex::kNRegions); }
 
   protected:
 
     /// Direct access to the underlying hit creator
     inline const Tf::IUTHitCreator * utHitCreator() const { return &*m_uthitcreator; }
-
-    /** Add a hit to the container
-     *  @param hit Pointer to the hit to add
-     *  @param[in] sta    The station number
-     *  @param[in] lay    The layer number
-     *  @param[in] region The region number
-     */
-    inline void addHit( Hit * hit,
-                        const UTStationID sta,
-                        const UTLayerID lay,
-                        const UTRegionID region ) const
-    {
-      m_hits[sta][lay][region].push_back(hit);
-      // temporary hack, to get things working. Needs to be done better
-      m_hits_layers[sta][lay].push_back(hit);
-      m_hits_stations[sta].push_back(hit);
-      m_hits_all.push_back(hit);
-    }
 
   public:
 
@@ -368,36 +324,15 @@ namespace Tf
      */
     inline void setAllHitsPrepared( const UTStationID sta,
                                     const UTLayerID lay,
-                                    const UTRegionID region,
-                                    const bool ok ) const { m_hits_ready[sta][lay][region] = ok; }
-
-    /** Set the hits ready flag for given layer
-     *  @param[in] sta    Station ID
-     *  @param[in] lay    Station layer ID
-     *  @param[in] ok     The status flag (true means hits ready, false means not ready)
-     */
-    inline void setAllHitsPrepared( const UTStationID sta,
-                                    const UTLayerID lay,
-                                    const bool ok ) const { m_hits_layers_ready[sta][lay] = ok; }
-
-    /** Set the hits ready flag for given station
-     *  @param[in] sta    Station ID
-     *  @param[in] ok     The status flag (true means hits ready, false means not ready)
-     */
-    inline void setAllHitsPrepared( const UTStationID sta,
-                                    const bool ok ) const { m_hits_stations_ready[sta] = ok; }
-
-    /** Set the hits ready flag for all hits
-     *  @param[in] ok     The status flag (true means hits ready, false means not ready)
-     */
-    inline void setAllHitsPrepared( const bool ok ) const { m_hits_all_ready = ok; }
+                                    const UTRegionID region
+                                  ) const { m_hits_ready.set(sta,lay,region); }
 
     /** Are all the hits ready
      *  @return boolean indicating if all the hits in the given region are ready or not
      *  @retval TRUE  Hits are ready
      *  @retval FALSE Hits are not ready
      */
-    inline bool allHitsPrepared() const { return m_hits_all_ready; }
+    inline bool allHitsPrepared() const { return m_hits_ready.all(); }
 
     /** Are all the hits ready in the given region
      *  @param[in] sta    Station ID
@@ -410,7 +345,7 @@ namespace Tf
     inline bool allHitsPrepared(const UTStationID sta,
                                 const UTLayerID lay,
                                 const UTRegionID region) const
-    { return m_hits_ready[sta][lay][region]; }
+    { return m_hits_ready.test(sta,lay,region); }
 
     /** Are all the hits ready in the given layer
      *  @param[in] sta    Station ID
@@ -421,7 +356,7 @@ namespace Tf
      */
     inline bool allHitsPrepared(const UTStationID sta,
                                 const UTLayerID lay) const
-    { return m_hits_layers_ready[sta][lay]; }
+    { return m_hits_ready.all(sta,lay); }
 
     /** Are all the hits ready in the given station
      *  @param[in] sta    Station ID
@@ -430,32 +365,21 @@ namespace Tf
      *  @retval FALSE Hits are not ready
      */
     inline bool allHitsPrepared(const UTStationID sta) const
-    { return m_hits_stations_ready[sta]; }
+    { return m_hits_ready.all(sta); }
 
   private:
 
-    /// max number of stations
-    static const int m_nSta = Tf::RegionID::UTIndex::kNStations;
-    /// max number of layers
-    static const int m_nLay = Tf::RegionID::UTIndex::kNLayers;
-    /// max number of regions
-    static const int m_nReg = Tf::RegionID::UTIndex::kNRegions;
 
     /// The underlying UT hit creator
     ToolHandle<Tf::IUTHitCreator> m_uthitcreator ;
 
-    mutable Hits m_hits[m_nSta][m_nLay][m_nReg]; ///< Hits in individual regions
-    // CRJ : This is ugly but just a first bash to provide *something* that works
-    //       Need to try and find a neater way to handle these different pointer containers
-    mutable Hits m_hits_layers[m_nSta][m_nLay]; ///< Hits in individual layers
-    mutable Hits m_hits_stations[m_nSta];       ///< Hits in individual stations
-    mutable Hits m_hits_all;                    ///< All hits
+    mutable Hits m_hits;
 
     // Flags to indicate which hits are ready
-    mutable bool m_hits_ready[m_nSta][m_nLay][m_nReg]; ///< Flags to indicate which regions have hits ready
-    mutable bool m_hits_layers_ready[m_nSta][m_nLay];  ///< Flag to indicate which layers are ready
-    mutable bool m_hits_stations_ready[m_nSta];        ///< Flag to indicate which stations are ready
-    mutable bool m_hits_all_ready;                     ///< Flag to indicate all hits are ready
+    mutable IndexedBitArray< Tf::RegionID::UTIndex::kNStations, 
+                             Tf::RegionID::UTIndex::kNLayers, 
+                             Tf::RegionID::UTIndex::kNRegions 
+                           >   m_hits_ready; ///< Flags to indicate which regions have hits ready
 
   };
 
@@ -497,28 +421,10 @@ namespace Tf
   template<class Hit>
   void UTStationHitManager<Hit>::clearHits() const
   {
-    m_hits_all.clear();
-    this->setAllHitsPrepared(false);
-    for ( UTStationID s = 0; s<maxStations(); ++s )
-    {
-      this->setAllHitsPrepared(s,false);
-      m_hits_stations[s].clear();
-      for ( UTLayerID l = 0; l<maxLayers(); ++l )
-      {
-        this->setAllHitsPrepared(s,l,false);
-        m_hits_layers[s][l].clear();
-        for ( UTRegionID t = 0; t<maxRegions(); ++t )
-        {
-          this->setAllHitsPrepared(s,l,t,false);
-          for ( typename Hits::iterator iHit = m_hits[s][l][t].begin();
-                iHit != m_hits[s][l][t].end(); ++iHit )
-          {
-            delete *iHit;
-          }
-          m_hits[s][l][t].clear();
-        }
-      }
-    }
+    HitRange rng = m_hits.range();
+    std::for_each( rng.begin(), rng.end(), std::default_delete<Hit>() ) ; 
+    m_hits.clear();
+    m_hits_ready.clear();
   }
 
   template<class Hit>
@@ -530,7 +436,6 @@ namespace Tf
       {
         this->prepareHits(sta);
       }
-      setAllHitsPrepared(true);
     }
   }
 
@@ -543,7 +448,6 @@ namespace Tf
       {
         this->prepareHits(sta,lay);
       }
-      setAllHitsPrepared(sta,true);
     }
   }
 
@@ -557,7 +461,6 @@ namespace Tf
       {
         this->prepareHits(sta,lay,reg);
       }
-      setAllHitsPrepared(sta,lay,true);
     }
   }
 
@@ -569,12 +472,11 @@ namespace Tf
     if ( !this->allHitsPrepared(sta,lay,region) )
     {
       Tf::STHitRange uthits = utHitCreator()->hits(sta,lay,region) ;
-      for (Tf::STHitRange::const_iterator itUTH = uthits.begin();
-           itUTH < uthits.end(); itUTH++)
-      {
-        this -> addHit(new Hit(**itUTH),sta,lay,region);
-      }
-      setAllHitsPrepared(sta,lay,region,true);
+      auto fun = [](Tf::STHitRange::const_reference hit) { return new Hit(*hit); };
+      auto b =  boost::make_transform_iterator(std::begin(uthits), std::cref(fun));
+      auto e =  boost::make_transform_iterator(std::end(uthits), std::cref(fun));
+      m_hits.insert( sta,lay,region, b, e);
+      setAllHitsPrepared(sta,lay,region);
     }
   }
 
@@ -585,13 +487,10 @@ namespace Tf
     this->clearHits();
     for (UTStationID sta=0; sta<maxStations(); ++sta )
     {
-      this->setAllHitsPrepared(sta,true);
       for (UTLayerID lay=0; lay<maxLayers(); ++lay )
       {
-        this->setAllHitsPrepared(sta,lay,true);
         for (UTRegionID reg=0; reg<maxRegions(); ++reg )
         {
-          this->setAllHitsPrepared(sta,lay,reg,true);
 
           // The z value for this region
           const double z = this->utHitCreator()->region(sta,lay,reg)->z();
@@ -606,11 +505,11 @@ namespace Tf
             debug() << "Found " << uthits.size() << " UTHits for station=" << sta
                     << " layer=" << lay << " region=" << reg << endmsg;
           }
-          for (Tf::STHitRange::const_iterator itUTH = uthits.begin();
-               itUTH < uthits.end(); itUTH++)
-          {
-            this -> addHit(new Hit(**itUTH),sta,lay,reg);
-          }
+          auto fun = [](Tf::STHitRange::const_reference hit) { return new Hit(*hit); };
+          auto b =  boost::make_transform_iterator(std::begin(uthits), std::cref(fun));
+          auto e =  boost::make_transform_iterator(std::end(uthits), std::cref(fun));
+          m_hits.insert( sta,lay,region, b, e);
+          this->setAllHitsPrepared(sta,lay,reg);
         }
       }// layer
     } // station
