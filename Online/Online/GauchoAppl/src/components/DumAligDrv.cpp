8c452f9f55e44b7e48213ea6aa3c95e0f160fe80
Beat Jost <beat.jost@cern.ch>
2014-08-07 16:00:54 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@176130 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: e44e7c6a97bdb5a4bc9f394b2b07e840d4bb6744

--- Online/Online/GauchoAppl/src/components/DumAligDrv.cpp
+++ Online/Online/GauchoAppl/src/components/DumAligDrv.cpp
@@ -221,7 +221,7 @@ StatusCode Fitter::init()
     ii = char(i);
     nam[0] = 'a'+ii;
     nam[1] = 0;
-    m_Minuit->DefineParameter(i,nam,DrvInstance->m_params[i],1.0,-10.0,10.0);
+    m_Minuit->DefineParameter(i,nam,DrvInstance->m_params[i],3.0,-10.0,10.0);
   };
   return StatusCode::SUCCESS;
 }
