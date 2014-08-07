90bf912dd9a6f6618b6acbc0353f1350750321f5
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-08-07 13:18:22 +0000

    replace size_t by int to specify layer/station/region -- int more than large enough to enumerate them, and int division is faster than size_t division.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@176119 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4a885588780ad0ff5341de72acad3677dfae381b

--- Rec/Tf/TfKernel/TfKernel/IndexedBitArray.h
+++ Rec/Tf/TfKernel/TfKernel/IndexedBitArray.h
@@ -6,32 +6,31 @@
 
 
 
-template <size_t NStations, size_t NLayersPerStation, size_t NRegionsPerLayer, typename Storage = unsigned long > 
+template <int NStations, int NLayersPerStation, int NRegionsPerLayer, typename Storage = unsigned long > 
 class IndexedBitArray 
 #ifndef __GCCXML__
 final 
 #endif 
 {
 
-    static const size_t NBits = NStations*NLayersPerStation*NRegionsPerLayer;
-    static const size_t NBitsPerWord = std::numeric_limits<Storage>::digits;
-    static const size_t NWords = 1+(NBits-1)/NBitsPerWord;
+    static const int NBits = NStations*NLayersPerStation*NRegionsPerLayer;
+    static const int NBitsPerWord = std::numeric_limits<Storage>::digits;
+    static const int NWords = 1+(NBits-1)/NBitsPerWord;
 
     Storage m_data[NWords];
 
 #ifndef __GCCXML__
-    static constexpr size_t iw(size_t ibit) { return ibit / NBitsPerWord; }
-    static constexpr size_t io(size_t ibit) { return ibit % NBitsPerWord; }
-    static constexpr size_t jo(size_t ibit) { return NBitsPerWord - ibit % NBitsPerWord; }
-    static constexpr Storage one  = 1;
-    static constexpr Storage zero = 0;
-    static constexpr Storage allset = ~zero;
+    static constexpr int iw(int ibit) { return ibit / NBitsPerWord; }
+    static constexpr int io(int ibit) { return ibit % NBitsPerWord; }
+    static constexpr int jo(int ibit) { return NBitsPerWord - ibit % NBitsPerWord; }
+    static constexpr Storage ib(int ibit) { return Storage(1) << io(ibit) ; }
+    static constexpr Storage allset = ~Storage(0);
 
     struct nbits_le_nbitsPerWord {};
     struct nbits_gt_nbitsPerWord {};
 
-    template <size_t nbits>
-    bool all_n_( size_t first, nbits_le_nbitsPerWord ) const {
+    template <int nbits>
+    bool all_n_( int first, nbits_le_nbitsPerWord ) const {
         // nbits<=NBitsPerWord -- so we span at most two words...
         auto bw = iw(first);
         auto ew = iw(first+nbits);
@@ -41,8 +40,8 @@ final
         return ( NWords==1 || bw == ew ) ? ( ( m_data[bw] & m1 & m2 ) == ( m1 & m2 ) ) 
                                          : ( ( m_data[bw] & m1 ) == m1 && ( m_data[ew] & m2 ) == m2 );
     }
-    template <size_t nbits>
-    bool all_n_( size_t first, nbits_gt_nbitsPerWord ) const {
+    template <int nbits>
+    bool all_n_( int first, nbits_gt_nbitsPerWord ) const {
         // nbits > NBitsPerWord -- so we span at least two words...
         auto bw = iw(first);
         auto ew = iw(first+nbits);
@@ -56,9 +55,9 @@ final
                                              [](const Storage& w) { return w == allset; } ) ) 
             && ( ( m_data[ew] & m2 ) == m2 );
     }
-    template <size_t nbits>
-    bool all_n( size_t first ) const { return nbits <= NBitsPerWord ? all_n_<nbits>( first, nbits_le_nbitsPerWord{} ) 
-                                                                    : all_n_<nbits>( first, nbits_gt_nbitsPerWord{} ); }
+    template <int nbits>
+    bool all_n( int first ) const { return nbits <= NBitsPerWord ? all_n_<nbits>( first, nbits_le_nbitsPerWord{} ) 
+                                                                 : all_n_<nbits>( first, nbits_gt_nbitsPerWord{} ); }
 #endif
 public:
 
@@ -67,28 +66,28 @@ public:
     }
 
 #ifdef __GCCXML__
-    void set( size_t station, size_t layer, size_t region ) ;
+    void set( int station, int layer, int region ) ;
     void clear();
-    bool test( size_t station, size_t layer, size_t region ) const;
-    bool all( size_t station, size_t layer ) const;
-    bool all( size_t station  ) const ;
+    bool test( int station, int layer, int region ) const;
+    bool all( int station, int layer ) const;
+    bool all( int station  ) const ;
     bool all( ) const ;
 #else
-    void set( size_t station, size_t layer, size_t region ) {
+    void set( int station, int layer, int region ) {
         auto b = region + NRegionsPerLayer*(layer + NLayersPerStation*station);
-        m_data[ iw(b) ] |=  one << io(b) ;
+        m_data[ iw(b) ] |=  ib(b); 
     }
     void clear() {
-        std::fill( std::begin(m_data), std::end(m_data), zero);
+        std::fill( std::begin(m_data), std::end(m_data), Storage(0));
     }
-    bool test( size_t station, size_t layer, size_t region ) const {
+    bool test( int station, int layer, int region ) const {
         auto b = region + NRegionsPerLayer*(layer + NLayersPerStation*station);
-        return m_data[iw(b)] & ( one << io(b) ) ;
+        return m_data[iw(b)] & ib(b)  ;
     }
-    bool all( size_t station, size_t layer ) const {
+    bool all( int station, int layer ) const {
         return all_n<NRegionsPerLayer>( NRegionsPerLayer*(layer + NLayersPerStation*station) );
     }
-    bool all( size_t station  ) const {
+    bool all( int station  ) const {
         return all_n<NRegionsPerLayer*NLayersPerStation>( NRegionsPerLayer*NLayersPerStation*station );
     }
     bool all( ) const {
