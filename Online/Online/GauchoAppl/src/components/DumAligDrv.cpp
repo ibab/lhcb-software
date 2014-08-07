6ac6d6f972dccdf6148e99deb301838a9d205b6b
Beat Jost <beat.jost@cern.ch>
2014-08-07 15:38:12 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@176127 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 8eebda2f6baf5912d6f033067844dfefb87db1d8

--- Online/Online/GauchoAppl/src/components/DumAligDrv.cpp
+++ Online/Online/GauchoAppl/src/components/DumAligDrv.cpp
@@ -37,12 +37,13 @@ extern "C"
     DrvInstance->waitRunOnce();
 //    DrvInstance->m_fitter->m_Lock->lockMutex();
     fval = DrvInstance->m_fitter->getIterationResult();
-    printf ("Chi2: Function value: %lf\nParameters:\n");
+    printf ("Chi2: Function value: %lf\nParameters:\n",fval);
     for (int i= 0;i<npar;i++)
     {
       printf("Paramter %d %lf\n",i,params[i]);
     }
     printf("\n");
+    fflush(stdout);
 //    DrvInstance->m_fitter->m_Lock->unlockMutex();
     return;
   };
