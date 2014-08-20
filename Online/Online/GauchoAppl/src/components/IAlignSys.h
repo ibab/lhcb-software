4728f4fe71ff01fa396e899f7386885bfbef57d0
Beat Jost <beat.jost@cern.ch>
2014-08-20 12:58:39 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@176573 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: c0f1f91a19fc9c078f2cc24d91f92137278efcb3

--- Online/Online/GauchoAppl/src/components/IAlignSys.h
+++ Online/Online/GauchoAppl/src/components/IAlignSys.h
@@ -21,7 +21,6 @@ namespace LHCb
       DeclareInterfaceID(IAlignDrv,1,0);
       virtual void writeReference()=0;
       virtual void waitRunOnce()=0;
-      virtual void setRunOnce()=0;
       virtual void doContinue()=0;
       virtual void doStop()=0;
   };
