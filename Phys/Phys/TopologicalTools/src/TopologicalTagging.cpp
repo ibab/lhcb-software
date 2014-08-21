23f5ef4cf7edd529d672ca62ce4c17fd6d4aaeaf
Cedric Potterat <cedric.potterat@cern.ch>
2014-08-21 21:21:30 +0000

    add TopologicalTagging for jet stripping
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@176686 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: aa19ce460958b3212056516f5053f64a55e1d92a

---
+++ Phys/Phys/TopologicalTools/src/TopologicalTagging.cpp
@@ -0,0 +1,360 @@
+#include "TopologicalTagging.h"
+
+//=======================================
+//
+// addapted from FilterDesktop
+//
+//=======================================
+
+DECLARE_ALGORITHM_FACTORY( TopologicalTagging )
+
+    //=============================================================================
+    // Constructor
+TopologicalTagging::TopologicalTagging(const std::string& name,
+        ISvcLocator* pSvcLocator)
+    : DaVinciAlgorithm(name,pSvcLocator)
+      , m_TriggerTisTosTool(0)
+    , m_tinputs              ("cedric")
+      ,m_distCalc(0)
+
+{
+    // Algorithm related properties
+    declareProperty("TriggerLine",
+            m_TLine = "Hlt2Topo.*BBDT.*Decision");
+    declareProperty("TestInputs", m_tinputs);
+
+}
+
+//=============================================================================
+// Initalise
+StatusCode  TopologicalTagging::initialize ()
+{
+    const StatusCode sc = DaVinciAlgorithm::initialize();
+    if ( sc.isFailure() ) return sc;
+
+
+    svc<LoKi::ILoKiSvc>("LoKiSvc", true);
+
+
+
+    m_distCalc = tool<IDistanceCalculator>("LoKi::DistanceCalculator",this);
+    m_TriggerTisTosTool = tool<ITriggerTisTos>( "TriggerTisTos","TriggerTisTos",this );
+    if(m_TriggerTisTosTool == 0)
+        return Error("Couldn't get requested jet tag tool", StatusCode::SUCCESS);
+
+
+    return sc;
+}
+
+StatusCode TopologicalTagging::execute()
+{
+    setFilterPassed(false);
+    i_markedParticles.clear();
+
+    const LHCb::Particle::ConstVector& jets = i_particles () ;
+
+    LHCb::Particle::Range topos;
+    bool locseed = exist<LHCb::Particle::Range>( m_tinputs );
+    if(!locseed){
+        debug()<< "Unable to get location at '"
+            << m_tinputs  << "'" << endreq;
+    }else if(locseed){
+        topos = get<LHCb::Particle::Range>(  m_tinputs );
+        if( !topos ){
+            debug()<< "Unable to get particles at '"
+                << m_tinputs  << "'" << endreq;
+        }
+    }
+    std::vector<const LHCb::HltObjectSummary*> hltObjs
+        = m_TriggerTisTosTool->hltObjectSummaries("Hlt2Topo.*BBDTDecision",2,2);
+    m_TriggerTisTosTool->setOfflineInput();
+    unsigned int num = hltObjs.size();
+
+
+    for ( LHCb::Particle::ConstVector::const_iterator ijet = jets.begin();
+            jets.end() != ijet ;++ijet )
+    {
+        const LHCb::Particle* jet = *ijet;
+        //std::cout<<"dsize: "<<(jet->daughtersVector()).size()<<std::endl;
+        //check if jet is TOS Hlt2Topo*BBDT, if yes add 10 to the tagging HLT value
+        if(jet->particleID().abspid() != 98) continue;
+        int tagTOPO = 0;
+        int tagHLT = 0;
+        bool closeTopo = false;
+        bool closeTopoOneTrk = false;
+        bool topoAllTracks = false;
+        ITriggerTisTos::TisTosDecision classifiedDec;
+        m_TriggerTisTosTool->setOfflineInput(*jet);
+        m_TriggerTisTosTool->setTriggerInput(m_TLine);
+        classifiedDec = m_TriggerTisTosTool->triggerTisTos();
+
+        if(classifiedDec.tos() == true) tagHLT+=10;
+
+
+
+        //get jet tracks.
+        std::vector<LHCb::Track*> jetTracks ;
+        LoKi::Extract::getTracks( jet , std::back_inserter( jetTracks ) ) ;
+
+
+        LHCb::Particle::Range::iterator itopo;
+
+        std::vector< LHCb::LHCbID > AllIDs;
+        AllIDs.clear();
+        getJetLHCbIDs(jet, AllIDs);
+        Gaudi::XYZVector A = jet->momentum().Vect();
+        if(locseed){
+
+            //   std::cout<<"topo size" << topos.size() << std::endl;
+
+            //    const LHCb::VertexBase* JetOriVtx = m_dva->bestVertex ( jet );
+
+            for(itopo = topos.begin(); itopo!=topos.end(); itopo++){
+
+
+                const LHCb::Particle* myTopo = *itopo;
+                const LHCb::VertexBase* evtx = myTopo->endVertex();
+                const LHCb::VertexBase* oriVtx = bestVertex ( myTopo );
+
+                //jet endVertex position is define as it's PV in the jet reco
+                if((jet->endVertex()->position() - oriVtx->position()).Mag2() > 1e-4) continue;
+
+
+                Gaudi::XYZVector B = evtx->position() - oriVtx->position();
+
+                std::vector<LHCb::Track*> topoTracks ;
+                LoKi::Extract::getTracks( myTopo , std::back_inserter( topoTracks ) ) ;
+
+
+                std::vector<LHCb::Track*>::iterator iTopoTracks ;
+                std::vector<LHCb::Track*>::iterator iJetTracks ;
+
+                int NTracksInJet = 0;
+                for(iTopoTracks=topoTracks.begin();iTopoTracks != topoTracks.end(); iTopoTracks++){
+                    for(iJetTracks=jetTracks.begin();iJetTracks != jetTracks.end(); iJetTracks++){
+                        if((*iTopoTracks)->key() == (*iJetTracks)->key())
+                            NTracksInJet++;
+                    }
+                }
+                if(NTracksInJet == (int)topoTracks.size() && !topoAllTracks){
+                    //check if jet contain all track of at least one particle of the Secondary verticies container, if yes add 100 to the tagging TOPO value
+                    topoAllTracks = true;
+                    tagTOPO+=100;
+
+                }
+                // LoKi::Cuts::DR2 deltaRFunction = Cuts::DR2(jet->momentum());
+
+                const double PI=3.14159265;
+                double Dphi,phi1,phi2,e1,e2;
+                phi1 = A.phi();
+                phi2 = B.phi();
+                e1 = A.eta();
+                e2 = B.eta();
+                Dphi = std::fabs(phi1-phi2);
+                if (Dphi>PI) { Dphi = 2*PI - Dphi; };
+                double distance = std::sqrt( Dphi*Dphi + (e1-e2)*(e1-e2) );
+                if(distance < 0.5) {
+                    if(!closeTopo){
+                        //check if any particle from the Secondary verticies container has it vertex inside the jet, if yes add 1 to the tagging TOPO value
+                        closeTopo=true;
+                        tagTOPO+=1;
+                    }
+                    if(!closeTopoOneTrk && NTracksInJet > 0){
+                        //check if any particle from the Secondary verticies container has it vertex inside the jet + one track matching a jet track, if yes add 10 to the tagging TOPO value
+                        closeTopoOneTrk=true;
+                        tagTOPO+=10;
+                    }
+                }
+
+                if(closeTopo && closeTopoOneTrk && topoAllTracks) break;
+
+            }
+        }
+        //check if any Hlt2Topo*BBDT object is within the jet cone (DeltaR 0.5), if yes add 1 to the tagging HLT value
+
+        float ratio = 0.;
+        for(unsigned int i = 0; i < num; i++){
+            std::vector< LHCb::LHCbID > hltLHCbIDs;
+            hltLHCbIDs.clear();
+            getHltObjLHCbIDs((hltObjs[i]),hltLHCbIDs);
+            float TotN = (float) hltLHCbIDs.size();
+            float TotMatching = 0.0;
+            ratio = 0.;
+            for(std::vector<LHCb::LHCbID>::iterator iID1 = hltLHCbIDs.begin(); iID1!= hltLHCbIDs.end(); iID1++){
+                for(std::vector<LHCb::LHCbID>::iterator iAllIDs = AllIDs.begin(); iAllIDs != AllIDs.end(); iAllIDs++){
+                    if((*iID1).lhcbID() == (*iAllIDs).lhcbID()){
+                        TotMatching+=1.0;
+                        ratio = TotMatching/TotN;
+                        if (ratio > 0.6)break;
+                    }
+                    if (ratio > 0.6)break;
+                }
+                if (ratio > 0.6)break;
+            }
+
+            //     std::cout<<"recVtx: "<<B1<<std::endl;
+            //   std::cout<<"PVcVtx: "<<B2<<std::endl;
+            if(ratio > 0.6) {
+                tagHLT+=1;
+                break;
+            }
+        }
+        if (tagHLT > 0 || tagTOPO > 0)
+        {
+
+            LHCb::Particle* tagJet = jet->clone();
+
+            if ( !m_SubPID.empty() )
+            {
+                LHCb::IParticlePropertySvc* ppSvc =
+                    svc<LHCb::IParticlePropertySvc>("LHCb::ParticlePropertySvc", true);
+                const LHCb::ParticleProperty* pp = ppSvc->find ( m_SubPID ) ;
+                tagJet->setParticleID(pp->particleID());
+            }
+
+            const LHCb::VertexBase* bestPV = getStoredBestPV(jet);
+            if ( 0!= bestPV ) { this->relate ( tagJet , bestPV ) ; }
+            if(!tagJet->hasInfo(9990)){
+                tagJet->addInfo(9990, tagHLT);
+                tagJet->addInfo(9991, tagTOPO);
+            }
+
+            i_markedParticles.push_back( tagJet ) ;
+
+            setFilterPassed(true);
+        }
+
+    }
+
+
+    return StatusCode::SUCCESS ;
+}
+
+
+// ============================================================================
+// save (clone if needed) selected particles in TES
+// ============================================================================
+template <class Particles>
+StatusCode TopologicalTagging::_save ( ) const
+{
+    //
+    Particles* p_tes = new Particles () ;
+    //
+    put ( p_tes ,    particleOutputLocation () ) ;
+
+
+    for ( std::vector<LHCb::Particle*>::const_iterator ip = i_markedParticles.begin();
+            ip != i_markedParticles.end(); ++ip )
+    {
+        //
+        LHCb::Particle* p = *ip ;
+        if ( NULL == p ) { continue ; } // CONTINUE
+
+        p_tes -> insert ( p ) ;
+        //
+
+    }
+
+    return ( i_markedParticles.size() != p_tes->size() ?
+            StatusCode::FAILURE : StatusCode::SUCCESS   );
+}
+
+// ============================================================================
+
+// ============================================================================
+
+StatusCode TopologicalTagging::_saveInTES ()
+{
+    if (msgLevel(MSG::VERBOSE))
+    {
+        verbose() << "TopologicalTagging::_saveInTES " << i_markedParticles.size() << " Particles" << endmsg;
+    }
+    return this -> _save<LHCb::Particle::Container> ();
+}
+// ============================================================================
+
+
+// ============================================================================
+void TopologicalTagging::writeEmptyTESContainers()
+{
+    return true ?
+        writeEmptyKeyedContainers()  :
+        writeEmptySharedContainers() ;
+
+}
+// ============================================================================
+void TopologicalTagging::writeEmptyKeyedContainers() const
+{
+    LHCb::Particle::Container* container = new LHCb::Particle::Container();
+    put(container, particleOutputLocation());
+}
+// ============================================================================
+void TopologicalTagging::writeEmptySharedContainers() const
+{
+    LHCb::Particle::Selection* container = new LHCb::Particle::Selection();
+    put(container, particleOutputLocation());
+}
+
+
+Gaudi::XYZVector TopologicalTagging::getHltObjVtxPos (const LHCb::HltObjectSummary *obj) const{
+    LHCb::HltObjectSummary::Info infod = obj->numericalInfo();
+
+    double p = 1/infod["7#Particle.1/p"];
+    double slopex = infod["5#Particle.slopes.x"];
+    double slopey = infod["6#Particle.slopes.y"];
+    double slopez = 1/sqrt(slopex*slopex + slopey*slopey + 1.0);
+    double pz = slopez*p;
+    double mm = infod["1#Particle.measuredMass"];
+    std::cout<<p<<"  "<<pz<<"   "<<mm<<std::endl;
+    double X = infod["0#RecVertex.position.x"];
+    double Y = infod["1#RecVertex.position.y"];
+    double Z = infod["2#RecVertex.position.z"];
+    return Gaudi::XYZVector(X,Y,Z);
+}
+//=========================================================================
+StatusCode TopologicalTagging::getHltObjLHCbIDs(const LHCb::HltObjectSummary * sum, std::vector< LHCb::LHCbID > & AllIDs) const{
+    if (0==sum) return StatusCode::SUCCESS ;
+    if(sum->substructure().size()>0){
+        for ( SmartRefVector< LHCb::HltObjectSummary >::const_iterator s = sum->substructure().begin() ;
+                s != sum->substructure().end() ; ++s)
+            getHltObjLHCbIDs(*s,AllIDs);
+
+
+        const std::vector< LHCb::LHCbID > lIDs = sum->lhcbIDsFlattened();
+        AllIDs.insert(AllIDs.end(), lIDs.begin(), lIDs.end());
+
+
+    }else{
+
+        const std::vector< LHCb::LHCbID > lIDs = sum->lhcbIDsFlattened();
+        AllIDs.insert(AllIDs.end(), lIDs.begin(), lIDs.end());
+    }
+
+    return StatusCode::SUCCESS ;
+
+}
+
+StatusCode TopologicalTagging::getJetLHCbIDs(const LHCb::Particle* part, std::vector< LHCb::LHCbID > & AllIDs) const{
+
+    LHCb::Particle::ConstVector daus = part->daughtersVector();
+    if(daus.size() > 0){
+        for(LHCb::Particle::ConstVector::iterator idaus = daus.begin(); idaus != daus.end(); idaus++)
+            getJetLHCbIDs((*idaus),AllIDs);
+    }else{
+
+        const LHCb::ProtoParticle* proto = part->proto();
+        if(proto != NULL){
+            const LHCb::Track* track = proto->track();
+            if(track != NULL){
+                const std::vector< LHCb::LHCbID > lIDs = track->lhcbIDs();
+                AllIDs.insert(AllIDs.end(), lIDs.begin(), lIDs.end());
+            }
+
+        }
+
+    }
+
+    return StatusCode::SUCCESS ;
+
+
+}
---
+++ Phys/Phys/TopologicalTools/src/TopologicalTagging.h
@@ -0,0 +1,131 @@
+// $Id: TopologicalTagging.h,v 1.3 2007/06/18 11:27:44 jpalac Exp $
+// TopologicalTagging created 25.02.06
+//
+//  author: Cedric Potteraz
+//  Based on FilterDesktop
+
+#ifndef TOPOLOGICALTAGGING_H
+#define TOPOLOGICALTAGGING_H 1
+
+// ============================================================================
+// Include files
+// ============================================================================
+// ============================================================================
+// from Gaudi
+// ============================================================================
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiKernel/IAlgTool.h"
+#include "GaudiKernel/AlgFactory.h"
+// ============================================================================
+// DaVinci Kernel
+// ============================================================================
+#include "Kernel/DaVinciAlgorithm.h"
+// ============================================================================
+// Event
+// ============================================================================
+#include "Event/Particle.h"
+// ============================================================================
+// Event
+// ============================================================================
+#include "Event/RecHeader.h"
+
+#include "LoKi/ILoKiSvc.h"
+#include "LoKi/LoKi.h"
+
+
+#include "Kernel/ITriggerTisTos.h"
+#include "Kernel/IAccept.h"
+#include "Event/HltObjectSummary.h"
+#include "GaudiKernel/Vector3DTypes.h"
+#include "GaudiKernel/Vector4DTypes.h"
+using namespace LHCb ;
+using namespace std ;
+
+
+
+// ============================================================================
+// Main Algorithm
+// ============================================================================
+class TopologicalTagging :  public DaVinciAlgorithm
+{
+    friend class AlgFactory<TopologicalTagging> ;
+    public:
+    /** Standard constructor
+     *  @param name instance name
+     *  @param pSvc pointer to Service Locator
+     */
+    TopologicalTagging(const std::string& name,
+            ISvcLocator* pSvcLocator) ;
+
+
+    /// destructor
+    virtual ~TopologicalTagging( ){};
+    virtual StatusCode initialize ();
+
+    /** standard execution of the algorithm
+     *  @return status code
+     */
+    virtual StatusCode execute   () ;
+    private:
+    // the default constructor is disabled
+    TopologicalTagging () ;
+    // the copy constructor is disabled
+    TopologicalTagging ( const  TopologicalTagging& )  ;
+    // number of particle that have to verify the cut
+    TopologicalTagging operator=( const  TopologicalTagging& )  ;
+
+    protected:
+    // ==========================================================================
+    /// Write empty container if selection fails.
+    void writeEmptyKeyedContainers  ( ) const;
+    /// Write empty container if selection fails.
+    void writeEmptySharedContainers ( ) const;
+    // ==========================================================================
+    private:
+    // ==========================================================================
+    /// save (clone if needed) selected particles in TES
+    /// Success if number of saved particles == number saved
+    /// to TES.
+    /// Overwritten from DaVinciAlgorithm. Is called automatically.
+    virtual StatusCode _saveInTES () ;
+    /// Write empty containers if selection fails.
+    virtual void writeEmptyTESContainers() ;
+
+    private:
+    // ============================================================================
+    /// save (clone if needed) selected particles in TES
+    template <class PARTICLES>
+        StatusCode _save () const ;
+
+    private:
+
+    // name to retrieve tool
+    std::string m_tagToolName;
+
+    bool m_save_all;
+    bool m_save_addinfo;
+    int  m_start_addinfo;
+    std::string  m_SubPID;
+    double  m_cut;
+
+    std::vector<LHCb::Particle*>  i_markedParticles;
+
+
+
+    ITriggerTisTos* m_TriggerTisTosTool;
+    public:
+    std::string m_TLine; // algorithm mode - PtRel or IPSig
+    std::string m_tinputs;
+
+
+    private:
+    IDistanceCalculator *m_distCalc; // distance calculator
+    Gaudi::XYZVector getHltObjVtxPos (const LHCb::HltObjectSummary *obj) const;
+    //StatusCode printObject(const LHCb::HltObjectSummary* sum, std::string increment) const;
+    StatusCode getJetLHCbIDs(const LHCb::Particle* part, std::vector< LHCb::LHCbID > &AllIDs) const;
+    StatusCode getHltObjLHCbIDs(const LHCb::HltObjectSummary* sum, std::vector< LHCb::LHCbID > &AllIDs) const;
+    StatusCode printObject(const LHCb::HltObjectSummary* sum, std::string increment) const;
+};
+
+
+#endif // TOPOLOGICALTAGGING_H
