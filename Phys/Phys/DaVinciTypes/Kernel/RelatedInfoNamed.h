c238cac4bd221fe8083ab6b6b157d7ccd917d8e8
Anton Poluektov <anton.poluektov@cern.ch>
2014-08-23 19:53:13 +0000

    Add Bs2Mumu isolation variables
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@176746 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4fc16a349deeb791a958b45f9977207714570c45

--- Phys/Phys/DaVinciTypes/Kernel/RelatedInfoNamed.h
+++ Phys/Phys/DaVinciTypes/Kernel/RelatedInfoNamed.h
@@ -68,74 +68,92 @@ class RelatedInfoNamed : public LHCb::RelatedInfoMap {
       VTXISOBDTSOFTTHIRDVALUE  = VTXISOBDTHARDFIRSTVALUE + 5 ,
       TRKISOBDTFIRSTVALUE      = VTXISOBDTHARDFIRSTVALUE + 6 ,
       TRKISOBDTSECONDVALUE     = VTXISOBDTHARDFIRSTVALUE + 7 ,
-      TRKISOBDTTHIRDVALUE      = VTXISOBDTHARDFIRSTVALUE + 8 
-
+      TRKISOBDTTHIRDVALUE      = VTXISOBDTHARDFIRSTVALUE + 8 ,
+      
+      BSMUMUCDFISO           = TRKISOBDTTHIRDVALUE + 1,
+      BSMUMUOTHERBMAG        = BSMUMUCDFISO + 1,
+      BSMUMUOTHERBANGLE      = BSMUMUCDFISO + 2,
+      BSMUMUOTHERBBOOSTMAG   = BSMUMUCDFISO + 3,
+      BSMUMUOTHERBBOOSTANGLE = BSMUMUCDFISO + 4,
+      BSMUMUTRACKPLUSISO        = BSMUMUCDFISO + 5,
+      BSMUMUTRACKMINUSISO        = BSMUMUCDFISO + 6,
+      BSMUMUOTHERBTRACKS     = BSMUMUCDFISO + 7
+      
     };
 
     static std::string nameByIndex(int key) {
       switch (key) {
-        case RelatedInfoNamed::CONEANGLE : return "CONEANGLE"; 
-        case RelatedInfoNamed::CONEMULT  : return "CONEMULT"; 
-        case RelatedInfoNamed::CONEPX    : return "CONEPX"; 
-        case RelatedInfoNamed::CONEPY    : return "CONEPY"; 
-        case RelatedInfoNamed::CONEPZ    : return "CONEPZ"; 
-        case RelatedInfoNamed::CONEP     : return "CONEP"; 
-        case RelatedInfoNamed::CONEPT    : return "CONEPT"; 
-        case RelatedInfoNamed::CONEPXASYM    : return "CONEPXASYM"; 
-        case RelatedInfoNamed::CONEPYASYM    : return "CONEPYASYM"; 
-        case RelatedInfoNamed::CONEPZASYM    : return "CONEPZASYM"; 
-        case RelatedInfoNamed::CONEPASYM     : return "CONEPASYM"; 
-        case RelatedInfoNamed::CONEPTASYM    : return "CONEPTASYM"; 
-        case RelatedInfoNamed::CONEDELTAETA  : return "CONEDELTAETA"; 
-        case RelatedInfoNamed::CONEDELTAPHI  : return "CONEDELTAPHI"; 
-
-        case RelatedInfoNamed::EWCONEANGLE : return "EWCONEANGLE"; 
-        case RelatedInfoNamed::EWCONEMULT  : return "EWCONEMULT"; 
-        case RelatedInfoNamed::EWCONEPX    : return "EWCONEPX"; 
-        case RelatedInfoNamed::EWCONEPY    : return "EWCONEPY"; 
-        case RelatedInfoNamed::EWCONEPZ    : return "EWCONEPZ"; 
-        case RelatedInfoNamed::EWCONEVP    : return "EWCONEVP"; 
-        case RelatedInfoNamed::EWCONEVPT   : return "EWCONEVPT"; 
-        case RelatedInfoNamed::EWCONESP    : return "EWCONESP"; 
-        case RelatedInfoNamed::EWCONESPT   : return "EWCONESPT"; 
-        case RelatedInfoNamed::EWCONETP    : return "EWCONETP"; 
-        case RelatedInfoNamed::EWCONETPT   : return "EWCONETPT"; 
-        case RelatedInfoNamed::EWCONEMINPTE   : return "EWCONEMINPTE"; 
-        case RelatedInfoNamed::EWCONEMAXPTE   : return "EWCONEMAXPTE"; 
-        case RelatedInfoNamed::EWCONEMINPTMU  : return "EWCONEMINPTMU"; 
-        case RelatedInfoNamed::EWCONEMAXPTMU  : return "EWCONEMAXPTMU"; 
-        case RelatedInfoNamed::EWCONENMULT  : return "EWCONENMULT"; 
-        case RelatedInfoNamed::EWCONENPX    : return "EWCONENPX"; 
-        case RelatedInfoNamed::EWCONENPY    : return "EWCONENPY"; 
-        case RelatedInfoNamed::EWCONENPZ    : return "EWCONENPZ"; 
-        case RelatedInfoNamed::EWCONENVP    : return "EWCONENVP"; 
-        case RelatedInfoNamed::EWCONENVPT   : return "EWCONENVPT"; 
-        case RelatedInfoNamed::EWCONENSP    : return "EWCONENSP"; 
-        case RelatedInfoNamed::EWCONENSPT   : return "EWCONENSPT"; 
-
-        case RelatedInfoNamed::VTXISONUMVTX            : return "VTXISONUMVTX"; 
-        case RelatedInfoNamed::VTXISODCHI2ONETRACK     : return "VTXISODCHI2ONETRACK"; 
-        case RelatedInfoNamed::VTXISODCHI2MASSONETRACK : return "VTXISODCHI2MASSONETRACK"; 
-        case RelatedInfoNamed::VTXISODCHI2TWOTRACK     : return "VTXISODCHI2TWOTRACK";
-        case RelatedInfoNamed::VTXISODCHI2MASSTWOTRACK : return "VTXISODCHI2MASSTWOTRACK";
-
-        case RelatedInfoNamed::VTXISOBDTHARDFIRSTVALUE : return "VTXISOBDTHARDFIRSTVALUE" ;
-        case RelatedInfoNamed::VTXISOBDTHARDSECONDVALUE : return "VTXISOBDTHARDSECONDVALUE" ;
-        case RelatedInfoNamed::VTXISOBDTHARDTHIRDVALUE : return "VTXISOBDTHARDTHIRDVALUE" ;
-        case RelatedInfoNamed::VTXISOBDTSOFTFIRSTVALUE : return "VTXISOBDTSOFTFIRSTVALUE" ;
-        case RelatedInfoNamed::VTXISOBDTSOFTSECONDVALUE : return "VTXISOBDTSOFTSECONDVALUE" ;
-        case RelatedInfoNamed::VTXISOBDTSOFTTHIRDVALUE : return "VTXISOBDTSOFTTHIRDVALUE" ;
-        case RelatedInfoNamed::TRKISOBDTFIRSTVALUE : return "TRKISOBDTFIRSTVALUE" ;
-        case RelatedInfoNamed::TRKISOBDTSECONDVALUE : return "TRKISOBDTSECONDVALUE" ;
-        case RelatedInfoNamed::TRKISOBDTTHIRDVALUE : return "TRKISOBDTTHIRDVALUE" ;
-
+      case RelatedInfoNamed::CONEANGLE : return "CONEANGLE"; 
+      case RelatedInfoNamed::CONEMULT  : return "CONEMULT"; 
+      case RelatedInfoNamed::CONEPX    : return "CONEPX"; 
+      case RelatedInfoNamed::CONEPY    : return "CONEPY"; 
+      case RelatedInfoNamed::CONEPZ    : return "CONEPZ"; 
+      case RelatedInfoNamed::CONEP     : return "CONEP"; 
+      case RelatedInfoNamed::CONEPT    : return "CONEPT"; 
+      case RelatedInfoNamed::CONEPXASYM    : return "CONEPXASYM"; 
+      case RelatedInfoNamed::CONEPYASYM    : return "CONEPYASYM"; 
+      case RelatedInfoNamed::CONEPZASYM    : return "CONEPZASYM"; 
+      case RelatedInfoNamed::CONEPASYM     : return "CONEPASYM"; 
+      case RelatedInfoNamed::CONEPTASYM    : return "CONEPTASYM"; 
+      case RelatedInfoNamed::CONEDELTAETA  : return "CONEDELTAETA"; 
+      case RelatedInfoNamed::CONEDELTAPHI  : return "CONEDELTAPHI"; 
+	
+      case RelatedInfoNamed::EWCONEANGLE : return "EWCONEANGLE"; 
+      case RelatedInfoNamed::EWCONEMULT  : return "EWCONEMULT"; 
+      case RelatedInfoNamed::EWCONEPX    : return "EWCONEPX"; 
+      case RelatedInfoNamed::EWCONEPY    : return "EWCONEPY"; 
+      case RelatedInfoNamed::EWCONEPZ    : return "EWCONEPZ"; 
+      case RelatedInfoNamed::EWCONEVP    : return "EWCONEVP"; 
+      case RelatedInfoNamed::EWCONEVPT   : return "EWCONEVPT"; 
+      case RelatedInfoNamed::EWCONESP    : return "EWCONESP"; 
+      case RelatedInfoNamed::EWCONESPT   : return "EWCONESPT"; 
+      case RelatedInfoNamed::EWCONETP    : return "EWCONETP"; 
+      case RelatedInfoNamed::EWCONETPT   : return "EWCONETPT"; 
+      case RelatedInfoNamed::EWCONEMINPTE   : return "EWCONEMINPTE"; 
+      case RelatedInfoNamed::EWCONEMAXPTE   : return "EWCONEMAXPTE"; 
+      case RelatedInfoNamed::EWCONEMINPTMU  : return "EWCONEMINPTMU"; 
+      case RelatedInfoNamed::EWCONEMAXPTMU  : return "EWCONEMAXPTMU"; 
+      case RelatedInfoNamed::EWCONENMULT  : return "EWCONENMULT"; 
+      case RelatedInfoNamed::EWCONENPX    : return "EWCONENPX"; 
+      case RelatedInfoNamed::EWCONENPY    : return "EWCONENPY"; 
+      case RelatedInfoNamed::EWCONENPZ    : return "EWCONENPZ"; 
+      case RelatedInfoNamed::EWCONENVP    : return "EWCONENVP"; 
+      case RelatedInfoNamed::EWCONENVPT   : return "EWCONENVPT"; 
+      case RelatedInfoNamed::EWCONENSP    : return "EWCONENSP"; 
+      case RelatedInfoNamed::EWCONENSPT   : return "EWCONENSPT"; 
+	
+      case RelatedInfoNamed::VTXISONUMVTX            : return "VTXISONUMVTX"; 
+      case RelatedInfoNamed::VTXISODCHI2ONETRACK     : return "VTXISODCHI2ONETRACK"; 
+      case RelatedInfoNamed::VTXISODCHI2MASSONETRACK : return "VTXISODCHI2MASSONETRACK"; 
+      case RelatedInfoNamed::VTXISODCHI2TWOTRACK     : return "VTXISODCHI2TWOTRACK";
+      case RelatedInfoNamed::VTXISODCHI2MASSTWOTRACK : return "VTXISODCHI2MASSTWOTRACK";
+	
+      case RelatedInfoNamed::VTXISOBDTHARDFIRSTVALUE : return "VTXISOBDTHARDFIRSTVALUE" ;
+      case RelatedInfoNamed::VTXISOBDTHARDSECONDVALUE : return "VTXISOBDTHARDSECONDVALUE" ;
+      case RelatedInfoNamed::VTXISOBDTHARDTHIRDVALUE : return "VTXISOBDTHARDTHIRDVALUE" ;
+      case RelatedInfoNamed::VTXISOBDTSOFTFIRSTVALUE : return "VTXISOBDTSOFTFIRSTVALUE" ;
+      case RelatedInfoNamed::VTXISOBDTSOFTSECONDVALUE : return "VTXISOBDTSOFTSECONDVALUE" ;
+      case RelatedInfoNamed::VTXISOBDTSOFTTHIRDVALUE : return "VTXISOBDTSOFTTHIRDVALUE" ;
+      case RelatedInfoNamed::TRKISOBDTFIRSTVALUE : return "TRKISOBDTFIRSTVALUE" ;
+      case RelatedInfoNamed::TRKISOBDTSECONDVALUE : return "TRKISOBDTSECONDVALUE" ;
+      case RelatedInfoNamed::TRKISOBDTTHIRDVALUE : return "TRKISOBDTTHIRDVALUE" ;
+	
+      case RelatedInfoNamed::BSMUMUCDFISO : return "BSMUMUCDFISO";
+      case RelatedInfoNamed::BSMUMUOTHERBMAG : return "BSMUMUOTHERBMAG";
+      case RelatedInfoNamed::BSMUMUOTHERBANGLE : return "BSMUMUOTHERBANGLE";
+      case RelatedInfoNamed::BSMUMUOTHERBBOOSTMAG : return "BSMUMUOTHERBBOOSTMAG";
+      case RelatedInfoNamed::BSMUMUOTHERBBOOSTANGLE : return "BSMUMUOTHERBBOOSTANGLE";
+      case RelatedInfoNamed::BSMUMUTRACKPLUSISO : return "BSMUMUTRACKPLUSISO"; 
+      case RelatedInfoNamed::BSMUMUTRACKMINUSISO : return "BSMUMUTRACKMINUSISO";
+      case RelatedInfoNamed::BSMUMUOTHERBTRACKS : return "BSMUMUOTHERBTRACKS";
+	
         default : return "UNKNOWN"; 
       }
     }
-
+    
     static short int indexByName(std::string name) {
       short i;
-      for (i=0; i<=RelatedInfoNamed::TRKISOBDTTHIRDVALUE; i++) {
+      for (i=0; i<=RelatedInfoNamed::BSMUMUOTHERBTRACKS; i++) {
         if ( name.compare(nameByIndex(i) ) == 0 ) return i; 
       }
       return RelatedInfoNamed::UNKNOWN; 
