d3053ebb388f93dc3e7be33fe2d4d0d706e7652d
Anton Poluektov <anton.poluektov@cern.ch>
2014-08-25 10:01:14 +0000

    Fix bug in ODIN argument
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@176815 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e09579741607ae3bb2da1e9bb62518576564b345

--- Stripping/Phys/StrippingConf/python/StrippingConf/StrippingLine.py
+++ Stripping/Phys/StrippingConf/python/StrippingConf/StrippingLine.py
@@ -392,12 +392,12 @@ class StrippingLine(object):
         if self._ODIN :
             if isinstance   ( self._ODIN , str   ) :
         	mdict.update( { 'ODIN'    : ODINFilter ( odinentryName ( line ) , Code = self._ODIN   )  } )
-            elif isinstance ( FILTER , ( tuple , list ) ) and 2 == len ( FILTER ) :
+            elif isinstance ( self._ODIN , ( tuple , list ) ) and 2 == len ( self._ODIN ) :
         	mdict.update( { 'ODIN'    : ODINFilter ( odinentryName ( line ) , Code = self._ODIN[0],  Preambulo = self._ODIN[1] )  } )
-            elif isinstance ( FILTER , dict     ) :
+            elif isinstance ( self._ODIN , dict     ) :
         	mdict.update( { 'ODIN'    : ODINFilter ( odinentryName ( line ) , **self._ODIN  )  } )
             else :
-    		raise TypeError, "Wrong ODIN attribute: %s " % FILTER
+    		raise TypeError, "Wrong ODIN attribute: %s " % self._ODIN
 
         if self._L0DU   : mdict.update( { 'L0DU'    : L0Filter   ( l0entryName   ( line ) , Code = self._L0DU   )  } )
         if self._HLT    : mdict.update( { 'HLT'     : HDRFilter  ( hltentryName  ( line ) , Code = self._HLT    ) } )
