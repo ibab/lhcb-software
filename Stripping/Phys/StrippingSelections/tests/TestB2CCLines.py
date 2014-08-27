873c82043348f9768515403aef932bb1535927e0
Carlos Vazquez Sierra <carlos.vazquez@cern.ch>
2014-08-27 23:27:10 +0000

    Adding TestB2CCLines.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@176961 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 17099a85d3f57649b190db3e7d60cd07f29cd1c3

---
+++ Stripping/Phys/StrippingSelections/tests/TestB2CCLines.py
@@ -0,0 +1,87 @@
+# Test all B2CC lines.
+# Carlos Vazquez Sierra (carlos.vazquez@cern.ch)
+
+
+from Gaudi.Configuration import *
+from Configurables import DaVinci
+from StrippingConf.Configuration import StrippingConf
+
+# Tighten Trk Chi2 to <3
+from CommonParticles.Utils import DefaultTrackingCuts
+DefaultTrackingCuts().Cuts  = { "Chi2Cut" : [ 0, 3 ],
+                                "CloneDistCut" : [5000, 9e+99 ] }
+
+# Now build the stream
+from StrippingConf.StrippingStream import StrippingStream
+streamBetaS = StrippingStream("Test_B2JpsiXforBeta_s")
+streamPsi2S = StrippingStream("Test_B2Psi2SX")
+streamPsi2SMuMu = StrippingStream("Test_B2Psi2SMuMu")
+streamJpsieePhi = StrippingStream("Test_Bs2JpsieePhi")
+streamCharmonium = StrippingStream("Test_B2CharmoniumX_6H")
+streamEtacPhiBDT = StrippingStream("Test_Bs2EtacPhiBDT")
+
+# Append the lines
+from StrippingSelections import buildersConf
+confs = buildersConf()
+from StrippingSelections.Utils import lineBuilder
+
+builderBetaS = lineBuilder(confs,'BetaS') #StrippingB2JpsiXforBeta_s.py
+builderPsi2S = lineBuilder(confs, 'BetaSPsi2S') #StrippingB2Psi2SX.py
+builderPsi2SMuMu = lineBuilder(confs, 'BetaSPsi2SMuMu') #StrippingB2Psi2SXMuMu.py
+builderBs2JpsieePhi = lineBuilder(confs, 'BetaSBs2JpsieePhi') #StrippingBs2JpsieePhi.py
+builderB2CharmoniumX = lineBuilder(confs, 'B2CharmoniumX_6H') #StrippingB2CharmoniumX_6H.py
+builderEtacPhiBDT = lineBuilder(confs, 'Bs2EtacPhiBDT') #StrippingBs2EtacPhiBDT.py
+
+streamBetaS.appendLines( builderBetaS.lines() )
+streamPsi2S.appendLines( builderPsi2S.lines() )
+streamPsi2SMuMu.appendLines( builderPsi2SMuMu.lines() )
+streamJpsieePhi.appendLines( builderBs2JpsieePhi.lines() )
+streamCharmonium.appendLines( builderB2CharmoniumX.lines() )
+streamEtacPhiBDT.appendLines( builderEtacPhiBDT.lines() )
+
+# Standard configuration of Stripping, do NOT change them
+from Configurables import  ProcStatusCheck
+filterBadEvents =  ProcStatusCheck()
+
+sc = StrippingConf( Streams = [ streamBetaS, streamPsi2S, streamPsi2SMuMu, streamJpsieePhi, streamCharmonium, streamEtacPhiBDT ],
+                    MaxCandidates = 2000,
+                    AcceptBadEvents = False,
+                    BadEventSelection = filterBadEvents,
+                    TESPrefix = 'Strip'
+                    )
+
+from Configurables import AuditorSvc, ChronoAuditor
+AuditorSvc().Auditors.append( ChronoAuditor("Chrono") )
+
+from Configurables import StrippingReport
+sr = StrippingReport(Selections = sc.selections())
+
+from Configurables import AlgorithmCorrelationsAlg
+ac = AlgorithmCorrelationsAlg(Algorithms = sc.selections())
+
+DaVinci().HistogramFile = 'DV_stripping_histos.root'
+DaVinci().EvtMax = 100000
+DaVinci().PrintFreq = 10000
+DaVinci().appendToMainSequence( [ sc.sequence() ] )
+DaVinci().appendToMainSequence( [ sr ] )
+DaVinci().appendToMainSequence( [ ac ] )
+DaVinci().DataType  = "2012"
+#DaVinci().DataType = "2011"
+DaVinci().InputType = "DST"
+
+# change the column size of timing table
+from Configurables import TimingAuditor, SequencerTimerTool
+TimingAuditor().addTool(SequencerTimerTool,name="TIMER")
+TimingAuditor().TIMER.NameSize = 60
+
+MessageSvc().Format = "% F%60W%S%7W%R%T %0W%M"
+
+# database
+DaVinci().DDDBtag  = "dddb-20120831" # 2012
+DaVinci().CondDBtag = "cond-20121008" # 2012
+#DaVinci().CondDBtag = "cond-20121025" # 2011
+
+# input file
+importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_Run125113.py") # 2012
+#importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_2011Data_MagDn_New.py") # MagDown sample, 2011
+#importOptions("$STRIPPINGSELECTIONSROOT/tests/data/Reco14_2011Data_MagUp_New.py") # MagUp sample, 2011
