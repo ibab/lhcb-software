1f416932c3be0755d13364e7d6fedd31623d9231
Ronan James Wallace <ronan.wallace@cern.ch>
2014-08-31 16:01:32 +0000

    Modified StrippingLLP2MuX
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@177137 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 639d9ca933dddad440516e3bec8657ff5bbfdb3d

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingLLP2MuX.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingLLP2MuX.py
@@ -3,7 +3,7 @@ Configuration for the high-PT displaced muon line (for exotic long-lived particl
 """
 __author__ = "Pieter David"
 __date__ = "2014-08-28"
-__all__ = ("LLP2MuX", "default_config")
+__all__ = ("LLP2MuXConf", "default_config")
 
 from LHCbKernel.Configuration import *
 from GaudiKernel import SystemOfUnits as units
@@ -14,11 +14,17 @@ from StrippingUtils.Utils import LineBuilder
 
 from GaudiConfUtils.ConfigurableGenerators import FilterDesktop, CombineParticles
 
-default_config = { "MinPT"  : 12.00*units.GeV
-                 , "MinIP"  :  0.25*units.mm
-                 }
+default_config= {
+    'NAME'        : 'LLP2MuX',
+    'WGs'         : ['QEE'],
+    'STREAMS'         : ['EW'],
+    'BUILDERTYPE' : 'LLP2MuXConf',
+    'CONFIG'      : { "MinPT"  : 12.00*units.GeV
+                     ,"MinIP"  :  0.25*units.mm
+                    }
+}
 
-class LLP2MuX(LineBuilder):
+class LLP2MuXConf(LineBuilder):
     """
     """
     __configuration_keys__ = ("MinPT", "MinIP")
