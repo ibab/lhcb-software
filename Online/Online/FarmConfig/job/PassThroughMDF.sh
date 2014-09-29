ff57ec78ed8c104d83832c72dd3872f7c16c57c7
Markus Frank <frankb@cern.ch>
2014-09-29 08:05:54 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@178052 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 067cf99a8512c370baf840910069ce7108b37852

---
+++ Online/Online/FarmConfig/job/PassThroughMDF.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# =========================================================================
+#
+#  Default script to start the passthrough process on a farm node.
+#
+#  Author   M.Frank
+#  Version: 1.0
+#  Date:    20/05/2013
+#
+# =========================================================================
+. ./PassThrough.sh $*
