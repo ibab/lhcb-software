451950f9b5cf51a9cb2b3707ed4e89f2aaa0982c
Maurizio Martinelli <maurizio.martinelli@cern.ch>
2014-10-02 14:36:57 +0000

    moved IAlignUpdateTool from TAlignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@178380 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a681ba39ec34f7f20f1674d3b3ec527d52dac0e0

--- Alignment/Alignment/AlignmentInterfaces/dict/AlignmentInterfacesDict.h
+++ Alignment/Alignment/AlignmentInterfaces/dict/AlignmentInterfacesDict.h
@@ -2,6 +2,7 @@
 #define DICT_ALIGNMENTINTERFACESDICT_H  1
 
 #include "AlignmentInterfaces/IAlignSolvTool.h"
+#include "AlignmentInterfaces/IAlignUpdateTool.h"
 #include "AlignmentInterfaces/IATrackSelectorTool.h"
 #include "AlignmentInterfaces/IDerivatives.h"
 #include "AlignmentInterfaces/ITAConfigTool.h"
