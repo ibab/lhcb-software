c80a58da195ce3d68fff28109aa57b5ed6e3eb7f
Markus Frank <frankb@cern.ch>
2014-10-03 17:38:18 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@178430 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 67589c2e96525b89194d67dba7a628d182689a2c

--- Online/Online/FarmConfig/job/MEPInjector_ReaderSvc.sh
+++ Online/Online/FarmConfig/job/MEPInjector_ReaderSvc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export UTGID=${1};
-. /group/online/dataflow/scripts/preamble.sh
-export HOST=$(hostname --short | awk '{ print toupper($1) }')
+. /group/online/dataflow/scripts/preamble.sh;
+export HOST=$(hostname --short | awk '{ print toupper($1) }');
+export READER_OPTS=`python -c "s='${UTGID}';print 'MEPInjector_'+s[s.find('_')+1:s.rfind('_')]+'.opts'"`;
 #
+echo "+++ Reader options:${READER_OPTS}";
 exec -a ${UTGID}  ${gaudi_exe} -tasktype=LHCb::Class2Task \
     -main=${GAUDIONLINEROOT}/options/Main.opts \
-    -opt=${FARMCONFIGROOT}/options/MEPInjector_ReaderSvc0.opts;
+    -opt=${FARMCONFIGROOT}/options/${READER_OPTS};
