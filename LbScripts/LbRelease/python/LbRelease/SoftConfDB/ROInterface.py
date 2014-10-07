6341838f091eff5861805f6c613341ad0e06f730
Ben Couturier <lben@cern.ch>
2014-10-07 14:35:06 +0000

    LBCORE-591 Added read-only interface to the SoftConfDB
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@178550 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 27663e5a1d8b0b7833b115ed276820e6d4921825

---
+++ LbScripts/LbRelease/python/LbRelease/SoftConfDB/ROInterface.py
@@ -0,0 +1,56 @@
+'''
+
+RO methods interfacing to the LHCb Software Configuration DB in Neo4j.
+
+Created on Oct 7, 2014
+@author: Ben Couturier
+'''
+
+import inspect
+import sys
+from SoftConfDB import SoftConfDB
+
+def initModule(moduleName, objclass, obj, methodList):
+    """ Add methods to  module """
+    # Locate module
+    module = sys.modules[moduleName]
+    
+    # Now iterate over the methods and add them to the module
+    allmethods =  list(inspect.getmembers(objclass, predicate=inspect.ismethod))
+    for method in allmethods:
+        methodName, methodObj = method
+
+        # Check if it's one of those we want to publish
+        if methodName in methodList:
+            func = getattr(obj, methodName)
+            setattr(module, methodName, func)
+
+ROMethods =['listProjects',
+            'listDatapkgs',
+            'listApplications',
+            'listReleaseReqs',
+            'listCMakeBuiltProjects',
+            'listReleaseStackFromPV',
+            'listReleaseStackTops',
+            'listReleaseStacks',
+            'listVersions',
+            'listDatapkgVersions',
+            'listStackPlatforms',
+            'listStackPlatformsToRelease',
+            'listPlatforms',
+            'listDependencies',
+            'listReferences',
+            'listActiveReferences',
+            'show',
+            'listActive',
+            'listActiveApplications',
+            'listUsed',
+            'checkUnused' ]
+
+
+confDB = SoftConfDB()
+initModule(__name__, SoftConfDB, confDB, ROMethods)
+
+
+
+
