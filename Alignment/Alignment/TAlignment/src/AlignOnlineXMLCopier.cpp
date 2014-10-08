ccc7a78ba25f69b186fa99e4a49606220c8de8ce
Wouter Hulsbergen <wouterh@nikhef.nl>
2014-10-08 10:29:15 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@178599 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 25d4cb8bc8d50350f32ee987874b142f7e951bf6

---
+++ Alignment/Alignment/TAlignment/src/AlignOnlineXMLCopier.cpp
@@ -0,0 +1,63 @@
+#include "AlignOnlineXMLCopier.h"
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+
+AlignOnlineXMLCopier::AlignOnlineXMLCopier( const std::string& onlinedir,
+					    const std::string& aligndir,
+					    const std::string& condname) 
+  : m_condname(condname),
+    m_onlinedir(onlinedir),
+    m_aligndir(aligndir)
+{}
+
+std::string AlignOnlineXMLCopier::onlinefilename( FileVersion v ) const 
+{
+  return m_onlinedir + "/" + m_condname + "/v" + boost::lexical_cast<std::string>( v ) + ".xml" ;
+}
+
+std::string AlignOnlineXMLCopier::alignfilename() const
+{
+  return m_aligndir + "/" + m_condname + "/current.xml" ;
+}
+
+StatusCode AlignOnlineXMLCopier::copyFromOnlineArea()
+{
+  StatusCode sc = StatusCode::SUCCESS ;
+  FileVersion v = 0 ;
+  while(1) {
+    // check that the file exists
+    boost::filesystem::path fn(onlinefilename( v )) ;
+    if( boost::filesystem::exists(fn) ) ++v ;
+    else break ;
+  }
+  if( v==0 ) {
+    sc = StatusCode::FAILURE ;
+  } else {
+    m_version = v-1 ;
+    boost::filesystem::path origin(onlinefilename(m_version)) ;
+    boost::filesystem::path target(alignfilename()) ;
+    boost::filesystem::copy_file(origin,target, boost::filesystem::copy_option::overwrite_if_exists) ;
+    if( boost::filesystem::exists(target) ){
+      m_time = boost::filesystem::last_write_time(target) ;
+    } else {
+      sc = StatusCode::FAILURE ;
+    }
+  }
+  return sc ;
+}
+
+StatusCode AlignOnlineXMLCopier::copyToOnlineArea() const
+{
+  StatusCode sc= StatusCode::SUCCESS ;
+  boost::filesystem::path origin(alignfilename()) ;
+  if( boost::filesystem::exists(origin) &&
+      boost::filesystem::last_write_time(origin) > m_time ) {
+    boost::filesystem::path target(onlinefilename(m_version+1)) ;
+    boost::filesystem::copy( origin, target ) ;
+    if( !boost::filesystem::exists(target) ) {
+      sc = StatusCode::FAILURE ;
+    }
+  }
+  return sc ;
+}
+
