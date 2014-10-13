da504a7b9ad3b6d51f026c0de324c49ba0e5fb5a
Beat Jost <beat.jost@cern.ch>
2014-10-13 15:17:18 +0000

    Fix Path for conditions
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@178752 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: db8f54f12a02b147c1c76a444f0bff8d4e9fb704

--- Alignment/Alignment/Escher/python/Escher/Configuration.py
+++ Alignment/Alignment/Escher/python/Escher/Configuration.py
@@ -25,15 +25,15 @@ class Escher(LHCbConfigurableUser):
     __used_configurables__ = [ TrackSys, RecSysConf, RecMoniConf, LHCbApp, DstConf, TAlignment, DecodeRawEvent ]
     ## Default main sequences for real and simulated data
     DefaultSequence = [ #CountingPrescaler("EscherPrescaler")
-                        #, 
+                        #,
                         "ProcessPhase/Init"
                         , GaudiSequencer("HltFilterSeq")
 			, "ProcessPhase/Reco"
 			, "ProcessPhase/Moni"
-                        , GaudiSequencer("AlignSequence") 
+                        , GaudiSequencer("AlignSequence")
 			]
 
-    
+
     # Steering options
     __slots__ = {
          "EvtMax"		: -1       # Maximum number of events to process
@@ -46,7 +46,7 @@ class Escher(LHCbConfigurableUser):
        , "OutputType"		: "NONE"   # or "RDST" or "NONE". Also forwarded to RecSys
        , "Persistency"          : None     # POOL or ROOT foraward to LHCbApp
        , "PackType"		: "TES"    # Flag whether or not to use packed containers
-       , "NoWarnings"		: False    # suppress all messages with MSG::WARNING or below 
+       , "NoWarnings"		: False    # suppress all messages with MSG::WARNING or below
        , "DatasetName"		: ""       # string used to build file names
        , "DDDBtag"		: ""       # Tag for DDDB. Default as set in DDDBConf for DataType
        , "CondDBtag"		: ""       # Tag for CondDB. Default as set in DDDBConf for DataType
@@ -56,7 +56,7 @@ class Escher(LHCbConfigurableUser):
        , "AlignSequence"	: []
        , "Kalman" 		: True    # run the kalman filter type alignment
        , "Millepede"		: False    # run the Millepede type alignment
-       , "OutputLevel" 		: 3        # 
+       , "OutputLevel" 		: 3        #
        , "Incident"     	:  ""      # for Millepede style alignment, there are two incident handles: GlobalMPedeFit and Converged
                                            # for Kalman style alignment, there is a handle: UpdateConstants.
         # Following are options forwarded to RecSys
@@ -77,14 +77,14 @@ class Escher(LHCbConfigurableUser):
         # DIGI is always simulation, as is usage of MC truth!
         if self.getProp( "WithMC" ) or self.getProp( "InputType" ).upper() == 'DIGI':
             self.setProp( "Simulation", True )
-        
+
         # Delegate handling to LHCbApp configurable
         self.setOtherProps(LHCbApp(),["DataType","CondDBtag","DDDBtag","Simulation"])
         # specify the use of the oracle database
         if self.getProp("UseOracle"):
             from Configurables import CondDB
             CondDB(UseOracle = True)
-        
+
     def defineEvents(self):
         # Delegate handling to LHCbApp configurable
         self.setOtherProps(LHCbApp(),["EvtMax","SkipEvents"])
@@ -103,16 +103,16 @@ class Escher(LHCbConfigurableUser):
         self.configureSequences( )
 
         self.configurePersistency()
-        
+
         self.configureInput( inputType )
-        
+
         self.configureOutput( outputType )
-        
+
         # ROOT persistency for histograms
         importOptions('$STDOPTS/RootHist.opts')
         from Configurables import RootHistCnv__PersSvc
         RootHistCnv__PersSvc('RootHistCnv').ForceAlphaIds = True
-        
+
         # Use a default histogram file name if not already set
         if not hasattr( HistogramPersistencySvc(), "OutputFile" ):
             histosName   = self.getProp("DatasetName")
@@ -140,14 +140,14 @@ class Escher(LHCbConfigurableUser):
 
         # set up the HltFilterSeq
         from Configurables import HltCompositionMonitor
-        from Configurables import LoKi__HDRFilter as HDRFilter 
+        from Configurables import LoKi__HDRFilter as HDRFilter
         hltFilterSeq = GaudiSequencer( "HltFilterSeq" )
         # identifies events that are not of type Hlt1ErrorEvent or Hlt2ErrorEvent
         hltErrCode = "HLT_PASS_RE('Hlt1(?!ErrorEvent).*Decision') & HLT_PASS_RE('Hlt2(?!ErrorEvent).*Decision')"
         hltErrorFilter = HDRFilter('HltErrorFilter', Code = hltErrCode )   # the filter
-        
+
         ## TODO: Watch out for the HLT1/2 Split here!!
-        ## probably the decoder DB will take care of it for you... 
+        ## probably the decoder DB will take care of it for you...
         from DAQSys.Decoders import DecoderDB
         from DAQSys.DecoderClass import decodersForBank
         hltdecs=decodersForBank(DecoderDB,"HltDecReports")
@@ -169,9 +169,9 @@ class Escher(LHCbConfigurableUser):
         # so that e.g. the track container(s) is unpacked:
         if not GaudiSequencer("RecoTrSeq").getProp("Enable"):
 	    DstConf( EnableUnpack = True )
-            
+
         TrackSys().ExpertTracking = self.getProp("ExpertTracking")
-   
+
         ta = TAlignment()
         ta.Sequencer = alignSeq
         if  self.getProp("Millepede") :
@@ -179,24 +179,24 @@ class Escher(LHCbConfigurableUser):
             self.setProp("Incident", "GlobalMPedeFit")
             ta.Method = "Millepede"
             ta.Sequencer = GaudiSequencer("MpedeAlignSeq")
-        
+
     def configurePersistency(self):
         if hasattr(self,"Persistency"):
             if self.getProp("Persistency") is not None:
                 LHCbApp().setProp("Persistency", self.getProp("Persistency"))
-    
+
     def configureInput(self, inputType):
         """
         Tune initialisation according to input type
         """
-        
+
         # POOL Persistency, now in LHCbApp
         #importOptions("$GAUDIPOOLDBROOT/options/GaudiPoolDbRoot.opts")
-        
+
         # By default, Escher only needs to open one input file at a time
         # Only set to zero if not previously set to something else.
         if not IODataManager().isPropertySet("AgeLimit") : IODataManager().AgeLimit = 0
-        
+
         if inputType in [ "XDST", "DST", "RDST", "ETC" ]:
             # Kill knowledge of any previous Brunel processing
             from Configurables import ( TESCheck, EventNodeKiller )
@@ -207,19 +207,19 @@ class Escher(LHCbConfigurableUser):
                 TESCheck().Inputs = ["Link/Rec/Track/Best"]
             InitReprocSeq.Members.append( "EventNodeKiller" )
             EventNodeKiller().Nodes = [ "pRec", "Rec", "Raw", "Link/Rec" ]
-        
+
         if inputType == "ETC":
             raise DeprecationWarning, "ETC are no longer supported by LHCb software"
             from Configurables import  TagCollectionSvc
             ApplicationMgr().ExtSvc  += [ TagCollectionSvc("EvtTupleSvc") ]
             # Read ETC selection results into TES for writing to DST
             IODataManager().AgeLimit += 1
-        
+
         #if inputType in [ "MDF", "RDST", "ETC" ]:
         #    # In case raw data resides in MDF file
         #    EventPersistencySvc().CnvServices.append("LHCb::RawDataCnvSvc")
         DecodeRawEvent()
-        
+
         if self.getProp("UseFileStager") :
             import os, getpass
             from FileStager.Configuration import configureFileStager
@@ -240,29 +240,29 @@ class Escher(LHCbConfigurableUser):
             if hasattr(self,"Persistency"):
                 if self.getProp("Persistency") is not None:
                     DstConf().Persistency=self.getProp("Persistency")
-            
+
             writerName = "DstWriter"
             packType  = self.getProp( "PackType" )
             # Do not pack DC06 DSTs, for consistency with existing productions
             if self.getProp("DataType") == "DC06": packType = "NONE"
-            
+
             dstWriter = OutputStream( writerName )
             dstWriter.RequireAlgs += ["Reco"] # Write only if Rec phase completed
-            
+
             # Set a default output file name if not already defined in the user job options
             if not hasattr( dstWriter, "Output" ):
                 DstConf().OutputName = self.outputName()
-            
+
             # Define the file content
             DstConf().Writer     = writerName
             DstConf().DstType    = dstType
             DstConf().PackType   = packType
             DstConf().setProp("WriteFSR", self.getProp("WriteFSR"))
-            
+
             if hasattr(self,"Persistency"):
                 if self.getProp("Persistency") is not None:
                     DstConf().setProp("Persistency", self.getProp("Persistency"))
-            
+
             from Configurables import TrackToDST
             if dstType == "DST":
                 # Sequence for altering DST content
@@ -279,16 +279,16 @@ class Escher(LHCbConfigurableUser):
                 trackFilter.TTrackStates = ["FirstMeasurement"]
                 trackFilter.downstreamStates = ["FirstMeasurement"]
                 trackFilter.upstreamStates = ["ClosestToBeam"]
-            
+
             GaudiSequencer("OutputDSTSeq").Members += [ trackFilter ]
-            
+
             if packType != "NONE":
                 # Add the sequence to pack the DST containers
                 packSeq = GaudiSequencer("PackDST")
                 DstConf().PackSequencer = packSeq
                 DstConf().AlwaysCreate  = True
                 GaudiSequencer("OutputDSTSeq").Members += [ packSeq ]
-                
+
         # Always write an ETC if ETC input
         if self.getProp( "InputType" ).upper() == "ETC":
             raise DeprecationWarning, "ETC are no longer supported by LHCb"
@@ -298,14 +298,14 @@ class Escher(LHCbConfigurableUser):
             importOptions( "$ESCHEROPTS/DefineETC.opts" )
             if not hasattr( etcWriter, "Output" ):
                etcWriter.Output = [ "EVTTAGS2 DATAFILE='" + self.getProp("DatasetName") + "-etc.root' TYP='POOL_ROOTTREE' OPT='RECREATE' " ]
-        
+
         # Do not print event number at every event (done already by Brunel)
         EventSelector().PrintFreq = -1
         CountingPrescaler("EscherPrescaler").PrintFreq = self.getProp( "PrintFreq" )
         # Modify printout defaults
         if self.getProp( "NoWarnings" ):
             importOptions( "$ESCHEROPTS/SuppressWarnings.opts" )
-        
+
     def outputName(self):
         """
         Build a name for the output file, based in input options
@@ -324,7 +324,7 @@ class Escher(LHCbConfigurableUser):
         """
         tag = { "DDDB":     self.getProp('DDDBtag')
                 , "LHCBCOND": self.getProp('CondDBtag')
-                , "SIMCOND" : self.getProp('CondDBtag') 
+                , "SIMCOND" : self.getProp('CondDBtag')
                 , "ONLINE"  : 'fake'
                 }
 
@@ -347,35 +347,35 @@ class Escher(LHCbConfigurableUser):
         rch = RunChangeHandlerSvc()
         ApplicationMgr().ExtSvc.append(rch)
         conditionmap = {
-            path + 'Velo/VeloGlobal.xml'  : [ 
+            path + 'Velo/VeloGlobal/current.xml'  : [
                 'Conditions/Alignment/Velo/VeloSystem',
                 'Conditions/Alignment/Velo/VeloRight',
                 'Conditions/Alignment/Velo/VeloLeft' ],
-            path + 'Velo/VeloModules.xml' : [] 
+            path + 'Velo/VeloModules/current.xml' : []
                      + [ 'Conditions/Alignment/Velo/Module%02d'%i for i in range(0,42) ]
                      + [ 'Conditions/Alignment/Velo/Detector%02d-%02d'%(i,(1+i/2)%2) for i in range(0,42)  ] ,
-            path + 'IT/ITGlobal.xml' : []
+            path + 'IT/ITGlobal/current.xml' : []
                      + [ 'Conditions/Alignment/IT/ITSystem' ]
                      + [ 'Conditions/Alignment/IT/ITT%d' % i for i in range(1,4) ]
                      + [ 'Conditions/Alignment/IT/ITT%d%sBox' % (i,b) for i in range(1,4) for b in ['Top','Bottom','ASide','CSide' ] ] ,
-            path + 'IT/ITModules.xml' : []
+            path + 'IT/ITModules/current.xml' : []
                      + [ 'Conditions/Alignment/IT/ITT%d%sLayer%s' % (i,b,l) for i in range(1,4) for b in ['Top','Bottom','ASide','CSide' ] for l in ['X1','U','V','X2' ] ]
                      + [ 'Conditions/Alignment/IT/ITT%d%sLayer%sLadder%d' % (i,b,l,a) for i in range(1,4) for b in ['Top','Bottom','ASide','CSide' ] for l in ['X1','U','V','X2' ] for a in range(1,8) ] ,
         #         + [ 'Conditions/Alignment/IT/ITT%d%sLayer%sLadder%dSector' % (i,b,l,a) for i in range(1,4) for b in ['Top','Bottom','ASide','CSide' ] for l in ['X1','U','V','X2' ] for a in range(1,8) ]
         #            + [ 'Conditions/Alignment/IT/ITT%d%sLayer%sLadder%dSector_Sensor1' % (i,b,l,a) for i in range(1,4) for b in ['Top','Bottom','ASide','CSide' ] for l in ['X1','U','V','X2' ] for a in range(1,8) ]
         #            + [ 'Conditions/Alignment/IT/ITT%d%sLayer%sLadder%dSector_Sensor2' % (i,b,l,a) for i in range(1,4) for b in ['ASide','CSide' ] for l in ['X1','U','V','X2' ] for a in range(1,8) ] ,
-            path + 'OT/OTGlobal.xml' : []
+            path + 'OT/OTGlobal/current.xml' : []
                      + [ 'Conditions/Alignment/OT/OTSystem' ]
                      + [ 'Conditions/Alignment/OT/T%d' %i for i in range(1,4) ]
                      + [ 'Conditions/Alignment/OT/T%d%s' % (i,l) for i in range(1,4) for l in ['X1','U','V','X2' ] ]
                      + [ 'Conditions/Alignment/OT/T%d%sQ%d' % (i,l,q) for i in range(1,4) for l in ['X1','U','V','X2' ] for q in range(0,4) ] ,
-            path + 'OT/OTModules.xml' : []
+            path + 'OT/OTModules/current.xml' : []
                      + [ 'Conditions/Alignment/OT/T%d%sQ%dM%d' % (i,l,q,m) for i in range(1,4) for l in ['X1','U','V','X2' ] for q in range(0,4) for m in range(1,10) ] ,
-            path + 'TT/TTGlobal.xml' : []
+            path + 'TT/TTGlobal/current.xml' : []
                      + [ 'Conditions/Alignment/TT/TTSystem' ]
                      + [ 'Conditions/Alignment/TT/TT%s' % i for i in ['a','b' ] ]
                      + [ 'Conditions/Alignment/TT/TT%sLayer' % (l) for l in ['aX','aU','bV','bX' ] ],
-            path + 'TT/TTModules.xml' : []
+            path + 'TT/TTModules/current.xml' : []
                      + [ 'Conditions/Alignment/TT/TT%sLayerR%dModule%d%s' % (l,r,m,w) for w in ['T','B'] for l in ['aX','aU','bV','bX'] for r in range(1,4) for m in range(1,4)]
                      + [ 'Conditions/Alignment/TT/TT%sLayerR%dModule%d%s' % (l,r,m,w) for w in ['T','B'] for l in ['bV','bX']           for r in range(1,4) for m in range(4,6)]
                      + [ 'Conditions/Alignment/TT/TT%sLayerR%dModule%d%s' % (l,r,m,w) for w in ['T','B'] for l in ['aX','aU','bV','bX'] for r in [1,3]      for m in range(6,7)]
@@ -383,13 +383,14 @@ class Escher(LHCbConfigurableUser):
         }
 
         rch.Conditions = dict( (c,f) for f,cs in conditionmap.iteritems() for c in cs )
-                
+#         print rch
+
         from Configurables import MagneticFieldSvc
         MagneticFieldSvc().UseSetCurrent = True
 
     ## Apply the configuration
     def __apply_configuration__(self):
-        
+
         GaudiKernel.ProcessJobOptions.PrintOff()
         GaudiKernel.ProcessJobOptions.PrintOn()
         log.info("Initializing sequences!")
@@ -416,7 +417,7 @@ class Escher(LHCbConfigurableUser):
         from Configurables import (ApplicationMgr,AuditorSvc,SequencerTimerTool)
         ApplicationMgr().ExtSvc += [ 'ToolSvc', 'AuditorSvc' ]
         ApplicationMgr().AuditAlgorithms = True
-        AuditorSvc().Auditors += [ 'TimingAuditor' ] 
+        AuditorSvc().Auditors += [ 'TimingAuditor' ]
         SequencerTimerTool().OutputLevel = 4
 
         log.info( self )
