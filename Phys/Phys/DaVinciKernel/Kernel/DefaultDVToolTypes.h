261f913caa89fa7b2117eae1d7128047df8eb22b
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2014-10-14 16:53:23 +0000

    Make the LoKi vertex fitter and propertime fitter the defaults.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@178880 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 84e01ed56ebfa610ef93b311da2eb0373789de2f

--- Phys/Phys/DaVinciKernel/Kernel/DefaultDVToolTypes.h
+++ Phys/Phys/DaVinciKernel/Kernel/DefaultDVToolTypes.h
@@ -8,10 +8,10 @@ namespace DaVinci
   namespace DefaultTools
   {
     static const std::string Distance         = "LoKi::DistanceCalculator:PUBLIC";
-    static const std::string VertexFitter     = "OfflineVertexFitter:PUBLIC";
-    static const std::string ParticleCombiner = "OfflineVertexFitter:PUBLIC";
+    static const std::string VertexFitter     = "LoKi::VertexFitter:PUBLIC";
+    static const std::string ParticleCombiner = "LoKi::VertexFitter:PUBLIC";
     static const std::string PVRelator = "GenericParticle2PVRelator<_p2PVWithIPChi2, OfflineDistanceCalculatorName>/P2PVWithIPChi2:PUBLIC";
-    static const std::string ParticleReFitter = "OfflineVertexFitter:PUBLIC";
+    static const std::string ParticleReFitter = "LoKi::VertexFitter:PUBLIC";
   }
 }
 
