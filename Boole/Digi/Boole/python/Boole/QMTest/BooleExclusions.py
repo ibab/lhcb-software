6a73f8816ecd00b2bf429e5bcd948e6d6cf57c21
Marco Cattaneo <marco.cattaneo@cern.ch>
2014-10-16 09:37:42 +0000

    Future proof upgrade tests against change in FTDet initialisation printout. Can be removed once we are on LHCb v38r3 stack
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@178964 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 81aebb390c05b88391e653712eba2ba646365a56

--- Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
+++ Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
@@ -1,8 +1,10 @@
-from GaudiTest import LineSkipper
+from GaudiTest import LineSkipper, RegexpReplacer
 from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 
 preprocessor = LHCbPreprocessor + \
     LineSkipper(["BooleInit.Boole...SUCCESS Exceptions/Errors/Warnings/Infos Statistics : 0/0/"]) + \
     LineSkipper(["SIMCOND_"]) + \
-    LineSkipper(["DDDB_"])
+    LineSkipper(["DDDB_"]) + \
+    RegexpReplacer(when = "DeFTDetector         INFO Current FT geometry version =   20",
+                 orig = r'Current FT geometry version =   20', repl = r'==> Initialize DeFTDetector')
  
