3e7283cee4f1e7d0294cf521c90915704e45c4b9
Alessandro Morda <alessandro.morda@cern.ch>
2014-10-27 16:06:42 +0000

    solved compilation error generated while solving conflict with rpevious version
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@179396 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cd399e007302b90632fa272972be05272578dd09

--- Stripping/Phys/IsolationTools/src/RelInfoBstautauZVisoBDT.cpp
+++ Stripping/Phys/IsolationTools/src/RelInfoBstautauZVisoBDT.cpp
@@ -286,43 +286,43 @@ StatusCode RelInfoBstautauZVisoBDT::calculateRelatedInfo( const LHCb::Particle* 
     }
     else if(part)
     {
-
-    
-     
-        StatusCode scInTracks =  Initialize_tracksVF_ZVtop(); //initialize all the tracks that will be used for VF and ZVtop. Then we call the VFiso and the ZVtop algorithm
-        StatusCode scIsoTopo  =  IsoTopo2Body(part);//, prefix, tuple);
-        StatusCode scGMPiso   =  GMPiso(part);//, prefix, tuple);
-        StatusCode scZViso    =  ZViso();
-        if ( msgLevel(MSG::DEBUG) ) debug()<<"ZVisoBDT_value : "<<m_ZViso<<endmsg;
-        if((!scIsoTopo)||(!scInTracks)||(!scGMPiso)||(!scZViso)) {return StatusCode::FAILURE;}	
-
-        Float_t P_charge = 0.0;
-        bool flag_OS = true;
-        bool flag_tau_mu = false;
-        for (LHCb::Particle::ConstVector::const_iterator i_daug_2 = Daughters.begin(); i_daug_2 != Daughters.end() ; i_daug_2++){
+      StatusCode scInTracks =  Initialize_tracksVF_ZVtop(); //initialize all the tracks that will be used for VF and ZVtop. Then we call the VFiso and the ZVtop algorithm
+      StatusCode scIsoTopo  =  IsoTopo2Body(part);//, prefix, tuple);
+      StatusCode scGMPiso   =  GMPiso(part);//, prefix, tuple);
+      StatusCode scZViso    =  ZViso();
+      if ( msgLevel(MSG::DEBUG) ) debug()<<"ZVisoBDT_value : "<<m_ZViso<<endmsg;
+      if((!scIsoTopo)||(!scInTracks)||(!scGMPiso)||(!scZViso)) {return StatusCode::FAILURE;}	
+      
+      Float_t P_charge = 0.0;
+      bool flag_OS = true;
+      bool flag_tau_mu = false;
+      for (LHCb::Particle::ConstVector::const_iterator i_daug_2 = Daughters.begin(); i_daug_2 != Daughters.end() ; i_daug_2++){
         const LHCb::Particle* part2 = *i_daug_2;
-	if(i_daug_2 !=i_daug){
+        if(i_daug_2 !=i_daug){
 	        if((part->charge())*(part2->charge())>0) flag_OS=false;
         	/*if((part2->p())>(part->p()))*/ P_charge = (part->p())-(part2->p());// false; 
-		if((part->isBasicParticle())||(part2->isBasicParticle())) flag_tau_mu=true;
+          if((part->isBasicParticle())||(part2->isBasicParticle())) flag_tau_mu=true;
         	if ( msgLevel(MSG::DEBUG) ) debug()<<"part2->p() : "<<part->p()<<"   ,    part2->p() : "<<part2->p()<<endmsg;
-	}
         }
-        if ( msgLevel(MSG::DEBUG) ) debug()<<"P_charge : "<<P_charge <<"   ,   flag_OS : "<<flag_OS<<"   ,    flag_tau_mu : "<<flag_tau_mu<<endmsg;
-
-        if((flag_OS==true)||(flag_tau_mu==true)){
-        if(part->charge()>0){m_ZViso_TauP=m_ZViso;}
-	if(part->charge()<0){m_ZViso_TauM=m_ZViso;}
-	}else if(flag_OS==false){// i.e. it's same sign
-	if(P_charge>0){m_ZViso_TauP=m_ZViso;}
-	else if(P_charge<0){m_ZViso_TauM=m_ZViso;}
-	}
-
-
-
       }
+      
+      if ( msgLevel(MSG::DEBUG) ) debug()<<"P_charge : "<<P_charge <<"   ,   flag_OS : "<<flag_OS<<"   ,    flag_tau_mu : "<<flag_tau_mu<<endmsg;
+      
+      if((flag_OS==true)||(flag_tau_mu==true)){
+        if(part->charge()>0){m_ZViso_TauP=m_ZViso;}
+        if(part->charge()<0){m_ZViso_TauM=m_ZViso;}
+      }else if(flag_OS==false){// i.e. it's same sign
+        if(P_charge>0){m_ZViso_TauP=m_ZViso;}
+        else if(P_charge<0){m_ZViso_TauM=m_ZViso;}
+      }
+
+      
+      
     }
   }
+
+  
+
 
 
 
