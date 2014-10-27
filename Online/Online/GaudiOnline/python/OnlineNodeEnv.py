8591b1a8ccf7d4f2e7ef62c911f6e973e806e225
Markus Frank <frankb@cern.ch>
2014-10-27 18:37:38 +0000

    Enhance OnlineEnv.py for HLT2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@179407 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: dbf67c0630d894273d9b4c209e875300d51e0c9d

---
+++ Online/Online/GaudiOnline/python/OnlineNodeEnv.py
@@ -0,0 +1,27 @@
+import sys, OnlineEnvBase
+nodeInfo = sys.modules['OnlineEnvBase']
+
+def load_node_info():
+  import os, imp, socket
+  dir_name = os.path.dirname(os.path.dirname(imp.find_module('OnlineEnvBase')[1])) + os.sep + 'Nodes'
+  mod_name = socket.gethostname().upper()+'_TriggerInfo.py'
+  nam,ext = os.path.splitext(mod_name)
+  if not sys.modules.has_key(nam):
+    if not dir_name in sys.path:
+      print '[DEBUG] add python path:'+dir_name
+      sys.path.insert(0,dir_name)
+    mod = imp.find_module(nam,None)
+    module=imp.load_module(nam,mod[0],mod[1],mod[2])
+    attrs = dir(module)
+    for i in attrs:
+      if len(i)>2 and i[:2] != '__':
+        setattr(nodeInfo,i,getattr(module,i))
+  return dir(sys.modules[nam])
+
+def printattrs(name):
+  print 50*'=',name,50*'='
+  mod = sys.modules[name]
+  attrs = dir(mod)
+  for i in attrs:
+    if len(i)>2 and i[:2] != '__':
+      print str(i)+'='+str(getattr(mod,i))
