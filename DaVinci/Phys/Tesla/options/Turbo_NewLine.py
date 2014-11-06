626ebbefe08b4974af2eb253f4b161099a4a928d
Sean Benson <sean.benson@cern.ch>
2014-11-06 13:07:29 +0000

    Turbo_NewLine.py to run on private TCK
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@179962 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a604e2732fbe3e75ca0e12372d7e1ccdce4b16b3

---
+++ DaVinci/Phys/Tesla/options/Turbo_NewLine.py
@@ -0,0 +1,21 @@
+from Configurables import Tesla 
+from Gaudi.Configuration import *
+
+from Configurables import RecombineRawEvent, DecodeRawEvent
+RecombineRawEvent()
+DecodeRawEvent().DataOnDemand = True
+
+# Path to private TCK
+from Configurables import ConfigTarFileAccessSvc
+ConfigTarFileAccessSvc().File='/afs/cern.ch/user/s/sbenson/cmtuser/Moore_v23r2/TCKData/config.tar'
+
+Tesla().TriggerLine = "Hlt2IncPhi" # Enter your trigger line here
+Tesla().EvtMax = -1
+
+from GaudiConf.IOHelper import IOHelper
+ioh = IOHelper()
+ioh.setupServices()
+ioh.inputFiles([
+    "PATH/TO/HLT/PROCESSED/DATA"
+])
+Tesla().outputFile = "PATH/TO/OUTPUT"
