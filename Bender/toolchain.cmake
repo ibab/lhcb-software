3e3248e9df2612ad40aa51063c40ae724e7b8236
Vanya Belyaev <ivan.belyaev@cern.ch>
2014-11-09 10:27:34 +0000

    prepare Bender v25r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@180091 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5441e169dd3638bed2385605a26380835e3fcc7a

--- Bender/toolchain.cmake
+++ Bender/toolchain.cmake
@@ -1,9 +1,1 @@
-# Special toolchain file that inherits the same heptools version as the
-# used projects.
-find_file(inherit_heptools_module InheritHEPTools.cmake)
-# this check is needed because the toolchain seem to be called a second time
-# without the proper cache
-if(inherit_heptools_module)
-  include(${inherit_heptools_module})
-  inherit_heptools()
-endif()
+include($ENV{LBUTILSROOT}/data/toolchain.cmake)
