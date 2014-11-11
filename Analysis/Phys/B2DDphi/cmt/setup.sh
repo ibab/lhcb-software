a720b492ae6e0f174a6927acdfd1e22287137e18
Maurizio Martinelli <maurizio.martinelli@cern.ch>
2014-11-11 14:44:53 +0000

    first import of B2DDphi under Phys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@180189 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f5b1e04cfb501e62b0571ee0df8f9a23dd5a4093

---
+++ Analysis/Phys/B2DDphi/cmt/setup.sh
@@ -0,0 +1,12 @@
+# echo "Setting B2DDphi v1r0 in /afs/cern.ch/user/m/mamartin/cmtuser/Urania_v3r0/Phys"
+
+if test "${CMTROOT}" = ""; then
+  CMTROOT=/afs/cern.ch/sw/contrib/CMT/v1r20p20090520; export CMTROOT
+fi
+. ${CMTROOT}/mgr/setup.sh
+
+tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
+if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
+${CMTROOT}/mgr/cmt setup -sh -pack=B2DDphi -version=v1r0 -path=/afs/cern.ch/user/m/mamartin/cmtuser/Urania_v3r0/Phys  -no_cleanup $* >${tempfile}; . ${tempfile}
+/bin/rm -f ${tempfile}
+
