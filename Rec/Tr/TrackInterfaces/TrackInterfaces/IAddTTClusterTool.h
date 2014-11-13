531eb47f495d9aa7fbfd107c15572b8b6cd6abab
Michel De Cian <michel.de.cian@cern.ch>
2014-11-13 17:23:10 +0000

    Remove two virtual functions in IAddTTClusterTool, as AddTTClusterTool is not in Rec anymore and they are not needed.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@180286 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9f08fd9924fb4d179f85e18b5d42ee51319ed3e7

--- Rec/Tr/TrackInterfaces/TrackInterfaces/IAddTTClusterTool.h
+++ Rec/Tr/TrackInterfaces/TrackInterfaces/IAddTTClusterTool.h
@@ -13,8 +13,11 @@ static const InterfaceID IID_IAddTTClusterTool ( "IAddTTClusterTool", 1, 0 );
 
 /** @class IAddTTClusterTool IAddTTClusterTool.h TrackInterfaces/IAddClusterTool.h
  *
+ *  Interface for tools that add TT hits to long tracks like PatAddTTCoord
+ *  Naming is mostly historical
+ *
  *  @author:  Jeroen van Tilburg Jeroen.van.Tilburg@cern.nl
- *  @auhtor:  Michel De Cian (removed obsolete pure virtual functions)	
+ *  @author:  Michel De Cian (removed obsolete pure virtual functions)	
  *  @date:    04-02-2010
  */
 
@@ -37,15 +40,6 @@ public:
   /// Add TT clusters to matched tracks
   virtual StatusCode addTTClusters( LHCb::Track& track ) = 0;
   virtual StatusCode returnTTClusters( LHCb::State& state, std::vector<PatTTHit*>& ttHits, double& finalChi2, double p) = 0;
-
-  virtual StatusCode addTTClusters( LHCb::Track& track, 
-                                    std::vector<LHCb::STCluster*>& ttClusters,
-                                    std::vector<double>& ttChi2s ) = 0;
-
-  virtual double distanceToStrip( const LHCb::Track& track, 
-                                  const LHCb::STCluster& ttCluster ) = 0;
-  
-  
 
 };
 #endif // TRACKINTERFACES_IADDTTCLUSTERTOOL_H
