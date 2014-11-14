ac93b84a984b02a8ed601de7ed50c0934faa73f7
Renato Quagliani <renato.quagliani@cern.ch>
2014-11-14 14:20:44 +0000

    Zone Set Geometry modified in PrFTHitManager.cpp
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@180306 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0f81309cec368a5b630198ccb59e9b22f0102a46

--- Rec/Pr/PrAlgorithms/src/PrFTHitManager.cpp
+++ Rec/Pr/PrAlgorithms/src/PrFTHitManager.cpp
@@ -57,21 +57,30 @@ void PrFTHitManager::buildGeometry ( ) {
   m_ftDet = getDet<DeFTDetector>( DeFTDetectorLocation::Default );
   //version 20 is the detector with monolayer and fibremat structure, including the dead regions
   if ( msgLevel( MSG::DEBUG) )  debug() << "DETECTOR VERSION: " << m_ftDet->version() << endmsg;  
-  DetectorSegment seg;
-
+  //Different Detector Segments has to be used to set the geometry of zone Up and Down to be consistent with new DeFTFibreMat
+  DetectorSegment segUp;
+  DetectorSegment segDown;
   for ( std::vector<DeFTLayer*>::const_iterator itL = m_ftDet->layers().begin();  //loop over layers
 	m_ftDet->layers().end() != itL; ++itL ) {
     int id = (*itL)->params()->param<int>("layerID");  //ask layerID
-    LHCb::FTChannelID tmp( id, 0u, 0u, 0u, 0u ); //"0u" means "unsigned 0" 
-    const DeFTFibreMat* ftMat = m_ftDet->findFibreMat(tmp);
-    seg = ftMat->createDetSegment( tmp, 0. ); 
-    if ( msgLevel( MSG::DEBUG) ) debug() << "STEREO ANGLE: " <<  ftMat->angle() << endmsg;
+    
+    //FTChannelID (layer,module,mat,SiPMID,netCellID)
+    LHCb::FTChannelID ChUp( id, 0u, 0u, 0u, 0u ); //"0u" means "unsigned 0" 
+    const DeFTFibreMat* ftMatUp = m_ftDet->findFibreMat(ChUp);
+    segUp = ftMatUp->createDetSegment( ChUp, 0. );
+    
+    LHCb::FTChannelID ChDown( id, 0u, 1u, 0u, 0u ); //"0u" means "unsigned 0"
+    const DeFTFibreMat* ftMatDown = m_ftDet->findFibreMat(ChDown);
+    segDown = ftMatDown->createDetSegment( ChDown, 0. );
+
+    if ( msgLevel( MSG::DEBUG) ) debug() << "STEREO ANGLE Up: " <<  ftMatUp->angle() << endmsg;
+    if ( msgLevel( MSG::DEBUG) ) debug() << "STEREO ANGLE Down: " <<  ftMatDown->angle() << endmsg;
     
     //The setGeometry defines the z at y=0, the dxDy and the dzDy, as well as the isX properties of the zone. 
     //This is important, since these are used in the following. 
     //They are set once for each zone in this method.
-    zone( 2*id   )->setGeometry( seg ); // ex:layers 0  down (up with new FTChannelID)
-    zone( 2*id+1 )->setGeometry( seg ); // ex:layers 0  up (down with new FTChannelID)
+    zone( 2*id   )->setGeometry( segUp ); // ex:layers 0  down (up with new FTChannelID)
+    zone( 2*id+1 )->setGeometry( segDown ); // ex:layers 0  up (down with new FTChannelID)
     //The boundaries are needed in case you will use the m_xMin, m_xMax, m_yMin, m_yMax of the zone, 
     //or methods that are indirectly using them, like dxOnAFibre() or isInside(x,y) 
     //(see https://svnweb.cern.ch/trac/lhcb/browser/Rec/trunk/Pr/PrKernel/PrKernel/PrHitZone.h?rev=164716). 
