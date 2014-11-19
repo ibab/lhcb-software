5628c98523002c8fbfc83452a08e19481455a217
Roel Aaij <roel.aaij@cern.ch>
2014-11-19 16:32:44 +0000

    Fix collision in anonymous namespace in for ROOT6.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@180462 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0bcd2913192382c4044010f308eb71369270b403

--- LHCb/Event/LinkerInstances/dict/linker_instances.h
+++ LHCb/Event/LinkerInstances/dict/linker_instances.h
@@ -13,31 +13,34 @@
 #include "Event/CaloDigit.h"
 #include "Event/Track.h"
 #include "Event/Particle.h"
-namespace {
-  LinkedTo<LHCb::MCParticle,LHCb::VeloCluster> i1 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::VPCluster> i31 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::FTCluster>   i41 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::STCluster>   i2 (0,0,"");
-  LinkedTo<LHCb::MCParticle,ContainedObject>      i3 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::MuonCoord>   i4 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::MuonDigit>   i7 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::CaloDigit>   i8 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::Track>       i5 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::MCHit>       i6 (0,0,"");
-  LinkedTo<LHCb::MCParticle,LHCb::Particle>    i10 (0,0,"");
-  LinkedTo<LHCb::MCParticle>                   i9 (0,0,"");
 
-  LinkedFrom<LHCb::VeloCluster,LHCb::MCParticle> i101 (0,0,"");
-  LinkedFrom<LHCb::VPCluster,LHCb::MCParticle> i301 (0,0,"");
-  LinkedFrom<LHCb::FTCluster,LHCb::MCParticle> i401 (0,0,"");
-  LinkedFrom<LHCb::STCluster,LHCb::MCParticle>   i102 (0,0,"");
-  LinkedFrom<ContainedObject,LHCb::MCParticle>      i103 (0,0,"");
-  LinkedFrom<LHCb::MuonCoord,LHCb::MCParticle>   i104 (0,0,"");
-  LinkedFrom<LHCb::MuonDigit,LHCb::MCParticle>   i107 (0,0,"");
-  LinkedFrom<LHCb::CaloDigit,LHCb::MCParticle>   i108 (0,0,"");
-  LinkedFrom<LHCb::Track,LHCb::MCParticle>       i105 (0,0,"");
-  LinkedFrom<LHCb::MCHit,LHCb::MCParticle>       i106 (0,0,"");
-  LinkedFrom<LHCb::Particle,LHCb::MCParticle>    i110 (0,0,"");
-  LinkedFrom<LHCb::MCParticle>                   i109 (0,0,"");
+namespace {
+   struct _Instantiations {
+      LinkedTo<LHCb::MCParticle,LHCb::VeloCluster> i1 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::VPCluster> i31 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::FTCluster>   i41 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::STCluster>   i2 {0,0,""};
+      LinkedTo<LHCb::MCParticle,ContainedObject>      i3 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::MuonCoord>   i4 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::MuonDigit>   i7 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::CaloDigit>   i8 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::Track>       i5 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::MCHit>       i6 {0,0,""};
+      LinkedTo<LHCb::MCParticle,LHCb::Particle>    i10 {0,0,""};
+      LinkedTo<LHCb::MCParticle>                   i9 {0,0,""};
+
+      LinkedFrom<LHCb::VeloCluster,LHCb::MCParticle> i101 {0,0,""};
+      LinkedFrom<LHCb::VPCluster,LHCb::MCParticle> i301 {0,0,""};
+      LinkedFrom<LHCb::FTCluster,LHCb::MCParticle> i401 {0,0,""};
+      LinkedFrom<LHCb::STCluster,LHCb::MCParticle>   i102 {0,0,""};
+      LinkedFrom<ContainedObject,LHCb::MCParticle>      i103 {0,0,""};
+      LinkedFrom<LHCb::MuonCoord,LHCb::MCParticle>   i104 {0,0,""};
+      LinkedFrom<LHCb::MuonDigit,LHCb::MCParticle>   i107 {0,0,""};
+      LinkedFrom<LHCb::CaloDigit,LHCb::MCParticle>   i108 {0,0,""};
+      LinkedFrom<LHCb::Track,LHCb::MCParticle>       i105 {0,0,""};
+      LinkedFrom<LHCb::MCHit,LHCb::MCParticle>       i106 {0,0,""};
+      LinkedFrom<LHCb::Particle,LHCb::MCParticle>    i110 {0,0,""};
+      LinkedFrom<LHCb::MCParticle>                   i109 {0,0,""};
+   };
 }
 
