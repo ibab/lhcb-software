c116aeeddab8e22926dab3ad2ddc195283bb51c2
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2014-11-20 22:59:39 +0000

    update TrackUniqueSegmentSelectorS20p3 to work with new TrackCloneData implementation
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@180527 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 76e9b1b6cbd7c9b78054fe0ff644a35ebe7d8c5c

--- Stripping/Phys/DisplVertices/src/TrackUniqueSegmentSelectorS20p3.cpp
+++ Stripping/Phys/DisplVertices/src/TrackUniqueSegmentSelectorS20p3.cpp
@@ -4,6 +4,8 @@
 // 2013-12-22 Wouter Hulsbergen
 //-----------------------------------------------------------------------------
 
+#include <functional>
+
 // Interface class
 #include "Kernel/ITrackUniqueSegmentSelector.h"
 
@@ -20,9 +22,6 @@
 
 // TrackKernel
 #include "TrackKernel/TrackCloneData.h"
-
-// from BOOST
-#include "boost/foreach.hpp"
 
 
 // Include files
@@ -140,22 +139,21 @@ namespace {
   /// but since we only use velo hits, it may be better to just copy
   /// what we need.
 
-  class TrackData : public LHCb::TrackCloneData 
+  class TrackData : public LHCb::TrackCloneData<false, false>
   {
   public:
-    TrackData(const LHCb::Track& track, size_t index) : LHCb::TrackCloneData(track)
+    TrackData(const LHCb::Track* track, size_t index) :
+	LHCb::TrackCloneData<false, false>(TrackPtr(track)),
+	m_weightFromHits(track->nDoF() - track->chi2()),
+	m_index(index),
+	m_flaggedByCloneCleaner(track->info(LHCb::Track::CloneDist, -1) > 0)
     {
       // the sorting criteria is based on track type, number of hits
       // and chi2/dof, just like in trackclonecleaner. I am not sure
       // if number of hits is so great criterion: can we somehow give
       // the chi2 a bit more weight?
-      
-      // these lines copied from base class. 
-      int weightFromType[10] = {0,11,10,20,15,16,12,2,3} ;
-      m_weightFromType = weightFromType[track.type()] ;
-      m_weightFromHits = track.nDoF() - track.chi2() ;
-      m_flaggedByCloneCleaner = track.info(LHCb::Track::CloneDist,-1) > 0 ? 1 : 0 ;
-      m_index = index ;
+      static const int weightFromType[10] = {0,11,10,20,15,16,12,2,3} ;
+      m_weightFromType = weightFromType[track->type()] ;
     }
     double weightFromHits() const { return m_weightFromHits ; }
     
@@ -175,7 +173,7 @@ namespace {
 	 (m_flaggedByCloneCleaner < rhs.m_flaggedByCloneCleaner ||
 	  ( m_flaggedByCloneCleaner == rhs.m_flaggedByCloneCleaner &&
 	    m_weightFromHits > rhs.m_weightFromHits ) ) ) ;
-	  }
+    }
 
     // void intersection( const LHCb::TrackCloneData& rhs, HitType type,
     // 		       LHCbIDs& commonhits ) const {
@@ -188,19 +186,11 @@ namespace {
     // }
     
   private:
-    int m_weightFromType ;
-    int m_flaggedByCloneCleaner ;
     double m_weightFromHits ;
     size_t m_index ;
+    int m_weightFromType ;
+    bool m_flaggedByCloneCleaner ;
   } ;
-
-  struct TrackDataSorter {
-    bool operator()(const TrackData* lhs,
-		    const TrackData* rhs) {
-      return *lhs < *rhs ;
-    }
-  } ;
-
 }
 
 //=============================================================================
@@ -218,33 +208,35 @@ StatusCode TrackUniqueSegmentSelectorS20p3::flagClones( const LHCb::Track::Range
   
   // we don't want dynamic memory allocation as it is expensive.
   std::vector<TrackData> alltracksstore ; alltracksstore.reserve(N) ;
-  size_t i(0) ;
-  for( LHCb::Track::Range::const_iterator it = tracks.begin() ; 
-       it != tracks.end() ; ++it,++i )
-    alltracksstore.push_back(TrackData(**it,i)) ;
+  size_t i(0);
+  for (const LHCb::Track* tr: tracks) 
+    alltracksstore.push_back(TrackData(tr,i++)) ;
   
   // create vector of pointers and sort it
-  std::vector<TrackData*> alltracks(N) ;
-  for(size_t i=0; i<N; ++i) alltracks[i] = &(alltracksstore[i]) ;
-  std::stable_sort( alltracks.begin(), alltracks.end(), TrackDataSorter() ) ;
+  std::vector<std::reference_wrapper<TrackData> > alltracks(
+	  std::begin(alltracksstore), std::end(alltracksstore));
+  std::stable_sort( alltracks.begin(), alltracks.end(), 
+	  [] (const TrackData &lhs, const TrackData &rhs)
+	  {return lhs < rhs; } ) ;
   
   // loop over all tracks and add them to output container if they
   // pass selector and are not yet there.
-  std::vector<TrackData*> selectedtracks ;
+  std::vector<std::reference_wrapper<TrackData> > selectedtracks ;
   selectedtracks.reserve( alltracks.size() ) ;
-  for( std::vector<TrackData*>::const_iterator it = alltracks.begin() ; 
-       it != alltracks.end() ; ++it ) {
+  for( auto it = alltracks.begin() ; it != alltracks.end() ; ++it ) {
+    TrackData& itd(*it);
     // std::cout << "processing track: " 
-    // 	      << (*it)->track().type() << " " << (*it)->lhcbIDs(TrackData::VeloPhi).size() << " " 
-    // 	      << (*it)->lhcbIDs(TrackData::VeloR).size() << " "
-    // 	      << (*it)->weightFromHits() << " "
-    // 	      << (*it)->track().info(LHCb::Track::CloneDist,-1) << std::endl ;
+    // 	      << itd.track().type() << " " << itd.lhcbIDs(TrackData::VeloPhi).size() << " " 
+    // 	      << itd.lhcbIDs(TrackData::VeloR).size() << " "
+    // 	      << itd.weightFromHits() << " "
+    // 	      << itd.track().info(LHCb::Track::CloneDist,-1) << std::endl ;
     
     bool found(false) ;
-    bool iHasVelo = (*it)->nHits(TrackData::VeloR) >0 ;
-    for( std::vector<TrackData*>::const_iterator jt(selectedtracks.begin()), jend(selectedtracks.end()); 
+    bool iHasVelo = itd.nHits(TrackData::VeloR) >0 ;
+    for( auto jt(selectedtracks.begin()), jend(selectedtracks.end()); 
 	 jt != jend &&!found; ++jt) {
-      bool jHasVelo = (*jt)->nHits(TrackData::VeloR) >0 ;
+      TrackData& jtd(*jt);
+      bool jHasVelo = jtd.nHits(TrackData::VeloR) >0 ;
 
       // our main goal is two remove velo segments that have been used
       // more than once to create different long tracks.  however, we
@@ -259,36 +251,36 @@ StatusCode TrackUniqueSegmentSelectorS20p3::flagClones( const LHCb::Track::Range
       //computing how many hits are in common, is expensive. that's why this looks a bit cumbersome.
       size_t nCommonR, nCommonPhi ;
       found = iHasVelo && jHasVelo &&
-	( (nCommonR = (*it)->nCommon(**jt, TrackData::VeloR )) >=2 &&
-	  (nCommonR >=3 || std::min( (*it)->nHits( TrackData::VeloR ), (*jt)->nHits( TrackData::VeloR )) < 5 ) ) &&
-	( (nCommonPhi = (*it)->nCommon(**jt, TrackData::VeloPhi )) >=2 &&
-	  (nCommonPhi >=3 || std::min( (*it)->nHits( TrackData::VeloPhi ), (*jt)->nHits( TrackData::VeloPhi )) < 5 ) ) ;
+	( (nCommonR = itd.nCommon(jtd, TrackData::VeloR )) >=2 &&
+	  (nCommonR >=3 || std::min( itd.nHits( TrackData::VeloR ), jtd.nHits( TrackData::VeloR )) < 5 ) ) &&
+	( (nCommonPhi = itd.nCommon(jtd, TrackData::VeloPhi )) >=2 &&
+	  (nCommonPhi >=3 || std::min( itd.nHits( TrackData::VeloPhi ), jtd.nHits( TrackData::VeloPhi )) < 5 ) ) ;
       
       if( !found &&
-	  ((*it)->overlapFraction(**jt, TrackData::VeloR ) >=0.5 &&
-	   (*it)->overlapFraction(**jt, TrackData::VeloPhi ) >=0.5) ) {
+	  (itd.overlapFraction(jtd, TrackData::VeloR ) >=0.5 &&
+	   itd.overlapFraction(jtd, TrackData::VeloPhi ) >=0.5) ) {
 	info() << "Huh? " 
-	       << (*it)->nCommon(**jt, TrackData::VeloR ) << " "
-	       << (*it)->nHits( TrackData::VeloR ) << " " <<  (*jt)->nHits( TrackData::VeloR ) << " | "
-	       << (*it)->nCommon(**jt, TrackData::VeloPhi ) << " "
-	       << (*it)->nHits( TrackData::VeloPhi ) << " " <<  (*jt)->nHits( TrackData::VeloPhi ) << endreq ;
+	       << itd.nCommon(jtd, TrackData::VeloR ) << " "
+	       << itd.nHits( TrackData::VeloR ) << " " <<  jtd.nHits( TrackData::VeloR ) << " | "
+	       << itd.nCommon(jtd, TrackData::VeloPhi ) << " "
+	       << itd.nHits( TrackData::VeloPhi ) << " " <<  jtd.nHits( TrackData::VeloPhi ) << endreq ;
       }
 
-      //(*it)->nCommon(**jt, TrackData::VeloPhi ) >=2) ;
-      //((*it)->overlapFraction(**jt, TrackData::VeloR ) >=0.5 &&
-      //(*it)->overlapFraction(**jt, TrackData::VeloPhi ) >=0.5) ;
+      //itd.nCommon(jtd, TrackData::VeloPhi ) >=2) ;
+      //(itd.overlapFraction(jtd, TrackData::VeloR ) >=0.5 &&
+      //itd.overlapFraction(jtd, TrackData::VeloPhi ) >=0.5) ;
       // if( !foundVelo &&
-      // 	  (*it)->nCommon(**jt, TrackData::VeloR ) >=2 &&
-      // 	  (*it)->nCommon(**jt, TrackData::VeloPhi ) >=2)  {
+      // 	  itd.nCommon(jtd, TrackData::VeloR ) >=2 &&
+      // 	  itd.nCommon(jtd, TrackData::VeloPhi ) >=2)  {
       // 	std::cout << "*****************************************************************" << std::endl ;
       // 	std::cout << "overlapfractionsL " 
-      // 		  << (*it)->overlapFraction(**jt, TrackData::VeloR ) << " "
-      // 		  << (*it)->overlapFraction(**jt, TrackData::VeloPhi ) << std::endl ;
-      // 	std::cout << LHCb::HitPattern( (*it)->track().lhcbIDs() ) << std::endl ;
-      // 	std::cout << LHCb::HitPattern( (*jt)->track().lhcbIDs() ) << std::endl ;
+      // 		  << itd.overlapFraction(jtd, TrackData::VeloR ) << " "
+      // 		  << itd.overlapFraction(jtd, TrackData::VeloPhi ) << std::endl ;
+      // 	std::cout << LHCb::HitPattern( itd.track().lhcbIDs() ) << std::endl ;
+      // 	std::cout << LHCb::HitPattern( jtd.track().lhcbIDs() ) << std::endl ;
 
-      // 	const LHCb::Track::LHCbIDContainer& ihits = (*it)->track().lhcbIDs() ;
-      // 	const LHCb::Track::LHCbIDContainer& jhits = (*jt)->track().lhcbIDs() ;
+      // 	const LHCb::Track::LHCbIDContainer& ihits = itd.track().lhcbIDs() ;
+      // 	const LHCb::Track::LHCbIDContainer& jhits = jtd.track().lhcbIDs() ;
       // 	printvelohits( ihits ) ;
       // 	printvelohits( jhits ) ;
       // 	LHCb::Track::LHCbIDContainer commonhits(std::max(ihits.size(),jhits.size() ) ) ;
@@ -304,25 +296,25 @@ StatusCode TrackUniqueSegmentSelectorS20p3::flagClones( const LHCb::Track::Range
       // really different in the Velo. Otherwise we just loose too
       // many good velo segments.
       if( !found && !iHasVelo )
-	found  = (*it)->overlapFraction( **jt, TrackData::T ) > 0.5 ;
+	found  = itd.overlapFraction( jtd, TrackData::T ) > 0.5 ;
       
       if( found && m_debugLevel ) {
 	verbose() << "Found clone: "
-		  <<  (*it)->nCommon(**jt, TrackData::VeloPhi ) 
+		  <<  itd.nCommon(jtd, TrackData::VeloPhi ) 
 		  << " "
-		  << (*it)->nCommon(**jt, TrackData::VeloR ) 
+		  << itd.nCommon(jtd, TrackData::VeloR ) 
 		  << " j: "
-		  << (*jt)->track().type() << " " << (*jt)->lhcbIDs(TrackData::VeloPhi).size() << " " << (*jt)->lhcbIDs(TrackData::VeloR).size() 
-		  << " " << (*jt)->track().info(LHCb::Track::CloneDist,-1) 
+		  << jtd.track().type() << " " << jtd.lhcbIDs(TrackData::VeloPhi).size() << " " << jtd.lhcbIDs(TrackData::VeloR).size() 
+		  << " " << jtd.track().info(LHCb::Track::CloneDist,-1) 
 		  <<  " i: "
-		  << (*it)->track().type() << " " <<  (*it)->lhcbIDs(TrackData::VeloPhi).size() << " " << (*it)->lhcbIDs(TrackData::VeloR).size()
-		  << " " << (*it)->track().info(LHCb::Track::CloneDist,-1) 
+		  << itd.track().type() << " " << itd.lhcbIDs(TrackData::VeloPhi).size() << " " << itd.lhcbIDs(TrackData::VeloR).size()
+		  << " " << itd.track().info(LHCb::Track::CloneDist,-1) 
 		  << endreq ;
       }
     }
-    if(!found) selectedtracks.push_back( *it ) ;
+    if(!found) selectedtracks.push_back( itd ) ;
     else {
-      isclone[ (*it)->index() ] = true ;
+      isclone[ itd.index() ] = true ;
     }
   }
   
@@ -380,7 +372,7 @@ StatusCode TrackUniqueSegmentSelectorS20p3::select(const LHCb::Track::Range& tra
   LHCb::Track::Range selrange = tracks ;
   if( !m_selector.empty() ) {
     selectedsubset.reserve( tracks.size() ) ;
-    BOOST_FOREACH( const LHCb::Track* trk, tracks ) 
+    for (const LHCb::Track* trk: tracks) 
       if( m_selector->accept(*trk) ) 
 	selectedsubset.push_back( trk ) ;
     selrange = LHCb::Track::Range( selectedsubset ) ;
@@ -419,7 +411,7 @@ StatusCode TrackUniqueSegmentSelectorS20p3::select(const LHCb::Particle::Range& 
   std::map< const LHCb::Track*, const LHCb::Particle*> track2particle ;
   std::vector<const LHCb::Track*> tracks ;
   const LHCb::Track* trk(0) ;
-  BOOST_FOREACH( const LHCb::Particle* itr, particles) {
+  for (const LHCb::Particle* itr: particles) {
     if( itr->proto() && (trk =itr->proto()->track()) ) {
       // make sure this track isn't yet there
       if( track2particle.find( trk ) == track2particle.end() ) {
