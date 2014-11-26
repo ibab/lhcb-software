1baa9c3e19dfab56fa42d2cd9a65e71b186eeffb
Gloria Corti <gloria.corti@cern.ch>
2014-11-26 13:15:09 +0000

    fixes for warning when compiling with --pedantic
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@180755 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2aa4afb6749542593eca9b5fe9515438847163f2

--- Gauss/Bls/BlsMoniSim/src/BlsHitChecker.cpp
+++ Gauss/Bls/BlsMoniSim/src/BlsHitChecker.cpp
@@ -21,7 +21,7 @@
 //-----------------------------------------------------------------------------
 
 // Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( BlsHitChecker );
+DECLARE_ALGORITHM_FACTORY( BlsHitChecker )
 
 //=============================================================================
 // Standard constructor, initializes variables
