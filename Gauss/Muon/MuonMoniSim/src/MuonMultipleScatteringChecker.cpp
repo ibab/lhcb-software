d37011ea1f3c22f2133bd3a0f1ad008711aed301
Gloria Corti <gloria.corti@cern.ch>
2014-11-26 13:11:34 +0000

    fix for warning with --pedantic
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@180754 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 78a84cfd45665b2fa99d45f46e52ef349df522f9

--- Gauss/Muon/MuonMoniSim/src/MuonMultipleScatteringChecker.cpp
+++ Gauss/Muon/MuonMoniSim/src/MuonMultipleScatteringChecker.cpp
@@ -21,7 +21,7 @@
 //-----------------------------------------------------------------------------
 
 // Declaration of the Algorithm Factory
-DECLARE_ALGORITHM_FACTORY( MuonMultipleScatteringChecker );
+DECLARE_ALGORITHM_FACTORY( MuonMultipleScatteringChecker )
 
 
 //=============================================================================
