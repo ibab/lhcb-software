666a6640db797f6a179d89c22542175e7c3310a1
Victor Coco <victor.coco@cern.ch>
2014-11-27 15:27:46 +0000

    added HCCellID
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@180792 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4d8e915ce191c5a04683a2b6fb52b166477a5fea

--- LHCb/Kernel/LHCbKernel/dict/LHCbKernelDict.h
+++ LHCb/Kernel/LHCbKernel/dict/LHCbKernelDict.h
@@ -23,6 +23,7 @@
 #include "Kernel/SiPositionInfo.h"
 #include "Kernel/PixelPositionInfo.h"
 #include "Kernel/RichSmartID.h"
+#include "Kernel/HCCellID.h"
 
 #include "Kernel/IAlgorithmCorrelations.h"
 #include "Kernel/IBdlTool.h"
@@ -65,24 +66,27 @@ namespace {
     // begin instantiations
     GaudiUtils::VectorMap<int,double>                   m_vector_map_int_double;
     std::pair<LHCb::VeloChannelID,int>                  m_std_pair_VeloChannelID_int;
-    std::pair<LHCb::VPChannelID,int>               m_std_pair_VPChannelID_int;
+    std::pair<LHCb::VPChannelID,int>                    m_std_pair_VPChannelID_int;
     std::pair<LHCb::STChannelID,int>                    m_std_pair_STChannelID_int;
     std::pair<LHCb::OTChannelID,int>                    m_std_pair_OTChannelID_int;
     std::pair<double,LHCb::CaloCellID>                  m_std_pair_double_CaloCellID;
     std::pair<LHCb::MuonTileID,int>                     m_std_pair_MuonTileID_int;
+    std::pair<LHCb::HCCellID,int>                       m_std_pair_HCCellID_int;
     std::vector<std::pair<LHCb::VeloChannelID,int> >    m_std_vector_std_pair_VeloChannelID_int;
-    std::vector<std::pair<LHCb::VPChannelID,int> > m_std_vector_std_pair_VPChannelID_int;
+    std::vector<std::pair<LHCb::VPChannelID,int> >      m_std_vector_std_pair_VPChannelID_int;
     std::vector<std::pair<LHCb::STChannelID,int> >      m_std_vector_std_pair_STChannelID_int;
     std::vector<std::pair<LHCb::OTChannelID,int> >      m_std_vector_std_pair_OTChannelID_int;
     std::vector<std::pair<double,LHCb::CaloCellID> >    m_std_vector_std_pair_double_CaloCellID;
     std::vector<std::pair<LHCb::MuonTileID,int> >       m_std_vector_std_pair_MuonTileID_int;
+    std::vector<std::pair<LHCb::HCCellID,int> >         m_std_vector_std_pair_HCCellID_int;
     std::auto_ptr<LHCb::Trajectory>                     m_auto_ptr_Trajectory;
     LHCb::SiPositionInfo<LHCb::STChannelID>             m_SiPositionInfo_STChannelID;
     LHCb::SiPositionInfo<LHCb::VeloChannelID>           m_SiPositionInfo_VeloChannelID;
-    LHCb::PixelPositionInfo<LHCb::VPChannelID>     m_PixelPositionInfo_VPChannelID;
+    LHCb::PixelPositionInfo<LHCb::VPChannelID>          m_PixelPositionInfo_VPChannelID;
     std::pair<Gaudi::XYZPoint,Gaudi::XYZPoint>          m_std_pair_XYZPoint_XYZPoint;
     std::vector<LHCb::VeloChannelID>                    m_std_vector_VeloChannelID;
-    std::vector<LHCb::VPChannelID>                 m_std_vector_VPChannelID;
+    std::vector<LHCb::VPChannelID>                      m_std_vector_VPChannelID;
+    std::vector<LHCb::HCCellID>                         m_std_vector_HCCellID;
     std::vector<LHCb::LHCbID>::const_iterator           m_std_vector_LHCbID_const_iterator;
     std::map<std::pair<int,unsigned int>,unsigned int>  m_L0DUReport_Summaries;
     Rich::Radiators                                     m_RICH_Radiators;
