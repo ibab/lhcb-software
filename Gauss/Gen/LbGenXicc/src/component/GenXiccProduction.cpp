f5bd92c8ad347025a9098f7a97e71d5676e2f2b1
Gloria Corti <gloria.corti@cern.ch>
2014-11-28 16:18:03 +0000

    fix warnings when compiling with --pedantic
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@180860 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6709055ee1c88e108e6007834d939206f3427e51

--- Gauss/Gen/LbGenXicc/src/component/GenXiccProduction.cpp
+++ Gauss/Gen/LbGenXicc/src/component/GenXiccProduction.cpp
@@ -27,7 +27,7 @@
 
 // Declaration of the Tool Factory
 
-DECLARE_TOOL_FACTORY( GenXiccProduction );
+DECLARE_TOOL_FACTORY( GenXiccProduction )
 
 
 //=============================================================================
