3f5e855af925e289451bc7c8a10707395bca3009
Matthew William Kenzie <matthew.william.kenzie@cern.ch>
2014-11-28 09:44:20 +0000

    Add default TES location for Particles and ProtoParticles created in Hlt1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@180821 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 23c442cadabbae2a6499886f277b3caa0eac7589

---
+++ Hlt/Hlt/HltBase/Event/HltParticle.h
@@ -0,0 +1,21 @@
+#ifndef HltEvent_Particle_H_
+#define HltEvent_Particle_H_ 1
+
+// ============================================================================
+// Just want to define default TES location for Hlt Particles
+// ============================================================================
+
+namespace Hlt
+{
+  // Namespace for locations in TES
+  namespace ParticleLocation
+  {
+  // =========================================================================
+  /** @var Default
+   *  The default TES-location of HLT-Particles
+   */
+    const std::string Default = "Hlt/Particles";
+  }
+}
+#endif
+
---
+++ Hlt/Hlt/HltBase/Event/HltProtoParticle.h
@@ -0,0 +1,20 @@
+#ifndef HltEvent_ProtoParticle_H_
+#define HltEvent_ProtoParticle_H_ 1
+
+// ============================================================================
+// Just want to define default TES location for Hlt ProtoParticles
+// ============================================================================
+
+namespace Hlt
+{
+  // Namespace for locations in TES
+  namespace ProtoParticleLocation
+  {
+  // =========================================================================
+  /** @var Default
+   *  The default TES-location of HLT-ProtoParticles
+   */
+    const std::string Default = "Hlt/ProtoParticles";
+  }
+}
+#endif
