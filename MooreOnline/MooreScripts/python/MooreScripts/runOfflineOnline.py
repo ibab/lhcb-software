6e4daad3774bb3ea58b6261558e5e3288574f7d3
Roel Aaij <roel.aaij@cern.ch>
2014-11-30 12:25:02 +0000

    Add script for running offline Moore in an online-like environment (script this time...).
    git-svn-id: http://svn.cern.ch/guest/lhcb/MooreOnline/trunk@180873 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1a9588aa7b8e53656a5a0a159e4449a9f9ea40b9

---
+++ MooreOnline/MooreScripts/python/MooreScripts/runOfflineOnline.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+def configure(**kwargs) :
+    moore_tests = __import__("MooreTests" , globals(), locals(), [kwargs.get('UserPackage')])
+    user_package = getattr(moore_tests, kwargs.pop('UserPackage'))
+    from Moore.Configuration import Moore, MooreExpert
+    from MooreOnlineConf.Configuration import MooreOnline
+
+    # Add some expected stuff to OnlineEnv
+    import OnlineEnv
+    OnlineEnv.MooreStartupMode = 1 # 1 is forking
+    OnlineEnv.TAE = 0
+    OnlineEnv.PartitionID = 0
+    OnlineEnv.PartitionIDName = "0000"
+    OnlineEnv.PartitionName = kwargs.pop('PartitionName')
+    OnlineEnv.OutputLevel = 4
+    moore = Moore()
+
+    # We need MooreOnline to setup the buffer manager infrastructure etc, but we
+    # don't want to use things like the RunChangeHandler and database snapshots.
+    mooreOnline = MooreOnline()
+    mooreOnline.RunOnline = False
+    mooreOnline.EnableRunChangeHandler = None
+    mooreOnline.UseDBSnapshot = False
+    mooreOnline.EnableTimer = False
+    mooreOnline.CheckOdin = False
+
+    # This is the stuff that should come from the PRConfig module
+    for conf, d in { moore: {'DDDBtag' : str, 'CondDBtag' : str, 'InitialTCK' : str},
+                     mooreOnline : {'UseTCK' : bool, 'Simulation' : bool,
+                                    'DataType' : ('2011', '2012', '2013', '2014'),
+                                    'HltLevel' : ('Hlt1', 'Hlt2', 'Hlt1Hlt2')}}.iteritems():
+        for a, t in d.iteritems():
+            ua = user_package.MooreOptions.pop(a)
+            if hasattr(t, '__iter__'):
+                assert(ua in t)
+            else:
+                assert(type(ua) == t)
+            conf.setProp(a, ua)
+            
+    if mooreOnline.HltLevel in ["Hlt1", "Hlt1Hlt2"]:
+        mooreOnline.REQ1 = "EvType=1;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
+    else:
+        mooreOnline.REQ1 = "EvType=2;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
+    
+    # Apparently we need to set this, otherwise something goes wrong with
+    # default properties being retrieved that have the wrong type.
+    from Gaudi.Configuration import EventSelector    
+    moore.inputFiles = [ ]
+    EventSelector().Input = []
+
+    # Extra options
+    from Configurables import MooreExpert
+    args = user_package.MooreOptions
+    args.update(kwargs)
+    for k,v in args.iteritems():
+        #iterate through the available configurables to set required properties
+        found=False
+        for conf in [mooreOnline, moore, MooreExpert()]:
+            if k in conf.__slots__ or hasattr(conf,k):
+                print "setting %s to %s on %s" % (k, v, conf)
+                setattr(conf,k,v)
+                found = True
+                break
+        if not found:
+            print "# WARNING: skipping setting '"+k+":"+v+"' because no configurable has that option"
+
+    user_package.configure()
+
+    import OnlineEnv 
+    OnlineEnv.end_config(False)
