6aee64b06244c1f59163a649d7eafb8086caebfe
Gerhard Raven <gerhard.raven@nikhef.nl>
2014-12-02 14:58:43 +0000

    follow changes in Kernel/SelectionLine to speed up the code
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@181020 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9550fbaffb3e19a9e959799fb7652f4d32ee9e0f

--- Stripping/Phys/StrippingAlgs/src/StrippingAlg.cpp
+++ Stripping/Phys/StrippingAlgs/src/StrippingAlg.cpp
@@ -13,8 +13,7 @@ public:
 
 private:
 
-  unsigned int numberOfCandidates() const;
-  unsigned int numberOfCandidates(const Algorithm*) const;
+  int numberOfCandidates() const;
   std::pair<std::string,unsigned> id() const;
 
 private:
@@ -38,17 +37,14 @@ StatusCode StrippingAlg::initialize()
   if ( sc.isFailure() ) return sc;
 
   SetupSelections();
-  //  m_id = SetupID();
-
   m_id = std::make_pair(std::string(decisionName()), 1);
-
   return sc;
 }
 
-unsigned int StrippingAlg::numberOfCandidates() const
+int StrippingAlg::numberOfCandidates() const
 {
   const LHCb::Particle::Range parts = getIfExists<LHCb::Particle::Range>(m_outputLocation);
-  const unsigned int num = parts.size();
+  const int num = parts.size();
 
   if ( msgLevel(MSG::VERBOSE))
     verbose() << "Selection " << m_outputLocation << " finds " << num
@@ -57,10 +53,6 @@ unsigned int StrippingAlg::numberOfCandidates() const
   return num;
 }
 
-unsigned int StrippingAlg::numberOfCandidates(const Algorithm* /*algorithm*/) const
-{
-  return 0 ;
-}
 
 void StrippingAlg::SetupSelections() { }
 
@@ -69,29 +61,6 @@ std::pair<std::string,unsigned> StrippingAlg::id() const
   return m_id;
 }
 
-std::pair<std::string,unsigned> StrippingAlg::SetupID()
-{
-  // TODO: do this during initialize, 'lock' updates after initialize...
-
-  const std::string *major = 0;
-  static const std::string major1("StrippingSelectionID");
-  if (decisionName().find("Stripping") == std::string::npos)
-  {
-    error() << " Could not find Stripping in decision " << decisionName() << " ???? " << endmsg;
-  }
-  else
-  {
-    major = &major1;
-  }
-  boost::optional<IANNSvc::minor_value_type> key = annSvc().value(*major,decisionName());
-
-  if (!key)
-  {
-    warning() << " DecisionName=" << decisionName() << " not known under major " << major << endmsg;
-    return std::make_pair(std::string(),0);
-  }
-  return *key;
-}
 
 // Declaration of the Algorithm Factory
 DECLARE_ALGORITHM_FACTORY( StrippingAlg )
