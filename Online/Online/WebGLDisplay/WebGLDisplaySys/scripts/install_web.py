cf4f872f62dd00af4e0529f38e61c2356ea2ca43
Markus Frank <frankb@cern.ch>
2014-12-03 12:58:15 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@181078 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 6b87697783d94b5a40266c33df9bd6df2f800f1a

--- Online/Online/WebGLDisplay/WebGLDisplaySys/scripts/install_web.py
+++ Online/Online/WebGLDisplay/WebGLDisplaySys/scripts/install_web.py
@@ -1,29 +1,41 @@
+#!/usr/bin/python
+
 import os
 target = '/group/online/dataflow/cmtuser/EventDisplay'
-home = os.environ['WEBGLDISPLAYSYSROOT']
-web = home+os.path.sep+'web'
+home   = os.environ['WEBGLDISPLAYSYSROOT']
 
-def copyFile(src,tar):
+web    = home+os.path.sep+'web'
+indent = 3
+
+def indentation(level):
+  s = ''
+  if level>1:
+    s = (indent*(level-1))*' '
+  return s
+  
+def copyFile(level,src,tar):
   data = open(src,'r').read()
   open(tar,'w').write(data)
-  print '---> ',tar
+  print indentation(level)+'---> ',tar
 
-def copyDir(src,tar):
+def copyDir(level,src,tar):
   try:
     os.stat(tar)
   except Exception,X:
     os.mkdir(tar)
 
+  print indentation(level)+'===> Directory:',tar
   entries=os.listdir(src)
   for e in entries:
-    if e != '.svn':
-      ent = src+os.path.sep+e
-      to  = tar+os.path.sep+e
-      if os.path.isdir(ent):
-        copyDir(ent,to)
-      else:
-        copyFile(ent,to)
+    ent = src+os.path.sep+e
+    if e != '.svn' and not os.path.isdir(ent):
+        copyFile(level+1,ent,tar+os.path.sep+e)
+
+  for e in entries:
+    ent = src+os.path.sep+e
+    if e != '.svn' and os.path.isdir(ent):
+        copyDir(level+1,ent,tar+os.path.sep+e)
 
 
 os.chdir(web)
-copyDir(web,target)
+copyDir(0,web,target)
