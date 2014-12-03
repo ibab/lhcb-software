c02368b00ca99fe69f2c1f47f3db712ee171f24e
Markus Frank <frankb@cern.ch>
2014-12-03 09:37:47 +0000

    First commit of the online event display
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@181062 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4013837e58525210ce357d2724399c8e68ae4802

---
+++ Online/Online/WebGLDisplay/WebGLDisplaySys/web/small.js
@@ -0,0 +1,17 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "small.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 80115,
+        "faces"         : 138656,
+        "normals"       : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+    "materials": [],
+    "buffers": "small.bin"
+
+}
