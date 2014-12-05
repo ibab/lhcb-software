1cab509976c0a262da00611f7d13143abec967d1
Jon Harrison <jonathan.harrison@manchester.ac.uk>
2014-12-05 18:32:38 +0000

    first import of LovellSys
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@181181 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 36c86cebd89412777ee3f089bfaa5bc8a640c5b5

---
+++ Lovell/LovellSys/doc/MainPage.h
@@ -0,0 +1,13 @@
+/** \mainpage notitle
+ *  \anchor lovelldoxygenmain
+ *
+ * This reference manual documents all %Gaudi classes accessible from
+ * Lovell, the Velo monitoring project.
+ *
+ * <hr>
+ * \htmlinclude new_release.notes
+
+ * <b>Top level Python option files</b>
+ * \li Example.py: default file to run a Lovell style job
+ 
+ */
