eb5c80f8eea69969b6ac91a223d43b6d148ba583
Ben Couturier <lben@cern.ch>
2014-12-16 16:08:41 +0000

    Added gcc49 to the list of known compilers
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@181794 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4bed4bcbd92bf5e646ffd551ea01d5d9b3b35491

--- LbScripts/LbConfiguration/python/LbConfiguration/Platform.py
+++ LbScripts/LbConfiguration/python/LbConfiguration/Platform.py
@@ -158,6 +158,7 @@ binary_opt_list = ["slc4_ia32_gcc34", "slc4_amd64_gcc34",
                    "x86_64-slc5-icc11-opt", "i686-slc5-icc11-opt",
                    "x86_64-slc6-gcc46-opt", "i686-slc6-gcc46-opt",
                    "x86_64-slc6-gcc47-opt", "x86_64-slc6-gcc48-opt",
+                   "x86_64-slc6-gcc49-opt",
                    "x86_64-slc6-icc13-opt", "x86_64-slc6-clang32-opt"]
 # future possible supported binaries
 extra_binary_opt_list = ["slc3_ia32_gcc323",
@@ -285,7 +286,7 @@ flavor_runtime_equivalence = {
                              }
 
 supported_compilers = {
-                       "slc6"   : ["gcc48", "gcc46","gcc45", "gcc44", "gcc47", "icc11", "icc13", "clang32"],
+                       "slc6"   : ["gcc48", "gcc46","gcc45", "gcc44", "gcc47", "icc11", "icc13", "clang32", "gcc49"],
                        "slc5"   : ["gcc46", "gcc43", "gcc45", "icc11"] ,
                        "slc4"   : ["gcc34"],
                        "slc3"   : ["gcc323"],
