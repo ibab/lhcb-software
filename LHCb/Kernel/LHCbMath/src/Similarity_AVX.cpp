4c79002d7eb6a6a30a00be0c7cb5e3ea05e19841
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-01-04 00:09:19 +0000

    faster version of avx similarity transforms
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@182102 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1c57eb443fcde936d594d6113b78bd895e7ccf7f

--- LHCb/Kernel/LHCbMath/src/Similarity_AVX.cpp
+++ LHCb/Kernel/LHCbMath/src/Similarity_AVX.cpp
@@ -18,38 +18,27 @@ __m128d hi128 = _mm256_extractf128_pd( temp, 1 );
 __m128d dotproduct = _mm_add_pd( (__m128d)temp, hi128 );
 }
 
- dot_4_4() { // compute 4 dot products of 4-vectors
-
-__m256d xy0 = _mm256_mul_pd( x[0], y[0] );
-__m256d xy1 = _mm256_mul_pd( x[1], y[1] );
-__m256d xy2 = _mm256_mul_pd( x[2], y[2] );
-__m256d xy3 = _mm256_mul_pd( x[3], y[3] );
-
-// low to high: xy00+xy01 xy10+xy11 xy02+xy03 xy12+xy13
-__m256d temp01 = _mm256_hadd_pd( xy0, xy1 );   
-
-// low to high: xy20+xy21 xy30+xy31 xy22+xy23 xy32+xy33
-__m256d temp23 = _mm256_hadd_pd( xy2, xy3 );
-
-// low to high: xy02+xy03 xy12+xy13 xy20+xy21 xy30+xy31
-__m256d swapped = _mm256_permute2f128_pd( temp01, temp23, 0x21 );
-
-// low to high: xy00+xy01 xy10+xy11 xy22+xy23 xy32+xy33
-__m256d blended = _mm256_blend_pd(temp01, temp23, 0b1100);
-
-__m256d dotproduct = _mm256_add_pd( swapped, blended );
-
- }
-
 #endif
 
 inline double dot5_avx(const double* f, __m256d  r0,  double r2) {
-        auto t = r0*_mm256_loadu_pd(f);
-        t = _mm256_hadd_pd( t, t ); 
-        return t[0]+t[2]+r2*f[4];
-        // auto s = _mm256_permute2f128_pd(t,t,0x01);
-        // auto s = __mm256d{ t[2],t[3],t[0],t[1] } ; 
-        // return (s+t)[0] + r2*f[4];
+    auto t = r0*_mm256_loadu_pd(f);
+    t = _mm256_hadd_pd( t, t ); 
+    return t[0]+t[2]+r2*f[4];
+    // auto s = _mm256_permute2f128_pd(t,t,0x01);
+    // auto s = _mm256d{ t[2],t[3],t[0],t[1] } ; 
+    // return (s+t)[0] + r2*f[4];
+}
+
+inline __m256d dots4_5_avx(const double* f, __m256d r0, double r2) {
+    auto x0 = r0*_mm256_loadu_pd(f);
+    auto x1 = r0*_mm256_loadu_pd(f+5);
+    auto x2 = r0*_mm256_loadu_pd(f+10);
+    auto x3 = r0*_mm256_loadu_pd(f+15);
+    auto temp01 = _mm256_hadd_pd( x0, x1 );   
+    auto temp23 = _mm256_hadd_pd( x2, x3 );
+    auto swapped = _mm256_permute2f128_pd( temp01, temp23, 0x21 );
+    auto blended = _mm256_blend_pd(temp01, temp23, 0xC ); // 0b1100
+    return swapped+blended+( __m256d{ f[4],f[9],f[14],f[19]} * r2 );
 }
 
 // reshuffle a symmetric, lower triangle, row major matrix for SIMD use...
@@ -80,30 +69,27 @@ void similarity_5_5_avx(const double* Ci, const double* Fi, double* Ti)  {
       // reshuffle the Ci matrix for SIMD use...
       avx_5_t m { Ci };
      
-      auto _0 = m.c0i(Fi);
-      auto _4 = m.c4i(Fi);
-      Ti[ 0] = dot5_avx(Fi   ,_0,_4);
-      Ti[ 1] = dot5_avx(Fi+ 5,_0,_4);
-      Ti[ 3] = dot5_avx(Fi+10,_0,_4);
-      Ti[ 6] = dot5_avx(Fi+15,_0,_4);
+      auto _0 = m.c0i(Fi); auto _4 = m.c4i(Fi);
+      auto r = dots4_5_avx(Fi,_0,_4);
+      Ti[ 0] = r[0];
+      Ti[ 1] = r[1];
+      Ti[ 3] = r[2];
+      Ti[ 6] = r[3];
       Ti[10] = dot5_avx(Fi+20,_0,_4);
-      _0 = m.c0i(Fi+5);
-      _4 = m.c4i(Fi+5);
-      Ti[ 2] = dot5_avx(Fi+ 5,_0,_4);
-      Ti[ 4] = dot5_avx(Fi+10,_0,_4);
-      Ti[ 7] = dot5_avx(Fi+15,_0,_4);
-      Ti[11] = dot5_avx(Fi+20,_0,_4);
-      _0 = m.c0i(Fi+10);
-      _4 = m.c4i(Fi+10);
+      _0 = m.c0i(Fi+5); _4 = m.c4i(Fi+5);
+      r = dots4_5_avx(Fi+5,_0,_4);
+      Ti[ 2] = r[0];
+      Ti[ 4] = r[1];
+      Ti[ 7] = r[2];
+      Ti[11] = r[3];
+      _0 = m.c0i(Fi+10); _4 = m.c4i(Fi+10);
       Ti[ 5] = dot5_avx(Fi+10,_0,_4);
       Ti[ 8] = dot5_avx(Fi+15,_0,_4);
       Ti[12] = dot5_avx(Fi+20,_0,_4);
-      _0 = m.c0i(Fi+15);
-      _4 = m.c4i(Fi+15);
+      _0 = m.c0i(Fi+15); _4 = m.c4i(Fi+15);
       Ti[ 9] = dot5_avx(Fi+15,_0,_4);
       Ti[13] = dot5_avx(Fi+20,_0,_4);
-      _0 = m.c0i(Fi+20);
-      _4 = m.c4i(Fi+20);
+      _0 = m.c0i(Fi+20); _4 = m.c4i(Fi+20);
       Ti[14] = dot5_avx(Fi+20,_0,_4);
 }
 
@@ -115,34 +101,38 @@ void similarity_5_7_avx(const double* Ci, const double* Fi, double* Ti)  {
       auto _0 = m.c0i(Fi);
       auto _4 = m.c4i(Fi);
       //TODO: switch to snippet above which computes 4 dot products at the same time....
-      Ti[ 0] = dot5_avx(Fi   ,_0,_4);
-      Ti[ 1] = dot5_avx(Fi+ 5,_0,_4);
-      Ti[ 3] = dot5_avx(Fi+10,_0,_4);
-      Ti[ 6] = dot5_avx(Fi+15,_0,_4);
+      auto r = dots4_5_avx(Fi,_0,_4);
+      Ti[ 0] = r[0];
+      Ti[ 1] = r[1];
+      Ti[ 3] = r[2];
+      Ti[ 6] = r[3];
       Ti[10] = dot5_avx(Fi+20,_0,_4);
       Ti[15] = dot5_avx(Fi+25,_0,_4);
       Ti[21] = dot5_avx(Fi+30,_0,_4);
       _0 = m.c0i(Fi+5);
       _4 = m.c4i(Fi+5);
-      Ti[ 2] = dot5_avx(Fi+ 5,_0,_4);
-      Ti[ 4] = dot5_avx(Fi+10,_0,_4);
-      Ti[ 7] = dot5_avx(Fi+15,_0,_4);
-      Ti[11] = dot5_avx(Fi+20,_0,_4);
+      r = dots4_5_avx(Fi+5,_0,_4);
+      Ti[ 2] = r[0];
+      Ti[ 4] = r[1];
+      Ti[ 7] = r[2];
+      Ti[11] = r[3];
       Ti[16] = dot5_avx(Fi+25,_0,_4);
       Ti[22] = dot5_avx(Fi+30,_0,_4);
       _0 = m.c0i(Fi+10);
       _4 = m.c4i(Fi+10);
-      Ti[ 5] = dot5_avx(Fi+10,_0,_4);
-      Ti[ 8] = dot5_avx(Fi+15,_0,_4);
-      Ti[12] = dot5_avx(Fi+20,_0,_4);
-      Ti[17] = dot5_avx(Fi+25,_0,_4);
+      r = dots4_5_avx(Fi+10,_0,_4);
+      Ti[ 5] = r[0];
+      Ti[ 8] = r[1];
+      Ti[12] = r[2];
+      Ti[17] = r[3];
       Ti[23] = dot5_avx(Fi+30,_0,_4);
       _0 = m.c0i(Fi+15);
       _4 = m.c4i(Fi+15);
-      Ti[ 9] = dot5_avx(Fi+15,_0,_4);
-      Ti[13] = dot5_avx(Fi+20,_0,_4);
-      Ti[18] = dot5_avx(Fi+25,_0,_4);
-      Ti[24] = dot5_avx(Fi+30,_0,_4);
+      r = dots4_5_avx(Fi+15,_0,_4);
+      Ti[ 9] = r[0];
+      Ti[13] = r[1];
+      Ti[18] = r[2];
+      Ti[24] = r[3];
       _0 = m.c0i(Fi+20);
       _4 = m.c4i(Fi+20);
       Ti[14] = dot5_avx(Fi+20,_0,_4);
