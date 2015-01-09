3d87ba91beb0c231f4d6af5079892b7c8223bda1
Michel De Cian <michel.de.cian@cern.ch>
2015-01-09 21:20:46 +0000

    New parametrisations in 'PatMatchTool', lower ghost right and higher efficiency, mostly for low p tracks.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@182242 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 01a9cc12035cdf9660f104dbff3a6127c8018177

--- Rec/Tf/PatAlgorithms/src/PatMatchTool.cpp
+++ Rec/Tf/PatAlgorithms/src/PatMatchTool.cpp
@@ -28,31 +28,41 @@ DECLARE_TOOL_FACTORY(PatMatchTool)
 //=============================================================================
 // Standard constructor, initializes variables
 //=============================================================================
-  PatMatchTool::PatMatchTool(const std::string& type,
-                             const std::string& name, const IInterface* parent) :
-    GaudiTool(type, name , parent),
-    m_fastMomentumTool(0), m_addTTClusterTool(0)
+PatMatchTool::PatMatchTool(const std::string& type,
+                           const std::string& name, const IInterface* parent) :
+GaudiTool(type, name , parent),
+  m_fastMomentumTool(0), m_addTTClusterTool(0)
 {
   declareInterface<IMatchTool>(this);
   declareInterface<ITrackMatch>(this);
-
-  declareProperty("zMagnet"         , m_zMagnet       = 5200. * Gaudi::Units::mm);
-  declareProperty("zMagnetTx2"      , m_zMagnetTx2    =-1700. * Gaudi::Units::mm);
-  declareProperty("zMagnetDsl2"     , m_zMagnetDsl2   =  500. * Gaudi::Units::mm);
-  declareProperty("zMatchY"         , m_zMatchY       = 8420. * Gaudi::Units::mm);
-  declareProperty("dxTol"           , m_dxTol         =    8. * Gaudi::Units::mm);
-  declareProperty("dxTolSlope"      , m_dxTolSlope    =   80. * Gaudi::Units::mm);
-  declareProperty("dyTol"           , m_dyTol         =    6. * Gaudi::Units::mm);
-  declareProperty("dyTolSlope"      , m_dyTolSlope    =  300. * Gaudi::Units::mm);
-  declareProperty("MagnetBend"      , m_magnetBend    =-1000. * Gaudi::Units::mm);
-  declareProperty("maxMatchChi2"    , m_maxChi2       = 4);
-  declareProperty("MinPt"                 , m_minPt                 =  0. * Gaudi::Units::MeV );
-  declareProperty("MinMomentum"           , m_minMomentum           =  0. * Gaudi::Units::GeV );
-  declareProperty("FastMomentumToolName",
-                  m_fastMomentumToolName = "FastMomentumEstimate");
-  declareProperty("AddTTClusters"   , m_addTT            = true);
-  declareProperty("AddTTClusterName", m_addTTClusterName = "PatAddTTCoord");
-  declareProperty("writeNNVariables", m_writeNNVariables = true);
+  
+  // -- The main cut value
+  declareProperty("maxMatchChi2"        , m_maxChi2              = 4.8);
+  // -- Parameters for z position of matching in x-coordinate
+  declareProperty("zMagnet"             , m_zMagnet              = 5197.83 * Gaudi::Units::mm);
+  declareProperty("zMagnetTx2"          , m_zMagnetTx2           = -1372.63 * Gaudi::Units::mm);
+  declareProperty("zMagnetDsl"          , m_zMagnetDsl           = 13.5453     );
+  declareProperty("zMagnetDsl2"         , m_zMagnetDsl2          = 297.853 * Gaudi::Units::mm);
+  declareProperty("zMagnetSeedX"        , m_zMagnetSeedX         = 0.0251294 );
+  // -- Parameters for matching in y-coordinate
+  declareProperty("zMatchY"             , m_zMatchY              = 8420. * Gaudi::Units::mm);
+  declareProperty("MagnetBendDsl"       , m_magnetBendDsl        = -646.047);
+  declareProperty("MagnetBendDslY"      , m_magnetBendDslY       = -44118.5);
+  // -- Tolerances
+  declareProperty("dxTol"               , m_dxTol                =    8. * Gaudi::Units::mm);
+  declareProperty("dxTolSlope"          , m_dxTolSlope           =   60. * Gaudi::Units::mm);
+  declareProperty("dyTol"               , m_dyTol                =    6. * Gaudi::Units::mm);
+  declareProperty("dyTolSlope"          , m_dyTolSlope           =  300. * Gaudi::Units::mm);
+  
+  declareProperty("MinPt"               , m_minPt                =  0. * Gaudi::Units::MeV );
+  declareProperty("MinMomentum"         , m_minMomentum          =  0. * Gaudi::Units::GeV );
+  declareProperty("FastMomentumToolName", m_fastMomentumToolName = "FastMomentumEstimate");
+  declareProperty("AddTTClusters"       , m_addTT                = true);
+  declareProperty("AddTTClusterName"    , m_addTTClusterName     = "PatAddTTCoord");
+  declareProperty("writeNNVariables"    , m_writeNNVariables     = true);
+  // -- Use the Velo and Seed tracks in more than one match candidate?
+  declareProperty("RejectVeloUsed"      , m_rejectVeloUsed       = true);
+  declareProperty("RejectSeedUsed"      , m_rejectSeedUsed       = true);
 
 }
 //=============================================================================
@@ -79,7 +89,9 @@ StatusCode PatMatchTool::initialize()
 
   return StatusCode::SUCCESS;
 }
-
+//=============================================================================
+// Match single combination only
+//=============================================================================
 StatusCode PatMatchTool::matchSingle(const LHCb::Track& velo,
                                      const LHCb::Track& seed , LHCb::Track& output, double& chi2)
 {
@@ -132,9 +144,14 @@ StatusCode PatMatchTool::match(const LHCb::Tracks& velos,
     }
 
     bool& vUsed = used[vTr];
-    if (vUsed) continue;
+    if( m_rejectVeloUsed ){
+      if (vUsed) continue;
+    }
+    
     bool& sUsed = used[sTr];
-    if (sUsed) continue;
+    if( m_rejectSeedUsed ){
+      if (sUsed) continue;
+    }
 
     //== estimate q/p
     double qOverP, sigmaQOverP;
@@ -174,8 +191,7 @@ StatusCode PatMatchTool::match(const LHCb::Tracks& velos,
 
     // added for NNTools -- check how many tracks have common hits
     // This is always 1.0 as we require that velo and seed tracks are used only once.
-    if (m_writeNNVariables)
-      match->addInfo(LHCb::Track::NCandCommonHits, 1.0);
+    if (m_writeNNVariables) match->addInfo(LHCb::Track::NCandCommonHits, 1.0);
   }//end loop match cands
 
   return StatusCode::SUCCESS;
@@ -192,7 +208,8 @@ StatusCode PatMatchTool::finalize()
 }
 
 //=============================================================================
-
+//  Calculate the matching chi2
+//=============================================================================
 double PatMatchTool::getChi2Match(const LHCb::Track& velo,
                                   const LHCb::Track& seed) const
 {
@@ -206,21 +223,37 @@ double PatMatchTool::getChi2Match(const LHCb::Track& velo,
   const LHCb::State& sState = seed.closestState(m_zMatchY);
 
   const double dSlope = vState.tx() - sState.tx();
-  const double zForX = m_zMagnet
-    + m_zMagnetTx2 * vState.tx() * vState.tx()
-    + m_zMagnetDsl2 * dSlope * dSlope;
+  const double dSlopeY = vState.ty() - sState.ty();
+  
+  // -- This is the function that calculates the z position to match the x-coordinates
+  // -- The parametrisation tries to take all (relevant) dependencies into account
+  // -- and can be derived with the MatchFitParams package
+  const double zForX = m_zMagnet + m_zMagnetDsl*std::abs(dSlope) + 
+    m_zMagnetDsl2*dSlope*dSlope + m_zMagnetSeedX*std::abs(sState.x()) + 
+    m_zMagnetTx2*vState.tx()*vState.tx();
+
   const double dxTol2 = m_dxTol * m_dxTol;
   const double dxTolSlope2 = m_dxTolSlope * m_dxTolSlope;
 
   const double xV = vState.x() + (zForX     - vState.z()) * vState.tx();
+  
+  // -- This is the function that calculates the 'bending' in y-direction
+  // -- The parametrisation can be derived with the MatchFitParams package
   const double yV = vState.y() + (m_zMatchY - vState.z()) * vState.ty() +
-    vState.ty() * dSlope * dSlope * m_magnetBend;
-
+    vState.ty() * ( m_magnetBendDsl*dSlope*dSlope + m_magnetBendDslY*dSlopeY*dSlopeY );
+  
   const double xS = sState.x() + (zForX     - sState.z()) * sState.tx();
   const double yS = sState.y() + (m_zMatchY - sState.z()) * sState.ty();
 
   const double distX = xS - xV;
   const double distY = yS - yV;
+  
+  // -- These are sanity cuts, such that unreasonable chi2 values 
+  // -- are not calculated
+  if( std::abs( distX ) > 50.0 ) return chi2;
+  if( std::abs( distY ) > 130.0 ) return chi2;
+
+
   const double dslx  = vState.tx() - sState.tx();
   const double tolX = dxTol2 + dslx * dslx *  dxTolSlope2;
   const double teta2 = vState.tx() * vState.tx() + vState.ty() * vState.ty();
@@ -234,6 +267,8 @@ double PatMatchTool::getChi2Match(const LHCb::Track& velo,
   return chi2;
 }
 
+//=============================================================================
+//  Make LHCb track out of internal class
 //=============================================================================
 void PatMatchTool::makeTrack(const LHCb::Track& velo,
                              const LHCb::Track& seed, LHCb::Track& output, double chi2) const
@@ -294,7 +329,8 @@ void PatMatchTool::makeTrack(const LHCb::Track& velo,
   //== add copied states to output track
   output.addToStates(newstates);
 }
-
+//=============================================================================
+//  Wrapper for matching a single combination
 //=============================================================================
 StatusCode PatMatchTool::match(const LHCb::Track& veloTrack,
                                const LHCb::Track& tTrack,
--- Rec/Tf/PatAlgorithms/src/PatMatchTool.h
+++ Rec/Tf/PatAlgorithms/src/PatMatchTool.h
@@ -23,6 +23,26 @@ class IAddTTClusterTool;
 /** @class PatMatchTool PatMatchTool.h
  *
  * Match Velo and Seed tracks
+ * The parameters used to calculate the chi2 can be derived using the "MatchFitParams" algorithm.
+ *
+ * Parameters:
+ * 
+ * - maxMatchChi2: Cut value for matching chi2. This is the main classifier that influences efficiency / ghost rate.
+ * - zMagnet, zMagnetTx2, zMagnetDsl, zMagnetDsl2, zMagnetSeedX: Parameters (coefficients) to calculate the z-position for the matching in the x-coordinate
+ * - zMatchY, MagnetBendDsl, MagnetBendDslY: Parameters (coefficients) for the matching in the y-coordinate
+ * - dxTol, dxTolSlope, dyTol, dyTolSlope: Tolerances for the chi2 calculation.
+ * - MinPt: Minimum pT for a track to be accepted
+ * - MinMomentum: Mimimum momentum for a track to be accepted.
+ * - FastMomentumToolName: Name of the tool that calculates the momentum.
+ * - AddTTClusters: Add TT hits to this track (when possible)?
+ * - AddTTClusterName: Name of the tool that adds TT hits.
+ * - writeNNVariables: Write out LHCb::Track::NCandCommonHits?
+ * - RejectVeloUsed: Use a velo track only once?
+ * - RejectSeedUsed: Use a seed track only once?
+ *
+ * @author Michel De Cian
+ * @date 2015-01-09
+ *  change parametrisation for z(x) and bending in y.
  *
  * @author Manuel Schiller
  * @date 2012-01-31
@@ -38,39 +58,39 @@ class IAddTTClusterTool;
  */
 
 class PatMatchTool : public GaudiTool,
-    virtual public ITrackMatch,
-    virtual public IMatchTool
+                     virtual public ITrackMatch,
+                     virtual public IMatchTool
 {
-    public:
+public:
 	/// Standard constructor
 	PatMatchTool(const std::string& type, const std::string& name,
-		const IInterface* parent);
+               const IInterface* parent);
 	/// Destructor
 	virtual ~PatMatchTool();
 	/// Algorithm initialization
 	virtual StatusCode initialize();
 	/// Algorithm finalization
 	virtual StatusCode finalize();
-
+  
 	/// create match tracks from containers of velo and seed tracks
 	virtual StatusCode match(const LHCb::Tracks& velos,
-		const LHCb::Tracks& seeds,
-		LHCb::Tracks& matchs);
-
+                           const LHCb::Tracks& seeds,
+                           LHCb::Tracks& matchs);
+  
 	/// create a match track from a velo and a seed track
 	virtual StatusCode matchSingle(const LHCb::Track& velo,
-		const LHCb::Track& seed,
-		LHCb::Track& output ,
-		double& chi2);
-
+                                 const LHCb::Track& seed,
+                                 LHCb::Track& output ,
+                                 double& chi2);
+  
 	/// Function for matching in Hlt
 	StatusCode match(const LHCb::Track& veloTrack,
-		const LHCb::Track& tTrack,
-		LHCb::Track& matchedTrack,
-		double& quality, double& quality2);
-
-    public:
-
+                   const LHCb::Track& tTrack,
+                   LHCb::Track& matchedTrack,
+                   double& quality, double& quality2);
+  
+public:
+  
 	/** @class MatchCandidate PatMatch.h
 	 *
 	 * Match candidate for Tf::PatMatch algorithm
@@ -85,64 +105,69 @@ class PatMatchTool : public GaudiTool,
 	 */
 	class MatchCandidate
 	{
-	    public:
+  public:
 		MatchCandidate(const LHCb::Track* vTr,
-			const LHCb::Track* sTr, double dist) :
-		    m_vTr(vTr), m_sTr(sTr), m_dist(dist)
-	        { }
-
+                   const LHCb::Track* sTr, double dist) :
+      m_vTr(vTr), m_sTr(sTr), m_dist(dist)
+    { }
+    
 		~MatchCandidate() { }
-
+    
 		const LHCb::Track* vTr() const { return m_vTr; }
 		const LHCb::Track* sTr() const { return m_sTr; }
 		double dist() const { return m_dist; }
-
+    
 		struct lowerByChi2 {
-		    bool operator()(const MatchCandidate& first,
-			    const MatchCandidate& second) const
-		    { return first.dist() < second.dist(); }
+      bool operator()(const MatchCandidate& first,
+                      const MatchCandidate& second) const
+      { return first.dist() < second.dist(); }
 		};
-
-	    private:
+    
+  private:
 		const LHCb::Track* m_vTr;
 		const LHCb::Track* m_sTr;
 		double m_dist;
 	};
-
-    private:
+  
+private:
 	/// calculate matching chi^2
 	double getChi2Match(const LHCb::Track& velo,
-		const LHCb::Track& seed) const;
-
+                      const LHCb::Track& seed) const;
+  
 	/// merge velo and seed segment to output track
 	void makeTrack(const LHCb::Track& velo,
-		const LHCb::Track& seed, LHCb::Track& output,
-		double chi2) const;
-
+                 const LHCb::Track& seed, LHCb::Track& output,
+                 double chi2) const;
+  
 	// use tool for momentum parametrisation
 	std::string m_fastMomentumToolName;
 	std::string m_addTTClusterName;
+  
 	bool m_addTT;
 	bool m_writeNNVariables;
-
+  bool m_rejectVeloUsed;
+  bool m_rejectSeedUsed;
+  
 	ITrackMomentumEstimate* m_fastMomentumTool;
-
+  
 	IAddTTClusterTool*   m_addTTClusterTool;
-
+  
 	double m_zMagnet;
 	double m_zMagnetTx2;
+  double m_zMagnetDsl;
 	double m_zMagnetDsl2;
+  double m_zMagnetSeedX;
 	double m_zMatchY;
+  double m_magnetBendDsl;
+  double m_magnetBendDslY;
 	double m_dxTol;
 	double m_dxTolSlope;
 	double m_dyTol;
 	double m_dyTolSlope;
-	double m_magnetBend;
-	double m_maxChi2;
+  double m_maxChi2;
   double m_minMomentum;
   double m_minPt;
   
-
 };
 
 #endif // PATMATCHTOOL_H
