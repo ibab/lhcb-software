8006cf6b74507be7b3e5c3f1cf1134071c4bc015
Michel De Cian <michel.de.cian@cern.ch>
2015-01-09 21:23:05 +0000

    Update parameters for 'ParamsVeloTCubic' for 2015 in FastMomentumEstimate
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@182243 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d9d59b728bc44cd323bb14925c0642bfb9d00348

--- Rec/Tr/TrackTools/src/FastMomentumEstimate.cpp
+++ Rec/Tr/TrackTools/src/FastMomentumEstimate.cpp
@@ -1,4 +1,3 @@
-
 // Include files
 #include <cmath>
 
@@ -24,11 +23,11 @@ DECLARE_TOOL_FACTORY( FastMomentumEstimate )
 // Standard constructor, initializes variables
 //=============================================================================
 FastMomentumEstimate::FastMomentumEstimate( const std::string& type,
-					    const std::string& name,
-					    const IInterface* parent )
-  : GaudiTool ( type, name , parent )
+                                            const std::string& name,
+                                            const IInterface* parent )
+: GaudiTool ( type, name , parent )
 {
-
+  
   declareInterface<ITrackMomentumEstimate>(this);
   
   std::vector<double> tmp(1, 0.0);
@@ -54,50 +53,41 @@ StatusCode FastMomentumEstimate::initialize()
   if (sc.isFailure()) return sc;  // error already reported by base class
   
   m_magFieldSvc = svc<ILHCbMagnetSvc>( "MagneticFieldSvc", true );
-
+  
   if ( 4 != m_paramsTParab.size() || 4 != m_paramsTCubic.size() || 
        6 != m_paramsVeloTParab.size() || 6 != m_paramsVeloTCubic.size()){
-  
+    
     m_paramsTParab.clear();
     m_paramsVeloTParab.clear();
     m_paramsTCubic.clear();
     m_paramsVeloTCubic.clear();
-
+    
     if (m_magFieldSvc->useRealMap()){
-      std::vector<double>
-        t1 = boost::assign::list_of (-6.30991) (-4.83533) (-12.9192) (4.23025e-08),
-        t2 = boost::assign::list_of (1.20812) (0.636694) (-0.251334) (0.414017) (2.87247) (-20.0982),
-        t3 = boost::assign::list_of (-6.34025) (-4.85287) (-12.4491) (4.25461e-08),
-        t4 = boost::assign::list_of (1.21174) (0.634127) (-0.242116) (0.412728) (2.82916) (-20.6599);      
-      m_paramsTParab      = t1;
-      m_paramsVeloTParab  = t2;
-      m_paramsTCubic      = t3;
-      m_paramsVeloTCubic  = t4;
+      m_paramsTParab      = { -6.30991, -4.83533, -12.9192, 4.23025e-08 } ;
+      m_paramsVeloTParab  = { 1.20812, 0.636694, -0.251334, 0.414017, 2.87247, -20.0982 };
+      m_paramsTCubic      = { -6.34025, -4.85287, -12.4491, 4.25461e-08 };
+      m_paramsVeloTCubic  = { 1.21352, 0.626691, -0.202483, 0.426262, 2.47057, -13.2917 };
     } else {
-      std::vector<double>
-        t1 = boost::assign::list_of (-6.3453)(-4.77725)(-14.9039)(3.13647e-08),
-        t2 = boost::assign::list_of (1.21909)(0.627841)(-0.235216)(0.433811)(2.92798)(-21.3909),
-        t3 = boost::assign::list_of (-6.31652)(-4.46153)(-16.694)(2.55588e-08),
-        t4 = boost::assign::list_of (1.21485)(0.64199)(-0.27158)(0.440325)(2.9191)(-20.4831);
-      m_paramsTParab      = t1;
-      m_paramsVeloTParab  = t2;
-      m_paramsTCubic      = t3;
-      m_paramsVeloTCubic  = t4;
+      m_paramsTParab      = { -6.3453, -4.77725, -14.9039, 3.13647e-08 };
+      m_paramsVeloTParab  = { 1.21909, 0.627841, -0.235216, 0.433811, 2.92798, -21.3909 };
+      m_paramsTCubic      = { -6.31652, -4.46153, -16.694, 2.55588e-08 };
+      m_paramsVeloTCubic  = { 1.21485, 0.64199, -0.27158, 0.440325, 2.9191, -20.4831 };
     }
   }
-
+  
   return StatusCode::SUCCESS;
 }
-
-
+//=============================================================================
+// Calculate momentum, given T state only
+//=============================================================================
 StatusCode FastMomentumEstimate::calculate( const LHCb::State* tState, double& qOverP, 
-					    double& sigmaQOverP, bool tCubicFit ) const
+                                            double& sigmaQOverP, bool tCubicFit ) const
 {
   double p;
-
+  
   const double tx = tState->tx(); 
   const double ty = tState->ty();
-  const double x0   = tState->x() - tx * tState->z();   
+  const double x0 = tState->x() - tx * tState->z();   
   
   if(tCubicFit){
     p = m_paramsTCubic[0] +
@@ -110,61 +100,59 @@ StatusCode FastMomentumEstimate::calculate( const LHCb::State* tState, double& q
       m_paramsTParab[2] * ty * ty +
       m_paramsTParab[3] * x0 * x0;
   }
-
+  
   const double scaleFactor = m_magFieldSvc->signedRelativeCurrent();
   const double denom = p * scaleFactor * 1e6 * (-1);
-
+  
   if (std::abs(scaleFactor) < 1e-6){
     qOverP = 0.01/Gaudi::Units::GeV;
     sigmaQOverP = 1.0/Gaudi::Units::MeV;
-  }
-  else {
+  } else {
     qOverP = x0/denom;
     sigmaQOverP = m_tResolution * std::fabs(qOverP);
   }
-
+  
   return StatusCode::SUCCESS;
 }
 
-
+//=============================================================================
+// Calculate momentum, given T and Velo state (for matching)
+//=============================================================================
 StatusCode FastMomentumEstimate::calculate( const LHCb::State* veloState, const LHCb::State* tState, 
-					    double& qOverP, double& sigmaQOverP, 
-					    bool tCubicFit ) const
+                                            double& qOverP, double& sigmaQOverP, 
+                                            bool tCubicFit ) const
 {
- 
+  
   double coef;
-
+  
   const double txT = tState->tx();
   const double txV = veloState->tx();
   const double tyV = veloState->ty();
-
+  
   if(tCubicFit){
     coef = ( m_paramsVeloTCubic[0] +
-	     m_paramsVeloTCubic[1] * txT*txT +
-	     m_paramsVeloTCubic[2] * txT*txT*txT*txT +
-	     m_paramsVeloTCubic[3] * txT * txV +
-	     m_paramsVeloTCubic[4] * tyV*tyV +
-	     m_paramsVeloTCubic[5] * tyV*tyV*tyV*tyV );
+             m_paramsVeloTCubic[1] * txT*txT +
+             m_paramsVeloTCubic[2] * txT*txT*txT*txT +
+             m_paramsVeloTCubic[3] * txT * txV +
+             m_paramsVeloTCubic[4] * tyV*tyV +
+             m_paramsVeloTCubic[5] * tyV*tyV*tyV*tyV );
   } else {
     coef = ( m_paramsVeloTParab[0] +
-	     m_paramsVeloTParab[1] * txT*txT +
-	     m_paramsVeloTParab[2] * txT*txT*txT*txT +
-	     m_paramsVeloTParab[3] * txT * txV +
-	     m_paramsVeloTParab[4] * tyV*tyV +
-	     m_paramsVeloTParab[5] * tyV*tyV*tyV*tyV );
+             m_paramsVeloTParab[1] * txT*txT +
+             m_paramsVeloTParab[2] * txT*txT*txT*txT +
+             m_paramsVeloTParab[3] * txT * txV +
+             m_paramsVeloTParab[4] * tyV*tyV +
+             m_paramsVeloTParab[5] * tyV*tyV*tyV*tyV );
   }
-    
-  double proj = sqrt( ( 1. + txV*txV + tyV*tyV ) / ( 1. + txV*txV ) );
-
-
+  
+  const double proj = sqrt( ( 1. + txV*txV + tyV*tyV ) / ( 1. + txV*txV ) );
 
   const double scaleFactor = m_magFieldSvc->signedRelativeCurrent();
 
   if (std::abs(scaleFactor) < 1e-6){
     qOverP = 1.0/Gaudi::Units::GeV;
     sigmaQOverP = 1.0/Gaudi::Units::MeV;
-  }
-  else {
+  } else {
     qOverP = (txV-txT)/( coef * Gaudi::Units::GeV * proj*scaleFactor);
     sigmaQOverP = m_veloPlusTResolution * std::fabs(qOverP);
   }
--- Rec/Tr/TrackTools/src/FastMomentumEstimate.h
+++ Rec/Tr/TrackTools/src/FastMomentumEstimate.h
@@ -12,39 +12,58 @@
 #include "TrackInterfaces/ITrackMomentumEstimate.h"
 
 /** @class FastMomentumEstimate FastMomentumEstimate.h
- *  
+ *
+ *  Calculate momentum of a seed track or a match track using the states and a parametrisation
+ *  The parameters for VeloTCubic can be obtained using the "MatchFitParams" algorithm.
+ *
+ *  Parameters:
+ *
+ *  - ParamsTCubic: Coefficients for momentum calculation using a state in the T stations when using a cubic fit.
+ *  - ParamsTParabola: Coefficients for momentum calculation using a state in the T stations when using a parbolic fit.
+ *  - ParamsVeloTCubic: Coefficients for momentum calculation using a state in the Velo and the T stations when using a cubic fit.
+ *  - ParamsVeloTParabola: Coefficients for momentum calculation using a state in the Velo and the T stations when using a parbolic fit.
+ *  - TResolution: Resolution for q/p, given as: sigma(q/p) = TResolution * (q/p)
+ *  - VeloPlusTResolution: Resolution for q/p, given as: sigma(q/p) = VeloPlusTResolution * (q/p)
+ *
+ *  Note: When giving the momentum parameters / coefficients in the options file, all 4 vectors have to be given.
+ *
+ *
  *  @author Stephanie Hansmann-Menzemer
  *  @date   2007-10-30
  */
 class FastMomentumEstimate : public GaudiTool, virtual public ITrackMomentumEstimate{
 public: 
-  /// Standard construct
+  /// Standard constructor
   FastMomentumEstimate( const std::string& type, 
-			const std::string& name,
-			const IInterface* parent);
-
-  virtual ~FastMomentumEstimate( ); ///< Destructor
-
-  StatusCode initialize();
-
+                        const std::string& name,
+                        const IInterface* parent);
   
-    // Estimate the momentum P of a State in T at ZAtMidT
+  virtual ~FastMomentumEstimate( ); ///< Destructor
+  
+  StatusCode initialize();
+  
+  
+  /// Estimate the momentum P of a State in T at ZAtMidT
   virtual StatusCode calculate( const LHCb::State* tState, double& qOverP, double& sigmaQOverP,
-				bool tCubicFit ) const; 
-
-  // Estimate the momentum P of a velo State and a State in T at ZAtMidT
+                                bool tCubicFit ) const; 
+  
+  /// Estimate the momentum P of a velo State and a State in T (for matching)
   virtual StatusCode calculate( const LHCb::State* veloState, const LHCb::State* tState,
-				double& qOverP, double& sigmaQOverP,
-				bool tCubicFit ) const;
-
+                                double& qOverP, double& sigmaQOverP,
+                                bool tCubicFit ) const;
+  
 protected:
 
 private:
+
   ILHCbMagnetSvc*     m_magFieldSvc;
-  /// Define the parameters of the Z dependance
+  /// Momentum parameters (coefficients) for T state when using cubic fit
   std::vector<double> m_paramsTCubic;
+  /// Momentum parameters (coefficients) for T state when using parabolic fit
   std::vector<double> m_paramsTParab;
+  /// Momentum parameters (coefficients) for Velo and T state when using cubic fit
   std::vector<double> m_paramsVeloTCubic;
+  /// Momentum parameters (coefficients) for Velo and T state when using parabolic fit
   std::vector<double> m_paramsVeloTParab;
   double m_tResolution;
   double m_veloPlusTResolution;
