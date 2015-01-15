7d17e371501796dab1ea7f2d703034675453eb96
Roel Aaij <roel.aaij@cern.ch>
2015-01-15 11:09:11 +0000

    Fix printing of timing table in runOfflineOnline.py and align with latest MooreOnlineConf.
    git-svn-id: http://svn.cern.ch/guest/lhcb/MooreOnline/trunk@182527 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eb6ff24dfe55808df18148644dd44002b7613bdc

--- MooreOnline/MooreScripts/python/MooreScripts/runOfflineOnline.py
+++ MooreOnline/MooreScripts/python/MooreScripts/runOfflineOnline.py
@@ -7,31 +7,69 @@ def configure(**kwargs) :
     OnlineEnv.PartitionID = 0
     OnlineEnv.PartitionIDName = "0000"
     OnlineEnv.PartitionName = kwargs.pop('PartitionName')
-    OnlineEnv.OutputLevel = kwargs.pop('OutputLevel', 4)
+    from Gaudi.Configuration import INFO, WARNING
+    output_level = kwargs.pop('OutputLevel', WARNING)
+    OnlineEnv.OutputLevel = output_level
 
     moore_tests = __import__("MooreTests" , globals(), locals(), [kwargs.get('UserPackage')])
     user_package = getattr(moore_tests, kwargs.pop('UserPackage'))
-
+    input_type = kwargs.pop('InputType', 'MEP')
+    
+    # Only a single setting directly for Moore
     from Moore.Configuration import Moore, MooreExpert
-    from MooreOnlineConf.Configuration import MooreOnline
-
     moore = Moore()
-    moore.OutputLevel = OnlineEnv.OutputLevel
-    if OnlineEnv.OutputLevel < 4:
-        moore.EnableTimer = True
+    moore.OutputLevel = output_level
+
     # We need MooreOnline to setup the buffer manager infrastructure etc, but we
     # don't want to use things like the RunChangeHandler and database snapshots.
+    from MooreOnlineConf.Configuration import MooreOnline
     mooreOnline = MooreOnline()
     mooreOnline.RunOnline = False
+    mooreOnline.EnableTimer = False
     mooreOnline.EnableRunChangeHandler = None
     mooreOnline.UseDBSnapshot = False
-    mooreOnline.EnableTimer = False
     mooreOnline.CheckOdin = False
 
-    # This is the stuff that should come from the PRConfig module
+    # Add the timing auditor by hand with output level INFO, as Moore is never
+    # going to do it for us if the rest is at WARNING
+    from Gaudi.Configuration import ApplicationMgr, AuditorSvc
+    from Configurables import LHCbTimingAuditor, LHCbSequencerTimerTool
+    ApplicationMgr().AuditAlgorithms = 1
+    ta = LHCbTimingAuditor('TIMER')
+    ta.OutputLevel = INFO
+    ta.addTool(LHCbSequencerTimerTool, name = 'TIMER')
+    ta.TIMER.NameSize = 90
+    ta.TIMER.OutputLevel = INFO
+    if 'AuditorSvc' not in ApplicationMgr().ExtSvc:
+        ApplicationMgr().ExtSvc.append('AuditorSvc')
+    AuditorSvc().Auditors.append(ta)
+    ta.Enable = True
+
+    # Hack the shit out of the CondDB services to stop them from spawning a
+    # thread that will segfault on finalize with forking
+    def no_timeout():
+        from Gaudi.Configuration import allConfigurables
+        from Configurables import CondDBAccessSvc
+        for conf in allConfigurables.itervalues():
+            if type(conf) == CondDBAccessSvc:  
+                conf.ConnectionTimeOut = 0
+        
+    from Gaudi.Configuration import appendPostConfigAction
+    appendPostConfigAction(no_timeout)
+    
+    ## from Gaudi.Configuration import appendPostConfigAction
+    ## def info_dammit():
+    ##     from Configurables import LHCbTimingAuditor, LHCbSequencerTimerTool
+    ##     ta = LHCbTimingAuditor('TIMER')
+    ##     ta.OutputLevel = OnlineEnv.OutputLevel
+    ##     ta.addTool(LHCbSequencerTimerTool, name = 'TIMER')
+    ##     ta.TIMER.OutputLevel = OnlineEnv.OutputLevel
+    ## appendPostConfigAction(info_dammit)
+                    
+    # This is the stuff that should come from the PRConfig user module
     for conf, d in { moore: {'DDDBtag' : str, 'CondDBtag' : str},
                      mooreOnline : {'UseTCK' : bool, 'Simulation' : bool,
-                                    'DataType' : ('2011', '2012', '2013', '2014'),
+                                    'DataType' : ('2011', '2012', '2013', '2014', '2015'),
                                     'HltLevel' : ('Hlt1', 'Hlt2', 'Hlt1Hlt2')}}.iteritems():
         for a, t in d.iteritems():
             ua = user_package.MooreOptions.pop(a)
@@ -42,12 +80,15 @@ def configure(**kwargs) :
             conf.setProp(a, ua)
     if 'InitialTCK' in user_package.MooreOptions:
         moore.setProp('InitialTCK', user_package.MooreOptions['InitialTCK'])
-                        
-    if mooreOnline.HltLevel in ["Hlt1", "Hlt1Hlt2"]:
+
+    if input_type == 'MEP' and 'Hlt1' in mooreOnline.HltLevel:
         mooreOnline.REQ1 = "EvType=1;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
-    else:
+    elif input_type == 'MDF' and 'Hlt1' in mooreOnline.HltLevel:
+        mooreOnline.ForceMDFInput = True
         mooreOnline.REQ1 = "EvType=2;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
-    
+    elif mooreOnline.HltLevel == 'Hlt2':
+        mooreOnline.REQ1 = "EvType=2;TriggerMask=0xffffffff,0xffffffff,0xffffffff,0xffffffff;VetoMask=0,0,0,0;MaskType=ANY;UserType=ONE;Frequency=PERC;Perc=100.0"
+        
     # Apparently we need to set this, otherwise something goes wrong with
     # default properties being retrieved that have the wrong type.
     from Gaudi.Configuration import EventSelector    
@@ -71,5 +112,4 @@ def configure(**kwargs) :
 
     user_package.configure()
 
-    import OnlineEnv 
     OnlineEnv.end_config(False)
