f7914e1d69ceba0dbabf13d0c98384d513a22491
Marco Cattaneo <marco.cattaneo@cern.ch>
2015-01-15 11:51:21 +0000

    Update tests to use an input file from PRCONFIG (on eos) instead of a private Castor file
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@182530 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 35d71832e6acb822d0cd652423821c2f678de167

--- LHCb/Kernel/XMLSummaryKernel/options/RootDst.py
+++ LHCb/Kernel/XMLSummaryKernel/options/RootDst.py
@@ -1,8 +1,7 @@
 from Gaudi.Configuration import *
 from GaudiConf import IOHelper
-IOHelper().inputFiles( [
-  'PFN:root://castorlhcb.cern.ch//castor/cern.ch/user/c/cattanem/Brunel/v34r5/00001820_00000001.dst?svcClass=default'
-], clear=True)
-
+# input file
+importOptions("$PRCONFIGOPTS/DaVinci/Stripping/Collision12-4TeV-Reco14-FULLDST.py")
+# output file
 IOHelper().outStream("RootDst.root","InputCopyStream")
 
