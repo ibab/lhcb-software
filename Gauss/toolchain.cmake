837bd114884394d6c9dc1e9672c4ee60f784bab5
Gloria Corti <gloria.corti@cern.ch>
2015-01-20 11:13:55 +0000

    remove comments, version match that in cmt
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@182903 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 440282dda06b87aa1658cca7f4f781969387fcde

--- Gauss/toolchain.cmake
+++ Gauss/toolchain.cmake
@@ -5,7 +5,7 @@ find_file(inherit_heptools_module InheritHEPTools.cmake)
 # FIXME: generator versions must be moved to another file
 set(generators_versions
     alpgen      2.1.4
-    herwig++    2.7.1      # 2.7.0a not available in LCG_70root6
+    herwig++    2.7.1
     hijing      1.383bs.2
     lhapdf      6.1.4
     photos++    3.56
@@ -14,7 +14,7 @@ set(generators_versions
     pythia8     186
     rivet       1.9.0
     tauola++    1.1.4
-    thepeg      1.9.2p1    # 1.9.0a not available in LCG_70root6
+    thepeg      1.9.2p1
     )
 
 # Process the lines of LCG_generators_*.txt file to extract the
