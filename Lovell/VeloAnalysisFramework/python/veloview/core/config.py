b27bad06eb282b84e02536a2fa7f9b28c6e9bd9e
Alex Pearce <alex.pearce@cern.ch>
2015-01-21 12:25:51 +0000

    Add run data directory modification method.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@183013 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6fb382df7a47563f113483160853f4a8dd73d755

--- Lovell/VeloAnalysisFramework/python/veloview/core/config.py
+++ Lovell/VeloAnalysisFramework/python/veloview/core/config.py
@@ -4,4 +4,13 @@
 run_data_dir = "/calib/velo/dqm/VeloView/VetraOutput"
 
 # Absolute path to the text file containing the list of processed run numbers
-processed_run_list_file = "/calib/velo/dqm/VeloView/VetraOutput/RunList.txt"
+processed_run_list_file = "{0}/RunList.txt".format(run_data_dir)
+
+def update_run_data_dir(path):
+    """Update run data directory configuration key and dependents."""
+    from veloview.core import config
+    old_dir = config.run_data_dir
+    config.run_data_dir = path
+    config.processed_run_list_file = config.processed_run_list_file.replace(
+        old_dir, path
+    )
