1a4ea004cfb301046c07f71c303d8d59a2e8b0af
Ben Couturier <lben@cern.ch>
2015-01-22 00:09:45 +0000

    Added NoPID particle maker
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@183067 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 575261b59f4c1f596acc4c28aa755ec5d66e6251

--- Online/Online/WebGLDisplay/JsonConverter/options/test-converter.py
+++ Online/Online/WebGLDisplay/JsonConverter/options/test-converter.py
@@ -1,5 +1,7 @@
 # Config for performance testing 
 from Gaudi.Configuration import *
+from Configurables import NoPIDsParticleMaker
+from CommonParticles.StdAllNoPIDsPions import StdAllNoPIDsPions
 
 importOptions("$BRUNELROOT/options/Brunel-Default.py")
 importOptions("$PRCONFIGOPTS/Brunel/PR-COLLISION12-Beam4000GeV-VeloClosed-MagDown.py")
@@ -9,19 +11,21 @@ from Gaudi.Configuration import FileCatalog, EventSelector
 from Configurables import Brunel
 Brunel().EvtMax=10
 
+pmaker =  NoPIDsParticleMaker ( 'StdNoPIDsPions' , Particle = 'pion'  )
+
 
 def postConfig():
 
     from Configurables import JsonConverter
     conv = JsonConverter()
-
+    #conv.OutputLevel = DEBUG
     from Configurables import StoreExplorerAlg
     storeExp = StoreExplorerAlg("StoreExplorer1")
     storeExp.Load = 1
     storeExp.PrintFreq = 1
 
     #GaudiSequencer("PhysicsSeq").Members += [ storeExp,  conv ]
-    GaudiSequencer("PhysicsSeq").Members += [ storeExp, conv ]
+    GaudiSequencer("PhysicsSeq").Members += [ pmaker, storeExp, conv ]
 
 
 
