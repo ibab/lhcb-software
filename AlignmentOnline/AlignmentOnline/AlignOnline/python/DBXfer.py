69f8e6d775794c2bcaeb89aba15705a28a052e82
Beat Jost <beat.jost@cern.ch>
2015-02-04 09:32:22 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/AlignmentOnline/trunk@183439 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: e45919c755dde2a00422a6f665da1fef74909795

--- AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer.py
+++ AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer.py
@@ -1,6 +1,10 @@
 from Gaudi.Configuration import *
 from Configurables import LHCbAlgsTests__TestTimeDecoderOdin as TimeDecoder
 from Configurables import (RunChangeHandlerSvc, DBXferAlg )
+import RunOption
 DetectorPersistencySvc( CnvServices = [ xmlCnvSvc ] )
 rch = RunChangeHandlerSvc()
 rch.Conditions = {"Condition": "../data/runchangehandler/run%04d.xml"}
+DBXferAlg.RunNumber = RunOption.RunNumber
+DBXferAlg.RunStartTime = RunOption.RunStartTime
+DBXferAlg.OnlineXmlDir = RunOption.OutputDirectory
