88aa9ecec855cf5af25c35b106b08153e9ffc3cd
Ben Couturier <ben.couturier@cern.ch>
2015-02-05 12:48:34 +0000

    LBCORE-735 Add --reprocess to cern-get-sso-cookie
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@183543 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cbea87a1b5bad9a44cb39a1a2a40c2cfe4d448d5

--- LbScripts/LbRelease/scripts/lb-sdb-env.sh
+++ LbScripts/LbRelease/scripts/lb-sdb-env.sh
@@ -16,7 +16,7 @@ setup_env () {
 
 command -v cern-get-sso-cookie >/dev/null 2>&1 || { echo >&2 "I need 'cern-get-sso-cookie' package to run but it's not installed. Aborting."; exit 1; }
 export cookie_file=/tmp/$USER/ssocookie-ariadne.txt
-cern-get-sso-cookie --krb -u https://ariadne-lhcb.cern.ch/ -o $cookie_file
+cern-get-sso-cookie --krb -u https://ariadne-lhcb.cern.ch/ --reprocess -o $cookie_file
 if [ $? != 0 ] ; then
     printf "WARNING: Error encountered while acquiring CERN SSO cookie for Ariadne. Attempting to connect with no SSO..\n\n"
 fi    
