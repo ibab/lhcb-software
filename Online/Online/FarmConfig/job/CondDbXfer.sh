30619de763f04eab6c18b18578bab912ecbe34a1
Beat Jost <beat.jost@cern.ch>
2015-02-11 15:12:24 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@183748 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 1430d3809c31bd6de27ab3c3e3d5421042cac8b1

--- Online/Online/FarmConfig/job/CondDbXfer.sh
+++ Online/Online/FarmConfig/job/CondDbXfer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo $1
-exit 0
+echo $2
+echo $3
+source /group/online/dataflow/cmtuser/ALIGNMENTONLINE/ALIGNMENTONLINE_HEAD/AlignmentOnline/AlignOnline/cmt/setup.x86_64-slc6-gcc48-dbg.vars
+export PYTHONPATH=$1:/group/online/hlt/conditions/RunChangeHandler:$PYTHONPATH
+unset GAUDIAPPNAME
+python $ALIGNONLINEROOT/python/DBXfer.py
+#exit 0
