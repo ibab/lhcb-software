8c841072ecf3b0da8bb5abd1d25d49e3a84360e7
Markus Frank <frankb@cern.ch>
2015-02-16 11:57:43 +0000

    v5r16
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@183922 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 121409e86e20ee43078a2042c94f854a9d649bc3

--- Online/OnlineSys/cmt/release_utils.sh
+++ Online/OnlineSys/cmt/release_utils.sh
@@ -85,6 +85,40 @@ remove_tag()
     svn rm -m "Need to remove tag Online/$1 version $2" svn+ssh://svn.cern.ch/reps/lhcb/Online/tags/Online/${1}/${2};
 }
 echo "+++"
+echo "+++ Define macro \$> remove_sys_tag <version> "
+remove_sys_tag()
+{
+    if test "$1" != "";
+    then
+	echo "+++ Package version = $1";
+    else
+	echo "+++ The second argument to make_tag must be the package version";
+    fi;
+#
+#
+#
+    if test "$ONLINESYSROOT" = "";
+    then
+	eval `cmt run printenv|grep ONLINESYSROOT`;
+    fi;
+#
+#
+#
+    while true; do
+	read -p "+++ Is this correct? Y/N Cr=[Y]" yn;
+	case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) return 0;;
+            * ) break;;
+	esac;
+    done;
+#
+#
+#
+    echo "+++ Removing tag from package: OnlineSys $2 ";
+    svn rm -m "Need to remove tag Online/$1 version $2" svn+ssh://svn.cern.ch/reps/lhcb/Online/tags/OnlineSys/${2};
+}
+echo "+++"
 echo "+++ Define macro \$> make_release <version> "
 make_release()
 {
