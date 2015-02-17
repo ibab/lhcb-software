414c8bf6fcc1c33e9a7f9d74b67541af1b7a4068
Markus Frank <frankb@cern.ch>
2015-02-17 16:31:36 +0000

    New package tag v1r29
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@183983 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 115a852d6652b73e028bf2b875e16a2cb9847a93

--- Online/Online/UPI/UPI/UpiSensor.h
+++ Online/Online/UPI/UPI/UpiSensor.h
@@ -1,33 +1,44 @@
+// ====================================================================
+//  UpiSensor
+// --------------------------------------------------------------------
+//
+//	Author    : Markus Frank
+//
+// ====================================================================
 #ifndef CPP_UPISENSOR_H
 #define CPP_UPISENSOR_H 1
 
 #include "CPP/Sensor.h"
 #include <map>
 
-/** @class UpiSensor UpiSensor.h CPP/UpiSensor.h
-  *
-  *	Sensor to receive UPI input type
-  *      The UpiSensor  class is derivated from the Sensor class
-  *
-  *  @author P. Mato
-  *
-  */
-class UpiSensor : public Sensor {
-  typedef std::map<int,Interactor*> Table;
-  Table    m_table;
-  UpiSensor();
-  virtual ~UpiSensor();
-public:
-  static UpiSensor& instance();
-  static int newID();
-  virtual void  add(Interactor* source, void* id)
-  {  Sensor::add(source,id);                             }
-  virtual void  add(Interactor* source, int id);
-  virtual void  remove(Interactor* source, int id);
-  virtual void  remove(Interactor* source, void* id)
-  {  Sensor::remove(source,id);                          }
-  virtual void  dispatch(void* param);
-  virtual void  rearm() { }
-};
+/// CPP namespace declaration
+namespace CPP {
+
+  /// Sensor to receive UPI input type
+  /**
+   *    The UpiSensor  class is derivated from the Sensor class
+   *
+   *  \author P. Mato
+   *  \author M. Frank
+   */
+  class UpiSensor : public CPP::Sensor {
+    typedef std::map<int,CPP::Interactor*> Table;
+    Table    m_table;
+    UpiSensor();
+    virtual ~UpiSensor();
+  public:
+    static UpiSensor& instance();
+    static int newID();
+    virtual void  add(CPP::Interactor* source, void* id)
+    {  Sensor::add(source,id);                             }
+    virtual void  add(CPP::Interactor* source, int id);
+    virtual void  remove(CPP::Interactor* source, int id);
+    virtual void  remove(CPP::Interactor* source, void* id)
+    {  Sensor::remove(source,id);                          }
+    virtual void  dispatch(void* param);
+    virtual void  rearm() { }
+  };
+}
+using CPP::UpiSensor;
 
 #endif    // CPP_UPISENSOR_H
