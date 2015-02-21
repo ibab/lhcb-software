8d3f13378bf42b3102276f8a1e2f1f11d59f741e
Manuel Tobias Schiller <manuel.schiller@cern.ch>
2015-02-21 19:43:19 +0000

    TrackUsedLHCbIDs: make hit lookup O(1) instead of O(ln(Nhits)) by using BloomFilters
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@184163 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6393f4ae895c983690047b06e855d33942398c62

--- Rec/Tr/TrackTools/src/TrackUsedLHCbID.cpp
+++ Rec/Tr/TrackTools/src/TrackUsedLHCbID.cpp
@@ -1,113 +1,149 @@
 #include "GaudiKernel/ToolFactory.h"
 #include "GaudiKernel/IIncidentSvc.h"
 
-
 #include "TrackInterfaces/ITrackSelector.h"
 
-// Tsa
 #include "TrackUsedLHCbID.h"
 #include "Event/Track.h"
 
-//stl
 #include <algorithm>
-
+#include <exception>
 
 DECLARE_TOOL_FACTORY( TrackUsedLHCbID )
-  
-TrackUsedLHCbID::TrackUsedLHCbID(const std::string& type,
-                                   const std::string& name,
-                                   const IInterface* parent):
-GaudiTool(type, name, parent),
-m_configured(false)
-{
 
+TrackUsedLHCbID::TrackUsedLHCbID(const std::string& type,
+    const std::string& name, const IInterface* parent) :
+  GaudiTool(type, name, parent), m_flags(0)
+{
+  declareInterface<IUsedLHCbID>(this);
   /** Define containers and corresponding selectors in same order.
    *  E.g. inputContainers = "Rec/Track/Forward" and selectorNames = "ForwardSelector".
    */
- 
   declareProperty("inputContainers", m_inputs = {""});
   declareProperty("selectorNames", m_names = {""});
-
-
-  declareInterface<IUsedLHCbID>(this);
-  m_usedCont.reserve(5000);
 }
 
-TrackUsedLHCbID::~TrackUsedLHCbID(){
-  // destructer
+TrackUsedLHCbID::~TrackUsedLHCbID()
+{
+  // destructor
 }
 
 
 StatusCode TrackUsedLHCbID::initialize()
 {
-
   StatusCode sc = GaudiTool::initialize();
-  if (sc.isFailure()){
-    return Error("Failed to initialize", sc);
-  }
+  if (sc.isFailure()) return sc;
 
-  if ( m_names.size() != m_inputs.size() ){
-    debug()<<"Number of selectors and number of inputs disagree. Initialize as many selectors as inputs."<<endmsg;
-    m_names.clear();
-    for (auto inputs : m_inputs )
-      m_names.push_back("");
+  if (m_names.size() != m_inputs.size()) {
+    if (m_names.size() > m_inputs.size()) {
+      Warning("More selector names than input locations, discarding excess selectors", StatusCode::FAILURE, 1).ignore();
+    } else {
+      Warning("More input locations than selectors, always accepting tracks where selectors are missing.", StatusCode::FAILURE, 1). ignore();
+    }
+    m_names.resize(m_inputs.size());
   }
-  
 
   // make the selector tools
-  for (auto name : m_names){
-    ITrackSelector* aTool = !name.empty() ? tool<ITrackSelector>(name, this) : nullptr;
-    m_selectors.push_back(aTool); 
-  } // iter
+  std::transform(std::begin(m_names), std::end(m_names),
+      std::back_inserter(m_selectors), [this] (const std::string& name) {
+      return name.empty() ? nullptr : tool<ITrackSelector>(name, this);
+      });
 
-  incSvc()->addListener( this, IncidentType::BeginEvent );
- 
-  return StatusCode::SUCCESS; 
+  // make sure we start from empty BloomFilters
+  m_vtx.m_veloHits.clear(), m_bmg.m_ttHits.clear(), m_amg.m_tHits.clear(),
+    m_vtx.m_vpHits.clear(), m_bmg.m_utHits.clear(), m_amg.m_ftHits.clear(),
+    m_otherHits.clear();
+  m_flags = 0;
+
+  incSvc()->addListener(this, IncidentType::BeginEvent);
+
+  if (msgLevel(MSG::DEBUG)) {
+    // printout to announce size of differnt BloomFilters
+    debug() << "BloomFilters for hits initialised, sizes: [ Velo " <<
+      sizeof(m_vtx.m_veloHits) << " TT " << sizeof(m_bmg.m_ttHits) << " T " <<
+      sizeof(m_amg.m_tHits) << " VP " << sizeof(m_vtx.m_vpHits) << " UT " <<
+      sizeof(m_bmg.m_utHits) << " FT " << sizeof(m_amg.m_ftHits) <<
+      " Other " << sizeof(m_otherHits) << "], class size is " <<
+      sizeof(*this) << endmsg;
+  }
+
+  return StatusCode::SUCCESS;
 }
 
-bool TrackUsedLHCbID::used(const LHCb::LHCbID id) const{
-
-  // get the input - seeds
-  if (!m_configured) initEvent();       
-
-  return std::binary_search(m_usedCont.begin(),m_usedCont.end(),id,lessByID());
-}
-
-void TrackUsedLHCbID::handle ( const Incident& incident )
+bool TrackUsedLHCbID::used(const LHCb::LHCbID id) const
 {
-  if ( IncidentType::BeginEvent == incident.type() ) { m_configured = false; }
+  // get the input - seeds
+  if (UNLIKELY(!(m_flags & Initialized))) initEvent();
+  switch (id.detectorType()) {
+    case LHCb::LHCbID::Velo: return ((~m_flags) & Velo) ? false : m_vtx.m_veloHits.find(id);
+    case LHCb::LHCbID::TT:   return ((~m_flags) & TT  ) ? false : m_bmg.m_ttHits.find(id);
+    case LHCb::LHCbID::IT:   // fallthrough intended
+    case LHCb::LHCbID::OT:   return ((~m_flags) & T   ) ? false : m_amg.m_tHits.find(id);
+    case LHCb::LHCbID::VP:   return ((~m_flags) & VP  ) ? false : m_vtx.m_vpHits.find(id);
+    case LHCb::LHCbID::UT:   return ((~m_flags) & UT  ) ? false : m_bmg.m_utHits.find(id);
+    case LHCb::LHCbID::FT:   return ((~m_flags) & FT  ) ? false : m_amg.m_ftHits.find(id);
+    default: return ((~m_flags) & Other) ? false : m_otherHits.find(id);
+  };
+  return false;
 }
- 
-void TrackUsedLHCbID::initEvent() const{
 
- m_configured = true;
- m_usedCont.clear();
+void TrackUsedLHCbID::handle(const Incident& incident)
+{
+  if (IncidentType::BeginEvent == incident.type()) {
+      // reset Initialized bit to trigger reading new tracks
+      m_flags &= ~Initialized;
+  }
+}
 
- auto iterSelector = m_selectors.begin();
+void TrackUsedLHCbID::initEvent() const
+{
+  // only clear the BloomFilters which actually need clearing
+  if (m_flags & Velo) m_vtx.m_veloHits.clear();
+  if (m_flags & TT) m_bmg.m_ttHits.clear();
+  if (m_flags & T) m_amg.m_tHits.clear();
+  if (m_flags & VP) m_vtx.m_vpHits.clear();
+  if (m_flags & UT) m_bmg.m_utHits.clear();
+  if (m_flags & FT) m_amg.m_ftHits.clear();
+  if (m_flags & Other) m_otherHits.clear();
+  m_flags = 0;
 
- for (auto iterS = m_inputs.begin(); iterS != m_inputs.end(); ++iterS, ++iterSelector){
- 
-   // get selection tool
-   ITrackSelector* tSelector = *iterSelector;
-    
-   // get the containers and extract the ids from the track
-   auto tCont = getIfExists<LHCb::Track::Range>(*iterS);
-   if ( tCont.empty() ){
-     if( msgLevel(MSG::DEBUG) ) debug() << "Track container '" << *iterS << "' does not exist" <<endmsg;
-     continue;
-   }
-   for ( auto iterTrack : tCont){
-     auto ids = iterTrack->lhcbIDs();
-     if ( tSelector && !(tSelector->accept(*iterTrack)) ) continue;
-     m_usedCont.insert(m_usedCont.begin(),ids.begin(),ids.end()); 
-   } //iterTrack
- }  // iterS
- 
- // sort
- std::sort(m_usedCont.begin(),m_usedCont.end(), lessByID());
- 
- // strip out duplicates
- m_usedCont.erase(std::unique(m_usedCont.begin(), m_usedCont.end()),
-                  m_usedCont.end());
- 
+  // loop over tracks locations
+  auto iterSelector = m_selectors.begin();
+  for (auto iterS = m_inputs.begin(); iterS != m_inputs.end();
+      ++iterS, ++iterSelector) {
+    // get selection tool
+    ITrackSelector* tSelector = *iterSelector;
+    // get the containers and extract the ids from the track
+    auto tCont = getIfExists<LHCb::Track::Range>(*iterS);
+    if (tCont.empty()) {
+      if (msgLevel(MSG::DEBUG))
+	debug() << "Track container '" << *iterS << "' does not exist" << endmsg;
+      continue;
+    }
+    for (auto iterTrack: tCont) { // loop over tracks in container
+      if (tSelector && !(tSelector->accept(*iterTrack))) continue;
+      // put hits on track into BloomFilters
+      for (const LHCb::LHCbID id: iterTrack->lhcbIDs()) {
+	switch (id.detectorType()) {
+	  case LHCb::LHCbID::Velo: m_flags |= Velo; m_vtx.m_veloHits.insert(id); break;
+	  case LHCb::LHCbID::TT:   m_flags |= TT  ; m_bmg.m_ttHits.insert(id); break;
+	  case LHCb::LHCbID::IT:   // fallthrough intended
+	  case LHCb::LHCbID::OT:   m_flags |= T   ; m_amg.m_tHits.insert(id); break;
+	  case LHCb::LHCbID::VP:   m_flags |= VP  ; m_vtx.m_vpHits.insert(id); break;
+	  case LHCb::LHCbID::UT:   m_flags |= UT  ; m_bmg.m_utHits.insert(id); break;
+	  case LHCb::LHCbID::FT:   m_flags |= FT  ; m_amg.m_ftHits.insert(id); break;
+	  default: m_flags |= Other; m_otherHits.insert(id); break;
+	};
+      }
+    } //iterTrack
+  }  // iterS
+
+  // make sure nobody mixes current and upgrade detector hits in the same job
+  if ((m_flags & Velo && m_flags & VP) || (m_flags & TT && m_flags & UT) ||
+      (m_flags & T && m_flags & FT)) {
+    throw std::logic_error("Cannot deal with current and upgrade detector hits in the same event.");
+  }
+
+  // tracks all read, set Initialized bit
+  m_flags |= Initialized;
 }
--- Rec/Tr/TrackTools/src/TrackUsedLHCbID.h
+++ Rec/Tr/TrackTools/src/TrackUsedLHCbID.h
@@ -7,8 +7,12 @@
  * Implementation of TrackUsedLHCbID
  * check if an LHCbID is used
  *
- *  @author M.Needham
- *  @date   2/08/2006
+ * @author M.Needham
+ * @date   2/08/2006
+ *
+ * @author M. Schiller
+ * @date 2015-02-21
+ *  - use BloomFilters to achieve O(1) lookup instead of O(log(nHits))
  */
 
 #include "GaudiAlg/GaudiTool.h"
@@ -17,65 +21,103 @@
 
 #include <string>
 #include <vector>
+#include <cstdint>
 
 #include "Kernel/LHCbID.h"
+#include "LHCbMath/BloomFilter.h"
 
 class ITrackSelector;
 
+namespace TrackUsedLHCbIDImplDetail {
+    /// calculate number of occupied channels with safety factor
+    constexpr uint64_t occCh(uint64_t nch, uint64_t occnumer,
+	uint64_t occdenom, uint64_t safety = 4)
+    { return (safety * nch * occnumer) / occdenom; }
+    /// calculate BloomFilter capacity for detector with given number of channels and occupancy
+    constexpr uint64_t cap(uint64_t nch, uint64_t occnumer, uint64_t occdenom)
+    { return nch < occCh(nch, occnumer, occdenom) ? nch :
+      occCh(nch, occnumer, occdenom); }
+}
+
 class TrackUsedLHCbID: public GaudiTool,
 	                virtual IUsedLHCbID,
-                        virtual public IIncidentListener  {
+                        virtual public IIncidentListener
+{
+  public:
+    /** constructor */
+    TrackUsedLHCbID(const std::string& type,
+	const std::string& name,
+	const IInterface* parent);
 
-public:
+    /** destructor */
+    virtual ~TrackUsedLHCbID();
 
-  /** constructer */
-  TrackUsedLHCbID(const std::string& type,
-                            const std::string& name,
-                            const IInterface* parent);  
+    /** intialize */
+    StatusCode initialize();
 
-  /** destructer */
-  virtual ~TrackUsedLHCbID();
+    /** Test if the LHCbID is used
+     * @param id to be test tested
+     *  @return true if used
+     */
+    bool used(const LHCb::LHCbID id) const;
 
-  /** intialize */
-  StatusCode initialize();
+    /** Implement the handle method for the Incident service.
+     *  This is used to nform the tool of software incidents.
+     *
+     *  @param incident The incident identifier
+     */
+    void handle( const Incident& incident );
 
-  /** Test if the LHCbID is used 
-  * @param id to be test tested
-  *  @return true if used
-  */  
-  bool used (const LHCb::LHCbID id) const;
+  private:
 
-  /** Implement the handle method for the Incident service.
-  *  This is used to nform the tool of software incidents.
-  *
-  *  @param incident The incident identifier
-  */
-  void handle( const Incident& incident );
+    void initEvent() const;
 
-private:
+    typedef std::vector<std::string> TrackContainers;
+    typedef std::vector<ITrackSelector*> Selectors;
+    TrackContainers m_inputs;
+    // for track selection
+    std::vector<std::string> m_names;
+    Selectors m_selectors;
 
-  void initEvent() const;
-  mutable bool m_configured;
+    // set maximum number of hits expected in the BloomFilter:
+    // if there's more hits in the detector than that, the number of collisions
+    // (one hit mistaken for another) will rise above the threshold (1e-4)
+    //
+    // strategy: max(safety * maxocc * nChannels, nChannels) with safety = 4
+    static constexpr uint64_t s_MaxVeloHits = TrackUsedLHCbIDImplDetail::cap( 180000u,   1u,    100u);
+    static constexpr uint64_t s_MaxTTHits =   TrackUsedLHCbIDImplDetail::cap( 150000u,   2u,    100u);
+    static constexpr uint64_t s_MaxITHits =   TrackUsedLHCbIDImplDetail::cap( 130000u,   2u,    100u);
+    static constexpr uint64_t s_MaxOTHits =   TrackUsedLHCbIDImplDetail::cap(  54000u,  15u,    100u);
+    static constexpr uint64_t s_MaxVPHits =   TrackUsedLHCbIDImplDetail::cap(4100000u, 125u, 100000u);
+    static constexpr uint64_t s_MaxUTHits =   TrackUsedLHCbIDImplDetail::cap( 540000u,  18u,   1000u);
+    static constexpr uint64_t s_MaxFTHits =   TrackUsedLHCbIDImplDetail::cap( 300000u,   2u,    100u);
+    static constexpr uint64_t s_MaxOtherHits = 1024u; // up to 1024 which don't fit above
+    static constexpr uint64_t s_denom = 1u << 20u;
+    static constexpr uint64_t s_numer = 1u * s_denom / 10000u;
 
-  typedef std::vector<std::string> TrackContainers;
-  mutable TrackContainers m_inputs;
-
-  typedef std::vector<LHCb::LHCbID> LHCbIDs;
-  mutable LHCbIDs m_usedCont;
- 
-  // for track selection 
-  std::vector<std::string> m_names;
-  typedef std::vector<ITrackSelector*> Selectors;
-  Selectors m_selectors;
-
-  // for comparing LHCbIDs
-  class lessByID{
-   public:
-     inline bool operator() ( const LHCb::LHCbID& obj1 , const LHCb::LHCbID& obj2 ) const { 
-        return obj1.lhcbID() < obj2.lhcbID() ; 
-     }
-  };
-
+    /// flag bits
+    enum { Initialized = 1, Velo = 2, TT = 4, T = 8, VP = 16, UT = 32, FT = 64, Other = 128 };
+    mutable unsigned m_flags; ///< flags
+    // since current and Upgrade detectors are never used in the same job, we
+    // can eke out some memory by putting corresponding detectors in a union
+    // each, so they "share" the memory by overlapping in the same physical
+    // memory location
+    mutable union VtxHits {
+      BloomFilter<LHCb::LHCbID, s_MaxVeloHits, s_numer, s_denom> m_veloHits;
+      BloomFilter<LHCb::LHCbID, s_MaxVPHits, s_numer, s_denom> m_vpHits;
+      VtxHits() : m_veloHits() { }
+    } m_vtx; ///< vertex detector hits
+    mutable union BeforeMagnetHits {
+      mutable BloomFilter<LHCb::LHCbID, s_MaxTTHits, s_numer, s_denom> m_ttHits;
+      mutable BloomFilter<LHCb::LHCbID, s_MaxUTHits, s_numer, s_denom> m_utHits;
+      BeforeMagnetHits() : m_ttHits() { }
+    } m_bmg; ///< hits in tracking detectors before the magnet
+    mutable union AfterMagnetHits {
+      mutable BloomFilter<LHCb::LHCbID, s_MaxITHits + s_MaxOTHits, s_numer, s_denom> m_tHits;
+      mutable BloomFilter<LHCb::LHCbID, s_MaxFTHits, s_numer, s_denom> m_ftHits;
+      AfterMagnetHits() : m_tHits() { }
+    } m_amg; ///< hits in tracking detectors after the magnet
+    mutable BloomFilter<LHCb::LHCbID, s_MaxOtherHits, s_numer, s_denom> m_otherHits;
 };
 
 #endif
