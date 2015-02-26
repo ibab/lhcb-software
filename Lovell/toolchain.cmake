296db93b6b833ee4de7a6cfb22a0bdaebaca9b97
Jon Harrison <jonathan.harrison@manchester.ac.uk>
2015-02-26 11:35:42 +0000

    Updated Gaudi dependence to v26r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@184565 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1c31da5f497b51397fde9c9fbf3aa30ddd0fbcf7

--- Lovell/toolchain.cmake
+++ Lovell/toolchain.cmake
@@ -1,6 +1,5 @@
 # Special wrapper to load the declared version of the heptools toolchain.
-#set(heptools_version 71root6)
-set(heptools_version 72root6)
+#set(heptools_version 72root6)
 
 if(DEFINED ENV{HEPTOOLS_VERSION})
   set(heptools_version $ENV{HEPTOOLS_VERSION})
