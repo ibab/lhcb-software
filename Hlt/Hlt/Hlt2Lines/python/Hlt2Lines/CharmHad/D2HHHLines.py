234b63d761286479a7351af946d81979c4d88de5
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2015-02-28 03:27:17 +0000

    slightly less dumb charmhad
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@184635 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 099cb5f156d361864e4686d2aacec933cc7f2c91

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/D2HHHLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/D2HHHLines.py
@@ -13,37 +13,37 @@ class CharmHadD2HHHLines(Hlt2LinesConfigurableUser):
                              'Trk_1OF3_MIPCHI2DV_MIN'   :  4.0,
                              'Kaon_PIDK_MIN'            :  0,
                              'Pion_PIDK_MAX'            :  10,
+                             'Proton_PIDp_MIN'          :  0,
                              'AM_MIN'                   :  1779 * MeV,
-                             'AM_MAX'                   :  2059 * MeV,
+                             'AM_MAX'                   :  2436 * MeV,
                              'D_ASUMPT_MIN'             :  0 * MeV,
                              'D_BPVDIRA_MIN'            :  0.9994,
                              'D_BPVVDCHI2_MIN'          :  20.0,
                              'D_VCHI2PDOF_MAX'          :  20.0,
                              'D_BPVLTIME_MIN'           :  0.2 * picosecond,
-                             'Dp_Mass_M_MIN'            :  1789.0 * MeV,
-                             'Dp_Mass_M_MAX'            :  1949.0 * MeV,
-                             'Ds_Mass_M_MIN'            :  1889.0 * MeV,
-                             'Ds_Mass_M_MAX'            :  2049.0 * MeV,
+                             'D_Mass_M_MIN'             :  1789.0 * MeV,
+                             'D_Mass_M_MAX'             :  2049.0 * MeV,
+                             'Lc_Mass_M_MIN'            :  2211.0 * MeV,
+                             'Lc_Mass_M_MAX'            :  2361.0 * MeV,
                             }
 
                 }
     
     def __apply_configuration__(self):
-        from Stages import MassFilterDp,MassFilterDs
+        from Stages import MassFilterD,MassFilterLc
         from Stages import D2KPiPi_SS,D2KPiPi_OS,D2KKPi_SS,D2KKPi_OS,D2PiPiPi,D2KKK
-        stages = {'Dp2KPiPi_SS'   : [MassFilterDp('Dp2KPiPi_SS',inputs=[D2KPiPi_SS])],
-                  'Dp2KPiPi_OS'   : [MassFilterDp('Dp2KPiPi_OS',inputs=[D2KPiPi_OS])],
-                  'Dp2KKPi_SS'    : [MassFilterDp('Dp2KKPi_SS',inputs=[D2KKPi_SS])],
-                  'Dp2KKPi_OS'    : [MassFilterDp('Dp2KKPi_OS',inputs=[D2KKPi_OS])],
-                  'Dp2PiPiPi'     : [MassFilterDp('Dp2PiPiPi',inputs=[D2PiPiPi])],
-                  'Dp2KKK'        : [MassFilterDp('Dp2KKK',inputs=[D2KKK])],
+        from Stages import Lc2KPPi, Lc2KPK, Lc2PiPPi, Lc2PiPK
+        stages = {'D2KPiPi_SS'   : [MassFilterD('D2KPiPi_SS',inputs=[D2KPiPi_SS])],
+                  'D2KPiPi_OS'   : [MassFilterD('D2KPiPi_OS',inputs=[D2KPiPi_OS])],
+                  'D2KKPi_SS'    : [MassFilterD('D2KKPi_SS',inputs=[D2KKPi_SS])],
+                  'D2KKPi_OS'    : [MassFilterD('D2KKPi_OS',inputs=[D2KKPi_OS])],
+                  'D2PiPiPi'     : [MassFilterD('D2PiPiPi',inputs=[D2PiPiPi])],
+                  'D2KKK'        : [MassFilterD('D2KKK',inputs=[D2KKK])],
                   #
-                  'Ds2KPiPi_SS'   : [MassFilterDs('Ds2KPiPi_SS',inputs=[D2KPiPi_SS])],
-                  'Ds2KPiPi_OS'   : [MassFilterDs('Ds2KPiPi_OS',inputs=[D2KPiPi_OS])],
-                  'Ds2KKPi_SS'    : [MassFilterDs('Ds2KKPi_SS',inputs=[D2KKPi_SS])],
-                  'Ds2KKPi_OS'    : [MassFilterDs('Ds2KKPi_OS',inputs=[D2KKPi_OS])],
-                  'Ds2PiPiPi'     : [MassFilterDs('Ds2PiPiPi',inputs=[D2PiPiPi])],
-                  'Ds2KKK'        : [MassFilterDs('Ds2KKK',inputs=[D2KKK])]  }
+                  'Lc2KPPi'      : [MassFilterLc('Lc2KPPi',inputs=[Lc2KPPi])],
+                  'Lc2KPK'       : [MassFilterLc('Lc2KPK',inputs=[Lc2KPK])],
+                  'Lc2PiPPi'     : [MassFilterLc('Lc2PiPPi',inputs=[Lc2PiPPi])],
+                  'Lc2PiPK'      : [MassFilterLc('Lc2PiPK',inputs=[Lc2PiPK])] }
                 
         from HltLine.HltLine import Hlt2Line
         for nickname, algos in self.algorithms(stages).iteritems():
--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/Stages.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/Stages.py
@@ -18,19 +18,19 @@ class TrackGEC(Hlt2VoidFilter):
 
 # Mass filter
 from Hlt2Lines.Utilities.Hlt2Filter import Hlt2ParticleFilter
-class MassFilterDs(Hlt2ParticleFilter):
+class MassFilterD(Hlt2ParticleFilter):
     def __init__(self, name, inputs):
-        cut = "in_range( %(Ds_Mass_M_MIN)s , M , %(Ds_Mass_M_MAX)s )"
+        cut = "in_range( %(D_Mass_M_MIN)s , M , %(D_Mass_M_MAX)s )"
         Hlt2ParticleFilter.__init__(self, linePrefix, name, cut, inputs)
 
-class MassFilterDp(Hlt2ParticleFilter):
+class MassFilterLc(Hlt2ParticleFilter):
     def __init__(self, name, inputs):
-        cut = "in_range( %(Dp_Mass_M_MIN)s , M , %(Dp_Mass_M_MAX)s )"
+        cut = "in_range( %(Lc_Mass_M_MIN)s , M , %(Lc_Mass_M_MAX)s )"
         Hlt2ParticleFilter.__init__(self, linePrefix, name, cut, inputs)
 
 # The class that creates the Hlt2Combiner
 from Hlt2Lines.Utilities.Hlt2Combiner import Hlt2Combiner
-class D2HHHCombiner(Hlt2Combiner):
+class HHHCombiner(Hlt2Combiner):
     def __init__(self, name):
         piK_cut =  ("(TRCHI2DOF < %(Trk_TRCHI2DOF_MAX)s )" +
                    "& (PT > %(Trk_PT_MIN)s)" +
@@ -38,8 +38,10 @@ class D2HHHCombiner(Hlt2Combiner):
                    "& (MIPCHI2DV(PRIMARY) > %(Trk_ALL_MIPCHI2DV_MIN)s)")
         pi_cut = ("(PIDK < %(Pion_PIDK_MAX)s) &"+piK_cut)
         K_cut  = ("(PIDK > %(Kaon_PIDK_MIN)s) &"+piK_cut)
+        p_cut  = ("(PIDp > %(Proton_PIDp_MIN)s) &"+piK_cut)
         dc =    {'pi+' : pi_cut,
-                 'K+' : K_cut}
+                 'K+'  : K_cut,
+                 'p+'  : p_cut}
         cc =    ("(in_range( %(AM_MIN)s, AM, %(AM_MAX)s ))" +
                  " & ((APT1+APT2+APT3) > %(D_ASUMPT_MIN)s )" +
                  " & (AHASCHILD((MIPCHI2DV(PRIMARY)) > %(Trk_1OF3_MIPCHI2DV_MIN)s))"+
@@ -49,7 +51,7 @@ class D2HHHCombiner(Hlt2Combiner):
                  " & (BPVVDCHI2 > %(D_BPVVDCHI2_MIN)s )" +
                  " & (BPVLTIME() > %(D_BPVLTIME_MIN)s )")
         from HltTracking.HltPVs import PV3D
-        from Inputs import Hlt2LoosePions, Hlt2LooseKaons
+        from Inputs import Hlt2LoosePions, Hlt2LooseKaons,Hlt2LooseProtons
         inputs = [Hlt2LooseKaons, Hlt2LoosePions]
         Hlt2Combiner.__init__(self, linePrefix, 'KPiPi_SS', "[D+ -> K- pi+ pi+]cc", inputs,
                               dependencies = [TrackGEC(linePrefix + 'TrackGEC'), PV3D('Hlt2')],
@@ -57,8 +59,8 @@ class D2HHHCombiner(Hlt2Combiner):
                               MotherCut = mc, Preambulo = [])
 
 # Combiners
-from Inputs import Hlt2LoosePions, Hlt2LooseKaons
-D2KPiPi_SS  = D2HHHCombiner('KPiPi_SS')
+from Inputs import Hlt2LoosePions, Hlt2LooseKaons,Hlt2LooseProtons
+D2KPiPi_SS  = HHHCombiner('KPiPi_SS')
 D2KPiPi_OS  = D2KPiPi_SS.clone('KPiPi_OS', decay = "[D+ -> K+ pi+ pi-]cc",
                                            inputs = [Hlt2LooseKaons, Hlt2LoosePions])
 D2KKPi_SS   = D2KPiPi_SS.clone('KKPi_SS',  decay = "[D+ -> K+ K+ pi-]cc",
@@ -67,6 +69,16 @@ D2KKPi_OS   = D2KPiPi_SS.clone('KKPi_OS',  decay = "[D+ -> K+ K- pi+]cc",
                                            inputs = [Hlt2LooseKaons, Hlt2LoosePions])
 D2PiPiPi    = D2KPiPi_SS.clone('PiPiPi',   decay = "[D+ -> pi+ pi+ pi-]cc",
                                                 inputs = [Hlt2LoosePions])
-D2KKK       = D2KPiPi_SS.clone('KKK',     decay = "[D+ -> K+ K+ K-]cc",
+D2KKK       = D2KPiPi_SS.clone('KKK',      decay = "[D+ -> K+ K+ K-]cc",
                                                 inputs = [Hlt2LooseKaons])
-        
+#
+Lc2KPPi     = D2KPiPi_SS.clone('KPPi',     decay = "[Lambda_c+ -> K- p+ pi+]cc",
+                                           inputs = [Hlt2LooseKaons, Hlt2LoosePions, Hlt2LooseProtons])     
+Lc2KPK      = D2KPiPi_SS.clone('KPK',      decay = "[Lambda_c+ -> K- p+ K+]cc",
+                                           inputs = [Hlt2LooseKaons, Hlt2LooseProtons])   
+Lc2PiPPi    = D2KPiPi_SS.clone('PiPPi',    decay = "[Lambda_c+ -> pi- p+ pi+]cc",
+                                           inputs = [Hlt2LoosePions, Hlt2LooseProtons])   
+Lc2PiPK     = D2KPiPi_SS.clone('PiPK',     decay = "[Lambda_c+ -> pi- p+ K+]cc",
+                                           inputs = [Hlt2LooseKaons, Hlt2LoosePions, Hlt2LooseProtons])   
+
+
