10fcaa54f1534fd0e42f6c8ffff706a6316c38bd
Beat Jost <beat.jost@cern.ch>
2015-03-02 12:34:33 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@184650 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 86852dffec08e1ccc7ff0e64c2678df101aaa724

--- Online/Online/FarmConfig/job/Hlt2Reader.sh
+++ Online/Online/FarmConfig/job/Hlt2Reader.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #
+NODE=`hostname -s | tr a-z A-Z`;
+export NODE_OPTIONS=${DYNAMIC_OPTS}/Nodes/${NODE}_TriggerInfo.opts;
 exec -a ${UTGID} ${Class2_task} -opts=../options/${TASK_TYPE}.opts
