8269b9fcd3940080b2a63ab33d866e11630b3fa1
Roel Aaij <roel.aaij@cern.ch>
2015-03-04 18:41:00 +0000

    Add scenarios for TED Velo and tracker only (no muon) alignment.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@184824 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: abf76c098a3f9bf85b43c133175ed26fa836c0cb

--- Alignment/Alignment/TAlignment/python/TAlignment/AlignmentScenarios.py
+++ Alignment/Alignment/TAlignment/python/TAlignment/AlignmentScenarios.py
@@ -118,6 +118,58 @@ def configureVeloSensorAlignment():
     #surveyconstraints.XmlUncertainties += [ 
     #    'Velo/VeloRight/Module(01|41) : 0.02 0.02 0.0001 0.000001 0.000001 0.0001' ]
 
+
+def configureTEDVeloSensorAlignment():
+    TAlignment().WriteCondSubDetList  += ['Velo']
+
+    # To align at the sensor level, we align 'modules' and
+    #'phi-sensors'. The R-sensor is always equal to the modules. (We
+    #keep its 'deltas' equal to zero.) However, because of a feature
+    #in the way we keep track of mother-daughter relations, it is
+    #important to create alignables for the R-sensors as well: use
+    #"None" to specify an alignable without active degrees of
+    #freedom.
+
+    # Note also that it doesn't make sense to align R modules for
+    # rotations around the z-axis. Rather than aligning the phi sensor
+    # for this rotation, we put the rotation at the module level.
+
+    elements = Alignables()
+    elements.Velo("None")
+    elements.VeloRight("None")
+    elements.VeloLeft("None")
+    elements.VeloModules("None")
+    elements.VeloPhiSensors("None")
+    elements.VeloRSensors("None")
+    # TAlignment().ElementsToAlign += list(elements)
+    myDOFs = '/dd/Structure/LHCb/BeforeMagnetRegion/Velo/VeloLeft/Module14:TxTy'
+    TAlignment().ElementsToAlign = [ myDOFs ]
+    
+
+    # make sure that the velo stays where it was. Important note: the
+    # dofs here must match the ones that we actually align for. If you
+    # specify too many, things will go rather wrong.
+    TAlignment().Constraints.append("VeloHalfAverage : Velo/Velo(Left|Right) : Tx Ty Tz Rx Ry Rz")
+    
+    # now constrain the total shearing and z-scale in each Velo half
+    # to get the z-axis nominal. we'll do this by tightening the
+    # survey errors.
+
+    # we need to align the relative twist of the two halves. the
+    # C-side (minus X, Right) is known to have a corkscrew ('twist')
+    # in the base plate. so, for the right half we don't fix the Rz rotation:
+    # surveyconstraints = SurveyConstraints()
+    # surveyconstraints.All()
+    #surveyconstraints.XmlUncertainties += [ 
+    #    'Velo/VeloLeft/Module(00|40) : 0.0001 0.0001 0.0001 0.000001 0.000001 0.000001' ]
+    ## for the right half, we fix the shearing and z-scale, but not the Rz rotation 
+    #surveyconstraints.XmlUncertainties += [ 
+    #    'Velo/VeloRight/Module(01|41) : 0.0001 0.0001 0.0001 0.000001 0.000001 0.0001' ]
+
+    # bad idea: release the shearing:
+    #surveyconstraints.XmlUncertainties += [ 
+    #    'Velo/VeloRight/Module(01|41) : 0.02 0.02 0.0001 0.000001 0.000001 0.0001' ]
+
 # Early 2012 data alignment
 def configureEarlyDataAlignment( fixQOverPBias = True ) :
     TAlignment().WriteCondSubDetList += ['Velo','TT','IT','OT','MUON']
@@ -153,6 +205,44 @@ def configureEarlyDataAlignment( fixQOverPBias = True ) :
     # make sure we fix the z-scale
     surveyconstraints.XmlUncertainties += ["OT/T3X1U : 0.5 0.5 0.00001 0.0001 0.0001 0.0001" ]
 
+# Alignment scenario used for 2012 data
+def configureTrackerAlignment( fixQOverPBias = False, surveyVer = 'latest' ) :
+    TAlignment().WriteCondSubDetList += ['TT','IT','OT']
+  
+    # define the alignment elements
+    elements = Alignables()
+    elements.Velo("None")
+    elements.VeloRight("None")
+    elements.VeloLeft("None")
+    elements.IT("None")
+    elements.ITBoxes("TxTzRz")
+    elements.ITLayers("None")
+    elements.OT("None")
+    elements.OTCFrames("TxRz")
+    elements.OTCFrameLayers("Tz")
+    elements.OTModules("TxRz")
+    elements.TT("None")
+    elements.TTLayers("Tz")
+    elements.TTModules("TxRz")
+    elements.Tracker("None")
+    TAlignment().ElementsToAlign = list(elements)
+
+    # make sure that the velo stays where it was
+    constraints = []
+    # fix the q/p scale by not moving T in X. note that you do not
+    # want to do this if you use D0 in the alignment
+    if fixQOverPBias:
+        constraints.append("OT3X : OT/T3X1U.Side : Tx")
+    TAlignment().Constraints = constraints
+    
+    if surveyVer == None: return
+    # tweak the survey a little bit to fix the z-scale to survey
+    surveyconstraints = SurveyConstraints()
+    surveyconstraints.All(surveyVer)
+    # make sure we fix the z-scale
+    surveyconstraints.XmlUncertainties += ["OT/T3X1U : 0.5 0.5 0.00001 0.0001 0.0001 0.0001" ]
+
+    
 # Alignment scenario used for 2012 data
 def configure2012DataAlignment( fixQOverPBias = False, surveyVer = 'latest' ) :
     TAlignment().WriteCondSubDetList += ['Velo','TT','IT','OT','MUON']
