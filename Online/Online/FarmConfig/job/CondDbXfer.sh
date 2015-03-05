daf5160d4575a9cf4faf1508a4df5dd033dd86fe
Markus Frank <frankb@cern.ch>
2015-03-05 10:46:45 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@184845 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 8d6664c49e65fbbdb8b06ea183b2a8073af005f1

--- Online/Online/FarmConfig/job/CondDbXfer.sh
+++ Online/Online/FarmConfig/job/CondDbXfer.sh
@@ -2,9 +2,14 @@
 echo $1
 echo $2
 echo $3
-#source /group/online/dataflow/cmtuser/ALIGNMENTONLINE/ALIGNMENTONLINE_HEAD/AlignmentOnline/AlignOnline/cmt/setup.x86_64-slc6-gcc48-dbg.vars
+export HOME=/home/online
+export USER=online # these dumb offline scripts....
+source /cvmfs/lhcb.cern.ch/lib/LbLogin.sh
 source /group/online/dataflow/cmtuser/AlignmentOnlineDev_v10r2/setup.x86_64-slc6-gcc48-dbg.vars
 export PYTHONPATH=$1:/group/online/hlt/conditions/RunChangeHandler:$PYTHONPATH
 unset GAUDIAPPNAME
+source /sw/oracle/set_oraenv.sh
+export CORAL_DBLOOKUP_PATH=/group/online/conddbserver/
+export CORAL_AUTH_PATH=/group/online/conddbserver
 #python $ALIGNONLINEROOT/python/DBXfer.py
 #exit 0
