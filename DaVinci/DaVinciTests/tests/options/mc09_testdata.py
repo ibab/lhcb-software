1e5ebbe5f0479f83b20f5560bdeda5a085691f4f
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2015-03-11 16:12:18 +0000

    Removed obsolete/redundant file tests/options/mc09_testdata.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@185258 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df2af88d2ec6d382de816bbdf87e789a5b46cc40

--- DaVinci/DaVinciTests/tests/options/mc09_testdata.py
+++
@@ -1,5 +0,0 @@
-from Gaudi.Configuration import EventSelector
-#event-type 24142000 
-EventSelector().Input   = [
-"   DATAFILE='root://castorlhcb.cern.ch//castor.cern.ch/user/r/rlambert/testfiles/110012001_MC09_Data_MC09.SeqB0q2DplusMuX.dst' TYP='POOL_ROOTTREE' OPT='READ'"
-]
