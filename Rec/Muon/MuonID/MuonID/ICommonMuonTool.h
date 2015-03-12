a233f9766bdf7543ad22adf05ff77ed6244f3d38
Kevin Dungs <kevin.dungs@cern.ch>
2015-03-12 20:53:28 +0000

    Forgot to add two files because SVN is totally non-intuitive. Please switch to Git.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@185306 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 301e8b14b0e7414792c40f19b4958aba0ea2e92d

---
+++ Rec/Muon/MuonID/MuonID/ICommonMuonTool.h
@@ -0,0 +1,36 @@
+#ifndef ICOMMONMUONTOOL_H_
+#define ICOMMONMUONTOOL_H_
+
+#include "GaudiKernel/IAlgTool.h"
+#include "MuonID/CommonMuonHit.h"
+
+static const InterfaceID IID_ICommonMuonTool("ICommonMuonTool", 1, 0);
+
+class ICommonMuonTool : virtual public IAlgTool {
+ public:
+  static const InterfaceID& interfaceID() { return IID_ICommonMuonTool; }
+  static const unsigned nStations = 5, nRegions = 4;
+
+  using MuonTrackExtrapolation =
+      std::array<std::pair<double, double>, nStations>;
+  using MuonTrackOccupancies = std::array<unsigned, nStations>;
+
+  virtual auto extrapolateTrack(const LHCb::Track&) const
+      -> MuonTrackExtrapolation = 0;
+  virtual auto preSelection(const LHCb::Track&,
+                            const MuonTrackExtrapolation&) const noexcept
+      -> bool = 0;
+  virtual auto hitsAndOccupancies(const LHCb::Track&,
+                                  const MuonTrackExtrapolation&) const
+      -> std::tuple<CommonConstMuonHits, MuonTrackOccupancies> = 0;
+  virtual auto extractCrossed(const CommonConstMuonHits&) const noexcept
+      -> std::tuple<CommonConstMuonHits, MuonTrackOccupancies> = 0;
+  virtual auto isMuon(const MuonTrackOccupancies&, double) const noexcept
+      -> bool = 0;
+  virtual auto isMuonLoose(const MuonTrackOccupancies&, double) const noexcept
+      -> bool = 0;
+  virtual auto foi(int, int, double) const noexcept
+      -> std::pair<double, double> = 0;
+};
+
+#endif  // ICOMMONMUONTOOL_H_
---
+++ Rec/Muon/MuonID/src/component/CommonMuonTool.h
@@ -0,0 +1,64 @@
+#ifndef COMMONMUONTOOL_H_
+#define COMMONMUONTOOL_H_
+
+#include <array>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "Event/MuonPID.h"
+#include "Event/Track.h"
+#include "GaudiAlg/GaudiTool.h"
+#include "MuonDet/DeMuonDetector.h"
+
+#include "MuonID/CommonMuonHitManager.h"
+#include "MuonID/ICommonMuonTool.h"
+
+/** @class CommonMuonTool CommonMuonTool.h
+ * A tool that provides functionality for all steps in muon identification that
+ *are the same in HLT and offline.
+ *
+ * It is designed to have no state associated with the event. The only members
+ *are related to conditions (such as detector information or FoI).
+ *
+ * @author Kevin Dungs
+ * @date 2015-01-06
+ */
+class CommonMuonTool final : public extends1<GaudiTool, ICommonMuonTool> {
+ public:
+  CommonMuonTool(const std::string& type, const std::string& name,
+                 const IInterface* parent);
+  virtual ~CommonMuonTool() override = default;
+
+  virtual auto initialize() -> StatusCode override;
+  virtual auto extrapolateTrack(const LHCb::Track&) const
+      -> MuonTrackExtrapolation override;
+  virtual auto preSelection(const LHCb::Track&,
+                            const MuonTrackExtrapolation&) const noexcept
+      -> bool override;
+  virtual auto hitsAndOccupancies(const LHCb::Track&,
+                                  const MuonTrackExtrapolation&) const
+      -> std::tuple<CommonConstMuonHits, MuonTrackOccupancies> override;
+  virtual auto extractCrossed(const CommonConstMuonHits&) const noexcept
+      -> std::tuple<CommonConstMuonHits, MuonTrackOccupancies> override;
+  virtual auto isMuon(const MuonTrackOccupancies&, double) const noexcept
+      -> bool override;
+  virtual auto isMuonLoose(const MuonTrackOccupancies&, double) const noexcept
+      -> bool override;
+  virtual auto foi(int, int, double) const noexcept
+      -> std::pair<double, double> override;
+ private:
+  // Helpers
+  auto i_foi(int, int, double) const noexcept -> std::pair<double, double>;
+  // Members
+  CommonMuonHitManager* m_hitManager;  // non-owning
+  DeMuonDetector* m_det;               // non-owning
+  double m_foiFactor, m_preSelMomentum;
+  std::array<double, nStations> m_stationZ;
+  std::array<std::pair<double, double>, nStations * nRegions> m_padSize;
+  std::array<std::pair<double, double>, nStations> m_regionInner, m_regionOuter;
+  std::array<std::vector<double>, 3> m_foiParamX, m_foiParamY;
+  std::vector<double> m_momentumCuts;
+};
+
+#endif  // COMMONMUONTOOL_H_
