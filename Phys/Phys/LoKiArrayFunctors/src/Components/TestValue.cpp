deb4a7ede93261fccee6fcf86b84b742d1d431b4
Vanya Belyaev <ivan.belyaev@cern.ch>
2015-03-12 12:24:52 +0000

    add SpecialValue'', useful for testing
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@185283 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e7e4257961bf13abd380084a891714bdc0139e27

---
+++ Phys/Phys/LoKiArrayFunctors/src/Components/TestValue.cpp
@@ -0,0 +1,66 @@
+// $Id:$ 
+// ============================================================================
+// Include files 
+// ============================================================================
+// GaudiKernel
+// ============================================================================
+#include "GaudiKernel/ToolFactory.h"
+// ============================================================================
+// GaudiAlg
+// ============================================================================
+#include "GaudiAlg/GaudiTool.h"
+// ============================================================================
+#include "Kernel/IParticleValue.h"            // Interface
+// ============================================================================
+namespace LoKi
+{
+  // ==========================================================================
+  /** @class SpecialValue 
+   *  the simplest possible implementation of IPArticleValue integrface 
+   *  @see IParticlValue 
+   *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
+   *  @date 2015-03-12
+   */
+  class SpecialValue: public extends1<GaudiTool, IParticleValue>
+  {
+    // ========================================================================
+    /// friend class factory for instantiation
+    friend class ToolFactory<LoKi::SpecialValue>;
+    // ========================================================================
+  protected:
+    // ========================================================================
+    /// Standard constructor
+    SpecialValue( const std::string& type,
+                  const std::string& name,
+                  const IInterface* parent)
+      : base_class ( type , name , parent )
+      , m_value    ( 42 )
+    {
+      declareProperty ( "Value" , m_value , "Value to be used" );
+    }
+    virtual ~SpecialValue() {}; ///< Destructor
+    // ========================================================================
+  private:
+    // ========================================================================
+    SpecialValue() ; 
+    SpecialValue           ( const SpecialValue& ) ;
+    SpecialValue& operator=( const SpecialValue& ) ;
+    // ========================================================================    
+  public:  // IParticleValue
+    // ========================================================================    
+    virtual double operator()( const LHCb::Particle* /* p */ ) const 
+    { return m_value ; }
+  // ========================================================================    
+  private:
+    // ========================================================================    
+    /// the value 
+    double m_value ; // the value    
+    // ========================================================================
+  };
+  // ==========================================================================
+}
+// ============================================================================
+DECLARE_NAMESPACE_TOOL_FACTORY(LoKi,SpecialValue) 
+// ============================================================================
+// The END 
+// ============================================================================
