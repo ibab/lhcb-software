53093181bc1800eafdeb968da7a99eb7249284d2
Jibo He <jibo.he@cern.ch>
2015-03-13 13:32:20 +0000

    Changed shared Phi to use Hlt2Member, to make things more clear
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@185379 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 697439e0fc9990f08841e2b440eb6519acb7854b

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Phi.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Phi.py
@@ -8,17 +8,17 @@
 ##
 from Gaudi.Configuration import *
 from Configurables import CombineParticles
-from HltLine.HltLine import bindMembers
+from HltLine.HltLine import bindMembers, Hlt2Member
 from Hlt2SharedParticles.TrackFittedBasicParticles import  BiKalmanFittedRichKaons
 __all__ = ( 'Phi2KK', 'UnbiasedPhi2KK' )
 
-Hlt2SharedPhi2KK = CombineParticles("Hlt2SharedPhi2KK")
-Hlt2SharedPhi2KK.Inputs = [ BiKalmanFittedRichKaons.outputSelection() ]
-Hlt2SharedPhi2KK.Output = 'Hlt2/Hlt2SharedPhi2KK/Particles'
-Hlt2SharedPhi2KK.DecayDescriptor = "phi(1020) -> K+ K-" 
-Hlt2SharedPhi2KK.DaughtersCuts = { "K+" : "ALL"} 
-Hlt2SharedPhi2KK.CombinationCut = "(ADAMASS('phi(1020)')<50*MeV)"
-Hlt2SharedPhi2KK.MotherCut = "(VFASPF(VCHI2PDOF)<25)"
+Hlt2SharedPhi2KK = Hlt2Member( CombineParticles
+                               , 'Phi2KK'
+                               , Inputs = [ BiKalmanFittedRichKaons ]
+                               , DecayDescriptor = "phi(1020) -> K+ K-"
+                               , DaughtersCuts = { "K+" : "ALL"}
+                               , CombinationCut = "(ADAMASS('phi(1020)')<50*MeV)"
+                               , MotherCut = "(VFASPF(VCHI2PDOF)<25)" )
 
 Phi2KK = bindMembers( 'Shared', [ BiKalmanFittedRichKaons, Hlt2SharedPhi2KK ] )
 
@@ -30,12 +30,13 @@ Phi2KK = bindMembers( 'Shared', [ BiKalmanFittedRichKaons, Hlt2SharedPhi2KK ] )
 #  @date 2008-10-09
 #
 ##
-Hlt2SharedUnbiasedPhi2KK = CombineParticles("Hlt2SharedUnbiasedPhi2KK")
-Hlt2SharedUnbiasedPhi2KK.Inputs = [ BiKalmanFittedRichKaons.outputSelection() ]
-Hlt2SharedUnbiasedPhi2KK.Output = 'Hlt2/Hlt2SharedUnbiasedPhi2KK/Particles'
-Hlt2SharedUnbiasedPhi2KK.DecayDescriptor = "phi(1020) -> K+ K-" 
-Hlt2SharedUnbiasedPhi2KK.DaughtersCuts = { "K+" : "(PT > 500*MeV) & (PIDK>0.)" } 
-Hlt2SharedUnbiasedPhi2KK.CombinationCut = "(ADAMASS('phi(1020)')<20*MeV)"
-Hlt2SharedUnbiasedPhi2KK.MotherCut = "(VFASPF(VCHI2PDOF)<25)"
+
+Hlt2SharedUnbiasedPhi2KK = Hlt2Member( CombineParticles
+                                       , "UnbiasedPhi2KK"
+                                       , Inputs = [ BiKalmanFittedRichKaons ]
+                                       , DecayDescriptor = "phi(1020) -> K+ K-"
+                                       , DaughtersCuts = { "K+" : "(PT > 500*MeV) & (PIDK>0.)" }
+                                       , CombinationCut = "(ADAMASS('phi(1020)')<30*MeV)"
+                                       , MotherCut = "(VFASPF(VCHI2PDOF)<25)" )
 
 UnbiasedPhi2KK = bindMembers( 'Shared', [ BiKalmanFittedRichKaons, Hlt2SharedUnbiasedPhi2KK ] )
