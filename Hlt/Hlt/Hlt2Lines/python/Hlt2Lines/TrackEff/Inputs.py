d12a52174bb74be19faf3109a822bef2a2cde36d
Mika Anton Vesterinen <mika.vesterinen@cern.ch>
2015-03-14 14:16:45 +0000

    TrackEff/Inputs.py now uses RevivedVelo for the velo tracks
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@185422 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 27a4c6d987402676a98d37a3414083a9823cdc5d

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEff/Inputs.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEff/Inputs.py
@@ -1,15 +1,11 @@
 __author__  = 'Mika Vesterinen'
 __date__    = '$Date: 2015-02-23$'
 __version__ = '$Revision: 0.0 $'
+
 from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedPions       as Hlt2SlowPions
 from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedRichPions   as Hlt2LoosePions
 from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedRichKaons   as Hlt2LooseKaons
 from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedRichProtons as Hlt2LooseProtons
-
-#from Hlt2SharedParticles.TagAndProbeParticles import ProbeVeloPions as Hlt2ProbeVeloPions
-#from Hlt2SharedParticles.TagAndProbeParticles import ProbeVeloKaons as Hlt2ProbeVeloKaons
-#from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedPions       as Hlt2ProbeVeloPions
-#from Hlt2SharedParticles.TrackFittedBasicParticles import Hlt2BiKalmanFittedKaons       as Hlt2ProbeVeloKaons
 
 ## velo probes made here for now ##
 from HltTracking.HltTrackNames import HltSharedTrackLoc, HltDefaultFitSuffix, _baseProtoPLocation, TrackName, Hlt2TrackEffRoot
@@ -17,15 +13,12 @@ from HltLine.HltLine import bindMembers
 from Configurables import CombinedParticleMaker, ChargedProtoParticleMaker, BestPIDParticleMaker, NoPIDsParticleMaker
 from Configurables import DelegatingTrackSelector
 from Configurables import TrackContainerCopy,TrackStateInitAlg,DelegatingTrackSelector, DecodeVeloRawBuffer
-
-DecodeVelo = DecodeVeloRawBuffer('DecodeVelo') 
-DecodeVelo.DecodeToVeloLiteClusters = True
-DecodeVelo.DecodeToVeloClusters = False
+from HltTracking.HltSharedTracking import RevivedVelo
 from TrackFitter.ConfiguredFitters import (ConfiguredEventFitter,
                                            ConfiguredForwardStraightLineEventFitter)
 
 copyVelo = TrackContainerCopy( "CopyVeloForFitForTrackEff" )
-copyVelo.inputLocation = HltSharedTrackLoc["Velo"]
+copyVelo.inputLocation = RevivedVelo.outputSelection()
 copyVelo.outputLocation = Hlt2TrackEffRoot+'/FittedVelo/VeloTracks'
 preve = TrackStateInitAlg("PrepareVeloTracks",TrackLocation = copyVelo.outputLocation )
 preve.StateInitTool.VeloFitterName = "FastVeloFitLHCbIDs"
@@ -35,7 +28,6 @@ MyVeloFit = ConfiguredEventFitter("VeloRefitterAlgForTrackEff",
                                   SimplifiedGeometry = True)
 
 Hlt2VeloProtos = ChargedProtoParticleMaker('Hlt2VeloProtosForTrackEff')
-#Hlt2VeloProtos.Inputs = [ HltSharedTrackLoc["Velo"]]
 Hlt2VeloProtos.Inputs = [copyVelo.outputLocation]
 Hlt2VeloProtos.Output = _baseProtoPLocation("Hlt2", Hlt2TrackEffRoot+"/"+TrackName["Velo"])
 Hlt2VeloProtos.addTool(DelegatingTrackSelector, name='delTrackSelVelo')
@@ -48,7 +40,8 @@ Hlt2VeloKaonParts = NoPIDsParticleMaker("Hlt2VeloKaonParts")
 Hlt2VeloKaonParts.Particle = 'kaon'
 Hlt2VeloKaonParts.Input =  Hlt2VeloProtos.Output
 Hlt2VeloKaonParts.Output =  Hlt2TrackEffRoot+"/Hlt2VeloKaons/Particles"
-ProbeVeloProtos  = bindMembers( None, [ DecodeVelo,copyVelo,preve,MyVeloFit,Hlt2VeloProtos ])
+
+ProbeVeloProtos  = bindMembers( None, [ RevivedVelo,copyVelo,preve,MyVeloFit,Hlt2VeloProtos ])
 Hlt2ProbeVeloPions  = bindMembers( None, [ ProbeVeloProtos , Hlt2VeloPionParts ] )
 Hlt2ProbeVeloKaons  = bindMembers( None, [ ProbeVeloProtos , Hlt2VeloKaonParts ] )
 
