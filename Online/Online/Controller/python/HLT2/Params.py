8b81d63e6bcd156f091ae3d5a41f4ea12ff76e06
Markus Frank <frankb@cern.ch>
2015-03-25 20:07:31 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@185905 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 4fce14e39c7156a8088cbf724c5a29a1925cf2a0

--- Online/Online/Controller/python/HLT2/Params.py
+++ Online/Online/Controller/python/HLT2/Params.py
@@ -1,3 +1,4 @@
+import rundb_params
 home        = '/home/frankm/HLT2mon'
 credentials = 'sqlite:'+home+'/HLT2MONITORING.db'
-rundb       = 'oracle:rundb_admin@lhcbonr_rundb/adm1n'
+rundb       = 'oracle:'+rundb_params.login+'@'+rundb_params.tns+'/'+rundb_params.pwd
