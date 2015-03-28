11362d543eb6ea88d64bb2d13ca57afe313f3d5f
Vanya Belyaev <ivan.belyaev@cern.ch>
2015-03-28 16:34:33 +0000

    fix a typo in print method
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@186003 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 33db8d58a322c6af136f7499ad235acfcde18723

--- LHCb/Phys/LoKiMC/src/MCParticles.cpp
+++ LHCb/Phys/LoKiMC/src/MCParticles.cpp
@@ -2642,7 +2642,7 @@ int LoKi::MCParticles::NinAncestors::nInAncestors
 // ============================================================================
 std::ostream& 
 LoKi::MCParticles::NinAncestors::fillStream( std::ostream& s ) const 
-{ return s << " MCNINANCESTOS( " << m_cut << " ) " ; }
+{ return s << " MCNINANCESTORS( " << m_cut << " ) " ; }
 
 // ============================================================================
 // constructor from the predicate 
@@ -2697,7 +2697,7 @@ bool LoKi::MCParticles::InAncestors::inAncestors
 // ============================================================================
 std::ostream& 
 LoKi::MCParticles::InAncestors::fillStream( std::ostream& s ) const 
-{ return s << " MCINANCESTOS( " << m_cut << " ) " ; }
+{ return s << " MCINANCESTORS( " << m_cut << " ) " ; }
 // ============================================================================
 
 
