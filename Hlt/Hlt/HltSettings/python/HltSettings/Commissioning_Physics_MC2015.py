37e97025f3d96400f9d4e34dcea67dad6e7dc281
Roel Aaij <roel.aaij@cern.ch>
2015-03-28 15:32:17 +0000

    Add Commissioning_Physics_MC2015.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@185998 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 895c32c0d8d0c00adf82fddc3a26d09aca1ab276

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/Commissioning_Physics_MC2015.py
@@ -0,0 +1,19 @@
+from Commissioning_Physics_2015 import Commissioning_Physics_2015
+
+class Commissioning_Physics_MC2015(Commissioning_Physics_2015):
+    def verifyType(self,ref) :
+        # verify self.ActiveLines is still consistent with
+        # our types self.ActiveHlt2Lines and self.ActiveHlt1Lines...
+        # so we can force that classes which inherit from us
+        # and overrule either ActiveHlt.Lines also overrule
+        # HltType...
+        if ( self.ActiveHlt1Lines() != ref.ActiveHlt1Lines(self)  or
+             self.ActiveHlt2Lines() != ref.ActiveHlt2Lines(self) ) :
+            raise RuntimeError( 'Must update HltType when modifying ActiveHlt.Lines()' )
+        
+    def L0TCK(self) :
+        return '0xFF65'
+
+    def HltType(self) :
+        self.verifyType( Commissioning_Physics_MC2015 ) 
+        return          'Commissioning_Physics_MC2015'
