1f2bab221d6018fa70f867c2a6e77628487e9d7b
Roel Aaij <roel.aaij@cern.ch>
2015-03-28 16:30:10 +0000

    Fix renaming error for DataType != 2015.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@186002 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8ddef9916da52f09a55ec68e6318965bbad29151

--- Rec/Tf/TrackSys/python/TrackSys/RecoTracking.py
+++ Rec/Tf/TrackSys/python/TrackSys/RecoTracking.py
@@ -250,8 +250,8 @@ def RecoTracking(exclude=[]):
       else :
          from Configurables import PatVeloTT
          GaudiSequencer("TrackVeloTTPatSeq").Members += [ PatVeloTT("PatVeloTT")]
-         from PatVeloTTAlgConf import PatVeloTTConf
-         PatVeloTTConf.PatVeloTTConf().configureAlg()
+         from PatVeloTT.PatVeloTTAlgConf import PatVeloTTConf
+         PatVeloTTConf().configureAlg()
          if TrackSys().timing() :
             PatVeloTT("PatVeloTT").TimingMeasurement = True;
       tracklists += ["Rec/Track/VeloTT"]
