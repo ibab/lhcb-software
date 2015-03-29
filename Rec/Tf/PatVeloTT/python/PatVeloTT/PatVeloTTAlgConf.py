ec8aca208ce3d9fff76c003281624f608112f3c4
Michel De Cian <michel.de.cian@cern.ch>
2015-03-29 11:41:26 +0000

    Add configuration for PatVeloTTHybrid in HLT1 in 2015 to 'PatVeloTTAlgConf'
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@186012 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5119d30be051986c0d85633bb322c52309301957

--- Rec/Tf/PatVeloTT/python/PatVeloTT/PatVeloTTAlgConf.py
+++ Rec/Tf/PatVeloTT/python/PatVeloTT/PatVeloTTAlgConf.py
@@ -2,12 +2,14 @@ from TrackSys.Configuration import *
 from TrackFitter.ConfiguredFitters import *
 
 from Configurables import ( PatVeloTT, PatVeloTTTool,
+                            PatVeloTTHybrid, PatVeloTTHybridTool,
                             TrackMasterFitter, TrackMasterExtrapolator,
                             TrackKalmanFilter, TrackSimpleExtraSelector )
 
 class PatVeloTTConf(object):
+
    def configureAlg(self, patVTT = PatVeloTT("PatVeloTT")):
-      
+      '''Configure the algorithm for Run I conditions'''
       patVTT.InputUsedTracksNames = ["Rec/Track/Forward","Rec/Track/Match"]
       
       patVTT.addTool(TrackMasterFitter,"Fitter")
@@ -21,7 +23,16 @@ class PatVeloTTConf(object):
          patVTT.fitTracks = False
       else:
          patVTT.maxChi2 = 5.
-   
+
+   def configureAlgRun2HLT1(self, patVTT = PatVeloTTHybrid("PatVeloTTHybrid")):
+      '''Configure VeloTT algorithm for HLT1 stage of run II offline tracking
+      Note the the output container has VeloTT tracks and Velo tracks which pass
+      through the hole in the TT around the beam pipe'''
+      
+      patVTT.OutputTracksName = "Rec/Track/VeloTTHybrid"
+      patVTT.addTool(PatVeloTTHybridTool, name="PatVeloTTHybridTool")
+      patVTT.PatVeloTTHybridTool.PassTracks = True
+         
       
    def configureFitTool(self, fitTool):
       
