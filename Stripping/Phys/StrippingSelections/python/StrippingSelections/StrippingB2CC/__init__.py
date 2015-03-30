c0891df0c85f59f184e20f447b61f0daa38439ac
Carlos Vazquez Sierra <carlos.vazquez@cern.ch>
2015-03-30 22:08:13 +0000

    Migration of B2CC lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@186067 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1b964538cf364d7e1f7c43eefa99330509878edd

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CC/__init__.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingB2CC/__init__.py
@@ -3,7 +3,14 @@ Module importing stripping selection line builder modules
 for B2CC WG.
 """
 
-_selections = [ ]
+# last update: 30th March 2015
+_selections = ['StrippingB2JpsiXforBeta_s',
+               'StrippingB2Psi2SX',
+               'StrippingB2Psi2SXMuMu',
+               'StrippingB2CharmoniumX_6H',
+               'StrippingBs2EtacPhiBDT',
+               'StrippingBd2JpsieeKS',
+               'StrippingBs2JpsieePhi']
 
 for _sel in _selections :
     try :
