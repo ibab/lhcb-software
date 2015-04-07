bba078d5db76243026aa9147dd484562e9042c80
Markus Frank <frankb@cern.ch>
2015-04-07 14:08:15 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@186368 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 76f4a303b594c2b79bbd8e98804c938df55c36cb

--- Online/Online/FarmConfig/job/AligWrk.sh
+++ Online/Online/FarmConfig/job/AligWrk.sh
@@ -21,13 +21,13 @@ elif [ "$act" = "Alignment|Velo" ]; then
   . /group/online/dataflow/cmtuser/AlignmentOnlineDev_v10r2/setup.x86_64-slc6-gcc48-dbg.vars
   export PYTHONPATH=${ALIGNONLINEROOT}/python:$PYTHONPATH
   cd ${FARMCONFIGROOT}/job
-  exec -a ${UTGID} ${Class1_task} libGaudiOnline.so OnlineTask -tasktype=LHCb::Class1Task -main=/group/online/dataflow/templates/options/Main.opts -opt=command="import Gaudi,GaudiKernel.ProcessJobOptions; from Gaudi.Configuration import importOptions; GaudiKernel.ProcessJobOptions.printing_level=999; from TrAligAnalyzer import doIt; doIt('/localdisk/Alignment/VeLo', 'VeloHalfAlignment');"
+  exec -a ${UTGID} ${Class1_task} libGaudiOnline.so OnlineTask -tasktype=LHCb::Class1Task -main=/group/online/dataflow/templates/options/Main.opts -opt=command="import Gaudi,GaudiKernel.ProcessJobOptions; from Gaudi.Configuration import importOptions; GaudiKernel.ProcessJobOptions.printing_level=999; from TrAligAnalyzer import doIt; doIt('/localdisk/Alignment/Velo', 'VeloHalfAlignment');"
 elif [ "$act" = "Alignment|TED" ]; then
   . /group/online/dataflow/cmtuser/AlignmentOnlineDev_v10r2/setup.x86_64-slc6-gcc48-dbg.vars
   export PYTHONPATH=${ALIGNONLINEROOT}/python:$PYTHONPATH
   cd ${FARMCONFIGROOT}/job
   exec -a ${UTGID} ${Class1_task} libGaudiOnline.so OnlineTask -tasktype=LHCb::Class1Task -main=/group/online/dataflow/templates/options/Main.opts -opt=command="import Gaudi,GaudiKernel.ProcessJobOptions; from Gaudi.Configuration import importOptions; GaudiKernel.ProcessJobOptions.printing_level=999; from TrAligAnalyzer import doIt; doIt('/localdisk/Alignment/Tracker', 'TEDAlignment');"
-elif [ "$act" = "RichMirror" ]; then
+elif [ "$act" = "Alignment|Rich" ]; then
   . /group/online/dataflow/cmtuser/Panoptes/OnlPanoptesSys/cmt/setup.x86_64-slc6-gcc48-dbg.vars
   cd ${FARMCONFIGROOT}/job
   export PYTHONPATH=${ONLPANOPTESSYSROOT}/python:$PYTHONPATH
