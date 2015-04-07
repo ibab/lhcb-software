39c6d5349558be066f697d7721afe71fca15f938
Albert Puig Navarro <albert.puig@epfl.ch>
2015-04-07 16:04:07 +0000

    Updated B2GammaGamma to make use of all the changes in LoKiTrigger
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@186392 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1ef1dd8832bcef1b38d5fae2455e07f8669b525a

--- Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1B2GammaGammaLines.py
+++ Hlt/Hlt/Hlt1Lines/python/Hlt1Lines/Hlt1B2GammaGammaLines.py
@@ -12,47 +12,30 @@ from HltLine.HltLinesConfigurableUser import HltLinesConfigurableUser
 
 
 class Hlt1B2GammaGammaLinesConf(HltLinesConfigurableUser) :
-    __slots__ = {'GAMMA_PT_MIN'             : 3000,     # MeV
-                 'B_SUMPT_MIN'              : 6000,     # MeV
-                 'B_PT_MIN'                 : 3000,     # MeV
+    __slots__ = {'GAMMA_PT_MIN'             : 3500,     # MeV
+                 'B_SUMPT_MIN'              : 8000,     # MeV
+                 'B_PT_MIN'                 : 2000,     # MeV
                  'B_MASS_MIN'               : 3500,     # MeV
-                 'B_MASS_MAX'               : 7500,     # MeV
+                 'B_MASS_MAX'               : 6000,     # MeV
                 }
 
     def hlt1B2GammaGammaLine_Preambulo(self, props):
         from HltTracking.Hlt1Tracking import L0CaloCandidates
 
-        preambulo = [L0CaloCandidates('Photon'),
-                     "from LoKiArrayFunctors.decorators import ASUM, AM",
-                     "from LoKiPhys.decorators import PT, ALL",
-                     "from LoKiArrayFunctors.decorators import APT, ADAMASS, ACUTDOCA",
-                     "CombinationConf = LoKi.Hlt1.Hlt1CombinerConf( 'B_s0 -> gamma gamma' \
-                                         , (APT>%(B_PT_MIN)s*MeV) & (in_range(%(B_MASS_MIN)s*MeV, AM, %(B_MASS_MAX)s*MeV)) & (ASUM(PT) > %(B_SUMPT_MIN)s*MeV)\
-                                         , ALL, 'MomentumCombiner')" % props]
+        preambulo = [L0CaloCandidates('DiPhoton')]
         return preambulo
 
     def hlt1B2GammaGammaLine_Streamer(self, name, props) :
         from Configurables import LoKi__HltUnit as HltUnit
         props['name'] = name
 
-        PhotonUnitLineCode = """
+        DiPhotonUnitLineCode = """
         L0CaloCandidates
         >>  tee  ( monitor( TC_SIZE > 0, '# pass CaloCandidates', LoKi.Monitoring.ContextSvc ) )
         >>  tee  ( monitor( TC_SIZE    , 'nCaloCandidates' , LoKi.Monitoring.ContextSvc ) )
-        >>  TC_FROMCALOTOPARTICLES( 'gamma',  '', (PT>%(GAMMA_PT_MIN)s*MeV) )
-        >>  tee ( monitor( TC_SIZE > 0, '# pass ToPhotons', LoKi.Monitoring.ContextSvc ) )
-        >>  tee ( monitor( TC_SIZE    , 'nPhotons',         LoKi.Monitoring.ContextSvc ) )
-        >>  SINK ('Hlt1B2GammaGammaPhotons')
-        >>  ~TC_EMPTY
-        """ % props
-
-        BUnitLineCode = """
-        TC_HLT1COMBINER('', 
-                        CombinationConf,
-                        'Hlt1B2GammaGammaPhotons',
-                        ALL)
-        >>  tee ( monitor( TC_SIZE > 0, '# pass SV', LoKi.Monitoring.ContextSvc ) )
-        >>  tee ( monitor( TC_SIZE    , 'nSV',       LoKi.Monitoring.ContextSvc ) )
+        >>  TC_HLT1DIPHOTONMAKER( 'B_s0',  '', %(GAMMA_PT_MIN)s, %(B_SUMPT_MIN)s, %(B_MASS_MIN)s, %(B_MASS_MAX)s, %(B_PT_MIN)s )
+        >>  tee ( monitor( TC_SIZE > 0, '# pass DiPhoton', LoKi.Monitoring.ContextSvc ) )
+        >>  tee ( monitor( TC_SIZE    , 'nDiPhotons',         LoKi.Monitoring.ContextSvc ) )
         >>  SINK ('Hlt1B2GammaGammaDecision')
         >>  ~TC_EMPTY
         """ % props
@@ -61,23 +44,16 @@ class Hlt1B2GammaGammaLinesConf(HltLinesConfigurableUser) :
         from Configurables import LoKi__HltUnit as HltUnit
         from HltTracking.HltPVs import PV3D
 
-        hlt1B2GammaGammaLine_PhotonUnit = HltUnit('Hlt1%sPhotonUnit' % name,
-                                                  #OutputLevel=1,
-                                                  Monitor=True,
-                                                  Preambulo=self.hlt1B2GammaGammaLine_Preambulo(props),
-                                                  Code=PhotonUnitLineCode)
+        hlt1B2GammaGammaLine_DiPhotonUnit = HltUnit('Hlt1%sDiPhotonUnit' % name,
+                                                    #OutputLevel=1,
+                                                    Monitor=True,
+                                                    Preambulo=self.hlt1B2GammaGammaLine_Preambulo(props),
+                                                    Code=DiPhotonUnitLineCode)
 
-        hlt1B2GammaGammaLine_BUnit = HltUnit('Hlt1%sUnit' % name,
-                                             PVSelection="PV3D",
-                                             #OutputLevel=1,
-                                             Monitor=True,
-                                             Preambulo=self.hlt1B2GammaGammaLine_Preambulo(props),
-                                             Code=BUnitLineCode)
 
         return [Hlt1GECUnit('Loose'),
                 PV3D('Hlt1'),
-                hlt1B2GammaGammaLine_PhotonUnit,
-                hlt1B2GammaGammaLine_BUnit]
+                hlt1B2GammaGammaLine_DiPhotonUnit]
 
     def __apply_configuration__(self) :
         from HltLine.HltLine import Hlt1Line
