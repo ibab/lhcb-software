84d1c8e8c8fa884c7b477e3232d27999bb30ba64
Michael David Sokoloff <michael.david.sokoloff@cern.ch>
2015-04-14 13:54:49 +0000

    code for Xi- --> Lambda,pi and Omega- --> Lambda,K
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@186629 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 94165fc8c019600851d7aadde8f5e98cc98f1b31

---
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/ChargedHyperonLines.py
@@ -0,0 +1,70 @@
+from GaudiKernel.SystemOfUnits import GeV, MeV, picosecond, mm
+from Hlt2Lines.Utilities.Hlt2LinesConfigurableUser import Hlt2LinesConfigurableUser
+
+
+class ChargedHyperonLines():
+    def localcuts(self):
+        return {
+                 # 
+                 'Ximinus2LambdaPi_LLL':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1280 * MeV,
+                                    'AM_MAX'                   :  1365 * MeV,
+                                    'Mass_M_MIN'               :  1290.0 * MeV,
+                                    'Mass_M_MAX'               :  1355.0 * MeV,
+                                   },
+                 'Ximinus2LambdaPi_DDL':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1280 * MeV,
+                                    'AM_MAX'                   :  1365 * MeV,
+                                    'Mass_M_MIN'               :  1290.0 * MeV,
+                                    'Mass_M_MAX'               :  1355.0 * MeV,
+                                   },
+                 # 
+                 'Omegaminus2LambdaK_LLL':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1630 * MeV,
+                                    'AM_MAX'                   :  1715 * MeV,
+                                    'Mass_M_MIN'               :  1640.0 * MeV,
+                                    'Mass_M_MAX'               :  1705.0 * MeV,
+                                   },
+                 'Omegaminus2LambdaK_DDL':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1630 * MeV,
+                                    'AM_MAX'                   :  1715 * MeV,
+                                    'Mass_M_MIN'               :  1640.0 * MeV,
+                                    'Mass_M_MAX'               :  1705.0 * MeV,
+                                   },
+                }
+    
+    def locallines(self):
+        from Stages import MassFilter
+        from Stages import Xi2LambdaPi_LLL, Xi2LambdaPi_DDL
+        from Stages import Omega2LambdaK_LLL, Omega2LambdaK_DDL
+        stages = {# 
+                  'Xi2LambdaPi_LLL'       : [MassFilter('Ximinus2LambdaPi_LLL', inputs=[Xi2LambdaPi_LLL])],
+                  'Xi2LambdaPi_DDL'       : [MassFilter('Ximinus2LambdaPi_DDL',inputs=[Xi2LambdaPi_DDL])],
+                  'Omega2LambdaK_LLL'     : [MassFilter('Omegaminus2LambdaK_LLL',inputs=[Omega2LambdaK_LLL])],
+                  'Omega2LambdaK_DDL'     : [MassFilter('Omegaminus2LambdaK_DDL',inputs=[Omega2LambdaK_DDL])],
+                 }
+        return stages
