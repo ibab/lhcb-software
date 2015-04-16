746ba393628819e2e8e4d5b55a2c4471618d0acb
Tom Hadavizadeh <tom.hadavizadeh@cern.ch>
2015-04-16 17:30:15 +0000

    Updated CharmHad/D2HHLines.py to include lines being developed by Simone Stracka
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@186746 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c53af3e366afdea4da0e412c1ed911132e1d9fdb

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/D2HHLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/D2HHLines.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+## @file
+#  Set of Hlt2-lines for D(s) -> h h0 with h0= pi0, eta, eta' 
+#   where pi0/eta -> e+ e- gamma and eta/eta'-> pi+ pi- gamma
+
+
+#
+#  @author Tom HADAVIZADEH tom.hadavizadeh@physics.ox.ac.uk
+#
+
 from GaudiKernel.SystemOfUnits import GeV, MeV, picosecond, mm
 from Hlt2Lines.Utilities.Hlt2LinesConfigurableUser import Hlt2LinesConfigurableUser
 
@@ -78,22 +88,79 @@ class CharmHadD2HHLines() :
                                                           
                  'Conv_Photon' : {'Elec_PT'               : 350  * MeV,
                                   'Trk_MIPCHI2DV_MIN'     : 6.0,
-                                  'Track_Chi2'            : 5         },                                                                  
+                                  'Track_Chi2'            : 5         }, 
+                 'D2EtaPi_hhgamma' : {
+                    'AM_MIN'            : 1550 * MeV, 
+                    'AM_MAX'            : 2550 * MeV,
+                    'DMASS_MIN'         : 1600 * MeV,
+                    'DMASS_MAX'         : 2500 * MeV,
+                    'APT_MIN'           : 1500.0,  # 2000 in stripping
+                    'BPVLTIME_MIN'      : 0.1,   # ps , ~ 0.15 ps in the stripping
+                    'VCHI2PDOF_MAX'     : 5,
+                    'AMRhoH_MIN'        : 0 * MeV,
+                    'AMRhoH_MAX'        : 2550 * MeV, #
+                    'AMEta_MIN'        : 450 * MeV,
+                    'AMEta_MAX'        : 1150 * MeV,
+                                                    },
+                    'D2EtaK_hhgamma' : {
+                    'AM_MIN'            : 1550 * MeV, 
+                    'AM_MAX'            : 2550 * MeV,
+                    'DMASS_MIN'         : 1600 * MeV,
+                    'DMASS_MAX'         : 2500 * MeV,
+                    'APT_MIN'           : 1500.0,  # 2000 in stripping
+                    'BPVLTIME_MIN'      : 0.1,   # ps , ~ 0.15 ps in the stripping
+                    'VCHI2PDOF_MAX'     : 5,
+                    'AMRhoH_MIN'        : 0 * MeV,
+                    'AMRhoH_MAX'        : 2550 * MeV, #
+                    'AMEta_MIN'        : 450 * MeV,
+                    'AMEta_MAX'        : 1150 * MeV,
+                                                    },
+                                                    
+                   'DiPion_forD2EtaH' : {
+                    'AM_MAX'                   : 1150 * MeV,     
+                    'ASUMPT_MIN'               :  0.0 * MeV,
+                    'ADOCA_MAX'                :  0.1 * mm,
+                    'VCHI2_MAX'                :  20.0,
+                    'BPVVD_MIN'                :  0.0,
+                    'BPVCORRM_MAX'             : 9999 * GeV, #3.5 * GeV
+                    'BPVVDCHI2_MIN'            :  0.0,
+                                              },                                                                 
                 }
     
     def locallines(self):
         from Stages import MassFilter
         from Stages import D2PiH0_eeg, D2KH0_eeg, D2PiH0_pipig, D2KH0_pipig, DStar2PiD0_eeg, DStar2PiD0_ee 
+        from Stages import D2RhoHG_3Body_Combiner
+        from Stages import DetachedRhoPIDChild
+        from Stages import SharedNoPIDDetachedChild_pi
+        from Stages import SharedNoPIDDetachedChild_K
+        from Stages import SharedNeutralLowPtChild_gamma
+
         stages = {'D2PiPi0_eegamma'           : [ D2PiH0_eeg('D2HPi0')    ],
                   'D2KPi0_eegamma'            : [ D2KH0_eeg('D2HPi0')     ],
                   'D2PiEta_eegamma'           : [ D2PiH0_eeg('D2HEta')    ],
                   'D2KEta_eegamma'            : [ D2KH0_eeg('D2HEta')     ],
+                  
                   'D2PiEta_pipigamma'         : [ D2PiH0_pipig('D2HEta')  ],
                   'D2KEta_pipigamma'          : [ D2KH0_pipig('D2HEta')   ],
                   'D2PiEtaPrime_pipigamma'    : [ D2PiH0_pipig('D2HEtaPrime')  ],
                   'D2KEtaPrime_pipigamma'     : [ D2KH0_pipig('D2HEtaPrime')   ],
                   
                   'Dstar2PiD0_eegamma'        : [ DStar2PiD0_eeg('Dstar2PiD0_eegamma')],
-                  'Dstar2PiD0_ee'             : [ DStar2PiD0_ee('Dstar2PiD0_ee') ]                                                
+                  'Dstar2PiD0_ee'             : [ DStar2PiD0_ee('Dstar2PiD0_ee') ],
+                  
+                  'D2EtaPi_hhgamma'           : [D2RhoHG_3Body_Combiner('D2EtaPi_hhgamma',
+                                                                        decay = ["D+ -> rho(770)0 pi+ gamma","D- -> rho(770)0 pi- gamma"],
+                                                                        inputs = [DetachedRhoPIDChild('DiPion_forD2EtaH'),
+                                                                                  SharedNoPIDDetachedChild_pi,
+                                                                                  SharedNeutralLowPtChild_gamma ])],
+                  'D2EtaK_hhgamma'            : [D2RhoHG_3Body_Combiner('D2EtaK_hhgamma',
+                                                                        decay = ["D+ -> rho(770)0 K+ gamma","D- -> rho(770)0 K- gamma"],
+                                                                        inputs = [DetachedRhoPIDChild('DiPion_forD2EtaH'),
+                                                                                  SharedNoPIDDetachedChild_K,
+                                                                                  SharedNeutralLowPtChild_gamma ])]
+                  
+                  
+                                                                 
                  }
         return stages
