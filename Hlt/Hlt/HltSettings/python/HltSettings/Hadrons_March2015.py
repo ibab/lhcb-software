f23dda676bdd9036ebb8e875f36bbb28f882a5fa
Roel Aaij <roel.aaij@cern.ch>
2015-05-06 15:32:38 +0000

    Reinclude B2XGammaUnbiased lines.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@187792 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5ca93e064de4c7c0fa026b8ee76f257db1215c44

--- Hlt/Hlt/HltSettings/python/HltSettings/Hadrons_March2015.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/Hadrons_March2015.py
@@ -1,16 +1,16 @@
 class Hadrons_March2015 :
     """
     Threshold settings for Hlt2 hadronic lines: minimal Sept for 2011
-    
+
     WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
-    
-    @author V. Gligorov 
+
+    @author V. Gligorov
     @date 2010-12-02
     """
-    
+
     __all__ = ( 'ActiveHlt2Lines' )
-    
-    
+
+
     def ActiveHlt2Lines(self) :
         """
         Returns a list of active lines
@@ -19,15 +19,14 @@ class Hadrons_March2015 :
         lines = [
             # XGamma
             'Hlt2Bs2PhiGamma',
-            #'Hlt2Bs2PhiGammaWideBMass',
             'Hlt2Bd2KstGamma',
-            #'Hlt2Bd2KstGammaWideBMass',
-            #'Hlt2Bd2KstGammaWideKMass',
+            'Hlt2Bs2PhiGammaUnbiased',
+            'Hlt2Bd2KstGammaULUnbiased',
             # B2HH
             'Hlt2B2HH',
             # DiProtons
             'Hlt2DiProton',
-            #'Hlt2DiProtonTF',   
+            #'Hlt2DiProtonTF',
             'Hlt2DiProtonLowMult',#,
             #'Hlt2DiProtonLowMultTF'
             #DiPhi
@@ -35,7 +34,7 @@ class Hadrons_March2015 :
             ]
         from TopoLines_April2015 import TopoLines_April2015
         lines.extend( TopoLines_April2015().ActiveHlt2Lines() )
-        
+
 
         # For inclusive phi extend using Albert's new setting
         # from Phi_draft2012 import Phi_draft2012 as PhiLines
@@ -46,10 +45,10 @@ class Hadrons_March2015 :
 
         return lines
 
-    def Thresholds(self) : 
+    def Thresholds(self) :
 
         d = {}
- 
+
         from TopoLines_April2015 import TopoLines_April2015
         d.update ( TopoLines_April2015().Thresholds() )
 
