f7cd1846e4db93700eda5c8e5ba7f67c5e193592
Roberta Cardinale <roberta.cardinale@cern.ch>
2015-05-13 12:38:08 +0000

    Added StrippingB2pphh in BnoC
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@188157 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: af9f1520015b5acaf17985143cb2f18334640686

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBnoC/__init__.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBnoC/__init__.py
@@ -7,7 +7,8 @@ _selections = [ 'StrippingHb2Charged2Body',
                 'StrippingB2CharmlessQuasi2Body', 
                 'StrippingB2HHBDT', 
                 'StrippingBc2hhh_BnoC',
-                'StrippingBu2hhh']
+                'StrippingBu2hhh',
+                'StrippingB2pphh']
 
 for _sel in _selections :
     try :
