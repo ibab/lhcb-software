768ae178c784cb8b8464cf6c7c426a5141beeb99
Roberta Cardinale <roberta.cardinale@cern.ch>
2015-05-18 14:24:43 +0000

    fixed small bug in relatedinfo in StrippingBandQ/StrippingXic2HHH.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@188401 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f6f8c48d257983d4e258567ac040e836361f533e

--- Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBandQ/StrippingXic2HHH.py
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingBandQ/StrippingXic2HHH.py
@@ -421,11 +421,11 @@ default_config = {
                      , 'PrescaleXic2KLam'        : 0.0 
                      , 'PostscaleXic2KLam'       : 0.0 
                      , 'RelatedInfoTools' : [
-    { "Type" : "RelInfoConeVariables", "ConeAngle" : 1.5, "Variables" : ['angle', 'mult', 'ptasy'], "Location": "Cone1"},
-    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.75,  "Variables" : ['angle', 'mult', 'ptasy'], "Location": "Cone2"},
-    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.5, "Variables" : ['angle', 'mult', 'ptasy'], "Location": "Cone3"},
-    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.25,  "Variables" : ['angle', 'mult', 'ptasy'], "Location": "Cone4"},
-    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.1,  "Variables" : ['angle', 'mult', 'ptasy'], "Location": "Cone5"}
+    { "Type" : "RelInfoConeVariables", "ConeAngle" : 1.5, "Variables" : ['CONEANGLE', 'CONEMULT', 'CONEPTASYM'], "Location": "Cone1"},
+    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.75,  "Variables" : ['CONEANGLE', 'CONEMULT', 'CONEPTASYM'], "Location": "Cone2"},
+    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.5, "Variables" : ['CONEANGLE', 'CONEMULT', 'CONEPTASYM'], "Location": "Cone3"},
+    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.25,  "Variables" : ['CONEANGLE', 'CONEMULT', 'CONEPTASYM'], "Location": "Cone4"},
+    { "Type" : "RelInfoConeVariables", "ConeAngle" : 0.1,  "Variables" : ['CONEANGLE', 'CONEMULT', 'CONEPTASYM'], "Location": "Cone5"}
     ] 
                      },
     'STREAMS' : [
