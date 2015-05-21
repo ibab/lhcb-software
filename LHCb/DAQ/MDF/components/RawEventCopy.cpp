6c69d4ec13f36f9f063a9019a2c6202e1654d93d
Markus Frank <frankb@cern.ch>
2015-05-21 08:15:29 +0000

    Remove unnecessary dependencies
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@188593 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b4ff0ea97c04424add579eaa227c262d8e15e221

--- LHCb/DAQ/MDF/components/RawEventCopy.cpp
+++ LHCb/DAQ/MDF/components/RawEventCopy.cpp
@@ -1,6 +1,6 @@
 // $Id: RawEventCopy.cpp,v 1.2 2009-05-25 08:56:21 cattanem Exp $
 // Include files from Gaudi
-#include "GaudiAlg/GaudiAlgorithm.h" 
+#include "GaudiKernel/Algorithm.h" 
 #include "GaudiKernel/IDataProviderSvc.h" 
 #include "MDF/RawEventHelpers.h"
 #include "Event/RawEvent.h"
@@ -9,7 +9,8 @@
  *    LHCb namespace declaration
  */
 namespace LHCb  {
-class RawEvent;
+
+  class RawEvent;
 
   /** @class RawEventCopy RawEventCopy.cpp
     *
@@ -19,7 +20,7 @@ class RawEvent;
     *  @author Markus Frank
     *  @date   2005-10-13
     */
-  class RawEventCopy : public GaudiAlgorithm {
+  class RawEventCopy : public Algorithm    {
     /// Property: source leaf for rawevent copy
     std::string m_source;
     /// Property: target leaf name for rawevent copy
@@ -28,7 +29,7 @@ class RawEvent;
   public: 
     /// Standard constructor
     RawEventCopy(const std::string& nam, ISvcLocator* pSvc) 
-    : GaudiAlgorithm(nam,pSvc) {
+    : Algorithm(nam,pSvc) {
       declareProperty("Destination",m_destination="/Event/DAQ/RawCopy");
       declareProperty("Source",     m_source=RawEventLocation::Default);
     }
