8964956d8f166158fe3169b19cb3740e8938224c
Marco Cattaneo <marco.cattaneo@cern.ch>
2015-05-27 13:47:47 +0000

    Force 2012 Datatype in test for the time being
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@189021 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 9ab528d2567a41964d36ed86ed895e97ea0a8017

--- Brunel/BrunelSys/tests/options/testBrunel.py
+++ Brunel/BrunelSys/tests/options/testBrunel.py
@@ -6,6 +6,7 @@ from Configurables import Brunel
 from Gaudi.Configuration import *
 
 Brunel().EvtMax    = 0
+Brunel().DataType  = "2012" # Temporarily, until 2015 configuration is stable
 Brunel().InputType = "MDF"
 Brunel().Monitors  = ["FPE","SC"]
 
