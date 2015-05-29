926d5a373431eb474544a6dd8c23a6b0e1029723
Lucia Grillo <lucia.grillo@cern.ch>
2015-05-29 09:45:26 +0000

    also the stand alone clbr algo compiles. Now it needs to be updated
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@189201 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3fc7e670812cd69c3132d48576654846bcb04128

---
+++ Alignment/Calibration/OTCalibration/scripts/clbr_grid_withMLA.py
@@ -0,0 +1,73 @@
+
+from Gaudi.Configuration import *
+
+#from Configurables import CondDB
+#CondDB(UseOracle = True)
+#importOptions("$APPCONFIGOPTS/DisableLFC.py")
+
+from Configurables import Brunel
+Brunel().DataType = "2012"
+Brunel().Simulation = False
+Brunel().InputType = "MDF"
+
+Brunel().DDDBtag = 'dddb-20130503-1'                                                                                                                             
+Brunel().CondDBtag = 'cond-20130710'
+
+from Configurables import LHCbApp
+#LHCbApp().DDDBtag   = "default"
+#LHCbApp().CondDBtag = "default"
+LHCbApp().DDDBtag = 'dddb-20130503-1'                                                                                                                             
+LHCbApp().CondDBtag = 'cond-20130710'
+
+from Configurables import TrajOTProjector
+Projector = TrajOTProjector("OTProjector", UseDrift = True)
+
+from TrackFitter.ConfiguredFitters import *
+Fitter = ConfiguredMasterFitter("TrackMonFitter")
+Fitter.Projector.OT = Projector
+Fitter.MaxNumberOutliers = 0
+
+from Configurables import ( GaudiSequencer, TrackHitAdder, TrackEventFitter, OTModuleClbrMon, TrackMon )
+GaudiSequencer("MoniOTSeq").Members = [
+  TrackHitAdder(TrackLocation = "Rec/Track/Best"),
+  TrackEventFitter(TracksInContainer = "Rec/Track/Best", Fitter = Fitter),
+  OTModuleClbrMon(
+    TrackType = 3, # Long tracks
+    MinP = 3.2, # GeV
+    MaxTrackChi2 = 100,
+    MaxTrackUbChi2 = 2,
+    ReadXMLs = False,
+    TrackLocation = "Rec/Track/Best",
+    Fitter = Fitter,
+    Projector = Projector
+  )
+]
+
+NTupleSvc().Output = [ "FILE1 DATAFILE='clbr.root' TYP='ROOT' OPT='NEW'" ]
+HistogramPersistencySvc().OutputFile = "clbr_hists.root"
+
+Brunel().OutputType = "None"
+Brunel().Histograms = "None"
+
+Brunel().RecoSequence = [ "Decoding", "VELO", "TT", "IT", "OT", "Tr", "Vertex" ]
+from Configurables import RecMoniConf
+RecMoniConf().setProp("MoniSequence", [ "OT" ] )
+
+path_monolayer = "/afs/cern.ch/user/w/wouter/public/AlignDB/"
+ddbs = []
+ddbs.append(path_monolayer + "OTMonoGeometry.db/DDDB")
+ddbs.append(path_monolayer + "OTMonoCatalog.db/LHCBCOND")
+ddbs.append(path_monolayer + "OTMonoAlign20141225.db/LHCBCOND")
+
+counter = 1
+for db in ddbs:
+     from Configurables import ( CondDB, CondDBAccessSvc )
+     alignCond = CondDBAccessSvc( 'AlignDDDB' + str(counter) )
+     alignCond.ConnectionString = 'sqlite_file:' + db
+     CondDB().addLayer( alignCond )
+     counter += 1
+
+#from Configurables import ( CondDBAccessSvc, CondDB )
+#CondDB().addLayer(CondDBAccessSvc("db_global",
+#  ConnectionString = "sqlite_file:alignDB.early2012.2days.jpsi.db/LHCBCOND")
+#)
--- Alignment/Calibration/OTCalibration/scripts/clbr_make.sh
+++ Alignment/Calibration/OTCalibration/scripts/clbr_make.sh
@@ -1,4 +1,5 @@
 
-gcc -Wall -c clbr.cpp -I`root-config --incdir`
-gcc -Wall -c readT0s.cpp -I"/afs/cern.ch/sw/lcg/external/Boost/1.48.0_python2.6/x86_64-slc5-gcc43-opt/include/boost-1_48"
+gcc -Wall -c -std=c++11 clbr.cpp -I`root-config --incdir`
+#gcc -Wall -c readT0s.cpp -I"/afs/cern.ch/sw/lcg/external/Boost/1.48.0_python2.6/x86_64-slc5-gcc43-opt/include/boost-1_48"
+gcc -Wall -c -std=c++11 readT0s.cpp -I"/afs/cern.ch/sw/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc47-opt/include/boost-1_53"
 gcc clbr.o readT0s.o -o clbr `root-config --libs`
