f3867057b0f8e238ea5c3ac15726d8fd63ad5a4c
Rose Koopman <rose.koopman@cern.ch>
2015-06-02 13:31:56 +0000

    add dictionaries for OTLiteTime and OTLiteTimeContainer classes; they are needed for OT the python code used in calibration jobs
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@189461 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 513d6bdf8ab85ede84ae77e7e4c9b931438db306

--- LHCb/OT/OTDAQ/dict/OTDAQDict.h
+++ LHCb/OT/OTDAQ/dict/OTDAQDict.h
@@ -1,6 +1,7 @@
 #ifndef DICT_OTDAQDICT_H 
 #define DICT_OTDAQDICT_H 1
 
+#include "Event/OTLiteTime.h"
 #include "OTDAQ/IOTChannelMapTool.h"
 #include "OTDAQ/IOTRawBankDecoder.h"
 #include "OTDAQ/IOTRawBankEncoder.h"
