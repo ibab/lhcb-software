7632aa7e97796ebca0952f58fbda788367ea4578
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-06-15 07:26:33 +0000

    revert the change making the Velo dE/dx interface method non-const
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@190281 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 95a671b3e059b591b0ad554e0fd7b4ac38201b8f

--- Rec/Tr/TrackInterfaces/TrackInterfaces/ITrackVelodEdxCharge.h
+++ Rec/Tr/TrackInterfaces/TrackInterfaces/ITrackVelodEdxCharge.h
@@ -64,7 +64,7 @@ public:
    *          performed or not
    */
   virtual StatusCode nTracks( const LHCb::Track * track,
-                              double & nTks ) = 0;
+                              double & nTks ) const = 0;
 
 };
 
