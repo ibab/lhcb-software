7e4cadcffb56395cb2b88e7d15cde0a006fa5496
Chitsanu Khurewathanakul <chitsanu.khurewathanakul@cern.ch>
2015-06-15 11:34:09 +0000

    Modified collect_dicts.py output engine to use json builtin-lib
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@190292 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: de8c4e6407bce9b81aaf5cacbd4d6e4efde45fa7

--- Stripping/Phys/StrippingSelections/tests/liaisons/collect_dicts.py
+++ Stripping/Phys/StrippingSelections/tests/liaisons/collect_dicts.py
@@ -2,17 +2,21 @@ __author__ = 'Carlos Vazquez Sierra'
 __date__ = '2015/05/18'
 
 '''
-Module for a quick collection of config dictionaries. After the environment is set run with:
+Module for a quick collection of config dictionaries. After the environment is 
+set run with:
 
-python collect_dicts.py <your WG>
+    python collect_dicts.py <your WG>
 
-It will create a file called dictionaries.py with all the configuration of the lines of your working group as specified by the
-users in their default_config
+It will create a file called `dictionaries.py` with all the configuration of the 
+lines of your working group as specified by the users in their default_config
 
 Modified by Andrea Contu 03/06/15
+Modified by C.Khurewathanakul 15/06/15
 '''
 
+import re
 import sys
+import json
 
 # Specify the name of your configuration
 my_wg=sys.argv[1] #FOR LIAISONS
@@ -22,67 +26,90 @@ my_wg=sys.argv[1] #FOR LIAISONS
 # default_config dictionary in the code where your LineBuilder 
 # is defined.
 from StrippingSelections import buildersConf
-confs = buildersConf()
-_dict={}
+confs = buildersConf(WGs=[ my_wg ])
+
+## Prepare outputfile
+fname = 'dictionaries.py'
+with open(fname, 'w') as fout:
+    ## Finally, json-dump each one.
+    for name, config in sorted(confs.iteritems()):
+        config.pop('NAME', None) # Strangely, some conf doesn't have 'NAME' 
+        val   = json.dumps( config, sort_keys=True, indent=4 )
+        val   = re.sub(r'\[\n\s+(\S+)\n\s+\]', r'[ \1 ]', val) # Pretty one-liner list
+        val   = re.sub(r'false', 'False', val) # Capitalize boolean patch
+        val   = re.sub(r'true', 'True', val) 
+        val   = re.sub(r'null', 'None', val) # null-value patch
+        entry = name + ' = ' + val + '\n\n'
+        fout.write(entry)
+        print 'Write: ', name
+print 'Write successfully: '+fname
+
+## Double-check: Re-exec the entire output file.
+execfile(fname)
 
 
-def makestring(input):
-    if isinstance(input,basestring):
-        if "\'" not in input:
-            return "\'"+input+"\'"
-        else:
-            return "\""+input+"\""
-    else:
-        return str(input)
+#--------
 
-for k,mod in confs.iteritems():
-    if mod.has_key("WGs"):
-        if my_wg== (mod["WGs"])[0] or my_wg==mod["WGs"]:
-            _dict[k]=mod
+# for k,mod in confs.iteritems():
+#     if mod.has_key("WGs"):
+#         print mod['WGs']
+#         if my_wg== (mod["WGs"])[0] or my_wg==mod["WGs"]:
+#             _dict[k]=mod
 
-_output = open('dictionaries.py','w')
 
-_ordkeys = ['BUILDERTYPE', 'STREAMS', 'WGs', 'CONFIG']
+# def makestring(input):
+#     if isinstance(input,basestring):
+#         if "\'" not in input:
+#             return "\'"+input+"\'"
+#         else:
+#             return "\""+input+"\""
+#     else:
+#         return str(input)
 
-_dictkeys=_dict.keys()
-_dictkeys.sort()
+# _output = open('dictionaries.py','w')
 
-for key in _dictkeys:
-    md=_dict[key]
-    #_output.write('\n'+key+' = '+s+'\n')
-    _output.write('\n'+key+' = {\n')
-    for k in _ordkeys:
-        if k!='CONFIG' and k!='STREAMS':
-            _output.write("    "+makestring(k)+" : "+makestring(md[k])+",\n")
-        elif k=='STREAMS':
-            if isinstance(md[k],basestring):
-                _output.write("    "+makestring(k)+" : "+makestring(md[k])+",\n")
-            elif isinstance(md[k],dict):
-                _output.write("    "+makestring(k)+" : {,\n")
-                for streamdict in (md[k]).keys():
-                    _output.write("        "+makestring(streamdict)+" : "+ makestring((md[k])[streamdict]) + ",\n")
-                _output.write("    },\n")
+# _ordkeys = ['BUILDERTYPE', 'STREAMS', 'WGs', 'CONFIG']
 
-            else:
-                _output.write("    "+makestring(k)+" : "+str(md[k])+",\n")
-        else:
-            _output.write("    "+makestring(k)+" : { \n")
-            confdictkeys=((md[k])).keys()
-            confdictkeys.sort()
-            for kconf in confdictkeys:
-                if isinstance(((md[k])[kconf]), basestring):
-                    _output.write("        "+makestring(kconf)+" : "+ makestring((md[k])[kconf]) +",\n")
-                elif isinstance(((md[k])[kconf]), dict):
-                    _output.write("        "+makestring(kconf)+" : {\n")
-                    for cdict in ((md[k])[kconf]).keys():
-                        _output.write("            "+makestring(cdict)+" : "+ makestring(((md[k])[kconf])[cdict]) +",\n")
+# _dictkeys=_dict.keys()
+# _dictkeys.sort()
+
+
+# for key in _dictkeys:
+#     md=_dict[key]
+#     #_output.write('\n'+key+' = '+s+'\n')
+#     _output.write('\n'+key+' = {\n')
+#     for k in _ordkeys:
+#         if k!='CONFIG' and k!='STREAMS':
+#             _output.write("    "+makestring(k)+" : "+makestring(md[k])+",\n")
+#         elif k=='STREAMS':
+#             if isinstance(md[k],basestring):
+#                 _output.write("    "+makestring(k)+" : "+makestring(md[k])+",\n")
+#             elif isinstance(md[k],dict):
+#                 _output.write("    "+makestring(k)+" : {,\n")
+#                 for streamdict in (md[k]).keys():
+#                     _output.write("        "+makestring(streamdict)+" : "+ makestring((md[k])[streamdict]) + ",\n")
+#                 _output.write("    },\n")
+
+#             else:
+#                 _output.write("    "+makestring(k)+" : "+str(md[k])+",\n")
+#         else:
+#             _output.write("    "+makestring(k)+" : { \n")
+#             confdictkeys=((md[k])).keys()
+#             confdictkeys.sort()
+#             for kconf in confdictkeys:
+#                 if isinstance(((md[k])[kconf]), basestring):
+#                     _output.write("        "+makestring(kconf)+" : "+ makestring((md[k])[kconf]) +",\n")
+#                 elif isinstance(((md[k])[kconf]), dict):
+#                     _output.write("        "+makestring(kconf)+" : {\n")
+#                     for cdict in ((md[k])[kconf]).keys():
+#                         _output.write("            "+makestring(cdict)+" : "+ makestring(((md[k])[kconf])[cdict]) +",\n")
                          
-                    _output.write("        "+"},\n")
-                else:
-                    sconf=str((md[k])[kconf])
-                    _output.write("        "+makestring(kconf)+" : "+ sconf +",\n")
-            _output.write("    } \n")
-    _output.write('}\n')
+#                     _output.write("        "+"},\n")
+#                 else:
+#                     sconf=str((md[k])[kconf])
+#                     _output.write("        "+makestring(kconf)+" : "+ sconf +",\n")
+#             _output.write("    } \n")
+#     _output.write('}\n')
 
 #from __init__ import _selections
 
