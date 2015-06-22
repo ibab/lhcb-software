bbf38953bf7efd3fc5bd31c9d4821d933306390f
Ben Couturier <lben@cern.ch>
2015-06-22 12:59:14 +0000

    LBCORE-840 Release of LbScripts v8r4p1
    git-svn-id: http://svn.cern.ch/guest/lhcb/LbScripts/trunk@190623 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8827ddfc68731b63f2365653a9ebac68df41e0b5

--- LbScripts/LbLegacy/python/LbLegacy/install_project.py
+++ LbScripts/LbLegacy/python/LbLegacy/install_project.py
@@ -21,10 +21,10 @@ import socket
 from urllib import urlretrieve, urlopen, urlcleanup
 from tempfile import mkdtemp
 
-script_version = '150527'
+script_version = '150622'
 python_version = sys.version_info[:3]
 txt_python_version = ".".join([str(k) for k in python_version])
-lbscripts_version = "v8r4"
+lbscripts_version = "v8r4p1"
 compat_version = None
 line_size = 120
 #-----------------------------------------------------------------------------------
