706c11c9d30acb121de4fa71c10141df4999210b
Giulio Dujany <giulio.dujany@cern.ch>
2015-06-25 21:17:16 +0000

    changed track selection for VeloHalfAlignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/AlignmentOnline/trunk@190849 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 91e8d0d7e7be6be66cfe49af1e2558875c84b808

--- AlignmentOnline/AlignmentOnline/AlignOnline/python/AlignmentConfigurations/VeloHalfAlignment.py
+++ AlignmentOnline/AlignmentOnline/AlignOnline/python/AlignmentConfigurations/VeloHalfAlignment.py
@@ -1,12 +1,13 @@
 def configureAlignment():
     from Configurables import TAlignment
-    from TAlignment.TrackSelections import GoodLongTracks, VeloOverlapTracks, VeloBackwardTracks
+    from TAlignment.TrackSelections import GoodLongTracks, VeloOverlapTracks, VeloBackwardTracks, GoodLongRefittedVeloTracks
     from TAlignment.VertexSelections import configuredPVSelection
     # specify what we actually align for
-    TAlignment().TrackSelections = [GoodLongTracks(),
-                                    VeloOverlapTracks(),
-                                    VeloBackwardTracks()]
-
+    TAlignment().TrackSelections = [
+        GoodLongRefittedVeloTracks('TrackRefitSeq'),
+        VeloOverlapTracks(),
+        VeloBackwardTracks()]
+    
     # add the default PV selection
     TAlignment().PVSelection = configuredPVSelection()
 
