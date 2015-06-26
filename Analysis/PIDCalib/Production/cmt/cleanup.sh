99cf60337578d296a542fea63124310dcde51127
Lucio Anderlini <lucio.anderlini@cern.ch>
2015-06-26 08:05:21 +0000

    mv PIDCalib/Production to Erasmus project
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@190851 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2984a20ee15ea86805900c7c052f3e4fd8d15a9e

--- Analysis/PIDCalib/Production/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/cern.ch/sw/contrib/CMT/v1r20p20090520; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=Production -version=v1r0 -path=/afs/cern.ch/user/l/landerli/cmtuser/DaVinci_v36r7p2/PIDCalib/ $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
