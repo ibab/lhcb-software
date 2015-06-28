6ba66f9b5b2a1b59e8102819ba8e140b5be24460
Roel Aaij <roel.aaij@cern.ch>
2015-06-28 12:16:36 +0000

    Add an expert property to TCKANNSvc to allow injection of selection IDs.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@190889 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 70810554f36be09059c4fd93c5d7424189e192f0

--- LHCb/Hlt/HltServices/src/TCKANNSvc.cpp
+++ LHCb/Hlt/HltServices/src/TCKANNSvc.cpp
@@ -1,20 +1,65 @@
-#include "Kernel/IIndexedANNSvc.h"
-#include "Kernel/IPropertyConfigSvc.h"
+#include <string>
+#include <map>
 
-#include "Kernel/TCK.h"
+// Define the parser
+#include "GaudiKernel/ParsersFactory.h"
+
+namespace {
+   using additionalIDs_t = std::map<std::string, std::map<std::string, unsigned int>>;
+}
+
+namespace Gaudi {
+namespace Parsers {
+
+// Parser grammar
+template <typename Iterator, typename Skipper>
+struct Grammar_<Iterator, additionalIDs_t, Skipper > {
+   // In this case, the type is a mapping type, so it requires the MapGrammar.
+   // For other grammars see GaudiKernel/GrammarsV2.h
+   typedef MapGrammar<Iterator, ::additionalIDs_t, Skipper> Grammar;
+};
+
+// Additional parser for our property
+StatusCode parse(additionalIDs_t& result, const std::string& input) {
+    return Gaudi::Parsers::parse_(result, input);
+}
+}
+}
+
+// We also need to be able to print an object of our type as a string that both
+// Python and our parser can understand,
+#include "GaudiKernel/ToStream.h"
+namespace std {
+  // This is an example valid for any mapping type.
+  ostream& operator<<(ostream& s, const additionalIDs_t& m) {
+    bool first = true;
+    s << '{';
+    for(const auto& i: m) {
+      if (first) first = false;
+      else s << ", ";
+      Gaudi::Utils::toStream(i.first, s) << ": ";
+      Gaudi::Utils::toStream(i.second, s);
+    }
+    s << '}';
+    return s;
+  }
+}
+
 #include "GaudiKernel/Service.h"
 #include "GaudiKernel/VectorMap.h"
-#include <map>
-#include <string>
+
+#include "Kernel/IIndexedANNSvc.h"
+#include "Kernel/IPropertyConfigSvc.h"
+#include "Kernel/TCK.h"
 
 
-class TCKANNSvc : public Service 
+class TCKANNSvc : public Service
                 , virtual public IIndexedANNSvc  {
 
 public:
   TCKANNSvc( const std::string& name, ISvcLocator* pSvcLocator);
-  ~TCKANNSvc() = default ; 
-  
+  ~TCKANNSvc() = default ;
+
   StatusCode queryInterface(const InterfaceID& riid, void** ppvUnknown) override;
   StatusCode initialize() override;
   StatusCode finalize() override;
@@ -22,6 +67,9 @@ public:
   GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> i2s(unsigned int index, const Gaudi::StringKey& major) const override;
 
 private:
+
+  // properties
+  additionalIDs_t m_additionals;
 
   mutable IPropertyConfigSvc * m_propertyConfigSvc;
   std::string       m_propertyConfigSvcName;
@@ -41,6 +89,7 @@ TCKANNSvc::TCKANNSvc( const std::string& name, ISvcLocator* pSvcLocator)
 {
   declareProperty("IPropertyConfigSvcInstance", m_propertyConfigSvcName = "PropertyConfigSvc");
   declareProperty("InstanceName", m_instanceName = "HltANNSvc");
+  declareProperty("AdditionalIDs", m_additionals);
 }
 
 StatusCode
@@ -58,6 +107,11 @@ TCKANNSvc::initialize()
     fatal() << "TCKANNSvc failed to get the IConfigAccessSvc." << endmsg;
     return StatusCode::FAILURE;
   }
+
+  if (!m_additionals.empty()) {
+     warning() << "Additional IDs specified for " << m_instanceName << "." << endmsg
+               << "This is an expert options, and not for general use." << endmsg;
+  }
   return sc;
 }
 
@@ -69,8 +123,8 @@ TCKANNSvc::finalize(  )
 }
 
 
-GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> 
-TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const 
+GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey>
+TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const
 {
     TCK _tck(tck); _tck.normalize();
     auto entry = m_cache.find(_tck);
@@ -84,12 +138,14 @@ TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const
         }
         PropertyConfig::digest_type child = m_propertyConfigSvc->findInTree(tree->digest(), m_instanceName);
         if (child.invalid()) {
-          error() << "Error finding configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+          error() << "Error finding configuration of " << m_instanceName << " for TCK " << _tck
+                  <<  " Returning an empty map... " << endmsg;
           return {};
         }
         const PropertyConfig *config = m_propertyConfigSvc->resolvePropertyConfig(child);
-        if (!config) { 
-          error() << "Error reading configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+        if (!config) {
+          error() << "Error reading configuration of " << m_instanceName << " for TCK " << _tck
+                  <<  " Returning an empty map... " << endmsg;
           return {};
         }
         auto status = m_cache.insert( { _tck, config } );
@@ -99,10 +155,11 @@ TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const
         }
         entry = status.first;
     }
-    auto prop = std::find_if( std::begin(entry->second->properties()), std::end( entry->second->properties() ), 
+    auto prop = std::find_if( std::begin(entry->second->properties()), std::end( entry->second->properties() ),
                               [&](const std::pair<std::string,std::string>& p ) { return major.str() == p.first ; } );
     if (prop == std::end(entry->second->properties())) {
-      error() << "Error finding requested major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+      error() << "Error finding requested major " << major <<" in  configuration of " << m_instanceName
+              << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
       return {};
     }
     // now use the property parser to do the hard work of converting the string rep into a map...
@@ -114,18 +171,31 @@ TCKANNSvc::i2s(unsigned int tck, const Gaudi::StringKey& major) const
     // auto status = prp.fromString( prop->second );
     auto status = Gaudi::Parsers::parse(map , prop->second);
     if (status.isFailure()) {
-      error() << "Error interpreting requested major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+      error() << "Error interpreting requested major " << major <<" in  configuration of " << m_instanceName
+              << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
       return {};
     }
     GaudiUtils::VectorMap<unsigned int, Gaudi::StringKey> result;
+    auto additionals = m_additionals.find(major.str());
     bool ok = true;
+    if (additionals != end(m_additionals)) {
+       for (const auto& item : additionals->second) {
+          ok = ok && result.insert(item.second, item.first).second;
+       }
+    }
+    if (!ok) {
+      error() << "Duplicate entries in additional map for  major " << major <<" in  configuration of "
+              << m_instanceName << " for TCK "
+              << _tck <<  " Returning an empty map... " << endmsg;
+      return {};
+    }
     for ( const auto& item : map )  {
         ok = ok && result.insert( item.second, item.first ).second;
     }
     if (!ok) {
-      error() << "Duplicate entries in map for  major " << major <<" in  configuration of " << m_instanceName << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
+      error() << "Duplicate entries in map for  major " << major <<" in  configuration of " << m_instanceName
+              << " for TCK " << _tck <<  " Returning an empty map... " << endmsg;
       return {};
-
     }
     return result;
 }
