eb90c9a7cad366bc50ea501ef4182cf4e7c160fc
Jordi Garra Tico <jordi.garra.tico@cern.ch>
2015-06-29 16:41:58 +0000

    Cleanup EvtD0mixDalitz.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@191092 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 13132b1f6c2fcadab63f7ed006cacbdc717960e7

--- Gauss/Gen/EvtGen/src/EvtD0mixDalitz.cpp
+++ Gauss/Gen/EvtGen/src/EvtD0mixDalitz.cpp
@@ -142,12 +142,6 @@ void EvtD0mixDalitz::decay( EvtParticle* part )
       ampAntiDalitz = dalitzKsKK( antiPoint );
     }
 
-  //_i1    += ampDalitz     * conj( ampDalitz     ) / 1.e8;
-  //_iChi  += ampAntiDalitz * conj( ampDalitz     ) / 1.e8;
-  //_iChi2 += ampAntiDalitz * conj( ampAntiDalitz ) / 1.e8;
-
-  //std::cout << "INTEGRALS: " << _i1 << " " << _iChi << " " << _iChi2 << " " << _iChi / _i1 << " " << _iChi2 / _i1 << std::endl;
-
   // Assume there's no direct CP violation.
   EvtComplex barAOverA = ampAntiDalitz / ampDalitz;
 
@@ -155,11 +149,11 @@ void EvtD0mixDalitz::decay( EvtParticle* part )
   EvtComplex chi = _qp * barAOverA;
 
   // Generate a negative exponential life time. p( gt ) = ( 1 - y ) * e^{ - ( 1 - y ) gt }
-  double gt = -log( EvtRandom::Flat() ) / ( 1. - _y );
+  double gt = -log( EvtRandom::Flat() ) / ( 1.0 - _y );
   part->setLifetime( gt / _gamma );
 
   // Compute time dependent amplitude.
-  EvtComplex amp = .5 * ampDalitz * exp( - _y * gt / 2. ) * ( ( 1. + chi ) * h1( gt ) + ( 1. - chi ) * h2( gt ) );
+  EvtComplex amp = 0.5 * ampDalitz * exp( - _y * gt / 2.0 ) * ( ( 1.0 + chi ) * h1( gt ) + ( 1.0 - chi ) * h2( gt ) );
 
   vertex( amp );
 
@@ -190,33 +184,7 @@ void EvtD0mixDalitz::readPDGValues()
   // Compute the decay rate from the parameter in the evt.pdl file.
   _ctau = EvtPDL::getctau( EvtPDL::getId( "D0" ) );
 
-  //_iChi  = _qp * EvtComplex( 0.089723 , 0.0004776  ); // All resonances RBW, also Rho0.
-
-  //_iChi  = _qp * EvtComplex( 0.0481807, 0.0003043  ); // KStarm only
-  //_iChi  = _qp * EvtComplex( 0.0594099, 0.00023803 ); // All resonances RBW but GS Rho
-  //_iChi  = _qp * EvtComplex( 0.0780186, 0.000417646 ); // All resonances for KsKK
-  //_iChi2 = _qp * 1.;
-
-  /*
-  // Compute the gamma correction factor avgBeta = Gamma tau.
-  //    Compute the norm of the unnormalized p(\beta).
-  double factorY = ( 1. + abs( _iChi2 ) ) / 2. - _y * real( _iChi );
-  double factorX = ( 1. - abs( _iChi2 ) ) / 2. + _x * imag( _iChi );
-  double norm = factorY / ( 1. - pow( _y, 2 ) ) + factorX / ( 1. + pow( _x, 2 ) );
-
-  //    Compute the integral of p(\beta) \beta d\beta.
-  double termY = ( 1. + abs( _iChi2 ) ) / 2. - 2. * _y / ( 1. + pow( _y, 2 ) ) * real( _iChi );
-  double termX = ( 1. - abs( _iChi2 ) ) / 2. + 2. * _x / ( 1. - pow( _x, 2 ) ) * imag( _iChi );
-  double quotientY = ( 1. + pow( _y, 2 ) ) / pow( 1. - pow( _y, 2 ), 2 );
-  double quotientX = ( 1. - pow( _x, 2 ) ) / pow( 1. + pow( _x, 2 ), 2 );
-  double normTimesAvg = termY * quotientY + termX * quotientX;
-
-  double avgBeta = normTimesAvg / norm;
-
-  _gamma = avgBeta / _ctau;
-  */
-
-  _gamma = 1. / _ctau; // ALERT: Gamma is not 1 / tau.
+  _gamma = 1.0 / _ctau; // ALERT: Gamma is not 1 / tau.
 }
 
 
@@ -246,21 +214,21 @@ EvtComplex EvtD0mixDalitz::dalitzKsPiPi( const EvtDalitzPoint& point )
       static EvtDalitzReso KStarm_1680 ( plot, _BC, _AC, _VECTOR, 1.677   , 0.205    , _RBW );
 
       // Adding terms to the amplitude with their corresponding amplitude and phase terms.
-      amp += EvtComplex(   .848984 ,   .893618  );
+      amp += EvtComplex(  0.848984 ,  0.893618  );
       amp += EvtComplex( -1.16356  ,  1.19933   ) * KStarm      .evaluate( point );
-      amp += EvtComplex(   .106051 , - .118513  ) * KStarp      .evaluate( point );
+      amp += EvtComplex(  0.106051 , -0.118513  ) * KStarp      .evaluate( point );
       amp += EvtComplex(  1.0      ,  0.0       ) * rho0        .evaluate( point );
-      amp += EvtComplex( - .0249569,   .0388072 ) * omega       .evaluate( point );
-      amp += EvtComplex( - .423586 , - .236099  ) * f0_980      .evaluate( point );
+      amp += EvtComplex( -0.0249569,  0.0388072 ) * omega       .evaluate( point );
+      amp += EvtComplex( -0.423586 , -0.236099  ) * f0_980      .evaluate( point );
       amp += EvtComplex( -2.16486  ,  3.62385   ) * f0_1370     .evaluate( point );
-      amp += EvtComplex(   .217748 , - .133327  ) * f2_1270     .evaluate( point );
+      amp += EvtComplex(  0.217748 , -0.133327  ) * f2_1270     .evaluate( point );
       amp += EvtComplex(  1.62128  ,  1.06816   ) * K0Starm_1430.evaluate( point );
-      amp += EvtComplex(   .148802 ,   .0897144 ) * K0Starp_1430.evaluate( point );
-      amp += EvtComplex(  1.15489  , - .773363  ) * K2Starm_1430.evaluate( point );
-      amp += EvtComplex(   .140865 , - .165378  ) * K2Starp_1430.evaluate( point );
-      amp += EvtComplex( -1.55556  , - .931685  ) * sigma       .evaluate( point );
-      amp += EvtComplex( - .273791 , - .0535596 ) * sigma2      .evaluate( point );
-      amp += EvtComplex( -1.69720  ,   .128038  ) * KStarm_1680 .evaluate( point );
+      amp += EvtComplex(  0.148802 ,  0.0897144 ) * K0Starp_1430.evaluate( point );
+      amp += EvtComplex(  1.15489  , -0.773363  ) * K2Starm_1430.evaluate( point );
+      amp += EvtComplex(  0.140865 , -0.165378  ) * K2Starp_1430.evaluate( point );
+      amp += EvtComplex( -1.55556  , -0.931685  ) * sigma       .evaluate( point );
+      amp += EvtComplex( -0.273791 , -0.0535596 ) * sigma2      .evaluate( point );
+      amp += EvtComplex( -1.69720  ,  0.128038  ) * KStarm_1680 .evaluate( point );
     }
   else
     {
@@ -271,41 +239,41 @@ EvtComplex EvtD0mixDalitz::dalitzKsPiPi( const EvtDalitzPoint& point )
       static EvtDalitzReso rho0        ( plot, _AC, _BC, _VECTOR, 0.7758  , 0.1464   , _GS  );
       static EvtDalitzReso omega       ( plot, _AC, _BC, _VECTOR, 0.78259 , 0.00849  , _RBW );
       static EvtDalitzReso f2_1270     ( plot, _AC, _BC, _TENSOR, 1.2754  , 0.1851   , _RBW );
-      static EvtDalitzReso K0Starm_1430( plot, _AC, 1.46312, 0.232393, 1.0746, -1.83214, .803516, 2.32788, 1., -5.31306 ); // LASS
-      static EvtDalitzReso K0Starp_1430( plot, _AB, 1.46312, 0.232393, 1.0746, -1.83214, .803516, 2.32788, 1., -5.31306 ); // LASS
+      static EvtDalitzReso K0Starm_1430( plot, _AC, 1.46312, 0.232393, 1.0746, -1.83214, .803516, 2.32788, 1.0, -5.31306 ); // LASS
+      static EvtDalitzReso K0Starp_1430( plot, _AB, 1.46312, 0.232393, 1.0746, -1.83214, .803516, 2.32788, 1.0, -5.31306 ); // LASS
       static EvtDalitzReso K2Starm_1430( plot, _BC, _AC, _TENSOR, 1.4256  , 0.0985   , _RBW );
       static EvtDalitzReso K2Starp_1430( plot, _BC, _AB, _TENSOR, 1.4256  , 0.0985   , _RBW );
       static EvtDalitzReso KStarm_1680 ( plot, _BC, _AC, _VECTOR, 1.677   , 0.205    , _RBW );
 
       // Defining K-matrix.
-      static EvtComplex fr12( 1.87981, -.628378 );
-      static EvtComplex fr13( 4.3242 , 2.75019  );
-      static EvtComplex fr14( 3.22336,  .271048 );
-      static EvtComplex fr15(  .0    ,  .0      );
-      static EvtDalitzReso Pole1  ( plot, _BC, "Pole1"  , _KMAT, fr12, fr13, fr14, fr15, -.0694725 );
-      static EvtDalitzReso Pole2  ( plot, _BC, "Pole2"  , _KMAT, fr12, fr13, fr14, fr15, -.0694725 );
-      static EvtDalitzReso Pole3  ( plot, _BC, "Pole3"  , _KMAT, fr12, fr13, fr14, fr15, -.0694725 );
-      static EvtDalitzReso Pole4  ( plot, _BC, "Pole4"  , _KMAT, fr12, fr13, fr14, fr15, -.0694725 );
-      static EvtDalitzReso kmatrix( plot, _BC, "f11prod", _KMAT, fr12, fr13, fr14, fr15, -.0694725 );
+      static EvtComplex fr12( 1.87981, -0.628378 );
+      static EvtComplex fr13( 4.3242 ,  2.75019  );
+      static EvtComplex fr14( 3.22336,  0.271048 );
+      static EvtComplex fr15( 0.0    ,  0.0      );
+      static EvtDalitzReso Pole1  ( plot, _BC, "Pole1"  , _KMAT, fr12, fr13, fr14, fr15, -0.0694725 );
+      static EvtDalitzReso Pole2  ( plot, _BC, "Pole2"  , _KMAT, fr12, fr13, fr14, fr15, -0.0694725 );
+      static EvtDalitzReso Pole3  ( plot, _BC, "Pole3"  , _KMAT, fr12, fr13, fr14, fr15, -0.0694725 );
+      static EvtDalitzReso Pole4  ( plot, _BC, "Pole4"  , _KMAT, fr12, fr13, fr14, fr15, -0.0694725 );
+      static EvtDalitzReso kmatrix( plot, _BC, "f11prod", _KMAT, fr12, fr13, fr14, fr15, -0.0694725 );
 
       // Adding terms to the amplitude with their corresponding amplitude and phase terms.
       amp += EvtComplex( - 1.31394   ,  1.14072   ) * KStarm      .evaluate( point );
-      amp += EvtComplex(    .116239  , - .107287  ) * KStarp      .evaluate( point );
+      amp += EvtComplex(   0.116239  , -0.107287  ) * KStarp      .evaluate( point );
       amp += EvtComplex(   1.0       ,  0.0       ) * rho0        .evaluate( point );
-      amp += EvtComplex( -  .0313343 ,   .0424013 ) * omega       .evaluate( point );
-      amp += EvtComplex(    .559412  , - .232336  ) * f2_1270     .evaluate( point );
+      amp += EvtComplex( - 0.0313343 ,  0.0424013 ) * omega       .evaluate( point );
+      amp += EvtComplex(   0.559412  , -0.232336  ) * f2_1270     .evaluate( point );
       amp += EvtComplex(   7.35400   , -3.67637   ) * K0Starm_1430.evaluate( point );
-      amp += EvtComplex(    .255913  , - .190459  ) * K0Starp_1430.evaluate( point );
-      amp += EvtComplex(   1.05397   , - .936297  ) * K2Starm_1430.evaluate( point );
-      amp += EvtComplex( -  .00760136, - .0908624 ) * K2Starp_1430.evaluate( point );
-      amp += EvtComplex( - 1.45336   , - .164494  ) * KStarm_1680 .evaluate( point );
+      amp += EvtComplex(   0.255913  , -0.190459  ) * K0Starp_1430.evaluate( point );
+      amp += EvtComplex(   1.05397   , -0.936297  ) * K2Starm_1430.evaluate( point );
+      amp += EvtComplex( - 0.00760136, -0.0908624 ) * K2Starp_1430.evaluate( point );
+      amp += EvtComplex( - 1.45336   , -0.164494  ) * KStarm_1680 .evaluate( point );
       amp += EvtComplex( - 1.81830   ,  9.10680   ) * Pole1       .evaluate( point );
       amp += EvtComplex(  10.1751    ,  3.87961   ) * Pole2       .evaluate( point );
       amp += EvtComplex(  23.6569    , -4.94551   ) * Pole3       .evaluate( point );
-      amp += EvtComplex(    .0725431 , -9.16264   ) * Pole4       .evaluate( point );
+      amp += EvtComplex(   0.0725431 , -9.16264   ) * Pole4       .evaluate( point );
       amp += EvtComplex( - 2.19449   , -7.62666   ) * kmatrix     .evaluate( point );
 
-      amp *= .97; // Multiply by a constant in order to use the same maximum as RBW model.
+      amp *= 0.97; // Multiply by a constant in order to use the same maximum as RBW model.
     }
 
   return amp;
@@ -317,29 +285,29 @@ EvtComplex EvtD0mixDalitz::dalitzKsKK( const EvtDalitzPoint& point )
   static const EvtDalitzPlot plot( _mKs, _mK, _mK, _mD0 );
 
   // Defining resonances.
-  static EvtDalitzReso a00_980 ( plot, _AC, _BC, _SCALAR, 0.999  , _RBW, .550173, .324, _EtaPic   );
-  static EvtDalitzReso phi     ( plot, _AC, _BC, _VECTOR, 1.01943,       .00459319    , _RBW      );
-  static EvtDalitzReso a0p_980 ( plot, _AC, _AB, _SCALAR, 0.999  , _RBW, .550173, .324, _EtaPic   );
-  static EvtDalitzReso f0_1370 ( plot, _AC, _BC, _SCALAR, 1.350  ,       .265         , _RBW      );
-  static EvtDalitzReso a0m_980 ( plot, _AB, _AC, _SCALAR, 0.999  , _RBW, .550173, .324, _EtaPic   );
-  static EvtDalitzReso f0_980  ( plot, _AC, _BC, _SCALAR, 0.965  , _RBW, .695   , .165, _PicPicKK );
-  static EvtDalitzReso f2_1270 ( plot, _AC, _BC, _TENSOR, 1.2754 ,       .1851        , _RBW      );
-  static EvtDalitzReso a00_1450( plot, _AC, _BC, _SCALAR, 1.474  ,       .265         , _RBW      );
-  static EvtDalitzReso a0p_1450( plot, _AC, _AB, _SCALAR, 1.474  ,       .265         , _RBW      );
-  static EvtDalitzReso a0m_1450( plot, _AB, _AC, _SCALAR, 1.474  ,       .265         , _RBW      );
+  static EvtDalitzReso a00_980 ( plot, _AC, _BC, _SCALAR, 0.999  , _RBW, 0.550173, 0.324, _EtaPic   );
+  static EvtDalitzReso phi     ( plot, _AC, _BC, _VECTOR, 1.01943,       0.00459319     , _RBW      );
+  static EvtDalitzReso a0p_980 ( plot, _AC, _AB, _SCALAR, 0.999  , _RBW, 0.550173, 0.324, _EtaPic   );
+  static EvtDalitzReso f0_1370 ( plot, _AC, _BC, _SCALAR, 1.350  ,       0.265          , _RBW      );
+  static EvtDalitzReso a0m_980 ( plot, _AB, _AC, _SCALAR, 0.999  , _RBW, 0.550173, 0.324, _EtaPic   );
+  static EvtDalitzReso f0_980  ( plot, _AC, _BC, _SCALAR, 0.965  , _RBW, 0.695   , 0.165, _PicPicKK );
+  static EvtDalitzReso f2_1270 ( plot, _AC, _BC, _TENSOR, 1.2754 ,       0.1851         , _RBW      );
+  static EvtDalitzReso a00_1450( plot, _AC, _BC, _SCALAR, 1.474  ,       0.265          , _RBW      );
+  static EvtDalitzReso a0p_1450( plot, _AC, _AB, _SCALAR, 1.474  ,       0.265          , _RBW      );
+  static EvtDalitzReso a0m_1450( plot, _AB, _AC, _SCALAR, 1.474  ,       0.265          , _RBW      );
 
   // Adding terms to the amplitude with their corresponding amplitude and phase terms.
   EvtComplex amp( 0., 0. ); // Phase space amplitude.
-  amp += EvtComplex( 1.0          , 0.0        ) * a00_980 .evaluate( point );
-  amp += EvtComplex( -.126314     ,  .188701   ) * phi     .evaluate( point );
-  amp += EvtComplex( -.561428     ,  .0135338  ) * a0p_980 .evaluate( point );
-  amp += EvtComplex(  .035        , -.00110488 ) * f0_1370 .evaluate( point );
-  amp += EvtComplex( -.0872735    ,  .0791190  ) * a0m_980 .evaluate( point );
-  amp += EvtComplex( 0.           , 0.         ) * f0_980  .evaluate( point );
-  amp += EvtComplex(  .257341     , -.0408343  ) * f2_1270 .evaluate( point );
-  amp += EvtComplex( -.0614342    , -.649930   ) * a00_1450.evaluate( point );
-  amp += EvtComplex( -.104629     ,  .830120   ) * a0p_1450.evaluate( point );
-  amp += EvtComplex( 0.           , 0.         ) * a0m_1450.evaluate( point );
+  amp += EvtComplex(  1.0          ,  0.0        ) * a00_980 .evaluate( point );
+  amp += EvtComplex( -0.126314     ,  0.188701   ) * phi     .evaluate( point );
+  amp += EvtComplex( -0.561428     ,  0.0135338  ) * a0p_980 .evaluate( point );
+  amp += EvtComplex(  0.035        , -0.00110488 ) * f0_1370 .evaluate( point );
+  amp += EvtComplex( -0.0872735    ,  0.0791190  ) * a0m_980 .evaluate( point );
+  amp += EvtComplex(  0.0          ,  0.0        ) * f0_980  .evaluate( point );
+  amp += EvtComplex(  0.257341     , -0.0408343  ) * f2_1270 .evaluate( point );
+  amp += EvtComplex( -0.0614342    , -0.649930   ) * a00_1450.evaluate( point );
+  amp += EvtComplex( -0.104629     ,  0.830120   ) * a0p_1450.evaluate( point );
+  amp += EvtComplex(  0.0          ,  0.0        ) * a0m_1450.evaluate( point );
 
   return 2.8 * amp; // Multiply by 2.8 in order to reuse the same probmax as Ks pi pi.
 }
