f34367d1b824c2e0ee9f78d5a0028b54e29a7c43
Maurizio Martinelli <maurizio.martinelli@cern.ch>
2015-07-09 15:12:08 +0000

    added examples directory
    git-svn-id: http://svn.cern.ch/guest/lhcb/Alignment/trunk@191558 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 40d229ba204ab44173606760f045223941826d8a

---
+++ Alignment/Alignment/AlignmentMonitoring/examples/CompareConstants.py
@@ -0,0 +1,15 @@
+from GaudiPython import gbl
+AlMon = gbl.Alignment.AlignmentMonitoring
+
+alDir = "/afs/cern.ch/user/m/mamartin/public/tests/alignment/"
+versions = {'ITG': {'set': 'IT/ITGlobal/', 'ver':'v9'},
+            'ITM': {'set': 'IT/ITModules/','ver':'v7'},
+            'TTG': {'set': 'TT/TTGlobal/', 'ver':'v9'},
+            'TTM': {'set': 'TT/TTModules/', 'ver':'v9'},
+            'OTG': {'set': 'OT/OTGlobal/', 'ver':'v9'},
+            'OTM': {'set': 'OT/OTModules/', 'ver':'v9'} }
+cc = {}
+for key, args in versions.iteritems():
+    cc[key] = AlMon.CompareConstants(args['set'], args['ver'], alDir)
+    cc[key].Compare('v14')
+    cc[key].PrintWarnings()
