d19cf8779c82363c6293d2aa4619827ba8004b16
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-07-10 09:25:21 +0000

    Clean up a little
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@191597 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 80a3bcac36fe79add597a2eb873e6b064ad164f5

--- Lbcom/Rich/RichAlgorithms/src/RichHPDAnalysisAlg.cpp
+++ Lbcom/Rich/RichAlgorithms/src/RichHPDAnalysisAlg.cpp
@@ -54,14 +54,14 @@ StatusCode HPDAnalysisAlg::initialize()
   // HPD tools
   if ( m_toolNames.empty() ) return Warning( "No HPD analysis tools configured" );
   info() << "Loading HPD Analysis tools : " << m_toolNames << endmsg;
-  for ( std::vector<std::string>::const_iterator iT = m_toolNames.begin();
-        iT != m_toolNames.end(); ++iT )
+  for ( const auto& t : m_toolNames )
   {
     const Rich::IGenericHPDAnalysisTool * tool(NULL);
-    acquireTool( *iT, tool, this );
+    acquireTool( t, tool, this );
     m_tools.push_back(tool);
   }
 
+  // return
   return sc;
 }
 
@@ -72,11 +72,7 @@ StatusCode HPDAnalysisAlg::execute()
 {
   StatusCode sc = StatusCode::SUCCESS;
 
-  for ( HPDToolNames::const_iterator iLoc = m_taeEvents.begin();
-        iLoc != m_taeEvents.end(); ++iLoc )
-  {
-    sc = sc && processTAEEvent( *iLoc );
-  }
+  for ( const auto& evt : m_taeEvents ) { sc = sc && processTAEEvent(evt); }
 
   return sc;
 }
@@ -84,48 +80,41 @@ StatusCode HPDAnalysisAlg::execute()
 StatusCode HPDAnalysisAlg::processTAEEvent( const std::string & taeEvent )
 {
   // get the decoded data for this tae event
-  const Rich::DAQ::L1Map & l1Map = m_SmartIDDecoder->allRichSmartIDs(taeEvent);
+  const auto & l1Map = m_SmartIDDecoder->allRichSmartIDs(taeEvent);
 
-  for ( Rich::DAQ::L1Map::const_iterator iL1Map = l1Map.begin();
-        iL1Map != l1Map.end(); ++iL1Map )
+  for ( const auto& L1 : l1Map )
   {
-    const Rich::DAQ::IngressMap & ingressMap  = iL1Map->second;
-    for ( Rich::DAQ::IngressMap::const_iterator iIngressMap = ingressMap.begin();
-          iIngressMap != ingressMap.end(); ++iIngressMap )
+    for ( const auto& ingress : L1.second )
     {
-      const Rich::DAQ::IngressInfo & ingressInfo = iIngressMap->second;
-      const Rich::DAQ::HPDMap & hpdMap           = ingressInfo.hpdData();
-      for ( Rich::DAQ::HPDMap::const_iterator iHPDMap = hpdMap.begin();
-            iHPDMap != hpdMap.end(); ++iHPDMap )
+      for ( const auto& hpd : ingress.second.hpdData() )
       {
-        const Rich::DAQ::HPDInfo & hpdInfo    = iHPDMap->second;
-        const LHCb::RichSmartID  & hpdID      = hpdInfo.hpdID();
+        // HPD ID
+        const LHCb::RichSmartID & hpdID = hpd.second.hpdID();
 
         // Vector of SmartIDs
-        const LHCb::RichSmartID::Vector & rawIDs = hpdInfo.smartIDs();
+        const LHCb::RichSmartID::Vector & rawIDs = hpd.second.smartIDs();
 
         // Loop over HPD tools
-        for ( HPDTools::const_iterator iT = m_tools.begin();
-              iT != m_tools.end(); ++iT )
+        for ( const auto* t : m_tools )
         {
           // Analysis the HPD data
           IGenericHPDAnalysisTool::Results results;
-          const StatusCode sc = (*iT)->analyse( hpdID, rawIDs, results );
-          if ( sc.isFailure() ) return Warning( "HPD Analysis failed", sc );
-          // count any results
-          for ( IGenericHPDAnalysisTool::Results::const_iterator iR = results.begin();
-                iR != results.end(); ++iR )
+          const StatusCode sc = t->analyse( hpdID, rawIDs, results );
+          if ( sc.isFailure() )
           {
-            if ( (*iR).status.isSuccess() )
+            Warning( "HPD Analysis failed", sc ).ignore(); 
+          }
+          else
+          {
+            // count any results
+            for ( const auto& res : results )
             {
-              ++counter( (*iR).message );
-            }
-            else
-            {
-              Warning( (*iR).message, StatusCode::SUCCESS, 5 ).ignore();
+              if ( res.status.isSuccess() ) { ++counter( res.message ); }
+              else
+              { Warning( res.message, StatusCode::SUCCESS, 5 ).ignore(); }
             }
           }
-        }
+        } // tools loop
 
       }
     }
