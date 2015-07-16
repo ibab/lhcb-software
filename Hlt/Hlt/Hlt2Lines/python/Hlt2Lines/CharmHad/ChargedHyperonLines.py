a69a85b5e72f2bd3a2517a369d6b06d7ead437a3
Michael David Sokoloff <michael.david.sokoloff@cern.ch>
2015-07-16 15:38:31 +0000

    added DDD lines to LLL and DDL; also changed line names to reflect this.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@191956 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 776a30ef4e38429f45e961e4d50b303ea431c5d4

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/ChargedHyperonLines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/ChargedHyperonLines.py
@@ -30,6 +30,18 @@ class ChargedHyperonLines():
                                     'Mass_M_MIN'               :  1290.0 * MeV,
                                     'Mass_M_MAX'               :  1355.0 * MeV,
                                    },
+                 'Ximinus2LambdaPi_DDD':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1280 * MeV,
+                                    'AM_MAX'                   :  1365 * MeV,
+                                    'Mass_M_MIN'               :  1290.0 * MeV,
+                                    'Mass_M_MAX'               :  1355.0 * MeV,
+                                   },
                  # 
                  'Omegaminus2LambdaK_LLL':{
                                     'VCHI2PDOF_MAX'            :  10.0,
@@ -55,16 +67,30 @@ class ChargedHyperonLines():
                                     'Mass_M_MIN'               :  1640.0 * MeV,
                                     'Mass_M_MAX'               :  1705.0 * MeV,
                                    },
+                 'Omegaminus2LambdaK_DDD':{
+                                    'VCHI2PDOF_MAX'            :  10.0,
+                                    'Trk_ALL_MIPCHI2DV_MIN'    :  16.0,
+                                    'BPVVDCHI2_MIN'            :  10.0,
+                                    'BPVLTIME_MIN'             :  2.0 * picosecond,
+                                    'BPVDIRA_MIN'              :  0.9994,
+                                    'ASUMPT_MIN'               :  500 * MeV,
+                                    'AM_MIN'                   :  1630 * MeV,
+                                    'AM_MAX'                   :  1715 * MeV,
+                                    'Mass_M_MIN'               :  1640.0 * MeV,
+                                    'Mass_M_MAX'               :  1705.0 * MeV,
+                                   },
                 }
     
     def locallines(self):
         from Stages import MassFilter
-        from Stages import Xi2LambdaPi_LLL, Xi2LambdaPi_DDL
-        from Stages import Omega2LambdaK_LLL, Omega2LambdaK_DDL
+        from Stages import Xi2LambdaPi_LLL, Xi2LambdaPi_DDL, Xi2LambdaPi_DDD
+        from Stages import Omega2LambdaK_LLL, Omega2LambdaK_DDL, Omega2LambdaK_DDD
         stages = {# 
-                  'Xim2LamPim_LamLL'       : [MassFilter('Ximinus2LambdaPi_LLL', inputs=[Xi2LambdaPi_LLL])],
-                  'Xim2LamPim_LamDD'       : [MassFilter('Ximinus2LambdaPi_DDL',inputs=[Xi2LambdaPi_DDL])],
-                  'Omm2LamKm_LamLL'        : [MassFilter('Omegaminus2LambdaK_LLL',inputs=[Omega2LambdaK_LLL])],
-                  'Omm2LamKm_LamDD'        : [MassFilter('Omegaminus2LambdaK_DDL',inputs=[Omega2LambdaK_DDL])],
+                  'Xim2LamPim_LLL'       : [MassFilter('Ximinus2LambdaPi_LLL', inputs=[Xi2LambdaPi_LLL])],
+                  'Xim2LamPim_DDL'       : [MassFilter('Ximinus2LambdaPi_DDL',inputs=[Xi2LambdaPi_DDL])],
+                  'Xim2LamPim_DDD'       : [MassFilter('Ximinus2LambdaPi_DDD',inputs=[Xi2LambdaPi_DDD])],
+                  'Omm2LamKm_LLL'        : [MassFilter('Omegaminus2LambdaK_LLL',inputs=[Omega2LambdaK_LLL])],
+                  'Omm2LamKm_DDL'        : [MassFilter('Omegaminus2LambdaK_DDL',inputs=[Omega2LambdaK_DDL])],
+                  'Omm2LamKm_DDD'        : [MassFilter('Omegaminus2LambdaK_DDD',inputs=[Omega2LambdaK_DDD])],
                  }
         return stages
