1037e026b0f70f6cec6bc18aacab86b066c86673
Sean Benson <sean.benson@cern.ch>
2015-07-20 14:03:21 +0000

    Remove silly scaling
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@192126 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 0a6ce9c6f965b498f34df7d2aabd6582d8b327ce

--- LHCb/Hlt/HltDAQ/src/ReportConvertTool.cpp
+++ LHCb/Hlt/HltDAQ/src/ReportConvertTool.cpp
@@ -282,10 +282,10 @@ void ReportConvertTool::ParticleObject2Summary( HltObjectSummary::Info* info, co
       case 35: info->insert( particle_it->first, float( object->posCovMatrix()(1,0) ) ); break;
       case 36: info->insert( particle_it->first, float( object->posCovMatrix()(2,0) ) ); break;
       case 37: info->insert( particle_it->first, float( object->posCovMatrix()(2,1) ) ); break;
-      case 38: info->insert( particle_it->first, float( object->momentum().M()*100.0 ) ); break;
-      case 39: info->insert( particle_it->first, float( object->momentum().px()*100.0 ) ); break;
-      case 40: info->insert( particle_it->first, float( object->momentum().py()*100.0 ) ); break;
-      case 41: info->insert( particle_it->first, float( object->momentum().pz()*100.0 ) ); break;
+      case 38: info->insert( particle_it->first, float( object->momentum().M() ) ); break;
+      case 39: info->insert( particle_it->first, float( object->momentum().px() ) ); break;
+      case 40: info->insert( particle_it->first, float( object->momentum().py() ) ); break;
+      case 41: info->insert( particle_it->first, float( object->momentum().pz() ) ); break;
     }
   }
 
@@ -693,10 +693,10 @@ void ReportConvertTool::ParticleObjectFromSummary( const HltObjectSummary::Info*
       case 35: posCov(1,0) = (*info)[ particle_it->first ] ; break;
       case 36: posCov(2,0) = (*info)[ particle_it->first ] ; break;
       case 37: posCov(2,1) = (*info)[ particle_it->first ] ; break;
-      case 38: raw_m = (*info)[ particle_it->first ]/100.0 ; break;
-      case 39: raw_p1 = (*info)[ particle_it->first ]/100.0 ; break;
-      case 40: raw_p2 = (*info)[ particle_it->first ]/100.0 ; break;
-      case 41: raw_p3 = (*info)[ particle_it->first ]/100.0 ; break;
+      case 38: raw_m = (*info)[ particle_it->first ] ; break;
+      case 39: raw_p1 = (*info)[ particle_it->first ] ; break;
+      case 40: raw_p2 = (*info)[ particle_it->first ] ; break;
+      case 41: raw_p3 = (*info)[ particle_it->first ] ; break;
     }
   }
   object->setReferencePoint(xyz);
