063b1b28b0c1439c849b810dc1d98ea6db797645
Patrick Haworth Owen <patrick.haworth.owen@gmail.com>
2015-07-24 08:33:03 +0000

    update dict
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@192443 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e61b310b117858503a7e7f5023c19a0a8ac61c45

--- Hlt/Hlt/HltSettings/python/HltSettings/XcMuXForTau/XcMuXForTau_25ns_August2015.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/XcMuXForTau/XcMuXForTau_25ns_August2015.py
@@ -43,8 +43,8 @@ class XcMuXForTau_25ns_August2015 :
         d.update (
             {
                 XcMuXForTauLines: { 
-                  'Prescale' : {},
-                  'Common' : {
+                 'Prescale' : {},
+                 'Xc' :   {
                               'Pair_AMINDOCA_MAX'        : 0.10 * mm
                             , 'MIN_D_CHILDPT'          : 800.0 * MeV
                             , 'D0_BPVVDCHI2_MIN'         : 25.0       # neuter
@@ -54,24 +54,29 @@ class XcMuXForTau_25ns_August2015 :
                             , 'DTrk_ALL_PT_MIN'           : 200.0 * MeV
                             , 'DTrk_ALL_P_MIN'            : 5.0  * GeV
                             , 'DTrk_ALL_MIPCHI2DV_MIN'    : 9.0
-                            , 'XcMu_DOCA_MAX'        : 0.50 * mm
-                            , 'XcMu_VTXCHI2'        : 15
-                            , 'XcMu_DIRA'        : 0.999
-                            , 'XcMu_FDCHI2'        : 50
-                            , 'Mu_IPCHI2'    : 16
                             , 'D_IPCHI2'    : 5
                             , 'D_SUMPT'    : 2500 * MeV
-                            , 'K_PIDK'    : 2
-                            , 'Pi_PIDK'    : 4
-                            , 'P_PIDp'    : 0
+                            , 'K_PIDK'    : 2 
+                            , 'Pi_PIDK'    : 4 
+                            , 'P_PIDp'    : 0 
                             , 'D_AM_MIN'              : 1830.0 * MeV
                             , 'D_AM_MAX'              : 1910.0 * MeV
                             , 'Ds_AM_MIN'              : 1920.0 * MeV
                             , 'Ds_AM_MAX'              : 2010.0 * MeV
                             , 'Lc_AM_MIN'              : 2230.0 * MeV
                             , 'Lc_AM_MAX'              : 2330.0 * MeV
-                            }
-                     }
+                          },
+                 'Muon' : {
+                             'Mu_IPCHI2'    : 16
+                          },
+                 'B'    : {
+                
+                             'XcMu_VTXCHI2'        : 15
+                            , 'XcMu_DOCA_MAX'        : 0.50 * mm
+                            , 'XcMu_DIRA'        : 0.999
+                            , 'XcMu_FDCHI2'        : 50
+                          }
+                   }
                }
             )
         
