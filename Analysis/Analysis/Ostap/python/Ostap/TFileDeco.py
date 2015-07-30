678ce1c503a58e76496bbe1dac8be25c80ff3292
Vanya Belyaev <ivan.belyaev@cern.ch>
2015-07-30 17:38:30 +0000

    update it!
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@192863 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 395dc48b0ddb1df9546427c572dab8394931dc48

--- Analysis/Analysis/Ostap/python/Ostap/TFileDeco.py
+++ Analysis/Analysis/Ostap/python/Ostap/TFileDeco.py
@@ -20,7 +20,7 @@ __author__  = "Vanya BELYAEV Ivan.Belyaev@itep.ru"
 __date__    = "2011-06-07"
 __all__     = () 
 # =============================================================================
-import ROOT, cppyy              ## attention here!!
+import ROOT, os , cppyy              ## attention here!!
 cpp = cppyy.makeNamespace('')
 VE  = cpp.Gaudi.Math.ValueWithError 
 # 
@@ -33,6 +33,23 @@ else                       : logger = getLogger( __name__ )
 # =============================================================================
 logger.debug ( 'Some useful decorations for TFile objects')
 # ==============================================================================
+## @class ROOTCURRDIR
+#  context manager to preserve current directory (rather confusing stuff in ROOT) 
+#  @author Vanya BELYAEV Ivan.Belyaev@iep.ru
+#  @date 2015-07-30
+class ROOTCURRDIR(object) :
+    """
+    Context manager to preserve current directory (rather confusing stuff in ROOT) 
+    """
+    def __enter__ ( self ) :
+        ## save global ROOT directory 
+        self._dir = ROOT.gDirectory
+        
+    def __exit__  ( self , *_ ) :
+        ## make global directory current again 
+        if self._dir : self._dir.cd()
+
+# =======================================================================================
 ## print ROOT file (actually a combination of ls&Print)
 #  @code
 #
@@ -50,24 +67,51 @@ def _rf_print_ ( rfile , opts = '') :
     >>> print f
     """
     #
-    rfile.ls    ( opts  )
-    #
-    rfile.Print ( 'v'   )
-    #
-    return rfile.GetName()
+    with ROOTCURRDIR() :
+        #
+        rfile.cd ()
+        #
+        rfile.ls    ( opts  )
+        #
+        rfile.Print ( 'v'   )
+        #
+        return rfile.GetName()
     
 ## write the object to ROOT-file 
-def _rf_setitem_ ( rfile , name , tobj ) :
+def _rd_setitem_ ( rfile , name , tobj ) :
     """
     Write the object to ROOT-file
-
     >>> f['myhisto'] = h1
-    
     """
-    return rfile.WriteTObject( tobj , name , 'WriteDelete' )
+    with ROOTCURRDIR() :
+        #
+        rfile.cd()
+        return rfile.WriteTObject( tobj , name , 'WriteDelete' )
 
+## get the object from ROOT-file 
+def _rd_getitem_ ( rfile , name ) :
+    """
+    Get the object form ROOT-file
+    >>> obj = f['A/tup']
+    """
+    if not rfile : return None
+    with ROOTCURRDIR() :
+        ##
+        rfile.cd()
+        ## 
+        dn , s , ff = name.partition('/')
+        ## 
+        if not s or not f : return rfile.Get( name )
+        #
+        ## try to get directory:
+        ldir = rdir.GetDirectory ( dn )
+        if not ldir :
+            print 'MISSING DIRECTORY %s ' % dn 
+            return None
+        # 
+        return _rd_getitem_ ( ldir , fn )
 
-## use ROOT-file with context-manager 
+## use ROOT-file with context-manager
 def _rf_enter_ ( self      ) :
     """
     Use ROOT-file with the context manager
@@ -86,12 +130,37 @@ def _rf_exit_  ( self , *_ ) :
     try :
         self.Close()
     except: pass
-    
+
+# =============================================================================a
+## get all keys from the directory 
+def _rd_keys_ ( rdir , recursive = True ) :
+    """
+    Get all keys from the directory
+    >>> keys = rfile.keys() 
+    """
+    if not rdir : return []
+    ## get the actual list of keys 
+    _lst = rdir.GetListOfKeys()
+    _res = []
+    for i in _lst :
+        inam = i.GetName() 
+        _res.append ( inam )    
+        if i.IsFolder() :
+            idir  = i.ReadObj ()
+            ikeys = _rd_keys_ ( idir , recursive )
+            for k in ikeys : _res.append ( inam + '/' + k )
+
+    return _res  
+
+
+ROOT.TDirectory.keys        = _rd_keys_
+ROOT.TDirectory.__getitem__ = _rd_getitem_ 
+ROOT.TDirectory.__setitem__ = _rd_setitem_
+
 ROOT.TFile.__repr__    = _rf_print_
 ROOT.TFile.  name      = ROOT.TFile.GetName
-ROOT.TFile.__getitem__ = ROOT.TFile.Get 
-ROOT.TFile.__getattr__ = ROOT.TFile.Get 
-ROOT.TFile.__setitem__ = _rf_setitem_ 
+
+ROOT.TFile.close       = ROOT.TFile.Close 
 
 if hasattr ( ROOT.TFile , '__enter__' ) and hasattr ( ROOT.TFile , '__exit__' ) : pass
 else :
