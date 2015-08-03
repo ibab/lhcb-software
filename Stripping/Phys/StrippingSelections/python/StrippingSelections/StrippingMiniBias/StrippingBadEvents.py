00cde57d4fc532bac2a58697d6e3c8e717ca390a
Ricardo Vazquez Gomez <ricardo.vazquez.gomez@cern.ch>
2015-08-03 15:03:41 +0000

    added MiniBias modules
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@193003 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6ed1b78757afe5703252d8422108f1de5aa1be96

---
+++ Stripping/Phys/StrippingSelections/python/StrippingSelections/StrippingMiniBias/StrippingBadEvents.py
@@ -0,0 +1,52 @@
+__author__ = 'Andrea Contu'
+__date__ = '21/11/2014'
+__version__ = '$Revision: 1 $'
+
+"""
+Stripping selections for BadEvents.
+"""
+
+# Begin StrippingBadEvents.py
+
+default_config = {
+    'NAME'        : 'BadEvents',
+    'BUILDERTYPE' : 'BadEventsConf',
+    'CONFIG'      : { "GEC"       : "((TrSOURCE('/Event/Rec/Track/Best', TrLONG)"\
+                                    " >> (sum(TrPT,TrP<1 * TeV))>1 * TeV))",
+                      "Preambulo" : ["from LoKiTracks.decorators import *" ,
+                                     "from LoKiCore.functions    import * ",
+                                     "from GaudiKernel.SystemOfUnits import *"],
+                      "prescale": 1.0,
+                      "postscale": 1.0 },
+    'WGs'         : [ 'ALL' ],
+    'STREAMS'     : [ 'MiniBias' ]
+}
+
+__all__ = ('BadEventsConf' )
+
+from StrippingConf.StrippingLine import StrippingLine
+from StrippingUtils.Utils import LineBuilder
+from PhysSelPython.Wrappers import Selection
+
+class BadEventsConf(LineBuilder) :
+    """
+    Builder for BadEvent lines
+    """
+
+    __configuration_keys__ = ( 
+        "GEC",
+        "prescale" ,
+        "postscale",
+       )
+
+    def __init__(self, name, config):
+      LineBuilder.__init__(self, name, config)
+      
+      _filter={'Code': config['GEC'], 'Preambulo' : config['Preambulo'] }
+                                      
+      line = StrippingLine(name
+                           ,FILTER = _filter
+                           ,prescale = config["prescale"]
+                           ,postscale = config["postscale"]
+                           , RequiredRawEvents = ["Trigger", "Muon", "Calo", "Rich", "Velo", "Tracker"])
+      self.registerLine( line )
