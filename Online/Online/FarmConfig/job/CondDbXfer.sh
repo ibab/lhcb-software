233ac4b5f14a5b66afbd645141a73bf51ba47efb
Markus Frank <frankb@cern.ch>
2015-08-05 13:41:08 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@193185 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 7dc1d71b1dea1a198a91696d1eabcaa3e715769f

--- Online/Online/FarmConfig/job/CondDbXfer.sh
+++ Online/Online/FarmConfig/job/CondDbXfer.sh
@@ -4,9 +4,8 @@ echo $2
 export HOME=/home/online
 export USER=online # these dumb offline scripts....
 source /cvmfs/lhcb.cern.ch/lib/LbLogin.sh
-source /group/online/dataflow/cmtuser/AlignmentOnlineDev_v10r3/setup.x86_64-slc6-gcc48-dbg.vars
-export PATH=/group/online/dataflow/cmtuser/LHCbDev_v38r5/InstallArea/x86_64-slc6-gcc48-dbg/bin:/group/online/dataflow/cmtuser/LHCbDev_v38r5/InstallArea/x86_64-slc6-gcc48-dbg/scripts:$PATH
-export PYTHONPATH=$1/..:$1:/group/online/hlt/conditions/RunChangeHandler:/group/online/dataflow/cmtuser/LHCbDev_v38r5/InstallArea/x86_64-slc6-gcc48-dbg/python:/group/online/dataflow/cmtuser/LHCbDev_v38r5/InstallArea/x86_64-slc6-gcc48-dbg/python/lib-dynload:/group/online/dataflow/cmtuser/LHCbDev_v38r5/InstallArea/x86_64-slc6-gcc48-dbg/python.zip:$PYTHONPATH
+source /group/online/dataflow/cmtuser/AlignmentOnlineDev_v10r5/setup.x86_64-slc6-gcc48-dbg.vars
+export PYTHONPATH=$1/..:$1:/group/online/hlt/conditions/RunChangeHandler:$PYTHONPATH
 unset GAUDIAPPNAME
 source /sw/oracle/set_oraenv.sh
 export CORAL_DBLOOKUP_PATH=/group/online/conddbserver/
