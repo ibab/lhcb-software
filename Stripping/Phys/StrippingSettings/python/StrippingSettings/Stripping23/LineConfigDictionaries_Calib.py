2443dfbdb39f24b777662c88ed2bd62b64828dd2
Ricardo Vazquez Gomez <ricardo.vazquez.gomez@cern.ch>
2015-08-11 20:29:16 +0000

    remove temporarely the TrackEffDownMuon lines until the memory problem is solved
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@193537 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3dcd1295cb9dae1f88469e60e759d961531e3dd1

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping23/LineConfigDictionaries_Calib.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping23/LineConfigDictionaries_Calib.py
@@ -7,51 +7,50 @@
 
 from GaudiKernel.SystemOfUnits import *
 
-TrackEffDownMuon = {
-    'NAME'        : 'TrackEffDownMuon',
-    'WGs'         : ['ALL'],
-    'BUILDERTYPE' : 'StrippingTrackEffDownMuonConf',
-    'STREAMS'           : [ 'Calibration' ],
-    'CONFIG'      : { 
-				'MuMom':		2000.	# MeV
-			,	'MuTMom':		200.	# MeV
-			,	'ZMuMom':		0.	# MeV
-			,	'ZMuTMom':		20000.	# MeV
-			,	'UpsilonMuMom':		0.	# MeV
-			,	'UpsilonMuTMom':	500.	# MeV
-			,	'ZMuMinEta':		2.	# adimensional
-			,	'ZMuMaxEta':		4.5	# adimensional
-			,	'TrChi2':		10.	# MeV
-			,	'MassPreComb':		2000.	# MeV
-			,	'MassPostComb':		200.	# MeV
-			,	'ZMassPreComb':		100000.	# MeV
-			,	'ZMassPostComb':	1500.	# MeV
-			,	'UpsilonMassPreComb':	100000.	# MeV
-			,	'UpsilonMassPostComb':	00.	# MeV
-			,	'Doca':			5.	# mm
-			,	'VertChi2':		25.	# adimensional
-			,	'SeedingMinP':		1500.	# MeV
-            ,   'DataType':             '2015'  # for ConfiguredMuonIDs configuration
-			,	'NominalLinePrescale':  0.2 # proposal: 0.2 to stay below 0.15% retention rate 
-			,	'NominalLinePostscale': 1.
-			,	'ValidationLinePrescale':0.0015 #0.5 in stripping15: 0.1 gives 1.42% retention rate , ValidationLine further prescaled
-			,	'ValidationLinePostscale': 1.
-			,	'ZLinePrescale':  1. # proposal: 0.2 to stay below 0.15% retention rate 
-			,	'ZLinePostscale':  1.  
-			,	'UpsilonLinePrescale':  1. # proposal: 0.2 to stay below 0.15% retention rate 
-			,	'UpsilonLinePostscale':  1. 
-            ,   'JpsiHlt1Filter' : 'Hlt1.*Decision'
-            ,   'JpsiHlt2Filter' : 'Hlt2.*Decision'
-			,	'HLT1TisTosSpecs': { "Hlt1TrackMuonDecision%TOS" : 0, "Hlt1SingleMuonNoIPL0Decision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
-			,	'ZHLT1TisTosSpecs': { "Hlt1SingleMuonHighPTDecision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
-			,	'UpsilonHLT1TisTosSpecs': { "Hlt1SingleMuonHighPTDecision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
-			,	'HLT1PassOnAll': True
-			,	'HLT2TisTosSpecs': { "Hlt2SingleMuon.*Decision%TOS" : 0, "Hlt2TrackEffDiMuonDownstream.*Decision%TUS" : 0 } #reg. expression allowed
-			,	'ZHLT2TisTosSpecs': { "Hlt2SingleMuonHighPTDecision%TOS" : 0 } #reg. expression allowed
-			,	'UpsilonHLT2TisTosSpecs': { "Hlt2SingleMuonLowPTDecision%TOS" : 0} #reg. expression allowed
-			,	'HLT2PassOnAll': False
-                    }
-    }
+#TrackEffDownMuon = {
+#    'WGs'         : ['ALL'],
+#    'BUILDERTYPE' : 'StrippingTrackEffDownMuonConf',
+#    'STREAMS'           : [ 'Calibration' ],
+#    'CONFIG'      : { 
+#				'MuMom':		2000.	# MeV
+#			,	'MuTMom':		200.	# MeV
+#			,	'ZMuMom':		0.	# MeV
+#			,	'ZMuTMom':		20000.	# MeV
+#			,	'UpsilonMuMom':		0.	# MeV
+#			,	'UpsilonMuTMom':	500.	# MeV
+#			,	'ZMuMinEta':		2.	# adimensional
+#			,	'ZMuMaxEta':		4.5	# adimensional
+#			,	'TrChi2':		10.	# MeV
+#			,	'MassPreComb':		2000.	# MeV
+#			,	'MassPostComb':		200.	# MeV
+#			,	'ZMassPreComb':		100000.	# MeV
+#			,	'ZMassPostComb':	1500.	# MeV
+#			,	'UpsilonMassPreComb':	100000.	# MeV
+#			,	'UpsilonMassPostComb':	00.	# MeV
+#			,	'Doca':			5.	# mm
+#			,	'VertChi2':		25.	# adimensional
+#			,	'SeedingMinP':		1500.	# MeV
+#            ,   'DataType':             '2015'  # for ConfiguredMuonIDs configuration
+#			,	'NominalLinePrescale':  0.2 # proposal: 0.2 to stay below 0.15% retention rate 
+#			,	'NominalLinePostscale': 1.
+#			,	'ValidationLinePrescale':0.0015 #0.5 in stripping15: 0.1 gives 1.42% retention rate , ValidationLine further prescaled
+#			,	'ValidationLinePostscale': 1.
+#			,	'ZLinePrescale':  1. # proposal: 0.2 to stay below 0.15% retention rate 
+#			,	'ZLinePostscale':  1.  
+#			,	'UpsilonLinePrescale':  1. # proposal: 0.2 to stay below 0.15% retention rate 
+#			,	'UpsilonLinePostscale':  1. 
+#            ,   'JpsiHlt1Filter' : 'Hlt1.*Decision'
+#            ,   'JpsiHlt2Filter' : 'Hlt2.*Decision'
+#			,	'HLT1TisTosSpecs': { "Hlt1TrackMuonDecision%TOS" : 0, "Hlt1SingleMuonNoIPL0Decision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
+#			,	'ZHLT1TisTosSpecs': { "Hlt1SingleMuonHighPTDecision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
+#			,	'UpsilonHLT1TisTosSpecs': { "Hlt1SingleMuonHighPTDecision%TOS" : 0} #no reg. expression allowed(see selHlt1Jpsi )
+#			,	'HLT1PassOnAll': True
+#			,	'HLT2TisTosSpecs': { "Hlt2SingleMuon.*Decision%TOS" : 0, "Hlt2TrackEffDiMuonDownstream.*Decision%TUS" : 0 } #reg. expression allowed
+#			,	'ZHLT2TisTosSpecs': { "Hlt2SingleMuonHighPTDecision%TOS" : 0 } #reg. expression allowed
+#			,	'UpsilonHLT2TisTosSpecs': { "Hlt2SingleMuonLowPTDecision%TOS" : 0} #reg. expression allowed
+#			,	'HLT2PassOnAll': False
+#                    }
+#    }
 
 TrackEffMuonTT = {
     'WGs'         : ['ALL'],
@@ -849,7 +848,6 @@ SigmacForPID = {
 }
 
 TrackEffD0ToK3Pi = {
-        'NAME'        : 'TrackEffD0ToK3Pi',
         'WGs'         : ['ALL'],
         'BUILDERTYPE' : 'TrackEffD0ToK3PiAllLinesConf',
         'STREAMS':["Calibration"],
