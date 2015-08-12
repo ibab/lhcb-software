9ef40a27a0acf0f6d8080724b0258d2b94d7b24b
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-08-12 11:22:31 +0000

    remove #include of obsolete Gaudi headers
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@193584 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c93f32f3820fbe62e721b7acd096f18e5657f2cd

--- Phys/Phys/TopologicalTools/src/DoubleTopoTool.cpp
+++ Phys/Phys/TopologicalTools/src/DoubleTopoTool.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include "GaudiKernel/ToolFactory.h"
 #include "GaudiAlg/GaudiTool.h"
 #include "Kernel/ITriggerTisTos.h"
 #include "Kernel/IAccept.h"
