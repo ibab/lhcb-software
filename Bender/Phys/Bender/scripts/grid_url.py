668e899c43d25acf7af58153520e825c1de6b93d
Vanya Belyaev <ivan.belyaev@cern.ch>
2015-08-16 15:44:04 +0000

    resurrect grid access; prepare v26r2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@193782 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: eacf8136200ea17d7d97d5a2b3d4ad7899332147

--- Bender/Phys/Bender/scripts/grid_url.py
+++ Bender/Phys/Bender/scripts/grid_url.py
@@ -60,10 +60,12 @@ def _accessURLs ( silent  = True ) :
     Get the dictionary { 'LFN' : 'access-URL' }
     Actually it is a bit modified version of dirac-dms-lfn-accessURL script 
     """
+
     import DIRAC
     from   LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript, printDMResult
     from   DIRAC.Core.Base import Script
-    
+
+
     dmScript = DMScript()
     dmScript.registerFileSwitches()
     dmScript.registerSiteSwitches()
@@ -73,6 +75,8 @@ def _accessURLs ( silent  = True ) :
                                          'Arguments:',
                                          '  LFN:      Logical File Name or file containing LFNs',
                                          '  SE:       Valid DIRAC SE' ] ) )
+
+
     Script.parseCommandLine( ignoreErrors = True )
     
     seList = dmScript.getOption('SEs',[])
@@ -87,7 +91,7 @@ def _accessURLs ( silent  = True ) :
                 print 'Site %s not known' % site
                 Script.showHelp()
             seList.extend( res['Value']['SE'].replace( ' ', '' ).split( ',' ) )
-                
+
     if not seList:
         if not args :
             if not silent :
@@ -102,33 +106,112 @@ def _accessURLs ( silent  = True ) :
     # This should be improved, with disk SEs first...
     seList.sort()
 
+
     lfns  = dmScript.getOption('LFNs',[])
     lfns += args
-    
-    from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
-    from DIRAC import gLogger
-  
-    if len(seList) > 1 and not silent :
-        gLogger.info( "Using the following list of SEs: %s" %str(seList))
-    
-    rm = ReplicaManager()
-    gLogger.setLevel("FATAL")
-  
-    #
-    ## return result in a form of list
-    # 
-    result = {}
 
-    for lfn in lfns :
-        #
-        result[lfn] = []
-        # 
-        for se in seList : 
-            res = rm.getReplicaAccessUrl( lfn , se )
-            if not res ['OK']                       : continue 
-            result[ lfn ].append ( res['Value']['Successful'][ lfn ] )
-            
-    return result
+    lfnList = lfns 
+
+    from DIRAC.DataManagementSystem.Client.DataManager          import DataManager
+    
+    dm       = DataManager()
+    result   = dm.getReplicas ( lfnList )
+    replicas = result.get('Value',{}).get('Successful',{}) 
+
+    from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient   import BookkeepingClient
+    bk           = BookkeepingClient()
+    results      = {'OK':True, 'Value':{'Successful':{}, 'Failed':{}}}
+    notFoundLfns = set( lfnList )
+
+    bkRes = bk.getFileTypeVersion( lfnList )
+
+    mdfFiles = set( [lfn for lfn, fileType in bkRes.get( 'Value', {} ).items() if fileType == 'MDF'] )
+    
+    from DIRAC.Resources.Storage.StorageElement                 import StorageElement
+    
+    protocol = None 
+    for se in seList:
+        lfns = [lfn for lfn in lfnList if se in replicas.get( lfn, [] )]
+        if lfns:
+            res = StorageElement( se ).getURL( lfns, protocol = protocol )
+            success = res.get( 'Value', {} ).get( 'Successful' )
+            failed = res.get( 'Value', {} ).get( 'Failed' )
+            if res['OK']:
+                if success:
+                    for lfn in set( success ) & mdfFiles:
+                        success[lfn] = 'mdf:' + success[lfn]
+                    notFoundLfns -= set( success )
+                    results['Value']['Successful'].setdefault( se, {} ).update( success )
+                if failed:
+                    results['Value']['Failed'].setdefault( se, {} ).update( failed )
+            else:
+                results['Value']['Failed'].setdefault( se, {} ).update( dict.fromkeys( lfns, res['Message'] ) )
+                
+    for se, failed in results['Value']['Failed'].items():
+        for lfn in list( failed ):
+            if lfn not in notFoundLfns:
+                failed.pop( lfn )
+            else:
+                notFoundLfns.remove( lfn )
+    if notFoundLfns:
+        results['Value']['Failed'] = dict.fromkeys( sorted( notFoundLfns ), 'File not found in required seList' )
+
+    ##print results
+    results = results.get('Value',{}).get('Successful',{})
+    res = {}
+    for se in results :
+        access = results[se]
+        for lfn in access :
+            if res.has_key( lfn ) : res[lfn].append( access[lfn] )
+            else                  : res[lfn] = [ access[lfn] ] 
+                            
+    return res
+
+## def getAccessURL( lfnList, seList, protocol = None ):
+##   dm = DataManager()
+##   res = dm.getReplicas( lfnList )
+##   replicas = res.get( 'Value', {} ).get( 'Successful', {} )
+##   if not seList:
+##     seList = sorted( set( [se for lfn in lfnList for se in replicas.get( lfn, {} )] ) )
+##     if len( seList ) > 1:
+##       gLogger.always( "Using the following list of SEs: %s" % str( seList ) )
+##   bk = BookkeepingClient()
+##   notFoundLfns = set( lfnList )
+##   results = {'OK':True, 'Value':{'Successful':{}, 'Failed':{}}}
+##   savedLevel = gLogger.getLevel()
+##   gLogger.setLevel( 'FATAL' )
+##   # Check if files are MDF
+##   bkRes = bk.getFileTypeVersion( lfnList )
+##   mdfFiles = set( [lfn for lfn, fileType in bkRes.get( 'Value', {} ).items() if fileType == 'MDF'] )
+##   for se in seList:
+##     lfns = [lfn for lfn in lfnList if se in replicas.get( lfn, [] )]
+##     if lfns:
+##       res = StorageElement( se ).getURL( lfns, protocol = protocol )
+##       success = res.get( 'Value', {} ).get( 'Successful' )
+##       failed = res.get( 'Value', {} ).get( 'Failed' )
+##       if res['OK']:
+##         if success:
+##           for lfn in set( success ) & mdfFiles:
+##             success[lfn] = 'mdf:' + success[lfn]
+##           notFoundLfns -= set( success )
+##           results['Value']['Successful'].setdefault( se, {} ).update( success )
+##         if failed:
+##           results['Value']['Failed'].setdefault( se, {} ).update( failed )
+##       else:
+##         results['Value']['Failed'].setdefault( se, {} ).update( dict.fromkeys( lfns, res['Message'] ) )
+##   gLogger.setLevel( savedLevel )
+
+##   for se, failed in results['Value']['Failed'].items():
+##     for lfn in list( failed ):
+##       if lfn not in notFoundLfns:
+##         failed.pop( lfn )
+##       else:
+##         notFoundLfns.remove( lfn )
+##   if notFoundLfns:
+##     results['Value']['Failed'] = dict.fromkeys( sorted( notFoundLfns ), 'File not found in required seList' )
+
+##   printDMResult( results, empty = "File not at SE", script = "dirac-dms-lfn-accessURL" )
+
 
 # =============================================================================
 ## get the dictionary { 'LFN' : 'access-URL' }
@@ -153,11 +236,15 @@ def accessURLs ( silent = True ) :
             
             sys.stderr = None ## open ( '/dev/null' )
             sys.stdout = None ## open ( '/dev/null' )
-            
+
         result = _accessURLs ( silent ) ## RETURN 
     
     except:
 
+        if not silent:
+            import traceback 
+            print  traceback.format_exc() 
+            
         result = {}           ## RETURN 
         
     finally:
