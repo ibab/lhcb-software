b627cc626add04113f50bf7e0b4422ee8fc9252f
Stefano Perazzini <stefano.perazzini@cern.ch>
2015-08-17 20:05:04 +0000

    Propagated a change to the config dictionary of StrippingB2CC/StrippingBd2JpsieeKS.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@193831 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 22a995989f8b7d1036a1fa90ca8e5d161967f0d6

--- Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping23/LineConfigDictionaries_B2CC.py
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping23/LineConfigDictionaries_B2CC.py
@@ -175,23 +175,35 @@ Bs2EtacPhiBDT = {
 ## Author: Ramon Niet
 ######################################################################
 
-Bd2JpsieeKS = {
-    'BUILDERTYPE' : 'Bd2JpsieeKSConf',
-    'CONFIG'      : { 'BPVLTIME'              :     0.2   # ps
-                    , 'ElectronPT'            :   500.    # MeV
-                    , 'ElectronPID'           :     0.    # adimensional
-                    , 'ElectronTrackCHI2pDOF' :     5.    # adimensional
-                    , 'TRCHI2DOF'             :     5.    # adimensional
-                    , 'JpsiVertexCHI2pDOF'    :    15.    # adimensional
-                    , 'JpsiMassMin'           :  2300.    # MeV
-                    , 'JpsiMassMax'           :  3300.    # MeV
-                    , 'BdVertexCHI2pDOF'      :     7.    # adimensional
-                    , 'BdMassMin'             :  4400.    # MeV
-                    , 'BdMassMax'             :  6000.    # MeV
-                    , 'Prescale'              :     0.1   # adimensional
-                    , 'Bd2JpsieeKstarPrescale':     0.1   # adimensional
-                    , 'Bu2JpsieeKPrescale'    :     0.1   # adimensional
-                    },
-    'STREAMS'     : [ 'Leptonic' ],
-    'WGs'         : [ 'B2CC' ]
+Bd2JpsieeKS ={
+    'BUILDERTYPE'       : 'Bd2JpsieeKSConf',
+    'CONFIG'    : {
+          'BPVLTIME'              :     0.2   # ps
+        , 'ElectronPT'            :   500.    # MeV
+        , 'ElectronPID'           :     0.    # adimensional
+        , 'ElectronTrackCHI2pDOF' :     5.    # adimensional
+        , 'KaonPID'               :     0.    # adimensional
+        , 'PionForKstarPT'        :   600.    # MeV
+        , 'TRCHI2DOF'             :     3.    # adimensional
+        , 'JpsiVertexCHI2pDOF'    :    15.    # adimensional
+        , 'JpsiMassMin'           :  2300.    # MeV
+        , 'JpsiMassMax'           :  3300.    # MeV
+        , 'KstarMassMin'          :   826.    # MeV
+        , 'KstarMassMax'          :   966.    # MeV
+        , 'KstarPT'               :  1500.    # MeV
+        , 'KstarVCHI2'            :    20.    # adimensional
+        , 'KplusPT'               :   500.    # MeV
+        , 'KSVCHI2'               :    20.    # adimensional
+        , 'KSBPVDLS'              :     5.    # adimensional
+        , 'BdVertexCHI2pDOF'      :     7.    # adimensional
+        , 'BdFromKstarVCHI2pDOF'  :    20.    # adimensional
+        , 'BdMassMin'             :  4400.    # MeV
+        , 'BdMassMax'             :  6000.    # MeV
+        , 'Prescale'              :     0.1   # adamenssional
+        , 'Bd2JpsieeKstarPrescale':     0.1   # adimensional
+        , 'Bu2JpsieeKPrescale'    :     0.1   # adimensional
+        },
+    'STREAMS' : [ 'Leptonic' ],
+    'WGs'    : [ 'B2CC' ]
     }
+
