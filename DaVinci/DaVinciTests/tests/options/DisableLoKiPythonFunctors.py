e71dd0d09f53410de0c608907d1d73616fa8035c
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-08-21 18:04:32 +0000

    Update functor disable options
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@194050 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1b0aeec825c76a5181f6e013d4090f79b124f89a

--- DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
+++ DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
@@ -1,3 +1,18 @@
-import os
 
-os.environ["LOKI_DISABLE_PYTHON"] = "1"
+#import os
+#os.environ["LOKI_DISABLE_PYTHON"] = "1"
+
+def post_action_disable_python( ) :
+    for factory in ('Tool', 'CoreFactory', 'TrackFunctorFactory',
+                    'HltFactory', 'Hlt1HltFactory', 'Hlt2HltFactory'):
+        factory = 'LoKi__Hybrid__%s' % factory
+        try:
+            factory = getattr(Configurables, factory)
+            factory( UsePython = False )
+        except AttributeError:
+            # ignore unknown factories
+            pass
+
+
+from Gaudi.Configuration import appendPostConfigAction
+appendPostConfigAction( post_action_disable_python )
