1909a6aec3ca8dce194a4b281afd7765601ce137
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-08-22 09:08:31 +0000

    Fix functor cache options
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@194065 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b70336cc07b8a7b8fb9bfe9f46f79175922b81d2

--- DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
+++ DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
@@ -1,18 +1,7 @@
 
-#import os
-#os.environ["LOKI_DISABLE_PYTHON"] = "1"
-
 def post_action_disable_python( ) :
-    for factory in ('Tool', 'CoreFactory', 'TrackFunctorFactory',
-                    'HltFactory', 'Hlt1HltFactory', 'Hlt2HltFactory'):
-        factory = 'LoKi__Hybrid__%s' % factory
-        try:
-            factory = getattr(Configurables, factory)
-            factory( UsePython = False )
-        except AttributeError:
-            # ignore unknown factories
-            pass
-
+    import os
+    os.environ["LOKI_DISABLE_PYTHON"] = "1"
 
 from Gaudi.Configuration import appendPostConfigAction
 appendPostConfigAction( post_action_disable_python )
