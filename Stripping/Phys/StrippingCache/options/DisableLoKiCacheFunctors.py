a79aa609706143fb363b0564b53099d43120b3c2
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-08-26 16:58:04 +0000

    - Update the CMake builds to make each cache independent, so does not skip    factors alrady cached. Increases a bit the overall size of the cached functor    libraries, but allows the MD5 name hack to be removed, and also allows the    builds to proceed in parrallel.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@194294 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c249e66e692ea1beba84d21de1faaa613501507b

--- Stripping/Phys/StrippingCache/options/DisableLoKiCacheFunctors.py
+++ Stripping/Phys/StrippingCache/options/DisableLoKiCacheFunctors.py
@@ -1,7 +1,3 @@
 
-def post_action_disable_cache( ) :
-    import os
-    os.environ["LOKI_DISABLE_CACHE"] = "1"
-
-from Gaudi.Configuration import appendPostConfigAction
-appendPostConfigAction( post_action_disable_cache )
+from Configurables import ApplicationMgr
+ApplicationMgr().Environment['LOKI_DISABLE_CACHE'] = '1'
