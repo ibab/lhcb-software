385cb510d3b064fedc6f689c0175c5776553896f
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-08-26 13:51:06 +0000

    apply as a post config action
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@194278 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 34b9b842b86f3d7f0d625297e7c07d9574fd57ca

--- Stripping/Phys/StrippingCache/options/DisableLoKiCacheFunctors.py
+++ Stripping/Phys/StrippingCache/options/DisableLoKiCacheFunctors.py
@@ -1,3 +1,7 @@
-import os
 
-os.environ["LOKI_DISABLE_CACHE"] = "1"
+def post_action_disable_cache( ) :
+    import os
+    os.environ["LOKI_DISABLE_CACHE"] = "1"
+
+from Gaudi.Configuration import appendPostConfigAction
+appendPostConfigAction( post_action_disable_cache )
