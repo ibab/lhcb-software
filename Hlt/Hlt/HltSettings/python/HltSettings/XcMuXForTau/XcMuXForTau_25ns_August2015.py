61d1335b511af1131dc7d33c395359c14b02ecea
Gregory Max Ciezarek <gregory.max.ciezarek@cern.ch>
2015-08-30 22:02:57 +0000

    Updated XcForTau settings work with restructured lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@194401 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: af3581ec3a62798fb084f1a6d6623e3f4bf0b070

--- Hlt/Hlt/HltSettings/python/HltSettings/XcMuXForTau/XcMuXForTau_25ns_August2015.py
+++ Hlt/Hlt/HltSettings/python/HltSettings/XcMuXForTau/XcMuXForTau_25ns_August2015.py
@@ -5,9 +5,9 @@ class XcMuXForTau_25ns_August2015 :
     Threshold settings for Hlt2 XcMuXForTau lines: XcMuXForTau_25ns_August2015
     
     WARNING :: DO NOT EDIT WITHOUT PERMISSION OF THE AUTHORS
-    
+    EDITED BY G.Ciezarek on 2015-08-30 to run with updated Hlt2Line 
     @author P. Owen < patrickowen22@gmail.com > 
-    @date 2015-07-22
+    @date 2015-08-30
     """
     
     __all__ = ( 'ActiveHlt2Lines' )
@@ -17,7 +17,7 @@ class XcMuXForTau_25ns_August2015 :
         """
         Returns a list of active lines
         """
-
+        """
         lines = [
             'B2D0KPiMu',
             'B2D0K3PiMu',
@@ -30,7 +30,11 @@ class XcMuXForTau_25ns_August2015 :
             'B2DsKKPiFakeMu',
             'B2LcPKPiFakeMu'
             ]
-        
+        """
+        lines = [
+            'B2XcMu',
+            'B2XcFakeMu'
+            ]
         return ['Hlt2XcMuXForTau' + l for l in lines]
 
 
@@ -44,42 +48,46 @@ class XcMuXForTau_25ns_August2015 :
             {
                 XcMuXForTauLines: { 
                  'Prescale' : {},
-                 'Xc' :   {
+                 'Prescale' : {'Hlt2XcMuXForTauB2XcFakeMu' :   0.1},
+                 'XcDaughter'  : {
+                             'DTrk_ALL_PT_MIN'           : 200.0 * MeV
+                            , 'DTrk_ALL_P_MIN'            : 5.0  * GeV
+                            , 'DTrk_ALL_MIPCHI2DV_MIN'    : 9.0
+                            , 'K_PIDK'    : 2 
+                            , 'Pi_PIDK'    : 4 
+                            , 'P_PIDp'    : 0 
+                 
+                            },
+                 'Xc' : {
                               'Pair_AMINDOCA_MAX'        : 0.10 * mm
                             , 'MIN_D_CHILDPT'          : 800.0 * MeV
                             , 'D0_BPVVDCHI2_MIN'         : 25.0       # neuter
                             , 'D0_BPVDIRA_MIN'           : 0.999    # neuter
                             , 'D0_VCHI2PDOF_MAX'         : 10.0       # neuter
                             , 'D0_PT_MIN'                : 2000.0 * MeV
-                            , 'DTrk_ALL_PT_MIN'           : 200.0 * MeV
-                            , 'DTrk_ALL_P_MIN'            : 5.0  * GeV
-                            , 'DTrk_ALL_MIPCHI2DV_MIN'    : 9.0
-                            , 'D_IPCHI2'    : 5
                             , 'D_SUMPT'    : 2500 * MeV
-                            , 'K_PIDK'    : 2 
-                            , 'Pi_PIDK'    : 4 
-                            , 'P_PIDp'    : 0 
                             , 'D_AM_MIN'              : 1830.0 * MeV
                             , 'D_AM_MAX'              : 1910.0 * MeV
                             , 'Ds_AM_MIN'              : 1920.0 * MeV
                             , 'Ds_AM_MAX'              : 2010.0 * MeV
                             , 'Lc_AM_MIN'              : 2230.0 * MeV
                             , 'Lc_AM_MAX'              : 2330.0 * MeV
-                          },
+                            },
                  'Muon' : {
                              'Mu_IPCHI2'    : 16
-                          },
-                 'B'    : {
+                            },
+                 'B' : {
                 
                              'XcMu_VTXCHI2'        : 15
                             , 'XcMu_DOCA_MAX'        : 0.50 * mm
                             , 'XcMu_DIRA'        : 0.999
                             , 'XcMu_FDCHI2'        : 50
-                          }
-                   }
+                            }
+		   
+                 }
                }
             )
-        
+            
         return d
 
 
