c81bfbe6d5f832499691175ac4b4acb1a81c5835
Dmitry Popov <dmitry.popov@cern.ch>
2015-08-31 09:25:29 +0000

    Removed deprecated upgrade tests and updated the actual ones
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@194406 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 7855be1fbd8da98b3db128892ef7ebe96df1e1d5

--- Gauss/Sim/Gauss/tests/options/testGauss-Upgrade-DBTags.py
+++ Gauss/Sim/Gauss/tests/options/testGauss-Upgrade-DBTags.py
@@ -1,3 +1,8 @@
+from Configurables import CondDB, Gauss
+CondDB().Upgrade = True
+Gauss().DataType = "Upgrade"
+
 from Configurables import LHCbApp
+LHCbApp().EvtMax    = 1
 LHCbApp().DDDBtag   = "dddb-20150729"
 LHCbApp().CondDBtag = "sim-20150716-vc-md100"
