0d3d845beb0b37294363cf8f88d3ea060b8d919d
Markus Frank <frankb@cern.ch>
2015-08-31 08:45:05 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@194404 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 8648e8bf49bf76a70c9480bc423fe1d15ec2fde2

---
+++ Online/Online/FarmConfig/job/Hlt2Publisher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# =========================================================================
+#
+#  Script to start the HLT2 histogram adder on the farm
+#
+#  Author   R. Aaij
+#  Version: 1.0
+#  Date:    16/07/2015
+#
+# =========================================================================
+#
+export TAN_PORT=YES;
+export TAN_NODE=${DATAINTERFACE};
+export CMTCONFIG=x86_64-slc6-gcc48-opt;
+read COMMAND <<EOF
+import GaudiKernel.ProcessJobOptions; from Gaudi.Configuration import importOptions; GaudiKernel.ProcessJobOptions.printing_level=3; from Hlt2Monitoring import Hlt2Publisher; Hlt2Publisher.configure()
+EOF
+
+exec -a ${UTGID} $gaudi_task -tasktype=LHCb::Class1Task -main=../options/Hlt2Main.opts -opt=command="${COMMAND}"
