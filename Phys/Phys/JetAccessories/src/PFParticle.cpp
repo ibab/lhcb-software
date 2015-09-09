cf3f221c452d3961ef74e6fed4954ce23d320005
Cedric Potterat <cedric.potterat@cern.ch>
2015-09-09 15:15:04 +0000

    adding cov matric for PF Velo Matrix
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@194752 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ecaa700b68f9bf03d08ed3565edecb0993f16768

--- Phys/Phys/JetAccessories/src/PFParticle.cpp
+++ Phys/Phys/JetAccessories/src/PFParticle.cpp
@@ -86,137 +86,112 @@ LHCb::PFParticle::PFParticle( const LHCb::ProtoParticle * pp ,
         int trackTag ,
         std::map< std::string , std::pair< const IProtoParticleFilter* , const LHCb::ParticleProperty * > > & protoMap ,  const LHCb::ParticleProperty* pprop) : Particle()
 {
+  // Start filling particle with ProtoParticle
+  this->setProto(pp);
 
-    // Depending on the track tag, fill the apropriate info
-    if ( trackTag == TurnTo0Momentum ){
-        // Start filling particle with ProtoParticle
-        this->setProto(pp);
-        // ParticleID
-        const int pID = protoMap["pi+"].second ->particleID().pid() * (int)(pp->charge());
-        this->setParticleID( LHCb::ParticleID( pID ) );
-        // Mass and error from the PID hypothesis
-        this->setMeasuredMass(protoMap["pi+"].second ->mass());
-        this->setMeasuredMassErr(0.);
-        //Set the momentum to 0.
-        const LHCb::State& state = pp->track()->firstState();
-        this->setReferencePoint( state.position() ) ;
-        // momentum
-        const Gaudi::XYZVector& mom = state.momentum();
+  // determine the PID
+  if( !pprop ) {
+    if( trackTag == Keep ) {
+      // TODO: move that to a "best PID function?"
+      bool pid_found(false);
+      //Create a map of PID and ProbNN pairs
+      typedef std::map< double , std::string > MapProbNNPID;
 
-        this -> setMomentum ( Gaudi::XYZTVector(mom.Unit().x()*1.e-6,
-                    mom.Unit().y()*1.e-6,
-                    mom.Unit().z()*1.e-6,
-                    1.e-6) );
-        // Set the particle type
-        this -> setPFType   (  Charged0Momentum  );
+      MapProbNNPID  probNNpid ;
+      probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNpi   ,-0.5) ] =  "pi+" ;
+      probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNp    ,-0.5) ] =  "p+"  ;
+      probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNk    ,-0.5) ] =  "K+"  ;
+      probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNe    ,-0.5) ] =  "e+"  ;
+      probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNmu   ,-0.5) ] =  "mu+" ;
+
+      // the map is by construction sorted by key value (lower first)
+      MapProbNNPID::iterator iNN = probNNpid.end();
+      iNN--;
+
+      //verbose()<<"Ordered hypothesis: type "<<(*iNN).first<<" "<<(*iNN).second<<endreq;
+      // Reverse loop, check if filter is satisfied
+      if (protoMap[(*iNN).second].first->isSatisfied( pp ))pid_found = true ;
+
+      while( !pid_found && iNN != probNNpid.begin() ){
+	iNN--;
+	//verbose()<<"Ordered hypothesis: type "<<(*iNN).first <<" with proba "<<(*iNN).second <<endreq;
+	if (protoMap[(*iNN).second].first->isSatisfied( pp ))pid_found = true ;
+      }
+      if( pid_found ){
+	pprop = protoMap[(*iNN).second].second ;
+	//verbose()<<"PID applied: "<<(*iNN).second <<endreq;
+      }
+      // if no PID information, apply pion hypothesis
+      if( !pid_found ){
+	pprop = protoMap["pi+"].second ;
+	//verbose()<<"PID applied: pi+ (by default)" <<endreq;
+      }
+    } else  {
+      pprop =  protoMap["pi+"].second ;
     }
-    else if( trackTag == Keep || trackTag == KeepInfMom ){
-        if ( trackTag == Keep ){
-            // TODO: move that to a "best PID function?"
+  }
 
-            if(pprop == NULL){
-                bool pid_found(false);
-                //Create a map of PID and ProbNN pairs
-                typedef std::map< double , std::string > MapProbNNPID;
+  // now set it, but take care of the charge
+  const int pID = pprop->particleID().pid() * (int)(pp->charge());
+  this->setParticleID( LHCb::ParticleID( pID ) );
+  this->setMeasuredMass(pprop->mass());
+  this->setMeasuredMassErr(0.);
 
-                MapProbNNPID  probNNpid ;
+  // set the PF type
+  if( trackTag == TurnTo0Momentum) {
+    this -> setPFType   (  Charged0Momentum  );
+  } else if ( trackTag == PFParticle::KeepInfMom ) {
+    //TODO what is the pid here???
+    // set the minus sigma momenta
+    this -> setPFType( PFParticle::ChargedInfMomentum );
+  } else {
+    if ( this->particleID().abspid() == 11  ) this -> setPFType   ( PFParticle::Electron );
+    else if ( this->particleID().abspid() == 13 ) this -> setPFType   (  PFParticle::Muon  );
+    else  this -> setPFType (  PFParticle::ChargedHadron  );
+  }
 
-                probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNpi   ,-0.5) ] =  "pi+" ;
-                probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNp    ,-0.5) ] =  "p+"  ;
-                probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNk    ,-0.5) ] =  "K+"  ;
-                probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNe    ,-0.5) ] =  "e+"  ;
-                probNNpid[ pp->info( LHCb::ProtoParticle::ProbNNmu   ,-0.5) ] =  "mu+" ;
+  // get track state
+  LHCb::State state = pp->track()->firstState() ;
+  this->setReferencePoint( state.position() ) ;
 
-                // the map is by construction sorted by key value (lower first)
-                MapProbNNPID::iterator iNN = probNNpid.end();
-                iNN--;
+  // fix the momentum for velo-only
+  if( trackTag == TurnTo0Momentum) {
+    const double fixedmomentum = 1.0*Gaudi::Units::MeV ;
+    state.setQOverP( pp->charge() / fixedmomentum ) ;
+  }
 
-                //verbose()<<"Ordered hypothesis: type "<<(*iNN).first<<" "<<(*iNN).second<<endmsg;
-                // Reverse loop, check if filter is satisfied
-                if (protoMap[(*iNN).second].first->isSatisfied( pp ))pid_found = true ;
+  // Update the energy from the mass and momenta a la state2Particle
+  // TODO: just use state2Particle!
+  const Gaudi::XYZVector& mom = state.momentum();
+  const double mass = this->measuredMass();
+  const double p = mom.R() ;
+  const double e = std::sqrt( p*p + mass*mass );
+  this->setMomentum( Gaudi::XYZTVector(mom.X(),mom.Y(),mom.Z(),e) ) ;
 
-                while( !pid_found && iNN != probNNpid.begin() ){
-                    iNN--;
-                    //verbose()<<"Ordered hypothesis: type "<<(*iNN).first <<" with proba "<<(*iNN).second <<endmsg;
-                    if (protoMap[(*iNN).second].first->isSatisfied( pp ))pid_found = true ;
-                }
-                if( pid_found ){
-                    pprop = protoMap[(*iNN).second].second ;
-                    //verbose()<<"PID applied: "<<(*iNN).second <<endmsg;
-                }
-                // if no PID information, apply pion hypothesis
-                if( !pid_found ){
-                    pprop = protoMap["pi+"].second ;
-                    //verbose()<<"PID applied: pi+ (by default)" <<endmsg;
-                }
-            }
-        }
-        // Start filling particle with orgininating ProtoParticle
-        this->setProto(pp);
-        //this -> setMomentum (Gaudi::XYZTVector(pp->track()->momentum().X(), pp->track()->momentum().Y(), pp->track()->momentum().Z(),0.)) ;
+  // get the jacobian for dp4/d(tx,ty,qop)
+  Gaudi::Matrix4x3 dP4dMom;
+  Gaudi::Math::JacobdP4dMom(state.stateVector().Sub<Gaudi::Vector3>(2),mass,dP4dMom);
 
-        // ParticleID
-        if ( pprop ) this->setParticleID( LHCb::ParticleID( pprop->particleID().pid() * (int)(pp->charge())));
-        else this->setParticleID( LHCb::ParticleID( protoMap["pi+"].second->particleID().pid() * (int)(pp->charge())));
-        // Mass and error from the PID hypothesis
-        this->setMeasuredMass(protoMap["pi+"].second ->mass());
-        if ( pprop ) this->setMeasuredMass(pprop->mass());
-        this->setMeasuredMassErr(0.);
+  // now fill the total jacobia, but only nonzero elements
+  ROOT::Math::SMatrix<double,7,5> Jacob ;
+  Jacob(0,0) = Jacob(1,1) = 1 ;
+  Jacob.Place_at(dP4dMom,3,2) ;
 
-        // Set the particle type depending on the PID and track tag
-        if ( trackTag == PFParticle::Keep ){
-            if ( this->particleID().abspid() == 11  ) this -> setPFType   ( PFParticle::Electron );
-            else if ( this->particleID().abspid() == 13 ) this -> setPFType   (  PFParticle::Muon  );
-            else  this -> setPFType (  PFParticle::ChargedHadron  );
-        }
-        else if ( trackTag == PFParticle::KeepInfMom ){
-            //TODO what is the pid here???
-            // set the minus sigma momenta
-            this -> setPFType( PFParticle::ChargedInfMomentum );
-            // TODO: set that as an option...
-            //this -> setMomentum ( Gaudi::XYZTVector(this->momentum().X() - this->momCovMatrix()(0,0),
-            //           this->momentum().Y() - this->momCovMatrix()(1,1),
-            //           this->momentum().Z() - this->momCovMatrix()(2,2),
-            //           0.));
-        }
-        //Update the energy from the mass and momenta a la sate2Particle
+  const Gaudi::SymMatrix7x7 cov = ROOT::Math::Similarity<double,7,5>( Jacob, state.covariance() );
 
-        const LHCb::State& state = pp->track()->firstState();
-        this->setReferencePoint( state.position() ) ;
-        // momentum
-        const Gaudi::XYZVector& mom = state.momentum();
-        const double mass = this->measuredMass();
-        const double p = mom.R() ;
-        const double e = std::sqrt( p*p + mass*mass );
-        this->setMomentum( Gaudi::XYZTVector(mom.X(),mom.Y(),mom.Z(),e) ) ;
+  // error on position
+  Gaudi::SymMatrix3x3& errPos = *(const_cast<Gaudi::SymMatrix3x3*>(&(this->posCovMatrix())));
+  errPos.Place_at(cov.Sub<Gaudi::SymMatrix3x3>( 0, 0 ), 0, 0 );
+  //m_posCovMatrix.Place_at(cov.Sub<Gaudi::SymMatrix3x3>( 0, 0 ), 0, 0 );
+  // error on momentum
+  Gaudi::SymMatrix4x4& errMom = *(const_cast<Gaudi::SymMatrix4x4*>(&(this->momCovMatrix())));
+  errMom.Place_at(cov.Sub<Gaudi::SymMatrix4x4>( 3, 3 ), 0, 0 );
+  // m_momCovMatrix.Place_at(cov.Sub<Gaudi::SymMatrix4x4>( 3, 3 ), 0, 0 );
+  // correlation
+  Gaudi::Matrix4x3& errPosMom = *(const_cast<Gaudi::Matrix4x3*>(&(this->posMomCovMatrix())));
+  errPosMom.Place_at(cov.Sub<Gaudi::Matrix4x3>( 3, 0 ), 0, 0 );
 
-        // get the jacobian for dp4/d(tx,ty,qop)
-        Gaudi::Matrix4x3 dP4dMom;
-        Gaudi::Math::JacobdP4dMom(state.stateVector().Sub<Gaudi::Vector3>(2),mass,dP4dMom);
-
-        // now fill the total jacobia, but only nonzero elements
-        ROOT::Math::SMatrix<double,7,5> Jacob ;
-        Jacob(0,0) = Jacob(1,1) = 1 ;
-        Jacob.Place_at(dP4dMom,3,2) ;
-
-        const Gaudi::SymMatrix7x7 cov =
-            ROOT::Math::Similarity<double,7,5>( Jacob, state.covariance() );
-
-        // error on position
-        Gaudi::SymMatrix3x3& errPos = *(const_cast<Gaudi::SymMatrix3x3*>(&(this->posCovMatrix())));
-        errPos.Place_at(cov.Sub<Gaudi::SymMatrix3x3>( 0, 0 ), 0, 0 );
-        //m_posCovMatrix.Place_at(cov.Sub<Gaudi::SymMatrix3x3>( 0, 0 ), 0, 0 );
-        // error on momentum
-        Gaudi::SymMatrix4x4& errMom = *(const_cast<Gaudi::SymMatrix4x4*>(&(this->momCovMatrix())));
-        errMom.Place_at(cov.Sub<Gaudi::SymMatrix4x4>( 3, 3 ), 0, 0 );
-        // m_momCovMatrix.Place_at(cov.Sub<Gaudi::SymMatrix4x4>( 3, 3 ), 0, 0 );
-        // correlation
-        Gaudi::Matrix4x3& errPosMom = *(const_cast<Gaudi::Matrix4x3*>(&(this->posMomCovMatrix())));
-        errPosMom.Place_at(cov.Sub<Gaudi::Matrix4x3>( 3, 0 ), 0, 0 );
-        //m_posMomCovMatrix.Place_at(cov.Sub<Gaudi::Matrix4x3>( 3, 0 ), 0, 0 );
-
-        //const double e = std::sqrt( this->p()*this->p() + this->measuredMass()*this->measuredMass() );
-        //this->setMomentum( Gaudi::LorentzVector(this->momentum().X(),this->momentum().Y(),this->momentum().Z(),e) ) ;
-        // TODO: Append the clusters and saturation values --> this might be done at the Neutral reco level??? because here we do not have the saturation info anymore
-    }
+  //const double e = std::sqrt( this->p()*this->p() + this->measuredMass()*this->measuredMass() );
+  //this->setMomentum( Gaudi::LorentzVector(this->momentum().X(),this->momentum().Y(),this->momentum().Z(),e) ) ;
+  // TODO: Append the clusters and saturation values --> this might be done at the Neutral reco level??? because here we do not have the saturation info anymore
 }
