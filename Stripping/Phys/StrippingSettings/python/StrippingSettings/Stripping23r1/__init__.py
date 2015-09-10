0847c797d27455e5c8d933346d2dcff3eaae2482
Andrea Contu <andrea.contu@cern.ch>
2015-09-10 09:19:14 +0000

    Create Stripping23r1
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@194806 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: ff0cd6eb751089ea5742bdac6e0b086efeed171d

---
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping23r1/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [ 'LineConfigDictionaries_B2CC',
+            'LineConfigDictionaries_B2OC',
+            'LineConfigDictionaries_BandQ',
+            'LineConfigDictionaries_Calib',
+            'LineConfigDictionaries_Charmless',
+            'LineConfigDictionaries_Charm',
+            'LineConfigDictionaries_MiniBias',
+            'LineConfigDictionaries_QEE',
+            'LineConfigDictionaries_RD',
+            'LineConfigDictionaries_Semileptonic'
+            ]
+
