3d3df587ff47b94d90879614416f55aea2076264
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-09-13 10:56:01 +0000

    - Add Stripping 23r1 tests  - Disable LoKi python functors in all stripping tests, to check C++ caches.
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@194924 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 92803e8a0acbae67ad1fe14450be2df6c08ac8e3

--- DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
+++ DaVinci/DaVinciTests/tests/options/DisableLoKiPythonFunctors.py
@@ -1,7 +1,3 @@
 
-def post_action_disable_python( ) :
-    import os
-    os.environ["LOKI_DISABLE_PYTHON"] = "1"
-
-from Gaudi.Configuration import appendPostConfigAction
-appendPostConfigAction( post_action_disable_python )
+from Configurables import ApplicationMgr
+ApplicationMgr().Environment['LOKI_DISABLE_PYTHON'] = '1'
