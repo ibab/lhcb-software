fad9018efe5e4a67537706bd89312f902b978cce
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-09-13 11:20:41 +0000

    remove BsEtaCPhi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@194926 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 993778b193e0fd58bbac25d4de2096557a3348d1

--- Analysis/Phys/BsEtaCPhi/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/cvmfs/lhcb.cern.ch/lib/contrib/CMT/v1r20p20090520; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=BsEtaCPhi -version=v1r0 -path=/lhcb/users/akar/cmtuser/Urania_v3r0/Phys $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
