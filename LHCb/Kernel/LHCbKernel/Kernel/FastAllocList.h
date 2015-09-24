1ad18d3331781b2fea841164f9598348d5877c04
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-09-24 19:19:30 +0000

    Update fast alloc containers
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@195300 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 49912fcbc6fb7adaf5490ae3ef69f0016a036156

--- LHCb/Kernel/LHCbKernel/Kernel/FastAllocList.h
+++ LHCb/Kernel/LHCbKernel/Kernel/FastAllocList.h
@@ -1,19 +1,19 @@
 
 //--------------------------------------------------------------------------------
-/** @file FastAllocVector.h
+/** @file FastAllocList.h
  *
- *  Header file for vector class LHCb::FastAllocVector
+ *  Header file for list class LHCb::FastAllocList
  *
  *  @author Chris Jones  Christopher.Rob.Jones@cern.ch
  *  @date   29/03/2007
  */
 //--------------------------------------------------------------------------------
 
-#ifndef KERNEL_FastAllocVector_H
-#define KERNEL_FastAllocVector_H 1
+#ifndef KERNEL_FastAllocList_H
+#define KERNEL_FastAllocList_H 1
 
 // STL
-#include <vector>
+#include <list>
 
 #ifndef GOD_NOALLOC
 // Memory pool allocator from boost
@@ -29,9 +29,9 @@ namespace LHCb
 {
 
   //--------------------------------------------------------------------------------
-  /** @class FastAllocVector Kernel/FastAllocVector.h
+  /** @class FastAllocList Kernel/FastAllocList.h
    *
-   *  Vector with custom allocator
+   *  List with custom allocator
    *
    *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
    *  @date   23/09/2015
@@ -39,35 +39,35 @@ namespace LHCb
   //--------------------------------------------------------------------------------
 
   template < typename TYPE, typename ALLOC >
-  class FastAllocVector : public std::vector< TYPE, ALLOC >
+  class FastAllocList : public std::list< TYPE, ALLOC >
   {
 
   public:
 
     /// Default constructor
-    FastAllocVector( ) { }
+    FastAllocList( ) { }
 
     /** Constructor with initial size
-     *  @param size Initialisation size for vector 
+     *  @param size Initialisation size for list 
      */
-    FastAllocVector( const typename std::vector<TYPE,ALLOC>::size_type size )
-      : std::vector<TYPE,ALLOC>(size) { }
+    FastAllocList( const typename std::list<TYPE,ALLOC>::size_type size )
+      : std::list<TYPE,ALLOC>(size) { }
 
     /** Constructor with initial size and initialisation value
-     *  @param size Initialisation size for vector
+     *  @param size Initialisation size for list
      *  @param init Initialisation value
      */
-    FastAllocVector( const typename std::vector<TYPE,ALLOC>::size_type size,
-                     const TYPE & init ) : std::vector<TYPE,ALLOC>(size,init) { }
+    FastAllocList( const typename std::list<TYPE,ALLOC>::size_type size,
+                     const TYPE & init ) : std::list<TYPE,ALLOC>(size,init) { }
 
     /** Copy Constructor
-     *  @param init Initialisation vector
+     *  @param init Initialisation list
      */
-    FastAllocVector( const FastAllocVector & init ) : std::vector<TYPE,ALLOC>(init) { }
+    FastAllocList( const FastAllocList & init ) : std::list<TYPE,ALLOC>(init) { }
 
     /// Operator overloading for ostream
     friend inline std::ostream& operator << ( std::ostream& str ,
-                                              const FastAllocVector<TYPE,ALLOC> & v )
+                                              const FastAllocList<TYPE,ALLOC> & v )
     {
       str << "[ ";
       for ( const auto& i : v ) { str << i << " "; }
@@ -80,9 +80,9 @@ namespace LHCb
   {
 
     //--------------------------------------------------------------------------------
-    /** @typedef PoolAllocVector Kernel/FastAllocVector.h
+    /** @typedef PoolAllocList Kernel/FastAllocList.h
      *
-     *  Vector with pool allocator from boost
+     *  List with pool allocator from boost
      *
      *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
      *  @date   23/09/2015
@@ -94,15 +94,14 @@ namespace LHCb
                typename USERALLOC = boost::default_user_allocator_new_delete,
                typename MUTEX     = boost::details::pool::null_mutex,
                unsigned NEXTSIZE  = 32 >
-    using VecPoolAlloc = boost::pool_allocator< TYPE, USERALLOC, MUTEX, NEXTSIZE >;
+    using ListPoolAlloc = boost::fast_pool_allocator< TYPE, USERALLOC, MUTEX, NEXTSIZE >;
 #else
     template < typename TYPE >
-    using VecPoolAlloc = std::allocator< TYPE >;
+    using ListPoolAlloc = std::allocator< TYPE >;
 #endif
 
-    template < typename TYPE,
-               typename ALLOC = VecPoolAlloc< TYPE > >
-    using PoolAllocVector = FastAllocVector< TYPE, ALLOC >;
+    template < typename TYPE, typename ALLOC = ListPoolAlloc<TYPE> >
+    using PoolAllocList = FastAllocList< TYPE, ALLOC >;
 
   }
 
@@ -110,9 +109,9 @@ namespace LHCb
   {
 
     //--------------------------------------------------------------------------------
-    /** @typedef MTAllocVector Kernel/FastAllocVector.h
+    /** @typedef MTAllocList Kernel/FastAllocList.h
      *
-     *  Vector with MT allocator
+     *  List with MT allocator
      *
      *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
      *  @date   23/09/2015
@@ -126,12 +125,12 @@ namespace LHCb
                typename ALLOC = std::allocator< TYPE >
 #endif
                >
-    using MTAllocVector = FastAllocVector< TYPE, ALLOC >;
+    using MTAllocList = FastAllocList< TYPE, ALLOC >;
 
     //--------------------------------------------------------------------------------
-    /** @typedef PoolAllocVector Kernel/FastAllocVector.h
+    /** @typedef PoolAllocList Kernel/FastAllocList.h
      *
-     *  Vector with STL Pool allocator
+     *  List with STL Pool allocator
      *
      *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
      *  @date   23/09/2015
@@ -145,12 +144,12 @@ namespace LHCb
                typename ALLOC = std::allocator< TYPE >
 #endif
                >
-    using PoolAllocVector = FastAllocVector< TYPE, ALLOC >;
+    using PoolAllocList = FastAllocList< TYPE, ALLOC >;
 
     //--------------------------------------------------------------------------------
-    /** @typedef Vector Kernel/FastAllocVector.h
+    /** @typedef List Kernel/FastAllocList.h
      *
-     *  Standard STL vector, with default allocator
+     *  Standard STL list, with default allocator
      *
      *  @author Chris Jones   Christopher.Rob.Jones@cern.ch
      *  @date   23/09/2015
@@ -159,10 +158,10 @@ namespace LHCb
   
     template < typename TYPE,
                typename ALLOC = std::allocator< TYPE > >
-    using Vector = FastAllocVector< TYPE, ALLOC >;
+    using List = FastAllocList< TYPE, ALLOC >;
   
   }
 
 }
 
-#endif // KERNEL_FastAllocVector_H
+#endif // KERNEL_FastAllocList_H
