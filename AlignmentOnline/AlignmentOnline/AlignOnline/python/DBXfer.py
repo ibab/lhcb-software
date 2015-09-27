1743b937e5cdb2476cbacd2cabf3c2f3e5e7139b
Markus Frank <frankb@cern.ch>
2015-09-27 10:35:54 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/AlignmentOnline/trunk@195335 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 5cd08f20d3446315a5ffe35defdfeb38c52c70b0

--- AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer.py
+++ AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer.py
@@ -267,58 +267,60 @@ if Fails:
   print "Readback check on DB failed\n"
   pprint(Fails)
   sys.exit(44)
+print "Creating the Run Tick File for run "+str(RunOption.RunNumber)
 MakeTick(RunOption.OutputDirectory,RunOption.RunNumber)
 RunEnd =  (HLT2Params.RunStartTime+HLT2Params.RunDuration)*1000000000
+print "Creating the 'Tranfer_done' File for run "+str(RunOption.RunNumber)
 db_doneFile = open(RunOption.OutputDirectory+"/../CondDBXfer_done","w")
 db_doneFile.write(str(HLT2Params.RunStartTime)+" "+str(HLT2Params.RunDuration)+"\n")
 db_doneFile.close()
-print "Adding the RunTick to the Database"
-print "IoV is ["+str(RunStart)+","+str(RunEnd)+"]"+", ["+time.ctime(HLT2Params.RunStartTime)+","+time.ctime(RunEnd/1000000000)+"]"
-db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=False)
-status = CondDBUI.Admin.MakeDBFromFiles(RunOption.OutputDirectory+"/onl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd,
-                                   writeDuplicate = False
-                                    )
-db.closeDatabase()
-db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=True)
-print "Checking the Run Tick"
-Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
-                                    )
-if Fails:
-  print "Readback check on DB failed\n"
-  pprint(Fails)
-  sys.exit(44)
-db.closeDatabase()
-DBString = "CondDBOnline/ONLINE" # use the central Oracle Database...
-db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=True)
-print "Checking Database Contents against entered files with account "+DBString
-Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/offl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
-                                    )
-if Fails:
-  print "Readback check on DB failed\n"
-  pprint(Fails)
-  sys.exit(44)
-print "Checking The Run Tick contents with account "+DBString
-Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
-                                    )
-if Fails:
-  print "Readback check on DB failed\n"
-  pprint(Fails)
-  sys.exit(44)
+print "NOT Adding the RunTick to the Database"
+# print "IoV is ["+str(RunStart)+","+str(RunEnd)+"]"+", ["+time.ctime(HLT2Params.RunStartTime)+","+time.ctime(RunEnd/1000000000)+"]"
+# db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=False)
+# status = CondDBUI.Admin.MakeDBFromFiles(RunOption.OutputDirectory+"/onl", db,
+#                                    includes = [], excludes = [],
+#                                    verbose = True,
+#                                    since = RunStart,
+#                                    until = RunEnd,
+#                                    writeDuplicate = False
+#                                     )
+# db.closeDatabase()
+# db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=True)
+# print "Checking the Run Tick"
+# Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
+#                                    includes = [], excludes = [],
+#                                    verbose = True,
+#                                    since = RunStart,
+#                                    until = RunEnd
+#                                     )
+# if Fails:
+#   print "Readback check on DB failed\n"
+#   pprint(Fails)
+#   sys.exit(44)
+# db.closeDatabase()
+# DBString = "CondDBOnline/ONLINE" # use the central Oracle Database...
+# db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=True)
+# print "Checking Database Contents against entered files with account "+DBString
+# Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/offl", db,
+#                                    includes = [], excludes = [],
+#                                    verbose = True,
+#                                    since = RunStart,
+#                                    until = RunEnd
+#                                     )
+# if Fails:
+#   print "Readback check on DB failed\n"
+#   pprint(Fails)
+#   sys.exit(44)
+# print "Checking The Run Tick contents with account "+DBString
+# Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
+#                                    includes = [], excludes = [],
+#                                    verbose = True,
+#                                    since = RunStart,
+#                                    until = RunEnd
+#                                     )
+# if Fails:
+#   print "Readback check on DB failed\n"
+#   pprint(Fails)
+#   sys.exit(44)
 
 print "===================== Updated the Database ======================== Status = ",status
--- AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer_doRunTick.py
+++ AlignmentOnline/AlignmentOnline/AlignOnline/python/DBXfer_doRunTick.py
@@ -4,21 +4,44 @@ import RunOption
 import HLT2Params
 import sys
 import os
-print "===================== Adding the Run Tick to the Database ========================"
+def MakeTick(root,runnr):
+  pth = root+"/onl/Conditions/Online/LHCb"
+  if not os.access(pth, os.F_OK):
+    try:
+      os.makedirs(pth,0777)
+    except:
+      print "Cannot create directory "+pth
+  else:
+    print "Path "+pth+" already exists"
+  xmlf = open(pth+"/RunStamp.xml","w")
+  xmlf.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
+  xmlf.write('<!DOCTYPE DDDB SYSTEM "conddb:/DTD/structure.dtd">\n')
+  xmlf.write('<DDDB>\n')
+  xmlf.write('<condition name="RunStamp"> <param name="RunNumber" type="int">'
+             +str(runnr)
+             +'</param> </condition>\n')
+  xmlf.write('</DDDB>\n')
+  xmlf.close()
+
+import time
+print "===================== Start transferring The Run Tick for Run "+str(RunOption.RunNumber)+" ========================"
+RunStart = HLT2Params.RunStartTime
+RunEnd = HLT2Params.RunStartTime+HLT2Params.RunDuration
+RunStartns = RunStart*1000000000
+RunEndns = RunEnd*1000000000
+DBString = "CondDBOnline(owner)/ONLINE" # use the central Oracle Database...
+print "Operating on Database "+DBString
+print "Adding the RunTick to the Database"
+print "IoV is ["+str(RunStartns)+","+str(RunEndns)+"], ["+time.ctime(RunStart)+","+time.ctime(RunEnd)+"]"
+status = 0
 import CondDBUI
 import CondDBUI.Admin
-DBString = "CondDBOnline(owner)/ONLINE" # use the central Oracle Database...
-db_doneFile = open(RunOption.OutputDirectory+"/../CondDBXfer_done","w")
-db_doneFile.write(str(HLT2Params.RunStartTime)+" "+str(HLT2Params.RunDuration))
-db_doneFile.close()
-print "Adding the RunTick to the Database"
-print "IoV is ["+str(RunStart)+","+str(RunEnd)+"]"+", ["+time.ctime(HLT2Params.RunStartTime)+","+time.ctime(RunEnd/1000000000)+"]"
 db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=False)
 status = CondDBUI.Admin.MakeDBFromFiles(RunOption.OutputDirectory+"/onl", db,
                                    includes = [], excludes = [],
                                    verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd,
+                                   since = RunStartns,
+                                   until = RunEndns,
                                    writeDuplicate = False
                                     )
 db.closeDatabase()
@@ -27,37 +50,12 @@ print "Checking the Run Tick"
 Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
                                    includes = [], excludes = [],
                                    verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
+                                   since = RunStartns,
+                                   until = RunEndns,
                                     )
 if Fails:
-  print "Readback of Run Tick check on DB failed\n"
+  print "Readback check on DB failed\n"
   pprint(Fails)
   sys.exit(44)
 db.closeDatabase()
-DBString = "CondDBOnline/ONLINE" # use the central Oracle Database...
-db = CondDBUI.CondDB(DBString, create_new_db = False, readOnly=True)
-print "Checking Database Contents against entered files with account "+DBString
-Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/offl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
-                                    )
-if Fails:
-  print "Readback check on DB failed\n"
-  pprint(Fails)
-  sys.exit(44)
-print "Checking The Run Tick contents with account "+DBString
-Fails = CondDBUI.Admin.CompareDBToFiles(RunOption.OutputDirectory+"/onl", db,
-                                   includes = [], excludes = [],
-                                   verbose = True,
-                                   since = RunStart,
-                                   until = RunEnd
-                                    )
-if Fails:
-  print "Readback check on DB failed\n"
-  pprint(Fails)
-  sys.exit(44)
-
-print "===================== Updated the Database ======================== Status = ",status
+print "===================== Done transferring The Run Tick for Run "+str(RunOption.RunNumber)+" ======================== Status = ",status
