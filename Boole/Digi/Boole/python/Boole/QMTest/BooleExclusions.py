17feab50d6f5a01687c07bdb4b1f478eb96a5640
Marco Cattaneo <marco.cattaneo@cern.ch>
2015-10-08 13:41:09 +0000

    Add temporary RegexpReplacers for forward compatibility with GAUDI-1078
    git-svn-id: http://svn.cern.ch/guest/lhcb/Boole/trunk@195729 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a3513837dbd5fb0483ba6520421ecad20f6c16da

--- Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
+++ Boole/Digi/Boole/python/Boole/QMTest/BooleExclusions.py
@@ -4,4 +4,6 @@ from GaudiConf.QMTest.LHCbExclusions import preprocessor as LHCbPreprocessor
 preprocessor = LHCbPreprocessor + \
     LineSkipper(["BooleInit.Boole...SUCCESS Exceptions/Errors/Warnings/Infos Statistics : 0/0/"]) + \
     LineSkipper(["SIMCOND_"]) + \
-    LineSkipper(["DDDB_"])
+    LineSkipper(["DDDB_"]) + \
+    RegexpReplacer(when = "ToolSvc.OTChann...SUCCESS", orig = r' \[OTFillRawBuffer\]', repl = r'' ) + \
+    RegexpReplacer(when = "ToolSvc.OTChann...SUCCESS", orig = r' \[OTTimeChecker\]', repl = r'' )
