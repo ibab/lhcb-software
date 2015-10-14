b3dde7d7412e084db52b4cebd72afa35f203464c
Kevin Dungs <kevin.dungs@cern.ch>
2015-10-14 11:26:45 +0000

    Add Python wrapper for HistTask.
    This allows for THxD and TProfile objects to be obtained from DIM.
    Using it is as easy as
    ```
    import Gaucho
    ht = Gaucho.HistTaskWrapper('LHCb_CALD0701_CalibHistos_0', 'mona08')
    hs = ht.histograms()
    ```
    
    
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@196263 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 30dfb8afd739f997d7f6fdaec3ca000e76536532

---
+++ Online/Online/Gaucho/python/Gaucho/HistTaskWrapper.py
@@ -0,0 +1,101 @@
+""" This module exposes a class that wraps around the HistTask C++ class from
+Gaucho.
+
+    Authors: Alex Pearce <alex.pearce@cern.ch>
+             Kevin Dungs <kevin.dungs@cern.ch>
+"""
+import ROOT
+ROOT.gSystem.Load("libGaucho.so")
+ROOT.gInterpreter.Declare('#include "Gaucho/HistTask.h"')
+
+# ROOT "typedefs"
+VectorOfString = ROOT.vector('string')
+VectorOfTObjectPtr = ROOT.vector('TObject*')
+
+
+class HistTaskWrapper(object):
+    """ This class wraps around the HistTask C++ class from Gaucho.
+    It exposes all of the methods in a pythonic way.
+    """
+    @classmethod
+    def _cast_TObject(cls, tobj):
+        """ """
+        cls_map = {
+            ROOT.TH1D.Class(): ROOT.TH1D,
+            ROOT.TH2D.Class(): ROOT.TH2D,
+            ROOT.TH3D.Class(): ROOT.TH3D,
+            ROOT.TProfile.Class(): ROOT.TProfile
+        }
+        cls = cls_map.get(tobj.IsA())
+        if cls is None:
+            raise Exception('Obtained TObject of unsupported type.')
+        return cls(tobj)
+
+    @classmethod
+    def _task_list(cls, dns):
+        """ Obtains a task list from DIM as `std::vector<std::string>`.
+        This is only internally by the classmethod `task_list` which returns a
+        Python list of Python strings.
+        """
+        locations = VectorOfString()
+        err = HistTask.TaskList(dns, locations)
+        if err != 0:
+            raise Exception('Could not obtain task list from DIM.')
+        return locations
+
+    @classmethod
+    def task_list(cls, dns):
+        """ Obtains task list from DIM. """
+        return [t for t in cls._task_list(dns)]
+
+    def __init__(self, task_name, dns):
+        """ Sets up a new wrapper object for a given task name and dns that can
+        then obtain the histograms associated with the task.
+        """
+        self._dns = dns
+        self._task_name = task_name
+        self._hist_task = ROOT.HistTask(task_name, dns)
+        # These need to be set, otherwise movefromdim segfaults
+        ROOT.TH1D.SetDefaultSumw2()
+        ROOT.TH2D.SetDefaultSumw2()
+        ROOT.TProfile.SetDefaultSumw2()
+
+    def _directory(self):
+        """ Obtains the histogram names for the task associated with this
+        instance from DIM and returns a `std::vector<std::string>`.
+        This is used internally by the `directory` method which unwraps the
+        result in a Python list of Python strings.
+        """
+        dir_ = VectorOfString()
+        err = self._hist_task.Directory(dir_)
+        if err != 0:
+            raise Exception('Could not obtain directory from DIM.')
+        return dir_
+
+    def directory(self):
+        """ Obtains the histogram names for the task associated with this
+        instance.
+        """
+        return [n for n in self._directory()]
+
+    def _histograms(self, histogram_names=None):
+        """ Obtains histograms of given names for task associated with this
+        instance as a `std::vector<TObject*>`.
+        This is used internally by the `histograms` method which unwraps the
+        result into a Python list of the correct type (`TH1D`, `TH2D`, `TH3D`,
+        or `TProfile`).
+        """
+        if histogram_names is None:
+            histogram_names = self._directory()
+        hists = VectorOfTObjectPtr()
+        err = self._hist_task.Histos(histogram_names, hists)
+        if err != 0:
+            raise Exception('Could not obtain histograms from DIM.')
+        return hists
+
+    def histograms(self, histogram_names=None):
+        """ Obtains histograms of given names for task associated with this
+        instance.
+        """
+        return [self._cast_TObject(t)
+                for t in self._histograms(histogram_names)]
---
+++ Online/Online/Gaucho/python/Gaucho/__init__.py
@@ -0,0 +1,6 @@
+""" Pythonic bindings for the Gaucho package.
+
+    Authors: Alex Pearce <alex.pearce@cern.ch>
+             Kevin Dungs <kevin.dungs@cern.ch>
+"""
+from .HistTaskWrapper import HistTaskWrapper
