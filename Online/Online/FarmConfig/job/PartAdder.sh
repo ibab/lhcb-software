133230c42b35cbd097e3c47e609924f9afa9b6f5
Markus Frank <frankb@cern.ch>
2015-10-16 13:21:43 +0000

    Updates and bug fixes
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@196377 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a71ef6f4d5bb0faae35adfb63e8a7802d9ec396b

--- Online/Online/FarmConfig/job/PartAdder.sh
+++ Online/Online/FarmConfig/job/PartAdder.sh
@@ -13,5 +13,6 @@
 export victimnodes="hlt02(.*)"
 export PYTHONPATH=${ONLINE_ENV_DIR}:${PYTHONPATH}
 export AdderOptsFile=/tmp/${PARTITION_NAME}_AdderOpts.opts
+export AdderStaticOpts=../options/PartAdderStatic.opts
 python ./Tasklist_from_architecture.py 3 ${AdderOptsFile}
 exec -a ${UTGID} ${Class1_task} -opts=../options/genAdder.opts
