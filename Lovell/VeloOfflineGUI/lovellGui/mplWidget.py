6aa72fc88cc40eb06fdb321327d6717ea843bdb0
Daniel Martin Saunders <daniel.saunders@cern.ch>
2015-10-26 17:31:48 +0000

    noise overview update
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@196864 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a53faaee28659cfd273a497c04fd17cd1c10005a

--- Lovell/VeloOfflineGUI/lovellGui/mplWidget.py
+++ Lovell/VeloOfflineGUI/lovellGui/mplWidget.py
@@ -123,13 +123,13 @@ class lPlottable():
                 z = nominal['data']['data']['values'][i][j]
                 
                 if z == 0.0: continue
-                x = nominal['data']['data']['xbinning'][i][1] + halfBinX
+                x = nominal['data']['data']['xbinning'][i][1] + halfBinX + 0.68
                 y = nominal['data']['data']['ybinning'][j][1] + halfBinY
                 
                 if int(z) % 2 == 0: y = 2.75
                 else: y = -2.75
                 self.axes.annotate(str(int(z)), xy=(x, y), xycoords='data', fontsize=9,
-                                   horizontalalignment='center', verticalalignment='center', rotation=90)
+                                   horizontalalignment='center', verticalalignment='center', rotation=90, weight = demibold)
         
         
     def runview_1d_dataMinusRef(self, nominal, reference, infactRatio = False):
