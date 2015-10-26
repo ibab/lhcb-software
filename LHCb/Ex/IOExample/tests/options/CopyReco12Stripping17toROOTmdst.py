1d024a766f9e31a92c8f15c3bdea176e5ea8c88a
Marco Cattaneo <marco.cattaneo@cern.ch>
2015-10-26 14:40:20 +0000

    Fix last two tests following ODINTimeDecoder fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@196822 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: c2d440f127f9a5be48ecb00eac35784d57545598

--- LHCb/Ex/IOExample/tests/options/CopyReco12Stripping17toROOTmdst.py
+++ LHCb/Ex/IOExample/tests/options/CopyReco12Stripping17toROOTmdst.py
@@ -8,5 +8,6 @@ IOTest( EvtMax = 5, LoadAll = True, DataContent = 'MDST', KillDAQ = True )
 ioh = IOHelper()
 ioh.setupServices()
 ioh.outStream('PFN:Reco12-Stripping17.mdst',"InputCopyStream")
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/swtest/lhcb/LHCb/Collision11/CHARM.MDST/00012547/0000/00012547_00000013_1.charm.mdst'])
 
-TestFileDB.test_file_db["R12S17-charm.mdst"].run()
+#TestFileDB.test_file_db["R12S17-charm.mdst"].run()
--- LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTmdst.py
+++ LHCb/Ex/IOExample/tests/options/CopyReco14Stripping20toROOTmdst.py
@@ -8,5 +8,6 @@ IOTest( EvtMax = 5, LoadAll = True, DataContent = 'MDST', KillDAQ = True )
 ioh = IOHelper()
 ioh.setupServices()
 ioh.outStream('PFN:Reco14-Stripping20.mdst',"InputCopyStream")
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/swtest/lhcb/LHCb/Collision12/BHADRON.MDST/00020198/0000/00020198_00000758_1.bhadron.mdst'])
 
-TestFileDB.test_file_db["R14S20-bhadron.mdst"].run()
+#TestFileDB.test_file_db["R14S20-bhadron.mdst"].run()
