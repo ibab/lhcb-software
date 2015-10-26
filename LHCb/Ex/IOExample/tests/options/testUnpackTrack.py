88c54e0e615569dd49d6689b5819f285c1590b78
Marco Cattaneo <marco.cattaneo@cern.ch>
2015-10-26 12:56:58 +0000

    Fix one more test following ODINTimeDecoder fix
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@196818 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: be9ea4bb964ff75b8ac7a7a59f5d665d3d94d100

--- LHCb/Ex/IOExample/tests/options/testUnpackTrack.py
+++ LHCb/Ex/IOExample/tests/options/testUnpackTrack.py
@@ -2,8 +2,14 @@
 from PRConfig import TestFileDB
 from Gaudi.Configuration import ApplicationMgr
 from Configurables import DumpTracks, UnpackTrack
+from GaudiConf.IOHelper import IOHelper
 
 upkTr = UnpackTrack()
 ApplicationMgr().TopAlg += [ upkTr, DumpTracks() ]
 
-TestFileDB.test_file_db["Reco10-sdst-10events"].run()
+ioh = IOHelper('ROOT')
+ioh.setupServices()
+ioh.inputFiles(['PFN:root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/swtest/lhcb/swtest/Reco10-sdst-10events/00011652_00000001_1-evt-18641to18650.sdst'])
+
+
+#TestFileDB.test_file_db["Reco10-sdst-10events"].run()
