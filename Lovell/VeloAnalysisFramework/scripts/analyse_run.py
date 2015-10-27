f0747d38ee3c310fd61dc299860a6c1a1b08e4ef
Lennaert Bel <l.bel@cern.ch>
2015-10-27 12:00:21 +0000

    Allow running over multiple runs when creating GRF, and check whether those runs already exist in the GRF.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@196893 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 406a86f730e5bc9976cf183358fba8d1a14572e6

--- Lovell/VeloAnalysisFramework/scripts/analyse_run.py
+++ Lovell/VeloAnalysisFramework/scripts/analyse_run.py
@@ -9,13 +9,13 @@ import argparse
 
 from veloview.config import Config
 from veloview.core import AnalysisConfigWrapper
-from veloview.runview.utils import run_file_path
+from veloview.runview.utils import run_file_path, is_in_grf, add_to_grf
 
 def create_parser():
     parser = argparse.ArgumentParser(
         description=__doc__.split("\n")[0],
         epilog="\n".join(__doc__.split("\n")[1:]))
-    parser.add_argument("run", type=int, help="Run number")
+    parser.add_argument("runs", type=int, nargs='+', help="Run number")
     parser.add_argument("--run-data-dir", default=Config().run_data_dir,
                         help="Directory to search for run list and data")
     parser.add_argument("--output-dir", default=None,
@@ -25,19 +25,24 @@ def create_parser():
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    run = args.run
+    runs = args.runs
     run_data_dir = args.run_data_dir
     output_dir = args.output_dir
     if not output_dir:
-        output_dir = run_file_path(run)
+        output_dir = run_file_path(runs[0])
+
 
     # Change the run data directory to the user-specified one
     Config().run_data_dir = run_data_dir
 
     configWrapper = AnalysisConfigWrapper(Config().analysis_config)
-    combinerTrunk = configWrapper.getTrunkForRun(run)
-    combinerTrunk.evaluate()
-    combinerTrunk.write_to_grf()
+
+    for run in runs:
+        if not is_in_grf(run):
+            combinerTrunk = configWrapper.getTrunkForRun(run)
+            combinerTrunk.evaluate()
+            combinerTrunk.write_to_grf()
+            add_to_grf(run)
 
 if __name__ == "__main__":
     main()
