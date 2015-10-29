d979c511d1541c6b740c86a12a089a0025b42732
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-10-29 15:25:02 +0000

    stop propagating globalTimeOffset (see GAUDI-1122)
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@196990 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 851f7faa43be598c02e77691cd6588a4e9edaaf0

--- LHCb/Phys/LoKiCore/src/AlgFunctors.cpp
+++ LHCb/Phys/LoKiCore/src/AlgFunctors.cpp
@@ -97,7 +97,7 @@ namespace
     SmartIF<IAlgContextSvc> cntx ( ls ) ;
     LoKi::Assert ( !(!cntx) , "IAlgContextSvc* points to NULL!" ) ;
     GaudiAlgorithm *parent = Gaudi::Utils::getGaudiAlg ( cntx ) ;
-    LoKi::Assert ( parent!=0 , "parent not a GaudiAlg!" ) ;
+    LoKi::Assert ( parent!=nullptr , "parent not a GaudiAlg!" ) ;
     IJobOptionsSvc* jos = parent->svc<IJobOptionsSvc>( "JobOptionsSvc" );
 
     /////////////////////////////
@@ -108,31 +108,24 @@ namespace
 
     bool addedContext = false;
     bool addedRootInTES = false;
-    bool addedGlobalTimeOffset = false;
     // do not create it now
     SmartIF<IAlgorithm> myIAlg = iam->algorithm( typeName , false); 
     if ( !myIAlg.isValid() ) {
       //== Set the Context if not in the jobOptions list
       if ( ""  != parent->context() ||
-           ""  != parent->rootInTES() ||
-           0.0 != parent->globalTimeOffset() ) {
+           ""  != parent->rootInTES() ) {
         bool foundContext = false;
         bool foundRootInTES = false;
-        bool foundGlobalTimeOffset = false;
         const std::vector<const Property*>* properties = 
           jos->getProperties( theName );
-        if ( 0 != properties ) {
+        if ( properties ) {
           // Iterate over the list to set the options
-          for ( std::vector<const Property*>::const_iterator itProp = 
-                  properties->begin();
-                itProp != properties->end();
-                itProp++ )   {
+          for (const auto & property : *properties)   {
             const StringProperty* sp =
-              dynamic_cast<const StringProperty*>(*itProp); // is GlobalTimeOffSet really a stringproperty....
-            if ( 0 != sp )    {
-              if ( "Context" == (*itProp)->name() ) foundContext = true;
-              if ( "RootInTES" == (*itProp)->name() ) foundRootInTES = true;
-              if ( "GlobalTimeOffset" == (*itProp)->name() ) foundGlobalTimeOffset = true;
+              dynamic_cast<const StringProperty*>(property);
+            if ( sp )    {
+              if ( "Context" == property->name() ) foundContext = true;
+              if ( "RootInTES" == property->name() ) foundRootInTES = true;
             }
           }
         }
@@ -146,16 +139,9 @@ namespace
           jos->addPropertyToCatalogue( theName, rootInTESProperty ).ignore();
           addedRootInTES = true;
         }
-        if ( !foundGlobalTimeOffset && 0.0 != parent->globalTimeOffset() ) {
-          DoubleProperty globalTimeOffsetProperty( "GlobalTimeOffset", 
-                                                   parent->globalTimeOffset() );
-          jos->addPropertyToCatalogue( theName, 
-                                       globalTimeOffsetProperty ).ignore();
-          addedGlobalTimeOffset = true;
-        }
       }
 
-      Algorithm *myAlg = 0;
+      Algorithm *myAlg = nullptr;
       StatusCode result = parent->createSubAlgorithm( theType, theName, myAlg );
       // (MCl) this should prevent bug #35199... even if I didn't manage to
       // reproduce it with a simple test.
@@ -178,10 +164,6 @@ namespace
     if ( addedRootInTES ) {
       jos->removePropertyFromCatalogue( theName, "RootInTES" ).ignore();
       addedRootInTES = false;
-    }
-    if ( addedGlobalTimeOffset ) {
-      jos->removePropertyFromCatalogue( theName, "GlobalTimeOffset" ).ignore();
-      addedGlobalTimeOffset = false;
     }
     ///////// end of code copied from GaudiSequencer...
 
@@ -218,7 +200,7 @@ namespace
   ( const std::string&    name , LoKi::ILoKiSvc* loki )  
   {
     SmartIF<IAlgManager> iam = getAlgManager  ( loki ) ;
-    if ( !iam ) { return 0 ; }  
+    if ( !iam ) { return nullptr ; }  
     return getAlgorithm ( name , iam ) ;
   }
   // ==========================================================================
