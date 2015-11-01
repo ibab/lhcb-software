49305b35bfe2b5fb43a976bd535214bb377679ab
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-11-01 11:56:13 +0000

    make pre-defined z-positions of states constexpr
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@197067 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8c90a988a04cacc08976398e6699fe8f8c3f9f3b

--- LHCb/Event/TrackEvent/Event/StateParameters.h
+++ LHCb/Event/TrackEvent/Event/StateParameters.h
@@ -18,16 +18,16 @@ namespace StateParameters
 {
   /// Pre-defined z-positions of states for certain State locations
   /// (see Location enum in State.h)
-  static const double ZBegRich1 =   990. * Gaudi::Units::mm;
-  static const double ZEndRich1 =  2165. * Gaudi::Units::mm;
-  static const double ZEndT     =  9410. * Gaudi::Units::mm;
-  static const double ZBegRich2 =  9450. * Gaudi::Units::mm;
-  static const double ZEndRich2 = 11900. * Gaudi::Units::mm;
-  static const double ZBegT     =  7500. * Gaudi::Units::mm;
-  static const double ZMidT     =  8520. * Gaudi::Units::mm;
-  static const double ZMidTT    =  2485. * Gaudi::Units::mm;
-  static const double ZEndTT    =  2700. * Gaudi::Units::mm;
-  static const double ZEndVelo  =   770. * Gaudi::Units::mm;
+  constexpr double ZBegRich1 =   990. * Gaudi::Units::mm;
+  constexpr double ZEndRich1 =  2165. * Gaudi::Units::mm;
+  constexpr double ZEndT     =  9410. * Gaudi::Units::mm;
+  constexpr double ZBegRich2 =  9450. * Gaudi::Units::mm;
+  constexpr double ZEndRich2 = 11900. * Gaudi::Units::mm;
+  constexpr double ZBegT     =  7500. * Gaudi::Units::mm;
+  constexpr double ZMidT     =  8520. * Gaudi::Units::mm;
+  constexpr double ZMidTT    =  2485. * Gaudi::Units::mm;
+  constexpr double ZEndTT    =  2700. * Gaudi::Units::mm;
+  constexpr double ZEndVelo  =   770. * Gaudi::Units::mm;
 }
 
 // ----------------------------------------------------------------------------
