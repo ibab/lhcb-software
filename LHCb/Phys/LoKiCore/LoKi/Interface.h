80e1d2494df41d1a1e89592feb41bea58eaf1104
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-11-01 16:02:22 +0000

    - augment LoKi::Interface so that it supports the same semantics as    regular smartpointers, i.e. add explicit operator bool and get
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@197080 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: d442da42e7ced19769c1c3611d09999551e3cda8

--- LHCb/Phys/LoKiCore/LoKi/Interface.h
+++ LHCb/Phys/LoKiCore/LoKi/Interface.h
@@ -1,111 +1,111 @@
 // ============================================================================
-#ifndef LOKI_INTERFACE_H 
+#ifndef LOKI_INTERFACE_H
 #define LOKI_INTERFACE_H 1
 // ============================================================================
-// Include files 
+// Include files
 // ============================================================================
-// LoKiCore 
+// LoKiCore
 // ============================================================================
 #include "LoKi/AddRef.h"
 #include "LoKi/Release.h"
 // ============================================================================
 /** @file
  *
- *  This file is a part of LoKi project - 
+ *  This file is a part of LoKi project -
  *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
  *
  *  The package has been designed with the kind help from
- *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
- *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas,
+ *  contributions and advices from G.Raven, J.van Tilburg,
  *  A.Golutvin, P.Koppenburg have been used in the design.
  *
  *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date 2006-01-23 
+ *  @date 2006-01-23
  */
 // ============================================================================
-namespace LoKi 
+namespace LoKi
 {
   // ==========================================================================
   /** @class Interface Interface.h LoKi/Interface.h
    *
-   *  Helper class for manipulations with interfaces 
-   *  It looks like reduced smart *INTRUSIVE* pointer 
+   *  Helper class for manipulations with interfaces
+   *  It looks like reduced smart *INTRUSIVE* pointer
    *
    *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
    *  @date   2006-03-10
    */
   template <class TYPE>
-  class Interface 
+  class Interface
   {
-  public: 
+  public:
     // ========================================================================
-    /** Standard/default constructor from the interface 
-     *  @param obj object to be used 
+    /** Standard/default constructor from the interface
+     *  @param obj object to be used
      */
-    Interface ( const TYPE* obj = 0 ) 
-      : m_object ( const_cast<TYPE*>( obj )  ) { LoKi::addRef ( m_object ) ; } 
-    /// copy constructor 
-    Interface ( const Interface<TYPE>& right ) 
-      : m_object( right.m_object )             { LoKi::addRef ( m_object ) ; } 
-    /// templated "copy" constructor 
+    Interface ( const TYPE* obj = 0 )
+      : m_object ( const_cast<TYPE*>( obj )  ) { LoKi::addRef ( m_object ) ; }
+    /// copy constructor
+    Interface ( const Interface<TYPE>& right )
+      : m_object( right.m_object )             { LoKi::addRef ( m_object ) ; }
+    /// templated "copy" constructor
     template <class OTHER>
-    Interface ( const Interface<OTHER>& right ) 
-      : m_object ( right.m_object )            { LoKi::addRef ( m_object ) ; } 
-    /// virtual destructor 
+    Interface ( const Interface<OTHER>& right )
+      : m_object ( right.m_object )            { LoKi::addRef ( m_object ) ; }
+    /// virtual destructor
     virtual ~Interface() { LoKi::release ( m_object ) ; } // virtual Destructor
     // ========================================================================
   public:
     // ========================================================================
-    /// assignement from the raw pointer 
-    Interface& operator= ( const TYPE* obj ) 
+    /// assignement from the raw pointer
+    Interface& operator= ( const TYPE* obj )
     {
       if ( m_object == obj ) { return *this ; }
       //
       TYPE* tmp = m_object ;
       m_object = const_cast<TYPE*> ( obj ) ;
-      //                                              the order *DOES* matter 
-      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter 
-      LoKi::release ( tmp      ) ;                 // 2) decrement the counter 
+      //                                              the order *DOES* matter
+      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter
+      LoKi::release ( tmp      ) ;                 // 2) decrement the counter
       //
       return *this ;
-    } 
-    /// the regular assignement 
-    Interface& operator= ( const Interface<TYPE>& right ) 
+    }
+    /// the regular assignement
+    Interface& operator= ( const Interface<TYPE>& right )
     {
       if ( &right == this ) { return *this ; }
       //
       TYPE* tmp = m_object ;
       m_object  = right.getObject () ;
-      //                                              the order *DOES* matter 
-      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter 
-      LoKi::release ( tmp      ) ;                 // 2) decrement the counter 
+      //                                              the order *DOES* matter
+      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter
+      LoKi::release ( tmp      ) ;                 // 2) decrement the counter
       //
-      return *this ;      
-    } 
+      return *this ;
+    }
     /// the templated assignement
     template <class OTHER>
-    Interface& operator= ( const Interface<OTHER>& right ) 
+    Interface& operator= ( const Interface<OTHER>& right )
     {
       TYPE* tmp = m_object ;
       m_object  = right.getObject () ;
-      //                                              the order *DOES* matter! 
-      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter 
-      LoKi::release ( tmp      ) ;                 // 2) decrement the counter 
+      //                                              the order *DOES* matter!
+      LoKi::addRef  ( m_object ) ;                 // 1) increment the counter
+      LoKi::release ( tmp      ) ;                 // 2) decrement the counter
       //
-      return *this ;                                  // RETURN 
-    } 
+      return *this ;                                  // RETURN
+    }
     // ========================================================================
   public:
     // ========================================================================
-    /// comparison with raw pointer 
-    bool operator== ( const TYPE* other ) const 
+    /// comparison with raw pointer
+    bool operator== ( const TYPE* other ) const
     { return m_object == other ; }
-    /// comparison with same type pointer 
-    bool operator== ( const Interface<TYPE>& other ) const 
+    /// comparison with same type pointer
+    bool operator== ( const Interface<TYPE>& other ) const
     { return m_object == other.m_object ; }
-    /// comparison with other type of pointer 
+    /// comparison with other type of pointer
     template <class OTHER>
-    bool operator== ( const Interface<OTHER>& other ) const 
+    bool operator== ( const Interface<OTHER>& other ) const
     { return m_object == other.m_object ; }
     // ========================================================================
   public:
@@ -114,15 +114,19 @@ namespace LoKi
     // ========================================================================
   public:
     // ========================================================================
-    /// valid pointer? 
+    /// valid pointer?
     bool validPointer         () const { return 0 != m_object   ; }
     /// invalid ?
     bool operator!            () const { return !validPointer() ; }
-    /// conversion to underlying type 
+    /// valid?
+    explicit operator bool    () const { return validPointer()  ; }
+    /// conversion to underlying type
     inline TYPE* operator->   () const { return getObject()     ; }
-    /// automatic conversion 
+    /// automatic conversion
     inline operator TYPE*     () const { return getObject()     ; }
-    /// access to the object 
+    /// access to the object
+    inline TYPE* get          () const { return m_object        ; }
+    /// access to the object
     inline TYPE* getObject    () const { return m_object        ; }
     // ========================================================================
   public :
@@ -131,8 +135,8 @@ namespace LoKi
     // ========================================================================
   public :
     // ========================================================================
-    /// release the underlying pointer & invalidate the object 
-    void release () 
+    /// release the underlying pointer & invalidate the object
+    void release ()
     {
       LoKi::release ( m_object ) ;
       m_object = 0 ;
@@ -140,14 +144,14 @@ namespace LoKi
     // ========================================================================
   private:
     // ========================================================================
-    /// the object to be stored 
-    TYPE* m_object ;                                 // the object to be stored 
+    /// the object to be stored
+    TYPE* m_object ;                                 // the object to be stored
     // ========================================================================
   } ;
   // ==========================================================================
 } // end of namespace LoKi
 // ============================================================================
-// The END 
+// The END
 // ============================================================================
 #endif // LOKI_INTERFACE_H
 // ============================================================================
