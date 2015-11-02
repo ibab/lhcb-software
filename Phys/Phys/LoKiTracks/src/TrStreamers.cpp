2d54e8ee74ed8959660e4b7b1b9042120cb47f3b
Gerhard Raven <gerhard.raven@nikhef.nl>
2015-11-02 20:35:15 +0000

    replace LoKi::select with std::copy_if
    git-svn-id: http://svn.cern.ch/guest/lhcb/Phys/trunk@197139 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 12b2e14504c7769ec987695445f9ef593b068839

--- Phys/Phys/LoKiTracks/src/TrStreamers.cpp
+++ Phys/Phys/LoKiTracks/src/TrStreamers.cpp
@@ -1,100 +1,101 @@
 // $Id: $
 // ============================================================================
-// Include files 
+// Include files
 // ============================================================================
 // STD & STL
 // ============================================================================
 #include <set>
+#include <algorithm>
+#include <functional>
 // ============================================================================
 // local
 // ============================================================================
 #include "LoKi/TrackTypes.h"
-#include "LoKi/select.h"
 // ============================================================================
-/** @file 
+/** @file
  *
- *  implementation of some streamers 
+ *  implementation of some streamers
  *
- *  This file is a part of LoKi project - 
+ *  This file is a part of LoKi project -
  *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
  *
  *  The package has been designed with the kind help from
- *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas, 
- *  contributions and advices from G.Raven, J.van Tilburg, 
+ *  Galina PAKHLOVA and Sergey BARSUK.  Many bright ideas,
+ *  contributions and advices from G.Raven, J.van Tilburg,
  *  A.Golutvin, P.Koppenburg have been used in the design.
  *
- *  By usage of this code one clearly states the disagreement 
- *  with the smear campaign of Dr.O.Callot et al.: 
+ *  By usage of this code one clearly states the disagreement
+ *  with the smear campaign of Dr.O.Callot et al.:
  *  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
  *
  *  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
- *  @date 2012-05-24 
+ *  @date 2012-05-24
  *
  *                    $Revision: 124207 $
  *  Last modification $Date: 2011-06-02 18:05:29 +0200 (Thu, 02 Jun 2011) $
  *                 by $Author: ibelyaev $
  */
 // ============================================================================
-namespace 
+namespace
 {
   // ==========================================================================
   template <class TRACK>
-  inline 
-  LHCb::Track::ConstVector 
-  _get_ ( TRACK                      first , 
-          TRACK                      last  , 
-          const LoKi::Types::TrCuts& cuts  ) 
+  inline
+  LHCb::Track::ConstVector
+  _get_ ( TRACK                      first ,
+          TRACK                      last  ,
+          const LoKi::Types::TrCuts& cuts  )
   {
     //
     LHCb::Track::ConstVector trks ; trks.reserve ( last - first ) ;
-    LoKi::select ( first , 
-                   last  , 
-                   std::back_inserter ( trks ) , cuts ) ;
+    std::copy_if ( first ,
+                   last  ,
+                   std::back_inserter ( trks ) , std::cref(cuts) ) ;
     //
     return trks ;
   }
-  // ==========================================================================  
+  // ==========================================================================
   template <class TRACKS>
-  inline 
-  LHCb::Track::ConstVector 
-  _get_ ( const TRACKS*              tracks , 
-          const LoKi::Types::TrCuts& cuts   ) 
+  inline
+  LHCb::Track::ConstVector
+  _get_ ( const TRACKS*              tracks ,
+          const LoKi::Types::TrCuts& cuts   )
   {
     //
-    LHCb::Track::ConstVector trks ; 
-    if ( 0 == tracks || tracks->empty() ) { return trks ; }
+    LHCb::Track::ConstVector trks ;
+    if ( !tracks || tracks->empty() ) { return trks ; }
     //
     trks.reserve ( tracks -> size  () ) ;
-    LoKi::select ( tracks -> begin () , 
-                   tracks -> end   () , 
-                   std::back_inserter ( trks ) , cuts ) ;
+    std::copy_if ( tracks -> begin () ,
+                   tracks -> end   () ,
+                   std::back_inserter ( trks ) , std::cref(cuts) ) ;
     //
     return trks ;
   }
   // ==========================================================================
   template <class TRACK>
-  inline 
+  inline
   std::vector<double>
-  _get_ ( TRACK                      first , 
-          TRACK                      last  , 
-          const LoKi::Types::TrFunc& func  ) 
+  _get_ ( TRACK                      first ,
+          TRACK                      last  ,
+          const LoKi::Types::TrFunc& func  )
   {
     //
     std::vector<double> r ; r.reserve ( last - first ) ;
     //
-    LoKi::apply ( first , 
+    LoKi::apply ( first ,
                   last  , func , std::back_inserter ( r ) ) ;
     //
     return r ;
   }
-  // ==========================================================================  
+  // ==========================================================================
   template <class TRACKS>
-  inline 
+  inline
   std::vector<double>
-  _get_ ( const TRACKS*              tracks , 
-          const LoKi::Types::TrFunc& func   ) 
+  _get_ ( const TRACKS*              tracks ,
+          const LoKi::Types::TrFunc& func   )
   {
-    std::vector<double> r ; 
+    std::vector<double> r ;
     if ( 0 == tracks || tracks -> empty() ) { return r ; }
     //
     r.reserve   ( tracks->size() ) ;
@@ -108,68 +109,68 @@ namespace
 }
 // ============================================================================
 LHCb::Track::ConstVector
-operator>> 
+operator>>
 ( const LHCb::Track::ConstVector&    input ,
   const LoKi::Types::TrCuts&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ============================================================================
 LHCb::Track::ConstVector
-operator>> 
+operator>>
 ( const LHCb::Track::Range&          input ,
-  const LoKi::Types::TrCuts&         cuts  ) 
+  const LoKi::Types::TrCuts&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ===========================================================================
 LHCb::Track::ConstVector
-operator>> 
+operator>>
 ( const SmartRefVector<LHCb::Track>& input ,
-  const LoKi::Types::TrCuts&         cuts  ) 
+  const LoKi::Types::TrCuts&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ===========================================================================
 LHCb::Track::ConstVector
-operator>> 
+operator>>
 ( const LHCb::Track::Container*      input ,
-  const LoKi::Types::TrCuts&         cuts  ) 
+  const LoKi::Types::TrCuts&         cuts  )
 { return _get_ ( input , cuts ) ; }
 // ===========================================================================
 LHCb::Track::ConstVector
-operator>> 
+operator>>
 ( const LHCb::Track::Selection*      input ,
-  const LoKi::Types::TrCuts&         cuts  ) 
+  const LoKi::Types::TrCuts&         cuts  )
 { return _get_ ( input , cuts ) ; }
 // ===========================================================================
-// functions 
+// functions
 // ===========================================================================
 std::vector<double>
-operator>> 
+operator>>
 ( const LHCb::Track::ConstVector&    input ,
-  const LoKi::Types::TrFunc&         cuts  ) 
+  const LoKi::Types::TrFunc&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ============================================================================
 std::vector<double>
-operator>> 
+operator>>
 ( const LHCb::Track::Range&          input ,
-  const LoKi::Types::TrFunc&         cuts  ) 
+  const LoKi::Types::TrFunc&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ===========================================================================
 std::vector<double>
-operator>> 
+operator>>
 ( const SmartRefVector<LHCb::Track>& input ,
-  const LoKi::Types::TrFunc&         cuts  ) 
+  const LoKi::Types::TrFunc&         cuts  )
 { return _get_ ( input.begin() , input.end() , cuts ) ; }
 // ===========================================================================
 std::vector<double>
-operator>> 
+operator>>
 ( const LHCb::Track::Container*      input ,
-  const LoKi::Types::TrFunc&         cuts  ) 
+  const LoKi::Types::TrFunc&         cuts  )
 { return _get_ ( input , cuts ) ; }
 // ===========================================================================
 std::vector<double>
-operator>> 
+operator>>
 ( const LHCb::Track::Selection*      input ,
-  const LoKi::Types::TrFunc&         cuts  ) 
+  const LoKi::Types::TrFunc&         cuts  )
 { return _get_ ( input , cuts ) ; }
 // ===========================================================================
 
 // ============================================================================
-// The END 
+// The END
 // ============================================================================
