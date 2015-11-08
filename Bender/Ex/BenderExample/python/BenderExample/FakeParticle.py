fe9adf0f9933d195bb150e2de5c65241fa35cbb1
Vanya Belyaev <ivan.belyaev@cern.ch>
2015-11-08 14:47:22 +0000

    add FakeParticle example and qmtest
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@197323 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 1508122979d046889d1531e969103d923660fb41

--- Bender/Ex/BenderExample/python/BenderExample/FakeParticle.py
+++ Bender/Ex/BenderExample/python/BenderExample/FakeParticle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # =============================================================================
-# $Id:$ 
+# $Id$ 
 # =============================================================================
 ## @file FakeParticle.py 
 #
@@ -25,9 +25,9 @@
 #  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
 #  @date 2015-10-31
 #
-#                    $Revision: 172885 $
-#  Last modification $Date: 2014-05-16 19:48:30 +0200 (Fri, 16 May 2014) $
-#                 by $Author: ibelyaev $
+#                    $Revision$
+#  Last modification $Date$
+#                 by $Author$
 # =============================================================================
 """Demonstration how to use ``Fake-H''-utility
 
@@ -52,14 +52,14 @@ By usage of this code one clearly states the disagreement
 with the smear campaign of Dr.O.Callot et al.: 
     ``No Vanya's lines are allowed in LHCb/Gaudi software.''
 
-                  $Revision: 172885 $
-Last modification $Date: 2014-05-16 19:48:30 +0200 (Fri, 16 May 2014) $
-               by $Author: ibelyaev $
+                  $Revision$
+Last modification $Date$
+               by $Author$
 """
 # =============================================================================
 __author__  = " Vanya BELYAEV Ivan.Belyaev@itep.ru "
 __date__    = " 2014-10-31" 
-__version__ = " $Revision:$"
+__version__ = " $Revision$"
 # ============================================================================= 
 ## import everything from bender 
 from Bender.Main               import *
