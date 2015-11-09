cd1d9d837999e4cfef4896b9d057e61824c758e3
Daniel Martin Saunders <daniel.saunders@cern.ch>
2015-11-09 09:37:22 +0000

    added tips for headerx_talk plots
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@197351 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: f0498a1b9961b437f42f110e6628c1152fead58f

--- Lovell/VeloAnalysisFramework/python/veloview/config/run_view.py
+++ Lovell/VeloAnalysisFramework/python/veloview/config/run_view.py
@@ -302,8 +302,15 @@ run_view_pages = OrderedDict([
                         {
                         'title': 'Noise difference between the first and the reference channel',
                         'name':  'Vetra/VeloBeetleHeaderXTalkCorrectionMoni/BHXT_DiffPlot_1D',
-                        'tip': ('The distribution should stay below 20. Any sensors whereby their ' +
-                               'number of noisy links extends 20 should be reported.'),
+                        'tip': ('This is a 1D summary plot of the noise difference between the ' +
+                                'first channel in each link and the reference channel. There should ' +
+                                'only be a small difference in ADC counts between the channels and the ' +
+                                'reference channel. Thus we usually get a broad distribution (usually ' +
+                                'centred somewhere between 0 and 1) with the majority of the entries ' +
+                                'lying somewhere between -2 and 2. If there are a large number of ' +
+                                'channels which differ significantly from the reference, i.e. entries ' +
+                                'with a high difference in ADC counts (greater/less than +/-3), then ' +
+                                'this should be reported.'),
                         'axis_titles': ['difference in ADC counts', 'number of channels']
                         },
                     ]
