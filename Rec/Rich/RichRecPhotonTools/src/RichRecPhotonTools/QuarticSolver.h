d7ebf9e02fc2fff84e6720db46e58f803e6ab8db
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2015-11-10 17:15:17 +0000

    Switch VectorClass Complex type on template parameter as well.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@197429 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 342fdc8c57ba902bc2087bd8a94f7b01a48d8830

--- Rec/Rich/RichRecPhotonTools/src/RichRecPhotonTools/QuarticSolver.h
+++ Rec/Rich/RichRecPhotonTools/src/RichRecPhotonTools/QuarticSolver.h
@@ -221,6 +221,7 @@ namespace Rich
 #ifdef __clang__
 
         // Implementation using STL classes
+
         const auto w1 = std::sqrt( std::complex<TYPE>(u1, u2) );
         const auto w2 = std::sqrt( std::complex<TYPE>(u1,-u2) );
         const auto  V = w1 * w2;
@@ -232,9 +233,13 @@ namespace Rich
 
         // Vectorised implementation using VectorClass
         // (Much) faster, but currently causes problems with clang
-        const Complex4f  W = sqrt( Complex4f(u1,u2,u1,-u2) );
+
+        using Complex4x = typename std::conditional<std::is_same<TYPE,float>::value,Complex4f,Complex4d>::type;
+        using Complex2x = typename std::conditional<std::is_same<TYPE,float>::value,Complex2f,Complex2d>::type;
+
+        const Complex4x  W = sqrt( Complex4f(u1,u2,u1,-u2) );
         const auto       V = W.get_low() * W.get_high();
-        const Complex2f w3 = ( abs(V) != 0.0 ? ( qq * -0.125f ) / V : Complex2f(0,0) );
+        const Complex2x w3 = ( abs(V) != 0.0 ? ( qq * -0.125f ) / V : Complex2x(0,0) );
         const TYPE     res = W.extract(0) + W.extract(2) + w3.extract(0) - (r4*a);
 
 #endif
