357f951cf34a6fc96e7bf01644443f35fd9b51a4
Mika Anton Vesterinen <mika.vesterinen@cern.ch>
2015-11-18 21:07:51 +0000

    Update the rate script to cover hlt2
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@197773 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: cf478c418926d8b362aa0f8325ef2e87a0ec507e

--- Moore/Hlt/Moore/tests/options/Moore_Rates.py
+++ Moore/Hlt/Moore/tests/options/Moore_Rates.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-#
-# Script to test independence of Hlt lines
+# Script to test the rates of a HLT configuration
+# Mika Vesterinen
 
 import os, sys, subprocess, re, optparse
 
@@ -9,54 +9,99 @@ import Gaudi.Configuration
 from Gaudi.Configuration import *
 from Configurables import GaudiSequencer as Sequence
 from Configurables import EventSelector, HltConf
-
 from Moore.Configuration import Moore
 
 # GaudiPython
 from GaudiPython import AppMgr
 
-def main():
+def getrate(scale,numer,denom):
+    eff = float(numer)/float(denom)
+    err = math.sqrt((eff-eff**2)/denom)
+    return [scale*eff,scale*err]
 
-    # Setup the option parser
-    usage = "usage: %prog [options] inputfile <inputfile>"
-    parser = optparse.OptionParser( usage = usage )
+def table_row(iLine,name,incl,excl):
+    print ('|%s|%s\t' %(iLine,name)).expandtabs(60),
+    print ('|%s\t|%s\t|' %(incl,excl)).expandtabs(20)
+
+### this was copied from the independence tests
+### I hoped that I could switch off some lines like this
+### but it doesn't work 
+### so I found another hackier solution...
+def setLines(desiredlines1,desiredlines2):
+    from HltLine.HltLine import hlt1Lines
+    lines1 = [ i for i in hlt1Lines() if  i.name() in desiredlines1 ]
+    Sequence( "Hlt1" ).Members = [ i.configurable() for i in lines1 ]
+    from HltLine.HltLine import hlt2Lines
+    lines2 = [ i for i in hlt2Lines() if  i.name() in desiredlines2 ]
+    Sequence( "Hlt2" ).Members = [ i.configurable() for i in lines2 ]
+    from HltConf.HltMonitoring import HltMonitoringConf
+    HltMonitoringConf().configureHltMonitoring(lines1, lines2)
+    Sequence( "HltEndSequence" ).Members = []        
+
+
+def main():
+    
+    parser = optparse.OptionParser( usage = "usage: %prog [options]" )
+
     parser.add_option( "-d", "--datatype", action="store", dest="DataType", 
                        default="2015", help="DataType to run on.")
+    
     parser.add_option( "-n", "--evtmax", type="int", action = "store", dest = "EvtMax",
                        default = 10000, help = "Number of events to run" )
+
     parser.add_option( "--dddbtag", action="store", dest="DDDBtag",
                        default='dddb-20150526', help="DDDBTag to use" )
+
     parser.add_option( "--conddbtag", action = "store", dest = "CondDBtag",
                        default = 'cond-20150617', help = "CondDBtag to use" )
+
     parser.add_option( "--settings", action = "store", dest="ThresholdSettings",
-                       default = 'Physics_25ns_October2015',
+                       default = 'Physics_25ns_September2015',
                        help = "ThresholdSettings to use")
-    parser.add_option( "-s", "--simulation", action = "store_true", dest = "Simulation",
+
+    parser.add_option( "--TCK", action = "store", dest="TCK",
+                       default = '',
+                       help = "HLT TCK. If unspecified,then run from settings")
+    
+    parser.add_option( "--simulation", action = "store_true", dest = "Simulation",
                        default = False, help = "Run on simulated data")
+
+    parser.add_option( "--input_rate",action="store",dest="input_rate",
+                       default=1.e6,help="Input rate from L0 in Hz")
+
+    parser.add_option( "--tuplefile",action="store",dest="tuplefile",
+                       default="",help="Output root file")
+
+    parser.add_option( "--inputdata",action="store",dest="inputdata",
+                       default="Physics1600EOS",help="Name of inputdata")
 
     # Parse the arguments
     (options, args) = parser.parse_args()
 
     #### configured from the arguments
+    input_rate = options.input_rate
     Moore().EvtMax = options.EvtMax
-    Moore().ThresholdSettings = options.ThresholdSettings
     Moore().DDDBtag   = options.DDDBtag
     Moore().CondDBtag = options.CondDBtag
     Moore().Simulation = options.Simulation
     Moore().DataType   = options.DataType
-    Moore().inputFiles = args
-
+    if options.TCK != "":
+        Moore().UseTCK = True
+        Moore().InitialTCK = options.TCK
+    else:
+        Moore().ThresholdSettings = options.ThresholdSettings
+        Moore().UseTCK = False
+    
     #### hard coded here
     Moore().ForceSingleL0Configuration = False
     Moore().OutputLevel = 6
     Moore().RemoveInputHltRawBanks = True
     Moore().Simulation = False
-    Moore().Split="Hlt1"
-    Moore().UseTCK = False
+    Moore().Split=""
     Moore().CheckOdin = False
     from Configurables import CondDB
     CondDB().IgnoreHeartBeat = True                                                                                  
-    CondDB().EnableRunChangeHandler = True                                                                              
+    CondDB().EnableRunChangeHandler = True                                                                    
     EventSelector().PrintFreq = 100
 
     #### configure raw data
@@ -66,86 +111,209 @@ def main():
     DataOnDemandSvc().AlgMap['Hlt/DecReports'] =  "HltDecReportsDecoder"
     
     from GaudiConf import IOHelper
-    IOHelper("MDF").inputFiles(["mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_0.mdf",
-                                "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_1.mdf",
-                                "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_2.mdf",
-                                "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_3.mdf",
-                                "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_4.mdf",
-                                "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_5.mdf"])
+    if options.inputdata == "Physics1600EOS":
+        IOHelper("MDF").inputFiles(["mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_0.mdf",
+                                    "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_1.mdf",
+                                    "mdf://root://eoslhcb.cern.ch//eos/lhcb/wg/HLT/BWdivData/Run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_2.mdf"])
+    elif options.inputdata == "Physics1600TestNode":
+        IOHelper("MDF").inputFiles(["/localdisk/bw_division/run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_0.mdf",
+                                    "/localdisk/bw_division/run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_1.mdf",
+                                    "/localdisk/bw_division/run164440_L0Filtered_0x00A2_Mika/2015NB_25ns_0x00A2_1.mdf"])
+    ### get the list of active line names
+    Lines = {}
+    for level in ["Hlt1","Hlt2"]:
+        Lines[level] = set()
+        for m in Sequence(level).Members:
+            Lines[level].add(m.name())
+
+    ### remove certain lines from the accounting
+    remove = set(["Hlt1MBNoBias",
+                  "Hlt2Transparent",
+                  "Hlt1Global",
+                  "Hlt2Global"])
+                  
+    Lines["Hlt1"] -= remove
+    Lines["Hlt2"] -= remove
+    ### this is what I would prefer to do...
+    #appendPostConfigAction( setLines(Lines["Hlt1"],Lines["Hlt2"]))
     
+    ### option to create a tuple with all of the decisions
+    if options.tuplefile != "":
+        from ROOT import (TTree, TFile)
+        from array import array
+        TF = TFile(options.tuplefile,"RECREATE")
+        DecMaps = {} ## dicto of branches for the TTrees
+        DecTrees = {} ## dicto of TTrees
+        for level in ["Hlt1","Hlt2"]:
+            DecTrees[level] = TTree('Tuple%s'%level,'Tuple%s'%level)
+            DecMaps[level] = {}
+            for l in Lines[level]:
+                DecMaps[level][l] = array( 'i', [ 0 ] )
+                DecTrees[level].Branch(l,DecMaps[level][l], '%sDecision/I'%l)
+    
+
+                
+    
+    ### this will be dictionary of lines and their counters for the rates
+    line_stats = {}
+    for line in Lines["Hlt1"].union(Lines["Hlt2"]).union(set(["Hlt1Global","Hlt2Global"])):
+        line_stats[line] = {"passed_incl":0,
+                            "passed_excl":0}
+    
+    ### counters for various regex
+    stream_stats = {"Turbo":{"filter":"Hlt2.(?!.*?TurboCalib).*Turbo"},
+                    "Turcal":{"filter":"Hlt2.*TurboCalib"},
+                    "Full":{"filter":"Hlt2.(?!.*?Turbo).(?!.*?TurboCalib)"}}
+    for k,v in stream_stats.iteritems():
+        v["pass_this_event"] = False
+        v["passed"] = 0
+
+    ### getting ready for the event loop
     gaudi = AppMgr(outputlevel=4)
-    gaudi.ExtSvc += ['ToolSvc']                                                                                         
+    gaudi.ExtSvc += ['ToolSvc']                                                                               
     gaudi.ExtSvc.append( 'DataOnDemandSvc' )    
     gaudi.initialize()
     evtmax = Moore().EvtMax
     i = 0
-
     processed = 0
-    line_stats = {}
+    #### start of the event loop
     while i < evtmax:
         i+=1
+        # run the sequences on this event
         gaudi.run(1)
-        try:
-            reps =  gaudi.evtsvc()['Hlt1/DecReports']
-            lines = reps.decReports().keys()
-            nPassed = 0
-            for line in lines:
-                if not line in line_stats.keys():
-                    line_stats[line] = {"passed_incl":0,
-                                        "passed_excl":0}
-                if reps.decReport(line).decision():
-                    line_stats[line]["passed_incl"]  += 1
-                    if not "Global" in line:
-                        nPassed +=1 
-            for line in lines:
-                if reps.decReport(line).decision() and nPassed == 1:
-                    if not "Global" in line:
-                        line_stats[line]["passed_excl"]  += 1
-            processed +=1
-        except:
-            print 'no more events?'
-            break
+        processed +=1
 
+        ### reset the stream counters
+        for s in stream_stats.keys():
+            stream_stats[s]["pass_this_event"] = False
+        
+        ### this is a nasty hack since I can't seem to switch
+        ### off some of the Hlt1 lines with a post config action
+        ### so I have to do my own book-keeping to see if one
+        ### of the good hlt1 lines fired.
+        ### I only consider events in Hlt2 if they pass my subset
+        ### of good hlt1 lines.
+        PassMyGlobal = {"Hlt1":False,
+                        "Hlt2":False}
+        
+        # loop over levels
+        for level in ["Hlt1","Hlt2"]:
+            if level == "Hlt2" and not PassMyGlobal["Hlt1"]: continue
+                
+            # do the dec reports exist?
+            # for Hlt2 this check does the job of telling us whether the event passed Hlt1
+            if gaudi.evtsvc()['%s/DecReports'%level]:
+                # get the dec reports
+                reps =  gaudi.evtsvc()['%s/DecReports'%level]
+                # get the list of lines from the reports
+                #lines = reps.decReports().keys()
+                # counter for how many hlt lines fired in this event
+                nPassed = 0
+                # loop over all lines
+                for line in Lines[level]:
+                    if reps.decReport(line).decision():PassMyGlobal[level] = True
+                        
+                    # does this event fire any lines that match my "streams"?
+                    if level == "Hlt2" and not line == "Hlt2Global" and not line in remove:
+                        for s in stream_stats.keys():
+                            if re.match(stream_stats[s]["filter"], line, flags=0):
+                                stream_stats[s]["pass_this_event"] = True
+                    
+                    # set the variable to be stored in the tuple
+                    if options.tuplefile != "":
+                        DecMaps[level][line] = reps.decReport(line).decision()
+                    # if this is the first fired event then 
+                    # need to initialise the dictionary entry
+                    if not line in line_stats.keys():
+                        line_stats[line] = {"passed_incl":0,
+                                            "passed_excl":0}
+                    # increment the counter for this line
+                    if reps.decReport(line).decision():
+                        line_stats[line]["passed_incl"]  += 1
+                        if not "Global" in line:
+                            nPassed +=1 
+                # my own global counter
+                if PassMyGlobal[level]:
+                    line_stats["%sGlobal"%level]["passed_incl"] += 1
+                # now go back and count the number of exclusive fires of this line
+                # just need to ignore HltXGlobal
+                for line in Lines[level]:
+                    if reps.decReport(line).decision() and nPassed == 1:
+                        if not "Global" in line:
+                            line_stats[line]["passed_excl"]  += 1
+                
+                # fill the tree
+                if options.tuplefile != "":
+                    DecTrees[level].Fill()
+
+                # stream accounting
+                for s in stream_stats.keys():
+                    if stream_stats[s]["pass_this_event"] == True:
+                        stream_stats[s]["passed"] +=1
+                    
+                
+    # write the root file
+    if options.tuplefile != "":
+        TF.Write()
+
+    # I was hoping that this would dump all of the gaudi related 
+    # printout before I print the summary that I'm interested in.
+    # Apparently this doesn't work, but it's only really a cosmetic thing.
+    #gaudi.finalize()
+
+    
+    #############################################
+    ###### print the summary tables #############
+    #############################################
     for k,v in line_stats.iteritems():
         v["processed"] = processed
+    for k,v in stream_stats.iteritems():
+        v["processed"] = processed
     
-    #import json
-    #with open('rates.json', 'w') as outfile:
-    #    json.dump(line_stats, outfile)
-    #with open('rates.json') as json_data:
-    #rates = json.load(json_data)
-    #json_data.close()
-    #pprint(rates)
-    
-    #################################
-    ###### print the summary 
-    #################################
     import math
-    iLine = 0
+    
+    GlobalRates = {}
+    print '-'*100
+    print 'HLT rates summary starts here'
+    print '-'*100
+    #### print the global rates
+    print 'removed lines: %s' %remove
+    for level in ['Hlt1','Hlt2']:
+        rate = getrate(1.e-3*input_rate,line_stats["%sGlobal"%level]["passed_incl"],line_stats["%sGlobal"%level]["processed"])
+        print '%sGlobal rate = (%s+-%s)kHz' %(level,rate[0],rate[1])
+    ### print the stream rates
+    for k,v in stream_stats.iteritems():
+        print '%s rate = %s kHz' %(k,v["passed"]/float(v["processed"]))
     #### order by inclusive rate
+    for level in ['Hlt1','Hlt2']:
+        iLine = -1 ## counter for line
+        table_row("**","*Line*","*Incl.*","*Excl.*")
+        OrderedStats = {}
+        for key, value in line_stats.iteritems():
+            if level in key:
+                OrderedStats[key] = value["passed_incl"]
+        for line_name, rate in sorted(OrderedStats.iteritems(), key=lambda (v,k): (k,v),reverse=True):
+            iLine += 1
+            rate_incl = getrate(1.e-3*input_rate,line_stats[line_name]["passed_incl"],line_stats[line_name]["processed"])
+            rate_excl = getrate(1.e-3*input_rate,line_stats[line_name]["passed_excl"],line_stats[line_name]["processed"])
+            if "Global" in line_name:
+                GlobalRates[level] = rate_incl
+            else:
+                table_row(iLine,line_name.replace("Decision",""),
+                          "%.2f+-%.2f" %(rate_incl[0],rate_incl[1]),
+                          "%.2f+-%.2f" %(rate_excl[0],rate_excl[1]))
+    
+    ## do we pass the test??
+    MaxRates = {"Hlt1":400.,
+                "Hlt2":40.}
+    for level in ['Hlt1','Hlt2']:
+        if GlobalRates[level][0] > MaxRates[level]:
+            'print %s rate = %s > max = %s' %(level,GlobalRates[level][0],MaxRates[level])
+        else:
+            print '%s rates OK' %level
     print '-'*100
-    print 'Hlt1 rates, inclusive, exclusive, both in kHz'
+    print 'HLT rates summary ends here'
     print '-'*100
-    OrderedStats = {}
-    for key, value in line_stats.iteritems():
-        OrderedStats[key] = value["passed_incl"]
-    for line_name, rate in sorted(OrderedStats.iteritems(), key=lambda (v,k): (k,v),reverse=True):
-        iLine += 1
-        eff_incl = line_stats[line_name]["passed_incl"]/float(line_stats[line_name]["processed"])
-        eff_excl = line_stats[line_name]["passed_excl"]/float(line_stats[line_name]["processed"])
-        if "Hlt1Global" in line_name:
-            GlobalRate = 1.e3*eff_incl
-        print ('%s:%s\t' %(iLine,line_name.replace("Decision",""))).expandtabs(40),
-        print ('%.2f+-%.2f\t' %(1.e3*eff_incl,1.e3*math.sqrt((eff_incl-eff_incl**2)/float(line_stats[line_name]["processed"])))).expandtabs(25),
-        print ('%.2f+-%.2f' %(1.e3*eff_excl,1.e3*math.sqrt((eff_excl-eff_excl**2)/float(line_stats[line_name]["processed"])))).expandtabs(25)
-
-    print '-'*100
-    ### give a large rate just to let things pass for now
-    if GlobalRate > 400:
-        print 'Hlt1Rate > 400 kHz!'
-    else:
-        print 'Hlt1 rates OK'
-    print '-'*100
-
+    
 if __name__ == "__main__":
     sys.exit( main() )
