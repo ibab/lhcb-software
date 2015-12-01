d05ee621d219b0536737e8258633e6e053da7dd2
Lennaert Bel <l.bel@cern.ch>
2015-12-01 13:01:57 +0000

    Add GRF filling cron job script.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lovell/trunk@198479 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: df563464e6211e6c68765159a4c3b698c44e04b0

---
+++ Lovell/VeloAnalysisFramework/scripts/cron_lovell_analyse_runs.bash
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+# for debugging
+# set -o xtrace
+
+# setup
+declare script=$(readlink -e $BASH_SOURCE)
+declare srcdir="${script%/*}"
+
+# save job arguments
+declare jobargs="$@"
+shift $#
+
+# initialise environment; LbLogin et al.
+source /cvmfs/lhcb.cern.ch/group_login.sh &> /dev/null
+echo ${LBSCRIPTS_HOME}
+
+# set resource limits
+ulimit -Sm 2500000
+ulimit -Sv 2500000
+echo "Running on:" $HOSTNAME
+
+# run job
+exec cat /calib/velo/dqm/VeloView/VetraOutput/RunList_Merged_NZS_Clusters_Brunel.txt | xargs /calib/velo/dqm/lhcb-prerelease/Today/LOVELL/LOVELL_HEAD/build.x86_64-slc6-gcc48-opt/run python /calib/velo/dqm/lhcb-prerelease/Today/LOVELL/LOVELL_HEAD/VeloAnalysisFramework/scripts/analyse_run.py
