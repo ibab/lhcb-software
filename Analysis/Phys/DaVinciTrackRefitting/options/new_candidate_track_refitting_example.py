f49010abfbb4d1e726005886f8dd81158e243dd4
Paul Seyfert <paul.seyfert@mib.infn.it>
2015-12-04 17:43:23 +0000

    updated options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Analysis/trunk@198726 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a95cfc657a09563234ce3113a511bca74e53aa9c

--- Analysis/Phys/DaVinciTrackRefitting/options/new_candidate_track_refitting_example.py
+++ Analysis/Phys/DaVinciTrackRefitting/options/new_candidate_track_refitting_example.py
@@ -1,31 +1,33 @@
-from Gaudi.Configuration import *
-
-MessageSvc().Format = "% F%90W%S%7W%R%T %0W%M"
 ##### Stream CONFIGURATION 
-MyStream = "Dimuon"
+MyStream = "Leptonic"
+is2015 = True
+fitasin = "2015" # "2015","EM2015","Reco14"
+DaVinci().InputType = "MDST"
+inputs = ['Phys/B2XMuMu_Line/Particles']
+
+
 
 ##### DAVINCI CONFIGURATION
 
 from Configurables import DaVinci, RefitParticleTracks
-DaVinci().EvtMax = 400
-DaVinci().PrintFreq = 400
-DaVinci().DataType = "2015"
-if MDST:
-   DaVinci().InputType = "MDST"
-   DaVinci().RootInTES = MyStream
+DaVinci().EvtMax = 600
+DaVinci().PrintFreq = 200
+if is2015:
+   DaVinci().DataType = "2015"
 else:
-   DaVinci().InputType = "DST"
+   DaVinci().DataType = "2012"
+if DaVinci().InputType = "MDST":
+   DaVinci().RootInTES = MyStream
 DaVinci().Simulation = False
 DaVinci().SkipEvents = 0
 
 #### the actual algorithm
 from DaVinciTrackRefitting import TrackRefitting
 refitter = TrackRefitting.TrackRefitting("refittingalg",
-                                          fitAsIn="2015",
-                                            # possible: "2015","EM2015","Reco14"
-                                          Stripping23=True,
-					  RootInTES = "Dimuon", # Also needed on FullDST!
-					  Inputs = ['Phys/StdLooseKsLL/Particles','Phys/StdLooseKsDD/Particles'] 
+                                          fitAsIn=fitasin,
+                                          Stripping23=is2015,
+					  RootInTES = MyStream, # Also needed on FullDST!
+					  Inputs = inputs
                                           ).fitter()
 
 DaVinci().UserAlgorithms += [ refitter ] 
@@ -38,7 +40,10 @@ DaVinci().UserAlgorithms += [ refitter ]
 from Gaudi.Configuration import *
 from GaudiConf import IOHelper
 IOHelper('ROOT').inputFiles([
-'root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/LHCb/Collision15/DIMUON.DST/00048460/0000/00048460_00000415_1.dimuon.dst'
+#'root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/LHCb/Collision15/DIMUON.DST/00048460/0000/00048460_00000415_1.dimuon.dst'
+'root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/LHCb/Collision15/LEPTONIC.MDST/00048279/0000/00048279_00001457_1.leptonic.mdst'          ## 'Phys/B2XMuMu_Line/Particles'
+#'root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/LHCb/Collision12/SEMILEPTONIC.DST/00041834/0000/00041834_00000332_1.semileptonic.dst'     ## 'Phys/PiforB2DMuNuX/Particles','Semileptonic/Phys/B2XuMuNuBs2KLine/Particles'
+#'root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/LHCb/Collision12/CHARM.MDST/00041834/0000/00041834_00000774_1.charm.mdst'                ## 'Phys/DstarPromptWithD02HHHHLine/Particles'
 ], clear=True)
 
 
