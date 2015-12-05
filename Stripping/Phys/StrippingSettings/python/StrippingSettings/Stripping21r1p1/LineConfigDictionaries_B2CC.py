ea627034d2a28a3e85a473259e4ddd683e73de86
Carlos Vazquez Sierra <carlos.vazquez@cern.ch>
2015-12-05 19:08:22 +0000

    Added dictionaries for s21p1 B2CC lines
    git-svn-id: http://svn.cern.ch/guest/lhcb/Stripping/trunk@198754 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 637137212044b64d77e4b08c32b2080edf6ca932

---
+++ Stripping/Phys/StrippingSettings/python/StrippingSettings/Stripping21r1p1/LineConfigDictionaries_B2CC.py
@@ -0,0 +1,70 @@
+################################################################################
+##                     S T R I P P I N G  2 1 r 1 p 1                         ##
+##                                                                            ##
+##  Configuration for B2CC WG                                                 ##
+##  Contact person: Carlos Vazquez Sierra (carlos.vazquez@cern.ch)            ##
+################################################################################
+
+######################################################################
+## StrippingBetaSBd2JpsiPi0PrescaledLine (MDST.DST)
+## StrippingBetaSBd2JpsiPi0DetachedLine (MDST.DST)
+## -------------------------------------------------------------------
+## Lines defined in StrippingB2JpsiPi0.py
+## Authors: Maximilien Chefdeville
+######################################################################
+
+BetaSPi0 = {
+    'BUILDERTYPE' : 'B2JpsiPi0Conf',
+    'CONFIG'      : { 'BPVLTIME'             :       0.2
+                    , 'VCHI2PDOF'            :       10
+                    , 'JpsiMassWindow'       :       80
+                    , 'HLTCuts'              :       "HLT_PASS_RE('Hlt2DiMuonJPsiDecision')"
+                    , 'Bs2JpsiPi0Prescale'   :       1.0
+                    },
+    'STREAMS'     : [ 'Leptonic' ],
+    'WGs'         : [ 'B2CC' ]
+    }
+
+######################################################################
+## StrippingBd2JpsieeKSBd2JpsieeKSFromTracksPrescaledLine (MDST.DST)
+## StrippingBd2JpsieeKSBd2JpsieeKSFromTracksDetachedLine (MDST.DST)
+## StrippingBd2JpsieeKSBd2JpsieeKSDetachedLine (MDST.DST)
+## StrippingBd2JpsieeKSBd2JpsieeKstarFromTracksPrescaledLine (MDST.DST)
+## StrippingBd2JpsieeKSBd2JpsieeKstarFromTracksDetachedLine (MDST.DST)
+## StrippingBd2JpsieeKSBu2JpsieeKFromTracksDetachedLine (MDST.DST)
+## -------------------------------------------------------------------
+## Lines defined in StrippingBd2JpsieeKS.py
+## Author: Ramon Niet
+######################################################################
+
+Bd2JpsieeKS = {
+    'BUILDERTYPE'       : 'Bd2JpsieeKSConf',
+    'CONFIG'    : {
+          'BPVLTIME'              :     0.2   # ps
+        , 'ElectronPT'            :   500.    # MeV
+        , 'ElectronPID'           :     0.    # adimensional
+        , 'ElectronTrackCHI2pDOF' :     5.    # adimensional
+        , 'KaonPID'               :     0.    # adimensional
+        , 'PionForKstarPT'        :   600.    # MeV
+        , 'TRCHI2DOF'             :     3.    # adimensional
+        , 'JpsiVertexCHI2pDOF'    :    15.    # adimensional
+        , 'JpsiMassMin'           :  2300.    # MeV
+        , 'JpsiMassMax'           :  3300.    # MeV
+        , 'KstarMassMin'          :   826.    # MeV
+        , 'KstarMassMax'          :   966.    # MeV
+        , 'KstarPT'               :  1500.    # MeV
+        , 'KstarVCHI2'            :    20.    # adimensional
+        , 'KplusPT'               :   500.    # MeV
+        , 'KSVCHI2'               :    20.    # adimensional
+        , 'KSBPVDLS'              :     5.    # adimensional
+        , 'BdVertexCHI2pDOF'      :     7.    # adimensional
+        , 'BdFromKstarVCHI2pDOF'  :    20.    # adimensional
+        , 'BdMassMin'             :  4400.    # MeV
+        , 'BdMassMax'             :  6000.    # MeV
+        , 'Prescale'              :     0.1   # adamenssional
+        , 'Bd2JpsieeKstarPrescale':     0.1   # adimensional
+        , 'Bu2JpsieeKPrescale'    :     0.1   # adimensional
+        },
+    'STREAMS' : [ 'Leptonic' ],
+    'WGs'    : [ 'B2CC' ]
+    }
