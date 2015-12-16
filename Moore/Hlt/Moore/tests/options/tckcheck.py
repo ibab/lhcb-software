d40e1fe0f42f95c2eec1934c44db2737a478f601
Rosen Matev <rosen.matev@cern.ch>
2015-12-16 22:20:24 +0000

    Improvements to tests
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@199134 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 4a7a8889a0315cc245ead0bbe2b3ec345dbf77e3

--- Moore/Hlt/Moore/tests/options/tckcheck.py
+++ Moore/Hlt/Moore/tests/options/tckcheck.py
@@ -1,14 +1,13 @@
+import os
+from TCKUtils.utils import *
+
 TCK1=0x00ed0046
 TCK2=0x00ee0046
-#TCK1=0x00990046
 TCK3=0x00ef0046
 configdest="../../../../InstallArea/$CMTCONFIG"
 
-from TCKUtils.utils import *
-from Configurables import ConfigCDBAccessSvc as ctas
-ctas(File=os.sep.join([configdest,"config.cdb"]))
+ConfigAccessSvc().File = os.path.join(configdest, "config.cdb")
 print "============= diffs zero->1 event ==============="
 diff(TCK1,TCK2)
 print "============= diffs 1->100 events ==============="
 diff(TCK2,TCK3)
-
