dff7eac5afedbdf81c1e46282a1187b38e14cc85
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-01-06 21:21:13 +0000

    DetectorSegment: classify as final and remove virtual destructor
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@199433 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 2553bc4599b22ec12e13cdd4f41b2812408b24fc

--- LHCb/Kernel/LHCbKernel/Kernel/DetectorSegment.h
+++ LHCb/Kernel/LHCbKernel/Kernel/DetectorSegment.h
@@ -1,4 +1,3 @@
-// $Id: $
 #ifndef KERNEL_DETECTORSEGMENT_H 
 #define KERNEL_DETECTORSEGMENT_H 1
 
@@ -10,56 +9,45 @@
  *  @author Olivier Callot
  *  @date   2012-03-16
  */
-class DetectorSegment {
+class DetectorSegment final {
 public: 
   /// Standard constructor
-  DetectorSegment( ) {
-    m_x0   = 0.;
-    m_z0   = 0.;
-    m_dxDy = 0.;
-    m_dzDy = 0.;
-    m_yMin = 0.;
-    m_yMax = 0.;
-  }
+  DetectorSegment( ) = default;
   /// Contstructor with the necessary information
-  DetectorSegment( float x0, float z0, float dxDy, float dzDy, float yMin, float yMax ) {
-    m_x0   = x0;
-    m_z0   = z0;
-    m_dxDy = dxDy;
-    m_dzDy = dzDy;
-    m_yMin = yMin;
-    m_yMax = yMax;
+  DetectorSegment( float x0, float z0, float dxDy, float dzDy, float yMin, float yMax ) :
+    m_x0   { x0 },
+    m_z0   { z0 },
+    m_dxDy { dxDy },
+    m_dzDy { dzDy },
+    m_yMin { yMin },
+    m_yMax { yMax }
+  {
     if ( m_yMin > m_yMax ) {
       m_yMin = yMax;
       m_yMax = yMin;
     }
   }  
 
-  virtual ~DetectorSegment( ) {}; ///< Destructor
-
   float x( float y ) const { return m_x0 + y * m_dxDy; }
   float z( float y ) const { return m_z0 + y * m_dzDy; }
 
-  float xMin() const { return m_dxDy > 0 ? x(m_yMin) : x(m_yMax); }
-  float xMax() const { return m_dxDy > 0 ? x(m_yMax) : x(m_yMin); }
+  float xMin() const { return x( m_dxDy > 0 ? m_yMin : m_yMax ); }
+  float xMax() const { return x( m_dxDy > 0 ? m_yMax : m_yMin ); }
 
   float yMin() const { return m_yMin; }
   float yMax() const { return m_yMax; }
 
   /// returns the y along the trajectory y = y0 + z * dzDy
-  float yOnTrack( float y0, float dyDz ) {
+  float yOnTrack( float y0, float dyDz ) const {
     return ( y0 + dyDz * m_z0 ) / ( 1. - dyDz * m_dzDy );
   }
   
-
-protected:
-
 private:
-  float m_x0;    ///< x coordinate at y = 0
-  float m_z0;    ///< z coordinate at y = 0
-  float m_dxDy;  ///< Slope x vs y, typically 0 for x layers
-  float m_dzDy;  ///< Slope z vs y, as detectors are vertical while Z axis is not horizontal
-  float m_yMin;  ///< minimum y coordinate along this segment
-  float m_yMax;  ///< maximum y coordinate along this segment
+  float m_x0=0.;    ///< x coordinate at y = 0
+  float m_z0=0.;    ///< z coordinate at y = 0
+  float m_dxDy=0.;  ///< Slope x vs y, typically 0 for x layers
+  float m_dzDy=0.;  ///< Slope z vs y, as detectors are vertical while Z axis is not horizontal
+  float m_yMin=0.;  ///< minimum y coordinate along this segment
+  float m_yMax=0.;  ///< maximum y coordinate along this segment
 };
 #endif // KERNEL_DETECTORSEGMENT_H
