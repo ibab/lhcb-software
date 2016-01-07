727a51fc97fcc6e0d4f8c8cfac7e7d84afd2c58e
Rosen Matev <rosen.matev@cern.ch>
2016-01-07 17:34:06 +0000

    A little friendlier Moore_2016draft.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Moore/trunk@199468 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 63c03bea128bd914a33da629479aee109399bac3

--- Moore/Hlt/Moore/tests/options/Moore_2016draft.py
+++ Moore/Hlt/Moore/tests/options/Moore_2016draft.py
@@ -1,16 +1,27 @@
-import Gaudi.Configuration
-from Moore.Configuration import Moore
+from Gaudi.Configuration import *
 
+from Configurables import Moore
 Moore().ThresholdSettings = "Physics_pp_Draft2016"
 Moore().UseTCK = False
 Moore().ForceSingleL0Configuration = False
-Moore().EvtMax = 1200
+Moore().Split = ''
+Moore().EnableTimer = True
+# Moore().OutputLevel = INFO
 
 from Configurables import LHCbTimingAuditor, LHCbSequencerTimerTool
-LHCbTimingAuditor("TIMING").addTool(LHCbSequencerTimerTool,"TIMING")
+LHCbTimingAuditor("TIMING").addTool(LHCbSequencerTimerTool, "TIMING")
 #normalize the timing table in tests
 LHCbTimingAuditor("TIMING").TIMING.Normalised = True
 
 from PRConfig import TestFileDB
 TestFileDB.test_file_db["2015NB_25ns_L0Filt0x0050"].run(configurable=Moore())
 Moore().RemoveInputHltRawBanks = True
+
+Moore().EvtMax = 1200
+
+from Configurables import EventSelector
+EventSelector().PrintFreq = 100
+
+# Add your new HLT2 lines here to run them
+# from Configurables import HltConf
+# HltConf().AdditionalHlt2Lines = ['Hlt2MyNewLineA', 'Hlt2MyNewLineB']
