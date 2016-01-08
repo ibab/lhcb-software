79168e080a9a1f13e285a5c4ecc1d5f308d0ab40
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-01-08 12:11:02 +0000

    major change: prefer public extend_interfaces<IInterface>  over virtual public IInterface
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@199492 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 567a745f8230c1d05b7973bedb8587147049ef78

--- LHCb/Det/DetDesc/DetDesc/IDetElemFinder.h
+++ LHCb/Det/DetDesc/DetDesc/IDetElemFinder.h
@@ -1,4 +1,3 @@
-// $Id: IDetElemFinder.h,v 1.1 2008-07-21 14:51:45 cattanem Exp $
 #ifndef KERNEL_IDETELEMFINDER_H 
 #define KERNEL_IDETELEMFINDER_H 1
 
@@ -11,7 +10,6 @@
 
 class IDetectorElement;
 
-static const InterfaceID IID_IDetElemFinder ( "IDetElemFinder", 1, 0 );
 
 /** @class IDetElemFinder IDetElemFinder.h Kernel/IDetElemFinder.h
  *  
@@ -21,19 +19,16 @@ static const InterfaceID IID_IDetElemFinder ( "IDetElemFinder", 1, 0 );
  *  @author Marco Clemencic
  *  @date   2006-09-01
  */
-class IDetElemFinder : virtual public IInterface {
+class IDetElemFinder : public extend_interfaces<IInterface> {
 public: 
 
   /// Return the interface ID
-  static const InterfaceID& interfaceID() { return IID_IDetElemFinder; }  
+  DeclareInterfaceID( IDetElemFinder, 2, 0 );
 
   /// Find the detector element instance associated to a given physical volume path.
   /// Returns 0 if there is no suitable detector element.
   virtual const IDetectorElement * detectorElementForPath(const std::string &path) const = 0;
 
-protected:
-
-private:
 
 };
 #endif // KERNEL_IDETELEMFINDER_H
