375e708a953686219d439b00aeba68bbb30efbbc
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-01-08 10:02:57 +0000

    major change: make IMuonFastPosToo::calcTilePos const, use extend_interfaces     x requires matching versions of MuonTrackRec and MuonTools
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@199477 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5dd99bc01bcaa38c7480534981461d78e09c7ce8

--- LHCb/Det/MuonDet/MuonDet/IMuonFastPosTool.h
+++ LHCb/Det/MuonDet/MuonDet/IMuonFastPosTool.h
@@ -1,4 +1,3 @@
-// $Id: IMuonFastPosTool.h,v 1.1 2009-04-29 14:11:20 asatta Exp $
 #ifndef MUONDET_IMUONFASTPOSTOOL_H 
 #define MUONDET_IMUONFASTPOSTOOL_H 1
 
@@ -15,18 +14,11 @@
  *  @author David Hutchcroft
  *  @date   11/03/2002
  */
-static const InterfaceID IID_IMuonFastPosTool( "IMuonFastPosTool" , 4 , 0 
-);
 
-class IMuonFastPosTool : public virtual IAlgTool {
+class IMuonFastPosTool : public extend_interfaces<IAlgTool> {
 public:
 
-  /** static interface identification
-   *  @return unique interface identifier
-   */
-  static const InterfaceID& interfaceID() { 
-    return  IID_IMuonFastPosTool; 
-  }
+  DeclareInterfaceID( IMuonFastPosTool , 5 , 0 );
   
   /** Calculate the x,y,z and dx,dy,dz of a MuonTileID in mm
    *  this ignores gaps: these can never be read out independently
@@ -34,11 +26,8 @@ public:
   virtual StatusCode calcTilePos(const LHCb::MuonTileID& tile, 
                                  double& x, double& deltax,
                                  double& y, double& deltay,
-                                 double& z, double& deltaz) = 0;
+                                 double& z, double& deltaz) const = 0;
 
-protected:
-
-private:
 
 };
 #endif // MUONDET_IMUONFASTPOSTOOL_H
