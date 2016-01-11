e86de49abc33316d133f2ff047d2cd1ea1b82953
Elena Graverini <elena.graverini@cern.ch>
2016-01-11 12:47:23 +0000

    Add option files for the analysis of ST CCE scans
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@199519 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: da43eb172896e6f1b69231ce781e803d0968244d

---
+++ Rec/Tr/TrackMonitors/options/TTAgeing.py
@@ -0,0 +1,97 @@
+##############################################################################
+#                                                                            #
+#  Configuration for ST Aging CCE scan analysis for TT                       #
+#                                                                            #
+#  @package Tr/TrackMonitors                                                 #
+#  @author  Ch. Elsasser, E. Graverini                                       #
+#  @date    15/06/2013                                                       #
+#                                                                            #
+##############################################################################
+
+
+from GaudiKernel.ProcessJobOptions import importOptions
+
+from Configurables import GaudiSequencer
+from TrackSys.Configuration import *
+from Configurables import ( TrackSys, RecSysConf )
+from Configurables import TrackContainerCopy
+from Configurables import Velo__VeloTrackMonitor
+from Configurables import EventClockSvc
+from Configurables import Vetra
+from Configurables import VetraRecoConf
+from Configurables import ST__STDumpADCs
+ 
+
+
+generalOutputLevel = 3
+
+Vetra().MainSequence = [ 'ProcessPhase/Reco', GaudiSequencer('MoniTTSeq'), GaudiSequencer('MoniSTSeq')]
+
+
+TrackSys().ExcludedLayers = ["TTaU"]
+#TrackSys().TrackPatRecAlgorithms = TrackSys().DefaultPatRecAlgorithmsRun2
+#VetraRecoConf().Sequence = [ "Decoding" ] + RecSysConf().DefaultTrackingSubdets # = ["Decoding", "VELO","TT","IT","OT","Tr","Vertex"]
+VetraRecoConf().Sequence = ["Decoding", "VELO","TT","IT","OT","TrHLT1", "Vertex", "TrHLT2" ]
+VetraRecoConf().TrackPatRecAlgorithms = TrackSys().DefaultPatRecAlgorithmsRun2
+
+print '----------------------------------------------------------------------------------------------------\n\n\n\n\n'
+print TrackSys().DefaultPatRecAlgorithms
+print RecSysConf().DefaultTrackingSubdets
+print '----------------------------------------------------------------------------------------------------\n\n\n\n\n'
+
+from Configurables import CondDB, CondDBAccessSvc, CondDBTimeSwitchSvc
+connection="sqlite_file:$STSQLDDDBROOT/db/STCOND.db/COND"
+CondDB().addLayer( CondDBAccessSvc("COND", ConnectionString = connection) )
+CondDB().IgnoreHeartBeat = True
+CondDB().EnableRunStampCheck = False
+
+
+#importOptions('$HOME/cmtuser/Vetra_v15r0/ST/STVetraAnalysis/options/TTEmulator.py')
+#importOptions('$STVETRAANALYSISROOT/options/TTEmulator.py')
+importOptions('$STTELL1ALGORITHMSROOT/options/TTEmulator.py')
+
+from Configurables import STNZSResolution, STADCTrackMonitor
+
+#ODINChecker = STODINCheck()
+#ODINChecker.OutputLevel = generalOutputLevel
+#ODINChecker.ODINData = "DAQ/ODIN";
+
+#GaudiSequencer('ODINPreChecker').Members = [ODINChecker]
+
+ttRes = STNZSResolution("TTResolution")
+ttRes.OutputLevel = 4
+ttRes.DetType = "TT"
+ttRes.UseNZSdata = True
+ttRes.InputData =  "/Event/Raw/TT/LCMSADCs";
+
+TrackMonitor = STADCTrackMonitor()
+TrackMonitor.OutputLevel = generalOutputLevel
+TrackMonitor.DetType = "TT"
+TrackMonitor.RawDataCent = "/Event/Raw/TT/LCMSADCs";
+TrackMonitor.ODINData = "DAQ/ODIN";
+TrackMonitor.Layer   = "TTaU"
+TrackMonitor.MaxNeighbours = 3
+
+
+
+GaudiSequencer('MoniTTSeq').Members = [ GaudiSequencer('EmuTTSeq'), GaudiSequencer('EmuTTSeqPrev'), GaudiSequencer('EmuTTSeqNext'),TrackMonitor]
+GaudiSequencer('MoniTTSeq').IgnoreFilterPassed = True#ReturnOK = True
+
+histoname = "Histo.root"
+tuplename = "Tuple.root"
+
+Vetra().EvtMax =         -1
+#Vetra().OutputLevel =    INFO
+Vetra().HistogramFile =  histoname
+Vetra().TupleFile =      tuplename
+Vetra().DataType = '2015'
+vetra = Vetra()
+
+def doMyChanges():
+    from Configurables import VetraInit, FilterByBankType
+    bankFilter = FilterByBankType()
+    bankFilter.BankNames = [ "TTFull", "ITFull" ]
+    from Configurables import LoKi__ODINFilter
+    odinFilter = LoKi__ODINFilter('BBODINFilter', Code = " LHCb.ODIN.BeamCrossing == ODIN_BXTYP ")
+    GaudiSequencer( 'Init' ).Members = [   VetraInit(), odinFilter, bankFilter   ]
+appendPostConfigAction(doMyChanges)
---
+++ Rec/Tr/TrackMonitors/options/ganga/STAgeingSubmit.py
@@ -0,0 +1,106 @@
+##############################################################################
+#                                                                            #
+#  Ganga submission script to extract CCE scans for IT and TT                #
+#                                                                            #
+#  @package Tr/TrackMonitors                                                 #
+#  @author  Ch. Elsasser, E. Graverini                                       #
+#  @date    15/06/2013                                                       #
+#                                                                            #
+##############################################################################
+
+# Insert your email address.
+# Leave empty if no notifications are desired
+notify_address = ''
+
+# List runs belonging to fills
+runDict = {
+    4643 : [168257],                                                # 2015-11-22 900k evt/step
+    4638 : [168092, 168139],                                        # 2015-11-20 500k evt/step. Actually 2nd run is in fill 4639!
+    4518 : [166262],                                                # 2015-10-20 Bad (OT was off)
+    3960 : [156887],                                                # 2015-07-05 Bad (OT was off)
+    3820 : [153583, 153584, 153586, 153587, 153601, 153602],        # 2015-06-03 Bad (some steps are missing)
+    3478 : [135603, 135604, 135605],                                # 2013-01-21
+    3108 : [129494, 129518, 129519],                                # 2012-09-28
+    2797 : [120006, 120009, 120010],                                # 2012-07-02
+    2472 : [111267, 111270, 111271],                                # 2012-04-05
+    2252 : [104090, 104091, 104092, 104093, 104106, 104107],        # 2011-10-25
+    2083 : [101357, 101359],                                        # 2011-09-07
+    1944 : [ 95930,  95932,  95933,  95934,  95936],                # 2011-07-14
+    1616 : [ 87144,  87146,  87147,  87151,  87152,  87153],        # 2011-03-14
+    1020 : [ 69505,  69506,  69507,  69508,  69509,  69510,
+                     69511,  69513,  69517,  69521,  69525,  69526,
+                     69528,  69531,  69555,  69556,  69558,  69570,
+                     69572,  69573,  69574,  69575,  69576,  69577,
+                     69578,  69579,  69580,  69583,  69586,
+                     69589,  69592,  69593,  69594,  69595,  69596,
+                     69597,  69599,  69602,  69603,  69604,  69605,
+                     69606,  69607,  69608,  69610,  69611],        # 2010-04-04
+    0    : [129494]
+}
+
+# FOR NEW DATA:
+# just update the run dictionary and run for ONE fill at a time
+fills = []
+
+print "=============================== SUBMITTING TT ==============================="
+for k in fills:
+    runs = runDict[k]
+    print "====================================="
+    print "Fill: " + str(k)
+    for i in runs:
+        print "====================================="
+        j = Job(application='Vetra', name="TT-" + str(i))
+        print "Run: " + str(i)
+        j.application.user_release_area = os.path.expandvars('$User_release_area')
+        j.application.optsfile = os.path.expandvars('$User_release_area') + "/Vetra_v16r0/Tr/TrackMonitors/options/TTAgeing.py"
+        j.application.version = 'v16r0'
+        bkq = BKQuery(type='Run', dqflag='All')
+        bkq.path = "/" + str(i) + "/Real Data/90000001/RAW"
+        print "BK Query path: " + bkq.path
+        j.inputdata = bkq.getDataset()
+        print "Input data contain " + str(len(j.inputdata.getFileNames())) + " file(s)"
+        j.backend = Dirac()
+        j.backend.settings['CPUTime'] = 2 * 172800
+        j.splitter = SplitByFiles(filesPerJob=5,
+                                  ignoremissing=True,
+                                  bulksubmit=True)
+        j.outputfiles = [DiracFile('Tuple.root')]
+        if notify_address:
+            j.postprocessors = [Notifier(address=notify_address)]
+        j.submit()
+        print "Job submitted!"
+        print "====================================="
+        print ""
+    print ""
+
+
+print "=============================== SUBMITTING IT ==============================="
+for k in fills:
+    runs = runDict[k]
+    print "====================================="
+    print "Fill: " + str(k)
+    for i in runs:
+        print "====================================="
+        j = Job(application='Vetra', name="IT-" + str(i))
+        print "Run: " + str(i)
+        j.application.user_release_area = os.path.expandvars('$User_release_area')
+        j.application.optsfile = os.path.expandvars('$User_release_area') + "/Vetra_v16r0/Tr/TrackMonitors/options/ITAgeing.py"
+        j.application.version = 'v16r0'
+        bkq = BKQuery(type='Run', dqflag='All')
+        bkq.path = "/" + str(i) + "/Real Data/90000001/RAW"
+        print "BK Query path: " + bkq.path
+        j.inputdata = bkq.getDataset()
+        print "Input data contain " + str(len(j.inputdata.getFileNames())) + " file(s)"
+        j.backend = Dirac()
+        j.backend.settings['CPUTime'] = 2 * 172800
+        j.splitter = SplitByFiles(filesPerJob=5,
+                                  ignoremissing=True,
+                                  bulksubmit=True)
+        j.outputfiles = [DiracFile('Tuple.root')]
+        if notify_address:
+            j.postprocessors = [Notifier(address=notify_address)]
+        j.submit()
+        print "Job submitted!"
+        print "====================================="
+        print ""
+    print ""
