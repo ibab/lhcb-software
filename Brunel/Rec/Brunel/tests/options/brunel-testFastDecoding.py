032491e5a8ba007b89e304d6d84c74f099f0338b
Michel De Cian <michel.de.cian@cern.ch>
2016-02-03 14:31:55 +0000

    Adapted FastDecoding test to run over 20 events only, remove err reference, add empty ref file
    git-svn-id: http://svn.cern.ch/guest/lhcb/Brunel/trunk@200844 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6dc98856ea8b481fada02935cef17faab53447c7

--- Brunel/Rec/Brunel/tests/options/brunel-testFastDecoding.py
+++ Brunel/Rec/Brunel/tests/options/brunel-testFastDecoding.py
@@ -2,7 +2,7 @@ from Gaudi.Configuration import *
 from Configurables import Brunel
 from PRConfig import TestFileDB
 TestFileDB.test_file_db['2015_raw_full'].run(configurable=Brunel())
-Brunel().EvtMax = 2000
+Brunel().EvtMax = 20
 Brunel().DatasetName="2015magdown"
 Brunel().Monitors=["SC","FPE"]
 
