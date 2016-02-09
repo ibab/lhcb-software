8dc8cc0eb2612ba882076670306cd6842ee6b806
Vanya Belyaev <ivan.belyaev@cern.ch>
2016-02-09 13:02:47 +0000

    - add qmtest for TURBO.MDST
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@201186 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 69c1aa7c474196c3f50dd045c591eee29f14dd07

--- Bender/Phys/BenderTools/tests/turbo.py
+++ Bender/Phys/BenderTools/tests/turbo.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env bender 
 # -*- coding: utf-8 -*-
 # =============================================================================
-# $Id: PyRoUts.py 200161 2016-01-24 14:41:23Z ibelyaev $
+# $Id:$
 # =============================================================================
 ## @file
-#  very simple "bender" scripts for batch processing 
+#  very simple "bender" scripts, read TURBO stream  
 #
 #  @author Vanya BELYAEV Ivan.Belyaev@itep.ru
-#  @date   2016-01-27
+#  @date   2016-02-09
 #  
 #                    $Revision: 200161 $
 #  Last modification $Date: 2016-01-24 15:41:23 +0100 (Sun, 24 Jan 2016) $
@@ -19,29 +19,13 @@ __date__    = "2011-06-07"
 # =============================================================================
 
 
-t = nTuple('MyTuple','Tuple')
-h = book  ('path/to/my/histo', ## path in Histogram Transient Store
-           'this it title '  , ## histogram title
-           100               , ## number of bins
-           0                 , ## low edge
-           100               ) ## high edge
-
-print 'TUPLE TYPE:', type(t)
-## event loop 
-for i in range(20) :
+for i in  range(10) :
     
+    data, nevt = seekForData('/Event/Turbo/Hlt2CharmHadDpToKpPimPipTurbo/Particles') 
+    if data :
+        print data
     run(1)
-    
-    if  0 == i%10 : print ls('/Event')
-    
-    t.column_int   ( 'i'  , i       )
-    t.column_float ( 'fi' , float(i)) 
-    t.write()
 
-    h.fill ( i , i ) 
-
-
-print h.dump(100,40) 
 
 
     
