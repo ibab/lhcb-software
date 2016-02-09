66293f0d08c408b00937d22c1049343b88cbba83
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-02-09 18:53:17 +0000

    C++11 modernization:   o prefer direct member initialization   o prefer std::unique_ptr over raw pointer + explicit delete   o prefer range-based for loops   o prefer to pass by const& instead of const* if nullptr is an invalid argument   o prefer override over virtual   o make internal struct final
    git-svn-id: http://svn.cern.ch/guest/lhcb/Lbcom/trunk@201221 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5eec704ca1e04ef59f91fb7b2aacc26a4dbe36e4

--- Lbcom/Velo/VeloSimulation/src/VeloRadDamgeTool.cpp
+++ Lbcom/Velo/VeloSimulation/src/VeloRadDamgeTool.cpp
@@ -1,4 +1,4 @@
-// Include files 
+// Include files
 
 // from MCEvent
 #include "Event/MCHit.h"
@@ -23,23 +23,11 @@ DECLARE_TOOL_FACTORY( VeloRadDamageTool )
 VeloRadDamageTool::VeloRadDamageTool(const std::string& type,
                                      const std::string& name,
                                      const IInterface* parent)
-  : GaudiTool ( type, name , parent ),
-    m_veloDet(0),
-    m_radConditionMap()
+  : GaudiTool ( type, name , parent )
 {
   declareInterface<IRadDamageTool>(this);
 }
 
-// Destructor
-VeloRadDamageTool::~VeloRadDamageTool(){
-  // delete the response splines (if any)
-  std::map<unsigned int,RadParam>::iterator iMap = m_radParamMap.begin();
-  for( ; iMap !=  m_radParamMap.end() ; ++iMap ){
-    if(iMap->second.responseSpline != 0){
-      delete iMap->second.responseSpline;
-    }
-  }
-}
 
 StatusCode VeloRadDamageTool::initialize(){
   StatusCode sc = GaudiTool::initialize();
@@ -50,19 +38,18 @@ StatusCode VeloRadDamageTool::initialize(){
   if( !existDet<DataObject>(detSvc(),"Conditions/HardwareProperties/Velo/RadiationDamage") ){
     Warning("VELO radiation damage parameters not in conditions DB",
             StatusCode::SUCCESS).ignore();
-    std::vector<DeVeloSensor*>::const_iterator iSens = m_veloDet->sensorsBegin();
-    for( ; iSens != m_veloDet->sensorsEnd(); ++iSens ){
+
+    for( auto iSens = m_veloDet->sensorsBegin(); iSens != m_veloDet->sensorsEnd(); ++iSens ){
       RadParam param;
       param.useConst = true;
       // fully set param so copy and finish this sensor
-      m_radParamMap[(*iSens)->sensorNumber()] = param;
+      m_radParamMap[(*iSens)->sensorNumber()] = std::move(param);
       continue; // next sensor
     }
     return StatusCode::SUCCESS;
   }
 
-  std::vector<DeVeloSensor*>::const_iterator iSens = m_veloDet->sensorsBegin();
-  for( ; iSens != m_veloDet->sensorsEnd() ; ++iSens ){
+  for(auto iSens = m_veloDet->sensorsBegin(); iSens != m_veloDet->sensorsEnd() ; ++iSens ){
 
     std::string conditionName =
       format("Conditions/HardwareProperties/Velo/RadiationDamage/Sensor%03i-RadiationParam",(*iSens)->sensorNumber());
@@ -89,49 +76,47 @@ StatusCode VeloRadDamageTool::initialize(){
 
 StatusCode VeloRadDamageTool::i_loadCondtion(){
 
-  std::vector<DeVeloSensor*>::const_iterator iSens = m_veloDet->sensorsBegin();
-  for( ; iSens != m_veloDet->sensorsEnd(); ++iSens ){
+
+  for(auto iSens = m_veloDet->sensorsBegin(); iSens != m_veloDet->sensorsEnd(); ++iSens ){
 
     unsigned int sNum = (*iSens)->sensorNumber();
-    
+
     if(msgLevel( MSG::DEBUG )) debug() << "For sensor " << sNum << endmsg;
     // param is the current parameter (created if not existing already)
     RadParam &param = m_radParamMap[sNum];
 
-    // delete the old spline (if it exists)
-    if( param.responseSpline != 0 ) delete param.responseSpline;
-    
+
     param.useConst = m_radConditionMap[sNum]->param<int>("UseConst");
     if( param.useConst ) {
       param.constantFrac =
         m_radConditionMap[sNum]->param<double>("ConstFrac");
       // no spline for const output
-      if(msgLevel( MSG::DEBUG )) debug() << "Constant charge fraction " << param.constantFrac 
-                                         << endmsg;    
+      if(msgLevel( MSG::DEBUG )) debug() << "Constant charge fraction " << param.constantFrac
+                                         << endmsg;
     }else{
       // so need spline
       std::vector<double> radii =
         m_radConditionMap[sNum]->param<std::vector<double> >("Radii");
       std::vector<double> chargeFrac =
         m_radConditionMap[sNum]->param<std::vector<double> >("ChargeFrac");
-      
+
       if(radii.size() == 0 ||
          ( radii.size() != chargeFrac.size() ) )
         return Error(format("radii and chargeFrac not consistent for sensor %i",sNum),
                      StatusCode::FAILURE);
-      
+
       // outside range make a constant at the extremal value
       param.rMin = radii.front();
       param.rMax = radii.back();
       param.responseMin = chargeFrac.front();
       param.responseMax = chargeFrac.back();
-      
-      param.responseSpline =
+
+      param.responseSpline.reset(
         new GaudiMath::SimpleSpline( radii, chargeFrac,
-                                     GaudiMath::Interpolation::Linear );
+                                     GaudiMath::Interpolation::Linear ) );
 
       if(msgLevel( MSG::DEBUG )){
-        debug() << "Spline parameters: pointer" << param.responseSpline << endmsg;
+        debug() << "Spline parameters: pointer" << param.responseSpline.get() << endmsg;
         debug() << "radius      ChargeFraction" <<endmsg;
         for ( unsigned int i = 0 ; i < radii.size() ; ++i ){
           debug() << format("%5.2f   %5.2f",
@@ -139,7 +124,7 @@ StatusCode VeloRadDamageTool::i_loadCondtion(){
                   << endmsg;
         }
       }
-      if(msgLevel( MSG::VERBOSE )) dumpResponse(*iSens);
+      if(msgLevel( MSG::VERBOSE )) dumpResponse(**iSens);
     }
 
     if( !(*iSens)->isR()  ){
@@ -166,39 +151,38 @@ StatusCode VeloRadDamageTool::i_loadCondtion(){
 
     // if debug/verbose dump parameters to the screen
     if(msgLevel( MSG::DEBUG )){
-      debug() << "M2 max " << param.chargeFracMax 
+      debug() << "M2 max " << param.chargeFracMax
               << " width " << param.chargeFracWidth
-              << " 1st mask " << param.stripDistScale 
+              << " 1st mask " << param.stripDistScale
               << " from outer " << param.fracOuter<< endmsg;
     }
   }
   return StatusCode::SUCCESS;
 }
 
-void VeloRadDamageTool::dumpResponse(const DeVeloSensor *sens){
+void VeloRadDamageTool::dumpResponse(const DeVeloSensor& sens) const{
     verbose() << "response in 1mm steps from 0 to 55mm from function "
               <<endmsg;
     verbose() << "radius chargeFrac" <<endmsg;
     LHCb::MCHit hit;
     hit.setDisplacement(Gaudi::XYZVector(0.,0.,0.)); // explicitly set
-    hit.setSensDetID(sens->sensorNumber()); // set the sensor
+    hit.setSensDetID(sens.sensorNumber()); // set the sensor
     for ( double r = 0 ; r < 55.5 ; r+=1.0 ){
       Gaudi::XYZPoint local(r,0.,0.); // want r in local frame so ...
-      hit.setEntry(sens->localToGlobal(local));
+      hit.setEntry(sens.localToGlobal(local));
       verbose() << format("%5.2f   %5.2f",r, chargeFrac( hit ) )
                 << endmsg;
     }
 }
 
 double VeloRadDamageTool::chargeFrac(const LHCb::MCHit & hit) const {
-  // "find" the map element as operator[] is non-const
-  RadParam const &param = m_radParamMap.find(hit.sensDetID())->second;
+  RadParam const &param = m_radParamMap.at(hit.sensDetID());
   if(param.useConst) return param.constantFrac; // in case spline not required
   const DeVeloSensor* sens=m_veloDet->sensor(hit.sensDetID());
   double localR = (sens->globalToLocal(hit.midPoint())).Rho();
   if( localR < param.rMin ) return param.responseMin; // less than spline range
   if( localR > param.rMax ) return param.responseMax; // more than spline range
-  
+
   return param.responseSpline->eval(localR); // actually use the spline
 }
 
@@ -207,10 +191,10 @@ StatusCode VeloRadDamageTool::m2CouplingFrac(const Gaudi::XYZPoint &point,
                                              LHCb::VeloChannelID &chM2,
                                              double &fracInner,
                                              double &fracOuter) const{
-  // if the hit in an R sensor and close enough to a routing line 
+  // if the hit in an R sensor and close enough to a routing line
   // then charge is lost to the inner strip through the line
   RadParam const &param = m_radParamMap.find(sens->sensorNumber())->second;
-  const DeVeloRType* sensR = dynamic_cast<const DeVeloRType*>(sens);  
+  const DeVeloRType* sensR = dynamic_cast<const DeVeloRType*>(sens);
   if( !sensR ) return StatusCode::FAILURE; // phi hit, no effect simulated presently
   double distM2(0.), distStrip(0.);
   StatusCode sc = sensR->distToM2Line(point,chM2,distM2,distStrip);
@@ -219,10 +203,10 @@ StatusCode VeloRadDamageTool::m2CouplingFrac(const Gaudi::XYZPoint &point,
   if ( (distStrip/param.stripDistScale)<1. ) {
     toStripFrac = distStrip/param.stripDistScale;
   }
-  fracInner = param.chargeFracMax*toStripFrac* 
+  fracInner = param.chargeFracMax*toStripFrac*
     exp((-1.*distM2*distM2) /(2.*param.chargeFracWidth*param.chargeFracWidth));
   fracOuter = fracInner * param.fracOuter;
-  if(msgLevel( MSG::VERBOSE )){ 
+  if(msgLevel( MSG::VERBOSE )){
     verbose() << "global pos " << format("(%6.3f, %6.3f, %8.3f)",point.x(),point.y(),point.z())
               << " dStrip " << distStrip
               << " dM2 " << distM2
--- Lbcom/Velo/VeloSimulation/src/VeloRadDamgeTool.h
+++ Lbcom/Velo/VeloSimulation/src/VeloRadDamgeTool.h
@@ -1,6 +1,8 @@
 #ifndef VeloRadDamageTool_H
 #define VeloRadDamageTool_H
 
+#include <memory>
+
 // Gaudi
 #include "GaudiAlg/GaudiTool.h"
 #include "GaudiMath/GaudiMath.h"
@@ -22,89 +24,74 @@
  *  @date   29/3/2011
  */
 
-class VeloRadDamageTool : public GaudiTool, 
+class VeloRadDamageTool : public GaudiTool,
                           virtual public IRadDamageTool {
-  
-public: 
+
+public:
 
   /** Constructer */
-  VeloRadDamageTool(const std::string& type, 
+  VeloRadDamageTool(const std::string& type,
                     const std::string& name,
                     const IInterface* parent);
 
-  /** destructer */
-  virtual ~VeloRadDamageTool();
-
   /** Initialize */
-  virtual StatusCode initialize();
+  StatusCode initialize() override;
 
   /** fraction of charge seen due to radiation damage
   * @param point : position (global frame) of hit
   * @return fraction of charge after rad damage
   */
-  virtual double chargeFrac(const LHCb::MCHit & hit) const;
+  double chargeFrac(const LHCb::MCHit & hit) const override;
 
   /** fraction of charge seen due to radiation damage
-  * @param point : position (global frame) of hit 
+  * @param point : position (global frame) of hit
   * @param sens : DeVeloSensor of the point
   * @param chM2 : the channel of the inner strip
   * @param fracInner : fraction of charge to inner strip
   * @param fracMain : fraction of charge from main strip
   * @return Success if coupled to inner strip
   */
-  virtual StatusCode m2CouplingFrac(const Gaudi::XYZPoint &point,
+  StatusCode m2CouplingFrac(const Gaudi::XYZPoint &point,
                                     const DeVeloSensor *sens,
                                     LHCb::VeloChannelID &chM2,
-                                    double &fracInner, 
-                                    double &fracMain) const;
+                                    double &fracInner,
+                                    double &fracMain) const override;
 
-  
-protected:
 
-  DeVelo* m_veloDet; ///< Detector Element
+private:
+
+  DeVelo* m_veloDet = nullptr; ///< Detector Element
 
   /// set up the tool by reading the conditions
   StatusCode i_loadCondtion();
 
   /// need the following per sensor to simulate radiation damage
-  struct RadParam{
-    /// zeroing constructor
-    RadParam(): 
-      responseSpline(0),
-      rMin(0.),
-      rMax(0.),
-      constantFrac(1.),
-      useConst(false),
-      responseMin(0.),
-      responseMax(0.),
-      chargeFracWidth(1.),
-      chargeFracMax(0.),
-      stripDistScale(1.),
-      fracOuter(0.){};
+  struct RadParam final{
+    RadParam() = default;
 
     /// The fitted spline to the radius
-    GaudiMath::SimpleSpline* responseSpline; 
-    
-    double rMin; ///< First entry in vector of radii
-    double rMax; ///< Last entry in vector of radii
-    double constantFrac; ///< set if you want a constant output
-    
-    bool useConst; ///< use the constant value (or if false the spline)
-    
-    double responseMin; ///< to return if r < rMin
-    double responseMax; ///< to return if r > rMax
+    std::unique_ptr<GaudiMath::SimpleSpline> responseSpline;
 
-    double chargeFracWidth; ///< Coupling to metal 2 width
-    double chargeFracMax; ///< coupling to metal 2 max fraction
-    double stripDistScale; ///< size of mask caused by 1st metal layer
-    double fracOuter; ///< fraction of inner charge to remove from outer
+    double rMin = 0.; ///< First entry in vector of radii
+    double rMax = 0.; ///< Last entry in vector of radii
+    double constantFrac = 1.; ///< set if you want a constant output
+
+    bool useConst = false; ///< use the constant value (or if false the spline)
+
+    double responseMin = 0.; ///< to return if r < rMin
+    double responseMax = 0.; ///< to return if r > rMax
+
+    double chargeFracWidth = 1.; ///< Coupling to metal 2 width
+    double chargeFracMax = 0.; ///< coupling to metal 2 max fraction
+    double stripDistScale =1.; ///< size of mask caused by 1st metal layer
+    double fracOuter = 0.; ///< fraction of inner charge to remove from outer
   };
 
   /// dump the tool response to the log
-  void dumpResponse(const DeVeloSensor *sens);
+  void dumpResponse(const DeVeloSensor& sens) const;
 
-  /// each value corresponds to a sensor that needs a radiation tool 
-  std::map<unsigned int,RadParam> m_radParamMap; 
+  /// each value corresponds to a sensor that needs a radiation tool
+  std::map<unsigned int,RadParam> m_radParamMap;
 
   /// a map of the conditions verse sensor number
   std::map<unsigned int,Condition*> m_radConditionMap;
