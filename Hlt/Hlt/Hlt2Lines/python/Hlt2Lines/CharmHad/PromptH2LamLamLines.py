ae62d7e55f6c7bf0bb6dc3ea00ba266676633cab
Michael David Sokoloff <michael.david.sokoloff@cern.ch>
2016-02-10 04:29:03 +0000

    creates three lines for prompt H-dibaryon  --> Lambda,Lambda candidates   using a new combiner in Stages.py:  H2LambdaLambdaCombiner(Hlt2Combiner)
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@201240 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 92438254f83009eff2094f07a0b373781f43415e

---
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/PromptH2LamLamLines.py
@@ -0,0 +1,77 @@
+## a set of Hlt2 lines designed to search for H-dibaryon decay to
+## pairs of Lambda0's.  In this scenario, the H is a resonance above threshold,
+## so should decay "promptly".
+##
+## measuring how often two Lambda0's are nearby in phase space provides
+## a good denominator in terms of nuclear coupling strengths.
+##
+## the best current limits (160209) come by Belle:  PRL 110, 222002 (2013).
+##
+__author__  = "Mike SOKOLOFF msokolof@cern.ch"
+
+from GaudiKernel.SystemOfUnits import GeV, MeV, picosecond, mm
+from Hlt2Lines.Utilities.Hlt2LinesConfigurableUser import Hlt2LinesConfigurableUser
+
+class PromptH2LamLamLines():
+    def localcuts(self):
+        return {
+                 # The combiner for the CPV lines, separate for 2LL, LLDD, and 2DD
+                 'H2LamLam_LL' : {
+                                 'Lam_ALL_PT_MIN'           :  1000.0 * MeV,
+                                 'Lam_ALL_MIPCHI2DV_MAX'    :  16.0,
+                                 'BPVVDCHI2_MAX'            :  16.0,
+                                 'BPVLTIME_MIN'             :  -0.1 * picosecond,
+                                 'BPVLTIME_MAX'             :  0.1 * picosecond,
+                                 'ASUMPT_MIN'               :  2000 * MeV,
+                                 'AM_MAX'                   :  2310 * MeV,
+                                 'Mass_M_MIN'               :  1789.0 * MeV,
+                                 'Mass_M_MAX'               :  2300.0 * MeV,
+                                },
+                  'H2LamLam_DD' : {
+                                 'Lam_ALL_PT_MIN'           :  1000.0 * MeV,
+                                 'Lam_ALL_MIPCHI2DV_MAX'    :  16.0,
+                                 'BPVVDCHI2_MAX'            :  16.0,
+                                 'BPVLTIME_MIN'             :  0.2 * picosecond,
+                                 'BPVLTIME_MAX'             :  0.2 * picosecond,
+                                 'ASUMPT_MIN'               :  2000 * MeV,
+                                 'AM_MAX'                   :  2310 * MeV,
+                                 'Mass_M_MIN'               :  1789.0 * MeV,
+                                 'Mass_M_MAX'               :  2300.0 * MeV,
+                                }, 
+                }
+    
+    def locallines(self):
+        from Stages import MassFilter, H2LambdaLambdaCombiner
+        from Stages import CharmHadSharedSecondaryLambdaDD, CharmHadSharedSecondaryLambdaLL
+
+        ## If either Lambda is DD,  use the DD dictionary, otherwise use LL.
+        H2LamLam_2LLComb = H2LambdaLambdaCombiner( 'Comb'
+                , decay = "[D0 -> Lambda0 Lambda0]cc"
+                , inputs = [ CharmHadSharedSecondaryLambdaLL ]
+                , nickname = 'H2LamLam_LL' )
+
+        H2LamLam_2LL = MassFilter('H2LamLam_LL', inputs=[ H2LamLam_2LLComb ])
+
+
+        H2LamLam_LLDDComb = H2LambdaLambdaCombiner( 'Comb'
+                , decay = "[D0 -> Lambda0 Lambda0]cc"
+                , inputs = [ CharmHadSharedSecondaryLambdaLL, CharmHadSharedSecondaryLambdaDD ]
+                , lldd = True, nickname = 'H2LamLam_DD')
+
+        H2LamLam_LLDD = MassFilter('H2LamLam_DD', inputs = [ H2LamLam_LLDDComb ])
+
+
+        H2LamLam_2DDComb = H2LambdaLambdaCombiner( 'Comb'
+                , decay = "[D0 -> Lambda0 Lambda0]cc"
+                , inputs = [ CharmHadSharedSecondaryLambdaDD ]
+                , nickname = 'H2LamLam_DD' )
+
+        H2LamLam_2DD = MassFilter('H2LamLam_DD', inputs = [H2LamLam_2DDComb])
+
+
+        stages = {
+                  'PromptH2LamLam_LamLLTurbo'        : [H2LamLam_2LL],
+                  'PromptH2LamLam_LamLL_LamDDTurbo'  : [H2LamLam_LLDD],
+                  'PromptH2LamLam_LamDDTurbo'        : [H2LamLam_2DD],
+                 }
+        return stages
