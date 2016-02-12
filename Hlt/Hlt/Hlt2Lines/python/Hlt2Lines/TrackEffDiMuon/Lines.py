2d52ee7fb0a1fbf21b1b66a58a9bb20f4b250600
Michael Kolpin <michael.kolpin@cern.ch>
2016-02-12 14:33:53 +0000

    Split TrackEffDiMuon lines in 'LowStat' and 'High lines to allowSplit TrackEffDiMuon lines in 'LowStat' and 'HighStat' lines to allow separate prescale. Regions are configurable.
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@201367 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: a4ef6be47fa6e9a1c0adab5e883d32de2b24921f

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEffDiMuon/Lines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEffDiMuon/Lines.py
@@ -40,45 +40,57 @@ class TrackEffDiMuonLines(Hlt2LinesConfigurableUser):
 			        'TagTrChi2'	: 5,
 			        'TagMinIP'	: 0 * mm,
 			        'TagMinIPchi2'	: 0, #dummy
-			        'ProbeP'	: 0 * GeV,
-			        'ProbePt'	: 0 * MeV,
+			        'ProbeP'	: 5 * GeV,
+			        'ProbePt'	: 500 * MeV,
 			        'ProbeTrChi2'	: 9999, #dummy
 			        'JPsiPt'	: 1000 * MeV,
 			        'JPsiDOCA'	: 9999 * mm, #dummy
 			        'JPsiMaxIP'	: 0.8 * mm,
 			        'JPsiMaxIPchi2'	: 9999, #dummy
 			        'JPsiVtxChi2'	: 2,
-			        'JPsiMassWin'	: 500 * MeV},
+			        'JPsiMassWin'	: 500 * MeV,
+			        'StatHighPT'	: 2600 * MeV,
+			        'StatLowETA'	: 2.7,
+			        'StatHighETA'	: 4.2
+                    },
 		 'VeloMuon' :  {'TagMuonID'	: 1,
 			        'TagP'		: 7 * GeV,
 			        'TagPt'		: 0 * MeV,
 			        'TagTrChi2'	: 3,
 			        'TagMinIP'	: 0.2 * mm,
 			        'TagMinIPchi2'	: 0, #dummy
-			        'ProbeP'	: 0 * GeV,
-			        'ProbePt'	: 0 * MeV,
+			        'ProbeP'	: 5 * GeV,
+			        'ProbePt'	: 500 * MeV,
 			        'ProbeTrChi2'	: 5,
 			        'JPsiPt'	: 500 * MeV,
 			        'JPsiDOCA'	: 9999 * mm, #dummy
 			        'JPsiMaxIP'	: 9999 * mm, #dummy
 			        'JPsiMaxIPchi2'	: 9999, #dummy
 			        'JPsiVtxChi2'	: 2,
-			        'JPsiMassWin'	: 500 * MeV},
+			        'JPsiMassWin'	: 500 * MeV,
+			        'StatHighPT'	: 2600 * MeV,
+			        'StatLowETA'	: 2.7,
+			        'StatHighETA'	: 4.2
+                    },
 		 'Downstream': {'TagMuonID'	: 2,
 			        'TagP'		: 5 * GeV,
 			        'TagPt'		: 700 * MeV,
 			        'TagTrChi2'	: 10,
 			        'TagMinIP'	: 0.5 * mm,
 			        'TagMinIPchi2'	: 0, #dummy
-			        'ProbeP'	: 3 * GeV,
-			        'ProbePt'	: 200 * MeV,
+			        'ProbeP'	: 5 * GeV,
+			        'ProbePt'	: 500 * MeV,
 			        'ProbeTrChi2'	: 10,
 			        'JPsiPt'	: 0 * MeV,
 			        'JPsiDOCA'	: 5 * mm,
 			        'JPsiMaxIP'	: 9999 * mm, #dummy
 			        'JPsiMaxIPchi2'	: 9999, #dummy
 			        'JPsiVtxChi2'	: 5,
-			        'JPsiMassWin'	: 200 * MeV}
+			        'JPsiMassWin'	: 200 * MeV,
+			        'StatHighPT'	: 2600 * MeV,
+			        'StatLowETA'	: 2.7,
+			        'StatHighETA'	: 4.2
+                    }
                 }
 
     def stages(self, nickname = ""):
@@ -90,18 +102,30 @@ class TrackEffDiMuonLines(Hlt2LinesConfigurableUser):
 
         from Stages import JPsiCombiner, N3BodyCombiner
 	#second arg gives mode = Plus/Minus refers to the charge of the Probe Muon
-        self._stages = {'MuonTTPlusTagged'              : [JPsiCombiner(1, 'MuonTT')],
-                        'MuonTTMinusTagged'             : [JPsiCombiner(0, 'MuonTT')],
-                        'VeloMuonPlusTagged'            : [JPsiCombiner(1, 'VeloMuon')],
-                        'VeloMuonMinusTagged'           : [JPsiCombiner(0, 'VeloMuon')],
-                        'DownstreamPlusTagged'          : [JPsiCombiner(1, 'Downstream')],
-                        'DownstreamMinusTagged'         : [JPsiCombiner(0, 'Downstream')],
-                        'MuonTTPlusMatched'             : [JPsiCombiner(1, 'MuonTT'), N3BodyCombiner(1, 'MuonTT')],
-                        'MuonTTMinusMatched'            : [JPsiCombiner(0, 'MuonTT'), N3BodyCombiner(0, 'MuonTT')],
-                        'VeloMuonPlusMatched'           : [JPsiCombiner(1, 'VeloMuon'), N3BodyCombiner(1, 'VeloMuon')],
-                        'VeloMuonMinusMatched'          : [JPsiCombiner(0, 'VeloMuon'), N3BodyCombiner(0, 'VeloMuon')],
-                        'DownstreamPlusMatched'         : [JPsiCombiner(1, 'Downstream'), N3BodyCombiner(1, 'Downstream')],
-                        'DownstreamMinusMatched'        : [JPsiCombiner(0, 'Downstream'), N3BodyCombiner(0, 'Downstream')]
+        self._stages = {'MuonTTPlusLowStatTagged'               : [JPsiCombiner(1, 'MuonTT', 'low')],
+                        'MuonTTMinusLowStatTagged'              : [JPsiCombiner(0, 'MuonTT', 'low')],
+                        'VeloMuonPlusLowStatTagged'             : [JPsiCombiner(1, 'VeloMuon', 'low')],
+                        'VeloMuonMinusLowStatTagged'            : [JPsiCombiner(0, 'VeloMuon', 'low')],
+                        'DownstreamPlusLowStatTagged'           : [JPsiCombiner(1, 'Downstream', 'low')],
+                        'DownstreamMinusLowStatTagged'          : [JPsiCombiner(0, 'Downstream', 'low')],
+                        'MuonTTPlusHighStatTagged'              : [JPsiCombiner(1, 'MuonTT', 'high')],
+                        'MuonTTMinusHighStatTagged'             : [JPsiCombiner(0, 'MuonTT', 'high')],
+                        'VeloMuonPlusHighStatTagged'            : [JPsiCombiner(1, 'VeloMuon', 'high')],
+                        'VeloMuonMinusHighStatTagged'           : [JPsiCombiner(0, 'VeloMuon', 'high')],
+                        'DownstreamPlusHighStatTagged'          : [JPsiCombiner(1, 'Downstream', 'high')],
+                        'DownstreamMinusHighStatTagged'         : [JPsiCombiner(0, 'Downstream', 'high')],
+                        'MuonTTPlusLowStatMatched'              : [JPsiCombiner(1, 'MuonTT', 'low'), N3BodyCombiner(1, 'MuonTT', 'low')],
+                        'MuonTTMinusLowStatMatched'             : [JPsiCombiner(0, 'MuonTT', 'low'), N3BodyCombiner(0, 'MuonTT', 'low')],
+                        'VeloMuonPlusLowStatMatched'            : [JPsiCombiner(1, 'VeloMuon', 'low'), N3BodyCombiner(1, 'VeloMuon', 'low')],
+                        'VeloMuonMinusLowStatMatched'           : [JPsiCombiner(0, 'VeloMuon', 'low'), N3BodyCombiner(0, 'VeloMuon', 'low')],
+                        'DownstreamPlusLowStatMatched'          : [JPsiCombiner(1, 'Downstream', 'low'), N3BodyCombiner(1, 'Downstream', 'low')],
+                        'DownstreamMinusLowStatMatched'         : [JPsiCombiner(0, 'Downstream', 'low'), N3BodyCombiner(0, 'Downstream', 'low')],
+                        'MuonTTPlusHighStatMatched'             : [JPsiCombiner(1, 'MuonTT', 'high'), N3BodyCombiner(1, 'MuonTT', 'high')],
+                        'MuonTTMinusHighStatMatched'            : [JPsiCombiner(0, 'MuonTT', 'high'), N3BodyCombiner(0, 'MuonTT', 'high')],
+                        'VeloMuonPlusHighStatMatched'           : [JPsiCombiner(1, 'VeloMuon', 'high'), N3BodyCombiner(1, 'VeloMuon', 'high')],
+                        'VeloMuonMinusHighStatMatched'          : [JPsiCombiner(0, 'VeloMuon', 'high'), N3BodyCombiner(0, 'VeloMuon', 'high')],
+                        'DownstreamPlusHighStatMatched'         : [JPsiCombiner(1, 'Downstream', 'high'), N3BodyCombiner(1, 'Downstream', 'high')],
+                        'DownstreamMinusHighStatMatched'        : [JPsiCombiner(0, 'Downstream', 'high'), N3BodyCombiner(0, 'Downstream', 'high')]
                         }
         if nickname:
             return self._stages[nickname]
--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEffDiMuon/Stages.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/TrackEffDiMuon/Stages.py
@@ -8,15 +8,15 @@ from Inputs import BiKalmanFittedMuons, TTMuons, VeloMuons, DownMuons, LongAssoc
 from Hlt2Lines.Utilities.Hlt2Combiner import Hlt2Combiner
 from HltTracking.HltPVs import PV3D
 class JPsiCombiner(Hlt2Combiner):
-    def __init__(self, mode, nickname):
+    def __init__(self, mode, nickname, stat):
         cc =    ("(AMAXDOCA('') < %(JPsiDOCA)s)")
         mc =    ("(VFASPF(VCHI2PDOF) < %(JPsiVtxChi2)s)" +
                  " & (PT > %(JPsiPt)s)" +
                  " & (MIPDV(PRIMARY) < %(JPsiMaxIP)s)" +
                  " & (MIPCHI2DV(PRIMARY) < %(JPsiMaxIPchi2)s)" +
                  " & (ADMASS('J/psi(1S)') < %(JPsiMassWin)s)")
-        inputs = [TagFilter(mode, nickname), ProbeFilter(mode, nickname)]
-        Hlt2Combiner.__init__(self, 'TrackEff'+nickname+str(mode)+'JPsi', "J/psi(1S) -> mu+ mu-", inputs,
+        inputs = [TagFilter(mode, nickname), ProbeFilter(mode, nickname, stat)]
+        Hlt2Combiner.__init__(self, 'TrackEff'+nickname+str(mode)+stat+'JPsi', "J/psi(1S) -> mu+ mu-", inputs,
                               dependencies = [PV3D('Hlt2')], nickname = nickname,
                               tistos = [], CombinationCut = cc,
                               MotherCut = mc, Preambulo = [], shared = True)
@@ -24,16 +24,16 @@ class JPsiCombiner(Hlt2Combiner):
 # 3Body combiner to check overlap between probe and long track (pion id)
 from Configurables import DaVinci__N3BodyDecays as N3Body 
 class N3BodyCombiner(Hlt2Combiner):
-    def __init__(self, mode, nickname):
+    def __init__(self, mode, nickname, stat):
         if nickname == 'MuonTT':
        	    overlap = ("(AOVERLAP(1,2, LHCb.LHCbID.TT) > %(OverlapTT)s) & (AOVERLAP(1,2, LHCb.LHCbID.Muon) > %(OverlapMuon)s)") 
         elif nickname == 'VeloMuon':
        	    overlap = ("(AOVERLAP(1,2, LHCb.LHCbID.Velo) > %(OverlapVelo)s) & (AOVERLAP(1,2, LHCb.LHCbID.Muon) > %(OverlapMuon)s)") 
         else:
        	    overlap = ("(AOVERLAP(1,2, LHCb.LHCbID.TT) > %(OverlapTT)s) & ((AOVERLAP(1,2, LHCb.LHCbID.IT) > %(OverlapIT)s) | (AOVERLAP(1,2, LHCb.LHCbID.OT) > %(OverlapOT)s))")
-        inputs = [TagFilter(mode, nickname), ProbeFilter(mode, nickname), LongFilter(mode, nickname)]
+        inputs = [TagFilter(mode, nickname), ProbeFilter(mode, nickname, stat), LongFilter(mode, nickname)]
         decay = "J/psi(1S) -> pi+ mu+ mu-" if mode == 1 else "J/psi(1S) -> pi- mu- mu+"
-        Hlt2Combiner.__init__(self, 'TrackEff'+nickname+str(mode)+'N3Body', decay , inputs, combiner = N3Body,
+        Hlt2Combiner.__init__(self, 'TrackEff'+nickname+str(mode)+stat+'N3Body', decay , inputs, combiner = N3Body,
                               dependencies = [PV3D('Hlt2')], nickname = nickname,
                               tistos = [], Combination12Cut = overlap, ParticleCombiners ={"" : "MomentumCombiner"},
                               MotherCut = "ALL", Preambulo = [], shared = True)
@@ -56,14 +56,15 @@ class TagFilter(Hlt2ParticleFilter):
 # Probe filter
 from HltLine.Hlt2Monitoring import Hlt2Monitor,Hlt2MonitorMinMax
 class ProbeFilter(Hlt2ParticleFilter):
-    def __init__(self, mode, nickname):
+    def __init__(self, mode, nickname, stat):
         inputs = [TTMuons] if nickname == 'MuonTT' else [VeloMuons] if nickname == 'VeloMuon' else [DownMuons]
         charge_cut = "(Q>0)" if mode == 1 else "(Q<0)"
-        cut = (charge_cut +
+        stat_cut = " & ((PT >%(StatHighPT)s) | (ETA >%(StatHighETA)s) | (ETA <%(StatLowETA)s))" if stat == 'low' else " & ((PT <%(StatHighPT)s) & (ETA <%(StatHighETA)s) & (ETA >%(StatLowETA)s))"
+        cut = (charge_cut + stat_cut +
                " & (TRCHI2DOF <%(ProbeTrChi2)s )" +
                " & (P >%(ProbeP)s )" +
                " & (PT >%(ProbePt)s )")
-        Hlt2ParticleFilter.__init__(self, 'TrackEff'+nickname+str(mode)+'Probe', cut, inputs, dependencies = [PV3D('Hlt2')], nickname = nickname, shared = True)
+        Hlt2ParticleFilter.__init__(self, 'TrackEff'+nickname+str(mode)+stat+'Probe', cut, inputs, dependencies = [PV3D('Hlt2')], nickname = nickname, shared = True)
 
 
 # Long filter
