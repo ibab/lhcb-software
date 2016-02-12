7455e51b033f85600a6c2f555e149e443e1034ff
Michael David Sokoloff <michael.david.sokoloff@cern.ch>
2016-02-12 06:36:00 +0000

    a new set of lines for "secondary" H-dibaryon decays to Lambda,p,pi
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@201327 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 5384eaba4fab11a97d9f5d9c08a662861137d848

---
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/CharmHad/SecondaryH2LamPrPiLines.py
@@ -0,0 +1,57 @@
+from GaudiKernel.SystemOfUnits import GeV, MeV, picosecond, mm
+from Hlt2Lines.Utilities.Hlt2LinesConfigurableUser import Hlt2LinesConfigurableUser
+
+
+class SecondaryH2LamPrPiLines(Hlt2LinesConfigurableUser):
+    def localcuts(self) :
+        return {
+                 # cuts for the mass filters
+                 'SecondaryH2LambdaPrPi_LLLL_Mass' : {
+                                     'Mass_M_MIN'       : 1765 * MeV,
+                                     'Mass_M_MAX'       : 1965 * MeV
+                                   }
+                ,'SecondaryH2LambdaPrPi_DDLL_Mass' : {
+                                     'Mass_M_MIN'       : 1765 * MeV,
+                                     'Mass_M_MAX'       : 1965 * MeV
+                                   }
+                ,'SecondaryH2LambdaPrPi_DDDD_Mass' : {
+                                     'Mass_M_MIN'       : 1765 * MeV,
+                                     'Mass_M_MAX'       : 1965 * MeV
+                                   }
+        }
+
+    def locallines(self):
+        from Stages import MassFilter
+        from Stages import H2LambdaPrPi_LLLL, H2LambdaPrPi_DDLL
+        from Stages import H2LambdaPrPi_DDDD
+        from Stages import H2LambdaPiPr_LLLL, H2LambdaPiPr_DDLL
+        from Stages import H2LambdaPiPr_DDDD
+
+#  These are the "real" H --> Lambda,p+,pi- lines
+        Filtered_LamPrPi_LLLL = MassFilter('SecondaryH2LambdaPrPi_LLLL_Mass',
+                                            inputs=[H2LambdaPrPi_LLLL])
+        Filtered_LamPrPi_DDLL = MassFilter('SecondaryH2LambdaPrPi_DDLL_Mass',
+                                            inputs=[H2LambdaPrPi_DDLL])
+        Filtered_LamPrPi_DDDD = MassFilter('SecondaryH2LambdaPrPi_DDLL_Mass',
+                                            inputs=[H2LambdaPrPi_DDDD])
+
+#  These are the "fake" H --> Lambda,pi+,pi- lines (no net baryon number)
+        Filtered_LamPiPr_LLLL = MassFilter('SecondaryH2LambdaPrPi_LLLL_Mass',
+                                            inputs=[H2LambdaPiPr_LLLL])
+        Filtered_LamPiPr_DDLL = MassFilter('SecondaryH2LambdaPrPi_DDLL_Mass',
+                                            inputs=[H2LambdaPiPr_DDLL])
+        Filtered_LamPiPr_DDDD = MassFilter('SecondaryH2LambdaPrPi_DDLL_Mass',
+                                            inputs=[H2LambdaPiPr_DDDD])
+
+
+        stages = {
+                  'SecondaryH2LamPpPimLLLLTurbo'  :  [Filtered_LamPrPi_LLLL],
+                  'SecondaryH2LamPpPimDDLLTurbo'  :  [Filtered_LamPrPi_DDLL],
+                  'SecondaryH2LamPpPimDDDDTurbo'  :  [Filtered_LamPrPi_DDDD],
+                  'SecondaryH2LamPipPmLLLLTurbo'  :  [Filtered_LamPiPr_LLLL],
+## mds                  'SecondaryH2LamPipPmDDLLTurbo'  :  [Filtered_LamPiPr_DDLL],
+## mds                  'SecondaryH2LamPipPmDDDDTurbo'  :  [Filtered_LamPiPr_DDDD],
+                 }
+        
+
+        return stages
