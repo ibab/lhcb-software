acbb64dadac9afda5512eb20c0880dc312ef1cec
Carla Marin Benito <carla.marin.benito@cern.ch>
2016-02-12 12:46:19 +0000

    Add folder for LFV settings
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@201343 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6bab7cd5a107c1790dd2ec21f14e29a906e83950

---
+++ Hlt/Hlt/HltSettings/python/HltSettings/LFV/LFV_25ns_Draft2016.py
@@ -0,0 +1,46 @@
+# =============================================================================
+# @file   LFV_25ns_Draft2016.py
+# @author Carla Marin (carla.marin@cern.ch)
+# @date   12.02.2016
+# =============================================================================
+"""Threshold settings for Hlt2 LFV lines for 2016.
+"""
+
+
+class LFV_25ns_Draft2016(object):
+    """Threshold settings for Hlt2 LFV lines for 2016."""
+    __all__ = ('ActiveHlt2Lines', 'Thresholds')
+
+    def ActiveHlt2Lines(self):
+        """Returns a list of active lines."""
+
+        lines = ['Hlt2LFVJpsiMuE']
+
+        return lines
+
+    def Thresholds(self):
+        """Set thresholds for the lines."""
+
+        from GaudiKernel.SystemOfUnits import MeV
+        m_jpsi = 3096
+        thresholds = {'Prescale': {},
+                      'Common': {},
+                      'JpsiMuE': {'CombMassHigh': (m_jpsi + 550) * MeV,
+                                  'CombMassLow': (m_jpsi - 1100) * MeV,
+                                  'ElectronProbNn': 0.93,
+                                  'ElectronTrChi2DoF': 3,
+                                  'ElectronTrGhostProb': 0.2,
+                                  'ElectronVertexChi2DoF': 3,
+                                  'MassHigh': (m_jpsi + 500) * MeV,
+                                  'MassLow': (m_jpsi - 1000) * MeV,
+                                  'MuonProbNn': 0.9,
+                                  'MuonTrChi2DoF': 3,
+                                  'MuonTrGhostProb': 0.1,
+                                  'MuonVertexChi2DoF': 3},
+                      'SpdCut': {'NSPD': 350}}
+
+        from Hlt2Lines.LFV.Lines import LFVLines
+        
+        return {LFVLines: thresholds}
+
+# EOF
