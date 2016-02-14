d89854961cadfa51aff94d13a9079197eef68701
Simone Stracka <simone.stracka@cern.ch>
2016-02-14 22:09:59 +0000

    ! 2016-02-14 - Simone Stracka  - Add TightKstar2KPi to Kstar.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@201528 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: e5511cc61f77a235928d738c134f9ea5828fa6c6

--- Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Kstar.py
+++ Hlt/Hlt/Hlt2SharedParticles/python/Hlt2SharedParticles/Kstar.py
@@ -10,9 +10,10 @@ from Gaudi.Configuration import *
 from Configurables import CombineParticles
 from Hlt2SharedParticles.GoodParticles import GoodKaons, GoodPions
 from Hlt2SharedParticles.TrackFittedBasicParticles import  BiKalmanFittedKaons, BiKalmanFittedPions
-from HltLine.HltLine import bindMembers
+from Hlt2SharedParticles.TrackFittedBasicParticles import  BiKalmanFittedRichKaons
+from HltLine.HltLine import bindMembers, Hlt2Member
 
-__all__ = ( 'Kstar2KPi' )
+__all__ = ( 'Kstar2KPi' , 'TightKstar2KPi' )
 
 Hlt2SharedKstar2KPi = CombineParticles("Hlt2SharedKstar2KPi")
 Hlt2SharedKstar2KPi.Inputs = [ BiKalmanFittedKaons.outputSelection(), BiKalmanFittedPions.outputSelection() ]
@@ -23,3 +24,17 @@ Hlt2SharedKstar2KPi.CombinationCut = "(ADAMASS('K*(892)0')<300)"
 Hlt2SharedKstar2KPi.MotherCut = "(VFASPF(VCHI2PDOF)<25)"
 
 Kstar2KPi = bindMembers( None, [ BiKalmanFittedKaons, BiKalmanFittedPions, Hlt2SharedKstar2KPi ] )
+
+
+# Tight K* with PID
+
+Hlt2SharedTightKstar2KPi = Hlt2Member( CombineParticles
+                                       , "Hlt2SharedKstar2KPi"
+                                       , Inputs = [ BiKalmanFittedRichKaons, BiKalmanFittedPions ]
+                                       , DecayDescriptor = "[K*(892)0 -> K+ pi-]cc"
+                                       , DaughtersCuts = { "pi+" : "(PT > 500*MeV)", "K+" : "(PT > 500*MeV) & (PIDK>0.)" }
+                                       , CombinationCut = "(ADAMASS('K*(892)0')<100)"
+                                       , MotherCut = "(VFASPF(VCHI2PDOF)<25)" )
+
+TightKstar2KPi = bindMembers( 'Shared', [ BiKalmanFittedRichKaons, BiKalmanFittedPions, Hlt2SharedTightKstar2KPi ] )
+
