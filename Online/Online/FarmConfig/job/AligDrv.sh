460e9efc9dbff1f28ef523465f5da566042ae73b
Claire Prouve <claire.prouve@cern.ch>
2016-02-17 13:16:29 +0000

    Newest version of Rich Alignment
    git-svn-id: http://svn.cern.ch/guest/lhcb/Online/trunk@201628 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 273da0021e0cf7c363c2002403f8cc9f4605bddc

--- Online/Online/FarmConfig/job/AligDrv.sh
+++ Online/Online/FarmConfig/job/AligDrv.sh
@@ -47,14 +47,16 @@ elif [ "$act" = "Alignment|TED" ]; then
   cd ${FARMCONFIGROOT}/job
   exec -a ${UTGID} ${Class1_task} libGaudiOnline.so OnlineTask -tasktype=LHCb::Class1Task -main=/group/online/dataflow/templates/options/Main.opts -opt=command="import Gaudi,GaudiKernel.ProcessJobOptions; from Gaudi.Configuration import importOptions; GaudiKernel.ProcessJobOptions.printing_level=999; from TrAligIterator import doIt; doIt('TEDAlignment');"
 elif [ "$act" = "Alignment|Rich1" ]; then
-  . /group/rich/sw/cmtuser/AlignmentOnlineDev_v10r5/setup.x86_64-slc6-gcc48-opt.vars
+    . /group/rich/sw/cmtuser/AlignmentOnlineDev_v10r6/setup.x86_64-slc6-gcc49-opt.vars  
 #  export PYTHONPATH=/group/online/bw_division/pydim/lib/python2.7/site-packages:$PYTHONPATH
   export PYTHONPATH=/group/online/dataflow/options/LHCbA/HLT:$PYTHONPATH
+  export PYTHONPATH=/home/raaij/pydim/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH
   python -c "from PyMirrAlignOnline import Iterator; Iterator.run(1)"
 elif [ "$act" = "Alignment|Rich2" ]; then
-  . /group/rich/sw/cmtuser/AlignmentOnlineDev_v10r5/setup.x86_64-slc6-gcc48-opt.vars
+  . /group/rich/sw/cmtuser/AlignmentOnlineDev_v10r6/setup.x86_64-slc6-gcc49-opt.vars  
 #  export PYTHONPATH=/group/online/bw_division/pydim/lib/python2.7/site-packages:$PYTHONPATH
   export PYTHONPATH=/group/online/dataflow/options/LHCbA/HLT:$PYTHONPATH
+  export PYTHONPATH=/home/raaij/pydim/x86_64-slc6-gcc49-opt/lib/python2.7/site-packages:$PYTHONPATH
   python -c "from PyMirrAlignOnline import Iterator; Iterator.run(2)"
 elif [ "$act" = "Calibration|Calo" ]; then
   . /group/calo/cmtuser/CaloCalibrationDev_v10r4p1/setup.x86_64-slc6-gcc48-dbg.vars
