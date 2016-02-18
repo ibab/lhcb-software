9b77fa42b3f37618c92c4f35072e8e2df9f504ba
Ricardo Vazquez Gomez <ricardo.vazquez.gomez@cern.ch>
2016-02-18 16:39:00 +0000

    add cache for S21r0p1 and S21r1p1
    git-svn-id: http://svn.cern.ch/guest/lhcb/DaVinci/trunk@201714 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 8cff0382043419407d5f9fe0334629d44dc56203

---
+++ DaVinci/Phys/StrippingCache/options/DV-Stripping21r0p1-Stripping.py
@@ -0,0 +1,146 @@
+"""
+Options for building Stripping21r0p1. 
+"""
+
+stripping='stripping21r0p1'
+
+#use CommonParticlesArchive
+from CommonParticlesArchive import CommonParticlesArchiveConf
+CommonParticlesArchiveConf().redirect(stripping)
+
+
+from Gaudi.Configuration import *
+MessageSvc().Format = "% F%30W%S%7W%R%T %0W%M"
+
+# Tighten Trk Chi2 to <3
+from CommonParticles.Utils import DefaultTrackingCuts
+DefaultTrackingCuts().Cuts  = { "Chi2Cut" : [ 0, 3 ],
+                                "CloneDistCut" : [5000, 9e+99 ] }
+
+#
+#Raw event juggler to split Other/RawEvent into Velo/RawEvent and Tracker/RawEvent
+#
+from Configurables import GaudiSequencer, RawEventJuggler
+jseq=GaudiSequencer("RawEventSplitSeq")
+juggler=RawEventJuggler("dstJuggler")
+juggler.Sequencer=jseq
+juggler.Input=2.0  # 2015 Online (Moore) format
+juggler.Output=4.2 # Reco15 format
+
+DaVinci().prependToMainSequence( [jseq] )
+
+#
+# Disable the cache in Tr/TrackExtrapolators 
+#
+from Configurables import TrackStateProvider
+TrackStateProvider().CacheStatesOnDemand = False
+
+#
+# Fix for TrackEff lines
+#
+from Configurables import DecodeRawEvent
+DecodeRawEvent().setProp("OverrideInputs",4.2)
+
+#
+# Build the streams and stripping object
+#
+from StrippingConf.Configuration import StrippingConf, StrippingStream
+from StrippingSettings.Utils import strippingConfiguration
+from StrippingArchive.Utils import buildStreams
+from StrippingArchive import strippingArchive
+
+#get the configuration dictionary from the database
+config  = strippingConfiguration(stripping)
+#get the line builders from the archive
+archive = strippingArchive(stripping)
+
+streams = buildStreams(stripping = config, archive = archive)
+	
+leptonicMicroDSTname   = 'Leptonic'
+charmMicroDSTname      = 'Charm'
+pidMicroDSTname        = 'PID'
+bhadronMicroDSTname    = 'Bhadron'
+mdstStreams = [ leptonicMicroDSTname,charmMicroDSTname,pidMicroDSTname,bhadronMicroDSTname ]
+dstStreams  = [ "BhadronCompleteEvent", "CharmCompleteEvent", "CharmToBeSwum", "Dimuon",
+                "EW", "Semileptonic", "Calibration", "MiniBias", "Radiative" ]
+
+stripTESPrefix = 'Strip'
+
+from Configurables import ProcStatusCheck
+	
+from PhysConf.Filters import LoKi_Filters
+flts = LoKi_Filters(VOID_Code = "( TrSource(TrSOURCE('/Event/Rec/Track/Best', TrLONG))"\
+                                " >> ( sum( TrPT,TrP < 1 * TeV ) > 1 * TeV ) )" ,
+                    VOID_Preambulo = ["from LoKiTracks.decorators import *" ,
+                                      "from LoKiCore.functions    import * ",
+                                      "from GaudiKernel.SystemOfUnits import *"])
+filterBadEvents = GaudiSequencer("BadEventFilter",
+                                  ModeOR = True,
+                                  Members = [ flts.sequencer("GECFilter"),
+                                              ProcStatusCheck() ] )
+streamFilter = { 'default'  : filterBadEvents,
+                 'MiniBias' : ProcStatusCheck() }
+
+sc = StrippingConf( Streams = streams,
+                    MaxCandidates = 2000,
+                    AcceptBadEvents = False,
+                    BadEventSelection = ProcStatusCheck(),
+                    TESPrefix = stripTESPrefix,
+                    ActiveMDSTStream = True,
+                    Verbose = True,
+                    DSTStreams = dstStreams,
+                    MicroDSTStreams = mdstStreams )
+
+from Configurables import ApplicationMgr, AuditorSvc, SequencerTimerTool
+	
+# Initial IOV time
+# http://www.onlineconversion.com/unix_time.htm
+# values in ns (so multiply values from above link by 1e9)
+#from Configurables import EventClockSvc
+#EventClockSvc().EventTimeDecoder = "OdinTimeDecoder"
+
+appMgr = ApplicationMgr()
+appMgr.OutputLevel = 6
+appMgr.ExtSvc += [ 'ToolSvc', 'AuditorSvc' ]
+
+appMgr.HistogramPersistency = "ROOT"
+ntSvc = NTupleSvc()
+appMgr.ExtSvc += [ ntSvc ]
+
+from Configurables import ( LHCbApp, PhysConf, AnalysisConf,
+                            DstConf, LumiAlgsConf, DDDBConf )
+
+#LHCbApp().DDDBtag   = "dddb-20150724"
+#LHCbApp().CondDBtag = "cond-20150805"
+
+# Can be enabled for next full stack release
+PhysConf().OutputLevel     = appMgr.OutputLevel
+#AnalysisConf().OutputLevel = appMgr.OutputLevel
+
+datatype =  "2012"
+PhysConf().DataType      = datatype
+AnalysisConf().DataType  = datatype
+LumiAlgsConf().DataType  = datatype
+DDDBConf().DataType      = datatype
+
+inputType = "DST"
+LumiAlgsConf().InputType = inputType
+PhysConf().InputType     = inputType
+
+unPack = ["Reconstruction"]
+PhysConf().EnableUnpack = unPack
+DstConf().EnableUnpack  = unPack
+
+lumiSeq = GaudiSequencer("LumiSeq")
+LumiAlgsConf().LumiSequencer = lumiSeq
+
+appMgr.TopAlg += [ PhysConf().initSequence(),
+                   AnalysisConf().initSequence(),
+                   sc.sequence(), lumiSeq ]
+
+#from Configurables import DaVinci
+#DaVinci().ProductionType = "Stripping"
+#DaVinci().DataType   = datatype
+#DaVinci().DDDBtag    = LHCbApp().DDDBtag
+#DaVinci().CondDBtag  = LHCbApp().CondDBtag
+#DaVinci().appendToMainSequence( [ sc.sequence() ] )
