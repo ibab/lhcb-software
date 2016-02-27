3256be65bb20bfe5b5c29165d11aa5e1d24428f3
Vanya Belyaev <ivan.belyaev@cern.ch>
2016-02-27 16:00:04 +0000

    non-empty __init__.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Bender/trunk@202197 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: b56fce9ea3cc2aea582618a9189af97bf9aacc9c

---
+++ Bender/Ex/BenderExample/python/BenderExample/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =============================================================================
+# $Id$
+# =============================================================================
+## @file
+#
+#  This file is a part of 
+#  <a href="http://cern.ch/lhcb-comp/Analysis/Bender/index.html">Bender project</a>
+#  <b>``Python-based Interactive Environment for Smart and Friendly 
+#   Physics Analysis''</b>
+#
+#  The package has been designed with the kind help from
+#  Pere MATO and Andrey TSAREGORODTSEV. 
+#  And it is based on the 
+#  <a href="http://cern.ch/lhcb-comp/Analysis/LoKi/index.html">LoKi project:</a>
+#  ``C++ ToolKit for Smart and Friendly Physics Analysis''
+#
+#  By usage of this code one clearly states the disagreement 
+#  with the smear campaign of Dr.O.Callot et al.: 
+#  ``No Vanya's lines are allowed in LHCb/Gaudi software.''
+#
+# =============================================================================
+""" This file is a part of BENDER project:
+``Python-based Interactive Environment for Smart and Friendly Physics Analysis''
+
+The project has been designed with the kind help from
+Pere MATO and Andrey TSAREGORODTSEV. 
+
+And it is based on the 
+LoKi project: ``C++ ToolKit for Smart and Friendly Physics Analysis''
+
+By usage of this code one clearly states the disagreement 
+with the smear campaign of Dr.O.Callot et al.: 
+``No Vanya's lines are allowed in LHCb/Gaudi software.''
+
+"""
+# =============================================================================
+import ROOT,cppyy
+
+# =============================================================================
+# The END 
+# =============================================================================
