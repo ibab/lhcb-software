8bb4557a24cbcec1776815cd1c8c5d75669cb180
Sean Benson <sean.benson@cern.ch>
2016-03-01 18:11:39 +0000

    Make TESMerger safer
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@202378 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 91f57ac70934b5ac66036be33783aee5f332091a

--- LHCb/Kernel/LHCbAlgs/src/TESMerger.cpp
+++ LHCb/Kernel/LHCbAlgs/src/TESMerger.cpp
@@ -47,10 +47,13 @@ StatusCode TESMerger<T>::execute()
   for( std::vector<std::string>::const_iterator ilist = m_inputLocations.begin() ;
        ilist != m_inputLocations.end(); ++ilist) {
     //typename Gaudi::NamedRange_<std::vector<const T*> > cont_in = get<typename Gaudi::NamedRange_<std::vector<const T*> > >(*ilist) ;
+    //KeyedContainer<T, Containers::HashMap >* cont_in = getOrCreate<KeyedContainer<T, Containers::HashMap >, KeyedContainer<T, Containers::HashMap > >(*ilist) ;
     KeyedContainer<T, Containers::HashMap >* cont_in = getIfExists<KeyedContainer<T, Containers::HashMap > >(*ilist) ;
-    for(auto i_obj = cont_in->begin(); 
-	 i_obj != cont_in->end(); ++i_obj) 
-	out->insert( (*i_obj)->clone() ) ;
+    if(cont_in){
+      for(auto i_obj = cont_in->begin(); 
+          i_obj != cont_in->end(); ++i_obj) 
+        out->insert( (*i_obj)->clone() ) ;
+    }
   }
   return StatusCode::SUCCESS;
 }
