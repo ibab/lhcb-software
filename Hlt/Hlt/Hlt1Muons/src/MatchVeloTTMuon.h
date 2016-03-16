6b8d13494c8e5adcc786318ebedbf0c76afb82fe
Jessica Prisciandaro <jessica.prisciandaro@cern.ch>
2016-03-16 20:44:36 +0000

    MatchVeloTTMuon update
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@203196 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 28b23152e642e64775b658fa9652117d77820c09

--- Hlt/Hlt/Hlt1Muons/src/MatchVeloTTMuon.h
+++ Hlt/Hlt/Hlt1Muons/src/MatchVeloTTMuon.h
@@ -4,6 +4,7 @@
 
 // Include files
 #include <vector>
+#include <unordered_map>
 
 // from Gaudi
 #include <GaudiAlg/GaudiHistoTool.h>
@@ -36,34 +37,42 @@ public:
    /// Standard constructor
    MatchVeloTTMuon( const std::string& type, const std::string& name,
                   const IInterface* parent );
-  
+
    StatusCode initialize() override;
 
    StatusCode finalize() override;
 
    StatusCode tracksFromTrack( const LHCb::Track& seed,
-                               std::vector<LHCb::Track*>& tracks ) const ;
+                               std::vector<LHCb::Track*>& tracks ) const override;
 
 private:
 
    // Properties for Velo seeds
-   double m_za;
-   double m_zb;
+   double m_z0;
+   double m_z1;
+   double m_c0;
+   double m_c1;
 
-   double m_xWindow;
-   double m_yWindow;
+   //double m_xWindow;
+   //double m_yWindow;
+   
+   std::unordered_map<unsigned int, std::pair<double,double>> m_Window;
 
    double m_minMomentum;
    double m_maxPt;
    double m_kickScale;
    double m_kickOffset;
 
-   // Properties for VeloTT seeds
-   double m_FoIToleranceX;
-   double m_FoIToleranceY;
-
-   bool  m_chi2withVertPlane;
    double m_maxChi2DoFX;
+
+   bool m_fitY;
+
+   // Corrections
+   using CorrectMap = std::unordered_map<std::string, std::vector<double> >;
+   CorrectMap m_errCorrect;
+
+   std::array<double, 5> m_corX;
+   std::array<double, 5> m_corY;
 
    // General properties
    unsigned int m_maxMissed;
@@ -77,46 +86,27 @@ private:
 
    // Muon Detector
    DeMuonDetector* m_det;
-    
+
    // Data from detector description and conditions
    std::vector<double> m_padSizeX;
    std::vector<double> m_padSizeY;
-   
-   // function that defines the field of interest size
-   // formula is p(1) + p(2)*exp(-p(3)*momentum)
-   std::vector<double> m_xFoIParam1;
-   std::vector<double> m_xFoIParam2;
-   std::vector<double> m_xFoIParam3;
-   
-   std::vector<double> m_yFoIParam1;
-   std::vector<double> m_yFoIParam2;
-   std::vector<double> m_yFoIParam3;
-   
-   double m_FoIFactorX;
-   double m_FoIFactorY;
-   
+
    // Temporary storage
-   mutable std::unique_ptr<CommonMuonHit> m_magnetHit;
+   mutable std::vector<std::unique_ptr<CommonMuonHit>> m_magnetHits;
    mutable std::vector<Candidate> m_seeds;
-    
+
    // Helper methods
    void i_findSeeds      ( const LHCb::Track& seed, const unsigned int seedStation ) const;
-   void i_findVeloSeeds  ( const Candidate& seed,   const unsigned int seedStation ) const;
-   void i_findVeloTTSeeds( const Candidate& seed,   const unsigned int seedStation ) const;
 
    void i_addHits( Candidate& seed ) const;
 
    void i_fitCandidate( Candidate& seed ) const;
 
    void i_clean() const;
- 
+
    const std::vector<Candidate>& seeds() const {
       return m_seeds;
    }
-
-   // FoI formulas
-   double FoIX( const int station, const int region, const double p ) const;
-   double FoIY( const int station, const int region, const double p ) const;
 
    // Interface methods
    void findSeeds( const LHCb::Track& seed, const unsigned int seedStation ) override;
@@ -133,6 +123,18 @@ private:
 
    inline double momentum( const double dtx ) const{
       return m_kickScale / fabs( dtx ) + m_kickOffset;
+   }
+
+
+   inline std::pair<double, double> hitx(const CommonMuonHit& hit, bool correct) const
+   {
+      unsigned int i = (hit.tile().key() == 0 ? 0 : hit.station());
+      return std::make_pair(hit.x(), 2 * hit.dx() * (correct ? m_corX[i] : 1.));
+   }
+   inline std::pair<double, double> hity(const CommonMuonHit& hit, bool correct) const
+   {
+     unsigned int i = (hit.tile().key() == 0 ? 0 : hit.station());
+     return std::make_pair(hit.y(), 2 * hit.dy() * (correct ? m_corY[i] : 1.));
    }
 
 };
