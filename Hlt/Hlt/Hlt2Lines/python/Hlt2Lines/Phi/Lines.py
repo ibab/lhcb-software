88afb0e671d782cebdd2969516c47a4ac17d1074
Jon Harrison <jonathan.harrison@manchester.ac.uk>
2016-03-16 11:17:39 +0000

    Line name changes in Phi lines and addition of header information in Lines.py
    git-svn-id: http://svn.cern.ch/guest/lhcb/Hlt/trunk@203139 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 6c05f84cf9ef0ea4edb60de757902b6fd58a9a5a

--- Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Phi/Lines.py
+++ Hlt/Hlt/Hlt2Lines/python/Hlt2Lines/Phi/Lines.py
@@ -1,3 +1,14 @@
+## @file
+#  HLT2 lines for decays involving one or more phi mesons
+#
+#  @author Jon Harrison jonathan.harrison@manchester.ac.uk, Matthew Kenzie matthew.william.kenzie@cern.ch
+#=============================================================================
+""" HLT2 lines for decays involving one or more phi mesons
+	
+"""
+#=============================================================================
+__author__  = "Jon Harrison jonathan.harrison@manchester.ac.uk, Matthew Kenzie matthew.william.kenzie@cern.ch"
+
 from GaudiKernel.SystemOfUnits import GeV, MeV, mm
 
 from Hlt2Lines.Utilities.Hlt2LinesConfigurableUser import Hlt2LinesConfigurableUser
@@ -5,7 +16,7 @@ class PhiLines(Hlt2LinesConfigurableUser) :
   __slots__ = {'_stages' : {},
                'Prescale' : {},
                'Postscale' : {},
-               'IncPhi' : {'TisTosSpec' : "Hlt1IncPhi.*Decision%TOS" ,
+               'PhiIncPhi' : {'TisTosSpec' : "Hlt1IncPhi.*Decision%TOS" ,
                            'KaonPT'     : 800 * MeV ,
                            'KaonIPS'    : 6 ,
                            'KaonPID'    : 0 ,
@@ -13,18 +24,18 @@ class PhiLines(Hlt2LinesConfigurableUser) :
                            'PhiPT'      : 1800 * MeV,
                            'PhiVCHI2'   : 20 ,
                            'PhiMassWin' : 20 * MeV},
-               'Bs2PhiPhi': {'TisTosSpec' : "Hlt1B2PhiPhi_LTUNB.*Decision%TOS" ,
+               'PhiBs2PhiPhi': {'TisTosSpec' : "Hlt1B2PhiPhi_LTUNB.*Decision%TOS" ,
                             },
                'GoodKs' : {'KS_MASS_WINDOW'   : 20 * MeV,
                            'KS_PT_MIN'        : 400 * MeV,
                            'KS_VCHI2NDOF_MAX' : 4,
                            'GHOSTPROB_MAX'    :       0.35,
                            'KS_FD_MIN'        :      10.0 * mm},
-               'Phi2KsKs' : {'Phi_MASS_WINDOW'   : 70 * MeV,
+               'PhiPhi2KsKs' : {'Phi_MASS_WINDOW'   : 70 * MeV,
                              'Phi_PT_MIN'        : 800 * MeV,
                              'Phi_DOCACHI2_MAX'  : 20,
                              'Phi_VCHI2NDOF_MAX' : 6},
-               'Phi2KsKsD0Ctrl' : {'D0_MASS_WINDOW'   : 70 * MeV,
+               'PhiPhi2KsKsD0Ctrl' : {'D0_MASS_WINDOW'   : 70 * MeV,
                                    'D0_PT_MIN'        : 800 * MeV,
                                    'D0_DOCACHI2_MAX'  : 20,
                                    'D0_VCHI2NDOF_MAX' : 6},
@@ -38,10 +49,10 @@ class PhiLines(Hlt2LinesConfigurableUser) :
         return self._stages
 
     from Stages import IncPhiFilter, Bs2PhiPhiCombiner, Phi2KsKsCombiner, D02KsKsCombiner
-    self._stages = {'IncPhi'         : [IncPhiFilter('IncPhi')],
-                    'Bs2PhiPhi'      : [Bs2PhiPhiCombiner('Bs2PhiPhi')],
-                    'Phi2KsKs'       : [Phi2KsKsCombiner('Phi2KsKs')],
-                    'Phi2KsKsD0Ctrl' : [D02KsKsCombiner('Phi2KsKsD0Ctrl')]}
+    self._stages = {'PhiIncPhi'         : [IncPhiFilter('PhiIncPhi')],
+                    'PhiBs2PhiPhi'      : [Bs2PhiPhiCombiner('PhiBs2PhiPhi')],
+                    'PhiPhi2KsKs'       : [Phi2KsKsCombiner('PhiPhi2KsKs')],
+                    'PhiPhi2KsKsD0Ctrl' : [D02KsKsCombiner('PhiPhi2KsKsD0Ctrl')]}
 
     if nickname:
       return self._stages[nickname]
