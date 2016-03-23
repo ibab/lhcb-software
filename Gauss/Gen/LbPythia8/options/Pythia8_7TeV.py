a8826f210373ca3dd934f5212821f41a58c50308
Gloria Corti <gloria.corti@cern.ch>
2016-03-23 21:10:27 +0000

    add options for cross sections tuning at 7TeV, add confihguration for pileup and spillover to all options
    git-svn-id: http://svn.cern.ch/guest/lhcb/Gauss/trunk@203793 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 3151883e3af4ca5a4ed1c674bba12872bf43111a

--- Gauss/Gen/LbPythia8/options/Pythia8_7TeV.py
+++ Gauss/Gen/LbPythia8/options/Pythia8_7TeV.py
@@ -1,18 +1,18 @@
-from Configurables import Generation, MinimumBias, Pythia8Production
-from Configurables import Inclusive, SignalPlain, SignalRepeatedHadronization
-from Configurables import Special
+from Configurables import Generation
+from Configurables import MinimumBias
+from Configurables import Inclusive
+from Configurables import ( SignalPlain, SignalRepeatedHadronization, Special )
+from Configurables import Pythia8Production
 
-Pythia8TurnOffFragmentation = [ "HadronLevel:all = off" ]
-
-# this is only a preliminary tuning that should probably need to be updated
-Pythia8NewFrag = [  "StringZ:useNonstandardB = on" ,
-                    "StringZ:aNonstandardB = 1.01" ,
-                    "StringZ:bNonstandardB = 0.46" ,
-                    "StringZ:rFactB = 0",
-                    "StringZ:useNonstandardC = on" ,
-                    "StringZ:aNonstandardC = 1.075" ,
-                    "StringZ:bNonstandardC = 0.694" ,
-                    "StringZ:rFactC = 0"]
+commandsTuning_XST_7TeV = [
+    'SigmaTotal:setOwn         = on', 
+    'SigmaTotal:sigmaTot       = 9.407715e+01',
+    'SigmaTotal:sigmaAX        = 7.560716e+00',
+    'SigmaTotal:sigmaXB        = 7.391497e+00',
+    'SigmaTotal:sigmaEl        = 2.946597e+01',
+    'SigmaTotal:sigmaXX        = 1.530803e+00',
+    'SigmaDiffractive:dampen   = off',
+    ]
 
 gen = Generation("Generation")
 
@@ -20,36 +20,36 @@ gen.addTool( MinimumBias , name = "MinimumBias" )
 gen.MinimumBias.ProductionTool = "Pythia8Production"
 gen.MinimumBias.addTool( Pythia8Production , name = "Pythia8Production" )
 gen.MinimumBias.Pythia8Production.Tuning = "LHCbDefault.cmd"
-gen.MinimumBias.Pythia8Production.Commands += Pythia8NewFrag
+gen.MinimumBias.Pythia8Production.Commands += commandsTuning_XST_7TeV
 
 gen.addTool( Inclusive , name = "Inclusive" )
 gen.Inclusive.ProductionTool = "Pythia8Production"
 gen.Inclusive.addTool( Pythia8Production , name = "Pythia8Production" )
 gen.Inclusive.Pythia8Production.Tuning = "LHCbDefault.cmd"
-gen.Inclusive.Pythia8Production.Commands += Pythia8NewFrag
+gen.MinimumBias.Pythia8Production.Commands += commandsTuning_XST_7TeV
 
 gen.addTool( SignalPlain , name = "SignalPlain" )
 gen.SignalPlain.ProductionTool = "Pythia8Production"
 gen.SignalPlain.addTool( Pythia8Production , name = "Pythia8Production" )
 gen.SignalPlain.Pythia8Production.Tuning = "LHCbDefault.cmd"
-gen.SignalPlain.Pythia8Production.Commands += Pythia8NewFrag
+gen.SignalPlain.Pythia8Production.Commands += commandsTuning_XST_7TeV
 
 gen.addTool( SignalRepeatedHadronization , name = "SignalRepeatedHadronization" )
 gen.SignalRepeatedHadronization.ProductionTool = "Pythia8Production"
 gen.SignalRepeatedHadronization.addTool( Pythia8Production , name = "Pythia8Production" )
 gen.SignalRepeatedHadronization.Pythia8Production.Tuning = "LHCbDefault.cmd"
 gen.SignalRepeatedHadronization.Pythia8Production.Commands += Pythia8TurnOffFragmentation
-gen.SignalRepeatedHadronization.Pythia8Production.Commands += Pythia8NewFrag
+gen.SignalRepeatedHadronization.Pythia8Production.Commands += commandsTuning_XST_7TeV
 
 gen.addTool( Special , name = "Special" )
 gen.Special.ProductionTool = "Pythia8Production"
 gen.Special.addTool( Pythia8Production , name = "Pythia8Production" )
 gen.Special.Pythia8Production.Tuning = "LHCbDefault.cmd"
-gen.Special.Pythia8Production.Commands += Pythia8NewFrag
+gen.Special.Pythia8Production.Commands += commandsTuning_XST_7TeV
 gen.Special.PileUpProductionTool = "Pythia8Production/Pythia8PileUp"
 gen.Special.addTool(Pythia8Production, name = "Pythia8PileUp")
 gen.Special.Pythia8PileUp.Tuning = "LHCbDefault.cmd"
-gen.Special.Pythia8PileUp.Commands += Pythia8NewFrag
+gen.Special.Pythia8PileUp.Commands += commandsTuning_XST_7TeV
 gen.Special.ReinitializePileUpGenerator  = False
 
 # Use same generator and configuration for spillover
@@ -60,6 +60,9 @@ for slot in spillOverList:
     genSlot.addTool(MinimumBias, name = "MinimumBias")
     genSlot.MinimumBias.ProductionTool = "Pythia8Production"
     genSlot.MinimumBias.addTool(Pythia8Production, name = "Pythia8Production")
-    genSlot.MinimumBias.Pythia8Production.Commands += Pythia8NewFrag
+    genSlot.MinimumBias.Pythia8Production.Commands += commandsTuning_XST_7TeV
     genSlot.MinimumBias.Pythia8Production.Tuning = "LHCbDefault.cmd"
+
+
+
 
