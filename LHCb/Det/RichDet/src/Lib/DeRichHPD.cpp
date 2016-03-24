8d951d5d2dbec87881d5dd2c898a4c16de643735
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2016-03-24 09:29:42 +0000

    - Extend the range of data passed to the magnification interpolators in    DeRichHPD to avoid subsequent attempts to interpolate outside the valid    range, which causes problems with GSL 2.x.
    git-svn-id: http://svn.cern.ch/guest/lhcb/LHCb/trunk@203810 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: fb0df3f1fa3cb96a09f3031fad916f486cc0f072

--- LHCb/Det/RichDet/src/Lib/DeRichHPD.cpp
+++ LHCb/Det/RichDet/src/Lib/DeRichHPD.cpp
@@ -41,7 +41,8 @@ namespace
 {
   static const std::string& XmlHpdDemagPath =
     "/dd/Materials/RichMaterialTabProperties/RichHpdDemag";
-  static const int totbins = 50; // do not change !!!
+  static const int simtotbins = 50; // do not change !!!
+  static const int rectotbins = 60;
 }
 
 //=============================================================================
@@ -432,7 +433,7 @@ StatusCode DeRichHPD::fillHpdDemagTable(const unsigned int field)
   }
   TabulatedProperty::Table & simTable = dem->table();
   simTable.clear();
-  simTable.reserve(totbins+1);
+  simTable.reserve(simtotbins+1);
 
   // working data tables, used to initialise the interpolators
   std::map<double,double> tableR, tablePhi;
@@ -463,11 +464,11 @@ StatusCode DeRichHPD::fillHpdDemagTable(const unsigned int field)
   BLong = m_LongitudinalBField;
 
   // Simulation from cathode->anode
-  for ( int i = 0; i < totbins+1; ++i )
+  for ( int i = 0; i < simtotbins+1; ++i )
   {
 
     double r_anode(0), phi_anode(0);
-    const double r_cathode = m_activeRadius/totbins * (double)i;
+    const double r_cathode = (m_activeRadius/simtotbins) * (double)i;
 
     if ( m_UseBFieldTestMap )
     {
@@ -576,20 +577,21 @@ StatusCode DeRichHPD::fillHpdMagTable( const unsigned int field )
 
     // Scale r1
     const double Rscale = ( R_data > 0.1 && R_MDMS > 0.1 ? R_data / R_MDMS : 1.0 );
-    if ( msgLevel(MSG::DEBUG) )
-      debug() << "R_data = " << R_data << " R_MDMS = " << R_MDMS 
+    _ri_debug << "R_data = " << R_data << " R_MDMS = " << R_MDMS 
               << " Scale = " << Rscale 
               << endmsg;
     r_a1 *= Rscale;
 
   }
 
+  // image size. Scale by 20% to add some leaway to the interpolator tables
+  const auto halfL = 1.2 * std::min(m_siliconHalfLengthX,m_siliconHalfLengthY);
+
   // Reconstruction from anode->cathode
-  for ( int i = 0; i < totbins+1; ++i )
+  for ( int i = 0; i < rectotbins+1; ++i )
   {
 
-    const double r_anode =
-      std::min(m_siliconHalfLengthX,m_siliconHalfLengthY)/totbins * (double)i;
+    const double r_anode = (halfL/rectotbins) * (double)i;
     double r_cathode(0), phi_cathode(0);
 
     if ( m_UseBFieldTestMap )
@@ -669,7 +671,7 @@ StatusCode DeRichHPD::magnifyToGlobalMagnetON( Gaudi::XYZPoint& detectPoint,
   }
   else
   {
-    rCathode = magnification_RtoR(field)->value( rAnode );
+    rCathode = magnification_RtoR(field)->value(rAnode);
   }
 
   // check if this point could have come from the photoCathode
