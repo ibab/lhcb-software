484103e1b7a4e254e60c7361b4799375eeeedaf9
Diego Martinez Santos <diego.martinez.santos@cern.ch>
2016-03-30 13:32:43 +0000

    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@203987 4525493e-7705-40b1-a816-d608a930855b
    Former-commit-id: 176e6153684d2c941f2007e73ff4ea15fd7bb79b

--- Rec/Muon/MuonID/python/MuonID/__init__.py
+++ Rec/Muon/MuonID/python/MuonID/__init__.py
@@ -1,7 +1,7 @@
 """
 """
-import os
-print "importing from ", os.path.abspath(__path__[0])
+#import os
+#print "importing from ", os.path.abspath(__path__[0])
 
-from GaudiPython.Bindings import gbl
-IMuonIDTool = gbl.IMuonIDTool
+#from GaudiPython.Bindings import gbl
+#IMuonIDTool = gbl.IMuonIDTool
