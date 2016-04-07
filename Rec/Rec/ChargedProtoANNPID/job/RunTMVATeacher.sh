05786231c7bc79bdaf128ab5b481d06ba74f25c3
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2016-04-07 07:16:28 +0000

    update scripts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@204264 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 366e25d2d6ba0c522f7a3d184aead810b383b466

--- Rec/Rec/ChargedProtoANNPID/job/RunTMVATeacher.sh
+++ Rec/Rec/ChargedProtoANNPID/job/RunTMVATeacher.sh
@@ -2,8 +2,10 @@
 
 # Data files
 export ROOTDIR=/r03/lhcb/jonesc/ANNPID/ProtoParticlePIDtuples/Test/V1/StripFiltered
-export SIGNALFILE=${ROOTDIR}"/2015-S23r1-ANNPID-BHADRONCOMPLETEEVENT.root:ANNPID/DecayTree"
-export BCKGRDFILE=${ROOTDIR}"/Incb-DevMCJun2015-S23r1-ANNPID-Pythia8.root:ANNPID/DecayTree"
+#export SIGNALFILE=${ROOTDIR}"/2015-S23r1-ANNPID-BHADRONCOMPLETEEVENT.root:ANNPID/DecayTree"
+#export BCKGRDFILE=${ROOTDIR}"/Incb-DevMCJun2015-S23r1-ANNPID-Pythia8.root:ANNPID/DecayTree"
+export SIGNALFILE=${ROOTDIR}"/2012-S20-ANNPID-BHADRONCOMPLETEEVENT.root:ANNPID/DecayTree"
+export BCKGRDFILE=${ROOTDIR}"/Incb-MC2012-S20-ANNPID-Pythia8.root:ANNPID/DecayTree"
 
 # Run the teacher
 TMVATeacher.exe --TrainingParameters TMVATraining.txt --Inputs TMVAInputs.txt --SignalTrainingFile $SIGNALFILE --BckgTrainingFile $BCKGRDFILE 2>&1 | tee TMVATrain.log
