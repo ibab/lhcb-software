306f4ed51219578297efba1b0508182c98cc8432
Christopher Rob Jones <jonesc@hep.phy.cam.ac.uk>
2016-04-07 15:56:37 +0000

    update scripts
    git-svn-id: http://svn.cern.ch/guest/lhcb/Rec/trunk@204305 4525493e-7705-40b1-a816-d608a930855b
    
    Former-commit-id: 40f6d9398525f55dc81e245d12869af368702536

--- Rec/Rec/ChargedProtoANNPID/job/AddANNReweighting.sh
+++ Rec/Rec/ChargedProtoANNPID/job/AddANNReweighting.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-export INFILE=/r03/lhcb/jonesc/ANNPID/ProtoParticlePIDtuples/Test/V1/StripFiltered/2015-S23r1-ANNPID-BHADRONCOMPLETEEVENT.root
-export OUTFILE=/r03/lhcb/jonesc/ANNPID/ProtoParticlePIDtuples/Test/V1/StripFiltered/WithMVAs/V1/2015-S23r1-ANNPID-BHADRONCOMPLETEEVENT.root
+export ROOTDIR="/r03/lhcb/jonesc/ANNPID/ProtoParticlePIDtuples/Test/V1/StripFiltered/"
 
-TMVAReader.exe --TreeName ANNPID/DecayTree --InRootFiles ${INFILE} --OutRootFile ${OUTFILE} --WorkDir output-100k/weights --Inputs TMVAInputs.txt
+export WORKDIR="output-100k/weights"
+export MVADIR=${ROOTDIR}"WithMVAs/V1/"
+
+export FILE="2015-S23r1-ANNPID-BHADRONCOMPLETEEVENT.root"
+TMVAReader.exe --TreeName ANNPID/DecayTree --InRootFiles ${ROOTDIR}${FILE} --OutRootFile ${MVADIR}${FILE} --WorkDir ${WORKDIR} --Inputs TMVAInputs.txt
+
+export FILE="Incb-DevMCJun2015-S23r1-ANNPID-Pythia8.root"
+TMVAReader.exe --TreeName ANNPID/DecayTree --InRootFiles ${ROOTDIR}${FILE} --OutRootFile ${MVADIR}${FILE} --WorkDir ${WORKDIR} --Inputs TMVAInputs.txt
 
 exit 0
